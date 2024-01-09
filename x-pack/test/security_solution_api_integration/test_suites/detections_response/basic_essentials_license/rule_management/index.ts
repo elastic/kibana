67eac2e585d406858b5086246777cea1c02c74ac
Davis Plumlee <56367316+dplumlee@users.noreply.github.com>
2024-01-09 13:09:40 -0600

    [Security Solution] Adds tests for coverage overview page (#168058)
    **Resolves: https://github.com/elastic/kibana/issues/162250**
    
    ## Summary
    
    Adds remaining unit, api integration, and e2e cypress tests for the
    coverage overview page in accordance to the [existing test
    plan](https://github.com/elastic/kibana/blob/main/x-pack/plugins/security_solution/docs/testing/test_plans/detection_response/rule_management/coverage_overview_dashboard.md)
    
    - [Flaky test runner
    build](https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/4756)
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/basic_essentials_license/rule_management/index.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/basic_essentials_license/rule_management/index.ts
@@ -4,7 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { FtrProviderContext } from '../../../../ftr_provider_context';
 
-export const RULES_MANAGEMENT_URL = '/app/security/rules/management';
-export const RULES_MONITORING_URL = '/app/security/rules/monitoring';
-export const RULES_COVERAGE_OVERVIEW_URL = '/app/security/rules_coverage_overview';
+export default function ({ loadTestFile }: FtrProviderContext) {
+  describe('Rule management API', function () {
+    loadTestFile(require.resolve('./coverage_overview'));
+  });
+}
