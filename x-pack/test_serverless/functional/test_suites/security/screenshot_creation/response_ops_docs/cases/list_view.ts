65fb5a2c303b4106a56fd519de07ba5a7cc88366
lcawl <lcawley@elastic.co>
2024-01-09 12:59:14 -0800

    [Cases] Automate security serverless screenshots

---
+++ x-pack/test_serverless/functional/test_suites/security/screenshot_creation/response_ops_docs/cases/list_view.ts
@@ -0,0 +1,64 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { SECURITY_SOLUTION_OWNER } from '@kbn/cases-plugin/common';
+import { FtrProviderContext } from '../../../../../ftr_provider_context';
+import { navigateToCasesApp } from '../../../../../../shared/lib/cases';
+
+export default function ({ getPageObject, getPageObjects, getService }: FtrProviderContext) {
+  const pageObjects = getPageObjects(['common', 'header', 'svlCommonPage']);
+  const svlCases = getService('svlCases');
+  const svlCommonScreenshots = getService('svlCommonScreenshots');
+  const screenshotDirectories = ['response_ops_docs', 'security_cases'];
+  const owner = SECURITY_SOLUTION_OWNER;
+
+  describe('list view', function () {
+    // before(async () => {
+    //   await svlCases.api.createCase(
+    //     svlCases.api.getPostCaseRequest(owner, {
+    //       title: 'Metrics inventory',
+    //       tags: ['IBM resilient'],
+    //       description: 'Test.',
+    //       owner,
+    //     })
+    //   );
+
+    //   await svlCases.api.createCase(
+    //     svlCases.api.getPostCaseRequest(owner, {
+    //       title: 'Logs threshold',
+    //       tags: ['jira'],
+    //       description: 'Test.',
+    //       owner,
+    //     })
+    //   );
+
+    //   await svlCases.api.createCase(
+    //     svlCases.api.getPostCaseRequest(owner, {
+    //       title: 'Monitor uptime',
+    //       tags: ['swimlane'],
+    //       description: 'Test.',
+    //       owner,
+    //     })
+    //   );
+    // });
+
+    after(async () => {
+      await svlCases.api.deleteAllCaseItems();
+      await pageObjects.svlCommonPage.forceLogout();
+    });
+
+    beforeEach(async () => {
+      await pageObjects.svlCommonPage.login();
+    });
+
+    it('cases list screenshot', async () => {
+      await navigateToCasesApp(getPageObject, getService, owner);
+      await pageObjects.header.waitUntilLoadingHasFinished();
+      await svlCommonScreenshots.takeScreenshot('cases-home-page', screenshotDirectories);
+    });
+  });
+}
