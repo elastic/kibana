b5353fbf2bed3482ff4733e66d31b795fdc5a1bd
Ying Mao <ying.mao@elastic.co>
2024-01-09 11:23:49 -0500

    [Response Ops][Alerting] AAD for alerting example rules (#174032)
    Towards https://github.com/elastic/response-ops-team/issues/164
    
    Registering alerting example rules with framework AAD. This creates a
    new alerts index `.alerts-default.alerts-default` that will eventually
    hold alerts for all rules that have not customized their registration.
    This index contains only the mappings for the basic alerts as data
    documents, no custom context or payload fields.
    
    Run kibana using `--run-examples` flag. Create one of the example rule
    types and let it alert and then resolve and see an alert document get
    created in the `.alerts-default.alerts-default` index.
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/alerting/server/index.ts
+++ x-pack/plugins/alerting/server/index.ts
@@ -34,6 +34,8 @@ export type {
   GetViewInAppRelativeUrlFnOpts,
   DataStreamAdapter,
 } from './types';
+export { DEFAULT_AAD_CONFIG } from './types';
+export { RULE_SAVED_OBJECT_TYPE } from './saved_objects';
 export { RuleNotifyWhen } from '../common';
 export { DEFAULT_MAX_EPHEMERAL_ACTIONS_PER_ALERT } from './config';
 export type { PluginSetupContract, PluginStartContract } from './plugin';
