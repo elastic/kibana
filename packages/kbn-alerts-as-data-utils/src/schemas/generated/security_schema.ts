6ebdcb1e32c5eb2bb7f08a4b365296ac20f6ef89
Nikita Khristinin <nkhristinin@gmail.com>
2024-01-11 13:33:53 +0100

    Add fields to shema

--- packages/kbn-alerts-as-data-utils/src/schemas/generated/security_schema.ts
+++ packages/kbn-alerts-as-data-utils/src/schemas/generated/security_schema.ts
@@ -121,6 +121,9 @@ const SecurityAlertOptional = rt.partial({
   'ecs.version': schemaString,
   'event.action': schemaString,
   'event.kind': schemaString,
+  'host.asset.criticality': schemaString,
+  'host.risk.calculated_level': schemaString,
+  'host.risk.calculated_score_norm': schemaNumber,
   'kibana.alert.action_group': schemaString,
   'kibana.alert.ancestors.rule': schemaString,
   'kibana.alert.building_block_type': schemaString,
@@ -203,6 +206,9 @@ const SecurityAlertOptional = rt.partial({
   'kibana.alert.workflow_user': schemaString,
   'kibana.version': schemaString,
   tags: schemaStringArray,
+  'user.asset.criticality': schemaString,
+  'user.risk.calculated_level': schemaString,
+  'user.risk.calculated_score_norm': schemaNumber,
 });
 
 // prettier-ignore
