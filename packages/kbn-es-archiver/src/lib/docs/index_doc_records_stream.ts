6d14826c9a1d16bb5a53fde26a715b8282c5d31b
Tre' Seymour <wayne.seymour@elastic.co>
2024-01-11 10:49:45 -0600

    Back to 4 per slack conversation

--- packages/kbn-es-archiver/src/lib/docs/index_doc_records_stream.ts
+++ packages/kbn-es-archiver/src/lib/docs/index_doc_records_stream.ts
@@ -33,7 +33,7 @@ export function createIndexDocRecordsStream(
     await client.helpers.bulk(
       {
         retries: 5,
-        concurrency: performance?.concurrency || 5,
+        concurrency: performance?.concurrency || 4,
         datasource: docs.map((doc) => {
           const body = doc.source;
           const op = doc.data_stream ? BulkOperation.Create : operation;
