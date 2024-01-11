088d5c7854fc0c1dbbdbe32f594b4d04ddd0311f
James Gowdy <jgowdy@elastic.co>
2024-01-11 17:09:21 +0000

    removing unused code

--- x-pack/plugins/data_visualizer/public/application/file_data_visualizer/components/doc_count_chart/axes.tsx
+++ x-pack/plugins/data_visualizer/public/application/file_data_visualizer/components/doc_count_chart/axes.tsx
@@ -7,7 +7,6 @@
 
 import React, { FC, Fragment } from 'react';
 import { Axis, Position, timeFormatter, niceTimeFormatByDay } from '@elastic/charts';
-import { getYRange } from './utils';
 import type { LineChartPoint } from './event_rate_chart';
 
 const dateFormatter = timeFormatter(niceTimeFormatByDay(3));
@@ -36,3 +35,32 @@ export const Axes: FC<Props> = ({ chartData }) => {
     </Fragment>
   );
 };
+
+function getYRange(chartData?: LineChartPoint[]) {
+  const fit = false;
+
+  if (chartData === undefined) {
+    return { fit, min: NaN, max: NaN };
+  }
+
+  if (chartData.length === 0) {
+    return { min: 0, max: 0, fit };
+  }
+
+  let max: number = Number.MIN_VALUE;
+  let min: number = Number.MAX_VALUE;
+  chartData.forEach((r) => {
+    max = Math.max(r.value, max);
+    min = Math.min(r.value, min);
+  });
+
+  const padding = (max - min) * 0.1;
+  max += padding;
+  min -= padding;
+
+  return {
+    min,
+    max,
+    fit,
+  };
+}
