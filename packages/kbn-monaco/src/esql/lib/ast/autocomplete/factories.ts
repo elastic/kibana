e464794b3d27fbf376bf19887e1739586ed523f5
dej611 <dej611@gmail.com>
2024-01-11 11:56:26 +0100

    :bug: quote fields with special chars

--- packages/kbn-monaco/src/esql/lib/ast/autocomplete/factories.ts
+++ packages/kbn-monaco/src/esql/lib/ast/autocomplete/factories.ts
@@ -27,6 +27,10 @@ export const TRIGGER_SUGGESTION_COMMAND = {
   id: 'editor.action.triggerSuggest',
 };
 
+function getSafeInsertText(text: string) {
+  return /[^a-zA-Z\d_\.@]/.test(text) ? `\`${text}\`` : text;
+}
+
 export function getAutocompleteFunctionDefinition(fn: FunctionDefinition) {
   const fullSignatures = getFunctionSignatures(fn);
   return {
@@ -104,7 +108,7 @@ export function getAutocompleteCommandDefinition(
 export const buildFieldsDefinitions = (fields: string[]): AutocompleteCommandDefinition[] =>
   fields.map((label) => ({
     label,
-    insertText: label,
+    insertText: getSafeInsertText(label),
     kind: 4,
     detail: i18n.translate('monaco.esql.autocomplete.fieldDefinition', {
       defaultMessage: `Field specified by the input table`,
@@ -115,7 +119,7 @@ export const buildFieldsDefinitions = (fields: string[]): AutocompleteCommandDef
 export const buildVariablesDefinitions = (variables: string[]): AutocompleteCommandDefinition[] =>
   variables.map((label) => ({
     label,
-    insertText: /[^a-zA-Z\d]/.test(label) ? `\`${label}\`` : label,
+    insertText: getSafeInsertText(label),
     kind: 4,
     detail: i18n.translate('monaco.esql.autocomplete.variableDefinition', {
       defaultMessage: `Variable specified by the user within the ES|QL query`,
@@ -126,7 +130,7 @@ export const buildVariablesDefinitions = (variables: string[]): AutocompleteComm
 export const buildSourcesDefinitions = (sources: string[]): AutocompleteCommandDefinition[] =>
   sources.map((label) => ({
     label,
-    insertText: label,
+    insertText: getSafeInsertText(label),
     kind: 21,
     detail: i18n.translate('monaco.esql.autocomplete.sourceDefinition', {
       defaultMessage: `Input table`,
