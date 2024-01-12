71777df76638f0a025d468adf726c0749ea8c4d2
Paulo Henrique <paulo.henrique@elastic.co>
2024-01-12 14:10:37 -0800

    share renderers grouping components

--- x-pack/plugins/cloud_security_posture/public/pages/vulnerabilities/latest_vulnerabilities_group_renderer.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/vulnerabilities/latest_vulnerabilities_group_renderer.tsx
@@ -8,88 +8,20 @@ import {
   EuiBadge,
   EuiFlexGroup,
   EuiFlexItem,
-  EuiIconTip,
-  EuiSkeletonTitle,
   EuiText,
   EuiTextBlockTruncate,
   EuiToolTip,
   useEuiTheme,
 } from '@elastic/eui';
 import { css } from '@emotion/react';
-import {
-  ECSField,
-  GroupPanelRenderer,
-  RawBucket,
-  StatRenderer,
-} from '@kbn/securitysolution-grouping/src';
+import { GroupPanelRenderer, RawBucket, StatRenderer } from '@kbn/securitysolution-grouping/src';
 import React from 'react';
-import { FormattedMessage } from '@kbn/i18n-react';
 import { VulnerabilitiesGroupingAggregation } from './hooks/use_grouped_vulnerabilities';
 import { GROUPING_OPTIONS } from './constants';
 import { VULNERABILITIES_GROUPING_COUNTER } from './test_subjects';
 import { NULL_GROUPING_MESSAGES, NULL_GROUPING_UNIT, VULNERABILITIES } from './translations';
 import { getAbbreviatedNumber } from '../../common/utils/get_abbreviated_number';
-
-/**
- * Return first non-null value. If the field contains an array, this will return the first value that isn't null. If the field isn't an array it'll be returned unless it's null.
- */
-export function firstNonNullValue<T>(valueOrCollection: ECSField<T>): T | undefined {
-  if (valueOrCollection === null) {
-    return undefined;
-  } else if (Array.isArray(valueOrCollection)) {
-    for (const value of valueOrCollection) {
-      if (value !== null) {
-        return value;
-      }
-    }
-  } else {
-    return valueOrCollection;
-  }
-}
-
-const NullGroupComponent = ({
-  title,
-  field,
-  unit = NULL_GROUPING_UNIT,
-}: {
-  title: string;
-  field: string;
-  unit?: string;
-}) => {
-  return (
-    <EuiFlexGroup alignItems="center" gutterSize="xs">
-      <strong>{title}</strong>
-      <EuiIconTip
-        anchorProps={{
-          css: css`
-            display: inline-flex;
-          `,
-        }}
-        content={
-          <>
-            <FormattedMessage
-              id="xpack.csp.vulnerabilities.grouping.nullGroupTooltip"
-              defaultMessage="The selected {groupingTitle} field, {field} is missing a value for this group of {unit}."
-              values={{
-                groupingTitle: (
-                  <strong>
-                    <FormattedMessage
-                      id="xpack.csp.vulnerabilities.grouping.nullGroupTooltip.groupingTitle"
-                      defaultMessage="group by"
-                    />
-                  </strong>
-                ),
-                field: <code>{field}</code>,
-                unit,
-              }}
-            />
-          </>
-        }
-        position="right"
-      />
-    </EuiFlexGroup>
-  );
-};
+import { LoadingGroup, NullGroup } from '../../components/cloud_security_grouping';
 
 export const groupPanelRenderer: GroupPanelRenderer<VulnerabilitiesGroupingAggregation> = (
   selectedGroup,
@@ -98,19 +30,17 @@ export const groupPanelRenderer: GroupPanelRenderer<VulnerabilitiesGroupingAggre
   isLoading
 ) => {
   if (isLoading) {
-    return (
-      <EuiSkeletonTitle size="s" isLoading={true}>
-        <FormattedMessage
-          id="xpack.csp.vulnerabilities.grouping.loadingGroupPanelTitle"
-          defaultMessage="Loading"
-        />
-      </EuiSkeletonTitle>
-    );
+    return <LoadingGroup />;
   }
+
+  const renderNullGroup = (title: string) => (
+    <NullGroup title={title} field={selectedGroup} unit={NULL_GROUPING_UNIT} />
+  );
+
   switch (selectedGroup) {
     case GROUPING_OPTIONS.RESOURCE_NAME:
       return nullGroupMessage ? (
-        <NullGroupComponent title={NULL_GROUPING_MESSAGES.RESOURCE_NAME} field={selectedGroup} />
+        renderNullGroup(NULL_GROUPING_MESSAGES.RESOURCE_NAME)
       ) : (
         <EuiFlexGroup alignItems="center">
           <EuiFlexItem>
@@ -138,7 +68,7 @@ export const groupPanelRenderer: GroupPanelRenderer<VulnerabilitiesGroupingAggre
       );
     default:
       return nullGroupMessage ? (
-        <NullGroupComponent title={NULL_GROUPING_MESSAGES.DEFAULT} field={selectedGroup} />
+        renderNullGroup(NULL_GROUPING_MESSAGES.DEFAULT)
       ) : (
         <EuiFlexGroup alignItems="center">
           <EuiFlexItem>
