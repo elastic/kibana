6ea56180cf985177be47b88e56f2c3d5065379dc
Kevin Qualters <56408403+kqualters-elastic@users.noreply.github.com>
2024-01-12 12:10:20 -0500

    [Security Solution] [Timeline] Delete saved searches on timeline delete, prevent creating double saved searches (#174562)
    ## Summary
    
    This pr fixes 2 issues currently present in the timeline es|ql via
    discover integration: first, when timelines are deleted, the saved
    object ids associated with those timelines that point to saved searches
    are currently not deleted. If a user deletes a timeline and then tries
    to create another with the same name, the esql is not able to be
    persisted because no new saved search saved object can be created, as
    the name of the old one collides with the new one. This pr fixes that by
    deleting the saved search saved object on delete, although the issue
    with timeline allowing multiple timelines with the same name while saved
    search does not will remain an issue, need to use the onDuplicateTitle
    callback that is currently a no-op to handle this here
    https://github.com/elastic/kibana/blob/main/x-pack/plugins/security_solution/public/common/components/discover_in_timeline/use_discover_in_timeline_actions.tsx#L213
    . Second, the use of react-query useMutation that creates the saved
    search saved object will no longer create two, as it currently does, as
    we check the status of the request in the useEffect that fires the
    request. This might be a race condition bug with the saved search saved
    objects client too, as normally objects are not supposed to be created
    with the same name,
    https://github.com/elastic/kibana/blob/main/src/plugins/saved_search/public/services/saved_searches/save_saved_searches.ts#L68,
    but I'm not sure.
    
    ### Checklist
    
    - [x]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    
    (cherry picked from commit 7b69611234ef7979f9f00df14609aeba40a66d62)
    
    # Conflicts:
    #	x-pack/plugins/security_solution/public/timelines/components/open_timeline/types.ts

--- x-pack/plugins/security_solution/public/timelines/components/open_timeline/types.ts
+++ x-pack/plugins/security_solution/public/timelines/components/open_timeline/types.ts
@@ -59,6 +59,7 @@ export interface OpenTimelineResult {
   pinnedEventIds?: Readonly<Record<string, boolean>> | null;
   queryType?: { hasEql: boolean; hasQuery: boolean };
   savedObjectId?: string | null;
+  savedSearchId?: string | null;
   status?: TimelineStatus | null;
   title?: string | null;
   templateTimelineId?: string | null;
@@ -77,7 +78,7 @@ export interface EuiSearchBarQuery {
 }
 
 /** Performs IO to delete the specified timelines */
-export type DeleteTimelines = (timelineIds: string[], variables?: AllTimelinesVariables) => void;
+export type DeleteTimelines = (timelineIds: string[], searchIds?: string[]) => void;
 
 /** Invoked when the user clicks the action create rule from timeline */
 export type OnCreateRuleFromTimeline = (savedObjectId: string) => void;
