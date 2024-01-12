b26ee12985a047d4f21cc4cd80a29095dca8fb66
Tiago Costa <tiago.costa@elastic.co>
2024-01-12 02:16:37 +0000

    skip flaky suite (#171178)

--- x-pack/plugins/cases/public/components/markdown_editor/editable_markdown_renderer.test.tsx
+++ x-pack/plugins/cases/public/components/markdown_editor/editable_markdown_renderer.test.tsx
@@ -61,6 +61,7 @@ const defaultProps = {
 };
 
 // FLAKY: https://github.com/elastic/kibana/issues/171177
+// FLAKY: https://github.com/elastic/kibana/issues/171178
 describe.skip('EditableMarkdown', () => {
   const MockHookWrapperComponent: React.FC<{ testProviderProps?: unknown }> = ({
     children,
