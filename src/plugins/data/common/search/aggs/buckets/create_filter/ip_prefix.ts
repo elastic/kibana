2e09727084e007e57fa0017339079848e0dfbf53
Jake Smith <jake@metactf.com>
2024-01-13 22:02:32 -0500

    Fix a couple small grammatical mistakes in a code comment

--- src/plugins/data/common/search/aggs/buckets/create_filter/ip_prefix.ts
+++ src/plugins/data/common/search/aggs/buckets/create_filter/ip_prefix.ts
@@ -15,13 +15,14 @@ export const createFilterIpPrefix = (aggConfig: IBucketAggConfig, key: IpPrefixK
   let ipAddress = key.address;
 
   /*
-   * Can occurs when both IPv4 and IPv6 addresses are both in the field being
+   * Can occur when both IPv4 and IPv6 addresses are in the field being
    * aggregated. When prefix_length is < 96, ES will group all IPv4 addresses
    * into an IPv6 address and display that as the key, thus no mapping is required.
    * Per RFC 4038 section 4.2, the IPv6 address ::FFFF:x.y.z.w represents the IPv4
    * address x.y.z.w. Therefore, if they key is an IPv4 address (e.g. it contains
-   * a dot) and the requested prefix is >= 96, then appending ::ffff: will properly
-   * map the IPv4 address to IPv6 according to the RFC mentioned above.
+   * a dot) and the requested prefix is >= 96, then prepending ::ffff: will properly
+   * map the IPv4 address to IPv6 according to the RFC mentioned above which will
+   * enable proper filtering on the visualization.
    */
   if (ipAddress.includes('.') && key.prefix_length >= 96) {
     ipAddress = '::ffff:' + key.address;
