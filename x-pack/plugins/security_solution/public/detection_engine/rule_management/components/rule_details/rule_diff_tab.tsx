a74458d18948d64af424de08804d3ddf72983670
Nikita Indik <nikita.indik@elastic.co>
2024-01-15 17:33:38 +0100

    Hide more fields

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_diff_tab.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_diff_tab.tsx
@@ -16,6 +16,7 @@ import {
   EuiTitle,
   EuiIconTip,
 } from '@elastic/eui';
+import { parseDuration } from '@kbn/alerting-plugin/common';
 import type { RuleResponse } from '../../../../../common/api/detection_engine/model/rule_schema/rule_schemas.gen';
 import { DiffView } from './json_diff/diff_view';
 import * as i18n from './json_diff/translations';
@@ -24,11 +25,32 @@ const sortAndStringifyJson = (jsObject: Record<string, unknown>): string =>
   stringify(jsObject, { space: 2 });
 
 const HIDDEN_PROPERTIES = [
-  'revision',
-  'output_index',
+  /* Generates diff when the rule is enabled/disabled by user */
   'enabled',
+
+  /* Never present in prebuilt rule updates. Generates diff if user adds exceptions to installed rule. */
   'exceptions_list',
+
+  /* Techical property. Can be confused with "version" by users. */
+  'revision',
+
+  /* Not used anymore, but gets defaulted to an empty string when the rule is updated */
+  'output_index',
+
+  /* Never present in prebuilt rule updates. Gets added to installed rule after execution. */
   'execution_summary',
+
+  /* Never present in prebuilt rule updates. These get added to installed rule once user saves the rule after editing. */
+  'actions',
+  'meta',
+  'filters',
+  'timestamp_override_fallback_disabled',
+
+  /* Not relevant to the upgrade flow. */
+  'updated_at',
+  'updated_by',
+  'created_at',
+  'created_by',
 ];
 
 interface RuleDiffTabProps {
@@ -38,7 +60,23 @@ interface RuleDiffTabProps {
 
 export const RuleDiffTab = ({ oldRule, newRule }: RuleDiffTabProps) => {
   const [oldSource, newSource] = useMemo(() => {
-    const visibleOldRuleProperties = omit(oldRule, ...HIDDEN_PROPERTIES);
+    let oldRuleFrom = oldRule.from;
+
+    if (
+      oldRule.from.startsWith('now-') &&
+      newRule.from.startsWith('now-') &&
+      parseDuration(oldRule.from.slice('now-'.length)) ===
+        parseDuration(newRule.from.slice('now-'.length))
+    ) {
+      /* 
+        We want to avoid showing the diff for the `from` property when duration is the same.
+        There are cases where different time units are used to represent the same duration.
+        For example, `now-1m` and `now-60s` both represent the same duration of 1 minute.
+      */
+      oldRuleFrom = newRule.from;
+    }
+
+    const visibleOldRuleProperties = { ...omit(oldRule, ...HIDDEN_PROPERTIES), from: oldRuleFrom };
     const visibleNewRuleProperties = omit(newRule, ...HIDDEN_PROPERTIES);
 
     return [
