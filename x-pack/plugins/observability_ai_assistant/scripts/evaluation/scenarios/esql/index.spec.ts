8d9a6331c208476634b38a28d5e4fd576c2093a3
almudenasanz <almudena.sanz@elastic.co>
2024-01-15 18:53:30 +0100

    improve esql tests

--- x-pack/plugins/observability_ai_assistant/scripts/evaluation/scenarios/esql/index.spec.ts
+++ x-pack/plugins/observability_ai_assistant/scripts/evaluation/scenarios/esql/index.spec.ts
@@ -53,9 +53,9 @@ async function evaluateEsqlQuery({
   const evaluation = await chatClient.evaluate(conversation, [
     ...(expected
       ? [
-          `Returns a ES|QL query that is functionally equivalent to:      
+        `Returns a ES|QL query that is functionally equivalent to:      
       ${expected}`,
-        ]
+      ]
       : []),
     ...(execute && expected ? [`The query successfully executed without an error`] : []),
     ...criteria,
@@ -135,6 +135,15 @@ describe('ES|QL query generation', () => {
             },
           },
         });
+
+        await esClient.index({
+          index: 'employees',
+          document: {
+            hire_date: "2099-11-15T13:12:00",
+            emp_no: 1,
+            salary: 100
+          },
+        });
       });
 
       it('five earliest employees', async () => {
@@ -191,23 +200,11 @@ describe('ES|QL query generation', () => {
     it('postgres avg duration', async () => {
       await evaluateEsqlQuery({
         question:
-          'extract the query duration from postgres log messages in postgres-logs*, using ECS fields, and calculate the avg',
+          'generate an esql query to extract the query duration from postgres log messages in postgres-logs*, using ECS fields, and calculate the avg',
         expected: `FROM postgres-logs
         | DISSECT message "%{} duration: %{query_duration} ms"
         | EVAL query_duration_num = TO_DOUBLE(query_duration)
         | STATS avg_duration = AVG(query_duration_num)`,
-      });
-    });
-
-    it('high cardinality logs', async () => {
-      await evaluateEsqlQuery({
-        question: `i have logs in high-cardinality-data-fake_stack.admin-console-* . errors are found when log.level contais the value ERROR. generate a query to obtain the error rate as a percetage of the total logs per day for the last 7 days`,
-        expected: `FROM high-cardinality-data-fake_stack.admin-console-*
-        | WHERE @timestamp >= NOW() - 7 days
-        | EVAL error = CASE(log.level == "ERROR", 1, 0), total = 1
-        | EVAL bucket = DATE_TRUNC(1 day, @timestamp)
-        | STATS total_errors = SUM(error), total_logs = SUM(total) BY bucket
-        | EVAL error_rate = (total_errors / total_logs) * 100`,
       });
     });
 
@@ -240,6 +237,21 @@ describe('ES|QL query generation', () => {
               .outcome('success')
           )
       );
+
+      await synthtraceEsClients.apmSynthtraceEsClient.index(
+        timerange(moment().subtract(15, 'minutes'), moment())
+          .interval('1m')
+          .rate(10)
+          .generator((timestamp) =>
+            myServiceInstance
+              .transaction('GET /api')
+              .timestamp(timestamp)
+              .duration(50)
+              .failure()
+              .errors(
+                myServiceInstance.error({ message: "errorMessage", type: 'My Type' }).timestamp(timestamp)
+              )
+          ));
     });
 
     it('metrics avg duration', async () => {
@@ -269,7 +281,7 @@ describe('ES|QL query generation', () => {
 
     it('exit span', async () => {
       await evaluateEsqlQuery({
-        question: `I've got APM data in \`metrics-apm\`. Filter on \`metricset.name:service_destination\` and the last 24 hours. Break down by span.destination.service.resource. Each document contains the count of total events (span.destination.service.response_time.count) for that document's interval and the total amount of latency (span.destination.service.response_time.sum.us). A document either contains an aggregate of failed events (event.outcome:success) or failed events (event.outcome:failure). A single document might represent multiple failures or successes, depending on the value of span.destination.service.response_time.count. For each value of span.destination.service.resource, give me the average throughput, latency per request, and failure rate, as a value between 0 and 1.  Just show me the query.`,
+        question: `I've got APM data in \`metrics-apm*\`. Filter on \`metricset.name:service_destination\` and the last 24 hours. Break down by span.destination.service.resource. Each document contains the count of total events (span.destination.service.response_time.count) for that document's interval and the total amount of latency (span.destination.service.response_time.sum.us). A document either contains an aggregate of failed events (event.outcome:success) or failed events (event.outcome:failure). A single document might represent multiple failures or successes, depending on the value of span.destination.service.response_time.count. For each value of span.destination.service.resource, give me the average throughput, latency per request, and failure rate, as a value between 0 and 1.  Just show me the query.`,
         expected: `FROM metrics-apm
         | WHERE metricset.name == "service_destination" AND @timestamp >= NOW() - 24 hours
         | EVAL total_response_time = span.destination.service.response_time.sum.us / span.destination.service.response_time.count, total_failures = CASE(event.outcome == "failure", 1, 0) * span.destination.service.response_time.count
@@ -288,6 +300,18 @@ describe('ES|QL query generation', () => {
         expected: `FROM traces-apm*
         | WHERE @timestamp > NOW() - 1 hour
         | STATS AVG(transaction.duration.us) BY service.name`,
+      });
+    });
+
+    it('error logs rate', async () => {
+      await evaluateEsqlQuery({
+        question: `i have logs in logs-apm* . errors are found when log.level contais the value ERROR. generate a query to obtain the error rate as a percetage of the total logs per day for the last 7 days`,
+        expected: `FROM logs-apm*
+        | WHERE @timestamp >= NOW() - 7 days
+        | EVAL error = CASE(log.level == "ERROR", 1, 0), total = 1
+        | EVAL bucket = DATE_TRUNC(1 day, @timestamp)
+        | STATS total_errors = SUM(error), total_logs = SUM(total) BY bucket
+        | EVAL error_rate = (total_errors / total_logs) * 100`,
       });
     });
 
