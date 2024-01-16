36f83dccb806d0938edc086c9ea4eb2aa988e6dc
Sébastien Loix <sabee77@gmail.com>
2024-01-16 16:29:34 +0000

    Fix TS issue

--- x-pack/plugins/serverless/public/mocks.ts
+++ x-pack/plugins/serverless/public/mocks.ts
@@ -8,10 +8,10 @@
 import { ServerlessPluginStart } from './types';
 
 const startMock = (): ServerlessPluginStart => ({
-  setNavigation: jest.fn(),
+  initNavigation: jest.fn(),
   setBreadcrumbs: jest.fn(),
   setProjectHome: jest.fn(),
-  setSideNavComponent: jest.fn(),
+  setSideNavComponentDeprecated: jest.fn(),
   getActiveNavigationNodes$: jest.fn(),
 });
 
