3f8ce18fc6b090e71353b522e31b81c75575b11c
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2024-01-16 16:18:22 -0700

    adds SOM FTR tests to serverless tests

---
+++ x-pack/test_serverless/functional/test_suites/common/saved_objects_management/find.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+import { FtrProviderContext } from '../../../ftr_provider_context';
+
+export default function ({ getService }: FtrProviderContext) {
+  const supertest = getService('supertest');
+  const esArchiver = getService('esArchiver');
+  const kibanaServer = getService('kibanaServer');
+
+  describe('find', () => {
+    describe('saved objects with hidden type', () => {
+      before(async () => {
+        await esArchiver.load(
+          'test/functional/fixtures/es_archiver/saved_objects_management/hidden_saved_objects'
+        );
+        await kibanaServer.importExport.load(
+          'x-pack/test/functional/fixtures/kbn_archiver/saved_objects_management/hidden_saved_objects'
+        );
+      });
+      after(async () => {
+        await esArchiver.unload(
+          'test/functional/fixtures/es_archiver/saved_objects_management/hidden_saved_objects'
+        );
+        await kibanaServer.savedObjects.clean({
+          types: ['test-hidden-importable-exportable'],
+        });
+      });
+      it('returns saved objects with importableAndExportable types', async () =>
+        await supertest
+          .get('/api/kibana/management/saved_objects/_find?type=test-hidden-importable-exportable')
+          .set('kbn-xsrf', 'true')
+          .expect(200)
+          .then((resp) => {
+            expect(
+              resp.body.saved_objects.map((so: { id: string; type: string }) => ({
+                id: so.id,
+                type: so.type,
+              }))
+            ).to.eql([
+              {
+                type: 'test-hidden-importable-exportable',
+                id: 'ff3733a0-9fty-11e7-ahb3-3dcb94193fab',
+              },
+            ]);
+          }));
+
+      it('returns empty response for non importableAndExportable types', async () =>
+        await supertest
+          .get(
+            '/api/kibana/management/saved_objects/_find?type=test-hidden-non-importable-exportable'
+          )
+          .set('kbn-xsrf', 'true')
+          .expect(200)
+          .then((resp) => {
+            expect(resp.body.saved_objects).to.eql([]);
+          }));
+    });
+  });
+}
