1e33a59363872b42e37675160aa9d02d1a893776
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-01-16 16:32:35 +0100

    [Discover] Extract saved search SO into a separate plugin

---
+++ src/plugins/saved_search_so/common/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export { MIN_SAVED_SEARCH_SAMPLE_SIZE, MAX_SAVED_SEARCH_SAMPLE_SIZE, VIEW_MODE } from './constants';
