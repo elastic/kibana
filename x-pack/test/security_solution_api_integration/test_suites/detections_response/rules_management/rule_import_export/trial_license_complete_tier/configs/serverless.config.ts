90f8f3203961bb39211de1a275fdb77600c1a4fe
Yara Tercero <yara.tercero@elastic.co>
2024-01-15 19:17:27 -0800

    updated buildkite ftr configs yml

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/rule_import_export/trial_license_complete_tier/configs/serverless.config.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/rule_import_export/trial_license_complete_tier/configs/serverless.config.ts
@@ -4,12 +4,12 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { createTestConfig } from '../../../../../config/serverless/config.base';
+import { createTestConfig } from '../../../../../../config/serverless/config.base';
 
 export default createTestConfig({
   testFiles: [require.resolve('..')],
   junit: {
     reportName:
-      'Rules Management - Rule Import And Export Integration Tests - Serverless Env - Complete License',
+      'Rules Management - Rule Import And Export Integration Tests - Serverless Env - Complete Tier',
   },
 });
