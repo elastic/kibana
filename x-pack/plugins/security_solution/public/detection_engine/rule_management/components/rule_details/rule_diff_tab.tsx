64298b96cc3721a811c16be9582db8754e883c60
Nikita Indik <nikita.indik@elastic.co>
2024-01-16 13:38:58 +0100

    Refactor

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_diff_tab.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_diff_tab.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { useMemo } from 'react';
-import { omit } from 'lodash';
+import { omit, pick } from 'lodash';
 import stringify from 'json-stable-stringify';
 import {
   EuiSpacer,
@@ -24,39 +24,108 @@ import * as i18n from './json_diff/translations';
 const sortAndStringifyJson = (jsObject: Record<string, unknown>): string =>
   stringify(jsObject, { space: 2 });
 
+// const PROPERTIES_NOT_PRESENT_IN_PREBUILT_RULES = [
+//   'actions',
+//   'alias_purpose',
+//   'alias_target_id',
+//   'data_view_id',
+//   'execution_summary',
+//   'meta',
+//   'namespace',
+//   'outcome',
+//   'output_index',
+//   'response_actions',
+//   'saved_id',
+//   'saved_id',
+//   'throttle',
+//   'to',
+// ];
+
+// const HIDDEN_PROPERTIES = [
+//   /* Not a user-facing property. Can be confused with "version" by users. */
+//   'revision',
+
+//   /* Generates diff when the rule is enabled/disabled by user */
+//   'enabled',
+
+//   /* Never present in prebuilt rule updates. Generates diff if user adds exceptions to installed rule. Rule's exception list is preseved on rule update. */
+//   'exceptions_list',
+
+//   /* Never present in prebuilt rule updates. Actions are preseved on rule update */
+//   'actions',
+
+//   /* Not used anymore, but gets defaulted to an empty string when the rule is updated */
+//   'output_index',
+
+//   /* Never present in prebuilt rule updates. Gets added to installed rule after execution. */
+//   'execution_summary',
+
+//   /* Never present in prebuilt rule updates. These get added to installed rule once user saves the rule after editing. */
+//   'meta',
+//   'filters',
+//   'timestamp_override_fallback_disabled',
+
+//   /* Not relevant to the upgrade flow until we allow rule customisation. */
+//   'updated_at',
+//   'updated_by',
+//   'created_at',
+//   'created_by',
+
+//   /* Never present in prebuilt rule updates. */
+//   'outcome',
+//   'alias_target_id',
+//   'alias_purpose',
+// ];
+
+/* Inclding these properties in diff display might be confusing to users. */
 const HIDDEN_PROPERTIES = [
-  /* Not a user-facing property. Can be confused with "version" by users. */
-  'revision',
-
-  /* Generates diff when the rule is enabled/disabled by user */
-  'enabled',
-
-  /* Never present in prebuilt rule updates. Generates diff if user adds exceptions to installed rule. */
+  /*
+    By default, prebuilt rules don't have any actions or exception lists. So if a user has defined actions or exception lists for a rule, it'll show up as diff. This looks confusing as the user might think that their actions and exceptions lists will get removed after the upgrade, which is not the case - they will be preserved.
+  */
+  'actions',
   'exceptions_list',
 
-  /* Not used anymore, but gets defaulted to an empty string when the rule is updated */
-  'output_index',
+  /*
+    Most prebuilt rules are installed as "disabled". Once user enables a rule, it'll show as diff, which doesn't add value.
+  */
+  'enabled',
 
-  /* Never present in prebuilt rule updates. Gets added to installed rule after execution. */
-  'execution_summary',
+  /* Technical property. Hidden because it can be confused with "version" by users. */
+  'revision',
 
-  /* Never present in prebuilt rule updates. These get added to installed rule once user saves the rule after editing. */
-  'actions',
-  'meta',
-  'filters',
-  'timestamp_override_fallback_disabled',
-
-  /* Not relevant to the upgrade flow until we allow rule customisation. */
+  /*
+    This info is not yet exposed by prebuilt rules.
+    Ticket to add support: https://github.com/elastic/detection-rules/issues/2826
+  */
   'updated_at',
-  'updated_by',
-  'created_at',
-  'created_by',
-
-  /* Never present in prebuilt rule updates. */
-  'outcome',
-  'alias_target_id',
-  'alias_purpose',
 ];
+
+/**
+ * Normalizes the representation of the 'from' property in rule responses.
+ *
+ * Helpful when different time units represent the same duration. For instance, 'now-1m' and 'now-60s'
+ * both indicate a duration of one minute. If the durations represented by the 'from' properties in
+ * oldRule and newRule are the same, the function updates the oldRule's 'from' property to match
+ * that of newRule. This would ensure that the 'from' property is not shown as a diff in the UI.
+ *
+ * @param {RuleResponse} oldRule - The original rule.
+ * @param {RuleResponse} newRule - The new rule to compare with.
+ * @returns {RuleResponse} - The updated rule object with a potentially normalized 'from' property.
+ */
+const normalizeFromProperty = (oldRule: RuleResponse, newRule: RuleResponse): RuleResponse => {
+  let oldRuleFrom = oldRule.from;
+
+  if (
+    oldRule.from.startsWith('now-') &&
+    newRule.from.startsWith('now-') &&
+    parseDuration(oldRule.from.slice('now-'.length)) ===
+      parseDuration(newRule.from.slice('now-'.length))
+  ) {
+    oldRuleFrom = newRule.from;
+  }
+
+  return { ...oldRule, from: oldRuleFrom };
+};
 
 interface RuleDiffTabProps {
   oldRule: RuleResponse;
@@ -65,23 +134,11 @@ interface RuleDiffTabProps {
 
 export const RuleDiffTab = ({ oldRule, newRule }: RuleDiffTabProps) => {
   const [oldSource, newSource] = useMemo(() => {
-    let oldRuleFrom = oldRule.from;
-
-    if (
-      oldRule.from.startsWith('now-') &&
-      newRule.from.startsWith('now-') &&
-      parseDuration(oldRule.from.slice('now-'.length)) ===
-        parseDuration(newRule.from.slice('now-'.length))
-    ) {
-      /* 
-        We want to avoid showing the diff for the `from` property when duration is the same.
-        There are cases where different time units are used to represent the same duration.
-        For example, `now-1m` and `now-60s` both represent the same duration of 1 minute.
-      */
-      oldRuleFrom = newRule.from;
-    }
-
-    const visibleOldRuleProperties = { ...omit(oldRule, ...HIDDEN_PROPERTIES), from: oldRuleFrom };
+    const visibleOldRuleProperties = omit(
+      /* Only compare properties that are present in the update. */
+      pick(normalizeFromProperty(oldRule, newRule), Object.keys(newRule)),
+      ...HIDDEN_PROPERTIES
+    );
     const visibleNewRuleProperties = omit(newRule, ...HIDDEN_PROPERTIES);
 
     return [
