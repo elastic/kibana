9a6e41bc8806e2e83884e0615b394f68748d6e7f
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2024-01-17 10:44:13 -0700

    Use test plugins' parent folder in config

--- x-pack/test_serverless/functional/test_suites/observability/config.saved_objects_management.ts
+++ x-pack/test_serverless/functional/test_suites/observability/config.saved_objects_management.ts
@@ -11,17 +11,12 @@ import { resolve } from 'path';
 import { createTestConfig } from '../../config.base';
 
 export default createTestConfig({
-  serverlessProject: 'es',
+  serverlessProject: 'oblt',
   testFiles: [require.resolve('../common/saved_objects_management')],
   junit: {
     reportName: 'Serverless Search Saved Objects Management Functional Tests',
   },
-  kbnServerArgs: findTestPluginPaths([
-    resolve(REPO_ROOT, 'test/plugin_functional/plugins/saved_object_import_warnings'),
-    resolve(REPO_ROOT, 'test/plugin_functional/plugins/saved_object_export_transforms'),
-    resolve(REPO_ROOT, 'test/plugin_functional/plugins/saved_objects_hidden_from_http_apis_type'),
-    resolve(REPO_ROOT, 'test/plugin_functional/plugins/saved_objects_hidden_type'),
-  ]),
+  kbnServerArgs: findTestPluginPaths([resolve(REPO_ROOT, 'test/plugin_functional/plugins')]),
 
   // include settings from project controller
   // https://github.com/elastic/project-controller/blob/main/internal/project/esproject/config/elasticsearch.yml
