4e3536dbe7ed2b036e869bb034dc1fa474db0182
Jean-Louis Leysens <jloleysens@gmail.com>
2024-01-17 14:52:34 +0100

    added new http config and passed it down to the routers

--- packages/core/http/core-http-server-internal/src/http_config.ts
+++ packages/core/http/core-http-server-internal/src/http_config.ts
@@ -203,6 +203,12 @@ const configSchema = schema.object(
        * same-build browsers can access the Kibana server.
        */
       strictClientVersionCheck: schema.boolean({ defaultValue: true }),
+
+      /** This should not be configurable in serverless */
+      useDefaultStrategyForInternalPaths: offeringBasedSchema({
+        traditional: schema.arrayOf(schema.string(), { defaultValue: [] }),
+        serverless: schema.never(),
+      }),
     }),
   },
   {
@@ -280,6 +286,7 @@ export class HttpConfig implements IHttpConfig {
   public versioned: {
     versionResolution: HandlerResolutionStrategy;
     strictClientVersionCheck: boolean;
+    useDefaultStrategyForInternalPaths: string[];
   };
   public shutdownTimeout: Duration;
   public restrictInternalApis: boolean;
