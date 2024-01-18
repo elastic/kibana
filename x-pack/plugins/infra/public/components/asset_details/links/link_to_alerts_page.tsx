b4a93c063786269b28b28d56b21e91df229169b0
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2024-01-18 10:35:27 +0100

    Change popovers to use actual buttons

--- x-pack/plugins/infra/public/components/asset_details/links/link_to_alerts_page.tsx
+++ x-pack/plugins/infra/public/components/asset_details/links/link_to_alerts_page.tsx
@@ -33,7 +33,7 @@ export const LinkToAlertsPage = ({ assetName, queryField, dateRange }: LinkToAle
   );
 
   return (
-    <RedirectAppLinks coreStart={services}>
+    <RedirectAppLinks coreStart={services} style={{ display: 'inline-block' }}>
       <EuiButtonEmpty
         data-test-subj="infraAssetDetailsAlertsShowAllButton"
         size="xs"
