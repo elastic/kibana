78597d96ceddf0bb4b4943e26f34957ce69acdcb
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2024-01-18 12:47:36 +0000

    push array tests

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/default_license/rule_execution_logic/execution_logic/indicator_match_alert_suppression.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/default_license/rule_execution_logic/execution_logic/indicator_match_alert_suppression.ts
@@ -153,7 +153,7 @@ export default ({ getService }: FtrProviderContext) => {
     },
   ];
 
-  describe('@ess @serverless Indicator match type rules, alert suppression', () => {
+  describe.only('@ess @serverless Indicator match type rules, alert suppression', () => {
     before(async () => {
       await esArchiver.load('x-pack/test/functional/es_archives/security_solution/ecs_compliant');
     });
@@ -1263,6 +1263,70 @@ export default ({ getService }: FtrProviderContext) => {
                 {
                   field: 'host.name',
                   value: 'host-a',
+                },
+              ],
+              [TIMESTAMP]: '2020-10-28T07:00:00.000Z',
+              [ALERT_LAST_DETECTED]: '2020-10-28T07:00:00.000Z',
+              [ALERT_ORIGINAL_TIME]: timestamp,
+              [ALERT_SUPPRESSION_START]: '2020-10-28T07:00:00.000Z',
+              [ALERT_SUPPRESSION_END]: '2020-10-28T07:00:00.000Z',
+              [ALERT_SUPPRESSION_DOCS_COUNT]: 2,
+            });
+          });
+
+          it('should suppress alerts during rule execution only for array field', async () => {
+            const id = uuidv4();
+            const timestamp = '2020-10-28T06:45:00.000Z';
+            const doc1 = {
+              id,
+              '@timestamp': timestamp,
+              host: { name: ['host-a', 'host-b'] },
+            };
+
+            await eventsFiller({ id, count: eventsCount, timestamp: [timestamp] });
+            await threatsFiller({ id, count: threatsCount, timestamp });
+
+            await indexListOfSourceDocuments([doc1, doc1, doc1]);
+
+            await addThreatDocuments({
+              id,
+              timestamp,
+              fields: {
+                host: {
+                  name: 'host-a',
+                },
+              },
+              count: 1,
+            });
+
+            const rule: ThreatMatchRuleCreateProps = {
+              ...indicatorMatchRule(id),
+              alert_suppression: {
+                group_by: ['host.name'],
+                missing_fields_strategy: 'suppress',
+              },
+              from: 'now-35m',
+              interval: '30m',
+            };
+
+            const { previewId } = await previewRule({
+              supertest,
+              rule,
+              timeframeEnd: new Date('2020-10-28T07:00:00.000Z'),
+              invocationCount: 1,
+            });
+            const previewAlerts = await getPreviewAlerts({
+              es,
+              previewId,
+              sort: [ALERT_ORIGINAL_TIME],
+            });
+            expect(previewAlerts.length).toEqual(1);
+            expect(previewAlerts[0]._source).toEqual({
+              ...previewAlerts[0]._source,
+              [ALERT_SUPPRESSION_TERMS]: [
+                {
+                  field: 'host.name',
+                  value: 'host-a,host-b',
                 },
               ],
               [TIMESTAMP]: '2020-10-28T07:00:00.000Z',
