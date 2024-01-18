d724785bde9939aa4229a87d8a3c5a23990996eb
Janki Salvi <117571355+js-jankisalvi@users.noreply.github.com>
2024-01-18 09:45:34 +0100

    remove loop

--- x-pack/plugins/cases/public/components/case_settings/sync_alerts_switch.test.tsx
+++ x-pack/plugins/cases/public/components/case_settings/sync_alerts_switch.test.tsx
@@ -13,55 +13,52 @@ import { createAppMockRenderer } from '../../common/mock';
 
 import { SyncAlertsSwitch } from './sync_alerts_switch';
 
-// FLAKY: https://github.com/elastic/kibana/issues/174259
-for (let i = 0; i < 500; i++) {
-  describe('SyncAlertsSwitch', () => {
-    let appMockRender: AppMockRenderer;
+describe('SyncAlertsSwitch', () => {
+  let appMockRender: AppMockRenderer;
 
-    beforeEach(() => {
-      appMockRender = createAppMockRenderer();
-    });
-
-    it('it renders', async () => {
-      appMockRender.render(<SyncAlertsSwitch disabled={false} />);
-
-      expect(await screen.findByTestId('sync-alerts-switch')).toBeInTheDocument();
-    });
-
-    it('it toggles the switch', async () => {
-      appMockRender.render(<SyncAlertsSwitch disabled={false} />);
-
-      userEvent.click(await screen.findByTestId('sync-alerts-switch'));
-
-      expect(await screen.findByTestId('sync-alerts-switch')).toHaveAttribute(
-        'aria-checked',
-        'false'
-      );
-    });
-
-    it('it disables the switch', async () => {
-      appMockRender.render(<SyncAlertsSwitch disabled={true} />);
-
-      expect(await screen.findByTestId('sync-alerts-switch')).toHaveProperty('disabled', true);
-    });
-
-    it('it start as off', async () => {
-      appMockRender.render(<SyncAlertsSwitch disabled={false} isSynced={false} showLabel={true} />);
-
-      expect(await screen.findByText('Off')).toBeInTheDocument();
-      expect(screen.queryByText('On')).not.toBeInTheDocument();
-    });
-
-    it('it shows the correct labels', async () => {
-      appMockRender.render(<SyncAlertsSwitch disabled={false} showLabel={true} />);
-
-      expect(await screen.findByText('On')).toBeInTheDocument();
-      expect(screen.queryByText('Off')).not.toBeInTheDocument();
-
-      userEvent.click(await screen.findByTestId('sync-alerts-switch'));
-
-      expect(await screen.findByText('Off')).toBeInTheDocument();
-      expect(screen.queryByText('On')).not.toBeInTheDocument();
-    });
+  beforeEach(() => {
+    appMockRender = createAppMockRenderer();
   });
-}
+
+  it('it renders', async () => {
+    appMockRender.render(<SyncAlertsSwitch disabled={false} />);
+
+    expect(await screen.findByTestId('sync-alerts-switch')).toBeInTheDocument();
+  });
+
+  it('it toggles the switch', async () => {
+    appMockRender.render(<SyncAlertsSwitch disabled={false} />);
+
+    userEvent.click(await screen.findByTestId('sync-alerts-switch'));
+
+    expect(await screen.findByTestId('sync-alerts-switch')).toHaveAttribute(
+      'aria-checked',
+      'false'
+    );
+  });
+
+  it('it disables the switch', async () => {
+    appMockRender.render(<SyncAlertsSwitch disabled={true} />);
+
+    expect(await screen.findByTestId('sync-alerts-switch')).toHaveProperty('disabled', true);
+  });
+
+  it('it start as off', async () => {
+    appMockRender.render(<SyncAlertsSwitch disabled={false} isSynced={false} showLabel={true} />);
+
+    expect(await screen.findByText('Off')).toBeInTheDocument();
+    expect(screen.queryByText('On')).not.toBeInTheDocument();
+  });
+
+  it('it shows the correct labels', async () => {
+    appMockRender.render(<SyncAlertsSwitch disabled={false} showLabel={true} />);
+
+    expect(await screen.findByText('On')).toBeInTheDocument();
+    expect(screen.queryByText('Off')).not.toBeInTheDocument();
+
+    userEvent.click(await screen.findByTestId('sync-alerts-switch'));
+
+    expect(await screen.findByText('Off')).toBeInTheDocument();
+    expect(screen.queryByText('On')).not.toBeInTheDocument();
+  });
+});
