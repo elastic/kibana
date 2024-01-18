e8425205f0b72dee74b6abdaa5774dc9fd50115e
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2024-01-18 10:25:51 +0100

    refactor tooltip for better rendering

--- x-pack/plugins/log_explorer/public/components/virtual_columns/column_tooltips/content_column_tooltip.tsx
+++ x-pack/plugins/log_explorer/public/components/virtual_columns/column_tooltips/content_column_tooltip.tsx
@@ -5,9 +5,10 @@
  * 2.0.
  */
 
-import { EuiFlexGroup, EuiFlexItem, EuiIcon, EuiText, EuiToken } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiIcon, EuiText, EuiToken, useEuiTheme } from '@elastic/eui';
 import React from 'react';
 import { CustomGridColumnProps } from '@kbn/unified-data-table';
+import { css } from '@emotion/react';
 import {
   contentHeaderTooltipParagraph1,
   contentHeaderTooltipParagraph2,
@@ -21,6 +22,11 @@ const ColumnHeaderTruncateContainer = dynamic(
 );
 
 export const ContentColumnTooltip = ({ column, headerRowHeight }: CustomGridColumnProps) => {
+  const { euiTheme } = useEuiTheme();
+  const spacingCSS = css`
+    margin-bottom: ${euiTheme.size.s};
+  `;
+
   const contentButtonComponent = (
     <ColumnHeaderTruncateContainer headerRowHeight={headerRowHeight}>
       {column.displayAsText} <EuiIcon type="questionInCircle" />
@@ -29,50 +35,40 @@ export const ContentColumnTooltip = ({ column, headerRowHeight }: CustomGridColu
   return (
     <HoverPopover button={contentButtonComponent} title={contentLabel}>
       <div style={{ width: '230px' }}>
-        <EuiFlexGroup gutterSize="s" justifyContent="spaceBetween" direction="column">
+        <EuiText size="s" css={spacingCSS}>
+          <p>{contentHeaderTooltipParagraph1}</p>
+        </EuiText>
+        <EuiText size="s" css={spacingCSS}>
+          <p>{contentHeaderTooltipParagraph2}</p>
+        </EuiText>
+        <EuiFlexGroup
+          responsive={false}
+          alignItems="center"
+          justifyContent="flexStart"
+          gutterSize="xs"
+        >
           <EuiFlexItem grow={false}>
-            <EuiText size="s">
-              <span>{contentHeaderTooltipParagraph1}</span>
-            </EuiText>
+            <EuiToken iconType="tokenKeyword" size="s" />
           </EuiFlexItem>
           <EuiFlexItem grow={false}>
             <EuiText size="s">
-              <span>{contentHeaderTooltipParagraph2}</span>
+              <strong>error.message</strong>
             </EuiText>
           </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiFlexGroup
+          responsive={false}
+          alignItems="center"
+          justifyContent="flexStart"
+          gutterSize="xs"
+        >
           <EuiFlexItem grow={false}>
-            <EuiFlexGroup
-              responsive={false}
-              alignItems="center"
-              justifyContent="flexStart"
-              gutterSize="xs"
-            >
-              <EuiFlexItem grow={false}>
-                <EuiToken iconType="tokenKeyword" size="s" />
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiText size="s">
-                  <strong>error.message</strong>
-                </EuiText>
-              </EuiFlexItem>
-            </EuiFlexGroup>
+            <EuiToken iconType="tokenEvent" size="s" />
           </EuiFlexItem>
           <EuiFlexItem grow={false}>
-            <EuiFlexGroup
-              responsive={false}
-              alignItems="center"
-              justifyContent="flexStart"
-              gutterSize="xs"
-            >
-              <EuiFlexItem grow={false}>
-                <EuiToken iconType="tokenEvent" size="s" />
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiText size="s">
-                  <strong>event.original</strong>
-                </EuiText>
-              </EuiFlexItem>
-            </EuiFlexGroup>
+            <EuiText size="s">
+              <strong>event.original</strong>
+            </EuiText>
           </EuiFlexItem>
         </EuiFlexGroup>
       </div>
