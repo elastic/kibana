ead779a295d453c3ceb8f636f534c1ab2fca549a
dej611 <dej611@gmail.com>
2024-01-18 17:32:41 +0100

    :fire: Remove debugging code

--- x-pack/test/functional/apps/lens/group1/smokescreen.ts
+++ x-pack/test/functional/apps/lens/group1/smokescreen.ts
@@ -98,7 +98,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await PageObjects.lens.assertLegacyMetric('Maximum of bytes', '19,986');
       await PageObjects.lens.switchToVisualization('lnsDatatable');
       expect(await PageObjects.lens.getDatatableHeaderText()).to.eql('Maximum of bytes');
-      await PageObjects.common.sleep(50000);
       expect(await PageObjects.lens.getDatatableCellText(0, 0)).to.eql('19,986');
       await PageObjects.lens.switchToVisualization('lnsLegacyMetric');
       await PageObjects.lens.assertLegacyMetric('Maximum of bytes', '19,986');
