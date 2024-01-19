6b3ab8c6118a82425916fd7db566a7df12261d22
Coen Warmer <coen.warmer@gmail.com>
2024-01-19 21:46:52 +0100

    Fix error

--- x-pack/plugins/observability/public/components/experimental_badge.tsx
+++ x-pack/plugins/observability/public/components/experimental_badge.tsx
@@ -15,6 +15,10 @@ export function ExperimentalBadge() {
       label={i18n.translate('xpack.observability.experimentalBadgeLabel', {
         defaultMessage: 'Technical preview',
       })}
+      tooltipContent={i18n.translate('xpack.observability.experimentalBadgeDescription', {
+        defaultMessage:
+          'This functionality is in technical preview and may be changed or removed completely in a future release. Elastic will work to fix any issues, but features in technical preview are not subject to the support SLA of official GA features.',
+      })}
     />
   );
 }
