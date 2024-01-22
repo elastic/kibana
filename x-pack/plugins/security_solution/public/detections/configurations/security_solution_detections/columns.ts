dc91312ef758ed40a83b2a6002a83a8c05dfa79c
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2024-01-22 14:29:15 +0100

    [Security Solution] User Alert Assignment feature: Assignee column in Alerts Table not displaying after upgrading to 8.12 (#173695) (#174370)
    ## Summary
    
    Addresses https://github.com/elastic/kibana/issues/173695
    
    This PR fixes the issue with the missing "Assignees" column specs in the
    alert's table after user upgrades to `v8.12+`.
    
    This happens because we store table mode specs in the local storage and
    do not update it after the upgrade. With this changes we add missing
    column specs if needed.
    
    To easily reproduce the bug (without actual upgrades):
    1. Run latest main
    2. Open web inspector > local storage data
    3. Find and update next items: `securityDataTable`,
    `detection-engine-alert-table-securitySolution-alerts-page-gridView` and
    `detection-engine-alert-table-securitySolution-rule-details-gridView`.
    You need to remove "Assignees" column specs which look like this:
    `{"columnHeaderType":"not-filtered","displayAsText":"Assignees","id":"kibana.alert.workflow_assignee_ids","initialWidth":190,"schema":"string"}`.
    Also, for the last to items remove
    `"kibana.alert.workflow_assignee_ids"` from `visibleColumns`.
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 586c35869310c03f266a359eeeb61f25139c85f0)

--- x-pack/plugins/security_solution/public/detections/configurations/security_solution_detections/columns.ts
+++ x-pack/plugins/security_solution/public/detections/configurations/security_solution_detections/columns.ts
@@ -21,6 +21,13 @@ import {
   DEFAULT_TABLE_DATE_COLUMN_MIN_WIDTH,
 } from './translations';
 
+export const assigneesColumn: ColumnHeaderOptions = {
+  columnHeaderType: defaultColumnHeaderType,
+  displayAsText: i18n.ALERTS_HEADERS_ASSIGNEES,
+  id: 'kibana.alert.workflow_assignee_ids',
+  initialWidth: DEFAULT_DATE_COLUMN_MIN_WIDTH,
+};
+
 const getBaseColumns = (
   license?: LicenseService
 ): Array<
@@ -28,12 +35,6 @@ const getBaseColumns = (
 > => {
   const isPlatinumPlus = license?.isPlatinumPlus?.() ?? false;
   return [
-    {
-      columnHeaderType: defaultColumnHeaderType,
-      displayAsText: i18n.ALERTS_HEADERS_ASSIGNEES,
-      id: 'kibana.alert.workflow_assignee_ids',
-      initialWidth: DEFAULT_DATE_COLUMN_MIN_WIDTH,
-    },
     {
       columnHeaderType: defaultColumnHeaderType,
       displayAsText: i18n.ALERTS_HEADERS_SEVERITY,
@@ -115,6 +116,7 @@ export const getColumns = (
     initialWidth: DEFAULT_COLUMN_MIN_WIDTH,
     linkField: 'kibana.alert.rule.uuid',
   },
+  assigneesColumn,
   ...getBaseColumns(license),
 ];
 
