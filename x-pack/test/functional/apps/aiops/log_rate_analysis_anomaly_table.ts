c6abcf2e54437b7761cfb4b81b3be7b2e48461f9
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-01-23 18:13:08 +0100

    add functional tests for anomaly explorer actions

--- x-pack/test/functional/apps/aiops/log_rate_analysis_anomaly_table.ts
+++ x-pack/test/functional/apps/aiops/log_rate_analysis_anomaly_table.ts
@@ -23,7 +23,7 @@ function getJobWithDataFeed(
   const postFix = `${detectorFunction}${detectorField ? `_${detectorField}` : ''}${
     partitionFieldName ? `_${partitionFieldName}` : ''
   }`;
-  const jobId = `fq_single_1_smv_lra_${postFix}`;
+  const jobId = `fq_lra_${postFix}`;
 
   // @ts-expect-error not full interface
   const jobConfig: Job = {
@@ -50,7 +50,7 @@ function getJobWithDataFeed(
 
   // @ts-expect-error not full interface
   const datafeedConfig: Datafeed = {
-    datafeed_id: `datafeed-fq_single_1_smv_lra_${postFix}`,
+    datafeed_id: `datafeed-fq_lra_${postFix}`,
     indices: ['ft_farequote'],
     job_id: jobId,
     query: { bool: { must: [{ match_all: {} }] } },
@@ -126,102 +126,115 @@ export default function ({ getService }: FtrProviderContext) {
   describe('anomaly table with link to log rate analysis', async function () {
     this.tags(['ml']);
 
-    for (const td of testData) {
-      const {
-        jobConfig,
-        datafeedConfig,
-        analysisType,
-        dataGenerator,
-        entitySelectionField,
-        entitySelectionValue,
-        expected,
-      } = td;
-      describe(`with single metric viewer job ${jobConfig.job_id}`, async function () {
-        before(async () => {
-          await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/ml/farequote');
-          await ml.testResources.createDataViewIfNeeded('ft_farequote', '@timestamp');
-          await ml.testResources.setKibanaTimeZoneToUTC();
+    before(async () => {
+      await esArchiver.loadIfNeeded('x-pack/test/functional/es_archives/ml/farequote');
+      await ml.testResources.createDataViewIfNeeded('ft_farequote', '@timestamp');
+      await ml.testResources.setKibanaTimeZoneToUTC();
+    });
 
-          await ml.api.createAndRunAnomalyDetectionLookbackJob(jobConfig, datafeedConfig);
+    for (const page of ['anomaly explorer', 'single metric viewer']) {
+      for (const td of testData) {
+        const {
+          jobConfig,
+          datafeedConfig,
+          analysisType,
+          dataGenerator,
+          entitySelectionField,
+          entitySelectionValue,
+          expected,
+        } = td;
+        describe(`via ${page} for job ${jobConfig.job_id}`, async function () {
+          before(async () => {
+            await ml.api.createAndRunAnomalyDetectionLookbackJob(jobConfig, datafeedConfig);
 
-          await ml.securityUI.loginAsMlPowerUser();
-        });
-
-        after(async () => {
-          await ml.api.cleanMlIndices();
-          await ml.testResources.deleteDataViewByTitle('ft_farequote');
-        });
-
-        it(`should load the single metric viewer for job '${jobConfig.job_id}`, async () => {
-          await ml.testExecution.logTestStep('navigate to job list');
-          await ml.navigation.navigateToMl();
-          await ml.navigation.navigateToJobManagement();
-
-          await ml.testExecution.logTestStep('open job in single metric viewer');
-          await ml.jobTable.filterWithSearchString(jobConfig.job_id, 1);
-
-          await ml.jobTable.clickOpenJobInSingleMetricViewerButton(jobConfig.job_id);
-          await ml.commonUI.waitForMlLoadingIndicatorToDisappear();
-
-          if (entitySelectionField && entitySelectionValue) {
-            await testSubjects.existOrFail(
-              `mlSingleMetricViewerEntitySelection ${entitySelectionField}`
-            );
-            await comboBox.set(
-              `mlSingleMetricViewerEntitySelection ${entitySelectionField} > comboBoxInput`,
-              entitySelectionValue
-            );
-          }
-
-          await ml.testExecution.logTestStep('displays the anomalies table');
-          await ml.anomaliesTable.assertTableExists();
-
-          await ml.testExecution.logTestStep('anomalies table is not empty');
-          await ml.anomaliesTable.assertTableNotEmpty();
-        });
-
-        if (expected.anomalyTableLogRateAnalysisButtonAvailable) {
-          it('should click the log rate analysis action', async () => {
-            await ml.anomaliesTable.assertAnomalyActionsMenuButtonExists(0);
-            await ml.anomaliesTable.scrollRowIntoView(0);
-            await ml.anomaliesTable.assertAnomalyActionsMenuButtonEnabled(0, true);
-            await ml.anomaliesTable.assertAnomalyActionLogRateAnalysisButtonExists(0);
-            await ml.anomaliesTable.ensureAnomalyActionLogRateAnalysisButtonClicked(0);
+            await ml.securityUI.loginAsMlPowerUser();
           });
 
-          const shouldHaveResults = expected.analysisResults !== undefined;
-
-          it('should complete the analysis', async () => {
-            await aiops.logRateAnalysisPage.assertAnalysisComplete(
-              analysisType,
-              dataGenerator,
-              !shouldHaveResults
-            );
+          after(async () => {
+            await ml.api.cleanMlIndices();
+            await ml.testResources.deleteDataViewByTitle('ft_farequote');
           });
 
-          if (shouldHaveResults) {
-            it('should show analysis results', async () => {
-              await aiops.logRateAnalysisResultsTable.assertLogRateAnalysisResultsTableExists();
-              const actualAnalysisTable =
-                await aiops.logRateAnalysisResultsTable.parseAnalysisTable();
+          it('should navigate to ML job management', async () => {
+            await ml.testExecution.logTestStep('navigate to job list');
+            await ml.navigation.navigateToMl();
+            await ml.navigation.navigateToJobManagement();
+          });
 
-              expect(actualAnalysisTable).to.be.eql(
-                expected.analysisResults,
-                `Expected analysis table to be ${JSON.stringify(
-                  expected.analysisResults
-                )}, got ${JSON.stringify(actualAnalysisTable)}`
+          it(`should load the ${page} for job '${jobConfig.job_id}`, async () => {
+            await ml.testExecution.logTestStep('open job in single metric viewer');
+            await ml.jobTable.filterWithSearchString(jobConfig.job_id, 1);
+
+            if (page === 'single metric viewer') {
+              await ml.jobTable.clickOpenJobInSingleMetricViewerButton(jobConfig.job_id);
+            } else {
+              await ml.jobTable.clickOpenJobInAnomalyExplorerButton(jobConfig.job_id);
+            }
+
+            await ml.commonUI.waitForMlLoadingIndicatorToDisappear();
+
+            if (page === 'single metric viewer' && entitySelectionField && entitySelectionValue) {
+              await testSubjects.existOrFail(
+                `mlSingleMetricViewerEntitySelection ${entitySelectionField}`
+              );
+              await comboBox.set(
+                `mlSingleMetricViewerEntitySelection ${entitySelectionField} > comboBoxInput`,
+                entitySelectionValue
+              );
+            }
+          });
+
+          it(`should show the anomaly table`, async () => {
+            await ml.testExecution.logTestStep('displays the anomalies table');
+            await ml.anomaliesTable.assertTableExists();
+
+            await ml.testExecution.logTestStep('anomalies table is not empty');
+            await ml.anomaliesTable.assertTableNotEmpty();
+          });
+
+          if (expected.anomalyTableLogRateAnalysisButtonAvailable) {
+            it('should click the log rate analysis action', async () => {
+              await ml.anomaliesTable.assertAnomalyActionsMenuButtonExists(0);
+              await ml.anomaliesTable.scrollRowIntoView(0);
+              await ml.anomaliesTable.assertAnomalyActionsMenuButtonEnabled(0, true);
+              await ml.anomaliesTable.assertAnomalyActionLogRateAnalysisButtonExists(0);
+              await ml.anomaliesTable.ensureAnomalyActionLogRateAnalysisButtonClicked(0);
+            });
+
+            const shouldHaveResults = expected.analysisResults !== undefined;
+
+            it('should complete the analysis', async () => {
+              await aiops.logRateAnalysisPage.assertAnalysisComplete(
+                analysisType,
+                dataGenerator,
+                !shouldHaveResults
               );
             });
+
+            if (shouldHaveResults) {
+              it('should show analysis results', async () => {
+                await aiops.logRateAnalysisResultsTable.assertLogRateAnalysisResultsTableExists();
+                const actualAnalysisTable =
+                  await aiops.logRateAnalysisResultsTable.parseAnalysisTable();
+
+                expect(actualAnalysisTable).to.be.eql(
+                  expected.analysisResults,
+                  `Expected analysis table to be ${JSON.stringify(
+                    expected.analysisResults
+                  )}, got ${JSON.stringify(actualAnalysisTable)}`
+                );
+              });
+            }
+          } else {
+            it('should not show the log rate analysis action', async () => {
+              await ml.anomaliesTable.assertAnomalyActionsMenuButtonExists(0);
+              await ml.anomaliesTable.scrollRowIntoView(0);
+              await ml.anomaliesTable.assertAnomalyActionsMenuButtonEnabled(0, true);
+              await ml.anomaliesTable.assertAnomalyActionLogRateAnalysisButtonNotExists(0);
+            });
           }
-        } else {
-          it('should not show the log rate analysis action', async () => {
-            await ml.anomaliesTable.assertAnomalyActionsMenuButtonExists(0);
-            await ml.anomaliesTable.scrollRowIntoView(0);
-            await ml.anomaliesTable.assertAnomalyActionsMenuButtonEnabled(0, true);
-            await ml.anomaliesTable.assertAnomalyActionLogRateAnalysisButtonNotExists(0);
-          });
-        }
-      });
+        });
+      }
     }
   });
 }
