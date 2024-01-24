8575e44a64c5a0d0d2ed67cd26ffec3f11853d38
James Gowdy <jgowdy@elastic.co>
2024-01-24 11:36:14 +0000

    updating tests

--- x-pack/plugins/aiops/public/components/log_categorization/format_category.test.tsx
+++ x-pack/plugins/aiops/public/components/log_categorization/format_category.test.tsx
@@ -8,46 +8,55 @@
 import React from 'react';
 
 import type { Category } from '../../../common/api/log_categorization/types';
-import { FormattedPatternExamples } from './format_category';
+import { FormattedPatternExamples, FormattedRegex, FormattedTokens } from './format_category';
 
 const categories = [
   {
     key: 'Processed messages out of',
     examples: ['Processed 676 messages out of 676'],
+    regex: '.*?Processed.+?messages.+?out.+?of.*?',
   },
   {
     key: 'Processed messages out of',
     examples: ['Processed out 676 messages messages out of 676 Processed'],
+    regex: '.*?Processed.+?messages.+?out.+?of.*?',
   },
   {
     key: 'Processed of',
     examples: ['Processed 676 messages out of 676'],
+    regex: '.*?Processed.+?of.*?',
   },
   {
     key: 'Processed messages out of',
     examples: ['Processed messages out of'],
+    regex: '.*?Processed.+?messages.+?out.+?of.*?',
   },
   {
     key: '',
     examples: ['Processed messages out of'],
+    regex: '.*?',
   },
   {
     key: 'Processed messages out of',
     examples: ['Processed 676 (*?) message* out of 676'],
+    regex: '.*?Processed.+?messages.+?out.+?of.*?',
   },
   {
     key: '2024 0000 admin to admin console.prod 6000 api HTTP 1.1 https admin Mozilla 5.0 AppleWebKit KHTML like Gecko Chrome Safari',
     examples: [
       '[05/Jan/2024 04:11:42 +0000] 40.69.144.53 - Lucio77 admin-console.you-got.mail to: admin-console.prod.008:6000: "GET /api/listCustomers HTTP/1.1" 200 383 "https://admin-console.you-got.mail" "Mozilla/5.0 (Windows; U; Windows NT 6.3) AppleWebKit/531.1.1 (KHTML, like Gecko) Chrome/23.0.835.0 Safari/531.1.1"',
     ],
+    regex:
+      '.*?2024.+?0000.+?admin.+?to.+?admin.+?console.prod.+?6000.+?api.+?HTTP.+?1.1.+?https.+?admin.+?Mozilla.+?5.0.+?AppleWebKit.+?KHTML.+?like.+?Gecko.+?Chrome.+?Safari.*?',
   },
 ] as Category[];
 
 describe('FormattedPatternExamples', () => {
   it('renders each category correctly', () => {
     categories.forEach((category) => {
-      const component = <FormattedPatternExamples category={category} />;
-      expect(component).toMatchSnapshot();
+      expect(<FormattedPatternExamples category={category} />).toMatchSnapshot();
+      expect(<FormattedRegex category={category} />).toMatchSnapshot();
+      expect(<FormattedTokens category={category} />).toMatchSnapshot();
     });
   });
 });
