60514b2a1d0e07f6acc969136a580b3a6bd92a30
Nikita Indik <nikita.indik@elastic.co>
2024-01-24 15:03:24 +0100

    Fix display for more properties

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_diff_tab.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_diff_tab.tsx
@@ -16,10 +16,16 @@ import {
   EuiTitle,
   EuiIconTip,
 } from '@elastic/eui';
-import { parseDuration } from '@kbn/alerting-plugin/common';
+import type { Filter } from '@kbn/es-query';
+import { normalizeMachineLearningJobIds } from '../../../../../common/detection_engine/utils';
+import {
+  formatScheduleStepData,
+  filterEmptyThreats,
+} from '../../../rule_creation_ui/pages/rule_creation/helpers';
 import type { RuleResponse } from '../../../../../common/api/detection_engine/model/rule_schema/rule_schemas.gen';
 import { DiffView } from './json_diff/diff_view';
 import * as i18n from './json_diff/translations';
+import { getHumanizedDuration } from '../../../../detections/pages/detection_engine/rules/helpers';
 
 /* Inclding these properties in diff display might be confusing to users. */
 const HIDDEN_PROPERTIES = [
@@ -48,30 +54,73 @@ const sortAndStringifyJson = (jsObject: Record<string, unknown>): string =>
   stringify(jsObject, { space: 2 });
 
 /**
- * Normalizes the representation of the 'from' property in rule responses.
+ * Normalizes the rule object, making it suitable for comparison with another normalized rule.
  *
- * Helpful when different time units represent the same duration. For instance, 'now-1m' and 'now-60s'
- * both indicate a duration of one minute. If the durations represented by the 'from' properties in
- * oldRule and newRule are the same, the function updates the oldRule's 'from' property to match
- * that of newRule. This would ensure that the 'from' property is not shown as a diff in the UI.
+ * Normalization is needed to avoid showing diffs for semantically equal property values.
+ * Saving a rule via the rule editing UI might change the format of some properties or set default values.
+ * This function compensates for those changes.
  *
- * @param {RuleResponse} oldRule - The original rule.
- * @param {RuleResponse} newRule - The new rule to compare with.
- * @returns {RuleResponse} - The updated rule object with a potentially normalized 'from' property.
+ * @param {RuleResponse} originalRule - Rule of a RuleResponse type.
+ * @returns {RuleResponse} - The updated normalized rule object.
  */
-const normalizeFromProperty = (oldRule: RuleResponse, newRule: RuleResponse): RuleResponse => {
-  let oldRuleFrom = oldRule.from;
+const normalizeRule = (originalRule: RuleResponse): RuleResponse => {
+  const rule = { ...originalRule };
 
-  if (
-    oldRule.from.startsWith('now-') &&
-    newRule.from.startsWith('now-') &&
-    parseDuration(oldRule.from.slice('now-'.length)) ===
-      parseDuration(newRule.from.slice('now-'.length))
-  ) {
-    oldRuleFrom = newRule.from;
+  /*
+    Convert the "from" property value to a humanized duration string, like 'now-1m' or 'now-2h'. 
+    Conversion is needed to skip showing the diff for the "from" property when the same 
+    duration is represented in different time units. For instance, 'now-1h' and 'now-3600s' 
+    indicate a one-hour duration.
+    The same helper is used in the rule editing UI to format "from" before submitting the edits. 
+    So, after the rule is saved, the "from" property unit/value might differ from what's in the package.
+  */
+  rule.from = formatScheduleStepData({
+    interval: rule.interval,
+    from: getHumanizedDuration(rule.from, rule.interval),
+    to: rule.to,
+  }).from;
+
+  /*
+    Default "note" to an empty string if it's not present.
+    Sometimes, in a new version of a rule, the "note" value equals an empty string, while 
+    in the old version, it wasn't specified at all (undefined becomes ''). In this case, 
+    it doesn't make sense to show diff, so we default falsy values to ''.
+  */
+  rule.note = rule.note ?? '';
+
+  /*
+    Removes empty arrays (techniques, subtechniques) from the MITRE ATT&CK value.
+    The same processing is done in the rule editing UI before submitting the edits.
+  */
+  rule.threat = filterEmptyThreats(rule.threat);
+
+  /*
+    The "machine_learning_job_id" property is converted from the legacy string format 
+    to the new array format during installation and upgrade. Thus, all installed rules 
+    use the new format. For correct comparison, we must ensure that the new rule is 
+    also in the new format before showing the diff.
+  */
+  if ('machine_learning_job_id' in rule) {
+    rule.machine_learning_job_id = normalizeMachineLearningJobIds(rule.machine_learning_job_id);
   }
 
-  return { ...oldRule, from: oldRuleFrom };
+  /*
+    Default the "alias" property to null for all threat filters that don't have it.
+    Setting a default is needed to match the behavior of the rule editing UI, 
+    which also defaults the "alias" property to null.
+  */
+  if (rule.type === 'threat_match' && Array.isArray(rule.threat_filters)) {
+    const threatFiltersWithDefaultMeta = (rule.threat_filters as Filter[]).map((filter) => {
+      if (!filter.meta.alias) {
+        return { ...filter, meta: { ...filter.meta, alias: null } };
+      }
+      return filter;
+    });
+
+    rule.threat_filters = threatFiltersWithDefaultMeta;
+  }
+
+  return rule;
 };
 
 interface RuleDiffTabProps {
@@ -81,12 +130,11 @@ interface RuleDiffTabProps {
 
 export const RuleDiffTab = ({ oldRule, newRule }: RuleDiffTabProps) => {
   const [oldSource, newSource] = useMemo(() => {
+    const visibleNewRuleProperties = omit(normalizeRule(newRule), ...HIDDEN_PROPERTIES);
     const visibleOldRuleProperties = omit(
       /* Only compare properties that are present in the update. */
-      pick(normalizeFromProperty(oldRule, newRule), Object.keys(newRule)),
-      ...HIDDEN_PROPERTIES
+      pick(normalizeRule(oldRule), Object.keys(visibleNewRuleProperties))
     );
-    const visibleNewRuleProperties = omit(newRule, ...HIDDEN_PROPERTIES);
 
     return [
       sortAndStringifyJson(visibleOldRuleProperties),
