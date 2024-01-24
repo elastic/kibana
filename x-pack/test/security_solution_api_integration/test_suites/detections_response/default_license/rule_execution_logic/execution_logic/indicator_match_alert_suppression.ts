c96e6b225314b10f45617166047ef9a3b09f9075
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2024-01-24 17:13:08 +0000

    skip IM tests

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/default_license/rule_execution_logic/execution_logic/indicator_match_alert_suppression.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/default_license/rule_execution_logic/execution_logic/indicator_match_alert_suppression.ts
@@ -155,7 +155,7 @@ export default ({ getService }: FtrProviderContext) => {
     },
   ];
 
-  describe.only('@ess @serverless Indicator match type rules, alert suppression', () => {
+  describe.skip('@ess @serverless Indicator match type rules, alert suppression', () => {
     before(async () => {
       await esArchiver.load('x-pack/test/functional/es_archives/security_solution/ecs_compliant');
     });
