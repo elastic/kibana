d1e2f74124e6ee9a7986af388d69c861426649dd
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-01-24 10:59:14 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- packages/kbn-search-connectors/types/native_connectors.ts
+++ packages/kbn-search-connectors/types/native_connectors.ts
@@ -330,9 +330,12 @@ export const NATIVE_CONNECTOR_DEFINITIONS: Record<string, NativeConnector | unde
           },
         ],
         display: DisplayType.TEXTBOX,
-        label: i18n.translate('searchConnectors.nativeConnectors.confluenceDataCenter.usernameLabel', {
-          defaultMessage: 'Confluence Data Center username',
-        }),
+        label: i18n.translate(
+          'searchConnectors.nativeConnectors.confluenceDataCenter.usernameLabel',
+          {
+            defaultMessage: 'Confluence Data Center username',
+          }
+        ),
         options: [],
         order: 4,
         required: true,
@@ -352,9 +355,12 @@ export const NATIVE_CONNECTOR_DEFINITIONS: Record<string, NativeConnector | unde
           },
         ],
         display: DisplayType.TEXTBOX,
-        label: i18n.translate('searchConnectors.nativeConnectors.confluenceDataCenter.passwordLabel', {
-          defaultMessage: 'Confluence Data Center password',
-        }),
+        label: i18n.translate(
+          'searchConnectors.nativeConnectors.confluenceDataCenter.passwordLabel',
+          {
+            defaultMessage: 'Confluence Data Center password',
+          }
+        ),
         options: [],
         order: 5,
         required: true,
