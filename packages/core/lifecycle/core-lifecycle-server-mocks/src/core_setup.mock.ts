8f981996c628235eab6ffbf5c3e0d4ce84d65d13
pgayvallet <pierre.gayvallet@elastic.co>
2024-01-24 13:09:57 +0100

    start adapting mocks

--- packages/core/lifecycle/core-lifecycle-server-mocks/src/core_setup.mock.ts
+++ packages/core/lifecycle/core-lifecycle-server-mocks/src/core_setup.mock.ts
@@ -29,6 +29,7 @@ import { userSettingsServiceMock } from '@kbn/core-user-settings-server-mocks';
 import { securityServiceMock } from '@kbn/core-security-server-mocks';
 import { userProfileServiceMock } from '@kbn/core-user-profile-server-mocks';
 import { createCoreStartMock } from './core_start.mock';
+import { injectionServiceMock } from '@kbn/core-di-server-mocks';
 
 type CoreSetupMockType = MockedKeys<CoreSetup> & {
   elasticsearch: ReturnType<typeof elasticsearchServiceMock.createSetup>;
@@ -78,6 +79,7 @@ export function createCoreSetupMock({
       onSetup: jest.fn(),
       onStart: jest.fn(),
     },
+    injection: injectionServiceMock.createSetupContract(),
     getStartServices: jest
       .fn<Promise<[ReturnType<typeof createCoreStartMock>, object, any]>, []>()
       .mockResolvedValue([createCoreStartMock(), pluginStartDeps, pluginStartContract]),
