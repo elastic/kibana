8896c1b8d64dbb12a4a72113272cd7cf67afcdcd
Tiago Costa <tiago.costa@elastic.co>
2024-01-25 15:05:49 +0000

    skip flaky suite (#175313)

--- x-pack/plugins/cases/public/components/user_actions/property_actions/user_comment_property_actions.test.tsx
+++ x-pack/plugins/cases/public/components/user_actions/property_actions/user_comment_property_actions.test.tsx
@@ -20,6 +20,7 @@ import { waitFor } from '@testing-library/react';
 // FLAKY: https://github.com/elastic/kibana/issues/175310
 // FLAKY: https://github.com/elastic/kibana/issues/175311
 // FLAKY: https://github.com/elastic/kibana/issues/175312
+// FLAKY: https://github.com/elastic/kibana/issues/175313
 describe.skip('UserCommentPropertyActions', () => {
   let appMock: AppMockRenderer;
 
