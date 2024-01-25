70b3a710bd4c77eb6b090f01cab3591ad89c9373
gbamparop <georgios.bamparopoulos@elastic.co>
2024-01-25 11:59:47 +0000

    Update rootDir for jest config

--- x-pack/plugins/observability_solution/logs_explorer/jest.config.js
+++ x-pack/plugins/observability_solution/logs_explorer/jest.config.js
@@ -7,7 +7,7 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
+  rootDir: '../../../..',
   roots: ['<rootDir>/x-pack/plugins/observability_solution/logs_explorer'],
   coverageDirectory:
     '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/observability_solution/logs_explorer',
