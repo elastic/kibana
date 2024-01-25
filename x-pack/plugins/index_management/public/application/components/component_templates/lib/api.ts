7ca07479bd577aac38a3415917041f5f5cd55746
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2024-01-25 09:40:25 -0500

    add tests and fix typos

--- x-pack/plugins/index_management/public/application/components/component_templates/lib/api.ts
+++ x-pack/plugins/index_management/public/application/components/component_templates/lib/api.ts
@@ -97,7 +97,7 @@ export const getApi = (
 
   async function postDataStreamRollover(name: string) {
     return sendRequest<ComponentTemplateDatastreams>({
-      path: `${apiBasePath}/data_streams/${encodeURIComponent(name)}/rollover?lazy`,
+      path: `${apiBasePath}/data_streams/${encodeURIComponent(name)}/rollover`,
       method: 'post',
     });
   }
