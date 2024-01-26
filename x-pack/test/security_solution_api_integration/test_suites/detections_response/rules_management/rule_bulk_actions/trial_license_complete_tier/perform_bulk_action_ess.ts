1851786698f5578bface25e11ed8faa3b51b447f
Nikita Indik <nikita.indik@elastic.co>
2024-01-26 15:35:20 +0100

    Rebase changes

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/rule_bulk_actions/trial_license_complete_tier/perform_bulk_action_ess.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/rule_bulk_actions/trial_license_complete_tier/perform_bulk_action_ess.ts
@@ -28,7 +28,6 @@ import {
   getLegacyActionSO,
   getSimpleRule,
   getWebHookAction,
-  removeServerGeneratedProperties,
   waitForRuleSuccess,
   getRuleSOById,
   createRuleThroughAlertingEndpoint,
@@ -64,7 +63,7 @@ export default ({ getService }: FtrProviderContext): void => {
   const createWebHookConnector = () => createConnector(getWebHookAction());
 
   // Failing: See https://github.com/elastic/kibana/issues/173804
-  describe.skip('@ess perform_bulk_action - ESS specific logic', () => {
+  describe('@ess perform_bulk_action - ESS specific logic', () => {
     beforeEach(async () => {
       await createAlertsIndex(supertest, log);
       await esArchiver.load('x-pack/test/functional/es_archives/auditbeat/hosts');
@@ -481,10 +480,12 @@ export default ({ getService }: FtrProviderContext): void => {
           supertest,
           getRuleSavedObjectWithLegacyInvestigationFields()
         );
+
         ruleWithLegacyInvestigationFieldEmptyArray = await createRuleThroughAlertingEndpoint(
           supertest,
           getRuleSavedObjectWithLegacyInvestigationFieldsEmptyArray()
         );
+
         ruleWithIntendedInvestigationField = await createRule(supertest, log, {
           ...getSimpleRule('rule-with-investigation-field'),
           name: 'Test investigation fields object',
@@ -505,23 +506,26 @@ export default ({ getService }: FtrProviderContext): void => {
           .parse(binaryToString);
 
         const [rule1, rule2, rule3, exportDetailsJson] = body.toString().split(/\n/);
+        const exportedRules = [rule1, rule2, rule3].map((rule) => JSON.parse(rule));
 
-        const ruleToCompareWithLegacyInvestigationField = removeServerGeneratedProperties(
-          JSON.parse(rule1)
+        const exportedRuleWithLegacyInvestigationField = exportedRules.find(
+          (rule) => rule.id === ruleWithLegacyInvestigationField.id
         );
-        expect(ruleToCompareWithLegacyInvestigationField.investigation_fields).to.eql({
+        expect(exportedRuleWithLegacyInvestigationField.investigation_fields).to.eql({
           field_names: ['client.address', 'agent.name'],
         });
 
-        const ruleToCompareWithLegacyInvestigationFieldEmptyArray = removeServerGeneratedProperties(
-          JSON.parse(rule2)
+        const exportedRuleWithLegacyInvestigationFieldEmptyArray = exportedRules.find(
+          (rule) => rule.id === ruleWithLegacyInvestigationFieldEmptyArray.id
         );
-        expect(ruleToCompareWithLegacyInvestigationFieldEmptyArray.investigation_fields).to.eql(
+        expect(exportedRuleWithLegacyInvestigationFieldEmptyArray.investigation_fields).to.eql(
           undefined
         );
 
-        const ruleWithInvestigationField = removeServerGeneratedProperties(JSON.parse(rule3));
-        expect(ruleWithInvestigationField.investigation_fields).to.eql({
+        const exportedRuleWithInvestigationField = exportedRules.find(
+          (rule) => rule.id === ruleWithIntendedInvestigationField.id
+        );
+        expect(exportedRuleWithInvestigationField.investigation_fields).to.eql({
           field_names: ['host.name'],
         });
 
