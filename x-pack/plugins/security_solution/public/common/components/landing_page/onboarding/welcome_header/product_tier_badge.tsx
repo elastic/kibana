0c1dcd5f613332c2a71974225a9656f60425ff88
Angela Chuang <yi-chun.chuang@elastic.co>
2024-01-27 01:45:55 +0800

    current plan component

--- x-pack/plugins/security_solution/public/common/components/landing_page/onboarding/welcome_header/product_tier_badge.tsx
+++ x-pack/plugins/security_solution/public/common/components/landing_page/onboarding/welcome_header/product_tier_badge.tsx
@@ -6,27 +6,24 @@
  */
 
 import React from 'react';
-import { EuiBadge, useEuiTheme } from '@elastic/eui';
-import { css } from '@emotion/css';
+import { EuiBadge } from '@elastic/eui';
+
+import classnames from 'classnames';
 import { PRODUCT_TIER_ESSENTIAL, PRODUCT_TIER_COMPLETE } from './translations';
 import { ProductTier } from '../configs';
+import { useProductTierBadgeStyles } from '../styles/product_tier_badge.styles';
 
-const ProductTierBadgeComponent = ({ productTier }: { productTier: ProductTier | undefined }) => {
-  const { euiTheme } = useEuiTheme();
-  return productTier ? (
-    <EuiBadge
-      color="warning"
-      className="eui-alignMiddle"
-      css={css`
-        font-size: ${euiTheme.size.m};
-        line-height: ${euiTheme.size.m};
-      `}
-      data-test-subj="product-tier-badge"
-    >
-      <strong>
-        {productTier === ProductTier.essentials && PRODUCT_TIER_ESSENTIAL}
-        {productTier === ProductTier.complete && PRODUCT_TIER_COMPLETE}
-      </strong>
+const PRODUCT_TIER_TRANSLATES: { [key: string]: string } = {
+  [ProductTier.essentials]: PRODUCT_TIER_ESSENTIAL,
+  [ProductTier.complete]: PRODUCT_TIER_COMPLETE,
+};
+
+const ProductTierBadgeComponent = ({ currentPlan }: { currentPlan: string | undefined }) => {
+  const { wrapperStyles, textStyles } = useProductTierBadgeStyles();
+  const wrapperClassNames = classnames('eui-alignMiddle', wrapperStyles);
+  return currentPlan ? (
+    <EuiBadge color="warning" className={wrapperClassNames} data-test-subj="product-tier-badge">
+      <strong className={textStyles}>{PRODUCT_TIER_TRANSLATES[currentPlan] ?? currentPlan}</strong>
     </EuiBadge>
   ) : null;
 };
