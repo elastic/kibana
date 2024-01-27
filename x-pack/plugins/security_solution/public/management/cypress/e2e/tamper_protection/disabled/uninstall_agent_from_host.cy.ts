05b0abf200c4145968e817591da4523192dc0b58
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-01-26 19:04:41 -0500

    skip failing test suite (#170667)

--- x-pack/plugins/security_solution/public/management/cypress/e2e/tamper_protection/disabled/uninstall_agent_from_host.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/tamper_protection/disabled/uninstall_agent_from_host.cy.ts
@@ -21,57 +21,54 @@ import { enableAllPolicyProtections } from '../../../tasks/endpoint_policy';
 import { createEndpointHost } from '../../../tasks/create_endpoint_host';
 import { deleteAllLoadedEndpointData } from '../../../tasks/delete_all_endpoint_data';
 
-describe(
-  'Uninstall agent from host when agent tamper protection is disabled',
-  { tags: ['@ess'] },
-  () => {
-    let indexedPolicy: IndexedFleetEndpointPolicyResponse;
-    let policy: PolicyData;
-    let createdHost: CreateAndEnrollEndpointHostResponse;
+// Failing: See https://github.com/elastic/kibana/issues/170667
+describe.skip('Uninstall agent from host when agent tamper protection is disabled', { tags: ['@ess'] }, () => {
+  let indexedPolicy: IndexedFleetEndpointPolicyResponse;
+  let policy: PolicyData;
+  let createdHost: CreateAndEnrollEndpointHostResponse;
 
-    before(() => {
-      getEndpointIntegrationVersion().then((version) => {
-        createAgentPolicyTask(version).then((data) => {
-          indexedPolicy = data;
-          policy = indexedPolicy.integrationPolicies[0];
-          return enableAllPolicyProtections(policy.id);
-        });
+  before(() => {
+    getEndpointIntegrationVersion().then((version) => {
+      createAgentPolicyTask(version).then((data) => {
+        indexedPolicy = data;
+        policy = indexedPolicy.integrationPolicies[0];
+        return enableAllPolicyProtections(policy.id);
       });
     });
+  });
 
-    beforeEach(() => {
-      login();
-      // Create and enroll a new Endpoint host
-      return createEndpointHost(policy.policy_id).then((host) => {
-        createdHost = host as CreateAndEnrollEndpointHostResponse;
+  beforeEach(() => {
+    login();
+    // Create and enroll a new Endpoint host
+    return createEndpointHost(policy.policy_id).then((host) => {
+      createdHost = host as CreateAndEnrollEndpointHostResponse;
+    });
+  });
+
+  after(() => {
+    if (indexedPolicy) {
+      cy.task('deleteIndexedFleetEndpointPolicies', indexedPolicy);
+    }
+  });
+
+  afterEach(() => {
+    if (createdHost) {
+      cy.task('destroyEndpointHost', createdHost);
+    }
+
+    if (createdHost) {
+      deleteAllLoadedEndpointData({ endpointAgentIds: [createdHost.agentId] });
+    }
+  });
+
+  it('should uninstall from host without issues', () => {
+    waitForEndpointListPageToBeLoaded(createdHost.hostname);
+    uninstallAgentFromHost(createdHost.hostname).then((response) => {
+      expect(response).to.not.match(/(.*)Invalid uninstall token(.*)/);
+      expect(response).to.match(/(.*)Elastic Agent has been uninstalled(.*)/);
+      isAgentAndEndpointUninstalledFromHost(createdHost.hostname).then((isUninstalled) => {
+        expect(isUninstalled).to.eql(true);
       });
     });
-
-    after(() => {
-      if (indexedPolicy) {
-        cy.task('deleteIndexedFleetEndpointPolicies', indexedPolicy);
-      }
-    });
-
-    afterEach(() => {
-      if (createdHost) {
-        cy.task('destroyEndpointHost', createdHost);
-      }
-
-      if (createdHost) {
-        deleteAllLoadedEndpointData({ endpointAgentIds: [createdHost.agentId] });
-      }
-    });
-
-    it('should uninstall from host without issues', () => {
-      waitForEndpointListPageToBeLoaded(createdHost.hostname);
-      uninstallAgentFromHost(createdHost.hostname).then((response) => {
-        expect(response).to.not.match(/(.*)Invalid uninstall token(.*)/);
-        expect(response).to.match(/(.*)Elastic Agent has been uninstalled(.*)/);
-        isAgentAndEndpointUninstalledFromHost(createdHost.hostname).then((isUninstalled) => {
-          expect(isUninstalled).to.eql(true);
-        });
-      });
-    });
-  }
-);
+  });
+});
