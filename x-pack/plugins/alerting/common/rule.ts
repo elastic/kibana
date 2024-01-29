dfa0e4776c978a88ab0869259302abff56f93b3c
Thom Heymann <thom.heymann@elastic.co>
2024-01-29 17:28:12 +0000

    Loosen types

--- x-pack/plugins/alerting/common/rule.ts
+++ x-pack/plugins/alerting/common/rule.ts
@@ -18,8 +18,8 @@ import { RuleSnooze } from './rule_snooze_type';
 export type { ActionVariable } from '@kbn/alerting-types';
 
 export type RuleTypeState = Record<string, unknown>;
-export type RuleTypeParams = {}; // eslint-disable-line @typescript-eslint/consistent-type-definitions
-export type RuleTypeMetaData = {}; // eslint-disable-line @typescript-eslint/consistent-type-definitions
+export type RuleTypeParams = Record<string, unknown>;
+export type RuleTypeMetaData = Record<string, unknown>;
 
 // rule type defined alert fields to persist in alerts index
 export type RuleAlertData = Record<string, unknown>;
