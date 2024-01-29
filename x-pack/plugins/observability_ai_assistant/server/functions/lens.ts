fcd9dc5cf20f808421f9f4385a5d8699a2ebc30b
ppisljar <peter.pisljar@elastic.co>
2024-01-29 12:19:27 +0100

    fixing

--- x-pack/plugins/observability_ai_assistant/server/functions/lens.ts
+++ x-pack/plugins/observability_ai_assistant/server/functions/lens.ts
@@ -4,11 +4,53 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { lensFunctionDefinition } from '../../common/functions/lens';
+import { lensTableFunctionDefinition } from '../../common/functions/lens_table';
+import { lensGaugeFunctionDefinition } from '../../common/functions/lens_gauge';
+import { lensHeatmapFunctionDefinition } from '../../common/functions/lens_heatmap';
+import { lensMetricFunctionDefinition } from '../../common/functions/lens_metric';
+import { lensRegionmapFunctionDefinition } from '../../common/functions/lens_regionmap';
+import { lensXYFunctionDefinition } from '../../common/functions/lens_xy';
+import { lensTreemapFunctionDefinition } from '../../common/functions/lens_treemap';
+import { lensTagcloudFunctionDefinition } from '../../common/functions/lens_tagcloud';
 import { RegisterFunction } from '../service/types';
 
 export function registerLensFunction({ registerFunction }: { registerFunction: RegisterFunction }) {
-  registerFunction(lensFunctionDefinition, async () => {
+  registerFunction(lensGaugeFunctionDefinition, async () => {
+    return {
+      content: {},
+    };
+  });
+  registerFunction(lensHeatmapFunctionDefinition, async () => {
+    return {
+      content: {},
+    };
+  });
+  registerFunction(lensMetricFunctionDefinition, async () => {
+    return {
+      content: {},
+    };
+  });
+  registerFunction(lensRegionmapFunctionDefinition, async () => {
+    return {
+      content: {},
+    };
+  });
+  registerFunction(lensTableFunctionDefinition, async () => {
+    return {
+      content: {},
+    };
+  });
+  registerFunction(lensTagcloudFunctionDefinition, async () => {
+    return {
+      content: {},
+    };
+  });
+  registerFunction(lensTreemapFunctionDefinition, async () => {
+    return {
+      content: {},
+    };
+  });
+  registerFunction(lensXYFunctionDefinition, async () => {
     return {
       content: {},
     };
