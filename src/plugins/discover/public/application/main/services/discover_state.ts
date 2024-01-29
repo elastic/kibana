0b4fda2a180b09269b2cc3da1c54c19b9eef97c1
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-01-29 17:00:54 +0100

    [Discover] Fix issue with pinned filters when openning a saved search

--- src/plugins/discover/public/application/main/services/discover_state.ts
+++ src/plugins/discover/public/application/main/services/discover_state.ts
@@ -356,6 +356,7 @@ export function getDiscoverStateContainer({
       dataStateContainer,
       internalStateContainer,
       savedSearchContainer,
+      globalStateContainer,
       services,
       setDataView,
     });
