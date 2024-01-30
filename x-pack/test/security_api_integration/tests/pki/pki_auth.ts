ebb5709f0b657853162b24a7c04cb3d266bf3fba
Sid Mantri <sid.mantri@elastic.co>
2024-01-30 14:51:14 +0100

    fix pki test

--- x-pack/test/security_api_integration/tests/pki/pki_auth.ts
+++ x-pack/test/security_api_integration/tests/pki/pki_auth.ts
@@ -80,7 +80,7 @@ export default function ({ getService }: FtrProviderContext) {
         .expect(401);
 
       expect(unauthenticatedResponse.headers['content-security-policy']).to.be.a('string');
-      expect(unauthenticatedResponse.text).to.contain('error');
+      expect(unauthenticatedResponse.text).to.contain('<div/>');
     });
 
     it('does not prevent basic login', async () => {
