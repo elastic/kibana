eb5697d4df916048be0430b4879812dc3807c76f
dej611 <dej611@gmail.com>
2024-01-30 15:42:04 +0100

    :fire: Remove unused branches and fns

--- packages/kbn-monaco/src/esql/lib/ast/shared/helpers.ts
+++ packages/kbn-monaco/src/esql/lib/ast/shared/helpers.ts
@@ -330,11 +330,6 @@ export function isEqualType(
   if (item.type === 'literal') {
     return compareLiteralType(argType, item);
   }
-  if (item.type === 'list') {
-    const listType = `${item.values[0].literalType}[]`;
-    // argType = 'list' means any list value is ok
-    return argType === item.type || argType === listType;
-  }
   if (item.type === 'function') {
     if (isSupportedFunction(item.name, parentCommand).supported) {
       const fnDef = buildFunctionLookup().get(item.name)!;
@@ -356,32 +351,6 @@ export function isEqualType(
     const wrappedTypes = Array.isArray(hit.type) ? hit.type : [hit.type];
     return wrappedTypes.some((ct) => argType === ct);
   }
-  if (item.type === 'source') {
-    return item.sourceType === argType;
-  }
-}
-
-export function endsWithOpenBracket(text: string) {
-  return /\($/.test(text);
-}
-
-export function isDateFunction(fnName: string) {
-  // TODO: improve this and rely in signature in the future
-  return ['to_datetime', 'date_trunc', 'date_parse'].includes(fnName.toLowerCase());
-}
-
-export function getDateMathOperation() {
-  return builtinFunctions.filter(({ name }) => ['+', '-'].includes(name));
-}
-
-export function getDurationItemsWithQuantifier(quantifier: number = 1) {
-  return timeLiterals
-    .filter(({ name }) => !/s$/.test(name))
-    .map(({ name, ...rest }) => ({
-      label: `${quantifier} ${name}`,
-      insertText: `${quantifier} ${name}`,
-      ...rest,
-    }));
 }
 
 function fuzzySearch(fuzzyName: string, resources: IterableIterator<string>) {
