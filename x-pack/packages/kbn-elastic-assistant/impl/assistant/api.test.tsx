6eacc9b8e5cc684bbc536ccebdd626da411886bb
Garrett Spong <garrett.spong@elastic.co>
2024-01-31 13:48:07 -0700

    Revert "This reverts commit 38f0a7aa46199beb1e8aa41b68b8e604f18fc554."
    This reverts commit 64f490820aba73078b4bccca6ea61aafdc1b1ac1.

--- x-pack/packages/kbn-elastic-assistant/impl/assistant/api.test.tsx
+++ x-pack/packages/kbn-elastic-assistant/impl/assistant/api.test.tsx
@@ -13,7 +13,6 @@ import {
   fetchConnectorExecuteAction,
   FetchConnectorExecuteAction,
   getKnowledgeBaseStatus,
-  postEvaluation,
   postKnowledgeBase,
 } from './api';
 import type { Conversation, Message } from '../assistant_context/types';
@@ -338,54 +337,6 @@ describe('API tests', () => {
       });
 
       await expect(deleteKnowledgeBase(knowledgeBaseArgs)).resolves.toThrowError('simulated error');
-    });
-  });
-
-  describe('postEvaluation', () => {
-    it('calls the knowledge base API when correct resource path', async () => {
-      (mockHttp.fetch as jest.Mock).mockResolvedValue({ success: true });
-      const testProps = {
-        http: mockHttp,
-        evalParams: {
-          agents: ['not', 'alphabetical'],
-          dataset: '{}',
-          datasetName: 'Test Dataset',
-          projectName: 'Test Project Name',
-          runName: 'Test Run Name',
-          evalModel: ['not', 'alphabetical'],
-          evalPrompt: 'evalPrompt',
-          evaluationType: ['not', 'alphabetical'],
-          models: ['not', 'alphabetical'],
-          outputIndex: 'outputIndex',
-        },
-      };
-
-      await postEvaluation(testProps);
-
-      expect(mockHttp.fetch).toHaveBeenCalledWith('/internal/elastic_assistant/evaluate', {
-        method: 'POST',
-        body: '{"dataset":{},"evalPrompt":"evalPrompt"}',
-        headers: { 'Content-Type': 'application/json' },
-        query: {
-          models: 'alphabetical,not',
-          agents: 'alphabetical,not',
-          datasetName: 'Test Dataset',
-          evaluationType: 'alphabetical,not',
-          evalModel: 'alphabetical,not',
-          outputIndex: 'outputIndex',
-          projectName: 'Test Project Name',
-          runName: 'Test Run Name',
-        },
-        signal: undefined,
-      });
-    });
-    it('returns error when error is an error', async () => {
-      const error = 'simulated error';
-      (mockHttp.fetch as jest.Mock).mockImplementation(() => {
-        throw new Error(error);
-      });
-
-      await expect(postEvaluation(knowledgeBaseArgs)).resolves.toThrowError('simulated error');
     });
   });
 });
