ec2da39e94efe2a7accfd72a5ab5604a19f67a23
Alex Szabo <alex.szabo@elastic.co>
2024-01-31 15:55:29 +0100

    chore: add linebreaks for more readability

--- packages/kbn-test/src/jest/result_processors/logging_result_processor.js
+++ packages/kbn-test/src/jest/result_processors/logging_result_processor.js
@@ -8,8 +8,10 @@
 
 const FAILURE_MESSAGE_TRIGGERS = ['but is not defined anymore'];
 const log = (...args) => {
-  const loggable = args.map((arg) => (typeof arg === 'string' ? arg : JSON.stringify(arg)));
-  process.stdout.write(`${loggable.join(' ')}\n`);
+  const loggable = args.map((arg) =>
+    typeof arg === 'string' ? arg : JSON.stringify(arg, null, 2)
+  );
+  process.stdout.write(`${loggable.join(' ')}\n`, 'utf8');
 };
 /**
  * This processor looks for specific errors, and logs the result context of test suites where they occur.
@@ -26,7 +28,7 @@ module.exports = (results) => {
   if (resultsThatMatchTriggers.length !== 0) {
     log('The following test suites failed, with notable errors:');
     resultsThatMatchTriggers.forEach((e) => {
-      log(` -> ${e.testFilePath}`, 'Details: ', e);
+      log(` -> ${e.testFilePath}`, 'Details: ', e, '\n');
     });
   }
 
