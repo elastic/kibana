e21e8233fbd001daa4eda8352a6213cb875c94ac
Samiul Monir <samiul.monir@elastic.co>
2024-02-01 08:49:08 -0500

    Fix broken indices test due to refactor in indices nav items

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/indices/indices_nav.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/indices/indices_nav.test.tsx
@@ -12,10 +12,10 @@ import { shallow } from 'enzyme';
 
 import { mockIndexNameValues } from '../_mocks_/index_name_logic.mock';
 
-jest.mock('../../../shared/layout', () => ({
+jest.mock('../../../../shared/layout', () => ({
   generateNavLink: jest.fn(({ to }) => ({ href: to })),
 }));
-jest.mock('../../../enterprise_search_content/utils/indices');
+jest.mock('../../../utils/indices');
 
 import { isConnectorIndex, isCrawlerIndex } from '../../../utils/indices';
 
@@ -47,24 +47,26 @@ describe('useIndicesNav', () => {
         name: 'index-name',
         renderItem: expect.any(Function),
         'data-test-subj': 'IndexLabel',
-      },
-      {
-        id: 'overview',
-        name: 'Overview',
-        href: '/search_indices/index-name',
-        'data-test-subj': 'IndexOverviewLink',
-      },
-      {
-        id: 'documents',
-        name: 'Documents',
-        href: '/search_indices/index-name/documents',
-        'data-test-subj': 'IndexDocumentsLink',
-      },
-      {
-        id: 'index_mappings',
-        name: 'Index mappings',
-        href: '/search_indices/index-name/index_mappings',
-        'data-test-subj': 'IndexIndexMappingsLink',
+        items: [
+          {
+            id: 'overview',
+            name: 'Overview',
+            href: '/search_indices/index-name',
+            'data-test-subj': 'IndexOverviewLink',
+          },
+          {
+            id: 'documents',
+            name: 'Documents',
+            href: '/search_indices/index-name/documents',
+            'data-test-subj': 'IndexDocumentsLink',
+          },
+          {
+            id: 'index_mappings',
+            name: 'Index mappings',
+            href: '/search_indices/index-name/index_mappings',
+            'data-test-subj': 'IndexIndexMappingsLink',
+          },
+        ],
       },
     ];
 
@@ -90,63 +92,72 @@ describe('useIndicesNav', () => {
         ...mockIndexNameValues,
         productFeatures: { hasDefaultIngestPipeline: true },
       });
-      const baseNavWithPipelines = [
-        ...BASE_NAV,
-        {
-          id: 'pipelines',
-          name: 'Pipelines',
-          href: '/search_indices/index-name/pipelines',
-          'data-test-subj': 'IndexPipelineLink',
-        },
-      ];
+
+      const pipelineItem = {
+        id: 'pipelines',
+        name: 'Pipelines',
+        href: '/search_indices/index-name/pipelines',
+        'data-test-subj': 'IndexPipelineLink',
+      };
+      const baseNavWithPipelines = BASE_NAV.map(navItem => ({
+        ...navItem,
+        items: [...navItem.items, pipelineItem]
+      }));
+
       expect(useIndicesNav()).toEqual(baseNavWithPipelines);
     });
 
     describe('connectors nav', () => {
       it('returns connectors nav with default navs when isConnectorIndex is true', () => {
         jest.mocked(isConnectorIndex).mockReturnValueOnce(true);
-        const baseNavWithConnectors = [
-          ...BASE_NAV,
-          {
-            'data-test-subj': 'IndexConnectorsConfigurationLink',
-            id: 'connectors_configuration',
-            name: 'Configuration',
-            href: '/search_indices/index-name/configuration',
-          },
-          {
-            'data-test-subj': 'IndexSchedulingLink',
-            id: 'scheduling',
-            name: 'Scheduling',
-            href: '/search_indices/index-name/scheduling',
-          },
-        ];
+
+        const configuration = {
+          'data-test-subj': 'IndexConnectorsConfigurationLink',
+          id: 'connectors_configuration',
+          name: 'Configuration',
+          href: '/search_indices/index-name/configuration',
+        };
+        const scheduling = {
+          'data-test-subj': 'IndexSchedulingLink',
+          id: 'scheduling',
+          name: 'Scheduling',
+          href: '/search_indices/index-name/scheduling',
+        };
+        const baseNavWithConnectors = BASE_NAV.map(navItem => ({
+          ...navItem,
+          items: [...navItem.items, configuration, scheduling]
+        }));
+        
         expect(useIndicesNav()).toEqual(baseNavWithConnectors);
       });
 
       it('returns connectors nav with sync rules with default navs when isConnectorIndex is true and hasFilteringFeature is true', () => {
         jest.mocked(isConnectorIndex).mockReturnValueOnce(true);
         setMockValues({ ...mockIndexNameValues, hasFilteringFeature: true });
-        const baseNavWithConnectors = [
-          ...BASE_NAV,
-          {
-            'data-test-subj': 'IndexConnectorsConfigurationLink',
-            id: 'connectors_configuration',
-            name: 'Configuration',
-            href: '/search_indices/index-name/configuration',
-          },
-          {
-            'data-test-subj': 'IndexSyncRulesLink',
-            id: 'syncRules',
-            name: 'Sync rules',
-            href: '/search_indices/index-name/sync_rules',
-          },
-          {
-            'data-test-subj': 'IndexSchedulingLink',
-            id: 'scheduling',
-            name: 'Scheduling',
-            href: '/search_indices/index-name/scheduling',
-          },
-        ];
+
+        const configuration = {
+          'data-test-subj': 'IndexConnectorsConfigurationLink',
+          id: 'connectors_configuration',
+          name: 'Configuration',
+          href: '/search_indices/index-name/configuration',
+        };
+        const syncRules = {
+          'data-test-subj': 'IndexSyncRulesLink',
+          id: 'syncRules',
+          name: 'Sync rules',
+          href: '/search_indices/index-name/sync_rules',
+        };
+        const scheduling = {
+          'data-test-subj': 'IndexSchedulingLink',
+          id: 'scheduling',
+          name: 'Scheduling',
+          href: '/search_indices/index-name/scheduling',
+        };
+        const baseNavWithConnectors = BASE_NAV.map(navItem => ({
+          ...navItem,
+          items: [...navItem.items, configuration, syncRules, scheduling]
+        }));
+
         expect(useIndicesNav()).toEqual(baseNavWithConnectors);
       });
     });
@@ -154,27 +165,30 @@ describe('useIndicesNav', () => {
     describe('crawlers nav', () => {
       it('returns crawlers nav with default navs when isCrawlerIndex is true', () => {
         jest.mocked(isCrawlerIndex).mockReturnValueOnce(true);
-        const baseNavWithCrawlers = [
-          ...BASE_NAV,
-          {
-            'data-test-subj': 'IndexDomainManagementLink',
-            id: 'domain_management',
-            name: 'Manage Domains',
-            href: '/search_indices/index-name/domain_management',
-          },
-          {
-            'data-test-subj': 'IndexCrawlerConfigurationLink',
-            id: 'crawler_configuration',
-            name: 'Configuration',
-            href: '/search_indices/index-name/crawler_configuration',
-          },
-          {
-            'data-test-subj': 'IndexCrawlerSchedulingLink',
-            id: 'crawler_scheduling',
-            name: 'Scheduling',
-            href: '/search_indices/index-name/scheduling',
-          },
-        ];
+
+        const domainManagement = {
+          'data-test-subj': 'IndexDomainManagementLink',
+          id: 'domain_management',
+          name: 'Manage Domains',
+          href: '/search_indices/index-name/domain_management',
+        };
+        const configuration = {
+          'data-test-subj': 'IndexCrawlerConfigurationLink',
+          id: 'crawler_configuration',
+          name: 'Configuration',
+          href: '/search_indices/index-name/crawler_configuration',
+        };
+        const scheduling = {
+          'data-test-subj': 'IndexCrawlerSchedulingLink',
+          id: 'crawler_scheduling',
+          name: 'Scheduling',
+          href: '/search_indices/index-name/scheduling',
+        };
+        const baseNavWithCrawlers = BASE_NAV.map(navItem => ({
+          ...navItem,
+          items: [...navItem.items, domainManagement, configuration, scheduling]
+        }));
+
         expect(useIndicesNav()).toEqual(baseNavWithCrawlers);
       });
     });
