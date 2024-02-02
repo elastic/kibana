e852c3589c41c5d7e7d0640a7e509b4f083a0743
Mike Pellegrini <mike.pellegrini@elastic.co>
2024-02-02 17:09:41 -0500

    Added PipelineSelect tests

---
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/ml_inference/pipeline_select.test.tsx
@@ -0,0 +1,69 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { setMockValues } from '../../../../../__mocks__/kea_logic';
+
+import React from 'react';
+
+import { shallow } from 'enzyme';
+
+import { EuiSelectable } from '@elastic/eui';
+
+import { PipelineSelect } from './pipeline_select';
+
+const DEFAULT_VALUES = {
+  addInferencePipelineModal: {
+    configuration: {
+      pipelineName: '', // TODO: Should this be null?
+    },
+  },
+  existingInferencePipelines: [],
+};
+
+describe('PipelineSelect', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    setMockValues({});
+  });
+  it('limits height to 4.5 options', () => {
+    setMockValues({
+      ...DEFAULT_VALUES,
+      existingInferencePipelines: [
+        {
+          modelId: 'model_1',
+          modelType: 'model_1_type',
+          pipelineName: 'pipeline_1',
+        },
+        {
+          modelId: 'model_2',
+          modelType: 'model_2_type',
+          pipelineName: 'pipeline_2',
+        },
+        {
+          modelId: 'model_3',
+          modelType: 'model_3_type',
+          pipelineName: 'pipeline_3',
+        },
+        {
+          modelId: 'model_4',
+          modelType: 'model_4_type',
+          pipelineName: 'pipeline_4',
+        },
+        {
+          modelId: 'model_5',
+          modelType: 'model_5_type',
+          pipelineName: 'pipeline_5',
+        },
+      ],
+    });
+
+    const wrapper = shallow(<PipelineSelect />);
+    expect(wrapper.find(EuiSelectable)).toHaveLength(1);
+    const selectable = wrapper.find(EuiSelectable);
+    expect(selectable.prop('height')).toEqual(16 * 6 * 4.5); // TODO: Use euiTheme.base to calculate height
+  });
+});
