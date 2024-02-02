a12394cbf7e02e20990ba95217bbccea98683be5
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2024-02-02 15:27:10 +0100

    unmount files in connector router

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/connector_detail/connector_detail_router.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/connector_detail/connector_detail_router.tsx
@@ -5,15 +5,34 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useActions } from 'kea';
 
 import { Routes, Route } from '@kbn/shared-ux-router';
 
 import { CONNECTOR_DETAIL_PATH, SEARCH_INDEX_TAB_PATH } from '../../routes';
 
+import { IndexNameLogic } from '../search_index/index_name_logic';
+
+import { IndexViewLogic } from '../search_index/index_view_logic';
+
 import { ConnectorDetail } from './connector_detail';
+import { ConnectorViewLogic } from './connector_view_logic';
 
 export const ConnectorDetailRouter: React.FC = () => {
+  const { stopFetchIndexPoll } = useActions(IndexViewLogic);
+  useEffect(() => {
+    const unmountName = IndexNameLogic.mount();
+    const unmountView = ConnectorViewLogic.mount();
+    const unmountIndexView = IndexViewLogic.mount();
+    return () => {
+      stopFetchIndexPoll();
+      unmountName();
+      unmountView();
+      unmountIndexView();
+    };
+  }, []);
   return (
     <Routes>
       <Route path={CONNECTOR_DETAIL_PATH} exact>
