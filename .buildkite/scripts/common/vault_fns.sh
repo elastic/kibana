843488ad1e6182a24dac57a65179a2d8a6d4c55c
Alex Szabo <alex.szabo@elastic.co>
2024-02-02 15:23:54 +0100

    chore: add stack trace to failed vault_gets

--- .buildkite/scripts/common/vault_fns.sh
+++ .buildkite/scripts/common/vault_fns.sh
@@ -12,6 +12,16 @@ else
 fi
 export IS_LEGACY_VAULT_ADDR
 
+print_stack() {
+  local i=0
+  local FRAMES=${#BASH_LINENO[@]}
+  for ((i=1; i < FRAMES; i++)); do
+    local frame=${BASH_LINENO[i]}
+    local source=${BASH_SOURCE[i]}
+    echo "  at $source:$frame" >&2
+  done
+}
+
 retry() {
   local retries=$1; shift
   local delay=$1; shift
@@ -24,6 +34,7 @@ retry() {
       return $retry_exit_status
     elif (( attempts == retries )); then
       echo "Failed $attempts retries" >&2
+      print_stack
       return $retry_exit_status
     else
       echo "Retrying $((retries - attempts)) more times..." >&2
