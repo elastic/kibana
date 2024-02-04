778ec198879c5156a95182f84bbf450492e70905
Maxim Kholod <maxim.kholod@elastic.co>
2024-02-04 12:18:29 +0100

    disable all tests except rules

--- x-pack/test/cloud_security_posture_functional/pages/index.ts
+++ x-pack/test/cloud_security_posture_functional/pages/index.ts
@@ -11,15 +11,15 @@ import { FtrProviderContext } from '../ftr_provider_context';
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('Cloud Security Posture', function () {
     loadTestFile(require.resolve('./rules'));
-    loadTestFile(require.resolve('./findings_onboarding'));
-    loadTestFile(require.resolve('./findings'));
-    loadTestFile(require.resolve('./findings_grouping'));
-    loadTestFile(require.resolve('./findings_alerts'));
-    loadTestFile(require.resolve('./compliance_dashboard'));
-    loadTestFile(require.resolve('./vulnerability_dashboard'));
-    loadTestFile(require.resolve('./cis_integration'));
-    loadTestFile(require.resolve('./findings_old_data'));
-    loadTestFile(require.resolve('./vulnerabilities'));
-    loadTestFile(require.resolve('./vulnerabilities_grouping'));
+    // loadTestFile(require.resolve('./findings_onboarding'));
+    // loadTestFile(require.resolve('./findings'));
+    // loadTestFile(require.resolve('./findings_grouping'));
+    // loadTestFile(require.resolve('./findings_alerts'));
+    // loadTestFile(require.resolve('./compliance_dashboard'));
+    // loadTestFile(require.resolve('./vulnerability_dashboard'));
+    // loadTestFile(require.resolve('./cis_integration'));
+    // loadTestFile(require.resolve('./findings_old_data'));
+    // loadTestFile(require.resolve('./vulnerabilities'));
+    // loadTestFile(require.resolve('./vulnerabilities_grouping'));
   });
 }
