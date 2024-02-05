097058ab09974664226d212e3d694f7488fda67c
Omolola Akinleye <omolola.akinleye@elastic.co>
2024-02-05 10:29:17 -0500

    remove cspm path

--- x-pack/performance/journeys/cloud_security_dashboard.ts
+++ x-pack/performance/journeys/cloud_security_dashboard.ts
@@ -48,6 +48,6 @@ export const journey = new Journey({
   //   maxDuration: '10m',
   // },
 }).step('Go to cloud security dashboards Page', async ({ page, kbnUrl }) => {
-  await page.goto(kbnUrl.get(`/app/security/cloud_security_posture/dashboard/cspm`));
+  await page.goto(kbnUrl.get(`/app/security/cloud_security_posture/dashboard`));
   await page.waitForSelector(subj('csp:dashboard-sections-table-header-score'));
 });
