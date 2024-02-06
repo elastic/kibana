bc076b2658d1376574bde8ca85023bb328512fcb
Sid Mantri <sid.mantri@elastic.co>
2024-02-06 17:08:50 +0100

    undo test changes

--- x-pack/test/security_api_integration/tests/anonymous/login.ts
+++ x-pack/test/security_api_integration/tests/anonymous/login.ts
@@ -121,7 +121,7 @@ export default function ({ getService }: FtrProviderContext) {
 
         expect(unauthenticatedResponse.headers['set-cookie']).to.be(undefined);
         expect(unauthenticatedResponse.headers['content-security-policy']).to.be.a('string');
-        expect(unauthenticatedResponse.text).to.contain('<div/>');
+        expect(unauthenticatedResponse.text).to.contain('error');
       });
     });
 
--- x-pack/test/security_api_integration/tests/kerberos/kerberos_login.ts
+++ x-pack/test/security_api_integration/tests/kerberos/kerberos_login.ts
@@ -102,7 +102,7 @@ export default function ({ getService }: FtrProviderContext) {
         // If browser and Kibana can successfully negotiate this HTML won't rendered, but if not
         // users will see a proper `Unauthenticated` page.
         expect(spnegoResponse.headers['content-security-policy']).to.be.a('string');
-        expect(spnegoResponse.text).to.contain('<div/>');
+        expect(spnegoResponse.text).to.contain('error');
       });
 
       it('AJAX requests should not initiate SPNEGO', async () => {
