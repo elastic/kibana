elastic/kibana

commit 4a2afa65a4b8c922f473117c32b73ec113fa8af0

[ML] AIOps: Adds link to log rate analysis from anomaly table (#175289)
## Summary

Part of #153753.

Adds an action to the actions menu of the ML anomaly table in both
Anomaly Explorer and Single Metric Viewer to Log Rate Analysis. The
action is available for the anomaly detection functions `count`,
`high_count`, `low_count`, `high_non_zero_count` and
`low_non_zero_count`.

- For multi-metric and population jobs, adds the current entity as a
Kuery search filter.
- Fixes an issue with restoring the correct time range from url state.
- Fixes an issue with restoring the correct query from url state.
- The action auto-selects an overall time range and baseline and
deviation time range based on the anomaly timestamp and multiples of
bucket spans.

Functional tests have been added that test the actions in both Anomaly
Explorer and Single Metric Viewer for single and multi metric `count`
job, as well as testing a job when the action should not be available.


[aiops-ad-link-0001.webm](https://github.com/elastic/kibana/assets/230104/c1d1336b-f62d-4861-b1a4-1a8ed9dbef4c)


### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed

https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/4929
- [x] This was checked for breaking API changes and was [labeled
appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

x-pack/plugins/aiops/public/components/log_rate_analysis/log_rate_analysis_content/log_rate_analysis_content.tsx
@@ -77,7 +77,7 @@ export interface LogRateAnalysisContentProps {
   /** Optional callback that exposes data of the completed analysis */
   onAnalysisCompleted?: (d: LogRateAnalysisResultsData) => void;
   /** Optional callback that exposes current window parameters */
-  onWindowParametersChange?: (wp?: WindowParameters) => void;
+  onWindowParametersChange?: (wp?: WindowParameters, replace?: boolean) => void;
   /** Identifier to indicate the plugin utilizing the component */
   embeddingOrigin: string;
 }
@@ -126,7 +126,7 @@ export const LogRateAnalysisContent: FC<LogRateAnalysisContentProps> = ({
     windowParametersTouched.current = true;
 
     if (onWindowParametersChange) {
-      onWindowParametersChange(windowParameters);
+      onWindowParametersChange(windowParameters, true);
     }
   }, [onWindowParametersChange, windowParameters]);
 

x-pack/plugins/aiops/public/components/log_rate_analysis/log_rate_analysis_page.tsx
@@ -113,10 +113,14 @@ export const LogRateAnalysisPage: FC<Props> = ({ stickyHistogram }) => {
 
   useEffect(() => {
     if (globalState?.time !== undefined) {
-      timefilter.setTime({
-        from: globalState.time.from,
-        to: globalState.time.to,
-      });
+      if (
+        !isEqual({ from: globalState.time.from, to: globalState.time.to }, timefilter.getTime())
+      ) {
+        timefilter.setTime({
+          from: globalState.time.from,
+          to: globalState.time.to,
+        });
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [JSON.stringify(globalState?.time), timefilter]);
@@ -136,11 +140,14 @@ export const LogRateAnalysisPage: FC<Props> = ({ stickyHistogram }) => {
     });
   }, [dataService, searchQueryLanguage, searchString]);
 
-  const onWindowParametersHandler = (wp?: WindowParameters) => {
-    if (!isEqual(wp, stateFromUrl.wp)) {
-      setUrlState({
-        wp: windowParametersToAppState(wp),
-      });
+  const onWindowParametersHandler = (wp?: WindowParameters, replace = false) => {
+    if (!isEqual(windowParametersToAppState(wp), stateFromUrl.wp)) {
+      setUrlState(
+        {
+          wp: windowParametersToAppState(wp),
+        },
+        replace
+      );
     }
   };
 

x-pack/plugins/aiops/public/hooks/use_data.ts
@@ -54,9 +54,14 @@ export const useData = (
     timeRangeSelector: selectedDataView?.timeFieldName !== undefined,
     autoRefreshSelector: true,
   });
+  const timeRangeMemoized = useMemo(
+    () => timefilter.getActiveBounds(),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [JSON.stringify(timefilter.getActiveBounds())]
+  );
 
   const fieldStatsRequest: DocumentStatsSearchStrategyParams | undefined = useMemo(() => {
-    const timefilterActiveBounds = timeRange ?? timefilter.getActiveBounds();
+    const timefilterActiveBounds = timeRange ?? timeRangeMemoized;
     if (timefilterActiveBounds !== undefined) {
       _timeBuckets.setInterval('auto');
       _timeBuckets.setBounds(timefilterActiveBounds);
@@ -72,7 +77,7 @@ export const useData = (
       };
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lastRefresh, searchQuery, timeRange]);
+  }, [lastRefresh, searchQuery, timeRange, timeRangeMemoized]);
 
   const overallStatsRequest = useMemo(() => {
     return fieldStatsRequest

x-pack/plugins/aiops/common/api/log_categorization/create_categorize_query.ts
@@ -10,11 +10,11 @@ import { cloneDeep } from 'lodash';
 import type { QueryDslQueryContainer } from '@elastic/elasticsearch/lib/api/types';
 
 export function createCategorizeQuery(
-  queryIn: QueryDslQueryContainer,
+  queryIn: QueryDslQueryContainer | undefined,
   timeField: string,
   timeRange: { from: number; to: number } | undefined
 ) {
-  const query = cloneDeep(queryIn);
+  const query = cloneDeep(queryIn ?? { match_all: {} });
 
   if (query.bool === undefined) {
     query.bool = {};

x-pack/plugins/aiops/public/application/url_state/common.test.ts
@@ -1,0 +1,22 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { isDefaultSearchQuery } from './common';
+
+describe('isDefaultSearchQuery', () => {
+  it('returns true for default search query', () => {
+    expect(isDefaultSearchQuery({ match_all: {} })).toBe(true);
+  });
+
+  it('returns false for non default search query', () => {
+    expect(
+      isDefaultSearchQuery({
+        bool: { must_not: [{ term: { 'the-term': 'the-value' } }] },
+      })
+    ).toBe(false);
+  });
+});

x-pack/plugins/aiops/public/application/url_state/common.ts
@@ -8,11 +8,16 @@
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 
 import type { Filter, Query } from '@kbn/es-query';
+import { isPopulatedObject } from '@kbn/ml-is-populated-object';
 
 import { SEARCH_QUERY_LANGUAGE, type SearchQueryLanguage } from '@kbn/ml-query-utils';
 
 const defaultSearchQuery = {
   match_all: {},
+} as const;
+
+export const isDefaultSearchQuery = (arg: unknown): arg is typeof defaultSearchQuery => {
+  return isPopulatedObject(arg, ['match_all']);
 };
 
 export interface AiOpsPageUrlState {

x-pack/plugins/aiops/public/components/log_categorization/use_discover_links.ts
@@ -56,10 +56,7 @@ export function useDiscoverLinks() {
       },
     });
 
-    let path = basePath.get();
-    path += '/app/discover#/';
-    path += '?_g=' + _g;
-    path += '&_a=' + encodeURIComponent(_a);
+    const path = `${basePath.get()}/app/discover#/?_g=${_g}&_a=${encodeURIComponent(_a)}`;
     window.open(path, '_blank');
   };
 

x-pack/plugins/aiops/public/hooks/use_search.ts
@@ -9,9 +9,15 @@ import { useMemo } from 'react';
 
 import type { DataView } from '@kbn/data-views-plugin/public';
 import type { SavedSearch } from '@kbn/saved-search-plugin/public';
+import { isQuery } from '@kbn/data-plugin/public';
 
 import { getEsQueryFromSavedSearch } from '../application/utils/search_utils';
-import type { AiOpsIndexBasedAppState } from '../application/url_state/common';
+import {
+  isDefaultSearchQuery,
+  type AiOpsIndexBasedAppState,
+} from '../application/url_state/common';
+import { createMergedEsQuery } from '../application/utils/search_utils';
+
 import { useAiopsAppContext } from './use_aiops_app_context';
 
 export const useSearch = (
@@ -37,23 +43,44 @@ export const useSearch = (
     [dataView, uiSettings, savedSearch, filterManager]
   );
 
-  if (searchData === undefined || (aiopsListState && aiopsListState.searchString !== '')) {
-    if (aiopsListState?.filters && readOnly === false) {
-      const globalFilters = filterManager?.getGlobalFilters();
+  return useMemo(() => {
+    if (searchData === undefined || (aiopsListState && aiopsListState.searchString !== '')) {
+      if (aiopsListState?.filters && readOnly === false) {
+        const globalFilters = filterManager?.getGlobalFilters();
 
-      if (filterManager) filterManager.setFilters(aiopsListState.filters);
-      if (globalFilters) filterManager?.addFilters(globalFilters);
+        if (filterManager) filterManager.setFilters(aiopsListState.filters);
+        if (globalFilters) filterManager?.addFilters(globalFilters);
+      }
+
+      // In cases where the url state contains only a KQL query and not yet
+      // the transformed ES query we regenerate it. This may happen if we restore
+      // url state on page load coming from another page like ML's Single Metric Viewer.
+      let searchQuery = aiopsListState?.searchQuery;
+      const query = {
+        language: aiopsListState?.searchQueryLanguage,
+        query: aiopsListState?.searchString,
+      };
+      if (
+        (aiopsListState.searchString !== '' ||
+          (Array.isArray(aiopsListState.filters) && aiopsListState.filters.length > 0)) &&
+        (isDefaultSearchQuery(searchQuery) || searchQuery === undefined) &&
+        isQuery(query)
+      ) {
+        searchQuery = createMergedEsQuery(query, aiopsListState.filters, dataView, uiSettings);
+      }
+
+      return {
+        ...(isDefaultSearchQuery(searchQuery) ? {} : { searchQuery }),
+        searchString: aiopsListState?.searchString,
+        searchQueryLanguage: aiopsListState?.searchQueryLanguage,
+      };
+    } else {
+      return {
+        searchQuery: searchData.searchQuery,
+        searchString: searchData.searchString,
+        searchQueryLanguage: searchData.queryLanguage,
+      };
     }
-    return {
-      searchQuery: aiopsListState?.searchQuery,
-      searchString: aiopsListState?.searchString,
-      searchQueryLanguage: aiopsListState?.searchQueryLanguage,
-    };
-  } else {
-    return {
-      searchQuery: searchData.searchQuery,
-      searchString: searchData.searchString,
-      searchQueryLanguage: searchData.queryLanguage,
-    };
-  }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [JSON.stringify([searchData, aiopsListState])]);
 };

commit 025c2d040de18c02e9d7def0d4fbbcd1cd88ba05

[ML] AIOps: Fix rerender issue with relative time ranges like "Last 15 minutes" (#176130)
## Summary

Fixes #176066. Regression introduced in #175289.

Fixes a rerendering issue with relative time ranges like "Last 15
minutes". The memoization didn't work correctly because for the relative
time range we would constantly get updated bounds.

### Checklist

Delete any items that are not applicable to this PR.

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [x] This was checked for breaking API changes and was [labeled
appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

x-pack/plugins/aiops/public/components/log_rate_analysis/log_rate_analysis_content/log_rate_analysis_content.tsx
@@ -15,7 +15,6 @@ import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 import type { DataView } from '@kbn/data-views-plugin/public';
-import type { Dictionary } from '@kbn/ml-url-state';
 import {
   LOG_RATE_ANALYSIS_TYPE,
   type LogRateAnalysisType,
@@ -62,7 +61,6 @@ export function getDocumentCountStatsSplitLabel(
 export interface LogRateAnalysisContentProps {
   /** The data view to analyze. */
   dataView: DataView;
-  setGlobalState?: (params: Dictionary<unknown>) => void;
   /** Timestamp for the start of the range for initial analysis */
   initialAnalysisStart?: number | WindowParameters;
   timeRange?: { min: Moment; max: Moment };
@@ -84,7 +82,6 @@ export interface LogRateAnalysisContentProps {
 
 export const LogRateAnalysisContent: FC<LogRateAnalysisContentProps> = ({
   dataView,
-  setGlobalState,
   initialAnalysisStart: incomingInitialAnalysisStart,
   timeRange,
   esSearchQuery = DEFAULT_SEARCH_QUERY,
@@ -150,7 +147,7 @@ export const LogRateAnalysisContent: FC<LogRateAnalysisContentProps> = ({
     dataView,
     'log_rate_analysis',
     searchQuery,
-    setGlobalState,
+    undefined,
     currentSelectedSignificantItem,
     currentSelectedGroup,
     undefined,

x-pack/plugins/aiops/public/components/log_rate_analysis/log_rate_analysis_content/log_rate_analysis_content_wrapper.tsx
@@ -40,8 +40,6 @@ export interface LogRateAnalysisContentWrapperProps {
   stickyHistogram?: boolean;
   /** App dependencies */
   appDependencies: AiopsAppDependencies;
-  /** On global timefilter update */
-  setGlobalState?: any;
   /** Timestamp for start of initial analysis */
   initialAnalysisStart?: number | WindowParameters;
   /** Optional time range */
@@ -66,7 +64,6 @@ export interface LogRateAnalysisContentWrapperProps {
 export const LogRateAnalysisContentWrapper: FC<LogRateAnalysisContentWrapperProps> = ({
   dataView,
   appDependencies,
-  setGlobalState,
   initialAnalysisStart,
   timeRange,
   esSearchQuery,
@@ -100,7 +97,6 @@ export const LogRateAnalysisContentWrapper: FC<LogRateAnalysisContentWrapperProp
               <DatePickerContextProvider {...datePickerDeps}>
                 <LogRateAnalysisContent
                   dataView={dataView}
-                  setGlobalState={setGlobalState}
                   initialAnalysisStart={initialAnalysisStart}
                   timeRange={timeRange}
                   esSearchQuery={esSearchQuery}

x-pack/plugins/aiops/public/components/log_rate_analysis/log_rate_analysis_page.tsx
@@ -172,7 +172,6 @@ export const LogRateAnalysisPage: FC<Props> = ({ stickyHistogram }) => {
             embeddingOrigin={AIOPS_TELEMETRY_ID.AIOPS_DEFAULT_SOURCE}
             esSearchQuery={searchQuery}
             onWindowParametersChange={onWindowParametersHandler}
-            setGlobalState={setGlobalState}
             stickyHistogram={stickyHistogram}
           />
         </EuiFlexGroup>

x-pack/plugins/aiops/public/hooks/use_data.ts
@@ -57,7 +57,7 @@ export const useData = (
   const timeRangeMemoized = useMemo(
     () => timefilter.getActiveBounds(),
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [JSON.stringify(timefilter.getActiveBounds())]
+    [lastRefresh, JSON.stringify(timefilter.getTime())]
   );
 
   const fieldStatsRequest: DocumentStatsSearchStrategyParams | undefined = useMemo(() => {

x-pack/plugins/aiops/public/hooks/use_document_count_stats.ts
@@ -77,12 +77,12 @@ export function useDocumentCountStats<TParams extends DocumentStatsSearchStrateg
 
   const [documentStatsCache, setDocumentStatsCache] = useState<Record<string, DocumentStats>>({});
 
+  const cacheKey = stringHash(
+    `${JSON.stringify(searchParams)}_${JSON.stringify(searchParamsCompare)}`
+  );
+
   const fetchDocumentCountData = useCallback(async () => {
     if (!searchParams) return;
-
-    const cacheKey = stringHash(
-      `${JSON.stringify(searchParams)}_${JSON.stringify(searchParamsCompare)}`
-    );
 
     if (documentStatsCache[cacheKey]) {
       setDocumentStats(documentStatsCache[cacheKey]);
@@ -172,7 +172,9 @@ export function useDocumentCountStats<TParams extends DocumentStatsSearchStrateg
         displayError(toasts, searchParams!.index, extractErrorProperties(error));
       }
     }
-  }, [data?.search, documentStatsCache, searchParams, searchParamsCompare, toasts]);
+    // custom comparison
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [data?.search, documentStatsCache, cacheKey]);
 
   useEffect(
     function getDocumentCountData() {

x-pack/test/functional/apps/aiops/log_rate_analysis.ts
@@ -274,8 +274,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
     });
   }
 
-  // FLAKY: https://github.com/elastic/kibana/issues/176066
-  describe.skip('log rate analysis', async function () {
+  describe('log rate analysis', async function () {
     for (const testData of logRateAnalysisTestData) {
       describe(`with '${testData.sourceIndexOrSavedSearch}'`, function () {
         before(async () => {