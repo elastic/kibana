c28648b08a2b70c584bb6eba33e05497d07daa91
Jonathan Budzenski <jon@elastic.co>
2024-02-06 14:58:34 -0600

    fix commit check

--- .buildkite/scripts/serverless/create_deploy_tag/create_fix_tag.sh
+++ .buildkite/scripts/serverless/create_deploy_tag/create_fix_tag.sh
@@ -2,6 +2,11 @@
 
 set -euo pipefail
 
-git cat-file -e "$BUILDKITE_COMMIT"
+echo "--- Verify $BUILDKITE_COMMIT exists in $BUILDKITE_BRANCH"
+git branch --contains $BUILDKITE_COMMIT | grep -q $BUILDKITE_BRANCH
+
+echo "--- Create tag $BUILDKITE_BRANCH"
 git tag "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT"
+
+echo "--- Push tag $BUILDKITE_BRANCH"
 git push origin tag "$BUILDKITE_BRANCH"
