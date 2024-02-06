df8f13d21769bfdd2cd4fb3fdff0cef82f5f2952
Navarone Feekery <13634519+navarone-feekery@users.noreply.github.com>
2024-02-06 17:56:18 +0100

    Fix string describing native connector API keys

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/connector/api_key_configuration.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/connector/api_key_configuration.tsx
@@ -98,17 +98,29 @@ export const ApiKeyConfig: React.FC<{
       {isModalVisible && <ConfirmModal onCancel={onCancel} onConfirm={onConfirm} />}
       <EuiFlexItem>
         <EuiText size="s">
-          {i18n.translate(
-            'xpack.enterpriseSearch.content.indices.configurationConnector.apiKey.description',
-            {
-              defaultMessage:
-                'First, generate an Elasticsearch API key. This {apiKeyName} key will enable read and write permissions for the connector to index documents to the created {indexName} index. Save the key in a safe place, as you will need it to configure your connector.',
-              values: {
-                apiKeyName: `${indexName}-connector`,
-                indexName,
-              },
-            }
-          )}
+          {isNative
+            ? i18n.translate(
+                'xpack.enterpriseSearch.content.indices.configurationConnector.apiKey.description',
+                {
+                  defaultMessage:
+                    'First, generate an Elasticsearch API key. This {apiKeyName} key will enable read and write permissions for the connector to index documents to the created {indexName} index. Save the key in a safe place, as you will need it to configure your connector.',
+                  values: {
+                    apiKeyName: `${indexName}-connector`,
+                    indexName,
+                  },
+                }
+              )
+            : i18n.translate(
+                'xpack.enterpriseSearch.content.indices.configurationConnector.nativeConnector.apiKey.description',
+                {
+                  defaultMessage:
+                    'This native connector\'s API key is managed internally by Elasticsearch. There\'s no need to do anything here, but if you need to rollover your API key you can do so by clicking "Generate API key".',
+                  values: {
+                    apiKeyName: `${indexName}-connector`,
+                    indexName,
+                  },
+                }
+              )}
         </EuiText>
       </EuiFlexItem>
       <EuiFlexItem>
