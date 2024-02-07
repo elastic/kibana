6ce0d7e9252f4d194f85c61bc011e5292306c898
Sebastián Zaffarano <sebastian.zaffarano@elastic.co>
2024-02-07 11:41:37 +0100

    Implement feature flag configuration

--- x-pack/plugins/security_solution/server/integration_tests/telemetry.test.ts
+++ x-pack/plugins/security_solution/server/integration_tests/telemetry.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 import Path from 'path';
-import axios from 'axios';
+import axios, { type AxiosRequestConfig } from 'axios';
 
 import type {
   ExceptionListItemSchema,
@@ -111,24 +111,7 @@ describe('telemetry tasks', () => {
 
   describe('detection-rules', () => {
     it('shuld execute when scheduled', async () => {
-      const task = getTelemetryTask(tasks, 'security:telemetry-detection-rules');
-
-      // create some data
-      await createMockedAlert(
-        kibanaServer.coreStart.elasticsearch.client.asInternalUser,
-        kibanaServer.coreStart.savedObjects
-      );
-      const { exceptionList, exceptionListItem } = await createMockedExceptionList(
-        kibanaServer.coreStart.savedObjects
-      );
-
-      exceptionsList.push(exceptionList);
-      exceptionsListItem.push(exceptionListItem);
-
-      // schedule task to run ASAP
-      await eventually(async () => {
-        await taskManagerPlugin.runSoon(task.getTaskId());
-      });
+      await mockAndScheduleDetectionRulesTask();
 
       // wait until the events are sent to the telemetry server
       const body = await eventually(async () => {
@@ -146,57 +129,35 @@ describe('telemetry tasks', () => {
     });
 
     it('should send task metrics', async () => {
-      const task = getTelemetryTask(tasks, 'security:telemetry-detection-rules');
+      const task = await mockAndScheduleDetectionRulesTask();
 
-      // create some data
-      await createMockedAlert(
-        kibanaServer.coreStart.elasticsearch.client.asInternalUser,
-        kibanaServer.coreStart.savedObjects
-      );
-      const { exceptionList, exceptionListItem } = await createMockedExceptionList(
-        kibanaServer.coreStart.savedObjects
-      );
+      const requests = await getTaskMetricsRequests(task);
 
-      exceptionsList.push(exceptionList);
-      exceptionsListItem.push(exceptionListItem);
-
-      // schedule task to run ASAP
-      await eventually(async () => {
-        await taskManagerPlugin.runSoon(task.getTaskId());
+      expect(requests.length).toBeGreaterThan(0);
+      requests.forEach(({ body }) => {
+        const asJson = JSON.parse(body);
+        expect(asJson).not.toBeFalsy();
+        expect(asJson.passed).toEqual(true);
       });
-
-      // wait until the events are sent to the telemetry server
-      const body = await eventually(
-        async () => {
-          const calls = mockedAxiosPost.mock.calls.flatMap(([url, data]) => {
-            return (data as string).split('\n').map((b) => {
-              return { url, body: b };
-            });
-          });
-
-          const found = calls.find(({ url, body: b }) => {
-            return (
-              b.indexOf(getTelemetryTaskTitle(task)) !== -1 &&
-              url.startsWith(ENDPOINT_STAGING) &&
-              url.endsWith('task-metrics')
-            );
-          });
-          expect(found).not.toBeFalsy();
-
-          return found !== undefined ? found.body : '{}';
-        },
-        60 * 1_000,
-        1_000
-      );
-
-      const asJson = JSON.parse(body);
-      expect(asJson).not.toBeFalsy();
-      expect(asJson.passed).toEqual(true);
     });
   });
 
   describe('configuration', () => {
-    it('should send task metrics', async () => {
+    it('should use legacy sender by default', async () => {
+      // launch a random task and verify it uses the new configuration
+      const task = await mockAndScheduleDetectionRulesTask();
+
+      const requests = await getTaskMetricsRequests(task);
+      expect(requests.length).toBeGreaterThan(0);
+      requests.forEach(({ config }) => {
+        expect(config).not.toBeFalsy();
+        if (config && config.headers) {
+          expect(config.headers['X-Telemetry-Sender']).not.toEqual('async');
+        }
+      });
+    });
+
+    it('should use new sender when configured', async () => {
       const configTaskType = 'security:telemetry-configuration';
       const configTask = getTelemetryTask(tasks, configTaskType);
 
@@ -214,9 +175,41 @@ describe('telemetry tasks', () => {
         expect(found).toBeFalsy();
       });
 
-      // launch a random task and verify it uses the new configuration
+      const task = await mockAndScheduleDetectionRulesTask();
+
+      const requests = await getTaskMetricsRequests(task);
+      expect(requests.length).toBeGreaterThan(0);
+      requests.forEach(({ config }) => {
+        expect(config).not.toBeFalsy();
+        if (config && config.headers) {
+          expect(config.headers['X-Telemetry-Sender']).toEqual('async');
+        }
+      });
     });
   });
+
+  async function mockAndScheduleDetectionRulesTask(): Promise<SecurityTelemetryTask> {
+    const task = getTelemetryTask(tasks, 'security:telemetry-detection-rules');
+
+    // create some data
+    await createMockedAlert(
+      kibanaServer.coreStart.elasticsearch.client.asInternalUser,
+      kibanaServer.coreStart.savedObjects
+    );
+    const { exceptionList, exceptionListItem } = await createMockedExceptionList(
+      kibanaServer.coreStart.savedObjects
+    );
+
+    exceptionsList.push(exceptionList);
+    exceptionsListItem.push(exceptionListItem);
+
+    // schedule task to run ASAP
+    await eventually(async () => {
+      await taskManagerPlugin.runSoon(task.getTaskId());
+    });
+
+    return task;
+  }
 
   function mockAxiosGet(bufferConfig: unknown = fakeBufferAndSizesConfigAsyncDisabled) {
     mockedAxiosGet.mockImplementation(async (url: string) => {
@@ -236,6 +229,36 @@ describe('telemetry tasks', () => {
       return { status: 404 };
     });
   }
+
+  async function getTaskMetricsRequests(task: SecurityTelemetryTask): Promise<
+    Array<{
+      url: string;
+      body: string;
+      config: AxiosRequestConfig<unknown> | undefined;
+    }>
+  > {
+    return eventually(
+      async () => {
+        const calls = mockedAxiosPost.mock.calls.flatMap(([url, data, config]) => {
+          return (data as string).split('\n').map((body) => {
+            return { url, body, config };
+          });
+        });
+
+        const requests = calls.filter(({ url, body: b }) => {
+          return (
+            b.indexOf(getTelemetryTaskTitle(task)) !== -1 &&
+            url.startsWith(ENDPOINT_STAGING) &&
+            url.endsWith('task-metrics')
+          );
+        });
+        expect(requests.length).toBeGreaterThan(0);
+        return requests;
+      },
+      60 * 1_000,
+      1_000
+    );
+  }
 });
 
 const fakeBufferAndSizesConfigAsyncDisabled = {
@@ -247,10 +270,6 @@ const fakeBufferAndSizesConfigAsyncDisabled = {
 };
 
 const fakeBufferAndSizesConfigAsyncEnabled = {
-  telemetry_max_buffer_size: 100,
-  max_security_list_telemetry_batch: 100,
-  max_endpoint_telemetry_batch: 300,
-  max_detection_rule_telemetry_batch: 1000,
-  max_detection_alerts_batch: 50,
+  ...fakeBufferAndSizesConfigAsyncDisabled,
   use_async_sender: true,
 };
