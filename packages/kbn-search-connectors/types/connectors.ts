f8db95dc98761050a956afd58364838947263e06
Chenhui Wang <chenhui.wang@elastic.co>
2024-02-07 15:10:56 +0800

    Create collectConnectorStats function

--- packages/kbn-search-connectors/types/connectors.ts
+++ packages/kbn-search-connectors/types/connectors.ts
@@ -230,7 +230,9 @@ export interface Connector {
   last_access_control_sync_error: string | null;
   last_access_control_sync_scheduled_at: string | null;
   last_access_control_sync_status: SyncStatus | null;
+  last_deleted_document_count: number | null;
   last_incremental_sync_scheduled_at: string | null;
+  last_indexed_document_count: number | null;
   last_seen: string | null;
   last_sync_error: string | null;
   last_sync_scheduled_at: string | null;
@@ -285,3 +287,118 @@ export interface NativeConnector {
 }
 
 export type ConnectorViewIndex = ConnectorIndex & ElasticsearchViewIndexExtension;
+
+export interface ConnectorStats {
+  id: string;
+  serviceType?: string | null;
+  isNative?: boolean;
+  isDeleted: boolean;
+  status?: ConnectorStatus;
+  indexName?: string | null;
+  dlsEnabled?: boolean;
+  sslEnabled?: boolean;
+  fetchSelectively?: boolean;
+  textExtractionServiceEnabled?: boolean;
+  documents?: DocumentsStats;
+  dataSourceSpecific?: DataSourceSpecificStats;
+  scheduling?: {
+    accessControl: Scheduling;
+    full: Scheduling;
+    incremental: Scheduling;
+  };
+  syncRules?: {
+    active: {
+      withBasicRules: boolean;
+      withAdvancedRules: boolean;
+    };
+    draft: {
+      withBasicRules: boolean;
+      withAdvancedRules: boolean;
+    };
+  };
+  ingestPipeline?: {
+    name: string;
+    extractBinaryContent: boolean;
+    reduceWhitespace: boolean;
+    runMLInference: boolean;
+  };
+  syncJobs?: SyncJobStats;
+}
+
+export interface DataSourceSpecificStats {
+  confluence?: {
+    dataSourceType: string;
+  };
+  github?: {
+    isCloud: boolean;
+  };
+  jira?: {
+    dataSourceType: string;
+  };
+  mongodb?: {
+    directConnect: boolean;
+  };
+  mssql?: {
+    validateHost: boolean;
+    tables: number;
+  };
+  mysql?: {
+    tables: number;
+  };
+  oracle?: {
+    tables: number;
+  };
+  postgresql?: {
+    tables: number;
+  };
+  slack?: {
+    autoJoinChannelsEnabled: boolean;
+    syncUsersEnabled: boolean;
+    fetchLastNDays: number;
+  };
+  zoom?: {
+    recordingAge: number;
+  };
+}
+
+export interface DocumentsStats {
+  total: number;
+  volume: number;
+  inLastSync: number;
+}
+
+interface Scheduling {
+  enabled: boolean;
+  interval: string;
+}
+
+export interface SyncJobStats {
+  overall: SyncJobStatsDetails;
+  withTextExtractionServiceEnabled?: SyncJobStatsDetails;
+}
+
+export interface SyncJobStatsDetails {
+  total: number;
+  last30Days?: SyncJobStatsByType;
+  last7Days?: SyncJobStatsByType;
+}
+
+export interface SyncJobStatsByType {
+  overall: SyncJobStatsByState;
+  accessControl?: SyncJobStatsByState;
+  full?: SyncJobStatsByState;
+  incremental?: SyncJobStatsByState;
+}
+
+export interface SyncJobStatsByState {
+  total: number;
+  manual: number;
+  scheduled: number;
+  completed: number;
+  errored: number;
+  canceled: number;
+  suspended: number;
+  idle: number;
+  running: number;
+  totalDurationSeconds: number;
+}
