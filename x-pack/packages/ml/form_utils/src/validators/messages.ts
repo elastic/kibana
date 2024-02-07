f206e06c46626180e64df51674738b90d476c8d7
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-02-07 16:40:45 +0100

    fix i18n

--- x-pack/packages/ml/form_utils/src/validators/messages.ts
+++ x-pack/packages/ml/form_utils/src/validators/messages.ts
@@ -7,15 +7,12 @@
 
 import { i18n } from '@kbn/i18n';
 
-export const requiredErrorMessage = i18n.translate(
-  'xpack.transform.transformList.editFlyoutFormRequiredErrorMessage',
-  {
-    defaultMessage: 'Required field.',
-  }
-);
+export const requiredErrorMessage = i18n.translate('xpack.ml.formUtils.requiredErrorMessage', {
+  defaultMessage: 'Required field.',
+});
 
 export const stringNotValidErrorMessage = i18n.translate(
-  'xpack.transform.transformList.editFlyoutFormStringNotValidErrorMessage',
+  'xpack.ml.formUtils.stringNotValidErrorMessage',
   {
     defaultMessage: 'Value needs to be of type string.',
   }
