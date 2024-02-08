8deccfa8a2727580aa05960e05acfc00f909c2fd
Omolola Akinleye <omolola.akinleye@elastic.co>
2024-02-08 10:16:38 -0500

    add expect

--- x-pack/test/cloud_security_posture_functional/pages/vulnerabilities_grouping.ts
+++ x-pack/test/cloud_security_posture_functional/pages/vulnerabilities_grouping.ts
@@ -8,6 +8,7 @@
 import { asyncForEach } from '@kbn/std';
 import type { FtrProviderContext } from '../ftr_provider_context';
 import { vulnerabilitiesLatestMock } from '../mocks/vulnerabilities_latest_mock';
+import expect from '@kbn/expect';
 
 // eslint-disable-next-line import/no-default-export
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
