234de9f30e160fe1246d43ee6d56fa2c2ed9f8f9
Julia Bardi <julia.bardi@elastic.co>
2024-02-08 13:11:37 +0100

    more detailed error message on top-level manifest archive error

--- x-pack/plugins/fleet/server/services/epm/archive/parse.ts
+++ x-pack/plugins/fleet/server/services/epm/archive/parse.ts
@@ -220,7 +220,11 @@ export function parseAndVerifyArchive(
   logger.debug(`Verifying archive - checking manifest file and manifest buffer`);
   if (!paths.includes(manifestFile) || !manifestBuffer) {
     throw new PackageInvalidArchiveError(
-      `Package at top-level directory ${toplevelDir} must contain a top-level ${MANIFEST_NAME} file.`
+      !paths.includes(manifestFile)
+        ? `Manifest file ${manifestFile} not found in paths ${paths}`
+        : `manifestBuffer is not found in assetsMap ${Object.keys(
+            assetsMap
+          )} for manifestFile ${manifestFile}`
     );
   }
 
