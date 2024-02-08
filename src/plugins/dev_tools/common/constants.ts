05555d0bce33c146130a92b7c81f412558d9d72d
Rodney Norris <rodney.norris@elastic.co>
2024-02-08 14:34:13 -0600

    [Dev Tools] Introduce UI setting for Docked Console (#176414)
    ## Summary
    
    Introduce a new UI setting to allow users to disable the "Docked
    Console" aka the EmbeddableConsole from the console plugin. This console
    is currently used on Search pages and Index Management pages for 8.13,
    and will be added to more pages in future releases.
    
    ### Screenshots
    
    ![image](https://github.com/elastic/kibana/assets/1972968/c8b7ed8a-8695-486d-aef5-d403ea4d4547)
    
    ### Checklist
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/dev_tools/common/constants.ts
+++ src/plugins/dev_tools/common/constants.ts
@@ -6,10 +6,11 @@
  * Side Public License, v 1.
  */
 
-import { COURIER_IGNORE_FILTER_IF_FIELD_NOT_IN_INDEX_ID } from '@kbn/management-settings-ids';
-import { ENABLE_DOCKED_CONSOLE_UI_SETTING_ID } from '@kbn/dev-tools-plugin/common';
-
-export const SEARCH_PROJECT_SETTINGS = [
-  COURIER_IGNORE_FILTER_IF_FIELD_NOT_IN_INDEX_ID,
-  ENABLE_DOCKED_CONSOLE_UI_SETTING_ID,
-];
+/**
+ * The UI Setting prefix and category for dev tools UI Settings
+ */
+export const DEV_TOOLS_FEATURE_ID = 'devTools';
+/**
+ * UI Setting ID for enabling / disabling the docked console in Kibana
+ */
+export const ENABLE_DOCKED_CONSOLE_UI_SETTING_ID = 'devTools:enableDockedConsole';
