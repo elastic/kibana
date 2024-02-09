c1851d0417a7acc77609a261e96256da513dfc8d
neptunian <sandra.gonzales@elastic.co>
2024-02-09 15:03:50 -0500

    use our FtrProviderContext

--- x-pack/test/functional/apps/infra/index.ts
+++ x-pack/test/functional/apps/infra/index.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { FtrProviderContext } from '../../ftr_provider_context';
+import { FtrProviderContext } from './types';
 
 export default ({ loadTestFile }: FtrProviderContext) => {
   describe('InfraOps App', function () {
