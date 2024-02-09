823d92d20ecef1cde55d6146755a6da49ff4e842
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2024-02-09 12:32:12 +0100

    add technical preview badge

--- x-pack/plugins/security_solution/public/flyout/document_details/isolate_host/header.tsx
+++ x-pack/plugins/security_solution/public/flyout/document_details/isolate_host/header.tsx
@@ -5,32 +5,48 @@
  * 2.0.
  */
 
-import { EuiTitle } from '@elastic/eui';
 import type { FC } from 'react';
 import React from 'react';
+import { EuiBetaBadge, EuiFlexGroup, EuiFlexItem, EuiTitle } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
+import {
+  TECHNICAL_PREVIEW,
+  TECHNICAL_PREVIEW_DESCRIPTION,
+} from '../../../timelines/components/side_panel/event_details/translations';
 import { useIsolateHostPanelContext } from './context';
 import { FLYOUT_HEADER_TITLE_TEST_ID } from './test_ids';
 import { FlyoutHeader } from '../../shared/components/flyout_header';
+import { isAlertFromSentinelOneEvent } from '../../../common/utils/sentinelone_alert_check';
 
 /**
  * Document details expandable right section header for the isolate host panel
  */
 export const PanelHeader: FC = () => {
-  const { isolateAction } = useIsolateHostPanelContext();
+  const { isolateAction, dataFormattedForFieldBrowser: data } = useIsolateHostPanelContext();
+  const isSentinelOneAlert = isAlertFromSentinelOneEvent({ data });
 
-  const title =
-    isolateAction === 'isolateHost' ? (
-      <FormattedMessage
-        id="xpack.securitySolution.flyout.isolateHost.isolateTitle"
-        defaultMessage="Isolate host"
-      />
-    ) : (
-      <FormattedMessage
-        id="xpack.securitySolution.flyout.isolateHost.releaseTitle"
-        defaultMessage="Release host"
-      />
-    );
+  const title = (
+    <EuiFlexGroup responsive gutterSize="s">
+      <EuiFlexItem grow={false}>
+        {isolateAction === 'isolateHost' ? (
+          <FormattedMessage
+            id="xpack.securitySolution.flyout.isolateHost.isolateTitle"
+            defaultMessage="Isolate host"
+          />
+        ) : (
+          <FormattedMessage
+            id="xpack.securitySolution.flyout.isolateHost.releaseTitle"
+            defaultMessage="Release host"
+          />
+        )}
+      </EuiFlexItem>
+      {isSentinelOneAlert && (
+        <EuiFlexItem grow={false}>
+          <EuiBetaBadge label={TECHNICAL_PREVIEW} tooltipContent={TECHNICAL_PREVIEW_DESCRIPTION} />
+        </EuiFlexItem>
+      )}
+    </EuiFlexGroup>
+  );
 
   return (
     <FlyoutHeader>
