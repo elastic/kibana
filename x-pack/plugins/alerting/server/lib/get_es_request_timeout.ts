0bbbd0030b839705e3b8bd0e0e839b037b24fe09
Alexandra Doak <alexandra.doak@elastic.co>
2024-02-09 10:58:21 -0800

    Using default rule task timeout

--- x-pack/plugins/alerting/server/lib/get_es_request_timeout.ts
+++ x-pack/plugins/alerting/server/lib/get_es_request_timeout.ts
@@ -7,15 +7,17 @@
 
 import { Logger } from '@kbn/core/server';
 import { parseDuration } from '../../common';
+import { DEFAULT_EXECUTION_TIMEOUT } from './get_rule_task_timeout';
 
 export function getEsRequestTimeout(logger: Logger, timeout?: string): number | undefined {
   if (!timeout) {
     return undefined;
   }
-  const maxRequestTimeout = 5 * 60 * 1000;
+
   try {
+    const maxRequestTimeout = parseDuration(DEFAULT_EXECUTION_TIMEOUT);
     const requestTimeout = parseDuration(timeout);
-    // return the ES request timeout in ms that is capped at 5 min.
+    // return the ES request timeout in ms that is capped at the default execution timeout (5 min).
     return requestTimeout > maxRequestTimeout ? maxRequestTimeout : requestTimeout;
   } catch (error) {
     logger.debug(`Invalid format for the rule ES requestTimeout duration: "${timeout}"`);
