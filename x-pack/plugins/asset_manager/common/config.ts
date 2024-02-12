c2f678756cd695e78a3e7ca3ac048b47504b164b
klacabane <kevin.lacabane@elastic.co>
2024-02-12 23:36:32 +0100

    fix plugin config schema

--- x-pack/plugins/asset_manager/common/config.ts
+++ x-pack/plugins/asset_manager/common/config.ts
@@ -8,7 +8,13 @@
 import { schema, TypeOf } from '@kbn/config-schema';
 
 export const INDEX_DEFAULTS = {
-  logs: 'filebeat-*,logs-*',
+  logs: 'filebeat-*,logs-*,remote_cluster:filebeat-*,remote_cluster:logs-*',
+  metrics: 'metrics-*,metricbeat-*,remote_cluster:metrics-*,remote_cluster:metricbeat-*',
+  apm: {
+    transaction: 'apm-*,traces-apm*,remote_cluster:apm-*,remote_cluster:traces-apm*',
+    error: 'apm-*,logs-apm*,remote_cluster:apm-*,remote_cluster:logs-apm*',
+    metric: 'apm-*,metrics-apm*,remote_cluster:apm-*,remote_cluster:metrics-apm*',
+  },
 };
 
 export const configSchema = schema.object({
@@ -20,8 +26,37 @@ export const configSchema = schema.object({
   sourceIndices: schema.object(
     {
       logs: schema.string({ defaultValue: INDEX_DEFAULTS.logs }),
+      metrics: schema.string({ defaultValue: INDEX_DEFAULTS.metrics }),
+      apm: schema.object(
+        {
+          transaction: schema.string({ defaultValue: INDEX_DEFAULTS.apm.transaction }),
+          error: schema.string({ defaultValue: INDEX_DEFAULTS.apm.error }),
+          metric: schema.string({ defaultValue: INDEX_DEFAULTS.apm.metric }),
+        },
+        { defaultValue: INDEX_DEFAULTS.apm }
+      ),
     },
     { defaultValue: INDEX_DEFAULTS }
+  ),
+  implicitCollection: schema.maybe(
+    schema.object({
+      enabled: schema.boolean({ defaultValue: true }),
+      interval: schema.duration({ defaultValue: '5m' }),
+      input: schema.maybe(
+        schema.object({
+          hosts: schema.string(),
+          username: schema.string(),
+          password: schema.string(),
+        })
+      ),
+      output: schema.maybe(
+        schema.object({
+          hosts: schema.string(),
+          username: schema.string(),
+          password: schema.string(),
+        })
+      ),
+    })
   ),
 });
 
