ce71b82ab926f4166ca11c46b5dec22eb54b83b3
neptunian <sandra.gonzales@elastic.co>
2024-02-12 14:46:33 -0500

    remove skipping ff test tag

--- x-pack/test/functional/apps/infra/index.ts
+++ x-pack/test/functional/apps/infra/index.ts
@@ -14,7 +14,6 @@ export default ({ loadTestFile }: FtrProviderContext) => {
     loadTestFile(require.resolve('./tour'));
 
     describe('Metrics UI', function () {
-      this.tags('skipFirefox');
       loadTestFile(require.resolve('./home_page'));
       loadTestFile(require.resolve('./metrics_source_configuration'));
       loadTestFile(require.resolve('./metrics_anomalies'));
