dee1da07b9108d323c3221ed519eb5628701d13d
Christos Nasikas <christos.nasikas@elastic.co>
2024-02-13 15:43:41 +0000

    Remove for-loop

--- x-pack/plugins/cases/public/components/user_actions/user_actions_list.test.tsx
+++ x-pack/plugins/cases/public/components/user_actions/user_actions_list.test.tsx
@@ -41,94 +41,92 @@ describe(`UserActionsList`, () => {
     appMockRender = createAppMockRenderer();
   });
 
-  for (let index = 0; index < 200; index++) {
-    it('renders list correctly with isExpandable option', async () => {
-      appMockRender.render(<UserActionsList {...defaultProps} isExpandable />);
+  it('renders list correctly with isExpandable option', async () => {
+    appMockRender.render(<UserActionsList {...defaultProps} isExpandable />);
 
-      expect(await screen.findByTestId('user-actions-list')).toBeInTheDocument();
-    });
+    expect(await screen.findByTestId('user-actions-list')).toBeInTheDocument();
+  });
 
-    it('renders list correctly with isExpandable=false option', async () => {
-      appMockRender.render(<UserActionsList {...defaultProps} />);
+  it('renders list correctly with isExpandable=false option', async () => {
+    appMockRender.render(<UserActionsList {...defaultProps} />);
 
-      expect(await screen.findByTestId('user-actions-list')).toBeInTheDocument();
-    });
+    expect(await screen.findByTestId('user-actions-list')).toBeInTheDocument();
+  });
 
-    it('renders user actions correctly', async () => {
-      appMockRender.render(<UserActionsList {...defaultProps} />);
+  it('renders user actions correctly', async () => {
+    appMockRender.render(<UserActionsList {...defaultProps} />);
 
-      expect(await screen.findByTestId(`description-create-action-${caseUserActions[0].id}`));
-      expect(await screen.findByTestId(`comment-create-action-${caseUserActions[1].commentId}`));
-      expect(await screen.findByTestId(`description-update-action-${caseUserActions[2].id}`));
-    });
+    expect(await screen.findByTestId(`description-create-action-${caseUserActions[0].id}`));
+    expect(await screen.findByTestId(`comment-create-action-${caseUserActions[1].commentId}`));
+    expect(await screen.findByTestId(`description-update-action-${caseUserActions[2].id}`));
+  });
 
-    it('renders bottom actions correctly', async () => {
-      const userName = 'Username';
-      const sample = 'This is an add comment bottom actions';
+  it('renders bottom actions correctly', async () => {
+    const userName = 'Username';
+    const sample = 'This is an add comment bottom actions';
 
-      const bottomActions = [
-        {
-          username: <div>{userName}</div>,
-          'data-test-subj': 'add-comment',
-          timelineAvatar: null,
-          className: 'isEdit',
-          children: <span>{sample}</span>,
-        },
-      ];
+    const bottomActions = [
+      {
+        username: <div>{userName}</div>,
+        'data-test-subj': 'add-comment',
+        timelineAvatar: null,
+        className: 'isEdit',
+        children: <span>{sample}</span>,
+      },
+    ];
 
-      appMockRender.render(<UserActionsList {...defaultProps} bottomActions={bottomActions} />);
+    appMockRender.render(<UserActionsList {...defaultProps} bottomActions={bottomActions} />);
 
-      expect(await screen.findByTestId('user-actions-list')).toBeInTheDocument();
-      expect(await screen.findByTestId('add-comment')).toBeInTheDocument();
-    });
+    expect(await screen.findByTestId('user-actions-list')).toBeInTheDocument();
+    expect(await screen.findByTestId('add-comment')).toBeInTheDocument();
+  });
 
-    it('Outlines comment when url param is provided', async () => {
-      const commentId = 'basic-comment-id';
-      jest.spyOn(routeData, 'useParams').mockReturnValue({ commentId });
+  it('Outlines comment when url param is provided', async () => {
+    const commentId = 'basic-comment-id';
+    jest.spyOn(routeData, 'useParams').mockReturnValue({ commentId });
 
-      const ourActions = [getUserAction('comment', UserActionActions.create)];
+    const ourActions = [getUserAction('comment', UserActionActions.create)];
 
-      const props = {
-        ...defaultProps,
-        caseUserActions: ourActions,
-      };
+    const props = {
+      ...defaultProps,
+      caseUserActions: ourActions,
+    };
 
-      appMockRender.render(<UserActionsList {...props} />);
+    appMockRender.render(<UserActionsList {...props} />);
 
-      expect(
-        await screen.findAllByTestId(`comment-create-action-${commentId}`)
-      )[0]?.classList.contains('outlined');
-    });
+    expect(
+      await screen.findAllByTestId(`comment-create-action-${commentId}`)
+    )[0]?.classList.contains('outlined');
+  });
 
-    it('Outlines comment when update move to link is clicked', async () => {
-      const ourActions = [
-        getUserAction('comment', UserActionActions.create),
-        getUserAction('comment', UserActionActions.update),
-      ];
+  it('Outlines comment when update move to link is clicked', async () => {
+    const ourActions = [
+      getUserAction('comment', UserActionActions.create),
+      getUserAction('comment', UserActionActions.update),
+    ];
 
-      const props = {
-        ...defaultProps,
-        caseUserActions: ourActions,
-      };
+    const props = {
+      ...defaultProps,
+      caseUserActions: ourActions,
+    };
 
-      appMockRender.render(<UserActionsList {...props} />);
-      expect(
-        (
-          await screen.findAllByTestId(`comment-create-action-${props.data.comments[0].id}`)
-        )[0]?.classList.contains('outlined')
-      ).toBe(false);
-
-      expect(
-        (
-          await screen.findAllByTestId(`comment-create-action-${props.data.comments[0].id}`)
-        )[0]?.classList.contains('outlined')
-      ).toBe(false);
-
-      userEvent.click(await screen.findByTestId(`comment-update-action-${ourActions[1].id}`));
-
-      expect(
+    appMockRender.render(<UserActionsList {...props} />);
+    expect(
+      (
         await screen.findAllByTestId(`comment-create-action-${props.data.comments[0].id}`)
-      )[0]?.classList.contains('outlined');
-    });
-  }
+      )[0]?.classList.contains('outlined')
+    ).toBe(false);
+
+    expect(
+      (
+        await screen.findAllByTestId(`comment-create-action-${props.data.comments[0].id}`)
+      )[0]?.classList.contains('outlined')
+    ).toBe(false);
+
+    userEvent.click(await screen.findByTestId(`comment-update-action-${ourActions[1].id}`));
+
+    expect(
+      await screen.findAllByTestId(`comment-create-action-${props.data.comments[0].id}`)
+    )[0]?.classList.contains('outlined');
+  });
 });
