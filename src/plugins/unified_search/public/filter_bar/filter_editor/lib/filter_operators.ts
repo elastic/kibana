62e250f34c242ae65793957f577e0b0ae8b13e30
Marta Bondyra <marta.bondyra@elastic.co>
2024-02-13 14:50:22 +0100

    [Lens] persist value when operator change is compatible

--- src/plugins/unified_search/public/filter_bar/filter_editor/lib/filter_operators.ts
+++ src/plugins/unified_search/public/filter_bar/filter_editor/lib/filter_operators.ts
@@ -7,9 +7,11 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import { FILTERS } from '@kbn/es-query';
+import { FILTERS, Filter } from '@kbn/es-query';
 import { ES_FIELD_TYPES } from '@kbn/field-types';
 import { DataViewField } from '@kbn/data-views-plugin/common';
+import { FilterMetaParams } from '@kbn/es-query/src/filters/build_filters';
+import { isRangeParams } from '../range_value_input';
 
 export const strings = {
   getIsOperatorOptionLabel: () =>
@@ -83,36 +85,72 @@ export interface Operator {
    * takes precedence over {@link fieldTypes}
    */
   field?: (field: DataViewField) => boolean;
+  /**
+   * List of operators that value preserves when switching to this operator
+   */
+  getParamsFromPrevOperator?: (
+    prevOperator: Operator | undefined,
+    params: FilterMetaParams
+  ) => FilterMetaParams | undefined;
 }
 
-export const isOperator = {
+export const isOperator: Operator = {
   message: strings.getIsOperatorOptionLabel(),
   type: FILTERS.PHRASE,
   negate: false,
   id: OPERATORS.IS,
+  getParamsFromPrevOperator: (prevOperator, params) => {
+    if (!prevOperator) return;
+    if ([OPERATORS.IS, OPERATORS.IS_NOT].includes(prevOperator.id)) return params;
+    if ([OPERATORS.IS_ONE_OF, OPERATORS.IS_NOT_ONE_OF].includes(prevOperator.id)) {
+      if (Array.isArray(params) && params.length > 0) return params[0];
+    }
+  },
 };
 
-export const isNotOperator = {
+export const isNotOperator: Operator = {
   message: strings.getIsNotOperatorOptionLabel(),
   type: FILTERS.PHRASE,
   negate: true,
   id: OPERATORS.IS_NOT,
+  getParamsFromPrevOperator: (prevOperator, params) => {
+    if (!prevOperator) return;
+    if ([OPERATORS.IS, OPERATORS.IS_NOT].includes(prevOperator.id)) return params;
+    if ([OPERATORS.IS_ONE_OF, OPERATORS.IS_NOT_ONE_OF].includes(prevOperator.id)) {
+      if (Array.isArray(params) && params.length > 0) return params[0];
+    }
+  },
 };
 
-export const isOneOfOperator = {
+export const isOneOfOperator: Operator = {
   message: strings.getIsOneOfOperatorOptionLabel(),
   type: FILTERS.PHRASES,
   negate: false,
   fieldTypes: ['string', 'number', 'date', 'ip', 'geo_point', 'geo_shape'],
   id: OPERATORS.IS_ONE_OF,
+  // todo: add support for array of values
+  getParamsFromPrevOperator: (prevOperator, params) => {
+    if (!prevOperator) return;
+    if ([OPERATORS.IS_ONE_OF, OPERATORS.IS_NOT_ONE_OF].includes(prevOperator.id)) return params;
+    if ([OPERATORS.IS, OPERATORS.IS_NOT].includes(prevOperator.id) && typeof params === 'string') {
+      if (!Array.isArray(params)) return [params];
+    }
+  },
 };
 
-export const isNotOneOfOperator = {
+export const isNotOneOfOperator: Operator = {
   message: strings.getIsNotOneOfOperatorOptionLabel(),
   type: FILTERS.PHRASES,
   negate: true,
   fieldTypes: ['string', 'number', 'date', 'ip', 'geo_point', 'geo_shape'],
   id: OPERATORS.IS_NOT_ONE_OF,
+  getParamsFromPrevOperator: (prevOperator, params) => {
+    if (!prevOperator) return;
+    if ([OPERATORS.IS_ONE_OF, OPERATORS.IS_NOT_ONE_OF].includes(prevOperator.id)) return params;
+    if ([OPERATORS.IS, OPERATORS.IS_NOT].includes(prevOperator.id) && typeof params === 'string') {
+      if (!Array.isArray(params)) return [params];
+    }
+  },
 };
 
 const rangeOperatorsSharedProps = {
@@ -127,42 +165,82 @@ const rangeOperatorsSharedProps = {
   },
 };
 
-export const isBetweenOperator = {
+export const isBetweenOperator: Operator = {
   ...rangeOperatorsSharedProps,
   message: strings.getIsBetweenOperatorOptionLabel(),
   id: OPERATORS.BETWEEN,
   negate: false,
+  getParamsFromPrevOperator: (prevOperator, params) => {
+    if (!prevOperator) return;
+    if (
+      [
+        OPERATORS.LESS,
+        OPERATORS.GREATER_OR_EQUAL,
+        OPERATORS.BETWEEN,
+        OPERATORS.NOT_BETWEEN,
+      ].includes(prevOperator.id)
+    )
+      return params;
+  },
 };
 
-export const isLessThanOperator = {
-  ...rangeOperatorsSharedProps,
-  message: strings.getLessThanOperatorOptionLabel(),
-  id: OPERATORS.LESS,
-  negate: false,
-};
-
-export const isGreaterOrEqualOperator = {
-  ...rangeOperatorsSharedProps,
-  message: strings.getIsGreaterOrEqualOperatorOptionLabel(),
-  id: OPERATORS.GREATER_OR_EQUAL,
-  negate: false,
-};
-
-export const isNotBetweenOperator = {
+export const isNotBetweenOperator: Operator = {
   ...rangeOperatorsSharedProps,
   message: strings.getIsNotBetweenOperatorOptionLabel(),
   negate: true,
   id: OPERATORS.NOT_BETWEEN,
+  getParamsFromPrevOperator: (prevOperator, params) => {
+    if (!prevOperator) return;
+    if (
+      [
+        OPERATORS.LESS,
+        OPERATORS.GREATER_OR_EQUAL,
+        OPERATORS.BETWEEN,
+        OPERATORS.NOT_BETWEEN,
+      ].includes(prevOperator.id)
+    )
+      return params;
+  },
 };
 
-export const existsOperator = {
+export const isLessThanOperator: Operator = {
+  ...rangeOperatorsSharedProps,
+  message: strings.getLessThanOperatorOptionLabel(),
+  id: OPERATORS.LESS,
+  negate: false,
+  getParamsFromPrevOperator: (prevOperator, params) => {
+    if (!prevOperator) return;
+    if (
+      [OPERATORS.BETWEEN, OPERATORS.NOT_BETWEEN].includes(prevOperator.id) &&
+      isRangeParams(params)
+    )
+      return { from: undefined, to: params?.to };
+  },
+};
+
+export const isGreaterOrEqualOperator: Operator = {
+  ...rangeOperatorsSharedProps,
+  message: strings.getIsGreaterOrEqualOperatorOptionLabel(),
+  id: OPERATORS.GREATER_OR_EQUAL,
+  negate: false,
+  getParamsFromPrevOperator: (prevOperator, params) => {
+    if (!prevOperator) return;
+    if (
+      [OPERATORS.BETWEEN, OPERATORS.NOT_BETWEEN].includes(prevOperator.id) &&
+      isRangeParams(params)
+    )
+      return { from: params?.from, to: undefined };
+  },
+};
+
+export const existsOperator: Operator = {
   message: strings.getExistsOperatorOptionLabel(),
   type: FILTERS.EXISTS,
   negate: false,
   id: OPERATORS.EXISTS,
 };
 
-export const doesNotExistOperator = {
+export const doesNotExistOperator: Operator = {
   message: strings.getDoesNotExistOperatorOptionLabel(),
   type: FILTERS.EXISTS,
   negate: true,
