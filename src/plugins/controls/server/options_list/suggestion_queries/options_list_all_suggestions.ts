20ccf4b55e7cb8f96dcf82101b2297f924aed836
Hannah Mudge <hannah.wright@elastic.co>
2024-02-13 10:56:47 -0700

    Add support for `key_as_string`

--- src/plugins/controls/server/options_list/suggestion_queries/options_list_all_suggestions.ts
+++ src/plugins/controls/server/options_list/suggestion_queries/options_list_all_suggestions.ts
@@ -72,7 +72,10 @@ const allSuggestionsAggregationBuilder: OptionsListSuggestionAggregationBuilder 
       rawEsResult,
       `aggregations.${subTypeNested ? 'nestedSuggestions.suggestions' : 'suggestions'}.buckets`
     )?.reduce((acc: OptionsListSuggestions, suggestion: EsBucket) => {
-      acc.push({ value: suggestion.key, docCount: suggestion.doc_count });
+      acc.push({
+        value: suggestion.key_as_string ? suggestion.key_as_string : suggestion.key,
+        docCount: suggestion.doc_count,
+      });
       return acc;
     }, []);
     return {
