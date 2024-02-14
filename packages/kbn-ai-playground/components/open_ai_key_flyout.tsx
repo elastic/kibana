ccb01dd3590de96f86b01ce82977c02b52667c3e
Yan Savitski <yan.savitski@elastic.co>
2024-02-14 06:49:02 +0100

    Prettier openAI update

--- packages/kbn-ai-playground/components/open_ai_key_flyout.tsx
+++ packages/kbn-ai-playground/components/open_ai_key_flyout.tsx
@@ -5,9 +5,24 @@
  * 2.0.
  */
 
-import { EuiButton, EuiButtonEmpty, EuiFieldText, EuiFlexGroup, EuiFlexItem, EuiFlyout, EuiFlyoutBody, EuiFlyoutFooter, EuiFlyoutHeader, EuiFormRow, EuiLink, EuiSpacer, EuiText, EuiTitle } from "@elastic/eui";
-import { i18n } from "@kbn/i18n";
-import React, { useState } from "react";
+import {
+  EuiButton,
+  EuiButtonEmpty,
+  EuiFieldText,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiFlyout,
+  EuiFlyoutBody,
+  EuiFlyoutFooter,
+  EuiFlyoutHeader,
+  EuiFormRow,
+  EuiLink,
+  EuiSpacer,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import React, { useState } from 'react';
 
 export interface OpenAIKeyFlyOutProps {
   openAPIKey: string;
@@ -15,25 +30,26 @@ export interface OpenAIKeyFlyOutProps {
   onSave: (key: string) => void;
 }
 
-export const OpenAIKeyFlyOut: React.FC<OpenAIKeyFlyOutProps> = ({ openAPIKey, onClose, onSave }) => {
+export const OpenAIKeyFlyOut: React.FC<OpenAIKeyFlyOutProps> = ({
+  openAPIKey,
+  onClose,
+  onSave,
+}) => {
   const [apiKey, setApiKey] = useState<string>(openAPIKey);
 
   const handleSave = () => {
     onSave(apiKey);
     onClose();
-  }
+  };
 
   return (
     <EuiFlyout onClose={onClose} size="m">
-      <EuiFlyoutHeader>
+      <EuiFlyoutHeader hasBorder>
         <EuiTitle size="m">
           <h3>
-            {i18n.translate(
-              'aiPlayground.sidebar.openAIFlyOut.headerTitle',
-              {
-                defaultMessage: 'OpenAI API Key',
-              }
-            )}
+            {i18n.translate('aiPlayground.sidebar.openAIFlyOut.headerTitle', {
+              defaultMessage: 'OpenAI API Key',
+            })}
           </h3>
         </EuiTitle>
         <EuiSpacer size="s" />
@@ -42,27 +58,28 @@ export const OpenAIKeyFlyOut: React.FC<OpenAIKeyFlyOutProps> = ({ openAPIKey, on
         <EuiFlexGroup direction="column" gutterSize="l">
           <EuiFormRow
             fullWidth
-            label={i18n.translate(
-              'aiPlayground.sidebar.openAIFlyOut.labelTitle',
-              { defaultMessage: 'OpenAI API Key' }
-            )}
+            label={i18n.translate('aiPlayground.sidebar.openAIFlyOut.labelTitle', {
+              defaultMessage: 'OpenAI API Key',
+            })}
             labelAppend={
               <EuiText size="xs">
-                <EuiLink target="_blank" href="https://platform.openai.com/api-keys">OpenAI API Keys</EuiLink>
+                <EuiLink target="_blank" href="https://platform.openai.com/api-keys">
+                  OpenAI API Keys
+                </EuiLink>
               </EuiText>
-            }>
+            }
+          >
             <EuiFlexItem grow>
               <EuiFieldText
                 fullWidth
-                placeholder={i18n.translate(
-                  'aiPlayground.sidebar.openAIFlyOut.placeholder',
-                  { defaultMessage: 'Enter API Key here' }
-                )}
+                placeholder={i18n.translate('aiPlayground.sidebar.openAIFlyOut.placeholder', {
+                  defaultMessage: 'Enter API Key here',
+                })}
                 value={apiKey}
                 onChange={(e) => setApiKey(e.target.value)}
               />
             </EuiFlexItem>
-          </EuiFormRow>  
+          </EuiFormRow>
         </EuiFlexGroup>
       </EuiFlyoutBody>
       <EuiFlyoutFooter>
@@ -72,11 +89,9 @@ export const OpenAIKeyFlyOut: React.FC<OpenAIKeyFlyOutProps> = ({ openAPIKey, on
               data-telemetry-id="entSearchAIPlayground-addingOpenAIKey-cancel"
               onClick={onClose}
             >
-              {i18n.translate('aiPlayground.sidebar.openAIFlyOut.cancelButtonLabel', 
-                {
-                  defaultMessage: 'Cancel',
-                })
-              }
+              {i18n.translate('aiPlayground.sidebar.openAIFlyOut.cancelButtonLabel', {
+                defaultMessage: 'Cancel',
+              })}
             </EuiButtonEmpty>
           </EuiFlexItem>
           <EuiFlexItem />
@@ -87,12 +102,9 @@ export const OpenAIKeyFlyOut: React.FC<OpenAIKeyFlyOutProps> = ({ openAPIKey, on
               fill
               onClick={handleSave}
             >
-              {i18n.translate(
-                'aiPlayground.sidebar.openAIFlyOut.saveButtonLabel',
-                {
-                  defaultMessage: 'Save',
-                }
-              )}
+              {i18n.translate('aiPlayground.sidebar.openAIFlyOut.saveButtonLabel', {
+                defaultMessage: 'Save',
+              })}
             </EuiButton>
           </EuiFlexItem>
         </EuiFlexGroup>
