3f93ddc169885164504f37b2b5645cb8a52bf7e0
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-02-14 21:34:20 +0100

    [Discover] Update test

--- test/functional/apps/discover/group3/_time_field_column.ts
+++ test/functional/apps/discover/group3/_time_field_column.ts
@@ -409,7 +409,12 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
               `${SEARCH_WITH_SELECTED_COLUMNS_AND_TIMESTAMP}${savedSearchSuffix}`
             );
             await PageObjects.discover.waitUntilSearchingHasFinished();
-            expect(await docTable.getHeaderFields()).to.eql(['bytes', 'extension', '@timestamp']);
+            expect(await docTable.getHeaderFields()).to.eql([
+              '@timestamp',
+              'bytes',
+              'extension',
+              '@timestamp',
+            ]);
 
             await PageObjects.discover.loadSavedSearch(
               `${SEARCH_WITH_SELECTED_COLUMNS_AND_TIMESTAMP}${savedSearchSuffix}-`
@@ -421,7 +426,12 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
               `${SEARCH_WITH_SELECTED_COLUMNS_AND_TIMESTAMP}${savedSearchSuffix}ESQL`
             );
             await PageObjects.discover.waitUntilSearchingHasFinished();
-            expect(await docTable.getHeaderFields()).to.eql(['bytes', 'extension', '@timestamp']);
+            expect(await docTable.getHeaderFields()).to.eql([
+              '@timestamp',
+              'bytes',
+              'extension',
+              '@timestamp',
+            ]);
           });
         });
       });
