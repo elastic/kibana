2cb4d2db34c79618519df50fbb33b0fffba8e041
Janki Salvi <117571355+js-jankisalvi@users.noreply.github.com>
2024-02-14 13:27:29 +0100

    add mocks

--- x-pack/plugins/cases/public/components/user_actions/property_actions/registered_attachments_property_actions.test.tsx
+++ x-pack/plugins/cases/public/components/user_actions/property_actions/registered_attachments_property_actions.test.tsx
@@ -17,119 +17,135 @@ import {
 } from '../../../common/mock';
 import { RegisteredAttachmentsPropertyActions } from './registered_attachments_property_actions';
 import { AttachmentActionType } from '../../../client/attachment_framework/types';
+import { useDeletePropertyAction } from './use_delete_property_action';
 
-// FLAKY: https://github.com/elastic/kibana/issues/174384
-describe.skip('RegisteredAttachmentsPropertyActions', () => {
-  let appMock: AppMockRenderer;
+jest.mock('./use_delete_property_action');
 
-  const props = {
-    isLoading: false,
-    registeredAttachmentActions: [],
-    onDelete: jest.fn(),
-    hideDefaultActions: false,
-  };
+for (let i = 0; i < 200; i++) {
+  describe('RegisteredAttachmentsPropertyActions', () => {
+    let appMock: AppMockRenderer;
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-    appMock = createAppMockRenderer();
-  });
+    const props = {
+      isLoading: false,
+      registeredAttachmentActions: [],
+      onDelete: jest.fn(),
+      hideDefaultActions: false,
+    };
 
-  it('renders the correct number of actions', async () => {
-    appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
+    const useDeletePropertyActionMock = useDeletePropertyAction as jest.Mock;
+    const onConfirmMock = jest.fn();
 
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+    beforeEach(() => {
+      jest.clearAllMocks();
+      appMock = createAppMockRenderer();
+      useDeletePropertyActionMock.mockReturnValue({
+        showDeletionModal: true,
+        onModalOpen: jest.fn(),
+        onConfirm: onConfirmMock,
+        onCancel: jest.fn(),
+      });
+    });
 
-    userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+    it('renders the correct number of actions', async () => {
+      appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
 
-    expect(await screen.findByTestId('property-actions-user-action-group')).toBeInTheDocument();
-  });
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
 
-  it('renders the modal info correctly', async () => {
-    appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
+      userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+      await waitForEuiPopoverOpen();
 
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+      expect(await screen.findByTestId('property-actions-user-action-group')).toBeInTheDocument();
+    });
 
-    userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
-    await waitForEuiPopoverOpen();
+    it('renders the modal info correctly', async () => {
+      appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
 
-    expect(await screen.findByTestId('property-actions-user-action-trash')).toBeInTheDocument();
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
 
-    userEvent.click(await screen.findByTestId('property-actions-user-action-trash'));
+      userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+      await waitForEuiPopoverOpen();
 
-    expect(await screen.findByTestId('property-actions-confirm-modal')).toBeInTheDocument();
+      expect(await screen.findByTestId('property-actions-user-action-trash')).toBeInTheDocument();
 
-    expect(await screen.findByTestId('confirmModalTitleText')).toHaveTextContent(
-      'Delete attachment'
-    );
+      userEvent.click(await screen.findByTestId('property-actions-user-action-trash'));
 
-    expect(await screen.findByText('Delete')).toBeInTheDocument();
-  });
+      expect(await screen.findByTestId('property-actions-confirm-modal')).toBeInTheDocument();
 
-  it('remove attachments correctly', async () => {
-    appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
+      expect(await screen.findByTestId('confirmModalTitleText')).toHaveTextContent(
+        'Delete attachment'
+      );
 
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+      expect(await screen.findByText('Delete')).toBeInTheDocument();
+    });
 
-    userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
-    await waitForEuiPopoverOpen();
+    it('remove attachments correctly', async () => {
+      appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
 
-    expect(await screen.findByTestId('property-actions-user-action-trash')).toBeInTheDocument();
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
 
-    userEvent.click(await screen.findByTestId('property-actions-user-action-trash'));
+      userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+      await waitForEuiPopoverOpen();
 
-    expect(await screen.findByTestId('property-actions-confirm-modal')).toBeInTheDocument();
+      expect(await screen.findByTestId('property-actions-user-action-trash')).toBeInTheDocument();
 
-    userEvent.click(await screen.findByText('Delete'));
+      userEvent.click(await screen.findByTestId('property-actions-user-action-trash'));
 
-    await waitFor(() => {
-      expect(props.onDelete).toHaveBeenCalled();
+      expect(await screen.findByTestId('property-actions-confirm-modal')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByText('Delete'));
+
+      await waitFor(() => {
+        expect(onConfirmMock).toHaveBeenCalled();
+      });
+    });
+
+    it('does not show the property actions without delete permissions', async () => {
+      appMock = createAppMockRenderer({ permissions: noCasesPermissions() });
+      appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
+
+      expect(screen.queryByTestId('property-actions-user-action')).not.toBeInTheDocument();
+    });
+
+    it('does not show the property actions when hideDefaultActions is enabled', async () => {
+      appMock.render(<RegisteredAttachmentsPropertyActions {...props} hideDefaultActions={true} />);
+
+      expect(screen.queryByTestId('property-actions-user-action')).not.toBeInTheDocument();
+    });
+
+    it('does show the property actions with only delete permissions', async () => {
+      appMock = createAppMockRenderer({ permissions: onlyDeleteCasesPermission() });
+      appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
+
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+    });
+
+    it('renders correctly registered attachments', async () => {
+      const onClick = jest.fn();
+      const action = [
+        {
+          type: AttachmentActionType.BUTTON as const,
+          label: 'My button',
+          iconType: 'download',
+          onClick,
+        },
+      ];
+
+      appMock.render(
+        <RegisteredAttachmentsPropertyActions {...props} registeredAttachmentActions={action} />
+      );
+
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+      await waitForEuiPopoverOpen();
+
+      expect(
+        (await screen.findByTestId('property-actions-user-action-group')).children.length
+      ).toBe(2);
+
+      expect(
+        await screen.findByTestId('property-actions-user-action-download')
+      ).toBeInTheDocument();
     });
   });
-
-  it('does not show the property actions without delete permissions', async () => {
-    appMock = createAppMockRenderer({ permissions: noCasesPermissions() });
-    appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
-
-    expect(screen.queryByTestId('property-actions-user-action')).not.toBeInTheDocument();
-  });
-
-  it('does not show the property actions when hideDefaultActions is enabled', async () => {
-    appMock.render(<RegisteredAttachmentsPropertyActions {...props} hideDefaultActions={true} />);
-
-    expect(screen.queryByTestId('property-actions-user-action')).not.toBeInTheDocument();
-  });
-
-  it('does show the property actions with only delete permissions', async () => {
-    appMock = createAppMockRenderer({ permissions: onlyDeleteCasesPermission() });
-    appMock.render(<RegisteredAttachmentsPropertyActions {...props} />);
-
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
-  });
-
-  it('renders correctly registered attachments', async () => {
-    const onClick = jest.fn();
-    const action = [
-      {
-        type: AttachmentActionType.BUTTON as const,
-        label: 'My button',
-        iconType: 'download',
-        onClick,
-      },
-    ];
-
-    appMock.render(
-      <RegisteredAttachmentsPropertyActions {...props} registeredAttachmentActions={action} />
-    );
-
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
-
-    userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
-    await waitForEuiPopoverOpen();
-
-    expect((await screen.findByTestId('property-actions-user-action-group')).children.length).toBe(
-      2
-    );
-
-    expect(await screen.findByTestId('property-actions-user-action-download')).toBeInTheDocument();
-  });
-});
+}
--- x-pack/plugins/cases/public/components/user_actions/property_actions/user_comment_property_actions.test.tsx
+++ x-pack/plugins/cases/public/components/user_actions/property_actions/user_comment_property_actions.test.tsx
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import { waitFor } from '@testing-library/react';
 import { waitForEuiPopoverOpen, screen } from '@elastic/eui/lib/test/rtl';
 import userEvent from '@testing-library/user-event';
 import type { AppMockRenderer } from '../../../common/mock';
@@ -15,100 +16,119 @@ import {
   createAppMockRenderer,
 } from '../../../common/mock';
 import { UserCommentPropertyActions } from './user_comment_property_actions';
-import { waitFor } from '@testing-library/react';
+import { useLensOpenVisualization } from '../../markdown_editor/plugins/lens/use_lens_open_visualization';
+import { useDeletePropertyAction } from './use_delete_property_action';
 
-describe('UserCommentPropertyActions', () => {
-  let appMock: AppMockRenderer;
+jest.mock('../../markdown_editor/plugins/lens/use_lens_open_visualization');
+jest.mock('./use_delete_property_action');
 
-  const props = {
-    isLoading: false,
-    onEdit: jest.fn(),
-    onQuote: jest.fn(),
-    onDelete: jest.fn(),
-  };
+for (let i = 0; i < 200; i++) {
+  describe('UserCommentPropertyActions', () => {
+    let appMock: AppMockRenderer;
+    const useDeletePropertyActionMock = useDeletePropertyAction as jest.Mock;
+    const useLensOpenVisualizationMock = useLensOpenVisualization as jest.Mock;
+    const onConfirmMock = jest.fn();
 
-  beforeEach(() => {
-    appMock = createAppMockRenderer();
-    jest.clearAllMocks();
-  });
+    const props = {
+      isLoading: false,
+      onEdit: jest.fn(),
+      onQuote: jest.fn(),
+      onDelete: jest.fn(),
+    };
 
-  it('renders the correct number of actions', async () => {
-    appMock.render(<UserCommentPropertyActions {...props} />);
-
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
-
-    userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
-    await waitForEuiPopoverOpen();
-
-    expect((await screen.findByTestId('property-actions-user-action-group')).children.length).toBe(
-      3
-    );
-    expect(screen.queryByTestId('property-actions-user-action-pencil')).toBeInTheDocument();
-    expect(screen.queryByTestId('property-actions-user-action-trash')).toBeInTheDocument();
-    expect(screen.queryByTestId('property-actions-user-action-quote')).toBeInTheDocument();
-  });
-
-  it('edits the comment correctly', async () => {
-    appMock.render(<UserCommentPropertyActions {...props} />);
-
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
-
-    userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
-    await waitForEuiPopoverOpen();
-
-    expect(screen.queryByTestId('property-actions-user-action-pencil')).toBeInTheDocument();
-
-    userEvent.click(await screen.findByTestId('property-actions-user-action-pencil'));
-
-    expect(props.onEdit).toHaveBeenCalled();
-  });
-
-  it('quotes the comment correctly', async () => {
-    appMock.render(<UserCommentPropertyActions {...props} />);
-
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
-
-    userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
-    await waitForEuiPopoverOpen();
-
-    expect(screen.queryByTestId('property-actions-user-action-quote')).toBeInTheDocument();
-
-    userEvent.click(await screen.findByTestId('property-actions-user-action-quote'));
-
-    expect(props.onQuote).toHaveBeenCalled();
-  });
-
-  it('deletes the comment correctly', async () => {
-    appMock.render(<UserCommentPropertyActions {...props} />);
-
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
-
-    userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
-    await waitForEuiPopoverOpen();
-
-    expect(screen.queryByTestId('property-actions-user-action-trash')).toBeInTheDocument();
-
-    userEvent.click(await screen.findByTestId('property-actions-user-action-trash'));
-
-    await waitFor(() => {
-      expect(screen.queryByTestId('property-actions-confirm-modal')).toBeInTheDocument();
+    beforeEach(() => {
+      appMock = createAppMockRenderer();
+      jest.clearAllMocks();
+      useDeletePropertyActionMock.mockReturnValue({
+        showDeletionModal: true,
+        onModalOpen: jest.fn(),
+        onConfirm: onConfirmMock,
+        onCancel: jest.fn(),
+      });
+      useLensOpenVisualizationMock.mockReturnValue({
+        canUseEditor: false,
+        actionConfig: {},
+      });
     });
 
-    userEvent.click(await screen.findByText('Delete'));
-    expect(props.onDelete).toHaveBeenCalled();
+    it('renders the correct number of actions', async () => {
+      appMock.render(<UserCommentPropertyActions {...props} />);
+
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+      await waitForEuiPopoverOpen();
+
+      expect(
+        (await screen.findByTestId('property-actions-user-action-group')).children.length
+      ).toBe(3);
+      expect(screen.queryByTestId('property-actions-user-action-pencil')).toBeInTheDocument();
+      expect(screen.queryByTestId('property-actions-user-action-trash')).toBeInTheDocument();
+      expect(screen.queryByTestId('property-actions-user-action-quote')).toBeInTheDocument();
+    });
+
+    it('edits the comment correctly', async () => {
+      appMock.render(<UserCommentPropertyActions {...props} />);
+
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+      await waitForEuiPopoverOpen();
+
+      expect(screen.queryByTestId('property-actions-user-action-pencil')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByTestId('property-actions-user-action-pencil'));
+
+      expect(props.onEdit).toHaveBeenCalled();
+    });
+
+    it('quotes the comment correctly', async () => {
+      appMock.render(<UserCommentPropertyActions {...props} />);
+
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+      await waitForEuiPopoverOpen();
+
+      expect(screen.queryByTestId('property-actions-user-action-quote')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByTestId('property-actions-user-action-quote'));
+
+      expect(props.onQuote).toHaveBeenCalled();
+    });
+
+    it('deletes the comment correctly', async () => {
+      appMock.render(<UserCommentPropertyActions {...props} />);
+
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByTestId('property-actions-user-action-ellipses'));
+      await waitForEuiPopoverOpen();
+
+      expect(screen.queryByTestId('property-actions-user-action-trash')).toBeInTheDocument();
+
+      userEvent.click(await screen.findByTestId('property-actions-user-action-trash'));
+
+      await waitFor(() => {
+        expect(screen.queryByTestId('property-actions-confirm-modal')).toBeInTheDocument();
+      });
+
+      userEvent.click(await screen.findByText('Delete'));
+      expect(onConfirmMock).toHaveBeenCalled();
+    });
+
+    it('does not show the property actions without delete permissions', async () => {
+      appMock = createAppMockRenderer({ permissions: noCasesPermissions() });
+      appMock.render(<UserCommentPropertyActions {...props} />);
+
+      expect(screen.queryByTestId('property-actions-user-action')).not.toBeInTheDocument();
+    });
+
+    it('does show the property actions with only delete permissions', async () => {
+      appMock = createAppMockRenderer({ permissions: onlyDeleteCasesPermission() });
+      appMock.render(<UserCommentPropertyActions {...props} />);
+
+      expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
+    });
   });
-
-  it('does not show the property actions without delete permissions', async () => {
-    appMock = createAppMockRenderer({ permissions: noCasesPermissions() });
-    appMock.render(<UserCommentPropertyActions {...props} />);
-
-    expect(screen.queryByTestId('property-actions-user-action')).not.toBeInTheDocument();
-  });
-
-  it('does show the property actions with only delete permissions', async () => {
-    appMock = createAppMockRenderer({ permissions: onlyDeleteCasesPermission() });
-    appMock.render(<UserCommentPropertyActions {...props} />);
-
-    expect(await screen.findByTestId('property-actions-user-action')).toBeInTheDocument();
-  });
-});
+}
