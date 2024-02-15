0917a1a5dcc6c4ef2ed896d7571d13b9cf5da93a
Sébastien Loix <sabee77@gmail.com>
2024-02-15 09:49:12 +0000

    Add comment for skipped test

--- x-pack/plugins/observability_onboarding/e2e/cypress/e2e/logs/system_logs.cy.ts
+++ x-pack/plugins/observability_onboarding/e2e/cypress/e2e/logs/system_logs.cy.ts
@@ -682,6 +682,8 @@ describe('[Logs onboarding] System logs', () => {
       });
     });
 
+    // Skpping this test because it's failing in the CI
+    // https://github.com/elastic/kibana/issues/176995
     xdescribe('when integration installation succeed', () => {
       beforeEach(() => {
         cy.deleteIntegration('system');
