3c72ac54c97fae9a73e27a550b7206bb35f4fa68
Alex Szabo <alex.szabo@elastic.co>
2024-02-15 13:46:08 +0100

    Revert "chore: add some debug messaging to the trigger script (#177009)"
    This reverts commit 715165fc54a4492488d8bc2f7120e0db00b3ca18.

--- .buildkite/scripts/steps/artifacts/trigger.sh
+++ .buildkite/scripts/steps/artifacts/trigger.sh
@@ -4,10 +4,6 @@ set -euo pipefail
 
 source .buildkite/scripts/steps/artifacts/env.sh
 
-echo BEATS_MANIFEST_LATEST = $BEATS_MANIFEST_LATEST
-echo KIBANA_MANIFEST_LATEST = $KIBANA_MANIFEST_LATEST
-
-set +x
 BEATS_MANIFEST_LATEST_URL=$(curl  "$BEATS_MANIFEST_LATEST" | jq -r '.manifest_url')
 KIBANA_MANIFEST_URL=$(curl  "$KIBANA_MANIFEST_LATEST" | jq -r '.manifest_url')
 KIBANA_BEATS_MANIFEST_URL=$(curl $KIBANA_MANIFEST_URL | jq -r '.projects.kibana.dependencies[] | select(.prefix == "beats") | .build_uri')
@@ -18,5 +14,3 @@ if [ "$BEATS_MANIFEST_LATEST_URL" = "$KIBANA_BEATS_MANIFEST_URL" ]; then
 else
   ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-artifacts-staging "$BUILDKITE_BRANCH"
 fi
-
-set -x
