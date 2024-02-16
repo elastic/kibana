88edebb9cdf2d12d3af4cf62b5842ea5828a0007
Tiago Costa <tiago.costa@elastic.co>
2024-02-16 22:02:11 +0000

    skip flaky suite (#177125)

--- x-pack/test/apm_api_integration/tests/dependencies/service_dependencies.spec.ts
+++ x-pack/test/apm_api_integration/tests/dependencies/service_dependencies.spec.ts
@@ -85,7 +85,8 @@ export default function ApiTest({ getService }: FtrProviderContext) {
     }
   );
 
-  registry.when('Dependency for services breakdown', { config: 'basic', archives: [] }, () => {
+  // FLAKY: https://github.com/elastic/kibana/issues/177125
+  registry.when.skip('Dependency for services breakdown', { config: 'basic', archives: [] }, () => {
     describe('when data is loaded', () => {
       before(async () => {
         await generateData({ synthtraceEsClient, start, end });
