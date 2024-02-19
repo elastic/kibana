1200cf30d65459da1a86d3f21534d0477a0bde34
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-02-19 15:32:14 -0500

    [8.12] [Fleet] Fix package showing &#x27;Needs authorization&#x27; warning even after transform assets were authorized successfully (#176647) (#177236)
    # Backport
    
    This will backport the following commits from `main` to `8.12`:
    - [[Fleet] Fix package showing &#x27;Needs authorization&#x27; warning
    even after transform assets were authorized successfully
    (#176647)](https://github.com/elastic/kibana/pull/176647)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Quynh Nguyen
    (Quinn)","email":"43350163+qn895@users.noreply.github.com"},"sourceCommit":{"committedDate":"2024-02-19T19:12:17Z","message":"[Fleet]
    Fix package showing 'Needs authorization' warning even after transform
    assets were authorized successfully
    (#176647)","sha":"4e10d1c70b30cf1c6d8eec8a87a9badc6ad422cb","branchLabelMapping":{"^v8.14.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["bug","release_note:fix",":ml","Feature:Transforms","Team:Fleet","v8.13.0","v8.12.2","v8.14.0"],"title":"[Fleet]
    Fix package showing 'Needs authorization' warning even after transform
    assets were authorized
    successfully","number":176647,"url":"https://github.com/elastic/kibana/pull/176647","mergeCommit":{"message":"[Fleet]
    Fix package showing 'Needs authorization' warning even after transform
    assets were authorized successfully
    (#176647)","sha":"4e10d1c70b30cf1c6d8eec8a87a9badc6ad422cb"}},"sourceBranch":"main","suggestedTargetBranches":["8.13","8.12"],"targetPullRequestStates":[{"branch":"8.13","label":"v8.13.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.12","label":"v8.12.2","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.14.0","branchLabelMappingKey":"^v8.14.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/176647","number":176647,"mergeCommit":{"message":"[Fleet]
    Fix package showing 'Needs authorization' warning even after transform
    assets were authorized successfully
    (#176647)","sha":"4e10d1c70b30cf1c6d8eec8a87a9badc6ad422cb"}}]}]
    BACKPORT-->
    
    Co-authored-by: Quynh Nguyen (Quinn) <43350163+qn895@users.noreply.github.com>

--- x-pack/plugins/fleet/server/services/epm/elasticsearch/transform/install.ts
+++ x-pack/plugins/fleet/server/services/epm/elasticsearch/transform/install.ts
@@ -447,7 +447,6 @@ const installTransformsAssets = async (
         })
       : // No need to generate api key/secondary auth if all transforms are run as kibana_system user
         undefined;
-
     // delete all previous transform
     await Promise.all([
       deleteTransforms(
@@ -761,7 +760,9 @@ async function handleTransformInstall({
         throw err;
       }
     }
-  } else {
+  }
+
+  if (startTransform === false || transform?.content?.settings?.unattended === true) {
     // if transform was not set to start automatically in yml config,
     // we need to check using _stats if the transform had insufficient permissions
     try {
@@ -773,7 +774,11 @@ async function handleTransformInstall({
           ),
         { logger, additionalResponseStatuses: [400] }
       );
-      if (Array.isArray(transformStats.transforms) && transformStats.transforms.length === 1) {
+      if (
+        transformStats &&
+        Array.isArray(transformStats.transforms) &&
+        transformStats.transforms.length === 1
+      ) {
         const transformHealth = transformStats.transforms[0].health;
         if (
           transformHealth &&
