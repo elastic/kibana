5d4a4386f387099e4047c912b791be5fa09ffd4c
Maxim Palenov <maxim.palenov@elastic.co>
2024-02-20 18:04:52 +0100

    set socket timeout for possibly long running rule management endpoints

---
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_management/constants.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export const RULE_MANAGEMENT_BULK_ACTION_SOCKET_TIMEOUT_MS = 1800000 as const; // 30 minutes
+export const RULE_MANAGEMENT_IMPORT_EXPORT_SOCKET_TIMEOUT_MS = 1800000 as const; // 30 minutes
