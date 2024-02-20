ddc59aaef24844571d1a73c0f5717f18da220d81
Matt Kime <matt@mattki.me>
2024-02-20 16:49:07 -0600

    failing to set type

--- x-pack/test_serverless/functional/test_suites/common/management/data_views/_runtime_fields.ts
+++ x-pack/test_serverless/functional/test_suites/common/management/data_views/_runtime_fields.ts
@@ -64,10 +64,11 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
           await testSubjects.existOrFail('flyoutTitle');
         });
         await PageObjects.settings.setFieldType('Long');
-        await new Promise((r) => setTimeout(r, 1000 * 10));
+        await new Promise((r) => setTimeout(r, 1000 * 60 * 10 * 10000));
         await PageObjects.settings.setFieldScriptWithoutToggle('emit(6);');
 
         await PageObjects.settings.toggleRow('formatRow');
+        // await new Promise((r) => setTimeout(r, 1000 * 10));
         await PageObjects.settings.setFieldFormat('bytes');
         await testSubjects.find('changeWarning');
         await PageObjects.settings.clickSaveField();
