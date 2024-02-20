23e36d7378458e5235ac2735649b75dce349555d
Tiago Costa <tiago.costa@elastic.co>
2024-02-20 15:27:10 +0000

    skip flaky suite (#177305)

--- x-pack/test/apm_api_integration/tests/environment/get_environment.spec.ts
+++ x-pack/test/apm_api_integration/tests/environment/get_environment.spec.ts
@@ -19,7 +19,8 @@ export default function environmentsAPITests({ getService }: FtrProviderContext)
   const apmApiClient = getService('apmApiClient');
   const synthtraceEsClient = getService('synthtraceEsClient');
 
-  registry.when('environments when data is loaded', { config: 'basic', archives: [] }, async () => {
+  // FLAKY: https://github.com/elastic/kibana/issues/177305
+  registry.when.skip('environments when data is loaded', { config: 'basic', archives: [] }, async () => {
     before(async () => {
       await generateData({
         synthtraceEsClient,
