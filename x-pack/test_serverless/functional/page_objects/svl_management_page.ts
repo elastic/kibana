a47b7b5187a9818d3c95f5debfb6adc72d75c9bb
Kurt <kc13greiner@users.noreply.github.com>
2024-02-20 14:47:59 -0500

    Update x-pack/test_serverless/functional/page_objects/svl_management_page.ts
    Co-authored-by: Aleh Zasypkin <aleh.zasypkin@gmail.com>

--- x-pack/test_serverless/functional/page_objects/svl_management_page.ts
+++ x-pack/test_serverless/functional/page_objects/svl_management_page.ts
@@ -7,7 +7,7 @@
 
 import { FtrProviderContext } from '../ftr_provider_context';
 
-export function SvlManagementPageProvider({ getService, getPageObjects }: FtrProviderContext) {
+export function SvlManagementPageProvider({ getService }: FtrProviderContext) {
   const testSubjects = getService('testSubjects');
 
   return {
