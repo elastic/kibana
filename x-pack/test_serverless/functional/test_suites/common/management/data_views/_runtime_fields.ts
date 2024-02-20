9ec5e68f8efbdbb5df502545874d35e87db87aa7
Matt Kime <matt@mattki.me>
2024-02-20 15:59:14 -0600

    move timeout

--- x-pack/test_serverless/functional/test_suites/common/management/data_views/_runtime_fields.ts
+++ x-pack/test_serverless/functional/test_suites/common/management/data_views/_runtime_fields.ts
@@ -64,8 +64,9 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
           await testSubjects.existOrFail('flyoutTitle');
         });
         await PageObjects.settings.setFieldType('Long');
-        await PageObjects.settings.setFieldScriptWithoutToggle('emit(6);');
         await new Promise((r) => setTimeout(r, 1000 * 10));
+        await PageObjects.settings.setFieldScriptWithoutToggle('emit(6);');
+
         await PageObjects.settings.toggleRow('formatRow');
         await PageObjects.settings.setFieldFormat('bytes');
         await testSubjects.find('changeWarning');
