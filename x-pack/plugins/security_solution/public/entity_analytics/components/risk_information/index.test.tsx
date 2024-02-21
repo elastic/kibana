3364ba04b95c9e1a78b608d667ac08b94ad8946f
Mark Hopkin <mark.hopkin@elastic.co>
2024-02-21 10:34:05 +0000

    remove unused RiskInformationButtonIcon component

--- x-pack/plugins/security_solution/public/entity_analytics/components/risk_information/index.test.tsx
+++ x-pack/plugins/security_solution/public/entity_analytics/components/risk_information/index.test.tsx
@@ -7,59 +7,51 @@
 
 import { render, fireEvent, within } from '@testing-library/react';
 import React from 'react';
-import { RiskInformationButtonEmpty, RiskInformationButtonIcon } from '.';
+import { RiskInformationButtonEmpty } from '.';
 import { TestProviders } from '../../../common/mock';
 import { RiskScoreEntity } from '../../../../common/search_strategy';
 
 describe.each([RiskScoreEntity.host, RiskScoreEntity.user])(
   'Risk Information entityType: %s',
   (riskEntity) => {
-    describe.each(['RiskInformationButtonIcon', 'RiskInformationButtonEmpty'])(
-      `%s component`,
-      (componentType) => {
-        const Component =
-          componentType === 'RiskInformationButtonIcon'
-            ? RiskInformationButtonIcon
-            : RiskInformationButtonEmpty;
+    describe('RiskInformationButtonEmpty', () => {
+      it('renders', () => {
+        const { queryByTestId } = render(<RiskInformationButtonEmpty riskEntity={riskEntity} />);
 
-        it('renders', () => {
-          const { queryByTestId } = render(<Component riskEntity={riskEntity} />);
+        expect(queryByTestId('open-risk-information-flyout-trigger')).toBeInTheDocument();
+      });
 
-          expect(queryByTestId('open-risk-information-flyout-trigger')).toBeInTheDocument();
-        });
+      it('opens and displays a risk levels table with 5 rows', () => {
+        const NUMBER_OF_ROWS = 1 + 5; // 1 header row + 5 severity rows
+        const { getByTestId, queryByTestId } = render(
+          <TestProviders>
+            <RiskInformationButtonEmpty riskEntity={riskEntity} />
+          </TestProviders>
+        );
 
-        it('opens and displays a risk levels table with 5 rows', () => {
-          const NUMBER_OF_ROWS = 1 + 5; // 1 header row + 5 severity rows
-          const { getByTestId, queryByTestId } = render(
-            <TestProviders>
-              <Component riskEntity={riskEntity} />
-            </TestProviders>
-          );
+        fireEvent.click(getByTestId('open-risk-information-flyout-trigger'));
 
-          fireEvent.click(getByTestId('open-risk-information-flyout-trigger'));
+        const riskLevelsTable = queryByTestId('risk-level-information-table');
 
-          const riskLevelsTable = queryByTestId('risk-level-information-table');
+        expect(riskLevelsTable).toBeInTheDocument();
+        expect(within(riskLevelsTable!).queryAllByRole('row')).toHaveLength(NUMBER_OF_ROWS);
+      });
 
-          expect(riskLevelsTable).toBeInTheDocument();
-          expect(within(riskLevelsTable!).queryAllByRole('row')).toHaveLength(NUMBER_OF_ROWS);
-        });
+      it('opens and displays an asset criticality tiers table with four (4) rows', () => {
+        const NUMBER_OF_ROWS = 1 + 4; // 1 header row + 4 tier rows
+        const { getByTestId, queryByTestId } = render(
+          <TestProviders>
+            <RiskInformationButtonEmpty riskEntity={riskEntity} />
+          </TestProviders>
+        );
 
-        it('opens and displays an asset criticality tiers table with four (4) rows', () => {
-          const NUMBER_OF_ROWS = 1 + 4; // 1 header row + 4 tier rows
-          const { getByTestId, queryByTestId } = render(
-            <TestProviders>
-              <Component riskEntity={riskEntity} />
-            </TestProviders>
-          );
+        fireEvent.click(getByTestId('open-risk-information-flyout-trigger'));
 
-          fireEvent.click(getByTestId('open-risk-information-flyout-trigger'));
+        const criticalityTiersTable = queryByTestId('criticality-level-information-table');
 
-          const criticalityTiersTable = queryByTestId('criticality-level-information-table');
-
-          expect(criticalityTiersTable).toBeInTheDocument();
-          expect(within(criticalityTiersTable!).queryAllByRole('row')).toHaveLength(NUMBER_OF_ROWS);
-        });
-      }
-    );
+        expect(criticalityTiersTable).toBeInTheDocument();
+        expect(within(criticalityTiersTable!).queryAllByRole('row')).toHaveLength(NUMBER_OF_ROWS);
+      });
+    });
   }
 );
