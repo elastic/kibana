d9dfdab9ddc48a865cca0f8936b0770ea407b452
Drew Tate <andrew.tate@elastic.co>
2024-02-21 15:02:34 -0700

    fix another test

--- x-pack/plugins/global_search_bar/public/lib/result_to_option.test.ts
+++ x-pack/plugins/global_search_bar/public/lib/result_to_option.test.ts
@@ -133,6 +133,7 @@ describe('resultToOption', () => {
               "color": "#000000",
               "description": "Known",
               "id": "known",
+              "managed": false,
               "name": "Known",
             },
           ]
