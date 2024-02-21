ff2549af1ecc7a561fc72ccdd7c330e71abb41ea
Ryan Keairns <contactryank@gmail.com>
2024-02-21 10:34:34 -0800

    Switcher design and IA changes (#25)

--- packages/core/chrome/core-chrome-browser-internal/src/ui/workspace_switcher_breadcrumb.tsx
+++ packages/core/chrome/core-chrome-browser-internal/src/ui/workspace_switcher_breadcrumb.tsx
@@ -5,7 +5,7 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-import { EuiContextMenuItem, EuiContextMenuPanel } from '@elastic/eui';
+import { EuiListGroup, EuiListGroupItem, EuiTitle, EuiSpacer, EuiLink } from '@elastic/eui';
 import React from 'react';
 import { i18n } from '@kbn/i18n';
 import type { ChromeProjectBreadcrumb, Workflows } from '@kbn/core-chrome-browser';
@@ -19,24 +19,30 @@ export const getWorkspaceSwitcherBreadCrumb = ({
 }): ChromeProjectBreadcrumb => {
   return {
     text: i18n.translate('core.ui.primaryNav.cloud.workflowLabel', {
-      defaultMessage: 'Workflow',
+      defaultMessage: 'My Kibana deployment',
     }),
     popoverContent: (
-      <EuiContextMenuPanel
-        size="s"
-        items={Object.values(workflows).map(({ id, title, icon = 'gear' }) => (
-          <EuiContextMenuItem
-            key={id}
-            icon={icon as string}
-            onClick={() => {
-              onWorkflowChange(id);
-            }}
-          >
-            {title}
-          </EuiContextMenuItem>
-        ))}
-      />
+      <>
+        <EuiTitle size="xxs">
+          <h3>Solution view</h3>
+        </EuiTitle>
+        <EuiSpacer size="s" />
+        <EuiListGroup bordered size="s">
+          {Object.values(workflows).map(({ id, title, icon = 'gear' }) => [
+            <EuiListGroupItem
+              key={id}
+              label={title}
+              iconType={icon as string}
+              onClick={() => {
+                onWorkflowChange(id);
+              }}
+            />,
+          ])}
+        </EuiListGroup>
+        <EuiSpacer size="m" />
+        <EuiLink href="">View all deployments</EuiLink>
+      </>
     ),
-    popoverProps: { panelPaddingSize: 'none', zIndex: 6000 },
+    popoverProps: { panelPaddingSize: 'm', zIndex: 6000 },
   };
 };
