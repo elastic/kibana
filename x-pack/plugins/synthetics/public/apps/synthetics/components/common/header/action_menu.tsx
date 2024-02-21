3fa3fcb30873e7f7d8b1262577bc787ec22a8b2e
shahzad31 <shahzad31comp@gmail.com>
2024-02-21 11:17:20 +0100

    condition

--- x-pack/plugins/synthetics/public/apps/synthetics/components/common/header/action_menu.tsx
+++ x-pack/plugins/synthetics/public/apps/synthetics/components/common/header/action_menu.tsx
@@ -27,9 +27,11 @@ export const ActionMenu = ({ appMountParameters }: { appMountParameters: AppMoun
         <EuiFlexItem>
           <ActionMenuContent />
         </EuiFlexItem>
-        <EuiFlexItem>
-          <ObservabilityAIAssistantActionMenuItem />
-        </EuiFlexItem>
+        {ObservabilityAIAssistantActionMenuItem && (
+          <EuiFlexItem>
+            <ObservabilityAIAssistantActionMenuItem />
+          </EuiFlexItem>
+        )}
       </EuiFlexGroup>
     </HeaderMenuPortal>
   );
