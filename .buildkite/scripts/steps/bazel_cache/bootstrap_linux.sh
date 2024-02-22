2d893ad32ba5e63d272b891e5499c27677a7ec9f
Jonathan Budzenski <jon@elastic.co>
2024-02-22 16:17:52 -0600

    [bazel_cache] Fix puppeteer skip download environment variable

--- .buildkite/scripts/steps/bazel_cache/bootstrap_linux.sh
+++ .buildkite/scripts/steps/bazel_cache/bootstrap_linux.sh
@@ -6,7 +6,7 @@ source .buildkite/scripts/common/util.sh
 
 export BAZEL_CACHE_MODE=populate-local-gcs
 export DISABLE_BOOTSTRAP_VALIDATION=true
-export PUPPETEER_SKIP_CHROME_DOWNLOAD=true
+export PUPPETEER_SKIP_DOWNLOAD=true
 
 # Clear out bazel cache between runs to make sure that any artifacts that don't exist in the cache are uploaded
 rm -rf ~/.bazel-cache
