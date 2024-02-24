1597daf2d0fb374a97431acc30933d2ce24128af
Tiago Costa <tiago.costa@elastic.co>
2024-02-24 00:01:00 +0000

    skip flaky suite (#168768)

--- x-pack/test/security_solution_cypress/cypress/e2e/investigations/dasbhoards/detection_response.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/investigations/dasbhoards/detection_response.cy.ts
@@ -44,6 +44,7 @@ const TEST_USER_NAME = 'test';
 const SIEM_KIBANA_HOST_NAME = 'siem-kibana';
 
 // Failing: See https://github.com/elastic/kibana/issues/177761
+// FLAKY: https://github.com/elastic/kibana/issues/168768
 describe.skip('Detection response view', { tags: ['@ess', '@serverless'] }, () => {
   before(() => {
     deleteAlertsAndRules();
