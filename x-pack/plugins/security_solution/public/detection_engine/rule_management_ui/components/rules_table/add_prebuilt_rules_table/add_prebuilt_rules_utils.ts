1be0e83cc26e5f5edc142e49a22403580d718941
jpdjere <jpdjeredjian@gmail.com>
2024-03-05 13:43:30 +0100

    [Security Solution] Fix infinite loading state on Add Rules page for users with Security: Read

---
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management_ui/components/rules_table/add_prebuilt_rules_table/add_prebuilt_rules_utils.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { PrebuiltRulesStatusStats } from '../../../../../../common/api/detection_engine';
+
+interface UpgradeReviewEnabledProps {
+  canUserCRUD: boolean | null;
+  isUpgradingSecurityPackages: boolean;
+  prebuiltRulesStatus?: PrebuiltRulesStatusStats;
+}
+
+export const isUpgradeReviewRequestEnabled = ({
+  canUserCRUD,
+  isUpgradingSecurityPackages,
+  prebuiltRulesStatus,
+}: UpgradeReviewEnabledProps) => {
+  // Wait until security package is updated
+  if (isUpgradingSecurityPackages) {
+    return false;
+  }
+
+  // If user is read-only, allow request to proceed even
+  // though package might not be installed.
+  if (canUserCRUD === false) {
+    return true;
+  }
+
+  return prebuiltRulesStatus && prebuiltRulesStatus.num_prebuilt_rules_total_in_package > 0;
+};
