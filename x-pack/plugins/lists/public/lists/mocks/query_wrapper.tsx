be843ea0557e39be392a58c223546ba404a54186
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2024-03-05 09:39:03 +0000

    Update x-pack/plugins/lists/public/lists/mocks/query_wrapper.tsx
    Co-authored-by: Ryland Herrick <ryalnd@gmail.com>

--- x-pack/plugins/lists/public/lists/mocks/query_wrapper.tsx
+++ x-pack/plugins/lists/public/lists/mocks/query_wrapper.tsx
@@ -8,7 +8,7 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export const createQueryWrapper = (): {
+export const createQueryWrapperMock = (): {
   queryClient: QueryClient;
   wrapper: React.FC;
 } => {
