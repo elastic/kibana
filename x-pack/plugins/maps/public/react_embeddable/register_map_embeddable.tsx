34f563f9b5637be8524a7c068ef23fd9e055b861
Nathan Reese <reese.nathan@gmail.com>
2024-03-05 11:27:47 -0700

    register map embeddable

---
+++ x-pack/plugins/maps/public/react_embeddable/register_map_embeddable.tsx
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import {
+  DefaultEmbeddableApi,
+  ReactEmbeddableFactory,
+  RegisterReactEmbeddable,
+  registerReactEmbeddableFactory,
+} from '@kbn/embeddable-plugin/public';
+import React from 'react';
+import { MAP_SAVED_OBJECT_TYPE } from '../../common/constants';
+
+export const registerMapEmbeddable = () => {
+  const factory: ReactEmbeddableFactory<
+    unknown,
+    DefaultEmbeddableApi
+  > = {
+    deserializeState: (state) => {
+      console.log('state', state);
+      return undefined;
+    },
+    getComponent: async (state, maybeId) => {
+      return RegisterReactEmbeddable((apiRef) => {
+        return <div>hello world</div>;
+      });
+    },
+  };
+
+  registerReactEmbeddableFactory(MAP_SAVED_OBJECT_TYPE, factory);
+};
