1745ae6de20d906e3bcf34d2d124aec12b72fd10
Nathan Reese <reese.nathan@gmail.com>
2024-03-05 13:13:56 -0700

    remove unused

--- x-pack/plugins/maps/public/react_embeddable/types.ts
+++ x-pack/plugins/maps/public/react_embeddable/types.ts
@@ -7,30 +7,6 @@
 
 import {
   DefaultEmbeddableApi,
-  SerializedReactEmbeddableTitles,
 } from '@kbn/embeddable-plugin/public';
-import type { Filter, Query, TimeRange } from '@kbn/es-query';
-import type { MapCenterAndZoom, MapExtent, MapSettings } from '../../common/descriptor_types';
-import type { MapAttributes } from '../../common/content_management';
-
-export type MapEmbeddableSerializedState = SerializedReactEmbeddableTitles &  {
-  // by-value embeddable stores map state inline
-  attributes?: MapAttributes;
-  // by-reference embeddable loads attributes from saved object
-  savedObjectId?: string;
-  isLayerTOCOpen?: boolean;
-  openTOCDetails?: string[];
-  mapCenter?: MapCenterAndZoom;
-  mapBuffer?: MapExtent;
-  mapSettings?: Partial<MapSettings>;
-  hiddenLayers?: string[];
-  hideFilterActions?: boolean;
-  filters?: Filter[];
-  query?: Query;
-  timeRange?: TimeRange;
-  timeslice?: [number, number];
-  filterByMapExtent?: boolean;
-  isMovementSynchronized?: boolean;
-}
 
 export type MapApi = DefaultEmbeddableApi;
