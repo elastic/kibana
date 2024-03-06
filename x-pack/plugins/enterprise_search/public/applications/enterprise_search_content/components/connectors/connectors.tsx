9ba1ae22d30a09ede6c76dd867b909348cc32f01
Tim Grein <tim.grein@elastic.co>
2024-03-06 10:35:59 +0100

    Show correct heading when deleting a crawler

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/connectors/connectors.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/connectors/connectors.tsx
@@ -72,7 +72,7 @@ export const Connectors: React.FC<ConnectorsProps> = ({ isCrawler }) => {
     <SelectConnector />
   ) : (
     <>
-      <DeleteConnectorModal />
+      <DeleteConnectorModal isCrawler={isCrawler} />
       <EnterpriseSearchContentPageTemplate
         pageChrome={baseBreadcrumbs}
         pageViewTelemetry={!isCrawler ? 'Connectors' : 'Web Crawlers'}
