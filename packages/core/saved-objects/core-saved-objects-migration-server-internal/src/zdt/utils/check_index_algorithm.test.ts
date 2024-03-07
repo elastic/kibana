136bae54783bfe79af284e59c6d5d814f7575c91
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-03-07 11:27:45 +0100

    Consider FIPS-compliant v2 state as `v2-partially-migrated`

--- packages/core/saved-objects/core-saved-objects-migration-server-internal/src/zdt/utils/check_index_algorithm.test.ts
+++ packages/core/saved-objects/core-saved-objects-migration-server-internal/src/zdt/utils/check_index_algorithm.test.ts
@@ -28,22 +28,6 @@ describe('checkIndexCurrentAlgorithm', () => {
     expect(checkIndexCurrentAlgorithm(mapping)).toEqual('unknown');
   });
 
-  it('returns `unknown` if both v2 and zdt metas are present', () => {
-    const mapping: IndexMapping = {
-      properties: {},
-      _meta: {
-        migrationMappingPropertyHashes: {
-          foo: 'someHash',
-        },
-        mappingVersions: {
-          foo: '8.8.0',
-        },
-      },
-    };
-
-    expect(checkIndexCurrentAlgorithm(mapping)).toEqual('unknown');
-  });
-
   it('returns `zdt` if all zdt metas are present', () => {
     const mapping: IndexMapping = {
       properties: {},
@@ -72,6 +56,23 @@ describe('checkIndexCurrentAlgorithm', () => {
 
     expect(checkIndexCurrentAlgorithm(mapping)).toEqual('v2-partially-migrated');
   });
+
+  it('returns `unknown` if if mappingVersions and v2 hashes are present', () => {
+    const mapping: IndexMapping = {
+      properties: {},
+      _meta: {
+        migrationMappingPropertyHashes: {
+          foo: 'someHash',
+        },
+        mappingVersions: {
+          foo: '8.8.0',
+        },
+      },
+    };
+
+    expect(checkIndexCurrentAlgorithm(mapping)).toEqual('v2-partially-migrated');
+  });
+
 
   it('returns `v2-incompatible` if v2 hashes are present but not indexTypesMap', () => {
     const mapping: IndexMapping = {
