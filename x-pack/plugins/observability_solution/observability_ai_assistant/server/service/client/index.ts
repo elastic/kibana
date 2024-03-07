2e1b6c01c39f1691d213fac3952ee471d7d83788
Milton Hultgren <milton.hultgren@elastic.co>
2024-03-07 12:50:42 +0100

    Revert unrelated prompt change

--- x-pack/plugins/observability_solution/observability_ai_assistant/server/service/client/index.ts
+++ x-pack/plugins/observability_solution/observability_ai_assistant/server/service/client/index.ts
@@ -631,7 +631,7 @@ export class ObservabilityAIAssistantClient {
           '@timestamp': new Date().toString(),
           message: {
             role: MessageRole.System,
-            content: `You are a helpful assistant for Elastic Observability. Assume the following messages are a conversation between you and a user; give this conversation a title based on the content below. DO NOT UNDER ANY CIRCUMSTANCES wrap this title in single or double quotes. This title is shown in a list of conversations to the user, so title it for the user, not for you. Please create the title in ${responseLanguage}.`,
+            content: `You are a helpful assistant for Elastic Observability. Assume the following message is the start of a conversation between you and a user; give this conversation a title based on the content below. DO NOT UNDER ANY CIRCUMSTANCES wrap this title in single or double quotes. This title is shown in a list of conversations to the user, so title it for the user, not for you. Please create the title in ${responseLanguage}.`,
           },
         },
         {
