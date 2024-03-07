f9a1e3175e973c42dd02cbe1c4c8133cab4cf3fc
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-03-07 15:27:44 +0100

    improve comments

--- x-pack/packages/ml/query_utils/src/bool_filter_based_simple_query.ts
+++ x-pack/packages/ml/query_utils/src/bool_filter_based_simple_query.ts
@@ -10,6 +10,20 @@ import { isPopulatedObject } from '@kbn/ml-is-populated-object';
 import { isSimpleQuery } from './simple_query';
 import type { BoolFilterBasedSimpleQuery } from './types';
 
+/**
+ * Type guard to check if the provided argument is a boolean filter-based simple query.
+ *
+ * A valid `BoolFilterBasedSimpleQuery` must have a `bool` property, which itself
+ * must have a `filter` property. This `filter` must be an array with exactly
+ * one element, and that element must satisfy the conditions of a simple query
+ * as defined by `isSimpleQuery`.
+ *
+ * The type guard is useful to identify simple queries within bool filter
+ * queries exposed from Kibana/EUI search bars.
+ *
+ * @param arg - The argument to be checked.
+ * @returns `true` if `arg` meets the criteria of a `BoolFilterBasedSimpleQuery`, otherwise `false`.
+ */
 export function isBoolFilterBasedSimpleQuery(arg: unknown): arg is BoolFilterBasedSimpleQuery {
   return (
     isPopulatedObject(arg, ['bool']) &&
--- x-pack/packages/ml/query_utils/src/filter_based_default_query.ts
+++ x-pack/packages/ml/query_utils/src/filter_based_default_query.ts
@@ -11,9 +11,25 @@ import { isDefaultQuery } from './default_query';
 
 const boolRequiredAttributes = ['filter', 'must', 'must_not'];
 
-// should identify variants of
-// `{ bool: { filter: [{ match_all: {} }], must: [], must_not: [], should: [] } }`
-// `{ bool: { filter: [], must: [{ match_all: {} }], must_not: [] } }`
+/**
+ * Determines if the provided argument is a filter-based default query within a boolean filter context.
+ *
+ * A valid filter-based default query must include a `bool` property that contains
+ * `filter`, `must`, and `must_not` properties. These properties should either be empty arrays
+ * or arrays containing exactly one default query. The function checks for these conditions
+ * to identify variants of default queries structured within a boolean filter.
+ *
+ * Examples of valid structures include:
+ * - `{ bool: { filter: [{ match_all: {} }], must: [], must_not: [], should: [] } }`
+ * - `{ bool: { filter: [], must: [{ match_all: {} }], must_not: [] } }`
+ *
+ * Useful to identify simple queries within bool queries
+ * exposed from Kibana/EUI search bars.
+ *
+ * @param arg - The argument to be checked, its structure is unknown upfront.
+ * @returns  Returns `true` if `arg` matches the expected structure of a
+ * filter-based default query, otherwise `false`.
+ */
 export function isFilterBasedDefaultQuery(arg: unknown): boolean {
   return (
     isPopulatedObject(arg, ['bool']) &&
