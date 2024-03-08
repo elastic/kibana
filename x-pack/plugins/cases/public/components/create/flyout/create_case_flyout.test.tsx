5962f4fe64ac8417b241c571b9341d37dd30bcf4
Tiago Costa <tiago.costa@elastic.co>
2024-03-08 17:44:12 +0000

    skip flaky suite (#174527)

--- x-pack/plugins/cases/public/components/create/flyout/create_case_flyout.test.tsx
+++ x-pack/plugins/cases/public/components/create/flyout/create_case_flyout.test.tsx
@@ -41,6 +41,7 @@ const defaultProps = {
 
 // FLAKY: https://github.com/elastic/kibana/issues/174525
 // FLAKY: https://github.com/elastic/kibana/issues/174526
+// FLAKY: https://github.com/elastic/kibana/issues/174527
 describe.skip('CreateCaseFlyout', () => {
   let appMockRenderer: AppMockRenderer;
 
