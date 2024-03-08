68fc2819700f5da4ff0aa20c65b38a205267bdb0
Christos Nasikas <christos.nasikas@elastic.co>
2024-03-08 17:36:07 +0200

    Add integration tests

--- x-pack/test/cases_api_integration/security_and_spaces/tests/trial/connectors/cases/cases_connector.ts
+++ x-pack/test/cases_api_integration/security_and_spaces/tests/trial/connectors/cases/cases_connector.ts
@@ -9,7 +9,14 @@ import expect from '@kbn/expect';
 import { createHash } from 'node:crypto';
 import stringify from 'json-stable-stringify';
 import { CasesConnectorRunParams } from '@kbn/cases-plugin/server/connectors/cases/types';
-import { deleteAllCaseItems, executeConnector, findCases } from '../../../../../common/lib/api';
+import { AttachmentType } from '@kbn/cases-plugin/common';
+import { AlertAttachment, Case } from '@kbn/cases-plugin/common/types/domain';
+import {
+  deleteAllCaseItems,
+  executeConnector,
+  findCases,
+  getAllComments,
+} from '../../../../../common/lib/api';
 import { FtrProviderContext } from '../../../../../common/ftr_provider_context';
 
 // eslint-disable-next-line import/no-default-export
@@ -55,7 +62,7 @@ export default ({ getService }: FtrProviderContext): void => {
       await deleteAllCaseItems(es);
     });
 
-    describe.only('Without grouping', () => {
+    describe('Without grouping', () => {
       const req: { subAction: 'run'; subActionParams: CasesConnectorRunParams } = {
         subAction: 'run',
         subActionParams: {
@@ -71,20 +78,222 @@ export default ({ getService }: FtrProviderContext): void => {
 
       it('should create a case correctly', async () => {
         const res = await executeConnector({ supertest, connectorId, req: { params: req } });
-        console.log(res);
+
         expect(res.status).to.be('ok');
         const cases = await findCases({ supertest });
-        console.log(cases);
+        expect(cases.total).to.be(1);
+      });
+
+      it('should create a case with the correct ID', async () => {
+        const res = await executeConnector({ supertest, connectorId, req: { params: req } });
+        expect(res.status).to.be('ok');
+        const cases = await findCases({ supertest });
         expect(cases.total).to.be(1);
 
         const theCase = cases.cases[0];
 
-        console.log(generateCaseId({ ruleId: rule.id }));
         expect(theCase.id).to.be(generateCaseId({ ruleId: rule.id }));
+      });
+
+      it('should create a case with the correct attributes', async () => {
+        const res = await executeConnector({ supertest, connectorId, req: { params: req } });
+        expect(res.status).to.be('ok');
+        const cases = await findCases({ supertest });
+        expect(cases.total).to.be(1);
+
+        const theCase = removeServerGeneratedData(cases.cases[0]);
+
+        expect(theCase).to.eql({
+          assignees: [],
+          category: null,
+          closed_at: null,
+          closed_by: null,
+          comments: [],
+          connector: {
+            fields: null,
+            id: 'none',
+            name: 'none',
+            type: '.none',
+          },
+          created_by: {
+            email: null,
+            full_name: null,
+            username: 'elastic',
+          },
+          customFields: [],
+          description:
+            'This case is auto-created by [Test rule](https://example.com/rules/rule-test-id). \n\n Grouping:',
+          duration: null,
+          external_service: null,
+          id: 'ee06877e50151293e75cd6c5bd81812c15c25be55ed970f91c6f7dc40e1eafa6',
+          owner: 'securitySolutionFixture',
+          settings: {
+            syncAlerts: false,
+          },
+          severity: 'low',
+          status: 'open',
+          tags: ['auto-generated', 'rule:rule-test-id', 'rule', 'test'],
+          title: 'Test rule (Auto-created)',
+          totalAlerts: 5,
+          totalComment: 0,
+          updated_by: {
+            email: null,
+            full_name: null,
+            username: 'elastic',
+          },
+        });
+      });
+
+      it('should attach the correct number of alerts', async () => {
+        const res = await executeConnector({ supertest, connectorId, req: { params: req } });
+        expect(res.status).to.be('ok');
+        const cases = await findCases({ supertest });
+        expect(cases.total).to.be(1);
+
+        const theCase = cases.cases[0];
+
+        expect(theCase.totalAlerts).to.be(alerts.length);
+      });
+
+      it('should attach the correct alerts', async () => {
+        const res = await executeConnector({ supertest, connectorId, req: { params: req } });
+        expect(res.status).to.be('ok');
+        const cases = await findCases({ supertest });
+        expect(cases.total).to.be(1);
+
+        const theCase = cases.cases[0];
+
+        const attachments = await getAllComments({ supertest, caseId: theCase.id });
+        const alertsAttachments = attachments.filter(
+          (attachment): attachment is AlertAttachment => attachment.type === AttachmentType.alert
+        );
+
+        expect(alertsAttachments.length).to.be(alerts.length);
+
+        for (const alert of alertsAttachments) {
+          const foundAlert = alerts.find((_alert) => _alert._id === alert.alertId[0]);
+
+          expect(foundAlert?._index).to.be(alert.index[0]);
+          expect(alert.rule.id).to.be(rule.id);
+          expect(alert.rule.name).to.be(rule.name);
+        }
       });
     });
 
-    describe('With grouping', () => {});
+    describe('With grouping', () => {
+      const req: { subAction: 'run'; subActionParams: CasesConnectorRunParams } = {
+        subAction: 'run',
+        subActionParams: {
+          alerts,
+          groupingBy: ['host.name'],
+          rule,
+          owner,
+          timeWindow,
+          reopenClosedCases,
+          maximumCasesToOpen: 5,
+        },
+      };
+
+      it('should create cases correctly', async () => {
+        const res = await executeConnector({ supertest, connectorId, req: { params: req } });
+
+        expect(res.status).to.be('ok');
+        const cases = await findCases({ supertest });
+
+        expect(cases.total).to.be(2);
+
+        const firstCaseId = generateCaseId({ ruleId: rule.id, grouping: { 'host.name': 'A' } });
+        const secondCaseId = generateCaseId({ ruleId: rule.id, grouping: { 'host.name': 'B' } });
+
+        const firstCase = removeServerGeneratedData(
+          cases.cases.find((theCase) => theCase.id === firstCaseId)!
+        );
+
+        const secondCase = removeServerGeneratedData(
+          cases.cases.find((theCase) => theCase.id === secondCaseId)!
+        );
+
+        expect(firstCase).to.eql({
+          assignees: [],
+          category: null,
+          closed_at: null,
+          closed_by: null,
+          comments: [],
+          connector: {
+            fields: null,
+            id: 'none',
+            name: 'none',
+            type: '.none',
+          },
+          created_by: {
+            email: null,
+            full_name: null,
+            username: 'elastic',
+          },
+          customFields: [],
+          description:
+            'This case is auto-created by [Test rule](https://example.com/rules/rule-test-id). \n\n Grouping: `host.name` equals `A`',
+          duration: null,
+          external_service: null,
+          id: firstCaseId,
+          owner: 'securitySolutionFixture',
+          settings: {
+            syncAlerts: false,
+          },
+          severity: 'low',
+          status: 'open',
+          tags: ['auto-generated', 'rule:rule-test-id', 'host.name:A', 'rule', 'test'],
+          title: 'Test rule (Auto-created)',
+          totalAlerts: 3,
+          totalComment: 0,
+          updated_by: {
+            email: null,
+            full_name: null,
+            username: 'elastic',
+          },
+        });
+
+        expect(secondCase).to.eql({
+          assignees: [],
+          category: null,
+          closed_at: null,
+          closed_by: null,
+          comments: [],
+          connector: {
+            fields: null,
+            id: 'none',
+            name: 'none',
+            type: '.none',
+          },
+          created_by: {
+            email: null,
+            full_name: null,
+            username: 'elastic',
+          },
+          customFields: [],
+          description:
+            'This case is auto-created by [Test rule](https://example.com/rules/rule-test-id). \n\n Grouping: `host.name` equals `B`',
+          duration: null,
+          external_service: null,
+          id: secondCaseId,
+          owner: 'securitySolutionFixture',
+          settings: {
+            syncAlerts: false,
+          },
+          severity: 'low',
+          status: 'open',
+          tags: ['auto-generated', 'rule:rule-test-id', 'host.name:B', 'rule', 'test'],
+          title: 'Test rule (Auto-created)',
+          totalAlerts: 2,
+          totalComment: 0,
+          updated_by: {
+            email: null,
+            full_name: null,
+            username: 'elastic',
+          },
+        });
+      });
+    });
   });
 };
 
@@ -104,7 +313,13 @@ const generateCaseId = ({
   const payload = [ruleId, spaceId, owner, stringify(grouping), counter].filter(Boolean).join(':');
   const hash = createHash('sha256');
 
-  console.log('our payload', payload);
   hash.update(payload);
   return hash.digest('hex');
+};
+
+const removeServerGeneratedData = (theCase: Case): Partial<Case> => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { created_at, updated_at, version, ...restCase } = theCase;
+
+  return restCase;
 };
