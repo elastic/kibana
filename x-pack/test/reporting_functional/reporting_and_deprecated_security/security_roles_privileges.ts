2c31ceca403fda0dc22096ba9b1b462a26b90d4e
Timothy Sullivan <tsullivan@elastic.co>
2024-03-09 01:36:44 -0700

    Fix functional tests

--- x-pack/test/reporting_functional/reporting_and_deprecated_security/security_roles_privileges.ts
+++ x-pack/test/reporting_functional/reporting_and_deprecated_security/security_roles_privileges.ts
@@ -31,7 +31,7 @@ export default function ({ getService }: FtrProviderContext) {
       });
 
       it('does allow user with reporting_user role', async () => {
-        await reportingFunctional.loginDataAnalyst();
+        await reportingFunctional.loginReportingUser();
         await reportingFunctional.openSavedDashboard(DASHBOARD_TITLE);
         await reportingFunctional.tryDashboardGenerateCsvSuccess('Ecommerce Data');
       });
