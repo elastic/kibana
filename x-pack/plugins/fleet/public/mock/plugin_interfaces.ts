3b6ad6079f408228a88ae2d0e1d1f7c31479a763
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2024-03-11 15:20:00 -0400

    fix type

--- x-pack/plugins/fleet/public/mock/plugin_interfaces.ts
+++ x-pack/plugins/fleet/public/mock/plugin_interfaces.ts
@@ -20,6 +20,7 @@ export const createStartMock = (extensionsStorage: UIExtensionsStorage = {}): Mo
         setup: true,
         readEnrollmentTokens: true,
         readAgentPolicies: true,
+        allAgentPolicies: true,
         allAgents: true,
         allSettings: true,
         readAgents: true,
