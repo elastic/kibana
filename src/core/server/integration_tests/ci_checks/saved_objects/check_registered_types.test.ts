0a2895716659a47891d184bc59903032ea592196
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-03-11 11:35:11 +0000

    [CI] Auto-commit changed files from 'node scripts/jest_integration -u src/core/server/integration_tests/ci_checks'

--- src/core/server/integration_tests/ci_checks/saved_objects/check_registered_types.test.ts
+++ src/core/server/integration_tests/ci_checks/saved_objects/check_registered_types.test.ts
@@ -83,6 +83,7 @@ describe('checking migration metadata changes on all registered SO types', () =>
         "core-usage-stats": "b3c04da317c957741ebcdedfea4524049fdc79ff",
         "csp-rule-template": "c151324d5f85178169395eecb12bac6b96064654",
         "dashboard": "0611794ce10d25a36da0770c91376c575e92e8f2",
+        "endpoint:unified-user-artifact-manifest": "1a9ebf2ee6875a8522436eb11967e3d8f97fb8a0",
         "endpoint:user-artifact-manifest": "1c3533161811a58772e30cdc77bac4631da3ef2b",
         "enterprise_search_telemetry": "9ac912e1417fc8681e0cd383775382117c9e3d3d",
         "epm-packages": "c23d3d00c051a08817335dba26f542b64b18a56a",
