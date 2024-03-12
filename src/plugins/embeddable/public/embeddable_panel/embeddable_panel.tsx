4fe80c9247000a83e08890cbf5250ca12afedd60
Nathan Reese <reese.nathan@gmail.com>
2024-03-12 14:56:17 -0600

    resolve timing issue when embeddable is object

--- src/plugins/embeddable/public/embeddable_panel/embeddable_panel.tsx
+++ src/plugins/embeddable/public/embeddable_panel/embeddable_panel.tsx
@@ -10,8 +10,7 @@ import { css } from '@emotion/react';
 import { PresentationPanel } from '@kbn/presentation-panel-plugin/public';
 import { PanelCompatibleComponent } from '@kbn/presentation-panel-plugin/public/panel_component/types';
 import { isPromise } from '@kbn/std';
-import React, { ReactNode, useEffect, useImperativeHandle, useMemo, useState } from 'react';
-import useMountedState from 'react-use/lib/useMountedState';
+import React, { ReactNode, useEffect, useImperativeHandle, useMemo, useState, useRef } from 'react';
 import { untilPluginStartServicesReady } from '../kibana_services';
 import { EmbeddablePanelProps } from './types';
 
@@ -65,7 +64,20 @@ const getComponentFromEmbeddable = async (
  * For example <Parent key={embeddableId}><EmbeddablePanel/></Parent>
  */
 export const EmbeddablePanel = (props: EmbeddablePanelProps) => {
-  const isMounted = useMountedState();
+  // can not use useMountedState
+  // 1. useMountedState defaults mountedRef to false and sets mountedRef to true in useEffect
+  // 2. embeddable can be an object or a function that returns a promise
+  // 3. when embeddable is an object, Promise.resolve(embeddable) returns before 
+  //    useMountedState useEffect is called and thus isMounted() returns false when component has not been unmounted
+  const mountedRef = useRef<boolean>(true);
+  useEffect(() => {
+    return () => {
+      mountedRef.current = false;
+    }
+  }, []);
+  const isMounted = () => {
+    return mountedRef.current;
+  };
   const { embeddable, ...passThroughProps } = props;
   const componentPromise = useMemo(
     () => getComponentFromEmbeddable(embeddable, isMounted),
