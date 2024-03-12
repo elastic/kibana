elastic/kibana

commit 283b4505346de721fc50268ac6b44cb588aefcf1

Fix CO2/cost values in diff flamegraph tooltip (#178481)
Fixes the wrong CO2 and cost values shown in the differential flamegraph
tooltip.

**example of wrong tooltip values**

![Screenshot_20240312_084224](https://github.com/elastic/kibana/assets/2087964/a13f4cf1-5d4b-4ff1-8569-be7e29338a7b)

x-pack/plugins/observability_solution/profiling/public/components/flamegraph/index.tsx
@@ -194,12 +194,8 @@ export function FlameGraph({
                             primaryFlamegraph.TotalAnnualCostsUSDItems[valueIndex]
                           }
                           baselineScaleFactor={baseline}
-                          comparisonAnnualCO2KgsInclusive={
-                            comparisonFlamegraph?.TotalAnnualCO2KgsItems[valueIndex]
-                          }
-                          comparisonAnnualCostsUSDInclusive={
-                            comparisonFlamegraph?.TotalAnnualCostsUSDItems[valueIndex]
-                          }
+                          comparisonAnnualCO2KgsInclusive={comparisonNode.TotalAnnualCO2Kgs}
+                          comparisonAnnualCostsUSDInclusive={comparisonNode?.TotalAnnualCostUSD}
                           comparisonCountExclusive={comparisonNode?.CountExclusive}
                           comparisonCountInclusive={comparisonNode?.CountInclusive}
                           comparisonScaleFactor={comparison}

commit b91962e01827060a4fe95512663258dd42eb4b58

Fix flamegraph regression (#178511)
Fix a regression introduced in
https://github.com/elastic/kibana/pull/178481

x-pack/plugins/observability_solution/profiling/public/components/flamegraph/index.tsx
@@ -194,7 +194,7 @@ export function FlameGraph({
                             primaryFlamegraph.TotalAnnualCostsUSDItems[valueIndex]
                           }
                           baselineScaleFactor={baseline}
-                          comparisonAnnualCO2KgsInclusive={comparisonNode.TotalAnnualCO2Kgs}
+                          comparisonAnnualCO2KgsInclusive={comparisonNode?.TotalAnnualCO2Kgs}
                           comparisonAnnualCostsUSDInclusive={comparisonNode?.TotalAnnualCostUSD}
                           comparisonCountExclusive={comparisonNode?.CountExclusive}
                           comparisonCountInclusive={comparisonNode?.CountInclusive}