e8f755a51e880300e69cfc4bee52102673d6d58e
Maxim Palenov <maxim.palenov@elastic.co>
2024-03-12 16:21:45 +0100

    extend checks with OpenAPI bundling

--- .buildkite/scripts/steps/checks.sh
+++ .buildkite/scripts/steps/checks.sh
@@ -9,4 +9,5 @@ export DISABLE_BOOTSTRAP_VALIDATION=false
 .buildkite/scripts/steps/checks/saved_objects_definition_change.sh
 .buildkite/scripts/steps/code_generation/elastic_assistant_codegen.sh
 .buildkite/scripts/steps/code_generation/security_solution_codegen.sh
+.buildkite/scripts/steps/openapi_bundling/security_solution_openapi_bundling.sh
 .buildkite/scripts/steps/code_generation/osquery_codegen.sh
