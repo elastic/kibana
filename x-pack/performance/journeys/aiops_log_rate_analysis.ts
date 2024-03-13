f28885f4a620a950c611fa990b404ac63085ffe0
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-03-13 09:47:15 +0100

    use esArchive/kbnArchive

--- x-pack/performance/journeys/aiops_log_rate_analysis.ts
+++ x-pack/performance/journeys/aiops_log_rate_analysis.ts
@@ -5,78 +5,21 @@
  * 2.0.
  */
 
-import * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
-
 import { Journey } from '@kbn/journeys';
 import { subj } from '@kbn/test-subj-selector';
-import type { DataView } from '@kbn/data-views-plugin/common';
-import { frequentItemSetsLargeArraysSource } from '@kbn/aiops-test-utils/frequent_item_sets_large_arrays';
 
-const indexName = 'large_arrays';
-let dataViewId: string | undefined;
-
-interface DocWithArray {
-  '@timestamp': number;
-  items: string[];
-}
+import dataView from '../../../test/functional/fixtures/kbn_archiver/large_arrays_data_view.json';
 
 export const journey = new Journey({
-  beforeSteps: async ({ es, kibanaServer }) => {
-    // Create index with mapping
-    await es.indices.create({
-      index: indexName,
-      mappings: {
-        properties: {
-          items: { type: 'keyword' },
-          '@timestamp': { type: 'date' },
-        },
-      },
-    });
-
-    // Ingest sample data
-    await es.bulk({
-      refresh: 'wait_for',
-      body: frequentItemSetsLargeArraysSource.reduce((docs, items) => {
-        if (docs === undefined) return [];
-        docs.push({ index: { _index: indexName } });
-        docs.push({
-          '@timestamp': 1562254538700,
-          items,
-        });
-        return docs;
-      }, [] as estypes.BulkRequest<DocWithArray, DocWithArray>['body']),
-    });
-
-    // Create data view
-    const dataViewResp = await kibanaServer.request<DataView>({
-      method: 'POST',
-      path: '/api/saved_objects/index-pattern',
-      body: {
-        attributes: { title: indexName, timeFieldName: '@timestamp' },
-      },
-    });
-
-    dataViewId = dataViewResp.data.id;
-  },
-  afterSteps: async ({ es, kibanaServer }) => {
-    // Delete index
-    await es.indices.delete({
-      index: indexName,
-    });
-
-    // Delete data view
-    await kibanaServer.request({
-      method: 'DELETE',
-      path: `/api/saved_objects/index-pattern/${dataViewId}?force=true`,
-    });
-  },
+  kbnArchives: ['test/functional/fixtures/kbn_archiver/large_arrays_data_view'],
+  esArchives: ['test/functional/fixtures/es_archiver/large_arrays'],
 })
   .step('Go to AIOps Log Rate Analysis', async ({ page, kbnUrl }) => {
     // Navigate to Log Rate Analysis with a prepopulated url state that runs
     // the analysis directly on page load without any necessary user interactions.
     await page.goto(
       kbnUrl.get(
-        `app/ml/aiops/log_rate_analysis?index=${dataViewId}&_g=%28refreshInterval%3A%28pause%3A%21t%2Cvalue%3A60000%29%2Ctime%3A%28from%3A%272019-07-01T15%3A35%3A38.700Z%27%2Cto%3A%272019-07-05T15%3A35%3A38.700Z%27%29%29&_a=%28logRateAnalysis%3A%28filters%3A%21%28%29%2CsearchQuery%3A%28match_all%3A%28%29%29%2CsearchQueryLanguage%3Akuery%2CsearchString%3A%27%27%2Cwp%3A%28bMax%3A1562198400000%2CbMin%3A1562097600000%2CdMax%3A1562270400000%2CdMin%3A1562234400000%29%29%29`
+        `app/ml/aiops/log_rate_analysis?index=${dataView.id}&_g=%28refreshInterval%3A%28pause%3A%21t%2Cvalue%3A60000%29%2Ctime%3A%28from%3A%272019-07-01T15%3A35%3A38.700Z%27%2Cto%3A%272019-07-05T15%3A35%3A38.700Z%27%29%29&_a=%28logRateAnalysis%3A%28filters%3A%21%28%29%2CsearchQuery%3A%28match_all%3A%28%29%29%2CsearchQueryLanguage%3Akuery%2CsearchString%3A%27%27%2Cwp%3A%28bMax%3A1562198400000%2CbMin%3A1562097600000%2CdMax%3A1562270400000%2CdMin%3A1562234400000%29%29%29`
       )
     );
 
