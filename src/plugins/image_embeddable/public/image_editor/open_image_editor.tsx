1893de6fca48396a11512d29010be9425d9d752f
Hannah Mudge <hannah.wright@elastic.co>
2024-03-13 12:31:47 -0600

    Change to new parent structure

--- src/plugins/image_embeddable/public/image_editor/open_image_editor.tsx
+++ src/plugins/image_embeddable/public/image_editor/open_image_editor.tsx
@@ -17,7 +17,7 @@ import { FileImageMetadata, imageEmbeddableFileKind } from '../imports';
 import { coreServices, filesService, securityService } from '../services/kibana_services';
 import { createValidateUrl } from '../utils/validate_url';
 
-export const openImageEditor = async (initialImageConfig?: ImageConfig) => {
+export const openImageEditor = async (initialImageConfig?: ImageConfig): Promise<ImageConfig> => {
   const { overlays, theme, application, i18n, http } = coreServices;
   const user = securityService ? await securityService.authc.getCurrentUser() : undefined;
   const filesClient = filesService.filesClientFactory.asUnscoped<FileImageMetadata>();
--- src/plugins/image_embeddable/public/image_embeddable/register_image_embeddable_factory.tsx
+++ src/plugins/image_embeddable/public/image_embeddable/register_image_embeddable_factory.tsx
@@ -6,28 +6,21 @@
  * Side Public License, v 1.
  */
 
-import React, { useEffect, useLayoutEffect, useMemo, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import deepEqual from 'react-fast-compare';
 import { BehaviorSubject } from 'rxjs';
 
 import {
   initializeReactEmbeddableTitles,
-  initializeReactEmbeddableUuid,
   ReactEmbeddableFactory,
-  RegisterReactEmbeddable,
   registerReactEmbeddableFactory,
-  useReactEmbeddableApiHandle,
-  useReactEmbeddableParentApi,
-  useReactEmbeddableUnsavedChanges,
 } from '@kbn/embeddable-plugin/public';
-import { apiIsPresentationContainer } from '@kbn/presentation-containers';
-import { usePublishingSubject, useStateFromPublishingSubject } from '@kbn/presentation-publishing';
-import { FileImageMetadata } from '@kbn/shared-ux-file-types';
+import { useStateFromPublishingSubject } from '@kbn/presentation-publishing';
 
 import { imageClickTrigger } from '../actions';
 import { openImageEditor } from '../image_editor/open_image_editor';
 import { ImageViewer, ImageViewerContext } from '../image_viewer';
-import { imageEmbeddableFileKind } from '../imports';
+import { FileImageMetadata, imageEmbeddableFileKind } from '../imports';
 import {
   coreServices,
   filesService,
@@ -44,130 +37,111 @@ export const registerImageEmbeddableFactory = () => {
     ImageEmbeddableSerializedState,
     ImageEmbeddableApi
   > = {
+    type: IMAGE_EMBEDDABLE_TYPE,
     deserializeState: (state) => {
       return state.rawState as ImageEmbeddableSerializedState;
     },
-    getComponent: async (initialState, maybeId) => {
-      const uuid = initializeReactEmbeddableUuid(maybeId);
+    buildEmbeddable: async (initialState, buildApi) => {
       const { titlesApi, titleComparators, serializeTitles } =
         initializeReactEmbeddableTitles(initialState);
+
       const filesClient = filesService.filesClientFactory.asUnscoped<FileImageMetadata>();
       const imageConfig$ = new BehaviorSubject<ImageConfig>(initialState.imageConfig);
+      const dataLoading$ = new BehaviorSubject<boolean | undefined>(true);
+      const blockingError$ = new BehaviorSubject<Error | undefined>(undefined);
 
-      return RegisterReactEmbeddable<ImageEmbeddableApi>((apiRef) => {
-        const [loading, setLoading] = useState(true);
-        const [error, setError] = useState<Error | undefined>(undefined);
-        const [hasTriggerActions, setHasTriggerActions] = useState(false);
-
-        const parentApi = useReactEmbeddableParentApi();
-        const imageConfig = useStateFromPublishingSubject(imageConfig$);
-        const dataLoading = usePublishingSubject<boolean | undefined>(loading);
-        const blockingError = usePublishingSubject<Error | undefined>(error);
-
-        useLayoutEffect(() => {
-          import('./image_embeddable_lazy');
-        }, []);
-
-        const { unsavedChanges, resetUnsavedChanges } = useReactEmbeddableUnsavedChanges(
-          uuid,
-          imageEmbeddableFactory,
-          {
-            imageConfig: [
-              imageConfig$,
-              (value) => imageConfig$.next(value),
-              (a, b) => deepEqual(a, b),
-            ],
-            ...titleComparators,
-          }
-        );
-
-        useReactEmbeddableApiHandle(
-          {
-            ...titlesApi,
-            unsavedChanges,
-            resetUnsavedChanges,
-            dataLoading,
-            blockingError,
-            onEdit: async () => {
-              const newImageConfig = await openImageEditor(imageConfig);
-
-              if (apiIsPresentationContainer(parentApi)) {
-                parentApi.replacePanel(uuid, {
-                  panelType: IMAGE_EMBEDDABLE_TYPE,
-                  initialState: { imageConfig: newImageConfig, ...serializeTitles() },
-                });
-              }
-            },
-            isEditingEnabled: () => true,
-            getTypeDisplayName: ImageEmbeddableStrings.getEditDisplayName,
-            serializeState: async () => {
-              return {
-                rawState: {
-                  imageConfig,
-                  ...serializeTitles(),
-                },
-              };
-            },
+      const api = buildApi(
+        {
+          ...titlesApi,
+          dataLoading: dataLoading$,
+          blockingError: blockingError$,
+          onEdit: async () => {
+            const newImageConfig = await openImageEditor(imageConfig$.getValue());
+            imageConfig$.next(newImageConfig);
           },
-          apiRef,
-          uuid
-        );
-
-        useEffect(() => {
-          let mounted = true;
-
-          // hack: timeout to give a chance for a drilldown action to be registered just after it is created by user
-          setTimeout(() => {
-            if (!mounted) return;
-            uiActionsService
-              .getTriggerCompatibleActions(imageClickTrigger.id, { embeddable: this })
-              .catch(() => [])
-              .then((actions) => mounted && setHasTriggerActions(actions.length > 0));
-          }, 0);
-          return () => {
-            mounted = false;
-          };
-        });
-
-        return (
-          <ImageViewerContext.Provider
-            value={{
-              getImageDownloadHref: (fileId: string) => {
-                return filesClient.getDownloadHref({
-                  id: fileId,
-                  fileKind: imageEmbeddableFileKind.id,
-                });
+          isEditingEnabled: () => true,
+          getTypeDisplayName: ImageEmbeddableStrings.getEditDisplayName,
+          serializeState: () => {
+            return {
+              rawState: {
+                ...serializeTitles(),
+                imageConfig: imageConfig$.getValue(),
               },
-              validateUrl: createValidateUrl(coreServices.http.externalUrl),
-            }}
-          >
-            <ImageViewer
-              className="imageEmbeddableImage"
-              imageConfig={imageConfig}
-              isScreenshotMode={screenshotModeService?.isScreenshotMode()}
-              onLoad={() => {
-                setLoading(false);
+            };
+          },
+        },
+        {
+          ...titleComparators,
+          imageConfig: [
+            imageConfig$,
+            (value) => imageConfig$.next(value),
+            (a, b) => deepEqual(a, b),
+          ],
+        }
+      );
+
+      return {
+        api,
+        Component: () => {
+          const imageConfig = useStateFromPublishingSubject(imageConfig$);
+          const [hasTriggerActions, setHasTriggerActions] = useState(false);
+
+          useEffect(() => {
+            let mounted = true;
+
+            // hack: timeout to give a chance for a drilldown action to be registered just after it is created by user
+            setTimeout(() => {
+              if (!mounted) return;
+              uiActionsService
+                .getTriggerCompatibleActions(imageClickTrigger.id, { embeddable: this })
+                .catch(() => [])
+                .then((actions) => mounted && setHasTriggerActions(actions.length > 0));
+            }, 0);
+            return () => {
+              mounted = false;
+            };
+          });
+
+          return (
+            <ImageViewerContext.Provider
+              value={{
+                getImageDownloadHref: (fileId: string) => {
+                  return filesClient.getDownloadHref({
+                    id: fileId,
+                    fileKind: imageEmbeddableFileKind.id,
+                  });
+                },
+                validateUrl: createValidateUrl(coreServices.http.externalUrl),
               }}
-              onError={() => {
-                setLoading(false);
-                setError(new Error(ImageEmbeddableStrings.getErrorMessage()));
-              }}
-              onClick={
-                // note: passing onClick enables the cursor pointer style, so we only pass it if there are compatible actions
-                hasTriggerActions
-                  ? () => {
-                      uiActionsService.executeTriggerActions(imageClickTrigger.id, {
-                        embeddable: this,
-                      });
-                    }
-                  : undefined
-              }
-            />
-          </ImageViewerContext.Provider>
-        );
-      });
+            >
+              <ImageViewer
+                className="imageEmbeddableImage"
+                imageConfig={imageConfig}
+                isScreenshotMode={screenshotModeService?.isScreenshotMode()}
+                onLoad={() => {
+                  dataLoading$.next(false);
+                }}
+                onError={() => {
+                  dataLoading$.next(false);
+                  blockingError$.next(new Error(ImageEmbeddableStrings.getErrorMessage()));
+                }}
+                onClick={
+                  // note: passing onClick enables the cursor pointer style, so we only pass it if there are compatible actions
+                  hasTriggerActions
+                    ? () => {
+                        uiActionsService.executeTriggerActions(imageClickTrigger.id, {
+                          embeddable: this,
+                        });
+                      }
+                    : undefined
+                }
+              />
+            </ImageViewerContext.Provider>
+          );
+        },
+      };
     },
   };
 
-  registerReactEmbeddableFactory(IMAGE_EMBEDDABLE_TYPE, imageEmbeddableFactory);
+  registerReactEmbeddableFactory(imageEmbeddableFactory);
 };
