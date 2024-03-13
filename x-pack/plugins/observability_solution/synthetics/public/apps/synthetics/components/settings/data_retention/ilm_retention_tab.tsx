bbbdef8f95f96b760397214d498106ea15625f2c
Justin Kambic <jk@elastic.co>
2024-03-13 17:04:39 -0400

    [Synthetics] Add DSL-compliant Data Retention settings view (#177764)
    ## Summary
    
    Resolves #176540.
    
    Adds a new settings view for Synthetics to use in Serverless. The old
    view is dependent on ILM data, which is not present in Serverless. This
    new view queries the data stream API and presents actionable information
    the user can make use of in Serverless, such as links to data stream
    info, the index template, etc., as well as providing the same extra
    information like storage usage.
    
    As a potential enhancement, we can also include things like `health`,
    the number of indices in the data stream, etc., but the primary goal of
    this patch is to achieve parity with what we have in stateful Kibana.
    
    ## Testing
    
    I've created a Serverless cluster for this PR that should allow the
    tester to see this in action. Create a few monitors, let them collect
    data, then navigate to Settings > Data Retention and view that there is
    information displayed.
    
    Additionally, you can create an ESS cluster and verify that the
    ILM-specific data is displayed there as well, there should be no
    deviation from the way it worked previously.

--- x-pack/plugins/observability_solution/synthetics/public/apps/synthetics/components/settings/data_retention/ilm_retention_tab.tsx
+++ x-pack/plugins/observability_solution/synthetics/public/apps/synthetics/components/settings/data_retention/ilm_retention_tab.tsx
@@ -6,24 +6,15 @@
  */
 
 import { FormattedMessage } from '@kbn/i18n-react';
-import {
-  EuiBasicTable,
-  EuiCallOut,
-  EuiEmptyPrompt,
-  EuiIcon,
-  EuiLink,
-  EuiMarkdownFormat,
-  EuiSpacer,
-} from '@elastic/eui';
+import { EuiBasicTable, EuiCallOut, EuiLink, EuiSpacer } from '@elastic/eui';
 import React from 'react';
 import { i18n } from '@kbn/i18n';
-import { css } from '@emotion/react';
 import { IHttpFetchError, ResponseErrorBody } from '@kbn/core-http-browser';
-import { useKibana } from '@kbn/kibana-react-plugin/public';
-import { PolicyLink, PolicyNameLabel } from './policy_link';
-import { useGetIlmPolicies } from './hooks/use_get_ilm_policies';
+import { PolicyLink, PolicyNameLabel } from '../policy_link';
+import { useGetIlmPolicies } from '../hooks/use_get_ilm_policies';
+import { Unprivileged } from './unprivileged';
 
-export const DataRetentionTab = () => {
+export const IlmRetentionTab = () => {
   const { data, loading, error } = useGetIlmPolicies();
 
   if (error && (error as unknown as IHttpFetchError<ResponseErrorBody>).body?.statusCode === 403) {
@@ -88,65 +79,6 @@ export const DataRetentionTab = () => {
     </div>
   );
 };
-
-const Unprivileged = () => {
-  const {
-    services: { docLinks },
-  } = useKibana();
-  return (
-    <EuiEmptyPrompt
-      data-test-subj="syntheticsUnprivileged"
-      color="plain"
-      icon={<EuiIcon type="logoObservability" size="xl" />}
-      title={
-        <h2>
-          <FormattedMessage
-            id="xpack.synthetics.dataRetention.unprivileged.unprivilegedTitle"
-            defaultMessage="Missing privileges"
-          />
-        </h2>
-      }
-      body={
-        <p>
-          <FormattedMessage
-            id="xpack.synthetics.params.unprivileged.unprivilegedDescription"
-            defaultMessage="You need additional privileges to view Synthetics app data usage and retention settings. {docsLink}"
-            values={{
-              docsLink: (
-                <EuiLink
-                  data-test-subj="syntheticsUnprivilegedLearnMoreLink"
-                  href={docLinks?.links.synthetics.featureRoles}
-                  target="_blank"
-                >
-                  {i18n.translate('xpack.synthetics.monitorManagement.projectDelete.docsLink', {
-                    defaultMessage: 'Learn more',
-                  })}
-                </EuiLink>
-              ),
-            }}
-          />
-        </p>
-      }
-      footer={
-        <EuiMarkdownFormat
-          css={css`
-            text-align: initial;
-          `}
-          children={`\n- ${INDEX_PRIVILEGES} \n- ${CLUSTER_PRIVILEGES}`}
-        />
-      }
-    />
-  );
-};
-
-const INDEX_PRIVILEGES = i18n.translate('xpack.synthetics.dataRetention.unprivileged.index', {
-  defaultMessage: '`read`, `monitor` on the following Elasticsearch indices: `synthetics-*`',
-});
-
-const CLUSTER_PRIVILEGES = i18n.translate('xpack.synthetics.dataRetention.unprivileged.cluster', {
-  defaultMessage:
-    '`read_ilm`, `monitor` to view and `manage_ilm` to manage ILM policies on the Elasticsearch cluster.',
-});
 
 const CALLOUT_TITLE = i18n.translate('xpack.synthetics.settingsRoute.retentionCalloutTitle', {
   defaultMessage: 'Synthetics data is configured by managed index lifecycle policies',
