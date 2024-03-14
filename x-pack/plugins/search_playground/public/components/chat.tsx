dc40cd9ab58a3fb726468ec4aa22feeb4b8cf89f
Yan Savitski <yan.savitski@elastic.co>
2024-03-14 12:44:15 +0100

    Add autoscroll hook with mechanic of desable it by user scroll

--- x-pack/plugins/search_playground/public/components/chat.tsx
+++ x-pack/plugins/search_playground/public/components/chat.tsx
@@ -20,6 +20,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 import { i18n } from '@kbn/i18n';
 
+import { useAutoBottomScroll } from '../hooks/use_auto_bottom_scroll';
 import { ChatSidebar } from './chat_sidebar';
 import { useChat } from '../hooks/use_chat';
 import { ChatForm, ChatFormFields, MessageRole, SummarizationModelName } from '../types';
@@ -43,6 +44,7 @@ export const Chat = () => {
   } = useFormContext<ChatForm>();
   const { messages, append, stop: stopRequest } = useChat();
   const selectedIndicesCount = watch(ChatFormFields.indices, []).length;
+  const messagesRef = useAutoBottomScroll([showStartPage]);
 
   const onSubmit = async (data: ChatForm) => {
     await append(
@@ -95,14 +97,15 @@ export const Chat = () => {
         >
           <EuiFlexGroup direction="column" className="eui-fullHeight">
             {/* // Set scroll at the border of parent element*/}
-            <EuiFlexItem
-              grow={1}
+            <EuiFlexGroup
+              direction="column"
               className="eui-yScroll"
               css={{ paddingLeft: euiTheme.size.l, paddingRight: euiTheme.size.l }}
               tabIndex={0}
+              ref={messagesRef}
             >
               <MessageList messages={chatMessages} />
-            </EuiFlexItem>
+            </EuiFlexGroup>
 
             <EuiFlexItem
               grow={false}
