d71375a3fcc7d0b5adeba260189917a3997b968a
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-03-15 00:12:39 +0100

    run only x-pack/performance/journeys/tsdb_logs_data_visualizer.ts

--- .buildkite/scripts/steps/functional/performance_playwright.sh
+++ .buildkite/scripts/steps/functional/performance_playwright.sh
@@ -20,7 +20,7 @@ if [ "$BUILDKITE_PIPELINE_SLUG" == "kibana-performance-data-set-extraction" ]; t
 else
   # pipeline should use bare metal static worker
   echo "--- Running performance tests"
-  node scripts/run_performance.js --kibana-install-dir "$KIBANA_BUILD_LOCATION"
+  node scripts/run_performance.js --kibana-install-dir "$KIBANA_BUILD_LOCATION" --journey-path x-pack/performance/journeys/tsdb_logs_data_visualizer.ts
 fi
 
 echo "--- Upload journey step screenshots"
