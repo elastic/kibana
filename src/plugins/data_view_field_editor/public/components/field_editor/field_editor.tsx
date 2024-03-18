71cd5213d4491a131b713b804d79dc3959df802a
Matt Kime <matt@mattki.me>
2024-03-17 21:55:08 -0500

    restore temp disabled tests

--- src/plugins/data_view_field_editor/public/components/field_editor/field_editor.tsx
+++ src/plugins/data_view_field_editor/public/components/field_editor/field_editor.tsx
@@ -208,13 +208,6 @@ const FieldEditorComponent = ({ field, onChange, onFormModifiedChange }: Props) 
   }, [onChange, isFormValid, isSubmitted, isSubmitting, submit]);
 
   useEffect(() => {
-    console.log(
-      '#####',
-      updatedName,
-      updatedType && updatedType[0].name,
-      updatedScript,
-      updatedFormat
-    );
     updatePreviewParams({
       name: Boolean(updatedName?.trim()) ? updatedName : null,
       type: updatedType?.[0].value,
