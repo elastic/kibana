e1526dd22f87b57b3e3ca343a8f44f29e1bdcea4
Angela Chuang <yi-chun.chuang@elastic.co>
2024-03-18 17:22:03 +0000

    remove unused endpoint tests

--- x-pack/test/api_integration/apis/security_solution/index.js
+++ x-pack/test/api_integration/apis/security_solution/index.js
@@ -11,9 +11,6 @@ export default function ({ loadTestFile }) {
     loadTestFile(require.resolve('./events'));
     loadTestFile(require.resolve('./hosts'));
     loadTestFile(require.resolve('./host_details'));
-    loadTestFile(require.resolve('./kpi_network'));
-    loadTestFile(require.resolve('./kpi_hosts'));
-    loadTestFile(require.resolve('./matrix_dns_histogram'));
     loadTestFile(require.resolve('./network_details'));
     loadTestFile(require.resolve('./network_dns'));
     loadTestFile(require.resolve('./network_top_n_flow'));
