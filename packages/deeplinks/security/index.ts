cb01d77ad368849ccfd9d6113b3575c5284d85b0
Sébastien Loix <sebastien.loix@elastic.co>
2024-03-18 15:48:49 +0000

    Export SecurityPageName from the deepLinks security package

--- packages/deeplinks/security/index.ts
+++ packages/deeplinks/security/index.ts
@@ -6,4 +6,14 @@
  * Side Public License, v 1.
  */
 
-export type { LinkId, AppId, DeepLinkId } from './deep_links';
+import type { SecurityPageName } from './deep_links';
+
+export { SecurityPageName } from './deep_links';
+
+export const SECURITY_APP_ID = 'securitySolutionUI';
+
+export type AppId = typeof SECURITY_APP_ID;
+
+export type LinkId = `${SecurityPageName}`;
+
+export type DeepLinkId = AppId | `${AppId}:${LinkId}`;
