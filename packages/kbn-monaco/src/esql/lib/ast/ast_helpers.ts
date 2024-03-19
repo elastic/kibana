4b95cc97e0348c022c4cd97ef5fd682165f8638e
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-03-19 16:22:59 +0000

    updating ES|QL lexer grammar

--- packages/kbn-monaco/src/esql/lib/ast/ast_helpers.ts
+++ packages/kbn-monaco/src/esql/lib/ast/ast_helpers.ts
@@ -201,15 +201,15 @@ export function computeLocationExtends(fn: ESQLFunction) {
 
 /* SCRIPT_MARKER_START */
 function getQuotedText(ctx: ParserRuleContext) {
-  return [66 /* esql_parser.QUOTED_IDENTIFIER */]
+  return [67 /* esql_parser.QUOTED_IDENTIFIER */]
     .map((keyCode) => ctx.getToken(keyCode, 0))
     .filter(nonNullable)[0];
 }
 
 function getUnquotedText(ctx: ParserRuleContext) {
   return [
-    65 /* esql_parser.UNQUOTED_IDENTIFIER */, 71 /* esql_parser.FROM_UNQUOTED_IDENTIFIER */,
-    105 /* esql_parser.UNQUOTED_ID_PATTERN */,
+    66 /* esql_parser.UNQUOTED_IDENTIFIER */, 72 /* esql_parser.FROM_UNQUOTED_IDENTIFIER */,
+    109 /* esql_parser.UNQUOTED_ID_PATTERN */,
   ]
     .map((keyCode) => ctx.getToken(keyCode, 0))
     .filter(nonNullable)[0];
