6eedc106ea671660ff8dd6fca63b0684697b4a17
Drew Tate <andrew.tate@elastic.co>
2024-03-19 09:20:37 -0600

    don't insert license header

--- .buildkite/scripts/steps/esql_grammar_sync.sh
+++ .buildkite/scripts/steps/esql_grammar_sync.sh
@@ -22,8 +22,8 @@ destination_file="./packages/kbn-monaco/src/esql/antlr/esql_lexer.g4"
 cp "$source_file" "$destination_file" || exit
 
 # Insert the license header
-license_header=$(cat "$license_header_file")
-echo -e "${license_header}\n\n$(cat ${destination_file})" > ${destination_file}
+# license_header=$(cat "$license_header_file")
+# echo -e "${license_header}\n\n$(cat ${destination_file})" > ${destination_file}
 
 # Replace the line containing "lexer grammar" with "lexer grammar esql_lexer;"
 sed -i -e 's/lexer grammar.*$/lexer grammar esql_lexer;/' "$destination_file" || exit
