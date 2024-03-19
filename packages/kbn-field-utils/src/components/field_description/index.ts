fc2c389cce74b936d733292923ff3cdaad497e38
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-03-19 17:30:59 +0100

    [DataViewField] Allow to add a custom description for data view fields (#168577)
    - Closes https://github.com/elastic/kibana/issues/89726
    
    ## Summary
    
    This PR extends Data View Field flyout with a new textarea to enter and
    save a custom field description. This description will be shown in a
    field popover for Discover sidebar, in Doc Viewer and also on Data View
    management page. Current limit for the custom description is 300.
    
    When creating/editing a field:
    <img width="600" alt="Screenshot 2024-03-07 at 18 59 24"
    src="https://github.com/elastic/kibana/assets/1415710/433e66d1-9366-4906-8aea-33b77ae81c16">
    
    In the field popover(truncated):
    <img width="500" alt="Screenshot 2024-03-07 at 18 56 52"
    src="https://github.com/elastic/kibana/assets/1415710/8753a11d-6b27-40c1-adaa-de35addb50df">
    
    In the field popover(expanded):
    <img width="500" alt="Screenshot 2024-03-07 at 18 57 00"
    src="https://github.com/elastic/kibana/assets/1415710/b593169e-305e-4d4b-853a-4937324e2470">
    
    In Doc Viewer popover(always expanded):
    <img width="500" alt="Screenshot 2024-03-07 at 18 57 21"
    src="https://github.com/elastic/kibana/assets/1415710/106562a2-baad-4952-a9cc-fa779f96c1e1">
    
    On Data View Management page(truncated):
    <img width="500" alt="Screenshot 2024-03-07 at 18 57 42"
    src="https://github.com/elastic/kibana/assets/1415710/031ed482-5c84-484f-ae9e-6b1e7622c17c">
    
    
    
    <details>
    <summary>Initial implementation examples</summary>
    
     
    ![Oct-11-2023
    11-45-45](https://github.com/elastic/kibana/assets/1415710/533ddd34-a1bd-4553-8fc9-7b9d006f0837)
    
    
    <img width="600" alt="Screenshot 2023-10-11 at 11 52 22"
    src="https://github.com/elastic/kibana/assets/1415710/8e40da6f-fcfc-4e36-9314-d3fc34e6ecab">
    
    <img width="600" alt="Screenshot 2023-10-11 at 11 46 44"
    src="https://github.com/elastic/kibana/assets/1415710/d5ee22a7-0314-4742-b75f-6534e1b4024d">
    
    <img width="600" alt="Screenshot 2023-10-11 at 11 46 29"
    src="https://github.com/elastic/kibana/assets/1415710/dcd809df-7942-4165-8b83-4a83267cea00">
    
    <img width="600" alt="Screenshot 2023-10-11 at 11 47 15"
    src="https://github.com/elastic/kibana/assets/1415710/197add4d-b185-4631-a2b9-eaf013aad8ba">
    
    <img width="600" alt="Screenshot 2023-10-11 at 12 05 29"
    src="https://github.com/elastic/kibana/assets/1415710/9b619e20-c3d1-4c20-ac65-8b922ad1da72">
    
    </details>
    
    ### Checklist
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [x] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Matthew Kime <matt@mattki.me>
    Co-authored-by: amyjtechwriter <61687663+amyjtechwriter@users.noreply.github.com>

--- packages/kbn-field-utils/src/components/field_description/index.ts
+++ packages/kbn-field-utils/src/components/field_description/index.ts
@@ -6,14 +6,4 @@
  * Side Public License, v 1.
  */
 
-export { TypeField } from './type_field';
-
-export { CustomLabelField } from './custom_label_field';
-
-export { CustomDescriptionField } from './custom_description_field';
-
-export { PopularityField } from './popularity_field';
-
-export { ScriptField } from './script_field';
-
-export { FormatField } from './format_field';
+export { FieldDescription, type FieldDescriptionProps } from './field_description';
--- packages/kbn-field-utils/src/components/field_description_icon_button/index.ts
+++ packages/kbn-field-utils/src/components/field_description_icon_button/index.ts
@@ -6,14 +6,7 @@
  * Side Public License, v 1.
  */
 
-export { TypeField } from './type_field';
-
-export { CustomLabelField } from './custom_label_field';
-
-export { CustomDescriptionField } from './custom_description_field';
-
-export { PopularityField } from './popularity_field';
-
-export { ScriptField } from './script_field';
-
-export { FormatField } from './format_field';
+export {
+  FieldDescriptionIconButton,
+  type FieldDescriptionIconButtonProps,
+} from './field_description_icon_button';
