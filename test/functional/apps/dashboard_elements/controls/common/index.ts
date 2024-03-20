df0ae58ec26cd162f9ba7a60fa1e13441d25f53b
Hannah Mudge <hannah.wright@elastic.co>
2024-03-20 10:27:18 -0600

    Try FTR

--- test/functional/apps/dashboard_elements/controls/common/index.ts
+++ test/functional/apps/dashboard_elements/controls/common/index.ts
@@ -40,11 +40,11 @@ export default function ({ loadTestFile, getService, getPageObjects }: FtrProvid
   describe('Controls', function () {
     before(setup);
     after(teardown);
-    loadTestFile(require.resolve('./control_group_settings'));
+    // loadTestFile(require.resolve('./control_group_settings'));
     loadTestFile(require.resolve('./range_slider'));
     loadTestFile(require.resolve('./time_slider'));
     loadTestFile(require.resolve('./control_group_chaining'));
     loadTestFile(require.resolve('./control_group_apply_button'));
-    loadTestFile(require.resolve('./replace_controls'));
+    // loadTestFile(require.resolve('./replace_controls'));
   });
 }
