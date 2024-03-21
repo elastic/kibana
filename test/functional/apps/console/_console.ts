56324aafe441b73d5cec6da805b72b1ca9be6e28
Rodney Norris <rodney.norris@elastic.co>
2024-03-21 01:00:26 +0000

    test(console): update ftrs with new default console snippet

--- test/functional/apps/console/_console.ts
+++ test/functional/apps/console/_console.ts
@@ -11,15 +11,28 @@ import { asyncForEach } from '@kbn/std';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 const DEFAULT_REQUEST = `
+# Welcome to the Dev Tools Console!
+#
+# You can use Console to explore the Elasticsearch API. See the Elasticsearch API reference to learn more: https://www.elastic.co/guide/en/elasticsearch/reference/current/rest-apis.html
+#
+# Here are a few examples to get you started.
 
-# Click the Variables button, above, to create your own variables.
-GET \${exampleVariable1} // _search
+
+# Create an index
+PUT /my-index
+
+
+# Add a document to my-index
+POST /my-index/_doc
 {
-  "query": {
-    "\${exampleVariable2}": {} // match_all
-  }
+    "id": "park_rocky-mountain",
+    "title": "Rocky Mountain",
+    "description": "Bisected north to south by the Continental Divide, this portion of the Rockies has ecosystems varying from over 150 riparian lakes to montane and subalpine forests to treeless alpine tundra."
 }
 
+
+# Perform a search in my-index
+GET /my-index/_search?q="rocky mountain"
 `.trim();
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
