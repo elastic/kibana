89b579708722220d4c08d7d62434533759abe063
Caue Marcondes <caue.marcondes@elastic.co>
2024-03-22 14:03:02 +0000

    [UX] Fixing APM data view id

--- packages/kbn-apm-data-view/index.ts
+++ packages/kbn-apm-data-view/index.ts
@@ -1,12 +1,10 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
-
-export const DO_NOT_USE_LEGACY_APM_STATIC_DATA_VIEW_ID =
-  'apm_static_index_pattern_id';
 
 const APM_STATIC_DATA_VIEW_ID_PREFIX = 'apm_static_data_view_id';
 
