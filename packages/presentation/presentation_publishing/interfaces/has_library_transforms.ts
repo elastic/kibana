61d8dbe46ac2aa143de9e3c25f6ff8f12b9bdb2f
Nathan Reese <reese.nathan@gmail.com>
2024-03-22 14:56:35 -0600

    eslint

--- packages/presentation/presentation_publishing/interfaces/has_library_transforms.ts
+++ packages/presentation/presentation_publishing/interfaces/has_library_transforms.ts
@@ -11,35 +11,47 @@ import type { OnSaveProps } from '@kbn/saved-objects-plugin/public';
 export interface HasLibraryTransforms<StateT extends object = object> {
   canLinkToLibrary: () => Promise<boolean>;
   canUnlinkFromLibrary: () => Promise<boolean>;
-  saveStateToSavedObject: (title: string) => Promise<{ state: StateT; savedObjectId: string; }>;
+  saveStateToSavedObject: (title: string) => Promise<{ state: StateT; savedObjectId: string }>;
   savedObjectAttributesToState: () => StateT;
   checkForDuplicateTitle: (props: OnSaveProps) => Promise<void>;
 }
 
-export const apiHasLibraryTransforms = <StateT extends object = object>(unknownApi: null | unknown): unknownApi is HasLibraryTransforms<StateT> => {
-  return Boolean(unknownApi &&
-    typeof (unknownApi as HasLibraryTransforms<StateT>).canLinkToLibrary === 'function' &&
-    typeof (unknownApi as HasLibraryTransforms<StateT>).canUnlinkFromLibrary === 'function' &&
-    typeof (unknownApi as HasLibraryTransforms<StateT>).saveStateToSavedObject === 'function' &&
-    typeof (unknownApi as HasLibraryTransforms<StateT>).savedObjectAttributesToState === 'function' &&
-    typeof (unknownApi as HasLibraryTransforms<StateT>).checkForDuplicateTitle === 'function');
+export const apiHasLibraryTransforms = <StateT extends object = object>(
+  unknownApi: null | unknown
+): unknownApi is HasLibraryTransforms<StateT> => {
+  return Boolean(
+    unknownApi &&
+      typeof (unknownApi as HasLibraryTransforms<StateT>).canLinkToLibrary === 'function' &&
+      typeof (unknownApi as HasLibraryTransforms<StateT>).canUnlinkFromLibrary === 'function' &&
+      typeof (unknownApi as HasLibraryTransforms<StateT>).saveStateToSavedObject === 'function' &&
+      typeof (unknownApi as HasLibraryTransforms<StateT>).savedObjectAttributesToState ===
+        'function' &&
+      typeof (unknownApi as HasLibraryTransforms<StateT>).checkForDuplicateTitle === 'function'
+  );
 };
 
 /**
  * @deprecated use HasLibraryTransforms instead
  */
-export type HasLegacyLibraryTransforms = Pick<HasLibraryTransforms, 'canLinkToLibrary' | 'canUnlinkFromLibrary'> & {
+export type HasLegacyLibraryTransforms = Pick<
+  HasLibraryTransforms,
+  'canLinkToLibrary' | 'canUnlinkFromLibrary'
+> & {
   linkToLibrary: () => Promise<void>;
   unlinkFromLibrary: () => Promise<void>;
-}
+};
 
 /**
  * @deprecated use apiHasLibraryTransforms instead
  */
-export const apiHasLegacyLibraryTransforms = (unknownApi: null | unknown): unknownApi is HasLegacyLibraryTransforms => {
-  return Boolean(unknownApi &&
-    typeof (unknownApi as HasLegacyLibraryTransforms).canLinkToLibrary === 'function' &&
-    typeof (unknownApi as HasLegacyLibraryTransforms).canUnlinkFromLibrary === 'function' &&
-    typeof (unknownApi as HasLegacyLibraryTransforms).linkToLibrary === 'function' &&
-    typeof (unknownApi as HasLegacyLibraryTransforms).unlinkFromLibrary === 'function');
+export const apiHasLegacyLibraryTransforms = (
+  unknownApi: null | unknown
+): unknownApi is HasLegacyLibraryTransforms => {
+  return Boolean(
+    unknownApi &&
+      typeof (unknownApi as HasLegacyLibraryTransforms).canLinkToLibrary === 'function' &&
+      typeof (unknownApi as HasLegacyLibraryTransforms).canUnlinkFromLibrary === 'function' &&
+      typeof (unknownApi as HasLegacyLibraryTransforms).linkToLibrary === 'function' &&
+      typeof (unknownApi as HasLegacyLibraryTransforms).unlinkFromLibrary === 'function'
+  );
 };
