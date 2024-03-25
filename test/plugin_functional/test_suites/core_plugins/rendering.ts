13a024db15dc710589874b4031f3a7644e8234c0
Dario Gieselaar <dario.gieselaar@elastic.co>
2024-03-25 14:27:02 +0100

    Update telemetry/tests

--- test/plugin_functional/test_suites/core_plugins/rendering.ts
+++ test/plugin_functional/test_suites/core_plugins/rendering.ts
@@ -340,6 +340,7 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
         'xpack.observability_onboarding.ui.enabled (boolean)',
         'xpack.observabilityLogsExplorer.navigation.showAppLink (any)', // conditional, is actually a boolean
         'share.new_version.enabled (boolean)',
+        'aiAssistantManagementSelection.preferredAIAssistantType (alternatives)',
       ];
       // We don't assert that actualExposedConfigKeys and expectedExposedConfigKeys are equal, because test failure messages with large
       // arrays are hard to grok. Instead, we take the difference between the two arrays and assert them separately, that way it's
