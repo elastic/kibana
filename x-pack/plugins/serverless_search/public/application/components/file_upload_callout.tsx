c3fb42e9eb62e9d392ee65990b9d13e6af6506cf
Samiul Monir <samiul.monir@elastic.co>
2024-03-25 15:14:56 -0400

    Fix linting issues

--- x-pack/plugins/serverless_search/public/application/components/file_upload_callout.tsx
+++ x-pack/plugins/serverless_search/public/application/components/file_upload_callout.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 import React from 'react';
-import { EuiButton, EuiCallOut, EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
+import { EuiButton, EuiCallOut, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 
