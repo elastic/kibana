fb68fc8a722c2288f17c229428c154f0298225a0
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-03-26 09:12:20 +0100

    jsdoc

--- x-pack/packages/ml/aiops_utils/log_rate_analysis/get_snapped_timestamps.ts
+++ x-pack/packages/ml/aiops_utils/log_rate_analysis/get_snapped_timestamps.ts
@@ -5,6 +5,14 @@
  * 2.0.
  */
 
+/**
+ * Generates an array of timestamps evenly spaced within a given time range.
+ *
+ * @param timeRangeEarliest The earliest timestamp in the time range.
+ * @param timeRangeLatest The latest timestamp in the time range.
+ * @param interval The interval between timestamps in milliseconds.
+ * @returns Array of timestamps spaced by the specified interval within the given range.
+ */
 export const getSnappedTimestamps = (
   timeRangeEarliest: number,
   timeRangeLatest: number,
