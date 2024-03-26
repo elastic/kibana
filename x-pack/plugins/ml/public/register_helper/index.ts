c2c81d32c6c689514b78efdf2484b1513fdc0b9e
Clint Andrew Hall <clint@clintandrewhall.com>
2024-03-26 16:38:34 -0400

    Fix bundle bloat -- utility being imported from main app module.

--- x-pack/plugins/ml/public/register_helper/index.ts
+++ x-pack/plugins/ml/public/register_helper/index.ts
@@ -6,7 +6,6 @@
  */
 
 export { registerEmbeddables } from '../embeddables';
-export { registerManagementSection } from '../application/management';
 export { registerMlUiActions } from '../ui_actions';
 export { registerSearchLinks } from './register_search_links';
 export { registerMlAlerts } from '../alerting';
