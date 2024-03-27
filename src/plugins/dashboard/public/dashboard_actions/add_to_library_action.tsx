7c7273fc035e0657567a01d4ae6e2d47769d659c
Nathan Reese <reese.nathan@gmail.com>
2024-03-27 07:36:26 -0600

    fallback to empty title when not provided

--- src/plugins/dashboard/public/dashboard_actions/add_to_library_action.tsx
+++ src/plugins/dashboard/public/dashboard_actions/add_to_library_action.tsx
@@ -109,7 +109,7 @@ export class AddToLibraryAction implements Action<EmbeddableApiContext> {
           <SavedObjectSaveModal
             onSave={onSave}
             onClose={() => {}}
-            title={title ?? embeddable.type}
+            title={title ?? ""}
             showCopyOnSave={false}
             objectType={
               typeof embeddable.getTypeDisplayName === 'function'
