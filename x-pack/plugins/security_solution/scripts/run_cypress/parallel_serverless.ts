873cb403a90be1bf5c74b9f767f9aaf3317c2e60
dkirchan <diamantis.kirchantzoglou@elastic.co>
2024-03-27 17:09:38 +0200

    Reintroducing the log line to debug project creation

--- x-pack/plugins/security_solution/scripts/run_cypress/parallel_serverless.ts
+++ x-pack/plugins/security_solution/scripts/run_cypress/parallel_serverless.ts
@@ -140,7 +140,7 @@ async function createSecurityProject(
     };
   } catch (error) {
     if (error instanceof AxiosError) {
-      log.error(`${error.response?.status}:${error.response?.data}`);
+      console.log(`${error.response?.status}:${JSON.stringify(error.response?.data)}`);
     } else {
       log.error(`${error.message}`);
     }
