510aeac5c178ce18593efb45c4b15670338ddf1b
dej611 <dej611@gmail.com>
2024-03-27 12:03:54 +0100

    :white_check_mark: Fix tests with wrong merge

--- packages/kbn-esql-validation-autocomplete/src/validation/validation.test.ts
+++ packages/kbn-esql-validation-autocomplete/src/validation/validation.test.ts
@@ -339,14 +339,14 @@ describe('validation logic', () => {
       ['eval', 'stats', 'rename', 'limit', 'keep', 'drop', 'mv_expand', 'dissect', 'grok'].map(
         (command) =>
           testErrorsAndWarnings(command, [
-            `SyntaxError: mismatched input '${command}' expecting {'explain', 'from', 'row', 'show'}`,
+            `SyntaxError: mismatched input '${command}' expecting {'explain', 'from', 'meta', 'row', 'show'}`,
           ])
       );
     });
 
     describe('from', () => {
       testErrorsAndWarnings('f', [
-        "SyntaxError: mismatched input 'f' expecting {'explain', 'from', 'row', 'show'}",
+        `SyntaxError: mismatched input 'f' expecting {'explain', 'from', 'meta', 'row', 'show'}`,
       ]);
       testErrorsAndWarnings(`from `, [
         "SyntaxError: missing {QUOTED_IDENTIFIER, FROM_UNQUOTED_IDENTIFIER} at '<EOF>'",
@@ -660,20 +660,41 @@ describe('validation logic', () => {
       });
     });
 
-    describe('show', () => {
-      testErrorsAndWarnings('show', ["SyntaxError: no viable alternative at input 'show'"]);
-      testErrorsAndWarnings('show functions', []);
-      testErrorsAndWarnings('show info', []);
-      testErrorsAndWarnings('show functions()', [
+    describe('meta', () => {
+      testErrorsAndWarnings('meta', ["SyntaxError: missing 'functions' at '<EOF>'"]);
+      testErrorsAndWarnings('meta functions', []);
+      testErrorsAndWarnings('meta functions()', [
         "SyntaxError: token recognition error at: '('",
         "SyntaxError: token recognition error at: ')'",
       ]);
-      testErrorsAndWarnings('show functions blah', [
+      testErrorsAndWarnings('meta functions blah', [
         "SyntaxError: token recognition error at: 'b'",
         "SyntaxError: token recognition error at: 'l'",
         "SyntaxError: token recognition error at: 'a'",
         "SyntaxError: token recognition error at: 'h'",
       ]);
+      testErrorsAndWarnings('meta info', [
+        "SyntaxError: token recognition error at: 'i'",
+        "SyntaxError: token recognition error at: 'n'",
+        "SyntaxError: token recognition error at: 'fo'",
+        "SyntaxError: missing 'functions' at '<EOF>'",
+      ]);
+    });
+
+    describe('show', () => {
+      testErrorsAndWarnings('show', ["SyntaxError: missing 'info' at '<EOF>'"]);
+      testErrorsAndWarnings('show functions', [
+        "SyntaxError: token recognition error at: 'f'",
+        "SyntaxError: token recognition error at: 'u'",
+        "SyntaxError: token recognition error at: 'n'",
+        "SyntaxError: token recognition error at: 'c'",
+        "SyntaxError: token recognition error at: 't'",
+        "SyntaxError: token recognition error at: 'io'",
+        "SyntaxError: token recognition error at: 'n'",
+        "SyntaxError: token recognition error at: 's'",
+        "SyntaxError: missing 'info' at '<EOF>'",
+      ]);
+      testErrorsAndWarnings('show info', []);
       testErrorsAndWarnings('show numberField', [
         "SyntaxError: token recognition error at: 'n'",
         "SyntaxError: token recognition error at: 'u'",
@@ -681,11 +702,11 @@ describe('validation logic', () => {
         "SyntaxError: token recognition error at: 'b'",
         "SyntaxError: token recognition error at: 'e'",
         "SyntaxError: token recognition error at: 'r'",
-        "SyntaxError: token recognition error at: 'Fi'",
-        "SyntaxError: token recognition error at: 'e'",
+        "SyntaxError: token recognition error at: 'F'",
+        "SyntaxError: token recognition error at: 'ie'",
         "SyntaxError: token recognition error at: 'l'",
         "SyntaxError: token recognition error at: 'd'",
-        "SyntaxError: no viable alternative at input 'show '",
+        "SyntaxError: missing 'info' at '<EOF>'",
       ]);
     });
 
@@ -2380,10 +2401,10 @@ describe('validation logic', () => {
         });
       });
 
-      it('should call fields callbacks also for show command', async () => {
+      it('should call fields callbacks also for meta command', async () => {
         const callbackMocks = getCallbackMocks();
         await validateQuery(
-          `show functions | keep name`,
+          `meta functions | keep name`,
           getAstAndSyntaxErrors,
           undefined,
           callbackMocks
@@ -2392,7 +2413,23 @@ describe('validation logic', () => {
         expect(callbackMocks.getPolicies).not.toHaveBeenCalled();
         expect(callbackMocks.getFieldsFor).toHaveBeenCalledTimes(1);
         expect(callbackMocks.getFieldsFor).toHaveBeenLastCalledWith({
-          query: 'show functions',
+          query: 'meta functions',
+        });
+      });
+
+      it('should call fields callbacks also for show command', async () => {
+        const callbackMocks = getCallbackMocks();
+        await validateQuery(
+          `show info | keep name`,
+          getAstAndSyntaxErrors,
+          undefined,
+          callbackMocks
+        );
+        expect(callbackMocks.getSources).not.toHaveBeenCalled();
+        expect(callbackMocks.getPolicies).not.toHaveBeenCalled();
+        expect(callbackMocks.getFieldsFor).toHaveBeenCalledTimes(1);
+        expect(callbackMocks.getFieldsFor).toHaveBeenLastCalledWith({
+          query: 'show info',
         });
       });
 
