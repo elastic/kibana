cba522717bc031fcff4314ba5448cbcdb58a9251
shahzad31 <shahzad31comp@gmail.com>
2024-03-27 15:05:30 +0500

    fix i18n

--- x-pack/plugins/observability_solution/slo/public/pages/slo_edit/components/common/runtime_field_used.tsx
+++ x-pack/plugins/observability_solution/slo/public/pages/slo_edit/components/common/runtime_field_used.tsx
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 import React from 'react';
 
@@ -24,10 +25,16 @@ export function RunTimeFieldUsed({ dataView }: { dataView?: DataView }) {
   }
 
   return (
-    <EuiCallOut title="Runtime fields being used" color="warning" iconType="warning">
+    <EuiCallOut
+      title={i18n.translate('xpack.slo.runTimeFieldUsed.euiCallOut.runtimeFieldsBeingUsedLabel', {
+        defaultMessage: 'Runtime fields being used',
+      })}
+      color="warning"
+      iconType="warning"
+    >
       <p>
         <FormattedMessage
-          id="xpack.observability.runTimeFieldUsed.p.theRuntimeFieldLabel"
+          id="xpack.slo.runTimeFieldUsed.p.theRuntimeFieldLabel"
           defaultMessage="The runtime field(s) {fields} from kibana dataview are being used in the query. If you update the runtime field, the query will not be updated automatically. You must save the slo definition again to update the underlying transform query."
           values={{
             fields: <strong>{fieldNames.join(', ')}</strong>,
