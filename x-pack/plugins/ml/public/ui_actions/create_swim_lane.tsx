ee1b3ce27a6a1066078222d404845290d503557b
Dima Arnautov <dmitrii.arnautov@elastic.co>
2024-03-28 16:31:26 +0100

    update create action

--- x-pack/plugins/ml/public/ui_actions/create_swim_lane.tsx
+++ x-pack/plugins/ml/public/ui_actions/create_swim_lane.tsx
@@ -10,6 +10,7 @@ import type { PresentationContainer } from '@kbn/presentation-containers';
 import type { EmbeddableApiContext } from '@kbn/presentation-publishing';
 import type { UiActionsActionDefinition } from '@kbn/ui-actions-plugin/public';
 import { IncompatibleActionError } from '@kbn/ui-actions-plugin/public';
+import { ML_APP_NAME, PLUGIN_ICON, PLUGIN_ID } from '../../common/constants/app';
 import { ANOMALY_SWIMLANE_EMBEDDABLE_TYPE } from '../embeddables';
 import type { AnomalySwimLaneEmbeddableApi } from '../embeddables/anomaly_swimlane/types';
 import type { MlCoreSetup } from '../plugin';
@@ -33,12 +34,20 @@ export function createAddSwimlanePanelAction(
 ): UiActionsActionDefinition<CreateSwimlanePanelActionContext> {
   return {
     id: 'create-anomaly-swimlane',
-    getIconType(): string {
-      return 'mlApp';
-    },
+    grouping: [
+      {
+        id: PLUGIN_ID,
+        getDisplayName: () => ML_APP_NAME,
+        getIconType: () => PLUGIN_ICON,
+      },
+    ],
     getDisplayName: () =>
-      i18n.translate('xpack.ml.actions.createAnomalySwimLaneTitle', {
+      i18n.translate('xpack.ml.components.jobAnomalyScoreEmbeddable.displayName', {
         defaultMessage: 'Anomaly swim lane',
+      }),
+    getDisplayNameTooltip: () =>
+      i18n.translate('xpack.ml.components.jobAnomalyScoreEmbeddable.description', {
+        defaultMessage: 'View anomaly detection results in a timeline.',
       }),
     async isCompatible(context: EmbeddableApiContext) {
       return Boolean(await parentApiIsCompatible(context.embeddable));
