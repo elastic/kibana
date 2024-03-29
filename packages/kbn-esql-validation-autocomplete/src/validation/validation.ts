08cfdc5827dbf6402b6a5180258dc9e622f7d272
Drew Tate <andrew.tate@elastic.co>
2024-03-29 13:46:51 -0600

    refine logic

--- packages/kbn-esql-validation-autocomplete/src/validation/validation.ts
+++ packages/kbn-esql-validation-autocomplete/src/validation/validation.ts
@@ -432,7 +432,7 @@ function validateFunction(
       }
 
       // check every element of the argument (may be an array of elements, or may be a single element)
-      const hasMultipleElements = Array.isArray(outerArg) && isArrayType(argDef.type);
+      const hasMultipleElements = Array.isArray(outerArg);
       const argElements = hasMultipleElements ? outerArg : [outerArg];
       const singularType = extractSingularType(argDef.type);
       let messagesFromAllArgElements = argElements
@@ -453,7 +453,7 @@ function validateFunction(
         })
         .flat();
 
-      if (hasMultipleElements) {
+      if (isArrayType(argDef.type) && hasMultipleElements) {
         messagesFromAllArgElements = collapseWrongArgumentTypeMessages(
           messagesFromAllArgElements,
           outerArg,
