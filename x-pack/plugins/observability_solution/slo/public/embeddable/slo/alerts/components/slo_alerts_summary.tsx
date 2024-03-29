2b95c50b1f64322cc827f7694e341fc015dc0647
nickofthyme <nicholas.partridge@elastic.co>
2024-03-29 14:05:27 -0700

    fix type error

--- x-pack/plugins/observability_solution/slo/public/embeddable/slo/alerts/components/slo_alerts_summary.tsx
+++ x-pack/plugins/observability_solution/slo/public/embeddable/slo/alerts/components/slo_alerts_summary.tsx
@@ -34,7 +34,6 @@ export function SloAlertsSummary({
   showAllGroupByInstances,
 }: Props) {
   const {
-    charts,
     triggersActionsUi: { getAlertSummaryWidget: AlertSummaryWidget },
   } = deps;
 
