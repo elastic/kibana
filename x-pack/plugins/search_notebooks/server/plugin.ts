0e24a91569dcdecb843235bad77569b26a42aecb
Rodney Norris <rodney.norris@elastic.co>
2024-04-01 14:48:46 +0000

    initial setup for search notebooks plugin

---
+++ x-pack/plugins/search_notebooks/server/plugin.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type {
+  PluginInitializerContext,
+  CoreSetup,
+  CoreStart,
+  Plugin,
+  Logger,
+} from '@kbn/core/server';
+
+import { SearchNotebooksPluginSetup, SearchNotebooksPluginStart } from './types';
+import { defineRoutes } from './routes';
+import { SearchNotebooksConfig } from './config';
+
+export class SearchNotebooksPlugin
+  implements Plugin<SearchNotebooksPluginSetup, SearchNotebooksPluginStart>
+{
+  private readonly config: SearchNotebooksConfig;
+  private readonly logger: Logger;
+
+  constructor(initializerContext: PluginInitializerContext) {
+    this.config = initializerContext.config.get<SearchNotebooksConfig>();
+    this.logger = initializerContext.logger.get();
+  }
+
+  public setup(core: CoreSetup) {
+    if (!this.config.enabled) return {};
+
+    this.logger.debug('searchNotebooks: Setup');
+    const router = core.http.createRouter();
+
+    // Register server side APIs
+    defineRoutes(router);
+
+    return {};
+  }
+
+  public start(core: CoreStart) {
+    return {};
+  }
+
+  public stop() {}
+}
