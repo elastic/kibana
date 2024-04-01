e364feefec794b4be0aa58d5e5b9fce8891299ee
Paul Tavares <56442535+paul-tavares@users.noreply.github.com>
2024-04-01 15:17:23 -0400

    [Security Solution][Endpoint] Re-enable skipped tests (#179743)
    ## Summary
    
    Re-enables test skipped between March 29 and April 1 due to a problem
    (not fixed) with the Fleet Server docker image, which caused tests to
    fail with:
    
    ```
     info [cy.dfw] 0 attempts of creating endpoint host failed, reason for the last failure was Error: Command failed with exit code 1: multipass exec test-host-3626 -- sudo ./elastic-agent-8.14.0-SNAPSHOT-linux-arm64/elastic-agent install --insecure --force --url https://192.168.64.1:8220 --enrollment-token 
    ....
          {"log.level":"error","@timestamp":"2024-04-01T08:46:59.930-0400","log.origin":{"file.name":"remote/client.go","file.line":243},"message":"fleet requested a different api version \"2023-06-01\" but this is currently not implemented","http.request.id":"01HTCV5Q86GKT1KW682V7FH3MQ","ecs.version":"1.6.0"}
    ```
    
    Issues (skipped tests) fixed with this PR:
    
    - fixes #168340
    - fixes #168284 
    - fixes #169958 
    - fixes #170373 
    - fixes #170424 
    - fixes #170563 
    - fixes #172326 
    - fixes #179744

--- x-pack/plugins/security_solution/public/management/cypress/e2e/response_actions/response_console/process_operations.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/response_actions/response_console/process_operations.cy.ts
@@ -24,8 +24,7 @@ import { enableAllPolicyProtections } from '../../../tasks/endpoint_policy';
 import { createEndpointHost } from '../../../tasks/create_endpoint_host';
 import { deleteAllLoadedEndpointData } from '../../../tasks/delete_all_endpoint_data';
 
-// FLAKY: https://github.com/elastic/kibana/issues/170563
-describe.skip('Response console', { tags: ['@ess', '@serverless'] }, () => {
+describe('Response console', { tags: ['@ess', '@serverless'] }, () => {
   beforeEach(() => {
     login();
   });
