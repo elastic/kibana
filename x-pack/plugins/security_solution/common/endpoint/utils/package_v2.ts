4a8931630d04098f5ce96d1883a3b51579c2bcf1
pzl <danpan@elastic.co>
2024-04-01 13:33:44 -0400

    enable pkg v2 check, fix cypress loader

--- x-pack/plugins/security_solution/common/endpoint/utils/package_v2.ts
+++ x-pack/plugins/security_solution/common/endpoint/utils/package_v2.ts
@@ -5,17 +5,15 @@
  * 2.0.
  */
 
-// import semverLte from 'semver/functions/lte';
+import semverLte from 'semver/functions/lte';
 
-// function parseSemver(semver: string) {
-//   return semver.includes('-') ? semver.substring(0, semver.indexOf('-')) : semver;
-// }
+function parseSemver(semver: string) {
+  return semver.includes('-') ? semver.substring(0, semver.indexOf('-')) : semver;
+}
 
-// until a release is confirmed, or another feature-detection method is used, do not automatically
 // switch to "v2" logic
-// const MIN_ENDPOINT_PACKAGE_V2_VERSION = '8.13.0';
+const MIN_ENDPOINT_PACKAGE_V2_VERSION = '8.14.0';
 export function isEndpointPackageV2(version: string) {
-  // const parsedVersion = parseSemver(version);
-  // return semverLte(MIN_ENDPOINT_PACKAGE_V2_VERSION, parsedVersion);
-  return false;
+  const parsedVersion = parseSemver(version);
+  return semverLte(MIN_ENDPOINT_PACKAGE_V2_VERSION, parsedVersion);
 }
