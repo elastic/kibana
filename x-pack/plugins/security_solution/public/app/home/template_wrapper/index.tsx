8765bb1ad09ff2c00b0cc9fa2fa47acd48e2cde7
Luke G <11671118+lgestc@users.noreply.github.com>
2024-04-02 11:00:03 +0200

    fix threat intelligence loading screen (#178701)
    ## Summary
    
    This PR fixes custom empty page layout in Threat Intelligence page, and
    also optimizes some of the loading that happens during the init phase
    (integrations retrieval).
    
    **Before**
    
    ![image](https://github.com/elastic/kibana/assets/11671118/11fa9ce2-f075-4940-a9ae-75f327d4b7cb)
    
    Broken api calls made:
    
    ![image](https://github.com/elastic/kibana/assets/11671118/4bc4b1a2-6424-42d4-9390-e51a36f943b7)
    
    **After**
    
    ![image](https://github.com/elastic/kibana/assets/11671118/d67b4ea8-e862-41d6-830e-f0b38af297c7)
    
    ### Checklist
    
    (cherry picked from commit 4461b86a7fcb844cfbd7f158266c29f38b0f0d62)
    
    # Conflicts:
    #	x-pack/plugins/security_solution/public/app/home/template_wrapper/index.tsx

--- x-pack/plugins/security_solution/public/app/home/template_wrapper/index.tsx
+++ x-pack/plugins/security_solution/public/app/home/template_wrapper/index.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { useMemo } from 'react';
+import React, { type ReactNode, useMemo } from 'react';
 import styled from 'styled-components';
 import { EuiThemeProvider, useEuiTheme, type EuiThemeComputed } from '@elastic/eui';
 import { IS_DRAGGING_CLASS_NAME } from '@kbn/securitysolution-t-grid';
@@ -45,7 +45,14 @@ const StyledKibanaPageTemplate = styled(KibanaPageTemplate)<
   }
 `;
 
-export const SecuritySolutionTemplateWrapper: React.FC<Omit<KibanaPageTemplateProps, 'ref'>> =
+export type SecuritySolutionTemplateWrapperProps = Omit<KibanaPageTemplateProps, 'ref'> & {
+  /**
+   * Combined with isEmptyState, this prop allows complete override of the empty page
+   */
+  emptyPageBody?: ReactNode;
+};
+
+export const SecuritySolutionTemplateWrapper: React.FC<SecuritySolutionTemplateWrapperProps> =
   React.memo(({ children, ...rest }) => {
     const solutionNavProps = useSecuritySolutionNavigation();
     const [isTimelineBottomBarVisible] = useShowTimeline();
@@ -54,6 +61,7 @@ export const SecuritySolutionTemplateWrapper: React.FC<Omit<KibanaPageTemplatePr
       getTimelineShowStatus(state, TimelineId.active)
     );
     const [routeProps] = useRouteSpy();
+    const isNotEmpty = !rest.isEmptyState;
     const isPreview = routeProps?.pageName === SecurityPageName.rulesCreate;
 
     // The bottomBar by default has a set 'dark' colorMode that doesn't match the global colorMode from the Advanced Settings
@@ -76,25 +84,29 @@ export const SecuritySolutionTemplateWrapper: React.FC<Omit<KibanaPageTemplatePr
           restrictWidth={false}
           {...rest}
         >
-          <GlobalKQLHeader />
-          <KibanaPageTemplate.Section
-            className="securityPageWrapper"
-            data-test-subj="pageContainer"
-            paddingSize={rest.paddingSize ?? 'l'}
-            alignment="top"
-            component="div"
-            grow={true}
-          >
-            {children}
-          </KibanaPageTemplate.Section>
-          {isTimelineBottomBarVisible && (
-            <KibanaPageTemplate.BottomBar data-test-subj="timeline-bottom-bar-container">
-              <EuiThemeProvider colorMode={globalColorMode}>
-                <Timeline />
-              </EuiThemeProvider>
-            </KibanaPageTemplate.BottomBar>
+          {isNotEmpty && (
+            <>
+              <GlobalKQLHeader />
+              <KibanaPageTemplate.Section
+                className="securityPageWrapper"
+                data-test-subj="pageContainer"
+                paddingSize={rest.paddingSize ?? 'l'}
+                alignment="top"
+                component="div"
+                grow={true}
+              >
+                {children}
+              </KibanaPageTemplate.Section>
+              {isTimelineBottomBarVisible && (
+                <KibanaPageTemplate.BottomBar data-test-subj="timeline-bottom-bar-container">
+                  <EuiThemeProvider colorMode={globalColorMode}>
+                    <Timeline />
+                  </EuiThemeProvider>
+                </KibanaPageTemplate.BottomBar>
+              )}
+              <SecuritySolutionFlyout />
+            </>
           )}
-          <SecuritySolutionFlyout />
         </StyledKibanaPageTemplate>
       </ExpandableFlyoutProvider>
     );
