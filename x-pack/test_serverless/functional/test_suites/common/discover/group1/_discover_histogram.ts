bc5606e02e310607d5bd2ef490538df3c724c876
Davis McPhee <davis.mcphee@elastic.co>
2024-04-01 22:23:29 -0300

    Attempt to fix failing functional tests

--- x-pack/test_serverless/functional/test_suites/common/discover/group1/_discover_histogram.ts
+++ x-pack/test_serverless/functional/test_suites/common/discover/group1/_discover_histogram.ts
@@ -85,7 +85,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
       await PageObjects.discover.brushHistogram();
       await PageObjects.discover.waitUntilSearchingHasFinished();
-      renderingCountInc = 3; // Multiple renders caused by https://github.com/elastic/kibana/issues/177055
+      renderingCountInc = 4; // Multiple renders caused by https://github.com/elastic/kibana/issues/177055
       await retry.waitFor('chart rendering complete after being brushed', async () => {
         const actualCount = await elasticChart.getVisualizationRenderingCount();
         const expectedCount = prevRenderingCount + renderingCountInc * 2;
