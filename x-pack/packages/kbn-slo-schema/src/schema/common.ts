ebba92796c87e8a49c836d988b5ba756d3723da1
Kevin Delemme <kevin.delemme@elastic.co>
2024-04-02 09:34:44 -0400

    chore: move kql query schema into indicator

--- x-pack/packages/kbn-slo-schema/src/schema/common.ts
+++ x-pack/packages/kbn-slo-schema/src/schema/common.ts
@@ -78,35 +78,6 @@ const groupSummarySchema = t.type({
 
 const dateRangeSchema = t.type({ from: dateType, to: dateType });
 
-const kqlQuerySchema = t.string;
-
-const kqlWithFiltersSchema = t.type({
-  kqlQuery: t.string,
-  filters: t.array(
-    t.type({
-      meta: t.partial({
-        alias: t.union([t.string, t.null]),
-        disabled: t.boolean,
-        negate: t.boolean,
-        // controlledBy is there to identify who owns the filter
-        controlledBy: t.string,
-        // allows grouping of filters
-        group: t.string,
-        // index and type are optional only because when you create a new filter, there are no defaults
-        index: t.string,
-        isMultiIndex: t.boolean,
-        type: t.string,
-        key: t.string,
-        params: t.any,
-        value: t.string,
-      }),
-      query: t.record(t.string, t.any),
-    })
-  ),
-});
-
-const querySchema = t.union([kqlQuerySchema, kqlWithFiltersSchema]);
-
 export {
   ALL_VALUE,
   allOrAnyString,
@@ -119,7 +90,4 @@ export {
   summarySchema,
   metaSchema,
   groupSummarySchema,
-  kqlWithFiltersSchema,
-  querySchema,
-  kqlQuerySchema,
 };
