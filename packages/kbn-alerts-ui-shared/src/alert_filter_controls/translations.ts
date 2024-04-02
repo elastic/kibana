2f95ce1bae7d4fb75dff34603d01a0bc6e4d1892
Umberto Pepato <umberto.pepato@elastic.co>
2024-04-02 13:51:52 +0200

    Fix alerts-ui-shared translations

--- packages/kbn-alerts-ui-shared/src/alert_filter_controls/translations.ts
+++ packages/kbn-alerts-ui-shared/src/alert_filter_controls/translations.ts
@@ -9,56 +9,47 @@
 import { i18n } from '@kbn/i18n';
 
 export const PENDING_CHANGES_REMINDER = i18n.translate(
-  'xpack.securitySolution.filtersGroup.pendingChanges',
+  'alertsUIShared.filtersGroup.pendingChanges',
   {
     defaultMessage: 'Save pending changes',
   }
 );
 
 export const EDIT_CONTROLS = i18n.translate(
-  'xpack.securitySolution.filtersGroup.contextMenu.editControls',
+  'alertsUIShared.filtersGroup.contextMenu.editControls',
   {
     defaultMessage: 'Edit Controls',
   }
 );
 
-export const ADD_CONTROLS = i18n.translate(
-  'xpack.securitySolution.filtersGroup.contextMenu.addControls',
-  {
-    defaultMessage: 'Add Controls',
-  }
-);
+export const ADD_CONTROLS = i18n.translate('alertsUIShared.filtersGroup.contextMenu.addControls', {
+  defaultMessage: 'Add Controls',
+});
 
 export const SAVE_CONTROLS = i18n.translate(
-  'xpack.securitySolution.filtersGroup.contextMenu.saveControls',
+  'alertsUIShared.filtersGroup.contextMenu.saveControls',
   {
     defaultMessage: 'Save Controls',
   }
 );
 
-export const DISCARD_CHANGES = i18n.translate(
-  'xpack.securitySolution.filtersGroup.discardChanges',
-  {
-    defaultMessage: 'Discard Changes',
-  }
-);
+export const DISCARD_CHANGES = i18n.translate('alertsUIShared.filtersGroup.discardChanges', {
+  defaultMessage: 'Discard Changes',
+});
 
-export const FILTER_GROUP_MENU = i18n.translate(
-  'xpack.securitySolution.filterGroup.groupMenuTitle',
-  {
-    defaultMessage: 'Filter group menu',
-  }
-);
+export const FILTER_GROUP_MENU = i18n.translate('alertsUIShared.filterGroup.groupMenuTitle', {
+  defaultMessage: 'Filter group menu',
+});
 
 export const FILTER_GROUP_BANNER_TITLE = i18n.translate(
-  'xpack.securitySolution.filterGroup.filtersChangedBanner',
+  'alertsUIShared.filterGroup.filtersChangedBanner',
   {
     defaultMessage: 'Filter Controls have changed',
   }
 );
 
 export const FILTER_GROUP_BANNER_MESSAGE = i18n.translate(
-  'xpack.securitySolution.filterGroup.filtersChangedTitle',
+  'alertsUIShared.filterGroup.filtersChangedTitle',
   {
     defaultMessage: `New filter controls on this page are different from what you have previously saved. You can either save the changes or discard them.
        Navigating away will automatically discard these changes`,
@@ -66,42 +57,36 @@ export const FILTER_GROUP_BANNER_MESSAGE = i18n.translate(
 );
 
 export const CONTEXT_MENU_RESET_TOOLTIP = i18n.translate(
-  'xpack.securitySolution.filterGroup.contextMenu.resetTooltip',
+  'alertsUIShared.filterGroup.contextMenu.resetTooltip',
   {
     defaultMessage: 'Reset Controls to factory settings',
   }
 );
 
-export const CONTEXT_MENU_RESET = i18n.translate(
-  'xpack.securitySolution.filterGroup.contextMenu.reset',
-  {
-    defaultMessage: 'Reset Controls',
-  }
-);
+export const CONTEXT_MENU_RESET = i18n.translate('alertsUIShared.filterGroup.contextMenu.reset', {
+  defaultMessage: 'Reset Controls',
+});
 
-export const SAVE_CHANGES = i18n.translate(
-  'xpack.securitySolution.filtersGroup.contextMenu.saveChanges',
-  {
-    defaultMessage: 'Save Changes',
-  }
-);
+export const SAVE_CHANGES = i18n.translate('alertsUIShared.filtersGroup.contextMenu.saveChanges', {
+  defaultMessage: 'Save Changes',
+});
 
 export const REVERT_CHANGES = i18n.translate(
-  'xpack.securitySolution.filtersGroup.contextMenu.revertChanges',
+  'alertsUIShared.filtersGroup.contextMenu.revertChanges',
   {
     defaultMessage: 'Revert Changes',
   }
 );
 
 export const ADD_CONTROLS_MAX_LIMIT = i18n.translate(
-  'xpack.securitySolution.filtersGroup.contextMenu.addControls.maxLimit',
+  'alertsUIShared.filtersGroup.contextMenu.addControls.maxLimit',
   {
     defaultMessage: 'Maximum of 4 controls can be added.',
   }
 );
 
 export const URL_PARAM_ARRAY_EXCEPTION_MSG = i18n.translate(
-  'xpack.securitySolution.filtersGroup.urlParam.arrayError',
+  'alertsUIShared.filtersGroup.urlParam.arrayError',
   {
     defaultMessage: 'Page filter URL Params must be an array',
   }
