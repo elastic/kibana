a4a2b3e2e39d2fd23416b10f7138ed3296937fa8
Rodney Norris <rodney.norris@elastic.co>
2024-04-02 18:44:41 +0000

    search_notebooks: default notebooks

--- x-pack/plugins/search_notebooks/server/routes/index.ts
+++ x-pack/plugins/search_notebooks/server/routes/index.ts
@@ -5,19 +5,57 @@
  * 2.0.
  */
 
+import { schema } from '@kbn/config-schema';
 import type { IRouter } from '@kbn/core/server';
+import type { Logger } from '@kbn/logging';
 
-export function defineRoutes(router: IRouter) {
+import { DEFAULT_NOTEBOOKS, NOTEBOOKS_MAP, getNotebook } from '../lib/notebook_catalog';
+import { NotebookDefinition } from '../types';
+
+export function defineRoutes(router: IRouter, logger: Logger) {
   router.get(
     {
-      path: '/api/search_notebooks/example',
-      validate: false,
+      path: '/internal/search_notebooks/notebooks',
+      validate: {},
+    },
+    async (_context, _request, response) => {
+      return response.ok({
+        body: DEFAULT_NOTEBOOKS,
+        headers: { 'content-type': 'application/json' },
+      });
+    }
+  );
+
+  router.get(
+    {
+      path: '/internal/search_notebooks/notebooks/{notebookId}',
+      validate: {
+        params: schema.object({
+          notebookId: schema.string(),
+        }),
+      },
     },
     async (context, request, response) => {
+      const notebookId = request.params.notebookId;
+
+      if (!NOTEBOOKS_MAP.hasOwnProperty(notebookId)) {
+        logger.warn(`Unknown search notebook requested ${notebookId}`);
+        return response.notFound();
+      }
+
+      const notebookMetadata = NOTEBOOKS_MAP[notebookId];
+      let notebook: NotebookDefinition;
+      try {
+        notebook = await getNotebook(notebookId, { logger });
+      } catch (e) {
+        return response.customError(e.message);
+      }
       return response.ok({
         body: {
-          time: new Date().toISOString(),
+          ...notebookMetadata,
+          notebook,
         },
+        headers: { 'content-type': 'application/json' },
       });
     }
   );
--- x-pack/plugins/search_notebooks/server/types.ts
+++ x-pack/plugins/search_notebooks/server/types.ts
@@ -9,3 +9,23 @@
 export interface SearchNotebooksPluginSetup {}
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface SearchNotebooksPluginStart {}
+
+export interface NotebookMetadata {
+  id: string;
+  title: string;
+  description: string;
+}
+export interface NotebookCatalog {
+  notebooks: NotebookMetadata[];
+}
+
+// TODO: Define Notebook type
+export type NotebookDefinition = object;
+
+export interface Notebook extends NotebookMetadata {
+  link?: {
+    title: string;
+    url: string;
+  };
+  notebook: NotebookDefinition;
+}
