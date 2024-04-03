14b76b08b7c32e688aa5340b0fea6da86479d8f7
machadoum <pablo.nevesmachado@elastic.co>
2024-04-03 09:46:21 +0200

    wip

--- x-pack/plugins/security_solution/public/entity_analytics/pages/entity_analytics_management_page.tsx
+++ x-pack/plugins/security_solution/public/entity_analytics/pages/entity_analytics_management_page.tsx
@@ -6,12 +6,39 @@
  */
 
 import React from 'react';
-import { MyTest } from './my_test';
+import { EuiBetaBadge, EuiFlexGroup, EuiFlexItem, EuiPageHeader, EuiSpacer } from '@elastic/eui';
+
+import { RiskScorePreviewSection } from '../components/risk_score_preview_section';
+import { RiskScoreEnableSection } from '../components/risk_score_enable_section';
+import { ENTITY_ANALYTICS_RISK_SCORE } from '../../app/translations';
+import { BETA } from '../../common/translations';
+import { RiskEnginePrivilegesCallOut } from '../components/risk_engine_privileges_callout';
+import { useMissingRiskEnginePrivileges } from '../hooks/use_missing_risk_engine_privileges';
 
 export const EntityAnalyticsManagementPage = () => {
+  const privileges = useMissingRiskEnginePrivileges();
   return (
     <>
-      <MyTest />
+      <RiskEnginePrivilegesCallOut privileges={privileges} />
+      <EuiFlexGroup gutterSize="s" alignItems="baseline">
+        <EuiFlexItem grow={false}>
+          <EuiPageHeader
+            data-test-subj="entityAnalyticsManagementPageTitle"
+            pageTitle={ENTITY_ANALYTICS_RISK_SCORE}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false} />
+        <EuiBetaBadge label={BETA} size="s" />
+      </EuiFlexGroup>
+      <EuiSpacer size="l" />
+      <EuiFlexGroup gutterSize="xl">
+        <EuiFlexItem grow={2}>
+          <RiskScoreEnableSection privileges={privileges} />
+        </EuiFlexItem>
+        <EuiFlexItem grow={2}>
+          <RiskScorePreviewSection privileges={privileges} />
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </>
   );
 };
