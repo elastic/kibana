ef32b2d3d9a24baeddad67ee309af348a6ecf826
Sander Philipse <sander.philipse@elastic.co>
2024-04-03 17:49:17 +0200

    Fix import order

--- x-pack/plugins/enterprise_search/public/plugin.ts
+++ x-pack/plugins/enterprise_search/public/plugin.ts
@@ -24,8 +24,8 @@ import { DataPublicPluginStart } from '@kbn/data-plugin/public';
 
 import { GuidedOnboardingPluginStart } from '@kbn/guided-onboarding-plugin/public';
 import type { HomePublicPluginSetup } from '@kbn/home-plugin/public';
-import type { IndexManagementPluginStart } from '@kbn/index-management';
 import { i18n } from '@kbn/i18n';
+import type { IndexManagementPluginStart } from '@kbn/index-management';
 import { LensPublicStart } from '@kbn/lens-plugin/public';
 import { LicensingPluginStart } from '@kbn/licensing-plugin/public';
 import { MlPluginStart } from '@kbn/ml-plugin/public';
