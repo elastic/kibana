7b108abfc17bad130ba23bc78f2fc751f50af00c
Marco Liberati <dej611@users.noreply.github.com>
2024-04-03 11:02:16 +0200

    Update x-pack/test/functional/apps/lens/open_in_lens/tsvb/dashboard.ts

--- x-pack/test/functional/apps/lens/open_in_lens/tsvb/dashboard.ts
+++ x-pack/test/functional/apps/lens/open_in_lens/tsvb/dashboard.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../../ftr_provider_context';
 
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
