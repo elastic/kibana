79b79cad3d2d38d5fa8a3f154e175953097417d7
Kyle Pollich <kyle.pollich@elastic.co>
2024-04-03 08:22:11 -0400

    Remove console.log

--- x-pack/plugins/fleet/server/services/setup.ts
+++ x-pack/plugins/fleet/server/services/setup.ts
@@ -86,7 +86,6 @@ async function createSetupSideEffects(
 ): Promise<SetupStatus> {
   const logger = appContextService.getLogger();
   logger.info('Beginning fleet setup');
-  console.log('Beginning fleet setup');
 
   await cleanUpOldFileIndices(esClient, logger);
 
