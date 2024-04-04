235d4ddae696bc22bfd22784d82efb2bdca8147a
miriam.aparicio <miriam.aparicio@gmail.com>
2024-04-04 13:32:35 +0100

    fix tests

--- x-pack/plugins/observability_solution/observability_shared/public/locators/infra/locators.test.ts
+++ x-pack/plugins/observability_solution/observability_shared/public/locators/infra/locators.test.ts
@@ -30,22 +30,23 @@ const setupHostsLocator = async () => {
 
 describe('Infra Locators', () => {
   describe('Asset Details Locator', () => {
-    it('should create a link to Asset Details with no state', async () => {
-      const params = {
-        assetType: 'host',
-        assetId: '1234',
-        assetDetails: {
-          tabId: 'testTab',
-          dashboardId: 'testDashboard',
-          dateRange: {
-            from: '2021-01-01T00:00:00.000Z',
-            to: '2021-01-02T00:00:00.000Z',
-          },
+    const params = {
+      assetType: 'host',
+      assetId: '1234',
+      assetDetails: {
+        tabId: 'testTab',
+        dashboardId: 'testDashboard',
+        dateRange: {
+          from: '2021-01-01T00:00:00.000Z',
+          to: '2021-01-02T00:00:00.000Z',
         },
-      };
+      },
+    };
+    const assetDetails = rison.encodeUnknown(params.assetDetails);
+
+    it('should create a link to Asset Details with no state', async () => {
       const { assetDetailsLocator } = await setupAssetDetailsLocator();
       const { app, path, state } = await assetDetailsLocator.getLocation(params);
-      const assetDetails = rison.encodeUnknown(params.assetDetails);
 
       expect(app).toBe('metrics');
       expect(path).toBe(
@@ -56,21 +57,8 @@ describe('Infra Locators', () => {
     });
 
     it('should return correct structured url', async () => {
-      const params = {
-        assetType: 'host',
-        assetId: '1234',
-        assetDetails: {
-          tabId: 'testTab',
-          dashboardId: 'testDashboard',
-          dateRange: {
-            from: '2021-01-01T00:00:00.000Z',
-            to: '2021-01-02T00:00:00.000Z',
-          },
-        },
-      };
       const { assetDetailsLocator } = await setupAssetDetailsLocator();
       const { app, path, state } = await assetDetailsLocator.getLocation(params);
-      const assetDetails = rison.encodeUnknown(params.assetDetails);
 
       expect(app).toBe('metrics');
       expect(path).toBe(
@@ -82,31 +70,32 @@ describe('Infra Locators', () => {
   });
 
   describe('Asset Details Flyout Locator', () => {
+    const params = {
+      tableProperties: {
+        detailsItemId: '1234',
+        pagination: {
+          pageIndex: 0,
+          pageSize: 10,
+        },
+        sorting: {
+          field: 'alertsCount',
+        },
+      },
+      assetDetails: {
+        tabId: 'testTab',
+        dashboardId: 'testDashboard',
+        dateRange: {
+          from: '2021-01-01T00:00:00.000Z',
+          to: '2021-01-02T00:00:00.000Z',
+        },
+      },
+    };
+    const tableProperties = rison.encodeUnknown(params.tableProperties);
+    const assetDetails = rison.encodeUnknown(params.assetDetails);
+
     it('should create a link to Asset Details Flyout with no state', async () => {
-      const params = {
-        tableProperties: {
-          detailsItemId: '1234',
-          pagination: {
-            pageIndex: 0,
-            pageSize: 10,
-          },
-          sorting: {
-            field: 'alertsCount',
-          },
-        },
-        assetDetails: {
-          tabId: 'testTab',
-          dashboardId: 'testDashboard',
-          dateRange: {
-            from: '2021-01-01T00:00:00.000Z',
-            to: '2021-01-02T00:00:00.000Z',
-          },
-        },
-      };
       const { assetDetailsFlyoutLocator } = await setupAssetDetailsLocator();
       const { app, path, state } = await assetDetailsFlyoutLocator.getLocation(params);
-      const tableProperties = rison.encodeUnknown(params.tableProperties);
-      const assetDetails = rison.encodeUnknown(params.assetDetails);
 
       expect(app).toBe('metrics');
       expect(path).toBe(`/hosts?tableProperties=${tableProperties}&assetDetails=${assetDetails}`);
@@ -115,30 +104,8 @@ describe('Infra Locators', () => {
     });
 
     it('should return correct structured url', async () => {
-      const params = {
-        tableProperties: {
-          detailsItemId: '1234',
-          pagination: {
-            pageIndex: 0,
-            pageSize: 10,
-          },
-          sorting: {
-            field: 'alertsCount',
-          },
-        },
-        assetDetails: {
-          tabId: 'testTab',
-          dashboardId: 'testDashboard',
-          dateRange: {
-            from: '2021-01-01T00:00:00.000Z',
-            to: '2021-01-02T00:00:00.000Z',
-          },
-        },
-      };
       const { assetDetailsFlyoutLocator } = await setupAssetDetailsLocator();
       const { app, path, state } = await assetDetailsFlyoutLocator.getLocation(params);
-      const tableProperties = rison.encodeUnknown(params.tableProperties);
-      const assetDetails = rison.encodeUnknown(params.assetDetails);
 
       expect(app).toBe('metrics');
       expect(path).toBe(`/hosts?tableProperties=${tableProperties}&assetDetails=${assetDetails}`);
@@ -148,50 +115,49 @@ describe('Infra Locators', () => {
   });
 
   describe('Hosts Locator', () => {
+    const params = {
+      query: {
+        language: 'kuery',
+        query: 'host.name: "foo"',
+      },
+      dateRange: {
+        from: '2021-01-01T00:00:00.000Z',
+        to: '2021-01-02T00:00:00.000Z',
+      },
+      filters: [],
+      panelFilters: [],
+      limit: 10,
+      tableProperties: {
+        detailsItemId: '1234',
+        pagination: {
+          pageIndex: 0,
+          pageSize: 10,
+        },
+        sorting: {
+          field: 'alertsCount',
+        },
+      },
+    };
+    const { query, dateRange, filters, panelFilters, limit, tableProperties } = params;
+    const searchString = rison.encodeUnknown({ query, dateRange, filters, panelFilters, limit });
+    const tablePropertiesString = rison.encodeUnknown(tableProperties);
+
     it('should create a link to Hosts with no state', async () => {
-      const params = {
-        query: {
-          language: 'kuery',
-          query: 'host.name: "foo"',
-        },
-        dateRange: {
-          from: '2021-01-01T00:00:00.000Z',
-          to: '2021-01-02T00:00:00.000Z',
-        },
-        filters: [],
-        panelFilters: [],
-        limit: 10,
-      };
       const { hostsLocator } = await setupHostsLocator();
       const { app, path, state } = await hostsLocator.getLocation(params);
-      const searchString = rison.encodeUnknown(params);
 
       expect(app).toBe('metrics');
-      expect(path).toBe(`/hosts?_a=${searchString}`);
+      expect(path).toBe(`/hosts?_a=${searchString}&tableProperties=${tablePropertiesString}`);
       expect(state).toBeDefined();
       expect(Object.keys(state)).toHaveLength(0);
     });
 
     it('should return correct structured url', async () => {
-      const params = {
-        query: {
-          language: 'kuery',
-          query: 'host.name: "test"',
-        },
-        dateRange: {
-          from: '2021-01-01T00:00:00.000Z',
-          to: '2021-01-02T00:00:00.000Z',
-        },
-        filters: [],
-        panelFilters: [],
-        limit: 10,
-      };
       const { hostsLocator } = await setupHostsLocator();
       const { app, path, state } = await hostsLocator.getLocation(params);
-      const searchString = rison.encodeUnknown(params);
 
       expect(app).toBe('metrics');
-      expect(path).toBe(`/hosts?_a=${searchString}`);
+      expect(path).toBe(`/hosts?_a=${searchString}&tableProperties=${tablePropertiesString}`);
       expect(state).toBeDefined();
       expect(Object.keys(state)).toHaveLength(0);
     });
