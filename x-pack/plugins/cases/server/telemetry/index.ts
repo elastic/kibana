c9186a236011e417ed527003c4e906c5598651c5
Christos Nasikas <christos.nasikas@elastic.co>
2024-04-04 17:05:33 +0300

    Add basic telemetry

--- x-pack/plugins/cases/server/telemetry/index.ts
+++ x-pack/plugins/cases/server/telemetry/index.ts
@@ -21,6 +21,7 @@ import {
   CASES_TELEMETRY_TASK_NAME,
   CASE_TELEMETRY_SAVED_OBJECT_ID,
   SAVED_OBJECT_TYPES,
+  CASE_ORACLE_SAVED_OBJECT,
 } from '../../common/constants';
 import type { CasesTelemetry } from './types';
 import { casesSchema } from './schema';
@@ -43,7 +44,11 @@ export const createCasesTelemetry = async ({
 }: CreateCasesTelemetryArgs) => {
   const getInternalSavedObjectClient = async (): Promise<ISavedObjectsRepository> => {
     const [coreStart] = await core.getStartServices();
-    return coreStart.savedObjects.createInternalRepository([...SAVED_OBJECT_TYPES, FILE_SO_TYPE]);
+    return coreStart.savedObjects.createInternalRepository([
+      ...SAVED_OBJECT_TYPES,
+      FILE_SO_TYPE,
+      CASE_ORACLE_SAVED_OBJECT,
+    ]);
   };
 
   taskManager.registerTaskDefinitions({
