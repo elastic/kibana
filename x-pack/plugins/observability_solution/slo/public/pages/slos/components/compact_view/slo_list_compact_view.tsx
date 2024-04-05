df498321bc5751b9e20ebbe0b6b587b7d17c3990
Kevin Delemme <kevin.delemme@elastic.co>
2024-04-05 13:41:37 -0400

    more todo

--- x-pack/plugins/observability_solution/slo/public/pages/slos/components/compact_view/slo_list_compact_view.tsx
+++ x-pack/plugins/observability_solution/slo/public/pages/slos/components/compact_view/slo_list_compact_view.tsx
@@ -101,6 +101,10 @@ export function SloListCompactView({ sloList, loading, error }: Props) {
 
   const navigateToClone = useCloneSlo();
 
+  // TODO Kevin: MISSING REMOTE ACTIONS HANDLING
+  // With some refactoring, we should be able to
+  // 1. extract the actions regardless of the view used
+  // 2. use the common actions everywhere
   const actions: Array<DefaultItemAction<SLOWithSummaryResponse>> = [
     {
       type: 'icon',
