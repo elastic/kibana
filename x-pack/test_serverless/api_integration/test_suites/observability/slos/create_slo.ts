654b0f12bad20007f8c070d85c0fffa8c8ea4f9f
Dominique Belcher <dominique.clarke@elastic.co>
2024-04-05 09:37:44 -0400

    adjust serverless test

--- x-pack/test_serverless/api_integration/test_suites/observability/slos/create_slo.ts
+++ x-pack/test_serverless/api_integration/test_suites/observability/slos/create_slo.ts
@@ -144,10 +144,10 @@ export default function ({ getService }: FtrProviderContext) {
         const expectedTransforms: ExpectedTransforms = {
           count: 2,
           results: {
-            transform0: { id: 'slo-my-custom-id1-1', destIndex: '.slo-observability.sli-v3' },
+            transform0: { id: 'slo-my-custom-id1-1', destIndex: '.slo-observability.sli-v3.1' },
             transform1: {
               id: 'slo-summary-my-custom-id1-1',
-              destIndex: '.slo-observability.summary-v3',
+              destIndex: '.slo-observability.summary-v3.1',
             },
           },
           typeOfVersion: 'string',
