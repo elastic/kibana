b00b3527923108e91baaa5369b2fac6c2ec4c0ce
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-04-05 14:51:04 +0200

    lint fix

--- x-pack/test/functional/apps/lens/group6x-pack/test/functional/services/ml/dashboard_embeddables.ts
+++ x-pack/test/functional/apps/lens/group6x-pack/test/functional/services/ml/dashboard_embeddables.ts
@@ -8,7 +8,6 @@
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
-  const retry = getService('retry');
   const dataViews = getService('dataViews');
   const esArchiver = getService('esArchiver');
   const es = getService('es');
