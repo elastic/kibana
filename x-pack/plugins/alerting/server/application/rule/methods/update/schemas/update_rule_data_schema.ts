df0e45ff05b3fc69063d3d29dbe7fd628437e154
Jiawei Wu <jiawei.wu@elastic.co>
2024-04-06 20:56:11 -0700

    Fix tests

--- x-pack/plugins/alerting/server/application/rule/methods/update/schemas/update_rule_data_schema.ts
+++ x-pack/plugins/alerting/server/application/rule/methods/update/schemas/update_rule_data_schema.ts
@@ -14,16 +14,19 @@ import {
   systemActionRequestSchema,
 } from '../../../schemas';
 
-export const updateRuleDataSchema = schema.object({
-  name: schema.string(),
-  tags: schema.arrayOf(schema.string(), { defaultValue: [] }),
-  schedule: schema.object({
-    interval: schema.string({ validate: validateDuration }),
-  }),
-  throttle: schema.maybe(schema.nullable(schema.string({ validate: validateDuration }))),
-  params: schema.recordOf(schema.string(), schema.maybe(schema.any()), { defaultValue: {} }),
-  actions: schema.arrayOf(actionRequestSchema, { defaultValue: [] }),
-  systemActions: schema.maybe(schema.arrayOf(systemActionRequestSchema, { defaultValue: [] })),
-  notifyWhen: schema.maybe(schema.nullable(notifyWhenSchema)),
-  alertDelay: schema.maybe(alertDelaySchema),
-});
+export const updateRuleDataSchema = schema.object(
+  {
+    name: schema.string(),
+    tags: schema.arrayOf(schema.string(), { defaultValue: [] }),
+    schedule: schema.object({
+      interval: schema.string({ validate: validateDuration }),
+    }),
+    throttle: schema.maybe(schema.nullable(schema.string({ validate: validateDuration }))),
+    params: schema.recordOf(schema.string(), schema.maybe(schema.any()), { defaultValue: {} }),
+    actions: schema.arrayOf(actionRequestSchema, { defaultValue: [] }),
+    systemActions: schema.maybe(schema.arrayOf(systemActionRequestSchema, { defaultValue: [] })),
+    notifyWhen: schema.maybe(schema.nullable(notifyWhenSchema)),
+    alertDelay: schema.maybe(alertDelaySchema),
+  },
+  { unknowns: 'allow' }
+);
