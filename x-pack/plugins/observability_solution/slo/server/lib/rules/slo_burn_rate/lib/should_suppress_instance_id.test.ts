bb79299d5647a29cbe3c686bba040880cbd40557
Chris Cowan <chris@elastic.co>
2024-04-07 09:37:49 -0600

    writing unit teset for shouldSupressInstanceId

---
+++ x-pack/plugins/observability_solution/slo/server/lib/rules/slo_burn_rate/lib/should_suppress_instance_id.test.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { SLO } from '../../../../domain/models';
+import { Rule } from '@kbn/alerting-plugin/common';
+import { BurnRateRuleParams } from '../types';
+import { shouldSuppressInstanceId } from './should_suppress_instance_id';
+import { ALL_VALUE } from '@kbn/slo-schema';
+
+describe('shouldSuppressInstanceId', () => {
+  it('should suppress when supressAll is encountered', () => {
+    const results = [
+      {
+        slo: {} as unknown as SLO,
+        rule: {} as unknown as Rule<BurnRateRuleParams>,
+        suppressAll: true,
+        instanceIdsToSuppress: [],
+      },
+    ];
+    expect(shouldSuppressInstanceId(results, 'foo')).toBeTruthy();
+    expect(shouldSuppressInstanceId(results, ALL_VALUE)).toBeTruthy();
+  });
+  it('should suppress when instanceId is ALL_VALUE and any instanceId matches', () => {
+    const results = [
+      {
+        slo: {} as unknown as SLO,
+        rule: {} as unknown as Rule<BurnRateRuleParams>,
+        suppressAll: false,
+        instanceIdsToSuppress: ['foo'],
+      },
+    ];
+    expect(shouldSuppressInstanceId(results, ALL_VALUE)).toBeTruthy();
+  });
+  it('should suppress when instanceId is maches the same instanceId in the results', () => {
+    const results = [
+      {
+        slo: {} as unknown as SLO,
+        rule: {} as unknown as Rule<BurnRateRuleParams>,
+        suppressAll: false,
+        instanceIdsToSuppress: ['foo'],
+      },
+    ];
+    expect(shouldSuppressInstanceId(results, 'foo')).toBeTruthy();
+    expect(shouldSuppressInstanceId(results, 'bar')).not.toBeTruthy();
+  });
+});
