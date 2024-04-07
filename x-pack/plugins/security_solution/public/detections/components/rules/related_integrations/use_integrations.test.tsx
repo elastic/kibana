5fd108667661f42dc4be670b95fd57d32cf23951
Maxim Palenov <maxim.palenov@elastic.co>
2024-04-07 09:52:15 +0200

    fix use_integrations unit tests

--- x-pack/plugins/security_solution/public/detections/components/rules/related_integrations/use_integrations.test.tsx
+++ x-pack/plugins/security_solution/public/detections/components/rules/related_integrations/use_integrations.test.tsx
@@ -73,6 +73,7 @@ describe('useInstalledIntegrations', () => {
       {
         package_name: 'o365',
         package_title: 'Microsoft 365',
+        latest_package_version: '1.2.0',
         installed_package_version: '1.0.0',
         is_installed: false,
         is_enabled: false,
@@ -80,18 +81,21 @@ describe('useInstalledIntegrations', () => {
       {
         integration_name: 'audit',
         integration_title: 'Audit Logs',
-        is_installed: true,
-        is_enabled: true,
+
         package_name: 'atlassian_bitbucket',
         package_title: 'Atlassian Bitbucket',
+        latest_package_version: '1.0.1',
         installed_package_version: '1.0.1',
-      },
-      {
         is_installed: true,
         is_enabled: true,
+      },
+      {
         package_name: 'system',
         package_title: 'System',
+        latest_package_version: '1.6.4',
         installed_package_version: '1.6.4',
+        is_installed: true,
+        is_enabled: true,
       },
     ]);
   });
