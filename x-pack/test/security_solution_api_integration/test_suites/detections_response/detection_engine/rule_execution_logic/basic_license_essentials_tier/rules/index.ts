9d2c085643cea3b925f39535cca2d04d5ab40770
Yara Tercero <yara.tercero@elastic.co>
2024-04-08 12:12:50 -0700

    continued testing

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/basic_license_essentials_tier/rules/index.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/basic_license_essentials_tier/rules/index.ts
@@ -9,13 +9,13 @@ import { FtrProviderContext } from '../../../../../../ftr_provider_context';
 
 export default ({ loadTestFile }: FtrProviderContext): void => {
   describe('Detection Engine - rule execution', function () {
-    // loadTestFile(require.resolve('./new_terms'));
-    // loadTestFile(require.resolve('./eql'));
-    // loadTestFile(require.resolve('./esql'));
-    // loadTestFile(require.resolve('./query'));
-    // loadTestFile(require.resolve('./query_ess'));
-    // loadTestFile(require.resolve('./saved_query'));
+    loadTestFile(require.resolve('./new_terms'));
+    loadTestFile(require.resolve('./eql'));
+    loadTestFile(require.resolve('./esql'));
+    loadTestFile(require.resolve('./query'));
+    loadTestFile(require.resolve('./query_ess'));
+    loadTestFile(require.resolve('./saved_query'));
     loadTestFile(require.resolve('./threshold'));
-    // loadTestFile(require.resolve('./threat_match'));
+    loadTestFile(require.resolve('./threat_match'));
   });
 };
