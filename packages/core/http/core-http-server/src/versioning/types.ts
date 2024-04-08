1142d49f8bd7c10c00eca3b2b0c8a8e191542b7c
Jean-Louis Leysens <jloleysens@gmail.com>
2024-04-08 13:44:12 +0200

    update types to allow lazy route schema registration

--- packages/core/http/core-http-server/src/versioning/types.ts
+++ packages/core/http/core-http-server/src/versioning/types.ts
@@ -228,7 +228,7 @@ export interface AddVersionOpts<P, Q, B> {
    * Validation for this version of a route
    * @public
    */
-  validate: false | FullValidationConfig<P, Q, B>;
+  validate: false | FullValidationConfig<P, Q, B> | (() => FullValidationConfig<P, Q, B>); // Provide a way to lazily load validation schemas
 }
 
 /**
