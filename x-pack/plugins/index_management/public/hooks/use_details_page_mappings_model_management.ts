c2df4583de3d147c86330d399e3d56f9b594f7c9
Saikat Sarkar <saikat.sarkar@elastic.co>
2024-04-09 15:58:15 -0600

    Refactor fetchInferenceModelsAndTrainedModelStats

--- x-pack/plugins/index_management/public/hooks/use_details_page_mappings_model_management.ts
+++ x-pack/plugins/index_management/public/hooks/use_details_page_mappings_model_management.ts
@@ -8,11 +8,11 @@
 import { useCallback, useState } from 'react';
 import { InferenceStatsResponse } from '@kbn/ml-plugin/public/application/services/ml_api_service/trained_models';
 import type { InferenceAPIConfigResponse } from '@kbn/ml-trained-models-utils';
-import { InferenceToModelIdMap } from '../../../../components/mappings_editor/components/document_fields/fields';
-import { deNormalize } from '../../../../components/mappings_editor/lib';
-import { useAppContext } from '../../../../app_context';
-import { useComponentTemplatesContext } from '../../../../components/component_templates/component_templates_context';
-import { State } from '../../../../components/mappings_editor/types';
+import { InferenceToModelIdMap } from '../application/components/mappings_editor/components/document_fields/fields';
+import { deNormalize } from '../application/components/mappings_editor/lib';
+import { useAppContext } from '../application/app_context';
+import { useComponentTemplatesContext } from '../application/components/component_templates/component_templates_context';
+import { State } from '../application/components/mappings_editor/types';
 
 interface InferenceModel {
   data: InferenceAPIConfigResponse[];
@@ -20,7 +20,7 @@ interface InferenceModel {
 
 type DeploymentStatusType = Record<string, 'deployed' | 'not_deployed'>;
 
-export const useDetailsPageMappingsContent = (state: State) => {
+export const useDetailsPageMappingsModelManagement = (state: State) => {
   const {
     plugins: { ml },
   } = useAppContext();
@@ -30,20 +30,13 @@ export const useDetailsPageMappingsContent = (state: State) => {
   const { inferenceToModelIdMap } = state;
 
   const [pendingDeployments, setPendingDeployments] = useState<Set<string>>(new Set());
-  const fetchInferenceModels = useCallback(async () => {
-    return (await api.getInferenceModels()) as InferenceModel;
-  }, [api]);
-
-  const fetchTrainedModelStats = useCallback(async () => {
-    return await ml?.mlApi?.trainedModels.getTrainedModelStats();
-  }, [ml]);
 
   const fetchInferenceModelsAndTrainedModelStats = useCallback(async () => {
-    const inferenceModels = fetchInferenceModels();
-    const trainedModelStats = fetchTrainedModelStats();
+    const inferenceModels = (await api.getInferenceModels()) as InferenceModel;
+    const trainedModelStats = await ml?.mlApi?.trainedModels.getTrainedModelStats();
 
-    return Promise.all([inferenceModels, trainedModelStats]);
-  }, [fetchInferenceModels, fetchTrainedModelStats]);
+    return { inferenceModels, trainedModelStats };
+  }, [api, ml]);
 
   const getTrainedModelStats = useCallback(
     (modelStats?: InferenceStatsResponse): DeploymentStatusType => {
@@ -77,8 +70,8 @@ export const useDetailsPageMappingsContent = (state: State) => {
   }, []);
 
   const getCustomInferenceIdMap = useCallback(
-    (models: InferenceModel, deploymentStatsByModelId: DeploymentStatusType) => {
-      return models.data.reduce<InferenceToModelIdMap>((inferenceMap, model) => {
+    (deploymentStatsByModelId: DeploymentStatusType, models?: InferenceModel) => {
+      return models?.data.reduce<InferenceToModelIdMap>((inferenceMap, model) => {
         const inferenceId = model.model_id;
         const trainedModelId =
           'model_id' in model.service_settings ? model.service_settings.model_id : '';
@@ -94,10 +87,10 @@ export const useDetailsPageMappingsContent = (state: State) => {
   );
 
   const processInferenceToModelIdMap = useCallback(
-    (models: InferenceModel, modelStats?: InferenceStatsResponse) => {
+    (models?: InferenceModel, modelStats?: InferenceStatsResponse) => {
       const deploymentStatsByModelId = getTrainedModelStats(modelStats);
       const defaultInferenceIds = getDefaultInferenceIds(deploymentStatsByModelId);
-      const modelIdMap = getCustomInferenceIdMap(models, deploymentStatsByModelId);
+      const modelIdMap = getCustomInferenceIdMap(deploymentStatsByModelId, models);
       return { ...defaultInferenceIds, ...modelIdMap };
     },
     [getCustomInferenceIdMap, getDefaultInferenceIds, getTrainedModelStats]
