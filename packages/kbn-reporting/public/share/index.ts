ac4bb4f5c23ec2453df1f341bffa688b4739da17
rshen91 <rshen@elastic.co>
2024-04-09 12:01:30 -0600

    clean

--- packages/kbn-reporting/public/share/index.ts
+++ packages/kbn-reporting/public/share/index.ts
@@ -6,9 +6,6 @@
  * Side Public License, v 1.
  */
 
-export { getSharedComponents } from './shared';
 export { reportingExportModalProvider } from './share_context_menu/register_pdf_png_modal_reporting';
-export { reportingCsvShareProvider } from './share_context_menu/register_csv_reporting';
 export { reportingCsvShareProvider as reportingCsvShareModalProvider } from './share_context_menu/register_csv_modal_reporting';
-export type { ReportingPublicComponents } from './shared/get_shared_components';
 export type { JobParamsProviderOptions } from './share_context_menu';
