2cd37c73cfe0e6aadbe5e572998748d157df5a4b
Nathan Reese <reese.nathan@gmail.com>
2024-04-09 08:18:40 -0600

    eslint

--- packages/presentation/presentation_publishing/publishing_subject/publishing_batcher.ts
+++ packages/presentation/presentation_publishing/publishing_subject/publishing_batcher.ts
@@ -26,7 +26,7 @@ const hasSubjectsArrayChanged = (
 /**
  * Batches the latest values of multiple publishing subjects into a single object. Use this to avoid unnecessary re-renders.
  * Use when `subjects` may not be defined on initial component render.
- * 
+ *
  * @param subjects Publishing subjects array.
  *   When 'subjects' is expected to change, 'subjects' must be part of component react state.
  */
@@ -100,7 +100,7 @@ export const useBatchedOptionalPublishingSubjects = <
 /**
  * Batches the latest values of multiple publishing subjects into a single object. Use this to avoid unnecessary re-renders.
  * Use when `subjects` are static and do not change over the lifetime of the component.
- * 
+ *
  * @param subjects Publishing subjects array.
  */
 export const useBatchedPublishingSubjects = <
