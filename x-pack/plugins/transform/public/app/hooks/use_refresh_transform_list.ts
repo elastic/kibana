da6e0613693b8280865b6026d830bfc907587f43
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-04-09 08:37:41 +0200

    invalidate all queries on transform list refresh

--- x-pack/plugins/transform/public/app/hooks/use_refresh_transform_list.ts
+++ x-pack/plugins/transform/public/app/hooks/use_refresh_transform_list.ts
@@ -7,16 +7,10 @@
 
 import { useQueryClient } from '@tanstack/react-query';
 
-import { TRANSFORM_REACT_QUERY_KEYS } from '../../../common/constants';
-
 export const useRefreshTransformList = () => {
   const queryClient = useQueryClient();
 
   return () => {
-    queryClient.invalidateQueries([TRANSFORM_REACT_QUERY_KEYS.GET_TRANSFORM_NODES]);
-    queryClient.invalidateQueries([TRANSFORM_REACT_QUERY_KEYS.GET_TRANSFORMS]);
-    queryClient.invalidateQueries([TRANSFORM_REACT_QUERY_KEYS.GET_TRANSFORM_STATS]);
-    queryClient.invalidateQueries([TRANSFORM_REACT_QUERY_KEYS.GET_TRANSFORMS_STATS]);
-    queryClient.invalidateQueries([TRANSFORM_REACT_QUERY_KEYS.GET_TRANSFORM_AUDIT_MESSAGES]);
+    queryClient.invalidateQueries();
   };
 };
