873b5b5a7ffac0eb2b94e78cbea21153df1769ff
Rodney Norris <rodney.norris@elastic.co>
2024-04-09 15:51:42 +0000

    search_notebooks: add rendering of more output types

--- packages/kbn-ipynb/components/notebook_cell_output.tsx
+++ packages/kbn-ipynb/components/notebook_cell_output.tsx
@@ -10,19 +10,60 @@ import React from 'react';
 import { EuiCodeBlock } from '@elastic/eui';
 
 import { NotebookOutputType } from '../types';
-import { isDefined, combineSource } from '../utils';
+import { isDefined, isDefinedAndHasValue, combineSource } from '../utils';
 
 export const NotebookCellOutput = ({ output }: { output: NotebookOutputType }) => {
-  if (output.text && output.text.length > 0) {
+  if (isDefined(output.text) && output.text.length > 0) {
     return <EuiCodeBlock>{combineSource(output.text)}</EuiCodeBlock>;
+  }
+  if (isDefinedAndHasValue(output.png)) {
+    return <OutputPng value={output.png} />;
+  }
+  if (isDefinedAndHasValue(output.jpeg)) {
+    return <OutputJpeg value={output.jpeg} />;
+  }
+  if (isDefinedAndHasValue(output.gif)) {
+    return <OutputGif value={output.gif} />;
   }
   if (!isDefined(output.data)) return null;
   if (isDefined(output.data['text/plain'])) {
     return <EuiCodeBlock>{combineSource(output.data['text/plain'])}</EuiCodeBlock>;
   }
-  if (isDefined(output.data['text/html'])) {
-    return <EuiCodeBlock>{combineSource(output.data['text/html'])}</EuiCodeBlock>;
+  if (isDefinedAndHasValue(output.data['image/png'])) {
+    return <OutputPng value={output.data['image/png']} />;
   }
-  // TODO: handle other output data types
+  if (isDefinedAndHasValue(output.data['image/jpeg'])) {
+    return <OutputJpeg value={output.data['image/jpeg']} />;
+  }
+  if (isDefinedAndHasValue(output.data['image/gif'])) {
+    return <OutputGif value={output.data['image/gif']} />;
+  }
+  if (isDefined(output.data['text/html'])) {
+    // We just present HTML instead of rendering it for safety
+    return <EuiCodeBlock lineNumbers>{combineSource(output.data['text/html'])}</EuiCodeBlock>;
+  }
+  if (isDefined(output.data['application/javascript'])) {
+    // We just present JS instead of rendering it for safety
+    return (
+      <EuiCodeBlock lineNumbers>
+        {combineSource(output.data['application/javascript'])}
+      </EuiCodeBlock>
+    );
+  }
+  if (isDefined(output.data['text/latex'])) {
+    return <EuiCodeBlock lineNumbers>{combineSource(output.data['text/latex'])}</EuiCodeBlock>;
+  }
+
   return null;
 };
+
+const OutputImage = ({ value, type }: { value: string; type: 'png' | 'jpeg' | 'gif' }) => (
+  <img
+    src={`data:image/${type};base64,${value}`}
+    alt={`output ${type} image`}
+    style={{ maxHeight: '400px', maxWidth: '400px' }}
+  />
+);
+const OutputPng = ({ value }: { value: string }) => <OutputImage value={value} type="png" />;
+const OutputJpeg = ({ value }: { value: string }) => <OutputImage value={value} type="jpeg" />;
+const OutputGif = ({ value }: { value: string }) => <OutputImage value={value} type="gif" />;
--- packages/kbn-ipynb/utils.ts
+++ packages/kbn-ipynb/utils.ts
@@ -10,6 +10,10 @@ export function isDefined<T>(value: T | undefined | null): value is T {
   return value !== undefined && value !== null;
 }
 
+export function isDefinedAndHasValue(value: string | undefined): value is string {
+  return isDefined(value) && value.length > 0;
+}
+
 export const combineSource = (value: string | string[], separator: string = ''): string => {
   if (Array.isArray(value)) return value.join(separator);
   return value;
