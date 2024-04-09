02f753466c45803faed6935095d8970127a6b8c1
Rodney Norris <rodney.norris@elastic.co>
2024-04-09 13:23:52 +0000

    search_notebooks: improve intro notebook copy

--- x-pack/plugins/search_notebooks/common/constants.ts
+++ x-pack/plugins/search_notebooks/common/constants.ts
@@ -11,16 +11,21 @@ import { Notebook } from './types';
 export const INTRODUCTION_NOTEBOOK: Notebook = {
   id: 'introduction',
   title: i18n.translate('xpack.searchNotebooks.introductionNotebook.title', {
-    defaultMessage: 'What are Notebooks?',
+    defaultMessage: 'What are Jupyter Notebooks?',
   }),
   description: i18n.translate('xpack.searchNotebooks.introductionNotebook.description', {
-    defaultMessage: 'This is a description for the introduction notebook',
+    defaultMessage:
+      'Jupyter notebooks are an open-source document format for sharing live code examples with narrative text.',
   }),
   notebook: {
     cells: [
       {
         cell_type: 'markdown',
-        source: ['# What are Notebooks', 'Notebooks are lorem ipsum....'],
+        source: [
+          '# What are Jupyter Notebooks\n',
+          '\n',
+          'Jupyter Notebooks are an innovative tool that utilizes code, computation output and rich text into a single interactive document. They serve as a versatile platform for data analysis, scientific research and educational purposes.',
+        ],
       },
       {
         cell_type: 'code',
