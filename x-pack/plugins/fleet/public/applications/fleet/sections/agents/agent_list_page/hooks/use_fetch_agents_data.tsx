4eabb3dac4a47c04cfdd908df44a478def33ef9f
jillguyonnet <jill.guyonnet@gmail.com>
2024-04-10 14:17:57 +0100

    Revert refresh rate change

--- x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/hooks/use_fetch_agents_data.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/hooks/use_fetch_agents_data.tsx
@@ -29,7 +29,7 @@ import { AGENT_POLICY_SAVED_OBJECT_TYPE, SO_SEARCH_LIMIT } from '../../../../con
 
 import { getKuery } from '../utils/get_kuery';
 
-const REFRESH_INTERVAL_MS = 10000;
+const REFRESH_INTERVAL_MS = 30000;
 const MAX_AGENT_ACTIONS = 100;
 
 export function useFetchAgentsData() {
