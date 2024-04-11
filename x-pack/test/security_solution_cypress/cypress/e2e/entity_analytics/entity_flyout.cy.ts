d3671431ce68601ee132ec1617df14568c9a3737
Mark Hopkin <mark.hopkin@elastic.co>
2024-04-11 14:47:06 +0100

    add cypress test

--- x-pack/test/security_solution_cypress/cypress/e2e/entity_analytics/entity_flyout.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/entity_analytics/entity_flyout.cy.ts
@@ -129,6 +129,18 @@ describe(
             .contains('High Impact')
             .should('be.visible');
         });
+        it('should unassign asset criticality state', () => {
+          cy.log('asset criticality update');
+          expandFirstAlertUserFlyout();
+          selectAssetCriticalityLevel('High Impact');
+          cy.get(ENTITY_DETAILS_FLYOUT_ASSET_CRITICALITY_LEVEL)
+            .contains('High Impact')
+            .should('be.visible');
+          selectAssetCriticalityLevel('Unassigned');
+          cy.get(ENTITY_DETAILS_FLYOUT_ASSET_CRITICALITY_LEVEL)
+            .contains('Unassigned')
+            .should('be.visible');
+        });
       });
 
       // https://github.com/elastic/kibana/issues/179248
