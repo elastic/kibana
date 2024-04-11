c7078f385b66858d6957fecfd6c2e719511af61c
Thom Heymann <190132+thomheymann@users.noreply.github.com>
2024-04-11 16:25:36 +0100

    [Observability Onboarding] Embed existing logs flows (#180054)
    Resolves #178948
    
    ## Summary
    
    Embeds existing custom and system log flows into new onboarding page. 
    
    Note: As discussed there are several usability issues with this approach
    which would need to be addressed before this can be merged into main.
    This PR acts as more of a starting point rather than a finished feature.
    
    ## Screenshots
    
    <img width="1372" alt="Screenshot 2024-04-04 at 17 10 46"
    src="https://github.com/elastic/kibana/assets/190132/21281d93-ee61-437e-9ef5-249c49c29e75">
    
    ## Testing
    
    The experimental onboarding flow can now be accessed using the hidden
    URL: `/app/experimental-onboarding`
    
    ---------
    
    Co-authored-by: Joe Reuter <johannes.reuter@elastic.co>

--- x-pack/plugins/observability_solution/observability_onboarding/public/application/experimental_onboarding_flow.tsx
+++ x-pack/plugins/observability_solution/observability_onboarding/public/application/experimental_onboarding_flow.tsx
@@ -4,18 +4,70 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { i18n } from '@kbn/i18n';
 
 import React from 'react';
-import { EuiPageTemplate, EuiSpacer } from '@elastic/eui';
+import {
+  EuiPageTemplate,
+  EuiSpacer,
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButtonEmpty,
+} from '@elastic/eui';
 import { css } from '@emotion/react';
+import { useHistory } from 'react-router-dom';
+import { Route, Routes } from '@kbn/shared-ux-router';
+import { reactRouterNavigate } from '@kbn/kibana-react-plugin/public';
+import { useNavigate, useLocation } from 'react-router-dom-v5-compat';
 import backgroundImageUrl from './header/background.svg';
 import { Footer } from './footer/footer';
 import { OnboardingFlowForm } from './onboarding_flow_form/onboarding_flow_form';
 import { Header } from './header/header';
+import { SystemLogsPanel } from './quickstart_flows/system_logs';
+import { CustomLogsPanel } from './quickstart_flows/custom_logs';
 
 export function ExperimentalOnboardingFlow() {
+  const history = useHistory();
+  const location = useLocation();
+
   return (
     <>
+      {/* Test buttons to be removed once integrations selector has been implemented */}
+      <EuiPageTemplate.Section grow={false} color="accent" restrictWidth>
+        <EuiFlexGroup>
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              data-test-subj="observabilityOnboardingExperimentalOnboardingFlowSystemLogsButton"
+              {...reactRouterNavigate(
+                history,
+                `/systemLogs/${location.search}`
+              )}
+              color="accent"
+            >
+              {i18n.translate(
+                'xpack.observability_onboarding.experimentalOnboardingFlow.systemLogsButtonLabel',
+                { defaultMessage: 'System Logs' }
+              )}
+            </EuiButton>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiButton
+              data-test-subj="observabilityOnboardingExperimentalOnboardingFlowCustomLogsButton"
+              {...reactRouterNavigate(
+                history,
+                `/customLogs/${location.search}`
+              )}
+              color="accent"
+            >
+              {i18n.translate(
+                'xpack.observability_onboarding.experimentalOnboardingFlow.customLogsButtonLabel',
+                { defaultMessage: 'Custom Logs' }
+              )}
+            </EuiButton>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiPageTemplate.Section>
       <EuiPageTemplate.Section
         paddingSize="xl"
         css={css`
@@ -32,7 +84,19 @@ export function ExperimentalOnboardingFlow() {
         <Header />
       </EuiPageTemplate.Section>
       <EuiPageTemplate.Section paddingSize="xl" color="subdued" restrictWidth>
-        <OnboardingFlowForm />
+        <Routes>
+          <Route path="/systemLogs">
+            <BackButton />
+            <SystemLogsPanel />
+          </Route>
+          <Route path="/customLogs">
+            <BackButton />
+            <CustomLogsPanel />
+          </Route>
+          <Route>
+            <OnboardingFlowForm />
+          </Route>
+        </Routes>
         <EuiSpacer size="xl" />
       </EuiPageTemplate.Section>
       <EuiPageTemplate.Section paddingSize="xl" grow={false} restrictWidth>
@@ -42,3 +106,25 @@ export function ExperimentalOnboardingFlow() {
     </>
   );
 }
+
+const BackButton = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <>
+      <EuiButtonEmpty
+        data-test-subj="observabilityOnboardingExperimentalOnboardingFlowBackToSelectionButton"
+        iconType="arrowLeft"
+        flush="left"
+        onClick={() => navigate(`../${location.search}`)}
+      >
+        {i18n.translate(
+          'xpack.observability_onboarding.experimentalOnboardingFlow.button.backToSelectionLabel',
+          { defaultMessage: 'Back to selection' }
+        )}
+      </EuiButtonEmpty>
+      <EuiSpacer size="m" />
+    </>
+  );
+};
