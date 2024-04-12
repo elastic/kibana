24585d979edbe92eb3ddcc5b309dce34b93983d5
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2024-04-12 09:57:46 +0200

    refactor(discover-features-registry): remove superflous comment

--- packages/kbn-discover-features-registry/src/features_registry.ts
+++ packages/kbn-discover-features-registry/src/features_registry.ts
@@ -10,11 +10,6 @@ import { BaseFeature } from './types';
 export class FeaturesRegistry<Feature extends BaseFeature = BaseFeature> {
   private readonly features = new Map<Feature['id'], Feature>();
 
-  /**
-   * Registers a new feature in the registry.
-   * @param {Feature} feature The actual feature instance to register.
-   * @throws {Error} If a feature with the same id already exists.
-   */
   register(feature: Feature): void {
     if (this.features.has(feature.id)) {
       throw new Error(
@@ -25,11 +20,6 @@ export class FeaturesRegistry<Feature extends BaseFeature = BaseFeature> {
     this.features.set(feature.id, feature);
   }
 
-  /**
-   * Retrieves a registered feature by its id.
-   * @param id The identifier of the feature to retrieve.
-   * @returns The feature if found, otherwise undefined.
-   */
   getById<Id extends Feature['id']>(id: Id) {
     return this.features.get(id) as Extract<Feature, { id: Id }> | undefined;
   }
