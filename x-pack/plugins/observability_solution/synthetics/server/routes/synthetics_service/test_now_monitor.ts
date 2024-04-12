29e8394c3dbf007470f737ab32214d51b06b62d0
Shahzad <shahzad31comp@gmail.com>
2024-04-13 00:10:11 +0500

    [Synthetics] Monitor CRUD's public api's (#169928)
    ## Summary
    
    Part of https://github.com/elastic/kibana/issues/169547
    
    View docs at [Changed
    pages](https://kibana_169928.docs-preview.app.elstc.co/diff)
    
    Add monitor api public api 
    
    ### Testing
    
    Make sure you have some monitors populated before testing this PR and
    before switching to the branch
    
    - [ ] Try editing already added monitors via API
    - [ ] Test adding monitors via API, and then edit those via and
    subsqeuently try editing via API the same monitor
    - [ ] Test editing monitors via API
    - [ ] Test deleting monitors via API
    - [ ] Test getting monitors via API
    - [ ] Testing private as well public locations
    
    Basic workflow that i am interesting in testing is to make sure, you can
    add/edit via both API and UI without any issues
    
    
    Test each of HTTP/TCP/ICMP browser examples
    
    <img width="1728" alt="image"
    src="https://github.com/elastic/kibana/assets/3505601/3575d93a-5f04-4c80-ac62-038643f466f8">
    
    ---------
    
    Co-authored-by: Justin Kambic <jk@elastic.co>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Dominique Clarke <dominique.clarke@elastic.co>

--- x-pack/plugins/observability_solution/synthetics/server/routes/synthetics_service/test_now_monitor.ts
+++ x-pack/plugins/observability_solution/synthetics/server/routes/synthetics_service/test_now_monitor.ts
@@ -8,12 +8,12 @@ import { schema } from '@kbn/config-schema';
 import { v4 as uuidv4 } from 'uuid';
 import { getDecryptedMonitor } from '../../saved_objects/synthetics_monitor';
 import { PrivateLocationAttributes } from '../../runtime_types/private_locations';
-import { getPrivateLocationsForMonitor } from '../monitor_cruds/add_monitor';
 import { RouteContext, SyntheticsRestApiRouteFactory } from '../types';
 import { TestNowResponse } from '../../../common/types';
 import { ConfigKey, MonitorFields } from '../../../common/runtime_types';
 import { SYNTHETICS_API_URLS } from '../../../common/constants';
 import { normalizeSecrets } from '../../synthetics_service/utils/secrets';
+import { getPrivateLocationsForMonitor } from '../monitor_cruds/add_monitor/utils';
 
 export const testNowMonitorRoute: SyntheticsRestApiRouteFactory<TestNowResponse> = () => ({
   method: 'POST',
