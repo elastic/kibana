41706f2e98eb1c1c78bc55ae3f03302dc4f86304
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-04-12 17:38:05 +0200

    [Discover] Relocate serverless tests accordingly

--- x-pack/test_serverless/functional/test_suites/common/discover/group5/index.ts
+++ x-pack/test_serverless/functional/test_suites/common/discover/group5/index.ts
@@ -11,16 +11,15 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
   const esArchiver = getService('esArchiver');
   const browser = getService('browser');
 
-  describe('discover/group2', function () {
+  describe('discover/group5', function () {
     before(async function () {
-      await browser.setWindowSize(1600, 1200);
+      await browser.setWindowSize(1300, 800);
     });
 
     after(async function unloadMakelogs() {
       await esArchiver.unload('test/functional/fixtures/es_archiver/logstash_functional');
     });
 
-    loadTestFile(require.resolve('./_data_grid_doc_navigation'));
-    loadTestFile(require.resolve('./_data_grid_doc_table'));
+    loadTestFile(require.resolve('./_url_state'));
   });
 }
