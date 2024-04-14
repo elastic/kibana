4b0a34beb727d1e4fcac1ba5330b28ca9d9b10f4
klacabane <kevin.lacabane@elastic.co>
2024-04-14 21:30:39 +0200

    fix tst

--- x-pack/test/alerting_api_integration/spaces_only/tests/actions/get_all_system.ts
+++ x-pack/test/alerting_api_integration/spaces_only/tests/actions/get_all_system.ts
@@ -247,7 +247,7 @@ export default function getAllActionTests({ getService }: FtrProviderContext) {
         },
         {
           connector_type_id: '.observability-ai-assistant',
-          name: 'System action: .observability-ai-assistant',
+          name: 'Observability AI Assistant',
           is_deprecated: false,
           is_preconfigured: false,
           is_system_action: true,
