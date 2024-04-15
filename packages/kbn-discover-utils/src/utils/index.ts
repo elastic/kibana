01f3647a9318fc1413d56438c0fa7042b9e9a02b
Hannah Mudge <hannah.wright@elastic.co>
2024-04-15 16:37:34 -0600

    Delete duplicated code

--- packages/kbn-discover-utils/src/utils/index.ts
+++ packages/kbn-discover-utils/src/utils/index.ts
@@ -15,5 +15,4 @@ export * from './get_log_document_overview';
 export * from './get_message_field_with_fallbacks';
 export * from './get_should_show_field_handler';
 export * from './nested_fields';
-export * from './sorting';
 export { isLegacyTableEnabled } from './is_legacy_table_enabled';
