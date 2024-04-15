105e5b3f967f4cdb25da930354ff0f684567c63e
dej611 <dej611@gmail.com>
2024-04-15 14:24:12 +0200

    :recycle: Unskip + shuffle the order

--- x-pack/test/functional/apps/lens/open_in_lens/tsvb/dashboard.ts
+++ x-pack/test/functional/apps/lens/open_in_lens/tsvb/dashboard.ts
@@ -28,7 +28,6 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const dashboardAddPanel = getService('dashboardAddPanel');
   const filterBar = getService('filterBar');
 
-  // FLAKY: https://github.com/elastic/kibana/issues/179307
   describe('Dashboard to TSVB to Lens', function describeIndexTests() {
     before(async () => {
       await visualize.initTests();
