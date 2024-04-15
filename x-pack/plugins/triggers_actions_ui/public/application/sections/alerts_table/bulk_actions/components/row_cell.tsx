32d961665162a8d4f73bc5e7fbef105de2d8837f
Alexey Antonov <alexwizp@gmail.com>
2024-04-15 15:42:35 +0300

    fix: [Alerts > Landing][AXE-CORE]: Form elements must have labels
    Closes: https://github.com/elastic/security-team/issues/8577

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/bulk_actions/components/row_cell.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alerts_table/bulk_actions/components/row_cell.tsx
@@ -8,6 +8,7 @@
 import { EuiCheckbox, EuiLoadingSpinner } from '@elastic/eui';
 import React, { ChangeEvent } from 'react';
 import { useContext } from 'react';
+import { SELECT_ROW_ARIA_LABEL } from '../translations';
 import { AlertsTableContext } from '../../contexts/alerts_table_context';
 import { BulkActionsVerbs } from '../../../../../types';
 
@@ -28,6 +29,7 @@ const BulkActionsRowCellComponent = ({ rowIndex }: { rowIndex: number }) => {
   return (
     <EuiCheckbox
       id={`bulk-actions-row-cell-${rowIndex}`}
+      aria-label={SELECT_ROW_ARIA_LABEL(rowIndex + 1)}
       checked={isChecked}
       onChange={(e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.checked) {
