6b5146241ee872905e02fd91640d85f4df070d46
Alexey Antonov <alexwizp@gmail.com>
2024-04-15 17:52:12 +0300

    revert changing sizing

--- x-pack/plugins/cases/public/components/header_page/editable_title.tsx
+++ x-pack/plugins/cases/public/components/header_page/editable_title.tsx
@@ -92,7 +92,7 @@ const EditableTitleComponent: React.FC<EditableTitleProps> = ({ onSubmit, isLoad
           }}
           inputAriaLabel="Editable title input field"
           heading="h1"
-          size="l"
+          size="s"
           isInvalid={hasErrors}
           isLoading={isLoading}
           isReadOnly={!permissions.update}
