6821652051a28804410b9584b3864ac20876c109
Timothy Sullivan <tsullivan@elastic.co>
2024-04-16 12:04:57 -0700

    [Core] Remove usage of deprecated React rendering utilities

--- packages/core/overlays/core-overlays-browser-internal/src/banners/user_banner_service.test.ts
+++ packages/core/overlays/core-overlays-browser-internal/src/banners/user_banner_service.test.ts
@@ -9,7 +9,7 @@
 import { uiSettingsServiceMock } from '@kbn/core-ui-settings-browser-mocks';
 import { UserBannerService } from './user_banner_service';
 import { overlayBannersServiceMock } from './banners_service.test.mocks';
-import { i18nServiceMock } from '@kbn/core-i18n-browser-mocks';
+import { analyticsServiceMock, i18nServiceMock, themeServiceMock } from '@kbn/core/public/mocks';
 import { Subject } from 'rxjs';
 
 describe('OverlayBannersService', () => {
@@ -33,7 +33,9 @@ describe('OverlayBannersService', () => {
     service = new UserBannerService();
     service.start({
       banners,
+      analytics: analyticsServiceMock.createAnalyticsServiceStart(),
       i18n: i18nServiceMock.createStartContract(),
+      theme: themeServiceMock.createStartContract(),
       uiSettings,
     });
   };
