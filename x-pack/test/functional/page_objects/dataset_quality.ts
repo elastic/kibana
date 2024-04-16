71ca804da1fc117dcf85ba5a6490afa4126a51e1
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2024-04-16 10:58:30 +0200

    Fix Tests

--- x-pack/test/functional/page_objects/dataset_quality.ts
+++ x-pack/test/functional/page_objects/dataset_quality.ts
@@ -219,8 +219,8 @@ export function DatasetQualityPageObject({ getPageObjects, getService }: FtrProv
           `[data-test-subj=${testSubjectSelectors.datasetQualityExpandButton}]`
         );
         expect(expanderButtons.length).to.be.greaterThan(0);
-
         await expanderButtons[testDatasetRowIndex].click(); // Click "Open"
+        await testSubjects.existOrFail(testSubjectSelectors.euiFlyoutCloseButton);
       });
     },
 
