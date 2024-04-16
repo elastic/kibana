f3ddba77b4e05d0345aa137351c6d6774a83390d
Dima Arnautov <dmitrii.arnautov@elastic.co>
2024-04-16 13:30:42 +0200

    provide execution context from the dashboard container

--- src/plugins/dashboard/public/dashboard_container/embeddable/dashboard_container.tsx
+++ src/plugins/dashboard/public/dashboard_container/embeddable/dashboard_container.tsx
@@ -148,6 +148,8 @@ export class DashboardContainer
   public timeslice$: BehaviorSubject<[number, number] | undefined>;
   public locator?: Pick<LocatorPublic<DashboardLocatorParams>, 'navigate' | 'getRedirectUrl'>;
 
+  public readonly executionContext: KibanaExecutionContext;
+
   // cleanup
   public stopSyncingWithUnifiedSearch?: () => void;
   private cleanupStateTools: () => void;
@@ -295,6 +297,11 @@ export class DashboardContainer
       string | undefined,
       DashboardContainerInput
     >(this.publishingSubscription, this, 'lastReloadRequestTime');
+
+    this.executionContext = {
+      type: DASHBOARD_CONTAINER_TYPE,
+      description: initialInput.title,
+    };
   }
 
   public getAppContext() {
--- x-pack/plugins/ml/public/embeddables/anomaly_swimlane/anomaly_swimlane_embeddable_factory.tsx
+++ x-pack/plugins/ml/public/embeddables/anomaly_swimlane/anomaly_swimlane_embeddable_factory.tsx
@@ -14,6 +14,7 @@ import { FormattedMessage } from '@kbn/i18n-react';
 import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
 import { useTimeBuckets } from '@kbn/ml-time-buckets';
 import {
+  apiHasExecutionContext,
   apiHasParentApi,
   apiPublishesTimeRange,
   initializeTimeRange,
@@ -211,11 +212,13 @@ export const getAnomalySwimLaneEmbeddableFactory = (
           const I18nContext = i18n.Context;
           const timeBuckets = useTimeBuckets(uiSettings);
 
+          if (!apiHasExecutionContext(parentApi)) {
+            throw new Error('Parent API does not have execution context');
+          }
+
           useReactEmbeddableExecutionContext(
             services[0].executionContext,
-            // TODO https://github.com/elastic/kibana/issues/180055
-            // @ts-ignore
-            parentApi?.executionContext?.value ?? { name: 'dashboard' },
+            parentApi.executionContext,
             ANOMALY_SWIMLANE_EMBEDDABLE_TYPE,
             uuid
           );
