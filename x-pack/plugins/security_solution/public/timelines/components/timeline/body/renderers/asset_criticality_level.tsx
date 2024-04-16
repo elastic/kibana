38fa0e2b504c0452e4e31027feb31d1f02f816af
machadoum <pablo.nevesmachado@elastic.co>
2024-04-16 09:59:16 +0200

    Prettify asset criticality alert column

--- x-pack/plugins/security_solution/public/timelines/components/timeline/body/renderers/asset_criticality_level.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/body/renderers/asset_criticality_level.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import { EuiBadge } from '@elastic/eui';
-import { isString } from 'lodash/fp';
+import { isString, startCase } from 'lodash/fp';
 import type { CriticalityLevel } from '../../../../../../common/entity_analytics/asset_criticality/types';
 import { CRITICALITY_LEVEL_COLOR } from '../../../../../entity_analytics/components/asset_criticality';
 import { DefaultDraggable } from '../../../../../common/components/draggables';
@@ -32,10 +32,11 @@ const AssetCriticalityLevelComponent: React.FC<Props> = ({
   value,
 }) => {
   const color = isString(value) ? CRITICALITY_LEVEL_COLOR[value as CriticalityLevel] : 'normal';
+  const stringValue = isString(value) ? value : '';
 
   const badge = (
     <EuiBadge color={color} data-test-subj="AssetCriticalityLevel-score-badge">
-      {value}
+      {startCase(stringValue)}
     </EuiBadge>
   );
 
