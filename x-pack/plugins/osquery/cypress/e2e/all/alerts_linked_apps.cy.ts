f9a8088f67434afe62e2514f15e82c4c0c885490
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-04-15 22:52:58 -0400

    skip failing test suite (#180852)

--- x-pack/plugins/osquery/cypress/e2e/all/alerts_linked_apps.cy.ts
+++ x-pack/plugins/osquery/cypress/e2e/all/alerts_linked_apps.cy.ts
@@ -19,84 +19,81 @@ import { closeModalIfVisible, closeToastIfVisible } from '../../tasks/integratio
 import { RESULTS_TABLE, RESULTS_TABLE_BUTTON } from '../../screens/live_query';
 
 // FLAKY: https://github.com/elastic/kibana/issues/180852
-describe.skip(
-  'Alert Event Details',
-  {
-    tags: ['@ess', '@serverless'],
-  },
-  () => {
-    let ruleId: string;
-    let ruleName: string;
-    before(() => {
-      initializeDataViews();
+// Failing: See https://github.com/elastic/kibana/issues/180852
+describe.skip('Alert Event Details', {
+  tags: ['@ess', '@serverless'],
+}, () => {
+  let ruleId: string;
+  let ruleName: string;
+  before(() => {
+    initializeDataViews();
+  });
+  beforeEach(() => {
+    loadRule().then((data) => {
+      ruleId = data.id;
+      ruleName = data.name;
+      loadRuleAlerts(data.name);
     });
-    beforeEach(() => {
-      loadRule().then((data) => {
-        ruleId = data.id;
-        ruleName = data.name;
-        loadRuleAlerts(data.name);
-      });
+  });
+
+  afterEach(() => {
+    cleanupRule(ruleId);
+  });
+
+  it('should be able to add investigation guides to response actions', () => {
+    cy.getBySel('editRuleSettingsLink').click();
+    cy.getBySel('globalLoadingIndicator').should('not.exist');
+    cy.getBySel('edit-rule-actions-tab').click();
+    cy.getBySel('osquery-investigation-guide-text').should('exist');
+    cy.getBySel('globalLoadingIndicator').should('not.exist');
+    cy.contains('Loading connectors...').should('not.exist');
+
+    cy.getBySel('osqueryAddInvestigationGuideQueries').click();
+    cy.getBySel('osquery-investigation-guide-text').should('not.exist');
+
+    cy.getBySel(RESPONSE_ACTIONS_ITEM_0).within(() => {
+      cy.contains("SELECT * FROM os_version where name='{{host.os.name}}';");
+      cy.get('input[value="host.os.platform"]').should('exist');
+      cy.contains('platform');
     });
-
-    afterEach(() => {
-      cleanupRule(ruleId);
-    });
-
-    it('should be able to add investigation guides to response actions', () => {
-      cy.getBySel('editRuleSettingsLink').click();
-      cy.getBySel('globalLoadingIndicator').should('not.exist');
-      cy.getBySel('edit-rule-actions-tab').click();
-      cy.getBySel('osquery-investigation-guide-text').should('exist');
-      cy.getBySel('globalLoadingIndicator').should('not.exist');
-      cy.contains('Loading connectors...').should('not.exist');
-
-      cy.getBySel('osqueryAddInvestigationGuideQueries').click();
-      cy.getBySel('osquery-investigation-guide-text').should('not.exist');
-
-      cy.getBySel(RESPONSE_ACTIONS_ITEM_0).within(() => {
-        cy.contains("SELECT * FROM os_version where name='{{host.os.name}}';");
-        cy.get('input[value="host.os.platform"]').should('exist');
-        cy.contains('platform');
-      });
-      cy.getBySel(RESPONSE_ACTIONS_ITEM_1).within(() => {
-        cy.contains('select * from users');
-      });
-
-      cy.contains('Save changes').click();
-      cy.contains(`${ruleName} was saved`).should('exist');
-      closeToastIfVisible();
+    cy.getBySel(RESPONSE_ACTIONS_ITEM_1).within(() => {
+      cy.contains('select * from users');
     });
 
-    it('should be able to run live query and add to timeline', () => {
-      const TIMELINE_NAME = 'Untitled timeline';
-      cy.getBySel('expand-event').first().click();
-      cy.getBySel('take-action-dropdown-btn').click();
-      cy.getBySel('osquery-action-item').click();
-      cy.contains('1 agent selected.');
-      selectAllAgents();
-      inputQueryInFlyout('select * from uptime;');
-      submitQuery();
-      checkResults();
-      cy.contains('Add to timeline investigation');
-      cy.contains('Save for later').click();
-      cy.contains('Save query');
-      cy.get('[data-test-subj="osquery-save-query-flyout"]').within(() => {
-        cy.get('.euiButtonEmpty').contains('Cancel').click();
-      });
-      cy.getBySel('add-to-timeline').first().click();
-      cy.getBySel('globalToastList').contains('Added');
-      closeToastIfVisible();
-      cy.getBySel(RESULTS_TABLE).within(() => {
-        cy.getBySel(RESULTS_TABLE_BUTTON).should('not.exist');
-      });
-      cy.contains('Cancel').click();
-      cy.getBySel('timeline-bottom-bar').within(() => {
-        cy.contains(TIMELINE_NAME).click();
-      });
-      cy.getBySel('draggableWrapperKeyboardHandler').contains('action_id: "');
-      // timeline unsaved changes modal
-      cy.visit('/app/osquery');
-      closeModalIfVisible();
+    cy.contains('Save changes').click();
+    cy.contains(`${ruleName} was saved`).should('exist');
+    closeToastIfVisible();
+  });
+
+  it('should be able to run live query and add to timeline', () => {
+    const TIMELINE_NAME = 'Untitled timeline';
+    cy.getBySel('expand-event').first().click();
+    cy.getBySel('take-action-dropdown-btn').click();
+    cy.getBySel('osquery-action-item').click();
+    cy.contains('1 agent selected.');
+    selectAllAgents();
+    inputQueryInFlyout('select * from uptime;');
+    submitQuery();
+    checkResults();
+    cy.contains('Add to timeline investigation');
+    cy.contains('Save for later').click();
+    cy.contains('Save query');
+    cy.get('[data-test-subj="osquery-save-query-flyout"]').within(() => {
+      cy.get('.euiButtonEmpty').contains('Cancel').click();
     });
-  }
-);
+    cy.getBySel('add-to-timeline').first().click();
+    cy.getBySel('globalToastList').contains('Added');
+    closeToastIfVisible();
+    cy.getBySel(RESULTS_TABLE).within(() => {
+      cy.getBySel(RESULTS_TABLE_BUTTON).should('not.exist');
+    });
+    cy.contains('Cancel').click();
+    cy.getBySel('timeline-bottom-bar').within(() => {
+      cy.contains(TIMELINE_NAME).click();
+    });
+    cy.getBySel('draggableWrapperKeyboardHandler').contains('action_id: "');
+    // timeline unsaved changes modal
+    cy.visit('/app/osquery');
+    closeModalIfVisible();
+  });
+});
