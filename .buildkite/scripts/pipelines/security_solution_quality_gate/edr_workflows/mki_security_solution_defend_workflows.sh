993d66653b287dafd3ce828dcbfec1c48d0a2714
konrad.szwarc <konrad.szwarc@elastic.co>
2024-04-17 18:08:56 +0200

    wip

--- .buildkite/scripts/pipelines/security_solution_quality_gate/edr_workflows/mki_security_solution_defend_workflows.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/edr_workflows/mki_security_solution_defend_workflows.sh
@@ -2,15 +2,31 @@
 
 set -euo pipefail
 
+if [ -z "$1" ]
+  then
+    echo "No target script from the package.json file, is supplied"
+    exit 1
+fi
+
+
 source .buildkite/scripts/common/util.sh
 source .buildkite/scripts/steps/functional/common_cypress.sh
 .buildkite/scripts/bootstrap.sh
 
 export JOB=kibana-defend-workflows-serverless-cypress
 
+buildkite-agent meta-data set "${BUILDKITE_JOB_ID}_is_test_execution_step" "true"
+
 cd x-pack/plugins/security_solution
 set +e
 
-QA_API_KEY=$(retry 5 5 vault read -field=qa_api_key secret/kibana-issues/dev/security-solution-qg-enc-key)
+if [ -z "${KIBANA_MKI_USE_LATEST_COMMIT+x}" ] || [ "$KIBANA_MKI_USE_LATEST_COMMIT" = "0" ]; then
+    KIBANA_OVERRIDE_FLAG=0
+else
+    KIBANA_OVERRIDE_FLAG=1
+fi
 
-CLOUD_QA_API_KEY=$QA_API_KEY yarn cypress:dw:qa:serverless:run; status=$?; yarn junit:merge || :; exit $status
+QA_API_KEY=$(vault_get security-solution-quality-gate qa_api_key)
+QA_CONSOLE_URL=$(vault_get security-solution-quality-gate qa_console_url)
+
+QA_CONSOLE_URL=$QA_CONSOLE_URL KIBANA_MKI_USE_LATEST_COMMIT=$KIBANA_OVERRIDE_FLAG BK_ANALYTICS_API_KEY=$BK_ANALYTICS_API_KEY CLOUD_QA_API_KEY=$QA_API_KEY yarn $1; status=$?; yarn junit:merge || :; exit $status
