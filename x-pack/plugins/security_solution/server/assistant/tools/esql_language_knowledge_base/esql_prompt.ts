d473595fc26d2df0134d3d01cae3d5922023789b
Steph Milovic <stephanie.milovic@elastic.co>
2024-04-17 12:51:58 -0500

    wip

--- x-pack/plugins/security_solution/server/assistant/tools/esql_language_knowledge_base/esql_prompt.ts
+++ x-pack/plugins/security_solution/server/assistant/tools/esql_language_knowledge_base/esql_prompt.ts
@@ -6,7 +6,7 @@
  */
 
 export const getEsqlPrompt = (xxx: string) => `
-Question: ${xxx}
+${xxx}
 
-Only return valid ES|QL queries as described above. Do not add any additional text to describe your output. Properly escape the ES|QL output for use within JSON.  Escape backslashes to respect JSON validation. New lines must always be escaped with double backslashes, i.e. \\\\n to ensure valid JSON.
+Only return valid ES|QL queries as described above. Do not add any additional text to describe your output. ES|QL query output should be on one line. Escape any special characters in the output for use within JSON. Escape backslashes to respect JSON validation. New lines must always be escaped with double backslashes, i.e. \\\\n to ensure valid JSON.
  `;
