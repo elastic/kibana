f7ddc3e9b2a9b9fe7d6ad041c2e850336da99edd
vadimkibana <vadimkibana@gmail.com>
2024-04-17 16:57:11 +0200

    create a prewired version

--- packages/cloud/connection_details/kibana/index.ts
+++ packages/cloud/connection_details/kibana/index.ts
@@ -10,4 +10,13 @@ export {
   KibanaConnectionDetailsProvider,
   type KibanaConnectionDetailsProviderProps,
 } from './kibana_connection_details_provider';
+export { setGlobalDependencies } from './global';
+export {
+  KibanaWiredConnectionDetailsProvider,
+  type KibanaWiredConnectionDetailsProviderProps,
+} from './kibana_wired_connection_details_provider';
 export { openConnectionDetails, type OpenConnectionDetailsParams } from './open_connection_details';
+export {
+  openWiredConnectionDetails,
+  type OpenWiredConnectionDetailsParams,
+} from './open_wired_connection_details';
