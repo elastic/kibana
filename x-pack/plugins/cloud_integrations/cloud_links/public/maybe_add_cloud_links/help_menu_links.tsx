5d727debdace7105903b1ef93df7cfd45cc97b8d
vadimkibana <vadimkibana@gmail.com>
2024-04-17 13:01:40 +0200

    open flyout from help link

--- x-pack/plugins/cloud_integrations/cloud_links/public/maybe_add_cloud_links/help_menu_links.tsx
+++ x-pack/plugins/cloud_integrations/cloud_links/public/maybe_add_cloud_links/help_menu_links.tsx
@@ -4,16 +4,15 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import React from 'react';
+
 import { i18n } from '@kbn/i18n';
 import { ChromeHelpMenuLink } from '@kbn/core-chrome-browser';
 import type { DocLinksStart } from '@kbn/core-doc-links-browser';
 import type { CoreStart } from '@kbn/core/public';
 import type { CloudStart } from '@kbn/cloud-plugin/public';
 import type { SharePluginStart } from '@kbn/share-plugin/public';
-import { toMountPoint } from '@kbn/react-kibana-mount';
 
-import { ConnectionDetailsModal } from './connection_details_modal';
+import { openConnectionDetails } from '@kbn/cloud/connection_details';
 
 export const createHelpMenuLinks = ({
   docLinks,
@@ -28,8 +27,6 @@ export const createHelpMenuLinks = ({
   share: SharePluginStart;
   helpSupportUrl: string;
 }) => {
-  const { overlays } = core;
-
   const helpMenuLinks: ChromeHelpMenuLink[] = [
     {
       title: i18n.translate('xpack.cloudLinks.helpMenuLinks.documentation', {
@@ -55,18 +52,20 @@ export const createHelpMenuLinks = ({
       }),
       dataTestSubj: 'connectionDetailsHelpLink',
       onClick: () => {
-        const modal = overlays.openModal(
-          toMountPoint(
-            <ConnectionDetailsModal
-              core={core}
-              share={share}
-              cloud={cloud}
-              docLinks={docLinks}
-              closeModal={() => modal.close()}
-            />,
-            { theme: core.theme, i18n: core.i18n }
-          )
-        );
+        openConnectionDetails({
+          props: {
+            start: {
+              core,
+              plugins: {
+                share,
+                cloud,
+              },
+            },
+          },
+          start: {
+            core,
+          },
+        });
       },
     },
   ];
