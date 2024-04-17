5dff67a6ee24d4ff6ee8ca5fba96e8582001fc82
jaredburgettelastic <jared.burgett@elastic.co>
2024-04-17 10:27:51 -0500

    Fixed comment spacing

--- x-pack/plugins/security_solution/public/entity_analytics/pages/entity_analytics_management_page.tsx
+++ x-pack/plugins/security_solution/public/entity_analytics/pages/entity_analytics_management_page.tsx
@@ -39,7 +39,7 @@ export const EntityAnalyticsManagementPage = () => {
           <RiskScorePreviewSection privileges={privileges} />
         </EuiFlexItem>
       </EuiFlexGroup>
-    {/*  Comment to trigger CI build */}
+      {/*  Comment to trigger CI build */}
     </>
   );
 };
