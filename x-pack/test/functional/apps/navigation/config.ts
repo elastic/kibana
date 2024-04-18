0896eb120ee98cf317691b6389e4afea6927722a
Sébastien Loix <sebastien.loix@elastic.co>
2024-04-18 15:28:45 +0200

    Revert changes to config.ts

--- x-pack/test/functional/apps/navigation/config.ts
+++ x-pack/test/functional/apps/navigation/config.ts
@@ -36,7 +36,6 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
       ...kibanaFunctionalConfig.get('kbnTestServer'),
       serverArgs: [
         ...kibanaFunctionalConfig.get('kbnTestServer.serverArgs'),
-        '--env.name=development',
         '--xpack.cloud_integrations.experiments.enabled=true',
         '--xpack.cloud_integrations.experiments.launch_darkly.sdk_key=a_string',
         '--xpack.cloud_integrations.experiments.launch_darkly.client_id=a_string',
