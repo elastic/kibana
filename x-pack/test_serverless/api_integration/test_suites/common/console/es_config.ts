6152273bc8a32742fc3359146dd30db380b86f87
Tre' Seymour <wayne.seymour@elastic.co>
2024-04-18 13:15:10 +0100

    [FTR] Update common serverless console API tests to use API keys

--- x-pack/test_serverless/api_integration/test_suites/common/console/es_config.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/console/es_config.ts
@@ -7,19 +7,25 @@
 
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
+import { RoleCredentials } from '../../../../shared/services';
 
 export default function ({ getService }: FtrProviderContext) {
-  const supertest = getService('supertest');
+  const supertestWithoutAuth = getService('supertestWithoutAuth');
+
   const svlCommonApi = getService('svlCommonApi');
+  const svlUserManager = getService('svlUserManager');
 
   describe('GET /api/console/es_config', () => {
     it('returns es host', async () => {
-      const { body } = await supertest
+      const roleAuthc: RoleCredentials = await svlUserManager.createApiKeyForRole('viewer');
+      const { body } = await supertestWithoutAuth
         .get('/api/console/es_config')
         .set('kbn-xsrf', 'true')
         .set(svlCommonApi.getInternalRequestHeader())
+        .set(roleAuthc.apiKeyHeader)
         .expect(200);
       expect(body.host).to.be.ok();
+      await svlUserManager.invalidateApiKeyForRole(roleAuthc);
     });
   });
 }
