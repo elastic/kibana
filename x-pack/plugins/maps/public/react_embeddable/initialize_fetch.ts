3446e9fbbdc2d4ebc0276c05c5eca77f24534efa
Nathan Reese <reese.nathan@gmail.com>
2024-04-21 13:33:50 -0600

    fetch$

--- x-pack/plugins/maps/public/react_embeddable/initialize_fetch.ts
+++ x-pack/plugins/maps/public/react_embeddable/initialize_fetch.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { FetchContext, onFetchContextChanged } from '@kbn/presentation-publishing';
+import { FetchContext, fetch$ } from '@kbn/presentation-publishing';
 import { Query } from '@kbn/es-query';
 import { MapExtent } from '../../common/descriptor_types';
 import { getSearchService } from '../kibana_services';
@@ -35,49 +35,46 @@ export function initializeFetch({
   store: MapStore;
 }) {
   let prevIsRestore: boolean | undefined;
-  return onFetchContextChanged({
-    api,
-    onFetch: (fetchContext: FetchContext) => {
-      // New search session id causes all layers from elasticsearch to refetch data.
-      // Dashboard provides a new search session id anytime filters change.
-      // Thus, filtering embeddable container by map extent causes a new search session id any time the map is moved.
-      // Disabling search session when filtering embeddable container by map extent.
-      // The use case for search sessions (restoring results because of slow responses) does not match the use case of
-      // filtering by map extent (rapid responses as users explore their map).
-      const searchSessionId = getIsFilterByMapExtent() ? undefined : fetchContext.searchSessionId;
-      const isRestore = getIsRestore(searchSessionId);
+  const fetchSubscription = fetch$(api).subscribe((fetchContext: FetchContext) => {
+    // New search session id causes all layers from elasticsearch to refetch data.
+    // Dashboard provides a new search session id anytime filters change.
+    // Thus, filtering embeddable container by map extent causes a new search session id any time the map is moved.
+    // Disabling search session when filtering embeddable container by map extent.
+    // The use case for search sessions (restoring results because of slow responses) does not match the use case of
+    // filtering by map extent (rapid responses as users explore their map).
+    const searchSessionId = getIsFilterByMapExtent() ? undefined : fetchContext.searchSessionId;
+    const isRestore = getIsRestore(searchSessionId);
 
-      // Map can not be interacted with when viewing session restore.
-      // Session restore only show data for cached extent and new data can not be fetch
-      if (isRestore !== prevIsRestore) {
-        prevIsRestore = isRestore;
-        store.dispatch(
-          setMapSettings({
-            disableInteractive: isRestore,
-            hideToolbarOverlay: isRestore,
-          })
-        );
-      }
-
-      store.dispatch<any>(
-        setQuery({
-          filters: fetchContext.filters
-            ? fetchContext.filters.filter(
-                (filter) => !filter.meta.disabled && filter.meta.controlledBy !== controlledBy
-              )
-            : [],
-          query: fetchContext.query as Query | undefined,
-          timeFilters: fetchContext.timeRange,
-          timeslice: fetchContext.timeslice
-            ? { from: fetchContext.timeslice[0], to: fetchContext.timeslice[1] }
-            : undefined,
-          clearTimeslice: fetchContext.timeslice === undefined,
-          forceRefresh: fetchContext.isReload,
-          searchSessionId,
-          searchSessionMapBuffer: isRestore ? searchSessionMapBuffer : undefined,
+    // Map can not be interacted with when viewing session restore.
+    // Session restore only show data for cached extent and new data can not be fetch
+    if (isRestore !== prevIsRestore) {
+      prevIsRestore = isRestore;
+      store.dispatch(
+        setMapSettings({
+          disableInteractive: isRestore,
+          hideToolbarOverlay: isRestore,
         })
       );
-    },
-    fetchOnSetup: true,
+    }
+
+    store.dispatch<any>(
+      setQuery({
+        filters: fetchContext.filters
+          ? fetchContext.filters.filter(
+              (filter) => !filter.meta.disabled && filter.meta.controlledBy !== controlledBy
+            )
+          : [],
+        query: fetchContext.query as Query | undefined,
+        timeFilters: fetchContext.timeRange,
+        timeslice: fetchContext.timeslice
+          ? { from: fetchContext.timeslice[0], to: fetchContext.timeslice[1] }
+          : undefined,
+        clearTimeslice: fetchContext.timeslice === undefined,
+        forceRefresh: fetchContext.isReload,
+        searchSessionId,
+        searchSessionMapBuffer: isRestore ? searchSessionMapBuffer : undefined,
+      })
+    );
   });
+  return fetchSubscription.unsubscribe;
 }
