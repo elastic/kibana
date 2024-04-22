18661b29836af6da31d19b60b7ec7085bbb113e8
Ashokaditya <ashokaditya@elastic.co>
2024-04-22 11:00:58 +0200

    show `observer.serial_number` on table view
    For sentinel one alerts, instead of showing agent status on table view for `observerl.serial_number` show the serial_number info.
    
    closes elastic/security-team/issues/8896

--- x-pack/plugins/security_solution/public/timelines/components/timeline/body/renderers/formatted_field.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/body/renderers/formatted_field.tsx
@@ -9,17 +9,15 @@
 
 import type { EuiButtonEmpty, EuiButtonIcon } from '@elastic/eui';
 import { EuiFlexGroup, EuiFlexItem, EuiToolTip } from '@elastic/eui';
-import { isNumber, isEmpty } from 'lodash/fp';
+import { isEmpty, isNumber } from 'lodash/fp';
 import React from 'react';
 import { css } from '@emotion/css';
 
-import type { BrowserField } from '../../../../../common/containers/source';
+import type { BrowserField } from '@kbn/timelines-plugin/common';
 import {
   ALERT_HOST_CRITICALITY,
   ALERT_USER_CRITICALITY,
 } from '../../../../../../common/field_maps/field_names';
-import { SENTINEL_ONE_AGENT_ID_FIELD } from '../../../../../common/utils/sentinelone_alert_check';
-import { SentinelOneAgentStatus } from '../../../../../detections/components/host_isolation/sentinel_one_agent_status';
 import { EndpointAgentStatusById } from '../../../../../common/components/endpoint/endpoint_agent_status';
 import { INDICATOR_REFERENCE } from '../../../../../../common/cti/constants';
 import { DefaultDraggable } from '../../../../../common/components/draggables';
@@ -32,21 +30,21 @@ import { Port } from '../../../../../explore/network/components/port';
 import { PORT_NAMES } from '../../../../../explore/network/components/port/helpers';
 import { TruncatableText } from '../../../../../common/components/truncatable_text';
 import {
+  AGENT_STATUS_FIELD_NAME,
   DATE_FIELD_TYPE,
+  EVENT_MODULE_FIELD_NAME,
+  EVENT_URL_FIELD_NAME,
+  GEO_FIELD_TYPE,
   HOST_NAME_FIELD_NAME,
-  USER_NAME_FIELD_NAME,
   IP_FIELD_TYPE,
   MESSAGE_FIELD_NAME,
-  EVENT_MODULE_FIELD_NAME,
+  REFERENCE_URL_FIELD_NAME,
   RULE_REFERENCE_FIELD_NAME,
   SIGNAL_RULE_NAME_FIELD_NAME,
-  REFERENCE_URL_FIELD_NAME,
-  EVENT_URL_FIELD_NAME,
   SIGNAL_STATUS_FIELD_NAME,
-  AGENT_STATUS_FIELD_NAME,
-  GEO_FIELD_TYPE,
+  USER_NAME_FIELD_NAME,
 } from './constants';
-import { RenderRuleName, renderEventModule, renderUrl } from './formatted_field_helpers';
+import { renderEventModule, RenderRuleName, renderUrl } from './formatted_field_helpers';
 import { RuleStatus } from './rule_status';
 import { HostName } from './host_name';
 import { UserName } from './user_name';
@@ -269,11 +267,6 @@ const FormattedFieldValueComponent: React.FC<{
         iconSide={isButton ? 'right' : undefined}
       />
     );
-  } else if (
-    fieldName === SENTINEL_ONE_AGENT_ID_FIELD ||
-    fieldFromBrowserField?.name === SENTINEL_ONE_AGENT_ID_FIELD
-  ) {
-    return <SentinelOneAgentStatus agentId={String(value ?? '')} />;
   } else if (fieldName === ALERT_HOST_CRITICALITY || fieldName === ALERT_USER_CRITICALITY) {
     return (
       <AssetCriticalityLevel
