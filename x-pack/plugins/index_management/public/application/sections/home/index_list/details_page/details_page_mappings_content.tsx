0146093d9c195f698cb7a0d181ebe82885f8410a
Saikat Sarkar <saikat.sarkar@elastic.co>
2024-04-22 12:42:01 -0600

    Resolve merge-conflicts

--- x-pack/plugins/index_management/public/application/sections/home/index_list/details_page/details_page_mappings_content.tsx
+++ x-pack/plugins/index_management/public/application/sections/home/index_list/details_page/details_page_mappings_content.tsx
@@ -81,7 +81,7 @@ export const DetailsPageMappingsContent: FunctionComponent<{
   jsonData,
   refetchMapping,
   showAboutMappings,
-  isSemanticTextEnabled = false,
+  isSemanticTextEnabled = true,
 }) => {
   const {
     services: { extensionsService },
