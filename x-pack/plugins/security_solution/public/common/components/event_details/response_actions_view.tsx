d57e29c24d7bc821b08c2a456a275e72eb43b159
Angela Chuang <yi-chun.chuang@elastic.co>
2024-04-22 13:01:33 +0100

    fix js error

--- x-pack/plugins/security_solution/public/common/components/event_details/response_actions_view.tsx
+++ x-pack/plugins/security_solution/public/common/components/event_details/response_actions_view.tsx
@@ -80,7 +80,7 @@ export const useResponseActionsView = <T extends object = JSX.Element>({
     : undefined;
 
   const responseActions =
-    expandedEventFieldsObject?.kibana?.alert?.rule?.parameters?.[0].response_actions;
+    expandedEventFieldsObject?.kibana?.alert?.rule?.parameters?.[0]?.response_actions;
   const shouldEarlyReturn = !rawEventData || !responseActionsEnabled;
 
   const alertId = rawEventData?._id ?? '';
