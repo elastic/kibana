e3d1a7fe4e8ebb22b4bd36a9c35e69702eaded16
Timothy Sullivan <tsullivan@elastic.co>
2024-04-22 13:01:30 -0700

    Presentation] Remove usage of deprecated React rendering utilities

--- src/plugins/dashboard/public/plugin.tsx
+++ src/plugins/dashboard/public/plugin.tsx
@@ -257,6 +257,7 @@ export class DashboardPlugin
         const { mountApp } = await import('./dashboard_app/dashboard_router');
         appMounted();
 
+        const [coreStart] = await core.getStartServices();
         const mountContext: DashboardMountContextProps = {
           restorePreviousUrl,
           scopedHistory: () => this.currentHistory!,
@@ -265,7 +266,7 @@ export class DashboardPlugin
         };
 
         return mountApp({
-          core,
+          coreStart,
           appUnMounted,
           element: params.element,
           mountContext,
