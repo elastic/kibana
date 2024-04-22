elastic/kibana

commit 51cc9cade2fc6480a7d23541ecf1bdf415ddd59a

[Security Solution] Add guided tour to document details flyout (#180318)
## Summary

This PR adds a guided tour for the new expandable flyout in alerts
table.

Where it will show up:
✅ Alerts page
✅ Alerts tab in Cases

Tour will not show up in:
❌ rule creation
❌ flyout in timeline
❌ event table.


https://github.com/elastic/kibana/assets/18648970/e9d0ce92-0eb4-4898-ad05-91d701aec01d



**How to test**
- Generate some alerts and go to Alerts page
- Expand a row in alerts table
- Guided tour should appear
- Note that rule preview is only available to alerts. Guided tour for an
event or alert preview does not have that step.

To test guided tour in event and timeline, enable
`expandableEventFlyoutEnabled`, `expandableTimelineFlyoutEnabled`
respectively.

### Checklist

- [x] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/plugins/security_solution/public/flyout/document_details/shared/components/flyout_tour.tsx
@@ -1,0 +1,160 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+/*
+ * This timeline tour only valid for 8.12 release is not needed for 8.13
+ *
+ * */
+
+import type { FC } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
+import { EuiButton, EuiButtonEmpty, EuiTourStep } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { tourConfig, type TourState } from '../utils/tour_step_config';
+import type { FlyoutTourStepsProps } from '../utils/tour_step_config';
+import { NEW_FEATURES_TOUR_STORAGE_KEYS } from '../../../../../common/constants';
+import { useKibana } from '../../../../common/lib/kibana';
+import { FLYOUT_TOUR_TEST_ID } from './test_ids';
+
+export interface FlyoutTourProps {
+  /**
+   * Content to be displayed in each tour card
+   */
+  tourStepContent: FlyoutTourStepsProps[];
+  /**
+   * Total number of tour steps
+   */
+  totalSteps: number;
+  /**
+   * Callback to go to overview tab before tour
+   */
+  goToOverviewTab?: () => void;
+  /**
+   * Callback to go to open left panel
+   */
+  goToLeftPanel?: () => void;
+}
+
+const MAX_POPOVER_WIDTH = 500;
+const TOUR_SUBTITLE = i18n.translate('xpack.securitySolution.flyout.tour.subtitle', {
+  defaultMessage: 'A redesigned alert experience',
+});
+
+/**
+ * Shared component that generates tour steps based on supplied tour step content.
+ * Supports tours being shown in different panels and manages state via local storage
+ */
+export const FlyoutTour: FC<FlyoutTourProps> = ({
+  tourStepContent,
+  totalSteps,
+  goToOverviewTab,
+  goToLeftPanel,
+}) => {
+  const {
+    services: { storage },
+  } = useKibana();
+
+  const [tourState, setTourState] = useState<TourState>(() => {
+    const restoredTourState = storage.get(NEW_FEATURES_TOUR_STORAGE_KEYS.FLYOUT);
+    if (restoredTourState != null) {
+      return restoredTourState;
+    }
+    return tourConfig;
+  });
+
+  useEffect(() => {
+    storage.set(NEW_FEATURES_TOUR_STORAGE_KEYS.FLYOUT, tourState);
+    if (tourState.currentTourStep === 1 && goToOverviewTab) {
+      goToOverviewTab();
+    }
+  }, [storage, tourState, goToOverviewTab]);
+
+  const nextStep = useCallback(() => {
+    setTourState((prev) => {
+      if (prev.currentTourStep === 3 && goToLeftPanel) {
+        goToLeftPanel();
+      }
+      return {
+        ...prev,
+        currentTourStep: prev.currentTourStep + 1,
+      };
+    });
+  }, [goToLeftPanel]);
+
+  const finishTour = useCallback(() => {
+    setTourState((prev) => {
+      return {
+        ...prev,
+        isTourActive: false,
+      };
+    });
+  }, []);
+
+  const getFooterAction = useCallback(
+    (step: number) => {
+      // if it's the last step, we don't want to show the next button
+      return step === totalSteps ? (
+        <EuiButton color="success" size="s" onClick={finishTour}>
+          {i18n.translate('xpack.securitySolution.flyout.tour.finish.text', {
+            defaultMessage: 'Finish',
+          })}
+        </EuiButton>
+      ) : (
+        [
+          <EuiButtonEmpty size="s" color="text" onClick={finishTour}>
+            {i18n.translate('xpack.securitySolution.flyout.tour.exit.text', {
+              defaultMessage: 'Exit',
+            })}
+          </EuiButtonEmpty>,
+          <EuiButton color="success" size="s" onClick={nextStep}>
+            {i18n.translate('xpack.securitySolution.flyout.tour.Next.text', {
+              defaultMessage: 'Next',
+            })}
+          </EuiButton>,
+        ]
+      );
+    },
+    [finishTour, nextStep, totalSteps]
+  );
+
+  // Do not show tour if it is inactive
+  if (!tourState.isTourActive) {
+    return null;
+  }
+
+  return (
+    <>
+      {tourStepContent.map((steps) => {
+        const stepCount = steps.stepNumber;
+        if (tourState.currentTourStep !== stepCount) return null;
+        const panelProps = {
+          'data-test-subj': `${FLYOUT_TOUR_TEST_ID}-${stepCount}`,
+        };
+
+        return (
+          <EuiTourStep
+            panelProps={panelProps}
+            key={stepCount}
+            step={stepCount}
+            isStepOpen={tourState.isTourActive}
+            maxWidth={MAX_POPOVER_WIDTH}
+            stepsTotal={totalSteps}
+            onFinish={finishTour}
+            title={steps.title}
+            content={steps.content}
+            anchor={`[data-test-subj=${steps.anchor}]`}
+            anchorPosition={steps.anchorPosition}
+            footerAction={getFooterAction(stepCount)}
+            subtitle={TOUR_SUBTITLE}
+          />
+        );
+      })}
+    </>
+  );
+};
+
+FlyoutTour.displayName = 'FlyoutTour';

test/functional/page_objects/common_page.ts
@@ -43,6 +43,7 @@ export class CommonPageObject extends FtrService {
     const NEW_FEATURES_TOUR_STORAGE_KEYS = {
       RULE_MANAGEMENT_PAGE: 'securitySolution.rulesManagementPage.newFeaturesTour.v8.9',
       TIMELINE: 'securitySolution.timeline.newFeaturesTour.v8.12',
+      FLYOUT: 'securitySolution.documentDetails.newFeaturesTour.v8.14',
     };
 
     const tourStorageKeys = Object.values(NEW_FEATURES_TOUR_STORAGE_KEYS);

x-pack/plugins/fleet/cypress/tasks/common.ts
@@ -51,6 +51,7 @@ const NEW_FEATURES_TOUR_STORAGE_KEYS = {
   RULE_MANAGEMENT_PAGE: 'securitySolution.rulesManagementPage.newFeaturesTour.v8.9',
   TIMELINES: 'securitySolution.security.timelineFlyoutHeader.saveTimelineTour',
   TIMELINE: 'securitySolution.timeline.newFeaturesTour.v8.12',
+  FLYOUT: 'securitySolution.documentDetails.newFeaturesTour.v8.14',
 };
 
 const disableNewFeaturesTours = (window: Window) => {

x-pack/plugins/security_solution/common/constants.ts
@@ -426,6 +426,7 @@ export const NEW_FEATURES_TOUR_STORAGE_KEYS = {
   RULE_MANAGEMENT_PAGE: 'securitySolution.rulesManagementPage.newFeaturesTour.v8.13',
   TIMELINES: 'securitySolution.security.timelineFlyoutHeader.saveTimelineTour',
   TIMELINE: 'securitySolution.timeline.newFeaturesTour.v8.12',
+  FLYOUT: 'securitySolution.documentDetails.newFeaturesTour.v8.14',
 };
 
 export const RULE_DETAILS_EXECUTION_LOG_TABLE_SHOW_METRIC_COLUMNS_STORAGE_KEY =

x-pack/plugins/security_solution/public/flyout/document_details/left/components/tour.test.tsx
@@ -1,0 +1,121 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
+import { LeftPanelTour } from './tour';
+import { LeftPanelContext } from '../context';
+import { mockContextValue } from '../mocks/mock_context';
+import {
+  createMockStore,
+  createSecuritySolutionStorageMock,
+  TestProviders,
+} from '../../../../common/mock';
+import { useKibana as mockUseKibana } from '../../../../common/lib/kibana/__mocks__';
+import { useKibana } from '../../../../common/lib/kibana';
+import { FLYOUT_TOUR_CONFIG_ANCHORS } from '../../shared/utils/tour_step_config';
+import { useIsTimelineFlyoutOpen } from '../../shared/hooks/use_is_timeline_flyout_open';
+import { FLYOUT_TOUR_TEST_ID } from '../../shared/components/test_ids';
+
+jest.mock('../../../../common/lib/kibana');
+jest.mock('../../shared/hooks/use_is_timeline_flyout_open');
+
+const mockedUseKibana = mockUseKibana();
+
+const { storage: storageMock } = createSecuritySolutionStorageMock();
+const mockStore = createMockStore(undefined, undefined, undefined, {
+  ...storageMock,
+});
+
+const renderLeftPanelTour = (context: LeftPanelContext = mockContextValue) =>
+  render(
+    <TestProviders store={mockStore}>
+      <LeftPanelContext.Provider value={context}>
+        <LeftPanelTour />
+        {Object.values(FLYOUT_TOUR_CONFIG_ANCHORS).map((i, idx) => (
+          <div key={idx} data-test-subj={i} />
+        ))}
+      </LeftPanelContext.Provider>
+    </TestProviders>
+  );
+
+describe('<LeftPanelTour />', () => {
+  beforeEach(() => {
+    (useKibana as jest.Mock).mockReturnValue({
+      ...mockedUseKibana,
+      services: {
+        ...mockedUseKibana.services,
+        storage: storageMock,
+      },
+    });
+    (useIsTimelineFlyoutOpen as jest.Mock).mockReturnValue(false);
+
+    storageMock.clear();
+  });
+
+  it('should render left panel tour for alerts starting as step 4', async () => {
+    storageMock.set('securitySolution.documentDetails.newFeaturesTour.v8.14', {
+      currentTourStep: 4,
+      isTourActive: true,
+    });
+
+    const { getByText, getByTestId } = renderLeftPanelTour();
+    await waitFor(() => {
+      expect(getByTestId(`${FLYOUT_TOUR_TEST_ID}-4`)).toBeVisible();
+    });
+    fireEvent.click(getByText('Next'));
+    await waitFor(() => {
+      expect(getByTestId(`${FLYOUT_TOUR_TEST_ID}-5`)).toBeVisible();
+    });
+    await waitFor(() => {
+      expect(getByText('Finish')).toBeVisible();
+    });
+  });
+
+  it('should not render left panel tour for preview', () => {
+    storageMock.set('securitySolution.documentDetails.newFeaturesTour.v8.14', {
+      currentTourStep: 3,
+      isTourActive: true,
+    });
+
+    const { queryByTestId, queryByText } = renderLeftPanelTour({
+      ...mockContextValue,
+      isPreview: true,
+    });
+    expect(queryByTestId(`${FLYOUT_TOUR_TEST_ID}-4`)).not.toBeInTheDocument();
+    expect(queryByText('Next')).not.toBeInTheDocument();
+  });
+
+  it('should not render left panel tour for non-alerts', async () => {
+    storageMock.set('securitySolution.documentDetails.newFeaturesTour.v8.14', {
+      currentTourStep: 3,
+      isTourActive: true,
+    });
+
+    const { queryByTestId, queryByText } = renderLeftPanelTour({
+      ...mockContextValue,
+      getFieldsData: () => '',
+    });
+    expect(queryByTestId(`${FLYOUT_TOUR_TEST_ID}-4`)).not.toBeInTheDocument();
+    expect(queryByText('Next')).not.toBeInTheDocument();
+  });
+
+  it('should not render left panel tour for flyout in timeline', () => {
+    (useIsTimelineFlyoutOpen as jest.Mock).mockReturnValue(true);
+    storageMock.set('securitySolution.documentDetails.newFeaturesTour.v8.14', {
+      currentTourStep: 3,
+      isTourActive: true,
+    });
+
+    const { queryByTestId, queryByText } = renderLeftPanelTour({
+      ...mockContextValue,
+      isPreview: true,
+    });
+    expect(queryByTestId(`${FLYOUT_TOUR_TEST_ID}-4`)).not.toBeInTheDocument();
+    expect(queryByText('Next')).not.toBeInTheDocument();
+  });
+});

x-pack/plugins/security_solution/public/flyout/document_details/left/components/tour.tsx
@@ -1,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { FC } from 'react';
+import React, { memo, useMemo } from 'react';
+import { getField } from '../../shared/utils';
+import { EventKind } from '../../shared/constants/event_kinds';
+import { useLeftPanelContext } from '../context';
+import { FlyoutTour } from '../../shared/components/flyout_tour';
+import { getLeftSectionTourSteps } from '../../shared/utils/tour_step_config';
+import { useIsTimelineFlyoutOpen } from '../../shared/hooks/use_is_timeline_flyout_open';
+
+/**
+ * Guided tour for the left panel in details flyout
+ */
+export const LeftPanelTour: FC = memo(() => {
+  const { getFieldsData, isPreview } = useLeftPanelContext();
+  const eventKind = getField(getFieldsData('event.kind'));
+  const isAlert = eventKind === EventKind.signal;
+  const isTimelineFlyoutOpen = useIsTimelineFlyoutOpen();
+  const showTour = isAlert && !isPreview && !isTimelineFlyoutOpen;
+
+  const tourStepContent = useMemo(() => getLeftSectionTourSteps(), []);
+
+  return showTour ? <FlyoutTour tourStepContent={tourStepContent} totalSteps={5} /> : null;
+});
+
+LeftPanelTour.displayName = 'LeftPanelTour';

x-pack/plugins/security_solution/public/flyout/document_details/left/index.tsx
@@ -17,6 +17,7 @@ import * as tabs from './tabs';
 import { getField } from '../shared/utils';
 import { EventKind } from '../shared/constants/event_kinds';
 import { useLeftPanelContext } from './context';
+import { LeftPanelTour } from './components/tour';
 
 export type LeftPanelPaths = 'visualize' | 'insights' | 'investigation' | 'response';
 export const DocumentDetailsLeftPanelKey: LeftPanelProps['key'] = 'document-details-left';
@@ -76,6 +77,7 @@ export const LeftPanel: FC<Partial<LeftPanelProps>> = memo(({ path }) => {
 
   return (
     <>
+      <LeftPanelTour />
       <PanelHeader
         selectedTabId={selectedTabId}
         setSelectedTabId={setSelectedTabId}

x-pack/plugins/security_solution/public/flyout/document_details/left/tabs/insights_tab.tsx
@@ -14,8 +14,10 @@ import { useExpandableFlyoutApi, useExpandableFlyoutState } from '@kbn/expandabl
 import { useKibana } from '../../../../common/lib/kibana';
 import {
   INSIGHTS_TAB_BUTTON_GROUP_TEST_ID,
+  INSIGHTS_TAB_ENTITIES_BUTTON_LABEL_TEST_ID,
   INSIGHTS_TAB_ENTITIES_BUTTON_TEST_ID,
   INSIGHTS_TAB_THREAT_INTELLIGENCE_BUTTON_TEST_ID,
+  INSIGHTS_TAB_PREVALENCE_BUTTON_LABEL_TEST_ID,
   INSIGHTS_TAB_PREVALENCE_BUTTON_TEST_ID,
   INSIGHTS_TAB_CORRELATIONS_BUTTON_TEST_ID,
 } from './test_ids';
@@ -35,10 +37,12 @@ const insightsButtons: EuiButtonGroupOptionProps[] = [
   {
     id: ENTITIES_TAB_ID,
     label: (
-      <FormattedMessage
-        id="xpack.securitySolution.flyout.left.insights.entitiesButtonLabel"
-        defaultMessage="Entities"
-      />
+      <div data-test-subj={INSIGHTS_TAB_ENTITIES_BUTTON_LABEL_TEST_ID}>
+        <FormattedMessage
+          id="xpack.securitySolution.flyout.left.insights.entitiesButtonLabel"
+          defaultMessage="Entities"
+        />
+      </div>
     ),
     'data-test-subj': INSIGHTS_TAB_ENTITIES_BUTTON_TEST_ID,
   },
@@ -55,10 +59,12 @@ const insightsButtons: EuiButtonGroupOptionProps[] = [
   {
     id: PREVALENCE_TAB_ID,
     label: (
-      <FormattedMessage
-        id="xpack.securitySolution.flyout.left.insights.prevalenceButtonLabel"
-        defaultMessage="Prevalence"
-      />
+      <div data-test-subj={INSIGHTS_TAB_PREVALENCE_BUTTON_LABEL_TEST_ID}>
+        <FormattedMessage
+          id="xpack.securitySolution.flyout.left.insights.prevalenceButtonLabel"
+          defaultMessage="Prevalence"
+        />
+      </div>
     ),
     'data-test-subj': INSIGHTS_TAB_PREVALENCE_BUTTON_TEST_ID,
   },

commit 9ea94d10b6588c93fea9cd08bfbc73f017b0c76c

[Security Solution][Alert Details] Fix alert flyout reseting to overview tab (#181202)
## Summary

This PR fixes a bug introduced by
https://github.com/elastic/kibana/pull/180318.

If user has not completed the flyout tour and click exit. Upon refresh,
the tabs in alerts flyout always reset to overview. This is because the
tour step is stuck in step 1 and it will keep calling `goToOverview` tab
to start the tour. This PR adds additional guard to check the tour is
active before navigating.

x-pack/plugins/security_solution/public/flyout/document_details/shared/components/flyout_tour.tsx
@@ -68,7 +68,7 @@ export const FlyoutTour: FC<FlyoutTourProps> = ({
 
   useEffect(() => {
     storage.set(NEW_FEATURES_TOUR_STORAGE_KEYS.FLYOUT, tourState);
-    if (tourState.currentTourStep === 1 && goToOverviewTab) {
+    if (tourState.isTourActive && tourState.currentTourStep === 1 && goToOverviewTab) {
       goToOverviewTab();
     }
   }, [storage, tourState, goToOverviewTab]);