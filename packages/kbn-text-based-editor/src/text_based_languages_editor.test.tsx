48c8c83eb46b26b731247e2faf3b85e9b489b4fd
Elena Stoeva <elenastoeva99@gmail.com>
2024-04-23 19:04:25 +0100

    Fix failing jest tests

--- packages/kbn-text-based-editor/src/text_based_languages_editor.test.tsx
+++ packages/kbn-text-based-editor/src/text_based_languages_editor.test.tsx
@@ -59,6 +59,21 @@ describe('TextBasedLanguagesEditor', () => {
     );
   }
   let props: TextBasedLanguagesEditorProps;
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
+  });
   beforeEach(() => {
     props = {
       query: { esql: 'from test' },
--- x-pack/plugins/index_management/__jest__/client_integration/index_template_wizard/template_clone.test.tsx
+++ x-pack/plugins/index_management/__jest__/client_integration/index_template_wizard/template_clone.test.tsx
@@ -50,6 +50,20 @@ describe('<TemplateClone />', () => {
     httpRequestsMockHelpers.setLoadTelemetryResponse({});
     httpRequestsMockHelpers.setLoadComponentTemplatesResponse([]);
     httpRequestsMockHelpers.setLoadTemplateResponse(templateToClone.name, templateToClone);
+
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
   });
 
   afterAll(() => {
