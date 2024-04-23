5a16966fcb1dd705f8acb3d5ccff78ae03a09d72
Kyle Pollich <kyle.pollich@elastic.co>
2024-04-23 09:27:35 -0400

    Switch Fleet SO namespaceType to multiple-isolated

--- x-pack/plugins/fleet/server/saved_objects/index.ts
+++ x-pack/plugins/fleet/server/saved_objects/index.ts
@@ -98,7 +98,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: GLOBAL_SETTINGS_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -124,7 +124,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: AGENT_POLICY_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -185,7 +185,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: OUTPUT_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -399,7 +399,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: PACKAGE_POLICY_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -535,7 +535,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: PACKAGES_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -628,7 +628,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: ASSETS_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -648,7 +648,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: PRECONFIGURATION_DELETION_RECORD_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -662,7 +662,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: DOWNLOAD_SOURCE_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -680,7 +680,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: FLEET_SERVER_HOST_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -711,7 +711,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: FLEET_PROXY_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -731,7 +731,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: MESSAGE_SIGNING_KEYS_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: true,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
@@ -744,7 +744,7 @@ export const getSavedObjectTypes = (): { [key: string]: SavedObjectsType } => ({
     name: UNINSTALL_TOKENS_SAVED_OBJECT_TYPE,
     indexPattern: INGEST_SAVED_OBJECT_INDEX,
     hidden: true,
-    namespaceType: 'agnostic',
+    namespaceType: 'multiple-isolated',
     management: {
       importableAndExportable: false,
     },
