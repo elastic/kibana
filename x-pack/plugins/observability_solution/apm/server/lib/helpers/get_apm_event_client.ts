cfcc9c1812d18b6dea733bc256fc2d0c2282b42b
Søren Louv-Jansen <soren.louv@elastic.co>
2024-04-23 12:48:08 +0200

    [Obs AI Assistant] Address feedback for contextual insights on alert details page (#181049)
    - This PR addresses comments made in
    https://github.com/elastic/kibana/pull/180766
    - Adds API tests for `GET
    /internal/apm/assistant/get_obs_alert_details_context`
    - Fixes some bugs I found during testing
    - Removes the option `forceSyntheticSource` in the APM app since it is
    no longer needed and causes problems when applied to the traces data
    stream
    
    (cherry picked from commit 2141ba76ce4bd690dd5c96e8e41480383797615f)

--- x-pack/plugins/observability_solution/apm/server/lib/helpers/get_apm_event_client.ts
+++ x-pack/plugins/observability_solution/apm/server/lib/helpers/get_apm_event_client.ts
@@ -36,7 +36,6 @@ export async function getApmEventClient({
       indices,
       options: {
         includeFrozen,
-        forceSyntheticSource: config.forceSyntheticSource,
       },
     });
   });
