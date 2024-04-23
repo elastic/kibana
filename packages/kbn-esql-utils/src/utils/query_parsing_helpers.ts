bf6751d02b3455f4107c10b0c68484e110f7c5ed
dej611 <dej611@gmail.com>
2024-04-23 14:47:45 +0200

    :wrench: Refactor back

--- packages/kbn-esql-utils/src/utils/query_parsing_helpers.ts
+++ packages/kbn-esql-utils/src/utils/query_parsing_helpers.ts
@@ -5,7 +5,7 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-import { type ESQLSource } from '@kbn/esql-ast';
+import { getAstAndSyntaxErrors, type ESQLSource } from '@kbn/esql-ast';
 
 const DEFAULT_ESQL_LIMIT = 500;
 
@@ -28,7 +28,6 @@ export function getIndexPatternFromSQLQuery(sqlQuery?: string): string {
 
 // retrieves the index pattern from the aggregate query for ES|QL using ast parsing
 export async function getIndexPatternFromESQLQuery(esql?: string) {
-  const { getAstAndSyntaxErrors } = await import('@kbn/esql-ast');
   const { ast } = await getAstAndSyntaxErrors(esql);
   const fromCommand = ast.find(({ name }) => name === 'from');
   const args = (fromCommand?.args ?? []) as ESQLSource[];
