eb5d11f1b098b9759ccf41bfc27714379d55e65d
Kurt <kc13greiner@users.noreply.github.com>
2024-04-23 16:57:35 -0400

    Updating snapshots and expected licenses

--- x-pack/plugins/security/server/plugin.test.ts
+++ x-pack/plugins/security/server/plugin.test.ts
@@ -121,6 +121,7 @@ describe('Security Plugin', () => {
               },
             },
             "getFeatures": [Function],
+            "getLicenseType": [Function],
             "getUnavailableReason": [Function],
             "hasAtLeast": [Function],
             "isEnabled": [Function],
