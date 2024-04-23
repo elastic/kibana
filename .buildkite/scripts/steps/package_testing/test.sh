74d90414a94eb2e54e194636e6ec69cab0550158
Brad White <brad.white@elastic.co>
2024-04-23 16:26:34 -0600

    cleanup

--- .buildkite/scripts/steps/package_testing/test.sh
+++ .buildkite/scripts/steps/package_testing/test.sh
@@ -3,8 +3,6 @@
 set -euo pipefail
 
 source "$(dirname "$0")/../../common/util.sh"
-source .buildkite/scripts/steps/artifacts/env.sh
-
 .buildkite/scripts/bootstrap.sh
 
 # temporary adding this to get screenshots
