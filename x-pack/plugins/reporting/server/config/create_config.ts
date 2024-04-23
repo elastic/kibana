627d7e663872f2b8f59d27f4ec1b43e9bd689bd0
Tim Sullivan <tsullivan@users.noreply.github.com>
2024-04-22 20:25:42 -0700

    [8.11] [Reporting] update logs and docs on headless browser connection (#179882) (#179952)
    # Backport
    
    This will backport the following commits from `main` to `8.11`:
    - [[Reporting] update logs and docs on headless browser connection
    (#179882)](https://github.com/elastic/kibana/pull/179882)
    
    <!--- Backport version: 8.9.8 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Tim
    Sullivan","email":"tsullivan@users.noreply.github.com"},"sourceCommit":{"committedDate":"2024-04-03T15:15:12Z","message":"[Reporting]
    update logs and docs on headless browser connection (#179882)\n\n##
    Summary\r\n\r\nThis resolves some issues we've seen over confusion on
    configuration and\r\nrefused access to Kibana while generating a PNG/PDF
    report.","sha":"982303882ed979add12b5f60288ef46975446869","branchLabelMapping":{"^v8.14.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","docs","v8.11.0","v8.12.0","v8.13.0","v8.14.0"],"number":179882,"url":"https://github.com/elastic/kibana/pull/179882","mergeCommit":{"message":"[Reporting]
    update logs and docs on headless browser connection (#179882)\n\n##
    Summary\r\n\r\nThis resolves some issues we've seen over confusion on
    configuration and\r\nrefused access to Kibana while generating a PNG/PDF
    report.","sha":"982303882ed979add12b5f60288ef46975446869"}},"sourceBranch":"main","suggestedTargetBranches":["8.11","8.12","8.13"],"targetPullRequestStates":[{"branch":"8.11","label":"v8.11.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.12","label":"v8.12.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.13","label":"v8.13.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.14.0","labelRegex":"^v8.14.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/179882","number":179882,"mergeCommit":{"message":"[Reporting]
    update logs and docs on headless browser connection (#179882)\n\n##
    Summary\r\n\r\nThis resolves some issues we've seen over confusion on
    configuration and\r\nrefused access to Kibana while generating a PNG/PDF
    report.","sha":"982303882ed979add12b5f60288ef46975446869"}}]}]
    BACKPORT-->
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/reporting/server/config/create_config.ts
+++ x-pack/plugins/reporting/server/config/create_config.ts
@@ -44,11 +44,7 @@ export function createConfig(
     ipaddr.isValid(kibanaServerHostname) &&
     !sum(ipaddr.parse(kibanaServerHostname).toByteArray())
   ) {
-    logger.warn(
-      `Found 'server.host: "0.0.0.0"' in Kibana configuration. Reporting is not able to use this as the Kibana server hostname.` +
-        ` To enable PNG/PDF Reporting to work, 'xpack.reporting.kibanaServer.hostname: localhost' is automatically set in the configuration.` +
-        ` You can prevent this message by adding 'xpack.reporting.kibanaServer.hostname: localhost' in kibana.yml.`
-    );
+    // A silent override to use "localhost" instead of "0.0.0.0" for connection of the headless browser
     kibanaServerHostname = 'localhost';
   }
   // kibanaServer.port, default to server.port
