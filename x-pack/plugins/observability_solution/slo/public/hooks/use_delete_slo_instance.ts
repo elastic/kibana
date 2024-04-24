bc48dd6f30d8d34eace600241111431c48d628b0
Kevin Delemme <kevin.delemme@elastic.co>
2024-04-24 15:53:40 -0400

    Create hook mutation for deleting one slo instance

--- x-pack/plugins/observability_solution/slo/public/hooks/use_delete_slo_instance.ts
+++ x-pack/plugins/observability_solution/slo/public/hooks/use_delete_slo_instance.ts
@@ -7,44 +7,55 @@
 
 import { IHttpFetchError, ResponseErrorBody } from '@kbn/core/public';
 import { i18n } from '@kbn/i18n';
+import { SLOWithSummaryResponse } from '@kbn/slo-schema';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useKibana } from '../utils/kibana_react';
 import { sloKeys } from './query_key_factory';
 
 type ServerError = IHttpFetchError<ResponseErrorBody>;
 
-export function useDeleteSlo() {
+export function useDeleteSloInstance() {
   const {
     http,
     notifications: { toasts },
   } = useKibana().services;
   const queryClient = useQueryClient();
 
-  return useMutation<string, ServerError, { id: string; name: string }>(
-    ['deleteSlo'],
-    ({ id }) => {
+  return useMutation<string, ServerError, { slo: SLOWithSummaryResponse; excludeRollup: boolean }>(
+    ['deleteSloInstance'],
+    ({ slo, excludeRollup }) => {
       try {
-        return http.delete<string>(`/api/observability/slos/${id}`);
+        return http.post(`/api/observability/slos/_delete_instances`, {
+          body: JSON.stringify({
+            list: [
+              {
+                sloId: slo.id,
+                instanceId: slo.instanceId,
+                excludeRollup,
+              },
+            ],
+          }),
+        });
       } catch (error) {
         return Promise.reject(`Something went wrong: ${String(error)}`);
       }
     },
     {
-      onError: (error, { name }) => {
+      onError: (error, { slo }) => {
         toasts.addError(new Error(error.body?.message ?? error.message), {
-          title: i18n.translate('xpack.slo.slo.delete.errorNotification', {
-            defaultMessage: 'Failed to delete {name}',
-            values: { name },
+          title: i18n.translate('xpack.slo.deleteInstance.errorNotification', {
+            defaultMessage: 'Failed to delete {name} [instance = {instanceId}]',
+            values: { name: slo.name, instanceId: slo.instanceId },
           }),
         });
       },
-      onSuccess: (_data, { name }) => {
+      onSuccess: (_data, { slo }) => {
         queryClient.invalidateQueries({ queryKey: sloKeys.lists(), exact: false });
 
         toasts.addSuccess(
           i18n.translate('xpack.slo.slo.delete.successNotification', {
-            defaultMessage: 'Deleted {name}',
-            values: { name },
+            defaultMessage: 'Deleted {name} [instance = {instanceId}]',
+            values: { name: slo.name, instanceId: slo.instanceId },
           })
         );
       },
