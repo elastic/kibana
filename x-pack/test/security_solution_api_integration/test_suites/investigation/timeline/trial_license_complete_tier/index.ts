568f2fc2337440889cf2c0b475a8ce785d586ca8
Angela Chuang <yi-chun.chuang@elastic.co>
2024-04-24 17:24:23 +0100

    move security solution api integration tests

---
+++ x-pack/test/security_solution_api_integration/test_suites/investigation/timeline/trial_license_complete_tier/index.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { FtrProviderContext } from '../../../../ftr_provider_context';
+
+export default function ({ loadTestFile }: FtrProviderContext) {
+  describe('SecuritySolution Timeline', () => {
+    loadTestFile(require.resolve('./events'));
+    loadTestFile(require.resolve('./timeline'));
+    loadTestFile(require.resolve('./timeline_migrations'));
+    loadTestFile(require.resolve('./timeline_details'));
+  });
+}
