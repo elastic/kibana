00368fdd918fec9742757ec422b7a70039d8fadb
Nikita Khristinin <nkhristinin@gmail.com>
2024-04-24 17:33:05 +0200

    Enable value list modal

--- x-pack/plugins/security_solution/common/experimental_features.ts
+++ x-pack/plugins/security_solution/common/experimental_features.ts
@@ -261,7 +261,7 @@ export const allowedExperimentalValues = Object.freeze({
   /**
    * Enables the new modal for the value list items
    */
-  valueListItemsModalEnabled: false,
+  valueListItemsModalEnabled: true,
 });
 
 type ExperimentalConfigKeys = Array<keyof ExperimentalFeatures>;
