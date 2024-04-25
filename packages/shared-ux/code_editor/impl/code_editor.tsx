ec5b6fd2140cd3f082398a43f96a5dd1c2108307
Elena Stoeva <59341489+ElenaStoeva@users.noreply.github.com>
2024-04-25 13:41:22 +0100

    [Code Editor] Move theme to kbn-monaco package (#181417)
    ## Summary
    
    This PR moves the Monaco themes used in the Code Editor component to the
    `kbn-monaco` package to prevent the circular dependency between the
    `kbn-monaco` and `code_editor` packages that occurs in
    https://github.com/elastic/kibana/pull/180321 (where the code editor
    theme is reused from inside the `kbn-monaco` package).
    
    <!--
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [ ] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [ ] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    
    ### Risk Matrix
    
    Delete this section if it is not applicable to this PR.
    
    Before closing this PR, invite QA, stakeholders, and other developers to
    identify risks that should be tested prior to the change/feature
    release.
    
    When forming the risk matrix, consider some of the following examples
    and how they may potentially impact the change:
    
    | Risk | Probability | Severity | Mitigation/Notes |
    
    |---------------------------|-------------|----------|-------------------------|
    | Multiple Spaces&mdash;unexpected behavior in non-default Kibana Space.
    | Low | High | Integration tests will verify that all features are still
    supported in non-default Kibana Space and when user switches between
    spaces. |
    | Multiple nodes&mdash;Elasticsearch polling might have race conditions
    when multiple Kibana nodes are polling for the same tasks. | High | Low
    | Tasks are idempotent, so executing them multiple times will not result
    in logical error, but will degrade performance. To test for this case we
    add plenty of unit tests around this logic and document manual testing
    procedure. |
    | Code should gracefully handle cases when feature X or plugin Y are
    disabled. | Medium | High | Unit tests will verify that any feature flag
    or plugin combination still results in our service operational. |
    | [See more potential risk
    examples](https://github.com/elastic/kibana/blob/main/RISK_MATRIX.mdx) |
    
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    -->
    
    ---------
    
    Co-authored-by: Stratoula Kalafateli <efstratia.kalafateli@elastic.co>

--- packages/shared-ux/code_editor/impl/code_editor.tsx
+++ packages/shared-ux/code_editor/impl/code_editor.tsx
@@ -23,7 +23,13 @@ import {
   EuiFlexItem,
   useEuiTheme,
 } from '@elastic/eui';
-import { monaco } from '@kbn/monaco';
+import {
+  monaco,
+  CODE_EDITOR_LIGHT_THEME_ID,
+  CODE_EDITOR_DARK_THEME_ID,
+  CODE_EDITOR_LIGHT_THEME_TRANSPARENT_ID,
+  CODE_EDITOR_DARK_THEME_TRANSPARENT_ID,
+} from '@kbn/monaco';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { css } from '@emotion/react';
@@ -31,13 +37,7 @@ import './register_languages';
 import { remeasureFonts } from './remeasure_fonts';
 
 import { PlaceholderWidget } from './placeholder_widget';
-import {
-  styles,
-  DARK_THEME,
-  LIGHT_THEME,
-  DARK_THEME_TRANSPARENT,
-  LIGHT_THEME_TRANSPARENT,
-} from './editor.styles';
+import { styles } from './editor.styles';
 
 export interface CodeEditorProps {
   /** Width of editor. Defaults to 100%. */
@@ -371,19 +371,11 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
           monaco.languages.registerCodeActionProvider(languageId, codeActions);
         }
       });
-
-      // Register themes
-      monaco.editor.defineTheme('euiColors', useDarkTheme ? DARK_THEME : LIGHT_THEME);
-      monaco.editor.defineTheme(
-        'euiColorsTransparent',
-        useDarkTheme ? DARK_THEME_TRANSPARENT : LIGHT_THEME_TRANSPARENT
-      );
     },
     [
       overrideEditorWillMount,
       editorWillMount,
       languageId,
-      useDarkTheme,
       suggestionProvider,
       signatureProvider,
       hoverProvider,
@@ -479,16 +471,15 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
 
   const { CopyButton } = useCopy({ isCopyable, value });
 
-  useEffect(() => {
-    // Register themes when 'useDarkTheme' changes
-    monaco.editor.defineTheme('euiColors', useDarkTheme ? DARK_THEME : LIGHT_THEME);
-    monaco.editor.defineTheme(
-      'euiColorsTransparent',
-      useDarkTheme ? DARK_THEME_TRANSPARENT : LIGHT_THEME_TRANSPARENT
-    );
-  }, [useDarkTheme]);
-
-  const theme = options?.theme ?? (transparentBackground ? 'euiColorsTransparent' : 'euiColors');
+  const theme =
+    options?.theme ??
+    (transparentBackground
+      ? useDarkTheme
+        ? CODE_EDITOR_DARK_THEME_TRANSPARENT_ID
+        : CODE_EDITOR_LIGHT_THEME_TRANSPARENT_ID
+      : useDarkTheme
+      ? CODE_EDITOR_DARK_THEME_ID
+      : CODE_EDITOR_LIGHT_THEME_ID);
 
   return (
     <div
