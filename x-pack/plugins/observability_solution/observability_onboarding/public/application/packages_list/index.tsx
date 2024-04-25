30e0c60d0c50742e7b7527f8474e4e3c4d8c8730
Mykola Harmash <mykola.harmash@elastic.co>
2024-04-25 15:14:30 +0200

    fixup! Merge remote-tracking branch 'upstream/main' into 181378-observability-onboarding-remove-feature-flag

--- x-pack/plugins/observability_solution/observability_onboarding/public/application/packages_list/index.tsx
+++ x-pack/plugins/observability_solution/observability_onboarding/public/application/packages_list/index.tsx
@@ -120,6 +120,7 @@ const PackageListGridWrapper = ({
             categories={[]}
             setUrlandReplaceHistory={() => {}}
             setUrlandPushHistory={() => {}}
+            showCardLabels={false}
           />
         )}
       </div>
