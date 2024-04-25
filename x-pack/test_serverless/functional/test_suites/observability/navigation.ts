3910b398690beb6ad1db66850affcd2fd33cdde0
Mykola Harmash <mykola.harmash@elastic.co>
2024-04-25 16:54:32 +0200

    fixup! fixup! fixup! Merge remote-tracking branch 'upstream/main' into 181378-observability-onboarding-remove-feature-flag

--- x-pack/test_serverless/functional/test_suites/observability/navigation.ts
+++ x-pack/test_serverless/functional/test_suites/observability/navigation.ts
@@ -32,7 +32,6 @@ export default function ({ getPageObject, getService }: FtrProviderContext) {
       // check serverless search side nav exists
       await svlCommonNavigation.expectExists();
       await svlCommonNavigation.breadcrumbs.expectExists();
-      await svlObltOnboardingPage.assertQuickstartBadgeExists();
 
       // check side nav links
       await svlCommonNavigation.sidenav.expectSectionOpen('observability_project_nav');
