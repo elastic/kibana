a708232d19d2ff3feea3b02eb9e9ab10dd1ce435
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2024-04-25 15:53:05 +0200

    remove lint rule for backport

--- .eslintrc.js
+++ .eslintrc.js
@@ -1516,7 +1516,6 @@ module.exports = {
         ],
         '@kbn/i18n/strings_should_be_translated_with_i18n': 'warn',
         '@kbn/i18n/strings_should_be_translated_with_formatted_message': 'warn',
-        '@kbn/telemetry/event_generating_elements_should_be_instrumented': 'warn',
       },
     },
     /**
