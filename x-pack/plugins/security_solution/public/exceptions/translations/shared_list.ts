32039459a8c9f472590514ad44fb489d530da578
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2024-04-25 09:58:46 +0200

    Adjust the message to follow guidelines

--- x-pack/plugins/security_solution/public/exceptions/translations/shared_list.ts
+++ x-pack/plugins/security_solution/public/exceptions/translations/shared_list.ts
@@ -270,7 +270,7 @@ export const IMPORT_EXCEPTION_LIST_AS_NEW_LIST = i18n.translate(
 export const IMPORT_EXCEPTION_ENDPOINT_LIST_WARNING = i18n.translate(
   'xpack.securitySolution.exceptionsTable.importExceptionEndpointListWarning',
   {
-    defaultMessage: 'We only allow one Exception List for Endpoint Security.',
+    defaultMessage: 'Multiple exception lists for Endpoint Security are not allowed.',
   }
 );
 
