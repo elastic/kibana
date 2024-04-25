579bb26c837eafe733041687d8cd88aa47b9038c
Kevin Delemme <kevin.delemme@elastic.co>
2024-04-25 09:26:58 -0400

    WIP: need to pause this

--- x-pack/plugins/observability_solution/slo/public/components/slo/delete_confirmation_modal/slo_delete_confirmation_modal.tsx
+++ x-pack/plugins/observability_solution/slo/public/components/slo/delete_confirmation_modal/slo_delete_confirmation_modal.tsx
@@ -5,14 +5,15 @@
  * 2.0.
  */
 
-import { EuiConfirmModal } from '@elastic/eui';
+import { EuiConfirmModal, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { ALL_VALUE, SLODefinitionResponse, SLOWithSummaryResponse } from '@kbn/slo-schema';
+import { FormattedMessage } from '@kbn/i18n-react';
+import { ALL_VALUE, SLOWithSummaryResponse } from '@kbn/slo-schema';
 import React from 'react';
 import { getGroupKeysProse } from '../../../utils/slo/groupings';
 
 export interface SloDeleteConfirmationModalProps {
-  slo: SLOWithSummaryResponse | SLODefinitionResponse;
+  slo: SLOWithSummaryResponse;
   onCancel: () => void;
   onConfirm: () => void;
 }
@@ -22,33 +23,68 @@ export function SloDeleteConfirmationModal({
   onCancel,
   onConfirm,
 }: SloDeleteConfirmationModalProps) {
-  const { name, groupBy } = slo;
+  const { name, groupBy, instanceId } = slo;
+
+  if ([groupBy].flat().every((group) => group === ALL_VALUE)) {
+    return (
+      <EuiConfirmModal
+        buttonColor="danger"
+        data-test-subj="sloDeleteConfirmationModal"
+        title={getTitleLabel(name)}
+        cancelButtonText={cancelButtonLabel}
+        confirmButtonText={confirmButtonLabel}
+        onCancel={onCancel}
+        onConfirm={onConfirm}
+      >
+        <FormattedMessage
+          id="xpack.slo.deleteConfirmationModal.descriptionText"
+          defaultMessage="You can't recover this SLO after deleting it."
+        />
+      </EuiConfirmModal>
+    );
+  }
+
   return (
     <EuiConfirmModal
       buttonColor="danger"
       data-test-subj="sloDeleteConfirmationModal"
-      title={i18n.translate('xpack.slo.deleteConfirmationModal.title', {
-        defaultMessage: 'Delete {name}?',
-        values: { name },
-      })}
-      cancelButtonText={i18n.translate('xpack.slo.deleteConfirmationModal.cancelButtonLabel', {
-        defaultMessage: 'Cancel',
-      })}
-      confirmButtonText={i18n.translate('xpack.slo.deleteConfirmationModal.deleteButtonLabel', {
-        defaultMessage: 'Delete',
-      })}
+      title={getTitleLabel(name)}
+      cancelButtonText={cancelButtonLabel}
+      confirmButtonText={confirmButtonLabel}
       onCancel={onCancel}
       onConfirm={onConfirm}
     >
-      {groupBy !== ALL_VALUE
-        ? i18n.translate('xpack.slo.deleteConfirmationModal.groupByDisclaimerText', {
-            defaultMessage:
-              'This SLO has been generated with a group key on {groupKey}. Deleting this SLO definition will result in all instances being deleted.',
-            values: { groupKey: getGroupKeysProse(slo.groupBy) },
-          })
-        : i18n.translate('xpack.slo.deleteConfirmationModal.descriptionText', {
-            defaultMessage: "You can't recover this SLO after deleting it.",
-          })}
+      <EuiFlexGroup direction="column">
+        <EuiFlexItem>
+          <FormattedMessage
+            id="xpack.slo.deleteConfirmationModal.groupByDisclaimerText"
+            defaultMessage="This SLO is an instance of its SLO definition using a group key on {groupKey} with the value {instanceId}."
+            values={{ groupKey: getGroupKeysProse(slo.groupBy), instanceId }}
+          />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <FormattedMessage
+            id="xpack.slo.deleteConfirmationModal.groupByDisclaimerTextFollow"
+            defaultMessage="Do you want to delete the SLO definition and all instances associated to it, or delete only this SLO instance '{instanceId}'?"
+            values={{ groupKey: getGroupKeysProse(slo.groupBy), instanceId }}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </EuiConfirmModal>
   );
+}
+
+const confirmButtonLabel = i18n.translate('xpack.slo.deleteConfirmationModal.deleteButtonLabel', {
+  defaultMessage: 'Delete',
+});
+
+const cancelButtonLabel = i18n.translate('xpack.slo.deleteConfirmationModal.cancelButtonLabel', {
+  defaultMessage: 'Cancel',
+});
+
+function getTitleLabel(name: string): React.ReactNode {
+  return i18n.translate('xpack.slo.deleteConfirmationModal.title', {
+    defaultMessage: 'Delete {name}?',
+    values: { name },
+  });
 }
