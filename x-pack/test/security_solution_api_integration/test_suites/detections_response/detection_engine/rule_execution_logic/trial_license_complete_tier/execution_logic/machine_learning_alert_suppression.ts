8c0f6c16db1b5f804edd67705de4d670f4f932f5
Ryland Herrick <ryalnd@gmail.com>
2024-04-26 16:18:50 -0500

    Fleshing out more of our suppression execution tests
    These now have type errors, since ML rules don't yet accept suppression
    fields. We have our next task!

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/trial_license_complete_tier/execution_logic/machine_learning_alert_suppression.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/trial_license_complete_tier/execution_logic/machine_learning_alert_suppression.ts
@@ -5,20 +5,103 @@
  * 2.0.
  */
 
+import supertestLib from 'supertest';
+import url from 'url';
+
+import { MachineLearningRuleCreateProps } from '@kbn/security-solution-plugin/common/api/detection_engine';
+import { ALERT_SUPPRESSION_DOCS_COUNT, ALERT_SUPPRESSION_TERMS } from '@kbn/rule-data-utils';
+import { EsArchivePathBuilder } from '../../../../../../es_archive_path_builder';
 import { FtrProviderContext } from '../../../../../../ftr_provider_context';
+import { executeSetupModuleRequest, forceStartDatafeeds, getAlerts } from '../../../../utils';
+import {
+  createRule,
+  deleteAllAlerts,
+  deleteAllRules,
+} from '../../../../../../../common/utils/security_solution';
 
 export default ({ getService }: FtrProviderContext) => {
-  // TODO: tags
-  describe('Machine Learning Detection Rule - Alert Suppression', () => {
+  const supertest = getService('supertest');
+  const esArchiver = getService('esArchiver');
+  const es = getService('es');
+  const log = getService('log');
+  const kibanaServer = getService('kibanaServer');
+  const config = getService('config');
+  const retry = getService('retry');
+
+  const request = supertestLib(url.format(config.get('servers.kibana')));
+  const isServerless = config.get('serverless');
+  const dataPathBuilder = new EsArchivePathBuilder(isServerless);
+  const auditbeatArchivePath = dataPathBuilder.getPath('auditbeat/hosts');
+
+  const mlModuleName = 'security_linux_v3';
+  const mlJobId = 'v3_linux_anomalous_network_activity';
+  const baseRuleProps: MachineLearningRuleCreateProps = {
+    name: 'Test ML rule',
+    description: 'Test ML rule description',
+    risk_score: 50,
+    severity: 'critical',
+    type: 'machine_learning',
+    anomaly_threshold: 30,
+    machine_learning_job_id: mlJobId,
+    from: '1900-01-01T00:00:00.000Z',
+    rule_id: 'ml-rule-id',
+  };
+  let ruleProps: MachineLearningRuleCreateProps;
+
+  // TODO: feature flag
+  describe('@ess @serverless @serverlessQA Machine Learning Detection Rule - Alert Suppression', () => {
     describe('with anomalies available to be alerted upon', () => {
+      before(async () => {
+        // Order is critical here: auditbeat data must be loaded before attempting to start the ML job,
+        // as the job looks for certain indices on start
+        await esArchiver.load(auditbeatArchivePath);
+        await executeSetupModuleRequest({ module: mlModuleName, rspCode: 200, supertest });
+        await forceStartDatafeeds({ jobId: mlJobId, rspCode: 200, supertest });
+        await esArchiver.load('x-pack/test/functional/es_archives/security_solution/anomalies');
+      });
+
+      after(async () => {
+        await esArchiver.load(auditbeatArchivePath);
+        await esArchiver.unload('x-pack/test/functional/es_archives/security_solution/anomalies');
+        await deleteAllAlerts(supertest, log, es);
+        await deleteAllRules(supertest, log);
+      });
+
+      afterEach(async () => {
+        await deleteAllAlerts(supertest, log, es);
+        await deleteAllRules(supertest, log);
+      });
+
       describe('with per-execution suppression duration', () => {
-        it.skip('performs no suppression if a single alert is generated');
-        it.skip('suppresses alerts within a single execution');
+        beforeEach(async () => {
+          ruleProps = {
+            ...baseRuleProps,
+            alert_suppression: {
+              group_by: ['user.name'],
+              missing_fields_strategy: 'suppress',
+            },
+          };
+        });
+
+        it.only('suppresses alerts within a single execution', async () => {
+          const createdRule = await createRule(supertest, log, ruleProps);
+          const alerts = await getAlerts(supertest, log, es, createdRule);
+
+          expect(alerts.hits.hits).toHaveLength(1);
+          expect(alerts.hits.hits[0]._source).toEqual(
+            expect.objectContaining({
+              [ALERT_SUPPRESSION_TERMS]: { field: 'user.name', value: ['root'] },
+              [ALERT_SUPPRESSION_DOCS_COUNT]: 0,
+            })
+          );
+        });
+
         it.skip('suppresses alerts in multiple executions');
         it.skip('suppresses alerts with timestamp override configured');
         it.skip(
           'deduplicates previously suppressed alerts if rule has overlapping execution windows'
         );
+        it.skip('performs no suppression if a single alert is generated');
       });
 
       describe('with interval suppression duration', () => {
