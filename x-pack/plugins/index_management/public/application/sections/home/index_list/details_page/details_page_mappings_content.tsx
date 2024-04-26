4a6b12302c5cc62a02666e2163d3f65740e9cdd3
Saikat Sarkar <saikat.sarkar@elastic.co>
2024-04-26 12:35:41 -0600

    Remove duplicates from inference_endpoint combo

--- x-pack/plugins/index_management/public/application/sections/home/index_list/details_page/details_page_mappings_content.tsx
+++ x-pack/plugins/index_management/public/application/sections/home/index_list/details_page/details_page_mappings_content.tsx
@@ -77,7 +77,7 @@ export const DetailsPageMappingsContent: FunctionComponent<{
   jsonData,
   refetchMapping,
   showAboutMappings,
-  isSemanticTextEnabled = false,
+  isSemanticTextEnabled = true,
 }) => {
   const {
     services: { extensionsService },
