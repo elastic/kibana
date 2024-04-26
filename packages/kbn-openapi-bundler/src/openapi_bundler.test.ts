14bcdd98d5c2c171e6605822cd9664fcc3093844
Maxim Palenov <maxim.palenov@elastic.co>
2024-04-26 15:35:43 +0200

    fix bundler tests

--- packages/kbn-openapi-bundler/src/openapi_bundler.test.ts
+++ packages/kbn-openapi-bundler/src/openapi_bundler.test.ts
@@ -6,122 +6,170 @@
  * Side Public License, v 1.
  */
 
-import { existsSync, rmSync } from 'fs';
-import { basename, join } from 'path';
+import { existsSync } from 'fs';
+import { join } from 'path';
 import { bundle } from './openapi_bundler';
 import { readYamlDocument } from './utils/read_yaml_document';
 
-const rootPath = join(__dirname, '__test__');
-const targetAbsoluteFilePath = join(rootPath, 'bundled.yaml');
+const ROOT_PATH = join(__dirname, '__test__');
+const BUNDLED_FILE_PATH_TEMPLATE = join(__dirname, 'target', 'bundled-{version}.yaml');
+const DEFAULT_BUNDLED_FILE_PATH = BUNDLED_FILE_PATH_TEMPLATE.replace('{version}', '2023-10-31');
 
 describe('OpenAPI Bundler', () => {
-  afterEach(() => {
-    removeTargetFile();
-  });
-
   it('bundles two simple specs', async () => {
     await bundleFolder('two_simple_specs');
-    await expectBundleToMatchFile('two_simple_specs', 'expected.yaml');
-  });
-
-  it('bundles one file with a local reference', async () => {
-    await bundleFolder('spec_with_local_ref');
-    await expectBundleToMatchFile('spec_with_local_ref', 'expected.yaml');
-  });
-
-  it('bundles one file with an external reference', async () => {
-    await bundleFolder('spec_with_external_ref');
-    await expectBundleToMatchFile('spec_with_external_ref', 'expected.yaml');
-  });
-
-  it('bundles files with external references', async () => {
-    await bundleFolder('two_specs_with_external_ref');
-    await expectBundleToMatchFile('two_specs_with_external_ref', 'expected.yaml');
-  });
-
-  // Fails because `writeYamlDocument()` has `noRefs: true` setting
-  // it('bundles recursive spec', async () => {
-  //   await bundleFolder('recursive_spec');
-  //   await expectBundleToMatchFile('recursive_spec', 'expected.yaml');
-  // });
-
-  it('bundles specs with recursive references', async () => {
-    await bundleFolder('recursive_ref_specs');
-    await expectBundleToMatchFile('recursive_ref_specs', 'expected.yaml');
-  });
-
-  it('bundles spec with a self-recursive reference', async () => {
-    await bundleFolder('self_recursive_ref');
-    await expectBundleToMatchFile('self_recursive_ref', 'expected.yaml');
+    await expectBundleToMatchFile(
+      DEFAULT_BUNDLED_FILE_PATH,
+      join('two_simple_specs', 'expected.yaml')
+    );
   });
 
   it('bundles one endpoint with different versions', async () => {
     await bundleFolder('different_endpoint_versions');
-    await expectBundleToMatchFile('different_endpoint_versions', 'expected.yaml');
+    await expectBundleToMatchFile(
+      BUNDLED_FILE_PATH_TEMPLATE.replace('{version}', '2023-10-31'),
+      join('different_endpoint_versions', 'expected_2023_10_31.yaml')
+    );
+    await expectBundleToMatchFile(
+      BUNDLED_FILE_PATH_TEMPLATE.replace('{version}', '2023-11-11'),
+      join('different_endpoint_versions', 'expected_2023_11_11.yaml')
+    );
   });
 
   it('bundles spec with different OpenAPI versions', async () => {
     await bundleFolder('different_openapi_versions');
-    await expectBundleToMatchFile('different_openapi_versions', 'expected.yaml');
-  });
-
-  it('bundles conflicting but equal references', async () => {
-    await bundleFolder('conflicting_but_equal_refs_in_different_specs');
-    await expectBundleToMatchFile('conflicting_but_equal_refs_in_different_specs', 'expected.yaml');
-  });
-
-  it('fails to bundle conflicting references encountered in separate specs', async () => {
-    await expectBundlingError(
-      'conflicting_refs_in_different_specs',
-      /\/components\/schemas\/ConflictTestSchema/
+    await expectBundleToMatchFile(
+      DEFAULT_BUNDLED_FILE_PATH,
+      join('different_openapi_versions', 'expected.yaml')
     );
+  });
+
+  describe('bundle references', () => {
+    it('bundles files with external references', async () => {
+      const folder = join('bundle_refs', 'two_specs_with_external_ref');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
+    });
+
+    it('bundles one file with a local reference', async () => {
+      const folder = join('bundle_refs', 'spec_with_local_ref');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
+    });
+
+    it('bundles one file with an external reference', async () => {
+      const folder = join('bundle_refs', 'spec_with_external_ref');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
+    });
+
+    it('bundles conflicting but equal references', async () => {
+      const folder = join('bundle_refs', 'conflicting_but_equal_refs_in_different_specs');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
+    });
+
+    it('fails to bundle external conflicting references encountered in on spec file', async () => {
+      const folder = join('bundle_refs', 'conflicting_refs');
+
+      await expectBundlingError(folder, /\/components\/schemas\/ConflictTestSchema/);
+    });
+
+    it('fails to bundle conflicting references encountered in separate specs', async () => {
+      const folder = join('bundle_refs', 'conflicting_refs_in_different_specs');
+
+      await expectBundlingError(folder, /\/components\/schemas\/ConflictTestSchema/);
+    });
+  });
+
+  describe('circular references', () => {
+    // Fails because `writeYamlDocument()` has `noRefs: true` setting
+    // it('bundles recursive spec', async () => {
+    //   const folder = join('circular', 'recursive_spec');
+
+    //   await bundleFolder(folder);
+    //   await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
+    // });
+
+    it('bundles specs with recursive references', async () => {
+      const folder = join('circular', 'circular_ref_specs');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
+    });
+
+    it('bundles spec with a self-recursive reference', async () => {
+      const folder = join('circular', 'self_recursive_ref');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
+    });
   });
 
   describe('x-modify', () => {
     it('makes properties in an object node partial', async () => {
-      await bundleFolder('modify_partial_node');
-      await expectBundleToMatchFile('modify_partial_node', 'expected.yaml');
+      const folder = join('x_modify', 'modify_partial_node');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
     });
 
     it('makes properties in a referenced object node partial', async () => {
-      await bundleFolder('modify_partial_ref');
-      await expectBundleToMatchFile('modify_partial_ref', 'expected.yaml');
+      const folder = join('x_modify', 'modify_partial_ref');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
     });
 
     it('makes properties in an object node required', async () => {
-      await bundleFolder('modify_required_node');
-      await expectBundleToMatchFile('modify_required_node', 'expected.yaml');
+      const folder = join('x_modify', 'modify_required_node');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
     });
 
     it('makes properties in a referenced object node required', async () => {
-      await bundleFolder('modify_required_ref');
-      await expectBundleToMatchFile('modify_required_ref', 'expected.yaml');
+      const folder = join('x_modify', 'modify_required_ref');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
     });
   });
 
   describe('x-inline', () => {
     it('inlines a reference', async () => {
       await bundleFolder('inline_ref');
-      await expectBundleToMatchFile('inline_ref', 'expected.yaml');
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join('inline_ref', 'expected.yaml'));
     });
   });
 
   describe('skip internal', () => {
     it('skips nodes with x-internal property', async () => {
-      await bundleFolder('skip_internal');
-      await expectBundleToMatchFile('skip_internal', 'expected.yaml');
+      const folder = join('skip', 'skip_internal');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
     });
 
     it('skips endpoints starting with /internal', async () => {
-      await bundleFolder('skip_internal_endpoint');
-      await expectBundleToMatchFile('skip_internal_endpoint', 'expected.yaml');
+      const folder = join('skip', 'skip_internal_endpoint');
+
+      await bundleFolder(folder);
+      await expectBundleToMatchFile(DEFAULT_BUNDLED_FILE_PATH, join(folder, 'expected.yaml'));
     });
   });
 
   describe('remove props', () => {
     it('removes "x-codegen-enabled" property', async () => {
       await bundleFolder('remove_props');
-      await expectBundleToMatchFile('remove_props', 'expected.yaml');
+      await expectBundleToMatchFile(
+        DEFAULT_BUNDLED_FILE_PATH,
+        join('remove_props', 'expected.yaml')
+      );
     });
   });
 });
@@ -129,9 +177,9 @@ describe('OpenAPI Bundler', () => {
 async function bundleFolder(folderName: string): Promise<void> {
   await expect(
     bundle({
-      rootDir: join(rootPath, folderName),
+      rootDir: join(ROOT_PATH, folderName),
       sourceGlob: '*.schema.yaml',
-      outputFilePath: join('..', basename(targetAbsoluteFilePath)),
+      outputFilePath: BUNDLED_FILE_PATH_TEMPLATE,
     })
   ).resolves.toBeUndefined();
 }
@@ -142,28 +190,22 @@ async function expectBundlingError(
 ): Promise<void> {
   return await expect(
     bundle({
-      rootDir: join(rootPath, folderName),
+      rootDir: join(ROOT_PATH, folderName),
       sourceGlob: '*.schema.yaml',
-      outputFilePath: join('..', basename(targetAbsoluteFilePath)),
+      outputFilePath: BUNDLED_FILE_PATH_TEMPLATE,
     })
   ).rejects.toThrowError(error);
 }
 
 async function expectBundleToMatchFile(
-  folderName: string,
-  expectedFileName: string
+  bundledFilePath: string,
+  relativeFilePathToMatch: string
 ): Promise<void> {
-  expect(existsSync(targetAbsoluteFilePath)).toBeTruthy();
+  expect(existsSync(bundledFilePath)).toBeTruthy();
 
-  const bundledSpec = await readYamlDocument(targetAbsoluteFilePath);
-  const expectedAbsoluteFilePath = join(rootPath, folderName, expectedFileName);
-  const expectedSpec = await readYamlDocument(expectedAbsoluteFilePath);
+  const bundledSpec = await readYamlDocument(bundledFilePath);
+  const expectedFilePath = join(ROOT_PATH, relativeFilePathToMatch);
+  const expectedSpec = await readYamlDocument(expectedFilePath);
 
   expect(bundledSpec).toEqual(expectedSpec);
-}
-
-function removeTargetFile(): void {
-  if (existsSync(targetAbsoluteFilePath)) {
-    rmSync(targetAbsoluteFilePath, { force: true });
-  }
 }
