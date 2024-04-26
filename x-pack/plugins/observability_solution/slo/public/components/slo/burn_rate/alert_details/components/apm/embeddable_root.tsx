9def7bab2dabc939402f792a41a8bf04760b1887
Dominique Belcher <dominique.clarke@elastic.co>
2024-04-26 13:57:36 -0400

    support groupings

--- x-pack/plugins/observability_solution/slo/public/components/slo/burn_rate/alert_details/components/apm/embeddable_root.tsx
+++ x-pack/plugins/observability_solution/slo/public/components/slo/burn_rate/alert_details/components/apm/embeddable_root.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 import { v4 as uuidv4 } from 'uuid';
-import { buildQueryFromFilters } from '@kbn/es-query';
+import { buildQueryFromFilters, Filter } from '@kbn/es-query';
 import { ErrorEmbeddable, IEmbeddable } from '@kbn/embeddable-plugin/public';
 import React, { useEffect, useRef, useState } from 'react';
 import { GetSLOResponse, APMTransactionDurationIndicator } from '@kbn/slo-schema';
@@ -49,15 +49,19 @@ export function APMEmbeddableRoot({
   const embeddableRoot: React.RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
   const filter = slo.indicator.params.filter;
   const isKueryFilter = typeof filter === 'string';
+  const groupingInput = getInputFromGroupings(slo);
 
   const kuery = isKueryFilter ? filter : undefined;
-  const filters = !isKueryFilter
-    ? buildQueryFromFilters(filter?.filters, undefined, undefined)
-    : undefined;
-
+  const allFilters =
+    !isKueryFilter && filter?.filters
+      ? [...filter?.filters, ...groupingInput.filters]
+      : groupingInput.filters;
+  const filters = buildQueryFromFilters(allFilters, undefined, undefined);
   useEffect(() => {
     async function setupEmbeddable() {
       const factory = getEmbeddableFactory(embeddableId);
+
+      const groupingsInput = getInputFromGroupings(slo);
 
       if (slo) {
         const { transactionName, transactionType, environment, service } = slo.indicator.params;
@@ -76,6 +80,7 @@ export function APMEmbeddableRoot({
           rule,
           comparisonEnabled: true,
           offset: '1d',
+          ...groupingsInput.input,
         };
 
         const embeddableObject = await factory?.create(input);
@@ -98,3 +103,52 @@ export function APMEmbeddableRoot({
 
   return <div ref={embeddableRoot} />;
 }
+
+const getInputFromGroupings = (slo: APMTransactionDurationSLOResponse) => {
+  const groupings = Object.entries(slo.groupings) as Array<[string, string]>;
+  const input: {
+    transactionName?: string;
+    transactionType?: string;
+    serviceName?: string;
+    environment?: string;
+  } = {};
+  const filters: Filter[] = [];
+  groupings.forEach(([key, value]) => {
+    switch (key) {
+      case 'transaction.name':
+        input.transactionName = value;
+        break;
+      case 'transaction.type':
+        input.transactionType = value;
+        break;
+      case 'service.name':
+        input.serviceName = value;
+        break;
+      case 'service.environment':
+        input.environment = value;
+        break;
+      default:
+        filters.push({
+          meta: {
+            type: 'custom',
+            alias: null,
+            key,
+            params: {
+              query: value,
+            },
+            disabled: false,
+          },
+          query: {
+            match_phrase: {
+              [key]: value,
+            },
+          },
+        });
+    }
+  });
+
+  return {
+    input,
+    filters,
+  };
+};
