7a04887ea1c038bc21d3a37a4d465dfa488cd036
Quynh Nguyen <quynh.nguyen@elastic.co>
2024-04-25 23:17:03 -0500

    Adding comments for API, fix types

--- src/plugins/discover/public/application/main/components/field_stats_table/field_stats_table.tsx
+++ src/plugins/discover/public/application/main/components/field_stats_table/field_stats_table.tsx
@@ -7,166 +7,22 @@
  */
 
 import React, { useEffect, useMemo, useRef, useCallback } from 'react';
-import type { Filter, Query, AggregateQuery } from '@kbn/es-query';
-import { METRIC_TYPE, type UiCounterMetricType } from '@kbn/analytics';
-import type { DataViewField, DataView } from '@kbn/data-views-plugin/public';
+import { METRIC_TYPE } from '@kbn/analytics';
 import { ReactEmbeddableRenderer } from '@kbn/embeddable-plugin/public';
-import type { SavedSearch } from '@kbn/saved-search-plugin/public';
 import { EuiFlexItem } from '@elastic/eui';
 import { css } from '@emotion/react';
 import useObservable from 'react-use/lib/useObservable';
-import { of } from 'rxjs';
-import { BehaviorSubject } from 'rxjs';
-import type { DefaultEmbeddableApi } from '@kbn/embeddable-plugin/public';
+import { of, BehaviorSubject } from 'rxjs';
 import type { Subscription } from 'rxjs';
 import { useDiscoverServices } from '../../../../hooks/use_discover_services';
 import { FIELD_STATISTICS_LOADED } from './constants';
-import type { DiscoverStateContainer } from '../../services/discover_state';
-
-export interface RandomSamplingOption {
-  mode: 'random_sampling';
-  seed: string;
-  probability: number;
-}
-
-export interface NormalSamplingOption {
-  mode: 'normal_sampling';
-  seed: string;
-  shardSize: number;
-}
-
-export interface NoSamplingOption {
-  mode: 'no_sampling';
-  seed: string;
-}
-
-export type SamplingOption = RandomSamplingOption | NormalSamplingOption | NoSamplingOption;
-
-export interface FieldStatisticsTableEmbeddableState {
-  /**
-   * Data view is required for esql:false or non-ESQL mode
-   */
-  dataView?: DataView;
-  /**
-   * Kibana saved search object
-   */
-  savedSearch?: SavedSearch;
-  /**
-   * Kibana query
-   */
-  query?: Query | AggregateQuery;
-  /**
-   * List of fields to visibile show in the table
-   * set shouldGetSubfields: true if table needs to show the sub multi-field like .keyword
-   */
-  visibleFieldNames?: string[];
-  /**
-   * List of filters
-   */
-  filters?: Filter[];
-  /**
-   * Whether to show the preview/mini distribution chart on the tables upon first table mounting
-   */
-  showPreviewByDefault?: boolean;
-  /**
-   * If true, will show a button action to edit the data view field in every row
-   */
-  allowEditDataView?: boolean;
-  /**
-   * Optional id to identify the embeddable
-   */
-  id?: string;
-  /**
-   * Callback to add a filter to filter bar
-   */
-  onAddFilter?: (field: DataViewField | string, value: string, type: '+' | '-') => void;
-  /**
-   * Optional search sessionId to save and restore long running search
-   * If not provided, will generate its own sessionId
-   */
-  sessionId?: string;
-  /**
-   * Optional list of fields provided to table to fetch a subset of fields only
-   * so it doesn't need to fetch all fields
-   */
-  fieldsToFetch?: string[];
-  /**
-   * Total documents optionally provided to help table have context of the fetch size
-   * so it can reduce redundant API requests
-   */
-  totalDocuments?: number;
-  /**
-   * For non-ESQL mode, the sampling option is used to determine the sampling strategy
-   */
-  samplingOption?: SamplingOption;
-  /**
-   * If esql:true, switch table to ES|QL mode
-   */
-  esql?: boolean;
-  /**
-   * If esql:true, the index pattern is used to validate time field
-   */
-  indexPattern?: string;
-  /**
-   * If true, the table will try to retrieve subfield information as well based on visibleFields
-   * For example: visibleFields: ['field1', 'field2'] => will show ['field1', 'field1.keyword', 'field2', 'field2.keyword']
-   */
-  shouldGetSubfields?: boolean;
-  /**
-   * Force refresh the table
-   */
-  lastReloadRequestTime?: number;
-}
-
-export interface FieldStatisticsTableEmbeddableApi {
-  showDistributions$?: BehaviorSubject<boolean>;
-}
-
-export interface FieldStatisticsTableProps {
-  /**
-   * Determines which columns are displayed
-   */
-  columns: string[];
-  /**
-   * The used data view
-   */
-  dataView: DataView;
-  /**
-   * Saved search description
-   */
-  searchDescription?: string;
-  /**
-   * Saved search title
-   */
-  searchTitle?: string;
-  /**
-   * Optional saved search
-   */
-  savedSearch?: SavedSearch;
-  /**
-   * Optional query to update the table content
-   */
-  query?: Query | AggregateQuery;
-  /**
-   * Filters query to update the table content
-   */
-  filters?: Filter[];
-  /**
-   * State container with persisted settings
-   */
-  stateContainer?: DiscoverStateContainer;
-  /**
-   * Callback to add a filter to filter bar
-   */
-  onAddFilter?: (field: DataViewField | string, value: string, type: '+' | '-') => void;
-  /**
-   * Metric tracking function
-   * @param metricType
-   * @param eventName
-   */
-  trackUiMetric?: (metricType: UiCounterMetricType, eventName: string | string[]) => void;
-  searchSessionId?: string;
-}
+import type {
+  NormalSamplingOption,
+  FieldStatisticsTableEmbeddableState,
+  FieldStatisticsTableEmbeddableApi,
+  FieldStatisticsTableProps,
+} from './types';
+export type { FieldStatisticsTableProps };
 
 const statsTableCss = css({
   width: '100%',
@@ -314,8 +170,8 @@ export const FieldStatisticsTable = (props: FieldStatisticsTableProps) => {
   const handleEmbeddableApiReady = useCallback(
     (api: FieldStatisticsTableEmbeddableApi) => {
       embeddableApi.current = api;
-      if (api.showDistributions$) {
-        embeddableApiSubscription.current = api.showDistributions$.subscribe(
+      if (api) {
+        embeddableApiSubscription.current = api.showDistributions$?.subscribe(
           (showDistributions?: boolean) => {
             if (showDistributions !== undefined && stateContainer) {
               stateContainer.appState.update({
@@ -332,18 +188,19 @@ export const FieldStatisticsTable = (props: FieldStatisticsTableProps) => {
   return (
     <EuiFlexItem css={statsTableCss}>
       <ReactEmbeddableRenderer<
-        DefaultEmbeddableApi<FieldStatisticsTableEmbeddableState>,
+        FieldStatisticsTableEmbeddableState,
         FieldStatisticsTableEmbeddableApi
       >
         maybeId={'discover_data_visualizer_grid'}
         type={'data_visualizer_grid'}
+        onApiAvailable={handleEmbeddableApiReady}
         state={{
           // initialState of the embeddable to be serialized
           rawState: initialState,
         }}
         parentApi={parentApi}
-        onApiAvailable={handleEmbeddableApiReady}
         panelProps={{
+          // @ts-expect-error
           css: css({
             height: 'auto',
             minHeight: 'auto',
