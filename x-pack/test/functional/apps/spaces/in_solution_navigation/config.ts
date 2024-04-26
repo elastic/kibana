0427f174a608b6737b040aba9582c29634606ab4
Sébastien Loix <sebastien.loix@elastic.co>
2024-04-26 16:16:48 +0100

    Update functional test config

--- x-pack/test/functional/apps/spaces/in_solution_navigation/config.ts
+++ x-pack/test/functional/apps/spaces/in_solution_navigation/config.ts
@@ -18,11 +18,10 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
       serverArgs: [
         ...functionalConfig.get('kbnTestServer.serverArgs'),
         '--xpack.cloud_integrations.experiments.enabled=true',
-        // '--xpack.cloud_integrations.experiments.launch_darkly.sdk_key=a_string',
-        // '--xpack.cloud_integrations.experiments.launch_darkly.client_id=a_string',
+        '--xpack.cloud_integrations.experiments.launch_darkly.sdk_key=a_string',
+        '--xpack.cloud_integrations.experiments.launch_darkly.client_id=a_string',
         '--xpack.cloud_integrations.experiments.flag_overrides.solutionNavEnabled=true',
         '--navigation.solutionNavigation.enabled=true',
-        // '--navigation.solutionNavigation.defaultSolution=es',
         // Note: the base64 string in the cloud.id config contains the ES endpoint required in the functional tests
         '--xpack.cloud.id=ftr_fake_cloud_id:aGVsbG8uY29tOjQ0MyRFUzEyM2FiYyRrYm4xMjNhYmM=',
         '--xpack.cloud.base_url=https://cloud.elastic.co',
