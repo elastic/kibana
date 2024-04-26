6f4ba02f8058b31ba4c5ddd49b18a36b6f21b488
Quynh Nguyen <quynh.nguyen@elastic.co>
2024-04-26 12:32:57 -0500

    Disable dashboard action

--- x-pack/plugins/data_visualizer/public/application/index_data_visualizer/embeddables/index.ts
+++ x-pack/plugins/data_visualizer/public/application/index_data_visualizer/embeddables/index.ts
@@ -9,15 +9,16 @@ import { registerReactEmbeddableFactory } from '@kbn/embeddable-plugin/public';
 import type { DataVisualizerCoreSetup } from '../../../plugin';
 import type { DataVisualizerSetupDependencies } from '../../common/types/data_visualizer_plugin';
 import { FIELD_STATS_EMBED_ID } from './grid_embeddable/constants';
-import { registerFieldStatsUIActions } from './grid_embeddable/ui_actions/create_field_stats_embeddable_ui_action';
 export function registerReactEmbeddablesAndActions(
   core: DataVisualizerCoreSetup,
   plugins: DataVisualizerSetupDependencies
 ) {
+  // @TODO: Renable in follow up PR
+  // See https://github.com/elastic/kibana/issues/181904
   // Embeddable factory for field stats table
-  if (plugins.uiActions) {
-    registerFieldStatsUIActions(plugins.uiActions);
-  }
+  // if (plugins.uiActions) {
+  //   registerFieldStatsUIActions(plugins.uiActions);
+  // }
   registerReactEmbeddableFactory(FIELD_STATS_EMBED_ID, async () => {
     const { getFieldStatsTableFactory } = await import(
       './grid_embeddable/field_stats_embeddable_factory'
