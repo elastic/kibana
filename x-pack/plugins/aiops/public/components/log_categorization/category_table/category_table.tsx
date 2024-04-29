elastic/kibana

commit cbc68cd40ceff582083c2cf0514c29270ffe496b

Upgrade EUI to v94.1.0 (major `EuiTable` refactors) (#180514)
`v93.6.0` ⏩ `v94.1.0`

> [!important]
> 👋 Hello everyone - this is a special release containing `EuiTable`'s
conversion to Emotion, several long-overdue cleanups and breaking
changes, and one or two fun new features. First, let's address the big
questions:

### Q: I'm listed as a codeowner, how much should I manually QA/review?

Answer: It depends on what exactly in your code changed, but _in
general_ I would strongly suggest at least pulling this branch down and
doing a quick visual smoke test of all tables (_note: **not**
datagrids_) in your apps/plugins. You should not expect to see any major
visual regressions.

If your table contained any kind of custom styling or behavior (e.g.
custom CSS, etc.) I **strongly** recommend taking more time to QA
thoroughly to ensure your table still looks and behaves as expected.
Teams with very manual or specific updates will be flagged below in
comment threads.

### Q: When do I need to review by?

This PR will be merged **after** 8.14FF. Because this upgrade touches so
many files and teams, we're aiming for asking for an admin merge by EOD
4/18 regardless of full approval status.

As always, you're welcome to ping us after merge if you find any issues
later ([see our
FAQ](https://github.com/elastic/eui/blob/main/wiki/eui-team-processes/upgrading-kibana.md#faq-for-kibana-teams)),
as you will have until 8.15FF to catch any bugs.

### Q: What breaking changes were made, and why?

Here's a quick shortlist of all the changes made that affected the
majority of the commits in this PR:

#### <u>Removed several top-level table props</u>
- The `responsive` prop has been removed in favor of the new
`responsiveBreakpoint` prop (same `false` behavior as before)
- The following props were removed from basic and in-memory tables:
  - `hasActions`, `isSelectable`, and `isExpandable`

x-pack/plugins/aiops/public/components/log_categorization/category_table/category_table.tsx
@@ -317,14 +317,12 @@ export const CategoryTable: FC<Props> = ({
         compressed
         items={categories}
         columns={columns}
-        isSelectable={true}
         selection={selectionValue}
         itemId="key"
         onTableChange={onTableChange}
         pagination={pagination}
         sorting={sorting}
         data-test-subj="aiopsLogPatternsTable"
-        isExpandable={true}
         itemIdToExpandedRowMap={itemIdToExpandedRowMap}
         rowProps={(category) => {
           return enableRowActions

commit 3609f882a523a6fa57eda3e01abfdcaf1beee5c9

[ML] AIOps: Fixing pattern analysis action buttons layout (#181960)
Widening actions column width.

**Before**


![image](https://github.com/elastic/kibana/assets/22172091/cd39d806-4bf1-4697-a15b-ceede90bd730)


**After**

![image](https://github.com/elastic/kibana/assets/22172091/6bf738a5-f110-4cf3-b530-e147341e1414)

Also fixes similar issue with the annotations table in the anomaly
explorer.

Issue introduced in https://github.com/elastic/kibana/pull/180514

x-pack/plugins/aiops/public/components/log_categorization/category_table/category_table.tsx
@@ -194,7 +194,7 @@ export const CategoryTable: FC<Props> = ({
         defaultMessage: 'Actions',
       }),
       sortable: false,
-      width: '60px',
+      width: '65px',
       actions: [
         {
           name: labels.singleSelect.in,

x-pack/plugins/ml/public/application/components/annotations/annotations_table/annotations_table.js
@@ -692,7 +692,7 @@ class AnnotationsTableUI extends Component {
     columns.push(
       {
         align: RIGHT_ALIGNMENT,
-        width: '60px',
+        width: '65px',
         name: i18n.translate('xpack.ml.annotationsTable.actionsColumnName', {
           defaultMessage: 'Actions',
         }),