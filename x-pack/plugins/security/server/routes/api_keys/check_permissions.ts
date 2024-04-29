4503efaa274a3373e8b8558c73b0e4c0b7886a5c
rshen91 <rshen@elastic.co>
2024-04-29 11:58:13 -0600

    try weakmap cache

--- x-pack/plugins/security/server/routes/api_keys/check_permissions.ts
+++ x-pack/plugins/security/server/routes/api_keys/check_permissions.ts
@@ -5,15 +5,29 @@
  * 2.0.
  */
 
+import type { IKibanaResponse, KibanaRequest } from '@kbn/core/server';
+
 import type { RouteDefinitionParams } from '..';
 import type { ApiKey } from '../../../common/model';
 import { wrapIntoCustomErrorResponse } from '../../errors';
 import { createLicensedRouteHandler } from '../licensed_route_handler';
 
+export function permissionsFactory(req: KibanaRequest, validResponse?: IKibanaResponse) {
+  const permissionsRequestCache = new WeakMap<KibanaRequest, IKibanaResponse>();
+  return {
+    permissionsRequestCache() {
+      if (!permissionsRequestCache.has(req) && validResponse) {
+        permissionsRequestCache.set(req, validResponse);
+      }
+
+      return permissionsRequestCache.get(req);
+    },
+  };
+}
+
 /**
  * Response of Kibana to confirm users have permissions to generate API keys
  */
-
 export function defineValidPermissionRoutes({
   router,
   getAuthenticationService,
@@ -26,6 +40,7 @@ export function defineValidPermissionRoutes({
         access: 'internal',
       },
     },
+    // @ts-ignore response will be cached
     createLicensedRouteHandler(async (context, request, response) => {
       try {
         const esClient = (await context.core).elasticsearch.client;
@@ -49,6 +64,8 @@ export function defineValidPermissionRoutes({
           });
         }
 
+        if (permissionsFactory(request)) return permissionsFactory(request);
+
         const apiResponse = await esClient.asCurrentUser.security.getApiKey({
           owner: !clusterPrivileges.manage_api_key && !clusterPrivileges.read_security,
         });
@@ -64,12 +81,16 @@ export function defineValidPermissionRoutes({
             if (clusterPrivileges.manage_own_api_key) return key;
           });
 
-        return response.ok<{ apiKeys: ApiKey[] }>({
+        const validResponse = response.ok<{ apiKeys: ApiKey[] }>({
           body: {
             // @ts-expect-error Elasticsearch client types do not know about cross-cluster API keys yet.
             apiKeys: validKeys,
           },
         });
+
+        // cache results
+        permissionsFactory(request, validResponse);
+        return validResponse;
       } catch (error) {
         return response.customError(wrapIntoCustomErrorResponse(error));
       }
