6907746797670d3517f3ea94baddd2cd4cd5d6ca
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2024-04-29 14:48:43 +0200

    Simplify code by removing redundant calculations

--- src/plugins/field_formats/common/converters/duration.ts
+++ src/plugins/field_formats/common/converters/duration.ts
@@ -106,13 +106,6 @@ export class DurationFormat extends FieldFormat {
   };
 }
 
-// function to calculate the precision part of the value
-const calculatePrecision = (valueInSeconds: number, unitValue: number, secondsPerUnit: number) => {
-  const unitValueFloor = Math.floor(unitValue);
-  const precision = (valueInSeconds - unitValueFloor * secondsPerUnit) / secondsPerUnit;
-  return unitValueFloor + precision;
-};
-
 // Array of units is to find the first unit duration value that is not 0
 const units = [
   { seconds: 31536000, method: 'asYears' },
@@ -146,13 +139,8 @@ function formatDurationHumanPrecise(
     const unitValue = valueInSeconds / unit.seconds;
     if (unitValue >= 1 || unit === units[units.length - 1]) {
       // return a value if it's the first iteration where the value > 1, or the last iteration
-      // calculate the fractional part of the value based on conversion to seconds
-      // So when 1 year is given as unit value and the raw value in seconds is more than that
-      // the overflow in seconds is used to calculate fractional part of the returned value
-      const finalValue =
-        unit.seconds >= 1 ? calculatePrecision(valueInSeconds, unitValue, unit.seconds) : unitValue;
       const prefix = negativeValue ? '-' : '';
-      return prefix + finalValue.toFixed(outputPrecision) + includeSpace + getUnitText(unit.method);
+      return prefix + unitValue.toFixed(outputPrecision) + includeSpace + getUnitText(unit.method);
     }
   }
 }
