ef095a0ed78d61728a47cfd41b769b756cd8c7ce
Ignacio Rivas <rivasign@gmail.com>
2024-04-29 18:14:29 +0200

    Remove comment

--- x-pack/plugins/index_management/public/application/sections/home/data_stream_list/edit_data_retention_modal/edit_data_retention_modal.tsx
+++ x-pack/plugins/index_management/public/application/sections/home/data_stream_list/edit_data_retention_modal/edit_data_retention_modal.tsx
@@ -285,10 +285,6 @@ export const EditDataRetentionModal: React.FunctionComponent<Props> = ({
             </>
           )}
 
-          {/*
-            We hide the field from the dom instead of not mounting it, otherwise
-            the formlib doesnt initialize the value for the field correctly.
-          */}
           {enableTogglingDataRetention && (
             <UseField
               path="dataRetentionEnabled"
