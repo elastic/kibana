3c85079d242e8a1a30ff0a8a209c71870443834e
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-04-30 16:32:13 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/security_solution/public/common/components/drag_and_drop/draggable_wrapper.tsx
+++ x-pack/plugins/security_solution/public/common/components/drag_and_drop/draggable_wrapper.tsx
@@ -24,14 +24,12 @@ import type { DataProvider } from '../../../timelines/components/timeline/data_p
 import { ROW_RENDERER_BROWSER_EXAMPLE_TIMELINE_ID } from '../../../timelines/components/row_renderers_browser/constants';
 
 import { TruncatableText } from '../truncatable_text';
-import { WithCellActions, WithHoverActions } from '../with_hover_actions';
+import { WithCellActions } from '../with_hover_actions';
 
 import { getDraggableId, getDroppableId } from './helpers';
 import { ProviderContainer } from './provider_container';
 
 import * as i18n from './translations';
-import { useHoverActions } from '../hover_actions/use_hover_actions';
-import { useDraggableKeyboardWrapper } from './draggable_keyboard_wrapper_hook';
 
 // As right now, we do not know what we want there, we will keep it as a placeholder
 export const DragEffects = styled.div``;
