d4872ebff18bd81dc74949972cfc27f7ecf7ec3f
Kevin Lacabane <kevin.lacabane@elastic.co>
2024-05-01 17:50:55 +0200

    [APM] Remove usage of internal client when fetching agent config etags metrics (#173001)
    Closes: https://github.com/elastic/kibana/issues/170031
    Replaces: https://github.com/elastic/elasticsearch/pull/101467
    
    **Problem**
    We need to know if an agent config has been applied at the edge (by APM
    agents). This is determined by comparing the etag (hash) of the config,
    with the etag applied at the edges.
    
    Previously the agent config itself contained this information
    (`config.applied_by_agent`) but when running with fleet this will
    instead be captured in `agent_config` metric documents.
    
    Currently the internal kibana user retrieves the `agent_config` metric
    documents from the APM metric index (`metrics-apm-*` by default). This
    index is configurable by the end-user so can be changed to something the
    internal user doesn't have access to. This is a problem.
    
    **Solution**
    
    This PR replaces the calls made by the internal client with calls made
    by the authenticated end user (via `APMEventClient`). This approach
    works for requests made from the browser/UI but doesn't work for
    background tasks made by fleet. To work around this we only
    conditionally query the metric index if the `APMEventClient` is
    available.
    If `APMEventClient` is not available `applied_by_agent` will be
    `undefined`
    
    (cherry picked from commit 58c79588ff55c74664f40e3267d4da4fa8d5654d)

--- x-pack/plugins/observability_solution/apm/server/routes/assistant_functions/get_observability_alert_details_context/index.ts
+++ x-pack/plugins/observability_solution/apm/server/routes/assistant_functions/get_observability_alert_details_context/index.ts
@@ -8,37 +8,22 @@
 import type { ScopedAnnotationsClient } from '@kbn/observability-plugin/server';
 import type { ElasticsearchClient } from '@kbn/core-elasticsearch-server';
 import type { CoreRequestHandlerContext, Logger } from '@kbn/core/server';
+import {
+  AlertDetailsContextualInsight,
+  AlertDetailsContextualInsightsHandlerQuery,
+} from '@kbn/observability-plugin/server/services';
 import moment from 'moment';
-import * as t from 'io-ts';
-import { LatencyAggregationType } from '../../../../common/latency_aggregation_types';
 import type { MlClient } from '../../../lib/helpers/get_ml_client';
 import type { APMEventClient } from '../../../lib/helpers/create_es_client/create_apm_event_client';
 import type { ApmAlertsClient } from '../../../lib/helpers/get_apm_alerts_client';
 import { getApmServiceSummary } from '../get_apm_service_summary';
 import { getAssistantDownstreamDependencies } from '../get_apm_downstream_dependencies';
 import { getLogCategories } from '../get_log_categories';
-import { ApmTimeseriesType, getApmTimeseries } from '../get_apm_timeseries';
 import { getAnomalies } from '../get_apm_service_summary/get_anomalies';
 import { getServiceNameFromSignals } from './get_service_name_from_signals';
 import { getContainerIdFromSignals } from './get_container_id_from_signals';
-
-export const observabilityAlertDetailsContextRt = t.intersection([
-  t.type({
-    alert_started_at: t.string,
-  }),
-  t.partial({
-    // apm fields
-    'service.name': t.string,
-    'service.environment': t.string,
-    'transaction.type': t.string,
-    'transaction.name': t.string,
-
-    // infrastructure fields
-    'host.name': t.string,
-    'container.id': t.string,
-    'kubernetes.pod.name': t.string,
-  }),
-]);
+import { getApmAlertDetailsContextPrompt } from './get_apm_alert_details_context_prompt';
+import { getExitSpanChangePoints, getServiceChangePoints } from '../get_changepoints';
 
 export async function getObservabilityAlertDetailsContext({
   coreContext,
@@ -50,15 +35,15 @@ export async function getObservabilityAlertDetailsContext({
   mlClient,
   query,
 }: {
-  coreContext: CoreRequestHandlerContext;
+  coreContext: Pick<CoreRequestHandlerContext, 'elasticsearch' | 'uiSettings'>;
   annotationsClient?: ScopedAnnotationsClient;
   apmAlertsClient: ApmAlertsClient;
   apmEventClient: APMEventClient;
   esClient: ElasticsearchClient;
   logger: Logger;
   mlClient?: MlClient;
-  query: t.TypeOf<typeof observabilityAlertDetailsContextRt>;
-}) {
+  query: AlertDetailsContextualInsightsHandlerQuery;
+}): Promise<AlertDetailsContextualInsight[]> {
   const alertStartedAt = query.alert_started_at;
   const serviceEnvironment = query['service.environment'];
   const hostName = query['host.name'];
@@ -182,141 +167,14 @@ export async function getObservabilityAlertDetailsContext({
     anomaliesPromise,
   ]);
 
-  return {
+  return getApmAlertDetailsContextPrompt({
+    serviceName,
+    serviceEnvironment,
     serviceSummary,
     downstreamDependencies,
     logCategories,
     serviceChangePoints,
     exitSpanChangePoints,
     anomalies,
-  };
-}
-
-async function getServiceChangePoints({
-  apmEventClient,
-  alertStartedAt,
-  serviceName,
-  serviceEnvironment,
-  transactionType,
-  transactionName,
-}: {
-  apmEventClient: APMEventClient;
-  alertStartedAt: string;
-  serviceName: string | undefined;
-  serviceEnvironment: string | undefined;
-  transactionType: string | undefined;
-  transactionName: string | undefined;
-}) {
-  if (!serviceName) {
-    return [];
-  }
-
-  const res = await getApmTimeseries({
-    apmEventClient,
-    arguments: {
-      start: moment(alertStartedAt).subtract(12, 'hours').toISOString(),
-      end: alertStartedAt,
-      stats: [
-        {
-          title: 'Latency',
-          'service.name': serviceName,
-          'service.environment': serviceEnvironment,
-          timeseries: {
-            name: ApmTimeseriesType.transactionLatency,
-            function: LatencyAggregationType.p95,
-            'transaction.type': transactionType,
-            'transaction.name': transactionName,
-          },
-        },
-        {
-          title: 'Throughput',
-          'service.name': serviceName,
-          'service.environment': serviceEnvironment,
-          timeseries: {
-            name: ApmTimeseriesType.transactionThroughput,
-            'transaction.type': transactionType,
-            'transaction.name': transactionName,
-          },
-        },
-        {
-          title: 'Failure rate',
-          'service.name': serviceName,
-          'service.environment': serviceEnvironment,
-          timeseries: {
-            name: ApmTimeseriesType.transactionFailureRate,
-            'transaction.type': transactionType,
-            'transaction.name': transactionName,
-          },
-        },
-        {
-          title: 'Error events',
-          'service.name': serviceName,
-          'service.environment': serviceEnvironment,
-          timeseries: {
-            name: ApmTimeseriesType.errorEventRate,
-          },
-        },
-      ],
-    },
   });
-
-  return res
-    .filter((timeseries) => timeseries.changes.length > 0)
-    .map((timeseries) => ({
-      title: timeseries.stat.title,
-      grouping: timeseries.id,
-      changes: timeseries.changes,
-    }));
-}
-
-async function getExitSpanChangePoints({
-  apmEventClient,
-  alertStartedAt,
-  serviceName,
-  serviceEnvironment,
-}: {
-  apmEventClient: APMEventClient;
-  alertStartedAt: string;
-  serviceName: string | undefined;
-  serviceEnvironment: string | undefined;
-}) {
-  if (!serviceName) {
-    return [];
-  }
-
-  const res = await getApmTimeseries({
-    apmEventClient,
-    arguments: {
-      start: moment(alertStartedAt).subtract(30, 'minute').toISOString(),
-      end: alertStartedAt,
-      stats: [
-        {
-          title: 'Exit span latency',
-          'service.name': serviceName,
-          'service.environment': serviceEnvironment,
-          timeseries: {
-            name: ApmTimeseriesType.exitSpanLatency,
-          },
-        },
-        {
-          title: 'Exit span failure rate',
-          'service.name': serviceName,
-          'service.environment': serviceEnvironment,
-          timeseries: {
-            name: ApmTimeseriesType.exitSpanFailureRate,
-          },
-        },
-      ],
-    },
-  });
-
-  return res
-    .filter((timeseries) => timeseries.changes.length > 0)
-    .map((timeseries) => {
-      return {
-        title: timeseries.stat.title,
-        grouping: timeseries.id,
-        changes: timeseries.changes,
-      };
-    });
 }
