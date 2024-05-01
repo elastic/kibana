554fc1b808ee8f10c0410ba8ee7374463bbf3a17
Khristinin Nikita <nikita.khristinin@elastic.co>
2024-05-01 20:58:10 +0200

    Enable value list modal (#181593)
    ## Enable value list modal
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 813741f2dfcce6d62faad10231bcf69ae6d7ffff)

--- x-pack/test/security_solution_cypress/cypress/e2e/detection_response/detection_engine/value_lists/value_list_items.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/detection_response/detection_engine/value_lists/value_list_items.cy.ts
@@ -43,16 +43,7 @@ import { RULES_MANAGEMENT_URL } from '../../../../urls/rules_management';
 describe(
   'Value list items',
   {
-    tags: ['@ess', '@serverless', '@skipInServerlessMKI'],
-    env: {
-      ftrConfig: {
-        kbnServerArgs: [
-          `--xpack.securitySolution.enableExperimental=${JSON.stringify([
-            'valueListItemsModalEnabled',
-          ])}`,
-        ],
-      },
-    },
+    tags: ['@ess', '@serverless'],
   },
   () => {
     beforeEach(() => {
