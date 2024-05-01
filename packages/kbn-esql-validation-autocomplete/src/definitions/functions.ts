5ba6a399f23282603011332b997044fc485fb270
Drew Tate <drew.tate@elastic.co>
2024-05-01 11:32:37 -0600

    [ES|QL] Sorting accepts expressions (#181916)
    ## Summary
    
    `SORT` accepts expressions as of
    https://github.com/elastic/elasticsearch/pull/107158.
    
    This PR updates the validator and autocompletion engine to account for
    this improvement.
    
    ### Checklist
    
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added — looks like docs haven't been added for this feature yet. I
    opened the discussion with ES team
    ([ref](https://github.com/elastic/elasticsearch/pull/107158#issuecomment-2080063533))
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- packages/kbn-esql-validation-autocomplete/src/definitions/functions.ts
+++ packages/kbn-esql-validation-autocomplete/src/definitions/functions.ts
@@ -1692,7 +1692,7 @@ export const evalFunctionsDefinitions: FunctionDefinition[] = [
   .sort(({ name: a }, { name: b }) => a.localeCompare(b))
   .map((def) => ({
     ...def,
-    supportedCommands: ['stats', 'eval', 'where', 'row'],
+    supportedCommands: ['stats', 'eval', 'where', 'row', 'sort'],
     supportedOptions: ['by'],
     type: 'eval',
   }));
