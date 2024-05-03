65f4a7cc5ed8107d0d9ed732a734f8ebaadb4d5f
Kevin Qualters <kevin.qualters@elastic.co>
2024-05-02 21:39:36 -0400

    Update snapshot and fix most tests

--- x-pack/plugins/security_solution/public/common/components/header_actions/actions.tsx
+++ x-pack/plugins/security_solution/public/common/components/header_actions/actions.tsx
@@ -228,7 +228,7 @@ const ActionsComponent: React.FC<ActionProps> = ({
     onEventDetailsPanelOpened();
   }, [activeStep, incrementStep, isTourAnchor, isTourShown, onEventDetailsPanelOpened]);
   const showExpandEvent = useMemo(
-    () => !isEventViewer && timelineId !== TimelineId.active && unifiedComponentsInTimelineEnabled,
+    () => !isEventViewer && timelineId !== TimelineId.active && !unifiedComponentsInTimelineEnabled,
     [isEventViewer, timelineId, unifiedComponentsInTimelineEnabled]
   );
 
