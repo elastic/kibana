68e4bfbc219fd5756b7971f722d96665957082ab
Kyle Pollich <kyle.pollich@elastic.co>
2024-05-03 09:46:54 -0400

    Remove console.log

--- x-pack/plugins/fleet/server/routes/setup/handlers.ts
+++ x-pack/plugins/fleet/server/routes/setup/handlers.ts
@@ -80,7 +80,6 @@ export const fleetSetupHandler: FleetRequestHandler = async (context, request, r
     };
     return response.ok({ body });
   } catch (error) {
-    console.log(JSON.stringify(error, null, 2));
     return defaultFleetErrorHandler({ error, response });
   }
 };
