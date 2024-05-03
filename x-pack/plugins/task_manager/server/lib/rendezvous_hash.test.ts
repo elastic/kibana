a2bb1ddb4d3b06370c02c36bb66652a7849f703e
Mike Cote <michel.cote@elastic.co>
2024-05-03 16:07:27 -0400

    Add extra test

--- x-pack/plugins/task_manager/server/lib/rendezvous_hash.test.ts
+++ x-pack/plugins/task_manager/server/lib/rendezvous_hash.test.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import { flatten, uniq } from 'lodash';
 import { rendezvousHash } from './rendezvous_hash';
 
 test(`two pods`, function () {
@@ -123,4 +124,53 @@ test(`three pods`, function () {
       20,
     ]
   `);
+});
+
+test('thirty two pods', () => {
+  const allPods = [
+    'instance-0000000036 - background tasks',
+    'instance-0000000037 - background tasks',
+    'instance-0000000038 - background tasks',
+    'instance-0000000039 - background tasks',
+    'instance-0000000040 - background tasks',
+    'instance-0000000041 - background tasks',
+    'instance-0000000042 - background tasks',
+    'instance-0000000043 - background tasks',
+    'instance-0000000044 - background tasks',
+    'instance-0000000045 - background tasks',
+    'instance-0000000046 - background tasks',
+    'instance-0000000047 - background tasks',
+    'instance-0000000048 - background tasks',
+    'instance-0000000049 - background tasks',
+    'instance-0000000050 - background tasks',
+    'instance-0000000051 - background tasks',
+    'instance-0000000052 - background tasks',
+    'instance-0000000053 - background tasks',
+    'instance-0000000054 - background tasks',
+    'instance-0000000055 - background tasks',
+    'instance-0000000056 - background tasks',
+    'instance-0000000057 - background tasks',
+    'instance-0000000058 - background tasks',
+    'instance-0000000059 - background tasks',
+    'instance-0000000060 - background tasks',
+    'instance-0000000061 - background tasks',
+    'instance-0000000062 - background tasks',
+    'instance-0000000063 - background tasks',
+    'instance-0000000064 - background tasks',
+    'instance-0000000065 - background tasks',
+    'instance-0000000066 - background tasks',
+    'instance-0000000067 - background tasks',
+  ];
+
+  const allPartitions: number[] = [];
+  for (let i = 0; i < 360; i++) {
+    allPartitions.push(i);
+  }
+
+  const partitionsByPod: number[][] = [];
+  for (const pod of allPods) {
+    partitionsByPod.push(rendezvousHash(pod, allPods, allPartitions, 2));
+  }
+
+  expect(uniq(flatten(partitionsByPod)).length).toEqual(360);
 });
