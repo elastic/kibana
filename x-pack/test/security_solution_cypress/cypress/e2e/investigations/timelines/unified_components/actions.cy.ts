acb31e217d7e152961d77059a8e49cf5b573a464
Kevin Qualters <kevin.qualters@elastic.co>
2024-05-03 07:07:24 +0000

    Fix tests

--- x-pack/test/security_solution_cypress/cypress/e2e/investigations/timelines/unified_components/actions.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/investigations/timelines/unified_components/actions.cy.ts
@@ -7,6 +7,12 @@
 
 import { login } from '../../../../tasks/login';
 import { visitWithTimeRange } from '../../../../tasks/navigation';
+import {
+  ROW_ADD_NOTES_BUTTON,
+  ADD_NOTE_CONTAINER,
+  RESOLVER_GRAPH_CONTAINER,
+} from '../../../../screens/timeline';
+import { OPEN_ANALYZER_BTN } from '../../../../screens/alerts';
 import { openTimelineUsingToggle } from '../../../../tasks/security_main';
 import { createNewTimeline, executeTimelineSearch } from '../../../../tasks/timeline';
 import { ALERTS_URL } from '../../../../urls/navigation';
@@ -14,14 +20,13 @@ import { ALERTS_URL } from '../../../../urls/navigation';
 describe(
   'Timeline row actions',
   {
-    tags: ['@ess', '@serverless', '@skipInServerlessMKI'],
+    tags: ['@ess', '@serverless'],
     env: {
       ftrConfig: {
         kbnServerArgs: [
           `--xpack.securitySolution.enableExperimental=${JSON.stringify([
             'unifiedComponentsInTimelineEnabled',
           ])}`,
-          '--uiSettings.overrides={"theme:darkMode":true}',
         ],
       },
     },
@@ -36,15 +41,15 @@ describe(
     });
 
     it('should render the add note button and display the markdown editor', () => {
-      cy.get('[data-test-subj="timeline-notes-button-small"]').should('be.visible');
-      cy.get('[data-test-subj="timeline-notes-button-small"]').click();
-      cy.get('[data-test-subj="add-note-container"]').should('be.visible');
+      cy.get(ROW_ADD_NOTES_BUTTON).should('be.visible');
+      cy.get(ROW_ADD_NOTES_BUTTON).click();
+      cy.get(ADD_NOTE_CONTAINER).should('be.visible');
     });
 
     it('should render the analyze event button and display the process analyzer visualization', () => {
-      cy.get('[data-test-subj="view-in-analyzer"]').should('be.visible');
-      cy.get('[data-test-subj="view-in-analyzer"]').click();
-      cy.get('[data-test-subj="resolver:graph"]').should('be.visible');
+      cy.get(OPEN_ANALYZER_BTN).should('be.visible');
+      cy.get(OPEN_ANALYZER_BTN).click();
+      cy.get(RESOLVER_GRAPH_CONTAINER).should('be.visible');
     });
   }
 );
