a7120767013c7f23927cf1adb5cdb918ab5742d5
Elena Shostak <elena.shostak@elastic.co>
2024-05-03 11:52:59 +0200

    Lint fix

--- x-pack/test_serverless/functional/page_objects/index.ts
+++ x-pack/test_serverless/functional/page_objects/index.ts
@@ -37,5 +37,5 @@ export const pageObjects = {
   svlTriggersActionsUI: SvlTriggersActionsPageProvider,
   svlRuleDetailsUI: SvlRuleDetailsPageProvider,
   svlManagementPage: SvlManagementPageProvider,
-  svlIngestPipelines: SvlIngestPipelines
+  svlIngestPipelines: SvlIngestPipelines,
 };
