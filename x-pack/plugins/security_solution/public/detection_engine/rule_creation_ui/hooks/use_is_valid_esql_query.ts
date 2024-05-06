b33de39c4f7e34c9ca0d3a9b5641aa3134d494a3
Nikita Indik <nikita.indik@elastic.co>
2024-05-06 23:03:16 +0200

    Update comments

--- x-pack/plugins/security_solution/public/detection_engine/rule_creation_ui/hooks/use_is_valid_esql_query.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_creation_ui/hooks/use_is_valid_esql_query.ts
@@ -8,28 +8,55 @@
 import { useEffect, useRef, useState } from 'react';
 import type { Query } from '@kbn/es-query';
 import type { FormHook } from '@kbn/es-ui-shared-plugin/static/forms/hook_form_lib';
+import type { Type } from '@kbn/securitysolution-io-ts-alerting-types';
+import { isEsqlRule } from '../../../../common/detection_engine/utils';
 
+/**
+ * Runs field validation on the queryBar form field for ES|QL queries
+ * @param query - ES|QL query to retrieve index from
+ * @param ruleType - rule type value
+ * @param isQueryReadEnabled - if not enabled, return empty array. Useful if we know form or query is not valid and we don't want to retrieve index
+ * @returns boolean - true if field is valid, false if field is invalid or if validation is in progress
+ */
 export const useIsValidEsqlQuery = (
   query: Query['query'],
+  ruleType: Type,
   validateFields: FormHook['validateFields']
 ) => {
-  const [isValid, setIsValid] = useState<{ isValid: boolean }>({ isValid: false });
-  const isValidationComplete = useRef<boolean>(false);
+  /*
+    Using an object to store isValid instead of a boolean to ensure 
+    React component re-renders if a valid query changes to another valid query.
+    If boolean was used, React would not re-render the component.
+  */
+  const [validity, setValidity] = useState<{ isValid: boolean }>({ isValid: false });
+  const isValidating = useRef<boolean>(true);
 
   const previousQueryRef = useRef<Query['query'] | undefined>(query);
-  if (previousQueryRef.current !== query) {
+
+  const hasQueryChanged = previousQueryRef.current !== query;
+  if (hasQueryChanged) {
     previousQueryRef.current = query;
-    isValidationComplete.current = false;
+    /*
+      Setting isValidating to true to make the hook return false
+      since the new query is not validated yet.
+    */
+    isValidating.current = true;
   }
 
   useEffect(() => {
-    isValidationComplete.current = false;
+    isValidating.current = true;
 
-    validateFields(['queryBar']).then((validationResult) => {
-      isValidationComplete.current = true;
-      setIsValid({ isValid: validationResult.areFieldsValid ?? false });
-    });
-  }, [query, validateFields]);
+    const esqlQuery = typeof query === 'string' && isEsqlRule(ruleType) ? query : undefined;
 
-  return isValidationComplete.current ? isValid.isValid : false;
+    if (esqlQuery) {
+      validateFields(['queryBar']).then((validationResult) => {
+        isValidating.current = false;
+        setValidity({ isValid: validationResult.areFieldsValid ?? false });
+      });
+    } else {
+      isValidating.current = false;
+    }
+  }, [query, validateFields, ruleType]);
+
+  return isValidating.current ? false : validity.isValid;
 };
