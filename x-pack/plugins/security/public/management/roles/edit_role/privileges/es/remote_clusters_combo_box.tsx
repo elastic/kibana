b2adf3a3a6aa03ec1b2cb69d68ee726b5b46a303
Elena Shostak <elena.shostak@elastic.co>
2024-05-06 12:49:56 +0200

    Fixes

--- x-pack/plugins/security/public/management/roles/edit_role/privileges/es/remote_clusters_combo_box.tsx
+++ x-pack/plugins/security/public/management/roles/edit_role/privileges/es/remote_clusters_combo_box.tsx
@@ -30,7 +30,7 @@ export const RemoteClusterComboBox: React.FunctionComponent<Props> = ({
       incompatible: EuiComboBoxOptionOption[];
     }>(
       (data, item) => {
-        const disabled = item.securityModel !== API_KEY_SECURITY_MODEL && item.name !== 'test-cluster';
+        const disabled = item.securityModel !== API_KEY_SECURITY_MODEL;
 
         if (!disabled) {
           data.remote.push({ label: item.name });
