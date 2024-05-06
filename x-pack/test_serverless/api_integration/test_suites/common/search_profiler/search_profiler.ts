aacc98bb2c25cd592684fd896313c613b6a5e65d
Tre' Seymour <wayne.seymour@elastic.co>
2024-05-06 16:05:45 +0100

    viewer

--- x-pack/test_serverless/api_integration/test_suites/common/search_profiler/search_profiler.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/search_profiler/search_profiler.ts
@@ -20,7 +20,7 @@ export default function ({ getService }: FtrProviderContext) {
 
   describe('Profile', () => {
     before(async () => {
-      roleAuthc = await svlUserManager.createApiKeyForRole('admin');
+      roleAuthc = await svlUserManager.createApiKeyForRole('viewer');
     });
     after(async () => {
       await svlUserManager.invalidateApiKeyForRole(roleAuthc);
