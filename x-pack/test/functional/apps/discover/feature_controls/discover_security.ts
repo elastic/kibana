05c664f3d37d6784991113ace6e920e18e5d73cd
rshen91 <rshen@elastic.co>
2024-05-07 13:35:31 -0600

    remove short url test

--- x-pack/test/functional/apps/discover/feature_controls/discover_security.ts
+++ x-pack/test/functional/apps/discover/feature_controls/discover_security.ts
@@ -119,13 +119,6 @@ export default function (ctx: FtrProviderContext) {
         await globalNav.badgeMissingOrFail();
       });
 
-      it.skip('Permalinks shows create short-url button', async () => {
-        await PageObjects.share.openShareMenuItem('Permalinks');
-        await PageObjects.share.createShortUrlExistOrFail();
-        // close the menu
-        await PageObjects.share.clickShareTopNavButton();
-      });
-
       it('shows CSV reports', async () => {
         await PageObjects.share.clickShareTopNavButton();
         await PageObjects.share.clickTab('Export');
