794a7be5b0da3571a83e1bcfc6c124e393ec6641
Samiul Monir <150824886+Samiul-TheSoccerFan@users.noreply.github.com>
2024-05-07 10:21:29 -0400

    [Serverless ]Fix Playground in Serverless (#182739)
    ## Summary
    
    We encountered an issue in Serverless
    ```core.entry.js:16 TypeError: Cannot read properties of undefined (reading 'reportUiCounter')
        at searchPlayground.chunk.0.js:3:27662
        at Object.useMemo (kbn-ui-shared-deps-npm.dll.js:425:65135)
        at t.useMemo (kbn-ui-shared-deps-npm.dll.js:401:5744)
        at u (searchPlayground.chunk.0.js:3:27646)
        at pe (searchPlayground.chunk.1.js:3:48026)
        at la (kbn-ui-shared-deps-npm.dll.js:425:59212)
        at ec (kbn-ui-shared-deps-npm.dll.js:425:114853)
        at Pc (kbn-ui-shared-deps-npm.dll.js:425:99713)
        at Nc (kbn-ui-shared-deps-npm.dll.js:425:99563)
        at Dc (kbn-ui-shared-deps-npm.dll.js:425:99396)
    ```
    
    This PR includes:
    - Fix Playground loading issue
    - Added Functional testing
    
    ## UX
    ![Screenshot 2024-05-06 at 2 15
    42 PM](https://github.com/elastic/kibana/assets/150824886/27e47cc9-a556-4c61-9b2e-3a327984181b)
    
    ![Screenshot 2024-05-06 at 2 15
    51 PM](https://github.com/elastic/kibana/assets/150824886/f21d22e0-40d2-4a9b-bd68-d673a30e147d)
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [ ] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [ ] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    (cherry picked from commit 2ad67e9f89a1810b41ee98f4011ce23325ffe143)
    
    # Conflicts:
    #	x-pack/test_serverless/functional/page_objects/index.ts

--- x-pack/plugins/search_playground/public/components/view_query/view_query_flyout.tsx
+++ x-pack/plugins/search_playground/public/components/view_query/view_query_flyout.tsx
@@ -102,7 +102,7 @@ export const ViewQueryFlyout: React.FC<ViewQueryFlyoutProps> = ({ onClose }) => 
       ...tempQueryFields,
       [index]: newFields,
     });
-    usageTracker.count(AnalyticsEvents.viewQueryFieldsUpdated, newFields.length);
+    usageTracker?.count(AnalyticsEvents.viewQueryFieldsUpdated, newFields.length);
   };
 
   const saveQuery = () => {
@@ -113,12 +113,12 @@ export const ViewQueryFlyout: React.FC<ViewQueryFlyoutProps> = ({ onClose }) => 
     const groupedQueryFields = groupTypeQueryFields(fields, tempQueryFields);
 
     groupedQueryFields.forEach((typeQueryFields) =>
-      usageTracker.click(`${AnalyticsEvents.viewQuerySaved}_${typeQueryFields}`)
+      usageTracker?.click(`${AnalyticsEvents.viewQuerySaved}_${typeQueryFields}`)
     );
   };
 
   useEffect(() => {
-    usageTracker.load(AnalyticsEvents.viewQueryFlyoutOpened);
+    usageTracker?.load(AnalyticsEvents.viewQueryFlyoutOpened);
   }, [usageTracker]);
 
   return (
