839663b266b2b94016561595d2e27c9c23f738df
Sébastien Loix <sebastien.loix@elastic.co>
2024-05-07 14:05:45 +0100

    Move search nav definition to plugin

--- x-pack/plugins/enterprise_search/public/navigation_tree.ts
+++ x-pack/plugins/enterprise_search/public/navigation_tree.ts
@@ -1,17 +1,17 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
  */
 
-import { i18n } from '@kbn/i18n';
+import { of } from 'rxjs';
+
 import type {
   SolutionNavigationDefinition,
   NavigationTreeDefinition,
 } from '@kbn/core-chrome-browser';
-import { of } from 'rxjs';
+import { i18n } from '@kbn/i18n';
 
 const title = i18n.translate('navigation.searchNav.headerSolutionSwitcher.searchSolutionTitle', {
   defaultMessage: 'Search',
