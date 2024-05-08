758340d07e4341a74c2e5a27bf5752c2d3d49acb
Julia Bardi <julia.bardi@elastic.co>
2024-05-08 15:24:33 +0200

    revert link change

--- x-pack/plugins/fleet/common/services/policy_template.ts
+++ x-pack/plugins/fleet/common/services/policy_template.ts
@@ -22,7 +22,7 @@ const DATA_STREAM_DATASET_VAR: RegistryVarsEntry = {
   type: 'text',
   title: 'Dataset name',
   description:
-    "Set the name for your dataset. Once selected a dataset cannot be changed without creating a new integration policy. You can't use `-` in the name of a dataset and only valid characters for [Elasticsearch index names](https://www.elastic.co/guide/en/elasticsearch/reference/current/indices-create-index.html) are permitted.\n",
+    "Set the name for your dataset. Once selected a dataset cannot be changed without creating a new integration policy. You can't use `-` in the name of a dataset and only valid characters for [Elasticsearch index names](https://www.elastic.co/guide/en/elasticsearch/reference/current/docs-index_.html) are permitted.\n",
   multi: false,
   required: true,
   show_user: true,
