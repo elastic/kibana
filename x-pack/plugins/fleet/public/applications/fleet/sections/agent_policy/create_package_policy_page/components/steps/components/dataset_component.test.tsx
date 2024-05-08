ba026c2caa44b3a734daede73e3a48e34baa96c9
Julia Bardi <julia.bardi@elastic.co>
2024-05-08 15:16:48 +0200

    add tests on dataset component

---
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/dataset_component.test.tsx
@@ -0,0 +1,81 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+
+import { createFleetTestRendererMock } from '../../../../../../../../mock';
+
+import { DatasetComponent } from './dataset_component';
+
+describe('DatasetComponent', () => {
+  function render(value = 'generic', datastreams: any = []) {
+    const renderer = createFleetTestRendererMock();
+    const mockOnChange = jest.fn();
+    const fieldLabel = 'Dataset name';
+
+    const utils = renderer.render(
+      <DatasetComponent
+        pkgName={'log'}
+        datastreams={datastreams}
+        value={{
+          dataset: value,
+          package: 'log',
+        }}
+        onChange={mockOnChange}
+        isDisabled={false}
+        fieldLabel={fieldLabel}
+      />
+    );
+
+    return { utils, mockOnChange };
+  }
+
+  it('should show validation error if dataset is invalid', () => {
+    const { utils } = render();
+
+    const inputEl = utils.getByTestId('comboBoxSearchInput');
+    fireEvent.change(inputEl, { target: { value: 'generic*' } });
+    fireEvent.keyDown(inputEl, { key: 'Enter', code: 'Enter' });
+
+    utils.getByText(
+      'Dataset name must contain only lowercase letters, numbers, dots, hyphens and underscores'
+    );
+  });
+
+  it('should not show validation error if dataset is valid', () => {
+    const { utils } = render();
+
+    const inputEl = utils.getByTestId('comboBoxSearchInput');
+    fireEvent.change(inputEl, { target: { value: 'test' } });
+    fireEvent.keyDown(inputEl, { key: 'Enter', code: 'Enter' });
+
+    expect(
+      utils.queryByText(
+        'Dataset name must contain only lowercase letters, numbers, dots, hyphens and underscores'
+      )
+    ).toBeNull();
+  });
+
+  it('should not show validation error if valid dataset selected from select', () => {
+    const { utils, mockOnChange } = render(undefined, [
+      { dataset: 'fleet_server.test-ds', package: 'log' },
+    ]);
+
+    const inputEl = utils.getByTestId('comboBoxSearchInput');
+    fireEvent.click(inputEl);
+    const option = utils.getByText('fleet_server.test-ds');
+    fireEvent.click(option);
+
+    expect(
+      utils.queryByText(
+        'Dataset name must contain only lowercase letters, numbers, dots, hyphens and underscores'
+      )
+    ).toBeNull();
+    expect(mockOnChange).toHaveBeenCalledWith({ dataset: 'fleet_server.test-ds', package: 'log' });
+  });
+});
