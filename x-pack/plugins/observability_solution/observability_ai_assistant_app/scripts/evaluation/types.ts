704f6465f71ffe03d265fe440bb65a36414b6868
Dario Gieselaar <dario.gieselaar@elastic.co>
2024-05-08 14:36:34 +0200

    [Obs AI Assistant] Fix import issues from /public folders

--- x-pack/plugins/observability_solution/observability_ai_assistant_app/scripts/evaluation/types.ts
+++ x-pack/plugins/observability_solution/observability_ai_assistant_app/scripts/evaluation/types.ts
@@ -6,8 +6,10 @@
  */
 
 import type { Client } from '@elastic/elasticsearch';
-import type { Message } from '@kbn/observability-ai-assistant-plugin/public';
-import { ChatCompletionErrorEvent } from '@kbn/observability-ai-assistant-plugin/common';
+import {
+  type Message,
+  ChatCompletionErrorEvent,
+} from '@kbn/observability-ai-assistant-plugin/common';
 import { KibanaClient } from './kibana_client';
 import { SynthtraceEsClients } from './setup_synthtrace';
 
