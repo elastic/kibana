31eb05a9bc22d3e4d90abc3c0db79506e5befd87
Thom Heymann <thom.heymann@elastic.co>
2024-05-08 08:46:40 +0100

    Enable landing page on stateful

---
+++ x-pack/plugins/observability_solution/observability_onboarding/public/application/onboarding_flow_form/use_scroll.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+const HEADER_OFFSET = 40;
+
+export const useScrollToHash = () => {
+  const location = useLocation();
+
+  const [documentReadyState, setReadyState] = useState(document.readyState);
+  useEffect(() => {
+    const readyStateListener = () => setReadyState(document.readyState);
+    document.addEventListener('readystatechange', readyStateListener);
+    return () =>
+      document.removeEventListener('readystatechange', readyStateListener);
+  }, []);
+
+  useEffect(() => {
+    if (documentReadyState !== 'complete') return; // Wait for page to finish loading before scrolling
+
+    const hash = location.hash.split('?')[0].replace('#', '');
+    const element = hash ? document.getElementById(hash) : null;
+
+    if (element) {
+      element.focus({ preventScroll: true }); // Scrolling already handled below
+
+      window.scrollTo({
+        top: element.offsetTop - HEADER_OFFSET,
+        behavior: 'smooth',
+      });
+    }
+  }, [location.hash, documentReadyState]);
+};
