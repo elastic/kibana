1772f815e0af084e73b4ffa546ccf902c7988425
Robert Oskamp <robert.oskamp@elastic.co>
2024-05-08 14:07:39 +0200

    Skip serverless playground UI tests for MKI

--- x-pack/test_serverless/functional/test_suites/search/playground_overview.ts
+++ x-pack/test_serverless/functional/test_suites/search/playground_overview.ts
@@ -5,21 +5,41 @@
  * 2.0.
  */
 
+import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
-export default function ({ getPageObjects }: FtrProviderContext) {
+export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const pageObjects = getPageObjects(['svlCommonPage', 'svlCommonNavigation', 'svlPlaygroundUI']);
+  const browser = getService('browser');
+
   describe('Playground', function () {
+    // playground UI is currently disabled in MKI
+    this.tags(['failsOnMKI']);
+
     before(async () => {
       await pageObjects.svlCommonPage.login();
-      await pageObjects.svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'searchPlayground' });
     });
 
     after(async () => {
       await pageObjects.svlCommonPage.forceLogout();
     });
 
+    it('navigate to playground from side nav', async () => {
+      await pageObjects.svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'searchPlayground' });
+      await pageObjects.svlCommonNavigation.breadcrumbs.expectBreadcrumbTexts([
+        'Build',
+        'Playground',
+      ]);
+
+      await pageObjects.svlCommonNavigation.sidenav.expectLinkActive({
+        deepLinkId: 'searchPlayground',
+      });
+
+      expect(await browser.getCurrentUrl()).contain('/app/search_playground/chat');
+    });
+
     it('playground app is loaded', async () => {
+      await pageObjects.svlCommonNavigation.sidenav.clickLink({ deepLinkId: 'searchPlayground' });
       await pageObjects.svlPlaygroundUI.PlaygrounStartChatPage.expectPlaygroundStartChatPageComponentsToExist();
       await pageObjects.svlPlaygroundUI.PlaygrounStartChatPage.expectPlaygroundHeaderComponentsToExist();
     });
