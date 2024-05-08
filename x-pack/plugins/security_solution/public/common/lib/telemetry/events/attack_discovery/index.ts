a71c51ce08cde7e2fdf8be5cfd52103c71485c7c
Steph Milovic <stephanie.milovic@elastic.co>
2024-05-08 15:43:57 -0600

    [8.14] [Security solution] Add additional properties to attack discovery telemetry (#182249) (#182997)
    # Backport
    
    This will backport the following commits from `main` to `8.14`:
    - [[Security solution] Add additional properties to attack discovery
    telemetry (#182249)](https://github.com/elastic/kibana/pull/182249)
    
    <!--- Backport version: 8.9.8 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Steph
    Milovic","email":"stephanie.milovic@elastic.co"},"sourceCommit":{"committedDate":"2024-05-02T04:48:36Z","message":"[Security
    solution] Add additional properties to attack discovery telemetry
    (#182249)","sha":"e306074aa9e8943ab342270a6f795e17ee37c1da","branchLabelMapping":{"^v8.15.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","Team:
    SecuritySolution","Team:Security Generative
    AI","v8.14.0","v8.15.0","Feature:Attack
    Discovery"],"number":182249,"url":"https://github.com/elastic/kibana/pull/182249","mergeCommit":{"message":"[Security
    solution] Add additional properties to attack discovery telemetry
    (#182249)","sha":"e306074aa9e8943ab342270a6f795e17ee37c1da"}},"sourceBranch":"main","suggestedTargetBranches":["8.14"],"targetPullRequestStates":[{"branch":"8.14","label":"v8.14.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.15.0","labelRegex":"^v8.15.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/182249","number":182249,"mergeCommit":{"message":"[Security
    solution] Add additional properties to attack discovery telemetry
    (#182249)","sha":"e306074aa9e8943ab342270a6f795e17ee37c1da"}}]}]
    BACKPORT-->

--- x-pack/plugins/security_solution/public/common/lib/telemetry/events/attack_discovery/index.ts
+++ x-pack/plugins/security_solution/public/common/lib/telemetry/events/attack_discovery/index.ts
@@ -18,6 +18,27 @@ export const insightsGeneratedEvent: TelemetryEvent = {
         optional: false,
       },
     },
+    durationMs: {
+      type: 'integer',
+      _meta: {
+        description: 'Duration of request in ms',
+        optional: false,
+      },
+    },
+    alertsCount: {
+      type: 'integer',
+      _meta: {
+        description: 'Number of unique alerts referenced in the attack discoveries',
+        optional: false,
+      },
+    },
+    configuredAlertsCount: {
+      type: 'integer',
+      _meta: {
+        description: 'Number of alerts configured by the user',
+        optional: false,
+      },
+    },
     model: {
       type: 'keyword',
       _meta: {
