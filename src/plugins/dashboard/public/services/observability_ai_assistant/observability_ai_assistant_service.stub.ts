0638905ba31cb9b72597f6445597541c1eb6f11c
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-05-08 11:49:12 +0200

    Fix dashboard stub types

--- src/plugins/dashboard/public/services/observability_ai_assistant/observability_ai_assistant_service.stub.ts
+++ src/plugins/dashboard/public/services/observability_ai_assistant/observability_ai_assistant_service.stub.ts
@@ -5,9 +5,16 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-import { ObservabilityAIAssistantServiceFactory } from './observability_ai_assistant_service';
+import { PluginServiceFactory } from '@kbn/presentation-util-plugin/public';
+import { observabilityAIAssistantPluginMock } from '@kbn/observability-ai-assistant-plugin/public/mock';
+import { ObservabilityAIAssistantService } from './types';
+
+type ObservabilityAIAssistantServiceFactory = PluginServiceFactory<ObservabilityAIAssistantService>;
 
 export const observabilityAIAssistantServiceStubFactory: ObservabilityAIAssistantServiceFactory =
   () => {
-    return {};
+    const pluginMock = observabilityAIAssistantPluginMock.createStartContract();
+    return {
+      start: pluginMock,
+    };
   };
