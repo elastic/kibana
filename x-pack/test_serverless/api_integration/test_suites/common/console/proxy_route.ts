e5859f08efd91fb3f5d47c6b6e53eac251d0cc06
Tre' Seymour <wayne.seymour@elastic.co>
2024-05-08 11:49:29 +0100

    use default role and and some debug

--- x-pack/test_serverless/api_integration/test_suites/common/console/proxy_route.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/console/proxy_route.ts
@@ -11,47 +11,58 @@ import { RoleCredentials } from '../../../../shared/services';
 
 export default function ({ getService }: FtrProviderContext) {
   const svlCommonApi = getService('svlCommonApi');
+  const log = getService('log');
   const svlUserManager = getService('svlUserManager');
-  const supertest = getService('supertest');
+  const supertestWithoutAuth = getService('supertestWithoutAuth');
   let roleAuthc: RoleCredentials;
 
   describe('POST /api/console/proxy', () => {
     before(async () => {
-      roleAuthc = await svlUserManager.createApiKeyForRole('viewer');
+      roleAuthc = await svlUserManager.createApiKeyForDefaultRole();
     });
     after(async () => {
       await svlUserManager.invalidateApiKeyForRole(roleAuthc);
     });
     describe('system indices behavior', () => {
       it('returns warning header when making requests to .kibana index', async () => {
-        return await supertest
+        return await supertestWithoutAuth
           .post('/api/console/proxy?method=GET&path=/.kibana/_settings')
           .set('kbn-xsrf', 'true')
           .set(svlCommonApi.getInternalRequestHeader())
           .set(roleAuthc.apiKeyHeader)
-          .then((response) => {
-            expect(response.header).to.have.property('warning');
-            const { warning } = response.header as { warning: string };
-            expect(warning.startsWith('299')).to.be(true);
-            expect(warning.includes('system indices')).to.be(true);
-          });
+          .then(assertOn('returns warning header when making requests to .kibana index'));
       });
 
       it('does not forward x-elastic-product-origin', async () => {
         // If we pass the header and we still get the warning back, we assume that the header was not forwarded.
-        return await supertest
+        return await supertestWithoutAuth
           .post('/api/console/proxy?method=GET&path=/.kibana/_settings')
           .set('kbn-xsrf', 'true')
           .set(svlCommonApi.getInternalRequestHeader())
           .set('x-elastic-product-origin', 'kibana')
           .set(roleAuthc.apiKeyHeader)
-          .then((response) => {
-            expect(response.header).to.have.property('warning');
-            const { warning } = response.header as { warning: string };
-            expect(warning.startsWith('299')).to.be(true);
-            expect(warning.includes('system indices')).to.be(true);
-          });
+          .then(assertOn('does not forward x-elastic-product-origin'));
       });
+
+      function assertOn(
+        testName:
+          | 'returns warning header when making requests to .kibana index'
+          | 'does not forward x-elastic-product-origin'
+      ) {
+        return function assertOnResponse(response: any) {
+          log.debug(`Running assertions on ${testName}`);
+          expect(response.header).to.have.property('warning');
+          const { warning } = response.header as { warning: string };
+          expect(warning.startsWith('299')).to.eql(
+            true,
+            `Expect warning.startsWith('299'), but got: [${warning}]`
+          );
+          expect(warning.includes('system indices')).to.eql(
+            true,
+            `Expect warning.includes('system indices'), but got: [${warning}]`
+          );
+        };
+      }
     });
   });
 }
