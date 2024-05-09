fd43740e3e0acbff9e06dfcf2ac30dc5e6ca612b
Nick Peihl <nick.peihl@elastic.co>
2024-05-09 15:55:43 -0400

    Revert "skip failing test suite (#183067)"
    This reverts commit 484eb9ecb61cc3735702162d96e3ea719eebe00a.

--- x-pack/test/functional/apps/maps/group4/add_layer_panel.js
+++ x-pack/test/functional/apps/maps/group4/add_layer_panel.js
@@ -12,8 +12,7 @@ export default function ({ getService, getPageObjects }) {
   const PageObjects = getPageObjects(['maps']);
   const security = getService('security');
 
-  // Failing: See https://github.com/elastic/kibana/issues/183067
-  describe.skip('Add layer panel', () => {
+  describe('Add layer panel', () => {
     before(async () => {
       await security.testUser.setRoles(['global_maps_all', 'test_logstash_reader']);
       await PageObjects.maps.openNewMap();
