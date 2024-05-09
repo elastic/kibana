d0f42781ad09db255a010eb37b7c00fa741df8c3
Jonathan Budzenski <jon@elastic.co>
2024-05-09 16:00:12 -0500

    fix

--- .buildkite/scripts/pipelines/pull_request/pipeline.ts
+++ .buildkite/scripts/pipelines/pull_request/pipeline.ts
@@ -52,19 +52,8 @@ const uploadPipeline = (pipelineContent: string | object) => {
     }
 
     const pipeline = [];
-    let removeBaseSteps = false;
-    // Start project build in parallel with quick_checks and snapshot build to reduce time to deploy
-    if (
-      GITHUB_PR_LABELS.includes('ci:project-deploy-elasticsearch') ||
-      GITHUB_PR_LABELS.includes('ci:project-deploy-observability') ||
-      GITHUB_PR_LABELS.includes('ci:project-deploy-security') ||
-      GITHUB_PR_LABELS.includes('ci:build-serverless-image')
-    ) {
-      pipeline.push(getPipeline('.buildkite/pipelines/pull_request/build_project.yml', false));
-      removeBaseSteps = true;
-    }
 
-    pipeline.push(getPipeline('.buildkite/pipelines/pull_request/base.yml', removeBaseSteps));
+    pipeline.push(getPipeline('.buildkite/pipelines/pull_request/base.yml', false));
 
     if (await doAnyChangesMatch([/^packages\/kbn-handlebars/])) {
       pipeline.push(getPipeline('.buildkite/pipelines/pull_request/kbn_handlebars.yml'));
@@ -167,7 +156,11 @@ const uploadPipeline = (pipelineContent: string | object) => {
       GITHUB_PR_LABELS.includes('ci:project-deploy-observability') ||
       GITHUB_PR_LABELS.includes('ci:project-deploy-security')
     ) {
-      pipeline.push(getPipeline('.buildkite/pipelines/pull_request/deploy_project.yml', false));
+      pipeline.push(getPipeline('.buildkite/pipelines/pull_request/deploy_project.yml'));
+    }
+
+    if (GITHUB_PR_LABELS.includes('ci:build-serverless-image')) {
+      pipeline.push(getPipeline('.buildkite/pipelines/artifacts_container_image.yml'));
     }
 
     if (
@@ -331,7 +324,7 @@ const uploadPipeline = (pipelineContent: string | object) => {
     }
 
     pipeline.push(getPipeline('.buildkite/pipelines/pull_request/post_build.yml'));
-    console.log(JSON.stringify(pipeline));
+
     // remove duplicated steps
     uploadPipeline([...new Set(pipeline)].join('\n'));
   } catch (ex) {
