2decf9547db4b1ed73250492797aa82c89ee3f8d
Joseph McElroy <joseph.mcelroy@elastic.co>
2024-05-10 16:52:41 +0100

    update indention and fix a few typos

--- x-pack/plugins/search_playground/public/components/view_code/examples/utils.test.ts
+++ x-pack/plugins/search_playground/public/components/view_code/examples/utils.test.ts
@@ -27,27 +27,25 @@ describe('getESQuery', () => {
         },
       },
     };
-    const expectedResult = `{
-  "query": {
-    "match": {
-      "field": query
-    }
-  }
-}`;
-    expect(getESQuery(query)).toEqual(expectedResult);
-  });
-
-  it('should handle invalid input gracefully', () => {
-    // Test when invalid input is provided
-    const invalidQuery = 'not a valid query';
-    // In this case, since the input is invalid, it should return an empty object
-    expect(getESQuery(invalidQuery)).toEqual('{}');
-  });
-
-  it('should handle empty input gracefully', () => {
-    // Test when empty input is provided
-    const emptyQuery = {};
-    // In this case, since the input is empty, it should return an empty object
-    expect(getESQuery(emptyQuery)).toEqual('{}');
+    expect(getESQuery(query)).toMatchInlineSnapshot(`
+      "{
+              \\"query\\": {
+                  \\"bool\\": {
+                      \\"should\\": [
+                          {
+                              \\"match\\": {
+                                  \\"field\\": query
+                              }
+                          },
+                          {
+                              \\"match\\": {
+                                  \\"field\\": query
+                              }
+                          }
+                      ]
+                  }
+              }
+          }"
+    `);
   });
 });
