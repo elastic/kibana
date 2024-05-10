63eb0c2abb43032843cfd122b745b8f9d9618126
Maxim Palenov <maxim.palenov@elastic.co>
2024-05-10 16:08:13 +0200

    add security solution OAS bundling to PR pipeline

--- x-pack/plugins/security_solution/scripts/openapi/bundle.js
+++ x-pack/plugins/security_solution/scripts/openapi/bundle.js
@@ -7,14 +7,18 @@
 
 require('../../../../../src/setup_node_env');
 const { bundle } = require('@kbn/openapi-bundler');
-const { join, resolve } = require('path');
+const { join } = require('path');
 
-const SECURITY_SOLUTION_ROOT = resolve(__dirname, '../..');
+const SECURITY_SOLUTION_ROOT = join(__dirname, '..', '..');
 
 bundle({
   sourceGlob: join(SECURITY_SOLUTION_ROOT, 'common/api/**/*.schema.yaml'),
   outputFilePath: join(
     SECURITY_SOLUTION_ROOT,
-    'target/openapi/security_solution-{version}.bundled.schema.yaml'
+    'docs/openapi/serverless/security_solution_{version}.bundled.schema.yaml'
   ),
+  specInfo: {
+    title: 'Elastic Security APIs',
+    description: 'You can use these APIs to interface with Elastic Security features.',
+  },
 });
