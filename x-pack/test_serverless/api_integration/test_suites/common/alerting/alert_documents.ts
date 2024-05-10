6a46bf7fe9bb74c92c2bae3cbd03bf61a003750f
Tre' Seymour <wayne.seymour@elastic.co>
2024-05-10 10:52:30 +0100

    both blocks passing now

--- x-pack/test_serverless/api_integration/test_suites/common/alerting/alert_documents.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/alerting/alert_documents.ts
@@ -204,92 +204,98 @@ export default function ({ getService }: FtrProviderContext) {
       expect(hits1).to.eql(expected);
     });
 
-    // it('should update an alert document for an ongoing alert', async () => {
-    //   const createdRule = await createEsQueryRule({
-    //     supertest,
-    //     consumer: 'alerts',
-    //     name: 'always fire',
-    //     ruleTypeId: RULE_TYPE_ID,
-    //     params: {
-    //       size: 100,
-    //       thresholdComparator: '>',
-    //       threshold: [-1],
-    //       index: ['alert-test-data'],
-    //       timeField: 'date',
-    //       esQuery: JSON.stringify({ query: { match_all: {} } }),
-    //       timeWindowSize: 20,
-    //       timeWindowUnit: 's',
-    //     },
-    //   });
-    //   ruleId = createdRule.id;
-    //   objectsToRemove = add('default', ruleId, 'rule', 'alerting')(objectsToRemove)
-    //
-    //   // get the first alert document written
-    //   const testStart1 = new Date();
-    //   await waitForNumRuleRuns({
-    //     supertest,
-    //     numOfRuns: 1,
-    //     ruleId,
-    //     esClient,
-    //     testStart: testStart1,
-    //   });
-    //
-    //   const alResp1 = await waitForAlertInIndex({
-    //     esClient,
-    //     filter: testStart1,
-    //     indexName: ALERT_INDEX,
-    //     ruleId,
-    //     num: 1,
-    //   });
-    //
-    //   // wait for another run, get the updated alert document
-    //   const testStart2 = new Date();
-    //   await waitForNumRuleRuns({
-    //     supertest,
-    //     numOfRuns: 1,
-    //     ruleId,
-    //     esClient,
-    //     testStart: testStart2,
-    //   });
-    //
-    //   const alResp2 = await waitForAlertInIndex({
-    //     esClient,
-    //     filter: testStart2,
-    //     indexName: ALERT_INDEX,
-    //     ruleId,
-    //     num: 1,
-    //   });
-    //
-    //   // check for differences we can check and expect
-    //   const hits1 = alResp1.hits.hits[0]._source as Record<string, any>;
-    //   const hits2 = alResp2.hits.hits[0]._source as Record<string, any>;
-    //
-    //   expect(hits2['@timestamp']).to.be.greaterThan(hits1['@timestamp']);
-    //   expect(OPEN_OR_ACTIVE.has(hits1[EVENT_ACTION])).to.be(true);
-    //   expect(hits2[EVENT_ACTION]).to.be('active');
-    //   expect(hits1[ALERT_DURATION]).to.not.be.lessThan(0);
-    //   expect(hits2[ALERT_DURATION]).not.to.be(0);
-    //   expect(hits2[ALERT_RULE_EXECUTION_TIMESTAMP]).to.eql(hits2['@timestamp']);
-    //   expect(hits2[ALERT_CONSECUTIVE_MATCHES]).to.be.greaterThan(hits1[ALERT_CONSECUTIVE_MATCHES]);
-    //
-    //   // remove fields we know will be different
-    //   const fields = [
-    //     '@timestamp',
-    //     'event.action',
-    //     'kibana.alert.duration.us',
-    //     'kibana.alert.flapping_history',
-    //     'kibana.alert.reason',
-    //     'kibana.alert.rule.execution.uuid',
-    //     'kibana.alert.rule.execution.timestamp',
-    //     'kibana.alert.consecutive_matches',
-    //   ];
-    //
-    //   for (const field of fields) {
-    //     unset(hits1, field);
-    //     unset(hits2, field);
-    //   }
-    //
-    //   expect(hits1).to.eql(hits2);
-    // });
+    it('should update an alert document for an ongoing alert', async () => {
+      const createdRule = await createEsQueryRule({
+        supertestWithoutAuth,
+        roleAuthc,
+        internalReqHeader,
+        consumer: 'alerts',
+        name: 'always fire',
+        ruleTypeId: RULE_TYPE_ID,
+        params: {
+          size: 100,
+          thresholdComparator: '>',
+          threshold: [-1],
+          index: ['alert-test-data'],
+          timeField: 'date',
+          esQuery: JSON.stringify({ query: { match_all: {} } }),
+          timeWindowSize: 20,
+          timeWindowUnit: 's',
+        },
+      });
+      ruleId = createdRule.id;
+      objectsToRemove = add('default', ruleId, 'rule', 'alerting')(objectsToRemove);
+
+      // get the first alert document written
+      const testStart1 = new Date();
+      await waitForNumRuleRuns({
+        supertestWithoutAuth,
+        roleAuthc,
+        internalReqHeader,
+        numOfRuns: 1,
+        ruleId,
+        esClient,
+        testStart: testStart1,
+      });
+
+      const alResp1 = await waitForAlertInIndex({
+        esClient,
+        filter: testStart1,
+        indexName: ALERT_INDEX,
+        ruleId,
+        num: 1,
+      });
+
+      // wait for another run, get the updated alert document
+      const testStart2 = new Date();
+      await waitForNumRuleRuns({
+        supertestWithoutAuth,
+        roleAuthc,
+        internalReqHeader,
+        numOfRuns: 1,
+        ruleId,
+        esClient,
+        testStart: testStart2,
+      });
+
+      const alResp2 = await waitForAlertInIndex({
+        esClient,
+        filter: testStart2,
+        indexName: ALERT_INDEX,
+        ruleId,
+        num: 1,
+      });
+
+      // check for differences we can check and expect
+      const hits1 = alResp1.hits.hits[0]._source as Record<string, any>;
+      const hits2 = alResp2.hits.hits[0]._source as Record<string, any>;
+
+      expect(hits2['@timestamp']).to.be.greaterThan(hits1['@timestamp']);
+      expect(OPEN_OR_ACTIVE.has(hits1[EVENT_ACTION])).to.be(true);
+      expect(hits2[EVENT_ACTION]).to.be('active');
+      expect(hits1[ALERT_DURATION]).to.not.be.lessThan(0);
+      expect(hits2[ALERT_DURATION]).not.to.be(0);
+      expect(hits2[ALERT_RULE_EXECUTION_TIMESTAMP]).to.eql(hits2['@timestamp']);
+      expect(hits2[ALERT_CONSECUTIVE_MATCHES]).to.be.greaterThan(hits1[ALERT_CONSECUTIVE_MATCHES]);
+
+      // remove fields we know will be different
+      const fields = [
+        '@timestamp',
+        'event.action',
+        'kibana.alert.duration.us',
+        'kibana.alert.flapping_history',
+        'kibana.alert.reason',
+        'kibana.alert.rule.execution.uuid',
+        'kibana.alert.rule.execution.timestamp',
+        'kibana.alert.consecutive_matches',
+      ];
+
+      for (const field of fields) {
+        unset(hits1, field);
+        unset(hits2, field);
+      }
+
+      expect(hits1).to.eql(hits2);
+    });
   });
 }
