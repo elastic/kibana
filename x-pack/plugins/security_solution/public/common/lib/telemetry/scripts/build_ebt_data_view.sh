2315adc977007618e3e4456463c3bbb369deed6b
Steph Milovic <stephanie.milovic@elastic.co>
2024-05-10 17:48:01 -0600

    wip

--- x-pack/plugins/security_solution/public/common/lib/telemetry/scripts/build_ebt_data_view.sh
+++ x-pack/plugins/security_solution/public/common/lib/telemetry/scripts/build_ebt_data_view.sh
@@ -7,4 +7,42 @@
 # 2.0.
 #
 
-npx ts-node "$(dirname "${0}")/build_ebt_data_view.ts" "$@"
+# Set default values for optional arguments if not provided
+kibana_url="https://telemetry-v2-staging.elastic.dev"
+space_id="securitysolution"
+data_view_name="security-solution-ebt-kibana-browser"
+
+# Parse named arguments
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --api_key=*)
+      api_key="${1#*=}"
+      ;;
+    --kibana_url=*)
+      kibana_url="${1#*=}"
+      ;;
+    --space_id=*)
+      space_id="${1#*=}"
+      ;;
+    --data_view_name=*)
+      data_view_name="${1#*=}"
+      ;;
+    *)
+      echo "Error: Invalid argument: $1" >&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# Check if mandatory arguments are provided
+if [ -z "$api_key" ]; then
+  echo "Error: api_key is a mandatory argument." >&2
+  exit 1
+fi
+
+npx ts-node "$(dirname "${0}")/build_ebt_data_view.ts" \
+  --api_key="$api_key" \
+  --kibana_url="$kibana_url" \
+  --space_id="$space_id" \
+  --data_view_name="$data_view_name"
--- x-pack/plugins/security_solution/public/common/lib/telemetry/scripts/build_ebt_data_view.ts
+++ x-pack/plugins/security_solution/public/common/lib/telemetry/scripts/build_ebt_data_view.ts
@@ -6,6 +6,7 @@
  */
 
 import { ToolingLog } from '@kbn/tooling-log';
+import axios from 'axios';
 
 const logger = new ToolingLog({
   level: 'info',
@@ -13,10 +14,81 @@ const logger = new ToolingLog({
 });
 
 cli()
-  .then(() => logger.success('Done'))
+  .then(() => logger.success('End ebt data view update'))
   .catch((e) => logger.error(e));
 
 async function cli(): Promise<void> {
-  console.log('hi hi');
-  logger.info(`hey hey`);
+  logger.info(`Begin ebt data view update`);
+  const args = process.argv.slice(2); // Ignore first two arguments (path to node and script file)
+
+  const namedArgs: { [key: string]: string } = {};
+
+  // Parse named arguments
+  args.forEach((arg) => {
+    // this strategy ensures that the value can contain an equals sign
+    const [key, ...valueParts] = arg.split('=');
+    const value = valueParts.join('=');
+    namedArgs[key.replace('--', '')] = value;
+  });
+
+  // Access named arguments
+  const {
+    api_key: apiKey,
+    kibana_url: kibanaUrl,
+    space_id: spaceId,
+    data_view_name: dataViewName,
+  } = namedArgs;
+  logger.info(`API key: ${apiKey}`);
+  logger.info(`Kibana URL: ${kibanaUrl}`);
+  logger.info(`Space ID: ${spaceId}`);
+  logger.info(`Data view name: ${dataViewName}`);
+  const requestHeaders = {
+    Authorization: `ApiKey ${apiKey}`,
+    'kbn-xsrf': 'xxx',
+    'Content-Type': 'application/json',
+  };
+  logger.info(`requestHeaders ID: ${requestHeaders}`);
+  const dataViewApiUrl = `${removeTrailingSlash(kibanaUrl)}/s/${spaceId}/api/data_views`;
+
+  try {
+    logger.info(`Fetching data view "${dataViewName}"...`);
+    const dataViews = await axios.get(dataViewApiUrl, {
+      headers: requestHeaders,
+    });
+    console.log('dataViews', dataViews);
+
+    logger.info(`Data view "" has been fetched`);
+  } catch (e) {
+    logger.error(`Error fetching data view "${dataViewName}" - ${e}`);
+    throw e;
+  }
+
+  // const runtimeFieldApiUrl = `${dataViewApiUrl}/data_view/${dataViewName}/runtime_field`;
+  //
+  // try {
+  //   logger.info(`Fetching data view "${data_view_name}"...`);
+  //   await axios.put(
+  //     `${KIBANA_URL}/api/security/role/${role}`,
+  //     {
+  //       elasticsearch: selectedRoleDefinition.elasticsearch,
+  //       kibana: selectedRoleDefinition.kibana,
+  //     },
+  //     {
+  //       headers: requestHeaders,
+  //     }
+  //   );
+  //
+  //   logger.info(`Role "${role}" has been created`);
+  // } catch (e) {
+  //   logger.error(`Unable to create role "${role}"`);
+  //   throw e;
+  // }
+}
+
+function removeTrailingSlash(url: string) {
+  if (url.endsWith('/')) {
+    return url.slice(0, -1);
+  } else {
+    return url;
+  }
 }
