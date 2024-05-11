b9bd4884ced84e9eab9fc34c8766c43ef6d0ceba
Maxim Palenov <maxim.palenov@elastic.co>
2024-05-11 08:54:38 +0200

    add security solution domains OAS bundling to PR pipeline

--- x-pack/plugins/security_solution/scripts/openapi/bundle.js
+++ x-pack/plugins/security_solution/scripts/openapi/bundle.js
@@ -7,14 +7,32 @@
 
 require('../../../../../src/setup_node_env');
 const { bundle } = require('@kbn/openapi-bundler');
-const { join, resolve } = require('path');
+const { join } = require('path');
 
-const SECURITY_SOLUTION_ROOT = resolve(__dirname, '../..');
+const SECURITY_SOLUTION_ROOT = join(__dirname, '..', '..');
 
 bundle({
-  sourceGlob: join(SECURITY_SOLUTION_ROOT, 'common/api/**/*.schema.yaml'),
+  sourceGlob: join(SECURITY_SOLUTION_ROOT, 'common/api/endpoint/**/*.schema.yaml'),
   outputFilePath: join(
     SECURITY_SOLUTION_ROOT,
-    'target/openapi/security_solution-{version}.bundled.schema.yaml'
+    'docs/openapi/serverless/security_solution_endpoint_{version}.bundled.schema.yaml'
   ),
+  specInfo: {
+    title: 'Endpoint management API',
+    description:
+      'Endpoint management APIs allow you to interact with and manage endpoints running the Elastic Defend integration.',
+  },
+});
+
+bundle({
+  sourceGlob: join(SECURITY_SOLUTION_ROOT, 'common/api/detection_engine/**/*.schema.yaml'),
+  outputFilePath: join(
+    SECURITY_SOLUTION_ROOT,
+    'docs/openapi/serverless/security_solution_detection_engine_{version}.bundled.schema.yaml'
+  ),
+  specInfo: {
+    title: 'Detections API',
+    description:
+      'You can create rules that automatically turn events and external alerts sent to Elastic Security into detection alerts. These alerts are displayed on the Detections page.',
+  },
 });
