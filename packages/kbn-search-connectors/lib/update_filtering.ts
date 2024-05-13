0932811988b7b87f91bccbab69b4a4df03c7c1bc
Jedr Blaszyk <jedrazb@gmail.com>
2024-05-13 14:44:44 +0200

    Update packages/kbn-search-connectors/lib/update_filtering.ts
    Co-authored-by: Rodney Norris <rodney@tattdcodemonkey.com>

--- packages/kbn-search-connectors/lib/update_filtering.ts
+++ packages/kbn-search-connectors/lib/update_filtering.ts
@@ -23,7 +23,7 @@ export const updateFiltering = async (
 
   if (activateDraftFilteringResult.result === 'updated') {
     const connector = await fetchConnectorById(client, connectorId);
-    return connector?.filtering[0].active;
+    return connector?.filtering?.[0]?.active;
   }
   return undefined;
 };
