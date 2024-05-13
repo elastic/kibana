551e1232b870654d036aff4d52f43443ee3ecf20
Dario Gieselaar <dario.gieselaar@elastic.co>
2024-05-13 13:44:43 +0200

    [Obs AI Assistant] Fix import issues from /public folders (#182940)
    Make sure the evaluation script doesn't import from `public` folders
    that include code that cannot run in Node.js.
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/observability_solution/observability_ai_assistant_app/scripts/evaluation/scenarios/alerts/index.spec.ts
+++ x-pack/plugins/observability_solution/observability_ai_assistant_app/scripts/evaluation/scenarios/alerts/index.spec.ts
@@ -11,7 +11,7 @@ import expect from '@kbn/expect';
 import { RuleResponse } from '@kbn/alerting-plugin/common/routes/rule/response/types/v1';
 import moment from 'moment';
 import { apm, timerange } from '@kbn/apm-synthtrace-client';
-import { MessageRole } from '@kbn/observability-ai-assistant-plugin/public';
+import { MessageRole } from '@kbn/observability-ai-assistant-plugin/common';
 import { chatClient, kibanaClient, synthtraceEsClients, logger } from '../../services';
 import {
   apmTransactionRateAIAssistant,
