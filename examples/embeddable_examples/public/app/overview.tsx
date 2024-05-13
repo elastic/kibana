fd0816f75d94cf06737f43b05ac58f944fdde2a9
Nathan Reese <reese.nathan@gmail.com>
2024-05-13 10:12:45 -0600

    eslint

--- examples/embeddable_examples/public/app/overview.tsx
+++ examples/embeddable_examples/public/app/overview.tsx
@@ -7,51 +7,19 @@
  */
 
 import React from 'react';
+import { css } from '@emotion/react';
 import { EuiMarkdownFormat } from '@elastic/eui';
 // @ts-ignore
 import overviewMarkdown from '!!raw-loader!@kbn/embeddable-plugin/README.md';
 
-import { EuiText } from '@elastic/eui';
-
 export const Overview = () => {
   return (
-    <EuiText>
-      <p>
-        Embeddables are React components that manage their own state, can be serialized and
-        deserialized, and return an API that can be used to interact with them imperatively.
-      </p>
-
-      <EuiMarkdownFormat>
-        {overviewMarkdown}
-      </EuiMarkdownFormat>
-
-      <h3>Guiding principles</h3>
-      <ul>
-        <li>
-          <strong>Coupled to React</strong>
-          Kibana is a React application, and the minimum unit of sharing is the React component. Embeddables enforce this by requiring a React component.
-        </li>
-        <li>
-          <strong>Composition over inheritence</strong>
-          Rather than an inheritance-based system with classes, imperative APIs are plain old typescript objects that implement any number of shared interfaces.
-          Interfaces are enforced via type guards and are shared via Packages.
-        </li>
-        <li>
-          <strong>Internal state management</strong>
-        </li>
-        <li>
-          <strong>Minimal API surface area</strong>
-        </li>
-      </ul>
-
-      <h3>Managing state</h3>
-
-      <h3>Imperative API</h3>
-
-      <h3>Embeddable vs React component</h3>
-      Prefer React components. Only use Embeddables if your component should be rendered on Dashboard
-
-      <h3>Rendering non-react embeddable</h3>
-    </EuiText>
+    <EuiMarkdownFormat
+      css={css`
+        width: 75%;
+      `}
+    >
+      {overviewMarkdown}
+    </EuiMarkdownFormat>
   );
 };
--- src/plugins/embeddable/README.md
+++ src/plugins/embeddable/README.md
@@ -1,4 +1,15 @@
-# Documentation and examples
+Embeddables are React components that manage their own state, can be serialized and deserialized, and return an API that can be used to interact with them imperatively.
 
-Embeddable documentation and examples are bundled with Kibana and accessable by running `yarn start --run-examples`.
+### Guiding principles
+* **Coupled to React:** Kibana is a React application, and the minimum unit of sharing is the React component. Embeddables enforce this by requiring a React component.
+* **Composition over inheritence:** Rather than an inheritance-based system with classes, imperative APIs are plain old typescript objects that implement any number of shared interfaces. Interfaces are enforced via type guards and are shared via Packages.
+* **Internal state management:** 
+
+### Best practices
+* **Do not use Embeddables to share Components between plugins: **
+* **Do not use Embeddables to avoid circular plugin dependencies: **
+* **Minimal API surface area: **
+
+### Developer docs and examples
+Access developer docs and examples by running `yarn start --run-examples`.
 Navigate to `http://localhost:5601/app/embeddablesApp`.
