0917e44b5c50db825ae6679149173d64fc749443
Panagiota Mitsopoulou <giota85@gmail.com>
2024-05-13 15:10:46 +0200

    fix ftr_provider_context

--- x-pack/test/observability_solution_api_integration/ftr_provider_context.d.ts
+++ x-pack/test/observability_solution_api_integration/ftr_provider_context.d.ts
@@ -4,8 +4,6 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { GenericFtrProviderContext } from '@kbn/test';
+import type { FtrProviderContext } from '../../test_serverless/api_integration/ftr_provider_context';
 
-import { services } from '../api_integration/services';
-
-export type FtrProviderContext = GenericFtrProviderContext<typeof services, {}>;
+export type { FtrProviderContext };
