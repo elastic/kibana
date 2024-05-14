a0c1575512f6864d5ab08f1cac63b474e48b6cc1
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2024-05-14 12:52:41 +0200

    [EDR Workflows] Fix flaky DW automated_response_actions test (#183274)
    (cherry picked from commit a38dc221403670df10669b25c357a5dbacf1b91e)

--- x-pack/plugins/security_solution/public/management/cypress/e2e/automated_response_actions/automated_response_actions.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/automated_response_actions/automated_response_actions.cy.ts
@@ -100,7 +100,7 @@ describe.skip(
         visitRuleAlerts(ruleName);
         closeAllToasts();
 
-        changeAlertsFilter('process.name: "agentbeat"');
+        changeAlertsFilter(`process.name: "agentbeat" and agent.id: "${createdHost.agentId}"`);
         cy.getByTestSubj('expand-event').eq(0).click();
         cy.getByTestSubj('securitySolutionFlyoutNavigationExpandDetailButton').click();
         cy.getByTestSubj('securitySolutionFlyoutResponseTab').click();
