d906a47ac90e37ad66966895128524e30fa7cfd4
dkirchan <diamantis.kirchantzoglou@elastic.co>
2024-05-14 12:29:20 +0300

    WIP : Testing chromium installation

--- .buildkite/scripts/pipelines/security_solution_quality_gate/upload_image_metadata.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/upload_image_metadata.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-echo "$KIBANA_DOCKER_PASSWORD" | docker login -u "$KIBANA_DOCKER_USERNAME" --password-stdin docker.elastic.co
 
-KIBANA_BASE_IMAGE="docker.elastic.co/kibana-ci/kibana-serverless"
-KIBANA_LATEST=${KIBANA_BASE_IMAGE}:latest
+sudo apt update && apt install -y chromium-browser
 
-if [ -v KIBANA_MKI_IMAGE_COMMIT ]; then
-    KBN_IMAGE=${KIBANA_BASE_IMAGE}:git-${KIBANA_MKI_IMAGE_COMMIT:0:12}
-else
-    KBN_IMAGE=${KIBANA_LATEST}
-fi
+echo "Done"
+# echo "$KIBANA_DOCKER_PASSWORD" | docker login -u "$KIBANA_DOCKER_USERNAME" --password-stdin docker.elastic.co
 
-docker pull ${KBN_IMAGE}
-build_date=$(docker inspect ${KBN_IMAGE} | jq -r '.[0].Config.Labels."org.label-schema.build-date"')
-vcs_ref=$(docker inspect ${KBN_IMAGE} | jq -r '.[0].Config.Labels."org.label-schema.vcs-ref"')
-vcs_url=$(docker inspect ${KBN_IMAGE} | jq -r '.[0].Config.Labels."org.label-schema.vcs-url"')
-version=$(docker inspect ${KBN_IMAGE} | jq -r '.[0].Config.Labels."org.label-schema.version"')   
+# KIBANA_BASE_IMAGE="docker.elastic.co/kibana-ci/kibana-serverless"
+# KIBANA_LATEST=${KIBANA_BASE_IMAGE}:latest
 
-markdown_text="""
-    # Kibana Container Metadata
-    - Build Date            : $build_date 
-    - Github Commit Hash    : $vcs_ref 
-    - Github Repo           : $vcs_url 
-    - Version               : $version
-"""
-echo "${markdown_text//[*\\_]/\\&}" | buildkite-agent annotate --style "info"
+# if [ -v KIBANA_MKI_IMAGE_COMMIT ]; then
+#     KBN_IMAGE=${KIBANA_BASE_IMAGE}:git-${KIBANA_MKI_IMAGE_COMMIT:0:12}
+# else
+#     KBN_IMAGE=${KIBANA_LATEST}
+# fi
+
+# docker pull ${KBN_IMAGE}
+# build_date=$(docker inspect ${KBN_IMAGE} | jq -r '.[0].Config.Labels."org.label-schema.build-date"')
+# vcs_ref=$(docker inspect ${KBN_IMAGE} | jq -r '.[0].Config.Labels."org.label-schema.vcs-ref"')
+# vcs_url=$(docker inspect ${KBN_IMAGE} | jq -r '.[0].Config.Labels."org.label-schema.vcs-url"')
+# version=$(docker inspect ${KBN_IMAGE} | jq -r '.[0].Config.Labels."org.label-schema.version"')   
+
+# markdown_text="""
+#     # Kibana Container Metadata
+#     - Build Date            : $build_date 
+#     - Github Commit Hash    : $vcs_ref 
+#     - Github Repo           : $vcs_url 
+#     - Version               : $version
+# """
+# echo "${markdown_text//[*\\_]/\\&}" | buildkite-agent annotate --style "info"
