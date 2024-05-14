a3c7b062a576149617d52744fb554f6a234aa157
Tre' Seymour <wayne.seymour@elastic.co>
2024-05-14 11:29:03 +0100

    reverting translations, to be in another pr

--- x-pack/test_serverless/api_integration/test_suites/common/core/translations.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/core/translations.ts
@@ -7,49 +7,28 @@
 
 import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
-import { RoleCredentials } from '../../../../shared/services';
 
 export default function ({ getService }: FtrProviderContext) {
-  const svlCommonApi = getService('svlCommonApi');
-  const svlUserManager = getService('svlUserManager');
-  let roleAuthc: RoleCredentials;
-  const supertestWithoutAuth = getService('supertestWithoutAuth');
+  const supertest = getService('supertest');
 
   describe('translations', () => {
-    before(async () => {
-      roleAuthc = await svlUserManager.createApiKeyForRole('admin');
-    });
-    after(async () => {
-      await svlUserManager.invalidateApiKeyForRole(roleAuthc);
-    });
     it(`returns the translations with the correct headers`, async () => {
-      await supertestWithoutAuth
-        .get('/translations/en.json')
-        .set(svlCommonApi.getInternalRequestHeader())
-        .set(roleAuthc.apiKeyHeader)
-        .then((response) => {
-          expect(response.body.locale).to.eql('en');
+      await supertest.get('/translations/en.json').then((response) => {
+        expect(response.body.locale).to.eql('en');
 
-          expect(response.header).to.have.property(
-            'content-type',
-            'application/json; charset=utf-8'
-          );
-          expect(response.header).to.have.property(
-            'cache-control',
-            'public, max-age=31536000, immutable'
-          );
-          expect(response.header).not.to.have.property('etag');
-        });
+        expect(response.header).to.have.property('content-type', 'application/json; charset=utf-8');
+        expect(response.header).to.have.property(
+          'cache-control',
+          'public, max-age=31536000, immutable'
+        );
+        expect(response.header).not.to.have.property('etag');
+      });
     });
 
     it(`returns a 404 when not using the correct locale`, async () => {
-      await supertestWithoutAuth
-        .get('/translations/foo.json')
-        .set(svlCommonApi.getInternalRequestHeader())
-        .set(roleAuthc.apiKeyHeader)
-        .then((response) => {
-          expect(response.status).to.eql(404);
-        });
+      await supertest.get('/translations/foo.json').then((response) => {
+        expect(response.status).to.eql(404);
+      });
     });
   });
 }
