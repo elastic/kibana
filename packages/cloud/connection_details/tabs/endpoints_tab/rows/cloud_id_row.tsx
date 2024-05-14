f4cb8c70603a92464bee4a7508aec60d417fbdd8
vadimkibana <vadimkibana@gmail.com>
2024-05-14 12:20:46 +0200

    add help popover for cloud id

--- packages/cloud/connection_details/tabs/endpoints_tab/rows/cloud_id_row.tsx
+++ packages/cloud/connection_details/tabs/endpoints_tab/rows/cloud_id_row.tsx
@@ -7,11 +7,81 @@
  */
 
 import * as React from 'react';
-import { EuiFormRow, EuiSpacer, EuiSwitch } from '@elastic/eui';
+import {
+  EuiButtonIcon,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiFormRow,
+  EuiLink,
+  EuiPopover,
+  EuiSpacer,
+  EuiSwitch,
+  EuiText,
+} from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n-react';
 import { CopyInput } from '../../../components/copy_input';
 import { useConnectionDetailsService } from '../../../context';
 import { useBehaviorSubject } from '../../../hooks/use_behavior_subject';
+
+const Label: React.FC<{ learnMoreUrl?: string }> = ({ learnMoreUrl }) => {
+  const [isPopoverOpen, setIsPopoverOpen] = React.useState(false);
+
+  const labelText = i18n.translate('cloud.connectionDetails.tab.endpoints.cloudIdField.label', {
+    defaultMessage: 'Cloud ID',
+  });
+
+  if (!learnMoreUrl) {
+    return <>{labelText}</>;
+  }
+
+  return (
+    <EuiFlexGroup css={{ minWidth: 200, height: 16 }} alignItems="center" gutterSize="xs">
+      <EuiFlexItem grow={false}>
+        <EuiText size="xs" css={{ fontWeight: 600 }}>
+          {labelText}
+        </EuiText>
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <EuiPopover
+          button={
+            <EuiButtonIcon
+              iconType="questionInCircle"
+              onClick={() => {
+                setIsPopoverOpen((x) => !x);
+              }}
+            />
+          }
+          isOpen={isPopoverOpen}
+          closePopover={() => {
+            setIsPopoverOpen(false);
+          }}
+          anchorPosition="upLeft"
+          hasArrow={false}
+        >
+          <p style={{ width: 270 }}>
+            <FormattedMessage
+              id="cloud.connectionDetails.tab.endpoints.cloudIdField.label.tooltip"
+              defaultMessage="Get started with Elastic Agent or Logstash quickly. The Cloud ID simplifies sending data to Elastic. {link}"
+              values={{
+                link: (
+                  <EuiLink href={learnMoreUrl}>
+                    {i18n.translate(
+                      'cloud.connectionDetails.tab.endpoints.cloudIdField.learnMore',
+                      {
+                        defaultMessage: 'Learn more',
+                      }
+                    )}
+                  </EuiLink>
+                ),
+              }}
+            />
+          </p>
+        </EuiPopover>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
 
 export interface CloudIdRowProps {
   value: string;
@@ -38,9 +108,7 @@ export const CloudIdRow: React.FC<CloudIdRowProps> = ({ value }) => {
 
       {showCloudId && (
         <EuiFormRow
-          label={i18n.translate('cloud.connectionDetails.tab.endpoints.cloudIdField.label', {
-            defaultMessage: 'Cloud ID',
-          })}
+          label={<Label learnMoreUrl={service.opts.endpoints?.cloudIdLearMoreLink} />}
           helpText={i18n.translate('cloud.connectionDetails.tab.endpoints.cloudIdField.helpText', {
             defaultMessage:
               'Specific client libraries and connectors can use this unique identifier specific to Elastic Cloud.',
