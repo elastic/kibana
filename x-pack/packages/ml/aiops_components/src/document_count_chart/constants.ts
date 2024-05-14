8e2c00520856cd09907ecf190b67d23a49bab1b5
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-05-14 10:55:16 +0200

    aiops_components: refactor document count chart

---
+++ x-pack/packages/ml/aiops_components/src/document_count_chart/constants.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export const DOCUMENT_COUNT_CHART_OVERALL_SERIES_SPEC_ID = 'document_count';
--- x-pack/packages/ml/aiops_components/src/document_count_chart/index.ts
+++ x-pack/packages/ml/aiops_components/src/document_count_chart/index.ts
@@ -5,6 +5,13 @@
  * 2.0.
  */
 
+export { getLogRateAnalysisBarStyleAccessor } from './bar_style';
+export { DOCUMENT_COUNT_CHART_OVERALL_SERIES_SPEC_ID } from './constants';
 export { DocumentCountChart } from './document_count_chart';
 export { DocumentCountChartRedux } from './document_count_chart_redux';
-export type { BrushSettings, DocumentCountChartProps } from './document_count_chart';
+export type {
+  BrushSelectionUpdateHandler,
+  BrushSettings,
+  DocumentCountChartProps,
+} from './document_count_chart';
+export { documentCountChartOverallSeriesName } from './i18n';
