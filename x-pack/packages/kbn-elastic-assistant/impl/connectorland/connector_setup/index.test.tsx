36480a82bfcfcb3c80627dbbaa427bb25a392bc1
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-05-15 21:35:14 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/packages/kbn-elastic-assistant/impl/connectorland/connector_setup/index.test.tsx
+++ x-pack/packages/kbn-elastic-assistant/impl/connectorland/connector_setup/index.test.tsx
@@ -6,7 +6,6 @@
  */
 
 import React from 'react';
-import { ConnectorSetup } from '.';
 import { act, renderHook } from '@testing-library/react-hooks';
 import { fireEvent, render } from '@testing-library/react';
 import { welcomeConvo } from '../../mock/conversation';
