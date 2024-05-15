28b301e862d24341e3185b222bcdb589e7fb1f60
Mike Cote <michel.cote@elastic.co>
2024-05-15 11:38:03 -0400

    Dummy commit

--- x-pack/plugins/task_manager/server/task_store.ts
+++ x-pack/plugins/task_manager/server/task_store.ts
@@ -25,7 +25,6 @@ import {
   SavedObjectsUpdateResponse,
   ElasticsearchClient,
 } from '@kbn/core/server';
-
 import { RequestTimeoutsConfig } from './config';
 import { asOk, asErr, Result } from './lib/result_type';
 
