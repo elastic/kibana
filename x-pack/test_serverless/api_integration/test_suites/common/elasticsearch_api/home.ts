575f85de1bd80117ac184b0894260865f8b33844
Tre' Seymour <wayne.seymour@elastic.co>
2024-05-15 12:42:10 +0100

    revert

--- x-pack/test_serverless/api_integration/test_suites/common/elasticsearch_api/home.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/elasticsearch_api/home.ts
@@ -6,28 +6,14 @@
  */
 
 import { FtrProviderContext } from '../../../ftr_provider_context';
-import { RoleCredentials } from '../../../../shared/services';
-export default function ({ getService }: FtrProviderContext) {
-  const supertestWithoutAuth = getService('supertestWithoutAuth');
 
+export default function ({ getService }: FtrProviderContext) {
+  const esSupertest = getService('esSupertest');
   const svlCommonApi = getService('svlCommonApi');
-  const svlUserManager = getService('svlUserManager');
-  let roleAuthc: RoleCredentials;
 
   describe('Home', function () {
-    before(async () => {
-      roleAuthc = await svlUserManager.createApiKeyForRole('admin');
-    });
-    after(async () => {
-      await svlUserManager.invalidateApiKeyForRole(roleAuthc);
-    });
     it('can request /', async () => {
-      const { body, status } = await supertestWithoutAuth
-        .get('/')
-        .set(svlCommonApi.getCommonRequestHeader())
-        .set(roleAuthc.apiKeyHeader)
-        .redirects(2);
-
+      const { body, status } = await esSupertest.get('/');
       svlCommonApi.assertResponseStatusCode(200, status, body);
     });
   });
