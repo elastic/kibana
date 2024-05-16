6ce3d6230f80470ea6679bfdb4ebf9296dc3d0d4
Ignacio Rivas <rivasign@gmail.com>
2024-05-16 08:48:46 +0200

    Add try in console cta

--- x-pack/plugins/ingest_pipelines/public/application/components/pipeline_form/bulk_request_panel.tsx
+++ x-pack/plugins/ingest_pipelines/public/application/components/pipeline_form/bulk_request_panel.tsx
@@ -8,6 +8,8 @@
 import React, { useState } from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
 import {
+  EuiFlexGroup,
+  EuiFlexItem,
   EuiSpacer,
   EuiPanel,
   EuiCodeBlock,
@@ -15,15 +17,18 @@ import {
   EuiSwitch,
   EuiSwitchEvent,
 } from '@elastic/eui';
+import { TryInConsoleButton } from '@kbn/try-in-console';
 
-const bulkRequestExample = `PUT my-data-stream/_bulk?pipeline=my-pipeline
+import { useKibana } from '../../../shared_imports';
+
+const bulkRequestExample = `PUT books/_bulk?pipeline=my-pipeline
 { "create":{ } }
 { "@timestamp": "2099-03-07T11:04:06.000Z", "my-keyword-field": "foo" }
 { "create":{ } }
 { "@timestamp": "2099-03-07T11:04:07.000Z", "my-keyword-field": "bar" }
 `;
 
-const singleRequestExample = `POST my-data-stream/_doc?pipeline=my-pipeline
+const singleRequestExample = `POST books/_doc?pipeline=my-pipeline-name
 {
   "@timestamp": "2099-03-07T11:04:05.000Z",
   "my-keyword-field": "foo"
@@ -31,6 +36,7 @@ const singleRequestExample = `POST my-data-stream/_doc?pipeline=my-pipeline
 `;
 
 export const BulkRequestPanel = () => {
+  const { services } = useKibana();
   const [showBulkToggle, setShowBulkToggle] = useState(true);
 
   return (
@@ -46,17 +52,30 @@ export const BulkRequestPanel = () => {
 
       <EuiSpacer size="m" />
 
-      <EuiSwitch
-        compressed
-        label={
-          <FormattedMessage
-            id="xpack.ingestPipelines.form.bulkRequestToggle"
-            defaultMessage="Bulk request"
+      <EuiFlexGroup gutterSize="none" justifyContent="spaceBetween" alignItems="center">
+        <EuiFlexItem grow={false}>
+          <EuiSwitch
+            compressed
+            label={
+              <FormattedMessage
+                id="xpack.ingestPipelines.form.bulkRequestToggle"
+                defaultMessage="Bulk request"
+              />
+            }
+            checked={showBulkToggle}
+            onChange={(e: EuiSwitchEvent) => setShowBulkToggle(e.target.checked)}
           />
-        }
-        checked={showBulkToggle}
-        onChange={(e: EuiSwitchEvent) => setShowBulkToggle(e.target.checked)}
-      />
+        </EuiFlexItem>
+
+        <EuiFlexItem grow={false}>
+          <TryInConsoleButton
+            request={showBulkToggle ? bulkRequestExample : singleRequestExample}
+            application={services.application}
+            consolePlugin={services.consolePlugin}
+            sharePlugin={services.share}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
 
       <EuiSpacer size="m" />
 
