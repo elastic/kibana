ad02c7c8aad91972c2963fea68e4555218837c27
Drew Tate <andrew.tate@elastic.co>
2024-05-16 11:24:22 -0600

    remove from options

--- packages/kbn-esql-ast/src/ast_helpers.ts
+++ packages/kbn-esql-ast/src/ast_helpers.ts
@@ -218,7 +218,7 @@ function getQuotedText(ctx: ParserRuleContext) {
 }
 
 function getUnquotedText(ctx: ParserRuleContext) {
-  return [66 /* esql_parser.UNQUOTED_IDENTIFIER */, 73 /* esql_parser.FROM_UNQUOTED_IDENTIFIER */]
+  return [66 /* esql_parser.UNQUOTED_IDENTIFIER */, 72 /* esql_parser.FROM_UNQUOTED_IDENTIFIER */]
     .map((keyCode) => ctx.getToken(keyCode, 0))
     .filter(nonNullable)[0];
 }
