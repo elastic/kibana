f7c253f55f199ff1b271b9d990f59e972c60475d
Nathan Reese <reese.nathan@gmail.com>
2024-05-17 11:46:23 -0600

    [canvas] fix parent API should implement unified search interface with react embeddables

--- x-pack/plugins/canvas/public/components/hooks/use_canvas_api.tsx
+++ x-pack/plugins/canvas/public/components/hooks/use_canvas_api.tsx
@@ -5,12 +5,13 @@
  * 2.0.
  */
 
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { BehaviorSubject } from 'rxjs';
 
 import { EmbeddableInput, ViewMode } from '@kbn/embeddable-plugin/common';
 
+import { AggregateQuery, Filter, Query, TimeRange } from '@kbn/es-query';
 import { embeddableInputToExpression } from '../../../canvas_plugin_src/renderers/embeddable/embeddable_input_to_expression';
 import { CanvasContainerApi } from '../../../types';
 import { METRIC_TYPE, trackCanvasUiMetric } from '../../lib/ui_metric';
@@ -18,7 +19,19 @@ import { METRIC_TYPE, trackCanvasUiMetric } from '../../lib/ui_metric';
 import { addElement } from '../../state/actions/elements';
 import { getSelectedPage } from '../../state/selectors/workpad';
 
-export const useCanvasApi: () => CanvasContainerApi = () => {
+export const useCanvasApi: (filters?: Filter[]) => CanvasContainerApi = (filters?: Filter[]) => {
+  const unifiedSearchApi = useMemo(() => {
+    return {
+      filters$: new BehaviorSubject<Filter[] | undefined>(undefined),
+      query$: new BehaviorSubject<Query | AggregateQuery | undefined>(undefined),
+      timeRange$: new BehaviorSubject<TimeRange | undefined>(undefined),
+    };
+  }, []);
+
+  useEffect(() => {
+    unifiedSearchApi.filters$.next(filters);
+  }, [filters, unifiedSearchApi.filters$]);
+
   const selectedPageId = useSelector(getSelectedPage);
   const dispatch = useDispatch();
 
@@ -47,8 +60,9 @@ export const useCanvasApi: () => CanvasContainerApi = () => {
       }) => {
         createNewEmbeddable(panelType, initialState);
       },
+      ...unifiedSearchApi,
     } as CanvasContainerApi;
-  }, [createNewEmbeddable]);
+  }, [createNewEmbeddable, unifiedSearchApi]);
 
   return useMemo(() => getCanvasApi(), [getCanvasApi]);
 };
--- x-pack/plugins/canvas/public/components/render_with_fn/render_with_fn.tsx
+++ x-pack/plugins/canvas/public/components/render_with_fn/render_with_fn.tsx
@@ -42,7 +42,7 @@ export const RenderWithFn: FC<Props> = ({
   width,
   height,
 }) => {
-  const canvasApi = useCanvasApi();
+  const canvasApi = useCanvasApi(config.input?.filters);
   const { error: onError } = useNotifyService();
 
   const [domNode, setDomNode] = useState<HTMLElement | null>(null);
