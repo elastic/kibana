779b51a9b58d6d6cb8f276bfb0eeccf61a5d0b09
Paul Tavares <paul.tavares@elastic.co>
2024-05-17 14:29:55 -0400

    clean up unused code and move legacy action list utilities still in use

--- x-pack/plugins/security_solution/server/endpoint/services/actions/actions.ts
+++ x-pack/plugins/security_solution/server/endpoint/services/actions/actions.ts
@@ -8,6 +8,13 @@
 import type { Logger } from '@kbn/core/server';
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import type { TransportResult } from '@elastic/elasticsearch';
+import type { SearchRequest } from '@kbn/data-plugin/common';
+import { AGENT_ACTIONS_INDEX, AGENT_ACTIONS_RESULTS_INDEX } from '@kbn/fleet-plugin/common';
+import {
+  ENDPOINT_ACTION_RESPONSES_INDEX_PATTERN,
+  ENDPOINT_ACTIONS_DS,
+  ENDPOINT_ACTIONS_INDEX,
+} from '../../../../common/endpoint/constants';
 import type { SecuritySolutionRequestHandlerContext } from '../../../types';
 import type {
   ActivityLog,
@@ -17,10 +24,26 @@ import type {
   LogsEndpointAction,
   LogsEndpointActionResponse,
 } from '../../../../common/endpoint/types';
-import { getActionRequestsResult, getActionResponsesResult, getTimeSortedData } from '../../utils';
+import { doesLogsEndpointActionsIndexExist } from '../../utils';
 
-import { categorizeActionResults, categorizeResponseResults, getUniqueLogData } from './utils';
+import {
+  categorizeActionResults,
+  categorizeResponseResults,
+  getDateFilters,
+  getUniqueLogData,
+} from './utils';
+import { ACTION_REQUEST_INDICES, ACTION_RESPONSE_INDICES } from './constants';
 
+const queryOptions = {
+  headers: {
+    'X-elastic-product-origin': 'fleet',
+  },
+  ignore: [404],
+};
+
+/**
+ * @deprecated Use newer response action services instead
+ */
 export const getAuditLogResponse = async ({
   elasticAgentId,
   page,
@@ -133,4 +156,134 @@ const getActivityLog = async ({
   const sortedData = getTimeSortedData(uniqueLogData);
 
   return sortedData;
+};
+
+const getTimeSortedData = (data: ActivityLog['data']): ActivityLog['data'] => {
+  return data.sort((a, b) =>
+    new Date(b.item.data['@timestamp']) > new Date(a.item.data['@timestamp']) ? 1 : -1
+  );
+};
+
+const getActionRequestsResult = async ({
+  context,
+  logger,
+  elasticAgentId,
+  startDate,
+  endDate,
+  size,
+  from,
+}: {
+  context: SecuritySolutionRequestHandlerContext;
+  logger: Logger;
+  elasticAgentId: string;
+  startDate: string;
+  endDate: string;
+  size: number;
+  from: number;
+}): Promise<{
+  actionIds: string[];
+  actionRequests: TransportResult<estypes.SearchResponse<unknown>, unknown>;
+}> => {
+  const dateFilters = getDateFilters({ startDate, endDate });
+  const baseActionFilters = [
+    { term: { agents: elasticAgentId } },
+    { term: { input_type: 'endpoint' } },
+    { term: { type: 'INPUT_ACTION' } },
+  ];
+  const actionsFilters = [...baseActionFilters, ...dateFilters];
+  const esClient = (await context.core).elasticsearch.client.asInternalUser;
+
+  const hasLogsEndpointActionsIndex = await doesLogsEndpointActionsIndexExist({
+    esClient,
+    logger,
+    indexName: ENDPOINT_ACTIONS_INDEX,
+  });
+
+  const actionsSearchQuery: SearchRequest = {
+    index: hasLogsEndpointActionsIndex ? ACTION_REQUEST_INDICES : AGENT_ACTIONS_INDEX,
+    size,
+    from,
+    body: {
+      query: {
+        bool: {
+          filter: actionsFilters,
+        },
+      },
+      sort: [
+        {
+          '@timestamp': {
+            order: 'desc',
+          },
+        },
+      ],
+    },
+  };
+
+  let actionRequests: TransportResult<estypes.SearchResponse<unknown>, unknown>;
+  try {
+    actionRequests = await esClient.search(actionsSearchQuery, { ...queryOptions, meta: true });
+    const actionIds = actionRequests?.body?.hits?.hits?.map((e) => {
+      return e._index.includes(ENDPOINT_ACTIONS_DS)
+        ? (e._source as LogsEndpointAction).EndpointActions.action_id
+        : (e._source as EndpointAction).action_id;
+    });
+
+    return { actionIds, actionRequests };
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+};
+
+const getActionResponsesResult = async ({
+  context,
+  logger,
+  elasticAgentId,
+  actionIds,
+  startDate,
+  endDate,
+}: {
+  context: SecuritySolutionRequestHandlerContext;
+  logger: Logger;
+  elasticAgentId: string;
+  actionIds: string[];
+  startDate: string;
+  endDate: string;
+}): Promise<TransportResult<estypes.SearchResponse<unknown>, unknown>> => {
+  const dateFilters = getDateFilters({ startDate, endDate });
+  const baseResponsesFilter = [
+    { term: { agent_id: elasticAgentId } },
+    { terms: { action_id: actionIds } },
+  ];
+  const responsesFilters = [...baseResponsesFilter, ...dateFilters];
+  const esClient = (await context.core).elasticsearch.client.asInternalUser;
+
+  const hasLogsEndpointActionResponsesIndex = await doesLogsEndpointActionsIndexExist({
+    esClient,
+    logger,
+    indexName: ENDPOINT_ACTION_RESPONSES_INDEX_PATTERN,
+  });
+
+  const responsesSearchQuery: SearchRequest = {
+    index: hasLogsEndpointActionResponsesIndex
+      ? ACTION_RESPONSE_INDICES
+      : AGENT_ACTIONS_RESULTS_INDEX,
+    size: 1000,
+    body: {
+      query: {
+        bool: {
+          filter: responsesFilters,
+        },
+      },
+    },
+  };
+
+  let actionResponses: TransportResult<estypes.SearchResponse<unknown>, unknown>;
+  try {
+    actionResponses = await esClient.search(responsesSearchQuery, { ...queryOptions, meta: true });
+  } catch (error) {
+    logger.error(error);
+    throw error;
+  }
+  return actionResponses;
 };
