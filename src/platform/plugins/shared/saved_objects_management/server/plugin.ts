1f1a52bebaefdeebe844aac26a5e1eee49c8e5ae
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-05-17 11:23:12 +0200

    Remove example service

--- src/platform/plugins/shared/saved_objects_management/server/plugin.ts
+++ src/platform/plugins/shared/saved_objects_management/server/plugin.ts
@@ -13,12 +13,6 @@ import { SavedObjectsManagementPluginSetup, SavedObjectsManagementPluginStart } 
 import { SavedObjectsManagement } from './services';
 import { registerRoutes } from './routes';
 import { capabilitiesProvider } from './capabilities_provider';
-import {
-  MyExampleService,
-  myExampleServiceId,
-  MyExampleRequestHandler,
-  myExampleRequestHandlerId,
-} from './injection_example';
 
 export class SavedObjectsManagementPlugin
   implements Plugin<SavedObjectsManagementPluginSetup, SavedObjectsManagementPluginStart, {}, {}>
@@ -30,7 +24,7 @@ export class SavedObjectsManagementPlugin
     this.logger = this.context.logger.get();
   }
 
-  public setup({ http, capabilities, injection }: CoreSetup) {
+  public setup({ http, capabilities }: CoreSetup) {
     this.logger.debug('Setting up SavedObjectsManagement plugin');
     registerRoutes({
       http,
@@ -39,30 +33,6 @@ export class SavedObjectsManagementPlugin
 
     capabilities.registerProvider(capabilitiesProvider);
 
-    injection.setupModule((pluginContainer, { createModule }) => {
-      pluginContainer.bind(myExampleServiceId).to(MyExampleService).inSingletonScope();
-
-      return {
-        global: createModule(({ bind }) => {
-          // bind a global service though Core's injector
-          // by re-exposing the plugin's local service
-          bind(myExampleServiceId)
-            .toDynamicValue(() => {
-              return pluginContainer.get(myExampleServiceId);
-            })
-            .inSingletonScope();
-        }),
-        request: createModule(({ bind }) => {
-          // bind a request-scoped service
-          // note that is can use the services from the plugin's container
-          bind(myExampleRequestHandlerId)
-            .to(MyExampleRequestHandler)
-            // yes, it's a singleton at the request container's level
-            .inSingletonScope();
-        }),
-      };
-    });
-
     return {};
   }
 
@@ -70,10 +40,6 @@ export class SavedObjectsManagementPlugin
     this.logger.debug('Starting up SavedObjectsManagement plugin');
     const managementService = new SavedObjectsManagement(core.savedObjects.getTypeRegistry());
     this.managementService$.next(managementService);
-
-    // retrieve the service and use it
-    const myService = core.injection.container.get(myExampleServiceId);
-    myService.doSomething();
 
     return {};
   }
