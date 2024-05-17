fab22276a8f89b51e5da71cef7b5bd98bb82a013
Ryland Herrick <ryalnd@gmail.com>
2024-05-17 11:53:03 -0500

    Log out more of our test execution
    These tests are now timing out, but it's not clear where.

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/trial_license_complete_tier/execution_logic/machine_learning.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/trial_license_complete_tier/execution_logic/machine_learning.ts
@@ -104,12 +104,27 @@ export default ({ getService }: FtrProviderContext) => {
   // FLAKY: https://github.com/elastic/kibana/issues/171426
   describe('@ess @serverless @serverlessQA Machine learning type rules', () => {
     before(async () => {
+      console.log('BEFORE MACHINE LEARNING RULES')
       // Order is critical here: auditbeat data must be loaded before attempting to start the ML job,
       // as the job looks for certain indices on start
       await esArchiver.load(auditPath);
-      await executeSetupModuleRequest({ module: siemModule, rspCode: 200, supertest });
-      await forceStartDatafeeds({ jobId: mlJobId, rspCode: 200, supertest });
-      await esArchiver.load('x-pack/test/functional/es_archives/security_solution/anomalies');
+      console.log('loaded auditbeat data', auditPath);
+      const moduleResponse = await executeSetupModuleRequest({
+        module: siemModule,
+        rspCode: 200,
+        supertest,
+      });
+      console.log('modules set up', JSON.stringify(moduleResponse), null, 2);
+      const datafeedResponse = await forceStartDatafeeds({
+        jobId: mlJobId,
+        rspCode: 200,
+        supertest,
+      });
+      // Thought: is there a race condition where the datafeed and es_archiver are both attempting to create an index?
+      console.log('datafeed set up', JSON.stringify(datafeedResponse), null, 2));
+      const loadResp = await esArchiver.load('x-pack/test/functional/es_archives/security_solution/anomalies');
+      console.log('anomalies loaded', JSON.stringify(loadResp, null, 2));
+      console.log('/BEFORE MACHINE LEARNING RULES')
     });
 
     after(async () => {
@@ -126,8 +141,11 @@ export default ({ getService }: FtrProviderContext) => {
 
     // First test creates a real rule - remaining tests use preview API
     it('should create 1 alert from ML rule when record meets anomaly_threshold', async () => {
+      console.log('STARTING TEST')
       const createdRule = await createRule(supertest, log, rule);
+      console.log('created rule', JSON.stringify(createdRule, null, 2));
       const alerts = await getAlerts(supertest, log, es, createdRule);
+      console.log('alerts got', JSON.stringify(alerts, null, 2));
       expect(alerts.hits.hits.length).toBe(1);
       const alert = alerts.hits.hits[0];
 
@@ -201,6 +219,7 @@ export default ({ getService }: FtrProviderContext) => {
           ]),
         })
       );
+      console.log('ENDING TEST')
     });
 
     it('classifies ml job missing errors as user errors', async () => {
