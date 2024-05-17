ae7b5d266cfcea09a088c3719832692ae74ad8fe
kaanyalti <kaan.yalti@elastic.co>
2024-05-17 03:13:53 -0400

    feature(179915): updated excluded inputs, updated validation error message

--- x-pack/plugins/fleet/common/constants/global_data_tags_exclude.ts
+++ x-pack/plugins/fleet/common/constants/global_data_tags_exclude.ts
@@ -12,7 +12,13 @@ export const GLOBAL_DATA_TAG_EXCLUDED_INPUTS = new Set<string>([
   'pf-elastic-collector',
   'fleet-server',
   'cloud-defend',
-  // all of the cloudbeat inputs are excluded (eg cloudbeat/cis_k8s)
+  // all of the cloudbeat inputs are exclude
   // https://github.com/elastic/elastic-agent/blob/main/specs/cloudbeat.spec.yml
   'cloudbeat',
+  'cloudbeat/cis_k8s',
+  'cloudbeat/cis_eks',
+  'cloudbeat/cis_aws',
+  'cloudbeat/cis_gcp',
+  'cloudbeat/cis_azure',
+  'cloudbeat/vuln_mgmt_aws',
 ]);
