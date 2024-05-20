24ac26d55141cee7753529a29a88094c43575019
parkiino <candace.park@elastic.co>
2024-05-20 01:36:42 -0400

    remove random file, remove unnecessary arg

--- x-pack/plugins/lists/public/exceptions/components/builder/exception_item_renderer.tsx
+++ x-pack/plugins/lists/public/exceptions/components/builder/exception_item_renderer.tsx
@@ -56,7 +56,7 @@ interface BuilderExceptionListItemProps {
   onDeleteExceptionItem: (item: ExceptionsBuilderExceptionItem, index: number) => void;
   onChangeExceptionItem: (item: ExceptionsBuilderExceptionItem, index: number) => void;
   setErrorsExist: (arg: EntryFieldError) => void;
-  setWarningsExist: (arg: boolean, arg2: string) => void;
+  setWarningsExist: (arg: boolean) => void;
   onlyShowListOperators?: boolean;
   isDisabled?: boolean;
   operatorsList?: OperatorOption[];
