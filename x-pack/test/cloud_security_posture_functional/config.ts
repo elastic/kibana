993189d0c2effebbd2c81477461ded105fe7b5e3
Dmitry Gurevich <99176494+gurevichdmitry@users.noreply.github.com>
2024-05-20 10:05:35 +0300

    fix review comments

--- x-pack/test/cloud_security_posture_functional/config.ts
+++ x-pack/test/cloud_security_posture_functional/config.ts
@@ -13,6 +13,7 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const xpackFunctionalConfig = await readConfigFile(
     require.resolve('../functional/config.base.js')
   );
+
   return {
     ...xpackFunctionalConfig.getAll(),
     pageObjects,
@@ -22,6 +23,18 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
     },
     kbnTestServer: {
       ...xpackFunctionalConfig.get('kbnTestServer'),
+      /**
+       * Package version is fixed (not latest) so FTR won't suddenly break when package is changed.
+       *
+       * test a new package:
+       * 1. build the package and start the registry with elastic-package and uncomment the 'registryUrl' flag below
+       * 2. locally checkout the kibana version that matches the new package
+       * 3. update the package version below to use the new package version
+       * 4. run tests with NODE_EXTRA_CA_CERTS pointing to the elastic-package certificate
+       * 5. when test pass:
+       *   1. release a new package to EPR
+       *   2. merge the updated version number change to kibana
+       */
       serverArgs: [
         ...xpackFunctionalConfig.get('kbnTestServer.serverArgs'),
         `--xpack.fleet.packages.0.name=cloud_security_posture`,
