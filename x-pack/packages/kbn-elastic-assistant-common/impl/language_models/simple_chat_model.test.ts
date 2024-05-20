265afe2a958dc32c306fce992a446d60883034af
Steph Milovic <stephanie.milovic@elastic.co>
2024-05-20 15:21:34 -0600

    rm disable

--- x-pack/packages/kbn-elastic-assistant-common/impl/language_models/simple_chat_model.test.ts
+++ x-pack/packages/kbn-elastic-assistant-common/impl/language_models/simple_chat_model.test.ts
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-// eslint-disable-next-line import/no-nodejs-modules
 import { PassThrough } from 'stream';
 import type { PluginStartContract as ActionsPluginStart } from '@kbn/actions-plugin/server';
 import { loggerMock } from '@kbn/logging-mocks';
