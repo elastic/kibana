c698f07d4449dd0629a8100f5b29afe067a96361
Sébastien Loix <sebastien.loix@elastic.co>
2024-05-20 12:40:54 +0100

    Add jest tests for analytics page template

--- x-pack/plugins/enterprise_search/public/applications/analytics/components/layout/page_template.test.tsx
+++ x-pack/plugins/enterprise_search/public/applications/analytics/components/layout/page_template.test.tsx
@@ -5,23 +5,38 @@
  * 2.0.
  */
 
+/* eslint-disable @kbn/i18n/strings_should_be_translated_with_i18n, @kbn/i18n/strings_should_be_translated_with_formatted_message */
+
+import '../../../__mocks__/shallow_useeffect.mock';
+import { setMockValues } from '../../../__mocks__/kea_logic';
+
 import React from 'react';
 
+import { of } from 'rxjs';
+
+const mockUseEnterpriseSearchAnalyticsNav = jest.fn().mockReturnValue([]);
+
 jest.mock('../../../shared/layout/nav', () => ({
-  useEnterpriseSearchAnalyticsNav: jest.fn().mockReturnValue([]),
+  useEnterpriseSearchAnalyticsNav: (...args: any[]) => mockUseEnterpriseSearchAnalyticsNav(...args),
 }));
 
 import { shallow } from 'enzyme';
 
 import { SetAnalyticsChrome } from '../../../shared/kibana_chrome';
 import { EnterpriseSearchPageTemplateWrapper } from '../../../shared/layout';
-import { useEnterpriseSearchAnalyticsNav } from '../../../shared/layout/nav';
 import { SendEnterpriseSearchTelemetry } from '../../../shared/telemetry';
 
 import { EnterpriseSearchAnalyticsPageTemplate } from './page_template';
 
+const mockValues = {
+  getChromeStyle$: () => of('classic'),
+  updateSideNavDefinition: jest.fn(),
+};
+
 describe('EnterpriseSearchAnalyticsPageTemplate', () => {
   it('renders', () => {
+    setMockValues(mockValues);
+
     const wrapper = shallow(
       <EnterpriseSearchAnalyticsPageTemplate>
         <div className="hello">world</div>
@@ -33,8 +48,36 @@ describe('EnterpriseSearchAnalyticsPageTemplate', () => {
     expect(wrapper.find('.hello').text()).toEqual('world');
   });
 
+  it('updates the side nav dynamic links', async () => {
+    const updateSideNavDefinition = jest.fn();
+    jest.spyOn(React, 'useEffect').mockImplementationOnce((f) => f());
+
+    setMockValues({ ...mockValues, updateSideNavDefinition });
+
+    const collectionsItems = [{ foo: 'bar' }];
+    mockUseEnterpriseSearchAnalyticsNav.mockReturnValueOnce([
+      {
+        id: 'build',
+        items: [
+          {
+            id: 'analyticsCollections',
+            items: collectionsItems,
+          },
+        ],
+      },
+    ]);
+
+    shallow(<EnterpriseSearchAnalyticsPageTemplate />);
+
+    expect(updateSideNavDefinition).toHaveBeenCalledWith({
+      collections: collectionsItems,
+    });
+  });
+
   describe('page chrome', () => {
     it('takes a breadcrumb array & renders a product-specific page chrome', () => {
+      setMockValues(mockValues);
+
       const wrapper = shallow(<EnterpriseSearchAnalyticsPageTemplate pageChrome={['Some page']} />);
       const setPageChrome = wrapper
         .find(EnterpriseSearchPageTemplateWrapper)
@@ -47,6 +90,8 @@ describe('EnterpriseSearchAnalyticsPageTemplate', () => {
 
   describe('page telemetry', () => {
     it('takes a metric & renders product-specific telemetry viewed event', () => {
+      setMockValues(mockValues);
+
       const wrapper = shallow(
         <EnterpriseSearchAnalyticsPageTemplate pageViewTelemetry="some_page" />
       );
@@ -58,6 +103,8 @@ describe('EnterpriseSearchAnalyticsPageTemplate', () => {
 
   describe('props', () => {
     it('passes down any ...pageTemplateProps that EnterpriseSearchPageTemplateWrapper accepts', () => {
+      setMockValues(mockValues);
+
       const wrapper = shallow(
         <EnterpriseSearchAnalyticsPageTemplate
           pageHeader={{ pageTitle: 'hello world' }}
@@ -74,6 +121,8 @@ describe('EnterpriseSearchAnalyticsPageTemplate', () => {
     });
 
     it('passes down analytics name and paths to useEnterpriseSearchAnalyticsNav', () => {
+      setMockValues(mockValues);
+
       const mockAnalyticsName = 'some_analytics_name';
       shallow(
         <EnterpriseSearchAnalyticsPageTemplate
@@ -84,11 +133,15 @@ describe('EnterpriseSearchAnalyticsPageTemplate', () => {
         />
       );
 
-      expect(useEnterpriseSearchAnalyticsNav).toHaveBeenCalledWith(mockAnalyticsName, {
-        explorer: '/collections/some_analytics_name/explorer',
-        integration: '/collections/some_analytics_name/integrate',
-        overview: '/collections/some_analytics_name/overview',
-      });
+      expect(mockUseEnterpriseSearchAnalyticsNav).toHaveBeenCalledWith(
+        mockAnalyticsName,
+        {
+          explorer: '/collections/some_analytics_name/explorer',
+          integration: '/collections/some_analytics_name/integrate',
+          overview: '/collections/some_analytics_name/overview',
+        },
+        true
+      );
     });
   });
 });
