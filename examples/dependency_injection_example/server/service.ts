c2b44f9590a810466f76fa925d9d74d30252eda2
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-05-21 10:23:12 +0200

    Add simple example plugin

---
+++ examples/dependency_injection_example/server/service.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { inject, injectable } from 'inversify';
+import { RequestToken } from '@kbn/core-http-server';
+import type { EchoRequest } from './route';
+
+@injectable()
+export class EchoService {
+  constructor(@inject(RequestToken) private readonly request: EchoRequest) {}
+
+  echo() {
+    return this.request.body;
+  }
+}
