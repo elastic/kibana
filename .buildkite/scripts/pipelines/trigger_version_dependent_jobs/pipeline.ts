6442a19522a8f2bbad06331dc09a553d6c7870dd
Alex Szabo <alex.szabo@elastic.co>
2024-05-22 19:07:15 +0200

    chore(CI): replace version-dependent pipelines' schedules with scheduled trigger through the new pipeline

--- .buildkite/scripts/pipelines/trigger_version_dependent_jobs/pipeline.ts
+++ .buildkite/scripts/pipelines/trigger_version_dependent_jobs/pipeline.ts
@@ -17,6 +17,8 @@ function emitPipeline(pipelineSteps: BuildkiteTriggerStep[]) {
 
 const pipelineSets = {
   'es-forward': 'kibana-es-forward',
+  'artifacts-snapshot': 'kibana-artifacts-snapshot',
+  'artifacts-staging': 'kibana-artifacts-staging',
 };
 
 /**
@@ -36,6 +38,14 @@ async function main() {
   switch (pipelineSetName) {
     case 'es-forward': {
       pipelineSteps.push(...getESForwardPipelineTriggers());
+      break;
+    }
+    case 'artifacts-snapshot': {
+      pipelineSteps.push(...getArtifactSnapshotPipelineTriggers());
+      break;
+    }
+    case 'artifacts-staging': {
+      pipelineSteps.push(...getArtifactStagingPipelineTriggers());
       break;
     }
     default: {
@@ -67,6 +77,44 @@ function getESForwardPipelineTriggers(): BuildkiteTriggerStep[] {
         env: {
           ES_SNAPSHOT_MANIFEST: `https://storage.googleapis.com/kibana-ci-es-snapshots-daily/${version}/manifest-latest-verified.json`,
         },
+      },
+    } as BuildkiteTriggerStep;
+  });
+}
+
+function getArtifactSnapshotPipelineTriggers() {
+  // Trigger for all named branches
+  const versions = getVersionsFile();
+  const branches = versions.map((v) => v.branch);
+
+  return branches.map((branch) => {
+    return {
+      trigger: 'kibana-artifacts-snapshot',
+      async: true,
+      label: `Snapshot artifacts for ${branch}`,
+      build: {
+        message: `Triggering artifact snapshot for ${branch}`,
+        branch,
+        commit: 'HEAD',
+      },
+    } as BuildkiteTriggerStep;
+  });
+}
+
+function getArtifactStagingPipelineTriggers() {
+  // Trigger for all branches, that are not current minor+major
+  const versions = getVersionsFile();
+  const branches = versions.filter((v) => !v.currentMajor && !v.currentMinor).map((v) => v.branch);
+
+  return branches.map((branch) => {
+    return {
+      trigger: 'kibana-artifacts-staging',
+      async: true,
+      label: `Staging artifacts for ${branch}`,
+      build: {
+        message: `Triggering artifact staging for ${branch}`,
+        branch,
+        commit: 'HEAD',
       },
     } as BuildkiteTriggerStep;
   });
