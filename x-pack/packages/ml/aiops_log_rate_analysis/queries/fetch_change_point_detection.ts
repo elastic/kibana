2e9cdef1cd6429af03d3753ca1d672a122f697f0
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-05-22 10:44:17 +0200

    fetch_simple_log_rate_analysis

---
+++ x-pack/packages/ml/aiops_log_rate_analysis/queries/fetch_change_point_detection.ts
@@ -0,0 +1,165 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
+
+import dateMath from '@kbn/datemath';
+import type { ElasticsearchClient } from '@kbn/core/server';
+
+import { getExtendedChangePoint } from '../get_extended_change_point';
+import { getWindowParametersForTrigger } from '../get_window_parameters_for_trigger';
+import type { DocumentCountStatsChangePoint } from '../types';
+import type { WindowParameters } from '../window_parameters';
+
+// Change point detection requires a minimum of 22 buckets to be able to run.
+const CHANGE_POINT_MIN_BUCKETS = 22;
+
+interface ChangePointDetectionData {
+  changePoint: DocumentCountStatsChangePoint;
+  changePointDocCount: number;
+  dateHistogramBuckets: Record<string, number>;
+  intervalMs: number;
+  windowParameters: WindowParameters;
+}
+type ChangePointDetectionError = [string, null];
+type ChangePointDetectionSuccess = [null, ChangePointDetectionData];
+type ChangePointDetectionResponse = ChangePointDetectionError | ChangePointDetectionSuccess;
+
+/**
+ * Fetches change points for doc count histogram.
+ *
+ * @param esClient Elasticsearch client.
+ * @param index The Elasticsearch source index pattern.
+ * @param start The start of the time range, in Elasticsearch date math, like `now`.
+ * @param end The end of the time range, in Elasticsearch date math, like `now-24h`.
+ * @param timefield The Elasticesarch source index pattern time field.
+ * @param abortSignal Abort signal.
+ * @returns change point data.
+ */
+export const fetchChangePointDetection = async (
+  esClient: ElasticsearchClient,
+  index: string,
+  earliestMs: number,
+  latestMs: number,
+  timefield: string,
+  searchQuery: estypes.QueryDslQueryContainer,
+  abortSignal: AbortSignal
+): Promise<ChangePointDetectionResponse> => {
+  const barTarget = 75;
+
+  const delta = latestMs - earliestMs;
+
+  const dayMs = 86400 * 1000;
+  const dayThreshold = dayMs * CHANGE_POINT_MIN_BUCKETS;
+
+  const weekMs = dayMs * 7;
+  const weekThreshold = weekMs * CHANGE_POINT_MIN_BUCKETS;
+
+  const monthMs = dayMs * 30;
+  const monthThreshold = monthMs * CHANGE_POINT_MIN_BUCKETS;
+
+  let intervalMs = Math.round(delta / barTarget);
+
+  if (delta > monthThreshold) {
+    intervalMs = monthMs;
+  } else if (delta > weekThreshold) {
+    intervalMs = weekMs;
+  } else if (delta > dayThreshold) {
+    intervalMs = dayMs;
+  }
+
+  const aggs: Record<string, estypes.AggregationsAggregationContainer> = {
+    eventRate: {
+      date_histogram: {
+        field: timefield,
+        fixed_interval: `${intervalMs}ms`,
+        min_doc_count: 0,
+        ...(earliestMs !== undefined && latestMs !== undefined
+          ? {
+              extended_bounds: {
+                min: earliestMs,
+                max: latestMs,
+              },
+            }
+          : {}),
+      },
+    },
+    change_point_request: {
+      // @ts-expect-error missing from ES spec
+      change_point: {
+        buckets_path: 'eventRate>_count',
+      },
+    },
+  };
+
+  const searchBody: estypes.MsearchMultisearchBody = {
+    query: searchQuery,
+    aggs,
+    track_total_hits: false,
+    size: 0,
+  };
+
+  const histogram = await esClient.search(
+    {
+      index,
+      body: searchBody,
+    },
+    {
+      signal: abortSignal,
+      maxRetries: 0,
+    }
+  );
+
+  if (histogram.aggregations === undefined) {
+    return ['No log rate change detected.', null];
+  }
+
+  if (histogram.aggregations.change_point_request.bucket === undefined) {
+    return ['No log rate change detected.', null];
+  }
+
+  const dateHistogramBuckets = histogram.aggregations.eventRate.buckets.reduce((acc, cur) => {
+    acc[cur.key] = cur.doc_count;
+    return acc;
+  }, {});
+
+  const changePointTs = dateMath
+    .parse(histogram.aggregations.change_point_request.bucket.key)
+    ?.valueOf();
+
+  if (changePointTs === undefined) {
+    return ['There was an error parsing the log rate change timestamp.', null];
+  }
+
+  const extendedChangePoint = getExtendedChangePoint(dateHistogramBuckets, changePointTs);
+  const logRateType = Object.keys(histogram.aggregations.change_point_request.type)[0];
+
+  const changePoint = {
+    ...extendedChangePoint,
+    key: changePointTs,
+    type: logRateType,
+  };
+
+  const windowParameters = getWindowParametersForTrigger(
+    extendedChangePoint.startTs,
+    intervalMs,
+    earliestMs,
+    latestMs,
+    changePoint
+  );
+
+  return [
+    null,
+    {
+      changePoint: { ...extendedChangePoint, key: changePointTs, type: logRateType },
+      changePointDocCount: histogram.aggregations.change_point_request.bucket.doc_count,
+      dateHistogramBuckets,
+      intervalMs,
+      windowParameters,
+    },
+  ];
+};
---
+++ x-pack/packages/ml/aiops_log_rate_analysis/queries/fetch_simple_log_rate_analysis.ts
@@ -0,0 +1,295 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { queue } from 'async';
+import { mean } from 'd3-array';
+import moment from 'moment';
+
+import dateMath from '@kbn/datemath';
+import type { ElasticsearchClient } from '@kbn/core/server';
+import type { SignificantItem } from '@kbn/ml-agg-utils';
+import { getSampleProbability } from '@kbn/ml-random-sampler-utils';
+import { isPopulatedObject } from '@kbn/ml-is-populated-object';
+
+import type { AiopsLogRateAnalysisSchema } from '../api/schema';
+
+import { fetchChangePointDetection } from './fetch_change_point_detection';
+import { fetchIndexInfo } from './fetch_index_info';
+import { fetchSignificantCategories } from './fetch_significant_categories';
+import { fetchSignificantTermPValues } from './fetch_significant_term_p_values';
+
+// Don't use more than 5 here otherwise Kibana will emit an error
+// regarding a limit of abort signal listeners of more than 10.
+const MAX_CONCURRENT_QUERIES = 5;
+
+interface FieldCandidate {
+  fieldCandidate: string;
+}
+const isFieldCandidate = (d: unknown): d is FieldCandidate =>
+  isPopulatedObject(d, ['fieldCandidate']);
+
+interface TextFieldCandidate {
+  textFieldCandidate: string;
+}
+const isTextFieldCandidate = (d: unknown): d is FieldCandidate =>
+  isPopulatedObject(d, ['textFieldCandidate']);
+
+type Candidate = FieldCandidate | TextFieldCandidate;
+
+export interface LogRateChange {
+  type: string;
+  timestamp: number;
+  logRateChangeCount: number;
+  averageLogRateCount: number;
+  logRateAggregationIntervalUsedForAnalysis: string;
+  documentSamplingFactorForAnalysis?: number;
+  extendedChangePoint: { startTs: number; endTs: number };
+}
+
+export interface SimpleSignificantItem {
+  field: string;
+  value: string | number;
+  type: 'metadata' | 'log message pattern';
+  documentCount: number;
+  baselineCount: number;
+  logRateChangeSort: number;
+  logRateChange: string;
+}
+
+/**
+ * Fetches log rate analysis data.
+ *
+ * @param esClient Elasticsearch client.
+ * @param index The Elasticsearch source index pattern.
+ * @param start The start of the time range, in Elasticsearch date math, like `now`.
+ * @param end The end of the time range, in Elasticsearch date math, like `now-24h`.
+ * @param timefield The Elasticesarch source index pattern time field.
+ * @param abortSignal Abort signal.
+ * @returns Log rate analysis data.
+ */
+export const fetchSimpleLogRateAnalysis = async (
+  esClient: ElasticsearchClient,
+  index: string,
+  start: string,
+  end: string,
+  timefield: string,
+  abortSignal: AbortSignal
+) => {
+  const debugStartTime = Date.now();
+
+  const earliestMs = dateMath.parse(start)?.valueOf();
+  const latestMs = dateMath.parse(end, { roundUp: true })?.valueOf();
+
+  if (earliestMs === undefined || latestMs === undefined) {
+    throw new Error('Could not parse time range');
+  }
+
+  const searchQuery = {
+    range: {
+      [timefield]: {
+        gte: earliestMs,
+        lte: latestMs,
+        format: 'epoch_millis',
+      },
+    },
+  };
+
+  // CHANGE POINT DETECTION
+  const [error, resp] = await fetchChangePointDetection(
+    esClient,
+    index,
+    earliestMs,
+    latestMs,
+    timefield,
+    searchQuery,
+    abortSignal
+  );
+
+  if (error !== null) {
+    throw new Error(error);
+  }
+
+  const { changePoint, changePointDocCount, dateHistogramBuckets, intervalMs, windowParameters } =
+    resp;
+
+  // FIELD CANDIDATES
+
+  const fieldCandidates: string[] = [];
+  const textFieldCandidates: string[] = [];
+
+  const indexInfoParams: AiopsLogRateAnalysisSchema = {
+    index,
+    start: earliestMs,
+    end: latestMs,
+    searchQuery: JSON.stringify(searchQuery),
+    timeFieldName: timefield,
+    ...windowParameters,
+  };
+
+  const indexInfo = await fetchIndexInfo(
+    esClient,
+    indexInfoParams,
+    ['message', 'error.message'],
+    abortSignal
+  );
+
+  const baselineNumBuckets =
+    (windowParameters.baselineMax - windowParameters.baselineMin) / intervalMs;
+  const baselinePerBucket = indexInfo.baselineTotalDocCount / baselineNumBuckets;
+
+  const deviationNumBuckets =
+    (windowParameters.deviationMax - windowParameters.deviationMin) / intervalMs;
+  const deviationPerBucket = indexInfo.deviationTotalDocCount / deviationNumBuckets;
+
+  const analysisWindowParameters =
+    deviationPerBucket > baselinePerBucket
+      ? windowParameters
+      : {
+          baselineMin: windowParameters.deviationMin,
+          baselineMax: windowParameters.deviationMax,
+          deviationMin: windowParameters.baselineMin,
+          deviationMax: windowParameters.baselineMax,
+        };
+
+  const logRateType = deviationPerBucket > baselinePerBucket ? 'spike' : 'dip';
+
+  const params: AiopsLogRateAnalysisSchema = {
+    ...indexInfoParams,
+    ...analysisWindowParameters,
+  };
+
+  fieldCandidates.push(...indexInfo.fieldCandidates);
+  textFieldCandidates.push(...indexInfo.textFieldCandidates);
+  const sampleProbability = getSampleProbability(
+    indexInfo.deviationTotalDocCount + indexInfo.baselineTotalDocCount
+  );
+
+  // SIGNIFICANT ITEMS
+
+  // This will store the combined count of detected significant log patterns and keywords
+  let fieldValuePairsCount = 0;
+
+  const significantCategories: SignificantItem[] = [];
+  const significantTerms: SignificantItem[] = [];
+  const fieldsToSample = new Set<string>();
+
+  const pValuesQueue = queue(async function (payload: Candidate) {
+    if (isFieldCandidate(payload)) {
+      const { fieldCandidate } = payload;
+      let pValues: Awaited<ReturnType<typeof fetchSignificantTermPValues>> = [];
+
+      try {
+        pValues = await fetchSignificantTermPValues(
+          esClient,
+          params,
+          [fieldCandidate],
+          undefined,
+          sampleProbability,
+          (e) => {
+            console.log('fetchSignificantTermPValues ERROR', e);
+          },
+          abortSignal
+        );
+      } catch (e) {
+        console.log('catch fetchSignificantTermPValues ERROR', e);
+        return;
+      }
+
+      if (pValues.length > 0) {
+        pValues.forEach((d) => {
+          fieldsToSample.add(d.fieldName);
+        });
+        significantTerms.push(...pValues);
+      }
+    } else if (isTextFieldCandidate(payload)) {
+      const { textFieldCandidate } = payload;
+
+      const significantCategoriesForField = await fetchSignificantCategories(
+        esClient,
+        params,
+        [textFieldCandidate],
+        undefined,
+        sampleProbability,
+        (e) => {
+          console.log('fetchSignificantCategories ERROR', e);
+        },
+        abortSignal
+      );
+
+      if (significantCategoriesForField.length > 0) {
+        significantCategories.push(...significantCategoriesForField);
+      }
+    }
+  }, MAX_CONCURRENT_QUERIES);
+
+  pValuesQueue.push(
+    [
+      ...textFieldCandidates.map((d) => ({ textFieldCandidate: d })),
+      ...fieldCandidates.map((d) => ({ fieldCandidate: d })),
+    ],
+    (err) => {
+      if (err) {
+        console.error('queue push error', err);
+        pValuesQueue.kill();
+      }
+    }
+  );
+  await pValuesQueue.drain();
+
+  fieldValuePairsCount = significantCategories.length + significantTerms.length;
+
+  const debugEndTime = Date.now();
+  const debugDelta = (debugEndTime - debugStartTime) / 1000;
+  console.log(`Took: ${debugDelta}s`);
+
+  // RETURN DATA
+
+  const logRateChange: LogRateChange = {
+    type: logRateType,
+    timestamp: changePoint.key,
+    logRateChangeCount: changePointDocCount,
+    averageLogRateCount: Math.round(mean(Object.values(dateHistogramBuckets)) ?? 0),
+    logRateAggregationIntervalUsedForAnalysis: moment
+      .duration(Math.round(intervalMs / 1000), 'seconds')
+      .humanize(),
+    ...(sampleProbability < 1 ? { documentSamplingFactorForAnalysis: sampleProbability } : {}),
+    extendedChangePoint: {
+      startTs: changePoint.startTs,
+      endTs: changePoint.endTs,
+    },
+  };
+
+  if (fieldValuePairsCount === 0) {
+    return { logRateChange, significantItems: [], dateHistogramBuckets, windowParameters };
+  }
+
+  const significantItems: SimpleSignificantItem[] = [...significantTerms, ...significantCategories]
+    .filter(({ bg_count: bgCount, doc_count: docCount }) => {
+      return docCount > bgCount;
+    })
+    .map(({ fieldName, fieldValue, type, doc_count: docCount, bg_count: bgCount }) => ({
+      field: fieldName,
+      value: fieldValue,
+      type: (type === 'keyword'
+        ? 'metadata'
+        : 'log message pattern') as SimpleSignificantItem['type'],
+      documentCount: docCount,
+      baselineCount: bgCount,
+      logRateChangeSort: bgCount > 0 ? docCount / bgCount : docCount,
+      logRateChange:
+        bgCount > 0
+          ? logRateType === 'spike'
+            ? `${Math.round((docCount / bgCount) * 100) / 100}x increase`
+            : `${Math.round((bgCount / docCount) * 100) / 100}x decrease`
+          : logRateType === 'spike'
+          ? `${docCount} docs up from 0 in baseline`
+          : `0 docs down from ${docCount} in baseline`,
+    }))
+    .sort((a, b) => b.logRateChangeSort - a.logRateChangeSort);
+
+  return { logRateChange, significantItems, dateHistogramBuckets, windowParameters };
+};
