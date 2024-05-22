98405176e0593c743ec934920e90d9069122ec52
Tiago Costa <tiago.costa@elastic.co>
2024-05-22 03:07:08 +0100

    skip flaky suite (#183928)

--- x-pack/test/functional/apps/rollup_job/rollup_jobs.js
+++ x-pack/test/functional/apps/rollup_job/rollup_jobs.js
@@ -25,6 +25,7 @@ export default function ({ getService, getPageObjects }) {
 
   // FLAKY: https://github.com/elastic/kibana/issues/183925
   // FLAKY: https://github.com/elastic/kibana/issues/183926
+  // FLAKY: https://github.com/elastic/kibana/issues/183928
   describe.skip('rollup job', function () {
     // Since rollups can only be created once with the same name (even if you delete it),
     // we add the Date.now() to avoid name collision.
