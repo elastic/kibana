f43ee8f45e1ca39ddb4d0b8f4f1464e75fead816
pgayvallet <pierre.gayvallet@elastic.co>
2024-05-23 09:44:31 +0200

    adapt bfetch to not use banned headers

--- src/plugins/bfetch/server/plugin.ts
+++ src/plugins/bfetch/server/plugin.ts
@@ -18,6 +18,7 @@ import {
   RequestHandler,
   KibanaResponseFactory,
   AnalyticsServiceStart,
+  HttpProtocol,
 } from '@kbn/core/server';
 
 import { map$ } from '@kbn/std';
@@ -65,11 +66,23 @@ export interface BfetchServerSetup {
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface BfetchServerStart {}
 
-const streamingHeaders = {
-  'Content-Type': 'application/x-ndjson',
-  Connection: 'keep-alive',
-  'Transfer-Encoding': 'chunked',
-  'X-Accel-Buffering': 'no',
+// NOTE: we will likely want to fully disable bfetch when a client supports/use http2,
+//       or maybe even fully disable bfetch overall now that we have http2 support
+//       but this should be done in a later stage by the appropriate team
+//       so the PR does the easiest fix to just remove http2 banned headers
+const getStreamingHeaders = (protocol: HttpProtocol): Record<string, string> => {
+  if (protocol === 'http2') {
+    return {
+      'Content-Type': 'application/x-ndjson',
+      'X-Accel-Buffering': 'no',
+    };
+  }
+  return {
+    'Content-Type': 'application/x-ndjson',
+    Connection: 'keep-alive',
+    'Transfer-Encoding': 'chunked',
+    'X-Accel-Buffering': 'no',
+  };
 };
 
 interface Query {
@@ -144,7 +157,7 @@ export class BfetchServerPlugin
         const data = request.body;
         const compress = request.query.compress;
         return response.ok({
-          headers: streamingHeaders,
+          headers: getStreamingHeaders(request.protocol),
           body: createStream(
             handlerInstance.getResponseStream(data),
             logger,
