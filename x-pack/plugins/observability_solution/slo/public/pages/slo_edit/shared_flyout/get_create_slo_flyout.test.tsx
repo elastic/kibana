90859a922c2a20bc997f8db4a7dabaf46da20999
Panagiota Mitsopoulou <giota85@gmail.com>
2024-05-23 09:46:52 +0200

    mock slo dependencies (a few left)

--- x-pack/plugins/observability_solution/slo/public/pages/slo_edit/shared_flyout/get_create_slo_flyout.test.tsx
+++ x-pack/plugins/observability_solution/slo/public/pages/slo_edit/shared_flyout/get_create_slo_flyout.test.tsx
@@ -8,76 +8,72 @@
 import React from 'react';
 import { mockCasesContract } from '@kbn/cases-plugin/public/mocks';
 import { chartPluginMock } from '@kbn/charts-plugin/public/mocks';
+// import { coreMock } from '@kbn/core/public/mocks';
 import { coreMock } from '@kbn/core/public/mocks';
+import { discoverPluginMock } from '@kbn/discover-plugin/server/mocks';
+
 import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
 import { lensPluginMock } from '@kbn/lens-plugin/public/mocks';
-import { createObservabilityRuleTypeRegistry } from '@kbn/observability-plugin/public/rules/create_observability_rule_type_registry';
 import { createObservabilityRuleTypeRegistryMock } from '@kbn/observability-plugin/public/rules/observability_rule_type_registry_mock';
 import { sharePluginMock } from '@kbn/share-plugin/public/mocks';
 import { unifiedSearchPluginMock } from '@kbn/unified-search-plugin/public/mocks';
 import { act, render, screen } from '@testing-library/react';
 import { getCreateSLOFlyoutLazy } from './get_create_slo_flyout';
+import { sloPublicPluginsStartMock } from '../../../plugin.mock';
+import { SloPublicPluginsStart } from '../../../types';
+// const triggersActionsUiStartMock = {
+//   createStart() {
+//     return {
+//       ruleTypeRegistry: {
+//         has: jest.fn(),
+//         register: jest.fn(),
+//         get: jest.fn(),
+//         list: jest.fn(),
+//       },
+//       actionTypeRegistry: {
+//         has: jest.fn((x) => true),
+//         register: jest.fn(),
+//         get: jest.fn(),
+//         list: jest.fn(),
+//       },
+//     };
+//   },
+// };
 
-const core = coreMock.createStart();
-const triggersActionsUiStartMock = {
-  createStart() {
-    return {
-      ruleTypeRegistry: {
-        has: jest.fn(),
-        register: jest.fn(),
-        get: jest.fn(),
-        list: jest.fn(),
-      },
-      actionTypeRegistry: {
-        has: jest.fn((x) => true),
-        register: jest.fn(),
-        get: jest.fn(),
-        list: jest.fn(),
-      },
-    };
-  },
-};
+// const dataViewEditor = {
+//   createStart() {
+//     return {
+//       userPermissions: {
+//         editDataView: jest.fn(),
+//         openEditor: jest.fn(),
+//       },
+//     };
+//   },
+// };
 
-const dataViewEditor = {
-  createStart() {
-    return {
-      userPermissions: {
-        editDataView: jest.fn(),
-        openEditor: jest.fn(),
-      },
-    };
-  },
-};
-
-const dataViews = {
-  createStart() {
-    return {
-      getIds: jest.fn().mockImplementation(() => []),
-      get: jest.fn(),
-      create: jest.fn().mockImplementation(() => ({
-        fields: {
-          getByName: jest.fn(),
-        },
-      })),
-    };
-  },
-};
+// const dataViews = {
+//   createStart() {
+//     return {
+//       getIds: jest.fn().mockImplementation(() => []),
+//       get: jest.fn(),
+//       create: jest.fn().mockImplementation(() => ({
+//         fields: {
+//           getByName: jest.fn(),
+//         },
+//       })),
+//     };
+//   },
+// };
 
 describe('render the flyout', () => {
   it('renders the flyout', async () => {
+    const coreSetup = coreMock.createSetup();
+    const [coreStartMock, startDepsMock] = await coreSetup.getStartServices();
     const Flyout = getCreateSLOFlyoutLazy({
-      core,
+      core: coreMock.createStart(),
       plugins: {
-        ...coreMock.createStart(),
-        cases: mockCasesContract(),
-        charts: chartPluginMock.createStartContract(),
-        data: dataPluginMock.createStartContract(),
-        dataViewEditor: dataViewEditor.createStart(),
-        dataViews: dataViews.createStart(),
-        lens: lensPluginMock.createStartContract(),
-        share: sharePluginMock.createStartContract(),
-        triggersActionsUi: triggersActionsUiStartMock.createStart(),
-        unifiedSearch: unifiedSearchPluginMock.createStartContract(),
+        ...coreStartMock,
+        ...sloPublicPluginsStartMock.createStart(),
       },
       observabilityRuleTypeRegistry: createObservabilityRuleTypeRegistryMock(),
       isDev: false,
--- x-pack/plugins/observability_solution/slo/public/plugin.mock.tsx
+++ x-pack/plugins/observability_solution/slo/public/plugin.mock.tsx
@@ -11,6 +11,21 @@ import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
 import { sharePluginMock } from '@kbn/share-plugin/public/mocks';
 import { unifiedSearchPluginMock } from '@kbn/unified-search-plugin/public/mocks';
 import { lensPluginMock } from '@kbn/lens-plugin/public/mocks';
+import { dataViewPluginMocks } from '@kbn/data-views-plugin/public/mocks';
+import { dashboardPluginMock } from '@kbn/dashboard-plugin/public/mocks';
+import { fieldFormatsServiceMock } from '@kbn/field-formats-plugin/public/mocks';
+import { uiActionsPluginMock } from '@kbn/ui-actions-plugin/public/mocks';
+import { embeddablePluginMock } from '@kbn/embeddable-plugin/public/mocks';
+import { coreMock } from '@kbn/core/public/mocks';
+import { presentationUtilPluginMock } from '@kbn/presentation-util-plugin/public/mocks';
+import { actionTypeRegistryMock } from '@kbn/triggers-actions-ui-plugin/public/application/action_type_registry.mock';
+import { navigationPluginMock } from '@kbn/navigation-plugin/public/mocks';
+import { createObservabilityRuleTypeRegistryMock } from '@kbn/observability-plugin/public';
+import { usageCollectionPluginMock } from '@kbn/usage-collection-plugin/public/mocks';
+import { discoverPluginMock } from '@kbn/discover-plugin/server/mocks';
+import { indexPatternFieldEditorPluginMock as dataViewFieldEditorPluginMock } from '@kbn/data-view-field-editor-plugin/public/mocks';
+import { ToastsStart } from '@kbn/core/public';
+import { licensingMock } from '@kbn/licensing-plugin/public/mocks';
 
 const triggersActionsUiStartMock = {
   createStart() {
@@ -41,32 +56,40 @@ const dataViewEditor = {
   },
 };
 
-const dataViews = {
-  createStart() {
-    return {
-      getIds: jest.fn().mockImplementation(() => []),
-      get: jest.fn(),
-      create: jest.fn().mockImplementation(() => ({
-        fields: {
-          getByName: jest.fn(),
-        },
-      })),
-    };
-  },
-};
-
 export const sloPublicPluginsStartMock = {
   createStart() {
     return {
+      actionTypeRegistry: actionTypeRegistryMock.create(),
       cases: mockCasesContract(),
       charts: chartPluginMock.createStartContract(),
       data: dataPluginMock.createStartContract(),
       dataViewEditor: dataViewEditor.createStart(),
-      dataViews: dataViews.createStart(),
+      dataViews: dataViewPluginMocks.createStartContract(),
       lens: lensPluginMock.createStartContract(),
       share: sharePluginMock.createStartContract(),
       triggersActionsUi: triggersActionsUiStartMock.createStart(),
       unifiedSearch: unifiedSearchPluginMock.createStartContract(),
+      dashboard: dashboardPluginMock.createStartContract(),
+      fieldFormats: fieldFormatsServiceMock.createStartContract(),
+      embeddable: embeddablePluginMock.createStartContract(),
+      uiActions: uiActionsPluginMock.createStartContract(),
+      presentationUtil: presentationUtilPluginMock.createStartContract(coreMock.createStart()),
+      navigation: navigationPluginMock.createStartContract(),
+      // security:
+      licensing: licensingMock.createStart(),
+      ruleTypeRegistry: createObservabilityRuleTypeRegistryMock(),
+      usageCollection: usageCollectionPluginMock.createSetupContract(),
+      discover: discoverPluginMock.createStartContract(),
+      dataViewFieldEditor: dataViewFieldEditorPluginMock.createStartContract(),
+      // observability: ,
+      // observabilityShared: ,
+      // aiops:
+      toastNotifications: {
+        addInfo: jest.fn(),
+        addWarning: jest.fn(),
+        addDanger: jest.fn(),
+        addSuccess: jest.fn(),
+      } as unknown as ToastsStart,
     };
   },
 };
