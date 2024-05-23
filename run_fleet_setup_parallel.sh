8b5c941ead5c627a289183986aeab6f4b069df88
Julia Bardi <julia.bardi@elastic.co>
2024-05-23 10:19:53 +0200

    integration test that starts 3 kibana

---
+++ run_fleet_setup_parallel.sh
@@ -0,0 +1,9 @@
+node scripts/jest_integration.js x-pack/plugins/fleet/server/integration_tests/es.test.ts &
+
+sleep 5 
+
+node scripts/jest_integration.js x-pack/plugins/fleet/server/integration_tests/fleet_setup.test.ts &
+node scripts/jest_integration.js x-pack/plugins/fleet/server/integration_tests/fleet_setup.test.ts &
+node scripts/jest_integration.js x-pack/plugins/fleet/server/integration_tests/fleet_setup.test.ts &
+
+exit 0
