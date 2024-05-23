d087b714375bae0332bd41db18d6922468bce503
Panagiota Mitsopoulou <giota85@gmail.com>
2024-05-23 11:14:04 +0200

    mock more stuff (hooks)

--- x-pack/plugins/observability_solution/slo/public/pages/slo_edit/shared_flyout/get_create_slo_flyout.test.tsx
+++ x-pack/plugins/observability_solution/slo/public/pages/slo_edit/shared_flyout/get_create_slo_flyout.test.tsx
@@ -6,69 +6,172 @@
  */
 
 import React from 'react';
-import { mockCasesContract } from '@kbn/cases-plugin/public/mocks';
-import { chartPluginMock } from '@kbn/charts-plugin/public/mocks';
 // import { coreMock } from '@kbn/core/public/mocks';
 import { coreMock } from '@kbn/core/public/mocks';
-import { discoverPluginMock } from '@kbn/discover-plugin/server/mocks';
-
-import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
-import { lensPluginMock } from '@kbn/lens-plugin/public/mocks';
 import { createObservabilityRuleTypeRegistryMock } from '@kbn/observability-plugin/public/rules/observability_rule_type_registry_mock';
-import { sharePluginMock } from '@kbn/share-plugin/public/mocks';
-import { unifiedSearchPluginMock } from '@kbn/unified-search-plugin/public/mocks';
 import { act, render, screen } from '@testing-library/react';
 import { getCreateSLOFlyoutLazy } from './get_create_slo_flyout';
 import { sloPublicPluginsStartMock } from '../../../plugin.mock';
-import { SloPublicPluginsStart } from '../../../types';
-// const triggersActionsUiStartMock = {
-//   createStart() {
-//     return {
-//       ruleTypeRegistry: {
-//         has: jest.fn(),
-//         register: jest.fn(),
-//         get: jest.fn(),
-//         list: jest.fn(),
-//       },
-//       actionTypeRegistry: {
-//         has: jest.fn((x) => true),
-//         register: jest.fn(),
-//         get: jest.fn(),
-//         list: jest.fn(),
-//       },
-//     };
-//   },
-// };
+import { useFetchDataViews, useCreateRule } from '@kbn/observability-plugin/public';
+import { useFetchIndices } from '../../../hooks/use_fetch_indices';
+import { useFetchApmSuggestions } from '../../../hooks/use_fetch_apm_suggestions';
+import { useFetchSloDetails } from '../../../hooks/use_fetch_slo_details';
+import { useCreateSlo } from '../../../hooks/use_create_slo';
+import { useUpdateSlo } from '../../../hooks/use_update_slo';
+import { useCapabilities } from '../../../hooks/use_capabilities';
+import { useFetchApmIndex } from '../../../hooks/use_fetch_apm_indices';
+import { useKibana } from '../../../utils/kibana_react';
+import { kibanaStartMock } from '../../../utils/kibana_react.mock';
+import { BehaviorSubject } from 'rxjs';
 
-// const dataViewEditor = {
-//   createStart() {
-//     return {
-//       userPermissions: {
-//         editDataView: jest.fn(),
-//         openEditor: jest.fn(),
-//       },
-//     };
-//   },
-// };
+import { cleanup } from '@testing-library/react';
 
-// const dataViews = {
-//   createStart() {
-//     return {
-//       getIds: jest.fn().mockImplementation(() => []),
-//       get: jest.fn(),
-//       create: jest.fn().mockImplementation(() => ({
-//         fields: {
-//           getByName: jest.fn(),
-//         },
-//       })),
-//     };
-//   },
-// };
+jest.mock('@kbn/observability-shared-plugin/public');
+jest.mock('@kbn/observability-plugin/public');
+jest.mock('../../../hooks/use_fetch_indices');
+jest.mock('../../../hooks/use_fetch_slo_details');
+jest.mock('../../../hooks/use_create_slo');
+jest.mock('../../../hooks/use_update_slo');
+jest.mock('../../../hooks/use_fetch_apm_suggestions');
+jest.mock('../../../hooks/use_capabilities');
+jest.mock('../../../hooks/use_fetch_apm_indices');
+
+const mockUseKibanaReturnValue = kibanaStartMock.startContract();
+
+jest.mock('../../../utils/kibana_react', () => ({
+  useKibana: jest.fn(() => mockUseKibanaReturnValue),
+}));
+
+const useKibanaMock = useKibana as jest.Mock;
+
+const useFetchIndicesMock = useFetchIndices as jest.Mock;
+const useFetchDataViewsMock = useFetchDataViews as jest.Mock;
+const useFetchSloMock = useFetchSloDetails as jest.Mock;
+const useCreateSloMock = useCreateSlo as jest.Mock;
+const useUpdateSloMock = useUpdateSlo as jest.Mock;
+const useCreateRuleMock = useCreateRule as jest.Mock;
+const useFetchApmSuggestionsMock = useFetchApmSuggestions as jest.Mock;
+const useCapabilitiesMock = useCapabilities as jest.Mock;
+const useFetchApmIndexMock = useFetchApmIndex as jest.Mock;
 
 describe('render the flyout', () => {
+  const mockCreateRule = jest.fn();
+  const mockUpdate = jest.fn();
+  const mockCreate = jest.fn(() => Promise.resolve({ id: 'mock-slo-id' }));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    useKibanaMock.mockReturnValue({
+      services: {
+        theme: {},
+        application: {
+          navigateToUrl: jest.fn(),
+        },
+        charts: {
+          theme: {
+            useChartsBaseTheme: () => {},
+          },
+        },
+        data: {
+          dataViews: {
+            find: jest.fn().mockReturnValue([]),
+            get: jest.fn().mockReturnValue([]),
+            getDefault: jest.fn(),
+          },
+        },
+        dataViews: {
+          create: jest.fn().mockResolvedValue({
+            getIndexPattern: jest.fn().mockReturnValue('some-index'),
+          }),
+        },
+        docLinks: {
+          links: {
+            query: {},
+          },
+        },
+        http: {
+          basePath: {
+            prepend: jest.fn(),
+          },
+        },
+        storage: {
+          get: () => {},
+        },
+        triggersActionsUi: {
+          getAddRuleFlyout: jest
+            .fn()
+            .mockReturnValue(<div data-test-subj="add-rule-flyout">Add Rule Flyout</div>),
+        },
+        uiSettings: {
+          get: () => {},
+        },
+        unifiedSearch: {
+          ui: {
+            QueryStringInput: () => <div>Query String Input</div>,
+            SearchBar: () => <div>Search Bar</div>,
+          },
+          autocomplete: {
+            hasQuerySuggestions: () => {},
+          },
+        },
+        licensing: {
+          license$: new BehaviorSubject(null),
+        },
+      },
+    });
+
+    useFetchDataViewsMock.mockReturnValue({
+      isLoading: false,
+      data: [{ getName: () => 'dataview', getIndexPattern: () => '.dataview-index' }],
+    });
+    useFetchIndicesMock.mockReturnValueOnce({
+      isLoading: false,
+      data: ['some-index', 'index-2'],
+    });
+
+    useCreateSloMock.mockReturnValue({
+      isLoading: false,
+      isSuccess: false,
+      isError: false,
+      mutateAsync: mockCreate,
+    });
+
+    useCreateRuleMock.mockReturnValue({
+      isLoading: false,
+      isSuccess: false,
+      isError: false,
+      mutateAsync: mockCreateRule,
+    });
+
+    useUpdateSloMock.mockReturnValue({
+      isLoading: false,
+      isSuccess: false,
+      isError: false,
+      mutateAsync: mockUpdate,
+    });
+
+    useFetchApmSuggestionsMock.mockReturnValue({
+      suggestions: ['cartService'],
+      isLoading: false,
+    });
+    useFetchSloMock.mockReturnValue({ isLoading: false, data: undefined });
+    useCapabilitiesMock.mockReturnValue({
+      hasWriteCapabilities: true,
+      hasReadCapabilities: true,
+    });
+
+    useFetchApmIndexMock.mockReturnValue({
+      isLoading: false,
+      data: '',
+    });
+  });
+
+  afterEach(cleanup);
+
   it('renders the flyout', async () => {
     const coreSetup = coreMock.createSetup();
-    const [coreStartMock, startDepsMock] = await coreSetup.getStartServices();
+    const [coreStartMock] = await coreSetup.getStartServices();
     const Flyout = getCreateSLOFlyoutLazy({
       core: coreMock.createStart(),
       plugins: {
