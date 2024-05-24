13f247d059c2de2b671fa2fac8136a25b3c9b332
jpdjere <jpdjeredjian@gmail.com>
2024-05-24 14:17:23 +0200

    Fix path

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_management/api/rules/bulk_actions/route.test.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_management/api/rules/bulk_actions/route.test.ts
@@ -33,7 +33,7 @@ import { loggingSystemMock } from '@kbn/core/server/mocks';
 import { readRules } from '../../../logic/rule_management/read_rules';
 
 jest.mock('../../../../../machine_learning/authz');
-jest.mock('../../../logic/crud/read_rules', () => ({ readRules: jest.fn() }));
+jest.mock('../../../logic/rule_management/read_rules', () => ({ readRules: jest.fn() }));
 
 describe('Perform bulk action route', () => {
   const readRulesMock = readRules as jest.Mock;
