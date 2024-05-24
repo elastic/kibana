fe97b177396879a48bac852830f4bb49a0543174
Paulo Henrique <paulo.henrique@elastic.co>
2024-05-23 17:01:10 -0700

    remove flickering empty state component

--- x-pack/plugins/cloud_security_posture/public/pages/configurations/latest_findings/use_latest_findings_table.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/configurations/latest_findings/use_latest_findings_table.tsx
@@ -43,6 +43,7 @@ export const useLatestFindingsTable = ({
     error: fetchError,
     isFetching,
     fetchNextPage,
+    isLoading,
   } = useLatestFindings({
     query,
     sort,
@@ -77,6 +78,7 @@ export const useLatestFindingsTable = ({
     rows,
     error,
     isFetching,
+    isLoading,
     fetchNextPage,
     passed,
     failed,
