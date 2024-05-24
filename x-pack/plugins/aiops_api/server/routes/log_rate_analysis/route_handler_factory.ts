5172965412bce8f8be57636894acf4de8ee3b06a
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-05-24 10:00:40 +0200

    update API endpoint

--- x-pack/plugins/aiops_api/server/routes/log_rate_analysis/route_handler_factory.ts
+++ x-pack/plugins/aiops_api/server/routes/log_rate_analysis/route_handler_factory.ts
@@ -13,35 +13,37 @@ import type {
   KibanaResponseFactory,
 } from '@kbn/core/server';
 import type { Logger } from '@kbn/logging';
-// import { createExecutionContext } from '@kbn/ml-route-utils';
+import { createExecutionContext } from '@kbn/ml-route-utils';
 import type { UsageCounter } from '@kbn/usage-collection-plugin/server';
-// import { AIOPS_TELEMETRY_ID, AIOPS_PLUGIN_ID } from '@kbn/aiops-common/constants';
-import type {
-  AiopsLogRateAnalysisSchema,
-  AiopsLogRateAnalysisApiVersion as ApiVersion,
-} from '@kbn/aiops-log-rate-analysis/api/schema';
+import {
+  // AIOPS_TELEMETRY_ID,
+  AIOPS_API_PLUGIN_ID,
+} from '@kbn/aiops-common/constants';
 // import { AIOPS_API_ENDPOINT } from '@kbn/aiops-common/constants';
 // import { isRequestAbortedError } from '@kbn/aiops-common/is_request_aborted_error';
+import { fetchSimpleLogRateAnalysis } from '@kbn/aiops-log-rate-analysis/queries/fetch_simple_log_rate_analysis';
 
 // import { trackAIOpsRouteUsage } from '../../lib/track_route_usage';
 import type { AiopsApiLicense } from '../../types';
 
+import type { AiopsLogRateAnalysisSchema } from './schema';
+
 /**
  * Log rate analysis route handler.
  */
-export function routeHandlerFactory<T extends ApiVersion>(
-  version: T,
+export function routeHandlerFactory(
+  version: '1',
   license: AiopsApiLicense,
   logger: Logger,
   coreStart: CoreStart,
   usageCounter?: UsageCounter
-): RequestHandler<unknown, unknown, AiopsLogRateAnalysisSchema<T>> {
+): RequestHandler<unknown, unknown, AiopsLogRateAnalysisSchema> {
   return async (
     context: RequestHandlerContext,
-    request: KibanaRequest<unknown, unknown, AiopsLogRateAnalysisSchema<T>>,
+    request: KibanaRequest<unknown, unknown, AiopsLogRateAnalysisSchema>,
     response: KibanaResponseFactory
   ) => {
-    // const { headers } = request;
+    const { events } = request;
 
     // trackAIOpsRouteUsage(
     //   `POST ${AIOPS_API_ENDPOINT.LOG_RATE_ANALYSIS}`,
@@ -53,11 +55,38 @@ export function routeHandlerFactory<T extends ApiVersion>(
       return response.forbidden();
     }
 
-    // const client = (await context.core).elasticsearch.client.asCurrentUser;
-    // const executionContext = createExecutionContext(coreStart, AIOPS_PLUGIN_ID, request.route.path);
+    const client = (await context.core).elasticsearch.client.asCurrentUser;
+    const executionContext = createExecutionContext(
+      coreStart,
+      AIOPS_API_PLUGIN_ID,
+      request.route.path
+    );
 
-    return response.ok({
-      body: 'CAN HAS LOG RATE ANALYSIS',
+    const controller = new AbortController();
+    const abortSignal = controller.signal;
+
+    events.aborted$.subscribe(() => {
+      controller.abort();
+    });
+    events.completed$.subscribe(() => {
+      controller.abort();
+    });
+
+    const { index, timefield, start, end } = request.body;
+
+    return await coreStart.executionContext.withContext(executionContext, async () => {
+      const logRateAnalysis = await fetchSimpleLogRateAnalysis(
+        client,
+        index,
+        start,
+        end,
+        timefield,
+        abortSignal
+      );
+
+      return response.ok({
+        body: logRateAnalysis,
+      });
     });
   };
 }
