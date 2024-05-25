248289291d6b88089d69a4bfd376cdca8bec05b2
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2024-05-24 18:10:26 -0700

    adds tests for bulk_get

--- x-pack/test_serverless/api_integration/test_suites/common/saved_objects_management/index.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/saved_objects_management/index.ts
@@ -10,5 +10,6 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('saved objects management apis', () => {
     loadTestFile(require.resolve('./find'));
+    loadTestFile(require.resolve('./bulk_get'));
   });
 }
