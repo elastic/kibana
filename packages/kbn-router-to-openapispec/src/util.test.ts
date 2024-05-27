c9620ededa5c349cdbee17158a8affc40bb60781
Jean-Louis Leysens <jloleysens@gmail.com>
2024-05-27 17:28:40 +0200

    added some utilities for tests

---
+++ packages/kbn-router-to-openapispec/src/util.test.ts
@@ -0,0 +1,35 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { OpenAPIV3 } from 'openapi-types';
+import { assignToPaths, extractTags } from './util';
+
+describe('extractTags', () => {
+  test.each([
+    [[], []],
+    [['a', 'b', 'c'], []],
+    [
+      ['oas-tag:foo', 'b', 'oas-tag:bar'],
+      ['foo', 'bar'],
+    ],
+  ])('given %s returns %s', (input, output) => {
+    expect(extractTags(input)).toEqual(output);
+  });
+});
+
+describe('assignToPaths', () => {
+  it('should transform path names', () => {
+    const paths = new Map<string, OpenAPIV3.PathItemObject>();
+    assignToPaths(paths, '/foo', {});
+    assignToPaths(paths, '/bar/{id?}', {});
+    expect(Array.from(paths.entries())).toEqual([
+      ['/foo', {}],
+      ['/bar/{id}', {}],
+    ]);
+  });
+});
