2e9bcb474f5508a2e45e2c089a77605a0e828a21
dkirchan <diamantis.kirchantzoglou@elastic.co>
2024-05-27 14:04:30 +0300

    PAths fix

--- .buildkite/scripts/pipelines/security_solution_quality_gate/api_integration/api-integration-tests.sh
+++ .buildkite/scripts/pipelines/security_solution_quality_gate/api_integration/api-integration-tests.sh
@@ -19,7 +19,7 @@ echo "--- Running test script $1"
 cd x-pack/test/security_solution_api_integration
 set +e
 
-TARGET_SCRIPT=$1 node .buildkite/scripts/pipelines/security_solution_quality_gate/api_integration/start_api_ftr_execution
+TARGET_SCRIPT=$1 node ../../../.buildkite/scripts/pipelines/security_solution_quality_gate/api_integration/start_api_ftr_execution
 cmd_status=$?
 echo "Exit code with status: $cmd_status"
 exit $cmd_status
