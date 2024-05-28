28302b946eb04d69b93fe79e5c2deb9c6f757711
Paulo Henrique <paulo.henrique@elastic.co>
2024-05-28 14:58:07 -0700

    fix Can't perform a React state update on an unmounted component.

--- x-pack/plugins/cloud_security_posture/public/components/no_findings_states.tsx
+++ x-pack/plugins/cloud_security_posture/public/components/no_findings_states.tsx
@@ -169,13 +169,10 @@ const Unprivileged = ({ unprivilegedIndices }: { unprivilegedIndices: string[] }
   />
 );
 
-const EmptySecurityFindingsPrompt = ({
-  kspmIntegrationLink,
-  cspmIntegrationLink,
-}: {
-  kspmIntegrationLink?: string;
-  cspmIntegrationLink?: string;
-}) => {
+const EmptySecurityFindingsPrompt = () => {
+  const kspmIntegrationLink = useCspIntegrationLink(KSPM_POLICY_TEMPLATE);
+  const cspmIntegrationLink = useCspIntegrationLink(CSPM_POLICY_TEMPLATE);
+
   return (
     <EuiEmptyPrompt
       data-test-subj={PACKAGE_NOT_INSTALLED_TEST_SUBJECT}
@@ -215,6 +212,7 @@ const EmptySecurityFindingsPrompt = ({
               color="primary"
               fill
               href={cspmIntegrationLink}
+              isDisabled={!cspmIntegrationLink}
               data-test-subj={CSPM_NOT_INSTALLED_ACTION_SUBJ}
             >
               <FormattedMessage
@@ -228,6 +226,7 @@ const EmptySecurityFindingsPrompt = ({
               color="primary"
               fill
               href={kspmIntegrationLink}
+              isDisabled={!kspmIntegrationLink}
               data-test-subj={KSPM_NOT_INSTALLED_ACTION_SUBJ}
             >
               <FormattedMessage
@@ -253,9 +252,6 @@ const NoFindingsStatesNotification = ({
   indicesStatus?: IndexDetails[];
   isNotInstalled: boolean;
 }) => {
-  const kspmIntegrationLink = useCspIntegrationLink(KSPM_POLICY_TEMPLATE);
-  const cspmIntegrationLink = useCspIntegrationLink(CSPM_POLICY_TEMPLATE);
-
   const unprivilegedIndices =
     indicesStatus &&
     indicesStatus
@@ -267,13 +263,7 @@ const NoFindingsStatesNotification = ({
     return <Unprivileged unprivilegedIndices={unprivilegedIndices || []} />;
   if (status === 'indexing' || status === 'waiting_for_results') return <Indexing />;
   if (status === 'index-timeout') return <IndexTimeout />;
-  if (isNotInstalled)
-    return (
-      <EmptySecurityFindingsPrompt
-        kspmIntegrationLink={kspmIntegrationLink}
-        cspmIntegrationLink={cspmIntegrationLink}
-      />
-    );
+  if (isNotInstalled) return <EmptySecurityFindingsPrompt />;
   if (status === 'not-deployed') return <NotDeployed postureType={postureType} />;
 
   return null;
