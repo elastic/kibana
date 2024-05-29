eed20977416934fff1d4819b1a7ebec517072611
Brad White <brad.white@elastic.co>
2024-05-28 22:57:45 -0600

    set env var

--- .buildkite/scripts/common/env.sh
+++ .buildkite/scripts/common/env.sh
@@ -14,6 +14,11 @@ PARENT_DIR="$(cd "$KIBANA_DIR/.."; pwd)"
 export PARENT_DIR
 export WORKSPACE="${WORKSPACE:-$PARENT_DIR}"
 
+echo "PARENT_DIR=$PARENT_DIR"
+echo "WORKSPACE=$WORKSPACE"
+echo "KIBANA_DIR=$KIBANA_DIR"
+echo "HOME=$HOME"
+
 export DOCS_REPO_CACHE_DIR="$HOME/.docs-repos"
 
 # A few things, such as Chrome, respect this variable
@@ -131,3 +136,6 @@ export TEST_GROUP_TYPE_FUNCTIONAL="Functional Tests"
 
 # tells the gh command what our default repo is
 export GH_REPO=github.com/elastic/kibana
+
+# used by FIPS agents to link FIPS OpenSSL modules
+export OPENSSL_MODULES=$HOME/openssl/lib/ossl-modules
