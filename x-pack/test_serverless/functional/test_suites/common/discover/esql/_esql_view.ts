0f1f08b99bf31c4cbdfff3cb07ead660add9636e
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-05-29 13:26:11 -0400

    skip failing test suite (#183493)

--- x-pack/test_serverless/functional/test_suites/common/discover/esql/_esql_view.ts
+++ x-pack/test_serverless/functional/test_suites/common/discover/esql/_esql_view.ts
@@ -35,7 +35,8 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     defaultIndex: 'logstash-*',
   };
 
-  describe('discover esql view', async function () {
+  // Failing: See https://github.com/elastic/kibana/issues/183493
+  describe.skip('discover esql view', async function () {
     before(async () => {
       await kibanaServer.savedObjects.cleanStandardList();
       log.debug('load kibana index with default index pattern');
