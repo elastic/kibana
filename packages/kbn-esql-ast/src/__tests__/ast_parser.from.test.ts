9ff9df6f933b48aa3df48f478247804981ddb420
vadimkibana <vadimkibana@gmail.com>
2024-05-29 23:11:07 +0200

    improve from source command test cases

--- packages/kbn-esql-ast/src/__tests__/ast_parser.from.test.ts
+++ packages/kbn-esql-ast/src/__tests__/ast_parser.from.test.ts
@@ -9,57 +9,152 @@
 import { getAstAndSyntaxErrors as parse } from '../ast_parser';
 
 describe('FROM', () => {
-  it('can parse basic FROM query', () => {
-    const text = 'FROM kibana_ecommerce_data';
-    const { ast } = parse(text);
+  describe('correctly formatted', () => {
+    it('can parse basic FROM query', () => {
+      const text = 'FROM kibana_ecommerce_data';
+      const { ast, errors } = parse(text);
 
-    expect(ast).toMatchObject([
-      {
-        type: 'command',
-        name: 'from',
-        args: [
-          {
-            type: 'source',
-            name: 'kibana_ecommerce_data',
-            sourceType: 'index',
-          },
-        ],
-      },
-    ]);
+      expect(errors.length).toBe(0);
+      expect(ast).toMatchObject([
+        {
+          type: 'command',
+          name: 'from',
+          args: [
+            {
+              type: 'source',
+              name: 'kibana_ecommerce_data',
+              sourceType: 'index',
+            },
+          ],
+        },
+      ]);
+    });
+
+    it('can parse FROM query with multiple index identifiers', () => {
+      const text = '\tFROM foo, bar \t\t, \n  baz';
+      const { ast, errors } = parse(text);
+
+      expect(errors.length).toBe(0);
+      expect(ast).toMatchObject([
+        {
+          type: 'command',
+          name: 'from',
+          args: [
+            {
+              type: 'source',
+              name: 'foo',
+              sourceType: 'index',
+            },
+            {
+              type: 'source',
+              name: 'bar',
+              sourceType: 'index',
+            },
+            {
+              type: 'source',
+              name: 'baz',
+              sourceType: 'index',
+            },
+          ],
+        },
+      ]);
+    });
+
+    it('can parse FROM query with a single metadata column', () => {
+      const text = 'from foo METADATA bar';
+      const { ast, errors } = parse(text);
+
+      expect(errors.length).toBe(0);
+      expect(ast).toMatchObject([
+        {
+          type: 'command',
+          name: 'from',
+          args: [
+            {
+              type: 'source',
+              name: 'foo',
+              sourceType: 'index',
+            },
+            {
+              type: 'option',
+              name: 'metadata',
+              args: [
+                {
+                  type: 'column',
+                  name: 'bar',
+                  quoted: false,
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+    });
+
+    it('can parse FROM query with multiple metadata columns', () => {
+      const text = 'from kibana_sample_data_ecommerce METADATA _index, \n _id\n';
+      const { ast, errors } = parse(text);
+
+      expect(errors.length).toBe(0);
+      expect(ast).toMatchObject([
+        {
+          type: 'command',
+          name: 'from',
+          args: [
+            {
+              type: 'source',
+              name: 'kibana_sample_data_ecommerce',
+              sourceType: 'index',
+            },
+            {
+              type: 'option',
+              name: 'metadata',
+              args: [
+                {
+                  type: 'column',
+                  name: '_index',
+                  quoted: false,
+                },
+                {
+                  type: 'column',
+                  name: '_id',
+                  quoted: false,
+                },
+              ],
+            },
+          ],
+        },
+      ]);
+    });
   });
 
-  it('can parse FROM query with metadata', () => {
-    const text = 'from kibana_sample_data_ecommerce METADATA _index, \n _id\n';
-    const { ast } = parse(text);
+  describe('when incorrectly formatted, returns errors', () => {
+    it('when no index identifier specified', () => {
+      const text = 'FROM \n\t';
+      const { errors } = parse(text);
 
-    expect(ast).toMatchObject([
-      {
-        type: 'command',
-        name: 'from',
-        args: [
-          {
-            type: 'source',
-            name: 'kibana_sample_data_ecommerce',
-            sourceType: 'index',
-          },
-          {
-            type: 'option',
-            name: 'metadata',
-            args: [
-              {
-                type: 'column',
-                name: '_index',
-                quoted: false,
-              },
-              {
-                type: 'column',
-                name: '_id',
-                quoted: false,
-              },
-            ],
-          },
-        ],
-      },
-    ]);
+      expect(errors.length > 0).toBe(true);
+    });
+
+    it('when comma is not followed by an index identifier', () => {
+      const text = '\tFROM foo, ';
+      const { errors } = parse(text);
+
+      expect(errors.length > 0).toBe(true);
+    });
+
+    it('when metadata has not columns', () => {
+      const text = 'from foo METADATA \t';
+      const { errors } = parse(text);
+
+      expect(errors.length > 0).toBe(true);
+    });
+
+    it('when metadata columns finish with a trailing comma', () => {
+      const text = 'from kibana_sample_data_ecommerce METADATA _index,';
+      const { errors } = parse(text);
+
+      expect(errors.length > 0).toBe(true);
+    });
   });
 });
