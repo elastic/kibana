7fc2de6f501a7aee232cae43e46f04e862a18ac9
Brad White <brad.white@elastic.co>
2024-05-30 11:01:24 -0600

    remove all invisible characters

--- .buildkite/scripts/steps/checks/verify_fips_enabled.sh
+++ .buildkite/scripts/steps/checks/verify_fips_enabled.sh
@@ -11,7 +11,7 @@ echo --- Verify FIPS enabled
 NODE_BINARY="$KIBANA_BUILD_LOCATION/node/bin/node"
 
 if [[ -x "$NODE_BINARY" ]]; then
-  if [[ "$("$NODE_BINARY" --enable-fips --openssl-config="$HOME/nodejs.cnf" -p 'crypto.getFips()' | tr -d '\n')" == "1" ]]; then
+  if [[ "$("$NODE_BINARY" --enable-fips --openssl-config="$HOME/nodejs.cnf" -p 'crypto.getFips()' | tr -d '[:cntrl:]')" == "1" ]]; then
     echo "FIPS enabled successfully"
     exit 0
   else
