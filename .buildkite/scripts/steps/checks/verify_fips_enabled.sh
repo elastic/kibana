1406c2fc5371a3d35e5fc9c19ce55a27382f2d1b
Brad White <brad.white@elastic.co>
2024-05-30 09:28:31 -0600

    adjust fips verification check

--- .buildkite/scripts/steps/checks/verify_fips_enabled.sh
+++ .buildkite/scripts/steps/checks/verify_fips_enabled.sh
@@ -11,13 +11,11 @@ echo --- Verify FIPS enabled
 NODE_BINARY="$KIBANA_BUILD_LOCATION/node/bin/node"
 
 if [[ -x "$NODE_BINARY" ]]; then
-  FIPS_STATUS=$("$NODE_BINARY" --enable-fips --openssl-config="$HOME/nodejs.cnf" -p 'crypto.getFips()' | tr -d '\n')
-
-  if [[ "$FIPS_STATUS" == "1" ]]; then
+  if [[ "$("$NODE_BINARY" --enable-fips --openssl-config="$HOME/nodejs.cnf" -p 'crypto.getFips()' | tr -d '\n')" == "1" ]]; then
     echo "FIPS enabled successfully"
     exit 0
   else
-    echo "Failed to enable FIPS: $FIPS_STATUS"
+    echo "Failed to enable FIPS"
     exit 1
   fi
 else
