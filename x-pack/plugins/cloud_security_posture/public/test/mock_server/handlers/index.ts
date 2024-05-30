d08358ca19fb1218ad954ac588c9e7193480744a
Paulo Henrique <paulo.henrique@elastic.co>
2024-05-30 12:34:13 -0700

    adding comment

--- x-pack/plugins/cloud_security_posture/public/test/mock_server/handlers/index.ts
+++ x-pack/plugins/cloud_security_posture/public/test/mock_server/handlers/index.ts
@@ -9,6 +9,11 @@ import { defaultFleetEpmPackagesCloudSecurityPosture } from './api/fleet_epm_pac
 import { defaultApiLicensingInfo } from './api/licensing_info_handler';
 import { defaultBenchmarks } from './internal/cloud_security_posture/benchmark_handlers';
 
+/**
+ * Default handlers for the mock server, these are the handlers that are always enabled
+ * when the mock server is started, but can be overridden by specific tests when needed.
+ * Recommended to use these handlers for common endpoints.
+ */
 export const defaultHandlers = [
   defaultApiLicensingInfo,
   defaultFleetEpmPackagesCloudSecurityPosture,
