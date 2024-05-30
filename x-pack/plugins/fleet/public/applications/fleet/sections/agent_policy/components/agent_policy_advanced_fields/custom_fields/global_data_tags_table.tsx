03c6a83124a4b6d71a2809659e8f99e518d67d9d
kaanyalti <kaan.yalti@elastic.co>
2024-05-30 03:35:18 -0400

    feature(179915): still refactoring

--- x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/components/agent_policy_advanced_fields/custom_fields/global_data_tags_table.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/components/agent_policy_advanced_fields/custom_fields/global_data_tags_table.tsx
@@ -8,7 +8,17 @@
 import React from 'react';
 import { useState } from 'react';
 
-import { EuiBasicTable, EuiBadge } from '@elastic/eui';
+import {
+  EuiBasicTable,
+  EuiBadge,
+  EuiPanel,
+  EuiText,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButton,
+  EuiFormRow,
+  EuiFieldText,
+} from '@elastic/eui';
 
 import type {
   NewAgentPolicy,
@@ -26,30 +36,112 @@ export const GlobalDataTagsTable: React.FunctionComponent<Props> = ({
   initialTags,
 }) => {
   const [globalDataTags, setGlobalDataTags] = useState<GlobalDataTag[]>(initialTags);
+  const [editIndexList, setEditIndexList] = useState<Set<number>>(new Set([]));
+
+  // Indicates whether a new row is currently being added to the table.
+  // When true, the table will display "Confirm" and "Cancel" actions for the new row
+  // to allow the user to either confirm the addition or cancel it.
+  const [isAdding, setIsAdding] = useState<boolean>(false);
+  const [newTag, setNewTag] = useState<GlobalDataTag>(null);
+
+  const handleAddField = () => {
+    setIsAdding(true);
+    setNewTag({ name: '', value: '' });
+  };
+
   const columns = [
     {
       field: 'name',
       name: 'Name',
       render: (name: string, item: GlobalDataTag) => {
-        return <EuiBadge>{name}</EuiBadge>;
+        const index = globalDataTags.indexOf(item);
+        const isEditing = editIndexList.has(index);
+        const isAddingRow = isAdding && item === newTag;
+        // const error = isAddingRow ? newTagErrors : errors[index] || {};
+        return isEditing || isAddingRow ? (
+          // <EuiFormRow isInvalid={!!error.name} error={error.name}>
+          <EuiFormRow>
+            <EuiFieldText
+              placeholder="Enter name"
+              value={isEditing ? globalDataTags[index].name : newTag.name}
+              name="name"
+            // onChange={(e) => (isEditing ? handleEditChange(e, index) : handleChange(e))}
+            // isInvalid={!!error.name}
+            />
+          </EuiFormRow>
+        ) : (
+          <EuiBadge>{name}</EuiBadge>
+        );
       },
     },
     {
       field: 'value',
       name: 'Value',
-      render: (value: string | number, item: GlobalDataTag) => {
-        return <EuiBadge>{value}</EuiBadge>;
+      render: (value: string, item: GlobalDataTag) => {
+        const index = globalDataTags.indexOf(item);
+        const isEditing = editIndexList.has(index);
+        const isAddingRow = isAdding && item === newTag;
+        // const error = isAddingRow ? newTagErrors : errors[index] || {};
+        return isEditing || isAddingRow ? (
+          // <EuiFormRow isInvalid={!!error.name} error={error.name}>
+          <EuiFormRow>
+            <EuiFieldText
+              placeholder="Enter name"
+              value={isEditing ? globalDataTags[index].value : newTag.value}
+              name="value"
+            // onChange={(e) => (isEditing ? handleEditChange(e, index) : handleChange(e))}
+            // isInvalid={!!error.name}
+            />
+          </EuiFormRow>
+        ) : (
+          <EuiBadge>{value}</EuiBadge>
+        );
       },
     },
   ];
 
+  const items = isAdding ? [...globalDataTags, newTag] : globalDataTags;
+
   return (
     <>
-      <EuiBasicTable
-        items={globalDataTags}
-        columns={columns}
-        noItemsMessage="No global data tags available"
-      />
+      {globalDataTags.length === 0 && !isAdding ? (
+        <EuiPanel>
+          <EuiText>
+            <h4>This policy has no custom fields</h4>
+          </EuiText>
+          <EuiFlexGroup justifyContent="center">
+            <EuiFlexItem grow={false}>
+              <EuiButton
+                iconType="plusInCircle"
+                onClick={handleAddField}
+                style={{ marginTop: '16px' }}
+              >
+                Add field
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiPanel>
+      ) : (
+        <>
+          <EuiBasicTable
+            items={items}
+            columns={columns}
+            noItemsMessage="No global data tags available"
+          />
+          <EuiFlexGroup justifyContent="flexStart">
+            <EuiFlexItem grow={false}>
+              <EuiButton
+                iconType="plusInCircle"
+                onClick={handleAddField}
+                style={{ marginTop: '16px' }}
+                isDisabled={isAdding}
+              >
+                Add another field
+              </EuiButton>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </>
+      )}
     </>
   );
 };
