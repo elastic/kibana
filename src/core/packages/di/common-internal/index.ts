dbec2b5cbb7425823787e35794874fcd0ac5594f
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-05-31 12:32:45 +0200

    Move DI service implementation to the common package

--- src/core/packages/di/common-internal/index.ts
+++ src/core/packages/di/common-internal/index.ts
@@ -6,3 +6,6 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
+
+export { CoreInjectionService } from './src/service';
+export type { InternalCoreDiServiceSetup, InternalCoreDiServiceStart } from './src/contracts';
