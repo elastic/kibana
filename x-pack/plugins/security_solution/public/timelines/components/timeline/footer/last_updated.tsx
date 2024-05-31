0041a74b8196700ebf2eb29ee6193de7c46b04c1
Jan Monschke <jan.monschke@elastic.co>
2024-05-31 11:31:07 +0200

    remove fixed width
    it's not required anymore, now that the flyout opens up in a separate modal

--- x-pack/plugins/security_solution/public/timelines/components/timeline/footer/last_updated.tsx
+++ x-pack/plugins/security_solution/public/timelines/components/timeline/footer/last_updated.tsx
@@ -5,9 +5,7 @@
  * 2.0.
  */
 
-import React, { useMemo } from 'react';
-import styled from 'styled-components';
-import { useEventDetailsWidthContext } from '../../../../common/components/events_viewer/event_details_width_context';
+import React from 'react';
 
 import { useKibana } from '../../../../common/lib/kibana';
 
@@ -20,24 +18,8 @@ export const isCompactFooter = (width: number): boolean => width < 600;
 export const FixedWidthLastUpdatedContainer = React.memo<FixedWidthLastUpdatedContainerProps>(
   ({ updatedAt }) => {
     const { timelines } = useKibana().services;
-    const width = useEventDetailsWidthContext();
-    const compact = useMemo(() => isCompactFooter(width), [width]);
-
-    return updatedAt > 0 ? (
-      <FixedWidthLastUpdated data-test-subj="fixed-width-last-updated" compact={compact}>
-        {timelines.getLastUpdated({ updatedAt, compact })}
-      </FixedWidthLastUpdated>
-    ) : null;
+    return updatedAt > 0 ? <span>{timelines.getLastUpdated({ updatedAt })}</span> : null;
   }
 );
 
 FixedWidthLastUpdatedContainer.displayName = 'FixedWidthLastUpdatedContainer';
-
-const FixedWidthLastUpdated = styled.span<{ compact?: boolean }>`
-  width: ${({ compact }) => (!compact ? 200 : 25)}px;
-  text-overflow: ellipsis;
-  text-align: end;
-  overflow: hidden;
-`;
-
-FixedWidthLastUpdated.displayName = 'FixedWidthLastUpdated';
