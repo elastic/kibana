07df4c3c3b7d6ca7c1f5c20cbfcb5d093876f525
vadimkibana <vadimkibana@gmail.com>
2024-05-31 12:34:24 +0200

    improve validation field load tests

--- packages/kbn-esql-validation-autocomplete/src/validation/validation.from.test.ts
+++ packages/kbn-esql-validation-autocomplete/src/validation/validation.from.test.ts
@@ -31,8 +31,17 @@ const setup = async () => {
 test('does not load fields when validating only a single FROM, SHOW, ROW command', async () => {
   const { validate, callbacks } = await setup();
   await validate('FROM kib');
+  await validate('FROM kibana_ecommerce METADATA _i');
+  await validate('FROM kibana_ecommerce METADATA _id | ');
   await validate('SHOW');
   await validate('ROW \t');
 
   expect(callbacks.getFieldsFor.mock.calls.length).toBe(0);
+});
+
+test('loads fields with FROM source when commands after pipe present', async () => {
+  const { validate, callbacks } = await setup();
+  await validate('FROM kibana_ecommerce METADATA _id | eval');
+
+  expect(callbacks.getFieldsFor.mock.calls.length).toBe(1);
 });
