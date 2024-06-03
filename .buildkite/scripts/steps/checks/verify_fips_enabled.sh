8342e13201f5a07df16ecb5f3596095766d2386c
Brad White <brad.white@elastic.co>
2024-06-03 10:41:37 -0600

    fix potential bug. add sed comment

--- .buildkite/scripts/steps/checks/verify_fips_enabled.sh
+++ .buildkite/scripts/steps/checks/verify_fips_enabled.sh
@@ -9,10 +9,12 @@ source .buildkite/scripts/common/util.sh
 echo --- Verify FIPS enabled
 
 NODE_BINARY="$KIBANA_BUILD_LOCATION/node/bin/node"
-FIPS_STATUS=$("$NODE_BINARY" --enable-fips --openssl-config="$HOME/nodejs.cnf" -p 'crypto.getFips()' | sed 's/\x1b\[[0-9;]*m//g' | tr -d \\n)
-echo "$FIPS_STATUS" | od -c
 
 if [[ -x "$NODE_BINARY" ]]; then
+  # sed is used to remove invisible ANSI color codes from the output
+  FIPS_STATUS=$("$NODE_BINARY" --enable-fips --openssl-config="$HOME/nodejs.cnf" -p 'crypto.getFips()' | sed 's/\x1b\[[0-9;]*m//g' | tr -d \\n)
+  echo "$FIPS_STATUS" | od -c
+
   if [[ "$FIPS_STATUS" == "1" ]]; then
     echo "FIPS enabled successfully"
     exit 0
