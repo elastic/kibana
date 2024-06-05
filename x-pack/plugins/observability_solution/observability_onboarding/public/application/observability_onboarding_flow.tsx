0c6aa4204291bb505fb139fc45937d4a73ebc638
Justin Kambic <jk@elastic.co>
2024-06-05 13:11:32 -0400

    Simplify by using `EuiPageTemplate`.

--- x-pack/plugins/observability_solution/observability_onboarding/public/application/observability_onboarding_flow.tsx
+++ x-pack/plugins/observability_solution/observability_onboarding/public/application/observability_onboarding_flow.tsx
@@ -7,12 +7,10 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { useEffect } from 'react';
-import { useKibana } from '@kbn/kibana-react-plugin/public';
 import { Route, Routes } from '@kbn/shared-ux-router';
 import { useLocation } from 'react-router-dom-v5-compat';
-import { CommonProps, EuiPageTemplate, EuiPanel, EuiSpacer, useEuiTheme } from '@elastic/eui';
+import { EuiPageTemplate, EuiPanel, EuiSpacer, useEuiTheme } from '@elastic/eui';
 import { css } from '@emotion/react';
-import { SerializedStyles } from '@emotion/serialize';
 import backgroundImageUrl from './header/background.svg';
 import { Footer } from './footer/footer';
 import { OnboardingFlowForm } from './onboarding_flow_form/onboarding_flow_form';
@@ -20,91 +18,78 @@ import { Header } from './header/header';
 import { SystemLogsPanel } from './quickstart_flows/system_logs';
 import { CustomLogsPanel } from './quickstart_flows/custom_logs';
 import { BackButton } from './shared/back_button';
-import { ConfigSchema } from '..';
 
 const queryClient = new QueryClient();
 
 export function ObservabilityOnboardingFlow() {
   const { pathname } = useLocation();
-
   const theme = useEuiTheme();
-  const [midSectionCss, footerContentCss] = useFooterCss();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
   return (
     <QueryClientProvider client={queryClient}>
-      <EuiPageTemplate.Section
-        paddingSize="xl"
+      <EuiPageTemplate
         css={css`
-          & > div {
-            background-image: url(${backgroundImageUrl});
-            background-position: right center;
-            background-repeat: no-repeat;
-          }
-          & {
-            background-color: ${theme.euiTheme.colors.ghost};
-          }
-        `}
-        grow={false}
-        restrictWidth
-      >
-        <EuiSpacer size="xl" />
-        <Header />
-      </EuiPageTemplate.Section>
-      <EuiPageTemplate.Section css={midSectionCss} paddingSize="xl" color="subdued" restrictWidth>
-        <Routes>
-          <Route path="/systemLogs">
-            <BackButton />
-            <SystemLogsPanel />
-          </Route>
-          <Route path="/customLogs">
-            <BackButton />
-            <CustomLogsPanel />
-          </Route>
-          <Route>
-            <OnboardingFlowForm />
-          </Route>
-        </Routes>
-        <EuiSpacer size="xl" />
-      </EuiPageTemplate.Section>
-      <EuiPageTemplate.Section
-        contentProps={footerContentCss}
-        css={css`
-          padding-inline: 0px;
+          padding-top: 0px !important;
         `}
       >
-        <EuiPanel
-          hasBorder
+        <EuiPageTemplate.Section
+          paddingSize="xl"
           css={css`
-            border-radius: 0px;
-            border-left: none;
-            border-bottom: none;
-            border-right: none;
+            & > div {
+              background-image: url(${backgroundImageUrl});
+              background-position: right center;
+              background-repeat: no-repeat;
+            }
+            & {
+              background-color: ${theme.euiTheme.colors.ghost};
+            }
+          `}
+          grow={false}
+          restrictWidth
+        >
+          <EuiSpacer size="xl" />
+          <Header />
+        </EuiPageTemplate.Section>
+        <EuiPageTemplate.Section paddingSize="xl" color="subdued" restrictWidth>
+          <Routes>
+            <Route path="/systemLogs">
+              <BackButton />
+              <SystemLogsPanel />
+            </Route>
+            <Route path="/customLogs">
+              <BackButton />
+              <CustomLogsPanel />
+            </Route>
+            <Route>
+              <OnboardingFlowForm />
+            </Route>
+          </Routes>
+          <EuiSpacer size="xl" />
+        </EuiPageTemplate.Section>
+        <EuiPageTemplate.Section
+          contentProps={{ css: { paddingBlock: 0 } }}
+          css={css`
+            padding-inline: 0px;
           `}
         >
-          <Footer />
-          <EuiSpacer size="xl" />
-        </EuiPanel>
-      </EuiPageTemplate.Section>
+          <EuiPanel
+            hasBorder
+            css={css`
+              border-radius: 0px;
+              border-left: none;
+              border-bottom: none;
+              border-right: none;
+            `}
+          >
+            <Footer />
+            <EuiSpacer size="xl" />
+          </EuiPanel>
+        </EuiPageTemplate.Section>
+      </EuiPageTemplate>
     </QueryClientProvider>
   );
-}
-
-function useFooterCss(): [SerializedStyles, CommonProps & React.HTMLAttributes<HTMLDivElement>] {
-  const kibana = useKibana<{ config: ConfigSchema }>();
-  return kibana.services.config?.serverless.enabled
-    ? [
-        css`
-          min-height: 900px;
-        `,
-        { css: { paddingBlock: 0 } },
-      ]
-    : [
-        css`
-          min-height: 850px;
-        `,
-        { css: { paddingBlock: 0, maxWidth: 'none !important' } },
-      ];
 }
