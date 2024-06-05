d5e8937ee95ad2a08030648cb533e78c327cd068
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-06-05 16:48:12 +0200

    Add browser-side example

---
+++ examples/dependency_injection_example/public/app.tsx
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React, { useCallback, useEffect } from 'react';
+import { useService } from '@redtea/react-inversify';
+import {
+  EuiHorizontalRule,
+  EuiPage,
+  EuiPageBody,
+  EuiPageSection,
+  EuiPageHeader,
+  EuiPageHeaderSection,
+  EuiText,
+  EuiTextArea,
+  EuiTitle,
+} from '@elastic/eui';
+import { EchoService } from './service';
+
+export function App() {
+  const service = useService(EchoService);
+  const [input, setInput] = React.useState<string>('');
+  const [output, setOutput] = React.useState<string>('');
+
+  useEffect(() => {
+    (async () => {
+      setOutput(await service.echo(input));
+    })();
+  }, [service, input]);
+
+  const handleChange = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.target.value);
+  }, []);
+
+  return (
+    <EuiPage>
+      <EuiPageBody style={{ maxWidth: 1200, margin: '0 auto' }}>
+        <EuiPageHeader>
+          <EuiPageHeaderSection>
+            <EuiTitle size="l">
+              <h1>Dependency Injection Demo</h1>
+            </EuiTitle>
+          </EuiPageHeaderSection>
+        </EuiPageHeader>
+        <EuiPageSection>
+          <EuiTextArea
+            placeholder="Message"
+            fullWidth
+            onChange={handleChange}
+            data-test-subj="input"
+          />
+          <EuiHorizontalRule />
+          <EuiText data-test-subj="output">{output}</EuiText>
+        </EuiPageSection>
+      </EuiPageBody>
+    </EuiPage>
+  );
+}
