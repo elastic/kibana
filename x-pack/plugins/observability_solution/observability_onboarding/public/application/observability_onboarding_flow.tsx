c222cd9b57151188619fb8e3e0efac788c672697
Joe Reuter <johannes.reuter@elastic.co>
2024-06-05 09:01:13 +0200

    poc

--- x-pack/plugins/observability_solution/observability_onboarding/public/application/observability_onboarding_flow.tsx
+++ x-pack/plugins/observability_solution/observability_onboarding/public/application/observability_onboarding_flow.tsx
@@ -7,10 +7,22 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { useEffect } from 'react';
+import { i18n } from '@kbn/i18n';
 import { Route, Routes } from '@kbn/shared-ux-router';
-import { useLocation } from 'react-router-dom-v5-compat';
-import { EuiPageTemplate, EuiSpacer } from '@elastic/eui';
+import { useLocation, useSearchParams } from 'react-router-dom-v5-compat';
+import {
+  EuiAccordion,
+  EuiButton,
+  EuiCodeBlock,
+  EuiPageTemplate,
+  EuiPanel,
+  EuiSpacer,
+  EuiTitle,
+} from '@elastic/eui';
 import { css } from '@emotion/react';
+import { useKibana } from '@kbn/kibana-react-plugin/public';
+import useAsyncFn from 'react-use/lib/useAsyncFn';
+import { generateCustomLogsYml } from '../../common/elastic_agent_logs';
 import backgroundImageUrl from './header/background.svg';
 import { Footer } from './footer/footer';
 import { OnboardingFlowForm } from './onboarding_flow_form/onboarding_flow_form';
@@ -27,6 +39,100 @@ export function ObservabilityOnboardingFlow() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
+  const DetectionsPanel = () => {
+    const { services } = useKibana();
+    const [searchParams, setSearchParams] = useSearchParams();
+    const host = searchParams.get('host');
+
+    const [{ value: detections }, fetchDetections] = useAsyncFn(async () => {
+      // This is a test
+      return await services.http!.post('/api/observability/detections', {
+        body: JSON.stringify({ hostName: host }),
+      });
+    }, []);
+
+    useEffect(() => {
+      fetchDetections();
+    }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+    const [elasticAgentYml, setElasticAgentYml] = React.useState('');
+
+    console.log(detections);
+    const detectionsByPackage =
+      detections &&
+      Object.fromEntries(detections.results?.map((d) => [d.detection.package, d.detection]));
+    console.log(detectionsByPackage);
+    return (
+      <EuiPanel hasBorder>
+        <EuiTitle size="s">
+          <strong>
+            {i18n.translate(
+              'xpack.observability_onboarding.detectionsPanel.h1.observabilityGapsForHostLabel',
+              { defaultMessage: 'Observability gaps for host ' }
+            )}
+            {searchParams.get('host')}
+          </strong>
+        </EuiTitle>
+        <br />
+        <br />
+        {detectionsByPackage &&
+          Object.values(detectionsByPackage).map((detection) => (
+            <EuiAccordion
+              id={'detection_' + detection.package}
+              buttonContent={`${detection.package} (${detection.paths.length} files)`}
+            >
+              {i18n.translate(
+                'xpack.observability_onboarding.detectionsPanel.filesAccordionLabel',
+                { defaultMessage: 'Files:' }
+              )}
+              <ul>
+                {detection.paths.map((path) => (
+                  <li key={path}>{path}</li>
+                ))}
+              </ul>
+              {detection.package !== 'custom' ? (
+                <EuiButton
+                  data-test-subj="observabilityOnboardingDetectionsPanelGoToIntegrationButton"
+                  onClick={() => {
+                    window.open(`/app/integrations/detail/${detection.package}`, '_blank');
+                  }}
+                >
+                  {i18n.translate(
+                    'xpack.observability_onboarding.detectionsPanel.goToIntegrationButtonLabel',
+                    { defaultMessage: 'Go to integration' }
+                  )}
+                </EuiButton>
+              ) : (
+                <EuiButton
+                  data-test-subj="observabilityOnboardingDetectionsPanelGoToIntegrationButton"
+                  onClick={() => {
+                    setElasticAgentYml(
+                      generateCustomLogsYml({
+                        datasetName: 'logs',
+                        namespace: 'default',
+                        logFilePaths: detection.paths,
+                        apiKey: '$API_KEY',
+                        esHost: ['http://localhost:9200'],
+                        logfileId: 'my-logs-id',
+                      })
+                    );
+                  }}
+                >
+                  {i18n.translate(
+                    'xpack.observability_onboarding.detectionsPanel.generateElasticAgentYmlButtonLabel',
+                    { defaultMessage: 'Generate elastic-agent.yml' }
+                  )}
+                </EuiButton>
+              )}
+              {elasticAgentYml && detection.package === 'custom' && (
+                <EuiCodeBlock isCopyable>{elasticAgentYml}</EuiCodeBlock>
+              )}
+            </EuiAccordion>
+          ))}
+      </EuiPanel>
+    );
+  };
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -54,6 +160,9 @@ export function ObservabilityOnboardingFlow() {
           <Route path="/customLogs">
             <BackButton />
             <CustomLogsPanel />
+          </Route>
+          <Route path="/detections">
+            <DetectionsPanel />
           </Route>
           <Route>
             <OnboardingFlowForm />
