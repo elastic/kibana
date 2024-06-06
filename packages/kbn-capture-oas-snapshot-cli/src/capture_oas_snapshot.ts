dd7b25305ac55b79c825f851ca121340a08b2947
Jean-Louis Leysens <jloleysens@gmail.com>
2024-06-06 11:18:26 +0200

    wip2

---
+++ packages/kbn-capture-oas-snapshot-cli/src/capture_oas_snapshot.ts
@@ -0,0 +1,117 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import fs from 'node:fs/promises';
+import { encode } from 'node:querystring';
+import fetch from 'node-fetch';
+import * as Rx from 'rxjs';
+import { startTSWorker } from '@kbn/dev-utils';
+import { createTestEsCluster } from '@kbn/test';
+import type { ToolingLog } from '@kbn/tooling-log';
+import type { Result } from './kibana_worker';
+import { sortAndPrettyPrint } from './run_capture_oas_snapshot_cli';
+
+interface CaptureOasSnapshotArgs {
+  log: ToolingLog;
+  buildFlavour: 'serverless' | 'traditional';
+  outputFile: string;
+  update: boolean;
+  filters?: {
+    pathStartsWith?: string[];
+    excludePathsMatching?: string[];
+  };
+}
+
+const MB = 1024 * 1024;
+const twoDeci = (num: number) => Math.round(num * 100) / 100;
+
+export async function captureOasSnapshot({
+  log,
+  filters = {},
+  buildFlavour,
+  update,
+  outputFile,
+}: CaptureOasSnapshotArgs): Promise<void> {
+  const { excludePathsMatching = [], pathStartsWith } = filters;
+  // internal consts
+  const port = 5622;
+  // We are only including /api/status for now
+  excludePathsMatching.push(
+    '/{path*}',
+    // Our internal asset paths
+    '/XXXXXXXXXXXX/'
+  );
+
+  log.info('Starting es...');
+  const esCluster = await log.indent(4, async () => {
+    const cluster = createTestEsCluster({ log });
+    await cluster.start();
+    return cluster;
+  });
+
+  log.info('Starting Kibana...');
+  const kbWorker = await log.indent(4, async () => {
+    log.info('Loading core with all plugins enabled so that we can capture OAS for all...');
+    const { msg$, proc } = startTSWorker<Result>({
+      log,
+      src: require.resolve('./kibana_worker'),
+      execArgv: [`--${buildFlavour}`],
+    });
+    await Rx.firstValueFrom(
+      msg$.pipe(
+        Rx.map((msg) => {
+          if (msg !== 'ready')
+            throw new Error(`received unexpected message from worker (expected "ready"): ${msg}`);
+        })
+      )
+    );
+    return proc;
+  });
+
+  addCleanupTask(async () => {
+    await esCluster.cleanup();
+    kbWorker.kill('SIGILL');
+  });
+
+  try {
+    const qs = encode({
+      access: 'public',
+      version: '2023-10-31', // hard coded for now, we can make this configurable later
+      pathStartsWith,
+      excludePathsMatching,
+    });
+    const url = `http://localhost:${port}/api/oas?${qs}`;
+    log.info(`Fetching OAS at ${url}...`);
+    const result = await fetch(url, {
+      headers: {
+        'kbn-xsrf': 'kbn-oas-snapshot',
+        authorization: `Basic ${Buffer.from('elastic:changeme').toString('base64')}`,
+      },
+    });
+    if (result.status !== 200) {
+      log.error(`Failed to fetch OAS: ${JSON.stringify(result, null, 2)}`);
+      throw new Error(`Failed to fetch OAS: ${result.status}`);
+    }
+    const currentOas = await result.json();
+    log.info(`Recieved OAS, writing to ${outputFile}...`);
+    if (update) {
+      await fs.writeFile(outputFile, sortAndPrettyPrint(currentOas));
+      const { size: sizeBytes } = await fs.stat(outputFile);
+      log.success(`OAS written to ${outputFile}. File size ~${twoDeci(sizeBytes / MB)} MB.`);
+    } else {
+      log.success(
+        `OAS recieved, not writing to file. Got OAS for ${
+          Object.keys(currentOas.paths).length
+        } paths.`
+      );
+    }
+  } catch (err) {
+    log.error(`Failed to capture OAS: ${JSON.stringify(err, null, 2)}`);
+    throw err;
+  }
+}
--- packages/kbn-capture-oas-snapshot-cli/src/run_capture_oas_snapshot_cli.ts
+++ packages/kbn-capture-oas-snapshot-cli/src/run_capture_oas_snapshot_cli.ts
@@ -7,18 +7,10 @@
  */
 
 import path from 'node:path';
-import fs from 'node:fs/promises';
-import { encode } from 'node:querystring';
-import fetch from 'node-fetch';
 import { run } from '@kbn/dev-cli-runner';
-import { startTSWorker } from '@kbn/dev-utils';
-import { createTestEsCluster } from '@kbn/test';
-import * as Rx from 'rxjs';
 import { REPO_ROOT } from '@kbn/repo-info';
 import chalk from 'chalk';
-import type { Result } from './kibana_worker';
-
-const OAS_FILE_PATH = path.resolve(REPO_ROOT, './oas_docs/bundle.json');
+import { captureOasSnapshot } from './capture_oas_snapshot';
 
 export const sortAndPrettyPrint = (object: object) => {
   const keys = new Set<string>();
@@ -29,91 +21,32 @@ export const sortAndPrettyPrint = (object: object) => {
   return JSON.stringify(object, Array.from(keys).sort(), 2);
 };
 
-const MB = 1024 * 1024;
-const twoDeci = (num: number) => Math.round(num * 100) / 100;
-
 run(
-  async ({ log, flagsReader, addCleanupTask }) => {
+  async ({ log, flagsReader }) => {
     const update = flagsReader.boolean('update');
     const serverless = flagsReader.boolean('serverless');
+    const traditional = flagsReader.boolean('traditional');
     const pathStartsWith = flagsReader.arrayOfStrings('include-path');
     const excludePathsMatching = flagsReader.arrayOfStrings('exclude-path') ?? [];
 
-    // internal consts
-    const port = 5622;
-    // We are only including /api/status for now
-    excludePathsMatching.push(
-      '/{path*}',
-      // Our internal asset paths
-      '/XXXXXXXXXXXX/'
-    );
-
-    log.info('Starting es...');
-    const esCluster = await log.indent(4, async () => {
-      const cluster = createTestEsCluster({ log });
-      await cluster.start();
-      return cluster;
-    });
-
-    log.info('Starting Kibana...');
-    const kbWorker = await log.indent(4, async () => {
-      log.info('Loading core with all plugins enabled so that we can capture OAS for all...');
-      const { msg$, proc } = startTSWorker<Result>({
+    if (traditional) {
+      await captureOasSnapshot({
         log,
-        src: require.resolve('./kibana_worker'),
-        execArgv: serverless ? ['--serverless'] : undefined,
+        buildFlavour: 'serverless',
+        outputFile: path.resolve(REPO_ROOT, './oas_docs/bundle.json'),
+        filters: { pathStartsWith, excludePathsMatching },
+        update,
       });
-      await Rx.firstValueFrom(
-        msg$.pipe(
-          Rx.map((msg) => {
-            if (msg !== 'ready')
-              throw new Error(`received unexpected message from worker (expected "ready"): ${msg}`);
-          })
-        )
-      );
-      return proc;
-    });
+    }
 
-    addCleanupTask(async () => {
-      await esCluster.cleanup();
-      kbWorker.kill('SIGILL');
-    });
-
-    try {
-      const qs = encode({
-        access: 'public',
-        version: '2023-10-31', // hard coded for now, we can make this configurable later
-        pathStartsWith,
-        excludePathsMatching,
+    if (serverless) {
+      await captureOasSnapshot({
+        log,
+        buildFlavour: 'serverless',
+        outputFile: path.resolve(REPO_ROOT, './oas_docs/bundle.serverless.json'),
+        filters: { pathStartsWith, excludePathsMatching },
+        update,
       });
-      const url = `http://localhost:${port}/api/oas?${qs}`;
-      log.info(`Fetching OAS at ${url}...`);
-      const result = await fetch(url, {
-        headers: {
-          'kbn-xsrf': 'kbn-oas-snapshot',
-          authorization: `Basic ${Buffer.from('elastic:changeme').toString('base64')}`,
-        },
-      });
-      if (result.status !== 200) {
-        log.error(`Failed to fetch OAS: ${JSON.stringify(result, null, 2)}`);
-        throw new Error(`Failed to fetch OAS: ${result.status}`);
-      }
-      const currentOas = await result.json();
-      log.info(`Recieved OAS, writing to ${OAS_FILE_PATH}...`);
-      if (update) {
-        await fs.writeFile(OAS_FILE_PATH, sortAndPrettyPrint(currentOas));
-        const { size: sizeBytes } = await fs.stat(OAS_FILE_PATH);
-        log.success(`OAS written to ${OAS_FILE_PATH}. File size ~${twoDeci(sizeBytes / MB)} MB.`);
-      } else {
-        log.success(
-          `OAS recieved, not writing to file. Got OAS for ${
-            Object.keys(currentOas.paths).length
-          } paths.`
-        );
-      }
-    } catch (err) {
-      log.error(`Failed to capture OAS: ${JSON.stringify(err, null, 2)}`);
-      throw err;
     }
   },
   {
@@ -121,17 +54,19 @@ run(
       Get the current OAS from Kibana's /api/oas API
     `,
     flags: {
-      boolean: ['update', 'serverless'],
-      string: ['include-path', 'exclude-path'],
+      boolean: ['update'],
+      string: ['include-path', 'exclude-path', 'build-flavor'],
       default: {
         fix: false,
-        serverless: false,
+        serverless: true,
+        traditional: true,
       },
       help: `
         --include-path            Path to include. Path must start with provided value. Can be passed multiple times.
         --exclude-path            Path to exclude. Path must NOT start with provided value. Can be passed multiple times.
         --update                  Write the current OAS to ${chalk.cyan(OAS_FILE_PATH)}.
-        --serverless              Whether to run Kibana in serverless mode, defaults to false.
+        --no-serverless           Whether to skip OAS for serverless Kibana. Defaults to false.
+        --no-traditional          Whether to skip OAS for traditional Kibana. Defaults to false.
       `,
     },
   }
