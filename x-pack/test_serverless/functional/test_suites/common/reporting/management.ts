a277b553034666df109eb3d27b313888dbccfec5
Tre' Seymour <wayne.seymour@elastic.co>
2024-06-07 10:43:43 +0100

    put this back

--- x-pack/test_serverless/functional/test_suites/common/reporting/management.ts
+++ x-pack/test_serverless/functional/test_suites/common/reporting/management.ts
@@ -36,7 +36,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
 
   describe('Reporting Management app', function () {
     // security_exception: action [indices:admin/create] is unauthorized for user [elastic] with effective roles [superuser] on restricted indices [.reporting-2020.04.19], this action is granted by the index privileges [create_index,manage,all]
-    // this.tags('failsOnMKI');
+    this.tags('failsOnMKI');
     const savedObjectsArchive = 'test/functional/fixtures/kbn_archiver/discover';
 
     const job: JobParamsCsvFromSavedObject = {
