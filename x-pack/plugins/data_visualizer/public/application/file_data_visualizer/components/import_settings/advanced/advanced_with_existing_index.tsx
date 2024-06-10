bb46c45be3c5c1d829265905691c9ab0a0612526
James Gowdy <jgowdy@elastic.co>
2024-06-10 21:32:20 +0100

    renaming functions

--- x-pack/plugins/data_visualizer/public/application/file_data_visualizer/components/import_settings/advanced/advanced_with_existing_index.tsx
+++ x-pack/plugins/data_visualizer/public/application/file_data_visualizer/components/import_settings/advanced/advanced_with_existing_index.tsx
@@ -103,48 +103,49 @@ export const AdvancedWithExistingIndexSettings: FC<Props> = ({
   >([]);
   const [missingFields, setMissingFields] = useState<Field[]>([]);
   const [pipelineIdErrors, setPipelineIdErrors] = useState<string[]>([]);
-  // const [existingPipeline, setExistingPipeline] = useState<boolean>(true);
 
-  useEffect(() => {
-    if (selectedIndexOptions.length === 0) {
-      onIndexChange('', true);
-      return;
-    }
-    onIndexChange(selectedIndexOptions[0].label, true);
-    getMapping(selectedIndexOptions[0].label).then((m) => {
-      // eslint-disable-next-line no-console
-      console.log(m);
-      const mappingsString2 = JSON.stringify(m, null, 2);
-      onMappingsStringChange(mappingsString2);
+  useEffect(
+    function onIndexSelectionChange() {
+      if (selectedIndexOptions.length === 0) {
+        onIndexChange('', true);
+        return;
+      }
+      onIndexChange(selectedIndexOptions[0].label, true);
+      getMapping(selectedIndexOptions[0].label).then((m) => {
+        // eslint-disable-next-line no-console
+        console.log(m);
+        const mappingsString2 = JSON.stringify(m, null, 2);
+        onMappingsStringChange(mappingsString2);
 
-      const fields = extractFields(mappingsString2);
-      // eslint-disable-next-line no-console
-      console.log('fields:', fields);
-    });
-  }, [
-    getMapping,
-    onIndexChange,
-    onMappingsStringChange,
-    originalMappingsString,
-    selectedIndexOptions,
-  ]);
+        const fields = extractFields(mappingsString2);
+        // eslint-disable-next-line no-console
+        console.log('fields:', fields);
+      });
+    },
+    [
+      getMapping,
+      onIndexChange,
+      onMappingsStringChange,
+      originalMappingsString,
+      selectedIndexOptions,
+    ]
+  );
 
-  useEffect(() => {
-    if (selectedPipelineOptions.length === 0) {
-      return;
-    }
-    const pipeline = pipelines.find((p) => p.name === selectedPipelineOptions[0].label);
-    if (pipeline !== undefined) {
-      onPipelineStringChange(JSON.stringify(pipeline, null, 2));
-      // eslint-disable-next-line no-console
-      console.log('pipeline:', pipeline);
-      onPipelineIdChange(pipeline.name);
-    }
-  }, [onPipelineIdChange, onPipelineStringChange, pipelines, selectedPipelineOptions]);
-
-  useEffect(() => {
-    setMissingFields(getMissingFields(originalMappingsString, mappingsString));
-  }, [originalMappingsString, mappingsString]);
+  useEffect(
+    function onPipelineSelectionChange() {
+      if (selectedPipelineOptions.length === 0) {
+        return;
+      }
+      const pipeline = pipelines.find((p) => p.name === selectedPipelineOptions[0].label);
+      if (pipeline !== undefined) {
+        onPipelineStringChange(JSON.stringify(pipeline, null, 2));
+        // eslint-disable-next-line no-console
+        console.log('pipeline:', pipeline);
+        onPipelineIdChange(pipeline.name);
+      }
+    },
+    [onPipelineIdChange, onPipelineStringChange, pipelines, selectedPipelineOptions]
+  );
 
   const addMissingField = useCallback(
     (field: Field) => {
@@ -155,22 +156,26 @@ export const AdvancedWithExistingIndexSettings: FC<Props> = ({
     [mappingsString, onMappingsStringChange]
   );
 
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('missingFields:', missingFields);
-  }, [missingFields]);
+  useEffect(
+    function initMissingFields() {
+      setMissingFields(getMissingFields(originalMappingsString, mappingsString));
+    },
+    [originalMappingsString, mappingsString]
+  );
 
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('indices:', indices);
-    setIndexOptions(indices.map((i) => ({ label: i.name })));
-  }, [getMapping, indices]);
+  useEffect(
+    function initIndicesOptions() {
+      setIndexOptions(indices.map((i) => ({ label: i.name })));
+    },
+    [getMapping, indices]
+  );
 
-  useEffect(() => {
-    // eslint-disable-next-line no-console
-    console.log('pipelines:', pipelines);
-    setPipelineOptions(pipelines.map((i) => ({ label: i.name })));
-  }, [getMapping, pipelines]);
+  useEffect(
+    function initPipelineOptions() {
+      setPipelineOptions(pipelines.map((i) => ({ label: i.name })));
+    },
+    [getMapping, pipelines]
+  );
 
   const findPipelineOption = useCallback(
     (id: string) => {
@@ -192,28 +197,37 @@ export const AdvancedWithExistingIndexSettings: FC<Props> = ({
     }
   }, [index, findPipelineOption]);
 
-  useEffect(() => {
-    chooseSuggestedPipeline();
-  }, [pipelineOptions, index, findPipelineOption, chooseSuggestedPipeline]);
-
-  useEffect(() => {
-    onPipelineIdChange(null);
-    setSelectedPipelineOptions([]);
-    if (createNewPipeline) {
-      onPipelineStringChange(originalPipelineString);
-      onPipelineIdChange(`${index}-pipeline`);
-    } else {
+  useEffect(
+    function autoSelectSuggestedPipeline() {
       chooseSuggestedPipeline();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [createNewPipeline]);
+    },
+    [pipelineOptions, index, findPipelineOption, chooseSuggestedPipeline]
+  );
 
-  useEffect(() => {
-    if (pipelineId !== null) {
-      const pipelineExists = findPipelineOption(pipelineId) !== null;
-      setPipelineIdErrors(pipelineExists ? ['Pipeline already exists'] : []);
-    }
-  }, [findPipelineOption, pipelineId]);
+  useEffect(
+    function toggleCreateNewPipeline() {
+      onPipelineIdChange(null);
+      setSelectedPipelineOptions([]);
+      if (createNewPipeline) {
+        onPipelineStringChange(originalPipelineString);
+        onPipelineIdChange(`${index}-pipeline`);
+      } else {
+        chooseSuggestedPipeline();
+      }
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [createNewPipeline]
+  );
+
+  useEffect(
+    function validatePipelineId() {
+      if (pipelineId !== null) {
+        const pipelineExists = findPipelineOption(pipelineId) !== null;
+        setPipelineIdErrors(pipelineExists ? ['Pipeline already exists'] : []);
+      }
+    },
+    [findPipelineOption, pipelineId]
+  );
 
   // !!!!!!!!  add a check to see if the mappings are too different
   // if so, block the import and show a warning
@@ -230,24 +244,6 @@ export const AdvancedWithExistingIndexSettings: FC<Props> = ({
         isInvalid={indexNameError !== ''}
         error={[indexNameError]}
       >
-        {/* <EuiFieldText
-          placeholder={i18n.translate(
-            'xpack.dataVisualizer.file.advancedImportSettings.indexNamePlaceholder',
-            {
-              defaultMessage: 'index name',
-            }
-          )}
-          value={index}
-          disabled={initialized === true}
-          onChange={onIndexChange}
-          isInvalid={indexNameError !== ''}
-          aria-label={i18n.translate(
-            'xpack.dataVisualizer.file.advancedImportSettings.indexNameAriaLabel',
-            {
-              defaultMessage: 'Index name, required field',
-            }
-          )}
-        /> */}
         <EuiComboBox<string>
           singleSelection={{ asPlainText: true }}
           selectedOptions={selectedIndexOptions}
@@ -258,63 +254,9 @@ export const AdvancedWithExistingIndexSettings: FC<Props> = ({
 
       <EuiSpacer size="m" />
 
-      {/* <CreateDataViewToolTip showTooltip={canCreateDataView === false}>
-        <EuiCheckbox
-          id="createDataView"
-          label={
-            <FormattedMessage
-              id="xpack.dataVisualizer.file.advancedImportSettings.createDataViewLabel"
-              defaultMessage="Create data view"
-            />
-          }
-          checked={createDataView === true}
-          disabled={initialized === true || canCreateDataView === false}
-          onChange={onCreateDataViewChange}
-        />
-      </CreateDataViewToolTip>
-
-      <EuiSpacer size="s" />
-
-      <EuiFormRow
-        label={
-          <FormattedMessage
-            id="xpack.dataVisualizer.file.advancedImportSettings.dataViewNameLabel"
-            defaultMessage="Data view name"
-          />
-        }
-        isInvalid={dataViewNameError !== ''}
-        error={[dataViewNameError]}
-      >
-        <EuiFieldText
-          disabled={createDataView === false || initialized === true}
-          placeholder={createDataView === true ? index : ''}
-          value={dataView}
-          onChange={onDataViewChange}
-          isInvalid={dataViewNameError !== ''}
-        />
-      </EuiFormRow>
-
-      <CombinedFieldsForm
-        mappingsString={mappingsString}
-        pipelineString={pipelineString}
-        onMappingsStringChange={onMappingsStringChange}
-        onPipelineStringChange={onPipelineStringChange}
-        combinedFields={combinedFields}
-        onCombinedFieldsChange={onCombinedFieldsChange}
-        results={results}
-        isDisabled={initialized === true}
-      /> */}
       {selectedIndexOptions.length > 0 ? (
         <>
           <EuiFlexGroup>
-            {/* <EuiFlexItem>
-          <IndexSettings
-            initialized={initialized}
-            data={indexSettingsString}
-            onChange={onIndexSettingsStringChange}
-          />
-        </EuiFlexItem> */}
-
             <EuiFlexItem>
               <Mappings
                 initialized={initialized}
@@ -355,24 +297,6 @@ export const AdvancedWithExistingIndexSettings: FC<Props> = ({
                 </>
               ) : null}
             </EuiFlexItem>
-            {/* </EuiFlexGroup>
-
-          <EuiFlexGroup> */}
-            {/* <EuiFlexItem>
-          <IndexSettings
-            initialized={initialized}
-            data={indexSettingsString}
-            onChange={onIndexSettingsStringChange}
-          />
-        </EuiFlexItem> */}
-            {/*
-        <EuiFlexItem>
-          <Mappings
-            initialized={initialized}
-            data={mappingsString}
-            onChange={onMappingsStringChange}
-          />
-        </EuiFlexItem> */}
 
             <EuiFlexItem>
               <EuiFormRow
