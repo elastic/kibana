a54f86b1e2b7fe8a3f3bbaeaa8d540866ca6223e
Nathan Reese <reese.nathan@gmail.com>
2024-06-10 14:05:12 -0600

    add test

---
+++ examples/controls_example/public/react_controls/timeslider_control/get_timeslider_control_factor.test.ts
@@ -0,0 +1,91 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { TimeRange } from "@kbn/es-query";
+import { StateComparators } from "@kbn/presentation-publishing";
+import { coreMock } from '@kbn/core/public/mocks';
+import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
+import dateMath from '@kbn/datemath';
+import { BehaviorSubject } from "rxjs";
+import { ControlGroupApi } from "../control_group/types";
+import { ControlApiRegistration } from "../types";
+import { getTimesliderControlFactory } from './get_timeslider_control_factory';
+import { TimesliderControlApi, TimesliderControlState } from "./types";
+
+describe('TimesliderControlApi', () => {
+  const uuid = 'myControl1';
+  const dashboardApi = {
+    timeRange$: new BehaviorSubject<TimeRange | undefined>(undefined)
+  };
+  const controlGroupApi = {
+    parentApi: dashboardApi
+  } as unknown as ControlGroupApi;
+  const dataStartServiceMock = dataPluginMock.createStartContract();
+  dataStartServiceMock.query.timefilter.timefilter.calculateBounds = (timeRange: TimeRange) => {
+    const now = new Date();
+    return {
+      min: dateMath.parse(timeRange.from, { forceNow: now }),
+      max: dateMath.parse(timeRange.to, { roundUp: true, forceNow: now }),
+    };
+  }
+  const factory = getTimesliderControlFactory({
+    core: coreMock.createStart(),
+    data: dataStartServiceMock,
+  });
+  function buildApiMock(api: ControlApiRegistration<TimesliderControlApi>, comparitors: StateComparators<TimesliderControlState>) {
+    return {
+      ...api,
+      uuid,
+      parentApi: controlGroupApi,
+      unsavedChanges: new BehaviorSubject<Partial<TimesliderControlState> | undefined>(undefined),
+      resetUnsavedChanges: () => {},
+      type: factory.type,
+    }
+  }
+
+  beforeEach(() => {
+    dashboardApi.timeRange$.next({
+      from: '2024-06-09T00:00:00.000Z',
+      to: '2024-06-10T00:00:00.000Z',
+    })
+  });
+
+  test('Should set timeslice to undefined when state does not provide percentage of timeRange', () => {
+    const { api } = factory.buildControl({}, buildApiMock, uuid, controlGroupApi);
+    expect(api.timeslice$.value).toBe(undefined);
+  });
+
+  test('Should set timeslice to values within time range when state provides percentage of timeRange', () => {
+    const { api } = factory.buildControl({
+      timesliceStartAsPercentageOfTimeRange: 0.25,
+      timesliceEndAsPercentageOfTimeRange: 0.5,
+    }, buildApiMock, uuid, controlGroupApi);
+
+    expect('2024-06-09T06:00:00.000Z').toEqual(new Date(api.timeslice$.value![0]).toISOString());
+    expect('2024-06-09T12:00:00.000Z').toEqual(new Date(api.timeslice$.value![1]).toISOString());
+  });
+
+  test('Should update timeslice when time range changes', async () => {
+    const { api } = factory.buildControl({
+      timesliceStartAsPercentageOfTimeRange: 0.25,
+      timesliceEndAsPercentageOfTimeRange: 0.5,
+    }, buildApiMock, uuid, controlGroupApi);
+
+    // change time range to single hour
+    dashboardApi.timeRange$.next({
+      from: '2024-06-08T00:00:00.000Z',
+      to: '2024-06-08T01:00:00.000Z',
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 0));
+
+    // update time slice to same percentage in new hour interval
+    expect('2024-06-08T00:15:00.000Z').toEqual(new Date(api.timeslice$.value![0]).toISOString());
+    expect('2024-06-08T00:30:00.000Z').toEqual(new Date(api.timeslice$.value![1]).toISOString());
+  });
+});
