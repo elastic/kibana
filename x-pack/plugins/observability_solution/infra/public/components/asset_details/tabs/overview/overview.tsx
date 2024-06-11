ac4021089298aa3632d570b6a79c95578ea31bd7
Jenny <dzheni.pavlova@elastic.co>
2024-06-11 18:46:44 +0200

    Show create rule only for docker containers

--- x-pack/plugins/observability_solution/infra/public/components/asset_details/tabs/overview/overview.tsx
+++ x-pack/plugins/observability_solution/infra/public/components/asset_details/tabs/overview/overview.tsx
@@ -23,8 +23,6 @@ import { useIntersectingState } from '../../hooks/use_intersecting_state';
 import { CpuProfilingPrompt } from './kpis/cpu_profiling_prompt';
 import { ServicesContent } from './services';
 import { MetricsContent } from './metrics/metrics';
-import { useIntegrationCheck } from '../../hooks/use_integration_check';
-import { INTEGRATIONS } from '../../constants';
 
 export const Overview = () => {
   const ref = useRef<HTMLDivElement>(null);
@@ -36,8 +34,6 @@ export const Overview = () => {
     error: fetchMetadataError,
   } = useMetadataStateContext();
   const { metrics } = useDataViewsContext();
-  const isDockerContainer = useIntegrationCheck({ dependsOn: INTEGRATIONS.docker });
-
   const isFullPageView = renderMode.mode === 'page';
 
   const state = useIntersectingState(ref, { dateRange });
@@ -64,7 +60,7 @@ export const Overview = () => {
         {fetchMetadataError && !metadataLoading ? <MetadataErrorCallout /> : metadataSummarySection}
         <SectionSeparator />
       </EuiFlexItem>
-      {asset.type === 'host' || isDockerContainer ? (
+      {asset.type === 'host' || asset.type === 'container' ? (
         <EuiFlexItem grow={false}>
           <AlertsSummaryContent
             assetId={asset.id}
