8d61b093e95049fc5fc1d099c2bfe93cce824266
Tre' Seymour <wayne.seymour@elastic.co>
2024-06-11 16:39:41 +0100

    [FTR](platform-security) update common serverless api tests to use api keys
    Contributes to: https://github.com/elastic/kibana/issues/180834

--- x-pack/test_serverless/api_integration/test_suites/common/platform_security/users.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/platform_security/users.ts
@@ -6,32 +6,41 @@
  */
 
 import expect from 'expect';
+import { RoleCredentials } from '../../../../shared/services';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getService }: FtrProviderContext) {
   const svlCommonApi = getService('svlCommonApi');
-  const supertest = getService('supertest');
+  const svlUserManager = getService('svlUserManager');
+  const supertestWithoutAuth = getService('supertestWithoutAuth');
+  let roleAuthc: RoleCredentials;
 
   describe('security/users', function () {
+    before(async () => {
+      roleAuthc = await svlUserManager.createApiKeyForRole('admin');
+    });
+    after(async () => {
+      await svlUserManager.invalidateApiKeyForRole(roleAuthc);
+    });
     describe('route access', () => {
       // ToDo: uncomment when we disable user APIs
       // describe('disabled', () => {
       // it('get', async () => {
-      //   const { body, status } = await supertest
+      //   const { body, status } = await supertestWithoutAuth
       //     .get('/internal/security/users/elastic')
-      //     .set(svlCommonApi.getInternalRequestHeader());
+      //     .set(svlCommonApi.getInternalRequestHeader()).set(roleAuthc.apiKeyHeader)
       //   svlCommonApi.assertApiNotFound(body, status);
       // });
 
       // it('get all', async () => {
-      //   const { body, status } = await supertest
+      //   const { body, status } = await supertestWithoutAuth
       //     .get('/internal/security/users')
-      //     .set(svlCommonApi.getInternalRequestHeader());
+      //     .set(svlCommonApi.getInternalRequestHeader()).set(roleAuthc.apiKeyHeader)
       //   svlCommonApi.assertApiNotFound(body, status);
       // });
 
       // it('create/update', async () => {
-      //   const { body, status } = await supertest
+      //   const { body, status } = await supertestWithoutAuth
       //     .post(`/internal/security/users/some_testuser`)
       //     .set(svlCommonApi.getInternalRequestHeader())
       //     .send({ username: 'some_testuser', password: 'testpassword', roles: [] });
@@ -39,28 +48,28 @@ export default function ({ getService }: FtrProviderContext) {
       // });
 
       // it('delete', async () => {
-      //   const { body, status } = await supertest
+      //   const { body, status } = await supertestWithoutAuth
       //     .delete(`/internal/security/users/elastic`)
-      //     .set(svlCommonApi.getInternalRequestHeader());
+      //     .set(svlCommonApi.getInternalRequestHeader()).set(roleAuthc.apiKeyHeader)
       //   svlCommonApi.assertApiNotFound(body, status);
       // });
 
       // it('disable', async () => {
-      //   const { body, status } = await supertest
+      //   const { body, status } = await supertestWithoutAuth
       //     .post(`/internal/security/users/elastic/_disable`)
-      //     .set(svlCommonApi.getInternalRequestHeader());
+      //     .set(svlCommonApi.getInternalRequestHeader()).set(roleAuthc.apiKeyHeader)
       //   svlCommonApi.assertApiNotFound(body, status);
       // });
 
       // it('enable', async () => {
-      //   const { body, status } = await supertest
+      //   const { body, status } = await supertestWithoutAuth
       //     .post(`/internal/security/users/elastic/_enable`)
-      //     .set(svlCommonApi.getInternalRequestHeader());
+      //     .set(svlCommonApi.getInternalRequestHeader()).set(roleAuthc.apiKeyHeader)
       //   svlCommonApi.assertApiNotFound(body, status);
       // });
 
       // it('set password', async () => {
-      //   const { body, status } = await supertest
+      //   const { body, status } = await supertestWithoutAuth
       //     .post(`/internal/security/users/{username}/password`)
       //     .set(svlCommonApi.getInternalRequestHeader())
       //     .send({
@@ -74,21 +83,23 @@ export default function ({ getService }: FtrProviderContext) {
       // ToDo: remove when we disable user APIs
       describe('internal', () => {
         it('get', async () => {
-          const { status } = await supertest
+          const { status } = await supertestWithoutAuth
             .get('/internal/security/users/elastic')
-            .set(svlCommonApi.getInternalRequestHeader());
+            .set(svlCommonApi.getInternalRequestHeader())
+            .set(roleAuthc.apiKeyHeader);
           expect(status).not.toBe(404);
         });
 
         it('get all', async () => {
-          const { status } = await supertest
+          const { status } = await supertestWithoutAuth
             .get('/internal/security/users')
-            .set(svlCommonApi.getInternalRequestHeader());
+            .set(svlCommonApi.getInternalRequestHeader())
+            .set(roleAuthc.apiKeyHeader);
           expect(status).not.toBe(404);
         });
 
         it('create/update', async () => {
-          const { status } = await supertest
+          const { status } = await supertestWithoutAuth
             .post(`/internal/security/users/some_testuser`)
             .set(svlCommonApi.getInternalRequestHeader())
             .send({ username: 'some_testuser', password: 'testpassword', roles: [] });
@@ -96,28 +107,31 @@ export default function ({ getService }: FtrProviderContext) {
         });
 
         it('delete', async () => {
-          const { status } = await supertest
+          const { status } = await supertestWithoutAuth
             .delete(`/internal/security/users/elastic`)
-            .set(svlCommonApi.getInternalRequestHeader());
+            .set(svlCommonApi.getInternalRequestHeader())
+            .set(roleAuthc.apiKeyHeader);
           expect(status).not.toBe(404);
         });
 
         it('disable', async () => {
-          const { status } = await supertest
+          const { status } = await supertestWithoutAuth
             .post(`/internal/security/users/elastic/_disable`)
-            .set(svlCommonApi.getInternalRequestHeader());
+            .set(svlCommonApi.getInternalRequestHeader())
+            .set(roleAuthc.apiKeyHeader);
           expect(status).not.toBe(404);
         });
 
         it('enable', async () => {
-          const { status } = await supertest
+          const { status } = await supertestWithoutAuth
             .post(`/internal/security/users/elastic/_enable`)
-            .set(svlCommonApi.getInternalRequestHeader());
+            .set(svlCommonApi.getInternalRequestHeader())
+            .set(roleAuthc.apiKeyHeader);
           expect(status).not.toBe(404);
         });
 
         it('set password', async () => {
-          const { status } = await supertest
+          const { status } = await supertestWithoutAuth
             .post(`/internal/security/users/{username}/password`)
             .set(svlCommonApi.getInternalRequestHeader())
             .send({
