13d555996618430e0f2a29d4ec5f7c77a411b054
rshen91 <rshen@elastic.co>
2024-06-11 08:46:00 -0600

    remove serverless test - copy url not available because public api

--- x-pack/test_serverless/functional/test_suites/common/discover/x_pack/reporting.ts
+++ x-pack/test_serverless/functional/test_suites/common/discover/x_pack/reporting.ts
@@ -6,7 +6,6 @@
  */
 
 import expect from '@kbn/expect';
-import { Key } from 'selenium-webdriver';
 import moment from 'moment';
 import { FtrProviderContext } from '../../../../ftr_provider_context';
 
@@ -27,7 +26,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     'share',
   ]);
   const filterBar = getService('filterBar');
-  const testSubjects = getService('testSubjects');
   const toasts = getService('toasts');
 
   const setFieldsFromSource = async (setValue: boolean) => {
@@ -115,41 +113,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         await PageObjects.discover.selectIndexPattern('ecommerce');
       });
 
-      it('generates a report with single timefilter', async () => {
-        await PageObjects.discover.clickNewSearchButton();
-        await PageObjects.timePicker.setCommonlyUsedTime('Last_24 hours');
-        await PageObjects.discover.saveSearch('single-timefilter-search');
-
-        // get shared URL value
-        const sharedURL = await browser.getCurrentUrl();
-
-        // click 'Copy POST URL'
-        await PageObjects.share.clickShareTopNavButton();
-        await PageObjects.reporting.openExportTab();
-        const copyButton = await testSubjects.find('shareReportingCopyURL');
-        const reportURL = decodeURIComponent(
-          (await copyButton.getAttribute('data-share-url')) ?? ''
-        );
-
-        // get number of filters in URLs
-        const timeFiltersNumberInReportURL =
-          reportURL.split('query:(range:(order_date:(format:strict_date_optional_time').length - 1;
-        const timeFiltersNumberInSharedURL = sharedURL.split('time:').length - 1;
-
-        expect(timeFiltersNumberInSharedURL).to.be(1);
-        expect(sharedURL.includes('time:(from:now-24h%2Fh,to:now))')).to.be(true);
-
-        expect(timeFiltersNumberInReportURL).to.be(1);
-        expect(
-          reportURL.includes(
-            `query:(range:(order_date:(format:strict_date_optional_time,gte:now-24h/h,lte:now))))`
-          )
-        ).to.be(true);
-
-        // return keyboard state
-        await browser.getActions().keyUp(Key.CONTROL).perform();
-        await browser.getActions().keyUp('v').perform();
-      });
       it('generates a report from a new search with data: default', async () => {
         await PageObjects.discover.clickNewSearchButton();
         await PageObjects.reporting.setTimepickerInEcommerceDataRange();
