4b278c371d532ae255c02a3399fcbf051a72b789
Paul Tavares <paul.tavares@elastic.co>
2024-06-11 08:15:46 -0400

    update mock

--- x-pack/plugins/security_solution/public/common/hooks/endpoint/__mocks__/use_alert_response_actions_support.ts
+++ x-pack/plugins/security_solution/public/common/hooks/endpoint/__mocks__/use_alert_response_actions_support.ts
@@ -14,6 +14,7 @@ import {
 const useAlertResponseActionsSupportMock = (): AlertResponseActionsSupport => {
   return {
     isSupported: true,
+    unsupportedReason: undefined,
     isAlert: true,
     details: {
       agentId: '123',
