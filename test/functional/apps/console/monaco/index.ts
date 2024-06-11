29e20e9beffbab50eb5db24b9f794480a60d15dc
Yulia Cech <yulia.cech@elastic.co>
2024-06-11 17:34:04 +0200

    [Console] Fix monaco tests

--- test/functional/apps/console/monaco/index.ts
+++ test/functional/apps/console/monaco/index.ts
@@ -26,7 +26,7 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
       loadTestFile(require.resolve('./_variables'));
       loadTestFile(require.resolve('./_xjson'));
       loadTestFile(require.resolve('./_misc_console_behavior'));
-      //loadTestFile(require.resolve('./_context_menu'));
+      loadTestFile(require.resolve('./_context_menu'));
       // loadTestFile(require.resolve('./_text_input'));
       // loadTestFile(require.resolve('./_settings'));
     }
