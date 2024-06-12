1a97be7fa16690d3396a3ee4f8dd096c8929885a
Christos Nasikas <christos.nasikas@elastic.co>
2024-06-12 19:21:11 +0300

    Rename package

--- packages/response-ops/feature_flag_service/jest.config.js
+++ packages/response-ops/feature_flag_service/jest.config.js
@@ -9,5 +9,5 @@
 module.exports = {
   preset: '@kbn/test/jest_node',
   rootDir: '../../..',
-  roots: ['<rootDir>/packages/response-ops/feature_flags'],
+  roots: ['<rootDir>/packages/response-ops/feature_flag_service'],
 };
