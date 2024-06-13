65c223abdbf484b0e7600aeb7f0c2e593b912ad2
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-06-13 10:09:34 +0200

    Skip endpoint tests

--- x-pack/test/security_solution_api_integration/test_suites/security_solution_endpoint/apps/integrations/index.ts
+++ x-pack/test/security_solution_api_integration/test_suites/security_solution_endpoint/apps/integrations/index.ts
@@ -16,7 +16,7 @@ import {
 export default function (providerContext: FtrProviderContext) {
   const { loadTestFile, getService, getPageObjects } = providerContext;
 
-  describe('endpoint', function () {
+  describe.skip('endpoint', function () {
     const ingestManager = getService('ingestManager');
     const log = getService('log');
     const endpointTestResources = getService('endpointTestResources');
