259013cdebe523392cd03b7483f9e168d54030fd
Samiul Monir <samiul.monir@elastic.co>
2024-06-14 10:51:42 -0400

    draft commit for tests

--- x-pack/test/functional/apps/search_playground/playground_overview.ess.ts
+++ x-pack/test/functional/apps/search_playground/playground_overview.ess.ts
@@ -5,9 +5,16 @@
  * 2.0.
  */
 
+// import expect from '@kbn/expect';
+import type OpenAI from 'openai';
 import { FtrProviderContext } from '../../ftr_provider_context';
-import { createOpenAIConnector } from './utils/create_openai_connector';
-import { MachineLearningCommonAPIProvider } from '../../services/ml/common_api';
+// import { createOpenAIConnector } from './utils/create_openai_connector';
+// import { MachineLearningCommonAPIProvider } from '../../services/ml/common_api';
+
+import {
+  createLlmProxy,
+  LlmProxy,
+} from '../../../observability_ai_assistant_api_integration/common/create_llm_proxy';
 
 const indexName = 'basic_index';
 const esArchiveIndex = 'test/api_integration/fixtures/es_archiver/index_patterns/basic_index';
@@ -15,80 +22,305 @@ const esArchiveIndex = 'test/api_integration/fixtures/es_archiver/index_patterns
 export default function (ftrContext: FtrProviderContext) {
   const { getService, getPageObjects } = ftrContext;
   const pageObjects = getPageObjects(['common', 'searchPlayground']);
-  const commonAPI = MachineLearningCommonAPIProvider(ftrContext);
+  // const commonAPI = MachineLearningCommonAPIProvider(ftrContext);
   const supertest = getService('supertest');
   const esArchiver = getService('esArchiver');
-  const createIndex = async () => await esArchiver.load(esArchiveIndex);
-  let removeOpenAIConnector: () => Promise<void>;
-  const createConnector = async () => {
-    removeOpenAIConnector = await createOpenAIConnector({
-      supertest,
-      requestHeader: commonAPI.getCommonRequestHeader(),
-    });
-  };
 
-  describe('Playground Overview', () => {
+  const log = getService('log');
+
+  const createIndex = async () => await esArchiver.load(esArchiveIndex);
+
+  async function deleteConnectors() {
+    const response = await supertest.get('/api/actions/connectors').expect(200);
+    for (const connector of response.body) {
+      if (connector.connector_type_id === '.gen-ai') {
+        await supertest
+          .delete(`/api/actions/connector/${connector.id}`)
+          .set('kbn-xsrf', 'foo')
+          .expect(204);
+      }
+    }
+  }
+
+  describe('Playground', () => {
+    let proxy: LlmProxy;
     before(async () => {
+      await deleteConnectors();
+      proxy = await createLlmProxy(log);
       await pageObjects.common.navigateToApp('enterpriseSearchApplications/playground');
     });
 
     after(async () => {
       await esArchiver.unload(esArchiveIndex);
-      await removeOpenAIConnector?.();
+      await deleteConnectors();
+      proxy.close();
+      // await removeOpenAIConnector?.();
     });
 
-    describe('start chat page', () => {
-      it('playground app is loaded', async () => {
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectPlaygroundStartChatPageComponentsToExist();
+    describe('setup Page', () => {
+      it('is loaded successfully', async () => {
         await pageObjects.searchPlayground.PlaygroundStartChatPage.expectPlaygroundHeaderComponentsToExist();
+        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectPlaygroundStartChatPageComponentsToExist();
       });
 
-      it('show no index callout', async () => {
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectNoIndexCalloutExists();
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectCreateIndexButtonToExists();
+      describe('without gen ai connectors', () => {
+        it('should display the set up connectors button', async () => {
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectAddConnectorButtonExists();
+        });
+
+        it('creates a connector successfully', async () => {
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectOpenConnectorPagePlayground();
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.createOpenAIConnector(proxy);
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectAddedConnectorCallout();
+        });
       });
 
-      it('hide no index callout when index added', async () => {
-        await createIndex();
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectSelectIndex(indexName);
+      describe('with gen ai connectors', () => {
+        it('hide gen ai panel', async () => {
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectHideGenAIPanelConnector();
+        });
       });
 
-      it('show add connector button', async () => {
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectAddConnectorButtonExists();
+      describe('without any indices', () => {
+        it('show no index callout', async () => {
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectNoIndexCalloutExists();
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectCreateIndexButtonToExists();
+        });
+
+        it('hide no index callout when index added', async () => {
+          await createIndex();
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectSelectIndex(indexName);
+        });
       });
 
-      it('click add connector button opens connector flyout', async () => {
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectOpenConnectorPagePlayground();
-      });
+      describe('with existing indices', () => {
+        it('dropdown shows up', async () => {
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectIndicesInDropdown();
+        });
 
-      it('hide gen ai panel when connector exists', async () => {
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectHideGenAIPanelConnector(
-          createConnector
-        );
-      });
-
-      it('show chat page', async () => {
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectSelectIndex(indexName);
-        await pageObjects.searchPlayground.PlaygroundStartChatPage.expectToStartChatPage();
+        it('can select index from dropdown and navigate to chat window', async () => {
+          await pageObjects.searchPlayground.PlaygroundStartChatPage.expectToSelectIndicesAndStartButtonEnabled(
+            indexName
+          );
+        });
       });
     });
 
     describe('chat page', () => {
-      it('chat works', async () => {
-        await pageObjects.searchPlayground.PlaygroundChatPage.expectChatWorks();
+      it('loads successfully', async () => {
+        await pageObjects.searchPlayground.PlaygroundChatPage.expectChatWindowLoaded();
       });
 
-      it('open view code', async () => {
-        await pageObjects.searchPlayground.PlaygroundChatPage.expectOpenViewCode();
-      });
+      describe('chat', () => {
+        it('works', async () => {
+          const titleInterceptor = proxy.intercept(
+            'title',
+            (body) =>
+              (JSON.parse(body) as OpenAI.Chat.ChatCompletionCreateParamsNonStreaming).tools?.find(
+                (fn) => fn.function.name === 'title_conversation'
+              ) !== undefined
+          );
 
-      it('show fields and code in view query', async () => {
-        await pageObjects.searchPlayground.PlaygroundChatPage.expectViewQueryHasFields();
-      });
+          const conversationInterceptor = proxy.intercept(
+            'conversation',
+            (body) =>
+              (JSON.parse(body) as OpenAI.Chat.ChatCompletionCreateParamsNonStreaming).tools?.find(
+                (fn) => fn.function.name === 'title_conversation'
+              ) === undefined
+          );
 
-      it('show edit context', async () => {
-        await pageObjects.searchPlayground.PlaygroundChatPage.expectEditContextOpens();
+          await pageObjects.searchPlayground.PlaygroundChatPage.sendQuestion();
+
+          const [conversationSimulator] = await Promise.all([
+            // titleInterceptor.waitForIntercept(),
+            conversationInterceptor.waitForIntercept(),
+          ]);
+
+          await conversationSimulator.next('My response');
+
+          await conversationSimulator.complete();
+
+          await pageObjects.searchPlayground.PlaygroundChatPage.expectChatWorks();
+        });
       });
     });
   });
 }
+
+// before(async () => {
+
+//   // await titleSimulator.next('My title');
+
+//   // await titleSimulator.complete();
+
+//   // await header.waitUntilLoadingHasFinished();
+// });
+
+// await testSubjects.setValue(ui.pages.conversations.chatInput, 'hello');
+
+// await testSubjects.pressEnter(ui.pages.conversations.chatInput);
+
+// const response = await supertest
+//   .post('/internal/search_playground/chat')
+//   .set(commonAPI.getCommonRequestHeader())
+//   .send({
+//     data: {
+//       citations: true,
+//       connector_id: 'random_id',
+//       doc_size: 3,
+//       elasticsearch_query:
+//         '{"retriever":{"rrf":{"retrievers":[{"standard":{"query":{"multi_match":{"query":"{query}","fields":["title"]}}}},{"standard":{"query":{"text_expansion":{"vector.tokens":{"model_id":".elser_model_2","model_text":"{query}"}}}}}]}}}',
+//       indices: 'basic_index',
+//       prompt: 'You are an assistant for question-answering tasks.',
+//       source_fields: '{"basic_index":["text"]}',
+//       summarization_model: 'gpt-3.5-turbo',
+//     },
+//     messages: [
+//       {
+//         role: 'human',
+//         content: 'chat',
+//       },
+//     ],
+//   });
+// console.log('responses', response);
+// expect(response.body.conversations.length).to.eql(2);
+// expect(response.body.conversations[0].conversation.title).to.be('My title');
+
+// let removeOpenAIConnector: () => Promise<void>;
+// const createConnector = async () => {
+//   removeOpenAIConnector = await createOpenAIConnector({
+//     supertest,
+//     requestHeader: commonAPI.getCommonRequestHeader(),
+//   });
+// };
+
+// describe('Playground Overview', () => {
+//   let proxy: LlmProxy;
+//   before(async () => {
+//     // await deleteConnectors();
+//     proxy = await createLlmProxy(log);
+//     await pageObjects.common.navigateToApp('enterpriseSearchApplications/playground');
+//   });
+
+//   after(async () => {
+//     await esArchiver.unload(esArchiveIndex);
+//     proxy.close();
+//     // await removeOpenAIConnector?.();
+//   });
+
+//   describe('start chat page', () => {
+//     it('playground app is loaded', async () => {
+//       await pageObjects.searchPlayground.PlaygroundStartChatPage.expectPlaygroundHeaderComponentsToExist();
+//       await pageObjects.searchPlayground.PlaygroundStartChatPage.expectPlaygroundStartChatPageComponentsToExist();
+//     });
+
+//     describe('without a connector', () => {
+//       it('should display the set up connectors button', async () => {
+//         await pageObjects.searchPlayground.PlaygroundStartChatPage.expectAddConnectorButtonExists();
+//       });
+
+//       // it('click add connector button opens connector flyout', async () => {
+//       //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectOpenConnectorPagePlayground();
+//       // });
+
+//       describe('setting up a connector', async () => {
+//         await pageObjects.searchPlayground.PlaygroundStartChatPage.expectOpenConnectorPagePlayground();
+//         await pageObjects.searchPlayground.PlaygroundStartChatPage.createOpenAIConnector(proxy);
+
+//         // await testSubjects.click('.gen-ai-card');
+//         // await testSubjects.setValue('nameInput', 'myConnector');
+//         // await testSubjects.setValue('config.apiUrl-input', `http://localhost:${proxy.getPort()}`);
+
+//         // await testSubjects.click(ui.pages.createConnectorFlyout.genAiCard);
+//         //   await testSubjects.setValue(ui.pages.createConnectorFlyout.nameInput, 'myConnector');
+//         //   await testSubjects.setValue(
+//         //     ui.pages.createConnectorFlyout.urlInput,
+//         //     `http://localhost:${proxy.getPort()}`
+//         //   );
+//         //   await testSubjects.setValue(ui.pages.createConnectorFlyout.apiKeyInput, 'myApiKey');
+
+//           // intercept the request to set up the knowledge base,
+//           // so we don't have to wait until it's fully downloaded
+//           // await interceptRequest(
+//           //   driver.driver,
+//           //   '*kb\\/setup*',
+//           //   (responseFactory) => {
+//           //     return responseFactory.fail();
+//           //   },
+//           //   async () => {
+//           //     await testSubjects.clickWhenNotDisabled(ui.pages.createConnectorFlyout.saveButton);
+//           //   }
+//           // );
+
+//           // await retry.waitFor('Connector created toast', async () => {
+//           //   const count = await toasts.getCount();
+//           //   return count > 0;
+//           // });
+
+//           // await toasts.dismissAll();
+//         });
+
+//         it('creates a connector', async () => {
+//           const response = await supertest.get('/api/actions/connectors').expect(200);
+//           console.log('response', response);
+//           expect(response.body.length).to.eql(1);
+//         });
+//       });
+//     });
+
+//     // it.skip('playground app is loaded', async () => {
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectPlaygroundHeaderComponentsToExist();
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectAddConnectorButtonExists();
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectPlaygroundStartChatPageComponentsToExist();
+
+//     // });
+
+//     // it('show no index callout', async () => {
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectNoIndexCalloutExists();
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectCreateIndexButtonToExists();
+//     // });
+
+//     // it('hide no index callout when index added', async () => {
+//     //   await createIndex();
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectSelectIndex(indexName);
+//     // });
+
+//     // it('show add connector button', async () => {
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectAddConnectorButtonExists();
+//     // });
+
+//     // it('click add connector button opens connector flyout', async () => {
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectOpenConnectorPagePlayground();
+//     // });
+
+//     // it('hide gen ai panel when connector exists', async () => {
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectHideGenAIPanelConnector(
+//     //     createConnector
+//     //   );
+//     // });
+
+//     // it('show chat page', async () => {
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectSelectIndex(indexName);
+//     //   await pageObjects.searchPlayground.PlaygroundStartChatPage.expectToStartChatPage();
+//     // });
+//   });
+
+//     describe.skip('chat page', () => {
+//       it('chat works', async () => {
+//         await pageObjects.searchPlayground.PlaygroundChatPage.expectChatWorks();
+//       });
+
+//       it('open view code', async () => {
+//         await pageObjects.searchPlayground.PlaygroundChatPage.expectOpenViewCode();
+//       });
+
+//       it('show fields and code in view query', async () => {
+//         await pageObjects.searchPlayground.PlaygroundChatPage.expectViewQueryHasFields();
+//       });
+
+//       it('show edit context', async () => {
+//         await pageObjects.searchPlayground.PlaygroundChatPage.expectEditContextOpens();
+//       });
+//     });
+//   });
+// }
--- x-pack/test/functional/page_objects/search_playground_page.ts
+++ x-pack/test/functional/page_objects/search_playground_page.ts
@@ -6,23 +6,33 @@
  */
 
 import expect from '@kbn/expect';
+import { LlmProxy } from '../../observability_ai_assistant_api_integration/common/create_llm_proxy';
 import { FtrProviderContext } from '../ftr_provider_context';
 
 export function SearchPlaygroundPageProvider({ getService }: FtrProviderContext) {
+  // const observabilityAIAssistantAPIClient = getService('observabilityAIAssistantAPIClient');
   const testSubjects = getService('testSubjects');
   const comboBox = getService('comboBox');
   const browser = getService('browser');
+
+  const driver = getService('__webdriver__');
+  const toasts = getService('toasts');
+  const retry = getService('retry');
 
   return {
     PlaygroundStartChatPage: {
       async expectPlaygroundStartChatPageComponentsToExist() {
         await testSubjects.existOrFail('startChatPage');
+        await testSubjects.existOrFail('connectToLLMChatPanel');
         await testSubjects.existOrFail('selectIndicesChatPanel');
         await testSubjects.existOrFail('startChatButton');
       },
 
       async expectPlaygroundHeaderComponentsToExist() {
         await testSubjects.existOrFail('playground-header-actions');
+        expect(await testSubjects.isEnabled('editContextActionButton')).to.be(false);
+        expect(await testSubjects.isEnabled('viewQueryActionButton')).to.be(false);
+        expect(await testSubjects.isEnabled('viewCodeActionButton')).to.be(false);
         await testSubjects.existOrFail('playground-documentation-link');
       },
 
@@ -45,6 +55,21 @@ export function SearchPlaygroundPageProvider({ getService }: FtrProviderContext)
         await comboBox.setCustom('selectIndicesComboBox', indexName);
       },
 
+      async expectIndicesInDropdown() {
+        await testSubjects.existOrFail('selectIndicesComboBox');
+      },
+
+      async expectToSelectIndicesAndStartButtonEnabled(indexName) {
+        await comboBox.setCustom('selectIndicesComboBox', indexName);
+        expect(await testSubjects.isEnabled('startChatButton')).to.be(true);
+        expect(await testSubjects.isEnabled('editContextActionButton')).to.be(true);
+        expect(await testSubjects.isEnabled('viewQueryActionButton')).to.be(true);
+        expect(await testSubjects.isEnabled('viewCodeActionButton')).to.be(true);
+
+        await testSubjects.click('startChatButton');
+        await testSubjects.existOrFail('chatPage');
+      },
+
       async expectNoIndicesFieldsWarningExists() {
         await testSubjects.existOrFail('NoIndicesFieldsMessage');
       },
@@ -58,9 +83,23 @@ export function SearchPlaygroundPageProvider({ getService }: FtrProviderContext)
         await testSubjects.existOrFail('create-connector-flyout');
       },
 
-      async expectHideGenAIPanelConnector(createConnector: () => Promise<void>) {
-        await createConnector();
-        await browser.refresh();
+      async createOpenAIConnector(proxy: LlmProxy) {
+        await testSubjects.click('.gen-ai-card');
+        await testSubjects.setValue('nameInput', 'myConnector');
+        await testSubjects.setValue('config.apiUrl-input', `http://localhost:${proxy.getPort()}`);
+        await testSubjects.setValue('secrets.apiKey-input', 'myApiKey');
+
+        await testSubjects.click('create-connector-flyout-save-btn');
+      },
+
+      async expectAddedConnectorCallout() {
+        await testSubjects.existOrFail('addedConnectorCallout');
+        await testSubjects.missingOrFail('connectToLLMChatPanel');
+      },
+
+      async expectHideGenAIPanelConnector() {
+        // await createConnector();
+        // await browser.refresh();
         await testSubjects.missingOrFail('connectToLLMChatPanel');
       },
 
@@ -71,11 +110,52 @@ export function SearchPlaygroundPageProvider({ getService }: FtrProviderContext)
       },
     },
     PlaygroundChatPage: {
-      async expectChatWorks() {
+      async expectChatWindowLoaded() {
+        expect(await testSubjects.isEnabled('editContextActionButton')).to.be(true);
+        expect(await testSubjects.isEnabled('viewQueryActionButton')).to.be(true);
+        expect(await testSubjects.isEnabled('viewCodeActionButton')).to.be(true);
+
+        expect(await testSubjects.isEnabled('regenerateActionButton')).to.be(false);
+        expect(await testSubjects.isEnabled('clearChatActionButton')).to.be(false);
+        expect(await testSubjects.isEnabled('sendQuestionButton')).to.be(false);
+
         await testSubjects.existOrFail('questionInput');
+        const model = await testSubjects.find('summarizationModelSelect');
+        const defaultModel = await model.getVisibleText();
+
+        expect(defaultModel).to.equal('OpenAI GPT-3.5 Turbo');
+        expect(defaultModel).not.to.be.empty();
+
+        expect(
+          await (await testSubjects.find('manageConnectorsLink')).getAttribute('href')
+        ).to.contain('/app/management/insightsAndAlerting/triggersActionsConnectors/connectors/');
+
+        await testSubjects.click('sourcesAccordion');
+
+        expect(await testSubjects.findAll('indicesInAccordian')).to.have.length(1);
+      },
+
+      async sendQuestion() {
         await testSubjects.setValue('questionInput', 'test question');
         await testSubjects.click('sendQuestionButton');
-        await testSubjects.existOrFail('userMessage');
+        // await testSubjects.existOrFail('userMessage');
+      },
+
+      // async expectChatWorks() {
+      //   await testSubjects.existOrFail('questionInput');
+      //   await testSubjects.setValue('questionInput', 'test question');
+      //   await testSubjects.click('sendQuestionButton');
+      //   await testSubjects.existOrFail('userMessage');
+      // },
+
+      async expectChatWorks() {
+        const userMessageElement = await testSubjects.find('userMessage');
+        const userMessage = await userMessageElement.getVisibleText();
+        expect(userMessage).to.contain('test question');
+
+        const assistantMessageElement = await testSubjects.find('assistant-message');
+        const assistantMessage = await assistantMessageElement.getVisibleText();
+        expect(assistantMessage).to.contain('My response');
       },
 
       async expectOpenViewCode() {
