67ba4256257e95dc9c873ee392e3cda22d1a4423
Tiago Costa <tiago.costa@elastic.co>
2024-06-17 15:27:26 +0100

    skip flaky suite (#186285)

--- x-pack/test/functional_with_es_ssl/apps/ml/index.ts
+++ x-pack/test/functional_with_es_ssl/apps/ml/index.ts
@@ -11,7 +11,8 @@ export default ({ loadTestFile, getService }: FtrProviderContext) => {
   const ml = getService('ml');
   const esArchiver = getService('esArchiver');
 
-  describe('ML app', function () {
+  // FLAKY: https://github.com/elastic/kibana/issues/186285
+  describe.skip('ML app', function () {
     this.tags(['mlqa', 'skipFirefox']);
 
     before(async () => {
