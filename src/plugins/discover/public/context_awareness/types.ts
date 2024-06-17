b7a0bafd716a9fc426dea4578cc78633dd95f06f
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-06-17 09:29:41 +0200

    [Discover] Update type names

--- src/plugins/discover/public/context_awareness/types.ts
+++ src/plugins/discover/public/context_awareness/types.ts
@@ -10,16 +10,16 @@ import type { CustomCellRenderer } from '@kbn/unified-data-table';
 import type { DocViewsRegistry } from '@kbn/unified-doc-viewer';
 import type { DataTableRecord } from '@kbn/discover-utils';
 
-export interface DocViewerProfileExtensions {
+export interface DocViewerExtension {
   title: string;
   docViewsRegistry: (prevRegistry: DocViewsRegistry) => DocViewsRegistry;
 }
 
-export interface DocViewerProfileParams {
+export interface DocViewerExtensionParams {
   record: DataTableRecord;
 }
 
 export interface Profile {
   getCellRenderers: () => CustomCellRenderer;
-  getDocViewer: (params: DocViewerProfileParams) => DocViewerProfileExtensions;
+  getDocViewer: (params: DocViewerExtensionParams) => DocViewerExtension;
 }
