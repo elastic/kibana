b34ba8d5cbb78ed01bcb1d87c4d1574515bea1a2
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-06-17 09:31:30 +0200

    [Discover] Revert changes in example_profiles

--- src/plugins/discover/public/context_awareness/profiles/example_profiles.tsx
+++ src/plugins/discover/public/context_awareness/profiles/example_profiles.tsx
@@ -102,19 +102,7 @@ export const logsDataSourceProfileProvider: DataSourceProfileProvider = {
 
 export const logDocumentProfileProvider: DocumentProfileProvider = {
   profileId: 'log-document-profile',
-  profile: {
-    // getDocViewer: (prev) => (params) => {
-    //   const recordId = params.record.id;
-    //   const prevValue = prev(params);
-    //   return {
-    //     title: `${prevValue.title} #${recordId}`,
-    //     docViewsRegistry: (registry) => {
-    //       registry.enableById('doc_view_logs_overview');
-    //       return prevValue.docViewsRegistry(registry);
-    //     },
-    //   };
-    // },
-  },
+  profile: {},
   resolve: (params) => {
     if (getFieldValue(params.record, 'data_stream.type') === 'logs') {
       return {
