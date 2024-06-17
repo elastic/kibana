8f10bb320ab3f0165ba2359737ef486a9320a5b2
shahzad31 <shahzad31comp@gmail.com>
2024-06-17 17:36:53 +0200

    added perf metrics

---
+++ x-pack/plugins/observability_solution/exploratory_view/public/components/shared/exploratory_view/hooks/use_ebt_telemetry.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { DefaultInspectorAdapters } from '@kbn/expressions-plugin/common';
+import { reportPerformanceMetricEvent } from '@kbn/ebt-tools';
+import { AnalyticsServiceSetup } from '@kbn/core-analytics-browser';
+
+export const useEBTTelemetry = ({
+  analytics,
+  queryName,
+}: {
+  analytics?: AnalyticsServiceSetup;
+  queryName?: string;
+}) => {
+  const reportEvent = (inspectorAdapters?: Partial<DefaultInspectorAdapters>) => {
+    if (inspectorAdapters) {
+      const { requests } = inspectorAdapters;
+      if (requests && analytics) {
+        const listReq = requests.getRequests();
+        if (listReq.length > 0) {
+          // find the highest query time
+          const duration = listReq.reduce((acc, req) => {
+            const queryTime = Number(req.stats?.queryTime.value.split('ms')?.[0]);
+            return queryTime > acc ? queryTime : acc;
+          }, 0);
+
+          if (duration) {
+            reportPerformanceMetricEvent(analytics, {
+              eventName: 'exploratory_view_query_time',
+              duration,
+              meta:
+                (queryName && {
+                  queryName,
+                }) ||
+                undefined,
+            });
+          }
+        }
+      }
+    }
+  };
+
+  return {
+    reportEvent,
+  };
+};
