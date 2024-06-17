524d29fb5e452a6aa92f9f710a49253a28ee4237
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-06-17 09:34:15 +0200

    Fix jest tests

--- packages/kbn-esql-utils/src/utils/get_esql_adhoc_dataview.ts
+++ packages/kbn-esql-utils/src/utils/get_esql_adhoc_dataview.ts
@@ -39,7 +39,7 @@ export async function getESQLAdHocDataview(
 
   // If the indexPattern is empty string means that the user used either the ROW or SHOW META / SHOW INFO commands
   // we don't want to add the @timestamp field in this case https://github.com/elastic/kibana/issues/163417
-  if (indexPattern && dataView.fields.getByName('@timestamp')?.type === 'date') {
+  if (indexPattern && dataView?.fields?.getByName?.('@timestamp')?.type === 'date') {
     dataView.timeFieldName = '@timestamp';
   }
 
