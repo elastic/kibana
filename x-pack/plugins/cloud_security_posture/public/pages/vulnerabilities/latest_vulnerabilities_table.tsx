cfd78591bf3481372f7956d43e88b96c35c30e4a
Rickyanto Ang <rickyangwyn@gmail.com>
2024-06-18 13:21:09 -0700

    putback filter for 8.14

--- x-pack/plugins/cloud_security_posture/public/pages/vulnerabilities/latest_vulnerabilities_table.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/vulnerabilities/latest_vulnerabilities_table.tsx
@@ -93,7 +93,7 @@ export const LatestVulnerabilitiesTable = ({
       getDefaultQuery,
       nonPersistedFilters,
     });
-
+  const { filters } = cloudPostureDataTable;
   return (
     <>
       {error ? (
