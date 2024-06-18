a3117a1505b634de7733f652421be10be13f8003
Ryland Herrick <ryalnd@gmail.com>
2024-06-18 14:19:42 -0500

    Revert change to form field types
    The undefined value was coming from our abstracted hook, and the hook
    now correctly handles the undefined value. This union is no longer
    needed.

--- x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/types.ts
+++ x-pack/plugins/security_solution/public/detections/pages/detection_engine/rules/types.ts
@@ -142,7 +142,7 @@ export interface DefineStepRule {
   anomalyThreshold: number;
   index: string[];
   indexPattern?: DataViewBase;
-  machineLearningJobId: string[] | undefined;
+  machineLearningJobId: string[];
   queryBar: FieldValueQueryBar;
   dataViewId?: string;
   dataViewTitle?: string;
