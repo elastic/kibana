b2962d24a1f9cd8cf735baeae781d5fdf9821277
Catherine Liu <catherine.liu@elastic.co>
2024-06-18 07:37:52 -0700

    Fix check for duplicate dashboard title tests

--- src/plugins/dashboard/public/services/dashboard_content_management/lib/check_for_duplicate_dashboard_title.test.ts
+++ src/plugins/dashboard/public/services/dashboard_content_management/lib/check_for_duplicate_dashboard_title.test.ts
@@ -7,7 +7,6 @@
  */
 
 import { ContentClient } from '@kbn/content-management-plugin/public';
-import type { DashboardCrudTypes } from '../../../../common/content_management';
 import { checkForDuplicateDashboardTitle } from './check_for_duplicate_dashboard_title';
 import { extractTitleAndCount } from '../../../dashboard_container/embeddable/api/lib/extract_title_and_count';
 
@@ -22,48 +21,6 @@ describe('checkForDuplicateDashboardTitle', () => {
 
   afterEach(() => {
     jest.clearAllMocks();
-  });
-
-  it('uses the passed searchLimit when performing name collision check', async () => {
-    const [baseDashboardName] = extractTitleAndCount(newTitle);
-
-    const pageResults = [
-      {
-        attributes: {
-          title: baseDashboardName,
-        },
-      },
-    ];
-
-    (
-      mockedContentManagementClient.search as jest.MockedFunction<ContentClient['search']>
-    ).mockImplementationOnce(() =>
-      Promise.resolve({
-        hits: pageResults,
-        pagination: {
-          total: pageResults.length,
-        },
-      })
-    );
-
-    await checkForDuplicateDashboardTitle(
-      {
-        title: newTitle,
-        lastSavedTitle: baseDashboardName,
-        copyOnSave: true,
-        isTitleDuplicateConfirmed: false,
-        searchLimit: 20,
-      },
-      { client: mockedContentManagementClient } as ContentManagementStart
-    );
-
-    expect(mockedContentManagementClient.search).toHaveBeenCalledWith(
-      expect.objectContaining({
-        query: expect.objectContaining({
-          limit: 20,
-        }),
-      })
-    );
   });
 
   it('will only search using the dashboard basename', async () => {
@@ -107,95 +64,7 @@ describe('checkForDuplicateDashboardTitle', () => {
     );
   });
 
-  it('will by default search using a cursor determined by the provided search limit and the duplication count', async () => {
-    const [baseDashboardName, duplicationCount] = extractTitleAndCount(newTitle);
-
-    const searchLimit = 5;
-
-    const pageResults = [
-      {
-        attributes: {
-          title: baseDashboardName,
-        },
-      },
-    ];
-
-    (
-      mockedContentManagementClient.search as jest.MockedFunction<ContentClient['search']>
-    ).mockImplementationOnce(() =>
-      Promise.resolve({
-        hits: pageResults,
-        pagination: {
-          total: pageResults.length,
-        },
-      })
-    );
-
-    await checkForDuplicateDashboardTitle(
-      {
-        title: newTitle,
-        lastSavedTitle: baseDashboardName,
-        copyOnSave: true,
-        isTitleDuplicateConfirmed: false,
-        searchLimit,
-      },
-      { client: mockedContentManagementClient } as ContentManagementStart
-    );
-
-    expect(mockedContentManagementClient.search).toHaveBeenCalledTimes(1);
-
-    expect(mockedContentManagementClient.search).toHaveBeenCalledWith(
-      expect.objectContaining({
-        query: expect.objectContaining({
-          cursor: String(Math.ceil(duplicationCount / searchLimit)),
-        }),
-      })
-    );
-  });
-
-  it('will by default search using a cursor of 1 when we can not determine the duplication count', async () => {
-    const [baseDashboardName] = extractTitleAndCount(newTitle);
-
-    const searchLimit = 10;
-
-    const pageResults: DashboardCrudTypes['SearchOut']['hits'] = [];
-
-    (
-      mockedContentManagementClient.search as jest.MockedFunction<ContentClient['search']>
-    ).mockImplementationOnce(() =>
-      Promise.resolve({
-        hits: pageResults,
-        pagination: {
-          total: pageResults.length,
-        },
-      })
-    );
-
-    await checkForDuplicateDashboardTitle(
-      {
-        title: baseDashboardName,
-        lastSavedTitle: '',
-        copyOnSave: true,
-        isTitleDuplicateConfirmed: false,
-        searchLimit,
-      },
-      { client: mockedContentManagementClient } as ContentManagementStart
-    );
-
-    expect(mockedContentManagementClient.search).toHaveBeenCalledTimes(1);
-
-    expect(mockedContentManagementClient.search).toHaveBeenCalledWith(
-      expect.objectContaining({
-        query: expect.objectContaining({
-          cursor: String(1),
-        }),
-      })
-    );
-  });
-
   it('invokes onTitleDuplicate with a speculative collision free value when the new title provided is a duplicate match', async () => {
-    const searchLimit = 20;
-
     const [baseDashboardName] = extractTitleAndCount(newTitle);
 
     const userTitleInput = `${baseDashboardName} (10)`;
@@ -230,7 +99,6 @@ describe('checkForDuplicateDashboardTitle', () => {
     await checkForDuplicateDashboardTitle(
       {
         title: userTitleInput,
-        searchLimit,
         lastSavedTitle: baseDashboardName,
         copyOnSave: true,
         isTitleDuplicateConfirmed: false,
