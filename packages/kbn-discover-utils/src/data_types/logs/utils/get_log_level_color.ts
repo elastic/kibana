16141b343100852375c34356d6e97c6c2e03fc47
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-06-18 17:56:33 +0200

    [Discover] Add proper colors

---
+++ packages/kbn-discover-utils/src/data_types/logs/utils/get_log_level_color.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { EuiThemeComputed } from '@elastic/eui';
+import { LogLevelCoalescedValue } from './get_log_level_coalesed_value';
+
+export const getLogLevelColor = (
+  logLevelCoalescedValue: LogLevelCoalescedValue,
+  euiTheme: EuiThemeComputed
+): string | undefined => {
+  switch (logLevelCoalescedValue) {
+    case LogLevelCoalescedValue.trace:
+      return euiTheme.colors.lightestShade;
+    case LogLevelCoalescedValue.debug:
+      return euiTheme.colors.lightShade;
+    case LogLevelCoalescedValue.info:
+      return euiTheme.colors.primary;
+    case LogLevelCoalescedValue.warning:
+      return euiTheme.colors.warning;
+    case LogLevelCoalescedValue.error:
+    case LogLevelCoalescedValue.critical:
+    case LogLevelCoalescedValue.fatal:
+      return euiTheme.colors.danger;
+    default:
+      return undefined;
+  }
+};
