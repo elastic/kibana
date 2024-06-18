dbfbfacbbdd8bb252500055aa5dbf2184e0644be
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-06-18 10:44:33 +0200

    add jest test. make use of the -parent option.

--- x-pack/plugins/observability_solution/apm/server/routes/correlations/queries/fetch_duration_field_candidates.ts
+++ x-pack/plugins/observability_solution/apm/server/routes/correlations/queries/fetch_duration_field_candidates.ts
@@ -12,7 +12,6 @@ import { rangeQuery } from '@kbn/observability-plugin/server';
 import type { CommonCorrelationsQueryParams } from '../../../../common/correlations/types';
 import {
   FIELD_PREFIX_TO_EXCLUDE_AS_CANDIDATE,
-  FIELDS_TO_ADD_AS_CANDIDATE,
   FIELDS_TO_EXCLUDE_AS_CANDIDATE,
 } from '../../../../common/correlations/constants';
 import { APMEventClient } from '../../../lib/helpers/create_es_client/create_apm_event_client';
@@ -23,13 +22,6 @@ const SUPPORTED_ES_FIELD_TYPES = [
   ES_FIELD_TYPES.BOOLEAN,
 ];
 
-export const shouldBeExcluded = (fieldName: string) => {
-  return (
-    FIELDS_TO_EXCLUDE_AS_CANDIDATE.has(fieldName) ||
-    FIELD_PREFIX_TO_EXCLUDE_AS_CANDIDATE.some((prefix) => fieldName.startsWith(prefix))
-  );
-};
-
 export interface DurationFieldCandidatesResponse {
   fieldCandidates: string[];
 }
@@ -37,11 +29,8 @@ export interface DurationFieldCandidatesResponse {
 export async function fetchDurationFieldCandidates({
   apmEventClient,
   eventType,
-  query,
   start,
   end,
-  environment,
-  kuery,
 }: CommonCorrelationsQueryParams & {
   query: estypes.QueryDslQueryContainer;
   apmEventClient: APMEventClient;
@@ -62,21 +51,12 @@ export async function fetchDurationFieldCandidates({
     types: SUPPORTED_ES_FIELD_TYPES,
   });
 
-  const finalFieldCandidates = new Set(FIELDS_TO_ADD_AS_CANDIDATE);
-
-  // There seems to be an issue with the `types` query parameter in the
-  // field caps API. It will return fields of type `object` even if that's not
-  // part of the given types. So here we need to filter out these fields manually.
-  Object.entries(respMapping.fields).forEach(([key, value]) => {
-    const fieldTypes = Object.keys(value) as ES_FIELD_TYPES[];
-    const isSupportedType = fieldTypes.some((type) => SUPPORTED_ES_FIELD_TYPES.includes(type));
-
-    if (isSupportedType) {
-      finalFieldCandidates.add(key);
-    }
-  });
-
   return {
-    fieldCandidates: [...finalFieldCandidates],
+    fieldCandidates: Object.keys(respMapping.fields).filter((fieldName: string) => {
+      return (
+        !FIELDS_TO_EXCLUDE_AS_CANDIDATE.has(fieldName) &&
+        !FIELD_PREFIX_TO_EXCLUDE_AS_CANDIDATE.some((prefix) => fieldName.startsWith(prefix))
+      );
+    }),
   };
 }
