f90c1a7e4b61923bcf735b4b14ef348531acaccb
Nikita Khristinin <nkhristinin@gmail.com>
2024-06-19 13:31:27 +0200

    fix types

--- x-pack/plugins/security_solution/public/common/lib/telemetry/events/event_log/types.ts
+++ x-pack/plugins/security_solution/public/common/lib/telemetry/events/event_log/types.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import type { RootSchema } from '@kbn/analytics-client';
+import type { RootSchema } from '@kbn/core/public';
 import type { TelemetryEventTypes } from '../../constants';
 
 export interface ReportEventLogFilterByRunTypeParams {
