946e55a1cd251cbad44061c19c0cc589d5491243
Agustina Nahir Ruidiaz <agustina.ruidiaz@elastic.co>
2024-06-19 17:28:11 +0200

    LandingLinksImageCard created and clean css styles

---
+++ x-pack/packages/security-solution/navigation/src/landing_links/landing_links_image_card.tsx
@@ -0,0 +1,123 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { EuiFlexGroup, EuiFlexItem, EuiImage, EuiPanel, EuiTitle, useEuiTheme } from '@elastic/eui';
+import React from 'react';
+import { css } from '@emotion/react';
+import { withLink } from '../links';
+import type { NavigationLink } from '../types';
+import { BetaBadge } from './beta_badge';
+import { getKibanaLinkProps } from './utils';
+
+export interface LandingLinksImageCardProps {
+  item: NavigationLink;
+  urlState?: string;
+  onLinkClick?: (id: string) => void;
+}
+
+const CARD_HEIGHT = 116;
+const CARD_HEIGHT_IMAGE = 100;
+
+const useStyles = () => {
+  const { euiTheme } = useEuiTheme();
+  return {
+    card: css`
+      // Needed to use the primary color in the title underlining on hover
+      .euiCard__title {
+        color: ${euiTheme.colors.primaryText};
+      }
+      height: ${CARD_HEIGHT}px;
+      display: flex;
+      justify-content: flex-start;
+    `,
+    cardWrapper: css`
+      height: 100%;
+    `,
+    titleContainer: css`
+      display: flex;
+      align-items: center;
+      height: ${euiTheme.size.l};
+    `,
+    title: css`
+      color: ${euiTheme.colors.primaryText};
+      font-weight: ${euiTheme.font.weight.semiBold};
+    `,
+    description: css`
+      padding-top: ${euiTheme.size.xs};
+      max-width: 550px;
+      font-size: ${euiTheme.size.m};
+    `,
+    imageContainer: css`
+      overflow: hidden;
+      height: 100%;
+      width: ${CARD_HEIGHT_IMAGE}px;
+      flex-grow: 0;
+    `,
+    cardInfoContainer: css`
+      width: min-content;
+    `,
+    image: css`
+      max-inline-size: none;
+      overflow: hidden;
+      height: ${CARD_HEIGHT_IMAGE}px;
+    `,
+  };
+};
+
+const EuiPanelWithLink = withLink(EuiPanel);
+
+export const LandingLinksImageCard: React.FC<LandingLinksImageCardProps> = React.memo(
+  function LandingLinksImageCard({ item, urlState, onLinkClick }) {
+    const styles = useStyles();
+
+    const linkProps = getKibanaLinkProps({ item, urlState, onLinkClick });
+    const { landingImage, title, description, isBeta, betaOptions } = item;
+    return (
+      <EuiFlexItem data-test-subj="LandingImageCard-item" grow={false}>
+        <EuiPanelWithLink {...linkProps} hasBorder paddingSize="xs" css={styles.card}>
+          <EuiFlexGroup
+            gutterSize="s"
+            direction="row"
+            wrap
+            justifyContent="flexStart"
+            alignItems="flexStart"
+            css={styles.cardWrapper}
+          >
+            {landingImage && (
+              <EuiPanel
+                paddingSize="none"
+                hasShadow={false}
+                hasBorder
+                borderRadius="m"
+                css={styles.imageContainer}
+              >
+                <EuiImage
+                  data-test-subj="LandingImageCard-image"
+                  role="presentation"
+                  alt={title}
+                  src={landingImage}
+                  css={styles.image}
+                />
+              </EuiPanel>
+            )}
+            <EuiFlexGroup css={styles.cardInfoContainer} gutterSize="s" direction="column">
+              <div css={styles.titleContainer}>
+                <EuiTitle size="xxxs" css={styles.title}>
+                  <h3>{title}</h3>
+                </EuiTitle>
+                {isBeta && <BetaBadge text={betaOptions?.text} />}
+              </div>
+              <span css={styles.description}>{description}</span>
+            </EuiFlexGroup>
+          </EuiFlexGroup>
+        </EuiPanelWithLink>
+      </EuiFlexItem>
+    );
+  }
+);
+
+// eslint-disable-next-line import/no-default-export
+export default LandingLinksImageCard;
--- x-pack/packages/security-solution/navigation/src/landing_links/landing_links_images_cards.tsx
+++ x-pack/packages/security-solution/navigation/src/landing_links/landing_links_images_cards.tsx
@@ -8,9 +8,7 @@ import {
   EuiAccordion,
   EuiFlexGrid,
   EuiFlexGroup,
-  EuiFlexItem,
   EuiIcon,
-  EuiImage,
   EuiPanel,
   EuiTitle,
   useEuiTheme,
@@ -19,10 +17,8 @@ import {
 import React from 'react';
 import { css } from '@emotion/react';
 import * as i18n from './translations';
-import { withLink } from '../links';
 import type { NavigationLink } from '../types';
-import { BetaBadge } from './beta_badge';
-import { getKibanaLinkProps } from './utils';
+import LandingLinksImageCard from './landing_links_image_card';
 
 export interface LandingLinksImagesProps {
   items: NavigationLink[];
@@ -30,68 +26,25 @@ export interface LandingLinksImagesProps {
   onLinkClick?: (id: string) => void;
 }
 
-const CARD_HEIGHT = 116;
-
 const useStyles = () => {
   const { euiTheme } = useEuiTheme();
   return {
-    card: css`
-      // Needed to use the primary color in the title underlining on hover
-      .euiCard__title {
-        color: ${euiTheme.colors.primaryText};
-      }
-      height: ${CARD_HEIGHT}px;
-      display: flex;
-      justify-content: flex-start;
-    `,
-    cardWrapper: css`
-      height: 100%;
-    `,
-    titleContainer: css`
-      display: flex;
-      align-items: center;
-      height: 24px;
-    `,
-    title: css`
-      color: ${euiTheme.colors.primaryText};
-      font-weight: ${euiTheme.font.weight.semiBold};
-    `,
-    description: css`
-      padding-top: ${euiTheme.size.xs};
-      max-width: 550px;
-      font-size: 12px;
-    `,
     cardsContainer: css`
       padding-top: 16px;
       gap: ${euiTheme.size.s};
     `,
-    imageContainer: css`
-      overflow: hidden;
-      height: 100%;
-      width: calc(${CARD_HEIGHT}px - 16px);
-      flex-grow: 0;
-    `,
-    cardInfoContainer: css`
-      width: min-content;
-    `,
-    image: css`
-      max-inline-size: none;
-      overflow: hidden;
-      height: calc(${CARD_HEIGHT}px - 16px);
-    `,
   };
 };
 
-const EuiPanelWithLink = withLink(EuiPanel);
-
 export const LandingLinksImageCards: React.FC<LandingLinksImagesProps> = React.memo(
   function LandingLinksImageCards({ items, urlState, onLinkClick }) {
-    const simpleAccordionId = useGeneratedHtmlId({ prefix: 'simpleAccordion' });
+    const landingLinksAccordionId = useGeneratedHtmlId({ prefix: 'landingLinksAccordion' });
     const styles = useStyles();
+
     return (
       <EuiPanel hasShadow={false} color="subdued" borderRadius="m" paddingSize="m">
         <EuiAccordion
-          id={simpleAccordionId}
+          id={landingLinksAccordionId}
           buttonContent={
             <EuiFlexGroup
               gutterSize="xs"
@@ -108,52 +61,14 @@ export const LandingLinksImageCards: React.FC<LandingLinksImagesProps> = React.m
         >
           <EuiFlexGrid css={styles.cardsContainer} columns={3}>
             {items.map((item) => {
-              const linkProps = getKibanaLinkProps({ item, urlState, onLinkClick });
-              const { id, landingImage, title, description, isBeta, betaOptions } = item;
+              const { id } = item;
               return (
-                <EuiFlexItem key={id} data-test-subj="LandingImageCard-item" grow={false}>
-                  <EuiPanelWithLink {...linkProps} hasBorder paddingSize="xs" css={styles.card}>
-                    <EuiFlexGroup
-                      gutterSize="s"
-                      direction="row"
-                      wrap
-                      justifyContent="flexStart"
-                      alignItems="flexStart"
-                      css={styles.cardWrapper}
-                    >
-                      {landingImage && (
-                        <EuiPanel
-                          paddingSize="none"
-                          hasShadow={false}
-                          hasBorder
-                          borderRadius="m"
-                          css={styles.imageContainer}
-                        >
-                          <EuiImage
-                            data-test-subj="LandingImageCard-image"
-                            role="presentation"
-                            alt={title}
-                            src={landingImage}
-                            css={styles.image}
-                          />
-                        </EuiPanel>
-                      )}
-                      <EuiFlexGroup
-                        css={styles.cardInfoContainer}
-                        gutterSize="s"
-                        direction="column"
-                      >
-                        <div css={styles.titleContainer}>
-                          <EuiTitle size="xxxs" css={styles.title}>
-                            <h3>{title}</h3>
-                          </EuiTitle>
-                          {isBeta && <BetaBadge text={betaOptions?.text} />}
-                        </div>
-                        <span css={styles.description}>{description}</span>
-                      </EuiFlexGroup>
-                    </EuiFlexGroup>
-                  </EuiPanelWithLink>
-                </EuiFlexItem>
+                <LandingLinksImageCard
+                  item={item}
+                  urlState={urlState}
+                  onLinkClick={onLinkClick}
+                  key={id}
+                />
               );
             })}
           </EuiFlexGrid>
