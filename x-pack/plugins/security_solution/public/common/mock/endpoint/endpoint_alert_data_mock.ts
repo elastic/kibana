edc4a84ef5691ab6045237a40db7a3b1bcd69ced
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2024-06-20 09:04:04 +0200

    fix mocks

--- x-pack/plugins/security_solution/public/common/mock/endpoint/endpoint_alert_data_mock.ts
+++ x-pack/plugins/security_solution/public/common/mock/endpoint/endpoint_alert_data_mock.ts
@@ -123,6 +123,20 @@ const generateEndpointAlertDetailsItemDataMock = (
       originalValue: ['windows'],
       isObjectArray: false,
     },
+    {
+      category: 'device',
+      field: 'device.id',
+      values: ['abfe4a35-d5b4-42a0-a539-bd054c791769'],
+      originalValue: ['abfe4a35-d5b4-42a0-a539-bd054c791769'],
+      isObjectArray: false,
+    },
+    {
+      category: 'host',
+      field: 'host.os.type',
+      values: ['windows'],
+      originalValue: ['windows'],
+      isObjectArray: false,
+    },
   ];
 
   setAlertDetailsItemDataOverrides(data, overrides);
@@ -185,22 +199,22 @@ const generateCrowdStrikeAlertDetailsItemDataMock = (
 
   data.push(
     {
-      category: 'crowdstrike',
+      category: 'device',
       field: RESPONSE_ACTIONS_ALERT_AGENT_ID_FIELD.crowdstrike,
       values: ['abfe4a35-d5b4-42a0-a539-bd054c791769'],
       originalValue: ['abfe4a35-d5b4-42a0-a539-bd054c791769'],
       isObjectArray: false,
     },
     {
-      category: 'crowdstrike',
-      field: 'crowdstrike.event.HostName',
+      category: 'host',
+      field: 'host.namee',
       values: ['elastic-host-win'],
       originalValue: ['windows-native'],
       isObjectArray: false,
     },
     {
-      category: 'crowdstrike',
-      field: 'crowdstrike.event.Platform',
+      category: 'host',
+      field: 'host.os.platform',
       values: ['windows'],
       originalValue: ['windows'],
       isObjectArray: false,
