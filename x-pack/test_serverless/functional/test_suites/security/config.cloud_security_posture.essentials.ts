6f71b08325168224dad5992d0730b0e417201fb8
animehart <rickyangwyn@gmail.com>
2024-06-21 11:22:37 -0700

    forgot to import

--- x-pack/test_serverless/functional/test_suites/security/config.cloud_security_posture.essentials.ts
+++ x-pack/test_serverless/functional/test_suites/security/config.cloud_security_posture.essentials.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import { CLOUD_SECURITY_PLUGIN_VERSION } from '@kbn/cloud-security-posture-plugin/common/constants';
 import { createTestConfig } from '../../config.base';
 
 export default createTestConfig({
