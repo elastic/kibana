e265ead89edf95b0e09a77b3820dcf3a4039b83e
Sergi Massaneda <sergi.massaneda@elastic.co>
2024-06-21 19:19:05 +0200

    [GenAI][Integrations] UI for the custom integration creation with AI (#186304)
    ## Summary 
    
    This ticket is the initial implementation for the UI side for the
    AI-driven custom integration creation.
    
    This PR only contains the implementation of the UI, due to the tight
    timing it will not include tests, everything will be tested manually for
    8.15 FF. We'll implement the tests later.
    
    #### Enable Feature
    
    The new integration assistant plugin is disabled by default, to enable
    it:
    
    ```
    xpack.integration_assistant.enabled: true
    ```
    
    
    #### Complete tasks
    
    - [x] New integration button on the /integrations page
    - [x] New integration "landing" page with buttons to upload zip and
    assistant
    - [x] Upload zip page to install integration
    - [x] Integration assistant:
      - [x] Connector selection step
      - [x] Integration details step
      - [x] Data stream step
      - [x] Review and install
    
    #### Follow-ups (will be implemented in separate PRs)
    
    - [ ] Add RBAC
    - [ ] Add telemetry
    - [ ] Documentation
    - [ ] Add license/productType controls
    - [ ] Add links to the create integration page
    - [ ] Improve package name retrieval:
    https://github.com/elastic/kibana/issues/185932
    - [ ] Add time estimation on the generation stage
    - [ ] Add support for multi-valuated "input type"
    - [ ] Enable Langsmith tracing using AI assistant settings
    
    #### Demo
    
    
    https://github.com/elastic/kibana/assets/17747913/b04c21c6-09cf-49bb-be8f-bf4b9d3feb8e
    
    
    ## Files by Code Owner
    
    ### elastic/docs
    
    * packages/kbn-doc-links/src/get_doc_links.ts
    * packages/kbn-doc-links/src/types.ts
    
    ### elastic/fleet
    
    * x-pack/plugins/fleet/kibana.jsonc
    *
    x-pack/plugins/fleet/public/applications/integrations/hooks/use_breadcrumbs.tsx
    *
    x-pack/plugins/fleet/public/applications/integrations/layouts/default.tsx
    *
    x-pack/plugins/fleet/public/applications/integrations/sections/epm/index.tsx
    *
    x-pack/plugins/fleet/public/applications/integrations/sections/epm/screens/create/index.tsx
    * x-pack/plugins/fleet/public/components/header.tsx
    * x-pack/plugins/fleet/public/constants/page_paths.ts
    * x-pack/plugins/fleet/public/plugin.ts
    * x-pack/plugins/fleet/tsconfig.json
    
    ### elastic/kibana-core
    
    * x-pack/plugins/fleet/kibana.jsonc
    * x-pack/plugins/integration_assistant/kibana.jsonc
    
    ### elastic/kibana-operations
    
    * packages/kbn-optimizer/limits.yml
    
    ### elastic/security-solution
    
    * x-pack/plugins/integration_assistant/**/*
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/review_step/review_step.tsx
@@ -0,0 +1,147 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import {
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiFlyout,
+  EuiFlyoutBody,
+  EuiFlyoutFooter,
+  EuiFlyoutHeader,
+  EuiLoadingSpinner,
+  EuiPanel,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
+import { css } from '@emotion/react';
+import { CodeEditor } from '@kbn/code-editor';
+import { XJsonLang } from '@kbn/monaco';
+import React, { useCallback, useState } from 'react';
+import type { Pipeline } from '../../../../../../common';
+import type { State } from '../../state';
+import { FieldsTable } from './fields_table';
+import { StepContentWrapper } from '../step_content_wrapper';
+import { useCheckPipeline } from './use_check_pipeline';
+import * as i18n from './translations';
+
+const flyoutBodyCss = css`
+  height: 100%;
+  .euiFlyoutBody__overflowContent {
+    height: 100%;
+    padding: 0;
+  }
+`;
+
+interface ReviewStepProps {
+  integrationSettings: State['integrationSettings'];
+  connectorId: State['connectorId'];
+  result: State['result'];
+  isGenerating: State['isGenerating'];
+}
+export const ReviewStep = React.memo<ReviewStepProps>(
+  ({ integrationSettings, connectorId, isGenerating, result }) => {
+    const [customPipeline, setCustomPipeline] = useState<Pipeline>();
+    const { error: checkPipelineError } = useCheckPipeline({
+      customPipeline,
+      integrationSettings,
+      connectorId,
+    });
+
+    const [isPipelineEditionVisible, setIsPipelineEditionVisible] = useState(false);
+    const [updatedPipeline, setUpdatedPipeline] = useState<string>();
+
+    const changeCustomPipeline = useCallback((value: string) => {
+      setUpdatedPipeline(value);
+    }, []);
+
+    const saveCustomPipeline = useCallback(() => {
+      if (updatedPipeline) {
+        try {
+          const pipeline = JSON.parse(updatedPipeline);
+          setCustomPipeline(pipeline);
+        } catch (_) {
+          return; // Syntax errors are already displayed in the code editor
+        }
+      }
+      setIsPipelineEditionVisible(false);
+    }, [updatedPipeline]);
+
+    return (
+      <StepContentWrapper
+        title={i18n.TITLE}
+        subtitle={i18n.DESCRIPTION}
+        right={
+          <EuiButton onClick={() => setIsPipelineEditionVisible(true)}>
+            {i18n.EDIT_PIPELINE_BUTTON}
+          </EuiButton>
+        }
+      >
+        <EuiPanel hasShadow={false} hasBorder>
+          {isGenerating ? (
+            <EuiLoadingSpinner size="l" />
+          ) : (
+            <>
+              {checkPipelineError && (
+                <EuiText color="danger" size="s">
+                  {checkPipelineError}
+                </EuiText>
+              )}
+              <FieldsTable documents={result?.docs} />
+            </>
+          )}
+          {isPipelineEditionVisible && (
+            <EuiFlyout onClose={() => setIsPipelineEditionVisible(false)}>
+              <EuiFlyoutHeader hasBorder>
+                <EuiTitle size="s">
+                  <h2>{i18n.INGEST_PIPELINE_TITLE}</h2>
+                </EuiTitle>
+              </EuiFlyoutHeader>
+              <EuiFlyoutBody css={flyoutBodyCss}>
+                <EuiFlexGroup
+                  direction="column"
+                  gutterSize="s"
+                  wrap={false}
+                  responsive={false}
+                  css={{ height: '100%' }}
+                >
+                  <EuiFlexItem grow={true} data-test-subj="inspectorRequestCodeViewerContainer">
+                    <CodeEditor
+                      languageId={XJsonLang.ID}
+                      value={JSON.stringify(result?.pipeline, null, 2)}
+                      onChange={changeCustomPipeline}
+                      width="100%"
+                      height="100%"
+                      options={{
+                        fontSize: 12,
+                        minimap: { enabled: true },
+                        folding: true,
+                        scrollBeyondLastLine: false,
+                        wordWrap: 'on',
+                        wrappingIndent: 'indent',
+                        automaticLayout: true,
+                      }}
+                    />
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlyoutBody>
+              <EuiFlyoutFooter>
+                <EuiFlexGroup direction="row" gutterSize="none" justifyContent="flexEnd">
+                  <EuiFlexItem grow={false}>
+                    <EuiButton fill onClick={saveCustomPipeline}>
+                      {i18n.SAVE_BUTTON}
+                    </EuiButton>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlyoutFooter>
+            </EuiFlyout>
+          )}
+        </EuiPanel>
+      </StepContentWrapper>
+    );
+  }
+);
+ReviewStep.displayName = 'ReviewStep';
