81821f48ce8ff1ffe3b58f608a6ebea6a89c90e6
Hannah Mudge <hannah.wright@elastic.co>
2024-06-21 10:49:06 -0600

    Fix styling + error component

--- examples/controls_example/public/react_controls/control_error_component.tsx
+++ examples/controls_example/public/react_controls/control_error_component.tsx
@@ -24,13 +24,15 @@ export const ControlError = ({ error }: ControlErrorProps) => {
 
   const popoverButton = (
     <EuiButtonEmpty
+      flush="left"
       color="danger"
       iconSize="m"
       iconType="error"
       data-test-subj="control-frame-error"
       onClick={() => setPopoverOpen((open) => !open)}
-      className={'errorEmbeddableCompact__button'}
+      className="errorEmbeddableCompact__button controlErrorButton"
       textProps={{ className: 'errorEmbeddableCompact__text' }}
+      contentProps={{ className: 'controlErrorButton--content' }}
     >
       <FormattedMessage
         id="controls.frame.error.message"
@@ -44,7 +46,7 @@ export const ControlError = ({ error }: ControlErrorProps) => {
       <EuiPopover
         button={popoverButton}
         isOpen={isPopoverOpen}
-        className="errorEmbeddableCompact__popover"
+        className="controlPanel errorEmbeddableCompact__popover"
         closePopover={() => setPopoverOpen(false)}
       >
         <Markdown data-test-subj="errorMessageMarkdown" readOnly>
