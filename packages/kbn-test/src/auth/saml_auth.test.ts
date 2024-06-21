cbcad13100d80520bb4de1dde7b5a5ce3045087a
Tre' Seymour <wayne.seymour@elastic.co>
2024-06-21 15:55:01 +0100

    Mostly a nit, because at first glance, it read like an infinite backoff algo.

--- packages/kbn-test/src/auth/saml_auth.test.ts
+++ packages/kbn-test/src/auth/saml_auth.test.ts
@@ -41,6 +41,7 @@ const mockGetOnce = (mockedUrl: string, response: any) => {
 };
 
 describe('saml_auth', () => {
+  const attemptsCount = 3;
   describe('createCloudSession', () => {
     afterEach(() => {
       axiosRequestMock.mockClear();
@@ -58,12 +59,12 @@ describe('saml_auth', () => {
       expect(axiosRequestMock).toBeCalledTimes(1);
     });
 
-    test('retries until response has the token value', async () => {
+    test(`retries until response has the token value, up to ${attemptsCount} attempts`, async () => {
       let callCount = 0;
       axiosRequestMock.mockImplementation((config: AxiosRequestConfig) => {
         if (config.url?.endsWith('/api/v1/saas/auth/_login')) {
           callCount += 1;
-          if (callCount !== 3) {
+          if (callCount !== attemptsCount) {
             return Promise.resolve({ data: { message: 'no token' }, status: 503 });
           } else {
             return Promise.resolve({
@@ -83,7 +84,7 @@ describe('saml_auth', () => {
           log,
         },
         {
-          attemptsCount: 3,
+          attemptsCount,
           attemptDelay: 100,
         }
       );
