df7aa5a93791d21f4bc967ee47d91df2ee7ba276
Brad White <brad.white@elastic.co>
2024-06-21 17:11:24 -0600

    set default as bash

--- .devcontainer/scripts/setup_shell.sh
+++ .devcontainer/scripts/setup_shell.sh
@@ -6,10 +6,10 @@ if [ -f "$ENV_PATH" ]; then
   source "$ENV_PATH"
 
   if [ -n "$SHELL" ] && [ -x "$SHELL" ]; then
-    echo "Switching to preferred shell: $SHELL"
+    echo "Switching to shell: $SHELL"
     sudo chsh -s "$SHELL" vscode
   else
-    echo "Preferred shell is not set or not executable. Continuing with the current shell."
+    echo "Shell is not set or not executable, using bash."
   fi
 else
   echo ".env file not found."
