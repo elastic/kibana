544f167a5756f90791c5005c6284b843abf93f38
criamico <mariacristina.amico@elastic.co>
2024-06-24 18:00:51 +0200

    Add link in flyout

--- x-pack/plugins/fleet/public/components/enrollment_instructions/unprivileged_info.tsx
+++ x-pack/plugins/fleet/public/components/enrollment_instructions/unprivileged_info.tsx
@@ -6,20 +6,31 @@
  */
 
 import React from 'react';
-import { EuiCode, EuiSpacer, EuiText } from '@elastic/eui';
+import { EuiCode, EuiLink, EuiSpacer, EuiText } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 
+import { useStartServices } from '../../hooks';
+
 export const UnprivilegedInfo: React.FC = () => {
+  const { docLinks } = useStartServices();
   return (
     <>
       <EuiText>
         <p>
           <FormattedMessage
             id="xpack.fleet.agentEnrollmentFlyout.unprivilegedMessage"
-            defaultMessage="To install Elastic Agent without root privileges, add the {flag} flag to the {command} install command below."
+            defaultMessage="To install Elastic Agent without root privileges, add the {flag} flag to the {command} install command below. For more information, see the {guideLink}"
             values={{
               flag: <EuiCode>--unprivileged</EuiCode>,
               command: <EuiCode>sudo ./elastic-agent</EuiCode>,
+              guideLink: (
+                <EuiLink href={docLinks.links.fleet.unprivilegedMode} target="_blank" external>
+                  <FormattedMessage
+                    id="xpack.fleet.agentEnrollmentFlyout.unprivilegedMessage.guideLink"
+                    defaultMessage="Fleet and Elastic Agent Guide"
+                  />
+                </EuiLink>
+              ),
             }}
           />
         </p>
