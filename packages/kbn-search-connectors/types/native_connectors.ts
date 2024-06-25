403371e761dd52bdc43b8c484528794ae6a148d7
moxarth-elastic <96762084+moxarth-elastic@users.noreply.github.com>
2024-06-25 18:28:37 +0530

    Update packages/kbn-search-connectors/types/native_connectors.ts
    Co-authored-by: Artem Shelkovnikov <lavatroublebubble@gmail.com>

--- packages/kbn-search-connectors/types/native_connectors.ts
+++ packages/kbn-search-connectors/types/native_connectors.ts
@@ -1868,7 +1868,7 @@ export const NATIVE_CONNECTOR_DEFINITIONS: Record<string, NativeConnector | unde
         placeholder: 'me@example.com',
         required: true,
         sensitive: false,
-        tooltip: i18n.translate('searchConnectors.nativeConnectors.jira.cloudServiceTooltip', {
+        tooltip: i18n.translate('searchConnectors.nativeConnectors.jira.cloudServiceAccountTooltip', {
           defaultMessage: 'Email address associated with Jira Cloud account. E.g. jane.doe@gmail.com',
         }),
         type: FieldType.STRING,
