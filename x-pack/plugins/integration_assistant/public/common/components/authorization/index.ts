71f8a47b3b6821ba43b24a7a035f1a610b48cfb6
Sergi Massaneda <sergi.massaneda@elastic.co>
2024-06-25 11:50:28 +0200

    [GenAI][Integrations] RBAC for the Integration assistant (#186481)
    ## Summary
    
    Adds Role Based Access Control (RBAC) to the new Integration assistant. 
    
    ### Privileges required
    
    Different parts of the UI require different privileges in order to work
    
    #### Create integration main page
    
    The essential functionality of the workflow is to create custom
    integrations, either via assistant or using the zip file upload.
    To create integrations users need:
    
    - "Fleet" _All_
    - "Integrations" _All_
    
    When some of these privileges are missing, the "Create Integration" main
    page will only display an informative callout. The `/create/upload` and
    `/create/assistant` routes are disabled accordingly.
    
    
    ![missing_all](https://github.com/elastic/kibana/assets/17747913/9a2eaca6-6509-4e27-865f-44b1793c66c9)
    
    ---
    
    #### Integration assistant page
    
    The integrations assistant workflow needs connectors privileges, along
    with the basic integration/fleet privileges. To use the integration
    assistant users need:
    
    - "Connectors" _Read_
    
    So, some users may be allowed to create an integration by uploading a
    zip, but not to use the integrations assistant. In this scenario, the
    button to go to the integration assistant is disabled with a tooltip.
    The `/create/assistant` route is disabled accordingly.
    
    
    ![missing_connectos_read](https://github.com/elastic/kibana/assets/17747913/d3401d86-0033-426f-91aa-3448fee38dce)
    
    ---
    
    #### Create connector
    
    Users may also need to create genAI connectors to use the Integrations
    assistant. The privilege required is:
    
    - "Connectors" _All_
    
    When this privilege is missing, in the connectors step of the assistant,
    we may have two different scenarios:
    
    1. With connectors available:
    
    The "Create new connector" button is disabled with a tooltip, but users
    are allowed to use any existing connector.
    
    
    ![connector_available](https://github.com/elastic/kibana/assets/17747913/2179f20b-d82d-4d1f-a1ae-75fb6df16f1f)
    
    2. Without connectors available:
    
    The missing privileges callout is displayed:
    
    
    ![no_connector_available](https://github.com/elastic/kibana/assets/17747913/1aa594de-34c1-4b97-baf5-3dbe2d5611a5)
    
    ---------
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/integration_assistant/public/common/components/authorization/index.ts
+++ x-pack/plugins/integration_assistant/public/common/components/authorization/index.ts
@@ -4,15 +4,5 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-export enum Page {
-  landing = 'landing',
-  upload = 'upload',
-  assistant = 'assistant',
-}
-
-export const PagePath = {
-  [Page.landing]: '/create',
-  [Page.upload]: '/create/upload',
-  [Page.assistant]: '/create/assistant',
-};
+export { AuthorizationWrapper } from './authorization_wrapper';
+export { MissingPrivilegesTooltip } from './missing_privileges_tooltip';
