31274c488520764a370ac6a37af3e7721cb902c2
Alexey Antonov <alexwizp@gmail.com>
2024-06-25 15:03:06 +0300

    update PR

--- x-pack/plugins/triggers_actions_ui/public/application/sections/alert_summary_widget/components/alert_counts.tsx
+++ x-pack/plugins/triggers_actions_ui/public/application/sections/alert_summary_widget/components/alert_counts.tsx
@@ -5,11 +5,17 @@
  * 2.0.
  */
 
-import React, { MouseEvent, useMemo, type ReactNode } from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiLink } from '@elastic/eui';
+import React, { type MouseEvent } from 'react';
+import { EuiFlexGroup, useEuiTheme } from '@elastic/eui';
 import { ALERT_STATUS_ACTIVE, AlertStatus } from '@kbn/rule-data-utils';
-import { ActiveAlertCounts } from './active_alert_counts';
-import { AllAlertCounts } from './all_alert_counts';
+import {
+  ACTIVE_ALERT_COUNT_DATA_TEST_SUBJ,
+  ACTIVE_NOW_LABEL,
+  ALERTS_LABEL,
+  ALL_ALERT_COLOR,
+  TOTAL_ALERT_COUNT_DATA_TEST_SUBJ,
+} from './constants';
+import { AlertItem } from './alert_item';
 
 interface Props {
   activeAlertCount: number;
@@ -20,55 +26,27 @@ interface Props {
   ) => void;
 }
 
-/** @internal **/
-const AlertItem = ({
-  children,
-  onClick,
-  'data-test-subj': dataTestSubj,
-}: {
-  'data-test-subj'?: string;
-  children?: ReactNode;
-  onClick: ((event: React.MouseEvent<HTMLAnchorElement>) => void) | undefined;
-}) => (
-  <EuiFlexItem
-    style={{ minWidth: 50, wordWrap: 'break-word' }}
-    grow={false}
-    data-test-subj={dataTestSubj}
-  >
-    {onClick ? <EuiLink onClick={onClick}>{children}</EuiLink> : children}
-  </EuiFlexItem>
-);
-
 export const AlertCounts = ({ activeAlertCount, recoveredAlertCount, handleClick }: Props) => {
-  const onAllClick = useMemo(
-    () =>
-      handleClick
-        ? (event: React.MouseEvent<HTMLAnchorElement>) => {
-            handleClick(event);
-          }
-        : undefined,
-    [handleClick]
-  );
-
-  const onActiveClick = useMemo(
-    () =>
-      handleClick
-        ? (event: React.MouseEvent<HTMLAnchorElement>) => {
-            handleClick(event, ALERT_STATUS_ACTIVE);
-          }
-        : undefined,
-    [handleClick]
-  );
+  const { euiTheme } = useEuiTheme();
 
   return (
     <EuiFlexGroup gutterSize="l" responsive={false}>
-      <AlertItem onClick={onAllClick} data-test-subj="allAlerts">
-        <AllAlertCounts count={activeAlertCount + recoveredAlertCount} />
-      </AlertItem>
-
-      <AlertItem onClick={onActiveClick} data-test-subj="activeAlerts">
-        <ActiveAlertCounts activeAlertCount={activeAlertCount} />
-      </AlertItem>
+      <AlertItem
+        label={ALERTS_LABEL}
+        count={activeAlertCount + recoveredAlertCount}
+        color={ALL_ALERT_COLOR}
+        data-test-subj={TOTAL_ALERT_COUNT_DATA_TEST_SUBJ}
+        handleClick={handleClick}
+      />
+      <AlertItem
+        label={ACTIVE_NOW_LABEL}
+        count={activeAlertCount}
+        color={activeAlertCount > 0 ? euiTheme.colors.dangerText : euiTheme.colors.successText}
+        alertType={ALERT_STATUS_ACTIVE}
+        handleClick={handleClick}
+        showWarningIcon={true}
+        data-test-subj={ACTIVE_ALERT_COUNT_DATA_TEST_SUBJ}
+      />
     </EuiFlexGroup>
   );
 };
