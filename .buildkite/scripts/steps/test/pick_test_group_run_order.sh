f23936e4782c8d9edd3fe0826abd5e0df988175f
Brad White <brad.white@elastic.co>
2024-06-24 18:14:38 -0600

    debug

--- .buildkite/scripts/steps/test/pick_test_group_run_order.sh
+++ .buildkite/scripts/steps/test/pick_test_group_run_order.sh
@@ -5,4 +5,8 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 echo '--- Pick Test Group Run Order'
+
+echo "pwd: $(pwd)"
+echo "dirname: $(dirname "${0}")"
+
 ts-node "$(dirname "${0}")/pick_test_group_run_order.ts"
