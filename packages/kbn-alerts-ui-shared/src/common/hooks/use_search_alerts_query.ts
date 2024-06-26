71c21433d66bce04485e533eedfef3cda4f27d38
Umberto Pepato <umberto.pepato@elastic.co>
2024-06-26 14:28:02 +0200

    Migrate alerts data fetching to TanStack Query

---
+++ packages/kbn-alerts-ui-shared/src/common/hooks/use_search_alerts_query.ts
@@ -0,0 +1,189 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { useQuery } from '@tanstack/react-query';
+import type { ValidFeatureId } from '@kbn/rule-data-utils';
+import type {
+  MappingRuntimeFields,
+  QueryDslFieldAndFormat,
+  QueryDslQueryContainer,
+  SortCombinations,
+} from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
+import type { DataPublicPluginStart } from '@kbn/data-plugin/public';
+import type {
+  RuleRegistrySearchRequest,
+  RuleRegistrySearchResponse,
+} from '@kbn/rule-registry-plugin/common';
+import { catchError, filter, lastValueFrom, map, of } from 'rxjs';
+import { set } from '@kbn/safer-lodash-set';
+import { DEFAULT_ALERTS_PAGE_SIZE } from '../constants';
+import { Alert, EsQuerySnapshot, LegacyField } from '../types';
+
+export interface UseSearchAlertsQueryProps {
+  // Dependencies
+  /**
+   * Kibana data plugin, used to perform the query
+   */
+  data: DataPublicPluginStart;
+  // Parameters
+  /**
+   * Array of feature ids used for authorization and area-based filtering
+   */
+  featureIds: ValidFeatureId[];
+  /**
+   * ES query to perform on the affected alert indices
+   */
+  query?: Pick<QueryDslQueryContainer, 'bool' | 'ids'>;
+  /**
+   * The alert document fields to include in the response
+   */
+  fields: QueryDslFieldAndFormat[];
+  /**
+   * Sort combinations to apply to the query
+   */
+  sort?: SortCombinations[];
+  /**
+   * Runtime mappings to apply to the query
+   */
+  runtimeMappings?: MappingRuntimeFields;
+  /**
+   * The page index to fetch
+   */
+  pageIndex?: number;
+  /**
+   * The page size to fetch
+   */
+  pageSize?: number;
+  /**
+   * Whether the query is enabled
+   */
+  enabled?: boolean;
+}
+
+export interface AlertsQueryData {
+  alerts: Alert[];
+  oldAlertsData: LegacyField[][];
+  ecsAlertsData: unknown[];
+  total: number;
+  querySnapshot?: EsQuerySnapshot;
+}
+
+/**
+ * Query alerts
+ */
+export const useSearchAlertsQuery = (props: UseSearchAlertsQueryProps) => {
+  const { data, ...params } = props;
+  const {
+    featureIds,
+    fields,
+    query = {
+      bool: {},
+    },
+    sort = [
+      {
+        '@timestamp': 'desc',
+      },
+    ],
+    runtimeMappings,
+    pageIndex = 0,
+    pageSize = DEFAULT_ALERTS_PAGE_SIZE,
+    enabled = true,
+  } = params;
+  return useQuery(
+    ['alertsUIShared', 'searchAlerts', JSON.stringify(params)],
+    ({ signal }): Promise<AlertsQueryData> =>
+      lastValueFrom(
+        data.search
+          .search<RuleRegistrySearchRequest, RuleRegistrySearchResponse>(
+            {
+              featureIds,
+              fields,
+              query,
+              pagination: { pageIndex, pageSize },
+              sort,
+              runtimeMappings,
+            },
+            {
+              strategy: 'privateRuleRegistryAlertsSearchStrategy',
+              abortSignal: signal,
+            }
+          )
+          .pipe(
+            filter((response) => !response.isRunning),
+            map((response) => {
+              const { rawResponse } = response;
+              let total = 0;
+              if (rawResponse.hits.total) {
+                if (typeof rawResponse.hits.total === 'number') {
+                  total = rawResponse.hits.total;
+                } else if (typeof rawResponse.hits.total === 'object') {
+                  total = rawResponse.hits.total?.value ?? 0;
+                }
+              }
+              const alerts = rawResponse.hits.hits.reduce<Alert[]>((acc, hit) => {
+                if (hit.fields) {
+                  acc.push({
+                    ...hit.fields,
+                    _id: hit._id,
+                    _index: hit._index,
+                  } as Alert);
+                }
+                return acc;
+              }, []);
+
+              const { oldAlertsData, ecsAlertsData } = alerts.reduce<{
+                oldAlertsData: LegacyField[][];
+                ecsAlertsData: unknown[];
+              }>(
+                (acc, alert) => {
+                  const itemOldData = Object.entries(alert).reduce<
+                    Array<{ field: string; value: string[] }>
+                  >((oldData, [key, value]) => {
+                    oldData.push({ field: key, value: value as string[] });
+                    return oldData;
+                  }, []);
+                  const ecsData = Object.entries(alert).reduce((ecs, [key, value]) => {
+                    set(ecs, key, value ?? []);
+                    return ecs;
+                  }, {});
+                  acc.oldAlertsData.push(itemOldData);
+                  acc.ecsAlertsData.push(ecsData);
+                  return acc;
+                },
+                { oldAlertsData: [], ecsAlertsData: [] }
+              );
+
+              return {
+                alerts,
+                oldAlertsData,
+                ecsAlertsData,
+                total,
+                querySnapshot: {
+                  request: response?.inspect?.dsl ?? [],
+                  response: [JSON.stringify(rawResponse)] ?? [],
+                },
+              };
+            }),
+            catchError((error) => {
+              data.search.showError(error);
+              return of({
+                alerts: [],
+                oldAlertsData: [],
+                ecsAlertsData: [],
+                total: 0,
+              });
+            })
+          )
+      ),
+    {
+      // To avoid flash of loading state with pagination, see https://tanstack.com/query/latest/docs/framework/react/guides/paginated-queries#better-paginated-queries-with-placeholderdata
+      keepPreviousData: true,
+      enabled,
+    }
+  );
+};
