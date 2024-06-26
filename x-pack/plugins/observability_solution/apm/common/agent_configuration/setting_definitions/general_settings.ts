95bb5a2402158ac360f202159459555834ee4b6d
Steve Gordon <sgordon@hotmail.co.uk>
2024-06-26 14:04:27 +0100

    Update general_settings.ts
    Fix comma

--- x-pack/plugins/observability_solution/apm/common/agent_configuration/setting_definitions/general_settings.ts
+++ x-pack/plugins/observability_solution/apm/common/agent_configuration/setting_definitions/general_settings.ts
@@ -565,6 +565,6 @@ export const generalSettings: RawSettingDefinition[] = [
         'such as `GET /users/42/cart` and `GET /users/73/cart` into a single transaction name `GET /users/*/cart`,\n' +
         'hence reducing the transaction name cardinality.',
     }),
-    includeAgents: ['java', 'dotnet']
+    includeAgents: ['java', 'dotnet'],
   },
 ];
