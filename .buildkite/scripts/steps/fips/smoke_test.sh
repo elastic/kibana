58b142a748b5e4e60200641b537725dc681808ba
Brad White <brad.white@elastic.co>
2024-06-25 20:54:54 -0600

    cleanup

--- .buildkite/scripts/steps/fips/smoke_test.sh
+++ .buildkite/scripts/steps/fips/smoke_test.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+# Limit the FTR configs for now to avoid running all the tests. Once we're
+# ready to utilize the full FTR suite in FIPS mode, we can remove this file and
+# call pick_test_group_run_order.sh directly in .buildkite/pipelines/fips.yml.
 configs=(
   "x-pack/test/reporting_functional/reporting_and_security.config.ts"
   "x-pack/test/saved_object_api_integration/security_and_spaces/config_trial.ts"
--- .buildkite/scripts/steps/test/pick_test_group_run_order.sh
+++ .buildkite/scripts/steps/test/pick_test_group_run_order.sh
@@ -5,5 +5,4 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 echo '--- Pick Test Group Run Order'
-
 ts-node "$(dirname "${0}")/pick_test_group_run_order.ts"
