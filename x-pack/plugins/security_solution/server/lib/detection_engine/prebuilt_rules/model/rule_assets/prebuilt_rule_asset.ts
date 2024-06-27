a92c5766ac0e3b044aec44ec2c638a370c610bad
jpdjere <jpdjeredjian@gmail.com>
2024-06-27 14:39:28 +0200

    Fixes PrebuiltRuleAsset test

--- x-pack/plugins/security_solution/server/lib/detection_engine/prebuilt_rules/model/rule_assets/prebuilt_rule_asset.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/prebuilt_rules/model/rule_assets/prebuilt_rule_asset.ts
@@ -84,8 +84,8 @@ export const RuleAssetTypeSpecificCreateProps = z.discriminatedUnion('type', [
   SavedQueryRuleAssetFields,
   ThresholdRuleAssetFields,
   ThreatMatchRuleAssetFields,
-  NewTermsRuleAssetFields,
   MachineLearningRuleAssetFields,
+  NewTermsRuleAssetFields,
   EsqlRuleAssetFields,
 ]);
 
