6cc372ac94e3e90be9690ae96e56bcb4247286cd
Abdul Zahid <awahab07@yahoo.com>
2024-06-27 10:38:43 +0200

    Change serverless card message to "Find and manage quality issues in your log data.".

--- packages/kbn-management/cards_navigation/src/consts.tsx
+++ packages/kbn-management/cards_navigation/src/consts.tsx
@@ -66,7 +66,7 @@ export const appDefinitions: Record<AppId, AppDefinition> = {
   [AppIds.DATA_QUALITY]: {
     category: appCategories.DATA,
     description: i18n.translate('management.landing.withCardNavigation.dataQualityDescription', {
-      defaultMessage: 'Manage the quality of your logs data.',
+      defaultMessage: 'Find and manage quality issues in your log data.',
     }),
     icon: 'documents',
   },
