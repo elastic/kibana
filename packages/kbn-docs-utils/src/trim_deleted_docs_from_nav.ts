c80eaf02b0a040db580670595664a935cf29d261
Clint Andrew Hall <clint@clintandrewhall.com>
2024-06-27 22:07:34 -0400

    [docs] Plugin optimization, case studies

--- packages/kbn-docs-utils/src/trim_deleted_docs_from_nav.ts
+++ packages/kbn-docs-utils/src/trim_deleted_docs_from_nav.ts
@@ -30,7 +30,7 @@ export async function trimDeletedDocsFromNav(
     log.info('no deleted doc files detected');
   }
 
-  const navPath = Path.resolve(REPO_ROOT, 'nav-kibana-dev.docnav.json');
+  const navPath = Path.resolve(REPO_ROOT, 'dev_docs/nav-kibana-dev.docnav.json');
 
   let navJson;
   try {
