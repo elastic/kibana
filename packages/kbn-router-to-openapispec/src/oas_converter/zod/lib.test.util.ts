f47b9659d689b4a0706238bee2a78f4915607e38
Maryam Saeidi <maryam.saeidi@elastic.co>
2024-06-28 15:39:42 +0200

    Add unit test for zod oas_conventor

--- packages/kbn-router-to-openapispec/src/oas_converter/zod/lib.test.util.ts
+++ packages/kbn-router-to-openapispec/src/oas_converter/zod/lib.test.util.ts
@@ -8,6 +8,25 @@
 
 import { z } from 'zod';
 
+export function createLargeSchema() {
+  return z.object({
+    string: z.string().max(10).min(1),
+    maybeNumber: z.number().max(1000).min(1).optional(),
+    booleanDefault: z.boolean({ description: 'defaults to to true' }).default(true),
+    ipType: z.string().ip({ version: 'v4' }),
+    literalType: z.literal('literallythis'),
+    neverType: z.never(),
+    map: z.map(z.string(), z.string()),
+    record: z.record(z.string(), z.string()),
+    union: z.union([
+      z.string({ description: 'Union string' }).max(1),
+      z.number({ description: 'Union number' }).min(0),
+    ]),
+    uri: z.string().url().default('prototest://something'),
+    any: z.any({ description: 'any type' }),
+  });
+}
+
 export function createSharedSchema() {
   return z.object({
     string: z.string().max(10).min(1),
