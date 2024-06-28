b70f5ffc6fc653996a6dcbcb40833ae52ffb6f4e
José Luis González <joseluisgj@gmail.com>
2024-06-28 12:52:22 +0200

    API key created Callout

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/connector_detail/components/generated_config_fields.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/connector_detail/components/generated_config_fields.tsx
@@ -9,12 +9,14 @@ import React from 'react';
 
 import {
   EuiButtonEmpty,
+  EuiCallOut,
   EuiCopy,
   EuiFlexGrid,
   EuiFlexGroup,
   EuiFlexItem,
   EuiIcon,
   EuiLink,
+  EuiSpacer,
   EuiText,
   useEuiTheme,
 } from '@elastic/eui';
@@ -42,128 +44,139 @@ export const GeneratedConfigFields: React.FC<GeneratedConfigFieldsProps> = ({
   const { euiTheme } = useEuiTheme();
 
   return (
-    <EuiFlexGrid columns={3} alignItems="center" gutterSize="m">
-      <EuiFlexItem css={{ minHeight: euiTheme.size.l }}>
-        <EuiFlexGroup responsive={false} gutterSize="xs">
-          <EuiFlexItem grow={false}>
-            <EuiIcon type="check" />
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiText size="s">
-              <p>
-                {i18n.translate(
-                  'xpack.enterpriseSearch.connectorDeployment.connectorCreatedFlexItemLabel',
-                  { defaultMessage: 'Connector created' }
-                )}
-              </p>
-            </EuiText>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiFlexItem>
-      <EuiFlexItem>
-        <EuiLinkTo
-          to={generateEncodedPath(CONNECTOR_DETAIL_PATH, {
-            connectorId: connector.id,
-          })}
-        >
-          {connector.name}
-        </EuiLinkTo>
-      </EuiFlexItem>
-      <EuiFlexItem>
-        <EuiFlexGroup
-          responsive={false}
-          gutterSize="xs"
-          justifyContent="flexEnd"
-          alignItems="center"
-        >
-          <EuiFlexItem grow={false}>
-            <EuiLinkTo
-              to={generateEncodedPath(CONNECTOR_DETAIL_PATH, {
-                connectorId: connector.id,
-              })}
-            >
-              {connector.id}
-            </EuiLinkTo>
-          </EuiFlexItem>
-          <EuiFlexItem grow={false}>
-            <EuiCopy textToCopy={connector.id}>
-              {(copy) => (
-                <EuiButtonEmpty
-                  size="xs"
-                  data-test-subj="enterpriseSearchConnectorDeploymentButton"
-                  iconType="copyClipboard"
-                  onClick={copy}
-                />
-              )}
-            </EuiCopy>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiFlexItem>
-      <EuiFlexItem css={{ minHeight: euiTheme.size.l }}>
-        <EuiFlexGroup responsive={false} gutterSize="xs">
-          <EuiFlexItem grow={false}>
-            <EuiIcon type="check" />
-          </EuiFlexItem>
-          <EuiFlexItem>
-            {i18n.translate(
-              'xpack.enterpriseSearch.connectorDeployment.indexCreatedFlexItemLabel',
-              { defaultMessage: 'Index created' }
-            )}
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiFlexItem>
-      <EuiFlexItem>
-        {connector.index_name && (
+    <>
+      <EuiFlexGrid columns={3} alignItems="center" gutterSize="m">
+        <EuiFlexItem css={{ minHeight: euiTheme.size.l }}>
+          <EuiFlexGroup responsive={false} gutterSize="xs">
+            <EuiFlexItem grow={false}>
+              <EuiIcon type="check" />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiText size="s">
+                <p>
+                  {i18n.translate(
+                    'xpack.enterpriseSearch.connectorDeployment.connectorCreatedFlexItemLabel',
+                    { defaultMessage: 'Connector created' }
+                  )}
+                </p>
+              </EuiText>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+        <EuiFlexItem>
           <EuiLinkTo
-            to={generateEncodedPath(SEARCH_INDEX_PATH, {
-              indexName: connector.index_name,
+            to={generateEncodedPath(CONNECTOR_DETAIL_PATH, {
+              connectorId: connector.id,
             })}
           >
-            {connector.index_name}
+            {connector.name}
           </EuiLinkTo>
-        )}
-      </EuiFlexItem>
-      <EuiFlexItem />
-      <EuiFlexItem css={{ minHeight: euiTheme.size.l }}>
-        <EuiFlexGroup responsive={false} gutterSize="xs">
-          <EuiFlexItem grow={false}>
-            <EuiIcon type="check" />
-          </EuiFlexItem>
-          <EuiFlexItem>
-            {i18n.translate(
-              'xpack.enterpriseSearch.connectorDeployment.apiKeyCreatedFlexItemLabel',
-              { defaultMessage: 'API key created' }
-            )}
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiFlexItem>
-      <EuiFlexItem>
-        <EuiLink
-          data-test-subj="enterpriseSearchConnectorDeploymentLink"
-          href={generateEncodedPath(MANAGE_API_KEYS_URL, {})}
-        >
-          {connector.api_key_id || apiKey?.id}
-        </EuiLink>
-      </EuiFlexItem>
-      <EuiFlexItem>
-        {apiKey?.encoded && (
-          <EuiCopy textToCopy={apiKey?.encoded}>
-            {(copy) => (
-              <EuiFlexGroup responsive={false}>
-                <EuiFlexItem>{apiKey?.encoded}</EuiFlexItem>
-                <EuiFlexItem grow={false}>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiFlexGroup
+            responsive={false}
+            gutterSize="xs"
+            justifyContent="flexEnd"
+            alignItems="center"
+          >
+            <EuiFlexItem grow={false}>
+              <EuiLinkTo
+                to={generateEncodedPath(CONNECTOR_DETAIL_PATH, {
+                  connectorId: connector.id,
+                })}
+              >
+                {connector.id}
+              </EuiLinkTo>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiCopy textToCopy={connector.id}>
+                {(copy) => (
                   <EuiButtonEmpty
                     size="xs"
                     data-test-subj="enterpriseSearchConnectorDeploymentButton"
                     iconType="copyClipboard"
                     onClick={copy}
                   />
-                </EuiFlexItem>
-              </EuiFlexGroup>
-            )}
-          </EuiCopy>
-        )}
-      </EuiFlexItem>
-    </EuiFlexGrid>
+                )}
+              </EuiCopy>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+        <EuiFlexItem css={{ minHeight: euiTheme.size.l }}>
+          <EuiFlexGroup responsive={false} gutterSize="xs">
+            <EuiFlexItem grow={false}>
+              <EuiIcon type="check" />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              {i18n.translate(
+                'xpack.enterpriseSearch.connectorDeployment.indexCreatedFlexItemLabel',
+                { defaultMessage: 'Index created' }
+              )}
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          {connector.index_name && (
+            <EuiLinkTo
+              to={generateEncodedPath(SEARCH_INDEX_PATH, {
+                indexName: connector.index_name,
+              })}
+            >
+              {connector.index_name}
+            </EuiLinkTo>
+          )}
+        </EuiFlexItem>
+        <EuiFlexItem />
+        <EuiFlexItem css={{ minHeight: euiTheme.size.l }}>
+          <EuiFlexGroup responsive={false} gutterSize="xs">
+            <EuiFlexItem grow={false}>
+              <EuiIcon type="check" />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              {i18n.translate(
+                'xpack.enterpriseSearch.connectorDeployment.apiKeyCreatedFlexItemLabel',
+                { defaultMessage: 'API key created *' }
+              )}
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiLink
+            data-test-subj="enterpriseSearchConnectorDeploymentLink"
+            href={generateEncodedPath(MANAGE_API_KEYS_URL, {})}
+          >
+            {connector.api_key_id || apiKey?.id}
+          </EuiLink>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          {apiKey?.encoded && (
+            <EuiCopy textToCopy={apiKey?.encoded}>
+              {(copy) => (
+                <EuiFlexGroup responsive={false}>
+                  <EuiFlexItem>{apiKey?.encoded}</EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    <EuiButtonEmpty
+                      size="xs"
+                      data-test-subj="enterpriseSearchConnectorDeploymentButton"
+                      iconType="copyClipboard"
+                      onClick={copy}
+                    />
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              )}
+            </EuiCopy>
+          )}
+        </EuiFlexItem>
+      </EuiFlexGrid>
+      <EuiSpacer size="m" />
+      <EuiCallOut
+        color="success"
+        size="s"
+        title={i18n.translate('xpack.enterpriseSearch.connectorDeployment.generatedConfigCallout', {
+          defaultMessage: `You'll only see this API key once, so save it somewhere safe. We don't store your API keys, so if you lose a key you'll need to generate a replacement`,
+        })}
+        iconType="asterisk"
+      />
+    </>
   );
 };
