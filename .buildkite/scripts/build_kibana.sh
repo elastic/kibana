9ebcc603df8ff6e70abdb2c971fd26fca969772b
Jonathan Budzenski <jon@elastic.co>
2024-06-28 08:52:02 -0500

    use absolute path

--- .buildkite/scripts/build_kibana.sh
+++ .buildkite/scripts/build_kibana.sh
@@ -45,7 +45,7 @@ fi
 
 echo "--- Archive Kibana Distribution"
 version="$(jq -r '.version' package.json)"
-linuxBuild="kibana-$version-SNAPSHOT-linux-x86_64.tar.gz"
+linuxBuild="$KIBANA_DIR/target/kibana-$version-SNAPSHOT-linux-x86_64.tar.gz"
 installDir="$KIBANA_DIR/install/kibana"
 mkdir -p "$installDir"
 tar -xzf "$linuxBuild" -C "$installDir" --strip=1
