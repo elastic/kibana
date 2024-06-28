36d28443ff54113f118517d8e207462dbfc09e39
machadoum <pablo.nevesmachado@elastic.co>
2024-06-28 14:30:08 +0200

    Add button to preview alerts from inside the risk contribution panel

---
+++ x-pack/plugins/security_solution/public/flyout/document_details/alert_preview_button.tsx
@@ -0,0 +1,51 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiButtonIcon } from '@elastic/eui';
+import { useExpandableFlyoutApi } from '@kbn/expandable-flyout';
+import React, { useCallback } from 'react';
+import type { FC } from 'react';
+import { ALERT_PREVIEW_BANNER } from './preview';
+import { DocumentDetailsPreviewPanelKey } from './shared/constants/panel_keys';
+
+interface AlertPreviewButtonProps {
+  /**
+   * Name of the index used in the parent's page
+   */
+  indexName: string;
+  id: string;
+  'data-test-subj'?: string;
+  scopeId: string;
+}
+
+export const AlertPreviewButton: FC<AlertPreviewButtonProps> = ({
+  id,
+  indexName,
+  'data-test-subj': dataTestSubj,
+  scopeId,
+}) => {
+  const { openPreviewPanel } = useExpandableFlyoutApi();
+
+  const openAlertPreview = useCallback(
+    () =>
+      openPreviewPanel({
+        id: DocumentDetailsPreviewPanelKey,
+        params: {
+          id,
+          indexName,
+          scopeId,
+          isPreviewMode: true,
+          banner: ALERT_PREVIEW_BANNER,
+        },
+      }),
+    [openPreviewPanel, id, indexName, scopeId]
+  );
+
+  return (
+    <EuiButtonIcon iconType="expand" data-test-subj={dataTestSubj} onClick={openAlertPreview} />
+  );
+};
