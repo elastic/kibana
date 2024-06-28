3ceb21164b2fc5449c81e4a60dd52c6d258d0857
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-06-28 12:48:42 +0200

    Improves the performance of the table ES|QL visualization

---
+++ x-pack/plugins/lens/common/expressions/map_to_columns/map_to_columns_fn_textbased.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { OriginalColumn, MapToColumnsExpressionFunction } from './types';
+
+export const mapToOriginalColumnsTextBased: MapToColumnsExpressionFunction['fn'] = (
+  data,
+  { idMap: encodedIdMap }
+) => {
+  const idMap = JSON.parse(encodedIdMap) as Record<string, OriginalColumn[]>;
+
+  return {
+    ...data,
+    rows: data.rows.map((row) => {
+      const mappedRow: Record<string, unknown> = {};
+
+      Object.entries(row).forEach(([id, value]) => {
+        if (id in idMap) {
+          idMap[id].forEach(({ id: originalId }) => {
+            mappedRow[originalId] = value;
+          });
+        }
+      });
+
+      return mappedRow;
+    }),
+    columns: data.columns
+      .filter((column) => idMap[column.id])
+      .map((column) => {
+        const originalColumns = idMap[column.id];
+        return (
+          originalColumns &&
+          originalColumns.map((originalColumn) => ({
+            ...column,
+            id: originalColumn.id,
+          }))
+        );
+      })
+      .flat(),
+  };
+};
