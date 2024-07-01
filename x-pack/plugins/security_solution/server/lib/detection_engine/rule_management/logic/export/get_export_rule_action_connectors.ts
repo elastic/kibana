b3f5498ad282a887c8db51f52c75fd6af8d4772a
Devin Hurley <devin.hurley@elastic.co>
2024-07-01 14:55:47 -0400

    remove comment

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_management/logic/export/get_export_rule_action_connectors.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_management/logic/export/get_export_rule_action_connectors.ts
@@ -76,8 +76,6 @@ export const getRuleActionConnectorsForExport = async (
     actionConnectorDetails: defaultActionConnectorDetails,
   };
 
-  // I should be able to undo the additional .filter
-  // check tomorrow
   const ids = [...new Set(rules.flatMap((rule) => rule.actions.map(({ id }) => id)))];
 
   let actionsIds = ids;
