07f778424941b9435973d15800ed80b3b998720d
Tre' Seymour <wayne.seymour@elastic.co>
2024-07-01 14:17:04 +0100

    Both of the settings tests need help from reviewers.

--- x-pack/test_serverless/api_integration/test_suites/common/index_management/settings.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/index_management/settings.ts
@@ -5,6 +5,8 @@
  * 2.0.
  */
 
+import expect from '@kbn/expect';
+
 import { FtrProviderContext } from '../../../ftr_provider_context';
 import { RoleCredentials } from '../../../../shared/services';
 
