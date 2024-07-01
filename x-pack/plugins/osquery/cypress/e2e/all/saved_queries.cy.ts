41055fca74dfa02b77484e64198b8b13632d6cf6
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2024-07-01 21:32:07 +0200

    fix test

--- x-pack/plugins/osquery/cypress/e2e/all/saved_queries.cy.ts
+++ x-pack/plugins/osquery/cypress/e2e/all/saved_queries.cy.ts
@@ -58,7 +58,7 @@ describe('ALL - Saved queries', { tags: ['@ess', '@serverless'] }, () => {
   it('checks that user cant add a saved query with an ID that already exists', () => {
     cy.contains('Saved queries').click();
     cy.contains('Add saved query').click();
-
+    cy.getBySel('globalLoadingIndicator').should('not.exist');
     cy.get('input[name="id"]').type(`users_elastic{downArrow}{enter}`);
 
     cy.contains('ID must be unique').should('not.exist');
@@ -76,8 +76,7 @@ describe('ALL - Saved queries', { tags: ['@ess', '@serverless'] }, () => {
     });
   });
 
-  // FAILING ES SERVERLESS PROMOTION: https://github.com/elastic/kibana/issues/169787
-  describe.skip('prebuilt', () => {
+  describe('prebuilt', () => {
     let packName: string;
     let packId: string;
     let savedQueryId: string;
