4f25f6fd4e54ac40bd239968d69b1088bcdbd4a1
pgayvallet <pierre.gayvallet@elastic.co>
2024-07-01 20:42:39 +0200

    fix more things

--- x-pack/plugins/security/server/user_profile/user_profile_service.ts
+++ x-pack/plugins/security/server/user_profile/user_profile_service.ts
@@ -196,10 +196,9 @@ export class UserProfileService {
 
     if (!userSession.value.userProfileId) {
       this.logger.debug(
-        () =>
-          `User profile missing from the current session [sid=${getPrintableSessionId(
-            userSession.value.sid
-          )}].`
+        `User profile missing from the current session [sid=${getPrintableSessionId(
+          userSession.value.sid
+        )}].`
       );
       return null;
     }
