701f99a0f5632c2357bc694a0078e76f888736e4
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-07-01 10:00:42 +0200

    [Discover] Unskip tests

--- test/functional/apps/discover/esql/_esql_columns.ts
+++ test/functional/apps/discover/esql/_esql_columns.ts
@@ -36,8 +36,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     defaultIndex: 'logstash-*',
   };
 
-  // FLAKY: https://github.com/elastic/kibana/issues/186416
-  describe.skip('discover esql columns', async function () {
+  describe('discover esql columns', async function () {
     before(async () => {
       await kibanaServer.savedObjects.cleanStandardList();
       await security.testUser.setRoles(['kibana_admin', 'test_logstash_reader']);
