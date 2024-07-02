af7671329b4f029e43e445a1979e30410780c5d5
Hannah Mudge <hannah.wright@elastic.co>
2024-07-02 07:44:14 -0600

    Add time range to `FieldStatsTable`

--- src/plugins/discover/public/embeddable/components/search_embeddable_field_stats_table_component.tsx
+++ src/plugins/discover/public/embeddable/components/search_embeddable_field_stats_table_component.tsx
@@ -50,6 +50,7 @@ export function SearchEmbeddablFieldStatsTableComponent({
       onAddFilter={onAddFilter}
       searchSessionId={fetchContext?.searchSessionId}
       isEsqlMode={isEsql}
+      timeRange={fetchContext?.timeRange}
     />
   );
 }
