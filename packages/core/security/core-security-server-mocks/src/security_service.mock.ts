10bdfbe5876c0d6fb4b4453b0eff5c074032decb
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2024-07-01 17:58:31 -0700

    Exposes authenticate user mock from core-security-server-mocks

--- packages/core/security/core-security-server-mocks/src/security_service.mock.ts
+++ packages/core/security/core-security-server-mocks/src/security_service.mock.ts
@@ -16,6 +16,7 @@ import type {
   InternalSecurityServiceStart,
 } from '@kbn/core-security-server-internal';
 import { auditServiceMock, type MockedAuditService } from './audit.mock';
+import { mockAuthenticatedUser, MockAuthenticatedUserProps } from '@kbn/core-security-common/mocks';
 
 const createSetupMock = () => {
   const mock: jest.Mocked<SecurityServiceSetup> = {
@@ -97,4 +98,6 @@ export const securityServiceMock = {
   createInternalSetup: createInternalSetupMock,
   createInternalStart: createInternalStartMock,
   createRequestHandlerContext: createRequestHandlerContextMock,
+  createMockAuthenticatedUser: (props: MockAuthenticatedUserProps = {}) =>
+    mockAuthenticatedUser(props),
 };
