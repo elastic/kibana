dc52be01304af7bc0a3ea67d787f6c29f6d55e46
Gergő Ábrahám <gergo.abraham@elastic.co>
2024-07-02 15:27:00 +0200

    wrap ProcessDescendantTooltip in `memo`

--- x-pack/plugins/security_solution/public/management/pages/event_filters/view/components/process_descendant_tooltip.tsx
+++ x-pack/plugins/security_solution/public/management/pages/event_filters/view/components/process_descendant_tooltip.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { memo } from 'react';
 import { EuiToolTip, EuiText, EuiIcon } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { PROCESS_DESCENDANT_EVENT_FILTER_EXTRA_ENTRY_TEXT } from '../../../../../../common/endpoint/service/artifacts/constants';
@@ -15,47 +15,47 @@ interface ProcessDescendantsTooltipProps {
   indicateExtraEntry?: boolean;
 }
 
-export const ProcessDescendantsTooltip = ({
-  testIdPrefix,
-  indicateExtraEntry = false,
-}: ProcessDescendantsTooltipProps) => {
-  return (
-    <EuiToolTip
-      content={
-        <EuiText size="s">
-          <p>
-            <FormattedMessage
-              id="xpack.securitySolution.eventFilters.filterProcessDescendants.tooltip"
-              defaultMessage="Filtering the descendants of a process means that events from the matched process are ingested, but events from its descendant processes are omitted."
-            />
-          </p>
-          {indicateExtraEntry && (
-            <>
-              <p>
-                <FormattedMessage
-                  id="xpack.securitySolution.eventFilters.filterProcessDescendants.tooltipExtraEntry"
-                  defaultMessage="Note: the following additional condition is applied:"
-                />
-              </p>
-              <p>
-                <code>{PROCESS_DESCENDANT_EVENT_FILTER_EXTRA_ENTRY_TEXT}</code>
-              </p>
-            </>
-          )}
-          <p>
-            <FormattedMessage
-              id="xpack.securitySolution.eventFilters.filterProcessDescendants.tooltipVersionInfo"
-              defaultMessage="Process descendant filtering works only with Agents v8.15 and newer."
-            />
-          </p>
-        </EuiText>
-      }
-      data-test-subj={testIdPrefix !== undefined ? `${testIdPrefix}-tooltipText` : undefined}
-    >
-      <EuiIcon
-        type="iInCircle"
-        data-test-subj={testIdPrefix !== undefined ? `${testIdPrefix}-tooltipIcon` : undefined}
-      />
-    </EuiToolTip>
-  );
-};
+export const ProcessDescendantsTooltip = memo<ProcessDescendantsTooltipProps>(
+  ({ testIdPrefix, indicateExtraEntry = false }: ProcessDescendantsTooltipProps) => {
+    return (
+      <EuiToolTip
+        content={
+          <EuiText size="s">
+            <p>
+              <FormattedMessage
+                id="xpack.securitySolution.eventFilters.filterProcessDescendants.tooltip"
+                defaultMessage="Filtering the descendants of a process means that events from the matched process are ingested, but events from its descendant processes are omitted."
+              />
+            </p>
+            {indicateExtraEntry && (
+              <>
+                <p>
+                  <FormattedMessage
+                    id="xpack.securitySolution.eventFilters.filterProcessDescendants.tooltipExtraEntry"
+                    defaultMessage="Note: the following additional condition is applied:"
+                  />
+                </p>
+                <p>
+                  <code>{PROCESS_DESCENDANT_EVENT_FILTER_EXTRA_ENTRY_TEXT}</code>
+                </p>
+              </>
+            )}
+            <p>
+              <FormattedMessage
+                id="xpack.securitySolution.eventFilters.filterProcessDescendants.tooltipVersionInfo"
+                defaultMessage="Process descendant filtering works only with Agents v8.15 and newer."
+              />
+            </p>
+          </EuiText>
+        }
+        data-test-subj={testIdPrefix !== undefined ? `${testIdPrefix}-tooltipText` : undefined}
+      >
+        <EuiIcon
+          type="iInCircle"
+          data-test-subj={testIdPrefix !== undefined ? `${testIdPrefix}-tooltipIcon` : undefined}
+        />
+      </EuiToolTip>
+    );
+  }
+);
+ProcessDescendantsTooltip.displayName = 'ProcessDescendantsTooltip';
