19aa3b1d44a34346baf2babdf4e7ed7be29ed03b
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2024-07-02 17:28:13 +0200

    fix timelines

--- x-pack/plugins/osquery/cypress/e2e/all/timelines.cy.ts
+++ x-pack/plugins/osquery/cypress/e2e/all/timelines.cy.ts
@@ -23,7 +23,7 @@ describe('ALL - Timelines', { tags: ['@ess'] }, () => {
       cy.getBySel('timeline-bottom-bar-title-button').click();
     });
     cy.getBySel('timelineQueryInput').type(
-      'NOT host.name: "dev-fleet-server.8220"{enter} and component.type: "osquery"'
+      'NOT host.name: "dev-fleet-server*"{enter} and component.type: "osquery"'
     );
     // Filter out alerts
     cy.getBySel('timeline-sourcerer-trigger').click();
