5f4dff19b22d9cae78d82cb7ad31c77ff14b2043
James Gowdy <jgowdy@elastic.co>
2024-07-03 17:56:01 +0100

    fixing tags and adding test

--- x-pack/test/api_integration/apis/ml/system/node_count.ts
+++ x-pack/test/api_integration/apis/ml/system/node_count.ts
@@ -15,12 +15,12 @@ export default ({ getService }: FtrProviderContext) => {
   const supertest = getService('supertestWithoutAuth');
   const ml = getService('ml');
 
-  async function runRequest(user: USER) {
+  async function runRequest(user: USER, expectedStatusCode: number) {
     const { body, status } = await supertest
       .get(`/internal/ml/ml_node_count`)
       .auth(user, ml.securityCommon.getPasswordForUser(user))
       .set(getCommonRequestHeader('1'));
-    ml.api.assertResponseStatusCode(200, status, body);
+    ml.api.assertResponseStatusCode(expectedStatusCode, status, body);
 
     return body;
   }
@@ -28,12 +28,16 @@ export default ({ getService }: FtrProviderContext) => {
   describe('POST ml/ml_node_count', function () {
     describe('get ml node count', () => {
       it('should match expected values', async () => {
-        const resp = await runRequest(USER.ML_POWERUSER);
+        const resp = await runRequest(USER.ML_POWERUSER, 200);
         expect(resp.count).to.be.greaterThan(0, 'count should be greater than 0');
         expect(resp.lazyNodeCount).to.be.greaterThan(
           -1,
           'lazyNodeCount should be greater or equal to 0'
         );
+      });
+
+      it('should should fail for a unauthorized user', async () => {
+        await runRequest(USER.ML_UNAUTHORIZED, 403);
       });
     });
   });
