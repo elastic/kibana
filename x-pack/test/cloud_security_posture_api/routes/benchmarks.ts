ee5f8b655d311ef458e7429f3cbf26072ff94cc4
animehart <rickyangwyn@gmail.com>
2024-07-03 18:25:34 -0700

    switched to just use 1 rule

--- x-pack/test/cloud_security_posture_api/routes/benchmarks.ts
+++ x-pack/test/cloud_security_posture_api/routes/benchmarks.ts
@@ -162,10 +162,9 @@ export default function (providerContext: FtrProviderContext) {
         const benchmark = 'cis_aws';
         const benchmarkRules = await getCspBenchmarkRules(benchmark);
 
-        const cspmFinding1 = getMockFinding(benchmarkRules[0], 'passed');
-        const cspmFinding2 = getMockFinding(benchmarkRules[1], 'failed');
+        const cspmFinding = getMockFinding(benchmarkRules[0], 'passed');
 
-        await index.addFindings([cspmFinding1, cspmFinding2]);
+        await index.addFindings([cspmFinding]);
 
         const { body: benchmarksBeforeMute } = await supertest
           .get('/internal/cloud_security_posture/benchmarks')
@@ -178,7 +177,7 @@ export default function (providerContext: FtrProviderContext) {
           (item: { id: string }) => item.id === benchmark
         );
 
-        expect(scoreBeforeMute.score.postureScore).to.equal(50);
+        expect(scoreBeforeMute.score.postureScore).to.equal(100);
 
         await supertest
           .post(`/internal/cloud_security_posture/rules/_bulk_action`)
@@ -189,10 +188,10 @@ export default function (providerContext: FtrProviderContext) {
             action: 'mute',
             rules: [
               {
-                benchmark_id: cspmFinding2.rule.benchmark.id,
-                benchmark_version: cspmFinding2.rule.benchmark.version,
-                rule_number: cspmFinding2.rule.benchmark.rule_number || '',
-                rule_id: cspmFinding2.rule.id,
+                benchmark_id: cspmFinding.rule.benchmark.id,
+                benchmark_version: cspmFinding.rule.benchmark.version,
+                rule_number: cspmFinding.rule.benchmark.rule_number || '',
+                rule_id: cspmFinding.rule.id,
               },
             ],
           })
@@ -209,17 +208,16 @@ export default function (providerContext: FtrProviderContext) {
           (item: { id: string }) => item.id === benchmark
         );
 
-        expect(scoreAfterMute.score.postureScore).to.equal(100);
+        expect(scoreAfterMute.score.postureScore).to.equal(0);
       });
 
       it('Verify kspm benchmark score is updated when muting rules', async () => {
         const benchmark = 'cis_k8s';
         const benchmarkRules = await getCspBenchmarkRules(benchmark);
 
-        const kspmFinding1 = getMockFinding(benchmarkRules[0], 'passed');
-        const kspmFinding2 = getMockFinding(benchmarkRules[1], 'failed');
+        const kspmFinding = getMockFinding(benchmarkRules[0], 'passed');
 
-        await index.addFindings([kspmFinding1, kspmFinding2]);
+        await index.addFindings([kspmFinding]);
         const { body: benchmarksBeforeMute } = await supertest
           .get('/internal/cloud_security_posture/benchmarks')
           .set(ELASTIC_HTTP_VERSION_HEADER, '2')
@@ -231,7 +229,7 @@ export default function (providerContext: FtrProviderContext) {
           (item: { id: string }) => item.id === benchmark
         );
 
-        expect(scoreBeforeMute.score.postureScore).to.equal(50);
+        expect(scoreBeforeMute.score.postureScore).to.equal(100);
 
         await supertest
           .post(`/internal/cloud_security_posture/rules/_bulk_action`)
@@ -242,10 +240,10 @@ export default function (providerContext: FtrProviderContext) {
             action: 'mute',
             rules: [
               {
-                benchmark_id: kspmFinding2.rule.benchmark.id,
-                benchmark_version: kspmFinding2.rule.benchmark.version,
-                rule_number: kspmFinding2.rule.benchmark.rule_number || '',
-                rule_id: kspmFinding2.rule.id,
+                benchmark_id: kspmFinding.rule.benchmark.id,
+                benchmark_version: kspmFinding.rule.benchmark.version,
+                rule_number: kspmFinding.rule.benchmark.rule_number || '',
+                rule_id: kspmFinding.rule.id,
               },
             ],
           })
@@ -262,7 +260,7 @@ export default function (providerContext: FtrProviderContext) {
           (item: { id: string }) => item.id === benchmark
         );
 
-        expect(scoreAfterMute.score.postureScore).to.equal(100);
+        expect(scoreAfterMute.score.postureScore).to.equal(0);
       });
     });
   });
