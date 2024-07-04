a7d1bc3d3320c7d9419074c5990cf690f341e9dc
konrad.szwarc <konrad.szwarc@elastic.co>
2024-07-04 13:11:27 +0200

    wip

---
+++ x-pack/test/security_solution_api_integration/test_suites/edr_workflows/apis/trial_license_complete_tier/serverless/index.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { getRegistryUrl as getRegistryUrlFromIngest } from '@kbn/fleet-plugin/server';
+import { FtrProviderContext } from '../common/ftr_provider_context';
+import { getRegistryUrlFromTestEnv, isRegistryEnabled } from '../common/registry';
+
+export default function endpointAPIIntegrationTests(providerContext: FtrProviderContext) {
+  const { loadTestFile, getService } = providerContext;
+
+  describe('Endpoint plugin', function () {
+    const ingestManager = getService('ingestManager');
+    const log = getService('log');
+
+    if (!isRegistryEnabled()) {
+      log.warning('These tests are being run with an external package registry');
+    }
+
+    const registryUrl = getRegistryUrlFromTestEnv() ?? getRegistryUrlFromIngest();
+    log.info(`Package registry URL for tests: ${registryUrl}`);
+
+    before(async () => {
+      try {
+        await ingestManager.setup();
+      } catch (err) {
+        log.warning(`Error setting up ingestManager: ${err}`);
+      }
+    });
+
+    // loadTestFile(require.resolve('./resolver'));
+    // loadTestFile(require.resolve('./metadata'));
+    // loadTestFile(require.resolve('./policy'));
+    // loadTestFile(require.resolve('./package'));
+    // loadTestFile(require.resolve('./endpoint_authz'));
+    loadTestFile(require.resolve('./endpoint_response_actions/execute'));
+    loadTestFile(require.resolve('./endpoint_response_actions/agent_type_support'));
+    // loadTestFile(require.resolve('./endpoint_artifacts/trusted_apps'));
+    // loadTestFile(require.resolve('./endpoint_artifacts/event_filters'));
+    // loadTestFile(require.resolve('./endpoint_artifacts/host_isolation_exceptions'));
+    // loadTestFile(require.resolve('./endpoint_artifacts/blocklists'));
+  });
+}
