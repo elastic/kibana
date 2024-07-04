f03d3eb3463756d9eb377ffe99b20146617fb21c
Joseph McElroy <joseph.mcelroy@elastic.co>
2024-07-04 16:27:52 +0100

    fix test

--- x-pack/plugins/search_playground/server/lib/conversational_chain.test.ts
+++ x-pack/plugins/search_playground/server/lib/conversational_chain.test.ts
@@ -445,8 +445,8 @@ describe('conversational chain', () => {
       ],
       // Even with body_content of 1000, the token count should be below or equal to model limit of 100
       expectedTokens: [
-        { type: 'context_token_count', count: 73 },
-        { type: 'prompt_token_count', count: 100 },
+        { type: 'context_token_count', count: 70 },
+        { type: 'prompt_token_count', count: 97 },
       ],
       expectedHasClipped: true,
       expectedSearchRequest: [
