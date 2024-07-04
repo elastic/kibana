3247a9a4c944825842621eecef3bb4d07f33ffa0
Sergi Massaneda <sergi.massaneda@elastic.co>
2024-07-04 18:23:33 +0200

    top level components tests

--- x-pack/plugins/integration_assistant/public/services/telemetry/mocks/service.ts
+++ x-pack/plugins/integration_assistant/public/services/telemetry/mocks/service.ts
@@ -4,5 +4,11 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-export * from './types';
-export { Telemetry } from './telemetry/service';
+
+export const mockReportEvent = jest.fn();
+
+export const telemetryServiceMock = {
+  createStart: () => ({
+    reportEvent: mockReportEvent,
+  }),
+};
