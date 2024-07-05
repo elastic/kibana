4143a1fb1678230c937df0e606e16e981c7e18c8
jpdjere <jpdjeredjian@gmail.com>
2024-07-05 15:25:29 +0200

    Add license header

--- x-pack/plugins/security_solution/common/api/detection_engine/model/rule_schema/time_duration.test.ts
+++ x-pack/plugins/security_solution/common/api/detection_engine/model/rule_schema/time_duration.test.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 import { expectParseError, expectParseSuccess, stringifyZodError } from '@kbn/zod-helpers';
 import { TimeDuration } from './time_duration'; // Update with the actual path to your TimeDuration file
 
