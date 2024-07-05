d428df9c005cdb70b33000861c65b870b55f3ef2
konrad.szwarc <konrad.szwarc@elastic.co>
2024-07-05 17:08:58 +0200

    enable more tests

--- x-pack/test/security_solution_api_integration/test_suites/edr_workflows/apis/trial_license_complete_tier/tests/index.ts
+++ x-pack/test/security_solution_api_integration/test_suites/edr_workflows/apis/trial_license_complete_tier/tests/index.ts
@@ -5,14 +5,18 @@
  * 2.0.
  */
 import { getRegistryUrl as getRegistryUrlFromIngest } from '@kbn/fleet-plugin/server';
+import { isServerlessKibanaFlavor } from '@kbn/security-solution-plugin/scripts/endpoint/common/stack_services';
 import { FtrProviderContext } from '../common/ftr_provider_context';
 import { getRegistryUrlFromTestEnv, isRegistryEnabled } from '../common/registry';
+import { ROLE } from '../common/services/roles_users';
 
 export default function endpointAPIIntegrationTests(providerContext: FtrProviderContext) {
   const { loadTestFile, getService } = providerContext;
 
   describe('Endpoint plugin', function () {
     const ingestManager = getService('ingestManager');
+    const rolesUsersProvider = getService('rolesUsersProvider');
+    const kbnClient = getService('kibanaServer');
     const log = getService('log');
 
     if (!isRegistryEnabled()) {
@@ -22,24 +26,41 @@ export default function endpointAPIIntegrationTests(providerContext: FtrProvider
     const registryUrl = getRegistryUrlFromTestEnv() ?? getRegistryUrlFromIngest();
     log.info(`Package registry URL for tests: ${registryUrl}`);
 
+    const roles = Object.values(ROLE);
     before(async () => {
       try {
         await ingestManager.setup();
       } catch (err) {
         log.warning(`Error setting up ingestManager: ${err}`);
       }
+
+      if (!(await isServerlessKibanaFlavor(kbnClient))) {
+        // create role/user
+        for (const role of roles) {
+          await rolesUsersProvider.createRole({ predefinedRole: role });
+          await rolesUsersProvider.createUser({ name: role, roles: [role] });
+        }
+      }
     });
 
-    // loadTestFile(require.resolve('./resolver'));
-    // loadTestFile(require.resolve('./metadata'));
+    after(async () => {
+      if (!(await isServerlessKibanaFlavor(kbnClient))) {
+        // delete role/user
+        await rolesUsersProvider.deleteUsers(roles);
+        await rolesUsersProvider.deleteRoles(roles);
+      }
+    });
+
+    loadTestFile(require.resolve('./resolver'));
+    loadTestFile(require.resolve('./metadata'));
     loadTestFile(require.resolve('./policy'));
-    // loadTestFile(require.resolve('./package'));
-    // loadTestFile(require.resolve('./endpoint_authz'));
+    loadTestFile(require.resolve('./package'));
+    loadTestFile(require.resolve('./endpoint_authz'));
     loadTestFile(require.resolve('./response_actions/execute'));
     loadTestFile(require.resolve('./response_actions/agent_type_support'));
-    loadTestFile(require.resolve('./artifacts/trusted_apps'));
-    loadTestFile(require.resolve('./artifacts/event_filters'));
-    loadTestFile(require.resolve('./artifacts/host_isolation_exceptions'));
-    loadTestFile(require.resolve('./artifacts/blocklists'));
+    // loadTestFile(require.resolve('./artifacts/trusted_apps'));
+    // loadTestFile(require.resolve('./artifacts/event_filters'));
+    // loadTestFile(require.resolve('./artifacts/host_isolation_exceptions'));
+    // loadTestFile(require.resolve('./artifacts/blocklists'));
   });
 }
