f63c764533a2c7e987c44116208d7b96746aed34
José Luis González <joseluisgj@gmail.com>
2024-07-05 11:41:02 +0200

    EuiSelectable custom list

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/connectors/create_connector/create_connector.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/connectors/create_connector/create_connector.tsx
@@ -13,6 +13,7 @@ import { css } from '@emotion/react';
 // import { useValues } from 'kea';
 
 import {
+  EuiBadge,
   EuiButton,
   EuiButtonEmpty,
   EuiFieldText,
@@ -20,6 +21,8 @@ import {
   EuiFlexItem,
   EuiFormControlLayout,
   EuiFormRow,
+  EuiHighlight,
+  EuiIcon,
   EuiInputPopover,
   EuiPanel,
   EuiRadio,
@@ -51,6 +54,7 @@ import { EnterpriseSearchContentPageTemplate } from '../../layout';
 import { connectorsBreadcrumbs } from '../connectors';
 
 import connectorsBackgroundImage from './assets/connector_logos_comp.png';
+import dropbox from './assets/dropbox.svg';
 
 import { ConnectorDescriptionBadge } from './connector_description_badge_popout';
 
@@ -152,23 +156,56 @@ export const CreateConnector: React.FC = () => {
     },
   ];
 
-  const OPTIONS: EuiSelectableOption[] = [
-    { prepend: <EuiButton size="s">Connect</EuiButton>, label: 'Titan' },
-    { prepend: <EuiButton size="s">Connect</EuiButton>, label: 'Enceladus' },
-    { prepend: <EuiButton size="s">Connect</EuiButton>, label: 'Mimas', checked: 'on' },
-    { prepend: <EuiButton size="s">Connect</EuiButton>, label: 'Dione' },
-    { prepend: <EuiButton size="s">Connect</EuiButton>, label: 'Iapetus' },
-    { prepend: <EuiButton size="s">Connect</EuiButton>, label: 'Phoebe' },
+  const connectorsData = [
+    {
+      name: 'Azure Blob Storage',
+      icon: <EuiIcon type={dropbox} size="l" title="Dropbox" />,
+      techPreview: false,
+      checked: 'on',
+    },
+    {
+      name: 'Confluence Cloud & Server',
+      icon: <EuiIcon type={dropbox} size="l" title="Dropbox" />,
+      techPreview: true,
+    },
+    {
+      name: 'Confluence Data Center',
+      icon: <EuiIcon type={dropbox} size="l" title="Dropbox" />,
+      techPreview: false,
+    },
+    {
+      name: 'Dropbox',
+      icon: <EuiIcon type={dropbox} size="l" title="Dropbox" />,
+      techPreview: true,
+    },
   ];
 
+  interface OptionData {
+    secondaryContent?: string;
+  }
+
+  const [options, setOptions] = useState<Array<EuiSelectableOption<OptionData>>>([
+    ...connectorsData.map(
+      (connector): EuiSelectableOption => ({
+        label: `${connector.name}`,
+        prepend: connector.icon,
+        append: connector.techPreview ? (
+          <EuiBadge iconType="beaker" color="hollow">
+            Thech preview
+          </EuiBadge>
+        ) : null,
+      })
+    ),
+  ]);
+
   const SelectableInputPopover = () => {
-    const [options, setOptions] = useState<EuiSelectableOption[]>(OPTIONS);
+    // const [options, setOptions] = useState<EuiSelectableOption[]>(OPTIONS);
     const [isOpen, setIsOpen] = useState(false);
     const [inputValue, setInputValue] = useState('');
     const [isSearching, setIsSearching] = useState(true);
 
     return (
-      <EuiFormControlLayout
+      /*       <EuiFormControlLayout
         clear={{ onClick: () => setInputValue('') }}
         icon={{ type: 'stopFilled', color: 'success', side: 'left', size: 'xl' }}
         css={({ euiTheme }) => css`
@@ -176,56 +213,58 @@ export const CreateConnector: React.FC = () => {
             left: ${euiTheme.size.xs};
           }
         `}
+      > */
+      <EuiSelectable
+        aria-label="Selectable + input popover example"
+        options={options}
+        onChange={(newOptions, event, changedOption) => {
+          setOptions(newOptions);
+          setIsOpen(false);
+          if (changedOption.checked === 'on') {
+            setInputValue(changedOption.label);
+            setIsSearching(false);
+          } else {
+            setInputValue('');
+          }
+        }}
+        listProps={{
+          rowHeight: 50,
+          showIcons: false,
+          css: { '.euiSelectableList__list': { maxBlockSize: 200 } },
+        }}
+        singleSelection
+        searchable
+        searchProps={{
+          isClearable: true,
+          placeholder: 'Choose a data source',
+          value: inputValue,
+          onChange: (value) => {
+            setInputValue(value);
+            setIsSearching(true);
+          },
+          onKeyDown: (event) => {
+            if (event.key === 'Tab') return setIsOpen(false);
+            if (event.key !== 'Escape') return setIsOpen(true);
+          },
+          onClick: () => setIsOpen(true),
+          onFocus: () => setIsOpen(true),
+        }}
+        isPreFiltered={isSearching ? false : { highlightSearch: false }} // Shows the full list when not actively typing to search
       >
-        <EuiSelectable
-          aria-label="Selectable + input popover example"
-          options={options}
-          onChange={(newOptions, event, changedOption) => {
-            setOptions(newOptions);
-            setIsOpen(false);
-
-            if (changedOption.checked === 'on') {
-              setInputValue(changedOption.label);
-              setIsSearching(false);
-            } else {
-              setInputValue('');
-            }
-          }}
-          singleSelection
-          searchable
-          searchProps={{
-            placeholder: 'Choose a data source',
-            value: inputValue,
-            onChange: (value) => {
-              setInputValue(value);
-              setIsSearching(true);
-            },
-            onKeyDown: (event) => {
-              if (event.key === 'Tab') return setIsOpen(false);
-              if (event.key !== 'Escape') return setIsOpen(true);
-            },
-            onClick: () => setIsOpen(true),
-            onFocus: () => setIsOpen(true),
-          }}
-          isPreFiltered={isSearching ? false : { highlightSearch: false }} // Shows the full list when not actively typing to search
-          listProps={{
-            css: { '.euiSelectableList__list': { maxBlockSize: 200 } },
-          }}
-        >
-          {(list, search) => (
-            <EuiInputPopover
-              closePopover={() => setIsOpen(false)}
-              disableFocusTrap
-              closeOnScroll
-              isOpen={isOpen}
-              input={search!}
-              panelPaddingSize="none"
-            >
-              {list}
-            </EuiInputPopover>
-          )}
-        </EuiSelectable>
-      </EuiFormControlLayout>
+        {(list, search) => (
+          <EuiInputPopover
+            closePopover={() => setIsOpen(false)}
+            disableFocusTrap
+            closeOnScroll
+            isOpen={isOpen}
+            input={search!}
+            panelPaddingSize="none"
+          >
+            {list}
+          </EuiInputPopover>
+        )}
+      </EuiSelectable>
+      // </EuiFormControlLayout>
     );
   };
 
