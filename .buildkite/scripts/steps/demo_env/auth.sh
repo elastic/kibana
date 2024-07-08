dfe26f4b7e2851a5baf9cd49e30ed0aa21a39594
Alex Szabo <alex.szabo@elastic.co>
2024-07-08 14:25:52 +0200

    [7.17] Clean up pipelines (#187490)
    ## Summary
    Removes unused pipelines, and their related scripts: 
     - package_testing.yml
     - demo_env.yml
    
    Updates a pipeline to the new buildkite agent targeting format:
     - performance/daily.yml (missed backport after migration)

--- .buildkite/scripts/steps/demo_env/auth.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-echo '--- Auth and set up kubectl'
-
-gcloud container clusters get-credentials demo-env --region us-central1 --project elastic-kibana-184716
-kubectl config use-context gke_elastic-kibana-184716_us-central1_demo-env
