445a9129d5f857e12c38a3def9e1744a3cbd542f
Gergő Ábrahám <gergo.abraham@elastic.co>
2024-07-08 11:45:53 +0200

    [EDR Workflows][8.15] Enable process descendant event filtering feature flag for 8.15 (#187627)
    <img width="664" alt="image"
    src="https://github.com/elastic/kibana/assets/39014407/04472587-23b1-4c9e-8eec-83e10abe6dac">

--- x-pack/plugins/security_solution/common/experimental_features.ts
+++ x-pack/plugins/security_solution/common/experimental_features.ts
@@ -262,7 +262,7 @@ export const allowedExperimentalValues = Object.freeze({
   /**
    * Adds a new option to filter descendants of a process for Management / Event Filters
    */
-  filterProcessDescendantsForEventFiltersEnabled: false,
+  filterProcessDescendantsForEventFiltersEnabled: true,
 });
 
 type ExperimentalConfigKeys = Array<keyof ExperimentalFeatures>;
