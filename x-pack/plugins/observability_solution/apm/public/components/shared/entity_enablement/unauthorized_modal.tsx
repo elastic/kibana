76916e99440d0c9b511952781f3c9acfab5503f3
Kate Patticha <aikaterini.patticha@elastic.co>
2024-07-09 20:32:49 +0300

    Update text for the unauthorised modal

--- x-pack/plugins/observability_solution/apm/public/components/shared/entity_enablement/unauthorized_modal.tsx
+++ x-pack/plugins/observability_solution/apm/public/components/shared/entity_enablement/unauthorized_modal.tsx
@@ -15,7 +15,19 @@
 
 import React from 'react';
 import { i18n } from '@kbn/i18n';
-import { EuiButton, EuiConfirmModal } from '@elastic/eui';
+import {
+  EuiButton,
+  EuiConfirmModal,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiImage,
+  EuiPanel,
+  EuiSpacer,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
+import { useKibanaUrl } from '../../../hooks/use_kibana_url';
 
 export function UnauthorisedModal({
   isUnauthorizedModalVisible = false,
@@ -24,13 +36,15 @@ export function UnauthorisedModal({
   isUnauthorizedModalVisible?: boolean;
   onClose: () => void;
 }) {
+  const servicesInventory = useKibanaUrl('/plugins/apm/assets/services_inventory.png');
+
   return (
     <>
       {isUnauthorizedModalVisible && (
         <EuiConfirmModal
-          title={i18n.translate('xpack.apm.eem.unauthorised.title', {
-            defaultMessage: 'This feature is turned off',
-          })}
+          style={{
+            width: '630px',
+          }}
           onCancel={onClose}
           onConfirm={onClose}
           confirmButtonText={
@@ -42,12 +56,49 @@ export function UnauthorisedModal({
           }
           defaultFocusedButton="confirm"
         >
-          <p>
-            {i18n.translate('xpack.apm.unauthorised.body', {
-              defaultMessage:
-                'To detect services from log files and view them in the services inventory, an administrator needs to enable the new logs and services experiences in APM inventory page ',
-            })}
-          </p>
+          <EuiPanel hasShadow={false}>
+            <EuiFlexGroup
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              gutterSize="m"
+            >
+              <EuiFlexItem>
+                <EuiIcon type="lock" size="l" />
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiTitle>
+                  <h2>
+                    {i18n.translate('xpack.apm.unauthorised.title', {
+                      defaultMessage: 'This feature is turned off',
+                    })}
+                  </h2>
+                </EuiTitle>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPanel>
+
+          <EuiPanel hasShadow={false} paddingSize="s">
+            <EuiText grow={false} textAlign="center">
+              <p>
+                {i18n.translate('xpack.apm.unauthorised.body', {
+                  defaultMessage:
+                    'To see services detected from logs and APM-instrumented services in our new service inventory, please ask an administrator to visit this page and try our new experience. ',
+                })}
+              </p>
+            </EuiText>
+          </EuiPanel>
+          <EuiSpacer size="m" />
+          <EuiPanel hasBorder paddingSize="none">
+            <EuiImage
+              size="xl"
+              src={servicesInventory}
+              alt={i18n.translate('xpack.apm.unauthorised.image.at,', {
+                defaultMessage:
+                  'Image of the new experience of the service inventory, showing services detected from logs and APM-instrumented services',
+              })}
+            />
+          </EuiPanel>
         </EuiConfirmModal>
       )}
     </>
