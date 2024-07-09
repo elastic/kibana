a0975fb1cdae8cc364a80e562ae418b0edaca560
vadimkibana <vadimkibana@gmail.com>
2024-07-09 09:23:32 +0200

    test for param validation in where expressions

--- packages/kbn-esql-validation-autocomplete/src/validation/__tests__/validation.params.test.ts
+++ packages/kbn-esql-validation-autocomplete/src/validation/__tests__/validation.params.test.ts
@@ -19,3 +19,25 @@ test('should allow param inside agg function argument', async () => {
   expect(res2).toMatchObject({ errors: [], warnings: [] });
   expect(res3).toMatchObject({ errors: [], warnings: [] });
 });
+
+test('allow params in WHERE command expressions', async () => {
+  const { validate } = await setup();
+
+  const res1 = await validate('FROM index | WHERE stringField >= ?earliest');
+  const res2 = await validate(`
+    FROM index
+      | WHERE stringField >= ?earliest
+      | WHERE stringField <= ?0
+      | WHERE stringField == ?
+  `);
+  const res3 = await validate(`
+    FROM index
+      | WHERE stringField >= ?earliest
+        AND stringField <= ?0
+        AND stringField == ?
+  `);
+
+  expect(res1).toMatchObject({ errors: [], warnings: [] });
+  expect(res2).toMatchObject({ errors: [], warnings: [] });
+  expect(res3).toMatchObject({ errors: [], warnings: [] });
+});
