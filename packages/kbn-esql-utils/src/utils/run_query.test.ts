46ebf56d3be44a7c249cadd9b4ea7b9109cc8ee7
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-07-10 10:39:07 +0200

    fix jest tests

--- packages/kbn-esql-utils/src/utils/run_query.test.ts
+++ packages/kbn-esql-utils/src/utils/run_query.test.ts
@@ -19,35 +19,24 @@ describe('getEarliestLatestParams', () => {
     const time = { from: 'Jul 5, 2024 @ 08:03:56.849', to: 'Jul 5, 2024 @ 10:03:56.849' };
     const query = 'FROM foo | where time > ?earliest';
     const params = getEarliestLatestParams(query, time);
-    expect(params).toEqual([
-      {
-        earliest: '2024-07-05T06:03:56.849Z',
-      },
-    ]);
+    expect(params).toHaveLength(1);
+    expect(params[0]).toHaveProperty('earliest');
   });
 
   it('should return an array with the latest param if exists at the query', () => {
     const time = { from: 'Jul 5, 2024 @ 08:03:56.849', to: 'Jul 5, 2024 @ 10:03:56.849' };
     const query = 'FROM foo | where time < ?latest';
     const params = getEarliestLatestParams(query, time);
-    expect(params).toEqual([
-      {
-        latest: '2024-07-05T08:03:56.849Z',
-      },
-    ]);
+    expect(params).toHaveLength(1);
+    expect(params[0]).toHaveProperty('latest');
   });
 
   it('should return an array with the latest and earliest params if exist at the query', () => {
     const time = { from: 'Jul 5, 2024 @ 08:03:56.849', to: 'Jul 5, 2024 @ 10:03:56.849' };
     const query = 'FROM foo | where time < ?latest amd time > ?earliest';
     const params = getEarliestLatestParams(query, time);
-    expect(params).toEqual([
-      {
-        earliest: '2024-07-05T06:03:56.849Z',
-      },
-      {
-        latest: '2024-07-05T08:03:56.849Z',
-      },
-    ]);
+    expect(params).toHaveLength(2);
+    expect(params[0]).toHaveProperty('earliest');
+    expect(params[1]).toHaveProperty('latest');
   });
 });
