cb4e9d1cb75230cc0d75707e230b67b8c84fed62
Garrett Spong <spong@users.noreply.github.com>
2024-07-10 09:40:44 -0600

    Fix LangSmith tracing for streaming

--- x-pack/plugins/elastic_assistant/server/lib/langchain/graphs/default_assistant_graph/index.ts
+++ x-pack/plugins/elastic_assistant/server/lib/langchain/graphs/default_assistant_graph/index.ts
@@ -138,7 +138,15 @@ export const callAssistantGraph: AgentExecutor<true | false> = async ({
   const inputs = { input: latestMessage[0]?.content as string };
 
   if (isStream) {
-    return streamGraph({ apmTracer, assistantGraph, inputs, logger, onLlmResponse, request });
+    return streamGraph({
+      apmTracer,
+      assistantGraph,
+      inputs,
+      logger,
+      onLlmResponse,
+      request,
+      traceOptions,
+    });
   }
 
   const graphResponse = await invokeGraph({
