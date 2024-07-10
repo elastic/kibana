cd20ae73973d8bc10c8091ac48275115dbb257dd
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-07-10 13:00:53 +0200

    Fix ES|QL code type errors

--- packages/kbn-esql-validation-autocomplete/src/definitions/functions.ts
+++ packages/kbn-esql-validation-autocomplete/src/definitions/functions.ts
@@ -1361,8 +1361,7 @@ const logDefinition: FunctionDefinition = {
     // do not really care here about the base and field
     // just need to check both values are not negative
     for (const arg of fnDef.args) {
-      // @ts-expect-error upgrade typescript v5.1.6
-      if (isLiteralItem(arg) && arg.value < 0) {
+      if (isLiteralItem(arg) && Number(arg.value) < 0) {
         messages.push({
           type: 'warning' as const,
           code: 'logOfNegativeValue',
@@ -1415,8 +1414,7 @@ const log10Definition: FunctionDefinition = {
     // do not really care here about the base and field
     // just need to check both values are not negative
     for (const arg of fnDef.args) {
-      // @ts-expect-error upgrade typescript v5.1.6
-      if (isLiteralItem(arg) && arg.value < 0) {
+      if (isLiteralItem(arg) && Number(arg.value) < 0) {
         messages.push({
           type: 'warning' as const,
           code: 'logOfNegativeValue',
