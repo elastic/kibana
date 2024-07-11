1ae879ae1dc252cf3ee74ab92afb3994a45d11c9
Kevin Delemme <kevin.delemme@elastic.co>
2024-07-11 14:13:17 +0200

    Remove widgets from other plugins

--- x-pack/plugins/observability_solution/investigate_app/public/hooks/workflow_blocks/use_workflow_blocks.tsx
+++ x-pack/plugins/observability_solution/investigate_app/public/hooks/workflow_blocks/use_workflow_blocks.tsx
@@ -6,11 +6,8 @@
  */
 import type { InvestigateWidgetCreate, WorkflowBlock } from '@kbn/investigate-plugin/common';
 import { compact } from 'lodash';
-import React, { useState } from 'react';
+import React from 'react';
 import { WorkflowBlocksControl } from '../../components/workflow_blocks_control';
-import { useAlertsWorkflowBlock } from './use_alerts_workflow_block';
-import { useApmWorkflowBlock } from './use_apm_workflow_block';
-import { useSloWorkflowBlock } from './use_slo_workflow_block';
 
 export function useWorkflowBlocks({
   isTimelineEmpty,
@@ -25,36 +22,11 @@ export function useWorkflowBlocks({
   end: string;
   onWidgetAdd: (create: InvestigateWidgetCreate) => Promise<void>;
 }) {
-  const [flyout, setFlyout] = useState<React.ReactNode | null>(null);
-  const sloWorkflowBlock = useSloWorkflowBlock({
-    setFlyout,
-    onWidgetAdd,
-  });
-
-  const alertsWorkflowBlock = useAlertsWorkflowBlock({
-    onWidgetAdd,
-    start,
-    end,
-  });
-
-  const apmWorkflowBlock = useApmWorkflowBlock({
-    start,
-    end,
-    onWidgetAdd,
-  });
-
-  const blocks = isTimelineEmpty
-    ? compact([sloWorkflowBlock, alertsWorkflowBlock, apmWorkflowBlock])
-    : dynamicBlocks;
+  const blocks = isTimelineEmpty ? compact([]) : dynamicBlocks;
 
   if (!blocks.length) {
     return null;
   }
 
-  return (
-    <>
-      {flyout}
-      <WorkflowBlocksControl blocks={blocks} compressed={!isTimelineEmpty} />
-    </>
-  );
+  return <WorkflowBlocksControl blocks={blocks} compressed={!isTimelineEmpty} />;
 }
