39d14a5a2bcdd1ac20eb67ac07a05dfd852f9cdb
Tre' Seymour <wayne.seymour@elastic.co>
2024-07-11 10:35:28 +0100

    Revert

--- packages/kbn-test/src/kbn_client/kbn_client_requester.test.ts
+++ packages/kbn-test/src/kbn_client/kbn_client_requester.test.ts
@@ -6,36 +6,30 @@
  * Side Public License, v 1.
  */
 
-import { pathWithSpace, redactUrl } from './kbn_client_requester';
+import { pathWithSpace } from './kbn_client_requester';
 
-describe('KBN Client Requester Functions', () => {
-  it('pathWithSpace() adds a space to the path', () => {
+describe('pathWithSpace()', () => {
+  it('adds a space to the path', () => {
     expect(pathWithSpace('hello')`/foo/bar`).toMatchInlineSnapshot(`"/s/hello/foo/bar"`);
   });
 
-  it('pathWithSpace() ignores the space when it is empty', () => {
+  it('ignores the space when it is empty', () => {
     expect(pathWithSpace(undefined)`/foo/bar`).toMatchInlineSnapshot(`"/foo/bar"`);
     expect(pathWithSpace('')`/foo/bar`).toMatchInlineSnapshot(`"/foo/bar"`);
   });
 
-  it('pathWithSpace() ignores the space when it is the default space', () => {
+  it('ignores the space when it is the default space', () => {
     expect(pathWithSpace('default')`/foo/bar`).toMatchInlineSnapshot(`"/foo/bar"`);
   });
 
-  it('pathWithSpace() uriencodes variables in the path', () => {
+  it('uriencodes variables in the path', () => {
     expect(pathWithSpace('space')`hello/${'funky/username🏴‍☠️'}`).toMatchInlineSnapshot(
       `"/s/space/hello/funky%2Fusername%F0%9F%8F%B4%E2%80%8D%E2%98%A0%EF%B8%8F"`
     );
   });
 
-  it('pathWithSpace() ensures the path always starts with a slash', () => {
+  it('ensures the path always starts with a slash', () => {
     expect(pathWithSpace('foo')`hello/world`).toMatchInlineSnapshot(`"/s/foo/hello/world"`);
     expect(pathWithSpace()`hello/world`).toMatchInlineSnapshot(`"/hello/world"`);
-  });
-
-  it(`redactUrl() takes a string such as 'http://some-user:some-password@localhost:5620' and returns the url without the auth info`, () => {
-    expect(redactUrl('http://some-user:some-password@localhost:5620')).toEqual(
-      'http://localhost:5620'
-    );
   });
 });
