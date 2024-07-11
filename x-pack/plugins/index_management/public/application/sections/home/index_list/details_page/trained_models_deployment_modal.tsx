460b52077ffa26673b1a40fff87a7ee182f0c9db
Sander Philipse <94373878+sphilipse@users.noreply.github.com>
2024-07-11 12:35:56 +0200

    [Semantic text] Make semantic text work with non-root level fields (#187154)
    ## Summary
    
    This makes semantic text work with non-root level reference fields. It
    also correctly adds copy_to to existing copy_to fields instead of
    replacing them, and streamlines a lot of the code.
    
    To test these changes:
    
    - Create an index
    - Go to the index mappings page at
    `app/management/data/index_management/indices/index_details?{yourIndexName}=blah&tab=mappings`
    - Add an object field with a text field inside
    - Add a semantic text field referencing that text field
    - If you're on a Macbook, create a new inference endpoint with the model
    `.elser_model_2` instead of using the default inference endpoint.
    - Add a second semantic text field referencing that text field
    - Save your mappings
    - Use JSON view to verify that the newly created text field contains a
    `copy_to` field referencing both newly created semantic text fields
    - Verify that the newly created semantic text fields are also in the
    JSON view
    
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [x] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [x] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [x] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)

--- x-pack/plugins/index_management/public/application/sections/home/index_list/details_page/trained_models_deployment_modal.tsx
+++ x-pack/plugins/index_management/public/application/sections/home/index_list/details_page/trained_models_deployment_modal.tsx
@@ -8,179 +8,175 @@ import { EuiConfirmModal, useGeneratedHtmlId, EuiHealth } from '@elastic/eui';
 import React from 'react';
 
 import { EuiLink } from '@elastic/eui';
-import { useEffect, useState } from 'react';
-import type { SharePluginStart } from '@kbn/share-plugin/public';
+import { useEffect, useMemo, useState } from 'react';
 import { i18n } from '@kbn/i18n';
+import { isSemanticTextField } from '../../../../components/mappings_editor/lib/utils';
+import { deNormalize } from '../../../../components/mappings_editor/lib';
+import { useMLModelNotificationToasts } from '../../../../../hooks/use_ml_model_status_toasts';
+import { useMappingsState } from '../../../../components/mappings_editor/mappings_state_context';
+import { useAppContext } from '../../../../app_context';
 
-interface SemanticTextProps {
-  setIsModalVisible: (isVisible: boolean) => void;
-  refreshModal: () => void;
-  pendingDeployments: Array<string | undefined>;
-  errorsInTrainedModelDeployment: string[];
-  url?: SharePluginStart['url'];
+export interface TrainedModelsDeploymentModalProps {
+  fetchData: () => void;
+  errorsInTrainedModelDeployment: Record<string, string | undefined>;
+  setErrorsInTrainedModelDeployment: React.Dispatch<
+    React.SetStateAction<Record<string, string | undefined>>
+  >;
 }
 
 const ML_APP_LOCATOR = 'ML_APP_LOCATOR';
 const TRAINED_MODELS_MANAGE = 'trained_models';
 
 export function TrainedModelsDeploymentModal({
-  setIsModalVisible,
-  refreshModal,
-  pendingDeployments = [],
-  errorsInTrainedModelDeployment = [],
-  url,
-}: SemanticTextProps) {
+  errorsInTrainedModelDeployment = {},
+  fetchData,
+  setErrorsInTrainedModelDeployment,
+}: TrainedModelsDeploymentModalProps) {
+  const { fields, inferenceToModelIdMap } = useMappingsState();
+  const {
+    plugins: { ml },
+    url,
+  } = useAppContext();
   const modalTitleId = useGeneratedHtmlId();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
   const closeModal = () => setIsModalVisible(false);
   const [mlManagementPageUrl, setMlManagementPageUrl] = useState<string>('');
+  const { showErrorToasts } = useMLModelNotificationToasts();
 
   useEffect(() => {
-    setIsModalVisible(pendingDeployments.length > 0);
-  }, [pendingDeployments, setIsModalVisible]);
-
-  useEffect(() => {
-    let isCancelled = false;
     const mlLocator = url?.locators.get(ML_APP_LOCATOR);
     const generateUrl = async () => {
       if (mlLocator) {
         const mlURL = await mlLocator.getUrl({
           page: TRAINED_MODELS_MANAGE,
         });
-        if (!isCancelled) {
-          setMlManagementPageUrl(mlURL);
-        }
+        setMlManagementPageUrl(mlURL);
       }
     };
     generateUrl();
-    return () => {
-      isCancelled = true;
-    };
   }, [url]);
 
-  const ErroredDeployments = pendingDeployments.filter(
-    (deployment) => deployment !== undefined && errorsInTrainedModelDeployment.includes(deployment)
-  );
+  const inferenceIdsInPendingList = useMemo(() => {
+    return Object.values(deNormalize(fields))
+      .filter(isSemanticTextField)
+      .map((field) => field.inference_id);
+  }, [fields]);
 
-  const PendingModelsDeploymentModal = () => {
-    const pendingDeploymentsList = pendingDeployments.map((deployment, index) => (
-      <li key={index}>
-        <EuiHealth textSize="xs" color="warning">
-          {deployment}
-        </EuiHealth>
-      </li>
-    ));
+  const [pendingDeployments, setPendingDeployments] = useState<string[]>([]);
 
-    return (
-      <EuiConfirmModal
-        aria-labelledby={modalTitleId}
-        style={{ width: 600 }}
-        title={i18n.translate(
-          'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.titleLabel',
-          {
-            defaultMessage: 'Models still deploying',
-          }
-        )}
-        titleProps={{ id: modalTitleId }}
-        onCancel={closeModal}
-        onConfirm={refreshModal}
-        cancelButtonText={i18n.translate(
-          'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.closeButtonLabel',
-          {
-            defaultMessage: 'Close',
-          }
-        )}
-        confirmButtonText={i18n.translate(
-          'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.refreshButtonLabel',
-          {
-            defaultMessage: 'Refresh',
-          }
-        )}
-        defaultFocusedButton="confirm"
-        data-test-subj="trainedModelsDeploymentModal"
-      >
-        <p data-test-subj="trainedModelsDeploymentModalText">
-          {i18n.translate(
-            'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.textAboutDeploymentsNotCompleted',
-            {
-              defaultMessage:
-                'Some fields are referencing models that have not yet completed deployment. Deployment may take a few minutes to complete.',
-            }
-          )}
-        </p>
-        <ul style={{ listStyleType: 'none' }}>{pendingDeploymentsList}</ul>
-        <EuiLink href={mlManagementPageUrl} target="_blank">
-          {i18n.translate(
-            'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.textTrainedModelManagementLink',
-            {
-              defaultMessage: 'Go to Trained Model Management',
-            }
-          )}
-        </EuiLink>
-      </EuiConfirmModal>
-    );
+  const startModelAllocation = async (trainedModelId: string) => {
+    try {
+      await ml?.mlApi?.trainedModels.startModelAllocation(trainedModelId);
+    } catch (error) {
+      setErrorsInTrainedModelDeployment((previousState) => ({
+        ...previousState,
+        [trainedModelId]: error.message,
+      }));
+      showErrorToasts(error);
+      setIsModalVisible(true);
+    }
   };
 
-  const ErroredModelsDeploymentModal = () => {
-    const pendingDeploymentsList = pendingDeployments.map((deployment, index) => (
-      <li key={index}>
-        <EuiHealth textSize="xs" color="danger">
-          {deployment}
-        </EuiHealth>
-      </li>
-    ));
-
-    return (
-      <EuiConfirmModal
-        aria-labelledby={modalTitleId}
-        style={{ width: 600 }}
-        title={i18n.translate(
-          'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.deploymentErrorTitle',
-          {
-            defaultMessage: 'Models could not be deployed',
-          }
-        )}
-        titleProps={{ id: modalTitleId }}
-        onCancel={closeModal}
-        onConfirm={refreshModal}
-        cancelButtonText={i18n.translate(
-          'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.deploymentErrorCancelButtonLabel',
-          {
-            defaultMessage: 'Cancel',
-          }
-        )}
-        confirmButtonText={i18n.translate(
-          'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.deploymentErrorTryAgainButtonLabel',
-          {
-            defaultMessage: 'Try again',
-          }
-        )}
-        defaultFocusedButton="confirm"
-        data-test-subj="trainedModelsErroredDeploymentModal"
-      >
-        <p data-test-subj="trainedModelsErrorDeploymentModalText">
-          {i18n.translate(
-            'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.deploymentErrorText',
-            {
-              defaultMessage: 'There was an error when trying to deploy the following models.',
-            }
-          )}
-        </p>
-        <ul style={{ listStyleType: 'none' }}>{pendingDeploymentsList}</ul>
-        <EuiLink href={mlManagementPageUrl} target="_blank">
-          {i18n.translate(
-            'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.deploymentErrorTrainedModelManagementLink',
-            {
-              defaultMessage: 'Go to Trained Model Management',
-            }
-          )}
-        </EuiLink>
-      </EuiConfirmModal>
+  useEffect(() => {
+    const models = inferenceIdsInPendingList.map(
+      (inferenceId) => inferenceToModelIdMap?.[inferenceId]
     );
-  };
+    for (const model of models) {
+      if (model && !model.isDownloading && !model.isDeployed) {
+        // Sometimes the model gets stuck in a ready to deploy state, so we need to trigger deployment manually
+        startModelAllocation(model.trainedModelId);
+      }
+    }
+    const pendingModels = models
+      .map((model) => {
+        return model?.trainedModelId && !model?.isDeployed ? model?.trainedModelId : '';
+      })
+      .filter((trainedModelId) => !!trainedModelId);
+    const uniqueDeployments = pendingModels.filter(
+      (deployment, index) => pendingModels.indexOf(deployment) === index
+    );
+    setPendingDeployments(uniqueDeployments);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [inferenceIdsInPendingList, inferenceToModelIdMap]);
 
-  return ErroredDeployments.length > 0 ? (
-    <ErroredModelsDeploymentModal />
-  ) : (
-    <PendingModelsDeploymentModal />
+  const erroredDeployments = pendingDeployments.filter(
+    (deployment) => errorsInTrainedModelDeployment[deployment]
   );
+
+  useEffect(() => {
+    if (erroredDeployments.length > 0 || pendingDeployments.length > 0) {
+      setIsModalVisible(true);
+    }
+  }, [erroredDeployments.length, pendingDeployments.length]);
+  return isModalVisible ? (
+    <EuiConfirmModal
+      aria-labelledby={modalTitleId}
+      style={{ width: 600 }}
+      title={
+        erroredDeployments.length > 0
+          ? i18n.translate(
+              'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.deploymentErrorTitle',
+              {
+                defaultMessage: 'Models could not be deployed',
+              }
+            )
+          : i18n.translate('xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.titleLabel', {
+              defaultMessage: 'Models still deploying',
+            })
+      }
+      titleProps={{ id: modalTitleId }}
+      onCancel={closeModal}
+      onConfirm={fetchData}
+      cancelButtonText={i18n.translate(
+        'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.closeButtonLabel',
+        {
+          defaultMessage: 'Close',
+        }
+      )}
+      confirmButtonText={i18n.translate(
+        'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.refreshButtonLabel',
+        {
+          defaultMessage: 'Refresh',
+        }
+      )}
+      defaultFocusedButton="confirm"
+      data-test-subj="trainedModelsDeploymentModal"
+    >
+      <p data-test-subj="trainedModelsDeploymentModalText">
+        {erroredDeployments.length > 0
+          ? i18n.translate(
+              'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.deploymentErrorText',
+              {
+                defaultMessage: 'There was an error when trying to deploy the following models.',
+              }
+            )
+          : i18n.translate(
+              'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.textAboutDeploymentsNotCompleted',
+              {
+                defaultMessage:
+                  'Some fields are referencing models that have not yet completed deployment. Deployment may take a few minutes to complete.',
+              }
+            )}
+      </p>
+      <ul style={{ listStyleType: 'none' }}>
+        {(erroredDeployments.length > 0 ? erroredDeployments : pendingDeployments).map(
+          (deployment) => (
+            <li key={deployment}>
+              <EuiHealth textSize="xs" color="danger">
+                {deployment}
+              </EuiHealth>
+            </li>
+          )
+        )}
+      </ul>
+      <EuiLink href={mlManagementPageUrl} target="_blank">
+        {i18n.translate(
+          'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.textTrainedModelManagementLink',
+          {
+            defaultMessage: 'Go to Trained Model Management',
+          }
+        )}
+      </EuiLink>
+    </EuiConfirmModal>
+  ) : null;
 }
