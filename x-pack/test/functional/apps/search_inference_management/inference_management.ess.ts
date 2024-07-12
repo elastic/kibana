9dfbc1589e2bc03cd73b257dd5e73440fdebe92c
Samiul Monir <samiul.monir@elastic.co>
2024-07-12 15:36:59 -0400

    Adding inference tests for ESS

---
+++ x-pack/test/functional/apps/search_inference_management/inference_management.ess.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { ModelConfig } from '@kbn/inference_integration_flyout/types';
+
+import { FtrProviderContext } from '../../ftr_provider_context';
+
+export default function ({ getPageObjects, getService }: FtrProviderContext) {
+  const pageObjects = getPageObjects(['common', 'inferenceManagement']);
+  const browser = getService('browser');
+  const ml = getService('ml');
+
+  describe('Serverless Inference Management UI', function () {
+    this.tags('skipMKI');
+
+    before(async () => {
+      await pageObjects.common.navigateToApp('enterpriseSearchApplications/inference_endpoints');
+    });
+
+    describe('endpoint empty view', () => {
+      it('is loaded successfully', async () => {
+        await pageObjects.inferenceManagement.InferenceEmptyPage.expectComponentsToBeExist();
+      });
+      it('opens add inference flyout', async () => {
+        await pageObjects.inferenceManagement.InferenceEmptyPage.expectFlyoutTobeOpened();
+      });
+    });
+
+    describe('endpoint tabular view', () => {
+      before(async () => {
+        const modelConfig: ModelConfig = {
+          service: 'elser',
+          service_settings: {
+            num_allocations: 1,
+            num_threads: 1,
+          },
+        };
+        await ml.api.createInferenceEndpoint('endpoint-1', 'sparse_embedding', modelConfig);
+        await browser.refresh();
+      });
+
+      it('is loaded successfully', async () => {
+        await pageObjects.inferenceManagement.InferenceTabularPage.expectHeaderToBeExist();
+        await pageObjects.inferenceManagement.InferenceTabularPage.expectTabularViewToBeLoaded();
+      });
+
+      it('can copy an endpoint id', async () => {
+        await pageObjects.inferenceManagement.InferenceTabularPage.expectToCopyEndpoint();
+      });
+
+      it('can delete an endpoint', async () => {
+        await pageObjects.inferenceManagement.InferenceTabularPage.expectEndPointTobeDeleted();
+      });
+    });
+  });
+}
