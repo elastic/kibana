12f9ff250a9dd6080930883d878ebe0e0b54621a
Davis McPhee <davis.mcphee@elastic.co>
2024-07-13 20:24:54 -0300

    Add log cell badge renderer

---
+++ src/plugins/discover/public/components/data_types/logs/log_level_badge_cell.tsx
@@ -0,0 +1,37 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { EuiBadge, mathWithUnits, useEuiTheme } from '@elastic/eui';
+import type { CSSObject } from '@emotion/react';
+import { getLogLevelCoalescedValue, getLogLevelColor } from '@kbn/discover-utils';
+import { euiThemeVars } from '@kbn/ui-theme';
+import type { DataGridCellValueElementProps } from '@kbn/unified-data-table';
+import React from 'react';
+
+const badgeCss: CSSObject = {
+  marginTop: '-4px',
+  maxWidth: mathWithUnits(euiThemeVars.euiSize, (size) => size * 7.5),
+};
+
+export const LogLevelBadgeCell = (props: DataGridCellValueElementProps) => {
+  const { euiTheme } = useEuiTheme();
+  const value = props.row.flattened['log.level'];
+
+  if (!value) {
+    return <>-</>;
+  }
+
+  const coalescedValue = getLogLevelCoalescedValue(value);
+  const color = coalescedValue ? getLogLevelColor(coalescedValue, euiTheme) : undefined;
+
+  return (
+    <EuiBadge color={color} css={badgeCss}>
+      {value}
+    </EuiBadge>
+  );
+};
