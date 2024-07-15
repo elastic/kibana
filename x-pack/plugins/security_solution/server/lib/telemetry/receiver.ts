655f25eaf9f098de88690dd1cd53c83fcb4a96c6
Sebastián Zaffarano <sebastian.zaffarano@elastic.co>
2024-07-15 11:28:24 +0200

    [8.14] [Telemetry][Security Solution] Use the proper index to query builtin alerts (#187859) (#188235)
    # Backport
    
    This will backport the following commits from `main` to `8.14`:
    - [[Telemetry][Security Solution] Use the proper index to query builtin
    alerts (#187859)](https://github.com/elastic/kibana/pull/187859)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Sebastián
    Zaffarano","email":"sebastian.zaffarano@elastic.co"},"sourceCommit":{"committedDate":"2024-07-12T13:17:43Z","message":"[Telemetry][Security
    Solution] Use the proper index to query builtin alerts (#187859)\n\n##
    Summary\r\n\r\nhttps://github.com/elastic/kibana/pull/177263 changed the
    way\r\n`telemetry-prebuilt-rule-alerts` get data from elastic, but it
    changed\r\nthe index used to run the queries. This PR fixes it using the
    proper\r\nindex.","sha":"a120c510b9738aab0fb5f9296515a82f6f0792a6","branchLabelMapping":{"^v8.16.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","Team:
    SecuritySolution","v8.14.0","v8.15.0","v8.16.0"],"title":"[Telemetry][Security
    Solution] Use the proper index to query builtin
    alerts","number":187859,"url":"https://github.com/elastic/kibana/pull/187859","mergeCommit":{"message":"[Telemetry][Security
    Solution] Use the proper index to query builtin alerts (#187859)\n\n##
    Summary\r\n\r\nhttps://github.com/elastic/kibana/pull/177263 changed the
    way\r\n`telemetry-prebuilt-rule-alerts` get data from elastic, but it
    changed\r\nthe index used to run the queries. This PR fixes it using the
    proper\r\nindex.","sha":"a120c510b9738aab0fb5f9296515a82f6f0792a6"}},"sourceBranch":"main","suggestedTargetBranches":["8.14","8.15"],"targetPullRequestStates":[{"branch":"8.14","label":"v8.14.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.15","label":"v8.15.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.16.0","branchLabelMappingKey":"^v8.16.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/187859","number":187859,"mergeCommit":{"message":"[Telemetry][Security
    Solution] Use the proper index to query builtin alerts (#187859)\n\n##
    Summary\r\n\r\nhttps://github.com/elastic/kibana/pull/177263 changed the
    way\r\n`telemetry-prebuilt-rule-alerts` get data from elastic, but it
    changed\r\nthe index used to run the queries. This PR fixes it using the
    proper\r\nindex.","sha":"a120c510b9738aab0fb5f9296515a82f6f0792a6"}}]}]
    BACKPORT-->
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/server/lib/telemetry/receiver.ts
+++ x-pack/plugins/security_solution/server/lib/telemetry/receiver.ts
@@ -203,6 +203,7 @@ export interface ITelemetryReceiver {
   }>;
 
   fetchPrebuiltRuleAlertsBatch(
+    index: string,
     executeFrom: string,
     executeTo: string
   ): AsyncGenerator<TelemetryEvent[], void, unknown>;
@@ -744,13 +745,17 @@ export class TelemetryReceiver implements ITelemetryReceiver {
     };
   }
 
-  public async *fetchPrebuiltRuleAlertsBatch(executeFrom: string, executeTo: string) {
+  public async *fetchPrebuiltRuleAlertsBatch(
+    index: string,
+    executeFrom: string,
+    executeTo: string
+  ) {
     this.logger.l('Searching prebuilt rule alerts from', {
       executeFrom,
       executeTo,
     });
 
-    let pitId = await this.openPointInTime(DEFAULT_DIAGNOSTIC_INDEX);
+    let pitId = await this.openPointInTime(index);
     let fetchMore = true;
     let searchAfter: SortResults | undefined;
 
