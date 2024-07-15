10c27a9bbf4464e4ed3589fa3f8614a8feb06dbd
Davis McPhee <davis.mcphee@elastic.co>
2024-07-15 19:09:44 -0300

    [Discover] Add support for log overview tab to Discover log profile (#186680)
    ## Summary
    
    This PR adds the log overview tab from Logs Explorer to the Discover log
    document profile. The only difference between the tab in Logs Explorer
    and Discover is that the one in Logs Explorer includes the O11y AI
    assistant while the Discover one doesn't (for now at least):
    
    ![log_overview](https://github.com/user-attachments/assets/3c5b3ea0-227e-41fa-ab1e-5618008b5d39)
    
    Resolves #187096.
    
    ### Checklist
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [ ] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [ ] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/plugins/unified_doc_viewer/public/__mocks__/services.ts
+++ src/plugins/unified_doc_viewer/public/__mocks__/services.ts
@@ -8,7 +8,6 @@
 
 import { analyticsServiceMock } from '@kbn/core-analytics-browser-mocks';
 import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
-import { discoverSharedPluginMock } from '@kbn/discover-shared-plugin/public/mocks';
 import { fieldFormatsMock } from '@kbn/field-formats-plugin/common/mocks';
 import { fieldsMetadataPluginPublicMock } from '@kbn/fields-metadata-plugin/public/mocks';
 import { uiSettingsServiceMock } from '@kbn/core-ui-settings-browser-mocks';
@@ -24,7 +23,6 @@ export const mockUnifiedDocViewer: jest.Mocked<UnifiedDocViewerStart> = {
 export const mockUnifiedDocViewerServices: jest.Mocked<UnifiedDocViewerServices> = {
   analytics: analyticsServiceMock.createAnalyticsServiceStart(),
   data: dataPluginMock.createStartContract(),
-  discoverShared: discoverSharedPluginMock.createStartContract(),
   fieldFormats: fieldFormatsMock,
   fieldsMetadata: fieldsMetadataPluginPublicMock.createStartContract(),
   storage: new Storage(localStorage),
