819758105619850f130e1b4ef3c2a1278c6b45f9
Carlos Crespo <crespocarlos@users.noreply.github.com>
2024-07-15 18:10:58 +0200

    [7.17] [Infra] Limit the number of metrics accepted by Snapshot API (#188181) (#188242)
    # Backport
    
    This will backport the following commits from `main` to `7.17`:
    - [[Infra] Limit the number of metrics accepted by Snapshot API
    (#188181)](https://github.com/elastic/kibana/pull/188181)
    
    <!--- Backport version: 8.9.8 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Carlos
    Crespo","email":"crespocarlos@users.noreply.github.com"},"sourceCommit":{"committedDate":"2024-07-12T13:53:53Z","message":"[Infra]
    Limit the number of metrics accepted by Snapshot API (#188181)\n\npart
    of [3628](https://github.com/elastic/observability-dev/issues/3628)\r\n-
    private\r\n\r\n\r\n## Summary\r\n\r\nAfter adding 20 items, users can no
    longer add more metrics and will see\r\nthe \"Add metric\" button
    disabled with a tooltip\r\n\r\n<img width=\"1713\"
    alt=\"image\"\r\nsrc=\"https://github.com/user-attachments/assets/c784b08b-e118-4491-b53d-46bfde898216\">\r\n\r\n\r\n###
    How to test\r\n\r\n- Start a local Kibana instance pointing to an oblt
    cluster\r\n- Navigate to Infrastructure\r\n- Try to add more than 20
    metrics in the Metrics
    dropdown.","sha":"f2d1a8b6d24486cedb0dad97e71cd660845f353c","branchLabelMapping":{"^v8.16.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","backport:all-open","ci:project-deploy-observability","Team:obs-ux-infra_services","v8.16.0"],"number":188181,"url":"https://github.com/elastic/kibana/pull/188181","mergeCommit":{"message":"[Infra]
    Limit the number of metrics accepted by Snapshot API (#188181)\n\npart
    of [3628](https://github.com/elastic/observability-dev/issues/3628)\r\n-
    private\r\n\r\n\r\n## Summary\r\n\r\nAfter adding 20 items, users can no
    longer add more metrics and will see\r\nthe \"Add metric\" button
    disabled with a tooltip\r\n\r\n<img width=\"1713\"
    alt=\"image\"\r\nsrc=\"https://github.com/user-attachments/assets/c784b08b-e118-4491-b53d-46bfde898216\">\r\n\r\n\r\n###
    How to test\r\n\r\n- Start a local Kibana instance pointing to an oblt
    cluster\r\n- Navigate to Infrastructure\r\n- Try to add more than 20
    metrics in the Metrics
    dropdown.","sha":"f2d1a8b6d24486cedb0dad97e71cd660845f353c"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v8.16.0","labelRegex":"^v8.16.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/188181","number":188181,"mergeCommit":{"message":"[Infra]
    Limit the number of metrics accepted by Snapshot API (#188181)\n\npart
    of [3628](https://github.com/elastic/observability-dev/issues/3628)\r\n-
    private\r\n\r\n\r\n## Summary\r\n\r\nAfter adding 20 items, users can no
    longer add more metrics and will see\r\nthe \"Add metric\" button
    disabled with a tooltip\r\n\r\n<img width=\"1713\"
    alt=\"image\"\r\nsrc=\"https://github.com/user-attachments/assets/c784b08b-e118-4491-b53d-46bfde898216\">\r\n\r\n\r\n###
    How to test\r\n\r\n- Start a local Kibana instance pointing to an oblt
    cluster\r\n- Navigate to Infrastructure\r\n- Try to add more than 20
    metrics in the Metrics
    dropdown.","sha":"f2d1a8b6d24486cedb0dad97e71cd660845f353c"}},{"url":"https://github.com/elastic/kibana/pull/188231","number":188231,"branch":"8.15","state":"OPEN"}]}]
    BACKPORT-->

--- x-pack/plugins/infra/common/constants.ts
+++ x-pack/plugins/infra/common/constants.ts
@@ -16,3 +16,5 @@ export const METRICS_FEATURE_ID = 'infrastructure';
 export const LOGS_FEATURE_ID = 'logs';
 
 export type InfraFeatureId = typeof METRICS_FEATURE_ID | typeof LOGS_FEATURE_ID;
+
+export const SNAPSHOT_API_MAX_METRICS = 20;
