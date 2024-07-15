c1979937fb0c4f55692f795601e9aa0a628b617d
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2024-07-15 11:33:17 +0100

    [Security Solution][Detection Engine] removes feature flag for ES|QL suppression in 8.15

--- x-pack/plugins/security_solution/public/detection_engine/rule_creation_ui/components/step_define_rule/use_experimental_feature_fields_transform.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_creation_ui/components/step_define_rule/use_experimental_feature_fields_transform.ts
@@ -8,7 +8,7 @@
 import { useCallback } from 'react';
 import type { DefineStepRule } from '../../../../detections/pages/detection_engine/rules/types';
 import { useIsExperimentalFeatureEnabled } from '../../../../common/hooks/use_experimental_features';
-import { isEsqlRule, isMlRule } from '../../../../../common/detection_engine/utils';
+import { isMlRule } from '../../../../../common/detection_engine/utils';
 
 /**
  * transforms  DefineStepRule fields according to experimental feature flags
@@ -19,15 +19,11 @@ export const useExperimentalFeatureFieldsTransform = <T extends Partial<DefineSt
   const isAlertSuppressionForMachineLearningRuleEnabled = useIsExperimentalFeatureEnabled(
     'alertSuppressionForMachineLearningRuleEnabled'
   );
-  const isAlertSuppressionForEsqlRuleEnabled = useIsExperimentalFeatureEnabled(
-    'alertSuppressionForEsqlRuleEnabled'
-  );
 
   const transformer = useCallback(
     (fields: T) => {
       const isSuppressionDisabled =
-        (isMlRule(fields.ruleType) && !isAlertSuppressionForMachineLearningRuleEnabled) ||
-        (isEsqlRule(fields.ruleType) && !isAlertSuppressionForEsqlRuleEnabled);
+        isMlRule(fields.ruleType) && !isAlertSuppressionForMachineLearningRuleEnabled;
 
       // reset any alert suppression values hidden behind feature flag
       if (isSuppressionDisabled) {
@@ -42,7 +38,7 @@ export const useExperimentalFeatureFieldsTransform = <T extends Partial<DefineSt
 
       return fields;
     },
-    [isAlertSuppressionForEsqlRuleEnabled, isAlertSuppressionForMachineLearningRuleEnabled]
+    [isAlertSuppressionForMachineLearningRuleEnabled]
   );
 
   return transformer;
--- x-pack/test/security_solution_api_integration/config/ess/config.base.ts
+++ x-pack/test/security_solution_api_integration/config/ess/config.base.ts
@@ -81,7 +81,6 @@ export function createTestConfig(options: CreateTestConfigOptions, testFiles?: s
           '--xpack.ruleRegistry.unsafe.legacyMultiTenancy.enabled=true',
           `--xpack.securitySolution.enableExperimental=${JSON.stringify([
             'previewTelemetryUrlEnabled',
-            'alertSuppressionForEsqlRuleEnabled',
             'riskScoringPersistence',
             'riskScoringRoutesEnabled',
             'bulkCustomHighlightedFieldsEnabled',
