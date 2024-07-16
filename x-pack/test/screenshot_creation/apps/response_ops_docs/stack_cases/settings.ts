0d97e8c379be95a57e1eb2500458716782c585f7
lcawl <lcawley@elastic.co>
2024-07-15 18:39:08 -0700

    Roll back changes for custom fields

--- x-pack/test/screenshot_creation/apps/response_ops_docs/stack_cases/settings.ts
+++ x-pack/test/screenshot_creation/apps/response_ops_docs/stack_cases/settings.ts
@@ -12,10 +12,11 @@ export default function ({ getPageObject, getService }: FtrProviderContext) {
   const testSubjects = getService('testSubjects');
   const screenshotDirectories = ['response_ops_docs', 'stack_cases'];
 
-  describe('add custom fields', function () {
+  describe('case settings', function () {
     it('case settings screenshot', async () => {
       await cases.navigation.navigateToApp();
       await cases.navigation.navigateToConfigurationPage();
+      await commonScreenshots.takeScreenshot('cases-settings', screenshotDirectories, 1400, 1024);
       await testSubjects.click('add-template');
       await commonScreenshots.takeScreenshot(
         'cases-templates-add',
@@ -24,20 +25,20 @@ export default function ({ getPageObject, getService }: FtrProviderContext) {
         1000
       );
       await testSubjects.click('common-flyout-cancel');
-      await testSubjects.click('add-custom-field');
-      await commonScreenshots.takeScreenshot(
-        'cases-custom-fields-add',
-        screenshotDirectories,
-        1400,
-        700
-      );
-      await testSubjects.setValue('custom-field-label-input', 'my-field');
-      await testSubjects.click('common-flyout-save');
-      await commonScreenshots.takeScreenshot('cases-settings', screenshotDirectories, 1400, 1024);
-      await cases.navigation.navigateToApp();
-      await testSubjects.click('createNewCaseBtn');
-      await commonScreenshots.takeScreenshot('cases-create', screenshotDirectories, 1400, 1900);
-      await testSubjects.click('create-case-cancel');
+      // await testSubjects.click('add-custom-field');
+      // await commonScreenshots.takeScreenshot(
+      //   'cases-custom-fields-add',
+      //   screenshotDirectories,
+      //   1400,
+      //   700
+      // );
+      // await testSubjects.setValue('custom-field-label-input', 'my-field');
+      // await testSubjects.click('common-flyout-save');
+      // await commonScreenshots.takeScreenshot('cases-settings', screenshotDirectories, 1400, 1024);
+      // await cases.navigation.navigateToApp();
+      // await testSubjects.click('createNewCaseBtn');
+      // await commonScreenshots.takeScreenshot('cases-create', screenshotDirectories, 1400, 1900);
+      // await testSubjects.click('create-case-cancel');
     });
   });
 }
