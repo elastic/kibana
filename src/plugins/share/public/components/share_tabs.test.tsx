ce0158547513870dd1b22d7b5d136d995b7778f8
rshen91 <rshen@elastic.co>
2024-07-16 14:13:00 -0600

    clean up test

--- src/plugins/share/public/components/share_tabs.test.tsx
+++ src/plugins/share/public/components/share_tabs.test.tsx
@@ -10,21 +10,65 @@ import React from 'react';
 import { ShareMenuTabs } from './share_tabs';
 import { ShareTabsContext } from './context';
 import { mountWithIntl } from '@kbn/test-jest-helpers';
-
+import { KibanaLocation, LocatorGetUrlParams, UrlService } from '../../common/url_service';
+import {
+  BrowserShortUrlClient,
+  BrowserShortUrlClientHttp,
+} from '../url_service/short_urls/short_url_client';
+import {
+  BrowserShortUrlClientFactoryCreateParams,
+  BrowserShortUrlClientFactory,
+} from '../url_service/short_urls/short_url_client_factory';
+import { themeServiceMock } from '@kbn/core-theme-browser-mocks';
+import { i18nServiceMock } from '@kbn/core-i18n-browser-mocks';
+import { toastsServiceMock } from '@kbn/core-notifications-browser-mocks/src/toasts_service.mock';
+const navigate = jest.fn(async () => {});
+const getUrl = jest.fn(
+  async (location: KibanaLocation, params: LocatorGetUrlParams): Promise<string> => {
+    return `${params.absolute ? 'https://example.com' : ''}/xyz/${location.app}/${location.path}`;
+  }
+);
+const http: BrowserShortUrlClientHttp = {
+  basePath: {
+    get: () => '/xyz',
+  },
+  fetch: jest.fn(async () => {
+    return {} as any;
+  }),
+};
+const service = new UrlService<BrowserShortUrlClientFactoryCreateParams, BrowserShortUrlClient>({
+  baseUrl: '/xyz',
+  version: '1.2.3',
+  navigate,
+  getUrl,
+  shortUrls: ({ locators }) =>
+    new BrowserShortUrlClientFactory({
+      http,
+      locators,
+    }),
+});
 const mockShareContext = {
   allowEmbed: true,
   allowShortUrl: true,
-  anonymousAccess: { getCapabilities: jest.fn() },
-  urlService: {},
+  anonymousAccess: { getCapabilities: jest.fn(), getState: jest.fn() },
+  urlService: service,
   isEmbedded: true,
-  theme: {},
+  theme: themeServiceMock.createStartContract(),
   objectTypeMeta: { title: 'title' },
+  objectType: 'type',
+  sharingData: { title: 'title', url: 'url' },
+  isDirty: false,
+  onClose: jest.fn(),
+  toasts: toastsServiceMock.createStartContract(),
+  i18n: i18nServiceMock.createStartContract(),
 };
-
+const CSV = 'CSV' as const;
 describe('Share modal tabs', () => {
   it('should render export tab when there are share menu items that are not disabled', async () => {
-    const testItem = [{ shareMenuItem: { disabled: false } }];
-    const wrapper = await mountWithIntl(
+    const testItem = [
+      { shareMenuItem: { name: 'test', disabled: false }, label: CSV, generateExport: jest.fn() },
+    ];
+    const wrapper = mountWithIntl(
       <ShareTabsContext.Provider value={{ ...mockShareContext, shareMenuItems: testItem }}>
         <ShareMenuTabs />
       </ShareTabsContext.Provider>
@@ -32,8 +76,10 @@ describe('Share modal tabs', () => {
     expect(wrapper.find('[data-test-subj="export"]').exists()).toBeTruthy();
   });
   it('should not render export tab when the license is disabled', async () => {
-    const testItems = [{ shareMenuItem: { disabled: true } }];
-    const wrapper = await mountWithIntl(
+    const testItems = [
+      { shareMenuItem: { name: 'test', disabled: true }, label: CSV, generateExport: jest.fn() },
+    ];
+    const wrapper = mountWithIntl(
       <ShareTabsContext.Provider value={{ ...mockShareContext, shareMenuItems: testItems }}>
         <ShareMenuTabs />
       </ShareTabsContext.Provider>
