01a223ae649401f309873c7f9c8fcc2b1bcede0e
Jean-Louis Leysens <jloleysens@gmail.com>
2024-07-17 10:52:38 +0200

    private TS -> private identifier syntax

--- packages/core/metrics/core-metrics-collectors-server-internal/src/load_window.ts
+++ packages/core/metrics/core-metrics-collectors-server-internal/src/load_window.ts
@@ -12,18 +12,18 @@ import assert from 'node:assert';
 const twoDeci = (num: number) => Math.ceil(num * 100) / 100;
 
 export class LoadWindow {
-  private readonly window: number[];
+  readonly #window: number[];
 
   constructor(private readonly size: number) {
-    this.window = new Array(size).fill(0);
+    this.#window = new Array(size).fill(0);
   }
 
   addObservation(value: number) {
-    this.window.unshift(Math.max(0, value));
-    this.window.pop();
+    this.#window.unshift(Math.max(0, value));
+    this.#window.pop();
     assert(
-      this.window.length === this.size,
-      `Expected window size of ${this.size}, but received ${this.window.length}`
+      this.#window.length === this.size,
+      `Expected window size of ${this.size}, but received ${this.#window.length}`
     );
   }
 
@@ -33,7 +33,7 @@ export class LoadWindow {
   getAverage(includeObservations: number) {
     includeObservations = Math.min(Math.max(1, includeObservations), this.size);
     return twoDeci(
-      this.window.slice(0, includeObservations).reduce((acc, val) => acc + val, 0) /
+      this.#window.slice(0, includeObservations).reduce((acc, val) => acc + val, 0) /
         includeObservations
     );
   }
