fd2a94d27ff27dc1bb1f03ec31770a3e84c40caa
Sébastien Loix <sebastien.loix@elastic.co>
2024-07-17 17:39:05 +0100

    [Stateful sidenav] Set solution default route (#187763)

--- packages/core/ui-settings/core-ui-settings-server-internal/src/clients/ui_settings_defaults_client.test.ts
+++ packages/core/ui-settings/core-ui-settings-server-internal/src/clients/ui_settings_defaults_client.test.ts
@@ -62,6 +62,43 @@ describe('ui settings defaults', () => {
         result.foo = 'bar';
       }).toThrow();
     });
+
+    it('returns default values from async getValue() handler', async () => {
+      const defaults = {
+        foo: {
+          schema: schema.string(),
+          getValue: async () => {
+            // simulate async operation
+            await new Promise((resolve) => setTimeout(resolve, 200));
+            return 'default foo';
+          },
+        },
+        baz: { schema: schema.string(), value: 'default baz' },
+      };
+
+      const uiSettings = new UiSettingsDefaultsClient({ defaults, log: logger });
+
+      await expect(uiSettings.getAll()).resolves.toStrictEqual({
+        foo: 'default foo',
+        baz: 'default baz',
+      });
+    });
+
+    it('pass down the context object to the getValue() handler', async () => {
+      const getValue = jest.fn().mockResolvedValue('default foo');
+      const defaults = {
+        foo: {
+          schema: schema.string(),
+          getValue,
+        },
+      };
+
+      const uiSettings = new UiSettingsDefaultsClient({ defaults, log: logger });
+
+      const context = { foo: 'bar' };
+      await uiSettings.getAll(context as any);
+      expect(getValue).toHaveBeenCalledWith(context);
+    });
   });
 
   describe('#get()', () => {
@@ -93,6 +130,38 @@ describe('ui settings defaults', () => {
       const uiSettings = new UiSettingsDefaultsClient({ defaults, overrides, log: logger });
 
       await expect(uiSettings.get('foo')).resolves.toBe('default foo');
+    });
+
+    it('returns default values from async getValue() handler', async () => {
+      const defaults = {
+        foo: {
+          schema: schema.string(),
+          getValue: async () => {
+            // simulate async operation
+            await new Promise((resolve) => setTimeout(resolve, 200));
+            return 'default foo';
+          },
+        },
+      };
+
+      const uiSettings = new UiSettingsDefaultsClient({ defaults, log: logger });
+      await expect(uiSettings.get('foo')).resolves.toBe('default foo');
+    });
+
+    it('pass down the context object to the getValue() handler', async () => {
+      const getValue = jest.fn().mockResolvedValue('default foo');
+      const defaults = {
+        foo: {
+          schema: schema.string(),
+          getValue,
+        },
+      };
+
+      const uiSettings = new UiSettingsDefaultsClient({ defaults, log: logger });
+
+      const context = { foo: 'bar' };
+      await uiSettings.get('foo', context as any);
+      expect(getValue).toHaveBeenCalledWith(context);
     });
   });
 
