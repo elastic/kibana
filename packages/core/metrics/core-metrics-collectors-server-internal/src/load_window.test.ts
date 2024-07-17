6524491bb4a91ce2ce5d37aaef129c37e20a4659
Jean-Louis Leysens <jloleysens@gmail.com>
2024-07-17 10:10:55 +0200

    added LoadWindow

---
+++ packages/core/metrics/core-metrics-collectors-server-internal/src/load_window.test.ts
@@ -0,0 +1,61 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { LoadWindow } from './load_window';
+
+describe('LoadWindow', () => {
+  it('#getAverage should work without any observations', () => {
+    const lw = new LoadWindow(3);
+    expect(lw.getAverage(1)).toBe(0);
+  });
+  it.each([
+    [-1000],
+    [-1],
+    [0],
+    [9999],
+    // [NaN] assuming this is nonsense input
+  ])('#getAverage works given bad input: %s', (badInput) => {
+    const lw = new LoadWindow(3);
+    expect(lw.getAverage(badInput)).toBe(0);
+  });
+
+  const WINDOW_SIZE = 3;
+  it.each([
+    { observations: [0.44, 0.55, 0.66], averageLast: 3, expected: 0.55 },
+    { observations: [0.44, 0.55, 0.66].reverse(), averageLast: 3, expected: 0.55 }, // should be same as above
+    { observations: [0.44, 0.55, 0.66], averageLast: 1, expected: 0.44 },
+    { observations: [0.201, 0.33, 0.44], averageLast: 3, expected: 0.33 },
+    { observations: [0.44, 0.55, 0.66], averageLast: 2, expected: 0.5 },
+    { observations: [0.44, 0.55, 0.66], averageLast: -1, expected: 0.44 },
+    {
+      observations: [1, 0.99, 0.55, 0.66, 0.44, 0.55, 0.66, 0.77],
+      averageLast: 1000,
+      expected: 0.85,
+    },
+    {
+      observations: [-1, -0.99, -0.55, -0.66, -0.44],
+      averageLast: 10000,
+      expected: 0,
+    },
+    {
+      observations: [1, 0.99, 0.55, 0.66],
+      averageLast: 1,
+      expected: 1,
+    },
+  ])(
+    '#getAverage produces the correct average for observations: $observations',
+    ({ observations, averageLast, expected }) => {
+      const lw = new LoadWindow(WINDOW_SIZE);
+      // reverse so that our test observations are in the order they appear above
+      for (const observation of observations.reverse()) {
+        lw.addObservation(observation);
+      }
+      expect(lw.getAverage(averageLast)).toBe(expected);
+    }
+  );
+});
---
+++ packages/core/metrics/core-metrics-collectors-server-internal/src/load_window.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import assert from 'node:assert';
+
+/** We .ciel to rather _slightly_ over-report usage in certain circumstances */
+const twoDeci = (num: number) => Math.ceil(num * 100) / 100;
+
+export class LoadWindow {
+  private readonly window: number[];
+
+  constructor(private readonly size: number) {
+    this.window = new Array(size).fill(0);
+  }
+
+  addObservation(value: number) {
+    this.window.unshift(Math.max(0, value));
+    this.window.pop();
+    assert(
+      this.window.length === this.size,
+      `Expected window size of ${this.size}, but received ${this.window.length}`
+    );
+  }
+
+  /**
+   * @param includeObservations number of observations to include in calculation. Will be normalized to be within the window size.
+   */
+  getAverage(includeObservations: number) {
+    includeObservations = Math.min(Math.max(1, includeObservations), this.size);
+    return twoDeci(
+      this.window.slice(0, includeObservations).reduce((acc, val) => acc + val, 0) /
+        includeObservations
+    );
+  }
+}
