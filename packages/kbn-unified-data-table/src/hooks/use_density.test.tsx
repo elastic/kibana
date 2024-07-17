61e9849b16f64e6390cd83144f73b5101322f51b
Lukas Olson <lukas@elastic.co>
2024-07-17 16:29:20 -0700

    Fill out unit tests

--- packages/kbn-unified-data-table/src/hooks/use_density.test.tsx
+++ packages/kbn-unified-data-table/src/hooks/use_density.test.tsx
@@ -6,10 +6,73 @@
  * Side Public License, v 1.
  */
 
+import type { EuiDataGridStyle } from '@elastic/eui';
+import type { Storage } from '@kbn/kibana-utils-plugin/public';
+import { renderHook } from '@testing-library/react-hooks';
+import { useDensity } from './use_density';
+
+const localStorageMock = {
+  get: jest.fn(),
+  set: jest.fn(),
+};
+
 describe('useDensity', () => {
-  it('should apply density from local storage', () => {});
+  beforeEach(() => {
+    localStorageMock.get.mockClear();
+    localStorageMock.set.mockClear();
+  });
 
-  it('should apply density from configDensity', () => {});
+  it('should read from local storage', () => {
+    localStorageMock.get.mockReturnValue({ foo: 'bar' });
+    const { result } = renderHook(() =>
+      useDensity({
+        storage: localStorageMock as unknown as Storage,
+        consumer: 'discover',
+      })
+    );
+    const {
+      current: { density },
+    } = result;
+    expect(density).toMatchInlineSnapshot(`
+      Object {
+        "foo": "bar",
+      }
+    `);
+  });
 
-  it('should return onChangeDensity', () => {});
+  it('should update local storage when onChangeDensity is called', () => {
+    const { result } = renderHook(() =>
+      useDensity({
+        storage: localStorageMock as unknown as Storage,
+        consumer: 'discover',
+      })
+    );
+    const {
+      current: { onChangeDensity },
+    } = result;
+
+    const newValue: EuiDataGridStyle = { border: 'all', footer: 'shade' };
+    onChangeDensity(newValue);
+
+    expect(localStorageMock.set).toBeCalledWith('discover:dataGridStyle', newValue);
+  });
+
+  it('should call provided onUpdateDensity with the updated value', () => {
+    const onUpdateDensity = jest.fn();
+    const { result } = renderHook(() =>
+      useDensity({
+        storage: localStorageMock as unknown as Storage,
+        consumer: 'discover',
+        onUpdateDensity,
+      })
+    );
+    const {
+      current: { onChangeDensity },
+    } = result;
+
+    const newValue: EuiDataGridStyle = { border: 'all', footer: 'shade' };
+    onChangeDensity(newValue);
+
+    expect(onUpdateDensity).toBeCalledWith(newValue);
+  });
 });
