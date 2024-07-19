59a14f8c516502039aa06467279d31523f8c2b62
Jean-Louis Leysens <jloleysens@gmail.com>
2024-07-19 14:09:42 +0200

    rename variable and simplify code!

--- packages/core/metrics/core-metrics-server-internal/src/routes/elu_load.ts
+++ packages/core/metrics/core-metrics-server-internal/src/routes/elu_load.ts
@@ -6,12 +6,12 @@
  * Side Public License, v 1.
  */
 
-import { IRouter } from '@kbn/core-http-server';
-import { performance, EventLoopUtilization } from 'perf_hooks';
+import type { IRouter } from '@kbn/core-http-server';
+import type { OpsMetrics } from '@kbn/core-metrics-server';
 import type { Observable } from 'rxjs';
 import { LoadWindow } from './load_window';
 
-interface Response {
+interface ELULoadResponse {
   /**
    * Event-loop utilization averaged over a set of sample buckets
    * @remark 3 load windows borrows from the `os` module's concept of load but is not necessarily windows of 1m, 5m, 15m. The
@@ -35,13 +35,11 @@ const LOAD_WINDOW_SIZE_LONG = 12;
 /**
  * Intended for exposing metrics over HTTP that we do not want to include in the /api/stats endpoint, yet.
  */
-export function registerEluLoadRoute(router: IRouter, tick$: Observable<unknown>) {
-  let elu: undefined | EventLoopUtilization;
-  const loadWindow = new LoadWindow(LOAD_WINDOW_SIZE_LONG);
+export function registerEluLoadRoute(router: IRouter, metrics$: Observable<OpsMetrics>) {
+  const eluLoadWindow = new LoadWindow(LOAD_WINDOW_SIZE_LONG);
 
-  tick$.subscribe(() => {
-    elu = performance.eventLoopUtilization(elu);
-    loadWindow.addObservation(elu.utilization);
+  metrics$.subscribe((metrics) => {
+    eluLoadWindow.addObservation(metrics.process.event_loop_utilization.utilization);
   });
 
   router.versioned
@@ -58,11 +56,11 @@ export function registerEluLoadRoute(router: IRouter, tick$: Observable<unknown>
         validate: false,
       },
       async (ctx, req, res) => {
-        const body: Response = {
+        const body: ELULoadResponse = {
           load: {
-            short: loadWindow.getAverage(LOAD_WINDOW_SIZE_SHORT),
-            medium: loadWindow.getAverage(LOAD_WINDOW_SIZE_MED),
-            long: loadWindow.getAverage(LOAD_WINDOW_SIZE_LONG),
+            short: eluLoadWindow.getAverage(LOAD_WINDOW_SIZE_SHORT),
+            medium: eluLoadWindow.getAverage(LOAD_WINDOW_SIZE_MED),
+            long: eluLoadWindow.getAverage(LOAD_WINDOW_SIZE_LONG),
           },
         };
         return res.ok({ body });
