193af7d0f3c0258b2617e6059b612504cd428eb2
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2024-07-19 14:21:19 +0100

    Update config.base.ts

--- x-pack/test/security_solution_api_integration/config/ess/config.base.ts
+++ x-pack/test/security_solution_api_integration/config/ess/config.base.ts
@@ -82,7 +82,6 @@ export function createTestConfig(options: CreateTestConfigOptions, testFiles?: s
             'previewTelemetryUrlEnabled',
             'riskScoringPersistence',
             'riskScoringRoutesEnabled',
-            'bulkCustomHighlightedFieldsEnabled',
             'manualRuleRunEnabled',
           ])}`,
           '--xpack.task_manager.poll_interval=1000',
