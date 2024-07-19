6a3bb12ece04b371cf749ea255f471a1a80b4363
Elena Shostak <elena.shostak@elastic.co>
2024-07-19 11:17:55 +0200

    Polishment

--- x-pack/plugins/security/server/authorization/api_authorization.ts
+++ x-pack/plugins/security/server/authorization/api_authorization.ts
@@ -23,67 +23,9 @@ export function initAPIAuthorization(
 
     const authz = request.route.options.authz;
 
-    const checkAuthz = async (apiActions: string[]) => {
-      const checkPrivileges = checkPrivilegesDynamicallyWithRequest(request);
-      const checkPrivilegesResponse = await checkPrivileges({ kibana: apiActions });
-      const kibanaPrivileges: Record<string, boolean> = {};
-
-      if (authz) {
-        for (const kbPrivilege of checkPrivilegesResponse.privileges.kibana) {
-          kibanaPrivileges[kbPrivilege.privilege.replace('api:', '')] = kbPrivilege.authorized;
-        }
-
-        for (const requiredPrivilege of authz.requiredPrivileges) {
-          if (typeof requiredPrivilege === 'string' && !kibanaPrivileges[requiredPrivilege]) {
-            logger.warn(
-              `User not authorized for "${request.url.pathname}${request.url.search}": responding with 403`
-            );
-            return response.forbidden();
-          }
-
-          if (
-            typeof requiredPrivilege === 'object' &&
-            (!requiredPrivilege.offering || requiredPrivilege.offering === buildFlavor)
-          ) {
-            const allRequired = requiredPrivilege.allRequired ?? [];
-            const anyRequired = requiredPrivilege.anyRequired ?? [];
-
-            if (
-              !allRequired.every((privilege) => kibanaPrivileges[privilege]) &&
-              !anyRequired.some((privilege) => kibanaPrivileges[privilege])
-            ) {
-              logger.warn(
-                `User not authorized for "${request.url.pathname}${request.url.search}": responding with 403`
-              );
-              return response.forbidden();
-            }
-          }
-        }
-
-        if (authz?.passThrough) {
-          Object.defineProperty(request, 'authzResponse', {
-            value: deepFreeze(checkPrivilegesResponse.privileges.kibana),
-            enumerable: false,
-            configurable: false,
-            writable: false,
-          });
-        }
-
-        return toolkit.next();
-      }
-
-      // we've actually authorized the request
-      if (checkPrivilegesResponse.hasAllRequested) {
-        logger.debug(`User authorized for "${request.url.pathname}${request.url.search}"`);
-        return toolkit.next();
-      }
-
-      logger.warn(
-        `User not authorized for "${request.url.pathname}${request.url.search}": responding with 403`
-      );
-      return response.forbidden();
-    };
-
+    /**
+     * Please note, that this code was intended for POC demo purposes only and is not production-ready.
+     */
     if (authz) {
       const requiredPrivileges = authz.requiredPrivileges.flatMap((privilegeEntry) => {
         if (typeof privilegeEntry === 'string') {
@@ -101,7 +43,51 @@ export function initAPIAuthorization(
       });
       const apiActions = requiredPrivileges.map((permission) => actions.api.get(permission));
 
-      return await checkAuthz(apiActions);
+      const checkPrivileges = checkPrivilegesDynamicallyWithRequest(request);
+      const checkPrivilegesResponse = await checkPrivileges({ kibana: apiActions });
+      const kibanaPrivileges: Record<string, boolean> = {};
+
+      for (const kbPrivilege of checkPrivilegesResponse.privileges.kibana) {
+        kibanaPrivileges[kbPrivilege.privilege.replace('api:', '')] = kbPrivilege.authorized;
+      }
+
+      for (const requiredPrivilege of authz.requiredPrivileges) {
+        if (typeof requiredPrivilege === 'string' && !kibanaPrivileges[requiredPrivilege]) {
+          logger.warn(
+            `User not authorized for "${request.url.pathname}${request.url.search}": responding with 403`
+          );
+          return response.forbidden();
+        }
+
+        if (
+          typeof requiredPrivilege === 'object' &&
+          (!requiredPrivilege.offering || requiredPrivilege.offering === buildFlavor)
+        ) {
+          const allRequired = requiredPrivilege.allRequired ?? [];
+          const anyRequired = requiredPrivilege.anyRequired ?? [];
+
+          if (
+            !allRequired.every((privilege) => kibanaPrivileges[privilege]) &&
+            !anyRequired.some((privilege) => kibanaPrivileges[privilege])
+          ) {
+            logger.warn(
+              `User not authorized for "${request.url.pathname}${request.url.search}": responding with 403`
+            );
+            return response.forbidden();
+          }
+        }
+      }
+
+      if (authz.passThrough) {
+        Object.defineProperty(request, 'authzResponse', {
+          value: deepFreeze(checkPrivilegesResponse.privileges.kibana),
+          enumerable: false,
+          configurable: false,
+          writable: false,
+        });
+      }
+
+      return toolkit.next();
     }
 
     const tags = request.route.options.tags;
@@ -115,6 +101,18 @@ export function initAPIAuthorization(
 
     const apiActions = actionTags.map((tag) => actions.api.get(tag.substring(tagPrefix.length)));
 
-    return await checkAuthz(apiActions);
+    const checkPrivileges = checkPrivilegesDynamicallyWithRequest(request);
+    const checkPrivilegesResponse = await checkPrivileges({ kibana: apiActions });
+
+    // we've actually authorized the request
+    if (checkPrivilegesResponse.hasAllRequested) {
+      logger.debug(`User authorized for "${request.url.pathname}${request.url.search}"`);
+      return toolkit.next();
+    }
+
+    logger.warn(
+      `User not authorized for "${request.url.pathname}${request.url.search}": responding with 403`
+    );
+    return response.forbidden();
   });
 }
