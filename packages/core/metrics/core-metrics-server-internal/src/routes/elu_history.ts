0c1e79b554e4fd488bd153533ee1e2fc9f834d5f
Jean-Louis Leysens <jloleysens@gmail.com>
2024-07-22 16:48:53 +0200

    OK, for realz

--- packages/core/metrics/core-metrics-server-internal/src/routes/elu_history.ts
+++ packages/core/metrics/core-metrics-server-internal/src/routes/elu_history.ts
@@ -11,7 +11,7 @@ import type { OpsMetrics } from '@kbn/core-metrics-server';
 import type { Observable } from 'rxjs';
 import { HistoryWindow } from './history_window';
 
-interface ELULoadResponse {
+interface ELUHistoryResponse {
   /**
    * Event-loop utilization averaged over a set of sample buckets
    * @remark 3 windows borrows from the `os` module's concept of load but is not necessarily windows of 1m, 5m, 15m. The
@@ -19,11 +19,11 @@ interface ELULoadResponse {
    *         and the number of samples held in each window. So by default short: 15s, medium: 30s and long 60s.
    */
   history: {
-    /** The load for the short window */
+    /** The history for the short window */
     short: number;
-    /** The load for the medium window */
+    /** The history for the medium window */
     medium: number;
-    /** The load for the long window */
+    /** The history for the long window */
     long: number;
   };
 }
@@ -36,10 +36,10 @@ const HISTORY_WINDOW_SIZE_LONG = 12;
  * Intended for exposing metrics over HTTP that we do not want to include in the /api/stats endpoint, yet.
  */
 export function registerEluHistoryRoute(router: IRouter, metrics$: Observable<OpsMetrics>) {
-  const eluLoadWindow = new HistoryWindow(HISTORY_WINDOW_SIZE_LONG);
+  const eluHistoryWindow = new HistoryWindow(HISTORY_WINDOW_SIZE_LONG);
 
   metrics$.subscribe((metrics) => {
-    eluLoadWindow.addObservation(metrics.process.event_loop_utilization.utilization);
+    eluHistoryWindow.addObservation(metrics.process.event_loop_utilization.utilization);
   });
 
   router.versioned
@@ -56,11 +56,11 @@ export function registerEluHistoryRoute(router: IRouter, metrics$: Observable<Op
         validate: false,
       },
       async (ctx, req, res) => {
-        const body: ELULoadResponse = {
+        const body: ELUHistoryResponse = {
           history: {
-            short: eluLoadWindow.getAverage(HISTORY_WINDOW_SIZE_SHORT),
-            medium: eluLoadWindow.getAverage(HISTORY_WINDOW_SIZE_MED),
-            long: eluLoadWindow.getAverage(HISTORY_WINDOW_SIZE_LONG),
+            short: eluHistoryWindow.getAverage(HISTORY_WINDOW_SIZE_SHORT),
+            medium: eluHistoryWindow.getAverage(HISTORY_WINDOW_SIZE_MED),
+            long: eluHistoryWindow.getAverage(HISTORY_WINDOW_SIZE_LONG),
           },
         };
         return res.ok({ body });
