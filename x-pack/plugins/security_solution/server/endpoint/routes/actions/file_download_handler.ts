7deedc28c49ff42f1699ce50dad91e510838d8f4
Paul Tavares <paul.tavares@elastic.co>
2024-07-22 08:50:23 -0400

    remove todo comment

--- x-pack/plugins/security_solution/server/endpoint/routes/actions/file_download_handler.ts
+++ x-pack/plugins/security_solution/server/endpoint/routes/actions/file_download_handler.ts
@@ -47,7 +47,6 @@ export const registerActionFileDownloadRoutes = (
         },
       },
       withEndpointAuthz(
-        // FIXME:PT this code shows a bug for any aciton that supports a download, but role does not have File Operations (ex. execute)
         { all: ['canWriteFileOperations'] },
         logger,
         getActionFileDownloadRouteHandler(endpointContext)
