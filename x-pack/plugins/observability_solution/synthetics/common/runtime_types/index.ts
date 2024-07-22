4b8b3b7ca53c9f2cb0ae30ff150bac3e0b5a8209
Dominique Belcher <dominique.clarke@elastic.co>
2024-07-21 22:26:17 -0400

    restore import

--- x-pack/plugins/observability_solution/synthetics/common/runtime_types/index.ts
+++ x-pack/plugins/observability_solution/synthetics/common/runtime_types/index.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
+export * from './alerts';
 export * from './certs';
 export * from './common';
 export * from './dynamic_settings';
