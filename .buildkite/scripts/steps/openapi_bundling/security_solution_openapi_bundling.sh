ba1c6f065999ee2c4683319eaade48437ea6bbeb
Maxim Palenov <maxim.palenov@elastic.co>
2024-07-22 15:21:17 +0200

    add Timeline API bundling into the pipeline script

--- .buildkite/scripts/steps/openapi_bundling/security_solution_openapi_bundling.sh
+++ .buildkite/scripts/steps/openapi_bundling/security_solution_openapi_bundling.sh
@@ -6,11 +6,22 @@ source .buildkite/scripts/common/util.sh
 
 echo --- Security Solution OpenAPI Bundling
 
-(cd x-pack/plugins/security_solution && yarn openapi:bundle)
-check_for_changed_files "yarn openapi:bundle" true
+echo -e "\n[Security Solution OpenAPI Bundling] Detections API\n"
+
+(cd x-pack/plugins/security_solution && yarn openapi:bundle:detections)
+check_for_changed_files "yarn openapi:bundle:detections" true
+
+echo -e "\n[Security Solution OpenAPI Bundling] Timeline API\n"
+
+(cd x-pack/plugins/security_solution && yarn openapi:bundle:timeline)
+check_for_changed_files "yarn openapi:bundle:timeline" true
+
+echo -e "\n[Security Solution OpenAPI Bundling] Lists API\n"
 
 (cd packages/kbn-securitysolution-lists-common && yarn openapi:bundle)
 check_for_changed_files "yarn openapi:bundle" true
+
+echo -e "\n[Security Solution OpenAPI Bundling] Exceptions API\n"
 
 (cd packages/kbn-securitysolution-exceptions-common && yarn openapi:bundle)
 check_for_changed_files "yarn openapi:bundle" true
