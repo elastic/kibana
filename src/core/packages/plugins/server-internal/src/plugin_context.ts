35ab6d4f3c5379ae406868da587074406d0c703e
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-07-23 16:23:44 +0200

    Simplify DI service API

--- src/core/packages/plugins/server-internal/src/plugin_context.ts
+++ src/core/packages/plugins/server-internal/src/plugin_context.ts
@@ -305,7 +305,7 @@ export function createPluginSetupContext<TPlugin, TPluginDependencies>({
         deps.userProfile.registerUserProfileDelegate(delegate),
     },
     injection: {
-      load: deps.injection.load.bind(deps.injection, plugin.opaqueId),
+      getContainer: deps.injection.getContainer.bind(deps.injection, plugin.opaqueId),
     },
   };
 }
