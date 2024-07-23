9f5118e4b11d5da6b41a25858eccd0b1e5066b9a
Sean Rathier <sean.rathier@elastic.co>
2024-07-23 15:22:19 -0400

    Cypress test working to add agentless policy

--- x-pack/plugins/fleet/cypress/e2e/agentless_policy.cy.ts
+++ x-pack/plugins/fleet/cypress/e2e/agentless_policy.cy.ts
@@ -4,22 +4,29 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import {
   ADD_INTEGRATION_POLICY_BTN,
   CREATE_PACKAGE_POLICY_SAVE_BTN,
 } from '../screens/integrations';
-import { cleanupAgentPolicies } from '../tasks/cleanup';
+// import { cleanupAgentPolicies } from '../tasks/cleanup';
 import { login } from '../tasks/login';
-import { deleteIntegrations } from '../tasks/integrations';
+// import { deleteIntegrations } from '../tasks/integrations';
 
 describe('Create Agentless policy', () => {
-  const agentlessIntegration = 'cloud_security_posture-1.9.0';
+  //   before(async () => {
+  //     await worker.start();
+  //   });
+
+  //   const server = setupServer(...handlers);
+
+  //   worker.start();
 
   beforeEach(() => {
     login();
     cy.intercept('POST', '/api/fleet/agent_policies?sys_monitoring=true', {
       id: 'policy-id',
-      name: agentlessIntegration,
+      name: 'some-name',
       namespace: 'default',
       monitoring_enabled: true,
       supports_agentless: true,
@@ -28,7 +35,7 @@ describe('Create Agentless policy', () => {
       package_policies: [],
       policy_ids: ['policy-id'],
       agents: ['agent-id'],
-    });
+    }).as('createAgentPolicy');
     // cy.intercept('/api/fleet/agent_policies', {
     //   items: [
     //     {
@@ -66,7 +73,12 @@ describe('Create Agentless policy', () => {
     // deleteIntegrations();
   });
 
-  afterEach(() => {});
+  //   afterEach(() => {
+  //     // server.resetHandlers();
+  //     // server.close();
+  //   });
+
+  //   after(() => worker.stop());
 
   //   it('should allow selecting agent-based option when agentless is available', () => {
   //     cy.visit('/app/integrations/detail/cloud_security_posture');
@@ -78,13 +90,57 @@ describe('Create Agentless policy', () => {
   //   });
 
   it('should allow selecting agentless option when agentless is available', () => {
-    cy.visit('/app/integrations/detail/cloud_security_posture');
+    const integrationName = `cloud_security_posture-${new Date().toISOString()}`;
+
+    cy.intercept('POST', 'api/fleet/agent_policies?sys_monitoring=true', {
+      item: {
+        id: 'policy-id',
+        name: `Agentless policy for ${integrationName}`,
+        namespace: 'default',
+        monitoring_enabled: ['logs', 'metrics'],
+        inactivity_timeout: 1209600,
+        is_protected: false,
+        supports_agentless: true,
+        status: 'active',
+        is_managed: true,
+        revision: 1,
+        updated_at: '2024-07-23T18:20:49.315Z',
+        updated_by: 'elastic',
+        schema_version: '1.1.1',
+      },
+      status: 200,
+    }).as('createAgentPolicy');
+    cy.intercept('POST', '/api/fleet/package_policies', {
+      item: {
+        id: 'package-policy-id',
+        name: integrationName,
+        description: '',
+        policy_ids: ['policy-id'],
+        spaceId: 'default',
+        inputs: [],
+        revision: 1,
+        enabled: true,
+        updated_by: 'elastic',
+        updated_at: '2024-07-23T18:20:49.315Z',
+        created_at: '2024-07-23T18:20:49.315Z',
+        created_by: 'elastic',
+      },
+    }).as('createPackagePolicies');
+
+    cy.visit('/app/integrations/detail/cloud_security_posture?');
 
     cy.getBySel(ADD_INTEGRATION_POLICY_BTN).click();
     cy.getBySel('setup-technology-selector-accordion').click();
     cy.getBySel('setup-technology-selector').click();
     cy.get('.euiContextMenuItem').contains('Agentless').click();
-    cy.get('#name').type(`cloud_security_posture-${new Date().toISOString()}`);
+    cy.get('#name').clear();
+    cy.get('#name').type(integrationName);
     cy.getBySel(CREATE_PACKAGE_POLICY_SAVE_BTN).click();
+
+    cy.wait('@createAgentPolicy').then(({ request }) => {
+      expect(request.body.name).to.eq(`Agentless policy for ${integrationName}`);
+      expect(request.body.supports_agentless).to.eq(true);
+      expect(request.body.namespace).to.eq('default');
+    });
   });
 });
