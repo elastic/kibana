6f7dfa4bb2748e66d027740775fc0e714e206afd
Brad White <brad.white@elastic.co>
2024-07-23 14:38:22 -0600

    wip bazel cache mounting

--- .devcontainer/scripts/bootstrap.sh
+++ .devcontainer/scripts/bootstrap.sh
@@ -5,3 +5,8 @@ SCRIPT_DIR=$(dirname "$0")
 
 # Source setup_shell.sh from the determined directory
 source "${SCRIPT_DIR}/setup_shell.sh"
+
+# Change ownership of the mounted .cache directory to vscode
+sudo chown -R vscode:vscode /home/vscode/.cache
+
+NODE_OPTIONS= yarn kbn bootstrap
