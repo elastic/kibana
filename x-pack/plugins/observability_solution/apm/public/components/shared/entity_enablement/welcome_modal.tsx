dc06ecb93e631859869953a3792c18330b4c3176
Kate Patticha <aikaterini.patticha@elastic.co>
2024-07-23 18:02:17 +0300

    Address PR comments

--- x-pack/plugins/observability_solution/apm/public/components/shared/entity_enablement/welcome_modal.tsx
+++ x-pack/plugins/observability_solution/apm/public/components/shared/entity_enablement/welcome_modal.tsx
@@ -41,81 +41,76 @@ export function Welcome({
 }) {
   const servicesInventory = useKibanaUrl('/plugins/apm/assets/services_inventory.png');
 
-  return (
-    <>
-      {isModalVisible && (
-        <EuiConfirmModal
-          style={{
-            width: '630px',
-          }}
-          onCancel={onClose}
-          onConfirm={onConfirm}
-          confirmButtonText={
-            <EuiButton data-test-subj="xpack.apm.welcome.button.open" fill size="s">
-              {i18n.translate('xpack.apm.welcome.button.openSurvey', {
-                defaultMessage: 'OK',
-              })}
-            </EuiButton>
-          }
-          cancelButtonText={
-            <EuiLink
-              target="_blank"
-              data-test-subj="apmWelcomeLinkExternal"
-              href="https://ela.st/new-experience-services"
-              external
-            >
-              {i18n.translate('xpack.apm.welcome.linkLabel', {
-                defaultMessage: 'Learn more',
-              })}
-            </EuiLink>
-          }
-          defaultFocusedButton="confirm"
-        >
-          <EuiPanel hasShadow={false}>
-            <EuiFlexGroup
-              direction="column"
-              justifyContent="center"
-              alignItems="center"
-              gutterSize="m"
-            >
-              <EuiFlexItem>
-                <EuiIcon type="logoElastic" size="l" />
-              </EuiFlexItem>
-              <EuiFlexItem>
-                <EuiTitle>
-                  <h2>
-                    {i18n.translate('xpack.apm.welcome.title', {
-                      defaultMessage: 'Welcome to our new experience!',
-                    })}
-                  </h2>
-                </EuiTitle>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiPanel>
+  if (!isModalVisible) {
+    return null;
+  }
 
-          <EuiPanel hasShadow={false} paddingSize="s">
-            <EuiText grow={false} textAlign="center">
-              <p>
-                {i18n.translate('xpack.apm.welcome.body', {
-                  defaultMessage:
-                    'You can now see services detected from logs alongside services instrumented with APM our new service inventory so you can view all of your services in a single place.',
+  return (
+    <EuiConfirmModal
+      style={{
+        width: '630px',
+      }}
+      onCancel={onClose}
+      onConfirm={onConfirm}
+      confirmButtonText={
+        <EuiButton data-test-subj="xpack.apm.welcome.button.open" fill size="s">
+          {i18n.translate('xpack.apm.welcome.button.openSurvey', {
+            defaultMessage: 'OK',
+          })}
+        </EuiButton>
+      }
+      cancelButtonText={
+        <EuiLink
+          target="_blank"
+          data-test-subj="apmWelcomeLinkExternal"
+          href="https://ela.st/new-experience-services"
+          external
+        >
+          {i18n.translate('xpack.apm.welcome.linkLabel', {
+            defaultMessage: 'Learn more',
+          })}
+        </EuiLink>
+      }
+      defaultFocusedButton="confirm"
+    >
+      <EuiPanel hasShadow={false}>
+        <EuiFlexGroup direction="column" justifyContent="center" alignItems="center" gutterSize="m">
+          <EuiFlexItem>
+            <EuiIcon type="logoElastic" size="l" />
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiTitle>
+              <h2>
+                {i18n.translate('xpack.apm.welcome.title', {
+                  defaultMessage: 'Welcome to our new experience!',
                 })}
-              </p>
-            </EuiText>
-          </EuiPanel>
-          <EuiSpacer size="m" />
-          <EuiPanel hasBorder paddingSize="none">
-            <EuiImage
-              size="xl"
-              src={servicesInventory}
-              alt={i18n.translate('xpack.apm.welcome.image.at,', {
-                defaultMessage:
-                  'Image of the new experience of the service inventory, showing services detected from logs and APM-instrumented services',
-              })}
-            />
-          </EuiPanel>
-        </EuiConfirmModal>
-      )}
-    </>
+              </h2>
+            </EuiTitle>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiPanel>
+
+      <EuiPanel hasShadow={false} paddingSize="s">
+        <EuiText grow={false} textAlign="center">
+          <p>
+            {i18n.translate('xpack.apm.welcome.body', {
+              defaultMessage:
+                'You can now see services detected from logs alongside services instrumented with APM our new service inventory so you can view all of your services in a single place.',
+            })}
+          </p>
+        </EuiText>
+      </EuiPanel>
+      <EuiSpacer size="m" />
+      <EuiPanel hasBorder paddingSize="none">
+        <EuiImage
+          size="xl"
+          src={servicesInventory}
+          alt={i18n.translate('xpack.apm.welcome.image.at,', {
+            defaultMessage:
+              'Image of the new experience of the service inventory, showing services detected from logs and APM-instrumented services',
+          })}
+        />
+      </EuiPanel>
+    </EuiConfirmModal>
   );
 }
