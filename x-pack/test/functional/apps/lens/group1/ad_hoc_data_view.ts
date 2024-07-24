997ec8dd49454b53bfbb690f8707a0427c934256
Marco Liberati <dej611@users.noreply.github.com>
2024-07-24 12:21:59 +0200

    [Lens][FTR] Skip failing test (#189039)
    ## Summary
    
    Skip #164623 for now
    
    ---------
    
    Co-authored-by: Alex Szabo <alex.szabo@elastic.co>

--- x-pack/test/functional/apps/lens/group1/ad_hoc_data_view.ts
+++ x-pack/test/functional/apps/lens/group1/ad_hoc_data_view.ts
@@ -73,8 +73,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     expect(await dataViews.isAdHoc()).to.be(true);
   };
 
-  // Failing: See https://github.com/elastic/kibana/issues/164623
-  describe.skip('lens ad hoc data view tests', () => {
+  describe('lens ad hoc data view tests', () => {
     it('should allow building a chart based on ad hoc data view', async () => {
       await setupAdHocDataView();
       await PageObjects.lens.configureDimension({
@@ -225,7 +224,8 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await browser.closeCurrentWindow();
     });
 
-    it('should navigate to discover from embeddable correctly', async () => {
+    // Failing: See https://github.com/elastic/kibana/issues/164623
+    it.skip('should navigate to discover from embeddable correctly', async () => {
       const [lensHandle] = await browser.getAllWindowHandles();
       await browser.switchToWindow(lensHandle);
       await PageObjects.header.waitUntilLoadingHasFinished();
