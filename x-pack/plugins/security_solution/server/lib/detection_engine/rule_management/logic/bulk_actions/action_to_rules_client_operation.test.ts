38481967c4774607ebd1fa75768e2fe6677ea08f
Devin Hurley <devin.hurley@elastic.co>
2024-07-24 15:42:20 -0400

    adds unit tests for changes to bulkEditActionToRulesClientOperation

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_management/logic/bulk_actions/action_to_rules_client_operation.test.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_management/logic/bulk_actions/action_to_rules_client_operation.test.ts
@@ -71,4 +71,157 @@ describe('bulkEditActionToRulesClientOperation', () => {
       },
     ]);
   });
+
+  test('should add_rule_actions non-system actions', () => {
+    expect(
+      bulkEditActionToRulesClientOperation(actionsClient, {
+        type: BulkActionEditTypeEnum.add_rule_actions,
+        value: {
+          actions: [
+            {
+              group: 'default',
+              id: 'b0d183b2-3e04-428d-9fc4-ca7e23604380',
+              params: {
+                message: 'Rule {{context.rule.name}} generated {{state.signals_count}} alerts',
+              },
+              frequency: { summary: true, notifyWhen: 'onActiveAlert', throttle: null },
+            },
+          ],
+        },
+      })
+    ).toEqual([
+      {
+        field: 'actions',
+        operation: 'add',
+        value: [
+          {
+            group: 'default',
+            id: 'b0d183b2-3e04-428d-9fc4-ca7e23604380',
+            params: {
+              message: 'Rule {{context.rule.name}} generated {{state.signals_count}} alerts',
+            },
+            frequency: { summary: true, notifyWhen: 'onActiveAlert', throttle: null },
+          },
+        ],
+      },
+    ]);
+  });
+
+  test('should add_rule_actions system actions', () => {
+    expect(
+      bulkEditActionToRulesClientOperation(actionsClient, {
+        type: BulkActionEditTypeEnum.add_rule_actions,
+        value: {
+          actions: [
+            {
+              id: 'system-connector-.cases',
+              params: {
+                subAction: 'run',
+                subActionParams: {
+                  timeWindow: '7d',
+                  reopenClosedCases: false,
+                  groupingBy: ['agent.name'],
+                },
+              },
+            },
+          ],
+        },
+      })
+    ).toEqual([
+      {
+        field: 'actions',
+        operation: 'add',
+        value: [
+          {
+            id: 'system-connector-.cases',
+            params: {
+              subAction: 'run',
+              subActionParams: {
+                timeWindow: '7d',
+                reopenClosedCases: false,
+                groupingBy: ['agent.name'],
+              },
+            },
+          },
+        ],
+      },
+    ]);
+  });
+
+  test('should set_rule_actions non-system actions', () => {
+    expect(
+      bulkEditActionToRulesClientOperation(actionsClient, {
+        type: BulkActionEditTypeEnum.set_rule_actions,
+        value: {
+          actions: [
+            {
+              group: 'default',
+              id: 'b0d183b2-3e04-428d-9fc4-ca7e23604380',
+              params: {
+                message:
+                  'How many alerts were generated? This many alerts: {{state.signals_count}}',
+              },
+              frequency: { summary: true, notifyWhen: 'onActiveAlert', throttle: null },
+            },
+          ],
+        },
+      })
+    ).toEqual([
+      {
+        field: 'actions',
+        operation: 'set',
+        value: [
+          {
+            id: 'b0d183b2-3e04-428d-9fc4-ca7e23604380',
+            params: {
+              message: 'How many alerts were generated? This many alerts: {{state.signals_count}}',
+            },
+            frequency: { summary: true, notifyWhen: 'onActiveAlert', throttle: null },
+            group: 'default',
+          },
+        ],
+      },
+    ]);
+  });
+
+  test('should set_rule_actions system actions', () => {
+    expect(
+      bulkEditActionToRulesClientOperation(actionsClient, {
+        type: BulkActionEditTypeEnum.set_rule_actions,
+        value: {
+          actions: [
+            {
+              id: 'system-connector-.cases',
+              params: {
+                subAction: 'run',
+                subActionParams: {
+                  timeWindow: '7d',
+                  reopenClosedCases: false,
+                  groupingBy: ['agent.type'],
+                },
+              },
+            },
+          ],
+        },
+      })
+    ).toEqual([
+      {
+        field: 'actions',
+        operation: 'set',
+        value: [
+          {
+            id: 'system-connector-.cases',
+            params: {
+              subAction: 'run',
+              subActionParams: {
+                timeWindow: '7d',
+                reopenClosedCases: false,
+                groupingBy: ['agent.type'],
+              },
+            },
+          },
+        ],
+      },
+    ]);
+  });
 });
