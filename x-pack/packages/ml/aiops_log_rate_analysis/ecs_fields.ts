6a45eb27c4e13328f72b2c6ba606414fd123ee11
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-07-24 16:30:04 +0200

    update ecs field list

--- x-pack/packages/ml/aiops_log_rate_analysis/ecs_fields.ts
+++ x-pack/packages/ml/aiops_log_rate_analysis/ecs_fields.ts
@@ -5,100 +5,188 @@
  * 2.0.
  */
 
+// A selection of fields derived from
+// https://www.elastic.co/guide/en/ecs/current/ecs-field-reference.html
 const ecsFields = [
-  // general fields
-  'ecs.version',
-  'data_stream.dataset',
-  'event.category',
-  'event.action',
-  'event.module',
-  'event.dataset',
-  'event.kind',
-  'event.severity',
-  'event.timezone',
-  'event.type',
-  'fileset.name',
-  'host.architecture',
-  'host.name',
-  'host.ip',
-  'host.os.codename',
-  'host.os.family',
-  'host.os.kernel',
-  'host.os.name',
-  'host.os.platform',
-  'host.os.type',
-  'host.os.version',
-  'input.type',
-  'process.name',
-  'process.executable',
-  'user.name',
-  'service.name',
-  'service.type',
-  'service.runtime.name',
-  'service.runtime.version',
-  'log.file.path',
-  'log.flags',
-  'log.level',
-  'log.logger',
-  'log.origin.file.name',
-  'log.origin.function',
-  'log.original',
-  'source.address',
-  'destination.address',
-  'http.request.method',
-  'url.domain',
-  'network.protocol',
-  'file.path',
-  'observer.name',
+  // Base fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-base.html
+  // Only picking `tags` here since `@timestamp` is not applicable,
+  // `message` is part of text field candidates and `label` acts
+  // as a prefix which we'll handle in filterByECSFields.
+  'tags',
+
+  // Agent fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-agent.html
   'agent.name',
   'agent.type',
   'agent.version',
 
-  // infrastructure fields
+  // Client fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-client.html
+  'client.domain',
+  'client.ip', // of type IP
+  'client.port',
+
+  // Cloud fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-cloud.html
+  'cloud.account.id',
+  'cloud.account.name',
   'cloud.availability_zone',
-  'cloud.project.id',
-  'cloud.service.name',
-  'cloud.provider', // Name of the cloud provider
-  'cloud.region', // Region in which the host is running
   'cloud.instance.id', // Instance ID in the cloud environment
   'cloud.machine.type', // Type of the cloud machine
+  'cloud.project.id',
+  'cloud.provider', // Name of the cloud provider
+  'cloud.region', // Region in which the host is running
+  'cloud.service.name',
+
+  // Container fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-container.html
   'container.id', // Unique identifier of the container
-  'container.name', // Name of the container
+  'container.image.hash.all',
   'container.image.name',
+  'container.image.tag',
+  'container.name', // Name of the container
   'container.runtime',
-  'orchestrator.cluster.name', // Name of the orchestrator cluster
-  'orchestrator.namespace', // Namespace of the orchestrator
-  'orchestrator.type', // Type of orchestrator (e.g., Kubernetes, Mesos)
+  'container.security_context.privileged', // boolean type
 
-  // web/http related fields
-  'user_agent.original', // Original user agent string
-  'user_agent.name', // Name of the user agent
-  'http.request.method', // HTTP request method
+  // Data Stream fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-data_stream.html
+  'data_stream.dataset',
+  'data_stream.namespace',
+  'data_stream.type',
 
-  // APM
-  'transaction.name',
+  // Destination fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-destination.html
+  'destination.address',
 
-  // Geo
-  'client.geo.city_name',
-  'client.geo.continent_name',
-  'client.geo.country_iso_code',
-  'client.geo.country_name',
-  'client.geo.region_iso_code',
-  'client.geo.region_name',
+  // ECS fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-ecs.html
+  'ecs.version',
 
-  // IP fields
-  'source.ip',
-  'destination.ip',
-  'client.ip',
-  'server.ip',
-  'host.ip',
+  // Error fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-error.html
+  'error.code',
+  'error.id',
+  'error.type',
 
-  // Boolean fields
+  // Event fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-event.html
+  'event.action',
+  'event.agent_id_status',
+  'event.category',
+  'event.code',
+  'event.dataset',
+  'event.kind',
+  'event.module',
   'event.outcome',
-  'event.success',
+  'event.provider',
+  'event.reason',
+  'event.reference',
+  'event.timezone',
+  'event.type',
+  'event.url',
+
+  // File fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-file.html
+  'file.attributes',
+  'file.path',
+  'file.type',
+
+  // Host fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-host.html
+  'host.architecture',
+  'host.domain',
+  'host.ip',
+  'host.name',
+
+  // HTTP fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-http.html
+  'http.request.method',
+  'http.request.mime_type',
+  'http.request.referrer',
+  'http.response.mime_type',
+  'http.response.status_code',
+  'http.version',
+
+  // Log fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-log.html
+  'log.file.path',
+  'log.level',
+  'log.logger',
+  'log.origin.file.name',
+  'log.origin.function',
+
+  // Network fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-network.html
+  'network.application',
   'network.direction',
-  'process.parent.executable',
-  'tls.established',
+  'network.name',
+  'network.protocol',
+  'network.type',
+
+  // Orchestrator fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-orchestrator.html
+  'orchestrator.cluster.name',
+  'orchestrator.cluster.version',
+  'orchestrator.namespace',
+  'orchestrator.organization',
+  'orchestrator.type',
+
+  // Process fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-process.html
+  'process.command_line',
+  'process.env_vars',
+  'process.name',
+
+  // Rule fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-rule.html
+  'rule.author',
+  'rule.category',
+  'rule.description',
+  'rule.name',
+  'rule.ruleset',
+  'rule.version',
+
+  // Server fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-server.html
+  'server.domain',
+  'server.ip', // IP field
+  'server.port',
+
+  // Service fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-service.html
+  'service.environment',
+  'service.name',
+  'service.node.name',
+  'service.node.role', // deprecated
+  'service.node.roles',
+  'service.state',
+  'service.type',
+  'service.version',
+
+  // Source fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-source.html
+  'source.domain',
+  'source.ip', // IP field
+  'source.port',
+
+  // URL fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-url.html
+  'url.domain',
+  'url.full',
+
+  // User fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-user.html
+  'user.domain',
+  'user.email',
+  'user.roles',
+
+  // User agent fields
+  // https://www.elastic.co/guide/en/ecs/current/ecs-user_agent.html
+  'user_agent.device.name',
+  'user_agent.name',
+  'user_agent.original',
+  'user_agent.version',
 ];
 
 const ecsFieldsMustHave = [
@@ -107,10 +195,47 @@ const ecsFieldsMustHave = [
   'log.level', // Severity level of the log event
 ];
 
+// These are the fields that are expected to be nested within other ECS fields.
+const ecsPostfixes = [
+  // For geo fields, we default to the more human readable variants.
+  // Those are supposed to be used nested within e.g. `client.*` that's
+  // why we cannot have them in the `ecsFields` array.
+  // https://www.elastic.co/guide/en/ecs/current/ecs-geo.html
+  '.geo.city_name',
+  '.geo.continent_name',
+  '.geo.country_name',
+  '.geo.region_name',
+  '.geo.timezone',
+
+  // The os fields are expected to be nested at: host.os, observer.os, user_agent.os
+  // https://www.elastic.co/guide/en/ecs/current/ecs-os.html
+  '.os.family',
+  '.os.full',
+  '.os.kernel',
+  '.os.name',
+  '.os.platform',
+  '.os.version',
+
+  // The risk fields are expected to be nested at: host.risk, user.risk
+  // https://www.elastic.co/guide/en/ecs/current/ecs-risk.html
+  '.risk.calculated_level',
+  '.risk.static_level',
+
+  // User fields are also in the ECS fields array but can be nested too.
+  '.user.domain',
+  '.user.email',
+  '.user.roles',
+];
+
 export function containsECSIdentifierFields(fieldsArray: string[]): boolean {
   return ecsFieldsMustHave.every((d) => fieldsArray.includes(d));
 }
 
 export function filterByECSFields(fieldsArray: string[]) {
-  return fieldsArray.filter((field) => ecsFields.includes(field) || field.startsWith('label.'));
+  return fieldsArray.filter(
+    (field) =>
+      ecsFields.includes(field) ||
+      field.startsWith('label.') ||
+      ecsPostfixes.some((postfix) => field.endsWith(postfix))
+  );
 }
