a73e9a47de5ebb31a95dc58412c446f92027524d
Brad White <brad.white@elastic.co>
2024-07-24 15:10:41 -0600

    comment on bootstrap

--- .devcontainer/scripts/bootstrap.sh
+++ .devcontainer/scripts/bootstrap.sh
@@ -9,4 +9,5 @@ source "${SCRIPT_DIR}/setup_shell.sh"
 # Change ownership of the mounted .cache directory to vscode
 sudo chown -R vscode:vscode /home/vscode/.cache
 
+# Disable FIPS to bootstrap due to some packages using non-compliant algorithms
 NODE_OPTIONS= yarn kbn bootstrap
