0a5244a9e826c1eca2781d1a31a283d0ba347b8d
Brad White <brad.white@elastic.co>
2024-07-24 18:15:46 -0600

    remove caching setup - not working

--- .devcontainer/scripts/bootstrap.sh
+++ .devcontainer/scripts/bootstrap.sh
@@ -7,7 +7,7 @@ SCRIPT_DIR=$(dirname "$0")
 source "${SCRIPT_DIR}/setup_shell.sh"
 
 # Change ownership of the mounted .cache directory to vscode
-sudo chown -R vscode:vscode /home/vscode/.cache
+sudo chown -R vscode:vscode /home/vscode/.cache ./node_modules ./bazel-*
 
 # Disable FIPS to bootstrap due to some packages using non-compliant algorithms
-NODE_OPTIONS= yarn kbn bootstrap
+NODE_OPTIONS= yarn kbn reset && NODE_OPTIONS= yarn kbn bootstrap
