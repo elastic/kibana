7e459386556c19597bdd3e0958820377e14c634d
Brad White <brad.white@elastic.co>
2024-07-25 16:55:49 -0600

    rename scripts. cleanup post create. add post start

--- .devcontainer/scripts/post_create.sh
+++ .devcontainer/scripts/post_create.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR=$(dirname "$0")
 # Source setup_shell.sh from the determined directory
 source "${SCRIPT_DIR}/setup_shell.sh"
 
-# Change ownership of the mounted .cache directory to vscode
-sudo chown -R vscode:vscode /home/vscode/.cache ./node_modules ./bazel-*
-
+# The node_modules are mounted from the host machine which can be a different platform,
+# so we re-install the dependencies to ensure they are compatible.
+#
 # Disable FIPS to bootstrap due to some packages using non-compliant algorithms
 NODE_OPTIONS= yarn kbn reset && NODE_OPTIONS= yarn kbn bootstrap
