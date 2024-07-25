79ee8da303e4a35d1e2f1c34a9dd31cf83d689d8
Elena Shostak <elena.shostak@elastic.co>
2024-07-25 13:54:14 +0200

    Integration test

--- x-pack/test/api_integration/apis/security/roles_bulk.ts
+++ x-pack/test/api_integration/apis/security/roles_bulk.ts
@@ -6,20 +6,26 @@
  */
 
 import expect from '@kbn/expect';
-import type { Role } from '@kbn/security-plugin-types-common';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ getService }: FtrProviderContext) {
   const es = getService('es');
   const supertest = getService('supertest');
-  const config = getService('config');
-  const basic = config.get('esTestCluster.license') === 'basic';
 
   describe('Roles Bulk', () => {
+    after(async () => {
+      await es.security.deleteRole({ name: 'bulk_role_1' });
+      await es.security.deleteRole({ name: 'bulk_role_2' });
+      await es.security.deleteRole({ name: 'bulk_role_with_privilege_1' });
+      await es.security.deleteRole({ name: 'bulk_role_with_privilege_2' });
+      await es.security.deleteRole({ name: 'bulk_role_valid' });
+      await es.security.deleteRole({ name: 'bulk_role_to_update_1' });
+      await es.security.deleteRole({ name: 'bulk_role_to_update_2' });
+    });
     describe('Create Roles', () => {
       it('should allow us to create empty roles', async () => {
         await supertest
-          .put('/api/security/roles')
+          .post('/api/security/roles')
           .set('kbn-xsrf', 'xxx')
           .send({
             roles: {
@@ -35,7 +41,7 @@ export default function ({ getService }: FtrProviderContext) {
 
       it('should create roles with kibana and elasticsearch privileges', async () => {
         await supertest
-          .put('/api/security/role')
+          .post('/api/security/roles')
           .set('kbn-xsrf', 'xxx')
           .send({
             roles: {
@@ -95,7 +101,8 @@ export default function ({ getService }: FtrProviderContext) {
 
         const role = await es.security.getRole({ name: 'bulk_role_with_privilege_1' });
         expect(role).to.eql({
-          role_with_privileges: {
+          bulk_role_with_privilege_1: {
+            metadata: {},
             cluster: ['manage'],
             indices: [
               {
@@ -117,9 +124,6 @@ export default function ({ getService }: FtrProviderContext) {
               },
             ],
             run_as: ['watcher_user'],
-            metadata: {
-              foo: 'test-metadata',
-            },
             transient_metadata: {
               enabled: true,
             },
@@ -127,71 +131,65 @@ export default function ({ getService }: FtrProviderContext) {
         });
       });
 
-      it(`should ${basic ? 'not' : ''} create a role with kibana and FLS/DLS elasticsearch
-      privileges on ${basic ? 'basic' : 'trial'} licenses`, async () => {
+      it('should return noop if roles exist and did not change', async () => {
         await supertest
-          .put('/api/security/role/role_with_privileges_dls_fls')
+          .post('/api/security/roles')
           .set('kbn-xsrf', 'xxx')
           .send({
-            metadata: {
-              foo: 'test-metadata',
-            },
-            elasticsearch: {
-              cluster: ['manage'],
-              indices: [
-                {
-                  field_security: {
-                    grant: ['*'],
-                    except: ['geo.*'],
-                  },
-                  names: ['logstash-*'],
-                  privileges: ['read', 'view_index_metadata'],
-                  query: `{ "match": { "geo.src": "CN" } }`,
-                },
-              ],
-              run_as: ['watcher_user'],
+            roles: {
+              bulk_role_1: {},
+              bulk_role_2: {},
             },
           })
-          .expect(basic ? 403 : 204);
+          .expect(200)
+          .then((response) => {
+            expect(response.body).to.eql({ noop: ['bulk_role_1', 'bulk_role_2'] });
+          });
       });
 
-      describe('with the createOnly option enabled', () => {
-        it('should fail when role already exists', async () => {
-          await es.security.putRole({
-            name: 'test_role',
-            body: {
-              cluster: ['monitor'],
-              indices: [
-                {
-                  names: ['beats-*'],
-                  privileges: ['write'],
+      it('should return validation errors for roles that failed', async () => {
+        await supertest
+          .post('/api/security/roles')
+          .set('kbn-xsrf', 'xxx')
+          .send({
+            roles: {
+              bulk_role_es_invalid: {
+                elasticsearch: {
+                  cluster: ['bla'],
                 },
-              ],
-              run_as: ['reporting_user'],
+              },
+              bulk_role_kibana_invalid: {
+                kibana: [
+                  {
+                    spaces: ['bar-space'],
+                    base: [],
+                    feature: {
+                      fleetv2: ['all', 'read'],
+                    },
+                  },
+                ],
+              },
+              bulk_role_valid: {
+                elasticsearch: {
+                  cluster: ['all'],
+                },
+              },
             },
+          })
+          .expect(200)
+          .then((response) => {
+            const { created, errors } = response.body;
+            expect(created).to.eql(['bulk_role_valid']);
+            expect(errors).to.have.property('bulk_role_es_invalid');
+            expect(errors).to.have.property('bulk_role_kibana_invalid');
           });
-
-          await supertest
-            .put('/api/security/role/test_role?createOnly=true')
-            .set('kbn-xsrf', 'xxx')
-            .send({})
-            .expect(409);
-        });
-
-        it('should succeed when role does not exist', async () => {
-          await supertest
-            .put('/api/security/role/new_role?createOnly=true')
-            .set('kbn-xsrf', 'xxx')
-            .send({})
-            .expect(204);
-        });
       });
     });
 
-    describe('Update Role', () => {
-      it('should update a role with elasticsearch, kibana and other applications privileges', async () => {
+    describe('Update Roles', () => {
+      it('should update roles with elasticsearch, kibana and other applications privileges', async () => {
         await es.security.putRole({
-          name: 'role_to_update',
+          name: 'bulk_role_to_update_1',
           body: {
             cluster: ['monitor'],
             indices: [
@@ -218,44 +216,72 @@ export default function ({ getService }: FtrProviderContext) {
             },
           },
         });
+        await es.security.putRole({ name: 'bulk_role_to_update_2', body: {} });
 
         await supertest
-          .put('/api/security/role/role_to_update')
+          .post('/api/security/roles')
           .set('kbn-xsrf', 'xxx')
           .send({
-            metadata: {
-              foo: 'test-metadata',
-            },
-            elasticsearch: {
-              cluster: ['manage'],
-              indices: [
-                {
-                  names: ['logstash-*'],
-                  privileges: ['read', 'view_index_metadata'],
-                  allow_restricted_indices: true,
+            roles: {
+              bulk_role_to_update_1: {
+                metadata: {
+                  foo: 'test-metadata',
                 },
-              ],
-              run_as: ['watcher_user'],
-            },
-            kibana: [
-              {
-                feature: {
-                  dashboard: ['read'],
-                  dev_tools: ['all'],
+                elasticsearch: {
+                  cluster: ['manage'],
+                  indices: [
+                    {
+                      names: ['logstash-*'],
+                      privileges: ['read', 'view_index_metadata'],
+                      allow_restricted_indices: true,
+                    },
+                  ],
+                  run_as: ['watcher_user'],
                 },
-                spaces: ['*'],
+                kibana: [
+                  {
+                    feature: {
+                      dashboard: ['read'],
+                      dev_tools: ['all'],
+                    },
+                    spaces: ['*'],
+                  },
+                  {
+                    base: ['all'],
+                    spaces: ['marketing', 'sales'],
+                  },
+                ],
               },
-              {
-                base: ['all'],
-                spaces: ['marketing', 'sales'],
+              bulk_role_to_update_2: {
+                kibana: [
+                  {
+                    feature: {
+                      dashboard: ['read'],
+                      dev_tools: ['all'],
+                    },
+                    spaces: ['*'],
+                  },
+                  {
+                    base: ['all'],
+                    spaces: ['observability', 'sales'],
+                  },
+                ],
               },
-            ],
+            },
           })
-          .expect(204);
+          .expect(200)
+          .then((response) => {
+            expect(response.body).to.eql({
+              updated: ['bulk_role_to_update_1', 'bulk_role_to_update_2'],
+            });
+          });
 
-        const role = await es.security.getRole({ name: 'role_to_update' });
+        const role = await es.security.getRole({
+          name: 'bulk_role_to_update_1,bulk_role_to_update_2',
+        });
+
         expect(role).to.eql({
-          role_to_update: {
+          bulk_role_to_update_1: {
             cluster: ['manage'],
             indices: [
               {
@@ -264,6 +290,10 @@ export default function ({ getService }: FtrProviderContext) {
                 allow_restricted_indices: true,
               },
             ],
+            metadata: {
+              bar: 'old-metadata',
+              foo: 'test-metadata',
+            },
             applications: [
               {
                 application: 'kibana-.kibana',
@@ -282,275 +312,32 @@ export default function ({ getService }: FtrProviderContext) {
               },
             ],
             run_as: ['watcher_user'],
-            metadata: {
-              foo: 'test-metadata',
-            },
             transient_metadata: {
               enabled: true,
             },
           },
-        });
-      });
-
-      it(`should ${basic ? 'not' : ''} update a role adding DLS and TLS priviledges
-      when using ${basic ? 'basic' : 'trial'} license`, async () => {
-        await es.security.putRole({
-          name: 'role_to_update_with_dls_fls',
-          body: {
-            cluster: ['monitor'],
-            indices: [
-              {
-                names: ['beats-*'],
-                privileges: ['write'],
-              },
-            ],
-            run_as: ['reporting_user'],
-          },
-        });
-
-        await supertest
-          .put('/api/security/role/role_to_update_with_dls_fls')
-          .set('kbn-xsrf', 'xxx')
-          .send({
-            elasticsearch: {
-              cluster: ['manage'],
-              indices: [
-                {
-                  field_security: {
-                    grant: ['*'],
-                    except: ['geo.*'],
-                  },
-                  names: ['logstash-*'],
-                  privileges: ['read'],
-                  query: `{ "match": { "geo.src": "CN" } }`,
-                },
-              ],
-              run_as: ['watcher_user'],
-            },
-          })
-          .expect(basic ? 403 : 204);
-
-        const role = await es.security.getRole({ name: 'role_to_update_with_dls_fls' });
-
-        expect(role.role_to_update_with_dls_fls.cluster).to.eql(basic ? ['monitor'] : ['manage']);
-        expect(role.role_to_update_with_dls_fls.run_as).to.eql(
-          basic ? ['reporting_user'] : ['watcher_user']
-        );
-        expect(role.role_to_update_with_dls_fls.indices[0].names).to.eql(
-          basic ? ['beats-*'] : ['logstash-*']
-        );
-        expect(role.role_to_update_with_dls_fls.indices[0].query).to.eql(
-          basic ? undefined : `{ "match": { "geo.src": "CN" } }`
-        );
-      });
-    });
-
-    describe('Get Role', () => {
-      it('should get roles', async () => {
-        await es.security.putRole({
-          name: 'role_to_get',
-          body: {
-            cluster: ['manage'],
-            indices: [
-              {
-                names: ['logstash-*'],
-                privileges: ['read', 'view_index_metadata'],
-                allow_restricted_indices: false,
-              },
-            ],
+          bulk_role_to_update_2: {
+            cluster: [],
+            indices: [],
             applications: [
               {
                 application: 'kibana-.kibana',
-                privileges: ['read'],
+                privileges: ['feature_dashboard.read', 'feature_dev_tools.all'],
                 resources: ['*'],
               },
               {
                 application: 'kibana-.kibana',
-                privileges: ['feature_dashboard.read', 'feature_discover.all', 'feature_ml.all'],
-                resources: ['space:marketing', 'space:sales'],
-              },
-              {
-                application: 'logstash-default',
-                privileges: ['logstash-privilege'],
-                resources: ['*'],
+                privileges: ['space_all'],
+                resources: ['space:observability', 'space:sales'],
               },
             ],
-            run_as: ['watcher_user'],
-            metadata: {
-              foo: 'test-metadata',
-            },
+            run_as: [],
+            metadata: {},
             transient_metadata: {
               enabled: true,
             },
           },
         });
-
-        await supertest
-          .get('/api/security/role/role_to_get')
-          .set('kbn-xsrf', 'xxx')
-          .expect(200, {
-            name: 'role_to_get',
-            metadata: {
-              foo: 'test-metadata',
-            },
-            transient_metadata: { enabled: true },
-            elasticsearch: {
-              cluster: ['manage'],
-              indices: [
-                {
-                  names: ['logstash-*'],
-                  privileges: ['read', 'view_index_metadata'],
-                  allow_restricted_indices: false,
-                },
-              ],
-              run_as: ['watcher_user'],
-            },
-            kibana: [
-              {
-                base: ['read'],
-                feature: {},
-                spaces: ['*'],
-              },
-              {
-                base: [],
-                feature: {
-                  dashboard: ['read'],
-                  discover: ['all'],
-                  ml: ['all'],
-                },
-                spaces: ['marketing', 'sales'],
-              },
-            ],
-
-            _transform_error: [],
-            _unrecognized_applications: ['logstash-default'],
-          });
-      });
-
-      it('should get roles by space id', async () => {
-        await es.security.putRole({
-          name: 'space_role_not_to_get',
-          body: {
-            cluster: ['manage'],
-            indices: [
-              {
-                names: ['logstash-*'],
-                privileges: ['read', 'view_index_metadata'],
-                allow_restricted_indices: false,
-              },
-            ],
-            applications: [
-              {
-                application: 'kibana-.kibana',
-                privileges: ['feature_dashboard.read', 'feature_discover.all', 'feature_ml.all'],
-                resources: ['space:marketing', 'space:sales'],
-              },
-            ],
-            run_as: ['watcher_user'],
-            metadata: {
-              foo: 'test-metadata',
-            },
-            transient_metadata: {
-              enabled: true,
-            },
-          },
-        });
-
-        await es.security.putRole({
-          name: 'space_role_to_get',
-          body: {
-            cluster: ['manage'],
-            indices: [
-              {
-                names: ['logstash-*'],
-                privileges: ['read', 'view_index_metadata'],
-                allow_restricted_indices: false,
-              },
-            ],
-            applications: [
-              {
-                application: 'kibana-.kibana',
-                privileges: ['feature_dashboard.read', 'feature_discover.all', 'feature_ml.all'],
-                resources: ['space:engineering', 'space:sales'],
-              },
-            ],
-            run_as: ['watcher_user'],
-            metadata: {
-              foo: 'test-metadata',
-            },
-            transient_metadata: {
-              enabled: true,
-            },
-          },
-        });
-
-        await supertest
-          .get('/internal/security/roles/engineering')
-          .set('kbn-xsrf', 'xxx')
-          .expect(200)
-          .expect((res: { body: Role[] }) => {
-            const roles = res.body;
-            expect(roles).to.be.an('array');
-
-            const success = roles.every((role) => {
-              return (
-                role.name !== 'space_role_not_to_get' &&
-                role.kibana.some((privilege) => {
-                  return privilege.spaces.includes('*') || privilege.spaces.includes('engineering');
-                })
-              );
-            });
-
-            const expectedRole = roles.find((role) => role.name === 'space_role_to_get');
-
-            expect(success).to.be(true);
-            expect(expectedRole).to.be.an('object');
-          });
-      });
-    });
-
-    describe('Delete Role', () => {
-      it('should delete the roles we created', async () => {
-        await supertest.delete('/api/security/role/empty_role').set('kbn-xsrf', 'xxx').expect(204);
-        await supertest
-          .delete('/api/security/role/role_with_privileges')
-          .set('kbn-xsrf', 'xxx')
-          .expect(204);
-        await supertest
-          .delete('/api/security/role/role_with_privileges_dls_fls')
-          .set('kbn-xsrf', 'xxx')
-          .expect(basic ? 404 : 204);
-        await supertest
-          .delete('/api/security/role/role_to_update')
-          .set('kbn-xsrf', 'xxx')
-          .expect(204);
-        await supertest
-          .delete('/api/security/role/role_to_update_with_dls_fls')
-          .set('kbn-xsrf', 'xxx')
-          .expect(204);
-
-        const emptyRole = await es.security.getRole({ name: 'empty_role' }, { ignore: [404] });
-        expect(emptyRole).to.eql({});
-        const roleWithPrivileges = await es.security.getRole(
-          { name: 'role_with_privileges' },
-          { ignore: [404] }
-        );
-        expect(roleWithPrivileges).to.eql({});
-        const roleWithPrivilegesDlsFls = await es.security.getRole(
-          { name: 'role_with_privileges_dls_fls' },
-          { ignore: [404] }
-        );
-        expect(roleWithPrivilegesDlsFls).to.eql({});
-        const roleToUpdate = await es.security.getRole(
-          { name: 'role_to_update' },
-          { ignore: [404] }
-        );
-        expect(roleToUpdate).to.eql({});
-        const roleToUpdateWithDlsFls = await es.security.getRole(
-          { name: 'role_to_update_with_dls_fls' },
-          { ignore: [404] }
-        );
-        expect(roleToUpdateWithDlsFls).to.eql({});
       });
     });
   });
