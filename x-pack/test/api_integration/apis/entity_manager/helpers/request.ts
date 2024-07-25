44ef83c50ad2005b428533c0ceba4a1cf5e999d5
klacabane <kevin.lacabane@elastic.co>
2024-07-25 15:18:37 +0200

    add missing import

--- x-pack/test/api_integration/apis/entity_manager/helpers/request.ts
+++ x-pack/test/api_integration/apis/entity_manager/helpers/request.ts
@@ -4,7 +4,9 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import { Agent } from 'supertest';
+import { EntityDefinition } from '@kbn/entities-schema';
 
 export interface Auth {
   username: string;
