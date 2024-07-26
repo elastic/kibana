a3b9da5c0828a145c78390ae79caad0c50a0ae83
Joe Reuter <johannes.reuter@elastic.co>
2024-07-26 13:07:05 +0200

    fix types

--- x-pack/plugins/observability_solution/observability_onboarding/server/types.ts
+++ x-pack/plugins/observability_solution/observability_onboarding/server/types.ts
@@ -15,7 +15,10 @@ import { FleetSetupContract, FleetStartContract } from '@kbn/fleet-plugin/server
 import { SecurityPluginSetup, SecurityPluginStart } from '@kbn/security-plugin/server';
 import { ObservabilityPluginSetup } from '@kbn/observability-plugin/server';
 import { UsageCollectionSetup } from '@kbn/usage-collection-plugin/server';
-import { CustomIntegrationsPluginSetup } from '@kbn/custom-integrations-plugin/server';
+import {
+  CustomIntegrationsPluginSetup,
+  CustomIntegrationsPluginStart,
+} from '@kbn/custom-integrations-plugin/server';
 
 export interface ObservabilityOnboardingPluginSetupDependencies {
   data: DataPluginSetup;
@@ -34,6 +37,7 @@ export interface ObservabilityOnboardingPluginStartDependencies {
   usageCollection: undefined;
   fleet: FleetStartContract;
   security: SecurityPluginStart;
+  customIntegrations: CustomIntegrationsPluginStart;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
