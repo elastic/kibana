ef474ed6d5c7489e469362d9c31de42c504de136
Drew Tate <andrew.tate@elastic.co>
2024-07-26 14:59:24 -0600

    skip tests instead of commenting

--- packages/kbn-esql-validation-autocomplete/src/autocomplete/autocomplete.test.ts
+++ packages/kbn-esql-validation-autocomplete/src/autocomplete/autocomplete.test.ts
@@ -908,8 +908,8 @@ describe('autocomplete', () => {
     ]);
     testSuggestions('FROM " "', [], ' ');
     // TODO — re-enable these tests when we can support this case
-    // testSuggestions('FROM "  a"', [], undefined, 9);
-    // testSuggestions('FROM "foo b"', [], undefined, 11);
+    testSuggestions.skip('FROM "  a"', [], undefined, 9);
+    testSuggestions.skip('FROM "foo b"', [], undefined, 11);
     testSuggestions('FROM a | WHERE tags == " "', [], ' ');
     testSuggestions('FROM a | WHERE tags == """ """', [], ' ');
     testSuggestions('FROM a | WHERE tags == "a"', [], undefined, 25);
