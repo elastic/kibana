87b6e5f7015ce67fd1c77a132fbdbf6a92dbb59c
Ryland Herrick <ryalnd@gmail.com>
2024-07-26 22:03:02 -0500

    Reduce testing rule interval to 30s
    This should give better granularity on the following:
    
    * How long it takes for the ML job to become "started"
    * How long it takes for the metrics to become available

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/trial_license_complete_tier/execution_logic/machine_learning.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/trial_license_complete_tier/execution_logic/machine_learning.ts
@@ -81,6 +81,7 @@ export default ({ getService }: FtrProviderContext) => {
     severity: 'critical',
     type: 'machine_learning',
     anomaly_threshold: 30,
+    interval: '30s',
     machine_learning_job_id: mlJobId,
     from: '1900-01-01T00:00:00.000Z',
     rule_id: 'ml-rule-id',
