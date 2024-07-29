27ff6f1a8c2c60049a503c72688578adf7affddf
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-07-29 12:29:05 +0200

    [ES|QL] Initialize with time named params

--- packages/kbn-esql-utils/src/utils/get_initial_esql_query.test.ts
+++ packages/kbn-esql-utils/src/utils/get_initial_esql_query.test.ts
@@ -5,11 +5,54 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
-
+import type { DataView } from '@kbn/data-views-plugin/public';
 import { getInitialESQLQuery } from './get_initial_esql_query';
 
+const getDataView = (name: string, timeFieldName?: string) => {
+  const fields = [
+    {
+      name: 'timestamp',
+      displayName: 'timestamp',
+      type: 'date',
+      scripted: false,
+      filterable: true,
+      aggregatable: true,
+      sortable: true,
+    },
+    {
+      name: 'message',
+      displayName: 'message',
+      type: 'string',
+      scripted: false,
+      filterable: false,
+    },
+  ] as DataView['fields'];
+  return {
+    id: `${name}-id`,
+    title: name,
+    metaFields: ['_index', '_score'],
+    fields,
+    type: 'default',
+    getName: () => name,
+    getIndexPattern: () => name,
+    getFieldByName: jest.fn((fieldName: string) => fields.getByName(fieldName)),
+    timeFieldName,
+    isPersisted: () => true,
+    toSpec: () => ({}),
+    toMinimalSpec: () => ({}),
+  } as unknown as DataView;
+};
+
 describe('getInitialESQLQuery', () => {
-  it('should work correctly', () => {
-    expect(getInitialESQLQuery('logs*')).toBe('FROM logs* | LIMIT 10');
+  it('should NOT add the where clause if there is @timestamp in the dataview', () => {
+    const dataView = getDataView('logs*', '@timestamp');
+    expect(getInitialESQLQuery(dataView)).toBe('FROM logs* | LIMIT 10');
+  });
+
+  it('should append a where clause correctly if the dataview timeFieldName is different than @timestamp', () => {
+    const dataView = getDataView('logs*', '@custom_timestamp');
+    expect(getInitialESQLQuery(dataView)).toBe(
+      'FROM logs* | WHERE @custom_timestamp >= ?start AND @custom_timestamp <= ?end | LIMIT 10'
+    );
   });
 });
--- packages/kbn-esql-utils/src/utils/get_initial_esql_query.ts
+++ packages/kbn-esql-utils/src/utils/get_initial_esql_query.ts
@@ -5,11 +5,17 @@
  * in compliance with, at your election, the Elastic License 2.0 or the Server
  * Side Public License, v 1.
  */
+import type { DataView } from '@kbn/data-views-plugin/public';
 
 /**
  * Builds an ES|QL query for the provided index or index pattern
- * @param indexOrIndexPattern
+ * @param dataView
  */
-export function getInitialESQLQuery(indexOrIndexPattern: string): string {
-  return `FROM ${indexOrIndexPattern} | LIMIT 10`;
+export function getInitialESQLQuery(dataView: DataView): string {
+  const timeFieldName = dataView?.timeFieldName;
+  const filterByTimeParams =
+    timeFieldName !== '@timestamp'
+      ? ` | WHERE ${timeFieldName} >= ?start AND ${timeFieldName} <= ?end`
+      : '';
+  return `FROM ${dataView.getIndexPattern()}${filterByTimeParams} | LIMIT 10`;
 }
