b999301c450ba1b9890dce471115cf591e488d48
Abdul Zahid <awahab07@yahoo.com>
2024-07-29 22:59:11 +0200

    Exclusively handle both ES version for highlight terms test suite.

--- x-pack/test/api_integration/apis/metrics_ui/log_entry_highlights.ts
+++ x-pack/test/api_integration/apis/metrics_ui/log_entry_highlights.ts
@@ -45,160 +45,171 @@ export default function ({ getService }: FtrProviderContext) {
     before(() => esArchiver.load('x-pack/test/functional/es_archives/infra/simple_logs'));
     after(() => esArchiver.unload('x-pack/test/functional/es_archives/infra/simple_logs'));
 
-    describe('/log_entries/highlights', function () {
-      this.onlyEsVersion('>=8.10'); // API change in 8.10
+    [
+      { esVersion: '>=8.10', splitHighlightText: false }, // API change in 8.10
+      { esVersion: '<8.10', splitHighlightText: true },
+    ].forEach(({ esVersion, splitHighlightText }) => {
+      describe('/log_entries/highlights', function () {
+        this.onlyEsVersion(esVersion);
 
-      describe('with the default source', () => {
-        before(() => kibanaServer.savedObjects.cleanStandardList());
-        after(() => kibanaServer.savedObjects.cleanStandardList());
+        describe('with the default source', () => {
+          before(() => kibanaServer.savedObjects.cleanStandardList());
+          after(() => kibanaServer.savedObjects.cleanStandardList());
 
-        it('Handles empty responses', async () => {
-          const { body } = await supertest
-            .post(LOG_ENTRIES_HIGHLIGHTS_PATH)
-            .set(COMMON_HEADERS)
-            .send(
-              logEntriesHighlightsRequestRT.encode({
-                logView: { type: 'log-view-reference', logViewId: 'default' },
-                startTimestamp: KEY_BEFORE_START.time,
-                endTimestamp: KEY_AFTER_END.time,
-                highlightTerms: ['some string that does not exist'],
-              })
-            )
-            .expect(200);
+          it('Handles empty responses', async () => {
+            const { body } = await supertest
+              .post(LOG_ENTRIES_HIGHLIGHTS_PATH)
+              .set(COMMON_HEADERS)
+              .send(
+                logEntriesHighlightsRequestRT.encode({
+                  logView: { type: 'log-view-reference', logViewId: 'default' },
+                  startTimestamp: KEY_BEFORE_START.time,
+                  endTimestamp: KEY_AFTER_END.time,
+                  highlightTerms: ['some string that does not exist'],
+                })
+              )
+              .expect(200);
 
-          const logEntriesHighlightsResponse = pipe(
-            logEntriesHighlightsResponseRT.decode(body),
-            fold(throwErrors(createPlainError), identity)
-          );
+            const logEntriesHighlightsResponse = pipe(
+              logEntriesHighlightsResponseRT.decode(body),
+              fold(throwErrors(createPlainError), identity)
+            );
 
-          expect(logEntriesHighlightsResponse.data).to.have.length(1);
+            expect(logEntriesHighlightsResponse.data).to.have.length(1);
 
-          const data = logEntriesHighlightsResponse.data[0];
+            const data = logEntriesHighlightsResponse.data[0];
 
-          expect(data.entries).to.have.length(0);
-          expect(data.topCursor).to.be(null);
-          expect(data.bottomCursor).to.be(null);
-        });
+            expect(data.entries).to.have.length(0);
+            expect(data.topCursor).to.be(null);
+            expect(data.bottomCursor).to.be(null);
+          });
 
-        it('highlights built-in message column', async () => {
-          const { body } = await supertest
-            .post(LOG_ENTRIES_HIGHLIGHTS_PATH)
-            .set(COMMON_HEADERS)
-            .send(
-              logEntriesHighlightsRequestRT.encode({
-                logView: { type: 'log-view-reference', logViewId: 'default' },
-                startTimestamp: KEY_BEFORE_START.time,
-                endTimestamp: KEY_AFTER_END.time,
-                highlightTerms: ['message of document 0'],
-              })
-            )
-            .expect(200);
+          it('highlights built-in message column', async () => {
+            const highlightTerms = 'message of document 0';
+            const { body } = await supertest
+              .post(LOG_ENTRIES_HIGHLIGHTS_PATH)
+              .set(COMMON_HEADERS)
+              .send(
+                logEntriesHighlightsRequestRT.encode({
+                  logView: { type: 'log-view-reference', logViewId: 'default' },
+                  startTimestamp: KEY_BEFORE_START.time,
+                  endTimestamp: KEY_AFTER_END.time,
+                  highlightTerms: [highlightTerms],
+                })
+              )
+              .expect(200);
 
-          const logEntriesHighlightsResponse = pipe(
-            logEntriesHighlightsResponseRT.decode(body),
-            fold(throwErrors(createPlainError), identity)
-          );
+            const logEntriesHighlightsResponse = pipe(
+              logEntriesHighlightsResponseRT.decode(body),
+              fold(throwErrors(createPlainError), identity)
+            );
 
-          expect(logEntriesHighlightsResponse.data).to.have.length(1);
+            expect(logEntriesHighlightsResponse.data).to.have.length(1);
 
-          const data = logEntriesHighlightsResponse.data[0];
-          const entries = data.entries;
-          const firstEntry = entries[0];
-          const lastEntry = entries[entries.length - 1];
+            const data = logEntriesHighlightsResponse.data[0];
+            const entries = data.entries;
+            const firstEntry = entries[0];
+            const lastEntry = entries[entries.length - 1];
 
-          // Finds expected entries
-          expect(entries).to.have.length(10);
+            // Finds expected entries
+            expect(entries).to.have.length(10);
 
-          // Cursors are set correctly
-          expect(firstEntry.cursor).to.eql(data.topCursor);
-          expect(lastEntry.cursor).to.eql(data.bottomCursor);
+            // Cursors are set correctly
+            expect(firstEntry.cursor).to.eql(data.topCursor);
+            expect(lastEntry.cursor).to.eql(data.bottomCursor);
 
-          // Entries fall within range
-          // @kbn/expect doesn't have a `lessOrEqualThan` or `moreOrEqualThan` comparators
-          expect(firstEntry.cursor.time >= moment(KEY_BEFORE_START.time).toISOString()).to.be(true);
-          expect(lastEntry.cursor.time <= moment(KEY_AFTER_END.time).toISOString()).to.be(true);
+            // Entries fall within range
+            // @kbn/expect doesn't have a `lessOrEqualThan` or `moreOrEqualThan` comparators
+            expect(firstEntry.cursor.time >= moment(KEY_BEFORE_START.time).toISOString()).to.be(
+              true
+            );
+            expect(lastEntry.cursor.time <= moment(KEY_AFTER_END.time).toISOString()).to.be(true);
 
-          // All entries contain the highlights
-          entries.forEach((entry) => {
-            entry.columns.forEach((column) => {
-              if ('message' in column && 'highlights' in column.message[0]) {
-                expect(column.message[0].highlights).to.eql(['message of document 0']);
-              }
+            // All entries contain the highlights
+            entries.forEach((entry) => {
+              entry.columns.forEach((column) => {
+                if ('message' in column && 'highlights' in column.message[0]) {
+                  const expectation = splitHighlightText
+                    ? highlightTerms.split(' ')
+                    : [highlightTerms];
+                  expect(column.message[0].highlights).to.eql(expectation);
+                }
+              });
             });
           });
-        });
 
-        it('highlights field columns', async () => {
-          const { body } = await supertest
-            .post(LOG_ENTRIES_HIGHLIGHTS_PATH)
-            .set(COMMON_HEADERS)
-            .send(
-              logEntriesHighlightsRequestRT.encode({
-                logView: { type: 'log-view-reference', logViewId: 'default' },
-                startTimestamp: KEY_BEFORE_START.time,
-                endTimestamp: KEY_AFTER_END.time,
-                highlightTerms: ['generate_test_data/simple_logs'],
-              })
-            )
-            .expect(200);
+          it('highlights field columns', async () => {
+            const { body } = await supertest
+              .post(LOG_ENTRIES_HIGHLIGHTS_PATH)
+              .set(COMMON_HEADERS)
+              .send(
+                logEntriesHighlightsRequestRT.encode({
+                  logView: { type: 'log-view-reference', logViewId: 'default' },
+                  startTimestamp: KEY_BEFORE_START.time,
+                  endTimestamp: KEY_AFTER_END.time,
+                  highlightTerms: ['generate_test_data/simple_logs'],
+                })
+              )
+              .expect(200);
 
-          const logEntriesHighlightsResponse = pipe(
-            logEntriesHighlightsResponseRT.decode(body),
-            fold(throwErrors(createPlainError), identity)
-          );
+            const logEntriesHighlightsResponse = pipe(
+              logEntriesHighlightsResponseRT.decode(body),
+              fold(throwErrors(createPlainError), identity)
+            );
 
-          expect(logEntriesHighlightsResponse.data).to.have.length(1);
+            expect(logEntriesHighlightsResponse.data).to.have.length(1);
 
-          const entries = logEntriesHighlightsResponse.data[0].entries;
+            const entries = logEntriesHighlightsResponse.data[0].entries;
 
-          // Finds expected entries
-          expect(entries).to.have.length(50);
+            // Finds expected entries
+            expect(entries).to.have.length(50);
 
-          // All entries contain the highlights
-          entries.forEach((entry) => {
-            entry.columns.forEach((column) => {
-              if ('field' in column && 'highlights' in column && column.highlights.length > 0) {
-                expect(column.highlights).to.eql(['generate_test_data/simple_logs']);
-              }
+            // All entries contain the highlights
+            entries.forEach((entry) => {
+              entry.columns.forEach((column) => {
+                if ('field' in column && 'highlights' in column && column.highlights.length > 0) {
+                  expect(column.highlights).to.eql(['generate_test_data/simple_logs']);
+                }
+              });
             });
           });
-        });
 
-        it('applies the query as well as the highlight', async () => {
-          const { body } = await supertest
-            .post(LOG_ENTRIES_HIGHLIGHTS_PATH)
-            .set(COMMON_HEADERS)
-            .send(
-              logEntriesHighlightsRequestRT.encode({
-                logView: { type: 'log-view-reference', logViewId: 'default' },
-                startTimestamp: KEY_BEFORE_START.time,
-                endTimestamp: KEY_AFTER_END.time,
-                query: JSON.stringify({
-                  multi_match: { query: 'host-a', type: 'phrase', lenient: true },
-                }),
-                highlightTerms: ['message'],
-              })
-            )
-            .expect(200);
+          it('applies the query as well as the highlight', async () => {
+            const { body } = await supertest
+              .post(LOG_ENTRIES_HIGHLIGHTS_PATH)
+              .set(COMMON_HEADERS)
+              .send(
+                logEntriesHighlightsRequestRT.encode({
+                  logView: { type: 'log-view-reference', logViewId: 'default' },
+                  startTimestamp: KEY_BEFORE_START.time,
+                  endTimestamp: KEY_AFTER_END.time,
+                  query: JSON.stringify({
+                    multi_match: { query: 'host-a', type: 'phrase', lenient: true },
+                  }),
+                  highlightTerms: ['message'],
+                })
+              )
+              .expect(200);
 
-          const logEntriesHighlightsResponse = pipe(
-            logEntriesHighlightsResponseRT.decode(body),
-            fold(throwErrors(createPlainError), identity)
-          );
+            const logEntriesHighlightsResponse = pipe(
+              logEntriesHighlightsResponseRT.decode(body),
+              fold(throwErrors(createPlainError), identity)
+            );
 
-          expect(logEntriesHighlightsResponse.data).to.have.length(1);
+            expect(logEntriesHighlightsResponse.data).to.have.length(1);
 
-          const entries = logEntriesHighlightsResponse.data[0].entries;
+            const entries = logEntriesHighlightsResponse.data[0].entries;
 
-          // Finds expected entries
-          expect(entries).to.have.length(25);
+            // Finds expected entries
+            expect(entries).to.have.length(25);
 
-          // All entries contain the highlights
-          entries.forEach((entry) => {
-            entry.columns.forEach((column) => {
-              if ('message' in column && 'highlights' in column.message[0]) {
-                expect(column.message[0].highlights).to.eql(['message', 'message']);
-              }
+            // All entries contain the highlights
+            entries.forEach((entry) => {
+              entry.columns.forEach((column) => {
+                if ('message' in column && 'highlights' in column.message[0]) {
+                  expect(column.message[0].highlights).to.eql(['message', 'message']);
+                }
+              });
             });
           });
         });
