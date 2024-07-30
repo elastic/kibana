483f31119313c1f427e1985751e8afb9c3d4e833
Ying Mao <ying.mao@elastic.co>
2024-07-30 12:53:27 -0400

    Revert TM resource based task scheduling issues (#189529)
    (cherry picked from commit 32459096ff32fa4523fea5f17e1ff9aa881dbef7)
    
    # Conflicts:
    #	x-pack/plugins/task_manager/server/task_type_dictionary.test.ts

--- x-pack/plugins/task_manager/server/config.test.ts
+++ x-pack/plugins/task_manager/server/config.test.ts
@@ -23,6 +23,7 @@ describe('config validation', () => {
           "warn_threshold": 5000,
         },
         "max_attempts": 3,
+        "max_workers": 10,
         "metrics_reset_interval": 30000,
         "monitored_aggregated_stats_refresh_rate": 60000,
         "monitored_stats_health_verbose_log": Object {
@@ -80,6 +81,7 @@ describe('config validation', () => {
           "warn_threshold": 5000,
         },
         "max_attempts": 3,
+        "max_workers": 10,
         "metrics_reset_interval": 30000,
         "monitored_aggregated_stats_refresh_rate": 60000,
         "monitored_stats_health_verbose_log": Object {
@@ -135,6 +137,7 @@ describe('config validation', () => {
           "warn_threshold": 5000,
         },
         "max_attempts": 3,
+        "max_workers": 10,
         "metrics_reset_interval": 30000,
         "monitored_aggregated_stats_refresh_rate": 60000,
         "monitored_stats_health_verbose_log": Object {
