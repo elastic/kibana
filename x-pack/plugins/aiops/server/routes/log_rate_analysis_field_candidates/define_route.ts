f391ed6bcf59636e13dd7c7513fbb4613e486fa2
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-07-30 10:12:53 +0200

    [ML] AIOps: Update fields filter popover to be able to filter fields from analysis (not just grouping)  (#188913)
    ## Summary
    
    Part of #187684.
    
    So far the popover to filter fields was only available when grouping was
    enabled. This PR updates the behavior so it's available all the time and
    can be used to exclude field candidates from the analysis. If we detect
    the index to be based on an ECS schema, we auto-select a set of
    predefined fields.
    
    Changes in this PR:
    
    - Creates a new route
    `/internal/aiops/log_rate_analysis/field_candidates` to be able to fetch
    field candidates independent of the main streaming API call.
    - Fixes the code to consider "remaining" field candidates to also
    consider text field candidates. This was originally developed to allow
    to continue an analysis that errored for some reason. We use that option
    to also pass on the custom field list from the field selection popover.
    - Fetching the field candidates is done in a new redux slice
    `logRateAnalysisFieldCandidatesSlice` using an async thunk.
    - Filters the list of field candidates by a predefined field of allowed
    fields when an ECS schema gets detected.
    - Renames `fieldCandidates` to `keywordFieldCandidates` for clearer
    distinction against `textFieldCandidates`.
    - Refactors `getLogRateAnalysisTypeForCounts` args to a config object.
    - Bump the API version for the full log rate analysis to version 3. We
    missed bumping the version in
    https://github.com/elastic/kibana/pull/188648. This update manages
    proper versioning between v2 and v3, also the API integration tests
    cover both versions.
    
    
    [aiops-log-rate-analysis-fields-filter-0001.webm](https://github.com/user-attachments/assets/e3ed8d5b-f01c-42ef-8033-caa7135b8cc0)
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [x] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- x-pack/plugins/aiops/server/routes/log_rate_analysis_field_candidates/define_route.ts
+++ x-pack/plugins/aiops/server/routes/log_rate_analysis_field_candidates/define_route.ts
@@ -6,10 +6,8 @@
  */
 
 import type { CoreStart, IRouter } from '@kbn/core/server';
-import type { Logger } from '@kbn/logging';
 import type { DataRequestHandlerContext } from '@kbn/data-plugin/server';
 import type { UsageCounter } from '@kbn/usage-collection-plugin/server';
-import { aiopsLogRateAnalysisSchemaV2 } from '@kbn/aiops-log-rate-analysis/api/schema_v2';
 import { aiopsLogRateAnalysisSchemaV3 } from '@kbn/aiops-log-rate-analysis/api/schema_v3';
 import { AIOPS_API_ENDPOINT } from '@kbn/aiops-common/constants';
 
@@ -19,42 +17,30 @@ import { routeHandlerFactory } from './route_handler_factory';
 
 /**
  * `defineRoute` is called in the root `plugin.ts` to set up the API route
- * for log rate analysis. Its purpose is to take care of the route setup
+ * for field candidates. Its purpose is to take care of the route setup
  * and versioning only. `routeHandlerFactory` is used to take care of
  * the actual route logic.
  */
 export const defineRoute = (
   router: IRouter<DataRequestHandlerContext>,
   license: AiopsLicense,
-  logger: Logger,
   coreStart: CoreStart,
   usageCounter?: UsageCounter
 ) => {
   router.versioned
     .post({
-      path: AIOPS_API_ENDPOINT.LOG_RATE_ANALYSIS,
+      path: AIOPS_API_ENDPOINT.LOG_RATE_ANALYSIS_FIELD_CANDIDATES,
       access: 'internal',
     })
     .addVersion(
       {
-        version: '2',
-        validate: {
-          request: {
-            body: aiopsLogRateAnalysisSchemaV2,
-          },
-        },
-      },
-      routeHandlerFactory('2', license, logger, coreStart, usageCounter)
-    )
-    .addVersion(
-      {
-        version: '3',
+        version: '1',
         validate: {
           request: {
             body: aiopsLogRateAnalysisSchemaV3,
           },
         },
       },
-      routeHandlerFactory('3', license, logger, coreStart, usageCounter)
+      routeHandlerFactory('1', license, coreStart, usageCounter)
     );
 };
