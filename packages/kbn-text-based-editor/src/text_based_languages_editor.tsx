ff09898c70db3b3fb8916b86e56d82f66173f257
Quynh Nguyen <quynh.nguyen@elastic.co>
2024-07-30 16:52:05 -0500

    Fallback esType to unknown

--- packages/kbn-text-based-editor/src/text_based_languages_editor.tsx
+++ packages/kbn-text-based-editor/src/text_based_languages_editor.tsx
@@ -469,8 +469,7 @@ export const TextBasedLanguagesEditor = memo(function TextBasedLanguagesEditor({
               abortController
             ).result;
             const columns =
-              // Forcing esType! here because esType will never be undefined
-              table?.columns.map((c) => ({ name: c.name, type: c.meta.esType! })) || [];
+              table?.columns.map((c) => ({ name: c.name, type: c.meta.esType ?? 'unknown' })) || [];
             return await getRateLimitedColumnsWithMetadata(columns, fieldsMetadata);
           } catch (e) {
             // no action yet
