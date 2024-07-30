e66c663b4eda2757f3b3b9cc3b71f395804d04be
Dmitrii <dmitrii.shevchenko@elastic.co>
2024-07-30 16:04:17 +0200

    Added an endpoint for bootstrapping prebuilt detection rules

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/api/hooks/use_bootstrap_prebuilt_rules.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/api/hooks/use_bootstrap_prebuilt_rules.ts
@@ -4,39 +4,37 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { EPM_API_ROUTES } from '@kbn/fleet-plugin/common';
-import type { BulkInstallPackagesResponse } from '@kbn/fleet-plugin/common/types';
 import type { UseMutationOptions } from '@tanstack/react-query';
 import { useMutation } from '@tanstack/react-query';
+import { BOOTSTRAP_PREBUILT_RULES_URL } from '../../../../../common/api/detection_engine';
+import type { BootstrapPrebuiltRulesResponse } from '../../../../../common/api/detection_engine/prebuilt_rules/bootstrap_prebuilt_rules/bootstrap_prebuilt_rules.gen';
 import { PREBUILT_RULES_PACKAGE_NAME } from '../../../../../common/detection_engine/constants';
-import type { BulkInstallFleetPackagesProps } from '../api';
-import { bulkInstallFleetPackages } from '../api';
+import { bootstrapPrebuiltRules } from '../api';
 import { useInvalidateFetchPrebuiltRulesInstallReviewQuery } from './prebuilt_rules/use_fetch_prebuilt_rules_install_review_query';
 import { useInvalidateFetchPrebuiltRulesStatusQuery } from './prebuilt_rules/use_fetch_prebuilt_rules_status_query';
 import { useInvalidateFetchPrebuiltRulesUpgradeReviewQuery } from './prebuilt_rules/use_fetch_prebuilt_rules_upgrade_review_query';
 
-export const BULK_INSTALL_FLEET_PACKAGES_MUTATION_KEY = [
-  'POST',
-  EPM_API_ROUTES.BULK_INSTALL_PATTERN,
-];
+export const BOOTSTRAP_PREBUILT_RULES_KEY = ['POST', BOOTSTRAP_PREBUILT_RULES_URL];
 
-export const useBulkInstallFleetPackagesMutation = (
-  options?: UseMutationOptions<BulkInstallPackagesResponse, Error, BulkInstallFleetPackagesProps>
+export const useBootstrapPrebuiltRulesMutation = (
+  options?: UseMutationOptions<BootstrapPrebuiltRulesResponse, Error>
 ) => {
   const invalidatePrePackagedRulesStatus = useInvalidateFetchPrebuiltRulesStatusQuery();
   const invalidatePrebuiltRulesInstallReview = useInvalidateFetchPrebuiltRulesInstallReviewQuery();
   const invalidatePrebuiltRulesUpdateReview = useInvalidateFetchPrebuiltRulesUpgradeReviewQuery();
 
-  return useMutation((props: BulkInstallFleetPackagesProps) => bulkInstallFleetPackages(props), {
+  return useMutation(() => bootstrapPrebuiltRules(), {
     ...options,
-    mutationKey: BULK_INSTALL_FLEET_PACKAGES_MUTATION_KEY,
+    mutationKey: BOOTSTRAP_PREBUILT_RULES_KEY,
     onSettled: (...args) => {
       const response = args[0];
-      const rulesPackage = response?.items.find(
-        (item) => item.name === PREBUILT_RULES_PACKAGE_NAME
-      );
-      if (rulesPackage && 'result' in rulesPackage && rulesPackage.result.status === 'installed') {
-        // The rules package was installed/updated, so invalidate the pre-packaged rules status query
+      if (
+        response?.packages.find((pkg) => pkg.name === PREBUILT_RULES_PACKAGE_NAME)?.status ===
+        'installed'
+      ) {
+        // Invalidate other pre-packaged rules related queries. We need to do
+        // that only in case the prebuilt rules package was installed indicating
+        // that there might be new rules to install.
         invalidatePrePackagedRulesStatus();
         invalidatePrebuiltRulesInstallReview();
         invalidatePrebuiltRulesUpdateReview();
