cfaa6daedf0cfc6e5b9256e95031436d4f2534cb
rshen91 <rshen@elastic.co>
2024-07-31 13:12:22 -0600

    clean up unit test

--- src/plugins/dashboard/public/dashboard_app/top_nav/managed_popover.test.tsx
+++ src/plugins/dashboard/public/dashboard_app/top_nav/managed_popover.test.tsx
@@ -10,33 +10,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { ManagedPopover } from './managed_popover';
 import { buildMockDashboard } from '../../mocks';
-import userEvent from '@testing-library/user-event';
 
 describe('ManagedPopover', () => {
-  xit('should render', async () => {
+  it('should render', async () => {
     const mockDashboard = buildMockDashboard();
     await render(<ManagedPopover dashboard={mockDashboard} redirectTo={jest.fn()} />);
     const [managedButton] = await screen.findAllByText('Managed');
     expect(managedButton).toBeInTheDocument();
   });
-  xit('opens the popover with duplicate button', async () => {
+  it('opens the popover with duplicate button', async () => {
     const mockDashboard = buildMockDashboard();
     await render(<ManagedPopover dashboard={mockDashboard} redirectTo={jest.fn()} />);
     const [managedButton] = await screen.findAllByText('Managed');
     await managedButton.click();
     const duplicateButton = await screen.findByRole('button', { name: 'Duplicate this dashboard' });
     expect(duplicateButton).toBeInTheDocument();
-  });
-  it('duplicates the dashboard', async () => {
-    const mockDashboard = buildMockDashboard();
-    mockDashboard.dispatch.setTitle('Test');
-    const mockFn = jest.fn();
-    await render(<ManagedPopover dashboard={mockDashboard} redirectTo={mockFn} />);
-    const [managedButton] = await screen.findAllByText('Managed');
-    await userEvent.click(managedButton);
-    const duplicateButton = await screen.findByRole('button', { name: 'Duplicate this dashboard' });
-    expect(duplicateButton).toBeInTheDocument();
-    await userEvent.click(duplicateButton);
-    // need to figure out how to test the redirect - the mock function is not being called
   });
 });
