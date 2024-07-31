e542797434d081d2ca07f957e9810e34080fadd7
parkiino <candace.park@elastic.co>
2024-07-31 11:51:02 -0400

    simplify date logic, update blog url

--- packages/kbn-avc-banner/src/index.tsx
+++ packages/kbn-avc-banner/src/index.tsx
@@ -6,7 +6,7 @@
  * Side Public License, v 1.
  */
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import { css } from '@emotion/css';
 import { i18n } from '@kbn/i18n';
 import { EuiButton, EuiCallOut, EuiSpacer, useEuiTheme } from '@elastic/eui';
@@ -16,9 +16,9 @@ import avcBannerBackground from './avc_banner_background.svg';
 
 // Logic to hide banner at EOY 2024
 export const useIsStillYear2024: boolean = () => {
-  const year2025 = new Date('January 01, 2025 00:00:00');
-  const today = new Date(Date.now());
-  return today < year2025;
+  useMemo(() => {
+    new Date().getFullYear() === 2024;
+  }, []);
 };
 
 export const AVCResultsBanner2024: React.FC<{ onDismiss: () => void }> = ({ onDismiss }) => {
