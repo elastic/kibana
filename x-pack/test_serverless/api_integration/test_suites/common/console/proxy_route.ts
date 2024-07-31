22fedf1631dca32c47c07c41b2649d53dd72dddd
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-07-31 12:05:22 +0200

    revert test change

--- x-pack/test_serverless/api_integration/test_suites/common/console/proxy_route.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/console/proxy_route.ts
@@ -25,12 +25,14 @@ export default function ({ getService }: FtrProviderContext) {
     describe('system indices behavior', () => {
       it('does not forward x-elastic-product-origin', async () => {
         // If we pass the header and we still get the warning back, we assume that the header was not forwarded.
-        const response = await supertestWithoutAuth
+        return await supertestWithoutAuth
           .post('/api/console/proxy?method=GET&path=/.kibana/_settings')
           .set('kbn-xsrf', 'true')
           .set(svlCommonApi.getInternalRequestHeader())
-          .set(roleAuthc.apiKeyHeader);
-        expect(response.header).to.have.property('warning');
+          .set(roleAuthc.apiKeyHeader)
+          .then((response) => {
+            expect(response.header).to.have.property('warning');
+          });
       });
     });
   });
