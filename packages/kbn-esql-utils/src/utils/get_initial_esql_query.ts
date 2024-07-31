914ea77e776d9c249875b134f70952d39138fb63
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-07-31 08:15:22 +0200

    Fix function description

--- packages/kbn-esql-utils/src/utils/get_initial_esql_query.ts
+++ packages/kbn-esql-utils/src/utils/get_initial_esql_query.ts
@@ -8,7 +8,9 @@
 import type { DataView } from '@kbn/data-views-plugin/public';
 
 /**
- * Builds an ES|QL query for the provided index or index pattern
+ * Builds an ES|QL query for the provided dataView
+ * If there is @timestamp field in the index, we don't add the WHERE clause
+ * If there is no @timestamp and there is a dataView timeFieldName, we add the WHERE clause with the timeFieldName
  * @param dataView
  */
 export function getInitialESQLQuery(dataView: DataView): string {
