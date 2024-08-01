f32455791cbf1dba88384afb99d94b176a12fe2a
Mark Hopkin <mark.hopkin@elastic.co>
2024-08-01 15:54:04 +0100

    risk engine

--- x-pack/test/api_integration/services/security_solution_api.gen.ts
+++ x-pack/test/api_integration/services/security_solution_api.gen.ts
@@ -737,6 +737,13 @@ detection engine rules.
         .set(X_ELASTIC_INTERNAL_ORIGIN_REQUEST, 'kibana')
         .query(props.query);
     },
+    riskEngineGetPrivileges() {
+      return supertest
+        .get('/internal/risk_engine/privileges')
+        .set('kbn-xsrf', 'true')
+        .set(ELASTIC_HTTP_VERSION_HEADER, '1')
+        .set(X_ELASTIC_INTERNAL_ORIGIN_REQUEST, 'kibana');
+    },
     rulePreview(props: RulePreviewProps) {
       return supertest
         .post('/api/detection_engine/rules/preview')
