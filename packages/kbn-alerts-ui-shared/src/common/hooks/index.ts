74d2a0b7dae5e071c53166f07b9e42658a344852
Umberto Pepato <umberto.pepato@elastic.co>
2024-08-01 13:41:10 +0200

    Remove unused exports

--- packages/kbn-alerts-ui-shared/src/common/hooks/index.ts
+++ packages/kbn-alerts-ui-shared/src/common/hooks/index.ts
@@ -6,34 +6,25 @@
  * Side Public License, v 1.
  */
 
-export * from './use_health_check';
-export * from './use_rule_aad_fields';
+export * from './use_alerts_data_view';
+export * from './use_create_rule';
 export * from './use_fetch_alerts_fields_query';
-export * from './use_load_rule_type_aad_template_fields';
-export * from './use_resolve_rule';
-export * from './use_find_alerts_query';
 export {
   queryKeyPrefix as fetchAlertsIndexNamesQueryKeyPrefix,
   useFetchAlertsIndexNamesQuery,
   type UseFetchAlertsIndexNamesQueryParams,
 } from './use_fetch_alerts_index_names_query';
-export * from './use_create_rule';
-export * from './use_update_rule';
+export * from './use_get_alerts_group_aggregations_query';
+export * from './use_health_check';
 export * from './use_load_alerting_framework_health';
-export * from './use_load_ui_config';
 export * from './use_load_rule_types_query';
+export * from './use_load_ui_config';
+export * from './use_load_ui_health';
+export * from './use_resolve_rule';
+export * from './use_rule_aad_fields';
 export {
   queryKeyPrefix as searchAlertsQueryKeyPrefix,
   useSearchAlertsQuery,
   type UseSearchAlertsQueryParams,
 } from './use_search_alerts_query';
-export * from './use_load_connector_types';
-export * from './use_load_connectors';
-export * from './use_get_alerts_group_aggregations_query';
-export * from './use_load_ui_health';
-export * from './use_alerts_data_view';
-export {
-  queryKeyPrefix as alertsDataViewQueryKeyPrefix,
-  useVirtualDataViewQuery,
-  type UseVirtualDataViewParams,
-} from './use_virtual_data_view_query';
+export * from './use_update_rule';
