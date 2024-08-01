f436610518df464052b876370470410d1c20e50e
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2024-08-01 10:15:39 +0200

    test

--- x-pack/plugins/security_solution/public/management/cypress/e2e/automated_response_actions/form.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/automated_response_actions/form.cy.ts
@@ -34,7 +34,7 @@ describe(
     },
   },
   () => {
-    describe('User with no access can not create an endpoint response action', () => {
+    describe('User1 with no access can not create an endpoint response action', () => {
       beforeEach(() => {
         login(ROLE.rule_author);
       });
