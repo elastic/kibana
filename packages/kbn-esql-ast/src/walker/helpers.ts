2bc32cbe3a903e5f1369136039b0f7571bc01a7c
vadimkibana <vadimkibana@gmail.com>
2024-08-02 13:40:55 +0200

    add match all function

--- packages/kbn-esql-ast/src/walker/helpers.ts
+++ packages/kbn-esql-ast/src/walker/helpers.ts
@@ -8,15 +8,32 @@
 
 import { ESQLProperNode } from '../types';
 
+export type NodeMatchTemplateKey<V> = V | V[] | RegExp;
+export type NodeMatchTemplate = {
+  [K in keyof ESQLProperNode]?: NodeMatchTemplateKey<ESQLProperNode[K]>;
+};
+
+/**
+ * Creates a predicate function which matches a single AST node against a
+ * template object. The template object should have the same keys as the
+ * AST node, and the values should be:
+ *
+ * - An array matches if the node key is in the array.
+ * - A RegExp matches if the node key matches the RegExp.
+ * - Any other value matches if the node key is triple-equal to the value.
+ *
+ * @param template Template from which to create a predicate function.
+ * @returns A predicate function that matches nodes against the template.
+ */
 export const templateToPredicate = (
-  template: Partial<ESQLProperNode>
+  template: NodeMatchTemplate
 ): ((node: ESQLProperNode) => boolean) => {
   const keys = Object.keys(template) as Array<keyof ESQLProperNode>;
   const predicate = (child: ESQLProperNode) => {
     for (const key of keys) {
       const matcher = template[key];
       if (matcher instanceof Array) {
-        if (!matcher.includes(child[key])) {
+        if (!(matcher as any[]).includes(child[key])) {
           return false;
         }
       } else if (matcher instanceof RegExp) {
@@ -27,6 +44,7 @@ export const templateToPredicate = (
         return false;
       }
     }
+
     return true;
   };
 
