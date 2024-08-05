3edd45bb4afd3484eb542e2b2f7238a74a07c018
criamico <mariacristina.amico@elastic.co>
2024-08-05 11:07:03 +0200

    Improve query and other parts of the task

--- x-pack/plugins/fleet/server/tasks/unenroll_inactive_agents_task.ts
+++ x-pack/plugins/fleet/server/tasks/unenroll_inactive_agents_task.ts
@@ -6,7 +6,12 @@
  */
 
 import { SavedObjectsClient } from '@kbn/core/server';
-import type { CoreSetup, Logger } from '@kbn/core/server';
+import type {
+  CoreSetup,
+  ElasticsearchClient,
+  Logger,
+  SavedObjectsClientContract,
+} from '@kbn/core/server';
 import type {
   ConcreteTaskInstance,
   TaskManagerSetupContract,
@@ -16,24 +21,22 @@ import { getDeleteTaskRunResult } from '@kbn/task-manager-plugin/server/task';
 import type { LoggerFactory } from '@kbn/core/server';
 import { errors } from '@elastic/elasticsearch';
 
-import { SO_SEARCH_LIMIT, AGENTS_PREFIX } from '../constants';
+import { SO_SEARCH_LIMIT, AGENTS_PREFIX, AGENT_POLICY_SAVED_OBJECT_TYPE } from '../constants';
 import { getAgentsByKuery } from '../services/agents';
 import { unenrollBatch } from '../services/agents/unenroll_action_runner';
+import { agentPolicyService } from '../services';
 
 export const TYPE = 'fleet:unenroll-inactive-agents-task';
 export const VERSION = '1.0.0';
 const TITLE = 'Fleet Deleted Files Periodic Tasks';
 const SCOPE = ['fleet'];
-const INTERVAL = '1d';
+const INTERVAL = '10m';
+const TIMEOUT = '1m';
 
 interface UnenrollInactiveAgentsTaskSetupContract {
   core: CoreSetup;
   taskManager: TaskManagerSetupContract;
   logFactory: LoggerFactory;
-}
-interface UnenrollInactiveAgentsParams {
-  timeout: string;
-  policyIds: string[];
 }
 
 interface UnenrollInactiveAgentsTaskStartContract {
@@ -45,25 +48,21 @@ export class UnenrollInactiveAgentsTask {
   private wasStarted: boolean = false;
   private abortController = new AbortController();
 
-  constructor(
-    setupContract: UnenrollInactiveAgentsTaskSetupContract,
-    params: UnenrollInactiveAgentsParams
-  ) {
+  constructor(setupContract: UnenrollInactiveAgentsTaskSetupContract) {
     const { core, taskManager, logFactory } = setupContract;
-    const { timeout } = params;
     this.logger = logFactory.get(this.taskId);
 
     taskManager.registerTaskDefinitions({
       [TYPE]: {
         title: TITLE,
-        timeout,
+        timeout: TIMEOUT,
         createTaskRunner: ({ taskInstance }: { taskInstance: ConcreteTaskInstance }) => {
           return {
             run: async () => {
-              return this.runTask(taskInstance, core, params);
+              return this.runTask(taskInstance, core);
             },
             cancel: async () => {
-              this.abortController.abort('task timed out');
+              this.abortController.abort('Task timed out');
             },
           };
         },
@@ -103,16 +102,58 @@ export class UnenrollInactiveAgentsTask {
     return `${TYPE}:${VERSION}`;
   }
 
-  private runTask = async (
-    taskInstance: ConcreteTaskInstance,
-    core: CoreSetup,
-    params: UnenrollInactiveAgentsParams
-  ) => {
+  private endRun(msg: string = '') {
+    this.logger.info(`[runTask()] ended${msg ? ': ' + msg : ''}`);
+  }
+
+  private async fetchAgentsWithUnenrollmentTimeout(
+    esClient: ElasticsearchClient,
+    soClient: SavedObjectsClientContract
+  ) {
+    this.logger.debug(
+      `Running UnenrollInactiveAgentsTask. Fetching agent policies with unenroll_timeout > 0`
+    );
+    // find all agent policies having unenroll_timeout > 0
+    const policiesKuery = `${AGENT_POLICY_SAVED_OBJECT_TYPE}.unenroll_timeout > 0`;
+    const agentPolicies = await agentPolicyService.list(soClient, {
+      kuery: policiesKuery,
+      perPage: SO_SEARCH_LIMIT,
+      withPackagePolicies: true,
+    });
+    const policyIds = agentPolicies?.items.map((policy) => policy.id);
+    this.logger.debug(
+      `UnenrollInactiveAgentsTask. Found "${policyIds.length}" agent policies with unenroll_timeout > 0`
+    );
+    if (!policyIds.length) {
+      this.endRun('No policies with with unenroll_timeout set');
+      return;
+    }
+
+    // find agents enrolled on above policies that are also inactive
+    const kuery = `(${AGENTS_PREFIX}.policy_id:${policyIds
+      .map((id) => `"${id}"`)
+      .join(' or ')}) and ${AGENTS_PREFIX}.status: inactive`;
+    const res = await getAgentsByKuery(esClient, soClient, {
+      kuery,
+      showInactive: true,
+      page: 1,
+      perPage: SO_SEARCH_LIMIT,
+    });
+    if (!res.agents.length) {
+      this.endRun('No inactive agents to unenroll');
+      return;
+    }
+    this.logger.debug(
+      `UnenrollInactiveAgentsTask. Found "${res.total}" inactive agents to unenroll`
+    );
+    return res;
+  }
+
+  public runTask = async (taskInstance: ConcreteTaskInstance, core: CoreSetup) => {
     if (!this.wasStarted) {
       this.logger.debug('[runTask()] Aborted. Task not started yet');
       return;
     }
-    const { policyIds, timeout } = params;
     // Check that this task is current
     if (taskInstance.id !== this.taskId) {
       this.logger.debug(
@@ -123,47 +164,31 @@ export class UnenrollInactiveAgentsTask {
 
     this.logger.info(`[runTask()] started`);
 
-    const endRun = (msg: string = '') => {
-      this.logger.info(`[runTask()] ended${msg ? ': ' + msg : ''}`);
-    };
-
     const [coreStart] = await core.getStartServices();
     const esClient = coreStart.elasticsearch.client.asInternalUser;
     const soClient = new SavedObjectsClient(coreStart.savedObjects.createInternalRepository());
 
     try {
-      this.logger.debug(
-        `Running UnenrollInactiveAgentsTask. Unenrolling inactive agents with policy ids ${policyIds
-          .map((id) => `"${id}"`)
-          .join(', ')} after timeout of ${timeout}m is reached.`
-      );
-      const kuery = `(${AGENTS_PREFIX}.policy_id:${policyIds
-        .map((id) => `"${id}"`)
-        .join(' or ')}) and ${AGENTS_PREFIX}.status: inactive`;
+      const res = await this.fetchAgentsWithUnenrollmentTimeout(esClient, soClient);
 
-      const res = await getAgentsByKuery(esClient, soClient, {
-        kuery,
-        showInactive: true,
-        page: 1,
-        perPage: SO_SEARCH_LIMIT,
-      });
-      this.logger.debug(`Attempting unenrollment of ${res.total} agents.`);
+      this.logger.debug(`Attempting unenrollment of ${res?.total} inactive agents.`);
       const actionId = await unenrollBatch(soClient, esClient, res.agents, {
         revoke: true,
         force: true,
       });
+      this.logger.debug(
+        `Executed unenrollment of ${res?.total} inactive agents.with actionId: ${actionId}`
+      );
 
-      this.logger.debug(`Executed unenrollment with actionId: ${actionId}`);
-
-      endRun('success');
+      this.endRun('success');
     } catch (err) {
       if (err instanceof errors.RequestAbortedError) {
         this.logger.warn(`Request aborted due to timeout: ${err}`);
-        endRun();
+        this.endRun();
         return;
       }
       this.logger.error(err);
-      endRun('error');
+      this.endRun('error');
     }
   };
 }
