93615b0dcd66986fe84523689e7c46cb219ebcc2
Dima Arnautov <dmitrii.arnautov@elastic.co>
2024-08-06 23:11:19 +0200

    fix ts error

--- x-pack/packages/ml/trained_models_utils/src/constants/trained_models.ts
+++ x-pack/packages/ml/trained_models_utils/src/constants/trained_models.ts
@@ -58,7 +58,10 @@ export const BUILT_IN_MODEL_TAG = 'prepackaged';
 
 export const ELASTIC_MODEL_TAG = 'elastic';
 
-export const ELASTIC_MODEL_DEFINITIONS: Record<string, ModelDefinition> = Object.freeze({
+export const ELASTIC_MODEL_DEFINITIONS: Record<
+  string,
+  Omit<ModelDefinition, 'supported'>
+> = Object.freeze({
   [ELSER_ID_V1]: {
     modelName: 'elser',
     hidden: true,
