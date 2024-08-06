f90259df041ad051d29714f1db890d9a2d50ba1f
konrad.szwarc <konrad.szwarc@elastic.co>
2024-08-06 13:40:11 +0200

    verbose

--- x-pack/plugins/osquery/cypress/cypress_base.config.ts
+++ x-pack/plugins/osquery/cypress/cypress_base.config.ts
@@ -11,7 +11,7 @@ import { safeLoad as loadYaml } from 'js-yaml';
 import { readFileSync } from 'fs';
 import type { YamlRoleDefinitions } from '@kbn/test-suites-serverless/shared/lib';
 import { setupUserDataLoader } from '@kbn/test-suites-serverless/functional/test_suites/security/cypress/support/setup_data_loader_tasks';
-import { samlAuthentication } from '@kbn/test-suites-xpack/security_solution_cypress/cypress/support/saml_auth';
+import { samlAuthentication } from '@kbn/security-solution-plugin/public/management/cypress/support/saml_authentication';
 import { getFailedSpecVideos } from './support/filter_videos';
 
 const ROLES_YAML_FILE_PATH = path.join(
