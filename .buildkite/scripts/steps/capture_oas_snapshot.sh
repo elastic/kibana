e112605ef22c76d8b7d20028e0dad98664b4290f
Jean-Louis Leysens <jloleysens@gmail.com>
2024-08-06 12:43:06 +0200

    update generate command

--- .buildkite/scripts/steps/capture_oas_snapshot.sh
+++ .buildkite/scripts/steps/capture_oas_snapshot.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 echo --- Capture OAS snapshot
-cmd="node scripts/capture_oas_snapshot --access public --include-path /api/status --include-path /api/alerting/rule/ --include-path /api/alerting/rules"
+cmd="node scripts/capture_oas_snapshot --include-path /api/status --include-path /api/alerting/rule/ --include-path /api/alerting/rules"
 if is_pr && ! is_auto_commit_disabled; then
   cmd="$cmd --update"
 fi
