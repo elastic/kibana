7243a5c3b5dea3211667d67569b6b100639597f1
Kevin Delemme <kevin.delemme@elastic.co>
2024-08-07 13:26:11 -0400

    chore(rca): remove revisions (#189983)
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Maryam Saeidi <maryam.saeidi@elastic.co>

--- x-pack/plugins/observability_solution/investigate_app/public/components/investigate_view/index.tsx
+++ x-pack/plugins/observability_solution/investigate_app/public/components/investigate_view/index.tsx
@@ -6,15 +6,13 @@
  */
 import datemath from '@elastic/datemath';
 import { EuiFlexGroup, EuiFlexItem, EuiLoadingSpinner } from '@elastic/eui';
-import type { InvestigateWidget, InvestigateWidgetCreate } from '@kbn/investigate-plugin/public';
-import { DATE_FORMAT_ID } from '@kbn/management-settings-ids';
+import type { InvestigateWidgetCreate } from '@kbn/investigate-plugin/public';
 import { AuthenticatedUser } from '@kbn/security-plugin/common';
-import { keyBy, omit, pick } from 'lodash';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { keyBy, noop } from 'lodash';
+import React, { useEffect, useMemo, useRef } from 'react';
 import useAsync from 'react-use/lib/useAsync';
 import { useDateRange } from '../../hooks/use_date_range';
 import { useKibana } from '../../hooks/use_kibana';
-import { getOverridesFromGlobalParameters } from '../../utils/get_overrides_from_global_parameters';
 import { AddNoteUI } from '../add_note_ui';
 import { AddObservationUI } from '../add_observation_ui';
 import { InvestigateSearchBar } from '../investigate_search_bar';
@@ -22,34 +20,25 @@ import { InvestigateWidgetGrid } from '../investigate_widget_grid';
 
 function InvestigateViewWithUser({ user }: { user: AuthenticatedUser }) {
   const {
-    core: { uiSettings },
     dependencies: {
       start: { investigate },
     },
   } = useKibana();
   const widgetDefinitions = useMemo(() => investigate.getWidgetDefinitions(), [investigate]);
   const [range, setRange] = useDateRange();
-  const [searchBarFocused, setSearchBarFocused] = useState(false);
 
   const {
     addItem,
-    setItemPositions,
-    setItemTitle,
     copyItem,
     deleteItem,
     investigation,
-    lockItem,
-    setItemParameters,
     setGlobalParameters,
-    unlockItem,
-    revision,
+    renderableInvestigation,
   } = investigate.useInvestigation({
     user,
     from: range.start.toISOString(),
     to: range.end.toISOString(),
   });
-
-  const [_editingItem, setEditingItem] = useState<InvestigateWidget | undefined>(undefined);
 
   const createWidget = (widgetCreate: InvestigateWidgetCreate) => {
     return addItem(widgetCreate);
@@ -59,30 +48,20 @@ function InvestigateViewWithUser({ user }: { user: AuthenticatedUser }) {
   createWidgetRef.current = createWidget;
 
   useEffect(() => {
-    const itemIds = revision?.items.map((item) => item.id) ?? [];
-    setEditingItem((prevEditingItem) => {
-      if (prevEditingItem && !itemIds.includes(prevEditingItem.id)) {
-        return undefined;
-      }
-      return prevEditingItem;
-    });
-  }, [revision]);
-
-  useEffect(() => {
     if (
-      revision?.parameters.timeRange.from &&
-      revision?.parameters.timeRange.to &&
-      range.start.toISOString() !== revision.parameters.timeRange.from &&
-      range.end.toISOString() !== revision.parameters.timeRange.to
+      renderableInvestigation?.parameters.timeRange.from &&
+      renderableInvestigation?.parameters.timeRange.to &&
+      range.start.toISOString() !== renderableInvestigation.parameters.timeRange.from &&
+      range.end.toISOString() !== renderableInvestigation.parameters.timeRange.to
     ) {
       setRange({
-        from: revision.parameters.timeRange.from,
-        to: revision.parameters.timeRange.to,
+        from: renderableInvestigation.parameters.timeRange.from,
+        to: renderableInvestigation.parameters.timeRange.to,
       });
     }
   }, [
-    revision?.parameters.timeRange.from,
-    revision?.parameters.timeRange.to,
+    renderableInvestigation?.parameters.timeRange.from,
+    renderableInvestigation?.parameters.timeRange.to,
     range.start,
     range.end,
     setRange,
@@ -91,7 +70,7 @@ function InvestigateViewWithUser({ user }: { user: AuthenticatedUser }) {
   const gridItems = useMemo(() => {
     const widgetDefinitionsByType = keyBy(widgetDefinitions, 'type');
 
-    return revision?.items.map((item) => {
+    return renderableInvestigation?.items.map((item) => {
       const definitionForType = widgetDefinitionsByType[item.type];
 
       return (
@@ -103,21 +82,13 @@ function InvestigateViewWithUser({ user }: { user: AuthenticatedUser }) {
           columns: item.columns,
           rows: item.rows,
           chrome: definitionForType.chrome,
-          locked: item.locked,
           loading: item.loading,
-          overrides: item.locked
-            ? getOverridesFromGlobalParameters(
-                pick(item.parameters, 'filters', 'timeRange'),
-                revision.parameters,
-                uiSettings.get<string>(DATE_FORMAT_ID) ?? 'Browser'
-              )
-            : [],
         } ?? []
       );
     });
-  }, [revision, widgetDefinitions, uiSettings]);
+  }, [renderableInvestigation, widgetDefinitions]);
 
-  if (!investigation || !revision || !gridItems) {
+  if (!investigation || !renderableInvestigation || !gridItems) {
     return <EuiLoadingSpinner />;
   }
 
@@ -136,17 +107,11 @@ function InvestigateViewWithUser({ user }: { user: AuthenticatedUser }) {
                     to: datemath.parse(dateRange.to)!.toISOString(),
                   };
                   await setGlobalParameters({
-                    ...revision.parameters,
+                    ...renderableInvestigation.parameters,
                     timeRange: nextDateRange,
                   });
 
                   setRange(nextDateRange);
-                }}
-                onFocus={() => {
-                  setSearchBarFocused(true);
-                }}
-                onBlur={() => {
-                  setSearchBarFocused(false);
                 }}
               />
             </EuiFlexItem>
@@ -155,16 +120,7 @@ function InvestigateViewWithUser({ user }: { user: AuthenticatedUser }) {
               <InvestigateWidgetGrid
                 items={gridItems}
                 onItemsChange={async (nextGridItems) => {
-                  return setItemPositions(
-                    nextGridItems.map((gridItem) => ({
-                      columns: gridItem.columns,
-                      rows: gridItem.rows,
-                      id: gridItem.id,
-                    }))
-                  );
-                }}
-                onItemTitleChange={async (item, title) => {
-                  return setItemTitle(item.id, title);
+                  noop();
                 }}
                 onItemCopy={async (copiedItem) => {
                   return copyItem(copiedItem.id);
@@ -172,30 +128,12 @@ function InvestigateViewWithUser({ user }: { user: AuthenticatedUser }) {
                 onItemDelete={async (deletedItem) => {
                   return deleteItem(deletedItem.id);
                 }}
-                onItemLockToggle={async (toggledItem) => {
-                  return toggledItem.locked ? unlockItem(toggledItem.id) : lockItem(toggledItem.id);
-                }}
-                fadeLockedItems={searchBarFocused}
-                onItemOverrideRemove={async (updatedItem, override) => {
-                  // TODO: remove filters
-                  const itemToUpdate = revision.items.find((item) => item.id === updatedItem.id);
-                  if (itemToUpdate) {
-                    return setItemParameters(updatedItem.id, {
-                      ...revision.parameters,
-                      ...omit(itemToUpdate.parameters, override.id),
-                    });
-                  }
-                }}
-                onItemEditClick={(itemToEdit) => {
-                  setEditingItem(revision.items.find((item) => item.id === itemToEdit.id));
-                }}
               />
             </EuiFlexItem>
           </EuiFlexGroup>
 
           <AddObservationUI
-            filters={revision.parameters.filters}
-            timeRange={revision.parameters.timeRange}
+            timeRange={renderableInvestigation.parameters.timeRange}
             onWidgetAdd={(widget) => {
               return createWidgetRef.current(widget);
             }}
@@ -206,8 +144,7 @@ function InvestigateViewWithUser({ user }: { user: AuthenticatedUser }) {
       <EuiFlexItem grow={2}>
         <AddNoteUI
           user={user}
-          filters={revision.parameters.filters}
-          timeRange={revision.parameters.timeRange}
+          timeRange={renderableInvestigation.parameters.timeRange}
           onWidgetAdd={(widget) => {
             return createWidgetRef.current(widget);
           }}
