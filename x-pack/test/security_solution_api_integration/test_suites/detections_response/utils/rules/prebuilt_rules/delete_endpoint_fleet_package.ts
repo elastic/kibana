6a3c98dbe15c50fa4205fc41de5f2c8954b1fba9
Dmitrii Shevchenko <dmitrii.shevchenko@elastic.co>
2024-08-07 14:45:52 +0200

    [Security Solution] Detection rules bootstrap endpoint (#189518)
    **Resolves: https://github.com/elastic/kibana/issues/187647**
    
    ## Summary
    
    Added a new API endpoint `POST
    /internal/detection_engine/prebuilt_rules/_bootstrap`. This endpoint is
    responsible for installing the necessary packages for prebuilt detection
    rules to function properly. This allows us to avoid calling Fleet
    directly from FE and helps encapsulate complex logic of the package
    version selection in a single place on the backend.
    
    Currently, it installs or upgrades (if already installed) two packages:
    `endpoint` and `security_detection_engine`.
    
    The response looks like this:
    
    ```json5
    {
      packages: [
        {
          name: 'detection_engine',
          version: '1.0.0',
          status: 'installed',
        },
        {
          name: 'endpoint',
          version: '1.0.0',
          status: 'already_installed',
        },
      ],
    }
    ```
    
    We call this endpoint from Kibana every time a user lands on any
    security solution page. The endpoint checks if the required packages are
    missing or if a newer version is available. If so, the newer version is
    installed, and the package status will be `installed` in the response.
    If all packages are up-to-date, the package status will be
    `already_installed` in the response. This allows us to invalidate
    prebuilt rule endpoints more efficiently and avoid sending extra
    requests from Kibana:
    
    ```ts
    if (
      response?.packages.find((pkg) => pkg.name === PREBUILT_RULES_PACKAGE_NAME)?.status === 'installed'
    ) {
      // Invalidate other pre-packaged rules related queries. We need to do
      // that only if the prebuilt rules package was installed, indicating
      // that there might be new rules to install.
      invalidatePrePackagedRulesStatus();
      invalidatePrebuiltRulesInstallReview();
      invalidatePrebuiltRulesUpdateReview();
    }
    ```
    
    The performance gain is that we do not invalidate prebuilt rules when
    the package is already installed.
    
    Previously:
    `Fetch rules initially -> Upgrade rules package -(always)-> Re-fetch
    rules`
    
    Now:
    `Fetch rules initially -> Upgrade rules package -(only if there's a new
    package version)-> Re-fetch rules`
    
    This will result in fewer redundant API requests from Kibana.

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/utils/rules/prebuilt_rules/delete_endpoint_fleet_package.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/utils/rules/prebuilt_rules/delete_endpoint_fleet_package.ts
@@ -6,26 +6,24 @@
  */
 
 import { epmRouteService } from '@kbn/fleet-plugin/common';
-import { PREBUILT_RULES_PACKAGE_NAME } from '@kbn/security-solution-plugin/common/detection_engine/constants';
+import { ENDPOINT_PACKAGE_NAME } from '@kbn/security-solution-plugin/common/detection_engine/constants';
 import type SuperTest from 'supertest';
 
 /**
- * Delete the security_detection_engine package using fleet API.
+ * Delete the endpoint package using fleet API.
  *
  * @param supertest Supertest instance
  */
-export async function deletePrebuiltRulesFleetPackage(supertest: SuperTest.Agent) {
+export async function deleteEndpointFleetPackage(supertest: SuperTest.Agent) {
   const resp = await supertest
-    .get(epmRouteService.getInfoPath(PREBUILT_RULES_PACKAGE_NAME))
+    .get(epmRouteService.getInfoPath(ENDPOINT_PACKAGE_NAME))
     .set('kbn-xsrf', 'true')
     .set('elastic-api-version', '2023-10-31')
     .send();
 
   if (resp.status === 200 && resp.body.response.status === 'installed') {
     await supertest
-      .delete(
-        epmRouteService.getRemovePath(PREBUILT_RULES_PACKAGE_NAME, resp.body.response.version)
-      )
+      .delete(epmRouteService.getRemovePath(ENDPOINT_PACKAGE_NAME, resp.body.response.version))
       .set('kbn-xsrf', 'true')
       .send({ force: true });
   }
