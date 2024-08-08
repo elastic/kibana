110a5cd8b7c6511a91f58c81b7e47b2614b9b19c
Kevin Delemme <kevin.delemme@elastic.co>
2024-08-08 09:30:02 -0400

    Add get

--- x-pack/plugins/observability_solution/investigate_app/server/routes/get_global_investigate_app_server_route_repository.ts
+++ x-pack/plugins/observability_solution/investigate_app/server/routes/get_global_investigate_app_server_route_repository.ts
@@ -11,6 +11,8 @@ import { createInvestigation } from '../services/create_investigation';
 import { investigationRepositoryFactory } from '../services/investigation_repository';
 import { createInvestigateAppServerRoute } from './create_investigate_app_server_route';
 import { findInvestigations } from '../services/find_investigations';
+import { getInvestigationParamsSchema } from '../../common/schema/get';
+import { getInvestigation } from '../services/get_investigation';
 
 const createInvestigationRoute = createInvestigateAppServerRoute({
   endpoint: 'POST /api/observability/investigations 2023-10-31',
@@ -40,10 +42,25 @@ const findInvestigationsRoute = createInvestigateAppServerRoute({
   },
 });
 
+const getInvestigationRoute = createInvestigateAppServerRoute({
+  endpoint: 'GET /api/observability/investigations/{id} 2023-10-31',
+  options: {
+    tags: [],
+  },
+  params: getInvestigationParamsSchema,
+  handler: async (params) => {
+    const soClient = (await params.context.core).savedObjects.client;
+    const repository = investigationRepositoryFactory({ soClient, logger: params.logger });
+
+    return await getInvestigation(params.params.path, repository);
+  },
+});
+
 export function getGlobalInvestigateAppServerRouteRepository() {
   return {
     ...createInvestigationRoute,
     ...findInvestigationsRoute,
+    ...getInvestigationRoute,
   };
 }
 
