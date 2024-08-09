a07f19356c17a7b865e7c3e46ad66090bd94bc84
Dominique Clarke <dominique.clarke@elastic.co>
2024-08-08 20:51:10 -0400

    Update x-pack/packages/kbn-synthetics-private-location/jest.config.js

--- x-pack/packages/kbn-synthetics-private-location/jest.config.js
+++ x-pack/packages/kbn-synthetics-private-location/jest.config.js
@@ -8,5 +8,5 @@
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-synthetics-private-location'],
+  roots: ['<rootDir>/x-pack/packages/kbn-synthetics-private-location'],
 };
