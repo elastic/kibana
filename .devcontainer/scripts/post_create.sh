e5ca28024cce17dc0d660b51524246b87a088a77
Brad White <brad.white@elastic.co>
2024-08-13 14:07:30 -0600

    remove post_create

--- .devcontainer/scripts/post_create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# The node_modules are mounted from the host machine which can be a different platform,
-# so we re-install the dependencies to ensure they are compatible.
-#
-# yarn kbn reset && yarn kbn bootstrap
