b17cad5a9fec8f0d9102bc6175538431d335ec16
Kevin Delemme <kevin.delemme@elastic.co>
2024-08-14 11:20:05 -0400

    simplify export

--- packages/kbn-investigation-schema/index.ts
+++ packages/kbn-investigation-schema/index.ts
@@ -6,31 +6,18 @@
  * Side Public License, v 1.
  */
 
-export type { CreateInvestigationInput, CreateInvestigationResponse } from './src/schema/create';
-export type {
-  CreateInvestigationNoteInput,
-  CreateInvestigationNoteResponse,
-} from './src/schema/create_notes';
-export type { FindInvestigationsParams, FindInvestigationsResponse } from './src/schema/find';
-export type { GetInvestigationParams, GetInvestigationResponse } from './src/schema/get';
-export type { GetInvestigationNotesResponse } from './src/schema/get_notes';
+export type * from './src/schema/create';
+export type * from './src/schema/create_notes';
+export type * from './src/schema/delete';
+export type * from './src/schema/find';
+export type * from './src/schema/get';
+export type * from './src/schema/get_notes';
+export type * from './src/schema/origin';
 
-export {
-  createInvestigationParamsSchema,
-  createInvestigationResponseSchema,
-} from './src/schema/create';
-export {
-  createInvestigationNoteParamsSchema,
-  createInvestigationNoteResponseSchema,
-} from './src/schema/create_notes';
-export { deleteInvestigationParamsSchema } from './src/schema/delete';
-export {
-  findInvestigationsParamsSchema,
-  findInvestigationsResponseSchema,
-} from './src/schema/find';
-export { getInvestigationParamsSchema, getInvestigationResponseSchema } from './src/schema/get';
-export {
-  getInvestigationNotesParamsSchema,
-  getInvestigationNotesResponseSchema,
-} from './src/schema/get_notes';
-export { alertOriginSchema, blankOriginSchema } from './src/schema/origin';
+export * from './src/schema/create';
+export * from './src/schema/create_notes';
+export * from './src/schema/delete';
+export * from './src/schema/find';
+export * from './src/schema/get';
+export * from './src/schema/get_notes';
+export * from './src/schema/origin';
