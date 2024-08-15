elastic/kibana

commit edf5c762d8ad0057e4872ba10e92c0deb34a7003

[ES|QL] Stronger typing for ESQL field interface (#189941)
## Summary

The heart of this PR is [this
change](https://github.com/elastic/kibana/pull/189941/files#diff-88513481c44d7b2de70ca2f7826c2b1fb1d8bda5b308aab0f8917a42ac2c24b7R11-R94)
where I clean up and clarify the various data-type-related types
floating around in the engines + [this
change](https://github.com/elastic/kibana/pull/189941/files#diff-f48b526b82119bd591cf781262173d7a0233d236ab26496a4c06f5ea9a441561R21)
where I add strong typing to the ES|QL field interface.

Pretty much everything else is a result of that. For example, strongly
typing the fields and test helpers highlighted a bunch of tests that
were still using Kibana types instead of Elasticsearch types. So, then
those had to be updated.

There's more work to do to extend the strong field typing to the rest of
the engines, but this got big and I decided to do it piece-meal.

Next plans
- Extend typing to subroutines in autocomplete and validation engines
(e.g. the stuff in `factories.ts`)
- Add typing to the [variable
interface](https://github.com/elastic/kibana/pull/189941/files#diff-f48b526b82119bd591cf781262173d7a0233d236ab26496a4c06f5ea9a441561R13)
- Consider merging `time_literal` and `time_duration` types
- "It looks like timespan literals are the way to write a constant
time_duration. and time_durations can only be constants at the moment.
so they aren't the same, but sure are about the same" - Nik
- Consider merging
`packages/kbn-esql-validation-autocomplete/src/shared/esql_types.ts`
with
`packages/kbn-esql-validation-autocomplete/src/definitions/types.ts` in
some common place


### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

packages/kbn-esql-validation-autocomplete/scripts/generate_function_validation_tests.ts
@@ -18,16 +18,17 @@ import { getFunctionSignatures } from '../src/definitions/helpers';
 import { timeUnits } from '../src/definitions/literals';
 import { nonNullable } from '../src/shared/helpers';
 import {
-  SupportedFieldType,
+  SupportedDataType,
   FunctionDefinition,
-  supportedFieldTypes,
-  isSupportedFieldType,
+  dataTypes,
+  isSupportedDataType,
+  fieldTypes,
 } from '../src/definitions/types';
 import { FUNCTION_DESCRIBE_BLOCK_NAME } from '../src/validation/function_describe_block_name';
 import { getMaxMinNumberOfParams } from '../src/validation/helpers';
 import { ESQL_NUMBER_TYPES, isNumericType, isStringType } from '../src/shared/esql_types';
 
-export const fieldNameFromType = (type: SupportedFieldType) => `${camelCase(type)}Field`;
+export const fieldNameFromType = (type: SupportedDataType) => `${camelCase(type)}Field`;
 
 function main() {
   const testCasesByFunction: Map<string, Map<string, string[]>> = new Map();
@@ -301,8 +302,8 @@ function generateWhereCommandTestsForEvalFunction(
   // TODO: not sure why there's this constraint...
   const supportedFunction = signatures.some(
     ({ returnType, params }) =>
-      [...ESQL_NUMBER_TYPES, 'string'].includes(returnType) &&
-      params.every(({ type }) => [...ESQL_NUMBER_TYPES, 'string'].includes(type))
+      [...ESQL_NUMBER_TYPES, 'string'].includes(returnType as string) &&
+      params.every(({ type }) => [...ESQL_NUMBER_TYPES, 'string'].includes(type as string))
   );
 
   if (!supportedFunction) {
@@ -312,7 +313,7 @@ function generateWhereCommandTestsForEvalFunction(
   const supportedSignatures = signatures.filter(({ returnType }) =>
     // TODO — not sure why the tests have this limitation... seems like any type
     // that can be part of a boolean expression should be allowed in a where clause
-    [...ESQL_NUMBER_TYPES, 'string'].includes(returnType)
+    [...ESQL_NUMBER_TYPES, 'string'].includes(returnType as string)
   );
   for (const { params, returnType, ...restSign } of supportedSignatures) {
     const correctMapping = getFieldMapping(params);
@@ -905,7 +906,7 @@ function generateStatsCommandTestsForGroupingFunction(
         fieldReplacedType
           // if a param of type time_literal or chrono_literal it will always be a literal
           // so no way to test the constantOnly thing
-          .filter((type) => !['time_literal', 'chrono_literal'].includes(type))
+          .filter((type) => !['time_literal'].includes(type as string))
           .map((type) => `Argument of [${name}] must be a constant, received [${type}Field]`)
       );
     }
@@ -965,7 +966,7 @@ function generateSortCommandTestsForAggFunction(
 
 const generateSortCommandTestsForGroupingFunction = generateSortCommandTestsForAggFunction;
 
-const fieldTypesToConstants: Record<SupportedFieldType, string> = {
+const fieldTypesToConstants: Record<SupportedDataType, string> = {
   text: '"a"',
   keyword: '"a"',
   double: '5.5',
@@ -984,14 +985,20 @@ const fieldTypesToConstants: Record<SupportedFieldType, string> = {
   geo_shape: 'to_geoshape("POINT (30 10)")',
   cartesian_point: 'to_cartesianpoint("POINT (30 10)")',
   cartesian_shape: 'to_cartesianshape("POINT (30 10)")',
+  null: 'NULL',
+  time_duration: '1 day',
+  // the following are never supplied
+  // by the ES function definitions. Just making types happy
+  time_literal: '1 day',
+  unsupported: '',
 };
 
-const supportedTypesAndFieldNames = supportedFieldTypes.map((type) => ({
+const supportedTypesAndFieldNames = fieldTypes.map((type) => ({
   name: fieldNameFromType(type),
   type,
 }));
 
-const supportedTypesAndConstants = supportedFieldTypes.map((type) => ({
+const supportedTypesAndConstants = dataTypes.map((type) => ({
   name: fieldTypesToConstants[type],
   type,
 }));
@@ -1029,7 +1036,7 @@ const toCartesianShapeSignature = evalFunctionDefinitions.find(
 const toVersionSignature = evalFunctionDefinitions.find(({ name }) => name === 'to_version')!;
 
 // We don't have full list for long, unsigned_long, etc.
-const nestedFunctions: Record<SupportedFieldType, string> = {
+const nestedFunctions: Record<SupportedDataType, string> = {
   double: prepareNestedFunction(toDoubleSignature),
   integer: prepareNestedFunction(toInteger),
   text: prepareNestedFunction(toStringSignature),
@@ -1046,7 +1053,7 @@ const nestedFunctions: Record<SupportedFieldType, string> = {
 };
 
 function getFieldName(
-  typeString: SupportedFieldType,
+  typeString: SupportedDataType,
   { useNestedFunction, isStats }: { useNestedFunction: boolean; isStats: boolean }
 ) {
   if (useNestedFunction && isStats) {
@@ -1082,7 +1089,7 @@ function tweakSignatureForRowCommand(signature: string): string {
    */
   let ret = signature;
   for (const [type, value] of Object.entries(fieldTypesToConstants)) {
-    ret = ret.replace(new RegExp(fieldNameFromType(type as SupportedFieldType), 'g'), value);
+    ret = ret.replace(new RegExp(fieldNameFromType(type as SupportedDataType), 'g'), value);
   }
   return ret;
 }
@@ -1101,8 +1108,8 @@ function getFieldMapping(
   };
 
   return params.map(({ name: _name, type, constantOnly, literalOptions, ...rest }) => {
-    const typeString: string = type;
-    if (isSupportedFieldType(typeString)) {
+    const typeString: string = type as string;
+    if (isSupportedDataType(typeString)) {
       if (useLiterals && literalOptions) {
         return {
           name: `"${literalOptions[0]}"`,
@@ -1146,7 +1153,7 @@ function generateIncorrectlyTypedParameters(
   name: string,
   signatures: FunctionDefinition['signatures'],
   currentParams: FunctionDefinition['signatures'][number]['params'],
-  availableFields: Array<{ name: string; type: SupportedFieldType }>
+  availableFields: Array<{ name: string; type: SupportedDataType }>
 ) {
   const literalValues = {
     string: `"a"`,
@@ -1167,7 +1174,7 @@ function generateIncorrectlyTypedParameters(
 
       if (type !== 'any') {
         // try to find an unacceptable field
-        const unacceptableField: { name: string; type: SupportedFieldType } | undefined =
+        const unacceptableField: { name: string; type: SupportedDataType } | undefined =
           availableFields
             // sort to make the test deterministic
             .sort((a, b) => a.type.localeCompare(b.type))
@@ -1187,7 +1194,7 @@ function generateIncorrectlyTypedParameters(
       }
 
       // failed to find a bad field... they must all be acceptable
-      const acceptableField: { name: string; type: SupportedFieldType } | undefined =
+      const acceptableField: { name: string; type: SupportedDataType } | undefined =
         type === 'any'
           ? availableFields[0]
           : availableFields.find(({ type: fieldType }) => fieldType === type);

packages/kbn-esql-validation-autocomplete/src/validation/validation.test.ts
@@ -11,7 +11,13 @@ import { writeFile, readFile } from 'fs/promises';
 import { ignoreErrorsMap, validateQuery } from './validation';
 import { evalFunctionDefinitions } from '../definitions/functions';
 import { getFunctionSignatures } from '../definitions/helpers';
-import { FunctionDefinition, SupportedFieldType, supportedFieldTypes } from '../definitions/types';
+import {
+  FieldType,
+  FunctionDefinition,
+  SupportedDataType,
+  dataTypes,
+  fieldTypes as _fieldTypes,
+} from '../definitions/types';
 import { timeUnits, timeUnitsToSuggest } from '../definitions/literals';
 import { statsAggregationFunctionDefinitions } from '../definitions/aggs';
 import capitalize from 'lodash/capitalize';
@@ -29,6 +35,8 @@ import {
 } from '../__tests__/helpers';
 import { validationFromCommandTestSuite as runFromTestSuite } from './__tests__/test_suites/validation.command.from';
 import { Setup, setup } from './__tests__/helpers';
+
+const fieldTypes = _fieldTypes.filter((type) => type !== 'unsupported');
 
 const NESTING_LEVELS = 4;
 const NESTED_DEPTHS = Array(NESTING_LEVELS)
@@ -76,7 +84,7 @@ function getLiteralType(typeString: 'time_literal') {
   return `1 ${literals[typeString]}`;
 }
 
-export const fieldNameFromType = (type: SupportedFieldType) => `${camelCase(type)}Field`;
+export const fieldNameFromType = (type: FieldType) => `${camelCase(type)}Field`;
 
 function getFieldName(
   typeString: string,
@@ -127,8 +135,8 @@ function getFieldMapping(
     date: 'now()',
   };
   return params.map(({ name: _name, type, constantOnly, literalOptions, ...rest }) => {
-    const typeString: string = type;
-    if (supportedFieldTypes.includes(typeString as SupportedFieldType)) {
+    const typeString: string = type as string;
+    if (dataTypes.includes(typeString as SupportedDataType)) {
       if (useLiterals && literalOptions) {
         return {
           name: `"${literalOptions[0]}"`,
@@ -541,13 +549,6 @@ describe('validation logic', () => {
       testErrorsAndWarnings('from index | keep m*', ['Unknown column [m*]']);
       testErrorsAndWarnings('from index | keep *m', ['Unknown column [*m]']);
       testErrorsAndWarnings('from index | keep d*m', ['Unknown column [d*m]']);
-      testErrorsAndWarnings(
-        'from unsupported_index | keep unsupported_field',
-        [],
-        [
-          'Field [unsupported_field] cannot be retrieved, it is unsupported or not indexed; returning null',
-        ]
-      );
 
       testErrorsAndWarnings(
         `FROM index | STATS ROUND(AVG(doubleField * 1.5)), COUNT(*), MIN(doubleField * 10) | KEEP \`MIN(doubleField * 10)\``,
@@ -874,7 +875,7 @@ describe('validation logic', () => {
         []
       );
 
-      for (const field of supportedFieldTypes) {
+      for (const field of fieldTypes) {
         testErrorsAndWarnings(`from a_index | where ${fieldNameFromType(field)} IS NULL`, []);
         testErrorsAndWarnings(`from a_index | where ${fieldNameFromType(field)} IS null`, []);
         testErrorsAndWarnings(`from a_index | where ${fieldNameFromType(field)} is null`, []);
@@ -937,7 +938,7 @@ describe('validation logic', () => {
       testErrorsAndWarnings('from a_index | eval a=["a", "b"]', []);
       testErrorsAndWarnings('from a_index | eval a=null', []);
 
-      for (const field of supportedFieldTypes) {
+      for (const field of fieldTypes) {
         testErrorsAndWarnings(`from a_index | eval ${fieldNameFromType(field)} IS NULL`, []);
         testErrorsAndWarnings(`from a_index | eval ${fieldNameFromType(field)} IS null`, []);
         testErrorsAndWarnings(`from a_index | eval ${fieldNameFromType(field)} is null`, []);
@@ -12110,8 +12111,6 @@ describe('validation logic', () => {
 
         testErrorsAndWarnings('from a_index | stats var = max(dateField)', []);
         testErrorsAndWarnings('from a_index | stats max(dateField)', []);
-        testErrorsAndWarnings('from a_index | stats var = max(datePeriodField)', []);
-        testErrorsAndWarnings('from a_index | stats max(datePeriodField)', []);
         testErrorsAndWarnings('from a_index | stats var = max(booleanField)', []);
         testErrorsAndWarnings('from a_index | stats max(booleanField)', []);
         testErrorsAndWarnings('from a_index | stats var = max(ipField)', []);
@@ -12150,14 +12149,6 @@ describe('validation logic', () => {
         ]);
 
         testErrorsAndWarnings('from a_index | where max(dateField) > 0', [
-          'WHERE does not support function max',
-        ]);
-
-        testErrorsAndWarnings('from a_index | where max(datePeriodField)', [
-          'WHERE does not support function max',
-        ]);
-
-        testErrorsAndWarnings('from a_index | where max(datePeriodField) > 0', [
           'WHERE does not support function max',
         ]);
 
@@ -12240,23 +12231,6 @@ describe('validation logic', () => {
         testErrorsAndWarnings('from a_index | eval max(dateField) > 0', [
           'EVAL does not support function max',
         ]);
-
-        testErrorsAndWarnings('from a_index | eval var = max(datePeriodField)', [
-          'EVAL does not support function max',
-        ]);
-
-        testErrorsAndWarnings('from a_index | eval var = max(datePeriodField) > 0', [
-          'EVAL does not support function max',
-        ]);
-
-        testErrorsAndWarnings('from a_index | eval max(datePeriodField)', [
-          'EVAL does not support function max',
-        ]);
-
-        testErrorsAndWarnings('from a_index | eval max(datePeriodField) > 0', [
-          'EVAL does not support function max',
-        ]);
-
         testErrorsAndWarnings('from a_index | eval var = max(booleanField)', [
           'EVAL does not support function max',
         ]);
@@ -12459,8 +12433,6 @@ describe('validation logic', () => {
 
         testErrorsAndWarnings('from a_index | stats var = min(dateField)', []);
         testErrorsAndWarnings('from a_index | stats min(dateField)', []);
-        testErrorsAndWarnings('from a_index | stats var = min(datePeriodField)', []);
-        testErrorsAndWarnings('from a_index | stats min(datePeriodField)', []);
         testErrorsAndWarnings('from a_index | stats var = min(booleanField)', []);
         testErrorsAndWarnings('from a_index | stats min(booleanField)', []);
         testErrorsAndWarnings('from a_index | stats var = min(ipField)', []);
@@ -12501,15 +12473,6 @@ describe('validation logic', () => {
         testErrorsAndWarnings('from a_index | where min(dateField) > 0', [
           'WHERE does not support function min',
         ]);
-
-        testErrorsAndWarnings('from a_index | where min(datePeriodField)', [
-          'WHERE does not support function min',
-        ]);
-
-        testErrorsAndWarnings('from a_index | where min(datePeriodField) > 0', [
-          'WHERE does not support function min',
-        ]);
-
         testErrorsAndWarnings('from a_index | where min(booleanField)', [
           'WHERE does not support function min',
         ]);
@@ -12587,22 +12550,6 @@ describe('validation logic', () => {
         ]);
 
         testErrorsAndWarnings('from a_index | eval min(dateField) > 0', [
-          'EVAL does not support function min',
-        ]);
-
-        testErrorsAndWarnings('from a_index | eval var = min(datePeriodField)', [
-          'EVAL does not support function min',
-        ]);
-
-        testErrorsAndWarnings('from a_index | eval var = min(datePeriodField) > 0', [
-          'EVAL does not support function min',
-        ]);
-
-        testErrorsAndWarnings('from a_index | eval min(datePeriodField)', [
-          'EVAL does not support function min',
-        ]);
-
-        testErrorsAndWarnings('from a_index | eval min(datePeriodField) > 0', [
           'EVAL does not support function min',
         ]);
 
@@ -14101,14 +14048,6 @@ describe('validation logic', () => {
           ]
         );
 
-        testErrorsAndWarnings('from a_index | stats by bucket(dateField, datePeriodField)', [
-          'Argument of [bucket] must be a constant, received [datePeriodField]',
-        ]);
-
-        testErrorsAndWarnings('from a_index | stats by bin(dateField, datePeriodField)', [
-          'Argument of [bin] must be a constant, received [datePeriodField]',
-        ]);
-
         testErrorsAndWarnings(
           'from a_index | stats by bucket(dateField, integerField, now(), now())',
           ['Argument of [bucket] must be a constant, received [integerField]']
@@ -14610,18 +14549,6 @@ describe('validation logic', () => {
             'Argument of [bin] must be a constant, received [longField]',
           ]
         );
-
-        testErrorsAndWarnings('from a_index | sort bucket(dateField, datePeriodField)', [
-          'SORT does not support function bucket',
-        ]);
-
-        testErrorsAndWarnings('from a_index | stats bucket("2022", datePeriodField)', [
-          'Argument of [bucket] must be a constant, received [datePeriodField]',
-        ]);
-        testErrorsAndWarnings('from a_index | stats bucket(concat("20", "22"), datePeriodField)', [
-          'Argument of [bucket] must be [date], found value [concat("20","22")] type [keyword]',
-          'Argument of [bucket] must be a constant, received [datePeriodField]',
-        ]);
       });
 
       describe('percentile', () => {

examples/esql_validation_example/public/app.tsx
@@ -23,7 +23,7 @@ import {
 
 import type { CoreStart } from '@kbn/core/public';
 
-import { ESQLCallbacks, validateQuery } from '@kbn/esql-validation-autocomplete';
+import { ESQLCallbacks, ESQLRealField, validateQuery } from '@kbn/esql-validation-autocomplete';
 import { getAstAndSyntaxErrors } from '@kbn/esql-ast';
 import type { StartDependencies } from './plugin';
 import { CodeSnippet } from './code_snippet';
@@ -52,10 +52,11 @@ export const App = (props: { core: CoreStart; plugins: StartDependencies }) => {
             ['index1', 'anotherIndex', 'dataStream'].map((name) => ({ name, hidden: false }))
         : undefined,
       getFieldsFor: callbacksEnabled.fields
-        ? async () => [
-            { name: 'numberField', type: 'number' },
-            { name: 'stringField', type: 'string' },
-          ]
+        ? async () =>
+            [
+              { name: 'doubleField', type: 'double' },
+              { name: 'keywordField', type: 'keyword' },
+            ] as ESQLRealField[]
         : undefined,
       getPolicies: callbacksEnabled.policies
         ? async () => [

packages/kbn-esql-validation-autocomplete/src/__tests__/helpers.ts
@@ -7,22 +7,27 @@
  */
 
 import { camelCase } from 'lodash';
-import { supportedFieldTypes } from '../definitions/types';
+import { ESQLRealField } from '../validation/types';
+import { fieldTypes } from '../definitions/types';
 
-export const fields = [
-  ...supportedFieldTypes.map((type) => ({ name: `${camelCase(type)}Field`, type })),
+export const fields: ESQLRealField[] = [
+  ...fieldTypes
+    .map((type) => ({ name: `${camelCase(type)}Field`, type }))
+    .filter((f) => f.type !== 'unsupported'),
   { name: 'any#Char$Field', type: 'double' },
   { name: 'kubernetes.something.something', type: 'double' },
   { name: '@timestamp', type: 'date' },
 ];
 
-export const enrichFields = [
+export const enrichFields: ESQLRealField[] = [
   { name: 'otherField', type: 'text' },
   { name: 'yetAnotherField', type: 'double' },
 ];
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-export const unsupported_field = [{ name: 'unsupported_field', type: 'unsupported' }];
+export const unsupported_field: ESQLRealField[] = [
+  { name: 'unsupported_field', type: 'unsupported' },
+];
 
 export const indexes = [
   'a_index',
@@ -58,7 +63,8 @@ export function getCallbackMocks() {
         return unsupported_field;
       }
       if (/dissect|grok/.test(query)) {
-        return [{ name: 'firstWord', type: 'text' }];
+        const field: ESQLRealField = { name: 'firstWord', type: 'text' };
+        return [field];
       }
       return fields;
     }),

commit bb2cc70a3b9abec090da1e2fac3439a9e53ccb60

[ES|QL] improve test case generator script (#190441)
## Summary

Fixes a bug I introduced in
https://github.com/elastic/kibana/pull/189941

Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

packages/kbn-esql-validation-autocomplete/scripts/generate_function_validation_tests.ts
@@ -21,8 +21,8 @@ import {
   SupportedDataType,
   FunctionDefinition,
   dataTypes,
-  isSupportedDataType,
   fieldTypes,
+  isFieldType,
 } from '../src/definitions/types';
 import { FUNCTION_DESCRIBE_BLOCK_NAME } from '../src/validation/function_describe_block_name';
 import { getMaxMinNumberOfParams } from '../src/validation/helpers';
@@ -1110,7 +1110,7 @@ function getFieldMapping(
 
   return params.map(({ name: _name, type, constantOnly, literalOptions, ...rest }) => {
     const typeString: string = type as string;
-    if (isSupportedDataType(typeString)) {
+    if (isFieldType(typeString)) {
       if (useLiterals && literalOptions) {
         return {
           name: `"${literalOptions[0]}"`,

packages/kbn-esql-validation-autocomplete/src/validation/validation.test.ts
@@ -12284,6 +12284,38 @@ describe('validation logic', () => {
         testErrorsAndWarnings('from a_index | stats max(concat("20", "22"))', [
           'Argument of [max] must be [double], found value [concat("20","22")] type [keyword]',
         ]);
+
+        testErrorsAndWarnings('from a_index | stats var = max(textField)', [
+          'Argument of [max] must be [double], found value [textField] type [text]',
+        ]);
+
+        testErrorsAndWarnings('from a_index | stats max(textField)', [
+          'Argument of [max] must be [double], found value [textField] type [text]',
+        ]);
+
+        testErrorsAndWarnings('from a_index | where max(textField)', [
+          'WHERE does not support function max',
+        ]);
+
+        testErrorsAndWarnings('from a_index | where max(textField) > 0', [
+          'WHERE does not support function max',
+        ]);
+
+        testErrorsAndWarnings('from a_index | eval var = max(textField)', [
+          'EVAL does not support function max',
+        ]);
+
+        testErrorsAndWarnings('from a_index | eval var = max(textField) > 0', [
+          'EVAL does not support function max',
+        ]);
+
+        testErrorsAndWarnings('from a_index | eval max(textField)', [
+          'EVAL does not support function max',
+        ]);
+
+        testErrorsAndWarnings('from a_index | eval max(textField) > 0', [
+          'EVAL does not support function max',
+        ]);
       });
 
       describe('min', () => {
@@ -12605,6 +12637,38 @@ describe('validation logic', () => {
         testErrorsAndWarnings('from a_index | stats min("2022")', []);
         testErrorsAndWarnings('from a_index | stats min(concat("20", "22"))', [
           'Argument of [min] must be [double], found value [concat("20","22")] type [keyword]',
+        ]);
+
+        testErrorsAndWarnings('from a_index | stats var = min(textField)', [
+          'Argument of [min] must be [double], found value [textField] type [text]',
+        ]);
+
+        testErrorsAndWarnings('from a_index | stats min(textField)', [
+          'Argument of [min] must be [double], found value [textField] type [text]',
+        ]);
+
+        testErrorsAndWarnings('from a_index | where min(textField)', [
+          'WHERE does not support function min',
+        ]);
+
+        testErrorsAndWarnings('from a_index | where min(textField) > 0', [
+          'WHERE does not support function min',
+        ]);
+
+        testErrorsAndWarnings('from a_index | eval var = min(textField)', [
+          'EVAL does not support function min',
+        ]);
+
+        testErrorsAndWarnings('from a_index | eval var = min(textField) > 0', [
+          'EVAL does not support function min',
+        ]);
+
+        testErrorsAndWarnings('from a_index | eval min(textField)', [
+          'EVAL does not support function min',
+        ]);
+
+        testErrorsAndWarnings('from a_index | eval min(textField) > 0', [
+          'EVAL does not support function min',
         ]);
       });
 
@@ -14564,6 +14628,26 @@ describe('validation logic', () => {
             'Argument of [bin] must be a constant, received [longField]',
           ]
         );
+
+        testErrorsAndWarnings('from a_index | stats by bucket(dateField, textField)', [
+          'Argument of [bucket] must be a constant, received [textField]',
+        ]);
+
+        testErrorsAndWarnings('from a_index | stats by bin(dateField, textField)', [
+          'Argument of [bin] must be a constant, received [textField]',
+        ]);
+
+        testErrorsAndWarnings('from a_index | sort bucket(dateField, textField)', [
+          'SORT does not support function bucket',
+        ]);
+
+        testErrorsAndWarnings('from a_index | stats bucket("2022", textField)', [
+          'Argument of [bucket] must be a constant, received [textField]',
+        ]);
+        testErrorsAndWarnings('from a_index | stats bucket(concat("20", "22"), textField)', [
+          'Argument of [bucket] must be [date], found value [concat("20","22")] type [keyword]',
+          'Argument of [bucket] must be a constant, received [textField]',
+        ]);
       });
 
       describe('percentile', () => {