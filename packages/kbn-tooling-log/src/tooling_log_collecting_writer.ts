8ef15da4ac3499b6d5ac408c79655e3bf441bd38
Alex Szabo <alex.szabo@elastic.co>
2024-08-15 11:54:23 +0200

    add the option to set context for a script's logger

--- packages/kbn-tooling-log/src/tooling_log_collecting_writer.ts
+++ packages/kbn-tooling-log/src/tooling_log_collecting_writer.ts
@@ -26,15 +26,17 @@ export class ToolingLogCollectingWriter extends ToolingLogTextWriter {
   }
 
   /**
-   * Called by ToolingLog, extends messages with the source if message includes one.
+   * Called by ToolingLog, extends messages with the source and context if message include it.
    */
   write(msg: Message) {
-    if (msg.source) {
-      return super.write({
-        ...msg,
-        args: [`source[${msg.source}]`, ...msg.args],
-      });
-    }
+    const args = [
+      msg.source ? `source[${msg.source}]` : null,
+      msg.context ? `context[${msg.context}]` : null,
+    ]
+      .filter(Boolean)
+      .concat(msg.args);
+
+    super.write({ ...msg, args });
 
     return super.write(msg);
   }
