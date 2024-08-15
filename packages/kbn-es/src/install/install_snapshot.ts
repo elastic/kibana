da1db2cdebfa28d0b9743e7e7038042aa7cfe09a
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-08-15 15:52:02 +0200

    [FTR] [deployment agnostic tests] move saml roles setup to kbn/es (#190489)
    ## Summary
    
    closes #190212
    
    This PR moves realm roles and mapping creation from FTR service to
    `kbn/es` package in oder to configure ES properly when you start servers
    with ` node scripts/functional_tests_server.js --config
    <path/to/stateful_config_with_SAML_setup>`
    
    It allows to start servers and investigate test scenario manually (e.g.
    reproduce failure)
    
    Thanks a lot to @azasypkin for ideas how to simplify and minimize the
    code change.
    
    Instead of 2 API calls to ES in FTR service we copy roles.yml to ES
    config directory before its start and create role mapping via API (but
    using template) as a part of security setup
    
    How to test: 
    ```
    node scripts/functional_tests_server.js --config x-pack/test/api_integration/deployment_agnostic/stateful.config.ts
    curl -X GET "http://localhost:9220/_security/role_mapping" -H "Content-Type: application/json" -u elastic:changeme
    ```
    
    you should get the valid mapping back
    
    Note: if you try to load Kibana manually, it won't work due to missing
    mock-idp-plugin in stateful #190221 But automated tests are functional
    and can be run via script.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Aleh Zasypkin <aleh.zasypkin@gmail.com>

--- packages/kbn-es/src/install/install_snapshot.ts
+++ packages/kbn-es/src/install/install_snapshot.ts
@@ -51,6 +51,7 @@ export async function installSnapshot({
   log = defaultLog,
   esArgs,
   useCached = false,
+  resources,
 }: InstallSnapshotOptions) {
   const { downloadPath } = await downloadSnapshot({
     license,
@@ -68,5 +69,6 @@ export async function installSnapshot({
     installPath,
     log,
     esArgs,
+    resources,
   });
 }
