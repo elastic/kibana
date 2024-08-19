93ae24ca551d313ec0c9961959f321f7c2ffe197
Elena Shostak <elena.shostak@elastic.co>
2024-08-19 12:05:43 +0200

    Fix

--- x-pack/plugins/fleet/server/services/epm/fields/field.ts
+++ x-pack/plugins/fleet/server/services/epm/fields/field.ts
@@ -322,8 +322,8 @@ export const loadDatastreamsFieldsFromYaml = (
   return fieldDefinitionFiles.reduce<Field[]>((acc, file) => {
     // Make sure it is defined as it is optional. Should never happen.
     if (file.buffer) {
-      const tmpFields = safeLoad(file.buffer.toString());
-      // safeLoad() returns undefined for empty files, we don't want that
+      const tmpFields = load(file.buffer.toString());
+      // load() returns undefined for empty files, we don't want that
       if (tmpFields) {
         acc = acc.concat(tmpFields);
       }
@@ -345,8 +345,8 @@ export const loadTransformFieldsFromYaml = (
   return fieldDefinitionFiles.reduce<Field[]>((acc, file) => {
     // Make sure it is defined as it is optional. Should never happen.
     if (file.buffer) {
-      const tmpFields = safeLoad(file.buffer.toString());
-      // safeLoad() returns undefined for empty files, we don't want that
+      const tmpFields = load(file.buffer.toString());
+      // load() returns undefined for empty files, we don't want that
       if (tmpFields) {
         acc = acc.concat(tmpFields);
       }
