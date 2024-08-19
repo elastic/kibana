b3f37bc838ba2656db3172e9bed803c3f1509690
saarikabhasi <nairsaarika.bhasi@elastic.co>
2024-08-19 11:22:29 -0400

    remove eslint file

--- x-pack/plugins/search_assistant/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-require('@kbn/babel-register').install();
-
-module.exports = {
-  root: true,
-  extends: ['@kbn/eslint-config', 'plugin:@elastic/eui/recommended'],
-  rules: {
-    '@kbn/eslint/require-license-header': 'off',
-  },
-};
