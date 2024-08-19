fa0c2722c0731af7438cbd6704ff804014b42c04
Alex Szabo <alex.szabo@elastic.co>
2024-08-19 10:33:28 +0200

    1st batch, 1st half

--- x-pack/test/functional/apps/ml/anomaly_detection_integrations/index.ts
+++ x-pack/test/functional/apps/ml/anomaly_detection_integrations/index.ts
@@ -34,8 +34,8 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./anomaly_charts_dashboard_embeddables'));
     loadTestFile(require.resolve('./single_metric_viewer_dashboard_embeddables'));
     loadTestFile(require.resolve('./anomaly_embeddables_migration'));
-    loadTestFile(require.resolve('./lens_to_ml'));
-    loadTestFile(require.resolve('./map_to_ml'));
-    loadTestFile(require.resolve('./lens_to_ml_with_wizard'));
+    // loadTestFile(require.resolve('./lens_to_ml'));
+    // loadTestFile(require.resolve('./map_to_ml'));
+    // loadTestFile(require.resolve('./lens_to_ml_with_wizard'));
   });
 }
