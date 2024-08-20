77b67fa33548c99baaed3de37016e7d238d28dc7
Gloria Hornero <gloria.hornero@elastic.co>
2024-08-20 16:00:49 +0200

    adds page objects

---
+++ x-pack/test/security_solution_playwright/page_objects/entity_analytics_management_po.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { Locator, Page, expect } from '@playwright/test';
+
+const PAGE_TITLE = '[data-test-subj="entityAnalyticsManagementPageTitle"]';
+
+export class EntityAnalyticsManagementPage {
+  page: Page;
+  entityAnalyticsManagementPageTitle!: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async initialize() {
+    this.entityAnalyticsManagementPageTitle = this.page.locator(PAGE_TITLE);
+  }
+
+  async entityAnalyticsManagementPageTitleShouldBeDisplayed() {
+    await expect(this.entityAnalyticsManagementPageTitle).toHaveText('Entity Risk Score', {
+      timeout: 60000,
+    });
+  }
+}
--- x-pack/test/security_solution_playwright/tests/enable_risk_score_redirect.spec.ts
+++ x-pack/test/security_solution_playwright/tests/enable_risk_score_redirect.spec.ts
@@ -5,43 +5,37 @@
  * 2.0.
  */
 
-import { expect } from '@playwright/test';
 import { test } from '../fixtures/es_archiver';
+import { PageFactory } from '../page_objects/page_factory';
+import { EntityAnalyticsPage } from '../page_objects/entity_analytics_po';
+import { EntityAnalyticsManagementPage } from '../page_objects/entity_analytics_management_po';
+
+let entityAnalyticsPage: EntityAnalyticsPage;
+let entityAnalyticsManagementPage: EntityAnalyticsManagementPage;
+
+test.beforeAll(async ({ esArchiver }) => {
+  await esArchiver.loadIfNeeded('auditbeat_single');
+});
+test.afterAll(async ({ esArchiver }) => {
+  await esArchiver.unload('auditbeat_single');
+});
 
 test.describe('Enable risk scores from dashboard', () => {
+  test.beforeEach(async ({ page }) => {
+    entityAnalyticsPage = await PageFactory.createEntityAnalyticsPage(page);
+    entityAnalyticsPage.navigates();
+  });
   test.use({ storageState: '.auth/user.json' });
-  test('host risk enable button should redirect to entity management page', async ({
-    page,
-    esArchiver,
-  }) => {
-    await esArchiver.load('auditbeat_single');
 
-    await page.goto('/app/security/entity_analytics');
-
-    await expect(page.locator('[data-test-subj="enable_host_risk_score"]')).toBeVisible({
-      timeout: 60000,
-    });
-
-    await page.locator('[data-test-subj="enable_host_risk_score"]').click();
-
-    await expect(page.locator('[data-test-subj="entityAnalyticsManagementPageTitle"]')).toHaveText(
-      'Entity Risk Score',
-      { timeout: 60000 }
-    );
+  test('host risk enable button should redirect to entity management page', async ({ page }) => {
+    await entityAnalyticsPage.waitForEnableHostRiskScoreToBePresent();
+    entityAnalyticsManagementPage = await entityAnalyticsPage.enableHostRisk();
+    entityAnalyticsManagementPage.entityAnalyticsManagementPageTitleShouldBeDisplayed();
   });
 
   test('user risk enable button should redirect to entity management page', async ({ page }) => {
-    await page.goto('/app/security/entity_analytics');
-
-    await expect(page.locator('[data-test-subj="enable_user_risk_score"]')).toBeVisible({
-      timeout: 60000,
-    });
-
-    await page.locator('[data-test-subj="enable_user_risk_score"]').click();
-
-    await expect(page.locator('[data-test-subj="entityAnalyticsManagementPageTitle"]')).toHaveText(
-      'Entity Risk Score',
-      { timeout: 60000 }
-    );
+    await entityAnalyticsPage.waitForEnableUserRiskScoreToBePresent();
+    entityAnalyticsManagementPage = await entityAnalyticsPage.enableUserRisk();
+    entityAnalyticsManagementPage.entityAnalyticsManagementPageTitleShouldBeDisplayed();
   });
 });
