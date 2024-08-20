9b8bdb005dcbb27fa6ed285ec6f2a7ad5dd19743
Elena Stoeva <elenastoeva99@gmail.com>
2024-08-20 11:07:17 +0100

    Use EuiFlexGroup

--- src/plugins/console/public/application/components/output_panel_empty_state.tsx
+++ src/plugins/console/public/application/components/output_panel_empty_state.tsx
@@ -7,7 +7,15 @@
  */
 
 import React, { FunctionComponent } from 'react';
-import { EuiHorizontalRule, EuiSpacer, EuiTitle, EuiLink, EuiText } from '@elastic/eui';
+import {
+  EuiHorizontalRule,
+  EuiSpacer,
+  EuiTitle,
+  EuiLink,
+  EuiText,
+  EuiFlexGroup,
+  EuiFlexItem,
+} from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { useServicesContext } from '../contexts';
 
@@ -15,44 +23,50 @@ export const OutputPanelEmptyState: FunctionComponent = () => {
   const { docLinks } = useServicesContext();
 
   return (
-    <div className="conApp__outputPanel__emptyState" data-test-subj="consoleOutputPanelEmptyState">
-      <EuiTitle size="s">
-        <h3>
-          <FormattedMessage
-            id="console.outputEmptyState.title"
-            defaultMessage="Enter a new request"
-          />
-        </h3>
-      </EuiTitle>
-      <EuiSpacer size="m" />
-      <EuiText size="relative">
-        <p>
-          <FormattedMessage
-            id="console.outputEmptyState.description"
-            defaultMessage="When you run a request in the input panel, you will see the output response here."
-          />
-        </p>
-      </EuiText>
-
-      <EuiHorizontalRule />
-
-      <EuiText size="xs">
-        <p>
-          <strong>
+    <EuiFlexGroup
+      alignItems="center"
+      className="conApp__outputPanel__emptyState"
+      data-test-subj="consoleOutputPanelEmptyState"
+    >
+      <EuiFlexItem>
+        <EuiTitle size="s">
+          <h3>
             <FormattedMessage
-              id="console.outputEmptyState.learnMore"
-              defaultMessage="Want to learn more?"
+              id="console.outputEmptyState.title"
+              defaultMessage="Enter a new request"
             />
-          </strong>
-          &nbsp;
-          <EuiLink href={docLinks.console.guide} target="_blank">
+          </h3>
+        </EuiTitle>
+        <EuiSpacer size="m" />
+        <EuiText size="relative">
+          <p>
             <FormattedMessage
-              id="console.outputEmptyState.docsLink"
-              defaultMessage="Read Console documentation"
+              id="console.outputEmptyState.description"
+              defaultMessage="When you run a request in the input panel, you will see the output response here."
             />
-          </EuiLink>
-        </p>
-      </EuiText>
-    </div>
+          </p>
+        </EuiText>
+
+        <EuiHorizontalRule />
+
+        <EuiText size="xs">
+          <p>
+            <strong>
+              <FormattedMessage
+                id="console.outputEmptyState.learnMore"
+                defaultMessage="Want to learn more?"
+              />
+            </strong>
+            &nbsp;
+            <EuiLink href={docLinks.console.guide} target="_blank">
+              <FormattedMessage
+                id="console.outputEmptyState.docsLink"
+                defaultMessage="Read Console documentation"
+              />
+            </EuiLink>
+          </p>
+        </EuiText>
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 };
