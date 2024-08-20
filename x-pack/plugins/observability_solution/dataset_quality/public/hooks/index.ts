3c141b82a00598a1dcc337bedef8d3ab20dd3ae0
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2024-08-20 15:40:41 +0200

    Fix deleted file reference

--- x-pack/plugins/observability_solution/dataset_quality/public/hooks/index.ts
+++ x-pack/plugins/observability_solution/dataset_quality/public/hooks/index.ts
@@ -6,7 +6,6 @@
  */
 
 export * from './use_dataset_quality_table';
-export * from './use_dataset_quality_flyout';
 export * from './use_degraded_docs_chart';
 export * from './use_redirect_link';
 export * from './use_summary_panel';
