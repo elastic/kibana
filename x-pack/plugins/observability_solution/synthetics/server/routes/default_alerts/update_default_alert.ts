858fc194fe9e2cc32bc4625207f3c566a143d778
Dominique Belcher <dominique.clarke@elastic.co>
2024-08-20 21:47:32 -0400

    add alert deletion logic

--- x-pack/plugins/observability_solution/synthetics/server/routes/default_alerts/update_default_alert.ts
+++ x-pack/plugins/observability_solution/synthetics/server/routes/default_alerts/update_default_alert.ts
@@ -8,21 +8,59 @@
 import { DefaultAlertService } from './default_alert_service';
 import { SyntheticsRestApiRouteFactory } from '../types';
 import { SYNTHETICS_API_URLS } from '../../../common/constants';
+import {
+  SYNTHETICS_STATUS_RULE,
+  SYNTHETICS_TLS_RULE,
+} from '../../../common/constants/synthetics_alerts';
+import { savedObjectsAdapter } from '../../saved_objects';
 
 export const updateDefaultAlertingRoute: SyntheticsRestApiRouteFactory = () => ({
   method: 'PUT',
   path: SYNTHETICS_API_URLS.ENABLE_DEFAULT_ALERTING,
   validate: {},
-  handler: async ({ context, server, savedObjectsClient }): Promise<any> => {
+  handler: async ({ request, context, server, savedObjectsClient }): Promise<any> => {
     const defaultAlertService = new DefaultAlertService(context, server, savedObjectsClient);
+    const { defaultTLSRuleEnabled, defaultStatusRuleEnabled } =
+      (await savedObjectsAdapter.getSyntheticsDynamicSettings(savedObjectsClient)) || {};
 
-    const [statusRule, tlsRule] = await Promise.all([
-      defaultAlertService.updateStatusRule(),
-      defaultAlertService.updateTlsRule(),
-    ]);
-    return {
-      statusRule,
-      tlsRule,
-    };
+    const deleteStatusRulePromise =
+      defaultStatusRuleEnabled === false
+        ? server.pluginsStart.alerting.getRulesClientWithRequest(request).bulkDeleteRules({
+            filter: `alert.attributes.alertTypeId:"${SYNTHETICS_STATUS_RULE}" AND alert.attributes.tags:"SYNTHETICS_DEFAULT_ALERT"`,
+          })
+        : new Promise<void>((resolve) => resolve());
+    const deleteTLSRulePromise =
+      defaultTLSRuleEnabled === false
+        ? server.pluginsStart.alerting.getRulesClientWithRequest(request).bulkDeleteRules({
+            filter: `alert.attributes.alertTypeId:"${SYNTHETICS_TLS_RULE}" AND alert.attributes.tags:"SYNTHETICS_DEFAULT_ALERT"`,
+          })
+        : new Promise<void>((resolve) => resolve());
+    const createStatusRulePromise =
+      defaultStatusRuleEnabled !== false
+        ? defaultAlertService.updateStatusRule()
+        : new Promise<void>((resolve) => resolve());
+    const createTLSRulePromise =
+      defaultTLSRuleEnabled !== false
+        ? defaultAlertService.updateTlsRule()
+        : new Promise<void>((resolve) => resolve());
+
+    try {
+      const [{ value: statusRule }, { value: tlsRule }] = await Promise.allSettled([
+        createStatusRulePromise,
+        createTLSRulePromise,
+        deleteStatusRulePromise,
+        deleteTLSRulePromise,
+      ]);
+      return {
+        statusRule: statusRule || null,
+        tlsRule: tlsRule || null,
+      };
+    } catch (e) {
+      server.logger.error(`Error updating default alerting rules: ${e}`);
+      return {
+        statusRule: null,
+        tlsRule: null,
+      };
+    }
   },
 });
