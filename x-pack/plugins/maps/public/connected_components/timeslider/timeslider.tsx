485eadd789ddbcf8e49826a9bd458898edc39029
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-08-21 15:25:09 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/maps/public/connected_components/timeslider/timeslider.tsx
+++ x-pack/plugins/maps/public/connected_components/timeslider/timeslider.tsx
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-import _ from 'lodash';
 import React, { Component } from 'react';
 import { Observable, Subscription } from 'rxjs';
 import { ViewMode } from '@kbn/embeddable-plugin/public';
