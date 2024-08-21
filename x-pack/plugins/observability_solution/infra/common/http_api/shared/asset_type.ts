b2ff808af371d49f1b6c5f07b23834e32af27a7f
Steph Milovic <stephanie.milovic@elastic.co>
2024-08-21 09:10:09 -0600

    revert

--- x-pack/plugins/observability_solution/infra/common/http_api/shared/asset_type.ts
+++ x-pack/plugins/observability_solution/infra/common/http_api/shared/asset_type.ts
@@ -5,18 +5,8 @@
  * 2.0.
  */
 
-<<<<<<<< HEAD:x-pack/packages/kbn-elastic-assistant/impl/assistant/quick_prompts/quick_prompt_settings/helpers.tsx
-import { euiPaletteColorBlind } from '@elastic/eui';
-
-const euiVisPalette = euiPaletteColorBlind();
-export const getRandomEuiColor = () => {
-  const randomIndex = Math.floor(Math.random() * euiVisPalette.length);
-  return euiVisPalette[randomIndex];
-};
-========
 import * as rt from 'io-ts';
 
 export const AssetTypeRT = rt.type({
   assetType: rt.literal('host'),
 });
->>>>>>>> main:x-pack/plugins/observability_solution/infra/common/http_api/shared/asset_type.ts
