913cfcf3bb0b71f010dfed37735ba62a5763c3ed
Gloria Hornero <gloria.hornero@elastic.co>
2024-08-21 14:56:25 +0200

    buildkite execution

---
+++ .buildkite/scripts/steps/functional/security_solution_playwright.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/steps/functional/common.sh
+
+export JOB=kibana-security-solution-playwright
+export KIBANA_INSTALL_DIR=${KIBANA_BUILD_LOCATION}
+
+echo "---ESS - Security Solution Playwright Tests"
+
+cd x-pack/test/security_solution_playwright
+
+set +e
+
+yarn run:ess; 
