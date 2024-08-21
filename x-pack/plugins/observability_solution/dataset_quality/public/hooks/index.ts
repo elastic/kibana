5744436fff68b2dcde03fcc28e3dc3f97ad13224
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2024-08-21 14:27:51 +0200

    Fix deleted files import

--- x-pack/plugins/observability_solution/dataset_quality/public/hooks/index.ts
+++ x-pack/plugins/observability_solution/dataset_quality/public/hooks/index.ts
@@ -10,7 +10,6 @@ export * from './use_degraded_docs_chart';
 export * from './use_redirect_link';
 export * from './use_summary_panel';
 export * from './use_create_dataview';
-export * from './use_dataset_quality_degraded_field';
 export * from './use_redirect_link_telemetry';
 export * from './use_dataset_quality_details_state';
 export * from './use_degraded_fields';
