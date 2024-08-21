5d132a67ee4e40ab79ba50eb70ddd04994a24124
Larry Gregory <larry.gregory@elastic.co>
2024-08-21 15:34:48 -0400

    switch to snapshot test

--- test/plugin_functional/test_suites/hardening/prototype.ts
+++ test/plugin_functional/test_suites/hardening/prototype.ts
@@ -6,43 +6,20 @@
  * Side Public License, v 1.
  */
 
-import expect from '@kbn/expect';
 import type { PluginFunctionalProviderContext } from '../../services';
 
 export default function ({ getService }: PluginFunctionalProviderContext) {
   const supertest = getService('supertest');
+  const snapshots = getService('snapshots');
 
   describe('prototype', function () {
     it('does not allow polluting most prototypes', async () => {
-      await supertest
+      const response = await supertest
         .get('/api/hardening/_pollute_prototypes')
         .set('kbn-xsrf', 'true')
-        .expect(200)
-        .then((response) => {
-          expect(response.body).to.eql({
-            object: {
-              prototype: {},
-              error: 'Cannot add property polluted, object is not extensible',
-            },
-            number: {
-              prototype: {},
-              error: 'Cannot add property polluted, object is not extensible',
-            },
-            string: {
-              prototype: {},
-              error: 'Cannot add property polluted, object is not extensible',
-            },
-            fn: {
-              prototype: {},
-              error: 'Cannot add property polluted, object is not extensible',
-            },
-            array: {
-              prototype: {
-                '0': 'polluted',
-              },
-            },
-          });
-        });
+        .expect(200);
+
+      await snapshots.compareAgainstBaseline('hardening/prototype', response.body);
     });
   });
 }
