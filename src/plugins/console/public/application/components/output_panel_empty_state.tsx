9b37970c7c02f58821de6856a2f1ac10fc7fa2fe
Elena Stoeva <elenastoeva99@gmail.com>
2024-08-21 09:50:56 +0100

    Add empty prompts

--- src/plugins/console/public/application/components/output_panel_empty_state.tsx
+++ src/plugins/console/public/application/components/output_panel_empty_state.tsx
@@ -7,15 +7,7 @@
  */
 
 import React, { FunctionComponent } from 'react';
-import {
-  EuiHorizontalRule,
-  EuiSpacer,
-  EuiTitle,
-  EuiLink,
-  EuiText,
-  EuiFlexGroup,
-  EuiFlexItem,
-} from '@elastic/eui';
+import { EuiEmptyPrompt, EuiTitle, EuiLink } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { useServicesContext } from '../contexts';
 
@@ -23,50 +15,42 @@ export const OutputPanelEmptyState: FunctionComponent = () => {
   const { docLinks } = useServicesContext();
 
   return (
-    <EuiFlexGroup
-      alignItems="center"
-      className="conApp__outputPanel__emptyState"
-      data-test-subj="consoleOutputPanelEmptyState"
-    >
-      <EuiFlexItem>
-        <EuiTitle size="s">
-          <h3>
-            <FormattedMessage
-              id="console.outputEmptyState.title"
-              defaultMessage="Enter a new request"
-            />
-          </h3>
-        </EuiTitle>
-        <EuiSpacer size="m" />
-        <EuiText size="relative">
-          <p>
-            <FormattedMessage
-              id="console.outputEmptyState.description"
-              defaultMessage="When you run a request in the input panel, you will see the output response here."
-            />
-          </p>
-        </EuiText>
-
-        <EuiHorizontalRule />
-
-        <EuiText size="xs">
-          <p>
-            <strong>
+    <EuiEmptyPrompt
+      title={
+        <h2>
+          <FormattedMessage
+            id="console.outputEmptyState.title"
+            defaultMessage="Enter a new request"
+          />
+        </h2>
+      }
+      body={
+        <p>
+          <FormattedMessage
+            id="console.outputEmptyState.description"
+            defaultMessage="When you run a request in the input panel, you will see the output response here."
+          />
+        </p>
+      }
+      footer={
+        <>
+          <EuiTitle size="xxs">
+            <h3>
               <FormattedMessage
                 id="console.outputEmptyState.learnMore"
                 defaultMessage="Want to learn more?"
               />
-            </strong>
-            &nbsp;
-            <EuiLink href={docLinks.console.guide} target="_blank">
-              <FormattedMessage
-                id="console.outputEmptyState.docsLink"
-                defaultMessage="Read Console documentation"
-              />
-            </EuiLink>
-          </p>
-        </EuiText>
-      </EuiFlexItem>
-    </EuiFlexGroup>
+            </h3>
+          </EuiTitle>
+          <EuiLink href={docLinks.console.guide} target="_blank">
+            <FormattedMessage
+              id="console.outputEmptyState.docsLink"
+              defaultMessage="Read the Console docs"
+            />
+          </EuiLink>
+        </>
+      }
+      data-test-subj="consoleOutputPanelEmptyState"
+    />
   );
 };
