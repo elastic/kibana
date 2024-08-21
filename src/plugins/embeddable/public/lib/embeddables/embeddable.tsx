7fc2c38e61b9ea62c4df19770ff1764cfaea0c0c
Hannah Mudge <hannah.wright@elastic.co>
2024-08-21 11:07:22 -0600

    Fix type error

--- src/plugins/embeddable/public/lib/embeddables/embeddable.tsx
+++ src/plugins/embeddable/public/lib/embeddables/embeddable.tsx
@@ -137,6 +137,7 @@ export abstract class Embeddable<
       defaultPanelDescription: this.defaultPanelDescription,
       canLinkToLibrary: this.canLinkToLibrary,
       disabledActionIds: this.disabledActionIds,
+      setDisabledActionIds: this.setDisabledActionIds,
       unlinkFromLibrary: this.unlinkFromLibrary,
       setHidePanelTitle: this.setHidePanelTitle,
       defaultPanelTitle: this.defaultPanelTitle,
@@ -179,6 +180,7 @@ export abstract class Embeddable<
   public panelDescription: LegacyEmbeddableAPI['panelDescription'];
   public defaultPanelDescription: LegacyEmbeddableAPI['defaultPanelDescription'];
   public disabledActionIds: LegacyEmbeddableAPI['disabledActionIds'];
+  public setDisabledActionIds: LegacyEmbeddableAPI['setDisabledActionIds'];
   public unlinkFromLibrary: LegacyEmbeddableAPI['unlinkFromLibrary'];
   public setTimeRange: LegacyEmbeddableAPI['setTimeRange'];
   public defaultPanelTitle: LegacyEmbeddableAPI['defaultPanelTitle'];
