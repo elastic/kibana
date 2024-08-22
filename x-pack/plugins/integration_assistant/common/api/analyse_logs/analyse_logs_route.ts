e15a9bb10c90d6af21f9aee3c221b2edcca50dfe
Bharat Pasupula <saibharatchandra.pasupula@elastic.co>
2024-08-22 18:25:45 +0200

    fix eslint errors

--- x-pack/plugins/integration_assistant/common/api/analyse_logs/analyse_logs_route.ts
+++ x-pack/plugins/integration_assistant/common/api/analyse_logs/analyse_logs_route.ts
@@ -14,7 +14,7 @@
  *   version: 1
  */
 
-import { z } from 'zod';
+import { z } from '@kbn/zod';
 
 import { EncodedRawSamples, Connector, LangSmithOptions } from '../model/common_attributes';
 import { AnalyseLogsAPIResponse } from '../model/response_schemas';
