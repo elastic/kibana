d63b7570ea2c5631b61d5b021c890ddc8a294ea8
Marius Iversen <marius.iversen@elastic.co>
2024-08-22 17:03:53 +0200

    add yarn scripts

--- x-pack/plugins/integration_assistant/scripts/draw_graphs_script.ts
+++ x-pack/plugins/integration_assistant/scripts/draw_graphs_script.ts
@@ -16,21 +16,19 @@ import { FakeLLM } from '@langchain/core/utils/testing';
 import type { IScopedClusterClient } from '@kbn/core-elasticsearch-server';
 import { getEcsGraph, getEcsSubGraph } from '../server/graphs/ecs/graph';
 import { getCategorizationGraph } from '../server/graphs/categorization/graph';
-import { getRelatedGraph } from '../server/graphs/related/graph'
+import { getRelatedGraph } from '../server/graphs/related/graph';
 
 // Some mock elements just to get the graph to compile
 const mockLlm = new FakeLLM({
   response: JSON.stringify({}, null, 2),
 }) as unknown as ActionsClientChatOpenAI | ActionsClientSimpleChatModel;
-const client = "test" as unknown as IScopedClusterClient;
-
+const client = 'test' as unknown as IScopedClusterClient;
 
 const logger = new ToolingLog({
   level: 'info',
   writeTo: process.stdout,
 });
 logger.info('Compiling graphs');
-
 
 async function saveFile(filename: string, buffer: Buffer) {
   const outputPath = path.join(__dirname, '../docs/img/', filename);
@@ -42,28 +40,28 @@ async function drawEcsGraph() {
   const ecsGraph = (await getEcsGraph(mockLlm)).getGraph();
   const output = await ecsGraph.drawMermaidPng();
   const buffer = Buffer.from(await output.arrayBuffer());
-  await saveFile('ecs_graph.png', buffer)
+  await saveFile('ecs_graph.png', buffer);
 }
 
 async function drawEcsSubGraph() {
   const ecsGraph = (await getEcsSubGraph(mockLlm)).getGraph();
   const output = await ecsGraph.drawMermaidPng();
   const buffer = Buffer.from(await output.arrayBuffer());
-  await saveFile('ecs_subgraph.png', buffer)
+  await saveFile('ecs_subgraph.png', buffer);
 }
 
 async function drawCategorizationGraph() {
   const categorizationGraph = (await getCategorizationGraph(client, mockLlm)).getGraph();
   const output = await categorizationGraph.drawMermaidPng();
   const buffer = Buffer.from(await output.arrayBuffer());
-  await saveFile('categorization_graph.png', buffer)
+  await saveFile('categorization_graph.png', buffer);
 }
 
 async function drawRelatedGraph() {
   const relatedGraph = (await getRelatedGraph(client, mockLlm)).getGraph();
   const output = await relatedGraph.drawMermaidPng();
   const buffer = Buffer.from(await output.arrayBuffer());
-  await saveFile('related_graph.png', buffer)
+  await saveFile('related_graph.png', buffer);
 }
 
 export async function drawGraphs() {
