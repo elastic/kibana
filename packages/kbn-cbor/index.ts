3d78488176d87cd80726a0c0e8cfec9be3ba3025
Tiago Costa <tiago.costa@elastic.co>
2024-08-22 23:21:59 +0100

    docs(NA): small comment on class

--- packages/kbn-cbor/index.ts
+++ packages/kbn-cbor/index.ts
@@ -6,6 +6,9 @@
  * Side Public License, v 1.
  */
 
+// NOTE: This can possibly be replaced with node-cbor using encode, and decodeFirstSync if we do need
+// to change into something better maintained but for now we are going to stick with borc as it is
+// a little faster
 import { encode as encodeJS, decode as decodeJS } from 'borc';
 
 export class KbnCbor {
