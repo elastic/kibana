e0a1b7ef77dc723c1648116bff3c4ae4c44c773c
Alexey Antonov <alexwizp@gmail.com>
2024-08-22 17:20:55 +0300

    update translations

--- x-pack/plugins/observability_solution/apm/public/components/app/onboarding/introduction.tsx
+++ x-pack/plugins/observability_solution/apm/public/components/app/onboarding/introduction.tsx
@@ -8,6 +8,7 @@
 import { EuiBetaBadge, EuiImage, EuiPageHeader, EuiLink, EuiText } from '@elastic/eui';
 import React from 'react';
 import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n-react';
 import { useKibanaUrl } from '../../../hooks/use_kibana_url';
 
 interface IntroductionProps {
@@ -57,14 +58,14 @@ export function Introduction({ isBeta, guideLink }: IntroductionProps) {
         }
         description={
           <EuiText>
-            {i18n.translate('xpack.apm.onboarding.specProvider.longDescription', {
-              defaultMessage:
-                'Application Performance Monitoring (APM) collects in-depth \
-                 performance metrics and errors from inside your application. \
-                 It allows you to monitor the performance of thousands of applications in real time. \
-                 {link}.',
-              values: {
-                link: (
+            <FormattedMessage
+              id="xpack.apm.onboarding.specProvider.longDescription"
+              defaultMessage="Application Performance Monitoring (APM) collects in-depth \
+            performance metrics and errors from inside your application. \
+            It allows you to monitor the performance of thousands of applications in real time. \
+            {learnMoreLink}."
+              values={{
+                learnMoreLink: (
                   <EuiLink
                     data-test-subj="apmIntroductionLearnMoreLink"
                     href={guideLink}
@@ -81,8 +82,8 @@ export function Introduction({ isBeta, guideLink }: IntroductionProps) {
                     })}
                   </EuiLink>
                 ),
-              },
-            })}
+              }}
+            />
           </EuiText>
         }
         rightSideItems={rightSideItems}
