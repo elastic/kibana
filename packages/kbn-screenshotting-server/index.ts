45967f51ad4f35bd4d2690a87b90329d83ca7f17
Tim Sullivan <tsullivan@users.noreply.github.com>
2024-08-22 11:03:03 -0700

    [Screenshotting] Server package for stateless code (#188390)
    ## Summary
    
    This PR moves code that is usually impacted by a Puppeteer/Chromium
    upgrade to a package. The same work is happening to a lesser degree in
    the 7.17 branch. Together, these changes will ease the manual labor of
    backporting the upgrades from main to 7.17.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit bf673d9c565c288223483c4c622f37f745ce70e6)
    
    # Conflicts:
    #	.github/CODEOWNERS

---
+++ packages/kbn-screenshotting-server/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+export { args } from './src/args';
+export { ChromiumArchivePaths, type PackageInfo } from './src/paths';
+export { getChromiumPackage } from './src/get_chromium_package';
+export { type ConfigType, createConfig, config, durationToNumber } from './src/config';
+export { ConfigSchema } from './src/config/schema';
