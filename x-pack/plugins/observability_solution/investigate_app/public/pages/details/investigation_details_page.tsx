39f5cd47ea7c7052792aa740c30c0dfada62b7b3
Kevin Delemme <kevin.delemme@elastic.co>
2024-08-22 09:28:26 -0400

    fix eslint

--- x-pack/plugins/observability_solution/investigate_app/public/pages/details/investigation_details_page.tsx
+++ x-pack/plugins/observability_solution/investigate_app/public/pages/details/investigation_details_page.tsx
@@ -11,7 +11,7 @@ import { alertOriginSchema } from '@kbn/investigation-shared';
 import { ALERT_RULE_CATEGORY } from '@kbn/rule-data-utils/src/default_alerts_as_data';
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { useAsync } from 'react-use';
+import useAsync from 'react-use/lib/useAsync';
 import { paths } from '../../../common/paths';
 import { useFetchAlert } from '../../hooks/use_get_alert_details';
 import { useFetchInvestigation } from '../../hooks/use_get_investigation_details';
