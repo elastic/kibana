3995e8358373eaf2d0cd8b71d868d4cd0a1d5098
Tre' Seymour <wayne.seymour@elastic.co>
2024-08-23 12:56:02 +0100

    try to drop cypress tests for now

--- .buildkite/scripts/steps/functional/defend_workflows.sh
+++ .buildkite/scripts/steps/functional/defend_workflows.sh
@@ -7,12 +7,10 @@ source .buildkite/scripts/steps/functional/common.sh
 export JOB=kibana-defend-workflows-cypress
 export KIBANA_INSTALL_DIR=${KIBANA_BUILD_LOCATION}
 
-echo "--- Defend Workflows Cypress tests"
+# echo "--- Defend Workflows Cypress tests"
+echo "--- Defend Workflows Cypress tests DROPPED"
 
-cd x-pack/plugins/security_solution
-
-set +e
-BK_ANALYTICS_API_KEY=$(vault_get security-solution-ci defend-workflows-bk-api-key)
-
-# BK_ANALYTICS_API_KEY=$BK_ANALYTICS_API_KEY yarn cypress:dw:run; status=$?; yarn junit:merge || :; exit $status
-BK_ANALYTICS_API_KEY=$BK_ANALYTICS_API_KEY yarn cypress:dw:run; status=$?;  exit $status
+# cd x-pack/plugins/security_solution
+#
+# set +e
+# BK_ANALYTICS_API_KEY=$(vault_get security-solution-ci defend-workflows-bk-api-key)
