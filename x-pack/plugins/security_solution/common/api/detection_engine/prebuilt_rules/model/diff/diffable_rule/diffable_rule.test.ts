6d8e13814cc0246743ab5034c55e85bd6546896a
jpdjere <jpdjeredjian@gmail.com>
2024-08-26 16:01:28 +0200

    Unit tests

--- x-pack/plugins/security_solution/common/api/detection_engine/prebuilt_rules/model/diff/diffable_rule/diffable_rule.test.ts
+++ x-pack/plugins/security_solution/common/api/detection_engine/prebuilt_rules/model/diff/diffable_rule/diffable_rule.test.ts
@@ -4,7 +4,6 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
 import {
   RuleFieldsToUpgrade,
   DiffableUpgradableFields,
@@ -12,6 +11,7 @@ import {
   DiffableAllFields,
   DiffableRuleTypes,
 } from './diffable_rule';
+import { expectParseError, expectParseSuccess, stringifyZodError } from '@kbn/zod-helpers';
 
 describe('Diffable rule schema', () => {
   describe('DiffableAllFields', () => {
@@ -37,8 +37,124 @@ describe('Diffable rule schema', () => {
       );
     });
 
-    it('correctly validates valid and invalid inputs', () => {
-      
-    })
+    describe('correctly validates valid and invalid inputs', () => {
+      it('validates 5 valid cases: BASE, CURRENT, TARGET, MERGED, RESOLVED', () => {
+        const validInputs = [
+          {
+            name: {
+              pick_version: 'BASE',
+            },
+          },
+          {
+            description: {
+              pick_version: 'CURRENT',
+            },
+          },
+          {
+            risk_score: {
+              pick_version: 'TARGET',
+            },
+          },
+          {
+            note: {
+              pick_version: 'MERGED',
+            },
+          },
+          {
+            references: {
+              pick_version: 'RESOLVED',
+              resolved_value: ['www.link1.com', 'www.link2.com'],
+            },
+          },
+        ];
+
+        validInputs.forEach((input) => {
+          const result = RuleFieldsToUpgrade.safeParse(input);
+          expectParseSuccess(result);
+          expect(result.data).toEqual(input);
+        });
+      });
+
+      it('does not validate invalid combination of pick_version and resolved_value', () => {
+        const input = {
+          references: {
+            pick_version: 'MERGED',
+            resolved_value: ['www.link1.com', 'www.link2.com'],
+          },
+        };
+        const result = RuleFieldsToUpgrade.safeParse(input);
+        expectParseError(result);
+        expect(stringifyZodError(result.error)).toMatchInlineSnapshot(
+          `"references: Unrecognized key(s) in object: 'resolved_value'"`
+        );
+      });
+
+      it('invalidates incorrect types of resolved_values provided to multiple fields', () => {
+        const input = {
+          references: {
+            pick_version: 'RESOLVED',
+            resolved_value: 'www.link1.com',
+          },
+          tags: {
+            pick_version: 'RESOLVED',
+            resolved_value: 4,
+          },
+          name: {
+            pick_version: 'RESOLVED',
+            resolved_value: 'Valid type name',
+          },
+        };
+        const result = RuleFieldsToUpgrade.safeParse(input);
+        expectParseError(result);
+        expect(stringifyZodError(result.error)).toMatchInlineSnapshot(
+          `"tags.resolved_value: Expected array, received number, references.resolved_value: Expected array, received string"`
+        );
+      });
+
+      it('invalidates unknown fields', () => {
+        const input = {
+          unknown_field: {
+            pick_version: 'CURRENT',
+          },
+        };
+        const result = RuleFieldsToUpgrade.safeParse(input);
+        expectParseError(result);
+        expect(stringifyZodError(result.error)).toMatchInlineSnapshot(
+          `"Unrecognized key(s) in object: 'unknown_field'"`
+        );
+      });
+
+      it('invalidates rule fields not part of RuleFieldsToUpgrade', () => {
+        const input = {
+          type: {
+            pick_version: 'BASE',
+          },
+          rule_id: {
+            pick_version: 'CURRENT',
+          },
+          version: {
+            pick_version: 'TARGET',
+          },
+          author: {
+            pick_version: 'MERGED',
+          },
+          license: {
+            pick_version: 'RESOLVED',
+            resolved_value: 'Elastic License',
+          },
+          concurrent_searches: {
+            pick_version: 'TARGET',
+          },
+          items_per_search: {
+            pick_version: 'TARGET',
+          },
+        };
+        const result = RuleFieldsToUpgrade.safeParse(input);
+        expectParseError(result);
+        expect(stringifyZodError(result.error)).toMatchInlineSnapshot(
+          `"Unrecognized key(s) in object: 'type', 'rule_id', 'version', 'author', 'license', 'concurrent_searches', 'items_per_search'"`
+        );
+      });
+    });
   });
 });
