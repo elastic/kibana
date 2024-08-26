3c178c68727754bf638871138a7fc81de516d280
Marius Iversen <marius.iversen@elastic.co>
2024-08-26 13:15:20 +0200

    add param types to all graphs, preparing for supporting more connectors

--- x-pack/plugins/integration_assistant/server/graphs/ecs/duplicates.ts
+++ x-pack/plugins/integration_assistant/server/graphs/ecs/duplicates.ts
@@ -4,18 +4,16 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import type {
-  ActionsClientChatOpenAI,
-  ActionsClientSimpleChatModel,
-} from '@kbn/langchain/server/language_models';
+
 import { JsonOutputParser } from '@langchain/core/output_parsers';
+import type { EcsNodeParams } from './types';
 import type { EcsMappingState } from '../../types';
 import { ECS_DUPLICATES_PROMPT } from './prompts';
 
-export async function handleDuplicates(
-  state: EcsMappingState,
-  model: ActionsClientChatOpenAI | ActionsClientSimpleChatModel
-) {
+export async function handleDuplicates({
+  state,
+  model,
+}: EcsNodeParams): Promise<Partial<EcsMappingState>> {
   const outputParser = new JsonOutputParser();
   const ecsDuplicatesGraph = ECS_DUPLICATES_PROMPT.pipe(model).pipe(outputParser);
 
