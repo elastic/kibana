b4ad81b529730d72b6eb6e4fb38d21dc70e014e6
Marta Bondyra <marta.bondyra@elastic.co>
2024-08-26 22:57:56 +0200

    [Lens] fix flaky test with deleting filters

--- test/functional/services/filter_bar.ts
+++ test/functional/services/filter_bar.ts
@@ -133,10 +133,11 @@ export class FilterBarService extends FtrService {
    * @param key field name
    */
   public async removeFilter(key: string): Promise<void> {
-    await this.retry.try(async () => {
+    await this.retry.waitFor('filter pill context menu is open', async () => {
       await this.testSubjects.click(`~filter & ~filter-key-${key}`);
-      await this.testSubjects.click(`deleteFilter`);
+      return await this.testSubjects.exists('deleteFilter');
     });
+    await this.testSubjects.click(`deleteFilter`);
     await this.header.awaitGlobalLoadingIndicatorHidden();
   }
 
