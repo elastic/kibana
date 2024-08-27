28e433a79abfe1e315ff3b531f556850ff013968
Dario Gieselaar <dario.gieselaar@elastic.co>
2024-08-27 20:46:48 +0200

    Rename folder to snake_case

--- x-pack/packages/observability/observability_utils/jest.config.js
+++ x-pack/packages/observability/observability_utils/jest.config.js
@@ -8,5 +8,5 @@
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/observability/observability-utils'],
+  roots: ['<rootDir>/x-pack/packages/observability/observability_utils'],
 };
