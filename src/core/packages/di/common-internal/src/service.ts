8d815ea8080b63b57b777f04db01e119ced42f0a
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-08-27 11:42:32 +0200

    Remove redundant services

--- src/core/packages/di/common-internal/src/service.ts
+++ src/core/packages/di/common-internal/src/service.ts
@@ -10,8 +10,8 @@
 import { Container, type interfaces } from 'inversify';
 import type { PluginOpaqueId } from '@kbn/core-base-common';
 import type { InternalCoreDiServiceSetup, InternalCoreDiServiceStart } from './contracts';
-import { InternalDiSetupService, InternalDiService } from './services';
-import { LifecycleModule, Plugin, PluginModule } from './modules';
+import { LifecycleModule } from './modules/lifecycle';
+import { Plugin, PluginModule } from './modules/plugin';
 
 /** @internal */
 export class CoreInjectionService {
@@ -28,7 +28,6 @@ export class CoreInjectionService {
     };
     this.root.load(new LifecycleModule());
     this.root.load(new PluginModule());
-    this.root.bind(InternalDiSetupService).toConstantValue(contract);
 
     return contract;
   }
@@ -58,8 +57,6 @@ export class CoreInjectionService {
       getContainer: this.getContainer,
       root: this.root,
     };
-
-    this.root.bind(InternalDiService).toConstantValue(contract);
 
     return contract;
   }
