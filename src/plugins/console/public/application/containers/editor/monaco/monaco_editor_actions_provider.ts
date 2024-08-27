2e5e850048c18178d78cb24c10e055d83e438a24
Elena Stoeva <elenastoeva99@gmail.com>
2024-08-27 15:43:48 +0100

    Add explanation for offset

--- src/plugins/console/public/application/containers/editor/monaco/monaco_editor_actions_provider.ts
+++ src/plugins/console/public/application/containers/editor/monaco/monaco_editor_actions_provider.ts
@@ -117,7 +117,8 @@ export class MonacoEditorActionsProvider {
       const offset = this.editor.getTopForLineNumber(lineNumber) - this.editor.getScrollTop();
       this.setEditorActionsCss({
         visibility: 'visible',
-        top: offset - 2, //  Move position up by 2 px
+        //  Move position up by 2 px to align the action buttons panel with the top border of the selected block
+        top: offset - 2,
       });
     }
   }
