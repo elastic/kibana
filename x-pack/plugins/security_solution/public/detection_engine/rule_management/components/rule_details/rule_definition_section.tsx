e22e0cd0d093303262d2368a9f31f7ac64644d34
Nikita Indik <nikita.indik@elastic.co>
2024-08-28 17:01:07 +0200

    Add a proposed fix that needs to be tested

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_definition_section.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_definition_section.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { isEmpty } from 'lodash/fp';
 import {
   EuiDescriptionList,
@@ -40,8 +40,6 @@ import { AlertSuppressionLabel } from '../../../rule_creation_ui/components/desc
 import { useGetSavedQuery } from '../../../../detections/pages/detection_engine/rules/use_get_saved_query';
 import * as threatMatchI18n from '../../../../common/components/threat_match/translations';
 import * as timelinesI18n from '../../../../timelines/components/timeline/translations';
-import { useRuleIndexPattern } from '../../../rule_creation_ui/pages/form';
-import { DataSourceType } from '../../../../detections/pages/detection_engine/rules/types';
 import type { Duration } from '../../../../detections/pages/detection_engine/rules/types';
 import { convertHistoryStartToSize } from '../../../../detections/pages/detection_engine/rules/helpers';
 import { MlJobsDescription } from '../../../rule_creation/components/ml_jobs_description/ml_jobs_description';
@@ -59,7 +57,6 @@ import {
   useRequiredFieldsStyles,
 } from './rule_definition_section.styles';
 import { getQueryLanguageLabel } from './helpers';
-import { useDefaultIndexPattern } from '../../hooks/use_default_index_pattern';
 import {
   EQL_OPTIONS_EVENT_CATEGORY_FIELD_LABEL,
   EQL_OPTIONS_EVENT_TIEBREAKER_FIELD_LABEL,
@@ -76,6 +73,47 @@ export const SavedQueryName = ({ savedQueryName }: SavedQueryNameProps) => (
   </EuiText>
 );
 
+interface UseFetchDataViewParams {
+  index?: string[];
+  dataViewId?: string;
+}
+
+interface UseFetchDataViewReturn {
+  dataView: DataView | null;
+  error: Error | null;
+  isLoading: boolean;
+}
+
+/*
+  Fetches a DataView by its ID if `dataViewId` is provided, or creates a new DataView out of index patterns if `index` is provided.
+*/
+function useFetchDataView({ dataViewId, index }: UseFetchDataViewParams): UseFetchDataViewReturn {
+  const { data } = useKibana().services;
+
+  const [dataView, setDataView] = useState<DataView | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    if (dataViewId) {
+      data.dataViews
+        .get(dataViewId)
+        .then((dv) => {
+          setDataView(dv);
+        })
+        .catch(setError);
+    } else if (index) {
+      data.dataViews
+        .create({ title: index.join(','), id: index.join(','), allowNoIndex: true })
+        .then((dv) => {
+          setDataView(dv);
+        })
+        .catch(setError);
+    }
+  }, [data, dataViewId, index]);
+
+  return { dataView, error, isLoading: !dataView && !error };
+}
+
 interface FiltersProps {
   filters: Filter[];
   dataViewId?: string;
@@ -89,16 +127,9 @@ export const Filters = ({
   index,
   'data-test-subj': dataTestSubj,
 }: FiltersProps) => {
+  const { dataView } = useFetchDataView({ dataViewId, index });
+
   const flattenedFilters = mapAndFlattenFilters(filters);
-
-  const defaultIndexPattern = useDefaultIndexPattern();
-
-  const { indexPattern } = useRuleIndexPattern({
-    dataSourceType: dataViewId ? DataSourceType.DataView : DataSourceType.IndexPatterns,
-    index: index ?? defaultIndexPattern,
-    dataViewId,
-  });
-
   const styles = filtersStyles;
 
   return (
@@ -109,7 +140,7 @@ export const Filters = ({
       responsive={false}
       gutterSize="xs"
     >
-      <FilterItems filters={flattenedFilters} indexPatterns={[indexPattern as DataView]} readOnly />
+      {dataView && <FilterItems filters={flattenedFilters} indexPatterns={[dataView]} readOnly />}
     </EuiFlexGroup>
   );
 };
