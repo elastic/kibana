e48d45f4f45cfc85bb69a10709f9ed517274ac13
Joseph McElroy <joseph.mcelroy@elastic.co>
2024-08-28 21:45:21 +0100

    fixes

--- x-pack/plugins/index_management/server/lib/fetch_indices.ts
+++ x-pack/plugins/index_management/server/lib/fetch_indices.ts
@@ -50,7 +50,7 @@ async function fetchIndicesCall(
 
   const indicesNames = Object.keys(indices);
 
-  if (!(config.isIndexStatsEnabled === false)) {
+  if (config.isIndexStatsEnabled) {
     const { indices: indicesStats } = await client.asCurrentUser.indices.stats({
       index: indexNamesString,
       expand_wildcards: ['hidden', 'all'],
