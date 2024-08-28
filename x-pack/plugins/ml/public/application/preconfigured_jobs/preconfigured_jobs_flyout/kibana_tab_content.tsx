9290b99f668386275c7279e53400548bc3c6e18f
Melissa <melissa.alvarez@elastic.co>
2024-08-28 15:26:40 -0600

    add links to kibana dashboards if they exist

--- x-pack/plugins/ml/public/application/preconfigured_jobs/preconfigured_jobs_flyout/kibana_tab_content.tsx
+++ x-pack/plugins/ml/public/application/preconfigured_jobs/preconfigured_jobs_flyout/kibana_tab_content.tsx
@@ -6,18 +6,22 @@
  */
 
 import type { FC } from 'react';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   EuiAccordion,
   EuiFlexGroup,
   EuiFlexItem,
   EuiHorizontalRule,
+  EuiLink,
   EuiNotificationBadge,
   EuiSpacer,
   EuiSplitPanel,
   EuiText,
 } from '@elastic/eui';
+import { asyncForEach } from '@kbn/std';
+import { ViewMode } from '@kbn/embeddable-plugin/public';
 import type { Module } from '../../../../common/types/modules';
+import { useDashboardService } from '../../services/dashboard_service';
 import { LABELS, type LabelId } from './overview_tab_content';
 import type { KibanaAssetType } from './flyout';
 
@@ -26,70 +30,110 @@ interface Props {
   selectedKibanaSubTab?: KibanaAssetType;
 }
 
-export const KibanaTabContent: FC<Props> = ({ module, selectedKibanaSubTab }) => (
-  <>
-    {Object.keys(module.kibana ?? {}).map((kibanaAsset) => {
-      return (
-        <>
-          <EuiAccordion
-            id={kibanaAsset}
-            initialIsOpen={kibanaAsset === selectedKibanaSubTab}
-            buttonContent={
-              <EuiFlexGroup
-                justifyContent="center"
-                alignItems="center"
-                gutterSize="s"
-                responsive={false}
-              >
-                <EuiFlexItem grow={false}>
-                  <EuiText size="m">
-                    <h4>{LABELS[kibanaAsset as LabelId]}</h4>
-                  </EuiText>
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  <EuiNotificationBadge color="subdued" size="m">
-                    <h3>{module.kibana[kibanaAsset]!.length}</h3>
-                  </EuiNotificationBadge>
-                </EuiFlexItem>
-              </EuiFlexGroup>
-            }
-            css={{ padding: '0 3%' }}
-          >
-            <EuiSpacer size="m" />
-            <EuiSplitPanel.Outer
-              hasBorder
-              hasShadow={false}
-              data-test-subj={`mlPreconfigJobsKibanaAssetAccordion.${kibanaAsset}`}
+export const KibanaTabContent: FC<Props> = ({ module, selectedKibanaSubTab }) => {
+  const [dashboardUrls, setDashboardUrls] = useState<Record<string, string>>({});
+  const dashboardService = useDashboardService();
+
+  useEffect(
+    function setUpDashboardUrls() {
+      const dashboards = module.kibana?.dashboard ?? [];
+      const dashboardIds = dashboards.map(({ id }) => id);
+      const urls = {};
+
+      async function getDashboardUrls() {
+        const result = await dashboardService.fetchDashboardsById(dashboardIds);
+
+        await asyncForEach(result, async ({ id }) => {
+          const url = await dashboardService.getDashboardUrl(id, ViewMode.VIEW);
+          if (url) {
+            urls[id] = url;
+          }
+        });
+
+        if (Object.keys(urls).length > 0) {
+          setDashboardUrls(urls);
+        }
+      }
+      if (dashboards.length > 0) {
+        getDashboardUrls();
+      }
+    },
+    [dashboardService, module.kibana?.dashboard]
+  );
+
+  return (
+    <>
+      {Object.keys(module.kibana ?? {}).map((kibanaAsset) => {
+        return (
+          <>
+            <EuiAccordion
+              id={kibanaAsset}
+              initialIsOpen={kibanaAsset === selectedKibanaSubTab}
+              buttonContent={
+                <EuiFlexGroup
+                  justifyContent="center"
+                  alignItems="center"
+                  gutterSize="s"
+                  responsive={false}
+                >
+                  <EuiFlexItem grow={false}>
+                    <EuiText size="m">
+                      <h4>{LABELS[kibanaAsset as LabelId]}</h4>
+                    </EuiText>
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    <EuiNotificationBadge color="subdued" size="m">
+                      <h3>{module.kibana[kibanaAsset]!.length}</h3>
+                    </EuiNotificationBadge>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              }
+              css={{ padding: '0 3%' }}
             >
-              {module.kibana[kibanaAsset]!.map(({ config, id, title }) => {
-                return (
-                  <>
-                    <EuiSplitPanel.Inner
-                      grow={false}
-                      key={id}
-                      data-test-subj={`mlPreconfigJobsKibanaAssetAccordion.${kibanaAsset}.${id}`}
-                    >
-                      <EuiText size="m">
-                        <p>{title}</p>
-                      </EuiText>
-                      {config.description && (
-                        <>
-                          <EuiSpacer size="s" />
-                          <EuiText size="s" color="subdued">
-                            <p>{config.description}</p>
-                          </EuiText>
-                        </>
-                      )}
-                    </EuiSplitPanel.Inner>
-                    <EuiHorizontalRule margin="none" />
-                  </>
-                );
-              })}
-            </EuiSplitPanel.Outer>
-          </EuiAccordion>
-          <EuiSpacer size="l" />
-        </>
-      );
-    })}
-  </>
-);
+              <EuiSpacer size="m" />
+              <EuiSplitPanel.Outer
+                hasBorder
+                hasShadow={false}
+                data-test-subj={`mlPreconfigJobsKibanaAssetAccordion.${kibanaAsset}`}
+              >
+                {module.kibana[kibanaAsset]!.map(({ config, id, title }) => {
+                  return (
+                    <>
+                      <EuiSplitPanel.Inner
+                        grow={false}
+                        key={id}
+                        data-test-subj={`mlPreconfigJobsKibanaAssetAccordion.${kibanaAsset}.${id}`}
+                      >
+                        <EuiText size="m">
+                          <p>
+                            {dashboardUrls && dashboardUrls[id] ? (
+                              <EuiLink href={dashboardUrls[id]} target="_blank">
+                                {title}
+                              </EuiLink>
+                            ) : (
+                              title
+                            )}
+                          </p>
+                        </EuiText>
+                        {config.description && (
+                          <>
+                            <EuiSpacer size="s" />
+                            <EuiText size="s" color="subdued">
+                              <p>{config.description}</p>
+                            </EuiText>
+                          </>
+                        )}
+                      </EuiSplitPanel.Inner>
+                      <EuiHorizontalRule margin="none" />
+                    </>
+                  );
+                })}
+              </EuiSplitPanel.Outer>
+            </EuiAccordion>
+            <EuiSpacer size="l" />
+          </>
+        );
+      })}
+    </>
+  );
+};
