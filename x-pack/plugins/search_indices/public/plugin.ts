e2a9fa0a9444336f6a2ee774612df47e1e97d8bc
Rodney Norris <rodney.norris@elastic.co>
2024-08-28 18:17:11 +0000

    search_indices: page setup
    Setting up the public application and scaffolding the start page to be
    used for onboarding.

--- x-pack/plugins/search_indices/public/plugin.ts
+++ x-pack/plugins/search_indices/public/plugin.ts
@@ -6,13 +6,41 @@
  */
 
 import type { CoreSetup, CoreStart, Plugin } from '@kbn/core/public';
-import type { SearchIndicesPluginSetup, SearchIndicesPluginStart } from './types';
+import { i18n } from '@kbn/i18n';
+import type {
+  SearchIndicesAppPluginStartDependencies,
+  SearchIndicesPluginSetup,
+  SearchIndicesPluginStart,
+  SearchIndicesServicesContext,
+} from './types';
 
 export class SearchIndicesPlugin
   implements Plugin<SearchIndicesPluginSetup, SearchIndicesPluginStart>
 {
-  public setup(core: CoreSetup): SearchIndicesPluginSetup {
-    return {};
+  public setup(
+    core: CoreSetup<SearchIndicesAppPluginStartDependencies, SearchIndicesPluginStart>
+  ): SearchIndicesPluginSetup {
+    core.application.register({
+      id: 'elasticsearchStart',
+      appRoute: '/app/elasticsearch/start',
+      title: i18n.translate('xpack.searchIndices.startAppTitle', {
+        defaultMessage: 'Elasticsearch Start',
+      }),
+      async mount({ element, history }) {
+        const { renderApp } = await import('./application');
+        const { ElasticsearchStartPage } = await import('./components/start/start_page');
+        const [coreStart, depsStart] = await core.getStartServices();
+        const startDeps: SearchIndicesServicesContext = {
+          ...depsStart,
+          history,
+        };
+        return renderApp(ElasticsearchStartPage, coreStart, startDeps, element);
+      },
+    });
+
+    return {
+      enabled: true,
+    };
   }
 
   public start(core: CoreStart): SearchIndicesPluginStart {
