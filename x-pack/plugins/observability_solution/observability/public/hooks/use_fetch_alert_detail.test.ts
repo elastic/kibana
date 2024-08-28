ff037a2d9bd61a37f68439e75c9e1b15016ecf98
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-08-28 10:29:00 +0200

    remove end from inline snapshot

--- x-pack/plugins/observability_solution/observability/public/hooks/use_fetch_alert_detail.test.ts
+++ x-pack/plugins/observability_solution/observability/public/hooks/use_fetch_alert_detail.test.ts
@@ -114,7 +114,6 @@ describe('useFetchAlertDetail', () => {
               "6": "o",
               "7": "n",
               "active": true,
-              "end": undefined,
               "fields": Object {
                 "@timestamp": "2022-01-31T18:20:57.204Z",
                 "_index": "alert-index",
