2678101fef6c75a04ce77d0e38f0b097d06b4282
Julia Bardi <90178898+juliaElastic@users.noreply.github.com>
2024-08-29 10:25:40 +0200

    [Fleet] add back monitoring for agentless policies (#191635)
    Added back agent monitoring on agentless policies as requested. It was
    incorrectly removed in https://github.com/elastic/kibana/pull/189612

--- x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.test.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.test.tsx
@@ -867,7 +867,7 @@ describe('When on the package policy create page', () => {
 
         expect(sendCreateAgentPolicy).toHaveBeenCalledWith(
           expect.objectContaining({
-            monitoring_enabled: [],
+            monitoring_enabled: ['logs', 'metrics'],
             name: 'Agentless policy for nginx-1',
             supports_agentless: true,
           }),
