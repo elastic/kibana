867575465339c8eff4f1c679feb40c9069a72972
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2024-08-29 11:46:59 -0400

    Update x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/list_page/index.tsx
    Co-authored-by: Julia Bardi <90178898+juliaElastic@users.noreply.github.com>

--- x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/list_page/index.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/list_page/index.tsx
@@ -98,7 +98,7 @@ export const AgentPolicyListPage: React.FunctionComponent<{}> = () => {
     sortField: sorting?.field,
     sortOrder: sorting?.direction,
     kuery: search,
-    noAgentCount: false, // Explicitely fetch agent count
+    noAgentCount: false, // Explicitly fetch agent count
     full: true,
   });
 
