8541db94f2ca3bd7d89ca6b39631ef440cc05928
Ilya Nikokoshev <ilya.nikokoshev@elastic.co>
2024-08-29 20:10:12 +0300

    Apply the partial shuffle

--- x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/data_stream_step/sample_logs_input.tsx
+++ x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/data_stream_step/sample_logs_input.tsx
@@ -176,6 +176,7 @@ const parseLogsContent = (
 
   let isTruncated = false;
   if (parsedContent.length > MaxLogsSampleRows) {
+    partialShuffleArray(parsedContent, 1, MaxLogsSampleRows);
     parsedContent = parsedContent.slice(0, MaxLogsSampleRows);
     isTruncated = true;
   }
