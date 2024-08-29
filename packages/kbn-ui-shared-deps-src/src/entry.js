5e128dbd2e639e2f411e2683ce706e4f06b3da0d
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2024-08-29 21:49:21 +0200

    Try with the package in both and no tree-shaking

--- packages/kbn-ui-shared-deps-src/src/entry.js
+++ packages/kbn-ui-shared-deps-src/src/entry.js
@@ -75,5 +75,4 @@ export const ReactQuery = require('@tanstack/react-query');
 export const ReactQueryDevtools = require('@tanstack/react-query-devtools');
 export const KbnCodeEditor = require('@kbn/code-editor');
 export const KbnEsqlAst = require('@kbn/esql-ast');
-const { afterFrame } = require('@elastic/apm-rum-core');
-export const ElasticApmRumCore = { afterFrame };
+export const ElasticApmRumCore = require('@elastic/apm-rum-core');
