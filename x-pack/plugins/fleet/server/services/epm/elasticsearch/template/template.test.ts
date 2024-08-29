f5a1a65a4e576d2ed5bed40b6e932d97035d0197
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2024-08-29 14:16:12 +0200

    [Fleet] Expand subfields of nested objects when generating template

--- x-pack/plugins/fleet/server/services/epm/elasticsearch/template/template.test.ts
+++ x-pack/plugins/fleet/server/services/epm/elasticsearch/template/template.test.ts
@@ -920,6 +920,35 @@ describe('EPM template', () => {
     expect(mappings).toEqual(expectedMapping);
   });
 
+  it('tests processing nested field with subfields', () => {
+    const nestedYaml = `
+  - name: a
+    type: nested
+    include_in_parent: true
+    fields:
+    - name: b
+      type: keyword
+    `;
+    const expectedMapping = {
+      properties: {
+        a: {
+          include_in_parent: true,
+          type: 'nested',
+          properties: {
+            b: {
+              ignore_above: 1024,
+              type: 'keyword',
+            },
+          },
+        },
+      },
+    };
+    const fields: Field[] = safeLoad(nestedYaml);
+    const processedFields = processFields(fields);
+    const mappings = generateMappings(processedFields);
+    expect(mappings).toEqual(expectedMapping);
+  });
+
   it('tests processing nested leaf field with properties', () => {
     const nestedYaml = `
   - name: a
