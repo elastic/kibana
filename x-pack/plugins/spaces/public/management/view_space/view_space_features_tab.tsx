70fb39faec93e29b7131e430e33ec0bb80f8aa94
Timothy Sullivan <tsullivan@elastic.co>
2024-08-29 11:26:11 -0700

    --wip-- [skip ci]

--- x-pack/plugins/spaces/public/management/view_space/view_space_features_tab.tsx
+++ x-pack/plugins/spaces/public/management/view_space/view_space_features_tab.tsx
@@ -23,7 +23,7 @@ interface Props {
   onChange: (updatedSpace: Partial<Space>) => void;
 }
 
-// FIXME: rename to EditSpaceEnabledFeatures
+// FIXME: rename to EditSpaceEnabledFeaturesPanel
 export const ViewSpaceEnabledFeatures: FC<Props> = ({ features, space, onChange }) => {
   const { capabilities, getUrlForApp } = useViewSpaceServices();
   const canManageRoles = capabilities.management?.security?.roles === true;
