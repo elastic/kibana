accc20c3502297f183009f020ac54fcb011cd0c1
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-08-29 02:45:39 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/security_solution/public/flyout/document_details/shared/components/cell_actions.tsx
+++ x-pack/plugins/security_solution/public/flyout/document_details/shared/components/cell_actions.tsx
@@ -9,7 +9,7 @@ import type { FC } from 'react';
 import React, { useMemo } from 'react';
 import { useDocumentDetailsContext } from '../context';
 import { getSourcererScopeId } from '../../../../helpers';
-import { useBasicDataFromDetailsData } from '../../shared/hooks/use_basic_data_from_details_data';
+import { useBasicDataFromDetailsData } from '../hooks/use_basic_data_from_details_data';
 import { SecurityCellActionType } from '../../../../app/actions/constants';
 import {
   CellActionsMode,
