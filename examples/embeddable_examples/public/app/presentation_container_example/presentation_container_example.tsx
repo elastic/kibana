0cf1d1e46bcb4dd9e81d880796c8da647728e60a
Nathan Reese <reese.nathan@gmail.com>
2024-08-30 11:21:51 -0600

    types

--- examples/embeddable_examples/public/app/presentation_container_example/presentation_container_example.tsx
+++ examples/embeddable_examples/public/app/presentation_container_example/presentation_container_example.tsx
@@ -12,18 +12,24 @@ import { EuiCallOut } from '@elastic/eui';
 export const PresentationContainerExample = () => {
   return (
     <div>
-      <EuiCallOut
-        title="PresentationContainer interface"
-      >
+      <EuiCallOut title="Presentation Container interfaces">
         <p>
-          Implement the <strong>PresentationContainer</strong> interface to allow users to add and remove embeddables in an applicaiton.
+          At times, you will need to render many embeddables and allow users to add, remove, and
+          re-arrange embeddables. Use the <strong>PresentationContainer</strong> and{' '}
+          <strong>CanAddNewPanel</strong> interfaces for this functionallity.
         </p>
         <p>
-          Each embeddable manages its own state.
-          The page is only responsible for persisting and providing the last persisted state to the embeddable on startup.
-          This example uses session storage to persist saved state and unsaved changes.
+          Each embeddable manages its own state. The page is only responsible for persisting and
+          providing the last persisted state to the embeddable. Implement{' '}
+          <strong>HasSerializedChildState</strong> interface to provide an embeddable with last
+          persisted state. Implement <strong>HasRuntimeChildState</strong> interface to provide an
+          embeddable with previous sessions&apos;s unsaved changes.
+        </p>
+        <p>
+          This example uses session storage to persist saved state and unsaved changes while a
+          production implemenation may choose to persist state elsewhere.
         </p>
       </EuiCallOut>
     </div>
   );
-}
+};
---
+++ examples/embeddable_examples/public/app/presentation_container_example/types.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import { TimeRange } from '@kbn/es-query';
+import {
+  CanAddNewPanel,
+  HasSerializedChildState,
+  HasRuntimeChildState,
+  PresentationContainer,
+  SerializedPanelState,
+} from '@kbn/presentation-containers';
+import { PublishesTimeRange } from '@kbn/presentation-publishing';
+
+export type ParentApi = PresentationContainer &
+  CanAddNewPanel &
+  HasSerializedChildState &
+  HasRuntimeChildState &
+  PublishesTimeRange;
+
+export interface SerializedState {
+  timeRange: TimeRange;
+  panels: SerializedPanelState[];
+}
