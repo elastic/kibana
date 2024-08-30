8d6d0e958b3de5ff731a7b5263b9b26a85f7636b
iblancof <irene.blanco@elastic.co>
2024-08-30 12:56:00 +0200

    [APM][Services] Comment blocked code in ServiceTabContent

--- x-pack/plugins/observability_solution/apm/public/components/app/service_tab_content/index.tsx
+++ x-pack/plugins/observability_solution/apm/public/components/app/service_tab_content/index.tsx
@@ -7,14 +7,7 @@
 
 import React from 'react';
 import { ReactNode } from 'react-markdown';
-// import { SignalTypes } from '../../../../common/entities/types';
-// import { useApmServiceContext } from '../../../context/apm_service/use_apm_service_context';
-// import { isApmSignal, isLogsSignal } from '../../../utils/get_signal_type';
-import { EuiSpacer } from '@elastic/eui';
-import { ServiceTabEmptyState } from '../service_tab_empty_state';
-import { emptyStateContent } from './constants';
 import { Tab } from './types';
-import { useLocalStorage } from '../../../hooks/use_local_storage';
 
 interface ServiceTabContentProps {
   tabName: Tab;
@@ -31,45 +24,46 @@ export function ServiceTabContent({ children, tabName }: ServiceTabContentProps)
   //   !isApmSignal(serviceEntitySummary.signalTypes as SignalTypes[]) &&
   // isLogsSignal(serviceEntitySummary.signalTypes as SignalTypes[]);
 
-  const hasOnlyLogsSignal = true;
+  // const [dismissedLogsOnlyEmptyState, setdismissedLogsOnlyEmptyState] = useLocalStorage(
+  //   `apm.dismissedLogsOnlyEmptyState.${tabName}`,
+  //   false
+  // );
 
-  const [dismissedLogsOnlyEmptyState, setdismissedLogsOnlyEmptyState] = useLocalStorage(
-    `apm.dismissedLogsOnlyEmptyState.${tabName}`,
-    false
-  );
+  // const displayEmptyStateOnly = tabName !== 'overview';
 
-  const displayEmptyStateOnly = tabName !== 'overview';
+  return <>{children}</>;
 
-  return (
-    <>
-      {hasOnlyLogsSignal ? (
-        <>
-          {displayEmptyStateOnly ? (
-            <ServiceTabEmptyState
-              title={emptyStateContent[tabName].title}
-              content={emptyStateContent[tabName].content}
-              imgSrc={emptyStateContent[tabName].imgSrc ?? null}
-            />
-          ) : (
-            <>
-              {!dismissedLogsOnlyEmptyState && (
-                <ServiceTabEmptyState
-                  title={emptyStateContent[tabName].title}
-                  content={emptyStateContent[tabName].content}
-                  imgSrc={emptyStateContent[tabName].imgSrc ?? null}
-                  dismissable={true}
-                  onDissmiss={() => setdismissedLogsOnlyEmptyState(true)}
-                />
-              )}
-              <EuiSpacer size="m" />
+  // commented/blocked until PR #191183 is merged
+  // return (
+  //   <>
+  //     {hasOnlyLogsSignal ? (
+  //       <>
+  //         {displayEmptyStateOnly ? (
+  //           <ServiceTabEmptyState
+  //             title={emptyStateContent[tabName].title}
+  //             content={emptyStateContent[tabName].content}
+  //             imgSrc={emptyStateContent[tabName].imgSrc ?? null}
+  //           />
+  //         ) : (
+  //           <>
+  //             {!dismissedLogsOnlyEmptyState && (
+  //               <ServiceTabEmptyState
+  //                 title={emptyStateContent[tabName].title}
+  //                 content={emptyStateContent[tabName].content}
+  //                 imgSrc={emptyStateContent[tabName].imgSrc ?? null}
+  //                 dismissable={true}
+  //                 onDissmiss={() => setdismissedLogsOnlyEmptyState(true)}
+  //               />
+  //             )}
+  //             <EuiSpacer size="m" />
 
-              {children}
-            </>
-          )}
-        </>
-      ) : (
-        <>{children}</>
-      )}
-    </>
-  );
+  //             {children}
+  //           </>
+  //         )}
+  //       </>
+  //     ) : (
+  //       <>{children}</>
+  //     )}
+  //   </>
+  // );
 }
