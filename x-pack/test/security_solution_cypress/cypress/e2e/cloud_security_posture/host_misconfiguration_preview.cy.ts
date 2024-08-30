a31584cbda48d3d25e15ea7781855924d2fb2440
animehart <rickyangwyn@gmail.com>
2024-08-30 00:01:14 -0700

    add more cypress stuff

--- x-pack/test/security_solution_cypress/cypress/e2e/cloud_security_posture/host_misconfiguration_preview.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/cloud_security_posture/host_misconfiguration_preview.cy.ts
@@ -7,7 +7,7 @@
 
 import { getDataTestSubjectSelector } from '../../helpers/common';
 
-import { deleteAlertsAndRules } from '../../tasks/api_calls/common';
+import { deleteAlertsAndRules, rootRequest } from '../../tasks/api_calls/common';
 import { expandFirstAlertHostFlyout } from '../../tasks/asset_criticality/common';
 import { waitForAlertsToPopulate } from '../../tasks/create_new_rule';
 import { login } from '../../tasks/login';
@@ -22,6 +22,52 @@ const HOST_INSIGHT_MISCONFIGURATION = getDataTestSubjectSelector(
 const HOST_INSIGHT_MISCONFIGURATION_TITLE = getDataTestSubjectSelector(
   'securitySolutionFlyoutInsightsMisconfigurationsTitleText'
 );
+const timestamp = Date.now();
+
+// Create a Date object using the timestamp
+const date = new Date(timestamp);
+
+// Convert the Date object to ISO 8601 format
+const iso8601String = date.toISOString();
+const mockFinding = {
+  '@timestamp': iso8601String,
+  resource: { id: '1234ABCD', name: `kubelet`, sub_type: 'lower case sub type' },
+  result: { evaluation: 'passed' },
+  rule: {
+    name: 'Upper case rule name',
+    section: 'Upper case section',
+    benchmark: {
+      id: 'cis_k8s',
+      posture_type: 'kspm',
+      name: 'CIS Kubernetes V1.23',
+      version: 'v1.0.0',
+    },
+    type: 'process',
+  },
+  cluster_id: 'Upper case cluster id',
+  data_stream: {
+    dataset: 'cloud_security_posture.findings',
+  },
+};
+
+const createMockFinding = (mockFindings) => {
+  return rootRequest({
+    method: 'POST',
+    url: `${Cypress.env(
+      'ELASTICSEARCH_URL'
+    )}/logs-cloud_security_posture.findings_latest-default/_doc`,
+    body: mockFinding,
+  });
+};
+
+const deleteDataStream = () => {
+  return rootRequest({
+    method: 'DELETE',
+    url: `${Cypress.env(
+      'ELASTICSEARCH_URL'
+    )}/_data_stream/logs-cloud_security_posture.findings_latest-default`,
+  });
+};
 
 describe('Alert Host details expandable flyout', { tags: ['@ess', '@serverless'] }, () => {
   beforeEach(() => {
@@ -33,11 +79,24 @@ describe('Alert Host details expandable flyout', { tags: ['@ess', '@serverless']
     expandFirstAlertHostFlyout();
   });
 
-  it('should not display Misconfiguration preview under Insights Entities when all Indices is empty', () => {
+  after(() => {
+    deleteDataStream();
+  });
+
+  it('should not display Misconfiguration preview under Insights Entities when it does not have Misconfiguration Findings', () => {
     cy.log('check if Misconfiguration preview section is not rendered');
     cy.get(HOST_INSIGHT_MISCONFIGURATION).should('not.exist');
 
     cy.log('check if Misconfiguration preview title is not shown');
     cy.get(HOST_INSIGHT_MISCONFIGURATION_TITLE).should('not.exist');
+  });
+
+  it('should display Misconfiguration preview under Insights Entities when it has Misconfiguration Findings', () => {
+    createMockFinding(mockFinding);
+    cy.log('check if Misconfiguration preview section is not rendered');
+    cy.get(HOST_INSIGHT_MISCONFIGURATION).should('exist');
+
+    cy.log('check if Misconfiguration preview title is not shown');
+    cy.get(HOST_INSIGHT_MISCONFIGURATION_TITLE).should('exist');
   });
 });
