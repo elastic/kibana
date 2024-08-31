74b46173341f1f568fce9b77192087e3b6383eee
Matt Kime <matt@mattki.me>
2024-08-30 21:13:45 -0500

    only call resolve/cluster when not in serverless

--- src/plugins/data_views/public/services/has_data.test.ts
+++ src/plugins/data_views/public/services/has_data.test.ts
@@ -25,7 +25,7 @@ describe('when calling hasData service', () => {
       );
 
       const hasData = new HasData();
-      const hasDataService = hasData.start(coreStart);
+      const hasDataService = hasData.start(coreStart, true);
       const response = hasDataService.hasDataView();
 
       expect(spy).toHaveBeenCalledTimes(1);
@@ -46,7 +46,7 @@ describe('when calling hasData service', () => {
       );
 
       const hasData = new HasData();
-      const hasDataService = hasData.start(coreStart);
+      const hasDataService = hasData.start(coreStart, true);
       const response = hasDataService.hasDataView();
 
       expect(spy).toHaveBeenCalledTimes(1);
@@ -64,7 +64,7 @@ describe('when calling hasData service', () => {
         .mockImplementation(() => Promise.reject(new Error('Oops')));
 
       const hasData = new HasData();
-      const hasDataService = hasData.start(coreStart);
+      const hasDataService = hasData.start(coreStart, true);
       const response = hasDataService.hasDataView();
 
       expect(spy).toHaveBeenCalledTimes(1);
@@ -85,7 +85,7 @@ describe('when calling hasData service', () => {
       );
 
       const hasData = new HasData();
-      const hasDataService = hasData.start(coreStart);
+      const hasDataService = hasData.start(coreStart, true);
       const response = hasDataService.hasUserDataView();
 
       expect(spy).toHaveBeenCalledTimes(1);
@@ -106,7 +106,7 @@ describe('when calling hasData service', () => {
       );
 
       const hasData = new HasData();
-      const hasDataService = hasData.start(coreStart);
+      const hasDataService = hasData.start(coreStart, true);
       const response = hasDataService.hasUserDataView();
 
       expect(spy).toHaveBeenCalledTimes(1);
@@ -124,7 +124,7 @@ describe('when calling hasData service', () => {
         .mockImplementation(() => Promise.reject(new Error('Oops')));
 
       const hasData = new HasData();
-      const hasDataService = hasData.start(coreStart);
+      const hasDataService = hasData.start(coreStart, true);
       const response = hasDataService.hasUserDataView();
 
       expect(spy).toHaveBeenCalledTimes(1);
@@ -144,7 +144,7 @@ describe('when calling hasData service', () => {
           .mockImplementation(() => Promise.resolve({ hasEsData: true }));
 
         const hasData = new HasData();
-        const hasDataService = hasData.start(coreStart);
+        const hasDataService = hasData.start(coreStart, true);
         const response = hasDataService.hasESData();
 
         expect(spy).toHaveBeenCalledTimes(1);
@@ -162,7 +162,7 @@ describe('when calling hasData service', () => {
           .mockImplementation(() => Promise.resolve({ hasEsData: false }));
 
         const hasData = new HasData();
-        const hasDataService = hasData.start(coreStart);
+        const hasDataService = hasData.start(coreStart, true);
         const response = hasDataService.hasESData();
 
         expect(spy).toHaveBeenCalledTimes(1);
@@ -171,31 +171,28 @@ describe('when calling hasData service', () => {
       });
     });
 
-    describe('resolve/cluster calls fail', () => {
+    describe('resolve/cluster not available', () => {
       it('should return true for hasESData when indices exist', async () => {
         const coreStart = coreMock.createStart();
         const http = coreStart.http;
 
         // Mock getIndices
-        const spy = jest
-          .spyOn(http, 'get')
-          .mockImplementationOnce(() => Promise.reject(new Error('oops')))
-          .mockImplementationOnce(() =>
-            Promise.resolve({
-              aliases: [],
-              data_streams: [],
-              indices: [
-                {
-                  aliases: [],
-                  attributes: ['open'],
-                  name: 'sample_data_logs',
-                },
-              ],
-            })
-          );
+        const spy = jest.spyOn(http, 'get').mockImplementationOnce(() =>
+          Promise.resolve({
+            aliases: [],
+            data_streams: [],
+            indices: [
+              {
+                aliases: [],
+                attributes: ['open'],
+                name: 'sample_data_logs',
+              },
+            ],
+          })
+        );
 
         const hasData = new HasData();
-        const hasDataService = hasData.start(coreStart);
+        const hasDataService = hasData.start(coreStart, false);
         const response = hasDataService.hasESData();
 
         expect(spy).toHaveBeenCalledTimes(1);
@@ -208,19 +205,16 @@ describe('when calling hasData service', () => {
         const http = coreStart.http;
 
         // Mock getIndices
-        const spy = jest
-          .spyOn(http, 'get')
-          .mockImplementationOnce(() => Promise.reject(new Error('oops')))
-          .mockImplementation(() =>
-            Promise.resolve({
-              aliases: [],
-              data_streams: [],
-              indices: [],
-            })
-          );
+        const spy = jest.spyOn(http, 'get').mockImplementation(() =>
+          Promise.resolve({
+            aliases: [],
+            data_streams: [],
+            indices: [],
+          })
+        );
 
         const hasData = new HasData();
-        const hasDataService = hasData.start(coreStart);
+        const hasDataService = hasData.start(coreStart, false);
         const response = hasDataService.hasESData();
 
         expect(spy).toHaveBeenCalledTimes(1);
@@ -233,27 +227,24 @@ describe('when calling hasData service', () => {
         const http = coreStart.http;
 
         // Mock getIndices
-        const spy = jest
-          .spyOn(http, 'get')
-          .mockImplementationOnce(() => Promise.reject(new Error('oops')))
-          .mockImplementation((path: any) =>
-            Promise.resolve({
-              aliases: [],
-              data_streams: path.includes('*:*')
-                ? [] // return empty on remote cluster call
-                : [
-                    {
-                      name: 'logs-enterprise_search.api-default',
-                      timestamp_field: '@timestamp',
-                      backing_indices: ['.ds-logs-enterprise_search.api-default-2022.03.07-000001'],
-                    },
-                  ],
-              indices: [],
-            })
-          );
+        const spy = jest.spyOn(http, 'get').mockImplementation((path: any) =>
+          Promise.resolve({
+            aliases: [],
+            data_streams: path.includes('*:*')
+              ? [] // return empty on remote cluster call
+              : [
+                  {
+                    name: 'logs-enterprise_search.api-default',
+                    timestamp_field: '@timestamp',
+                    backing_indices: ['.ds-logs-enterprise_search.api-default-2022.03.07-000001'],
+                  },
+                ],
+            indices: [],
+          })
+        );
 
         const hasData = new HasData();
-        const hasDataService = hasData.start(coreStart);
+        const hasDataService = hasData.start(coreStart, false);
         const response = hasDataService.hasESData();
 
         expect(spy).toHaveBeenCalledTimes(1);
@@ -273,12 +264,12 @@ describe('when calling hasData service', () => {
           .spyOn(http, 'post')
           .mockImplementation(() => Promise.resolve({ total: 10 }));
         const hasData = new HasData();
-        const hasDataService = hasData.start(coreStart);
+        const hasDataService = hasData.start(coreStart, false);
         const response = await hasDataService.hasESData();
 
         expect(response).toBe(true);
 
-        expect(spyGetIndices).toHaveBeenCalledTimes(2);
+        expect(spyGetIndices).toHaveBeenCalledTimes(1);
         expect(spySearch).toHaveBeenCalledTimes(1);
       });
 
@@ -294,12 +285,12 @@ describe('when calling hasData service', () => {
           .spyOn(http, 'post')
           .mockImplementation(() => Promise.reject(new Error('oops')));
         const hasData = new HasData();
-        const hasDataService = hasData.start(coreStart);
+        const hasDataService = hasData.start(coreStart, false);
         const response = await hasDataService.hasESData();
 
         expect(response).toBe(true);
 
-        expect(spyGetIndices).toHaveBeenCalledTimes(2);
+        expect(spyGetIndices).toHaveBeenCalledTimes(1);
         expect(spySearch).toHaveBeenCalledTimes(1);
       });
     });
