567e25aa907a74ca26e3c3f293b5f422cf1d4546
Cee Chen <constance.chen@elastic.co>
2024-09-03 14:37:02 -0700

    Fix other Jest tests toggling column actions to use data-test-subj instead of text

--- x-pack/plugins/lens/public/visualizations/datatable/components/table_basic.test.tsx
+++ x-pack/plugins/lens/public/visualizations/datatable/components/table_basic.test.tsx
@@ -172,7 +172,7 @@ describe('DatatableComponent', () => {
 
   test('it should render hide, reset, and sort actions on header even when it is in read only mode', () => {
     renderDatatableComponent({ renderMode: 'view' });
-    userEvent.click(screen.getByRole('button', { name: 'a' }));
+    userEvent.click(screen.getByTestId('dataGridHeaderCellActionButton-a'));
     const actionPopover = screen.getByRole('dialog');
     const actions = within(actionPopover)
       .getAllByRole('button')
