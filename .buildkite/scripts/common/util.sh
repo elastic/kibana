ba1efad437b3efb93af9a22e15181d6b5c4a0905
Alex Szabo <alex.szabo@elastic.co>
2024-09-03 15:06:16 +0200

    add retries to the only docker pull

--- .buildkite/scripts/common/util.sh
+++ .buildkite/scripts/common/util.sh
@@ -124,3 +124,32 @@ set_git_merge_base() {
 
   export GITHUB_PR_MERGE_BASE
 }
+
+docker_with_retry () {
+  cmd=$1
+  shift
+  args=("$@")
+  attempt=0
+  max_retries=5
+  sleep_time=15
+
+  while true
+  do
+    attempt=$((attempt+1))
+
+    if [ $attempt -gt $max_retries ]
+    then
+      echo "Docker $cmd retries exceeded, aborting."
+      exit 1
+    fi
+
+    if docker "$cmd" "${args[@]}"
+    then
+      echo "Docker $cmd successful."
+      break
+    else
+      echo "Docker $cmd unsuccessful, attempt '$attempt'... Retrying in $sleep_time"
+      sleep $sleep_time
+    fi
+  done
+}
