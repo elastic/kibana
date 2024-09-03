1680ce9845653069666385cb18dc108043b1acd1
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-09-03 16:20:32 +0200

    use download script

--- .buildkite/scripts/steps/performance/get_kibana_build.sh
+++ .buildkite/scripts/steps/performance/get_kibana_build.sh
@@ -4,36 +4,40 @@ set -euo pipefail
 
 .buildkite/scripts/bootstrap.sh
 
+source .buildkite/scripts/common/util.sh
+
 KIBANA_BUILD_ID=$(ts-node "$(dirname "${0}")/last_successful_build.ts")
 
 echo "...${KIBANA_BUILD_ID}..."
 export KIBANA_BUILD_ID
 
+.buildkite/scripts/download_build_artifacts.sh
 
-if [[ "${KIBANA_BUILD_ID:-}" != "false" ]]; then
-  if [[ ! -d "$KIBANA_BUILD_LOCATION/bin" ]]; then
-    echo '--- Downloading Distribution and Plugin artifacts'
 
-    cd "$WORKSPACE"
+# if [[ "${KIBANA_BUILD_ID:-}" != "false" ]]; then
+#   if [[ ! -d "$KIBANA_BUILD_LOCATION/bin" ]]; then
+#     echo '--- Downloading Distribution and Plugin artifacts'
 
-    download_artifact kibana-default.tar.gz . --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
-    download_artifact kibana-default-plugins.tar.gz . --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
+#     cd "$WORKSPACE"
 
-    mkdir -p "$KIBANA_BUILD_LOCATION"
-    tar -xzf kibana-default.tar.gz -C "$KIBANA_BUILD_LOCATION" --strip=1
+#     download_artifact kibana-default.tar.gz . --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
+#     download_artifact kibana-default-plugins.tar.gz . --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
 
-    if is_pr_with_label "ci:build-example-plugins"; then
-      # Testing against an example plugin distribution is not supported,
-      # mostly due to snapshot failures when testing UI element lists
-      rm -rf "$KIBANA_BUILD_LOCATION/plugins"
-      mkdir "$KIBANA_BUILD_LOCATION/plugins"
-    fi
+#     mkdir -p "$KIBANA_BUILD_LOCATION"
+#     tar -xzf kibana-default.tar.gz -C "$KIBANA_BUILD_LOCATION" --strip=1
 
-    cd "$KIBANA_DIR"
+#     if is_pr_with_label "ci:build-example-plugins"; then
+#       # Testing against an example plugin distribution is not supported,
+#       # mostly due to snapshot failures when testing UI element lists
+#       rm -rf "$KIBANA_BUILD_LOCATION/plugins"
+#       mkdir "$KIBANA_BUILD_LOCATION/plugins"
+#     fi
 
-    tar -xzf ../kibana-default-plugins.tar.gz
-  fi
-fi
+#     cd "$KIBANA_DIR"
+
+#     tar -xzf ../kibana-default-plugins.tar.gz
+#   fi
+# fi
 
 # .buildkite/scripts/build_kibana.sh
 # .buildkite/scripts/build_kibana_plugins.sh
