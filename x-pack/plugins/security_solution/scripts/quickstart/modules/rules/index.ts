55305a34bf5c04f3145b7d84f9e55a6cc6215357
Marshall Main <marshall.main@elastic.co>
2024-09-03 11:37:43 -0400

    Remove outdated use of clientConcurrency

--- x-pack/plugins/security_solution/scripts/quickstart/modules/rules/index.ts
+++ x-pack/plugins/security_solution/scripts/quickstart/modules/rules/index.ts
@@ -6,29 +6,13 @@
  */
 
 import { range } from 'lodash';
-import { clientConcurrency } from '@kbn/securitysolution-utils/src/client_concurrency';
-import type { RuleCreateProps, RuleResponse } from '../../../../common/api/detection_engine';
-import type { Client } from '../../../../common/api/quickstart_client.gen';
+import type { RuleCreateProps } from '../../../../common/api/detection_engine';
 
 export const duplicateRuleParams = (
   rule: RuleCreateProps,
   numCopies: number
 ): RuleCreateProps[] => {
   return range(numCopies).map((idx) => ({ ...rule, name: `${rule.name}_${idx}` }));
-};
-
-export const createRulesConcurrently = async (
-  client: Client,
-  rules: RuleCreateProps[],
-  concurrency: number = 5
-): Promise<RuleResponse[]> => {
-  return (
-    await clientConcurrency(
-      client.createRule,
-      rules.map((rule) => ({ body: rule })),
-      concurrency
-    )
-  ).map((response) => response.data);
 };
 
 export const getBasicRuleMetadata = () => ({
