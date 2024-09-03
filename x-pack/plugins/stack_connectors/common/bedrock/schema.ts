elastic/kibana

commit 3e0a431144d6afbc35c5bab5cf09735d73cb879f

Inference plugin: Add Bedrock model adapter  (#191434)
## Summary

Add the `bedrock` (well, bedrock-claude) model adapter for the inference
plugin. Also had to perform minor changes on the associated connector to
add support for new capabilities.

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/stack_connectors/common/bedrock/schema.ts
@@ -28,13 +28,30 @@ export const RunActionParamsSchema = schema.object({
   raw: schema.maybe(schema.boolean()),
 });
 
+export const BedrockMessageSchema = schema.object(
+  {
+    role: schema.string(),
+    content: schema.maybe(schema.string()),
+    rawContent: schema.maybe(schema.arrayOf(schema.any())),
+  },
+  {
+    validate: (value) => {
+      if (value.content === undefined && value.rawContent === undefined) {
+        return 'Must specify either content or rawContent';
+      } else if (value.content !== undefined && value.rawContent !== undefined) {
+        return 'content and rawContent can not be used at the same time';
+      }
+    },
+  }
+);
+
+export const BedrockToolChoiceSchema = schema.object({
+  type: schema.oneOf([schema.literal('auto'), schema.literal('any'), schema.literal('tool')]),
+  name: schema.maybe(schema.string()),
+});
+
 export const InvokeAIActionParamsSchema = schema.object({
-  messages: schema.arrayOf(
-    schema.object({
-      role: schema.string(),
-      content: schema.string(),
-    })
-  ),
+  messages: schema.arrayOf(BedrockMessageSchema),
   model: schema.maybe(schema.string()),
   temperature: schema.maybe(schema.number()),
   stopSequences: schema.maybe(schema.arrayOf(schema.string())),
@@ -53,6 +70,7 @@ export const InvokeAIActionParamsSchema = schema.object({
       })
     )
   ),
+  toolChoice: schema.maybe(BedrockToolChoiceSchema),
 });
 
 export const InvokeAIActionResponseSchema = schema.object({
@@ -60,12 +78,7 @@ export const InvokeAIActionResponseSchema = schema.object({
 });
 
 export const InvokeAIRawActionParamsSchema = schema.object({
-  messages: schema.arrayOf(
-    schema.object({
-      role: schema.string(),
-      content: schema.any(),
-    })
-  ),
+  messages: schema.arrayOf(BedrockMessageSchema),
   model: schema.maybe(schema.string()),
   temperature: schema.maybe(schema.number()),
   stopSequences: schema.maybe(schema.arrayOf(schema.string())),
@@ -84,6 +97,7 @@ export const InvokeAIRawActionParamsSchema = schema.object({
       })
     )
   ),
+  toolChoice: schema.maybe(BedrockToolChoiceSchema),
 });
 
 export const InvokeAIRawActionResponseSchema = schema.object({}, { unknowns: 'allow' });

x-pack/plugins/stack_connectors/common/bedrock/types.ts
@@ -19,6 +19,8 @@ import {
   InvokeAIRawActionResponseSchema,
   StreamingResponseSchema,
   RunApiLatestResponseSchema,
+  BedrockMessageSchema,
+  BedrockToolChoiceSchema,
 } from './schema';
 
 export type Config = TypeOf<typeof ConfigSchema>;
@@ -33,3 +35,5 @@ export type RunActionResponse = TypeOf<typeof RunActionResponseSchema>;
 export type StreamingResponse = TypeOf<typeof StreamingResponseSchema>;
 export type DashboardActionParams = TypeOf<typeof DashboardActionParamsSchema>;
 export type DashboardActionResponse = TypeOf<typeof DashboardActionResponseSchema>;
+export type BedRockMessage = TypeOf<typeof BedrockMessageSchema>;
+export type BedrockToolChoice = TypeOf<typeof BedrockToolChoiceSchema>;

x-pack/plugins/stack_connectors/server/connector_types/bedrock/bedrock.ts
@@ -22,7 +22,7 @@ import {
   RunActionResponseSchema,
   RunApiLatestResponseSchema,
 } from '../../../common/bedrock/schema';
-import {
+import type {
   Config,
   Secrets,
   RunActionParams,
@@ -32,6 +32,8 @@ import {
   InvokeAIRawActionParams,
   InvokeAIRawActionResponse,
   RunApiLatestResponse,
+  BedRockMessage,
+  BedrockToolChoice,
 } from '../../../common/bedrock/types';
 import {
   SUB_ACTION,
@@ -309,13 +311,14 @@ The Kibana Connector in use may need to be reconfigured with an updated Amazon B
       signal,
       timeout,
       tools,
+      toolChoice,
     }: InvokeAIActionParams | InvokeAIRawActionParams,
     connectorUsageCollector: ConnectorUsageCollector
   ): Promise<IncomingMessage> {
     const res = (await this.streamApi(
       {
         body: JSON.stringify(
-          formatBedrockBody({ messages, stopSequences, system, temperature, tools })
+          formatBedrockBody({ messages, stopSequences, system, temperature, tools, toolChoice })
         ),
         model,
         signal,
@@ -344,13 +347,23 @@ The Kibana Connector in use may need to be reconfigured with an updated Amazon B
       maxTokens,
       signal,
       timeout,
+      tools,
+      toolChoice,
     }: InvokeAIActionParams,
     connectorUsageCollector: ConnectorUsageCollector
   ): Promise<InvokeAIActionResponse> {
     const res = (await this.runApi(
       {
         body: JSON.stringify(
-          formatBedrockBody({ messages, stopSequences, system, temperature, maxTokens })
+          formatBedrockBody({
+            messages,
+            stopSequences,
+            system,
+            temperature,
+            maxTokens,
+            tools,
+            toolChoice,
+          })
         ),
         model,
         signal,
@@ -372,6 +385,7 @@ The Kibana Connector in use may need to be reconfigured with an updated Amazon B
       signal,
       timeout,
       tools,
+      toolChoice,
       anthropicVersion,
     }: InvokeAIRawActionParams,
     connectorUsageCollector: ConnectorUsageCollector
@@ -385,6 +399,7 @@ The Kibana Connector in use may need to be reconfigured with an updated Amazon B
           temperature,
           max_tokens: maxTokens,
           tools,
+          tool_choice: toolChoice,
           anthropic_version: anthropicVersion,
         }),
         model,
@@ -405,14 +420,16 @@ const formatBedrockBody = ({
   system,
   maxTokens = DEFAULT_TOKEN_LIMIT,
   tools,
+  toolChoice,
 }: {
-  messages: Array<{ role: string; content?: string }>;
+  messages: BedRockMessage[];
   stopSequences?: string[];
   temperature?: number;
   maxTokens?: number;
   // optional system message to be sent to the API
   system?: string;
   tools?: Array<{ name: string; description: string }>;
+  toolChoice?: BedrockToolChoice;
 }) => ({
   anthropic_version: 'bedrock-2023-05-31',
   ...ensureMessageFormat(messages, system),
@@ -420,7 +437,13 @@ const formatBedrockBody = ({
   stop_sequences: stopSequences,
   temperature,
   tools,
+  tool_choice: toolChoice,
 });
+
+interface FormattedBedRockMessage {
+  role: string;
+  content: string | BedRockMessage['rawContent'];
+}
 
 /**
  * Ensures that the messages are in the correct format for the Bedrock API
@@ -429,19 +452,32 @@ const formatBedrockBody = ({
  * @param messages
  */
 const ensureMessageFormat = (
-  messages: Array<{ role: string; content?: string }>,
+  messages: BedRockMessage[],
   systemPrompt?: string
-): { messages: Array<{ role: string; content?: string }>; system?: string } => {
+): {
+  messages: FormattedBedRockMessage[];
+  system?: string;
+} => {
   let system = systemPrompt ? systemPrompt : '';
 
-  const newMessages = messages.reduce((acc: Array<{ role: string; content?: string }>, m) => {
-    const lastMessage = acc[acc.length - 1];
+  const newMessages = messages.reduce<FormattedBedRockMessage[]>((acc, m) => {
     if (m.role === 'system') {
       system = `${system.length ? `${system}\n` : ''}${m.content}`;
       return acc;
     }
 
-    if (lastMessage && lastMessage.role === m.role) {
+    const messageRole = () => (['assistant', 'ai'].includes(m.role) ? 'assistant' : 'user');
+
+    if (m.rawContent) {
+      acc.push({
+        role: messageRole(),
+        content: m.rawContent,
+      });
+      return acc;
+    }
+
+    const lastMessage = acc[acc.length - 1];
+    if (lastMessage && lastMessage.role === m.role && typeof lastMessage.content === 'string') {
       // Bedrock only accepts assistant and user roles.
       // If 2 user or 2 assistant messages are sent in a row, combine the messages into a single message
       return [
@@ -451,11 +487,9 @@ const ensureMessageFormat = (
     }
 
     // force role outside of system to ensure it is either assistant or user
-    return [
-      ...acc,
-      { content: m.content, role: ['assistant', 'ai'].includes(m.role) ? 'assistant' : 'user' },
-    ];
+    return [...acc, { content: m.content, role: messageRole() }];
   }, []);
+
   return system.length ? { system, messages: newMessages } : { messages: newMessages };
 };
 

x-pack/plugins/actions/server/integration_tests/__snapshots__/connector_types.test.ts.snap
@@ -273,8 +273,15 @@ Object {
           "keys": Object {
             "content": Object {
               "flags": Object {
+                "default": [Function],
                 "error": [Function],
+                "presence": "optional",
               },
+              "metas": Array [
+                Object {
+                  "x-oas-optional": true,
+                },
+              ],
               "rules": Array [
                 Object {
                   "args": Object {
@@ -284,6 +291,33 @@ Object {
                 },
               ],
               "type": "string",
+            },
+            "rawContent": Object {
+              "flags": Object {
+                "default": [Function],
+                "error": [Function],
+                "presence": "optional",
+              },
+              "items": Array [
+                Object {
+                  "flags": Object {
+                    "error": [Function],
+                    "presence": "optional",
+                  },
+                  "metas": Array [
+                    Object {
+                      "x-oas-any-type": true,
+                    },
+                  ],
+                  "type": "any",
+                },
+              ],
+              "metas": Array [
+                Object {
+                  "x-oas-optional": true,
+                },
+              ],
+              "type": "array",
             },
             "role": Object {
               "flags": Object {
@@ -300,6 +334,14 @@ Object {
               "type": "string",
             },
           },
+          "rules": Array [
+            Object {
+              "args": Object {
+                "method": [Function],
+              },
+              "name": "custom",
+            },
+          ],
           "type": "object",
         },
       ],
@@ -418,6 +460,86 @@ Object {
         },
       ],
       "type": "number",
+    },
+    "toolChoice": Object {
+      "flags": Object {
+        "default": [Function],
+        "error": [Function],
+        "presence": "optional",
+      },
+      "keys": Object {
+        "name": Object {
+          "flags": Object {
+            "default": [Function],
+            "error": [Function],
+            "presence": "optional",
+          },
+          "metas": Array [
+            Object {
+              "x-oas-optional": true,
+            },
+          ],
+          "rules": Array [
+            Object {
+              "args": Object {
+                "method": [Function],
+              },
+              "name": "custom",
+            },
+          ],
+          "type": "string",
+        },
+        "type": Object {
+          "flags": Object {
+            "error": [Function],
+          },
+          "matches": Array [
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "auto",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "any",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "tool",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+          ],
+          "type": "alternatives",
+        },
+      },
+      "metas": Array [
+        Object {
+          "x-oas-optional": true,
+        },
+      ],
+      "type": "object",
     },
     "tools": Object {
       "flags": Object {
@@ -556,8 +678,15 @@ Object {
           "keys": Object {
             "content": Object {
               "flags": Object {
+                "default": [Function],
                 "error": [Function],
+                "presence": "optional",
               },
+              "metas": Array [
+                Object {
+                  "x-oas-optional": true,
+                },
+              ],
               "rules": Array [
                 Object {
                   "args": Object {
@@ -567,6 +696,33 @@ Object {
                 },
               ],
               "type": "string",
+            },
+            "rawContent": Object {
+              "flags": Object {
+                "default": [Function],
+                "error": [Function],
+                "presence": "optional",
+              },
+              "items": Array [
+                Object {
+                  "flags": Object {
+                    "error": [Function],
+                    "presence": "optional",
+                  },
+                  "metas": Array [
+                    Object {
+                      "x-oas-any-type": true,
+                    },
+                  ],
+                  "type": "any",
+                },
+              ],
+              "metas": Array [
+                Object {
+                  "x-oas-optional": true,
+                },
+              ],
+              "type": "array",
             },
             "role": Object {
               "flags": Object {
@@ -583,6 +739,14 @@ Object {
               "type": "string",
             },
           },
+          "rules": Array [
+            Object {
+              "args": Object {
+                "method": [Function],
+              },
+              "name": "custom",
+            },
+          ],
           "type": "object",
         },
       ],
@@ -701,6 +865,86 @@ Object {
         },
       ],
       "type": "number",
+    },
+    "toolChoice": Object {
+      "flags": Object {
+        "default": [Function],
+        "error": [Function],
+        "presence": "optional",
+      },
+      "keys": Object {
+        "name": Object {
+          "flags": Object {
+            "default": [Function],
+            "error": [Function],
+            "presence": "optional",
+          },
+          "metas": Array [
+            Object {
+              "x-oas-optional": true,
+            },
+          ],
+          "rules": Array [
+            Object {
+              "args": Object {
+                "method": [Function],
+              },
+              "name": "custom",
+            },
+          ],
+          "type": "string",
+        },
+        "type": Object {
+          "flags": Object {
+            "error": [Function],
+          },
+          "matches": Array [
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "auto",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "any",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "tool",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+          ],
+          "type": "alternatives",
+        },
+      },
+      "metas": Array [
+        Object {
+          "x-oas-optional": true,
+        },
+      ],
+      "type": "object",
     },
     "tools": Object {
       "flags": Object {
@@ -839,14 +1083,51 @@ Object {
           "keys": Object {
             "content": Object {
               "flags": Object {
+                "default": [Function],
                 "error": [Function],
+                "presence": "optional",
               },
               "metas": Array [
                 Object {
-                  "x-oas-any-type": true,
+                  "x-oas-optional": true,
                 },
               ],
-              "type": "any",
+              "rules": Array [
+                Object {
+                  "args": Object {
+                    "method": [Function],
+                  },
+                  "name": "custom",
+                },
+              ],
+              "type": "string",
+            },
+            "rawContent": Object {
+              "flags": Object {
+                "default": [Function],
+                "error": [Function],
+                "presence": "optional",
+              },
+              "items": Array [
+                Object {
+                  "flags": Object {
+                    "error": [Function],
+                    "presence": "optional",
+                  },
+                  "metas": Array [
+                    Object {
+                      "x-oas-any-type": true,
+                    },
+                  ],
+                  "type": "any",
+                },
+              ],
+              "metas": Array [
+                Object {
+                  "x-oas-optional": true,
+                },
+              ],
+              "type": "array",
             },
             "role": Object {
               "flags": Object {
@@ -863,6 +1144,14 @@ Object {
               "type": "string",
             },
           },
+          "rules": Array [
+            Object {
+              "args": Object {
+                "method": [Function],
+              },
+              "name": "custom",
+            },
+          ],
           "type": "object",
         },
       ],
@@ -981,6 +1270,86 @@ Object {
         },
       ],
       "type": "number",
+    },
+    "toolChoice": Object {
+      "flags": Object {
+        "default": [Function],
+        "error": [Function],
+        "presence": "optional",
+      },
+      "keys": Object {
+        "name": Object {
+          "flags": Object {
+            "default": [Function],
+            "error": [Function],
+            "presence": "optional",
+          },
+          "metas": Array [
+            Object {
+              "x-oas-optional": true,
+            },
+          ],
+          "rules": Array [
+            Object {
+              "args": Object {
+                "method": [Function],
+              },
+              "name": "custom",
+            },
+          ],
+          "type": "string",
+        },
+        "type": Object {
+          "flags": Object {
+            "error": [Function],
+          },
+          "matches": Array [
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "auto",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "any",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+            Object {
+              "schema": Object {
+                "allow": Array [
+                  "tool",
+                ],
+                "flags": Object {
+                  "error": [Function],
+                  "only": true,
+                },
+                "type": "any",
+              },
+            },
+          ],
+          "type": "alternatives",
+        },
+      },
+      "metas": Array [
+        Object {
+          "x-oas-optional": true,
+        },
+      ],
+      "type": "object",
     },
     "tools": Object {
       "flags": Object {

x-pack/plugins/inference/server/chat_complete/adapters/bedrock/types.ts
@@ -1,0 +1,98 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+/**
+ * BedRock message as expected by the bedrock connector
+ */
+export interface BedRockMessage {
+  role: 'user' | 'assistant';
+  content?: string;
+  rawContent?: BedRockMessagePart[];
+}
+
+/**
+ * Bedrock message parts
+ */
+export type BedRockMessagePart =
+  | { type: 'text'; text: string }
+  | {
+      type: 'tool_use';
+      id: string;
+      name: string;
+      input: Record<string, unknown>;
+    }
+  | { type: 'tool_result'; tool_use_id: string; content: string };
+
+export type BedrockToolChoice = { type: 'auto' } | { type: 'any' } | { type: 'tool'; name: string };
+
+interface CompletionChunkBase {
+  type: string;
+}
+
+export interface MessageStartChunk extends CompletionChunkBase {
+  type: 'message_start';
+  message: unknown;
+}
+
+export interface ContentBlockStartChunk extends CompletionChunkBase {
+  type: 'content_block_start';
+  index: number;
+  content_block:
+    | {
+        type: 'text';
+        text: string;
+      }
+    | { type: 'tool_use'; id: string; name: string; input: string };
+}
+
+export interface ContentBlockDeltaChunk extends CompletionChunkBase {
+  type: 'content_block_delta';
+  index: number;
+  delta:
+    | {
+        type: 'text_delta';
+        text: string;
+      }
+    | {
+        type: 'input_json_delta';
+        partial_json: string;
+      };
+}
+
+export interface ContentBlockStopChunk extends CompletionChunkBase {
+  type: 'content_block_stop';
+  index: number;
+}
+
+export interface MessageDeltaChunk extends CompletionChunkBase {
+  type: 'message_delta';
+  delta: {
+    stop_reason: string;
+    stop_sequence: null | string;
+    usage: {
+      output_tokens: number;
+    };
+  };
+}
+
+export interface MessageStopChunk extends CompletionChunkBase {
+  type: 'message_stop';
+  'amazon-bedrock-invocationMetrics': {
+    inputTokenCount: number;
+    outputTokenCount: number;
+    invocationLatency: number;
+    firstByteLatency: number;
+  };
+}
+
+export type CompletionChunk =
+  | MessageStartChunk
+  | ContentBlockStartChunk
+  | ContentBlockDeltaChunk
+  | ContentBlockStopChunk
+  | MessageDeltaChunk
+  | MessageStopChunk;

x-pack/plugins/inference/server/chat_complete/adapters/bedrock/index.ts
@@ -1,0 +1,8 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export { bedrockClaudeAdapter } from './bedrock_claude_adapter';

commit 84c6815a1152049c74db00da48522d0624603856

Fix `InvokeAIRaw` bedrock subaction's schema (#192015)
## Summary

Fix regression on `InvokeAIRaw` bedrock subaction introduced by
https://github.com/elastic/kibana/pull/191434

x-pack/plugins/stack_connectors/common/bedrock/schema.ts
@@ -78,7 +78,12 @@ export const InvokeAIActionResponseSchema = schema.object({
 });
 
 export const InvokeAIRawActionParamsSchema = schema.object({
-  messages: schema.arrayOf(BedrockMessageSchema),
+  messages: schema.arrayOf(
+    schema.object({
+      role: schema.string(),
+      content: schema.any(),
+    })
+  ),
   model: schema.maybe(schema.string()),
   temperature: schema.maybe(schema.number()),
   stopSequences: schema.maybe(schema.arrayOf(schema.string())),

x-pack/plugins/stack_connectors/common/bedrock/types.ts
@@ -35,5 +35,5 @@ export type RunActionResponse = TypeOf<typeof RunActionResponseSchema>;
 export type StreamingResponse = TypeOf<typeof StreamingResponseSchema>;
 export type DashboardActionParams = TypeOf<typeof DashboardActionParamsSchema>;
 export type DashboardActionResponse = TypeOf<typeof DashboardActionResponseSchema>;
-export type BedRockMessage = TypeOf<typeof BedrockMessageSchema>;
+export type BedrockMessage = TypeOf<typeof BedrockMessageSchema>;
 export type BedrockToolChoice = TypeOf<typeof BedrockToolChoiceSchema>;

x-pack/plugins/stack_connectors/server/connector_types/bedrock/bedrock.ts
@@ -32,7 +32,7 @@ import type {
   InvokeAIRawActionParams,
   InvokeAIRawActionResponse,
   RunApiLatestResponse,
-  BedRockMessage,
+  BedrockMessage,
   BedrockToolChoice,
 } from '../../../common/bedrock/types';
 import {
@@ -422,7 +422,7 @@ const formatBedrockBody = ({
   tools,
   toolChoice,
 }: {
-  messages: BedRockMessage[];
+  messages: BedrockMessage[];
   stopSequences?: string[];
   temperature?: number;
   maxTokens?: number;
@@ -440,9 +440,9 @@ const formatBedrockBody = ({
   tool_choice: toolChoice,
 });
 
-interface FormattedBedRockMessage {
+interface FormattedBedrockMessage {
   role: string;
-  content: string | BedRockMessage['rawContent'];
+  content: string | BedrockMessage['rawContent'];
 }
 
 /**
@@ -452,15 +452,15 @@ interface FormattedBedRockMessage {
  * @param messages
  */
 const ensureMessageFormat = (
-  messages: BedRockMessage[],
+  messages: BedrockMessage[],
   systemPrompt?: string
 ): {
-  messages: FormattedBedRockMessage[];
+  messages: FormattedBedrockMessage[];
   system?: string;
 } => {
   let system = systemPrompt ? systemPrompt : '';
 
-  const newMessages = messages.reduce<FormattedBedRockMessage[]>((acc, m) => {
+  const newMessages = messages.reduce<FormattedBedrockMessage[]>((acc, m) => {
     if (m.role === 'system') {
       system = `${system.length ? `${system}\n` : ''}${m.content}`;
       return acc;

x-pack/plugins/actions/server/integration_tests/__snapshots__/connector_types.test.ts.snap
@@ -1083,51 +1083,14 @@ Object {
           "keys": Object {
             "content": Object {
               "flags": Object {
-                "default": [Function],
                 "error": [Function],
-                "presence": "optional",
               },
               "metas": Array [
                 Object {
-                  "x-oas-optional": true,
+                  "x-oas-any-type": true,
                 },
               ],
-              "rules": Array [
-                Object {
-                  "args": Object {
-                    "method": [Function],
-                  },
-                  "name": "custom",
-                },
-              ],
-              "type": "string",
-            },
-            "rawContent": Object {
-              "flags": Object {
-                "default": [Function],
-                "error": [Function],
-                "presence": "optional",
-              },
-              "items": Array [
-                Object {
-                  "flags": Object {
-                    "error": [Function],
-                    "presence": "optional",
-                  },
-                  "metas": Array [
-                    Object {
-                      "x-oas-any-type": true,
-                    },
-                  ],
-                  "type": "any",
-                },
-              ],
-              "metas": Array [
-                Object {
-                  "x-oas-optional": true,
-                },
-              ],
-              "type": "array",
+              "type": "any",
             },
             "role": Object {
               "flags": Object {
@@ -1144,14 +1107,6 @@ Object {
               "type": "string",
             },
           },
-          "rules": Array [
-            Object {
-              "args": Object {
-                "method": [Function],
-              },
-              "name": "custom",
-            },
-          ],
           "type": "object",
         },
       ],