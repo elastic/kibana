9d77f9fe6de11d1aa289166f3daa4df93a8b65c3
Mike Cote <michel.cote@elastic.co>
2024-09-03 05:24:01 -0400

    Revert and unskip

--- x-pack/plugins/task_manager/server/integration_tests/task_manager_capacity_based_claiming.test.ts
+++ x-pack/plugins/task_manager/server/integration_tests/task_manager_capacity_based_claiming.test.ts
@@ -94,196 +94,89 @@ jest.mock('../queries/task_claiming', () => {
 
 const taskManagerStartSpy = jest.spyOn(TaskManagerPlugin.prototype, 'start');
 
-// Failing: See https://github.com/elastic/kibana/issues/191117
-for (let i = 0; i < 100; i++) {
-  describe(`capacity based claiming ${i}`, () => {
-    const taskIdsToRemove: string[] = [];
-    let esServer: TestElasticsearchUtils;
-    let kibanaServer: TestKibanaUtils;
-    let taskManagerPlugin: TaskManagerStartContract;
-    let createMonitoringStatsOpts: CreateMonitoringStatsOpts;
+describe('capacity based claiming', () => {
+  const taskIdsToRemove: string[] = [];
+  let esServer: TestElasticsearchUtils;
+  let kibanaServer: TestKibanaUtils;
+  let taskManagerPlugin: TaskManagerStartContract;
+  let createMonitoringStatsOpts: CreateMonitoringStatsOpts;
 
-    beforeAll(async () => {
-      const setupResult = await setupTestServers({
-        xpack: {
-          task_manager: {
-            claim_strategy: `mget`,
-            capacity: 10,
-            poll_interval: POLLING_INTERVAL,
-            unsafe: {
-              exclude_task_types: ['[A-Za-z]*'],
-            },
+  beforeAll(async () => {
+    const setupResult = await setupTestServers({
+      xpack: {
+        task_manager: {
+          claim_strategy: `mget`,
+          capacity: 10,
+          poll_interval: POLLING_INTERVAL,
+          unsafe: {
+            exclude_task_types: ['[A-Za-z]*'],
           },
         },
+      },
+    });
+    esServer = setupResult.esServer;
+    kibanaServer = setupResult.kibanaServer;
+
+    expect(taskManagerStartSpy).toHaveBeenCalledTimes(1);
+    taskManagerPlugin = taskManagerStartSpy.mock.results[0].value;
+
+    expect(TaskPollingLifecycleMock).toHaveBeenCalledTimes(1);
+
+    expect(createMonitoringStatsMock).toHaveBeenCalledTimes(1);
+    createMonitoringStatsOpts = createMonitoringStatsMock.mock.calls[0][0];
+  });
+
+  afterAll(async () => {
+    if (kibanaServer) {
+      await kibanaServer.stop();
+    }
+    if (esServer) {
+      await esServer.stop();
+    }
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  afterEach(async () => {
+    while (taskIdsToRemove.length > 0) {
+      const id = taskIdsToRemove.pop();
+      await taskManagerPlugin.removeIfExists(id!);
+    }
+  });
+
+  it('should claim tasks to full capacity', async () => {
+    const backgroundTaskLoads: number[] = [];
+    createMonitoringStatsOpts.taskPollingLifecycle?.events
+      .pipe(
+        filter(isTaskManagerWorkerUtilizationStatEvent),
+        map<TaskLifecycleEvent, number>((taskEvent: TaskLifecycleEvent) => {
+          return (taskEvent.event as unknown as Ok<number>).value;
+        })
+      )
+      .subscribe((load: number) => {
+        backgroundTaskLoads.push(load);
       });
-      esServer = setupResult.esServer;
-      kibanaServer = setupResult.kibanaServer;
-
-      expect(taskManagerStartSpy).toHaveBeenCalledTimes(1);
-      taskManagerPlugin = taskManagerStartSpy.mock.results[0].value;
-
-      expect(TaskPollingLifecycleMock).toHaveBeenCalledTimes(1);
-
-      expect(createMonitoringStatsMock).toHaveBeenCalledTimes(1);
-      createMonitoringStatsOpts = createMonitoringStatsMock.mock.calls[0][0];
+    const taskRunAtDates: Date[] = [];
+    mockTaskTypeNormalCostRunFn.mockImplementation(() => {
+      taskRunAtDates.push(new Date());
+      return { state: { foo: 'test' } };
     });
 
-    afterAll(async () => {
-      if (kibanaServer) {
-        await kibanaServer.stop();
-      }
-      if (esServer) {
-        await esServer.stop();
-      }
-    });
+    // inject 10 normal cost tasks with the same runAt value
+    const ids: string[] = [];
+    times(10, () => ids.push(uuidV4()));
 
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
-
-    afterEach(async () => {
-      while (taskIdsToRemove.length > 0) {
-        const id = taskIdsToRemove.pop();
-        await taskManagerPlugin.removeIfExists(id!);
-      }
-    });
-
-    it('should claim tasks to full capacity', async () => {
-      const backgroundTaskLoads: number[] = [];
-      createMonitoringStatsOpts.taskPollingLifecycle?.events
-        .pipe(
-          filter(isTaskManagerWorkerUtilizationStatEvent),
-          map<TaskLifecycleEvent, number>((taskEvent: TaskLifecycleEvent) => {
-            return (taskEvent.event as unknown as Ok<number>).value;
-          })
-        )
-        .subscribe((load: number) => {
-          backgroundTaskLoads.push(load);
-        });
-      const taskRunAtDates: Date[] = [];
-      mockTaskTypeNormalCostRunFn.mockImplementation(() => {
-        taskRunAtDates.push(new Date());
-        return { state: { foo: 'test' } };
-      });
-
-      // inject 10 normal cost tasks with the same runAt value
-      const ids: string[] = [];
-      times(10, () => ids.push(uuidV4()));
-
-      const runAt = new Date();
-      for (const id of ids) {
-        await injectTask(kibanaServer.coreStart.elasticsearch.client.asInternalUser, {
-          id,
-          taskType: '_normalCostType',
-          params: {},
-          state: { foo: 'test' },
-          stateVersion: 1,
-          runAt,
-          enabled: true,
-          scheduledAt: new Date(),
-          attempts: 0,
-          status: TaskStatus.Idle,
-          startedAt: null,
-          retryAt: null,
-          ownerId: null,
-        });
-        taskIdsToRemove.push(id);
-      }
-
-      await retry(async () => {
-        expect(mockTaskTypeNormalCostRunFn).toHaveBeenCalledTimes(10);
-      });
-
-      expect(taskRunAtDates.length).toBe(10);
-
-      // run at dates should be within a few seconds of each other
-      const firstRunAt = taskRunAtDates[0].getTime();
-      const lastRunAt = taskRunAtDates[taskRunAtDates.length - 1].getTime();
-
-      expect(lastRunAt - firstRunAt).toBeLessThanOrEqual(1000);
-
-      // background task load should be 0 or 100 since we're only running these tasks
-      for (const load of backgroundTaskLoads) {
-        expect(load === 0 || load === 100).toBe(true);
-      }
-    });
-
-    it('should claim tasks until the next task will exceed capacity', async () => {
-      const backgroundTaskLoads: number[] = [];
-      createMonitoringStatsOpts.taskPollingLifecycle?.events
-        .pipe(
-          filter(isTaskManagerWorkerUtilizationStatEvent),
-          map<TaskLifecycleEvent, number>((taskEvent: TaskLifecycleEvent) => {
-            return (taskEvent.event as unknown as Ok<number>).value;
-          })
-        )
-        .subscribe((load: number) => {
-          backgroundTaskLoads.push(load);
-        });
-      const now = new Date();
-      const taskRunAtDates: Array<{ runAt: Date; type: string }> = [];
-      mockTaskTypeNormalCostRunFn.mockImplementation(() => {
-        taskRunAtDates.push({ type: 'normal', runAt: new Date() });
-        return { state: { foo: 'test' } };
-      });
-      mockTaskTypeXLCostRunFn.mockImplementation(() => {
-        taskRunAtDates.push({ type: 'xl', runAt: new Date() });
-        return { state: { foo: 'test' } };
-      });
-
-      // inject 6 normal cost tasks for total cost of 12
-      const ids: string[] = [];
-      times(6, () => ids.push(uuidV4()));
-      const runAt1 = new Date(now.valueOf() - 5);
-      for (const id of ids) {
-        await injectTask(kibanaServer.coreStart.elasticsearch.client.asInternalUser, {
-          id,
-          taskType: '_normalCostType',
-          params: {},
-          state: { foo: 'test' },
-          stateVersion: 1,
-          runAt: runAt1,
-          enabled: true,
-          scheduledAt: new Date(),
-          attempts: 0,
-          status: TaskStatus.Idle,
-          startedAt: null,
-          retryAt: null,
-          ownerId: null,
-        });
-        taskIdsToRemove.push(id);
-      }
-
-      // inject 1 XL cost task that will put us over the max cost capacity of 20
-      const xlid = uuidV4();
-      const runAt2 = now;
+    const runAt = new Date();
+    for (const id of ids) {
       await injectTask(kibanaServer.coreStart.elasticsearch.client.asInternalUser, {
-        id: xlid,
-        taskType: '_xlCostType',
-        params: {},
-        state: { foo: 'test' },
-        stateVersion: 1,
-        runAt: runAt2,
-        enabled: true,
-        scheduledAt: new Date(),
-        attempts: 0,
-        status: TaskStatus.Idle,
-        startedAt: null,
-        retryAt: null,
-        ownerId: null,
-      });
-      taskIdsToRemove.push(xlid);
-
-      // inject one more normal cost task
-      const runAt3 = new Date(now.valueOf() + 5);
-      const lastid = uuidV4();
-      await injectTask(kibanaServer.coreStart.elasticsearch.client.asInternalUser, {
-        id: lastid,
+        id,
         taskType: '_normalCostType',
         params: {},
         state: { foo: 'test' },
         stateVersion: 1,
-        runAt: runAt3,
+        runAt,
         enabled: true,
         scheduledAt: new Date(),
         attempts: 0,
@@ -292,43 +185,143 @@ for (let i = 0; i < 100; i++) {
         retryAt: null,
         ownerId: null,
       });
-      taskIdsToRemove.push(lastid);
+      taskIdsToRemove.push(id);
+    }
 
-      // retry until all tasks have been run
-      await retry(async () => {
-        expect(mockTaskTypeNormalCostRunFn).toHaveBeenCalledTimes(7);
-        expect(mockTaskTypeXLCostRunFn).toHaveBeenCalledTimes(1);
-      });
-
-      expect(taskRunAtDates.length).toBe(8);
-
-      const firstRunAt = taskRunAtDates[0].runAt.getTime();
-
-      // the first 6 tasks should have been run at the same time (adding some fudge factor)
-      // and they should all be normal cost tasks
-      for (let j = 0; j < 6; j++) {
-        expect(taskRunAtDates[j].type).toBe('normal');
-        expect(taskRunAtDates[j].runAt.getTime() - firstRunAt).toBeLessThanOrEqual(500);
-      }
-
-      // the next task should be XL cost task and be run after one polling interval has passed (with some fudge factor)
-      expect(taskRunAtDates[6].type).toBe('xl');
-      expect(taskRunAtDates[6].runAt.getTime() - firstRunAt).toBeGreaterThan(
-        POLLING_INTERVAL - 500
-      );
-
-      // last task should be normal cost and be run after one polling interval has passed
-      expect(taskRunAtDates[7].type).toBe('normal');
-      expect(taskRunAtDates[7].runAt.getTime() - firstRunAt).toBeGreaterThan(
-        POLLING_INTERVAL - 500
-      );
-
-      // background task load should be 0 or 60 or 100 since we're only running these tasks
-      // should be 100 during the claim cycle where we claimed 6 normal tasks but left the large capacity task in the queue
-      // should be 60 during the next claim cycle where we claimed the large capacity task and the normal capacity: 10 + 2 / 20 = 60%
-      for (const load of backgroundTaskLoads) {
-        expect(load === 0 || load === 60 || load === 100).toBe(true);
-      }
+    await retry(async () => {
+      expect(mockTaskTypeNormalCostRunFn).toHaveBeenCalledTimes(10);
     });
+
+    expect(taskRunAtDates.length).toBe(10);
+
+    // run at dates should be within a few seconds of each other
+    const firstRunAt = taskRunAtDates[0].getTime();
+    const lastRunAt = taskRunAtDates[taskRunAtDates.length - 1].getTime();
+
+    expect(lastRunAt - firstRunAt).toBeLessThanOrEqual(1000);
+
+    // background task load should be 0 or 100 since we're only running these tasks
+    for (const load of backgroundTaskLoads) {
+      expect(load === 0 || load === 100).toBe(true);
+    }
   });
-}
+
+  it('should claim tasks until the next task will exceed capacity', async () => {
+    const backgroundTaskLoads: number[] = [];
+    createMonitoringStatsOpts.taskPollingLifecycle?.events
+      .pipe(
+        filter(isTaskManagerWorkerUtilizationStatEvent),
+        map<TaskLifecycleEvent, number>((taskEvent: TaskLifecycleEvent) => {
+          return (taskEvent.event as unknown as Ok<number>).value;
+        })
+      )
+      .subscribe((load: number) => {
+        backgroundTaskLoads.push(load);
+      });
+    const now = new Date();
+    const taskRunAtDates: Array<{ runAt: Date; type: string }> = [];
+    mockTaskTypeNormalCostRunFn.mockImplementation(() => {
+      taskRunAtDates.push({ type: 'normal', runAt: new Date() });
+      return { state: { foo: 'test' } };
+    });
+    mockTaskTypeXLCostRunFn.mockImplementation(() => {
+      taskRunAtDates.push({ type: 'xl', runAt: new Date() });
+      return { state: { foo: 'test' } };
+    });
+
+    // inject 6 normal cost tasks for total cost of 12
+    const ids: string[] = [];
+    times(6, () => ids.push(uuidV4()));
+    const runAt1 = new Date(now.valueOf() - 5);
+    for (const id of ids) {
+      await injectTask(kibanaServer.coreStart.elasticsearch.client.asInternalUser, {
+        id,
+        taskType: '_normalCostType',
+        params: {},
+        state: { foo: 'test' },
+        stateVersion: 1,
+        runAt: runAt1,
+        enabled: true,
+        scheduledAt: new Date(),
+        attempts: 0,
+        status: TaskStatus.Idle,
+        startedAt: null,
+        retryAt: null,
+        ownerId: null,
+      });
+      taskIdsToRemove.push(id);
+    }
+
+    // inject 1 XL cost task that will put us over the max cost capacity of 20
+    const xlid = uuidV4();
+    const runAt2 = now;
+    await injectTask(kibanaServer.coreStart.elasticsearch.client.asInternalUser, {
+      id: xlid,
+      taskType: '_xlCostType',
+      params: {},
+      state: { foo: 'test' },
+      stateVersion: 1,
+      runAt: runAt2,
+      enabled: true,
+      scheduledAt: new Date(),
+      attempts: 0,
+      status: TaskStatus.Idle,
+      startedAt: null,
+      retryAt: null,
+      ownerId: null,
+    });
+    taskIdsToRemove.push(xlid);
+
+    // inject one more normal cost task
+    const runAt3 = new Date(now.valueOf() + 5);
+    const lastid = uuidV4();
+    await injectTask(kibanaServer.coreStart.elasticsearch.client.asInternalUser, {
+      id: lastid,
+      taskType: '_normalCostType',
+      params: {},
+      state: { foo: 'test' },
+      stateVersion: 1,
+      runAt: runAt3,
+      enabled: true,
+      scheduledAt: new Date(),
+      attempts: 0,
+      status: TaskStatus.Idle,
+      startedAt: null,
+      retryAt: null,
+      ownerId: null,
+    });
+    taskIdsToRemove.push(lastid);
+
+    // retry until all tasks have been run
+    await retry(async () => {
+      expect(mockTaskTypeNormalCostRunFn).toHaveBeenCalledTimes(7);
+      expect(mockTaskTypeXLCostRunFn).toHaveBeenCalledTimes(1);
+    });
+
+    expect(taskRunAtDates.length).toBe(8);
+
+    const firstRunAt = taskRunAtDates[0].runAt.getTime();
+
+    // the first 6 tasks should have been run at the same time (adding some fudge factor)
+    // and they should all be normal cost tasks
+    for (let i = 0; i < 6; i++) {
+      expect(taskRunAtDates[i].type).toBe('normal');
+      expect(taskRunAtDates[i].runAt.getTime() - firstRunAt).toBeLessThanOrEqual(500);
+    }
+
+    // the next task should be XL cost task and be run after one polling interval has passed (with some fudge factor)
+    expect(taskRunAtDates[6].type).toBe('xl');
+    expect(taskRunAtDates[6].runAt.getTime() - firstRunAt).toBeGreaterThan(POLLING_INTERVAL - 500);
+
+    // last task should be normal cost and be run after one polling interval has passed
+    expect(taskRunAtDates[7].type).toBe('normal');
+    expect(taskRunAtDates[7].runAt.getTime() - firstRunAt).toBeGreaterThan(POLLING_INTERVAL - 500);
+
+    // background task load should be 0 or 60 or 100 since we're only running these tasks
+    // should be 100 during the claim cycle where we claimed 6 normal tasks but left the large capacity task in the queue
+    // should be 60 during the next claim cycle where we claimed the large capacity task and the normal capacity: 10 + 2 / 20 = 60%
+    for (const load of backgroundTaskLoads) {
+      expect(load === 0 || load === 60 || load === 100).toBe(true);
+    }
+  });
+});
