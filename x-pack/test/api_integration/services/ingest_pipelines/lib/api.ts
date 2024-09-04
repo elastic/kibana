c738f1c03479fd7fb8acfd449af536168578ceea
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-09-04 10:38:46 +0200

    move ingest_pipelines service to test/api_integration/services

--- x-pack/test/api_integration/services/ingest_pipelines/lib/api.ts
+++ x-pack/test/api_integration/services/ingest_pipelines/lib/api.ts
@@ -7,8 +7,7 @@
 
 import { IngestPutPipelineRequest } from '@elastic/elasticsearch/lib/api/types';
 import expect from '@kbn/expect';
-
-import { FtrProviderContext } from '../../../../ftr_provider_context';
+import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export function IngestPipelinesAPIProvider({ getService }: FtrProviderContext) {
   const es = getService('es');
