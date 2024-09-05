a0bf2fa33dd2efb0e412826e2b43788a0972f683
Caue Marcondes <caue.marcondes@elastic.co>
2024-09-05 14:03:14 +0100

    adding href support to highlight component

---
+++ src/plugins/unified_doc_viewer/public/components/doc_viewer_logs_overview/sub_components/service_name_highlight_field.tsx
@@ -0,0 +1,24 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import React from 'react';
+import { EuiLink } from '@elastic/eui';
+import { HighlightField, HighlightFieldProps } from './highlight_field';
+
+export function ServiceNameHighlightField(props: HighlightFieldProps) {
+  // Locator would be defined here
+  const href = `/foo/app/apm/services/${props.value}`;
+  // context
+  // kibana settings is enabled
+  // EEM is enabled
+  return (
+    <HighlightField {...props}>
+      {({ content }) => <EuiLink href={href}>{content}</EuiLink>}
+    </HighlightField>
+  );
+}
