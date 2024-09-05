fabe07d94ed5a7fa90a7e5e14f8806607ddf543a
Shahzad <shahzad31comp@gmail.com>
2024-09-05 10:18:30 +0200

    [Dashboard] Fix add panels order of items !! (#192082)
    ## Summary
    
    Fixes part of https://github.com/elastic/kibana/issues/190853 !!
    
    Combines synthetics and SLO embeddables under common observability group
    !!
    
    ### After
    <img width="1728" alt="image"
    src="https://github.com/user-attachments/assets/5cc629c5-de10-4304-ad6e-da114a7927b6">
    
    ### Before
    <img width="1716" alt="image"
    src="https://github.com/user-attachments/assets/9ed35068-46fd-4e50-80f7-de67529f8c79">

--- x-pack/plugins/observability_solution/observability_shared/common/embeddable_grouping.ts
+++ x-pack/plugins/observability_solution/observability_shared/common/embeddable_grouping.ts
@@ -4,17 +4,19 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import { i18n } from '@kbn/i18n';
 
-export const COMMON_SLO_GROUPING = [
+export const COMMON_OBSERVABILITY_GROUPING = [
   {
-    id: 'slos',
+    id: 'observability',
     getDisplayName: () =>
-      i18n.translate('xpack.slo.common.constants.grouping.legacy', {
+      i18n.translate('xpack.observabilityShared.common.constants.grouping', {
         defaultMessage: 'Observability',
       }),
     getIconType: () => {
-      return 'visGauge';
+      return 'online';
     },
+    order: -1,
   },
 ];
