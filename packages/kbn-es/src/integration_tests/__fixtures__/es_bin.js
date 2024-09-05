4662e976d75684fb6c00fd3015afc5b3f5eaa9b5
Luke Elmers <luke.elmers@elastic.co>
2024-09-05 14:31:12 -0600

    Update headers with eslint --fix.

--- packages/kbn-es/src/integration_tests/__fixtures__/es_bin.js
+++ packages/kbn-es/src/integration_tests/__fixtures__/es_bin.js
@@ -1,13 +1,5 @@
 #!/usr/bin/env node
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
- */
-
 const fs = require('fs');
 const { format: formatUrl } = require('url');
 const { exitCode, start, ssl } = JSON.parse(process.argv[2]);
