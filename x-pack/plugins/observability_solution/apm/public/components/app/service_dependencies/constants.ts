2a3358b105ed3527f0ce580eb35433ad3cc50345
iblancof <irene.blanco@elastic.co>
2024-09-05 12:21:18 +0200

    Integrate ServiceTabEmptyState in ServiceDependencies

---
+++ x-pack/plugins/observability_solution/apm/public/components/app/service_dependencies/constants.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { i18n } from '@kbn/i18n';
+import dependenciesTabImgSrc from '../../../assets/service_tab_empty_state/service_tab_empty_state_dependencies.png';
+
+export const logsOnlyEmptyStateContent: { title: string; content: string; imgSrc: string } = {
+  title: i18n.translate('xpack.apm.serviceTabContent.serviceTabEmptyState.dependenciesTitle', {
+    defaultMessage: 'Understand the dependencies for your service',
+  }),
+  content: i18n.translate('xpack.apm.serviceTabContent.serviceTabEmptyState.dependenciesContent', {
+    defaultMessage:
+      'See your services dependencies on both internal and third-party services by instrumenting with APM.',
+  }),
+  imgSrc: dependenciesTabImgSrc,
+};
--- x-pack/plugins/observability_solution/apm/public/components/app/service_dependencies/index.tsx
+++ x-pack/plugins/observability_solution/apm/public/components/app/service_dependencies/index.tsx
@@ -9,30 +9,45 @@ import { EuiTitle } from '@elastic/eui';
 import { EuiPanel } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
+import { SignalTypes } from '../../../../common/entities/types';
+import { isLogsOnlySignal } from '../../../utils/get_signal_type';
 import { ChartPointerEventContextProvider } from '../../../context/chart_pointer_event/chart_pointer_event_context';
 import { ServiceOverviewDependenciesTable } from '../service_overview/service_overview_dependencies_table';
 import { ServiceDependenciesBreakdownChart } from './service_dependencies_breakdown_chart';
-import { ServiceTabContent } from '../service_tab_content';
+import { useApmServiceContext } from '../../../context/apm_service/use_apm_service_context';
+import { ServiceTabEmptyState } from '../service_tab_empty_state';
+import { logsOnlyEmptyStateContent } from './constants';
 
 export function ServiceDependencies() {
+  const { serviceEntitySummary } = useApmServiceContext();
+
+  const hasLogsOnlySignal =
+    serviceEntitySummary?.signalTypes &&
+    isLogsOnlySignal(serviceEntitySummary.signalTypes as SignalTypes[]);
+
   return (
     <>
-      <ServiceTabContent tabName="dependencies">
-        <ChartPointerEventContextProvider>
-          <EuiPanel hasBorder={true}>
-            <EuiTitle size="xs">
-              <h2>
-                {i18n.translate('xpack.apm.serviceDependencies.breakdownChartTitle', {
-                  defaultMessage: 'Time spent by dependency',
-                })}
-              </h2>
-            </EuiTitle>
-            <ServiceDependenciesBreakdownChart height={200} />
-          </EuiPanel>
-        </ChartPointerEventContextProvider>
-        <EuiSpacer size="l" />
-        <ServiceOverviewDependenciesTable />
-      </ServiceTabContent>
+      {hasLogsOnlySignal && (
+        <ServiceTabEmptyState
+          title={logsOnlyEmptyStateContent.title}
+          content={logsOnlyEmptyStateContent.content}
+          imgSrc={logsOnlyEmptyStateContent.imgSrc}
+        />
+      )}
+      <ChartPointerEventContextProvider>
+        <EuiPanel hasBorder={true}>
+          <EuiTitle size="xs">
+            <h2>
+              {i18n.translate('xpack.apm.serviceDependencies.breakdownChartTitle', {
+                defaultMessage: 'Time spent by dependency',
+              })}
+            </h2>
+          </EuiTitle>
+          <ServiceDependenciesBreakdownChart height={200} />
+        </EuiPanel>
+      </ChartPointerEventContextProvider>
+      <EuiSpacer size="l" />
+      <ServiceOverviewDependenciesTable />
     </>
   );
 }
