22b537067a7c16c88f3a9bbdafc69f76aa1f5358
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-09-05 14:36:25 +0200

    replace here be dragons with actual embedded content

--- x-pack/plugins/aiops/public/components/log_rate_analysis/log_rate_analysis_for_embeddable/log_rate_analysis_for_embeddable.tsx
+++ x-pack/plugins/aiops/public/components/log_rate_analysis/log_rate_analysis_for_embeddable/log_rate_analysis_for_embeddable.tsx
@@ -5,23 +5,68 @@
  * 2.0.
  */
 import type { FC } from 'react';
-import React from 'react';
+import React, { useMemo } from 'react';
+import { pick } from 'lodash';
+import { createBrowserHistory } from 'history';
 
+import datemath from '@elastic/datemath';
+
+import { Router } from '@kbn/shared-ux-router';
 import type { EmbeddableLogRateAnalysisInput } from '@kbn/aiops-log-rate-analysis/embeddable';
 
-interface LogRateAnalysisProps {
-  dummy: string;
-}
+import { LogRateAnalysisContent } from '../../../shared_lazy_components';
+import type { LogRateAnalysisProps } from '../../../shared_components/log_rate_analysis';
+import { useAiopsAppContext } from '../../../hooks/use_aiops_app_context';
 
 export type LogRateAnalysisEmbeddableProps = Readonly<
   EmbeddableLogRateAnalysisInput & LogRateAnalysisProps
 >;
 
-const BAR_TARGET = 20;
+export const LogRateAnalysisEmbeddable: FC<LogRateAnalysisEmbeddableProps> = ({
+  dataView,
+  timeRange,
+}) => {
+  const services = useAiopsAppContext();
 
-export const LogCategorizationEmbeddable: FC<LogRateAnalysisEmbeddableProps> = ({ dummy }) => {
-  return <>here be dragons: {dummy}</>;
+  const timeRangeParsed = useMemo(() => {
+    if (timeRange) {
+      const min = datemath.parse(timeRange.from);
+      const max = datemath.parse(timeRange.to);
+      if (min && max) {
+        return { min, max };
+      }
+    }
+  }, [timeRange]);
+
+  const history = createBrowserHistory();
+
+  return (
+    <Router history={history}>
+      <LogRateAnalysisContent
+        embeddingOrigin="dashboard"
+        dataView={dataView}
+        timeRange={timeRangeParsed}
+        appDependencies={pick(services, [
+          'analytics',
+          'application',
+          'data',
+          'executionContext',
+          'charts',
+          'fieldFormats',
+          'http',
+          'notifications',
+          'share',
+          'storage',
+          'uiSettings',
+          'unifiedSearch',
+          'theme',
+          'lens',
+          'i18n',
+        ])}
+      />
+    </Router>
+  );
 };
 
 // eslint-disable-next-line import/no-default-export
-export default LogCategorizationEmbeddable;
+export default LogRateAnalysisEmbeddable;
