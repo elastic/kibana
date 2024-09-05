5f68ac3e6bd759be4b777c308f3566a9eb05670c
Ilya Nikokoshev <ilya.nikokoshev@elastic.co>
2024-09-05 16:14:26 +0300

    Update the docs

--- x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/data_stream_step/utils.tsx
+++ x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/data_stream_step/utils.tsx
@@ -10,19 +10,32 @@ import seedrandom from 'seedrandom';
 const DEFAULT_PARTIAL_SHUFFLE_SEED = 'seed';
 
 /**
- * Partially shuffles an array using the Fisher-Yates algorithm.
+ * Partially shuffles an array using the modified Fisher-Yates algorithm.
  *
- * The array is shuffled in place, so that:
+ * The array is partially shuffled in place, meaning that:
  *   - the first `start` elements are kept in place;
- *   - the elements in the slice from start to end represent the random sample;.
- *   - the order of elements after end can be arbitrary (but the same over invocations).
+ *   - the slice from `start` to `end` is filled with the random sample from remaining elements.
+ * We do not make any guarantees regarding the order of elements after `end`.
  *
- * The result is reproducible for the given random seed.
+ * Reproducibility:
+ *   - the result is deterministic for the given random seed.
+ *
+ * Performance:
+ *   - we perform exactly `end-start` operations, each including:
+ *     - a random number generation; and
+ *     - possibly an array element swap; also
+ *   - we use constant extra memory.
+ *
+ * Implementation note:
+ *   - A naive implementation would be to shuffle the whole array starting from `start` using
+ *     the Fisher-Yates algorithm. We modify it to stop once we reach the end so the result on
+ *     the slice from `start` to `end` is indistinguishable (samples from the same distribution)
+ *     from the naive one, while still being more efficient.
  *
  * Examples:
  *   - shuffle the whole array: partialShuffleArray(arr)
  *   - shuffle the first 5 elements: partialShuffleArray(arr, 0, 5)
- *   - keep the first element, shuffle the rest: partialShuffleArray(arr,1)
+ *   - keep the first element, shuffle the rest: partialShuffleArray(arr, 1)
  *   - shuffle the last 5 elements: partialShuffleArray(arr, arr.length - 5)
  *
  * @param arr - The array to be partially shuffled.
