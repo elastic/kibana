e7f159ebdd9a1474389454fe741b75a178768d4f
Caue Marcondes <caue.marcondes@elastic.co>
2024-09-06 16:17:50 +0100

    adding image

--- x-pack/plugins/observability_solution/apm/public/components/app/entities/entity_link/index.tsx
+++ x-pack/plugins/observability_solution/apm/public/components/app/entities/entity_link/index.tsx
@@ -5,20 +5,23 @@
  * 2.0.
  */
 
-import { EuiButtonEmpty, EuiEmptyPrompt, EuiLink, EuiLoadingSpinner } from '@elastic/eui';
+import { EuiButtonEmpty, EuiEmptyPrompt, EuiLink, EuiLoadingSpinner, EuiImage } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 import React from 'react';
 import { Redirect } from 'react-router-dom';
+import { dashboardsDark, dashboardsLight } from '@kbn/shared-svg';
 import { ENVIRONMENT_ALL_VALUE } from '../../../../../common/environment_filter_values';
 import { useServiceEntitySummaryFetcher } from '../../../../context/apm_service/use_service_entity_summary_fetcher';
 import { useEntityManagerEnablementContext } from '../../../../context/entity_manager_context/use_entity_manager_enablement_context';
 import { useApmParams } from '../../../../hooks/use_apm_params';
 import { isPending } from '../../../../hooks/use_fetcher';
 import { useApmRouter } from '../../../../hooks/use_apm_router';
+import { useTheme } from '../../../../hooks/use_theme';
 
 export function EntityLink() {
   const router = useApmRouter({ prependBasePath: false });
+  const theme = useTheme();
   const {
     path: { serviceName },
   } = useApmParams('/link-to/entity/{serviceName}');
@@ -41,7 +44,7 @@ export function EntityLink() {
     // When EEM is disabled we'll show the APM overview page.
     isEntityCentricExperienceViewEnabled === false ||
     // When EEM is enabled and the Service has APM and/or Logs data
-    (serviceEntitySummary?.signalTypes && serviceEntitySummary.signalTypes.length > 0)
+    (serviceEntitySummary?.dataStreamTypes && serviceEntitySummary.dataStreamTypes.length > 0)
   ) {
     return (
       <Redirect
@@ -63,7 +66,9 @@ export function EntityLink() {
   // When EEM is enabled and the service is not found on the EEM indices display a callout guiding on the limitations of EEM
   return (
     <EuiEmptyPrompt
-      iconType="logoObservability"
+      icon={
+        <EuiImage size="fullWidth" src={theme.darkMode ? dashboardsDark : dashboardsLight} alt="" />
+      }
       title={
         <h2>
           {i18n.translate('xpack.apm.entityLink.eemGuide.title', {
