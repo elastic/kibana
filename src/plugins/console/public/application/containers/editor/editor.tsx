6f2af5088faefa6315eae14573c9f6fdf39ee861
Elena Stoeva <elenastoeva99@gmail.com>
2024-09-06 10:35:24 +0300

    Resolve merge conflicts

--- src/plugins/console/public/application/containers/editor/editor.tsx
+++ src/plugins/console/public/application/containers/editor/editor.tsx
@@ -26,6 +26,7 @@ import {
   NetworkRequestStatusBar,
 } from '../../components';
 import { Panel, PanelsContainer } from '..';
+import { Editor as EditorUI, EditorOutput } from './legacy/console_editor';
 import { getAutocompleteInfo, StorageKeys } from '../../../services';
 import {
   useEditorReadContext,
@@ -35,8 +36,7 @@ import {
   useEditorActionContext,
 } from '../../contexts';
 import type { SenseEditor } from '../../models';
-import { MonacoEditor } from './monaco_editor';
-import { MonacoEditorOutput } from './monaco_editor_output';
+import { MonacoEditor, MonacoEditorOutput } from './monaco';
 import { getResponseWithMostSevereStatusCode } from '../../../lib/utils';
 
 const DEBOUNCE_DELAY = 500;
@@ -46,186 +46,198 @@ const PANEL_MIN_WIDTH = '100px';
 interface Props {
   loading: boolean;
   setEditorInstance: (instance: SenseEditor) => void;
+  inputEditorValue: string;
+  setInputEditorValue: (value: string) => void;
 }
 
-export const Editor = memo(({ loading, setEditorInstance }: Props) => {
-  const {
-    services: { storage },
-  } = useServicesContext();
+export const Editor = memo(
+  ({ loading, setEditorInstance, inputEditorValue, setInputEditorValue }: Props) => {
+    const {
+      services: { storage },
+      config: { isMonacoEnabled } = {},
+    } = useServicesContext();
 
-  const { currentTextObject } = useEditorReadContext();
-  const {
-    requestInFlight,
-    lastResult: { data: requestData, error: requestError },
-  } = useRequestReadContext();
+    const { currentTextObject } = useEditorReadContext();
+    const {
+      requestInFlight,
+      lastResult: { data: requestData, error: requestError },
+    } = useRequestReadContext();
 
-  const dispatch = useRequestActionContext();
-  const editorDispatch = useEditorActionContext();
+    const dispatch = useRequestActionContext();
+    const editorDispatch = useEditorActionContext();
 
-  const [fetchingMappings, setFetchingMappings] = useState(false);
+    const [fetchingMappings, setFetchingMappings] = useState(false);
 
-  const [inputEditorValue, setInputEditorValue] = useState<string>(currentTextObject?.text ?? '');
+    useEffect(() => {
+      const subscription = getAutocompleteInfo().mapping.isLoading$.subscribe(setFetchingMappings);
+      return () => {
+        subscription.unsubscribe();
+      };
+    }, []);
 
-  useEffect(() => {
-    const subscription = getAutocompleteInfo().mapping.isLoading$.subscribe(setFetchingMappings);
-    return () => {
-      subscription.unsubscribe();
-    };
-  }, []);
+    const [firstPanelWidth, secondPanelWidth] = storage.get(StorageKeys.WIDTH, [
+      INITIAL_PANEL_WIDTH,
+      INITIAL_PANEL_WIDTH,
+    ]);
 
-  /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  const debouncedUpdateLocalStorageValue = useCallback(
-    debounce((textObject: TextObject) => {
-      editorDispatch({ type: 'setCurrentTextObject', payload: textObject });
-    }, DEBOUNCE_DELAY),
-    []
-  );
-
-  // Always keep the currentTextObject in sync with the value in the editor
-  // to avoid losing the text object when the user navigates away from the shell
-  useEffect(() => {
-    // Only update when its not empty, this is to avoid setting the currentTextObject
-    // to the example text when the user clears the editor.
-    if (inputEditorValue !== '') {
-      const textObject = {
-        ...currentTextObject,
-        text: inputEditorValue,
-        updatedAt: Date.now(),
-      } as TextObject;
-
-      debouncedUpdateLocalStorageValue(textObject);
-    }
     /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  }, [inputEditorValue, debouncedUpdateLocalStorageValue]);
+    const onPanelWidthChange = useCallback(
+      debounce((widths: number[]) => {
+        storage.set(StorageKeys.WIDTH, widths);
+      }, 300),
+      []
+    );
 
-  const [firstPanelWidth, secondPanelWidth] = storage.get(StorageKeys.WIDTH, [
-    INITIAL_PANEL_WIDTH,
-    INITIAL_PANEL_WIDTH,
-  ]);
+    /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    const debouncedUpdateLocalStorageValue = useCallback(
+      debounce((textObject: TextObject) => {
+        editorDispatch({ type: 'setCurrentTextObject', payload: textObject });
+      }, DEBOUNCE_DELAY),
+      []
+    );
 
-  /* eslint-disable-next-line react-hooks/exhaustive-deps */
-  const onPanelWidthChange = useCallback(
-    debounce((widths: number[]) => {
-      storage.set(StorageKeys.WIDTH, widths);
-    }, 300),
-    []
-  );
+    // Always keep the currentTextObject in sync with the value in the editor
+    // to avoid losing the text object when the user navigates away from the shell
+    useEffect(() => {
+      // Only update when its not empty, this is to avoid setting the currentTextObject
+      // to the example text when the user clears the editor.
+      if (inputEditorValue !== '') {
+        const textObject = {
+          ...currentTextObject,
+          text: inputEditorValue,
+          updatedAt: Date.now(),
+        } as TextObject;
 
-  const data = getResponseWithMostSevereStatusCode(requestData) ?? requestError;
-  const isLoading = loading || requestInFlight;
+        debouncedUpdateLocalStorageValue(textObject);
+      }
+      /* eslint-disable-next-line react-hooks/exhaustive-deps */
+    }, [inputEditorValue, debouncedUpdateLocalStorageValue]);
 
-  if (!currentTextObject) return null;
+    const data = getResponseWithMostSevereStatusCode(requestData) ?? requestError;
+    const isLoading = loading || requestInFlight;
 
-  return (
-    <>
-      {fetchingMappings ? (
-        <div className="conApp__requestProgressBarContainer">
-          <EuiProgress size="xs" color="accent" position="absolute" />
-        </div>
-      ) : null}
-      <PanelsContainer onPanelWidthChange={onPanelWidthChange} resizerClassName="conApp__resizer">
-        <Panel
-          style={{ height: '100%', position: 'relative', minWidth: PANEL_MIN_WIDTH }}
-          initialWidth={firstPanelWidth}
-        >
-          <EuiSplitPanel.Outer grow={true} borderRadius="none" hasShadow={false}>
-            <EuiSplitPanel.Inner paddingSize="none">
-              {loading ? (
-                <EditorContentSpinner />
-              ) : (
-                <MonacoEditor
-                  localStorageValue={currentTextObject.text}
-                  value={inputEditorValue}
-                  setValue={setInputEditorValue}
-                />
-              )}
-            </EuiSplitPanel.Inner>
+    if (!currentTextObject) return null;
 
-            {!loading && (
-              <EuiSplitPanel.Inner
-                grow={false}
-                paddingSize="s"
-                css={{
-                  backgroundColor: euiThemeVars.euiFormBackgroundColor,
-                }}
-              >
-                <EuiButtonEmpty
-                  size="xs"
-                  color="primary"
-                  data-test-subj="clearConsoleInput"
-                  onClick={() => setInputEditorValue('')}
+    return (
+      <>
+        {fetchingMappings ? (
+          <div className="conApp__requestProgressBarContainer">
+            <EuiProgress size="xs" color="accent" position="absolute" />
+          </div>
+        ) : null}
+        <PanelsContainer onPanelWidthChange={onPanelWidthChange} resizerClassName="conApp__resizer">
+          <Panel
+            style={{ height: '100%', position: 'relative', minWidth: PANEL_MIN_WIDTH }}
+            initialWidth={firstPanelWidth}
+          >
+            <EuiSplitPanel.Outer grow={true} borderRadius="none" hasShadow={false}>
+              <EuiSplitPanel.Inner paddingSize="none">
+                {loading ? (
+                  <EditorContentSpinner />
+                ) : isMonacoEnabled ? (
+                  <MonacoEditor
+                    localStorageValue={currentTextObject.text}
+                    value={inputEditorValue}
+                    setValue={setInputEditorValue}
+                  />
+                ) : (
+                  <EditorUI
+                    initialTextValue={currentTextObject.text}
+                    setEditorInstance={setEditorInstance}
+                  />
+                )}
+              </EuiSplitPanel.Inner>
+
+              {!loading && (
+                <EuiSplitPanel.Inner
+                  grow={false}
+                  paddingSize="s"
+                  css={{
+                    backgroundColor: euiThemeVars.euiFormBackgroundColor,
+                  }}
                 >
-                  {i18n.translate('console.editor.clearConsoleInputButton', {
-                    defaultMessage: 'Clear this input',
-                  })}
-                </EuiButtonEmpty>
-              </EuiSplitPanel.Inner>
-            )}
-          </EuiSplitPanel.Outer>
-        </Panel>
-        <Panel
-          style={{ height: '100%', position: 'relative', minWidth: PANEL_MIN_WIDTH }}
-          initialWidth={secondPanelWidth}
-        >
-          <EuiSplitPanel.Outer grow borderRadius="none" hasShadow={false}>
-            <EuiSplitPanel.Inner paddingSize="none" css={{ alignContent: 'center' }}>
-              {data ? (
-                <MonacoEditorOutput />
-              ) : isLoading ? (
-                <EditorContentSpinner />
-              ) : (
-                <OutputPanelEmptyState />
+                  <EuiButtonEmpty
+                    size="xs"
+                    color="primary"
+                    data-test-subj="clearConsoleInput"
+                    onClick={() => setInputEditorValue('')}
+                  >
+                    {i18n.translate('console.editor.clearConsoleInputButton', {
+                      defaultMessage: 'Clear this input',
+                    })}
+                  </EuiButtonEmpty>
+                </EuiSplitPanel.Inner>
               )}
-            </EuiSplitPanel.Inner>
-
-            {(data || isLoading) && (
-              <EuiSplitPanel.Inner
-                grow={false}
-                paddingSize="s"
-                css={{
-                  backgroundColor: euiThemeVars.euiFormBackgroundColor,
-                }}
-              >
-                <EuiFlexGroup gutterSize="none">
-                  {data ? (
-                    <EuiFlexItem grow={false}>
-                      <EuiButtonEmpty
-                        size="xs"
-                        color="primary"
-                        data-test-subj="clearConsoleOutput"
-                        onClick={() => dispatch({ type: 'cleanRequest', payload: undefined })}
-                      >
-                        {i18n.translate('console.editor.clearConsoleOutputButton', {
-                          defaultMessage: 'Clear this output',
-                        })}
-                      </EuiButtonEmpty>
-                    </EuiFlexItem>
+            </EuiSplitPanel.Outer>
+          </Panel>
+          <Panel
+            style={{ height: '100%', position: 'relative', minWidth: PANEL_MIN_WIDTH }}
+            initialWidth={secondPanelWidth}
+          >
+            <EuiSplitPanel.Outer grow borderRadius="none" hasShadow={false}>
+              <EuiSplitPanel.Inner paddingSize="none" css={{ alignContent: 'center' }}>
+                {data ? (
+                  isMonacoEnabled ? (
+                    <MonacoEditorOutput />
                   ) : (
-                    <EuiFlexItem grow={false} />
-                  )}
-
-                  <EuiFlexItem>
-                    <NetworkRequestStatusBar
-                      requestInProgress={requestInFlight}
-                      requestResult={
-                        data
-                          ? {
-                              method: data.request.method.toUpperCase(),
-                              endpoint: data.request.path,
-                              statusCode: data.response.statusCode,
-                              statusText: data.response.statusText,
-                              timeElapsedMs: data.response.timeMs,
-                            }
-                          : undefined
-                      }
-                    />
-                  </EuiFlexItem>
-                </EuiFlexGroup>
+                    <EditorOutput />
+                  )
+                ) : isLoading ? (
+                  <EditorContentSpinner />
+                ) : (
+                  <OutputPanelEmptyState />
+                )}
               </EuiSplitPanel.Inner>
-            )}
-          </EuiSplitPanel.Outer>
-        </Panel>
-      </PanelsContainer>
-    </>
-  );
-});
+
+              {(data || isLoading) && (
+                <EuiSplitPanel.Inner
+                  grow={false}
+                  paddingSize="s"
+                  css={{
+                    backgroundColor: euiThemeVars.euiFormBackgroundColor,
+                  }}
+                >
+                  <EuiFlexGroup gutterSize="none">
+                    {data ? (
+                      <EuiFlexItem grow={false}>
+                        <EuiButtonEmpty
+                          size="xs"
+                          color="primary"
+                          data-test-subj="clearConsoleOutput"
+                          onClick={() => dispatch({ type: 'cleanRequest', payload: undefined })}
+                        >
+                          {i18n.translate('console.editor.clearConsoleOutputButton', {
+                            defaultMessage: 'Clear this output',
+                          })}
+                        </EuiButtonEmpty>
+                      </EuiFlexItem>
+                    ) : (
+                      <EuiFlexItem grow={false} />
+                    )}
+
+                    <EuiFlexItem>
+                      <NetworkRequestStatusBar
+                        requestInProgress={requestInFlight}
+                        requestResult={
+                          data
+                            ? {
+                                method: data.request.method.toUpperCase(),
+                                endpoint: data.request.path,
+                                statusCode: data.response.statusCode,
+                                statusText: data.response.statusText,
+                                timeElapsedMs: data.response.timeMs,
+                              }
+                            : undefined
+                        }
+                      />
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiSplitPanel.Inner>
+              )}
+            </EuiSplitPanel.Outer>
+          </Panel>
+        </PanelsContainer>
+      </>
+    );
+  }
+);
