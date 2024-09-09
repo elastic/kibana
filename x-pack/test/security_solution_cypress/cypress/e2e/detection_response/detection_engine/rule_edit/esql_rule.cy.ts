441d41d03e7fd34216b572631795030ce863de25
Yara Tercero <yara.tercero@elastic.co>
2024-09-09 16:35:58 -0700

    trying to address common periodic pipeline flakes

--- x-pack/test/security_solution_cypress/cypress/e2e/detection_response/detection_engine/rule_edit/esql_rule.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/detection_response/detection_engine/rule_edit/esql_rule.cy.ts
@@ -83,6 +83,8 @@ describe(
     });
 
     it('edits ES|QL rule query and override rule name with new property', () => {
+      cy.intercept('PUT', /(\/api\/detection_engine\/rules)/).as('saveRule');
+
       fillEsqlQueryBar(expectedValidEsqlQuery);
 
       goToAboutStepTab();
@@ -91,8 +93,9 @@ describe(
 
       saveEditedRule();
 
-      // ensure rule name override is displayed on details page
-      getDetails(RULE_NAME_OVERRIDE_DETAILS).should('have.text', 'event.category');
+      cy.wait('@saveRule').then(({ response }) => {
+        cy.wrap(response?.body.rule_name_override).should('eql', 'event.category');
+      });
     });
 
     it('adds ES|QL override rule name on edit', () => {
