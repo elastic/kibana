41a2148ca1f5d429bfccdf9e8e92b4325cc540bf
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2024-09-09 15:13:45 +0200

    Update x-pack/plugins/lens/public/visualizations/xy/visualization.tsx

--- x-pack/plugins/lens/public/visualizations/xy/visualization.tsx
+++ x-pack/plugins/lens/public/visualizations/xy/visualization.tsx
@@ -1113,7 +1113,6 @@ export const getXyVisualization = ({
   },
 
   getRenderEventCounters(state) {
-    console.log(shouldDisplayTable(state.legend.legendStats ?? []));
     if (shouldDisplayTable(state.legend.legendStats ?? [])) {
       return [`legend_stats`];
     }
