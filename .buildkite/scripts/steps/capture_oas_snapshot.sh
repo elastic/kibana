eceedf5f252c14c272f166a073e743d9abafec9c
Jon <jon@elastic.co>
2024-09-09 11:27:48 -0500

    [OAS] Only build serverless docs off main (#192294)
    Fixes
    https://buildkite.com/elastic/kibana-on-merge/builds/49782#0191c7a8-ddf4-4ae9-8e1e-9a4c603af4f0
    
    (cherry picked from commit f0164e73a93163fc43e8505be94d3d18ad90a76d)

--- .buildkite/scripts/steps/capture_oas_snapshot.sh
+++ .buildkite/scripts/steps/capture_oas_snapshot.sh
@@ -10,5 +10,9 @@ if is_pr && ! is_auto_commit_disabled; then
   cmd="$cmd --update"
 fi
 
+if [[ $BUILDKITE_PULL_REQUEST != "false" && "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" != "main" ]] || [[ $BUILDKITE_PULL_REQUEST == "false" && "$BUILDKITE_BRANCH" != "main" ]]; then
+  cmd="$cmd --no-serverless"
+fi
+
 eval "$cmd"
 check_for_changed_files "$cmd" true
