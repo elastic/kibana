9c6bbaee45756aab3d459ce0601627ce3e19ffaf
Marshall Main <marshall.main@elastic.co>
2024-09-09 15:02:58 -0400

    Fix field access patterns in integration tests

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/trial_license_complete_tier/keyword_family/const_keyword.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/trial_license_complete_tier/keyword_family/const_keyword.ts
@@ -6,6 +6,7 @@
  */
 
 import expect from '@kbn/expect';
+import { get } from 'lodash';
 import {
   EqlRuleCreateProps,
   ThresholdRuleCreateProps,
@@ -73,7 +74,7 @@ export default ({ getService }: FtrProviderContext) => {
         await waitForRuleSuccess({ supertest, log, id });
         await waitForAlertsToBePresent(supertest, log, 4, [id]);
         const alertsOpen = await getAlertsById(supertest, log, id);
-        const hits = alertsOpen.hits.hits.map((hit) => hit._source?.['event.dataset']).sort();
+        const hits = alertsOpen.hits.hits.map((hit) => get(hit, '_source.event.dataset')).sort();
         expect(hits).to.eql([
           'dataset_name_1',
           'dataset_name_1',
@@ -107,7 +108,7 @@ export default ({ getService }: FtrProviderContext) => {
         await waitForRuleSuccess({ supertest, log, id });
         await waitForAlertsToBePresent(supertest, log, 4, [id]);
         const alertsOpen = await getAlertsById(supertest, log, id);
-        const hits = alertsOpen.hits.hits.map((hit) => hit._source?.['event.dataset']).sort();
+        const hits = alertsOpen.hits.hits.map((hit) => get(hit, '_source.event.dataset')).sort();
         expect(hits).to.eql([
           'dataset_name_1',
           'dataset_name_1',
