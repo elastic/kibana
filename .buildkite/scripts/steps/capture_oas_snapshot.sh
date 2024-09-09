da5e805114b02aa1dfa9ecb04372fd9538eb87a5
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-09-10 05:14:09 +1000

    [8.15] [OAS] Only build serverless docs off main (#192294) (#192390)
    # Backport
    
    This will backport the following commits from `main` to `8.15`:
    - [[OAS] Only build serverless docs off main
    (#192294)](https://github.com/elastic/kibana/pull/192294)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT
    [{"author":{"name":"Jon","email":"jon@elastic.co"},"sourceCommit":{"committedDate":"2024-09-09T16:27:48Z","message":"[OAS]
    Only build serverless docs off main
    (#192294)\n\nFixes\r\nhttps://buildkite.com/elastic/kibana-on-merge/builds/49782#0191c7a8-ddf4-4ae9-8e1e-9a4c603af4f0","sha":"f0164e73a93163fc43e8505be94d3d18ad90a76d","branchLabelMapping":{"^v8.16.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["Team:Core","Team:Operations","release_note:skip","backport:prev-minor","v8.16.0"],"title":"[OAS]
    Only build serverless docs off
    main","number":192294,"url":"https://github.com/elastic/kibana/pull/192294","mergeCommit":{"message":"[OAS]
    Only build serverless docs off main
    (#192294)\n\nFixes\r\nhttps://buildkite.com/elastic/kibana-on-merge/builds/49782#0191c7a8-ddf4-4ae9-8e1e-9a4c603af4f0","sha":"f0164e73a93163fc43e8505be94d3d18ad90a76d"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v8.16.0","branchLabelMappingKey":"^v8.16.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/192294","number":192294,"mergeCommit":{"message":"[OAS]
    Only build serverless docs off main
    (#192294)\n\nFixes\r\nhttps://buildkite.com/elastic/kibana-on-merge/builds/49782#0191c7a8-ddf4-4ae9-8e1e-9a4c603af4f0","sha":"f0164e73a93163fc43e8505be94d3d18ad90a76d"}}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: Jon <jon@elastic.co>

--- .buildkite/scripts/steps/capture_oas_snapshot.sh
+++ .buildkite/scripts/steps/capture_oas_snapshot.sh
@@ -5,9 +5,13 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 echo --- Capture OAS snapshot
-cmd="node scripts/capture_oas_snapshot --include-path /api/status --no-serverless"
+cmd="node scripts/capture_oas_snapshot --include-path /api/status"
 if is_pr && ! is_auto_commit_disabled; then
   cmd="$cmd --update"
+fi
+
+if [[ $BUILDKITE_PULL_REQUEST != "false" && "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" != "main" ]] || [[ $BUILDKITE_PULL_REQUEST == "false" && "$BUILDKITE_BRANCH" != "main" ]]; then
+  cmd="$cmd --no-serverless"
 fi
 
 eval "$cmd"
