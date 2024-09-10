f15336a01e72a34915217581b119a4ec0913fbf2
Catherine Liu <catherine.liu@elastic.co>
2024-09-09 23:37:37 -0700

    Fix ts error

--- x-pack/test/accessibility/apps/group1/dashboard_panel_options.ts
+++ x-pack/test/accessibility/apps/group1/dashboard_panel_options.ts
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-import type { WebElementWrapper } from '@kbn/ftr-common-functional-ui-services';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
@@ -18,7 +17,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
   // Failing: See https://github.com/elastic/kibana/issues/147667
   describe.skip('Dashboard panel options a11y tests', () => {
-    let header: WebElementWrapper;
+    const title = '[Flights] Flight count';
     before(async () => {
       await PageObjects.common.navigateToUrl('home', '/tutorial_directory/sampleData', {
         useActualUrl: true,
@@ -28,7 +27,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
       await PageObjects.common.navigateToApp('dashboard');
       await testSubjects.click('dashboardListingTitleLink-[Flights]-Global-Flight-Dashboard');
-      header = await dashboardPanelActions.getPanelHoverActions('[Flights] Flight count');
     });
 
     after(async () => {
@@ -40,13 +38,13 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
     // dashboard panel options in view mode
     it('dashboard panel - open menu', async () => {
-      await dashboardPanelActions.toggleContextMenu(header);
+      await dashboardPanelActions.toggleContextMenuByTitle(title);
       await a11y.testAppSnapshot();
-      await dashboardPanelActions.toggleContextMenu(header);
+      await dashboardPanelActions.toggleContextMenuByTitle(title);
     });
 
     it('dashboard panel - customize time range', async () => {
-      await dashboardPanelActions.toggleContextMenu(header);
+      await dashboardPanelActions.toggleContextMenuByTitle(title);
       await testSubjects.click('embeddablePanelAction-CUSTOM_TIME_RANGE');
       await a11y.testAppSnapshot();
       await testSubjects.click('cancelPerPanelTimeRangeButton');
@@ -86,7 +84,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     });
 
     it('dashboard panel - copy to dashboard', async () => {
-      await dashboardPanelActions.openContextMenu(header);
+      await dashboardPanelActions.openContextMenuByTitle(title);
       await testSubjects.click('embeddablePanelAction-copyToDashboard');
       await a11y.testAppSnapshot();
       await testSubjects.click('cancelCopyToButton');
@@ -96,7 +94,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
     it('dashboard panel - clone panel', async () => {
       await testSubjects.click('dashboardEditMode');
-      await dashboardPanelActions.openContextMenu(header);
+      await dashboardPanelActions.openContextMenuByTitle(title);
       await testSubjects.click('embeddablePanelAction-clonePanel');
       await toasts.dismissAll();
       await a11y.testAppSnapshot();
@@ -112,7 +110,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     });
 
     it('dashboard panel - Create drilldown panel', async () => {
-      await dashboardPanelActions.openContextMenu(header);
+      await dashboardPanelActions.openContextMenuByTitle(title);
       await testSubjects.click('embeddablePanelAction-OPEN_FLYOUT_ADD_DRILLDOWN');
       await a11y.testAppSnapshot();
       await testSubjects.click('actionFactoryItem-DASHBOARD_TO_DASHBOARD_DRILLDOWN');
@@ -127,7 +125,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     });
 
     it('dashboard panel - manage drilldown', async () => {
-      await dashboardPanelActions.openContextMenu(header);
+      await dashboardPanelActions.openContextMenuByTitle(title);
       await testSubjects.click('embeddablePanelMore-mainMenu');
       await testSubjects.click('embeddablePanelAction-OPEN_FLYOUT_EDIT_DRILLDOWN');
       await a11y.testAppSnapshot();
@@ -135,7 +133,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     });
 
     it('dashboard panel - save to library', async () => {
-      await dashboardPanelActions.legacySaveToLibrary('', header);
+      await dashboardPanelActions.legacySaveToLibrary('', title);
       await a11y.testAppSnapshot();
       await testSubjects.click('saveCancelButton');
     });
