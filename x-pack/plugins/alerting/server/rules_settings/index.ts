95578cbfde1e2ff8262152b7050df831372caf74
Ying <ying.mao@elastic.co>
2024-09-09 22:13:39 -0400

    Caching rule settings

--- x-pack/plugins/alerting/server/rules_settings/index.ts
+++ x-pack/plugins/alerting/server/rules_settings/index.ts
@@ -10,3 +10,4 @@ export * from './flapping/rules_settings_flapping_client';
 export * from './query_delay/rules_settings_query_delay_client';
 export * from './rules_settings_client_factory';
 export * from './rules_settings_feature';
+export * from './rules_settings_service';
