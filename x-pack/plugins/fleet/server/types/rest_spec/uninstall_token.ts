8ad1aaf1b7a33a199ba6d1dd6d40a7b7bcc15abf
Julia Bardi <julia.bardi@elastic.co>
2024-09-10 13:46:24 +0200

    uninstall_tokens path

--- x-pack/plugins/fleet/server/types/rest_spec/uninstall_token.ts
+++ x-pack/plugins/fleet/server/types/rest_spec/uninstall_token.ts
@@ -8,9 +8,20 @@ import { schema } from '@kbn/config-schema';
 
 export const GetUninstallTokensMetadataRequestSchema = {
   query: schema.object({
-    policyId: schema.maybe(schema.string({ maxLength: 50 })),
+    policyId: schema.maybe(
+      schema.string({
+        maxLength: 50,
+        meta: { description: 'Partial match filtering for policy IDs' },
+      })
+    ),
     search: schema.maybe(schema.string({ maxLength: 50 })),
-    perPage: schema.maybe(schema.number({ defaultValue: 20, min: 5 })),
+    perPage: schema.maybe(
+      schema.number({
+        defaultValue: 20,
+        min: 5,
+        meta: { description: 'The number of items to return' },
+      })
+    ),
     page: schema.maybe(schema.number({ defaultValue: 1, min: 1 })),
   }),
 };
