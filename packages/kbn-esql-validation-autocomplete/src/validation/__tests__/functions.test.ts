f984fc2d0c2e5116e0852cc2fc295bc12bc04869
Drew Tate <andrew.tate@elastic.co>
2024-09-09 19:25:05 -0600

    add optional argument test

--- packages/kbn-esql-validation-autocomplete/src/validation/__tests__/functions.test.ts
+++ packages/kbn-esql-validation-autocomplete/src/validation/__tests__/functions.test.ts
@@ -1,9 +1,10 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0 and the Server Side Public License, v 1; you may not use this file except
- * in compliance with, at your election, the Elastic License 2.0 or the Server
- * Side Public License, v 1.
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
 import { FunctionDefinition } from '../../definitions/types';
@@ -266,6 +267,33 @@ describe('function validation', () => {
         `FROM a_index | EVAL VARIADIC_FN(${new Array(100).fill(1).join(', ')})`,
         []
       );
+    });
+
+    it('allows for optional arguments', async () => {
+      const testFns: FunctionDefinition[] = [
+        {
+          name: 'test',
+          type: 'eval',
+          description: '',
+          supportedCommands: ['eval'],
+          signatures: [
+            {
+              params: [
+                { name: 'arg1', type: 'keyword' },
+                { name: 'arg2', type: 'keyword', optional: true },
+              ],
+              returnType: 'keyword',
+            },
+          ],
+        },
+      ];
+
+      setTestFunctions(testFns);
+
+      const { expectErrors } = await setup();
+
+      await expectErrors('FROM a_index | EVAL TEST("")', []);
+      await expectErrors('FROM a_index | EVAL TEST("", "")', []);
     });
 
     it('validates "all" parameter (wildcard)', async () => {
