a4a368914b229ff9582adb79d4c02ded55f4ff4a
vadimkibana <vadimkibana@gmail.com>
2024-09-10 22:13:07 +0200

    setup inspector state and context

---
+++ examples/esql_ast_inspector/public/components/esql_inspector/context.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
+ */
+
+import * as React from 'react';
+import { EsqlInspectorState } from './esql_inspector_state';
+
+export const context = React.createContext<EsqlInspectorState | null>(null);
+
+export const useEsqlInspector = () => React.useContext(context)!;
