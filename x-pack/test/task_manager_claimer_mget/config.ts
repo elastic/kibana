300fa232f2a0d6ae8756160d7bca34ce663a66aa
Ying <ying.mao@elastic.co>
2024-09-10 12:49:39 -0400

    Increasing metrics reset interval

--- x-pack/test/task_manager_claimer_mget/config.ts
+++ x-pack/test/task_manager_claimer_mget/config.ts
@@ -32,7 +32,7 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
         '--xpack.task_manager.monitored_aggregated_stats_refresh_rate=5000',
         '--xpack.task_manager.ephemeral_tasks.enabled=false',
         '--xpack.task_manager.ephemeral_tasks.request_capacity=100',
-        '--xpack.task_manager.metrics_reset_interval=120000',
+        '--xpack.task_manager.metrics_reset_interval=40000',
         `--xpack.stack_connectors.enableExperimental=${JSON.stringify(['crowdstrikeConnectorOn'])}`,
         ...findTestPluginPaths(path.resolve(__dirname, 'plugins')),
       ],
