f677a2280a5a839e67643477230eab731d9c6b52
Steph Milovic <stephanie.milovic@elastic.co>
2024-09-10 14:57:37 -0600

    better strings

--- x-pack/plugins/elastic_assistant/server/lib/langchain/graphs/default_assistant_graph/nodes/translations.ts
+++ x-pack/plugins/elastic_assistant/server/lib/langchain/graphs/default_assistant_graph/nodes/translations.ts
@@ -4,25 +4,14 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-import { i18n } from '@kbn/i18n';
-
-export const NEW_CHAT = i18n.translate('xpack.elasticAssistantPlugin.server.newChat', {
-  defaultMessage: 'New chat',
-});
-export const YOU_ARE_A_HELPFUL_EXPERT_ASSISTANT = i18n.translate(
-  'xpack.elasticAssistantPlugin.server.prompts.youAreAHelpfulExpertAssistant',
-  {
-    defaultMessage:
-      'You are a security analyst and expert in resolving security incidents. Your role is to assist by answering questions about Elastic Security.',
-  }
-);
-
-export const IF_YOU_DONT_KNOW_THE_ANSWER = i18n.translate(
-  'xpack.elasticAssistantPlugin.server.prompts.ifYouDontKnowTheAnswer',
-  {
-    defaultMessage: 'Do not answer questions unrelated to Elastic Security.',
-  }
-);
+// TODO determine whether or not system prompts should be i18n'd
+const YOU_ARE_A_HELPFUL_EXPERT_ASSISTANT =
+  'You are a security analyst and expert in resolving security incidents. Your role is to assist by answering questions about Elastic Security.';
+const IF_YOU_DONT_KNOW_THE_ANSWER = 'Do not answer questions unrelated to Elastic Security.';
 
 export const DEFAULT_SYSTEM_PROMPT = `${YOU_ARE_A_HELPFUL_EXPERT_ASSISTANT} ${IF_YOU_DONT_KNOW_THE_ANSWER}`;
+
+export const GEMINI_SYSTEM_PROMPT =
+  `ALWAYS use the provided tools, as they have access to the latest data and syntax.` +
+  "The final response is the only output the user sees and should be a complete answer to the user's question. Do not leave out important tool output. The final response should never be empty. Don't forget to use tools.";
+export const BEDROCK_SYSTEM_PROMPT = `Use tools as often as possible, as they have access to the latest data and syntax. Always return value from ESQLKnowledgeBaseTool as is. Never return <thinking> tags in the response, but make sure to include <result> tags content in the response. Do not reflect on the quality of the returned search results in your response.`;
--- x-pack/plugins/elastic_assistant/server/lib/langchain/graphs/default_assistant_graph/prompts.ts
+++ x-pack/plugins/elastic_assistant/server/lib/langchain/graphs/default_assistant_graph/prompts.ts
@@ -6,7 +6,11 @@
  */
 
 import { ChatPromptTemplate } from '@langchain/core/prompts';
-import { DEFAULT_SYSTEM_PROMPT } from './nodes/translations';
+import {
+  BEDROCK_SYSTEM_PROMPT,
+  DEFAULT_SYSTEM_PROMPT,
+  GEMINI_SYSTEM_PROMPT,
+} from './nodes/translations';
 
 export const formatPrompt = (prompt: string, additionalPrompt?: string) =>
   ChatPromptTemplate.fromMessages([
@@ -18,41 +22,63 @@ export const formatPrompt = (prompt: string, additionalPrompt?: string) =>
 
 export const systemPrompts = {
   openai: DEFAULT_SYSTEM_PROMPT,
-  bedrock: `${DEFAULT_SYSTEM_PROMPT} Use tools as often as possible, as they have access to the latest data and syntax. Always return value from ESQLKnowledgeBaseTool as is. Never return <thinking> tags in the response, but make sure to include <result> tags content in the response. Do not reflect on the quality of the returned search results in your response.`,
-  gemini:
-    `${DEFAULT_SYSTEM_PROMPT} ALWAYS use the provided tools, as they have access to the latest data and syntax.` +
-    "The final response is the only output the user sees and should be a complete answer to the user's question. Do not leave out important tool output. The final response should never be empty. Don't forget to use tools.",
-  structuredChat:
-    'Respond to the human as helpfully and accurately as possible. You have access to the following tools:\n\n' +
-    '{tools}\n\n' +
-    `The tool action_input should ALWAYS follow the tool JSON schema args.\n\n` +
-    'Valid "action" values: "Final Answer" or {tool_names}\n\n' +
-    'Use a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input strictly adhering to the tool JSON schema args).\n\n' +
-    'Provide only ONE action per $JSON_BLOB, as shown:\n\n' +
-    '```\n\n' +
-    '{{\n\n' +
-    '  "action": $TOOL_NAME,\n\n' +
-    '  "action_input": $TOOL_INPUT\n\n' +
-    '}}\n\n' +
-    '```\n\n' +
-    'Follow this format:\n\n' +
-    'Question: input question to answer\n\n' +
-    'Thought: consider previous and subsequent steps\n\n' +
-    'Action:\n\n' +
-    '```\n\n' +
-    '$JSON_BLOB\n\n' +
-    '```\n\n' +
-    'Observation: action result\n\n' +
-    '... (repeat Thought/Action/Observation N times)\n\n' +
-    'Thought: I know what to respond\n\n' +
-    'Action:\n\n' +
-    '```\n\n' +
-    '{{\n\n' +
-    '  "action": "Final Answer",\n\n' +
-    // important, no new line here
-    '  "action_input": "Final response to human"' +
-    '}}\n\n' +
-    'Begin! Reminder to ALWAYS respond with a valid json blob of a single action with no additional output. When using tools, ALWAYS input the expected JSON schema args. Your answer will be parsed as JSON, so never use double quotes within the output and instead use backticks. Single quotes may be used, such as apostrophes. Response format is Action:```$JSON_BLOB```then Observation',
+  bedrock: `${DEFAULT_SYSTEM_PROMPT} ${BEDROCK_SYSTEM_PROMPT}`,
+  gemini: `${DEFAULT_SYSTEM_PROMPT} ${GEMINI_SYSTEM_PROMPT}`,
+  structuredChat: `Respond to the human as helpfully and accurately as possible. You have access to the following tools:
+
+{tools}
+
+The tool action_input should ALWAYS follow the tool JSON schema args.
+
+Valid "action" values: "Final Answer" or {tool_names}
+
+Use a json blob to specify a tool by providing an action key (tool name) and an action_input key (tool input strictly adhering to the tool JSON schema args).
+
+Provide only ONE action per $JSON_BLOB, as shown:
+
+\`\`\`
+
+{{
+
+  "action": $TOOL_NAME,
+
+  "action_input": $TOOL_INPUT
+
+}}
+
+\`\`\`
+
+Follow this format:
+
+Question: input question to answer
+
+Thought: consider previous and subsequent steps
+
+Action:
+
+\`\`\`
+
+$JSON_BLOB
+
+\`\`\`
+
+Observation: action result
+
+... (repeat Thought/Action/Observation N times)
+
+Thought: I know what to respond
+
+Action:
+
+\`\`\`
+
+{{
+
+  "action": "Final Answer",
+
+  "action_input": "Final response to human"}}
+
+Begin! Reminder to ALWAYS respond with a valid json blob of a single action with no additional output. When using tools, ALWAYS input the expected JSON schema args. Your answer will be parsed as JSON, so never use double quotes within the output and instead use backticks. Single quotes may be used, such as apostrophes. Response format is Action:\`\`\`$JSON_BLOB\`\`\`then Observation`,
 };
 
 export const openAIFunctionAgentPrompt = formatPrompt(systemPrompts.openai);
