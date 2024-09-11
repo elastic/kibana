c341b2789c0ba7abbc9d1ca3ab2a0c8f81eafc9f
Lene Gadewoll <lene.gadewoll@elastic.co>
2024-09-11 17:32:35 +0200

    rebase cleanup

--- test/functional/apps/discover/group2_data_grid1/_data_grid_context.ts
+++ test/functional/apps/discover/group2_data_grid1/_data_grid_context.ts
@@ -68,11 +68,6 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       expect(columnNames).to.eql(['@timestamp', ...TEST_COLUMN_NAMES]);
     });
 
-    it('should open the context view with the same columns', async () => {
-      const columnNames = await dataGrid.getHeaderFields();
-      expect(columnNames).to.eql(['@timestamp', ...TEST_COLUMN_NAMES]);
-    });
-
     it('should open the context view with the selected document as anchor', async () => {
       // check the anchor timestamp in the context view
       await retry.waitFor('selected document timestamp matches anchor timestamp ', async () => {
