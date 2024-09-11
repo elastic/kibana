ca58ca6eb3e237ab7b6db88f4437160e405b4fcc
Timothy Sullivan <tsullivan@elastic.co>
2024-09-11 13:39:45 -0700

    explanation comment for SpaceContentTypeSummaryItem

--- x-pack/plugins/spaces/public/types.ts
+++ x-pack/plugins/spaces/public/types.ts
@@ -62,9 +62,13 @@ export interface SpacesApi {
   ui: SpacesApiUi;
 }
 
+/**
+ * The API for retrieving content associated with a space returns an array of summary data for each type of
+ * saved object content. SpaceContentTypeSummaryItem is the format of the items included in this summary data.
+ */
 export interface SpaceContentTypeSummaryItem {
   displayName: string;
   icon?: string;
   count: number;
-  type: string;
+  type: string; // the type of saved object content (dashboard, search, config, etc)
 }
