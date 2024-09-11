8ba04d023097e29c42c845ca49de7c94ecb6af1c
Timothy Sullivan <tsullivan@elastic.co>
2024-09-11 09:20:47 -0700

    fix spaces manager mock

--- x-pack/plugins/spaces/public/spaces_manager/spaces_manager.mock.ts
+++ x-pack/plugins/spaces/public/spaces_manager/spaces_manager.mock.ts
@@ -26,7 +26,7 @@ function createSpacesManagerMock() {
     updateSavedObjectsSpaces: jest.fn().mockResolvedValue(undefined),
     resolveCopySavedObjectsErrors: jest.fn().mockResolvedValue(undefined),
     getShareSavedObjectPermissions: jest.fn().mockResolvedValue(undefined),
-    getContentForSpace: jest.fn().mockResolvedValue([]),
+    getContentForSpace: jest.fn().mockResolvedValue({ summary: [], total: 0 }),
     getRolesForSpace: jest.fn().mockResolvedValue([]),
     redirectToSpaceSelector: jest.fn().mockResolvedValue(undefined),
   } as unknown as jest.Mocked<SpacesManager>;
