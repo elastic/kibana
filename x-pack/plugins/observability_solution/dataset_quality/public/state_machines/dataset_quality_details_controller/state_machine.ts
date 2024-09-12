cac9ae7c0a6296047b2c80e45bc45cdd6dab348a
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2024-09-12 17:00:11 +0200

    Fix state machine to clear the expandedDegradedField from URL when flyout closes

--- x-pack/plugins/observability_solution/dataset_quality/public/state_machines/dataset_quality_details_controller/state_machine.ts
+++ x-pack/plugins/observability_solution/dataset_quality/public/state_machines/dataset_quality_details_controller/state_machine.ts
@@ -369,6 +369,7 @@ export const createPureDatasetQualityDetailsControllerStateMachine = (
                   },
                   {
                     target: '.closed',
+                    actions: ['storeExpandedDegradedField'],
                   },
                 ],
               },
