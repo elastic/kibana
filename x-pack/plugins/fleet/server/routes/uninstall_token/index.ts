1b5166032329efd70e7d0fe3ff086e261d161eef
Julia Bardi <90178898+juliaElastic@users.noreply.github.com>
2024-09-13 19:51:29 +0200

    [8.x] Revert "[Fleet] use @kbn/config-schema in Fleet API (Part 1) (#192447… (#192904)
    backport https://github.com/elastic/kibana/pull/192840 to branch 8.x

--- x-pack/plugins/fleet/server/routes/uninstall_token/index.ts
+++ x-pack/plugins/fleet/server/routes/uninstall_token/index.ts
@@ -4,8 +4,6 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { schema } from '@kbn/config-schema';
-
 import { UNINSTALL_TOKEN_ROUTES, API_VERSIONS } from '../../../common/constants';
 import type { FleetConfigType } from '../../config';
 
@@ -15,8 +13,6 @@ import {
   GetUninstallTokensMetadataRequestSchema,
 } from '../../types/rest_spec/uninstall_token';
 import { parseExperimentalConfigValue } from '../../../common/experimental_features';
-
-import { genericErrorResponse } from '../schema/errors';
 
 import { getUninstallTokenHandler, getUninstallTokensMetadataHandler } from './handlers';
 
@@ -30,39 +26,11 @@ export const registerRoutes = (router: FleetAuthzRouter, config: FleetConfigType
         fleetAuthz: {
           fleet: { allAgents: true },
         },
-        description: 'List metadata for latest uninstall tokens per agent policy',
-        options: {
-          tags: ['oas-tag:Fleet uninstall tokens'],
-        },
       })
       .addVersion(
         {
           version: API_VERSIONS.public.v1,
-          validate: {
-            request: GetUninstallTokensMetadataRequestSchema,
-            response: {
-              200: {
-                body: () =>
-                  schema.object({
-                    items: schema.arrayOf(
-                      schema.object({
-                        id: schema.string(),
-                        policy_id: schema.string(),
-                        policy_name: schema.maybe(schema.string()),
-                        created_at: schema.string(),
-                        namespaces: schema.maybe(schema.arrayOf(schema.string())),
-                      })
-                    ),
-                    total: schema.number(),
-                    page: schema.number(),
-                    perPage: schema.number(),
-                  }),
-              },
-              400: {
-                body: genericErrorResponse,
-              },
-            },
-          },
+          validate: { request: GetUninstallTokensMetadataRequestSchema },
         },
         getUninstallTokensMetadataHandler
       );
@@ -73,35 +41,11 @@ export const registerRoutes = (router: FleetAuthzRouter, config: FleetConfigType
         fleetAuthz: {
           fleet: { allAgents: true },
         },
-        description: 'Get one decrypted uninstall token by its ID',
-        options: {
-          tags: ['oas-tag:Fleet uninstall tokens'],
-        },
       })
       .addVersion(
         {
           version: API_VERSIONS.public.v1,
-          validate: {
-            request: GetUninstallTokenRequestSchema,
-            response: {
-              200: {
-                body: () =>
-                  schema.object({
-                    item: schema.object({
-                      id: schema.string(),
-                      token: schema.string(),
-                      policy_id: schema.string(),
-                      policy_name: schema.maybe(schema.string()),
-                      created_at: schema.string(),
-                      namespaces: schema.maybe(schema.arrayOf(schema.string())),
-                    }),
-                  }),
-              },
-              400: {
-                body: genericErrorResponse,
-              },
-            },
-          },
+          validate: { request: GetUninstallTokenRequestSchema },
         },
         getUninstallTokenHandler
       );
