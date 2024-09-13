6137e68c1432f8655d56139fbd1d7495d391094a
pgayvallet <pierre.gayvallet@elastic.co>
2024-09-13 13:50:42 +0200

    refactor main workflow

--- x-pack/plugins/inference/server/tasks/nl_to_esql/index.ts
+++ x-pack/plugins/inference/server/tasks/nl_to_esql/index.ts
@@ -34,6 +34,27 @@ type NlToEsqlTaskEvent<TToolOptions extends ToolOptions> =
 
 const loadDocBase = once(() => EsqlDocumentBase.load());
 
+const requestDocumentationSchema = {
+  type: 'object',
+  properties: {
+    commands: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+      description:
+        'ES|QL source and processing commands you want to analyze before generating the query.',
+    },
+    functions: {
+      type: 'array',
+      items: {
+        type: 'string',
+      },
+      description: 'ES|QL functions you want to analyze before generating the query.',
+    },
+  },
+} satisfies ToolSchema;
+
 export function naturalLanguageToEsql<TToolOptions extends ToolOptions>({
   client,
   connectorId,
@@ -47,64 +68,93 @@ export function naturalLanguageToEsql<TToolOptions extends ToolOptions>({
   logger: Pick<Logger, 'debug'>;
 } & TToolOptions &
   ({ input: string } | { messages: Message[] })): Observable<NlToEsqlTaskEvent<TToolOptions>> {
-  const hasTools = !isEmpty(tools) && toolChoice !== ToolChoiceType.none;
-
-  const requestDocumentationSchema = {
-    type: 'object',
-    properties: {
-      commands: {
-        type: 'array',
-        items: {
-          type: 'string',
-        },
-        description:
-          'ES|QL source and processing commands you want to analyze before generating the query.',
-      },
-      functions: {
-        type: 'array',
-        items: {
-          type: 'string',
-        },
-        description: 'ES|QL functions you want to analyze before generating the query.',
-      },
-    },
-  } satisfies ToolSchema;
-
   const messages: Message[] =
     'input' in rest ? [{ role: MessageRole.User, content: rest.input }] : rest.messages;
 
   return from(loadDocBase()).pipe(
     switchMap((docBase) => {
-      function askLlmToRespond({
-        documentationRequest: { commands, functions },
-      }: {
-        documentationRequest: { commands?: string[]; functions?: string[] };
-      }): Observable<NlToEsqlTaskEvent<TToolOptions>> {
-        const keywords = [...(commands ?? []), ...(functions ?? [])];
+      const systemMessage = docBase.getSystemMessage();
 
-        const systemMessage = docBase.getSystemMessage();
-        const requestedDocumentation = docBase.getDocumentation(keywords);
-        const fakeRequestDocsToolCall = createFakeTooCall(commands, functions);
+      const askLlmToRespond = generateEsqlTask({
+        connectorId,
+        chatCompleteApi: client.chatComplete,
+        messages,
+        docBase,
+        logger,
+        systemMessage,
+        toolOptions: {
+          tools,
+          toolChoice,
+        },
+      });
 
-        return merge(
-          of<
-            OutputCompleteEvent<
-              'request_documentation',
-              { keywords: string[]; requestedDocumentation: Record<string, string> }
-            >
-          >({
-            type: OutputEventType.OutputComplete,
-            id: 'request_documentation',
-            output: {
-              keywords,
-              requestedDocumentation,
+      return requestDocumentation({
+        connectorId,
+        outputApi: client.output,
+        messages,
+        system: systemMessage,
+        toolOptions: {
+          tools,
+          toolChoice,
+        },
+      }).pipe(
+        switchMap((documentationEvent) => {
+          return askLlmToRespond({
+            documentationRequest: {
+              commands: documentationEvent.output.commands,
+              functions: documentationEvent.output.functions,
             },
-            content: '',
-          }),
-          client
-            .chatComplete({
-              connectorId,
-              system: `${systemMessage}
+          });
+        })
+      );
+    })
+  );
+}
+
+const generateEsqlTask = <TToolOptions extends ToolOptions>({
+  chatCompleteApi,
+  connectorId,
+  systemMessage,
+  messages,
+  toolOptions: { tools, toolChoice },
+  docBase,
+  logger,
+}: {
+  connectorId: string;
+  systemMessage: string;
+  messages: Message[];
+  toolOptions: ToolOptions;
+  chatCompleteApi: InferenceClient['chatComplete'];
+  docBase: EsqlDocumentBase;
+  logger: Pick<Logger, 'debug'>;
+}) => {
+  return function askLlmToRespond({
+    documentationRequest: { commands, functions },
+  }: {
+    documentationRequest: { commands?: string[]; functions?: string[] };
+  }): Observable<NlToEsqlTaskEvent<TToolOptions>> {
+    const keywords = [...(commands ?? []), ...(functions ?? [])];
+    const requestedDocumentation = docBase.getDocumentation(keywords);
+    const fakeRequestDocsToolCall = createFakeTooCall(commands, functions);
+
+    return merge(
+      of<
+        OutputCompleteEvent<
+          'request_documentation',
+          { keywords: string[]; requestedDocumentation: Record<string, string> }
+        >
+      >({
+        type: OutputEventType.OutputComplete,
+        id: 'request_documentation',
+        output: {
+          keywords,
+          requestedDocumentation,
+        },
+        content: '',
+      }),
+      chatCompleteApi({
+        connectorId,
+        system: `${systemMessage}
 
           # Current task
 
@@ -117,106 +167,123 @@ export function naturalLanguageToEsql<TToolOptions extends ToolOptions>({
           <query>
           \`\`\`
 
-          When generating ES|QL, you must use commands and functions present on the
-          requested documentation, and follow the syntax as described in the documentation
-          and its examples.
-
-          DO NOT UNDER ANY CIRCUMSTANCES use commands, functions, parameters, or syntaxes that are not
-          explicitly mentioned as supported capability by ES|QL, either in the system message or documentation.
-          assume that ONLY the set of capabilities described in the requested documentation is valid.
-          Do not try to guess parameters or syntax based on other query languages.
+          When generating ES|QL, it is VERY important that you only use commands and functions present in the
+          requested documentation, and follow the syntax as described in the documentation and its examples.
+          Assume that ONLY the set of capabilities described in the provided ES|QL documentation is valid, and
+          do not try to guess parameters or syntax based on other query languages.
 
           If what the user is asking for is not technically achievable with ES|QL's capabilities, just inform
           the user. DO NOT invent capabilities not described in the documentation just to provide
-          a positive answer to the user. E.g. LIMIT only has one parameter, do not assume you can add more.
+          a positive answer to the user. E.g. Pagination is not supported by the language, do not try to invent
+          workarounds based on other languages.
 
           When converting queries from one language to ES|QL, make sure that the functions are available
           and documented in ES|QL. E.g., for SPL's LEN, use LENGTH. For IF, use CASE.
-`,
-              messages: messages.concat([
-                {
-                  role: MessageRole.Assistant,
-                  content: null,
-                  toolCalls: [fakeRequestDocsToolCall],
+        `,
+        messages: [
+          ...messages,
+          {
+            role: MessageRole.Assistant,
+            content: null,
+            toolCalls: [fakeRequestDocsToolCall],
+          },
+          {
+            role: MessageRole.Tool,
+            response: {
+              documentation: requestedDocumentation,
+            },
+            toolCallId: fakeRequestDocsToolCall.toolCallId,
+          },
+        ],
+        toolChoice,
+        tools: {
+          ...tools,
+          request_documentation: {
+            description: 'Request additional ES|QL documentation if needed',
+            schema: requestDocumentationSchema,
+          },
+        },
+      }).pipe(
+        withoutTokenCountEvents(),
+        map((generateEvent) => {
+          if (isChatCompletionMessageEvent(generateEvent)) {
+            return {
+              ...generateEvent,
+              content: generateEvent.content
+                ? correctEsqlMistakes({ content: generateEvent.content, logger })
+                : generateEvent.content,
+            };
+          }
+
+          return generateEvent;
+        }),
+        switchMap((generateEvent) => {
+          if (isChatCompletionMessageEvent(generateEvent)) {
+            const onlyToolCall =
+              generateEvent.toolCalls.length === 1 ? generateEvent.toolCalls[0] : undefined;
+
+            if (onlyToolCall?.function.name === 'request_documentation') {
+              const args = onlyToolCall.function.arguments;
+
+              return askLlmToRespond({
+                documentationRequest: {
+                  commands: args.commands,
+                  functions: args.functions,
                 },
-                {
-                  role: MessageRole.Tool,
-                  response: {
-                    documentation: requestedDocumentation,
-                  },
-                  toolCallId: fakeRequestDocsToolCall.toolCallId,
-                },
-              ]),
-              toolChoice,
-              tools: {
-                ...tools,
-                request_documentation: {
-                  description: 'Request additional documentation if needed',
-                  schema: requestDocumentationSchema,
-                },
-              },
-            })
-            .pipe(
-              withoutTokenCountEvents(),
-              map((generateEvent) => {
-                if (isChatCompletionMessageEvent(generateEvent)) {
-                  const correctedContent = generateEvent.content?.replaceAll(
-                    INLINE_ESQL_QUERY_REGEX,
-                    (_match, query) => {
-                      const correction = correctCommonEsqlMistakes(query);
-                      if (correction.isCorrection) {
-                        logger.debug(
-                          `Corrected query, from: \n${correction.input}\nto:\n${correction.output}`
-                        );
-                      }
-                      return '```esql\n' + correction.output + '\n```';
-                    }
-                  );
+              });
+            }
+          }
 
-                  return {
-                    ...generateEvent,
-                    content: correctedContent,
-                  };
-                }
+          return of(generateEvent);
+        })
+      )
+    );
+  };
+};
 
-                return generateEvent;
-              }),
-              switchMap((generateEvent) => {
-                if (isChatCompletionMessageEvent(generateEvent)) {
-                  const onlyToolCall =
-                    generateEvent.toolCalls.length === 1 ? generateEvent.toolCalls[0] : undefined;
+const correctEsqlMistakes = ({
+  content,
+  logger,
+}: {
+  content: string;
+  logger: Pick<Logger, 'debug'>;
+}) => {
+  return content.replaceAll(INLINE_ESQL_QUERY_REGEX, (_match, query) => {
+    const correction = correctCommonEsqlMistakes(query);
+    if (correction.isCorrection) {
+      logger.debug(`Corrected query, from: \n${correction.input}\nto:\n${correction.output}`);
+    }
+    return '```esql\n' + correction.output + '\n```';
+  });
+};
 
-                  if (onlyToolCall?.function.name === 'request_documentation') {
-                    const args = onlyToolCall.function.arguments;
+const requestDocumentation = ({
+  outputApi,
+  system,
+  messages,
+  connectorId,
+  toolOptions: { tools, toolChoice },
+}: {
+  outputApi: InferenceClient['output'];
+  system: string;
+  messages: Message[];
+  connectorId: string;
+  toolOptions: ToolOptions;
+}) => {
+  const hasTools = !isEmpty(tools) && toolChoice !== ToolChoiceType.none;
 
-                    return askLlmToRespond({
-                      documentationRequest: {
-                        commands: args.commands,
-                        functions: args.functions,
-                      },
-                    });
-                  }
-                }
-
-                return of(generateEvent);
-              })
-            )
-        );
-      }
-
-      return client
-        .output('request_documentation', {
-          connectorId,
-          system: systemMessage,
-          previousMessages: messages,
-          input: `Based on the previous conversation, request documentation
+  return outputApi('request_documentation', {
+    connectorId,
+    system,
+    previousMessages: messages,
+    input: `Based on the previous conversation, request documentation
         from the ES|QL handbook to help you get the right information
         needed to generate a query.
 
         Examples for functions and commands:
-        Do you need to group data? Request \`STATS\`.
-        Extract data? Request \`DISSECT\` AND \`GROK\`.
-        Convert a column based on a set of conditionals? Request \`EVAL\` and \`CASE\`.
+        - Do you need to group data? Request \`STATS\`.
+        - Extract data? Request \`DISSECT\` AND \`GROK\`.
+        - Convert a column based on a set of conditionals? Request \`EVAL\` and \`CASE\`.
 
         ${
           hasTools
@@ -233,22 +300,9 @@ export function naturalLanguageToEsql<TToolOptions extends ToolOptions>({
             : ''
         }
       `,
-          schema: requestDocumentationSchema,
-        })
-        .pipe(
-          withoutOutputUpdateEvents(),
-          switchMap((documentationEvent) => {
-            return askLlmToRespond({
-              documentationRequest: {
-                commands: documentationEvent.output.commands,
-                functions: documentationEvent.output.functions,
-              },
-            });
-          })
-        );
-    })
-  );
-}
+    schema: requestDocumentationSchema,
+  }).pipe(withoutOutputUpdateEvents());
+};
 
 const createFakeTooCall = (
   commands: string[] | undefined,
