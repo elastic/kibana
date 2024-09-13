c3bcaaec9fa0d81fc3f34a29f5ba414d56a6ab7e
Maxim Palenov <maxim.palenov@elastic.co>
2024-09-13 07:46:38 +0200

    integrate state and components

---
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/three_way_diff_tab.tsx
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { EuiSpacer } from '@elastic/eui';
+import type {
+  RuleUpgradeState,
+  SetFieldResolvedValueFn,
+} from '../../../../rule_management_ui/components/rules_table/upgrade_prebuilt_rules_table/use_prebuilt_rules_upgrade_state';
+import { UpgradeInfoBar } from './upgrade_info_bar';
+import { RuleDiffSection } from './rule_diff_section';
+
+interface ThreeWayDiffTabProps {
+  ruleUpgradeState: RuleUpgradeState;
+  setFieldResolvedValue: SetFieldResolvedValueFn;
+}
+
+export function ThreeWayDiffTab({
+  ruleUpgradeState,
+  setFieldResolvedValue,
+}: ThreeWayDiffTabProps): JSX.Element {
+  return (
+    <>
+      <EuiSpacer size="s" />
+      <UpgradeInfoBar ruleUpgradeState={ruleUpgradeState} />
+      <EuiSpacer size="s" />
+      <RuleDiffSection ruleUpgradeState={ruleUpgradeState} />
+    </>
+  );
+}
