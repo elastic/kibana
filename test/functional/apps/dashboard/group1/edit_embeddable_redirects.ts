589a24476d7b320e5ff8826440a8b225009c00c8
Catherine Liu <catherine.liu@elastic.co>
2024-09-16 09:44:40 -0700

    Remove exclusive suite

--- test/functional/apps/dashboard/group1/edit_embeddable_redirects.ts
+++ test/functional/apps/dashboard/group1/edit_embeddable_redirects.ts
@@ -17,7 +17,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const dashboardPanelActions = getService('dashboardPanelActions');
   const dashboardAddPanel = getService('dashboardAddPanel');
 
-  describe.only('edit embeddable redirects', () => {
+  describe('edit embeddable redirects', () => {
     before(async () => {
       await kibanaServer.savedObjects.cleanStandardList();
       await kibanaServer.importExport.load(
@@ -36,10 +36,10 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       await kibanaServer.savedObjects.cleanStandardList();
     });
 
-    // it('redirects via save and return button after edit', async () => {
-    //   await dashboardPanelActions.clickEdit();
-    //   await visualize.saveVisualizationAndReturn();
-    // });
+    it('redirects via save and return button after edit', async () => {
+      await dashboardPanelActions.clickEdit();
+      await visualize.saveVisualizationAndReturn();
+    });
 
     it('redirects via save as button after edit, renaming itself', async () => {
       const newTitle = 'wowee, looks like I have a new title';
