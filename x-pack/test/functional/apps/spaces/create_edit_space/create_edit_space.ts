0fe3f4bf84fc024174495aa81f892cf28a4fa3a9
Timothy Sullivan <tsullivan@elastic.co>
2024-09-16 16:07:23 -0700

    Reorganize create_edit_space tests to dedicated folder

--- x-pack/test/functional/apps/spaces/create_edit_space/create_edit_space.ts
+++ x-pack/test/functional/apps/spaces/create_edit_space/create_edit_space.ts
@@ -7,7 +7,7 @@
 
 import expect from '@kbn/expect';
 import { faker } from '@faker-js/faker';
-import { FtrProviderContext } from '../../ftr_provider_context';
+import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const kibanaServer = getService('kibanaServer');
