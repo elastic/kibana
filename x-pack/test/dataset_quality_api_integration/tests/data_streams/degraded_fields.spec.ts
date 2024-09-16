1356d8d42bbb0367cc8ad666f3c44291ee07716a
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2024-09-16 14:32:51 +0200

    Add test for lastBackingIndex in degraded field specs

--- x-pack/test/dataset_quality_api_integration/tests/data_streams/degraded_fields.spec.ts
+++ x-pack/test/dataset_quality_api_integration/tests/data_streams/degraded_fields.spec.ts
@@ -10,6 +10,8 @@ import expect from '@kbn/expect';
 import { DegradedField } from '@kbn/dataset-quality-plugin/common/api_types';
 import { DatasetQualityApiClientKey } from '../../common/config';
 import { FtrProviderContext } from '../../common/ftr_provider_context';
+import { rolloverDataStream } from '../../utils';
+import { getBackingIndexNameWithoutLastPart } from './es_utils';
 
 const MORE_THAN_1024_CHARS =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
@@ -18,6 +20,7 @@ export default function ApiTest({ getService }: FtrProviderContext) {
   const registry = getService('registry');
   const synthtrace = getService('logSynthtraceEsClient');
   const datasetQualityApiClient = getService('datasetQualityApiClient');
+  const esClient = getService('es');
   const start = '2024-05-22T08:00:00.000Z';
   const end = '2024-05-23T08:02:00.000Z';
   const type = 'logs';
@@ -129,6 +132,56 @@ export default function ApiTest({ getService }: FtrProviderContext) {
         )?.timeSeries;
 
         expect(logLevelTimeSeries).to.eql(logsTimeSeriesData);
+      });
+
+      it('should return the backing index where the ignored field was last seen', async () => {
+        await rolloverDataStream(esClient, `${type}-${degradedFieldDataset}-${namespace}`);
+        await synthtrace.index([
+          timerange(start, end)
+            .interval('1m')
+            .rate(1)
+            .generator((timestamp) =>
+              log
+                .create()
+                .message('This is a error message')
+                .logLevel(MORE_THAN_1024_CHARS)
+                .timestamp(timestamp)
+                .dataset(degradedFieldDataset)
+                .namespace(namespace)
+                .defaults({
+                  'log.file.path': '/error.log',
+                  'service.name': serviceName + 1,
+                })
+            ),
+        ]);
+
+        const resp = await callApiAs(
+          'datasetQualityMonitorUser',
+          `${type}-${degradedFieldDataset}-${namespace}`
+        );
+
+        const logLevelLastBackingIndex = resp.body.degradedFields.find(
+          (dFields) => dFields.name === 'log.level'
+        )?.indexFieldWasLastPresentIn;
+
+        const traceIdLastBackingIndex = resp.body.degradedFields.find(
+          (dFields) => dFields.name === 'trace.id'
+        )?.indexFieldWasLastPresentIn;
+
+        expect(logLevelLastBackingIndex).to.be(
+          `${getBackingIndexNameWithoutLastPart({
+            type,
+            dataset: degradedFieldDataset,
+            namespace,
+          })}-000002`
+        );
+        expect(traceIdLastBackingIndex).to.be(
+          `${getBackingIndexNameWithoutLastPart({
+            type,
+            dataset: degradedFieldDataset,
+            namespace,
+          })}-000001`
+        );
       });
     });
   });
