f678118b195ea81c81c606a4f202d3b394a726f4
Kerry Gallagher <471693+Kerry350@users.noreply.github.com>
2024-09-16 13:00:37 +0100

    Make Log sources setting disabled

--- x-pack/plugins/observability_solution/observability_ai_assistant_management/public/routes/components/settings_tab/settings_tab.test.tsx
+++ x-pack/plugins/observability_solution/observability_ai_assistant_management/public/routes/components/settings_tab/settings_tab.test.tsx
@@ -6,19 +6,11 @@
  */
 
 import React from 'react';
-import { fireEvent, waitFor } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import { render } from '../../../helpers/test_helper';
 import { SettingsTab } from './settings_tab';
-import { aiAssistantLogsIndexPattern } from '@kbn/observability-ai-assistant-plugin/server';
-import { uiSettings } from '../../../../common/ui_settings';
-import { uiSettings as logsDataAccessUiSettings } from '@kbn/logs-data-access-plugin/common/ui_settings';
 
 jest.mock('../../../hooks/use_app_context');
-
-const testUiSettings = {
-  ...uiSettings,
-  ...logsDataAccessUiSettings,
-};
 
 describe('SettingsTab', () => {
   it('should offer a way to configure Observability AI Assistant visibility in apps', () => {
@@ -46,60 +38,6 @@ describe('SettingsTab', () => {
 
     expect(navigateToAppMock).toBeCalledWith('management', {
       path: '/insightsAndAlerting/triggersActionsConnectors/connectors',
-    });
-  });
-
-  describe('allows updating the AI Assistant settings', () => {
-    const windowLocationReloadMock = jest.fn();
-    const windowLocationOriginal = window.location;
-    const settingsClientSet = jest.fn();
-
-    beforeEach(async () => {
-      Object.defineProperty(window, 'location', {
-        value: {
-          reload: windowLocationReloadMock,
-        },
-        writable: true,
-      });
-
-      const { getByTestId, container } = render(<SettingsTab />, {
-        coreStart: {
-          settings: {
-            client: {
-              set: settingsClientSet,
-              getAll: () => testUiSettings,
-              validateValue: () => Promise.resolve({ successfulValidation: true, valid: true }),
-            },
-          },
-        },
-      });
-
-      await waitFor(() => expect(container.querySelector('.euiLoadingSpinner')).toBeNull());
-      fireEvent.change(
-        getByTestId(`management-settings-editField-${aiAssistantLogsIndexPattern}`),
-        {
-          target: { value: 'observability-ai-assistant-*' },
-        }
-      );
-
-      await waitFor(() =>
-        expect(
-          getByTestId('observabilityAiAssistantManagementBottomBarActionsButton')
-        ).toBeInTheDocument()
-      );
-
-      fireEvent.click(getByTestId('observabilityAiAssistantManagementBottomBarActionsButton'));
-      await waitFor(() => expect(windowLocationReloadMock).toHaveBeenCalledTimes(1));
-    });
-
-    afterEach(() => {
-      window.location = windowLocationOriginal;
-    });
-
-    it('calls the settings client with correct args', async () => {
-      expect(settingsClientSet).toBeCalledWith(aiAssistantLogsIndexPattern, [
-        'observability-ai-assistant-*',
-      ]);
     });
   });
 });
