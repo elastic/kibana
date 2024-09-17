0fc191aa320db4b7949f1c2d47a273a5362029c8
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2024-09-17 18:54:36 +0200

    [NL-to-ESQL] refactor and improve the task's workflow (#192850)
    ## Summary
    
    Some cleanup and minor enhancements, just to get my hands on that part
    of the code.
    
    evaluation framework was run against gemini1.5, claude-sonnet and GPT-4,
    with a few improvements
    
    ### Cleanup
    
    - Refactor the code to improve readability and maintainability
    
    ### Improvements
    
    - Add support for keyword aliases (turns out, some models asks for
    `STATS...BY` and not `STATS`)
    - Add (naive for now) support for suggestion (to try to influence the
    model on using some function instead of others, e.g group by time with
    BUCKET instead of DATE_TRUNC)
    - Generate "this command does not exist" documentation when the model
    request a missing command (help making it understand it shouldn't use
    the command, e.g gpt-4 was hallucinating a `REVERSE` command)
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

---
+++ x-pack/plugins/inference/server/tasks/nl_to_esql/actions/request_documentation.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { isEmpty } from 'lodash';
+import { InferenceClient, withoutOutputUpdateEvents } from '../../..';
+import { Message } from '../../../../common';
+import { ToolChoiceType, ToolOptions } from '../../../../common/chat_complete/tools';
+import { requestDocumentationSchema } from './shared';
+
+export const requestDocumentation = ({
+  outputApi,
+  system,
+  messages,
+  connectorId,
+  toolOptions: { tools, toolChoice },
+}: {
+  outputApi: InferenceClient['output'];
+  system: string;
+  messages: Message[];
+  connectorId: string;
+  toolOptions: ToolOptions;
+}) => {
+  const hasTools = !isEmpty(tools) && toolChoice !== ToolChoiceType.none;
+
+  return outputApi('request_documentation', {
+    connectorId,
+    system,
+    previousMessages: messages,
+    input: `Based on the previous conversation, request documentation
+        from the ES|QL handbook to help you get the right information
+        needed to generate a query.
+
+        Examples for functions and commands:
+        - Do you need to group data? Request \`STATS\`.
+        - Extract data? Request \`DISSECT\` AND \`GROK\`.
+        - Convert a column based on a set of conditionals? Request \`EVAL\` and \`CASE\`.
+
+        ${
+          hasTools
+            ? `### Tools
+
+        The following tools will be available to be called in the step after this.
+
+        \`\`\`json
+        ${JSON.stringify({
+          tools,
+          toolChoice,
+        })}
+        \`\`\``
+            : ''
+        }
+      `,
+    schema: requestDocumentationSchema,
+  }).pipe(withoutOutputUpdateEvents());
+};
