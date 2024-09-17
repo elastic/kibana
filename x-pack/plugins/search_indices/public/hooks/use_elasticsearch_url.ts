e348d5bf185243994a48df38ff0920c892abf384
Sander Philipse <sander.philipse@elastic.co>
2024-09-17 15:05:46 +0200

    Fix elasticsearch url in search_indices

--- x-pack/plugins/search_indices/public/hooks/use_elasticsearch_url.ts
+++ x-pack/plugins/search_indices/public/hooks/use_elasticsearch_url.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import { useEffect, useState } from 'react';
 import { useKibana } from './use_kibana';
 
 import { ELASTICSEARCH_URL_PLACEHOLDER } from '../constants';
@@ -13,6 +14,14 @@ export const useElasticsearchUrl = (): string => {
   const {
     services: { cloud },
   } = useKibana();
+
+  const [elasticsearchUrl, setElasticsearchUrl] = useState<string>(ELASTICSEARCH_URL_PLACEHOLDER);
+
+  useEffect(() => {
+    cloud?.fetchElasticsearchConfig().then((config) => {
+      setElasticsearchUrl(config?.elasticsearchUrl || ELASTICSEARCH_URL_PLACEHOLDER);
+    });
+  }, [cloud]);
 
   return cloud?.elasticsearchUrl ?? ELASTICSEARCH_URL_PLACEHOLDER;
 };
