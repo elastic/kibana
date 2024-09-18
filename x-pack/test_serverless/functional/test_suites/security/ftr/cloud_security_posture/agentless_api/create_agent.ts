df246a531cb0f03cf305ce63f2029b358f07adb4
Sean Rathier <sean.rathier@elastic.co>
2024-09-17 20:14:16 -0400

    omit loggin in as admin if running in cloud

--- x-pack/test_serverless/functional/test_suites/security/ftr/cloud_security_posture/agentless_api/create_agent.ts
+++ x-pack/test_serverless/functional/test_suites/security/ftr/cloud_security_posture/agentless_api/create_agent.ts
@@ -27,11 +27,15 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   describe('Agentless API Serverless', function () {
     let mockApiServer: http.Server;
     let cisIntegration: typeof pageObjects.cisAddIntegration;
+    const isCloud = !!process.env.TEST_CLOUD;
 
     before(async () => {
       // If process.env.TEST_CLOUD is set, then the test is running in the Serverless Quality Gates
       // and this MSW server will be listening for a request that will never come.
       mockApiServer = mockAgentlessApiService.listen(8089); // Start the usage api mock server on port 8089
+      if (!isCloud) {
+        await pageObjects.svlCommonPage.loginAsAdmin();
+      }
       cisIntegration = pageObjects.cisAddIntegration;
     });
 
