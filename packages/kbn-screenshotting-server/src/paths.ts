5a522bfe14bc6d06c20bc337477fd53f7c538973
Tim Sullivan <tsullivan@users.noreply.github.com>
2024-09-18 13:22:18 -0700

    [8.15] [Screenshotting] Server package for stateless code (#188390) (#193231)
    # Backport
    
    This will backport the following commits from `main` to `8.15`:
    - [[Screenshotting] Server package for stateless code
    (#188390)](https://github.com/elastic/kibana/pull/188390)
    
    <!--- Backport version: 8.9.8 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Tim
    Sullivan","email":"tsullivan@users.noreply.github.com"},"sourceCommit":{"committedDate":"2024-08-22T18:03:03Z","message":"[Screenshotting]
    Server package for stateless code (#188390)\n\n## Summary\r\n\r\nThis PR
    moves code that is usually impacted by a Puppeteer/Chromium\r\nupgrade
    to a package. The same work is happening to a lesser degree in\r\nthe
    7.17 branch. Together, these changes will ease the manual labor
    of\r\nbackporting the upgrades from main to
    7.17.\r\n\r\n---------\r\n\r\nCo-authored-by: kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"bf673d9c565c288223483c4c622f37f745ce70e6","branchLabelMapping":{"^v8.16.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","v8.14.0","v8.15.0","v8.16.0","backport:version"],"number":188390,"url":"https://github.com/elastic/kibana/pull/188390","mergeCommit":{"message":"[Screenshotting]
    Server package for stateless code (#188390)\n\n## Summary\r\n\r\nThis PR
    moves code that is usually impacted by a Puppeteer/Chromium\r\nupgrade
    to a package. The same work is happening to a lesser degree in\r\nthe
    7.17 branch. Together, these changes will ease the manual labor
    of\r\nbackporting the upgrades from main to
    7.17.\r\n\r\n---------\r\n\r\nCo-authored-by: kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"bf673d9c565c288223483c4c622f37f745ce70e6"}},"sourceBranch":"main","suggestedTargetBranches":["8.14","8.15"],"targetPullRequestStates":[{"branch":"8.14","label":"v8.14.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.15","label":"v8.15.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v8.16.0","labelRegex":"^v8.16.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/188390","number":188390,"mergeCommit":{"message":"[Screenshotting]
    Server package for stateless code (#188390)\n\n## Summary\r\n\r\nThis PR
    moves code that is usually impacted by a Puppeteer/Chromium\r\nupgrade
    to a package. The same work is happening to a lesser degree in\r\nthe
    7.17 branch. Together, these changes will ease the manual labor
    of\r\nbackporting the upgrades from main to
    7.17.\r\n\r\n---------\r\n\r\nCo-authored-by: kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"bf673d9c565c288223483c4c622f37f745ce70e6"}}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: Eyo O. Eyo <7893459+eokoneyo@users.noreply.github.com>

--- packages/kbn-screenshotting-server/src/paths.ts
+++ packages/kbn-screenshotting-server/src/paths.ts
@@ -1,8 +1,9 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * 2.0 and the Server Side Public License, v 1; you may not use this file except
+ * in compliance with, at your election, the Elastic License 2.0 or the Server
+ * Side Public License, v 1.
  */
 
 import path from 'path';
@@ -101,7 +102,7 @@ export class ChromiumArchivePaths {
   ];
 
   // zip files get downloaded to a .chromium directory in the kibana root
-  public readonly archivesPath = path.resolve(__dirname, '../../../../../../.chromium');
+  public readonly archivesPath = path.resolve(__dirname, '../../../.chromium');
 
   public find(platform: string, architecture: string, packages: PackageInfo[] = this.packages) {
     return packages.find((p) => p.platform === platform && p.architecture === architecture);
