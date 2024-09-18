e3f3c68e8d2e106740db9b4612df867c583eb512
Cauê Marcondes <55978943+cauemarcondes@users.noreply.github.com>
2024-09-18 16:06:13 +0100

    [Inventory][ECO] Entities table (#193272)
    Real data:
    <img width="1237" alt="Screenshot 2024-09-18 at 14 23 17"
    src="https://github.com/user-attachments/assets/ecc496aa-1c43-4c3c-9ac8-d6e4e6cb8aad">
    
    Storybook:
    <img width="1256" alt="Screenshot 2024-09-18 at 14 23 22"
    src="https://github.com/user-attachments/assets/03d9f940-7b3f-4aea-9221-42b1c07119d1">
    
    Tooltips:
    <img width="1250" alt="Screenshot 2024-09-18 at 13 49 19"
    src="https://github.com/user-attachments/assets/dc99b4cc-4eba-4815-8892-8e3fe7a041bb">
    
    
    - Use ESQL to fetch the top 500 entities sorted by last seen property.
    - Display 20 entities per page.
    - Sorting is handles by the server and saved on the URL
    - Current page is saved on the URL
    - Filter entities types `service`, `host` or `container`
    - Filter only entities from the built in definition
    - LIMITATION: The EuiGrid doesn't have an embedded loading state, for
    now, I'm switching the entire view to display a loading spinner while
    data is being fetched.
    - PLUS: Storybook created with mock data.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/observability_solution/inventory/public/pages/inventory_page/index.tsx
+++ x-pack/plugins/observability_solution/inventory/public/pages/inventory_page/index.tsx
@@ -5,12 +5,63 @@
  * 2.0.
  */
 import React from 'react';
+import { useAbortableAsync } from '@kbn/observability-utils/hooks/use_abortable_async';
+import { EuiDataGridSorting } from '@elastic/eui';
 import { EntitiesGrid } from '../../components/entities_grid';
+import { useKibana } from '../../hooks/use_kibana';
+import { useInventoryParams } from '../../hooks/use_inventory_params';
+import { useInventoryRouter } from '../../hooks/use_inventory_router';
 
 export function InventoryPage() {
+  const {
+    services: { inventoryAPIClient },
+  } = useKibana();
+  const { query } = useInventoryParams('/');
+  const { sortDirection, sortField, pageIndex } = query;
+  const inventoryRoute = useInventoryRouter();
+
+  const { value = { entities: [] }, loading } = useAbortableAsync(
+    ({ signal }) => {
+      return inventoryAPIClient.fetch('GET /internal/inventory/entities', {
+        params: {
+          query: {
+            sortDirection,
+            sortField,
+          },
+        },
+        signal,
+      });
+    },
+    [inventoryAPIClient, sortDirection, sortField]
+  );
+
+  function handlePageChange(nextPage: number) {
+    inventoryRoute.push('/', {
+      path: {},
+      query: { ...query, pageIndex: nextPage },
+    });
+  }
+
+  function handleSortChange(sorting: EuiDataGridSorting['columns'][0]) {
+    inventoryRoute.push('/', {
+      path: {},
+      query: {
+        ...query,
+        sortField: sorting.id,
+        sortDirection: sorting.direction,
+      },
+    });
+  }
+
   return (
-    <div>
-      <EntitiesGrid />
-    </div>
+    <EntitiesGrid
+      entities={value.entities}
+      loading={loading}
+      sortDirection={sortDirection}
+      sortField={sortField}
+      onChangePage={handlePageChange}
+      onChangeSort={handleSortChange}
+      pageIndex={pageIndex}
+    />
   );
 }
