2e97e813c59c97734b5db9d0751cac4c5bb27df2
Milosz Marcinkowski <milosz.marcinkowski@elastic.co>
2024-09-18 15:26:26 +0200

    'dependency spans' mapping

--- x-pack/plugins/observability_solution/apm/server/utils/es_fields_mappings.ts
+++ x-pack/plugins/observability_solution/apm/server/utils/es_fields_mappings.ts
@@ -5,12 +5,56 @@
  * 2.0.
  */
 
+import { AgentName } from '@kbn/elastic-agent-utils';
+import { EventOutcome } from '../../common/event_outcome';
+
 export const metadataForDependencyMapping = (fields: Partial<Record<string, unknown[]>>) => {
   return {
     span: {
       type: normalizeValue<string>(fields['span.type']),
       subtype: normalizeValue<string>(fields['span.subtype']),
     },
+  };
+};
+
+export const transactionsForDependencySpansMapping = (
+  fields: Partial<Record<string, unknown[]>>
+) => {
+  return {
+    transaction: {
+      id: normalizeValue<string>(fields['transaction.id']),
+      name: normalizeValue<string>(fields['transaction.name']),
+      type: normalizeValue<string>(fields['transaction.type']),
+    },
+  };
+};
+
+export const topDependencySpansMapping = (fields: Partial<Record<string, unknown[]>>) => {
+  return {
+    transaction: {
+      id: normalizeValue<string>(fields['transaction.id']),
+    },
+    span: {
+      id: normalizeValue<string>(fields['span.id']),
+      name: normalizeValue<string>(fields['span.name']),
+      duration: {
+        us: normalizeValue<number>(fields['span.duration.us']),
+      },
+    },
+    trace: {
+      id: normalizeValue<string>(fields['trace.id']),
+    },
+    service: {
+      name: normalizeValue<string>(fields['service.name']),
+      environment: normalizeValue<string>(fields['service.environment']),
+    },
+    agent: {
+      name: normalizeValue<AgentName>(fields['agent.name']),
+    },
+    event: {
+      outcome: normalizeValue<EventOutcome>(fields['event.outcome']),
+    },
+    '@timestamp': normalizeValue<string>(fields['@timestamp']),
   };
 };
 
