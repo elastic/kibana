0614796682c3cdd11d4d219b3a4d1ea2b9b73ac3
Sean Rathier <sean.rathier@elastic.co>
2024-09-18 16:32:06 -0400

    Added login as admin for serverless

--- x-pack/test_serverless/functional/test_suites/security/ftr/cloud_security_posture/agentless_api/create_agent.ts
+++ x-pack/test_serverless/functional/test_suites/security/ftr/cloud_security_posture/agentless_api/create_agent.ts
@@ -33,9 +33,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       // If process.env.TEST_CLOUD is set, then the test is running in the Serverless Quality Gates
       // and this MSW server will be listening for a request that will never come.
       mockApiServer = mockAgentlessApiService.listen(8089); // Start the usage api mock server on port 8089
-      if (!isCloud) {
-        await pageObjects.svlCommonPage.loginAsAdmin();
-      }
+      await pageObjects.svlCommonPage.loginAsAdmin();
       cisIntegration = pageObjects.cisAddIntegration;
     });
 
