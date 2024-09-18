5f4419406f4a7b978d1851353dd75acd6a8987cf
rshen91 <rshen@elastic.co>
2024-09-18 13:18:57 -0600

    working test

--- src/plugins/dashboard/public/dashboard_app/dashboard_app.test.tsx
+++ src/plugins/dashboard/public/dashboard_app/dashboard_app.test.tsx
@@ -8,14 +8,14 @@
  */
 
 import { render, waitFor } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
+import { MemoryHistory, createMemoryHistory } from 'history';
 import React, { useEffect } from 'react';
 import type { DashboardRendererProps } from '../dashboard_container/external_api/dashboard_renderer';
 import { buildMockDashboard } from '../mocks';
 import { DashboardApp } from './dashboard_app';
-import { DashboardApi } from '..';
 
 import * as dashboardRendererStuff from '../dashboard_container/external_api/lazy_dashboard_renderer';
+import { DashboardApi } from '..';
 
 /* These tests circumvent the need to test the router and legacy code
 /* the dashboard app will be passed the expanded panel id from the DashboardRouter through mountApp()
@@ -24,12 +24,21 @@ import * as dashboardRendererStuff from '../dashboard_container/external_api/laz
 
 describe('Dashboard App', () => {
   const mockDashboard = buildMockDashboard();
-  const mockHistory = createMemoryHistory();
-  const historySpy = jest.spyOn(mockHistory, 'replace');
+  const expandedPanelIdSpy = jest.spyOn(mockDashboard.expandedPanelId, 'next');
+  let mockHistory: MemoryHistory;
+  let historySpy: jest.SpyInstance;
 
-  beforeAll(() => {
+  beforeEach(() => {
+    mockHistory = createMemoryHistory();
+    historySpy = jest.spyOn(mockHistory, 'replace');
+
+    /**
+     * Mock the LazyDashboardRenderer component to avoid rendering the actual dashboard
+     * and hitting errors that aren't relevant
+     */
     jest
       .spyOn(dashboardRendererStuff, 'LazyDashboardRenderer')
+      // we need overwrite the onApiAvailable prop to get the dashboard Api in the dashboard app
       .mockImplementation(({ onApiAvailable }: DashboardRendererProps) => {
         useEffect(() => {
           onApiAvailable?.(mockDashboard as DashboardApi);
@@ -37,17 +46,21 @@ describe('Dashboard App', () => {
 
         return <div>Test renderer</div>;
       });
-  });
 
-  beforeEach(() => {
+    // reset the mock history calls per test
     historySpy.mockClear();
+    // reset the expanded panel id between tests
+    expandedPanelIdSpy.mockClear();
   });
 
   it('test the default behavior without an expandedPanel id passed as a prop to the DashboardApp', async () => {
+    // default conditions with no expanded panel id and no urls being passed with expanded panel ID in the url
     render(<DashboardApp redirectTo={jest.fn()} history={mockHistory} />);
 
     await waitFor(() => {
+      // this value should be undefined by default
       expect(mockDashboard.expandedPanelId.getValue()).toBe(undefined);
+      // history should not be called
       expect(historySpy).toHaveBeenCalledTimes(0);
       expect(mockHistory.location.pathname).toBe('/');
     });
@@ -62,32 +75,31 @@ describe('Dashboard App', () => {
     });
   });
 
-  // it.skip('test that the expanded panel behavior subject and history is called when passed as a prop to the DashboardApp', async () => {
-  //   const expandedPanelIdSpy = jest.spyOn(mockDashboard.expandedPanelId, 'next');
-  //   const historySpy = jest.spyOn(mockHistory, 'replace');
+  it('test that the expanded panel behavior subject and history is called when passed as a prop to the DashboardApp', async () => {
+    // this tests a user getting a url from another user that has an expanded panel on the dashboard
+    // test someone giving a url with an expanded panel id, as if it goes through the router
+    mockHistory.push('/create/456');
 
-  //   render(
-  //     <DashboardApp
-  //       savedDashboardId=""
-  //       redirectTo={jest.fn()}
-  //       history={mockHistory}
-  //       expandedPanelId="456"
-  //     />
-  //   );
+    render(
+      <DashboardApp
+        savedDashboardId=""
+        redirectTo={jest.fn()}
+        history={mockHistory}
+        expandedPanelId="456"
+      />
+    );
 
-  //   await waitFor(() => {
-  //     expect(expandedPanelIdSpy).toHaveBeenCalledTimes(1);
-  //     expect(historySpy).toHaveBeenCalledTimes(0);
-  //     // render a dashboard with an expanded panel initially,
-  //     // react testing library limitation on showing the expanded panel subscription
-  //     // expect(expandedPanelIdSpy).toHaveBeenCalledWith('456');
-  //   });
+    await waitFor(() => {
+      expect(expandedPanelIdSpy).toHaveBeenCalledTimes(1);
+      // history replace should not be called until minimizing the panel
+      expect(historySpy).toHaveBeenCalledTimes(0);
+      expect(mockDashboard.getExpandedPanelId());
+      expect(mockHistory.location.pathname).toBe('/create/456');
+    });
 
-  //   // // simulate minimizing a panel
-  //   // act(() => {
-  //   //   mockDashboard.expandedPanelId.next(undefined);
-  //   // });
-
-  //   // expect(mockHistory.location.pathname).toBe('/create');
-  // });
+    // simulate minimizing a panel
+    mockDashboard.expandedPanelId.next(undefined);
+    expect(historySpy).toHaveBeenCalledTimes(1);
+    expect(mockHistory.location.pathname).toBe('/create');
+  });
 });
