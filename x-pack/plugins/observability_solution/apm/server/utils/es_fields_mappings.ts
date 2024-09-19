a6244f58c8fbc72450a319bdf6290df16d5c36b0
Milosz Marcinkowski <milosz.marcinkowski@elastic.co>
2024-09-19 11:37:28 +0200

    `span` mappings

--- x-pack/plugins/observability_solution/apm/server/utils/es_fields_mappings.ts
+++ x-pack/plugins/observability_solution/apm/server/utils/es_fields_mappings.ts
@@ -6,6 +6,7 @@
  */
 
 import { AgentName } from '@kbn/elastic-agent-utils';
+import { Span } from '@kbn/apm-types';
 import { EventOutcome } from '../../common/event_outcome';
 
 export const metadataForDependencyMapping = (fields: Partial<Record<string, unknown[]>>) => {
@@ -55,6 +56,59 @@ export const topDependencySpansMapping = (fields: Partial<Record<string, unknown
       outcome: normalizeValue<EventOutcome>(fields['event.outcome']),
     },
     '@timestamp': normalizeValue<string>(fields['@timestamp']),
+  };
+};
+
+// todo(milosz): test with otel APM POC, mapping format wasn't confirmed with `fields`
+export const spanMapping = (fields: Partial<Record<string, unknown[]>>): Span => {
+  return {
+    parent: {
+      id: normalizeValue<string>(fields['parent.id']),
+    },
+    observer: {
+      type: normalizeValue<string>(fields['observer.type']),
+      version: normalizeValue<string>(fields['observer.version']),
+      version_major: normalizeValue<number>(fields['observer.version_major']),
+    },
+    agent: {
+      name: normalizeValue<AgentName>(fields['agent.name']),
+      version: normalizeValue<string>(fields['agent.version']),
+    },
+    trace: {
+      id: normalizeValue<string>(fields['trace.id']),
+    },
+    '@timestamp': normalizeValue<string>(fields['@timestamp']),
+    service: {
+      name: normalizeValue<string>(fields['service.name']),
+      environment: normalizeValue<string>(fields['service.environment']),
+    },
+    event: {
+      outcome: normalizeValue<EventOutcome>(fields['event.outcome']),
+    },
+    processor: {
+      name: normalizeValue<'transaction'>(fields['processor.name']),
+      event: normalizeValue<'span'>(fields['processor.event']),
+    },
+    transaction: {
+      id: normalizeValue<string>(fields['transaction.id']),
+    },
+    span: {
+      duration: {
+        us: normalizeValue<number>(fields['span.duration.us']),
+      },
+      subtype: normalizeValue<string>(fields['span.subtype']),
+      name: normalizeValue<string>(fields['span.name']),
+      destination: {
+        service: {
+          resource: normalizeValue<string>(fields['span.destination.service.resource']),
+        },
+      },
+      id: normalizeValue<string>(fields['span.id']),
+      type: normalizeValue<string>(fields['span.type']),
+    },
+    timestamp: {
+      us: normalizeValue<number>(fields['timestamp.us']),
+    },
   };
 };
 
