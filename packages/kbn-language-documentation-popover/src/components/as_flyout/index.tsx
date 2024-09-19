2e776662158f27447e9d65c013dfdaff1b523313
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-09-19 17:10:39 +0200

    [ES|QL] Enhances the inline documentation experience (#192156)
    ## Summary
    
    Closes https://github.com/elastic/kibana/issues/166907
    
    This PR:
    
    1. Changes the inline docs implementation to a flyout for Discover
    
    <img width="1256" alt="image"
    src="https://github.com/user-attachments/assets/3665c73a-82d5-49b9-88c3-e129eda63885">
    
    2. Adds the flyout to open from the help menu and removes it from the
    editor footer
    
    3. For the inline editing changes the docs to be rendered as a component
    inside the editor (exactly as the history component)
    
    Note: The documentation in the inline editing has limited space (same
    problem has the history component). I need to sync with Ryan to see what
    we can do, I am exploring some ideas in follow up PRs
    
    <img width="647" alt="image"
    src="https://github.com/user-attachments/assets/2fa9fcc3-2a07-4bea-b675-5ae6578696d3">
    
    4. Moves the esql docs to the language package and the script associated
    to this
    
    ### Follow up
    - The language package should be renamed as the `popover` makes sense
    only for the formula implementation now. I will do it after this PR gets
    merged
    - See how we can give more space to the docs / history container when in
    inline mode
    
    ### Note
    The async bundle got increased as we want the ES|QL docs in unified
    search too. It wont be a problem as is loaded asynchronously.
    
    ### Checklist
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [ ] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [ ] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [ ] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Ryan Keairns <contactryank@gmail.com>
    Co-authored-by: Drew Tate <andrew.tate@elastic.co>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    (cherry picked from commit dfbd7de3f53cae4b81d8643283f106cbf16e3415)

---
+++ packages/kbn-language-documentation-popover/src/components/as_flyout/index.tsx
@@ -0,0 +1,117 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+import React, { useCallback, useEffect, useState, useRef, useMemo } from 'react';
+import {
+  EuiFlyout,
+  useEuiTheme,
+  EuiFlyoutBody,
+  EuiFlyoutHeader,
+  EuiTitle,
+  EuiSpacer,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { getFilteredGroups } from '../../utils/get_filtered_groups';
+import { DocumentationMainContent, DocumentationNavigation } from '../shared';
+import { getESQLDocsSections } from '../../sections';
+import type { LanguageDocumentationSections } from '../../types';
+
+interface DocumentationFlyoutProps {
+  isHelpMenuOpen: boolean;
+  onHelpMenuVisibilityChange: (status: boolean) => void;
+  searchInDescription?: boolean;
+  linkToDocumentation?: string;
+}
+
+function DocumentationFlyout({
+  searchInDescription,
+  linkToDocumentation,
+  isHelpMenuOpen,
+  onHelpMenuVisibilityChange,
+}: DocumentationFlyoutProps) {
+  const [documentationSections, setDocumentationSections] =
+    useState<LanguageDocumentationSections>();
+
+  const { euiTheme } = useEuiTheme();
+  const DEFAULT_WIDTH = euiTheme.base * 34;
+
+  const [selectedSection, setSelectedSection] = useState<string | undefined>();
+  const [searchText, setSearchText] = useState('');
+
+  const scrollTargets = useRef<Record<string, HTMLElement>>({});
+
+  const onNavigationChange = useCallback((selectedOptions) => {
+    setSelectedSection(selectedOptions.length ? selectedOptions[0].label : undefined);
+    if (selectedOptions.length) {
+      const scrollToElement = scrollTargets.current[selectedOptions[0].label];
+      scrollToElement.scrollIntoView();
+    }
+  }, []);
+
+  useEffect(() => {
+    onHelpMenuVisibilityChange(isHelpMenuOpen ?? false);
+  }, [isHelpMenuOpen, onHelpMenuVisibilityChange]);
+
+  useEffect(() => {
+    async function getDocumentation() {
+      const sections = await getESQLDocsSections();
+      setDocumentationSections(sections);
+    }
+    if (!documentationSections) {
+      getDocumentation();
+    }
+  }, [documentationSections]);
+
+  const filteredGroups = useMemo(() => {
+    return getFilteredGroups(searchText, searchInDescription, documentationSections, 1);
+  }, [documentationSections, searchText, searchInDescription]);
+
+  return (
+    <>
+      {isHelpMenuOpen && (
+        <EuiFlyout
+          ownFocus
+          onClose={() => onHelpMenuVisibilityChange(false)}
+          aria-labelledby="esqlInlineDocumentationFlyout"
+          type="push"
+          size={DEFAULT_WIDTH}
+          paddingSize="m"
+        >
+          <EuiFlyoutHeader hasBorder>
+            <EuiTitle size="s">
+              <h3>
+                {i18n.translate('languageDocumentationPopover.documentationFlyoutTitle', {
+                  defaultMessage: 'ES|QL quick reference',
+                })}
+              </h3>
+            </EuiTitle>
+            <EuiSpacer size="m" />
+            <DocumentationNavigation
+              searchText={searchText}
+              setSearchText={setSearchText}
+              onNavigationChange={onNavigationChange}
+              filteredGroups={filteredGroups}
+              linkToDocumentation={linkToDocumentation}
+              selectedSection={selectedSection}
+            />
+          </EuiFlyoutHeader>
+          <EuiFlyoutBody>
+            <DocumentationMainContent
+              searchText={searchText}
+              scrollTargets={scrollTargets}
+              filteredGroups={filteredGroups}
+              sections={documentationSections}
+            />
+          </EuiFlyoutBody>
+        </EuiFlyout>
+      )}
+    </>
+  );
+}
+
+export const LanguageDocumentationFlyout = React.memo(DocumentationFlyout);
