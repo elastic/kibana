c48f534caf226055723c0489253e8335332ba4ba
Maxim Palenov <maxim.palenov@elastic.co>
2024-09-19 13:25:06 +0200

    add auto-committing step

--- .buildkite/scripts/steps/openapi_bundling/final_merge.sh
+++ .buildkite/scripts/steps/openapi_bundling/final_merge.sh
@@ -7,3 +7,6 @@ source .buildkite/scripts/common/util.sh
 echo --- Merge Kibana OpenAPI specs
 
 (cd oas_docs && make api-docs && make api-docs-lint)
+(cd oas_docs && make api-docs-staging && make api-docs-lint)
+
+check_for_changed_files "make api-docs && make api-docs-staging" true
