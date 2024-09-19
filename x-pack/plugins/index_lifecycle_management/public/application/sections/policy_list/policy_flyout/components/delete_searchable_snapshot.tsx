ebde681943996596b08b38ba81e3ff7a1d1de4b7
Yulia Cech <yulia.cech@elastic.co>
2024-09-19 17:42:13 +0200

    [ILM] Update delete phase description

--- x-pack/plugins/index_lifecycle_management/public/application/sections/policy_list/policy_flyout/components/delete_searchable_snapshot.tsx
+++ x-pack/plugins/index_lifecycle_management/public/application/sections/policy_list/policy_flyout/components/delete_searchable_snapshot.tsx
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import { i18nTexts as i18nTextsFlyout } from './i18n_texts';
 import { SerializedDeletePhase } from '../../../../../../common/types';
 import { i18nTexts } from '../../../edit_policy/i18n_texts';
 import { ActionDescription } from './action_description';
@@ -15,7 +16,12 @@ export const DeleteSearchableSnapshot = ({ phase, phases }: ActionComponentProps
   const phaseConfig = phases[phase];
   const deleteSearchableSnapshot = (phaseConfig as SerializedDeletePhase)?.actions.delete
     ?.delete_searchable_snapshot;
-  return deleteSearchableSnapshot ? (
-    <ActionDescription title={i18nTexts.editPolicy.deleteSearchableSnapshotLabel} />
-  ) : null;
+  return (
+    <ActionDescription
+      title={i18nTexts.editPolicy.deleteSearchableSnapshotLabel}
+      descriptionItems={[
+        Boolean(deleteSearchableSnapshot) ? i18nTextsFlyout.yes : i18nTextsFlyout.no,
+      ]}
+    />
+  );
 };
