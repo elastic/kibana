618c4d06855fbf7e5b662ab16eb06a080548786e
Maryam Saeidi <maryam.saeidi@elastic.co>
2024-09-20 15:40:13 +0200

    [Alert details page] Add related alerts tab to the alert details page (#193263)
    Closes #188014
    
    ## Summary
    
    This PR adds a related alerts tab to the alert details page and saves
    the filters in the URL. The logic for filtering alerts:
    
    - Alerts that have tags similar to the existing alert (one or more)
    - Alerts for similar group by/source fields ("or" logic)
    - Either at the root level (e.g. host.hostname: 'host-1'), or have this
    information available in `kibana.alert.group.value`.
    
    By default, we filter to only show active alerts.
    
    |Alert|Related alerts|
    |---|---|
    
    |![image](https://github.com/user-attachments/assets/2117d41b-85d6-4d09-836f-d0a0858f0f89)|![image](https://github.com/user-attachments/assets/df3cb061-122e-451c-94e5-1bbcda74cabc)|
    
    |![image](https://github.com/user-attachments/assets/4308afdf-5940-49b6-80a3-bfe9de23c805)|![image](https://github.com/user-attachments/assets/43011507-1148-4188-aa12-f8692c0e6ccf)|
    
    #### Follow-up work
    I will create some follow-up tickets for these topics.
    - Selecting `Rule name` as the default group by and opening the first
    group
    - Considering adding an event timeline to this tab
    - Showing the number of active alerts in the tab
    
    (cherry picked from commit 96dd4c78637cfdff4c33391eebc68587985312b7)

---
+++ x-pack/plugins/observability_solution/observability/public/pages/alert_details/helpers/get_related_alerts_query.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { Group } from '../../../../common/typings';
+
+export interface Query {
+  query: string;
+  language: string;
+}
+
+export const getRelatedAlertKuery = (tags?: string[], groups?: Group[]): string | undefined => {
+  const tagKueries: string[] =
+    tags?.map((tag) => {
+      return `tags: "${tag}"`;
+    }) ?? [];
+  const groupKueries =
+    (groups &&
+      groups.map(({ field, value }) => {
+        return `(${field}: "${value}" or kibana.alert.group.value: "${value}")`;
+      })) ??
+    [];
+
+  const tagKueriesStr = tagKueries.length > 0 ? [`(${tagKueries.join(' or ')})`] : [];
+  const groupKueriesStr = groupKueries.length > 0 ? [`${groupKueries.join(' or ')}`] : [];
+  const kueries = [...tagKueriesStr, ...groupKueriesStr];
+
+  return kueries.length ? kueries.join(' or ') : undefined;
+};
