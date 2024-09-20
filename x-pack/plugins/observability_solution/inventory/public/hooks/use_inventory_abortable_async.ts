31bf014c21dd8aab53305fef91e97b4c8ec12f4e
Caue Marcondes <caue.marcondes@elastic.co>
2024-09-20 14:14:46 +0100

    error

--- x-pack/plugins/observability_solution/inventory/public/hooks/use_inventory_abortable_async.ts
+++ x-pack/plugins/observability_solution/inventory/public/hooks/use_inventory_abortable_async.ts
@@ -6,7 +6,11 @@
  */
 import { useAbortableAsync } from '@kbn/observability-utils/hooks/use_abortable_async';
 import { i18n } from '@kbn/i18n';
+import { IHttpFetchError, ResponseErrorBody } from '@kbn/core-http-browser';
 import { useKibana } from './use_kibana';
+
+const getDetailsFromErrorResponse = (error: IHttpFetchError<ResponseErrorBody>) =>
+  error.body?.message ?? error.response?.statusText;
 
 export function useInventoryAbortableAsync<T>(...args: Parameters<typeof useAbortableAsync<T>>) {
   const {
@@ -15,11 +19,16 @@ export function useInventoryAbortableAsync<T>(...args: Parameters<typeof useAbor
   const response = useAbortableAsync(...args);
 
   if (response.error) {
+    const errorMessage =
+      'response' in response.error
+        ? getDetailsFromErrorResponse(response.error as IHttpFetchError<ResponseErrorBody>)
+        : response.error.message;
+
     notifications.toasts.addDanger({
       title: i18n.translate('xpack.inventory.apiCall.error.title', {
         defaultMessage: `Error while fetching resource`,
       }),
-      text: response.error.message,
+      text: errorMessage,
     });
   }
 
