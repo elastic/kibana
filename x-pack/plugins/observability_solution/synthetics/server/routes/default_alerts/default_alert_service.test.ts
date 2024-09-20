3418925759b6a2a3c8424002f47cb79bb4a61742
Justin Kambic <jk@elastic.co>
2024-09-20 16:00:49 -0400

    Add more alerts.

--- x-pack/plugins/observability_solution/synthetics/server/routes/default_alerts/default_alert_service.test.ts
+++ x-pack/plugins/observability_solution/synthetics/server/routes/default_alerts/default_alert_service.test.ts
@@ -5,11 +5,14 @@
  * 2.0.
  */
 
+import { SanitizedRule } from '@kbn/alerting-types';
+import { omit } from 'lodash';
 import {
   SYNTHETICS_STATUS_RULE,
   SYNTHETICS_TLS_RULE,
 } from '../../../common/constants/synthetics_alerts';
 import { DefaultAlertService } from './default_alert_service';
+import { DYNAMIC_SETTINGS_DEFAULTS } from '../../constants/settings';
 
 describe('DefaultAlertService', () => {
   describe('getSettings', () => {
@@ -169,6 +172,295 @@ describe('DefaultAlertService', () => {
       const result = await service.setupTlsRule();
       expect(service.createDefaultAlertIfNotExist).not.toHaveBeenCalled();
       expect(result).toBeUndefined();
+    });
+  });
+
+  describe('existing alerts', () => {
+    function setUpExistingAlerts<T>(
+      alertOverride?: Partial<SanitizedRule<T>>,
+      getRulesClientMocks = {}
+    ) {
+      const getRulesClient = jest.fn();
+      const mockAlert: any = alertOverride ?? {
+        actions: [{ alertsFilter: { query: { kql: 'some kql', filters: [] } } }],
+        systemActions: [{ id: 'some system action', actionTypeId: 'actionTypeId', params: {} }],
+        ruleTypeId: 'xpack.synthetics.alerts.monitorStatus',
+        alertTypeId: 'xpack.synthetics.alerts.monitorStatus',
+        id: '123',
+      };
+      const find = jest.fn();
+      find.mockResolvedValue({
+        data: [mockAlert],
+      });
+      getRulesClient.mockReturnValue({ find, ...getRulesClientMocks });
+
+      return { getRulesClient, mockAlert };
+    }
+
+    function formatMockAlertResult(mockAlert: any) {
+      return {
+        ...omit(mockAlert, 'systemActions'),
+        actions: [...mockAlert.actions, ...mockAlert.systemActions],
+      };
+    }
+
+    afterEach(() => jest.resetAllMocks());
+
+    describe('getExistingAlert', () => {
+      it('returns alert if exists', async () => {
+        const { getRulesClient, mockAlert } = setUpExistingAlerts();
+        const service = new DefaultAlertService(
+          { alerting: { getRulesClient } } as any,
+          {} as any,
+          {} as any
+        );
+        const result = await service.getExistingAlert('xpack.synthetics.alerts.monitorStatus');
+        expect(result).toEqual(formatMockAlertResult(mockAlert));
+      });
+
+      it('returns undefined if alert does not exist', async () => {
+        const find = jest.fn().mockResolvedValue({ data: [] });
+        const getRulesClient = jest.fn();
+        getRulesClient.mockReturnValue({ find });
+        const service = new DefaultAlertService(
+          { alerting: { getRulesClient } } as any,
+          {} as any,
+          {} as any
+        );
+        const result = await service.getExistingAlert('xpack.synthetics.alerts.monitorStatus');
+        expect(result).toBeUndefined();
+      });
+    });
+    describe('createDefaultAlertIfNotExist', () => {
+      it('returns alert if exists', async () => {
+        const { getRulesClient, mockAlert } = setUpExistingAlerts();
+        const service = new DefaultAlertService(
+          { alerting: { getRulesClient } } as any,
+          {} as any,
+          {} as any
+        );
+        const alert = await service.createDefaultAlertIfNotExist(
+          'xpack.synthetics.alerts.monitorStatus',
+          'name',
+          '1m'
+        );
+        expect(alert).toEqual(formatMockAlertResult(mockAlert));
+        expect(getRulesClient).toHaveBeenCalled();
+      });
+
+      it('creates alert if does not exist', async () => {
+        const sampleAction = { alertsFilter: { query: { kql: 'some kql', filters: [] } } };
+        const find = jest.fn().mockResolvedValue({ data: [] });
+        const create = jest.fn().mockResolvedValue({
+          actions: [sampleAction],
+          systemActions: [],
+          id: '123',
+          alertTypeId: 'testalertid',
+        });
+        const getActionsClient = jest.fn();
+        getActionsClient.mockReturnValue({
+          getAll: jest
+            .fn()
+            .mockResolvedValue([{ id: 'id', actionTypeId: 'actionTypeId', name: 'action name' }]),
+        });
+        const getRulesClient = jest.fn();
+        getRulesClient.mockReturnValue({ find, create });
+        const service = new DefaultAlertService(
+          { actions: { getActionsClient }, alerting: { getRulesClient } } as any,
+          {} as any,
+          {} as any
+        );
+        service.settings = { defaultConnectors: ['slack', 'email'] } as any;
+        const result = await service.createDefaultAlertIfNotExist(
+          'xpack.synthetics.alerts.monitorStatus',
+          'name',
+          '1m'
+        );
+        expect(result).toEqual({
+          actions: [sampleAction],
+          id: '123',
+          alertTypeId: 'testalertid',
+          ruleTypeId: 'testalertid',
+        });
+      });
+    });
+
+    function setUpUpdateTest<T>(mockAlert?: Partial<SanitizedRule<T>>) {
+      const update = jest.fn().mockResolvedValue({
+        alertTypeId: 'test-alert-type-id',
+        actions: [{ id: 'id', actionTypeId: 'actionTypeId', name: 'action name' }],
+        systemActions: [{ id: 'sys-id', actionTypeId: 'actionTypeId', name: 'action name' }],
+        updatedAlertField: 'value',
+      });
+      const { getRulesClient } = setUpExistingAlerts(
+        mockAlert ?? { schedule: { interval: '1m' } },
+        { update }
+      );
+      const getConfig = jest.fn().mockReturnValue({ minimumScheduleInterval: { value: '3m' } });
+      const server = {
+        alerting: {
+          getConfig,
+        },
+      } as any;
+      const getActionsClient = jest.fn();
+      const getAll = jest
+        .fn()
+        .mockResolvedValue([{ id: 'id', actionTypeId: 'actionTypeId', name: 'action name' }]);
+      getActionsClient.mockReturnValue({
+        getAll,
+      });
+      const context = { actions: { getActionsClient }, alerting: { getRulesClient } };
+
+      return {
+        context,
+        server,
+        mocks: { update, getRulesClient, getConfig, getActionsClient, getAll },
+      };
+    }
+
+    describe('updateStatusRule', () => {
+      it('updates the rule if it is enabled', async () => {
+        const {
+          context,
+          server,
+          mocks: { update, getAll },
+        } = setUpUpdateTest({
+          id: 'test-alert-id',
+          name: 'test-alert-name',
+          tags: ['test-alert-tags'],
+          schedule: { interval: '1m' },
+          params: { param: 'value' },
+        });
+        const service = new DefaultAlertService(context as any, server as any, {} as any);
+        service.settings = { defaultConnectors: ['slack', 'email'] } as any;
+        const result = await service.updateStatusRule(true);
+        expect(result).toEqual({
+          actions: [
+            { actionTypeId: 'actionTypeId', id: 'id', name: 'action name' },
+            { actionTypeId: 'actionTypeId', id: 'sys-id', name: 'action name' },
+          ],
+          alertTypeId: 'test-alert-type-id',
+          ruleTypeId: 'test-alert-type-id',
+          updatedAlertField: 'value',
+        });
+        expect(update).toHaveBeenCalledTimes(1);
+        expect(update.mock.calls[0][0]).toEqual({
+          data: {
+            actions: [],
+            name: 'test-alert-name',
+            params: { param: 'value' },
+            schedule: { interval: '3m' },
+            tags: ['test-alert-tags'],
+          },
+          id: 'test-alert-id',
+        });
+        expect(getAll).toHaveBeenCalled();
+      });
+
+      it('deletes the rule if it is disabled', async () => {
+        const server = {
+          alerting: {
+            getConfig: jest.fn().mockReturnValue({ minimumScheduleInterval: { value: '3m' } }),
+          },
+        } as any;
+        const bulkDeleteRules = jest.fn();
+        const { getRulesClient } = setUpExistingAlerts(undefined, { bulkDeleteRules });
+        const service = new DefaultAlertService(
+          { alerting: { getRulesClient } } as any,
+          server as any,
+          {} as any
+        );
+        await service.updateStatusRule(false);
+        expect(bulkDeleteRules).toHaveBeenCalled();
+        expect(bulkDeleteRules.mock.calls[0][0]).toEqual({
+          filter:
+            'alert.attributes.alertTypeId:"xpack.synthetics.alerts.monitorStatus" AND alert.attributes.tags:"SYNTHETICS_DEFAULT_ALERT"',
+        });
+      });
+    });
+
+    describe('updateTlsRule', () => {
+      it('updates the rule if it is enabled', async () => {
+        const { context, server } = setUpUpdateTest();
+        const service = new DefaultAlertService(context as any, server as any, {} as any);
+        service.settings = { defaultConnectors: ['slack', 'email'] } as any;
+        const result = await service.updateTlsRule(true);
+        expect(result).toEqual({
+          actions: [
+            { actionTypeId: 'actionTypeId', id: 'id', name: 'action name' },
+            { actionTypeId: 'actionTypeId', id: 'sys-id', name: 'action name' },
+          ],
+          alertTypeId: 'test-alert-type-id',
+          ruleTypeId: 'test-alert-type-id',
+          updatedAlertField: 'value',
+        });
+      });
+
+      it('creates the rule if it does not exist', async () => {
+        const { context, server } = setUpUpdateTest();
+        const service = new DefaultAlertService(context as any, server as any, {} as any);
+        service.settings = { defaultConnectors: ['slack', 'email'] } as any;
+        const getExistingAlertMock = jest.fn().mockResolvedValue(undefined);
+        service.getExistingAlert = getExistingAlertMock;
+        const createDefaultAlertIfNotExistMock = jest.fn();
+        service.createDefaultAlertIfNotExist = createDefaultAlertIfNotExistMock;
+        const result = await service.updateTlsRule(true);
+        expect(result).toBeUndefined();
+        expect(service.getExistingAlert).toHaveBeenCalled();
+        expect(service.createDefaultAlertIfNotExist).toHaveBeenCalled();
+        expect(getExistingAlertMock.mock.calls[0][0]).toBe('xpack.synthetics.alerts.tls');
+        expect(createDefaultAlertIfNotExistMock.mock.calls[0]).toStrictEqual([
+          'xpack.synthetics.alerts.tls',
+          'Synthetics internal TLS rule',
+          '3m',
+        ]);
+      });
+
+      it('deletes the rule if it is disabled', async () => {
+        const server = {
+          alerting: {
+            getConfig: jest.fn().mockReturnValue({ minimumScheduleInterval: { value: '3m' } }),
+          },
+        } as any;
+        const bulkDeleteRules = jest.fn();
+        const { getRulesClient } = setUpExistingAlerts(undefined, { bulkDeleteRules });
+        const service = new DefaultAlertService(
+          { alerting: { getRulesClient } } as any,
+          server as any,
+          {} as any
+        );
+        await service.updateTlsRule(false);
+        expect(bulkDeleteRules).toHaveBeenCalled();
+        expect(bulkDeleteRules.mock.calls[0][0]).toEqual({
+          filter:
+            'alert.attributes.alertTypeId:"xpack.synthetics.alerts.tls" AND alert.attributes.tags:"SYNTHETICS_DEFAULT_ALERT"',
+        });
+      });
+    });
+  });
+
+  describe('getActionConnectors', () => {
+    it('fetches settings if not set', async () => {
+      const getActionsClient = jest.fn();
+      const getAll = jest.fn().mockResolvedValue([{ id: 'id', actionTypeId: 'actionTypeId' }]);
+      getActionsClient.mockReturnValue({
+        getAll,
+      });
+      const service = new DefaultAlertService(
+        { actions: { getActionsClient } } as any,
+        {} as any,
+        { get: jest.fn() } as any
+      );
+      const connectors = await service.getActionConnectors();
+      expect(connectors).toEqual({
+        actionConnectors: [{ id: 'id', actionTypeId: 'actionTypeId' }],
+        settings: {
+          ...DYNAMIC_SETTINGS_DEFAULTS,
+          defaultStatusRuleEnabled: true,
+          defaultTLSRuleEnabled: true,
+        },
+      });
+      expect(getAll).toHaveBeenCalled();
     });
   });
 });
