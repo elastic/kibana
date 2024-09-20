776120123e4495989dc84e7baf8b8f0c2a3f3537
Kevin Delemme <kevin.delemme@elastic.co>
2024-09-20 10:27:35 -0400

    Extract Tag component

--- x-pack/plugins/observability_solution/investigate_app/public/pages/details/components/investigation_header/investigation_header.tsx
+++ x-pack/plugins/observability_solution/investigate_app/public/pages/details/components/investigation_header/investigation_header.tsx
@@ -9,6 +9,8 @@ import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import React from 'react';
 import { useInvestigation } from '../../contexts/investigation_context';
 import { AlertDetailsButton } from './alert_details_button';
+import { InvestigationTag } from '../../../../components/investigation_tag/investigation_tag';
+import { InvestigationStatusBadge } from '../../../../components/investigation_status_badge/investigation_status_badge';
 
 export function InvestigationHeader() {
   const { investigation } = useInvestigation();
@@ -22,7 +24,22 @@ export function InvestigationHeader() {
       <EuiFlexItem>
         <AlertDetailsButton />
       </EuiFlexItem>
-      <EuiFlexItem>{investigation.title}</EuiFlexItem>
+      <EuiFlexGroup direction="column" gutterSize="s" alignItems="flexStart">
+        <EuiFlexItem>{investigation.title}</EuiFlexItem>
+
+        <EuiFlexGroup direction="row" gutterSize="s">
+          <EuiFlexItem grow={false}>
+            <InvestigationStatusBadge status={investigation.status} />
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiFlexGroup responsive={false} wrap gutterSize="s">
+              {investigation.tags.map((tag) => (
+                <InvestigationTag key={tag} tag={tag} />
+              ))}
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFlexGroup>
     </EuiFlexGroup>
   );
 }
