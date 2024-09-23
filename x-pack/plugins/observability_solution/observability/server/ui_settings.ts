886d009418821ae4b6a22d7630bab0c484f137c2
Cauê Marcondes <55978943+cauemarcondes@users.noreply.github.com>
2024-09-23 16:54:50 +0100

    [APM] Turn 'fast filter' on by default and ensure tech preview badge shows when turned on (#193710)
    closes https://github.com/elastic/kibana/issues/193406
    
    <img width="1057" alt="Screenshot 2024-09-23 at 12 37 41"
    src="https://github.com/user-attachments/assets/189eef68-e5ab-4472-a924-8c417960b09b">
    <img width="1077" alt="Screenshot 2024-09-23 at 12 37 56"
    src="https://github.com/user-attachments/assets/c0129aeb-2bb3-4055-a6f9-ccbbdb608451">
    <img width="1202" alt="Screenshot 2024-09-23 at 12 38 04"
    src="https://github.com/user-attachments/assets/ec2b1fa7-0342-440b-966c-965d24ef61c2">

--- x-pack/plugins/observability_solution/observability/server/ui_settings.ts
+++ x-pack/plugins/observability_solution/observability/server/ui_settings.ts
@@ -372,8 +372,8 @@ export const uiSettings: Record<string, UiSettings> = {
       }
     ),
     schema: schema.boolean(),
-    value: false,
-    requiresPageReload: false,
+    value: true,
+    requiresPageReload: true,
     type: 'boolean',
   },
   [apmAWSLambdaPriceFactor]: {
