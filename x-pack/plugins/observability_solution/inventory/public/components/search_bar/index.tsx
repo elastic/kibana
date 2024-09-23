f8b420e13e6ba23ddcc12f6af22fd324d72035f3
Caue Marcondes <caue.marcondes@elastic.co>
2024-09-23 09:37:54 +0100

    fixing padding

--- x-pack/plugins/observability_solution/inventory/public/components/search_bar/index.tsx
+++ x-pack/plugins/observability_solution/inventory/public/components/search_bar/index.tsx
@@ -77,6 +77,7 @@ export function SearchBar() {
   return (
     <UnifiedSearchBar
       appName="Inventory"
+      displayStyle="inPage"
       showDatePicker={false}
       showFilterBar={false}
       indexPatterns={dataView ? [dataView] : undefined}
