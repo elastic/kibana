7d781d5d7031715464c9fbed52dec79792cd878a
Hanna Tamoudi <hanna.tamoudi@elastic.co>
2024-09-23 15:04:14 +0200

    add mapping to readme

--- x-pack/plugins/integration_assistant/server/integration_builder/fields.ts
+++ x-pack/plugins/integration_assistant/server/integration_builder/fields.ts
@@ -6,35 +6,48 @@
  */
 
 import nunjucks from 'nunjucks';
-
+import { load } from 'js-yaml';
+import { flattenObjectsList } from '../util/samples';
 import { createSync, generateFields, mergeSamples } from '../util';
+import { Docs } from '../../common';
 
 export function createFieldMapping(
   packageName: string,
   dataStreamName: string,
   specificDataStreamDir: string,
   docs: object[]
-): void {
-  createBaseFields(specificDataStreamDir, packageName, dataStreamName);
-  createCustomFields(specificDataStreamDir, docs);
+): object[] {
+  const baseFields = createBaseFields(specificDataStreamDir, packageName, dataStreamName);
+  const customFields = createCustomFields(specificDataStreamDir, docs);
+
+  return mergeFields(baseFields, customFields);
 }
 
 function createBaseFields(
   specificDataStreamDir: string,
   packageName: string,
   dataStreamName: string
-): void {
+): Docs[] {
   const datasetName = `${packageName}.${dataStreamName}`;
   const baseFields = nunjucks.render('base_fields.yml.njk', {
     module: packageName,
     dataset: datasetName,
   });
-
   createSync(`${specificDataStreamDir}/base-fields.yml`, baseFields);
+
+  return load(baseFields) as Docs[];
 }
 
-function createCustomFields(specificDataStreamDir: string, pipelineResults: object[]): void {
+function createCustomFields(specificDataStreamDir: string, pipelineResults: object[]): Docs[] {
   const mergedResults = mergeSamples(pipelineResults);
   const fieldKeys = generateFields(mergedResults);
   createSync(`${specificDataStreamDir}/fields/fields.yml`, fieldKeys);
+
+  return load(fieldKeys) as Docs[];
+}
+
+function mergeFields(baseFields: object[], customFields: object[]): object[] {
+  const fields = [...baseFields, ...customFields];
+
+  return flattenObjectsList(fields);
 }
