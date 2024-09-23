601c8a8f3ce734b752eb1d2ed9ca2ca083b0a5e3
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2024-09-23 13:48:05 +0200

    [8.x] [ES|QL] Renames the language package to a more generic name (#193517) (#193691)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[ES|QL] Renames the language package to a more generic name
    (#193517)](https://github.com/elastic/kibana/pull/193517)
    
    <!--- Backport version: 8.9.8 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Stratoula
    Kalafateli","email":"efstratia.kalafateli@elastic.co"},"sourceCommit":{"committedDate":"2024-09-23T07:58:53Z","message":"[ES|QL]
    Renames the language package to a more generic name (#193517)\n\n##
    Summary\r\n\r\nRenames the language-documentation-popover to
    language-documentation as\r\nit stores additional layouts:\r\n-
    popover\r\n- flyout\r\n-
    inline","sha":"9d0d41ce871be49f4d18b8bbfad7791173c6ebc8","branchLabelMapping":{"^v9.0.0$":"main","^v8.16.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","v9.0.0","backport:prev-minor","Feature:ES|QL","Team:ESQL","v8.16.0"],"number":193517,"url":"https://github.com/elastic/kibana/pull/193517","mergeCommit":{"message":"[ES|QL]
    Renames the language package to a more generic name (#193517)\n\n##
    Summary\r\n\r\nRenames the language-documentation-popover to
    language-documentation as\r\nit stores additional layouts:\r\n-
    popover\r\n- flyout\r\n-
    inline","sha":"9d0d41ce871be49f4d18b8bbfad7791173c6ebc8"}},"sourceBranch":"main","suggestedTargetBranches":["8.x"],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","labelRegex":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/193517","number":193517,"mergeCommit":{"message":"[ES|QL]
    Renames the language package to a more generic name (#193517)\n\n##
    Summary\r\n\r\nRenames the language-documentation-popover to
    language-documentation as\r\nit stores additional layouts:\r\n-
    popover\r\n- flyout\r\n-
    inline","sha":"9d0d41ce871be49f4d18b8bbfad7791173c6ebc8"}},{"branch":"8.x","label":"v8.16.0","labelRegex":"^v8.16.0$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->

---
+++ packages/kbn-language-documentation/src/components/as_inline/index.test.tsx
@@ -0,0 +1,97 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React from 'react';
+import { screen, render, fireEvent, waitFor } from '@testing-library/react';
+import { Markdown } from '@kbn/shared-ux-markdown';
+import { LanguageDocumentationInline } from '.';
+
+const mockMarkDownDescription = () => (
+  <Markdown markdownContent="Section three item 1 blah blah blah" />
+);
+
+jest.mock('../../sections', () => {
+  const module = jest.requireActual('../../sections');
+  return {
+    ...module,
+    getESQLDocsSections: () => ({
+      groups: [
+        {
+          label: 'Section one',
+          description: 'Section 1 description',
+          items: [],
+        },
+        {
+          label: 'Section two',
+          items: [
+            {
+              label: 'Section two item 1',
+              description: 'Section two item 1 description',
+            },
+            {
+              label: 'Section two item 2',
+              description: 'Section two item 2 description',
+            },
+          ],
+        },
+        {
+          label: 'Section three',
+          items: [
+            {
+              label: 'Section three item 1',
+              description: mockMarkDownDescription(),
+            },
+            {
+              label: 'Section three item 2',
+              description: 'Section three item 2 description',
+            },
+          ],
+        },
+      ],
+      initialSection: <span>Here is the initial section</span>,
+    }),
+  };
+});
+
+describe('###Documentation flyout component', () => {
+  const renderInlineComponent = (searchInDescription = false) => {
+    return render(<LanguageDocumentationInline searchInDescription={searchInDescription} />);
+  };
+  it('has a header element for navigation through the sections', () => {
+    renderInlineComponent();
+    expect(screen.getByTestId('language-documentation-navigation-search')).toBeInTheDocument();
+    expect(screen.getByTestId('language-documentation-navigation-dropdown')).toBeInTheDocument();
+  });
+
+  it('contains the two last sections', async () => {
+    renderInlineComponent();
+    await waitFor(() => {
+      expect(screen.getByText('Section two')).toBeInTheDocument();
+      expect(screen.getByText('Section three')).toBeInTheDocument();
+    });
+  });
+
+  it('contains the correct section if user updates the search input', async () => {
+    renderInlineComponent();
+    const input = screen.getByTestId('language-documentation-navigation-search');
+    fireEvent.change(input, { target: { value: 'two' } });
+    await waitFor(() => {
+      expect(screen.getByText('Section two')).toBeInTheDocument();
+    });
+  });
+
+  it('contains the correct section if user updates the search input with a text that exist in the description', async () => {
+    renderInlineComponent(true);
+    const input = screen.getByTestId('language-documentation-navigation-search');
+    fireEvent.change(input, { target: { value: 'blah' } });
+    await waitFor(() => {
+      expect(screen.getByText('Section three')).toBeInTheDocument();
+    });
+  });
+});
