ee116db5128eef8d55c8008686bb546fe1727fb8
Nikita Khristinin <nkhristinin@gmail.com>
2024-09-24 14:21:45 +0200

    revert interval

--- x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/utils/utils.ts
+++ x-pack/plugins/security_solution/server/lib/detection_engine/rule_types/utils/utils.ts
@@ -68,7 +68,7 @@ import type {
 import { ENABLE_CCS_READ_WARNING_SETTING } from '../../../../../common/constants';
 import type { GenericBulkCreateResponse } from '../factories';
 
-export const MAX_RULE_GAP_RATIO = 1;
+export const MAX_RULE_GAP_RATIO = 4;
 
 export const hasReadIndexPrivileges = async (args: {
   privileges: Privilege;
