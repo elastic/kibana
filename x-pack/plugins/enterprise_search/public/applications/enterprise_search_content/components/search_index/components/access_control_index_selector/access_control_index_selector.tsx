8f8be5c087cab69b0ca68cab9fae2d8f728ea5cb
José Luis González <joseluisgj@gmail.com>
2024-09-25 09:45:59 +0200

    AccessControlIndexSelector component can be more customized

--- x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/components/access_control_index_selector/access_control_index_selector.tsx
+++ x-pack/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/components/access_control_index_selector/access_control_index_selector.tsx
@@ -7,76 +7,110 @@
 
 import React from 'react';
 
-import { EuiFlexGrid, EuiFlexItem, EuiSuperSelect, EuiText, EuiTitle } from '@elastic/eui';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiSuperSelect,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
 export interface AccessControlSelectorOption {
   description: string;
+  error: boolean;
   title: string;
   value: 'content-index' | 'access-control-index';
 }
 
-const indexSelectorOptions: AccessControlSelectorOption[] = [
-  {
-    description: i18n.translate(
-      'xpack.enterpriseSearch.content.searchIndex.documents.selector.contentIndex.description',
-      {
-        defaultMessage: 'Browse content fields',
-      }
-    ),
-    title: i18n.translate(
-      'xpack.enterpriseSearch.content.searchIndex.documents.selector.contentIndex.title',
-      {
-        defaultMessage: 'Content index',
-      }
-    ),
-    value: 'content-index',
-  },
-  {
-    description: i18n.translate(
-      'xpack.enterpriseSearch.content.searchIndex.documents.selector.accessControl.description',
-      {
-        defaultMessage: 'Browse document level security fields',
-      }
-    ),
-    title: i18n.translate(
-      'xpack.enterpriseSearch.content.searchIndex.documents.selector.accessControl.title',
-      {
-        defaultMessage: 'Access control index',
-      }
-    ),
-    value: 'access-control-index',
-  },
-];
-
 interface IndexSelectorProps {
+  accessControlIndexDescription?: string;
+  accessControlIndexTitle?: string;
+  accessSyncError?: boolean;
+  contentIndexDescription?: string;
+  contentIndexTitle?: string;
+  contentSyncError?: boolean;
+  fullWidth?: boolean;
   onChange(value: AccessControlSelectorOption['value']): void;
   valueOfSelected?: AccessControlSelectorOption['value'];
 }
 
 export const AccessControlIndexSelector: React.FC<IndexSelectorProps> = ({
-  valueOfSelected,
+  accessControlIndexDescription,
+  accessControlIndexTitle,
+  accessSyncError,
+  contentIndexDescription,
+  contentIndexTitle,
+  contentSyncError,
   onChange,
+  valueOfSelected,
+  fullWidth,
 }) => {
+  const indexSelectorOptions: AccessControlSelectorOption[] = [
+    {
+      description: i18n.translate(
+        'xpack.enterpriseSearch.content.searchIndex.documents.selector.contentIndex.description',
+        {
+          defaultMessage: contentIndexDescription || 'Browse content fields',
+        }
+      ),
+      error: Boolean(contentSyncError),
+      title: i18n.translate(
+        'xpack.enterpriseSearch.content.searchIndex.documents.selector.contentIndex.title',
+        {
+          defaultMessage: contentIndexTitle || 'Content index',
+        }
+      ),
+      value: 'content-index',
+    },
+    {
+      description: i18n.translate(
+        'xpack.enterpriseSearch.content.searchIndex.documents.selector.accessControl.description',
+        {
+          defaultMessage: accessControlIndexDescription || 'Browse document level security fields',
+        }
+      ),
+      error: Boolean(accessSyncError),
+      title: i18n.translate(
+        'xpack.enterpriseSearch.content.searchIndex.documents.selector.accessControl.title',
+        {
+          defaultMessage: accessControlIndexTitle || 'Access control index',
+        }
+      ),
+      value: 'access-control-index',
+    },
+  ];
+
   return (
     <EuiSuperSelect
+      fullWidth={fullWidth}
       valueOfSelected={valueOfSelected}
       onChange={onChange}
+      prepend={contentSyncError || accessSyncError ? <EuiIcon type={'warning'} /> : undefined}
       options={indexSelectorOptions.map((option) => {
         return {
           dropdownDisplay: (
-            <EuiFlexGrid gutterSize="none">
-              <EuiFlexItem>
-                <EuiTitle size="xs">
-                  <h4>{option.title}</h4>
-                </EuiTitle>
-              </EuiFlexItem>
-              <EuiFlexItem>
-                <EuiText size="xs">
-                  <p>{option.description}</p>
-                </EuiText>
-              </EuiFlexItem>
-            </EuiFlexGrid>
+            <EuiFlexGroup direction="row" alignItems="center" gutterSize="m">
+              {option.error ? (
+                <EuiFlexItem grow={false} align>
+                  <EuiIcon type={'warning'} />{' '}
+                </EuiFlexItem>
+              ) : null}
+
+              <EuiFlexGroup direction="column" gutterSize="none">
+                <EuiFlexItem>
+                  <EuiTitle size="xs">
+                    <h4>{option.title}</h4>
+                  </EuiTitle>
+                </EuiFlexItem>
+                <EuiFlexItem>
+                  <EuiText size="xs">
+                    <p>{option.description}</p>
+                  </EuiText>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexGroup>
           ),
           inputDisplay: option.title,
           value: option.value,
