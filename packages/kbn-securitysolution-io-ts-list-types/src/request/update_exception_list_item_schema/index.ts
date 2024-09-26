70731c87e90b68e1aff6d738520c631eaa15a598
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2024-09-26 16:09:17 +0100

    cleanup

--- packages/kbn-securitysolution-io-ts-list-types/src/request/update_exception_list_item_schema/index.ts
+++ packages/kbn-securitysolution-io-ts-list-types/src/request/update_exception_list_item_schema/index.ts
@@ -7,7 +7,6 @@
  */
 
 import * as t from 'io-ts';
-import { NonEmptyString } from '@kbn/securitysolution-io-ts-types';
 import { NamespaceType } from '../../common/default_namespace';
 import { DefaultUpdateCommentsArray } from '../../common/default_update_comments_array';
 import { EntriesArray } from '../../common/entries';
@@ -21,6 +20,7 @@ import { description } from '../../common/description';
 import { name } from '../../common/name';
 import { _version } from '../../common/underscore_version';
 import { id } from '../../common/id';
+import { item_id } from '../../common/item_id';
 import { meta } from '../../common/meta';
 import { namespace_type } from '../../common/namespace_type';
 import { ExpireTimeOrUndefined, expireTimeOrUndefined } from '../../common';
@@ -40,7 +40,7 @@ export const updateExceptionListItemSchema = t.intersection([
       comments: DefaultUpdateCommentsArray, // defaults to empty array if not set during decode
       expire_time: expireTimeOrUndefined,
       id, // defaults to undefined if not set during decode
-      item_id: t.union([NonEmptyString, t.undefined]),
+      item_id,
       meta, // defaults to undefined if not set during decode
       namespace_type, // defaults to 'single' if not set during decode
       os_types: osTypeArrayOrUndefined, // defaults to empty array if not set during decode
