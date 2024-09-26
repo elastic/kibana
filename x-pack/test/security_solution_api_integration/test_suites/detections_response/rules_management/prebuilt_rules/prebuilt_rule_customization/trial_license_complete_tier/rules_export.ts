13153949ab1d682f3c1d8c43b355dbc96e25cd75
Ryland Herrick <ryalnd@gmail.com>
2024-09-26 16:06:33 -0500

    Adding failing integration tests
    These mainly fail because we don't include prebuilt rules in export.

---
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/prebuilt_rule_customization/trial_license_complete_tier/rules_export.ts
@@ -0,0 +1,244 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from 'expect';
+
+import {
+  BulkActionEditTypeEnum,
+  BulkActionTypeEnum,
+} from '@kbn/security-solution-plugin/common/api/detection_engine';
+import { FtrProviderContext } from '../../../../../../ftr_provider_context';
+import { deleteAllRules } from '../../../../../../../common/utils/security_solution';
+import {
+  binaryToString,
+  createPrebuiltRuleAssetSavedObjects,
+  createRuleAssetSavedObject,
+  deleteAllPrebuiltRuleAssets,
+  getCustomQueryRuleParams,
+  installPrebuiltRules,
+} from '../../../../utils';
+
+const parseNdJson = (ndJson: Buffer): unknown[] =>
+  ndJson
+    .toString()
+    .split('\n')
+    .filter((line) => !!line)
+    .map((line) => JSON.parse(line));
+
+export default ({ getService }: FtrProviderContext): void => {
+  const es = getService('es');
+  const supertest = getService('supertest');
+  const securitySolutionApi = getService('securitySolutionApi');
+  const log = getService('log');
+
+  /**
+   * This test suite is skipped in Serverless MKI environments due to reliance on the
+   * feature flag for prebuilt rule customization.
+   */
+  describe.only('@ess @serverless @skipInServerlessMKI Exporting Rules with Prebuilt Rule Customization', () => {
+    beforeEach(async () => {
+      await deleteAllPrebuiltRuleAssets(es, log);
+      await deleteAllRules(supertest, log);
+    });
+
+    it('exports a set of custom installed rules', async () => {
+      await securitySolutionApi
+        .bulkCreateRules({
+          body: [
+            getCustomQueryRuleParams({ rule_id: 'rule-id-1' }),
+            getCustomQueryRuleParams({ rule_id: 'rule-id-2' }),
+          ],
+        })
+        .expect(200);
+
+      const { body: exportResult } = await securitySolutionApi
+        .exportRules({ query: {}, body: null })
+        .expect(200)
+        .parse(binaryToString);
+
+      const ndJson = parseNdJson(exportResult);
+
+      expect(ndJson).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            rule_id: 'rule-id-1',
+            rule_source: {
+              type: 'internal',
+            },
+          }),
+          expect.objectContaining({
+            rule_id: 'rule-id-2',
+            rule_source: {
+              type: 'internal',
+            },
+          }),
+        ])
+      );
+    });
+
+    describe('with prebuilt rules installed', () => {
+      let ruleAssets: Array<ReturnType<typeof createRuleAssetSavedObject>>;
+
+      beforeEach(async () => {
+        ruleAssets = [
+          createRuleAssetSavedObject({
+            rule_id: '000047bb-b27a-47ec-8b62-ef1a5d2c9e19',
+            tags: ['test-tag'],
+          }),
+          createRuleAssetSavedObject({
+            rule_id: '60b88c41-c45d-454d-945c-5809734dfb34',
+            tags: ['test-tag-2'],
+          }),
+        ];
+        await createPrebuiltRuleAssetSavedObjects(es, ruleAssets);
+        await installPrebuiltRules(es, supertest);
+      });
+
+      it('exports a set of prebuilt installed rules', async () => {
+        const { body: exportResult } = await securitySolutionApi
+          .exportRules({ query: {}, body: null })
+          .expect(200)
+          .parse(binaryToString);
+
+        expect(parseNdJson(exportResult)).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              rule_id: ruleAssets[0]['security-rule'].rule_id,
+              rule_source: {
+                type: 'external',
+                is_customized: false,
+              },
+            }),
+            expect.objectContaining({
+              rule_id: ruleAssets[1]['security-rule'].rule_id,
+              rule_source: {
+                type: 'external',
+                is_customized: false,
+              },
+            }),
+          ])
+        );
+
+        const { body: bulkEditResult } = await securitySolutionApi
+          .performRulesBulkAction({
+            query: {},
+            body: {
+              ids: [ruleAssets[0]['security-rule'].rule_id],
+              action: BulkActionTypeEnum.edit,
+              [BulkActionTypeEnum.edit]: [
+                {
+                  type: BulkActionEditTypeEnum.add_tags,
+                  value: ['new-tag'],
+                },
+              ],
+            },
+          })
+          .expect(200);
+
+        expect(bulkEditResult.attributes.summary).toEqual({
+          failed: 0,
+          skipped: 0,
+          succeeded: 1,
+          total: 1,
+        });
+        expect(bulkEditResult.attributes.results.updated[0].rule_source.is_customized).toEqual(
+          true
+        );
+
+        const { body: secondExportResult } = await securitySolutionApi
+          .exportRules({ query: {}, body: null })
+          .expect(200)
+          .parse(binaryToString);
+
+        expect(parseNdJson(secondExportResult)).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              rule_id: ruleAssets[0]['security-rule'].rule_id,
+              rule_source: {
+                type: 'external',
+                is_customized: true,
+              },
+            }),
+            expect.objectContaining({
+              rule_id: ruleAssets[1]['security-rule'].rule_id,
+              rule_source: {
+                type: 'external',
+                is_customized: false,
+              },
+            }),
+          ])
+        );
+      });
+
+      it('exports a set of custom and prebuilt installed rules', async () => {
+        await securitySolutionApi
+          .bulkCreateRules({
+            body: [
+              getCustomQueryRuleParams({ rule_id: 'rule-id-1' }),
+              getCustomQueryRuleParams({ rule_id: 'rule-id-2' }),
+            ],
+          })
+          .expect(200);
+
+        const { body: exportResult } = await securitySolutionApi
+          .exportRules({ query: {}, body: null })
+          .expect(200)
+          .parse(binaryToString);
+
+        const exportJson = parseNdJson(exportResult);
+        expect(exportJson).toHaveLength(5); // 2 prebuilt rules + 2 custom rules + 1 stats object
+
+        expect(exportJson).toEqual(
+          expect.arrayContaining([
+            expect.objectContaining({
+              rule_id: ruleAssets[0]['security-rule'].rule_id,
+              rule_source: {
+                type: 'external',
+                is_customized: false,
+              },
+            }),
+            expect.objectContaining({
+              rule_id: ruleAssets[1]['security-rule'].rule_id,
+              rule_source: {
+                type: 'external',
+                is_customized: false,
+              },
+            }),
+            expect.objectContaining({
+              rule_id: 'rule-id-1',
+              rule_source: {
+                type: 'internal',
+              },
+            }),
+            expect.objectContaining({
+              rule_id: 'rule-id-2',
+              rule_source: {
+                type: 'internal',
+              },
+            }),
+          ])
+        );
+      });
+    });
+
+    // TODO I discovered this while writing the above tests?
+    it('imports a rule if a duplicate was attempted', async () => {
+      await securitySolutionApi
+        .bulkCreateRules({
+          body: [getCustomQueryRuleParams(), getCustomQueryRuleParams()],
+        })
+        .expect(200);
+
+      const { body: exportResult } = await securitySolutionApi
+        .exportRules({ query: {}, body: null })
+        .expect(200)
+        .parse(binaryToString);
+
+      expect(parseNdJson(exportResult)).toHaveLength(2);
+    });
+  });
+};
