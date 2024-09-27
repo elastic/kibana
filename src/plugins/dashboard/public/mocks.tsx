35186a8a3ed3a7c81d55558b72fbd9460ccd2c5f
Nathan Reese <reese.nathan@gmail.com>
2024-09-27 12:02:22 -0600

    fix test

--- src/plugins/dashboard/public/mocks.tsx
+++ src/plugins/dashboard/public/mocks.tsx
@@ -93,7 +93,13 @@ export function buildMockDashboard({
     undefined,
     undefined,
     undefined,
-    { lastSavedInput: initialInput, lastSavedId: savedObjectId }
+    {
+      anyMigrationRun: false,
+      isEmbeddedExternally: false,
+      lastSavedInput: initialInput,
+      lastSavedId: savedObjectId,
+      managed: false,
+    }
   );
   dashboardContainer?.setControlGroupApi(mockControlGroupApi);
   return dashboardContainer;
