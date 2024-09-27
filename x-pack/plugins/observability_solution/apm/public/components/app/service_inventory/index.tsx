6169c019f56284fbf33aaaccbc28016842f1e92e
iblancof <irene.blanco@elastic.co>
2024-09-27 13:26:19 +0200

    Remove isEnablementPending check in ServiceInventory

--- x-pack/plugins/observability_solution/apm/public/components/app/service_inventory/index.tsx
+++ x-pack/plugins/observability_solution/apm/public/components/app/service_inventory/index.tsx
@@ -5,28 +5,8 @@
  * 2.0.
  */
 import React from 'react';
-import { i18n } from '@kbn/i18n';
-import { EuiEmptyPrompt, EuiLoadingLogo } from '@elastic/eui';
 import { ApmServiceInventory } from './apm_signal_inventory';
-import { useEntityManagerEnablementContext } from '../../../context/entity_manager_context/use_entity_manager_enablement_context';
 
 export function ServiceInventory() {
-  const { isEnablementPending } = useEntityManagerEnablementContext();
-
-  if (isEnablementPending) {
-    return (
-      <EuiEmptyPrompt
-        icon={<EuiLoadingLogo logo="logoObservability" size="xl" />}
-        title={
-          <h2>
-            {i18n.translate('xpack.apm.loadingService', {
-              defaultMessage: 'Loading services',
-            })}
-          </h2>
-        }
-      />
-    );
-  }
-
   return <ApmServiceInventory />;
 }
