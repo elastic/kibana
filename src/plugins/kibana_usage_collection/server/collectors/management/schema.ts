02a6e81887375173ae68a86b3e30afb55e495d24
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2024-10-01 15:53:03 +0200

    fix(infra): update usage collector

--- src/plugins/kibana_usage_collection/server/collectors/management/schema.ts
+++ src/plugins/kibana_usage_collection/server/collectors/management/schema.ts
@@ -506,6 +506,10 @@ export const stackManagementSchema: MakeSchemaFrom<UsageStats> = {
       _meta: { description: 'Non-default value of setting.' },
     },
   },
+  'observability:enableLogsStream': {
+    type: 'boolean',
+    _meta: { description: 'Non-default value of setting.' },
+  },
   'banners:placement': {
     type: 'keyword',
     _meta: { description: 'Non-default value of setting.' },
