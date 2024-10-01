1534834bc85b00bbc23c066223d08ecddfb273e5
James Gowdy <jgowdy@elastic.co>
2024-10-01 13:49:31 +0100

    adding check for prototype

--- x-pack/packages/ml/nested_property/src/set_nested_property.test.ts
+++ x-pack/packages/ml/nested_property/src/set_nested_property.test.ts
@@ -72,5 +72,9 @@ describe('object_utils', () => {
     const test12 = setNestedProperty(getTestObj(), 'the.__proto__', 'update');
     expect(test12.the).toBe('update');
     expect(test12.the.__proto__.update).toBe(undefined);
+
+    const test13 = setNestedProperty(getTestObj(), 'the.prototype', 'update');
+    expect(test13.the).toBe('update');
+    expect(test13.the.prototype.update).toBe(undefined);
   });
 });
