82aa40df1d29bfa79e27b98a3124a358fbfbcae4
Caue Marcondes <caue.marcondes@elastic.co>
2024-10-01 09:07:32 +0100

    increasing version

--- x-pack/plugins/entity_manager/server/lib/entities/built_in/services_from_ecs_data.ts
+++ x-pack/plugins/entity_manager/server/lib/entities/built_in/services_from_ecs_data.ts
@@ -20,7 +20,7 @@ const serviceTransactionFilter = (additionalFilters: string[] = []) => {
 
 export const builtInServicesFromEcsEntityDefinition: EntityDefinition =
   entityDefinitionSchema.parse({
-    version: '1.0.3',
+    version: '1.0.4',
     id: `${BUILT_IN_ID_PREFIX}services_from_ecs_data`,
     name: 'Services from ECS data',
     description:
