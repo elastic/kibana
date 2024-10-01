25717314a03125dd3f04337c67d860a2b07ce102
Cee Chen <constance.chen@elastic.co>
2024-10-01 15:17:13 -0700

    [Canvas] Replace Sass tooltip mixin with CSS-in-JS styles
    - normally we don't recommend reaching directly into components for styles, but this is the easiest migration path forward currently

--- x-pack/plugins/canvas/public/components/layout_annotations/tooltip_annotation.tsx
+++ x-pack/plugins/canvas/public/components/layout_annotations/tooltip_annotation.tsx
@@ -7,6 +7,9 @@
 
 import React, { FC } from 'react';
 import PropTypes from 'prop-types';
+import { useEuiMemoizedStyles } from '@elastic/eui';
+// @ts-ignore Kibana does not have types for eui/lib
+import { euiToolTipStyles } from '@elastic/eui/lib/components/tool_tip/tool_tip.styles';
 import { matrixToCSS } from '../../lib/dom';
 import { TransformMatrix3d } from '../../lib/aeroelastic';
 
@@ -19,8 +22,9 @@ export const TooltipAnnotation: FC<Props> = ({ transformMatrix, text }) => {
   const newStyle = {
     transform: `${matrixToCSS(transformMatrix)} translate(1em, -1em)`,
   };
+  const { euiToolTip } = useEuiMemoizedStyles(euiToolTipStyles);
   return (
-    <div className="tooltipAnnotation canvasLayoutAnnotation" style={newStyle}>
+    <div className="tooltipAnnotation canvasLayoutAnnotation" css={euiToolTip} style={newStyle}>
       <p>{text}°</p>
     </div>
   );
