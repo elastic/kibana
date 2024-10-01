d0d28c2a3e9a5553f9285f8a05f7af0203d7491a
Hannah Mudge <hannah.wright@elastic.co>
2024-10-01 16:32:50 -0600

    Make workpad service a class

--- x-pack/plugins/canvas/public/services/canvas_workpad_service.ts
+++ x-pack/plugins/canvas/public/services/canvas_workpad_service.ts
@@ -36,21 +36,6 @@ export interface ResolveWorkpadResponse {
   aliasPurpose?: ResolvedSimpleSavedObject['alias_purpose'];
 }
 
-export interface CanvasWorkpadService {
-  get: (id: string) => Promise<CanvasWorkpad>;
-  resolve: (id: string) => Promise<ResolveWorkpadResponse>;
-  create: (workpad: CanvasWorkpad) => Promise<CanvasWorkpad>;
-  import: (workpad: CanvasWorkpad) => Promise<CanvasWorkpad>;
-  createFromTemplate: (templateId: string) => Promise<CanvasWorkpad>;
-  find: (term: string) => Promise<WorkpadFindResponse>;
-  remove: (id: string) => Promise<void>;
-  findTemplates: () => Promise<TemplateFindResponse>;
-  update: (id: string, workpad: CanvasWorkpad) => Promise<void>;
-  updateWorkpad: (id: string, workpad: CanvasWorkpad) => Promise<void>;
-  updateAssets: (id: string, assets: CanvasWorkpad['assets']) => Promise<void>;
-  getRuntimeZip: (workpad: CanvasRenderedWorkpad) => Promise<Blob>;
-}
-
 /*
   Remove any top level keys from the workpad which will be rejected by validation
 */
@@ -82,114 +67,139 @@ const sanitizeWorkpad = function (workpad: CanvasWorkpad) {
   return workpad;
 };
 
-export const getCanvasWorkpadService: () => CanvasWorkpadService = () => {
-  const getApiPath = function () {
-    return `${API_ROUTE_WORKPAD}`;
-  };
+class CanvasWorkpadService {
+  private apiPath = `${API_ROUTE_WORKPAD}`;
+  private http;
 
-  return {
-    get: async (id: string) => {
-      const workpad = await coreServices.http.get<any>(`${getApiPath()}/${id}`, { version: '1' });
+  constructor() {
+    ({ http: this.http } = coreServices);
+  }
 
-      return { css: DEFAULT_WORKPAD_CSS, variables: [], ...workpad };
-    },
-    export: async (id: string) => {
-      const workpad = await coreServices.http.get<SavedObject<CanvasWorkpad>>(
-        `${getApiPath()}/export/${id}`,
-        { version: '1' }
-      );
-      const { attributes } = workpad;
+  public async get(id: string): Promise<CanvasWorkpad> {
+    const workpad = await this.http.get<any>(`${this.apiPath}/${id}`, { version: '1' });
 
-      return {
+    return { css: DEFAULT_WORKPAD_CSS, variables: [], ...workpad };
+  }
+
+  public async export(id: string) {
+    const workpad = await this.http.get<SavedObject<CanvasWorkpad>>(
+      `${this.apiPath}/export/${id}`,
+      { version: '1' }
+    );
+    const { attributes } = workpad;
+
+    return {
+      ...workpad,
+      attributes: {
+        ...attributes,
+        css: attributes.css ?? DEFAULT_WORKPAD_CSS,
+        variables: attributes.variables ?? [],
+      },
+    };
+  }
+
+  public async resolve(id: string): Promise<ResolveWorkpadResponse> {
+    const { workpad, ...resolveProps } = await this.http.get<ResolveWorkpadResponse>(
+      `${this.apiPath}/resolve/${id}`,
+      { version: '1' }
+    );
+
+    return {
+      ...resolveProps,
+      workpad: {
+        // @ts-ignore: Shimming legacy workpads that might not have CSS
+        css: DEFAULT_WORKPAD_CSS,
+        // @ts-ignore: Shimming legacy workpads that might not have variables
+        variables: [],
         ...workpad,
-        attributes: {
-          ...attributes,
-          css: attributes.css ?? DEFAULT_WORKPAD_CSS,
-          variables: attributes.variables ?? [],
-        },
-      };
-    },
-    resolve: async (id: string) => {
-      const { workpad, ...resolveProps } = await coreServices.http.get<ResolveWorkpadResponse>(
-        `${getApiPath()}/resolve/${id}`,
-        { version: '1' }
-      );
+      },
+    };
+  }
 
-      return {
-        ...resolveProps,
-        workpad: {
-          // @ts-ignore: Shimming legacy workpads that might not have CSS
-          css: DEFAULT_WORKPAD_CSS,
-          // @ts-ignore: Shimming legacy workpads that might not have variables
-          variables: [],
-          ...workpad,
-        },
-      };
-    },
-    create: (workpad: CanvasWorkpad) => {
-      return coreServices.http.post(getApiPath(), {
-        body: JSON.stringify({
-          ...sanitizeWorkpad({ ...workpad }),
-          assets: workpad.assets || {},
-          variables: workpad.variables || [],
-        }),
-        version: '1',
-      });
-    },
-    import: (workpad: CanvasWorkpad) =>
-      coreServices.http.post(`${getApiPath()}/import`, {
-        body: JSON.stringify({
-          ...sanitizeWorkpad({ ...workpad }),
-          assets: workpad.assets || {},
-          variables: workpad.variables || [],
-        }),
-        version: '1',
+  public async create(workpad: CanvasWorkpad): Promise<CanvasWorkpad> {
+    return this.http.post(this.apiPath, {
+      body: JSON.stringify({
+        ...sanitizeWorkpad({ ...workpad }),
+        assets: workpad.assets || {},
+        variables: workpad.variables || [],
       }),
-    createFromTemplate: (templateId: string) => {
-      return coreServices.http.post(getApiPath(), {
-        body: JSON.stringify({ templateId }),
-        version: '1',
-      });
-    },
-    findTemplates: async () => coreServices.http.get(API_ROUTE_TEMPLATES, { version: '1' }),
-    find: (searchTerm: string) => {
-      // TODO: this shouldn't be necessary.  Check for usage.
-      const validSearchTerm = typeof searchTerm === 'string' && searchTerm.length > 0;
+      version: '1',
+    });
+  }
 
-      return coreServices.http.get(`${getApiPath()}/find`, {
-        query: {
-          perPage: 10000,
-          name: validSearchTerm ? searchTerm : '',
-        },
-        version: '1',
-      });
-    },
-    remove: (id: string) => {
-      return coreServices.http.delete(`${getApiPath()}/${id}`, { version: '1' });
-    },
-    update: (id, workpad) => {
-      return coreServices.http.put(`${getApiPath()}/${id}`, {
-        body: JSON.stringify({ ...sanitizeWorkpad({ ...workpad }) }),
-        version: '1',
-      });
-    },
-    updateWorkpad: (id, workpad) => {
-      return coreServices.http.put(`${API_ROUTE_WORKPAD_STRUCTURES}/${id}`, {
-        body: JSON.stringify({ ...sanitizeWorkpad({ ...workpad }) }),
-        version: '1',
-      });
-    },
-    updateAssets: (id, assets) => {
-      return coreServices.http.put(`${API_ROUTE_WORKPAD_ASSETS}/${id}`, {
-        body: JSON.stringify(assets),
-        version: '1',
-      });
-    },
-    getRuntimeZip: (workpad) => {
-      return coreServices.http.post<Blob>(API_ROUTE_SHAREABLE_ZIP, {
-        body: JSON.stringify(workpad),
-        version: '1',
-      });
-    },
-  };
+  public async import(workpad: CanvasWorkpad): Promise<CanvasWorkpad> {
+    return this.http.post(`${this.apiPath}/import`, {
+      body: JSON.stringify({
+        ...sanitizeWorkpad({ ...workpad }),
+        assets: workpad.assets || {},
+        variables: workpad.variables || [],
+      }),
+      version: '1',
+    });
+  }
+
+  public async createFromTemplate(templateId: string): Promise<CanvasWorkpad> {
+    return this.http.post(this.apiPath, {
+      body: JSON.stringify({ templateId }),
+      version: '1',
+    });
+  }
+
+  public async findTemplates(): Promise<TemplateFindResponse> {
+    return this.http.get(API_ROUTE_TEMPLATES, { version: '1' });
+  }
+
+  public async find(searchTerm: string): Promise<WorkpadFindResponse> {
+    // TODO: this shouldn't be necessary.  Check for usage.
+    const validSearchTerm = typeof searchTerm === 'string' && searchTerm.length > 0;
+
+    return this.http.get(`${this.apiPath}/find`, {
+      query: {
+        perPage: 10000,
+        name: validSearchTerm ? searchTerm : '',
+      },
+      version: '1',
+    });
+  }
+
+  public async remove(id: string) {
+    this.http.delete(`${this.apiPath}/${id}`, { version: '1' });
+  }
+
+  public async update(id: string, workpad: CanvasWorkpad) {
+    this.http.put(`${this.apiPath}/${id}`, {
+      body: JSON.stringify({ ...sanitizeWorkpad({ ...workpad }) }),
+      version: '1',
+    });
+  }
+
+  public async updateWorkpad(id: string, workpad: CanvasWorkpad) {
+    this.http.put(`${API_ROUTE_WORKPAD_STRUCTURES}/${id}`, {
+      body: JSON.stringify({ ...sanitizeWorkpad({ ...workpad }) }),
+      version: '1',
+    });
+  }
+
+  public async updateAssets(id: string, assets: CanvasWorkpad['assets']) {
+    this.http.put(`${API_ROUTE_WORKPAD_ASSETS}/${id}`, {
+      body: JSON.stringify(assets),
+      version: '1',
+    });
+  }
+
+  public async getRuntimeZip(workpad: CanvasRenderedWorkpad): Promise<Blob> {
+    return this.http.post<Blob>(API_ROUTE_SHAREABLE_ZIP, {
+      body: JSON.stringify(workpad),
+      version: '1',
+    });
+  }
+}
+
+let canvasWorkpadService: CanvasWorkpadService;
+
+export const getCanvasWorkpadService: () => CanvasWorkpadService = () => {
+  if (!canvasWorkpadService) {
+    canvasWorkpadService = new CanvasWorkpadService();
+  }
+  return canvasWorkpadService;
 };
