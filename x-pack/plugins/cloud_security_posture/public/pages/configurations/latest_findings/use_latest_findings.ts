bc93b8443a084ef840664fd586a015f6bc19910d
Ido Cohen <cohenido42@gmail.com>
2024-10-01 16:27:33 +0300

    ignore unabilable index

--- x-pack/plugins/cloud_security_posture/public/pages/configurations/latest_findings/use_latest_findings.ts
+++ x-pack/plugins/cloud_security_posture/public/pages/configurations/latest_findings/use_latest_findings.ts
@@ -135,7 +135,6 @@ export const useLatestFindings = (options: UseFindingsOptions) => {
           params: getFindingsQuery(options, rulesStates!, pageParam), // ruleStates always exists since it under the `enabled` dependency.
         })
       );
-
       if (!aggregations) throw new Error('expected aggregations to be an defined');
       if (!Array.isArray(aggregations.count.buckets))
         throw new Error('expected buckets to be an array');
