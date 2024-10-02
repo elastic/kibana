f964c4a0ef369375872f3f55e029a79239d11c2b
Georgiana-Andreea Onoleata <georgiana.onoleata@elastic.co>
2024-10-02 13:48:08 +0300

    changes for the limit of templates/custom fields issue

--- x-pack/plugins/cases/public/components/custom_fields/index.tsx
+++ x-pack/plugins/cases/public/components/custom_fields/index.tsx
@@ -80,15 +80,6 @@ const CustomFieldsComponent: React.FC<Props> = ({
               onDeleteCustomField={handleDeleteCustomField}
               onEditCustomField={onEditCustomField}
             />
-            {error ? (
-              <EuiFlexGroup justifyContent="center">
-                <EuiFlexItem grow={false}>
-                  <EuiText color="danger">
-                    {i18n.MAX_CUSTOM_FIELD_LIMIT(MAX_CUSTOM_FIELDS_PER_CASE)}
-                  </EuiText>
-                </EuiFlexItem>
-              </EuiFlexGroup>
-            ) : null}
           </>
         ) : null}
         <EuiSpacer size="m" />
@@ -102,18 +93,27 @@ const CustomFieldsComponent: React.FC<Props> = ({
         ) : null}
         <EuiFlexGroup justifyContent="center">
           <EuiFlexItem grow={false}>
-            <EuiButtonEmpty
-              isLoading={isLoading}
-              isDisabled={disabled || error}
-              size="s"
-              onClick={onAddCustomField}
-              iconType="plusInCircle"
-              data-test-subj="add-custom-field"
-            >
-              {i18n.ADD_CUSTOM_FIELD}
-            </EuiButtonEmpty>
+            {customFields.length < MAX_CUSTOM_FIELDS_PER_CASE ? (
+              <EuiButtonEmpty
+                isLoading={isLoading}
+                isDisabled={disabled || error}
+                size="s"
+                onClick={onAddCustomField}
+                iconType="plusInCircle"
+                data-test-subj="add-custom-field"
+              >
+                {i18n.ADD_CUSTOM_FIELD}
+              </EuiButtonEmpty>
+            ) : (
+              <EuiFlexGroup justifyContent="center">
+                <EuiFlexItem grow={false}>
+                  <EuiText>{i18n.MAX_CUSTOM_FIELD_LIMIT(MAX_CUSTOM_FIELDS_PER_CASE)}</EuiText>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            )}
           </EuiFlexItem>
         </EuiFlexGroup>
+        <EuiSpacer size="m" />
       </EuiPanel>
     </EuiDescribedFormGroup>
   ) : null;
