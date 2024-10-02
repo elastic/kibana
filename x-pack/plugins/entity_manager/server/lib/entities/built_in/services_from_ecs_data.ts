dd04fbf76bd299e1d3567a77f547f7a0a7dea732
Cauê Marcondes <55978943+cauemarcondes@users.noreply.github.com>
2024-10-02 11:28:38 +0100

    Update services_from_ecs_data.ts

--- x-pack/plugins/entity_manager/server/lib/entities/built_in/services_from_ecs_data.ts
+++ x-pack/plugins/entity_manager/server/lib/entities/built_in/services_from_ecs_data.ts
@@ -20,7 +20,7 @@ const serviceTransactionFilter = (additionalFilters: string[] = []) => {
 
 export const builtInServicesFromEcsEntityDefinition: EntityDefinition =
   entityDefinitionSchema.parse({
-    version: '2.0.0',
+    version: '0.2.0',
     id: `${BUILT_IN_ID_PREFIX}services_from_ecs_data`,
     name: 'Services from ECS data',
     description:
