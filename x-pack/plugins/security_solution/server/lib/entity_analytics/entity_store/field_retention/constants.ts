45d8b7469ce22302ab9c131f5a400ba14f95ef70
Mark Hopkin <mark.hopkin@elastic.co>
2024-10-02 13:23:29 +0100

    debug mode

--- x-pack/plugins/security_solution/server/lib/entity_analytics/entity_store/field_retention/constants.ts
+++ x-pack/plugins/security_solution/server/lib/entity_analytics/entity_store/field_retention/constants.ts
@@ -9,4 +9,4 @@
 export const ENRICH_FIELD = 'historical';
 
 // only to be used in development, adds debug steps to the ingest pipeline
-export const DEBUG_MODE = false;
+export const DEBUG_MODE = true; // TODO: once we are happy everything is working we will change this back
