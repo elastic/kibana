ee9f55307b81859c93972a7825123db56e018188
Alex Szabo <alex.szabo@elastic.co>
2024-10-02 16:04:54 +0200

    quiet i18n check outputs

--- src/dev/i18n_tools/tasks/check_untracked_namespaces/task.ts
+++ src/dev/i18n_tools/tasks/check_untracked_namespaces/task.ts
@@ -48,10 +48,9 @@ export const checkUntrackedNamespacesTask: TaskSignature<TaskOptions> = (
               rootPath,
               definedPathsForRoot
             )) {
-              const { untrackedFilePath, extractedMessages, totalChecked, totalToCheck } =
-                untrackedMessageDetails;
+              const { untrackedFilePath, extractedMessages } = untrackedMessageDetails;
 
-              task.output = `[${totalChecked}/${totalToCheck}] Found ${extractedMessages.size} untracked messages in file ${untrackedFilePath}`;
+              // task.output = `[${totalChecked}/${totalToCheck}] Found ${extractedMessages.size} untracked messages in file ${untrackedFilePath}`;
               if (extractedMessages.size > 0) {
                 const error = new Error(
                   `The file ${untrackedFilePath} contains i18n messages but is not defined in the .i18nrc namespaces paths`
