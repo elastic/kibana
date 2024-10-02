0ffba49bd85a1b90593adc45b99383aca4288284
Angela Chuang <yi-chun.chuang@elastic.co>
2024-10-02 14:54:06 +0100

    test clean up

--- x-pack/plugins/security_solution/public/common/components/visualization_actions/use_save_to_library.test.tsx
+++ x-pack/plugins/security_solution/public/common/components/visualization_actions/use_save_to_library.test.tsx
@@ -8,8 +8,6 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import { toMountPoint } from '@kbn/react-kibana-mount';
 import { useSaveToLibrary } from './use_save_to_library';
 import { useKibana } from '../../lib/kibana';
-import { useRedirectToDashboardFromLens } from './use_redirect_to_dashboard_from_lens';
-import { useGetSecuritySolutionUrl } from '../link_to';
 import { kpiHostMetricLensAttributes } from './lens_attributes/hosts/kpi_host_metric';
 
 jest.mock('../../lib/kibana', () => ({
@@ -17,17 +15,21 @@ jest.mock('../../lib/kibana', () => ({
 }));
 
 jest.mock('./use_redirect_to_dashboard_from_lens', () => ({
-  useRedirectToDashboardFromLens: jest.fn(),
+  useRedirectToDashboardFromLens: jest.fn().mockReturnValue({
+    redirectTo: jest.fn(),
+    getEditOrCreateDashboardPath: jest.fn().mockReturnValue('mockDashboardPath'),
+  }),
 }));
 
 jest.mock('../link_to', () => ({
-  useRedirectToDashboardFromLens: jest.fn(),
   useGetSecuritySolutionUrl: jest.fn(),
 }));
 
 jest.mock('@kbn/react-kibana-mount', () => ({
-  toMountPoint: jest.fn(),
+  toMountPoint: jest.fn().mockReturnValue(jest.fn()),
 }));
+
+const mockUseKibana = useKibana as jest.Mock;
 
 describe('useSaveToLibrary hook', () => {
   const mockStartServices = {
@@ -35,23 +37,12 @@ describe('useSaveToLibrary hook', () => {
     lens: { SaveModalComponent: jest.fn() },
   };
 
-  const mockGetSecuritySolutionUrl = jest.fn();
-  const mockRedirectTo = jest.fn();
-  const mockGetEditOrCreateDashboardPath = jest.fn().mockReturnValue('mockDashboardPath');
-
   beforeEach(() => {
-    (useKibana as jest.Mock).mockReturnValue({ services: mockStartServices });
-    (useRedirectToDashboardFromLens as jest.Mock).mockReturnValue({
-      redirectTo: mockRedirectTo,
-      getEditOrCreateDashboardPath: mockGetEditOrCreateDashboardPath,
-    });
-    (useGetSecuritySolutionUrl as jest.Mock).mockReturnValue(mockGetSecuritySolutionUrl);
-    (toMountPoint as jest.Mock).mockImplementation(() => jest.fn());
+    jest.clearAllMocks();
+    mockUseKibana.mockReturnValue({ services: mockStartServices });
   });
 
   it('should open the save visualization flyout when openSaveVisualizationFlyout is called', () => {
-    const mockAttributes = kpiHostMetricLensAttributes;
-
     const { result } = renderHook(() =>
       useSaveToLibrary({ attributes: kpiHostMetricLensAttributes })
     );
@@ -68,23 +59,18 @@ describe('useSaveToLibrary hook', () => {
       ...mockStartServices,
       application: { capabilities: { visualize: { save: false } } },
     };
-    (useKibana as jest.Mock).mockReturnValue({ services: noSaveCapabilities });
+    mockUseKibana.mockReturnValue({ services: noSaveCapabilities });
 
-    const { result: result1 } = renderHook(() =>
+    const { result } = renderHook(() =>
       useSaveToLibrary({ attributes: kpiHostMetricLensAttributes })
     );
-    expect(result1.current.disableVisualizations).toBe(true);
+    expect(result.current.disableVisualizations).toBe(true);
   });
 
   it('should disable visualizations if attributes are missing', () => {
-    const noSaveCapabilities = {
-      ...mockStartServices,
-      application: { capabilities: { visualize: { save: false } } },
-    };
-
-    (useKibana as jest.Mock).mockReturnValue({ services: mockStartServices });
-    const { result: result2 } = renderHook(() => useSaveToLibrary({ attributes: null }));
-    expect(result2.current.disableVisualizations).toBe(true);
+    mockUseKibana.mockReturnValue({ services: mockStartServices });
+    const { result } = renderHook(() => useSaveToLibrary({ attributes: null }));
+    expect(result.current.disableVisualizations).toBe(true);
   });
 
   it('should enable visualizations if user can save and attributes are present', () => {
