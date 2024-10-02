f881402039059085955dda43651d1448b0fcae0a
iblancof <irene.blanco@elastic.co>
2024-10-02 12:14:40 +0200

    Use EntityName in  EntitiesGrid

--- x-pack/plugins/observability_solution/inventory/public/components/entities_grid/index.tsx
+++ x-pack/plugins/observability_solution/inventory/public/components/entities_grid/index.tsx
@@ -10,23 +10,12 @@ import {
   EuiDataGridCellValueElementProps,
   EuiDataGridColumn,
   EuiDataGridSorting,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiLink,
   EuiLoadingSpinner,
   EuiText,
   EuiToolTip,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedDate, FormattedMessage, FormattedTime } from '@kbn/i18n-react';
-import { useKibana } from '@kbn/kibana-react-plugin/public';
-import type { SharePluginStart } from '@kbn/share-plugin/public';
-import {
-  ASSET_DETAILS_LOCATOR_ID,
-  type AssetDetailsLocatorParams,
-  type ServiceOverviewParams,
-} from '@kbn/observability-shared-plugin/common';
-
 import { last } from 'lodash';
 import React, { useCallback, useState } from 'react';
 import type { EntityType } from '../../../common/entities';
@@ -37,14 +26,11 @@ import {
 } from '../../../common/es_fields/entities';
 import type { APIReturnType } from '../../api';
 import { getEntityTypeLabel } from '../../utils/get_entity_type_label';
-import { parseServiceParams } from '../../utils/parse_service_params';
 import { BadgeFilterWithPopover } from '../badge_filter_with_popover';
-import { EntityIcon } from '../entity_icon';
+import { EntityName } from './entity_name';
 
 type InventoryEntitiesAPIReturnType = APIReturnType<'GET /internal/inventory/entities'>;
-
 type LatestEntities = InventoryEntitiesAPIReturnType['entities'];
-type LatestEntity = LatestEntities extends Array<infer Entity> ? Entity : never;
 
 export type EntityColumnIds =
   | typeof ENTITY_DISPLAY_NAME
@@ -140,13 +126,6 @@ export function EntitiesGrid({
   onFilterByType,
 }: Props) {
   const [visibleColumns, setVisibleColumns] = useState(columns.map(({ id }) => id));
-  const { services } = useKibana<{ share?: SharePluginStart }>();
-
-  const assetDetailsLocator =
-    services.share?.url.locators.get<AssetDetailsLocatorParams>(ASSET_DETAILS_LOCATOR_ID);
-
-  const serviceOverviewLocator =
-    services.share?.url.locators.get<ServiceOverviewParams>('serviceOverviewLocator');
 
   const onSort: EuiDataGridSorting['onSort'] = useCallback(
     (newSortingColumns) => {
@@ -156,49 +135,6 @@ export function EntitiesGrid({
       }
     },
     [onChangeSort]
-  );
-
-  const getEntityRedirectUrl = useCallback(
-    (entity: LatestEntity) => {
-      const type = entity[ENTITY_TYPE] as EntityType;
-
-      // Any unrecognised types will always return undefined
-      switch (type) {
-        case 'host':
-        case 'container':
-          return assetDetailsLocator?.getRedirectUrl({
-            assetId: entity[ENTITY_DISPLAY_NAME],
-            assetType: type,
-          });
-
-        case 'service':
-          // For services, the format of the display name is `service.name:service.environment`.
-          // We just want the first part of the name for the locator.
-          // TODO: Replace this with a better approach for handling service names. See https://github.com/elastic/kibana/issues/194131
-          return serviceOverviewLocator?.getRedirectUrl(
-            parseServiceParams(entity[ENTITY_DISPLAY_NAME])
-          );
-      }
-    },
-    [assetDetailsLocator, serviceOverviewLocator]
-  );
-
-  const renderNameCellValue = useCallback(
-    (entity: LatestEntity) => {
-      return (
-        <EuiLink data-test-subj="inventoryCellValueLink" href={getEntityRedirectUrl(entity)}>
-          <EuiFlexGroup gutterSize="s" alignItems="center">
-            <EuiFlexItem grow={0}>
-              <EntityIcon entity={entity} />
-            </EuiFlexItem>
-            <EuiFlexItem className="eui-textTruncate">
-              <span className="eui-textTruncate">{entity[ENTITY_DISPLAY_NAME]}</span>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        </EuiLink>
-      );
-    },
-    [getEntityRedirectUrl]
   );
 
   const renderCellValue = useCallback(
@@ -247,12 +183,12 @@ export function EntitiesGrid({
             />
           );
         case ENTITY_DISPLAY_NAME:
-          return renderNameCellValue(entity);
+          return <EntityName entity={entity} />;
         default:
           return entity[columnId as EntityColumnIds] || '';
       }
     },
-    [entities, onFilterByType, renderNameCellValue]
+    [entities, onFilterByType]
   );
 
   if (loading) {
