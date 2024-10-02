c8f7710d6de86c79dbc21f6a0df9fc437fd3e943
Nick Peihl <nick.peihl@elastic.co>
2024-10-02 15:53:06 -0400

    Additional tests for Create endpoint

--- test/api_integration/apis/dashboards/create_dashboard/main.ts
+++ test/api_integration/apis/dashboards/create_dashboard/main.ts
@@ -9,69 +9,64 @@
 
 import expect from '@kbn/expect';
 import { PUBLIC_API_PATH } from '@kbn/dashboard-plugin/server';
-import type { DashboardAttributes } from '@kbn/dashboard-plugin/server/content_management';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
   describe('main', () => {
-    it('can create a dashboard with controls', async () => {
+    it('sets top level default values', async () => {
       const title = `foo-${Date.now()}-${Math.random()}`;
-
-      const dashboardAttrs: DashboardAttributes = {
-        title,
-        description: '',
-        kibanaSavedObjectMeta: {},
-        controlGroupInput: {
-          chainingSystem: 'HIERARCHICAL',
-          controlStyle: 'oneLine',
-          ignoreParentSettings: {},
-          panels: [
-            {
-              type: 'optionsListControl',
-              order: 0,
-              width: 'medium',
-              grow: true,
-              embeddableConfig: {
-                title: 'Origin City',
-                fieldName: 'OriginCityName',
-                dataViewId: 'd3d7af60-4c81-11e8-b3d7-01146121b73d',
-                selectedOptions: [],
-                enhancements: {},
-              },
-            },
-          ],
-        },
-        panels: [
-          {
-            type: 'visualization',
-            gridData: {
-              x: 0,
-              y: 0,
-              w: 24,
-              h: 15,
-            },
-            embeddableConfig: {},
-          },
-        ],
-        timeRestore: false,
-        version: 3,
-      };
 
       const response = await supertest
         .post(PUBLIC_API_PATH)
         .set('kbn-xsrf', 'true')
         .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
         .send({
-          attributes: dashboardAttrs,
+          attributes: {
+            title,
+          },
         });
 
       expect(response.status).to.be(200);
-      // generates a random saved object id
-      expect(response.body.item.id).match(/^[0-9a-f-]{36}$/);
-      // saved object stores controls panels as an object, but the API should return as an array
-      expect(response.body.item.attributes.controlGroupInput.panels).to.be.an('array');
+      expect(response.body.item.attributes.kibanaSavedObjectMeta).to.eql({});
+      expect(response.body.item.attributes.panels).to.eql([]);
+      expect(response.body.item.attributes.timeRestore).to.be(false);
+      expect(response.body.item.attributes.options).to.eql({
+        hidePanelTitles: false,
+        useMargins: true,
+        syncColors: true,
+        syncTooltips: true,
+        syncCursor: true,
+      });
+    });
 
+    it('sets panels default values', async () => {
+      const title = `foo-${Date.now()}-${Math.random()}`;
+
+      const response = await supertest
+        .post(PUBLIC_API_PATH)
+        .set('kbn-xsrf', 'true')
+        .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
+        .send({
+          attributes: {
+            title,
+            panels: [
+              {
+                type: 'visualization',
+                gridData: {
+                  x: 0,
+                  y: 0,
+                  w: 24,
+                  h: 15,
+                },
+                embeddableConfig: {},
+              },
+            ],
+          },
+        });
+
+      expect(response.status).to.be(200);
+      expect(response.body.item.attributes.panels).to.be.an('array');
       // panel index is a random uuid when not provided
       expect(response.body.item.attributes.panels[0].panelIndex).match(/^[0-9a-f-]{36}$/);
       expect(response.body.item.attributes.panels[0].panelIndex).to.eql(
@@ -79,25 +74,53 @@ export default function ({ getService }: FtrProviderContext) {
       );
     });
 
+    it('sets controls default values', async () => {
+      const title = `foo-${Date.now()}-${Math.random()}`;
+
+      const response = await supertest
+        .post(PUBLIC_API_PATH)
+        .set('kbn-xsrf', 'true')
+        .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
+        .send({
+          attributes: {
+            title,
+            controlGroupInput: {
+              panels: [
+                {
+                  type: 'optionsListControl',
+                  order: 0,
+                  width: 'medium',
+                  grow: true,
+                  embeddableConfig: {
+                    title: 'Origin City',
+                    fieldName: 'OriginCityName',
+                    dataViewId: 'd3d7af60-4c81-11e8-b3d7-01146121b73d',
+                    selectedOptions: [],
+                    enhancements: {},
+                  },
+                },
+              ],
+            },
+          },
+        });
+
+      expect(response.status).to.be(200);
+      // generates a random saved object id
+      expect(response.body.item.id).match(/^[0-9a-f-]{36}$/);
+      // saved object stores controls panels as an object, but the API should return as an array
+      expect(response.body.item.attributes.controlGroupInput.panels).to.be.an('array');
+    });
+
     it('can create a dashboard with a specific id', async () => {
       const title = `foo-${Date.now()}-${Math.random()}`;
       const id = `bar-${Date.now()}-${Math.random()}`;
-
-      const dashboardAttrs: DashboardAttributes = {
-        title,
-        description: '',
-        kibanaSavedObjectMeta: {},
-        panels: [],
-        timeRestore: false,
-        version: 3,
-      };
 
       const response = await supertest
         .post(`${PUBLIC_API_PATH}/${id}`)
         .set('kbn-xsrf', 'true')
         .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
         .send({
-          attributes: dashboardAttrs,
+          attributes: { title },
         });
 
       expect(response.status).to.be(200);
@@ -107,35 +130,29 @@ export default function ({ getService }: FtrProviderContext) {
     it('creates a dashboard with references', async () => {
       const title = `foo-${Date.now()}-${Math.random()}`;
 
-      const dashboardAttrs: DashboardAttributes = {
-        title,
-        description: '',
-        kibanaSavedObjectMeta: {},
-        panels: [
-          {
-            type: 'visualization',
-            gridData: {
-              x: 0,
-              y: 0,
-              w: 24,
-              h: 15,
-              i: 'bizz',
-            },
-            embeddableConfig: {},
-            panelIndex: 'bizz',
-            panelRefName: 'panel_bizz',
-          },
-        ],
-        timeRestore: false,
-        version: 3,
-      };
-
       const response = await supertest
         .post(PUBLIC_API_PATH)
         .set('kbn-xsrf', 'true')
         .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
         .send({
-          attributes: dashboardAttrs,
+          attributes: {
+            title,
+            panels: [
+              {
+                type: 'visualization',
+                gridData: {
+                  x: 0,
+                  y: 0,
+                  w: 24,
+                  h: 15,
+                  i: 'bizz',
+                },
+                embeddableConfig: {},
+                panelIndex: 'bizz',
+                panelRefName: 'panel_bizz',
+              },
+            ],
+          },
           references: [
             {
               name: 'bizz:panel_bizz',
@@ -153,15 +170,6 @@ export default function ({ getService }: FtrProviderContext) {
     it('in a defined space', async () => {
       const title = `foo-${Date.now()}-${Math.random()}`;
 
-      const dashboardAttrs: DashboardAttributes = {
-        title,
-        description: '',
-        kibanaSavedObjectMeta: {},
-        panels: [],
-        timeRestore: false,
-        version: 3,
-      };
-
       const spaceId = 'space-1';
 
       const response = await supertest
@@ -169,7 +177,9 @@ export default function ({ getService }: FtrProviderContext) {
         .set('kbn-xsrf', 'true')
         .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
         .send({
-          attributes: dashboardAttrs,
+          attributes: {
+            title,
+          },
           spaces: [spaceId],
         });
 
@@ -182,29 +192,14 @@ export default function ({ getService }: FtrProviderContext) {
       // id is a saved object loaded by the kbn_archiver
       const id = 'be3733a0-9efe-11e7-acb3-3dab96693fab';
 
-      const dashboardAttrs: DashboardAttributes = {
-        title,
-        description: '',
-        kibanaSavedObjectMeta: {},
-        panels: [],
-        timeRestore: false,
-        version: 3,
-      };
-
-      await supertest
-        .post(`${PUBLIC_API_PATH}/${id}`)
-        .set('kbn-xsrf', 'true')
-        .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
-        .send({
-          attributes: dashboardAttrs,
-        });
-
       const response = await supertest
         .post(`${PUBLIC_API_PATH}/${id}`)
         .set('kbn-xsrf', 'true')
         .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
         .send({
-          attributes: dashboardAttrs,
+          attributes: {
+            title,
+          },
         });
 
       expect(response.status).to.be(409);
--- test/api_integration/apis/dashboards/create_dashboard/validation.ts
+++ test/api_integration/apis/dashboards/create_dashboard/validation.ts
@@ -14,15 +14,50 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
   describe('validation', () => {
-    describe('dashboards - create', () => {
-      it('returns error when attributes object is not provided', async () => {
-        const response = await supertest.post(PUBLIC_API_PATH).send({});
-        expect(response.status).to.be(400);
-        expect(response.body.statusCode).to.be(400);
-        expect(response.body.message).to.be(
-          '[request body.attributes.title]: expected value of type [string] but got [undefined]'
-        );
-      });
+    it('returns error when attributes object is not provided', async () => {
+      const response = await supertest
+        .post(PUBLIC_API_PATH)
+        .set('kbn-xsrf', 'true')
+        .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
+        .send({});
+      expect(response.status).to.be(400);
+      expect(response.body.statusCode).to.be(400);
+      expect(response.body.message).to.be(
+        '[request body.attributes.title]: expected value of type [string] but got [undefined]'
+      );
+    });
+
+    it('returns error when title is not provided', async () => {
+      const response = await supertest
+        .post(PUBLIC_API_PATH)
+        .set('kbn-xsrf', 'true')
+        .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
+        .send({
+          attributes: {},
+        });
+      expect(response.status).to.be(400);
+      expect(response.body.statusCode).to.be(400);
+      expect(response.body.message).to.be(
+        '[request body.attributes.title]: expected value of type [string] but got [undefined]'
+      );
+    });
+
+    it('returns error if panels is not an array', async () => {
+      const response = await supertest
+        .post(PUBLIC_API_PATH)
+        .set('kbn-xsrf', 'true')
+        .set('ELASTIC_HTTP_VERSION_HEADER', '2023-10-31')
+        .send({
+          attributes: {
+            title: 'foo',
+            panels: {},
+          },
+        });
+      expect(response.status).to.be(400);
+      expect(response.body.statusCode).to.be(400);
+      expect(response.body.message).to.be(
+        '[request body.attributes.panels]: expected value of type [array] but got [Object]'
+      );
     });
   });
 }
