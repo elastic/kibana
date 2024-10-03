11d9fd3bad92920ea18153c1de28b3bafce2457e
Bharat Pasupula <123897612+bhapas@users.noreply.github.com>
2024-10-03 11:14:39 +0200

    [Automatic Import] Fix Non-ecs compatible fields in grok processor (#194727)
    ## Release Note
    
    Fixes a bug to resolve non-ecs compatible fields in Structured /
    Unstructured syslog processing in Automatic Import.
    
    ## Summary
    
    https://github.com/elastic/kibana/issues/194205 explains the issue. 
    
    This PR fixes `packageName.dataStreamName` for handling header values
    from grok processor for KV graph so that ecs mapping gets the header
    values in the converted json Samples too..
    
    ### Before this PR
    
    ![image](https://github.com/user-attachments/assets/d2660f7d-2cca-413c-ab90-1a0f3e1b4a03)
    
    
    ### After this PR
    
    <img width="706" alt="image"
    src="https://github.com/user-attachments/assets/954b5a91-2123-46f9-b822-1709c3247901">
    
    
    - Closes https://github.com/elastic/kibana/issues/194205
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios

--- x-pack/plugins/integration_assistant/server/graphs/unstructured/error.ts
+++ x-pack/plugins/integration_assistant/server/graphs/unstructured/error.ts
@@ -20,6 +20,8 @@ export async function handleUnstructuredError({
   const currentPatterns = state.grokPatterns;
 
   const pattern = await grokErrorGraph.invoke({
+    packageName: state.packageName,
+    dataStreamName: state.dataStreamName,
     current_pattern: JSON.stringify(currentPatterns, null, 2),
     errors: JSON.stringify(state.errors, null, 2),
     ex_answer: JSON.stringify(GROK_ERROR_EXAMPLE_ANSWER, null, 2),
