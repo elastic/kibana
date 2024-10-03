623ff4ef86164de38e074aeaae0b275eeb4fe6dd
Mike Côté <mikecote@users.noreply.github.com>
2024-10-03 13:12:22 -0400

    Add more logs to Task Manager poller (#194741)
    In this PR, I'm adding a few more logs to the task poller to indicate
    critical events to the task poller.
    
    ## To verify
    1. Startup Elasticsearch and Kibana (and ensure Elasticsearch data is
    persisted somewhere `yarn es snapshot -E path.data=...`)
    2. Observe the `Starting the task poller` message on startup
    3. Shut down Elasticsearch
    4. Observe the following messages:
    - `Stopping the task poller because Elasticsearch and/or saved-objects
    service became unavailable`
      - `Stopping the task poller`
      - `Task poller finished running its last cycle`
    5. Startup Elasticsearch again
    6. Wait a while and observe the `Starting the task poller` message

--- x-pack/plugins/task_manager/server/polling_lifecycle.ts
+++ x-pack/plugins/task_manager/server/polling_lifecycle.ts
@@ -199,6 +199,9 @@ export class TaskPollingLifecycle implements ITaskEventEmitter<TaskLifecycleEven
         // start polling for work
         poller.start();
       } else if (!areESAndSOAvailable) {
+        this.logger.info(
+          `Stopping the task poller because Elasticsearch and/or saved-objects service became unavailable`
+        );
         poller.stop();
         this.pool.cancelRunningTasks();
       }
@@ -285,7 +288,7 @@ export class TaskPollingLifecycle implements ITaskEventEmitter<TaskLifecycleEven
                 mapOptional((id) => this.emitEvent(asTaskRunRequestEvent(id, asErr(error))))
               );
             }
-            this.logger.error(error.message);
+            this.logger.error(error.message, { error: { stack_trace: error.stack } });
 
             // Emit event indicating task manager utilization % at the end of a polling cycle
             // Because there was a polling error, no tasks were claimed so this represents the number of workers busy
