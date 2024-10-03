90b5323b76a1b49bddf61cfc12322944229a824d
Jen Huang <its.jenetic@gmail.com>
2024-10-03 16:07:04 -0700

    [UII] Fix first time integration flow when using multiple spaces (#194350)
    ## Summary
    
    Resolves #182736. This PR fixes the first time Elastic Agent user
    onboarding flow (only shown on Cloud) by prefixing the user's current
    space ID to the default agent policy that is created behind the scenes
    if the user has opted into using Fleet's space awareness.
    
    This makes it so that Fleet correctly identifies whether there are any
    agents enrolled in the current space, and directs the user to the
    multi-step onboarding flow for adding integrations if there are no
    agents enrolled in the current space's scoped agent policies.
    
    An example policy ID created this way may look like
    `second-space:fleet-first-agent-policy`. If the current space is the
    `default` space, the ID remains as `fleet-first-agent-policy`.
    
    Due to prefixing using a `:`, I also had to put policy ID in quotes
    where ever it appears in kueries for filtering, i.e. `policy_id:"<some
    id>"`. This change comprises most of the changed files in this PR.

--- x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/multi_page_layout/hooks/use_get_agent_policy_or_default.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/multi_page_layout/hooks/use_get_agent_policy_or_default.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 import { i18n } from '@kbn/i18n';
 
 import { generateNewAgentPolicyWithDefaults } from '../../../../../../../../common/services/generate_new_agent_policy';
@@ -14,26 +14,13 @@ import {
   sendCreateAgentPolicy,
   sendGetOneAgentPolicy,
   sendGetEnrollmentAPIKeys,
+  useFleetStatus,
 } from '../../../../../../../hooks';
 
-import type { AgentPolicy, NewAgentPolicy, EnrollmentAPIKey } from '../../../../../../../types';
+import type { AgentPolicy, EnrollmentAPIKey } from '../../../../../../../types';
 
-interface UseGetAgentPolicyOrDefaultResponse {
-  isLoading: boolean;
-  error?: Error;
-  agentPolicy?: AgentPolicy;
-  enrollmentAPIKey?: EnrollmentAPIKey;
-  created?: boolean;
-}
-export const DEFAULT_AGENT_POLICY_ID: string = 'fleet-first-agent-policy';
-export const DEFAULT_AGENT_POLICY: NewAgentPolicy = Object.freeze(
-  generateNewAgentPolicyWithDefaults({
-    id: DEFAULT_AGENT_POLICY_ID,
-    name: i18n.translate('xpack.fleet.createPackagePolicy.firstAgentPolicyNameText', {
-      defaultMessage: 'My first agent policy',
-    }),
-  })
-);
+// Manual default space ID because importing from `@kbn/core-saved-objects-utils-server` is not allowed here
+const DEFAULT_NAMESPACE_STRING = 'default';
 
 const sendGetAgentPolicy = async (agentPolicyId: string) => {
   let result;
@@ -54,71 +41,107 @@ const sendGetAgentPolicy = async (agentPolicyId: string) => {
   return { data: result?.data };
 };
 
-const sendCreateDefaultAgentPolicy = sendCreateAgentPolicy.bind(null, DEFAULT_AGENT_POLICY);
-
 export function useGetAgentPolicyOrDefault(agentPolicyIdIn?: string) {
-  const [result, setResult] = useState<UseGetAgentPolicyOrDefaultResponse>({ isLoading: true });
+  const { spaceId, isSpaceAwarenessEnabled } = useFleetStatus();
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<Error>();
+  const [agentPolicyResponse, setAgentPolicyResponse] = useState<AgentPolicy>();
+  const [enrollmentAPIKey, setEnrollmentAPIKey] = useState<EnrollmentAPIKey>();
+  const [created, setCreated] = useState<boolean>();
+
+  // If space awareness is enabled, append current space ID to the agent policy ID
+  // If current space is the default space, do not append the space ID for BWC
+  const defaultFirstPolicyIdBase = 'fleet-first-agent-policy';
+  const defaultFirstPolicyId = useMemo(
+    () =>
+      isSpaceAwarenessEnabled && spaceId !== DEFAULT_NAMESPACE_STRING
+        ? `${spaceId}:${defaultFirstPolicyIdBase}`
+        : defaultFirstPolicyIdBase,
+    [isSpaceAwarenessEnabled, spaceId]
+  );
+
+  const defaultFirstPolicy = useMemo(
+    () =>
+      Object.freeze(
+        generateNewAgentPolicyWithDefaults({
+          id: defaultFirstPolicyId,
+          name: i18n.translate('xpack.fleet.createPackagePolicy.firstAgentPolicyNameText', {
+            defaultMessage: 'My first agent policy',
+          }),
+        })
+      ),
+    [defaultFirstPolicyId]
+  );
 
   useEffect(() => {
     const getAgentPolicyOrDefault = async () => {
-      const agentPolicyId = agentPolicyIdIn || DEFAULT_AGENT_POLICY_ID;
-      const { data: agentPolicyData, error: getError } = await sendGetAgentPolicy(agentPolicyId);
+      setIsLoading(true);
 
+      const agentPolicyId = agentPolicyIdIn || defaultFirstPolicyId;
+      const { data: agentPolicyData, error: getError } = await sendGetAgentPolicy(agentPolicyId);
       const existingAgentPolicy = agentPolicyData?.item;
 
       if (agentPolicyIdIn && !existingAgentPolicy) {
-        setResult({
-          isLoading: false,
-          error: new Error(`Agent policy ${agentPolicyId} not found`),
-        });
+        setIsLoading(false);
+        setError(new Error(`Agent policy ${agentPolicyId} not found`));
         return;
       }
+
       let createdAgentPolicy;
+
       if (getError) {
-        setResult({ isLoading: false, error: getError });
+        setIsLoading(false);
+        setError(getError);
         return;
       }
 
       if (!existingAgentPolicy) {
         const { data: createdAgentPolicyData, error: createError } =
-          await sendCreateDefaultAgentPolicy();
+          await sendCreateAgentPolicy.bind(null, defaultFirstPolicy)();
 
         if (createError) {
-          setResult({ isLoading: false, error: createError });
+          setIsLoading(false);
+          setError(createError);
           return;
         }
 
         createdAgentPolicy = createdAgentPolicyData!.item;
+        setCreated(true);
       }
 
       const agentPolicy = (existingAgentPolicy || createdAgentPolicy) as AgentPolicy;
+      setAgentPolicyResponse(agentPolicy);
 
       const { data: apiKeysData, error: apiKeysError } = await sendGetEnrollmentAPIKeys({
         page: 1,
         perPage: 1,
-        kuery: `policy_id:${agentPolicyId}`,
+        kuery: `policy_id:"${agentPolicyId}"`,
       });
 
       if (apiKeysError) {
-        setResult({ isLoading: false, error: apiKeysError });
+        setIsLoading(false);
+        setError(apiKeysError);
         return;
       }
 
       if (!apiKeysData || !apiKeysData.items?.length) {
-        setResult({
-          isLoading: false,
-          error: new Error(`No enrollment API key found for policy ${agentPolicyId}`),
-        });
+        setIsLoading(false);
+        setError(new Error(`No enrollment API key found for policy ${agentPolicyId}`));
         return;
       }
 
-      const enrollmentAPIKey = apiKeysData.items[0];
-
-      setResult({ isLoading: false, created: !!createdAgentPolicy, agentPolicy, enrollmentAPIKey });
+      setIsLoading(false);
+      setEnrollmentAPIKey(apiKeysData.items[0]);
     };
 
     getAgentPolicyOrDefault();
-  }, [agentPolicyIdIn]);
+  }, [agentPolicyIdIn, defaultFirstPolicy, defaultFirstPolicyId]);
 
-  return result;
+  return {
+    isLoading,
+    error,
+    agentPolicy: agentPolicyResponse,
+    enrollmentAPIKey,
+    created,
+  };
 }
