8c5edea10a86c4f3dd6778f81089d4c32d8772f9
Janki Salvi <jankigaurav.salvi@elastic.co>
2024-10-04 13:17:09 +0100

    type fix

--- x-pack/plugins/alerting/public/pages/maintenance_windows/components/maintenance_window_scoped_query.tsx
+++ x-pack/plugins/alerting/public/pages/maintenance_windows/components/maintenance_window_scoped_query.tsx
@@ -90,7 +90,7 @@ export const MaintenanceWindowScopedQuery = React.memo(
               http={http}
               toasts={toasts}
               unifiedSearchBar={SearchBar}
-              dataService={data}
+              dataViewsService={data.dataViews}
             />
           </EuiFormRow>
         </EuiFlexItem>
