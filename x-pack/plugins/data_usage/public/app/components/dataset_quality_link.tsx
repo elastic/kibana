15521118d6434cc050d1e0b7df46b0b819228722
neptunian <sandra.gonzales@elastic.co>
2024-10-04 13:44:39 -0400

    clean up DatasetQualityLink component

--- x-pack/plugins/data_usage/public/app/components/dataset_quality_link.tsx
+++ x-pack/plugins/data_usage/public/app/components/dataset_quality_link.tsx
@@ -5,11 +5,43 @@
  * 2.0.
  */
 
-import { EuiListGroupItem } from '@elastic/eui';
-
 import React from 'react';
+import { EuiListGroupItem } from '@elastic/eui';
 import { DataQualityLocatorParams, DATA_QUALITY_LOCATOR_ID } from '@kbn/deeplinks-observability';
 import { useKibanaContextForPlugin } from '../../utils/use_kibana';
+
+interface DatasetQualityLinkProps {
+  dataStreamName: string;
+}
+
+export const DatasetQualityLink: React.FC<DatasetQualityLinkProps> = React.memo(
+  ({ dataStreamName }) => {
+    const {
+      services: {
+        share: { url },
+      },
+    } = useKibanaContextForPlugin();
+
+    const locator = url.locators.get<DataQualityLocatorParams>(DATA_QUALITY_LOCATOR_ID);
+
+    const onClickDataQuality = async () => {
+      const locatorParams: DataQualityLocatorParams = {
+        filters: {
+          // TODO: get time range from our page state
+          timeRange: { from: 'now-15m', to: 'now', refresh: { pause: true, value: 0 } },
+        },
+      };
+      const dataset = getDatasetFromDataStream(dataStreamName);
+      if (locatorParams?.filters && dataset) {
+        locatorParams.filters.query = dataset;
+      }
+      if (locator) {
+        await locator.navigate(locatorParams);
+      }
+    };
+    return <EuiListGroupItem label="View data quality" onClick={onClickDataQuality} />;
+  }
+);
 
 function getDatasetFromDataStream(dataStreamName: string): string | null {
   const parts = dataStreamName.split('-');
@@ -18,30 +50,3 @@ function getDatasetFromDataStream(dataStreamName: string): string | null {
   }
   return parts[1];
 }
-
-export const DatasetQualityLink = React.memo(({ dataStreamName }: { dataStreamName: string }) => {
-  const {
-    services: {
-      share: { url },
-    },
-  } = useKibanaContextForPlugin();
-
-  const locator = url.locators.get<DataQualityLocatorParams>(DATA_QUALITY_LOCATOR_ID);
-  const onClickDataQuality = async () => {
-    const locatorParams: DataQualityLocatorParams = {
-      filters: {
-        // TODO: get time range from our page state
-        timeRange: { from: 'now-15m', to: 'now', refresh: { pause: true, value: 0 } },
-      },
-    };
-    const dataset = getDatasetFromDataStream(dataStreamName);
-    if (locatorParams?.filters && dataset) {
-      locatorParams.filters.query = dataset;
-    }
-    if (locator) {
-      await locator.navigate(locatorParams);
-    }
-  };
-
-  return <EuiListGroupItem label="View data quality" onClick={() => onClickDataQuality()} />;
-});
