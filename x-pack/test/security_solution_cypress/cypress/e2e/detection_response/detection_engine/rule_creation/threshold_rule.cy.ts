14098748f1233e4181c5569e139b44ef0299e741
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2024-10-04 18:08:59 +0100

    Update threshold_rule.cy.ts

--- x-pack/test/security_solution_cypress/cypress/e2e/detection_response/detection_engine/rule_creation/threshold_rule.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/detection_response/detection_engine/rule_creation/threshold_rule.cy.ts
@@ -160,7 +160,7 @@ describe(
 
       // ensures duration displayed on define step in preview mode
       cy.get(DEFINITION_DETAILS).within(() => {
-        getDetails(SUPPRESS_FOR_DETAILS).should('have.text', '6h');
+        getDetails(SUPPRESS_FOR_DETAILS).should('have.text', '5h');
       });
 
       fillAboutRuleMinimumAndContinue(rule);
