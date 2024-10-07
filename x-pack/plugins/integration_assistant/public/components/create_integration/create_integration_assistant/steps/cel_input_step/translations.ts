a7a7e8fa847615f734cab6cf83776931850426a3
Kylie Meli <kylie.geller@elastic.co>
2024-10-07 15:23:31 -0400

    fixing il8n for cel steps

--- x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/cel_input_step/translations.ts
+++ x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/cel_input_step/translations.ts
@@ -12,34 +12,34 @@ export const ANALYZING = i18n.translate('xpack.integrationAssistant.step.dataStr
 });
 
 export const CEL_INPUT_TITLE = i18n.translate(
-  'xpack.integrationAssistant.step.dataStream.dataStreamTitle',
+  'xpack.integrationAssistant.step.celInput.celInputTitle',
   {
     defaultMessage: 'Generate CEL input configuration',
   }
 );
 export const CEL_INPUT_DESCRIPTION = i18n.translate(
-  'xpack.integrationAssistant.step.dataStream.dataStreamDescription',
+  'xpack.integrationAssistant.step.celInput.celInputDescription',
   {
     defaultMessage: 'Upload an OpenAPI spec file to generate a configuration for the CEL input',
   }
 );
 
 export const API_DEFINITION_LABEL = i18n.translate(
-  'xpack.integrationAssistant.step.dataStream.apiDefinition.label',
+  'xpack.integrationAssistant.step.celInput.apiDefinition.label',
   {
     defaultMessage: 'OpenAPI spec',
   }
 );
 
 export const API_DEFINITION_DESCRIPTION = i18n.translate(
-  'xpack.integrationAssistant.step.dataStream.apiDefinition.description',
+  'xpack.integrationAssistant.step.celInput.apiDefinition.description',
   {
     defaultMessage: 'Drag and drop a file or browse files.',
   }
 );
 
 export const API_DEFINITION_DESCRIPTION_2 = i18n.translate(
-  'xpack.integrationAssistant.step.dataStream.apiDefinition.description2',
+  'xpack.integrationAssistant.step.celInput.apiDefinition.description2',
   {
     defaultMessage: 'OpenAPI specification',
   }
@@ -47,21 +47,21 @@ export const API_DEFINITION_DESCRIPTION_2 = i18n.translate(
 
 export const API_DEFINITION_ERROR = {
   CAN_NOT_READ: i18n.translate(
-    'xpack.integrationAssistant.step.dataStream.logsSample.errorCanNotRead',
+    'xpack.integrationAssistant.step.celInput.openapiSpec.errorCanNotRead',
     {
       defaultMessage: 'Failed to read the logs sample file',
     }
   ),
   CAN_NOT_READ_WITH_REASON: (reason: string) =>
     i18n.translate(
-      'xpack.integrationAssistant.step.dataStream.logsSample.errorCanNotReadWithReason',
+      'xpack.integrationAssistant.step.celInput.openapiSpec.errorCanNotReadWithReason',
       {
         values: { reason },
         defaultMessage: 'An error occurred when reading spec file: {reason}',
       }
     ),
   TOO_LARGE_TO_PARSE: i18n.translate(
-    'xpack.integrationAssistant.step.dataStream.logsSample.errorTooLargeToParse',
+    'xpack.integrationAssistant.step.celInput.openapiSpec.errorTooLargeToParse',
     {
       defaultMessage: 'This spec file is too large to parse',
     }
@@ -69,18 +69,18 @@ export const API_DEFINITION_ERROR = {
 };
 
 export const PROGRESS_CEL_INPUT_GRAPH = i18n.translate(
-  'xpack.integrationAssistant.step.dataStream.progress.relatedGraph',
+  'xpack.integrationAssistant.step.celInput.progress.relatedGraph',
   {
     defaultMessage: 'Generating CEL input configuration',
   }
 );
 
 export const GENERATION_ERROR = (progressStep: string) =>
-  i18n.translate('xpack.integrationAssistant.step.dataStream.generationError', {
+  i18n.translate('xpack.integrationAssistant.step.celInput.generationError', {
     values: { progressStep },
     defaultMessage: 'An error occurred during: {progressStep}',
   });
 
-export const RETRY = i18n.translate('xpack.integrationAssistant.step.dataStream.retryButtonLabel', {
+export const RETRY = i18n.translate('xpack.integrationAssistant.step.celInput.retryButtonLabel', {
   defaultMessage: 'Retry',
 });
--- x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/review_cel_step/translations.ts
+++ x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/review_cel_step/translations.ts
@@ -7,24 +7,24 @@
 
 import { i18n } from '@kbn/i18n';
 
-export const TITLE = i18n.translate('xpack.integrationAssistant.step.review.title', {
+export const TITLE = i18n.translate('xpack.integrationAssistant.step.reviewCel.title', {
   defaultMessage: 'Review results',
 });
-export const DESCRIPTION = i18n.translate('xpack.integrationAssistant.step.review.description', {
+export const DESCRIPTION = i18n.translate('xpack.integrationAssistant.step.reviewCel.description', {
   defaultMessage:
     'Review the generated CEL input configuration settings for your integration. These settings will be auto-populated into the integration configuration where editing will be possible.',
 });
 
-export const PROGRAM = i18n.translate('xpack.integrationAssistant.step.review.program', {
+export const PROGRAM = i18n.translate('xpack.integrationAssistant.step.reviewCel.program', {
   defaultMessage: 'The CEL program to be run for each polling',
 });
-export const STATE = i18n.translate('xpack.integrationAssistant.step.review.state', {
+export const STATE = i18n.translate('xpack.integrationAssistant.step.reviewCel.state', {
   defaultMessage: 'Initial CEL evaluation state',
 });
-export const REDACT_VARS = i18n.translate('xpack.integrationAssistant.step.review.redact', {
+export const REDACT_VARS = i18n.translate('xpack.integrationAssistant.step.reviewCel.redact', {
   defaultMessage: 'Redacted fields',
 });
 
-export const SAVE_BUTTON = i18n.translate('xpack.integrationAssistant.step.review.save', {
+export const SAVE_BUTTON = i18n.translate('xpack.integrationAssistant.step.reviewCel.save', {
   defaultMessage: 'Save',
 });
