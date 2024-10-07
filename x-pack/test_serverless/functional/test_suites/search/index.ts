d4afd7b5819d17e0e343ee7ebbcda06e543bae0a
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-10-07 13:00:32 +0200

    post merge fix

--- x-pack/test_serverless/functional/test_suites/search/index.ts
+++ x-pack/test_serverless/functional/test_suites/search/index.ts
@@ -24,7 +24,6 @@ export default function ({ loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./console_notebooks'));
     loadTestFile(require.resolve('./search_playground/playground_overview'));
     loadTestFile(require.resolve('./ml'));
-    loadTestFile(require.resolve('./search_homepage'));
     loadTestFile(require.resolve('./custom_role_access'));
   });
 }
