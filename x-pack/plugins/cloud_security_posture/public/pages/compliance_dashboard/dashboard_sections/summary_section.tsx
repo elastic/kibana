c183bce0be5d1f66093d1634a1da8294ac3874c3
Jordan <yarden.shalom@elastic.co>
2024-10-07 19:15:03 +0300

    removed unrelated files

--- x-pack/plugins/cloud_security_posture/public/pages/compliance_dashboard/dashboard_sections/summary_section.tsx
+++ x-pack/plugins/cloud_security_posture/public/pages/compliance_dashboard/dashboard_sections/summary_section.tsx
@@ -40,9 +40,13 @@ export const dashboardColumnsGrow: Record<string, EuiFlexItemProps['grow']> = {
   third: 8,
 };
 
-export const getPolicyTemplateQuery = (policyTemplate: PosturePolicyTemplate): NavFilter => ({
-  'rule.benchmark.posture_type': policyTemplate,
-});
+export const getPolicyTemplateQuery = (policyTemplate: PosturePolicyTemplate): NavFilter => {
+  if (policyTemplate === CSPM_POLICY_TEMPLATE) {
+    return { 'rule.benchmark.posture_type': CSPM_POLICY_TEMPLATE };
+  }
+
+  return { 'rule.benchmark.posture_type': { value: CSPM_POLICY_TEMPLATE, negate: true } };
+};
 
 export const SummarySection = ({
   dashboardType,
