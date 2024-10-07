b0cf4023e09322af777ee27ad86f5e02f677cb7b
Marco Antonio Ghiani <marcoantonio.ghiani@elastic.co>
2024-10-07 17:09:34 +0200

    fix(logs-ml): check permissions before granting access to features

--- x-pack/plugins/observability_solution/infra/public/pages/logs/log_entry_rate/page.tsx
+++ x-pack/plugins/observability_solution/infra/public/pages/logs/log_entry_rate/page.tsx
@@ -7,7 +7,10 @@
 
 import { EuiErrorBoundary } from '@elastic/eui';
 import React from 'react';
-import { LogEntryRatePageContent } from './page_content';
+import { SubscriptionSplashPage } from '../../../components/subscription_splash_content';
+import { MissingResultsPrivilegesPrompt } from '../../../components/logging/log_analysis_setup';
+import { useLogAnalysisCapabilitiesContext } from '../../../containers/logs/log_analysis';
+import { AnomaliesPageTemplate, LogEntryRatePageContent } from './page_content';
 import { LogEntryRatePageProviders } from './page_providers';
 import { useLogsBreadcrumbs } from '../../../hooks/use_logs_breadcrumbs';
 import { anomaliesTitle } from '../../../translations';
@@ -19,6 +22,29 @@ export const LogEntryRatePage = () => {
       text: anomaliesTitle,
     },
   ]);
+
+  const { hasLogAnalysisReadCapabilities, hasLogAnalysisCapabilites } =
+    useLogAnalysisCapabilitiesContext();
+
+  if (!hasLogAnalysisCapabilites) {
+    return (
+      <SubscriptionSplashPage
+        data-test-subj="logsLogEntryRatePage"
+        pageHeader={{
+          pageTitle: anomaliesTitle,
+        }}
+      />
+    );
+  }
+
+  if (!hasLogAnalysisReadCapabilities) {
+    return (
+      <AnomaliesPageTemplate isEmptyState={true}>
+        <MissingResultsPrivilegesPrompt />
+      </AnomaliesPageTemplate>
+    );
+  }
+
   return (
     <EuiErrorBoundary>
       <LogMlJobIdFormatsShimProvider>
