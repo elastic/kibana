8b6ffeb807f857ad10aa732fada8c4928013cf13
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-10-07 20:43:17 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- test/api_integration/apis/dashboards/get_dashboard/validation.ts
+++ test/api_integration/apis/dashboards/get_dashboard/validation.ts
@@ -7,8 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import expect from '@kbn/expect';
-import { PUBLIC_API_PATH } from '@kbn/dashboard-plugin/server';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getService }: FtrProviderContext) {
