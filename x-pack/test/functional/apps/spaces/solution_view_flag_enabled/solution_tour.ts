cf246a8c5524448476a50b4315c939db4928b8cd
Sébastien Loix <sebastien.loix@elastic.co>
2024-10-07 12:42:28 +0100

    Add more functional tests

--- x-pack/test/functional/apps/spaces/solution_view_flag_enabled/solution_tour.ts
+++ x-pack/test/functional/apps/spaces/solution_view_flag_enabled/solution_tour.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import expect from '@kbn/expect';
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
@@ -13,8 +14,25 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const testSubjects = getService('testSubjects');
   const spacesService = getService('spaces');
   const browser = getService('browser');
+  const es = getService('es');
 
   describe('space solution tour', () => {
+    let version: string | undefined;
+
+    const removeGlobalSettings = async () => {
+      version = version ?? (await kibanaServer.version.get());
+
+      await es
+        .delete(
+          { id: `config-global:${version}`, index: '.kibana', refresh: true },
+          { headers: { 'kbn-xsrf': 'spaces' } }
+        )
+        .catch((error) => {
+          if (error.statusCode === 404) return; // ignore 404 errors
+          throw error;
+        });
+    };
+
     before(async () => {
       await kibanaServer.savedObjects.cleanStandardList();
     });
@@ -24,32 +42,87 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
     });
 
     describe('solution tour', () => {
+      let defaultSpace: any;
+
       before(async () => {
+        defaultSpace = await spacesService.getSpace('default');
+
         await PageObjects.common.navigateToUrl('management', 'kibana/spaces', {
           shouldUseHashForSubUrl: false,
         });
+        await PageObjects.common.sleep(1000); // wait to save the setting
       });
 
       it('does not show the solution tour for the classic space', async () => {
-        await testSubjects.missingOrFail('spaceSolutionTour');
+        await testSubjects.missingOrFail('spaceSolutionTour', { timeout: 3000 });
       });
 
       it('does show the solution tour if the default space has a solution set', async () => {
-        const defaultSpace = await spacesService.getSpace('default');
+        await spacesService.update('default', {
+          ...defaultSpace,
+          solution: 'es', // set a solution
+        });
 
+        await removeGlobalSettings(); // Make sure we start from a clean state
+
+        await browser.refresh();
+        await testSubjects.existOrFail('spaceSolutionTour', { timeout: 3000 });
+
+        await testSubjects.click('closeTourBtn'); // close the tour
+        await PageObjects.common.sleep(1000); // wait to save the setting
+
+        await browser.refresh();
+        await testSubjects.missingOrFail('spaceSolutionTour', { timeout: 3000 }); // The tour does not appear after refresh
+
+        await spacesService.update('default', {
+          ...defaultSpace,
+          solution: 'classic', // revert to not impact future tests
+        });
+      });
+
+      it('does now show the solution tour after updating the default space from classic to solution', async () => {
         await spacesService.update('default', {
           ...defaultSpace,
           solution: 'es', // set a solution
         });
 
         await browser.refresh();
-        await testSubjects.existOrFail('spaceSolutionTour');
 
-        await testSubjects.click('closeTourBtn'); // close the tour
-        await PageObjects.common.sleep(1000); // wait to save the setting
+        // The tour does not appear after refresh, even with the default space with a solution set
+        await testSubjects.missingOrFail('spaceSolutionTour', { timeout: 3000 });
 
+        await spacesService.update('default', {
+          ...defaultSpace,
+          solution: 'classic', // revert to not impact future tests
+        });
+      });
+
+      it('does now show the solution tour after deleting spaces and leave only the default', async () => {
+        await spacesService.update('default', {
+          ...defaultSpace,
+          solution: 'es', // set a solution
+        });
+
+        await spacesService.create({
+          id: 'foo-space',
+          name: 'Foo Space',
+          disabledFeatures: [],
+          color: '#AABBCC',
+        });
+
+        const allSpaces = await spacesService.getAll();
+        expect(allSpaces).to.have.length(2); // Make sure we have 2 spaces
+
+        await removeGlobalSettings(); // Make sure we start from a clean state
         await browser.refresh();
-        await testSubjects.missingOrFail('spaceSolutionTour'); // The tour does not appear after refresh
+
+        await testSubjects.missingOrFail('spaceSolutionTour', { timeout: 3000 });
+
+        await spacesService.delete('foo-space');
+        await browser.refresh();
+
+        // The tour does not appear after refresh, even with 1 space with a solution set
+        await testSubjects.missingOrFail('spaceSolutionTour', { timeout: 3000 });
 
         await spacesService.update('default', {
           ...defaultSpace,
