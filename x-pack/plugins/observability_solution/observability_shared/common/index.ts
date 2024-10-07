0aff979d9972538f9c1c05914a338e2f061bb09e
iblancof <irene.blanco@elastic.co>
2024-10-07 14:54:28 +0200

    Move EntityDataStreamType

--- x-pack/plugins/observability_solution/observability_shared/common/index.ts
+++ x-pack/plugins/observability_solution/observability_shared/common/index.ts
@@ -217,4 +217,4 @@ export {
 
 export { COMMON_OBSERVABILITY_GROUPING } from './embeddable_grouping';
 
-export { EntityDataStreamType } from './entity_data_stream_types';
+export { EntityDataStreamType } from './entity/entity_data_stream_types';
