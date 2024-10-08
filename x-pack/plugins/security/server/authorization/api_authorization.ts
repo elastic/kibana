22ba01d106746b4f27deca8ce2115b820cf31f23
Elena Shostak <elena.shostak@elastic.co>
2024-10-08 11:41:35 +0200

    [Authz] Adjusted forbidden message for new security route configuration

--- x-pack/plugins/security/server/authorization/api_authorization.ts
+++ x-pack/plugins/security/server/authorization/api_authorization.ts
@@ -87,13 +87,13 @@ export function initAPIAuthorization(
           const missingPrivileges = Object.keys(kibanaPrivileges).filter(
             (key) => !kibanaPrivileges[key]
           );
-          const forbiddenMessage = `API [${request.route.method.toUpperCase()} ${
+          const forbiddenMessage = `API [${request.route.method.toLocaleUpperCase('en')} ${
             request.url.pathname
           }${
             request.url.search
           }] is unauthorized for user, this action is granted by the Kibana privileges [${missingPrivileges}]`;
 
-          logger.warn(forbiddenMessage);
+          logger.warn(`Responding with 403: ${forbiddenMessage}}`);
 
           return response.forbidden({
             body: {
