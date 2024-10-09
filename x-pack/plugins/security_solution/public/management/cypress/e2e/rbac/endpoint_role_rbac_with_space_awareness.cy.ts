6ceae9de76083cceb08de75808ea4cc68dd249fc
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2024-10-09 08:46:18 +0200

    headers

--- x-pack/plugins/security_solution/public/management/cypress/e2e/rbac/endpoint_role_rbac_with_space_awareness.cy.ts
+++ x-pack/plugins/security_solution/public/management/cypress/e2e/rbac/endpoint_role_rbac_with_space_awareness.cy.ts
@@ -23,8 +23,7 @@ import {
   setSecuritySolutionEndpointGroupPrivilege,
 } from '../../screens/stack_management/role_page';
 
-// FLAKY: https://github.com/elastic/kibana/issues/195477
-describe.skip(
+describe(
   'When defining a kibana role for Endpoint security access with space awareness enabled',
   {
     // TODO:PR Remove `'@skipInServerlessMKI` once PR merges to `main`
