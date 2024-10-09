1bdeecfa29835d23ae3a882a9d91896a5ed02697
Nathan Reese <reese.nathan@gmail.com>
2024-10-09 08:59:16 -0600

    remove legacy embeddable registry

--- src/plugins/embeddable/public/plugin.tsx
+++ src/plugins/embeddable/public/plugin.tsx
@@ -8,8 +8,6 @@
  */
 
 import { Subscription } from 'rxjs';
-import { identity } from 'lodash';
-import type { SerializableRecord } from '@kbn/utility-types';
 import { UiActionsSetup, UiActionsStart } from '@kbn/ui-actions-plugin/public';
 import { Start as InspectorStart } from '@kbn/inspector-plugin/public';
 import {
@@ -21,42 +19,17 @@ import {
 } from '@kbn/core/public';
 import { Storage } from '@kbn/kibana-utils-plugin/public';
 import { UsageCollectionStart } from '@kbn/usage-collection-plugin/public';
-import { migrateToLatest, PersistableStateService } from '@kbn/kibana-utils-plugin/common';
 import { SavedObjectsManagementPluginStart } from '@kbn/saved-objects-management-plugin/public';
 import type { ContentManagementPublicStart } from '@kbn/content-management-plugin/public';
 import type { SavedObjectTaggingOssPluginStart } from '@kbn/saved-objects-tagging-oss-plugin/public';
 import { FinderAttributes } from '@kbn/saved-objects-finder-plugin/common';
-import {
-  EmbeddableFactoryRegistry,
-  EmbeddableFactoryProvider,
-  EnhancementsRegistry,
-  EnhancementRegistryDefinition,
-  EnhancementRegistryItem,
-} from './types';
 import { bootstrap } from './bootstrap';
 import {
-  EmbeddableFactory,
-  EmbeddableInput,
-  EmbeddableOutput,
-  defaultEmbeddableFactoryProvider,
-  IEmbeddable,
-  SavedObjectEmbeddableInput,
   registerReactEmbeddableSavedObject,
   ReactEmbeddableSavedObject,
   getReactEmbeddableSavedObjects,
 } from './lib';
-import { EmbeddableFactoryDefinition } from './lib/embeddables/embeddable_factory_definition';
 import { EmbeddableStateTransfer } from './lib/state_transfer';
-import { ATTRIBUTE_SERVICE_KEY, AttributeService } from './lib/attribute_service';
-import { AttributeServiceOptions } from './lib/attribute_service/attribute_service';
-import { EmbeddableStateWithType, CommonEmbeddableStartContract } from '../common/types';
-import {
-  getExtractFunction,
-  getInjectFunction,
-  getMigrateFunction,
-  getTelemetryFunction,
-} from '../common/lib';
-import { getAllMigrations } from '../common/lib/get_all_migrations';
 import { setKibanaServices } from './kibana_services';
 import {
   reactEmbeddableRegistryHasKey,
@@ -108,29 +81,9 @@ export interface EmbeddableSetup {
    * Registers an async {@link ReactEmbeddableFactory} getter.
    */
   registerReactEmbeddableFactory: typeof registerReactEmbeddableFactory;
-
-  /**
-   * @deprecated use {@link registerReactEmbeddableFactory} instead.
-   */
-  registerEmbeddableFactory: <
-    I extends EmbeddableInput,
-    O extends EmbeddableOutput,
-    E extends IEmbeddable<I, O> = IEmbeddable<I, O>
-  >(
-    id: string,
-    factory: EmbeddableFactoryDefinition<I, O, E>
-  ) => () => EmbeddableFactory<I, O, E>;
-  /**
-   * @deprecated
-   */
-  registerEnhancement: (enhancement: EnhancementRegistryDefinition) => void;
-  /**
-   * @deprecated
-   */
-  setCustomEmbeddableFactoryProvider: (customProvider: EmbeddableFactoryProvider) => void;
 }
 
-export interface EmbeddableStart extends PersistableStateService<EmbeddableStateWithType> {
+export interface EmbeddableStart {
   /**
    * Checks if a {@link ReactEmbeddableFactory} has been registered using {@link registerReactEmbeddableFactory}
    */
@@ -144,44 +97,10 @@ export interface EmbeddableStart extends PersistableStateService<EmbeddableState
     TSavedObjectAttributes extends FinderAttributes
   >() => IterableIterator<[string, ReactEmbeddableSavedObject<TSavedObjectAttributes>]>;
 
-  /**
-   * @deprecated use {@link registerReactEmbeddableFactory} instead.
-   */
-  getEmbeddableFactory: <
-    I extends EmbeddableInput = EmbeddableInput,
-    O extends EmbeddableOutput = EmbeddableOutput,
-    E extends IEmbeddable<I, O> = IEmbeddable<I, O>
-  >(
-    embeddableFactoryId: string
-  ) => EmbeddableFactory<I, O, E> | undefined;
-
-  /**
-   * @deprecated
-   */
-  getEmbeddableFactories: () => IterableIterator<EmbeddableFactory>;
   getStateTransfer: (storage?: Storage) => EmbeddableStateTransfer;
-  getAttributeService: <
-    A extends { title: string },
-    V extends EmbeddableInput & {
-      [ATTRIBUTE_SERVICE_KEY]: A;
-    } = EmbeddableInput & {
-      [ATTRIBUTE_SERVICE_KEY]: A;
-    },
-    R extends SavedObjectEmbeddableInput = SavedObjectEmbeddableInput,
-    M extends unknown = unknown
-  >(
-    type: string,
-    options: AttributeServiceOptions<A, M>
-  ) => AttributeService<A, V, R, M>;
 }
 export class EmbeddablePublicPlugin implements Plugin<EmbeddableSetup, EmbeddableStart> {
-  private readonly embeddableFactoryDefinitions: Map<string, EmbeddableFactoryDefinition> =
-    new Map();
-  private readonly embeddableFactories: EmbeddableFactoryRegistry = new Map();
-  private readonly enhancements: EnhancementsRegistry = new Map();
-  private customEmbeddableFactoryProvider?: EmbeddableFactoryProvider;
   private stateTransferService: EmbeddableStateTransfer = {} as EmbeddableStateTransfer;
-  private isRegistryReady = false;
   private appList?: ReadonlyMap<string, PublicAppInfo>;
   private appListSubscription?: Subscription;
 
@@ -194,30 +113,10 @@ export class EmbeddablePublicPlugin implements Plugin<EmbeddableSetup, Embeddabl
       registerReactEmbeddableFactory,
       registerSavedObjectToPanelMethod,
       registerReactEmbeddableSavedObject,
-
-      registerEmbeddableFactory: this.registerEmbeddableFactory,
-      registerEnhancement: this.registerEnhancement,
-      setCustomEmbeddableFactoryProvider: (provider: EmbeddableFactoryProvider) => {
-        if (this.customEmbeddableFactoryProvider) {
-          throw new Error(
-            'Custom embeddable factory provider is already set, and can only be set once'
-          );
-        }
-        this.customEmbeddableFactoryProvider = provider;
-      },
     };
   }
 
   public start(core: CoreStart, deps: EmbeddableStartDependencies): EmbeddableStart {
-    this.embeddableFactoryDefinitions.forEach((def) => {
-      this.embeddableFactories.set(
-        def.type,
-        this.customEmbeddableFactoryProvider
-          ? this.customEmbeddableFactoryProvider(def)
-          : defaultEmbeddableFactoryProvider(def)
-      );
-    });
-
     this.appListSubscription = core.application.applications$.subscribe((appList) => {
       this.appList = appList;
     });
@@ -227,29 +126,10 @@ export class EmbeddablePublicPlugin implements Plugin<EmbeddableSetup, Embeddabl
       core.application.currentAppId$,
       this.appList
     );
-    this.isRegistryReady = true;
-
-    const commonContract: CommonEmbeddableStartContract = {
-      getEmbeddableFactory: this
-        .getEmbeddableFactory as unknown as CommonEmbeddableStartContract['getEmbeddableFactory'],
-      getEnhancement: this.getEnhancement,
-    };
-
-    const getAllMigrationsFn = () =>
-      getAllMigrations(
-        Array.from(this.embeddableFactories.values()),
-        Array.from(this.enhancements.values()),
-        getMigrateFunction(commonContract)
-      );
 
     const embeddableStart: EmbeddableStart = {
       reactEmbeddableRegistryHasKey,
       getReactEmbeddableSavedObjects,
-
-      getEmbeddableFactory: this.getEmbeddableFactory,
-      getEmbeddableFactories: this.getEmbeddableFactories,
-      getAttributeService: (type: string, options) =>
-        new AttributeService(type, core.notifications.toasts, options, this.getEmbeddableFactory),
       getStateTransfer: (storage?: Storage) =>
         storage
           ? new EmbeddableStateTransfer(
@@ -259,13 +139,6 @@ export class EmbeddablePublicPlugin implements Plugin<EmbeddableSetup, Embeddabl
               storage
             )
           : this.stateTransferService,
-      telemetry: getTelemetryFunction(commonContract),
-      extract: getExtractFunction(commonContract),
-      inject: getInjectFunction(commonContract),
-      getAllMigrations: getAllMigrationsFn,
-      migrateToLatest: (state) => {
-        return migrateToLatest(getAllMigrationsFn(), state) as EmbeddableStateWithType;
-      },
     };
 
     setKibanaServices(core, embeddableStart, deps);
@@ -277,94 +150,4 @@ export class EmbeddablePublicPlugin implements Plugin<EmbeddableSetup, Embeddabl
       this.appListSubscription.unsubscribe();
     }
   }
-
-  private registerEnhancement = (enhancement: EnhancementRegistryDefinition) => {
-    if (this.enhancements.has(enhancement.id)) {
-      throw new Error(`enhancement with id ${enhancement.id} already exists in the registry`);
-    }
-    this.enhancements.set(enhancement.id, {
-      id: enhancement.id,
-      telemetry: enhancement.telemetry || ((state, stats) => stats),
-      inject: enhancement.inject || identity,
-      extract:
-        enhancement.extract ||
-        ((state: SerializableRecord) => {
-          return { state, references: [] };
-        }),
-      migrations: enhancement.migrations || {},
-    });
-  };
-
-  private getEnhancement = (id: string): EnhancementRegistryItem => {
-    return (
-      this.enhancements.get(id) || {
-        id: 'unknown',
-        telemetry: (state, stats) => stats,
-        inject: identity,
-        extract: (state: SerializableRecord) => {
-          return { state, references: [] };
-        },
-        migrations: {},
-      }
-    );
-  };
-
-  private getEmbeddableFactories = () => {
-    this.ensureFactoriesExist();
-    return this.embeddableFactories.values();
-  };
-
-  private registerEmbeddableFactory = <
-    I extends EmbeddableInput = EmbeddableInput,
-    O extends EmbeddableOutput = EmbeddableOutput,
-    E extends IEmbeddable<I, O> = IEmbeddable<I, O>
-  >(
-    embeddableFactoryId: string,
-    factory: EmbeddableFactoryDefinition<I, O, E>
-  ): (() => EmbeddableFactory<I, O, E>) => {
-    if (this.embeddableFactoryDefinitions.has(embeddableFactoryId)) {
-      throw new Error(
-        `Embeddable factory [embeddableFactoryId = ${embeddableFactoryId}] already registered in Embeddables API.`
-      );
-    }
-    this.embeddableFactoryDefinitions.set(embeddableFactoryId, factory);
-
-    return () => {
-      return this.getEmbeddableFactory(embeddableFactoryId);
-    };
-  };
-
-  private getEmbeddableFactory = <
-    I extends EmbeddableInput = EmbeddableInput,
-    O extends EmbeddableOutput = EmbeddableOutput,
-    E extends IEmbeddable<I, O> = IEmbeddable<I, O>
-  >(
-    embeddableFactoryId: string
-  ): EmbeddableFactory<I, O, E> => {
-    if (!this.isRegistryReady) {
-      throw new Error('Embeddable factories can only be retrieved after setup lifecycle.');
-    }
-    this.ensureFactoryExists(embeddableFactoryId);
-    const factory = this.embeddableFactories.get(embeddableFactoryId);
-
-    return factory as EmbeddableFactory<I, O, E>;
-  };
-
-  // These two functions are only to support legacy plugins registering factories after the start lifecycle.
-  private ensureFactoriesExist = () => {
-    this.embeddableFactoryDefinitions.forEach((def) => this.ensureFactoryExists(def.type));
-  };
-
-  private ensureFactoryExists = (type: string) => {
-    if (!this.embeddableFactories.get(type)) {
-      const def = this.embeddableFactoryDefinitions.get(type);
-      if (!def) return;
-      this.embeddableFactories.set(
-        type,
-        this.customEmbeddableFactoryProvider
-          ? this.customEmbeddableFactoryProvider(def)
-          : defaultEmbeddableFactoryProvider(def)
-      );
-    }
-  };
 }
