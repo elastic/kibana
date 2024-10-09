e59375fe0f2b4d0f435c22504565aa72794d31ee
Mark Hopkin <mark.hopkin@elastic.co>
2024-10-09 14:48:38 +0100

    fix unit tests

--- x-pack/plugins/security_solution/server/lib/entity_analytics/entity_store/entity_store_data_client.test.ts
+++ x-pack/plugins/security_solution/server/lib/entity_analytics/entity_store/entity_store_data_client.test.ts
@@ -62,8 +62,8 @@ describe('EntityStoreDataClient', () => {
       expect(esClientMock.search).toHaveBeenCalledWith(
         expect.objectContaining({
           index: [
-            '.entities.v1.latest.ea_default_host_entity_store',
-            '.entities.v1.latest.ea_default_user_entity_store',
+            '.entities.v1.latest.security_host_default',
+            '.entities.v1.latest.security_user_default',
           ],
         })
       );
