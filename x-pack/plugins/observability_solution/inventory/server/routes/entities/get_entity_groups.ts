6bd81069e0034bc29d1b9fea5319d44dd12d99dd
Gonçalo Rica <goncalo.rica@elastic.co>
2024-10-09 16:06:10 +0200

    Use kuery params for filtering group_by API calls

--- x-pack/plugins/observability_solution/inventory/server/routes/entities/get_entity_groups.ts
+++ x-pack/plugins/observability_solution/inventory/server/routes/entities/get_entity_groups.ts
@@ -5,28 +5,31 @@
  * 2.0.
  */
 
-import { ObservabilityElasticsearchClient } from '@kbn/observability-utils/es/client/create_observability_es_client';
+import type { ObservabilityElasticsearchClient } from '@kbn/observability-utils/es/client/create_observability_es_client';
+import { kqlQuery } from '@kbn/observability-utils/es/queries/kql_query';
 import { esqlResultToPlainObjects } from '@kbn/observability-utils/es/utils/esql_result_to_plain_objects';
 import { ENTITIES_LATEST_ALIAS, MAX_NUMBER_OF_ENTITIES } from '../../../common/entities';
 
 export async function getEntityGroupsBy({
   inventoryEsClient,
   field,
-  where,
+  kuery,
 }: {
   inventoryEsClient: ObservabilityElasticsearchClient;
   field: string;
-  where?: string[];
+  kuery?: string;
 }) {
-  const whereClauses = where?.map((filter) => `WHERE ${filter}`).join('\n |');
-
   const groups = await inventoryEsClient.esql('get_entities_groups', {
     query: `
         FROM ${ENTITIES_LATEST_ALIAS}
-        ${whereClauses ? `| ${whereClauses}` : ''}
         | STATS count = COUNT(*) by ${field}
         | LIMIT ${MAX_NUMBER_OF_ENTITIES}
       `,
+    filter: {
+      bool: {
+        filter: [...kqlQuery(kuery)],
+      },
+    },
   });
 
   return esqlResultToPlainObjects(groups);
