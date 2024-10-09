cf4080ba578f211ff1b272513b41e8180f715eeb
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2024-10-09 10:05:49 +0200

    fix CI

--- x-pack/plugins/security_solution/scripts/run_cypress/parallel.ts
+++ x-pack/plugins/security_solution/scripts/run_cypress/parallel.ts
@@ -97,7 +97,7 @@ ${JSON.stringify(cypressConfigFile, null, 2)}
       const grepSpecPattern = grep({
         ...cypressConfigFile,
         specPattern,
-        excludeSpecPattern: cypressConfigFile.excludeSpecPattern ?? [],
+        excludeSpecPattern: cypressConfigFile.e2e?.excludeSpecPattern ?? [],
       }).specPattern;
 
       log.info('Resolved spec files or pattern after grep:', grepSpecPattern);
