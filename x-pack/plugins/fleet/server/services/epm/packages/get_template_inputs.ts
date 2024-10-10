1e7c0a65103b0632fa431dd339c912f5921d5268
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2024-10-10 12:25:06 +0200

    [Fleet] Add placeholder and comments to integration config

--- x-pack/plugins/fleet/server/services/epm/packages/get_template_inputs.ts
+++ x-pack/plugins/fleet/server/services/epm/packages/get_template_inputs.ts
@@ -8,8 +8,14 @@ import type { SavedObjectsClientContract } from '@kbn/core/server';
 
 import { merge } from 'lodash';
 import { dump } from 'js-yaml';
+import yamlDoc from 'yaml';
 
-import { packageToPackagePolicy } from '../../../../common/services/package_to_package_policy';
+import { getNormalizedInputs, isIntegrationPolicyTemplate } from '../../../../common/services';
+
+import {
+  getStreamsForInputType,
+  packageToPackagePolicy,
+} from '../../../../common/services/package_to_package_policy';
 import { getInputsWithStreamIds, _compilePackagePolicyInputs } from '../../package_policy';
 import { appContextService } from '../../app_context';
 import type {
@@ -17,6 +23,7 @@ import type {
   NewPackagePolicy,
   PackagePolicyInput,
   TemplateAgentPolicyInput,
+  RegistryVarsEntry,
 } from '../../../../common/types';
 import { _sortYamlKeys } from '../../../../common/services/full_agent_policy_to_yaml';
 
@@ -26,6 +33,21 @@ import { getPackageInfo } from '.';
 import { getPackageAssetsMap } from './get';
 
 type Format = 'yml' | 'json';
+
+type PackageComments = Record<
+  string,
+  {
+    comment: string;
+    vars: Record<string, RegistryVarsEntry>;
+    streams: Record<
+      string,
+      {
+        comment: string;
+        vars: Record<string, RegistryVarsEntry>;
+      }
+    >;
+  }
+>;
 
 // Function based off storedPackagePolicyToAgentInputs, it only creates the `streams` section instead of the FullAgentPolicyInput
 export const templatePackagePolicyToFullInputStreams = (
@@ -38,7 +60,7 @@ export const templatePackagePolicyToFullInputStreams = (
   packagePolicyInputs.forEach((input) => {
     const fullInputStream = {
       // @ts-ignore-next-line the following id is actually one level above the one in fullInputStream, but the linter thinks it gets overwritten
-      id: input.policy_template ? `${input.type}-${input.policy_template}` : `${input.type}`,
+      id: input.policy_template ? `${input.policy_template}-${input.type}` : `${input.type}`,
       type: input.type,
       ...getFullInputStreams(input, true),
     };
@@ -95,8 +117,57 @@ export async function getTemplateInputs(
       ignoreUnverified,
     });
   }
+
   const emptyPackagePolicy = packageToPackagePolicy(packageInfo, '');
+
   const inputsWithStreamIds = getInputsWithStreamIds(emptyPackagePolicy, undefined, true);
+
+  // Add a placeholder <VAR_NAME> to all variables without default value
+  for (const inputWithStreamIds of inputsWithStreamIds) {
+    const policyTemplate = packageInfo.policy_templates?.find(
+      (_policyTemplate) => _policyTemplate.name === inputWithStreamIds.policy_template
+    );
+    if (!policyTemplate) {
+      continue;
+    }
+    const normalizedInputs = getNormalizedInputs(policyTemplate);
+
+    const packageInput = normalizedInputs.find((_input) => _input.type === inputWithStreamIds.type);
+    if (!packageInput) {
+      continue;
+    }
+    const streams = getStreamsForInputType(
+      packageInput.type,
+      packageInfo,
+      isIntegrationPolicyTemplate(policyTemplate) && policyTemplate.data_streams
+        ? policyTemplate.data_streams
+        : []
+    );
+
+    for (const [inputVarKey, inputVarValue] of Object.entries(inputWithStreamIds.vars ?? {})) {
+      const varDef = packageInput.vars?.find((_varDef) => _varDef.name === inputVarKey);
+      if (varDef && !inputVarValue.value && varDef.type !== 'yaml') {
+        const placeHolder = `<${inputVarKey.toUpperCase()}>`;
+        inputVarValue.value = placeHolder;
+      }
+    }
+    for (const stream of inputWithStreamIds.streams) {
+      const packageStream = streams.find(
+        (_stream) => _stream.data_stream.dataset === stream.data_stream.dataset
+      );
+      if (!packageStream) {
+        continue;
+      }
+      for (const [streamVarKey, streamVarValue] of Object.entries(stream.vars ?? {})) {
+        const varDef = packageStream.vars?.find((_varDef) => _varDef.name === streamVarKey);
+        if (varDef && !streamVarValue.value && varDef.type !== 'yaml') {
+          const placeHolder = `<${streamVarKey.toUpperCase()}>`;
+          streamVarValue.value = placeHolder;
+        }
+      }
+    }
+  }
+
   const assetsMap = await getPackageAssetsMap({
     logger: appContextService.getLogger(),
     packageInfo,
@@ -128,7 +199,143 @@ export async function getTemplateInputs(
         sortKeys: _sortYamlKeys,
       }
     );
-    return yaml;
+    return addCommentsToYaml(yaml, buildPackageComments(packageInfo));
   }
+
   return { inputs: [] };
+}
+
+function buildPackageComments(packageInfo: PackageInfo): PackageComments {
+  return (
+    packageInfo.policy_templates?.reduce<
+      Record<
+        string,
+        {
+          comment: string;
+          vars: Record<string, RegistryVarsEntry>;
+          streams: Record<
+            string,
+            {
+              comment: string;
+              vars: Record<string, RegistryVarsEntry>;
+            }
+          >;
+        }
+      >
+    >((inputComments, policyTemplate) => {
+      const inputs = getNormalizedInputs(policyTemplate);
+
+      inputs.forEach((packageInput) => {
+        const inputId = `${policyTemplate.name}-${packageInput.type}`;
+
+        const streams = getStreamsForInputType(
+          packageInput.type,
+          packageInfo,
+          isIntegrationPolicyTemplate(policyTemplate) && policyTemplate.data_streams
+            ? policyTemplate.data_streams
+            : []
+        ).reduce<Record<string, { comment: string; vars: Record<string, RegistryVarsEntry> }>>(
+          (acc, stream) => {
+            const streamId = `${packageInput.type}-${stream.data_stream.dataset}`;
+            acc[streamId] = {
+              comment: ` ${stream.title} ${stream.description}`,
+              vars:
+                stream.vars?.reduce<Record<string, RegistryVarsEntry>>((varsAcc, varDef) => {
+                  varsAcc[`<${varDef.name.toUpperCase()}>`] = varDef;
+                  return varsAcc;
+                }, {}) ?? {},
+            };
+            return acc;
+          },
+          {}
+        );
+
+        const vars =
+          packageInput.vars?.reduce<Record<string, RegistryVarsEntry>>((acc, varDef) => {
+            acc[`<${varDef.name.toUpperCase()}>`] = varDef;
+            return acc;
+          }, {}) ?? {};
+
+        inputComments[inputId] = {
+          comment: ` ${packageInput.title} ${packageInput.description}`,
+          streams,
+          vars,
+        };
+      });
+      return inputComments;
+    }, {}) ?? {}
+  );
+}
+
+function addCommentsToYaml(yaml: string, packageComments: PackageComments) {
+  const doc = yamlDoc.parseDocument(yaml);
+  // Add input and streams comments
+  const yamlInputs = doc.get('inputs');
+  if (yamlDoc.isCollection(yamlInputs)) {
+    yamlInputs.items.forEach((inputItem) => {
+      if (!yamlDoc.isMap(inputItem)) {
+        return;
+      }
+      const inputIdNode = inputItem.get('id', true);
+      if (!yamlDoc.isScalar(inputIdNode)) {
+        return;
+      }
+      const inputId = inputIdNode.value as string;
+      const inputComments = packageComments[inputId];
+      if (inputComments) {
+        inputItem.commentBefore = inputComments.comment;
+
+        yamlDoc.visit(inputItem, {
+          Scalar(key, node) {
+            if (node.value) {
+              const val = node.value.toString();
+              for (const placeholder of Object.keys(inputComments.vars)) {
+                if (val.includes(placeholder)) {
+                  const varDef = inputComments.vars[placeholder];
+                  node.comment = ` ${varDef.title}${
+                    varDef.description ? ` ${varDef.description}` : ''
+                  }`;
+                }
+              }
+            }
+          },
+        });
+
+        const yamlStreams = inputItem.get('streams');
+        if (!yamlDoc.isCollection(yamlStreams)) {
+          return;
+        }
+        yamlStreams.items.forEach((streamItem) => {
+          if (!yamlDoc.isMap(streamItem)) {
+            return;
+          }
+          const streamIdNode = streamItem.get('id', true);
+          if (yamlDoc.isScalar(streamIdNode)) {
+            const streamId = streamIdNode.value as string;
+            const streamComments = inputComments.streams[streamId];
+            if (streamComments) {
+              streamItem.commentBefore = streamComments.comment;
+              yamlDoc.visit(streamItem, {
+                Scalar(key, node) {
+                  if (node.value) {
+                    const val = node.value.toString();
+                    for (const placeholder of Object.keys(streamComments.vars)) {
+                      if (val.includes(placeholder)) {
+                        const varDef = streamComments.vars[placeholder];
+                        node.comment = ` ${varDef.title}${
+                          varDef.description ? ` ${varDef.description}` : ''
+                        }`;
+                      }
+                    }
+                  }
+                },
+              });
+            }
+          }
+        });
+      }
+    });
+  }
+
+  return doc.toString();
 }
