c3bf12553f44040c3d2380d6103ad09d6436a040
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-10-10 17:21:32 +0200

    Update ELU history to use exponential moving average

---
+++ src/core/packages/metrics/server-internal/src/exponential_moving_average.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { type OperatorFunction, map, startWith, pairwise } from 'rxjs';
+
+/**
+ * An RxJS operator implementing the exponential moving average function.
+ *
+ * @see https://en.wikipedia.org/wiki/Exponential_smoothing
+ * @param period The period of time.
+ * @param interval The interval between values.
+ * @returns An operator emitting smoothed values.
+ */
+export function exponentialMovingAverage(
+  period: number,
+  interval: number
+): OperatorFunction<number, number> {
+  const alpha = 1 - Math.exp(-interval / period);
+
+  return (inner) =>
+    inner.pipe(
+      startWith(undefined),
+      pairwise(),
+      map(([previous, current]) =>
+        previous == null ? current! : alpha * current! + (1 - alpha) * previous
+      )
+    );
+}
