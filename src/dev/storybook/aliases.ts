29e854133f7ca5fa3ad94e5d36453b4ffce32bb5
Nathan L Smith <nathan.smith@elastic.co>
2024-10-10 21:49:19 -0500

    add obs inventory storybook to aliases

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -55,6 +55,7 @@ export const storybookAliases = {
     'x-pack/plugins/observability_solution/observability_ai_assistant/.storybook',
   observability_ai_assistant_app:
     'x-pack/plugins/observability_solution/observability_ai_assistant_app/.storybook',
+  observability_inventory: 'x-pack/plugins/observability_solution/inventory/.storybook',
   observability_slo: 'x-pack/plugins/observability_solution/slo/.storybook',
   presentation: 'src/plugins/presentation_util/storybook',
   random_sampling: 'x-pack/packages/kbn-random-sampling/.storybook',
