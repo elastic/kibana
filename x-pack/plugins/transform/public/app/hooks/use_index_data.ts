743f3a5ac05f5743cbb3a80a55957e47fe20ab5c
Thomas Neirynck <thomas@elastic.co>
2024-10-11 15:53:03 -0400

    fix transform

--- x-pack/plugins/transform/public/app/hooks/use_index_data.ts
+++ x-pack/plugins/transform/public/app/hooks/use_index_data.ts
@@ -28,6 +28,7 @@ import {
 } from '@kbn/ml-data-grid';
 import type { TimeRange as TimeRangeMs } from '@kbn/ml-date-picker';
 
+import { isCCSRemoteIndexName } from '@kbn/es-query';
 import {
   hasKeywordDuplicate,
   isKeywordDuplicate,
@@ -176,7 +177,7 @@ export const useIndexData = (options: UseIndexDataOptions): UseIndexDataReturnTy
       setErrorMessage(getErrorMessage(dataGridDataError));
       setStatus(INDEX_STATUS.ERROR);
     } else if (!dataGridDataIsLoading && !dataGridDataIsError && dataGridData !== undefined) {
-      const isCrossClusterSearch = indexPattern.includes(':');
+      const isCrossClusterSearch = isCCSRemoteIndexName(indexPattern);
       const isMissingFields = dataGridData.hits.hits.every((d) => typeof d.fields === 'undefined');
 
       const docs = dataGridData.hits.hits.map((d) => getProcessedFields(d.fields ?? {}));
