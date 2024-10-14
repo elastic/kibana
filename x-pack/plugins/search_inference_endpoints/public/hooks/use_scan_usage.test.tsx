9f41a2f913d3422e17523fc70d6a9094405ecd9b
Eyo Okon Eyo <eyo.eyo@elastic.co>
2024-10-14 18:42:26 +0100

    fix missed waitForNextUpdate

--- x-pack/plugins/search_inference_endpoints/public/hooks/use_scan_usage.test.tsx
+++ x-pack/plugins/search_inference_endpoints/public/hooks/use_scan_usage.test.tsx
@@ -7,6 +7,7 @@
 
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { waitFor } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
 
 import { useScanUsage } from './use_scan_usage';
@@ -43,7 +44,7 @@ describe('useScanUsage', () => {
   });
 
   test('should call API endpoint with the correct parameters and return response', async () => {
-    const { result, waitForNextUpdate } = renderHook(
+    const { result } = renderHook(
       () =>
         useScanUsage({
           type: 'text_embedding',
@@ -52,18 +53,18 @@ describe('useScanUsage', () => {
       { wrapper }
     );
 
-    await waitForNextUpdate();
+    await waitFor(() => {
+      expect(mockDelete).toHaveBeenCalledWith(
+        '/internal/inference_endpoint/endpoints/text_embedding/in-1',
+        { query: { scanUsage: true } }
+      );
 
-    expect(mockDelete).toHaveBeenCalledWith(
-      '/internal/inference_endpoint/endpoints/text_embedding/in-1',
-      { query: { scanUsage: true } }
-    );
-
-    expect(result.current.data).toEqual({
-      acknowledge: true,
-      error_message: 'inference id is being used',
-      indexes: ['index1', 'index2'],
-      pipelines: ['pipeline1', 'pipeline2'],
+      expect(result.current.data).toEqual({
+        acknowledge: true,
+        error_message: 'inference id is being used',
+        indexes: ['index1', 'index2'],
+        pipelines: ['pipeline1', 'pipeline2'],
+      });
     });
   });
 });
