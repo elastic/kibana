9a4741b55ba2220334660a7c388b06b6786b2179
Nikita Indik <nikita.indik@elastic.co>
2024-10-14 13:15:02 +0200

    Update code comment text

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/comparison_side/utils.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/comparison_side/utils.ts
@@ -17,7 +17,7 @@ import type {
  *
  * @param version - The version for which the field value is to be picked.
  * @param fieldThreeWayDiff - The three-way diff object containing the field values for different versions.
- * @param resolvedValue - Field value that the upgraded rule will have.
+ * @param resolvedValue - A value field will be upgraded to.
  * @returns - The field value for the specified version
  */
 export function pickFieldValueForVersion<FieldName extends keyof DiffableAllFields>(
