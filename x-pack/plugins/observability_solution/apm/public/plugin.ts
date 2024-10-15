2a32915daa3292dcc5bef6d25afd50763978a98b
iblancof <irene.blanco@elastic.co>
2024-10-15 15:33:28 +0200

    Create useEntityManager

--- x-pack/plugins/observability_solution/apm/public/plugin.ts
+++ x-pack/plugins/observability_solution/apm/public/plugin.ts
@@ -70,6 +70,7 @@ import { map } from 'rxjs';
 import type { CloudSetup } from '@kbn/cloud-plugin/public';
 import type { ServerlessPluginStart } from '@kbn/serverless/public';
 import { LogsSharedClientStartExports } from '@kbn/logs-shared-plugin/public';
+import { EntityManagerPublicPluginStart } from '@kbn/entityManager-plugin/public';
 import type { ConfigSchema } from '.';
 import { registerApmRuleTypes } from './components/alerting/rule_types/register_apm_rule_types';
 import { registerEmbeddables } from './embeddable/register_embeddables';
@@ -144,6 +145,7 @@ export interface ApmPluginStartDeps {
   metricsDataAccess: MetricsDataPluginStart;
   uiSettings: IUiSettingsClient;
   logsShared: LogsSharedClientStartExports;
+  entityManager: EntityManagerPublicPluginStart;
 }
 
 const applicationsTitle = i18n.translate('xpack.apm.navigation.rootTitle', {
