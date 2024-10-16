9bca8b744af8bf2b2b5e5ff74680c494ae7d1824
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-10-16 23:12:22 +1100

    skip failing test suite (#196462)

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/rule_bulk_actions/trial_license_complete_tier/perform_bulk_action_ess.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/rule_bulk_actions/trial_license_complete_tier/perform_bulk_action_ess.ts
@@ -48,6 +48,7 @@ export default ({ getService }: FtrProviderContext): void => {
 
   // Failing: See https://github.com/elastic/kibana/issues/173804
   // Failing: See https://github.com/elastic/kibana/issues/196470
+  // Failing: See https://github.com/elastic/kibana/issues/196462
   describe.skip('@ess perform_bulk_action - ESS specific logic', () => {
     beforeEach(async () => {
       await deleteAllRules(supertest, log);
