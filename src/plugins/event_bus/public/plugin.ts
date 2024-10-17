a20ab5c51b0c53480fbf9516b72c8811fbde86c8
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-10-17 23:17:09 +0200

    make event bus available for other plugins

--- src/plugins/event_bus/public/plugin.ts
+++ src/plugins/event_bus/public/plugin.ts
@@ -7,23 +7,15 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { i18n } from '@kbn/i18n';
 import type { CoreSetup, CoreStart, Plugin } from '@kbn/core/public';
+
+import { eventBus } from './event_bus';
 import type { EventBusPluginSetup, EventBusPluginStart } from './types';
-import { PLUGIN_NAME } from '../common';
 
 export class EventBusPlugin implements Plugin<EventBusPluginSetup, EventBusPluginStart> {
   public setup(core: CoreSetup): EventBusPluginSetup {
-    // Return methods that should be available to other plugins
     return {
-      getGreeting() {
-        return i18n.translate('eventBus.greetingText', {
-          defaultMessage: 'Hello from {name}!',
-          values: {
-            name: PLUGIN_NAME,
-          },
-        });
-      },
+      eventBus,
     };
   }
 
