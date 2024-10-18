debb649356c2bcb79fb21a0aabfdc00f495c2d1e
Thomas Neirynck <thomas@elastic.co>
2024-10-18 08:40:04 -0400

    [Chore] Isolate CCS check (#195988)
    ## Summary
    
    Isolate CCS check in single location.
    
    Closes
    https://github.com/elastic/kibana/issues/193906#issuecomment-2400370478
    
    ### For maintainers
    
    - [x] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#_add_your_labels)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit f86d1716b171d846851041d50da074683624fa02)

--- packages/kbn-es-query/index.ts
+++ packages/kbn-es-query/index.ts
@@ -127,6 +127,7 @@ export {
   getDataViewFieldSubtypeNested,
   isDataViewFieldSubtypeMulti,
   isDataViewFieldSubtypeNested,
+  isCCSRemoteIndexName,
 } from './src/utils';
 
 export type { ExecutionContextSearch } from './src/expressions/types';
