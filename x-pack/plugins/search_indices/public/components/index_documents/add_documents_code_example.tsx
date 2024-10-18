4b73c779e84c4969f7be24292d25becfa47d48a7
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-10-19 06:56:42 +1100

    [8.x] [Onboarding] Better onboarding copy (#196895) (#196934)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[Onboarding] Better onboarding copy
    (#196895)](https://github.com/elastic/kibana/pull/196895)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Joe
    McElroy","email":"joseph.mcelroy@elastic.co"},"sourceCommit":{"committedDate":"2024-10-18T18:03:51Z","message":"[Onboarding]
    Better onboarding copy (#196895)\n\n## Summary\r\n\r\nImproved
    onboarding copy\r\n\r\n<img width=\"1433\"
    alt=\"image\"\r\nsrc=\"https://github.com/user-attachments/assets/b25b7677-8acc-4875-b775-5276956ba69b\">\r\n\r\n---------\r\n\r\nCo-authored-by:
    Michael DeFazio <michael.defazio@elastic.co>\r\nCo-authored-by: Liam
    Thompson
    <32779855+leemthompo@users.noreply.github.com>","sha":"5aad97651dba2fd0049f1599a78aa84d39403526","branchLabelMapping":{"^v9.0.0$":"main","^v8.17.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","v9.0.0","Team:Search","backport:prev-minor"],"title":"[Onboarding]
    Better onboarding
    copy","number":196895,"url":"https://github.com/elastic/kibana/pull/196895","mergeCommit":{"message":"[Onboarding]
    Better onboarding copy (#196895)\n\n## Summary\r\n\r\nImproved
    onboarding copy\r\n\r\n<img width=\"1433\"
    alt=\"image\"\r\nsrc=\"https://github.com/user-attachments/assets/b25b7677-8acc-4875-b775-5276956ba69b\">\r\n\r\n---------\r\n\r\nCo-authored-by:
    Michael DeFazio <michael.defazio@elastic.co>\r\nCo-authored-by: Liam
    Thompson
    <32779855+leemthompo@users.noreply.github.com>","sha":"5aad97651dba2fd0049f1599a78aa84d39403526"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","branchLabelMappingKey":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/196895","number":196895,"mergeCommit":{"message":"[Onboarding]
    Better onboarding copy (#196895)\n\n## Summary\r\n\r\nImproved
    onboarding copy\r\n\r\n<img width=\"1433\"
    alt=\"image\"\r\nsrc=\"https://github.com/user-attachments/assets/b25b7677-8acc-4875-b775-5276956ba69b\">\r\n\r\n---------\r\n\r\nCo-authored-by:
    Michael DeFazio <michael.defazio@elastic.co>\r\nCo-authored-by: Liam
    Thompson
    <32779855+leemthompo@users.noreply.github.com>","sha":"5aad97651dba2fd0049f1599a78aa84d39403526"}}]}]
    BACKPORT-->
    
    Co-authored-by: Joe McElroy <joseph.mcelroy@elastic.co>

--- x-pack/plugins/search_indices/public/components/index_documents/add_documents_code_example.tsx
+++ x-pack/plugins/search_indices/public/components/index_documents/add_documents_code_example.tsx
@@ -7,7 +7,7 @@
 
 import React, { useCallback, useMemo, useState } from 'react';
 import { MappingProperty } from '@elastic/elasticsearch/lib/api/types';
-import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiText } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiPanel } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { TryInConsoleButton } from '@kbn/try-in-console';
 
@@ -112,16 +112,12 @@ export const AddDocumentsCodeExample = ({
             </EuiFlexItem>
           )}
         </EuiFlexGroup>
-        <EuiText>
-          <p>{ingestCodeExamples.description}</p>
-        </EuiText>
         {selectedCodeExamples.installCommand && (
           <EuiFlexItem>
             <CodeSample
               id="installCodeExample"
-              title={i18n.translate('xpack.searchIndices.indexDetails.installLibrary.title', {
-                defaultMessage: 'Install Elasticsearch library',
-              })}
+              title={ingestCodeExamples.installTitle}
+              description={ingestCodeExamples.installDescription}
               language="shell"
               code={selectedCodeExamples.installCommand}
               onCodeCopyClick={() => {
@@ -137,9 +133,8 @@ export const AddDocumentsCodeExample = ({
           <EuiFlexItem>
             <CodeSample
               id="addMappingsCodeExample"
-              title={i18n.translate('xpack.searchIndices.indexDetails.addMappingsCode.title', {
-                defaultMessage: 'Add mappings to your index',
-              })}
+              title={ingestCodeExamples.addMappingsTitle}
+              description={ingestCodeExamples.addMappingsDescription}
               language={Languages[selectedLanguage].codeBlockLanguage}
               code={selectedCodeExamples.updateMappingsCommand(codeParams)}
               onCodeCopyClick={() => {
@@ -154,7 +149,16 @@ export const AddDocumentsCodeExample = ({
         <EuiFlexItem>
           <CodeSample
             id="ingestDataCodeExample"
-            title={ingestCodeExamples.ingestTitle}
+            title={i18n.translate('xpack.searchIndices.indexDetails.ingestDocuments.title', {
+              defaultMessage: 'Ingest documents',
+            })}
+            description={i18n.translate(
+              'xpack.searchIndices.indexDetails.ingestDocuments.description',
+              {
+                defaultMessage:
+                  'Next, use the Elasticsearch bulk API to ingest an array of documents into the index.',
+              }
+            )}
             language={Languages[selectedLanguage].codeBlockLanguage}
             code={selectedCodeExamples.ingestCommand(codeParams)}
             onCodeCopyClick={() => {
