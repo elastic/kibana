2b795e89ad1ef6934a34e85404a40a7a684e05d8
dkirchan <diamantis.kirchantzoglou@elastic.co>
2024-10-18 15:09:10 +0300

    Added prepended title reference

--- packages/kbn-failed-test-reporter-cli/failed_tests_reporter/failed_tests_reporter_cli.ts
+++ packages/kbn-failed-test-reporter-cli/failed_tests_reporter/failed_tests_reporter_cli.ts
@@ -44,6 +44,7 @@ run(
 
     let branch: string = '';
     let pipeline: string = '';
+    let prependTitle: string = '';
     if (updateGithub) {
       let isPr = false;
 
@@ -52,6 +53,7 @@ run(
         pipeline = process.env.BUILDKITE_PIPELINE_SLUG || '';
         isPr = process.env.BUILDKITE_PULL_REQUEST === 'true';
         updateGithub = process.env.REPORT_FAILED_TESTS_TO_GITHUB === 'true';
+        prependTitle = process.env.PREPEND_FAILURE_TITLE || '';
       } else {
         // JOB_NAME is formatted as `elastic+kibana+7.x` in some places and `elastic+kibana+7.x/JOB=kibana-intake,node=immutable` in others
         const jobNameSplit = (process.env.JOB_NAME || '').split(/\+|\//);
