0d947691961f297e3bea0a1687ba958d3d735767
Brad White <brad.white@elastic.co>
2024-10-18 01:35:39 -0700

    reorder

--- packages/kbn-storybook/src/lib/default_config.ts
+++ packages/kbn-storybook/src/lib/default_config.ts
@@ -92,22 +92,6 @@ export const defaultConfig: StorybookConfig = {
       })
     );
 
-    config?.module?.rules?.push({
-      test: /\.(js|jsx|ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-          options: {
-            presets: [
-              require.resolve('@babel/preset-env'),
-              require.resolve('@babel/preset-react'),
-              [require.resolve('@babel/preset-typescript'), { allowDeclareFields: true }],
-            ],
-          },
-        },
-      ],
-    });
-
     // Same, but for imports statements which import modules outside of the directory (../)
     config.plugins?.push(
       new webpack.NormalModuleReplacementPlugin(/^\.\.\//, async (resource: any) => {
@@ -127,6 +111,22 @@ export const defaultConfig: StorybookConfig = {
         }
       })
     );
+
+    config?.module?.rules?.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('babel-loader'),
+          options: {
+            presets: [
+              require.resolve('@babel/preset-env'),
+              require.resolve('@babel/preset-react'),
+              [require.resolve('@babel/preset-typescript'), { allowDeclareFields: true }],
+            ],
+          },
+        },
+      ],
+    });
 
     config.resolve = {
       ...config.resolve,
