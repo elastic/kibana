b8ad058f41123a45469b9abeadfed7d31c8c3d1c
Jon <jon@elastic.co>
2024-10-18 08:00:57 -0500

    [ci] Use es snapshot cache for jest integration tests (#196695)

--- .buildkite/scripts/steps/test/jest_integration.sh
+++ .buildkite/scripts/steps/test/jest_integration.sh
@@ -7,6 +7,7 @@ source .buildkite/scripts/common/util.sh
 is_test_execution_step
 
 .buildkite/scripts/bootstrap.sh
+.buildkite/scripts/copy_es_snapshot_cache.sh
 
 echo '--- Jest Integration Tests'
 checks-reporter-with-killswitch "Jest Integration Tests $((BUILDKITE_PARALLEL_JOB+1))" \
