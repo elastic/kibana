37f420aa25a1f5ac24ebc5e43fa6ea6cd034d8d7
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-10-20 23:27:59 +0200

    support optional selector

--- src/plugins/event_bus/public/event_bus.ts
+++ src/plugins/event_bus/public/event_bus.ts
@@ -7,9 +7,36 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { distinctUntilChanged, scan, BehaviorSubject } from 'rxjs';
+import { distinctUntilChanged, map, scan, BehaviorSubject, type Subscription } from 'rxjs';
 import { isEqual } from 'lodash';
 import { Slice } from '@reduxjs/toolkit';
+
+/**
+ * Check if a value is a JS primitive.
+ * @param value
+ * @returns whether the value is a JS primitive.
+ */
+function isPrimitive(value: unknown) {
+  return value === null || (typeof value !== 'object' && typeof value !== 'function');
+}
+
+/**
+ * Custom isEqual function that handles NaN and delegates to lodash's isEqual for objects.
+ * @param prev The previous value
+ * @param curr The current value
+ * @returns whether the two values are equal
+ */
+function customIsEqual(prev: any, curr: any): boolean {
+  if (isPrimitive(prev) && isPrimitive(curr)) {
+    // Special handling for NaN
+    if (typeof prev === 'number' && typeof curr === 'number') {
+      return Object.is(prev, curr);
+    }
+    return prev === curr;
+  } else {
+    return isEqual(prev, curr);
+  }
+}
 
 export interface Action<T = any> {
   type: string;
@@ -18,7 +45,7 @@ export interface Action<T = any> {
 
 export class EventBus<S extends Slice> {
   public subject: BehaviorSubject<Action>;
-  private slice: S;
+  public slice: S;
   public actions: S['actions'];
 
   constructor(slice: S) {
@@ -27,10 +54,26 @@ export class EventBus<S extends Slice> {
     this.actions = this.wrapActions(slice.actions);
   }
 
-  // Subscribe to this event bus
-  subscribe(cb: (state: ReturnType<S['reducer']>) => void) {
+  // Subscribe to this event bus with an optional selector
+  // without needing to provide the generic type.
+  subscribe(cb: (state: ReturnType<S['reducer']>) => void): Subscription;
+  subscribe(cb: (state: ReturnType<S['reducer']>) => void): Subscription;
+  subscribe<SelectedState>(
+    cb: (selectedState: SelectedState) => void,
+    selector: (state: ReturnType<S['reducer']>) => SelectedState
+  ): Subscription;
+  subscribe<SelectedState = ReturnType<S['reducer']>>(
+    cb: (state: SelectedState) => void,
+    selector?: (state: ReturnType<S['reducer']>) => SelectedState
+  ): Subscription {
     return this.subject
-      .pipe(scan(this.slice.reducer, this.slice.getInitialState()), distinctUntilChanged(isEqual))
+      .pipe(
+        scan(this.slice.reducer, this.slice.getInitialState()),
+        // Apply selector if provided
+        map((state) => (selector ? selector(state) : state)),
+        // Emit only if the selected state has changed
+        distinctUntilChanged(customIsEqual)
+      )
       .subscribe(cb);
   }
 
