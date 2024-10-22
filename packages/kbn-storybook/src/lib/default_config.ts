0088df15a8244a1f597bf128203e9a077fd21349
Brad White <brad.white@elastic.co>
2024-10-22 17:23:15 +1000

    remove extra babel config

--- packages/kbn-storybook/src/lib/default_config.ts
+++ packages/kbn-storybook/src/lib/default_config.ts
@@ -158,22 +158,6 @@ export const defaultConfig: StorybookConfig = {
       })
     );
 
-    // config?.module?.rules?.push({
-    //   test: /\.(js|jsx|ts|tsx)$/,
-    //   use: [
-    //     {
-    //       loader: require.resolve('babel-loader'),
-    //       options: {
-    //         presets: [
-    //           require.resolve('@babel/preset-env'),
-    //           require.resolve('@babel/preset-react'),
-    //           [require.resolve('@babel/preset-typescript'), { allowDeclareFields: true }],
-    //         ],
-    //       },
-    //     },
-    //   ],
-    // });
-
     config.resolve = {
       ...config.resolve,
       fallback: {
