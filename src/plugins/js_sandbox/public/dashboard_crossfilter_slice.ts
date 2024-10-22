9c276aafe7bdf3db1f43d3f7d2388d4fd6ec7eb0
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-10-22 16:40:32 +0200

    crossfilter support

---
+++ src/plugins/js_sandbox/public/dashboard_crossfilter_slice.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import type { PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+
+interface DashboardCrossfilterState {
+  filters: Record<string, string>;
+}
+
+interface SetCrossfilterActionPayload {
+  id: string;
+  filter: string;
+}
+
+function getDefaultState(): DashboardCrossfilterState {
+  return {
+    filters: {},
+  };
+}
+
+export const dashboardCrossfilterSlice = createSlice({
+  name: 'dashboardCrossfilter',
+  initialState: getDefaultState(),
+  reducers: {
+    setCrossfilter: (
+      state: DashboardCrossfilterState,
+      action: PayloadAction<SetCrossfilterActionPayload>
+    ) => {
+      if (action.payload.filter) {
+        state.filters[action.payload.id] = action.payload.filter;
+      } else {
+        delete state.filters[action.payload.id];
+      }
+    },
+  },
+});
+
+export type DashboardCrossfilterSlice = typeof dashboardCrossfilterSlice;
