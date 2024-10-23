0610b6d6107f151f38d4ebb0e987a460164b35f9
Steph Milovic <stephanie.milovic@elastic.co>
2024-10-23 08:45:11 -0600

    fix tests

--- x-pack/packages/kbn-elastic-assistant/impl/tour/knowledge_base/index.test.tsx
+++ x-pack/packages/kbn-elastic-assistant/impl/tour/knowledge_base/index.test.tsx
@@ -7,131 +7,136 @@
 
 import React from 'react';
 
-import { useIsElementMounted } from '../../detection_engine/rule_management_ui/components/rules_table/rules_table/guided_onboarding/use_is_element_mounted';
 import { render, screen } from '@testing-library/react';
-import {
-  createMockStore,
-  createSecuritySolutionStorageMock,
-  TestProviders,
-} from '../../common/mock';
-import { useKibana as mockUseKibana } from '../../common/lib/kibana/__mocks__';
-import { useKibana } from '../../common/lib/kibana';
+import { EuiTourStepProps } from '@elastic/eui';
+import useLocalStorage from 'react-use/lib/useLocalStorage';
 import { KnowledgeBaseTour } from '.';
-import { KNOWLEDGE_BASE_TOUR_CONFIG_ANCHORS } from './step_config';
-import { NEW_FEATURES_TOUR_STORAGE_KEYS, SecurityPageName } from '../../../common/constants';
-import type { RouteSpyState } from '../../common/utils/route/types';
-import { useRouteSpy } from '../../common/utils/route/use_route_spy';
-
-const mockRouteSpy: RouteSpyState = {
-  pageName: SecurityPageName.overview,
-  detailName: undefined,
-  tabName: undefined,
-  search: '',
-  pathName: '/',
-};
-jest.mock(
-  '../../detection_engine/rule_management_ui/components/rules_table/rules_table/guided_onboarding/use_is_element_mounted'
-);
-jest.mock('../../common/lib/kibana');
-jest.mock('../../common/utils/route/use_route_spy');
+import { TestProviders } from '../../mock/test_providers/test_providers';
+import { useAssistantContext } from '../../..';
+jest.mock('../../..');
+jest.mock('react-use/lib/useLocalStorage');
 jest.mock('@elastic/eui', () => {
   const original = jest.requireActual('@elastic/eui');
   return {
     ...original,
-    EuiTourStep: () => <div data-test-subj="knowledgeBase-tour-step-1" />,
+    EuiTourStep: ({ children, panelProps }: EuiTourStepProps) =>
+      children ? (
+        <div data-test-subj={panelProps?.['data-test-subj']}>{children}</div>
+      ) : (
+        <div data-test-subj={panelProps?.['data-test-subj']} />
+      ),
   };
 });
-const mockedUseKibana = mockUseKibana();
-
-const { storage: storageMock } = createSecuritySolutionStorageMock();
-const mockStore = createMockStore(undefined, undefined, undefined, storageMock);
-
-const TestComponent = () => {
-  return (
-    <TestProviders store={mockStore}>
-      <div id={KNOWLEDGE_BASE_TOUR_CONFIG_ANCHORS.NAV_LINK} />
-      <KnowledgeBaseTour />
-    </TestProviders>
-  );
-};
 
 describe('Attack discovery tour', () => {
-  beforeAll(() => {
-    (useIsElementMounted as jest.Mock).mockReturnValue(true);
-    (useRouteSpy as jest.Mock).mockReturnValue([mockRouteSpy]);
-  });
-
+  const persistToLocalStorage = jest.fn();
+  const navigateToApp = jest.fn();
   beforeEach(() => {
-    (useKibana as jest.Mock).mockReturnValue({
-      ...mockedUseKibana,
-      services: {
-        ...mockedUseKibana.services,
-        storage: storageMock,
+    jest.clearAllMocks();
+    (useAssistantContext as jest.Mock).mockReturnValue({
+      navigateToApp,
+      assistantFeatures: {
+        assistantKnowledgeBaseByDefault: true,
       },
     });
-
-    storageMock.clear();
-  });
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it('should not render tour step 1 when element is not mounted', () => {
-    (useIsElementMounted as jest.Mock).mockReturnValueOnce(false);
-    render(<TestComponent />);
-    expect(screen.queryByTestId('knowledgeBase-tour-step-1')).toBeNull();
+    jest.mocked(useLocalStorage).mockReturnValue([
+      {
+        currentTourStep: 1,
+        isTourActive: true,
+      },
+      persistToLocalStorage,
+    ] as unknown as ReturnType<typeof useLocalStorage>);
   });
 
   it('should not render any tour steps when tour is not activated', () => {
-    storageMock.set(NEW_FEATURES_TOUR_STORAGE_KEYS.KNOWLEDGE_BASE, {
-      currentTourStep: 1,
-      isTourActive: false,
+    jest.mocked(useLocalStorage).mockReturnValue([
+      {
+        currentTourStep: 1,
+        isTourActive: false,
+      },
+      persistToLocalStorage,
+    ] as unknown as ReturnType<typeof useLocalStorage>);
+    render(
+      <KnowledgeBaseTour>
+        <h1>{'Hello world'}</h1>
+      </KnowledgeBaseTour>,
+      {
+        wrapper: TestProviders,
+      }
+    );
+    expect(screen.queryByTestId('knowledgeBase-tour-step-1')).toBeNull();
+    expect(screen.queryByTestId('knowledgeBase-tour-step-2')).toBeNull();
+  });
+
+  it('should not render any tour steps when knowledge base feature flag is not activated', () => {
+    (useAssistantContext as jest.Mock).mockReturnValue({
+      navigateToApp,
+      assistantFeatures: {
+        assistantKnowledgeBaseByDefault: false,
+      },
     });
-    render(<TestComponent />);
+    render(
+      <KnowledgeBaseTour>
+        <h1>{'Hello world'}</h1>
+      </KnowledgeBaseTour>,
+      {
+        wrapper: TestProviders,
+      }
+    );
     expect(screen.queryByTestId('knowledgeBase-tour-step-1')).toBeNull();
     expect(screen.queryByTestId('knowledgeBase-tour-step-2')).toBeNull();
   });
 
   it('should not render any tour steps when tour is on step 2 and page is not knowledge base', () => {
-    storageMock.set(NEW_FEATURES_TOUR_STORAGE_KEYS.KNOWLEDGE_BASE, {
-      currentTourStep: 2,
-      isTourActive: true,
-    });
-    const { debug } = render(<TestComponent />);
+    jest.mocked(useLocalStorage).mockReturnValue([
+      {
+        currentTourStep: 2,
+        isTourActive: true,
+      },
+      persistToLocalStorage,
+    ] as unknown as ReturnType<typeof useLocalStorage>);
+    render(
+      <KnowledgeBaseTour>
+        <h1>{'Hello world'}</h1>
+      </KnowledgeBaseTour>,
+      {
+        wrapper: TestProviders,
+      }
+    );
     expect(screen.queryByTestId('knowledgeBase-tour-step-1')).toBeNull();
-    debug();
   });
 
-  it('should  render tour step 1 when element is mounted', async () => {
-    const { getByTestId } = render(<TestComponent />);
+  it('should render tour step 1 when element is mounted', async () => {
+    const { getByTestId } = render(
+      <KnowledgeBaseTour>
+        <h1>{'Hello world'}</h1>
+      </KnowledgeBaseTour>,
+      {
+        wrapper: TestProviders,
+      }
+    );
 
     expect(getByTestId('knowledgeBase-tour-step-1')).toBeInTheDocument();
   });
 
   it('should render tour video when tour is on step 2 and page is knowledge base', () => {
-    (useRouteSpy as jest.Mock).mockReturnValue([
-      { ...mockRouteSpy, pageName: SecurityPageName.knowledgeBase },
-    ]);
-    storageMock.set(NEW_FEATURES_TOUR_STORAGE_KEYS.KNOWLEDGE_BASE, {
-      currentTourStep: 2,
-      isTourActive: true,
+    jest.mocked(useLocalStorage).mockReturnValue([
+      {
+        currentTourStep: 2,
+        isTourActive: true,
+      },
+      persistToLocalStorage,
+    ] as unknown as ReturnType<typeof useLocalStorage>);
+    const { getByTestId } = render(<KnowledgeBaseTour isKbSettingsPage />, {
+      wrapper: TestProviders,
     });
-    const { getByTestId } = render(<TestComponent />);
     expect(screen.queryByTestId('knowledgeBase-tour-step-1')).toBeNull();
     expect(getByTestId('knowledgeBase-tour-step-2')).toBeInTheDocument();
   });
 
   it('should advance to tour step 2 when page is knowledge base', () => {
-    (useRouteSpy as jest.Mock).mockReturnValue([
-      { ...mockRouteSpy, pageName: SecurityPageName.knowledgeBase },
-    ]);
-    storageMock.set(NEW_FEATURES_TOUR_STORAGE_KEYS.KNOWLEDGE_BASE, {
-      currentTourStep: 1,
-      isTourActive: true,
-    });
-    render(<TestComponent />);
-    expect(storageMock.get(NEW_FEATURES_TOUR_STORAGE_KEYS.KNOWLEDGE_BASE).currentTourStep).toEqual(
-      2
-    );
+    render(<KnowledgeBaseTour isKbSettingsPage />, { wrapper: TestProviders });
+    const nextStep = persistToLocalStorage.mock.calls[0][0];
+    expect(nextStep()).toEqual({ isTourActive: true, currentTourStep: 2 });
   });
 });
--- x-pack/packages/kbn-elastic-assistant/impl/tour/knowledge_base/step_config.tsx
+++ x-pack/packages/kbn-elastic-assistant/impl/tour/knowledge_base/step_config.tsx
@@ -7,14 +7,9 @@
 
 import * as i18n from './translations';
 
-export const KNOWLEDGE_BASE_TOUR_CONFIG_ANCHORS = {
-  NAV_LINK: 'solutionSideNavItemLink-attack_discovery',
-};
-
 export const knowledgeBaseTourStepOne = {
   title: i18n.KNOWLEDGE_BASE_TOUR_KNOWLEDGE_BASE_TITLE,
   content: i18n.KNOWLEDGE_BASE_TOUR_KNOWLEDGE_BASE_DESC,
-  // anchor: KNOWLEDGE_BASE_TOUR_CONFIG_ANCHORS.NAV_LINK,
 };
 
 export const tourConfig = {
