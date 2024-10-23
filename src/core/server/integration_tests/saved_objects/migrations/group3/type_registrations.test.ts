8af795e89c3b53272ad473e61c9a7fe1a79993ff
shahzad31 <shahzad31comp@gmail.com>
2024-10-23 15:58:06 +0200

    update test

--- src/core/server/integration_tests/saved_objects/migrations/group3/type_registrations.test.ts
+++ src/core/server/integration_tests/saved_objects/migrations/group3/type_registrations.test.ts
@@ -139,6 +139,7 @@ const previouslyRegisteredTypes = [
   'synthetics-monitor',
   'synthetics-param',
   'synthetics-privates-locations',
+  'synthetics-privates-locations-multiple',
   'tag',
   'task',
   'telemetry',
