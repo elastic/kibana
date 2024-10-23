f1e4d8fe3e2f4b1d6b08f46c7dc5b0ee30de8d77
Tiago Costa <tiago.costa@elastic.co>
2024-10-23 18:45:48 +0100

    skip flaky suite (#196166)

--- x-pack/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/init_and_status_apis.ts
+++ x-pack/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/init_and_status_apis.ts
@@ -581,6 +581,7 @@ export default ({ getService }: FtrProviderContext) => {
     });
 
     // FLAKY: https://github.com/elastic/kibana/issues/196319
+    // FLAKY: https://github.com/elastic/kibana/issues/196166
     describe.skip('status api', () => {
       it('should disable / enable risk engine', async () => {
         const status1 = await riskEngineRoutes.getStatus();
