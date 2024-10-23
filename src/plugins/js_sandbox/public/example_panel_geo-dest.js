9fb6a8d62ed36fcea489358d818625b5258e7619
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-10-23 10:20:01 +0200

    more panel examples

--- src/plugins/js_sandbox/public/example_panel_geo-dest.js
+++ src/plugins/js_sandbox/public/example_panel_geo-dest.js
@@ -7,19 +7,18 @@ function(props) {
 
   const data = React.useMemo(() => {
     const data = [
-      ...props.crossfilter.map(d => ({
-        ...d,
-        type: 'crossfilter',
-        typeOrder: 0,
+      ...props.data.map((d, i) => ({
+        geo_dest: d['geo.dest'],
+        count: d.count - (props.crossfilter.find(d2 => d2['geo.dest'] === d['geo.dest'])?.count ?? 0),
+        type: '02_context'
       })),
-      ...props.data.map((d,i) => ({
-        ...d,
-        count: d.count - (props.crossfilter.find(d2 => d2.date === d.date)?.count ?? 0),
-        type: 'context',
-        typeOrder: 1,
+      ...props.crossfilter.map(d => ({
+        geo_dest: d['geo.dest'],
+        count: d.count,
+        type: '01_filter'
       }))
-      ];
-    console.log('data histogram',data);
+    ];
+    console.log('data geo.dest', data);
     return data;
   }, [props.data, props.crossfilter]);
 
@@ -41,53 +40,50 @@ function(props) {
   }, [props.width, props.height, initalized])
 
   React.useEffect(() => {
-  // setup API options
-  const options = {
-    config: {
-      // Vega-Lite default configuration
-    },
-    init: (view) => {
-      console.log('init',view);
-      vegaRef.current = view;
-      // initialize tooltip handler
-      view.tooltip(new vegaTooltip.Handler().call);
+    // setup API options
+    const options = {
+      config: {
+        // Vega-Lite default configuration
+      },
+      init: (view) => {
+        vegaRef.current = view;
+        // initialize tooltip handler
+        view.tooltip(new vegaTooltip.Handler().call);
 
-      view.addSignalListener('brushX', function(event, item) {
-          if (item.monthdate_date) {
-            props.dispatch(`WHERE @timestamp >= "${new Date(item.monthdate_date[0]).toISOString().replace('2012-', '2024-')}" AND @timestamp < "${new Date(item.monthdate_date[1]).toISOString().replace('2012-', '2024-')}"`);
-          } else {
-            props.dispatch('');
-          }
-        })
+        view.addSignalListener('geo_dest', function(event, item) {
+            console.log('item',item);
+          if (item['geo_dest']) {
+              props.dispatch(`WHERE geo.dest == "${item['geo_dest'][0]}"`);
+            } else {
+              props.dispatch('');
+            }
+          })
 
-      setInitialized(true);
-    },
-    view: {
-      renderer: "canvas",
-    },
-  };
-
-      // register vega and vega-lite with the API
+        setInitialized(true);
+      },
+      view: {
+        renderer: "canvas",
+      },
+    };
+    // register vega and vega-lite with the API
     vl.register(vega, vegaLite, options);
 
-
-    const brush = vl.selectInterval().name('brushX').encodings('x');
+    const click = vl.selectMulti().encodings('y').name('geo_dest');
 
     // now you can use the API!
     const spec = vl.markBar({ tooltip: true })
       .data({ name: 'table'})
       .encode(
         // https://vega.github.io/vega-lite/docs/timeunit.html
-        vl.x().timeMD('date').axis({title: 'Date'}),
-        vl.y().sum("count"),
-        vl.color().fieldN('type').scale({ range: ['#eee','#00a69b'] }).legend({disable: true}),
-        vl.order().field('typeOrder'),
-        vl.tooltip([vl.fieldT("date"), vl.fieldQ("count")])
+        vl.x().sum("count"),
+        vl.y().fieldN('geo_dest').sort('-x'),
+        vl.color().fieldN('type').scale({ range: ['#00a69b', '#eee'] }).legend({disable: true}),
+        vl.tooltip([vl.fieldN("geo_dest"), vl.fieldQ("count")])
       )
       .width(props.width)
       .height(props.height)
       .autosize({ type: 'fit-x'})
-      .params(brush)
+      .params(click)
 
       spec.render()
       .then((viewElement) => {
