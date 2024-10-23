0bafb9632c2e1b09dd56586f15dca83d8ad5b358
Pablo Machado <pablo.nevesmachado@elastic.co>
2024-10-23 10:05:05 +0200

    [SecuritySolution] Update entity store source field (#197186)
    ## Summary
    
    In this PR the source field will only store the first identified index
    for an entity.
    The PR also updates the entities list panel to display a textual
    description of the source index and adds a new source field filter.
    
    
    
    https://github.com/user-attachments/assets/c7aad254-f871-4035-9dac-89decce31a55
    
    
    
    ### Checklist
    
    Delete any items that are not applicable to this PR.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: Mark Hopkin <mark.hopkin@elastic.co>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/security_solution/public/entity_analytics/components/entity_store/hooks/use_entities_list_columns.tsx
+++ x-pack/plugins/security_solution/public/entity_analytics/components/entity_store/hooks/use_entities_list_columns.tsx
@@ -17,9 +17,9 @@ import { RiskScoreLevel } from '../../severity/common';
 import { getEmptyTagValue } from '../../../../common/components/empty_value';
 import type { Columns } from '../../../../explore/components/paginated_table';
 import type { Entity } from '../../../../../common/api/entity_analytics/entity_store/entities/common.gen';
-import type { CriticalityLevels } from '../../../../../common/constants';
+import { type CriticalityLevels } from '../../../../../common/constants';
 import { ENTITIES_LIST_TABLE_ID } from '../constants';
-import { isUserEntity } from '../helpers';
+import { isUserEntity, sourceFieldToText } from '../helpers';
 import { CRITICALITY_LEVEL_TITLE } from '../../asset_criticality/translations';
 
 export type EntitiesListColumns = [
@@ -110,7 +110,7 @@ export const useEntitiesListColumns = (): EntitiesListColumns => {
       truncateText: { lines: 2 },
       render: (source: string | undefined) => {
         if (source != null) {
-          return <span>{source}</span>;
+          return sourceFieldToText(source);
         }
 
         return getEmptyTagValue();
