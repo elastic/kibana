0d7653c3d63f1b9b18121903513497220bfa8806
Jean-Louis Leysens <jloleysens@gmail.com>
2024-10-23 17:19:09 +0200

    updated sh

--- .buildkite/scripts/steps/publish_oas_docs.sh
+++ .buildkite/scripts/steps/publish_oas_docs.sh
@@ -4,15 +4,25 @@ set -euo pipefail
 
 echo "--- Publish OAS docs"
 
-BUMP_KIBANA_DOC_NAME="$(vault_get TODO TODO)"
-BUMP_KIBANA_DOC_TOKEN="$(vault_get TODO TODO)"
-bump deploy oas_docs/output/kibana.yaml \
-  --doc $BUMP_KIBANA_DOC_NAME \
-  --token $BUMP_KIBANA_DOC_TOKEN
+deploy_to_bump() {
+  local file_path="${1:-}"
+  local doc_name="${2:-}"
+  local doc_token="${3:-}"
+  local change_count=$(bump diff $file_path --doc $doc_name --token $doc_token --format=json) | jq -R 'fromjson? | length')
+  if [[ $change_count -gt 0 ]]; then
+    echo "About to deploy $file_path to bump.sh..."
+    bump deploy $file_path \
+      --doc $doc_name \
+      --token $doc_token;
+  fi
+}
 
-BUMP_KIBANA_SERVERLESS_DOC_NAME="$(vault_get TODO TODO)"
-BUMP_KIBANA_SERVERLESS_DOC_TOKEN="$(vault_get TODO TODO)"
-bump deploy oas_docs/output/kibana.serverless.yaml \
-  --doc $BUMP_KIBANA_SERVERLESS_DOC_NAME \
-  --token $BUMP_KIBANA_SERVERLESS_DOC_TOKEN
+if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
+  BUMP_KIBANA_DOC_NAME="$(vault_get TODO TODO)"
+  BUMP_KIBANA_DOC_TOKEN="$(vault_get TODO TODO)"
+  deploy_to_bump oas_docs/output/kibana.yaml $BUMP_KIBANA_DOC_NAME $BUMP_KIBANA_DOC_TOKEN;
 
+  BUMP_KIBANA_SERVERLESS_DOC_NAME="$(vault_get TODO TODO)"
+  BUMP_KIBANA_SERVERLESS_DOC_TOKEN="$(vault_get TODO TODO)"
+  deploy_to_bump oas_docs/output/kibana.serverless.yaml $BUMP_KIBANA_SERVERLESS_DOC_NAME $BUMP_KIBANA_SERVERLESS_DOC_TOKEN;
+fi
