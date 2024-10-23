1e1b2ad9bece78f1ba2dd445f62df0b51c4d0131
neptunian <sandra.gonzales@elastic.co>
2024-10-23 16:11:26 -0400

    use certs from dev_utils

--- x-pack/test_serverless/api_integration/test_suites/observability/config.ts
+++ x-pack/test_serverless/api_integration/test_suites/observability/config.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import path from 'path';
+import { CA_CERT_PATH, KBN_CERT_PATH, KBN_KEY_PATH } from '@kbn/dev-utils';
 import { createTestConfig } from '../../config.base';
 import { services as apmServices } from './apm_api_integration/common/services';
 import { services as datasetQualityServices } from './dataset_quality_api_integration/common/services';
@@ -30,20 +30,8 @@ export default createTestConfig({
     // dataUsage.autoops* config is set in kibana controller
     '--xpack.dataUsage.autoops.enabled=true',
     '--xpack.dataUsage.autoops.api.url=http://localhost:9000',
-    `--xpack.dataUsage.autoops.api.tls.certificate=${path.resolve(
-      __dirname,
-      '../common/data_usage/test_certs',
-      'certificate.crt'
-    )}`,
-    `--xpack.dataUsage.autoops.api.tls.key=${path.resolve(
-      __dirname,
-      '../common/data_usage/test_certs',
-      'key.key'
-    )}`,
-    `--xpack.dataUsage.autoops.api.tls.ca=${path.resolve(
-      __dirname,
-      '../common/data_usage/test_certs',
-      'ca.crt'
-    )}`,
+    `--xpack.dataUsage.autoops.api.tls.certificate=${KBN_CERT_PATH}`,
+    `--xpack.dataUsage.autoops.api.tls.key=${KBN_KEY_PATH}`,
+    `--xpack.dataUsage.autoops.api.tls.ca=${CA_CERT_PATH}`,
   ],
 });
--- x-pack/test_serverless/api_integration/test_suites/security/config.ts
+++ x-pack/test_serverless/api_integration/test_suites/security/config.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import path from 'path';
+import { CA_CERT_PATH, KBN_CERT_PATH, KBN_KEY_PATH } from '@kbn/dev-utils';
 import { createTestConfig } from '../../config.base';
 
 export default createTestConfig({
@@ -29,20 +29,8 @@ export default createTestConfig({
     '--xpack.dataUsage.enabled=true',
     // dataUsage.autoops* config is set in kibana controller
     '--xpack.dataUsage.autoops.api.url=http://localhost:9000',
-    `--xpack.dataUsage.autoops.api.tls.certificate=${path.resolve(
-      __dirname,
-      '../common/data_usage/test_certs',
-      'certificate.crt'
-    )}`,
-    `--xpack.dataUsage.autoops.api.tls.key=${path.resolve(
-      __dirname,
-      '../common/data_usage/test_certs',
-      'key.key'
-    )}`,
-    `--xpack.dataUsage.autoops.api.tls.ca=${path.resolve(
-      __dirname,
-      '../common/data_usage/test_certs',
-      'ca.crt'
-    )}`,
+    `--xpack.dataUsage.autoops.api.tls.certificate=${KBN_CERT_PATH}`,
+    `--xpack.dataUsage.autoops.api.tls.key=${KBN_KEY_PATH}`,
+    `--xpack.dataUsage.autoops.api.tls.ca=${CA_CERT_PATH}`,
   ],
 });
