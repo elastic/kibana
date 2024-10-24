07a00506e56a38d4aababbef31db16d8df813278
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2024-10-24 12:52:11 +0200

    Add tests

--- x-pack/plugins/observability_solution/inventory/common/entities.ts
+++ x-pack/plugins/observability_solution/inventory/common/entities.ts
@@ -7,18 +7,12 @@
 import { z } from '@kbn/zod';
 import { ENTITY_LATEST, entitiesAliasPattern, entityLatestSchema } from '@kbn/entities-schema';
 import {
-  AGENT_NAME,
-  CLOUD_PROVIDER,
-  CONTAINER_ID,
   ENTITY_DEFINITION_ID,
   ENTITY_DISPLAY_NAME,
   ENTITY_ID,
   ENTITY_IDENTITY_FIELDS,
   ENTITY_LAST_SEEN,
   ENTITY_TYPE,
-  HOST_NAME,
-  SERVICE_ENVIRONMENT,
-  SERVICE_NAME,
 } from '@kbn/observability-shared-plugin/common';
 import { isRight } from 'fp-ts/lib/Either';
 import * as t from 'io-ts';
@@ -64,7 +58,7 @@ export const entityTypesRt = new t.Type<string[], string, unknown>(
   (arr) => arr.join()
 );
 
-export interface BaseEntity {
+export interface Entity {
   [ENTITY_LAST_SEEN]: string;
   [ENTITY_ID]: string;
   [ENTITY_TYPE]: string;
@@ -74,30 +68,6 @@ export interface BaseEntity {
   alertsCount?: number;
   [key: string]: any;
 }
-
-/**
- * These types are based on service, host and container from the built in definition.
- */
-export interface ServiceEntity extends BaseEntity {
-  [ENTITY_TYPE]: 'service';
-  [SERVICE_NAME]: string;
-  [SERVICE_ENVIRONMENT]?: string | string[] | null;
-  [AGENT_NAME]: string | string[] | null;
-}
-
-export interface HostEntity extends BaseEntity {
-  [ENTITY_TYPE]: 'host';
-  [HOST_NAME]: string;
-  [CLOUD_PROVIDER]: string | string[] | null;
-}
-
-export interface ContainerEntity extends BaseEntity {
-  [ENTITY_TYPE]: 'container';
-  [CONTAINER_ID]: string;
-  [CLOUD_PROVIDER]: string | string[] | null;
-}
-
-export type Entity = ServiceEntity | HostEntity | ContainerEntity;
 
 export type InventoryEntityLatest = z.infer<typeof entityLatestSchema> & {
   alertsCount?: number;
