1b206b9ffd78298585f7d50ba59294e498e14267
Thom Heymann <190132+thomheymann@users.noreply.github.com>
2024-10-24 15:43:01 +0100

    [Observability Onboarding] Display next steps (#197179)
    ## Summary
    
    Display next steps after the auto-detect script completes.
    
    ## Screenshot
    
    <img width="905" alt="Screenshot 2024-10-22 at 09 30 51"
    src="https://github.com/user-attachments/assets/85108ea2-510a-46f7-8b79-135be91b586f">
    
    (cherry picked from commit 48959e769cb29b02e8c49d68fb2c7f9f8c3418d0)
    
    # Conflicts:
    #	x-pack/plugins/observability_solution/observability_onboarding/server/routes/flow/route.ts
    #	x-pack/plugins/observability_solution/observability_onboarding/server/routes/types.ts

--- x-pack/plugins/fleet/server/services/epm/package_service.ts
+++ x-pack/plugins/fleet/server/services/epm/package_service.ts
@@ -28,7 +28,6 @@ import type {
   InstallablePackage,
   Installation,
   RegistryPackage,
-  TemplateAgentPolicyInput,
 } from '../../types';
 
 import type { FleetAuthzRouteConfig } from '../security/types';
@@ -120,12 +119,12 @@ export interface PackageClient {
     prerelease?: false;
   }): Promise<PackageList>;
 
-  getAgentPolicyInputs(
+  getAgentPolicyConfigYAML(
     pkgName: string,
     pkgVersion?: string,
     prerelease?: false,
     ignoreUnverified?: boolean
-  ): Promise<TemplateAgentPolicyInput[]>;
+  ): Promise<string>;
 
   reinstallEsAssets(
     packageInfo: InstallablePackage,
@@ -288,7 +287,7 @@ class PackageClientImpl implements PackageClient {
     return generatePackageInfoFromArchiveBuffer(archiveBuffer, 'application/zip');
   }
 
-  public async getAgentPolicyInputs(
+  public async getAgentPolicyConfigYAML(
     pkgName: string,
     pkgVersion?: string,
     prerelease?: false,
@@ -302,16 +301,14 @@ class PackageClientImpl implements PackageClient {
       pkgVersion = pkg.version;
     }
 
-    const { inputs } = await getTemplateInputs(
+    return getTemplateInputs(
       this.internalSoClient,
       pkgName,
       pkgVersion,
-      'json',
+      'yml',
       prerelease,
       ignoreUnverified
     );
-
-    return inputs;
   }
 
   public async getPackage(
