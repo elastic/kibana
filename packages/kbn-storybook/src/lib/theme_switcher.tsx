bd2e98b2f8b419bba15ac4bc45499f5cdf2d7737
Brad White <brad.white@elastic.co>
2024-10-23 20:39:20 -0600

    remove deprecated icon component

--- packages/kbn-storybook/src/lib/theme_switcher.tsx
+++ packages/kbn-storybook/src/lib/theme_switcher.tsx
@@ -8,8 +8,9 @@
  */
 
 import React, { useCallback, useEffect } from 'react';
-import { Icons, IconButton, TooltipLinkList, WithTooltip } from '@storybook/components';
+import { IconButton, TooltipLinkList, WithTooltip } from '@storybook/components';
 import { useGlobals } from '@storybook/manager-api';
+import { HeartIcon, HeartHollowIcon } from '@storybook/icons';
 
 type PropsOf<T extends React.FC<any>> = T extends React.FC<infer P> ? P : never;
 type ArrayItem<T extends any[]> = T extends Array<infer I> ? I : never;
@@ -46,9 +47,8 @@ export function ThemeSwitcher() {
         />
       )}
     >
-      {/* @ts-ignore Remove when @storybook has moved to @emotion v11 */}
       <IconButton key="eui-theme" title="Change the EUI theme">
-        <Icons icon={selectedTheme?.includes('dark') ? 'heart' : 'hearthollow'} />
+        {selectedTheme?.includes('dark') ? <HeartIcon /> : <HeartHollowIcon />}
       </IconButton>
     </WithTooltip>
   );
