5e9b979149767f65ee724ce5adabdf3ddaa95b55
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-10-24 23:00:32 +1100

    [8.16] [Discover][Embeddable] Pass embeddable filters to Surrounding Docs page (#197190) (#197593)
    # Backport
    
    This will backport the following commits from `main` to `8.16`:
    - [[Discover][Embeddable] Pass embeddable filters to Surrounding Docs
    page (#197190)](https://github.com/elastic/kibana/pull/197190)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Julia
    Rechkunova","email":"julia.rechkunova@elastic.co"},"sourceCommit":{"committedDate":"2024-10-24T10:16:21Z","message":"[Discover][Embeddable]
    Pass embeddable filters to Surrounding Docs page (#197190)\n\n##
    Summary\r\n\r\nThis PR makes sure to pass `filters` to DocViewer from
    the search panel\r\non Dashboard. And DocViewer will pass `filters` over
    to Surrounding Docs\r\npage.\r\n\r\n### Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"1c3705ba5b5aa60ea451a7d0ff7f95f2d21a1b80","branchLabelMapping":{"^v9.0.0$":"main","^v8.17.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:fix","v9.0.0","Team:DataDiscovery","v8.16.0","backport:version","v8.17.0"],"title":"[Discover][Embeddable]
    Pass embeddable filters to Surrounding Docs
    page","number":197190,"url":"https://github.com/elastic/kibana/pull/197190","mergeCommit":{"message":"[Discover][Embeddable]
    Pass embeddable filters to Surrounding Docs page (#197190)\n\n##
    Summary\r\n\r\nThis PR makes sure to pass `filters` to DocViewer from
    the search panel\r\non Dashboard. And DocViewer will pass `filters` over
    to Surrounding Docs\r\npage.\r\n\r\n### Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"1c3705ba5b5aa60ea451a7d0ff7f95f2d21a1b80"}},"sourceBranch":"main","suggestedTargetBranches":["8.16","8.x"],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","branchLabelMappingKey":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/197190","number":197190,"mergeCommit":{"message":"[Discover][Embeddable]
    Pass embeddable filters to Surrounding Docs page (#197190)\n\n##
    Summary\r\n\r\nThis PR makes sure to pass `filters` to DocViewer from
    the search panel\r\non Dashboard. And DocViewer will pass `filters` over
    to Surrounding Docs\r\npage.\r\n\r\n### Checklist\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common
    scenarios","sha":"1c3705ba5b5aa60ea451a7d0ff7f95f2d21a1b80"}},{"branch":"8.16","label":"v8.16.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.x","label":"v8.17.0","branchLabelMappingKey":"^v8.17.0$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    Co-authored-by: Julia Rechkunova <julia.rechkunova@elastic.co>

--- test/functional/apps/discover/group2_data_grid1/_data_grid_context.ts
+++ test/functional/apps/discover/group2_data_grid1/_data_grid_context.ts
@@ -103,6 +103,9 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
     it('navigates to context view from embeddable', async () => {
       await common.navigateToApp('discover');
+      await header.waitUntilLoadingHasFinished();
+      await filterBar.addFilter({ field: 'extension.raw', operation: 'is', value: 'jpg' });
+      await header.waitUntilLoadingHasFinished();
       await discover.saveSearch('my search');
       await header.waitUntilLoadingHasFinished();
 
@@ -133,6 +136,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         log.debug('document table length', nrOfDocs);
         return nrOfDocs === 6;
       });
+      await filterBar.hasFilter('extension.raw', 'jpg', false);
     });
   });
 }
