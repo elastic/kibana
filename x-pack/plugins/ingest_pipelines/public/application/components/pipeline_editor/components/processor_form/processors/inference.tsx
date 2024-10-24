eff049b33066e95aef2ce0c28ae70a46f46ac034
István Zoltán Szabó <szabosteve@gmail.com>
2024-10-24 16:32:07 +0200

    Updates labels and desc.

--- x-pack/plugins/ingest_pipelines/public/application/components/pipeline_editor/components/processor_form/processors/inference.tsx
+++ x-pack/plugins/ingest_pipelines/public/application/components/pipeline_editor/components/processor_form/processors/inference.tsx
@@ -56,13 +56,13 @@ const fieldsConfig: FieldsConfig = {
   model_id: {
     type: FIELD_TYPES.TEXT,
     label: i18n.translate('xpack.ingestPipelines.pipelineEditor.inferenceForm.modelIDFieldLabel', {
-      defaultMessage: 'Deployment or model ID',
+      defaultMessage: 'Deployment, inference, or model ID',
     }),
     deserializer: String,
     helpText: i18n.translate(
       'xpack.ingestPipelines.pipelineEditor.inferenceForm.modelIDFieldHelpText',
       {
-        defaultMessage: 'ID of the deployment or the model to infer against.',
+        defaultMessage: 'ID of the deployment, the inference endpoint, or the model to infer against.',
       }
     ),
     validations: [
@@ -71,7 +71,7 @@ const fieldsConfig: FieldsConfig = {
           i18n.translate(
             'xpack.ingestPipelines.pipelineEditor.inferenceForm.patternRequiredError',
             {
-              defaultMessage: 'A deployment or model ID value is required.',
+              defaultMessage: 'A deployment, an inference, or a model ID value is required.',
             }
           )
         ),
