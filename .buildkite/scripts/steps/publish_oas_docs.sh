f9bea42352437a120a7e8e6f747d703b16221416
Jean-Louis Leysens <jloleysens@gmail.com>
2024-10-24 16:45:40 +0200

    updated script after tests

--- .buildkite/scripts/steps/publish_oas_docs.sh
+++ .buildkite/scripts/steps/publish_oas_docs.sh
@@ -2,30 +2,43 @@
 
 set -euo pipefail
 
+source .buildkite/scripts/common/util.sh
+
 echo "--- Publish OAS docs"
 
 deploy_to_bump() {
   local file_path="${1:-}"
   local doc_name="${2:-}"
   local doc_token="${3:-}"
-  local change_count=$(bump diff $file_path --doc $doc_name --token $doc_token --format=json | jq -R 'fromjson? | length')
+  local branch="${4:-}"
+
+  echo "Checking diff for doc '$doc_name' against file '$file_path'..."
+  local result=$(bump diff $file_path --doc $doc_name --token $doc_token --branch $branch --format=json)
+  ## Bump.sh does not respond with JSON when the diff is empty so we need to handle possibly not JSON :'(
+  local change_count=$(tr '\n' ' '<<<$result | jq -R 'fromjson? | length')
   if [[ ! -z $change_count && $change_count -gt 0 ]]; then
-    echo "About to deploy $file_path to bump.sh..."
+    echo "Found $change_count changes..."
+    echo "About to deploy file '$file_path' to doc '$doc_name' on bump.sh..."
     bump deploy $file_path \
+      --branch $branch \
       --doc $doc_name \
-      --token $doc_token;
+      --token $doc_token ;
+    echo ""
+    echo "Note: if there is a warning of unchanged docs we probably have unpublished deployments waiting."
+    echo "Go to https://bump.sh/elastic/dashboard to see all the docs in the hub."
   else
-    echo "Did not detect changes in $file_path; not deploying."
+    echo "Did not detect changes for '$file_path'; not deploying. Got response: $result"
   fi
 }
 
 if [[ "$BUILDKITE_BRANCH" == "main" ]]; then
   BUMP_KIBANA_DOC_NAME="$(vault_get kibana-bump-sh kibana-doc-name)"
   BUMP_KIBANA_DOC_TOKEN="$(vault_get kibana-bump-sh kibana-token)"
-  deploy_to_bump oas_docs/output/kibana.yaml $BUMP_KIBANA_DOC_NAME $BUMP_KIBANA_DOC_TOKEN;
+  deploy_to_bump oas_docs/output/kibana.yaml $BUMP_KIBANA_DOC_NAME $BUMP_KIBANA_DOC_TOKEN main;
+fi
 
-  # We should only do this after a successfull rollout to prod, i.e. once e2e tests passed
-  # BUMP_KIBANA_SERVERLESS_DOC_NAME="$(vault_get TODO TODO)"
-  # BUMP_KIBANA_SERVERLESS_DOC_TOKEN="$(vault_get TODO TODO)"
-  # deploy_to_bump oas_docs/output/kibana.serverless.yaml $BUMP_KIBANA_SERVERLESS_DOC_NAME $BUMP_KIBANA_SERVERLESS_DOC_TOKEN;
+if [[ "$BUILDKITE_BRANCH" == "8.x" ]]; then
+  BUMP_KIBANA_DOC_NAME="$(vault_get kibana-bump-sh kibana-doc-name)"
+  BUMP_KIBANA_DOC_TOKEN="$(vault_get kibana-bump-sh kibana-token)"
+  deploy_to_bump oas_docs/output/kibana.yaml $BUMP_KIBANA_DOC_NAME $BUMP_KIBANA_DOC_TOKEN v8;
 fi
