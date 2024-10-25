e92ad37c28e1a2f78309516ee79d11fb09e72e57
Thomas Neirynck <thomas@elastic.co>
2024-10-25 10:26:19 -0400

    fix test after workpad removal

--- test/api_integration/apis/home/sample_data.ts
+++ test/api_integration/apis/home/sample_data.ts
@@ -72,7 +72,7 @@ export default function ({ getService }: FtrProviderContext) {
 
           expect(resp.body).to.eql({
             elasticsearchIndicesCreated: { kibana_sample_data_flights: 13014 },
-            kibanaSavedObjectsLoaded: 8,
+            kibanaSavedObjectsLoaded: 7,
           });
         });
 
