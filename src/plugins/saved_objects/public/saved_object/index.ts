750c9bd0aad2143fd4460bfdef2bcba06eb7cae8
Mohamed Nabeel <137497525+nabeelmohamed@users.noreply.github.com>
2024-10-25 16:55:27 +0530

    fix: Re-add removed lines in index.ts

--- src/plugins/saved_objects/public/saved_object/index.ts
+++ src/plugins/saved_objects/public/saved_object/index.ts
@@ -7,4 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
+export { checkForDuplicateTitle } from './helpers/check_for_duplicate_title';
+export { saveWithConfirmation } from './helpers/save_with_confirmation';
 export { isErrorNonFatal } from './helpers/save_saved_object';
