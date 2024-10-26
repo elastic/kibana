dee4e6aef51edb2c7f1a3dc46aea7aae909d6691
kyracho <wsc2119@columbia.edu>
2024-10-25 19:52:16 -0700

    move logging onto importSavedObjectsFromStream

--- packages/core/saved-objects/core-saved-objects-import-export-server-internal/src/import/saved_objects_importer.ts
+++ packages/core/saved-objects/core-saved-objects-import-export-server-internal/src/import/saved_objects_importer.ts
@@ -62,16 +62,6 @@ export class SavedObjectsImporter implements ISavedObjectsImporter {
     compatibilityMode,
     managed,
   }: SavedObjectsImportOptions): Promise<SavedObjectsImportResponse> {
-    // Get the allowed types for import from the typeRegistry
-    const allowedTypes = this.#typeRegistry
-      .getImportableAndExportableTypes()
-      .map((type) => type.name);
-    // Log the allowed types, similar to how the exporter logs types
-    this.#log.debug(`Initiating import process.`);
-    this.#log.debug(`Allowed types: [${allowedTypes.join(', ')}]`);
-    this.#log.info(`Import size limit: ${this.#importSizeLimit} saved objects.`);
-    const overwriteStatus = overwrite ? 'enabled' : 'disabled';
-    this.#log.info(`Automatic overwrite is ${overwriteStatus} for the current import operation.`);
     return importSavedObjectsFromStream({
       readStream,
       createNewCopies,
@@ -84,19 +74,8 @@ export class SavedObjectsImporter implements ISavedObjectsImporter {
       typeRegistry: this.#typeRegistry,
       importHooks: this.#importHooks,
       managed,
+      log: this.#log,
     })
-      .then((response: SavedObjectsImportResponse) => {
-        this.#log.info(`🌸 Successfully imported ${response.successCount} saved objects.`);
-        return response;
-      })
-      .catch((error) => {
-        this.#log.error('Failed to import saved objects');
-        const errors = Array.isArray(error) ? error : [error];
-        errors.forEach((err) => {
-          this.#log.error(`Import error: ${err.message}`);
-        });
-        throw error;
-      });
   }
 
   public resolveImportErrors({
