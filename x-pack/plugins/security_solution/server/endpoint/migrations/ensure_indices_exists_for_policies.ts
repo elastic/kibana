56bd867fdd1436df8fb33ea1a251a56ada88be9e
Paul Tavares <paul.tavares@elastic.co>
2024-10-28 15:51:43 -0400

    Add step during plugin start up (non-blocking) to check policy indices

---
+++ x-pack/plugins/security_solution/server/endpoint/migrations/ensure_indices_exists_for_policies.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { createPolicyDataStreamsIfNeeded } from '../../fleet_integration/handlers/create_policy_datastreams';
+import type { EndpointAppContextService } from '../endpoint_app_context_services';
+
+export const ensureIndicesExistsForPolicies = async (
+  endpointServices: EndpointAppContextService
+): Promise<void> => {
+  const fleetServices = endpointServices.getInternalFleetServices();
+  const soClient = fleetServices.savedObjects.createInternalUnscopedSoClient();
+  const endpointPoliciesIds = await fleetServices.packagePolicy.listIds(soClient, {
+    kuery: fleetServices.endpointPolicyKuery,
+    perPage: 10000,
+  });
+
+  await createPolicyDataStreamsIfNeeded({
+    endpointServices,
+    endpointPolicyIds: endpointPoliciesIds.items,
+  });
+};
