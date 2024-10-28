eaa1aae42d157c8f76abdf5afecb2ba1f8fb0474
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-10-28 14:02:44 +0100

    refactor license

--- x-pack/platform/plugins/shared/ml/server/lib/alerts/register_jobs_monitoring_rule_type.ts
+++ x-pack/platform/plugins/shared/ml/server/lib/alerts/register_jobs_monitoring_rule_type.ts
@@ -37,7 +37,7 @@ import {
   ML_ALERT_TYPES,
 } from '../../../common/constants/alerts';
 import { PLUGIN_ID } from '../../../common/constants/app';
-import { MINIMUM_FULL_LICENSE } from '../../../common/license';
+import { MINIMUM_FULL_LICENSE } from '../../../common/license/constants';
 import type { AnomalyDetectionJobsHealthRuleParams } from '../../routes/schemas/alerting_schema';
 import type { RegisterAlertParams } from './register_ml_alerts';
 import type { JobMessage } from '../../../common/types/audit_message';
