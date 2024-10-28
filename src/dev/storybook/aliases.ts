438fdce23822955ed007d32ca5ef898b936deec2
Tiago Costa <tiago.costa@elastic.co>
2024-10-28 02:45:08 +0000

    fix(NA): storybook aliases for cloud_security_posture_packages

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -17,7 +17,7 @@ export const storybookAliases = {
   cases: 'packages/kbn-cases-components/.storybook',
   cell_actions: 'packages/kbn-cell-actions/.storybook',
   cloud_security_posture_packages:
-    'x-pack/packages/kbn-cloud-security-posture/graph/storybook/config',
+    'x-pack/packages/kbn-cloud-security-posture/storybook/config',
   cloud: 'packages/cloud/.storybook',
   coloring: 'packages/kbn-coloring/.storybook',
   language_documentation_popover: 'packages/kbn-language-documentation/.storybook',
