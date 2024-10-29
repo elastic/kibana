5d6a396fb682a93588be97a1b917ab044739daae
Elena Stoeva <elenastoeva99@gmail.com>
2024-10-29 11:34:30 +0000

    Fix functional tests

--- x-pack/test_serverless/api_integration/test_suites/common/index_management/datastreams.ts
+++ x-pack/test_serverless/api_integration/test_suites/common/index_management/datastreams.ts
@@ -80,6 +80,7 @@ export default function ({ getService }: FtrProviderContext) {
           health: 'green',
           indexTemplateName: testDataStreamName,
           hidden: false,
+          indexMode: 'standard',
         });
       });
 
