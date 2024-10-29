97036957f9151e8ddb1298e59691ed4178e8636e
Steph Milovic <stephanie.milovic@elastic.co>
2024-10-29 13:07:42 -0600

    fix tests

--- x-pack/packages/kbn-elastic-assistant/impl/tour/knowledge_base/video_toast.tsx
+++ x-pack/packages/kbn-elastic-assistant/impl/tour/knowledge_base/video_toast.tsx
@@ -19,7 +19,7 @@ import * as i18n from './translations';
 import theGif from './overview.gif';
 
 const VIDEO_CONTENT_WIDTH = 330;
-const VIDEO_PAGE = `https://ela.st/seckb`;
+export const VIDEO_PAGE = `https://ela.st/seckb`;
 
 const VideoComponent: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   const openVideoInNewTab = useCallback(() => {
