21dd62044397a090bb8ddd8f44f7729f7bf18767
Ahmad Bamieh <ahmad.bamyeh@elastic.co>
2024-10-30 07:21:00 +0300

    [8.x][Core] [UA] Support API Deprecations #196081 (#198210)
    ## Summary
    
    Backport https://github.com/elastic/kibana/pull/196081

--- x-pack/plugins/alerting/server/routes/legacy/enable.ts
+++ x-pack/plugins/alerting/server/routes/legacy/enable.ts
@@ -35,6 +35,7 @@ export const enableAlertRoute = (
         access: isServerless ? 'internal' : 'public',
         summary: 'Enable an alert',
         tags: ['oas-tag:alerting'],
+        // @ts-expect-error TODO(https://github.com/elastic/kibana/issues/196095): Replace {RouteDeprecationInfo}
         deprecated: true,
       },
     },
