981895fbe0eed905e58ab9375e1831b680894a79
Sébastien Loix <sebastien.loix@elastic.co>
2024-10-30 08:29:48 +0000

    Add persistence to ESQLEditor > QueryHistory

--- packages/kbn-esql-editor/src/editor_footer/query_history.tsx
+++ packages/kbn-esql-editor/src/editor_footer/query_history.tsx
@@ -17,7 +17,6 @@ import {
   EuiInMemoryTable,
   EuiBasicTableColumn,
   EuiButtonEmpty,
-  Criteria,
   EuiButtonIcon,
   CustomItemAction,
   EuiCopy,
@@ -25,6 +24,7 @@ import {
   euiScrollBarStyles,
 } from '@elastic/eui';
 import { css, Interpolation, Theme } from '@emotion/react';
+import { useEuiTablePersist } from '@kbn/shared-ux-table-persist';
 import { type QueryHistoryItem, getHistoryItems } from '../history_local_storage';
 import { getReducedSpaceStyling, swapArrayElements } from './query_history_helpers';
 
@@ -212,7 +212,16 @@ export function QueryHistory({
 }) {
   const theme = useEuiTheme();
   const scrollBarStyles = euiScrollBarStyles(theme);
-  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('desc');
+
+  const { sorting, onTableChange } = useEuiTablePersist<QueryHistoryItem>({
+    tableId: 'obltAlertFields',
+    initialSort: {
+      field: 'timeRan',
+      direction: 'desc',
+    },
+  });
+
+  const sortDirection = typeof sorting === 'boolean' ? 'desc' : sorting.sort.direction;
   const historyItems: QueryHistoryItem[] = getHistoryItems(sortDirection);
 
   const actions: Array<CustomItemAction<QueryHistoryItem>> = useMemo(() => {
@@ -276,19 +285,6 @@ export function QueryHistory({
     return getTableColumns(containerWidth, isOnReducedSpaceLayout, actions);
   }, [actions, containerWidth, isOnReducedSpaceLayout]);
 
-  const onTableChange = ({ page, sort }: Criteria<QueryHistoryItem>) => {
-    if (sort) {
-      const { direction } = sort;
-      setSortDirection(direction);
-    }
-  };
-
-  const sorting = {
-    sort: {
-      field: 'timeRan',
-      direction: sortDirection,
-    },
-  };
   const { euiTheme } = theme;
   const extraStyling = isOnReducedSpaceLayout ? getReducedSpaceStyling() : '';
 
