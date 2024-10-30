3df44e825e85f13343ce54157fbb254d15894615
Drew Tate <andrew.tate@elastic.co>
2024-10-30 07:43:23 -0600

    separate out util

--- packages/kbn-esql-validation-autocomplete/src/autocomplete/commands/stats/index.ts
+++ packages/kbn-esql-validation-autocomplete/src/autocomplete/commands/stats/index.ts
@@ -8,93 +8,11 @@
  */
 
 import type { ESQLCommand } from '@kbn/esql-ast';
-import { i18n } from '@kbn/i18n';
-import {
-  findPreviousWord,
-  getLastNonWhitespaceChar,
-  isOptionItem,
-  noCaseCompare,
-} from '../../../shared/helpers';
 import type { GetColumnsByTypeFn, SuggestionRawDefinition } from '../../types';
-import {
-  TIME_SYSTEM_PARAMS,
-  TRIGGER_SUGGESTION_COMMAND,
-  allFunctions,
-  getFunctionSuggestion,
-} from '../../factories';
+import { TRIGGER_SUGGESTION_COMMAND, allFunctions, getFunctionSuggestion } from '../../factories';
 import { commaCompleteItem, pipeCompleteItem } from '../../complete_items';
 import { pushItUpInTheList } from '../../helper';
-
-// STATS <expression>
-
-/**
- * Position of the caret in the sort command:
-*
-* ```
-* STATS [column1 =] expression1[, ..., [columnN =] expressionN] [BY grouping_expression1[, ..., grouping_expressionN]]
-        |                      |                                     |
-        expression_start       expression_complete                   grouping_expression_start
-
-* ```
-*/
-export type CaretPosition =
-  | 'expression_start'
-  | 'expression_complete'
-  | 'grouping_expression_start'
-  | 'grouping_expression_complete';
-
-export const getPosition = (innerText: string, command: ESQLCommand): CaretPosition => {
-  if (
-    command.args.some(
-      (arg) => isOptionItem(arg) && arg.name === 'by' && arg.location.min < innerText.length
-    )
-  ) {
-    if (
-      getLastNonWhitespaceChar(innerText) === ',' ||
-      noCaseCompare(findPreviousWord(innerText), 'by')
-    ) {
-      return 'grouping_expression_start';
-    } else {
-      return 'grouping_expression_complete';
-    }
-  }
-
-  if (
-    getLastNonWhitespaceChar(innerText) === ',' ||
-    noCaseCompare(findPreviousWord(innerText), 'stats')
-  ) {
-    return 'expression_start';
-  } else {
-    return 'expression_complete';
-  }
-};
-
-const byCompleteItem: SuggestionRawDefinition = {
-  label: 'BY',
-  text: 'BY ',
-  kind: 'Reference',
-  detail: 'By',
-  sortText: '1',
-  command: TRIGGER_SUGGESTION_COMMAND,
-};
-
-const dateHistogramCompleteItem: SuggestionRawDefinition = {
-  label: i18n.translate('kbn-esql-validation-autocomplete.esql.autocomplete.addDateHistogram', {
-    defaultMessage: 'Add date histogram',
-  }),
-  // TODO preferences?.histogramBarTarget
-  text: `BUCKET($0, ${1000}, ${TIME_SYSTEM_PARAMS.join(', ')})`,
-  asSnippet: true,
-  kind: 'Issue',
-  detail: i18n.translate(
-    'kbn-esql-validation-autocomplete.esql.autocomplete.addDateHistogramDetail',
-    {
-      defaultMessage: 'Add date histogram using bucket()',
-    }
-  ),
-  sortText: '1A',
-  command: TRIGGER_SUGGESTION_COMMAND,
-};
+import { byCompleteItem, dateHistogramCompleteItem, getPosition } from './util';
 
 export async function suggest(
   innerText: string,
@@ -103,8 +21,9 @@ export async function suggest(
   _columnExists: (column: string) => boolean
 ): Promise<SuggestionRawDefinition[]> {
   const pos = getPosition(innerText, command);
+
   switch (pos) {
-    case 'expression_start':
+    case 'expression':
       return allFunctions()
         .filter((func) => func.supportedCommands.includes('stats'))
         .map(getFunctionSuggestion);
@@ -116,7 +35,7 @@ export async function suggest(
         { ...commaCompleteItem, command: TRIGGER_SUGGESTION_COMMAND, text: ', ' },
       ];
 
-    case 'grouping_expression_start':
+    case 'grouping_expression':
       const columnSuggestions = pushItUpInTheList(await getColumnsByType('any'), true);
       return [
         ...allFunctions()
---
+++ packages/kbn-esql-validation-autocomplete/src/autocomplete/commands/stats/util.ts
@@ -0,0 +1,90 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { ESQLCommand } from '@kbn/esql-ast';
+import { i18n } from '@kbn/i18n';
+import {
+  findPreviousWord,
+  getLastNonWhitespaceChar,
+  isOptionItem,
+  noCaseCompare,
+} from '../../../shared/helpers';
+import { TIME_SYSTEM_PARAMS, TRIGGER_SUGGESTION_COMMAND } from '../../factories';
+import { SuggestionRawDefinition } from '../../types';
+
+// STATS <expression>
+
+/**
+ * Position of the caret in the sort command:
+*
+* ```
+* STATS [column1 =] expression1[, ..., [columnN =] expressionN] [BY grouping_expression1[, ..., grouping_expressionN]]
+        |                      |                                     |                  |
+        expression             expression_complete                  grouping_expression grouping_expression_complete
+
+* ```
+*/
+export type CaretPosition =
+  | 'expression'
+  | 'expression_complete'
+  | 'grouping_expression'
+  | 'grouping_expression_complete';
+
+export const getPosition = (innerText: string, command: ESQLCommand): CaretPosition => {
+  if (
+    command.args.some(
+      (arg) => isOptionItem(arg) && arg.name === 'by' && arg.location.min < innerText.length
+    )
+  ) {
+    if (
+      getLastNonWhitespaceChar(innerText) === ',' ||
+      noCaseCompare(findPreviousWord(innerText), 'by')
+    ) {
+      return 'grouping_expression';
+    } else {
+      return 'grouping_expression_complete';
+    }
+  }
+
+  if (
+    getLastNonWhitespaceChar(innerText) === ',' ||
+    noCaseCompare(findPreviousWord(innerText), 'stats')
+  ) {
+    return 'expression';
+  } else {
+    return 'expression_complete';
+  }
+};
+
+export const byCompleteItem: SuggestionRawDefinition = {
+  label: 'BY',
+  text: 'BY ',
+  kind: 'Reference',
+  detail: 'By',
+  sortText: '1',
+  command: TRIGGER_SUGGESTION_COMMAND,
+};
+
+export const dateHistogramCompleteItem: SuggestionRawDefinition = {
+  label: i18n.translate('kbn-esql-validation-autocomplete.esql.autocomplete.addDateHistogram', {
+    defaultMessage: 'Add date histogram',
+  }),
+  // TODO preferences?.histogramBarTarget
+  text: `BUCKET($0, ${1000}, ${TIME_SYSTEM_PARAMS.join(', ')})`,
+  asSnippet: true,
+  kind: 'Issue',
+  detail: i18n.translate(
+    'kbn-esql-validation-autocomplete.esql.autocomplete.addDateHistogramDetail',
+    {
+      defaultMessage: 'Add date histogram using bucket()',
+    }
+  ),
+  sortText: '1A',
+  command: TRIGGER_SUGGESTION_COMMAND,
+};
