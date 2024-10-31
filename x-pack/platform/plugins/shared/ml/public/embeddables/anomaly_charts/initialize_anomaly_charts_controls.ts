16ceda29851ab5b5bda4362f89df67414fc52518
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-10-31 10:10:23 +0100

    break anomaly explorer charts constants into their own file

--- x-pack/platform/plugins/shared/ml/public/embeddables/anomaly_charts/initialize_anomaly_charts_controls.ts
+++ x-pack/platform/plugins/shared/ml/public/embeddables/anomaly_charts/initialize_anomaly_charts_controls.ts
@@ -11,7 +11,7 @@ import fastIsEqual from 'fast-deep-equal';
 import type { MlEntityField } from '@kbn/ml-anomaly-utils';
 import type { StateComparators } from '@kbn/presentation-publishing';
 import type { JobId } from '@kbn/ml-common-types/anomaly_detection_jobs/job';
-import { DEFAULT_MAX_SERIES_TO_PLOT } from '../../application/services/anomaly_explorer_charts_service';
+import { DEFAULT_MAX_SERIES_TO_PLOT } from '../../application/services/anomaly_explorer_charts_service_constants';
 import type {
   AnomalyChartsComponentApi,
   AnomalyChartsDataLoadingApi,
