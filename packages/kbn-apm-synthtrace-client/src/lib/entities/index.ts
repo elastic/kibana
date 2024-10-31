3144ffbe47218a0c580e2ae2a9849a0392142d7f
klacabane <kevin.lacabane@elastic.co>
2024-10-31 14:22:04 +0100

    fix synthtrace imports

--- packages/kbn-apm-synthtrace-client/src/lib/entities/index.ts
+++ packages/kbn-apm-synthtrace-client/src/lib/entities/index.ts
@@ -11,16 +11,6 @@ import { Fields } from '../entity';
 import { serviceEntity } from './service_entity';
 import { hostEntity } from './host_entity';
 import { containerEntity } from './container_entity';
-import { k8sClusterJobEntity } from './kubernetes/cluster_entity';
-import { k8sCronJobEntity } from './kubernetes/cron_job_entity';
-import { k8sDaemonSetEntity } from './kubernetes/daemon_set_entity';
-import { k8sDeploymentEntity } from './kubernetes/deployment_entity';
-import { k8sJobSetEntity } from './kubernetes/job_set_entity';
-import { k8sNodeEntity } from './kubernetes/node_entity';
-import { k8sPodEntity } from './kubernetes/pod_entity';
-import { k8sReplicaSetEntity } from './kubernetes/replica_set';
-import { k8sStatefulSetEntity } from './kubernetes/stateful_set';
-import { k8sContainerEntity } from './kubernetes/container_entity';
 
 export type EntityDataStreamType = 'metrics' | 'logs' | 'traces';
 export type Schema = 'ecs' | 'semconv';
@@ -43,20 +33,4 @@ export type EntityFields = Fields &
     [key: string]: any;
   }>;
 
-export const entities = {
-  serviceEntity,
-  hostEntity,
-  containerEntity,
-  k8s: {
-    k8sClusterJobEntity,
-    k8sCronJobEntity,
-    k8sDaemonSetEntity,
-    k8sDeploymentEntity,
-    k8sJobSetEntity,
-    k8sNodeEntity,
-    k8sPodEntity,
-    k8sReplicaSetEntity,
-    k8sStatefulSetEntity,
-    k8sContainerEntity,
-  },
-};
+export const entities = { serviceEntity, hostEntity, containerEntity };
