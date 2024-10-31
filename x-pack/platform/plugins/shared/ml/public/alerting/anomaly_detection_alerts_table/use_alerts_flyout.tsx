3ff7d163e312e5a7245646958ecde514f8a1f847
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-10-31 08:45:36 +0100

    lazy alert flyout

--- x-pack/platform/plugins/shared/ml/public/alerting/anomaly_detection_alerts_table/use_alerts_flyout.tsx
+++ x-pack/platform/plugins/shared/ml/public/alerting/anomaly_detection_alerts_table/use_alerts_flyout.tsx
@@ -9,47 +9,21 @@ import type {
   AlertsTableFlyoutBaseProps,
   AlertTableFlyoutComponent,
 } from '@kbn/triggers-actions-ui-plugin/public';
-import { get } from 'lodash';
 import React from 'react';
-import { type EuiDataGridColumn, EuiDescriptionList, EuiPanel, EuiTitle } from '@elastic/eui';
-import { ALERT_RULE_NAME } from '@kbn/rule-data-utils';
-import { isDefined } from '@kbn/ml-is-defined';
+import { type EuiDataGridColumn } from '@elastic/eui';
 import type { RegisterFormatter } from './render_cell_value';
-
-const FlyoutHeader: AlertTableFlyoutComponent = ({ alert }: AlertsTableFlyoutBaseProps) => {
-  const name = alert[ALERT_RULE_NAME];
-  return (
-    <EuiTitle size="s">
-      <h3>{name}</h3>
-    </EuiTitle>
-  );
-};
+import { AlertFlyoutLazy } from './alert_flyout_lazy';
+import { AlertFlyoutHeaderLazy } from './alert_flyout_header_lazy';
 
 export const getAlertFlyout =
   (columns: EuiDataGridColumn[], formatter: RegisterFormatter) => () => {
     const FlyoutBody: AlertTableFlyoutComponent = ({ alert, id }: AlertsTableFlyoutBaseProps) => (
-      <EuiPanel>
-        <EuiDescriptionList
-          listItems={columns.map((column) => {
-            const alertFieldValue = get(alert, column.id);
-            const value = (
-              Array.isArray(alertFieldValue) ? alertFieldValue.at(-1) : alertFieldValue
-            ) as string;
-
-            return {
-              title: column.displayAsText as string,
-              description: isDefined(value) ? formatter(column.id, value) : '—',
-            };
-          })}
-          type="column"
-          columnWidths={[1, 3]} // Same as [25, 75]
-        />
-      </EuiPanel>
+      <AlertFlyoutLazy isLoading={false} columns={columns} formatter={formatter} alert={alert} />
     );
 
     return {
       body: FlyoutBody,
-      header: FlyoutHeader,
+      header: AlertFlyoutHeaderLazy,
       footer: null,
     };
   };
