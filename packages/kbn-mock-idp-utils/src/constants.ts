ba19ff8ed078b40810865bb90a48de881864d3fb
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-11-01 18:52:52 +0100

    [kbn-mock-idp-utils] export plugin and metadata files paths

--- packages/kbn-mock-idp-utils/src/constants.ts
+++ packages/kbn-mock-idp-utils/src/constants.ts
@@ -7,6 +7,10 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
+import path from 'path';
+
+import { REPO_ROOT } from '@kbn/repo-info';
+
 export const MOCK_IDP_LOGIN_PATH = '/mock_idp/login';
 export const MOCK_IDP_LOGOUT_PATH = '/mock_idp/logout';
 
@@ -19,3 +23,30 @@ export const MOCK_IDP_ATTRIBUTE_PRINCIPAL = 'http://saml.elastic-cloud.com/attri
 export const MOCK_IDP_ATTRIBUTE_ROLES = 'http://saml.elastic-cloud.com/attributes/roles';
 export const MOCK_IDP_ATTRIBUTE_EMAIL = 'http://saml.elastic-cloud.com/attributes/email';
 export const MOCK_IDP_ATTRIBUTE_NAME = 'http://saml.elastic-cloud.com/attributes/name';
+
+export const MOCK_IDP_TEST_PLUGIN_PATH = path.resolve(
+  REPO_ROOT,
+  'x-pack',
+  'test',
+  'security_api_integration',
+  'plugins',
+  'saml_provider'
+);
+
+const idPResourcesPath = path.resolve(
+  REPO_ROOT,
+  'x-pack',
+  'test',
+  'security_api_integration',
+  'packages',
+  'helpers',
+  'saml'
+);
+
+export const IDP_METADATA_PATHS = {
+  default: path.resolve(MOCK_IDP_TEST_PLUGIN_PATH, 'metadata.xml'),
+  saml1: path.resolve(idPResourcesPath, 'idp_metadata.xml'),
+  saml2: path.resolve(idPResourcesPath, 'idp_metadata_2.xml'),
+  neverLogin: path.resolve(idPResourcesPath, 'idp_metadata_never_login.xml'),
+  mockIdpPlugin: path.resolve(idPResourcesPath, 'idp_metadata_mock_idp.xml'),
+};
