c96f20c442ebec577fc0ff6afb2d86189bc2aa4f
Aleh Zasypkin <aleh.zasypkin@elastic.co>
2024-11-01 11:27:12 +0100

    fix(security, features): do not expose UI capabilities of the deprecated features

--- x-pack/plugins/spaces/server/lib/utils/space_solution_disabled_features.ts
+++ x-pack/plugins/spaces/server/lib/utils/space_solution_disabled_features.ts
@@ -28,6 +28,7 @@ const getFeatureIdsForCategories = (
  * This function takes the current space's disabled features and the space solution and returns
  * the updated array of disabled features.
  *
+ * @param features The list of all Kibana registered features.
  * @param spaceDisabledFeatures The current space's disabled features
  * @param spaceSolution The current space's solution (es, oblt, security or classic)
  * @returns The updated array of disabled features
