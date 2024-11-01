dc731192148072dc8d3760fecf3294228e4afcd7
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2024-11-01 12:52:43 -0400

    fix tests

--- x-pack/test/functional/services/ml/test_resources.ts
+++ x-pack/test/functional/services/ml/test_resources.ts
@@ -594,7 +594,7 @@ export function MachineLearningTestResourcesProvider(
 
       await retry.tryForTime(10 * 1000, async () => {
         const { body, status } = await supertest
-          .get(`/api/fleet/epm/packages?experimental=true`)
+          .get(`/api/fleet/epm/packages?prerelease=true`)
           .set(getCommonRequestHeader(`${API_VERSIONS.public.v1}`));
         mlApi.assertResponseStatusCode(200, status, body);
 
