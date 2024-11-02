0af32744b5a352ba54b5004a5a1aa51dec0832e8
Maxim Palenov <maxim.palenov@elastic.co>
2024-11-02 21:50:46 +0100

    split useDataView implementation for better reuse

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_edit/fields/hooks/use_diffable_rule_data_view.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_edit/fields/hooks/use_diffable_rule_data_view.ts
@@ -5,12 +5,11 @@
  * 2.0.
  */
 
-import { useEffect, useState } from 'react';
 import type { DataView } from '@kbn/data-views-plugin/common';
 import type { DiffableRule } from '../../../../../../../../../common/api/detection_engine';
-import { useKibana } from '../../../../../../../../common/lib/kibana';
 import { DataSourceType } from '../../../../../../../../../common/api/detection_engine';
 import { useDefaultIndexPattern } from '../../../../../../hooks/use_default_index_pattern';
+import { useDataView } from './use_data_view';
 
 interface UseDiffableRuleDataViewResult {
   dataView: DataView | undefined;
@@ -18,53 +17,17 @@ interface UseDiffableRuleDataViewResult {
 }
 
 export function useDiffableRuleDataView(diffableRule: DiffableRule): UseDiffableRuleDataViewResult {
-  const {
-    data: { dataViews: dataViewsService },
-  } = useKibana().services;
   const defaultIndexPattern = useDefaultIndexPattern();
-  const [dataView, setDataView] = useState<DataView | undefined>();
-  const [isLoading, setIsLoading] = useState(false);
+  const defaultDataSource = {
+    type: DataSourceType.index_patterns,
+    index_patterns: defaultIndexPattern,
+  } as const;
+  const dataSource =
+    ('data_source' in diffableRule && diffableRule.data_source) || defaultDataSource;
 
-  useEffect(() => {
-    setIsLoading(true);
-
-    (async () => {
-      if (
-        'data_source' in diffableRule &&
-        diffableRule.data_source?.type === DataSourceType.index_patterns
-      ) {
-        const indexPatternsDataView = await dataViewsService.create({
-          title: diffableRule.data_source.index_patterns.join(','),
-          allowNoIndex: true,
-        });
-
-        setDataView(indexPatternsDataView);
-        setIsLoading(false);
-        return;
-      }
-
-      if (
-        'data_source' in diffableRule &&
-        diffableRule.data_source?.type === DataSourceType.data_view &&
-        diffableRule.data_source.data_view_id != null &&
-        diffableRule.data_source.data_view_id !== ''
-      ) {
-        const ruleDataView = await dataViewsService.get(diffableRule.data_source.data_view_id);
-
-        setDataView(ruleDataView);
-        setIsLoading(false);
-        return;
-      }
-
-      const defaultIndexPatternsDataView = await dataViewsService.create({
-        title: defaultIndexPattern.join(','),
-        allowNoIndex: true,
-      });
-
-      setDataView(defaultIndexPatternsDataView);
-      setIsLoading(false);
-    })();
-  }, [dataViewsService, diffableRule, defaultIndexPattern]);
-
-  return { dataView, isLoading };
+  return useDataView(
+    dataSource.type === DataSourceType.index_patterns
+      ? { indexPatterns: dataSource.index_patterns }
+      : { dataViewId: dataSource.data_view_id }
+  );
 }
