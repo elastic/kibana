e5d634f1ea8b702585969dd4d3a8bcfb53c37011
Maxim Palenov <maxim.palenov@elastic.co>
2024-11-03 13:59:48 +0100

    correct i18n keys

--- x-pack/plugins/security_solution/public/detection_engine/rule_creation/components/alert_suppression_edit/translations.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_creation/components/alert_suppression_edit/translations.tsx
@@ -8,70 +8,70 @@
 import { i18n } from '@kbn/i18n';
 
 export const ALERT_SUPPRESSION_SUPPRESS_BY_FIELD_LABEL = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.fieldsSelector.label',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.fieldsSelector.label',
   {
     defaultMessage: 'Suppress alerts by',
   }
 );
 
 export const ALERT_SUPPRESSION_SUPPRESS_BY_FIELD_HELP_TEXT = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.suppressByFields.helpText',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.suppressByFields.helpText',
   {
     defaultMessage: 'Select field(s) to use for suppressing extra alerts',
   }
 );
 
 export const ALERT_SUPPRESSION_DURATION_PER_RULE_EXECUTION_OPTION = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.suppressionDuration.perRuleExecutionOption',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.suppressionDuration.perRuleExecutionOption',
   {
     defaultMessage: 'Per rule execution',
   }
 );
 
 export const ALERT_SUPPRESSION_DURATION_PER_TIME_PERIOD_OPTION = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.suppressionDuration.perTimePeriodOption',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.suppressionDuration.perTimePeriodOption',
   {
     defaultMessage: 'Per time period',
   }
 );
 
 export const ALERT_SUPPRESSION_MISSING_FIELDS_LABEL = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.missingFields.label',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.missingFields.label',
   {
     defaultMessage: 'If a suppression field is missing',
   }
 );
 
 export const ALERT_SUPPRESSION_MISSING_FIELDS_SUPPRESS_OPTION = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.missingFields.suppressOption',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.missingFields.suppressOption',
   {
     defaultMessage: 'Suppress and group alerts for events with missing fields',
   }
 );
 
 export const ALERT_SUPPRESSION_MISSING_FIELDS_DO_NOT_SUPPRESS_OPTION = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.missingFields.doNotSuppressOption',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.missingFields.doNotSuppressOption',
   {
     defaultMessage: 'Do not suppress alerts for events with missing fields',
   }
 );
 
 export const ALERT_SUPPRESSION_NOT_SUPPORTED_FOR_EQL_SEQUENCE = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.notSupportedForEqlSequence',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.notSupportedForEqlSequence',
   {
     defaultMessage: 'Suppression is not supported for EQL sequence queries',
   }
 );
 
 export const MACHINE_LEARNING_SUPPRESSION_FIELDS_LOADING = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.machineLearningSuppressionFieldsLoading',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.machineLearningSuppressionFieldsLoading',
   {
     defaultMessage: 'Machine Learning suppression fields are loading',
   }
 );
 
 export const MACHINE_LEARNING_NO_SUPPRESSION_FIELDS = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.machineLearningNoSuppressionFields',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.machineLearningNoSuppressionFields',
   {
     defaultMessage:
       'Unable to load machine Learning suppression fields, start relevant Machine Learning jobs.',
@@ -79,14 +79,14 @@ export const MACHINE_LEARNING_NO_SUPPRESSION_FIELDS = i18n.translate(
 );
 
 export const ESQL_SUPPRESSION_FIELDS_LOADING = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.esqlFieldsLoading',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.esqlFieldsLoading',
   {
     defaultMessage: 'ES|QL suppression fields are loading',
   }
 );
 
 export const MACHINE_LEARNING_SUPPRESSION_INCOMPLETE_LABEL = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.alertSuppression.machineLearningSuppressionIncomplete',
+  'xpack.securitySolution.ruleManagement.ruleFields.alertSuppression.machineLearningSuppressionIncomplete',
   {
     defaultMessage:
       'This list of fields might be incomplete as some Machine Learning jobs are not running. Start all relevant jobs for a complete list.',
--- x-pack/plugins/security_solution/public/detection_engine/rule_creation/components/threshold_alert_suppression_edit/translations.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_creation/components/threshold_alert_suppression_edit/translations.tsx
@@ -10,14 +10,14 @@ import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 
 export const ALERT_SUPPRESSION_DURATION_PER_RULE_EXECUTION_OPTION = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.thresholdAlertSuppression.suppressionDuration.perRuleExecutionOption',
+  'xpack.securitySolution.ruleManagement.ruleFields.thresholdAlertSuppression.suppressionDuration.perRuleExecutionOption',
   {
     defaultMessage: 'Per rule execution',
   }
 );
 
 export const ALERT_SUPPRESSION_DURATION_PER_TIME_PERIOD_OPTION = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.thresholdAlertSuppression.suppressionDuration.perTimePeriodOption',
+  'xpack.securitySolution.ruleManagement.ruleFields.thresholdAlertSuppression.suppressionDuration.perTimePeriodOption',
   {
     defaultMessage: 'Per time period',
   }
@@ -25,21 +25,21 @@ export const ALERT_SUPPRESSION_DURATION_PER_TIME_PERIOD_OPTION = i18n.translate(
 
 export const enableSuppressionForFields = (fields: string[]) => (
   <FormattedMessage
-    id="xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.thresholdAlertSuppression.enableForFields"
+    id="xpack.securitySolution.ruleManagement.ruleFields.thresholdAlertSuppression.enableForFields"
     defaultMessage="Suppress alerts by selected fields: {fieldsString}"
     values={{ fieldsString: <strong>{fields.join(', ')}</strong> }}
   />
 );
 
 export const SUPPRESS_ALERTS = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.thresholdAlertSuppression.enable',
+  'xpack.securitySolution.ruleManagement.ruleFields.thresholdAlertSuppression.enable',
   {
     defaultMessage: 'Suppress alerts',
   }
 );
 
 export const THRESHOLD_SUPPRESSION_PER_RULE_EXECUTION_WARNING = i18n.translate(
-  'xpack.securitySolution.ruleManagement.threeWayDiff.finalEdit.thresholdAlertSuppression.perRuleExecutionWarning',
+  'xpack.securitySolution.ruleManagement.ruleFields.thresholdAlertSuppression.perRuleExecutionWarning',
   {
     defaultMessage: 'Per rule execution option is not available for Threshold rule type',
   }
