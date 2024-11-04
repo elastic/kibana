7a9056d00e7901efb2554a427bb6e00cfd7f904c
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-11-05 03:23:23 +1100

    skip failing test suite (#197335)

--- x-pack/plugins/osquery/cypress/e2e/all/alerts_automated_action_results.cy.ts
+++ x-pack/plugins/osquery/cypress/e2e/all/alerts_automated_action_results.cy.ts
@@ -13,6 +13,7 @@ const UUID_REGEX = '[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-
 
 // FLAKY: https://github.com/elastic/kibana/issues/178404
 // FLAKY: https://github.com/elastic/kibana/issues/197335
+// Failing: See https://github.com/elastic/kibana/issues/197335
 describe.skip('Alert Flyout Automated Action Results', () => {
   let ruleId: string;
 
