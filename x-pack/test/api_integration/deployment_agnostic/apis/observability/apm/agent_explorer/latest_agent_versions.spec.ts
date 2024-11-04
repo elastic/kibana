c45c66213a683d27e22691d1bf268c4bd77f0e6a
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2024-11-04 14:29:37 +0100

    Updated latest_agent_versions.spec.ts after moving

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/apm/agent_explorer/latest_agent_versions.spec.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/apm/agent_explorer/latest_agent_versions.spec.ts
@@ -6,45 +6,42 @@
  */
 import { ElasticApmAgentLatestVersion } from '@kbn/apm-plugin/common/agent_explorer';
 import expect from '@kbn/expect';
-import { FtrProviderContext } from '../../common/ftr_provider_context';
+import type { DeploymentAgnosticFtrProviderContext } from '../../../../ftr_provider_context';
 
-export default function ApiTest({ getService }: FtrProviderContext) {
+export default function ApiTest({ getService }: DeploymentAgnosticFtrProviderContext) {
   const registry = getService('registry');
-  const apmApiClient = getService('apmApiClient');
+  const apiApi = getService('apmApi');
 
   const nodeAgentName = 'nodejs';
   const unlistedAgentName = 'unlistedAgent';
 
   async function callApi() {
+    const apmApiClient = await apiApi.createApmApiClient();
     return await apmApiClient.readUser({
       endpoint: 'GET /internal/apm/get_latest_agent_versions',
     });
   }
 
-  registry.when(
-    'Agent latest versions when configuration is defined',
-    { config: 'basic', archives: [] },
-    () => {
-      it('returns a version when agent is listed in the file', async () => {
-        const { status, body } = await callApi();
-        expect(status).to.be(200);
+  registry.when('Agent latest versions when configuration is defined', () => {
+    it('returns a version when agent is listed in the file', async () => {
+      const { status, body } = await callApi();
+      expect(status).to.be(200);
 
-        const agents = body.data;
+      const agents = body.data;
 
-        const nodeAgent = agents[nodeAgentName] as ElasticApmAgentLatestVersion;
-        expect(nodeAgent?.latest_version).not.to.be(undefined);
-      });
+      const nodeAgent = agents[nodeAgentName] as ElasticApmAgentLatestVersion;
+      expect(nodeAgent?.latest_version).not.to.be(undefined);
+    });
 
-      it('returns undefined when agent is not listed in the file', async () => {
-        const { status, body } = await callApi();
-        expect(status).to.be(200);
+    it('returns undefined when agent is not listed in the file', async () => {
+      const { status, body } = await callApi();
+      expect(status).to.be(200);
 
-        const agents = body.data;
+      const agents = body.data;
 
-        // @ts-ignore
-        const unlistedAgent = agents[unlistedAgentName] as ElasticApmAgentLatestVersion;
-        expect(unlistedAgent?.latest_version).to.be(undefined);
-      });
-    }
-  );
+      // @ts-ignore
+      const unlistedAgent = agents[unlistedAgentName] as ElasticApmAgentLatestVersion;
+      expect(unlistedAgent?.latest_version).to.be(undefined);
+    });
+  });
 }
