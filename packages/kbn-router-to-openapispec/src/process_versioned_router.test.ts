c9f4dfaa452898cc46e8532c077eff96186376ff
Sid <siddharthmantri1@gmail.com>
2024-11-04 16:57:45 +0100

    [Authz] Fix description generation for Open API spec for an API (#198054)
    Closes https://github.com/elastic/kibana/issues/198058.
    
    Adds a fix for https://github.com/elastic/kibana/pull/197001
    
    ## Summary
    There was an error in how descriptions were added to the Open API spec
    for a given route - for the specific case when both a route description
    and security authz required privileges were present. The code with the
    error is:
    https://github.com/elastic/kibana/pull/197001/files#diff-5942307fac5a7b321e7f317bacd2837a7f766f3e79d5aad285513b1f82951b46R79-R80
    
    This PR fixes that error.
    
    Also updated: Description field for required privileges now includes a
    more intuitive descriptor: `Required authorization` as well as a line
    break.
    
    <img width="838" alt="image"
    src="https://github.com/user-attachments/assets/e6af0459-28e8-40e5-873d-924d1a49b01b">
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit b12e7d0e79af8150ea9f2b5940a6ad1d428cff72)
    
    # Conflicts:
    #	packages/kbn-router-to-openapispec/src/process_router.ts
    #	packages/kbn-router-to-openapispec/src/process_versioned_router.ts

--- packages/kbn-router-to-openapispec/src/process_versioned_router.test.ts
+++ packages/kbn-router-to-openapispec/src/process_versioned_router.test.ts
@@ -156,7 +156,7 @@ describe('processVersionedRouter', () => {
     expect(results.paths['/foo']!.get).toBeDefined();
 
     expect(results.paths['/foo']!.get!.description).toBe(
-      '[Authz] Route required privileges: ALL of [manage_spaces].'
+      'This is a test route description.<br/><br/>[Required authorization] Route required privileges: ALL of [manage_spaces].'
     );
   });
 });
@@ -175,6 +175,7 @@ const createTestRoute: () => any = () => ({
         requiredPrivileges: ['manage_spaces'],
       },
     },
+    description: 'This is a test route description.',
   },
   handlers: [
     {
--- packages/kbn-router-to-openapispec/src/process_versioned_router.ts
+++ packages/kbn-router-to-openapispec/src/process_versioned_router.ts
@@ -91,12 +91,13 @@ export const processVersionedRouter = (
           ...queryObjects,
         ];
       }
-
-      let description = '';
+      let description = `${route.options.description ?? ''}`;
       if (route.options.security) {
         const authzDescription = extractAuthzDescription(route.options.security);
 
-        description = `${route.options.description ?? ''}${authzDescription ?? ''}`;
+        description += `${route.options.description && authzDescription ? '<br/><br/>' : ''}${
+          authzDescription ?? ''
+        }`;
       }
 
       const hasBody = Boolean(extractValidationSchemaFromVersionedHandler(handler)?.request?.body);
@@ -106,7 +107,6 @@ export const processVersionedRouter = (
         summary: route.options.summary ?? '',
         tags: route.options.options?.tags ? extractTags(route.options.options.tags) : [],
         ...(description ? { description } : {}),
-        ...(route.options.description ? { description: route.options.description } : {}),
         ...(route.options.deprecated ? { deprecated: route.options.deprecated } : {}),
         ...(route.options.discontinued ? { 'x-discontinued': route.options.discontinued } : {}),
         requestBody: hasBody
