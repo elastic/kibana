768a135b509555c8fbc30212bb75f3f36a1760fb
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-11-05 04:05:05 +1100

    [ML] Unauthorized route migration for routes owned by ml-ui (#198337)
    ### Authz API migration for unauthorized routes
    
    This PR migrates unauthorized routes owned by your team to a new
    security configuration.
    Please refer to the documentation for more information: [Authorization
    API](https://docs.elastic.dev/kibana-dev-docs/key-concepts/security-api-authorization)
    
    ### **Before migration:**
    ```ts
    router.get({
      path: '/api/path',
      ...
    }, handler);
    ```
    
    ### **After migration:**
    ```ts
    router.get({
      path: '/api/path',
      security: {
        authz: {
          enabled: false,
          reason: 'This route is opted out from authorization because ...',
        },
      },
      ...
    }, handler);
    ```
    
    ### What to do next?
    1. Review the changes in this PR.
    2. Elaborate on the reasoning to opt-out of authorization.
    3. Routes without a compelling reason to opt-out of authorization should
    plan to introduce them as soon as possible.
    2. You might need to update your tests to reflect the new security
    configuration:
      - If you have snapshot tests that include the route definition.
    
    ## Any questions?
    If you have any questions or need help with API authorization, please
    reach out to the `@elastic/kibana-security` team.
    
    ---------
    
    Co-authored-by: James Gowdy <jgowdy@elastic.co>

--- x-pack/plugins/aiops/server/routes/log_rate_analysis_field_candidates/define_route.ts
+++ x-pack/plugins/aiops/server/routes/log_rate_analysis_field_candidates/define_route.ts
@@ -35,6 +35,13 @@ export const defineRoute = (
     .addVersion(
       {
         version: '1',
+        security: {
+          authz: {
+            enabled: false,
+            reason:
+              'This route is opted out from authorization because permissions will be checked by elasticsearch',
+          },
+        },
         validate: {
           request: {
             body: aiopsLogRateAnalysisSchemaV3,
