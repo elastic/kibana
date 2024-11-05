564f8da15b8f35e5548825cb2e7f314993069c6f
Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>
2024-11-05 12:32:49 +0100

    Allow unescaped json as input

--- x-pack/plugins/ingest_pipelines/public/application/components/pipeline_editor/components/processor_form/field_components/xjson_editor.tsx
+++ x-pack/plugins/ingest_pipelines/public/application/components/pipeline_editor/components/processor_form/field_components/xjson_editor.tsx
@@ -6,7 +6,7 @@
  */
 
 import { XJsonLang } from '@kbn/monaco';
-import React, { FunctionComponent, useCallback } from 'react';
+import React, { FunctionComponent, useCallback, useState } from 'react';
 import { FieldHook, XJson } from '../../../../../../shared_imports';
 
 const { useXJsonMode } = XJson;
@@ -27,23 +27,25 @@ const defaultEditorOptions = {
 export const XJsonEditor: FunctionComponent<Props> = ({ field, editorProps, rawValue }) => {
   const { value, setValue } = field;
   const { xJson, setXJson, convertToJson } = useXJsonMode(value);
+  const [rawEditorValue, setRawEditorValue] = useState(value);
 
   const onChange = useCallback(
     (s: string) => {
       if (rawValue) {
-        setValue(s);
+        setRawEditorValue(s);
       } else {
         setXJson(s);
-        setValue(convertToJson(s));
       }
+      setValue(convertToJson(s));
     },
-    [setValue, setXJson, convertToJson, rawValue]
+    [setValue, setXJson, convertToJson, rawValue, setRawEditorValue]
   );
+
   return (
     <TextEditor
       field={field}
       editorProps={{
-        value: rawValue ? value : xJson,
+        value: rawValue ? rawEditorValue : xJson,
         languageId: XJsonLang.ID,
         options: defaultEditorOptions,
         onChange,
