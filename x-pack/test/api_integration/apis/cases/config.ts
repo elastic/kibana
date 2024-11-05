e28128e1666e22900eae67b2db34b5fca182aac3
Xavier Mouligneau <xavier.mouligneau@elastic.co>
2024-11-05 14:18:18 -0500

    wrong file

--- x-pack/test/api_integration/apis/cases/config.ts
+++ x-pack/test/api_integration/apis/cases/config.ts
@@ -13,14 +13,5 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   return {
     ...baseIntegrationTestsConfig.getAll(),
     testFiles: [require.resolve('.')],
-    kbnTestServer: {
-      ...baseIntegrationTestsConfig.get('kbnTestServer'),
-      serverArgs: [
-        ...baseIntegrationTestsConfig.get('kbnTestServer.serverArgs'),
-        '--xpack.cloud.id="ftr_fake_cloud_id:aGVsbG8uY29tOjQ0MyRFUzEyM2FiYyRrYm4xMjNhYmM="',
-        '--xpack.cloud.base_url="https://cloud.elastic.co"',
-        '--xpack.spaces.allowSolutionVisibility=true',
-      ],
-    },
   };
 }
