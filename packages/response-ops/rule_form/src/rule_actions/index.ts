2ad2f426d3d2dca19af7e5294a64008da5ce8490
Zacqary Xeper <zacqary.xeper@elastic.co>
2024-11-05 17:32:51 -0600

    Fix typecheck

--- packages/response-ops/rule_form/src/rule_actions/index.ts
+++ packages/response-ops/rule_form/src/rule_actions/index.ts
@@ -8,3 +8,6 @@
  */
 
 export * from './rule_actions';
+export * from './rule_actions_notify_when';
+export * from './rule_actions_alerts_filter';
+export * from './rule_actions_alerts_filter_timeframe';
