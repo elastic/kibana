9a06f57f5d1b0067f4896c64bd04916fe8ad0670
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-11-06 10:27:41 +0100

    Update saml_auth_provider.ts

--- packages/kbn-ftr-common-functional-services/services/saml_auth/saml_auth_provider.ts
+++ packages/kbn-ftr-common-functional-services/services/saml_auth/saml_auth_provider.ts
@@ -97,7 +97,7 @@ export function SamlAuthProvider({ getService }: FtrProviderContext) {
      *
      * @param role - The SAML role for which the session token is required.
      * @param options - Optional settings to control session behavior, such as forcing a new session.
-     * @returns An object with the Cookie header for API authentication.
+     * @returns A string with the Cookie token 
      *
      * @throws If the specified role is a custom role without a predefined descriptor.
      */
