ef4fc6a3337653b9c96be9ec2356ffc34e5be647
Davis McPhee <davis.mcphee@elastic.co>
2024-11-05 20:48:33 -0400

    [Discover] Add `getRenderAppWrapper` extension (#197556)
    ## Summary
    
    This PR adds a `getRenderAppWrapper` extension to Discover to support
    advanced state management use cases. It also includes new documentation
    for the extension point, and overriding default profile implementations:
    
    https://github.com/user-attachments/assets/70633cbb-1cfe-47fe-984e-ba8afb18fc90
    
    To test, add the following config to `kibana.dev.yml`:
    ```yml
    discover.experimental.enabledProfiles:
      [
        'example-root-profile',
        'example-solution-view-root-profile',
        'example-data-source-profile',
        'example-document-profile',
      ]
    ```
    
    Then ingest demo logs and run this in dev tools:
    ```
    POST _aliases
    {
      "actions": [
        {
          "add": {
            "index": "kibana_sample_data_logs",
            "alias": "my-example-logs"
          }
        }
      ]
    }
    ```
    
    Flaky test runs:
    - 🔴 x25:
    https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/7238
    - 🔴 x25:
    https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/7289
    - 🟢 x25:
    https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/7291
    - x25:
    https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/7303
    
    ### Checklist
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [x]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] Any UI touched in this PR is usable by keyboard only (learn more
    about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
    - [ ] Any UI touched in this PR does not create any new axe failures
    (run axe in browser:
    [FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),
    [Chrome](https://chrome.google.com/webstore/detail/axe-web-accessibility-tes/lhdoppojpmngadmnindnejefpokejbdd?hl=en-US))
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This renders correctly on smaller devices using a responsive
    layout. (You can test this [in your
    browser](https://www.browserstack.com/guide/responsive-testing-on-local-server))
    - [ ] This was checked for [cross-browser
    compatibility](https://www.elastic.co/support/matrix#matrix_browsers)
    
    ### For maintainers
    
    - [ ] This was checked for breaking API changes and was [labeled
    appropriately](https://www.elastic.co/guide/en/kibana/master/contributing.html#_add_your_labels)
    - [ ] This will appear in the **Release Notes** and follow the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: Julia Rechkunova <julia.rechkunova@elastic.co>
    (cherry picked from commit 4a95eec82f0a3072ae3f9787942b77ca48f70cbf)

--- src/plugins/discover/public/context_awareness/profile_providers/example/example_root_profile/index.ts
+++ src/plugins/discover/public/context_awareness/profile_providers/example/example_root_profile/index.ts
@@ -7,4 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export { createExampleRootProfileProvider } from './profile';
+export {
+  createExampleRootProfileProvider,
+  createExampleSolutionViewRootProfileProvider,
+} from './profile';
