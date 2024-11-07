732f4ae5430dab1dd35a4aeebf61b65ea899b887
Søren Louv-Jansen <soren.louv@elastic.co>
2024-11-07 08:15:25 +0100

    Fix summarisation test

--- x-pack/test/observability_ai_assistant_api_integration/tests/complete/functions/summarize.spec.ts
+++ x-pack/test/observability_ai_assistant_api_integration/tests/complete/functions/summarize.spec.ts
@@ -32,7 +32,6 @@ export default function ApiTest({ getService }: FtrProviderContext) {
     let connectorId: string;
 
     before(async () => {
-      await clearKnowledgeBase(es);
       await createKnowledgeBaseModel(ml);
       await observabilityAIAssistantAPIClient
         .editor({
@@ -55,7 +54,7 @@ export default function ApiTest({ getService }: FtrProviderContext) {
           name: 'summarize',
           trigger: MessageRole.User,
           arguments: JSON.stringify({
-            id: 'my-id',
+            title: 'My Title',
             text: 'Hello world',
             is_correction: false,
             confidence: 'high',
@@ -72,6 +71,7 @@ export default function ApiTest({ getService }: FtrProviderContext) {
 
       await deleteActionConnector({ supertest, connectorId, log });
       await deleteKnowledgeBaseModel(ml);
+      await clearKnowledgeBase(es);
     });
 
     it('persists entry in knowledge base', async () => {
@@ -86,14 +86,14 @@ export default function ApiTest({ getService }: FtrProviderContext) {
         },
       });
 
-      const { role, public: isPublic, text, type, user, id } = res.body.entries[0];
+      const { role, public: isPublic, text, type, user, title } = res.body.entries[0];
 
       expect(role).to.eql('assistant_summarization');
       expect(isPublic).to.eql(false);
       expect(text).to.eql('Hello world');
       expect(type).to.eql('contextual');
       expect(user?.name).to.eql('editor');
-      expect(id).to.eql('my-id');
+      expect(title).to.eql('My Title');
       expect(res.body.entries).to.have.length(1);
     });
   });
