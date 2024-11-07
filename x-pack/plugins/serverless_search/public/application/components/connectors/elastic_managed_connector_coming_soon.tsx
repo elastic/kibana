119e644b9546678244f3864923db69d07dbd25a7
José Luis González <joseluisgj@gmail.com>
2024-11-07 11:19:17 +0100

    Elastic managed connector coming soon page

---
+++ x-pack/plugins/serverless_search/public/application/components/connectors/elastic_managed_connector_coming_soon.tsx
@@ -0,0 +1,235 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiPanel,
+  EuiIcon,
+  EuiTitle,
+  EuiText,
+  EuiToolTip,
+  EuiBadge,
+  EuiStepsHorizontal,
+  EuiStepsHorizontalProps,
+  EuiButtonEmpty,
+} from '@elastic/eui';
+
+import { i18n } from '@kbn/i18n';
+
+import { css } from '@emotion/react';
+import { useKibanaServices } from '../../hooks/use_kibana';
+import { useConnectorTypes } from '../../hooks/api/use_connector_types';
+import { useAssetBasePath } from '../../hooks/use_asset_base_path';
+// import { FeedbackForm } from '../coming_soon/feedback_form';
+
+import { BACK_LABEL } from '../../../../common/i18n_string';
+
+export const ElasticManagedConnectorComingSoon: React.FC = () => {
+  const connectorTypes = useConnectorTypes();
+
+  const getConnectorByName = (connectors: typeof connectorTypes, serviceType: string) => {
+    return connectors.find((connector) => connector.serviceType === serviceType);
+  };
+
+  const connectorIcon = (connector: { name: string; serviceType: string; iconPath?: string }) => {
+    return (
+      <EuiToolTip content={connector.name}>
+        <EuiIcon
+          size="l"
+          title={connector?.name}
+          id={connector?.serviceType}
+          type={connector?.iconPath || 'defaultIcon'}
+        />
+      </EuiToolTip>
+    );
+  };
+
+  const connectorExample1 = getConnectorByName(connectorTypes, 'gmail');
+  const connectorExample2 = getConnectorByName(connectorTypes, 'sharepoint_online');
+  const connectorExample3 = getConnectorByName(connectorTypes, 'jira');
+  const connectorExample4 = getConnectorByName(connectorTypes, 'dropbox');
+
+  const {
+    application: { navigateToUrl },
+  } = useKibanaServices();
+
+  const assetBasePath = useAssetBasePath();
+  const connectorsIcon = assetBasePath + '/connectors.svg';
+  const horizontalSteps: EuiStepsHorizontalProps['steps'] = [
+    {
+      title: '',
+      status: 'incomplete',
+      onClick: () => {},
+    },
+    {
+      title: '',
+      status: 'incomplete',
+      onClick: () => {},
+    },
+  ];
+  return (
+    <EuiFlexGroup alignItems="center" direction="column">
+      <EuiFlexItem>
+        <EuiPanel paddingSize="l" hasShadow={false} hasBorder>
+          <EuiFlexGroup
+            alignItems="center"
+            justifyContent="center"
+            direction="column"
+            gutterSize="l"
+          >
+            <EuiFlexItem>
+              <EuiButtonEmpty
+                data-test-subj="serverlessSearchElasticManagedConnectorEmptyBackButton"
+                iconType="arrowLeft"
+                onClick={() => navigateToUrl(`./`)}
+              >
+                {BACK_LABEL}
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiIcon size="xxl" type={connectorsIcon} />
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiTitle>
+                <h2>
+                  {i18n.translate('xpack.serverlessSearch.elasticManagedConnectorEmpty.title', {
+                    defaultMessage: 'Elastic managed connectors',
+                  })}
+                </h2>
+              </EuiTitle>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiBadge color="accent">Coming soon</EuiBadge>
+            </EuiFlexItem>
+            <EuiFlexItem>
+              <EuiText textAlign="center" color="subdued">
+                <p>
+                  {i18n.translate(
+                    'xpack.serverlessSearch.elasticManagedConnectorEmpty.description',
+                    {
+                      defaultMessage:
+                        "We're actively developing Elastic managed connectors, that won't require any self-managed infrastructure. You'll be able to handle all configuration in the UI. This will simplify syncing your data into a serverless Elasticsearch project. This new workflow will have two steps:",
+                    }
+                  )}
+                </p>
+              </EuiText>
+            </EuiFlexItem>
+            <EuiFlexGroup
+              alignItems="stretch"
+              justifyContent="center"
+              direction="column"
+              gutterSize="s"
+            >
+              <EuiFlexItem>
+                <EuiPanel color="subdued">
+                  <EuiFlexItem grow={false}>
+                    <EuiStepsHorizontal
+                      css={css`
+                        pointer-events: none;
+                      `}
+                      steps={horizontalSteps}
+                      size="s"
+                      role="presentation"
+                      // @ts-ignore
+                      inert=""
+                    />
+                  </EuiFlexItem>
+                  <EuiFlexGroup>
+                    <EuiFlexItem>
+                      <EuiFlexGroup
+                        justifyContent="flexStart"
+                        alignItems="center"
+                        direction="column"
+                      >
+                        <EuiFlexItem grow={false}>
+                          <EuiFlexGroup
+                            justifyContent="center"
+                            alignItems="center"
+                            direction="row"
+                            gutterSize="s"
+                          >
+                            <EuiFlexItem grow={false}>
+                              {connectorExample1 && connectorIcon(connectorExample1)}
+                            </EuiFlexItem>
+                            <EuiFlexItem grow={false}>
+                              {connectorExample2 && connectorIcon(connectorExample2)}
+                            </EuiFlexItem>
+                            <EuiFlexItem grow={false}>
+                              <EuiIcon color="primary" size="l" type="documents" />
+                            </EuiFlexItem>
+                            <EuiFlexItem grow={false}>
+                              {connectorExample3 && connectorIcon(connectorExample3)}
+                            </EuiFlexItem>
+                            <EuiFlexItem grow={false}>
+                              {connectorExample4 && connectorIcon(connectorExample4)}
+                            </EuiFlexItem>
+                          </EuiFlexGroup>
+                        </EuiFlexItem>
+                        <EuiFlexItem grow={false}>
+                          <EuiText>
+                            <p>
+                              {i18n.translate(
+                                'xpack.serverlessSearch.elasticManagedConnectorEmpty.guideOneDescription',
+                                {
+                                  defaultMessage:
+                                    "Choose from over 30 third-party data sources you'd like to sync",
+                                }
+                              )}
+                            </p>
+                          </EuiText>
+                        </EuiFlexItem>
+                      </EuiFlexGroup>
+                    </EuiFlexItem>
+                    <EuiFlexItem>
+                      <EuiFlexGroup
+                        justifyContent="flexStart"
+                        alignItems="center"
+                        direction="column"
+                      >
+                        <EuiFlexItem grow={false}>
+                          <EuiFlexGroup
+                            gutterSize="s"
+                            direction="row"
+                            alignItems="center"
+                            justifyContent="center"
+                          >
+                            <EuiFlexItem>
+                              <EuiIcon color="primary" size="l" type={connectorsIcon} />
+                            </EuiFlexItem>
+                            <EuiFlexItem>
+                              <EuiIcon color="primary" size="l" type="logoElastic" />
+                            </EuiFlexItem>
+                          </EuiFlexGroup>
+                        </EuiFlexItem>
+                        <EuiFlexItem>
+                          <EuiText>
+                            <p>
+                              {i18n.translate(
+                                'xpack.serverlessSearch.elasticManagedConnectorEmpty.guideThreeDescription',
+                                {
+                                  defaultMessage:
+                                    'Enter access and connection details for your data source and run your first sync using the Kibana UI',
+                                }
+                              )}
+                            </p>
+                          </EuiText>
+                        </EuiFlexItem>
+                      </EuiFlexGroup>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiPanel>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            {/* <FeedbackForm /> */}
+          </EuiFlexGroup>
+        </EuiPanel>
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
---
+++ x-pack/plugins/serverless_search/public/application/components/connectors_elastic_managed.tsx
@@ -0,0 +1,124 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import {
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiLink,
+  EuiPageTemplate,
+  EuiText,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { FormattedMessage } from '@kbn/i18n-react';
+import React, { useMemo } from 'react';
+
+import { LEARN_MORE_LABEL } from '../../../common/i18n_string';
+import { PLUGIN_ID } from '../../../common';
+import { useConnectors } from '../hooks/api/use_connectors';
+import { useCreateConnector } from '../hooks/api/use_create_connector';
+import { useKibanaServices } from '../hooks/use_kibana';
+import { ElasticManagedConnectorComingSoon } from './connectors/elastic_managed_connector_coming_soon';
+
+import { CONNECTORS } from '../constants';
+
+export const ConnectorsElasticManaged = () => {
+  const { data, isLoading: connectorsLoading } = useConnectors();
+  const { http, console: consolePlugin } = useKibanaServices();
+  const { createConnector, isLoading } = useCreateConnector();
+  const embeddableConsole = useMemo(
+    () => (consolePlugin?.EmbeddableConsole ? <consolePlugin.EmbeddableConsole /> : null),
+    [consolePlugin]
+  );
+
+  return (
+    <EuiPageTemplate offset={0} grow restrictWidth data-test-subj="svlSearchConnectorsPage">
+      <EuiPageTemplate.Header
+        pageTitle={i18n.translate('xpack.serverlessSearch.connectors.title', {
+          defaultMessage: 'Connectors',
+        })}
+        data-test-subj="serverlessSearchConnectorsTitle"
+        restrictWidth
+        rightSideItems={
+          connectorsLoading || (data?.connectors || []).length > 0
+            ? [
+                <EuiFlexGroup direction="row" alignItems="center" justifyContent="center">
+                  <EuiFlexItem>
+                    <EuiFlexGroup
+                      alignItems="center"
+                      gutterSize="xs"
+                      justifyContent="flexEnd"
+                      direction="row"
+                    >
+                      <EuiFlexItem grow={false}>
+                        <EuiIcon
+                          size="s"
+                          type={http.basePath.prepend(`/plugins/${PLUGIN_ID}/assets/github.svg`)}
+                        />
+                      </EuiFlexItem>
+                      <EuiFlexItem grow={false}>
+                        <EuiText size="s">
+                          <EuiLink
+                            data-test-subj="serverlessSearchConnectorsOverviewElasticConnectorsLink"
+                            target="_blank"
+                            href={CONNECTORS.github_repo}
+                          >
+                            {i18n.translate('xpack.serverlessSearch.connectorsPythonLink', {
+                              defaultMessage: 'elastic/connectors',
+                            })}
+                          </EuiLink>
+                        </EuiText>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiButton
+                      data-test-subj="serverlessSearchConnectorsOverviewCreateConnectorButton"
+                      isLoading={isLoading}
+                      fill
+                      iconType="plusInCircleFilled"
+                      onClick={() => createConnector()}
+                    >
+                      {i18n.translate('xpack.serverlessSearch.connectors.createConnector', {
+                        defaultMessage: 'Create connector',
+                      })}
+                    </EuiButton>
+                  </EuiFlexItem>
+                </EuiFlexGroup>,
+              ]
+            : undefined
+        }
+      >
+        <EuiText>
+          <p>
+            <FormattedMessage
+              id="xpack.serverlessSearch.connectors.headerContent"
+              defaultMessage="Sync third-party data sources to Elasticsearch, by deploying Elastic connectors on your own infrastructure. {learnMoreLink}"
+              values={{
+                learnMoreLink: (
+                  <EuiLink
+                    data-test-subj="serverlessSearchConnectorsOverviewLink"
+                    external
+                    target="_blank"
+                    href={CONNECTORS.self_managed_docs}
+                  >
+                    {LEARN_MORE_LABEL}
+                  </EuiLink>
+                ),
+              }}
+            />
+          </p>
+        </EuiText>
+      </EuiPageTemplate.Header>
+      <EuiPageTemplate.Section restrictWidth color="subdued">
+        <ElasticManagedConnectorComingSoon />
+      </EuiPageTemplate.Section>
+      {embeddableConsole}
+    </EuiPageTemplate>
+  );
+};
