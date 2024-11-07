0676d5315365697b86652c725d884ae180c4fa93
neptunian <sandra.gonzales@elastic.co>
2024-11-07 15:49:58 -0500

    cleanup

--- x-pack/test_serverless/functional/test_suites/common/data_usage/privileges.ts
+++ x-pack/test_serverless/functional/test_suites/common/data_usage/privileges.ts
@@ -14,44 +14,42 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const retry = getService('retry');
   const dataUsageAppUrl = 'management/data/data_usage';
 
-  describe('privileges', function () {
-    // - plugin needs to be anbled in serverless
-    // - observability needs to support custom roles otherwise we'll need to seprate the custom role test out
-    // which is being enabled by feature flag config not available in MKI (x-pack/test_serverless/functional/test_suites/observability/config.feature_flags.ts)
-    this.tags(['skipMKI']);
-    it('renders for the admin role', async () => {
-      await pageObjects.svlCommonPage.loginAsAdmin();
-      await pageObjects.common.navigateToApp('management');
-      await retry.waitFor('page to be visible', async () => {
-        return await testSubjects.exists('cards-navigation-page');
-      });
+  const navigateAndVerify = async (expectedVisible: boolean) => {
+    await pageObjects.common.navigateToApp('management');
+    await retry.waitFor('page to be visible', async () =>
+      testSubjects.exists('cards-navigation-page')
+    );
+
+    if (expectedVisible) {
       await pageObjects.svlManagementPage.assertDataUsageManagementCardExists();
       await pageObjects.common.navigateToApp(dataUsageAppUrl);
-      await retry.waitFor('page to be visible', async () => {
-        return await testSubjects.exists('DataUsagePage');
-      });
+      await testSubjects.exists('DataUsagePage');
+    } else {
+      await pageObjects.svlManagementPage.assertDataUsageManagementCardDoesNotExist();
+      await pageObjects.common.navigateToApp(dataUsageAppUrl);
+      await testSubjects.missingOrFail('DataUsagePage');
+    }
+  };
+
+  describe('privileges', function () {
+    // - plugin needs to be enabled in serverless
+    this.tags(['skipMKI']);
+
+    it('renders for the admin role', async () => {
+      await pageObjects.svlCommonPage.loginAsAdmin();
+      await navigateAndVerify(true);
     });
 
     it('does not render for viewer', async () => {
       await pageObjects.svlCommonPage.loginAsViewer();
-      await pageObjects.common.navigateToApp('management');
-      await retry.waitFor('page to be visible', async () => {
-        return await testSubjects.exists('cards-navigation-page');
-      });
-      await pageObjects.svlManagementPage.assertDataUsageManagementCardDoesNotExist();
-      await pageObjects.common.navigateToApp(dataUsageAppUrl);
-      await testSubjects.missingOrFail('DataUsagePage');
+      await navigateAndVerify(false);
     });
+
     it('does not render for default (editor/developer) role', async () => {
       await pageObjects.svlCommonPage.loginWithPrivilegedRole();
-      await pageObjects.common.navigateToApp('management');
-      await retry.waitFor('page to be visible', async () => {
-        return await testSubjects.exists('cards-navigation-page');
-      });
-      await pageObjects.svlManagementPage.assertDataUsageManagementCardDoesNotExist();
-      await pageObjects.common.navigateToApp(dataUsageAppUrl);
-      await testSubjects.missingOrFail('DataUsagePage');
+      await navigateAndVerify(false);
     });
+
     it('renders with a custom role that has the monitor cluster privilege', async () => {
       await samlAuth.setCustomRole({
         elasticsearch: {
@@ -67,15 +65,10 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
         ],
       });
       await pageObjects.svlCommonPage.loginWithCustomRole();
-      await pageObjects.common.navigateToApp('management');
-      await retry.waitFor('page to be visible', async () => {
-        return await testSubjects.exists('cards-navigation-page');
-      });
-      await pageObjects.svlManagementPage.assertDataUsageManagementCardExists();
-      await pageObjects.common.navigateToApp(dataUsageAppUrl);
-      await testSubjects.exists('DataUsagePage');
+      await navigateAndVerify(true);
       await samlAuth.deleteCustomRole();
     });
+
     it('does not render with a custom role that does not have the monitor cluster privilege', async () => {
       await samlAuth.setCustomRole({
         elasticsearch: {
@@ -90,13 +83,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
         ],
       });
       await pageObjects.svlCommonPage.loginWithCustomRole();
-      await pageObjects.common.navigateToApp('management');
-      await retry.waitFor('page to be visible', async () => {
-        return await testSubjects.exists('cards-navigation-page');
-      });
-      await pageObjects.svlManagementPage.assertDataUsageManagementCardDoesNotExist();
-      await pageObjects.common.navigateToApp(dataUsageAppUrl);
-      await testSubjects.missingOrFail('DataUsagePage');
+      await navigateAndVerify(false);
       await samlAuth.deleteCustomRole();
     });
   });
