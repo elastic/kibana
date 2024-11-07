7d49cc7f51629808514abb18727cfbad7f56a7d3
Matthew Kime <matt@mattki.me>
2024-11-07 15:11:35 -0600

    move some dependencies to package

--- x-pack/packages/index-lifecycle-management/index-lifecycle-management-common-shared/src/policies.ts
+++ x-pack/packages/index-lifecycle-management/index-lifecycle-management-common-shared/src/policies.ts
@@ -5,6 +5,13 @@
  * 2.0.
  */
 
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 export type Phase = keyof Phases;
 
 export type PhaseWithAllocation = 'warm' | 'cold';
