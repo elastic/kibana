05519cdb5539b06da80157f574818779ae35b99d
Caue Marcondes <caue.marcondes@elastic.co>
2024-11-07 13:58:59 +0000

    fixing tests

--- x-pack/plugins/observability_solution/inventory/e2e/cypress/e2e/home.cy.ts
+++ x-pack/plugins/observability_solution/inventory/e2e/cypress/e2e/home.cy.ts
@@ -134,7 +134,7 @@ describe('Home page', () => {
         );
       });
 
-      it('Navigates to discover with kuery filter', () => {
+      it.skip('Navigates to discover with kuery filter', () => {
         cy.intercept('GET', '/internal/entities/managed/enablement', {
           fixture: 'eem_enabled.json',
         }).as('getEEMStatus');
@@ -165,16 +165,17 @@ describe('Home page', () => {
         cy.intercept('GET', '/internal/entities/managed/enablement', {
           fixture: 'eem_enabled.json',
         }).as('getEEMStatus');
+        cy.intercept('POST', 'internal/controls/optionsList/entities-*-latest').as(
+          'entityTypeControlGroupOptions'
+        );
         cy.intercept('GET', '/internal/inventory/entities?**').as('getEntities');
         cy.intercept('GET', '/internal/inventory/entities/group_by/**').as('getGroups');
         cy.visitKibana('/app/inventory');
         cy.wait('@getEEMStatus');
-        cy.getByTestSubj('entityTypesFilterComboBox')
-          .click()
-          .getByTestSubj('entityTypesFilterserviceOption')
-          .click();
+        cy.getByTestSubj('optionsList-control-entity.type').click();
+        cy.wait('@entityTypeControlGroupOptions');
+        cy.getByTestSubj('optionsList-control-selection-service').click();
         cy.wait('@getGroups');
-        cy.contains('service');
         cy.getByTestSubj('inventoryGroupTitle_entity.type_service').click();
         cy.wait('@getEntities');
         cy.get('server1').should('not.exist');
@@ -188,16 +189,17 @@ describe('Home page', () => {
         cy.intercept('GET', '/internal/entities/managed/enablement', {
           fixture: 'eem_enabled.json',
         }).as('getEEMStatus');
+        cy.intercept('POST', 'internal/controls/optionsList/entities-*-latest').as(
+          'entityTypeControlGroupOptions'
+        );
         cy.intercept('GET', '/internal/inventory/entities?**').as('getEntities');
         cy.intercept('GET', '/internal/inventory/entities/group_by/**').as('getGroups');
         cy.visitKibana('/app/inventory');
         cy.wait('@getEEMStatus');
-        cy.getByTestSubj('entityTypesFilterComboBox')
-          .click()
-          .getByTestSubj('entityTypesFilterhostOption')
-          .click();
+        cy.getByTestSubj('optionsList-control-entity.type').click();
+        cy.wait('@entityTypeControlGroupOptions');
+        cy.getByTestSubj('optionsList-control-selection-host').click();
         cy.wait('@getGroups');
-        cy.contains('host');
         cy.getByTestSubj('inventoryGroupTitle_entity.type_host').click();
         cy.wait('@getEntities');
         cy.contains('server1');
@@ -211,16 +213,17 @@ describe('Home page', () => {
         cy.intercept('GET', '/internal/entities/managed/enablement', {
           fixture: 'eem_enabled.json',
         }).as('getEEMStatus');
+        cy.intercept('POST', 'internal/controls/optionsList/entities-*-latest').as(
+          'entityTypeControlGroupOptions'
+        );
         cy.intercept('GET', '/internal/inventory/entities?**').as('getEntities');
         cy.intercept('GET', '/internal/inventory/entities/group_by/**').as('getGroups');
         cy.visitKibana('/app/inventory');
         cy.wait('@getEEMStatus');
-        cy.getByTestSubj('entityTypesFilterComboBox')
-          .click()
-          .getByTestSubj('entityTypesFiltercontainerOption')
-          .click();
+        cy.getByTestSubj('optionsList-control-entity.type').click();
+        cy.wait('@entityTypeControlGroupOptions');
+        cy.getByTestSubj('optionsList-control-selection-container').click();
         cy.wait('@getGroups');
-        cy.contains('container');
         cy.getByTestSubj('inventoryGroupTitle_entity.type_container').click();
         cy.wait('@getEntities');
         cy.contains('server1').should('not.exist');
