elastic/kibana

commit 257688d9a763218bf07f02425a8355b9cdf0477b

Move consumers off bsearch endpoint (#196962)
## Summary

Part of https://github.com/elastic/kibana/issues/186139.

Moves direct consumers off of the `internal/bsearch` endpoint and onto
the `internal/search`.

This is in preparation for removing the `internal/bsearch` endpoint
entirely.

Updates automated tests to mock/use the correct service moving forward.

### Checklist

- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
Co-authored-by: Dzmitry Lemechko <dzmitry.lemechko@elastic.co>

packages/kbn-ftr-common-functional-services/services/search.ts
@@ -8,25 +8,10 @@
  */
 
 import expect from '@kbn/expect';
-import request from 'superagent';
 import type SuperTest from 'supertest';
 import type { IEsSearchResponse } from '@kbn/search-types';
 import { ELASTIC_HTTP_VERSION_HEADER } from '@kbn/core-http-common';
-import { BFETCH_ROUTE_VERSION_LATEST } from '@kbn/bfetch-plugin/common';
 import { FtrService } from './ftr_provider_context';
-
-/**
- * Function copied from here:
- * test/api_integration/apis/search/bsearch.ts without the compress
- *
- * Splits the JSON lines from bsearch
- */
-const parseBfetchResponse = (resp: request.Response): Array<Record<string, any>> => {
-  return resp.text
-    .trim()
-    .split('\n')
-    .map((item) => JSON.parse(item));
-};
 
 /**
  * Function copied from here:
@@ -48,13 +33,13 @@ export interface SendOptions {
 }
 
 /**
- * Bsearch Service that can reduce flake on the CI systems when they are under
- * pressure and bsearch returns an async search response or a sync response.
+ * Search Service that can reduce flake on the CI systems when they are under
+ * pressure and search returns an async search response or a sync response.
  *
  * @example
  * const supertest = getService('supertest');
- * const bsearch = getService('bsearch');
- * const response = await bsearch.send<MyType>({
+ * const search = getService('search');
+ * const response = await search.send<MyType>({
  *   supertest,
  *   options: {
  *     defaultIndex: ['large_volume_dns_data'],
@@ -64,7 +49,7 @@ export interface SendOptions {
  * expect(response).eql({ ... your value ... });
  */
 
-export class BsearchService extends FtrService {
+export class SearchService extends FtrService {
   private readonly retry = this.ctx.getService('retry');
 
   /** Send method to send in your supertest, url, options, and strategy name */
@@ -85,26 +70,13 @@ export class BsearchService extends FtrService {
 
     const result = await this.retry.try(async () => {
       const resp = await supertest
-        .post(`${spaceUrl}/internal/bsearch`)
+        .post(`${spaceUrl}/internal/search/${strategy}/${body.id}`)
         .set('kbn-xsrf', 'true')
-        .set(ELASTIC_HTTP_VERSION_HEADER, BFETCH_ROUTE_VERSION_LATEST)
-        .send({
-          batch: [
-            {
-              request: {
-                id: body.id,
-                ...options,
-              },
-              options: {
-                strategy,
-              },
-            },
-          ],
-        })
+        .set(ELASTIC_HTTP_VERSION_HEADER, '1')
+        .send()
         .expect(200);
-      const [parsedResponse] = parseBfetchResponse(resp);
-      expect(parsedResponse.result.isRunning).equal(false);
-      return parsedResponse.result as T;
+      expect(resp.body.isRunning).equal(false);
+      return resp.body as T;
     });
     return result;
   }

x-pack/test_serverless/shared/services/search_secure.ts
@@ -13,7 +13,6 @@ import { GenericFtrService } from '@kbn/test';
 import request from 'superagent';
 import type { IEsSearchResponse } from '@kbn/search-types';
 import { ELASTIC_HTTP_VERSION_HEADER } from '@kbn/core-http-common';
-import { BFETCH_ROUTE_VERSION_LATEST } from '@kbn/bfetch-plugin/common';
 import { SupertestWithoutAuthProviderType } from '@kbn/ftr-common-functional-services';
 import { FtrProviderContext } from '../../functional/ftr_provider_context';
 
@@ -35,7 +34,7 @@ export interface SendOptions {
   internalOrigin: string;
 }
 
-export class BsearchSecureService extends GenericFtrService<FtrProviderContext> {
+export class SearchSecureService extends GenericFtrService<FtrProviderContext> {
   private readonly retry = this.ctx.getService('retry');
 
   async send<T extends IEsSearchResponse>({
@@ -104,24 +103,12 @@ export class BsearchSecureService extends GenericFtrService<FtrProviderContext> 
 
     const result = await this.retry.try(async () => {
       const resp = await supertestWithoutAuth
-        .post(`/internal/bsearch`)
+        .post(`/internal/search/${strategy}/${body.id}`)
         .set(apiKeyHeader)
         .set('kbn-xsrf', 'true')
         .set('x-elastic-internal-origin', 'Kibana')
-        .set(ELASTIC_HTTP_VERSION_HEADER, BFETCH_ROUTE_VERSION_LATEST)
-        .send({
-          batch: [
-            {
-              request: {
-                id: body.id,
-                ...options,
-              },
-              options: {
-                strategy,
-              },
-            },
-          ],
-        })
+        .set(ELASTIC_HTTP_VERSION_HEADER, '1')
+        .send()
         .expect(200);
       const [parsedResponse] = parseBfetchResponse(resp);
       expect(parsedResponse.result.isRunning).equal(false);

x-pack/test/common/services/search_secure.ts
@@ -38,7 +38,7 @@ interface SendOptions {
   internalOrigin: string;
 }
 
-export class BsearchSecureService extends FtrService {
+export class SearchSecureService extends FtrService {
   private readonly retry = this.ctx.getService('retry');
 
   async send<T extends IEsSearchResponse>({
@@ -109,24 +109,12 @@ export class BsearchSecureService extends FtrService {
 
     const result = await this.retry.try(async () => {
       const resp = await supertestWithoutAuth
-        .post(`${spaceUrl}/internal/bsearch`)
+        .post(`${spaceUrl}/internal/search/${strategy}/${body.id}`)
         .auth(auth.username, auth.password)
         .set('kbn-xsrf', 'true')
         .set('x-elastic-internal-origin', 'Kibana')
         .set(ELASTIC_HTTP_VERSION_HEADER, BFETCH_ROUTE_VERSION_LATEST)
-        .send({
-          batch: [
-            {
-              request: {
-                id: body.id,
-                ...options,
-              },
-              options: {
-                strategy,
-              },
-            },
-          ],
-        })
+        .send()
         .expect(200);
       const [parsedResponse] = parseBfetchResponse(resp);
       expect(parsedResponse.result.isRunning).equal(false);

x-pack/test/security_solution_api_integration/test_suites/explore/network/trial_license_complete_tier/tests/network_dns.ts
@@ -14,7 +14,7 @@ import {
   NetworkDnsStrategyResponse,
 } from '@kbn/security-solution-plugin/common/search_strategy';
 import TestAgent from 'supertest/lib/agent';
-import { BsearchService } from '@kbn/ftr-common-functional-services';
+import { SearchService } from '@kbn/ftr-common-functional-services';
 import { FtrProviderContextWithSpaces } from '../../../../../ftr_provider_context_with_spaces';
 
 export default function ({ getService }: FtrProviderContextWithSpaces) {
@@ -23,11 +23,11 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
 
   describe('Network DNS', () => {
     let supertest: TestAgent;
-    let bsearch: BsearchService;
+    let search: SearchService;
     describe('With packetbeat', () => {
       before(async () => {
         supertest = await utils.createSuperTest();
-        bsearch = await utils.createBsearch();
+        search = await utils.createSearch();
         await esArchiver.load('x-pack/test/functional/es_archives/packetbeat/dns');
       });
       after(
@@ -38,7 +38,7 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
       const TO = '3000-01-01T00:00:00.000Z';
 
       it('Make sure that we get Dns data and sorting by uniqueDomains ascending', async () => {
-        const networkDns = await bsearch.send<NetworkDnsStrategyResponse>({
+        const networkDns = await search.send<NetworkDnsStrategyResponse>({
           supertest,
           options: {
             defaultIndex: ['packetbeat-*'],
@@ -66,7 +66,7 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
       });
 
       it('Make sure that we get Dns data and sorting by uniqueDomains descending', async () => {
-        const networkDns = await bsearch.send<NetworkDnsStrategyResponse>({
+        const networkDns = await search.send<NetworkDnsStrategyResponse>({
           supertest,
           options: {
             ip: '151.205.0.17',

x-pack/test/security_solution_api_integration/test_suites/explore/network/trial_license_complete_tier/tests/network_top_n_flow.ts
@@ -15,7 +15,7 @@ import {
   NetworkTopNFlowStrategyResponse,
 } from '@kbn/security-solution-plugin/common/search_strategy';
 import TestAgent from 'supertest/lib/agent';
-import { BsearchService } from '@kbn/ftr-common-functional-services';
+import { SearchService } from '@kbn/ftr-common-functional-services';
 
 import { FtrProviderContextWithSpaces } from '../../../../../ftr_provider_context_with_spaces';
 
@@ -27,11 +27,11 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
 
   describe('Network Top N Flow', () => {
     let supertest: TestAgent;
-    let bsearch: BsearchService;
+    let search: SearchService;
     describe('With filebeat', () => {
       before(async () => {
         supertest = await utils.createSuperTest();
-        bsearch = await utils.createBsearch();
+        search = await utils.createSearch();
         await esArchiver.load('x-pack/test/functional/es_archives/filebeat/default');
       });
       after(
@@ -42,7 +42,7 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
       const TO = '2019-02-12T01:57:24.870Z';
 
       it('should get Source NetworkTopNFlow data with bytes_in descending sort', async () => {
-        const networkTopNFlow = await bsearch.send<NetworkTopNFlowStrategyResponse>({
+        const networkTopNFlow = await search.send<NetworkTopNFlowStrategyResponse>({
           supertest,
           options: {
             defaultIndex: ['filebeat-*'],
@@ -77,7 +77,7 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
       });
 
       it('should get Source NetworkTopNFlow data with bytes_in ascending sort ', async () => {
-        const networkTopNFlow = await bsearch.send<NetworkTopNFlowStrategyResponse>({
+        const networkTopNFlow = await search.send<NetworkTopNFlowStrategyResponse>({
           supertest,
           options: {
             defaultIndex: ['filebeat-*'],
@@ -114,7 +114,7 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
       });
 
       it('should get Destination NetworkTopNFlow data', async () => {
-        const networkTopNFlow = await bsearch.send<NetworkTopNFlowStrategyResponse>({
+        const networkTopNFlow = await search.send<NetworkTopNFlowStrategyResponse>({
           supertest,
           options: {
             defaultIndex: ['filebeat-*'],
@@ -146,7 +146,7 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
       });
 
       it('should paginate NetworkTopNFlow query', async () => {
-        const networkTopNFlow = await bsearch.send<NetworkTopNFlowStrategyResponse>({
+        const networkTopNFlow = await search.send<NetworkTopNFlowStrategyResponse>({
           supertest,
           options: {
             defaultIndex: ['filebeat-*'],

packages/kbn-ftr-common-functional-services/index.ts
@@ -39,7 +39,7 @@ export type SamlAuthProviderType = ProvidedType<typeof SamlAuthProvider>;
 export type { FtrProviderContext } from './services/ftr_provider_context';
 export { runSavedObjInfoSvc } from './services/saved_object_info';
 
-export type { BsearchService, SendOptions } from './services/bsearch';
+export type { SearchService, SendOptions } from './services/search';
 export { SavedObjectInfoService } from './services/saved_object_info';
 export { DeploymentService } from './services/deployment';
 export { IndexPatternsService } from './services/index_patterns';

packages/kbn-ftr-common-functional-services/services/all.ts
@@ -11,7 +11,7 @@ import { EsArchiverProvider } from './es_archiver';
 import { EsProvider } from './es';
 import { KibanaServerProvider } from './kibana_server';
 import { RetryService } from './retry';
-import { BsearchService } from './bsearch';
+import { SearchService } from './search';
 import { ConsoleProvider } from './console';
 import { DeploymentService } from './deployment';
 import { EsDeleteAllIndicesProvider } from './es_delete_all_indices';
@@ -27,7 +27,7 @@ export const services = {
   kibanaServer: KibanaServerProvider,
   esArchiver: EsArchiverProvider,
   retry: RetryService,
-  bsearch: BsearchService,
+  search: SearchService,
   console: ConsoleProvider,
   deployment: DeploymentService,
   esDeleteAllIndices: EsDeleteAllIndicesProvider,

test/common/services/index.ts
@@ -21,7 +21,7 @@ const {
   esDeleteAllIndices,
   savedObjectInfo,
   indexPatterns,
-  bsearch,
+  search,
   console,
   supertest,
   esSupertest,
@@ -42,7 +42,7 @@ export const services = {
   esDeleteAllIndices,
   savedObjectInfo,
   indexPatterns,
-  bsearch,
+  search,
   console,
   supertest,
   esSupertest,

commit 116cd9d1708bb698f704689f388c2001361978ef

Fix missing params from search strategy polling requests causing validation errors in tests (#199539)
## Summary

This PR fixes an issue introduced in #196962 where search strategy
polling requests sent from some integration tests were missing necessary
body parameters, resulting in validation errors and test failures. This
issue was not caught by CI in the original PR because it only occurs
when a search request runs slow enough to return partial results (and
therefore requires polling), which is not common in test runs.

Fixes #199537
Fixes #199499
Fixes #199460
Fixes #199449
Fixes #199412
Fixes #199410
Fixes #199394
Fixes #199379
Fixes #199372
Fixes #199363
Fixes #199541
Fixes #199446
Fixes #199433
Fixes #199432
Fixes #199381
Fixes #199369
Fixes #199364

Flaky test runner x200:
https://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/7362.

### Checklist

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [ ] Any UI touched in this PR is usable by keyboard only (learn more
about [keyboard accessibility](https://webaim.org/techniques/keyboard/))
- [ ] Any UI touched in this PR does not create any new axe failures
(run axe in browser:
[FF](https://addons.mozilla.org/en-US/firefox/addon/axe-devtools/),

packages/kbn-ftr-common-functional-services/services/search.ts
@@ -73,7 +73,7 @@ export class SearchService extends FtrService {
         .post(`${spaceUrl}/internal/search/${strategy}/${body.id}`)
         .set('kbn-xsrf', 'true')
         .set(ELASTIC_HTTP_VERSION_HEADER, '1')
-        .send()
+        .send(options)
         .expect(200);
       expect(resp.body.isRunning).equal(false);
       return resp.body as T;

x-pack/test_serverless/shared/services/search_secure.ts
@@ -10,18 +10,10 @@
 
 import expect from '@kbn/expect';
 import { GenericFtrService } from '@kbn/test';
-import request from 'superagent';
 import type { IEsSearchResponse } from '@kbn/search-types';
 import { ELASTIC_HTTP_VERSION_HEADER } from '@kbn/core-http-common';
 import { SupertestWithoutAuthProviderType } from '@kbn/ftr-common-functional-services';
 import { FtrProviderContext } from '../../functional/ftr_provider_context';
-
-const parseBfetchResponse = (resp: request.Response): Array<Record<string, any>> => {
-  return resp.text
-    .trim()
-    .split('\n')
-    .map((item) => JSON.parse(item));
-};
 
 export interface SendOptions {
   supertestWithoutAuth: SupertestWithoutAuthProviderType;
@@ -108,11 +100,10 @@ export class SearchSecureService extends GenericFtrService<FtrProviderContext> {
         .set('kbn-xsrf', 'true')
         .set('x-elastic-internal-origin', 'Kibana')
         .set(ELASTIC_HTTP_VERSION_HEADER, '1')
-        .send()
+        .send(options)
         .expect(200);
-      const [parsedResponse] = parseBfetchResponse(resp);
-      expect(parsedResponse.result.isRunning).equal(false);
-      return parsedResponse.result;
+      expect(resp.body.isRunning).equal(false);
+      return resp.body;
     });
 
     return result as T;

x-pack/test/apm_api_integration/common/utils/parse_b_fetch.ts
@@ -1,15 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import request from 'superagent';
-
-export function parseBfetchResponse(resp: request.Response): Array<Record<string, any>> {
-  return resp.text
-    .trim()
-    .split('\n')
-    .map((item) => JSON.parse(item));
-}

x-pack/test/common/services/search_secure.ts
@@ -9,19 +9,11 @@
 // but with the ability to provide custom auth
 
 import expect from '@kbn/expect';
-import request from 'superagent';
 import type { IEsSearchResponse } from '@kbn/search-types';
 import { ELASTIC_HTTP_VERSION_HEADER } from '@kbn/core-http-common';
 import { BFETCH_ROUTE_VERSION_LATEST } from '@kbn/bfetch-plugin/common';
 import { SupertestWithoutAuthProviderType } from '@kbn/ftr-common-functional-services';
 import { FtrService } from '../ftr_provider_context';
-
-const parseBfetchResponse = (resp: request.Response): Array<Record<string, any>> => {
-  return resp.text
-    .trim()
-    .split('\n')
-    .map((item) => JSON.parse(item));
-};
 
 const getSpaceUrlPrefix = (spaceId?: string): string => {
   return spaceId && spaceId !== 'default' ? `/s/${spaceId}` : ``;
@@ -114,11 +106,10 @@ export class SearchSecureService extends FtrService {
         .set('kbn-xsrf', 'true')
         .set('x-elastic-internal-origin', 'Kibana')
         .set(ELASTIC_HTTP_VERSION_HEADER, BFETCH_ROUTE_VERSION_LATEST)
-        .send()
+        .send(options)
         .expect(200);
-      const [parsedResponse] = parseBfetchResponse(resp);
-      expect(parsedResponse.result.isRunning).equal(false);
-      return parsedResponse.result;
+      expect(resp.body.isRunning).equal(false);
+      return resp.body;
     });
 
     return result as T;

x-pack/test/security_solution_api_integration/test_suites/explore/network/trial_license_complete_tier/tests/network_dns.ts
@@ -21,8 +21,7 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
   const esArchiver = getService('esArchiver');
   const utils = getService('securitySolutionUtils');
 
-  // Failing: See https://github.com/elastic/kibana/issues/199372
-  describe.skip('Network DNS', () => {
+  describe('Network DNS', () => {
     let supertest: TestAgent;
     let search: SearchService;
     describe('With packetbeat', () => {

x-pack/test/security_solution_api_integration/test_suites/explore/network/trial_license_complete_tier/tests/network_top_n_flow.ts
@@ -25,8 +25,7 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
   const esArchiver = getService('esArchiver');
   const utils = getService('securitySolutionUtils');
 
-  // FLAKY: https://github.com/elastic/kibana/issues/199363
-  describe.skip('Network Top N Flow', () => {
+  describe('Network Top N Flow', () => {
     let supertest: TestAgent;
     let search: SearchService;
     describe('With filebeat', () => {