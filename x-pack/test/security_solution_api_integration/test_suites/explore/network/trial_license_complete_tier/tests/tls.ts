3780eae8e1f737d1d2ab8b3cd2930a7246ed8949
Tiago Costa <tiago.costa@elastic.co>
2024-11-08 21:27:20 +0000

    skip flaky suite (#199379)

--- x-pack/test/security_solution_api_integration/test_suites/explore/network/trial_license_complete_tier/tests/tls.ts
+++ x-pack/test/security_solution_api_integration/test_suites/explore/network/trial_license_complete_tier/tests/tls.ts
@@ -91,7 +91,8 @@ export default function ({ getService }: FtrProviderContextWithSpaces) {
   describe('Tls Test with Packetbeat', () => {
     let supertest: TestAgent;
     let search: SearchService;
-    describe('Tls Test', () => {
+    // FLAKY: https://github.com/elastic/kibana/issues/199379
+    describe.skip('Tls Test', () => {
       before(async () => {
         supertest = await utils.createSuperTest();
         search = await utils.createSearch();
