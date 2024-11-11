57ed9d4e20a16d766ce1fc912f562be09a210910
pgayvallet <pierre.gayvallet@elastic.co>
2024-11-11 09:56:45 +0100

    nit

--- x-pack/plugins/observability_solution/observability_ai_assistant_app/server/functions/documentation.ts
+++ x-pack/plugins/observability_solution/observability_ai_assistant_app/server/functions/documentation.ts
@@ -20,7 +20,7 @@ export async function registerDocumentationFunction({
 
   functions.registerInstruction(({ availableFunctionNames }) => {
     return availableFunctionNames.includes(RETRIEVE_DOCUMENTATION_NAME)
-      ? `When asked questions about the Elastic stack, You should use the ${RETRIEVE_DOCUMENTATION_NAME} function before answering,
+      ? `When asked questions about the Elastic stack or products, You should use the ${RETRIEVE_DOCUMENTATION_NAME} function before answering,
       to retrieve documentation related to the question. Consider that the documentation returned by the function
       is always more up to date and accurate than any own internal knowledge you might have.`
       : undefined;
