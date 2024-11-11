e02560d003a96584d37e5e651beb6b5db0d9507f
Marta Bondyra <marta.bondyra@elastic.co>
2024-11-11 15:49:59 +0100

    simplifications of MappedColors

--- src/plugins/charts/public/services/mapped_colors/mapped_colors.test.ts
+++ src/plugins/charts/public/services/mapped_colors/mapped_colors.test.ts
@@ -11,66 +11,30 @@ import _ from 'lodash';
 import { MappedColors } from './mapped_colors';
 
 describe('Mapped Colors', () => {
-  const mappedColors = new MappedColors();
-
-  beforeEach(() => {
-    mappedColors.purge();
-  });
-
   it('should properly map keys to unique colors', () => {
+    const mappedColors = new MappedColors();
     const arr = [1, 2, 3, 4, 5];
     mappedColors.mapKeys(arr);
+
     expect(_(mappedColors.mapping).values().uniq().size()).toBe(arr.length);
+    expect(_.keys(mappedColors.mapping).length).toBe(5);
   });
 
-  it('should have a flush method that moves the current map to the old map', function () {
+  it('should allow to map keys multiple times and add new colors when doing so', function () {
+    const mappedColors = new MappedColors();
     const arr = [1, 2, 3, 4, 5];
     mappedColors.mapKeys(arr);
-    expect(_.keys(mappedColors.mapping).length).toBe(5);
-    expect(_.keys(mappedColors.oldMap).length).toBe(0);
+    mappedColors.mapKeys([6, 7]);
 
-    mappedColors.flush();
-
-    expect(_.keys(mappedColors.oldMap).length).toBe(5);
-    expect(_.keys(mappedColors.mapping).length).toBe(0);
-
-    mappedColors.flush();
-
-    expect(_.keys(mappedColors.oldMap).length).toBe(0);
-    expect(_.keys(mappedColors.mapping).length).toBe(0);
-  });
-
-  it('should use colors in the oldMap if they are available', function () {
-    const arr = [1, 2, 3, 4, 5];
-    mappedColors.mapKeys(arr);
-    expect(_.keys(mappedColors.mapping).length).toBe(5);
-    expect(_.keys(mappedColors.oldMap).length).toBe(0);
-
-    mappedColors.flush();
-
-    mappedColors.mapKeys([3, 4, 5]);
-    expect(_.keys(mappedColors.oldMap).length).toBe(5);
-    expect(_.keys(mappedColors.mapping).length).toBe(3);
-
-    expect(mappedColors.mapping[1]).toBe(undefined);
-    expect(mappedColors.mapping[2]).toBe(undefined);
-    expect(mappedColors.mapping[3]).toEqual(mappedColors.oldMap[3]);
-    expect(mappedColors.mapping[4]).toEqual(mappedColors.oldMap[4]);
-    expect(mappedColors.mapping[5]).toEqual(mappedColors.oldMap[5]);
-  });
-
-  it('should have a purge method that clears both maps', function () {
-    const arr = [1, 2, 3, 4, 5];
-    mappedColors.mapKeys(arr);
-    mappedColors.flush();
-    mappedColors.mapKeys(arr);
-
-    expect(_.keys(mappedColors.mapping).length).toBe(5);
-    expect(_.keys(mappedColors.oldMap).length).toBe(5);
-
-    mappedColors.purge();
-
-    expect(_.keys(mappedColors.mapping).length).toBe(0);
-    expect(_.keys(mappedColors.oldMap).length).toBe(0);
+    expect(_.keys(mappedColors.mapping).length).toBe(7);
+    expect(mappedColors.mapping).toEqual({
+      '1': '#00a69b',
+      '2': '#57c17b',
+      '3': '#6f87d8',
+      '4': '#663db8',
+      '5': '#bc52bc',
+      '6': '#9e3533',
+      '7': '#daa05d',
+    });
   });
 });
