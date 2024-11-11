553be3c58fa666464cd37c289842b605d956ed5d
José Luis González <joseluisgj@gmail.com>
2024-11-11 09:37:27 +0100

    Fixing FTR test

--- x-pack/test_serverless/functional/page_objects/svl_search_connectors_page.ts
+++ x-pack/test_serverless/functional/page_objects/svl_search_connectors_page.ts
@@ -53,9 +53,8 @@ export function SvlSearchConnectorsPageProvider({ getService }: FtrProviderConte
         await testSubjects.existOrFail('serverlessSearchEditConnectorType');
         await testSubjects.existOrFail('serverlessSearchEditConnectorTypeChoices');
         await testSubjects.click('serverlessSearchEditConnectorTypeChoices');
-        await testSubjects.exists('serverlessSearchConnectorServiceType-zoom');
+        await testSubjects.exists(`serverlessSearchConnectorServiceType-${type}`);
         await testSubjects.click(`serverlessSearchConnectorServiceType-${type}`);
-        await testSubjects.existOrFail('serverlessSearchConnectorServiceType-zoom');
       },
       async expectConnectorIdToMatchUrl(connectorId: string) {
         expect(await browser.getCurrentUrl()).contain(`/app/connectors/${connectorId}`);
