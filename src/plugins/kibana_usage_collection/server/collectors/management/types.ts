6e858339e16e42e91621947a6333d13083884fe4
Tomasz Kajtoch <tomasz.kajtoch@elastic.co>
2024-11-12 23:38:14 +0100

    add `theme:name` to stack telemetry collectors

--- src/plugins/kibana_usage_collection/server/collectors/management/types.ts
+++ src/plugins/kibana_usage_collection/server/collectors/management/types.ts
@@ -108,6 +108,7 @@ export interface UsageStats {
   'timepicker:quickRanges': string;
   'theme:version': string;
   'theme:darkMode': boolean;
+  'theme:name': string;
   'state:storeInSessionStorage': boolean;
   'savedObjects:perPage': number;
   'search:queryLanguage': string;
