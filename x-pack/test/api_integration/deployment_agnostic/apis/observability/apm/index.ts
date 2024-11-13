4c5908434ddbcc330ebe6eaeca6783aae6d70559
Irene Blanco <irene.blanco@elastic.co>
2024-11-13 17:50:15 +0100

    [APM] Migrate historical data API tests to be deployment-agnostic (#199765)
    ### How to test
    Closes https://github.com/elastic/kibana/issues/198975
    Part of https://github.com/elastic/kibana/issues/193245
    
    This PR contains the changes to migrate `historical_data` test folder to
    deployment-agnostic testing strategy.
    
    ### How to test
    
    - Serverless
    
    ```
    node scripts/functional_tests_server --config x-pack/test/api_integration/deployment_agnostic/configs/serverless/oblt.serverless.config.ts
    node scripts/functional_test_runner --config x-pack/test/api_integration/deployment_agnostic/configs/serverless/oblt.serverless.config.ts --grep="APM"
    ```
    
    It's recommended to be run against
    [MKI](https://github.com/crespocarlos/kibana/blob/main/x-pack/test_serverless/README.md#run-tests-on-mki)
    
    - Stateful
    ```
    node scripts/functional_tests_server --config x-pack/test/api_integration/deployment_agnostic/configs/stateful/oblt.stateful.config.ts
    node scripts/functional_test_runner --config x-pack/test/api_integration/deployment_agnostic/configs/stateful/oblt.stateful.config.ts --grep="APM"
    ```
    
    ## Checks
    
    - [x] (OPTIONAL, only if a test has been unskipped) Run flaky test suite
    - [x] local run for serverless
    - [x] local run for stateful
    - [x] MKI run for serverless
    
    (cherry picked from commit 55da11e9188ad1e68a8cc9b45013cafa6e2c3b33)

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/apm/index.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/apm/index.ts
@@ -21,6 +21,7 @@ export default function apmApiIntegrationTests({
     loadTestFile(require.resolve('./correlations'));
     loadTestFile(require.resolve('./entities'));
     loadTestFile(require.resolve('./cold_start'));
+    loadTestFile(require.resolve('./historical_data'));
     loadTestFile(require.resolve('./observability_overview'));
     loadTestFile(require.resolve('./latency'));
     loadTestFile(require.resolve('./infrastructure'));
