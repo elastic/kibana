b280d71a9d8a6398dd4cb3285fcb02889a43f7b5
lcawl <lcawley@elastic.co>
2024-11-13 18:00:08 -0800

    [OpenAPI][DOCS] Add descriptions for flapping rule settings

--- x-pack/plugins/alerting/common/routes/rule/common/flapping/schemas/v1.ts
+++ x-pack/plugins/alerting/common/routes/rule/common/flapping/schemas/v1.ts
@@ -17,13 +17,24 @@ import { validateFlapping as validateFlappingV1 } from '../../../validation/vali
 export const flappingSchema = schema.object(
   {
     look_back_window: schema.number({
+      meta: { description: 'The minimum number of runs in which the threshold must be met.' },
       min: MIN_LOOK_BACK_WINDOW_V1,
       max: MAX_LOOK_BACK_WINDOW_V1,
     }),
     status_change_threshold: schema.number({
+      meta: {
+        description:
+          'The minimum number of times an alert must switch states in the look back window.',
+      },
       min: MIN_STATUS_CHANGE_THRESHOLD_V1,
       max: MAX_STATUS_CHANGE_THRESHOLD_V1,
     }),
   },
-  { validate: validateFlappingV1 }
+  {
+    validate: validateFlappingV1,
+    meta: {
+      description:
+        'When flapping detection is turned on, alerts that switch quickly between active and recovered states are identified as “flapping” and notifications are reduced.',
+    },
+  }
 );
