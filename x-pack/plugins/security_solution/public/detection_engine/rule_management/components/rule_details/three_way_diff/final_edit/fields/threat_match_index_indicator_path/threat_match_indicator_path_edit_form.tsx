7fbea7d4b13f62ed93f3751d8f450dcd36b9afb0
Maxim Palenov <maxim.palenov@elastic.co>
2024-11-15 21:25:06 +0100

    add Threat Match Indicator Path editable component

---
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_edit/fields/threat_match_index_indicator_path/threat_match_indicator_path_edit_form.tsx
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { RuleFieldEditFormWrapper } from '../rule_field_edit_form_wrapper';
+import { ThreatMatchIndicatorPathEditAdapter } from './threat_match_indicator_path_edit_adapter';
+
+export function ThreatMatchIndicatorPathEditForm(): JSX.Element {
+  return (
+    <RuleFieldEditFormWrapper
+      component={ThreatMatchIndicatorPathEditAdapter}
+      ruleFieldFormSchema={schema}
+    />
+  );
+}
+
+const schema = {};
