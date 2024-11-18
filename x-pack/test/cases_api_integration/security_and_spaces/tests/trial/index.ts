b6ef009cb86120d86cb9bc4fc1c1a591e6eb0be8
lgestc <lukasz.gmys@elastic.co>
2024-11-18 13:16:31 +0100

    move observables test

--- x-pack/test/cases_api_integration/security_and_spaces/tests/trial/index.ts
+++ x-pack/test/cases_api_integration/security_and_spaces/tests/trial/index.ts
@@ -38,7 +38,7 @@ export default ({ loadTestFile, getService }: FtrProviderContext): void => {
     loadTestFile(require.resolve('./delete_sub_privilege'));
     loadTestFile(require.resolve('./user_profiles/get_current'));
     // case observables are only available with a license above basic
-    loadTestFile(require.resolve('./cases/observables'));
+    loadTestFile(require.resolve('./internal/observables'));
 
     // Internal routes
     loadTestFile(require.resolve('./internal/get_user_action_stats'));
