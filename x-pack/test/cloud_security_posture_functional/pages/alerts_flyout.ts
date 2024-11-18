519d46209b6ccb09ff853b700e8064d6b6371c3e
Kfir Peled <61654899+kfirpeled@users.noreply.github.com>
2024-11-18 13:58:38 +0000

    [8.x] [Cloud Security] Fixed failing FTR (#199683) (#200530)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[Cloud Security] Fixed failing FTR
    (#199683)](https://github.com/elastic/kibana/pull/199683)
    
    <!--- Backport version: 8.9.8 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Kfir
    Peled","email":"61654899+kfirpeled@users.noreply.github.com"},"sourceCommit":{"committedDate":"2024-11-13T01:43:58Z","message":"[Cloud
    Security] Fixed failing FTR (#199683)\n\n## Summary\r\n\r\nfixes:
    https://github.com/elastic/kibana/issues/198632\r\n\r\nFTR failed due to
    the usage of relative start and end when querying for\r\nthe
    graph.\r\n\r\nTogether with @tinnytintin10 we decided it is safe to
    assume the time to\r\nquery would be 30 minutes before and after the
    alert.\r\n\r\n**Some enhancements and fixes:**\r\n- Disabled re-fetch
    /graph API when window focus returned\r\n\r\n### Checklist\r\n\r\nDelete
    any items that are not applicable to this PR.\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common scenarios\r\n- [ ] [Flaky
    Test\r\nRunner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1)
    was\r\nused on any tests
    changed","sha":"5add2c82db7d26b96b66e078606fbdc428fefc28","branchLabelMapping":{"^v9.0.0$":"main","^v8.17.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","backport
    missing","v9.0.0","backport:prev-minor"],"number":199683,"url":"https://github.com/elastic/kibana/pull/199683","mergeCommit":{"message":"[Cloud
    Security] Fixed failing FTR (#199683)\n\n## Summary\r\n\r\nfixes:
    https://github.com/elastic/kibana/issues/198632\r\n\r\nFTR failed due to
    the usage of relative start and end when querying for\r\nthe
    graph.\r\n\r\nTogether with @tinnytintin10 we decided it is safe to
    assume the time to\r\nquery would be 30 minutes before and after the
    alert.\r\n\r\n**Some enhancements and fixes:**\r\n- Disabled re-fetch
    /graph API when window focus returned\r\n\r\n### Checklist\r\n\r\nDelete
    any items that are not applicable to this PR.\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common scenarios\r\n- [ ] [Flaky
    Test\r\nRunner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1)
    was\r\nused on any tests
    changed","sha":"5add2c82db7d26b96b66e078606fbdc428fefc28"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","labelRegex":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/199683","number":199683,"mergeCommit":{"message":"[Cloud
    Security] Fixed failing FTR (#199683)\n\n## Summary\r\n\r\nfixes:
    https://github.com/elastic/kibana/issues/198632\r\n\r\nFTR failed due to
    the usage of relative start and end when querying for\r\nthe
    graph.\r\n\r\nTogether with @tinnytintin10 we decided it is safe to
    assume the time to\r\nquery would be 30 minutes before and after the
    alert.\r\n\r\n**Some enhancements and fixes:**\r\n- Disabled re-fetch
    /graph API when window focus returned\r\n\r\n### Checklist\r\n\r\nDelete
    any items that are not applicable to this PR.\r\n\r\n- [x] [Unit or
    functional\r\ntests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)\r\nwere
    updated or added to match the most common scenarios\r\n- [ ] [Flaky
    Test\r\nRunner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1)
    was\r\nused on any tests
    changed","sha":"5add2c82db7d26b96b66e078606fbdc428fefc28"}}]}]
    BACKPORT-->

--- x-pack/test/cloud_security_posture_functional/pages/alerts_flyout.ts
+++ x-pack/test/cloud_security_posture_functional/pages/alerts_flyout.ts
@@ -17,9 +17,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   const pageObjects = getPageObjects(['common', 'header', 'alerts']);
   const alertsPage = pageObjects.alerts;
 
-  // Failing: See https://github.com/elastic/kibana/issues/198632
-  // Failing: See https://github.com/elastic/kibana/issues/198632
-  describe.skip('Security Alerts Page - Graph visualization', function () {
+  describe('Security Alerts Page - Graph visualization', function () {
     this.tags(['cloud_security_posture_graph_viz']);
 
     before(async () => {
