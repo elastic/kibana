94ef46fc1f96a1112a44069bb3c66dc44f11c328
Milton Hultgren <milton.hultgren@elastic.co>
2024-11-19 22:23:52 +0100

    [EEM] Add built in definitions for core Kubernetes entities (#196916)
    ## 🍒 Summary
    
    This PR adds the OTEL and ECS entity definition for Kubernetes. This
    covers the following datasets:
    - Cluster
    - Service (ECS Only)
    - Pod
    - ReplicaSet
    - Deployment
    - Statefulset
    - DaemonSet
    - Job
    - CronJob
    - Node
    
    This PR does not include Container per @roshan-elastic
    
    ### ✅ TODO
    - [X] Use correct index pattern for SemConv data
    (`metrics-k8sclusterreceiver.otel-default`,
    `metrics-kubeletstatsreceiver.otel-default`)
    Use global IDs instead of local IDs
    - [X] Add minimal list of labels to track beyond what was already added
    (wildcards are not supported, example `container.image.name` for
    containers to allow to find all "redis" containers)
    - [ ] Test with ECS data, SemConv data and mixed data (to check if we
    get duplicates, with the container definition for example).
    
    ### 🐴 Follow up EEM features
    https://github.com/elastic/elastic-entity-model/issues/170 (Add
    dedicated aggregation for display name and use that instead to provide a
    better label than the global ID)
    https://github.com/elastic/elastic-entity-model/issues/193 (Add entity
    type display label to allow UI to not hard code a user friendly label)
    
    ---------
    
    Co-authored-by: Chris Cowan <chris@elastic.co>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    (cherry picked from commit 080d0ff97f00bf564dedfd8fd37cdac0370e1349)

---
+++ x-pack/plugins/entity_manager/server/lib/entities/built_in/kubernetes/ecs/node.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EntityDefinition, entityDefinitionSchema } from '@kbn/entities-schema';
+import { BUILT_IN_ID_PREFIX } from '../../constants';
+import { commonEcsIndexPatterns } from '../common/ecs_index_patterns';
+import { commonEcsMetadata } from '../common/ecs_metadata';
+
+export const builtInKubernetesNodeEcsEntityDefinition: EntityDefinition =
+  entityDefinitionSchema.parse({
+    id: `${BUILT_IN_ID_PREFIX}kubernetes_node_ecs`,
+    filer: 'kubernetes.node.uid : *',
+    managed: true,
+    version: '0.1.0',
+    name: 'Kubernetes Node from ECS data',
+    description:
+      'This definition extracts Kubernetes node entities from the Kubernetes integration data streams',
+    type: 'k8s.node.ecs',
+    indexPatterns: commonEcsIndexPatterns,
+    identityFields: ['kubernetes.node.uid'],
+    displayNameTemplate: '{{kubernetes.node.name}}',
+    latest: {
+      timestampField: '@timestamp',
+      lookbackPeriod: '10m',
+      settings: {
+        frequency: '5m',
+      },
+    },
+    metadata: commonEcsMetadata,
+  });
