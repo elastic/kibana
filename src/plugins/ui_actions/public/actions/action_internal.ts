052644d375c0771738db06ecc7d2636b7dbe7340
Catherine Liu <catherine.liu@elastic.co>
2024-11-19 11:16:46 -0800

    Only log error once

--- src/plugins/ui_actions/public/actions/action_internal.ts
+++ src/plugins/ui_actions/public/actions/action_internal.ts
@@ -27,6 +27,7 @@ export class ActionInternal<Context extends object = object>
 
   public readonly subscribeToCompatibilityChanges?: Action<Context>['subscribeToCompatibilityChanges'];
   public readonly couldBecomeCompatible?: Action<Context>['couldBecomeCompatible'];
+  public errorLogged?: boolean;
 
   constructor(public readonly definition: ActionDefinition<Context>) {
     this.id = this.definition.id;
@@ -36,6 +37,7 @@ export class ActionInternal<Context extends object = object>
     this.grouping = this.definition.grouping;
     this.showNotification = this.definition.showNotification;
     this.disabled = this.definition.disabled;
+    this.errorLogged = false;
 
     if (this.definition.subscribeToCompatibilityChanges) {
       this.subscribeToCompatibilityChanges = definition.subscribeToCompatibilityChanges;
@@ -69,8 +71,11 @@ export class ActionInternal<Context extends object = object>
     try {
       return await this.definition.isCompatible(context);
     } catch (e) {
-      // eslint-disable-next-line no-console
-      console.error(e);
+      if (!this.errorLogged) {
+        // eslint-disable-next-line no-console
+        console.error(e);
+        this.errorLogged = true;
+      }
       return false;
     }
   }
