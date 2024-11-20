32dc285c039c71d88c772239d7e161726aab1a1d
Julia Bardi <julia.bardi@elastic.co>
2024-11-20 13:49:49 +0100

    fix test

--- x-pack/plugins/fleet/common/types/models/package_policy.ts
+++ x-pack/plugins/fleet/common/types/models/package_policy.ts
@@ -112,7 +112,6 @@ export interface PackagePolicy extends Omit<NewPackagePolicy, 'inputs'> {
   updated_by: string;
   created_at: string;
   created_by: string;
-  // bump_agent_policy_revision?: boolean;
 }
 
 export type DryRunPackagePolicy = NewPackagePolicy & {
--- x-pack/plugins/fleet/server/services/agent_policies/bump_agent_policies_task.test.ts
+++ x-pack/plugins/fleet/server/services/agent_policies/bump_agent_policies_task.test.ts
@@ -7,8 +7,10 @@
 
 import { loggingSystemMock } from '@kbn/core/server/mocks';
 
-import { packagePolicyService } from '../package_policy';
-import type { PackagePolicy } from '../../types';
+import { agentPolicyService } from '../agent_policy';
+
+import { appContextService } from '..';
+import { getPackagePolicySavedObjectType } from '../package_policy';
 
 import { _updatePackagePoliciesThatNeedBump } from './bump_agent_policies_task';
 
@@ -16,27 +18,50 @@ jest.mock('../app_context');
 jest.mock('../agent_policy');
 jest.mock('../package_policy');
 
-const mockedPackagePolicyService = jest.mocked(packagePolicyService);
+const mockedAgentPolicyService = jest.mocked(agentPolicyService);
+const mockedAppContextService = jest.mocked(appContextService);
+const mockSoClient = {
+  find: jest.fn(),
+  bulkUpdate: jest.fn(),
+} as any;
+const mockGetPackagePolicySavedObjectType = jest.mocked(getPackagePolicySavedObjectType);
 
 describe('_updatePackagePoliciesThatNeedBump', () => {
   beforeEach(() => {
     jest.clearAllMocks();
-    mockedPackagePolicyService.list.mockResolvedValueOnce({
-      total: 1,
-      items: [
+    mockSoClient.find.mockResolvedValue({
+      total: 3,
+      saved_objects: [
         {
           id: 'packagePolicy1',
-        } as PackagePolicy,
+          namespaces: ['default'],
+          attributes: {
+            policy_ids: ['policy1'],
+          },
+        },
+        {
+          id: 'packagePolicy2',
+          namespaces: ['space'],
+          attributes: {
+            policy_ids: ['policy2'],
+          },
+        },
+        {
+          id: 'packagePolicy3',
+          namespaces: ['space'],
+          attributes: {
+            policy_ids: ['policy3'],
+          },
+        },
       ],
       page: 1,
       perPage: 100,
     });
-    mockedPackagePolicyService.list.mockResolvedValueOnce({
-      total: 0,
-      items: [],
-      page: 1,
-      perPage: 100,
-    });
+    mockedAppContextService.getInternalUserSOClientWithoutSpaceExtension.mockReturnValue(
+      mockSoClient
+    );
+    mockedAppContextService.getInternalUserSOClientForSpaceId.mockReturnValue(mockSoClient);
+    mockGetPackagePolicySavedObjectType.mockResolvedValue('fleet-package-policies');
   });
 
   it('should update package policy if bump agent policy revision needed', async () => {
@@ -44,8 +69,40 @@ describe('_updatePackagePoliciesThatNeedBump', () => {
 
     await _updatePackagePoliciesThatNeedBump(logger, false);
 
-    expect(mockedPackagePolicyService.bulkUpdate).toHaveBeenCalledWith(undefined, undefined, [
-      { bump_agent_policy_revision: false, id: 'packagePolicy1' },
+    expect(mockSoClient.bulkUpdate).toHaveBeenCalledWith([
+      {
+        attributes: { bump_agent_policy_revision: false },
+        id: 'packagePolicy1',
+        type: 'fleet-package-policies',
+      },
     ]);
+    expect(mockSoClient.bulkUpdate).toHaveBeenCalledWith([
+      {
+        attributes: { bump_agent_policy_revision: false },
+        id: 'packagePolicy2',
+        type: 'fleet-package-policies',
+      },
+      {
+        attributes: { bump_agent_policy_revision: false },
+        id: 'packagePolicy3',
+        type: 'fleet-package-policies',
+      },
+    ]);
+
+    expect(mockedAgentPolicyService.bumpRevision).toHaveBeenCalledWith(
+      expect.anything(),
+      undefined,
+      'policy1'
+    );
+    expect(mockedAgentPolicyService.bumpRevision).toHaveBeenCalledWith(
+      expect.anything(),
+      undefined,
+      'policy2'
+    );
+    expect(mockedAgentPolicyService.bumpRevision).toHaveBeenCalledWith(
+      expect.anything(),
+      undefined,
+      'policy3'
+    );
   });
 });
