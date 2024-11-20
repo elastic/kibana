f7e280d564103b95a2c749b084a233f8b55fb416
Sid <siddharthmantri1@gmail.com>
2024-11-20 16:06:41 +0100

    add empty line

--- x-pack/test/api_integration/apis/security/query_api_keys.ts
+++ x-pack/test/api_integration/apis/security/query_api_keys.ts
@@ -115,8 +115,9 @@ export default function ({ getService }: FtrProviderContext) {
       }
     });
 
-    it.only('should paginate keys', async () => {
+    it('should paginate keys', async () => {
       await createKey('first-api-key', 'rest', false);
+
       await createKey('second-api-key', 'rest', false);
       const { body: keys } = await supertest
         .post('/internal/security/api_key/_query')
