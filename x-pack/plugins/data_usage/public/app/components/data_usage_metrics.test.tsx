c969193acf2506876e4ae598e8f21630d239df74
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-11-21 15:47:21 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/data_usage/public/app/components/data_usage_metrics.test.tsx
+++ x-pack/plugins/data_usage/public/app/components/data_usage_metrics.test.tsx
@@ -6,7 +6,7 @@
  */
 import React from 'react';
 import { TestProvider } from '../../../common/test_utils';
-import { render, screen, waitFor, within, type RenderResult } from '@testing-library/react';
+import { render, waitFor, within, type RenderResult } from '@testing-library/react';
 import userEvent, { type UserEvent } from '@testing-library/user-event';
 import { DataUsageMetrics } from './data_usage_metrics';
 import { useGetDataUsageMetrics } from '../../hooks/use_get_usage_metrics';
