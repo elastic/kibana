elastic/kibana

commit 96c9b5b5d0a82c6e4b0a9b86fc1ab4dd9fa4d707

[Synthetics] Handle private locations simultaneous edits !! (#195874)
## Summary

Fixes https://github.com/elastic/kibana/issues/190801 !!

Handle private locations simultaneous edits !! 

Registered a new saved object to handle private locations properly.
Instead of using a singleton, now each private location will be
represented by it's own saved object.

### Existing private locations
When we are doing any write operation, we migrate them to new kind of
saved object and remove the legacy saved object type.


### Testing

- Create multiple private locations on main 
- Switch to branch and create few more locations
- Make sure all private locations are editable, deleteable and have been
migrated to new saved object types in the course

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/plugins/observability_solution/synthetics/server/feature.ts
@@ -14,7 +14,10 @@ import {
 import { KibanaFeatureScope } from '@kbn/features-plugin/common';
 import { syntheticsMonitorType, syntheticsParamType } from '../common/types/saved_objects';
 import { SYNTHETICS_RULE_TYPES } from '../common/constants/synthetics_alerts';
-import { privateLocationsSavedObjectName } from '../common/saved_objects/private_locations';
+import {
+  legacyPrivateLocationsSavedObjectName,
+  privateLocationSavedObjectName,
+} from '../common/saved_objects/private_locations';
 import { PLUGIN } from '../common/constants/plugin';
 import {
   syntheticsSettingsObjectType,
@@ -71,7 +74,8 @@ export const syntheticsFeature = {
           syntheticsSettingsObjectType,
           syntheticsMonitorType,
           syntheticsApiKeyObjectType,
-          privateLocationsSavedObjectName,
+          privateLocationSavedObjectName,
+          legacyPrivateLocationsSavedObjectName,
           syntheticsParamType,
           // uptime settings object is also registered here since feature is shared between synthetics and uptime
           uptimeSettingsObjectType,
@@ -102,7 +106,7 @@ export const syntheticsFeature = {
           syntheticsSettingsObjectType,
           syntheticsMonitorType,
           syntheticsApiKeyObjectType,
-          privateLocationsSavedObjectName,
+          legacyPrivateLocationsSavedObjectName,
           // uptime settings object is also registered here since feature is shared between synthetics and uptime
           uptimeSettingsObjectType,
         ],

x-pack/test_serverless/api_integration/test_suites/observability/platform_security/authorization.ts
@@ -8728,6 +8728,18 @@ export default function ({ getService }: FtrProviderContext) {
                 "saved_object:uptime-synthetics-api-key/delete",
                 "saved_object:uptime-synthetics-api-key/bulk_delete",
                 "saved_object:uptime-synthetics-api-key/share_to_space",
+                "saved_object:synthetics-private-location/bulk_get",
+                "saved_object:synthetics-private-location/get",
+                "saved_object:synthetics-private-location/find",
+                "saved_object:synthetics-private-location/open_point_in_time",
+                "saved_object:synthetics-private-location/close_point_in_time",
+                "saved_object:synthetics-private-location/create",
+                "saved_object:synthetics-private-location/bulk_create",
+                "saved_object:synthetics-private-location/update",
+                "saved_object:synthetics-private-location/bulk_update",
+                "saved_object:synthetics-private-location/delete",
+                "saved_object:synthetics-private-location/bulk_delete",
+                "saved_object:synthetics-private-location/share_to_space",
                 "saved_object:synthetics-privates-locations/bulk_get",
                 "saved_object:synthetics-privates-locations/get",
                 "saved_object:synthetics-privates-locations/find",
@@ -9417,6 +9429,18 @@ export default function ({ getService }: FtrProviderContext) {
                 "saved_object:uptime-synthetics-api-key/delete",
                 "saved_object:uptime-synthetics-api-key/bulk_delete",
                 "saved_object:uptime-synthetics-api-key/share_to_space",
+                "saved_object:synthetics-private-location/bulk_get",
+                "saved_object:synthetics-private-location/get",
+                "saved_object:synthetics-private-location/find",
+                "saved_object:synthetics-private-location/open_point_in_time",
+                "saved_object:synthetics-private-location/close_point_in_time",
+                "saved_object:synthetics-private-location/create",
+                "saved_object:synthetics-private-location/bulk_create",
+                "saved_object:synthetics-private-location/update",
+                "saved_object:synthetics-private-location/bulk_update",
+                "saved_object:synthetics-private-location/delete",
+                "saved_object:synthetics-private-location/bulk_delete",
+                "saved_object:synthetics-private-location/share_to_space",
                 "saved_object:synthetics-privates-locations/bulk_get",
                 "saved_object:synthetics-privates-locations/get",
                 "saved_object:synthetics-privates-locations/find",

packages/kbn-test/src/kbn_client/kbn_client_saved_objects.ts
@@ -109,6 +109,7 @@ const STANDARD_LIST_TYPES = [
   'synthetics-monitor',
   'uptime-dynamic-settings',
   'synthetics-privates-locations',
+  'synthetics-private-location',
 
   'osquery-saved-query',
   'osquery-pack',

src/core/server/integration_tests/ci_checks/saved_objects/check_registered_types.test.ts
@@ -165,6 +165,7 @@ describe('checking migration metadata changes on all registered SO types', () =>
         "synthetics-dynamic-settings": "4b40a93eb3e222619bf4e7fe34a9b9e7ab91a0a7",
         "synthetics-monitor": "5ceb25b6249bd26902c9b34273c71c3dce06dbea",
         "synthetics-param": "3ebb744e5571de678b1312d5c418c8188002cf5e",
+        "synthetics-private-location": "8cecc9e4f39637d2f8244eb7985c0690ceab24be",
         "synthetics-privates-locations": "f53d799d5c9bc8454aaa32c6abc99a899b025d5c",
         "tag": "e2544392fe6563e215bb677abc8b01c2601ef2dc",
         "task": "3c89a7c918d5b896a5f8800f06e9114ad7e7aea3",

src/core/server/integration_tests/saved_objects/migrations/group3/type_registrations.test.ts
@@ -139,6 +139,7 @@ const previouslyRegisteredTypes = [
   'synthetics-monitor',
   'synthetics-param',
   'synthetics-privates-locations',
+  'synthetics-private-location',
   'tag',
   'task',
   'telemetry',

x-pack/plugins/observability_solution/synthetics/common/saved_objects/private_locations.ts
@@ -5,5 +5,7 @@
  * 2.0.
  */
 
-export const privateLocationsSavedObjectId = 'synthetics-privates-locations-singleton';
-export const privateLocationsSavedObjectName = 'synthetics-privates-locations';
+export const legacyPrivateLocationsSavedObjectId = 'synthetics-privates-locations-singleton';
+export const legacyPrivateLocationsSavedObjectName = 'synthetics-privates-locations';
+
+export const privateLocationSavedObjectName = 'synthetics-private-location';

x-pack/plugins/observability_solution/synthetics/e2e/synthetics/journeys/private_locations.journey.ts
@@ -7,17 +7,14 @@
 
 import { journey, step, before, after, expect } from '@elastic/synthetics';
 import { waitForLoadingToFinish } from '@kbn/ux-plugin/e2e/journeys/utils';
+import { SyntheticsServices } from './services/synthetics_services';
 import { byTestId } from '../../helpers/utils';
-import {
-  addTestMonitor,
-  cleanPrivateLocations,
-  cleanTestMonitors,
-  getPrivateLocations,
-} from './services/add_monitor';
+import { addTestMonitor, cleanPrivateLocations, cleanTestMonitors } from './services/add_monitor';
 import { syntheticsAppPageProvider } from '../page_objects/synthetics_app';
 
 journey(`PrivateLocationsSettings`, async ({ page, params }) => {
   const syntheticsApp = syntheticsAppPageProvider({ page, kibanaUrl: params.kibanaUrl, params });
+  const services = new SyntheticsServices(params);
 
   page.setDefaultTimeout(2 * 30000);
 
@@ -78,16 +75,14 @@ journey(`PrivateLocationsSettings`, async ({ page, params }) => {
     await page.click('text=Private Locations');
     await page.click('h1:has-text("Settings")');
 
-    const privateLocations = await getPrivateLocations(params);
+    const privateLocations = await services.getPrivateLocations();
 
-    const locations = privateLocations.attributes.locations;
+    expect(privateLocations.length).toBe(1);
 
-    expect(locations.length).toBe(1);
-
-    locationId = locations[0].id;
+    locationId = privateLocations[0].id;
 
     await addTestMonitor(params.kibanaUrl, 'test-monitor', {
-      locations: [locations[0]],
+      locations: [privateLocations[0]],
       type: 'browser',
     });
   });

x-pack/plugins/observability_solution/synthetics/e2e/synthetics/journeys/services/add_monitor.ts
@@ -7,10 +7,7 @@
 
 import axios from 'axios';
 import { SYNTHETICS_API_URLS } from '@kbn/synthetics-plugin/common/constants';
-import {
-  privateLocationsSavedObjectId,
-  privateLocationsSavedObjectName,
-} from '@kbn/synthetics-plugin/common/saved_objects/private_locations';
+import { legacyPrivateLocationsSavedObjectName } from '@kbn/synthetics-plugin/common/saved_objects/private_locations';
 
 export const enableMonitorManagedViaApi = async (kibanaUrl: string) => {
   try {
@@ -46,21 +43,6 @@ export const addTestMonitor = async (
   }
 };
 
-export const getPrivateLocations = async (params: Record<string, any>) => {
-  const getService = params.getService;
-  const server = getService('kibanaServer');
-
-  try {
-    return await server.savedObjects.get({
-      id: privateLocationsSavedObjectId,
-      type: privateLocationsSavedObjectName,
-    });
-  } catch (e) {
-    // eslint-disable-next-line no-console
-    console.log(e);
-  }
-};
-
 export const cleanTestMonitors = async (params: Record<string, any>) => {
   const getService = params.getService;
   const server = getService('kibanaServer');
@@ -79,7 +61,11 @@ export const cleanPrivateLocations = async (params: Record<string, any>) => {
 
   try {
     await server.savedObjects.clean({
-      types: [privateLocationsSavedObjectName, 'ingest-agent-policies', 'ingest-package-policies'],
+      types: [
+        legacyPrivateLocationsSavedObjectName,
+        'ingest-agent-policies',
+        'ingest-package-policies',
+      ],
     });
   } catch (e) {
     // eslint-disable-next-line no-console

commit 5d4282e91621b433e2bdf7201f9bedf47661d152

[Synthetics] Add new object into read synthetics feature !! (#201170)
## Summary

This is a regression from the PR
https://github.com/elastic/kibana/pull/195874, where we added the new
type into all but didn't do it for read.

x-pack/plugins/observability_solution/synthetics/server/feature.ts
@@ -106,6 +106,7 @@ export const syntheticsFeature = {
           syntheticsSettingsObjectType,
           syntheticsMonitorType,
           syntheticsApiKeyObjectType,
+          privateLocationSavedObjectName,
           legacyPrivateLocationsSavedObjectName,
           // uptime settings object is also registered here since feature is shared between synthetics and uptime
           uptimeSettingsObjectType,

x-pack/test_serverless/api_integration/test_suites/observability/platform_security/authorization.ts
@@ -10108,6 +10108,11 @@ export default function ({ getService }: FtrProviderContext) {
                 "saved_object:uptime-synthetics-api-key/find",
                 "saved_object:uptime-synthetics-api-key/open_point_in_time",
                 "saved_object:uptime-synthetics-api-key/close_point_in_time",
+                "saved_object:synthetics-private-location/bulk_get",
+                "saved_object:synthetics-private-location/get",
+                "saved_object:synthetics-private-location/find",
+                "saved_object:synthetics-private-location/open_point_in_time",
+                "saved_object:synthetics-private-location/close_point_in_time",
                 "saved_object:synthetics-privates-locations/bulk_get",
                 "saved_object:synthetics-privates-locations/get",
                 "saved_object:synthetics-privates-locations/find",
@@ -10399,6 +10404,11 @@ export default function ({ getService }: FtrProviderContext) {
                 "saved_object:uptime-synthetics-api-key/find",
                 "saved_object:uptime-synthetics-api-key/open_point_in_time",
                 "saved_object:uptime-synthetics-api-key/close_point_in_time",
+                "saved_object:synthetics-private-location/bulk_get",
+                "saved_object:synthetics-private-location/get",
+                "saved_object:synthetics-private-location/find",
+                "saved_object:synthetics-private-location/open_point_in_time",
+                "saved_object:synthetics-private-location/close_point_in_time",
                 "saved_object:synthetics-privates-locations/bulk_get",
                 "saved_object:synthetics-privates-locations/get",
                 "saved_object:synthetics-privates-locations/find",