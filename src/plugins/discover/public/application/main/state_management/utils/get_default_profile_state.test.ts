bef877d0dc3e2c4c2ae1d5484a65735349404b01
Davis McPhee <davis.mcphee@elastic.co>
2024-11-20 21:31:32 -0400

    Fix Jest tests

--- src/plugins/discover/public/application/main/state_management/utils/get_default_profile_state.test.ts
+++ src/plugins/discover/public/application/main/state_management/utils/get_default_profile_state.test.ts
@@ -7,122 +7,134 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-describe('getDefaultProfileState', () => {
-  it('temp', () => {});
+import { fieldList } from '@kbn/data-views-plugin/common';
+import { buildDataViewMock } from '@kbn/discover-utils/src/__mocks__';
+import { createContextAwarenessMocks } from '../../../../context_awareness/__mocks__';
+import { dataViewWithTimefieldMock } from '../../../../__mocks__/data_view_with_timefield';
+import { getDefaultProfileState } from './get_default_profile_state';
+
+const emptyDataView = buildDataViewMock({
+  name: 'emptyDataView',
+  fields: fieldList(),
 });
+const { profilesManagerMock } = createContextAwarenessMocks();
 
-// import { fieldList } from '@kbn/data-views-plugin/common';
-// import { buildDataViewMock } from '@kbn/discover-utils/src/__mocks__';
-// import { createContextAwarenessMocks } from '../../../../context_awareness/__mocks__';
-// import { dataViewWithTimefieldMock } from '../../../../__mocks__/data_view_with_timefield';
-// import { getDefaultProfileState } from './get_default_profile_state';
+profilesManagerMock.resolveDataSourceProfile({});
 
-// const emptyDataView = buildDataViewMock({
-//   name: 'emptyDataView',
-//   fields: fieldList(),
-// });
-// const { profilesManagerMock } = createContextAwarenessMocks();
+describe('getDefaultProfileState', () => {
+  describe('getPreFetchState', () => {
+    it('should return expected breakdownField', () => {
+      let appState = getDefaultProfileState({
+        profilesManager: profilesManagerMock,
+        resetDefaultProfileState: {
+          columns: false,
+          rowHeight: false,
+          breakdownField: true,
+        },
+        dataView: dataViewWithTimefieldMock,
+      }).getPreFetchState();
+      expect(appState).toEqual({
+        breakdownField: 'extension',
+      });
+      appState = getDefaultProfileState({
+        profilesManager: profilesManagerMock,
+        resetDefaultProfileState: {
+          columns: false,
+          rowHeight: false,
+          breakdownField: true,
+        },
+        dataView: emptyDataView,
+      }).getPreFetchState();
+      expect(appState).toEqual(undefined);
+    });
+  });
 
-// profilesManagerMock.resolveDataSourceProfile({});
+  describe('getPostFetchState', () => {
+    it('should return expected columns', () => {
+      let appState = getDefaultProfileState({
+        profilesManager: profilesManagerMock,
+        resetDefaultProfileState: {
+          columns: true,
+          rowHeight: false,
+          breakdownField: false,
+        },
+        dataView: dataViewWithTimefieldMock,
+      }).getPostFetchState({
+        defaultColumns: ['messsage', 'bytes'],
+        esqlQueryColumns: undefined,
+      });
+      expect(appState).toEqual({
+        columns: ['message', 'extension', 'bytes'],
+        grid: {
+          columns: {
+            extension: {
+              width: 200,
+            },
+            message: {
+              width: 100,
+            },
+          },
+        },
+      });
+      appState = getDefaultProfileState({
+        profilesManager: profilesManagerMock,
+        resetDefaultProfileState: {
+          columns: true,
+          rowHeight: false,
+          breakdownField: false,
+        },
+        dataView: emptyDataView,
+      }).getPostFetchState({
+        defaultColumns: ['messsage', 'bytes'],
+        esqlQueryColumns: [
+          { id: '1', name: 'foo', meta: { type: 'string' } },
+          { id: '2', name: 'bar', meta: { type: 'string' } },
+        ],
+      });
+      expect(appState).toEqual({
+        columns: ['foo', 'bar'],
+        grid: {
+          columns: {
+            foo: {
+              width: 300,
+            },
+          },
+        },
+      });
+    });
 
-// describe('getDefaultProfileState', () => {
-//   it('should return expected columns', () => {
-//     let appState = getDefaultProfileState({
-//       profilesManager: profilesManagerMock,
-//       resetDefaultProfileState: {
-//         columns: true,
-//         rowHeight: false,
-//         breakdownField: false,
-//       },
-//       defaultColumns: ['messsage', 'bytes'],
-//       dataView: dataViewWithTimefieldMock,
-//       esqlQueryColumns: undefined,
-//     });
-//     expect(appState).toEqual({
-//       columns: ['message', 'extension', 'bytes'],
-//       grid: {
-//         columns: {
-//           extension: {
-//             width: 200,
-//           },
-//           message: {
-//             width: 100,
-//           },
-//         },
-//       },
-//     });
-//     appState = getDefaultProfileState({
-//       profilesManager: profilesManagerMock,
-//       resetDefaultProfileState: {
-//         columns: true,
-//         rowHeight: false,
-//         breakdownField: false,
-//       },
-//       defaultColumns: ['messsage', 'bytes'],
-//       dataView: emptyDataView,
-//       esqlQueryColumns: [
-//         { id: '1', name: 'foo', meta: { type: 'string' } },
-//         { id: '2', name: 'bar', meta: { type: 'string' } },
-//       ],
-//     });
-//     expect(appState).toEqual({
-//       columns: ['foo', 'bar'],
-//       grid: {
-//         columns: {
-//           foo: {
-//             width: 300,
-//           },
-//         },
-//       },
-//     });
-//   });
+    it('should return expected rowHeight', () => {
+      const appState = getDefaultProfileState({
+        profilesManager: profilesManagerMock,
+        resetDefaultProfileState: {
+          columns: false,
+          rowHeight: true,
+          breakdownField: false,
+        },
+        dataView: dataViewWithTimefieldMock,
+      }).getPostFetchState({
+        defaultColumns: [],
+        esqlQueryColumns: undefined,
+      });
+      expect(appState).toEqual({
+        rowHeight: 3,
+      });
+    });
 
-//   it('should return expected rowHeight', () => {
-//     const appState = getDefaultProfileState({
-//       profilesManager: profilesManagerMock,
-//       resetDefaultProfileState: {
-//         columns: false,
-//         rowHeight: true,
-//         breakdownField: false,
-//       },
-//       defaultColumns: [],
-//       dataView: dataViewWithTimefieldMock,
-//       esqlQueryColumns: undefined,
-//     });
-//     expect(appState).toEqual({
-//       rowHeight: 3,
-//     });
-//   });
-
-//   it('should return expected breakdownField', () => {
-//     const appState = getDefaultProfileState({
-//       profilesManager: profilesManagerMock,
-//       resetDefaultProfileState: {
-//         columns: false,
-//         rowHeight: false,
-//         breakdownField: true,
-//       },
-//       defaultColumns: [],
-//       dataView: dataViewWithTimefieldMock,
-//       esqlQueryColumns: undefined,
-//     });
-//     expect(appState).toEqual({
-//       breakdownField: 'breakdown.field',
-//     });
-//   });
-
-//   it('should return undefined', () => {
-//     const appState = getDefaultProfileState({
-//       profilesManager: profilesManagerMock,
-//       resetDefaultProfileState: {
-//         columns: false,
-//         rowHeight: false,
-//         breakdownField: false,
-//       },
-//       defaultColumns: [],
-//       dataView: dataViewWithTimefieldMock,
-//       esqlQueryColumns: undefined,
-//     });
-//     expect(appState).toEqual(undefined);
-//   });
-// });
+    it('should return undefined', () => {
+      const appState = getDefaultProfileState({
+        profilesManager: profilesManagerMock,
+        resetDefaultProfileState: {
+          columns: false,
+          rowHeight: false,
+          breakdownField: false,
+        },
+        dataView: dataViewWithTimefieldMock,
+      }).getPostFetchState({
+        defaultColumns: [],
+        esqlQueryColumns: undefined,
+      });
+      expect(appState).toEqual(undefined);
+    });
+  });
+});
