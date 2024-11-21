a8ea09476f94bb5e3267f9d88d8807992241b388
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-11-21 00:47:29 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- packages/kbn-grid-layout/grid/grid_panel.tsx
+++ packages/kbn-grid-layout/grid/grid_panel.tsx
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import React, { forwardRef, useEffect, useMemo, Profiler } from 'react';
+import React, { forwardRef, useEffect, useMemo } from 'react';
 import { combineLatest, skip } from 'rxjs';
 
 import {
