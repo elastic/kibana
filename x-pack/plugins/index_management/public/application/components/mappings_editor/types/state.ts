22e2764d2bf3321233e57bf295bba46ac28852ee
Elena Stoeva <elenastoeva99@gmail.com>
2024-11-21 13:25:13 +0000

    [Mappings editor] Add code improvements for source field

--- x-pack/plugins/index_management/public/application/components/mappings_editor/types/state.ts
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/types/state.ts
@@ -108,7 +108,6 @@ export interface State {
   };
   templates: TemplatesFormState;
   inferenceToModelIdMap?: InferenceToModelIdMap;
-  hasEnterpriseLicense: boolean;
   mappingViewFields: NormalizedFields; // state of the incoming index mappings, separate from the editor state above
 }
 
@@ -142,6 +141,5 @@ export type Action =
   | { type: 'search:update'; value: string }
   | { type: 'validity:update'; value: boolean }
   | { type: 'filter:update'; value: { selectedOptions: EuiSelectableOption[] } }
-  | { type: 'hasEnterpriseLicense.update'; value: boolean };
 
 export type Dispatch = (action: Action) => void;
