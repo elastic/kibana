c0b5df63e46a34fb218eb9580057182016020011
Jen Huang <its.jenetic@gmail.com>
2024-11-21 16:57:06 -0800

    [UII] Allow to create integration policy with no agent policies (#201051)
    ## Summary
    
    Resolves #198165. This PR fixes an issue where `policy_ids` array was
    not able to be cleared behind the scenes to create an orphaned
    integration policy, even when cluster is able to use the multiple agent
    policies feature.
    
    (cherry picked from commit 3952dea3962c52dbd8d0cf01f7966670a64639d4)
    
    # Conflicts:
    #	x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/edit_package_policy_page/components/step_edit_hosts.test.tsx

--- x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/edit_package_policy_page/components/step_edit_hosts.test.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agent_policy/edit_package_policy_page/components/step_edit_hosts.test.tsx
@@ -120,10 +120,9 @@ describe('StepEditHosts', () => {
 
     render();
 
-    waitFor(() => {
-      expect(renderResult.getByText('Agent policy 1')).toBeInTheDocument();
-    });
     expect(renderResult.queryByRole('tablist')).not.toBeInTheDocument();
+    expect(renderResult.getByText('For existing hosts:')).toBeInTheDocument();
+    expect(renderResult.getByText('For a new host:')).toBeInTheDocument();
   });
 
   it('should display new policy button and existing policies when agent policies exist', () => {
