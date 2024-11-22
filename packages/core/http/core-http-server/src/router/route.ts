013ef3122bad84397de1e9f7deeae9efa471e150
Elena Shostak <elena.shostak@elastic.co>
2024-11-22 11:46:33 +0100

    [Authz] Removed security property from route.options

--- packages/core/http/core-http-server/src/router/route.ts
+++ packages/core/http/core-http-server/src/router/route.ts
@@ -399,13 +399,6 @@ export interface RouteConfigOptions<Method extends RouteMethod> {
   discontinued?: string;
 
   /**
-   * Defines the security requirements for a route, including authorization and authentication.
-   *
-   * @remarks This will be surfaced in OAS documentation.
-   */
-  security?: RouteSecurity;
-
-  /**
    * Whether this endpoint is being used to serve generated or static HTTP resources
    * like JS, CSS or HTML. _Do not set to `true` for HTTP APIs._
    *
@@ -523,7 +516,7 @@ export interface RouteConfig<P, Q, B, Method extends RouteMethod> {
   /**
    * Additional route options {@link RouteConfigOptions}.
    */
-  options?: RouteConfigOptions<Method>;
+  options?: Omit<RouteConfigOptions<Method>, 'security'>;
 }
 
 /**
