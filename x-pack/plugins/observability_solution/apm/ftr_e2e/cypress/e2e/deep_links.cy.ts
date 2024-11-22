422035c1e3d47c6ad92755ec903cc0ed6c39b625
Milosz Marcinkowski <milosz.marcinkowski@elastic.co>
2024-11-22 20:17:46 +0100

    cover deep links search using both keywords in e2e tests

--- x-pack/plugins/observability_solution/apm/ftr_e2e/cypress/e2e/deep_links.cy.ts
+++ x-pack/plugins/observability_solution/apm/ftr_e2e/cypress/e2e/deep_links.cy.ts
@@ -5,64 +5,77 @@
  * 2.0.
  */
 
-describe('APM deep links', () => {
+describe('Applications deep links', () => {
   beforeEach(() => {
     cy.loginAsViewerUser();
   });
-  it('navigates to apm links on search elastic', () => {
+
+  it('navigates to Application links on "application" search', () => {
     cy.visitKibana('/');
+    navigatesToApmLinks('applications');
+  });
+
+  it('navigates to Application links on "apm" search', () => {
+    cy.visitKibana('/');
+    navigatesToApmLinks('apm');
+  });
+
+  function navigatesToApmLinks(keyword: string) {
     // Wait until the page content is fully loaded
     // otherwise, the search results may disappear before all checks are completed, making this test flaky
     cy.waitUntilPageContentIsLoaded();
-    cy.getByTestSubj('nav-search-input').should('be.visible').type('APM', { force: true }).focus();
-    cy.contains('APM');
-    cy.contains('APM / Service Inventory');
-    cy.contains('APM / Service groups');
-    cy.contains('APM / Traces');
-    cy.contains('APM / Service Map');
-    cy.contains('APM / Dependencies');
-    cy.contains('APM / Settings');
+    cy.getByTestSubj('nav-search-input')
+      .should('be.visible')
+      .type(keyword, { force: true })
+      .focus();
+    cy.contains('Applications');
+    cy.contains('Applications / Service Inventory');
+    cy.contains('Applications / Service groups');
+    cy.contains('Applications / Traces');
+    cy.contains('Applications / Service Map');
+    cy.contains('Applications / Dependencies');
+    cy.contains('Applications / Settings');
 
     // navigates to home page
     // Force click because welcome screen changes
     // https://github.com/elastic/kibana/pull/108193
-    cy.contains('APM').click({ force: true });
+    cy.contains('Applications').click({ force: true });
     cy.url().should('include', '/apm/services');
 
     cy.waitUntilPageContentIsLoaded();
-    cy.getByTestSubj('nav-search-input').should('be.visible').type('APM', { force: true });
+    cy.getByTestSubj('nav-search-input').should('be.visible').type(keyword, { force: true });
     // navigates to services page
-    cy.contains('APM / Service Inventory').click({ force: true });
+    cy.contains('Applications / Service Inventory').click({ force: true });
     cy.url().should('include', '/apm/services');
 
     cy.waitUntilPageContentIsLoaded();
-    cy.getByTestSubj('nav-search-input').should('be.visible').type('APM', { force: true });
+    cy.getByTestSubj('nav-search-input').should('be.visible').type(keyword, { force: true });
     // navigates to service groups page
-    cy.contains('APM / Service groups').click({ force: true });
+    cy.contains('Applications / Service groups').click({ force: true });
     cy.url().should('include', '/apm/service-groups');
 
     cy.waitUntilPageContentIsLoaded();
-    cy.getByTestSubj('nav-search-input').should('be.visible').type('APM', { force: true });
+    cy.getByTestSubj('nav-search-input').should('be.visible').type(keyword, { force: true });
     // navigates to traces page
-    cy.contains('APM / Traces').click({ force: true });
+    cy.contains('Applications / Traces').click({ force: true });
     cy.url().should('include', '/apm/traces');
 
     cy.waitUntilPageContentIsLoaded();
-    cy.getByTestSubj('nav-search-input').should('be.visible').type('APM', { force: true });
+    cy.getByTestSubj('nav-search-input').should('be.visible').type(keyword, { force: true });
     // navigates to service maps
-    cy.contains('APM / Service Map').click({ force: true });
+    cy.contains('Applications / Service Map').click({ force: true });
     cy.url().should('include', '/apm/service-map');
 
     cy.waitUntilPageContentIsLoaded();
-    cy.getByTestSubj('nav-search-input').should('be.visible').type('APM', { force: true });
+    cy.getByTestSubj('nav-search-input').should('be.visible').type(keyword, { force: true });
     // navigates to dependencies page
-    cy.contains('APM / Dependencies').click({ force: true });
+    cy.contains('Applications / Dependencies').click({ force: true });
     cy.url().should('include', '/apm/dependencies/inventory');
 
     cy.waitUntilPageContentIsLoaded();
-    cy.getByTestSubj('nav-search-input').should('be.visible').type('APM', { force: true });
+    cy.getByTestSubj('nav-search-input').should('be.visible').type(keyword, { force: true });
     // navigates to settings page
-    cy.contains('APM / Settings').click({ force: true });
+    cy.contains('Applications / Settings').click({ force: true });
     cy.url().should('include', '/apm/settings/general-settings');
-  });
+  }
 });
