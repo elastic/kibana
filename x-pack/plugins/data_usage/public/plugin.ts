ed434a7282d4b5f99d14f19ed2699636cf2664fd
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-11-24 00:54:00 +1100

    [8.x] [Data Usage] Enabled plugin for Serverless and added feature flag to manage availability (#201465) (#201498)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[Data Usage] Enabled plugin for Serverless and added feature flag to
    manage availability
    (#201465)](https://github.com/elastic/kibana/pull/201465)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Yuliia
    Naumenko","email":"jo.naumenko@gmail.com"},"sourceCommit":{"committedDate":"2024-11-23T12:06:23Z","message":"[Data
    Usage] Enabled plugin for Serverless and added feature flag to manage
    availability (#201465)\n\nThis PR enables data_usage plugin for
    Serverless environment for all 3\r\nsolutions.\r\nTo manage feature
    availability added feature flag, which is turning Data\r\nUsage off by
    default.","sha":"5342f327ee62219c5cf12d50e424da9b91330d5e","branchLabelMapping":{"^v9.0.0$":"main","^v8.18.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","v9.0.0","backport:all-open","ci:build-serverless-image","v8.18.0"],"title":"[Data
    Usage] Enabled plugin for Serverless and added feature flag to manage
    availability","number":201465,"url":"https://github.com/elastic/kibana/pull/201465","mergeCommit":{"message":"[Data
    Usage] Enabled plugin for Serverless and added feature flag to manage
    availability (#201465)\n\nThis PR enables data_usage plugin for
    Serverless environment for all 3\r\nsolutions.\r\nTo manage feature
    availability added feature flag, which is turning Data\r\nUsage off by
    default.","sha":"5342f327ee62219c5cf12d50e424da9b91330d5e"}},"sourceBranch":"main","suggestedTargetBranches":["8.x"],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","branchLabelMappingKey":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/201465","number":201465,"mergeCommit":{"message":"[Data
    Usage] Enabled plugin for Serverless and added feature flag to manage
    availability (#201465)\n\nThis PR enables data_usage plugin for
    Serverless environment for all 3\r\nsolutions.\r\nTo manage feature
    availability added feature flag, which is turning Data\r\nUsage off by
    default.","sha":"5342f327ee62219c5cf12d50e424da9b91330d5e"}},{"branch":"8.x","label":"v8.18.0","branchLabelMappingKey":"^v8.18.0$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    Co-authored-by: Yuliia Naumenko <jo.naumenko@gmail.com>

--- x-pack/plugins/data_usage/public/plugin.ts
+++ x-pack/plugins/data_usage/public/plugin.ts
@@ -5,17 +5,21 @@
  * 2.0.
  */
 
-import { CoreSetup, CoreStart, Plugin } from '@kbn/core/public';
+import { CoreSetup, CoreStart, Plugin, PluginInitializerContext } from '@kbn/core/public';
 import { ManagementAppMountParams } from '@kbn/management-plugin/public';
 import {
   DataUsagePublicSetup,
   DataUsagePublicStart,
   DataUsageStartDependencies,
   DataUsageSetupDependencies,
+  DataUsagePublicConfigType,
 } from './types';
 import { PLUGIN_ID } from '../common';
 import { PLUGIN_NAME } from './translations';
-
+import {
+  ExperimentalFeatures,
+  parseExperimentalConfigValue,
+} from '../common/experimental_features';
 export class DataUsagePlugin
   implements
     Plugin<
@@ -25,30 +29,46 @@ export class DataUsagePlugin
       DataUsageStartDependencies
     >
 {
+  private config: DataUsagePublicConfigType;
+  private experimentalFeatures: ExperimentalFeatures;
+
+  constructor(private readonly initializerContext: PluginInitializerContext) {
+    this.config = this.initializerContext.config.get<DataUsagePublicConfigType>();
+    this.experimentalFeatures = {} as ExperimentalFeatures;
+  }
+
   public setup(
     core: CoreSetup<DataUsageStartDependencies, DataUsagePublicStart>,
     plugins: DataUsageSetupDependencies
   ): DataUsagePublicSetup {
     const { management } = plugins;
-    management.sections.section.data.registerApp({
-      id: PLUGIN_ID,
-      title: PLUGIN_NAME,
-      order: 6,
-      keywords: ['data usage', 'usage'],
-      async mount(params: ManagementAppMountParams) {
-        const [{ renderApp }, [coreStart, pluginsStartDeps, pluginStart]] = await Promise.all([
-          import('./application'),
-          core.getStartServices(),
-        ]);
 
-        return renderApp(coreStart, pluginsStartDeps, pluginStart, params);
-      },
-    });
+    this.experimentalFeatures = parseExperimentalConfigValue(
+      this.config.enableExperimental
+    ).features;
 
+    const experimentalFeatures = this.experimentalFeatures;
+
+    if (!experimentalFeatures.dataUsageDisabled) {
+      management.sections.section.data.registerApp({
+        id: PLUGIN_ID,
+        title: PLUGIN_NAME,
+        order: 6,
+        keywords: ['data usage', 'usage'],
+        async mount(params: ManagementAppMountParams) {
+          const [{ renderApp }, [coreStart, pluginsStartDeps, pluginStart]] = await Promise.all([
+            import('./application'),
+            core.getStartServices(),
+          ]);
+
+          return renderApp(coreStart, pluginsStartDeps, pluginStart, params);
+        },
+      });
+    }
     return {};
   }
 
-  public start(_core: CoreStart): DataUsagePublicStart {
+  public start(_core: CoreStart, plugins: DataUsageStartDependencies): DataUsagePublicStart {
     return {};
   }
 
