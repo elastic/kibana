fcef6bbd81fa477c4edab6bec9f573c107ef450b
Saarika Bhasi <55930906+saarikabhasi@users.noreply.github.com>
2024-11-23 11:11:22 +0530

    Update search_result_item.tsx

--- x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/search_fields/search_result_item.tsx
+++ x-pack/plugins/index_management/public/application/components/mappings_editor/components/document_fields/search_fields/search_result_item.tsx
@@ -108,7 +108,7 @@ export const SearchResultItem = React.memo(function FieldListItemFlatComponent({
               'mappingsEditor__fieldsListItem__content--toggle': hasChildFields || hasMultiFields,
               'mappingsEditor__fieldsListItem__content--multiField': isMultiField,
             })}
-            tabindex={0}
+            tabIndex={0}
           >
             <EuiFlexItem
               grow={false}
