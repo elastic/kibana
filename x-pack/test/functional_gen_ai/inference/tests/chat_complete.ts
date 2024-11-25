bbfb0be377a01050b3e76fc8540aba3fc23dacce
pgayvallet <pierre.gayvallet@elastic.co>
2024-11-25 09:19:21 +0100

    add more tests

--- x-pack/test/functional_gen_ai/inference/tests/chat_complete.ts
+++ x-pack/test/functional_gen_ai/inference/tests/chat_complete.ts
@@ -12,7 +12,7 @@ import { FtrProviderContext } from '../ftr_provider_context';
 import type { AvailableConnectorWithId } from '../../common/connectors';
 
 export const chatCompleteSuite = (
-  { id: connectorId }: AvailableConnectorWithId,
+  { id: connectorId, actionTypeId: connectorType }: AvailableConnectorWithId,
   { getService }: FtrProviderContext
 ) => {
   const supertest = getService('supertest');
@@ -35,6 +35,78 @@ export const chatCompleteSuite = (
         expect(message.toolCalls.length).to.eql(0);
         expect(message.content).to.contain('4');
       });
+
+      it('executes a tool with native function calling', async () => {
+        const response = await supertest
+          .post(`/internal/inference/chat_complete`)
+          .set('kbn-xsrf', 'kibana')
+          .send({
+            connectorId,
+            system:
+              'Please answer the user question. You can use the available tools if you think it can help',
+            messages: [{ role: 'user', content: 'What is the result of 2*4*6*8*10*123 ?' }],
+            toolChoice: 'required',
+            tools: {
+              calculator: {
+                description: 'The calculator can be used to resolve mathematical calculations',
+                schema: {
+                  type: 'object',
+                  properties: {
+                    formula: {
+                      type: 'string',
+                      description: `The input for the calculator, in plain text, e.g. "2+(4*8)"`,
+                    },
+                  },
+                },
+              },
+            },
+          })
+          .expect(200);
+
+        const message = response.body;
+
+        expect(message.toolCalls.length).to.eql(1);
+        expect(message.toolCalls[0].function.name).to.eql('calculator');
+        expect(message.toolCalls[0].function.arguments.formula).to.contain('123');
+      });
+
+      // simulated FC is only for openAI
+      if (connectorType === '.gen-ai') {
+        it('executes a tool with simulated function calling', async () => {
+          const response = await supertest
+            .post(`/internal/inference/chat_complete`)
+            .set('kbn-xsrf', 'kibana')
+            .send({
+              connectorId,
+              system:
+                'Please answer the user question. You can use the available tools if you think it can help',
+              messages: [{ role: 'user', content: 'What is the result of 2*4*6*8*10*123 ?' }],
+              functionCalling: 'simulated',
+              toolChoice: 'required',
+              tools: {
+                calculator: {
+                  description: 'The calculator can be used to resolve mathematical calculations',
+                  schema: {
+                    type: 'object',
+                    properties: {
+                      formula: {
+                        type: 'string',
+                        description: `The input for the calculator, in plain text, e.g. "2+(4*8)"`,
+                      },
+                    },
+                  },
+                },
+              },
+            })
+            .expect(200);
+
+          const message = response.body;
+
+          expect(message.toolCalls.length).to.eql(1);
+          expect(message.toolCalls[0].function.name).to.eql('calculator');
+          expect(message.toolCalls[0].function.arguments.formula).to.contain('123');
+        });
+      }
 
       it('returns token counts', async () => {
         const response = await supertest
@@ -78,6 +150,42 @@ export const chatCompleteSuite = (
         expect(message.content).to.contain('4');
       });
 
+      it('executes a tool when explicitly requested', async () => {
+        const response = supertest
+          .post(`/internal/inference/chat_complete/stream`)
+          .set('kbn-xsrf', 'kibana')
+          .send({
+            connectorId,
+            system:
+              'Please answer the user question. You can use the available tools if you think it can help',
+            messages: [{ role: 'user', content: 'What is the result of 2*4*6*8*10*123 ?' }],
+            toolChoice: 'required',
+            tools: {
+              calculator: {
+                description: 'The calculator can be used to resolve mathematical calculations',
+                schema: {
+                  type: 'object',
+                  properties: {
+                    formula: {
+                      type: 'string',
+                      description: `The input for the calculator, in plain text, e.g. "2+(4*8)"`,
+                    },
+                  },
+                },
+              },
+            },
+          })
+          .expect(200);
+
+        const observable = supertestToObservable(response);
+
+        const message = await lastValueFrom(observable);
+
+        expect(message.toolCalls.length).to.eql(1);
+        expect(message.toolCalls[0].function.name).to.eql('calculator');
+        expect(message.toolCalls[0].function.arguments.formula).to.contain('123');
+      });
+
       it('returns a token count event', async () => {
         const response = supertest
           .post(`/internal/inference/chat_complete/stream`)
@@ -93,8 +201,6 @@ export const chatCompleteSuite = (
 
         const events = await lastValueFrom(observable.pipe(toArray()));
         const tokenEvent = events[events.length - 2];
-
-        console.log(JSON.stringify(events));
 
         expect(tokenEvent.type).to.eql('chatCompletionTokenCount');
         expect(tokenEvent.tokens.prompt).to.be.greaterThan(0);
