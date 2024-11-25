9d29d45eb46ff7b6b458847ab677ba69164cfad7
Eyo O. Eyo <7893459+eokoneyo@users.noreply.github.com>
2024-11-25 11:49:51 +0100

    [React18] Migrate test suites to account for testing library upgrades obs-ux-logs-team (#201148)
    This PR migrates test suites that use `renderHook` from the library
    `@testing-library/react-hooks` to adopt the equivalent and replacement
    of `renderHook` from the export that is now available from
    `@testing-library/react`. This work is required for the planned
    migration to react18.
    
    ##  Context
    
    In this PR, usages of `waitForNextUpdate` that previously could have
    been destructured from `renderHook` are now been replaced with `waitFor`
    exported from `@testing-library/react`, furthermore `waitFor`
    that would also have been destructured from the same renderHook result
    is now been replaced with `waitFor` from the export of
    `@testing-library/react`.
    
    ***Why is `waitFor` a sufficient enough replacement for
    `waitForNextUpdate`, and better for testing values subject to async
    computations?***
    
    WaitFor will retry the provided callback if an error is returned, till
    the configured timeout elapses. By default the retry interval is `50ms`
    with a timeout value of `1000ms` that
    effectively translates to at least 20 retries for assertions placed
    within waitFor. See
    https://testing-library.com/docs/dom-testing-library/api-async/#waitfor
    for more information.
    This however means that for person's writing tests, said person has to
    be explicit about expectations that describe the internal state of the
    hook being tested.
    This implies checking for instance when a react query hook is being
    rendered, there's an assertion that said hook isn't loading anymore.
    
    In this PR you'd notice that this pattern has been adopted, with most
    existing assertions following an invocation of `waitForNextUpdate` being
    placed within a `waitFor`
    invocation. In some cases the replacement is simply a `waitFor(() => new
    Promise((resolve) => resolve(null)))` (many thanks to @kapral18, for
    point out exactly why this works),
    where this suffices the assertions that follow aren't placed within a
    waitFor so this PR doesn't get larger than it needs to be.
    
    It's also worth pointing out this PR might also contain changes to test
    and application code to improve said existing test.
    
    ### What to do next?
    1. Review the changes in this PR.
    2. If you think the changes are correct, approve the PR.
    
    ## Any questions?
    If you have any questions or need help with this PR, please leave
    comments in this PR.
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/plugins/observability_solution/logs_shared/public/utils/data_search/use_latest_partial_data_search_response.test.tsx
+++ x-pack/plugins/observability_solution/logs_shared/public/utils/data_search/use_latest_partial_data_search_response.test.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { act, renderHook } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react';
 import { BehaviorSubject, Observable, of, Subject } from 'rxjs';
 import { IKibanaSearchRequest } from '@kbn/search-types';
 import { ParsedDataSearchRequestDescriptor, ParsedKibanaSearchResponse } from './types';
