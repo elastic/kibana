e13b0ef0bcd9207e9ce285e26bc7decff1aacb54
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-11-26 17:33:49 +0100

    [kbn-scout] initial package implementation (playwright-test POC) (#194006)
    ## Summary
    
    This PR introduces a new package, **kbn-scout**, designed to streamline
    the setup and execution of Playwright tests for Kibana. The `kbn-scout`
    package consolidates server management and testing capabilities by
    wrapping both the Kibana/Elasticsearch server launcher and the
    Playwright test runner. It includes:
    
    - Test and worker-scoped fixtures for reliable setup across test suites
    - Page objects combined into the fixture for Kibana UI interactions
    - Configurations for seamless test execution in both local and CI
    environments (`Cloud` execution is out of scope)
    - This package aims to simplify test setup and enhance modularity,
    making it easier to create, run, and maintain deployment-agnostic tests,
    that are located in the plugin they actually test.
    
    Tests example is available in `x-pack/plugins/discover_enhanced` plugin
    under `ui_tests` folder
    
    How to run:
    1) As a single script (servers + tests):
    ```
    node scripts/scout_test.js --config=x-pack/plugins/discover_enhanced/ui_tests/playwright.config.ts --serverless=es
    ```
    
    2) Start servers first
    ```
    node scripts/scout_start_servers.js --serverless=es
    ```
    then run tests:
    ```
    npx playwright test --config=x-pack/plugins/discover_enhanced/ui_tests/playwright.config.ts
    ```
    if you have Playwright plugin in IDEA, just use it to run tests files
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 310d922f137ed5c1117e2660ffa8af618810f350)
    
    # Conflicts:
    #	.github/CODEOWNERS
    #	packages/kbn-test/index.ts

---
+++ packages/kbn-scout/src/playwright/fixtures/test/page.ts
@@ -0,0 +1,83 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { Page, test as base } from '@playwright/test';
+import { subj } from '@kbn/test-subj-selector';
+import { ScoutPage, KibanaUrl } from '../types';
+
+/**
+ * Instead of defining each method individually, we use a list of method names and loop through them, creating methods dynamically.
+ * All methods must have 'selector: string' as the first argument
+ */
+function extendPageWithTestSubject(page: Page) {
+  const methods: Array<keyof Page> = [
+    'check',
+    'click',
+    'dblclick',
+    'fill',
+    'focus',
+    'getAttribute',
+    'hover',
+    'isEnabled',
+    'innerText',
+    'isChecked',
+    'isHidden',
+    'locator',
+  ];
+
+  const extendedMethods: Partial<Record<keyof Page, Function>> = {};
+
+  for (const method of methods) {
+    extendedMethods[method] = (...args: any[]) => {
+      const selector = args[0];
+      const testSubjSelector = subj(selector);
+      return (page[method] as Function)(testSubjSelector, ...args.slice(1));
+    };
+  }
+
+  return extendedMethods as Record<keyof Page, any>;
+}
+
+/**
+ * Extends the 'page' fixture with Kibana-specific functionality
+ *
+ * 1. Allow calling methods with simplified 'data-test-subj' selectors.
+ * Instead of manually constructing 'data-test-subj' selectors, this extension provides a `testSubj` object on the page
+ * Supported methods include `click`, `check`, `fill`, and others that interact with `data-test-subj`.
+ *
+ * Example Usage:
+ *
+ * ```typescript
+ * // Without `testSubj` extension:
+ * await page.locator('[data-test-subj="foo"][data-test-subj="bar"]').click();
+ *
+ * // With `testSubj` extension:
+ * await page.testSubj.click('foo & bar');
+ * ```
+ *
+ * 2. Navigate to Kibana apps by using 'kbnUrl' fixture
+ *
+ * Example Usage:
+ *
+ * ```typescript
+ * // Navigate to '/app/discover'
+ * await page.gotoApp('discover);
+ * ```
+ */
+export const scoutPageFixture = base.extend<{ page: ScoutPage; kbnUrl: KibanaUrl }>({
+  page: async ({ page, kbnUrl }, use) => {
+    // Extend page with '@kbn/test-subj-selector' support
+    page.testSubj = extendPageWithTestSubject(page);
+
+    // Method to navigate to specific Kibana apps
+    page.gotoApp = (appName: string) => page.goto(kbnUrl.app(appName));
+
+    await use(page);
+  },
+});
---
+++ x-pack/plugins/discover_enhanced/ui_tests/fixtures/index.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import {
+  test as base,
+  PageObjects,
+  createLazyPageObject,
+  ScoutTestFixtures,
+  ScoutWorkerFixtures,
+} from '@kbn/scout';
+import { DemoPage } from './page_objects';
+
+interface ExtendedScoutTestFixtures extends ScoutTestFixtures {
+  pageObjects: PageObjects & {
+    demo: DemoPage;
+  };
+}
+
+export const test = base.extend<ExtendedScoutTestFixtures, ScoutWorkerFixtures>({
+  pageObjects: async ({ pageObjects, page }, use) => {
+    const extendedPageObjects = {
+      ...pageObjects,
+      demo: createLazyPageObject(DemoPage, page),
+    };
+
+    await use(extendedPageObjects);
+  },
+});
