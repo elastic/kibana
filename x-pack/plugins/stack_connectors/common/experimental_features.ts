f70618bfacb73c63d73220f10b0ab797e64616ed
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2024-11-26 16:43:55 +0100

    set flag to false

--- x-pack/plugins/stack_connectors/common/experimental_features.ts
+++ x-pack/plugins/stack_connectors/common/experimental_features.ts
@@ -16,7 +16,7 @@ export const allowedExperimentalValues = Object.freeze({
   sentinelOneConnectorOn: true,
   crowdstrikeConnectorOn: true,
   inferenceConnectorOn: false,
-  crowdstrikeConnectorRTROn: true,
+  crowdstrikeConnectorRTROn: false,
 });
 
 export type ExperimentalConfigKeys = Array<keyof ExperimentalFeatures>;
