7ddf284e46d33fa33161e4e041ad461ebe6e4d0b
Gonçalo Rica <goncalo.rica@elastic.co>
2024-11-26 17:49:09 +0100

    Fix false typing of excludedDataTiers

--- x-pack/plugins/observability_solution/apm_data_access/server/lib/helpers/tier_filter.ts
+++ x-pack/plugins/observability_solution/apm_data_access/server/lib/helpers/tier_filter.ts
@@ -13,13 +13,13 @@ export function getDataTierFilterCombined({
   excludedDataTiers,
 }: {
   filter?: QueryDslQueryContainer;
-  excludedDataTiers: DataTier[];
+  excludedDataTiers?: DataTier[];
 }): QueryDslQueryContainer | undefined {
   if (!filter) {
     return excludedDataTiers.length > 0 ? excludeTiersQuery(excludedDataTiers)[0] : undefined;
   }
 
-  return !excludedDataTiers.length
+  return !excludedDataTiers
     ? filter
     : {
         bool: {
