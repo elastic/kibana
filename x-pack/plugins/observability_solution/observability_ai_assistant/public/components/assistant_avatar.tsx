0bc90988567ce4e830738884a6ce5b53edd14b46
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2024-11-26 09:50:37 -0500

    [Obs AI Assistant] Use textParagraph instead of textHeading

--- x-pack/plugins/observability_solution/observability_ai_assistant/public/components/assistant_avatar.tsx
+++ x-pack/plugins/observability_solution/observability_ai_assistant/public/components/assistant_avatar.tsx
@@ -39,7 +39,7 @@ export function AssistantAvatar({ size = 's', css, className }: AssistantAvatarP
       <path fill="#F04E98" d="M36 28h24v36H36V28Z" />
       <path fill="#00BFB3" d="M4 46c0-9.941 8.059-18 18-18h6v36h-6c-9.941 0-18-8.059-18-18Z" />
       <path
-        fill={euiTheme.colors.textHeading}
+        fill={euiTheme.colors.textParagraph}
         d="M60 12c0 6.627-5.373 12-12 12s-12-5.373-12-12S41.373 0 48 0s12 5.373 12 12Z"
       />
       <path fill="#FA744E" d="M6 23C6 10.85 15.85 1 28 1v22H6Z" />
