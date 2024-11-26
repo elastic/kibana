615f9866e3c2b4eede93ce0a40262be972fbb21d
Matthew Kime <matt@mattki.me>
2024-11-26 00:57:42 -0600

    fix tests

--- x-pack/plugins/index_management/__jest__/client_integration/home/enrich_policies.test.tsx
+++ x-pack/plugins/index_management/__jest__/client_integration/home/enrich_policies.test.tsx
@@ -38,11 +38,6 @@ describe('Enrich policies tab', () => {
   describe('empty states', () => {
     beforeEach(async () => {
       setDelayResponse(false);
-
-      httpRequestsMockHelpers.setGetPrivilegesResponse({
-        hasAllPrivileges: true,
-        missingPrivileges: { cluster: [] },
-      });
     });
 
     test('displays a loading prompt', async () => {
@@ -84,11 +79,6 @@ describe('Enrich policies tab', () => {
 
   describe('permissions check', () => {
     it('shows a permissions error when the user does not have sufficient privileges', async () => {
-      httpRequestsMockHelpers.setGetPrivilegesResponse({
-        hasAllPrivileges: false,
-        missingPrivileges: { cluster: ['manage_enrich'] },
-      });
-
       testBed = await setup(httpSetup);
       await act(async () => {
         testBed.actions.goToEnrichPoliciesTab();
@@ -109,11 +99,6 @@ describe('Enrich policies tab', () => {
         testPolicy,
         createTestEnrichPolicy('policy-range', 'range'),
       ]);
-
-      httpRequestsMockHelpers.setGetPrivilegesResponse({
-        hasAllPrivileges: true,
-        missingPrivileges: { cluster: [] },
-      });
 
       testBed = await setup(httpSetup);
       await act(async () => {
