0c6b4115d22e74f53dd0c722b7ab7f583b4733e2
Sean Rathier <sean.rathier@elastic.co>
2024-11-27 17:28:42 -0500

    Fixed tests and the mki config

--- x-pack/test_serverless/functional/test_suites/security/config.mki_only.ts
+++ x-pack/test_serverless/functional/test_suites/security/config.mki_only.ts
@@ -4,12 +4,27 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { KBN_CERT_PATH, KBN_KEY_PATH } from '@kbn/dev-utils';
+import { createTestConfig } from '../../config.base';
 
-import { FtrProviderContext } from '../../ftr_provider_context';
+export default createTestConfig({
+  serverlessProject: 'security',
+  testFiles: [require.resolve('./mki_only.ts')],
+  junit: {
+    reportName: 'Serverless Security MKI Functional Tests',
+  },
+  suiteTags: { exclude: ['skipSvlSec'] },
 
-export default function ({ loadTestFile }: FtrProviderContext) {
-  describe('serverless security UI', function () {
-    this.tags(['security-mki-only']);
-    loadTestFile(require.resolve('./ftr/cloud_security_posture/mki_only'));
-  });
-}
+  // include settings from project controller
+  // https://github.com/elastic/project-controller/blob/main/internal/project/security/config/elasticsearch.yml
+  esServerArgs: ['xpack.ml.nlp.enabled=true'],
+  kbnServerArgs: [
+    '--xpack.dataUsage.enabled=true',
+    '--xpack.dataUsage.enableExperimental=[]',
+    // dataUsage.autoops* config is set in kibana controller
+    '--xpack.dataUsage.autoops.enabled=true',
+    '--xpack.dataUsage.autoops.api.url=http://localhost:9000',
+    `--xpack.dataUsage.autoops.api.tls.certificate=${KBN_CERT_PATH}`,
+    `--xpack.dataUsage.autoops.api.tls.key=${KBN_KEY_PATH}`,
+  ],
+});
