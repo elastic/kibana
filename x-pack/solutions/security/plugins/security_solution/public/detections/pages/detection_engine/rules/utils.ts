46bdae7ad60651061a5b397e0d11010dbc4ebb94
Maxim Palenov <maxim.palenov@elastic.co>
2024-11-27 22:50:01 +0100

    fix validation upon saving

--- x-pack/solutions/security/plugins/security_solution/public/detections/pages/detection_engine/rules/utils.ts
+++ x-pack/solutions/security/plugins/security_solution/public/detections/pages/detection_engine/rules/utils.ts
@@ -6,6 +6,7 @@
  */
 
 import type { Type } from '@kbn/securitysolution-io-ts-alerting-types';
+import { DEFAULT_THREAT_MAPPING_VALUE } from '../../../../detection_engine/rule_creation/components/threat_match_mapping_edit';
 import {
   ALERT_SUPPRESSION_DURATION_FIELD_NAME,
   ALERT_SUPPRESSION_DURATION_TYPE_FIELD_NAME,
@@ -53,7 +54,7 @@ export const stepDefineDefaultValue: DefineStepRule = {
   },
   requiredFields: [],
   relatedIntegrations: [],
-  threatMapping: [],
+  threatMapping: DEFAULT_THREAT_MAPPING_VALUE,
   threshold: {
     field: [],
     value: '200',
