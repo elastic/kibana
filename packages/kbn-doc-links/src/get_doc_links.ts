5ac378343abe48ffb972fd0a1e241c67b24aa935
Julian Gernun <17549662+jcger@users.noreply.github.com>
2024-11-27 11:37:49 +0100

    [Response Ops][Cases] Use deprecation object (#201004)
    ## Summary
    
    Adds deprecation object in deprecated cases routes
    
    Closes [#196557](https://github.com/elastic/kibana/issues/196557)
    
    ---------
    
    Co-authored-by: Lisa Cawley <lcawley@elastic.co>
    (cherry picked from commit 0735ab8196e7d11fb099b564b2c9b03ce840aa36)

--- packages/kbn-doc-links/src/get_doc_links.ts
+++ packages/kbn-doc-links/src/get_doc_links.ts
@@ -1008,5 +1008,8 @@ export const getDocLinks = ({ kibanaBranch, buildFlavor }: GetDocLinkOptions): D
     inferenceManagement: {
       inferenceAPIDocumentation: `${ELASTIC_WEBSITE_URL}docs/api/doc/elasticsearch/operation/operation-inference-put`,
     },
+    cases: {
+      legacyApiDeprecations: `${KIBANA_DOCS}breaking-changes-summary.html#breaking-201004`,
+    },
   });
 };
