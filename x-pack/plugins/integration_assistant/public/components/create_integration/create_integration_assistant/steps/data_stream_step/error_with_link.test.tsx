d154be0ee367f11857a8e3bc4f3795b289e60fca
Bharat Pasupula <saibharatchandra.pasupula@elastic.co>
2024-11-27 13:27:41 +0100

    fix tests

--- x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/data_stream_step/error_with_link.test.tsx
+++ x-pack/plugins/integration_assistant/public/components/create_integration/create_integration_assistant/steps/data_stream_step/error_with_link.test.tsx
@@ -5,7 +5,9 @@
  * 2.0.
  */
 
-import { isErrorMessageWithLink } from './error_with_link';
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ErrorMessage, isErrorMessageWithLink, MessageLink } from './error_with_link';
 
 describe('isErrorMessageWithLink', () => {
   it('should return true when error is an ErrorMessageWithLink', () => {
@@ -22,8 +24,20 @@ describe('isErrorMessageWithLink', () => {
     expect(isErrorMessageWithLink(error)).toBe(false);
   });
 
-  it('should return false when error is null', () => {
-    const error = null;
-    expect(isErrorMessageWithLink(error)).toBe(false);
+  describe('MessageLink', () => {
+    it('should render link with correct href and text', () => {
+      const { getByText } = render(<MessageLink link="http://example.com" linkText="decode_cef" />);
+      const linkElement = getByText('decode_cef');
+      expect(linkElement).toBeInTheDocument();
+      expect(linkElement).toHaveAttribute('href', 'http://example.com');
+    });
+  });
+
+  describe('ErrorMessage', () => {
+    it('should render error message when error is a string', () => {
+      const error = 'An error occurred';
+      const { getByText } = render(<ErrorMessage error={error} />);
+      expect(getByText('An error occurred')).toBeInTheDocument();
+    });
   });
 });
