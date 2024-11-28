d92aa91c3a8329057a2156ea7f1ecdc94a7a3cc2
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2024-11-28 19:31:09 +0100

    [Dashboard][Collapsable Panels] Responsive layout (#200771)
    ## Summary
    
    Fixes https://github.com/elastic/kibana/issues/197714
    
    ### Key Features
    
    #### Responsiveness
    
    1. Adds a responsive view controlled by the `accessMode` prop. 
    2. For the responsive version (in the `VIEW` mode), panels retain height
    and are arranged based on screen order (left-to-right, top-to-bottom).
    3. Interactivity (drag/resize) is disabled in `view` mode.
    
    <img width="514" alt="Screenshot 2024-11-25 at 17 34 56"
    src="https://github.com/user-attachments/assets/6a5a97aa-de9b-495a-b1de-301bc935a5ab">
    
    #### Maximization
    1. Supports expanded panel view using the `expandedPanelId` prop.
    2. Interactivity (drag/resize) is disabled when a panel is expanded.
    
    <img width="1254" alt="Screenshot 2024-11-25 at 17 35 05"
    src="https://github.com/user-attachments/assets/c83014f6-18ad-435b-a59d-1d3ba3f80d84">
    
    #### Considerations
    1. CSS elements naming convention: Main component uses `kbnGrid` class,
    with modifiers like `kbnGrid--nonInteractive`. For the drag handle of
    `GridPanel` I used `kbnGridPanel__dragHandle` classname.
    2. Classes vs. Inline Styles: I opted for using
    `kbnGrid--nonInteractive` instead of adding one more subscription to
    `GridPanel` to modify the styles inline. It's the first time in this
    package that I used classes instead of inline styles for no-initial
    styles setting.
    3. Naming Convention: I opted for using the `expanded` word to describe
    an expanded panel. Another one could be `maximized` as it's more used in
    UI, but less in the legacy code.
    4. Interactivity (drag/resize) is disabled in responsive mode but we
    could consider to limit this to small viewports only (<768px).
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- packages/kbn-grid-layout/grid/utils/mobile_view.ts
+++ packages/kbn-grid-layout/grid/utils/mobile_view.ts
@@ -7,13 +7,11 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export { GridLayout } from './grid/grid_layout';
-export type {
-  GridLayoutData,
-  GridPanelData,
-  GridRowData,
-  GridSettings,
-  GridAccessMode,
-} from './grid/types';
+import { euiThemeVars } from '@kbn/ui-theme';
+import { GridAccessMode } from '../types';
 
-export { isLayoutEqual } from './grid/utils/equality_checks';
+const getViewportWidth = () =>
+  window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+export const shouldShowMobileView = (accessMode: GridAccessMode) =>
+  accessMode === 'VIEW' && getViewportWidth() < parseFloat(euiThemeVars.euiBreakpoints.m);
