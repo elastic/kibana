a7f052a656bb3c6abd1d4c6e6ec701b2ad3bf5a5
Mike Côté <mikecote@users.noreply.github.com>
2024-11-28 12:10:56 -0500

    Set refresh according to stateful vs stateless when indexing alert documents (#201209)
    In this PR, I'm making the change so when Kibana is running with
    Elasticsearch stateful we set refresh to `wait_for` (instead of `true`)
    so we are not putting too much pressure on the Elasticsearch indices
    when under load.
    
    ## To verify
    
    Very using the Cloud deployment and Serverless project created from this
    PR
    
    1. Create an always firing ES Query rule
    2. Create an always firing security detection rule w/ alert suppression
    3. Verify the ECH cluster logs and observe `*** Refresh value when
    indexing alerts: wait_for` and `*** Rule registry - refresh value when
    indexing alerts: wait_for` messages
    4. Verify the serverless project logs on QA overview and observe `***
    Refresh value when indexing alerts: true` and `*** Rule registry -
    refresh value when indexing alerts: true` messages
    
    ## To-Do
    
    - [x] Revert commit
    https://github.com/elastic/kibana/pull/201209/commits/7c19b458e6f55866bfff6a5b0f39188ae95dc6c6
    that was added for testing purposes
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit a4cb330af2d414e383d75efce526513171098ece)
    
    # Conflicts:
    #	x-pack/plugins/alerting/kibana.jsonc
    #	x-pack/plugins/alerting/server/plugin.ts

--- x-pack/plugins/security_solution/server/plugin.ts
+++ x-pack/plugins/security_solution/server/plugin.ts
@@ -382,7 +382,8 @@ export class Plugin implements ISecuritySolutionPlugin {
       core.getStartServices,
       securityRuleTypeOptions,
       previewRuleDataClient,
-      this.telemetryReceiver
+      this.telemetryReceiver,
+      this.pluginContext.env.packageInfo.buildFlavor === 'serverless'
     );
 
     registerEndpointRoutes(router, this.endpointContext);
