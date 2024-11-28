ab147192301b4db50b717101491185fac45f8b8a
Faisal Kanout <faisal@kanout.com>
2024-11-28 14:20:42 +0100

    Update tests

--- x-pack/plugins/observability_solution/infra/server/lib/alerting/metric_threshold/lib/create_timerange.test.ts
+++ x-pack/plugins/observability_solution/infra/server/lib/alerting/metric_threshold/lib/create_timerange.test.ts
@@ -10,122 +10,139 @@ import { Aggregators } from '../../../../../common/alerting/metrics';
 import { createTimerange } from './create_timerange';
 
 describe('createTimerange(interval, aggType, timeframe)', () => {
-  describe('without timeframe', () => {
+  describe('with current timeframe', () => {
+    const end = moment();
+    const timeframe = {
+      start: end.clone().toISOString(),
+      end: end.toISOString(),
+    };
     describe('Basic Metric Aggs', () => {
       it('should return a second range for last 1 second', () => {
-        const subject = createTimerange(1000, Aggregators.COUNT);
+        const subject = createTimerange(1000, Aggregators.COUNT, timeframe);
         expect(subject.end - subject.start).toEqual(1000);
       });
       it('should return a minute range for last 1 minute', () => {
-        const subject = createTimerange(60000, Aggregators.COUNT);
+        const subject = createTimerange(60000, Aggregators.COUNT, timeframe);
         expect(subject.end - subject.start).toEqual(60000);
       });
       it('should return 5 minute range for last 5 minutes', () => {
-        const subject = createTimerange(300000, Aggregators.COUNT);
+        const subject = createTimerange(300000, Aggregators.COUNT, timeframe);
         expect(subject.end - subject.start).toEqual(300000);
       });
       it('should return a hour range for last 1 hour', () => {
-        const subject = createTimerange(3600000, Aggregators.COUNT);
+        const subject = createTimerange(3600000, Aggregators.COUNT, timeframe);
         expect(subject.end - subject.start).toEqual(3600000);
       });
       it('should return a day range for last 1 day', () => {
-        const subject = createTimerange(86400000, Aggregators.COUNT);
+        const subject = createTimerange(86400000, Aggregators.COUNT, timeframe);
         expect(subject.end - subject.start).toEqual(86400000);
       });
     });
     describe('Rate Aggs', () => {
       it('should return a 20 second range for last 1 second', () => {
-        const subject = createTimerange(1000, Aggregators.RATE);
+        const subject = createTimerange(1000, Aggregators.RATE, timeframe);
         expect(subject.end - subject.start).toEqual(1000 * 2);
       });
       it('should return a 5 minute range for last 1 minute', () => {
-        const subject = createTimerange(60000, Aggregators.RATE);
+        const subject = createTimerange(60000, Aggregators.RATE, timeframe);
         expect(subject.end - subject.start).toEqual(60000 * 2);
       });
       it('should return 25 minute range for last 5 minutes', () => {
-        const subject = createTimerange(300000, Aggregators.RATE);
+        const subject = createTimerange(300000, Aggregators.RATE, timeframe);
         expect(subject.end - subject.start).toEqual(300000 * 2);
       });
       it('should return 5 hour range for last hour', () => {
-        const subject = createTimerange(3600000, Aggregators.RATE);
+        const subject = createTimerange(3600000, Aggregators.RATE, timeframe);
         expect(subject.end - subject.start).toEqual(3600000 * 2);
       });
       it('should return a 5 day range for last day', () => {
-        const subject = createTimerange(86400000, Aggregators.RATE);
+        const subject = createTimerange(86400000, Aggregators.RATE, timeframe);
         expect(subject.end - subject.start).toEqual(86400000 * 2);
       });
     });
   });
   describe('with full timeframe', () => {
     describe('Basic Metric Aggs', () => {
-      it('should return 5 minute range when given 4 minute timeframe', () => {
+      it('should return 9 minute range when given 4 minute timeframe', () => {
         const end = moment();
         const timeframe = {
-          start: end.clone().subtract(4, 'minutes').valueOf(),
-          end: end.valueOf(),
+          start: end.clone().subtract(4, 'minutes').toISOString(),
+          end: end.toISOString(),
         };
         const subject = createTimerange(300000, Aggregators.COUNT, timeframe);
-        expect(subject.end - subject.start).toEqual(300000);
+        expect(subject.end - subject.start).toEqual(540000);
       });
-      it('should return 6 minute range when given 6 minute timeframe', () => {
+      it('should return 11 minute range when given 6 minute timeframe', () => {
         const end = moment();
         const timeframe = {
-          start: end.clone().subtract(6, 'minutes').valueOf(),
-          end: end.valueOf(),
+          start: end.clone().subtract(6, 'minutes').toISOString(),
+          end: end.toISOString(),
         };
         const subject = createTimerange(300000, Aggregators.COUNT, timeframe);
-        expect(subject.end - subject.start).toEqual(360000);
+        expect(subject.end - subject.start).toEqual(660000);
       });
     });
     describe('Rate Aggs', () => {
-      it('should return 8 minute range when given 4 minute timeframe', () => {
+      it('should return 14 minute range when given 4 minute timeframe', () => {
         const end = moment();
         const timeframe = {
-          start: end.clone().subtract(4, 'minutes').valueOf(),
-          end: end.valueOf(),
+          start: end.clone().subtract(4, 'minutes').toISOString(),
+          end: end.toISOString(),
         };
         const subject = createTimerange(300000, Aggregators.RATE, timeframe);
-        expect(subject.end - subject.start).toEqual(300000 * 2);
+        expect(subject.end - subject.start).toEqual(840000);
       });
-      it('should return 12 minute range when given 6 minute timeframe', () => {
+      it('should return 16 minute range when given 6 minute timeframe', () => {
         const end = moment();
         const timeframe = {
-          start: end.clone().subtract(6, 'minutes').valueOf(),
-          end: end.valueOf(),
+          start: end.clone().subtract(6, 'minutes').toISOString(),
+          end: end.toISOString(),
         };
         const subject = createTimerange(300000, Aggregators.RATE, timeframe);
-        expect(subject.end - subject.start).toEqual(300000 * 2);
+        expect(subject.end - subject.start).toEqual(960000);
       });
     });
   });
-  describe('with partial timeframe', () => {
-    describe('Basic Metric Aggs', () => {
-      it('should return 5 minute range for last 5 minutes', () => {
-        const end = moment();
-        const timeframe = {
-          end: end.valueOf(),
-        };
-        const subject = createTimerange(300000, Aggregators.AVERAGE, timeframe);
-        expect(subject).toEqual({
-          start: end.clone().subtract(5, 'minutes').valueOf(),
-          end: end.valueOf(),
-        });
-      });
+  describe('With lastPeriodEnd', () => {
+    const end = moment();
+    const timeframe = {
+      start: end.clone().toISOString(),
+      end: end.toISOString(),
+    };
+    it('should return a minute and 1 second range for last 1 second when the lastPeriodEnd is less than the timeframe start', () => {
+      const subject = createTimerange(
+        1000,
+        Aggregators.COUNT,
+        timeframe,
+        end.clone().subtract(1, 'minutes').valueOf()
+      );
+      expect(subject.end - subject.start).toEqual(61000);
     });
-    describe('Rate Aggs', () => {
-      it('should return 10 minute range for last 5 minutes', () => {
-        const end = moment();
-        const timeframe = {
-          end: end.valueOf(),
-        };
-        const subject = createTimerange(300000, Aggregators.RATE, timeframe);
-        expect(subject).toEqual({
-          start: end
-            .clone()
-            .subtract(300 * 2, 'seconds')
-            .valueOf(),
-          end: end.valueOf(),
-        });
+    it('should return a second range for last 1 second when the lastPeriodEnd is not less than the timeframe start', () => {
+      const subject = createTimerange(
+        1000,
+        Aggregators.COUNT,
+        timeframe,
+        end.clone().add(2, 'seconds').valueOf()
+      );
+      expect(subject.end - subject.start).toEqual(1000);
+    });
+  });
+
+  describe('Rate Aggs', () => {
+    it('should return 10 minute range for last 5 minutes', () => {
+      const end = moment();
+      const timeframe = {
+        start: end.clone().toISOString(),
+        end: end.toISOString(),
+      };
+      const subject = createTimerange(300000, Aggregators.RATE, timeframe);
+      expect(subject).toEqual({
+        start: end
+          .clone()
+          .subtract(300 * 2, 'seconds')
+          .valueOf(),
+        end: end.valueOf(),
       });
     });
   });
