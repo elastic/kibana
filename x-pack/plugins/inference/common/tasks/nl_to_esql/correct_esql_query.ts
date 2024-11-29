71d646e845deb23bc9a690bd8d5ac120e31743bf
pgayvallet <pierre.gayvallet@elastic.co>
2024-11-29 09:37:36 +0100

    add unit tests

--- x-pack/plugins/inference/common/tasks/nl_to_esql/correct_esql_query.ts
+++ x-pack/plugins/inference/common/tasks/nl_to_esql/correct_esql_query.ts
@@ -8,6 +8,13 @@
 import { correctQueryWithAst } from './ast';
 import { correctCommonEsqlMistakes as correctQueryWithoutAst } from './non_ast';
 
+/**
+ * Correct some common ES|QL syntax and grammar mistakes that LLM can potentially do.
+ *
+ * Correcting the query is done in two steps:
+ * 1. we try to correct the *syntax*, without AST (given it requires a valid syntax)
+ * 2. we try to correct the *grammar*, using AST this time.
+ */
 export const correctCommonEsqlMistakes = (query: string) => {
   const { output: outputWithoutAst, isCorrection: correctionWithoutAst } =
     correctQueryWithoutAst(query);
