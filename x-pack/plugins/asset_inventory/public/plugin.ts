c084342a49d9c3d868f4cef72bc90c4a98cb9ee3
Alberto Blázquez <alberto.blazquez@elastic.co>
2024-11-29 18:35:49 +0100

    Add copyright section above all source files

--- x-pack/plugins/asset_inventory/public/plugin.ts
+++ x-pack/plugins/asset_inventory/public/plugin.ts
@@ -1,3 +1,9 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
 import { i18n } from '@kbn/i18n';
 import type { AppMountParameters, CoreSetup, CoreStart, Plugin } from '@kbn/core/public';
 import type {
