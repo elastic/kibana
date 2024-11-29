6ee7c256d14d8cf8a564b92b51005fe0389f96ab
Davis McPhee <davis.mcphee@elastic.co>
2024-11-29 02:07:23 -0400

    Update docs and UA copy

--- src/plugins/data_views/server/deprecations/scripted_fields.ts
+++ src/plugins/data_views/server/deprecations/scripted_fields.ts
@@ -46,15 +46,16 @@ export const createScriptedFieldsDeprecationsConfig: (
             defaultMessage: 'Found data views using scripted fields',
           }),
           message: i18n.translate('dataViews.deprecations.scriptedFieldsMessage', {
-            defaultMessage: `You have {numberOfIndexPatternsWithScriptedFields} data views ({titlesPreview}...) that use scripted fields. Scripted fields are deprecated and will be removed in future. Use runtime fields instead.`,
+            defaultMessage: `You have {numberOfIndexPatternsWithScriptedFields} {numberOfIndexPatternsWithScriptedFields, plural, one {data view} other {data views}} ({titlesPreview}{ellipsis}) containing scripted fields. Scripted fields are deprecated and will be removed in the future. The ability to create new scripted fields in the Data Views management page has been disabled in 9.0, and it is recommended to migrate to runtime fields or the Elasticsearch Query Language (ES|QL) instead.`,
             values: {
               titlesPreview: indexPatternTitles.slice(0, PREVIEW_LIMIT).join('; '),
               numberOfIndexPatternsWithScriptedFields: indexPatternsWithScriptedFields.length,
+              ellipsis: indexPatternTitles.length > PREVIEW_LIMIT ? '...' : '',
             },
           }),
-          documentationUrl:
-            'https://www.elastic.co/guide/en/elasticsearch/reference/7.x/runtime.html', // TODO: documentation service is not available serverside https://github.com/elastic/kibana/issues/95389
-          level: 'warning', // warning because it is not set in stone WHEN we remove scripted fields, hence this deprecation is not a blocker for 8.0 upgrade
+          documentationUrl: core.docLinks.links.runtimeFields.overview,
+          deprecationType: 'feature',
+          level: 'warning', // warning because it is not set in stone WHEN we remove scripted fields, hence this deprecation is not a blocker for 9.0 upgrade
           correctiveActions: {
             manualSteps: [
               i18n.translate('dataViews.deprecations.scriptedFields.manualStepOneMessage', {
@@ -62,12 +63,16 @@ export const createScriptedFieldsDeprecationsConfig: (
               }),
               i18n.translate('dataViews.deprecations.scriptedFields.manualStepTwoMessage', {
                 defaultMessage:
-                  'Update {numberOfIndexPatternsWithScriptedFields} data views that have scripted fields to use runtime fields instead. In most cases, to migrate existing scripts, you will need to change "return <value>;" to "emit(<value>);". Data views with at least one scripted field: {allTitles}',
+                  'Update data views that have scripted fields to use runtime fields instead. In most cases, to migrate existing scripts, you will need to change "return <value>;" to "emit(<value>);". Data views with at least one scripted field: {allTitles}.',
                 values: {
                   allTitles: indexPatternTitles.join('; '),
                   numberOfIndexPatternsWithScriptedFields: indexPatternsWithScriptedFields.length,
                 },
                 ignoreTag: true,
+              }),
+              i18n.translate('dataViews.deprecations.scriptedFields.manualStepThreeMessage', {
+                defaultMessage:
+                  'Alternatively, you can achieve similar functionality by computing values at query time using the Elasticsearch Query Language (ES|QL).',
               }),
             ],
           },
