7ccae6a129c06007ee5d0ebecba553591c912479
Karen Grigoryan <karen.grigoryan@elastic.co>
2024-12-02 23:27:23 +0100

    [React@18] Improve console.error suppression in react-testing-library setup (#201142)

--- packages/kbn-test/src/jest/setup/react_testing_library.js
+++ packages/kbn-test/src/jest/setup/react_testing_library.js
@@ -34,6 +34,11 @@ jest.mock('@testing-library/react', () => {
   };
 });
 
+const consoleFilters = [
+  /^The above error occurred in the <.*?> component:/, // error boundary output
+  /^Error: Uncaught .+/, // jsdom output
+];
+
 // This is a workaround to run tests with React 17 and the latest @testing-library/react
 // And prevent the act warnings that were supposed to be muted by @testing-library
 // The testing library mutes the act warnings in some cases by setting IS_REACT_ACT_ENVIRONMENT which is React@18 feature https://github.com/testing-library/react-testing-library/pull/1137/
@@ -42,12 +47,25 @@ jest.mock('@testing-library/react', () => {
 // NOTE: we're not muting all the act warnings but only those that testing-library wanted to mute
 const originalConsoleError = console.error;
 console.error = (...args) => {
+  const message = args[0]?.toString();
+
   if (global.IS_REACT_ACT_ENVIRONMENT === false) {
-    if (
-      args[0].toString().includes('Warning: An update to %s inside a test was not wrapped in act')
-    ) {
+    if (message.includes('Warning: An update to %s inside a test was not wrapped in act')) {
       return;
     }
+  }
+
+  // Additionally this is a restoration of the original console.error suppression
+  // expected by the usage of renderHook from react-hooks-testing-library
+  // which has been moved into latest react-testing-library but the suppression
+  // of the console.error was not moved with it.
+  //
+  // So adding by example from the original implementation:
+  // https://github.com/testing-library/react-hooks-testing-library/blob/1e01273374af4e48a0feb1f2233bf6c76d742167/src/core/console.ts
+  // with a slight modification to catch non-string errors as well
+
+  if (message && consoleFilters.some((filter) => filter.test(message))) {
+    return;
   }
 
   originalConsoleError(...args);
