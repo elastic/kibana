9a7ace8af091ae6c54b1cbf1317a6934f1d3b9f1
Maxim Palenov <maxim.palenov@elastic.co>
2024-12-03 08:42:40 +0100

    fix translations

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/comparison_side/versions_picker/translations.ts
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/comparison_side/versions_picker/translations.ts
@@ -8,98 +8,45 @@
 import { i18n } from '@kbn/i18n';
 
 export const BASE_VERSION = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.baseVersionLabel',
+  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versions.baseVersionLabel',
   {
     defaultMessage: 'Original',
   }
 );
 
 export const CURRENT_VERSION = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.currentVersionLabel',
+  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versions.currentVersionLabel',
   {
     defaultMessage: 'Current',
   }
 );
 
 export const TARGET_VERSION = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.targetVersionLabel',
+  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versions.targetVersionLabel',
   {
     defaultMessage: 'Elastic update',
   }
 );
 
 export const FINAL_VERSION = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.finalVersionLabel',
+  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versions.finalVersionLabel',
   {
     defaultMessage: 'Final',
   }
 );
 
-export const BASE_VS_TARGET = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.baseVsTargetLabel',
-  {
-    defaultMessage: '{base} vs {target}',
-    values: {
-      base: BASE_VERSION,
-      target: TARGET_VERSION,
-    },
-  }
-);
-
-export const BASE_VS_CURRENT = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.baseVsCurrentLabel',
-  {
-    defaultMessage: '{base} vs {current}',
-    values: {
-      base: BASE_VERSION,
-      current: CURRENT_VERSION,
-    },
-  }
-);
-
-export const BASE_VS_FINAL = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.baseVsFinalLabel',
-  {
-    defaultMessage: '{base} vs {final}',
-    values: {
-      base: BASE_VERSION,
-      final: FINAL_VERSION,
-    },
-  }
-);
-
-export const CURRENT_VS_TARGET = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.currentVsTargetLabel',
-  {
-    defaultMessage: '{current} vs {target}',
-    values: {
-      current: CURRENT_VERSION,
-      target: TARGET_VERSION,
-    },
-  }
-);
-
-export const CURRENT_VS_FINAL = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.currentVsFinalLabel',
-  {
-    defaultMessage: '{current} vs {final}',
-    values: {
-      current: CURRENT_VERSION,
-      final: FINAL_VERSION,
-    },
-  }
-);
-
-export const TARGET_VS_FINAL = i18n.translate(
-  'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.targetVsFinalLabel',
-  {
-    defaultMessage: '{target} vs {final}',
-    values: {
-      target: TARGET_VERSION,
-      final: FINAL_VERSION,
-    },
-  }
-);
+export const VERSION1_VS_VERSION2 = (version1: string, version2: string): string => {
+  return i18n.translate(
+    'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.version1VsVersion2',
+    {
+      defaultMessage: '{version1} vs {version2}',
+      values: {
+        version1,
+        version2,
+      },
+    }
+  );
+};
 
 export const VERSION_PICKER_ARIA_LABEL = i18n.translate(
   'xpack.securitySolution.detectionEngine.rules.upgradeRules.versionsPicker.ariaLabel',
