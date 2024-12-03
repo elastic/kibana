d5a4a9bc5bd500b2eab1b051fe28ac6479d03cbf
Julia Rechkunova <julia.rechkunova@elastic.co>
2024-12-03 12:03:08 +0100

    [Discover] Remove the legacy table (#201254)
    - Closes https://github.com/elastic/kibana/issues/167582
    
    ## Summary
    
    This PR removes the code related to the legacy doc table and 2 Advanced
    Settings: `doc_table:legacy` and `truncate:maxHeight`.
    
    The legacy table in Discover was replaced by the new data grid in v8.3.
    The `doc_table:legacy` Advanced Setting was added to let users switch
    back to the legacy table if necessary. The removal of the setting and
    the legacy table entirely would allow us to reduce bundle size,
    maintenance burden, and code complexity.
    
    Also the legacy table does not support many new features which were
    added to the grid only (e.g. comparing selected documents, context-aware
    UI based on current solution project, column resizing, bulk row
    selection, copy actions, new doc viewer flyout, and more).
    
    Since v8.15 `doc_table:legacy` is marked as deprecated on Advanced
    Settings page via https://github.com/elastic/kibana/issues/179899
    
    Since v8.16 `truncate:maxHeight` is marked as deprecated too via
    https://github.com/elastic/kibana/pull/183736
    
    The removal of these 2 settings and the associated code is planned for
    v9.
    
    ### Checklist
    
    - [x]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/plugins/unified_doc_viewer/public/components/doc_viewer_table/table_cell_value.test.tsx
+++ src/plugins/unified_doc_viewer/public/components/doc_viewer_table/table_cell_value.test.tsx
@@ -9,23 +9,9 @@
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { TRUNCATE_MAX_HEIGHT, TRUNCATE_MAX_HEIGHT_DEFAULT_VALUE } from '@kbn/discover-utils';
-import type { IUiSettingsClient } from '@kbn/core-ui-settings-browser';
 import { TableFieldValue } from './table_cell_value';
 import { setUnifiedDocViewerServices } from '../../plugin';
 import { mockUnifiedDocViewerServices } from '../../__mocks__';
-
-const mockServices = {
-  ...mockUnifiedDocViewerServices,
-};
-
-let mockTruncateMaxHeightSetting: number | undefined;
-mockServices.uiSettings.get = ((key: string) => {
-  if (key === TRUNCATE_MAX_HEIGHT) {
-    return mockTruncateMaxHeightSetting ?? TRUNCATE_MAX_HEIGHT_DEFAULT_VALUE;
-  }
-  return;
-}) as IUiSettingsClient['get'];
 
 setUnifiedDocViewerServices(mockUnifiedDocViewerServices);
 
@@ -35,7 +21,6 @@ jest.spyOn(HTMLElement.prototype, 'scrollHeight', 'get').mockImplementation(() =
 describe('TableFieldValue', () => {
   afterEach(() => {
     mockScrollHeight = 0;
-    mockTruncateMaxHeightSetting = undefined;
   });
 
   it('should render correctly', async () => {
@@ -111,99 +96,6 @@ describe('TableFieldValue', () => {
         field="message"
         ignoreReason={undefined}
         isDetails={true}
-      />
-    );
-
-    expect(screen.getByText(value)).toBeInTheDocument();
-    expect(screen.queryByTestId('toggleLongFieldValue-message')).toBeNull();
-
-    const valueElement = screen.getByTestId('tableDocViewRow-message-value');
-    expect(valueElement.getAttribute('css')).toBeNull();
-    expect(valueElement.classList.contains('kbnDocViewer__value--truncated')).toBe(false);
-  });
-
-  it('should truncate a long value in legacy table correctly', async () => {
-    mockScrollHeight = 1000;
-
-    const value = 'long value'.repeat(300);
-    render(
-      <TableFieldValue
-        formattedValue={value}
-        rawValue={value}
-        field="message"
-        ignoreReason={undefined}
-        isDetails={false}
-        isLegacy={true}
-      />
-    );
-
-    expect(screen.getByText(value)).toBeInTheDocument();
-
-    let toggleButton = screen.getByTestId('toggleLongFieldValue-message');
-    expect(toggleButton).toBeInTheDocument();
-    expect(toggleButton.getAttribute('aria-expanded')).toBe('false');
-
-    let valueElement = screen.getByTestId('tableDocViewRow-message-value');
-    expect(valueElement.getAttribute('css')).toBeDefined();
-    expect(valueElement.classList.contains('kbnDocViewer__value--truncated')).toBe(true);
-
-    toggleButton.click();
-
-    toggleButton = screen.getByTestId('toggleLongFieldValue-message');
-    expect(toggleButton).toBeInTheDocument();
-    expect(toggleButton.getAttribute('aria-expanded')).toBe('true');
-
-    valueElement = screen.getByTestId('tableDocViewRow-message-value');
-    expect(valueElement.getAttribute('css')).toBeNull();
-    expect(valueElement.classList.contains('kbnDocViewer__value--truncated')).toBe(false);
-
-    toggleButton.click();
-
-    toggleButton = screen.getByTestId('toggleLongFieldValue-message');
-    expect(toggleButton).toBeInTheDocument();
-    expect(toggleButton.getAttribute('aria-expanded')).toBe('false');
-
-    valueElement = screen.getByTestId('tableDocViewRow-message-value');
-    expect(valueElement.getAttribute('css')).toBeDefined();
-    expect(valueElement.classList.contains('kbnDocViewer__value--truncated')).toBe(true);
-  });
-
-  it('should not truncate a long value in legacy table if limit is not reached', async () => {
-    mockScrollHeight = 112;
-
-    const value = 'long value'.repeat(300);
-    render(
-      <TableFieldValue
-        formattedValue={value}
-        rawValue={value}
-        field="message"
-        ignoreReason={undefined}
-        isDetails={true}
-        isLegacy={true}
-      />
-    );
-
-    expect(screen.getByText(value)).toBeInTheDocument();
-    expect(screen.queryByTestId('toggleLongFieldValue-message')).toBeNull();
-
-    const valueElement = screen.getByTestId('tableDocViewRow-message-value');
-    expect(valueElement.getAttribute('css')).toBeNull();
-    expect(valueElement.classList.contains('kbnDocViewer__value--truncated')).toBe(false);
-  });
-
-  it('should not truncate a long value in legacy table if setting is 0', async () => {
-    mockScrollHeight = 1000;
-    mockTruncateMaxHeightSetting = 0;
-
-    const value = 'long value'.repeat(300);
-    render(
-      <TableFieldValue
-        formattedValue={value}
-        rawValue={value}
-        field="message"
-        ignoreReason={undefined}
-        isDetails={true}
-        isLegacy={true}
       />
     );
 
