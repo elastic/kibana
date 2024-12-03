2a1610e092000e47aa91622bde89a06846cca855
Joey F. Poon <joey.poon@elastic.co>
2024-12-03 23:53:07 +0900

    create workflow insight from defend insight

---
+++ x-pack/plugins/security_solution/server/assistant/tools/defend_insights/workflow_insights_builders/index.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { KibanaRequest } from '@kbn/core/server';
+import type { DefendInsightsPostRequestBody } from '@kbn/elastic-assistant-common';
+
+import { DefendInsight, DefendInsightType } from '@kbn/elastic-assistant-common';
+
+import type { SecurityWorkflowInsight } from '../../../../../common/endpoint/types/workflow_insights';
+
+import { InvalidDefendInsightTypeError } from '../errors';
+import { buildIncompatibleAntivirusWorkflowInsight } from './incompatible_antivirus';
+
+export interface BuildWorkflowInsightParams {
+  defendInsights: DefendInsight[];
+  request: KibanaRequest<unknown, unknown, DefendInsightsPostRequestBody>;
+}
+
+export function buildWorkflowInsights(
+  params: BuildWorkflowInsightParams
+): SecurityWorkflowInsight[] {
+  if (params.request.body.insightType == DefendInsightType.Enum.incompatible_antivirus) {
+    return buildIncompatibleAntivirusWorkflowInsight(params);
+  }
+
+  throw new InvalidDefendInsightTypeError();
+}
