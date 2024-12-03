e0fd1ac795ff24f727c7ea849fc10a966b6ce44c
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-03 23:51:22 +0100

    Moving modules owned by `@elastic/observability-ui`

--- x-pack/solutions/observability/packages/observability/observability_utils/observability_utils_server/jest.config.js
+++ x-pack/solutions/observability/packages/observability/observability_utils/observability_utils_server/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../..',
-  roots: ['<rootDir>/x-pack/packages/observability/observability_utils/observability_utils_common'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/x-pack/solutions/observability/solutions/observability/packages/observability/observability_utils/observability_utils_server'],
 };
