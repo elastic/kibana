ed0823e5b5da8327b7f358176efd74734de0d148
Nathan Reese <reese.nathan@gmail.com>
2024-12-04 13:55:38 -0700

    remove embeddable story book

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -28,7 +28,6 @@ export const storybookAliases = {
   dashboard: 'src/plugins/dashboard/.storybook',
   data: 'src/plugins/data/.storybook',
   discover: 'src/plugins/discover/.storybook',
-  embeddable: 'src/plugins/embeddable/.storybook',
   esql_ast_inspector: 'examples/esql_ast_inspector/.storybook',
   es_ui_shared: 'src/plugins/es_ui_shared/.storybook',
   expandable_flyout: 'packages/kbn-expandable-flyout/.storybook',
