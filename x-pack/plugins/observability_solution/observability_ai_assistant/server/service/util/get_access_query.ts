90df2f779a7bbb2a55ced450d0906857d0bb9759
Arturo Liduena <arturo.liduena@elastic.co>
2024-12-04 10:10:20 +0100

    Refactor access query logic to use a dedicated function for user access filters

--- x-pack/plugins/observability_solution/observability_ai_assistant/server/service/util/get_access_query.ts
+++ x-pack/plugins/observability_solution/observability_ai_assistant/server/service/util/get_access_query.ts
@@ -17,22 +17,7 @@ export function getAccessQuery({
         filter: [
           {
             bool: {
-              should: [
-                { term: { public: true } },
-                ...(user
-                  ? user.id
-                    ? [
-                        { term: { 'user.id': user.id } },
-                        {
-                          bool: {
-                            must_not: { exists: { field: 'user.id' } },
-                            must: { term: { 'user.name': user.name } },
-                          },
-                        },
-                      ]
-                    : [{ term: { 'user.name': user.name } }]
-                  : []),
-              ],
+              should: [{ term: { public: true } }, ...getUserAccessFilters(user)],
               minimum_should_match: 1,
             },
           },
@@ -60,4 +45,24 @@ export function getAccessQuery({
       },
     },
   ];
+}
+
+function getUserAccessFilters(user?: { name: string; id?: string }) {
+  if (!user) {
+    return [];
+  }
+
+  if (user.id) {
+    return [
+      { term: { 'user.id': user.id } },
+      {
+        bool: {
+          must_not: { exists: { field: 'user.id' } },
+          must: { term: { 'user.name': user.name } },
+        },
+      },
+    ];
+  }
+
+  return [{ term: { 'user.name': user.name } }];
 }
