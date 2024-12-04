cd368ee9744508c907796513850fb3cb1d7badbd
Davis Plumlee <56367316+dplumlee@users.noreply.github.com>
2024-12-04 13:17:55 -0500

    [Security Solution] Disables `author` and `license` fields in rule edit form for prebuilt rule types (#201887)
    ## Summary
    
    Fixes https://github.com/elastic/kibana/issues/200251
    
    > [!NOTE]  
    > This bug/related fix is only visible with the
    `prebuiltRulesCustomizationEnabled` feature flag turned on.
    
    Disables `author` and `license` fields in rule edit form for prebuilt
    rule types as we throw API errors when they are changed from the
    existing rule value if the rule source is external.
    
    
    ### Screenshots - the same prebuilt rule in the Rule edit form
    **Before**
    <img width="738" alt="Screenshot 2024-11-26 at 5 32 00 PM"
    src="https://github.com/user-attachments/assets/6262cdb2-750a-47fb-b6b8-ec07f4acd8aa">
    
    
    **After**
    ![Screenshot 2024-12-03 at 3 22
    34 PM](https://github.com/user-attachments/assets/bfb4c468-3ea2-4fa0-bd36-a90c32eacce4)
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios

--- x-pack/plugins/security_solution/public/detection_engine/rule_creation_ui/pages/rule_editing/index.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_creation_ui/pages/rule_editing/index.tsx
@@ -278,6 +278,7 @@ const EditRulePageComponent: FC<{ rule: RuleResponse }> = ({ rule }) => {
                   form={aboutStepForm}
                   esqlQuery={esqlQueryForAboutStep}
                   key="aboutStep"
+                  ruleSource={rule.rule_source}
                 />
               )}
               <EuiSpacer />
@@ -343,6 +344,7 @@ const EditRulePageComponent: FC<{ rule: RuleResponse }> = ({ rule }) => {
     [
       isPrebuiltRulesCustomizationEnabled,
       rule?.immutable,
+      rule.rule_source,
       rule?.id,
       activeStep,
       loading,
