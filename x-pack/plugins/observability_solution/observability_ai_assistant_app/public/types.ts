7766631ad641ba94e9736c6b19b0f7ce9cb15da8
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2024-12-04 14:44:24 -0500

    [Obs AI Assistant] Remove unused dependancies

--- x-pack/plugins/observability_solution/observability_ai_assistant_app/public/types.ts
+++ x-pack/plugins/observability_solution/observability_ai_assistant_app/public/types.ts
@@ -31,7 +31,6 @@ import type {
   AIAssistantManagementSelectionPluginPublicStart,
   AIAssistantManagementSelectionPluginPublicSetup,
 } from '@kbn/ai-assistant-management-plugin/public';
-import { ServerlessPluginStart } from '@kbn/serverless/public';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface ObservabilityAIAssistantAppPublicStart {}
@@ -50,7 +49,6 @@ export interface ObservabilityAIAssistantAppPluginStartDependencies {
   triggersActionsUi: TriggersAndActionsUIPublicPluginStart;
   data: DataPublicPluginStart;
   aiAssistantManagementSelection: AIAssistantManagementSelectionPluginPublicStart;
-  serverless?: ServerlessPluginStart;
 }
 
 export interface ObservabilityAIAssistantAppPluginSetupDependencies {
@@ -64,5 +62,4 @@ export interface ObservabilityAIAssistantAppPluginSetupDependencies {
   ml: MlPluginSetup;
   triggersActionsUi: TriggersAndActionsUIPublicPluginSetup;
   aiAssistantManagementSelection: AIAssistantManagementSelectionPluginPublicSetup;
-  serverless?: ServerlessPluginStart;
 }
