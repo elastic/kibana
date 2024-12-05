bea0b0b766eb247707693abf74e2759e7360e7e0
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 16:47:37 +0100

    Relocating module `@kbn/deeplinks-ml`

--- src/platform/packages/shared/deeplinks/ml/jest.config.js
+++ src/platform/packages/shared/deeplinks/ml/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/deeplinks/ml'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/deeplinks/ml'],
 };
