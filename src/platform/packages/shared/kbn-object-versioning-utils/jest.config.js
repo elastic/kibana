d3d7adea9bce0f6d33b8133026d9b03b078cd938
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 11:45:50 +0100

    Relocating module `@kbn/object-versioning-utils`

--- src/platform/packages/shared/kbn-object-versioning-utils/jest.config.js
+++ src/platform/packages/shared/kbn-object-versioning-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-object-versioning-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-object-versioning-utils'],
 };
