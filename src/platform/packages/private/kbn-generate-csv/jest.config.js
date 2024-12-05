683040e584d9497c268cb17532b0ad4390b401cd
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 09:29:45 +0100

    Relocating module `@kbn/generate-csv`

--- src/platform/packages/private/kbn-generate-csv/jest.config.js
+++ src/platform/packages/private/kbn-generate-csv/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-generate-csv/'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-generate-csv/'],
 };
