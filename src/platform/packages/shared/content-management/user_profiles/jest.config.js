2c87ac8b4f69fb1f3c221fdb246f712074aad63f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 09:09:16 +0100

    Relocating module `@kbn/content-management-user-profiles`

--- src/platform/packages/shared/content-management/user_profiles/jest.config.js
+++ src/platform/packages/shared/content-management/user_profiles/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/content-management/user_profiles'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/content-management/user_profiles'],
 };
