05aa3df69e007f393e4b58d222bbe146f03290b0
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 16:03:09 +0100

    Relocating module `@kbn/url-forwarding-plugin`

--- src/platform/plugins/private/url_forwarding/jest.config.js
+++ src/platform/plugins/private/url_forwarding/jest.config.js
@@ -9,9 +9,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/src/plugins/url_forwarding'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/url_forwarding',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/private/url_forwarding'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/private/url_forwarding',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/src/plugins/url_forwarding/public/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/platform/plugins/private/url_forwarding/public/**/*.{ts,tsx}'],
 };
