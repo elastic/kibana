3696cde110b0538bb1bf1dbd4c0a45030569d937
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 09:33:57 +0100

    Relocating module `@kbn/global-search-bar-plugin`

--- x-pack/platform/plugins/private/global_search_bar/jest.config.js
+++ x-pack/platform/plugins/private/global_search_bar/jest.config.js
@@ -7,9 +7,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/global_search_bar'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/global_search_bar',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/plugins/private/global_search_bar'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/private/global_search_bar',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/global_search_bar/public/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/private/global_search_bar/public/**/*.{ts,tsx}'],
 };
