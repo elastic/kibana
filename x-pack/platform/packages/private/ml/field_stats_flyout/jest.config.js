f9e20474def30b857c6d09e61f5c1077041c9eaf
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:11:02 +0100

    Relocating module `@kbn/ml-field-stats-flyout`

--- x-pack/platform/packages/private/ml/field_stats_flyout/jest.config.js
+++ x-pack/platform/packages/private/ml/field_stats_flyout/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/field_stats_flyout'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/field_stats_flyout'],
 };
