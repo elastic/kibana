fc323a36d4bc311d38b9f7488b66fb69c5e51ee4
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 11:27:41 +0100

    Relocating module `@kbn/navigation-plugin`

--- src/platform/plugins/shared/navigation/jest.config.js
+++ src/platform/plugins/shared/navigation/jest.config.js
@@ -9,9 +9,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/src/plugins/navigation'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/navigation',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/shared/navigation'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/shared/navigation',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/src/plugins/navigation/public/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/platform/plugins/shared/navigation/public/**/*.{ts,tsx}'],
 };
