a663e003356a52a8af79f7c640ccb554780deae6
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 18:47:46 +0100

    Relocating module `@kbn/default-nav-management`

--- src/platform/packages/private/default-nav/management/jest.config.js
+++ src/platform/packages/private/default-nav/management/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/default-nav/management'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/default-nav/management'],
 };
