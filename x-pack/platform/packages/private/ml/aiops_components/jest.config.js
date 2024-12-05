ec9aa4f0b863c26d2038d24b6ccecf5d8d95fb6d
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 16:37:32 +0100

    Relocating module `@kbn/aiops-components`

--- x-pack/platform/packages/private/ml/aiops_components/jest.config.js
+++ x-pack/platform/packages/private/ml/aiops_components/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/aiops_components'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/aiops_components'],
 };
