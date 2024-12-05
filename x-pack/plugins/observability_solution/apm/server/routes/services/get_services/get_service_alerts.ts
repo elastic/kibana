efd88f6bfb415b92c46cbca58b6f08cbc36e41b2
iblancof <irene.blanco@elastic.co>
2024-12-05 14:12:14 +0100

    Update APM alerting rule consumer with apmAlertingConsumers

--- x-pack/plugins/observability_solution/apm/server/routes/services/get_services/get_service_alerts.ts
+++ x-pack/plugins/observability_solution/apm/server/routes/services/get_services/get_service_alerts.ts
@@ -18,7 +18,7 @@ import {
   ALERT_STATUS_ACTIVE,
   ALERT_UUID,
 } from '@kbn/rule-data-utils';
-import { observabilityFeatureId } from '@kbn/observability-shared-plugin/common';
+import { apmAlertingConsumers } from '../../../../common/alerting/config/apm_alerting_feature_ids';
 import { SERVICE_NAME } from '../../../../common/es_fields/apm';
 import { ServiceGroup } from '../../../../common/service_groups';
 import { ApmAlertsClient } from '../../../lib/helpers/get_apm_alerts_client';
@@ -58,7 +58,7 @@ export async function getServicesAlerts({
     query: {
       bool: {
         filter: [
-          ...termsQuery(ALERT_RULE_PRODUCER, 'apm', observabilityFeatureId),
+          ...termsQuery(ALERT_RULE_PRODUCER, ...apmAlertingConsumers),
           ...termQuery(ALERT_STATUS, ALERT_STATUS_ACTIVE),
           ...rangeQuery(start, end),
           ...kqlQuery(kuery),
@@ -85,6 +85,8 @@ export async function getServicesAlerts({
       },
     },
   };
+
+  console.log('APM - PARAMS', JSON.stringify(params));
 
   const result = await apmAlertsClient.search(params);
 
