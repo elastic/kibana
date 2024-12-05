e72a17d333c23463b0d7f09a363711f66b4cf5c1
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 19:46:21 +0100

    Relocating module `@kbn/dashboard-plugin`

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -25,7 +25,7 @@ export const storybookAliases = {
   custom_icons: 'packages/kbn-custom-icons/.storybook',
   custom_integrations: 'src/plugins/custom_integrations/storybook',
   dashboard_enhanced: 'x-pack/platform/plugins/shared/dashboard_enhanced/.storybook',
-  dashboard: 'src/plugins/dashboard/.storybook',
+  dashboard: 'src/platform/plugins/shared/dashboard/.storybook',
   data: 'src/plugins/data/.storybook',
   discover: 'src/plugins/discover/.storybook',
   embeddable: 'src/plugins/embeddable/.storybook',
