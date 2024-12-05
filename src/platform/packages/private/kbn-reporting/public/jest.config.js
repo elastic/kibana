d343fecbe94177fc92f0974b6db4787c70fc7995
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 13:11:34 +0100

    Relocating module `@kbn/reporting-public`

--- src/platform/packages/private/kbn-reporting/public/jest.config.js
+++ src/platform/packages/private/kbn-reporting/public/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/kbn-reporting/public'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-reporting/public'],
 };
