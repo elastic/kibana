2f54249c49514567ece439f7127b680f90cdd080
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 11:59:56 +0100

    Relocating module `@kbn/react-kibana-context-root`

--- src/platform/packages/private/react/kibana_context/root/jest.config.js
+++ src/platform/packages/private/react/kibana_context/root/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/react/kibana_context/root'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/react/kibana_context/root'],
 };
