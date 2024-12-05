5fd09a96996fb72cb684b853715f069ef79bc525
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 16:55:40 +0100

    Relocating module `@kbn/ml-anomaly-utils`

--- x-pack/platform/packages/shared/ml/anomaly_utils/jest.config.js
+++ x-pack/platform/packages/shared/ml/anomaly_utils/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/anomaly_utils'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/shared/ml/anomaly_utils'],
 };
