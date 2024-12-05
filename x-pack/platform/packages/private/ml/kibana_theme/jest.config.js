e937420af1de8360dbf63313fc4f2492c21345df
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:16:23 +0100

    Relocating module `@kbn/ml-kibana-theme`

--- x-pack/platform/packages/private/ml/kibana_theme/jest.config.js
+++ x-pack/platform/packages/private/ml/kibana_theme/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/kibana_theme'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/kibana_theme'],
 };
