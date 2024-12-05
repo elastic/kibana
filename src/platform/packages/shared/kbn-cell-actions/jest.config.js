94ee8e2754810dd0e7bd8cdda4889be9b5e80186
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 06:45:12 +0100

    Relocating module `@kbn/cell-actions`

--- src/platform/packages/shared/kbn-cell-actions/jest.config.js
+++ src/platform/packages/shared/kbn-cell-actions/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-cell-actions'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-cell-actions'],
 };
