d57d11de21158f361ed9ff3e17af8b4af2fc9044
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:07:07 +0100

    Relocating module `@kbn/ml-date-picker`

--- x-pack/platform/packages/private/ml/date_picker/jest.config.js
+++ x-pack/platform/packages/private/ml/date_picker/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/date_picker'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/date_picker'],
 };
