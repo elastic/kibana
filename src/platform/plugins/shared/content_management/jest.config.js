1287d029650b0df5922f4b05132241b961abfa0c
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 08:48:35 +0100

    Relocating module `@kbn/content-management-plugin`

--- src/platform/plugins/shared/content_management/jest.config.js
+++ src/platform/plugins/shared/content_management/jest.config.js
@@ -9,11 +9,11 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/src/plugins/content_management'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/content_management',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/shared/content_management'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/shared/content_management',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/src/plugins/content_management/{common,public,server}/**/*.{js,ts,tsx}',
+    '<rootDir>/src/platform/plugins/shared/content_management/{common,public,server}/**/*.{js,ts,tsx}',
   ],
 };
--- src/platform/plugins/shared/content_management/jest.integration.config.js
+++ src/platform/plugins/shared/content_management/jest.integration.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_integration',
-  rootDir: '../../..',
-  roots: ['<rootDir>/src/plugins/content_management'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/shared/content_management'],
 };
