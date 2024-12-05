c2d3622c6979567739ba0e832891d24ec8d914a6
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:40:02 +0100

    Relocating module `@kbn/ml-validators`

--- x-pack/platform/packages/private/ml/validators/jest.config.js
+++ x-pack/platform/packages/private/ml/validators/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/validators'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/validators'],
 };
