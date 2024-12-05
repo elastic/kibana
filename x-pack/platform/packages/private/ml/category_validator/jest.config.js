7a6ab7f9108671bcd58ee06673179c9e34f96fed
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 16:58:41 +0100

    Relocating module `@kbn/ml-category-validator`

--- x-pack/platform/packages/private/ml/category_validator/jest.config.js
+++ x-pack/platform/packages/private/ml/category_validator/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/category_validator'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/category_validator'],
 };
