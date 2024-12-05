37e0205d62b5df4d292cae214ee5852e838087ee
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:29:26 +0100

    Relocating module `@kbn/ml-route-utils`

--- x-pack/platform/packages/private/ml/route_utils/jest.config.js
+++ x-pack/platform/packages/private/ml/route_utils/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/route_utils'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/route_utils'],
 };
