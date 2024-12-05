987233ae970f8684226627869f8b61384ee6f5b9
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-12-05 14:52:52 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/plugins/security_solution/public/app/home/nav_controls/index.test.tsx
+++ x-pack/plugins/security_solution/public/app/home/nav_controls/index.test.tsx
@@ -1,111 +1,127 @@
-import React, { FC, PropsWithChildren } from 'react';
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { FC, PropsWithChildren } from 'react';
+import React from 'react';
 import { render, renderHook } from '@testing-library/react';
 import { NavControls } from '.';
-import { ChromeNavControl } from '@kbn/core/public';
+import type { ChromeNavControl } from '@kbn/core/public';
 import { createHtmlPortalNode, OutPortal } from 'react-reverse-portal';
 import { TestProviders } from '@kbn/elastic-assistant/impl/mock/test_providers/test_providers';
-import { useAssistantContext } from "@kbn/elastic-assistant/impl/assistant_context"
+import { useAssistantContext } from '@kbn/elastic-assistant/impl/assistant_context';
 
-const MockNavigationBar = OutPortal
+const MockNavigationBar = OutPortal;
 
 const mockShowAssistantOverlay = jest.fn();
-const mockRegisterRight = jest.fn()
+const mockRegisterRight = jest.fn();
 
 jest.mock('@kbn/elastic-assistant/impl/assistant_context', () => ({
-    ...jest.requireActual('@kbn/elastic-assistant/impl/assistant_context'),
-    useAssistantContext: jest.fn()
+  ...jest.requireActual('@kbn/elastic-assistant/impl/assistant_context'),
+  useAssistantContext: jest.fn(),
 }));
 
 jest.mock('@kbn/security-solution-navigation/src/context', () => ({
-    ...jest.requireActual('@kbn/security-solution-navigation/src/context'),
-    useNavigationContext: () => ({
-        chrome: {
-            navControls: {
-                registerRight: mockRegisterRight,
-            }
-        }
-    })
+  ...jest.requireActual('@kbn/security-solution-navigation/src/context'),
+  useNavigationContext: () => ({
+    chrome: {
+      navControls: {
+        registerRight: mockRegisterRight,
+      },
+    },
+  }),
 }));
 
-
 const ProviderWrapper: FC<PropsWithChildren> = ({ children }) => {
-    return <TestProviders>
-        {children}
-    </TestProviders>
-}
+  return <TestProviders>{children}</TestProviders>;
+};
 
 describe('NavControls', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
+  beforeEach(() => {
+    jest.clearAllMocks();
 
-        (useAssistantContext as jest.Mock).mockReturnValue({
-            showAssistantOverlay: mockShowAssistantOverlay,
-            assistantAvailability: {
-                hasAssistantPrivilege: true
-            }
-        })
+    (useAssistantContext as jest.Mock).mockReturnValue({
+      showAssistantOverlay: mockShowAssistantOverlay,
+      assistantAvailability: {
+        hasAssistantPrivilege: true,
+      },
+    });
+  });
+
+  it('should register link in nav bar', () => {
+    render(<NavControls />, { wrapper: ProviderWrapper });
+    expect(mockRegisterRight).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render the header link text', async () => {
+    const { result: portalNode } = renderHook(() =>
+      React.useMemo(() => createHtmlPortalNode(), [])
+    );
+
+    mockRegisterRight.mockImplementationOnce((chromeNavControl: ChromeNavControl) => {
+      chromeNavControl.mount(portalNode.current.element);
     });
 
-    it('should register link in nav bar', () => {
-        render(<NavControls />, { wrapper: ProviderWrapper });
-        expect(mockRegisterRight).toHaveBeenCalledTimes(1)
+    const { queryByText, queryByTestId } = render(
+      <>
+        <MockNavigationBar node={portalNode.current} />
+        <NavControls />
+      </>,
+      { wrapper: ProviderWrapper }
+    );
+
+    expect(queryByTestId('assistantNavLink')).toBeInTheDocument();
+    expect(queryByText('AI Assistant')).toBeInTheDocument();
+  });
+
+  it('should not render the header link if not authorized', () => {
+    const { result: portalNode } = renderHook(() =>
+      React.useMemo(() => createHtmlPortalNode(), [])
+    );
+
+    (useAssistantContext as jest.Mock).mockReturnValue({
+      showAssistantOverlay: mockShowAssistantOverlay,
+      assistantAvailability: {
+        hasAssistantPrivilege: false,
+      },
     });
 
-
-    it('should render the header link text', async () => {
-        const { result: portalNode } = renderHook(() => React.useMemo(() => createHtmlPortalNode(), []));
-
-        mockRegisterRight.mockImplementationOnce((chromeNavControl: ChromeNavControl) => {
-            chromeNavControl.mount(portalNode.current.element)
-        })
-
-        const { queryByText, queryByTestId } = render(
-            <>
-                <MockNavigationBar node={portalNode.current} />
-                <NavControls />
-            </>
-            , { wrapper: ProviderWrapper });
-
-        expect(queryByTestId('assistantNavLink')).toBeInTheDocument()
-        expect(queryByText('AI Assistant')).toBeInTheDocument();
+    mockRegisterRight.mockImplementationOnce((chromeNavControl: ChromeNavControl) => {
+      chromeNavControl.mount(portalNode.current.element);
     });
 
-    it('should not render the header link if not authorized', () => {
-        const { result: portalNode } = renderHook(() => React.useMemo(() => createHtmlPortalNode(), []));
+    const { queryByText, queryByTestId } = render(
+      <>
+        <MockNavigationBar node={portalNode.current} />
+        <NavControls />
+      </>,
+      { wrapper: ProviderWrapper }
+    );
+    expect(queryByTestId('assistantNavLink')).not.toBeInTheDocument();
+    expect(queryByText('AI Assistant')).not.toBeInTheDocument();
+  });
 
-        (useAssistantContext as jest.Mock).mockReturnValue({
-            showAssistantOverlay: mockShowAssistantOverlay,
-            assistantAvailability: {
-                hasAssistantPrivilege: false
-            }
-        })
+  it('should call the assistant overlay to show on click', () => {
+    const { result: portalNode } = renderHook(() =>
+      React.useMemo(() => createHtmlPortalNode(), [])
+    );
 
-        mockRegisterRight.mockImplementationOnce((chromeNavControl: ChromeNavControl) => {
-            chromeNavControl.mount(portalNode.current.element)
-        })
-
-        const { queryByText, queryByTestId } = render(
-            <>
-                <MockNavigationBar node={portalNode.current} />
-                <NavControls />
-            </>, { wrapper: ProviderWrapper });
-        expect(queryByTestId('assistantNavLink')).not.toBeInTheDocument();
-        expect(queryByText('AI Assistant')).not.toBeInTheDocument();
+    mockRegisterRight.mockImplementationOnce((chromeNavControl: ChromeNavControl) => {
+      chromeNavControl.mount(portalNode.current.element);
     });
 
-    it('should call the assistant overlay to show on click', () => {
-        const { result: portalNode } = renderHook(() => React.useMemo(() => createHtmlPortalNode(), []));
-
-        mockRegisterRight.mockImplementationOnce((chromeNavControl: ChromeNavControl) => {
-            chromeNavControl.mount(portalNode.current.element)
-        })
-
-        const { queryByTestId } = render(<>
-            <MockNavigationBar node={portalNode.current} />
-            <NavControls />
-        </>, { wrapper: ProviderWrapper });
-        queryByTestId('assistantNavLink')?.click();
-        expect(mockShowAssistantOverlay).toHaveBeenCalledTimes(1);
-        expect(mockShowAssistantOverlay).toHaveBeenCalledWith({ showOverlay: true });
-    });
-})
+    const { queryByTestId } = render(
+      <>
+        <MockNavigationBar node={portalNode.current} />
+        <NavControls />
+      </>,
+      { wrapper: ProviderWrapper }
+    );
+    queryByTestId('assistantNavLink')?.click();
+    expect(mockShowAssistantOverlay).toHaveBeenCalledTimes(1);
+    expect(mockShowAssistantOverlay).toHaveBeenCalledWith({ showOverlay: true });
+  });
+});
--- x-pack/plugins/security_solution/public/app/home/nav_controls/index.tsx
+++ x-pack/plugins/security_solution/public/app/home/nav_controls/index.tsx
@@ -36,4 +36,4 @@ export const NavControls = () => {
       <AssistantNavLink />
     </InPortal>
   );
-}
+};
