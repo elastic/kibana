9d75b4bfbdc63e5a259b00cf6d1727497046822b
Brad White <brad.white@elastic.co>
2024-12-05 05:32:48 +0000

    revert dedupe. add classnames ignore

--- src/dev/yarn_deduplicate/index.ts
+++ src/dev/yarn_deduplicate/index.ts
@@ -16,7 +16,7 @@ const yarnLock = readFileSync(yarnLockFile, 'utf-8');
 const output = fixDuplicates(yarnLock, {
   useMostCommon: false,
   excludeScopes: ['@types'],
-  excludePackages: ['axe-core', '@babel/types', 'csstype', 'yaml'],
+  excludePackages: ['axe-core', '@babel/types', 'classnames', 'csstype', 'yaml'],
 });
 
 writeFileSync(yarnLockFile, output);
