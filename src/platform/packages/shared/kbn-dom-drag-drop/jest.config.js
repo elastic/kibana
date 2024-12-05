93b66bdd34f5b4db7cf00f3f91ce8956902b8b6f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 15:24:23 +0100

    Relocating module `@kbn/dom-drag-drop`

--- src/platform/packages/shared/kbn-dom-drag-drop/jest.config.js
+++ src/platform/packages/shared/kbn-dom-drag-drop/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-dom-drag-drop'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-dom-drag-drop'],
 };
