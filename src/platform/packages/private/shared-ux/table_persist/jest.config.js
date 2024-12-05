6678ddadf2d27cd459670ec9bbd7adaa2fa5e42b
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 18:53:09 +0100

    Relocating module `@kbn/shared-ux-table-persist`

--- src/platform/packages/private/shared-ux/table_persist/jest.config.js
+++ src/platform/packages/private/shared-ux/table_persist/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/shared-ux/table_persist'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/shared-ux/table_persist'],
 };
