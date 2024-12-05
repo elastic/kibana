21dea386667829c0fe54a68d14808b30a9f48f44
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 16:44:09 +0100

    Relocating module `@kbn/aiops-test-utils`

--- x-pack/platform/packages/private/ml/aiops_test_utils/jest.config.js
+++ x-pack/platform/packages/private/ml/aiops_test_utils/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/aiops_test_utils'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/aiops_test_utils'],
 };
