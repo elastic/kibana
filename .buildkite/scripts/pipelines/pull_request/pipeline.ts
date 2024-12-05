1020119ae5eff519970790f3cd80db0c54a52888
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 18:03:08 +0100

    Relocating module `@kbn/field-formats-plugin`

--- .buildkite/scripts/pipelines/pull_request/pipeline.ts
+++ .buildkite/scripts/pipelines/pull_request/pipeline.ts
@@ -310,7 +310,7 @@ const getPipeline = (filename: string, removeSteps = true) => {
         /^src\/plugins\/data/,
         /^src\/platform\/plugins\/shared\/data_views/,
         /^src\/platform\/plugins\/shared\/discover/,
-        /^src\/plugins\/field_formats/,
+        /^src\/platform\/plugins\/shared\/field_formats/,
         /^src\/plugins\/inspector/,
         /^src\/plugins\/kibana_react/,
         /^src\/plugins\/kibana_utils/,
