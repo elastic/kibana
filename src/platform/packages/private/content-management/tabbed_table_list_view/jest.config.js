d07d47c82b5b236b556bbdce012ea7a3b0ed9b23
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 08:52:39 +0100

    Relocating module `@kbn/content-management-tabbed-table-list-view`

--- src/platform/packages/private/content-management/tabbed_table_list_view/jest.config.js
+++ src/platform/packages/private/content-management/tabbed_table_list_view/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/content-management/tabbed_table_list_view'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/content-management/tabbed_table_list_view'],
 };
