39d2df7a01b86cf4a03eb9371da77409d3982b79
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:46:01 +0100

    Relocating module `@kbn/data-view-management-plugin`

--- src/platform/plugins/shared/data_view_management/jest.config.js
+++ src/platform/plugins/shared/data_view_management/jest.config.js
@@ -9,9 +9,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/src/plugins/data_view_management'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/data_view_management',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/shared/data_view_management'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/shared/data_view_management',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/src/plugins/data_view_management/{public,server}/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/platform/plugins/shared/data_view_management/{public,server}/**/*.{ts,tsx}'],
 };
