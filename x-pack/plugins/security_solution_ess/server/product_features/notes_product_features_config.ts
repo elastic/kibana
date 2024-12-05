fba346fb50a0ecd41cb2ecd5e679253230383768
Jan Monschke <jan.monschke@elastic.co>
2024-12-05 13:17:18 +0100

    fix types

--- x-pack/plugins/security_solution_ess/server/product_features/notes_product_features_config.ts
+++ x-pack/plugins/security_solution_ess/server/product_features/notes_product_features_config.ts
@@ -4,20 +4,20 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
 import type {
   ProductFeatureKeys,
+  ProductFeatureKibanaConfig,
   ProductFeaturesNotesConfig,
 } from '@kbn/security-solution-features';
 import {
-  createEnabledProductFeaturesConfigMap,
   notesDefaultProductFeaturesConfig,
+  createEnabledProductFeaturesConfigMap,
 } from '@kbn/security-solution-features/config';
 import type { ProductFeatureNotesFeatureKey } from '@kbn/security-solution-features/keys';
 
 /**
  * Maps the ProductFeatures keys to Kibana privileges that will be merged
- * into the base privileges config for the Security app.
+ * into the base privileges config for the app.
  *
  * Privileges can be added in different ways:
  * - `privileges`: the privileges that will be added directly into the main Attack discovery feature.
@@ -26,7 +26,7 @@ import type { ProductFeatureNotesFeatureKey } from '@kbn/security-solution-featu
  */
 const notesProductFeaturesConfig: Record<
   ProductFeatureNotesFeatureKey,
-  ProductFeaturesNotesConfig
+  ProductFeatureKibanaConfig
 > = {
   ...notesDefaultProductFeaturesConfig,
   // ess-specific app features configs here
