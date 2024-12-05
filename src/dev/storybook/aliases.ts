daa11bea5c36efd9bd8c00e3af54f8e859d30e1f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 18:50:34 +0100

    Relocating module `@kbn/es-ui-shared-plugin`

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -30,7 +30,7 @@ export const storybookAliases = {
   discover: 'src/plugins/discover/.storybook',
   embeddable: 'src/plugins/embeddable/.storybook',
   esql_ast_inspector: 'examples/esql_ast_inspector/.storybook',
-  es_ui_shared: 'src/plugins/es_ui_shared/.storybook',
+  es_ui_shared: 'src/platform/plugins/shared/es_ui_shared/.storybook',
   expandable_flyout: 'packages/kbn-expandable-flyout/.storybook',
   expression_error: 'src/plugins/expression_error/.storybook',
   expression_image: 'src/plugins/expression_image/.storybook',
