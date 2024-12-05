fc9ce7979efd23a6d3e860ff90c62cc045f7e501
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 08:03:25 +0100

    Relocating module `@kbn/banners-plugin`

--- x-pack/platform/plugins/private/banners/jest.config.js
+++ x-pack/platform/plugins/private/banners/jest.config.js
@@ -7,9 +7,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/banners'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/banners',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/plugins/private/banners'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/private/banners',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/banners/{common,public,server}/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/private/banners/{common,public,server}/**/*.{ts,tsx}'],
 };
