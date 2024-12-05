9616bfdbaa952959c2b68a7bd0ac7a55cb6cb408
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:38:02 +0100

    Relocating module `@kbn/shared-ux-page-no-data-config`

--- src/platform/packages/shared/shared-ux/page/no_data_config/impl/jest.config.js
+++ src/platform/packages/shared/shared-ux/page/no_data_config/impl/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../..',
-  roots: ['<rootDir>/packages/shared-ux/page/no_data_config/impl'],
+  rootDir: '../../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/shared-ux/page/no_data_config/impl'],
 };
