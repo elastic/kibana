69ce9d9041351b3d5318111d58e0a99ef7ff065f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:19:09 +0100

    Relocating module `@kbn/ml-nested-property`

--- x-pack/platform/packages/private/ml/nested_property/jest.config.js
+++ x-pack/platform/packages/private/ml/nested_property/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/ml/nested_property'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/private/ml/nested_property'],
 };
