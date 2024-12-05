e6116dba0b793a871cae9933f5cdf108a55e713f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 17:24:01 +0100

    Relocating module `@kbn/fleet-plugin`

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -41,7 +41,7 @@ export const storybookAliases = {
   expression_reveal_image: 'src/plugins/expression_reveal_image/.storybook',
   expression_shape: 'src/plugins/expression_shape/.storybook',
   expression_tagcloud: 'src/plugins/chart_expressions/expression_tagcloud/.storybook',
-  fleet: 'x-pack/plugins/fleet/.storybook',
+  fleet: 'x-pack/platform/plugins/shared/fleet/.storybook',
   grouping: 'packages/kbn-grouping/.storybook',
   home: 'src/plugins/home/.storybook',
   infra: 'x-pack/plugins/observability_solution/infra/.storybook',
