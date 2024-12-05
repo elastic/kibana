9b6b15641d3e15676e105445d0b77f704a93eaaa
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-05 18:47:39 +0100

    Relocating module `@kbn/shared-ux-tabbed-modal`

--- src/platform/packages/private/shared-ux/modal/tabbed/jest.config.js
+++ src/platform/packages/private/shared-ux/modal/tabbed/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/shared-ux/modal/tabbed'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/shared-ux/modal/tabbed'],
 };
