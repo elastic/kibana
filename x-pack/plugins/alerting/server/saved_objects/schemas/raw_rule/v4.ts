638214bc7d278d9462ccefe0fd7517a1f2af050c
Ersin Erdal <ersin.erdal@elastic.co>
2024-12-06 18:20:48 +0100

    add error reason to schemas

---
+++ x-pack/plugins/alerting/server/saved_objects/schemas/raw_rule/v4.ts
@@ -0,0 +1,40 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { schema } from '@kbn/config-schema';
+import { RuleExecutionStatusErrorReasons } from '@kbn/alerting-types';
+import {
+  rawRuleExecutionStatusSchema as rawRuleExecutionStatusSchemaV3,
+  rawRuleSchema as rawRuleSchemaV3,
+} from './v3';
+
+export * from './v3';
+
+export const executionStatusErrorReason = schema.oneOf([
+  schema.literal(RuleExecutionStatusErrorReasons.Read),
+  schema.literal(RuleExecutionStatusErrorReasons.Decrypt),
+  schema.literal(RuleExecutionStatusErrorReasons.Execute),
+  schema.literal(RuleExecutionStatusErrorReasons.Unknown),
+  schema.literal(RuleExecutionStatusErrorReasons.License),
+  schema.literal(RuleExecutionStatusErrorReasons.Timeout),
+  schema.literal(RuleExecutionStatusErrorReasons.Disabled),
+  schema.literal(RuleExecutionStatusErrorReasons.Validate),
+  schema.literal(RuleExecutionStatusErrorReasons.Blocked), // new
+]);
+
+const rawRuleExecutionStatusSchema = rawRuleExecutionStatusSchemaV3.extends({
+  error: schema.nullable(
+    schema.object({
+      reason: executionStatusErrorReason,
+      message: schema.string(),
+    })
+  ),
+});
+
+export const rawRuleSchema = rawRuleSchemaV3.extends({
+  executionStatus: rawRuleExecutionStatusSchema,
+});
