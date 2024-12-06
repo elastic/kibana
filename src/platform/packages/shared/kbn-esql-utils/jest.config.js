1190d4650d5f13f589c178f57d52f5453ab0b71c
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-06 14:52:55 +0100

    Relocating module `@kbn/esql-utils`

--- src/platform/packages/shared/kbn-esql-utils/jest.config.js
+++ src/platform/packages/shared/kbn-esql-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-esql-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-esql-utils'],
 };
