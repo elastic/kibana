5b8ee135a577ce80d30e145ce11e19a11f241e69
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 13:40:16 +0100

    Relocating module `@kbn/reporting-plugin`

--- x-pack/platform/plugins/private/reporting/common/errors/map_to_reporting_error.ts
+++ x-pack/platform/plugins/private/reporting/common/errors/map_to_reporting_error.ts
@@ -37,7 +37,7 @@ function getErrorName(error: ExecutionError | unknown): string | undefined {
  * Map an error object from the Screenshotting plugin into an error type of the Reporting domain.
  *
  * NOTE: each type of ReportingError code must be referenced in each applicable `errorCodesSchema*` object in
- * x-pack/plugins/reporting/server/usage/schema.ts
+ * x-pack/platform/plugins/private/reporting/server/usage/schema.ts
  *
  * @param {unknown} error - a kind of error object
  * @returns {ReportingError} - the converted error object
