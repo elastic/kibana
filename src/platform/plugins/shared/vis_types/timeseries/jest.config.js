c6ba8f5049e986f93a92cf21abdcfabb367e1557
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 16:23:44 +0100

    Relocating module `@kbn/vis-type-timeseries-plugin`

--- src/platform/plugins/shared/vis_types/timeseries/jest.config.js
+++ src/platform/plugins/shared/vis_types/timeseries/jest.config.js
@@ -9,11 +9,11 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/src/plugins/vis_types/timeseries'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/vis_types/timeseries',
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/shared/vis_types/timeseries'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/shared/vis_types/timeseries',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/src/plugins/vis_types/timeseries/{common,public,server}/**/*.{js,ts,tsx}',
+    '<rootDir>/src/platform/plugins/shared/vis_types/timeseries/{common,public,server}/**/*.{js,ts,tsx}',
   ],
 };
