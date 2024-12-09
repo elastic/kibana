f70848850b89e5dd57ac3a41bca5c7dea404102a
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 13:15:09 +0100

    Relocating module `@kbn/field-types`

--- src/platform/packages/shared/kbn-field-types/jest.config.js
+++ src/platform/packages/shared/kbn-field-types/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-field-types'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-field-types'],
 };
