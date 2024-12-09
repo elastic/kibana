0ceebfdd176efc06e38d3c090d493d873d6b9917
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 13:18:29 +0100

    Relocating module `@kbn/react-kibana-context-render`

--- src/platform/packages/shared/react/kibana_context/render/jest.config.js
+++ src/platform/packages/shared/react/kibana_context/render/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/react/kibana_context/render'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/react/kibana_context/render'],
 };
