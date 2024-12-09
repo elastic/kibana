1116b661eb00201e2308382182e078d675d74c49
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 13:31:44 +0100

    Relocating module `@kbn/reporting-export-types-csv-common`

--- src/platform/packages/private/kbn-reporting/export_types/csv_common/jest.config.js
+++ src/platform/packages/private/kbn-reporting/export_types/csv_common/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/kbn-reporting/export_types/csv_common'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-reporting/export_types/csv_common'],
 };
