772dc6f5499cde86597b834be0c021a2c4a72497
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2024-12-09 10:02:28 +0000

    use string template for new styles

--- x-pack/packages/kbn-elastic-assistant/impl/assistant/assistant_animated_icon.tsx
+++ x-pack/packages/kbn-elastic-assistant/impl/assistant/assistant_animated_icon.tsx
@@ -5,119 +5,100 @@
  * 2.0.
  */
 
-import React, { PropsWithChildren } from 'react';
+import React from 'react';
 import styled from '@emotion/styled';
-import { css } from '@emotion/react';
-import { useEuiTheme } from '@elastic/eui';
 import { AssistantAvatar } from './assistant_avatar/assistant_avatar';
 
-const Container = ({ children }: PropsWithChildren) => {
-  const { euiTheme } = useEuiTheme();
-  return (
-    <div
-      css={css`
-        display: inline-block;
-        position: relative;
-        width: 56px;
-        height: 56px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-top: ${euiTheme.size.xxl};
-        margin-bottom: ${euiTheme.size.l};
+const Container = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 56px;
+  height: 56px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: ${props => props.theme.euiTheme.size.xxl};
+  margin-bottom: ${props => props.theme.euiTheme.size.l};
 
-        :before,
-        :after {
-          content: '';
-          position: absolute;
-        }
-      `}
-    >
-      {children}
-    </div>
-  );
-};
+  :before,
+  :after {
+    content: '';
+    position: absolute;
+  }
+`;
 
-const Animation = () => {
-  const { euiTheme } = useEuiTheme();
+const Animation = styled.div`
+  width: 99%;
+  height: 99%;
+  border-radius: 50px;
+  z-index: 1;
+  position: relative;
 
-  return (
-    <div
-      css={css`
-        width: 99%;
-        height: 99%;
-        border-radius: 50px;
-        z-index: 1;
-        position: relative;
+  &:before,
+  &:after {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    border: inherit;
+    top: 0;
+    left: 0;
+    z-index: 0;
+    border: 1px solid ${props => props.theme.euiTheme.colors.borderBasePlain};
+    border-radius: inherit;
+    animation: 4s cubic-bezier(0.42, 0, 0.37, 1) 0.5s infinite normal none running pulsing;
+  }
+  &:after {
+    animation: 4s cubic-bezier(0.42, 0, 0.37, 1) 0.5s infinite normal none running pulsing1;
+  }
 
-        &:before,
-        &:after {
-          content: '';
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          border: inherit;
-          top: 0;
-          left: 0;
-          z-index: 0;
-          border: 1px solid ${euiTheme.colors.borderBasePlain};
-          border-radius: inherit;
-          animation: 4s cubic-bezier(0.42, 0, 0.37, 1) 0.5s infinite normal none running pulsing;
-        }
-        &:after {
-          animation: 4s cubic-bezier(0.42, 0, 0.37, 1) 0.5s infinite normal none running pulsing1;
-        }
-
-        @keyframes pulsing {
-          0% {
-            opacity: 1;
-            transform: scaleY(1) scaleX(1);
-          }
-          20% {
-            opacity: 0.5;
-          }
-          70% {
-            opacity: 0.2;
-            transform: scaleY(2) scaleX(2);
-          }
-          80% {
-            opacity: 0;
-            transform: scaleY(2) scaleX(2);
-          }
-          90% {
-            opacity: 0;
-            transform: scaleY(1) scaleX(1);
-          }
-        }
-        @keyframes pulsing1 {
-          0% {
-            opacity: 1;
-            transform: scaleY(1) scaleX(1);
-          }
-          15% {
-            opacity: 1;
-            transform: scaleY(1) scaleX(1);
-          }
-          40% {
-            opacity: 0.5;
-          }
-          70% {
-            opacity: 0.2;
-            transform: scaleY(1.5) scaleX(1.5);
-          }
-          80% {
-            opacity: 0;
-            transform: scaleY(1.5) scaleX(1.5);
-          }
-          90% {
-            opacity: 0;
-            transform: scaleY(1) scaleX(1);
-          }
-        }
-      `}
-    />
-  );
-};
+  @keyframes pulsing {
+    0% {
+      opacity: 1;
+      transform: scaleY(1) scaleX(1);
+    }
+    20% {
+      opacity: 0.5;
+    }
+    70% {
+      opacity: 0.2;
+      transform: scaleY(2) scaleX(2);
+    }
+    80% {
+      opacity: 0;
+      transform: scaleY(2) scaleX(2);
+    }
+    90% {
+      opacity: 0;
+      transform: scaleY(1) scaleX(1);
+    }
+  }
+  @keyframes pulsing1 {
+    0% {
+      opacity: 1;
+      transform: scaleY(1) scaleX(1);
+    }
+    15% {
+      opacity: 1;
+      transform: scaleY(1) scaleX(1);
+    }
+    40% {
+      opacity: 0.5;
+    }
+    70% {
+      opacity: 0.2;
+      transform: scaleY(1.5) scaleX(1.5);
+    }
+    80% {
+      opacity: 0;
+      transform: scaleY(1.5) scaleX(1.5);
+    }
+    90% {
+      opacity: 0;
+      transform: scaleY(1) scaleX(1);
+    }
+  }
+`;
 
 const AvatarWrapper = styled.span`
   width: 100%;
