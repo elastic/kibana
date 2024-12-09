be0ac14cacabce7367baab8168adaf7d80090a75
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 12:45:50 +0100

    Relocating module `@kbn/global-search-plugin`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1910,7 +1910,7 @@ module.exports = {
         'src/core/**',
         'x-pack/plugins/features/**',
         'x-pack/plugins/licensing/**',
-        'x-pack/plugins/global_search/**',
+        'x-pack/platform/plugins/shared/global_search/**',
         'x-pack/plugins/cloud/**',
         'packages/kbn-config-schema',
         'src/plugins/status_page/**',
