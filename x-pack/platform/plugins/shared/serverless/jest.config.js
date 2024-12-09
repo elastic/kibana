02f9a25758d3b651dcd463111268d97405340b39
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 13:53:14 +0100

    Relocating module `@kbn/serverless`

--- x-pack/platform/plugins/shared/serverless/jest.config.js
+++ x-pack/platform/plugins/shared/serverless/jest.config.js
@@ -7,9 +7,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/serverless'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/serverless',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/plugins/shared/serverless'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/shared/serverless',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/serverless/{common,public,server}/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/shared/serverless/{common,public,server}/**/*.{ts,tsx}'],
 };
