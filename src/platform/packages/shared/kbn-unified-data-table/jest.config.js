a469ef8e133007025056a07c635bc8fffe434417
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 13:28:41 +0100

    Relocating module `@kbn/unified-data-table`

--- src/platform/packages/shared/kbn-unified-data-table/jest.config.js
+++ src/platform/packages/shared/kbn-unified-data-table/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-unified-data-table'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-unified-data-table'],
 };
