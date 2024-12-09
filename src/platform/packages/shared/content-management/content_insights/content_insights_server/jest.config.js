1b46dadf8e6236ba31b7185339dbdfce87eb434c
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 12:23:28 +0100

    Relocating module `@kbn/content-management-content-insights-server`

--- src/platform/packages/shared/content-management/content_insights/content_insights_server/jest.config.js
+++ src/platform/packages/shared/content-management/content_insights/content_insights_server/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/content-management/content_insights/content_insights_server'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/content-management/content_insights/content_insights_server'],
 };
