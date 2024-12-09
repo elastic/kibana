9ef41118bcf15329542403254ef28382cf511a76
Sid <siddharthmantri1@gmail.com>
2024-12-09 09:51:20 +0100

    revert euihealth change

--- examples/feature_control_examples/public/app.tsx
+++ examples/feature_control_examples/public/app.tsx
@@ -52,7 +52,7 @@ export const MyPluginComponent: React.FC = () => {
         ).map(([capability, value]) => {
           return value === true ? (
             <div key={capability}>
-              <EuiHealth color="accentSecondary">{capability}</EuiHealth>
+              <EuiHealth color="success">{capability}</EuiHealth>
               <EuiSpacer />
             </div>
           ) : null;
