be432bf4e54377fc8bb1fccbeb570e2137cbc070
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-09 16:22:48 +0100

    Relocating module `@kbn/io-ts-utils`

--- src/platform/packages/shared/kbn-io-ts-utils/jest.config.js
+++ src/platform/packages/shared/kbn-io-ts-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-io-ts-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-io-ts-utils'],
 };
