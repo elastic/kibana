479244ab3fda5cd8dd95d78e13bb11ba25b2dd4a
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-10 16:23:56 +0100

    Relocating module `@kbn/lens-embeddable-utils`

--- src/platform/packages/shared/kbn-lens-embeddable-utils/jest.config.js
+++ src/platform/packages/shared/kbn-lens-embeddable-utils/jest.config.js
@@ -9,7 +9,7 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-lens-embeddable-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-lens-embeddable-utils'],
   setupFiles: ['jest-canvas-mock'],
 };
