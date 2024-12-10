7f6f1960e117e23603bb88e22f1e299f510f59fd
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-10 16:48:28 +0100

    Relocating module `@kbn/core-ui-settings-browser`

--- src/core/packages/ui-settings/browser/jest.config.js
+++ src/core/packages/ui-settings/browser/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/ui-settings/core-ui-settings-browser'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/ui-settings/browser'],
 };
