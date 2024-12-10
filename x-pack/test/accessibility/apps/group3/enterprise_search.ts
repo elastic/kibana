9e00424061758095b73341f85d1e0ce3cddf03d5
Saikat Sarkar <132922331+saikatsarkar056@users.noreply.github.com>
2024-12-10 08:55:15 -0700

    [Ent Search Deprecation] Update enterprise_search URLs for Elasticsearch->Build (#202371)
    This change is related to this
    [issue](https://github.com/elastic/search-team/issues/7961). This PR
    replaces 'enterprise_search' with 'elasticsearch' in all urls for
    Elasticsearch->Build. In the PR, we replaced the following URLs.
    
    | Old URL | New URL |
    | :------------------------------------- |
    :------------------------------------- |
    | app/enterprise_search/applications/search_applications |
    app/elasticsearch/applications/search_applications |
    | app/enterprise_search/analytics | app/elasticsearch/analytics |
    
    
    
    https://github.com/user-attachments/assets/e7b6ba66-e47b-4d99-8edc-37a9d30edc69
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/test/accessibility/apps/group3/enterprise_search.ts
+++ x-pack/test/accessibility/apps/group3/enterprise_search.ts
@@ -106,7 +106,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
     describe('Playground', () => {
       before(async () => {
-        await common.navigateToApp('enterprise_search/applications');
+        await common.navigateToApp('elasticsearch/applications');
       });
 
       it('loads playground', async function () {
@@ -120,7 +120,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
     describe('Search Applications', () => {
       before(async () => {
-        await common.navigateToApp('enterprise_search/applications/search_applications');
+        await common.navigateToApp('elasticsearch/applications/search_applications');
       });
 
       it('loads search applications list', async function () {
@@ -133,7 +133,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     });
     describe('Behavioral Analytics', () => {
       before(async () => {
-        await common.navigateToApp('enterprise_search/analytics');
+        await common.navigateToApp('elasticsearch/analytics');
       });
 
       it('loads Behavioral Analytics page', async function () {
