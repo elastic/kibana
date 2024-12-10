ff009cfd390684b35a1f01d82f82fbda4f89027f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-10 16:09:15 +0100

    Relocating module `@kbn/avc-banner`

--- src/platform/packages/shared/kbn-avc-banner/jest.config.js
+++ src/platform/packages/shared/kbn-avc-banner/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-avc-banner'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-avc-banner'],
 };
