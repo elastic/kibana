b0f36f3b651c03bc5295e51b2fcf48668161e388
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-12-10 15:32:55 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/osquery/jest.config.js
+++ x-pack/platform/plugins/shared/osquery/jest.config.js
@@ -11,5 +11,7 @@ module.exports = {
   roots: ['<rootDir>/x-pack/platform/plugins/shared/osquery'],
   coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/shared/osquery',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/shared/osquery/{common,public,server}/**/*.{ts,tsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/x-pack/platform/plugins/shared/osquery/{common,public,server}/**/*.{ts,tsx}',
+  ],
 };
