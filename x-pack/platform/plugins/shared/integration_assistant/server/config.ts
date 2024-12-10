de10b75439568aae9de9434bb87e8df5768c4c87
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-10 15:37:51 +0100

    Relocating module `@kbn/integration-assistant-plugin`

--- x-pack/platform/plugins/shared/integration_assistant/server/config.ts
+++ x-pack/platform/plugins/shared/integration_assistant/server/config.ts
@@ -14,7 +14,7 @@ export const configSchema = schema.object({
    * For internal use. A list of string values (comma delimited) that will enable experimental
    * type of functionality that is not yet released. Valid values for this settings need to
    * be defined in:
-   * `x-pack/plugins/integration_assistant/common/experimental_features.ts`
+   * `x-pack/platform/plugins/shared/integration_assistant/common/experimental_features.ts`
    * under the `allowedExperimentalValues` object
    *
    * @example
