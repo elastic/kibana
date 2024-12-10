592847f973651689083a083ca0a722ef94932f06
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-10 16:34:31 +0100

    Relocating module `@kbn/ux-plugin`

--- .eslintrc.js
+++ .eslintrc.js
@@ -917,7 +917,7 @@ module.exports = {
         'x-pack/solutions/observability/plugins/apm/**/*.{js,mjs,ts,tsx}',
         'x-pack/plugins/observability_solution/observability/**/*.{js,mjs,ts,tsx}',
         'x-pack/plugins/observability_solution/exploratory_view/**/*.{js,mjs,ts,tsx}',
-        'x-pack/plugins/observability_solution/ux/**/*.{js,mjs,ts,tsx}',
+        'x-pack/solutions/observability/plugins/ux/**/*.{js,mjs,ts,tsx}',
         'x-pack/plugins/observability_solution/slo/**/*.{js,mjs,ts,tsx}',
       ],
       rules: {
