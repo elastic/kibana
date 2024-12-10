870ab8b527c9011a384a8dd3972e829fef63861f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-10 17:05:34 +0100

    Relocating module `@kbn/security-solution-side-nav`

--- x-pack/solutions/security/packages/side_nav/jest.config.js
+++ x-pack/solutions/security/packages/side_nav/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/security-solution/side_nav'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/solutions/security/packages/side_nav'],
 };
