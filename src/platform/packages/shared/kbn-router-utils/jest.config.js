9ab1832514a2c7d393d5e68e0abe9826167c3ac5
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-10 16:03:51 +0100

    Relocating module `@kbn/router-utils`

--- src/platform/packages/shared/kbn-router-utils/jest.config.js
+++ src/platform/packages/shared/kbn-router-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-router-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-router-utils'],
 };
