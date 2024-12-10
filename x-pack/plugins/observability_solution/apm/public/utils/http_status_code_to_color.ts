cb5d6fdf29c278ea59075719238a996290ce3e72
Bryce Buchanan <75274611+bryce-b@users.noreply.github.com>
2024-12-10 10:37:52 -0800

    Update x-pack/plugins/observability_solution/apm/public/utils/http_status_code_to_color.ts
    Co-authored-by: Miriam <31922082+MiriamAparicio@users.noreply.github.com>

--- x-pack/plugins/observability_solution/apm/public/utils/http_status_code_to_color.ts
+++ x-pack/plugins/observability_solution/apm/public/utils/http_status_code_to_color.ts
@@ -10,11 +10,11 @@ import { useEuiTheme } from '@elastic/eui';
 export const useGetStatusColor = (status: string | number) => {
   const { euiTheme } = useEuiTheme();
   const httpStatusCodeColors: Record<string, string> = {
-    1: euiTheme.colors.textSubdued,
-    2: euiTheme.colors.textSuccess,
-    3: euiTheme.colors.textSubdued,
-    4: euiTheme.colors.textWarning,
-    5: euiTheme.colors.textDanger,
+    1: euiTheme.colors.backgroundBaseSubdued,
+    2: euiTheme.colors.backgroundBaseSuccess,
+    3: euiTheme.colors.backgroundBaseSubdued,
+    4: euiTheme.colors.backgroundBaseWarning,
+    5: euiTheme.colors.backgroundBaseDanger,
   };
   const intStatus = typeof status === 'string' ? parseInt(status.replace(/\D/g, ''), 10) : status;
   return httpStatusCodeColors[intStatus.toString().substring(0, 1)];
