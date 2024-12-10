elastic/kibana

commit 61d0320c6422116dcf1c4e26f8f80760d7a3bb81

[Lens] Embeddable react refactor (#186642)
## Summary

This PR contains the refactor of the Lens embeddable with the new React
architecture.

fix https://github.com/elastic/kibana/issues/174957
fixes https://github.com/elastic/kibana/issues/180672

**Current status**:
✅ Ready to review

### Notes for testing and reviewers

Other than reworking the Lens embeddable with the new architecture this
PR contains the following major changes.

#### Edit flow
The `Edit` flow has changed to in-line first using the new `Edit` API
provided by the new system
* The impact of this change can be noticed in the code on the `Canvas`
case where the Custom Lens component is instructed to avoid the inline
editing. In all the other cases in-line editing is enabled by default
now.
* Another side effect of this has been the replacement of the special
`INLINE_EDIT` action id into the regular `EDIT` action. Some tests have
been affected by this replacing the `clickEdit` function with the
`openEditorFromFlyout` one.
* The Inline editing codebase **as been reworked entirely** so make sure
to stress test this side of things.

#### Attribute service

Another important aspect changed in this PR is the `attributeService`:
this was tied to the previous Embeddable system and it is now completely
skipped. The Lens wrapper around that has been reworked to be thinner
and directly call the CM services.
* Please make sure to test thoroughly save/load SO flows

#### Transformation API (by-value <=> by-reference flow)

The new system adopts the new Transformation API (who prevents the panel
to fully reload on change).
* Please make sure to test thoroughly Visualize library <=> by value
flows
* In particular moving from one type and another should change how the
Panel Settings interpret "default" values to reset

#### Message system

Also this part of the code was partially rewritten to be more manageable

x-pack/plugins/lens/public/react_embeddable/renderer/lens_custom_renderer_component.tsx
@@ -1,0 +1,158 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { ReactEmbeddableRenderer } from '@kbn/embeddable-plugin/public';
+import { useSearchApi } from '@kbn/presentation-publishing';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import { BehaviorSubject } from 'rxjs';
+import type { PresentationPanelProps } from '@kbn/presentation-panel-plugin/public';
+import type { LensApi, LensRendererProps, LensRuntimeState, LensSerializedState } from '../types';
+import { LENS_EMBEDDABLE_TYPE } from '../../../common/constants';
+import { createEmptyLensState } from '../helper';
+
+// This little utility uses the same pattern of the useSearchApi hook:
+// create the Subject once and then update its value on change
+function useObservableVariable<T extends unknown>(value: T) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const observable = useMemo(() => new BehaviorSubject<T>(value), []);
+
+  // update the observable on change
+  useEffect(() => {
+    observable.next(value);
+  }, [observable, value]);
+
+  return observable;
+}
+
+type PanelProps = Pick<
+  PresentationPanelProps<LensApi>,
+  | 'showShadow'
+  | 'showBorder'
+  | 'showBadges'
+  | 'showNotifications'
+  | 'hideLoader'
+  | 'hideHeader'
+  | 'hideInspector'
+  | 'getActions'
+>;
+
+/**
+ * The aim of this component is to provide a wrapper for other plugins who want to
+ * use a Lens component into their own page. This hides the embeddable parts of it
+ * by wrapping it into a ReactEmbeddableRenderer component and exposing a custom API
+ */
+export function LensRenderer({
+  title,
+  withDefaultActions,
+  extraActions,
+  showInspector,
+  syncColors,
+  syncCursor,
+  syncTooltips,
+  viewMode,
+  id,
+  query,
+  filters,
+  timeRange,
+  disabledActions,
+  ...props
+}: LensRendererProps) {
+  // Use the settings interface to store panel settings
+  const settings = useMemo(() => {
+    return {
+      syncColors$: new BehaviorSubject(false),
+      syncCursor$: new BehaviorSubject(false),
+      syncTooltips$: new BehaviorSubject(false),
+    };
+  }, []);
+  const disabledActionIds$ = useObservableVariable(disabledActions);
+  const viewMode$ = useObservableVariable(viewMode);
+
+  // Lens API will be set once, but when set trigger a reflow to adopt the latest attributes
+  const [lensApi, setLensApi] = useState<LensApi | undefined>(undefined);
+  const initialStateRef = useRef<LensSerializedState>(
+    props.attributes ? { attributes: props.attributes } : createEmptyLensState(null, title)
+  );
+
+  const searchApi = useSearchApi({ query, filters, timeRange });
+
+  const showPanelChrome = Boolean(withDefaultActions) || (extraActions?.length || 0) > 0;
+
+  // Re-render on changes
+  // internally the embeddable will evaluate whether it is worth to actual render or not
+  useEffect(() => {
+    // trigger a re-render if the attributes change
+    if (lensApi) {
+      lensApi.updateAttributes({
+        ...('attributes' in initialStateRef.current
+          ? initialStateRef.current.attributes
+          : initialStateRef.current),
+        ...props.attributes,
+      });
+      lensApi.updateOverrides(props.overrides);
+    }
+  }, [lensApi, props.attributes, props.overrides]);
+
+  useEffect(() => {
+    if (syncColors != null && settings.syncColors$.getValue() !== syncColors) {
+      settings.syncColors$.next(syncColors);
+    }
+    if (syncCursor != null && settings.syncCursor$.getValue() !== syncCursor) {
+      settings.syncCursor$.next(syncCursor);
+    }
+    if (syncTooltips != null && settings.syncTooltips$.getValue() !== syncTooltips) {
+      settings.syncTooltips$.next(syncTooltips);
+    }
+  }, [settings, syncColors, syncCursor, syncTooltips]);
+
+  const panelProps: PanelProps = useMemo(() => {
+    return {
+      hideInspector: !showInspector,
+      hideHeader: showPanelChrome,
+      showNotifications: false,
+      showShadow: false,
+      showBadges: false,
+      getActions: async (triggerId, context) => {
+        const actions = withDefaultActions
+          ? await lensApi?.getTriggerCompatibleActions(triggerId, context)
+          : [];
+
+        return (extraActions ?? []).concat(actions || []);
+      },
+    };
+  }, [showInspector, showPanelChrome, withDefaultActions, extraActions, lensApi]);
+
+  return (
+    <ReactEmbeddableRenderer<LensSerializedState, LensRuntimeState, LensApi>
+      type={LENS_EMBEDDABLE_TYPE}
+      maybeId={id}
+      getParentApi={() => ({
+        // forward the Lens components to the embeddable
+        ...props,
+        // forward the unified search context
+        ...searchApi,
+        disabledActionIds: disabledActionIds$,
+        setDisabledActionIds: (ids: string[] | undefined) => disabledActionIds$.next(ids),
+        viewMode: viewMode$,
+        // pass the sync* settings with the unified settings interface
+        settings,
+        // make sure to provide the initial state (useful for the comparison check)
+        getSerializedStateForChild: () => ({ rawState: initialStateRef.current, references: [] }),
+        // update the runtime state on changes
+        getRuntimeStateForChild: () => ({
+          ...initialStateRef.current,
+          attributes: props.attributes,
+        }),
+      })}
+      onApiAvailable={setLensApi}
+      hidePanelChrome={!showPanelChrome}
+      panelProps={panelProps}
+    />
+  );
+}
+
+export type EmbeddableComponent = React.ComponentType<LensRendererProps>;

commit 6e57a23d18d949e7ac3118c1f27550e2ba9eedf8

[Lens] Fix missing embeddable panel titles (#203355)
## Summary

When using the new Custom Lens embeddable component, the titles are
hidden by default due to to a regression introduced in
https://github.com/elastic/kibana/pull/186642.
This PR should fix the correct title handling of the header visibility
by hiding or showing the wrapping Panel title depending on the
`hidePanelTitles` props of the `LensRenderer` component


fix https://github.com/elastic/kibana/issues/203354

x-pack/plugins/lens/public/react_embeddable/renderer/lens_custom_renderer_component.tsx
@@ -59,6 +59,7 @@ export function LensRenderer({
   filters,
   timeRange,
   disabledActions,
+  hidePanelTitles,
   ...props
 }: LensRendererProps) {
   // Use the settings interface to store panel settings
@@ -71,6 +72,7 @@ export function LensRenderer({
   }, []);
   const disabledActionIds$ = useObservableVariable(disabledActions);
   const viewMode$ = useObservableVariable(viewMode);
+  const hidePanelTitles$ = useObservableVariable(hidePanelTitles);
 
   // Lens API will be set once, but when set trigger a reflow to adopt the latest attributes
   const [lensApi, setLensApi] = useState<LensApi | undefined>(undefined);
@@ -112,7 +114,6 @@ export function LensRenderer({
   const panelProps: PanelProps = useMemo(() => {
     return {
       hideInspector: !showInspector,
-      hideHeader: showPanelChrome,
       showNotifications: false,
       showShadow: false,
       showBadges: false,
@@ -124,7 +125,7 @@ export function LensRenderer({
         return (extraActions ?? []).concat(actions || []);
       },
     };
-  }, [showInspector, showPanelChrome, withDefaultActions, extraActions, lensApi]);
+  }, [showInspector, withDefaultActions, extraActions, lensApi]);
 
   return (
     <ReactEmbeddableRenderer<LensSerializedState, LensRuntimeState, LensApi>
@@ -147,6 +148,7 @@ export function LensRenderer({
           ...initialStateRef.current,
           attributes: props.attributes,
         }),
+        hidePanelTitle: hidePanelTitles$,
       })}
       onApiAvailable={setLensApi}
       hidePanelChrome={!showPanelChrome}