eb1e69392528b1029279d6210e697304dc746266
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-10 15:48:01 +0100

    Relocating module `@kbn/dataset-quality-plugin`

--- x-pack/solutions/observability/plugins/dataset_quality/jest.config.js
+++ x-pack/solutions/observability/plugins/dataset_quality/jest.config.js
@@ -7,12 +7,12 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/plugins/observability_solution/dataset_quality'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/solutions/observability/plugins/dataset_quality'],
   coverageDirectory:
-    '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/observability_solution/dataset_quality',
+    '<rootDir>/target/kibana-coverage/jest/x-pack/solutions/observability/plugins/dataset_quality',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/x-pack/plugins/observability_solution/dataset_quality/{common,public}/**/*.{ts,tsx}',
+    '<rootDir>/x-pack/solutions/observability/plugins/dataset_quality/{common,public}/**/*.{ts,tsx}',
   ],
 };
