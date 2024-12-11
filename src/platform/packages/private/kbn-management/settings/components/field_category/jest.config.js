b0e1d02eb534853202e6822ef2dd2aa154006bb4
Matthew Kime <matt@mattki.me>
2024-12-11 12:08:55 -0600

    Relocating module `@kbn/management-settings-components-field-category`

--- src/platform/packages/private/kbn-management/settings/components/field_category/jest.config.js
+++ src/platform/packages/private/kbn-management/settings/components/field_category/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../..',
-  roots: ['<rootDir>/packages/kbn-management/settings/components/field_category'],
+  rootDir: '../../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-management/settings/components/field_category'],
 };
