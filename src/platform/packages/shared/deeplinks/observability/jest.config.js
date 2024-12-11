26f16de8fb8c21f2c5badd912b076397503791f5
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-11 15:00:38 +0100

    Relocating module `@kbn/deeplinks-observability`

--- src/platform/packages/shared/deeplinks/observability/jest.config.js
+++ src/platform/packages/shared/deeplinks/observability/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/deeplinks/observability'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/deeplinks/observability'],
 };
