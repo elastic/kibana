e8574bb43754338b32e76537ab824d86a5b88ac9
Marshall Main <marshall.main@elastic.co>
2024-12-11 14:07:01 -0500

    Relocating module `@kbn/securitysolution-hook-utils`

--- x-pack/solutions/security/packages/kbn-securitysolution-hook-utils/jest.config.js
+++ x-pack/solutions/security/packages/kbn-securitysolution-hook-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-securitysolution-hook-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/solutions/security/packages/kbn-securitysolution-hook-utils'],
 };
