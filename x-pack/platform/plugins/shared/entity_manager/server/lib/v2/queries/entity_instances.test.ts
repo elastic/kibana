8de0525cb94ef872511e2e7d24da3d185765224a
klacabane <kevin.lacabane@elastic.co>
2024-12-11 22:25:48 +0100

    unit test

--- x-pack/platform/plugins/shared/entity_manager/server/lib/v2/queries/entity_instances.test.ts
+++ x-pack/platform/plugins/shared/entity_manager/server/lib/v2/queries/entity_instances.test.ts
@@ -8,86 +8,84 @@
 import { getEntityInstancesQuery } from './entity_instances';
 
 describe('getEntityInstancesQuery', () => {
-  describe('getEntityInstancesQuery', () => {
-    it('generates a valid esql query', () => {
-      const { query, filter } = getEntityInstancesQuery({
-        source: {
-          id: 'service_source',
-          type_id: 'service',
-          index_patterns: ['logs-*', 'metrics-*'],
-          identity_fields: ['service.name'],
-          metadata_fields: ['host.name'],
-          filters: [],
-          timestamp_field: 'custom_timestamp_field',
-          display_name: 'service.id',
-        },
-        limit: 5,
-        start: '2024-11-20T19:00:00.000Z',
-        end: '2024-11-20T20:00:00.000Z',
-        sort: { field: 'entity.id', direction: 'DESC' },
-      });
+  it('generates a valid esql query', () => {
+    const { query, filter } = getEntityInstancesQuery({
+      source: {
+        id: 'service_source',
+        type_id: 'service',
+        index_patterns: ['logs-*', 'metrics-*'],
+        identity_fields: ['service.name'],
+        metadata_fields: ['host.name'],
+        filters: [],
+        timestamp_field: 'custom_timestamp_field',
+        display_name: 'service.id',
+      },
+      limit: 5,
+      start: '2024-11-20T19:00:00.000Z',
+      end: '2024-11-20T20:00:00.000Z',
+      sort: { field: 'entity.id', direction: 'DESC' },
+    });
 
-      expect(query).toEqual(
-        'FROM logs-*, metrics-* | ' +
-          'STATS host.name = VALUES(host.name::keyword), entity.last_seen_timestamp = MAX(custom_timestamp_field), service.id = MAX(service.id::keyword) BY service.name::keyword | ' +
-          'RENAME `service.name::keyword` AS service.name | ' +
-          'EVAL entity.type = "service", entity.id = service.name, entity.display_name = COALESCE(service.id, entity.id) | ' +
-          'SORT entity.id DESC | ' +
-          'LIMIT 5'
-      );
+    expect(query).toEqual(
+      'FROM logs-*, metrics-* | ' +
+        'STATS host.name = VALUES(host.name::keyword), entity.last_seen_timestamp = MAX(custom_timestamp_field), service.id = MAX(service.id::keyword) BY service.name::keyword | ' +
+        'RENAME `service.name::keyword` AS service.name | ' +
+        'EVAL entity.type = "service", entity.id = service.name, entity.display_name = COALESCE(service.id, entity.id) | ' +
+        'SORT entity.id DESC | ' +
+        'LIMIT 5'
+    );
 
-      expect(filter).toEqual({
-        bool: {
-          filter: [
-            {
-              bool: {
-                should: [
-                  {
-                    exists: {
-                      field: 'service.name',
-                    },
+    expect(filter).toEqual({
+      bool: {
+        filter: [
+          {
+            bool: {
+              should: [
+                {
+                  exists: {
+                    field: 'service.name',
                   },
-                ],
-                minimum_should_match: 1,
-              },
+                },
+              ],
+              minimum_should_match: 1,
             },
-            {
-              bool: {
-                filter: [
-                  {
-                    bool: {
-                      should: [
-                        {
-                          range: {
-                            custom_timestamp_field: {
-                              gte: '2024-11-20T19:00:00.000Z',
-                            },
+          },
+          {
+            bool: {
+              filter: [
+                {
+                  bool: {
+                    should: [
+                      {
+                        range: {
+                          custom_timestamp_field: {
+                            gte: '2024-11-20T19:00:00.000Z',
                           },
                         },
-                      ],
-                      minimum_should_match: 1,
-                    },
+                      },
+                    ],
+                    minimum_should_match: 1,
                   },
-                  {
-                    bool: {
-                      should: [
-                        {
-                          range: {
-                            custom_timestamp_field: {
-                              lte: '2024-11-20T20:00:00.000Z',
-                            },
+                },
+                {
+                  bool: {
+                    should: [
+                      {
+                        range: {
+                          custom_timestamp_field: {
+                            lte: '2024-11-20T20:00:00.000Z',
                           },
                         },
-                      ],
-                      minimum_should_match: 1,
-                    },
+                      },
+                    ],
+                    minimum_should_match: 1,
                   },
-                ],
-              },
+                },
+              ],
             },
-          ],
-        },
-      });
+          },
+        ],
+      },
     });
   });
 });
