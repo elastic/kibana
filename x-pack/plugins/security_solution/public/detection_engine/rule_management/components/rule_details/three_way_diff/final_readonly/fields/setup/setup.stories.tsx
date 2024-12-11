2dcbc5d5b8b5e539f09268cad9f86646042e5bfd
Maxim Palenov <maxim.palenov@elastic.co>
2024-12-11 12:02:25 +0100

    [Security Solution] Finalize Prebuilt Rule Customization workflow layout (#201501)
    **Addresses:** https://github.com/elastic/kibana/issues/171520
    
    ## Summary
    
    This PR updates Prebuilt Rules Customization UI to match the [design](https://www.figma.com/design/gLHm8LpTtSkAUQHrkG3RHU/%5B8.7%5D-%5BRules%5D-Rule-Immutability%2FCustomization?node-id=5146-77820). On top of that it renames and toss files to organize them in a much more clear way.
    
    ## Details
    
    A number of changes required to match the [design](https://www.figma.com/design/gLHm8LpTtSkAUQHrkG3RHU/%5B8.7%5D-%5BRules%5D-Rule-Immutability%2FCustomization?node-id=5146-77820)
    
    - move save/accept button to the field's right side header
    - expose additional field upgrade state `NoConflicts` to distinguish between `Accepted` and `NoConflicts` states
    - consume `ruleUpgradeState` in different components to display proper badges and texts
    
    Additional refactoring was done to facilitate the changes
    
    - `FinalRuleContext` and `FieldFinalEditContext` were merged together into `FieldUpgradeContext`
    - files and some folders were renamed and tossed to make the structure simpler and transparent
      There is still space for improvement. Current PR tries to minimize the changes to avoid too many conflicts since development is still in progress. And an additional limit of 200 path characters is imposed in Kibana preventing too deep foldering. Some functionality could be moved to a package later on to mitigate that obstacle.
    
    ## How to test locally
    
    - Ensure the `prebuiltRulesCustomizationEnabled` feature flag is enabled
    - Allow internal APIs via adding `server.restrictInternalApis: false` to `kibana.dev.yaml`
    - Clear Elasticsearch data
    - Run Elasticsearch and Kibana locally (do not open Kibana in a web browser)
    - Install an outdated version of the `security_detection_engine` Fleet package
    ```bash
    curl -X POST --user elastic:changeme  -H 'Content-Type: application/json' -H 'kbn-xsrf: 123' -H "elastic-api-version: 2023-10-31" -d '{"force":true}' http://localhost:5601/kbn/api/fleet/epm/packages/security_detection_engine/8.14.1
    ```
    
    - Install prebuilt rules
    ```bash
    curl -X POST --user elastic:changeme  -H 'Content-Type: application/json' -H 'kbn-xsrf: 123' -H "elastic-api-version: 1" -d '{"mode":"ALL_RULES"}' http://localhost:5601/kbn/internal/detection_engine/prebuilt_rules/installation/_perform
    ```
    - Open `Detection Rules (SIEM)` Page -> `Rule Updates`
    - Modify one or more prebuilt rules with updates to see unresolved conflicts (don't forget to refresh the page after rule's modification, it's required to clear the cache)
    
    ## Screenshots and screen recordings
    
    ![Screenshot 2024-11-26 at 13 42 16](https://github.com/user-attachments/assets/126bdc64-b810-424a-b81e-7674480a66cf)
    
    ![Screenshot 2024-11-26 at 13 42 27](https://github.com/user-attachments/assets/af5d9c2c-055d-423d-880a-b69e95ba129c)
    
    ![Screenshot 2024-11-26 at 13 42 40](https://github.com/user-attachments/assets/b0f87a95-df23-42a8-883b-29665f3db325)
    
    
    https://github.com/user-attachments/assets/7befe77a-3af7-48ea-a23f-aa0017d94be5
    
    
    https://github.com/user-attachments/assets/2837b405-3889-44a0-b1cb-d6c51cd9d051
    
    
    https://github.com/user-attachments/assets/7cbc411d-a927-45e9-8607-7ef515337ab5

--- x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_readonly/fields/setup/setup.stories.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_readonly/fields/setup/setup.stories.tsx
@@ -8,7 +8,7 @@
 import React from 'react';
 import type { Story } from '@storybook/react';
 import { SetupReadOnly } from './setup';
-import { FieldReadOnly } from '../../field_readonly';
+import { FieldFinalReadOnly } from '../../field_final_readonly';
 import type { DiffableRule } from '../../../../../../../../../common/api/detection_engine';
 import { mockCustomQueryRule } from '../../storybook/mocks';
 import { ThreeWayDiffStorybookProviders } from '../../storybook/three_way_diff_storybook_providers';
@@ -24,8 +24,8 @@ interface TemplateProps {
 
 const Template: Story<TemplateProps> = (args) => {
   return (
-    <ThreeWayDiffStorybookProviders finalDiffableRule={args.finalDiffableRule}>
-      <FieldReadOnly fieldName="setup" />
+    <ThreeWayDiffStorybookProviders finalDiffableRule={args.finalDiffableRule} fieldName="setup">
+      <FieldFinalReadOnly />
     </ThreeWayDiffStorybookProviders>
   );
 };
