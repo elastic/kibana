ae7f54d042676ffb7c2d155e07cc985beb072cea
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2024-12-11 10:43:31 +0100

    update time series explorer styles

--- x-pack/platform/plugins/shared/ml/public/application/timeseriesexplorer/timeseriesexplorer_page.tsx
+++ x-pack/platform/plugins/shared/ml/public/application/timeseriesexplorer/timeseriesexplorer_page.tsx
@@ -19,16 +19,13 @@ import { HelpMenu } from '../components/help_menu';
 import { useMlKibana } from '../contexts/kibana';
 import { MlPageHeader } from '../components/page_header';
 import { PageTitle } from '../components/page_title';
-import { getAnnotationStyles, getTimeseriesExplorerStyles } from './styles';
+import { useAnnotationStyles, useTimeseriesExplorerStyles } from './styles';
 
 interface TimeSeriesExplorerPageProps {
   dateFormatTz?: string;
   resizeRef?: any;
   noSingleMetricJobsFound?: boolean;
 }
-
-const timeseriesExplorerStyles = getTimeseriesExplorerStyles();
-const annotationStyles = getAnnotationStyles();
 
 export const TimeSeriesExplorerPage: FC<PropsWithChildren<TimeSeriesExplorerPageProps>> = ({
   children,
@@ -42,6 +39,9 @@ export const TimeSeriesExplorerPage: FC<PropsWithChildren<TimeSeriesExplorerPage
   const CasesContext = cases?.ui.getCasesContext() ?? React.Fragment;
   const casesPermissions = cases?.helpers.canUseCases();
   const helpLink = docLinks.links.ml.anomalyDetection;
+
+  const timeseriesExplorerStyles = useTimeseriesExplorerStyles();
+  const annotationStyles = useAnnotationStyles();
 
   return (
     <>
