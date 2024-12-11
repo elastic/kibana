e0f9cf7442d4074199e0ea7889d4b0b25607e820
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-12-11 22:34:53 +1100

    [8.x] Fix Custom Threshold Rule &#x60;ViewInAppUrl&#x60; does not honor space (#201793) (#203736)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [Fix Custom Threshold Rule &#x60;ViewInAppUrl&#x60; does not honor
    space (#201793)](https://github.com/elastic/kibana/pull/201793)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Anton
    Dosov","email":"anton.dosov@elastic.co"},"sourceCommit":{"committedDate":"2024-12-11T09:39:01Z","message":"Fix
    Custom Threshold Rule `ViewInAppUrl` does not honor space
    (#201793)\n\n## Summary\r\n\r\nClose
    https://github.com/elastic/kibana/issues/201378\r\nFix
    https://github.com/elastic/kibana/issues/201333\r\n\r\n- [Share] Allow
    to pass `spaceId` to `getRedirectUrl` to build a URL\r\nwith a specific
    `spaceId`\r\n- Fix Custom Threshold Rule ViewInAppUrl does not honor
    Space\r\n\r\n---------\r\n\r\nCo-authored-by: Maryam Saeidi
    <maryam.saeidi@elastic.co>","sha":"f1f3a4fddd34a08e33d2778a09f2166d6d1b02a0","branchLabelMapping":{"^v9.0.0$":"main","^v8.18.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:fix","v9.0.0","Team:SharedUX","ci:project-deploy-observability","Team:obs-ux-management","backport:version","v8.18.0","v8.17.1"],"title":"Fix
    Custom Threshold Rule `ViewInAppUrl` does not honor
    space","number":201793,"url":"https://github.com/elastic/kibana/pull/201793","mergeCommit":{"message":"Fix
    Custom Threshold Rule `ViewInAppUrl` does not honor space
    (#201793)\n\n## Summary\r\n\r\nClose
    https://github.com/elastic/kibana/issues/201378\r\nFix
    https://github.com/elastic/kibana/issues/201333\r\n\r\n- [Share] Allow
    to pass `spaceId` to `getRedirectUrl` to build a URL\r\nwith a specific
    `spaceId`\r\n- Fix Custom Threshold Rule ViewInAppUrl does not honor
    Space\r\n\r\n---------\r\n\r\nCo-authored-by: Maryam Saeidi
    <maryam.saeidi@elastic.co>","sha":"f1f3a4fddd34a08e33d2778a09f2166d6d1b02a0"}},"sourceBranch":"main","suggestedTargetBranches":["8.x","8.17"],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","branchLabelMappingKey":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/201793","number":201793,"mergeCommit":{"message":"Fix
    Custom Threshold Rule `ViewInAppUrl` does not honor space
    (#201793)\n\n## Summary\r\n\r\nClose
    https://github.com/elastic/kibana/issues/201378\r\nFix
    https://github.com/elastic/kibana/issues/201333\r\n\r\n- [Share] Allow
    to pass `spaceId` to `getRedirectUrl` to build a URL\r\nwith a specific
    `spaceId`\r\n- Fix Custom Threshold Rule ViewInAppUrl does not honor
    Space\r\n\r\n---------\r\n\r\nCo-authored-by: Maryam Saeidi
    <maryam.saeidi@elastic.co>","sha":"f1f3a4fddd34a08e33d2778a09f2166d6d1b02a0"}},{"branch":"8.x","label":"v8.18.0","branchLabelMappingKey":"^v8.18.0$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.17","label":"v8.17.1","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    Co-authored-by: Anton Dosov <anton.dosov@elastic.co>

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/alerting/custom_threshold/avg_ticks_fired.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/alerting/custom_threshold/avg_ticks_fired.ts
@@ -5,45 +5,44 @@
  * 2.0.
  */
 
-import moment from 'moment';
-import { format } from 'url';
+import { omit } from 'lodash';
 import expect from '@kbn/expect';
+import { cleanup, generate, Dataset, PartialConfig } from '@kbn/data-forge';
 import { COMPARATORS } from '@kbn/alerting-comparators';
-import { ApmSynthtraceEsClient } from '@kbn/apm-synthtrace';
 import { Aggregators } from '@kbn/observability-plugin/common/custom_threshold_rule/types';
 import { FIRED_ACTIONS_ID } from '@kbn/observability-plugin/server/lib/rules/custom_threshold/constants';
 import { OBSERVABILITY_THRESHOLD_RULE_TYPE_ID } from '@kbn/rule-data-utils';
+import { parseSearchParams } from '@kbn/share-plugin/common/url_service';
 import { kbnTestConfig } from '@kbn/test';
 import type { InternalRequestHeader, RoleCredentials } from '@kbn/ftr-common-functional-services';
 import { DeploymentAgnosticFtrProviderContext } from '../../../../ftr_provider_context';
-import { getSyntraceClient, generateData } from './helpers/syntrace';
-import { ActionDocument } from './types';
+import { ISO_DATE_REGEX } from './constants';
+import { ActionDocument, LogsExplorerLocatorParsedParams } from './types';
 
 export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
-  const start = moment(Date.now()).subtract(10, 'minutes').valueOf();
-  const end = moment(Date.now()).add(15, 'minutes').valueOf();
   const esClient = getService('es');
   const samlAuth = getService('samlAuth');
   const supertestWithoutAuth = getService('supertestWithoutAuth');
   const esDeleteAllIndices = getService('esDeleteAllIndices');
   const alertingApi = getService('alertingApi');
   const dataViewApi = getService('dataViewApi');
+  const logger = getService('log');
   const config = getService('config');
-  const kibanaServerConfig = config.get('servers.kibana');
   const isServerless = config.get('serverless');
   const expectedConsumer = isServerless ? 'observability' : 'logs';
-  const kibanaUrl = format(kibanaServerConfig);
+  const spacesService = getService('spaces');
   let roleAuthc: RoleCredentials;
   let internalReqHeader: InternalRequestHeader;
 
-  describe('AVG - US - FIRED', () => {
+  describe('AVG - TICKS - FIRED', () => {
     const CUSTOM_THRESHOLD_RULE_ALERT_INDEX = '.alerts-observability.threshold.alerts-default';
     const ALERT_ACTION_INDEX = 'alert-action-threshold';
-    const DATA_VIEW = 'traces-apm*,metrics-apm*,logs-apm*';
+    const DATA_VIEW = 'kbn-data-forge-fake_hosts.fake_hosts-*';
     const DATA_VIEW_ID = 'data-view-id';
     const DATA_VIEW_NAME = 'test-data-view-name';
-
-    let synthtraceEsClient: ApmSynthtraceEsClient;
+    const SPACE_ID = 'test-space';
+    let dataForgeConfig: PartialConfig;
+    let dataForgeIndices: string[];
     let actionId: string;
     let ruleId: string;
     let alertId: string;
@@ -51,13 +50,46 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
     before(async () => {
       roleAuthc = await samlAuth.createM2mApiKeyWithRoleScope('admin');
       internalReqHeader = samlAuth.getInternalRequestHeader();
-      synthtraceEsClient = await getSyntraceClient({ esClient, kibanaUrl });
-      await generateData({ synthtraceEsClient, start, end });
+      dataForgeConfig = {
+        schedule: [
+          {
+            template: 'good',
+            start: 'now-10m',
+            end: 'now+5m',
+            metrics: [
+              {
+                name: 'system.core.system.ticks',
+                method: 'linear',
+                start: 10_000_000,
+                end: 10_000_000,
+              },
+            ],
+          },
+        ],
+        indexing: {
+          dataset: 'fake_hosts' as Dataset,
+          eventsPerCycle: 1,
+          interval: 10000,
+          alignEventsToInterval: true,
+        },
+      };
+      dataForgeIndices = await generate({ client: esClient, config: dataForgeConfig, logger });
+      await alertingApi.waitForDocumentInIndex({
+        indexName: dataForgeIndices.join(','),
+        docCountTarget: 270,
+      });
       await dataViewApi.create({
         name: DATA_VIEW_NAME,
         id: DATA_VIEW_ID,
         title: DATA_VIEW,
+        spaceId: SPACE_ID,
         roleAuthc,
+      });
+      await spacesService.create({
+        id: SPACE_ID,
+        name: 'Test Space',
+        disabledFeatures: [],
+        color: '#AABBCC',
       });
     });
 
@@ -70,7 +102,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
         .delete(`/api/actions/connector/${actionId}`)
         .set(roleAuthc.apiKeyHeader)
         .set(internalReqHeader);
-      await esDeleteAllIndices([ALERT_ACTION_INDEX]);
+      await esDeleteAllIndices([ALERT_ACTION_INDEX, ...dataForgeIndices]);
       await esClient.deleteByQuery({
         index: CUSTOM_THRESHOLD_RULE_ALERT_INDEX,
         query: { term: { 'kibana.alert.rule.uuid': ruleId } },
@@ -79,11 +111,13 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
         index: '.kibana-event-log-*',
         query: { term: { 'kibana.alert.rule.consumer': expectedConsumer } },
       });
-      await synthtraceEsClient.clean();
       await dataViewApi.delete({
         id: DATA_VIEW_ID,
+        spaceId: SPACE_ID,
         roleAuthc,
       });
+      await cleanup({ client: esClient, config: dataForgeConfig, logger });
+      await spacesService.delete(SPACE_ID);
       await samlAuth.invalidateM2mApiKeyWithRoleScope(roleAuthc);
     });
 
@@ -93,10 +127,12 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
           roleAuthc,
           name: 'Index Connector: Threshold API test',
           indexName: ALERT_ACTION_INDEX,
+          spaceId: SPACE_ID,
         });
 
         const createdRule = await alertingApi.createRule({
           roleAuthc,
+          spaceId: SPACE_ID,
           tags: ['observability'],
           consumer: expectedConsumer,
           name: 'Threshold rule',
@@ -109,7 +145,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
                 timeSize: 5,
                 timeUnit: 'm',
                 metrics: [
-                  { name: 'A', field: 'span.self_time.sum.us', aggType: Aggregators.AVERAGE },
+                  { name: 'A', field: 'system.core.system.ticks', aggType: Aggregators.AVERAGE },
                 ],
               },
             ],
@@ -134,6 +170,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
                     alertDetailsUrl: '{{context.alertDetailsUrl}}',
                     reason: '{{context.reason}}',
                     value: '{{context.value}}',
+                    viewInAppUrl: '{{context.viewInAppUrl}}',
                   },
                 ],
               },
@@ -154,6 +191,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
           roleAuthc,
           ruleId,
           expectedStatus: 'active',
+          spaceId: SPACE_ID,
         });
         expect(executionStatus).to.be('active');
       });
@@ -178,7 +216,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
           'observability.rules.custom_threshold'
         );
         expect(resp.hits.hits[0]._source).property('kibana.alert.rule.uuid', ruleId);
-        expect(resp.hits.hits[0]._source).property('kibana.space_ids').contain('default');
+        expect(resp.hits.hits[0]._source).property('kibana.space_ids').contain(SPACE_ID);
         expect(resp.hits.hits[0]._source)
           .property('kibana.alert.rule.tags')
           .contain('observability');
@@ -203,7 +241,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
                 threshold: [7500000],
                 timeSize: 5,
                 timeUnit: 'm',
-                metrics: [{ name: 'A', field: 'span.self_time.sum.us', aggType: 'avg' }],
+                metrics: [{ name: 'A', field: 'system.core.system.ticks', aggType: 'avg' }],
               },
             ],
             alertOnNoData: true,
@@ -220,12 +258,30 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
         const { protocol, hostname, port } = kbnTestConfig.getUrlPartsWithStrippedDefaultPort();
         expect(resp.hits.hits[0]._source?.ruleType).eql('observability.rules.custom_threshold');
         expect(resp.hits.hits[0]._source?.alertDetailsUrl).eql(
-          `${protocol}://${hostname}${port ? `:${port}` : ''}/app/observability/alerts/${alertId}`
+          `${protocol}://${hostname}${
+            port ? `:${port}` : ''
+          }/s/${SPACE_ID}/app/observability/alerts/${alertId}`
         );
         expect(resp.hits.hits[0]._source?.reason).eql(
-          `Average span.self_time.sum.us is 10,000,000, above the threshold of 7,500,000. (duration: 5 mins, data view: ${DATA_VIEW_NAME})`
+          `Average system.core.system.ticks is 10,000,000, above the threshold of 7,500,000. (duration: 5 mins, data view: ${DATA_VIEW_NAME})`
         );
         expect(resp.hits.hits[0]._source?.value).eql('10,000,000');
+
+        const parsedViewInAppUrl = parseSearchParams<LogsExplorerLocatorParsedParams>(
+          new URL(resp.hits.hits[0]._source?.viewInAppUrl || '').search
+        );
+        const viewInAppUrlPathName = new URL(resp.hits.hits[0]._source?.viewInAppUrl || '')
+          .pathname;
+
+        expect(viewInAppUrlPathName).contain(`/s/${SPACE_ID}/app/r`);
+        expect(resp.hits.hits[0]._source?.viewInAppUrl).contain('LOGS_EXPLORER_LOCATOR');
+        expect(omit(parsedViewInAppUrl.params, 'timeRange.from')).eql({
+          dataset: DATA_VIEW_ID,
+          timeRange: { to: 'now' },
+          query: { query: '', language: 'kuery' },
+          filters: [],
+        });
+        expect(parsedViewInAppUrl.params.timeRange.from).match(ISO_DATE_REGEX);
       });
     });
   });
