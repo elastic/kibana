18fde3b649de8fe54af104f8408c518e833937c7
Marshall Main <marshall.main@elastic.co>
2024-12-11 13:57:23 -0500

    Relocating module `@kbn/securitysolution-es-utils`

--- src/platform/packages/shared/kbn-securitysolution-es-utils/jest.config.js
+++ src/platform/packages/shared/kbn-securitysolution-es-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-securitysolution-es-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-securitysolution-es-utils'],
 };
