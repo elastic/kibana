799430290546603f9e3fa40297410114d8d9115d
Matthew Kime <matt@mattki.me>
2024-12-12 11:18:01 -0600

    Relocating module `@kbn/searchprofiler-plugin`

--- x-pack/platform/plugins/shared/searchprofiler/jest.config.js
+++ x-pack/platform/plugins/shared/searchprofiler/jest.config.js
@@ -7,11 +7,11 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/searchprofiler'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/searchprofiler',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/plugins/shared/searchprofiler'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/shared/searchprofiler',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/x-pack/plugins/searchprofiler/{common,public,server}/**/*.{ts,tsx}',
+    '<rootDir>/x-pack/platform/plugins/shared/searchprofiler/{common,public,server}/**/*.{ts,tsx}',
   ],
 };
