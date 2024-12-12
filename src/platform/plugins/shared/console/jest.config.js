c35c6ee2bd253ae0330cb6529e3d8e292d4fc82b
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-12-12 18:11:28 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/platform/plugins/shared/console/jest.config.js
+++ src/platform/plugins/shared/console/jest.config.js
@@ -13,5 +13,7 @@ module.exports = {
   roots: ['<rootDir>/src/platform/plugins/shared/console'],
   coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/shared/console',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/src/platform/plugins/shared/console/{common,public,server}/**/*.{js,ts,tsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/src/platform/plugins/shared/console/{common,public,server}/**/*.{js,ts,tsx}',
+  ],
 };
--- x-pack/platform/plugins/private/painless_lab/jest.config.js
+++ x-pack/platform/plugins/private/painless_lab/jest.config.js
@@ -9,7 +9,8 @@ module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
   roots: ['<rootDir>/x-pack/platform/plugins/private/painless_lab'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/private/painless_lab',
+  coverageDirectory:
+    '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/private/painless_lab',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
     '<rootDir>/x-pack/platform/plugins/private/painless_lab/{common,public,server}/**/*.{ts,tsx}',
