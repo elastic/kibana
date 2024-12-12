86b24627dc4ac435f8030eb752fad75133ff0cbb
shahzad31 <shahzad31comp@gmail.com>
2024-12-12 12:17:05 +0100

    Relocating module `@kbn/investigation-shared`

--- x-pack/solutions/observability/packages/kbn-investigation-shared/jest.config.js
+++ x-pack/solutions/observability/packages/kbn-investigation-shared/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-investigation-shared'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/solutions/observability/packages/kbn-investigation-shared'],
 };
