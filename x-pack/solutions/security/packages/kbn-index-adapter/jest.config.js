7cc85d225b97b7b31b3c3ab389edc4451538a346
PhilippeOberti <philippe.oberti@elastic.co>
2024-12-12 16:33:53 -0600

    Relocating module `@kbn/index-adapter`

--- x-pack/solutions/security/packages/kbn-index-adapter/jest.config.js
+++ x-pack/solutions/security/packages/kbn-index-adapter/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-index-adapter'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/solutions/security/packages/kbn-index-adapter'],
 };
