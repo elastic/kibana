f7c006dcea971e4e8137a7ab9e5e9b64b61712cd
Alexandra Doak <alexandra.doak@elastic.co>
2024-12-12 13:30:00 -0800

    Adding functional tests

--- x-pack/plugins/stack_connectors/server/connector_types/crowdstrike/index.ts
+++ x-pack/plugins/stack_connectors/server/connector_types/crowdstrike/index.ts
@@ -9,10 +9,22 @@ import {
   SubActionConnectorType,
   ValidatorType,
 } from '@kbn/actions-plugin/server/sub_action_framework/types';
-import { SecurityConnectorFeatureId } from '@kbn/actions-plugin/common';
-import { urlAllowListValidator } from '@kbn/actions-plugin/server';
+import { EdrForSecurityConnectorFeatureId } from '@kbn/actions-plugin/common';
+import {
+  urlAllowListValidator,
+  SubFeatureType,
+  ActionExecutionSourceType,
+} from '@kbn/actions-plugin/server';
+import {
+  EDR_EXECUTE_PRIVILEGE,
+  EDR_SUB_ACTIONS_EXECUTE_PRIVILEGE,
+} from '@kbn/actions-plugin/server/feature';
 import { ExperimentalFeatures } from '../../../common/experimental_features';
-import { CROWDSTRIKE_CONNECTOR_ID, CROWDSTRIKE_TITLE } from '../../../common/crowdstrike/constants';
+import {
+  CROWDSTRIKE_CONNECTOR_ID,
+  CROWDSTRIKE_TITLE,
+  SUB_ACTION,
+} from '../../../common/crowdstrike/constants';
 import {
   CrowdstrikeConfigSchema,
   CrowdstrikeSecretsSchema,
@@ -31,6 +43,17 @@ export const getCrowdstrikeConnectorType = (
     secrets: CrowdstrikeSecretsSchema,
   },
   validators: [{ type: ValidatorType.CONFIG, validator: urlAllowListValidator('url') }],
-  supportedFeatureIds: [SecurityConnectorFeatureId],
+  supportedFeatureIds: [EdrForSecurityConnectorFeatureId],
   minimumLicenseRequired: 'enterprise' as const,
+  subFeatureType: SubFeatureType.EDR,
+  getKibanaPrivileges: (args) => {
+    const privileges = [EDR_EXECUTE_PRIVILEGE];
+    if (
+      args?.source === ActionExecutionSourceType.HTTP_REQUEST &&
+      args?.params?.subAction !== SUB_ACTION.GET_AGENT_DETAILS
+    ) {
+      privileges.push(EDR_SUB_ACTIONS_EXECUTE_PRIVILEGE);
+    }
+    return privileges;
+  },
 });
