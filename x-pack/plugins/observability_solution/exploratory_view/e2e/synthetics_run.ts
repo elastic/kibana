0203bba44f9dcac037f23a23e4945298ef6b5912
Shahzad <shahzad31comp@gmail.com>
2024-12-12 10:52:19 +0100

    [Synthetics] Clean up e2e test helpers !! (#203812)
    ## Summary
    
    Clean up e2e test helpers

--- x-pack/plugins/observability_solution/exploratory_view/e2e/synthetics_run.ts
+++ x-pack/plugins/observability_solution/exploratory_view/e2e/synthetics_run.ts
@@ -6,8 +6,8 @@
  */
 import { FtrConfigProviderContext } from '@kbn/test';
 import path from 'path';
-import { SyntheticsRunner } from './synthetics_runner';
-import { argv } from './parse_args_params';
+import { REPO_ROOT } from '@kbn/repo-info';
+import { SyntheticsRunner, argv } from '@kbn/observability-synthetics-test-data';
 
 const { headless, grep, bail: pauseOnError } = argv;
 
@@ -24,13 +24,12 @@ async function runE2ETests({ readConfigFile }: FtrConfigProviderContext) {
       });
 
       await syntheticsRunner.setup();
-
-      await syntheticsRunner.loadTestData(path.join(__dirname, '../../ux/e2e/fixtures/'), [
-        'rum_8.0.0',
-        'rum_test_data',
-      ]);
       await syntheticsRunner.loadTestData(
-        path.join(__dirname, '../../synthetics/e2e/fixtures/es_archiver/'),
+        `${REPO_ROOT}/x-pack/plugins/observability_solution/ux/e2e/fixtures/`,
+        ['rum_8.0.0', 'rum_test_data']
+      );
+      await syntheticsRunner.loadTestData(
+        `${REPO_ROOT}/x-pack/plugins/observability_solution/synthetics/e2e/fixtures/es_archiver/`,
         ['full_heartbeat', 'browser']
       );
       await syntheticsRunner.loadTestFiles(async () => {
