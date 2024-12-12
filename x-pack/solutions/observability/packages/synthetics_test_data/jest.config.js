33873ab4b44cb9d3d7f66fa83ab2930f8a0bfa52
shahzad31 <shahzad31comp@gmail.com>
2024-12-12 12:24:58 +0100

    Relocating module `@kbn/observability-synthetics-test-data`

--- x-pack/solutions/observability/packages/synthetics_test_data/jest.config.js
+++ x-pack/solutions/observability/packages/synthetics_test_data/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/x-pack/packages/observability/synthetics_test_data'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/solutions/observability/packages/synthetics_test_data'],
 };
