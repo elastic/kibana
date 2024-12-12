a05b391c297828d959481de78da773e4daaec26c
Paul Tavares <paul.tavares@elastic.co>
2024-12-12 14:48:11 -0500

    Started to add mocks for MS defend

--- x-pack/plugins/stack_connectors/server/connector_types/microsoft_defender_endpoint/mocks.ts
+++ x-pack/plugins/stack_connectors/server/connector_types/microsoft_defender_endpoint/mocks.ts
@@ -4,3 +4,33 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { SubActionRequestParams } from '@kbn/actions-plugin/server/sub_action_framework/types';
+import { createAxiosResponseMock } from '../lib/mocks';
+import { MicrosoftDefenderEndpointConnector } from './microsoft_defender_endpoint';
+
+class MicrosoftDefenderEndpointConnectorTestClass extends MicrosoftDefenderEndpointConnector {
+  public mockResponses = {};
+
+  public requestSpy = jest.fn(async ({ url }: SubActionRequestParams<any>) => {
+    const response = createAxiosResponseMock({});
+
+    // Mocks some of the MS API responses
+    if (url.endsWith('/agents')) {
+      // TODO:PT implement
+    }
+
+    return response;
+  });
+}
+
+const createMicrosoftDefenderTestInstance = () => {
+  //
+};
+
+export const microsoftDefenderEndpointConnectorMocks = Object.freeze({
+  create: createMicrosoftDefenderTestInstance,
+  createAxiosResponseMock,
+});
