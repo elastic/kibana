d7917005c8c598d271c02a138fb0ef8190a152b4
Nathan Reese <reese.nathan@elastic.co>
2024-12-12 08:38:11 -0700

    [embeddable] remove getAttributeService from start API (#203660)
    Part of embeddable refactor cleanup
    
    AttributeService is moved from embeddable plugin to visualizations
    plugin.
    
    PR reduces visualizations bundle size by avoiding importing
    `legacy/embeddable/index.ts` in plugin page load
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    (cherry picked from commit 9ce523939279bdeba399b447849db47fa0877a23)

--- src/plugins/embeddable/public/mocks.tsx
+++ src/plugins/embeddable/public/mocks.tsx
@@ -41,7 +41,6 @@ import {
 } from './react_embeddable_system';
 import { registerAddFromLibraryType } from './add_from_library/registry';
 
-export { mockAttributeService } from './lib/attribute_service/attribute_service.mock';
 export type Setup = jest.Mocked<EmbeddableSetup>;
 export type Start = jest.Mocked<EmbeddableStart>;
 
@@ -118,7 +117,6 @@ const createStartContract = (): Start => {
     inject: jest.fn(),
     getAllMigrations: jest.fn(),
     getStateTransfer: jest.fn(() => createEmbeddableStateTransferMock() as EmbeddableStateTransfer),
-    getAttributeService: jest.fn(),
   };
   return startContract;
 };
