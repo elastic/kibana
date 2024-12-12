fe522dfe723fbd3e719c2a5e6b8684fee6183f3a
Matthew Kime <matt@mattki.me>
2024-12-12 11:22:52 -0600

    Relocating module `@kbn/watcher-plugin`

--- x-pack/platform/plugins/private/watcher/jest.config.js
+++ x-pack/platform/plugins/private/watcher/jest.config.js
@@ -7,9 +7,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/watcher'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/watcher',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/plugins/private/watcher'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/private/watcher',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/watcher/{common,public,server}/**/*.{js,ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/private/watcher/{common,public,server}/**/*.{js,ts,tsx}'],
 };
