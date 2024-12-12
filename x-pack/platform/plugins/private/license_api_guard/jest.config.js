6ae1466f65969c77e234ade72f7991e937f718df
Matthew Kime <matt@mattki.me>
2024-12-12 10:49:01 -0600

    Relocating module `@kbn/license-api-guard-plugin`

--- x-pack/platform/plugins/private/license_api_guard/jest.config.js
+++ x-pack/platform/plugins/private/license_api_guard/jest.config.js
@@ -7,9 +7,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/license_api_guard'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/license_api_guard',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/plugins/private/license_api_guard'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/private/license_api_guard',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/license_api_guard/server/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/private/license_api_guard/server/**/*.{ts,tsx}'],
 };
