30c07d9f8eb14d2848b9d5c98431f84ed4dd2f34
Brad White <brad.white@elastic.co>
2024-12-12 21:48:01 -0700

    fix crash in story from theme

--- packages/kbn-esql-editor/src/esql_editor.tsx
+++ packages/kbn-esql-editor/src/esql_editor.tsx
@@ -98,7 +98,7 @@ export const ESQLEditor = memo(function ESQLEditor({
     fieldsMetadata,
     uiSettings,
   } = kibana.services;
-  const darkMode = core.theme?.getTheme().darkMode;
+  const darkMode = core?.theme?.getTheme().darkMode;
 
   const histogramBarTarget = uiSettings?.get('histogram:barTarget') ?? 50;
   const [code, setCode] = useState<string>(query.esql ?? '');
