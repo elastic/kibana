7bbd1afb98c4f01f6bd3d2f7009598a62ab3ac01
dej611 <dej611@gmail.com>
2024-12-13 15:41:34 +0100

    :white_check_mark: Use the new internal api

--- x-pack/plugins/lens/public/react_embeddable/initializers/initialize_actions.test.ts
+++ x-pack/plugins/lens/public/react_embeddable/initializers/initialize_actions.test.ts
@@ -13,8 +13,8 @@ import {
   getLensApiMock,
   makeEmbeddableServices,
   getLensRuntimeStateMock,
-  getVisualizationContextHelperMock,
   createUnifiedSearchApi,
+  getLensInternalApiMock,
 } from '../mocks';
 import { createEmptyLensState } from '../helper';
 const DATAVIEW_ID = 'myDataView';
@@ -40,11 +40,17 @@ function setupActionsApi(
 ) {
   const services = makeEmbeddableServices(undefined, undefined, {
     visOverrides: { id: 'lnsXY' },
-    dataOverrides: { id: 'form_based' },
+    dataOverrides: { id: 'formBased' },
   });
   const uuid = faker.string.uuid();
   const runtimeState = getLensRuntimeStateMock(stateOverrides);
   const apiMock = getLensApiMock();
+  // create the internal API and customize internal state
+  const internalApi = getLensInternalApiMock();
+  internalApi.updateAttributes(runtimeState.attributes);
+  if (contextOverrides) {
+    internalApi.updateVisualizationContext(contextOverrides);
+  }
 
   const { api } = initializeActionApi(
     uuid,
@@ -53,7 +59,7 @@ function setupActionsApi(
     createUnifiedSearchApi(),
     pick(apiMock, ['timeRange$']),
     pick(apiMock, ['panelTitle']),
-    getVisualizationContextHelperMock(stateOverrides?.attributes, contextOverrides),
+    internalApi,
     {
       ...services,
       data: {
