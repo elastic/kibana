dd2634551648c619d8fcc90039d64e9f9ffdd15c
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-13 10:38:36 +0100

    Relocating module `@kbn/server-route-repository-client`

--- src/platform/packages/shared/kbn-server-route-repository-client/jest.config.js
+++ src/platform/packages/shared/kbn-server-route-repository-client/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-server-route-repository-client'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-server-route-repository-client'],
 };
