e15e583f0654c993ccec0f297b424d7adf7f1cf8
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2024-12-13 13:47:07 -0500

    test

--- x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/hooks/use_fetch_agents_data.test.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/sections/agents/agent_list_page/hooks/use_fetch_agents_data.test.tsx
@@ -5,6 +5,9 @@
  * 2.0.
  */
 
+/* eslint-disable ban/ban */
+/* eslint-disable jest/no-focused-tests */
+
 import { act, waitFor } from '@testing-library/react';
 
 import { useStartServices } from '../../../../hooks';
@@ -101,74 +104,99 @@ jest.mock('../../../../hooks', () => ({
   }),
 }));
 
-describe('useFetchAgentsData', () => {
-  const startServices = useStartServices();
-  const mockErrorToast = startServices.notifications.toasts.addError as jest.Mock;
+describe.only('Attempt to reproduce test flakiness', () => {
+  for (let i = 0; i < 1000; i++) {
+    describe(`useFetchAgentsData - ${i + 1}`, () => {
+      const startServices = useStartServices();
+      const mockErrorToast = startServices.notifications.toasts.addError as jest.Mock;
 
-  beforeAll(() => {
-    mockedExperimentalFeaturesService.get.mockReturnValue({
-      displayAgentMetrics: true,
-    } as any);
-  });
+      beforeAll(() => {
+        mockedExperimentalFeaturesService.get.mockReturnValue({
+          displayAgentMetrics: true,
+        } as any);
+      });
 
-  beforeEach(() => {
-    mockErrorToast.mockReset();
-    mockErrorToast.mockResolvedValue({});
-  });
+      beforeEach(() => {
+        mockErrorToast.mockReset();
+        mockErrorToast.mockResolvedValue({});
+      });
 
-  it('should fetch agents and agent policies data', async () => {
-    const renderer = createFleetTestRendererMock();
-    const { result } = renderer.renderHook(() => useFetchAgentsData());
-    await waitFor(() => new Promise((resolve) => resolve(null)));
+      it('should fetch agents and agent policies data', async () => {
+        const renderer = createFleetTestRendererMock();
+        const { result } = renderer.renderHook(() => useFetchAgentsData());
+        await waitFor(() => new Promise((resolve) => resolve(null)));
 
-    expect(result?.current.selectedStatus).toEqual(['healthy', 'unhealthy', 'updating', 'offline']);
-    expect(result?.current.allAgentPolicies).toEqual([
-      {
-        id: 'agent-policy-1',
-        name: 'Agent policy 1',
-        namespace: 'default',
-      },
-      {
-        id: 'agent-policy-managed',
-        managed: true,
-        name: 'Managed Agent policy',
-        namespace: 'default',
-      },
-    ]);
+        expect(result?.current.selectedStatus).toEqual([
+          'healthy',
+          'unhealthy',
+          'updating',
+          'offline',
+        ]);
+        expect(result?.current.allAgentPolicies).toEqual([
+          {
+            id: 'agent-policy-1',
+            name: 'Agent policy 1',
+            namespace: 'default',
+          },
+          {
+            id: 'agent-policy-managed',
+            managed: true,
+            name: 'Managed Agent policy',
+            namespace: 'default',
+          },
+        ]);
 
-    expect(result?.current.agentPoliciesIndexedById).toEqual({
-      'agent-policy-1': {
-        id: 'agent-policy-1',
-        name: 'Agent policy 1',
-        namespace: 'default',
-      },
+        expect(result?.current.agentPoliciesIndexedById).toEqual({
+          'agent-policy-1': {
+            id: 'agent-policy-1',
+            name: 'Agent policy 1',
+            namespace: 'default',
+          },
+        });
+        expect(result?.current.kuery).toEqual(
+          'status:online or (status:error or status:degraded) or (status:updating or status:unenrolling or status:enrolling) or status:offline'
+        );
+        expect(result?.current.currentRequestRef).toEqual({ current: 2 });
+        expect(result?.current.pagination).toEqual({ currentPage: 1, pageSize: 5 });
+        expect(result?.current.pageSizeOptions).toEqual([5, 20, 50]);
+      });
+
+      it('sync querystring kuery with current search', async () => {
+        process.stdout.write(`\niteration ${i + 1}\n`);
+        let start = Date.now();
+
+        const renderer = createFleetTestRendererMock();
+        const { result } = renderer.renderHook(() => useFetchAgentsData());
+        process.stdout.write(`\n initial render ${Date.now() - start}ms \n`);
+
+        start = Date.now();
+        await waitFor(() => expect(renderer.history.location.search).toEqual(''));
+        process.stdout.write(`\n empty search ${Date.now() - start}ms \n`);
+
+        // Set search
+        start = Date.now();
+        await act(async () => {
+          result.current.setSearch('active:true');
+        });
+        process.stdout.write(`\n set search to active: true ${Date.now() - start}ms \n`);
+
+        start = Date.now();
+        await waitFor(() =>
+          expect(renderer.history.location.search).toEqual('?kuery=active%3Atrue')
+        );
+        process.stdout.write(`\n check query params ${Date.now() - start}ms \n`);
+
+        // Clear search
+        start = Date.now();
+        await act(async () => {
+          result.current.setSearch('');
+        });
+        process.stdout.write(`\n clear search ${Date.now() - start}ms \n`);
+
+        start = Date.now();
+        await waitFor(() => expect(renderer.history.location.search).toEqual(''));
+        process.stdout.write(`\n reset query params ${Date.now() - start}ms \n`);
+      });
     });
-    expect(result?.current.kuery).toEqual(
-      'status:online or (status:error or status:degraded) or (status:updating or status:unenrolling or status:enrolling) or status:offline'
-    );
-    expect(result?.current.currentRequestRef).toEqual({ current: 2 });
-    expect(result?.current.pagination).toEqual({ currentPage: 1, pageSize: 5 });
-    expect(result?.current.pageSizeOptions).toEqual([5, 20, 50]);
-  });
-
-  it('sync querystring kuery with current search', async () => {
-    const renderer = createFleetTestRendererMock();
-    const { result } = renderer.renderHook(() => useFetchAgentsData());
-
-    await waitFor(() => expect(renderer.history.location.search).toEqual(''));
-
-    // Set search
-    await act(async () => {
-      result.current.setSearch('active:true');
-    });
-
-    await waitFor(() => expect(renderer.history.location.search).toEqual('?kuery=active%3Atrue'));
-
-    // Clear search
-    await act(async () => {
-      result.current.setSearch('');
-    });
-
-    await waitFor(() => expect(renderer.history.location.search).toEqual(''));
-  });
+  }
 });
