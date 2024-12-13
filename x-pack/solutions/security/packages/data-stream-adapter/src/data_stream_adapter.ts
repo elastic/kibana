2bb7f3b88666fa1a45543623a17640813f2991a4
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-13 15:25:18 +0100

    Fix incorrect paths in eslintrc.js (#204200)
    ## Summary
    
    They were already wrong [before the
    relocation](https://github.com/elastic/kibana/pull/183149/files#diff-e2954b558f2aa82baff0e30964490d12942e0e251c1aa56c3294de6ec67b7cf5):
    
    `x-pack/packages/kbn-data-stream-adapter` // was never under x-pack
    
    I use the opportunity to sort them alphabetically.
    
    cc @semd
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/solutions/security/packages/data-stream-adapter/src/data_stream_adapter.ts
+++ x-pack/solutions/security/packages/data-stream-adapter/src/data_stream_adapter.ts
@@ -5,7 +5,8 @@
  * 2.0.
  */
 
-import { IndexAdapter, SetIndexTemplateParams, type InstallParams } from '@kbn/index-adapter';
+import type { SetIndexTemplateParams } from '@kbn/index-adapter';
+import { IndexAdapter, type InstallParams } from '@kbn/index-adapter';
 import { createOrUpdateDataStream } from './create_or_update_data_stream';
 
 export class DataStreamAdapter extends IndexAdapter {
