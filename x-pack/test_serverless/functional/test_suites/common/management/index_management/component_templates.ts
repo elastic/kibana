7529c26158b657dfbb41ca9e666b8a06b2828ade
Matthew Kime <matt@mattki.me>
2024-12-16 00:07:18 -0600

    fix functional test auth issue

--- x-pack/test_serverless/functional/test_suites/common/management/index_management/component_templates.ts
+++ x-pack/test_serverless/functional/test_suites/common/management/index_management/component_templates.ts
@@ -20,88 +20,88 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const TEST_COMPONENT_TEMPLATE = '.a_test_component_template';
 
   describe('Index component templates', function () {
-    before(async () => {
-      // await security.testUser.setRoles(['index_management_user']);
-      await pageObjects.svlCommonPage.loginAsAdmin();
-      // await security.testUser.setRoles(['index_management_manage_index_templates']);
-    });
-
-    beforeEach(async () => {
-      await pageObjects.common.navigateToApp('indexManagement');
-      // Navigate to the index templates tab
-      await pageObjects.indexManagement.changeTabs('component_templatesTab');
-      await pageObjects.header.waitUntilLoadingHasFinished();
-    });
-
-    it('renders the component templates tab', async () => {
-      const url = await browser.getCurrentUrl();
-      expect(url).to.contain(`/component_templates`);
-    });
-
-    describe('Component templates list', () => {
+    describe('with access', () => {
       before(async () => {
-        await es.cluster.putComponentTemplate({
-          name: TEST_COMPONENT_TEMPLATE,
-          body: {
-            template: {
-              settings: {
-                index: {
-                  number_of_shards: 1,
+        await pageObjects.svlCommonPage.loginAsAdmin();
+      });
+
+      beforeEach(async () => {
+        await pageObjects.common.navigateToApp('indexManagement');
+        // Navigate to the index templates tab
+        await pageObjects.indexManagement.changeTabs('component_templatesTab');
+        await pageObjects.header.waitUntilLoadingHasFinished();
+      });
+
+      it('renders the component templates tab', async () => {
+        const url = await browser.getCurrentUrl();
+        expect(url).to.contain(`/component_templates`);
+      });
+
+      describe('Component templates list', () => {
+        before(async () => {
+          await es.cluster.putComponentTemplate({
+            name: TEST_COMPONENT_TEMPLATE,
+            body: {
+              template: {
+                settings: {
+                  index: {
+                    number_of_shards: 1,
+                  },
                 },
               },
             },
-          },
+          });
+        });
+
+        after(async () => {
+          await es.cluster.deleteComponentTemplate(
+            { name: TEST_COMPONENT_TEMPLATE },
+            { ignore: [404] }
+          );
+        });
+
+        it('Displays the test component template in the list', async () => {
+          const templates = await testSubjects.findAll('componentTemplateTableRow');
+
+          const getTemplateName = async (template: WebElementWrapper) => {
+            const templateNameElement = await template.findByTestSubject('templateDetailsLink');
+            return await templateNameElement.getVisibleText();
+          };
+
+          const componentTemplateList = await Promise.all(
+            templates.map((template) => getTemplateName(template))
+          );
+
+          const newComponentTemplateExists = Boolean(
+            componentTemplateList.find((templateName) => templateName === TEST_COMPONENT_TEMPLATE)
+          );
+
+          expect(newComponentTemplateExists).to.be(true);
         });
       });
 
-      after(async () => {
-        await es.cluster.deleteComponentTemplate(
-          { name: TEST_COMPONENT_TEMPLATE },
-          { ignore: [404] }
-        );
-      });
+      describe('Create component template', () => {
+        after(async () => {
+          await es.cluster.deleteComponentTemplate(
+            { name: TEST_COMPONENT_TEMPLATE },
+            { ignore: [404] }
+          );
+        });
 
-      it('Displays the test component template in the list', async () => {
-        const templates = await testSubjects.findAll('componentTemplateTableRow');
+        it('Creates component template', async () => {
+          await testSubjects.click('createPipelineButton');
 
-        const getTemplateName = async (template: WebElementWrapper) => {
-          const templateNameElement = await template.findByTestSubject('templateDetailsLink');
-          return await templateNameElement.getVisibleText();
-        };
+          await testSubjects.setValue('nameField', TEST_COMPONENT_TEMPLATE);
 
-        const componentTemplateList = await Promise.all(
-          templates.map((template) => getTemplateName(template))
-        );
+          // Finish wizard flow
+          await testSubjects.click('nextButton');
+          await testSubjects.click('nextButton');
+          await testSubjects.click('nextButton');
+          await testSubjects.click('nextButton');
+          await testSubjects.click('nextButton');
 
-        const newComponentTemplateExists = Boolean(
-          componentTemplateList.find((templateName) => templateName === TEST_COMPONENT_TEMPLATE)
-        );
-
-        expect(newComponentTemplateExists).to.be(true);
-      });
-    });
-
-    describe('Create component template', () => {
-      after(async () => {
-        await es.cluster.deleteComponentTemplate(
-          { name: TEST_COMPONENT_TEMPLATE },
-          { ignore: [404] }
-        );
-      });
-
-      it('Creates component template', async () => {
-        await testSubjects.click('createPipelineButton');
-
-        await testSubjects.setValue('nameField', TEST_COMPONENT_TEMPLATE);
-
-        // Finish wizard flow
-        await testSubjects.click('nextButton');
-        await testSubjects.click('nextButton');
-        await testSubjects.click('nextButton');
-        await testSubjects.click('nextButton');
-        await testSubjects.click('nextButton');
-
-        expect(await testSubjects.getVisibleText('title')).to.contain(TEST_COMPONENT_TEMPLATE);
+          expect(await testSubjects.getVisibleText('title')).to.contain(TEST_COMPONENT_TEMPLATE);
+        });
       });
     });
 
