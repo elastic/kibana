b3bb5ebc305ebec488c313b7b1c72b7f8b94268e
Sergi Massaneda <sergi.massaneda@elastic.co>
2024-12-16 12:33:48 +0100

    move code to siem_migrations public directory

---
+++ x-pack/plugins/security_solution/public/siem_migrations/rules/components/panels/migration_progress_panel.tsx
@@ -0,0 +1,76 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import {
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiText,
+  EuiPanel,
+  EuiProgress,
+  EuiLoadingSpinner,
+  EuiIcon,
+} from '@elastic/eui';
+import { AssistantIcon } from '@kbn/ai-assistant-icon';
+import type { RuleMigrationStats } from '../../types';
+import * as i18n from '../../../../onboarding/components/onboarding_body/cards/siem_migrations/start_migration/translations';
+import { TITLE_CLASS_NAME } from '../../../../onboarding/components/onboarding_body/cards/siem_migrations/start_migration/start_migration_card.styles';
+import { CardSubduedText } from '../../../../onboarding/components/onboarding_body/cards/common/card_subdued_text';
+
+export interface MigrationProgressPanelProps {
+  migrationStats: RuleMigrationStats;
+}
+export const MigrationProgressPanel = React.memo<MigrationProgressPanelProps>(
+  ({ migrationStats }) => {
+    const finished = migrationStats.rules.completed + migrationStats.rules.failed;
+    const progressValue = (finished / migrationStats.rules.total) * 100;
+
+    const preparing = migrationStats.rules.pending === migrationStats.rules.total;
+
+    return (
+      <EuiPanel hasShadow={false} hasBorder paddingSize="m">
+        <EuiFlexGroup direction="column" gutterSize="m">
+          <EuiFlexItem grow={false}>
+            <EuiText size="s" className={TITLE_CLASS_NAME}>
+              <p>{i18n.START_MIGRATION_CARD_MIGRATION_TITLE(migrationStats.number)}</p>
+            </EuiText>
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiText size="s">
+              {i18n.START_MIGRATION_CARD_PROGRESS_DESCRIPTION(migrationStats.rules.total)}
+            </EuiText>
+          </EuiFlexItem>
+          {preparing ? (
+            <EuiFlexGroup
+              direction="row"
+              justifyContent="flexStart"
+              alignItems="center"
+              gutterSize="s"
+            >
+              <EuiFlexItem grow={false}>
+                <EuiIcon size="m" type={AssistantIcon} />
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <CardSubduedText size="s">
+                  {i18n.START_MIGRATION_CARD_PREPARING_DESCRIPTION}
+                </CardSubduedText>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiLoadingSpinner size="s" />
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          ) : (
+            <EuiFlexItem grow={false}>
+              <EuiProgress value={progressValue} max={100} color="success" />
+            </EuiFlexItem>
+          )}
+        </EuiFlexGroup>
+      </EuiPanel>
+    );
+  }
+);
+MigrationProgressPanel.displayName = 'MigrationProgressPanel';
