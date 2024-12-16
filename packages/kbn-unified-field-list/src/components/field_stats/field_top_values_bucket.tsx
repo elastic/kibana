elastic/kibana

commit 42594ebe8bfd9f02f24d257ff74de529294effb6

[Discover][Borealis] Update colors in field stats for Borealis theme (#203579)
- Closes https://github.com/elastic/kibana/issues/202044

## Summary

This PR updates the colors for field stats for Borealis theme. For
Amsterdam theme there should be no visual changes.

<img width="513" alt="Screenshot 2024-12-10 at 13 50 37"
src="https://github.com/user-attachments/assets/b5bc37da-eb95-45f6-b196-37511f6c936a">
<img width="514" alt="Screenshot 2024-12-10 at 13 51 07"
src="https://github.com/user-attachments/assets/cdf3c206-c88c-4f82-9905-3c9187d7461c">


### Testing

* Add the following to your `kibana.dev.yml`
```
uiSettings.experimental.themeSwitcherEnabled: true
uiSettings.overrides.theme:name: borealis
```
* Start Kibana with `KBN_OPTIMIZER_THEMES="experimental" yarn start`

packages/kbn-unified-field-list/src/components/field_stats/field_top_values_bucket.tsx
@@ -15,7 +15,10 @@ import {
   EuiProgress,
   EuiText,
   EuiTextBlockTruncate,
+  EuiThemeComputed,
   EuiToolTip,
+  makeHighContrastColor,
+  useEuiTheme,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { css } from '@emotion/react';
@@ -53,6 +56,7 @@ const FieldTopValuesBucket: React.FC<FieldTopValuesBucketProps> = ({
   overrideFieldTopValueBar,
   ...fieldTopValuesBucketOverridableProps
 }) => {
+  const { euiTheme } = useEuiTheme();
   const overrides = overrideFieldTopValueBar
     ? overrideFieldTopValueBar(fieldTopValuesBucketOverridableProps)
     : ({} as FieldTopValuesBucketParams);
@@ -141,7 +145,7 @@ const FieldTopValuesBucket: React.FC<FieldTopValuesBucketProps> = ({
               })}
               delay="long"
             >
-              <EuiText size="xs" textAlign="left" color={color}>
+              <EuiText size="xs" textAlign="left" color={getPercentageColor(euiTheme, color)}>
                 {formattedPercentage}
               </EuiText>
             </EuiToolTip>
@@ -206,6 +210,11 @@ const FieldTopValuesBucket: React.FC<FieldTopValuesBucketProps> = ({
     </EuiFlexGroup>
   );
 };
+
+const getPercentageColor = (euiTheme: EuiThemeComputed, color: string) =>
+  euiTheme.themeName?.toLowerCase().includes('borealis')
+    ? makeHighContrastColor(color)(euiTheme)
+    : color; // FIXME: remove in 9.x when Borealis becomes the default theme
 
 // Necessary for React.lazy
 // eslint-disable-next-line import/no-default-export

packages/kbn-unified-field-list/src/components/field_stats/field_stats.tsx
@@ -19,7 +19,14 @@ import type { DataPublicPluginStart } from '@kbn/data-plugin/public';
 import type { FieldFormatsStart } from '@kbn/field-formats-plugin/public';
 import type { ChartsPluginSetup } from '@kbn/charts-plugin/public';
 import DateMath from '@kbn/datemath';
-import { EuiButtonGroup, EuiLoadingSpinner, EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
+import {
+  EuiButtonGroup,
+  EuiLoadingSpinner,
+  EuiSpacer,
+  EuiText,
+  EuiTitle,
+  useEuiTheme,
+} from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import {
   Axis,
@@ -120,7 +127,7 @@ const FieldStatsComponent: React.FC<FieldStatsProps> = ({
   toDate,
   dataViewOrDataViewId,
   field,
-  color = getDefaultColor(),
+  color: originalColor,
   'data-test-subj': dataTestSubject = 'fieldStats',
   overrideMissingContent,
   overrideFooter,
@@ -128,6 +135,9 @@ const FieldStatsComponent: React.FC<FieldStatsProps> = ({
   overrideFieldTopValueBar,
   onStateChange,
 }) => {
+  const { euiTheme } = useEuiTheme();
+  const color = originalColor ?? getDefaultColor(euiTheme.themeName);
+
   const { fieldFormats, uiSettings, charts, dataViews, data } = services;
   const [state, changeState] = useState<FieldStatsState>({
     isLoading: false,

packages/kbn-unified-field-list/src/components/field_stats/field_top_values.test.tsx
@@ -59,6 +59,7 @@ describe('UnifiedFieldList <FieldTopValues />', () => {
           key: 'sourceB',
         },
       ],
+      color: '#000',
       'data-test-subj': 'testing',
     };
   });

packages/kbn-unified-field-list/src/components/field_stats/field_top_values.tsx
@@ -20,7 +20,7 @@ export interface FieldTopValuesProps {
   dataView: DataView;
   field: DataViewField;
   sampledValuesCount: number;
-  color?: string;
+  color: string;
   'data-test-subj': string;
   onAddFilter?: AddFieldFilterHandler;
   overrideFieldTopValueBar?: OverrideFieldTopValueBarCallback;
@@ -32,7 +32,7 @@ export const FieldTopValues: React.FC<FieldTopValuesProps> = ({
   dataView,
   field,
   sampledValuesCount,
-  color = getDefaultColor(),
+  color,
   'data-test-subj': dataTestSubject,
   onAddFilter,
   overrideFieldTopValueBar,
@@ -106,7 +106,10 @@ export const FieldTopValues: React.FC<FieldTopValuesProps> = ({
   );
 };
 
-export const getDefaultColor = () => euiPaletteColorBlind()[1];
+export const getDefaultColor = (euiThemeName: string) =>
+  euiThemeName?.toLowerCase().includes('borealis')
+    ? euiPaletteColorBlind()[2]
+    : euiPaletteColorBlind()[1]; // FIXME: remove in 9.x when Borealis becomes the default theme
 
 export const getFormattedPercentageValue = (
   currentValue: number,

commit be1f60ae19f921b5480ad94a5620bfad4375b040

[ML] AIOps: Field stats fix  (#204403)
## Summary

Fixes a regression introduced in
https://github.com/elastic/kibana/pull/203579.

`EuiText` accepts EUI colors like `primary` as well as hex colors. The
PR above introduced `getPercentageColor()` which assumed all colors
passed in are hex colors. In case it was an EUI color token the code
threw an error and the fields stats popover wouldn't fully render. This
fix adds an additional check to apply `makeHighContrastColor` only if
the color provided is a hex color.

Before:

![CleanShot 2024-12-16 at 14 50
15@2x](https://github.com/user-attachments/assets/53a5df17-da4a-4188-a80c-5dc52cae9f39)

After:

![CleanShot 2024-12-16 at 14 49
20@2x](https://github.com/user-attachments/assets/75fc093a-6cf6-4a21-bc10-05dd7d253c70)

### Checklist

- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

packages/kbn-unified-field-list/src/components/field_stats/field_top_values_bucket.tsx
@@ -211,8 +211,14 @@ const FieldTopValuesBucket: React.FC<FieldTopValuesBucketProps> = ({
   );
 };
 
+// copied from x-pack/plugins/banners/server/utils.ts
+const hexColorRegexp = /^#([0-9a-f]{6}|[0-9a-f]{3})$/i;
+const isHexColor = (color: string) => {
+  return hexColorRegexp.test(color);
+};
+
 const getPercentageColor = (euiTheme: EuiThemeComputed, color: string) =>
-  euiTheme.themeName?.toLowerCase().includes('borealis')
+  euiTheme.themeName?.toLowerCase().includes('borealis') && isHexColor(color)
     ? makeHighContrastColor(color)(euiTheme)
     : color; // FIXME: remove in 9.x when Borealis becomes the default theme
 