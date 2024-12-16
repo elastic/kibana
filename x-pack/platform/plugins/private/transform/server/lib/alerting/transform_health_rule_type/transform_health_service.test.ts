78172cc94f71e170a8d237592bc4b8cd8a0df537
Dima Arnautov <dmitrii.arnautov@elastic.co>
2024-12-16 17:58:58 +0100

    update populateTransformsWithAssignedRules, add tests

--- x-pack/platform/plugins/private/transform/server/lib/alerting/transform_health_rule_type/transform_health_service.test.ts
+++ x-pack/platform/plugins/private/transform/server/lib/alerting/transform_health_rule_type/transform_health_service.test.ts
@@ -5,16 +5,18 @@
  * 2.0.
  */
 
-import { transformHealthServiceProvider } from './transform_health_service';
-import type { ElasticsearchClient } from '@kbn/core/server';
-import type { RulesClient } from '@kbn/alerting-plugin/server';
-import type { FieldFormatsRegistry } from '@kbn/field-formats-plugin/common';
-import { elasticsearchServiceMock } from '@kbn/core-elasticsearch-server-mocks';
-import { rulesClientMock } from '@kbn/alerting-plugin/server/rules_client.mock';
 import type {
   TransformGetTransformResponse,
   TransformGetTransformStatsResponse,
+  TransformGetTransformTransformSummary,
 } from '@elastic/elasticsearch/lib/api/types';
+import type { FindResult, RulesClient } from '@kbn/alerting-plugin/server';
+import { rulesClientMock } from '@kbn/alerting-plugin/server/rules_client.mock';
+import { elasticsearchServiceMock } from '@kbn/core-elasticsearch-server-mocks';
+import type { ElasticsearchClient } from '@kbn/core/server';
+import type { FieldFormatsRegistry } from '@kbn/field-formats-plugin/common';
+import { transformHealthServiceProvider } from './transform_health_service';
+import type { TransformHealthRuleParams } from './schema';
 
 describe('transformHealthServiceProvider', () => {
   let esClient: jest.Mocked<ElasticsearchClient>;
@@ -50,11 +52,15 @@ describe('transformHealthServiceProvider', () => {
               // Mock continuous transforms
               ...new Array(102).fill(null).map((_, i) => ({
                 id: `transform${i}`,
-                sync: true,
+                sync: {
+                  time: {
+                    field: 'order_date',
+                    delay: '60s',
+                  },
+                },
               })),
               {
                 id: 'transform102',
-                sync: false,
               },
             ],
           } as unknown as TransformGetTransformResponse;
@@ -157,5 +163,132 @@ describe('transformHealthServiceProvider', () => {
     expect(notStarted.context.message).toEqual(
       'Transform transform_with_a_very_long_id_that_result_in_long_url_for_sure_0, transform_with_a_very_long_id_that_result_in_long_url_for_sure_1, transform_with_a_very_long_id_that_result_in_long_url_for_sure_2, transform_with_a_very_long_id_that_result_in_long_url_for_sure_3, transform_with_a_very_long_id_that_result_in_long_url_for_sure_4, transform_with_a_very_long_id_that_result_in_long_url_for_sure_5, transform_with_a_very_long_id_that_result_in_long_url_for_sure_6, transform_with_a_very_long_id_that_result_in_long_url_for_sure_7, transform_with_a_very_long_id_that_result_in_long_url_for_sure_8, transform_with_a_very_long_id_that_result_in_long_url_for_sure_9, transform_with_a_very_long_id_that_result_in_long_url_for_sure_10, transform_with_a_very_long_id_that_result_in_long_url_for_sure_11, transform_with_a_very_long_id_that_result_in_long_url_for_sure_12, transform_with_a_very_long_id_that_result_in_long_url_for_sure_13, transform_with_a_very_long_id_that_result_in_long_url_for_sure_14, transform_with_a_very_long_id_that_result_in_long_url_for_sure_15, transform_with_a_very_long_id_that_result_in_long_url_for_sure_16, transform_with_a_very_long_id_that_result_in_long_url_for_sure_17, transform_with_a_very_long_id_that_result_in_long_url_for_sure_18, transform_with_a_very_long_id_that_result_in_long_url_for_sure_19, transform_with_a_very_long_id_that_result_in_long_url_for_sure_20, transform_with_a_very_long_id_that_result_in_long_url_for_sure_21, transform_with_a_very_long_id_that_result_in_long_url_for_sure_22, transform_with_a_very_long_id_that_result_in_long_url_for_sure_23, transform_with_a_very_long_id_that_result_in_long_url_for_sure_24, transform_with_a_very_long_id_that_result_in_long_url_for_sure_25, transform_with_a_very_long_id_that_result_in_long_url_for_sure_26, transform_with_a_very_long_id_that_result_in_long_url_for_sure_27, transform_with_a_very_long_id_that_result_in_long_url_for_sure_28, transform_with_a_very_long_id_that_result_in_long_url_for_sure_29, transform_with_a_very_long_id_that_result_in_long_url_for_sure_30, transform_with_a_very_long_id_that_result_in_long_url_for_sure_31, transform_with_a_very_long_id_that_result_in_long_url_for_sure_32, transform_with_a_very_long_id_that_result_in_long_url_for_sure_33, transform_with_a_very_long_id_that_result_in_long_url_for_sure_34, transform_with_a_very_long_id_that_result_in_long_url_for_sure_35, transform_with_a_very_long_id_that_result_in_long_url_for_sure_36, transform_with_a_very_long_id_that_result_in_long_url_for_sure_37, transform_with_a_very_long_id_that_result_in_long_url_for_sure_38, transform_with_a_very_long_id_that_result_in_long_url_for_sure_39, transform_with_a_very_long_id_that_result_in_long_url_for_sure_40, transform_with_a_very_long_id_that_result_in_long_url_for_sure_41, transform_with_a_very_long_id_that_result_in_long_url_for_sure_42, transform_with_a_very_long_id_that_result_in_long_url_for_sure_43, transform_with_a_very_long_id_that_result_in_long_url_for_sure_44, transform_with_a_very_long_id_that_result_in_long_url_for_sure_45, transform_with_a_very_long_id_that_result_in_long_url_for_sure_46, transform_with_a_very_long_id_that_result_in_long_url_for_sure_47, transform_with_a_very_long_id_that_result_in_long_url_for_sure_48, transform_with_a_very_long_id_that_result_in_long_url_for_sure_49, transform_with_a_very_long_id_that_result_in_long_url_for_sure_50, transform_with_a_very_long_id_that_result_in_long_url_for_sure_51, transform_with_a_very_long_id_that_result_in_long_url_for_sure_52, transform_with_a_very_long_id_that_result_in_long_url_for_sure_53, transform_with_a_very_long_id_that_result_in_long_url_for_sure_54, transform_with_a_very_long_id_that_result_in_long_url_for_sure_55, transform_with_a_very_long_id_that_result_in_long_url_for_sure_56, transform_with_a_very_long_id_that_result_in_long_url_for_sure_57, transform_with_a_very_long_id_that_result_in_long_url_for_sure_58, transform_with_a_very_long_id_that_result_in_long_url_for_sure_59 are not started.'
     );
+  });
+
+  describe('populateTransformsWithAssignedRules', () => {
+    it('should throw an error if rulesClient is missing', async () => {
+      const service = transformHealthServiceProvider({ esClient, fieldFormatsRegistry });
+
+      await expect(service.populateTransformsWithAssignedRules([])).rejects.toThrow(
+        'Rules client is missing'
+      );
+    });
+
+    it('should return an empty list if no transforms are provided', async () => {
+      const service = transformHealthServiceProvider({
+        esClient,
+        rulesClient,
+        fieldFormatsRegistry,
+      });
+
+      const result = await service.populateTransformsWithAssignedRules([]);
+      expect(result).toEqual([]);
+    });
+
+    it('should return transforms with associated alerting rules', async () => {
+      const transforms = [
+        { id: 'transform1', sync: {} },
+        { id: 'transform2', sync: {} },
+        { id: 'transform3', sync: {} },
+      ] as TransformGetTransformTransformSummary[];
+
+      const rules = [
+        {
+          id: 'rule1',
+          params: {
+            includeTransforms: ['transform1', 'transform2'],
+            excludeTransforms: [],
+          },
+        },
+        {
+          id: 'rule2',
+          params: {
+            includeTransforms: ['transform3'],
+            excludeTransforms: null,
+          },
+        },
+      ];
+
+      rulesClient.find.mockResolvedValue({ data: rules } as FindResult<TransformHealthRuleParams>);
+
+      const service = transformHealthServiceProvider({
+        esClient,
+        rulesClient,
+        fieldFormatsRegistry,
+      });
+
+      const result = await service.populateTransformsWithAssignedRules(transforms);
+
+      expect(result).toEqual([
+        {
+          id: 'transform1',
+          sync: {},
+          alerting_rules: [rules[0]],
+        },
+        {
+          id: 'transform2',
+          sync: {},
+          alerting_rules: [rules[0]],
+        },
+        {
+          id: 'transform3',
+          sync: {},
+          alerting_rules: [rules[1]],
+        },
+      ]);
+    });
+
+    it('should exclude transforms based on excludeTransforms parameter', async () => {
+      const transforms = [
+        { id: 'transform1', sync: {} },
+        { id: 'transform2', sync: {} },
+        { id: 'transform3', sync: {} },
+      ] as TransformGetTransformTransformSummary[];
+
+      const rules = [
+        {
+          id: 'rule1',
+          params: {
+            includeTransforms: ['transform*'],
+            excludeTransforms: ['transform2'],
+          },
+        },
+        {
+          id: 'rule2',
+          params: {
+            includeTransforms: ['*'],
+            excludeTransforms: [],
+          },
+        },
+      ];
+
+      rulesClient.find.mockResolvedValue({ data: rules } as FindResult<TransformHealthRuleParams>);
+
+      const service = transformHealthServiceProvider({
+        esClient,
+        rulesClient,
+        fieldFormatsRegistry,
+      });
+
+      const result = await service.populateTransformsWithAssignedRules(transforms);
+
+      expect(result).toEqual([
+        {
+          id: 'transform1',
+          sync: {},
+          alerting_rules: [rules[0], rules[1]],
+        },
+        {
+          id: 'transform2',
+          sync: {},
+          alerting_rules: [rules[1]],
+        },
+        {
+          id: 'transform3',
+          sync: {},
+          alerting_rules: [rules[0], rules[1]],
+        },
+      ]);
+    });
   });
 });
