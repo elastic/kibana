9d16fe726c998cfd2ffe41aa558f436812e4a965
klacabane <kevin.lacabane@elastic.co>
2024-12-17 13:00:12 +0100

    Revert "remove enablement routes"
    This reverts commit b850ef6e7f322dcf67bce0068f4b6676e8f8a22b.

--- x-pack/platform/plugins/shared/entity_manager/server/routes/index.ts
+++ x-pack/platform/plugins/shared/entity_manager/server/routes/index.ts
@@ -5,10 +5,12 @@
  * 2.0.
  */
 
+import { enablementRoutes } from './enablement';
 import { entitiesRoutes } from './entities';
 import { v2Routes } from './v2';
 
 export const entityManagerRouteRepository = {
+  ...enablementRoutes,
   ...entitiesRoutes,
   ...v2Routes,
 };
