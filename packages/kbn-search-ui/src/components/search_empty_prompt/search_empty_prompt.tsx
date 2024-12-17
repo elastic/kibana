c5560165f31a9ca08c15515423c165c68ad2b5a4
José Luis González <joseluisgj@gmail.com>
2024-12-17 11:22:01 +0100

    Consuming search_empty_prompt package from serverless

--- packages/kbn-search-ui/src/components/search_empty_prompt/search_empty_prompt.tsx
+++ packages/kbn-search-ui/src/components/search_empty_prompt/search_empty_prompt.tsx
@@ -8,219 +8,49 @@
  */
 
 import React from 'react';
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiPanel,
-  EuiIcon,
-  EuiTitle,
-  EuiText,
-  EuiLink,
-  EuiButton,
-  EuiBadge,
-} from '@elastic/eui';
-import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n-react';
+import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiIcon, EuiTitle, EuiText } from '@elastic/eui';
 
-export const SearchEmptyPrompt: React.FC = () => {
+interface SearchEmptyPromptProps {
+  actions?: React.ReactNode;
+  body?: React.ReactNode;
+  description?: string;
+  icon?: string;
+  title: string;
+}
+
+export const SearchEmptyPrompt: React.FC<SearchEmptyPromptProps> = ({
+  actions,
+  body,
+  description,
+  icon,
+  title,
+}) => {
   return (
     <EuiPanel paddingSize="l" hasShadow={false} hasBorder>
       <EuiFlexGroup alignItems="center" justifyContent="center" direction="column" gutterSize="l">
-        <EuiFlexItem>
-          <EuiIcon size="xxl" type="trash" />
-        </EuiFlexItem>
+        {icon && (
+          <EuiFlexItem>
+            <EuiIcon size="xxl" type={icon} />
+          </EuiFlexItem>
+        )}
         <EuiFlexItem>
           <EuiTitle>
-            <h2>
-              {i18n.translate('xpack.serverlessSearch.webCrawlersEmpty.title', {
-                defaultMessage: 'Set up a web crawler',
-              })}
-            </h2>
+            <h2>{title}</h2>
           </EuiTitle>
         </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiText textAlign="center" color="subdued">
-            <p>
-              {i18n.translate('xpack.serverlessSearch.webCrawlersEmpty.description', {
-                defaultMessage:
-                  "To set up and deploy a web crawler you'll be working between data source, your terminal, and the Kibana UI. The high level process looks like this:",
-              })}
-            </p>
-          </EuiText>
-        </EuiFlexItem>
-        <EuiFlexGroup
-          alignItems="stretch"
-          justifyContent="center"
-          direction="column"
-          gutterSize="s"
-        >
+        {description && (
           <EuiFlexItem>
-            <EuiPanel color="subdued">
-              <EuiFlexItem grow={false}>
-                {/* <DecorativeHorizontalStepper stepCount={3} /> */}
-              </EuiFlexItem>
-              <EuiFlexGroup>
-                <EuiFlexItem>
-                  <EuiFlexGroup justifyContent="flexStart" alignItems="center" direction="column">
-                    <EuiFlexGroup
-                      gutterSize="s"
-                      direction="row"
-                      alignItems="center"
-                      justifyContent="center"
-                    >
-                      <EuiFlexItem grow={false}>
-                        <EuiIcon color="primary" size="l" type="trash" />
-                      </EuiFlexItem>
-                      <EuiFlexItem>
-                        <EuiIcon size="m" type="sortRight" />
-                      </EuiFlexItem>
-                      <EuiFlexItem>
-                        <EuiIcon color="primary" size="l" type="launch" />
-                      </EuiFlexItem>
-                    </EuiFlexGroup>
-                    <EuiFlexItem>
-                      <EuiText>
-                        <p>
-                          <FormattedMessage
-                            id="xpack.serverlessSearch.webCrawlersEmpty.guideTwoDescription"
-                            defaultMessage="Deploy web crawler code on your own infrastructure by running from {source}, or using {docker}"
-                            values={{
-                              source: (
-                                <EuiLink
-                                  target="_blank"
-                                  data-test-subj="serverlessSearchEmptyConnectorsPromptSourceLink"
-                                  href={'https://github.com/elastic/crawler'}
-                                >
-                                  {i18n.translate(
-                                    'xpack.serverlessSearch.webCrawlersEmpty.sourceLabel',
-                                    { defaultMessage: 'source' }
-                                  )}
-                                </EuiLink>
-                              ),
-                              docker: (
-                                <EuiLink
-                                  target="_blank"
-                                  data-test-subj="serverlessSearchEmptyConnectorsPromptDockerLink"
-                                  href={
-                                    'https://github.com/elastic/crawler?tab=readme-ov-file#running-open-crawler-with-docker'
-                                  }
-                                >
-                                  {i18n.translate(
-                                    'xpack.serverlessSearch.webCrawlersEmpty.dockerLabel',
-                                    { defaultMessage: 'Docker' }
-                                  )}
-                                </EuiLink>
-                              ),
-                            }}
-                          />
-                        </p>
-                      </EuiText>
-                    </EuiFlexItem>
-                  </EuiFlexGroup>
-                </EuiFlexItem>
-                <EuiFlexItem>
-                  <EuiFlexGroup justifyContent="flexStart" alignItems="center" direction="column">
-                    <EuiFlexItem grow={false}>
-                      <EuiFlexGroup
-                        justifyContent="center"
-                        alignItems="center"
-                        direction="row"
-                        gutterSize="s"
-                      >
-                        <EuiFlexItem grow={false}>
-                          <EuiIcon color="primary" size="l" type="globe" />
-                        </EuiFlexItem>
-                      </EuiFlexGroup>
-                    </EuiFlexItem>
-                    <EuiFlexItem grow={false}>
-                      <EuiText>
-                        <p>
-                          {i18n.translate(
-                            'xpack.serverlessSearch.webCrawlersEmpty.guideOneDescription',
-                            {
-                              defaultMessage: 'Set one or more domain URLs you want to crawl',
-                            }
-                          )}
-                        </p>
-                      </EuiText>
-                    </EuiFlexItem>
-                  </EuiFlexGroup>
-                </EuiFlexItem>
-
-                <EuiFlexItem>
-                  <EuiFlexGroup justifyContent="flexStart" alignItems="center" direction="column">
-                    <EuiFlexItem grow={false}>
-                      <EuiFlexGroup
-                        gutterSize="s"
-                        direction="row"
-                        alignItems="center"
-                        justifyContent="center"
-                      >
-                        <EuiFlexItem>
-                          <EuiIcon color="primary" size="l" type="globe" />
-                        </EuiFlexItem>
-                        <EuiFlexItem>
-                          <EuiIcon size="m" type="sortRight" />
-                        </EuiFlexItem>
-                        <EuiFlexItem>
-                          <EuiIcon color="primary" size="l" type="trash" />
-                        </EuiFlexItem>
-                        <EuiFlexItem>
-                          <EuiIcon size="m" type="sortRight" />
-                        </EuiFlexItem>
-                        <EuiFlexItem>
-                          <EuiIcon color="primary" size="l" type="logoElasticsearch" />
-                        </EuiFlexItem>
-                      </EuiFlexGroup>
-                    </EuiFlexItem>
-                    <EuiFlexItem>
-                      <EuiText>
-                        <p>
-                          {i18n.translate(
-                            'xpack.serverlessSearch.webCrawlersEmpty.guideThreeDescription',
-                            {
-                              defaultMessage:
-                                'Configure your web crawler and connect it to Elasticsearch',
-                            }
-                          )}
-                        </p>
-                      </EuiText>
-                    </EuiFlexItem>
-                  </EuiFlexGroup>
-                </EuiFlexItem>
-              </EuiFlexGroup>
-            </EuiPanel>
+            <EuiText textAlign="center" color="subdued">
+              <p>{description}</p>
+            </EuiText>
           </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiFlexGroup direction="row" gutterSize="m">
-          <EuiFlexItem>
-            <EuiButton
-              data-test-subj="serverlessSearchEmptyConnectorsPromptCreateSelfManagedConnectorButton"
-              fill
-              iconType="plusInCircle"
-              href={'https://github.com/elastic/crawler'}
-              target="_blank"
-            >
-              {i18n.translate('xpack.serverlessSearch.webCrawlersEmpty.selfManagedButton', {
-                defaultMessage: 'Self-managed web crawler',
-              })}
-            </EuiButton>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiFlexGroup direction="column" gutterSize="s" alignItems="center">
-              <EuiFlexItem>
-                <EuiButton data-test-subj="serverlessSearchEmptyConnectorsPromptCreateElasticManagedConnectorButton">
-                  {i18n.translate('xpack.serverlessSearch.webCrawlersEmpty.elasticManagedButton', {
-                    defaultMessage: 'Elastic managed web crawler',
-                  })}
-                </EuiButton>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiBadge color="accent">Coming soon</EuiBadge>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiFlexItem>
-        </EuiFlexGroup>
+        )}
+        {body && <>{body}</>}
+        {actions && (
+          <EuiFlexGroup direction="row" gutterSize="m">
+            {actions}
+          </EuiFlexGroup>
+        )}
       </EuiFlexGroup>
     </EuiPanel>
   );
