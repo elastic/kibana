7cd2ace06ed4fa96ae35067c84bb2b6e46ef4415
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2024-12-19 01:04:57 +1100

    [8.x] [Cloud Security] Move @kbn/cloud-security-posture-storybook-config for Kibana sustainability  (#204500) (#204736)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[Cloud Security] Move @kbn/cloud-security-posture-storybook-config
    for Kibana sustainability
    (#204500)](https://github.com/elastic/kibana/pull/204500)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT
    [{"author":{"name":"seanrathier","email":"sean.rathier@gmail.com"},"sourceCommit":{"committedDate":"2024-12-18T12:09:38Z","message":"[Cloud
    Security] Move @kbn/cloud-security-posture-storybook-config for Kibana
    sustainability (#204500)\n\n## Summary\r\n\r\nMove
    @kbn/cloud-security-posture-storybook-config package
    to\r\n`x-pack/solutions/security/packages/kbn-cloud-security-posture/.storybook`\r\n\r\nRenamed
    removed the `config` folder to align with the
    `.storybook`\r\n[naming\r\nconvention](https://github.com/elastic/kibana/pull/204500/files#diff-91918a63f6365a8f40f674ab54a8df7ce9aeb313f77fe8eda76284d712ef5425R21).\r\n\r\n![Screenshot
    2024-12-16 at 5
    50\r\n53 PM](https://github.com/user-attachments/assets/de1d0a81-353f-434c-bb2d-989210b35b43)\r\n\r\n###
    Related Issues\r\n-
    https://github.com/elastic/kibana/pull/202862\r\n\r\n---------\r\n\r\nCo-authored-by:
    Brad White
    <Ikuni17@users.noreply.github.com>","sha":"d6cb398d1ab09af93369e84f0aa13f0688357082","branchLabelMapping":{"^v9.0.0$":"main","^v8.18.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","v9.0.0","Team:Cloud
    Security","backport:prev-minor","ci:build-storybooks"],"title":"[Cloud
    Security] Move @kbn/cloud-security-posture-storybook-config for Kibana
    sustainability
    ","number":204500,"url":"https://github.com/elastic/kibana/pull/204500","mergeCommit":{"message":"[Cloud
    Security] Move @kbn/cloud-security-posture-storybook-config for Kibana
    sustainability (#204500)\n\n## Summary\r\n\r\nMove
    @kbn/cloud-security-posture-storybook-config package
    to\r\n`x-pack/solutions/security/packages/kbn-cloud-security-posture/.storybook`\r\n\r\nRenamed
    removed the `config` folder to align with the
    `.storybook`\r\n[naming\r\nconvention](https://github.com/elastic/kibana/pull/204500/files#diff-91918a63f6365a8f40f674ab54a8df7ce9aeb313f77fe8eda76284d712ef5425R21).\r\n\r\n![Screenshot
    2024-12-16 at 5
    50\r\n53 PM](https://github.com/user-attachments/assets/de1d0a81-353f-434c-bb2d-989210b35b43)\r\n\r\n###
    Related Issues\r\n-
    https://github.com/elastic/kibana/pull/202862\r\n\r\n---------\r\n\r\nCo-authored-by:
    Brad White
    <Ikuni17@users.noreply.github.com>","sha":"d6cb398d1ab09af93369e84f0aa13f0688357082"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","branchLabelMappingKey":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/204500","number":204500,"mergeCommit":{"message":"[Cloud
    Security] Move @kbn/cloud-security-posture-storybook-config for Kibana
    sustainability (#204500)\n\n## Summary\r\n\r\nMove
    @kbn/cloud-security-posture-storybook-config package
    to\r\n`x-pack/solutions/security/packages/kbn-cloud-security-posture/.storybook`\r\n\r\nRenamed
    removed the `config` folder to align with the
    `.storybook`\r\n[naming\r\nconvention](https://github.com/elastic/kibana/pull/204500/files#diff-91918a63f6365a8f40f674ab54a8df7ce9aeb313f77fe8eda76284d712ef5425R21).\r\n\r\n![Screenshot
    2024-12-16 at 5
    50\r\n53 PM](https://github.com/user-attachments/assets/de1d0a81-353f-434c-bb2d-989210b35b43)\r\n\r\n###
    Related Issues\r\n-
    https://github.com/elastic/kibana/pull/202862\r\n\r\n---------\r\n\r\nCo-authored-by:
    Brad White
    <Ikuni17@users.noreply.github.com>","sha":"d6cb398d1ab09af93369e84f0aa13f0688357082"}}]}]
    BACKPORT-->
    
    Co-authored-by: seanrathier <sean.rathier@gmail.com>

--- x-pack/solutions/security/packages/kbn-cloud-security-posture/.storybook/main.ts
+++ x-pack/solutions/security/packages/kbn-cloud-security-posture/.storybook/main.ts
@@ -9,7 +9,7 @@ import { defaultConfig } from '@kbn/storybook';
 
 module.exports = {
   ...defaultConfig,
-  stories: ['../../**/*.stories.+(tsx|mdx)'],
+  stories: ['../**/*.stories.+(tsx|mdx)'],
   reactOptions: {
     strictMode: true,
   },
