75a35a7ce9ad9f0467b7843b2b98422a6b763c61
Paul Tavares <paul.tavares@elastic.co>
2024-12-18 15:58:06 -0500

    Initial script to run MS host - creates an event

---
+++ x-pack/solutions/security/plugins/security_solution/scripts/endpoint/microsoft_defender_host/services/onboard_microsoft_vm.ts
@@ -0,0 +1,51 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { ToolingLog } from '@kbn/tooling-log';
+import type { KbnClient } from '@kbn/test';
+import { MicrosoftDefenderDataGenerator } from '../../../../common/endpoint/data_generators/microsoft_defender_data_generator';
+import { MICROSOFT_DEFENDER_INTEGRATION_PACKAGE_NAME } from './constants';
+import { installIntegration } from '../../common/fleet_services';
+import { createToolingLogger } from '../../../../common/endpoint/data_loaders/utils';
+
+export interface OnboardVmHostWithMicrosoftDefenderOptions {
+  kbnClient: KbnClient;
+  log?: ToolingLog;
+}
+
+export const onboardVmHostWithMicrosoftDefender = async ({
+  kbnClient,
+  log = createToolingLogger(),
+}: OnboardVmHostWithMicrosoftDefenderOptions) => {
+  log?.warning(`
+  Creation of a VM host running Microsoft Defender for Endpoint is not yet implemented. However, an event
+  will be indexed that will enable testing within Kibana only.
+  `);
+
+  await installIntegration(kbnClient, MICROSOFT_DEFENDER_INTEGRATION_PACKAGE_NAME);
+
+  const indexName = 'logs-microsoft_defender_endpoint.log-default';
+  const docToCreate = new MicrosoftDefenderDataGenerator().generateEndpointLog();
+
+  log.verbose(`Creating event in [l${indexName}]:\n ${JSON.stringify(docToCreate, null, 2)}`);
+
+  const indexedDoc = await kbnClient
+    .request({
+      method: 'POST',
+      path: 'api/console/proxy',
+      headers: { 'elastic-api-version': '2023-10-31' },
+      query: {
+        path: `${indexName}/_doc`,
+        method: 'POST',
+      },
+      body: docToCreate,
+    })
+    .then((response) => response.data);
+
+  log.verbose(`event create response:\n ${JSON.stringify(indexedDoc, null, 2)}`);
+  log.info(`A host log event with id [${indexedDoc._id}] has been indexed in [${indexName}]`);
+};
