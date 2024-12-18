aa4ce0a69d4e78f67b5070e421eb0499351d440a
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 20:32:58 +0100

    Relocating module `@kbn/core-status-server-internal`

--- src/core/packages/status/server-internal/jest.config.js
+++ src/core/packages/status/server-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/status/core-status-server-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/status/server-internal'],
 };
