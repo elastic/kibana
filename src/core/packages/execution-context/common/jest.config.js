b512030c3b138bd9ec1435f4546f0cfeb871bc97
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 18:36:12 +0100

    Relocating module `@kbn/core-execution-context-common`

--- src/core/packages/execution-context/common/jest.config.js
+++ src/core/packages/execution-context/common/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/execution-context/core-execution-context-common'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/execution-context/common'],
 };
