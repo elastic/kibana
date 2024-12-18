244e898995ea11bc064bb40b34a4a1c6150f80cc
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-12-18 21:05:54 +0100

    Relocating module `@kbn/alerting-comparators`

--- x-pack/platform/packages/shared/kbn-alerting-comparators/jest.config.js
+++ x-pack/platform/packages/shared/kbn-alerting-comparators/jest.config.js
@@ -7,6 +7,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/packages/kbn-alerting-comparators'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/shared/kbn-alerting-comparators'],
 };
