544213d3b9a364bec00a063ba0be45eb3e13269e
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-12-18 16:08:33 +0100

    Relocating module `@kbn/expression-shape-plugin`

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -41,7 +41,7 @@ export const storybookAliases = {
   expression_partition_vis: 'src/plugins/chart_expressions/expression_partition_vis/.storybook',
   expression_repeat_image: 'src/platform/plugins/shared/expression_repeat_image/.storybook',
   expression_reveal_image: 'src/platform/plugins/shared/expression_reveal_image/.storybook',
-  expression_shape: 'src/plugins/expression_shape/.storybook',
+  expression_shape: 'src/platform/plugins/shared/expression_shape/.storybook',
   expression_tagcloud: 'src/plugins/chart_expressions/expression_tagcloud/.storybook',
   fleet: 'x-pack/plugins/fleet/.storybook',
   grouping: 'packages/kbn-grouping/.storybook',
