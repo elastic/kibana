7ea2d6bfd5ab4702d90b2eb7ce00ca6b2fec86fd
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 18:33:28 +0100

    Relocating module `@kbn/core-execution-context-browser`

--- src/core/packages/execution-context/browser/jest.config.js
+++ src/core/packages/execution-context/browser/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/execution-context/core-execution-context-browser'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/execution-context/browser'],
 };
