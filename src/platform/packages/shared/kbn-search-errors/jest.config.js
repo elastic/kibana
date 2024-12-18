50c6a1c72a08a8341344570f7cab9fbda8c2751b
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 15:04:46 +0100

    Relocating module `@kbn/search-errors`

--- src/platform/packages/shared/kbn-search-errors/jest.config.js
+++ src/platform/packages/shared/kbn-search-errors/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-search-errors'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-search-errors'],
 };
