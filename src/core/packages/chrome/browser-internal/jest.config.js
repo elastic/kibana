0edcc15a0df4c676f3b4fdc8613075fcc355d839
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 18:02:37 +0100

    Relocating module `@kbn/core-chrome-browser-internal`

--- src/core/packages/chrome/browser-internal/jest.config.js
+++ src/core/packages/chrome/browser-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/chrome/core-chrome-browser-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/chrome/browser-internal'],
 };
