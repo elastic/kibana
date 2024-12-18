29f2853439542642b0817a709aca839959124e41
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 14:00:36 +0100

    Relocating module `@kbn/content-management-utils`

--- src/platform/packages/shared/kbn-content-management-utils/jest.config.js
+++ src/platform/packages/shared/kbn-content-management-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-content-management-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-content-management-utils'],
 };
