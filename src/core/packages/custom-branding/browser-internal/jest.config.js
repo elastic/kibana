bec564bba015722611d59139eeefb2f1174bcb14
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 18:06:46 +0100

    Relocating module `@kbn/core-custom-branding-browser-internal`

--- src/core/packages/custom-branding/browser-internal/jest.config.js
+++ src/core/packages/custom-branding/browser-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../',
-  roots: ['<rootDir>/packages/core/custom-branding/core-custom-branding-browser-internal'],
+  rootDir: '../../../../../',
+  roots: ['<rootDir>/src/core/packages/custom-branding/browser-internal'],
 };
