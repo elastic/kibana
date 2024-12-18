15a1a848fe91d6f76e0d8971a66478d16536a8d5
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 19:32:20 +0100

    Relocating module `@kbn/core-metrics-server-internal`

--- src/core/packages/metrics/server-internal/jest.config.js
+++ src/core/packages/metrics/server-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/metrics/core-metrics-server-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/metrics/server-internal'],
 };
