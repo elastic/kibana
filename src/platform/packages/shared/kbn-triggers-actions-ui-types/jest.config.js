abbe0649cc3d34e1048d54fa55f9fc4e3f5bba0d
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-12-18 21:58:49 +0100

    Relocating module `@kbn/triggers-actions-ui-types`

--- src/platform/packages/shared/kbn-triggers-actions-ui-types/jest.config.js
+++ src/platform/packages/shared/kbn-triggers-actions-ui-types/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-triggers-actions-ui-types'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-triggers-actions-ui-types'],
 };
