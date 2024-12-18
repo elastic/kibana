667c164882181340cde346012cde48596b6dbe74
Marco Liberati <dej611@users.noreply.github.com>
2024-12-18 12:15:56 +0100

    [SecuritySolutions][Lens][Embeddable] Use the correct Lens typeguard for the new embeddable (#204189)
    ## Summary
    
    Fixes #180726
    
    Use the new `isLensApi` helper in place of the previous system.
    
    Co-authored-by: Angela Chuang <yi-chun.chuang@elastic.co>
    (cherry picked from commit 9b0879cb302e9251a47c95393799e90683076c2a)
    
    # Conflicts:
    #	x-pack/solutions/security/plugins/security_solution/public/app/actions/copy_to_clipboard/lens/copy_to_clipboard.ts
    #	x-pack/solutions/security/plugins/security_solution/public/app/actions/filter/lens/create_action.ts
    #	x-pack/solutions/security/plugins/security_solution/public/app/actions/utils.ts

--- x-pack/solutions/security/plugins/security_solution/public/app/actions/copy_to_clipboard/lens/copy_to_clipboard.ts
+++ x-pack/solutions/security/plugins/security_solution/public/app/actions/copy_to_clipboard/lens/copy_to_clipboard.ts
@@ -10,7 +10,8 @@ import { isErrorEmbeddable } from '@kbn/embeddable-plugin/public';
 import { createAction } from '@kbn/ui-actions-plugin/public';
 import copy from 'copy-to-clipboard';
 import { KibanaServices } from '../../../../common/lib/kibana';
-import { fieldHasCellActions, isCountField, isInSecurityApp, isLensEmbeddable } from '../../utils';
+import { fieldHasCellActions, isCountField, isInSecurityApp } from '../../utils';
+import { isLensApi } from '@kbn/lens-plugin/public';
 import { COPY_TO_CLIPBOARD, COPY_TO_CLIPBOARD_ICON, COPY_TO_CLIPBOARD_SUCCESS } from '../constants';
 
 export const ACTION_ID = 'embeddable_copyToClipboard';
@@ -38,7 +39,7 @@ export const createCopyToClipboardLensAction = ({ order }: { order?: number }) =
     getDisplayName: () => COPY_TO_CLIPBOARD,
     isCompatible: async ({ embeddable, data }) =>
       !isErrorEmbeddable(embeddable as IEmbeddable) &&
-      isLensEmbeddable(embeddable as IEmbeddable) &&
+      isLensApi(embeddable) &&
       isDataColumnsValid(data) &&
       isInSecurityApp(currentAppId),
     execute: async ({ data }) => {
