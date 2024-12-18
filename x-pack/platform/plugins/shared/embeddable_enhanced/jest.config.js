4131d2de03e6a7c240fbb89eaf1452d088cbdb3a
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-12-18 15:48:07 +0100

    Relocating module `@kbn/embeddable-enhanced-plugin`

--- x-pack/platform/plugins/shared/embeddable_enhanced/jest.config.js
+++ x-pack/platform/plugins/shared/embeddable_enhanced/jest.config.js
@@ -7,9 +7,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/embeddable_enhanced'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/embeddable_enhanced',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/plugins/shared/embeddable_enhanced'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/shared/embeddable_enhanced',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/embeddable_enhanced/public/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/shared/embeddable_enhanced/public/**/*.{ts,tsx}'],
 };
