b0ebc8c585d70c8476827e215d2f8b427f57f6c4
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-18 18:08:12 +0100

    Relocating module `@kbn/core-custom-branding-browser-mocks`

--- src/core/packages/custom-branding/browser-mocks/jest.config.js
+++ src/core/packages/custom-branding/browser-mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/custom-branding/core-custom-branding-browser-mocks'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/custom-branding/browser-mocks'],
 };
