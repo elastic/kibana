e21fc9d8ab4ec3108ccc9f98c46e0b37767e15f1
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-12-19 01:16:07 +0100

    Relocating module `@kbn/rison`

--- .buildkite/scripts/pipelines/pull_request/pipeline.ts
+++ .buildkite/scripts/pipelines/pull_request/pipeline.ts
@@ -301,7 +301,7 @@ const getPipeline = (filename: string, removeSteps = true) => {
         /^packages\/kbn-i18n-react/,
         /^packages\/kbn-grouping/,
         /^packages\/kbn-resizable-layout/,
-        /^packages\/kbn-rison/,
+        /^src\/platform\/packages\/shared\/kbn-rison/,
         /^src\/platform\/packages\/shared\/kbn-rule-data-utils/,
         /^packages\/kbn-safer-lodash-set/,
         /^packages\/kbn-search-types/,
