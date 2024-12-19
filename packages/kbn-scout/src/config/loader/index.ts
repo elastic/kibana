9bda7d5a49ae7cffa0c0c2a2fe4f0e049790958d
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2024-12-19 17:36:07 +0100

    [scout] adding unit tests (#204567)
    ## Summary
    
    Adding tests and making adjustments/fixes based on the findings.
    
    Note: no integration tests were added to verify servers start as it is
    mostly equal to `@kbn-test` functionality that has jest integration
    tests. We can add it later, when Scout has specific logic.
    
    How to run: `node scripts/jest --config
    packages/kbn-scout/jest.config.js`
    
    Scope:
    
    ```
     PASS  packages/kbn-scout/src/config/config.test.ts
     PASS  packages/kbn-scout/src/config/loader/read_config_file.test.ts
     PASS  packages/kbn-scout/src/config/utils/get_config_file.test.ts
     PASS  packages/kbn-scout/src/config/utils/load_servers_config.test.ts
     PASS  packages/kbn-scout/src/config/utils/save_scout_test_config.test.ts
     PASS  packages/kbn-scout/src/playwright/config/create_config.test.ts
     PASS  packages/kbn-scout/src/playwright/runner/config_validator.test.ts
     PASS  packages/kbn-scout/src/playwright/runner/flags.test.ts
     PASS  packages/kbn-scout/src/playwright/utils/runner_utils.test.ts
     PASS  packages/kbn-scout/src/servers/flags.test.ts
    ```

--- packages/kbn-scout/src/config/loader/index.ts
+++ packages/kbn-scout/src/config/loader/index.ts
@@ -7,7 +7,4 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { ScoutServerConfig } from '../../types';
-import { defaultConfig } from './base.config';
-
-export const servers: ScoutServerConfig = defaultConfig;
+export { readConfigFile } from './read_config_file';
