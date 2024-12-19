8184a42333a83cf1e1c462639790516aff5a4d8c
Giorgos Bamparopoulos <georgios.bamparopoulos@elastic.co>
2024-12-19 14:25:08 +0000

    Relocating module `@kbn/discover-contextual-components`

--- src/platform/packages/shared/kbn-discover-contextual-components/jest.config.js
+++ src/platform/packages/shared/kbn-discover-contextual-components/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-discover-contextual-components'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-discover-contextual-components'],
 };
