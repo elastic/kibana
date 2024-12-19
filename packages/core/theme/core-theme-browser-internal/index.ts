9bd3876d876cce3820f159b7519276e84203ac47
Sébastien Loix <sebastien.loix@elastic.co>
2024-12-19 16:12:59 +0000

    Show toast message whenever the system color mode changes

--- packages/core/theme/core-theme-browser-internal/index.ts
+++ packages/core/theme/core-theme-browser-internal/index.ts
@@ -10,3 +10,4 @@
 export { ThemeService } from './src/theme_service';
 export { CoreThemeProvider } from './src/core_theme_provider';
 export type { ThemeServiceSetupDeps } from './src/theme_service';
+export { browsersSupportsSystemTheme } from './src/system_theme';
