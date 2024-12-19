0a67ca39f2bab1e371f901c90bea2a37ad9f1d43
Kfir Peled <61654899+kfirpeled@users.noreply.github.com>
2024-12-19 14:50:39 +0000

    [Cloud Security] Update graph appearance (#204610)
    ## Summary
    
    - [x] Remove the unknown or question symbols for entities who's type we
    don't know
    - [x] Replace edges to be sharp path instead of a smooth one 
    - [x] Changed background of alerted node from `danger` to `primary`
    - [x] Fix arrow popover color not in sync with background color 
    - [x] Handles should be visible as a small dot
    - [x] Expand button's color should be similar to the nodes color
    - [x] Main alert path should be a solid line
    
    Before:
    
    ![Screenshot 2024-12-18 at 18 45
    31](https://github.com/user-attachments/assets/4fd24ec7-8a8b-4659-8605-a53685be8d78)
    
    
    After:
    
    ![Screenshot 2024-12-18 at 19 08
    31](https://github.com/user-attachments/assets/ebeae458-075d-48a3-8a42-66787440adc1)
    
    
    
    https://github.com/user-attachments/assets/4253ec83-19da-4c34-bb80-cb4ca900672d
    
    
    **How to test**
    
    To test this PR you can run
    
    ```
    yarn storybook cloud_security_posture_packages
    ```
    
    To test e2e
    
    - Enable the feature flag 
    
    `kibana.dev.yml`:
    
    ```yaml
    uiSettings.overrides.securitySolution:enableVisualizationsInFlyout: true
    xpack.securitySolution.enableExperimental: ['graphVisualizationInFlyoutEnabled']
    ```
    
    - Load mocked data:
    
    ```bash
    node scripts/es_archiver load x-pack/test/cloud_security_posture_functional/es_archives/logs_gcp_audit \ 
      --es-url http://elastic:changeme@localhost:9200 \
      --kibana-url http://elastic:changeme@localhost:5601
    
    node scripts/es_archiver load x-pack/test/cloud_security_posture_functional/es_archives/security_alerts \
      --es-url http://elastic:changeme@localhost:9200 \
      --kibana-url http://elastic:changeme@localhost:5601
    ```
    
    - Make sure you include data from Oct 13 2024. (in the video I use Last
    year)
    
    
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: Sean Rathier <sean.rathier@elastic.co>
    Co-authored-by: Brad White <Ikuni17@users.noreply.github.com>
    Co-authored-by: seanrathier <sean.rathier@gmail.com>

--- x-pack/solutions/security/packages/kbn-cloud-security-posture/graph/src/components/node/hexagon_node.tsx
+++ x-pack/solutions/security/packages/kbn-cloud-security-posture/graph/src/components/node/hexagon_node.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { memo } from 'react';
-import { useEuiBackgroundColor, useEuiTheme } from '@elastic/eui';
+import { useEuiTheme } from '@elastic/eui';
 import { Handle, Position } from '@xyflow/react';
 import {
   NodeShapeContainer,
@@ -15,6 +15,7 @@ import {
   NodeIcon,
   NodeButton,
   HandleStyleOverride,
+  useNodeFillColor,
 } from './styles';
 import type { EntityNodeViewModel, NodeProps } from '../types';
 import { HexagonHoverShape, HexagonShape } from './shapes/hexagon_shape';
@@ -51,7 +52,7 @@ export const HexagonNode: React.FC<NodeProps> = memo((props: NodeProps) => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <HexagonShape
-            fill={useEuiBackgroundColor(color ?? 'primary')}
+            fill={useNodeFillColor(color)}
             stroke={euiTheme.colors[color ?? 'primary']}
           />
           {icon && <NodeIcon x="11" y="15" icon={icon} color={color} />}
@@ -60,6 +61,7 @@ export const HexagonNode: React.FC<NodeProps> = memo((props: NodeProps) => {
           <>
             <NodeButton onClick={(e) => nodeClick?.(e, props)} />
             <NodeExpandButton
+              color={color}
               onClick={(e, unToggleCallback) => expandButtonClick?.(e, props, unToggleCallback)}
               x={`${NODE_WIDTH - NodeExpandButton.ExpandButtonSize / 2 + 2}px`}
               y={`${(NODE_HEIGHT - NodeExpandButton.ExpandButtonSize) / 2 - 2}px`}
