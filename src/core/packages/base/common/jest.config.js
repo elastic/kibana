1e865e50bccb06cde71d8ee870921036e7886053
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-19 07:39:47 +0100

    Relocating module `@kbn/core-base-common`

--- src/core/packages/base/common/jest.config.js
+++ src/core/packages/base/common/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/base/core-base-common'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/base/common'],
 };
