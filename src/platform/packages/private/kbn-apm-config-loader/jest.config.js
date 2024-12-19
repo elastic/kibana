b760239171d467935d275c253fb6e00f66a910c4
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2024-12-19 01:06:30 +0100

    Relocating module `@kbn/apm-config-loader`

--- src/platform/packages/private/kbn-apm-config-loader/jest.config.js
+++ src/platform/packages/private/kbn-apm-config-loader/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-apm-config-loader'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-apm-config-loader'],
 };
