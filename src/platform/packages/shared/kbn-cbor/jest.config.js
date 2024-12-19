9906f88b21ab909063ebc0f16eb2738f3426901a
Michael Dokolin <mikhail.dokolin@elastic.co>
2024-12-19 01:06:26 +0100

    Relocating module `@kbn/cbor`

--- src/platform/packages/shared/kbn-cbor/jest.config.js
+++ src/platform/packages/shared/kbn-cbor/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-cbor'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-cbor'],
 };
