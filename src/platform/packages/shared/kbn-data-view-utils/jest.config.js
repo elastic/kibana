8d9b4e5c15fd28103ded4be4eb88bef1015b2949
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 13:41:09 +0100

    Relocating module `@kbn/data-view-utils`

--- src/platform/packages/shared/kbn-data-view-utils/jest.config.js
+++ src/platform/packages/shared/kbn-data-view-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-data-view-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-data-view-utils'],
 };
