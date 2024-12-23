e4803be79bb1098b4053077f534e85c67b2df113
Nick Peihl <nick.peihl@elastic.co>
2024-12-23 15:24:08 -0500

    Relocating module `@kbn/expression-image-plugin`

--- src/platform/plugins/shared/expression_image/jest.config.js
+++ src/platform/plugins/shared/expression_image/jest.config.js
@@ -9,11 +9,11 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/src/plugins/expression_image'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/expression_image',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/shared/expression_image'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/shared/expression_image',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/src/plugins/expression_image/{common,public,server}/**/*.{ts,tsx}',
+    '<rootDir>/src/platform/plugins/shared/expression_image/{common,public,server}/**/*.{ts,tsx}',
   ],
 };
