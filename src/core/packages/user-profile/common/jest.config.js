936e3401893efa676b5f2eecda9332db9e45603c
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 22:47:12 +0100

    Relocating module `@kbn/core-user-profile-common`

--- src/core/packages/user-profile/common/jest.config.js
+++ src/core/packages/user-profile/common/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/user-profile/core-user-profile-common'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/user-profile/common'],
 };
