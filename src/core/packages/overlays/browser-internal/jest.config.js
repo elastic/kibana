e7bbcbe805e503eb48107c7c754f817ef480c45f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 21:02:38 +0100

    Relocating module `@kbn/core-overlays-browser-internal`

--- src/core/packages/overlays/browser-internal/jest.config.js
+++ src/core/packages/overlays/browser-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/overlays/core-overlays-browser-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/overlays/browser-internal'],
 };
