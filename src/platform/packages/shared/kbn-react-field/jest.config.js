5090f41cfa97c4d852dab6f699112e4c8a168027
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 14:00:21 +0100

    Relocating module `@kbn/react-field`

--- src/platform/packages/shared/kbn-react-field/jest.config.js
+++ src/platform/packages/shared/kbn-react-field/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-react-field'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-react-field'],
 };
