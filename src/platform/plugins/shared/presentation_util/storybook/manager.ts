118a9dad4dd7f5b68e12e5f6c8891705480c77d2
Nick Peihl <nick.peihl@elastic.co>
2024-12-23 15:40:24 -0500

    Relocating module `@kbn/presentation-util-plugin`

--- src/platform/plugins/shared/presentation_util/storybook/manager.ts
+++ src/platform/plugins/shared/presentation_util/storybook/manager.ts
@@ -15,7 +15,7 @@ addons.setConfig({
   theme: create({
     base: 'light',
     brandTitle: 'Kibana Presentation Utility Storybook',
-    brandUrl: 'https://github.com/elastic/kibana/tree/main/src/plugins/presentation_util',
+    brandUrl: 'https://github.com/elastic/kibana/tree/main/src/platform/plugins/shared/presentation_util',
   }),
   showPanel: true.valueOf,
   selectedPanel: PANEL_ID,
