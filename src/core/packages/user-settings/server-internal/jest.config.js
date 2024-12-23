dbea7ba4c8617221b96c76bd08f3ec720cc64f0b
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 23:09:28 +0100

    Relocating module `@kbn/core-user-settings-server-internal`

--- src/core/packages/user-settings/server-internal/jest.config.js
+++ src/core/packages/user-settings/server-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/user-settings/core-user-settings-server-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/user-settings/server-internal'],
 };
