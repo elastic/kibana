99f284040f3b26b15603fd3cbfb715acad9f1685
Brad White <brad.white@elastic.co>
2024-12-23 13:58:28 -0700

    fix hook in story

--- x-pack/plugins/fleet/public/applications/fleet/components/fleet_server_instructions/flyout.stories.tsx
+++ x-pack/plugins/fleet/public/applications/fleet/components/fleet_server_instructions/flyout.stories.tsx
@@ -8,28 +8,25 @@
 import React from 'react';
 import { EuiButton } from '@elastic/eui';
 
-import { FleetServerFlyout as FleetServerFlyoutComponent } from '.';
+import { FleetServerFlyout } from '.';
 
-export const FleetServerFlyout = {
-  render: () => {
-    const [isOpen, setIsOpen] = React.useState(false);
+const FleetServerFlyoutComponent = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
 
-    return (
-      <div style={{ width: 900 }}>
-        <EuiButton size="m" fill color="primary" onClick={() => setIsOpen(true)}>
-          Show flyout
-        </EuiButton>
-        {isOpen && <FleetServerFlyoutComponent onClose={() => setIsOpen(false)} />}
-      </div>
-    );
-  },
-
-  args: {
-    isCloudEnabled: false,
-  },
+  return (
+    <div style={{ width: 900 }}>
+      <EuiButton size="m" fill color="primary" onClick={() => setIsOpen(true)}>
+        Show flyout
+      </EuiButton>
+      {isOpen && <FleetServerFlyout onClose={() => setIsOpen(false)} />}
+    </div>
+  );
 };
 
 export default {
-  component: FleetServerFlyout,
+  component: FleetServerFlyoutComponent,
   title: 'Sections/Fleet/Agents/Fleet Server Instructions/In Flyout',
+  args: {
+    isCloudEnabled: false,
+  },
 };
