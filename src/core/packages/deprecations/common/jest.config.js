d1f4ea21b7ab0d090e608aa11b7e88a9922ae394
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 19:40:00 +0100

    Relocating module `@kbn/core-deprecations-common`

--- src/core/packages/deprecations/common/jest.config.js
+++ src/core/packages/deprecations/common/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/deprecations/core-deprecations-common'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/deprecations/common'],
 };
