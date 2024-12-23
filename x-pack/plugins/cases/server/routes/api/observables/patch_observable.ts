a4d80f430dc679f2b3511f93f6b25d207bbc61ae
Luke Gmys <11671118+lgestc@users.noreply.github.com>
2024-12-23 14:25:58 +0100

    [Security Solution] [Cases] Introduce case observables (phase 0 & 1) (#190237)
    ## Summary
    
    ### Introducting Case Observables - _phases 0 and 1_
    
    This pull request introduces case observables to Kibana, enhancing the
    platform's case management capabilities. It adds support for capturing
    and displaying observables (e.g., IP addresses, URLs, file hashes)
    linked to cases. The feature integrates with the Cases UI, allowing
    users to easily associate observables with cases for better tracking and
    analysis in incident response workflows. This improves investigative
    efficiency by correlating observables across multiple cases.
    
    #### Requirements:
    
    https://docs.google.com/document/d/12hZTpyn0eXy3Xnq8qLBd6_sJxBhNZoI7vXztxWHhUds/edit#heading=h.srf6mb8ifiad
    
    #### Design document:
    https://docs.google.com/document/d/1MeDLl6OEWast1RC1M3_hQXnRCd8frrXdGkFnypIYKJQ/edit#heading=h.kb5lrp2j62id
    
    Notable Cases sections are added in this pr:
    
    **1. Observables section in the case view, allowing for adding and
    listing up to 10 observables for the case**
    
    ![image](https://github.com/user-attachments/assets/f517803d-a6a3-4428-b3e3-478e70c60050)
    
    **2. Similar cases view for every case, allowing for similar case
    discovery**
    
    ![image](https://github.com/user-attachments/assets/388fddfb-9533-4f0d-aa8b-f5601e5323e0)
    
    **3. Observable types management view in Cases settings**
    
    ![image](https://github.com/user-attachments/assets/2d76f8be-c234-4f24-a419-da54228fb111)
    
    Original issue:
    
    https://github.com/elastic/kibana/issues/180360
    
    Things skipped for now from MVP:
    - [ ] Allow users to manually create observables from the cases alerts
    table using the table actions (Phase 1)
    - [ ] Allow users to manually create observables of type “hash” from the
    files table using the table actions (Phase 1)
    
    ---------
    
    Co-authored-by: Christos Nasikas <xristosnasikas@gmail.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Christos Nasikas <christos.nasikas@elastic.co>
    (cherry picked from commit 3083706bc9541d84700b81252f0e4880949e4ea0)

---
+++ x-pack/plugins/cases/server/routes/api/observables/patch_observable.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { schema } from '@kbn/config-schema';
+import { INTERNAL_CASE_OBSERVABLES_PATCH_URL } from '../../../../common/constants';
+import { createCaseError } from '../../../common/error';
+import { createCasesRoute } from '../create_cases_route';
+import type { observableApiV1 } from '../../../../common/types/api';
+
+export const patchObservableRoute = createCasesRoute({
+  method: 'patch',
+  path: INTERNAL_CASE_OBSERVABLES_PATCH_URL,
+  params: {
+    params: schema.object({
+      case_id: schema.string(),
+      observable_id: schema.string(),
+    }),
+  },
+  routerOptions: {
+    access: 'internal',
+    summary: `Update a case observable`,
+  },
+  handler: async ({ context, request, response }) => {
+    try {
+      const caseContext = await context.cases;
+      const casesClient = await caseContext.getCasesClient();
+      const caseId = request.params.case_id;
+      const observableId = request.params.observable_id;
+
+      const { observable } = request.body as observableApiV1.UpdateObservableRequest;
+
+      const theCase = await casesClient.cases.updateObservable(caseId, observableId, {
+        observable,
+      });
+
+      return response.ok({
+        body: theCase,
+      });
+    } catch (error) {
+      throw createCaseError({
+        message: `Failed to patch observable in route case id: ${request.params.case_id}, observable id: ${request.params.observable_id}: ${error}`,
+        error,
+      });
+    }
+  },
+});
