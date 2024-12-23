2f4402511ce5c392c4dabac36408ccc2edd6e80b
Nick Peihl <nick.peihl@elastic.co>
2024-12-23 15:39:18 -0500

    Relocating module `@kbn/presentation-publishing`

--- src/platform/packages/shared/presentation/presentation_publishing/jest.config.js
+++ src/platform/packages/shared/presentation/presentation_publishing/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/presentation/presentation_publishing'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/presentation/presentation_publishing'],
 };
