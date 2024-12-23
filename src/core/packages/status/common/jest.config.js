e716c4409ae0db8a47db439db4c5d8faa431cfb1
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 22:20:54 +0100

    Relocating module `@kbn/core-status-common`

--- src/core/packages/status/common/jest.config.js
+++ src/core/packages/status/common/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/status/core-status-common'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/status/common'],
 };
