8dc3ea15869033194b9d3f026f43c72913fd84b0
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 19:50:48 +0100

    Relocating module `@kbn/core-elasticsearch-server`

--- src/core/packages/elasticsearch/server/jest.config.js
+++ src/core/packages/elasticsearch/server/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/elasticsearch/core-elasticsearch-server'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/elasticsearch/server'],
 };
