2855ea0f3c4d861bd7da90cce3979046d2f3b19b
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-23 17:39:56 +0100

    Relocating module `@kbn/search-api-panels`

--- src/platform/packages/shared/kbn-search-api-panels/jest.config.js
+++ src/platform/packages/shared/kbn-search-api-panels/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-search-api-panels'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-search-api-panels'],
 };
