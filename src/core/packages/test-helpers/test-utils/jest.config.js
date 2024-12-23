d246579105ab7c0534568ca1e0d882a9df14f342
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 22:28:49 +0100

    Relocating module `@kbn/core-test-helpers-test-utils`

--- src/core/packages/test-helpers/test-utils/jest.config.js
+++ src/core/packages/test-helpers/test-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/test-helpers/core-test-helpers-test-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/test-helpers/test-utils'],
 };
