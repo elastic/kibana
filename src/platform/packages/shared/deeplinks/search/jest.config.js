8db8ab4e5a0bf8064feb84d73d666fe940919339
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-23 17:30:04 +0100

    Relocating module `@kbn/deeplinks-search`

--- src/platform/packages/shared/deeplinks/search/jest.config.js
+++ src/platform/packages/shared/deeplinks/search/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/deeplinks/search'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/deeplinks/search'],
 };
