3e7b3560e03ef3de593324f37d02021507e41b06
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-23 20:39:44 +0100

    Relocating module `@kbn/core-lifecycle-browser-internal`

--- src/core/packages/lifecycle/browser-internal/jest.config.js
+++ src/core/packages/lifecycle/browser-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/lifecycle/core-lifecycle-browser-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/lifecycle/browser-internal'],
 };
