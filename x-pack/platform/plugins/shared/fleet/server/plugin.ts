9738e8697d704e867d8a7d2f48ad10405e8f1e69
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2024-12-25 13:24:22 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/fleet/server/plugin.ts
+++ x-pack/platform/plugins/shared/fleet/server/plugin.ts
@@ -101,12 +101,7 @@ import { registerEncryptedSavedObjects, registerSavedObjects } from './saved_obj
 import { registerRoutes } from './routes';
 
 import type { ExternalCallback, FleetRequestHandlerContext } from './types';
-import type {
-  AgentPolicyServiceInterface,
-  AgentService,
-  ArtifactsClientInterface,
-  PackageService,
-} from './services';
+import type { AgentService, ArtifactsClientInterface, PackageService } from './services';
 import {
   agentPolicyService,
   AgentServiceImpl,
