6cb35fc84e6e559eb6b77b2e808b16374d17fc97
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-26 09:53:23 +0100

    Relocating module `@kbn/alerting-types`

--- src/platform/packages/shared/kbn-alerting-types/jest.config.js
+++ src/platform/packages/shared/kbn-alerting-types/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-alerting-types'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-alerting-types'],
 };
