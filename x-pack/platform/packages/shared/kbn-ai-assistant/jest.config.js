62ac3d74bec6d23fa9aa6ba0cc585fe26509a1ce
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-26 10:28:27 +0100

    Relocating module `@kbn/ai-assistant`

--- x-pack/platform/packages/shared/kbn-ai-assistant/jest.config.js
+++ x-pack/platform/packages/shared/kbn-ai-assistant/jest.config.js
@@ -9,10 +9,10 @@ module.exports = {
   coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/packages/kbn_ai_assistant_src',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/x-pack/packages/kbn-ai-assistant/src/**/*.{ts,tsx}',
-    '!<rootDir>/x-pack/packages/kbn-ai-assistant/src/*.test.{ts,tsx}',
+    '<rootDir>/x-pack/platform/packages/shared/kbn-ai-assistant/src/**/*.{ts,tsx}',
+    '!<rootDir>/x-pack/platform/packages/shared/kbn-ai-assistant/src/*.test.{ts,tsx}',
   ],
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/packages/kbn-ai-assistant'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/packages/shared/kbn-ai-assistant'],
 };
