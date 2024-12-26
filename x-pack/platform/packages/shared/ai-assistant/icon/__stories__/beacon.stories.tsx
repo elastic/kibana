54c06ad752975908f428a2a31c6536a5a5a32301
Brad White <brad.white@elastic.co>
2024-12-26 13:01:11 -0700

    fix componentmeta and componentstory

--- x-pack/platform/packages/shared/ai-assistant/icon/__stories__/beacon.stories.tsx
+++ x-pack/platform/packages/shared/ai-assistant/icon/__stories__/beacon.stories.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Meta, StoryFn } from '@storybook/react';
 
 import { AssistantBeacon as Component } from '../beacon';
 
@@ -20,6 +20,6 @@ export default {
       defaultValue: 'xxl',
     },
   },
-} as ComponentMeta<typeof Component>;
+} as Meta<typeof Component>;
 
-export const Beacon: ComponentStory<typeof Component> = (args) => <Component {...args} />;
+export const Beacon: StoryFn<typeof Component> = (args) => <Component {...args} />;
