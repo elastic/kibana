77b709033c8eebef6b602eee02ef752773f246b9
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-30 09:45:47 +0100

    Relocating module `@kbn/xstate-utils`

--- src/platform/packages/shared/kbn-xstate-utils/jest.config.js
+++ src/platform/packages/shared/kbn-xstate-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-xstate-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-xstate-utils'],
 };
