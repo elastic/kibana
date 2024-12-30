36c5dbd0dd66ebb9cbceb75a6159cee88ebec629
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-30 10:42:53 +0100

    Relocating module `@kbn/search-response-warnings`

--- src/platform/packages/shared/kbn-search-response-warnings/jest.config.js
+++ src/platform/packages/shared/kbn-search-response-warnings/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-search-response-warnings'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-search-response-warnings'],
 };
