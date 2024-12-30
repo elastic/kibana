be8cac0aef7bb312153be5483ccd32de02fdaa73
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-30 09:44:09 +0100

    Relocating module `@kbn/timerange`

--- src/platform/packages/shared/kbn-timerange/jest.config.js
+++ src/platform/packages/shared/kbn-timerange/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-timerange'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-timerange'],
 };
