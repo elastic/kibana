dbb34cc0d67b5187cc37fc3fcc618547ba63fb2c
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2024-12-30 08:42:08 -0500

    [Obs AI Assistant] Avoid passing data only if data is empty

--- x-pack/solutions/observability/plugins/observability_ai_assistant_app/scripts/evaluation/kibana_client.ts
+++ x-pack/solutions/observability/plugins/observability_ai_assistant_app/scripts/evaluation/kibana_client.ts
@@ -124,7 +124,7 @@ export class KibanaClient {
     return this.axios<T>({
       method,
       url,
-      ...(method.toLowerCase() !== 'delete' ? { data: data || {} } : {}),
+      ...(method.toLowerCase() === 'delete' && !data ? {} : { data: data || {} }),
       headers: {
         'kbn-xsrf': 'true',
         'x-elastic-internal-origin': 'Kibana',
