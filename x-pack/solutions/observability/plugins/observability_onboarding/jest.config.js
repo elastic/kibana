372b535801d35fec7cb2b59915310ceea32293ef
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-30 09:39:13 +0100

    Relocating module `@kbn/observability-onboarding-plugin`

--- x-pack/solutions/observability/plugins/observability_onboarding/jest.config.js
+++ x-pack/solutions/observability/plugins/observability_onboarding/jest.config.js
@@ -9,6 +9,6 @@ const path = require('path');
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: path.resolve(__dirname, '../../../..'),
-  roots: ['<rootDir>/x-pack/plugins/observability_solution/observability_onboarding'],
+  rootDir: path.resolve(__dirname, '../../../../..'),
+  roots: ['<rootDir>/x-pack/solutions/observability/plugins/observability_onboarding'],
 };
