f1f760052406fb2842a010d03fcd630386930148
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-30 09:21:02 +0100

    Relocating module `@kbn/data-quality-plugin`

--- x-pack/platform/plugins/shared/data_quality/jest.config.js
+++ x-pack/platform/plugins/shared/data_quality/jest.config.js
@@ -7,9 +7,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/x-pack/plugins/data_quality'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/data_quality',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/x-pack/platform/plugins/shared/data_quality'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/shared/data_quality',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: ['<rootDir>/x-pack/plugins/datas_quality/{common,public}/**/*.{ts,tsx}'],
 };
