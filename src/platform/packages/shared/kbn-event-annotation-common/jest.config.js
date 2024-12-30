ab0198593b02d18b2bde5a32d4a2a8343949884f
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-30 13:46:35 +0100

    Relocating module `@kbn/event-annotation-common`

--- src/platform/packages/shared/kbn-event-annotation-common/jest.config.js
+++ src/platform/packages/shared/kbn-event-annotation-common/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-event-annotation-common'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-event-annotation-common'],
 };
