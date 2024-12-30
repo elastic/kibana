d8af42931f5adf8d25d0246c0d0f396d7da1f9cb
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-30 09:25:57 +0100

    Relocating module `@kbn/elastic-agent-utils`

--- src/platform/packages/shared/kbn-elastic-agent-utils/jest.config.js
+++ src/platform/packages/shared/kbn-elastic-agent-utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-elastic-agent-utils'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-elastic-agent-utils'],
 };
