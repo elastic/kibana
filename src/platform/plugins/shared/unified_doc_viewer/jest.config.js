404c103c524e9f41cb64f5efbc5bad59f06e62f7
Gerard Soldevila <gerard.soldevila@elastic.co>
2024-12-30 10:47:58 +0100

    Relocating module `@kbn/unified-doc-viewer-plugin`

--- src/platform/plugins/shared/unified_doc_viewer/jest.config.js
+++ src/platform/plugins/shared/unified_doc_viewer/jest.config.js
@@ -9,11 +9,11 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/src/plugins/unified_doc_viewer'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/unified_doc_viewer',
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/shared/unified_doc_viewer'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/shared/unified_doc_viewer',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/src/plugins/unified_doc_viewer/{common,public,server}/**/*.{ts,tsx}',
+    '<rootDir>/src/platform/plugins/shared/unified_doc_viewer/{common,public,server}/**/*.{ts,tsx}',
   ],
 };
