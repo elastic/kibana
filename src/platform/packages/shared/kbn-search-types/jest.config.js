c9ff2280234445487221a5745d4d478b3f270dde
pgayvallet <pierre.gayvallet@elastic.co>
2024-12-30 15:22:35 +0100

    Relocating module `@kbn/search-types`

--- src/platform/packages/shared/kbn-search-types/jest.config.js
+++ src/platform/packages/shared/kbn-search-types/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-search-types'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-search-types'],
 };
