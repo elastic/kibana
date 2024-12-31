7a29fed73ae384c58505ceb62a14f67a82deba3e
Kevin Delemme <kevin.delemme@elastic.co>
2024-12-31 15:04:38 -0500

    Store last selected view, group and sort options

--- x-pack/solutions/observability/plugins/slo/public/pages/slos/hooks/use_url_search_state.ts
+++ x-pack/solutions/observability/plugins/slo/public/pages/slos/hooks/use_url_search_state.ts
@@ -6,14 +6,19 @@
  */
 
 import type { Filter } from '@kbn/es-query';
-import { createKbnUrlStateStorage } from '@kbn/kibana-utils-plugin/public';
+import {
+  createKbnUrlStateStorage,
+  createSessionStorageStateStorage,
+} from '@kbn/kibana-utils-plugin/public';
 import deepmerge from 'deepmerge';
+import { pick } from 'lodash';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { DEFAULT_SLO_PAGE_SIZE } from '../../../../common/constants';
 import type { GroupByField, SortDirection, SortField, ViewType } from '../types';
 
 export const SLO_LIST_SEARCH_URL_STORAGE_KEY = 'search';
+export const SLO_LIST_SEARCH_SESSION_STORAGE_KEY = 'slo_list_search_state';
 
 export interface SearchState {
   kqlQuery: string;
@@ -56,6 +61,8 @@ export function useUrlSearchState(): {
     })
   );
 
+  const sessionStorage = useRef(createSessionStorageStateStorage());
+
   useEffect(() => {
     const sub = urlStateStorage.current
       ?.change$<SearchState>(SLO_LIST_SEARCH_URL_STORAGE_KEY)
@@ -66,21 +73,30 @@ export function useUrlSearchState(): {
       });
 
     setState(
-      urlStateStorage.current?.get<SearchState>(SLO_LIST_SEARCH_URL_STORAGE_KEY) ?? DEFAULT_STATE
+      urlStateStorage.current?.get<SearchState>(SLO_LIST_SEARCH_URL_STORAGE_KEY) ??
+        sessionStorage.current?.get<SearchState>(SLO_LIST_SEARCH_SESSION_STORAGE_KEY) ??
+        DEFAULT_STATE
     );
 
     return () => {
       sub?.unsubscribe();
     };
-  }, [urlStateStorage]);
+  }, [urlStateStorage, sessionStorage]);
 
   const onStateChange = useCallback(
     (newState: Partial<SearchState>) => {
       const updatedState = { ...state, page: 0, ...newState };
       setState(() => updatedState);
+
       urlStateStorage.current?.set(SLO_LIST_SEARCH_URL_STORAGE_KEY, updatedState, {
         replace: true,
       });
+
+      // Discard search itself from session storage. Keep only view preferences
+      sessionStorage.current?.set(
+        SLO_LIST_SEARCH_SESSION_STORAGE_KEY,
+        pick(updatedState, 'sort', 'view', 'groupBy')
+      );
     },
     [state]
   );
