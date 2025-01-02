1d8aead5587cf95685513f7444a44c0119a03360
Anton Dosov <anton.dosov@elastic.co>
2025-01-02 13:48:27 +0100

    fix prettier

--- x-pack/platform/plugins/private/banners/jest.config.js
+++ x-pack/platform/plugins/private/banners/jest.config.js
@@ -9,7 +9,10 @@ module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
   roots: ['<rootDir>/x-pack/platform/plugins/private/banners'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/private/banners',
+  coverageDirectory:
+    '<rootDir>/target/kibana-coverage/jest/x-pack/platform/plugins/private/banners',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/private/banners/{common,public,server}/**/*.{ts,tsx}'],
+  collectCoverageFrom: [
+    '<rootDir>/x-pack/platform/plugins/private/banners/{common,public,server}/**/*.{ts,tsx}',
+  ],
 };
