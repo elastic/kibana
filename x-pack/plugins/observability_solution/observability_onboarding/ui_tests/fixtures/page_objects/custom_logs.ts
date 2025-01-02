12b2204caea75c4e80cbe79a3e8c8791154f1de2
Mykola Harmash <mykola.harmash@elastic.co>
2025-01-02 11:11:51 +0100

    fixup! [Kibana Overview] Shift SCSS import and lazy-load main component (#204661)

---
+++ x-pack/plugins/observability_solution/observability_onboarding/ui_tests/fixtures/page_objects/custom_logs.ts
@@ -0,0 +1,107 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { ScoutPage, KibanaUrl, KbnClient } from '@kbn/scout';
+
+export class CustomLogsPage {
+  constructor(
+    private readonly page: ScoutPage,
+    private readonly kbnUrl: KibanaUrl,
+    private readonly kbnClient: KbnClient
+  ) {}
+
+  async goto() {
+    this.page.goto(`${this.kbnUrl.app('observabilityOnboarding')}/customLogs`);
+  }
+
+  async clickBackButton() {
+    await this.page.testSubj.click('observabilityOnboardingFlowBackToSelectionButton');
+  }
+
+  logFilePathList() {
+    return this.page.locator(`[data-test-subj^=obltOnboardingLogFilePath-]`);
+  }
+
+  logFilePathInput(index: number) {
+    return this.page.testSubj.locator(`obltOnboardingLogFilePath-${index}`).getByRole('textbox');
+  }
+
+  continueButton() {
+    return this.page.testSubj.locator('obltOnboardingCustomLogsContinue');
+  }
+
+  addLogFilePathButton() {
+    return this.page.testSubj.locator('obltOnboardingCustomLogsAddFilePath');
+  }
+
+  logFilePathDeleteButton(index: number) {
+    return this.page.testSubj.locator(`obltOnboardingLogFilePathDelete-${index}`);
+  }
+
+  integrationNameInput() {
+    return this.page.testSubj.locator('obltOnboardingCustomLogsIntegrationsName');
+  }
+
+  datasetNameInput() {
+    return this.page.testSubj.locator('obltOnboardingCustomLogsDatasetName');
+  }
+
+  serviceNameInput() {
+    return this.page.testSubj.locator('obltOnboardingCustomLogsServiceName');
+  }
+
+  async clickAdvancedSettingsButton() {
+    return this.page.testSubj
+      .locator('obltOnboardingCustomLogsAdvancedSettings')
+      .getByRole('button')
+      .first()
+      .click();
+  }
+
+  namespaceInput() {
+    return this.page.testSubj.locator('obltOnboardingCustomLogsNamespace');
+  }
+
+  customConfigInput() {
+    return this.page.testSubj.locator('obltOnboardingCustomLogsCustomConfig');
+  }
+
+  async installCustomIntegration(name: string) {
+    await this.kbnClient.request({
+      method: 'POST',
+      path: `/api/fleet/epm/custom_integrations`,
+      body: {
+        force: true,
+        integrationName: name,
+        datasets: [
+          { name: `${name}.access`, type: 'logs' },
+          { name: `${name}.error`, type: 'metrics' },
+          { name: `${name}.warning`, type: 'logs' },
+        ],
+      },
+    });
+  }
+
+  async deleteIntegration(name: string) {
+    const packageInfo = await this.kbnClient.request<{ item: { status: string } }>({
+      method: 'GET',
+      path: `/api/fleet/epm/packages/${name}`,
+      ignoreErrors: [404],
+    });
+
+    if (packageInfo.data.item?.status === 'installed') {
+      await this.kbnClient.request({
+        method: 'DELETE',
+        path: `/api/fleet/epm/packages/${name}`,
+      });
+    }
+  }
+
+  customIntegrationErrorCallout() {
+    return this.page.testSubj.locator('obltOnboardingCustomIntegrationErrorCallout');
+  }
+}
