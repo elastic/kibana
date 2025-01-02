2d32b5486823e343845424bc7c526866cc1dfe40
PhilippeOberti <philippe.oberti@elastic.co>
2025-01-02 16:28:09 -0600

    memoize value

--- x-pack/solutions/security/plugins/security_solution/public/flyout/document_details/right/components/alert_status.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/document_details/right/components/alert_status.tsx
@@ -8,8 +8,7 @@
 import { EuiFlexGroup, EuiFlexItem, EuiSpacer, EuiTitle } from '@elastic/eui';
 import { getUserDisplayName } from '@kbn/user-profile-components';
 import { FormattedMessage } from '@kbn/i18n-react';
-import type { FC } from 'react';
-import React from 'react';
+import React, { memo, useMemo } from 'react';
 import { WORKFLOW_STATUS_DETAILS_TEST_ID, WORKFLOW_STATUS_TITLE_TEST_ID } from './test_ids';
 import { useBulkGetUserProfiles } from '../../../../common/components/user_profiles/use_bulk_get_user_profiles';
 import { PreferenceFormattedDate } from '../../../../common/components/formatted_date';
@@ -19,13 +18,31 @@ import { getField } from '../../shared/utils';
 /**
  * Displays info about who last updated the alert's workflow status and when.
  */
-export const AlertStatus: FC = () => {
+export const AlertStatus = memo(() => {
   const { getFieldsData } = useDocumentDetailsContext();
   const statusUpdatedBy = getFieldsData('kibana.alert.workflow_user');
   const statusUpdatedAt = getField(getFieldsData('kibana.alert.workflow_status_updated_at'));
 
   const result = useBulkGetUserProfiles({ uids: new Set(statusUpdatedBy) });
   const user = result.data?.[0]?.user;
+
+  const lastStatusChange = useMemo(
+    () => (
+      <>
+        {user && statusUpdatedAt && (
+          <FormattedMessage
+            id="xpack.securitySolution.flyout.right.about.status.statusHistoryDetails"
+            defaultMessage="Alert status updated by {user} on {date}"
+            values={{
+              user: getUserDisplayName(user),
+              date: <PreferenceFormattedDate value={new Date(statusUpdatedAt)} />,
+            }}
+          />
+        )}
+      </>
+    ),
+    [statusUpdatedAt, user]
+  );
 
   if (!statusUpdatedBy || !statusUpdatedAt || result.isLoading || user == null) {
     return null;
@@ -44,18 +61,9 @@ export const AlertStatus: FC = () => {
           </h5>
         </EuiTitle>
       </EuiFlexItem>
-      <EuiFlexItem data-test-subj={WORKFLOW_STATUS_DETAILS_TEST_ID}>
-        <FormattedMessage
-          id="xpack.securitySolution.flyout.right.about.status.statusHistoryDetails"
-          defaultMessage="Alert status updated by {user} on {date}"
-          values={{
-            user: getUserDisplayName(user),
-            date: <PreferenceFormattedDate value={new Date(statusUpdatedAt)} />,
-          }}
-        />
-      </EuiFlexItem>
+      <EuiFlexItem data-test-subj={WORKFLOW_STATUS_DETAILS_TEST_ID}>{lastStatusChange}</EuiFlexItem>
     </EuiFlexGroup>
   );
-};
+});
 
 AlertStatus.displayName = 'AlertStatus';
