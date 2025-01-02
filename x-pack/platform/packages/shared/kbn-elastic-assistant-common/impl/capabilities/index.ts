07a50b881280ec60a2d2f28760c7eb6d12e8ace5
konrad.szwarc <konrad.szwarc@elastic.co>
2025-01-02 17:19:49 +0100

    cleanup

--- x-pack/platform/packages/shared/kbn-elastic-assistant-common/impl/capabilities/index.ts
+++ x-pack/platform/packages/shared/kbn-elastic-assistant-common/impl/capabilities/index.ts
@@ -21,5 +21,5 @@ export type AssistantFeatureKey = keyof AssistantFeatures;
 export const defaultAssistantFeatures = Object.freeze({
   assistantModelEvaluation: false,
   attackDiscoveryAlertFiltering: false,
-  defendInsights: true,
+  defendInsights: false,
 });
