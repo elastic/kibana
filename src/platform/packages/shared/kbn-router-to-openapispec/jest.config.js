5fb2799fa989b509caedde615b900dd78bbdd348
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-04 00:09:05 +0100

    Relocating module `@kbn/router-to-openapispec`

--- src/platform/packages/shared/kbn-router-to-openapispec/jest.config.js
+++ src/platform/packages/shared/kbn-router-to-openapispec/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-router-to-openapispec'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-router-to-openapispec'],
 };
