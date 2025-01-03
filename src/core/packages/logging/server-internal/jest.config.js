652cfa5b8cefd30bb30cd6fa3557ae2ea3341930
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 22:57:19 +0100

    Relocating module `@kbn/core-logging-server-internal`

--- src/core/packages/logging/server-internal/jest.config.js
+++ src/core/packages/logging/server-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/logging/core-logging-server-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/logging/server-internal'],
 };
