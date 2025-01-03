7a25f38e0049934ad2dc2549999f9a28d6b2164c
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 23:43:56 +0100

    Relocating module `@kbn/core-test-helpers-so-type-serializer`

--- src/core/packages/test-helpers/so-type-serializer/jest.config.js
+++ src/core/packages/test-helpers/so-type-serializer/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/test-helpers/core-test-helpers-so-type-serializer'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/test-helpers/so-type-serializer'],
 };
