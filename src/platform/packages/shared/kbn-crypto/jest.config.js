6aa0d3de11779e124a3daf2a78e551e1e82bbbfb
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 16:46:33 +0100

    Relocating module `@kbn/crypto`

--- src/platform/packages/shared/kbn-crypto/jest.config.js
+++ src/platform/packages/shared/kbn-crypto/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-crypto'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-crypto'],
 };
