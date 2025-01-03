9909ba51d7efaa031d5398b5b43abc58857d3eeb
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 17:28:12 +0100

    Relocating module `@kbn/core-user-profile-browser`

--- src/core/packages/user-profile/browser/jest.config.js
+++ src/core/packages/user-profile/browser/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/user-profile/core-user-profile-browser'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/user-profile/browser'],
 };
