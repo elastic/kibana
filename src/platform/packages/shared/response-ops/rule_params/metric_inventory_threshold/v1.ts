73a77bb97a0baac39246dfd431dcb584128ca1ab
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-01-03 14:42:12 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/platform/packages/shared/response-ops/rule_params/metric_inventory_threshold/v1.ts
+++ src/platform/packages/shared/response-ops/rule_params/metric_inventory_threshold/v1.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { schema, Type, TypeOf } from '@kbn/config-schema';
+import { schema, Type } from '@kbn/config-schema';
 import { COMPARATORS } from '@kbn/alerting-comparators';
 
 import {
