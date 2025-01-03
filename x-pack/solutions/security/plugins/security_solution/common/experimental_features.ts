f65d4cdcc6303844ad568442832b36f1d6a2d90f
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-01-03 15:51:41 +0100

    [DO NOT MERGE][Rules migration] ES|QL editor experiment

--- x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts
+++ x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts
@@ -251,7 +251,7 @@ export const allowedExperimentalValues = Object.freeze({
   /**
    * Enables the siem migrations feature
    */
-  siemMigrationsEnabled: false,
+  siemMigrationsEnabled: true,
 
   /**
    * Enables the Defend Insights feature
