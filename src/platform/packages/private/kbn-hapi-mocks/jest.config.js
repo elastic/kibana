7d491f3dfc848152be9b682ca8f33f4ed244d438
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 17:39:50 +0100

    Relocating module `@kbn/hapi-mocks`

--- src/platform/packages/private/kbn-hapi-mocks/jest.config.js
+++ src/platform/packages/private/kbn-hapi-mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-hapi-mocks'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-hapi-mocks'],
 };
