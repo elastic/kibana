e4dd40641c0c4122e6308c4eae2ca9dfa8e6a7a6
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 16:57:54 +0100

    Relocating module `@kbn/core-rendering-server-internal`

--- src/core/packages/rendering/server-internal/jest.config.js
+++ src/core/packages/rendering/server-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/rendering/core-rendering-server-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/rendering/server-internal'],
 };
