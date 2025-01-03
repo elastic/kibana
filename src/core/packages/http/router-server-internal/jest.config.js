7df2d3415f10bdba83b3a7bf8e1f03fdfc666374
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 16:18:44 +0100

    Relocating module `@kbn/core-http-router-server-internal`

--- src/core/packages/http/router-server-internal/jest.config.js
+++ src/core/packages/http/router-server-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/http/core-http-router-server-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/http/router-server-internal'],
 };
