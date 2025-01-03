14c0c374855e378846e3cbd8559036a075daf6ec
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 17:34:36 +0100

    Relocating module `@kbn/user-profile-components`

--- src/platform/packages/shared/kbn-user-profile-components/jest.config.js
+++ src/platform/packages/shared/kbn-user-profile-components/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-user-profile-components'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-user-profile-components'],
 };
