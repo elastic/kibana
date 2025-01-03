18f637f4ded86c8ad4c05c234c4fc7fdc1cc446a
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 23:42:11 +0100

    Relocating module `@kbn/core-test-helpers-deprecations-getters`

--- src/core/packages/test-helpers/deprecations-getters/jest.config.js
+++ src/core/packages/test-helpers/deprecations-getters/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/test-helpers/core-test-helpers-deprecations-getters'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/test-helpers/deprecations-getters'],
 };
