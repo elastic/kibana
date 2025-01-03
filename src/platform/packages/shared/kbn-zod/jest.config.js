988c3c19b1afb77287c264be97a31414c75d973e
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 17:57:57 +0100

    Relocating module `@kbn/zod`

--- src/platform/packages/shared/kbn-zod/jest.config.js
+++ src/platform/packages/shared/kbn-zod/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-zod'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-zod'],
 };
