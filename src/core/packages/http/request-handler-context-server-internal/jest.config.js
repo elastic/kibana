9afd9e2f15e34755f79fccc93960fd6a9bbad50c
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 22:38:48 +0100

    Relocating module `@kbn/core-http-request-handler-context-server-internal`

--- src/core/packages/http/request-handler-context-server-internal/jest.config.js
+++ src/core/packages/http/request-handler-context-server-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/http/core-http-request-handler-context-server-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/http/request-handler-context-server-internal'],
 };
