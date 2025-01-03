0a311738e35fa9297286869bf819e11daf49e051
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 15:41:51 +0100

    Relocating module `@kbn/config-schema`

--- src/platform/packages/shared/kbn-config-schema/jest.config.js
+++ src/platform/packages/shared/kbn-config-schema/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-config-schema'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-config-schema'],
 };
