a2ac7639b708361f234b7612dac426e5baab0c73
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 21:55:25 +0100

    Relocating module `@kbn/analytics-collection-utils`

--- src/platform/packages/private/analytics/utils/analytics_collection_utils/jest.config.js
+++ src/platform/packages/private/analytics/utils/analytics_collection_utils/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/analytics/utils/analytics_collection_utils'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/analytics/utils/analytics_collection_utils'],
 };
