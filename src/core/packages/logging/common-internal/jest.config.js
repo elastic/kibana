99f4bc7703722627aa11d19028279f15cc8a792f
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-01-03 22:55:34 +0100

    Relocating module `@kbn/core-logging-common-internal`

--- src/core/packages/logging/common-internal/jest.config.js
+++ src/core/packages/logging/common-internal/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/logging/core-logging-common-internal'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/logging/common-internal'],
 };
