b1c64a143132f7fe5027d33ba67920268c9883dd
Timothy Sullivan <tsullivan@elastic.co>
2025-01-03 13:18:54 -0700

    Preparation for High Contrast Mode, ML domain

--- x-pack/platform/plugins/shared/aiops/public/components/change_point_detection/change_point_detection_root.tsx
+++ x-pack/platform/plugins/shared/aiops/public/components/change_point_detection/change_point_detection_root.tsx
@@ -64,7 +64,15 @@ export const ChangePointDetectionAppState: FC<ChangePointDetectionAppStateProps>
   showFrozenDataTierChoice = true,
 }) => {
   const datePickerDeps: DatePickerDependencies = {
-    ...pick(appContextValue, ['data', 'http', 'notifications', 'theme', 'uiSettings', 'i18n']),
+    ...pick(appContextValue, [
+      'data',
+      'http',
+      'notifications',
+      'theme',
+      'uiSettings',
+      'userProfile',
+      'i18n',
+    ]),
     uiSettingsKeys: UI_SETTINGS,
     showFrozenDataTierChoice,
   };
