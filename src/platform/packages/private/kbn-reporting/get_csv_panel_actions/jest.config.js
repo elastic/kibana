a996d52fed3fcff9595444e414b4967020e1e774
Anton Dosov <anton.dosov@elastic.co>
2025-01-06 10:33:50 +0100

    Relocating module `@kbn/reporting-csv-share-panel`

--- src/platform/packages/private/kbn-reporting/get_csv_panel_actions/jest.config.js
+++ src/platform/packages/private/kbn-reporting/get_csv_panel_actions/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../..',
-  roots: ['<rootDir>/packages/kbn-reporting/get_csv_panel_actions'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-reporting/get_csv_panel_actions'],
 };
