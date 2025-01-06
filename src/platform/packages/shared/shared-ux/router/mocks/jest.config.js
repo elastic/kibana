8fea7c5a43aa1de71acd4f396772b51dbd97fb37
Anton Dosov <anton.dosov@elastic.co>
2025-01-06 13:14:40 +0100

    Relocating module `@kbn/shared-ux-router-mocks`

--- src/platform/packages/shared/shared-ux/router/mocks/jest.config.js
+++ src/platform/packages/shared/shared-ux/router/mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/shared-ux/router/mocks'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/shared-ux/router/mocks'],
 };
