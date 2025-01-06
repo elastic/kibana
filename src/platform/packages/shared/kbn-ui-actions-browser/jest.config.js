ac0e89ef6f615ae470796aa6a9293d528c448b7c
Anton Dosov <anton.dosov@elastic.co>
2025-01-06 11:30:49 +0100

    Relocating module `@kbn/ui-actions-browser`

--- src/platform/packages/shared/kbn-ui-actions-browser/jest.config.js
+++ src/platform/packages/shared/kbn-ui-actions-browser/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-ui-actions-browser'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-ui-actions-browser'],
 };
