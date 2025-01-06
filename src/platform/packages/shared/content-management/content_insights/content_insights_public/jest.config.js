60d067e590b1283911711a3e850fed6fa26b9329
Anton Dosov <anton.dosov@elastic.co>
2025-01-06 11:11:17 +0100

    Relocating module `@kbn/content-management-content-insights-public`

--- src/platform/packages/shared/content-management/content_insights/content_insights_public/jest.config.js
+++ src/platform/packages/shared/content-management/content_insights/content_insights_public/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/content-management/content_insights/content_insights_public'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/content-management/content_insights/content_insights_public'],
 };
