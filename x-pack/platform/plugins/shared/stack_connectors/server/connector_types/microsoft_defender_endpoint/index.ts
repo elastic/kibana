af687a443fb02f51fa047f092aedf5b9c55d1bd0
Paul Tavares <paul.tavares@elastic.co>
2025-01-06 14:58:30 -0500

    adjust connector registration with new sub-feature id

--- x-pack/platform/plugins/shared/stack_connectors/server/connector_types/microsoft_defender_endpoint/index.ts
+++ x-pack/platform/plugins/shared/stack_connectors/server/connector_types/microsoft_defender_endpoint/index.ts
@@ -9,9 +9,14 @@ import {
   SubActionConnectorType,
   ValidatorType,
 } from '@kbn/actions-plugin/server/sub_action_framework/types';
-import { SecurityConnectorFeatureId } from '@kbn/actions-plugin/common';
-import { urlAllowListValidator } from '@kbn/actions-plugin/server';
+import { EndpointSecurityConnectorFeatureId } from '@kbn/actions-plugin/common';
+import { ActionExecutionSourceType, urlAllowListValidator } from '@kbn/actions-plugin/server';
+import {
+  ENDPOINT_SECURITY_EXECUTE_PRIVILEGE,
+  ENDPOINT_SECURITY_SUB_ACTIONS_EXECUTE_PRIVILEGE,
+} from '@kbn/actions-plugin/server/feature';
 import { MicrosoftDefenderEndpointConnector } from './microsoft_defender_endpoint';
+
 import {
   MicrosoftDefenderEndpointConfig,
   MicrosoftDefenderEndpointSecrets,
@@ -22,6 +27,7 @@ import {
 } from '../../../common/microsoft_defender_endpoint/schema';
 import {
   MICROSOFT_DEFENDER_ENDPOINT_CONNECTOR_ID,
+  MICROSOFT_DEFENDER_ENDPOINT_SUB_ACTION,
   MICROSOFT_DEFENDER_ENDPOINT_TITLE,
 } from '../../../common/microsoft_defender_endpoint/constants';
 
@@ -40,6 +46,17 @@ export const getMicrosoftDefenderEndpointConnectorType = (): SubActionConnectorT
     { type: ValidatorType.CONFIG, validator: urlAllowListValidator('oAuthServerUrl') },
     { type: ValidatorType.CONFIG, validator: urlAllowListValidator('apiUrl') },
   ],
-  supportedFeatureIds: [SecurityConnectorFeatureId],
+  supportedFeatureIds: [EndpointSecurityConnectorFeatureId],
   minimumLicenseRequired: 'enterprise' as const,
+  subFeature: 'endpointSecurity',
+  getKibanaPrivileges: (args) => {
+    const privileges = [ENDPOINT_SECURITY_EXECUTE_PRIVILEGE];
+    if (
+      args?.source === ActionExecutionSourceType.HTTP_REQUEST &&
+      args?.params?.subAction !== MICROSOFT_DEFENDER_ENDPOINT_SUB_ACTION.TEST_CONNECTOR
+    ) {
+      privileges.push(ENDPOINT_SECURITY_SUB_ACTIONS_EXECUTE_PRIVILEGE);
+    }
+    return privileges;
+  },
 });
