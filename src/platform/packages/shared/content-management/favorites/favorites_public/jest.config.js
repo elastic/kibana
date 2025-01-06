d6d58bb470d8471e0055e6d8eb5946dc32fd8f57
Anton Dosov <anton.dosov@elastic.co>
2025-01-06 11:15:37 +0100

    Relocating module `@kbn/content-management-favorites-public`

--- src/platform/packages/shared/content-management/favorites/favorites_public/jest.config.js
+++ src/platform/packages/shared/content-management/favorites/favorites_public/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/content-management/favorites/favorites_public'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/content-management/favorites/favorites_public'],
 };
