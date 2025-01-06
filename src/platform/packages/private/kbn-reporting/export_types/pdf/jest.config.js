7ed7b9a8118458cb8b5ef26300ceeb6615eb2e57
Anton Dosov <anton.dosov@elastic.co>
2025-01-06 10:38:18 +0100

    Relocating module `@kbn/reporting-export-types-pdf`

--- src/platform/packages/private/kbn-reporting/export_types/pdf/jest.config.js
+++ src/platform/packages/private/kbn-reporting/export_types/pdf/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/kbn-reporting/export_types/pdf'],
+  rootDir: '../../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-reporting/export_types/pdf'],
 };
