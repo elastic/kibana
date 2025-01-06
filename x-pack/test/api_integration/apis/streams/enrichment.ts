9eafd43ac5ec606bb4771f56c7a8772fca7cf62d
Kerry Gallagher <471693+Kerry350@users.noreply.github.com>
2025-01-06 14:04:23 +0000

    Fix enrichment tests

--- x-pack/test/api_integration/apis/streams/enrichment.ts
+++ x-pack/test/api_integration/apis/streams/enrichment.ts
@@ -8,7 +8,14 @@
 import expect from '@kbn/expect';
 import { SearchTotalHits } from '@elastic/elasticsearch/lib/api/types';
 import { WiredStreamConfigDefinition } from '@kbn/streams-schema';
-import { enableStreams, fetchDocument, indexDocument, putStream } from './helpers/requests';
+import {
+  deleteStream,
+  enableStreams,
+  fetchDocument,
+  forkStream,
+  indexDocument,
+  putStream,
+} from './helpers/requests';
 import { FtrProviderContext } from '../../ftr_provider_context';
 import { waitForDocumentInIndex } from '../../../alerting_api_integration/observability/helpers/alerting_wait_for_helpers';
 import { cleanUpRootStream } from './helpers/cleanup';
@@ -21,11 +28,24 @@ export default function ({ getService }: FtrProviderContext) {
 
   describe('Enrichment', () => {
     after(async () => {
+      await deleteStream(supertest, 'logs.nginx');
       await cleanUpRootStream(esClient);
     });
 
     before(async () => {
       await enableStreams(supertest);
+      const body = {
+        stream: {
+          name: 'logs.nginx',
+        },
+        condition: {
+          field: 'host.name',
+          operator: 'eq',
+          value: 'routeme',
+        },
+      };
+      // We use a forked stream as processing changes cannot be made to the root stream
+      await forkStream(supertest, 'logs', body);
     });
 
     it('Place processing steps', async () => {
@@ -78,7 +98,7 @@ export default function ({ getService }: FtrProviderContext) {
           },
         },
       };
-      const response = await putStream(supertest, 'logs', body);
+      const response = await putStream(supertest, 'logs.nginx', body);
       expect(response).to.have.property('acknowledged', true);
     });
 
@@ -86,15 +106,28 @@ export default function ({ getService }: FtrProviderContext) {
       const doc = {
         '@timestamp': '2024-01-01T00:00:10.000Z',
         message: '2023-01-01T00:00:10.000Z error test',
+        ['host.name']: 'routeme',
       };
       const response = await indexDocument(esClient, 'logs', doc);
       expect(response.result).to.eql('created');
-      await waitForDocumentInIndex({ esClient, indexName: 'logs', retryService, logger });
+      const reroutedDocResponse = await waitForDocumentInIndex({
+        esClient,
+        indexName: 'logs.nginx',
+        retryService,
+        logger,
+      });
 
-      const result = await fetchDocument(esClient, 'logs', response._id);
+      const result = await fetchDocument(
+        esClient,
+        'logs.nginx',
+        reroutedDocResponse.hits?.hits[0]?._id!
+      );
       expect(result._source).to.eql({
         '@timestamp': '2024-01-01T00:00:10.000Z',
         message: '2023-01-01T00:00:10.000Z error test',
+        host: {
+          name: 'routeme',
+        },
         inner_timestamp: '2023-01-01T00:00:10.000Z',
         message2: 'test',
         log: {
@@ -107,22 +140,30 @@ export default function ({ getService }: FtrProviderContext) {
       const doc = {
         '@timestamp': '2024-01-01T00:00:11.000Z',
         message: '2023-01-01T00:00:10.000Z info mylogger this is the message',
+        ['host.name']: 'routeme',
       };
       const response = await indexDocument(esClient, 'logs', doc);
       expect(response.result).to.eql('created');
-      await waitForDocumentInIndex({
+      const reroutedDocResponse = await waitForDocumentInIndex({
         esClient,
-        indexName: 'logs',
+        indexName: 'logs.nginx',
         retryService,
         logger,
         docCountTarget: 2,
       });
 
-      const result = await fetchDocument(esClient, 'logs', response._id);
+      const result = await fetchDocument(
+        esClient,
+        'logs.nginx',
+        reroutedDocResponse.hits?.hits[0]?._id!
+      );
       expect(result._source).to.eql({
         '@timestamp': '2024-01-01T00:00:11.000Z',
         message: '2023-01-01T00:00:10.000Z info mylogger this is the message',
         inner_timestamp: '2023-01-01T00:00:10.000Z',
+        host: {
+          name: 'routeme',
+        },
         log: {
           level: 'info',
           logger: 'mylogger',
@@ -134,7 +175,7 @@ export default function ({ getService }: FtrProviderContext) {
 
     it('Doc is searchable', async () => {
       const response = await esClient.search({
-        index: 'logs',
+        index: 'logs.nginx',
         body: {
           query: {
             match: {
@@ -148,7 +189,7 @@ export default function ({ getService }: FtrProviderContext) {
 
     it('Non-indexed field is not searchable', async () => {
       const response = await esClient.search({
-        index: 'logs',
+        index: 'logs.nginx',
         body: {
           query: {
             match: {
