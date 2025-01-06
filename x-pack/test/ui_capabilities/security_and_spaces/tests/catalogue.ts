432cc4a67313c497b70036cf0191b5351f02ba9d
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2025-01-06 20:15:19 +0100

    [Synonyms UI] Synonyms UI base plugin (#203284)
    ## Summary
    
    Creates a plugin for Synonyms UI implementation. It is hidden under the
    UI flag and config option which is off by default.
    ```
    POST kbn:/internal/kibana/settings/searchSynonyms:synonymsEnabled
    {"value": true}
    ```
    
    Serverless Search:
    <img width="379" alt="Screenshot 2024-12-17 at 13 18 02"
    src="https://github.com/user-attachments/assets/8c2cb6f0-ce2a-4be6-8605-4f994adeefd7"
    />
    
    Stack Search
    <img width="293" alt="Screenshot 2024-12-17 at 13 21 43"
    src="https://github.com/user-attachments/assets/0d61de0e-2cd3-46a6-990f-1f1a70843324"
    />
    
    
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/packages/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/ui_capabilities/security_and_spaces/tests/catalogue.ts
+++ x-pack/test/ui_capabilities/security_and_spaces/tests/catalogue.ts
@@ -89,6 +89,7 @@ export default function catalogueTests({ getService }: FtrProviderContext) {
               'enterpriseSearchElasticsearch',
               'searchPlayground',
               'searchInferenceEndpoints',
+              'searchSynonyms',
               'appSearch',
               'observabilityAIAssistant',
               'workplaceSearch',
