d4481ac19229abc0cae9ebd59b1fc7e4337dc7d0
Mykola Harmash <mykola.harmash@elastic.co>
2025-01-07 10:44:23 +0100

    Update CI pipeline to include Onboarding playwright config

--- .buildkite/scripts/pipelines/pull_request/pipeline.ts
+++ .buildkite/scripts/pipelines/pull_request/pipeline.ts
@@ -380,7 +380,7 @@ const getPipeline = (filename: string, removeSteps = true) => {
     if (
       (await doAnyChangesMatch([
         /^x-pack\/platform\/plugins\/private\/discover_enhanced\/ui_tests/,
-        /^x-pack\/solutions\/observability\/plugins\/observability_onboarding\/ui_tests/,
+        /^x-pack\/solutions\/observability\/plugins\/observability_onboarding/,
         /^packages\/kbn-scout/,
       ])) ||
       GITHUB_PR_LABELS.includes('ci:scout-ui-tests')
--- .buildkite/scripts/steps/functional/scout_ui_tests.sh
+++ .buildkite/scripts/steps/functional/scout_ui_tests.sh
@@ -6,29 +6,45 @@ source .buildkite/scripts/steps/functional/common.sh
 
 export JOB=kibana-scout-ui-tests
 
-TEST_CONFIG="x-pack/platform/plugins/private/discover_enhanced/ui_tests/playwright.config.ts"
-KIBANA_DIR="$KIBANA_BUILD_LOCATION"
-
-declare -A TESTS=(
-  ["Stateful"]="--stateful"
-  ["Serverless Elasticsearch"]="--serverless=es"
-  ["Serverless Observability"]="--serverless=oblt"
-  ["Serverless Security"]="--serverless=security"
+declare -A TEST_CONFIGS=(
+  ["Discovery Enhanced"]="x-pack/platform/plugins/private/discover_enhanced/ui_tests/playwright.config.ts"
+  ["Observability Onboarding"]="x-pack/solutions/observability/plugins/observability_onboarding/ui_tests/playwright.config.ts"
 )
-
-ORDER=("Stateful" "Serverless Elasticsearch" "Serverless Observability" "Serverless Security")
-
+RUN_MODES_ORDER=("Stateful" "Serverless Elasticsearch" "Serverless Observability" "Serverless Security")
+KIBANA_DIR="$KIBANA_BUILD_LOCATION"
 EXIT_CODE=0
 
-for TEST_NAME in "${ORDER[@]}"; do
-  RUN_MODE="${TESTS[$TEST_NAME]}"
-  echo "--- $TEST_NAME: 'discover_enhanced' plugin UI Tests"
-  if ! node scripts/scout run-tests "$RUN_MODE" --config "$TEST_CONFIG" --kibana-install-dir "$KIBANA_DIR"; then
-    echo "$TEST_NAME: failed"
-    EXIT_CODE=1
-  else
-    echo "$TEST_NAME: passed"
-  fi
+for SUITE_NAME in "${!TEST_CONFIGS[@]}"; do
+  CONFIG_PATH="${TEST_CONFIGS[$SUITE_NAME]}"
+
+  for RUN_MODE_NAME in "${RUN_MODES_ORDER[@]}"; do
+    case "$RUN_MODE_NAME" in
+      "Stateful")
+      RUN_MODE="--stateful"
+      ;;
+      "Serverless Elasticsearch")
+      RUN_MODE="--serverless=es"
+      ;;
+      "Serverless Observability")
+      RUN_MODE="--serverless=oblt"
+      ;;
+      "Serverless Security")
+      RUN_MODE="--serverless=security"
+      ;;
+      *)
+      echo "Unknown run mode: $RUN_MODE_NAME"
+      exit 1
+      ;;
+    esac
+
+    echo "--- $TEST_NAME: '$SUITE_NAME' UI Tests"
+    if ! node scripts/scout run-tests "$RUN_MODE" --config "$TEST_CONFIG" --kibana-install-dir "$KIBANA_DIR"; then
+      echo "$TEST_NAME: failed"
+      EXIT_CODE=1
+    else
+      echo "$TEST_NAME: passed"
+    fi
+  done
 done
 
 exit $EXIT_CODE
