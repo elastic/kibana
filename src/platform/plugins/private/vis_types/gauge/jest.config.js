18248b2606b7976ae0dfff06e7ed3f2f65601439
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-07 16:50:22 +0100

    Relocating module `@kbn/vis-type-gauge-plugin`

--- src/platform/plugins/private/vis_types/gauge/jest.config.js
+++ src/platform/plugins/private/vis_types/gauge/jest.config.js
@@ -9,11 +9,11 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/src/plugins/vis_types/gauge'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/vis_types/gauge',
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/private/vis_types/gauge'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/private/vis_types/gauge',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/src/plugins/vis_types/gauge/{common,public,server}/**/*.{ts,tsx}',
+    '<rootDir>/src/platform/plugins/private/vis_types/gauge/{common,public,server}/**/*.{ts,tsx}',
   ],
 };
