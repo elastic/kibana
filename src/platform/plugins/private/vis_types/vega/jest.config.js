e4e2c1132a612b84f6780f2c8dea05831e9522fd
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-07 17:01:07 +0100

    Relocating module `@kbn/vis-type-vega-plugin`

--- src/platform/plugins/private/vis_types/vega/jest.config.js
+++ src/platform/plugins/private/vis_types/vega/jest.config.js
@@ -9,9 +9,9 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/src/plugins/vis_types/vega'],
-  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/plugins/vis_types/vega',
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/plugins/private/vis_types/vega'],
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/private/vis_types/vega',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/src/plugins/vis_types/vega/{public,server}/**/*.{js,ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/src/platform/plugins/private/vis_types/vega/{public,server}/**/*.{js,ts,tsx}'],
 };
