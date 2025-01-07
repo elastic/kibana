c24f2dc1be50ada04813c1dd51458f93cfbbdf2e
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-07 16:34:10 +0100

    Relocating module `@kbn/interpreter`

--- src/platform/packages/shared/kbn-interpreter/jest.config.js
+++ src/platform/packages/shared/kbn-interpreter/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-interpreter'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-interpreter'],
 };
