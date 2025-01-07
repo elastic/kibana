8b9d374c75574c27de26c15e9046130ee3732922
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-07 16:04:36 +0100

    Relocating module `@kbn/chart-expressions-common`

--- src/platform/packages/shared/chart-expressions-common/jest.config.js
+++ src/platform/packages/shared/chart-expressions-common/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/src/plugins/chart_expressions/common'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/chart-expressions-common'],
 };
