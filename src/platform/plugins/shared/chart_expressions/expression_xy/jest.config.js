d678106f1f13385d0a4d55998c0ccb52adfa73ab
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-07 16:29:46 +0100

    Relocating module `@kbn/expression-xy-plugin`

--- src/platform/plugins/shared/chart_expressions/expression_xy/jest.config.js
+++ src/platform/plugins/shared/chart_expressions/expression_xy/jest.config.js
@@ -9,12 +9,12 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../',
-  roots: ['<rootDir>/src/plugins/chart_expressions/expression_xy'],
+  rootDir: '../../../../../../',
+  roots: ['<rootDir>/src/platform/plugins/shared/chart_expressions/expression_xy'],
   coverageDirectory:
-    '<rootDir>/target/kibana-coverage/jest/src/plugins/chart_expressions/expression_xy',
+    '<rootDir>/target/kibana-coverage/jest/src/platform/plugins/shared/chart_expressions/expression_xy',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
-    '<rootDir>/src/plugins/chart_expressions/expression_xy/{common,public,server}/**/*.{ts,tsx}',
+    '<rootDir>/src/platform/plugins/shared/chart_expressions/expression_xy/{common,public,server}/**/*.{ts,tsx}',
   ],
 };
