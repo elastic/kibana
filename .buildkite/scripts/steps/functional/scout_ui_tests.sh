aa8f3d146ba96e4ff1e7eb718e70a678618a1119
Mykola Harmash <mykola.harmash@elastic.co>
2025-01-07 16:44:35 +0100

    fixup! fixup! fixup! fixup! Update CI pipeline to include Onboarding playwright config

--- .buildkite/scripts/steps/functional/scout_ui_tests.sh
+++ .buildkite/scripts/steps/functional/scout_ui_tests.sh
@@ -6,45 +6,53 @@ source .buildkite/scripts/steps/functional/common.sh
 
 export JOB=kibana-scout-ui-tests
 
-declare -A TEST_CONFIGS=(
-  ["Discovery Enhanced"]="x-pack/platform/plugins/private/discover_enhanced/ui_tests/playwright.config.ts"
-  ["Observability Onboarding"]="x-pack/solutions/observability/plugins/observability_onboarding/ui_tests/playwright.config.ts"
-)
-RUN_MODES_ORDER=("Stateful" "Serverless Elasticsearch" "Serverless Observability" "Serverless Security")
 KIBANA_DIR="$KIBANA_BUILD_LOCATION"
-EXIT_CODE=0
 
-for SUITE_NAME in "${!TEST_CONFIGS[@]}"; do
-  CONFIG_PATH="${TEST_CONFIGS[$SUITE_NAME]}"
+run_tests() {
+  local suit_name=$1
+  local config_path=$2
+  local run_mode=$3
+  local run_mode_param=""
 
-  for RUN_MODE_NAME in "${RUN_MODES_ORDER[@]}"; do
-    case "$RUN_MODE_NAME" in
+  case "$run_mode" in
       "Stateful")
-      RUN_MODE="--stateful"
+      run_mode_param="--stateful"
       ;;
       "Serverless Elasticsearch")
-      RUN_MODE="--serverless=es"
+      run_mode_param="--serverless=es"
       ;;
       "Serverless Observability")
-      RUN_MODE="--serverless=oblt"
+      run_mode_param="--serverless=oblt"
       ;;
       "Serverless Security")
-      RUN_MODE="--serverless=security"
+      run_mode_param="--serverless=security"
       ;;
       *)
-      echo "Unknown run mode: $RUN_MODE_NAME"
+      echo "Unknown run mode: $run_mode"
       exit 1
       ;;
     esac
 
-    echo "--- $SUITE_NAME ($RUN_MODE_NAME) UI Tests"
-    if ! node scripts/scout run-tests "$RUN_MODE" --config "$CONFIG_PATH" --kibana-install-dir "$KIBANA_DIR"; then
-      echo "$SUITE_NAME: failed"
+    echo "--- $suit_name ($run_mode) UI Tests"
+    if ! node scripts/scout run-tests "$run_mode_param" --config "$config_path" --kibana-install-dir "$KIBANA_DIR"; then
+      echo "$suit_name: failed"
       EXIT_CODE=1
     else
-      echo "$SUITE_NAME: passed"
+      echo "$suit_name: passed"
     fi
-  done
+}
+
+EXIT_CODE=0
+
+# Discovery Enhanced
+for run_mode in "Stateful" "Serverless Elasticsearch" "Serverless Observability" "Serverless Security"; do
+  run_tests "Discovery Enhanced" "x-pack/platform/plugins/private/discover_enhanced/ui_tests/playwright.config.ts" "$run_mode"
 done
+
+# Observability Onboarding
+for run_mode in "Stateful" "Serverless Observability"; do
+  run_tests "Observability Onboarding" "x-pack/solutions/observability/plugins/observability_onboarding/ui_tests/playwright.config.ts" "$run_mode"
+done
+
 
 exit $EXIT_CODE
