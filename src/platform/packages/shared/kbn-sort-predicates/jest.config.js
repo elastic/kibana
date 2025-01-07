717b06e784bc0cde0464e085d2a8e8447c5d3b8c
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-07 16:41:27 +0100

    Relocating module `@kbn/sort-predicates`

--- src/platform/packages/shared/kbn-sort-predicates/jest.config.js
+++ src/platform/packages/shared/kbn-sort-predicates/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-sort-predicates'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-sort-predicates'],
 };
