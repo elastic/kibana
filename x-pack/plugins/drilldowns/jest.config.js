1e55cfc2f6dbc6350b092221281ea4dc5bad6df8
Anton Dosov <anton.dosov@elastic.co>
2025-01-08 16:52:51 +0100

    Relocating module `@kbn/url-drilldown-plugin`

--- x-pack/plugins/drilldowns/jest.config.js
+++ x-pack/plugins/drilldowns/jest.config.js
@@ -11,5 +11,5 @@ module.exports = {
   roots: ['<rootDir>/x-pack/plugins/drilldowns'],
   coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/plugins/drilldowns',
   coverageReporters: ['text', 'html'],
-  collectCoverageFrom: ['<rootDir>/x-pack/plugins/drilldowns/url_drilldown/public/**/*.{ts,tsx}'],
+  collectCoverageFrom: ['<rootDir>/x-pack/platform/plugins/private/drilldowns/url_drilldown/public/**/*.{ts,tsx}'],
 };
