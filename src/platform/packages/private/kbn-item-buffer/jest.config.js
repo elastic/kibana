6b93283fd597d851d60ab0de6b982e9402104e9c
Anton Dosov <anton.dosov@elastic.co>
2025-01-08 16:30:14 +0100

    Relocating module `@kbn/item-buffer`

--- src/platform/packages/private/kbn-item-buffer/jest.config.js
+++ src/platform/packages/private/kbn-item-buffer/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-item-buffer'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-item-buffer'],
 };
