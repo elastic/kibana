f87ad7ad0bf10a6350aededbf321333d6e54c6cb
Paul Tavares <paul.tavares@elastic.co>
2025-01-08 11:47:16 -0500

    Update agent status client to use new connector method

--- x-pack/solutions/security/plugins/security_solution/server/endpoint/services/agent/clients/microsoft_defender_endpoint/microsoft_defender_endpoint_agent_status_client.ts
+++ x-pack/solutions/security/plugins/security_solution/server/endpoint/services/agent/clients/microsoft_defender_endpoint/microsoft_defender_endpoint_agent_status_client.ts
@@ -10,7 +10,10 @@ import {
   MICROSOFT_DEFENDER_ENDPOINT_SUB_ACTION,
 } from '@kbn/stack-connectors-plugin/common/microsoft_defender_endpoint/constants';
 import { keyBy } from 'lodash';
-import type { MicrosoftDefenderEndpointMachine } from '@kbn/stack-connectors-plugin/common/microsoft_defender_endpoint/types';
+import type {
+  MicrosoftDefenderEndpointAgentListResponse,
+  MicrosoftDefenderEndpointMachine,
+} from '@kbn/stack-connectors-plugin/common/microsoft_defender_endpoint/types';
 import type { ResponseActionAgentType } from '../../../../../../common/endpoint/service/response_actions/constants';
 import { AgentStatusClientError } from '../errors';
 import { getPendingActionsSummary, NormalizedExternalConnectorClient } from '../../..';
@@ -67,49 +70,48 @@ export class MicrosoftDefenderEndpointAgentStatusClient extends AgentStatusClien
     const esClient = this.options.esClient;
     const metadataService = this.options.endpointService.getEndpointMetadataService();
 
-    const [{ value: machineList }, allPendingActions] = await Promise.all([
-      // FIXME:PT need to update connector to include a getAgentDetailsList() method
-      this.connectorActions
-        .execute({
+    try {
+      const [{ data: msMachineListResponse }, allPendingActions] = await Promise.all([
+        this.connectorActions.execute<MicrosoftDefenderEndpointAgentListResponse>({
           params: {
-            subAction: MICROSOFT_DEFENDER_ENDPOINT_SUB_ACTION.GET_AGENT_DETAILS,
-            subActionParams: { id: agentIds[0] },
+            subAction: MICROSOFT_DEFENDER_ENDPOINT_SUB_ACTION.GET_AGENT_LIST,
+            subActionParams: { id: agentIds },
           },
-        })
-        .then<{ value: MicrosoftDefenderEndpointMachine[] }>((response) => {
-          // FIXME:PT remove this once we have new method to get list of machines
-          // This attempts to mimic what the return structure will be once the new method is implemented
-          return {
-            value: [response.data],
-          };
         }),
 
-      // Fetch pending actions summary
-      getPendingActionsSummary(esClient, metadataService, this.log, agentIds),
-    ]);
+        // Fetch pending actions summary
+        getPendingActionsSummary(esClient, metadataService, this.log, agentIds),
+      ]);
 
-    const machinesById = keyBy(machineList, 'id');
-    const pendingActionsByAgentId = keyBy(allPendingActions, 'agent_id');
+      const machinesById = keyBy(msMachineListResponse?.value ?? [], 'id');
+      const pendingActionsByAgentId = keyBy(allPendingActions, 'agent_id');
 
-    const response = agentIds.reduce<AgentStatusRecords>((acc, agentId) => {
-      const thisMachine = machinesById[agentId];
-      const thisAgentPendingActions = pendingActionsByAgentId[agentId];
+      return agentIds.reduce<AgentStatusRecords>((acc, agentId) => {
+        const thisMachine = machinesById[agentId];
+        const thisAgentPendingActions = pendingActionsByAgentId[agentId];
 
-      acc[agentId] = {
-        agentId,
-        agentType: this.agentType,
-        found: !!thisMachine,
-        // Unfortunately, it does not look like MS Defender has a way to determine
-        // if a host is isolated or not via API, so we just set this to false
-        isolated: false,
-        lastSeen: thisMachine.lastSeen,
-        status: this.getAgentStatusFromMachineHealthStatus(thisMachine?.healthStatus),
-        pendingActions: thisAgentPendingActions.pending_actions ?? {},
-      };
+        acc[agentId] = {
+          agentId,
+          agentType: this.agentType,
+          found: !!thisMachine,
+          // Unfortunately, it does not look like MS Defender has a way to determine
+          // if a host is isolated or not via API, so we just set this to false
+          isolated: false,
+          lastSeen: thisMachine?.lastSeen ?? '',
+          status: this.getAgentStatusFromMachineHealthStatus(thisMachine?.healthStatus),
+          pendingActions: thisAgentPendingActions.pending_actions ?? {},
+        };
 
-      return acc;
-    }, {});
-
-    return response;
+        return acc;
+      }, {});
+    } catch (err) {
+      const error = new AgentStatusClientError(
+        `Failed to fetch Microsoft Defender for Endpoint agent status for agentIds: [${agentIds}], failed with: ${err.message}`,
+        500,
+        err
+      );
+      this.log.error(error);
+      throw error;
+    }
   }
 }
