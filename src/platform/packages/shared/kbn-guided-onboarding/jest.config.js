7b3e2c75ff5a6ba00a8b6829d1ed1382e426f11d
Anton Dosov <anton.dosov@elastic.co>
2025-01-08 16:28:46 +0100

    Relocating module `@kbn/guided-onboarding`

--- src/platform/packages/shared/kbn-guided-onboarding/jest.config.js
+++ src/platform/packages/shared/kbn-guided-onboarding/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-guided-onboarding'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-guided-onboarding'],
 };
