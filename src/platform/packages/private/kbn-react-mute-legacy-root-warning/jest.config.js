f4b4b503f7ffd3fc87e386ea17cf326b457e0d29
Anton Dosov <anton.dosov@elastic.co>
2025-01-08 16:34:39 +0100

    Relocating module `@kbn/react-mute-legacy-root-warning`

--- src/platform/packages/private/kbn-react-mute-legacy-root-warning/jest.config.js
+++ src/platform/packages/private/kbn-react-mute-legacy-root-warning/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-react-mute-legacy-root-warning'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-react-mute-legacy-root-warning'],
 };
