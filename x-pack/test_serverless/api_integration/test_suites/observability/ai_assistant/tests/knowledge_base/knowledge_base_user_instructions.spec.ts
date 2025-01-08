135f53e5650caf683b1a5872ae10e7edecb1714c
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-01-08 16:41:49 -0500

    [Obs AI Assistant] Unskip user instructions test (#205656)

--- x-pack/test_serverless/api_integration/test_suites/observability/ai_assistant/tests/knowledge_base/knowledge_base_user_instructions.spec.ts
+++ x-pack/test_serverless/api_integration/test_suites/observability/ai_assistant/tests/knowledge_base/knowledge_base_user_instructions.spec.ts
@@ -248,12 +248,9 @@ export default function ApiTest({ getService }: FtrProviderContext) {
           })
           .expect(200);
 
-        const interceptPromises = [
-          proxy.interceptConversationTitle('LLM-generated title').completeAfterIntercept(),
-          proxy
-            .interceptConversation({ name: 'conversation', response: 'I, the LLM, hear you!' })
-            .completeAfterIntercept(),
-        ];
+        const interceptPromise = proxy
+          .interceptConversation({ name: 'conversation', response: 'I, the LLM, hear you!' })
+          .completeAfterIntercept();
 
         const messages: Message[] = [
           {
@@ -298,8 +295,7 @@ export default function ApiTest({ getService }: FtrProviderContext) {
           },
         });
 
-        // wait for all interceptors to be settled
-        await Promise.all(interceptPromises);
+        await interceptPromise;
 
         const conversation = res.body;
         return conversation;
@@ -318,6 +314,8 @@ export default function ApiTest({ getService }: FtrProviderContext) {
 
       after(async () => {
         proxy.close();
+        await clearKnowledgeBase(es);
+        await clearConversations(es);
         await deleteActionConnector({
           supertest: supertestWithoutAuth,
           connectorId,
