b037cb8a305822bbca21053dd2a75bae977f5bc1
Maxim Kholod <maxim.kholod@elastic.co>
2025-01-08 18:27:47 +0100

    fix eslint

--- x-pack/solutions/security/packages/kbn-cloud-security-posture/public/src/hooks/use_get_cvs_score_color.ts
+++ x-pack/solutions/security/packages/kbn-cloud-security-posture/public/src/hooks/use_get_cvs_score_color.ts
@@ -1,13 +1,20 @@
-import {useEuiTheme} from '@elastic/eui';
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useEuiTheme } from '@elastic/eui';
 
 import { getCvsScoreColor as getCvsScoreColorUtil } from '../..';
 
 export const useGetCvsScoreColor = () => {
-    const { euiTheme } = useEuiTheme();
+  const { euiTheme } = useEuiTheme();
 
-    const getCvsScoreColor = (score: number) => {
-        return getCvsScoreColorUtil(score, euiTheme);
-    }
+  const getCvsScoreColor = (score: number) => {
+    return getCvsScoreColorUtil(score, euiTheme);
+  };
 
-    return { getCvsScoreColor };
-}
+  return { getCvsScoreColor };
+};
