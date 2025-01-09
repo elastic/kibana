fb57f55ea129dae33bb1bb000cff8d0eedb1056d
Agustina Nahir Ruidiaz <agustina.ruidiaz@elastic.co>
2025-01-09 12:49:12 +0100

    fix build

--- x-pack/solutions/security/plugins/security_solution/public/onboarding/components/onboarding_body/hooks/use_delayed_visibility.test.ts
+++ x-pack/solutions/security/plugins/security_solution/public/onboarding/components/onboarding_body/hooks/use_delayed_visibility.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook, act } from '@testing-library/react';
 import { useDelayedVisibility } from './use_delayed_visibility';
 import { HEIGHT_ANIMATION_DURATION } from '../onboarding_card_panel.styles';
 
