d8571a850eb576742dc033e2677fa8c082bf68b8
Kevin Delemme <kevin.delemme@elastic.co>
2025-01-09 14:31:35 -0500

    Add new telemetry metrics for SLO

--- x-pack/solutions/observability/plugins/slo/server/lib/collectors/fetcher.test.ts
+++ x-pack/solutions/observability/plugins/slo/server/lib/collectors/fetcher.test.ts
@@ -5,17 +5,21 @@
  * 2.0.
  */
 
-import { savedObjectsRepositoryMock, ElasticsearchClientMock } from '@kbn/core/server/mocks';
+import {
+  savedObjectsRepositoryMock,
+  ElasticsearchClientMock,
+  elasticsearchServiceMock,
+} from '@kbn/core/server/mocks';
 import { CollectorFetchContext } from '@kbn/usage-collection-plugin/server';
 import { fetcher } from './fetcher';
 
 let savedObjectClient: ReturnType<typeof savedObjectsRepositoryMock.create>;
-
 let closeMock: jest.Mock;
-let esClient: ElasticsearchClientMock;
+let esClientMock: ElasticsearchClientMock;
 
 describe('SLO usage collector fetcher', () => {
   beforeEach(() => {
+    esClientMock = elasticsearchServiceMock.createElasticsearchClient();
     savedObjectClient = savedObjectsRepositoryMock.create();
     closeMock = jest.fn();
   });
@@ -31,12 +35,45 @@ describe('SLO usage collector fetcher', () => {
       close: closeMock,
     });
 
+    esClientMock.count.mockResolvedValue({
+      count: 13,
+      _shards: {
+        total: 2,
+        successful: 2,
+        skipped: 0,
+        failed: 0,
+      },
+    });
+
     const results = await fetcher({
       soClient: savedObjectClient,
-      esClient,
+      esClient: esClientMock,
     } as CollectorFetchContext);
 
     expect(closeMock).toHaveBeenCalled();
-    expect(results.slo.total).toEqual(0);
+    expect(results.slo).toMatchInlineSnapshot(`
+      Object {
+        "by_budgeting_method": Object {
+          "occurrences": 0,
+          "timeslices": 0,
+        },
+        "by_calendar_aligned_duration": Object {},
+        "by_rolling_duration": Object {},
+        "by_sli_type": Object {},
+        "by_status": Object {
+          "disabled": 0,
+          "enabled": 0,
+        },
+        "definitions": Object {
+          "total": 0,
+          "total_with_ccs": 0,
+          "total_with_groups": 0,
+        },
+        "instances": Object {
+          "total": 13,
+        },
+        "total": 0,
+      }
+    `);
   });
 });
