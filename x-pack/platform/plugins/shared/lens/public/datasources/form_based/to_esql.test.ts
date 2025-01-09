1102f5edc80b3a4da7346aa023f51cc06dff890b
ppisljar <peter.pisljar@elastic.co>
2025-01-09 08:22:33 +0100

    more tests

--- x-pack/platform/plugins/shared/lens/public/datasources/form_based/to_esql.test.ts
+++ x-pack/platform/plugins/shared/lens/public/datasources/form_based/to_esql.test.ts
@@ -120,4 +120,161 @@ describe('to_esql', () => {
 
     expect(esql?.esql).toEqual(undefined);
   });
+
+  it('should return undefined if lens formula is used', () => {
+    const esql = getESQLForLayer(
+      [
+        [
+          '1',
+          {
+            operationType: 'formula',
+            label: 'Formula',
+            isBucketed: false,
+            params: {},
+            dataType: 'number',
+          },
+        ],
+      ],
+      layer,
+      indexPattern,
+      uiSettings,
+      {
+        fromDate: '2021-01-01T00:00:00.000Z',
+        toDate: '2021-01-01T23:59:59.999Z',
+      },
+      new Date()
+    );
+
+    expect(esql).toEqual(undefined);
+  });
+
+  test('it should add a where condition to esql if timeField is set', () => {
+    const esql = getESQLForLayer(
+      [
+        [
+          '1',
+          {
+            operationType: 'date_histogram',
+            sourceField: 'order_date',
+            label: 'Date histogram',
+            dataType: 'date',
+            isBucketed: true,
+            interval: 'auto',
+          },
+        ],
+        [
+          '2',
+          {
+            operationType: 'count',
+            sourceField: 'records',
+            label: 'Count',
+            dataType: 'number',
+            isBucketed: false,
+          },
+        ],
+      ],
+      layer,
+      indexPattern,
+      uiSettings,
+      {
+        fromDate: '2021-01-01T00:00:00.000Z',
+        toDate: '2021-01-01T23:59:59.999Z',
+      },
+      new Date()
+    );
+
+    expect(esql?.esql).toEqual(
+      'FROM myIndexPattern | WHERE order_date >= ?_tstart AND order_date <= ?_tend | STATS bucket_0_0 = COUNT(*) BY order_date = BUCKET(`order_date`, 30 minutes) | SORT order_date ASC'
+    );
+  });
+
+  it('should not add a where condition to esql if timeField is not set', () => {
+    const esql = getESQLForLayer(
+      [
+        [
+          '1',
+          {
+            operationType: 'date_histogram',
+            sourceField: 'order_date',
+            label: 'Date histogram',
+            dataType: 'date',
+            isBucketed: true,
+            interval: 'auto',
+          },
+        ],
+        [
+          '2',
+          {
+            operationType: 'count',
+            sourceField: 'records',
+            label: 'Count',
+            dataType: 'number',
+            isBucketed: false,
+          },
+        ],
+      ],
+      layer,
+      {
+        title: 'myIndexPattern',
+        getFieldByName: (field: string) => {
+          if (field === 'records') return undefined;
+          return { name: field };
+        },
+        getFormatterForField: () => ({ convert: (v: unknown) => v }),
+      } as unknown as IndexPattern,
+      uiSettings,
+      {
+        fromDate: '2021-01-01T00:00:00.000Z',
+        toDate: '2021-01-01T23:59:59.999Z',
+      },
+      new Date()
+    );
+
+    expect(esql?.esql).toEqual(
+      'FROM myIndexPattern | STATS bucket_0_0 = COUNT(*) BY order_date = BUCKET(`order_date`, 30 minutes) | SORT order_date ASC'
+    );
+  });
+
+  it('should return undefined if timezone is not UTC', () => {
+    uiSettings.get.mockImplementation((key: string) => {
+      if (key === 'dateFormat:tz') return 'America/Chicago';
+      return uiSettings.get(key);
+    });
+
+    const esql = getESQLForLayer(
+      [
+        [
+          '1',
+          {
+            operationType: 'date_histogram',
+            sourceField: 'order_date',
+            label: 'Date histogram',
+            dataType: 'date',
+            isBucketed: true,
+            interval: 'auto',
+          },
+        ],
+        [
+          '2',
+          {
+            operationType: 'count',
+            sourceField: 'records',
+            label: 'Count',
+            dataType: 'number',
+            isBucketed: false,
+          },
+        ],
+      ],
+      layer,
+      indexPattern,
+      uiSettings,
+      {
+        fromDate: '2021-01-01T00:00:00.000Z',
+        toDate: '2021-01-01T23:59:59.999Z',
+      },
+      new Date()
+    );
+
+    expect(esql).toEqual(undefined);
+  });
 });
