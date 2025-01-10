24f3cdd5d90251f2e2e37115a6964b8fc4f1a362
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-10 13:55:58 +0100

    Relocating module `@kbn/palettes`

--- src/platform/packages/shared/kbn-palettes/jest.config.js
+++ src/platform/packages/shared/kbn-palettes/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-palettes'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-palettes'],
 };
