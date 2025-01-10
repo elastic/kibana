64f344964c7c52ade2610d6215052ad943e9d258
Dario Gieselaar <dario.gieselaar@elastic.co>
2025-01-10 13:18:21 +0100

    Manually convert backing indices to data streams

--- x-pack/plugins/data_definition_registry/server/data_definition_registry/get_data_streams_for_query.ts
+++ x-pack/plugins/data_definition_registry/server/data_definition_registry/get_data_streams_for_query.ts
@@ -5,10 +5,11 @@
  * 2.0.
  */
 
-import { compact, uniq } from 'lodash';
+import { castArray, uniq } from 'lodash';
 import { ElasticsearchClient } from '@kbn/core/server';
 import { QueryDslQueryContainer } from '@elastic/elasticsearch/lib/api/types';
 import { errors } from '@elastic/elasticsearch';
+import pLimit from 'p-limit';
 
 export async function getDataStreamsForQuery({
   esClient,
@@ -19,63 +20,94 @@ export async function getDataStreamsForQuery({
   index: string | string[];
   query: QueryDslQueryContainer;
 }) {
-  const response = await esClient
-    .search({
-      track_total_hits: false,
-      index,
-      size: 0,
-      terminate_after: 1,
-      timeout: '1ms',
-      allow_no_indices: true,
-      aggs: {
-        indices: {
-          terms: {
-            field: '_index',
-            size: 10000,
-          },
-        },
-      },
-      query: {
-        bool: {
-          filter: [
-            query,
-            {
-              bool: {
-                must_not: {
-                  terms: {
-                    _tier: ['data_frozen'],
-                  },
+  const chunks: string[][] = [];
+
+  let currentChunk: string[] = [];
+  let currentChunkLength = 0;
+
+  chunks.push(currentChunk);
+
+  const MAX_CHUNK_LENGTH = 2048;
+
+  for (const currentIndex of castArray(index)) {
+    const nextLength = currentChunkLength + currentIndex.length + 1;
+    if (nextLength <= MAX_CHUNK_LENGTH) {
+      currentChunk.push(currentIndex);
+      currentChunkLength = nextLength;
+    } else {
+      currentChunk = [currentIndex];
+      chunks.push(currentChunk);
+      currentChunkLength = currentIndex.length;
+    }
+  }
+
+  const limiter = pLimit(5);
+
+  const allDataStreams = await Promise.all(
+    chunks.map(async (chunk) => {
+      return limiter(async () => {
+        const response = await esClient
+          .search({
+            track_total_hits: false,
+            index: chunk,
+            size: 0,
+            terminate_after: 1,
+            timeout: '1ms',
+            allow_no_indices: true,
+            aggs: {
+              indices: {
+                terms: {
+                  field: '_index',
+                  size: 10000,
                 },
               },
             },
-          ],
-        },
-      },
+            query: {
+              bool: {
+                filter: [
+                  query,
+                  {
+                    bool: {
+                      must_not: {
+                        terms: {
+                          _tier: ['data_frozen'],
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          })
+          .catch((error) => {
+            if (error instanceof errors.ResponseError && error.statusCode === 404) {
+              return undefined;
+            }
+            throw error;
+          });
+
+        if (!response?.aggregations) {
+          return [];
+        }
+
+        const indices = response.aggregations.indices as { buckets: Array<{ key: string }> };
+
+        const allIndices = indices.buckets.map((bucket) => bucket.key) ?? [];
+
+        if (!allIndices.length) {
+          return [];
+        }
+
+        const allResolvedIndicesOrDataStreams = allIndices.map((currentIndex) => {
+          return currentIndex.replace(/((?:.*):)?\.ds-(.*)-\d{4}\.\d{2}\.\d{2}-\d{6}/, '$1$2');
+        });
+
+        const dataStreams = uniq(allResolvedIndicesOrDataStreams);
+
+        return dataStreams;
+      });
     })
-    .catch((error) => {
-      if (error instanceof errors.ResponseError && error.statusCode === 404) {
-        return undefined;
-      }
-      throw error;
-    });
+  );
 
-  if (!response?.aggregations) {
-    return [];
-  }
-
-  const indices = response.aggregations.indices as { buckets: Array<{ key: string }> };
-
-  const allIndices = indices.buckets.map((bucket) => bucket.key) ?? [];
-
-  if (!allIndices.length) {
-    return [];
-  }
-
-  const resolveIndexResponse = await esClient.indices.resolveIndex({
-    name: allIndices,
-  });
-
-  const dataStreams = uniq(compact(resolveIndexResponse.indices.flatMap((idx) => idx.data_stream)));
-
-  return dataStreams;
+  return allDataStreams.flat();
 }
