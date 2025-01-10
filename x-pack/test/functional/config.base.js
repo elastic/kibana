29965f294fba866e65572f1e55505a53c054a8ad
Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>
2025-01-10 14:24:09 +0100

    Logsdb snapshot functional tests

--- x-pack/test/functional/config.base.js
+++ x-pack/test/functional/config.base.js
@@ -625,6 +625,13 @@ export default async function ({ readConfigFile }) {
               'manage_slm',
               'cluster:admin/snapshot',
               'cluster:admin/repository',
+              'manage_index_templates',
+            ],
+            indices: [
+              {
+                names: ['*'],
+                privileges: ['all'],
+              },
             ],
           },
           kibana: [
