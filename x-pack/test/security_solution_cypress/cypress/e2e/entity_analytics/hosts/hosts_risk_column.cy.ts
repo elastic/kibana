6ea6296d34a6ccf1c87ee310530f41289d59be10
Mark Hopkin <mark.hopkin@elastic.co>
2025-01-10 08:57:50 +0000

    remove extra describe block

--- x-pack/test/security_solution_cypress/cypress/e2e/entity_analytics/hosts/hosts_risk_column.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/entity_analytics/hosts/hosts_risk_column.cy.ts
@@ -12,27 +12,22 @@ import { TABLE_CELL } from '../../../screens/alerts_details';
 import { kqlSearch } from '../../../tasks/security_header';
 import { mockRiskEngineEnabled } from '../../../tasks/entity_analytics';
 
-describe('All hosts table', { tags: ['@ess'] }, () => {
-  describe('with risk score', { tags: ['@serverless'] }, () => {
-    before(() => {
-      cy.task('esArchiverLoad', { archiveName: 'risk_scores_new' });
-    });
+describe('All hosts table', { tags: ['@ess', '@serverless'] }, () => {
+  before(() => {
+    cy.task('esArchiverLoad', { archiveName: 'risk_scores_new' });
+    login();
+    mockRiskEngineEnabled();
+  });
 
-    beforeEach(() => {
-      login();
-      mockRiskEngineEnabled();
-    });
+  after(() => {
+    cy.task('esArchiverUnload', { archiveName: 'risk_scores_new' });
+  });
 
-    after(() => {
-      cy.task('esArchiverUnload', { archiveName: 'risk_scores_new' });
-    });
+  it('it renders risk column', () => {
+    visitWithTimeRange(hostsUrl('allHosts'));
+    kqlSearch('host.name: "siem-kibana" {enter}');
 
-    it('it renders risk column', () => {
-      visitWithTimeRange(hostsUrl('allHosts'));
-      kqlSearch('host.name: "siem-kibana" {enter}');
-
-      cy.get('[data-test-subj="tableHeaderCell_node.risk_4"]').should('exist');
-      cy.get(TABLE_CELL).eq(4).should('have.text', 'Critical');
-    });
+    cy.get('[data-test-subj="tableHeaderCell_node.risk_4"]').should('exist');
+    cy.get(TABLE_CELL).eq(4).should('have.text', 'Critical');
   });
 });
