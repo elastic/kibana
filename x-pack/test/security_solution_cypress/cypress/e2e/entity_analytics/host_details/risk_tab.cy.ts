dd58dd96d9d6f1728495f210150321de92382230
Mark Hopkin <mark.hopkin@elastic.co>
2025-01-10 10:40:55 +0000

    remove describe block

--- x-pack/test/security_solution_cypress/cypress/e2e/entity_analytics/host_details/risk_tab.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/entity_analytics/host_details/risk_tab.cy.ts
@@ -14,39 +14,37 @@ import { RISK_INFORMATION_FLYOUT_HEADER } from '../../../screens/entity_analytic
 import { navigateToHostRiskDetailTab } from '../../../tasks/host_risk';
 import { deleteAlertsAndRules } from '../../../tasks/api_calls/common';
 
-describe('risk tab', { tags: ['@ess'] }, () => {
-  describe('with risk score', { tags: ['@serverless'] }, () => {
-    before(() => {
-      cy.task('esArchiverLoad', { archiveName: 'risk_scores_new_complete_data' });
-      cy.task('esArchiverLoad', { archiveName: 'query_alert', useCreate: true, docsOnly: true });
-    });
+describe('risk tab', { tags: ['@ess', '@serverless'] }, () => {
+  before(() => {
+    cy.task('esArchiverLoad', { archiveName: 'risk_scores_new_complete_data' });
+    cy.task('esArchiverLoad', { archiveName: 'query_alert', useCreate: true, docsOnly: true });
+  });
 
-    beforeEach(() => {
-      mockRiskEngineEnabled();
-      login();
-    });
+  beforeEach(() => {
+    mockRiskEngineEnabled();
+    login();
+  });
 
-    after(() => {
-      cy.task('esArchiverUnload', { archiveName: 'risk_scores_new_complete_data' });
-      deleteAlertsAndRules(); // esArchiverUnload doesn't work properly when using with `useCreate` and `docsOnly` flags
-      deleteRiskEngineConfiguration();
-    });
+  after(() => {
+    cy.task('esArchiverUnload', { archiveName: 'risk_scores_new_complete_data' });
+    deleteAlertsAndRules(); // esArchiverUnload doesn't work properly when using with `useCreate` and `docsOnly` flags
+    deleteRiskEngineConfiguration();
+  });
 
-    it('renders risk tab', () => {
-      visitHostDetailsPage('Host-fwarau82er');
-      navigateToHostRiskDetailTab();
+  it('renders risk tab', () => {
+    visitHostDetailsPage('Host-fwarau82er');
+    navigateToHostRiskDetailTab();
 
-      cy.get(ALERTS_COUNT).should('have.text', '1 alert');
-      cy.get(ALERT_GRID_CELL).contains('Endpoint Security');
-    });
+    cy.get(ALERTS_COUNT).should('have.text', '1 alert');
+    cy.get(ALERT_GRID_CELL).contains('Endpoint Security');
+  });
 
-    it('shows risk information overlay when button is clicked', () => {
-      visitHostDetailsPage('siem-kibana');
-      navigateToHostRiskDetailTab();
+  it('shows risk information overlay when button is clicked', () => {
+    visitHostDetailsPage('siem-kibana');
+    navigateToHostRiskDetailTab();
 
-      openRiskInformationFlyout();
+    openRiskInformationFlyout();
 
-      cy.get(RISK_INFORMATION_FLYOUT_HEADER).contains('Entity Risk Analytics');
-    });
+    cy.get(RISK_INFORMATION_FLYOUT_HEADER).contains('Entity Risk Analytics');
   });
 });
