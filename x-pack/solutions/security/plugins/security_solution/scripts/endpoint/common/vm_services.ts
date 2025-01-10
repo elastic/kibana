d20dc00821eb044cf70f3dad7c26e5a215a42a87
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2025-01-10 13:44:36 +0100

    test

--- x-pack/solutions/security/plugins/security_solution/scripts/endpoint/common/vm_services.ts
+++ x-pack/solutions/security/plugins/security_solution/scripts/endpoint/common/vm_services.ts
@@ -273,7 +273,7 @@ const createVagrantVm = async ({
 
   try {
     const vagrantUpResponse = (
-      await execa.command(`vagrant init hashicorp/precise32 && vagrant up`, {
+      await execa.command(`vagrant up`, {
         env: {
           VAGRANT_DISABLE_VBOXSYMLINKCREATE: '1',
           VAGRANT_CWD,
@@ -359,10 +359,7 @@ export const createVagrantHostVmClient = (
   };
 
   const start = async () => {
-    const response = await execa.command(
-      `vagrant init hashicorp/precise32 && vagrant up`,
-      execaOptions
-    );
+    const response = await execa.command(`vagrant up`, execaOptions);
     log.verbose('vagrant up response:\n', response);
   };
 
