19b4550ae4b7e949ccaa86217f121d3d6ec7c46d
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-01-10 11:26:10 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/test/functional/page_objects/dataset_quality.ts
+++ x-pack/test/functional/page_objects/dataset_quality.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 import querystring from 'querystring';
 import rison from '@kbn/rison';
 import { WebElementWrapper } from '@kbn/ftr-common-functional-ui-services';
