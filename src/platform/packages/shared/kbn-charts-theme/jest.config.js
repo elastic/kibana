45afaec79da88f883b6a7cf9d07a2eb527c1bc42
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-10 13:54:25 +0100

    Relocating module `@kbn/charts-theme`

--- src/platform/packages/shared/kbn-charts-theme/jest.config.js
+++ src/platform/packages/shared/kbn-charts-theme/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-charts-theme'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-charts-theme'],
 };
