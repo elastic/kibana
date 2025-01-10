b4937f306313a0793a14f60fa3fd106231768d97
Rodney Norris <rodney.norris@elastic.co>
2025-01-10 19:00:46 +0000

    test(search): streamline delete connector test case

--- x-pack/test_serverless/functional/test_suites/search/connectors/connectors_overview.ts
+++ x-pack/test_serverless/functional/test_suites/search/connectors/connectors_overview.ts
@@ -97,26 +97,27 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       before(async () => {
         await pageObjects.common.navigateToApp('serverlessConnectors');
       });
-      it('delete connector button exist in table', async () => {
+      it('can delete a connector', async () => {
+        // Ensure the page is rendered and we have items
+        await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.expectConnectorTableToExist();
+        await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.expectSearchBarToExist();
+        await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.expectConnectorTableToHaveItems();
+        await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.setSearchBarValue(
+          TEST_CONNECTOR_NAME
+        );
+        await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.connectorNameExists(
+          TEST_CONNECTOR_NAME
+        );
         await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.expectDeleteConnectorButtonExist();
-      });
-      it('open delete connector modal', async () => {
+
         await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.openDeleteConnectorModal();
-      });
-      it('delete connector button open modal', async () => {
         await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.confirmDeleteConnectorModalComponentsExists();
-      });
-      it('delete connector field is disabled if field name does not match connector name', async () => {
         await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.deleteConnectorIncorrectName(
           'invalid'
         );
-      });
-      it('delete connector button deletes connector', async () => {
         await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.deleteConnectorWithCorrectName(
           TEST_CONNECTOR_NAME
         );
-      });
-      it('confirm connector table is disappeared after delete ', async () => {
         await pageObjects.svlSearchConnectorsPage.connectorOverviewPage.confirmConnectorTableIsDisappearedAfterDelete();
       });
     });
