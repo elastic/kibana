d0b7a82971dd5168468cbe0b5c43753fadd7dccc
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-01-13 09:59:01 -0500

    Update jest_parallel.sh

--- .buildkite/scripts/steps/test/jest_parallel.sh
+++ .buildkite/scripts/steps/test/jest_parallel.sh
@@ -66,7 +66,7 @@ while read -r config; do
     cmd=$cmd" --enable-fips --openssl-config=$HOME/nodejs.cnf"
   fi
 
-  cmd=$cmd"\" node --expose-gc ./scripts/jest  --config=\"$config\" $parallelism --logHeapUsage --coverage=false --passWithNoTests"
+  cmd=$cmd"\" node --expose-gc ./scripts/jest  --config=\"$config\" $parallelism --coverage=false --passWithNoTests"
 
   echo "actual full command is:"
   echo "$cmd"
