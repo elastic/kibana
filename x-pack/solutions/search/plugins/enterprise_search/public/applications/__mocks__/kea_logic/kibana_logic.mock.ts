7579eed70b11a9460cb88db8ef5ebc6d4a994a46
José Luis González <joseluisgj@gmail.com>
2025-01-13 09:55:35 +0100

    Manual fleet mocks

--- x-pack/solutions/search/plugins/enterprise_search/public/applications/__mocks__/kea_logic/kibana_logic.mock.ts
+++ x-pack/solutions/search/plugins/enterprise_search/public/applications/__mocks__/kea_logic/kibana_logic.mock.ts
@@ -11,10 +11,7 @@ import { chartPluginMock } from '@kbn/charts-plugin/public/mocks';
 import { cloudMock } from '@kbn/cloud-plugin/public/mocks';
 import { ApplicationStart, Capabilities } from '@kbn/core/public';
 import { uiSettingsServiceMock } from '@kbn/core-ui-settings-browser-mocks';
-
 import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
-
-import { createIntegrationsTestRendererMock } from '@kbn/fleet-plugin/public/mock';
 import { LensPublicStart } from '@kbn/lens-plugin/public';
 import { mlPluginMock } from '@kbn/ml-plugin/public/mocks';
 import { securityMock } from '@kbn/security-plugin/public/mocks';
@@ -40,7 +37,11 @@ export const mockKibanaValues = {
   consolePlugin: null,
   data: dataPluginMock.createStartContract(),
   esConfig: { elasticsearch_host: 'https://your_deployment_url' },
-  fleet: createIntegrationsTestRendererMock(),
+  fleet: {
+    config: {
+      agentless: true,
+    },
+  },
   getChromeStyle$: jest.fn().mockReturnValue(of('classic')),
   getNavLinks: jest.fn().mockReturnValue([]),
   guidedOnboarding: {},
