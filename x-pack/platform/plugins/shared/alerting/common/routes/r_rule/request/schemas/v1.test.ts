4290ec1a42fda3e8997e451ffbf3cae9afcc8468
Christos Nasikas <christos.nasikas@elastic.co>
2025-01-13 11:12:51 +0100

    Add more unit tests for schema validation

--- x-pack/platform/plugins/shared/alerting/common/routes/r_rule/request/schemas/v1.test.ts
+++ x-pack/platform/plugins/shared/alerting/common/routes/r_rule/request/schemas/v1.test.ts
@@ -15,10 +15,69 @@ describe('rRuleRequestSchema', () => {
     byweekday: ['MO'],
     bymonthday: [1],
     bymonth: [2],
+    interval: 2,
+    until: '2021-02-01T00:00:00Z',
+    count: 2,
   };
+
+  beforeEach(() => {
+    jest.useFakeTimers().setSystemTime(new Date('2021-01-01T00:00:00Z'));
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
 
   test('no errors on proper request', () => {
     expect(rRuleRequestSchema.validate(basicRequest)).toEqual(basicRequest);
+  });
+
+  describe('tzid', () => {
+    test('returns an error with invalid values', () => {
+      expect(() => rRuleRequestSchema.validate({ ...basicRequest, tzid: 'invalid' })).toThrow();
+    });
+  });
+
+  describe('interval', () => {
+    test('returns an error with invalid values', () => {
+      expect(() => rRuleRequestSchema.validate({ ...basicRequest, interval: 'invalid' })).toThrow();
+    });
+
+    test('returns an error when it is a number but not an integer', () => {
+      expect(() => rRuleRequestSchema.validate({ ...basicRequest, interval: 1.5 })).toThrow();
+    });
+
+    test('returns an error when is set to less than one', () => {
+      expect(() => rRuleRequestSchema.validate({ ...basicRequest, interval: 0 })).toThrow();
+    });
+  });
+
+  describe('until', () => {
+    test('returns an error with invalid values', () => {
+      expect(() => rRuleRequestSchema.validate({ ...basicRequest, until: 'invalid' })).toThrow();
+    });
+
+    test('returns an error if the date is in the past', () => {
+      expect(() =>
+        // one year ago
+        rRuleRequestSchema.validate({ ...basicRequest, until: '2020-01-01T00:00:00Z' })
+      ).toThrow();
+    });
+  });
+
+  describe('count', () => {
+    test('returns an error with invalid values', () => {
+      expect(() => rRuleRequestSchema.validate({ ...basicRequest, count: 'invalid' })).toThrow();
+    });
+
+    test('returns an error when it is a number but not an integer', () => {
+      expect(() => rRuleRequestSchema.validate({ ...basicRequest, count: 1.5 })).toThrow();
+    });
+
+    test('returns an error when is set to less than one', () => {
+      expect(() => rRuleRequestSchema.validate({ ...basicRequest, count: 0 })).toThrow();
+    });
   });
 
   describe('byweekday', () => {
