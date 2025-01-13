2686548fd41e8a9da08e4836ea4d60a26d2609c0
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-13 16:29:21 +0100

    Relocating module `@kbn/saved-search-component`

--- src/platform/packages/shared/kbn-saved-search-component/jest.config.js
+++ src/platform/packages/shared/kbn-saved-search-component/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-saved-search-component'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-saved-search-component'],
 };
