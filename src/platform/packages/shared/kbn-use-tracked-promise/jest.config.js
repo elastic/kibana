4b0f73eae2b64ad16448314d3bb9df7174ee99e1
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-13 16:30:48 +0100

    Relocating module `@kbn/use-tracked-promise`

--- src/platform/packages/shared/kbn-use-tracked-promise/jest.config.js
+++ src/platform/packages/shared/kbn-use-tracked-promise/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-use-tracked-promise'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-use-tracked-promise'],
 };
