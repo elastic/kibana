503eb47990ea4c9aecb4d3e7f59b4aa11aff5c6f
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2025-01-13 10:05:16 +0100

    [product doc] adapt for new format of semantic_text field (#206051)
    ## Summary
    
    fix https://github.com/elastic/kibana/issues/205908
    
    Adapt the product documentation's usages of `semantic_text` for the
    breaking changes that will be introduced in 8.18 and 9.0.
    
    This PR introduces a new format version (`2.0.0`) for the product
    documentation, introducing the required changes for the incoming
    `semantic_text` breaking change.
    - include the `_inference_fields` meta field when bundling the doc
    artifacts
    - set the `index.mapping.semantic_text.use_legacy_format` index setting
    to `false` to force the new format
    - change the way we're internally overriding the `inference_id` when
    ingesting the data
    - adapt the `search` logic to retrieve the data at the right place  
    
    
    Doing that with a new format version also makes the transition
    invisible, as our system will simply adapt depending on the version of
    the artifact's manifest.
    
    
    ### How to test
    
    **1. test that the behavior is not broken for current artifacts**
    
    Run the branch, install the product doc from the prod repository, make
    sure that the 8.17 artifacts are installed, then check if the feature
    still works using the o11y assistant.
    
    **2. test that the behavior works with the new artifacts**
    
    **Keeping your ES instance up**, configure your local Kibana to use the
    dev repository (where the 8.18 artifacts with the new format are
    present)
    
    ```yaml
    xpack.productDocBase.artifactRepositoryUrl: "https://storage.googleapis.com/kibana-ai-assistant-kb-artifacts-dev"
    ```
    
    Then restart Kibana, confirms the artifacts gets updated to 8.18
    automatically, and then test that the feature still works as expected
    using the o11y assistant.

--- x-pack/platform/plugins/shared/ai_infra/product_doc_base/server/services/search/utils/map_result.test.ts
+++ x-pack/platform/plugins/shared/ai_infra/product_doc_base/server/services/search/utils/map_result.test.ts
@@ -22,6 +22,31 @@ describe('mapResult', () => {
   it('returns the expected shape', () => {
     const input = createHit({
       content_title: 'content_title',
+      content_body: 'content_body',
+      product_name: 'kibana',
+      root_type: 'documentation',
+      slug: 'foo.html',
+      url: 'http://lost.com/foo.html',
+      version: '8.16',
+      ai_subtitle: 'ai_subtitle',
+      ai_summary: 'ai_summary',
+      ai_questions_answered: ['question A'],
+      ai_tags: ['foo', 'bar', 'test'],
+    });
+
+    const output = mapResult(input);
+
+    expect(output).toEqual({
+      content: 'content_body',
+      productName: 'kibana',
+      title: 'content_title',
+      url: 'http://lost.com/foo.html',
+    });
+  });
+
+  it('returns the expected shape for legacy semantic_text fields', () => {
+    const input = createHit({
+      content_title: 'content_title',
       content_body: { text: 'content_body' },
       product_name: 'kibana',
       root_type: 'documentation',
