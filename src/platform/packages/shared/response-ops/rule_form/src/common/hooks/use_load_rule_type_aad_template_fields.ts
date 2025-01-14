3e9ebe4d870fc29acea4e1ffa90e73f56f971bf2
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-01-14 16:48:34 -0600

    Fix bundle size

--- src/platform/packages/shared/response-ops/rule_form/src/common/hooks/use_load_rule_type_aad_template_fields.ts
+++ src/platform/packages/shared/response-ops/rule_form/src/common/hooks/use_load_rule_type_aad_template_fields.ts
@@ -7,7 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { EcsFlat } from '@elastic/ecs';
+import { useRef } from 'react';
+import { isEmpty } from 'lodash';
 import { ActionVariable } from '@kbn/alerting-types';
 import type { HttpStart } from '@kbn/core-http-browser';
 import { useQuery } from '@tanstack/react-query';
@@ -24,12 +25,24 @@ export interface UseLoadRuleTypeAadTemplateFieldProps {
 }
 
 export const useLoadRuleTypeAadTemplateField = (props: UseLoadRuleTypeAadTemplateFieldProps) => {
+  const ecsFlat = useRef<Record<string, any>>({});
   const { http, ruleTypeId, enabled, cacheTime } = props;
 
   const queryFn = async () => {
     if (!ruleTypeId) {
       return;
     }
+    if (isEmpty(ecsFlat.current)) {
+      // If we use import { EcsFlat } from '@elastic/ecs' then it will sometimes balloon the bundle size
+      // by about 1MB, or not, depending on how Webpack is feeling today. If you delete this dynamic import,
+      // it's possible that it won't make the bundle size explode, and then you will commit that change, and
+      // we will all continue to live our lives safe and secure, falsely believing that the danger has passed,
+      // until one day somebody makes another change to this package and, for mysterious, unknowable reasons,
+      // Webpack decides that today the bundle size shall be engorged once again.
+      // To avoid all that maybe just don't delete this dynamic import.
+      ecsFlat.current = await import('@elastic/ecs').then((ecs) => ecs.EcsFlat);
+    }
+
     return fetchRuleTypeAadTemplateFields({ http, ruleTypeId });
   };
 
@@ -44,7 +57,7 @@ export const useLoadRuleTypeAadTemplateField = (props: UseLoadRuleTypeAadTemplat
     select: (dataViewFields) => {
       return dataViewFields?.map<ActionVariable>((d) => ({
         name: d.name,
-        description: getDescription(d.name, EcsFlat),
+        description: getDescription(d.name, ecsFlat.current),
       }));
     },
     cacheTime,
