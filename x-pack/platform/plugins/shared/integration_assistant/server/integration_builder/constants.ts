eae7b7dcff1ac87fca48d90fe731df6a6dad0dc8
Hanna Tamoudi <hanna.tamoudi@elastic.co>
2025-01-14 14:32:04 +0100

    [Automatic Import] upgrade version in manifest (#205942)

--- x-pack/platform/plugins/shared/integration_assistant/server/integration_builder/constants.ts
+++ x-pack/platform/plugins/shared/integration_assistant/server/integration_builder/constants.ts
@@ -27,3 +27,10 @@ export const DEFAULT_CEL_PROGRAM = `# // Fetch the agent's public IP every minut
 # })`;
 
 export const DEFAULT_URL = 'https://server.example.com:8089/api';
+
+// The version of the package specification format used by this package https://github.com/elastic/package-spec/blob/main/spec/changelog.yml
+export const FORMAT_VERSION = '3.1.4';
+
+// Kibana versions compatible with this package
+// Explicitly specifying both 8.13.0 and 9.0.0 ensures compatibility across major versions, as semantic versioning does not assume forward compatibility.
+export const KIBANA_MINIMUM_VERSION = '^8.13.0 || ^9.0.0';
