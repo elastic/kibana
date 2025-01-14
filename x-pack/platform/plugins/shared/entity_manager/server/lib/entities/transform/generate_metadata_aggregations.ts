91444e8409c9eaaa802dc6e2252702ee009f7605
Tiago Vila Verde <tiago.vilaverde@elastic.co>
2025-01-14 16:18:15 +0100

    revert to static @timestamp

--- x-pack/platform/plugins/shared/entity_manager/server/lib/entities/transform/generate_metadata_aggregations.ts
+++ x-pack/platform/plugins/shared/entity_manager/server/lib/entities/transform/generate_metadata_aggregations.ts
@@ -19,7 +19,7 @@ export function generateLatestMetadataAggregations(definition: EntityDefinition)
       agg = {
         filter: {
           range: {
-            [definition.latest.timestampField]: {
+            '@timestamp': {
               gte: `now-${lookbackPeriod}`,
             },
           },
@@ -40,7 +40,7 @@ export function generateLatestMetadataAggregations(definition: EntityDefinition)
             must: [
               {
                 range: {
-                  [definition.latest.timestampField]: {
+                  '@timestamp': {
                     gte: `now-${lookbackPeriod}`,
                   },
                 },
