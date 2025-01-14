01aa6d5edb927f06d18ff9316a16291f36cd88c8
David Sanchez Soler <david.sanchezsoler@elastic.co>
2025-01-14 09:38:25 +0100

    Adds upgrade notes for management deprecated apis

--- src/platform/packages/shared/kbn-doc-links/src/types.ts
+++ src/platform/packages/shared/kbn-doc-links/src/types.ts
@@ -376,6 +376,7 @@ export interface DocLinks {
     };
     readonly detectionEngineOverview: string;
     readonly signalsMigrationApi: string;
+    readonly legacyEndpointManagementApiDeprecations: string;
   };
   readonly query: {
     readonly eql: string;
