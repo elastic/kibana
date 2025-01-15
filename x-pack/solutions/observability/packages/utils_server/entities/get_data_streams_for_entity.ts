b7977e12c048ec49d1e95c601f0a9a0463f9ef2b
Dominique Clarke <dominique.clarke@elastic.co>
2025-01-15 09:00:33 -0500

    Update x-pack/solutions/observability/packages/utils_server/entities/get_data_streams_for_entity.ts
    Co-authored-by: Dario Gieselaar <d.gieselaar@gmail.com>

--- x-pack/solutions/observability/packages/utils_server/entities/get_data_streams_for_entity.ts
+++ x-pack/solutions/observability/packages/utils_server/entities/get_data_streams_for_entity.ts
@@ -57,7 +57,7 @@ export async function getDataStreamsForEntity({
     compact([
       /* Check both data streams and indices.
        * The response body shape differs depending on the request. Example:
-       * GET _resolve/index/.logs-*-default* will return data in the `dataStreams` key.
+       * GET _resolve/index/logs-*-default* will return data in the `data_streams` key.
        * GET _resolve/index/.ds-logs-*-default* will return data in the `indices` key */
       ...resolveIndexResponse.indices.flatMap((idx) => {
         const remoteCluster = idx.name.includes(':') ? idx.name.split(':')[0] : null;
