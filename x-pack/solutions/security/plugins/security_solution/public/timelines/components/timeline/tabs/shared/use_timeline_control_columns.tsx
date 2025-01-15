aa4dbab425ee52faba45dacf997dc4b1a0963953
Philippe Oberti <philippe.oberti@elastic.co>
2025-01-15 16:14:28 +0100

    [Security Solution] - remove styled-components and cleanup for timeline header actions (#206694)
    ## Summary
    
    Similar to [this previous
    PR](https://github.com/elastic/kibana/pull/206523), this PR originally
    aimed at replacing the usages styled-components with @emotion/react in
    the security_solution/public/common/components/header_actions folder. I
    quickly realized that a lot of props and event components were actually
    not used at all, so a cleanup was welcome.
    
    Absolutely no UI or behavior logic changes should be introduced by this
    PR.
    
    This PR will impact the tables in timeline, and the markdown use in
    osquery.
    
    The biggest code cleanup are:
    - removing a couple of components related to the header actions used in
    timeline
    - removing random unused translations, constants or helper functions
    
    #### Timeline query, correlation and pinned tabs
    
    ![Screenshot 2025-01-14 at 5 29
    49 PM](https://github.com/user-attachments/assets/8c06c0f3-c323-4dd7-96d1-14357abc551d)
    
    ![Screenshot 2025-01-14 at 5 30
    05 PM](https://github.com/user-attachments/assets/7332fbe2-8744-46d5-a5e8-bd056fba0375)
    
    ![Screenshot 2025-01-14 at 5 30
    27 PM](https://github.com/user-attachments/assets/86053a20-f0e4-4f0a-8bdd-0c2678194903)
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios

--- x-pack/solutions/security/plugins/security_solution/public/timelines/components/timeline/tabs/shared/use_timeline_control_columns.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/timelines/components/timeline/tabs/shared/use_timeline_control_columns.tsx
@@ -5,29 +5,18 @@
  * 2.0.
  */
 
-import React, { useMemo, useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import type { EuiDataGridCellValueElementProps } from '@elastic/eui';
-import type { SortColumnTable } from '@kbn/securitysolution-data-table';
 import type { TimelineItem } from '@kbn/timelines-plugin/common';
 import { JEST_ENVIRONMENT } from '../../../../../../common/constants';
 import { useLicense } from '../../../../../common/hooks/use_license';
-import { SourcererScopeName } from '../../../../../sourcerer/store/model';
-import { useSourcererDataView } from '../../../../../sourcerer/containers';
 import { getDefaultControlColumn } from '../../body/control_columns';
-import type { UnifiedActionProps } from '../../unified_components/data_table/control_column_cell_render';
-import type { TimelineTabs } from '../../../../../../common/types/timeline';
-import { HeaderActions } from '../../../../../common/components/header_actions/header_actions';
 import { TimelineControlColumnCellRender } from '../../unified_components/data_table/control_column_cell_render';
-import type { ColumnHeaderOptions } from '../../../../../../common/types';
-import { useTimelineColumns } from './use_timeline_columns';
 import type { UnifiedTimelineDataGridCellContext } from '../../types';
 import { useTimelineUnifiedDataTableContext } from '../../unified_components/data_table/use_timeline_unified_data_table_context';
 
 interface UseTimelineControlColumnArgs {
-  columns: ColumnHeaderOptions[];
-  sort: SortColumnTable[];
   timelineId: string;
-  activeTab: TimelineTabs;
   refetch: () => void;
   events: TimelineItem[];
   pinnedEventIds: Record<string, boolean>;
@@ -38,23 +27,16 @@ interface UseTimelineControlColumnArgs {
 const EMPTY_STRING_ARRAY: string[] = [];
 
 const noOp = () => {};
-const noSelectAll = ({ isSelected }: { isSelected: boolean }) => {};
 export const useTimelineControlColumn = ({
-  columns,
-  sort,
   timelineId,
-  activeTab,
   refetch,
   events,
   pinnedEventIds,
   eventIdToNoteIds,
   onToggleShowNotes,
 }: UseTimelineControlColumnArgs) => {
-  const { browserFields } = useSourcererDataView(SourcererScopeName.timeline);
-
   const isEnterprisePlus = useLicense().isEnterprise();
-  const ACTION_BUTTON_COUNT = useMemo(() => (isEnterprisePlus ? 6 : 5), [isEnterprisePlus]);
-  const { localColumns } = useTimelineColumns(columns);
+  const ACTION_BUTTON_COUNT = useMemo(() => (isEnterprisePlus ? 5 : 4), [isEnterprisePlus]);
 
   const RowCellRender = useMemo(
     () =>
@@ -108,39 +90,10 @@ export const useTimelineControlColumn = ({
     [events, timelineId, refetch, pinnedEventIds, eventIdToNoteIds, onToggleShowNotes]
   );
 
-  // We need one less when the unified components are enabled because the document expand is provided by the unified data table
-  const UNIFIED_COMPONENTS_ACTION_BUTTON_COUNT = ACTION_BUTTON_COUNT - 1;
   return useMemo(() => {
-    return getDefaultControlColumn(UNIFIED_COMPONENTS_ACTION_BUTTON_COUNT).map((x) => ({
+    return getDefaultControlColumn(ACTION_BUTTON_COUNT).map((x) => ({
       ...x,
-      headerCellRender: function HeaderCellRender(props: UnifiedActionProps) {
-        return (
-          <HeaderActions
-            width={x.width}
-            browserFields={browserFields}
-            columnHeaders={localColumns}
-            isEventViewer={false}
-            isSelectAllChecked={false}
-            onSelectAll={noSelectAll}
-            showEventsSelect={false}
-            showSelectAllCheckbox={false}
-            showFullScreenToggle={false}
-            sort={sort}
-            tabType={activeTab}
-            {...props}
-            timelineId={timelineId}
-          />
-        );
-      },
       rowCellRender: JEST_ENVIRONMENT ? RowCellRender : React.memo(RowCellRender),
     }));
-  }, [
-    UNIFIED_COMPONENTS_ACTION_BUTTON_COUNT,
-    browserFields,
-    localColumns,
-    sort,
-    activeTab,
-    timelineId,
-    RowCellRender,
-  ]);
+  }, [ACTION_BUTTON_COUNT, RowCellRender]);
 };
