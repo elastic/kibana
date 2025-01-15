936162ff8ca6618134e8695c9b3c022a98cef73b
Gergő Ábrahám <gergo.abraham@elastic.co>
2025-01-15 17:35:31 +0100

    add unit tests for TelemetryWatcher

---
+++ x-pack/solutions/security/plugins/security_solution/server/endpoint/lib/policy/telemetry_watch.test.ts
@@ -0,0 +1,145 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { Subject } from 'rxjs';
+import {
+  elasticsearchServiceMock,
+  loggingSystemMock,
+  savedObjectsServiceMock,
+} from '@kbn/core/server/mocks';
+import { createPackagePolicyServiceMock } from '@kbn/fleet-plugin/server/mocks';
+import type { PackagePolicyClient } from '@kbn/fleet-plugin/server';
+import type { PackagePolicy } from '@kbn/fleet-plugin/common';
+import { createPackagePolicyMock } from '@kbn/fleet-plugin/common/mocks';
+import { policyFactory } from '../../../../common/endpoint/models/policy_config';
+import type { PolicyConfig } from '../../../../common/endpoint/types';
+import { TelemetryConfigWatcher } from './telemetry_watch';
+import { TelemetryConfigProvider } from '../../../../common/telemetry_config/telemetry_config_provider';
+
+const MockPPWithEndpointPolicy = (cb?: (p: PolicyConfig) => PolicyConfig): PackagePolicy => {
+  const packagePolicy = createPackagePolicyMock();
+  if (!cb) {
+    // eslint-disable-next-line no-param-reassign
+    cb = (p) => p;
+  }
+  const policyConfig = cb(policyFactory());
+  packagePolicy.inputs[0].config = { policy: { value: policyConfig } };
+  return packagePolicy;
+};
+
+describe('Telemetry config watcher', () => {
+  const logger = loggingSystemMock.create().get('telemetry_watch.test');
+  const soStartMock = savedObjectsServiceMock.createStartContract();
+  const esStartMock = elasticsearchServiceMock.createStart();
+  let packagePolicySvcMock: jest.Mocked<PackagePolicyClient>;
+  let telemetryWatcher: TelemetryConfigWatcher;
+
+  const preparePackagePolicyMock = ({
+    isGlobalTelemetryEnabled,
+  }: {
+    isGlobalTelemetryEnabled: boolean;
+  }) => {
+    packagePolicySvcMock.list.mockResolvedValueOnce({
+      items: [
+        MockPPWithEndpointPolicy((pc: PolicyConfig): PolicyConfig => {
+          pc.global_telemetry_enabled = isGlobalTelemetryEnabled;
+          return pc;
+        }),
+      ],
+      total: 1,
+      page: 1,
+      perPage: 100,
+    });
+  };
+
+  beforeEach(() => {
+    packagePolicySvcMock = createPackagePolicyServiceMock();
+
+    telemetryWatcher = new TelemetryConfigWatcher(
+      packagePolicySvcMock,
+      soStartMock,
+      esStartMock,
+      logger
+    );
+  });
+
+  it('is activated on telemetry config changes', () => {
+    const telemetryConfigEmitter: Subject<boolean> = new Subject();
+    const telemetryConfigProvider = new TelemetryConfigProvider();
+
+    // spy on the watch() function
+    const mockWatch = jest.fn();
+    telemetryWatcher.watch = mockWatch;
+
+    telemetryConfigProvider.start(telemetryConfigEmitter);
+    telemetryWatcher.start(telemetryConfigProvider);
+
+    telemetryConfigEmitter.next(true);
+
+    expect(mockWatch).toBeCalledTimes(1);
+
+    telemetryWatcher.stop();
+    telemetryConfigProvider.stop();
+    telemetryConfigEmitter.complete();
+  });
+
+  it('pages through all endpoint policies', async () => {
+    const TOTAL = 247;
+
+    // set up the mocked package policy service to return and do what we want
+    packagePolicySvcMock.list
+      .mockResolvedValueOnce({
+        items: Array.from({ length: 100 }, () => MockPPWithEndpointPolicy()),
+        total: TOTAL,
+        page: 1,
+        perPage: 100,
+      })
+      .mockResolvedValueOnce({
+        items: Array.from({ length: 100 }, () => MockPPWithEndpointPolicy()),
+        total: TOTAL,
+        page: 2,
+        perPage: 100,
+      })
+      .mockResolvedValueOnce({
+        items: Array.from({ length: TOTAL - 200 }, () => MockPPWithEndpointPolicy()),
+        total: TOTAL,
+        page: 3,
+        perPage: 100,
+      });
+
+    await telemetryWatcher.watch(true); // manual trigger
+
+    expect(packagePolicySvcMock.list).toBeCalledTimes(3);
+
+    // Assert: on the first call to packagePolicy.list, we asked for page 1
+    expect(packagePolicySvcMock.list.mock.calls[0][1].page).toBe(1);
+    expect(packagePolicySvcMock.list.mock.calls[1][1].page).toBe(2); // second call, asked for page 2
+    expect(packagePolicySvcMock.list.mock.calls[2][1].page).toBe(3); // etc
+  });
+
+  it.each([true, false])(
+    'does not update policies if both global telemetry config and policy fields are %s',
+    async (value) => {
+      preparePackagePolicyMock({ isGlobalTelemetryEnabled: value });
+
+      await telemetryWatcher.watch(value);
+
+      expect(packagePolicySvcMock.update).not.toHaveBeenCalled();
+    }
+  );
+
+  it.each([true, false])('updates `global_telemetry_config` field to %s', async (value) => {
+    preparePackagePolicyMock({ isGlobalTelemetryEnabled: !value });
+
+    await telemetryWatcher.watch(value);
+
+    expect(packagePolicySvcMock.update).toHaveBeenCalled();
+    const updatedPolicy: PolicyConfig =
+      packagePolicySvcMock.update.mock.calls[0][3].inputs[0].config?.policy.value;
+    expect(updatedPolicy.global_telemetry_enabled).toBe(value);
+  });
+});
