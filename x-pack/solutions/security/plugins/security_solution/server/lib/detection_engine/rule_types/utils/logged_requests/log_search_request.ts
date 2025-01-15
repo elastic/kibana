e0a2bba4174cd9fff66a5a64e7d4636801e4b901
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2025-01-15 11:47:30 +0000

    [Security Solution][Detection Engine] fixes logger

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/utils/logged_requests/log_search_request.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/utils/logged_requests/log_search_request.ts
@@ -7,26 +7,23 @@
 
 import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 
-export const logSearchRequest = (searchRequest: estypes.SearchRequest): string => {
-  // handle deprecated body property which still used widely across Detection Engine
-  if (searchRequest.body) {
-    const { body, index, ...params } = searchRequest;
-    const urlParams = Object.entries(params)
-      .reduce<string[]>((acc, [key, value]) => {
-        if (value != null) {
-          acc.push(`${key}=${value}`);
-        }
+export const logSearchRequest = (searchRequest: estypes.SearchRequest): string | undefined => {
+  const { body, index, ...params } = searchRequest;
+  const urlParams = Object.entries(params)
+    .reduce<string[]>((acc, [key, value]) => {
+      if (value != null) {
+        acc.push(`${key}=${value}`);
+      }
 
-        return acc;
-      }, [])
-      .join('&');
-    return `POST /${index}/_search${urlParams ? `?${urlParams}` : ''}\n${JSON.stringify(
-      { ...searchRequest.body },
-      null,
-      2
-    )}`;
+      return acc;
+    }, [])
+    .join('&');
+
+  const url = `/${index}/_search${urlParams ? `?${urlParams}` : ''}`;
+
+  if (body) {
+    return `POST ${url}\n${JSON.stringify({ ...body }, null, 2)}`;
   }
 
-  // TODO: fix me
-  return '???';
+  return `GET ${url}`;
 };
