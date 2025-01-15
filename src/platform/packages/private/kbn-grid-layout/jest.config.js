8254e404abfb3b5baecd21778f316dded8e029c6
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-15 18:39:41 +0100

    SKA: Relocate @kbn/grid-layout (#206821)
    ## Summary
    
    This PR aims at relocating some of the Kibana modules (plugins and
    packages) into a new folder structure, according to the _Sustainable
    Kibana Architecture_ initiative.
    
    > [!IMPORTANT]
    > * We kindly ask you to:
    > * Manually fix the errors in the error section below (if there are
    any).
    > * Search for the `packages[\/\\]` and `plugins[\/\\]` patterns in the
    source code (Babel and Eslint config files), and update them
    appropriately.
    > * Manually review
    `.buildkite/scripts/pipelines/pull_request/pipeline.ts` to ensure that
    any CI pipeline customizations continue to be correctly applied after
    the changed path names
    > * Review all of the updated files, specially the `.ts` and `.js` files
    listed in the sections below, as some of them contain relative paths
    that have been updated.
    > * Think of potential impact of the move, including tooling and
    configuration files that can be pointing to the relocated modules. E.g.:
    >     * customised eslint rules
    >     * docs pointing to source code
    
    > [!NOTE]
    > * This PR has been auto-generated.
    > * Any manual contributions will be lost if the 'relocate' script is
    re-run.
    > * Try to obtain the missing reviews / approvals before applying manual
    fixes, and/or keep your changes in a .patch / git stash.
    > * Please use
    [#sustainable_kibana_architecture](https://elastic.slack.com/archives/C07TCKTA22E)
    Slack channel for feedback.
    
    Are you trying to rebase this PR to solve merge conflicts? Please follow
    the steps describe
    [here](https://elastic.slack.com/archives/C07TCKTA22E/p1734019532879269?thread_ts=1734019339.935419&cid=C07TCKTA22E).
    
    #### 1 packages(s) are going to be relocated:
    
    | Id | Target folder |
    | -- | ------------- |
    | `@kbn/grid-layout` | `src/platform/packages/private/kbn-grid-layout` |
    
    
    <details >
    <summary>Updated references</summary>
    
    ```
    ./.i18nrc.json
    ./examples/grid_example/tsconfig.type_check.json
    ./package.json
    ./packages/kbn-ts-projects/config-paths.json
    ./src/platform/packages/private/kbn-grid-layout/jest.config.js
    ./src/platform/packages/private/kbn-repo-packages/package-map.json
    ./tsconfig.base.json
    ./tsconfig.base.type_check.json
    ./tsconfig.refs.json
    ./yarn.lock
    .github/CODEOWNERS
    ```
    
    </details><details >
    <summary>Updated relative paths</summary>
    
    ```
    src/platform/packages/private/kbn-grid-layout/jest.config.js:12
    src/platform/packages/private/kbn-grid-layout/tsconfig.json:2
    src/platform/packages/private/kbn-grid-layout/tsconfig.type_check.json:2
    ```
    
    </details>

--- src/platform/packages/private/kbn-grid-layout/jest.config.js
+++ src/platform/packages/private/kbn-grid-layout/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-grid-layout'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-grid-layout'],
 };
