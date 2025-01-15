8c44123f468fdbf43b0ec29e373eed7edbf7b0a7
Paul Tavares <paul.tavares@elastic.co>
2025-01-15 16:37:40 -0500

    caches the token from MS and reuses it

--- x-pack/platform/plugins/shared/stack_connectors/server/connector_types/microsoft_defender_endpoint/o_auth_token_manager.ts
+++ x-pack/platform/plugins/shared/stack_connectors/server/connector_types/microsoft_defender_endpoint/o_auth_token_manager.ts
@@ -16,10 +16,12 @@ import {
 } from '../../../common/microsoft_defender_endpoint/types';
 
 export class OAuthTokenManager {
-  private accessToken: string = '';
-  private connectorToken: ConnectorToken | undefined = undefined;
+  private connectorToken: ConnectorToken | null = null;
   private readonly oAuthTokenUrl: string;
+  // NOTE:  this `tokenType` here MUST be `access_token` due to the use of
+  //        `ConnectorTokenClient.updateOrCreate()` method, which hardcodes the `tokenType`
   private readonly tokenType = 'access_token';
+  private generatingNewTokenPromise: Promise<void> | null = null;
 
   constructor(
     private readonly params: ServiceParams<
@@ -39,7 +41,7 @@ export class OAuthTokenManager {
 
   private isTokenExpired(token: ConnectorToken): boolean {
     const now = new Date();
-    now.setSeconds(-30); // Allows for a threshold of 30s before considering the token expired
+    now.setSeconds(now.getSeconds() - 30); // Allows for a threshold of 30s before considering the token expired
 
     const isExpired = token.expiresAt < now.toISOString();
 
@@ -50,74 +52,114 @@ export class OAuthTokenManager {
     return isExpired;
   }
 
-  private async generateNewToken(connectorUsageCollector: ConnectorUsageCollector): Promise<void> {
-    const {
-      connector: { id: connectorId },
-      logger,
-    } = this.params;
-    const connectorTokenClient = this.params.services.connectorTokenClient;
+  private async retrieveOrGenerateNewTokenIfNeeded(
+    connectorUsageCollector: ConnectorUsageCollector
+  ): Promise<void> {
+    if (this.generatingNewTokenPromise) {
+      return await this.generatingNewTokenPromise;
+    }
 
-    if (!this.connectorToken) {
-      logger.debug(`Retrieving cached connector access token (if any)`);
+    this.generatingNewTokenPromise = (async () => {
+      const {
+        connector: { id: connectorId },
+        logger,
+      } = this.params;
+      const connectorTokenClient = this.params.services.connectorTokenClient;
 
-      const cachedToken = await connectorTokenClient.get({
+      if (!this.connectorToken) {
+        logger.debug(`Retrieving cached connector access token (if any)`);
+
+        const cachedToken = await connectorTokenClient.get({
+          connectorId,
+          tokenType: this.tokenType,
+        });
+
+        if (cachedToken.connectorToken) {
+          this.connectorToken = cachedToken.connectorToken;
+
+          const logToken = {
+            ...this.connectorToken,
+            token: '[redacted]',
+          };
+
+          logger.debug(() => `using cached access token:\n${JSON.stringify(logToken, null, 2)}`);
+        } else {
+          logger.debug(`No cached access token found`);
+        }
+      }
+
+      if (this.connectorToken && !this.isTokenExpired(this.connectorToken)) {
+        logger.debug('Cached token is not expired - no need to request a new one');
+        return;
+      }
+
+      logger.debug(`Requesting a new Microsoft access token for connector id [${connectorId}]]`);
+
+      // FYI: API Docs: https://learn.microsoft.com/en-us/entra/identity-platform/v2-oauth2-client-creds-grant-flow#get-a-token
+      const { oAuthScope, clientId } = this.params.config;
+      const tokenRequestDate = Date.now();
+      const newToken = await this.params.apiRequest<MicrosoftDefenderEndpointApiTokenResponse>(
+        {
+          url: this.oAuthTokenUrl,
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          data: {
+            grant_type: 'client_credentials',
+            client_id: clientId,
+            scope: oAuthScope,
+            client_secret: this.params.secrets.clientSecret,
+          },
+          responseSchema: MicrosoftDefenderEndpointDoNotValidateResponseSchema,
+        },
+        connectorUsageCollector
+      );
+
+      logger.debug(
+        () =>
+          `Successfully created an access token for Microsoft Defend for Endpoint:\n${JSON.stringify(
+            {
+              ...newToken.data,
+              access_token: '[REDACTED]',
+            }
+          )}`
+      );
+
+      await connectorTokenClient.updateOrReplace({
+        connectorId,
+        tokenRequestDate,
+        deleteExisting: true,
+        token: this.connectorToken,
+        newToken: newToken.data.access_token,
+        expiresInSec: newToken.data.expires_in,
+      });
+
+      const updatedCachedToken = await connectorTokenClient.get({
         connectorId,
         tokenType: this.tokenType,
       });
 
-      if (cachedToken.connectorToken) {
-        this.connectorToken = cachedToken.connectorToken;
+      if (!updatedCachedToken.connectorToken) {
+        throw new Error(`Failed to retrieve cached [${this.tokenType}] after it was updated.`);
       }
-    }
 
-    if (this.connectorToken && !this.isTokenExpired(this.connectorToken)) {
-      // If current token is not expires, then there is nothing else to do
-      return;
-    }
+      this.connectorToken = updatedCachedToken.connectorToken;
+    })().finally(() => {
+      this.generatingNewTokenPromise = null;
+    });
 
-    logger.debug(`Requesting a new Microsoft access token for connector id [${connectorId}]]`);
-
-    // FYI: API Docs: https://learn.microsoft.com/en-us/entra/identity-platform/v2-oauth2-client-creds-grant-flow#get-a-token
-    const { oAuthScope, clientId } = this.params.config;
-    const newToken = await this.params.apiRequest<MicrosoftDefenderEndpointApiTokenResponse>(
-      {
-        url: this.oAuthTokenUrl,
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        data: {
-          grant_type: 'client_credentials',
-          client_id: clientId,
-          scope: oAuthScope,
-          client_secret: this.params.secrets.clientSecret,
-        },
-        responseSchema: MicrosoftDefenderEndpointDoNotValidateResponseSchema,
-      },
-      connectorUsageCollector
-    );
-
-    logger.debug(
-      () =>
-        `Successfully created an access token for Microsoft Defend for Endpoint:\n${JSON.stringify({
-          ...newToken.data,
-          access_token: '[REDACTED]',
-        })}`
-    );
-
-    this.accessToken = newToken.data.access_token;
+    return this.generatingNewTokenPromise;
   }
 
   /**
    * Returns the Bearer token that should be used in API calls
    */
   public async get(connectorUsageCollector: ConnectorUsageCollector): Promise<string> {
-    if (!this.accessToken) {
-      await this.generateNewToken(connectorUsageCollector);
-    }
+    await this.retrieveOrGenerateNewTokenIfNeeded(connectorUsageCollector);
 
-    if (!this.accessToken) {
+    if (!this.connectorToken) {
       throw new Error('Access token for Microsoft Defend for Endpoint not available!');
     }
 
-    return this.accessToken;
+    return this.connectorToken.token;
   }
 }
