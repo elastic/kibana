97932cd2ffcf2fd3d3bf9f97d204dab13ed2ac1c
Jonathan Budzenski <jon@elastic.co>
2025-01-15 14:27:45 -0600

    update test

--- test/functional/apps/dashboard/dashboard_snapshots.ts
+++ test/functional/apps/dashboard/dashboard_snapshots.ts
@@ -84,7 +84,7 @@ export default function ({
       );
 
       await PageObjects.dashboard.clickExitFullScreenLogoButton();
-      expect(percentDifference).to.be.lessThan(0.02);
+      expect(percentDifference).to.be.lessThan(0.03);
     });
   });
 }
