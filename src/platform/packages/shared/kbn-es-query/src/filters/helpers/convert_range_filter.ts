3378bdf1bf31b0ca01b7c24b5a25bd27187b480d
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-01-16 11:53:18 +0200

    [ES|QL][Discover] Fixes CSV export with named params

--- src/platform/packages/shared/kbn-es-query/src/filters/helpers/convert_range_filter.ts
+++ src/platform/packages/shared/kbn-es-query/src/filters/helpers/convert_range_filter.ts
@@ -7,18 +7,31 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import moment from 'moment';
+import moment, { type Moment } from 'moment';
+import dateMath from '@kbn/datemath';
 import { keys } from 'lodash';
 import type { RangeFilter } from '../build_filters';
 import type { TimeRange } from './types';
+
+const isRelativeTime = (value: string | number | undefined): boolean => {
+  return typeof value === 'string' && value.includes('now');
+};
+
+const calculateLowerBound = (from: string | number): undefined | Moment =>
+  dateMath.parse(String(from));
+
+const calculateUpperBound = (to: string | number): undefined | Moment =>
+  dateMath.parse(String(to), { roundUp: true });
 
 export function convertRangeFilterToTimeRange(filter: RangeFilter) {
   const key = keys(filter.query.range)[0];
   const values = filter.query.range[key];
 
+  const from = values.gt || values.gte;
+  const to = values.lt || values.lte;
   return {
-    from: moment(values.gt || values.gte),
-    to: moment(values.lt || values.lte),
+    from: from && isRelativeTime(from) ? calculateLowerBound(from) ?? moment(from) : moment(from),
+    to: to && isRelativeTime(to) ? calculateUpperBound(to) ?? moment(to) : moment(to),
   };
 }
 
