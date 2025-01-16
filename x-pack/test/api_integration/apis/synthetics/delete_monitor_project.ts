0572b669b75946640861923116ba122ff955e1d6
Shahzad <shahzad31comp@gmail.com>
2025-01-16 18:05:42 +0100

    update tests

--- x-pack/test/api_integration/apis/synthetics/delete_monitor_project.ts
+++ x-pack/test/api_integration/apis/synthetics/delete_monitor_project.ts
@@ -47,10 +47,10 @@ export default function ({ getService }: FtrProviderContext) {
       projectMonitors = setUniqueIds(getFixtureJson('project_browser_monitor'));
     });
 
-    it('only allows 250 requests at a time', async () => {
+    it('only allows 500 requests at a time', async () => {
       const project = 'test-brower-suite';
       const monitors = [];
-      for (let i = 0; i < 251; i++) {
+      for (let i = 0; i < 550; i++) {
         monitors.push({
           ...projectMonitors.monitors[0],
           id: `test-id-${i}`,
