f60dbc883e63b6e4ddb5d7b440fd8c44f89d482a
Nick Peihl <nick.peihl@elastic.co>
2025-01-17 16:27:04 -0500

    Inject and extract library references in embeddable

---
+++ src/platform/plugins/shared/embeddable/common/lib/saved_object_id_references.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import type { SavedObjectReference } from '@kbn/core/server';
+import { EmbeddableStateWithType } from '../types';
+
+type EmbeddableStateWithTypeAndSavedObjectId = EmbeddableStateWithType & { savedObjectId?: string };
+const stateHasSavedObjectId = (
+  state: EmbeddableStateWithType
+): state is EmbeddableStateWithTypeAndSavedObjectId => {
+  return Boolean((state as EmbeddableStateWithTypeAndSavedObjectId).savedObjectId);
+};
+
+const panelSignifier = 'panel_';
+
+export const injectSavedObjectIdRef = (
+  state: EmbeddableStateWithType,
+  references: SavedObjectReference[]
+): EmbeddableStateWithType => {
+  const savedObjectReference = references.find(
+    (reference) => reference.name.indexOf(`${panelSignifier}${state.id}`) === 0
+  );
+  if (!savedObjectReference) {
+    return state;
+  }
+
+  const nextState: EmbeddableStateWithTypeAndSavedObjectId = {
+    ...state,
+    savedObjectId: savedObjectReference.id,
+  };
+  return nextState;
+};
+
+export const extractSavedObjectIdRef = (
+  state: EmbeddableStateWithType,
+  references: SavedObjectReference[]
+): { state: EmbeddableStateWithType; references: SavedObjectReference[] } => {
+  if (!stateHasSavedObjectId(state) || !state.savedObjectId) return { state, references };
+  references.push({
+    name: `${panelSignifier}${state.id}`,
+    type: state.type,
+    id: state.savedObjectId,
+  });
+
+  delete state.savedObjectId;
+  return { state, references };
+};
