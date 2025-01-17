0d91b981f21baf0843f3b1dc253cdd7c70c0db95
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-01-17 08:47:46 +0200

    [ES|QL][Discover] Fixes CSV export with named params (#206914)
    ## Summary
    
    Closes https://github.com/elastic/kibana/issues/206719
    
    Allows the csv report to get generated when there are time named params
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/platform/packages/shared/kbn-es-query/src/filters/helpers/convert_range_filter.test.ts
+++ src/platform/packages/shared/kbn-es-query/src/filters/helpers/convert_range_filter.test.ts
@@ -24,4 +24,15 @@ describe('convertRangeFilterToTimeRange', () => {
 
     expect(convertedRangeFilter).toEqual(filterAfterConvertedRangeFilter);
   });
+
+  it('should return converted range for relative dates', () => {
+    const filter: any = { query: { range: { '@timestamp': { gte: 'now-1d', lte: 'now' } } } };
+    const filterAfterConvertedRangeFilter = {
+      from: 'now-1d',
+      to: 'now',
+    };
+    const convertedRangeFilter = convertRangeFilterToTimeRange(filter);
+
+    expect(convertedRangeFilter).toEqual(filterAfterConvertedRangeFilter);
+  });
 });
