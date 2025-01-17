4e48d2ab9f01847337bfc374074a561482154c8a
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-01-17 14:08:41 -0500

    [Obs AI Assistant] Update zod schemas

--- x-pack/platform/plugins/shared/observability_solution/observability_ai_assistant/server/routes/schemas.ts
+++ x-pack/platform/plugins/shared/observability_solution/observability_ai_assistant/server/routes/schemas.ts
@@ -11,89 +11,77 @@ import { type Message, MessageRole } from '../../common/types';
 
 const messageSchema: z.ZodType<Message> = z.object({
   '@timestamp': z.string(),
-  message: z.intersection(
-    z.object({
-      role: z.enum([
-        MessageRole.System,
-        MessageRole.Assistant,
-        MessageRole.Function,
-        MessageRole.User,
-        MessageRole.Elastic,
-      ]),
-    }),
-    z.object({
-      content: z.string().optional(),
-      name: z.string().optional(),
-      event: z.string().optional(),
-      data: z.string().optional(),
-      function_call: z
-        .intersection(
-          z.object({
-            name: z.string(),
-            trigger: z.enum([MessageRole.Assistant, MessageRole.User, MessageRole.Elastic]),
-          }),
-          z.object({
-            arguments: z.string().optional(),
-          })
-        )
-        .optional(),
-    })
-  ),
+  message: z.object({
+    role: z.enum([
+      MessageRole.System,
+      MessageRole.Assistant,
+      MessageRole.Function,
+      MessageRole.User,
+      MessageRole.Elastic,
+    ]),
+    content: z.string().optional(),
+    name: z.string().optional(),
+    event: z.string().optional(),
+    data: z.string().optional(),
+    function_call: z
+      .intersection(
+        z.object({
+          name: z.string(),
+          trigger: z.enum([MessageRole.Assistant, MessageRole.User, MessageRole.Elastic]),
+        }),
+        z.object({
+          arguments: z.string().optional(),
+        })
+      )
+      .optional(),
+  }),
 });
 
-export const functionSchema = z.intersection(
-  z.object({
-    name: z.string().describe('Name of the function'),
-    description: z.string().describe('Description of the function'),
-  }),
-  z.object({
-    parameters: z.any().optional().describe('Parameters for the function'),
-  })
-);
+export const functionSchema = z.object({
+  name: z.string().describe('Name of the function'),
+  description: z.string().describe('Description of the function'),
+  parameters: z.any().optional().describe('Parameters for the function'),
+});
 
-export const chatCompleteBodyBaseSchema = z.intersection(
-  z.object({
-    messages: z
-      .array(messageSchema)
-      .describe('Array of message objects containing the conversation history'),
-    connectorId: z.string().describe('Unique identifier for the connector'),
-    persist: z.boolean().describe('Flag indicating whether to persist the conversation'),
-  }),
-  z.object({
-    conversationId: z
-      .string()
-      .optional()
-      .describe('Unique identifier for the conversation (if continuing a conversation)'),
-    title: z.string().optional().describe('Title of the conversation'),
-    disableFunctions: z
-      .union([
-        BooleanFromPrimitiveType,
+export const chatCompleteBodyBaseSchema = z.object({
+  messages: z
+    .array(messageSchema)
+    .describe('Array of message objects containing the conversation history'),
+  connectorId: z.string().describe('Unique identifier for the connector'),
+  persist: z.boolean().describe('Flag indicating whether to persist the conversation'),
+  conversationId: z
+    .string()
+    .optional()
+    .describe('Unique identifier for the conversation (if continuing a conversation)'),
+  title: z.string().optional().describe('Title of the conversation'),
+  disableFunctions: z
+    .union([
+      BooleanFromPrimitiveType,
+      z.object({
+        except: z.array(z.string()).describe('Array of function names to exclude from disabling'),
+      }),
+    ])
+    .optional()
+    .describe(
+      'Control function availability. Can be either a boolean to disable all functions, or an object specifying exceptions.'
+    ),
+  instructions: z
+    .array(
+      z.intersection(
         z.object({
-          except: z.array(z.string()).describe('Array of function names to exclude from disabling'),
+          id: z.string().optional().describe('Unique identifier for the instruction'),
         }),
-      ])
-      .optional()
-      .describe(
-        'Control function availability. Can be either a boolean to disable all functions, or an object specifying exceptions.'
-      ),
-    instructions: z
-      .array(
-        z.intersection(
-          z.object({
-            id: z.string().optional().describe('Unique identifier for the instruction'),
-          }),
-          z.object({
-            text: z.string().describe('The instruction content'),
-            instruction_type: z
-              .enum(['user_instruction', 'application_instruction'])
-              .describe('Type of instruction'),
-          })
-        )
+        z.object({
+          text: z.string().describe('The instruction content'),
+          instruction_type: z
+            .enum(['user_instruction', 'application_instruction'])
+            .describe('Type of instruction'),
+        })
       )
-      .optional()
-      .describe('Array of instruction objects'),
-  })
-);
+    )
+    .optional()
+    .describe('Array of instruction objects'),
+});
 
 export const chatCompletePublicQuerySchema = z
   .object({
