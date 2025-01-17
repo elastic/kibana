3d04ae2fcd66f2d9bb30065a259b9aa16375957c
Jean-Louis Leysens <jloleysens@gmail.com>
2025-01-17 14:09:11 +0100

    remove the addition for Elastic-Api-Version since we are likely not going to merging different versions of the same spec

--- packages/kbn-openapi-bundler/src/bundler/merge_documents/merge_documents.ts
+++ packages/kbn-openapi-bundler/src/bundler/merge_documents/merge_documents.ts
@@ -17,7 +17,6 @@ import { mergeSecurityRequirements } from './merge_security_requirements';
 import { mergeTags } from './merge_tags';
 import { getOasVersion } from '../../utils/get_oas_version';
 import { getOasDocumentVersion } from '../../utils/get_oas_document_version';
-import { enrichWithVersionMimeParam } from './enrich_with_version_mime_param';
 import { MergeOptions } from './merge_options';
 
 interface MergeDocumentsOptions extends MergeOptions {
@@ -33,10 +32,6 @@ export async function mergeDocuments(
     ? splitByVersion(resolvedDocuments)
     : new Map([['', resolvedDocuments]]);
   const mergedByVersion = new Map<string, OpenAPIV3.Document>();
-
-  if (!options.splitDocumentsByVersion) {
-    enrichWithVersionMimeParam(resolvedDocuments);
-  }
 
   for (const [apiVersion, documentsGroup] of documentsByVersion.entries()) {
     validateSameOasVersion(documentsGroup);
