a91dfdc3790d121db7e3f58b61659deec7a3e9b5
Sander Philipse <sander.philipse@elastic.co>
2025-01-17 16:23:14 +0100

    Update examples

--- x-pack/solutions/search/plugins/search_indices/public/components/index_documents/add_documents_code_example.tsx
+++ x-pack/solutions/search/plugins/search_indices/public/components/index_documents/add_documents_code_example.tsx
@@ -6,11 +6,10 @@
  */
 
 import React, { useCallback, useMemo, useState } from 'react';
-import { MappingDenseVectorProperty, MappingProperty } from '@elastic/elasticsearch/lib/api/types';
+import { MappingProperty } from '@elastic/elasticsearch/lib/api/types';
 import { EuiFlexGroup, EuiFlexItem, EuiPanel, EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { TryInConsoleButton } from '@kbn/try-in-console';
-import { isEqual } from 'lodash';
 
 import { useSearchApiKey } from '@kbn/search-api-keys-components';
 import { useKibana } from '../../hooks/use_kibana';
@@ -27,12 +26,11 @@ import { GuideSelector } from '../shared/guide_selector';
 import { useWorkflow } from '../shared/hooks/use_workflow';
 import { WorkflowId } from '../../code_examples/workflows';
 
-export const basicExampleTexts = [
-  'Yellowstone National Park',
-  'Yosemite National Park',
-  'Rocky Mountain National Park',
+export const exampleTexts = [
+  'Yellowstone National Park is one of the largest national parks in the United States. It ranges from the Wyoming to Montana and Idaho, and contains an area of 2,219,791 acress across three different states. Its most famous for hosting the geyser Old Faithful and is centered on the Yellowstone Caldera, the largest super volcano on the American continent. Yellowstone is host to hundreds of species of animal, many of which are endangered or threatened. Most notably, it contains free-ranging herds of bison and elk, alongside bears, cougars and wolves. The national park receives over 4.5 million visitors annually and is a UNESCO World Heritage Site.',
+  'Yosemite National Park is a United States National Park, covering over 750,000 acres of land in California. A UNESCO World Heritage Site, the park is best known for its granite cliffs, waterfalls and giant sequoia trees. Yosemite hosts over four million visitors in most years, with a peak of five million visitors in 2016. The park is home to a diverse range of wildlife, including mule deer, black bears, and the endangered Sierra Nevada bighorn sheep. The park has 1,200 square miles of wilderness, and is a popular destination for rock climbers, with over 3,000 feet of vertical granite to climb. Its most famous and cliff is the El Capitan, a 3,000 feet monolith along its tallest face.',
+  'Rocky Mountain National Park  is one of the most popular national parks in the United States. It receives over 4.5 million visitors annually, and is known for its mountainous terrain, including Longs Peak, which is the highest peak in the park. The park is home to a variety of wildlife, including elk, mule deer, moose, and bighorn sheep. The park is also home to a variety of ecosystems, including montane, subalpine, and alpine tundra. The park is a popular destination for hiking, camping, and wildlife viewing, and is a UNESCO World Heritage Site.',
 ];
-export const exampleTextsWithCustomMapping = [1, 2, 3].map((num) => `Example text ${num}`);
 
 export interface AddDocumentsCodeExampleProps {
   indexName: string;
@@ -64,19 +62,8 @@ export const AddDocumentsCodeExample = ({
     [usageTracker]
   );
   const sampleDocuments = useMemo(() => {
-    // If the default mapping was used, we need to exclude generated vector fields
-    const copyCodeSampleMappings = {
-      ...codeSampleMappings,
-      vector: {
-        type: codeSampleMappings.vector?.type,
-        dims: (codeSampleMappings.vector as MappingDenseVectorProperty)?.dims,
-      },
-    };
-    const isDefaultMapping = isEqual(copyCodeSampleMappings, ingestExamples.defaultMapping);
-    const sampleTexts = isDefaultMapping ? basicExampleTexts : exampleTextsWithCustomMapping;
-
-    return sampleTexts.map((text) => generateSampleDocument(codeSampleMappings, text));
-  }, [codeSampleMappings, ingestExamples.defaultMapping]);
+    return exampleTexts.map((text) => generateSampleDocument(codeSampleMappings, text));
+  }, [codeSampleMappings]);
   const { apiKey } = useSearchApiKey();
   const codeParams: IngestCodeSnippetParameters = useMemo(() => {
     return {
