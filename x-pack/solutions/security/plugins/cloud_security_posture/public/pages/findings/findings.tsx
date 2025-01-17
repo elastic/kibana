891664cb5d7a3cd56187c39aa1bc976053cf0b05
Paulo Henrique <paulo.henrique@elastic.co>
2025-01-17 15:54:20 -0300

    POC loading FindingsFlyout from security solution into CloudSecurity

--- x-pack/solutions/security/plugins/cloud_security_posture/public/pages/findings/findings.tsx
+++ x-pack/solutions/security/plugins/cloud_security_posture/public/pages/findings/findings.tsx
@@ -18,6 +18,7 @@ import { LOCAL_STORAGE_FINDINGS_LAST_SELECTED_TAB_KEY } from '../../common/const
 import { VULNERABILITIES_INDEX_NAME, FINDINGS_INDEX_NAME } from '../../../common/constants';
 import { getStatusForIndexName } from '../../../common/utils/helpers';
 import { Vulnerabilities } from '../vulnerabilities';
+import { GetFindingsExpandableFlyout } from '../../application/csp_router';
 
 type FindingsTabKey = 'vuln_mgmt' | 'configurations';
 
@@ -56,7 +57,11 @@ const FindingsTabRedirecter = ({ lastTabSelected }: { lastTabSelected?: Findings
   );
 };
 
-export const Findings = () => {
+export const Findings = ({
+  getFindingsExpandableFlyout,
+}: {
+  getFindingsExpandableFlyout: GetFindingsExpandableFlyout;
+}) => {
   const history = useHistory();
   const location = useLocation();
 
@@ -100,6 +105,10 @@ export const Findings = () => {
               <FormattedMessage id="xpack.csp.findings.title" defaultMessage="Findings" />
             </h1>
           </EuiTitle>
+          {getFindingsExpandableFlyout({
+            ruleId: 'test-rule-id-from-findings-page',
+            resourceId: 'test-resource-from-findings-page',
+          })}
           <EuiSpacer />
           <EuiTabs size="l">
             <EuiTab
