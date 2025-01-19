92559479d37a82707d90f272bff8baa11ac9f862
Nathan Reese <reese.nathan@gmail.com>
2025-01-19 08:22:47 -0700

    async mapHitSource

--- src/platform/plugins/shared/visualizations/public/utils/saved_visualize_utils.ts
+++ src/platform/plugins/shared/visualizations/public/utils/saved_visualize_utils.ts
@@ -8,6 +8,7 @@
  */
 
 import _ from 'lodash';
+import { asyncMap } from '@kbn/std';
 import type { SavedObjectAttributes, SavedObjectReference } from '@kbn/core/public';
 import { SavedObjectNotFound } from '@kbn/kibana-utils-plugin/public';
 import {
@@ -56,7 +57,7 @@ export function urlFor(id: string) {
   return `#/edit/${encodeURIComponent(id)}`;
 }
 
-export function mapHitSource(
+export async function mapHitSource(
   visTypes: Pick<TypesStart, 'get'>,
   {
     attributes,
@@ -109,13 +110,13 @@ export function mapHitSource(
     return newAttributes;
   }
 
-  newAttributes.type = visTypes.get(typeName as string);
+  newAttributes.type = await visTypes.get(typeName as string);
   newAttributes.savedObjectType = 'visualization';
   newAttributes.icon = newAttributes.type?.icon;
   newAttributes.image = newAttributes.type?.image;
   newAttributes.typeTitle = newAttributes.type?.title;
   newAttributes.editor = { editUrl: `/edit/${id}` };
-  newAttributes.readOnly = Boolean(visTypes.get(typeName as string)?.disableEdit);
+  newAttributes.readOnly = Boolean(newAttributes.type?.disableEdit);
 
   return newAttributes;
 }
@@ -197,7 +198,7 @@ export async function findListItems(
 
   return {
     total,
-    hits: savedObjects.map((savedObject: VisualizationSavedObject) => {
+    hits: await asyncMap(savedObjects, async (savedObject: VisualizationSavedObject) => {
       const config = extensionByType[savedObject.type];
 
       if (config) {
@@ -206,7 +207,7 @@ export async function findListItems(
           references: savedObject.references,
         };
       } else {
-        return mapHitSource(visTypes, savedObject);
+        return await mapHitSource(visTypes, savedObject);
       }
     }),
   };
