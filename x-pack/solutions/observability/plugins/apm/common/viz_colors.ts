088858f83ca46d1bf6f004965fce716efd4930f6
miriam.aparicio <miriam.aparicio@gmail.com>
2025-01-20 08:35:42 +0000

    fix test, review improvements

--- x-pack/solutions/observability/plugins/apm/common/viz_colors.ts
+++ x-pack/solutions/observability/plugins/apm/common/viz_colors.ts
@@ -7,22 +7,6 @@
 
 import type { EuiThemeComputed } from '@elastic/eui';
 
-function getVizColorsForTheme(euiTheme: EuiThemeComputed) {
-  return [
-    euiTheme.colors.vis.euiColorVis0,
-    euiTheme.colors.vis.euiColorVis1,
-    euiTheme.colors.vis.euiColorVis2,
-    euiTheme.colors.vis.euiColorVis3,
-    euiTheme.colors.vis.euiColorVis4,
-    euiTheme.colors.vis.euiColorVis5,
-    euiTheme.colors.vis.euiColorVis6,
-    euiTheme.colors.vis.euiColorVis7,
-    euiTheme.colors.vis.euiColorVis8,
-    euiTheme.colors.vis.euiColorVis9,
-  ];
-}
-
 export function getVizColorForIndex(index = 0, euiTheme: EuiThemeComputed) {
-  const colors = getVizColorsForTheme(euiTheme);
-  return colors[index % colors.length];
+  return euiTheme.colors.vis[`euiColorVis${index % 10}`];
 }
