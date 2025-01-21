de9d2aa59a09a9d3b9c9c0269190c14176c0b937
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-01-21 10:14:27 +0100

    Fix jest tests

--- src/platform/packages/shared/kbn-esql-variables-types/jest.config.js
+++ src/platform/packages/shared/kbn-esql-variables-types/jest.config.js
@@ -10,5 +10,5 @@
 module.exports = {
   preset: '@kbn/test/jest_node',
   rootDir: '../../../../..',
-  roots: ['<rootDir>/src/platform/packages/shared/kbn-es-types'],
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-esql-variable-types'],
 };
