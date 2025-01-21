ac071a9816d5c0c356e57199434b18b8ef528cb6
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-01-21 14:50:15 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/packages/shared/kbn-elastic-assistant-common/impl/content_references/content_references_store/__mocks__/content_references_store.mock.ts
+++ x-pack/platform/packages/shared/kbn-elastic-assistant-common/impl/content_references/content_references_store/__mocks__/content_references_store.mock.ts
@@ -1,4 +1,11 @@
-import { ContentReferencesStore } from "../../types";
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { ContentReferencesStore } from '../../types';
 
 export const contentReferencesStoreFactoryMock: () => ContentReferencesStore = jest
   .fn()
