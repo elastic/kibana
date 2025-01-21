fdfb6c5ee948722bfdf08d2ba14df8b6d2d92c83
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2025-01-21 13:36:55 +0000

    fix types

--- x-pack/platform/packages/shared/kbn-elastic-assistant-common/impl/content_references/content_references_store/content_references_store_factory.test.ts
+++ x-pack/platform/packages/shared/kbn-elastic-assistant-common/impl/content_references/content_references_store/content_references_store_factory.test.ts
@@ -9,6 +9,15 @@ import { contentReferencesStoreFactory } from './content_references_store_factor
 import { securityAlertsPageReference } from '../references';
 import { ContentReferencesStore } from '../types';
 
+export const contentReferencesStoreFactoryMock: () => ContentReferencesStore = jest
+  .fn()
+  .mockReturnValue({
+    add: jest.fn().mockImplementation((creator: Parameters<ContentReferencesStore['add']>[0]) => {
+      return creator({ id: 'exampleContentReferenceId' });
+    }),
+    getStore: jest.fn().mockReturnValue({}),
+  });
+
 describe('contentReferencesStoreFactory', () => {
   let contentReferencesStore: ContentReferencesStore;
   beforeEach(() => {
