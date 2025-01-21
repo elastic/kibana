6e42c414b429da7dcdafb8d2079de444510a5d84
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-01-21 20:08:45 +0100

    fetch repo before running the sstep script

--- .buildkite/scripts/steps/functional/performance_playwright.sh
+++ .buildkite/scripts/steps/functional/performance_playwright.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-
-echo "Manually fetching Kibana with SHA=$BUILDKITE_COMMIT"
-wget -q "https://github.com/elastic/kibana/archive/$BUILDKITE_COMMIT.tar.gz" -O "kibana.tar.gz"
-tar -xzf kibana.tar.gz
-cd "kibana-$BUILDKITE_COMMIT"
-
 echo "Running Kibana setup scripts"
 
 source .buildkite/scripts/common/util.sh
