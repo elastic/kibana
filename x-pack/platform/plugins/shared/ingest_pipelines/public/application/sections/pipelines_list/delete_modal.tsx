20a3eb55dcd8020c26935a50590cb8367a7d3552
Ignacio Rivas <rivasign@gmail.com>
2025-01-21 17:49:37 +0100

    Should be lowercase

--- x-pack/platform/plugins/shared/ingest_pipelines/public/application/sections/pipelines_list/delete_modal.tsx
+++ x-pack/platform/plugins/shared/ingest_pipelines/public/application/sections/pipelines_list/delete_modal.tsx
@@ -114,7 +114,7 @@ export const PipelineDeleteModal = ({
           <>
             <DeleteManagedAssetsCallout
               assetName={i18n.translate('xpack.ingestPipelines.deleteModal.assetName', {
-                defaultMessage: 'Ingest Pipeline',
+                defaultMessage: 'ingest pipeline',
               })}
             />
 
