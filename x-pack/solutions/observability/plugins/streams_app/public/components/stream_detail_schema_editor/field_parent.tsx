35b5785c78ef6e32901e4d9465b62d5066bd8434
Kerry Gallagher <471693+Kerry350@users.noreply.github.com>
2025-01-21 13:24:15 +0000

    Amend field parent badge link styling

--- x-pack/solutions/observability/plugins/streams_app/public/components/stream_detail_schema_editor/field_parent.tsx
+++ x-pack/solutions/observability/plugins/streams_app/public/components/stream_detail_schema_editor/field_parent.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { EuiBadge } from '@elastic/eui';
+import { EuiBadge, EuiLink } from '@elastic/eui';
 import React from 'react';
 import { useStreamsAppRouter } from '../../hooks/use_streams_app_router';
 
@@ -18,17 +18,19 @@ export const FieldParent = ({
 }) => {
   const router = useStreamsAppRouter();
   return linkEnabled ? (
-    <EuiBadge
-      color="hollow"
-      href={router.link('/{key}/{tab}/{subtab}', {
-        path: {
-          key: parent,
-          tab: 'management',
-          subtab: 'schemaEditor',
-        },
-      })}
-    >
-      {parent}
+    <EuiBadge color="hollow">
+      <EuiLink
+        href={router.link('/{key}/{tab}/{subtab}', {
+          path: {
+            key: parent,
+            tab: 'management',
+            subtab: 'schemaEditor',
+          },
+        })}
+        target="_blank"
+      >
+        {parent}
+      </EuiLink>
     </EuiBadge>
   ) : (
     <EuiBadge color="hollow">{parent}</EuiBadge>
