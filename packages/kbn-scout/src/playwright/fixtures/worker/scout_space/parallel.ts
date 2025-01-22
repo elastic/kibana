46a9b7875cd3e0a96028efe9062d04a1f2ddcde1
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-01-22 12:08:56 +0100

    add perf tracking

--- packages/kbn-scout/src/playwright/fixtures/worker/scout_space/parallel.ts
+++ packages/kbn-scout/src/playwright/fixtures/worker/scout_space/parallel.ts
@@ -8,7 +8,7 @@
  */
 
 import { UiSettingValues } from '@kbn/test/src/kbn_client/kbn_client_ui_settings';
-import { formatTime, isValidUTCDate, serviceLoadedMsg } from '../../../utils';
+import { formatTime, isValidUTCDate, measurePerformance, serviceLoadedMsg } from '../../../utils';
 import { coreWorkerFixtures } from '..';
 import { ImportSavedObjects, ScoutSpaceParallelFixture } from '.';
 
@@ -24,13 +24,15 @@ export const scoutSpaceParallelFixture = coreWorkerFixtures.extend<
         name: spaceId,
         disabledFeatures: [],
       };
-      log.debug(`[scoutSpace] creating space ${spaceId}`);
-      await kbnClient.spaces.create(spacePayload);
+      await measurePerformance(log, `scoutSpace:${spaceId} 'spaces.create'`, async () => {
+        return kbnClient.spaces.create(spacePayload);
+      });
 
       // cache saved objects ids in space
       const savedObjectsCache = new Map<string, string>();
 
       const load = async (path: string) => {
+        const startTime = performance.now();
         const response = await kbnClient.importExport.load(path, {
           space: spaceId,
           createNewCopies: true,
@@ -44,46 +46,66 @@ export const scoutSpaceParallelFixture = coreWorkerFixtures.extend<
 
         results.forEach((r) => savedObjectsCache.set(r.title, r.id));
 
+        const endTime = performance.now() - startTime;
+        log.debug(`scoutSpace:${spaceId} 'savedObjects.load' took ${endTime.toFixed(2)}ms`);
         return results;
       };
 
       const cleanStandardList = async () => {
-        // reset cache
-        savedObjectsCache.clear();
-        return kbnClient.savedObjects.cleanStandardList({
-          space: spaceId,
-        });
+        return measurePerformance(
+          log,
+          `scoutSpace:${spaceId} 'savedObjects.cleanStandardList'`,
+          async () => {
+            savedObjectsCache.clear();
+            await kbnClient.savedObjects.cleanStandardList({
+              space: spaceId,
+            });
+          }
+        );
       };
 
       const setDefaultIndex = async (dataViewName: string) => {
-        if (savedObjectsCache.has(dataViewName)) {
-          await kbnClient.uiSettings.update(
-            {
-              defaultIndex: savedObjectsCache.get(dataViewName)!,
-            },
-            { space: spaceId }
-          );
-        } else {
-          throw new Error(`Data view id ${dataViewName} not found in space ${spaceId}`);
-        }
+        return measurePerformance(
+          log,
+          `scoutSpace:${spaceId} 'savedObjects.setDefaultIndex'`,
+          async () => {
+            if (savedObjectsCache.has(dataViewName)) {
+              return kbnClient.uiSettings.update(
+                {
+                  defaultIndex: savedObjectsCache.get(dataViewName)!,
+                },
+                { space: spaceId }
+              );
+            } else {
+              throw new Error(`Data view id ${dataViewName} not found in space ${spaceId}`);
+            }
+          }
+        );
       };
 
       const set = async (values: UiSettingValues) => {
-        log.info(`Setting UI settings for space ${spaceId}: ${JSON.stringify(values)}`);
-        return kbnClient.uiSettings.update(values, { space: spaceId });
+        return measurePerformance(log, `scoutSpace:${spaceId} 'uiSettings.set'`, async () => {
+          return kbnClient.uiSettings.update(values, { space: spaceId });
+        });
       };
 
       const unset = async (...keys: string[]) => {
         return Promise.all(keys.map((key) => kbnClient.uiSettings.unset(key, { space: spaceId })));
       };
       const setDefaultTime = async ({ from, to }: { from: string; to: string }) => {
-        const utcFrom = isValidUTCDate(from) ? from : formatTime(from);
-        const untcTo = isValidUTCDate(to) ? to : formatTime(to);
-        await kbnClient.uiSettings.update(
-          {
-            'timepicker:timeDefaults': `{ "from": "${utcFrom}", "to": "${untcTo}"}`,
-          },
-          { space: spaceId }
+        return measurePerformance(
+          log,
+          `scoutSpace:${spaceId} 'uiSettings.setDefaultTime'`,
+          async () => {
+            const utcFrom = isValidUTCDate(from) ? from : formatTime(from);
+            const untcTo = isValidUTCDate(to) ? to : formatTime(to);
+            return kbnClient.uiSettings.update(
+              {
+                'timepicker:timeDefaults': `{ "from": "${utcFrom}", "to": "${untcTo}"}`,
+              },
+              { space: spaceId }
+            );
+          }
         );
       };
 
@@ -103,8 +125,9 @@ export const scoutSpaceParallelFixture = coreWorkerFixtures.extend<
       await use({ savedObjects, uiSettings, id: spaceId });
 
       // Cleanup space after tests via API call
-      log.debug(`[scoutSpace] deleting space ${spaceId}`);
-      await kbnClient.spaces.delete(spaceId);
+      await measurePerformance(log, `scoutSpace:${spaceId} 'space.delete'`, async () => {
+        return kbnClient.spaces.delete(spaceId);
+      });
     },
     { scope: 'worker', auto: true },
   ],
