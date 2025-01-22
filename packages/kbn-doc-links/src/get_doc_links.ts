9c25cb8cc2c87eb40a7c98fc9e356abd40dcb1d2
Candace Park <56409205+parkiino@users.noreply.github.com>
2025-01-22 15:19:07 -0500

    [8.16] [AVC Banner] Updates the AVC Banner for 2025 (#205467) (#205819)
    # Backport
    
    This will backport the following commits from `main` to `8.16`:
    - [[AVC Banner] Updates the AVC Banner for 2025
    (#205467)](https://github.com/elastic/kibana/pull/205467)
    
    <!--- Backport version: 8.9.8 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Candace
    Park","email":"56409205+parkiino@users.noreply.github.com"},"sourceCommit":{"committedDate":"2025-01-07T18:30:27Z","message":"[AVC
    Banner] Updates the AVC Banner for 2025 (#205467)\n\n## Summary\r\n\r\n-
    [x] Updates the AVC banner title and blog link\r\n- [x] Banner will
    remain visible until EOY 2025 \r\n- [x] Confirm that the original AVC
    banner code (not the part that hides\r\nit EOY of 2024) is present in
    8.16, 8.17 and 8.x branches\r\n\r\n#
    Screenshots\r\n\r\n![image](https://github.com/user-attachments/assets/c2ec37fc-4d42-45f9-aaa1-47b5a6cc715a)\r\n\r\n---------\r\n\r\nCo-authored-by:
    Elastic Machine
    <elasticmachine@users.noreply.github.com>","sha":"08535f54a0d9b172f8cd695a5330bcd87f896d7c","branchLabelMapping":{"^v9.0.0$":"main","^v8.18.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","Team:Fleet","v9.0.0","Team:Defend
    Workflows","backport:prev-major","v8.17.0","v8.18.0","v8.16.3","v8.17.1"],"number":205467,"url":"https://github.com/elastic/kibana/pull/205467","mergeCommit":{"message":"[AVC
    Banner] Updates the AVC Banner for 2025 (#205467)\n\n## Summary\r\n\r\n-
    [x] Updates the AVC banner title and blog link\r\n- [x] Banner will
    remain visible until EOY 2025 \r\n- [x] Confirm that the original AVC
    banner code (not the part that hides\r\nit EOY of 2024) is present in
    8.16, 8.17 and 8.x branches\r\n\r\n#
    Screenshots\r\n\r\n![image](https://github.com/user-attachments/assets/c2ec37fc-4d42-45f9-aaa1-47b5a6cc715a)\r\n\r\n---------\r\n\r\nCo-authored-by:
    Elastic Machine
    <elasticmachine@users.noreply.github.com>","sha":"08535f54a0d9b172f8cd695a5330bcd87f896d7c"}},"sourceBranch":"main","suggestedTargetBranches":["8.17","8.x","8.16"],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","labelRegex":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/205467","number":205467,"mergeCommit":{"message":"[AVC
    Banner] Updates the AVC Banner for 2025 (#205467)\n\n## Summary\r\n\r\n-
    [x] Updates the AVC banner title and blog link\r\n- [x] Banner will
    remain visible until EOY 2025 \r\n- [x] Confirm that the original AVC
    banner code (not the part that hides\r\nit EOY of 2024) is present in
    8.16, 8.17 and 8.x branches\r\n\r\n#
    Screenshots\r\n\r\n![image](https://github.com/user-attachments/assets/c2ec37fc-4d42-45f9-aaa1-47b5a6cc715a)\r\n\r\n---------\r\n\r\nCo-authored-by:
    Elastic Machine
    <elasticmachine@users.noreply.github.com>","sha":"08535f54a0d9b172f8cd695a5330bcd87f896d7c"}},{"branch":"8.17","label":"v8.17.0","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.x","label":"v8.18.0","labelRegex":"^v8.18.0$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.16","label":"v8.16.3","labelRegex":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- packages/kbn-doc-links/src/get_doc_links.ts
+++ packages/kbn-doc-links/src/get_doc_links.ts
@@ -470,7 +470,8 @@ export const getDocLinks = ({ kibanaBranch, buildFlavor }: GetDocLinkOptions): D
     },
     securitySolution: {
       artifactControl: `${SECURITY_SOLUTION_DOCS}artifact-control.html`,
-      avcResults: `${ELASTIC_WEBSITE_URL}blog/elastic-av-comparatives-business-security-test`,
+      avcResults: `${ELASTIC_WEBSITE_URL}blog/elastic-security-av-comparatives-business-test`,
+      bidirectionalIntegrations: `${SECURITY_SOLUTION_DOCS}third-party-actions.html`,
       trustedApps: `${SECURITY_SOLUTION_DOCS}trusted-apps-ov.html`,
       eventFilters: `${SECURITY_SOLUTION_DOCS}event-filters.html`,
       blocklist: `${SECURITY_SOLUTION_DOCS}blocklist.html`,
