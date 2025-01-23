40a71e96cd3aa9b37958fc013a5a08382e183d70
Paulo Silva <paulo.henrique@elastic.co>
2025-01-22 20:32:49 -0800

    Update links.ts

--- x-pack/solutions/security/plugins/security_solution/public/asset_inventory/links.ts
+++ x-pack/solutions/security/plugins/security_solution/public/asset_inventory/links.ts
@@ -17,7 +17,6 @@ import type { LinkItem } from '../common/links/types';
 
 export const links: LinkItem = {
   capabilities: [`${SECURITY_FEATURE_ID}.show`],
-  hideTimeline: true,
   globalNavPosition: 10,
   globalSearchKeywords: [
     i18n.translate('xpack.securitySolution.appLinks.inventory', {
