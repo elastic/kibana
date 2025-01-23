d04abc5519d8b269ab4bdcc36c3f30920aaf5abc
meghanmurphy1 <meghan.murphy@elastic.co>
2025-01-23 16:22:04 -0500

    add extra conditional if package_policy_id matches connector_id

--- x-pack/solutions/search/plugins/search_connectors/server/services/index.ts
+++ x-pack/solutions/search/plugins/search_connectors/server/services/index.ts
@@ -264,9 +264,10 @@ export const getConnectorsToDeploy = (
     if (connector.is_deleted) continue;
 
     // If no package policies reference this connector by id then it should be deployed
-    if (
-      packagePolicies.every((packagePolicy) => packagePolicy.connector_settings.id !== connector.id)
-    ) {
+    if (packagePolicies.every((packagePolicy) => 
+      connector.id !== packagePolicy.connector_settings.id &&
+      packagePolicy.package_policy_id !== connector.id
+   )) {
       results.push(connector);
     }
   }
