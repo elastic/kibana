c33ecb98ce4bbfbb936a089159b611f798d42bae
Dario Gieselaar <dario.gieselaar@elastic.co>
2025-01-23 18:09:21 +0100

    [Streams] Centralize error handling (#207858)
    Centralizes the error handling by:
    - creating a base StatusError exception that reports a status code
    - having all known errors extend from StatusError and setting a default
    status code for that error
    - handling both ES response and status errors into a single place,
    converting them into Boom errors
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Joe Reuter <johannes.reuter@elastic.co>

--- x-pack/solutions/observability/plugins/streams/server/lib/streams/errors/component_template_not_found_error.ts
+++ x-pack/solutions/observability/plugins/streams/server/lib/streams/errors/component_template_not_found_error.ts
@@ -5,9 +5,11 @@
  * 2.0.
  */
 
-export class SecurityException extends Error {
+import { StatusError } from './status_error';
+
+export class ComponentTemplateNotFoundError extends StatusError {
   constructor(message: string) {
-    super(message);
-    this.name = 'SecurityException';
+    super(message, 404);
+    this.name = 'ComponentTemplateNotFoundError';
   }
 }
