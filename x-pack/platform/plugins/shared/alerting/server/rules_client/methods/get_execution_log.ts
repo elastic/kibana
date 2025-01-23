12af20a077347525d4a3c738e409b897da7e6aec
Ying Mao <ying.mao@elastic.co>
2025-01-22 23:47:06 -0500

    Revert "[ES `body` removal] `@elastic/response-ops` (#204882)" (#207899)
    This reverts commit 7bb2dad38f8938569374ce5c99d5e4a2f1ff9b95.
    
    Original PR https://github.com/elastic/kibana/pull/204882 caused errors
    updating alert data stream index mappings in serverless. This seems to
    be a difference in the Elasticsearch client code handling requests with
    a body param vs requests without a body param
    https://github.com/elastic/elasticsearch-js/commit/a4315a905e818f1aaed39cd3f72b11c65f343842#diff-07b3475acb306ea63796d4e5cc559c073a63b84c8deeb9948d9ef24fb04c6439
    
    (cherry picked from commit b219962bda1e2183d2d74f3fec2959ebeee52b2e)

--- x-pack/platform/plugins/shared/alerting/server/rules_client/methods/get_execution_log.ts
+++ x-pack/platform/plugins/shared/alerting/server/rules_client/methods/get_execution_log.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type * as estypes from '@elastic/elasticsearch/lib/api/types';
+import type * as estypes from '@elastic/elasticsearch/lib/api/typesWithBodyKey';
 import { KueryNode } from '@kbn/es-query';
 import { SanitizedRuleWithLegacyId } from '../../types';
 import {
