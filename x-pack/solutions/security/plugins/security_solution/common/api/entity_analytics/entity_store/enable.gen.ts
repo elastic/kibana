5d13ab0faf949c92ffa9664032c0e564006ed0b0
Pablo Machado <pablo.nevesmachado@elastic.co>
2025-01-23 11:46:46 +0100

    [SecuritySolution] Update Entity Store Dashboard to prompt for Service Entity Type  (#207336)
    ## Summary
    
    * Display a callout to install uninstalled entity types
    * It will be displayed when the entity store is running, but some
    available entity types are not installed
    * Add `entityTypes` param to the init entity store API
    * Enable `serviceEntityStoreEnabled` flag by default
    ### Checklist
    
    ### How to test it?
    * Disable the experimental flag on
    `x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts`
    * Open `Manage/Entity Store/Engine Status`
    * Verify that the service entity store is NOT installed
    * Reenable the flag on
    `x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts`
    * Open the Entity Dashboard page
    * It should display a callout to install more entity types
    * Click "enable"
    * It should install the service entity store
    * Open `Manage/Entity Store/Engine Status`
    * Verify that the service entity store IS installed
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 5c59f3bdcdd9524667047ed546c6370015ed96d2)
    
    # Conflicts:
    #	oas_docs/output/kibana.yaml
    #	x-pack/solutions/security/plugins/security_solution/common/api/entity_analytics/entity_store/enable.gen.ts
    #	x-pack/solutions/security/plugins/security_solution/common/api/entity_analytics/entity_store/enable.schema.yaml
    #	x-pack/solutions/security/plugins/security_solution/docs/openapi/ess/security_solution_entity_analytics_api_2023_10_31.bundled.schema.yaml
    #	x-pack/solutions/security/plugins/security_solution/docs/openapi/serverless/security_solution_entity_analytics_api_2023_10_31.bundled.schema.yaml
    #	x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/entity_store/components/dashboard_enablement_panel.tsx
    #	x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/entity_store/entity_store_data_client.ts

--- x-pack/solutions/security/plugins/security_solution/common/api/entity_analytics/entity_store/enable.gen.ts
+++ x-pack/solutions/security/plugins/security_solution/common/api/entity_analytics/entity_store/enable.gen.ts
@@ -16,7 +16,7 @@
 
 import { z } from '@kbn/zod';
 
-import { IndexPattern, EntityType, Interval, EngineDescriptor } from './common.gen';
+import { IndexPattern, EntityType, EngineDescriptor } from './common.gen';
 
 export type InitEntityStoreRequestBody = z.infer<typeof InitEntityStoreRequestBody>;
 export const InitEntityStoreRequestBody = z.object({
@@ -27,7 +27,6 @@ export const InitEntityStoreRequestBody = z.object({
   indexPattern: IndexPattern.optional(),
   filter: z.string().optional(),
   entityTypes: z.array(EntityType).optional(),
-  enrichPolicyExecutionInterval: Interval.optional(),
 });
 export type InitEntityStoreRequestBodyInput = z.input<typeof InitEntityStoreRequestBody>;
 
