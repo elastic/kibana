9edb175059a2a14243c8ce86e38e1354d69037ff
Jean-Louis Leysens <jloleysens@gmail.com>
2025-01-23 10:44:00 +0100

    remove code example and fix link to file

--- src/core/packages/ui-settings/server-internal/src/saved_objects/migrations.ts
+++ src/core/packages/ui-settings/server-internal/src/saved_objects/migrations.ts
@@ -10,9 +10,23 @@
 import type {
   SavedObjectUnsanitizedDoc,
   SavedObjectSanitizedDoc,
+  SavedObjectMigrationMap,
+  SavedObjectsModelVersionMap,
 } from '@kbn/core-saved-objects-server';
 
-export const migrations = {
+/**
+ * @note Since all `uiSettings` migrations are added to the same migration function,
+ * while not required, grouping settings by team, using a consistent naming prefix,
+ * is good practice. For example: `ml:<setting-name>`.
+ */
+
+export const modelVersions: SavedObjectsModelVersionMap = {};
+
+/**
+ * Migrations using legacy upgrade mechanism, do not add to or remove from this map.
+ * Future migrations should live in modelVersions map.
+ */
+export const migrations: SavedObjectMigrationMap = {
   '7.9.0': (doc: SavedObjectUnsanitizedDoc<any>): SavedObjectSanitizedDoc<any> => ({
     ...doc,
     ...(doc.attributes && {
