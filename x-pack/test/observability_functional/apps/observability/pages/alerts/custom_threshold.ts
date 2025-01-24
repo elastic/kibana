6ce9d5c1c17adc53b09d140f7973c3648d697e14
Maryam Saeidi <maryam.saeidi@elastic.co>
2025-01-24 11:20:25 +0100

    Add test for logs consumer

--- x-pack/test/observability_functional/apps/observability/pages/alerts/custom_threshold.ts
+++ x-pack/test/observability_functional/apps/observability/pages/alerts/custom_threshold.ts
@@ -226,6 +226,7 @@ export default ({ getService, getPageObjects }: FtrProviderContext) => {
         expect.objectContaining({
           name: 'test custom threshold rule',
           tags: ['tag1'],
+          consumer: 'logs',
           params: expect.objectContaining({
             alertOnGroupDisappear: false,
             alertOnNoData: false,
