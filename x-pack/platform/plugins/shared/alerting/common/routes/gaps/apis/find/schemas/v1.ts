76c1d0962614eded8acc7b93637db9f2721a7302
Nikita Khristinin <nkhristinin@gmail.com>
2025-01-26 15:15:07 +0100

    Change schema for pagination

--- x-pack/platform/plugins/shared/alerting/common/routes/gaps/apis/find/schemas/v1.ts
+++ x-pack/platform/plugins/shared/alerting/common/routes/gaps/apis/find/schemas/v1.ts
@@ -11,7 +11,7 @@ export const findGapsBodySchema = schema.object(
   {
     end: schema.maybe(schema.string()),
     page: schema.number({ defaultValue: 1, min: 1 }),
-    per_page: schema.number({ defaultValue: 10, min: 0 }),
+    per_page: schema.number({ defaultValue: 10, min: 1 }),
     rule_id: schema.string(),
     start: schema.maybe(schema.string()),
     sort_field: schema.maybe(
