0d919a7630c0eda742a99663dfe78940ab00cb49
Nikita Khristinin <nkhristinin@gmail.com>
2025-01-26 15:14:50 +0100

    Fix type

--- x-pack/platform/plugins/shared/alerting/server/routes/gaps/apis/fill/transforms/transform_request/v1.ts
+++ x-pack/platform/plugins/shared/alerting/server/routes/gaps/apis/fill/transforms/transform_request/v1.ts
@@ -7,9 +7,9 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 
 import { FillGapByIdQueryV1 } from '../../../../../../../common/routes/gaps/apis/fill';
-import { FindGapByIdParams } from '../../../../../../lib/rule_gaps/types';
+import { FillGapByIdParams } from '../../../../../../application/rule/methods/fill_gap_by_id/types';
 
-export const transformRequest = ({ rule_id, gap_id }: FillGapByIdQueryV1): FindGapByIdParams => ({
+export const transformRequest = ({ rule_id, gap_id }: FillGapByIdQueryV1): FillGapByIdParams => ({
   gapId: gap_id,
   ruleId: rule_id,
 });
