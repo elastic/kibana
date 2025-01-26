95faaa0de5d60704d18b480eb34e8ac940b3c3b7
Sean Rathier <sean.rathier@elastic.co>
2025-01-26 15:30:00 -0500

    creating new const version

--- x-pack/test/cloud_security_posture_functional/agentless/security_posture.ts
+++ x-pack/test/cloud_security_posture_functional/agentless/security_posture.ts
@@ -6,6 +6,7 @@
  */
 
 import expect from '@kbn/expect';
+import { AGENTLESS_SECURITY_POSTURE_PACKAGE_VERSION } from '../constants';
 import type { FtrProviderContext } from '../ftr_provider_context';
 import { AGENTLESS_SECURITY_POSTURE_PACKAGE_VERSION } from '../constants';
 // eslint-disable-next-line import/no-default-export
