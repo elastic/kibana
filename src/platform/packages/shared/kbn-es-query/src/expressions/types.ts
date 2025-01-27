afcdd74f0855cae7e156cec46091a87395d7fb21
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-01-27 13:42:12 +0100

    Revert 2

--- src/platform/packages/shared/kbn-es-query/src/expressions/types.ts
+++ src/platform/packages/shared/kbn-es-query/src/expressions/types.ts
@@ -6,7 +6,7 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
-import type { ESQLControlVariable } from '@kbn/esql-variables-types';
+import type { ESQLControlVariable } from '@kbn/esql-validation-autocomplete';
 import { Filter, Query, TimeRange } from '../filters';
 
 export interface ExecutionContextSearch {
