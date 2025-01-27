d5c56b03b586620fca8fa2f04d069a7c001dcee8
Shahzad <shahzad31comp@gmail.com>
2025-01-27 19:37:22 +0100

    [Synthetics] Space aware private  locations !! (#202634)
    ## Summary
    
    Fixes https://github.com/elastic/kibana/issues/199976
    
    User can choose which space the location will be visible in while
    creating a location !!
    
    ### Testing
    
    - [ ] Create location in all spaces and make sure it's visible
    everywhere.
    - [ ] Creation location in a specific space and make sure it's only
    visible in specified space
    
    <img width="1728" alt="image"
    src="https://github.com/user-attachments/assets/6aa5cac9-500a-447a-8ef5-bf53e91a16dd"
    />
    
    (cherry picked from commit e51b2bda277ee4f532c901bcd65478fac86caeb8)

--- x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/state/agent_policies/api.ts
+++ x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/state/agent_policies/api.ts
@@ -8,16 +8,16 @@
 import { NewLocation } from '../../components/settings/private_locations/add_location_flyout';
 import { AgentPolicyInfo } from '../../../../../common/types';
 import { INITIAL_REST_VERSION, SYNTHETICS_API_URLS } from '../../../../../common/constants';
-import { PrivateLocation, SyntheticsPrivateLocations } from '../../../../../common/runtime_types';
+import { SyntheticsPrivateLocations } from '../../../../../common/runtime_types';
 import { apiService } from '../../../../utils/api_service/api_service';
 
 export const fetchAgentPolicies = async (): Promise<AgentPolicyInfo[]> => {
   return await apiService.get(SYNTHETICS_API_URLS.AGENT_POLICIES);
 };
 
-export const createSyntheticsPrivateLocation = async (
+export const addSyntheticsPrivateLocations = async (
   newLocation: NewLocation
-): Promise<PrivateLocation> => {
+): Promise<SyntheticsPrivateLocations> => {
   return await apiService.post(SYNTHETICS_API_URLS.PRIVATE_LOCATIONS, newLocation, undefined, {
     version: INITIAL_REST_VERSION,
   });
@@ -29,7 +29,7 @@ export const getSyntheticsPrivateLocations = async (): Promise<SyntheticsPrivate
   });
 };
 
-export const deleteSyntheticsPrivateLocation = async (
+export const deleteSyntheticsPrivateLocations = async (
   locationId: string
 ): Promise<SyntheticsPrivateLocations> => {
   return await apiService.delete(SYNTHETICS_API_URLS.PRIVATE_LOCATIONS + `/${locationId}`, {
