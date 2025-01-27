ad84cce62ca57eca8bed6112284c489c249de29b
José Luis González <joseluisgj@gmail.com>
2025-01-27 14:48:53 +0100

    [Search] New Connectors and Web Crawlers temp icons (#208341)
    ## Summary
    
    The new Connectors and Web Crawler icons were redesigned and will be
    provided by EUI with [this
    PR](https://github.com/elastic/eui/pull/8285). While those are not
    available waiting for next EUI release, [we agreed with EUI
    team](https://elastic.slack.com/archives/C7QC1JV6F/p1737712560812099) to
    update any reference with custom EUI icons in the meantime to later get
    them from EUI and remove this custom implementation.
    
    ![CleanShot 2025-01-27 at 12 00
    09@2x](https://github.com/user-attachments/assets/f243204e-468b-4193-9f5e-17bce22c56d2)
    
    ![CleanShot 2025-01-27 at 11 59
    34@2x](https://github.com/user-attachments/assets/a19dff22-6a49-43a5-adef-bcf08209e514)
    
    ![CleanShot 2025-01-27 at 11 59
    22@2x](https://github.com/user-attachments/assets/d0c153ae-f0c5-483b-bcd8-f4677e0749f0)
    
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ### Identify risks
    
    Does this PR introduce any risks? For example, consider risks like hard
    to test bugs, performance regression, potential of data loss.
    
    Describe the risk, its severity, and mitigation for each identified
    risk. Invite stakeholders and evaluate how to proceed before merging.
    
    - [ ] [See some risk
    examples](https://github.com/elastic/kibana/blob/main/RISK_MATRIX.mdx)
    - [ ] ...

--- x-pack/solutions/search/packages/search/shared_ui/src/icons/index.ts
+++ x-pack/solutions/search/packages/search/shared_ui/src/icons/index.ts
@@ -4,9 +4,5 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-export * from './src/connector_icon';
-export * from './src/decorative_horizontal_stepper';
-export * from './src/form_info_field/form_info_field';
-export * from './src/search_empty_prompt';
-export * from './src/icons';
+export * from './EuiIconPlugs';
+export * from './EuiIconWeb';
