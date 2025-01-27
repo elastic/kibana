b49f774b8686598076283acbfffe8220262616a6
Bailey Cash <bailey.cash@elastic.co>
2025-01-27 10:25:11 -0500

    remove last function

--- x-pack/solutions/observability/plugins/slo/server/routes/slo/route.ts
+++ x-pack/solutions/observability/plugins/slo/server/routes/slo/route.ts
@@ -5,12 +5,6 @@
  * 2.0.
  */
 
-import { getOverviewParamsSchema } from '@kbn/slo-schema/src/rest_specs/routes/get_overview';
-import { executeWithErrorHandler } from '../../errors';
-import { GetSLOsOverview } from '../../services/get_slos_overview';
-import { createSloServerRoute } from '../create_slo_server_route';
-import { assertPlatinumLicense } from './utils/assert_platinum_license';
-import { getSpaceId } from './utils/get_space_id';
 import { fetchSloHealthRoute } from './fetch_health';
 import { getSloSettingsRoute } from './get_slo_settings';
 import { createSLORoute } from './create_slo';
@@ -32,42 +26,7 @@ import { getSloBurnRates } from './get_slo_burn_rates';
 import { getSLOSuggestionsRoute } from './get_suggestions';
 import { putSloSettings } from './put_slo_settings';
 import { resetSLORoute } from './reset_slo';
-
-const getSLOsOverview = createSloServerRoute({
-  endpoint: 'GET /internal/observability/slos/overview',
-  options: { access: 'internal' },
-  security: {
-    authz: {
-      requiredPrivileges: ['slo_read'],
-    },
-  },
-  params: getOverviewParamsSchema,
-  handler: async ({ context, params, request, logger, plugins }) => {
-    await assertPlatinumLicense(plugins);
-
-    const soClient = (await context.core).savedObjects.client;
-    const esClient = (await context.core).elasticsearch.client.asCurrentUser;
-
-    const ruleRegistry = await plugins.ruleRegistry.start();
-    const racClient = await ruleRegistry.getRacClientWithRequest(request);
-
-    const spaceId = await getSpaceId(plugins, request);
-
-    const alerting = await plugins.alerting.start();
-    const rulesClient = await alerting.getRulesClientWithRequest(request);
-
-    const slosOverview = new GetSLOsOverview(
-      soClient,
-      esClient,
-      spaceId,
-      logger,
-      rulesClient,
-      racClient
-    );
-
-    return await executeWithErrorHandler(() => slosOverview.execute(params?.query ?? {}));
-  },
-});
+import { getSLOsOverview } from './get_slos_overview';
 
 export const getSloRouteRepository = (isServerless?: boolean) => {
   return {
