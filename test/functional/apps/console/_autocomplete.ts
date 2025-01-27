e3644ff47e9e9cf47e417ae3e35c816d94f74f44
Ignacio Rivas <rivasign@gmail.com>
2025-01-27 10:17:09 +0100

    Unskip

--- test/functional/apps/console/_autocomplete.ts
+++ test/functional/apps/console/_autocomplete.ts
@@ -34,8 +34,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     });
   }
 
-  // Failing: See https://github.com/elastic/kibana/issues/156926
-  describe.skip('console autocomplete feature', function describeIndexTests() {
+  describe('console autocomplete feature', function describeIndexTests() {
     before(async () => {
       log.debug('navigateTo console');
       await PageObjects.common.navigateToApp('console');
