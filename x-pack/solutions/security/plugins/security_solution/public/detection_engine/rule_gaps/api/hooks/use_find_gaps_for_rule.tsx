28c507be9cfad661f28a2bad29358884479974a2
Nikita Khristinin <nkhristinin@gmail.com>
2025-01-27 12:15:04 +0100

    Fix types

--- x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_gaps/api/hooks/use_find_gaps_for_rule.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_gaps/api/hooks/use_find_gaps_for_rule.tsx
@@ -9,7 +9,7 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { useCallback } from 'react';
 import type { FindGapsResponseBody } from '@kbn/alerting-plugin/common/routes/gaps/apis/find';
 import { findGapsForRule } from '../api';
-import type { Gap, GapStatus } from '../../types';
+import type { GapStatus } from '../../types';
 
 const FIND_GAPS_FOR_RULE = 'FIND_GAP_FOR_RULE';
 
@@ -40,10 +40,7 @@ export const useFindGapsForRule = (
     start: string;
     end: string;
     statuses: GapStatus[];
-    sortField:
-      | 'kibana.alert.rule.gap.total_gap_duration_ms'
-      | '@timestamp'
-      | 'kibana.alert.rule.gap.status';
+    sortField: string;
     sortOrder: string;
   },
   options?: UseQueryOptions<FindGapsResponseBody>
