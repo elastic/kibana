1cdd8c9e76fce37d666c67ab1a584be1b1743f29
Matthew Kime <matt@mattki.me>
2025-01-28 17:08:50 -0600

    Update profile_query_editor.tsx

--- x-pack/plugins/searchprofiler/public/application/components/profile_query_editor/profile_query_editor.tsx
+++ x-pack/plugins/searchprofiler/public/application/components/profile_query_editor/profile_query_editor.tsx
@@ -86,13 +86,9 @@ export const ProfileQueryEditor = memo(() => {
   );
   const licenseEnabled = getLicenseStatus().valid;
 
-  const tooltipContentDisabled = !licenseEnabled
-    ? i18n.translate('xpack.searchProfiler.formProfileButton.noLicenseTooltip', {
-        defaultMessage: 'You need an active license to use Search Profiler',
-      })
-    : i18n.translate('xpack.searchProfiler.formProfileButton.noIndicesTooltip', {
-        defaultMessage: 'You must have at least one index to use Search Profiler',
-      });
+  const tooltipContentDisabled = i18n.translate('xpack.searchProfiler.formProfileButton.noLicenseTooltip', {
+    defaultMessage: 'You need an active license to use Search Profiler',
+  });
 
   const tooltipContentEnabled = i18n.translate(
     'xpack.searchProfiler.sendRequestButtonTooltipContent',
