dd060a80f9805726cc5405ca1501e5430d7d7d09
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-28 13:38:00 +0100

    Relocating module `@kbn/core-analytics-server-mocks`

--- src/core/packages/analytics/server-mocks/jest.config.js
+++ src/core/packages/analytics/server-mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/analytics/core-analytics-server-mocks'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/analytics/server-mocks'],
 };
