a52a4d405b9be8f7d4fc645fe4c3d56af0316b87
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-28 14:07:33 +0100

    Relocating module `@kbn/core-i18n-browser-mocks`

--- src/core/packages/i18n/browser-mocks/jest.config.js
+++ src/core/packages/i18n/browser-mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/i18n/core-i18n-browser-mocks'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/i18n/browser-mocks'],
 };
