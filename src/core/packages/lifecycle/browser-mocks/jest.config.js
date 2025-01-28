f0c3fd57deaf64c16882c88dc06af2a9fcb5b1d7
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-28 14:13:26 +0100

    Relocating module `@kbn/core-lifecycle-browser-mocks`

--- src/core/packages/lifecycle/browser-mocks/jest.config.js
+++ src/core/packages/lifecycle/browser-mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/lifecycle/core-lifecycle-browser-mocks'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/lifecycle/browser-mocks'],
 };
