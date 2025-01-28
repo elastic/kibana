d4ba07739320679f4417bbaee4f100c0a46d7a70
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-28 13:51:40 +0100

    Relocating module `@kbn/core-elasticsearch-client-server-mocks`

--- src/core/packages/elasticsearch/client-server-mocks/jest.config.js
+++ src/core/packages/elasticsearch/client-server-mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/elasticsearch/core-elasticsearch-client-server-mocks'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/elasticsearch/client-server-mocks'],
 };
