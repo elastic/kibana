6c6f7f3ff5d6344904dc0bc29bf892dbbf89b01a
dej611 <dej611@gmail.com>
2025-01-28 12:14:23 +0100

    :white_check_mark: Add tests

--- x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_edit.test.ts
+++ x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_edit.test.ts
@@ -17,7 +17,10 @@ import { BehaviorSubject } from 'rxjs';
 import { ApplicationStart } from '@kbn/core/public';
 import { LensEmbeddableStartServices } from '../types';
 
-function createEditApi(servicesOverrides: Partial<LensEmbeddableStartServices> = {}) {
+function createEditApi(
+  servicesOverrides: Partial<LensEmbeddableStartServices> = {},
+  viewMode: 'edit' | 'view' = 'edit'
+) {
   const internalApi = getLensInternalApiMock();
   const runtimeState = getLensRuntimeStateMock();
   const api = getLensApiMock();
@@ -38,34 +41,87 @@ function createEditApi(servicesOverrides: Partial<LensEmbeddableStartServices> =
     api,
     () => false, // DSL based
     services,
-    { getAppContext: () => ({ currentAppId: 'lens' }), viewMode: new BehaviorSubject('edit') }
+    { getAppContext: () => ({ currentAppId: 'lens' }), viewMode: new BehaviorSubject(viewMode) }
   );
 }
 
 describe('edit features', () => {
-  it('should be editable if visualize library privileges allow it', () => {
-    const editApi = createEditApi();
-    expect(editApi.api.isEditingEnabled()).toBe(true);
-  });
-
-  it('should not be editable if visualize library privileges do not allow it', () => {
-    const editApi = createEditApi({
-      capabilities: {
-        visualize: {
-          // cannot save
-          save: false,
-          saveQuery: true,
-          // cannot see the visualization
-          show: true,
-          createShortUrl: true,
-        },
-        dashboard: {
-          // cannot edit in dashboard
-          showWriteControls: false,
-        },
-      } as unknown as ApplicationStart['capabilities'],
+  describe('can edit', () => {
+    it('should be editable if visualize library privileges allow it', () => {
+      const editApi = createEditApi();
+      expect(editApi.api.isEditingEnabled()).toBe(true);
+      expect(editApi.api.isReadOnlyEnabled()).toBe(false);
     });
 
-    expect(editApi.api.isEditingEnabled()).toBe(false);
+    it('should not be editable if visualize library privileges do not allow it', () => {
+      const editApi = createEditApi({
+        capabilities: {
+          visualize: {
+            // cannot save
+            save: false,
+            saveQuery: true,
+            // can see the visualization
+            show: true,
+            createShortUrl: true,
+          },
+          dashboard: {
+            // cannot edit in dashboard
+            showWriteControls: false,
+          },
+        } as unknown as ApplicationStart['capabilities'],
+      });
+
+      expect(editApi.api.isEditingEnabled()).toBe(false);
+      expect(editApi.api.isReadOnlyEnabled()).toBe(false);
+    });
+  });
+  describe('can show config', () => {
+    it('should should be read only enabled if edit capabilities are off', () => {
+      const editApi = createEditApi(
+        {
+          capabilities: {
+            visualize: {
+              // cannot save
+              save: false,
+              saveQuery: true,
+              // can see the visualization
+              show: true,
+              createShortUrl: true,
+            },
+            dashboard: {
+              // cannot edit in dashboard
+              showWriteControls: false,
+            },
+          } as unknown as ApplicationStart['capabilities'],
+        },
+        'view'
+      );
+      expect(editApi.api.isReadOnlyEnabled()).toBe(true);
+      expect(editApi.api.isEditingEnabled()).toBe(false);
+    });
+
+    it('should should be read only disabled if show capabilities are off', () => {
+      const editApi = createEditApi(
+        {
+          capabilities: {
+            visualize: {
+              // cannot save
+              save: false,
+              saveQuery: true,
+              // cannot see the visualization
+              show: false,
+              createShortUrl: true,
+            },
+            dashboard: {
+              // cannot edit in dashboard
+              showWriteControls: false,
+            },
+          } as unknown as ApplicationStart['capabilities'],
+        },
+        'view'
+      );
+      expect(editApi.api.isReadOnlyEnabled()).toBe(false);
+      expect(editApi.api.isEditingEnabled()).toBe(false);
+    });
   });
 });
