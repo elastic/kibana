974ef209776ccfacb07da336fa856804626e19af
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-01-28 10:54:37 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/test/cases_api_integration/common/lib/api/index.ts
+++ x-pack/test/cases_api_integration/common/lib/api/index.ts
@@ -25,14 +25,9 @@ import {
   INTERNAL_CASE_METRICS_URL,
   INTERNAL_GET_CASE_CATEGORIES_URL,
   INTERNAL_CASE_SIMILAR_CASES_URL,
-  CASE_FIND_USER_ACTIONS_URL,
 } from '@kbn/cases-plugin/common/constants';
 import { CaseMetricsFeature } from '@kbn/cases-plugin/common';
-import type {
-  SingleCaseMetricsResponse,
-  CasesMetricsResponse,
-  UserActionFindResponse,
-} from '@kbn/cases-plugin/common';
+import type { SingleCaseMetricsResponse, CasesMetricsResponse } from '@kbn/cases-plugin/common';
 import { SignalHit } from '@kbn/security-solution-plugin/server/lib/detection_engine/rule_types/types';
 import { CasePersistedAttributes } from '@kbn/cases-plugin/server/common/types/case';
 import type { SavedObjectsRawDocSource } from '@kbn/core/server';
