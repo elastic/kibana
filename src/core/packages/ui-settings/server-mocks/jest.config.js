0a4c092a33a781c50d61530c2c46447e55370c76
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-28 14:51:10 +0100

    Relocating module `@kbn/core-ui-settings-server-mocks`

--- src/core/packages/ui-settings/server-mocks/jest.config.js
+++ src/core/packages/ui-settings/server-mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/ui-settings/core-ui-settings-server-mocks'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/ui-settings/server-mocks'],
 };
