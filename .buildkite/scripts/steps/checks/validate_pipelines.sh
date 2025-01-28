e2427e234187b697c50788dfa40a5edeac74ddba
Alex Szabo <alex.szabo@elastic.co>
2025-01-28 11:26:10 +0100

    tidy up validate_pipelines.sh

--- .buildkite/scripts/steps/checks/validate_pipelines.sh
+++ .buildkite/scripts/steps/checks/validate_pipelines.sh
@@ -10,9 +10,14 @@ check_for_changed_files "$cmd" true
 
 if [[ "${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-}" != "" ]]; then
   echo --- Check if all new pipelines are valid
-  git diff "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" --name-only | \
+  AFFECTED_PIPELINES=$(git diff "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" --name-only | \
     grep -E '^.buildkite/pipeline-resource-definitions/.*\.yml$' | \
-    grep -Ev '(/locations.yml|_templates)' | while read -r pipeline; do
+    grep -Ev '(/locations.yml|_templates)' || true)
+  echo "Pipelines affected by this PR: $AFFECTED_PIPELINES"
+
+  for pipeline in $AFFECTED_PIPELINES; do
     .buildkite/pipeline-resource-definitions/scripts/validate-pipeline-definition.sh "$pipeline"
   done
 fi
+
+exit 0
