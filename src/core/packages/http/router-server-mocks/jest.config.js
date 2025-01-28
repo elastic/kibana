c16df5184412cb1f3221678afcd32362aaab26f4
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-28 14:04:42 +0100

    Relocating module `@kbn/core-http-router-server-mocks`

--- src/core/packages/http/router-server-mocks/jest.config.js
+++ src/core/packages/http/router-server-mocks/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../..',
-  roots: ['<rootDir>/packages/core/http/core-http-router-server-mocks'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/core/packages/http/router-server-mocks'],
 };
