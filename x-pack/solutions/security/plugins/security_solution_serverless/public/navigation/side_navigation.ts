f2f9e2a2a39861bc70e3cf54e0ebbc3b8bda19c0
Timothy Sullivan <tsullivan@elastic.co>
2025-01-28 10:34:44 -0700

    Stack Management flyout for Serverless Security

--- x-pack/solutions/security/plugins/security_solution_serverless/public/navigation/side_navigation.ts
+++ x-pack/solutions/security/plugins/security_solution_serverless/public/navigation/side_navigation.ts
@@ -5,10 +5,10 @@
  * 2.0.
  */
 
-import { i18n } from '@kbn/i18n';
-import type { GroupDefinition } from '@kbn/core-chrome-browser';
-import produce from 'immer';
 import { map } from 'rxjs';
+import produce from 'immer';
+import { i18n } from '@kbn/i18n';
+import type { AppDeepLinkId, GroupDefinition, NodeDefinition } from '@kbn/core-chrome-browser';
 import { type Services } from '../common/services';
 
 const PROJECT_SETTINGS_TITLE = i18n.translate(
@@ -29,6 +29,15 @@ export const initSideNavigation = async (services: Services) => {
         const footerGroup: GroupDefinition | undefined = draft.footer?.find(
           ({ type }) => type === 'navGroup'
         ) as GroupDefinition;
+        // Adds children for Stack Management panel
+        const management = footerGroup?.children.find((child) => child.link === 'management');
+        if (management) {
+          management.renderAs = 'panelOpener';
+          management.id = 'stack_management';
+          management.spaceBefore = null;
+          management.children = stackManagementLinks;
+          delete management.link; // no landing page for stack management
+        }
         if (footerGroup) {
           footerGroup.title = PROJECT_SETTINGS_TITLE;
           footerGroup.children.push({ cloudLink: 'billingAndSub', openInNewTab: true });
@@ -42,3 +51,32 @@ export const initSideNavigation = async (services: Services) => {
     dataTestSubj: 'securitySolutionSideNav',
   });
 };
+
+// Stack Management static node definition
+const stackManagementLinks: Array<NodeDefinition<AppDeepLinkId, string, string>> = [
+  {
+    title: 'Data',
+    children: [{ link: 'management:data_quality' }],
+  },
+  {
+    title: 'Access',
+    children: [{ cloudLink: 'userAndRoles' }],
+  },
+  {
+    title: 'Alerts and Insights',
+    children: [
+      { link: 'management:triggersActions' },
+      { link: 'management:triggersActionsConnectors' },
+      { link: 'securitySolutionUI:entity_analytics-management' },
+      { link: 'securitySolutionUI:entity_analytics-entity_store_management' },
+    ],
+  },
+  {
+    title: 'Content',
+    children: [{ link: 'maps' }, { link: 'visualize' }],
+  },
+  {
+    title: 'Other',
+    children: [{ link: 'management:securityAiAssistantManagement' }],
+  },
+];
