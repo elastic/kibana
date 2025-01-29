4228d3336557db149122ccc554331b0fa59f7a24
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:59:49 +0100

    Relocating module `@kbn/kibana-manifest-schema`

--- src/dev/packages/kibana-manifest-schema/jest.config.js
+++ src/dev/packages/kibana-manifest-schema/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-kibana-manifest-schema'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/kibana-manifest-schema'],
 };
