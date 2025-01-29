6fad8090172159aef7514a47fc28240e856af221
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:50:39 +0100

    Relocating module `@kbn/cypress-config`

--- src/dev/packages/cypress-config/jest.config.js
+++ src/dev/packages/cypress-config/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-cypress-config'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/cypress-config'],
 };
