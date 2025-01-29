f355e8328b737a8a8b51f8b2b53b4e0aca32e34d
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:54:06 +0100

    Relocating module `@kbn/eslint-plugin-css`

--- src/dev/packages/eslint-plugin-css/jest.config.js
+++ src/dev/packages/eslint-plugin-css/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-eslint-plugin-css'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/eslint-plugin-css'],
 };
