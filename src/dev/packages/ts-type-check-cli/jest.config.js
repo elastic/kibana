6813d781b5d6c56e93254fc0b5bef1f0bef28916
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:11:21 +0100

    Relocating module `@kbn/ts-type-check-cli`

--- src/dev/packages/ts-type-check-cli/jest.config.js
+++ src/dev/packages/ts-type-check-cli/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-ts-type-check-cli'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/ts-type-check-cli'],
 };
