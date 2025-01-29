4c2fcbcf0ce1792ee3cacf69b73dfb8fc2aab892
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:55:29 +0100

    Relocating module `@kbn/eslint-plugin-telemetry`

--- src/dev/packages/eslint-plugin-telemetry/jest.config.js
+++ src/dev/packages/eslint-plugin-telemetry/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-eslint-plugin-telemetry'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/eslint-plugin-telemetry'],
 };
