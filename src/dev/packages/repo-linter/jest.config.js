4f0095f538e7e0085058b06f16bc942edb637db2
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:05:49 +0100

    Relocating module `@kbn/repo-linter`

--- src/dev/packages/repo-linter/jest.config.js
+++ src/dev/packages/repo-linter/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-repo-linter'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/repo-linter'],
 };
