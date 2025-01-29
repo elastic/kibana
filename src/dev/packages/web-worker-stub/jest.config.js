641d61eda84d50d13ecda8e58ef22f27a191fff5
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:11:53 +0100

    Relocating module `@kbn/web-worker-stub`

--- src/dev/packages/web-worker-stub/jest.config.js
+++ src/dev/packages/web-worker-stub/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-web-worker-stub'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/web-worker-stub'],
 };
