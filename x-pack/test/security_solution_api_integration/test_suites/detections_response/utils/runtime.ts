8a5b3d6dd4cef6fb4acf1d26bf2198fa044b308d
Devin Hurley <devin.hurley@elastic.co>
2025-01-29 16:47:22 -0500

    add missing types

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/utils/runtime.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/utils/runtime.ts
@@ -4,6 +4,13 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+import type { Client } from '@elastic/elasticsearch';
+
+interface UpdateMappingsProps {
+  es: Client;
+  index: string | string[];
+}
 export const setBrokenRuntimeField = async ({ es, index }: UpdateMappingsProps) => {
   await es.indices.putMapping({
     runtime: {
