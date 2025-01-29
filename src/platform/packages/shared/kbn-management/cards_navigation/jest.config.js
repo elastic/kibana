cf86d9eed68dbfd6fbdddeddb064d9e173d5613d
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 14:26:43 +0100

    Remove duplicate jest.config.js files

--- src/platform/packages/shared/kbn-management/cards_navigation/jest.config.js
+++ src/platform/packages/shared/kbn-management/cards_navigation/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../..',
-  roots: ['<rootDir>/src/platform/packages/private/kbn-management'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-management/cards_navigation'],
 };
--- src/platform/packages/shared/kbn-management/delete_managed_assets_callout/jest.config.js
+++ src/platform/packages/shared/kbn-management/delete_managed_assets_callout/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../..',
-  roots: ['<rootDir>/src/platform/packages/shared/kbn-management'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-management/delete_managed_assets_callout'],
 };
