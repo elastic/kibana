0a10ec32e1e88eda4a7d42dc0011e38b969b33eb
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:58:28 +0100

    Relocating module `@kbn/import-locator`

--- src/dev/packages/import-locator/jest.config.js
+++ src/dev/packages/import-locator/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-import-locator'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/import-locator'],
 };
