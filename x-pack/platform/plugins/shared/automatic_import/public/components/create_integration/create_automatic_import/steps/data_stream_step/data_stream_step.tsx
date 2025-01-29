e40f45330f6f5765e82850409f38f08893422bf4
Bharat Pasupula <saibharatchandra.pasupula@elastic.co>
2025-01-29 16:58:23 +0100

    add techpreview to CEL input

--- x-pack/platform/plugins/shared/automatic_import/public/components/create_integration/create_automatic_import/steps/data_stream_step/data_stream_step.tsx
+++ x-pack/platform/plugins/shared/automatic_import/public/components/create_integration/create_automatic_import/steps/data_stream_step/data_stream_step.tsx
@@ -7,6 +7,7 @@
 
 import type { EuiComboBoxOptionOption } from '@elastic/eui';
 import {
+  EuiBetaBadge,
   EuiButton,
   EuiButtonEmpty,
   EuiComboBox,
@@ -255,6 +256,14 @@ export const DataStreamStep = React.memo<DataStreamStepProps>(
                     label={
                       <EuiFlexGroup direction="row" gutterSize="s">
                         {i18n.ADD_OPEN_API_SPEC_LABEL}
+                        <EuiBetaBadge
+                          iconType="beaker"
+                          label={i18n.TECH_PREVIEW}
+                          tooltipContent={i18n.TECH_PREVIEW_TOOLTIP}
+                          size="s"
+                          color="hollow"
+                          data-test-subj="techPreviewBadge"
+                        />
                       </EuiFlexGroup>
                     }
                     labelAppend={
