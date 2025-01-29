d0c20691350daf2c092a1f1e96382a0d983918e6
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:49:48 +0100

    Relocating module `@kbn/ci-stats-shipper-cli`

--- src/dev/packages/ci-stats-shipper-cli/jest.config.js
+++ src/dev/packages/ci-stats-shipper-cli/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-ci-stats-shipper-cli'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/ci-stats-shipper-cli'],
 };
