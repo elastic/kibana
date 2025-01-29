8623f9008f6433009589cbe0b2b52c2c599fcaa6
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:00:23 +0100

    Relocating module `@kbn/lint-ts-projects-cli`

--- src/dev/packages/lint-ts-projects-cli/jest.config.js
+++ src/dev/packages/lint-ts-projects-cli/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-lint-ts-projects-cli'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/lint-ts-projects-cli'],
 };
