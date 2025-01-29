ef09aff7f8ebe5bdeb4db043af81fb78d39b14b0
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:58:12 +0100

    Relocating module `@kbn/get-repo-files`

--- src/dev/packages/get-repo-files/jest.config.js
+++ src/dev/packages/get-repo-files/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-get-repo-files'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/get-repo-files'],
 };
