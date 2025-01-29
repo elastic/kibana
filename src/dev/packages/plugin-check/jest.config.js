eb78f7e60f5b0166dff03e5a6b45cba996d83fc7
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:04:28 +0100

    Relocating module `@kbn/plugin-check`

--- src/dev/packages/plugin-check/jest.config.js
+++ src/dev/packages/plugin-check/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-plugin-check'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/plugin-check'],
 };
