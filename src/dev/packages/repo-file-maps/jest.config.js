707bbf4f20fcb89c4807e14af08a5c260eaefa60
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:05:33 +0100

    Relocating module `@kbn/repo-file-maps`

--- src/dev/packages/repo-file-maps/jest.config.js
+++ src/dev/packages/repo-file-maps/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-repo-file-maps'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/repo-file-maps'],
 };
