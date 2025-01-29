00cffab38d0d1871354c12c6582c4abe75d0c37c
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:46:01 +0100

    Relocating module `@kbn/ambient-common-types`

--- src/dev/packages/ambient-common-types/jest.config.js
+++ src/dev/packages/ambient-common-types/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-ambient-common-types'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/ambient-common-types'],
 };
