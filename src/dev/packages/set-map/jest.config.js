f0fd9389f9fea5960fde446d9925dadbae22be76
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:07:45 +0100

    Relocating module `@kbn/set-map`

--- src/dev/packages/set-map/jest.config.js
+++ src/dev/packages/set-map/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-set-map'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/set-map'],
 };
