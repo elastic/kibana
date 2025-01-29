58a744b1ddb6356f3b52bc50c5bb0a7a1fe3cc69
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:52:16 +0100

    Relocating module `@kbn/dev-proc-runner`

--- src/dev/packages/dev-proc-runner/jest.config.js
+++ src/dev/packages/dev-proc-runner/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-dev-proc-runner'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/dev-proc-runner'],
 };
