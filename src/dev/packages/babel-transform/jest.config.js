dd254b5f153dabf086e42ff6df6b01438d755a14
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 11:18:55 +0100

    Relocating module `@kbn/babel-transform`

--- src/dev/packages/babel-transform/jest.config.js
+++ src/dev/packages/babel-transform/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-babel-transform'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/babel-transform'],
 };
