c5a55b276f377834ed0dcac9707c9b9b20f85c32
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:57:56 +0100

    Relocating module `@kbn/generate-console-definitions`

--- src/dev/packages/generate-console-definitions/jest.config.js
+++ src/dev/packages/generate-console-definitions/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-generate-console-definitions'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/generate-console-definitions'],
 };
