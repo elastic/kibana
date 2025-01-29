fa4dbed1ee5f11789638b70191a4e1ee49ffe177
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:00:57 +0100

    Relocating module `@kbn/managed-vscode-config-cli`

--- src/dev/packages/managed-vscode-config-cli/jest.config.js
+++ src/dev/packages/managed-vscode-config-cli/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-managed-vscode-config-cli'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/managed-vscode-config-cli'],
 };
