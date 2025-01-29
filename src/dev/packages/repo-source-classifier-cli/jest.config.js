b9753c5d7e4dc177b23281c1b2c0c8617927e194
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 10:06:40 +0100

    Relocating module `@kbn/repo-source-classifier-cli`

--- src/dev/packages/repo-source-classifier-cli/jest.config.js
+++ src/dev/packages/repo-source-classifier-cli/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-repo-source-classifier-cli'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/repo-source-classifier-cli'],
 };
