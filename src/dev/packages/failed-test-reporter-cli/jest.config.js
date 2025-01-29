cd56d0c98bb60649eb4724a20bc0bdc00570ddb8
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-01-29 09:56:02 +0100

    Relocating module `@kbn/failed-test-reporter-cli`

--- src/dev/packages/failed-test-reporter-cli/jest.config.js
+++ src/dev/packages/failed-test-reporter-cli/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-failed-test-reporter-cli'],
+  rootDir: '../../../..',
+  roots: ['<rootDir>/src/dev/packages/failed-test-reporter-cli'],
 };
