4686d18b62aa067312e0c0b7779c5d5e21f7c7e7
Nathan Reese <reese.nathan@gmail.com>
2025-01-29 13:27:44 -0700

    remove blank lines

--- test/functional/apps/dashboard/group1/dashboard_unsaved_listing.ts
+++ test/functional/apps/dashboard/group1/dashboard_unsaved_listing.ts
@@ -11,8 +11,6 @@ import expect from '@kbn/expect';
 
 import { FtrProviderContext } from '../../../ftr_provider_context';
 
-
-
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const { dashboard, header } = getPageObjects(['dashboard', 'header']);
   const kibanaServer = getService('kibanaServer');
