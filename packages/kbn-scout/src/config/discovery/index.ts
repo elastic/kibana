fcf7345b267fdb055c7f3e4f374ed6f80214b3be
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-01-30 09:42:15 +0100

    [scout] add script to discover playwright configs in repo (#208733)
    ## Summary
    
    Adding script to discover Scout playwright tests in Kibana repo, will be
    used to build CI pipeline step (running tests per plugin in a separate
    worker for the start). We can also consider using it to decide if code
    change should trigger only specific plugin tests to run.
    
    Usage:
    ```
    node scripts/scout.js discover-playwright-configs --searchPaths x-pack/platform/plugins/private/discover_enhanced
    ```
    
    Output:
    ```
     info Searching for playwright config files in the following paths:
     info - x-pack/platform/plugins/private/discover_enhanced/**/ui_tests/{playwright.config.ts,parallel.playwright.config.ts}
     info
     info Discovered playwright config files in '1' plugins
     info [discover_enhanced] plugin:
     info - x-pack/platform/plugins/private/discover_enhanced/ui_tests/parallel.playwright.config.ts
     info - x-pack/platform/plugins/private/discover_enhanced/ui_tests/playwright.config.ts
    ```
    
    More usage examples:
    ```
    node scripts/scout.js discover-playwright-configs // by default will search in ['src/platform/plugins', 'x-pack/**/plugins'] and return all existing ones
    node scripts/scout.js discover-playwright-configs --searchPaths x-pack/platform // platform ones under x-pack
    node scripts/scout.js discover-playwright-configs --searchPaths x-pack/** // all under x-pack
    ```
    
    (cherry picked from commit cf7debdfa3fb4a253ac0ec1b43e588e5163f8a98)

--- packages/kbn-scout/src/config/discovery/index.ts
+++ packages/kbn-scout/src/config/discovery/index.ts
@@ -7,16 +7,4 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { ScoutLogger } from '../fixtures/worker';
-
-export const measurePerformance = async <T>(
-  log: ScoutLogger,
-  label: string,
-  fn: () => Promise<T>
-): Promise<T> => {
-  const startTime = performance.now();
-  const result = await fn();
-  const duration = performance.now() - startTime;
-  log.debug(`${label} took ${duration.toFixed(2)}ms`);
-  return result;
-};
+export * from './search_configs';
