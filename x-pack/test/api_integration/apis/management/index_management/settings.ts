56205eba4cc61be6bfcd53d438def91b25d298c1
Tiago Costa <tiago.costa@elastic.co>
2025-01-30 17:20:57 +0000

    Revert "skip failing es promotion (#208873)"
    This reverts commit 60d1deeb62d2684d6259211c55180c35040cb5bb.

--- x-pack/test/api_integration/apis/management/index_management/settings.ts
+++ x-pack/test/api_integration/apis/management/index_management/settings.ts
@@ -15,8 +15,7 @@ export default function ({ getService }: FtrProviderContext) {
   const { createIndex, deleteAllIndices } = indicesHelpers(getService);
   const { getIndexSettings, updateIndexSettings } = settingsApi(getService);
 
-  // FAILING ES PROMOTION: https://github.com/elastic/kibana/issues/208873
-  describe.skip('settings', () => {
+  describe('settings', () => {
     after(async () => await deleteAllIndices());
 
     it('should fetch an index settings', async () => {
