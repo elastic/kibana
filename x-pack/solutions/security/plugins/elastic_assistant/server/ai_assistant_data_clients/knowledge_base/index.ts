1935cedeaae0290af3910a15033546c3625d0d99
Patryk Kopyciński <contact@patrykkopycinski.com>
2025-01-30 01:00:33 +0100

    [Security Assistant] Switch to use default inference endpoint (#208668)
    ## Summary
    
    In 8.17 we have introduced `semantic_text`
    https://github.com/elastic/kibana/pull/197007 which required dedicated
    inference endpoint.
    As we now have default `.elser-2-elasticsearch` inference endpoint
    available we want to migrate it out, but it's not possible to just
    override `inference_id` mapping for the Knowledge Base data stream, so
    instead we decided to first update the mapping by adding
    `search_inference_id` pointing to the `.elser-2-elasticsearch` (to make
    sure the data is queryable without the dedicated endpoint). Then we
    update the Data Stream mapping to use the default endpoint and after
    that we rollover the DS index to make sure new index is created and new
    inference endpoint is used for new Knowledge Base data ingestion.
    
    Will add testing steps soon

--- x-pack/solutions/security/plugins/elastic_assistant/server/ai_assistant_data_clients/knowledge_base/index.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/server/ai_assistant_data_clients/knowledge_base/index.ts
@@ -59,7 +59,10 @@ import {
   loadSecurityLabs,
   getSecurityLabsDocsCount,
 } from '../../lib/langchain/content_loaders/security_labs_loader';
-import { ASSISTANT_ELSER_INFERENCE_ID } from './field_maps_configuration';
+import {
+  ASSISTANT_ELSER_INFERENCE_ID,
+  ELASTICSEARCH_ELSER_INFERENCE_ID,
+} from './field_maps_configuration';
 import { BulkOperationError } from '../../lib/data_stream/documents_data_writer';
 import { AUDIT_OUTCOME, KnowledgeBaseAuditAction, knowledgeBaseAuditEvent } from './audit_events';
 
@@ -79,6 +82,7 @@ export interface KnowledgeBaseDataClientParams extends AIAssistantDataClientPara
   ingestPipelineResourceName: string;
   setIsKBSetupInProgress: (isInProgress: boolean) => void;
   manageGlobalKnowledgeBaseAIAssistant: boolean;
+  assistantDefaultInferenceEndpoint: boolean;
 }
 export class AIAssistantKnowledgeBaseDataClient extends AIAssistantDataClient {
   constructor(public readonly options: KnowledgeBaseDataClientParams) {
@@ -150,17 +154,44 @@ export class AIAssistantKnowledgeBaseDataClient extends AIAssistantDataClient {
     }
   };
 
+  public getInferenceEndpointId = async () => {
+    if (!this.options.assistantDefaultInferenceEndpoint) {
+      return ASSISTANT_ELSER_INFERENCE_ID;
+    }
+    const esClient = await this.options.elasticsearchClientPromise;
+
+    try {
+      const elasticsearchInference = await esClient.inference.get({
+        inference_id: ASSISTANT_ELSER_INFERENCE_ID,
+        task_type: 'sparse_embedding',
+      });
+
+      if (elasticsearchInference) {
+        return ASSISTANT_ELSER_INFERENCE_ID;
+      }
+    } catch (error) {
+      this.options.logger.debug(
+        `Error checking if Inference endpoint ${ASSISTANT_ELSER_INFERENCE_ID} exists: ${error}`
+      );
+    }
+
+    // Fallback to the dedicated inference endpoint
+    return ELASTICSEARCH_ELSER_INFERENCE_ID;
+  };
+
   /**
    * Checks if the inference endpoint is deployed and allocated in Elasticsearch
    *
    * @returns Promise<boolean> indicating whether the model is deployed
    */
-  public isInferenceEndpointExists = async (): Promise<boolean> => {
+  public isInferenceEndpointExists = async (inferenceEndpointId?: string): Promise<boolean> => {
+    const inferenceId = inferenceEndpointId || (await this.getInferenceEndpointId());
+
     try {
       const esClient = await this.options.elasticsearchClientPromise;
 
       const inferenceExists = !!(await esClient.inference.get({
-        inference_id: ASSISTANT_ELSER_INFERENCE_ID,
+        inference_id: inferenceId,
         task_type: 'sparse_embedding',
       }));
       if (!inferenceExists) {
@@ -181,7 +212,7 @@ export class AIAssistantKnowledgeBaseDataClient extends AIAssistantDataClient {
           (node) => node.routing_state.routing_state === 'started'
         );
 
-      return getResponse.trained_model_stats?.some(
+      return !!getResponse.trained_model_stats?.some(
         (stats) => isReadyESS(stats) || isReadyServerless(stats)
       );
     } catch (error) {
@@ -196,46 +227,57 @@ export class AIAssistantKnowledgeBaseDataClient extends AIAssistantDataClient {
     const elserId = await this.options.getElserId();
     this.options.logger.debug(`Deploying ELSER model '${elserId}'...`);
     const esClient = await this.options.elasticsearchClientPromise;
+    const inferenceId = await this.getInferenceEndpointId();
+    const inferenceExists = await this.isInferenceEndpointExists(inferenceId);
 
-    try {
-      await esClient.inference.delete({
-        inference_id: ASSISTANT_ELSER_INFERENCE_ID,
-        // it's being used in the mapping so we need to force delete
-        force: true,
-      });
-      this.options.logger.debug(`Deleted existing inference endpoint for ELSER model '${elserId}'`);
-    } catch (error) {
-      this.options.logger.error(
-        `Error deleting inference endpoint for ELSER model '${elserId}':\n${error}`
-      );
-    }
+    // Don't try to create the inference endpoint for ELASTICSEARCH_ELSER_INFERENCE_ID
+    if (inferenceId === ASSISTANT_ELSER_INFERENCE_ID) {
+      if (inferenceExists) {
+        try {
+          await esClient.inference.delete({
+            inference_id: ASSISTANT_ELSER_INFERENCE_ID,
+            // it's being used in the mapping so we need to force delete
+            force: true,
+          });
+          this.options.logger.debug(
+            `Deleted existing inference endpoint for ELSER model '${elserId}'`
+          );
+        } catch (error) {
+          this.options.logger.error(
+            `Error deleting inference endpoint for ELSER model '${elserId}':\n${error}`
+          );
+        }
+      }
 
-    try {
-      await esClient.inference.put({
-        task_type: 'sparse_embedding',
-        inference_id: ASSISTANT_ELSER_INFERENCE_ID,
-        inference_config: {
-          service: 'elasticsearch',
-          service_settings: {
-            adaptive_allocations: {
-              enabled: true,
-              min_number_of_allocations: 0,
-              max_number_of_allocations: 8,
+      try {
+        await esClient.inference.put({
+          task_type: 'sparse_embedding',
+          inference_id: ASSISTANT_ELSER_INFERENCE_ID,
+          inference_config: {
+            service: 'elasticsearch',
+            service_settings: {
+              adaptive_allocations: {
+                enabled: true,
+                min_number_of_allocations: 0,
+                max_number_of_allocations: 8,
+              },
+              num_threads: 1,
+              model_id: elserId,
             },
-            num_threads: 1,
-            model_id: elserId,
+            task_settings: {},
           },
-          task_settings: {},
-        },
-      });
+        });
 
-      // await for the model to be deployed
-      await this.isInferenceEndpointExists();
-    } catch (error) {
-      this.options.logger.error(
-        `Error creating inference endpoint for ELSER model '${elserId}':\n${error}`
-      );
-      throw new Error(`Error creating inference endpoint for ELSER model '${elserId}':\n${error}`);
+        // await for the model to be deployed
+        await this.isInferenceEndpointExists(inferenceId);
+      } catch (error) {
+        this.options.logger.error(
+          `Error creating inference endpoint for ELSER model '${elserId}':\n${error}`
+        );
+        throw new Error(
+          `Error creating inference endpoint for ELSER model '${elserId}':\n${error}`
+        );
+      }
     }
   };
 
