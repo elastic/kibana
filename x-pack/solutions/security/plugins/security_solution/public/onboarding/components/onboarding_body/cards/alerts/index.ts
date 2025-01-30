96f9c0752a23aea9b0105f57018d43de0430ca41
Agustina Nahir Ruidiaz <agustina.ruidiaz@elastic.co>
2025-01-30 12:46:51 +0100

    WIP

--- x-pack/solutions/security/plugins/security_solution/public/onboarding/components/onboarding_body/cards/alerts/index.ts
+++ x-pack/solutions/security/plugins/security_solution/public/onboarding/components/onboarding_body/cards/alerts/index.ts
@@ -10,11 +10,21 @@ import type { OnboardingCardConfig } from '../../../../types';
 import { OnboardingCardId } from '../../../../constants';
 import { ALERTS_CARD_TITLE } from './translations';
 import alertsIcon from './images/alerts_icon.png';
+import alertsIconDark from './images/alerts_icon_dark.png';
+import { useEuiTheme, EuiIcon } from '@elastic/eui';
+
+const AlertsIconComponent = () => {
+  const { colorMode } = useEuiTheme();
+  const isDark = colorMode === 'DARK';
+  const icon = isDark ? alertsIconDark : alertsIcon;
+
+  return <img src={icon} alt="Alerts Icon" width={24} height={24} />;
+};
 
 export const alertsCardConfig: OnboardingCardConfig = {
   id: OnboardingCardId.alerts,
   title: ALERTS_CARD_TITLE,
-  icon: alertsIcon,
+  icon: AlertsIconComponent,
   Component: React.lazy(
     () =>
       import(
