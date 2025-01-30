92fc287096af0e05362683e4388c6a88fa986f52
Tiago Costa <tiago.costa@elastic.co>
2025-01-30 20:40:29 +0000

    skip flaky es promotion suites (#163254)

--- src/core/server/integration_tests/saved_objects/migrations/group3/read_batch_size.test.ts
+++ src/core/server/integration_tests/saved_objects/migrations/group3/read_batch_size.test.ts
@@ -19,7 +19,8 @@ import { getFips } from 'crypto';
 
 const logFilePath = join(__dirname, 'read_batch_size.log');
 
-describe('migration v2 - read batch size', () => {
+// FAILING ES PROMOTION: https://github.com/elastic/kibana/issues/163254
+describe.skip('migration v2 - read batch size', () => {
   let esServer: TestElasticsearchUtils;
   let root: Root;
   let logs: string;
