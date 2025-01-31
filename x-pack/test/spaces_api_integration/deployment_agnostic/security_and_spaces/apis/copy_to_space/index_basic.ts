9120c36e165a40e26b08c07a892a3bd70f0a05f5
Elena Shostak <165678770+elena-shostak@users.noreply.github.com>
2025-01-31 13:56:29 +0100

    [FTR] Skipped basic license tests for FIPS (#208916)
    ## Summary
    All tests in
    `deployment_agnostic/security_and_spaces/stateful.config_basic.ts` and
    `deployment_agnostic/security_and_spaces/stateful.copy_to_space.config_basic.ts`
    are intended to be run only with `basic` license, since FIPS overrides
    it we need to skip that test for FIPS.
    
    Separated index entries for `basic` and `trial` license, so tests with
    trial config would still run.
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/test/spaces_api_integration/deployment_agnostic/security_and_spaces/apis/copy_to_space/index_basic.ts
+++ x-pack/test/spaces_api_integration/deployment_agnostic/security_and_spaces/apis/copy_to_space/index_basic.ts
@@ -9,20 +9,13 @@ import { createUsersAndRoles } from '../../../../common/lib/create_users_and_rol
 import type { DeploymentAgnosticFtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ loadTestFile, getService }: DeploymentAgnosticFtrProviderContext) {
-  const config = getService('config');
-  const license = config.get('esTestCluster.license');
   const es = getService('es');
   const supertest = getService('supertest');
-  const isServerless = config.get('serverless');
 
   describe('spaces api with security', function () {
-    // Should be enabled when custom roles can be provisioned for MKI
-    // See: https://github.com/elastic/kibana/issues/207361
-    this.tags('skipMKI');
+    this.tags('skipFIPS');
     before(async () => {
-      if (license === 'basic' && !isServerless) {
-        await createUsersAndRoles(es, supertest);
-      }
+      await createUsersAndRoles(es, supertest);
     });
     loadTestFile(require.resolve('./copy_to_space'));
   });
