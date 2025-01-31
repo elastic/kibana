c4938931e312835e3a5bee8404b8c8853c5ccc44
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2025-02-01 02:58:43 +1100

    [9.0] [SIEM Migration] Add Telemetry (#11588) (#208932) (#209115)
    # Backport
    
    This will backport the following commits from `main` to `9.0`:
    - [[SIEM Migration] Add Telemetry (#11588)
    (#208932)](https://github.com/elastic/kibana/pull/208932)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Ievgen
    Sorokopud","email":"ievgen.sorokopud@elastic.co"},"sourceCommit":{"committedDate":"2025-01-31T13:56:04Z","message":"[SIEM
    Migration] Add Telemetry (#11588) (#208932)\n\n## Summary\r\n\r\nThese
    changes add next UI telemetries:\r\n\r\n* Update migration rule
    action\r\n* Install single migration rule action\r\n* Bulk install
    migration rules
    action","sha":"31b010d375ba217ae2eebfb1053e4af22a6f6c92","branchLabelMapping":{"^v9.1.0$":"main","^v8.19.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","v9.0.0","Team:Threat
    Hunting","Team:
    SecuritySolution","backport:prev-minor","v8.18.0","v9.1.0"],"title":"[SIEM
    Migration] Add Telemetry
    (#11588)","number":208932,"url":"https://github.com/elastic/kibana/pull/208932","mergeCommit":{"message":"[SIEM
    Migration] Add Telemetry (#11588) (#208932)\n\n## Summary\r\n\r\nThese
    changes add next UI telemetries:\r\n\r\n* Update migration rule
    action\r\n* Install single migration rule action\r\n* Bulk install
    migration rules
    action","sha":"31b010d375ba217ae2eebfb1053e4af22a6f6c92"}},"sourceBranch":"main","suggestedTargetBranches":["9.0","8.18"],"targetPullRequestStates":[{"branch":"9.0","label":"v9.0.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.18","label":"v8.18.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/208932","number":208932,"mergeCommit":{"message":"[SIEM
    Migration] Add Telemetry (#11588) (#208932)\n\n## Summary\r\n\r\nThese
    changes add next UI telemetries:\r\n\r\n* Update migration rule
    action\r\n* Install single migration rule action\r\n* Bulk install
    migration rules
    action","sha":"31b010d375ba217ae2eebfb1053e4af22a6f6c92"}}]}]
    BACKPORT-->
    
    Co-authored-by: Ievgen Sorokopud <ievgen.sorokopud@elastic.co>

--- x-pack/solutions/security/plugins/security_solution/public/siem_migrations/rules/logic/use_update_migration_rule.ts
+++ x-pack/solutions/security/plugins/security_solution/public/siem_migrations/rules/logic/use_update_migration_rule.ts
@@ -6,7 +6,11 @@
  */
 
 import { useMutation } from '@tanstack/react-query';
-import type { UpdateRuleMigrationData } from '../../../../common/siem_migrations/model/rule_migration.gen';
+import { useCallback } from 'react';
+import type {
+  RuleMigration,
+  UpdateRuleMigrationData,
+} from '../../../../common/siem_migrations/model/rule_migration.gen';
 import { SIEM_RULE_MIGRATIONS_PATH } from '../../../../common/siem_migrations/constants';
 import type { UpdateRuleMigrationResponse } from '../../../../common/siem_migrations/model/api/rules/rule_migration.gen';
 import { useAppToasts } from '../../../common/hooks/use_app_toasts';
@@ -14,21 +18,39 @@ import * as i18n from './translations';
 import { useInvalidateGetMigrationRules } from './use_get_migration_rules';
 import { useInvalidateGetMigrationTranslationStats } from './use_get_migration_translation_stats';
 import { updateMigrationRules } from '../api';
+import { useTranslatedRuleTelemetry } from '../hooks/use_translated_rule_telemetry';
 
-export const UPDATE_MIGRATION_RULES_MUTATION_KEY = ['PUT', SIEM_RULE_MIGRATIONS_PATH];
+export const UPDATE_MIGRATION_RULE_MUTATION_KEY = ['PUT', SIEM_RULE_MIGRATIONS_PATH];
 
-export const useUpdateMigrationRules = (migrationId: string) => {
+export const useUpdateMigrationRule = (ruleMigration: RuleMigration) => {
   const { addError } = useAppToasts();
+
+  const migrationId = ruleMigration.migration_id;
+
+  const { reportTranslatedRuleUpdate } = useTranslatedRuleTelemetry();
+  const reportTelemetry = useCallback(
+    (error?: Error) => {
+      reportTranslatedRuleUpdate({
+        migrationId,
+        ruleMigrationId: ruleMigration.id,
+        result: error ? 'failed' : 'success',
+        errorMessage: error?.message,
+      });
+    },
+    [migrationId, reportTranslatedRuleUpdate, ruleMigration.id]
+  );
 
   const invalidateGetRuleMigrations = useInvalidateGetMigrationRules();
   const invalidateGetMigrationTranslationStats = useInvalidateGetMigrationTranslationStats();
 
-  return useMutation<UpdateRuleMigrationResponse, Error, UpdateRuleMigrationData[]>(
-    (rulesToUpdate) => updateMigrationRules({ rulesToUpdate }),
+  return useMutation<UpdateRuleMigrationResponse, Error, UpdateRuleMigrationData>(
+    (ruleUpdateData) => updateMigrationRules({ rulesToUpdate: [ruleUpdateData] }),
     {
-      mutationKey: UPDATE_MIGRATION_RULES_MUTATION_KEY,
+      mutationKey: UPDATE_MIGRATION_RULE_MUTATION_KEY,
+      onSuccess: () => reportTelemetry(),
       onError: (error) => {
         addError(error, { title: i18n.UPDATE_MIGRATION_RULES_FAILURE });
+        reportTelemetry(error);
       },
       onSettled: () => {
         invalidateGetRuleMigrations(migrationId);
