fd1f63317e511bd0d29dc9c57f98da2bf1ed01ea
Julia <iuliia.guskova@elastic.co>
2025-01-31 16:51:08 +0100

    initial commit

--- x-pack/platform/plugins/shared/alerting/public/pages/maintenance_windows/components/table_actions_popover.test.tsx
+++ x-pack/platform/plugins/shared/alerting/public/pages/maintenance_windows/components/table_actions_popover.test.tsx
@@ -5,19 +5,52 @@
  * 2.0.
  */
 
-import { fireEvent } from '@testing-library/react';
+import { fireEvent, waitFor } from '@testing-library/react';
 import React from 'react';
+import { notificationServiceMock } from '@kbn/core-notifications-browser-mocks';
 
 import { AppMockRenderer, createAppMockRenderer } from '../../../lib/test_utils';
 import { TableActionsPopover } from './table_actions_popover';
 import { MaintenanceWindowStatus } from '../../../../common';
 
+const mockAddSuccess = jest.fn();
+jest.mock('../../../utils/kibana_react', () => {
+  const originalModule = jest.requireActual('../../../utils/kibana_react');
+  return {
+    ...originalModule,
+    useKibana: () => {
+      const { services } = originalModule.useKibana();
+      return {
+        services: {
+          ...services,
+          notifications: { toasts: { addSuccess: mockAddSuccess } },
+        },
+      };
+    },
+  };
+});
+
 describe('TableActionsPopover', () => {
   let appMockRenderer: AppMockRenderer;
+  const originalClipboard = global.window.navigator.clipboard;
+  // const notifications = notificationServiceMock.createStartContract();
+  // const addSuccessToastSpy = jest.spyOn(notifications.toasts, 'addSuccess');
 
   beforeEach(() => {
     jest.clearAllMocks();
+    Object.defineProperty(navigator, 'clipboard', {
+      value: {
+        writeText: jest.fn().mockImplementation(() => Promise.resolve()),
+      },
+      writable: true,
+    });
     appMockRenderer = createAppMockRenderer();
+  });
+
+  afterEach(() => {
+    Object.defineProperty(navigator, 'clipboard', {
+      value: originalClipboard,
+    });
   });
 
   test('it renders', () => {
@@ -102,5 +135,28 @@ describe('TableActionsPopover', () => {
     );
     fireEvent.click(result.getByTestId('table-actions-icon-button'));
     expect(result.getByTestId('table-actions-unarchive')).toBeInTheDocument();
+  });
+
+  test('it shows the success toast when a copied maintenance window id', async () => {
+    const result = appMockRenderer.render(
+      <TableActionsPopover
+        id={'123'}
+        isLoading={false}
+        status={MaintenanceWindowStatus.Archived}
+        onEdit={() => {}}
+        onCancel={() => {}}
+        onArchive={() => {}}
+        onCancelAndArchive={() => {}}
+      />
+    );
+
+    fireEvent.click(result.getByTestId('table-actions-icon-button'));
+    expect(result.getByTestId('table-actions-copy-id')).toBeInTheDocument();
+    fireEvent.click(result.getByTestId('table-actions-copy-id'));
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith('123');
+
+    await waitFor(() => {
+      expect(mockAddSuccess).toBeCalledWith('Copied maintenance window ID to clipboard');
+    });
   });
 });
