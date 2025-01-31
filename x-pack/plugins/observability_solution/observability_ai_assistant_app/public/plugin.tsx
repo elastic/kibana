60477b0824b830f3fee1f9e2ead7c471d03ab84f
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2025-02-01 08:50:40 +1100

    [8.16] [Obs AI Assistant] Fix multiple flyouts (#209158) (#209167)
    # Backport
    
    This will backport the following commits from `main` to `8.16`:
    - [[Obs AI Assistant] Fix multiple flyouts
    (#209158)](https://github.com/elastic/kibana/pull/209158)
    
    <!--- Backport version: 9.4.3 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sqren/backport)
    
    <!--BACKPORT [{"author":{"name":"Viduni
    Wickramarachchi","email":"viduni.wickramarachchi@elastic.co"},"sourceCommit":{"committedDate":"2025-01-31T19:59:01Z","message":"[Obs
    AI Assistant] Fix multiple flyouts
    (#209158)","sha":"50b8776b1fb6e7e6e2a96a8b61edc499579bf260","branchLabelMapping":{"^v9.1.0$":"main","^v8.19.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:fix","v9.0.0","Team:Obs
    AI
    Assistant","backport:version","v8.18.0","v8.16.4","v8.17.2","v9.1.0"],"title":"[Obs
    AI Assistant] Fix multiple
    flyouts","number":209158,"url":"https://github.com/elastic/kibana/pull/209158","mergeCommit":{"message":"[Obs
    AI Assistant] Fix multiple flyouts
    (#209158)","sha":"50b8776b1fb6e7e6e2a96a8b61edc499579bf260"}},"sourceBranch":"main","suggestedTargetBranches":["9.0","8.18","8.16","8.17"],"targetPullRequestStates":[{"branch":"9.0","label":"v9.0.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.18","label":"v8.18.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.16","label":"v8.16.4","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.17","label":"v8.17.2","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/209158","number":209158,"mergeCommit":{"message":"[Obs
    AI Assistant] Fix multiple flyouts
    (#209158)","sha":"50b8776b1fb6e7e6e2a96a8b61edc499579bf260"}}]}]
    BACKPORT-->
    
    Co-authored-by: Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>

--- x-pack/plugins/observability_solution/observability_ai_assistant_app/public/plugin.tsx
+++ x-pack/plugins/observability_solution/observability_ai_assistant_app/public/plugin.tsx
@@ -116,7 +116,9 @@ export class ObservabilityAIAssistantAppPlugin
             () => {}
           );
 
-          return () => {};
+          return () => {
+            ReactDOM.unmountComponentAtNode(element);
+          };
         },
         // right before the user profile
         order: 1001,
