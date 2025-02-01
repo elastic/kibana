07a3eec482b1d3eee929ce0d9dcff080940f3aa8
Tiago Costa <tiago.costa@elastic.co>
2025-02-01 01:42:18 +0000

    skip flaky suite (#209064)

--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/response_actions/document_signing.cy.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/response_actions/document_signing.cy.ts
@@ -24,6 +24,7 @@ import { deleteAllLoadedEndpointData } from '../../tasks/delete_all_endpoint_dat
 
 // Failing: https://github.com/elastic/kibana/issues/209063
 // Failing: See https://github.com/elastic/kibana/issues/209063
+// FLAKY: https://github.com/elastic/kibana/issues/209064
 describe.skip('Document signing:', { tags: ['@ess', '@serverless', '@brokenInServerless'] }, () => {
   let indexedPolicy: IndexedFleetEndpointPolicyResponse;
   let policy: PolicyData;
