ca0013b0fb0659f879c2e29d13c0882eb972a8bb
lgestc <lukasz.gmys@elastic.co>
2025-02-03 09:03:56 +0100

    add test for the dataview fallback hook

--- x-pack/solutions/security/plugins/security_solution/public/sourcerer/components/use_data_view_fallback.test.ts
+++ x-pack/solutions/security/plugins/security_solution/public/sourcerer/components/use_data_view_fallback.test.ts
@@ -5,6 +5,72 @@
  * 2.0.
  */
 
+import { act, renderHook } from '@testing-library/react';
+import { useDataViewFallback } from './use_data_view_fallback';
+import { useCreateAdhocDataView } from './use_create_adhoc_data_view';
+import type { DataView } from '@kbn/data-views-plugin/common';
+
+jest.mock('./use_create_adhoc_data_view');
+
+const mockDataView = {
+  id: 'mock',
+  getIndexPattern: () => 'pattern1,pattern2',
+} as unknown as DataView;
+
 describe('useDataViewFallback', () => {
-  it.todo('should create adhoc data views when enabled', () => {});
+  let onResolveErrorManually: VoidFunction;
+  let onApplyFallbackDataView: VoidFunction;
+
+  beforeEach(() => {
+    onResolveErrorManually = jest.fn();
+    onApplyFallbackDataView = jest.fn();
+    jest.mocked(useCreateAdhocDataView).mockReturnValue({
+      createAdhocDataView: jest.fn(async (): Promise<DataView | null> => mockDataView),
+    });
+  });
+
+  it('should not create an adhoc data view when `enableFallback` is false', async () => {
+    renderHook(() =>
+      useDataViewFallback({
+        onResolveErrorManually,
+        missingPatterns: ['pattern1'],
+        enableFallback: false,
+        onApplyFallbackDataView,
+      })
+    );
+
+    expect(useCreateAdhocDataView(() => {}).createAdhocDataView).not.toHaveBeenCalled();
+  });
+
+  it('should create adhoc data views when `enableFallback` and `missingPatterns` are provided', async () => {
+    renderHook(() =>
+      useDataViewFallback({
+        onResolveErrorManually,
+        missingPatterns: ['pattern1'],
+        enableFallback: true,
+        onApplyFallbackDataView,
+      })
+    );
+
+    expect(useCreateAdhocDataView(() => {}).createAdhocDataView).toHaveBeenCalledWith(['pattern1']);
+  });
+
+  it('should apply fallback data view once when a data view is successfully created', async () => {
+    const result = renderHook(() =>
+      useDataViewFallback({
+        onResolveErrorManually,
+        missingPatterns: ['pattern1'],
+        enableFallback: true,
+        onApplyFallbackDataView,
+      })
+    );
+
+    // NOTE: test if  multiple renders with the same props result with a single dataview selection call
+    await act(async () => result.rerender());
+    await act(async () => result.rerender());
+    await act(async () => result.rerender());
+
+    expect(onApplyFallbackDataView).toHaveBeenCalledWith('mock', ['pattern1', 'pattern2'], false);
+    expect(onApplyFallbackDataView).toHaveBeenCalledTimes(1);
+  });
 });
