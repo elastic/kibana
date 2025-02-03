9546b87787bfc74abf648c72df15f4729eabe257
Nathan Reese <reese.nathan@gmail.com>
2025-02-03 11:38:56 -0700

    lazy load Add image action

--- src/platform/plugins/private/image_embeddable/public/components/image_editor/image_editor_flyout.tsx
+++ src/platform/plugins/private/image_embeddable/public/components/image_editor/image_editor_flyout.tsx
@@ -30,17 +30,18 @@ import {
   useEuiTheme,
 } from '@elastic/eui';
 import React, { useState } from 'react';
+import useAsync from 'react-use/lib/useAsync';
 import { css } from '@emotion/react';
 import { FileUpload } from '@kbn/shared-ux-file-upload';
 import { FilePicker } from '@kbn/shared-ux-file-picker';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
-import type { AuthenticatedUser } from '@kbn/security-plugin/common';
 import { FileImageMetadata, imageEmbeddableFileKind } from '../../imports';
 import { ImageConfig } from '../../types';
 import { ImageViewer } from '../image_viewer/image_viewer'; // use eager version to avoid flickering
 import { validateImageConfig, DraftImageConfig } from '../../utils/validate_image_config';
 import { useImageViewerContext } from '../image_viewer/image_viewer_context';
+import { coreServices } from '../../services/kibana_services';
 
 /**
  * Shared sizing css for image, upload placeholder, empty and not found state
@@ -57,13 +58,17 @@ export interface ImageEditorFlyoutProps {
   onCancel: () => void;
   onSave: (imageConfig: ImageConfig) => void;
   initialImageConfig?: ImageConfig;
-  user?: AuthenticatedUser;
 }
 
 export function ImageEditorFlyout(props: ImageEditorFlyoutProps) {
   const isEditing = !!props.initialImageConfig;
   const { euiTheme } = useEuiTheme();
   const { validateUrl } = useImageViewerContext();
+
+  const { value: userProfileId } = useAsync(async () => {
+    const user = await coreServices.security.authc.getCurrentUser();
+    return user ? user.profile_uid : undefined;
+  }, []);
 
   const [fileId, setFileId] = useState<undefined | string>(() =>
     props.initialImageConfig?.src?.type === 'file' ? props.initialImageConfig.src.fileId : undefined
@@ -425,9 +430,7 @@ export function ImageEditorFlyout(props: ImageEditorFlyoutProps) {
       {isFilePickerOpen && (
         <FilePicker
           kind={imageEmbeddableFileKind.id}
-          shouldAllowDelete={(file) =>
-            props.user ? props.user.profile_uid === file.user?.id : false
-          }
+          shouldAllowDelete={(file) => userProfileId === file.user?.id}
           multiple={false}
           onClose={() => {
             setIsFilePickerOpen(false);
