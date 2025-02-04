700f133b4f7ad60b6c6b466c02f2485213332661
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2025-02-04 12:24:28 +0530

    Add redirect for Logs Settings page to Kibana Advance Settings

--- x-pack/solutions/observability/plugins/infra/public/pages/logs/page_content.tsx
+++ x-pack/solutions/observability/plugins/infra/public/pages/logs/page_content.tsx
@@ -16,6 +16,7 @@ import {
   type ObservabilityOnboardingLocatorParams,
   OBSERVABILITY_ONBOARDING_LOCATOR,
 } from '@kbn/deeplinks-observability';
+import { MANAGEMENT_APP_LOCATOR } from '@kbn/deeplinks-management/constants';
 import { dynamic } from '@kbn/shared-ux-utility';
 import { type LogsLocatorParams, LOGS_LOCATOR_ID } from '@kbn/logs-shared-plugin/common';
 import { LazyAlertDropdownWrapper } from '../../alerting/log_threshold';
@@ -40,6 +41,9 @@ export const LogsPageContent: React.FunctionComponent = () => {
   const onboardingLocator = share?.url.locators.get<ObservabilityOnboardingLocatorParams>(
     OBSERVABILITY_ONBOARDING_LOCATOR
   );
+
+  const managementLocator = share?.url.locators.get(MANAGEMENT_APP_LOCATOR);
+
   const { setHeaderActionMenu, theme$ } = useContext(HeaderActionMenuContext);
 
   useReadOnlyBadge(!uiCapabilities?.logs?.save);
@@ -75,6 +79,18 @@ export const LogsPageContent: React.FunctionComponent = () => {
           exact
           render={() => {
             share.url.locators.get<LogsLocatorParams>(LOGS_LOCATOR_ID)?.navigate({});
+
+            return null;
+          }}
+        />
+        <Route
+          path="/settings"
+          exact
+          render={() => {
+            managementLocator?.navigate({
+              sectionId: 'kibana',
+              appId: 'settings?query=observability%3AlogSources',
+            });
 
             return null;
           }}
