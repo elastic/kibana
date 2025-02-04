e0d7964b18f1434268fd6af36cd4439c20e3ac51
iblancof <irene.blanco@elastic.co>
2025-02-04 12:38:26 +0100

    Refactor doc viewer to use UnifiedDocViewerTracesOverview and remove unused attribute IDs

--- src/platform/plugins/shared/discover/public/context_awareness/profile_providers/observability/traces_data_source_profile/accessors/get_doc_viewer.tsx
+++ src/platform/plugins/shared/discover/public/context_awareness/profile_providers/observability/traces_data_source_profile/accessors/get_doc_viewer.tsx
@@ -7,18 +7,9 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import {
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiHorizontalRule,
-  EuiPanel,
-  EuiSpacer,
-  EuiTitle,
-} from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
-import { spanAttributeIds, transactionAttributeIds } from '../resources/attribute_ids';
-import { getAttributeConfiguration } from '../resources/get_attribute_configuration';
+import { UnifiedDocViewerTracesOverview } from '@kbn/unified-doc-viewer-plugin/public';
 
 export const getDocViewer =
   (prev, { context }) =>
@@ -37,7 +28,6 @@ export const getDocViewer =
     }
     const parentId = params.record.flattened['parent.id'];
     const documentName = parentId ? 'Span' : 'Transaction'; // TODO: use i18n (?)
-    const isTransaction = !parentId;
 
     return {
       title: `Record #${recordId}`,
@@ -48,38 +38,8 @@ export const getDocViewer =
             defaultMessage: 'Overview',
           })}`,
           order: 0,
-          component: () => {
-            const panelTitle = `${documentName}  ${i18n.translate(
-              'discover.docViews.tracesOverview.details.title',
-              {
-                defaultMessage: 'detail',
-              }
-            )}`;
-
-            return (
-              <EuiPanel color="transparent" hasShadow={false} paddingSize="none">
-                <EuiSpacer size="m" />
-                <EuiTitle size="s">
-                  <h1>{panelTitle}</h1>
-                </EuiTitle>
-                <EuiSpacer size="m" />
-                {(isTransaction ? transactionAttributeIds : spanAttributeIds).map((attributeId) => (
-                  <>
-                    <EuiFlexGroup>
-                      <EuiFlexItem>
-                        <EuiTitle size="xxxs">
-                          <h3>{getAttributeConfiguration(params)[attributeId].title}</h3>
-                        </EuiTitle>
-                      </EuiFlexItem>
-                      <EuiFlexItem>
-                        {getAttributeConfiguration(params)[attributeId].content}
-                      </EuiFlexItem>
-                    </EuiFlexGroup>
-                    <EuiHorizontalRule margin="xs" />
-                  </>
-                ))}
-              </EuiPanel>
-            );
+          component: (props) => {
+            return <UnifiedDocViewerTracesOverview {...props} />;
           },
         });
 
