db4f79c18e384b7ea4c40b31d34ea314f897db55
Sergi Massaneda <sergi.massaneda@elastic.co>
2025-02-04 20:27:04 +0100

    new telemetry client

---
+++ x-pack/solutions/security/plugins/security_solution/public/siem_migrations/rules/service/telemetry.ts
@@ -0,0 +1,145 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { ActionConnector } from '@kbn/triggers-actions-ui-plugin/public';
+import type { SiemMigrationRetryFilter } from '../../../../common/siem_migrations/constants';
+import type { RuleMigration } from '../../../../common/siem_migrations/model/rule_migration.gen';
+import type { TelemetryServiceStart } from '../../../common/lib/telemetry';
+import type { BaseResultActionParams } from '../../../common/lib/telemetry/events/siem_migrations/types';
+import { SiemMigrationsEventTypes } from '../../../common/lib/telemetry/events/siem_migrations/types';
+
+export class SiemRulesMigrationsTelemetry {
+  constructor(private readonly telemetryService: TelemetryServiceStart) {}
+
+  private getBaseResultParams = (error: Error | undefined): BaseResultActionParams => ({
+    result: error ? 'failed' : 'success',
+    ...(error && { errorMessage: error.message }),
+  });
+
+  // Setup actions
+
+  reportConnectorSelected = (params: { connector: ActionConnector }) => {
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupConnectorSelected, {
+      connectorId: params.connector.id,
+      connectorType: params.connector.actionTypeId,
+    });
+  };
+
+  reportSetupMigrationOpen = (params: { isFirstMigration: boolean }) => {
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupMigrationOpenNew, params);
+  };
+
+  reportSetupMigrationOpenUpload = (params: {
+    migrationId: string;
+    missingResourcesCount: number;
+  }) => {
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupMigrationOpenUpload, params);
+  };
+
+  reportSetupRulesQueryCopied = (params: { connectorId: string }) => {
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupRulesQueryCopied, params);
+  };
+
+  reportSetupRulesUploaded = (params: { connectorId: string; count: number; error?: Error }) => {
+    const { connectorId, count, error } = params;
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupRulesUploaded, {
+      connectorId,
+      count,
+      ...this.getBaseResultParams(error),
+    });
+  };
+
+  reportSetupMacrosQueryCopied = (params: { migrationId: string }) => {
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupMacrosQueryCopied, params);
+  };
+
+  reportSetupMacrosUploaded = (params: { migrationId: string; count: number; error?: Error }) => {
+    const { migrationId, count, error } = params;
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupMacrosUploaded, {
+      migrationId,
+      count,
+      ...this.getBaseResultParams(error),
+    });
+  };
+
+  reportSetupLookupsQueryCopied = (params: { migrationId: string; lookupName: string }) => {
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupLookupsQueryCopied, params);
+  };
+
+  reportSetupLookupsUploaded = (params: { migrationId: string; count: number; error?: Error }) => {
+    const { migrationId, count, error } = params;
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.SetupLookupsUploaded, {
+      migrationId,
+      count,
+      ...this.getBaseResultParams(error),
+    });
+  };
+
+  reportStartTranslation = (params: {
+    migrationId: string;
+    connectorId: string;
+    retry?: SiemMigrationRetryFilter;
+    error?: Error;
+  }) => {
+    const { migrationId, connectorId, retry, error } = params;
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.StartTranslation, {
+      migrationId,
+      connectorId,
+      isRetry: !!retry,
+      ...(retry && { retryFilter: retry }),
+      ...this.getBaseResultParams(error),
+    });
+  };
+
+  // Translated rule actions
+
+  reportTranslatedRuleUpdate = (params: { ruleMigration: RuleMigration; error?: Error }) => {
+    const { ruleMigration, error } = params;
+
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.TranslatedRuleUpdate, {
+      migrationId: ruleMigration.migration_id,
+      ruleMigrationId: ruleMigration.id,
+      ...this.getBaseResultParams(error),
+    });
+  };
+
+  reportTranslatedRuleInstall = (params: {
+    ruleMigration: RuleMigration;
+    enabled: boolean;
+    error?: Error;
+  }) => {
+    const { ruleMigration, enabled, error } = params;
+    const elasticRule = ruleMigration.elastic_rule;
+
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.TranslatedRuleInstall, {
+      migrationId: ruleMigration.migration_id,
+      ruleMigrationId: ruleMigration.id,
+      author: elasticRule?.prebuilt_rule_id ? 'elastic' : 'custom',
+      ...(elasticRule?.prebuilt_rule_id && {
+        prebuiltRule: { id: elasticRule?.prebuilt_rule_id, title: elasticRule.title },
+      }),
+      enabled,
+      ...this.getBaseResultParams(error),
+    });
+  };
+
+  reportTranslatedRuleBulkInstall = (params: {
+    migrationId: string;
+    count: number;
+    enabled: boolean;
+    error?: Error;
+  }) => {
+    const { migrationId, count, enabled, error } = params;
+
+    this.telemetryService.reportEvent(SiemMigrationsEventTypes.TranslatedRuleBulkInstall, {
+      migrationId,
+      count,
+      enabled,
+      ...this.getBaseResultParams(error),
+    });
+  };
+}
