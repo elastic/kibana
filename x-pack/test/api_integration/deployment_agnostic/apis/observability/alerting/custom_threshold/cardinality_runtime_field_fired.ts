d8fdfd2f78302b8697ba11c536525d852013006b
Maryam Saeidi <maryam.saeidi@elastic.co>
2025-02-04 08:44:36 +0100

    Update x-pack/test/api_integration/deployment_agnostic/apis/observability/alerting/custom_threshold/cardinality_runtime_field_fired.ts
    Fix checking the locator

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/alerting/custom_threshold/cardinality_runtime_field_fired.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/alerting/custom_threshold/cardinality_runtime_field_fired.ts
@@ -277,7 +277,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
           new URL(resp.hits.hits[0]._source?.viewInAppUrl || '').search
         );
 
-        expect(resp.hits.hits[0]._source?.viewInAppUrl).contain('DISCOVER_APP_LOCATOR');
+        expect(resp.hits.hits[0]._source?.viewInAppUrl).contain('LOGS_EXPLORER_LOCATOR');
         expect(omit(parsedViewInAppUrl.params, 'timeRange.from')).eql({
           dataViewId: DATA_VIEW_ID,
           dataViewSpec: DATA_VIEW_ID,
