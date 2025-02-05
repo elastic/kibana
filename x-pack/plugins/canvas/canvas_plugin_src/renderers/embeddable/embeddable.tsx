192d273800885a1aec4dcc775769224b351eedc0
Nathan Reese <reese.nathan@gmail.com>
2025-02-05 12:11:10 -0700

    tslint

--- x-pack/plugins/canvas/canvas_plugin_src/renderers/embeddable/embeddable.tsx
+++ x-pack/plugins/canvas/canvas_plugin_src/renderers/embeddable/embeddable.tsx
@@ -15,7 +15,7 @@ import {
   ReactEmbeddableRenderer,
 } from '@kbn/embeddable-plugin/public';
 import { KibanaRenderContextProvider } from '@kbn/react-kibana-context-render';
-import React, { FC } from 'react';
+import React, { FC, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 import { omit } from 'lodash';
 import {
