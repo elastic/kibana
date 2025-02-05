8484aa96c9d29c63784062e1b36ec90fd379ed06
Michael DeFazio <michael.defazio@elastic.co>
2025-02-05 15:55:48 -0500

    Fixes UI bugs in semantic text UI and index mgmt mappings tab (#209638)
    ## Summary
    
    A few minor UI clean up fixes on the index mappings tab for Stack
    Management. Also touches the inference popover (shows on both search and
    stack management views)
    
    - Fixes layout shift when choosing semantic text as a field option
    - Changs about panel to `subdued`
    - Fixes responsive behavior for about panel
      - max-width on large breakpoint
    - Inference popover styling updates
    - Decreases gap size in mapping column to reduce space between sections
    
    _Responsive and About panel updates_
    ![CleanShot 2025-02-04 at 14 24
    16@2x](https://github.com/user-attachments/assets/448bc537-6f98-4c34-96df-f553f089dca2)
    ![CleanShot 2025-02-04 at 14 14
    15@2x](https://github.com/user-attachments/assets/0dd6851c-6299-47b6-9549-38f7ce822427)
    
    _Inference popover styling cleanup_
    ![CleanShot 2025-02-04 at 11 19
    50@2x](https://github.com/user-attachments/assets/fdfef447-6d35-419b-a276-55c1b442d749)
    
    ### Checklist
    
    Check the PR satisfies following conditions.
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] ~Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)~
    - [ ]
    ~[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials~
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] ~If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)~
    - [ ] ~This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.~
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ### Identify risks
    
    Does this PR introduce any risks? For example, consider risks like hard
    to test bugs, performance regression, potential of data loss.
    
    Describe the risk, its severity, and mitigation for each identified
    risk. Invite stakeholders and evaluate how to proceed before merging.
    
    - [ ]
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Samiul Monir <samiul.monir@elastic.co>
    (cherry picked from commit ab7aae4c498eb8787775d07c186886049752ea45)
    
    # Conflicts:
    #	x-pack/platform/plugins/shared/index_management/public/application/components/mappings_editor/components/document_fields/fields/create_field/create_field.tsx
    #	x-pack/platform/plugins/shared/index_management/public/application/sections/home/index_list/details_page/details_page_mappings_content.tsx

--- x-pack/platform/plugins/shared/index_management/public/application/sections/home/index_list/details_page/details_page_mappings_content.tsx
+++ x-pack/platform/plugins/shared/index_management/public/application/sections/home/index_list/details_page/details_page_mappings_content.tsx
@@ -22,6 +22,7 @@ import {
   EuiText,
   EuiTitle,
   useGeneratedHtmlId,
+  useEuiBreakpoint,
 } from '@elastic/eui';
 import { css } from '@emotion/react';
 import { i18n } from '@kbn/i18n';
@@ -231,15 +232,15 @@ export const DetailsPageMappingsContent: FunctionComponent<{
         const inferenceIdsInPendingList = forceSaveMappings
           ? []
           : Object.values(denormalizedFields)
-              .filter(isSemanticTextField)
-              .map((field) => field.inference_id)
-              .filter(
-                (inferenceId: string) =>
-                  inferenceId &&
-                  inferenceToModelIdMap?.[inferenceId].trainedModelId && // third-party inference models don't have trainedModelId
-                  !inferenceToModelIdMap?.[inferenceId].isDeployed &&
-                  !isInferencePreconfigured(inferenceId)
-              );
+            .filter(isSemanticTextField)
+            .map((field) => field.inference_id)
+            .filter(
+              (inferenceId: string) =>
+                inferenceId &&
+                inferenceToModelIdMap?.[inferenceId].trainedModelId && // third-party inference models don't have trainedModelId
+                !inferenceToModelIdMap?.[inferenceId].isDeployed &&
+                !isInferencePreconfigured(inferenceId)
+            );
         setHasSavedFields(true);
         if (inferenceIdsInPendingList.length === 0) {
           const { error } = await updateIndexMappings(indexName, denormalizedFields);
@@ -299,9 +300,9 @@ export const DetailsPageMappingsContent: FunctionComponent<{
           '',
           previousState.filter.selectedDataTypes.length > 0
             ? getFieldsMatchingFilterFromState(
-                previousState,
-                previousState.filter.selectedDataTypes
-              )
+              previousState,
+              previousState.filter.selectedDataTypes
+            )
             : previousState.fields.byId
         ),
       },
@@ -400,24 +401,27 @@ export const DetailsPageMappingsContent: FunctionComponent<{
       <EuiSpacer />
     </EuiFlexItem>
   );
+
+  const showAboutMappingsStyles = css`
+    ${useEuiBreakpoint(['xl'])} {
+      max-width: 480px;
+    }
+  `;
+
+  const mappingsWrapperStyles = css`
+    height: 100%;
+    ${useEuiBreakpoint(['xl'])} {
+      flex-wrap: nowrap;
+    }
+  `;
+
   return (
     // using "rowReverse" to keep docs links on the top of the mappings code block on smaller screen
     <>
-      <EuiFlexGroup
-        wrap
-        direction="rowReverse"
-        css={css`
-          height: 100%;
-        `}
-      >
+      <EuiFlexGroup wrap direction="rowReverse" css={mappingsWrapperStyles}>
         {showAboutMappings && (
-          <EuiFlexItem
-            grow={1}
-            css={css`
-              min-width: 400px;
-            `}
-          >
-            <EuiPanel grow={false} paddingSize="l">
+          <EuiFlexItem grow={false} css={showAboutMappingsStyles}>
+            <EuiPanel grow={false} paddingSize="l" color="subdued">
               <EuiFlexGroup alignItems="center" gutterSize="s">
                 <EuiFlexItem grow={false}>
                   <EuiIcon type="iInCircle" />
@@ -465,7 +469,7 @@ export const DetailsPageMappingsContent: FunctionComponent<{
             )}
           </EuiFlexItem>
         )}
-        <EuiFlexGroup direction="column">
+        <EuiFlexGroup direction="column" gutterSize="s">
           <EuiFlexGroup gutterSize="s" justifyContent="spaceBetween">
             <EuiFlexItem grow={false}>
               <MappingsFilter
@@ -535,14 +539,14 @@ export const DetailsPageMappingsContent: FunctionComponent<{
               </EuiFilterGroup>
             </EuiFlexItem>
           </EuiFlexGroup>
-          <EuiFlexItem grow={true}>
-            {hasMLPermissions && (
+          {hasMLPermissions && (
+            <EuiFlexItem grow={true}>
               <SemanticTextBanner
                 isSemanticTextEnabled={isSemanticTextEnabled}
                 isPlatinumLicense={isPlatinumLicense}
               />
-            )}
-          </EuiFlexItem>
+            </EuiFlexItem>
+          )}
           {errorSavingMappings}
           {isAddingFields && (
             <EuiFlexItem grow={false} ref={pendingFieldsRef} tabIndex={0}>
