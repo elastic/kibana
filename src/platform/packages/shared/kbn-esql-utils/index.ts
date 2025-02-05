99cd5ee919facaaee3bac3186f4d352c6073a4f2
Peter Pisljar <peter.pisljar@elastic.co>
2025-02-05 18:03:40 +0100

    [8.x] prerequesites part 1 for lens ESQL generation (#203962) (#209775)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [prerequesites part 1 for lens ESQL generation
    (#203962)](https://github.com/elastic/kibana/pull/203962)
    
    <!--- Backport version: 9.6.4 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Peter
    Pisljar","email":"peter.pisljar@elastic.co"},"sourceCommit":{"committedDate":"2024-12-19T12:56:56Z","message":"prerequesites
    part 1 for lens ESQL generation
    (#203962)","sha":"094e4ae2d5e0c87a0adee394c650f1a9d27fb494","branchLabelMapping":{"^v9.0.0$":"main","^v8.18.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["Team:Visualizations","release_note:skip","backport:skip","v9.0.0"],"title":"prerequesites
    part 1 for lens ESQL
    generation","number":203962,"url":"https://github.com/elastic/kibana/pull/203962","mergeCommit":{"message":"prerequesites
    part 1 for lens ESQL generation
    (#203962)","sha":"094e4ae2d5e0c87a0adee394c650f1a9d27fb494"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v9.0.0","branchLabelMappingKey":"^v9.0.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/203962","number":203962,"mergeCommit":{"message":"prerequesites
    part 1 for lens ESQL generation
    (#203962)","sha":"094e4ae2d5e0c87a0adee394c650f1a9d27fb494"}}]}]
    BACKPORT-->

--- src/platform/packages/shared/kbn-esql-utils/index.ts
+++ src/platform/packages/shared/kbn-esql-utils/index.ts
@@ -35,6 +35,7 @@ export {
   isESQLColumnGroupable,
   isESQLFieldGroupable,
   TextBasedLanguages,
+  sanitazeESQLInput,
   queryCannotBeSampled,
   mapVariableToColumn,
   getValuesFromQueryField,
