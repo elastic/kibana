f1cdd0afc21dd6b1e824e6085f5d22509f34da6a
Nathan Reese <reese.nathan@gmail.com>
2025-02-05 10:30:54 -0700

    tslint

--- x-pack/solutions/search/plugins/search_solution/search_navigation/public/types.ts
+++ x-pack/solutions/search/plugins/search_solution/search_navigation/public/types.ts
@@ -7,7 +7,7 @@
 
 import type { ReactNode } from 'react';
 import type { AppDeepLinkId, ChromeBreadcrumb } from '@kbn/core-chrome-browser';
-import type { CoreStart, ScopedHistory } from '@kbn/core/public';
+import type { ScopedHistory } from '@kbn/core/public';
 import type { ServerlessPluginSetup, ServerlessPluginStart } from '@kbn/serverless/public';
 import type { SolutionNavProps } from '@kbn/shared-ux-page-solution-nav';
 
