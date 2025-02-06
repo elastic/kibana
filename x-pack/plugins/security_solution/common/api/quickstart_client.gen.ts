90b568b81a77797e8093eb21ea43f91fa8375725
Yara Tercero <yctercero@users.noreply.github.com>
2025-02-06 00:33:31 -0800

    [8.16] [Detection Engine][Docs] Updating examples to meet old ascii docs (#207558) (#209951)
    # Backport
    
    This will backport the following commits from `main` to `8.16`:
    - [[Detection Engine][Docs] Updating examples to meet old ascii docs
    (#207558)](https://github.com/elastic/kibana/pull/207558)
    
    <!--- Backport version: 9.6.4 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Yara
    Tercero","email":"yctercero@users.noreply.github.com"},"sourceCommit":{"committedDate":"2025-02-05T07:02:48Z","message":"[Detection
    Engine][Docs] Updating examples to meet old ascii docs (#207558)\n\n#
    Summary\r\n\r\nAs part of the effort to add missing content for Security
    APIs, this PR\r\nintroduces a few missing request, response, and
    parameter examples for\r\nDetection Engine Alert and migration
    APIs.","sha":"d4199dcac1f0bff5f3511e79a860c77534b35c74","branchLabelMapping":{"^v9.1.0$":"main","^v8.19.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","v9.0.0","Team:Detection
    Engine","v8.16.0","backport:version","v8.17.0","v8.18.0","v9.1.0"],"title":"[Detection
    Engine][Docs] Updating examples to meet old ascii
    docs","number":207558,"url":"https://github.com/elastic/kibana/pull/207558","mergeCommit":{"message":"[Detection
    Engine][Docs] Updating examples to meet old ascii docs (#207558)\n\n#
    Summary\r\n\r\nAs part of the effort to add missing content for Security
    APIs, this PR\r\nintroduces a few missing request, response, and
    parameter examples for\r\nDetection Engine Alert and migration
    APIs.","sha":"d4199dcac1f0bff5f3511e79a860c77534b35c74"}},"sourceBranch":"main","suggestedTargetBranches":["9.0","8.16","8.17","8.18"],"targetPullRequestStates":[{"branch":"9.0","label":"v9.0.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.16","label":"v8.16.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.17","label":"v8.17.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.18","label":"v8.18.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/207558","number":207558,"mergeCommit":{"message":"[Detection
    Engine][Docs] Updating examples to meet old ascii docs (#207558)\n\n#
    Summary\r\n\r\nAs part of the effort to add missing content for Security
    APIs, this PR\r\nintroduces a few missing request, response, and
    parameter examples for\r\nDetection Engine Alert and migration
    APIs.","sha":"d4199dcac1f0bff5f3511e79a860c77534b35c74"}}]}] BACKPORT-->

--- x-pack/plugins/security_solution/common/api/quickstart_client.gen.ts
+++ x-pack/plugins/security_solution/common/api/quickstart_client.gen.ts
@@ -1703,7 +1703,7 @@ finalize it.
         headers: {
           [ELASTIC_HTTP_VERSION_HEADER]: '2023-10-31',
         },
-        method: 'POST',
+        method: 'GET',
 
         query: props.query,
       })
