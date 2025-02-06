3c8bfbbcda3acfc15650ea1186e4f62edd4ade34
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-02-06 15:17:35 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/fleet/server/services/output.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/output.ts
@@ -1129,7 +1129,7 @@ class OutputService {
         }
       }
     }
-    
+
     patchUpdateDataWithRequireEncryptedAADFields(updateData, originalOutput);
 
     auditLoggingService.writeCustomSoAuditLog({
