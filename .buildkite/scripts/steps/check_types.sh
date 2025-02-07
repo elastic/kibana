c6982fd03b2046c68f1e963bc1cc830c8f2060ca
Alex Szabo <alex.szabo@elastic.co>
2025-02-07 19:08:11 +0100

    trace typecheck for a few example packages

--- .buildkite/scripts/steps/check_types.sh
+++ .buildkite/scripts/steps/check_types.sh
@@ -7,4 +7,6 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/bootstrap.sh
 
 echo --- Check Types
-moon ci :typecheck -c 3
+moon ci @kbn/health-gateway-server:typecheck @kbn/discover-plugin:typecheck -c 1 --log trace
+
+#moon ci :typecheck -c 3
