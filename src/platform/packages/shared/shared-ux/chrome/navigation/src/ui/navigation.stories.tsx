39804ee50d098cdbc7e6d45f28192fdfddebe7ca
Timothy Sullivan <tsullivan@elastic.co>
2025-02-07 14:49:21 -0700

    dynamically generate the nav item paths

--- src/platform/packages/shared/shared-ux/chrome/navigation/src/ui/navigation.stories.tsx
+++ src/platform/packages/shared/shared-ux/chrome/navigation/src/ui/navigation.stories.tsx
@@ -20,7 +20,10 @@ import {
   EuiPageTemplate,
 } from '@elastic/eui';
 
-import type { NavigationTreeDefinitionUI } from '@kbn/core-chrome-browser';
+import type {
+  ChromeProjectNavigationNode,
+  NavigationTreeDefinitionUI,
+} from '@kbn/core-chrome-browser';
 import { NavigationStorybookMock } from '../../mocks';
 import mdx from '../../README.mdx';
 import type { NavigationServices } from '../types';
@@ -88,39 +91,39 @@ const NavigationWrapper: FC<Props & Omit<Partial<EuiCollapsibleNavBetaProps>, 'c
   );
 };
 
-const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
+const generalLayoutNavTree: NavigationTreeDefinitionUI = {
   id: 'es',
   body: [
     // My custom project
     {
       id: 'example_project',
-      path: 'example_project',
+      path: '',
       title: 'Solution name',
       isCollapsible: false,
       icon: 'logoElastic',
       children: [
         {
-          id: 'item1',
-          path: 'example_project.item1',
+          id: 'item01',
+          path: '',
           title: 'Item 01',
           href: '/app/kibana',
           icon: 'iInCircle',
         },
         {
-          id: 'item2',
-          path: 'example_project.item2',
+          id: 'item02',
+          path: '',
           title: 'Item 02',
           href: '/app/kibana',
           icon: 'iInCircle',
         },
         {
           id: 'section1',
-          path: 'example_project.section1',
+          path: '',
           title: 'Section one',
           children: [
             {
-              id: 'item1',
-              path: 'example_project.section1.item1',
+              id: 'item03',
+              path: '',
               title: 'Item 03',
               icon: 'iInCircle',
               renderAs: 'panelOpener',
@@ -128,47 +131,47 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
                 // FIXME: group mixed with items causes crash
                 // {
                 //   id: 'sub1',
-                //   path: 'example_project.section1.item1.sub1',
+                //   path: '',
                 //   title: 'Item 11',
                 //   href: '/app/kibana',
                 //   icon: 'iInCircle',
                 // },
                 // {
                 //   id: 'sub2',
-                //   path: 'example_project.section1.item1.sub2',
+                //   path: '',
                 //   title: 'Item 12',
                 //   href: '/app/kibana',
                 //   icon: 'iInCircle',
                 // },
                 // {
                 //   id: 'sub3',
-                //   path: 'example_project.section1.item1.sub3',
+                //   path: '',
                 //   title: 'Item 13',
                 //   href: '/app/kibana',
                 //   icon: 'iInCircle',
                 // },
                 {
                   id: 'section1',
-                  path: 'example_project.section1.item1.section1',
+                  path: '',
                   title: 'Section one',
                   children: [
                     {
                       id: 'sub1',
-                      path: 'example_project.section1.item1.section1.sub1',
+                      path: '',
                       title: 'Item 14',
                       href: '/app/kibana',
                       icon: 'iInCircle',
                     },
                     {
                       id: 'sub2',
-                      path: 'example_project.section1.item1.section1.sub2',
+                      path: '',
                       title: 'Item 15',
                       href: '/app/kibana',
                       icon: 'iInCircle',
                     },
                     {
                       id: 'sub3',
-                      path: 'example_project.section1.item1.section1.sub3',
+                      path: '',
                       title: 'Item 16',
                       href: '/app/kibana',
                       icon: 'iInCircle',
@@ -177,26 +180,26 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
                 },
                 {
                   id: 'section2',
-                  path: 'example_project.section1.item1.section2',
+                  path: '',
                   title: 'Section two',
                   children: [
                     {
                       id: 'sub1',
-                      path: 'example_project.section1.item1.section2.sub1',
+                      path: '',
                       title: 'Item 17',
                       href: '/app/kibana',
                       icon: 'iInCircle',
                     },
                     {
                       id: 'sub2',
-                      path: 'example_project.section1.item1.section2.sub2',
+                      path: '',
                       title: 'Just if we want to bring back those icons at some point',
                       href: '/app/kibana',
                       icon: 'dashboardApp',
                     },
                     {
                       id: 'sub3',
-                      path: 'example_project.section1.item1.section2.sub3',
+                      path: '',
                       title: 'Item 18',
                       href: '/app/kibana',
                       icon: 'iInCircle',
@@ -205,20 +208,20 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
                 },
                 {
                   id: 'section2',
-                  path: 'example_project.section1.item1.section2',
+                  path: '',
                   title: 'Item 19',
                   icon: 'iInCircle',
                   renderAs: 'accordion',
                   children: [
                     {
                       id: 'sub1',
-                      path: 'example_project.section1.item1.section2.sub1',
+                      path: '',
                       title: 'Item-Beta',
                       href: '/app/kibana',
                     },
                     {
                       id: 'sub2',
-                      path: 'example_project.section1.item1.section2.sub2',
+                      path: '',
                       title: 'Item-Labs',
                       href: '/app/kibana',
                     },
@@ -227,25 +230,25 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
                 {
                   id: 'section3',
                   title: 'Parent item, opened',
-                  path: 'example_project.section1.item1.section3',
+                  path: '',
                   icon: 'iInCircle',
                   renderAs: 'accordion',
                   children: [
                     {
                       id: 'sub1',
-                      path: 'example_project.section1.item1.section3.sub1',
+                      path: '',
                       title: 'Item 20',
                       href: '/app/kibana',
                     },
                     {
                       id: 'sub2',
-                      path: 'example_project.section1.item1.section3.sub2',
+                      path: '',
                       title: 'Item 21',
                       href: '/app/kibana',
                     },
                     {
                       id: 'sub3',
-                      path: 'example_project.section1.item1.section3.sub3',
+                      path: '',
                       title: 'Item 22',
                       href: '/app/kibana',
                     },
@@ -254,22 +257,22 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
               ],
             },
             {
-              id: 'item2',
-              path: 'example_project.section1.item2',
+              id: 'item04',
+              path: '',
               title: 'Item 04',
               href: '/app/kibana',
               icon: 'iInCircle',
             },
             {
-              id: 'item3',
+              id: 'item05',
               title: 'Item 05',
-              path: 'example_project.section1.item3',
+              path: '',
               icon: 'iInCircle',
               renderAs: 'panelOpener',
               children: [
                 {
                   id: 'sub1',
-                  path: 'example_project.section1.item3.sub1',
+                  path: '',
                   title: 'Item 23',
                   href: '/app/kibana',
                   icon: 'iInCircle',
@@ -281,18 +284,18 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
         {
           id: 'section2',
           title: 'Section two',
-          path: 'example_project.section2',
+          path: '',
           children: [
             {
-              id: 'item1',
+              id: 'item06',
               icon: 'iInCircle',
               title: 'Item 06',
-              path: 'example_project.section2.item1',
+              path: '',
               renderAs: 'panelOpener',
               children: [
                 {
                   id: 'sub1',
-                  path: 'example_project.section2.item1.sub1',
+                  path: '',
                   title: 'Item 24',
                   href: '/app/kibana',
                   icon: 'iInCircle',
@@ -300,15 +303,15 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
               ],
             },
             {
-              id: 'item2',
-              path: 'example_project.section2.item2',
+              id: 'item07',
+              path: '',
               title: 'Item 07',
               href: '/app/kibana',
               icon: 'iInCircle',
             },
             {
-              id: 'item3',
-              path: 'example_project.section2.item3',
+              id: 'item08',
+              path: '',
               title: 'Item 08',
               href: '/app/kibana',
               icon: 'iInCircle',
@@ -318,34 +321,34 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
         {
           id: 'section3',
           title: 'Standalone item with long name',
-          path: 'example_project.section3',
+          path: '',
           href: '/app/kibana',
           icon: 'iInCircle',
         },
         {
           id: 'section3',
           title: 'Standalone group item with long name',
-          path: 'example_project.section3',
+          path: '',
           icon: 'iInCircle',
           renderAs: 'panelOpener',
           children: [
             {
-              id: 'item1',
-              path: 'example_project.section3.item1',
+              id: 'item25',
+              path: '',
               title: 'Item 25',
               href: '/app/kibana',
               icon: 'iInCircle',
             },
             {
-              id: 'item2',
-              path: 'example_project.section3.item2',
+              id: 'item26',
+              path: '',
               title: 'Item 26',
               href: '/app/kibana',
               icon: 'iInCircle',
             },
             {
-              id: 'item3',
-              path: 'example_project.section3.item3',
+              id: 'item27',
+              path: '',
               title: 'Item 27',
               href: '/app/kibana',
               icon: 'iInCircle',
@@ -353,35 +356,35 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
           ],
         },
         {
-          id: 'section4',
+          id: 'item09',
           title: 'Item 09',
-          path: 'example_project.section4',
+          path: '',
           renderAs: 'accordion',
           icon: 'iInCircle',
           children: [
             {
-              id: 'item1',
-              path: 'example_project.section4.item1',
+              id: 'item-beta',
+              path: '',
               title: 'Item-Beta',
               href: '/app/kibana',
               withBadge: true, // FIXME: show "beta" badge in circle
             },
             {
-              id: 'item2',
-              path: 'example_project.section4.item2',
+              id: 'item-labs',
+              path: '',
               title: 'Item-Labs',
               href: '/app/kibana',
               withBadge: true, // FIXME: show "beaker" badge
             },
             {
-              id: 'item3',
-              path: 'example_project.section4.item3',
+              id: 'item27',
+              path: '',
               title: 'Item 27 - name plus badge & icon',
               renderAs: 'panelOpener',
               children: [
                 {
                   id: 'sub1',
-                  path: 'example_project.section4.item3.sub1',
+                  path: '',
                   title: 'Item 28',
                   href: '/app/kibana',
                   icon: 'iInCircle',
@@ -397,41 +400,41 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
     {
       id: 'section5',
       title: 'Parent item, closed',
-      path: 'footer.section5',
+      path: '',
       renderAs: 'accordion',
       icon: 'iInCircle',
       children: [
         {
-          id: 'item1',
-          path: 'footer.section5.item1',
+          id: 'item29',
+          path: '',
           title: 'Item 29',
           href: '/app/kibana',
           icon: 'iInCircle',
         },
         {
-          id: 'item2',
-          path: 'footer.section5.item2',
+          id: 'item30',
+          path: '',
           title: 'Item 30',
           href: '/app/kibana',
           icon: 'iInCircle',
         },
         {
-          id: 'item3',
-          path: 'footer.section5.item3',
+          id: 'item31',
+          path: '',
           title: 'Item 31',
           href: '/app/kibana',
           icon: 'iInCircle',
         },
         {
-          id: 'item4',
+          id: 'sub-accordion',
           icon: 'iInCircle',
           title: 'Sub-Accordion',
-          path: 'footer.section5.item4',
+          path: '',
           renderAs: 'accordion',
           children: [
             {
               id: 'sub1',
-              path: 'footer.section5.item4.sub1',
+              path: '',
               title: 'Item 32',
               href: '/app/kibana',
               icon: 'iInCircle',
@@ -440,38 +443,32 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
         },
       ],
     },
-    {
-      id: 'item1',
-      path: 'footer.item1',
-      title: 'Item 10',
-      icon: 'iInCircle',
-      href: '/app/kibana',
-    },
+    { id: 'item10', path: '', title: 'Item 10', icon: 'iInCircle', href: '/app/kibana' },
     {
       id: 'section6',
       title: 'Parent item, opened',
-      path: 'footer.section6',
+      path: '',
       renderAs: 'accordion',
       icon: 'iInCircle',
       defaultIsCollapsed: false,
       children: [
         {
-          id: 'item1',
-          path: 'footer.section6.item1',
+          id: 'item33',
+          path: '',
           title: 'Item 33',
           href: '/app/kibana',
           icon: 'iInCircle',
         },
         {
-          id: 'item2',
-          path: 'footer.section6.item2',
+          id: 'item34',
+          path: '',
           title: 'Item 34',
           href: '/app/kibana',
           icon: 'iInCircle',
         },
         {
-          id: 'item3',
-          path: 'footer.section6.item3',
+          id: 'item35',
+          path: '',
           title: 'Item 35',
           href: '/app/kibana',
           icon: 'iInCircle',
@@ -482,6 +479,34 @@ const navigationTreeWithGroups: NavigationTreeDefinitionUI = {
   ],
 };
 
+/**
+ * The "path" fields are statically defined as empty strings.
+ * In Kibana, these will be dynamically defined based on where the item is within the tree.
+ * This helper function dynamically defines each path.
+ */
+function correctPaths(sourceTree: ChromeProjectNavigationNode[], contextId = '') {
+  const result: ChromeProjectNavigationNode[] = [];
+  for (const key in sourceTree) {
+    if (Object.prototype.hasOwnProperty.call(sourceTree, key)) {
+      const current = sourceTree[key];
+      const currentId = contextId ? `${contextId}.${current.id}` : current.id;
+      current.path = currentId;
+      if (current.children) {
+        current.children = correctPaths(current.children, currentId);
+      }
+      result[key] = current;
+    }
+  }
+  return result;
+}
+
+generalLayoutNavTree.body = correctPaths(
+  generalLayoutNavTree.body as ChromeProjectNavigationNode[]
+);
+generalLayoutNavTree.footer = correctPaths(
+  generalLayoutNavTree.footer as ChromeProjectNavigationNode[]
+);
+
 export const GeneralLayoutStructure = (args: NavigationServices) => {
   const services = storybookMock.getServices(args);
 
@@ -489,7 +514,7 @@ export const GeneralLayoutStructure = (args: NavigationServices) => {
     <NavigationWrapper>
       {({ isCollapsed }) => (
         <NavigationProvider {...services} isSideNavCollapsed={isCollapsed}>
-          <Navigation navigationTree$={of(navigationTreeWithGroups)} />
+          <Navigation navigationTree$={of(generalLayoutNavTree)} />
         </NavigationProvider>
       )}
     </NavigationWrapper>
