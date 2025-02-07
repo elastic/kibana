22a727f34f9feeccef9975d5df2d15eacee39f83
lgestc <lukasz.gmys@elastic.co>
2025-02-07 13:51:47 +0100

    document new scope field

--- x-pack/solutions/security/plugins/security_solution/public/sourcerer/store/model.ts
+++ x-pack/solutions/security/plugins/security_solution/public/sourcerer/store/model.ts
@@ -36,6 +36,10 @@ export interface SourcererScope {
    * saved timeline has pattern that is not in the default */
   missingPatterns: string[];
 
+  /**
+   * The full dataview spec that might be set, depending on the data view loading strategy.
+   * Currently only available in the legacy compatibility flow when adhoc dv is created.
+   */
   dataViewSpec?: DataViewSpec;
 }
 
