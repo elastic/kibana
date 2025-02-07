f85f06fbd8323cbf258ec187f4f779dbbd13988f
Nikita Khristinin <nkhristinin@gmail.com>
2025-02-07 16:19:35 +0100

    Revert "[8.17] Remove beta badge for manual rule run (#209535) (#210069)"
    This reverts commit 795b5af0548f8bd56d80e38fa5b65978a93e8be3.

--- x-pack/plugins/security_solution/public/detection_engine/rule_gaps/components/manual_rule_run/index.tsx
+++ x-pack/plugins/security_solution/public/detection_engine/rule_gaps/components/manual_rule_run/index.tsx
@@ -5,6 +5,7 @@
  * 2.0.
  */
 import {
+  EuiBetaBadge,
   EuiConfirmModal,
   EuiDatePicker,
   EuiDatePickerRange,
@@ -19,6 +20,7 @@ import {
 import moment from 'moment';
 import React, { useCallback, useMemo, useState } from 'react';
 import { MAX_MANUAL_RULE_RUN_LOOKBACK_WINDOW_DAYS } from '../../../../../common/constants';
+import { BETA, BETA_TOOLTIP } from '../../../../common/translations';
 
 import * as i18n from './translations';
 
@@ -70,6 +72,9 @@ const ManualRuleRunModalComponent = ({ onCancel, onConfirm }: ManualRuleRunModal
       title={
         <EuiFlexGroup justifyContent="spaceBetween">
           <EuiFlexItem>{i18n.MANUAL_RULE_RUN_MODAL_TITLE}</EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiBetaBadge label={BETA} tooltipContent={BETA_TOOLTIP} />
+          </EuiFlexItem>
         </EuiFlexGroup>
       }
       titleProps={{ id: modalTitleId, style: { width: '100%' } }}
