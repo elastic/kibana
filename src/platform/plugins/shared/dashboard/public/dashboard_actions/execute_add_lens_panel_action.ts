3f7aa95b12a2a822010876b414924be533f8f022
Nathan Reese <reese.nathan@gmail.com>
2025-02-10 16:16:46 -0700

    execute

--- src/platform/plugins/shared/dashboard/public/dashboard_actions/execute_add_lens_panel_action.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_actions/execute_add_lens_panel_action.ts
@@ -6,3 +6,22 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
+
+import { addPanelMenuTrigger } from '@kbn/ui-actions-plugin/public';
+import { i18n } from '@kbn/i18n';
+import { coreServices, uiActionsService } from '../services/kibana_services';
+
+export async function executeAddLensPanelAction() {
+  try {
+    const addLensPanelAction = await uiActionsService.getAction('addLensPanelAction');
+    addLensPanelAction.execute({
+      trigger: addPanelMenuTrigger,
+    });
+  } catch (error) {
+    coreServices.notifications.toasts.addWarning(
+      i18n.translate('dashboard.addNewPanelError', {
+        defaultMessage: 'Unable to add new panel',
+      })
+    );
+  }
+}
