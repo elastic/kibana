9a54a18394a36abaf0b608b8b59246853c52af84
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-02-10 20:05:53 +0100

    enable parallel run

--- x-pack/solutions/observability/plugins/observability_onboarding/ui_tests/playwright.config.ts
+++ x-pack/solutions/observability/plugins/observability_onboarding/ui_tests/playwright.config.ts
@@ -10,4 +10,5 @@ import { createPlaywrightConfig } from '@kbn/scout';
 // eslint-disable-next-line import/no-default-export
 export default createPlaywrightConfig({
   testDir: './tests',
+  workers: 2,
 });
