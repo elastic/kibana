1643a232e957020ed16b5da81db8bd58adcdaaf0
Alex Szabo <alex.szabo@elastic.co>
2025-02-10 11:56:46 +0100

    make logging in tsc verbose, not in moon

--- .buildkite/scripts/steps/check_types.sh
+++ .buildkite/scripts/steps/check_types.sh
@@ -8,7 +8,6 @@ source .buildkite/scripts/common/util.sh
 
 echo --- Check Types
 export MOON_LOG_FILE=moon.log
-export MOON_LOG=debug
-moon ci @kbn/health-gateway-server:typecheck @kbn/discover-plugin:typecheck -c 1
+moon ci @kbn/health-gateway-server:typecheck @kbn/discover-plugin:typecheck -c 1 -- --verbose
 
 #moon ci :typecheck -c 3
