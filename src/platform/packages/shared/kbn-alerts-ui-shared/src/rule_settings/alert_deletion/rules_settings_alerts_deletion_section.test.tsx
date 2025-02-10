f55926bada0de86f8de6cf7834176905d9d5be29
Julian Gernun <17549662+jcger@users.noreply.github.com>
2025-02-10 20:39:42 +0100

    fix more types

--- src/platform/packages/shared/kbn-alerts-ui-shared/src/rule_settings/alert_deletion/rules_settings_alerts_deletion_section.test.tsx
+++ src/platform/packages/shared/kbn-alerts-ui-shared/src/rule_settings/alert_deletion/rules_settings_alerts_deletion_section.test.tsx
@@ -9,7 +9,7 @@
 
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { RulesSettingsAlertsDeletionSection } from './rules_settings_alert_deletion_section';
+import { RulesSettingsAlertsDeletionSection } from './rules_settings_alerts_deletion_section';
 import { RulesSettingsAlertsDeletionProperties } from '@kbn/alerting-types/rule_settings';
 
 const initialSettings: RulesSettingsAlertsDeletionProperties = {
--- src/platform/packages/shared/kbn-alerts-ui-shared/src/rule_settings/index.ts
+++ src/platform/packages/shared/kbn-alerts-ui-shared/src/rule_settings/index.ts
@@ -12,4 +12,4 @@ export * from './rule_settings_flapping_inputs';
 export * from './rule_settings_flapping_message';
 export * from './rule_settings_flapping_title_tooltip';
 export * from './rule_settings_range_input';
-export { RulesSettingsAlertDeletionSection } from './alert_deletion/rules_settings_alert_deletion_section';
+export { RulesSettingsAlertsDeletionSection } from './alert_deletion/rules_settings_alerts_deletion_section';
