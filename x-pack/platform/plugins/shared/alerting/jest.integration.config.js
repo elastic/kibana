5b3344564ae8c447442694c4c8bb1ac7b5ee3981
Aleh Zasypkin <aleh.zasypkin@elastic.co>
2025-02-11 13:47:21 +0200

    fix: switch more Jest integration tests configs to `@kbn/test/jest_integration_node` preset

--- x-pack/platform/plugins/shared/alerting/jest.integration.config.js
+++ x-pack/platform/plugins/shared/alerting/jest.integration.config.js
@@ -6,7 +6,12 @@
  */
 
 module.exports = {
-  preset: '@kbn/test/jest_integration',
+  // TODO remove the line below once we've addressed all the open handles
+  // We stop the server very soon, and plugins installing (and retrying indices) might keep Kibana running until a timeout occurs.
+  // to do so, we must fix all integration tests first
+  // see https://github.com/elastic/kibana/pull/130255/
+  forceExit: true,
+  preset: '@kbn/test/jest_integration_node',
   rootDir: '../../../../..',
   roots: ['<rootDir>/x-pack/platform/plugins/shared/alerting'],
 };
