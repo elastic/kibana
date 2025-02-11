b39f0c5086589771a231b82b38315bb4ff15e4a0
Kevin Delemme <kevin.delemme@elastic.co>
2025-02-11 18:03:28 -0500

    Fix tests

--- test/plugin_functional/test_suites/core_plugins/rendering.ts
+++ test/plugin_functional/test_suites/core_plugins/rendering.ts
@@ -377,6 +377,7 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
         'xpack.observability.unsafe.ruleFormV2.enabled (boolean?)',
         'xpack.slo.experimental.ruleFormV2.enabled (boolean?)',
         /**/
+        'xpack.slo.experimental.management.enabled (boolean?)',
       ];
       // We don't assert that actualExposedConfigKeys and expectedExposedConfigKeys are equal, because test failure messages with large
       // arrays are hard to grok. Instead, we take the difference between the two arrays and assert them separately, that way it's
