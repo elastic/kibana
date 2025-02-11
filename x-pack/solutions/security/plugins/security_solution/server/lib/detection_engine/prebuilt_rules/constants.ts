096b0fe3ed1a6144a7dd281deb94d24ab5a9b294
Dmitrii Shevchenko <dmitrii.shevchenko@elastic.co>
2025-02-11 18:12:03 +0100

    [Security Solution] Added concurrency limits and request throttling to prebuilt rule routes (#209551)
    **Resolves: https://github.com/elastic/kibana/issues/208357**
    **Resolves: https://github.com/elastic/kibana/issues/208355**
    
    ## Summary
    
    To prevent possible OOM errors, we need to limit concurrent requests to
    prebuilt rule routes (see attached tickets for more details).
    
    - `installation/_perform` and `upgrade/_perform` endpoints
    - Concurrency is limited to one parallel call. If another call is made
    simultaneously, the server responds with 429 Too Many Requests.
    - On the front end, all rule install and upgrade operations are retried
    in case of a 429 response. This ensures proper handling when a user
    clicks multiple times an update or install rule buttons
    
    - `prebuilt_rules/_bootstrap` endpoint
    - Install prebuilt rules and endpoint packages sequentially instead of
    in parallel to prevent from having them both downloaded into memory
    simultaneously.
    - Added a 30-minute socket timeout to prevent the proxy from closing the
    connection while rule installation is in progress.
    - Introduced a `throttleRequests` wrapper, ensuring the endpoint handler
    is called only once when multiple concurrent requests are received.
    - The first request triggers the handler, while subsequent requests wait
    for the first one to complete and reuse its result.
    - This prevents costly prebuilt rule package installation from running
    in parallel.
    - Reusing the response ensures the frontend correctly invalidates cached
    prebuilt rule queries. Since concurrent frontend requests should receive
    the same installed package information, responding with 421 and using
    the retry logic as in cases above is not an option here because the
    second request would receive a package installation skipped response
    leading to no cache invalidation.
    
    - `installation/_review` and `upgrade/_review` endpoints
    - Concurrency is limited to one parallel call. If another call is made
    simultaneously, the server responds with 429 Too Many Requests.
    - On the front end, all rule install and upgrade operations are retried
    in case of a 429 response. This ensures proper handling when a user
    clicks multiple times an update or install rule buttons
    
    (cherry picked from commit c5557f33213f699acd9bb656af9166b1449d18f9)

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/prebuilt_rules/constants.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/prebuilt_rules/constants.ts
@@ -5,4 +5,8 @@
  * 2.0.
  */
 
-export const PREBUILT_RULES_OPERATION_SOCKET_TIMEOUT_MS = 1800000 as const; // 30 minutes
+export const PREBUILT_RULES_OPERATION_SOCKET_TIMEOUT_MS = 1_800_000 as const; // 30 minutes
+
+// Only one rule installation or upgrade request can be processed at a time.
+// Multiple requests can lead to high memory usage and unexpected behavior.
+export const PREBUILT_RULES_OPERATION_CONCURRENCY = 1;
