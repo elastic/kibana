d2b3214bef64b05f5634898d555e988237b17d30
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-02-11 15:03:10 +0100

    remove useLegacyTimeAxis from data lens

--- x-pack/platform/plugins/shared/lens/public/visualizations/xy/xy_config_panel/axis_settings_popover.tsx
+++ x-pack/platform/plugins/shared/lens/public/visualizations/xy/xy_config_panel/axis_settings_popover.tsx
@@ -24,10 +24,8 @@ import {
   ToolbarTitleSettings,
   AxisBoundsControl,
   AxisTicksSettings,
-  AxisLabelOrientationSelector,
-  allowedOrientations,
 } from '../../../shared_components';
-import type { Orientation, AxesSettingsConfigKeys } from '../../../shared_components';
+import type { AxesSettingsConfigKeys } from '../../../shared_components';
 import { XYLayerConfig } from '../types';
 
 import { validateExtent } from '../../../shared_components/axis/extent/helpers';
@@ -209,7 +207,6 @@ export const AxisSettingsPopover: React.FunctionComponent<AxisSettingsPopoverPro
   hasBarOrAreaOnAxis,
   hasPercentageAxis,
   dataBounds,
-  useMultilayerTimeAxis,
   scale,
   setScale,
   setScaleWithExtent,
@@ -279,19 +276,6 @@ export const AxisSettingsPopover: React.FunctionComponent<AxisSettingsPopoverPro
         }}
         isAxisLabelVisible={areTickLabelsVisible}
       />
-      {!useMultilayerTimeAxis && areTickLabelsVisible && (
-        <AxisLabelOrientationSelector
-          axis={axis}
-          selectedLabelOrientation={
-            allowedOrientations.includes(orientation as Orientation)
-              ? (orientation as Orientation)
-              : 0 // Default to 0 if the value is not valid
-          }
-          setLabelOrientation={(newOrientation) => {
-            setOrientation(axis, newOrientation);
-          }}
-        />
-      )}
       {setEndzoneVisibility && (
         <EuiFormRow
           display="columnCompressed"
