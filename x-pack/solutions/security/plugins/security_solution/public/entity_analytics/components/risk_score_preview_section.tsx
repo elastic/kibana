cbee96c3293c51788ad4c575c7e619aa2cb06f64
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-02-11 22:40:11 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/risk_score_preview_section.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/risk_score_preview_section.tsx
@@ -34,7 +34,6 @@ import { useSourcererDataView } from '../../sourcerer/containers';
 import type { RiskEngineMissingPrivilegesResponse } from '../hooks/use_missing_risk_engine_privileges';
 import { userHasRiskEngineReadPermissions } from '../common';
 import { EntityIconByType } from './entity_store/helpers';
-import { useRiskEngineEntityTypes } from '../hooks/use_enabled_entity_types';
 interface IRiskScorePreviewPanel {
   showMessage: React.ReactNode;
   hideMessage: React.ReactNode;
