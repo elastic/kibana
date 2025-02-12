976c9056d9058a595d0ae224e20e4f7513111079
Agustina Nahir Ruidiaz <agustina.ruidiaz@elastic.co>
2025-02-12 15:29:53 +0100

    get car icon function

--- x-pack/solutions/security/plugins/security_solution/public/onboarding/components/onboarding_body/cards/alerts/index.ts
+++ x-pack/solutions/security/plugins/security_solution/public/onboarding/components/onboarding_body/cards/alerts/index.ts
@@ -9,22 +9,12 @@ import React from 'react';
 import type { OnboardingCardConfig } from '../../../../types';
 import { OnboardingCardId } from '../../../../constants';
 import { ALERTS_CARD_TITLE } from './translations';
-import alertsIcon from './images/alerts_icon.png';
-import alertsIconDark from './images/alerts_icon_dark.png';
-import { useEuiTheme, EuiIcon } from '@elastic/eui';
-
-const AlertsIconComponent = () => {
-  const { colorMode } = useEuiTheme();
-  const isDark = colorMode === 'DARK';
-  const icon = isDark ? alertsIconDark : alertsIcon;
-
-  return <img src={icon} alt="Alerts Icon" width={24} height={24} />;
-};
+import { getCardIcon } from '../common/card_icon';
 
 export const alertsCardConfig: OnboardingCardConfig = {
   id: OnboardingCardId.alerts,
   title: ALERTS_CARD_TITLE,
-  icon: AlertsIconComponent,
+  icon: () => getCardIcon(OnboardingCardId.alerts),
   Component: React.lazy(
     () =>
       import(
---
+++ x-pack/solutions/security/plugins/security_solution/public/onboarding/components/onboarding_body/cards/common/card_icon.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React from 'react';
+import { useDarkMode } from '@kbn/kibana-react-plugin/public';
+import { OnboardingCardId } from '../../../../constants';
+import rulesIcon from '../rules/images/rules_icon.png';
+import rulesDarkIcon from '../rules/images/rules_icon_dark.png';
+import integrationsIcon from '../integrations/images/integrations_icon.png';
+import integrationsDarkIcon from '../integrations/images/integrations_icon_dark.png';
+import dashboardsIcon from '../dashboards/images/dashboards_icon.png';
+import dashboardsDarkIcon from '../dashboards/images/dashboards_icon_dark.png';
+import alertsIcon from '../alerts/images/alerts_icon.png';
+import alertsDarkIcon from '../alerts/images/alerts_icon_dark.png';
+
+interface CardIcons {
+  [key: string]: {
+    light: string;
+    dark: string;
+  };
+}
+
+const cardIcons: CardIcons = {
+  [OnboardingCardId.rules]: {
+    light: rulesIcon,
+    dark: rulesDarkIcon,
+  },
+  [OnboardingCardId.integrations]: {
+    light: integrationsIcon,
+    dark: integrationsDarkIcon,
+  },
+  [OnboardingCardId.dashboards]: {
+    light: dashboardsIcon,
+    dark: dashboardsDarkIcon,
+  },
+  [OnboardingCardId.alerts]: {
+    light: alertsIcon,
+    dark: alertsDarkIcon,
+  },
+};
+
+const CardIcon = ({ cardId }: { cardId: OnboardingCardId }) => {
+  const isDark = useDarkMode();
+  const icon = cardIcons[cardId]?.[isDark ? 'dark' : 'light'] || '';
+
+  if (!icon) return null;
+
+  return <img src={icon} alt={`${cardId}-card-icon`} width={24} height={24} />;
+};
+
+export const getCardIcon = (cardId: OnboardingCardId) => <CardIcon cardId={cardId} />;
