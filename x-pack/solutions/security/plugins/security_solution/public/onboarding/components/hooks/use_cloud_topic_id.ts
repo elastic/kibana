54333d825078488b8f883be668e5568954eed42e
Sergi Massaneda <sergi.massaneda@elastic.co>
2025-02-12 20:10:26 +0100

    use cloud onboarding config

---
+++ x-pack/solutions/security/plugins/security_solution/public/onboarding/components/hooks/use_cloud_topic_id.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useCallback, useState } from 'react';
+import type { CloudDataAttributes } from '@kbn/cloud-plugin/server/routes/types';
+import { useKibana } from '../../../common/lib/kibana/kibana_react';
+import { OnboardingTopicId } from '../../constants';
+
+const URL = '/internal/cloud/solution';
+
+interface UseCloudTopicIdParams {
+  onComplete: (topicId: OnboardingTopicId | null) => void;
+}
+
+export const useCloudTopicId = ({ onComplete }: UseCloudTopicIdParams) => {
+  const { http } = useKibana().services;
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const start = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const data = await http.get<CloudDataAttributes>(URL, { version: '1' });
+      const { security } = data.onboardingData ?? {};
+      if (
+        security?.useCase === 'siem' &&
+        security?.migration?.value &&
+        security?.migration?.type === 'splunk'
+      ) {
+        onComplete(OnboardingTopicId.siemMigrations);
+      } else {
+        onComplete(null);
+      }
+    } catch (_) {
+      // ignore the error, we will just show the default topic
+      onComplete(null);
+    }
+    setIsLoading(false);
+  }, [onComplete, http]);
+
+  return { start, isLoading };
+};
--- x-pack/solutions/security/plugins/security_solution/public/onboarding/components/hooks/use_url_detail.ts
+++ x-pack/solutions/security/plugins/security_solution/public/onboarding/components/hooks/use_url_detail.ts
@@ -5,12 +5,13 @@
  * 2.0.
  */
 
-import { useCallback } from 'react';
+import { useCallback, useEffect } from 'react';
 import { SecurityPageName, useNavigateTo } from '@kbn/security-solution-navigation';
 import { useStoredUrlDetails } from './use_stored_state';
 import { OnboardingTopicId, type OnboardingCardId } from '../../constants';
 import { useOnboardingContext } from '../onboarding_context';
 import { useTopicId } from './use_topic_id';
+import { useCloudTopicId } from './use_cloud_topic_id';
 
 export const getCardIdFromHash = (hash: string): OnboardingCardId | null =>
   (hash.split('?')[0].replace('#', '') as OnboardingCardId) || null;
@@ -28,22 +29,29 @@ const getCardHash = (cardId: OnboardingCardId | null) => (cardId ? `#${cardId}` 
  * This hook manages the expanded card id state in the LocalStorage and the hash in the URL.
  */
 export const useUrlDetail = () => {
-  const { config, spaceId, telemetry } = useOnboardingContext();
+  const { spaceId, telemetry } = useOnboardingContext();
   const topicId = useTopicId();
   const [storedUrlDetail, setStoredUrlDetail] = useStoredUrlDetails(spaceId);
 
   const { navigateTo } = useNavigateTo();
 
-  const setTopicDetail = useCallback(
-    (newTopicId: OnboardingTopicId) => {
-      const path = newTopicId === OnboardingTopicId.default ? undefined : newTopicId;
-      setStoredUrlDetail(path ?? null);
-      navigateTo({ deepLinkId: SecurityPageName.landing, path });
+  const navigateToPath = useCallback(
+    (path?: string | null) => {
+      navigateTo({ deepLinkId: SecurityPageName.landing, path: path || undefined });
     },
-    [setStoredUrlDetail, navigateTo]
+    [navigateTo]
   );
 
-  const setCardDetail = useCallback(
+  const setTopic = useCallback(
+    (newTopicId: OnboardingTopicId) => {
+      const path = newTopicId === OnboardingTopicId.default ? null : newTopicId;
+      setStoredUrlDetail(path);
+      navigateToPath(path);
+    },
+    [setStoredUrlDetail, navigateToPath]
+  );
+
+  const setCard = useCallback(
     (newCardId: OnboardingCardId | null) => {
       setHash(newCardId);
       setStoredUrlDetail(`${getTopicPath(topicId)}${getCardHash(newCardId)}` || null);
@@ -54,29 +62,59 @@ export const useUrlDetail = () => {
     [setStoredUrlDetail, topicId, telemetry]
   );
 
-  const syncUrlDetails = useCallback(
-    (pathTopicId: OnboardingTopicId | null, hashCardId: OnboardingCardId | null) => {
-      if (storedUrlDetail) {
-        // If the stored topic is not valid, clear it
-        const [storedTopicId] = storedUrlDetail.split('#');
-        if (storedTopicId && !config.has(storedTopicId as OnboardingTopicId)) {
-          setStoredUrlDetail(null);
-          return;
-        }
-      }
-      const urlDetail = `${pathTopicId || ''}${hashCardId ? `#${hashCardId}` : ''}`;
-      if (urlDetail && urlDetail !== storedUrlDetail) {
-        if (hashCardId) {
-          telemetry.reportCardOpen(hashCardId, { auto: true });
-        }
-        setStoredUrlDetail(urlDetail);
-      }
-      if (!urlDetail && storedUrlDetail) {
-        navigateTo({ deepLinkId: SecurityPageName.landing, path: storedUrlDetail });
-      }
-    },
-    [config, navigateTo, setStoredUrlDetail, storedUrlDetail, telemetry]
-  );
+  return { topicId, setTopic, setCard, navigateToPath, storedUrlDetail, setStoredUrlDetail };
+};
 
-  return { setTopicDetail, setCardDetail, syncUrlDetails };
+interface UseSyncUrlDetailsParams {
+  pathTopicId: OnboardingTopicId | null;
+  hashCardId: OnboardingCardId | null;
+}
+/**
+ * This hook manages the expanded card id state in the LocalStorage and the hash in the URL.
+ */
+export const useSyncUrlDetails = ({ pathTopicId, hashCardId }: UseSyncUrlDetailsParams) => {
+  const { config, telemetry } = useOnboardingContext();
+  const { storedUrlDetail, setStoredUrlDetail, navigateToPath, setTopic } = useUrlDetail();
+
+  const onComplete = useCallback((cloudTopicId: OnboardingTopicId | null) => {
+    if (cloudTopicId && config.has(cloudTopicId)) {
+      setTopic(cloudTopicId);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const { start: startGetCloudTopicId, isLoading } = useCloudTopicId({ onComplete });
+
+  useEffect(() => {
+    // Create the URL detail
+    const urlDetail = `${pathTopicId || ''}${hashCardId ? `#${hashCardId}` : ''}`;
+
+    // If the URL has a topic it has prevalence, we need to set it to the local storage
+    if (urlDetail && urlDetail !== storedUrlDetail) {
+      if (hashCardId) {
+        telemetry.reportCardOpen(hashCardId, { auto: true });
+      }
+      setStoredUrlDetail(urlDetail);
+      return;
+    }
+
+    // If the URL has no topic, but the local storage has a topic, we need to navigate to the topic
+    if (!urlDetail && storedUrlDetail) {
+      // Check if the stored topic is not valid, if so clear it to prevent inconsistencies
+      const [storedTopicId] = storedUrlDetail.split('#');
+      if (storedTopicId && !config.has(storedTopicId as OnboardingTopicId)) {
+        setStoredUrlDetail(null);
+        return;
+      }
+      navigateToPath(storedUrlDetail);
+    }
+
+    // If nothing is stored and nothing is in the URL, let's see if we have a cloud topic (first time onboarding)
+    if (!urlDetail && storedUrlDetail === undefined) {
+      startGetCloudTopicId();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { isLoading };
 };
