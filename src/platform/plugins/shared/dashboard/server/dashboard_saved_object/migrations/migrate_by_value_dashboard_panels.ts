8da3ae0ad7a56db73582534766126e23090962a6
Nathan Reese <reese.nathan@gmail.com>
2025-02-12 14:52:57 -0700

    clean up

--- src/platform/plugins/shared/dashboard/server/dashboard_saved_object/migrations/migrate_by_value_dashboard_panels.ts
+++ src/platform/plugins/shared/dashboard/server/dashboard_saved_object/migrations/migrate_by_value_dashboard_panels.ts
@@ -15,7 +15,6 @@ import {
 import { Serializable, SerializableRecord } from '@kbn/utility-types';
 import { SavedObjectMigrationFn } from '@kbn/core/server';
 import { MigrateFunction } from '@kbn/kibana-utils-plugin/common';
-import { SavedObjectEmbeddableInput } from '@kbn/embeddable-plugin/common';
 
 import {
   convertPanelStateToSavedDashboardPanel,
@@ -23,7 +22,7 @@ import {
 } from './utils';
 import type { SavedDashboardPanel } from '..';
 
-type ValueOrReferenceInput = SavedObjectEmbeddableInput & {
+type ValueOrReferenceInput = {
   attributes?: Serializable;
   savedVis?: Serializable;
 };
