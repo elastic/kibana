fc4cb195d109873291281ffa515b1d5636efe68d
Jenny <dzheni.pavlova@elastic.co>
2025-02-12 18:59:16 +0100

    Service map tests

--- x-pack/solutions/observability/plugins/apm/ui_tests/tests/sevice_map/service_map.spec.ts
+++ x-pack/solutions/observability/plugins/apm/ui_tests/tests/sevice_map/service_map.spec.ts
@@ -9,12 +9,12 @@ import { expect } from '@kbn/scout';
 import { test } from '../../fixtures';
 import { opbeans } from '../../fixtures/synthtrace/opbeans';
 
-const start = '2024-10-10T10:00:00.000Z';
-const end = '2024-10-10T00:11:00.000Z';
+const start = '2021-10-10T00:00:00.000Z';
+const end = '2021-10-10T00:15:00.000Z';
 
 test.describe('Service Map', { tag: ['@ess', '@svlOblt'] }, () => {
   test.beforeEach(
-    async ({ browserAuth, apmSynthtraceEsClient, pageObjects: { serviceMapPage }, page }) => {
+    async ({ apmSynthtraceEsClient, browserAuth, page, pageObjects: { serviceMapPage } }) => {
       await apmSynthtraceEsClient.index(
         opbeans({
           from: new Date(start).getTime(),
@@ -30,27 +30,38 @@ test.describe('Service Map', { tag: ['@ess', '@svlOblt'] }, () => {
     }
   );
 
-  // test.afterAll(async ({ apmSynthtraceEsClient }) => {
-  // await apmSynthtraceEsClient.clean();
-  // });
+  test.afterAll(async ({ apmSynthtraceEsClient }) => {
+    await apmSynthtraceEsClient.clean();
+  });
 
-  test('The page loads correctly', async ({ page }) => {
+  test('shows the service map', async ({ page, pageObjects: { serviceMapPage } }) => {
+    await serviceMapPage.gotoWithDateSelected(start, end);
     expect(page.url()).toContain('/app/apm/service-map');
     await page.waitForSelector('[data-test-subj="serviceMap"]');
-    const loadingSpinner = page.getByTestId('serviceMap').getByLabel('Loading');
-    await expect(loadingSpinner).not.toBeVisible();
-
-    // expect(page.url()).toContain(start);
-    // expect(page.url()).toContain(end);
-    // await expect(page).toHaveScreenshot();
+    await expect(page.getByTestId('serviceMap').getByLabel('Loading')).not.toBeVisible();
+    await page.getByTestId('centerServiceMap').click();
+    await expect(page.getByTestId('serviceMap').getByLabel('Loading')).not.toBeVisible();
+    await expect(await page.getByTestId('serviceMap')).toHaveScreenshot('service_map.png');
   });
-  // test('shows empty state when there is no data', async ({
-  //   page,
-  //   pageObjects: { serviceMapPage },
-  // }) => {
-  //   await serviceMapPage.typeInTheSearchBar();
-  //   await page.getByText('No services available');
-  //   // search bar is still visible
-  //   await serviceMapPage.getSearchBar();
-  // });
+
+  test('shows a detailed service map', async ({ page, pageObjects: { serviceMapPage } }) => {
+    await serviceMapPage.gotoDetailedServiceMapWithDateSelected(start, end);
+    expect(page.url()).toContain('/services/opbeans-java/service-map');
+    await page.waitForSelector('[data-test-subj="serviceMap"]');
+    await expect(page.getByTestId('serviceMap').getByLabel('Loading')).not.toBeVisible();
+    await page.getByTestId('centerServiceMap').click();
+    await expect(page.getByTestId('serviceMap').getByLabel('Loading')).not.toBeVisible();
+    await expect(await page.getByTestId('serviceMap')).toHaveScreenshot('detailed_service_map.png');
+  });
+
+  test('shows empty state when there is no data', async ({
+    page,
+    pageObjects: { serviceMapPage },
+  }) => {
+    await serviceMapPage.typeInTheSearchBar();
+    await expect(page.getByTestId('serviceMap').getByLabel('Loading')).not.toBeVisible();
+    await page.getByText('No services available');
+    // search bar is still visible
+    await expect(page.getByTestId('apmUnifiedSearchBar')).toBeVisible();
+  });
 });
