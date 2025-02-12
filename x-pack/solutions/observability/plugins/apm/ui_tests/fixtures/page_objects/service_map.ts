c0990babdc7171887d19102d97f28951db9bee30
Jenny <dzheni.pavlova@elastic.co>
2025-02-12 18:59:16 +0100

    Service map tests

--- x-pack/solutions/observability/plugins/apm/ui_tests/fixtures/page_objects/service_map.ts
+++ x-pack/solutions/observability/plugins/apm/ui_tests/fixtures/page_objects/service_map.ts
@@ -10,14 +10,31 @@ import type { KibanaUrl, ScoutPage } from '@kbn/scout';
 export class ServiceMapPage {
   constructor(private readonly page: ScoutPage, private readonly kbnUrl: KibanaUrl) {}
 
+  async waitForPageToLoad() {
+    await this.page.waitForSelector(
+      '[data-test-subj="kbnAppWrapper visibleChrome"] [aria-busy="false"]',
+      { state: 'visible' }
+    );
+  }
+
   async gotoWithDateSelected(start: string, end: string) {
     this.page.goto(`${this.kbnUrl.app('apm')}/service-map?&rangeFrom=${start}&rangeTo=${end}`);
+    await this.waitForPageToLoad();
+  }
+  async gotoDetailedServiceMapWithDateSelected(start: string, end: string) {
+    this.page.goto(
+      `${this.kbnUrl.app(
+        'apm'
+      )}/services/opbeans-java/service-map?&rangeFrom=${start}&rangeTo=${end}`
+    );
+    await this.waitForPageToLoad();
   }
   async getSearchBar() {
     await this.page.testSubj.waitForSelector('apmUnifiedSearchBar');
   }
   async typeInTheSearchBar() {
     await this.getSearchBar();
-    await this.page.testSubj.typeWithDelay('apmUnifiedSearchBar', `_id : foo{enter}`);
+    await this.page.testSubj.typeWithDelay('apmUnifiedSearchBar', '_id : foo');
+    await this.page.getByTestId('querySubmitButton').press('Enter');
   }
 }
