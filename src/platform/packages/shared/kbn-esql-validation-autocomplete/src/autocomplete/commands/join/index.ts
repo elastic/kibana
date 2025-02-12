6a49ac0124ce165733fba524f111362ee9872b44
Dima Arnautov <dmitrii.arnautov@elastic.co>
2025-02-12 18:38:45 +0100

    [ES|QL] Fix WHERE autocomplete with MATCH before LIMIT (#210607)
    ## Summary
    
    Related PR https://github.com/elastic/kibana/pull/199032
    
    Fixes `WHERE` autocomplete with `MATCH` before `LIMIT`.
    
    The previous check was filtering suggestions based on all present
    commands, not just the previous one.
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    (cherry picked from commit 1ccb6db350425f8cdfbacd6341a3e4defaba1bcb)
    
    # Conflicts:
    #	src/platform/packages/shared/kbn-esql-validation-autocomplete/src/autocomplete/commands/join/index.ts

--- src/platform/packages/shared/kbn-esql-validation-autocomplete/src/autocomplete/commands/join/index.ts
+++ src/platform/packages/shared/kbn-esql-validation-autocomplete/src/autocomplete/commands/join/index.ts
@@ -8,7 +8,7 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import { type ESQLAstItem, ESQLAst } from '@kbn/esql-ast';
+import { type ESQLAstItem, type ESQLCommand } from '@kbn/esql-ast';
 import { ESQLCommand } from '@kbn/esql-ast/src/types';
 import type { ESQLCallbacks } from '../../../shared/types';
 import {
@@ -45,7 +45,7 @@ export const suggest: CommandBaseDefinition<'join'>['suggest'] = async (
   getSuggestedVariableName: () => string,
   getExpressionType: (expression: ESQLAstItem | undefined) => SupportedDataType | 'unknown',
   getPreferences?: () => Promise<{ histogramBarTarget: number } | undefined>,
-  fullTextAst?: ESQLAst,
+  previousCommands?: ESQLCommand[],
   definition?: CommandDefinition<'join'>,
   callbacks?: ESQLCallbacks
 ): Promise<SuggestionRawDefinition[]> => {
