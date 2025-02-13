d414b7c40daea9fbd8cfac42892f100009cd8e8c
Angela Chuang <yi-chun.chuang@elastic.co>
2025-02-13 22:14:55 +0800

    hover actions on network overview page

--- x-pack/solutions/security/plugins/security_solution/public/timelines/components/field_renderers/field_renderers.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/timelines/components/field_renderers/field_renderers.tsx
@@ -19,6 +19,7 @@ import { DefaultDraggable } from '../../../common/components/draggables';
 import { getEmptyTagValue } from '../../../common/components/empty_value';
 import { HostDetailsLink, ReputationLink, WhoIsLink } from '../../../common/components/links';
 import * as i18n from '../../../explore/network/components/details/translations';
+import type { SourcererScopeName } from '../../../sourcerer/store/model';
 
 export const IpOverviewId = 'ip-overview';
 
@@ -129,6 +130,7 @@ export const hostIdRenderer = ({
   );
 
 export const hostNameRenderer = (
+  scopeId: SourcererScopeName,
   host?: HostEcs,
   ipFilter?: string,
   contextID?: string
@@ -146,6 +148,7 @@ export const hostNameRenderer = (
       value={host.name[0]}
       isAggregatable={true}
       fieldType={'keyword'}
+      scopeId={scopeId}
     >
       <HostDetailsLink hostName={host.name[0]}>
         {host.name ? host.name : getEmptyTagValue()}
