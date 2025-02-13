646d2d014a0d81a1d01f5227518f9892d65db69c
Julia Bardi <90178898+juliaElastic@users.noreply.github.com>
2025-02-13 14:15:10 +0100

    Update hooks.tsx

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/hooks.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/hooks.tsx
@@ -35,7 +35,7 @@ export function useOutputs(
       return [];
     }
     return outputsData.items.filter(
-      (output) => allowedOutputTypes.includes(output.type) && !output.is_managed
+      (output) => allowedOutputTypes.includes(output.type) && !output.is_preconfigured
     );
   }, [allowedOutputTypes, canUseOutputPerIntegration, outputsData]);
   return {
