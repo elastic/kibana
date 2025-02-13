a2be6065b13508182df88986b8afe1cd9631e28d
seanrathier <sean.rathier@gmail.com>
2025-02-13 14:06:37 -0500

    [Cloud Security] fix setupTechnology GCP credential type swapping (#210668)
    (cherry picked from commit 4fe72b62b114d9b12f581ac4938e133eb869529f)
    
    # Conflicts:
    #	x-pack/test/cloud_security_posture_functional/constants/test_subject_ids.ts
    #	x-pack/test/security_solution_api_integration/test_suites/detections_response/utils/indices/index.ts
    #	x-pack/test_serverless/functional/test_suites/security/ftr/cloud_security_posture/agentless_api/constants.ts
    #	x-pack/test_serverless/functional/test_suites/security/ftr/cloud_security_posture/mki_only/agentless/mki_create_agent.ts

--- x-pack/test/cloud_security_posture_functional/page_objects/add_cis_integration_form_page.ts
+++ x-pack/test/cloud_security_posture_functional/page_objects/add_cis_integration_form_page.ts
@@ -303,12 +303,16 @@ export function AddCisIntegrationFormPageProvider({
     await radio.click();
   };
 
+  const getSetupTechnologyRadio = async (setupTechnology: 'agentless' | 'agent-based') => {
+    const radioGroup = await testSubjects.find(testSubjectIds.SETUP_TECHNOLOGY_SELECTOR);
+    return await radioGroup.findByCssSelector(`input[value='${setupTechnology}']`);
+  };
+
   const showSetupTechnologyComponent = async () => {
     return await testSubjects.exists(testSubjectIds.SETUP_TECHNOLOGY_SELECTOR);
   };
 
   const selectAwsCredentials = async (credentialType: 'direct' | 'temporary') => {
-    await clickOptionButton(AWS_CREDENTIAL_SELECTOR);
     await selectValue(
       AWS_CREDENTIAL_SELECTOR,
       credentialType === 'direct' ? 'direct_access_keys' : 'temporary_keys'
@@ -429,7 +433,7 @@ export function AddCisIntegrationFormPageProvider({
 
     await clickOptionButton(testSubjectIds.CIS_AWS_OPTION_TEST_ID);
 
-    await clickOptionButton(testSubjectIds.SETUP_TECHNOLOGY_SELECTOR_AGENTLESS_RADIO);
+    await selectSetupTechnology('agentless');
     await selectValue(testSubjectIds.AWS_CREDENTIAL_SELECTOR, 'direct_access_keys');
     await fillInTextField(testSubjectIds.DIRECT_ACCESS_KEY_ID_TEST_ID, directAccessKeyId);
     await fillInTextField(testSubjectIds.DIRECT_ACCESS_SECRET_KEY_TEST_ID, directAccessSecretKey);
@@ -441,7 +445,7 @@ export function AddCisIntegrationFormPageProvider({
 
     await clickOptionButton(testSubjectIds.CIS_GCP_OPTION_TEST_ID);
     await clickOptionButton(testSubjectIds.GCP_SINGLE_ACCOUNT_TEST_ID);
-    await clickOptionButton(testSubjectIds.SETUP_TECHNOLOGY_SELECTOR_AGENTLESS_RADIO);
+    await selectSetupTechnology('agentless');
     await fillInTextField(testSubjectIds.PRJ_ID_TEST_ID, projectId);
     await fillInTextField(testSubjectIds.CREDENTIALS_JSON_TEST_ID, credentialJson);
   };
@@ -466,6 +470,8 @@ export function AddCisIntegrationFormPageProvider({
     await navigateToAddIntegrationCspmPage();
     await PageObjects.header.waitUntilLoadingHasFinished();
 
+    await inputIntegrationName(`cloud_security_posture-${new Date().toISOString()}`);
+
     await fillOutForm(cloudProvider);
 
     // Click Save Button to create the Integration then navigate to Integration Policies Tab Page
@@ -482,6 +488,12 @@ export function AddCisIntegrationFormPageProvider({
 
     // Fill out form to edit an agentless integration
     await fillInTextField(testSubjectId, value);
+
+    // TechDebt: This is a workaround to ensure the form is saved
+    // const agentlessRadio = await getSetupTechnologyRadio('agentless');
+    // const agentBasedRadio = await getSetupTechnologyRadio('agent-based');
+    // expect(await agentlessRadio.isEnabled()).to.be(true);
+    // expect(agentBasedRadio.isEnabled()).to.be(true);
 
     // Clicking Save Button updates and navigates to Integration Policies Tab Page
     await clickSaveIntegrationButton();
@@ -548,6 +560,7 @@ export function AddCisIntegrationFormPageProvider({
     clickOptionButton,
     selectAwsCredentials,
     selectSetupTechnology,
+    getSetupTechnologyRadio,
     clickSaveButton,
     clickSaveIntegrationButton,
     clickAccordianButton,
