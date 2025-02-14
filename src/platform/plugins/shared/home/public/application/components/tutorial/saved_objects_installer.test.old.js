fdfeed8ad16962903f56e67d47d11851879d2d6d
paulina.shakirova <paulina.shakirova@elastic.co>
2025-02-14 18:12:45 +0100

    comment out old test files to silent errors about enzyme dep location

--- src/platform/plugins/shared/home/public/application/components/tutorial/saved_objects_installer.test.old.js
+++ src/platform/plugins/shared/home/public/application/components/tutorial/saved_objects_installer.test.old.js
@@ -7,87 +7,87 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import React from 'react';
-import { findTestSubject } from '@elastic/eui/lib/test';
-import { shallowWithIntl, mountWithIntl } from '@kbn/test-jest-helpers';
+// import React from 'react';
+// import { findTestSubject } from '@elastic/eui/lib/test';
+// import { shallowWithIntl, mountWithIntl } from '@kbn/test-jest-helpers';
 
-import { SavedObjectsInstaller } from './saved_objects_installer';
+// import { SavedObjectsInstaller } from './saved_objects_installer';
 
-test('renders', () => {
-  const component = shallowWithIntl(
-    <SavedObjectsInstaller.WrappedComponent bulkCreate={() => {}} savedObjects={[]} />
-  );
-  expect(component).toMatchSnapshot();
-});
+// test('renders', () => {
+//   const component = shallowWithIntl(
+//     <SavedObjectsInstaller.WrappedComponent bulkCreate={() => {}} savedObjects={[]} />
+//   );
+//   expect(component).toMatchSnapshot();
+// });
 
-describe('bulkCreate', () => {
-  const savedObject = {
-    id: '1',
-    type: 'index-pattern',
-    attributes: {},
-  };
-  test('should display success message when bulkCreate is successful', async () => {
-    const bulkCreateMock = () => {
-      return Promise.resolve({
-        savedObjects: [savedObject],
-      });
-    };
-    const component = mountWithIntl(
-      <SavedObjectsInstaller.WrappedComponent
-        bulkCreate={bulkCreateMock}
-        savedObjects={[savedObject]}
-      />
-    );
+// describe('bulkCreate', () => {
+//   const savedObject = {
+//     id: '1',
+//     type: 'index-pattern',
+//     attributes: {},
+//   };
+//   test('should display success message when bulkCreate is successful', async () => {
+//     const bulkCreateMock = () => {
+//       return Promise.resolve({
+//         savedObjects: [savedObject],
+//       });
+//     };
+//     const component = mountWithIntl(
+//       <SavedObjectsInstaller.WrappedComponent
+//         bulkCreate={bulkCreateMock}
+//         savedObjects={[savedObject]}
+//       />
+//     );
 
-    findTestSubject(component, 'loadSavedObjects').simulate('click');
+//     findTestSubject(component, 'loadSavedObjects').simulate('click');
 
-    // Ensure all promises resolve
-    await new Promise((resolve) => process.nextTick(resolve));
-    // Ensure the state changes are reflected
-    component.update();
+//     // Ensure all promises resolve
+//     await new Promise((resolve) => process.nextTick(resolve));
+//     // Ensure the state changes are reflected
+//     component.update();
 
-    expect(component.render()).toMatchSnapshot();
-  });
+//     expect(component.render()).toMatchSnapshot();
+//   });
 
-  test('should display error message when bulkCreate request fails', async () => {
-    const bulkCreateMock = () => {
-      return Promise.reject(new Error('simulated bulkRequest error'));
-    };
-    const component = mountWithIntl(
-      <SavedObjectsInstaller.WrappedComponent
-        bulkCreate={bulkCreateMock}
-        savedObjects={[savedObject]}
-      />
-    );
+//   test('should display error message when bulkCreate request fails', async () => {
+//     const bulkCreateMock = () => {
+//       return Promise.reject(new Error('simulated bulkRequest error'));
+//     };
+//     const component = mountWithIntl(
+//       <SavedObjectsInstaller.WrappedComponent
+//         bulkCreate={bulkCreateMock}
+//         savedObjects={[savedObject]}
+//       />
+//     );
 
-    findTestSubject(component, 'loadSavedObjects').simulate('click');
+//     findTestSubject(component, 'loadSavedObjects').simulate('click');
 
-    // Ensure all promises resolve
-    await new Promise((resolve) => process.nextTick(resolve));
-    // Ensure the state changes are reflected
-    component.update();
+//     // Ensure all promises resolve
+//     await new Promise((resolve) => process.nextTick(resolve));
+//     // Ensure the state changes are reflected
+//     component.update();
 
-    expect(component.render()).toMatchSnapshot();
-  });
+//     expect(component.render()).toMatchSnapshot();
+//   });
 
-  test('should filter out saved object version before calling bulkCreate', async () => {
-    const bulkCreateMock = jest.fn().mockResolvedValue({
-      savedObjects: [savedObject],
-    });
-    const component = mountWithIntl(
-      <SavedObjectsInstaller.WrappedComponent
-        bulkCreate={bulkCreateMock}
-        savedObjects={[{ ...savedObject, version: 'foo' }]}
-      />
-    );
+//   test('should filter out saved object version before calling bulkCreate', async () => {
+//     const bulkCreateMock = jest.fn().mockResolvedValue({
+//       savedObjects: [savedObject],
+//     });
+//     const component = mountWithIntl(
+//       <SavedObjectsInstaller.WrappedComponent
+//         bulkCreate={bulkCreateMock}
+//         savedObjects={[{ ...savedObject, version: 'foo' }]}
+//       />
+//     );
 
-    findTestSubject(component, 'loadSavedObjects').simulate('click');
+//     findTestSubject(component, 'loadSavedObjects').simulate('click');
 
-    // Ensure all promises resolve
-    await new Promise((resolve) => process.nextTick(resolve));
-    // Ensure the state changes are reflected
-    component.update();
+//     // Ensure all promises resolve
+//     await new Promise((resolve) => process.nextTick(resolve));
+//     // Ensure the state changes are reflected
+//     component.update();
 
-    expect(bulkCreateMock).toHaveBeenCalledWith([savedObject], expect.any(Object));
-  });
-});
+//     expect(bulkCreateMock).toHaveBeenCalledWith([savedObject], expect.any(Object));
+//   });
+// });
