b0046af437a40f75918f8318d0330f286ee4dff3
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-02-14 17:54:18 +0100

    resolve conflict

--- x-pack/solutions/observability/packages/kbn-scout-oblt/src/playwright/index.ts
+++ x-pack/solutions/observability/packages/kbn-scout-oblt/src/playwright/index.ts
@@ -5,9 +5,5 @@
  * 2.0.
  */
 
-<<<<<<<< HEAD:x-pack/platform/plugins/shared/logs_shared/public/containers/logs/log_highlights/index.ts
-export * from './log_highlights';
-========
 export * from './fixtures';
 export type { ObltPageObjects } from './page_objects';
->>>>>>>> bd13e829498 ([scout] adding test helper `@kbn/scout-oblt` package and uptate onboarding tests (#209761)):x-pack/solutions/observability/packages/kbn-scout-oblt/src/playwright/index.ts
