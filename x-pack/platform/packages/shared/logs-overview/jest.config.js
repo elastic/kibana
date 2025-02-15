d54cf11cfdec31223c5249d8244a9eaa0e407121
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-15 10:20:00 +0100

    Relocating module `@kbn/logs-overview`

--- x-pack/platform/packages/shared/logs-overview/jest.config.js
+++ x-pack/platform/packages/shared/logs-overview/jest.config.js
@@ -8,5 +8,5 @@
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
-  roots: ['<rootDir>/x-pack/platform/packages/shared/logs_overview'],
+  roots: ['<rootDir>/x-pack/platform/packages/shared/logs-overview'],
 };
