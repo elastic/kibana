759983fbc3c5d4f73f260e952f6f3730678bd960
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-17 12:36:43 +0100

    SKA: Fix kebab-case issues in obs-ux-management packages (#211312)
    ## Summary
    
    This PR aims at relocating some of the Kibana modules (plugins and
    packages) into a new folder structure, according to the _Sustainable
    Kibana Architecture_ initiative.
    
    > [!IMPORTANT]
    > * We kindly ask you to:
    > * Manually fix the errors in the error section below (if there are
    any).
    > * Search for the `packages[\/\\]` and `plugins[\/\\]` patterns in the
    source code (Babel and Eslint config files), and update them
    appropriately.
    > * Manually review
    `.buildkite/scripts/pipelines/pull_request/pipeline.ts` to ensure that
    any CI pipeline customizations continue to be correctly applied after
    the changed path names
    > * Review all of the updated files, specially the `.ts` and `.js` files
    listed in the sections below, as some of them contain relative paths
    that have been updated.
    > * Think of potential impact of the move, including tooling and
    configuration files that can be pointing to the relocated modules. E.g.:
    >     * customised eslint rules
    >     * docs pointing to source code
    
    > [!NOTE]
    > * This PR has been auto-generated.
    > * Any manual contributions will be lost if the 'relocate' script is
    re-run.
    > * Try to obtain the missing reviews / approvals before applying manual
    fixes, and/or keep your changes in a .patch / git stash.
    > * Please use
    [#sustainable_kibana_architecture](https://elastic.slack.com/archives/C07TCKTA22E)
    Slack channel for feedback.
    
    Are you trying to rebase this PR to solve merge conflicts? Please follow
    the steps describe
    [here](https://elastic.slack.com/archives/C07TCKTA22E/p1734019532879269?thread_ts=1734019339.935419&cid=C07TCKTA22E).
    
    #### 4 packages(s) are going to be relocated:
    
    | Id | Target folder |
    | -- | ------------- |
    | `@kbn/alerting-rule-utils` |
    `x-pack/platform/packages/shared/alerting-rule-utils` |
    | `@kbn/observability-alert-details` |
    `x-pack/solutions/observability/packages/alert-details` |
    | `@kbn/observability-alerting-test-data` |
    `x-pack/solutions/observability/packages/alerting-test-data` |
    | `@kbn/observability-get-padded-alert-time-range-util` |
    `x-pack/solutions/observability/packages/get-padded-alert-time-range-util`
    |
    
    <details >
    <summary>Updated references</summary>
    
    ```
    ./.i18nrc.json
    ./package.json
    ./packages/kbn-ts-projects/config-paths.json
    ./src/platform/packages/private/kbn-repo-packages/package-map.json
    ./tsconfig.base.json
    ./x-pack/platform/packages/shared/alerting-rule-utils/jest.config.js
    ./x-pack/solutions/observability/packages/alert-details/jest.config.js
    ./x-pack/solutions/observability/packages/alerting-test-data/jest.config.js
    ./x-pack/solutions/observability/packages/get-padded-alert-time-range-util/jest.config.js
    ./yarn.lock
    .github/CODEOWNERS
    ```
    
    </details><details >
    <summary>Updated relative paths</summary>
    
    ```
    x-pack/platform/packages/shared/alerting-rule-utils/jest.config.js:10
    x-pack/platform/packages/shared/alerting-rule-utils/tsconfig.json:2
    x-pack/solutions/observability/packages/alert-details/jest.config.js:10
    x-pack/solutions/observability/packages/alert-details/tsconfig.json:2
    x-pack/solutions/observability/packages/alerting-test-data/jest.config.js:10
    x-pack/solutions/observability/packages/alerting-test-data/tsconfig.json:2
    x-pack/solutions/observability/packages/get-padded-alert-time-range-util/jest.config.js:10
    x-pack/solutions/observability/packages/get-padded-alert-time-range-util/tsconfig.json:2
    ```
    
    </details>
    
    (cherry picked from commit 171dd6b0759710bf29df61485ebca068993b43f9)
    
    # Conflicts:
    #	.github/CODEOWNERS
    #	package.json
    #	tsconfig.base.json

--- x-pack/solutions/observability/packages/alerting-test-data/jest.config.js
+++ x-pack/solutions/observability/packages/alerting-test-data/jest.config.js
@@ -8,5 +8,5 @@
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
-  roots: ['<rootDir>/x-pack/platform/packages/shared/alerting_rule_utils'],
+  roots: ['<rootDir>/x-pack/solutions/observability/packages/alerting-test-data'],
 };
