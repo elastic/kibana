a31800d44cdbe49cc3cad81d670f1cc4cbde72ed
Søren Louv-Jansen <soren.louv@elastic.co>
2025-02-17 18:20:50 +0100

    [Obs AI Assistant] Add KB re-indexing when encountering `semantic_text` bug (#210386)
    Closes https://github.com/elastic/kibana/issues/210204
    
    This will automatically re-index the knowledge base if upon adding a KB
    entry there is this error:
    
    > The [sparse_vector] field type is not supported on indices created on
    versions 8.0 to 8.10
    
    That error means that semantic_text is not supported in the given index,
    and it should therefore be re-indexed.
    
    **How to test this PR:**
    
    **8.10**
    - `git checkout -B 8.10 origin/8.10`
    - Start Kibana:
      - `nvm use && yarn kbn bootstrap && yarn start`
    - Start ES
    - `nvm use && yarn es snapshot --license trial --E
    path.data="/Users/sorenlouv/elastic/kbn_es_data/upgrade_testing"`
    
    **8.19**
    - `git checkout -B 8.19 origin/8.x`
    - Start Kibana:
      - `nvm use && yarn kbn bootstrap && yarn start`
    - Start ES
    - `nvm use && yarn es snapshot --license trial --E
    path.data="/Users/sorenlouv/elastic/kbn_es_data/upgrade_testing"`
    - Install Knowledge base
    - Try adding an item to KB (it should fail ❌️)
    
    **9.1.0**
    - `gh pr checkout 210386`
    - Start Kibana:
      - `nvm use && yarn kbn bootstrap && yarn start`
    - Start ES
    - `nvm use && yarn es snapshot --license trial --E
    path.data="/Users/sorenlouv/elastic/kbn_es_data/upgrade_testing"`
    - Try adding an item to KB (it should succeed ✅️)
    
    **TODO:**
    
    - Add an upgrade test that covers this flow
    
    ---------
    
    Co-authored-by: Viduni Wickramarachchi <viduni.ushanka@gmail.com>
    (cherry picked from commit df67a09afab22521dfa9ff3ec3a4f624a039c462)

--- x-pack/test/api_integration/config.ts
+++ x-pack/test/api_integration/config.ts
@@ -38,7 +38,7 @@ export async function getApiIntegrationConfig({ readConfigFile }: FtrConfigProvi
       serverArgs: [
         ...xPackFunctionalTestsConfig.get('esTestCluster.serverArgs'),
         'node.attr.name=apiIntegrationTestNode',
-        'path.repo=/tmp/repo,/tmp/repo_1,/tmp/repo_2,/tmp/cloud-snapshots/',
+        `path.repo=/tmp/repo,/tmp/repo_1,/tmp/repo_2,/tmp/cloud-snapshots/`,
       ],
     },
   };
