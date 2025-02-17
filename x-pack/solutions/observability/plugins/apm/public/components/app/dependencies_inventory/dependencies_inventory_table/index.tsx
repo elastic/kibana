b9d27611bbc358c2be35304d4cb5155906c5ffb8
Caue Marcondes <caue.marcondes@elastic.co>
2025-02-17 10:24:55 -0300

    [APM] Breakdown Top dependencies API

--- x-pack/solutions/observability/plugins/apm/public/components/app/dependencies_inventory/dependencies_inventory_table/index.tsx
+++ x-pack/solutions/observability/plugins/apm/public/components/app/dependencies_inventory/dependencies_inventory_table/index.tsx
@@ -7,7 +7,7 @@
 
 import { METRIC_TYPE } from '@kbn/analytics';
 import { i18n } from '@kbn/i18n';
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useEuiTheme } from '@elastic/eui';
 import { css } from '@emotion/react';
 import { useUiTracker } from '@kbn/observability-shared-plugin/public';
@@ -15,9 +15,13 @@ import { usePerformanceContext } from '@kbn/ebt-tools';
 import { isTimeComparison } from '../../../shared/time_comparison/get_comparison_options';
 import { getNodeName, NodeType } from '../../../../../common/connections';
 import { useApmParams } from '../../../../hooks/use_apm_params';
-import { FETCH_STATUS, useFetcher } from '../../../../hooks/use_fetcher';
+import { FETCH_STATUS, useFetcher, isPending } from '../../../../hooks/use_fetcher';
 import { useTimeRange } from '../../../../hooks/use_time_range';
 import { DependencyLink } from '../../../shared/links/dependency_link';
+import type {
+  DependenciesItem,
+  FormattedSpanMetricGroup,
+} from '../../../shared/dependencies_table';
 import { DependenciesTable } from '../../../shared/dependencies_table';
 import { RandomSamplerBadge } from '../random_sampler_badge';
 
@@ -27,6 +31,7 @@ export function DependenciesInventoryTable() {
   } = useApmParams('/dependencies/inventory');
   const { onPageReady } = usePerformanceContext();
   const { start, end } = useTimeRange({ rangeFrom, rangeTo });
+  const [renderedItems, setRenderedItems] = useState<FormattedSpanMetricGroup[]>([]);
 
   const { euiTheme } = useEuiTheme();
   const trackEvent = useUiTracker();
@@ -38,19 +43,41 @@ export function DependenciesInventoryTable() {
       }
 
       return callApmApi('GET /internal/apm/dependencies/top_dependencies', {
-        params: {
-          query: {
-            start,
-            end,
-            environment,
-            numBuckets: 8,
-            offset: comparisonEnabled && isTimeComparison(offset) ? offset : undefined,
-            kuery,
-          },
-        },
+        params: { query: { start, end, environment, numBuckets: 8, kuery } },
       });
     },
-    [start, end, environment, offset, kuery, comparisonEnabled]
+    [start, end, environment, kuery]
+  );
+
+  const renderedDependencyNames = useMemo(
+    () =>
+      renderedItems.length
+        ? JSON.stringify(renderedItems.map((item) => item.name).sort())
+        : undefined,
+    [renderedItems]
+  );
+
+  const { data: timeseriesData, status: timeseriesStatus } = useFetcher(
+    (callApmApi) => {
+      if (renderedDependencyNames) {
+        return callApmApi('POST /internal/apm/dependencies/top_dependencies/statistics', {
+          params: {
+            query: {
+              start,
+              end,
+              environment,
+              numBuckets: 8,
+              offset: comparisonEnabled && isTimeComparison(offset) ? offset : undefined,
+              kuery,
+            },
+            body: { dependencyNames: renderedDependencyNames },
+          },
+        });
+      }
+    },
+    // Disables exhaustive deps because the statistics api must only be called when the rendered items changed or when comparison is toggled or changed.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [renderedDependencyNames, comparisonEnabled, offset]
   );
 
   useEffect(() => {
@@ -64,46 +91,91 @@ export function DependenciesInventoryTable() {
     }
   }, [status, onPageReady, rangeFrom, rangeTo]);
 
-  const dependencies =
-    data?.dependencies.map((dependency) => {
-      const { location } = dependency;
-      const name = getNodeName(location);
+  const dependencies: DependenciesItem[] = useMemo(
+    () =>
+      data?.dependencies.map((dependency) => {
+        const { location } = dependency;
+        const name = getNodeName(location);
 
-      if (location.type !== NodeType.dependency) {
-        throw new Error('Expected a dependency node');
-      }
-      const link = (
-        <DependencyLink
-          type={location.spanType}
-          subtype={location.spanSubtype}
-          query={{
-            dependencyName: location.dependencyName,
-            comparisonEnabled,
-            offset,
-            environment,
-            kuery,
-            rangeFrom,
-            rangeTo,
-          }}
-          onClick={() => {
-            trackEvent({
-              app: 'apm',
-              metricType: METRIC_TYPE.CLICK,
-              metric: 'dependencies_inventory_to_dependency_detail',
-            });
-          }}
-        />
-      );
+        if (location.type !== NodeType.dependency) {
+          throw new Error('Expected a dependency node');
+        }
+        const link = (
+          <DependencyLink
+            type={location.spanType}
+            subtype={location.spanSubtype}
+            query={{
+              dependencyName: location.dependencyName,
+              comparisonEnabled,
+              offset,
+              environment,
+              kuery,
+              rangeFrom,
+              rangeTo,
+            }}
+            onClick={() => {
+              trackEvent({
+                app: 'apm',
+                metricType: METRIC_TYPE.CLICK,
+                metric: 'dependencies_inventory_to_dependency_detail',
+              });
+            }}
+          />
+        );
 
-      return {
-        name,
-        currentStats: dependency.currentStats,
-        previousStats: dependency.previousStats,
-        link,
-      };
-    }) ?? [];
-
+        return {
+          name,
+          currentStats: {
+            impact: dependency.currentStats.impact,
+            totalTime: { value: dependency.currentStats.totalTime.value },
+            latency: {
+              value: dependency.currentStats.latency.value,
+              timeseries: timeseriesData?.currentTimeseries[name]?.latency,
+            },
+            throughput: {
+              value: dependency.currentStats.throughput.value,
+              timeseries: timeseriesData?.currentTimeseries[name]?.throughput,
+            },
+            errorRate: {
+              value: dependency.currentStats.errorRate.value,
+              timeseries: timeseriesData?.currentTimeseries[name]?.errorRate,
+            },
+          },
+          previousStats: {
+            impact: dependency.previousStats?.impact ?? 0,
+            totalTime: { value: dependency.previousStats?.totalTime.value ?? null },
+            latency: {
+              value: dependency.previousStats?.latency.value ?? null,
+              timeseries: timeseriesData?.comparisonTimeseries?.[name]?.latency,
+            },
+            throughput: {
+              value: dependency.previousStats?.throughput.value ?? null,
+              timeseries: timeseriesData?.comparisonTimeseries?.[name]?.throughput,
+            },
+            errorRate: {
+              value: dependency.previousStats?.errorRate.value ?? null,
+              timeseries: timeseriesData?.comparisonTimeseries?.[name]?.errorRate,
+            },
+          },
+          link,
+        };
+      }) ?? [],
+    [
+      comparisonEnabled,
+      data?.dependencies,
+      environment,
+      kuery,
+      offset,
+      rangeFrom,
+      rangeTo,
+      timeseriesData?.comparisonTimeseries,
+      timeseriesData?.currentTimeseries,
+      trackEvent,
+    ]
+  );
   const showRandomSamplerBadge = data?.sampled && status === FETCH_STATUS.SUCCESS;
+  const fetchingStatus =
+    isPending(status) || isPending(timeseriesStatus) ? FETCH_STATUS.LOADING : FETCH_STATUS.SUCCESS;
 
   return (
     <>
@@ -121,9 +193,10 @@ export function DependenciesInventoryTable() {
         nameColumnTitle={i18n.translate('xpack.apm.dependenciesInventory.dependencyTableColumn', {
           defaultMessage: 'Dependency',
         })}
-        status={status}
+        status={fetchingStatus}
         compact={false}
         initialPageSize={25}
+        onChangeRenderedItems={setRenderedItems}
       />
     </>
   );
