b93724f9ef42afdc4ea109d5d9bdfbe955f7a95f
Ying <ying.mao@elastic.co>
2025-02-18 16:11:31 -0500

    stubs

--- x-pack/platform/plugins/shared/alerting/server/alert_deletion/alert_deletion_client.ts
+++ x-pack/platform/plugins/shared/alerting/server/alert_deletion/alert_deletion_client.ts
@@ -5,18 +5,23 @@
  * 2.0.
  */
 
-import { ElasticsearchClient, Logger } from '@kbn/core/server';
+import { ElasticsearchClient, ISavedObjectsRepository, Logger } from '@kbn/core/server';
 import {
   TaskManagerSetupContract,
   TaskManagerStartContract,
   ConcreteTaskInstance,
 } from '@kbn/task-manager-plugin/server';
+import { RULES_SETTINGS_SAVED_OBJECT_TYPE, RulesSettingsAlertDeletionProperties } from '../types';
+import { SpacesPluginStart } from '@kbn/spaces-plugin/server';
+import { spaceIdToNamespace } from '../lib';
 
 export const ALERT_DELETION_TASK_TYPE = 'alert-deletion';
 
 interface ConstructorOpts {
   elasticsearchClientPromise: Promise<ElasticsearchClient>;
+  internalSavedObjectsRepositoryPromise: Promise<ISavedObjectsRepository>;
   logger: Logger;
+  spacesStartPromise: Promise<SpacesPluginStart | undefined>;
   taskManagerSetup: TaskManagerSetupContract;
   taskManagerStartPromise: Promise<TaskManagerStartContract>;
 }
@@ -24,11 +29,15 @@ interface ConstructorOpts {
 export class AlertDeletionClient {
   private logger: Logger;
   private elasticsearchClientPromise: Promise<ElasticsearchClient>;
+  private readonly internalSavedObjectsRepositoryPromise: Promise<ISavedObjectsRepository>;
+  private readonly spacesPluginStartPromise: Promise<SpacesPluginStart | undefined>;
   private readonly taskManagerStartPromise: Promise<TaskManagerStartContract>;
 
   constructor(opts: ConstructorOpts) {
     this.elasticsearchClientPromise = opts.elasticsearchClientPromise;
+    this.internalSavedObjectsRepositoryPromise = opts.internalSavedObjectsRepositoryPromise;
     this.logger = opts.logger;
+    this.spacesPluginStartPromise = opts.spacesStartPromise;
     this.taskManagerStartPromise = opts.taskManagerStartPromise;
 
     // Registers the task that handles alert deletion
@@ -67,15 +76,27 @@ export class AlertDeletionClient {
     }
   }
 
+  public previewTask(settings: RulesSettingsAlertDeletionProperties, spaceId: string): number {
+    // Call library function to preview the number of alerts that would be deleted
+    return 0;
+  }
+
   private runTask = async (
     taskInstance: ConcreteTaskInstance,
     abortController: AbortController
   ) => {
     try {
       const esClient = await this.elasticsearchClientPromise;
+      const internalSavedObjectsRepository = await this.internalSavedObjectsRepositoryPromise;
+      const spaces = await this.spacesPluginStartPromise;
       const spaceIds = taskInstance.params.spaceIds;
+      const namespaces = spaceIds.map((spaceId: string) => spaceIdToNamespace(spaces, spaceId));
 
-      // Query for rules settings in the specified space
+      // Query for rules settings in the specified spaces
+      const rulesSettings = internalSavedObjectsRepository.find({
+        type: RULES_SETTINGS_SAVED_OBJECT_TYPE,
+        namespaces,
+      });
 
       // For each rules settings, call the library function to delete alerts
     } catch (err) {}
--- x-pack/platform/plugins/shared/alerting/server/plugin.ts
+++ x-pack/platform/plugins/shared/alerting/server/plugin.ts
@@ -72,7 +72,7 @@ import {
 } from './rules_settings';
 import { MaintenanceWindowClientFactory } from './maintenance_window_client_factory';
 import { ILicenseState, LicenseState } from './lib/license_state';
-import { AlertingRequestHandlerContext, ALERTING_FEATURE_ID, RuleAlertData } from './types';
+import { AlertingRequestHandlerContext, ALERTING_FEATURE_ID, RuleAlertData, RULES_SETTINGS_SAVED_OBJECT_TYPE } from './types';
 import { defineRoutes } from './routes';
 import {
   AlertInstanceContext,
@@ -113,9 +113,10 @@ import { maintenanceWindowFeature } from './maintenance_window_feature';
 import { ConnectorAdapterRegistry } from './connector_adapters/connector_adapter_registry';
 import { ConnectorAdapter, ConnectorAdapterParams } from './connector_adapters/types';
 import { DataStreamAdapter, getDataStreamAdapter } from './alerts_service/lib/data_stream_adapter';
-import { createGetAlertIndicesAliasFn, GetAlertIndicesAlias } from './lib';
+import { createGetAlertIndicesAliasFn, GetAlertIndicesAlias, spaceIdToNamespace } from './lib';
 import { BackfillClient } from './backfill_client/backfill_client';
 import { MaintenanceWindowsService } from './task_runner/maintenance_windows';
+import { AlertDeletionClient } from './alert_deletion';
 
 export const EVENT_LOG_PROVIDER = 'alerting';
 export const EVENT_LOG_ACTIONS = {
@@ -233,6 +234,7 @@ export class AlertingPlugin {
   private pluginStop$: Subject<void>;
   private dataStreamAdapter?: DataStreamAdapter;
   private backfillClient?: BackfillClient;
+  private alertDeletionClient?: AlertDeletionClient;
   private readonly isServerless: boolean;
   private nodeRoles: PluginInitializerContext['node']['roles'];
   private readonly connectorAdapterRegistry = new ConnectorAdapterRegistry();
@@ -299,6 +301,18 @@ export class AlertingPlugin {
       taskManagerSetup: plugins.taskManager,
       taskManagerStartPromise,
       taskRunnerFactory: this.taskRunnerFactory,
+    });
+
+    this.alertDeletionClient = new AlertDeletionClient({
+      elasticsearchClientPromise: core
+            .getStartServices()
+            .then(([{ elasticsearch }]) => elasticsearch.client.asInternalUser),
+      internalSavedObjectsRepositoryPromise: core.getStartServices().then(([{ savedObjects }]) =>
+        savedObjects.createInternalRepository([RULES_SETTINGS_SAVED_OBJECT_TYPE])),
+      logger: this.logger,
+      spacesStartPromise: core.getStartServices().then(([_, alertingStart]) => alertingStart.spaces),
+      taskManagerSetup: plugins.taskManager,
+      taskManagerStartPromise,
     });
 
     this.eventLogger = plugins.eventLog.getLogger({
@@ -409,6 +423,7 @@ export class AlertingPlugin {
       config$: plugins.unifiedSearch.autocomplete.getInitializerContextConfig().create(),
       isServerless: this.isServerless,
       docLinks: core.docLinks,
+      alertDeletionClient: this.alertDeletionClient,
     });
 
     return {
@@ -507,12 +522,6 @@ export class AlertingPlugin {
       includedHiddenTypes: [RULE_SAVED_OBJECT_TYPE, AD_HOC_RUN_SAVED_OBJECT_TYPE],
     });
 
-    const spaceIdToNamespace = (spaceId?: string) => {
-      return plugins.spaces && spaceId
-        ? plugins.spaces.spacesService.spaceIdToNamespace(spaceId)
-        : undefined;
-    };
-
     alertingAuthorizationClientFactory.initialize({
       ruleTypeRegistry: ruleTypeRegistry!,
       securityPluginStart: plugins.security,
@@ -536,7 +545,7 @@ export class AlertingPlugin {
         AD_HOC_RUN_SAVED_OBJECT_TYPE,
       ]),
       encryptedSavedObjectsClient,
-      spaceIdToNamespace,
+      spaceIdToNamespace: (spaceId?: string) => spaceIdToNamespace(plugins.spaces, spaceId),
       getSpaceId(request: KibanaRequest) {
         return plugins.spaces?.spacesService.getSpaceId(request) ?? DEFAULT_SPACE_ID;
       },
@@ -621,7 +630,7 @@ export class AlertingPlugin {
       }),
       savedObjects: core.savedObjects,
       share: plugins.share,
-      spaceIdToNamespace,
+      spaceIdToNamespace: (spaceId?: string) => spaceIdToNamespace(plugins.spaces, spaceId),
       uiSettings: core.uiSettings,
       usageCounter: this.usageCounter,
       getEventLogClient: (request: KibanaRequest) => plugins.eventLog.getClient(request),
