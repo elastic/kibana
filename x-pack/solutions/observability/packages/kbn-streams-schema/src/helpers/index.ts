707cc2c63cf4f4f460cad2c213f02d0a08cb2cfa
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2025-02-18 10:07:34 +0100

    refactor(streams): fix exports order

--- x-pack/solutions/observability/packages/kbn-streams-schema/src/helpers/index.ts
+++ x-pack/solutions/observability/packages/kbn-streams-schema/src/helpers/index.ts
@@ -5,9 +5,9 @@
  * 2.0.
  */
 
+export * from './type_guards';
+export * from './hierarchy';
+export * from './lifecycle';
 export * from './condition_fields';
 export * from './condition_to_query_dsl';
 export * from './field_definition';
-export * from './hierarchy';
-export * from './lifecycle';
-export * from './type_guards';
