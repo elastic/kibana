3f08dde6aa8e0def0dea8cfab8922f5f18b5e157
Julia <iuliia.guskova@elastic.co>
2025-02-18 17:11:20 +0100

    remove waitFor in mports

--- x-pack/platform/plugins/shared/alerting/public/pages/maintenance_windows/components/table_actions_popover.test.tsx
+++ x-pack/platform/plugins/shared/alerting/public/pages/maintenance_windows/components/table_actions_popover.test.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { fireEvent, waitFor } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
 import React from 'react';
 import userEvent from '@testing-library/user-event';
 
