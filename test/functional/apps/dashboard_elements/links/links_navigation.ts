9a8b3b63ce04e7b6dd8a690f90e1fda54a4a06c6
Hannah Mudge <hannah.wright@elastic.co>
2025-02-18 13:46:53 -0700

    add `.only`

--- test/functional/apps/dashboard_elements/links/links_navigation.ts
+++ test/functional/apps/dashboard_elements/links/links_navigation.ts
@@ -29,7 +29,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const FROM_TIME = 'Oct 22, 2018 @ 00:00:00.000';
   const TO_TIME = 'Dec 3, 2018 @ 00:00:00.000';
 
-  describe('links panel navigation', () => {
+  describe.only('links panel navigation', () => {
     before(async () => {
       await kibanaServer.savedObjects.cleanStandardList();
       await security.testUser.setRoles([
