c3aa9b79e96a722d8cbc82f353ab25f214b3bc8d
Charlotte Alexandra Wilson <CAWilson94@users.noreply.github.com>
2025-02-18 15:09:12 +0000

    Updated EnablementModalCallout name to AdditionalChargesMessage (#203061) This PR renames the enablementModalCallout component to AdditionalChargesMessage for better clarity and alignment with its purpose, addressing [this comment](https://github.com/elastic/kibana/pull/201920#pullrequestreview-2467486755) from the original PR.
    ![image](https://github.com/user-attachments/assets/c07ac4a4-51db-4d5c-9040-3cde4fbd199e)
    
    Check the PR satisfies following conditions.
    
    Reviewers should verify this PR satisfies this list as well.
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    Co-authored-by: Mark Hopkin <mark.hopkin@elastic.co>
    (cherry picked from commit a21fb6c7aeb06fc21f552d613f8387f73f5d3a27)

--- x-pack/plugins/security_solution/public/flyout/entity_details/user_right/content.tsx
+++ x-pack/plugins/security_solution/public/flyout/entity_details/user_right/content.tsx
@@ -57,7 +57,9 @@ export const UserPanelContent = ({
 
   return (
     <FlyoutBody>
-      {riskScoreState.isModuleEnabled && riskScoreState.data?.length !== 0 && (
+      {Array.isArray(riskScoreState.data) &&
+        riskScoreState.isModuleEnabled &&
+        riskScoreState.data.length > 0 && (
         <>
           <FlyoutRiskSummary
             riskScoreData={riskScoreState}
