60e5033863ea386652f00e9c0acffe354cad46bf
Tiago Costa <tiago.costa@elastic.co>
2025-02-19 01:03:16 +0000

    fix(NA): jest tests setup

--- x-pack/platform/plugins/shared/observability_ai_assistant/.storybook/jest_setup.js
+++ x-pack/platform/plugins/shared/observability_ai_assistant/.storybook/jest_setup.js
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { setGlobalConfig } from '@storybook/test';
+import { setProjectAnnotations } from '@storybook/react';
 import * as globalStorybookConfig from './preview';
 
-setGlobalConfig(globalStorybookConfig);
+setProjectAnnotations(globalStorybookConfig);
