11385afdbbbdf76e8db8d45c5db2e97a7c2f46a2
Maryam Saeidi <maryam.saeidi@elastic.co>
2025-02-19 14:08:07 +0100

    Replace alert status test with controls

--- x-pack/test/functional/page_objects/alert_controls.ts
+++ x-pack/test/functional/page_objects/alert_controls.ts
@@ -10,15 +10,11 @@ import { FtrProviderContext } from '../ftr_provider_context';
 
 export function AlertControlsProvider({ getService }: FtrProviderContext) {
   const testSubjects = getService('testSubjects');
+  const find = getService('find');
   const log = getService('log');
   const retry = getService('retry');
 
   return {
-    /* -----------------------------------------------------------
-     Individual controls functions
-     ----------------------------------------------------------- */
-
-    // Control Frame functions
     async getControlElementById(controlId: string): Promise<WebElementWrapper> {
       const errorText = `Control frame ${controlId} could not be found`;
       let controlElement: WebElementWrapper | undefined;
@@ -50,6 +46,57 @@ export function AlertControlsProvider({ getService }: FtrProviderContext) {
       log.debug(`clearing all selections from control ${controlId}`);
       await this.hoverOverExistingControl(controlId);
       await testSubjects.click(`control-action-${controlId}-erase`);
+    },
+
+    async optionsListOpenPopover(controlId: string) {
+      log.debug(`Opening popover for Options List: ${controlId}`);
+      await retry.try(async () => {
+        await testSubjects.click(`optionsList-control-${controlId}`);
+        await retry.waitForWithTimeout('popover to open', 500, async () => {
+          return await testSubjects.exists(`optionsList-control-popover`);
+        });
+      });
+    },
+
+    async optionsListPopoverAssertOpen() {
+      await retry.try(async () => {
+        if (!(await testSubjects.exists(`optionsList-control-available-options`))) {
+          throw new Error('options list popover must be open before calling selectOption');
+        }
+      });
+    },
+
+    async optionsListPopoverWaitForLoading() {
+      log.debug(`wait for the suggestions in the popover to load`);
+      await this.optionsListPopoverAssertOpen();
+      await testSubjects.waitForDeleted('optionsList-control-popover-loading');
+    },
+
+    async optionsListPopoverSelectOption(availableOption: string) {
+      log.debug(`selecting ${availableOption} from options list`);
+      await this.optionsListPopoverAssertOpen();
+
+      await retry.try(async () => {
+        await testSubjects.existOrFail(`optionsList-control-selection-${availableOption}`);
+        await testSubjects.click(`optionsList-control-selection-${availableOption}`);
+      });
+    },
+
+    async isOptionsListPopoverOpen(controlId: string) {
+      const isPopoverOpen = await find.existsByCssSelector(`#control-popover-${controlId}`);
+      log.debug(`Is popover open: ${isPopoverOpen} for Options List: ${controlId}`);
+      return isPopoverOpen;
+    },
+
+    async optionsListEnsurePopoverIsClosed(controlId: string) {
+      log.debug(`Ensure popover is closed for Options List: ${controlId}`);
+      await retry.try(async () => {
+        const isPopoverOpen = await this.isOptionsListPopoverOpen(controlId);
+        if (isPopoverOpen) {
+          await testSubjects.click(`optionsList-control-${controlId}`);
+          await testSubjects.waitForDeleted(`optionsList-control-available-options`);
+        }
+      });
     },
   };
 }
