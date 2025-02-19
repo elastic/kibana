b396cbfb320a859c9b97beec4ad652cf473d61e4
mbondyra <marta.bondyra@elastic.co>
2025-02-19 13:05:56 +0100

    layer_panel

--- x-pack/platform/plugins/shared/lens/public/editor_frame_service/editor_frame/config_panel/layer_panel.tsx
+++ x-pack/platform/plugins/shared/lens/public/editor_frame_service/editor_frame/config_panel/layer_panel.tsx
@@ -363,7 +363,13 @@ export function LayerPanel(props: LayerPanelProps) {
       <section
         tabIndex={-1}
         ref={registerLayerRef}
-        className="lnsLayerPanel"
+        css={css`
+          margin-bottom:  ${euiTheme.size.base};
+          // disable focus ring for mouse clicks, leave it for keyboard users
+          &:focus:not(:focus-visible) {
+            animation: none !important; // sass-lint:disable-line no-important
+          }
+        `}
         data-test-subj={`lns-layerPanel-${layerIndex}`}
       >
         <EuiPanel paddingSize="none" hasShadow={false} hasBorder>
@@ -485,6 +491,29 @@ export function LayerPanel(props: LayerPanelProps) {
               const isOptional = !group.requiredMinDimensionCount && !group.suggestedValue;
               return (
                 <EuiFormRow
+                  css={css`
+                     padding: ${euiTheme.size.base};
+                    &:last-child {
+                      border-radius: 0 0 ${euiTheme.border.radius.medium} ${euiTheme.border.radius.medium};
+                    }
+
+                    // Add border to the top of the next same panel
+                    &+& {
+                      border-top: ${euiTheme.border.thin};
+                      margin-top: 0;
+                    }
+
+                    &>* {
+                      margin-bottom: 0;
+                    }
+
+                    // Targeting EUI class as we are unable to apply a class to this element in component
+                    &,
+                    .euiFormRow__fieldWrapper {
+                      &>*+* {
+                        margin-top: ${euiTheme.size.s};
+                      }
+                    }`}
                   className="lnsLayerPanel__row"
                   fullWidth
                   label={
