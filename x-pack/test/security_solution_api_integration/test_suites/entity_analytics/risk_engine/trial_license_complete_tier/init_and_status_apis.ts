617f60a5f67e1d872942237bac5d8a897a654b0a
Mark Hopkin <mark.hopkin@elastic.co>
2025-02-20 08:22:32 +0000

    allow task to be running

--- x-pack/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/init_and_status_apis.ts
+++ x-pack/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/init_and_status_apis.ts
@@ -5,14 +5,14 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect';
+import expect from '@kbn/expect/expect';
 import { riskEngineConfigurationTypeName } from '@kbn/security-solution-plugin/server/lib/entity_analytics/risk_engine/saved_object';
 
 import { riskEngineRouteHelpersFactory } from '../../utils';
 import { FtrProviderContext } from '../../../../ftr_provider_context';
 
-const expectTaskIsNotRunning = (taskStatus?: string) => {
-  expect(['idle', 'claiming']).contain(taskStatus);
+const expectTaskIsHealthy = (taskStatus?: string) => {
+  expect(['idle', 'claiming', 'running']).contain(taskStatus);
 };
 
 export default ({ getService }: FtrProviderContext) => {
@@ -762,7 +762,7 @@ export default ({ getService }: FtrProviderContext) => {
         expect(status2.body.risk_engine_status).to.be('ENABLED');
 
         expect(status2.body.risk_engine_task_status?.runAt).to.be.a('string');
-        expectTaskIsNotRunning(status2.body.risk_engine_task_status?.status);
+        expectTaskIsHealthy(status2.body.risk_engine_task_status?.status);
         expect(status2.body.risk_engine_task_status?.startedAt).to.be(undefined);
 
         await riskEngineRoutes.disable();
@@ -778,7 +778,7 @@ export default ({ getService }: FtrProviderContext) => {
         expect(status4.body.risk_engine_status).to.be('ENABLED');
 
         expect(status4.body.risk_engine_task_status?.runAt).to.be.a('string');
-        expectTaskIsNotRunning(status4.body.risk_engine_task_status?.status);
+        expectTaskIsHealthy(status4.body.risk_engine_task_status?.status);
         expect(status4.body.risk_engine_task_status?.startedAt).to.be(undefined);
       });
     });
