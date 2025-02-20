c688bfccf49e218d6422808d089fa72497b07758
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-20 13:12:03 +0100

    Relocating module `@kbn/expect`

--- src/platform/packages/shared/kbn-expect/jest.config.js
+++ src/platform/packages/shared/kbn-expect/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-expect'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-expect'],
 };
