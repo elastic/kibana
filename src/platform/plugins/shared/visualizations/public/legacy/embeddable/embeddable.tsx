868575b2e1ddd1227f10abe620585007873f580c
Nathan Reese <reese.nathan@gmail.com>
2025-02-20 11:09:45 -0700

    [embeddable] remove EmbeddableInput type

--- src/platform/plugins/shared/visualizations/public/legacy/embeddable/embeddable.tsx
+++ src/platform/plugins/shared/visualizations/public/legacy/embeddable/embeddable.tsx
@@ -14,8 +14,8 @@ import { merge } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map, skip } from 'rxjs';
 import { RenderCompleteDispatcher } from '@kbn/kibana-utils-plugin/public';
 import { Adapters } from '@kbn/inspector-plugin/public';
-import { EmbeddableInput, ViewMode } from '@kbn/embeddable-plugin/common';
-import { EmbeddableError, EmbeddableOutput, IEmbeddable } from './i_embeddable';
+import { ViewMode } from '@kbn/embeddable-plugin/common';
+import { EmbeddableError, EmbeddableInput, EmbeddableOutput, IEmbeddable } from './i_embeddable';
 import { genericEmbeddableInputIsEqual, omitGenericEmbeddableInput } from './diff_embeddable_input';
 
 function getPanelTitle(input: EmbeddableInput, output: EmbeddableOutput) {
