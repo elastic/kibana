1b86f2d755dfd431db7715a51181e213e78ce596
Kevin Delemme <kevin.delemme@elastic.co>
2025-02-20 11:35:24 -0500

    feat(slo): cleanup temp summary documents task (#210264)
    (cherry picked from commit 875a42cf99e45c07ab922af51d3f903c2b5b00a3)
    
    # Conflicts:
    #	x-pack/solutions/observability/plugins/slo/server/routes/slo/get_slo_stats_overview.ts

--- x-pack/solutions/observability/plugins/slo/server/routes/slo/get_slo_stats_overview.ts
+++ x-pack/solutions/observability/plugins/slo/server/routes/slo/get_slo_stats_overview.ts
@@ -5,14 +5,13 @@
  * 2.0.
  */
 
-import { getOverviewParamsSchema } from '@kbn/slo-schema/src/rest_specs/routes/get_overview';
-import { executeWithErrorHandler } from '../../errors';
-import { GetSLOsOverview } from '../../services/get_slos_overview';
+import { getSLOStatsOverviewParamsSchema } from '@kbn/slo-schema';
+import { GetSLOStatsOverview } from '../../services/get_slo_stats_overview';
 import { createSloServerRoute } from '../create_slo_server_route';
 import { assertPlatinumLicense } from './utils/assert_platinum_license';
 import { getSpaceId } from './utils/get_space_id';
 
-export const getSLOsOverview = createSloServerRoute({
+export const getSLOStatsOverview = createSloServerRoute({
   endpoint: 'GET /internal/observability/slos/overview',
   options: { access: 'internal' },
   security: {
@@ -20,7 +19,7 @@ export const getSLOsOverview = createSloServerRoute({
       requiredPrivileges: ['slo_read'],
     },
   },
-  params: getOverviewParamsSchema,
+  params: getSLOStatsOverviewParamsSchema,
   handler: async ({ context, params, request, logger, plugins }) => {
     await assertPlatinumLicense(plugins);
 
@@ -35,7 +34,7 @@ export const getSLOsOverview = createSloServerRoute({
     const alerting = await plugins.alerting.start();
     const rulesClient = await alerting.getRulesClientWithRequest(request);
 
-    const slosOverview = new GetSLOsOverview(
+    const slosOverview = new GetSLOStatsOverview(
       soClient,
       esClient,
       spaceId,
@@ -44,6 +43,6 @@ export const getSLOsOverview = createSloServerRoute({
       racClient
     );
 
-    return await executeWithErrorHandler(() => slosOverview.execute(params?.query ?? {}));
+    return await slosOverview.execute(params?.query ?? {})
   },
 });
