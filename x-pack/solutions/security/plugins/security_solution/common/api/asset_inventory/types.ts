c035cc77c10d1f2f04d0283ea6b358c66577a28e
Paulo Henrique <paulo.henrique@elastic.co>
2025-02-20 10:15:45 -0800

    adding api types

---
+++ x-pack/solutions/security/plugins/security_solution/common/api/asset_inventory/types.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { EntityAnalyticsPrivileges } from '../entity_analytics';
+import { InitEntityStoreResponse } from '../entity_analytics/entity_store/enable.gen';
+
+export type AssetInventoryStatus =
+  | 'disabled'
+  | 'initializing'
+  | 'empty'
+  | 'permission_denied'
+  | 'ready';
+
+export interface AssetInventoryStatusResponse {
+  status: AssetInventoryStatus;
+  privileges?: EntityAnalyticsPrivileges['privileges'];
+}
+
+export type AssetInventoryEnableResponse = InitEntityStoreResponse;
