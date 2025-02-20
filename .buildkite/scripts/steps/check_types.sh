5d1c2c85cb77bec99e73ed2df6884ad850998f9e
Alex Szabo <alex.szabo@elastic.co>
2025-02-20 11:24:44 +0100

    use nx for typecheck and lint

--- .buildkite/scripts/steps/check_types.sh
+++ .buildkite/scripts/steps/check_types.sh
@@ -7,4 +7,4 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/bootstrap.sh
 
 echo --- Check Types
-node scripts/type_check
+nx affected --target=typecheck --nxIgnoreCycles --parallel 3
