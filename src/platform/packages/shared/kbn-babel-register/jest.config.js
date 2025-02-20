0eab4b88f3cda6de2f2f04edda2b302dc7788b7f
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-20 12:54:48 +0100

    Relocating module `@kbn/babel-register`

--- src/platform/packages/shared/kbn-babel-register/jest.config.js
+++ src/platform/packages/shared/kbn-babel-register/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-babel-register'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-babel-register'],
 };
