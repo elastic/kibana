4f27dcb81a01324b4da5cc8b9b1978b05f865f1f
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-20 18:37:46 +0100

    Relocating module `@kbn/gen-ai-functional-testing`

--- src/platform/packages/private/kbn-gen-ai-functional-testing/jest.config.js
+++ src/platform/packages/private/kbn-gen-ai-functional-testing/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-gen-ai-functional-testing'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-gen-ai-functional-testing'],
 };
