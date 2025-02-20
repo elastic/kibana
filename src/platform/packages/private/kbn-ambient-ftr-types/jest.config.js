8859e0dc8fa39a4845edf682f6a9941fcd8cea57
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-20 15:52:27 +0100

    Relocating module `@kbn/ambient-ftr-types`

--- src/platform/packages/private/kbn-ambient-ftr-types/jest.config.js
+++ src/platform/packages/private/kbn-ambient-ftr-types/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-ambient-ftr-types'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-ambient-ftr-types'],
 };
