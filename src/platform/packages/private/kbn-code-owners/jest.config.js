8278707700d35d9475feac8acef2a6d3490c91bf
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-20 13:00:39 +0100

    Relocating module `@kbn/code-owners`

--- src/platform/packages/private/kbn-code-owners/jest.config.js
+++ src/platform/packages/private/kbn-code-owners/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-code-owners'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-code-owners'],
 };
