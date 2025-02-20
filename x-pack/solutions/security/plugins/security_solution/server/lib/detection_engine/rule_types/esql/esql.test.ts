48fb025ad03b2dff7897f09b5802a6b6a901a8f8
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2025-02-20 11:18:07 +0000

    Update esql.test.ts

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/esql/esql.test.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/esql/esql.test.ts
@@ -69,60 +69,58 @@ describe('esqlExecutor', () => {
       scheduleNotificationResponseActionsService: mockScheduleNotificationResponseActionsService,
     } as unknown as Parameters<typeof esqlExecutor>[0];
   });
-
-  describe('eqlExecutor', () => {
-    describe('errors', () => {
-      it('should return result with user error equal true when request fails with data verification exception', async () => {
-        alertServices.scopedClusterClient.asCurrentUser.transport.request.mockRejectedValue(
-          new KbnServerError(
-            'verification_exception: Found 1 problem\nline 1:45: invalid [test_not_lookup] resolution in lookup mode to an index in [standard] mode',
-            400,
-            {
-              error: {
-                root_cause: [
-                  {
-                    type: 'verification_exception',
-                    reason:
-                      'Found 1 problem\nline 1:45: invalid [test_not_lookup] resolution in lookup mode to an index in [standard] mode',
-                  },
-                ],
-                type: 'verification_exception',
-                reason:
-                  'Found 1 problem\nline 1:45: invalid [test_not_lookup] resolution in lookup mode to an index in [standard] mode',
-              },
-            }
-          )
-        );
-
-        const result = await esqlExecutor(mockedArguments);
-
-        expect(result).toHaveProperty('userError', true);
-        expect(result).toHaveProperty('errors', [
+  
+  describe('errors', () => {
+    it('should return result with user error equal true when request fails with data verification exception', async () => {
+      alertServices.scopedClusterClient.asCurrentUser.transport.request.mockRejectedValue(
+        new KbnServerError(
           'verification_exception: Found 1 problem\nline 1:45: invalid [test_not_lookup] resolution in lookup mode to an index in [standard] mode',
-        ]);
-      });
-
-      it('should return result without user error when request fails with non-categorized error', async () => {
-        alertServices.scopedClusterClient.asCurrentUser.transport.request.mockRejectedValue(
-          new KbnServerError('Unknown Error', 500, {
+          400,
+          {
             error: {
               root_cause: [
                 {
-                  type: 'unknown',
-                  reason: 'Unknown Error',
+                  type: 'verification_exception',
+                  reason:
+                    'Found 1 problem\nline 1:45: invalid [test_not_lookup] resolution in lookup mode to an index in [standard] mode',
                 },
               ],
-              type: 'unknown',
-              reason: 'Unknown Error',
+              type: 'verification_exception',
+              reason:
+                'Found 1 problem\nline 1:45: invalid [test_not_lookup] resolution in lookup mode to an index in [standard] mode',
             },
-          })
-        );
+          }
+        )
+      );
 
-        const result = await esqlExecutor(mockedArguments);
+      const result = await esqlExecutor(mockedArguments);
 
-        expect(result).not.toHaveProperty('userError');
-        expect(result).toHaveProperty('errors', ['Unknown Error']);
-      });
+      expect(result).toHaveProperty('userError', true);
+      expect(result).toHaveProperty('errors', [
+        'verification_exception: Found 1 problem\nline 1:45: invalid [test_not_lookup] resolution in lookup mode to an index in [standard] mode',
+      ]);
+    });
+
+    it('should return result without user error when request fails with non-categorized error', async () => {
+      alertServices.scopedClusterClient.asCurrentUser.transport.request.mockRejectedValue(
+        new KbnServerError('Unknown Error', 500, {
+          error: {
+            root_cause: [
+              {
+                type: 'unknown',
+                reason: 'Unknown Error',
+              },
+            ],
+            type: 'unknown',
+            reason: 'Unknown Error',
+          },
+        })
+      );
+
+      const result = await esqlExecutor(mockedArguments);
+
+      expect(result).not.toHaveProperty('userError');
+      expect(result).toHaveProperty('errors', ['Unknown Error']);
     });
   });
 });
