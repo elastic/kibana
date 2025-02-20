ec7082914d485dd10bce6d735dae12c92154c2d9
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-20 13:40:31 +0100

    Relocating module `@kbn/tooling-log`

--- src/platform/packages/shared/kbn-tooling-log/jest.config.js
+++ src/platform/packages/shared/kbn-tooling-log/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-tooling-log'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-tooling-log'],
 };
