1d4cb1c10322533c7263d4895b90d4dd2f0e5a6d
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-20 13:16:18 +0100

    Relocating module `@kbn/ftr-screenshot-filename`

--- src/platform/packages/private/kbn-ftr-screenshot-filename/jest.config.js
+++ src/platform/packages/private/kbn-ftr-screenshot-filename/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-ftr-screenshot-filename'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-ftr-screenshot-filename'],
 };
