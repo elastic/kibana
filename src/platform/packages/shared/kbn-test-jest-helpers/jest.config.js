cc1b847087b29723a025d04bac6d7d95fea04d93
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-20 13:37:45 +0100

    Relocating module `@kbn/test-jest-helpers`

--- src/platform/packages/shared/kbn-test-jest-helpers/jest.config.js
+++ src/platform/packages/shared/kbn-test-jest-helpers/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-test-jest-helpers'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-test-jest-helpers'],
 };
