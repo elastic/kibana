0bf34f51820c5a46b896a68986abedbc8fc39272
Tre' Seymour <wayne.seymour@elastic.co>
2025-02-21 13:58:59 +0000

    Instead of using jest-tags or jest-runneer-groups, simply use --testNamePattern cli param.

--- packages/kbn-scout/src/jest_api_tests/index.ts
+++ packages/kbn-scout/src/jest_api_tests/index.ts
@@ -6,6 +6,3 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
-
-
-// TODO: Add things like supertest here?
