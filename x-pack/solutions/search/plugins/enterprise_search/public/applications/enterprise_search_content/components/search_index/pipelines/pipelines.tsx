f0e17fadf435111dbf9989f10a2ee4a8e19b55f5
meghanmurphy1 <meghan.murphy@elastic.co>
2025-02-21 15:08:20 -0500

    lower delay

--- x-pack/solutions/search/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/pipelines.tsx
+++ x-pack/solutions/search/plugins/enterprise_search/public/applications/enterprise_search_content/components/search_index/pipelines/pipelines.tsx
@@ -73,7 +73,7 @@ export const SearchIndexPipelines: React.FC = () => {
       if (buttonRef.current) {
         buttonRef.current.focus();
       }
-    }, 300);
+    }, 100);
   };
 
   useEffect(() => {
