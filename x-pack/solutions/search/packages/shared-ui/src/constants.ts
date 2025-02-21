ecc69de6f4332fdc5fd66a061363c846688ebcea
Sander Philipse <94373878+sphilipse@users.noreply.github.com>
2025-02-21 17:16:08 +0100

    Search indices consume onboarding token (#211755)
    ## Summary
    
    This consumes the onboarding token propagated by Cloud to determine
    which workflow to show in Serverless and ECH.
    
    Best way to test this locally when running on localhost:5601:
    
    In Serverless:
    - Go to
    http://localhost:5601/app/cloud/onboarding?next=/app/elasticsearch&onboarding_token=vector
    - You should be redirected to the getting started flow
    - Switch to code view and you should have vector search selected
    - Go to
    http://localhost:5601/app/cloud/onboarding?next=/app/enterprise_search/overview&onboarding_token=vectorsearch
    - You should be redirected to the getting started flow
    - Switch to code view and now you should have vector search selected
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/solutions/search/packages/shared-ui/src/constants.ts
+++ x-pack/solutions/search/packages/shared-ui/src/constants.ts
@@ -5,9 +5,4 @@
  * 2.0.
  */
 
-export * from './src/connector_icon';
-export * from './src/decorative_horizontal_stepper';
-export * from './src/form_info_field/form_info_field';
-export * from './src/search_empty_prompt';
-export * from './src/constants';
-export * from './src/types';
+export const WORKFLOW_LOCALSTORAGE_KEY = 'search_onboarding_workflow';
