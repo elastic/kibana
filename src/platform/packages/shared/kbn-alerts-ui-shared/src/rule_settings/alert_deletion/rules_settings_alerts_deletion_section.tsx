dfc1fbee05bba004148df0bdc59f6b7fc69a8a61
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-02-21 13:32:38 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/platform/packages/shared/kbn-alerts-ui-shared/src/rule_settings/alert_deletion/rules_settings_alerts_deletion_section.tsx
+++ src/platform/packages/shared/kbn-alerts-ui-shared/src/rule_settings/alert_deletion/rules_settings_alerts_deletion_section.tsx
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import React, { memo } from 'react';
+import React from 'react';
 import {
   EuiDescribedFormGroup,
   EuiEmptyPrompt,
