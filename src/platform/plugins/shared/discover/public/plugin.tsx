17df57aa486c51fb9fcf21a90631c6abf4bcbfc4
Davis McPhee <davis.mcphee@elastic.co>
2025-02-21 22:35:41 -0400

    More init work

--- src/platform/plugins/shared/discover/public/plugin.tsx
+++ src/platform/plugins/shared/discover/public/plugin.tsx
@@ -214,7 +214,6 @@ export class DiscoverPlugin
           element: params.element,
           services,
           customizationContext: defaultCustomizationContext,
-          experimentalFeatures: this.experimentalFeatures,
         });
 
         return () => {
