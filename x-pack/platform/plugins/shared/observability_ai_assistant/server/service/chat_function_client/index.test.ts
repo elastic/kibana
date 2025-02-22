elastic/kibana

commit 0ae28aa8bc73ce682166ffd5666828b1fdb7c017

[Obs Ai Assistant] Add system message (#209773)
Fix: System Message Missing in Inference Plugin
Closes #209548
## Summary

A regression was introduced in 8.18
([#199286](https://github.com/elastic/kibana/pull/199286)), where the
system message is no longer passed to the inference plugin and,
consequently, the LLM.

Currently, only user messages are being sent, which impacts conversation
guidance and guardrails. The system message is crucial for steering
responses and maintaining contextual integrity.

The filtering of the system message happens here:

https://github.com/elastic/kibana/blob/771a080ffa99e501e72c9cb98c833795769483ae/x-pack/platform/plugins/shared/observability_ai_assistant/server/service/client/index.ts#L510-L512

Fix Approach
- Ensure the `system` message is included as a parameter in
`inferenceClient.chatComplete.`
```typescript
const options = {
      connectorId,
      system,
      messages: convertMessagesForInference(messages),
      toolChoice,
      tools,
      functionCalling: (simulateFunctionCalling ? 'simulated' : 'native') as FunctionCallingMode,
    };
    if (stream) {
      return defer(() =>
        this.dependencies.inferenceClient.chatComplete({
          ...options,
          stream: true,
        })
      ).pipe(
        convertInferenceEventsToStreamingEvents(),
        instrumentAndCountTokens(name),
        failOnNonExistingFunctionCall({ functions }),
        tap((event) => {
          if (
            event.type === StreamingChatResponseEventType.ChatCompletionChunk &&
            this.dependencies.logger.isLevelEnabled('trace')
          ) {
            this.dependencies.logger.trace(`Received chunk: ${JSON.stringify(event.message)}`);
          }
        }),
        shareReplay()
      ) as TStream extends true
        ? Observable<ChatCompletionChunkEvent | TokenCountEvent | ChatCompletionMessageEvent>
        : never;
    } else {

x-pack/platform/plugins/shared/observability_ai_assistant/server/functions/get_dataset_info/get_relevant_field_names.ts
@@ -100,20 +100,14 @@ export async function getRelevantFieldNames({
         await chat('get_relevant_dataset_names', {
           signal,
           stream: true,
-          messages: [
-            {
-              '@timestamp': new Date().toISOString(),
-              message: {
-                role: MessageRole.System,
-                content: `You are a helpful assistant for Elastic Observability.
+          systemMessage: `You are a helpful assistant for Elastic Observability.
             Your task is to create a list of field names that are relevant
             to the conversation, using ONLY the list of fields and
             types provided in the last user message. DO NOT UNDER ANY
             CIRCUMSTANCES include fields not mentioned in this list.`,
-              },
-            },
-            // remove the system message and the function request
-            ...messages.slice(1, -1),
+          messages: [
+            // remove the function request
+            ...messages.filter((msg) => !msg.message?.function_call),
             {
               '@timestamp': new Date().toISOString(),
               message: {

x-pack/platform/plugins/shared/observability_ai_assistant/server/service/client/operators/continue_conversation.ts
@@ -35,8 +35,6 @@ import { withoutTokenCountEvents } from '../../../../common/utils/without_token_
 import type { ChatFunctionClient } from '../../chat_function_client';
 import type { AutoAbortedChatFunction } from '../../types';
 import { createServerSideFunctionResponseError } from '../../util/create_server_side_function_response_error';
-import { getSystemMessageFromInstructions } from '../../util/get_system_message_from_instructions';
-import { replaceSystemMessage } from '../../util/replace_system_message';
 import { LangTracer } from '../instrumentation/lang_tracer';
 import { catchFunctionNotFoundError } from './catch_function_not_found_error';
 import { extractMessages } from './extract_messages';
@@ -213,20 +211,7 @@ export function continueConversation({
     disableFunctions,
   });
 
-  const registeredAdhocInstructions = functionClient.getAdhocInstructions();
-  const allAdHocInstructions = adHocInstructions.concat(registeredAdhocInstructions);
-
-  const messagesWithUpdatedSystemMessage = replaceSystemMessage(
-    getSystemMessageFromInstructions({
-      applicationInstructions: functionClient.getInstructions(),
-      userInstructions,
-      adHocInstructions: allAdHocInstructions,
-      availableFunctionNames: definitions.map((def) => def.name),
-    }),
-    initialMessages
-  );
-
-  const lastMessage = last(messagesWithUpdatedSystemMessage)?.message;
+  const lastMessage = last(initialMessages)?.message;
   const isUserMessage = lastMessage?.role === MessageRole.User;
 
   return executeNextStep().pipe(handleEvents());
@@ -239,7 +224,7 @@ export function continueConversation({
           : 'user_message';
 
       return chat(operationName, {
-        messages: messagesWithUpdatedSystemMessage,
+        messages: initialMessages,
         functions: definitions,
         tracer,
         connectorId,
@@ -314,7 +299,7 @@ export function continueConversation({
       args: lastMessage.function_call!.arguments,
       chat,
       functionClient,
-      messages: messagesWithUpdatedSystemMessage,
+      messages: initialMessages,
       signal,
       logger,
       tracer,
@@ -337,7 +322,7 @@ export function continueConversation({
               return EMPTY;
             }
             return continueConversation({
-              messages: messagesWithUpdatedSystemMessage.concat(extractedMessages),
+              messages: initialMessages.concat(extractedMessages),
               chat,
               functionCallsLeft: nextFunctionCallsLeft,
               functionClient,

x-pack/platform/plugins/shared/observability_ai_assistant/server/service/client/index.test.ts
@@ -44,7 +44,7 @@ interface ChunkDelta {
 
 type LlmSimulator = ReturnType<typeof createLlmSimulator>;
 
-const EXPECTED_STORED_SYSTEM_MESSAGE = `system`;
+const EXPECTED_STORED_SYSTEM_MESSAGE = `this is a system message`;
 
 const nextTick = () => {
   return new Promise(process.nextTick);
@@ -185,7 +185,7 @@ describe('Observability AI Assistant client', () => {
 
     knowledgeBaseServiceMock.getUserInstructions.mockResolvedValue([]);
 
-    functionClientMock.getInstructions.mockReturnValue(['system']);
+    functionClientMock.getInstructions.mockReturnValue([EXPECTED_STORED_SYSTEM_MESSAGE]);
     functionClientMock.getAdhocInstructions.mockReturnValue([]);
 
     return new ObservabilityAIAssistantClient({
@@ -206,18 +206,6 @@ describe('Observability AI Assistant client', () => {
       },
       scopes: ['all'],
     });
-  }
-
-  function system(content: string | Omit<Message['message'], 'role'>): Message {
-    return merge(
-      {
-        '@timestamp': new Date().toString(),
-        message: {
-          role: MessageRole.System,
-        },
-      },
-      typeof content === 'string' ? { message: { content } } : content
-    );
   }
 
   function user(content: string | Omit<Message['message'], 'role'>): Message {
@@ -286,7 +274,7 @@ describe('Observability AI Assistant client', () => {
       stream = observableIntoStream(
         client.complete({
           connectorId: 'foo',
-          messages: [system('This is a system message'), user('How many alerts do I have?')],
+          messages: [user('How many alerts do I have?')],
           functionClient: functionClientMock,
           signal: new AbortController().signal,
           persist: true,
@@ -313,6 +301,8 @@ describe('Observability AI Assistant client', () => {
           expect.objectContaining({
             connectorId: 'foo',
             stream: false,
+            system:
+              'You are a helpful assistant for Elastic Observability. Assume the following message is the start of a conversation between you and a user; give this conversation a title based on the content below. DO NOT UNDER ANY CIRCUMSTANCES wrap this title in single or double quotes. This title is shown in a list of conversations to the user, so title it for the user, not for you.',
             functionCalling: 'auto',
             toolChoice: expect.objectContaining({
               function: 'title_conversation',
@@ -498,14 +488,8 @@ describe('Observability AI Assistant client', () => {
               user: {
                 name: 'johndoe',
               },
+              systemMessage: EXPECTED_STORED_SYSTEM_MESSAGE,
               messages: [
-                {
-                  '@timestamp': expect.any(String),
-                  message: {
-                    content: EXPECTED_STORED_SYSTEM_MESSAGE,
-                    role: MessageRole.System,
-                  },
-                },
                 {
                   '@timestamp': expect.any(String),
                   message: {
@@ -568,10 +552,7 @@ describe('Observability AI Assistant client', () => {
                   labels: {},
                   numeric_labels: {},
                   public: false,
-                  messages: [
-                    system('This is a system message'),
-                    user('How many alerts do I have?'),
-                  ],
+                  messages: [user('How many alerts do I have?')],
                 },
               },
             ],
@@ -586,7 +567,7 @@ describe('Observability AI Assistant client', () => {
       stream = observableIntoStream(
         await client.complete({
           connectorId: 'foo',
-          messages: [system('This is a system message'), user('How many alerts do I have?')],
+          messages: [user('How many alerts do I have?')],
           functionClient: functionClientMock,
           signal: new AbortController().signal,
           conversationId: 'my-conversation-id',
@@ -646,14 +627,8 @@ describe('Observability AI Assistant client', () => {
           user: {
             name: 'johndoe',
           },
+          systemMessage: EXPECTED_STORED_SYSTEM_MESSAGE,
           messages: [
-            {
-              '@timestamp': expect.any(String),
-              message: {
-                content: EXPECTED_STORED_SYSTEM_MESSAGE,
-                role: MessageRole.System,
-              },
-            },
             {
               '@timestamp': expect.any(String),
               message: {
@@ -695,7 +670,7 @@ describe('Observability AI Assistant client', () => {
       stream = observableIntoStream(
         await client.complete({
           connectorId: 'foo',
-          messages: [system('This is a system message'), user('How many alerts do I have?')],
+          messages: [user('How many alerts do I have?')],
           functionClient: functionClientMock,
           signal: new AbortController().signal,
           title: 'My predefined title',
@@ -785,7 +760,7 @@ describe('Observability AI Assistant client', () => {
       stream = observableIntoStream(
         await client.complete({
           connectorId: 'foo',
-          messages: [system('This is a system message'), user('How many alerts do I have?')],
+          messages: [user('How many alerts do I have?')],
           functionClient: functionClientMock,
           signal: new AbortController().signal,
           title: 'My predefined title',
@@ -846,13 +821,6 @@ describe('Observability AI Assistant client', () => {
           signal: expect.any(AbortSignal),
           connectorId: 'foo',
           messages: [
-            {
-              '@timestamp': expect.any(String),
-              message: {
-                role: MessageRole.System,
-                content: EXPECTED_STORED_SYSTEM_MESSAGE,
-              },
-            },
             {
               '@timestamp': expect.any(String),
               message: {
@@ -985,13 +953,6 @@ describe('Observability AI Assistant client', () => {
           expect(
             (internalUserEsClientMock.index.mock.lastCall![0] as any).document.messages
           ).toEqual([
-            {
-              '@timestamp': expect.any(String),
-              message: {
-                content: EXPECTED_STORED_SYSTEM_MESSAGE,
-                role: MessageRole.System,
-              },
-            },
             {
               '@timestamp': expect.any(String),
               message: {
@@ -1224,7 +1185,7 @@ describe('Observability AI Assistant client', () => {
       stream = observableIntoStream(
         await client.complete({
           connectorId: 'foo',
-          messages: [system('This is a system message'), user('How many alerts do I have?')],
+          messages: [user('How many alerts do I have?')],
           functionClient: functionClientMock,
           signal: new AbortController().signal,
           persist: false,
@@ -1349,7 +1310,7 @@ describe('Observability AI Assistant client', () => {
       stream = observableIntoStream(
         await client.complete({
           connectorId: 'foo',
-          messages: [system('This is a system message'), user('How many alerts do I have?')],
+          messages: [user('How many alerts do I have?')],
           functionClient: functionClientMock,
           signal: new AbortController().signal,
           title: 'My predefined title',
@@ -1427,7 +1388,7 @@ describe('Observability AI Assistant client', () => {
       const stream = observableIntoStream(
         await client.complete({
           connectorId: 'foo',
-          messages: [system('This is a system message'), user('How many alerts do I have?')],
+          messages: [user('How many alerts do I have?')],
           functionClient: functionClientMock,
           signal: new AbortController().signal,
           persist: false,
@@ -1515,7 +1476,7 @@ describe('Observability AI Assistant client', () => {
       stream = observableIntoStream(
         await client.complete({
           connectorId: 'foo',
-          messages: [system('This is a system message'), user('How many alerts do I have?')],
+          messages: [user('How many alerts do I have?')],
           functionClient: functionClientMock,
           signal: new AbortController().signal,
           title: 'My predefined title',
@@ -1580,7 +1541,7 @@ describe('Observability AI Assistant client', () => {
     client
       .complete({
         connectorId: 'foo',
-        messages: [system('This is a system message'), user('A user message to cause completion')],
+        messages: [user('A user message to cause completion')],
         functionClient: functionClientMock,
         signal: new AbortController().signal,
         title: 'My predefined title',
@@ -1589,9 +1550,7 @@ describe('Observability AI Assistant client', () => {
       .subscribe(() => {}); // To trigger call to chat
     await nextTick();
 
-    expect(chatSpy.mock.calls[0][1].messages[0].message.content).toEqual(
-      EXPECTED_STORED_SYSTEM_MESSAGE
-    );
+    expect(chatSpy.mock.calls[0][1].systemMessage).toEqual(EXPECTED_STORED_SYSTEM_MESSAGE);
   });
 
   describe('when executing an action', () => {
@@ -1608,10 +1567,7 @@ describe('Observability AI Assistant client', () => {
 
       const complete$ = await client.complete({
         connectorId: 'foo',
-        messages: [
-          system('This is a system message'),
-          user('Can you call the my_action function?'),
-        ],
+        messages: [user('Can you call the my_action function?')],
         functionClient: new ChatFunctionClient([
           {
             actions: [

commit 54f96d0070306bf13eeace9a55a157b5a8341526

[Obs AI Assistant] Fix bug with `get_alerts_dataset_info` (#212077)
Closes https://github.com/elastic/kibana/issues/212005

Regression introduced in:
https://github.com/elastic/kibana/pull/209773/files#diff-83722bb07633512b20beb965628b18290628de985d399989011cc20c82fa483cL116

TODO:

- Add a test that would have caught this

x-pack/platform/plugins/shared/observability_ai_assistant/server/functions/get_dataset_info/get_relevant_field_names.ts
@@ -106,8 +106,8 @@ export async function getRelevantFieldNames({
             types provided in the last user message. DO NOT UNDER ANY
             CIRCUMSTANCES include fields not mentioned in this list.`,
           messages: [
-            // remove the function request
-            ...messages.filter((msg) => !msg.message?.function_call),
+            // remove the last function request
+            ...messages.slice(0, -1),
             {
               '@timestamp': new Date().toISOString(),
               message: {

x-pack/platform/plugins/shared/observability_ai_assistant/server/service/chat_function_client/index.ts
@@ -9,6 +9,7 @@
 import Ajv, { type ErrorObject, type ValidateFunction } from 'ajv';
 import dedent from 'dedent';
 import { compact, keyBy } from 'lodash';
+import { Logger } from '@kbn/logging';
 import { FunctionVisibility, type FunctionResponse } from '../../../common/functions/types';
 import type {
   AdHocInstruction,
@@ -166,6 +167,7 @@ export class ChatFunctionClient {
     args,
     messages,
     signal,
+    logger,
     connectorId,
     simulateFunctionCalling,
   }: {
@@ -174,6 +176,7 @@ export class ChatFunctionClient {
     args: string | undefined;
     messages: Message[];
     signal: AbortSignal;
+    logger: Logger;
     connectorId: string;
     simulateFunctionCalling: boolean;
   }): Promise<FunctionResponse> {
@@ -187,16 +190,26 @@ export class ChatFunctionClient {
 
     this.validate(name, parsedArguments);
 
-    return await fn.handler.respond(
-      {
-        arguments: parsedArguments,
-        messages,
-        screenContexts: this.screenContexts,
-        chat,
-        connectorId,
-        simulateFunctionCalling,
-      },
-      signal
+    logger.debug(
+      () => `Executing function ${name} with arguments: ${JSON.stringify(parsedArguments)}`
     );
+
+    try {
+      return await fn.handler.respond(
+        {
+          arguments: parsedArguments,
+          messages,
+          screenContexts: this.screenContexts,
+          chat,
+          connectorId,
+          simulateFunctionCalling,
+        },
+        signal
+      );
+    } catch (e) {
+      logger.error(`Error executing function "${name}": ${e.message}`);
+      logger.error(e);
+      throw e;
+    }
   }
 }

x-pack/platform/plugins/shared/observability_ai_assistant/server/service/client/operators/continue_conversation.ts
@@ -79,6 +79,7 @@ function executeFunctionAndCatchError({
         },
         args,
         signal,
+        logger,
         messages,
         connectorId,
         simulateFunctionCalling,

x-pack/solutions/observability/plugins/observability_ai_assistant_app/server/functions/alerts.ts
@@ -20,6 +20,8 @@ import { omit } from 'lodash';
 import { OBSERVABILITY_RULE_TYPE_IDS_WITH_SUPPORTED_STACK_RULE_TYPES } from '@kbn/observability-plugin/common/constants';
 import { FunctionRegistrationParameters } from '.';
 
+export const GET_ALERTS_DATASET_INFO_NAME = 'get_alerts_dataset_info';
+
 const defaultFields = [
   '@timestamp',
   'kibana.alert.start',
@@ -72,7 +74,7 @@ export function registerAlertsFunction({
   if (scopes.includes('observability')) {
     functions.registerFunction(
       {
-        name: 'get_alerts_dataset_info',
+        name: GET_ALERTS_DATASET_INFO_NAME,
         visibility: FunctionVisibility.AssistantOnly,
         description: `Use this function to get information about alerts data.`,
         parameters: {
@@ -133,7 +135,7 @@ export function registerAlertsFunction({
     functions.registerFunction(
       {
         name: 'alerts',
-        description: `Get alerts for Observability.  Make sure get_alerts_dataset_info was called before.
+        description: `Get alerts for Observability.  Make sure ${GET_ALERTS_DATASET_INFO_NAME} was called before.
         Use this to get open (and optionally recovered) alerts for Observability assets, like services,
         hosts or containers.
         Display the response in tabular format if appropriate.

x-pack/platform/plugins/shared/observability_ai_assistant/server/service/chat_function_client/index.test.ts
@@ -8,6 +8,7 @@ import dedent from 'dedent';
 import { ChatFunctionClient, GET_DATA_ON_SCREEN_FUNCTION_NAME } from '.';
 import { FunctionVisibility } from '../../../common/functions/types';
 import { AdHocInstruction } from '../../../common/types';
+import { Logger } from '@kbn/logging';
 
 describe('chatFunctionClient', () => {
   describe('when executing a function with invalid arguments', () => {
@@ -49,6 +50,7 @@ describe('chatFunctionClient', () => {
           }),
           messages: [],
           signal: new AbortController().signal,
+          logger: getLoggerMock(),
           connectorId: 'foo',
           simulateFunctionCalling: false,
         });
@@ -111,6 +113,7 @@ describe('chatFunctionClient', () => {
         args: JSON.stringify({ data: ['my_dummy_data'] }),
         messages: [],
         signal: new AbortController().signal,
+        logger: getLoggerMock(),
         connectorId: 'foo',
         simulateFunctionCalling: false,
       });
@@ -179,3 +182,15 @@ describe('chatFunctionClient', () => {
     });
   });
 });
+
+function getLoggerMock() {
+  // const consoleOrPassThrough = console.log.bind(console);
+  const consoleOrPassThrough = () => {};
+  return {
+    log: jest.fn().mockImplementation(consoleOrPassThrough),
+    error: jest.fn().mockImplementation(consoleOrPassThrough),
+    debug: jest.fn().mockImplementation(consoleOrPassThrough),
+    trace: jest.fn().mockImplementation(consoleOrPassThrough),
+    isLevelEnabled: jest.fn().mockReturnValue(true),
+  } as unknown as Logger;
+}

x-pack/platform/plugins/shared/observability_ai_assistant/server/service/client/index.test.ts
@@ -766,6 +766,7 @@ describe('Observability AI Assistant client', () => {
           chat: expect.any(Function),
           args: JSON.stringify({ foo: 'bar' }),
           signal: expect.any(AbortSignal),
+          logger: expect.any(Object),
           connectorId: 'foo',
           messages: [
             {