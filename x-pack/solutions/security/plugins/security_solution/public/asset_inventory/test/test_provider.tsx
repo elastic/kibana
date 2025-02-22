9fd0c972b5f900feb269838136d73f9dfd7b4f10
Paulo Henrique <paulo.henrique@elastic.co>
2025-02-22 12:17:46 -0800

    updating test provider to a minimum

--- x-pack/solutions/security/plugins/security_solution/public/asset_inventory/test/test_provider.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/asset_inventory/test/test_provider.tsx
@@ -5,44 +5,15 @@
  * 2.0.
  */
 
-import type { AppMountParameters } from '@kbn/core/public';
 import React from 'react';
 import { I18nProvider } from '@kbn/i18n-react';
-// eslint-disable-next-line no-restricted-imports
-import { Router } from 'react-router-dom';
-import { Route, Routes } from '@kbn/shared-ux-router';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { coreMock } from '@kbn/core/public/mocks';
-
 interface TestProviderProps {
-  params: AppMountParameters;
   children: React.ReactNode;
 }
 
 /**
  * A provider that wraps the necessary context for testing components.
  */
-export const TestProvider: React.FC<Partial<TestProviderProps>> = ({
-  params = coreMock.createAppMountParameters(),
-  children,
-} = {}) => {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        retry: false,
-      },
-    },
-  });
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Router history={params.history}>
-        <I18nProvider>
-          <Routes>
-            <Route path="*" render={() => <>{children}</>} />
-          </Routes>
-        </I18nProvider>
-      </Router>
-    </QueryClientProvider>
-  );
+export const TestProvider: React.FC<Partial<TestProviderProps>> = ({ children } = {}) => {
+  return <I18nProvider>{children}</I18nProvider>;
 };
