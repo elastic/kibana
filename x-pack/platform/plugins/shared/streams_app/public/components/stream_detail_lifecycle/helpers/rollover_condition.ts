77b5a75c064173f967799a77615f72eab2e6471e
klacabane <kevin.lacabane@elastic.co>
2025-02-24 16:28:32 +0100

    i18n

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_lifecycle/helpers/rollover_condition.ts
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_lifecycle/helpers/rollover_condition.ts
@@ -5,15 +5,41 @@
  * 2.0.
  */
 
+import { i18n } from '@kbn/i18n';
 import { IlmPolicyHotPhase } from '@kbn/streams-schema';
 
 export function rolloverCondition(rollover?: IlmPolicyHotPhase['rollover']) {
   const conditions = [
-    rollover?.max_age && 'max age ' + rollover.max_age,
-    rollover?.max_docs && 'max docs ' + rollover.max_docs,
-    rollover?.max_primary_shard_docs && 'primary shard docs ' + rollover.max_primary_shard_docs,
-    rollover?.max_primary_shard_size && 'primary shard size ' + rollover.max_primary_shard_size,
-    rollover?.max_size && 'index size ' + rollover.max_size,
+    rollover?.max_age &&
+      i18n.translate('xpack.streams.streamDetailLifecycle.rolloverConditionMaxAge', {
+        defaultMessage: 'max age {maxAge}',
+        values: { maxAge: rollover.max_age },
+      }),
+    rollover?.max_docs &&
+      i18n.translate('xpack.streams.streamDetailLifecycle.rolloverConditionMaxDocs', {
+        defaultMessage: 'max docs {maxDocs}',
+        values: { maxDocs: rollover.max_docs },
+      }),
+    rollover?.max_primary_shard_docs &&
+      i18n.translate('xpack.streams.streamDetailLifecycle.rolloverConditionMaxPrimaryShardDocs', {
+        defaultMessage: 'primary shard docs {shardDocs}',
+        values: { shardDocs: rollover.max_primary_shard_docs },
+      }),
+    rollover?.max_primary_shard_size &&
+      i18n.translate('xpack.streams.streamDetailLifecycle.rolloverConditionMaxPrimaryShardSize', {
+        defaultMessage: 'primary shard size {shardSize}',
+        values: { shardSize: rollover.max_primary_shard_size },
+      }),
+    rollover?.max_size &&
+      i18n.translate('xpack.streams.streamDetailLifecycle.rolloverConditionMaxSize', {
+        defaultMessage: 'index size {maxSize}',
+        values: { maxSize: rollover.max_size },
+      }),
   ].filter(Boolean);
-  return conditions.join(' or ');
+
+  return conditions.join(
+    i18n.translate('xpack.streams.streamDetailLifecycle.rolloverConditionOr', {
+      defaultMessage: ' or ',
+    })
+  );
 }
