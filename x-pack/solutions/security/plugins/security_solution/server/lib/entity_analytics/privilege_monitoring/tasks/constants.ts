6d388772080b4d7bee839b7ec99e0d712fc858ee
CAWilson94 <charlotte.wilson@elastic.co>
2025-02-24 15:16:50 +0000

    kibana task boilerplate for monitoring WiP

---
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/tasks/constants.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export const TYPE = 'entity_analytics:monitoring_engine:great_success';
+export const VERSION = '1.0.0';
+export const TIMEOUT = '10m';
