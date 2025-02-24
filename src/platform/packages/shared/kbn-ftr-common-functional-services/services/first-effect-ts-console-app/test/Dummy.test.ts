75cd1519966092bbd73a7188bc93cbf5dc65d3cd
Tre' Seymour <wayne.seymour@elastic.co>
2025-02-24 15:35:19 +0000

    Drop this

--- src/platform/packages/shared/kbn-ftr-common-functional-services/services/first-effect-ts-console-app/test/Dummy.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "@effect/vitest"
-
-describe("Dummy", () => {
-  it("should pass", () => {
-    expect(true).toBe(true)
-  })
-})
--- src/platform/packages/shared/kbn-ftr-common-functional-services/services/first-effect-ts-console-app/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "tsup"
-
-export default defineConfig({
-  entry: ["src/bin.ts"],
-  clean: true,
-  publicDir: true,
-  treeshake: "smallest",
-  external: ["@parcel/watcher"]
-})
