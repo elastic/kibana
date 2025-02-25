32d510d03cae58f2b4c2832d97166325f779ad3b
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-25 22:13:52 +0100

    Relocating module `@kbn/scout-reporting`

--- src/platform/packages/private/kbn-scout-reporting/jest.config.js
+++ src/platform/packages/private/kbn-scout-reporting/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-scout-reporting'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-scout-reporting'],
 };
