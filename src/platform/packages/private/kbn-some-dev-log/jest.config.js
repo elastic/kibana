39cac842b809bb37e7e4cb07b6f12b833fe481fc
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-25 22:15:24 +0100

    Relocating module `@kbn/some-dev-log`

--- src/platform/packages/private/kbn-some-dev-log/jest.config.js
+++ src/platform/packages/private/kbn-some-dev-log/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-some-dev-log'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-some-dev-log'],
 };
