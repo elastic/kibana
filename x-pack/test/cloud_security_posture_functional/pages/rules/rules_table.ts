bfa044065d4692941addf2de65f1f10adacec044
alex prozorov <alex.prozorov@elastic.co>
2025-02-25 15:59:14 +0200

    split rules page test suite into 3 test suites - counters, headers and table sections
    updated before and after hooks

--- x-pack/test/cloud_security_posture_functional/pages/rules/rules_table.ts
+++ x-pack/test/cloud_security_posture_functional/pages/rules/rules_table.ts
@@ -7,13 +7,13 @@
 
 import expect from '@kbn/expect';
 import { ELASTIC_HTTP_VERSION_HEADER } from '@kbn/core-http-common';
-import { createPackagePolicy } from '../../api_integration/apis/cloud_security_posture/helper';
-import type { FtrProviderContext } from '../ftr_provider_context';
+import { createPackagePolicy } from '../../../api_integration/apis/cloud_security_posture/helper';
+import type { FtrProviderContext } from '../../ftr_provider_context';
+import { k8sFindingsMock } from '../../mocks/latest_findings_mock';
 import {
   RULES_BULK_ACTION_OPTION_DISABLE,
   RULES_BULK_ACTION_OPTION_ENABLE,
-} from '../page_objects/rule_page';
-import { k8sFindingsMock } from '../mocks/latest_findings_mock';
+} from '../../page_objects/rule_page';
 
 // eslint-disable-next-line import/no-default-export
 export default function ({ getPageObjects, getService }: FtrProviderContext) {
@@ -29,7 +29,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
   ]);
   const retryService = getService('retry');
 
-  describe('Cloud Posture Rules Page', function () {
+  describe('Cloud Posture Rules Page - Table', function () {
     this.tags(['cloud_security_posture_rules_page']);
     let rule: typeof pageObjects.rule;
     let findings: typeof pageObjects.findings;
@@ -38,8 +38,17 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
     before(async () => {
       rule = pageObjects.rule;
       findings = pageObjects.findings;
+      await findings.index.remove();
 
-      await kibanaServer.savedObjects.cleanStandardList();
+      // cleanup agent and package policies
+      await kibanaServer.savedObjects.clean({
+        types: [
+          'ingest-agent-policies',
+          'fleet-agent-policies',
+          'ingest-package-policies',
+          'fleet-package-policies',
+        ],
+      });
       await esArchiver.load('x-pack/test/functional/es_archives/fleet/empty_fleet_server');
 
       const { body: agentPolicyResponse } = await supertest
@@ -61,92 +70,28 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
         'vanilla',
         'kspm'
       );
-      await rule.waitForPluginInitialized();
-      await findings.index.add(k8sFindingsMock);
     });
 
     beforeEach(async () => {
+      await rule.waitForPluginInitialized();
+      await findings.index.add(k8sFindingsMock);
       await rule.navigateToRulePage('cis_k8s', '1.0.1');
     });
 
     after(async () => {
-      await kibanaServer.savedObjects.cleanStandardList();
       await esArchiver.unload('x-pack/test/functional/es_archives/fleet/empty_fleet_server');
-      await findings.index.remove();
     });
 
-    describe('Rules Page - Rules Counters', () => {
-      it('Shows posture score when there are findings', async () => {
-        const isEmptyStateVisible = await rule.rulePage.getCountersEmptyState();
-        expect(isEmptyStateVisible).to.be(false);
-
-        const postureScoreCounter = await rule.rulePage.getPostureScoreCounter();
-        expect((await postureScoreCounter.getVisibleText()).includes('33%')).to.be(true);
+    afterEach(async () => {
+      await kibanaServer.savedObjects.clean({
+        types: [
+          'ingest-agent-policies',
+          'fleet-agent-policies',
+          'ingest-package-policies',
+          'fleet-package-policies',
+        ],
       });
-
-      it('Clicking the posture score button leads to the dashboard', async () => {
-        await retryService.tryForTime(5000, async () => {
-          await rule.rulePage.clickPostureScoreButton();
-          await pageObjects.common.waitUntilUrlIncludes('cloud_security_posture/dashboard');
-        });
-      });
-
-      it('Shows integrations count when there are findings', async () => {
-        const integrationsCounter = await rule.rulePage.getIntegrationsEvaluatedCounter();
-        expect((await integrationsCounter.getVisibleText()).includes('1')).to.be(true);
-      });
-
-      it('Clicking the integrations counter button leads to the integration page', async () => {
-        await rule.rulePage.clickIntegrationsEvaluatedButton();
-        await pageObjects.common.waitUntilUrlIncludes('add-integration/kspm');
-      });
-
-      it('Shows the failed findings counter when there are findings', async () => {
-        const failedFindingsCounter = await rule.rulePage.getFailedFindingsCounter();
-        expect((await failedFindingsCounter.getVisibleText()).includes('2')).to.be(true);
-      });
-
-      it('Clicking the failed findings button leads to the findings page', async () => {
-        await rule.rulePage.clickFailedFindingsButton();
-        await pageObjects.common.waitUntilUrlIncludes(
-          'cloud_security_posture/findings/configurations'
-        );
-      });
-
-      it('Shows the disabled rules count', async () => {
-        const disabledRulesCounter = await rule.rulePage.getDisabledRulesCounter();
-        expect((await disabledRulesCounter.getVisibleText()).includes('0')).to.be(true);
-
-        // disable rule 1.1.1 (k8s findings mock contains a findings from that rule)
-        await rule.rulePage.clickEnableRulesRowSwitchButton(0);
-        await pageObjects.header.waitUntilLoadingHasFinished();
-        expect((await disabledRulesCounter.getVisibleText()).includes('1')).to.be(true);
-
-        const postureScoreCounter = await rule.rulePage.getPostureScoreCounter();
-        expect((await postureScoreCounter.getVisibleText()).includes('0%')).to.be(true);
-
-        // enable rule back
-        await rule.rulePage.clickEnableRulesRowSwitchButton(0);
-      });
-
-      it('Clicking the disabled rules button shows enables the disabled filter', async () => {
-        await rule.rulePage.clickEnableRulesRowSwitchButton(0);
-        await pageObjects.header.waitUntilLoadingHasFinished();
-
-        await rule.rulePage.clickDisabledRulesButton();
-        await pageObjects.header.waitUntilLoadingHasFinished();
-        expect((await rule.rulePage.getEnableRulesRowSwitchButton()) === 1).to.be(true);
-      });
-
-      it('Shows empty state when there are no findings', async () => {
-        // Ensure there are no findings initially
-        await findings.index.remove();
-        await rule.navigateToRulePage('cis_k8s', '1.0.1');
-
-        const isEmptyStateVisible = await rule.rulePage.getCountersEmptyState();
-        expect(isEmptyStateVisible).to.be(true);
-        await rule.rulePage.clickEnableRulesRowSwitchButton(0);
-      });
+      await findings.index.remove();
     });
 
     describe('Rules Page - Bulk Action buttons', () => {
@@ -205,45 +150,6 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
           (await rule.rulePage.isBulkActionOptionDisabled(RULES_BULK_ACTION_OPTION_DISABLE)) ===
             'true'
         ).to.be(false);
-      });
-    });
-
-    describe('Rules Page - Enable Rules and Disabled Rules Filter Toggle', () => {
-      it('Should only display Enabled rules when Enabled Rules filter is ON', async () => {
-        await rule.rulePage.clickFilterButton('enabled');
-        await pageObjects.header.waitUntilLoadingHasFinished();
-        expect((await rule.rulePage.getEnableRulesRowSwitchButton()) === 1).to.be(true);
-      });
-
-      it('Should only display Disabled rules when Disabled Rules filter is ON', async () => {
-        await rule.rulePage.clickFilterButton('disabled');
-        await pageObjects.header.waitUntilLoadingHasFinished();
-        expect((await rule.rulePage.getEnableRulesRowSwitchButton()) > 1).to.be(true);
-      });
-    });
-
-    describe('Rules Page - CIS Section & Rule Number filters', () => {
-      it('Table should only show result that has the same section as in the Section filter', async () => {
-        await rule.rulePage.clickFilterPopover('section');
-        await rule.rulePage.clickFilterPopOverOption('etcd');
-        await rule.rulePage.clickFilterPopOverOption('Scheduler');
-        expect((await rule.rulePage.getEnableRulesRowSwitchButton()) < 10).to.be(true);
-      });
-
-      it('Table should only show result that has the same section as in the Rule number filter', async () => {
-        await rule.rulePage.clickFilterPopover('ruleNumber');
-        await rule.rulePage.clickFilterPopOverOption('1.1.1');
-        await rule.rulePage.clickFilterPopOverOption('1.1.2');
-        expect((await rule.rulePage.getEnableRulesRowSwitchButton()) === 2).to.be(true);
-      });
-
-      it('Table should only show result that passes both Section and Rule number filter', async () => {
-        await rule.rulePage.clickFilterPopover('section');
-        await rule.rulePage.clickFilterPopOverOption('Control-Plane-Node-Configuration-Files');
-        await rule.rulePage.clickFilterPopover('section');
-        await rule.rulePage.clickFilterPopover('ruleNumber');
-        await rule.rulePage.clickFilterPopOverOption('1.1.5');
-        expect((await rule.rulePage.getEnableRulesRowSwitchButton()) === 1).to.be(true);
       });
     });
 
