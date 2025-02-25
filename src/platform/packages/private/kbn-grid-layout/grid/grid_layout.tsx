elastic/kibana

commit 219f31ab2da2fd31486109920694a56d60df8212

[kbn-grid-layout] Cleanup memoization and styling (#210285)
## Summary

This PR cleans up the `kbn-grid-layout` code in two ways: 
1. Rather than memoizing components in their parents, I swapped to using
`React.memo` for all components, which accomplishes the same behaviour
in a slightly cleaner way.
2. I moved all Emotion style definitions **outside** of the React
components so that we no longer have to re-parse the CSS string on every
render (see [this
comment](https://github.com/elastic/eui/discussions/6828#discussioncomment-11247425)).


### Checklist

- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

src/platform/packages/private/kbn-grid-layout/grid/grid_layout.tsx
@@ -9,7 +9,7 @@
 
 import classNames from 'classnames';
 import { cloneDeep } from 'lodash';
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { combineLatest, distinctUntilChanged, map, pairwise, skip } from 'rxjs';
 
 import { css } from '@emotion/react';
@@ -134,22 +134,6 @@ export const GridLayout = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  /**
-   * Memoize row children components to prevent unnecessary re-renders
-   */
-  const children = useMemo(() => {
-    return Array.from({ length: rowCount }, (_, rowIndex) => {
-      return (
-        <GridRow
-          key={rowIndex}
-          rowIndex={rowIndex}
-          renderPanelContents={renderPanelContents}
-          gridLayoutStateManager={gridLayoutStateManager}
-        />
-      );
-    });
-  }, [rowCount, gridLayoutStateManager, renderPanelContents]);
-
   return (
     <GridHeightSmoother gridLayoutStateManager={gridLayoutStateManager}>
       <div
@@ -158,72 +142,83 @@ export const GridLayout = ({
           setDimensionsRef(divElement);
         }}
         className={classNames('kbnGrid', className)}
-        css={css`
-          padding: calc(var(--kbnGridGutterSize) * 1px);
-
-          // disable pointer events and user select on drag + resize
-          &:has(.kbnGridPanel--active) {
-            user-select: none;
-            pointer-events: none;
-          }
-
-          &:has(.kbnGridPanel--expanded) {
-            ${expandedPanelStyles}
-          }
-          &.kbnGrid--mobileView {
-            ${singleColumnStyles}
-          }
-        `}
+        css={[
+          styles.layoutPadding,
+          styles.hasActivePanel,
+          styles.singleColumn,
+          styles.hasExpandedPanel,
+        ]}
       >
-        {children}
+        {Array.from({ length: rowCount }, (_, rowIndex) => {
+          return (
+            <GridRow
+              key={rowIndex}
+              rowIndex={rowIndex}
+              renderPanelContents={renderPanelContents}
+              gridLayoutStateManager={gridLayoutStateManager}
+            />
+          );
+        })}
       </div>
     </GridHeightSmoother>
   );
 };
 
-const singleColumnStyles = css`
-  .kbnGridRow {
-    grid-template-columns: 100%;
-    grid-template-rows: auto;
-    grid-auto-flow: row;
-    grid-auto-rows: auto;
-  }
-
-  .kbnGridPanel {
-    grid-area: unset !important;
-  }
-`;
-
-const expandedPanelStyles = css`
-  height: 100%;
-
-  & .kbnGridRowContainer:has(.kbnGridPanel--expanded) {
-    // targets the grid row container that contains the expanded panel
-    .kbnGridRowHeader {
-      height: 0px; // used instead of 'display: none' due to a11y concerns
-    }
-    .kbnGridRow {
-      display: block !important; // overwrite grid display
-      height: 100%;
-      .kbnGridPanel {
-        &.kbnGridPanel--expanded {
-          height: 100% !important;
-        }
-        &:not(.kbnGridPanel--expanded) {
-          // hide the non-expanded panels
-          position: absolute;
-          top: -9999px;
-          left: -9999px;
-          visibility: hidden; // remove hidden panels and their contents from tab order for a11y
-        }
-      }
-    }
-  }
-
-  & .kbnGridRowContainer:not(:has(.kbnGridPanel--expanded)) {
-    // targets the grid row containers that **do not** contain the expanded panel
-    position: absolute;
-    top: -9999px;
-    left: -9999px;
-  }
-`;
+const styles = {
+  layoutPadding: css({
+    padding: 'calc(var(--kbnGridGutterSize) * 1px)',
+  }),
+  hasActivePanel: css({
+    '&:has(.kbnGridPanel--active)': {
+      // disable pointer events and user select on drag + resize
+      userSelect: 'none',
+      pointerEvents: 'none',
+    },
+  }),
+  singleColumn: css({
+    '&.kbnGrid--mobileView': {
+      '.kbnGridRow': {
+        gridTemplateAreas: '100%',
+        gridTemplateRows: 'auto',
+        gridAutoFlow: 'row',
+        gridAutoRows: 'auto',
+      },
+      '.kbnGridPanel': {
+        gridArea: 'unset !important',
+      },
+    },
+  }),
+  hasExpandedPanel: css({
+    '&:has(.kbnGridPanel--expanded)': {
+      height: '100%',
+      // targets the grid row container that contains the expanded panel
+      '& .kbnGridRowContainer:has(.kbnGridPanel--expanded)': {
+        '.kbnGridRowHeader': {
+          height: '0px', // used instead of 'display: none' due to a11y concerns
+        },
+        '.kbnGridRow': {
+          display: 'block !important', // overwrite grid display
+          height: '100%',
+          '.kbnGridPanel': {
+            '&.kbnGridPanel--expanded': {
+              height: '100% !important',
+            },
+            // hide the non-expanded panels
+            '&:not(.kbnGridPanel--expanded)': {
+              position: 'absolute',
+              top: '-9999px',
+              left: '-9999px',
+              visibility: 'hidden', // remove hidden panels and their contents from tab order for a11y
+            },
+          },
+        },
+      },
+      // targets the grid row containers that **do not** contain the expanded panel
+      '& .kbnGridRowContainer:not(:has(.kbnGridPanel--expanded))': {
+        position: 'absolute',
+        top: '-9999px',
+        left: '-9999px',
+      },
+    },
+  }),
+};

src/platform/packages/private/kbn-grid-layout/grid/drag_preview.tsx
@@ -14,54 +14,51 @@ import { css } from '@emotion/react';
 
 import { GridLayoutStateManager } from './types';
 
-export const DragPreview = ({
-  rowIndex,
-  gridLayoutStateManager,
-}: {
-  rowIndex: number;
-  gridLayoutStateManager: GridLayoutStateManager;
-}) => {
-  const dragPreviewRef = useRef<HTMLDivElement | null>(null);
+export const DragPreview = React.memo(
+  ({
+    rowIndex,
+    gridLayoutStateManager,
+  }: {
+    rowIndex: number;
+    gridLayoutStateManager: GridLayoutStateManager;
+  }) => {
+    const dragPreviewRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(
-    () => {
-      /** Update the styles of the drag preview via a subscription to prevent re-renders */
-      const styleSubscription = combineLatest([
-        gridLayoutStateManager.activePanel$,
-        gridLayoutStateManager.proposedGridLayout$,
-      ])
-        .pipe(skip(1)) // skip the first emit because the drag preview is only rendered after a user action
-        .subscribe(([activePanel, proposedGridLayout]) => {
-          if (!dragPreviewRef.current) return;
+    useEffect(
+      () => {
+        /** Update the styles of the drag preview via a subscription to prevent re-renders */
+        const styleSubscription = combineLatest([
+          gridLayoutStateManager.activePanel$,
+          gridLayoutStateManager.proposedGridLayout$,
+        ])
+          .pipe(skip(1)) // skip the first emit because the drag preview is only rendered after a user action
+          .subscribe(([activePanel, proposedGridLayout]) => {
+            if (!dragPreviewRef.current) return;
 
-          if (!activePanel || !proposedGridLayout?.[rowIndex].panels[activePanel.id]) {
-            dragPreviewRef.current.style.display = 'none';
-          } else {
-            const panel = proposedGridLayout[rowIndex].panels[activePanel.id];
-            dragPreviewRef.current.style.display = 'block';
-            dragPreviewRef.current.style.gridColumnStart = `${panel.column + 1}`;
-            dragPreviewRef.current.style.gridColumnEnd = `${panel.column + 1 + panel.width}`;
-            dragPreviewRef.current.style.gridRowStart = `${panel.row + 1}`;
-            dragPreviewRef.current.style.gridRowEnd = `${panel.row + 1 + panel.height}`;
-          }
-        });
+            if (!activePanel || !proposedGridLayout?.[rowIndex].panels[activePanel.id]) {
+              dragPreviewRef.current.style.display = 'none';
+            } else {
+              const panel = proposedGridLayout[rowIndex].panels[activePanel.id];
+              dragPreviewRef.current.style.display = 'block';
+              dragPreviewRef.current.style.gridColumnStart = `${panel.column + 1}`;
+              dragPreviewRef.current.style.gridColumnEnd = `${panel.column + 1 + panel.width}`;
+              dragPreviewRef.current.style.gridRowStart = `${panel.row + 1}`;
+              dragPreviewRef.current.style.gridRowEnd = `${panel.row + 1 + panel.height}`;
+            }
+          });
 
-      return () => {
-        styleSubscription.unsubscribe();
-      };
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
+        return () => {
+          styleSubscription.unsubscribe();
+        };
+      },
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      []
+    );
 
-  return (
-    <div
-      ref={dragPreviewRef}
-      className={'kbnGridPanel--dragPreview'}
-      css={css`
-        display: none;
-        pointer-events: none;
-      `}
-    />
-  );
-};
+    return <div ref={dragPreviewRef} className={'kbnGridPanel--dragPreview'} css={styles} />;
+  }
+);
+
+const styles = css({ display: 'none', pointerEvents: 'none' });
+
+DragPreview.displayName = 'KbnGridLayoutDragPreview';

src/platform/packages/private/kbn-grid-layout/grid/grid_height_smoother.tsx
@@ -12,58 +12,67 @@ import React, { PropsWithChildren, useEffect, useRef } from 'react';
 import { combineLatest } from 'rxjs';
 import { GridLayoutStateManager } from './types';
 
-export const GridHeightSmoother = ({
-  children,
-  gridLayoutStateManager,
-}: PropsWithChildren<{ gridLayoutStateManager: GridLayoutStateManager }>) => {
-  // set the parent div size directly to smooth out height changes.
-  const smoothHeightRef = useRef<HTMLDivElement | null>(null);
+export const GridHeightSmoother = React.memo(
+  ({
+    children,
+    gridLayoutStateManager,
+  }: PropsWithChildren<{ gridLayoutStateManager: GridLayoutStateManager }>) => {
+    // set the parent div size directly to smooth out height changes.
+    const smoothHeightRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    /**
-     * When the user is interacting with an element, the page can grow, but it cannot
-     * shrink. This is to stop a behaviour where the page would scroll up automatically
-     * making the panel shrink or grow unpredictably.
-     */
-    const interactionStyleSubscription = combineLatest([
-      gridLayoutStateManager.gridDimensions$,
-      gridLayoutStateManager.interactionEvent$,
-    ]).subscribe(([dimensions, interactionEvent]) => {
-      if (!smoothHeightRef.current || gridLayoutStateManager.expandedPanelId$.getValue()) return;
+    useEffect(() => {
+      /**
+       * When the user is interacting with an element, the page can grow, but it cannot
+       * shrink. This is to stop a behaviour where the page would scroll up automatically
+       * making the panel shrink or grow unpredictably.
+       */
+      const interactionStyleSubscription = combineLatest([
+        gridLayoutStateManager.gridDimensions$,
+        gridLayoutStateManager.interactionEvent$,
+      ]).subscribe(([dimensions, interactionEvent]) => {
+        if (!smoothHeightRef.current || gridLayoutStateManager.expandedPanelId$.getValue()) return;
 
-      if (!interactionEvent) {
-        smoothHeightRef.current.style.minHeight = `${dimensions.height}px`;
-        return;
-      }
-      smoothHeightRef.current.style.minHeight = `${
-        smoothHeightRef.current.getBoundingClientRect().height
-      }px`;
-    });
-
-    return () => {
-      interactionStyleSubscription.unsubscribe();
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <div
-      ref={smoothHeightRef}
-      className={'kbnGridWrapper'}
-      css={css`
-        height: 100%;
-        overflow-anchor: none;
-        transition: min-height 500ms linear;
-
-        &:has(.kbnGridPanel--expanded) {
-          min-height: 100% !important;
-          max-height: 100vh; // fallback in case if the parent doesn't set the height correctly
-          position: relative;
-          transition: none;
+        if (!interactionEvent) {
+          smoothHeightRef.current.style.minHeight = `${dimensions.height}px`;
+          return;
         }
-      `}
-    >
-      {children}
-    </div>
-  );
+        smoothHeightRef.current.style.minHeight = `${
+          smoothHeightRef.current.getBoundingClientRect().height
+        }px`;
+      });
+
+      return () => {
+        interactionStyleSubscription.unsubscribe();
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+      <div
+        ref={smoothHeightRef}
+        className={'kbnGridWrapper'}
+        css={[styles.heightSmoothing, styles.hasActivePanel]}
+      >
+        {children}
+      </div>
+    );
+  }
+);
+
+const styles = {
+  heightSmoothing: css({
+    height: '100%',
+    overflowAnchor: 'none',
+    transition: 'min-height 500ms linear',
+  }),
+  hasActivePanel: css({
+    '&:has(.kbnGridPanel--expanded)': {
+      minHeight: '100% !important',
+      maxHeight: '100vh', // fallback in case if the parent doesn't set the height correctly
+      position: 'relative',
+      transition: 'none',
+    },
+  }),
 };
+
+GridHeightSmoother.displayName = 'KbnGridLayoutHeightSmoother';

src/platform/packages/private/kbn-grid-layout/grid/grid_panel/drag_handle/default_drag_handle.tsx
@@ -7,61 +7,57 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 import React from 'react';
-import { EuiIcon, useEuiTheme } from '@elastic/eui';
+import { EuiIcon, type UseEuiTheme } from '@elastic/eui';
 import { css } from '@emotion/react';
 import { i18n } from '@kbn/i18n';
 import { UserInteractionEvent } from '../../use_grid_layout_events/types';
 
-export const DefaultDragHandle = ({
-  onDragStart,
-}: {
-  onDragStart: (e: UserInteractionEvent) => void;
-}) => {
-  const { euiTheme } = useEuiTheme();
+export const DefaultDragHandle = React.memo(
+  ({ onDragStart }: { onDragStart: (e: UserInteractionEvent) => void }) => {
+    return (
+      <button
+        onMouseDown={onDragStart}
+        onTouchStart={onDragStart}
+        aria-label={i18n.translate('kbnGridLayout.dragHandle.ariaLabel', {
+          defaultMessage: 'Drag to move',
+        })}
+        className="kbnGridPanel__dragHandle"
+        css={styles}
+      >
+        <EuiIcon type="grabOmnidirectional" />
+      </button>
+    );
+  }
+);
 
-  return (
-    <button
-      onMouseDown={onDragStart}
-      onTouchStart={onDragStart}
-      aria-label={i18n.translate('kbnGridLayout.dragHandle.ariaLabel', {
-        defaultMessage: 'Drag to move',
-      })}
-      className="kbnGridPanel__dragHandle"
-      css={css`
-        opacity: 0;
-        display: flex;
-        cursor: move;
-        position: absolute;
-        align-items: center;
-        justify-content: center;
-        top: -${euiTheme.size.l};
-        width: ${euiTheme.size.l};
-        height: ${euiTheme.size.l};
-        z-index: ${euiTheme.levels.modal};
-        margin-left: ${euiTheme.size.s};
-        border: 1px solid ${euiTheme.border.color};
-        border-bottom: none;
-        background-color: ${euiTheme.colors.backgroundBasePlain};
-        border-radius: ${euiTheme.border.radius} ${euiTheme.border.radius} 0 0;
-        cursor: grab;
-        transition: ${euiTheme.animation.slow} opacity;
-        .kbnGridPanel:hover &,
-        .kbnGridPanel:focus-within &,
-        &:active,
-        &:focus {
-          opacity: 1 !important;
-        }
-        &:active {
-          cursor: grabbing;
-        }
-        .kbnGrid--static &,
-        .kbnGridPanel--expanded & {
-          display: none;
-        }
-        touch-action: none;
-      `}
-    >
-      <EuiIcon type="grabOmnidirectional" />
-    </button>
-  );
-};
+const styles = ({ euiTheme }: UseEuiTheme) =>
+  css({
+    opacity: 0,
+    display: 'flex',
+    cursor: 'grab',
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: `-${euiTheme.size.l}`,
+    width: euiTheme.size.l,
+    height: euiTheme.size.l,
+    zIndex: euiTheme.levels.modal,
+    marginLeft: euiTheme.size.s,
+    border: `1px solid ${euiTheme.border.color}`,
+    borderBottom: 'none',
+    backgroundColor: euiTheme.colors.backgroundBasePlain,
+    borderRadius: `${euiTheme.border.radius} ${euiTheme.border.radius} 0 0`,
+    transition: `${euiTheme.animation.slow} opacity`,
+    touchAction: 'none',
+    '.kbnGridPanel:hover &, .kbnGridPanel:focus-within &, &:active, &:focus': {
+      opacity: '1 !important',
+    },
+    '&:active': {
+      cursor: 'grabbing',
+    },
+    '.kbnGrid--static &, .kbnGridPanel--expanded &': {
+      display: 'none',
+    },
+  });
+
+DefaultDragHandle.displayName = 'KbnGridLayoutDefaultDragHandle';

src/platform/packages/private/kbn-grid-layout/grid/grid_panel/drag_handle/index.tsx
@@ -17,53 +17,57 @@ export interface DragHandleApi {
   setDragHandles: (refs: Array<HTMLElement | null>) => void;
 }
 
-export const DragHandle = React.forwardRef<
-  DragHandleApi,
-  {
-    gridLayoutStateManager: GridLayoutStateManager;
-    panelId: string;
-    rowIndex: number;
-  }
->(({ gridLayoutStateManager, panelId, rowIndex }, ref) => {
-  const startInteraction = useGridLayoutEvents({
-    interactionType: 'drag',
-    gridLayoutStateManager,
-    panelId,
-    rowIndex,
-  });
+export const DragHandle = React.memo(
+  React.forwardRef<
+    DragHandleApi,
+    {
+      gridLayoutStateManager: GridLayoutStateManager;
+      panelId: string;
+      rowIndex: number;
+    }
+  >(({ gridLayoutStateManager, panelId, rowIndex }, ref) => {
+    const startInteraction = useGridLayoutEvents({
+      interactionType: 'drag',
+      gridLayoutStateManager,
+      panelId,
+      rowIndex,
+    });
 
-  const [dragHandleCount, setDragHandleCount] = useState<number>(0);
-  const removeEventListenersRef = useRef<(() => void) | null>(null);
+    const [dragHandleCount, setDragHandleCount] = useState<number>(0);
+    const removeEventListenersRef = useRef<(() => void) | null>(null);
 
-  const setDragHandles = useCallback(
-    (dragHandles: Array<HTMLElement | null>) => {
-      setDragHandleCount(dragHandles.length);
-      for (const handle of dragHandles) {
-        if (handle === null) return;
-        handle.addEventListener('mousedown', startInteraction, { passive: true });
-        handle.addEventListener('touchstart', startInteraction, { passive: true });
-        handle.style.touchAction = 'none';
-      }
-      removeEventListenersRef.current = () => {
+    const setDragHandles = useCallback(
+      (dragHandles: Array<HTMLElement | null>) => {
+        setDragHandleCount(dragHandles.length);
         for (const handle of dragHandles) {
           if (handle === null) return;
-          handle.removeEventListener('mousedown', startInteraction);
-          handle.removeEventListener('touchstart', startInteraction);
+          handle.addEventListener('mousedown', startInteraction, { passive: true });
+          handle.addEventListener('touchstart', startInteraction, { passive: true });
+          handle.style.touchAction = 'none';
         }
-      };
-    },
-    [startInteraction]
-  );
+        removeEventListenersRef.current = () => {
+          for (const handle of dragHandles) {
+            if (handle === null) return;
+            handle.removeEventListener('mousedown', startInteraction);
+            handle.removeEventListener('touchstart', startInteraction);
+          }
+        };
+      },
+      [startInteraction]
+    );
 
-  useEffect(
-    () => () => {
-      // on unmount, remove all drag handle event listeners
-      removeEventListenersRef.current?.();
-    },
-    []
-  );
+    useEffect(
+      () => () => {
+        // on unmount, remove all drag handle event listeners
+        removeEventListenersRef.current?.();
+      },
+      []
+    );
 
-  useImperativeHandle(ref, () => ({ setDragHandles }), [setDragHandles]);
+    useImperativeHandle(ref, () => ({ setDragHandles }), [setDragHandles]);
 
-  return Boolean(dragHandleCount) ? null : <DefaultDragHandle onDragStart={startInteraction} />;
-});
+    return Boolean(dragHandleCount) ? null : <DefaultDragHandle onDragStart={startInteraction} />;
+  })
+);
+
+DragHandle.displayName = 'KbnGridLayoutDragHandle';

commit 464a4714468fa70552e4b110af5fdb17a3e44161

[kbn-grid-layout] Fix mobile styles (#212312)
## Summary

This PR fixes a broken style introduced in
https://github.com/elastic/kibana/pull/210285 where, when rewriting the
single column styles from strings to objects, I accidentally set the
property `gridTemplateAreas` instead of `gridTemplateColumns` - this
resulted in the single column / mobile view being broken:

| Before | After |
|--------|--------|
|
![image](https://github.com/user-attachments/assets/e0541cc7-66dd-41a0-8e21-6931c491cea4)
|
![image](https://github.com/user-attachments/assets/12bc1453-1da4-4784-9242-b1cccdf9e24e)
|


### Checklist

- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

src/platform/packages/private/kbn-grid-layout/grid/grid_layout.tsx
@@ -181,7 +181,7 @@ const styles = {
   singleColumn: css({
     '&.kbnGrid--mobileView': {
       '.kbnGridRow': {
-        gridTemplateAreas: '100%',
+        gridTemplateColumns: '100%',
         gridTemplateRows: 'auto',
         gridAutoFlow: 'row',
         gridAutoRows: 'auto',