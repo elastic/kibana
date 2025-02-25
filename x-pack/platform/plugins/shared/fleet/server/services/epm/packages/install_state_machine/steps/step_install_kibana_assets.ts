60fa794ff95fb02e1df88c79befbbd9cb34b36ae
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-02-25 15:14:36 -0500

    fix tests

--- x-pack/platform/plugins/shared/fleet/server/services/epm/packages/install_state_machine/steps/step_install_kibana_assets.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/epm/packages/install_state_machine/steps/step_install_kibana_assets.ts
@@ -98,9 +98,10 @@ export async function cleanUpKibanaAssetsStep(context: InstallContext) {
  *
  */
 export async function cleanUpUnusedKibanaAssetsStep(context: InstallContext) {
-  const { logger, installedPkg, packageInstallContext, spaceId, installedKibanaAssetsRefs } =
-    context;
+  const { logger, installedPkg, packageInstallContext, spaceId, kibanaAssetPromise } = context;
   const { packageInfo } = packageInstallContext;
+
+  const installedKibanaAssetsRefs = await kibanaAssetPromise;
 
   if (!installedKibanaAssetsRefs) {
     return;
