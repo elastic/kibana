80e8d2d1c5b293b36ef47b0aa2055892da59b7fa
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-25 21:59:56 +0100

    Relocating module `@kbn/mock-idp-utils`

--- .eslintrc.js
+++ .eslintrc.js
@@ -1862,7 +1862,7 @@ module.exports = {
         'test/interactive_setup_functional/**/*.{js,mjs,ts,tsx}',
 
         'packages/kbn-mock-idp-plugin/**/*.{js,mjs,ts,tsx}',
-        'packages/kbn-mock-idp-utils/**/*.{js,mjs,ts,tsx}',
+        'src/platform/packages/private/kbn-mock-idp-utils/**/*.{js,mjs,ts,tsx}',
         'src/platform/packages/shared/kbn-security-hardening/**/*.{js,mjs,ts,tsx}',
         'src/platform/packages/shared/kbn-user-profile-components/**/*.{js,mjs,ts,tsx}',
 
