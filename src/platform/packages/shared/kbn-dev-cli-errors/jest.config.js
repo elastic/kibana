a763bf2da8d5b568a07b4aa2f6085f0dac1cf08a
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-25 21:37:11 +0100

    Relocating module `@kbn/dev-cli-errors`

--- src/platform/packages/shared/kbn-dev-cli-errors/jest.config.js
+++ src/platform/packages/shared/kbn-dev-cli-errors/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-dev-cli-errors'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-dev-cli-errors'],
 };
