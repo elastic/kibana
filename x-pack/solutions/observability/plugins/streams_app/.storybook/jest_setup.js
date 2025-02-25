2f6e81b64cd82d4bd2ad6990805b96c790a762c0
Sergi Romeu <sergi.romeu@elastic.co>
2025-02-25 11:39:37 +0100

    refactor: use @storybook/react instead of deprecated @storybook/testing-react

--- x-pack/solutions/observability/plugins/streams_app/.storybook/jest_setup.js
+++ x-pack/solutions/observability/plugins/streams_app/.storybook/jest_setup.js
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { setGlobalConfig } from '@storybook/testing-react';
+import { setGlobalConfig } from '@storybook/react';
 import * as globalStorybookConfig from './preview';
 
 setGlobalConfig(globalStorybookConfig);
