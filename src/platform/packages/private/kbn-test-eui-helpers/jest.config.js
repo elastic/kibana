14e8e009ab9df0a7c08fc2f02f486d0ac27fd7fb
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-25 22:23:08 +0100

    Relocating module `@kbn/test-eui-helpers`

--- src/platform/packages/private/kbn-test-eui-helpers/jest.config.js
+++ src/platform/packages/private/kbn-test-eui-helpers/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-test-eui-helpers'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/private/kbn-test-eui-helpers'],
 };
