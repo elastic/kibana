f17eed9cc87d0096a423516e01e0f9e170d3173d
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-02-25 21:26:17 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/search/plugins/workchat/workchat_app/server/plugin.ts
+++ x-pack/solutions/search/plugins/workchat/workchat_app/server/plugin.ts
@@ -14,8 +14,6 @@ import type {
   WorkChatAppPluginStartDependencies,
 } from './types';
 
-import { registerFunctions } from './functions';
-
 export class WorkChatAppPlugin
   implements
     Plugin<
