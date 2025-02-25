4190de905bc11e02ce539c32f5f98166479aef65
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-02-25 21:44:27 +0100

    Relocating module `@kbn/es-archiver`

--- src/platform/packages/shared/kbn-es-archiver/jest.config.js
+++ src/platform/packages/shared/kbn-es-archiver/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-es-archiver'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-es-archiver'],
 };
