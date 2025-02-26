40b3e011ce693a09aea4cf9971d1889f4280d6ab
Tre' Seymour <wayne.seymour@elastic.co>
2025-02-26 10:41:46 +0000

    Interesting, so runFork is a background thing.

--- src/platform/packages/shared/kbn-ftr-common-functional-services/services/fp_experiments/effect_ts_research/index.ts
+++ src/platform/packages/shared/kbn-ftr-common-functional-services/services/fp_experiments/effect_ts_research/index.ts
@@ -1,50 +1,45 @@
-import { Console, Effect } from 'effect';
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
 
-// Function to make the API call
-const getJson = (url: string) =>
-  Effect.tryPromise(() =>
-    fetch(url).then((res) => {
-      if (!res.ok) {
-        console.log('error');
-        throw new Error(res.statusText);
-      }
-      console.log('ok');
-      return res.json() as unknown;
-    })
-  );
-
-// Program that retries the API call twice, times out after 4 seconds,
-// and logs errors
-const program = (url: string) =>
-  getJson(url).pipe(
-    Effect.retry({ times: 2 }),
-    Effect.timeout('4 seconds'),
-    Effect.catchAll(Console.error)
-  );
+import { Random, Console, Effect } from 'effect';
 
 export const runEffectTSExperiments = () => {
+  const flipTheCoin = Effect.if(Random.nextBoolean, {
+    onTrue: () => Console.log('Head'), // Runs if the predicate is true
+    onFalse: () => Console.log('Tail'), // Runs if the predicate is false
+  });
+
+  Effect.runFork(flipTheCoin);
+};
+
+function ep1() {
   // Test case: successful API response
   Effect.runFork(program('https://dummyjson.com/products/1?delay=1000'));
   /*
-Output:
-ok
-*/
+   Output:
+   ok
+   */
 
   // Test case: API call exceeding timeout limit
   Effect.runFork(program('https://dummyjson.com/products/1?delay=5000'));
   /*
-Output:
-TimeoutException: Operation timed out before the specified duration of '4s' elapsed
-*/
+   Output:
+   TimeoutException: Operation timed out before the specified duration of '4s' elapsed
+   */
 
   // Test case: API returning an error response
   // Effect.runFork(program('https://dummyjson.com/auth/products/1?delay=500'));
   /*
-Output:
-error
-error
-error
-UnknownException: An unknown error occurred
-*/
+   Output:
+   error
+   error
+   error
+   UnknownException: An unknown error occurred
+   */
 }
-
