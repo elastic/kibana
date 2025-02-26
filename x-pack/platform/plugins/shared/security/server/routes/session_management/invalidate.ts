a710dbf9d116610005647dbb9f1f50c39666cb6b
lcawl <lcawley@elastic.co>
2025-02-26 10:00:24 -0800

    Add minimal user session management API docs

--- x-pack/platform/plugins/shared/security/server/routes/session_management/invalidate.ts
+++ x-pack/platform/plugins/shared/security/server/routes/session_management/invalidate.ts
@@ -52,6 +52,11 @@ export function defineInvalidateSessionsRoutes({
         access: buildFlavor === 'serverless' ? 'internal' : 'public',
 
         summary: `Invalidate user sessions`,
+        description: `Invalidate user sessions that match a query.`,
+        tags: ['oas-tag:user session'],
+        availability: {
+          stability: 'experimental'
+        },
       },
     },
     async (_context, request, response) => {
