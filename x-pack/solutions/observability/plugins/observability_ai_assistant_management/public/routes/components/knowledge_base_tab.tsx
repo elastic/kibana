69ae01ad9bfa6e71cd9ddf7187e1b15db7f23731
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-02-27 12:27:38 -0500

    [Obs AI Assistant] Show loaders to fix the flicker in KB settings tab (#208431)

--- x-pack/solutions/observability/plugins/observability_ai_assistant_management/public/routes/components/knowledge_base_tab.tsx
+++ x-pack/solutions/observability/plugins/observability_ai_assistant_management/public/routes/components/knowledge_base_tab.tsx
@@ -21,6 +21,7 @@ import {
   EuiFlexGroup,
   EuiFlexItem,
   EuiIcon,
+  EuiLoadingSpinner,
   EuiPopover,
   EuiScreenReaderOnly,
   EuiSpacer,
@@ -221,6 +222,16 @@ export function KnowledgeBaseTab() {
   const handleChangeQuery = (e: React.ChangeEvent<HTMLInputElement> | undefined) => {
     setQuery(e?.currentTarget.value || '');
   };
+
+  if (knowledgeBase.status.loading) {
+    return (
+      <EuiFlexGroup alignItems="center" direction="column">
+        <EuiFlexItem grow>
+          <EuiLoadingSpinner size="xl" />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+  }
 
   return knowledgeBase.status.value?.ready ? (
     <>
