84fda3babcf458cabcb9a712ccc47e0ad32f30fb
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-02-27 17:49:49 +0100

    connect the dots

---
+++ .buildkite/scripts/pipelines/chromium_linux_build/chromium_version.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/common/util.sh
+.buildkite/scripts/bootstrap.sh
+
+echo "---Attempting to compute chromium version for provided puppeteer version"
+
+echo "$(node scripts/chromium_version $PUPPETEER_VERSION)" | grep -i "chromium commit" | awk '{print $5}' | buildkite-agent meta-data set "chromium_commit_hash"
