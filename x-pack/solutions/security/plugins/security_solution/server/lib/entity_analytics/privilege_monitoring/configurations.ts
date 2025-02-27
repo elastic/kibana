a9964678bf7b2438902373894964ad3101458daa
Charlotte Alexandra Wilson <CAWilson94@users.noreply.github.com>
2025-02-27 11:32:46 +0000

    remove unnecessary fieldMap config

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/configurations.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/configurations.ts
@@ -4,9 +4,3 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-import type { FieldMap } from '@kbn/alerts-as-data-utils';
-
-export const privilegedMonitorUserFieldMap: FieldMap = {
-  // TODO: fill this in.
-};
