e05e4867aad566e225d9f55664c410ac3037b3ae
Bailey Cash <bailey.cash@elastic.co>
2025-02-28 15:52:53 -0500

    finalize new header

--- x-pack/solutions/observability/plugins/slo/public/pages/slo_details/components/header_title.tsx
+++ x-pack/solutions/observability/plugins/slo/public/pages/slo_details/components/header_title.tsx
@@ -10,19 +10,14 @@ import {
   EuiFlexItem,
   EuiMarkdownFormat,
   EuiSkeletonText,
-  EuiSpacer,
   EuiText,
 } from '@elastic/eui';
-import { i18n } from '@kbn/i18n';
 import { SLOWithSummaryResponse } from '@kbn/slo-schema';
 import React from 'react';
 import { TagsList } from '@kbn/observability-shared-plugin/public';
-
+import { SloStateBadge, SloStatusBadge, SloValueBadge } from '../../../components/slo/slo_badges';
 import { SloRemoteBadge } from '../../slos/components/badges/slo_remote_badge';
 import { SLOGroupings } from './groupings/slo_groupings';
-import moment from 'moment';
-import { SloStatusPanel } from './slo_status_panel';
-import { SloStatusPanelDisabled } from './slo_status_panel_disabled';
 
 export interface Props {
   slo?: SLOWithSummaryResponse;
@@ -35,80 +30,35 @@ export function HeaderTitle({ isLoading, slo }: Props) {
   }
 
   return (
-    <EuiFlexGroup direction="row" gutterSize="m">
-      <EuiFlexGroup direction="column" gutterSize="m">
-        <EuiFlexGroup
-          direction="row"
-          gutterSize="s"
-          alignItems="center"
-          justifyContent="flexStart"
-          responsive={true}
-          wrap={true}
-        >
-          {isLoading || slo.enabled ? (
-            <SloStatusPanel isLoading={isLoading} slo={slo} />
-          ) : (
-            <SloStatusPanelDisabled />
-          )}
-          <EuiFlexItem>
-            <EuiFlexItem grow={true}>
-              <EuiText className={'eui-textBreakWord'}>
-                <EuiMarkdownFormat textSize="xs" color="subdued">
-                  {i18n.translate('xpack.slo.sloDetails.overview.description', {
-                    defaultMessage: '**Description** {description}',
-                    values: {
-                      description: slo.description ? slo.description : '--',
-                    },
-                  })}
-                </EuiMarkdownFormat>
-              </EuiText>
-            </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <EuiSpacer size="s" />
-            </EuiFlexItem>
-            <EuiFlexGroup>
-              <EuiFlexItem grow={true}>
-                <EuiMarkdownFormat textSize="xs" color="subdued">
-                  {i18n.translate('xpack.slo.sloDetails.headerTitle.lastUpdatedLabel', {
-                    defaultMessage:
-                      '**Last updated by** reallylongusername@longdomainname.org **on** {updatedAt}',
-                    values: {
-                      updatedBy: slo.updatedBy ?? NOT_AVAILABLE_LABEL,
-                      updatedAt: moment(slo.updatedAt).format('ll'),
-                    },
-                  })}
-                </EuiMarkdownFormat>
-              </EuiFlexItem>
-              <EuiFlexItem grow={true}>
-                <EuiMarkdownFormat textSize="xs" color="subdued">
-                  {i18n.translate('xpack.slo.sloDetails.headerTitle.createdLabel', {
-                    defaultMessage:
-                      '**Created by** reallylongusername@longdomainname.org **on** {createdAt}',
-                    values: {
-                      createdBy: slo.createdBy ?? NOT_AVAILABLE_LABEL,
-                      createdAt: moment(slo.createdAt).format('ll'),
-                    },
-                  })}
-                </EuiMarkdownFormat>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-            <EuiFlexItem grow={false}>
-              <EuiSpacer size="s" />
-            </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <TagsList tags={slo.tags} />
-            </EuiFlexItem>
+    <EuiFlexGroup direction="column" gutterSize="m">
+      <EuiFlexGroup
+        direction="row"
+        gutterSize="s"
+        alignItems="center"
+        justifyContent="flexStart"
+        responsive={false}
+        wrap={true}
+      >
+        <SloValueBadge slo={slo} isLoading={isLoading} />
+        <SloStatusBadge slo={slo} isLoading={isLoading} />
+        <SloStateBadge slo={slo} />
+        <SloRemoteBadge slo={slo} />
+        {slo?.tags?.length && (
+          <EuiFlexItem grow={true}>
+            <TagsList tags={slo.tags} />
           </EuiFlexItem>
-        </EuiFlexGroup>
-        <EuiFlexGroup direction="row" alignItems="center">
-          <SloRemoteBadge slo={slo} />
-          <SLOGroupings slo={slo} />
-        </EuiFlexGroup>
+        )}
       </EuiFlexGroup>
+      {slo.description && (
+        <EuiFlexItem grow={true}>
+          <EuiText className={'eui-textBreakWord'}>
+            <EuiMarkdownFormat textSize="xs" color="subdued">
+              {slo.description}
+            </EuiMarkdownFormat>
+          </EuiText>
+        </EuiFlexItem>
+      )}
+      <SLOGroupings slo={slo} />
     </EuiFlexGroup>
   );
 }
-
-const NOT_AVAILABLE_LABEL = i18n.translate('xpack.slo.sloDetails.headerTitle.notAvailableLabel', {
-  defaultMessage: 'n/a',
-});
