633656fe0f8227f059ece82bed422a6f113612d1
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-02-28 10:02:40 +0100

    Small but important fix

--- src/platform/plugins/shared/controls/public/controls/esql_control/esql_control_selections.ts
+++ src/platform/plugins/shared/controls/public/controls/esql_control/esql_control_selections.ts
@@ -36,7 +36,9 @@ export function initializeESQLControlSelections(initialState: ESQLControlState) 
   // derive ESQL control variable from state.
   const getEsqlVariable = () => ({
     key: variableName$.value,
-    value: selectedOptions$.value[0],
+    value: Number.isNaN(selectedOptions$.value[0])
+      ? selectedOptions$.value[0]
+      : Number(selectedOptions$.value[0]),
     type: variableType$.value,
   });
   const esqlVariable$ = new BehaviorSubject<ESQLControlVariable>(getEsqlVariable());
