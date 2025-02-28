60761fdaacb21f8dfee1aa27fd65b85b07e29dd7
Sergi Romeu <sergi.romeu@elastic.co>
2025-02-28 12:22:47 +0100

    refactor: replace setGlobalConfig to setProjectAnnotations

--- x-pack/solutions/observability/plugins/exploratory_view/.storybook/jest_setup.js
+++ x-pack/solutions/observability/plugins/exploratory_view/.storybook/jest_setup.js
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { setGlobalConfig } from '@storybook/react';
+import { setProjectAnnotations } from '@storybook/react';
 import * as globalStorybookConfig from './preview';
 
-setGlobalConfig(globalStorybookConfig);
+setProjectAnnotations(globalStorybookConfig);
