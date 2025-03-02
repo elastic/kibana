elastic/kibana

commit 9a1d70d5d2430e1ad79e74119c5d95affdf11056

[Perfomance] Add `is_initial_load` meta (#206645)
closes https://github.com/elastic/observability-dev/issues/4185 

## Summary

This PR adds the `is_initial_load` parameter to the meta field to
distinguish whether the `onPageReady` trigger occurs during the initial
load or a page refresh.

Refactoring: 
- Removed the `target` field. as `context.pageName` now provides the
necessary information
- Refactor APM instrumentation to simplify it

Fixes: 
- https://github.com/elastic/observability-dev/issues/3464  


### ⚠️  Instrumentation 

The plugins need to call the following function: 

``` onPageRefreshStart()```


This method adds a performance marker `start::pageRefresh` to indicate when a page refresh begins. This marker is used along with an end marker `end::pageReady` to determine the total refresh duration.


 
https://github.com/user-attachments/assets/62587d18-b33e-437b-9774-d8e196dbf764

https://github.com/user-attachments/assets/e9c9a761-57bc-4743-9cc7-ea7634696ee3



### How to test
- Checkout the PR
- make sure you run `yarn kbn bootstrap`
- go to any page that has onPageReady function instrumented (ex services)  


### TODO
- Once approved, update docs

---------

Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

x-pack/solutions/observability/plugins/apm/public/context/apm_plugin/mock_apm_plugin_context.tsx
@@ -208,6 +208,8 @@ export function MockApmPluginContextWrapper({
     createCallApmApi(contextValue.core);
   }
 
+  performance.mark = jest.fn();
+
   const contextHistory = useHistory();
 
   const usedHistory = useMemo(() => {

x-pack/solutions/observability/plugins/infra/public/pages/metrics/metrics_explorer/hooks/use_metric_explorer_state.test.tsx
@@ -5,7 +5,9 @@
  * 2.0.
  */
 
-import { renderHook, act } from '@testing-library/react';
+import { renderHook, act, waitFor } from '@testing-library/react';
+import { PerformanceContextProvider } from '@kbn/ebt-tools';
+import { useLocation } from 'react-router-dom';
 import { useMetricsExplorerState } from './use_metric_explorer_state';
 import { MetricsExplorerOptionsContainer } from './use_metrics_explorer_options';
 import React from 'react';
@@ -14,6 +16,11 @@ import { resp, createSeries } from '../../../../utils/fixtures/metrics_explorer'
 jest.mock('../../../../hooks/use_kibana_timefilter_time', () => ({
   useKibanaTimefilterTime: (defaults: { from: string; to: string }) => [() => defaults],
   useSyncKibanaTimeFilterTime: () => [() => {}],
+}));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useLocation: jest.fn(),
 }));
 
 jest.mock('../../../../alerting/use_alert_prefill', () => ({
@@ -27,7 +34,9 @@ jest.mock('../../../../alerting/use_alert_prefill', () => ({
 const renderUseMetricsExplorerStateHook = () =>
   renderHook(() => useMetricsExplorerState(), {
     wrapper: ({ children }: React.PropsWithChildren<{}>) => (
-      <MetricsExplorerOptionsContainer>{children}</MetricsExplorerOptionsContainer>
+      <PerformanceContextProvider>
+        <MetricsExplorerOptionsContainer>{children}</MetricsExplorerOptionsContainer>
+      </PerformanceContextProvider>
     ),
   });
 
@@ -73,6 +82,12 @@ describe('useMetricsExplorerState', () => {
     });
     delete STORE.MetricsExplorerOptions;
     delete STORE.MetricsExplorerTimeRange;
+
+    const pathname = '/hosts';
+    (useLocation as jest.Mock).mockReturnValue(() => ({
+      pathname,
+    }));
+    performance.mark = jest.fn();
   });
 
   afterEach(() => {
@@ -88,9 +103,11 @@ describe('useMetricsExplorerState', () => {
       },
     });
     const { result } = renderUseMetricsExplorerStateHook();
-    expect(result.current.data!.pages[0]).toEqual(resp);
-    expect(result.current.error).toBe(null);
-    expect(result.current.isLoading).toBe(false);
+    await waitFor(() => {
+      expect(result.current.data!.pages[0]).toEqual(resp);
+      expect(result.current.error).toBe(null);
+      expect(result.current.isLoading).toBe(false);
+    });
   });
 
   describe('handleRefresh', () => {

src/core/packages/analytics/browser-internal/src/track_performance_measure_entries.test.ts
@@ -114,6 +114,11 @@ describe('trackPerformanceMeasureEntries', () => {
             anyKey: 'anyKey',
             anyValue: 'anyValue',
           },
+          meta: {
+            isInitialLoad: true,
+            queryRangeSecs: 86400,
+            queryOffsetSecs: 0,
+          },
         },
       },
     ]);
@@ -124,7 +129,7 @@ describe('trackPerformanceMeasureEntries', () => {
       duration: 1000,
       eventName: 'kibana:plugin_render_time',
       key1: 'key1',
-      meta: { target: '/' },
+      meta: { is_initial_load: true, query_range_secs: 86400, query_offset_secs: 0 },
       value1: 'value1',
     });
   });
@@ -152,7 +157,7 @@ describe('trackPerformanceMeasureEntries', () => {
     expect(analyticsClientMock.reportEvent).toHaveBeenCalledWith('performance_metric', {
       duration: 1000,
       eventName: 'kibana:plugin_render_time',
-      meta: { target: '/', query_range_secs: 86400, query_offset_secs: 0 },
+      meta: { query_range_secs: 86400, query_offset_secs: 0 },
     });
   });
 });

src/core/packages/analytics/browser-internal/src/track_performance_measure_entries.ts
@@ -72,9 +72,9 @@ export function trackPerformanceMeasureEntries(analytics: AnalyticsClient, isDev
             duration,
             ...customMetrics,
             meta: {
-              target,
               query_range_secs: meta?.queryRangeSecs,
               query_offset_secs: meta?.queryOffsetSecs,
+              is_initial_load: meta?.isInitialLoad,
             },
           });
         } catch (error) {

src/platform/packages/shared/kbn-ebt-tools/src/performance_metrics/context/measure_interaction/index.ts
@@ -12,24 +12,25 @@ import {
   getOffsetFromNowInSeconds,
   getTimeDifferenceInSeconds,
 } from '@kbn/timerange';
-import { perfomanceMarkers } from '../../performance_markers';
 import { EventData } from '../performance_context';
+import { perfomanceMarkers } from '../../performance_markers';
 
 interface PerformanceMeta {
   queryRangeSecs: number;
   queryOffsetSecs: number;
+  isInitialLoad?: boolean;
 }
 
-export function measureInteraction() {
+export function measureInteraction(pathname: string) {
   performance.mark(perfomanceMarkers.startPageChange);
-  const trackedRoutes: string[] = [];
+
   return {
     /**
      * Marks the end of the page ready state and measures the performance between the start of the page change and the end of the page ready state.
      * @param pathname - The pathname of the page.
      * @param customMetrics - Custom metrics to be included in the performance measure.
      */
-    pageReady(pathname: string, eventData?: EventData) {
+    pageReady(eventData?: EventData) {
       let performanceMeta: PerformanceMeta | undefined;
       performance.mark(perfomanceMarkers.endPageReady);
 
@@ -49,19 +50,48 @@ export function measureInteraction() {
         };
       }
 
-      if (!trackedRoutes.includes(pathname)) {
-        performance.measure(pathname, {
+      if (
+        performance.getEntriesByName(perfomanceMarkers.startPageChange).length > 0 &&
+        performance.getEntriesByName(perfomanceMarkers.endPageReady).length > 0
+      ) {
+        performance.measure(`[ttfmp:initial] - ${pathname}`, {
           detail: {
             eventName: 'kibana:plugin_render_time',
             type: 'kibana:performance',
             customMetrics: eventData?.customMetrics,
-            meta: performanceMeta,
+            meta: { ...performanceMeta, isInitialLoad: true },
           },
           start: perfomanceMarkers.startPageChange,
           end: perfomanceMarkers.endPageReady,
         });
-        trackedRoutes.push(pathname);
+
+        // Clean up the marks once the measure is done
+        performance.clearMarks(perfomanceMarkers.startPageChange);
+        performance.clearMarks(perfomanceMarkers.endPageReady);
       }
+
+      if (
+        performance.getEntriesByName(perfomanceMarkers.startPageRefresh).length > 0 &&
+        performance.getEntriesByName(perfomanceMarkers.endPageReady).length > 0
+      ) {
+        performance.measure(`[ttfmp:refresh] - ${pathname}`, {
+          detail: {
+            eventName: 'kibana:plugin_render_time',
+            type: 'kibana:performance',
+            customMetrics: eventData?.customMetrics,
+            meta: { ...performanceMeta, isInitialLoad: false },
+          },
+          start: perfomanceMarkers.startPageRefresh,
+          end: perfomanceMarkers.endPageReady,
+        });
+
+        // // Clean up the marks once the measure is done
+        performance.clearMarks(perfomanceMarkers.startPageRefresh);
+        performance.clearMarks(perfomanceMarkers.endPageReady);
+      }
+    },
+    pageRefreshStart() {
+      performance.mark(perfomanceMarkers.startPageRefresh);
     },
   };
 }

src/platform/packages/shared/kbn-ebt-tools/src/performance_metrics/context/measure_interaction/measure_interaction.test.tsx
@@ -15,147 +15,196 @@ describe('measureInteraction', () => {
     jest.restoreAllMocks();
   });
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-    performance.mark = jest.fn();
-    performance.measure = jest.fn();
-  });
+  describe('Initial load', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
+      performance.mark = jest.fn();
+      performance.measure = jest.fn();
 
-  it('should mark the start of the page change', () => {
-    measureInteraction();
-    expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.startPageChange);
-  });
-
-  it('should mark the end of the page ready state and measure performance', () => {
-    const interaction = measureInteraction();
-    const pathname = '/test-path';
-    interaction.pageReady(pathname);
-
-    expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
-    expect(performance.measure).toHaveBeenCalledWith(pathname, {
-      detail: {
-        eventName: 'kibana:plugin_render_time',
-        type: 'kibana:performance',
-      },
-      start: perfomanceMarkers.startPageChange,
-      end: perfomanceMarkers.endPageReady,
+      performance.getEntriesByName = jest
+        .fn()
+        .mockReturnValueOnce([{ name: 'start::pageChange' }])
+        .mockReturnValueOnce([{ name: 'end::pageReady' }])
+        .mockReturnValueOnce([]);
+      performance.clearMarks = jest.fn();
     });
-  });
 
-  it('should include custom metrics and meta in the performance measure', () => {
-    const interaction = measureInteraction();
-    const pathname = '/test-path';
-    const eventData = {
-      customMetrics: { key1: 'foo-metric', value1: 100 },
-      meta: { rangeFrom: 'now-15m', rangeTo: 'now' },
-    };
+    it('should mark the start of the page change', () => {
+      const pathname = '/test-path';
+      measureInteraction(pathname);
+      expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.startPageChange);
+    });
 
-    interaction.pageReady(pathname, eventData);
+    it('should mark the end of the page ready state and measure performance', () => {
+      const pathname = '/test-path';
+      const interaction = measureInteraction(pathname);
 
-    expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
-    expect(performance.measure).toHaveBeenCalledWith(pathname, {
-      detail: {
-        eventName: 'kibana:plugin_render_time',
-        type: 'kibana:performance',
-        customMetrics: eventData.customMetrics,
-        meta: {
-          queryRangeSecs: 900,
-          queryOffsetSecs: 0,
+      interaction.pageReady();
+
+      expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
+      expect(performance.measure).toHaveBeenCalledWith(`[ttfmp:initial] - ${pathname}`, {
+        detail: {
+          customMetrics: undefined,
+          eventName: 'kibana:plugin_render_time',
+          type: 'kibana:performance',
+          meta: {
+            isInitialLoad: true,
+          },
         },
-      },
-      end: 'end::pageReady',
-      start: 'start::pageChange',
+        start: perfomanceMarkers.startPageChange,
+        end: perfomanceMarkers.endPageReady,
+      });
     });
-  });
 
-  it('should handle absolute date format correctly', () => {
-    const interaction = measureInteraction();
-    const pathname = '/test-path';
-    jest.spyOn(global.Date, 'now').mockReturnValue(1733704200000); // 2024-12-09T00:30:00Z
+    it('should include custom metrics and meta in the performance measure', () => {
+      const pathname = '/test-path';
+      const interaction = measureInteraction(pathname);
+      const eventData = {
+        customMetrics: { key1: 'foo-metric', value1: 100 },
+        meta: { rangeFrom: 'now-15m', rangeTo: 'now' },
+      };
 
-    const eventData = {
-      meta: { rangeFrom: '2024-12-09T00:00:00Z', rangeTo: '2024-12-09T00:30:00Z' },
-    };
+      interaction.pageReady(eventData);
 
-    interaction.pageReady(pathname, eventData);
-
-    expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
-    expect(performance.measure).toHaveBeenCalledWith(pathname, {
-      detail: {
-        eventName: 'kibana:plugin_render_time',
-        type: 'kibana:performance',
-        customMetrics: undefined,
-        meta: {
-          queryRangeSecs: 1800,
-          queryOffsetSecs: 0,
+      expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
+      expect(performance.measure).toHaveBeenCalledWith(`[ttfmp:initial] - ${pathname}`, {
+        detail: {
+          eventName: 'kibana:plugin_render_time',
+          type: 'kibana:performance',
+          customMetrics: eventData.customMetrics,
+          meta: {
+            queryRangeSecs: 900,
+            queryOffsetSecs: 0,
+            isInitialLoad: true,
+          },
         },
-      },
-      end: 'end::pageReady',
-      start: 'start::pageChange',
+        end: 'end::pageReady',
+        start: 'start::pageChange',
+      });
     });
-  });
 
-  it('should handle negative offset when rangeTo is in the past', () => {
-    const interaction = measureInteraction();
-    const pathname = '/test-path';
-    jest.spyOn(global.Date, 'now').mockReturnValue(1733704200000); // 2024-12-09T00:30:00Z
+    it('should handle absolute date format correctly', () => {
+      const pathname = '/test-path';
+      const interaction = measureInteraction(pathname);
+      jest.spyOn(global.Date, 'now').mockReturnValue(1733704200000); // 2024-12-09T00:30:00Z
 
-    const eventData = {
-      meta: { rangeFrom: '2024-12-08T00:00:00Z', rangeTo: '2024-12-09T00:00:00Z' },
-    };
+      const eventData = {
+        meta: { rangeFrom: '2024-12-09T00:00:00Z', rangeTo: '2024-12-09T00:30:00Z' },
+      };
 
-    interaction.pageReady(pathname, eventData);
+      interaction.pageReady(eventData);
 
-    expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
-    expect(performance.measure).toHaveBeenCalledWith(pathname, {
-      detail: {
-        eventName: 'kibana:plugin_render_time',
-        type: 'kibana:performance',
-        customMetrics: undefined,
-        meta: {
-          queryRangeSecs: 86400,
-          queryOffsetSecs: -1800,
+      expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
+      expect(performance.measure).toHaveBeenCalledWith(`[ttfmp:initial] - ${pathname}`, {
+        detail: {
+          eventName: 'kibana:plugin_render_time',
+          type: 'kibana:performance',
+          customMetrics: undefined,
+          meta: {
+            queryRangeSecs: 1800,
+            queryOffsetSecs: 0,
+            isInitialLoad: true,
+          },
         },
-      },
-      end: 'end::pageReady',
-      start: 'start::pageChange',
+        end: 'end::pageReady',
+        start: 'start::pageChange',
+      });
     });
-  });
 
-  it('should handle positive offset when rangeTo is in the future', () => {
-    const interaction = measureInteraction();
-    const pathname = '/test-path';
-    jest.spyOn(global.Date, 'now').mockReturnValue(1733704200000); // 2024-12-09T00:30:00Z
+    it('should handle negative offset when rangeTo is in the past', () => {
+      const pathname = '/test-path';
+      const interaction = measureInteraction(pathname);
+      jest.spyOn(global.Date, 'now').mockReturnValue(1733704200000); // 2024-12-09T00:30:00Z
 
-    const eventData = {
-      meta: { rangeFrom: '2024-12-08T01:00:00Z', rangeTo: '2024-12-09T01:00:00Z' },
-    };
+      const eventData = {
+        meta: { rangeFrom: '2024-12-08T00:00:00Z', rangeTo: '2024-12-09T00:00:00Z' },
+      };
 
-    interaction.pageReady(pathname, eventData);
+      interaction.pageReady(eventData);
 
-    expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
-    expect(performance.measure).toHaveBeenCalledWith(pathname, {
-      detail: {
-        eventName: 'kibana:plugin_render_time',
-        type: 'kibana:performance',
-        customMetrics: undefined,
-        meta: {
-          queryRangeSecs: 86400,
-          queryOffsetSecs: 1800,
+      expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
+      expect(performance.measure).toHaveBeenCalledWith(`[ttfmp:initial] - ${pathname}`, {
+        detail: {
+          eventName: 'kibana:plugin_render_time',
+          type: 'kibana:performance',
+          customMetrics: undefined,
+          meta: {
+            queryRangeSecs: 86400,
+            queryOffsetSecs: -1800,
+            isInitialLoad: true,
+          },
         },
-      },
-      end: 'end::pageReady',
-      start: 'start::pageChange',
+        end: 'end::pageReady',
+        start: 'start::pageChange',
+      });
+    });
+
+    it('should handle positive offset when rangeTo is in the future', () => {
+      const pathname = '/test-path';
+
+      const interaction = measureInteraction(pathname);
+      jest.spyOn(global.Date, 'now').mockReturnValue(1733704200000); // 2024-12-09T00:30:00Z
+
+      const eventData = {
+        meta: { rangeFrom: '2024-12-08T01:00:00Z', rangeTo: '2024-12-09T01:00:00Z' },
+      };
+
+      interaction.pageReady(eventData);
+
+      expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
+      expect(performance.measure).toHaveBeenCalledWith(`[ttfmp:initial] - ${pathname}`, {
+        detail: {
+          eventName: 'kibana:plugin_render_time',
+          type: 'kibana:performance',
+          customMetrics: undefined,
+          meta: {
+            queryRangeSecs: 86400,
+            queryOffsetSecs: 1800,
+            isInitialLoad: true,
+          },
+        },
+        end: 'end::pageReady',
+        start: 'start::pageChange',
+      });
+      expect(performance.clearMarks).toHaveBeenCalledWith(perfomanceMarkers.startPageChange);
+      expect(performance.clearMarks).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
     });
   });
 
-  it('should not measure the same route twice', () => {
-    const interaction = measureInteraction();
-    const pathname = '/test-path';
+  describe('Refresh', () => {
+    beforeEach(() => {
+      performance.getEntriesByName = jest
+        .fn()
+        .mockReturnValue([{ name: 'start::pageRefresh' }])
+        .mockReturnValue([{ name: 'end::pageReady' }]);
+    });
+    it('should set isInitialLoad to false on refresh calls', () => {
+      const pathname = '/test-path';
+      const interaction = measureInteraction(pathname);
 
-    interaction.pageReady(pathname);
-    interaction.pageReady(pathname);
+      jest.spyOn(global.Date, 'now').mockReturnValue(1733704200000); // 2024-12-09T00:30:00Z
 
-    expect(performance.measure).toHaveBeenCalledTimes(1);
+      const eventData = {
+        meta: { rangeFrom: '2024-12-08T01:00:00Z', rangeTo: '2024-12-09T01:00:00Z' },
+      };
+
+      interaction.pageReady(eventData);
+
+      expect(performance.mark).toHaveBeenCalledWith(perfomanceMarkers.endPageReady);
+      expect(performance.measure).toHaveBeenCalledWith(`[ttfmp:refresh] - ${pathname}`, {
+        detail: {
+          eventName: 'kibana:plugin_render_time',
+          type: 'kibana:performance',
+          customMetrics: undefined,
+          meta: {
+            queryRangeSecs: 86400,
+            queryOffsetSecs: 1800,
+            isInitialLoad: false,
+          },
+        },
+        end: 'end::pageReady',
+        start: 'start::pageRefresh',
+      });
+    });
   });
 });

src/platform/packages/shared/kbn-ebt-tools/src/performance_metrics/context/performance_context.tsx
@@ -28,7 +28,8 @@ export interface EventData {
 export function PerformanceContextProvider({ children }: { children: React.ReactElement }) {
   const [isRendered, setIsRendered] = useState(false);
   const location = useLocation();
-  const interaction = measureInteraction();
+
+  const interaction = useMemo(() => measureInteraction(location.pathname), [location.pathname]);
 
   React.useEffect(() => {
     afterFrame(() => {
@@ -44,11 +45,14 @@ export function PerformanceContextProvider({ children }: { children: React.React
     () => ({
       onPageReady(eventData) {
         if (isRendered) {
-          interaction.pageReady(location.pathname, eventData);
+          interaction.pageReady(eventData);
         }
       },
+      onPageRefreshStart() {
+        interaction.pageRefreshStart();
+      },
     }),
-    [isRendered, location.pathname, interaction]
+    [isRendered, interaction]
   );
 
   return <PerformanceContext.Provider value={api}>{children}</PerformanceContext.Provider>;

src/platform/packages/shared/kbn-ebt-tools/src/performance_metrics/context/use_performance_context.tsx
@@ -15,6 +15,19 @@ export interface PerformanceApi {
    * @param eventData - Data to send with the performance measure, conforming the structure of a {@link EventData}.
    */
   onPageReady(eventData?: EventData): void;
+  /**
+   * Marks the start of a page refresh event for performance tracking.
+   * This method adds a performance marker start::pageRefresh to indicate when a page refresh begins.
+   *
+   * Usage:
+   * ```ts
+   * onPageRefreshStart();
+   * ```
+   *
+   * The marker set by this function can later be used in performance measurements
+   * along with an end marker end::pageReady to determine the total refresh duration.
+   */
+  onPageRefreshStart(): void;
 }
 
 export const PerformanceContext = createContext<PerformanceApi | undefined>(undefined);

commit efafe38a6072da58559c9df402ddc6f3899e1d1d

fix apm issues introduced in #206645

x-pack/solutions/observability/plugins/apm/public/context/apm_plugin/mock_apm_plugin_context.tsx
@@ -209,6 +209,7 @@ export function MockApmPluginContextWrapper({
   }
 
   performance.mark = jest.fn();
+  performance.clearMeasures = jest.fn();
 
   const contextHistory = useHistory();
 

x-pack/solutions/observability/plugins/infra/public/pages/metrics/metrics_explorer/hooks/use_metric_explorer_state.test.tsx
@@ -88,6 +88,7 @@ describe('useMetricsExplorerState', () => {
       pathname,
     }));
     performance.mark = jest.fn();
+    performance.clearMeasures = jest.fn();
   });
 
   afterEach(() => {