eea43c4b98079c7c27b67678f62a962ddfd58d0e
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-03-03 11:16:20 +0000

    [CI] Auto-commit changed files from 'yarn openapi:generate'

--- x-pack/test/api_integration/services/security_solution_api.gen.ts
+++ x-pack/test/api_integration/services/security_solution_api.gen.ts
@@ -61,6 +61,7 @@ import { EndpointKillProcessActionRequestBodyInput } from '@kbn/security-solutio
 import { EndpointScanActionRequestBodyInput } from '@kbn/security-solution-plugin/common/api/endpoint/actions/response_actions/scan/scan.gen';
 import { EndpointSuspendProcessActionRequestBodyInput } from '@kbn/security-solution-plugin/common/api/endpoint/actions/response_actions/suspend_process/suspend_process.gen';
 import { EndpointUnisolateActionRequestBodyInput } from '@kbn/security-solution-plugin/common/api/endpoint/actions/response_actions/unisolate/unisolate.gen';
+import { EndpointUnisolateRedirectRequestBodyInput } from '@kbn/security-solution-plugin/common/api/endpoint/actions/response_actions/unisolate/deprecated_unisolate.gen';
 import {
   ExportRulesRequestQueryInput,
   ExportRulesRequestBodyInput,
@@ -1604,6 +1605,9 @@ export interface EndpointSuspendProcessActionProps {
 }
 export interface EndpointUnisolateActionProps {
   body: EndpointUnisolateActionRequestBodyInput;
+}
+export interface EndpointUnisolateRedirectProps {
+  body: EndpointUnisolateRedirectRequestBodyInput;
 }
 export interface ExportRulesProps {
   query: ExportRulesRequestQueryInput;
