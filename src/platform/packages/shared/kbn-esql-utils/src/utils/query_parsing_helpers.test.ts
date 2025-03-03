048e94081932993cbecb288e137b8bdcf70800d8
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-03-03 12:04:32 +0100

    [ES|QL] Introduces a new package for esql types (#212754)
    ## Summary
    
    The esql types are around many packages and plugins creating problems
    either with the bundle size or cyclic dependencies.
    
    This PR is moving some of them on the esql-types package. I am going to
    move more there in follow up PRs.
    
    (I decided to rename it as it makes more sense to have a generic one for
    esql rather one only for the variables)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Marco Vettorello <vettorello.marco@gmail.com>
    (cherry picked from commit dab569b70ac054b1b5416f086b62a54376495def)
    
    # Conflicts:
    #	.github/CODEOWNERS

--- src/platform/packages/shared/kbn-esql-utils/src/utils/query_parsing_helpers.test.ts
+++ src/platform/packages/shared/kbn-esql-utils/src/utils/query_parsing_helpers.test.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 import type { DatatableColumn } from '@kbn/expressions-plugin/common';
-import { ESQLVariableType, type ESQLControlVariable } from '@kbn/esql-validation-autocomplete';
+import { ESQLVariableType, type ESQLControlVariable } from '@kbn/esql-types';
 import {
   getIndexPatternFromESQLQuery,
   getLimitFromESQLQuery,
