c6ffc1240f8fe403b6abfd457a19f4da986494cb
Nathan Reese <reese.nathan@gmail.com>
2025-03-04 13:51:05 -0700

    [controls] fix Korean characters split into 2 characters with space in between when typing in options list search input

--- src/platform/packages/shared/presentation/presentation_publishing/publishing_subject/publishing_batcher.ts
+++ src/platform/packages/shared/presentation/presentation_publishing/publishing_subject/publishing_batcher.ts
@@ -101,6 +101,8 @@ export const useBatchedOptionalPublishingSubjects = <
 /**
  * Batches the latest values of multiple publishing subjects into a single object. Use this to avoid unnecessary re-renders.
  * Use when `subjects` are static and do not change over the lifetime of the component.
+ * 
+ * Do not use when value is used as an input value to avoid debouncing user interactions
  *
  * @param subjects Publishing subjects array.
  */
