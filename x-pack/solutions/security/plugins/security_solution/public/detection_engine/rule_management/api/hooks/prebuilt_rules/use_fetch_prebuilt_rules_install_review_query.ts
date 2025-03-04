1e76dfc5d2ce29b357060d1cc9071a5b0897e1de
Davis Plumlee <davis.plumlee@elastic.co>
2025-03-04 12:58:58 -0500

    adds back merge omition

--- x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/api/hooks/prebuilt_rules/use_fetch_prebuilt_rules_install_review_query.ts
+++ x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/api/hooks/prebuilt_rules/use_fetch_prebuilt_rules_install_review_query.ts
@@ -12,6 +12,7 @@ import { REVIEW_RULE_INSTALLATION_URL } from '../../../../../../common/api/detec
 import type { ReviewRuleInstallationResponseBody } from '../../../../../../common/api/detection_engine/prebuilt_rules';
 import { DEFAULT_QUERY_OPTIONS } from '../constants';
 import { retryOnRateLimitedError } from './retry_on_rate_limited_error';
+import { cappedExponentialBackoff } from './capped_exponential_backoff';
 
 export const REVIEW_RULE_INSTALLATION_QUERY_KEY = ['POST', REVIEW_RULE_INSTALLATION_URL];
 
@@ -28,6 +29,7 @@ export const useFetchPrebuiltRulesInstallReviewQuery = (
       ...DEFAULT_QUERY_OPTIONS,
       ...options,
       retry: retryOnRateLimitedError,
+      retryDelay: cappedExponentialBackoff,
     }
   );
 };
