341e5d520b31309ebd02f164b48f86f83365abb9
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2025-03-04 15:20:28 +0100

    feat(streams): set foundation for schema editor

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/simulation_playground.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/simulation_playground.tsx
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+<<<<<<< HEAD
 import React from 'react';
 import { i18n } from '@kbn/i18n';
 import { EuiFlexItem, EuiSpacer, EuiTab, EuiTabs } from '@elastic/eui';
@@ -14,6 +15,22 @@ import {
   useStreamEnrichmentEvents,
   useStreamsEnrichmentSelector,
 } from './state_management/stream_enrichment_state_machine';
+=======
+import React, { useMemo, useState } from 'react';
+import { i18n } from '@kbn/i18n';
+import { EuiFlexItem, EuiSpacer, EuiTab, EuiTabs, EuiText, useEuiTheme } from '@elastic/eui';
+import {
+  getAdvancedParameters,
+  IngestStreamGetResponse,
+  WiredStreamGetResponse,
+  isWiredStreamGetResponse,
+} from '@kbn/streams-schema';
+import { css } from '@emotion/react';
+import { ProcessorOutcomePreview } from './processor_outcome_preview';
+import { TableColumn, UseProcessingSimulatorReturn } from './hooks/use_processing_simulator';
+import { SchemaEditor } from '../schema_editor';
+import { SchemaField } from '../schema_editor/types';
+>>>>>>> 16bf980cd90 (feat(streams): set foundation for schema editor)
 
 export const SimulationPlayground = () => {
   const isViewingDataPreview = useStreamsEnrichmentSelector((state) =>
@@ -54,10 +71,94 @@ export const SimulationPlayground = () => {
       </EuiFlexItem>
       <EuiSpacer size="m" />
       {isViewingDataPreview && <ProcessorOutcomePreview />}
-      {isViewingDetectedFields &&
-        i18n.translate('xpack.streams.simulationPlayground.div.detectedFieldsLabel', {
-          defaultMessage: 'WIP',
-        })}
+      {isViewingDetectedFields &&<DetectedFieldsEditor
+        definition={definition}
+        isLoading={isLoading}
+        simulation={simulation}
+      />}
     </>
   );
+};
+
+interface DetectedFieldsEditorProps {
+  definition: WiredStreamGetResponse;
+  isLoading: boolean;
+  simulation: UseProcessingSimulatorReturn['simulation'];
+}
+
+const DetectedFieldsEditor = ({ definition, isLoading, simulation }: DetectedFieldsEditorProps) => {
+  const { euiTheme } = useEuiTheme();
+
+  const fields = useMemo(() => {
+    const simulationFields = simulation?.detected_fields ?? [];
+
+    const schemaFields: SchemaField[] = simulationFields.map((field) => {
+      // Detected fields already inherited
+      if ('from' in field) {
+        return {
+          name: field.name,
+          type: field.type,
+          format: field.format,
+          parent: field.from,
+          status: 'inherited',
+        };
+      }
+      // Detected fields already mapped
+      if ('type' in field) {
+        return {
+          name: field.name,
+          type: field.type,
+          format: field.format,
+          parent: definition.stream.name,
+          status: 'mapped',
+        };
+      }
+      // Detected fields still unmapped
+      return {
+        name: field.name,
+        parent: definition.stream.name,
+        status: 'unmapped',
+      };
+    });
+
+    return schemaFields.sort(compareFieldsByStatus);
+  }, [definition, simulation]);
+
+  const unmapField = (fieldName: string) => {};
+
+  const updateField = (field: SchemaField) => {};
+
+  const refreshFields = () => {};
+
+  return (
+    <>
+      <EuiText
+        component="p"
+        color="subdued"
+        size="xs"
+        css={css`
+          margin-bottom: ${euiTheme.size.base};
+        `}
+      >
+        {i18n.translate(
+          'xpack.streams.streamDetailView.managementTab.enrichment.simulationPlayground.detectedFieldsHeadline',
+          { defaultMessage: 'You can review and adjust saved fields further in the Schema Editor.' }
+        )}
+      </EuiText>
+      <SchemaEditor
+        fields={fields}
+        isLoading={isLoading}
+        stream={definition.stream}
+        onFieldUnmap={unmapField}
+        onFieldUpdate={updateField}
+        onRefreshData={refreshFields}
+        withTableActions
+      />
+    </>
+  );
+};
+
+const statusOrder = { inherited: 0, mapped: 1, unmapped: 2 };
+const compareFieldsByStatus = (curr: SchemaField, next: SchemaField) => {
+  return statusOrder[curr.status] - statusOrder[next.status];
 };
