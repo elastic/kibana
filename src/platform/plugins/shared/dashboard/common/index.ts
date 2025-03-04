f1cace89b3a2fa415529036be3c980f58337c8c8
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-03-04 02:45:10 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/platform/plugins/shared/dashboard/common/index.ts
+++ src/platform/plugins/shared/dashboard/common/index.ts
@@ -9,10 +9,7 @@
 
 export type { DashboardCapabilities } from './types';
 
-export type {
-  DashboardPanelMap,
-  DashboardPanelState,
-} from './dashboard_container/types';
+export type { DashboardPanelMap, DashboardPanelState } from './dashboard_container/types';
 
 export {
   type InjectExtractDeps,
