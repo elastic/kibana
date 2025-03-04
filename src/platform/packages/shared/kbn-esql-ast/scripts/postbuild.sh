3cbfa0029202be10c1adf3ec37e04d17b95bbe0c
drewdaemon <drew.tate@elastic.co>
2025-03-03 17:42:53 -0700

    lolz

--- src/platform/packages/shared/kbn-esql-ast/scripts/postbuild.sh
+++ src/platform/packages/shared/kbn-esql-ast/scripts/postbuild.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Print the current working directory
+echo "Current working directory: $(pwd)"
 
 # Check if the parser file exists and add @ts-nocheck
 if [ -f src/antlr/esql_parser.ts ]; then
+  echo "Adding @ts-nocheck to src/antlr/esql_parser.ts"
   sed -i '' -e '1s/^/\/\/ @ts-nocheck\n/' src/antlr/esql_parser.ts
 else
   echo "src/antlr/esql_parser.ts not found!"
@@ -9,6 +14,7 @@ fi
 
 # Check if the lexer file exists and add @ts-nocheck
 if [ -f src/antlr/esql_lexer.ts ]; then
+  echo "Adding @ts-nocheck to src/antlr/esql_lexer.ts"
   sed -i '' -e '1s/^/\/\/ @ts-nocheck\n/' src/antlr/esql_lexer.ts
 else
   echo "src/antlr/esql_lexer.ts not found!"
@@ -16,6 +22,7 @@ fi
 
 # Rename the parser listener file if it exists
 if [ -f src/antlr/esql_parserListener.ts ]; then
+  echo "Renaming src/antlr/esql_parserListener.ts to src/antlr/esql_parser_listener.ts"
   mv src/antlr/esql_parserListener.ts src/antlr/esql_parser_listener.ts
 else
   echo "src/antlr/esql_parserListener.ts not found!"
