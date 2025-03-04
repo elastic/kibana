bc01fb4d720d438b1c136e2d972a395ae5a9c6ec
Milton Hultgren <milton.hultgren@elastic.co>
2025-03-04 22:37:58 +0100

    Draft for ExecutionPlan structure

--- x-pack/platform/plugins/shared/streams/server/lib/streams/state_management/execution_plan.ts
+++ x-pack/platform/plugins/shared/streams/server/lib/streams/state_management/execution_plan.ts
@@ -7,15 +7,24 @@
 
 import { IScopedClusterClient } from '@kbn/core/server';
 import { Logger } from 'elastic-apm-node';
+import { groupBy } from 'lodash';
 import { AssetClient } from '../assets/asset_client';
 import { StreamsStorageClient } from '../service';
 
 // A bunch of types to model based on Joe's list
-type ActionType = 'upsert_component_template';
-
-export interface ElasticsearchAction {
-  type: ActionType;
+interface UpsertComponentTemplateAction {
+  type: 'upsert_component_template';
 }
+
+interface UpsertIndexTemplateAction {
+  type: 'upsert_index_template';
+}
+
+export type ElasticsearchAction = UpsertComponentTemplateAction | UpsertIndexTemplateAction;
+
+type ActionsByType = {
+  [Type in ElasticsearchAction['type']]: ElasticsearchAction[] | undefined;
+};
 
 interface ExecutionPlanDependencies {
   scopedClusterClient: IScopedClusterClient;
@@ -27,17 +36,41 @@ interface ExecutionPlanDependencies {
 
 export class ExecutionPlan {
   private dependencies: ExecutionPlanDependencies;
-  private actions: ElasticsearchAction[] = [];
+  private actionsByType: ActionsByType = {} as ActionsByType;
 
   constructor(dependencies: ExecutionPlanDependencies) {
     this.dependencies = dependencies;
   }
 
   plan(elasticsearchActions: ElasticsearchAction[]) {
-    this.actions = elasticsearchActions;
+    this.actionsByType = groupBy(elasticsearchActions, 'type') as ActionsByType;
   }
 
   execute() {
-    // To do
+    // Unpack actions by type
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    const { upsert_component_template, upsert_index_template, ...rest } = this.actionsByType;
+    assertEmptyObject(rest);
+
+    // Execute actions in the appropriate order
+    if (upsert_component_template) {
+      this.upsertComponentTemplates(upsert_component_template);
+    }
+
+    if (upsert_index_template) {
+      this.upsertIndexTemplates(upsert_index_template);
+    }
   }
+
+  private upsertComponentTemplates(actions: ElasticsearchAction[]) {
+    // Bulk em
+  }
+
+  private upsertIndexTemplates(actions: ElasticsearchAction[]) {
+    // Bulk em
+  }
+}
+
+function assertEmptyObject(object: Record<string, never>) {
+  // This is for type checking only
 }
