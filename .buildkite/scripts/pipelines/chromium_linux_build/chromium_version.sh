773ccc771cdb6e2848262979d9f72f82347d4bdc
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-03-04 20:06:49 +0100

    provide feedback to user on completing build

--- .buildkite/scripts/pipelines/chromium_linux_build/chromium_version.sh
+++ .buildkite/scripts/pipelines/chromium_linux_build/chromium_version.sh
@@ -8,5 +8,5 @@ source .buildkite/scripts/common/util.sh
 echo "---Attempting to compute chromium version for provided puppeteer version"
 
 echo "$(node scripts/chromium_version $PUPPETEER_VERSION)" | grep -i "chromium commit" | awk '{print $5}' | buildkite-agent meta-data set "chromium_commit_hash"
-# echo "$(node scripts/chromium_version $PUPPETEER_VERSION)" | grep -i "chrome version" | awk '{print $5}' | buildkite-agent meta-data set "chromium_version"
-# echo "$(node scripts/chromium_version $PUPPETEER_VERSION)" | grep -i "chromium revision" | awk '{print $5}' | buildkite-agent meta-data set "chromium_revision"
+echo "$(node scripts/chromium_version $PUPPETEER_VERSION)" | grep -i "chrome version" | awk '{print $5}' | buildkite-agent meta-data set "chromium_version"
+echo "$(node scripts/chromium_version $PUPPETEER_VERSION)" | grep -i "chromium revision" | awk '{print $5}' | buildkite-agent meta-data set "chromium_revision"
