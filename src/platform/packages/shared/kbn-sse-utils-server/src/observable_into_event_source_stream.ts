1b83f3a4c914e48b0239ff21e46af7d258ff70a5
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2025-03-04 18:55:46 +0100

    [SSE] Fix EventSource streams

--- src/platform/packages/shared/kbn-sse-utils-server/src/observable_into_event_source_stream.ts
+++ src/platform/packages/shared/kbn-sse-utils-server/src/observable_into_event_source_stream.ts
@@ -16,6 +16,17 @@ import {
 import { ServerSentEvent, ServerSentEventType } from '@kbn/sse-utils/src/events';
 import { catchError, map, Observable, of } from 'rxjs';
 import { PassThrough } from 'stream';
+import type { Zlib } from 'zlib';
+
+class ResponseStream extends PassThrough {
+  private _compressor?: Zlib;
+  setCompressor(compressor: Zlib) {
+    this._compressor = compressor;
+  }
+  flush() {
+    this._compressor?.flush();
+  }
+}
 
 export function observableIntoEventSourceStream(
   source$: Observable<ServerSentEvent>,
@@ -54,21 +65,23 @@ export function observableIntoEventSourceStream(
     }),
     map((event) => {
       const { type, ...rest } = event;
-      return `event: ${type}\ndata: ${JSON.stringify(rest)}\n\n`;
+      return createLine({ event: type, data: rest });
     })
   );
 
-  const stream = new PassThrough();
+  const stream = new ResponseStream();
 
   const intervalId = setInterval(() => {
     // `:` denotes a comment - this is to keep the connection open
     // it will be ignored by the SSE parser on the client
-    stream.write(': keep-alive');
+    stream.write(': keep-alive\n');
   }, 10000);
 
   const subscription = withSerializedErrors$.subscribe({
     next: (line) => {
       stream.write(line);
+      // Make sure to flush the written lines to emit them immediately (instead of waiting for buffer to fill)
+      stream.flush();
     },
     complete: () => {
       stream.end();
@@ -77,13 +90,17 @@ export function observableIntoEventSourceStream(
     error: (error) => {
       clearTimeout(intervalId);
       stream.write(
-        `event:error\ndata: ${JSON.stringify({
-          error: {
-            code: ServerSentEventErrorCode.internalError,
-            message: error.message,
+        createLine({
+          event: 'error',
+          data: {
+            error: {
+              code: ServerSentEventErrorCode.internalError,
+              message: error.message,
+            },
           },
-        })}\n\n`
+        })
       );
+      // No need to flush because we're ending the stream anyway
       stream.end();
     },
   });
@@ -94,4 +111,14 @@ export function observableIntoEventSourceStream(
   });
 
   return stream;
+}
+
+function createLine({ event, data }: { event: string; data: unknown }) {
+  return [
+    `event: ${event}`,
+    `data: ${JSON.stringify(data)}`,
+    // We could also include `id` and `retry` if we see fit in the future.
+    // https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#fields
+    `\n`,
+  ].join(`\n`);
 }
