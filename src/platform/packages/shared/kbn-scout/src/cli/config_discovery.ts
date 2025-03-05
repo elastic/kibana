083783c15e88d4e7674cdc6bc1a5be215a2f0680
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-03-05 20:11:16 +0100

    create .scout dir

--- src/platform/packages/shared/kbn-scout/src/cli/config_discovery.ts
+++ src/platform/packages/shared/kbn-scout/src/cli/config_discovery.ts
@@ -45,6 +45,9 @@ export const discoverPlaywrightConfigs: Command<void> = {
         : `Found Playwright config files in '${pluginsMap.size}' plugins`;
 
     if (pluginsMap.size > 0 && flagsReader.boolean('save')) {
+      if (!Fs.existsSync(SCOUT_OUTPUT_ROOT)) {
+        Fs.mkdirSync(SCOUT_OUTPUT_ROOT, { recursive: true });
+      }
       const scoutConfigsFilePath = resolve(SCOUT_OUTPUT_ROOT, 'scout_playwright_configs.json');
       fs.writeFileSync(
         scoutConfigsFilePath,
