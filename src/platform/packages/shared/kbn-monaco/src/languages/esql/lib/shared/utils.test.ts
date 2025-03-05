aa3572df6d4fbfe945541a7abdb58722fdcfde9a
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-03-05 14:34:06 +0100

    [ES|QL] Fixes the suggestion problem in where for multiline queries

---
+++ src/platform/packages/shared/kbn-monaco/src/languages/esql/lib/shared/utils.test.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+import { offsetRangeToMonacoRange } from './utils';
+
+describe('offsetRangeToMonacoRange', () => {
+  test('should convert offset range to monaco range when the cursor is not at the end', () => {
+    const expression = 'FROM test | WHERE test == | LIMIT 1';
+    const range = { start: 26, end: 26 };
+    const monacoRange = offsetRangeToMonacoRange(expression, range);
+
+    expect(monacoRange).toEqual({
+      startColumn: 26,
+      endColumn: 26,
+      startLineNumber: 1,
+      endLineNumber: 1,
+    });
+  });
+
+  test('should convert offset range to monaco range when the cursor is at the end', () => {
+    const expression = 'FROM test | WHERE test == 1 | LIMIT 1';
+    const range = { start: 37, end: 37 };
+    const monacoRange = offsetRangeToMonacoRange(expression, range);
+
+    expect(monacoRange).toEqual({
+      startColumn: 37,
+      endColumn: 37,
+      startLineNumber: 1,
+      endLineNumber: 1,
+    });
+  });
+
+  test('should convert offset range to monaco range for multiple lines query when the cursor is in not at the end', () => {
+    const expression = 'FROM test \n| WHERE test == \n| LIMIT 1';
+    const range = { start: 27, end: 27 };
+    const monacoRange = offsetRangeToMonacoRange(expression, range);
+
+    expect(monacoRange).toEqual({
+      startColumn: 16,
+      endColumn: 16,
+      startLineNumber: 2,
+      endLineNumber: 2,
+    });
+  });
+
+  test('should convert offset range to monaco range for multiple lines query when the cursor is in at the end', () => {
+    const expression = 'FROM test \n| WHERE test == \n| LIMIT 1';
+    const range = { start: 35, end: 35 };
+    const monacoRange = offsetRangeToMonacoRange(expression, range);
+
+    expect(monacoRange).toEqual({
+      startColumn: 7,
+      endColumn: 7,
+      startLineNumber: 3,
+      endLineNumber: 3,
+    });
+  });
+});
