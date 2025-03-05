9648e79a8d76ad0cbab4189191e6255ba5b7b16f
Paul Tavares <paul.tavares@elastic.co>
2025-03-05 17:15:01 -0500

    Updated card actions to disable menu if user is not able to manage artifact

--- x-pack/solutions/security/plugins/security_solution/public/management/components/artifact_entry_card/components/card_actions_flex_item.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/management/components/artifact_entry_card/components/card_actions_flex_item.tsx
@@ -5,11 +5,19 @@
  * 2.0.
  */
 
-import React, { memo } from 'react';
+import type { ReactNode } from 'react';
+import React, { memo, useMemo } from 'react';
 import type { CommonProps } from '@elastic/eui';
 import { EuiFlexItem } from '@elastic/eui';
+import type { ExceptionListItemSchema } from '@kbn/securitysolution-io-ts-list-types';
+import { useSpaceId } from '../../../../common/hooks/use_space_id';
+import { useEndpointPrivileges } from '../../../../common/components/user_privileges/endpoint';
+import { useIsExperimentalFeatureEnabled } from '../../../../common/hooks/use_experimental_features';
+import { isArtifactGlobal } from '../../../../../common/endpoint/service/artifacts';
+import { useCardArtifact } from './card_artifact_context';
 import type { ActionsContextMenuProps } from '../../actions_context_menu';
 import { ActionsContextMenu } from '../../actions_context_menu';
+import { getArtifactOwnerSpaceIds } from '../../../../../common/endpoint/service/artifacts/utils';
 
 export interface CardActionsFlexItemProps extends Pick<CommonProps, 'data-test-subj'> {
   /** If defined, then an overflow menu will be shown with the actions provided */
@@ -18,9 +26,49 @@ export interface CardActionsFlexItemProps extends Pick<CommonProps, 'data-test-s
 
 export const CardActionsFlexItem = memo<CardActionsFlexItemProps>(
   ({ actions, 'data-test-subj': dataTestSubj }) => {
+    const item = useCardArtifact() as ExceptionListItemSchema;
+    const canManageGlobalArtifacts = useEndpointPrivileges().canManageGlobalArtifacts;
+    const isGlobal = useMemo(() => isArtifactGlobal(item), [item]);
+    const ownerSpaceIds = useMemo(() => getArtifactOwnerSpaceIds(item), [item]);
+    const isSpacesEnabled = useIsExperimentalFeatureEnabled(
+      'endpointManagementSpaceAwarenessEnabled'
+    );
+    const activeSpaceId = useSpaceId();
+
+    const { isDisabled, disabledTooltip } = useMemo<{
+      isDisabled: boolean;
+      disabledTooltip: ReactNode;
+    }>(() => {
+      if (!isSpacesEnabled || canManageGlobalArtifacts) {
+        return { isDisabled: false, disabledTooltip: undefined };
+      }
+
+      if (isGlobal) {
+        return {
+          isDisabled: true,
+          disabledTooltip: 'something here: not allowed to manage global artifacts',
+        };
+      }
+
+      if (!activeSpaceId || !ownerSpaceIds.includes(activeSpaceId)) {
+        return {
+          isDisabled: true,
+          disabledTooltip: 'SOmething here: not allowed to manage shared artifact in active space',
+        };
+      }
+
+      return { isDisabled: false, disabledTooltip: undefined };
+    }, [activeSpaceId, canManageGlobalArtifacts, isGlobal, isSpacesEnabled, ownerSpaceIds]);
+
     return actions && actions.length > 0 ? (
       <EuiFlexItem grow={false}>
-        <ActionsContextMenu items={actions} icon="boxesHorizontal" data-test-subj={dataTestSubj} />
+        <ActionsContextMenu
+          items={actions}
+          icon="boxesHorizontal"
+          isDisabled={isDisabled}
+          disabledTooltip={disabledTooltip}
+          data-test-subj={dataTestSubj}
+        />
       </EuiFlexItem>
     ) : null;
   }
