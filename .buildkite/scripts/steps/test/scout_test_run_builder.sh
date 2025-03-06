8dd9e34ff3288fd0b71ad9daae91269e9e1e5bac
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-03-06 18:06:00 +0100

    fix uploading artifact

--- .buildkite/scripts/steps/test/scout_test_run_builder.sh
+++ .buildkite/scripts/steps/test/scout_test_run_builder.sh
@@ -7,7 +7,8 @@ source .buildkite/scripts/common/util.sh
 
 echo '--- Discover Playwright Configs and upload to Buildkite artifacts'
 node scripts/scout discover-playwright-configs --save
-buildkite-agent artifact upload ".scout/scout_playwright_configs.json:scout_playwright_configs.json"
+cp .scout/scout_playwright_configs.json ./scout_playwright_configs.json
+buildkite-agent artifact upload "scout_playwright_configs.json"
 
 echo '--- Scout Test Run Builder'
 ts-node "$(dirname "${0}")/scout_test_run_builder.ts"
