f42f40b7a9251b72e2a784569c1ca4257c5a5d7d
Nathan Reese <reese.nathan@gmail.com>
2025-03-06 10:39:00 -0700

    [dashboard] fix map center change shows unsaved change after clicking reset

--- x-pack/platform/plugins/shared/maps/public/react_embeddable/initialize_redux_sync.ts
+++ x-pack/platform/plugins/shared/maps/public/react_embeddable/initialize_redux_sync.ts
@@ -219,7 +219,16 @@ export function initializeReduxSync({
         (nextValue: MapCenterAndZoom) => {
           store.dispatch(setGotoWithCenter(nextValue));
         },
-        fastIsEqual,
+        (a, b) => {
+          if (a?.zoom === undefined || b?.zoom === undefined) {
+            return a === b;
+          }
+
+          if (a!.lat !== b!.lat) return false;
+          if (a!.lon !== b!.lon) return false;
+          // Map may not restore reset zoom exactly
+          return Math.abs(a!.zoom - b!.zoom) < 0.05
+        },
       ],
       openTOCDetails: [
         openTOCDetails$,
