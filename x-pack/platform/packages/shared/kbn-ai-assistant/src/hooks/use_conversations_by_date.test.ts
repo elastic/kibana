7095bf5ff6604af25fc3c5e88cdad54b161d053e
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-03-06 18:55:30 -0500

    Fix test (#206590)

--- x-pack/platform/packages/shared/kbn-ai-assistant/src/hooks/use_conversations_by_date.test.ts
+++ x-pack/platform/packages/shared/kbn-ai-assistant/src/hooks/use_conversations_by_date.test.ts
@@ -15,6 +15,8 @@ jest.mock('../utils/date', () => ({
   isValidDateMath: jest.fn(),
 }));
 
+jest.unmock('./use_conversations_by_date');
+
 export const getDisplayedConversation = (conversation: Conversation) => {
   return {
     id: conversation.conversation.id,
@@ -71,6 +73,10 @@ describe('useConversationsByDate', () => {
       return validTimestamps.includes(value);
     });
   });
+
+  // afterEach(() => {
+  //   jest.resetModules();
+  // });
 
   afterAll(() => {
     jest.useRealTimers();
