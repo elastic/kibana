903a1933669f775d66687899af8b3d3590f9319e
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2025-03-07 02:38:18 +1100

    [8.x] [ML] Anomaly Explorer: Fix incorrect overall swim lane height in Anomaly Explorer (#213245) (#213385)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[ML] Anomaly Explorer: Fix incorrect overall swim lane height in
    Anomaly Explorer
    (#213245)](https://github.com/elastic/kibana/pull/213245)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Robert
    Jaszczurek","email":"92210485+rbrtj@users.noreply.github.com"},"sourceCommit":{"committedDate":"2025-03-06T13:33:20Z","message":"[ML]
    Anomaly Explorer: Fix incorrect overall swim lane height in Anomaly
    Explorer (#213245)\n\nFix for:
    https://github.com/elastic/kibana/issues/211017\n\nWe were using legacy
    theme which introduced some margins to the chart.\nHowever, the issue is
    visible from `69.1.0` (introduced in Kibana `9.0`)\nversion of
    `@elastic/charts` as they have fixed heatmap margins and\npaddings -
    https://github.com/elastic/elastic-charts/issues/2577.\n\nAfter the
    fix:\n\n![image](https://github.com/user-attachments/assets/18277a20-3bf0-44b4-adde-c876086c4a01)\n\n![image](https://github.com/user-attachments/assets/19073e4d-b600-4333-8c85-cf7939a071ac)","sha":"ae74cc35a9f35d503412e08d545ff5c76989b391","branchLabelMapping":{"^v9.1.0$":"main","^v8.19.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:fix",":ml","v9.0.0","Team:ML","backport:version","v9.1.0","v8.19.0"],"title":"[ML]
    Anomaly Explorer: Fix incorrect overall swim lane height in Anomaly
    Explorer","number":213245,"url":"https://github.com/elastic/kibana/pull/213245","mergeCommit":{"message":"[ML]
    Anomaly Explorer: Fix incorrect overall swim lane height in Anomaly
    Explorer (#213245)\n\nFix for:
    https://github.com/elastic/kibana/issues/211017\n\nWe were using legacy
    theme which introduced some margins to the chart.\nHowever, the issue is
    visible from `69.1.0` (introduced in Kibana `9.0`)\nversion of
    `@elastic/charts` as they have fixed heatmap margins and\npaddings -
    https://github.com/elastic/elastic-charts/issues/2577.\n\nAfter the
    fix:\n\n![image](https://github.com/user-attachments/assets/18277a20-3bf0-44b4-adde-c876086c4a01)\n\n![image](https://github.com/user-attachments/assets/19073e4d-b600-4333-8c85-cf7939a071ac)","sha":"ae74cc35a9f35d503412e08d545ff5c76989b391"}},"sourceBranch":"main","suggestedTargetBranches":["9.0","8.x"],"targetPullRequestStates":[{"branch":"9.0","label":"v9.0.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/213245","number":213245,"mergeCommit":{"message":"[ML]
    Anomaly Explorer: Fix incorrect overall swim lane height in Anomaly
    Explorer (#213245)\n\nFix for:
    https://github.com/elastic/kibana/issues/211017\n\nWe were using legacy
    theme which introduced some margins to the chart.\nHowever, the issue is
    visible from `69.1.0` (introduced in Kibana `9.0`)\nversion of
    `@elastic/charts` as they have fixed heatmap margins and\npaddings -
    https://github.com/elastic/elastic-charts/issues/2577.\n\nAfter the
    fix:\n\n![image](https://github.com/user-attachments/assets/18277a20-3bf0-44b4-adde-c876086c4a01)\n\n![image](https://github.com/user-attachments/assets/19073e4d-b600-4333-8c85-cf7939a071ac)","sha":"ae74cc35a9f35d503412e08d545ff5c76989b391"}},{"branch":"8.x","label":"v8.19.0","branchLabelMappingKey":"^v8.19.0$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    Co-authored-by: Robert Jaszczurek <92210485+rbrtj@users.noreply.github.com>

--- x-pack/platform/plugins/shared/ml/public/application/explorer/swimlane_container.tsx
+++ x-pack/platform/plugins/shared/ml/public/application/explorer/swimlane_container.tsx
@@ -27,15 +27,7 @@ import type {
   TooltipProps,
   TooltipValue,
 } from '@elastic/charts';
-import {
-  Chart,
-  Heatmap,
-  Position,
-  ScaleType,
-  Settings,
-  Tooltip,
-  LEGACY_LIGHT_THEME,
-} from '@elastic/charts';
+import { Chart, Heatmap, Position, ScaleType, Settings, Tooltip } from '@elastic/charts';
 import moment from 'moment';
 import { i18n } from '@kbn/i18n';
 import type { ChartsPluginStart } from '@kbn/charts-plugin/public';
@@ -198,6 +190,10 @@ export const SwimlaneContainer: FC<SwimlaneProps> = ({
   yAxisWidth,
   onRenderComplete,
 }) => {
+  const {
+    theme: { useChartsBaseTheme },
+  } = chartsService;
+
   const [chartWidth, setChartWidth] = useState<number>(0);
 
   const {
@@ -219,6 +215,8 @@ export const SwimlaneContainer: FC<SwimlaneProps> = ({
     }, RESIZE_THROTTLE_TIME_MS),
     [chartWidth]
   );
+
+  const baseTheme = useChartsBaseTheme();
 
   const swimLanePoints = useMemo(() => {
     const showFilterContext = filterActive === true && swimlaneType === SWIMLANE_TYPE.OVERALL;
@@ -458,8 +456,7 @@ export const SwimlaneContainer: FC<SwimlaneProps> = ({
                       <Tooltip {...tooltipOptions} />
                       <Settings
                         theme={themeOverrides}
-                        // TODO connect to charts.theme service see src/plugins/charts/public/services/theme/README.md
-                        baseTheme={LEGACY_LIGHT_THEME}
+                        baseTheme={baseTheme}
                         onElementClick={onElementClick}
                         onPointerUpdate={handleCursorUpdate}
                         showLegend={showLegend}
