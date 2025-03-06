66a038ec3bd7e0ff8e81586450b64367cc523345
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2025-03-06 12:41:21 +0100

    Combined logic to add customisation in 1 place so that it affects both embeddable and discover

--- src/platform/plugins/shared/discover/public/embeddable/components/saved_search_grid.tsx
+++ src/platform/plugins/shared/discover/public/embeddable/components/saved_search_grid.tsx
@@ -19,7 +19,6 @@ import {
   getRenderCustomToolbarWithElements,
   getDataGridDensity,
   getRowHeight,
-  DEFAULT_PAGINATION_MODE,
 } from '@kbn/unified-data-table';
 import { DiscoverGrid } from '../../components/discover_grid';
 import './saved_search_grid.scss';
@@ -118,13 +117,6 @@ export function DiscoverGridEmbeddable(props: DiscoverGridEmbeddableProps) {
     gridProps.rowHeightState,
   ]);
 
-  const getPaginationConfigAccessor = useProfileAccessor('getPaginationConfig');
-  const paginationModeConfig = useMemo(() => {
-    return getPaginationConfigAccessor(() => ({
-      paginationMode: DEFAULT_PAGINATION_MODE,
-    }))();
-  }, [getPaginationConfigAccessor]);
-
   return (
     <SavedSearchEmbeddableBase
       totalHitCount={undefined} // it will be rendered inside the custom grid toolbar instead
@@ -135,7 +127,6 @@ export function DiscoverGridEmbeddable(props: DiscoverGridEmbeddableProps) {
       <DiscoverGridMemoized
         {...gridProps}
         isPaginationEnabled={!gridProps.isPlainRecord}
-        paginationMode={paginationModeConfig.paginationMode}
         totalHits={props.totalHitCount}
         setExpandedDoc={setExpandedDoc}
         expandedDoc={expandedDoc}
