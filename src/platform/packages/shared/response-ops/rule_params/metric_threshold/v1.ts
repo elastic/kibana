9085c3cb6da88f3b6ba03a24849565e846fa5a2c
lcawl <lcawley@elastic.co>
2025-03-06 16:36:05 -0800

    Fix location of descriptions

--- src/platform/packages/shared/response-ops/rule_params/metric_threshold/v1.ts
+++ src/platform/packages/shared/response-ops/rule_params/metric_threshold/v1.ts
@@ -32,34 +32,11 @@ const METRIC_EXPLORER_AGGREGATIONS = [
 const comparator = Object.values({ ...COMPARATORS, ...LEGACY_COMPARATORS });
 
 const baseCriterion = {
-  /**
-   * The threshold value that is used with the `comparator`.
-   * If the `comparator` is `between`, you must specify the boundary values.
-   */
-  threshold: schema.arrayOf(schema.number()),
-  /**
-   * The comparison function for the threshold.
-   * For example, "is above", "is above or equals", "is below", "is below or equals", "is between", and "outside".
-   */
+  threshold: schema.arrayOf(schema.number(),{ meta: { description: 'The threshold value that is used with the `comparator`. If the `comparator` is `between`, you must specify the boundary values.'}}),
   comparator: oneOfLiterals(comparator),
-  /**
-   * The type of units for the time window: seconds, minutes, hours, or days.
-   */
-  timeUnit: schema.string(),
-  /**
-   * The size of the time window (in `timeUnit` units), which determines how far back to search for documents.
-   * Generally it should be a value higher than the rule check interval to avoid gaps in detection.
-   */
-  timeSize: schema.number(),
-  /**
-   * The threshold value that is used with the `warningComparator`.
-   * If the `warningComparator` is `between`, you must specify the boundary values.
-   */
-  warningThreshold: schema.maybe(schema.arrayOf(schema.number())),
-  /**
-   * The comparison function for the warning threshold.
-   * For example, "is above", "is above or equals", "is below", "is below or equals", "is between", and "outside".
-   */
+  timeUnit: schema.string({meta: { description: 'The type of units for the time window: seconds, minutes, hours, or days.'}}),
+  timeSize: schema.number({meta: { description: 'The size of the time window (in `timeUnit` units), which determines how far back to search for documents. Generally it should be a value higher than the rule check interval to avoid gaps in detection.'}}),
+  warningThreshold: schema.maybe(schema.arrayOf(schema.number({meta: { description: 'The threshold value that is used with the `warningComparator`. If the `warningComparator` is `between`, you must specify the boundary values.'}}))),
   warningComparator: schema.maybe(oneOfLiterals(comparator)),
 };
 
@@ -89,11 +66,6 @@ const customCriterion = schema.object({
     schema.oneOf([
       schema.object({
         name: schema.string(),
-        /**
-         * An aggregation to gather data for the rule.
-         * For example, find the average, highest or lowest value of a numeric field.
-         * Or use a cardinality aggregation to find the approximate number of unique values in a field. 
-         */
         aggType: oneOfLiterals(['avg', 'sum', 'max', 'min', 'cardinality']),
         field: schema.string(),
         filter: schema.never(),
@@ -113,33 +85,16 @@ const customCriterion = schema.object({
 export const metricThresholdRuleParamsSchema = schema.object(
   {
     criteria: schema.arrayOf(schema.oneOf([countCriterion, nonCountCriterion, customCriterion])),
-    /**
-     * Create an alert for every unique value of the specified fields.
-     * For example, you can create a rule per host or every mount point of each host.
-     *
-     * IMPORTANT: If you include the same field in both the `filterQuery` and `groupBy`, you might receive fewer results than you expect.
-     * For example, if you filter by `cloud.region: us-east`, grouping by `cloud.region` will have no effect because the filter query can match only one region.
-     */
-    groupBy: schema.maybe(schema.oneOf([schema.string(), schema.arrayOf(schema.string())])),
-    /**
-     * A query that limits the scope of the rule.
-      The rule evaluates only metric data that matches the query.
-     */
+    groupBy: schema.maybe(schema.oneOf([schema.string(), schema.arrayOf(schema.string())],{meta: { description: 'Create an alert for every unique value of the specified fields. For example, you can create a rule per host or every mount point of each host. IMPORTANT: If you include the same field in both the `filterQuery` and `groupBy`, you might receive fewer results than you expect. For example, if you filter by `cloud.region: us-east`, grouping by `cloud.region` will have no effect because the filter query can match only one region.'}})),
     filterQuery: schema.maybe(
       schema.string({
         validate: validateIsStringElasticsearchJSONFilter,
+        meta: { description: 'A query that limits the scope of the rule. The rule evaluates only metric data that matches the query.'}
       })
     ),
     sourceId: schema.string(),
-    /**
-     * If true, an alert occurs if the metrics do not report any data over the expected period or if the query fails.
-     */
-    alertOnNoData: schema.maybe(schema.boolean()),
-    /**
-     * If true, an alert occurs if a group that previously reported metrics does not report them again over the expected time period.
-     * This check is not recommended for dynamically scaling infrastructures that might rapidly start and stop nodes automatically.
-     */
-    alertOnGroupDisappear: schema.maybe(schema.boolean()),
+    alertOnNoData: schema.maybe(schema.boolean({meta: { description: 'If true, an alert occurs if the metrics do not report any data over the expected period or if the query fails.'}})),
+    alertOnGroupDisappear: schema.maybe(schema.boolean({meta: { description: 'If true, an alert occurs if a group that previously reported metrics does not report them again over the expected time period. This check is not recommended for dynamically scaling infrastructures that might rapidly start and stop nodes automatically.'}})),
   },
   { unknowns: 'allow' }
 );
