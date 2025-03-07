991c7ba2884c05f328fdbfcc31c5254c4e9953b1
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-03-07 16:40:26 +0100

    tweak dispatch/subscribe

--- src/plugins/event_bus/public/plugin.ts
+++ src/plugins/event_bus/public/plugin.ts
@@ -9,18 +9,16 @@
 
 import type { CoreSetup, CoreStart, Plugin } from '@kbn/core/public';
 
-import { eventBus$ } from './event_bus';
+import { dispatch, subscribe } from './event_bus';
 import type { EventBusPluginSetup, EventBusPluginStart } from './types';
 
 export class EventBusPlugin implements Plugin<EventBusPluginSetup, EventBusPluginStart> {
   public setup(core: CoreSetup): EventBusPluginSetup {
-    return {
-      eventBus$,
-    };
+    return { dispatch, subscribe };
   }
 
   public start(core: CoreStart): EventBusPluginStart {
-    return {};
+    return { dispatch, subscribe };
   }
 
   public stop() {}
