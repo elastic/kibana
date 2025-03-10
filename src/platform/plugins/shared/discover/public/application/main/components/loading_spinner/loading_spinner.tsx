c5e92c6212d572c5db553463cca51bea75e4657a
Ania Kowalska <ania.kowalska@elastic.co>
2025-03-10 12:53:22 +0100

    loading spinner

--- src/platform/plugins/shared/discover/public/application/main/components/loading_spinner/loading_spinner.tsx
+++ src/platform/plugins/shared/discover/public/application/main/components/loading_spinner/loading_spinner.tsx
@@ -8,24 +8,11 @@
  */
 
 import React from 'react';
-import type { UseEuiTheme } from '@elastic/eui';
-import { EuiLoadingSpinner, EuiTitle, EuiSpacer } from '@elastic/eui';
+import { EuiLoadingSpinner, EuiTitle, EuiSpacer, type UseEuiTheme } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { css } from '@emotion/react';
 
 export function LoadingSpinner() {
-  const loadingSpinnerCss = ({ euiTheme }: UseEuiTheme) => css`
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    padding: ${euiTheme.size.l} 0;
-    background-color: ${euiTheme.colors.backgroundBasePlain};
-    z-index: 3;
-  `;
-
   return (
     <div className="dscLoading" css={loadingSpinnerCss}>
       <EuiTitle size="s" data-test-subj="loadingSpinnerText">
@@ -38,3 +25,15 @@ export function LoadingSpinner() {
     </div>
   );
 }
+
+const loadingSpinnerCss = ({ euiTheme }: UseEuiTheme) => css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  padding: ${euiTheme.size.l} 0;
+  background-color: ${euiTheme.colors.backgroundBasePlain};
+  z-index: 3;
+`;
