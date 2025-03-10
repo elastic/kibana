fdf64794877edea664ee2d405e8815584f2499ca
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-03-10 19:34:54 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/security/plugins/elastic_assistant/server/lib/langchain/graphs/default_assistant_graph/nodes/agent_outcome_parser.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/server/lib/langchain/graphs/default_assistant_graph/nodes/agent_outcome_parser.ts
@@ -10,12 +10,14 @@ import { AgentAction, AgentFinish } from 'langchain/agents';
 import { v4 as uuidv4 } from 'uuid';
 import type { Logger } from '@kbn/logging';
 
-export const AgentOutcomeParser = ({ logger }: { logger: Logger }) => (item: unknown | unknown[]): BaseMessage => {
+export const AgentOutcomeParser =
+  ({ logger }: { logger: Logger }) =>
+  (item: unknown | unknown[]): BaseMessage => {
     if (isAgentFinish(item)) {
-        return parseAgentFinish(item);
+      return parseAgentFinish(item);
     }
     if (isAgentAction(item)) {
-        return parseAgentAction(item);
+      return parseAgentAction(item);
     }
 
     logger.error(`Unable to parse agent outcome: ${JSON.stringify(item)}`);
@@ -25,60 +27,64 @@ export const AgentOutcomeParser = ({ logger }: { logger: Logger }) => (item: unk
   };
 
 const parseAgentAction = (item: AgentAction | AgentAction[]): BaseMessage => {
-    if(Array.isArray(item)) {
-        return new AIMessage({
-            content: item.map(i=>i.log).join('\n'),
-            tool_calls: item.map(i=>({
-                name: i.tool,
-                args: i.toolInput as Record<string, unknown>,
-                id: "toolCallId" in i ? i.toolCallId as string : uuidv4()
-            }))
-        })
-    }
-
+  if (Array.isArray(item)) {
     return new AIMessage({
-        content: item.log,
-        tool_calls: [
-            {
-                name: item.tool,
-                args: item.toolInput as Record<string, unknown>,
-                id: "toolCallId" in item ? item.toolCallId as string : uuidv4()
-            }
-        ]
+      content: item.map((i) => i.log).join('\n'),
+      tool_calls: item.map((i) => ({
+        name: i.tool,
+        args: i.toolInput as Record<string, unknown>,
+        id: 'toolCallId' in i ? (i.toolCallId as string) : uuidv4(),
+      })),
     });
-}
+  }
+
+  return new AIMessage({
+    content: item.log,
+    tool_calls: [
+      {
+        name: item.tool,
+        args: item.toolInput as Record<string, unknown>,
+        id: 'toolCallId' in item ? (item.toolCallId as string) : uuidv4(),
+      },
+    ],
+  });
+};
 
 const parseAgentFinish = (item: AgentFinish | AgentFinish[]): BaseMessage => {
-    if(Array.isArray(item)) {
-        return new AIMessage({
-            content: item.map(i=>i.returnValues.output).join('\n'),
-        })
-    }
-
+  if (Array.isArray(item)) {
     return new AIMessage({
-        content: item.returnValues.output,
+      content: item.map((i) => i.returnValues.output).join('\n'),
     });
-}
+  }
+
+  return new AIMessage({
+    content: item.returnValues.output,
+  });
+};
 
 const isAgentFinish = (item: unknown | unknown[]): item is AgentFinish | AgentFinish[] => {
-    if(Array.isArray(item)) {
-        return item.every(isAgentFinish);
-    }
-    if (item !== undefined && typeof item === 'object') {
-        const agentFinish = item as AgentFinish;
-        return agentFinish.returnValues !== undefined && agentFinish.log !== undefined;
-    }
-    return false;
-}
+  if (Array.isArray(item)) {
+    return item.every(isAgentFinish);
+  }
+  if (item !== undefined && typeof item === 'object') {
+    const agentFinish = item as AgentFinish;
+    return agentFinish.returnValues !== undefined && agentFinish.log !== undefined;
+  }
+  return false;
+};
 
 const isAgentAction = (item: unknown | unknown[]): item is AgentAction | AgentAction[] => {
-    if(Array.isArray(item)) {
-        return item.every(isAgentAction);
-    }
-    if (item !== undefined && typeof item === 'object') {
-        const agentAction = item as AgentAction;
-        return agentAction.tool !== undefined && agentAction.toolInput !== undefined && agentAction.log !== undefined;
-    }
+  if (Array.isArray(item)) {
+    return item.every(isAgentAction);
+  }
+  if (item !== undefined && typeof item === 'object') {
+    const agentAction = item as AgentAction;
+    return (
+      agentAction.tool !== undefined &&
+      agentAction.toolInput !== undefined &&
+      agentAction.log !== undefined
+    );
+  }
 
   return false;
 };
