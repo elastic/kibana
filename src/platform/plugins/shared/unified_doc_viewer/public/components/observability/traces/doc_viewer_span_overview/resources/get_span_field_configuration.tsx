ac5ce2ddd986d63f66f6a908c7e448208047f719
iblancof <irene.blanco@elastic.co>
2025-03-10 12:10:27 +0100

    Remove string casting left behind

--- src/platform/plugins/shared/unified_doc_viewer/public/components/observability/traces/doc_viewer_span_overview/resources/get_span_field_configuration.tsx
+++ src/platform/plugins/shared/unified_doc_viewer/public/components/observability/traces/doc_viewer_span_overview/resources/get_span_field_configuration.tsx
@@ -64,7 +64,7 @@ export const getSpanFieldConfiguration = (
       content: (value) =>
         value && (
           <DependencyNameLink
-            dependencyName={value as string}
+            dependencyName={value}
             environment={attributes[SERVICE_ENVIRONMENT_FIELD]}
           />
         ),
