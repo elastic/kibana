82ced65881c90f621956812d98be42ba84f8bb60
Tre' Seymour <wayne.seymour@elastic.co>
2025-03-10 12:31:44 +0000

    [FTR] Retrying Without Retry Services

--- x-pack/test/api_integration/apis/slos/helper/wait_for_index_state.ts
+++ x-pack/test/api_integration/apis/slos/helper/wait_for_index_state.ts
@@ -7,10 +7,7 @@
 
 import type { Client } from '@elastic/elasticsearch';
 import type { AggregationsAggregate, SearchResponse } from '@elastic/elasticsearch/lib/api/types';
-import { retryForSuccess } from '@kbn/ftr-common-functional-services';
-import { ToolingLog } from '@kbn/tooling-log';
-
-const debugLog = ToolingLog.bind(ToolingLog, { level: 'debug', writeTo: process.stdout });
+import { Console, Effect } from 'effect';
 
 export async function waitForDocumentInIndex<T>({
   esClient,
@@ -21,22 +18,27 @@ export async function waitForDocumentInIndex<T>({
   indexName: string;
   docCountTarget?: number;
 }): Promise<SearchResponse<T, Record<string, AggregationsAggregate>>> {
-  return await retryForSuccess(new debugLog({ context: 'waitForDocumentInIndex' }), {
-    timeout: 20_000,
-    methodName: 'waitForDocumentInIndex',
-    block: async () => {
-      const response = await esClient.search<T>({ index: indexName, rest_total_hits_as_int: true });
-      if (
-        !response.hits.total ||
-        typeof response.hits.total !== 'number' ||
-        response.hits.total < docCountTarget
-      ) {
-        throw new Error('No hits found');
-      }
-      return response;
-    },
-    retryCount: 10,
-  });
+  const search = () =>
+    esClient.search<T>({ index: indexName, rest_total_hits_as_int: true }).then(inspect);
+
+  function inspect(response) {
+    if (
+      !response.hits.total ||
+      typeof response.hits.total !== 'number' ||
+      response.hits.total < docCountTarget
+    )
+      throw new Error('No hits found');
+
+    return response;
+  }
+
+  return await Effect.runPromise(
+    Effect.tryPromise(search).pipe(
+      Effect.retry({ times: 100 }),
+      Effect.timeout('90 seconds'),
+      Effect.catchAll(Console.error)
+    )
+  );
 }
 
 export async function waitForIndexToBeEmpty<T>({
@@ -46,17 +48,21 @@ export async function waitForIndexToBeEmpty<T>({
   esClient: Client;
   indexName: string;
 }): Promise<SearchResponse<T, Record<string, AggregationsAggregate>>> {
-  return await retryForSuccess(new debugLog({ context: 'waitForIndexToBeEmpty' }), {
-    timeout: 20_000,
-    methodName: 'waitForIndexToBeEmpty',
-    block: async () => {
-      const response = await esClient.search<T>({ index: indexName, rest_total_hits_as_int: true });
-      // @ts-expect-error upgrade typescript v5.1.6
-      if (response.hits.total != null && response.hits.total > 0) {
-        throw new Error(`Found ${response.hits.total} docs.`);
-      }
-      return response;
-    },
-    retryCount: 10,
-  });
+  const search = () =>
+    esClient.search<T>({ index: indexName, rest_total_hits_as_int: true }).then(inspect);
+
+  function inspect(response) {
+    if (response.hits.total != null && response.hits.total > 0)
+      throw new Error(`Found ${response.hits.total} docs.`);
+
+    return response;
+  }
+
+  return await Effect.runPromise(
+    Effect.tryPromise(search).pipe(
+      Effect.retry({ times: 100 }),
+      Effect.timeout('2 minutes'),
+      Effect.catchAll(Console.error)
+    )
+  );
 }
--- x-pack/test/apm_api_integration/tests/alerts/helpers/wait_for_alerts_for_rule.ts
+++ x-pack/test/apm_api_integration/tests/alerts/helpers/wait_for_alerts_for_rule.ts
@@ -7,21 +7,19 @@
 
 import type { Client } from '@elastic/elasticsearch';
 import type { AggregationsAggregate, SearchResponse } from '@elastic/elasticsearch/lib/api/types';
-import { ToolingLog } from '@kbn/tooling-log';
-import { retryForSuccess } from '@kbn/ftr-common-functional-services';
+import { Console, Effect } from 'effect';
 import {
   APM_ALERTS_INDEX,
   ApmAlertFields,
 } from '../../../../api_integration/deployment_agnostic/apis/observability/apm/alerts/helpers/alerting_helper';
-
-const debugLog = ToolingLog.bind(ToolingLog, { level: 'debug', writeTo: process.stdout });
-
 async function getAlertByRuleId({ es, ruleId }: { es: Client; ruleId: string }) {
   const response = (await es.search({
     index: APM_ALERTS_INDEX,
-    query: {
-      term: {
-        'kibana.alert.rule.uuid': ruleId,
+    body: {
+      query: {
+        term: {
+          'kibana.alert.rule.uuid': ruleId,
+        },
       },
     },
   })) as SearchResponse<ApmAlertFields, Record<string, AggregationsAggregate>>;
@@ -38,17 +36,20 @@ export async function waitForAlertsForRule({
   ruleId: string;
   minimumAlertCount?: number;
 }) {
-  return await retryForSuccess(new debugLog({ context: 'waitForAlertsForRule' }), {
-    timeout: 20_000,
-    methodName: 'waitForAlertsForRule',
-    block: async () => {
-      const alerts = await getAlertByRuleId({ es, ruleId });
+  const getAlerts = () =>
+    getAlertByRuleId({ es, ruleId }).then((alerts) => {
       const actualAlertCount = alerts.length;
       if (actualAlertCount < minimumAlertCount)
         throw new Error(`Expected ${minimumAlertCount} but got ${actualAlertCount} alerts`);
 
       return alerts;
-    },
-    retryCount: 5,
-  });
+    });
+
+  return await Effect.runPromise(
+    Effect.tryPromise(getAlerts).pipe(
+      Effect.retry({ times: 100 }),
+      Effect.timeout('2 minutes'),
+      Effect.catchAll(Console.error)
+    )
+  );
 }
