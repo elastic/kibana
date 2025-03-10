a7452ac74fa74044e2e6a97bd12c753b4224a756
jennypavlova <dzheni.pavlova@elastic.co>
2025-03-10 19:47:07 +0100

    [APM] Fix: Span Links with OTel data (#212806)
    Closes #212796
    
    This PR fixes OTel span links
    
    ## Testing
    
    - Using the edge oblt go to Service inventory and find the `accounting`
    service and click on it
    - Click on the transactions tab and scroll to the waterfall
    - Click on the Span link inside the Span
    - Navigate to the linked service
    - Then try the navigation back by finding the span link again in the
    waterfall and go back to the accounting service / its trace
    
    https://github.com/user-attachments/assets/83bb6ec3-86d5-45ad-8b81-6df73751fa31
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 662c30a260f676c9e1180e7d413553d0166726d7)
    
    # Conflicts:
    #	x-pack/solutions/observability/plugins/apm/server/routes/span_links/get_linked_children.ts
    #	x-pack/solutions/observability/plugins/apm/server/routes/span_links/get_linked_parents.ts

--- x-pack/solutions/observability/plugins/apm/server/routes/span_links/get_linked_parents.ts
+++ x-pack/solutions/observability/plugins/apm/server/routes/span_links/get_linked_parents.ts
@@ -4,14 +4,18 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { rangeQuery } from '@kbn/observability-plugin/server';
+import { existsQuery, rangeQuery } from '@kbn/observability-plugin/server';
 import { ProcessorEvent } from '@kbn/observability-plugin/common';
+import { unflattenKnownApmEventFields } from '@kbn/apm-data-access-plugin/server/utils';
+import { asMutableArray } from '../../../common/utils/as_mutable_array';
 import {
   SPAN_ID,
   SPAN_LINKS,
   TRACE_ID,
   TRANSACTION_ID,
   PROCESSOR_EVENT,
+  LINKS_SPAN_ID,
+  LINKS_TRACE_ID,
 } from '../../../common/es_fields/apm';
 import type { SpanRaw } from '../../../typings/es_schemas/raw/span_raw';
 import type { TransactionRaw } from '../../../typings/es_schemas/raw/transaction_raw';
@@ -32,12 +36,19 @@ export async function getLinkedParentsOfSpan({
   end: number;
   processorEvent: ProcessorEvent;
 }) {
+  const optionalFields = asMutableArray([LINKS_SPAN_ID, LINKS_TRACE_ID] as const);
+  const events =
+    processorEvent === ProcessorEvent.span
+      ? [ProcessorEvent.span]
+      : [processorEvent, ProcessorEvent.span];
+
   const response = await apmEventClient.search('get_linked_parents_of_span', {
     apm: {
-      events: [processorEvent],
+      events,
     },
     _source: [SPAN_LINKS],
     body: {
+      fields: [...optionalFields],
       track_total_hits: false,
       size: 1,
       query: {
@@ -45,7 +56,12 @@ export async function getLinkedParentsOfSpan({
           filter: [
             ...rangeQuery(start, end),
             { term: { [TRACE_ID]: traceId } },
-            { exists: { field: SPAN_LINKS } },
+            {
+              bool: {
+                should: [...existsQuery(SPAN_LINKS), ...existsQuery(LINKS_SPAN_ID)],
+                minimum_should_match: 1,
+              },
+            },
             { term: { [PROCESSOR_EVENT]: processorEvent } },
             ...(processorEvent === ProcessorEvent.transaction
               ? [{ term: { [TRANSACTION_ID]: spanId } }]
@@ -53,10 +69,20 @@ export async function getLinkedParentsOfSpan({
           ],
         },
       },
-    },
+    }
   });
 
   const source = response.hits.hits?.[0]?._source as Pick<TransactionRaw | SpanRaw, 'span'>;
+  const fields = unflattenKnownApmEventFields(response.hits.hits?.[0]?.fields);
+  const spanLinksFromFields =
+    fields?.links?.span_id && fields?.links?.trace_id
+      ? [
+          {
+            trace: { id: fields.links.trace_id as string },
+            span: { id: fields.links.span_id as string },
+          },
+        ]
+      : [];
 
-  return source?.span?.links || [];
+  return source?.span?.links ?? spanLinksFromFields;
 }
