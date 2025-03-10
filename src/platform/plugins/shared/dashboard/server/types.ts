05b0f7bebcd20425b20b089c3127b98b9c5e96a7
Nick Peihl <nick.peihl@elastic.co>
2025-03-10 16:56:43 -0400

    Revert "Expose content management request method on Dashboard server contract"
    This reverts commit b3d4e486ee015f69f303202352549cd621247ab9.

--- src/platform/plugins/shared/dashboard/server/types.ts
+++ src/platform/plugins/shared/dashboard/server/types.ts
@@ -12,5 +12,41 @@ import { ContentManagementServerSetup } from '@kbn/content-management-plugin/ser
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface DashboardPluginSetup {}
 export interface DashboardPluginStart {
+  /**
+   * Use contentClient.getForRequest to get a scoped client to perform CRUD and search operations for dashboards using the methods available in the {@link DashboardStorage} class.
+   *
+   * @example
+   * Get a dashboard client for the current request
+   * ```ts
+   * // dashboardClient is scoped to the current user
+   * // specifying the version is recommended to return a consistent result
+   * const dashboardClient = plugins.dashboard.contentClient.getForRequest({ requestHandlerContext, request, version: 3 });
+   *
+   * const { search, create, update, delete: deleteDashboard } = dashboardClient;
+   * ```
+   *
+   * @example
+   * Search using {@link DashboardStorage#search}
+   * ```ts
+   * const dashboardList = await search({ text: 'my dashboard' }, { spaces: ['default'] } });
+   * ```
+   * @example
+   * Create a new dashboard using {@link DashboardCreateIn}
+   * ```ts
+   * const newDashboard = await create({ attributes: { title: 'My Dashboard' } });
+   * ```
+   *
+   * @example
+   * Update an existing dashboard using {@link DashboardUpdateIn}
+   * ```ts
+   * const updatedDashboard = await update({ id: 'dashboard-id', attributes: { title: 'My Updated Dashboard' } });
+   * ```
+   *
+   * @example
+   * Delete an existing dashboard using {@link DashboardDeleteIn}
+   * ```ts
+   * deleteDashboard({ id: 'dashboard-id' });
+   * ```
+   */
   contentClient?: ReturnType<ContentManagementServerSetup['register']>['contentClient'];
 }
