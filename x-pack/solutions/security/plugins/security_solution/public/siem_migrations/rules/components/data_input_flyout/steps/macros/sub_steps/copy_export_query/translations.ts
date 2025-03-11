f6626dc59eb0d2446eab1f69ce785ac2821d5571
Sergi Massaneda <sergi.massaneda@elastic.co>
2025-03-11 17:49:28 +0100

    UI changes in the upload rules data flyout

--- x-pack/solutions/security/plugins/security_solution/public/siem_migrations/rules/components/data_input_flyout/steps/macros/sub_steps/copy_export_query/translations.ts
+++ x-pack/solutions/security/plugins/security_solution/public/siem_migrations/rules/components/data_input_flyout/steps/macros/sub_steps/copy_export_query/translations.ts
@@ -9,7 +9,7 @@ import { i18n } from '@kbn/i18n';
 
 export const MACROS_DATA_INPUT_COPY_TITLE = i18n.translate(
   'xpack.securitySolution.siemMigrations.rules.dataInputFlyout.macros.copyExportQuery.title',
-  { defaultMessage: 'Copy macros query' }
+  { defaultMessage: 'Export macros' }
 );
 
 export const MACROS_DATA_INPUT_COPY_DESCRIPTION_SECTION = i18n.translate(
