7ada4958d6f8ab975122ad8383a4532eda9a5d2c
Janki Salvi <jankigaurav.salvi@elastic.co>
2025-03-12 10:08:40 +0000

    resolve snooze rule route internal conflict

--- x-pack/platform/plugins/shared/alerting/server/routes/rule/apis/snooze/internal/snooze_rule_route.ts
+++ x-pack/platform/plugins/shared/alerting/server/routes/rule/apis/snooze/internal/snooze_rule_route.ts
@@ -11,12 +11,11 @@ import {
   snoozeBodyInternalSchema,
   snoozeParamsInternalSchema,
 } from '../../../../../../common/routes/rule/apis/snooze';
-import { type ILicenseState, RuleMutedError } from '../../../../../lib';
+import type { ILicenseState } from '../../../../../lib';
+import { RuleMutedError } from '../../../../../lib';
 import { verifyAccessAndContext } from '../../../../lib';
-import {
-  type AlertingRequestHandlerContext,
-  INTERNAL_ALERTING_SNOOZE_RULE,
-} from '../../../../../types';
+import type { AlertingRequestHandlerContext } from '../../../../../types';
+import { INTERNAL_ALERTING_SNOOZE_RULE } from '../../../../../types';
 import { transformSnoozeBodyV1 } from './transforms';
 import { DEFAULT_ALERTING_ROUTE_SECURITY } from '../../../../constants';
 
