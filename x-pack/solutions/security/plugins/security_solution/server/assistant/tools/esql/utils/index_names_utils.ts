2799c8646665183037b6a6bcda26c479db6f0bc5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-03-12 19:24:50 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/security/plugins/security_solution/server/assistant/tools/esql/utils/index_names_utils.ts
+++ x-pack/solutions/security/plugins/security_solution/server/assistant/tools/esql/utils/index_names_utils.ts
@@ -1,22 +1,28 @@
-export const generateIndexNamesWithWildcards = (fullIndexNames: string[]): string[] => {
-    const result = new Set(fullIndexNames);
-    const partsList = fullIndexNames.map(str => str.split('.'));
-    const occurrences = new Map();
-    
-    for (const parts of partsList) {
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
 
-        for (let mask = 1; mask < (1 << parts.length); mask++) { // eslint-disable-line no-bitwise
-            let wildcardVersion = parts.map((part, idx) => (mask & (1 << idx)) ? '*' : part); // eslint-disable-line no-bitwise
-            let wildcardStr = wildcardVersion.join('.');
-            occurrences.set(wildcardStr, (occurrences.get(wildcardStr) || 0) + 1);
-        }
+export const generateIndexNamesWithWildcards = (fullIndexNames: string[]): string[] => {
+  const result = new Set(fullIndexNames);
+  const partsList = fullIndexNames.map((str) => str.split('.'));
+  const occurrences = new Map();
+
+  for (const parts of partsList) {
+    for (let mask = 1; mask < 1 << parts.length; mask++) {
+      const wildcardVersion = parts.map((part, idx) => (mask & (1 << idx) ? '*' : part)); // eslint-disable-line no-bitwise
+      const wildcardStr = wildcardVersion.join('.');
+      occurrences.set(wildcardStr, (occurrences.get(wildcardStr) || 0) + 1);
     }
-    
-    for (const [wildcardStr, count] of occurrences.entries()) {
-        if (count > 1) {
-            result.add(wildcardStr);
-        }
+  }
+
+  for (const [wildcardStr, count] of occurrences.entries()) {
+    if (count > 1) {
+      result.add(wildcardStr);
     }
-    
-    return Array.from(result).slice(0, fullIndexNames.length * 3);
-}
+  }
+
+  return Array.from(result).slice(0, fullIndexNames.length * 3);
+};
