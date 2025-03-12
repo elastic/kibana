e0a1521be7e27cd88c6aef370adc99ae3bb28b84
paulina.shakirova <paulina.shakirova@elastic.co>
2025-03-12 18:18:20 +0100

    Custom link colour option top banner

--- x-pack/platform/plugins/private/banners/public/plugin.test.tsx
+++ x-pack/platform/plugins/private/banners/public/plugin.test.tsx
@@ -17,6 +17,7 @@ const createBannerConfig = (parts: Partial<BannerConfiguration> = {}): BannerCon
   placement: 'disabled',
   textContent: 'foo',
   textColor: '#FFFFFF',
+  linkColor: '#61A2FF',
   backgroundColor: '#000000',
   ...parts,
 });
