76b512b9360922ee13c6def18c8b5fb2bb3f2963
Georgiana-Andreea Onoleață <georgiana.onoleata@elastic.co>
2025-03-12 19:23:40 +0200

    [8.x] [ResponseOps] consistent-type-imports linting rule for RO packages/plugins - PR2 (#212366) (#214177)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[ResponseOps] consistent-type-imports linting rule for RO
    packages/plugins - PR2
    (#212366)](https://github.com/elastic/kibana/pull/212366)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Georgiana-Andreea
    Onoleață","email":"georgiana.onoleata@elastic.co"},"sourceCommit":{"committedDate":"2025-03-12T13:29:44Z","message":"[ResponseOps]
    consistent-type-imports linting rule for RO packages/plugins - PR2
    (#212366)\n\n- Enabled @typescript-eslint/consistent-type-imports eslint
    rule for\nResponseOps packages and plugins:\n- this rule ensures that
    imports used only for type declarations are\nconsistently written using
    import type syntax\n \n- fixed eslint errors for:\n -
    `x-pack/platform/plugins/shared/actions`\n -
    `x-pack/platform/plugins/shared/stack_alerts`\n -
    `x-pack/platform/plugins/shared/stack_connectors`\n -
    `x-pack/platform/plugins/shared/triggers_actions_ui`\n\n---------\n\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"6f07d18265122b0868bb04875104cd1b55c3423a","branchLabelMapping":{"^v9.1.0$":"main","^v8.19.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","Team:ResponseOps","backport:version","v9.1.0","v8.19.0"],"title":"[ResponseOps]
    consistent-type-imports linting rule for RO packages/plugins -
    PR2","number":212366,"url":"https://github.com/elastic/kibana/pull/212366","mergeCommit":{"message":"[ResponseOps]
    consistent-type-imports linting rule for RO packages/plugins - PR2
    (#212366)\n\n- Enabled @typescript-eslint/consistent-type-imports eslint
    rule for\nResponseOps packages and plugins:\n- this rule ensures that
    imports used only for type declarations are\nconsistently written using
    import type syntax\n \n- fixed eslint errors for:\n -
    `x-pack/platform/plugins/shared/actions`\n -
    `x-pack/platform/plugins/shared/stack_alerts`\n -
    `x-pack/platform/plugins/shared/stack_connectors`\n -
    `x-pack/platform/plugins/shared/triggers_actions_ui`\n\n---------\n\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"6f07d18265122b0868bb04875104cd1b55c3423a"}},"sourceBranch":"main","suggestedTargetBranches":["8.x"],"targetPullRequestStates":[{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/212366","number":212366,"mergeCommit":{"message":"[ResponseOps]
    consistent-type-imports linting rule for RO packages/plugins - PR2
    (#212366)\n\n- Enabled @typescript-eslint/consistent-type-imports eslint
    rule for\nResponseOps packages and plugins:\n- this rule ensures that
    imports used only for type declarations are\nconsistently written using
    import type syntax\n \n- fixed eslint errors for:\n -
    `x-pack/platform/plugins/shared/actions`\n -
    `x-pack/platform/plugins/shared/stack_alerts`\n -
    `x-pack/platform/plugins/shared/stack_connectors`\n -
    `x-pack/platform/plugins/shared/triggers_actions_ui`\n\n---------\n\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"6f07d18265122b0868bb04875104cd1b55c3423a"}},{"branch":"8.x","label":"v8.19.0","branchLabelMappingKey":"^v8.19.0$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/plugins/shared/actions/server/routes/legacy/list_action_types.test.ts
+++ x-pack/platform/plugins/shared/actions/server/routes/legacy/list_action_types.test.ts
@@ -10,7 +10,7 @@ import { httpServiceMock } from '@kbn/core/server/mocks';
 import { licenseStateMock } from '../../lib/license_state.mock';
 import { verifyApiAccess } from '../../lib';
 import { mockHandlerArguments } from './_mock_handler_arguments';
-import { LicenseType } from '@kbn/licensing-plugin/server';
+import type { LicenseType } from '@kbn/licensing-plugin/server';
 import { actionsClientMock } from '../../mocks';
 import { trackLegacyRouteUsage } from '../../lib/track_legacy_route_usage';
 import { usageCountersServiceMock } from '@kbn/usage-collection-plugin/server/usage_counters/usage_counters_service.mock';
