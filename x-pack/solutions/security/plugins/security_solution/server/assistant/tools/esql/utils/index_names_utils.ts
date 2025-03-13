b0cbb9c151eadf7ccced8add94295267ac1d1568
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2025-03-13 10:55:55 +0000

    lint and types

--- x-pack/solutions/security/plugins/security_solution/server/assistant/tools/esql/utils/index_names_utils.ts
+++ x-pack/solutions/security/plugins/security_solution/server/assistant/tools/esql/utils/index_names_utils.ts
@@ -9,14 +9,16 @@ export const generateIndexNamesWithWildcards = (fullIndexNames: string[]): strin
   const result = new Set(fullIndexNames);
   const partsList = fullIndexNames.map((str) => str.split('.'));
   const occurrences = new Map();
-
+  /* eslint-disable no-bitwise */
   for (const parts of partsList) {
-    for (let mask = 1; mask < 1 << parts.length; mask++) { // eslint-disable-line
-      const wildcardVersion = parts.map((part, idx) => (mask & (1 << idx) ? '*' : part)); // eslint-disable-line no-bitwise
+    for (let mask = 1; mask < 1 << parts.length; mask++) { 
+      const wildcardVersion = parts.map((part, idx) => (mask & (1 << idx) ? '*' : part)); 
       const wildcardStr = wildcardVersion.join('.');
       occurrences.set(wildcardStr, (occurrences.get(wildcardStr) || 0) + 1);
     }
   }
+  /* eslint-enable no-bitwise */
+
 
   for (const [wildcardStr, count] of occurrences.entries()) {
     if (count > 1) {
