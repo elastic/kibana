c00124ddc32de074fac9810a5cbb95e43558ab8b
Jatin Kathuria <jatin.kathuria@elastic.co>
2025-03-13 14:50:30 +0100

    Revert "Revert "feat: add event name to siem migrations telemetry""
    This reverts commit 4879d699e642a4837a99f260f7a49042d3f8f1c5.

--- x-pack/solutions/security/plugins/security_solution/server/lib/telemetry/event_based/events.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/telemetry/event_based/events.ts
@@ -12,6 +12,7 @@ import type {
   ResponseActionsApiCommandNames,
 } from '../../../../common/endpoint/service/response_actions/constants';
 import type { DataStreams, IlmPolicies, IlmsStats, IndicesStats } from '../indices.metadata.types';
+import { SiemMigrationsEventTypes } from './types';
 
 export const RISK_SCORE_EXECUTION_SUCCESS_EVENT: EventTypeOpts<{
   scoresWritten: number;
@@ -708,9 +709,17 @@ export const SIEM_MIGRATIONS_MIGRATION_SUCCESS: EventTypeOpts<{
   completed: number;
   failed: number;
   total: number;
+  eventName: string;
 }> = {
-  eventType: 'siem_migrations_migration_success',
+  eventType: SiemMigrationsEventTypes.MigrationSuccess,
   schema: {
+    eventName: {
+      type: 'keyword',
+      _meta: {
+        description: 'The event name/description',
+        optional: false,
+      },
+    },
     model: {
       type: 'keyword',
       _meta: {
@@ -756,9 +765,17 @@ export const SIEM_MIGRATIONS_RULE_TRANSLATION_SUCCESS: EventTypeOpts<{
   duration: number;
   translationResult: string;
   prebuiltMatch: boolean;
+  eventName: string;
 }> = {
-  eventType: 'siem_migrations_rule_translation_success',
+  eventType: SiemMigrationsEventTypes.TranslationSucess,
   schema: {
+    eventName: {
+      type: 'keyword',
+      _meta: {
+        description: 'The event name/description',
+        optional: false,
+      },
+    },
     translationResult: {
       type: 'keyword',
       _meta: {
@@ -799,9 +816,17 @@ export const SIEM_MIGRATIONS_PREBUILT_RULES_MATCH: EventTypeOpts<{
   preFilterRuleCount: number;
   postFilterRuleName: string;
   postFilterRuleCount: number;
+  eventName: string;
 }> = {
-  eventType: 'siem_migrations_prebuilt_rules_match',
+  eventType: SiemMigrationsEventTypes.PrebuiltRulesMatch,
   schema: {
+    eventName: {
+      type: 'keyword',
+      _meta: {
+        description: 'The event name/description',
+        optional: false,
+      },
+    },
     model: {
       type: 'keyword',
       _meta: {
@@ -851,9 +876,17 @@ export const SIEM_MIGRATIONS_INTEGRATIONS_MATCH: EventTypeOpts<{
   preFilterIntegrationCount: number;
   postFilterIntegrationName: string;
   postFilterIntegrationCount: number;
+  eventName: string;
 }> = {
-  eventType: 'siem_migrations_integration_match',
+  eventType: SiemMigrationsEventTypes.IntegrationsMatch,
   schema: {
+    eventName: {
+      type: 'keyword',
+      _meta: {
+        description: 'The event name/description',
+        optional: false,
+      },
+    },
     model: {
       type: 'keyword',
       _meta: {
@@ -904,9 +937,17 @@ export const SIEM_MIGRATIONS_MIGRATION_FAILURE: EventTypeOpts<{
   completed: number;
   failed: number;
   total: number;
+  eventName: string;
 }> = {
-  eventType: 'siem_migrations_migration_failure',
+  eventType: SiemMigrationsEventTypes.MigrationFailure,
   schema: {
+    eventName: {
+      type: 'keyword',
+      _meta: {
+        description: 'The event name/description',
+        optional: false,
+      },
+    },
     error: {
       type: 'keyword',
       _meta: {
@@ -956,9 +997,17 @@ export const SIEM_MIGRATIONS_RULE_TRANSLATION_FAILURE: EventTypeOpts<{
   model: string;
   error: string;
   migrationId: string;
+  eventName: string;
 }> = {
-  eventType: 'siem_migrations_rule_translation_failure',
+  eventType: SiemMigrationsEventTypes.TranslationFailure,
   schema: {
+    eventName: {
+      type: 'keyword',
+      _meta: {
+        description: 'The event name/description',
+        optional: false,
+      },
+    },
     error: {
       type: 'keyword',
       _meta: {
