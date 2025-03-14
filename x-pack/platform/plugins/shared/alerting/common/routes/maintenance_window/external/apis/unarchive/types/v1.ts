b357248f7f0ebbf1a3a577be4d63ce59b0e253db
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-03-14 15:03:40 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/alerting/common/routes/maintenance_window/external/apis/unarchive/types/v1.ts
+++ x-pack/platform/plugins/shared/alerting/common/routes/maintenance_window/external/apis/unarchive/types/v1.ts
@@ -7,7 +7,7 @@
 
 import type { TypeOf } from '@kbn/config-schema';
 import type { MaintenanceWindowResponseV1 } from '../../../response';
-import { unarchiveMaintenanceWindowRequestParamsSchemaV1 } from '..';
+import type { unarchiveMaintenanceWindowRequestParamsSchemaV1 } from '..';
 
 export type UnarchiveMaintenanceWindowRequestParams = TypeOf<
   typeof unarchiveMaintenanceWindowRequestParamsSchemaV1
