9d13b605c053a060277b88aa92c748f1c1d04366
Maryam Saeidi <maryam.saeidi@elastic.co>
2025-03-14 17:20:05 +0100

    Add a temporary console.log

--- x-pack/solutions/observability/plugins/observability/public/pages/alerts/alerts.tsx
+++ x-pack/solutions/observability/plugins/observability/public/pages/alerts/alerts.tsx
@@ -242,6 +242,11 @@ function InternalAlertsPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  // TODO: remove
+  useEffect(() => {
+    console.log('filterControls:', filterControls);
+  }, [filterControls]);
+
   const manageRulesHref = http.basePath.prepend('/app/observability/alerts/rules');
 
   return (
