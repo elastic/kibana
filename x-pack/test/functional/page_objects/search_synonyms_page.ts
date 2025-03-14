6350fac37b435e3977bdcfaa95b441058721e00a
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2025-03-14 16:50:31 +0100

    Add detail page coverage

--- x-pack/test/functional/page_objects/search_synonyms_page.ts
+++ x-pack/test/functional/page_objects/search_synonyms_page.ts
@@ -91,6 +91,13 @@ export function SearchSynonymsPageProvider({ getService }: FtrProviderContext) {
           'searchSynonymsSynonymsSetEmptyRulesCardsAddEquivalentRuleButton',
         EMPTY_RULES_CARDS_ADD_EXPLICIT_RULE_BUTTON:
           'searchSynonymsSynonymsSetEmptyRulesCardsAddExplicitRuleButton',
+        TABLE_EXPAND_RULE_BUTTON: 'searchSynonymsColumnsButton',
+        TABLE_FROM_RULES: 'synonyms-set-item-explicit-from',
+        TABLE_TO_RULES: 'synonyms-set-item-explicit-to',
+        TABLE_EQUIVALENT_RULES: 'synonyms-set-item-equivalent',
+        RULE_ACTIONS_DELETE_BUTTON: 'searchSynonymsSynonymsSetTableDeleteRuleButton',
+        RULE_ACTIONS_EDIT_BUTTON: 'searchSynonymsSynonymsSetTableEditRuleButton',
+        ADD_NEW_RULE_BUTTON: 'searchSynonymsSynonymsSetRuleTableAddRuleButton',
       },
       async expectSynonymsSetDetailPageNavigated(name: string) {
         const h1Element = await find.byCssSelector('main header h1');
@@ -103,6 +110,87 @@ export function SearchSynonymsPageProvider({ getService }: FtrProviderContext) {
       async expectEmptyPromptToExist() {
         await testSubjects.existOrFail(this.TEST_IDS.EMPTY_RULES_CARDS_ADD_EQUIVALENT_RULE_BUTTON);
         await testSubjects.existOrFail(this.TEST_IDS.EMPTY_RULES_CARDS_ADD_EXPLICIT_RULE_BUTTON);
+      },
+      async getSynonymsRules() {
+        const table = await testSubjects.find('synonyms-set-table');
+        const allRows = await table
+          .findByTagName('tbody')
+          .then((tbody) => tbody.findAllByTagName('tr'));
+
+        return Promise.all(
+          allRows.map(async (row) => {
+            const $ = await row.parseDomContent();
+            if ($.findTestSubject('synonyms-set-item-explicit-from').length > 0) {
+              return {
+                synonyms: `${$.findTestSubject('synonyms-set-item-explicit-from')
+                  .text()
+                  .trim()} => ${$.findTestSubject('synonyms-set-item-explicit-to').text().trim()}`,
+              };
+            } else {
+              return {
+                synonyms: $.findTestSubject('synonyms-set-item-equivalent').text().trim(),
+              };
+            }
+          })
+        );
+      },
+      async clickCreateRuleButton() {
+        await testSubjects.click(this.TEST_IDS.ADD_NEW_RULE_BUTTON);
+      },
+      async clickEquivalentRule() {
+        await testSubjects.click(this.TEST_IDS.EMPTY_RULES_CARDS_ADD_EQUIVALENT_RULE_BUTTON);
+      },
+      async clickExplicitRule() {
+        await testSubjects.click(this.TEST_IDS.EMPTY_RULES_CARDS_ADD_EXPLICIT_RULE_BUTTON);
+      },
+      async deleteRule(index: number) {
+        const table = await testSubjects.find('synonyms-set-table');
+        const allRows = await table
+          .findByTagName('tbody')
+          .then((tbody) => tbody.findAllByTagName('tr'));
+        const deleteButton = await allRows[index].findByTestSubject(
+          this.TEST_IDS.RULE_ACTIONS_DELETE_BUTTON
+        );
+        await deleteButton.click();
+        await testSubjects.click('confirmModalConfirmButton');
+      },
+      async editRule(index: number) {
+        const table = await testSubjects.find('synonyms-set-table');
+        const allRows = await table
+          .findByTagName('tbody')
+          .then((tbody) => tbody.findAllByTagName('tr'));
+        const editButton = await allRows[index].findByTestSubject(
+          this.TEST_IDS.RULE_ACTIONS_EDIT_BUTTON
+        );
+        await editButton.click();
+      },
+    },
+    SynonymRuleFlyout: {
+      TEST_IDS: {
+        FLYOUT_CLOSE_BUTTON: 'euiFlyoutCloseButton',
+        FLYOUT_SAVE_BUTTON: 'searchSynonymsSynonymsRuleFlyoutSaveButton',
+        FLYOUT_FROM_INPUT: 'searchSynonymsSynonymsRuleFlyoutFromTermsInput',
+        FLYOUT_MAPTO_INPUT: 'searchSynonymsSynonymsRuleFlyoutMapToTermsInput',
+        FLYOUT_FROM_BADGE: 'searchSynonymsSynonymsRuleFlyoutFromTermBadge',
+      },
+      async addFromSynonym(synonym: string) {
+        await testSubjects.setValue(this.TEST_IDS.FLYOUT_FROM_INPUT, synonym);
+        await testSubjects.setValue(this.TEST_IDS.FLYOUT_FROM_INPUT, '\uE007');
+      },
+      async addMapTo(synonym: string) {
+        await testSubjects.setValue(this.TEST_IDS.FLYOUT_MAPTO_INPUT, synonym);
+      },
+      async clickSaveButton() {
+        await testSubjects.click(this.TEST_IDS.FLYOUT_SAVE_BUTTON);
+      },
+      async removeSynonym(index: number) {
+        // get the badges and click on the one with the index
+        const badges = await testSubjects.findAll(this.TEST_IDS.FLYOUT_FROM_BADGE);
+        if (index >= badges.length) {
+          throw new Error(`Badge with index ${index} not found`);
+        }
+        const deleteButton = await badges[index].findByTagName('button');
+        await deleteButton.click();
       },
     },
   };
