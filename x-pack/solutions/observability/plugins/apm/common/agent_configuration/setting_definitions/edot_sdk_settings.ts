8874a8bda891f0f9762b3a7450a97ecfe0a015d1
Caue Marcondes <caue.marcondes@elastic.co>
2025-03-17 09:37:15 -0300

    new settings and reverting json

--- x-pack/solutions/observability/plugins/apm/common/agent_configuration/setting_definitions/edot_sdk_settings.ts
+++ x-pack/solutions/observability/plugins/apm/common/agent_configuration/setting_definitions/edot_sdk_settings.ts
@@ -10,20 +10,80 @@ import type { RawSettingDefinition } from './types';
 
 export const edotSDKSettings: RawSettingDefinition[] = [
   {
-    key: 'disable_instrumentations',
+    key: 'deactivate_instrumentations',
     type: 'text',
     defaultValue: '',
-    label: i18n.translate('xpack.apm.agentConfig.disableInstrumentations.label', {
-      defaultMessage: 'Disable instrumentations',
+    label: i18n.translate('xpack.apm.agentConfig.deactivate_instrumentations.label', {
+      defaultMessage: 'Deactivate instrumentations',
     }),
-    description: i18n.translate('xpack.apm.agentConfig.edot.disableInstrumentations.description', {
+    description: i18n.translate(
+      'xpack.apm.agentConfig.edot.deactivate_instrumentations.description',
+      {
+        defaultMessage:
+          'Comma-separated list of modules to disable instrumentation for.\n' +
+          'When instrumentation is disabled for a module, no spans will be collected for that module.\n' +
+          '\n' +
+          'The up-to-date list of modules for which instrumentation can be disabled is language specific ' +
+          'and can be found under the following links: ' +
+          '[opentelemetry/java/elastic](https://ela.st/otel-agent-instructions)',
+      }
+    ),
+    includeAgents: ['opentelemetry/java/elastic'],
+  },
+  {
+    key: 'deactivate_all_instrumentations',
+    type: 'boolean',
+    defaultValue: 'false',
+    label: i18n.translate('xpack.apm.agentConfig.deactivate_all_instrumentations.label', {
+      defaultMessage: 'Deactivate all instrumentations',
+    }),
+    description: i18n.translate(
+      'xpack.apm.agentConfig.edot.deactivate_all_instrumentations.description',
+      {
+        defaultMessage: 'No spans will be collected for any instrumentation modules.\n' + '\n',
+      }
+    ),
+    includeAgents: ['opentelemetry/java/elastic'],
+  },
+  {
+    key: 'send_traces',
+    type: 'boolean',
+    defaultValue: 'true',
+    label: i18n.translate('xpack.apm.agentConfig.send_traces.label', {
+      defaultMessage: 'Send traces',
+    }),
+    description: i18n.translate('xpack.apm.agentConfig.edot.send_traces.description', {
       defaultMessage:
-        'Comma-separated list of modules to disable instrumentation for.\n' +
-        'When instrumentation is disabled for a module, no spans will be collected for that module.\n' +
-        '\n' +
-        'The up-to-date list of modules for which instrumentation can be disabled is language specific ' +
-        'and can be found under the following links: ' +
-        '[opentelemetry/java/elastic](https://ela.st/otel-agent-instructions)',
+        'Set the traces exporter to send or discard traces. When set to false, all traces will be discarded so there will be no traces sent to the collector\n' +
+        '\n',
+    }),
+    includeAgents: ['opentelemetry/java/elastic'],
+  },
+  {
+    key: 'send_metrics',
+    type: 'boolean',
+    defaultValue: 'true',
+    label: i18n.translate('xpack.apm.agentConfig.send_metrics.label', {
+      defaultMessage: 'Send metrics',
+    }),
+    description: i18n.translate('xpack.apm.agentConfig.edot.send_metrics.description', {
+      defaultMessage:
+        'Set the metrics exporter to send or discard metrics. When set to false, all metrics will be discarded so there will be no metrics sent to the collector\n' +
+        '\n',
+    }),
+    includeAgents: ['opentelemetry/java/elastic'],
+  },
+  {
+    key: 'send_logs',
+    type: 'boolean',
+    defaultValue: 'true',
+    label: i18n.translate('xpack.apm.agentConfig.send_logs.label', {
+      defaultMessage: 'Send logs',
+    }),
+    description: i18n.translate('xpack.apm.agentConfig.edot.send_logs.description', {
+      defaultMessage:
+        'Set the logs exporter to send or discard logs. When set to false, all logs (that are normally sent by the agent) will be discarded so there will be no logs sent to the collector\n' +
+        '\n',
     }),
     includeAgents: ['opentelemetry/java/elastic'],
   },
