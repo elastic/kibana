3ef84298597f8aecb1a6c1546f6a55bc7f2b489c
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-03-17 18:53:17 +0100

    Add ignored files

--- src/platform/test/common/fixtures/plugins/coverage/public/index.ts
+++ src/platform/test/common/fixtures/plugins/coverage/public/index.ts
@@ -7,8 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { DocViewerLegacyTable } from './table';
+import { CodeCoverageReportingPlugin } from './plugin';
 
-// Required for usage in React.lazy
-// eslint-disable-next-line import/no-default-export
-export default DocViewerLegacyTable;
+export function plugin() {
+  return new CodeCoverageReportingPlugin();
+}
