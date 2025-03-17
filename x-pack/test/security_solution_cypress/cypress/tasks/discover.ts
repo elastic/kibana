00532d0641348344bdeaca4ffd3efa4753d13232
Jatin Kathuria <jatin.kathuria@elastic.co>
2025-03-17 15:48:14 +0100

    fix: cypress tests

--- x-pack/test/security_solution_cypress/cypress/tasks/discover.ts
+++ x-pack/test/security_solution_cypress/cypress/tasks/discover.ts
@@ -17,9 +17,10 @@ import {
   DISCOVER_FIELD_LIST_LOADING,
   DISCOVER_ESQL_EDITABLE_INPUT,
   AVAILABLE_FIELD_COUNT,
+  DISCOVER_ESQL_INPUT,
+  DISCOVER_ESQL_INPUT_TEXT_CONTAINER,
 } from '../screens/discover';
 import { GET_LOCAL_SEARCH_BAR_SUBMIT_BUTTON } from '../screens/search_bar';
-import { goToEsqlTab } from './timeline';
 
 export const switchDataViewTo = (dataviewName: string) => {
   openDataViewSwitcher();
@@ -61,7 +62,6 @@ export const fillEsqlQueryBar = (query: string) => {
 export const selectCurrentDiscoverEsqlQuery = (
   discoverEsqlInput = DISCOVER_ESQL_EDITABLE_INPUT
 ) => {
-  goToEsqlTab();
   // eslint-disable-next-line cypress/no-force
   cy.get(discoverEsqlInput).click({ force: true });
   fillEsqlQueryBar(Cypress.platform === 'darwin' ? '{cmd+a}' : '{ctrl+a}');
@@ -71,16 +71,29 @@ export const addDiscoverEsqlQuery = (esqlQuery: string) => {
   recurse(
     () => {
       // ESQL input uses the monaco editor which doesn't allow for traditional input updates
-      selectCurrentDiscoverEsqlQuery(DISCOVER_ESQL_EDITABLE_INPUT);
-      fillEsqlQueryBar(esqlQuery);
       selectCurrentDiscoverEsqlQuery();
-      return cy.get(DISCOVER_ESQL_EDITABLE_INPUT);
+      fillEsqlQueryBar(esqlQuery);
+      return cy
+        .get(DISCOVER_ESQL_INPUT_TEXT_CONTAINER)
+        .then(($el) => $el.text().replaceAll(String.fromCharCode(160), ' '));
     },
-    ($el) => $el.val() === esqlQuery || $el.val() === esqlQuery.replaceAll(' ', '\u00b7'),
-    { delay: 1000, limit: 5 }
+    (val) =>
+      val === esqlQuery || val.replaceAll(/\s/, '\u00b7') === esqlQuery.replaceAll(/\s/, '\u00b7'),
+    {
+      delay: 1000,
+      limit: 5,
+      log: (k) => {
+        cy.log(`query found-${k}.`);
+      },
+    }
   );
   cy.get(DISCOVER_ESQL_EDITABLE_INPUT).blur();
   cy.get(GET_LOCAL_SEARCH_BAR_SUBMIT_BUTTON(DISCOVER_CONTAINER)).click();
+};
+
+export const waitForESQLInputToBeVisible = () => {
+  cy.get(DISCOVER_ESQL_INPUT).should('be.visible');
+  cy.get('[role="code"]').should('have.class', 'focused');
 };
 
 export const convertEditorNonBreakingSpaceToSpace = (str: string) => {
@@ -96,7 +109,9 @@ export const verifyDiscoverEsqlQuery = (esqlQueryToVerify: string) => {
    * https://github.com/cypress-io/cypress/issues/15863#issuecomment-816746693
    */
   const unicodeReplacedQuery = esqlQueryToVerify.replaceAll(' ', '\u00b7');
-  cy.get(DISCOVER_ESQL_EDITABLE_INPUT).should(($input) => $input.val() === unicodeReplacedQuery);
+  cy.get(DISCOVER_ESQL_INPUT_TEXT_CONTAINER).should(
+    ($input) => $input.text() === unicodeReplacedQuery
+  );
 };
 
 export const submitDiscoverSearchBar = () => {
