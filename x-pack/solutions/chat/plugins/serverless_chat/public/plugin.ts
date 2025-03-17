c60bff62a4308c03c57b5127d7acd863265f5647
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-03-17 13:12:51 +0100

    Bootstrap 'chat' serverless modules

---
+++ x-pack/solutions/chat/plugins/serverless_chat/public/plugin.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { CoreSetup, CoreStart, Plugin, PluginInitializerContext } from '@kbn/core/public';
+
+import type {
+  ChatServerlessPluginSetup,
+  ChatServerlessPluginStart,
+  ChatServerlessPluginSetupDeps,
+  ChatServerlessPluginStartDeps,
+} from './types';
+
+export class ChatServerlessPlugin
+  implements
+    Plugin<
+      ChatServerlessPluginSetup,
+      ChatServerlessPluginStart,
+      ChatServerlessPluginSetupDeps,
+      ChatServerlessPluginStartDeps
+    >
+{
+  constructor(private readonly initializerContext: PluginInitializerContext) {}
+
+  public setup(
+    core: CoreSetup,
+    setupDeps: ChatServerlessPluginSetupDeps
+  ): ChatServerlessPluginSetup {
+    return {};
+  }
+
+  public start(
+    core: CoreStart,
+    startDeps: ChatServerlessPluginStartDeps
+  ): ChatServerlessPluginStart {
+    return {};
+  }
+
+  public stop() {}
+}
