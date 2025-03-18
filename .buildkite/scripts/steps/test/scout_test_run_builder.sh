6ba3326beb19ccb9c260979105c4d0785ede238f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-03-18 10:56:18 +0100

    revert annotation and remove validation

--- .buildkite/scripts/steps/test/scout_test_run_builder.sh
+++ .buildkite/scripts/steps/test/scout_test_run_builder.sh
@@ -8,22 +8,7 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/copy_es_snapshot_cache.sh
 
 echo '--- Discover Playwright Configs and upload to Buildkite artifacts'
-
-set +e;
-OUTPUT=$(node scripts/scout discover-playwright-configs --save --validate 2>&1)
-EXIT_CODE=$?
-set -e;
-
-if [[ $EXIT_CODE -ne 0 ]]; then
-  echo "Exiting with code 10 without retrying"
-  ERROR_MSG=$(echo "$OUTPUT" | grep -A 10 "ERROR" | sed -E 's/\x1b\[[0-9;]*m//g')
-{
-  echo "$ERROR_MSG"
-} | buildkite-agent annotate --style "error" --context "unregistered-playwright-configs"
-  exit 10
-fi
-
-echo "$OUTPUT"
+node scripts/scout discover-playwright-configs --save
 
 cp .scout/test_configs/scout_playwright_configs.json scout_playwright_configs.json
 buildkite-agent artifact upload "scout_playwright_configs.json"
