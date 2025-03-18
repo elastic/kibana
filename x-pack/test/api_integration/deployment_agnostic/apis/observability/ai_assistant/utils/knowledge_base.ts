6f97c9de3d829317d405aff3a2a64bfb61c9b65d
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-03-18 14:37:21 -0400

    [Obs AI Assistant] Fix contextual insights scoring (#214259)
    Closes https://github.com/elastic/kibana/issues/209572
    
    ### Summary
    
    Scoring in contextual insights is broken because the
    `get_contextual_insight_instructions` tool call is not followed by the
    tool response. This happens because we replace the last user message (in
    this case tool response) with the user message related to scoring.
    
    ### Solution
    
    We should include the tool call name when replacing this message, so
    that it gets converted to inference messages correctly here:
    
    https://github.com/elastic/kibana/blob/07012811b29b487a3b4a664469c7a198355e44bf/x-pack/platform/plugins/shared/observability_ai_assistant/common/convert_messages_for_inference.ts#L60-L81
    
    ### Checklist
    
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    (cherry picked from commit 70e3a6096ebe22cb94902a3567f49e0335506a23)
    
    # Conflicts:
    #	x-pack/test/api_integration/deployment_agnostic/apis/observability/ai_assistant/connectors/connectors.spec.ts

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/ai_assistant/utils/knowledge_base.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/ai_assistant/utils/knowledge_base.ts
@@ -10,9 +10,12 @@ import { Client } from '@elastic/elasticsearch';
 import { AI_ASSISTANT_KB_INFERENCE_ID } from '@kbn/observability-ai-assistant-plugin/server/service/inference_endpoint';
 import { ToolingLog } from '@kbn/tooling-log';
 import { RetryService } from '@kbn/ftr-common-functional-services';
+import { Instruction } from '@kbn/observability-ai-assistant-plugin/common/types';
+import { DeploymentAgnosticFtrProviderContext } from '../../../../ftr_provider_context';
 import type { ObservabilityAIAssistantApiClient } from '../../../../services/observability_ai_assistant_api';
 import { MachineLearningProvider } from '../../../../../services/ml';
 import { SUPPORTED_TRAINED_MODELS } from '../../../../../../functional/services/ml/api';
+import { setAdvancedSettings } from './advanced_settings';
 
 export const TINY_ELSER = {
   ...SUPPORTED_TRAINED_MODELS.TINY_ELSER,
@@ -82,17 +85,6 @@ export async function clearKnowledgeBase(es: Client) {
   });
 }
 
-export async function clearConversations(es: Client) {
-  const KB_INDEX = '.kibana-observability-ai-assistant-conversations-*';
-
-  return es.deleteByQuery({
-    index: KB_INDEX,
-    conflicts: 'proceed',
-    query: { match_all: {} },
-    refresh: true,
-  });
-}
-
 export async function deleteInferenceEndpoint({
   es,
   name = AI_ASSISTANT_KB_INFERENCE_ID,
@@ -101,4 +93,68 @@ export async function deleteInferenceEndpoint({
   name?: string;
 }) {
   return es.inference.delete({ inference_id: name, force: true });
+}
+
+export async function addSampleDocsToInternalKb(
+  getService: DeploymentAgnosticFtrProviderContext['getService'],
+  sampleDocs: Array<Instruction & { title: string }>
+) {
+  const log = getService('log');
+  const ml = getService('ml');
+  const retry = getService('retry');
+  const observabilityAIAssistantAPIClient = getService('observabilityAIAssistantApi');
+
+  await importTinyElserModel(ml);
+  await setupKnowledgeBase(observabilityAIAssistantAPIClient);
+  await waitForKnowledgeBaseReady({ observabilityAIAssistantAPIClient, log, retry });
+
+  await observabilityAIAssistantAPIClient.editor({
+    endpoint: 'POST /internal/observability_ai_assistant/kb/entries/import',
+    params: {
+      body: {
+        entries: sampleDocs,
+      },
+    },
+  });
+}
+
+export async function addSampleDocsToCustomIndex(
+  getService: DeploymentAgnosticFtrProviderContext['getService'],
+  sampleDocs: Array<Instruction & { title: string }>,
+  customSearchConnectorIndex: string
+) {
+  const es = getService('es');
+  const supertest = getService('supertest');
+  const log = getService('log');
+
+  // create index with semantic_text mapping for `text` field
+  log.info('Creating custom index with sample animal docs...');
+  await es.indices.create({
+    index: customSearchConnectorIndex,
+    mappings: {
+      properties: {
+        title: { type: 'text' },
+        text: { type: 'semantic_text', inference_id: AI_ASSISTANT_KB_INFERENCE_ID },
+      },
+    },
+  });
+
+  log.info('Indexing sample animal docs...');
+  // ingest sampleDocs
+  await Promise.all(
+    sampleDocs.map(async (doc) => {
+      const { id, ...restDoc } = doc;
+      return es.index({
+        refresh: 'wait_for',
+        index: customSearchConnectorIndex,
+        id,
+        body: restDoc,
+      });
+    })
+  );
+
+  // update the advanced settings (`observability:aiAssistantSearchConnectorIndexPattern`) to include the custom index
+  await setAdvancedSettings(supertest, {
+    'observability:aiAssistantSearchConnectorIndexPattern': customSearchConnectorIndex,
+  });
 }
