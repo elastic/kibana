b0caf859b0b0e902cdcd283eeced1ce49b2f733e
Julia Rechkunova <julia.rechkunova@elastic.co>
2025-03-18 11:33:51 +0100

    [Discover] Update types

--- src/platform/packages/shared/kbn-unified-tabs/src/utils/manage_tabs.ts
+++ src/platform/packages/shared/kbn-unified-tabs/src/utils/manage_tabs.ts
@@ -25,7 +25,7 @@ export const isLastTab = ({ items }: TabsState, item: TabItem): boolean => {
 export const addTab = (
   { items, selectedItem }: TabsState,
   item: TabItem,
-  maxItemsCount?: number
+  maxItemsCount: number | undefined
 ): TabsState => {
   if (maxItemsCount && items.length >= maxItemsCount) {
     return {
@@ -80,7 +80,7 @@ export const insertTabAfter = (
   { items, selectedItem }: TabsState,
   item: TabItem,
   insertAfterItem: TabItem,
-  maxItemsCount?: number
+  maxItemsCount: number | undefined
 ): TabsState => {
   if (maxItemsCount && items.length >= maxItemsCount) {
     return {
