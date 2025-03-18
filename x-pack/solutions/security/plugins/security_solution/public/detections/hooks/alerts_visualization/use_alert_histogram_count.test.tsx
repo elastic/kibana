390b05931dc7bbd563d219e5da01994ee0e89305
Karen Grigoryan <karen.grigoryan@elastic.co>
2025-03-18 01:07:21 +0100

    [Security Solution][Explore][Overview Dashboard] Handle undefined and empty responses in alert histogram count
    Addresses #214733
    
    - Modify `useAlertHistogramCount` to use optional chaining and nullish coalescing for safer access to response data
    - Update test cases to cover scenarios with undefined, empty array, and incomplete response objects

--- x-pack/solutions/security/plugins/security_solution/public/detections/hooks/alerts_visualization/use_alert_histogram_count.test.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/detections/hooks/alerts_visualization/use_alert_histogram_count.test.tsx
@@ -8,21 +8,54 @@
 import { renderHook } from '@testing-library/react';
 import { TestProviders } from '../../../common/mock';
 import { useAlertHistogramCount } from './use_alert_histogram_count';
+import { useVisualizationResponse } from '../../../common/components/visualization_actions/use_visualization_response';
 
 jest.mock('../../../common/components/visualization_actions/use_visualization_response', () => ({
   useVisualizationResponse: jest.fn().mockReturnValue({ responses: [{ hits: { total: 100 } }] }),
 }));
 
 describe('useAlertHistogramCount', () => {
-  const props = {
+  const defaultProps = {
     visualizationId: 'mockVisualizationId',
   };
 
   it('returns visualization alerts count', () => {
-    const testPops = { ...props, isChartEmbeddablesEnabled: true };
-    const { result } = renderHook(() => useAlertHistogramCount(testPops), {
+    const props = { ...defaultProps, isChartEmbeddablesEnabled: true };
+    const { result } = renderHook(() => useAlertHistogramCount(props), {
       wrapper: TestProviders,
     });
     expect(result.current).toEqual('Showing: 100 alerts');
+  });
+
+  describe.each([
+    {
+      responses: undefined,
+      title: 'undefined',
+    },
+    {
+      responses: [],
+      title: 'empty array',
+    },
+    {
+      responses: [{}],
+      title: 'array with empty object',
+    },
+    {
+      responses: [{ hits: {} }],
+      title: 'array with object with empty hits',
+    },
+    {
+      responses: [{ hits: { total: undefined } }],
+      title: 'array with object with hits with undefined total',
+    },
+  ])('when visualization responses is $title', () => {
+    it('returns Showing: 0 alerts', () => {
+      const props = { ...defaultProps, isChartEmbeddablesEnabled: true };
+      (useVisualizationResponse as jest.Mock).mockReturnValue({ responses: [] });
+      const { result } = renderHook(() => useAlertHistogramCount(props), {
+        wrapper: TestProviders,
+      });
+      expect(result.current).toEqual('Showing: 0 alerts');
+    });
   });
 });
