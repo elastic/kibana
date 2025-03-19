c126194c178123419a0e4edd9de434dd8c006030
“jeramysoucy” <jeramy.soucy@elastic.co>
2025-03-19 15:29:06 +0100

    Skips all cases basic tests in FIPS mode

--- x-pack/test/functional_with_es_ssl/apps/cases/basic/index.ts
+++ x-pack/test/functional_with_es_ssl/apps/cases/basic/index.ts
@@ -9,6 +9,7 @@ import { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default ({ loadTestFile }: FtrProviderContext) => {
   describe('Cases basic license', function () {
+    this.tags('skipFIPS');
     loadTestFile(require.resolve('./list_view'));
   });
 };
