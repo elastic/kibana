bdfa46d4b6df1f33f801d0ba65df8c922e1bfd27
Julian Gernun <17549662+jcger@users.noreply.github.com>
2025-03-19 13:06:35 +0100

    version update alert deletion setting

--- x-pack/platform/plugins/shared/alerting/server/routes/rules_settings/transforms/index.ts
+++ x-pack/platform/plugins/shared/alerting/server/routes/rules_settings/transforms/index.ts
@@ -7,6 +7,8 @@
 
 export { transformQueryDelaySettingsToResponse } from './transform_query_delay_settings_to_response/latest';
 export { transformAlertDeletionSettingsToResponse } from './transform_alert_deletion_settings_to_response/latest';
+export { transformAlertDeletionSettingsRequest } from './transform_alert_deletion_settings_request/latest';
 
 export { transformQueryDelaySettingsToResponse as transformQueryDelaySettingsToResponseV1 } from './transform_query_delay_settings_to_response/v1';
 export { transformAlertDeletionSettingsToResponse as transformAlertDeletionSettingsToResponseV1 } from './transform_alert_deletion_settings_to_response/latest';
+export { transformAlertDeletionSettingsRequest as transformAlertDeletionSettingsRequestV1 } from './transform_alert_deletion_settings_request/latest';
