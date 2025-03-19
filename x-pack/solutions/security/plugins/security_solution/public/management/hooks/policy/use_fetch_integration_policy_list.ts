7c82f872f5feb93cbe3c438fb0f9fc9591caa492
Paul Tavares <paul.tavares@elastic.co>
2025-03-19 16:04:19 -0400

    Initial implementation of policy selector component

---
+++ x-pack/solutions/security/plugins/security_solution/public/management/hooks/policy/use_fetch_integration_policy_list.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type {
+  GetPackagePoliciesRequest,
+  GetPackagePoliciesResponse,
+} from '@kbn/fleet-plugin/common';
+import { API_VERSIONS, packagePolicyRouteService } from '@kbn/fleet-plugin/common';
+import type { UseQueryOptions, UseQueryResult } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query';
+import type { IHttpFetchError } from '@kbn/core-http-browser';
+import { useHttp } from '../../../common/lib/kibana';
+
+/**
+ * Fetch integration policies from Fleet.
+ */
+export const useFetchIntegrationPolicyList = (
+  query: GetPackagePoliciesRequest['query'] = {},
+  options: Omit<UseQueryOptions<GetPackagePoliciesResponse, IHttpFetchError>, 'queryFn'> = {}
+): UseQueryResult<GetPackagePoliciesResponse, IHttpFetchError> => {
+  const http = useHttp();
+
+  return useQuery<GetPackagePoliciesResponse, IHttpFetchError>({
+    queryKey: ['fetch-integration-policy-list', query],
+    ...options,
+    queryFn: async () => {
+      return http.get<GetPackagePoliciesResponse>(packagePolicyRouteService.getListPath(), {
+        query,
+        version: API_VERSIONS.public.v1,
+      });
+    },
+  });
+};
