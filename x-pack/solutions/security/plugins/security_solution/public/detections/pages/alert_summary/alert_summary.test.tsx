c58d4e257d84a1ffe02917f0dbe9164610b12339
Philippe Oberti <philippe.oberti@elastic.co>
2025-03-20 13:42:36 -0500

    [AI4DSOC] Alert summary page routing and initialization (#214889)
    ## Summary
    
    This PR is the setting the foundations for the AI for SOC Alert summary
    page. It has very little UI, instead it focuses on the following:
    - add routing for the `alert_summary` page
    - fetches the integrations, filters them to only keep the ones related
    to AI for SOC, then decides what to render depending on if some AI for
    SOC packages have been installed or not
    
    The PR also makes a small change to the `SecurityRoutePageWrapper`
    component, to allow us to redirect to the Security Solution HomePage
    instead of the NoPrivilegesPage. While this might not be a long term
    solution, it is the easiest path forward. In the future, AI for SOC will
    most likely be its own plugin (leaving outside of Security Solution)
    hence this will not be needed anymore.
    
    Here's the basic behavior of the Alert summary page:
    - The `Landing page` will be shown if none of the hardcoded AI for SOC
    packages are installed (these values are hardcoded as we currently do
    not have a way to filter integrations for the AI for SOC ones only):
      - splunk // doesnt yet exist
      - google_secops
      - microsoft_sentinel
      - sentinel_one
      - crowdstrike
    - The `Wrapper` component will only be shown if you have at least one of
    the above AI for SOC packages installed.
    
    ### Very limited UI added in this PR
    
    | Loading integrations | No installed packages | Some installed packages
    |
    | ------------- | ------------- | ------------- |
    | ![Screenshot 2025-03-17 at 6 58
    45 PM](https://github.com/user-attachments/assets/68089c33-fa40-4201-8b51-3e7236d50d5a)
    | ![Screenshot 2025-03-17 at 6 59
    15 PM](https://github.com/user-attachments/assets/e7e5af2d-bdab-4bef-881e-bb5e512c3545)
    | ![Screenshot 2025-03-17 at 6 59
    40 PM](https://github.com/user-attachments/assets/61b346bb-799f-4a0b-95cb-e3092ea58d37)
    |
    
    ### Notes
    
    We need to remove the section at the top of the page that currently
    shows the `Add integrations` button. A follow PR will take care of that.
    [This](https://github.com/elastic/kibana/blob/main/x-pack/solutions/security/plugins/security_solution/public/app/home/index.tsx#L54)
    is where that bar is being added. We will have to find a way to not show
    that for the AI for SOC tier.
    
    ## How to test
    
    This needs to be ran in Serverless:
    - `yarn es serverless --projectType security`
    - `yarn serverless-security --no-base-path`
    
    You also need to enable the AI for SOC tier, by adding the following to
    your `serverless.security.dev.yaml` file:
    ```
    xpack.securitySolutionServerless.productTypes:
      [
        { product_line: 'ai_soc', product_tier: 'search_ai_lake' },
      ]
    ```
    
    The Alert summary navigation will NOT be shown for the following
    Serverless users: `viewer`, `t1_analyst`.
    and `t2_analyst`. For those, the navigation entry is not present, and
    navigating to the url directly will automatically re-route to the
    Security home page.
    
    Currently, retrieving the integrations (via the `fleet/epm/packages`
    endpoint) is also unauthorized for the following users: `editor`,
    `t3_analyst`, `threat_intelligence_analyst`, `rule_author`,
    `soc_manager` and `detections_admin`.
    
    This means that the only users that can be currently used to test this
    PR are:
    - `platform_engineer`
    - `endpoint_operations_analyst`
    - `endpoint_policy_manager`
    - `admin`
    - `system_indices_superuser`
    
    ### Checklist
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    Will help close https://github.com/elastic/security-team/issues/11954 as
    well as https://github.com/elastic/security-team/issues/11979.
    
    (cherry picked from commit 3605a331b92e75b64eaba0d724b9a2cdf5c34910)

---
+++ x-pack/solutions/security/plugins/security_solution/public/detections/pages/alert_summary/alert_summary.test.tsx
@@ -0,0 +1,55 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { render } from '@testing-library/react';
+import { AlertSummaryPage, LOADING_INTEGRATIONS_TEST_ID } from './alert_summary';
+import { useFetchIntegrations } from '../../hooks/alert_summary/use_fetch_integrations';
+import { LANDING_PAGE_PROMPT_TEST_ID } from '../../components/alert_summary/landing_page/landing_page';
+import { useAddIntegrationsUrl } from '../../../common/hooks/use_add_integrations_url';
+import { DATA_VIEW_LOADING_PROMPT_TEST_ID } from '../../components/alert_summary/wrapper';
+
+jest.mock('../../hooks/alert_summary/use_fetch_integrations');
+jest.mock('../../../common/hooks/use_add_integrations_url');
+
+describe('<AlertSummaryPage />', () => {
+  it('should render loading logo', () => {
+    (useFetchIntegrations as jest.Mock).mockReturnValue({
+      isLoading: true,
+    });
+
+    const { getByTestId } = render(<AlertSummaryPage />);
+    expect(getByTestId(LOADING_INTEGRATIONS_TEST_ID)).toHaveTextContent('Loading integrations');
+  });
+
+  it('should render landing page if no packages are installed', () => {
+    (useFetchIntegrations as jest.Mock).mockReturnValue({
+      installedPackages: [],
+      isLoading: false,
+    });
+    (useAddIntegrationsUrl as jest.Mock).mockReturnValue({
+      onClick: jest.fn(),
+    });
+
+    const { getByTestId, queryByTestId } = render(<AlertSummaryPage />);
+    expect(queryByTestId(LOADING_INTEGRATIONS_TEST_ID)).not.toBeInTheDocument();
+    expect(getByTestId(LANDING_PAGE_PROMPT_TEST_ID)).toBeInTheDocument();
+  });
+
+  it('should render wrapper if packages are installed', () => {
+    (useFetchIntegrations as jest.Mock).mockReturnValue({
+      availablePackage: [],
+      installedPackages: [{ id: 'id' }],
+      isLoading: false,
+    });
+
+    const { getByTestId, queryByTestId } = render(<AlertSummaryPage />);
+    expect(queryByTestId(LOADING_INTEGRATIONS_TEST_ID)).not.toBeInTheDocument();
+    expect(queryByTestId(LANDING_PAGE_PROMPT_TEST_ID)).not.toBeInTheDocument();
+    expect(getByTestId(DATA_VIEW_LOADING_PROMPT_TEST_ID)).toBeInTheDocument();
+  });
+});
