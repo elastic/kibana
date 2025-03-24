25de1861d95cf66d22208ee68664d79a1b26e06c
pgayvallet <pierre.gayvallet@elastic.co>
2025-03-24 08:02:21 +0100

    move @kbn/wci-external-server

--- x-pack/solutions/chat/plugins/wci-external-server/jest.config.js
+++ x-pack/solutions/chat/plugins/wci-external-server/jest.config.js
@@ -7,7 +7,7 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../../../../../..',
+  rootDir: '../../../../..',
   roots: ['<rootDir>/x-pack/solutions/search/plugins/workchat/wci_external_server/server'],
   testMatch: ['**/*.test.ts', '**/*.test.tsx'],
 };
