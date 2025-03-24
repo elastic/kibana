308baedf5c91d6893ca2492255dd6fb56e3921d1
Jason Rhodes <jason.rhodes@elastic.co>
2025-03-24 13:28:26 -0400

    Adjusts oblt serverless nav for custom roles
    - Adds roles link in the Management > Access section
    - Moves users from top level to the Management > Access section
    
    Note: to view the users link locally, you need to set a value in the
    Kibana config: --xpack.cloud.users_and_roles_url="https://test_users_and_roles_url"

--- x-pack/solutions/observability/plugins/serverless_observability/public/navigation_tree.ts
+++ x-pack/solutions/observability/plugins/serverless_observability/public/navigation_tree.ts
@@ -294,6 +294,74 @@ export const createNavigationTree = ({
                 spaceBefore: null,
                 renderAs: 'panelOpener',
                 children: [
+                  { link: 'management:index_management', breadcrumbStatus: 'hidden' },
+                  { link: 'management:transform', breadcrumbStatus: 'hidden' },
+                  { link: 'management:ingest_pipelines', breadcrumbStatus: 'hidden' },
+                  { link: 'management:dataViews', breadcrumbStatus: 'hidden' },
+                  { link: 'management:jobsListLink', breadcrumbStatus: 'hidden' },
+                  { link: 'management:pipelines', breadcrumbStatus: 'hidden' },
+                  { link: 'management:data_quality', breadcrumbStatus: 'hidden' },
+                  { link: 'management:data_usage', breadcrumbStatus: 'hidden' },
+                  { link: 'management:content_connectors', breadcrumbStatus: 'hidden' },
+                ],
+              },
+              {
+                title: i18n.translate('xpack.serverlessObservability.nav.mngt.access', {
+                  defaultMessage: 'Access',
+                }),
+                breadcrumbStatus: 'hidden',
+                children: [
+                  { link: 'management:api_keys', breadcrumbStatus: 'hidden' },
+                  { link: 'management:roles', breadcrumbStatus: 'hidden' },
+                  {
+                    cloudLink: 'userAndRoles',
+                    title: i18n.translate(
+                      'xpack.serverlessObservability.navLinks.projectSettings.mngt.usersAndRoles',
+                      { defaultMessage: 'Manage organization members' }
+                    ),
+                  },
+                ],
+              },
+              {
+                title: i18n.translate('xpack.serverlessObservability.nav.mngt.alertsAndInsights', {
+                  defaultMessage: 'Alerts and insights',
+                }),
+                breadcrumbStatus: 'hidden',
+                children: [
+                  { link: 'management:triggersActionsConnectors', breadcrumbStatus: 'hidden' },
+                  { link: 'management:maintenanceWindows', breadcrumbStatus: 'hidden' },
+                ],
+              },
+              {
+                title: 'Machine Learning',
+                children: [
+                  { link: 'management:overview' },
+                  { link: 'management:anomaly_detection' },
+                  { link: 'management:analytics' },
+                  { link: 'management:trained_models' },
+                  { link: 'management:supplied_configurations' },
+                ],
+              },
+              {
+                title: i18n.translate('xpack.serverlessObservability.nav.mngt.content', {
+                  defaultMessage: 'Content',
+                }),
+                breadcrumbStatus: 'hidden',
+                children: [
+                  { link: 'management:spaces', breadcrumbStatus: 'hidden' },
+                  { link: 'management:objects', breadcrumbStatus: 'hidden' },
+                  { link: 'management:filesManagement', breadcrumbStatus: 'hidden' },
+                  { link: 'management:reporting', breadcrumbStatus: 'hidden' },
+                  { link: 'management:tags', breadcrumbStatus: 'hidden' },
+                ],
+              },
+              {
+                title: i18n.translate('xpack.serverlessObservability.nav.mngt.other', {
+                  defaultMessage: 'Other',
+                }),
+                breadcrumbStatus: 'hidden',
+                children: [
+                  { link: 'management:settings', breadcrumbStatus: 'hidden' },
                   {
                     title: i18n.translate('xpack.serverlessObservability.nav.mngt.data', {
                       defaultMessage: 'Data',
@@ -384,6 +452,16 @@ export const createNavigationTree = ({
                 cloudLink: 'billingAndSub',
               },
             ],
+          },
+          {
+            link: 'integrations',
+          },
+          {
+            link: 'fleet',
+          },
+          {
+            id: 'cloudLinkBilling',
+            cloudLink: 'billingAndSub',
           },
         ],
       },
