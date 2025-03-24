b102d4d97e5c1384c52a175dcffe1454ef45b602
pgayvallet <pierre.gayvallet@elastic.co>
2025-03-24 13:51:54 +0100

    fix jest configurations

--- x-pack/solutions/chat/packages/wci-browser/jest.config.js
+++ x-pack/solutions/chat/packages/wci-browser/jest.config.js
@@ -8,5 +8,5 @@
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
-  roots: ['<rootDir>/x-pack/solutions/search/packages/workchat/wci-browser'],
+  roots: ['<rootDir>/x-pack/solutions/chat/packages/wci-browser'],
 };
