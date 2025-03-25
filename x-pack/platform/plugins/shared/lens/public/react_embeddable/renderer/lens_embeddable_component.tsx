082f6960d54e0e690fb699b34a8ff0530f0d1851
Marco Liberati <dej611@users.noreply.github.com>
2025-03-25 16:10:29 +0100

    Update x-pack/platform/plugins/shared/lens/public/react_embeddable/renderer/lens_embeddable_component.tsx

--- x-pack/platform/plugins/shared/lens/public/react_embeddable/renderer/lens_embeddable_component.tsx
+++ x-pack/platform/plugins/shared/lens/public/react_embeddable/renderer/lens_embeddable_component.tsx
@@ -42,7 +42,7 @@ export function LensEmbeddableComponent({
     api.rendered$,
     api.viewMode$
   );
-  const canEdit = Boolean(api.isEditingEnabled?.() && [latestViewMode, getViewMode(api)].includes('edit');
+  const canEdit = Boolean(api.isEditingEnabled?.() && [latestViewMode, getViewMode(api)].includes('edit'));
 
   const [warningOrErrors, infoMessages] = useMessages(internalApi);
 
