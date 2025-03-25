2d52dc1a25a48afc701860ce2ee6f3be6920e023
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2025-03-25 16:20:00 +0100

    Treat "links" field as array

--- x-pack/platform/packages/shared/kbn-apm-types/src/es_schemas/raw/fields/span_links.ts
+++ x-pack/platform/packages/shared/kbn-apm-types/src/es_schemas/raw/fields/span_links.ts
@@ -9,3 +9,8 @@ export interface SpanLink {
   trace: { id: string };
   span: { id: string };
 }
+
+export interface RawOtelSpanLink {
+  span_id: Array<string | undefined>;
+  trace_id: Array<string | undefined>;
+}
--- x-pack/solutions/observability/plugins/apm/server/routes/span_links/utils.ts
+++ x-pack/solutions/observability/plugins/apm/server/routes/span_links/utils.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import type { RawOtelSpanLink, SpanLink } from '@kbn/apm-types';
 import moment from 'moment';
 
 export function getBufferedTimerange({
@@ -20,4 +21,28 @@ export function getBufferedTimerange({
     startWithBuffer: moment(start).subtract(bufferSize, 'days').valueOf(),
     endWithBuffer: moment(end).add(bufferSize, 'days').valueOf(),
   };
+}
+
+export function mapOtelToSpanLink(otelSpanLinks: Partial<RawOtelSpanLink> | undefined): SpanLink[] {
+  const spanLink: SpanLink[] = [];
+
+  const { span_id: spanIds = [], trace_id: traceIds = [] } = otelSpanLinks ?? {};
+
+  for (let i = 0; i < spanIds.length; i++) {
+    const spanId = spanIds[i];
+    const traceId = traceIds[i];
+
+    if (traceId && spanId) {
+      spanLink.push({
+        span: {
+          id: spanId,
+        },
+        trace: {
+          id: traceId,
+        },
+      });
+    }
+  }
+
+  return spanLink;
 }
