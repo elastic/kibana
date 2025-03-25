397dd2e7aa429781d0ea382b84e8d8de9a2d44c9
Jedr Blaszyk <jedr.blaszyk@elastic.co>
2025-03-25 18:39:09 +0100

    [Salesforce integration] Add configuration page to set index name

--- x-pack/solutions/chat/plugins/wci-salesforce/public/integration/configuration.tsx
+++ x-pack/solutions/chat/plugins/wci-salesforce/public/integration/configuration.tsx
@@ -7,18 +7,33 @@
 
 import React from 'react';
 import { IntegrationConfigurationFormProps } from '@kbn/wci-browser';
-import { EuiDescribedFormGroup } from '@elastic/eui';
+import { EuiDescribedFormGroup, EuiFieldText, EuiFormRow } from '@elastic/eui';
+import { Controller } from 'react-hook-form';
 
 export const SalesforceConfigurationForm: React.FC<IntegrationConfigurationFormProps> = ({
   form,
 }) => {
+  const { control } = form;
+
   return (
     <EuiDescribedFormGroup
       ratio="third"
       title={<h3>Salesforce Configuration</h3>}
       description="Configure the salesforce details"
     >
-      none
+      <EuiFormRow label="Index">
+        <Controller
+          name="configuration.index"
+          control={control}
+          render={({ field }) => (
+            <EuiFieldText
+              data-test-subj="workchatAppIntegrationEditViewIndex"
+              placeholder="Enter index name"
+              {...field}
+            />
+          )}
+        />
+      </EuiFormRow>
     </EuiDescribedFormGroup>
   );
 };
