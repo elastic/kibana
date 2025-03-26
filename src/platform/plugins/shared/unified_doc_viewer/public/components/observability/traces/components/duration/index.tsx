440738e3702d169ea3e801fbc3ddda8f72218457
iblancof <irene.blanco@elastic.co>
2025-03-26 14:57:38 +0100

    Create Duration

---
+++ src/platform/plugins/shared/unified_doc_viewer/public/components/observability/traces/components/duration/index.tsx
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React from 'react';
+import { EuiLoadingSpinner, EuiText } from '@elastic/eui';
+import { asDuration } from '../../utils';
+import { PercentOfParent } from './percent_of_parent';
+
+export interface DurationProps {
+  duration: number;
+  showParentDuration?: boolean;
+  parent?: {
+    duration: number | undefined;
+    type: 'trace' | 'transaction';
+    loading: boolean;
+  };
+}
+
+// TODO: Move this component to a shared library for use here in Discover and in APM.
+// https://github.com/elastic/kibana/issues/211781
+
+export function Duration({ duration, showParentDuration = false, parent }: DurationProps) {
+  if (!showParentDuration || (showParentDuration && !parent)) {
+    <EuiText size="xs">{asDuration(duration)}</EuiText>;
+  }
+  return (
+    <EuiText size="xs">
+      {asDuration(duration)} &nbsp;
+      {parent?.loading && <EuiLoadingSpinner />}
+      {!parent?.loading && parent?.duration && (
+        <PercentOfParent
+          duration={duration}
+          totalDuration={parent?.duration}
+          parentType={parent?.type}
+        />
+      )}
+    </EuiText>
+  );
+}
