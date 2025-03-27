276d97d8dfc4516e962c3b025b65635699a22405
Kevin Delemme <kevin.delemme@elastic.co>
2025-03-27 15:13:36 -0400

    reset UI work

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_significant_events_view/index.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_significant_events_view/index.tsx
@@ -4,105 +4,13 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import {
-  EuiBadge,
-  EuiBasicTable,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiLink,
-  EuiSuperDatePicker,
-  EuiTableFieldDataColumnType,
-  OnTimeChangeProps,
-} from '@elastic/eui';
-import { IngestStreamGetResponse, SignificantEventsResponse } from '@kbn/streams-schema';
-import React, { useState } from 'react';
-import { useFetchSignificantEvents } from '../../hooks/use_fetch_significant_events';
-import { useKibana } from '../../hooks/use_kibana';
-import { SignificantEventsHistogramChart } from './significant_events_histogram';
-import { buildDiscoverParams } from './utils/discover_helpers';
+import { IngestStreamGetResponse } from '@kbn/streams-schema';
+import React from 'react';
 
 export function StreamDetailSignificantEventsView({
   definition,
 }: {
   definition?: IngestStreamGetResponse;
 }) {
-  const {
-    dependencies: {
-      start: { discover },
-    },
-  } = useKibana();
-  const [range, setRange] = useState({ from: 'now-1d', to: 'now' });
-  const { isLoading, data: significantEvents } = useFetchSignificantEvents({
-    name: definition?.stream.name,
-    range,
-  });
-
-  const onTimeChange = ({ start, end }: OnTimeChangeProps) => {
-    setRange({ from: start, to: end });
-  };
-
-  const columns: Array<EuiTableFieldDataColumnType<SignificantEventsResponse>> = [
-    {
-      field: 'title',
-      name: 'Event name',
-      render: (_, record: SignificantEventsResponse) => (
-        <EuiLink
-          target="_blank"
-          href={discover?.locator?.getRedirectUrl(
-            buildDiscoverParams(record, definition?.stream.name)
-          )}
-        >
-          {record.title}
-        </EuiLink>
-      ),
-    },
-    {
-      field: 'query',
-      name: 'Query',
-      render: (_, record: SignificantEventsResponse) => <>{record.kql.query}</>,
-    },
-    {
-      field: 'occurrences',
-      name: 'Occurrences',
-      render: (_, record: SignificantEventsResponse) => (
-        <SignificantEventsHistogramChart occurrences={record.occurrences} />
-      ),
-    },
-    {
-      field: 'change_points',
-      name: 'Change points',
-      render: (_, record: SignificantEventsResponse) => (
-        <EuiBadge>
-          {Object.keys(record.change_points.type)[0]} -{' '}
-          {Object.values(record.change_points.type)[0]?.p_value ?? 0}
-        </EuiBadge>
-      ),
-    },
-  ];
-
-  return (
-    <EuiFlexGroup direction="column" gutterSize="m">
-      <EuiFlexItem grow={false}>
-        <EuiFlexGroup direction="row" alignItems="flexStart" justifyContent="flexEnd">
-          <EuiSuperDatePicker
-            isLoading={isLoading}
-            start={range.from}
-            end={range.to}
-            onTimeChange={onTimeChange}
-            showUpdateButton
-          />
-        </EuiFlexGroup>
-      </EuiFlexItem>
-
-      <EuiFlexItem grow={false}>
-        <EuiBasicTable
-          tableCaption="Significant Events"
-          items={significantEvents ?? []}
-          rowHeader="title"
-          columns={columns}
-          loading={isLoading}
-        />
-      </EuiFlexItem>
-    </EuiFlexGroup>
-  );
+  return <></>;
 }
