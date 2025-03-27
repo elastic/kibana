c54a9c5baec4f1470d6d39bb9b3a6a060c118ff6
Kylie Meli <kylie.geller@elastic.co>
2025-03-27 16:30:58 -0400

    cleanup

--- x-pack/platform/plugins/shared/fleet/public/components/transform_install_as_current_user_callout.tsx
+++ x-pack/platform/plugins/shared/fleet/public/components/transform_install_as_current_user_callout.tsx
@@ -35,7 +35,7 @@ export const TransformInstallWithCurrentUserPermissionCallout: React.FunctionCom
       <FormattedMessage
         id="xpack.fleet.createPackagePolicy.transformInstallWithCurrentUserPermissionCallout"
         defaultMessage="This package has {count, plural, one {one} other {#}} {docsLink} which will be created and started with the same roles as the user installing the package."
-        values={{ count, isSingular: count === 1, docsLink: (
+        values={{ count, docsLink: (
           <EuiLink
             href={docLinks.links.transforms.overview}
           >
