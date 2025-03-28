3d19c3b1aafbba05e360e3ca9dc93ef1ee047402
Joe Reuter <johannes.reuter@elastic.co>
2025-03-28 12:09:20 +0100

    fix stuff

--- x-pack/platform/plugins/shared/streams_app/public/components/streams_app_page_header/streams_app_page_header_title.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/streams_app_page_header/streams_app_page_header_title.tsx
@@ -4,19 +4,12 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { EuiTitle, useEuiTheme } from '@elastic/eui';
-import { css } from '@emotion/css';
+import { EuiTitle } from '@elastic/eui';
 import React from 'react';
 
 export function StreamsAppPageHeaderTitle({ title }: { title: React.ReactNode }) {
-  const theme = useEuiTheme().euiTheme;
   return (
-    <EuiTitle
-      size="l"
-      className={css`
-        padding-top: ${theme.size.m};
-      `}
-    >
+    <EuiTitle size="l">
       <h1>{title}</h1>
     </EuiTitle>
   );
