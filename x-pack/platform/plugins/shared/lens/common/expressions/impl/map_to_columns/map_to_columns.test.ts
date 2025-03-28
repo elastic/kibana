db98203d09c58d43e2423418553cb5425331c03a
Marco Liberati <dej611@users.noreply.github.com>
2025-03-28 16:17:23 +0100

    [Lens] Optimize async chunks (#211921)
    ## Summary
    
    While documenting some Lens architecture I've noticed how chunks are
    organized in Lens, so I've taken a quick spin to reduce the amount of
    requests to load a dashboard panel and more in general the number of
    async chunks produced by webpack.
    
    This PR aims to do 2 things:
    * optimize the number of chunks generated by webpack
    * optimize the number of requests done to load a Lens embedable panel
    * optimize any `await` flow to do **other things ™️ ** while in idle to
    load
    
    ### Final results
    
    #### Reduce the number of chunks
    
    The final number of chunks got reduced from 24 to 15. The bundle size
    has remained almost the same.
    
    | Before  | After |
    | ------------- | ------------- |
    | <img width="1443" alt="Screenshot 2025-03-10 at 12 53 21"
    src="https://github.com/user-attachments/assets/65030955-7b7c-493c-9559-fbb9ef9089d4"
    /> | <img width="1101" alt="Screenshot 2025-03-10 at 12 51 53"
    src="https://github.com/user-attachments/assets/99b9b78d-931a-40ed-bda6-820584c1337e"
    /> |
    
    There's still some improvement margin here, but the changes mainly
    includes he followings:
    * the embeddable is now bundled together with the `async_services`
    bundle
    * as both are required to render the panel in a dashboard, this change
    should speed up a bit the dashboard use case vs the Lens editor one
    * Expression implementations has been deferred into their separate
    bundles
      *  this should reduce a bit the initial `plugin.js` bundle by few kb
    * most of the times the bundled expressions are used together, so it
    makes sense to bundle those 3/4 together rather than have 4 tiny bundles
    to async load and prevent waterfall `async import` calls which led to
    poor performance
    * Defer a component in the `@kbn/unified-field-list` component
    * this was making load some edit component within the `async_services`
    bundle even in dashboard.
    * because this is a component only required in Lens editor, this has
    been deferred
    * Async register actions as recommended by @nreese  
    
    #### Reduce the number of requests
    
    The final number of chunks requested to load a lens panel on a dashboard
    went from 12 to 4.
    The overall bundle size has remained almost the same, but the
    distributions of weights have slightly changed.
    Surprisingly there's 1 MB which is gone somewhere, but that doesn't seem
    to have an actual impact on the overall loading experience yet.
    
    | Before  | After |
    | ------------- | ------------- |
    | <img width="1445" alt="Screenshot 2025-03-10 at 12 58 08"
    src="https://github.com/user-attachments/assets/faab091b-305d-43ad-8be2-2f3bb83913a3"
    /> | <img width="1110" alt="Screenshot 2025-03-10 at 12 58 32"
    src="https://github.com/user-attachments/assets/f88ac9f5-80a6-42d3-8e3b-3013df05cb8b"
    /> |
    
    #### Optimize any `await` call
    
    Well, this is hard to test. So I have no direct proof at the moment. 🤷 
    
    cc @thomasneirynck
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/platform/plugins/shared/lens/common/expressions/impl/map_to_columns/map_to_columns.test.ts
@@ -0,0 +1,385 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { mapToColumns } from '../../defs/map_to_columns/map_to_columns';
+import { Datatable } from '@kbn/expressions-plugin/common';
+import { createMockExecutionContext } from '@kbn/expressions-plugin/common/mocks';
+
+describe('map_to_columns', () => {
+  it('should rename columns of a given datatable', async () => {
+    const input: Datatable = {
+      type: 'datatable',
+      columns: [
+        { id: 'a', name: 'A', meta: { type: 'number' } },
+        { id: 'b', name: 'B', meta: { type: 'number' } },
+      ],
+      rows: [
+        { a: 1, b: 2 },
+        { a: 3, b: 4 },
+        { a: 5, b: 6 },
+        { a: 7, b: 8 },
+      ],
+    };
+
+    const idMap = {
+      a: [
+        {
+          id: 'b',
+          label: 'Austrailia',
+        },
+      ],
+      b: [
+        {
+          id: 'c',
+          label: 'Boomerang',
+        },
+      ],
+    };
+
+    const result = await mapToColumns.fn(
+      input,
+      { idMap: JSON.stringify(idMap) },
+      createMockExecutionContext()
+    );
+
+    expect(result).toMatchInlineSnapshot(`
+      Object {
+        "columns": Array [
+          Object {
+            "id": "b",
+            "meta": Object {
+              "type": "number",
+            },
+            "name": "Austrailia",
+          },
+          Object {
+            "id": "c",
+            "meta": Object {
+              "type": "number",
+            },
+            "name": "Boomerang",
+          },
+        ],
+        "rows": Array [
+          Object {
+            "b": 1,
+            "c": 2,
+          },
+          Object {
+            "b": 3,
+            "c": 4,
+          },
+          Object {
+            "b": 5,
+            "c": 6,
+          },
+          Object {
+            "b": 7,
+            "c": 8,
+          },
+        ],
+        "type": "datatable",
+      }
+    `);
+  });
+
+  it('should keep columns which are not mapped', async () => {
+    const input: Datatable = {
+      type: 'datatable',
+      columns: [
+        { id: 'a', name: 'A', meta: { type: 'number' } },
+        { id: 'b', name: 'B', meta: { type: 'number' } },
+      ],
+      rows: [
+        { a: 1, b: 2 },
+        { a: 3, b: 4 },
+        { a: 5, b: 6 },
+        { a: 7, b: 8 },
+      ],
+    };
+
+    const idMap = {
+      b: [{ id: 'c', label: 'Catamaran' }],
+    };
+
+    const result = await mapToColumns.fn(
+      input,
+      { idMap: JSON.stringify(idMap) },
+      createMockExecutionContext()
+    );
+
+    expect(result).toMatchInlineSnapshot(`
+      Object {
+        "columns": Array [
+          Object {
+            "id": "a",
+            "meta": Object {
+              "type": "number",
+            },
+            "name": "A",
+          },
+          Object {
+            "id": "c",
+            "meta": Object {
+              "type": "number",
+            },
+            "name": "Catamaran",
+          },
+        ],
+        "rows": Array [
+          Object {
+            "a": 1,
+            "c": 2,
+          },
+          Object {
+            "a": 3,
+            "c": 4,
+          },
+          Object {
+            "a": 5,
+            "c": 6,
+          },
+          Object {
+            "a": 7,
+            "c": 8,
+          },
+        ],
+        "type": "datatable",
+      }
+    `);
+  });
+
+  it('should map to multiple original columns', async () => {
+    const input: Datatable = {
+      type: 'datatable',
+      columns: [{ id: 'b', name: 'B', meta: { type: 'number' } }],
+      rows: [{ b: 2 }, { b: 4 }, { b: 6 }, { b: 8 }],
+    };
+
+    const idMap = {
+      b: [
+        { id: 'c', label: 'Catamaran' },
+        { id: 'd', label: 'Dinghy' },
+      ],
+    };
+
+    const result = await mapToColumns.fn(
+      input,
+      { idMap: JSON.stringify(idMap) },
+      createMockExecutionContext()
+    );
+
+    expect(result).toMatchInlineSnapshot(`
+      Object {
+        "columns": Array [
+          Object {
+            "id": "c",
+            "meta": Object {
+              "type": "number",
+            },
+            "name": "Catamaran",
+          },
+          Object {
+            "id": "d",
+            "meta": Object {
+              "type": "number",
+            },
+            "name": "Dinghy",
+          },
+        ],
+        "rows": Array [
+          Object {
+            "c": 2,
+            "d": 2,
+          },
+          Object {
+            "c": 4,
+            "d": 4,
+          },
+          Object {
+            "c": 6,
+            "d": 6,
+          },
+          Object {
+            "c": 8,
+            "d": 8,
+          },
+        ],
+        "type": "datatable",
+      }
+    `);
+  });
+
+  it('should rename date histograms', async () => {
+    const input: Datatable = {
+      type: 'datatable',
+      columns: [
+        { id: 'a', name: 'A', meta: { type: 'number' } },
+        { id: 'b', name: 'banana per 30 seconds', meta: { type: 'number' } },
+      ],
+      rows: [
+        { a: 1, b: 2 },
+        { a: 3, b: 4 },
+        { a: 5, b: 6 },
+        { a: 7, b: 8 },
+      ],
+    };
+
+    const idMap = {
+      b: [{ id: 'c', label: 'Apple', operationType: 'date_histogram', sourceField: 'banana' }],
+    };
+
+    const result = await mapToColumns.fn(
+      input,
+      { idMap: JSON.stringify(idMap) },
+      createMockExecutionContext()
+    );
+
+    expect(result).toMatchInlineSnapshot(`
+      Object {
+        "columns": Array [
+          Object {
+            "id": "a",
+            "meta": Object {
+              "type": "number",
+            },
+            "name": "A",
+          },
+          Object {
+            "id": "c",
+            "meta": Object {
+              "type": "number",
+            },
+            "name": "Apple per 30 seconds",
+          },
+        ],
+        "rows": Array [
+          Object {
+            "a": 1,
+            "c": 2,
+          },
+          Object {
+            "a": 3,
+            "c": 4,
+          },
+          Object {
+            "a": 5,
+            "c": 6,
+          },
+          Object {
+            "a": 7,
+            "c": 8,
+          },
+        ],
+        "type": "datatable",
+      }
+    `);
+  });
+
+  describe('map_to_columns_text_based', () => {
+    it('should keep columns that exist in idMap only', async () => {
+      const input: Datatable = {
+        type: 'datatable',
+        columns: [
+          { id: 'a', name: 'A', meta: { type: 'number' } },
+          { id: 'b', name: 'B', meta: { type: 'number' } },
+          { id: 'c', name: 'C', meta: { type: 'string' } },
+        ],
+        rows: [
+          { a: 1, b: 2, c: '3' },
+          { a: 3, b: 4, c: '5' },
+          { a: 5, b: 6, c: '7' },
+          { a: 7, b: 8, c: '9' },
+        ],
+      };
+
+      const idMap = {
+        a: [
+          {
+            id: 'a',
+            label: 'A',
+          },
+        ],
+        b: [
+          {
+            id: 'b',
+            label: 'B',
+          },
+        ],
+      };
+
+      const result = await mapToColumns.fn(
+        input,
+        { idMap: JSON.stringify(idMap), isTextBased: true },
+        createMockExecutionContext()
+      );
+
+      expect(result.columns).toStrictEqual([
+        { id: 'a', name: 'A', meta: { type: 'number', sourceParams: {} } },
+        { id: 'b', name: 'B', meta: { type: 'number', sourceParams: {} } },
+      ]);
+
+      expect(result.rows).toStrictEqual([
+        { a: 1, b: 2 },
+        { a: 3, b: 4 },
+        { a: 5, b: 6 },
+        { a: 7, b: 8 },
+      ]);
+    });
+
+    it('should handle correctly columns controlled by variables', async () => {
+      const input: Datatable = {
+        type: 'datatable',
+        columns: [
+          { id: 'a', name: 'A', meta: { type: 'number' } },
+          { id: 'b', name: 'B', meta: { type: 'number' } },
+          { id: 'c', name: 'C', meta: { type: 'string' }, variable: 'field' },
+        ],
+        rows: [
+          { a: 1, b: 2, c: '3' },
+          { a: 3, b: 4, c: '5' },
+          { a: 5, b: 6, c: '7' },
+          { a: 7, b: 8, c: '9' },
+        ],
+      };
+
+      const idMap = {
+        a: [
+          {
+            id: 'a',
+            label: 'A',
+          },
+        ],
+        '?field': [
+          {
+            id: 'field',
+            label: '?field',
+            variable: 'field',
+          },
+        ],
+      };
+
+      const result = await mapToColumns.fn(
+        input,
+        { idMap: JSON.stringify(idMap), isTextBased: true },
+        createMockExecutionContext()
+      );
+
+      expect(result.columns).toStrictEqual([
+        { id: 'a', name: 'A', meta: { type: 'number', sourceParams: {} } },
+        { id: 'field', name: 'C', meta: { type: 'string' }, variable: 'field' },
+      ]);
+
+      expect(result.rows).toStrictEqual([
+        { a: 1, field: '3' },
+        { a: 3, field: '5' },
+        { a: 5, field: '7' },
+        { a: 7, field: '9' },
+      ]);
+    });
+  });
+});
