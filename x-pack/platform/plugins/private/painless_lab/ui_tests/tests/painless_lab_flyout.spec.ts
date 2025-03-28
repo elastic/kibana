2b16b4385d6dd84b9339eed0712946628c342920
fake-haris <charis.kalpakis@elastic.co>
2025-03-28 19:35:38 +0200

    added updated script response in test file

--- x-pack/platform/plugins/private/painless_lab/ui_tests/tests/painless_lab_flyout.spec.ts
+++ x-pack/platform/plugins/private/painless_lab/ui_tests/tests/painless_lab_flyout.spec.ts
@@ -8,6 +8,7 @@
 import { expect, test, tags } from '@kbn/scout';
 
 const TEST_SCRIPT_RESULT = '45';
+const UPDATED_TEST_SCRIPT_RESPONSE = '"45"'
 const TEST_SCRIPT = `
 int total = 0;
 
@@ -17,8 +18,7 @@ for (int i = 0; i < 10; ++i) {
 
 return total;
 `.trim();
-
-const TEST_SCRIPT_REQUEST = `POST _scripts/painless/_execute\n{\n  \"script\": {\n    \"source\": \"\"\"int total = 0;\n    \n    for (int i = 0; i < 10; ++i) {\n      total += i;\n    }\n    \n    return total;\"\"\",\n    \"params\": {\n      \"string_parameter\": \"string value\",\n      \"number_parameter\": 1.5,\n      \"boolean_parameter\": true\n    }\n  }\n}`;
+const TEST_SCRIPT_REQUEST = 'POST _scripts/painless/_execute\n{\n  \"script\": {\n    \"source\": \"\"\"int total = 0;\n    \n    for (int i = 0; i < 10; ++i) {\n      total += i;\n    }\n    \n    return total;\"\"\",\n    \"params\": {\n      \"string_parameter\": \"string value\",\n      \"number_parameter\": 1.5,\n      \"boolean_parameter\": true\n    }\n  }\n}';
 
 test.describe('test scenario', { tag: tags.ESS_ONLY }, () => {
   test.beforeEach(async ({ browserAuth, pageObjects }) => {
@@ -31,7 +31,6 @@ test.describe('test scenario', { tag: tags.ESS_ONLY }, () => {
     pageObjects.painlessLab.setCodeEditorValue(TEST_SCRIPT);
     await expect(pageObjects.painlessLab.outputValueElement).toContainText(TEST_SCRIPT_RESULT);
 
-    // pageObjects.painlessLab.setCodeEditorValue(UPDATED_TEST_SCRIPT);
     pageObjects.painlessLab.clickShowApiRequest();
     await expect(pageObjects.painlessLab.requestFlyoutHeader).toBeVisible();
 
