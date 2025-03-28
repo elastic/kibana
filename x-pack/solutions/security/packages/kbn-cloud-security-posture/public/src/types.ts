925ac1f6a29845571113fe3414858396359109c8
animehart <rickyangwyn@gmail.com>
2025-03-28 14:42:28 -0700

    preliminary work on updating vuln findings flyout render method

--- x-pack/solutions/security/packages/kbn-cloud-security-posture/public/src/types.ts
+++ x-pack/solutions/security/packages/kbn-cloud-security-posture/public/src/types.ts
@@ -64,7 +64,7 @@ export interface CspBaseEsQuery {
 }
 
 export interface UseCspOptions extends CspBaseEsQuery {
-  sort: Array<{
+  sort?: Array<{
     [key: string]: string;
   }>;
   enabled: boolean;
