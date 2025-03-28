42543a6be1355ef6d15186bb600d1dd9f9d5490f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-03-28 16:24:15 +0100

    3 workers

--- x-pack/platform/plugins/private/discover_enhanced/ui_tests/parallel.playwright.config.ts
+++ x-pack/platform/plugins/private/discover_enhanced/ui_tests/parallel.playwright.config.ts
@@ -10,5 +10,5 @@ import { createPlaywrightConfig } from '@kbn/scout';
 // eslint-disable-next-line import/no-default-export
 export default createPlaywrightConfig({
   testDir: './parallel_tests',
-  workers: 2,
+  workers: 3,
 });
