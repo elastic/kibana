a4089bbfc21e1000d29ddaa03b504644ea3cc77b
alex prozorov <alex.prozorov@elastic.co>
2025-03-30 12:14:00 +0300

    unify popover table items component with FindingsMultiValueCellRenderComponent

--- x-pack/solutions/security/plugins/security_solution/public/cloud_security_posture/components/csp_details/vulnerabilities_findings_details_table.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/cloud_security_posture/components/csp_details/vulnerabilities_findings_details_table.tsx
@@ -19,11 +19,13 @@ import {
   useVulnerabilitiesFindings,
   VULNERABILITY,
 } from '@kbn/cloud-security-posture/src/hooks/use_vulnerabilities_findings';
+import type { MultiValueCellAction } from '@kbn/cloud-security-posture';
 import {
   getVulnerabilityStats,
   CVSScoreBadge,
   SeverityStatusBadge,
   getNormalizedSeverity,
+  ActionableBadge,
 } from '@kbn/cloud-security-posture';
 import {
   ENTITY_FLYOUT_EXPAND_VULNERABILITY_VIEW_VISITS,
@@ -34,9 +36,7 @@ import { METRIC_TYPE } from '@kbn/analytics';
 import { SecurityPageName } from '@kbn/deeplinks-security';
 import { useGetNavigationUrlParams } from '@kbn/cloud-security-posture/src/hooks/use_get_navigation_url_params';
 import { useHasVulnerabilities } from '@kbn/cloud-security-posture/src/hooks/use_has_vulnerabilities';
-import type { MultiValueCellAction } from '@kbn/cloud-security-posture-plugin/public/common/component/actionable_badge';
 import {
-  ActionableBadge,
   FindingsMultiValueCellRender,
   findReferenceLink,
 } from '@kbn/cloud-security-posture-plugin/public';
