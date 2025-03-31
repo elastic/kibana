0e642ed16a3696d686dcf3b07b7dd46046a0f3b5
Georgiana-Andreea Onoleata <georgiana.onoleata@elastic.co>
2025-03-31 14:19:45 +0300

    added some fixes

--- x-pack/platform/plugins/shared/cases/public/components/case_settings/sync_alerts_switch.tsx
+++ x-pack/platform/plugins/shared/cases/public/components/case_settings/sync_alerts_switch.tsx
@@ -41,7 +41,7 @@ const SyncAlertsSwitchComponent: React.FC<Props> = ({
       onChange={onChange}
       disabled={disabled}
       data-test-subj="sync-alerts-switch"
-      aria-label="Sync alerts"
+      aria-label={i18n.SYNC_ALERTS}
     />
   );
 };
