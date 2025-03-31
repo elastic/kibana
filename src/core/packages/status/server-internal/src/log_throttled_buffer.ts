37dba7717e8c050b7b8b2bf2c56c0422ffef18ff
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2025-03-31 21:24:23 +0200

    [Status logging] More performant throttling

---
+++ src/core/packages/status/server-internal/src/log_throttled_buffer.ts
@@ -0,0 +1,68 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { concatAll, map, type Observable, type Subject, takeUntil, bufferTime } from 'rxjs';
+import { uniq } from 'lodash';
+import type { LoggableServiceStatus } from './types';
+
+export interface CreateLogThrottledBufferOptions<LoggableStatus extends LoggableServiceStatus> {
+  buffer$: Subject<LoggableStatus>;
+  stop$: Observable<void>;
+  throttleIntervalMillis: number;
+  maxThrottledMessages: number;
+}
+
+export function createLogThrottledBuffer<LoggableStatus extends LoggableServiceStatus>({
+  buffer$,
+  stop$,
+  throttleIntervalMillis,
+  maxThrottledMessages,
+}: CreateLogThrottledBufferOptions<LoggableStatus>): Observable<LoggableStatus | string> {
+  const throttled$: Observable<LoggableStatus | string> = buffer$.asObservable().pipe(
+    takeUntil(stop$),
+    bufferTime(throttleIntervalMillis),
+    map((statuses) => {
+      const aggregated = // aggregate repeated messages, and count nbr. of repetitions
+        statuses.filter((candidateStatus, index) => {
+          const firstMessageIndex = statuses.findIndex(
+            (status) =>
+              candidateStatus.name === status.name &&
+              candidateStatus.level === status.level &&
+              candidateStatus.summary === status.summary
+          );
+          if (index !== firstMessageIndex) {
+            // this is not the first time this message is logged, increase 'repeats' counter for the first occurrence
+            statuses[firstMessageIndex].repeats = (statuses[firstMessageIndex].repeats ?? 1) + 1;
+            return false;
+          } else {
+            // this is the first time this message is logged, let it through
+            return true;
+          }
+        });
+
+      if (aggregated.length > maxThrottledMessages) {
+        const list: string = uniq(
+          aggregated.slice(maxThrottledMessages).map(({ name }) => name)
+        ).join(', ');
+
+        return [
+          ...aggregated.slice(0, maxThrottledMessages),
+          `${
+            aggregated.length - maxThrottledMessages
+          } other status updates from [${list}] have been truncated to avoid flooding the logs`,
+        ];
+      } else {
+        return aggregated;
+      }
+    }),
+    concatAll()
+  );
+
+  return throttled$;
+}
