106fc48ff93eb380794e91fc61eb3910f17ed6e0
Hannah Mudge <hannah.wright@elastic.co>
2025-04-01 15:12:58 -0600

    Fix import

--- src/plugins/controls/public/control_group/utils/serialization_utils.ts
+++ src/plugins/controls/public/control_group/utils/serialization_utils.ts
@@ -8,7 +8,7 @@
  */
 
 import { v4 as uuidv4 } from 'uuid';
-import { SerializedPanelState } from '@kbn/presentation-publishing';
+import type { SerializedPanelState } from '@kbn/presentation-containers';
 import type {
   ControlGroupRuntimeState,
   ControlGroupSerializedState,
