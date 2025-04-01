9329baaf376fabccc319312935ac2e6e11cad3dd
Janki Salvi <jankigaurav.salvi@elastic.co>
2025-04-01 15:51:33 +0100

    fix skipped tests

--- x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/alert_create_flyout.ts
+++ x-pack/test/functional_with_es_ssl/apps/triggers_actions_ui/alert_create_flyout.ts
@@ -118,15 +118,54 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     await rules.common.cancelRuleCreation();
   }
 
+  const esQueryRule = {
+    tags: [],
+    params: {
+      searchType: 'esQuery',
+      timeWindowSize: 5,
+      timeWindowUnit: 'm',
+      threshold: [1],
+      thresholdComparator: '>',
+      size: 100,
+      esQuery: '{\n    "query":{\n      "match_all" : {}\n    }\n  }',
+      aggType: 'count',
+      groupBy: 'all',
+      termSize: 5,
+      excludeHitsFromPreviousRun: false,
+      sourceFields: [],
+      index: ['.kibana_alerting_cases'],
+      timeField: 'updated_at',
+    },
+    schedule: {
+      interval: '1m',
+    },
+    consumer: 'stackAlerts',
+    name: 'Elasticsearch query rule',
+    rule_type_id: '.es-query',
+    actions: [],
+    alert_delay: {
+      active: 1,
+    },
+  };
+
   describe('create alert', function () {
     let apmSynthtraceEsClient: ApmSynthtraceEsClient;
     const webhookConnectorName = 'webhook-test';
+    let esQueryRuleId: string;
     before(async () => {
       await esArchiver.load(
         'src/platform/test/api_integration/fixtures/es_archiver/index_patterns/constant_keyword'
       );
 
       await createWebhookConnector(webhookConnectorName);
+
+      const { body: createdESRule } = await supertest
+        .post('/api/alerting/rule')
+        .set('kbn-xsrf', 'foo')
+        .send(esQueryRule)
+        .expect(200);
+
+      esQueryRuleId = createdESRule.id;
 
       const version = (await apmSynthtraceKibanaClient.installApmPackage()).version;
       apmSynthtraceEsClient = await getApmSynthtraceEsClient({
@@ -168,6 +207,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       await esArchiver.unload(
         'src/platform/test/api_integration/fixtures/es_archiver/index_patterns/constant_keyword'
       );
+      await supertest.delete(`/api/alerting/rule/${esQueryRuleId}`).set('kbn-xsrf', 'foo');
 
       await deleteConnectorByName(webhookConnectorName);
     });
@@ -216,8 +256,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       // FLAKY: https://github.com/elastic/kibana/issues/167444
     });
 
-    // commenting for now to get cloud deployment working
-    it.skip('should create an alert', async () => {
+    it('should create an alert', async () => {
       const alertName = generateUniqueKey();
       await rules.common.defineIndexThresholdAlert(alertName);
 
@@ -294,8 +333,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       await deleteAlerts(alertsToDelete.map((alertItem: { id: string }) => alertItem.id));
     });
 
-    // commenting for now to get cloud deployment working
-    it.skip('should create an alert with composite query in filter for conditional action', async () => {
+    it('should create an alert with composite query in filter for conditional action', async () => {
       const alertName = generateUniqueKey();
       await rules.common.defineIndexThresholdAlert(alertName);
 
@@ -387,8 +425,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       await deleteAlerts(alertsToDelete.map((alertItem: { id: string }) => alertItem.id));
     });
 
-    // commenting for now to get cloud deployment working
-    it.skip('should create an alert with DSL filter for conditional action', async () => {
+    it('should create an alert with DSL filter for conditional action', async () => {
       const alertName = generateUniqueKey();
       await rules.common.defineIndexThresholdAlert(alertName);
 
--- x-pack/test/observability_functional/apps/observability/pages/alerts/index.ts
+++ x-pack/test/observability_functional/apps/observability/pages/alerts/index.ts
@@ -6,6 +6,7 @@
  */
 
 import expect from '@kbn/expect';
+import { RuleNotifyWhen } from '@kbn/alerting-plugin/common';
 import { FtrProviderContext } from '../../../../ftr_provider_context';
 import { asyncForEach } from '../../helpers';
 
@@ -17,25 +18,98 @@ export default ({ getService, getPageObjects }: FtrProviderContext) => {
   const esArchiver = getService('esArchiver');
   const find = getService('find');
   const { alertControls } = getPageObjects(['alertControls']);
+  const PageObjects = getPageObjects(['home', 'common']);
+  const supertest = getService('supertest');
+  const browser = getService('browser');
+
+  const customThresholdRule = {
+    tags: [],
+    params: {
+      criteria: [
+        {
+          comparator: '>',
+          metrics: [
+            {
+              name: 'A',
+              aggType: 'count',
+            },
+          ],
+          threshold: [1],
+          timeSize: 15,
+          timeUnit: 'm',
+        },
+      ],
+      alertOnNoData: false,
+      alertOnGroupDisappear: false,
+      searchConfiguration: {
+        query: {
+          query: '',
+          language: 'kuery',
+        },
+        index: '90943e30-9a47-11e8-b64d-95841ca0b247',
+      },
+    },
+    schedule: {
+      interval: '1m',
+    },
+    consumer: 'logs',
+    name: 'Custom threshold rule',
+    rule_type_id: 'observability.rules.custom_threshold',
+    actions: [
+      {
+        group: 'custom_threshold.fired',
+        id: 'my-server-log',
+        params: {
+          message:
+            '{{context.reason}}\n\n{{rule.name}} is active.\n\n[View alert details]({{context.alertDetailsUrl}})\n',
+          level: 'info',
+        },
+        frequency: {
+          summary: false,
+          notify_when: RuleNotifyWhen.THROTTLE,
+          throttle: '1m',
+        },
+      },
+    ],
+    alert_delay: {
+      active: 1,
+    },
+  };
 
   describe('Observability alerts >', function () {
     this.tags('includeFirefox');
-
     const testSubjects = getService('testSubjects');
     const retry = getService('retry');
     const observability = getService('observability');
+    let customThresholdRuleId: string;
 
     before(async () => {
       await esArchiver.load('x-pack/test/functional/es_archives/observability/alerts');
       const setup = async () => {
         await observability.alerts.common.setKibanaTimeZoneToUTC();
         await observability.alerts.common.navigateToTimeWithData();
+        await PageObjects.common.navigateToUrl('home', '/tutorial_directory/sampleData', {
+          useActualUrl: true,
+        });
+        await PageObjects.home.addSampleDataSet('logs');
+        const { body: createdRule } = await supertest
+          .post('/api/alerting/rule')
+          .set('kbn-xsrf', 'foo')
+          .send(customThresholdRule)
+          .expect(200);
+
+        customThresholdRuleId = createdRule.id;
       };
       await setup();
     });
 
     after(async () => {
       await esArchiver.unload('x-pack/test/functional/es_archives/observability/alerts');
+      await PageObjects.common.navigateToUrl('home', '/tutorial_directory/sampleData', {
+        useActualUrl: true,
+      });
+      await supertest.delete(`/api/alerting/rule/${customThresholdRuleId}`).set('kbn-xsrf', 'foo');
+      await PageObjects.home.removeSampleDataSet('logs');
     });
 
     describe('Alerts table', () => {
@@ -71,8 +145,7 @@ export default ({ getService, getPageObjects }: FtrProviderContext) => {
         });
       });
 
-      // commenting for now to create a cloud deployment for testing
-      describe.skip('Filtering', () => {
+      describe('Filtering', () => {
         afterEach(async () => {
           await observability.alerts.common.clearQueryBar();
         });
@@ -84,6 +157,7 @@ export default ({ getService, getPageObjects }: FtrProviderContext) => {
         });
 
         it('Autocompletion works', async () => {
+          await browser.refresh();
           await observability.alerts.common.typeInQueryBar('kibana.alert.s');
           await observability.alerts.common.clickOnQueryBar();
           await testSubjects.existOrFail('autocompleteSuggestion-field-kibana.alert.start-');
