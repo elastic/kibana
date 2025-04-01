f521de79e1eb92e0fe48727235ec8a07584bba43
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2025-04-01 11:29:02 +0200

    [kbn-grid-layout][dashboard] Basic keyboard interaction (#208286)
    ## Summary
    Adds keyboard navigation for drag-and-drop interactions
    Fixes https://github.com/elastic/kibana/issues/211925
    Fixes https://github.com/elastic/kibana/issues/190448
    
    ### Supported features
    1. Resize panels
    
    https://github.com/user-attachments/assets/ba7add16-a0c6-4f15-9f3b-0f8ef7caf8ac
    
    2. Drag panels within the same section (dragging between sections is
    pending)
    
    https://github.com/user-attachments/assets/a1fd80af-63ca-4fa2-bded-3db9968a8366
    
    3. Move rows up/down
    
    https://github.com/user-attachments/assets/8d7e8d7d-b1bf-4abe-9cc2-28eeea9b43f8
    
    ### Interaction Flow
    
    1. Start interaction with `Space` or `Enter`
    2. Move using arrow keys
    3. Finish by confirming (`Enter`/`Space`) or canceling (`Escape`)
    (blurring also confirms the changes)
    
    ### Scrolling Behavior:
    * Default browser scrolling is disabled in interaction mode to avoid
    unexpected behavior and edge cases that would overcomplicate this simple
    implementation.
    * Scrolling occurs when the user reaches the edge of the screen while
    resizing or dragging, allowing them to continue the interaction
    smoothly.
    * When the operation is confirmed, we also scroll to the element to make
    sure it's in view.
    
    ### Missing (planned for another PR):
    * A11y announcements
    * Dragging between sections
    * This feature is not well unit-tested, but it's very difficult to do it
    without mocking the crucial pieces of functionality. I'd vote to leave
    it for now and add a few functional tests once we decide a strategy for
    it, since drag and drop interactions are anyway quite difficult to
    unit-test reliably anyway.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit f5b185a28f9b1af41ca6a36f010b50b470e34ddb)

--- src/platform/packages/private/kbn-grid-layout/grid/use_grid_layout_events/sensors/keyboard/types.ts
+++ src/platform/packages/private/kbn-grid-layout/grid/use_grid_layout_events/sensors/keyboard/types.ts
@@ -7,5 +7,22 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export { useGridLayoutPanelEvents } from './panel/events';
-export { useGridLayoutRowEvents } from './row/events';
+export enum KeyboardCode {
+  Space = 'Space',
+  Down = 'ArrowDown',
+  Right = 'ArrowRight',
+  Left = 'ArrowLeft',
+  Up = 'ArrowUp',
+  Esc = 'Escape',
+  Enter = 'Enter',
+  Tab = 'Tab',
+}
+
+export interface KeyboardCodes {
+  start: Array<KeyboardEvent['code']>;
+  cancel: Array<KeyboardEvent['code']>;
+  end: Array<KeyboardEvent['code']>;
+  move: Array<KeyboardEvent['code']>;
+}
+
+export type UserKeyboardEvent = KeyboardEvent | React.KeyboardEvent<HTMLButtonElement>;
