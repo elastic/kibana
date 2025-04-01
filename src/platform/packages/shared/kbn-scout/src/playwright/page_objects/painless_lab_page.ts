48eb5f56c5b991b8a11dc58b00f7ecbfb2af4d48
fake-haris <charis.kalpakis@elastic.co>
2025-04-01 13:35:18 +0300

    reverted import

--- src/platform/packages/shared/kbn-scout/src/playwright/page_objects/painless_lab_page.ts
+++ src/platform/packages/shared/kbn-scout/src/playwright/page_objects/painless_lab_page.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { MonacoEnvironment } from '@kbn/monaco';
+import '@kbn/monaco';
 import { Locator } from 'playwright/test';
 import { ScoutPage } from '..';
 
