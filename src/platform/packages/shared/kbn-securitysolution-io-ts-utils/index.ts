cea8cdd331fc58096d136990c81711cf9cad2776
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-04-01 13:06:47 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- src/platform/packages/shared/kbn-securitysolution-io-ts-utils/index.ts
+++ src/platform/packages/shared/kbn-securitysolution-io-ts-utils/index.ts
@@ -11,6 +11,6 @@ export * from './src/format_errors';
 export * from './src/parse_schedule_dates';
 export * from './src/exact_check';
 export * from './src/format_errors';
-// eslint-disable-next-line @kbn/imports/no_boundary_crossing
+
 export * from './src/test_utils';
 export * from './src/validate';
