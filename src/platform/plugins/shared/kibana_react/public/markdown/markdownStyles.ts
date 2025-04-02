320255d577bedbbc482fea554eadbf0367bd47fe
paulina.shakirova <paulina.shakirova@elastic.co>
2025-04-02 18:14:19 +0200

    fix tests

--- src/platform/plugins/shared/kibana_react/public/markdown/markdownStyles.ts
+++ src/platform/plugins/shared/kibana_react/public/markdown/markdownStyles.ts
@@ -36,237 +36,235 @@ export const markdownStyles = () => {
   const kbnMarkdownAlphaLightShadeReversed = `rgba(${euiTheme.colors.emptyShade}, .15)`;
 
   return css({
-    '.kbnMarkdown__body': {
-      '&--reversed': {
-        color: `${euiTheme.colors.lightestShade}`,
-      },
+    '&--reversed': {
+      color: `${euiTheme.colors.lightestShade}`,
+    },
 
-      '> *:first-child': {
-        marginTop: '0 !important',
-      },
+    '> *:first-child': {
+      marginTop: '0 !important',
+    },
 
-      '> *:last-child': {
-        marginBottom: '0 !important',
-      },
+    '> *:last-child': {
+      marginBottom: '0 !important',
+    },
 
-      'p, blockquote, ul, ol, dl, table, pre': {
-        marginTop: 0,
-        marginBottom: kbnMarkdownSize,
-        lineHeight: '1.5em',
-      },
+    'p, blockquote, ul, ol, dl, table, pre': {
+      marginTop: 0,
+      marginBottom: kbnMarkdownSize,
+      lineHeight: '1.5em',
+    },
 
-      strong: {
-        fontWeight: 600,
-      },
+    strong: {
+      fontWeight: 600,
+    },
 
-      // Links
-      a: {
+    // Links
+    a: {
+      color: 'inherit',
+      textDecoration: 'underline',
+      '&:hover': {
+        textDecoration: 'underline dotted',
+      },
+      '&:active, &:hover': {
+        outlineWidth: 0,
+      },
+      '&:not([href])': {
         color: 'inherit',
-        textDecoration: 'underline',
-        '&:hover': {
-          textDecoration: 'underline dotted',
-        },
-        '&:active, &:hover': {
-          outlineWidth: 0,
-        },
-        '&:not([href])': {
-          color: 'inherit',
-          textDecoration: 'none',
-        },
+        textDecoration: 'none',
       },
+    },
 
-      // Headings
-      'h1, h2, h3, h4, h5, h6': {
-        marginTop: 0,
-        marginBottom: kbnMarkdownSizeXS,
-      },
-      h1: {
-        fontSize: kbnMarkdownFontSizeXXL,
-        lineHeight: '1.333333em',
-        fontWeight: 300,
-      },
-      h2: {
-        fontSize: kbnMarkdownFontSizeXL,
-        lineHeight: '1.428571em',
-        fontWeight: 300,
-      },
-      h3: {
-        fontSize: kbnMarkdownFontSizeL,
-        lineHeight: '1.6em',
-        fontWeight: 600,
-      },
-      h4: {
-        fontSize: kbnMarkdownSize,
-        lineHeight: '1.5em',
-        fontWeight: 600,
-      },
-      h5: {
-        fontSize: kbnMarkdownFontSize,
-        lineHeight: '1.142857em',
-        fontWeight: 700,
-      },
-      h6: {
-        fontSize: kbnMarkdownFontSizeS,
-        lineHeight: '1.333333em',
-        fontWeight: 700,
-        textTransform: 'uppercase',
-      },
+    // Headings
+    'h1, h2, h3, h4, h5, h6': {
+      marginTop: 0,
+      marginBottom: kbnMarkdownSizeXS,
+    },
+    h1: {
+      fontSize: kbnMarkdownFontSizeXXL,
+      lineHeight: '1.333333em',
+      fontWeight: 300,
+    },
+    h2: {
+      fontSize: kbnMarkdownFontSizeXL,
+      lineHeight: '1.428571em',
+      fontWeight: 300,
+    },
+    h3: {
+      fontSize: kbnMarkdownFontSizeL,
+      lineHeight: '1.6em',
+      fontWeight: 600,
+    },
+    h4: {
+      fontSize: kbnMarkdownSize,
+      lineHeight: '1.5em',
+      fontWeight: 600,
+    },
+    h5: {
+      fontSize: kbnMarkdownFontSize,
+      lineHeight: '1.142857em',
+      fontWeight: 700,
+    },
+    h6: {
+      fontSize: kbnMarkdownFontSizeS,
+      lineHeight: '1.333333em',
+      fontWeight: 700,
+      textTransform: 'uppercase',
+    },
 
-      // Images
-      img: {
-        maxWidth: '100%',
-        boxSizing: 'content-box',
-        borderStyle: 'none',
-        pointerEvents: 'auto',
-      },
+    // Images
+    img: {
+      maxWidth: '100%',
+      boxSizing: 'content-box',
+      borderStyle: 'none',
+      pointerEvents: 'auto',
+    },
 
-      // Blockquotes
-      blockquote: {
-        padding: '0 1em',
-        borderLeft: `${kbnMarkdownSizeXXS} solid ${kbnMarkdownAlphaLightShade}`,
-      },
-      '&--reversed blockquote': {
-        borderLeftColor: `${kbnMarkdownAlphaLightShadeReversed}`,
-      },
+    // Blockquotes
+    blockquote: {
+      padding: '0 1em',
+      borderLeft: `${kbnMarkdownSizeXXS} solid ${kbnMarkdownAlphaLightShade}`,
+    },
+    '&--reversed blockquote': {
+      borderLeftColor: `${kbnMarkdownAlphaLightShadeReversed}`,
+    },
 
-      // Horizontal rules
-      hr: {
-        overflow: 'hidden',
-        background: 'transparent',
-        height: '2px',
-        padding: 0,
-        margin: `${kbnMarkdownSizeL} 0`,
-        backgroundColor: `${kbnMarkdownAlphaLightShade}`,
-        border: 0,
+    // Horizontal rules
+    hr: {
+      overflow: 'hidden',
+      background: 'transparent',
+      height: '2px',
+      padding: 0,
+      margin: `${kbnMarkdownSizeL} 0`,
+      backgroundColor: `${kbnMarkdownAlphaLightShade}`,
+      border: 0,
 
-        '::before': {
-          display: 'table',
-          content: '',
-        },
-        '::after': {
-          display: 'table',
-          clear: 'both',
-          content: '',
-        },
+      '::before': {
+        display: 'table',
+        content: '"',
       },
-      '&--reversed hr': {
-        backgroundColor: `${kbnMarkdownAlphaLightShadeReversed}`,
+      '::after': {
+        display: 'table',
+        clear: 'both',
+        content: '"',
       },
+    },
+    '&--reversed hr': {
+      backgroundColor: `${kbnMarkdownAlphaLightShadeReversed}`,
+    },
 
-      // Lists
-      'ul, ol': {
-        paddingLeft: `${kbnMarkdownFontSizeL}`,
-        marginTop: 0,
-        marginBottom: `${kbnMarkdownFontSize}`,
-      },
-      ul: {
-        listStyleType: 'disc',
-      },
-      ol: {
-        listStyleType: 'decimal',
-      },
-      'ul ul': {
-        listStyleType: 'circle',
-      },
-      'ol ol, ul ol': {
-        listStyleType: 'lower-roman',
-      },
-      'ul ul ol, ul ol ol, ol ul ol, ol ol ol': {
-        listStyleType: 'lower-alpha',
-      },
-      dd: {
-        marginLeft: 0,
-      },
-      // Nested lists with no margin
-      'ul ul, ul ol, ol ol, ol ul': {
-        marginTop: 0,
-        marginBottom: 0,
-      },
-      'li > p': {
-        marginBottom: `${kbnMarkdownSizeXS}`,
-      },
-      'li + li': {
-        marginTop: `${kbnMarkdownSizeXXS}`,
-      },
+    // Lists
+    'ul, ol': {
+      paddingLeft: `${kbnMarkdownFontSizeL}`,
+      marginTop: 0,
+      marginBottom: `${kbnMarkdownFontSize}`,
+    },
+    ul: {
+      listStyleType: 'disc',
+    },
+    ol: {
+      listStyleType: 'decimal',
+    },
+    'ul ul': {
+      listStyleType: 'circle',
+    },
+    'ol ol, ul ol': {
+      listStyleType: 'lower-roman',
+    },
+    'ul ul ol, ul ol ol, ol ul ol, ol ol ol': {
+      listStyleType: 'lower-alpha',
+    },
+    dd: {
+      marginLeft: 0,
+    },
+    // Nested lists with no margin
+    'ul ul, ul ol, ol ol, ol ul': {
+      marginTop: 0,
+      marginBottom: 0,
+    },
+    'li > p': {
+      marginBottom: `${kbnMarkdownSizeXS}`,
+    },
+    'li + li': {
+      marginTop: `${kbnMarkdownSizeXXS}`,
+    },
 
-      // Tables
-      table: {
-        display: 'block',
-        width: '100%',
-        overflow: 'auto',
-        borderLeft: `1px solid ${kbnMarkdownAlphaLightShade}`,
-        borderSpacing: 0,
-        borderCollapse: 'collapse',
+    // Tables
+    table: {
+      display: 'block',
+      width: '100%',
+      overflow: 'auto',
+      borderLeft: `1px solid ${kbnMarkdownAlphaLightShade}`,
+      borderSpacing: 0,
+      borderCollapse: 'collapse',
+    },
+    '&--reversed table': {
+      borderLeftColor: `${kbnMarkdownAlphaLightShadeReversed}`,
+    },
+    'td, th': {
+      padding: 0,
+    },
+    'table th, table td': {
+      padding: `${kbnMarkdownSizeXXS} ${kbnMarkdownSizeS}`,
+      borderTop: `1px solid ${kbnMarkdownAlphaLightShade}`,
+      borderBottom: `1px solid ${kbnMarkdownAlphaLightShade}`,
+      '&:last-child': {
+        borderRight: `1px solid ${kbnMarkdownAlphaLightShade}`,
       },
-      '&--reversed table': {
-        borderLeftColor: `${kbnMarkdownAlphaLightShadeReversed}`,
-      },
-      'td, th': {
-        padding: 0,
-      },
-      'table th, table td': {
-        padding: `${kbnMarkdownSizeXXS} ${kbnMarkdownSizeS}`,
-        borderTop: `1px solid ${kbnMarkdownAlphaLightShade}`,
-        borderBottom: `1px solid ${kbnMarkdownAlphaLightShade}`,
-        '&:last-child': {
-          borderRight: `1px solid ${kbnMarkdownAlphaLightShade}`,
-        },
-      },
-      '&--reversed table th, &--reversed table td': {
-        borderColor: `${kbnMarkdownAlphaLightShadeReversed}`,
-      },
-      'table tr': {
-        backgroundColor: 'transparent',
-        borderTop: `1px solid ${kbnMarkdownAlphaLightShade}`,
-      },
-      '&--reversed table tr': {
-        borderTopColor: `${kbnMarkdownAlphaLightShadeReversed}`,
-      },
+    },
+    '&--reversed table th, &--reversed table td': {
+      borderColor: `${kbnMarkdownAlphaLightShadeReversed}`,
+    },
+    'table tr': {
+      backgroundColor: 'transparent',
+      borderTop: `1px solid ${kbnMarkdownAlphaLightShade}`,
+    },
+    '&--reversed table tr': {
+      borderTopColor: `${kbnMarkdownAlphaLightShadeReversed}`,
+    },
 
-      // Code blocks
-      'code, pre': {
-        marginBottom: `${kbnMarkdownSizeXS}`,
-        fontFamily: `'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace`,
-        fontSize: `${kbnMarkdownFontSizeS}`,
+    // Code blocks
+    'code, pre': {
+      marginBottom: `${kbnMarkdownSizeXS}`,
+      fontFamily: `'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace`,
+      fontSize: `${kbnMarkdownFontSizeS}`,
+    },
+    code: {
+      padding: `${kbnMarkdownSizeXXS} 0`,
+      margin: 0,
+      backgroundColor: `${kbnMarkdownAlphaLightestShade}`,
+      borderRadius: `${kbnMarkdownSizeXXS}`,
+      '&::before, &::after': {
+        letterSpacing: '-.2em',
       },
-      code: {
-        padding: `${kbnMarkdownSizeXXS} 0`,
-        margin: 0,
-        backgroundColor: `${kbnMarkdownAlphaLightestShade}`,
-        borderRadius: `${kbnMarkdownSizeXXS}`,
-        '&::before, &::after': {
-          letterSpacing: '-.2em',
-        },
-      },
-      '&--reversed code': {
-        backgroundColor: `${kbnMarkdownAlphaLightestShadeReversed}`,
-      },
+    },
+    '&--reversed code': {
+      backgroundColor: `${kbnMarkdownAlphaLightestShadeReversed}`,
+    },
 
-      pre: {
-        padding: `${kbnMarkdownSize}`,
-        overflow: 'auto',
-        fontSize: `${kbnMarkdownFontSizeS}`,
-        lineHeight: '1.333333em',
-        backgroundColor: `${kbnMarkdownAlphaLightestShade}`,
-        borderRadius: `${kbnMarkdownSizeXXS}`,
-        wordWrap: 'normal',
-      },
-      '&--reversed pre': {
-        backgroundColor: `${kbnMarkdownAlphaLightestShadeReversed}`,
-      },
-      'pre code': {
-        display: 'inline',
-        maxWidth: 'auto',
-        padding: 0,
-        overflow: 'visible',
-        lineHeight: 'inherit',
-        wordWrap: 'normal',
-        whiteSpace: 'pre',
-        backgroundColor: 'transparent',
-        border: 0,
-        '&::before, &::after': {
-          content: 'normal',
-        },
+    pre: {
+      padding: `${kbnMarkdownSize}`,
+      overflow: 'auto',
+      fontSize: `${kbnMarkdownFontSizeS}`,
+      lineHeight: '1.333333em',
+      backgroundColor: `${kbnMarkdownAlphaLightestShade}`,
+      borderRadius: `${kbnMarkdownSizeXXS}`,
+      wordWrap: 'normal',
+    },
+    '&--reversed pre': {
+      backgroundColor: `${kbnMarkdownAlphaLightestShadeReversed}`,
+    },
+    'pre code': {
+      display: 'inline',
+      maxWidth: 'auto',
+      padding: 0,
+      overflow: 'visible',
+      lineHeight: 'inherit',
+      wordWrap: 'normal',
+      whiteSpace: 'pre',
+      backgroundColor: 'transparent',
+      border: 0,
+      '&::before, &::after': {
+        content: 'normal',
       },
     },
   });
