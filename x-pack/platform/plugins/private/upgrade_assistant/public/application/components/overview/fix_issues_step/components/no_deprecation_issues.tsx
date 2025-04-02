94530e2652d17205c35ba3f1ceeaceb99964f178
Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>
2025-04-02 14:08:00 +0200

    [UA] Update the styling of Elasticsearch and Kibana cards (#216604)
    Fixes https://github.com/elastic/kibana/issues/211502
    
    ## Summary
    This PR introduces some UI changes in the Upgrade Assistant deprecation
    cards. Since the `EuiStat` component had an `isLoading` prop and this
    component is no longer used, the loading state is now handled with some
    spinner. No changes have been made for the error state.
    
    Also removed the `_deprecation_issues_panel.scss` file since this css
    classes are not longer needed.

--- x-pack/platform/plugins/private/upgrade_assistant/public/application/components/overview/fix_issues_step/components/no_deprecation_issues.tsx
+++ x-pack/platform/plugins/private/upgrade_assistant/public/application/components/overview/fix_issues_step/components/no_deprecation_issues.tsx
@@ -10,34 +10,25 @@ import { EuiFlexGroup, EuiFlexItem, EuiText, EuiIcon } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
 const i18nTexts = {
-  noPartialDeprecationIssuesText: i18n.translate(
-    'xpack.upgradeAssistant.noPartialDeprecationsMessage',
-    {
-      defaultMessage: 'None',
-    }
-  ),
   noDeprecationIssuesText: i18n.translate('xpack.upgradeAssistant.noDeprecationsMessage', {
     defaultMessage: 'No issues',
   }),
 };
 
 interface Props {
-  isPartial?: boolean;
   'data-test-subj'?: string;
 }
 
 export const NoDeprecationIssues: FunctionComponent<Props> = (props) => {
-  const { isPartial = false } = props;
-
   return (
     <EuiText color="success">
       <EuiFlexGroup gutterSize="s" alignItems="center">
         <EuiFlexItem grow={false}>
-          <EuiIcon type="check" />
+          <EuiIcon type="checkInCircleFilled" />
         </EuiFlexItem>
 
         <EuiFlexItem grow={false} data-test-subj={props['data-test-subj']}>
-          {isPartial ? i18nTexts.noPartialDeprecationIssuesText : i18nTexts.noDeprecationIssuesText}
+          {i18nTexts.noDeprecationIssuesText}
         </EuiFlexItem>
       </EuiFlexGroup>
     </EuiText>
