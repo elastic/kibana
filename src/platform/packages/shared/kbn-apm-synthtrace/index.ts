6505a1f13215f3186e0d7267b99f5aaf943847cf
Carlos Crespo <crespocarlos@users.noreply.github.com>
2025-04-02 15:27:20 +0200

    [Synthtrace] Fluent API for APM otel (#216099)
    closes https://github.com/elastic/kibana/issues/216032
    
    ## Summary
    
    Create a fluent API for APM Otel. The interface is similar to the one
    currently used to create scenarios for elastic APM data.
    
    This will make it easier to create tests and more flexible synthetic
    scenarios to cover Otel specificities.
    
    ### How to test
    
    - run `node scripts/synthtrace otel_simple_trace.ts --live --uniqueIds
    --clean`
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    (cherry picked from commit 03f4b4892dc548917c9daeda738a2cbe23b4cd82)
    
    # Conflicts:
    #	src/platform/packages/shared/kbn-apm-synthtrace-client/index.ts
    #	src/platform/packages/shared/kbn-apm-synthtrace-client/src/lib/otel/error.ts
    #	src/platform/packages/shared/kbn-apm-synthtrace-client/src/lib/otel/index.ts
    #	src/platform/packages/shared/kbn-apm-synthtrace-client/src/lib/otel/metric.ts
    #	src/platform/packages/shared/kbn-apm-synthtrace-client/src/lib/otel/transaction.ts
    #	src/platform/packages/shared/kbn-apm-synthtrace/src/cli/utils/get_clients.ts
    #	src/platform/packages/shared/kbn-apm-synthtrace/src/scenarios/otel_simple_trace.ts
    #	x-pack/solutions/observability/plugins/apm/ftr_e2e/cypress/fixtures/synthtrace/adservice_edot.ts

--- src/platform/packages/shared/kbn-apm-synthtrace/index.ts
+++ src/platform/packages/shared/kbn-apm-synthtrace/index.ts
@@ -18,7 +18,7 @@ export { LogsSynthtraceEsClient } from './src/lib/logs/logs_synthtrace_es_client
 export { EntitiesSynthtraceEsClient } from './src/lib/entities/entities_synthtrace_es_client';
 export { EntitiesSynthtraceKibanaClient } from './src/lib/entities/entities_synthtrace_kibana_client';
 export { SyntheticsSynthtraceEsClient } from './src/lib/synthetics/synthetics_synthtrace_es_client';
-export { OtelSynthtraceEsClient } from './src/lib/otel/otel_synthtrace_es_client';
+export { OtelSynthtraceEsClient } from './src/lib/apm/client/apm_otel_synthtrace_es_client/otel_synthtrace_es_client';
 export {
   addObserverVersionTransform,
   deleteSummaryFieldTransform,
