cf1994a6ed72079631a8d247d9ccaf45651c8756
alex prozorov <alex.prozorov@elastic.co>
2025-04-06 12:21:26 +0300

    make actionable badge hover items UI changes to be aligned with data grid styling
    adjust flyout font style

--- x-pack/solutions/security/plugins/cloud_security_posture/public/pages/vulnerabilities/types.ts
+++ x-pack/solutions/security/plugins/cloud_security_posture/public/pages/vulnerabilities/types.ts
@@ -19,3 +19,9 @@ export interface Vector {
   vector: string;
   score: number | undefined;
 }
+
+export type AddFieldFilterHandler = (
+  field: string,
+  value: unknown,
+  type: '+' | '-'
+) => void;
