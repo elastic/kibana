04cfda94f67d0e93eb4dbbf0fab8ceda6be461f7
machadoum <pablo.nevesmachado@elastic.co>
2025-04-07 16:35:28 +0200

    Bootstrap Privileged User Monitoring page layout

---
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring_onboarding/add_data_source.tsx
@@ -0,0 +1,164 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React from 'react';
+import {
+  EuiCard,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiHorizontalRule,
+  EuiIcon,
+  EuiPanel,
+  EuiSpacer,
+  EuiText,
+  EuiTitle,
+} from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n-react';
+import entraIdIcon from '../../icons/entra_id.svg';
+import oktaIcon from '../../icons/okta.svg';
+import activeDirectoryIcon from '../../icons/active_directory.svg';
+
+export const AddDataSourcePanel = () => (
+  <EuiPanel paddingSize="xl" hasShadow={false} hasBorder={false}>
+    <EuiTitle>
+      <h3>
+        <FormattedMessage
+          id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.title"
+          defaultMessage="Add data source of your privileged users"
+        />
+      </h3>
+    </EuiTitle>
+    <EuiSpacer size="m" />
+    <EuiText size="m">
+      <p>
+        <FormattedMessage
+          id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.description"
+          defaultMessage="To get started, define your privileged users by adding an integration with your organization’s user identities, select an index with the relevant data, or import your list of privileged users from a CSV file."
+        />
+      </p>
+    </EuiText>
+
+    <EuiSpacer size="xl" />
+
+    <EuiFlexGroup direction="row" justifyContent="spaceBetween">
+      <EuiFlexItem grow={1}>
+        <EuiCard
+          layout="horizontal"
+          icon={<EuiIcon size="xl" type={oktaIcon} />}
+          title={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.okta.title"
+              defaultMessage="Okta"
+            />
+          }
+          description={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.okta.description"
+              defaultMessage="Collect user identities and event logs from Okta with Elastic Agent."
+            />
+          }
+          onClick={() => {}}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={1}>
+        <EuiCard
+          layout="horizontal"
+          icon={<EuiIcon size="xl" type={entraIdIcon} />}
+          title={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.entra.title"
+              defaultMessage="Microsoft Entra ID"
+            />
+          }
+          description={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.entra.description"
+              defaultMessage="Collect user identities and event logs from Microsoft Entra ID with Elastic Agent."
+            />
+          }
+          onClick={() => {}}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={1}>
+        <EuiCard
+          layout="horizontal"
+          icon={<EuiIcon size="xl" type={activeDirectoryIcon} />}
+          title={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.activeDirectory.title"
+              defaultMessage="Active Directory"
+            />
+          }
+          description={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.activeDirectory.description"
+              defaultMessage="Collect user identities from Active Directory with Elastic Agent."
+            />
+          }
+          onClick={() => {}}
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+    <EuiSpacer size="m" />
+    <EuiFlexGroup alignItems="center" justifyContent="spaceAround">
+      <EuiFlexItem grow={true}>
+        <EuiHorizontalRule size="full" margin="none" />
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <FormattedMessage
+          id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.or"
+          defaultMessage="OR"
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={true}>
+        <EuiHorizontalRule size="full" margin="none" />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+    <EuiSpacer size="m" />
+    <EuiFlexGroup direction="row" justifyContent="spaceBetween">
+      <EuiFlexItem grow={1}>
+        <EuiCard
+          layout="horizontal"
+          icon={<EuiIcon size="l" type="indexOpen" />}
+          titleSize="xs"
+          title={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.index.title"
+              defaultMessage="Index"
+            />
+          }
+          description={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.index.description"
+              defaultMessage="Select an index that contains relevant user activity data"
+            />
+          }
+          onClick={() => {}}
+        />
+      </EuiFlexItem>
+      <EuiFlexItem grow={1}>
+        <EuiCard
+          layout="horizontal"
+          icon={<EuiIcon size="l" type="importAction" />}
+          titleSize="xs"
+          title={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.file.title"
+              defaultMessage="File"
+            />
+          }
+          description={
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.addDataSource.file.description"
+              defaultMessage="Import a list of privileged users from a CSV, TXT, or TSV file"
+            />
+          }
+          onClick={() => {}}
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  </EuiPanel>
+);
