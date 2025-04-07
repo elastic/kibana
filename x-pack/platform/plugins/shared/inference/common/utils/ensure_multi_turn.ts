2580e0ed62328eea90f04e888b6b283a9cf6bac4
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2025-04-07 13:03:44 +0100

    remove multi turn change

--- x-pack/platform/plugins/shared/inference/common/utils/ensure_multi_turn.ts
+++ x-pack/platform/plugins/shared/inference/common/utils/ensure_multi_turn.ts
@@ -7,50 +7,8 @@
 
 import { Message, MessageRole } from '@kbn/inference-common';
 
-type MessageRoleSequenceResult =
-  | {
-      roleSequenceValid: true;
-    }
-  | {
-      roleSequenceValid: false;
-      intermediaryRole: MessageRole.User | MessageRole.Assistant;
-    };
-
-/**
- * Two consecutive messages with USER role or two consecutive messages with ASSISTANT role are not allowed.
- * Consecutive messages with TOOL role are allowed.
- */
-function checkMessageRoleSequenceValid(
-  prevMessage: Message | undefined,
-  message: Message
-): MessageRoleSequenceResult {
-  if (!prevMessage) {
-    return {
-      roleSequenceValid: true,
-    };
-  }
-
-  if (prevMessage.role === MessageRole.User && message.role === MessageRole.User) {
-    return {
-      roleSequenceValid: false,
-      intermediaryRole: MessageRole.Assistant,
-    };
-  }
-  if (prevMessage.role === MessageRole.Assistant && message.role === MessageRole.Assistant) {
-    return {
-      roleSequenceValid: false,
-      intermediaryRole: MessageRole.User,
-    };
-  }
-  if (prevMessage.role === MessageRole.Tool && message.role === MessageRole.Tool) {
-    return {
-      roleSequenceValid: true,
-    };
-  }
-
-  return {
-    roleSequenceValid: true,
-  };
+function isUserMessage(message: Message): boolean {
+  return message.role !== MessageRole.Assistant;
 }
 
 export function ensureMultiTurn(messages: Message[]): Message[] {
@@ -59,11 +17,10 @@ export function ensureMultiTurn(messages: Message[]): Message[] {
   messages.forEach((message) => {
     const prevMessage = next[next.length - 1];
 
-    const result = checkMessageRoleSequenceValid(prevMessage, message);
-    if (!result.roleSequenceValid) {
+    if (prevMessage && isUserMessage(prevMessage) === isUserMessage(message)) {
       next.push({
         content: '-',
-        role: result.intermediaryRole,
+        role: isUserMessage(message) ? MessageRole.Assistant : MessageRole.User,
       });
     }
 
