elastic/kibana

commit be8e6246562c01d45f6231430f5328e514785a48

[Upgrade assistant] Filter deprecation issues by status (#211581)
Fixes https://github.com/elastic/kibana/issues/208262

## Summary
On the Elasticsearch list of deprecations in Upgrade Assistant, replace
the `Critical` button filter with a Dropdown allowing to select between
`Critical` and `Warning`.



https://github.com/user-attachments/assets/c332d876-a161-4f6f-bb2c-d677b4d445a4

x-pack/platform/plugins/private/upgrade_assistant/__jest__/client_integration/es_deprecations/es_deprecations.helpers.ts
@@ -50,18 +50,28 @@ const createActions = (testBed: TestBed) => {
     },
   };
 
-  const searchBar = {
-    clickTypeFilterDropdownAt: async (index: number) => {
-      await act(async () => {
-        // EUI doesn't support data-test-subj's on the filter buttons, so we must access via CSS selector
-        find('searchBarContainer')
-          .find('.euiPopover')
-          .find('button.euiFilterButton')
-          .at(index)
-          .simulate('click');
-      });
+  const clickFilterByIndex = async (index: number) => {
+    await act(async () => {
+      // EUI doesn't support data-test-subj's on the filter buttons, so we must access via CSS selector
+      find('searchBarContainer')
+        .find('.euiPopover')
+        .find('button.euiFilterButton')
+        .at(index)
+        .simulate('click');
+    });
 
-      component.update();
+    component.update();
+
+    // Wait for the filter dropdown to be displayed
+    await new Promise(requestAnimationFrame);
+  };
+
+  const searchBar = {
+    clickTypeFilterDropdown: async () => {
+      await clickFilterByIndex(1); // Type filter is the second filter button
+    },
+    clickStatusFilterDropdown: async () => {
+      await clickFilterByIndex(0); // Status filter is the first filter button
     },
     setSearchInputValue: async (searchValue: string) => {
       await act(async () => {
@@ -72,10 +82,17 @@ const createActions = (testBed: TestBed) => {
 
       component.update();
     },
-    clickCriticalFilterButton: async () => {
+    clickFilterByTitle: async (title: string) => {
+      // We need to read the document "body" as the filter dropdown (an EuiSelectable)
+      // is added in a portalled popover and not inside the component DOM tree.
+      const filterButton: HTMLButtonElement | null = document.body.querySelector(
+        `.euiSelectableListItem[title=${title}]`
+      );
+
+      expect(filterButton).not.toBeNull();
+
       await act(async () => {
-        // EUI doesn't support data-test-subj's on the filter buttons, so we must access via CSS selector
-        find('searchBarContainer').find('button.euiFilterButton').at(0).simulate('click');
+        filterButton!.click();
       });
 
       component.update();

x-pack/platform/plugins/private/upgrade_assistant/__jest__/client_integration/es_deprecations/deprecations_list.test.ts
@@ -128,10 +128,11 @@ describe('ES deprecations table', () => {
   });
 
   describe('search bar', () => {
-    it('filters results by "critical" status', async () => {
+    it('filters results by status', async () => {
       const { find, actions } = testBed;
 
-      await actions.searchBar.clickCriticalFilterButton();
+      await actions.searchBar.clickStatusFilterDropdown();
+      await actions.searchBar.clickFilterByTitle('Critical');
 
       const criticalDeprecations = esDeprecationsMockResponse.migrationsDeprecations.filter(
         (deprecation) => deprecation.isCritical
@@ -139,7 +140,7 @@ describe('ES deprecations table', () => {
 
       expect(find('deprecationTableRow').length).toEqual(criticalDeprecations.length);
 
-      await actions.searchBar.clickCriticalFilterButton();
+      await actions.searchBar.clickFilterByTitle('Critical'); // Reset filter
 
       expect(find('deprecationTableRow').length).toEqual(
         esDeprecationsMockResponse.migrationsDeprecations.length
@@ -147,23 +148,11 @@ describe('ES deprecations table', () => {
     });
 
     it('filters results by type', async () => {
-      const { component, find, actions } = testBed;
+      const { find, actions } = testBed;
 
-      await actions.searchBar.clickTypeFilterDropdownAt(0);
+      await actions.searchBar.clickTypeFilterDropdown();
 
-      // We need to read the document "body" as the filter dropdown (an EuiSelectable)
-      // is added in a portalled popover and not inside the component DOM tree.
-      const clusterTypeFilterButton: HTMLButtonElement | null = document.body.querySelector(
-        '.euiSelectableList .euiSelectableListItem'
-      );
-
-      expect(clusterTypeFilterButton).not.toBeNull();
-
-      await act(async () => {
-        clusterTypeFilterButton!.click();
-      });
-
-      component.update();
+      await actions.searchBar.clickFilterByTitle('Cluster');
 
       const clusterDeprecations = esDeprecationsMockResponse.migrationsDeprecations.filter(
         (deprecation) => deprecation.type === 'cluster_settings'
@@ -272,7 +261,8 @@ describe('ES deprecations table', () => {
         (deprecation) => deprecation.isCritical
       );
 
-      await actions.searchBar.clickCriticalFilterButton();
+      await actions.searchBar.clickStatusFilterDropdown();
+      await actions.searchBar.clickFilterByTitle('Critical');
 
       // Only 40 critical deprecations, so only one page should show
       expect(find('esDeprecationsPagination').find('.euiPagination__item').length).toEqual(1);

x-pack/platform/plugins/private/upgrade_assistant/public/application/components/es_deprecations/es_deprecations_table.tsx
@@ -55,10 +55,10 @@ const i18nTexts = {
   typeFilterLabel: i18n.translate('xpack.upgradeAssistant.esDeprecations.table.typeFilterLabel', {
     defaultMessage: 'Type',
   }),
-  criticalFilterLabel: i18n.translate(
-    'xpack.upgradeAssistant.esDeprecations.table.criticalFilterLabel',
+  statusFilterLabel: i18n.translate(
+    'xpack.upgradeAssistant.esDeprecations.table.statusFilterLabel',
     {
-      defaultMessage: 'Critical',
+      defaultMessage: 'Status',
     }
   ),
   searchPlaceholderLabel: i18n.translate(
@@ -165,6 +165,21 @@ const getSortedItems = (deprecations: EnrichedDeprecationInfo[], sortConfig: Sor
   return isSortAscending ? sorted : sorted.reverse();
 };
 
+const statusFilterOptions = [
+  {
+    value: true,
+    name: i18n.translate('xpack.upgradeAssistant.esDeprecations.table.filter.critical', {
+      defaultMessage: 'Critical',
+    }),
+  },
+  {
+    value: false,
+    name: i18n.translate('xpack.upgradeAssistant.esDeprecations.table.filter.warning', {
+      defaultMessage: 'Warning',
+    }),
+  },
+];
+
 export const EsDeprecationsTable: React.FunctionComponent<Props> = ({
   deprecations = [],
   reload,
@@ -244,9 +259,11 @@ export const EsDeprecationsTable: React.FunctionComponent<Props> = ({
             }}
             filters={[
               {
-                type: 'is',
+                type: 'field_value_selection',
                 field: 'isCritical',
-                name: i18nTexts.criticalFilterLabel,
+                name: i18nTexts.statusFilterLabel,
+                multiSelect: false,
+                options: statusFilterOptions,
               },
               {
                 type: 'field_value_selection',

commit 6774014cf3143471dfa5e53816277caa22aa9a51

[UA] Wait filter button to exists (#217303)
Fixes https://github.com/elastic/kibana/issues/214423

## Summary
Fix flaky test by waiting for the filterButton to be available. Probably
caused by https://github.com/elastic/kibana/pull/211581

x-pack/platform/plugins/private/upgrade_assistant/__jest__/client_integration/es_deprecations/es_deprecations.helpers.ts
@@ -8,6 +8,7 @@ import { act } from 'react-dom/test-utils';
 
 import { registerTestBed, TestBed, AsyncTestBedConfig } from '@kbn/test-jest-helpers';
 import { HttpSetup } from '@kbn/core/public';
+import { waitFor } from '@testing-library/dom';
 import { EsDeprecations } from '../../../public/application/components';
 import { WithAppDependencies } from '../helpers';
 
@@ -95,13 +96,11 @@ const createActions = (testBed: TestBed) => {
     clickFilterByTitle: async (title: string) => {
       // We need to read the document "body" as the filter dropdown (an EuiSelectable)
       // is added in a portalled popover and not inside the component DOM tree.
-      const filterButton: HTMLButtonElement | null = document.body.querySelector(
-        `.euiSelectableListItem[title=${title}]`
-      );
-
-      expect(filterButton).not.toBeNull();
-
-      await act(async () => {
+      await waitFor(() => {
+        const filterButton: HTMLButtonElement | null = document.body.querySelector(
+          `.euiSelectableListItem[title=${title}]`
+        );
+        expect(filterButton).not.toBeNull();
         filterButton!.click();
       });
 