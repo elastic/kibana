c10470725f25cedff92993f34a27bea366610249
drewdaemon <drew.tate@elastic.co>
2025-04-07 08:20:06 -0600

    Fix ranges problem once and for all

--- src/platform/packages/shared/kbn-monaco/src/languages/esql/lib/shared/utils.test.ts
+++ src/platform/packages/shared/kbn-monaco/src/languages/esql/lib/shared/utils.test.ts
@@ -9,30 +9,17 @@
 import { offsetRangeToMonacoRange } from './utils';
 
 describe('offsetRangeToMonacoRange', () => {
-  test('should convert offset range to monaco range when the cursor is not at the end', () => {
-    const expression = 'FROM test | WHERE test == | LIMIT 1';
-    const range = { start: 26, end: 26 };
-    const monacoRange = offsetRangeToMonacoRange(expression, range);
-
-    expect(monacoRange).toEqual({
-      startColumn: 26,
-      endColumn: 26,
-      startLineNumber: 1,
-      endLineNumber: 1,
-    });
+  test('single line', () => {
+    const expression = 'FROM kibana_sample_data_logs';
+    const range = offsetRangeToMonacoRange(expression, { start: 5, end: 28 });
+    expect(range).toEqual({ startColumn: 6, endColumn: 28, startLineNumber: 1, endLineNumber: 1 });
   });
 
-  test('should convert offset range to monaco range when the cursor is at the end', () => {
-    const expression = 'FROM test | WHERE test == 1 | LIMIT 1';
-    const range = { start: 37, end: 37 };
-    const monacoRange = offsetRangeToMonacoRange(expression, range);
-
-    expect(monacoRange).toEqual({
-      startColumn: 37,
-      endColumn: 37,
-      startLineNumber: 1,
-      endLineNumber: 1,
-    });
+  test('next line', () => {
+    const expression = `FROM kibana_sample_data_logs
+| KEEP foo`;
+    const range = offsetRangeToMonacoRange(expression, { start: 36, end: 39 });
+    expect(range).toEqual({ startColumn: 8, endColumn: 10, startLineNumber: 2, endLineNumber: 2 });
   });
 
   test('should convert offset range to monaco range for multiple lines query when the cursor is not at the end', () => {
@@ -42,9 +29,25 @@ describe('offsetRangeToMonacoRange', () => {
 
     expect(monacoRange).toEqual({
       startColumn: 17,
-      endColumn: 18,
+      endColumn: 17,
       startLineNumber: 2,
       endLineNumber: 2,
     });
+  });
+
+  test('returns undefined if the start is past the end of the expression', () => {
+    const expression = 'FROM test | WHERE test == 1 | LIMIT 1';
+    const range = { start: 37, end: 37 };
+    const monacoRange = offsetRangeToMonacoRange(expression, range);
+
+    expect(monacoRange).toEqual(undefined);
+  });
+
+  test('should not create a range if start and end are equal', () => {
+    const expression = 'FROM test | WHERE test == | LIMIT 1';
+    const range = { start: 26, end: 26 };
+    const monacoRange = offsetRangeToMonacoRange(expression, range);
+
+    expect(monacoRange).toEqual(undefined);
   });
 });
