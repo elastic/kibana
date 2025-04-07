9d7c6b626360fbfae0587e9e8509294621a6ce6e
Andrew Cholakian <andrew@andrewvc.com>
2025-04-07 11:54:26 -0500

    proper capitalization

--- x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/test_now_mode/browser/browser_test_results.tsx
+++ x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/test_now_mode/browser/browser_test_results.tsx
@@ -187,7 +187,7 @@ export const FAILED_TO_RUN = i18n.translate('xpack.synthetics.monitorManagement.
 
 export const ERROR_RUNNING_TEST = i18n.translate('xpack.synthetics.testRun.testErrorLabel', {
   defaultMessage:
-    'This synthetics script could not be run as written. Debug this locally by running elastic-synthetics with the --dry-run flag.',
+    'This Elastic Synthetics script could not be run as written. Debug this locally by running elastic-synthetics with the --dry-run flag.',
 });
 
 const LOADING_STEPS = i18n.translate('xpack.synthetics.monitorManagement.loadingSteps', {
