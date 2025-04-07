cf0977365c335093bad97bef27153304337ec031
Robert Oskamp <robert.oskamp@elastic.co>
2025-04-07 15:08:21 +0200

    Update scout tags

--- src/platform/packages/shared/kbn-scout/src/playwright/tags.ts
+++ src/platform/packages/shared/kbn-scout/src/playwright/tags.ts
@@ -7,9 +7,10 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-const SERVERLESS_ONLY = ['@svlSecurity', '@svlOblt', '@svlSearch'];
+const SERVERLESS_ONLY = ['@svlSecurity', '@svlOblt', '@svlSearch', '@svlChat'];
 const ESS_ONLY = ['@ess'];
-const DEPLOYMENT_AGNOSTIC = SERVERLESS_ONLY.concat(ESS_ONLY);
+// svlChat is truly serverless only and doesn't have a stateful counterpart
+const DEPLOYMENT_AGNOSTIC = SERVERLESS_ONLY.filter((t) => t !== '@svlChat').concat(ESS_ONLY);
 const PERFORMANCE = ['@perf'];
 
 export const tags = {
