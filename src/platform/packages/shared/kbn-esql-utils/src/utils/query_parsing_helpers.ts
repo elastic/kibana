bf30cd6d69d297b01bf40f0dd97c784bed93a335
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-04-08 07:37:25 +0200

    [8.x] [ES|QL] Update grammars (#217277) (#217326)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[ES|QL] Update grammars
    (#217277)](https://github.com/elastic/kibana/pull/217277)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Kibana
    Machine","email":"42973632+kibanamachine@users.noreply.github.com"},"sourceCommit":{"committedDate":"2025-04-07T13:25:32Z","message":"[ES|QL]
    Update grammars (#217277)\n\nThis PR updates the ES|QL grammars (lexer
    and parser) to match the\nlatest version in
    Elasticsearch.\n\n---------\n\nCo-authored-by: Stratoula Kalafateli
    <efstratia.kalafateli@elastic.co>\nCo-authored-by: Vadim Kibana
    <82822460+vadimkibana@users.noreply.github.com>","sha":"338aa07e0828fe5b7f2a5049c16399abe365777a","branchLabelMapping":{"^v9.1.0$":"main","^v8.19.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","Feature:ES|QL","Team:ESQL","backport:version","v9.1.0","v8.19.0"],"title":"[ES|QL]
    Update
    grammars","number":217277,"url":"https://github.com/elastic/kibana/pull/217277","mergeCommit":{"message":"[ES|QL]
    Update grammars (#217277)\n\nThis PR updates the ES|QL grammars (lexer
    and parser) to match the\nlatest version in
    Elasticsearch.\n\n---------\n\nCo-authored-by: Stratoula Kalafateli
    <efstratia.kalafateli@elastic.co>\nCo-authored-by: Vadim Kibana
    <82822460+vadimkibana@users.noreply.github.com>","sha":"338aa07e0828fe5b7f2a5049c16399abe365777a"}},"sourceBranch":"main","suggestedTargetBranches":["8.x"],"targetPullRequestStates":[{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/217277","number":217277,"mergeCommit":{"message":"[ES|QL]
    Update grammars (#217277)\n\nThis PR updates the ES|QL grammars (lexer
    and parser) to match the\nlatest version in
    Elasticsearch.\n\n---------\n\nCo-authored-by: Stratoula Kalafateli
    <efstratia.kalafateli@elastic.co>\nCo-authored-by: Vadim Kibana
    <82822460+vadimkibana@users.noreply.github.com>","sha":"338aa07e0828fe5b7f2a5049c16399abe365777a"}},{"branch":"8.x","label":"v8.19.0","branchLabelMappingKey":"^v8.19.0$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    Co-authored-by: Kibana Machine <42973632+kibanamachine@users.noreply.github.com>

--- src/platform/packages/shared/kbn-esql-utils/src/utils/query_parsing_helpers.ts
+++ src/platform/packages/shared/kbn-esql-utils/src/utils/query_parsing_helpers.ts
@@ -25,7 +25,7 @@ const DEFAULT_ESQL_LIMIT = 1000;
 // retrieves the index pattern from the aggregate query for ES|QL using ast parsing
 export function getIndexPatternFromESQLQuery(esql?: string) {
   const { ast } = parse(esql);
-  const sourceCommand = ast.find(({ name }) => ['from', 'metrics'].includes(name));
+  const sourceCommand = ast.find(({ name }) => ['from', 'ts'].includes(name));
   const args = (sourceCommand?.args ?? []) as ESQLSource[];
   const indices = args.filter((arg) => arg.sourceType === 'index');
   return indices?.map((index) => index.name).join(',');
