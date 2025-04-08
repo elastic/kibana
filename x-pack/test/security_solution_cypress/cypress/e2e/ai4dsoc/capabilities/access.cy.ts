e9c9770a1458de24755f2b911401519072cf6ec3
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2025-04-08 21:24:36 +0200

    test access with v1 and v2 roles

--- x-pack/test/security_solution_cypress/cypress/e2e/ai4dsoc/capabilities/access.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/ai4dsoc/capabilities/access.cy.ts
@@ -10,22 +10,113 @@ import { visit } from '../../../tasks/navigation';
 import { ALERTS_SUMMARY_PROMPT, GET_STARTED_PAGE } from '../constants';
 import { ALERT_SUMMARY_URL, ALERTS_URL, RULES_LANDING_URL } from '../../../urls/navigation';
 
-describe('Cababilities', { tags: '@serverless' }, () => {
-  beforeEach(() => {
-    login('admin');
+const testPageAccess = () => {
+  it('should display the alerts summary prompt when visiting the Alerts Summary page', () => {
+    visit(ALERT_SUMMARY_URL);
+    cy.get(ALERTS_SUMMARY_PROMPT).should('exist');
   });
-  describe('are set properly in order to visit pages', () => {
-    it('alerts_summary', () => {
-      visit(ALERT_SUMMARY_URL);
-      cy.get(ALERTS_SUMMARY_PROMPT).should('exist');
+
+  it('should redirect to Get Started page when visiting the Alerts page', () => {
+    visit(ALERTS_URL);
+    cy.get(GET_STARTED_PAGE).should('exist');
+  });
+
+  it('should redirect to Get Started page when visiting the Rules page', () => {
+    visit(RULES_LANDING_URL);
+    cy.get(GET_STARTED_PAGE).should('exist');
+  });
+};
+
+describe('Capabilities', { tags: '@serverless' }, () => {
+  describe('Admin user capabilities', () => {
+    beforeEach(() => {
+      login('admin');
     });
-    it('alerts - should get redirected', () => {
-      visit(ALERTS_URL);
-      cy.get(GET_STARTED_PAGE).should('exist');
+
+    describe('Page access checks', () => {
+      testPageAccess();
     });
-    it('rules - should get redirected', () => {
-      visit(RULES_LANDING_URL);
-      cy.get(GET_STARTED_PAGE).should('exist');
+  });
+
+  describe('User with siem v1 role', () => {
+    const v1Role = {
+      elasticsearch: {
+        indices: [
+          {
+            names: ['*'],
+            privileges: ['all'],
+          },
+        ],
+      },
+      kibana: [
+        {
+          feature: {
+            siem: ['all'],
+            fleet: ['all'],
+          },
+          spaces: ['*'],
+        },
+      ],
+    };
+
+    before(() => {
+      cy.task('createServerlessCustomRole', {
+        v1Role,
+        roleName: 'siemv1',
+      });
+    });
+
+    beforeEach(() => {
+      login('siemv1');
+    });
+
+    after(() => {
+      cy.task('deleteServerlessCustomRole', 'siemv1');
+    });
+
+    describe('Page access checks', () => {
+      testPageAccess();
+    });
+  });
+
+  describe('User with siem v2 role', () => {
+    const v2Role = {
+      elasticsearch: {
+        indices: [
+          {
+            names: ['*'],
+            privileges: ['all'],
+          },
+        ],
+      },
+      kibana: [
+        {
+          feature: {
+            siemV2: ['all'],
+            fleet: ['all'],
+          },
+          spaces: ['*'],
+        },
+      ],
+    };
+
+    before(() => {
+      cy.task('createServerlessCustomRole', {
+        v2Role,
+        roleName: 'siemV2',
+      });
+    });
+
+    beforeEach(() => {
+      login('siemV2');
+    });
+
+    after(() => {
+      cy.task('deleteServerlessCustomRole', 'siemV2');
+    });
+
+    describe('Page access checks', () => {
+      testPageAccess();
     });
   });
 });
