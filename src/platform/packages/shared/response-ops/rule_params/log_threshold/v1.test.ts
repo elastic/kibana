9c8b246db9e8a4daa683ad61667d05640a3a594a
Christos Nasikas <christos.nasikas@elastic.co>
2025-04-08 15:15:39 +0300

    PR feedback

--- src/platform/packages/shared/response-ops/rule_params/log_threshold/v1.test.ts
+++ src/platform/packages/shared/response-ops/rule_params/log_threshold/v1.test.ts
@@ -36,7 +36,7 @@ describe('logThresholdParamsSchema', () => {
     ],
   };
 
-  const rationParams = {
+  const ratioParams = {
     ...base,
     criteria: [
       [
@@ -54,32 +54,92 @@ describe('logThresholdParamsSchema', () => {
     outputIndex: '.alerts-observability.logs.alerts-default',
   };
 
+  const ratioParamsWithExcess = {
+    ...ratioParams,
+    outputIndex: '.alerts-observability.logs.alerts-default',
+  };
+
   it('validates count params correctly', () => {
     expect(() => logThresholdParamsSchema.validate(countParams)).not.toThrow();
   });
 
-  it('validates ration params correctly', () => {
-    expect(() => logThresholdParamsSchema.validate(rationParams)).not.toThrow();
+  it('validates ratio params correctly', () => {
+    expect(() => logThresholdParamsSchema.validate(ratioParams)).not.toThrow();
   });
 
-  it('does not throw with excess fields', () => {
+  it('does not throw with excess fields in count params', () => {
     const result = logThresholdParamsSchema.validate(countParamsWithExcess);
     expect(result).toEqual(omit(countParamsWithExcess, 'outputIndex'));
   });
 
-  it('strips out excess fields', () => {
-    const result = logThresholdParamsSchema.validate(countParams);
-    expect(result).toEqual(omit(countParams, 'outputIndex'));
+  it('does not throw with excess fields in ration params', () => {
+    const result = logThresholdParamsSchema.validate(ratioParamsWithExcess);
+    expect(result).toEqual(omit(ratioParamsWithExcess, 'outputIndex'));
+  });
+
+  it('strips out excess fields in count params', () => {
+    const result = logThresholdParamsSchema.validate(countParamsWithExcess);
+    expect(result).toEqual(omit(countParamsWithExcess, 'outputIndex'));
+  });
+
+  it('strips out excess fields in ratio params', () => {
+    const result = logThresholdParamsSchema.validate(ratioParamsWithExcess);
+    expect(result).toEqual(omit(ratioParamsWithExcess, 'outputIndex'));
   });
 
   it.each(['criteria', 'count', 'timeUnit', 'timeSize', 'logView'])(
-    'fails without %s required field',
+    'fails without %s required field in count params',
     (field) => {
       expect(() => logThresholdParamsSchema.validate(omit(countParams, field))).toThrow();
     }
   );
 
-  it.each(['groupBy'])('does not fail without %s optional field', (field) => {
+  it.each(['groupBy'])('does not fail without %s optional field in count params', (field) => {
     expect(() => logThresholdParamsSchema.validate(omit(countParams, field))).not.toThrow();
+  });
+
+  it.each(['criteria', 'count', 'timeUnit', 'timeSize', 'logView'])(
+    'fails without %s required field in ratio params',
+    (field) => {
+      expect(() => logThresholdParamsSchema.validate(omit(ratioParams, field))).toThrow();
+    }
+  );
+
+  it.each(['groupBy'])('does not fail without %s optional field in ratio params', (field) => {
+    expect(() => logThresholdParamsSchema.validate(omit(ratioParams, field))).not.toThrow();
+  });
+
+  it('trips out excess fields in logView', () => {
+    expect(() =>
+      logThresholdParamsSchema.validate({
+        ...countParams,
+        logView: { ...countParams.logView, excessField: 'foo' },
+      })
+    ).not.toThrow();
+  });
+
+  it('trips out excess fields in threshold', () => {
+    expect(() =>
+      logThresholdParamsSchema.validate({
+        ...countParams,
+        count: { ...countParams.count, excessField: 'foo' },
+      })
+    ).not.toThrow();
+  });
+
+  it('trips out excess fields in criteria', () => {
+    expect(() =>
+      logThresholdParamsSchema.validate({
+        ...countParams,
+        criteria: [
+          {
+            field: 'bytes',
+            comparator: 'more than',
+            value: 1,
+            excessField: 'foo',
+          },
+        ],
+      })
+    ).not.toThrow();
   });
 });
