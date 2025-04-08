f37ee8524da0059a8448ac156b463d6480b592b8
PhilippeOberti <philippe.oberti@elastic.co>
2025-04-08 16:17:43 -0500

    [AI4DSOC] Alert summary AI Assistant action in table and flyout

--- x-pack/solutions/security/plugins/security_solution/public/detections/components/alert_summary/table/table.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/detections/components/alert_summary/table/table.tsx
@@ -73,7 +73,7 @@ const columns: EuiDataGridProps['columns'] = [
   },
 ];
 
-const ACTION_COLUMN_WIDTH = 72; // px
+const ACTION_COLUMN_WIDTH = 98; // px
 const ALERT_TABLE_CONSUMERS: AlertsTableProps['consumers'] = [AlertConsumers.SIEM];
 const RULE_TYPE_IDS = [ESQL_RULE_TYPE_ID, QUERY_RULE_TYPE_ID];
 const ROW_HEIGHTS_OPTIONS = { defaultHeight: 40 };
