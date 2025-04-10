9c69f9b9c40e5dea9e8cb4a71f28d46b4a3d658d
Dario Gieselaar <dario.gieselaar@elastic.co>
2025-04-10 11:28:13 +0200

    Revert changes in zod helpers

--- src/platform/packages/shared/kbn-zod-helpers/src/kbn_zod_types/boolean_from_string.ts
+++ src/platform/packages/shared/kbn-zod-helpers/src/kbn_zod_types/boolean_from_string.ts
@@ -8,6 +8,7 @@
  */
 
 import * as z from '@kbn/zod';
+import { KbnZodType, KbnZodTypes } from './kbn_zod_type';
 
 /**
  * This is a helper schema to convert a boolean string ("true" or "false") to a
@@ -15,10 +16,33 @@ import * as z from '@kbn/zod';
  *
  * Accepts "true" or "false" as strings, or a boolean.
  */
+class KbnZodBooleanFromString extends z.ZodUnion<any> implements KbnZodType {
+  readonly kbnTypeName = KbnZodTypes.BooleanFromString;
 
-export const BooleanFromString = z
-  .union([z.boolean(), z.string()])
-  .refine((val) => val === 'true' || val === 'false', {
-    message: 'Input must be either "true" or "false"',
-  })
-  .transform((val) => val === 'true');
+  static create() {
+    return new KbnZodBooleanFromString({
+      typeName: z.ZodFirstPartyTypeKind.ZodUnion,
+      options: [z.enum(['true', 'false']), z.boolean()],
+    }).describe("A boolean value, which can be 'true' or 'false' as string or a native boolean.");
+  }
+
+  override _parse(input: z.ParseInput): z.ParseReturnType<this['_output']> {
+    const result = super._parse(input); // Use ZodUnion's default parsing
+
+    if (z.isValid(result)) {
+      const value = result.value;
+      return {
+        status: 'valid',
+        value: value === 'true' ? true : value === 'false' ? false : value,
+      };
+    }
+
+    return result;
+  }
+}
+
+export const BooleanFromString = KbnZodBooleanFromString.create();
+
+export const isBooleanFromString = (val: unknown): val is KbnZodBooleanFromString => {
+  return (val as KbnZodBooleanFromString).kbnTypeName === KbnZodTypes.BooleanFromString;
+};
