80869d494ec3bb40c415d2d90db7bee561622f17
Kevin Delemme <kevin.delemme@elastic.co>
2025-04-11 14:29:27 -0400

    fix type

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/streams/queries.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/streams/queries.ts
@@ -6,7 +6,7 @@
  */
 
 import expect from '@kbn/expect';
-import { WiredIngestUpsertRequest } from '@kbn/streams-schema';
+import { WiredStreamDefinitionBase } from '@kbn/streams-schema';
 import { v4 } from 'uuid';
 import { DeploymentAgnosticFtrProviderContext } from '../../../ftr_provider_context';
 import {
@@ -20,7 +20,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
   let apiClient: StreamsSupertestRepositoryClient;
 
   const STREAM_NAME = 'logs.queries-test';
-  const stream: WiredIngestUpsertRequest = {
+  const stream: WiredStreamDefinitionBase = {
     description: '',
     ingest: {
       lifecycle: { inherit: {} },
