f7d18c7019a32dd0a1af0c8243667c594c0381c2
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-04-11 22:45:08 +0200

    Comment

--- x-pack/solutions/security/plugins/elastic_assistant/server/lib/attack_discovery/schedules/types.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/server/lib/attack_discovery/schedules/types.ts
@@ -10,6 +10,12 @@ import { SecurityAttackDiscoveryAlert } from '@kbn/alerts-as-data-utils';
 import { AttackDiscoveryScheduleParams } from '@kbn/elastic-assistant-common';
 import { ALERT_ATTACK_DISCOVERY_USERS } from './fields';
 
+/**
+ * This is a WORKAROUND until the `createSchemaFromFieldMap` can handle complex mappings.
+ * Right now that tool cannot properly handle the combination of optional and required fields within the same nested/object field type.
+ * Instead of creating an intersection type it generates a separate fields in required and optional sections as separate flattened fields.
+ * As a workaround, we strip out incorrectly generated fields and re-add them in a correct format.
+ */
 export type AttackDiscoveryAlert = Omit<
   SecurityAttackDiscoveryAlert,
   | 'kibana.alert.attack_discovery.users'
