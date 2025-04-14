269a92d497d50e73db2dfae2e81c399d7cbaa3d6
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2025-04-14 17:48:46 +0200

    refactor(streams): improve fields simulation error handling

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/schema_editor/flyout/index.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/schema_editor/flyout/index.tsx
@@ -15,7 +15,7 @@ import {
   EuiTitle,
   EuiButton,
 } from '@elastic/eui';
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import { i18n } from '@kbn/i18n';
 import { IngestStreamDefinition } from '@kbn/streams-schema';
 import useAsyncFn from 'react-use/lib/useAsyncFn';
@@ -43,6 +43,8 @@ export const SchemaEditorFlyout = ({
   withFieldSimulation = false,
 }: SchemaEditorFlyoutProps) => {
   const [isEditing, toggleEditMode] = useToggle(isEditingByDefault);
+  const [isValidAdvancedFieldMappings, setValidAdvancedFieldMappings] = useState(true);
+  const [isValidSimulation, setValidSimulation] = useState(true);
 
   const [nextField, setNextField] = useReducer(
     (prev: SchemaField, updated: Partial<SchemaField>) =>
@@ -78,11 +80,16 @@ export const SchemaEditorFlyout = ({
           <AdvancedFieldMappingOptions
             field={nextField}
             onChange={setNextField}
+            onValidate={setValidAdvancedFieldMappings}
             isEditing={isEditing}
           />
           {withFieldSimulation && (
             <EuiFlexItem grow={false}>
-              <SamplePreviewTable stream={stream} nextField={nextField} />
+              <SamplePreviewTable
+                stream={stream}
+                nextField={nextField}
+                onValidate={setValidSimulation}
+              />
             </EuiFlexItem>
           )}
         </EuiFlexGroup>
@@ -103,6 +110,7 @@ export const SchemaEditorFlyout = ({
             </EuiButtonEmpty>
             <EuiButton
               data-test-subj="streamsAppSchemaEditorFieldSaveButton"
+              disabled={isSaving || !isValidAdvancedFieldMappings || !isValidSimulation}
               isLoading={isSaving}
               onClick={saveChanges}
             >
