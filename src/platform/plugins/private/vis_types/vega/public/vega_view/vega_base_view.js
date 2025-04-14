5e8e9a131ff13190ddca7f0c1b035c1e895a6d7d
Andreana Malama <72010092+andrimal@users.noreply.github.com>
2025-04-14 17:42:32 +0300

    [Vega] Add ability to open links in Vega in new window (#216200)
    ## Summary
    
    Fix #70745
    
    Enables the option to open the clickable mark links of a Vega
    Visualization in a new tab.
    
    To achieve this functionality:
    - use the `usermeta.embedOptions.loader` property in the vega chart spec
    to pass the `"target": "_blank"` configuration:
    ```javascript
    "usermeta": {
        "embedOptions": {
          "loader": {"target": "_blank"}
        }
      }
    ```
    
    Link opens in a new tab:
    ![Kapture 2025-03-31 at 20 44
    35](https://github.com/user-attachments/assets/bd4f9495-28d7-4a27-89a9-dcb9fb8913dc)
    
    ### Checklist
    
    Check the PR satisfies following conditions.
    
    Reviewers should verify this PR satisfies this list as well.
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: Marco Liberati <dej611@users.noreply.github.com>
    (cherry picked from commit f8e688f88135bf7675cfd0c6dfb918e23b6ebbb8)

--- src/platform/plugins/private/vis_types/vega/public/vega_view/vega_base_view.js
+++ src/platform/plugins/private/vis_types/vega/public/vega_view/vega_base_view.js
@@ -236,6 +236,11 @@ export class VegaBaseView {
 
       return result;
     };
+
+    const vegaSpec = this._parser.isVegaLite ? this._parser.vlspec : this._parser.spec;
+    const usermetaLoaderOptions = vegaSpec.usermeta?.embedOptions?.loader;
+    vegaLoader.options = usermetaLoaderOptions ?? {};
+
     config.loader = vegaLoader;
 
     const vegaLogger = logger(Warn);
