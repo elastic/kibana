449b7851746cec41d9b7a05e5993eb87ca6ae5c5
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-04-15 22:54:42 +0200

    Code cleanup

--- src/platform/plugins/shared/share/public/components/tabs/link/time_type_section.tsx
+++ src/platform/plugins/shared/share/public/components/tabs/link/time_type_section.tsx
@@ -10,55 +10,15 @@
 import { EuiSpacer, EuiSwitch, EuiSwitchEvent, EuiText, useEuiTheme } from '@elastic/eui';
 import React, { ReactNode, useEffect, useState } from 'react';
 import { FormattedMessage, FormattedRelativeTime, FormattedDate } from '@kbn/i18n-react';
-import dateMath from '@kbn/datemath';
-import moment from 'moment';
 import { css } from '@emotion/react';
 import { i18n } from '@kbn/i18n';
+import {
+  convertRelativeToAbsoluteDate,
+  getRelativeValueAndUnit,
+  isTimeRangeAbsoluteTime,
+} from './time_utils';
 
-const unitMap = new Map([
-  ['s', 'second'],
-  ['m', 'minute'],
-  ['h', 'hour'],
-  ['d', 'day'],
-  ['w', 'week'],
-  ['M', 'month'],
-  ['y', 'year'],
-]);
-
-const getRelativeValueAndUnit = (input?: string) => {
-  if (!input) return;
-  const match = input.match(/^now([+-]\d+)([smhdwMy])$/);
-  if (!match) return;
-
-  const [, signAndNumber, unit] = match;
-
-  return {
-    value: Number(signAndNumber),
-    unit: unitMap.get(unit),
-  };
-};
-
-const getParsedDates = (isAbsolute: boolean, from?: string, to?: string) => {
-  const fromParsed = dateMath.parse(from || '');
-  const toParsed = dateMath.parse(to || '');
-
-  const fromDate = fromParsed?.isValid() ? fromParsed.toDate() : moment(from).toDate();
-  const toDate = toParsed?.isValid() ? toParsed.toDate() : moment(to).toDate();
-
-  if (isAbsolute) return { from: fromDate, to: toDate };
-
-  const relativeFrom = getRelativeValueAndUnit(from);
-  const relativeTo = getRelativeValueAndUnit(to);
-
-  return {
-    from: relativeFrom?.value,
-    to: relativeTo?.value,
-    fromUnit: relativeFrom?.unit,
-    toUnit: relativeTo?.unit,
-  };
-};
-
-const BoldText = ({ text }: { text: ReactNode }) => {
+const BoldText = ({ children }: { children: ReactNode }) => {
   const { euiTheme } = useEuiTheme();
   const boldText = css`
     font-weight: ${euiTheme.font.weight.bold};
@@ -66,22 +26,38 @@ const BoldText = ({ text }: { text: ReactNode }) => {
 
   return (
     <EuiText css={boldText} component="span" size="s">
-      {text}
+      {children}
     </EuiText>
   );
 };
 
 const AbsoluteTimeText = ({ date }: { date: Date }) => (
-  <FormattedDate
-    value={date}
-    year="numeric"
-    month="long"
-    day="2-digit"
-    hour="numeric"
-    minute="numeric"
-    hour12
-  />
+  <BoldText>
+    <FormattedDate
+      value={date}
+      year="numeric"
+      month="long"
+      day="2-digit"
+      hour="numeric"
+      minute="numeric"
+      hour12
+    />
+  </BoldText>
 );
+
+const RelativeTimeText = ({ date }: { date: string }) => {
+  const result = getRelativeValueAndUnit(date);
+
+  return (
+    <BoldText>
+      <FormattedRelativeTime
+        value={result?.value}
+        // @ts-expect-error - RelativeTimeFormatSingularUnit expected here is not exported so a cast from string is not possible
+        unit={result?.unit}
+      />
+    </BoldText>
+  );
+};
 
 interface Props {
   timeRange?: {
@@ -94,19 +70,12 @@ interface Props {
 
 export const TimeTypeSection = ({ timeRange, isAbsoluteTime, changeTimeType }: Props) => {
   const [isAbsoluteTimeByDefault, setIsAbsoluteTimeByDefault] = useState(false);
-  const { from, to, fromUnit, toUnit } = getParsedDates(
-    isAbsoluteTime,
-    timeRange?.from,
-    timeRange?.to
-  );
 
   useEffect(() => {
-    setIsAbsoluteTimeByDefault(
-      !(timeRange?.from?.includes('now') || timeRange?.to?.includes('now'))
-    );
+    setIsAbsoluteTimeByDefault(isTimeRangeAbsoluteTime(timeRange));
   }, [timeRange]);
 
-  if (!from || !to) {
+  if (!timeRange) {
     return null;
   }
 
@@ -127,8 +96,8 @@ export const TimeTypeSection = ({ timeRange, isAbsoluteTime, changeTimeType }: P
             id="share.link.timeRange.absoluteTimeInfoText"
             defaultMessage="The users will see all data from {from} to {to}."
             values={{
-              from: <BoldText text={<AbsoluteTimeText date={from as Date} />} />,
-              to: <BoldText text={<AbsoluteTimeText date={to as Date} />} />,
+              from: <AbsoluteTimeText date={convertRelativeToAbsoluteDate(timeRange?.from)} />,
+              to: <AbsoluteTimeText date={convertRelativeToAbsoluteDate(timeRange?.to)} />,
             }}
           />
         ) : (
@@ -136,28 +105,8 @@ export const TimeTypeSection = ({ timeRange, isAbsoluteTime, changeTimeType }: P
             id="share.link.timeRange.relativeTimeInfoText"
             defaultMessage="The users will see all data from {from} to {to}, based on when they view it."
             values={{
-              from: (
-                <BoldText
-                  text={
-                    <FormattedRelativeTime
-                      value={from as number}
-                      // @ts-expect-error - RelativeTimeFormatSingularUnit expected here is not exported so a cast from string is not possible
-                      unit={fromUnit}
-                    />
-                  }
-                />
-              ),
-              to: (
-                <BoldText
-                  text={
-                    <FormattedRelativeTime
-                      value={to as number}
-                      // @ts-expect-error - RelativeTimeFormatSingularUnit expected here is not exported so a cast from string is not possible
-                      unit={toUnit}
-                    />
-                  }
-                />
-              ),
+              from: <RelativeTimeText date={timeRange?.from} />,
+              to: <RelativeTimeText date={timeRange?.to} />,
             }}
           />
         )}
---
+++ src/platform/plugins/shared/share/public/components/tabs/link/time_utils.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import kbnDatemath from '@kbn/datemath';
+import moment from 'moment';
+
+const unitMap = new Map([
+  ['s', 'second'],
+  ['m', 'minute'],
+  ['h', 'hour'],
+  ['d', 'day'],
+  ['w', 'week'],
+  ['M', 'month'],
+  ['y', 'year'],
+]);
+
+export const getRelativeValueAndUnit = (date?: string) => {
+  if (!date) return;
+  const match = date.match(/^now([+-]\d+)([smhdwMy])$/);
+  if (!match) return;
+
+  const [, signAndNumber, unit] = match;
+
+  return {
+    value: Number(signAndNumber),
+    unit: unitMap.get(unit),
+  };
+};
+
+export const convertRelativeToAbsoluteDate = (date?: string) => {
+  const valueParsed = kbnDatemath.parse(date || '');
+
+  return valueParsed?.isValid() ? valueParsed.toDate() : moment(date).toDate();
+};
+
+export const isTimeRangeAbsoluteTime = (timeRange?: { from: string; to: string }) =>
+  !(timeRange?.from?.includes('now') || timeRange?.to?.includes('now'));
