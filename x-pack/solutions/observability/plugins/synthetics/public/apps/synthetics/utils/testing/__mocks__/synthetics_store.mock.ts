979cc24b304e3b54fab7f103d17bd94088ddcfb5
Francesco Fagnani <fagnani.francesco@gmail.com>
2025-04-15 16:53:14 +0200

    [Synthetics] Sync global parameters is called in the endpoints to add, edit or delete global params (#216197)
    This PR closes #215668.
    
    The global parameters are synched in the endpoints where they are
    created, edited or deleted.
    
    ---------
    
    Co-authored-by: Shahzad <shahzad31comp@gmail.com>

--- x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/utils/testing/__mocks__/synthetics_store.mock.ts
+++ x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/utils/testing/__mocks__/synthetics_store.mock.ts
@@ -120,11 +120,6 @@ export const mockState: SyntheticsAppState = {
     error: null,
     data: [],
   },
-  settings: {
-    loading: false,
-    error: null,
-    success: null,
-  },
   dynamicSettings: {
     loading: false,
     locationMonitors: [],
