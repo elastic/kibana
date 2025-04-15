3b5e1553b3d0d55e6a00d79216e76c1120537cdc
Nathan Reese <reese.nathan@gmail.com>
2025-04-15 13:53:01 -0600

    migrate control group embeddable

--- src/platform/plugins/shared/controls/public/control_group/control_group_unsaved_changes_api.ts
+++ src/platform/plugins/shared/controls/public/control_group/control_group_unsaved_changes_api.ts
@@ -7,23 +7,27 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { omit } from 'lodash';
-import { combineLatest, map } from 'rxjs';
+import { combineLatest, map, of } from 'rxjs';
 
 import {
+  apiHasLastSavedChildState,
   childrenUnsavedChanges$,
   initializeUnsavedChanges,
   type PresentationContainer,
 } from '@kbn/presentation-containers';
-import {
-  apiPublishesUnsavedChanges,
-  type PublishesUnsavedChanges,
-  type StateComparators,
-} from '@kbn/presentation-publishing';
+import { apiPublishesUnsavedChanges, SerializedPanelState } from '@kbn/presentation-publishing';
 
-import type { ControlGroupRuntimeState, ControlPanelsState } from '../../common';
+import { StateManager } from '@kbn/presentation-publishing/state_manager/types';
+import {
+  DEFAULT_CONTROL_CHAINING,
+  type ControlGroupRuntimeState,
+  type ControlGroupSerializedState,
+  type ControlPanelsState,
+} from '../../common';
 import { apiPublishesAsyncFilters } from '../controls/data_controls/publishes_async_filters';
-import { getControlsInOrder, type ControlsInOrder } from './init_controls_manager';
+import type { ControlsInOrder } from './init_controls_manager';
+import { deserializeControlGroup } from './utils/serialization_utils';
+import { ControlGroupEditorState } from './types';
 
 export type ControlGroupComparatorState = Pick<
   ControlGroupRuntimeState,
@@ -32,46 +36,77 @@ export type ControlGroupComparatorState = Pick<
   controlsInOrder: ControlsInOrder;
 };
 
-export function initializeControlGroupUnsavedChanges(
-  applySelections: () => void,
-  children$: PresentationContainer['children$'],
-  comparators: StateComparators<ControlGroupComparatorState>,
-  snapshotControlsRuntimeState: () => ControlPanelsState,
-  resetControlsUnsavedChanges: () => void,
-  parentApi: unknown,
-  lastSavedRuntimeState: ControlGroupRuntimeState
-) {
-  const controlGroupUnsavedChanges = initializeUnsavedChanges<ControlGroupComparatorState>(
-    {
-      autoApplySelections: lastSavedRuntimeState.autoApplySelections,
-      chainingSystem: lastSavedRuntimeState.chainingSystem,
-      controlsInOrder: getControlsInOrder(lastSavedRuntimeState.initialChildControlState),
-      ignoreParentSettings: lastSavedRuntimeState.ignoreParentSettings,
-      labelPosition: lastSavedRuntimeState.labelPosition,
-    },
+export function initializeControlGroupUnsavedChanges({
+  applySelections,
+  children$,
+  controlGroupId,
+  controlGroupStateManager,
+  parentApi,
+  resetControlsUnsavedChanges,
+  serializeControlGroupState,
+}: {
+  applySelections: () => void;
+  children$: PresentationContainer['children$'];
+  controlGroupId: string;
+  controlGroupStateManager: StateManager<ControlGroupEditorState>;
+  parentApi: unknown;
+  resetControlsUnsavedChanges: (lastSavedControlsState: ControlPanelsState) => void;
+  serializeControlGroupState: () => SerializedPanelState<ControlGroupSerializedState>;
+}) {
+  function getLastSavedControlsState() {
+    if (!apiHasLastSavedChildState<ControlGroupSerializedState>(parentApi)) {
+      return {};
+    }
+    const lastSavedControlGroupState = parentApi.getLastSavedStateForChild(controlGroupId);
+    return lastSavedControlGroupState
+      ? deserializeControlGroup(serializeControlGroupState()).initialChildControlState
+      : {};
+  }
+
+  function getLastSavedStateForControl(controlId: string) {
+    const controlState = getLastSavedControlsState()[controlId];
+    return controlState ? { rawState: controlState } : undefined;
+  }
+
+  const lastSavedControlsState$ = apiHasLastSavedChildState<ControlGroupSerializedState>(parentApi)
+    ? parentApi.lastSavedStateForChild$(controlGroupId).pipe(map(() => getLastSavedControlsState()))
+    : of({});
+
+  const controlGroupEditorUnsavedChangesApi = initializeUnsavedChanges<ControlGroupEditorState>({
+    uuid: controlGroupId,
     parentApi,
-    comparators
-  );
+    serializeState: serializeControlGroupState,
+    anyStateChange$: controlGroupStateManager.anyStateChange$,
+    getComparators: () => {
+      return {
+        autoApplySelections: 'referenceEquality',
+        chainingSystem: (a, b) =>
+          (a ?? DEFAULT_CONTROL_CHAINING) === (b ?? DEFAULT_CONTROL_CHAINING),
+        ignoreParentSettings: 'deepEquality',
+        labelPosition: 'referenceEquality',
+      };
+    },
+    onReset: (lastSaved) => {
+      controlGroupStateManager.reinitializeState(lastSaved?.rawState);
+    },
+  });
 
   return {
     api: {
-      unsavedChanges$: combineLatest([
-        controlGroupUnsavedChanges.api.unsavedChanges$,
+      lastSavedStateForChild$: (controlId: string) =>
+        lastSavedControlsState$.pipe(map(() => getLastSavedStateForControl(controlId))),
+      getLastSavedStateForChild: getLastSavedStateForControl,
+      hasUnsavedChanges$: combineLatest([
+        controlGroupEditorUnsavedChangesApi.hasUnsavedChanges$,
         childrenUnsavedChanges$(children$),
       ]).pipe(
-        map(([unsavedControlGroupState, unsavedControlsState]) => {
-          const unsavedChanges: Partial<ControlGroupRuntimeState> = unsavedControlGroupState
-            ? omit(unsavedControlGroupState, 'controlsInOrder')
-            : {};
-          if (unsavedControlsState || unsavedControlGroupState?.controlsInOrder) {
-            unsavedChanges.initialChildControlState = snapshotControlsRuntimeState();
-          }
-          return Object.keys(unsavedChanges).length ? unsavedChanges : undefined;
+        map(([hasUnsavedControlGroupChanges, unsavedControlsState]) => {
+          return hasUnsavedControlGroupChanges || unsavedControlsState !== undefined;
         })
       ),
-      asyncResetUnsavedChanges: async () => {
-        controlGroupUnsavedChanges.api.resetUnsavedChanges();
-        resetControlsUnsavedChanges();
+      resetUnsavedChanges: async () => {
+        controlGroupEditorUnsavedChangesApi.resetUnsavedChanges();
+        resetControlsUnsavedChanges(getLastSavedControlsState());
 
         const filtersReadyPromises: Array<Promise<void>> = [];
         Object.values(children$.value).forEach((controlApi) => {
@@ -83,12 +118,10 @@ export function initializeControlGroupUnsavedChanges(
 
         await Promise.all(filtersReadyPromises);
 
-        if (!comparators.autoApplySelections[0].value) {
+        if (!controlGroupStateManager.api.autoApplySelections$.value) {
           applySelections();
         }
       },
-    } as Pick<PublishesUnsavedChanges, 'unsavedChanges$'> & {
-      asyncResetUnsavedChanges: () => Promise<void>;
     },
   };
 }
--- src/platform/plugins/shared/controls/public/control_group/get_control_group_factory.tsx
+++ src/platform/plugins/shared/controls/public/control_group/get_control_group_factory.tsx
@@ -8,35 +8,27 @@
  */
 
 import { DataView } from '@kbn/data-views-plugin/common';
-import { ReactEmbeddableFactory } from '@kbn/embeddable-plugin/public';
+import { EmbeddableFactory } from '@kbn/embeddable-plugin/public';
 import type { ESQLControlVariable } from '@kbn/esql-types';
 import { PublishesESQLVariable, apiPublishesESQLVariable } from '@kbn/esql-types';
 import { i18n } from '@kbn/i18n';
-import {
-  apiHasSaveNotification,
-  combineCompatibleChildrenApis,
-} from '@kbn/presentation-containers';
+import { combineCompatibleChildrenApis } from '@kbn/presentation-containers';
 import {
   PublishesDataViews,
   apiPublishesDataViews,
+  initializeStateManager,
   useBatchedPublishingSubjects,
 } from '@kbn/presentation-publishing';
 import { apiPublishesReload } from '@kbn/presentation-publishing/interfaces/fetch/publishes_reload';
-import fastIsEqual from 'fast-deep-equal';
 import React, { useEffect } from 'react';
 import { BehaviorSubject } from 'rxjs';
-import type {
-  ControlGroupChainingSystem,
-  ControlGroupRuntimeState,
-  ControlGroupSerializedState,
-  ControlLabelPosition,
-  ControlPanelsState,
-  ParentIgnoreSettings,
-} from '../../common';
+import type { ControlGroupSerializedState } from '../../common';
 import {
   CONTROL_GROUP_TYPE,
+  DEFAULT_AUTO_APPLY_SELECTIONS,
   DEFAULT_CONTROL_CHAINING,
   DEFAULT_CONTROL_LABEL_POSITION,
+  DEFAULT_IGNORE_PARENT_SETTINGS,
 } from '../../common';
 import { openDataControlEditor } from '../controls/data_controls/open_data_control_editor';
 import { coreServices, dataViewsService } from '../services/kibana_services';
@@ -46,90 +38,63 @@ import { initializeControlGroupUnsavedChanges } from './control_group_unsaved_ch
 import { initControlsManager } from './init_controls_manager';
 import { openEditControlGroupFlyout } from './open_edit_control_group_flyout';
 import { initSelectionsManager } from './selections_manager';
-import type { ControlGroupApi } from './types';
+import type { ControlGroupApi, ControlGroupEditorState } from './types';
 import { deserializeControlGroup } from './utils/serialization_utils';
 
 export const getControlGroupEmbeddableFactory = () => {
-  const controlGroupEmbeddableFactory: ReactEmbeddableFactory<
+  const controlGroupEmbeddableFactory: EmbeddableFactory<
     ControlGroupSerializedState,
-    ControlGroupRuntimeState,
     ControlGroupApi
   > = {
     type: CONTROL_GROUP_TYPE,
-    deserializeState: (state) => deserializeControlGroup(state),
-    buildEmbeddable: async (
-      initialRuntimeState,
-      buildApi,
-      uuid,
-      parentApi,
-      setApi,
-      lastSavedRuntimeState
-    ) => {
-      const {
-        labelPosition: initialLabelPosition,
-        chainingSystem,
-        autoApplySelections,
-        ignoreParentSettings,
-      } = initialRuntimeState;
+    buildEmbeddable: async ({ initialState, finalizeApi, uuid, parentApi }) => {
+      const initialRuntimeState = deserializeControlGroup(initialState);
 
-      const autoApplySelections$ = new BehaviorSubject<boolean>(autoApplySelections);
+      const controlGroupStateManager = initializeStateManager<ControlGroupEditorState>(
+        initialState?.rawState,
+        {
+          autoApplySelections: DEFAULT_AUTO_APPLY_SELECTIONS,
+          chainingSystem: DEFAULT_CONTROL_CHAINING,
+          ignoreParentSettings: DEFAULT_IGNORE_PARENT_SETTINGS,
+          labelPosition: DEFAULT_CONTROL_LABEL_POSITION,
+        }
+      );
+
       const defaultDataViewId = await dataViewsService.getDefaultId();
-      const lastSavedControlsState$ = new BehaviorSubject<ControlPanelsState>(
-        lastSavedRuntimeState.initialChildControlState
-      );
-      const controlsManager = initControlsManager(
-        initialRuntimeState.initialChildControlState,
-        lastSavedControlsState$
-      );
+
+      const controlsManager = initControlsManager(initialRuntimeState.initialChildControlState);
       const selectionsManager = initSelectionsManager({
         ...controlsManager.api,
-        autoApplySelections$,
+        autoApplySelections$: controlGroupStateManager.api.autoApplySelections$,
       });
       const esqlVariables$ = new BehaviorSubject<ESQLControlVariable[]>([]);
       const dataViews$ = new BehaviorSubject<DataView[] | undefined>(undefined);
-      const chainingSystem$ = new BehaviorSubject<ControlGroupChainingSystem>(
-        chainingSystem ?? DEFAULT_CONTROL_CHAINING
-      );
-      const ignoreParentSettings$ = new BehaviorSubject<ParentIgnoreSettings | undefined>(
-        ignoreParentSettings
-      );
-      const labelPosition$ = new BehaviorSubject<ControlLabelPosition>(
-        initialLabelPosition ?? DEFAULT_CONTROL_LABEL_POSITION
-      );
+
       const allowExpensiveQueries$ = new BehaviorSubject<boolean>(true);
       const disabledActionIds$ = new BehaviorSubject<string[] | undefined>(undefined);
 
-      const unsavedChanges = initializeControlGroupUnsavedChanges(
-        selectionsManager.applySelections,
-        controlsManager.api.children$,
-        {
-          ...controlsManager.comparators,
-          autoApplySelections: [
-            autoApplySelections$,
-            (next: boolean) => autoApplySelections$.next(next),
-          ],
-          chainingSystem: [
-            chainingSystem$,
-            (next: ControlGroupChainingSystem) => chainingSystem$.next(next),
-            (a, b) => (a ?? DEFAULT_CONTROL_CHAINING) === (b ?? DEFAULT_CONTROL_CHAINING),
-          ],
-          ignoreParentSettings: [
-            ignoreParentSettings$,
-            (next: ParentIgnoreSettings | undefined) => ignoreParentSettings$.next(next),
-            fastIsEqual,
-          ],
-          labelPosition: [
-            labelPosition$,
-            (next: ControlLabelPosition) => labelPosition$.next(next),
-          ],
-        },
-        controlsManager.snapshotControlsRuntimeState,
-        controlsManager.resetControlsUnsavedChanges,
-        parentApi,
-        lastSavedRuntimeState
-      );
+      function serializeState() {
+        const { controls, references } = controlsManager.serializeControls();
+        return {
+          rawState: {
+            ...controlGroupStateManager.getLatestState(),
+            controls,
+          },
+          references,
+        };
+      }
 
-      const api = setApi({
+      const unsavedChanges = initializeControlGroupUnsavedChanges({
+        applySelections: selectionsManager.applySelections,
+        children$: controlsManager.api.children$,
+        controlGroupId: uuid,
+        controlGroupStateManager,
+        parentApi,
+        resetControlsUnsavedChanges: controlsManager.resetControlsUnsavedChanges,
+        serializeControlGroupState: serializeState,
+      });
+
+      const api = finalizeApi({
         ...controlsManager.api,
         esqlVariables$,
         disabledActionIds$,
@@ -139,31 +104,20 @@ export const getControlGroupEmbeddableFactory = () => {
           controlFetch$(
             chaining$(
               controlUuid,
-              chainingSystem$,
+              controlGroupStateManager.api.chainingSystem$,
               controlsManager.controlsInOrder$,
               controlsManager.api.children$
             ),
-            controlGroupFetch$(ignoreParentSettings$, parentApi ? parentApi : {})
+            controlGroupFetch$(
+              controlGroupStateManager.api.ignoreParentSettings$,
+              parentApi ? parentApi : {}
+            )
           ),
-        ignoreParentSettings$,
-        autoApplySelections$,
+        ignoreParentSettings$: controlGroupStateManager.api.ignoreParentSettings$,
+        autoApplySelections$: controlGroupStateManager.api.autoApplySelections$,
         allowExpensiveQueries$,
-        snapshotRuntimeState: () => {
-          return {
-            chainingSystem: chainingSystem$.getValue(),
-            labelPosition: labelPosition$.getValue(),
-            autoApplySelections: autoApplySelections$.getValue(),
-            ignoreParentSettings: ignoreParentSettings$.getValue(),
-            initialChildControlState: controlsManager.snapshotControlsRuntimeState(),
-          };
-        },
         onEdit: async () => {
-          openEditControlGroupFlyout(api, {
-            chainingSystem: chainingSystem$,
-            labelPosition: labelPosition$,
-            autoApplySelections: autoApplySelections$,
-            ignoreParentSettings: ignoreParentSettings$,
-          });
+          openEditControlGroupFlyout(api, controlGroupStateManager);
         },
         isEditingEnabled: () => true,
         openAddDataControlFlyout: (settings) => {
@@ -190,24 +144,9 @@ export const getControlGroupEmbeddableFactory = () => {
             controlGroupApi: api,
           });
         },
-        serializeState: () => {
-          const { controls, references } = controlsManager.serializeControls();
-          return {
-            rawState: {
-              chainingSystem: chainingSystem$.getValue(),
-              labelPosition: labelPosition$.getValue(),
-              autoApplySelections: autoApplySelections$.getValue(),
-              ignoreParentSettings: ignoreParentSettings$.getValue(),
-              controls,
-            },
-            references,
-          };
-        },
+        serializeState,
         dataViews$,
-        labelPosition: labelPosition$,
-        saveNotification$: apiHasSaveNotification(parentApi)
-          ? parentApi.saveNotification$
-          : undefined,
+        labelPosition: controlGroupStateManager.api.labelPosition$,
         reload$: apiPublishesReload(parentApi) ? parentApi.reload$ : undefined,
 
         /** Public getters */
@@ -216,13 +155,10 @@ export const getControlGroupEmbeddableFactory = () => {
             defaultMessage: 'Controls',
           }),
         getEditorConfig: () => initialRuntimeState.editorConfig,
-        getLastSavedControlState: (controlUuid: string) => {
-          return lastSavedRuntimeState.initialChildControlState[controlUuid] ?? {};
-        },
 
         /** Public setters */
         setDisabledActionIds: (ids) => disabledActionIds$.next(ids),
-        setChainingSystem: (newChainingSystem) => chainingSystem$.next(newChainingSystem),
+        setChainingSystem: controlGroupStateManager.api.setChainingSystem,
       });
 
       /** Subscribe to all children's output data views, combine them, and output them */
@@ -241,26 +177,12 @@ export const getControlGroupEmbeddableFactory = () => {
         esqlVariables$.next(newESQLVariables);
       });
 
-      const saveNotificationSubscription = apiHasSaveNotification(parentApi)
-        ? parentApi.saveNotification$.subscribe(() => {
-            lastSavedControlsState$.next(controlsManager.snapshotControlsRuntimeState());
-
-            if (
-              typeof autoApplySelections$.value === 'boolean' &&
-              !autoApplySelections$.value &&
-              selectionsManager.hasUnappliedSelections$.value
-            ) {
-              selectionsManager.applySelections();
-            }
-          })
-        : undefined;
-
       return {
         api,
         Component: () => {
           const [hasUnappliedSelections, labelPosition] = useBatchedPublishingSubjects(
             selectionsManager.hasUnappliedSelections$,
-            labelPosition$
+            controlGroupStateManager.api.labelPosition$
           );
 
           useEffect(() => {
@@ -286,7 +208,6 @@ export const getControlGroupEmbeddableFactory = () => {
               selectionsManager.cleanup();
               childrenDataViewsSubscription.unsubscribe();
               childrenESQLVariablesSubscription.unsubscribe();
-              saveNotificationSubscription?.unsubscribe();
             };
           }, []);
 
