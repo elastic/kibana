de6ea008254c6f47ac0910b8be1905af1080032b
Bailey Cash <bailey.cash@elastic.co>
2025-04-15 03:10:26 -0400

    [8.x] [SLO]: Add filtering to SLO Management table, improve UX (#216040) (#218171)
    # Backport
    
    This will backport the following commits from `main` to `8.x`:
    - [[SLO]: Add filtering to SLO Management table, improve UX
    (#216040)](https://github.com/elastic/kibana/pull/216040)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Bailey
    Cash","email":"bailey.cash@elastic.co"},"sourceCommit":{"committedDate":"2025-03-27T22:36:16Z","message":"[SLO]:
    Add filtering to SLO Management table, improve UX (#216040)\n\n##
    Summary\n\nResolves #214258 \n\n- Updates search bar to utilize
    UnifiedSearchBar\n- Adds ability to filter SLOs by tags (OR operator)\n-
    Makes improvements to version display\n\n![Screenshot 2025-03-26 at 2
    55\n01 PM](https://github.com/user-attachments/assets/cf8c19e4-7a9f-4f2e-bd5d-b820b8f9bf23)\n![Screenshot
    2025-03-26 at 2
    54\n20 PM](https://github.com/user-attachments/assets/46e968ff-352a-4f4e-b762-a96c727c08f4)\n\n---------\n\nCo-authored-by:
    kdelemme <kdelemme@users.noreply.github.com>\nCo-authored-by: Elastic
    Machine <elasticmachine@users.noreply.github.com>\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"40e95f00f1b2f3b2ba76f958727e9bea24424679","branchLabelMapping":{"^v9.1.0$":"main","^v8.19.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","backport:skip","Team:obs-ux-management","v9.1.0"],"title":"[SLO]:
    Add filtering to SLO Management table, improve
    UX","number":216040,"url":"https://github.com/elastic/kibana/pull/216040","mergeCommit":{"message":"[SLO]:
    Add filtering to SLO Management table, improve UX (#216040)\n\n##
    Summary\n\nResolves #214258 \n\n- Updates search bar to utilize
    UnifiedSearchBar\n- Adds ability to filter SLOs by tags (OR operator)\n-
    Makes improvements to version display\n\n![Screenshot 2025-03-26 at 2
    55\n01 PM](https://github.com/user-attachments/assets/cf8c19e4-7a9f-4f2e-bd5d-b820b8f9bf23)\n![Screenshot
    2025-03-26 at 2
    54\n20 PM](https://github.com/user-attachments/assets/46e968ff-352a-4f4e-b762-a96c727c08f4)\n\n---------\n\nCo-authored-by:
    kdelemme <kdelemme@users.noreply.github.com>\nCo-authored-by: Elastic
    Machine <elasticmachine@users.noreply.github.com>\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"40e95f00f1b2f3b2ba76f958727e9bea24424679"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/216040","number":216040,"mergeCommit":{"message":"[SLO]:
    Add filtering to SLO Management table, improve UX (#216040)\n\n##
    Summary\n\nResolves #214258 \n\n- Updates search bar to utilize
    UnifiedSearchBar\n- Adds ability to filter SLOs by tags (OR operator)\n-
    Makes improvements to version display\n\n![Screenshot 2025-03-26 at 2
    55\n01 PM](https://github.com/user-attachments/assets/cf8c19e4-7a9f-4f2e-bd5d-b820b8f9bf23)\n![Screenshot
    2025-03-26 at 2
    54\n20 PM](https://github.com/user-attachments/assets/46e968ff-352a-4f4e-b762-a96c727c08f4)\n\n---------\n\nCo-authored-by:
    kdelemme <kdelemme@users.noreply.github.com>\nCo-authored-by: Elastic
    Machine <elasticmachine@users.noreply.github.com>\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"40e95f00f1b2f3b2ba76f958727e9bea24424679"}}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Shahzad <shahzad31comp@gmail.com>

--- x-pack/solutions/observability/plugins/slo/public/hooks/use_fetch_slo_definitions.ts
+++ x-pack/solutions/observability/plugins/slo/public/hooks/use_fetch_slo_definitions.ts
@@ -21,6 +21,7 @@ export interface UseFetchSloDefinitionsResponse {
 interface SLODefinitionParams {
   name?: string;
   includeOutdatedOnly?: boolean;
+  tags?: string[];
   page?: number;
   perPage?: number;
 }
@@ -28,14 +29,16 @@ interface SLODefinitionParams {
 export function useFetchSloDefinitions({
   name = '',
   includeOutdatedOnly = false,
+  tags = [],
   page = 1,
   perPage = 100,
 }: SLODefinitionParams): UseFetchSloDefinitionsResponse {
   const { sloClient } = usePluginContext();
   const search = name.endsWith('*') ? name : `${name}*`;
+  const validTags = tags.filter((tag) => !!tag).join();
 
   const { isLoading, isError, isSuccess, data, refetch } = useQuery({
-    queryKey: sloKeys.definitions({ search, page, perPage, includeOutdatedOnly }),
+    queryKey: sloKeys.definitions({ search, page, perPage, includeOutdatedOnly, validTags }),
     queryFn: async ({ signal }) => {
       try {
         return await sloClient.fetch('GET /api/observability/slos/_definitions 2023-10-31', {
@@ -43,6 +46,7 @@ export function useFetchSloDefinitions({
             query: {
               ...(search !== undefined && { search }),
               ...(includeOutdatedOnly !== undefined && { includeOutdatedOnly }),
+              ...(validTags?.length && { tags: validTags }),
               ...(page !== undefined && { page: String(page) }),
               ...(perPage !== undefined && { perPage: String(perPage) }),
             },
