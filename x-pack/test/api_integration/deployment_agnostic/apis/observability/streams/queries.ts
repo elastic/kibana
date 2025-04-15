00c2c3e14962a8f505b3f90eac110851105f0b40
Joe Reuter <johannes.reuter@elastic.co>
2025-04-15 10:43:10 +0200

    fix test

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/streams/queries.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/streams/queries.ts
@@ -27,7 +27,7 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
       wired: {
         routing: [],
         fields: {
-          numberfield: {
+          'attributes.numberfield': {
             type: 'long',
           },
         },
