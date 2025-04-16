f2e3ac3d71fe97f44fdb43f9ad0e3a5113252465
Francesco Fagnani <fagnani.francesco@gmail.com>
2025-04-16 10:05:39 +0200

    [8.17] [Synthetics] Sync global parameters is called in the endpoints to add, edit or delete global params (#216197) (#218296)
    # Backport
    
    This will backport the following commits from `main` to `8.17`:
    - [[Synthetics] Sync global parameters is called in the endpoints to
    add, edit or delete global params
    (#216197)](https://github.com/elastic/kibana/pull/216197)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Francesco
    Fagnani","email":"fagnani.francesco@gmail.com"},"sourceCommit":{"committedDate":"2025-04-15T14:53:14Z","message":"[Synthetics]
    Sync global parameters is called in the endpoints to add, edit or delete
    global params (#216197)\n\nThis PR closes #215668.\n\nThe global
    parameters are synched in the endpoints where they are\ncreated, edited
    or deleted.\n\n---------\n\nCo-authored-by: Shahzad
    <shahzad31comp@gmail.com>","sha":"f693e7218e0edb145f1d4344b484b5b48e80998d","branchLabelMapping":{"^v9.1.0$":"main","^v8.19.0$":"8.x","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:fix","Team:obs-ux-management","backport:version","v9.1.0","v8.19.0","v8.18.1","v9.0.1","v8.17.5"],"title":"[Synthetics]
    Sync global parameters is called in the endpoints to add, edit or delete
    global
    params","number":216197,"url":"https://github.com/elastic/kibana/pull/216197","mergeCommit":{"message":"[Synthetics]
    Sync global parameters is called in the endpoints to add, edit or delete
    global params (#216197)\n\nThis PR closes #215668.\n\nThe global
    parameters are synched in the endpoints where they are\ncreated, edited
    or deleted.\n\n---------\n\nCo-authored-by: Shahzad
    <shahzad31comp@gmail.com>","sha":"f693e7218e0edb145f1d4344b484b5b48e80998d"}},"sourceBranch":"main","suggestedTargetBranches":["8.x","8.18","8.17"],"targetPullRequestStates":[{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/216197","number":216197,"mergeCommit":{"message":"[Synthetics]
    Sync global parameters is called in the endpoints to add, edit or delete
    global params (#216197)\n\nThis PR closes #215668.\n\nThe global
    parameters are synched in the endpoints where they are\ncreated, edited
    or deleted.\n\n---------\n\nCo-authored-by: Shahzad
    <shahzad31comp@gmail.com>","sha":"f693e7218e0edb145f1d4344b484b5b48e80998d"}},{"branch":"8.x","label":"v8.19.0","branchLabelMappingKey":"^v8.19.0$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.18","label":"v8.18.1","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"9.0","label":"v9.0.1","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"url":"https://github.com/elastic/kibana/pull/218290","number":218290,"state":"OPEN"},{"branch":"8.17","label":"v8.17.5","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/plugins/observability_solution/synthetics/public/apps/synthetics/utils/testing/__mocks__/synthetics_store.mock.ts
+++ x-pack/plugins/observability_solution/synthetics/public/apps/synthetics/utils/testing/__mocks__/synthetics_store.mock.ts
@@ -114,11 +114,6 @@ export const mockState: SyntheticsAppState = {
     error: null,
     data: null,
   },
-  settings: {
-    loading: false,
-    error: null,
-    success: null,
-  },
   dynamicSettings: {
     loading: false,
     locationMonitors: [],
