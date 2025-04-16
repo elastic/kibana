9e6acedb2d964f565e7bf5ef2e5becbac899e913
Kfir Peled <61654899+kfirpeled@users.noreply.github.com>
2025-04-16 17:25:37 +0300

    Fixed root css decorator

--- x-pack/solutions/security/packages/kbn-cloud-security-posture/graph/src/components/graph_layout.stories.tsx
+++ x-pack/solutions/security/packages/kbn-cloud-security-posture/graph/src/components/graph_layout.stories.tsx
@@ -167,8 +167,8 @@ const Template: StoryFn<GraphData> = ({ nodes, edges, interactive }: GraphData) 
     <ThemeProvider theme={{ darkMode: false }}>
       <Graph
         css={css`
-          width: 100vw;
-          height: 100vh;
+          height: 100%;
+          width: 100%;
         `}
         nodes={nodes}
         edges={edges}
