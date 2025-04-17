b210aacba511e336b125a1d9db3d504fc949f8ec
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-04-17 23:55:46 +0200

    Add kibana mount to shared bundle

--- src/platform/packages/private/kbn-ui-shared-deps-src/src/definitions.js
+++ src/platform/packages/private/kbn-ui-shared-deps-src/src/definitions.js
@@ -122,6 +122,7 @@ const externals = {
   '@kbn/react-kibana-context-render': '__kbnSharedDeps__.KbnReactKibanaContextRender',
   '@kbn/react-kibana-context-theme': '__kbnSharedDeps__.KbnReactKibanaContextTheme',
   '@kbn/shared-ux-router': '__kbnSharedDeps__.KbnSharedUxRouter',
+  '@kbn/kibana-mount': '__kbnSharedDeps__.KbnKibanaMount',
 };
 
 module.exports = { distDir, jsFilename, cssDistFilename, externals };
