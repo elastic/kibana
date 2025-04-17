4e07277143cb9d891cd12a7ec27417b748c22f9c
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2025-04-17 13:38:35 +0200

    fix license header

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/routes.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/routes.tsx
@@ -6,21 +6,6 @@
  */
 import React from 'react';
 import { Route, Routes } from '@kbn/shared-ux-router';
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-import React from 'react';
-import { Route, Routes } from '@kbn/shared-ux-router';
 
 import { NotFoundPage } from '../app/404';
 import { withSecurityRoutePageWrapper } from '../common/components/security_route_page_wrapper';
