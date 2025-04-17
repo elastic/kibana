1c196313920c3f58199ea095a7bd1b83201e0c65
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-04-17 16:51:18 +0200

    Handle change_point

--- src/platform/packages/shared/kbn-esql-validation-autocomplete/src/definitions/commands.ts
+++ src/platform/packages/shared/kbn-esql-validation-autocomplete/src/definitions/commands.ts
@@ -806,6 +806,46 @@ export const commandDefinitions: Array<CommandDefinition<any>> = [
       return messages;
     },
     suggest: suggestForChangePoint,
+    fieldsSuggestionsAfter: (
+      command: ESQLAstCommand,
+      previousCommandFields: ESQLRealField[],
+      userDefinedColumns: ESQLRealField[]
+    ) => {
+      const columns: ESQLRealField[] = [];
+
+      walk(command, {
+        visitCommand: (node) => {
+          const optionArgs = node.args.filter((arg) => isOptionItem(arg) && arg.name === 'as');
+          if (optionArgs.length) {
+            const [optionArg] = optionArgs;
+            const args = (optionArg as ESQLCommandOption).args.filter(isColumnItem);
+            const asColumns = args.map((arg, index) => {
+              // The first column is always a keyword, the second one is a double
+              const type = index === 0 ? ('keyword' as const) : ('double' as const);
+              return {
+                name: arg.name,
+                type,
+              };
+            });
+            columns.push(...asColumns);
+          } else {
+            // If no AS clause is provided, use the default column names
+            columns.push(
+              {
+                name: 'type',
+                type: 'keyword' as const,
+              },
+              {
+                name: 'pvalue',
+                type: 'double' as const,
+              }
+            );
+          }
+        },
+      });
+
+      return [...previousCommandFields, ...columns];
+    },
   },
   {
     hidden: true,
