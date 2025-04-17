72c3ddf34a2eef9091f3b638e087edef89a29745
paulina.shakirova <paulina.shakirova@elastic.co>
2025-04-17 16:35:43 +0200

    [A11y][ML] Fix job wizard fields step 3 title announcements

--- x-pack/platform/plugins/shared/ml/public/application/jobs/new_job/pages/components/job_details_step/components/additional_section/components/calendars/description_dst.tsx
+++ x-pack/platform/plugins/shared/ml/public/application/jobs/new_job/pages/components/job_details_step/components/additional_section/components/calendars/description_dst.tsx
@@ -12,40 +12,46 @@ import { FormattedMessage } from '@kbn/i18n-react';
 import { EuiDescribedFormGroup, EuiFormRow, EuiLink } from '@elastic/eui';
 import { useMlKibana } from '../../../../../../../../../contexts/kibana';
 
-export const DescriptionDst: FC<PropsWithChildren<unknown>> = memo(({ children }) => {
-  const {
-    services: { docLinks },
-  } = useMlKibana();
-  const docsUrl = docLinks.links.ml.calendars;
-  const title = i18n.translate(
-    'xpack.ml.newJob.wizard.jobDetailsStep.additionalSection.calendarsDstSelection.title',
-    {
-      defaultMessage: 'DST Calendars',
-    }
-  );
-  return (
-    <EuiDescribedFormGroup
-      title={<h3>{title}</h3>}
-      description={
-        <FormattedMessage
-          id="xpack.ml.newJob.wizard.jobDetailsStep.additionalSection.calendarsDstSelection.description"
-          defaultMessage="A list of scheduled events you want to ignore, taking into account daylight saving time shifts. {learnMoreLink}"
-          values={{
-            learnMoreLink: (
-              <EuiLink href={docsUrl} target="_blank">
-                <FormattedMessage
-                  id="xpack.ml.newJob.wizard.jobDetailsStep.additionalSection.calendarsDstSelection.learnMoreLinkText"
-                  defaultMessage="Learn more"
-                />
-              </EuiLink>
-            ),
-          }}
-        />
+interface Props {
+  calendarsTitleId: string;
+}
+
+export const DescriptionDst: FC<PropsWithChildren<Props>> = memo(
+  ({ children, calendarsTitleId }) => {
+    const {
+      services: { docLinks },
+    } = useMlKibana();
+    const docsUrl = docLinks.links.ml.calendars;
+    const title = i18n.translate(
+      'xpack.ml.newJob.wizard.jobDetailsStep.additionalSection.calendarsDstSelection.title',
+      {
+        defaultMessage: 'DST Calendars',
       }
-    >
-      <EuiFormRow>
-        <>{children}</>
-      </EuiFormRow>
-    </EuiDescribedFormGroup>
-  );
-});
+    );
+    return (
+      <EuiDescribedFormGroup
+        title={<h3 id={calendarsTitleId}>{title}</h3>}
+        description={
+          <FormattedMessage
+            id="xpack.ml.newJob.wizard.jobDetailsStep.additionalSection.calendarsDstSelection.description"
+            defaultMessage="A list of scheduled events you want to ignore, taking into account daylight saving time shifts. {learnMoreLink}"
+            values={{
+              learnMoreLink: (
+                <EuiLink href={docsUrl} target="_blank">
+                  <FormattedMessage
+                    id="xpack.ml.newJob.wizard.jobDetailsStep.additionalSection.calendarsDstSelection.learnMoreLinkText"
+                    defaultMessage="Learn more"
+                  />
+                </EuiLink>
+              ),
+            }}
+          />
+        }
+      >
+        <EuiFormRow>
+          <>{children}</>
+        </EuiFormRow>
+      </EuiDescribedFormGroup>
+    );
+  }
+);
