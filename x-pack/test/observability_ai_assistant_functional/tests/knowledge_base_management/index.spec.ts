1a3122926e784ce7c454329a552354bb1d0201ab
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-04-17 10:48:15 -0400

    [Obs AI Assistant] Skip summarize tests (#218498)
    Closes https://github.com/elastic/kibana/issues/218327
    
    ## Problem
    2 tests related to the summarize function is failing. One is the API
    test, the other is a functional test. Haven't found the root cause yet.
    Skipping the tests in order to unblock the pipelines
    
    
    ### Checklist
    
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- x-pack/test/observability_ai_assistant_functional/tests/knowledge_base_management/index.spec.ts
+++ x-pack/test/observability_ai_assistant_functional/tests/knowledge_base_management/index.spec.ts
@@ -48,7 +48,8 @@ export default function ApiTest({ getService, getPageObjects }: FtrProviderConte
     }).expect(200);
   }
 
-  describe('Knowledge management tab', () => {
+  // Failing test: https://github.com/elastic/kibana/issues/218327
+  describe.skip('Knowledge management tab', () => {
     before(async () => {
       await clearKnowledgeBase(es);
 
