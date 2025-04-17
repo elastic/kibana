5d4af8d671a446cfeb07ffa60c7581ef3772075b
Cristina Amico <criamico@users.noreply.github.com>
2025-04-17 17:44:08 +0200

    Split tests

---
+++ x-pack/platform/plugins/shared/fleet/server/services/epm/packages/input_type_packages.test.ts
@@ -0,0 +1,137 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { savedObjectsClientMock } from '@kbn/core/server/mocks';
+import type { ElasticsearchClient } from '@kbn/core/server';
+
+import { appContextService } from '../../app_context';
+import { PackageNotFoundError } from '../../../errors';
+
+import { dataStreamService } from '../../data_streams';
+
+import { getInstalledPackageWithAssets } from './get';
+import { optimisticallyAddEsAssetReferences } from './es_assets_reference';
+import { installAssetsForInputPackagePolicy } from './input_type_packages';
+
+jest.mock('../../data_streams');
+jest.mock('./get');
+jest.mock('./install_index_template_pipeline');
+jest.mock('./es_assets_reference');
+
+jest.mock('../../app_context', () => {
+  const logger = { error: jest.fn(), debug: jest.fn(), warn: jest.fn(), info: jest.fn() };
+  const mockedSavedObjectTagging = {
+    createInternalAssignmentService: jest.fn(),
+    createTagClient: jest.fn(),
+  };
+
+  return {
+    appContextService: {
+      getLogger: jest.fn(() => {
+        return logger;
+      }),
+      getTelemetryEventsSender: jest.fn(),
+      getSavedObjects: jest.fn(() => ({
+        createImporter: jest.fn(),
+      })),
+      getConfig: jest.fn(() => ({})),
+      getSavedObjectsTagging: jest.fn(() => mockedSavedObjectTagging),
+      getInternalUserSOClientForSpaceId: jest.fn(),
+      getExperimentalFeatures: jest.fn(),
+    },
+  };
+});
+
+describe('installAssetsForInputPackagePolicy', () => {
+  beforeEach(() => {
+    jest.mocked(optimisticallyAddEsAssetReferences).mockReset();
+  });
+  it('should do nothing for non input package', async () => {
+    const mockedLogger = jest.mocked(appContextService.getLogger());
+    await installAssetsForInputPackagePolicy({
+      pkgInfo: {
+        type: 'integration',
+      } as any,
+      soClient: savedObjectsClientMock.create(),
+      esClient: {} as ElasticsearchClient,
+      force: false,
+      logger: mockedLogger,
+      packagePolicy: {} as any,
+    });
+  });
+  const TEST_PKG_INFO_INPUT = {
+    type: 'input',
+    name: 'test',
+    version: '1.0.0',
+    policy_templates: [
+      {
+        name: 'log',
+        type: 'log',
+      },
+    ],
+  };
+  it('should throw for input package if package is not installed', async () => {
+    jest.mocked(dataStreamService).getMatchingDataStreams.mockResolvedValue([]);
+    jest.mocked(getInstalledPackageWithAssets).mockResolvedValue(undefined);
+    const mockedLogger = jest.mocked(appContextService.getLogger());
+
+    await expect(() =>
+      installAssetsForInputPackagePolicy({
+        pkgInfo: TEST_PKG_INFO_INPUT as any,
+        soClient: savedObjectsClientMock.create(),
+        esClient: {} as ElasticsearchClient,
+        force: false,
+        logger: mockedLogger,
+        packagePolicy: {
+          inputs: [{ type: 'log', streams: [{ type: 'log', vars: { dataset: 'test.tata' } }] }],
+        } as any,
+      })
+    ).rejects.toThrowError(PackageNotFoundError);
+  });
+
+  it('should install es index patterns for input package if package is installed', async () => {
+    jest.mocked(dataStreamService).getMatchingDataStreams.mockResolvedValue([]);
+
+    jest.mocked(getInstalledPackageWithAssets).mockResolvedValue({
+      installation: {
+        name: 'test',
+        version: '1.0.0',
+      },
+      packageInfo: TEST_PKG_INFO_INPUT,
+      assetsMap: new Map(),
+      paths: [],
+    } as any);
+    const mockedLogger = jest.mocked(appContextService.getLogger());
+
+    await installAssetsForInputPackagePolicy({
+      pkgInfo: TEST_PKG_INFO_INPUT as any,
+
+      soClient: savedObjectsClientMock.create(),
+      esClient: {} as ElasticsearchClient,
+      force: false,
+      logger: mockedLogger,
+      packagePolicy: {
+        inputs: [
+          {
+            name: 'log',
+            type: 'log',
+            streams: [{ type: 'log', vars: { 'data_stream.dataset': { value: 'test.tata' } } }],
+          },
+        ],
+      } as any,
+    });
+
+    expect(jest.mocked(optimisticallyAddEsAssetReferences)).toBeCalledWith(
+      expect.anything(),
+      expect.anything(),
+      expect.anything(),
+      {
+        'test.tata': 'log-test.tata-*',
+      }
+    );
+  });
+});
