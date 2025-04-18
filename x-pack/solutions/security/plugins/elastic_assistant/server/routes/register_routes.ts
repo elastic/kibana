bcfa5266d0a01f11ae4e8263cac284096eed1ff2
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-04-18 17:57:38 +0200

    [Attack Discovery][Scheduling] Revert removed AD scheduling route registration (#218649)
    ## Summary
    
    These changes revert accidentally removed attack discovery scheduling
    routes registration by this PR
    https://github.com/elastic/kibana/pull/218018/files#diff-fc08114e3940ca525cd8a2b7d746786ddabf8d27f8595438cdfc19371ee23831L44
    
    Since the changes from that PR did not go into the `8.19`, we would not
    need the backport to that branch.
    
    ## NOTES
    
    The feature is hidden behind the feature flag (in `kibana.dev.yml`):
    
    ```
    feature_flags.overrides:
      securitySolution.assistantAttackDiscoverySchedulingEnabled: true
    ```

--- x-pack/solutions/security/plugins/elastic_assistant/server/routes/register_routes.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/server/routes/register_routes.ts
@@ -43,6 +43,13 @@ import { deleteKnowledgeBaseEntryRoute } from './knowledge_base/entries/delete_r
 import { updateKnowledgeBaseEntryRoute } from './knowledge_base/entries/update_route';
 import { getKnowledgeBaseEntryRoute } from './knowledge_base/entries/get_route';
 import { bulkAlertSummaryRoute } from './alert_summary/bulk_actions_route';
+import { createAttackDiscoverySchedulesRoute } from './attack_discovery/schedules/create';
+import { getAttackDiscoverySchedulesRoute } from './attack_discovery/schedules/get';
+import { updateAttackDiscoverySchedulesRoute } from './attack_discovery/schedules/update';
+import { deleteAttackDiscoverySchedulesRoute } from './attack_discovery/schedules/delete';
+import { findAttackDiscoverySchedulesRoute } from './attack_discovery/schedules/find';
+import { disableAttackDiscoverySchedulesRoute } from './attack_discovery/schedules/disable';
+import { enableAttackDiscoverySchedulesRoute } from './attack_discovery/schedules/enable';
 
 export const registerRoutes = (
   router: ElasticAssistantPluginRouter,
@@ -102,6 +109,15 @@ export const registerRoutes = (
   getAttackDiscoveryRoute(router);
   postAttackDiscoveryRoute(router);
   cancelAttackDiscoveryRoute(router);
+
+  // Attack Discovery Schedules
+  createAttackDiscoverySchedulesRoute(router);
+  getAttackDiscoverySchedulesRoute(router);
+  findAttackDiscoverySchedulesRoute(router);
+  updateAttackDiscoverySchedulesRoute(router);
+  deleteAttackDiscoverySchedulesRoute(router);
+  disableAttackDiscoverySchedulesRoute(router);
+  enableAttackDiscoverySchedulesRoute(router);
 
   // Alert Summary
   bulkAlertSummaryRoute(router, logger);
