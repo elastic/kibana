284e437c71541cdba6a32099ee3e96dbaea0579f
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-04-19 01:03:35 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/security/plugins/elastic_assistant/server/routes/post_actions_connector_execute.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/server/routes/post_actions_connector_execute.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { IKibanaResponse, IRouter, Logger } from '@kbn/core/server';
+import { IRouter, Logger } from '@kbn/core/server';
 import { transformError } from '@kbn/securitysolution-es-utils';
 import { getRequestAbortedSignal } from '@kbn/data-plugin/server';
 
