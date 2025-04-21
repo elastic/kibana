b86ec5caa58299b759fda59f9df938093394c588
Sid <siddharthmantri1@gmail.com>
2025-04-21 15:24:13 +0200

    revert val check

--- src/platform/packages/shared/kbn-openapi-generator/src/template_service/register_helpers.ts
+++ src/platform/packages/shared/kbn-openapi-generator/src/template_service/register_helpers.ts
@@ -48,9 +48,7 @@ export function registerHelpers(handlebarsInstance: typeof Handlebars) {
    * Check if the OpenAPI schema is unknown
    */
   handlebarsInstance.registerHelper('isUnknown', (val: object) => {
-    return (
-      val && !('type' in val || '$ref' in val || 'anyOf' in val || 'oneOf' in val || 'allOf' in val)
-    );
+    return !('type' in val || '$ref' in val || 'anyOf' in val || 'oneOf' in val || 'allOf' in val);
   });
   handlebarsInstance.registerHelper(
     'replace',
