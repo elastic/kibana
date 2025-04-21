cfe143afaf2f6afab128a2d678f8bd4c15f95e9d
Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>
2025-04-21 13:28:30 +0200

    [UA] Show complete status when marking as read-only (#218261)
    Fixes: https://github.com/elastic/kibana/issues/213348
    
    ## Summary
    When a user marked as read-only an index that had previously failed to
    be reindexed, we weren't showing the completed read-only step but the
    failure of the reindex. This PR changes that so the status of the flyout
    and the table shows status of the reindexing.

--- x-pack/platform/plugins/private/upgrade_assistant/__jest__/client_integration/es_deprecations/reindex_deprecation_flyout.test.ts
+++ x-pack/platform/plugins/private/upgrade_assistant/__jest__/client_integration/es_deprecations/reindex_deprecation_flyout.test.ts
@@ -267,6 +267,35 @@ describe('Reindex deprecation flyout', () => {
     });
   });
 
+  describe('readonly', () => {
+    it('shows succed state when marking as readonly an index that has failed to reindex', async () => {
+      httpRequestsMockHelpers.setReindexStatusResponse(MOCK_REINDEX_DEPRECATION.index!, {
+        reindexOp: {
+          status: ReindexStatus.failed,
+          lastCompletedStep: ReindexStep.reindexCompleted,
+          reindexTaskPercComplete: 1,
+        },
+        warnings: [],
+        hasRequiredPrivileges: true,
+        meta: defaultReindexStatusMeta,
+      });
+
+      const { actions, find, exists } = testBed;
+
+      await actions.table.clickDeprecationRowAt('reindex', 0);
+      await actions.reindexDeprecationFlyout.clickReadOnlyButton();
+      await actions.reindexDeprecationFlyout.clickReadOnlyButton();
+
+      expect(exists('resolvedDeprecationBadge')).toBe(true);
+
+      expect(exists('updateIndexFlyoutTitle')).toBe(true);
+      expect(find('updateIndexFlyoutTitle').text()).toBe('Operation completed');
+
+      expect(exists('stepProgressStep')).toBe(true);
+      expect(find('stepProgressStep').text()).toBe('Setting foo index to read-only.');
+    });
+  });
+
   describe('follower index', () => {
     it('displays follower index callout and only shows mark as read-only button when index is a follower index', async () => {
       httpRequestsMockHelpers.setReindexStatusResponse(MOCK_REINDEX_DEPRECATION.index!, {
