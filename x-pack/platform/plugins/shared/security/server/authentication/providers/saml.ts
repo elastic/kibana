3c977d3cda688ace1ec425e6069aae00a2ea3f65
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-04-22 19:15:25 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/security/server/authentication/providers/saml.ts
+++ x-pack/platform/plugins/shared/security/server/authentication/providers/saml.ts
@@ -383,7 +383,6 @@ export class SAMLAuthenticationProvider extends BaseAuthenticationProvider {
         },
       })) as any;
     } catch (err) {
-
       this.logger.error(
         `Failed to log in with SAML response, ${
           !isIdPInitiatedLogin ? `current requestIds: ${stateRequestIds}, ` : ''
