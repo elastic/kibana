4a4edbff626ba68d0125d0475ff7e59895c063bf
Jesus Wahrman <jesus.wahrman@elastic.co>
2025-04-22 17:03:57 +0200

    remove REMOVED_TYPES import and update test snapshot

--- src/core/packages/saved-objects/migration-server-internal/src/core/unused_types.test.ts
+++ src/core/packages/saved-objects/migration-server-internal/src/core/unused_types.test.ts
@@ -8,128 +8,27 @@
  */
 
 import { buildExcludeUnusedTypesQuery } from './unused_types';
-import { REMOVED_TYPES } from '@kbn/core-saved-objects-base-server-internal';
 
 describe('buildExcludeUnusedTypesQuery', () => {
   it('should build the correct query with REMOVED_TYPES', () => {
-    const query = buildExcludeUnusedTypesQuery(REMOVED_TYPES);
+    const query = buildExcludeUnusedTypesQuery(['exclude-type', 'exclude_type', 'excludeType']);
     expect(query).toMatchInlineSnapshot(`
       Object {
         "bool": Object {
           "must_not": Array [
             Object {
               "term": Object {
-                "type": "apm-services-telemetry",
+                "type": "exclude-type",
               },
             },
             Object {
               "term": Object {
-                "type": "application_usage_transactional",
+                "type": "exclude_type",
               },
             },
             Object {
               "term": Object {
-                "type": "background-session",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "cases-sub-case",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "csp_rule",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "endpoint:user-artifact",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "file-upload-telemetry",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "fleet-agent-actions",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "fleet-agent-events",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "fleet-agents",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "fleet-enrollment-api-keys",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "guided-setup-state",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "investigation",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "maps-telemetry",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "ml-telemetry",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "osquery-usage-metric",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "server",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "siem-detection-engine-rule-execution-info",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "siem-detection-engine-rule-status",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "timelion-sheet",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "tsvb-validation-telemetry",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "ui-counter",
-              },
-            },
-            Object {
-              "term": Object {
-                "type": "upgrade-assistant-telemetry",
+                "type": "excludeType",
               },
             },
           ],
