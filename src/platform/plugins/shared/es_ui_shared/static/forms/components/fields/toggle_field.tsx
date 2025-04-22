elastic/kibana

commit efe44b2f2d97fd35f152fd93c5a2ffdcf57dbe2e

fix: [Security:Cases] Sync alert status with case status toggle button under case settings on create case page is incorrectly announced (#205604)
Closes: #205544

## Description
When user tabs over sync alert status with case status toggle button
under case settings on create case page, screenreader announces On, On
switch without giving any context.

## Preconditions
Security solution -> on cases page -> create case

## Changes made: 
1. added context for **EuiSwitch** by passing `aria-labelledby`
attribute

## Screen


![image](https://github.com/user-attachments/assets/94fb0413-d0c6-4a1c-9071-a6458e2cd379)

src/platform/plugins/shared/es_ui_shared/static/forms/components/fields/toggle_field.tsx
@@ -8,7 +8,7 @@
  */
 
 import React from 'react';
-import { EuiFormRow, EuiSwitch, EuiSwitchEvent } from '@elastic/eui';
+import { EuiFormRow, EuiSwitch, EuiSwitchEvent, useGeneratedHtmlId } from '@elastic/eui';
 
 import { FieldHook, getFieldValidityAndErrorMessage } from '../../hook_form_lib';
 
@@ -21,6 +21,7 @@ interface Props {
 
 export const ToggleField = ({ field, euiFieldProps = {}, idAria, ...rest }: Props) => {
   const { isInvalid, errorMessage } = getFieldValidityAndErrorMessage(field);
+  const formId = useGeneratedHtmlId();
 
   // Shim for sufficient overlap between EuiSwitchEvent and FieldHook[onChange] event
   const onChange = (e: EuiSwitchEvent) => {
@@ -32,6 +33,7 @@ export const ToggleField = ({ field, euiFieldProps = {}, idAria, ...rest }: Prop
 
   return (
     <EuiFormRow
+      id={formId}
       helpText={field.helpText}
       error={errorMessage}
       isInvalid={isInvalid}
@@ -44,6 +46,7 @@ export const ToggleField = ({ field, euiFieldProps = {}, idAria, ...rest }: Prop
         checked={field.value as boolean}
         onChange={onChange}
         data-test-subj="input"
+        aria-labelledby={formId}
         {...euiFieldProps}
       />
     </EuiFormRow>

commit 3ad8bad478008c5587c6b41d84430695d148d561

fix: [Platform:StackManagement:IngestPipelines:Create pipeline page]Switch button missing title from announcement (#218214)
Closes: #217971
Rework of: https://github.com/elastic/kibana/pull/205604

**Description**
This update improves accessibility by ensuring that dialog modals,
flyouts, and field titles are properly announced for users—particularly
those using assistive technologies. It ensures users are clearly
informed about which dialog or flyout has opened, which field is active,
and what input is expected.

**Changes Made**
- Reverted a regression introduced in
[#205604](https://github.com/elastic/kibana/pull/205604), which involved
incorrect usage of `EuiSwitch`. The component now uses the default
`EuiSwitch` behavior as intended.

- Updated the Cases page UI to better align with the EUI style guide.

**Screenshots**

- Cases Page

<img width="947" alt="Cases Page Screenshot"
src="https://github.com/user-attachments/assets/0b8d7fd4-543f-4751-a17b-3b774c0e1c8c"
/>

- Ingest Pipelines Page

<img width="947" alt="Ingest Pipelines Page Screenshot"
src="https://github.com/user-attachments/assets/78185612-9a01-4227-a1a1-b928e848ca78"
/>

src/platform/plugins/shared/es_ui_shared/static/forms/components/fields/toggle_field.tsx
@@ -8,7 +8,7 @@
  */
 
 import React from 'react';
-import { EuiFormRow, EuiSwitch, EuiSwitchEvent, useGeneratedHtmlId } from '@elastic/eui';
+import { EuiFormRow, EuiSwitch, EuiSwitchEvent } from '@elastic/eui';
 
 import { FieldHook, getFieldValidityAndErrorMessage } from '../../hook_form_lib';
 
@@ -21,7 +21,6 @@ interface Props {
 
 export const ToggleField = ({ field, euiFieldProps = {}, idAria, ...rest }: Props) => {
   const { isInvalid, errorMessage } = getFieldValidityAndErrorMessage(field);
-  const formId = useGeneratedHtmlId();
 
   // Shim for sufficient overlap between EuiSwitchEvent and FieldHook[onChange] event
   const onChange = (e: EuiSwitchEvent) => {
@@ -33,7 +32,6 @@ export const ToggleField = ({ field, euiFieldProps = {}, idAria, ...rest }: Prop
 
   return (
     <EuiFormRow
-      id={formId}
       helpText={field.helpText}
       error={errorMessage}
       isInvalid={isInvalid}
@@ -46,7 +44,6 @@ export const ToggleField = ({ field, euiFieldProps = {}, idAria, ...rest }: Prop
         checked={field.value as boolean}
         onChange={onChange}
         data-test-subj="input"
-        aria-labelledby={formId}
         {...euiFieldProps}
       />
     </EuiFormRow>

x-pack/platform/plugins/shared/cases/public/components/case_form_fields/sync_alerts_toggle.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { memo } from 'react';
-import { UseField, useFormData } from '@kbn/es-ui-shared-plugin/static/forms/hook_form_lib';
+import { UseField } from '@kbn/es-ui-shared-plugin/static/forms/hook_form_lib';
 import { ToggleField } from '@kbn/es-ui-shared-plugin/static/forms/components';
 import * as i18n from '../create/translations';
 
@@ -15,8 +15,6 @@ interface Props {
 }
 
 const SyncAlertsToggleComponent: React.FC<Props> = ({ isLoading }) => {
-  const [{ syncAlerts }] = useFormData({ watch: ['syncAlerts'] });
-
   return (
     <UseField
       path="syncAlerts"
@@ -25,10 +23,9 @@ const SyncAlertsToggleComponent: React.FC<Props> = ({ isLoading }) => {
       componentProps={{
         idAria: 'caseSyncAlerts',
         'data-test-subj': 'caseSyncAlerts',
-        label: i18n.SYNC_ALERTS_LABEL,
         euiFieldProps: {
           disabled: isLoading,
-          label: syncAlerts ? i18n.SYNC_ALERTS_SWITCH_LABEL_ON : i18n.SYNC_ALERTS_SWITCH_LABEL_OFF,
+          label: i18n.SYNC_ALERTS_LABEL,
         },
       }}
     />

x-pack/platform/plugins/shared/cases/public/components/case_form_fields/sync_alerts_toggle.test.tsx
@@ -36,7 +36,7 @@ describe('SyncAlertsToggle', () => {
     const syncAlerts = await screen.findByTestId('caseSyncAlerts');
     expect(syncAlerts).toBeInTheDocument();
     expect(within(syncAlerts).getByRole('switch')).toHaveAttribute('aria-checked', 'true');
-    expect(within(syncAlerts).getByText('On')).toBeInTheDocument();
+    expect(within(syncAlerts).getByText('Sync alert status with case status')).toBeInTheDocument();
   });
 
   it('it toggles the switch', async () => {
@@ -51,7 +51,7 @@ describe('SyncAlertsToggle', () => {
     await userEvent.click(within(syncAlerts).getByRole('switch'));
 
     expect(await screen.findByRole('switch')).toHaveAttribute('aria-checked', 'false');
-    expect(await screen.findByText('Off')).toBeInTheDocument();
+    expect(await screen.findByText('Sync alert status with case status')).toBeInTheDocument();
   });
 
   it('calls onSubmit with correct data', async () => {