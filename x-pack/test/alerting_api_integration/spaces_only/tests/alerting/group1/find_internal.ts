f203d03d3d54398e09a84bf5c32024b974579e66
Panagiota Mitsopoulou <panagiota.mitsopoulou@elastic.co>
2025-04-22 23:20:49 +0200

    New artifacts schema in the rule creation/update/find (#216292)
    We want to add a new `root-level` key in the rule called `artifacts`.
    The new key will be added to the `create`, `update` and `find` Rule apis
    and will contain `dashboards` and `investigation_guide`. This PR handles
    only the addition of dashboards.
    
    ```
    artifacts: {
        dasboards: Array<{ id: string }]>;
    }
    ```
    
    <img width="1265" alt="Screenshot 2025-04-02 at 12 35 00"
    src="https://github.com/user-attachments/assets/51783fd5-f63e-4a2e-80b0-7a724675c11c"
    />
    
    This PR includes only backend changes to the Rule APIs. In order to test
    you can use Kibana > Dev Tools.
    
    ```
    POST kbn:/api/alerting/rule
    {
      "name": "TEST RULE",
      "params": {
        "criteria": [
          {
            "comparator": ">",
            "metrics": [
              {
                "name": "A",
                "aggType": "count"
              }
            ],
            "threshold": [
              3
            ],
            "timeSize": 1,
            "timeUnit": "m"
          }
        ],
        "alertOnNoData": false,
        "alertOnGroupDisappear": false,
        "searchConfiguration": {
          "query": {
            "query": "",
            "language": "kuery"
          },
          "index": "e7744dbe-a7a4-457b-83aa-539e9c88764c"
        }
      },
      "rule_type_id": "observability.rules.custom_threshold",
      "consumer": "logs",
      "schedule": {
        "interval": "1m"
      },
      "artifacts": {
        "dashboards": [
          { "id":"1" }
        ]
      }
    }
    ```
    The artifacts shouldn't be returned in the rule response. The new
    dashboard artifacts should be saved in the Rules SO. You can check it
    directly under `Kibana > Stack Management > Saved Objects`. You should
    verify a new `artifacts` entry is added in the rules SO:
    
    ```
    "artifacts": {
          "dashboards": [
            {
              "refId": "dashboard_0"
            }
          ]
        },
    ```
    
    Also the new dashboard should be added into the references array:
    
    ```
    "references": [
        ..
        {
          "id": "1",
          "name": "dashboard_0",
          "type": "dashboard"
        }
      ],
    ```
    
    ```
    PUT kbn:/api/alerting/rule/{RULE_ID}
    {
      "name": "TEST RULE",
      "schedule": {
        "interval": "1m"
      },
      "params": {
        "criteria": [
          {
            "comparator": ">",
            "metrics": [
              {
                "name": "A",
                "aggType": "count"
              }
            ],
            "threshold": [
              3
            ],
            "timeSize": 1,
            "timeUnit": "m"
          }
        ],
        "alertOnNoData": false,
        "alertOnGroupDisappear": false,
        "searchConfiguration": {
          "query": {
            "query": "",
            "language": "kuery"
          },
          "index": "e7744dbe-a7a4-457b-83aa-539e9c88764c"
        }
      },
      "artifacts": {
        "dashboards": [{"id": "2"}, {"id": "1"}]
      }
    }
    ```
    The artifacts shouldn't be returned in the rule response. The updated
    artifacts should be saved in the Rules SO. You can check it directly
    under `Kibana > Stack Management > Saved Objects`. You should verify the
    updated dashboards list is correct.
    
    ```
    GET kbn:/api/alerting/rules/_find?search="TEST RULE"
    
    ```
    
    The artifacts shouldn't be returned in the rule response. Only the
    internal Find API returns the artifacts.
    
    ```
    POST kbn:/internal/alerting/rules/_find
    {
      "search": "TEST RULE"
    }
    ```
    
    - The dashboard IDs should be saved as `references in the Rule SO
    - The Rule APIs will not return the artifacts
    - Only the `internal Find API` used by the UI will return the artifacts
    - Integrations and unit tests should be added
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Justin Kambic <jk@elastic.co>
    Co-authored-by: Christos Nasikas <xristosnasikas@gmail.com>
    (cherry picked from commit 09740a63d14c6448bd948e952e3e70236b3fc634)

--- x-pack/test/alerting_api_integration/spaces_only/tests/alerting/group1/find_internal.ts
+++ x-pack/test/alerting_api_integration/spaces_only/tests/alerting/group1/find_internal.ts
@@ -119,6 +119,9 @@ export default function createFindTests({ getService }: FtrProviderContext) {
           params: {},
           created_by: null,
           api_key_owner: null,
+          artifacts: {
+            dashboards: [],
+          },
           api_key_created_by_user: null,
           scheduled_task_id: match.scheduled_task_id,
           updated_by: null,
