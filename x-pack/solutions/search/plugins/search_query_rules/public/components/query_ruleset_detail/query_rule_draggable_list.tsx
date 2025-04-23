064b5d0a4d6343489ba1984ce94babe58d7d682f
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2025-04-23 19:26:36 +0200

    Add custom heading element to draggable list

--- x-pack/solutions/search/plugins/search_query_rules/public/components/query_ruleset_detail/query_rule_draggable_list.tsx
+++ x-pack/solutions/search/plugins/search_query_rules/public/components/query_ruleset_detail/query_rule_draggable_list.tsx
@@ -38,8 +38,15 @@ export const QueryRuleDraggableList: React.FC<QueryRuleDraggableListProps> = ({
   onReorder,
 }) => {
   const {
-    euiTheme: { colors },
+    euiTheme: { colors, size, base },
   } = useEuiTheme();
+  const DOC_COUNT_COLUMN_SIZING = {
+    columnMinWidth: base * 10,
+    actionMinWidth: base * 5,
+    docCountMinWidth: base * 4,
+    docTextMinWidth: size.l,
+    headerpaddingLeft: base * 3.5,
+  };
   const criteriaDisplay = (criteria: QueryRulesQueryRuleCriteria) => (
     <EuiText size="s">
       <EuiBadge>{criteria.metadata}</EuiBadge>&nbsp;
@@ -48,28 +55,43 @@ export const QueryRuleDraggableList: React.FC<QueryRuleDraggableListProps> = ({
     </EuiText>
   );
   const actionIcon = (queryRule: QueryRulesQueryRule) => (
-    <EuiFlexGroup responsive={false} alignItems="center" gutterSize="m">
+    <EuiFlexGroup
+      responsive={false}
+      alignItems="center"
+      gutterSize="m"
+      justifyContent="center"
+      css={{ minWidth: DOC_COUNT_COLUMN_SIZING.columnMinWidth }}
+    >
       <EuiFlexItem grow={false}>
-        <EuiBadge iconType={queryRule.type === 'exclude' ? 'eyeClosed' : 'pinFilled'}>
-          {queryRule.type === 'exclude' ? (
-            <FormattedMessage
-              id="xpack.search.queryRulesetDetail.draggableList.excludeLabel"
-              defaultMessage="Exclude"
-            />
-          ) : (
-            <FormattedMessage
-              id="xpack.search.queryRulesetDetail.draggableList.pinLabel"
-              defaultMessage="Pin"
-            />
-          )}
-        </EuiBadge>
+        <div css={{ minWidth: DOC_COUNT_COLUMN_SIZING.actionMinWidth, textAlign: 'end' }}>
+          <EuiBadge iconType={queryRule.type === 'exclude' ? 'eyeClosed' : 'pinFilled'}>
+            {queryRule.type === 'exclude' ? (
+              <FormattedMessage
+                id="xpack.search.queryRulesetDetail.draggableList.excludeLabel"
+                defaultMessage="Exclude"
+              />
+            ) : (
+              <FormattedMessage
+                id="xpack.search.queryRulesetDetail.draggableList.pinLabel"
+                defaultMessage="Pin"
+              />
+            )}
+          </EuiBadge>
+        </div>
       </EuiFlexItem>
-      <EuiFlexItem grow>
+      <EuiFlexItem
+        grow={false}
+        css={{
+          minWidth: DOC_COUNT_COLUMN_SIZING.docCountMinWidth,
+        }}
+      >
         <EuiFlexGroup responsive={false} alignItems="center" gutterSize="s">
           <EuiFlexItem grow={false}>
             <EuiIcon type="documents" />
           </EuiFlexItem>
-          <EuiFlexItem grow={false}>{queryRule.actions.docs?.length ?? 0}</EuiFlexItem>
+          <EuiFlexItem grow={false} css={{ minWidth: DOC_COUNT_COLUMN_SIZING.docTextMinWidth }}>
+            {queryRule.actions.docs?.length ?? 0}
+          </EuiFlexItem>
         </EuiFlexGroup>
       </EuiFlexItem>
     </EuiFlexGroup>
@@ -84,88 +106,145 @@ export const QueryRuleDraggableList: React.FC<QueryRuleDraggableListProps> = ({
         }
       }}
     >
-      <EuiDroppable droppableId="queryRuleListDropabble" spacing="m">
-        {rules.map((queryRule, index) => (
-          <EuiDraggable
-            spacing="m"
-            key={queryRule.rule_id}
-            index={index}
-            draggableId={queryRule.rule_id}
-            customDragHandle={true}
-            hasInteractiveChildren={true}
-            data-test-subj="searchQueryRulesDraggableItem"
-          >
-            {(provided) => (
-              <EuiPanel paddingSize="s">
-                <EuiFlexGroup alignItems="center" gutterSize="s">
-                  <EuiFlexItem grow={false}>
-                    <EuiPanel
-                      color="transparent"
-                      paddingSize="s"
-                      {...provided.dragHandleProps}
-                      aria-label="Drag Handle"
-                    >
-                      <EuiIcon type="grab" />
-                    </EuiPanel>
-                  </EuiFlexItem>
-                  <EuiFlexItem grow={true}>
-                    <EuiFlexGroup responsive={false} alignItems="center">
-                      <EuiFlexItem grow={7}>
-                        <EuiFlexGroup direction="column" gutterSize="xs" responsive={false}>
-                          {Array.isArray(queryRule.criteria) ? (
-                            <>
-                              {queryRule.criteria.slice(0, 3).map((criteria, criteriaIndex) => (
-                                <EuiFlexItem key={criteria.type + criteriaIndex} grow={false}>
-                                  {criteriaDisplay(criteria)}
-                                </EuiFlexItem>
-                              ))}
-                              {queryRule.criteria.length > 3 && (
-                                <EuiFlexItem grow={false}>
-                                  <EuiText size="s">
-                                    <FormattedMessage
-                                      id="xpack.search.queryRulesetDetail.draggableList.queryRulesCriteriaMoreLabel"
-                                      defaultMessage="+{count} more"
-                                      values={{ count: queryRule.criteria.length - 3 }}
-                                    />
-                                  </EuiText>
-                                </EuiFlexItem>
-                              )}
-                            </>
-                          ) : (
-                            <EuiFlexItem grow={false}>
-                              {criteriaDisplay(queryRule.criteria)}
-                            </EuiFlexItem>
-                          )}
-                        </EuiFlexGroup>
-                      </EuiFlexItem>
-                      <EuiFlexItem grow={1}>{actionIcon(queryRule)}</EuiFlexItem>
-                      <EuiFlexItem grow={false}>
-                        <EuiButtonIcon
-                          aria-label={i18n.translate(
-                            'xpack.search.queryRulesetDetail.draggableList.actionsButton.aria.label',
-                            {
-                              defaultMessage: 'Click to reach actions menu for rule {ruleId}',
-                              values: {
-                                ruleId: queryRule.rule_id,
-                              },
-                            }
-                          )}
-                          data-test-subj="searchQueryRulesQueryRulesetDetailButton"
-                          iconType="boxesHorizontal"
-                          color="text"
-                          onClick={() => {
-                            // Logic to open actions menu
-                          }}
-                        />
-                      </EuiFlexItem>
-                    </EuiFlexGroup>
-                  </EuiFlexItem>
-                </EuiFlexGroup>
-              </EuiPanel>
-            )}
-          </EuiDraggable>
-        ))}
-      </EuiDroppable>
+      <>
+        <EuiFlexGroup
+          css={{
+            padding: `0 ${base * 2.25}px 0 ${base * 3.5}px`,
+          }}
+        >
+          <EuiFlexItem grow={7}>
+            <EuiText size="xs">
+              <b>
+                <FormattedMessage
+                  id="xpack.search.queryRulesetDetail.draggableList.ruleConditionsLabel"
+                  defaultMessage="Rule Conditions"
+                />
+              </b>
+            </EuiText>
+          </EuiFlexItem>
+
+          <EuiFlexItem grow={false}>
+            <EuiFlexGroup
+              alignItems="center"
+              justifyContent="center"
+              gutterSize="m"
+              responsive={false}
+              css={{ minWidth: DOC_COUNT_COLUMN_SIZING.columnMinWidth }}
+            >
+              <EuiFlexItem grow={false} css={{ minWidth: size.xl, textAlign: 'end' }}>
+                <EuiText size="xs">
+                  <b>
+                    <FormattedMessage
+                      id="xpack.search.queryRulesetDetail.draggableList.actionLabel"
+                      defaultMessage="Action"
+                    />
+                  </b>
+                </EuiText>
+              </EuiFlexItem>
+              <EuiFlexItem
+                grow={false}
+                css={{ minWidth: DOC_COUNT_COLUMN_SIZING.docCountMinWidth }}
+              >
+                <EuiText size="xs">
+                  <b>
+                    <FormattedMessage
+                      id="xpack.search.queryRulesetDetail.draggableList.documentCountLabel"
+                      defaultMessage="Document Count"
+                    />
+                  </b>
+                </EuiText>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiDroppable
+          droppableId="queryRuleListDropabble"
+          spacing="m"
+          css={{ backgroundColor: colors.backgroundBaseSubdued }}
+          data-test-subj="searchQueryRulesDroppable"
+        >
+          {rules.map((queryRule, index) => (
+            <EuiDraggable
+              spacing="m"
+              key={queryRule.rule_id}
+              index={index}
+              draggableId={queryRule.rule_id}
+              customDragHandle={true}
+              hasInteractiveChildren={true}
+              data-test-subj="searchQueryRulesDraggableItem"
+            >
+              {(provided) => (
+                <EuiPanel paddingSize="s" hasShadow={false}>
+                  <EuiFlexGroup alignItems="center" gutterSize="s">
+                    <EuiFlexItem grow={false}>
+                      <EuiPanel
+                        color="transparent"
+                        paddingSize="s"
+                        {...provided.dragHandleProps}
+                        aria-label="Drag Handle"
+                      >
+                        <EuiIcon type="grab" />
+                      </EuiPanel>
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={true}>
+                      <EuiFlexGroup responsive={false} alignItems="center">
+                        <EuiFlexItem grow={7}>
+                          <EuiFlexGroup direction="column" gutterSize="xs" responsive={false}>
+                            {Array.isArray(queryRule.criteria) ? (
+                              <>
+                                {queryRule.criteria.slice(0, 3).map((criteria, criteriaIndex) => (
+                                  <EuiFlexItem key={criteria.type + criteriaIndex} grow={false}>
+                                    {criteriaDisplay(criteria)}
+                                  </EuiFlexItem>
+                                ))}
+                                {queryRule.criteria.length > 3 && (
+                                  <EuiFlexItem grow={false}>
+                                    <EuiText size="s">
+                                      <FormattedMessage
+                                        id="xpack.search.queryRulesetDetail.draggableList.queryRulesCriteriaMoreLabel"
+                                        defaultMessage="+{count} more"
+                                        values={{ count: queryRule.criteria.length - 3 }}
+                                      />
+                                    </EuiText>
+                                  </EuiFlexItem>
+                                )}
+                              </>
+                            ) : (
+                              <EuiFlexItem grow={false}>
+                                {criteriaDisplay(queryRule.criteria)}
+                              </EuiFlexItem>
+                            )}
+                          </EuiFlexGroup>
+                        </EuiFlexItem>
+                        <EuiFlexItem grow={1}>{actionIcon(queryRule)}</EuiFlexItem>
+                        <EuiFlexItem grow={false}>
+                          <EuiButtonIcon
+                            aria-label={i18n.translate(
+                              'xpack.search.queryRulesetDetail.draggableList.actionsButton.aria.label',
+                              {
+                                defaultMessage: 'Click to reach actions menu for rule {ruleId}',
+                                values: {
+                                  ruleId: queryRule.rule_id,
+                                },
+                              }
+                            )}
+                            data-test-subj="searchQueryRulesQueryRulesetDetailButton"
+                            iconType="boxesHorizontal"
+                            color="text"
+                            onClick={() => {
+                              // Logic to open actions menu
+                            }}
+                          />
+                        </EuiFlexItem>
+                      </EuiFlexGroup>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiPanel>
+              )}
+            </EuiDraggable>
+          ))}
+        </EuiDroppable>
+      </>
     </EuiDragDropContext>
   );
 };
