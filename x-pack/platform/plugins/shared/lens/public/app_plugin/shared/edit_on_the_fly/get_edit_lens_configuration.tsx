b70c33cdaa02d45b2fb8147906ec3dacc8e9ae83
Nick Partridge <nicholas.partridge@elastic.co>
2025-04-23 12:01:07 -0500

    fix: vis type issue on active vis

--- x-pack/platform/plugins/shared/lens/public/app_plugin/shared/edit_on_the_fly/get_edit_lens_configuration.tsx
+++ x-pack/platform/plugins/shared/lens/public/app_plugin/shared/edit_on_the_fly/get_edit_lens_configuration.tsx
@@ -24,7 +24,7 @@ import {
   loadInitial,
   initExisting,
   initEmpty,
-  LensStoreDeps,
+  type LensStoreDeps,
 } from '../../../state_management';
 import { generateId } from '../../../id_generator';
 import type { DatasourceMap, VisualizationMap } from '../../../types';
@@ -165,6 +165,7 @@ export async function getEditLensConfiguration(
     }
     const [currentAttributes, setCurrentAttributes] =
       useState<TypedLensSerializedState['attributes']>(attributes);
+
     /**
      * During inline editing of a by reference panel, the panel is converted to a by value one.
      * When the user applies the changes we save them to the Lens SO
