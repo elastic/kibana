6bdc0ebfcb83cc39b2eeb38733916775384307cb
José Luis González <joseluisgj@gmail.com>
2025-04-23 14:59:37 +0200

    [Search][Query Rules] Run in Console ruleset creation  (#218549)
    ## Summary
    
    Using Run in Console button to create a ruleset using the Embedded
    console in the Empty Prompt screen:
    
    
    https://github.com/user-attachments/assets/a40623d0-5da6-4ca0-b0f5-fd2cc6ee809a
    
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ### Identify risks
    
    Does this PR introduce any risks? For example, consider risks like hard
    to test bugs, performance regression, potential of data loss.
    
    Describe the risk, its severity, and mitigation for each identified
    risk. Invite stakeholders and evaluate how to proceed before merging.
    
    - [ ] [See some risk
    examples](https://github.com/elastic/kibana/blob/main/RISK_MATRIX.mdx)
    - [ ] ...
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Rodney Norris <rodney@tattdcodemonkey.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/solutions/search/plugins/search_query_rules/public/components/empty_prompt/empty_prompt.tsx
+++ x-pack/solutions/search/plugins/search_query_rules/public/components/empty_prompt/empty_prompt.tsx
@@ -20,10 +20,17 @@ import {
   useEuiTheme,
   transparentize,
   EuiHorizontalRule,
+  EuiHideFor,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { css } from '@emotion/react';
+import { TryInConsoleButton } from '@kbn/try-in-console';
+import { i18n } from '@kbn/i18n';
 import { docLinks } from '../../../common/doc_links';
+
+import { CREATE_QUERY_RULE_SET_API_SNIPPET } from '../../../common/constants';
+
+import { useKibana } from '../../hooks/use_kibana';
 
 import queryRulesImg from '../../assets/query-rules-context-alt.svg';
 import backgroundPanelImg from '../../assets/query-rule-panel-background.svg';
@@ -32,6 +39,7 @@ interface EmptyPromptProps {
   getStartedAction: () => void;
 }
 export const EmptyPrompt: React.FC<EmptyPromptProps> = ({ getStartedAction }) => {
+  const { application, share, console: consolePlugin } = useKibana().services;
   const { euiTheme } = useEuiTheme();
   const gradientOverlay = css({
     background: `linear-gradient(180deg, ${transparentize(
@@ -202,26 +210,49 @@ export const EmptyPrompt: React.FC<EmptyPromptProps> = ({ getStartedAction }) =>
         </EuiSplitPanel.Inner>
         <EuiHorizontalRule margin="none" />
         <EuiSplitPanel.Inner color="subdued" paddingSize="l">
-          <EuiTitle size="xxs">
-            <span>
-              <FormattedMessage
-                id="xpack.queryRules.emptyPrompt.footer"
-                defaultMessage="Prefer to use the APIs?"
-              />
-            </span>
-          </EuiTitle>
-          &nbsp;
-          <EuiLink
-            data-test-subj="searchQueryRulesEmptyPromptFooterLink"
-            href={docLinks.queryRulesApi}
-            target="_blank"
-            external
-          >
-            <FormattedMessage
-              id="xpack.queryRules.emptyPrompt.footerLink"
-              defaultMessage="Query Rules API documentation"
-            />
-          </EuiLink>
+          <EuiFlexGroup direction="row" alignItems="center">
+            <EuiFlexGroup direction="row" gutterSize="s" alignItems="center">
+              <EuiFlexItem grow={false}>
+                <EuiTitle size="xxs">
+                  <span>
+                    <FormattedMessage
+                      id="xpack.queryRules.emptyPrompt.footer"
+                      defaultMessage="Prefer to use the APIs?"
+                    />
+                  </span>
+                </EuiTitle>
+              </EuiFlexItem>
+              <EuiHideFor sizes={['xs', 's']}>
+                <EuiFlexItem grow={false}>
+                  <TryInConsoleButton
+                    application={application}
+                    sharePlugin={share ?? undefined}
+                    consolePlugin={consolePlugin ?? undefined}
+                    request={CREATE_QUERY_RULE_SET_API_SNIPPET}
+                    type="emptyButton"
+                    content={i18n.translate('xpack.queryRules.emptyPrompt.TryInConsoleLabel', {
+                      defaultMessage: 'Create in Console',
+                    })}
+                    showIcon
+                  />
+                </EuiFlexItem>
+              </EuiHideFor>
+            </EuiFlexGroup>
+
+            <EuiFlexItem grow={false}>
+              <EuiLink
+                data-test-subj="searchQueryRulesEmptyPromptFooterLink"
+                href={docLinks.queryRulesApi}
+                target="_blank"
+                external
+              >
+                <FormattedMessage
+                  id="xpack.queryRules.emptyPrompt.footerLink"
+                  defaultMessage="Query Rules API documentation"
+                />
+              </EuiLink>
+            </EuiFlexItem>
+          </EuiFlexGroup>
         </EuiSplitPanel.Inner>
       </EuiSplitPanel.Outer>
     </EuiFlexItem>
