e937f466623734ef746a01dd518797d1557a415f
Andreana Malama <andreana.malama@elastic.co>
2025-04-23 18:42:49 +0300

    Add test for checking the query$ and filters$ updates

---
+++ x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_search_context.test.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { LensRuntimeState } from '../types';
+import { getLensRuntimeStateMock, getLensInternalApiMock, makeEmbeddableServices } from '../mocks';
+import { initializeSearchContext } from './initialize_search_context';
+
+function setupSearchContextApi(runtimeOverrides?: Partial<LensRuntimeState>) {
+  const runtimeState = getLensRuntimeStateMock(runtimeOverrides);
+  const internalApiMock = getLensInternalApiMock();
+  const services = makeEmbeddableServices();
+  const { api } = initializeSearchContext(runtimeState, internalApiMock, {}, services);
+  return { api, internalApi: internalApiMock };
+}
+
+describe('Context API', () => {
+  it('should update the context query and filters when the corresponding attributes change', async () => {
+    const { api, internalApi } = setupSearchContextApi();
+    internalApi.updateAttributes({
+      ...internalApi.attributes$.getValue(),
+      state: {
+        ...internalApi.attributes$.getValue().state,
+        query: { esql: 'FROM kibana_sample_data_logs | LIMIT 1' },
+        filters: [{ meta: { alias: 'test', disabled: false, negate: false, index: 'test' } }],
+      },
+    });
+    expect(api.query$.getValue()).toEqual(internalApi.attributes$.getValue().state.query);
+    expect(api.filters$.getValue()).toEqual(internalApi.attributes$.getValue().state.filters);
+  });
+});
