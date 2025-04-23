b7f865f4a0e771591df659ba97d582c23de56246
Pedro Jaramillo <pedro.jaramillo@elastic.co>
2025-04-23 12:45:29 -0400

    testing ai4dsoc

--- x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts
+++ x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts
@@ -7,6 +7,8 @@
 
 export type ExperimentalFeatures = { [K in keyof typeof allowedExperimentalValues]: boolean };
 
+// testing
+
 /**
  * A list of allowed values that can be used in `xpack.securitySolution.enableExperimental`.
  * This object is then used to validate and parse the value entered.
