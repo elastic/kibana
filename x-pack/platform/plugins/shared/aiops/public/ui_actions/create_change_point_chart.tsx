f575b4585aec1a43bf3ba1d902686e9f30047256
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-04-24 20:56:31 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/aiops/public/ui_actions/create_change_point_chart.tsx
+++ x-pack/platform/plugins/shared/aiops/public/ui_actions/create_change_point_chart.tsx
@@ -16,7 +16,7 @@ import type { CoreStart } from '@kbn/core-lifecycle-browser';
 import type { AiopsPluginStartDeps } from '../types';
 
 import type { ChangePointChartActionContext } from './change_point_action_context';
-import { ChangePointEmbeddableState } from '../embeddables/change_point_chart/types';
+import type { ChangePointEmbeddableState } from '../embeddables/change_point_chart/types';
 
 const parentApiIsCompatible = async (
   parentApi: unknown
