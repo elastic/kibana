06a864873dd3d77b5fdc9a4fb8951c031d98db5e
Bryce Buchanan <bryce.buchanan@elastic.co>
2025-04-24 11:12:17 -0700

    improved environment badge test

--- x-pack/solutions/observability/plugins/apm/public/components/shared/environment_badge/environment_badge.test.ts
+++ x-pack/solutions/observability/plugins/apm/public/components/shared/environment_badge/environment_badge.test.ts
@@ -8,11 +8,15 @@
 import { EnvironmentBadge } from '.';
 import { render } from '@testing-library/react';
 import { expectTextsInDocument, expectTextsNotInDocument } from '../../../utils/test_helpers';
+import {
+  ENVIRONMENT_NOT_DEFINED_VALUE,
+  getEnvironmentLabel,
+} from '../../../../common/environment_filter_values';
 
 describe('EnvironmentBadge', () => {
   it('shows unset when evironments list is empty', () => {
     const component = render(EnvironmentBadge({ environments: [] }));
-    expectTextsInDocument(component, ['Not defined']);
+    expectTextsInDocument(component, [getEnvironmentLabel(ENVIRONMENT_NOT_DEFINED_VALUE)]);
   });
   it('shows environment set', () => {
     const component = render(EnvironmentBadge({ environments: ['DEMO'] }));
@@ -27,11 +31,11 @@ describe('EnvironmentBadge', () => {
   it('handles empty object', () => {
     // @ts-ignore-next-line
     const component = render(EnvironmentBadge({}));
-    expectTextsInDocument(component, ['Not defined']);
+    expectTextsInDocument(component, [getEnvironmentLabel(ENVIRONMENT_NOT_DEFINED_VALUE)]);
   });
   it('handled undefined environments', () => {
     // @ts-ignore-next-line
     const component = render(EnvironmentBadge({ environments: undefined }));
-    expectTextsInDocument(component, ['Not defined']);
+    expectTextsInDocument(component, [getEnvironmentLabel(ENVIRONMENT_NOT_DEFINED_VALUE)]);
   });
 });
