12d33955950f40ef967496cbb10a287ea5179f52
Kurt <kc13greiner@users.noreply.github.com>
2025-04-24 12:48:17 -0400

    sourcetreetest

--- src/platform/packages/shared/kbn-test/jest-preset.js
+++ src/platform/packages/shared/kbn-test/jest-preset.js
@@ -9,6 +9,7 @@
 
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
+// Kurt test
 
 /** @type {import("@jest/types").Config.InitialOptions} */
 module.exports = {
