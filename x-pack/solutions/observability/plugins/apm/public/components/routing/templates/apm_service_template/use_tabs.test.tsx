1791ba476b9d6e4ebf432367a6b840a137a7543f
Sergi Romeu <sergi.romeu@elastic.co>
2025-04-24 16:51:16 +0200

    fix test

--- x-pack/solutions/observability/plugins/apm/public/components/routing/templates/apm_service_template/use_tabs.test.tsx
+++ x-pack/solutions/observability/plugins/apm/public/components/routing/templates/apm_service_template/use_tabs.test.tsx
@@ -66,8 +66,6 @@ describe('APM service template', () => {
         { agentName: 'js-base' },
         { agentName: 'rum-js' },
         { agentName: 'opentelemetry/webjs' },
-        { serverlessType: ServerlessType.AWS_LAMBDA },
-        { serverlessType: ServerlessType.AZURE_FUNCTIONS },
       ].map((input) => {
         it(`when input ${JSON.stringify(input)}`, () => {
           expect(isMetricsTabHidden(input)).toBeTruthy();
@@ -86,6 +84,8 @@ describe('APM service template', () => {
         { agentName: 'ruby', runtimeName: 'jruby' },
         { agentName: 'java' },
         { agentName: 'opentelemetry/java' },
+        { serverlessType: ServerlessType.AWS_LAMBDA },
+        { serverlessType: ServerlessType.AZURE_FUNCTIONS },
       ].map((input) => {
         it(`when input ${JSON.stringify(input)}`, () => {
           expect(isMetricsTabHidden(input)).toBeFalsy();
