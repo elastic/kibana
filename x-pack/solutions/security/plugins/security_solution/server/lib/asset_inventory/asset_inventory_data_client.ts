ff03b1a806c355ee5dd6b02b40900f6eba5872c9
Paulo Henrique <paulo.henrique@elastic.co>
2025-04-24 09:47:00 -0700

    updating status API to fetch data from generic entity store

--- x-pack/solutions/security/plugins/security_solution/server/lib/asset_inventory/asset_inventory_data_client.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/asset_inventory/asset_inventory_data_client.ts
@@ -22,8 +22,8 @@ interface AssetInventoryClientOpts {
 
 type EntityStoreEngineStatus = GetEntityStoreStatusResponse['engines'][number];
 
-interface HostEntityEngineStatus extends Omit<EntityStoreEngineStatus, 'type'> {
-  type: 'host';
+interface GenericEntityEngineStatus extends Omit<EntityStoreEngineStatus, 'type'> {
+  type: 'generic';
 }
 
 interface TransformMetadata {
@@ -144,18 +144,18 @@ export class AssetInventoryDataClient {
       return { status: ASSET_INVENTORY_STATUS.INITIALIZING };
     }
 
-    // Check for host entity engine
-    const hostEntityEngine = entityStoreStatus.engines.find(this.isHostEntityEngine);
-    // If the host engine is not installed, the asset inventory is disabled.
-    if (!hostEntityEngine) {
+    // Check for the Generic entity engine
+    const genericEntityEngine = entityStoreStatus.engines.find(this.isGenericEntityEngine);
+    // If the generic engine is not installed, the asset inventory is disabled.
+    if (!genericEntityEngine) {
       return { status: ASSET_INVENTORY_STATUS.DISABLED };
     }
 
     // Determine final status based on transform metadata
-    if (this.hasDocumentsProcessed(hostEntityEngine)) {
+    if (this.hasDocumentsProcessed(genericEntityEngine)) {
       return { status: ASSET_INVENTORY_STATUS.READY };
     }
-    if (this.hasTransformTriggered(hostEntityEngine)) {
+    if (this.hasTransformTriggered(genericEntityEngine)) {
       return { status: ASSET_INVENTORY_STATUS.EMPTY };
     }
 
@@ -179,10 +179,11 @@ export class AssetInventoryDataClient {
     return isAssetInventoryEnabled;
   }
 
-  // Type guard to check if an entity engine is a host entity engine
-  // Todo: Change to the new 'generic' entity engine once it's ready
-  private isHostEntityEngine(engine: EntityStoreEngineStatus): engine is HostEntityEngineStatus {
-    return engine.type === 'host';
+  // Type guard to check if an entity engine is a generic entity engine
+  private isGenericEntityEngine(
+    engine: EntityStoreEngineStatus
+  ): engine is GenericEntityEngineStatus {
+    return engine.type === 'generic';
   }
 
   // Type guard function to validate entity store component metadata
@@ -197,7 +198,7 @@ export class AssetInventoryDataClient {
     );
   }
 
-  private hasDocumentsProcessed(engine: HostEntityEngineStatus): boolean {
+  private hasDocumentsProcessed(engine: GenericEntityEngineStatus): boolean {
     return !!engine.components?.some((component) => {
       if (component.resource === 'transform' && this.isTransformMetadata(component.metadata)) {
         return component.metadata.documents_processed > 0;
@@ -206,7 +207,7 @@ export class AssetInventoryDataClient {
     });
   }
 
-  private hasTransformTriggered(engine: HostEntityEngineStatus): boolean {
+  private hasTransformTriggered(engine: GenericEntityEngineStatus): boolean {
     return !!engine.components?.some((component) => {
       if (component.resource === 'transform' && this.isTransformMetadata(component.metadata)) {
         return component.metadata.trigger_count > 0;
