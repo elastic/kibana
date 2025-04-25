4d0a514246e3e0cce4c032ba68f814d0c99d85a2
Kevin Delemme <kevin.delemme@elastic.co>
2025-04-24 20:11:26 -0400

    feat(slo): bulk delete APIs (#217405)
    (cherry picked from commit 18826975c7321cfa1a11d392d852e43e179d4f2f)
    
    # Conflicts:
    #	x-pack/solutions/observability/plugins/slo/tsconfig.json

--- x-pack/test/plugin_api_integration/test_suites/task_manager/check_registered_task_types.ts
+++ x-pack/test/plugin_api_integration/test_suites/task_manager/check_registered_task_types.ts
@@ -178,6 +178,7 @@ export default function ({ getService }: FtrProviderContext) {
         'security:telemetry-prebuilt-rule-alerts',
         'security:telemetry-timelines',
         'session_cleanup',
+        'slo:bulk-delete-task',
         'slo:temp-summary-cleanup-task',
         'task_manager:delete_inactive_background_task_nodes',
         'task_manager:mark_removed_tasks_as_unrecognized',
