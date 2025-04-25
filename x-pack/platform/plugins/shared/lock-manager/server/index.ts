7e8cbd525c1240e6ced1ad376b617b3508fb8bc4
Søren Louv-Jansen <soren.louv@elastic.co>
2025-04-25 16:05:57 +0200

    Add plugin suffix

--- x-pack/platform/plugins/shared/lock-manager/server/index.ts
+++ x-pack/platform/plugins/shared/lock-manager/server/index.ts
@@ -4,6 +4,9 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import type { PluginInitializerContext } from '@kbn/core/server';
 
 export { LockAcquisitionError } from './lock_manager_client';
 export { LockManagerService } from './lock_manager_service';
+
+export async function plugin(initializerContext: PluginInitializerContext) {}
