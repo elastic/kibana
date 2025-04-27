64d0cde5a1dbb3890361fc3888fd6a1be6676bd3
Jordan <yarden.shalom@elastic.co>
2025-04-27 15:03:51 +0300

    switched to using insight package

--- x-pack/solutions/security/plugins/security_solution/public/flyout/entity_details/generic_right/content.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/entity_details/generic_right/content.tsx
@@ -7,25 +7,14 @@
 
 import React from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
-import {
-  MISCONFIGURATION_INSIGHT_HOST_ENTITY_OVERVIEW,
-  VULNERABILITIES_INSIGHT_HOST_ENTITY_OVERVIEW,
-} from '@kbn/cloud-security-posture-common/utils/ui_metrics';
 import { EuiHorizontalRule } from '@elastic/eui';
+import { EntityInsight } from '../../../cloud_security_posture/components/entity_insight';
 import { useExpandSection } from '../../document_details/right/hooks/use_expand_section';
-import { AlertCountInsight } from '../../document_details/shared/components/alert_count_insight';
 import { GENERIC_FLYOUT_STORAGE_KEYS } from './constants';
 import type { GenericEntityRecord } from '../../../asset_inventory/types/generic_entity_record';
 import { FieldsTable } from './components/fields_table';
 import { ExpandableSection } from '../../document_details/right/components/expandable_section';
 import { FlyoutBody } from '../../shared/components/flyout_body';
-import {
-  ENTITIES_HOST_OVERVIEW_ALERT_COUNT_TEST_ID,
-  ENTITIES_HOST_OVERVIEW_MISCONFIGURATIONS_TEST_ID,
-  ENTITIES_HOST_OVERVIEW_VULNERABILITIES_TEST_ID,
-} from '../../document_details/right/components/test_ids';
-import { MisconfigurationsInsight } from '../../document_details/shared/components/misconfiguration_insight';
-import { VulnerabilitiesInsight } from '../../document_details/shared/components/vulnerabilities_insight';
 
 interface GenericEntityFlyoutContentProps {
   source: GenericEntityRecord;
@@ -62,38 +51,24 @@ export const GenericEntityFlyoutContent = ({ source }: GenericEntityFlyoutConten
 
       <EuiHorizontalRule />
 
-      <ExpandableSection
-        title={
-          <FormattedMessage
-            id="xpack.securitySolution.genericEntityFlyout.flyoutContent.expandableSection.insightsLabel"
-            defaultMessage="Insights"
-          />
-        }
-        expanded={insightsSectionExpandedState}
-        localStorageKey={GENERIC_FLYOUT_STORAGE_KEYS.OVERVIEW_INSIGHTS_SECTION}
-      >
-        <AlertCountInsight
-          fieldName={'entity.id'}
-          name={source?.entity?.id}
-          // fieldName={'agent.type'}
-          // name={'cloudbeat'}
-          // openDetailsPanel={() => {}}
-          data-test-subj={ENTITIES_HOST_OVERVIEW_ALERT_COUNT_TEST_ID}
-        />
-        <MisconfigurationsInsight
-          fieldName={'entity.id'}
-          name={source?.entity?.id}
-          // openDetailsPanel={openDetailsPanel}
-          data-test-subj={ENTITIES_HOST_OVERVIEW_MISCONFIGURATIONS_TEST_ID}
-          telemetryKey={MISCONFIGURATION_INSIGHT_HOST_ENTITY_OVERVIEW}
-        />
-        <VulnerabilitiesInsight
-          hostName={source?.entity?.id}
-          // openDetailsPanel={openDetailsPanel}
-          data-test-subj={ENTITIES_HOST_OVERVIEW_VULNERABILITIES_TEST_ID}
-          telemetryKey={VULNERABILITIES_INSIGHT_HOST_ENTITY_OVERVIEW}
-        />
-      </ExpandableSection>
+      {/* <ExpandableSection*/}
+      {/*  title={*/}
+      {/*    <FormattedMessage*/}
+      {/*      id="xpack.securitySolution.genericEntityFlyout.flyoutContent.expandableSection.insightsLabel"*/}
+      {/*      defaultMessage="Insights"*/}
+      {/*    />*/}
+      {/*  }*/}
+      {/*  expanded={insightsSectionExpandedState}*/}
+      {/*  localStorageKey={GENERIC_FLYOUT_STORAGE_KEYS.OVERVIEW_INSIGHTS_SECTION}*/}
+      {/* >*/}
+      <EntityInsight
+        field={'agent.type'}
+        value={'cloudbeat'}
+        isPreviewMode={false}
+        isLinkEnabled={false}
+        openDetailsPanel={() => {}}
+      />
+      {/* </ExpandableSection>*/}
     </FlyoutBody>
   );
 };
