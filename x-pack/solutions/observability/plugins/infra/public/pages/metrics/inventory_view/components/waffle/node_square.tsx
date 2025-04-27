985e238c12f41827459f2403cc3c316734d0554c
Miriam <31922082+MiriamAparicio@users.noreply.github.com>
2025-04-27 09:55:14 +0100

    [ObsUX] [A11y] Improve color alternative for element announcement (#219250)
    Closes https://github.com/elastic/kibana/issues/212739
    
    ### Summary
    
    Focus on the value for this cell, the value of the metric determines the
    color. And since we don’t want to name all possible options, instead of
    announcing the color, set only aria-label attribute to determine cell
    state.
    Also update role for elements on the accessibility tree.
    
    <img width="861" alt="Screenshot 2025-04-25 at 14 41 06"
    src="https://github.com/user-attachments/assets/35ca8b9c-8bd8-424d-bd2e-a11612a0e470"
    />
    
    <img width="861" alt="Screenshot 2025-04-25 at 14 40 07"
    src="https://github.com/user-attachments/assets/3d676f0c-36af-441f-bba3-5fd63014ae4b"
    />
    
    <img width="1130" alt="Screenshot 2025-04-25 at 12 27 24"
    src="https://github.com/user-attachments/assets/83b2a816-c5e2-4a47-998c-7c3e97660a32"
    />
    
    (cherry picked from commit 3fd7ddb5aae6a426f6928e79c03ff02bff75a578)

--- x-pack/solutions/observability/plugins/infra/public/pages/metrics/inventory_view/components/waffle/node_square.tsx
+++ x-pack/solutions/observability/plugins/infra/public/pages/metrics/inventory_view/components/waffle/node_square.tsx
@@ -160,6 +160,7 @@ export const NodeSquare = ({
   nodeName,
   value,
   showBorder,
+  nodeMetric,
 }: {
   squareSize: number;
   togglePopover: UseBooleanHandlers['toggle'];
@@ -167,12 +168,13 @@ export const NodeSquare = ({
   nodeName: string;
   value: string;
   showBorder?: boolean;
+  nodeMetric?: string;
 }) => {
   const valueMode = squareSize > 70;
   const ellipsisMode = squareSize > 30;
   const nodeAriaLabel = i18n.translate('xpack.infra.node.ariaLabel', {
-    defaultMessage: '{nodeName}, click to open menu',
-    values: { nodeName },
+    defaultMessage: '{nodeName} {value} {nodeMetric} ',
+    values: { nodeName, nodeMetric, value },
   });
   const style: CSSProperties | undefined = showBorder ? { border: 'solid 4px #000' } : undefined;
 
