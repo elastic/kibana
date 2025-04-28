bbbdf58b562276bc9e016d94a0757a25909ee7a1
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-04-28 15:20:51 -0500

    Fix lint

--- x-pack/platform/plugins/shared/cases/server/client/utils.ts
+++ x-pack/platform/plugins/shared/cases/server/client/utils.ts
@@ -20,17 +20,17 @@ import { escapeQuotes } from '@kbn/es-query/src/kuery/utils/escape_kuery';
 import type { FileJSON } from '@kbn/shared-ux-file-types';
 import { FILE_SO_TYPE } from '@kbn/files-plugin/common/constants';
 
-import {
+import type {
   CaseCustomField,
   CaseSeverity,
   CaseStatuses,
   CustomFieldsConfiguration,
   ExternalReferenceAttachmentPayload,
   TemplatesConfiguration,
-  CustomFieldTypes,
   ListCustomFieldConfiguration,
 } from '../../common/types/domain';
 import {
+  CustomFieldTypes,
   ActionsAttachmentPayloadRt,
   AlertAttachmentPayloadRt,
   AttachmentType,
