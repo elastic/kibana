b87e251b60c92ecb99e8849755f7482247821b2a
Panagiota Mitsopoulou <giota85@gmail.com>
2025-04-29 22:59:51 +0300

    add more unit tests

--- x-pack/platform/plugins/shared/alerting/server/routes/rule/apis/get/transforms/transform_get_response/v1.test.ts
+++ x-pack/platform/plugins/shared/alerting/server/routes/rule/apis/get/transforms/transform_get_response/v1.test.ts
@@ -12,148 +12,283 @@ import type {
 } from '../../../../../../../common/routes/rule/response';
 
 describe('transformGetResponse', () => {
-  it('should not return artifacts by default', () => {
-    const mockedRule = {
-      id: '1',
-      alertTypeId: '1',
-      schedule: { interval: '10s' },
-      params: {
-        bar: true,
-      },
-      createdAt: new Date('2020-08-20T19:23:38Z'),
-      updatedAt: new Date('2020-08-20T19:23:38Z'),
-      actions: [],
-      consumer: 'bar',
-      name: 'abc',
-      tags: ['foo'],
-      enabled: true,
-      muteAll: false,
-      notifyWhen: 'onActionGroupChange' as const,
-      createdBy: '',
-      updatedBy: '',
-      apiKeyOwner: '',
-      throttle: '30s',
-      mutedInstanceIds: [],
-      executionStatus: {
-        status: 'unknown' as const,
-        lastExecutionDate: new Date('2020-08-20T19:23:38Z'),
-      },
-      revision: 0,
-      artifacts: {
-        dashboards: [
-          {
-            id: 'dashboard-1'
-          }
-        ],
-      },
-    };
-
-    const expectedResult: RuleResponseV1<RuleParamsV1> = {
-      id: '1',
-      rule_type_id: '1',
-      params: {
-        bar: true,
-      },
-      notify_when: 'onActionGroupChange' as const,
-      schedule: { interval: '10s' },
-      created_at: new Date('2020-08-20T19:23:38Z').toISOString(),
-      updated_at: new Date('2020-08-20T19:23:38Z').toISOString(),
-      actions: [],
-      consumer: 'bar',
-      name: 'abc',
-      tags: ['foo'],
-      enabled: true,
-      mute_all: false,
-      created_by: '',
-      updated_by: '',
-      api_key_owner: '',
-      throttle: '30s',
-      muted_alert_ids: [],
-      execution_status: {
-        status: 'unknown' as const,
-        last_execution_date: new Date('2020-08-20T19:23:38Z').toISOString(),
-      },
-      revision: 0,
-    };
-
-    const result = transformGetResponse(mockedRule);
-    expect(result).toEqual(expectedResult);
+  describe('includeArtifacts is not specified', () => {
+    it('should not return artifacts by default', () => {
+      const mockedRule = {
+        id: '1',
+        alertTypeId: '1',
+        schedule: { interval: '10s' },
+        params: {
+          bar: true,
+        },
+        createdAt: new Date('2020-08-20T19:23:38Z'),
+        updatedAt: new Date('2020-08-20T19:23:38Z'),
+        actions: [],
+        consumer: 'bar',
+        name: 'abc',
+        tags: ['foo'],
+        enabled: true,
+        muteAll: false,
+        notifyWhen: 'onActionGroupChange' as const,
+        createdBy: '',
+        updatedBy: '',
+        apiKeyOwner: '',
+        throttle: '30s',
+        mutedInstanceIds: [],
+        executionStatus: {
+          status: 'unknown' as const,
+          lastExecutionDate: new Date('2020-08-20T19:23:38Z'),
+        },
+        revision: 0,
+        artifacts: {
+          dashboards: [
+            {
+              id: 'dashboard-1'
+            }
+          ],
+        },
+      };
+  
+      const expectedResult: RuleResponseV1<RuleParamsV1> = {
+        id: '1',
+        rule_type_id: '1',
+        params: {
+          bar: true,
+        },
+        notify_when: 'onActionGroupChange' as const,
+        schedule: { interval: '10s' },
+        created_at: new Date('2020-08-20T19:23:38Z').toISOString(),
+        updated_at: new Date('2020-08-20T19:23:38Z').toISOString(),
+        actions: [],
+        consumer: 'bar',
+        name: 'abc',
+        tags: ['foo'],
+        enabled: true,
+        mute_all: false,
+        created_by: '',
+        updated_by: '',
+        api_key_owner: '',
+        throttle: '30s',
+        muted_alert_ids: [],
+        execution_status: {
+          status: 'unknown' as const,
+          last_execution_date: new Date('2020-08-20T19:23:38Z').toISOString(),
+        },
+        revision: 0,
+      };
+  
+      const result = transformGetResponse(mockedRule);
+      expect(result).toEqual(expectedResult);
+    });
   });
 
-  it('should return artifacts if explicitly specified', () => {
+  describe('includeArtifacts is true', () => {
+    it('should return artifacts if artifacts are specified', () => {
+      const mockedRule = {
+        id: '1',
+        alertTypeId: '1',
+        schedule: { interval: '10s' },
+        params: {
+          bar: true,
+        },
+        createdAt: new Date('2020-08-20T19:23:38Z'),
+        updatedAt: new Date('2020-08-20T19:23:38Z'),
+        actions: [],
+        consumer: 'bar',
+        name: 'abc',
+        tags: ['foo'],
+        enabled: true,
+        muteAll: false,
+        notifyWhen: 'onActionGroupChange' as const,
+        createdBy: '',
+        updatedBy: '',
+        apiKeyOwner: '',
+        throttle: '30s',
+        mutedInstanceIds: [],
+        executionStatus: {
+          status: 'unknown' as const,
+          lastExecutionDate: new Date('2020-08-20T19:23:38Z'),
+        },
+        revision: 0,
+        artifacts: {
+          dashboards: [
+            {
+              id: 'dashboard-1'
+            }
+          ],
+        },
+      };
+  
+      const expectedResult: RuleResponseV1<RuleParamsV1> = {
+        id: '1',
+        rule_type_id: '1',
+        params: {
+          bar: true,
+        },
+        notify_when: 'onActionGroupChange' as const,
+        schedule: { interval: '10s' },
+        created_at: new Date('2020-08-20T19:23:38Z').toISOString(),
+        updated_at: new Date('2020-08-20T19:23:38Z').toISOString(),
+        actions: [],
+        consumer: 'bar',
+        name: 'abc',
+        tags: ['foo'],
+        enabled: true,
+        mute_all: false,
+        created_by: '',
+        updated_by: '',
+        api_key_owner: '',
+        throttle: '30s',
+        muted_alert_ids: [],
+        execution_status: {
+          status: 'unknown' as const,
+          last_execution_date: new Date('2020-08-20T19:23:38Z').toISOString(),
+        },
+        revision: 0,
+        artifacts: {
+          dashboards: [
+            {
+              id: 'dashboard-1'
+            }
+          ],
+        },
+      };
+  
+      const result = transformGetResponse(mockedRule, true);
+      expect(result).toEqual(expectedResult);
+    });
 
-    const mockedRule = {
-      id: '1',
-      alertTypeId: '1',
-      schedule: { interval: '10s' },
-      params: {
-        bar: true,
-      },
-      createdAt: new Date('2020-08-20T19:23:38Z'),
-      updatedAt: new Date('2020-08-20T19:23:38Z'),
-      actions: [],
-      consumer: 'bar',
-      name: 'abc',
-      tags: ['foo'],
-      enabled: true,
-      muteAll: false,
-      notifyWhen: 'onActionGroupChange' as const,
-      createdBy: '',
-      updatedBy: '',
-      apiKeyOwner: '',
-      throttle: '30s',
-      mutedInstanceIds: [],
-      executionStatus: {
-        status: 'unknown' as const,
-        lastExecutionDate: new Date('2020-08-20T19:23:38Z'),
-      },
-      revision: 0,
-      artifacts: {
-        dashboards: [
-          {
-            id: 'dashboard-1'
-          }
-        ],
-      },
-    };
+    it('should not return any artifacts if no artifacts are specified', () => {
+      const mockedRule = {
+        id: '1',
+        alertTypeId: '1',
+        schedule: { interval: '10s' },
+        params: {
+          bar: true,
+        },
+        createdAt: new Date('2020-08-20T19:23:38Z'),
+        updatedAt: new Date('2020-08-20T19:23:38Z'),
+        actions: [],
+        consumer: 'bar',
+        name: 'abc',
+        tags: ['foo'],
+        enabled: true,
+        muteAll: false,
+        notifyWhen: 'onActionGroupChange' as const,
+        createdBy: '',
+        updatedBy: '',
+        apiKeyOwner: '',
+        throttle: '30s',
+        mutedInstanceIds: [],
+        executionStatus: {
+          status: 'unknown' as const,
+          lastExecutionDate: new Date('2020-08-20T19:23:38Z'),
+        },
+        revision: 0,
+      };
+  
+      const expectedResult: RuleResponseV1<RuleParamsV1> = {
+        id: '1',
+        rule_type_id: '1',
+        params: {
+          bar: true,
+        },
+        notify_when: 'onActionGroupChange' as const,
+        schedule: { interval: '10s' },
+        created_at: new Date('2020-08-20T19:23:38Z').toISOString(),
+        updated_at: new Date('2020-08-20T19:23:38Z').toISOString(),
+        actions: [],
+        consumer: 'bar',
+        name: 'abc',
+        tags: ['foo'],
+        enabled: true,
+        mute_all: false,
+        created_by: '',
+        updated_by: '',
+        api_key_owner: '',
+        throttle: '30s',
+        muted_alert_ids: [],
+        execution_status: {
+          status: 'unknown' as const,
+          last_execution_date: new Date('2020-08-20T19:23:38Z').toISOString(),
+        },
+        revision: 0,
+      };
+  
+      const result = transformGetResponse(mockedRule, true);
+      expect(result).toEqual(expectedResult);
+    })
 
-    const expectedResult: RuleResponseV1<RuleParamsV1> = {
-      id: '1',
-      rule_type_id: '1',
-      params: {
-        bar: true,
-      },
-      notify_when: 'onActionGroupChange' as const,
-      schedule: { interval: '10s' },
-      created_at: new Date('2020-08-20T19:23:38Z').toISOString(),
-      updated_at: new Date('2020-08-20T19:23:38Z').toISOString(),
-      actions: [],
-      consumer: 'bar',
-      name: 'abc',
-      tags: ['foo'],
-      enabled: true,
-      mute_all: false,
-      created_by: '',
-      updated_by: '',
-      api_key_owner: '',
-      throttle: '30s',
-      muted_alert_ids: [],
-      execution_status: {
-        status: 'unknown' as const,
-        last_execution_date: new Date('2020-08-20T19:23:38Z').toISOString(),
-      },
-      revision: 0,
-      artifacts: {
-        dashboards: [
-          {
-            id: 'dashboard-1'
-          }
-        ],
-      },
-    };
+  });
 
-    const result = transformGetResponse(mockedRule, true);
-    expect(result).toEqual(expectedResult);
+  describe('includeArtifacts is false', () => {
+    it('should not return any artifacts', () => {
+      const mockedRule = {
+        id: '1',
+        alertTypeId: '1',
+        schedule: { interval: '10s' },
+        params: {
+          bar: true,
+        },
+        createdAt: new Date('2020-08-20T19:23:38Z'),
+        updatedAt: new Date('2020-08-20T19:23:38Z'),
+        actions: [],
+        consumer: 'bar',
+        name: 'abc',
+        tags: ['foo'],
+        enabled: true,
+        muteAll: false,
+        notifyWhen: 'onActionGroupChange' as const,
+        createdBy: '',
+        updatedBy: '',
+        apiKeyOwner: '',
+        throttle: '30s',
+        mutedInstanceIds: [],
+        executionStatus: {
+          status: 'unknown' as const,
+          lastExecutionDate: new Date('2020-08-20T19:23:38Z'),
+        },
+        revision: 0,
+        artifacts: {
+          dashboards: [
+            {
+              id: 'dashboard-1'
+            }
+          ],
+        },
+      };
+  
+      const expectedResult: RuleResponseV1<RuleParamsV1> = {
+        id: '1',
+        rule_type_id: '1',
+        params: {
+          bar: true,
+        },
+        notify_when: 'onActionGroupChange' as const,
+        schedule: { interval: '10s' },
+        created_at: new Date('2020-08-20T19:23:38Z').toISOString(),
+        updated_at: new Date('2020-08-20T19:23:38Z').toISOString(),
+        actions: [],
+        consumer: 'bar',
+        name: 'abc',
+        tags: ['foo'],
+        enabled: true,
+        mute_all: false,
+        created_by: '',
+        updated_by: '',
+        api_key_owner: '',
+        throttle: '30s',
+        muted_alert_ids: [],
+        execution_status: {
+          status: 'unknown' as const,
+          last_execution_date: new Date('2020-08-20T19:23:38Z').toISOString(),
+        },
+        revision: 0,
+      };
+  
+      const result = transformGetResponse(mockedRule, false);
+      expect(result).toEqual(expectedResult);
+    });
   });
 });
 
