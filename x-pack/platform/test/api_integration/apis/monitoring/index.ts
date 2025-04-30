5048528ca8886efde5f0d3af9455702a549c84a8
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-04-30 11:40:50 +0200

    relocate api_integration/apis/monitoring

--- x-pack/platform/test/api_integration/apis/monitoring/index.ts
+++ x-pack/platform/test/api_integration/apis/monitoring/index.ts
@@ -5,7 +5,9 @@
  * 2.0.
  */
 
-export default function ({ loadTestFile }) {
+import { FtrProviderContext } from '../../ftr_provider_context';
+
+export default function ({ loadTestFile }: FtrProviderContext) {
   describe('Monitoring', () => {
     loadTestFile(require.resolve('./apm'));
     loadTestFile(require.resolve('./beats'));
