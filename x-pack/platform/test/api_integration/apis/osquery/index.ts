36fe8fb9d6d9d5764b801b2f9ad3586c3f369b94
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-04-30 11:36:21 +0200

    relocate api_integration/apis/osquery + indexManagement service

--- x-pack/platform/test/api_integration/apis/osquery/index.ts
+++ x-pack/platform/test/api_integration/apis/osquery/index.ts
@@ -5,7 +5,9 @@
  * 2.0.
  */
 
-export default function ({ loadTestFile }) {
+import { FtrProviderContext } from '../../ftr_provider_context';
+
+export default function ({ loadTestFile }: FtrProviderContext) {
   describe('Osquery Endpoints', () => {
     loadTestFile(require.resolve('./packs'));
   });
