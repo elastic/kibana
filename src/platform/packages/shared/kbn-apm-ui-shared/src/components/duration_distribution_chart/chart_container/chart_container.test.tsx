4a216885150cde7764a607eaf22a9d58c98f1a5b
iblancof <irene.blanco@elastic.co>
2025-04-30 16:52:05 +0200

    Fix ChartContainer tests

--- src/platform/packages/shared/kbn-apm-ui-shared/src/components/duration_distribution_chart/chart_container/chart_container.test.tsx
+++ src/platform/packages/shared/kbn-apm-ui-shared/src/components/duration_distribution_chart/chart_container/chart_container.test.tsx
@@ -10,37 +10,32 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import { ChartContainer } from '.';
-import { FETCH_STATUS } from '../../../enums';
 
 describe('ChartContainer', () => {
   describe('loading indicator', () => {
-    it('shows loading when status equals to Loading or Not initiated and has no data', () => {
-      [FETCH_STATUS.NOT_INITIATED, FETCH_STATUS.LOADING].forEach((status) => {
-        const { queryAllByTestId } = render(
-          <ChartContainer height={100} status={status} hasData={false}>
-            <div>My amazing component</div>
-          </ChartContainer>
-        );
+    it('shows loading when loading, not error and has no data', () => {
+      const { queryAllByTestId } = render(
+        <ChartContainer height={100} loading={true} hasError={false} hasData={false}>
+          <div>My amazing component</div>
+        </ChartContainer>
+      );
 
-        expect(queryAllByTestId('loading')[0]).toBeInTheDocument();
-      });
+      expect(queryAllByTestId('loading')[0]).toBeInTheDocument();
     });
-    it('does not show loading when status equals to Loading or Pending and has data', () => {
-      [FETCH_STATUS.NOT_INITIATED, FETCH_STATUS.LOADING].forEach((status) => {
-        const { queryAllByText } = render(
-          <ChartContainer height={100} status={status} hasData={true}>
-            <div>My amazing component</div>
-          </ChartContainer>
-        );
-        expect(queryAllByText('My amazing component')[0]).toBeInTheDocument();
-      });
+    it('does not show loading when loading not error and has data', () => {
+      const { queryAllByText } = render(
+        <ChartContainer height={100} loading={true} hasError={false} hasData={true}>
+          <div>My amazing component</div>
+        </ChartContainer>
+      );
+      expect(queryAllByText('My amazing component')[0]).toBeInTheDocument();
     });
   });
 
   describe('failure indicator', () => {
-    it('shows failure message when status equals to Failure and has data', () => {
+    it('shows failure message when has error, has data and no loading', () => {
       const { getByText } = render(
-        <ChartContainer height={100} status={FETCH_STATUS.FAILURE} hasData={true}>
+        <ChartContainer height={100} loading={false} hasError={true} hasData={true}>
           <div>My amazing component</div>
         </ChartContainer>
       );
@@ -48,9 +43,9 @@ describe('ChartContainer', () => {
         getByText('An error happened when trying to fetch data. Please try again')
       ).toBeInTheDocument();
     });
-    it('shows failure message when status equals to Failure and has no data', () => {
+    it('shows failure message when has error, has no data and no loading', () => {
       const { getByText } = render(
-        <ChartContainer height={100} status={FETCH_STATUS.FAILURE} hasData={false}>
+        <ChartContainer height={100} loading={false} hasError={true} hasData={false}>
           <div>My amazing component</div>
         </ChartContainer>
       );
@@ -61,17 +56,17 @@ describe('ChartContainer', () => {
   });
 
   describe('render component', () => {
-    it('shows children component when status Success and has data', () => {
+    it('shows children component when no loading, no error and has data', () => {
       const { getByText } = render(
-        <ChartContainer height={100} status={FETCH_STATUS.SUCCESS} hasData={true}>
+        <ChartContainer height={100} loading={false} hasError={false} hasData={true}>
           <div>My amazing component</div>
         </ChartContainer>
       );
       expect(getByText('My amazing component')).toBeInTheDocument();
     });
-    it('shows children component when status Success and has no data', () => {
+    it('shows children component when no loading, no error and has no data', () => {
       const { getByText } = render(
-        <ChartContainer height={100} status={FETCH_STATUS.SUCCESS} hasData={false}>
+        <ChartContainer height={100} loading={false} hasError={false} hasData={false}>
           <div>My amazing component</div>
         </ChartContainer>
       );
