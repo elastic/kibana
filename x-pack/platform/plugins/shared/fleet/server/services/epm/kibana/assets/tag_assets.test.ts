43d11cf0e957356b568b5450f2b7177a8dab0f3b
Steph Milovic <stephanie.milovic@elastic.co>
2025-05-01 11:27:31 -0600

    rm tag assets test

--- x-pack/platform/plugins/shared/fleet/server/services/epm/kibana/assets/tag_assets.test.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/epm/kibana/assets/tag_assets.test.ts
@@ -134,7 +134,6 @@ describe('tagKibanaAssets', () => {
       { id: 'v1', type: 'visualization' },
       { id: 'osquery-pack-asset1', type: 'osquery-pack-asset' },
       { id: 'osquery_saved_query1', type: 'osquery-saved-query' },
-      { id: 'security-ai-prompt1', type: 'security-ai-prompt' },
     ] as any;
 
     await tagKibanaAssets({
