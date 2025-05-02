66dc63363c4c3d0445d9d264be403d2da49cd621
Ersin Erdal <92688503+ersin-erdal@users.noreply.github.com>
2025-05-02 15:54:00 +0200

    Add rrule schema to task schedule with intermediate release (#219429)
    Towards: #216308
    
    This PR adds the `rrule` schema to the task SO with an intermediate
    release for the following PR:
    #217728
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 65cc4167b57008f5b3577be4476f0932c5c8d278)
    
    # Conflicts:
    #	src/core/server/integration_tests/ci_checks/saved_objects/check_registered_types.test.ts

--- src/core/server/integration_tests/ci_checks/saved_objects/check_registered_types.test.ts
+++ src/core/server/integration_tests/ci_checks/saved_objects/check_registered_types.test.ts
@@ -171,7 +171,7 @@ describe('checking migration metadata changes on all registered SO types', () =>
         "synthetics-private-location": "8cecc9e4f39637d2f8244eb7985c0690ceab24be",
         "synthetics-privates-locations": "f53d799d5c9bc8454aaa32c6abc99a899b025d5c",
         "tag": "e2544392fe6563e215bb677abc8b01c2601ef2dc",
-        "task": "42a95c6563df655a3fd7dea412b5f9bd27cded4f",
+        "task": "4bd8e19960b83c88f3cdf766ace268c081a1c619",
         "telemetry": "7b00bcf1c7b4f6db1192bb7405a6a63e78b699fd",
         "threshold-explorer-view": "175306806f9fc8e13fcc1c8953ec4ba89bda1b70",
         "ui-metric": "d227284528fd19904e9d972aea0a13716fc5fe24",
