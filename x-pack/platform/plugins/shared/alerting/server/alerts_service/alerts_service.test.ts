elastic/kibana

commit f6c30d6b9ad1a46a73cc5c084a5e70051d78a7cb

Auto increase fields limit of the alert indices (#216719)
This PR adds the auto-increase the fields limit on startup when an
alerts index reaches its limits because of the dynamic fields.

# To verify:
To be able to test this PR we need a rule type that adds dynamic fields.
I used the custom threshold rule for this:

Go to the custom threshold rule type definition and change its
alerts.mappings to:
```
  mappings: {
    // dynamic: true,
    fieldMap: {
      'kibana.alerting.grouping': {
        type: 'object',
        dynamic: true,
        array: false,
        required: false,
      },
      ...legacyExperimentalFieldMap,
      ...Array(412)
        .fill(0)
        .reduce((acc, val, i) => {
          acc[`${i + 1}`] = { type: 'keyword', array: false, required: false };
          return acc;
        }, {}),
    },
    dynamicTemplates: [
      {
        strings_as_keywords: {
          path_match: 'kibana.alert.grouping.*',
          match_mapping_type: 'string',
          mapping: {
            type: 'keyword',
            ignore_above: 1024,
          },
        },
      },
    ],
  },
  ```
    
  Above changes adds 412 dummy fields to the alerts index to make it close to reach its fields limit (default: 2500).
  And makes everything under `kibana.alert.grouping` path to be added to the index as dynamic fields.
  
  Then apply the below changes to the custom threshold rule executor:
  ```
  const grouping: Record<string, string> = {};
        groups?.forEach((groupObj) => (grouping[groupObj.field] = groupObj.value));
  
        const { uuid, start } = alertsClient.report({
          id: `${group}`,
          actionGroup: actionGroupId,
          payload: {
            [ALERT_REASON]: reason,
            [ALERT_EVALUATION_VALUES]: evaluationValues,
            [ALERT_EVALUATION_THRESHOLD]: threshold,
            [ALERT_GROUP]: groups,
            // @ts-ignore
            ['kibana.alerting.grouping']: grouping,

x-pack/platform/plugins/shared/alerting/server/alerts_service/alerts_service.test.ts
@@ -174,6 +174,7 @@ const getIndexTemplatePutBody = (opts?: GetIndexTemplatePutBodyOpts) => {
             }),
         'index.mapping.ignore_malformed': true,
         'index.mapping.total_fields.limit': 2500,
+        'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
       },
       mappings: {
         dynamic: false,
@@ -479,6 +480,7 @@ describe('Alerts Service', () => {
               settings: {
                 ...existingIndexTemplate.index_template.template?.settings,
                 'index.mapping.total_fields.limit': 2500,
+                'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
               },
             },
           });
@@ -899,6 +901,7 @@ describe('Alerts Service', () => {
                       },
                     }),
                 'index.mapping.ignore_malformed': true,
+                'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
                 'index.mapping.total_fields.limit': 2500,
               },
               mappings: {

x-pack/test/alerting_api_integration/spaces_only/tests/alerting/group4/alerts_as_data/alerts_as_data_dynamic_templates.ts
@@ -1,0 +1,225 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+import type {
+  MappingProperty,
+  PropertyName,
+  SearchHit,
+} from '@elastic/elasticsearch/lib/api/types';
+import { alertFieldMap, type Alert } from '@kbn/alerts-as-data-utils';
+import { TOTAL_FIELDS_LIMIT } from '@kbn/alerting-plugin/server';
+import { get } from 'lodash';
+import type { FtrProviderContext } from '../../../../../common/ftr_provider_context';
+import { Spaces } from '../../../../scenarios';
+import {
+  getEventLog,
+  getTestRuleData,
+  getUrlPrefix,
+  ObjectRemover,
+} from '../../../../../common/lib';
+
+// eslint-disable-next-line import/no-default-export
+export default function createAlertsAsDataDynamicTemplatesTest({ getService }: FtrProviderContext) {
+  const es = getService('es');
+  const retry = getService('retry');
+  const supertestWithoutAuth = getService('supertestWithoutAuth');
+  const objectRemover = new ObjectRemover(supertestWithoutAuth);
+
+  const alertsAsDataIndex =
+    '.internal.alerts-observability.test.alerts.dynamic.templates.alerts-default-000001';
+
+  describe('dynamic templates', function () {
+    this.tags('skipFIPS');
+    describe('alerts as data fields limit', function () {
+      afterEach(async () => {
+        await objectRemover.removeAll();
+        await es.deleteByQuery({
+          index: alertsAsDataIndex,
+          query: { match_all: {} },
+          conflicts: 'proceed',
+        });
+      });
+
+      it(`should add the dynamic fields`, async () => {
+        // First run doesn't add the dynamic fields
+        const createdRule = await supertestWithoutAuth
+          .post(`${getUrlPrefix(Spaces.space1.id)}/api/alerting/rule`)
+          .set('kbn-xsrf', 'foo')
+          .send(
+            getTestRuleData({
+              rule_type_id: 'test.always-firing-alert-as-data-with-dynamic-templates',
+              schedule: { interval: '1d' },
+              throttle: null,
+              params: {},
+              actions: [],
+            })
+          );
+        expect(createdRule.status).to.eql(200);
+        const ruleId = createdRule.body.id;
+        objectRemover.add(Spaces.space1.id, ruleId, 'rule', 'alerting');
+
+        await waitForEventLogDocs(ruleId, new Map([['execute', { equal: 1 }]]));
+
+        const existingFields = alertFieldMap;
+        const numberOfExistingFields = Object.keys(existingFields).length;
+        // there is no way to get the real number of fields from ES.
+        // Eventhough we have only as many as alertFieldMap fields,
+        // ES counts the each childs of the nested objects and multi_fields as seperate fields.
+        // therefore we add 9 to get the real number.
+        const nestedObjectsAndMultiFields = 9;
+        // Number of free slots that we want to have, so we can add dynamic fields as many
+        const numberofFreeSlots = 3;
+        const totalFields =
+          numberOfExistingFields + nestedObjectsAndMultiFields + numberofFreeSlots;
+
+        const dummyFields: Record<PropertyName, MappingProperty> = {};
+        for (let i = 0; i < TOTAL_FIELDS_LIMIT - totalFields; i++) {
+          const key = `${i}`.padStart(4, '0');
+          dummyFields[key] = { type: 'keyword' };
+        }
+        // add dummyFields to the index mappings, so it will reach the fields limits.
+        await es.indices.putMapping({
+          index: alertsAsDataIndex,
+          properties: dummyFields,
+          dynamic: false,
+        });
+
+        await supertestWithoutAuth
+          .put(`${getUrlPrefix(Spaces.space1.id)}/api/alerting/rule/${ruleId}`)
+          .set('kbn-xsrf', 'foo')
+          .send(
+            getTestRuleData({
+              schedule: { interval: '1d' },
+              throttle: null,
+              params: {
+                dynamic_fields: { 'host.id': '1', 'host.name': 'host-1' },
+              },
+              actions: [],
+              enabled: undefined,
+              rule_type_id: undefined,
+              consumer: undefined,
+            })
+          )
+          .expect(200);
+
+        const runSoon = await supertestWithoutAuth
+          .post(`${getUrlPrefix(Spaces.space1.id)}/internal/alerting/rule/${ruleId}/_run_soon`)
+          .set('kbn-xsrf', 'foo');
+        expect(runSoon.status).to.eql(204);
+
+        await waitForEventLogDocs(ruleId, new Map([['execute', { equal: 2 }]]));
+
+        // Query for alerts
+        const alerts = await queryForAlertDocs<Alert>();
+        const alert = alerts[0];
+
+        // host.name is ignored
+        expect(alert._ignored).to.eql(['kibana.alert.dynamic.host.name']);
+
+        const mapping = await es.indices.getMapping({ index: alertsAsDataIndex });
+        const dynamicField = get(
+          mapping[alertsAsDataIndex],
+          'mappings.properties.kibana.properties.alert.properties.dynamic.properties.host.properties.id.type'
+        );
+
+        // new dynamic field has been added
+        expect(dynamicField).to.eql('text');
+      });
+    });
+
+    describe('index field limits', () => {
+      afterEach(async () => {
+        await es.indices.delete({
+          index: 'index-fields-limit-test-index',
+        });
+        await es.indices.deleteIndexTemplate({
+          name: 'index-fields-limit-test-template',
+        });
+      });
+      it('should return an exceeded limit error', async () => {
+        const template = await es.indices.putIndexTemplate({
+          name: 'index-fields-limit-test-template',
+          template: {
+            mappings: {
+              properties: {
+                '@timestamp': {
+                  type: 'date',
+                },
+                'field-2': {
+                  type: 'keyword',
+                },
+                'field-3': {
+                  type: 'keyword',
+                },
+                'field-4': {
+                  type: 'keyword',
+                },
+                'field-5': {
+                  type: 'keyword',
+                },
+              },
+            },
+            settings: {
+              'index.mapping.total_fields.limit': 5,
+              'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
+            },
+          },
+          index_patterns: ['index-fields-limit-test-*'],
+        });
+
+        expect(template).to.eql({ acknowledged: true });
+
+        const index = await es.indices.create({
+          index: 'index-fields-limit-test-index',
+        });
+
+        expect(index).to.eql({
+          acknowledged: true,
+          index: 'index-fields-limit-test-index',
+          shards_acknowledged: true,
+        });
+
+        try {
+          await es.indices.putMapping({
+            index: 'index-fields-limit-test-index',
+            properties: {
+              'field-6': {
+                type: 'keyword',
+              },
+            },
+          });
+        } catch (e) {
+          expect(e.message).to.contain('Limit of total fields [5] has been exceeded');
+        }
+      });
+    });
+  });
+  async function queryForAlertDocs<T>(): Promise<Array<SearchHit<T>>> {
+    const searchResult = await es.search({
+      index: alertsAsDataIndex,
+      query: { match_all: {} },
+    });
+    return searchResult.hits.hits as Array<SearchHit<T>>;
+  }
+
+  async function waitForEventLogDocs(
+    id: string,
+    actions: Map<string, { gte: number } | { equal: number }>
+  ) {
+    return await retry.try(async () => {
+      return await getEventLog({
+        getService,
+        spaceId: Spaces.space1.id,
+        type: 'alert',
+        id,
+        provider: 'alerting',
+        actions,
+      });
+    });
+  }
+}

x-pack/platform/plugins/shared/alerting/common/alert_schema/field_maps/component_template_from_field_map.ts
@@ -5,7 +5,10 @@
  * 2.0.
  */
 
-import type { ClusterPutComponentTemplateRequest } from '@elastic/elasticsearch/lib/api/types';
+import type {
+  ClusterPutComponentTemplateRequest,
+  MappingDynamicTemplate,
+} from '@elastic/elasticsearch/lib/api/types';
 import { type FieldMap } from '@kbn/alerts-as-data-utils';
 import { mappingFromFieldMap } from './mapping_from_field_map';
 
@@ -14,12 +17,14 @@ export interface GetComponentTemplateFromFieldMapOpts {
   fieldMap: FieldMap;
   includeSettings?: boolean;
   dynamic?: 'strict' | false;
+  dynamicTemplates?: Array<Record<string, MappingDynamicTemplate>>;
 }
 export const getComponentTemplateFromFieldMap = ({
   name,
   fieldMap,
   dynamic,
   includeSettings,
+  dynamicTemplates,
 }: GetComponentTemplateFromFieldMapOpts): ClusterPutComponentTemplateRequest => {
   return {
     name,
@@ -37,7 +42,10 @@ export const getComponentTemplateFromFieldMap = ({
           : {}),
       },
 
-      mappings: mappingFromFieldMap(fieldMap, dynamic ?? 'strict'),
+      mappings: {
+        ...mappingFromFieldMap(fieldMap, dynamic ?? 'strict'),
+        ...(dynamicTemplates ? { dynamic_templates: dynamicTemplates } : {}),
+      },
     },
   };
 };

x-pack/platform/plugins/shared/alerting/server/alerts_service/lib/create_concrete_write_index.ts
@@ -11,6 +11,7 @@ import { get, sortBy } from 'lodash';
 import type { IIndexPatternString } from '../resource_installer_utils';
 import { retryTransientEsErrors } from './retry_transient_es_errors';
 import type { DataStreamAdapter } from './data_stream_adapter';
+import { updateIndexTemplateFieldsLimit } from './update_index_template_fields_limit';
 
 export interface ConcreteIndexInfo {
   index: string;
@@ -31,7 +32,10 @@ interface UpdateIndexOpts {
   esClient: ElasticsearchClient;
   totalFieldsLimit: number;
   concreteIndexInfo: ConcreteIndexInfo;
+  attempt?: number;
 }
+
+const MAX_FIELDS_LIMIT_INCREASE_ATTEMPTS = 100;
 
 const updateTotalFieldLimitSetting = async ({
   logger,
@@ -45,7 +49,10 @@ const updateTotalFieldLimitSetting = async ({
       () =>
         esClient.indices.putSettings({
           index,
-          settings: { 'index.mapping.total_fields.limit': totalFieldsLimit },
+          settings: {
+            'index.mapping.total_fields.limit': totalFieldsLimit,
+            'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
+          },
         }),
       { logger }
     );
@@ -66,6 +73,7 @@ const updateUnderlyingMapping = async ({
   logger,
   esClient,
   concreteIndexInfo,
+  attempt = 1,
 }: UpdateIndexOpts) => {
   const { index, alias } = concreteIndexInfo;
   let simulatedIndexMapping: IndicesSimulateIndexTemplateResponse;
@@ -96,6 +104,38 @@ const updateUnderlyingMapping = async ({
 
     return;
   } catch (err) {
+    if (attempt <= MAX_FIELDS_LIMIT_INCREASE_ATTEMPTS) {
+      try {
+        const newLimit = await increaseFieldsLimit({
+          err,
+          esClient,
+          concreteIndexInfo,
+          logger,
+          increment: attempt,
+        });
+        if (newLimit) {
+          logger.debug(
+            `Retrying PUT mapping for ${alias} with increased total_fields.limit of ${newLimit}. Attempt: ${attempt}`
+          );
+          await updateUnderlyingMapping({
+            logger,
+            esClient,
+            concreteIndexInfo,
+            totalFieldsLimit: newLimit,
+            attempt: attempt + 1,
+          });
+          return;
+        }
+      } catch (e) {
+        logger.error(
+          `An error occured while increasing total_fields.limit of ${alias} - ${e.message}`,
+          e
+        );
+        // Throw the original error
+        throw err;
+      }
+    }
+
     logger.error(`Failed to PUT mapping for ${alias}: ${err.message}`);
     throw err;
   }
@@ -205,3 +245,59 @@ export async function setConcreteWriteIndex(opts: SetConcreteWriteIndexOpts) {
     );
   }
 }
+
+const increaseFieldsLimit = async ({
+  err,
+  esClient,
+  concreteIndexInfo,
+  logger,
+  increment,
+}: {
+  err: Error;
+  esClient: ElasticsearchClient;
+  concreteIndexInfo: ConcreteIndexInfo;
+  logger: Logger;
+  increment: number;
+}): Promise<number | undefined> => {
+  const { alias } = concreteIndexInfo;
+  const match = err.message
+    ? err.message.match(/Limit of total fields \[(\d+)\] has been exceeded/)
+    : null;
+
+  if (match !== null) {
+    const exceededLimit = parseInt(match[1], 10);
+    const newLimit = exceededLimit + increment;
+
+    const { index_templates: indexTemplates } = await retryTransientEsErrors(
+      () =>
+        esClient.indices.getIndexTemplate({
+          name: `${alias}-index-template`,
+        }),
+      { logger }
+    );
+
+    if (indexTemplates.length <= 0) {
+      logger.error(`No index template found for ${alias}`);
+      return;
+    }
+    const template = indexTemplates[0];
+
+    // Update the limit in the index
+    await updateTotalFieldLimitSetting({
+      logger,
+      esClient,
+      totalFieldsLimit: newLimit,
+      concreteIndexInfo,
+    });
+    // Update the limit in the index template
+    await retryTransientEsErrors(
+      () => updateIndexTemplateFieldsLimit({ esClient, template, limit: newLimit }),
+      { logger }
+    );
+    logger.info(
+      `total_fields.limit of ${alias} has been increased from ${exceededLimit} to ${newLimit}`
+    );
+
+    return newLimit;
+  }
+};

x-pack/platform/plugins/shared/alerting/server/alerts_service/lib/create_or_update_component_template.test.ts
@@ -183,9 +183,99 @@ describe('createOrUpdateComponentTemplate', () => {
         settings: {
           ...existingIndexTemplate.index_template.template?.settings,
           'index.mapping.total_fields.limit': 2500,
+          'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
         },
       },
     });
+  });
+
+  it(`should flatten ignore_missing_component_templates when ignore_missing_component_templates is provided`, async () => {
+    clusterClient.cluster.putComponentTemplate.mockRejectedValueOnce(
+      new EsErrors.ResponseError(
+        elasticsearchClientMock.createApiResponse({
+          statusCode: 400,
+          body: {
+            error: {
+              root_cause: [
+                {
+                  type: 'illegal_argument_exception',
+                  reason:
+                    'updating component template [.alerts-ecs-mappings] results in invalid composable template [.alerts-security.alerts-default-index-template] after templates are merged',
+                },
+              ],
+              type: 'illegal_argument_exception',
+              reason:
+                'updating component template [.alerts-ecs-mappings] results in invalid composable template [.alerts-security.alerts-default-index-template] after templates are merged',
+              caused_by: {
+                type: 'illegal_argument_exception',
+                reason:
+                  'composable template [.alerts-security.alerts-default-index-template] template after composition with component templates [.alerts-ecs-mappings, .alerts-security.alerts-mappings, .alerts-technical-mappings] is invalid',
+                caused_by: {
+                  type: 'illegal_argument_exception',
+                  reason:
+                    'invalid composite mappings for [.alerts-security.alerts-default-index-template]',
+                  caused_by: {
+                    type: 'illegal_argument_exception',
+                    reason: 'Limit of total fields [2500] has been exceeded',
+                  },
+                },
+              },
+            },
+          },
+        })
+      )
+    );
+    const existingIndexTemplate = {
+      name: 'test-template',
+      index_template: {
+        index_patterns: ['test*'],
+        composed_of: ['test-mappings'],
+        ignore_missing_component_templates: ['test-mappings', 'test-mappings-2'],
+        template: {
+          settings: {
+            auto_expand_replicas: '0-1',
+            hidden: true,
+            'index.lifecycle': {
+              name: '.alerts-ilm-policy',
+              rollover_alias: `.alerts-empty-default`,
+            },
+            'index.mapping.total_fields.limit': 1800,
+          },
+          mappings: {
+            dynamic: false,
+          },
+        },
+      },
+    };
+
+    clusterClient.indices.getIndexTemplate.mockResolvedValueOnce({
+      index_templates: [existingIndexTemplate],
+    });
+
+    await createOrUpdateComponentTemplate({
+      logger,
+      esClient: clusterClient,
+      template: ComponentTemplate,
+      totalFieldsLimit: 2500,
+    });
+
+    expect(clusterClient.indices.putIndexTemplate).toHaveBeenCalledWith({
+      name: existingIndexTemplate.name,
+      ...existingIndexTemplate.index_template,
+      template: {
+        ...existingIndexTemplate.index_template.template,
+        settings: {
+          ...existingIndexTemplate.index_template.template?.settings,
+          'index.mapping.total_fields.limit': 2500,
+          'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
+        },
+      },
+      ignore_missing_component_templates: ['test-mappings', 'test-mappings-2'],
+    });
+
+    expect(logger.info).toHaveBeenCalledWith(
+      'The total number of fields defined by the templates cannot exceed the limit [2500]. if you want to add more fields, please increase the limit'
+    );
   });
 
   it(`should update index template field limit and retry if putTemplate throws error with field limit error when there are malformed index templates`, async () => {
@@ -287,6 +377,7 @@ describe('createOrUpdateComponentTemplate', () => {
         settings: {
           ...existingIndexTemplate.index_template.template?.settings,
           'index.mapping.total_fields.limit': 2500,
+          'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
         },
       },
     });

x-pack/platform/plugins/shared/alerting/server/alerts_service/lib/create_or_update_component_template.ts
@@ -12,6 +12,7 @@ import type {
 import type { Logger, ElasticsearchClient } from '@kbn/core/server';
 import { asyncForEach } from '@kbn/std';
 import { retryTransientEsErrors } from './retry_transient_es_errors';
+import { updateIndexTemplateFieldsLimit } from './update_index_template_fields_limit';
 
 interface CreateOrUpdateComponentTemplateOpts {
   logger: Logger;
@@ -48,21 +49,10 @@ const getIndexTemplatesUsingComponentTemplate = async (
     async (template: IndicesGetIndexTemplateIndexTemplateItem) => {
       await retryTransientEsErrors(
         () =>
-          esClient.indices.putIndexTemplate({
-            name: template.name,
-            ...template.index_template,
-            template: {
-              ...template.index_template.template,
-              settings: {
-                ...template.index_template.template?.settings,
-                'index.mapping.total_fields.limit': totalFieldsLimit,
-              },
-            },
-            // GET brings string | string[] | undefined but this PUT expects string[]
-            ignore_missing_component_templates: template.index_template
-              .ignore_missing_component_templates
-              ? [template.index_template.ignore_missing_component_templates].flat()
-              : undefined,
+          updateIndexTemplateFieldsLimit({
+            esClient,
+            template,
+            limit: totalFieldsLimit,
           }),
         { logger }
       );
@@ -81,6 +71,11 @@ const createOrUpdateComponentTemplateHelper = async (
   } catch (error) {
     const reason = error?.meta?.body?.error?.caused_by?.caused_by?.caused_by?.reason;
     if (reason && reason.match(/Limit of total fields \[\d+\] has been exceeded/) != null) {
+      if (reason === `Limit of total fields [${totalFieldsLimit}] has been exceeded`) {
+        logger.info(
+          `The total number of fields defined by the templates cannot exceed the limit [${totalFieldsLimit}]. if you want to add more fields, please increase the limit`
+        );
+      }
       // This error message occurs when there is an index template using this component template
       // that contains a field limit setting that using this component template exceeds
       // Specifically, this can happen for the ECS component template when we add new fields

x-pack/platform/plugins/shared/alerting/server/alerts_service/lib/create_or_update_index_template.test.ts
@@ -48,6 +48,7 @@ const IndexTemplate = (namespace = 'default', useDataStream = false) => ({
             },
           }),
       'index.mapping.ignore_malformed': true,
+      'index.mapping.total_fields.ignore_dynamic_beyond_limit': true,
       'index.mapping.total_fields.limit': 2500,
     },
   },

commit 8c6c928e7cde2233da4145c6e712785acf54c5f9

Fix ignored dynamic templates (#219875)
This PR fixes the bug introduced with:
https://github.com/elastic/kibana/pull/216719

We didn't pass the dynamic_templates param while creating the component
template.
The fields were still being added because the fields were marked as
`dynamic: true`

As the dynamic_template was ignored, the fields were added with an extra
mapping like `filedname.keyword`
This PR fixes that too.

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/platform/plugins/shared/alerting/server/alerts_service/alerts_service.ts
@@ -432,6 +432,7 @@ export class AlertsService implements IAlertsService {
       const componentTemplate = getComponentTemplate({
         fieldMap: mappings.fieldMap,
         dynamic: mappings.dynamic,
+        dynamicTemplates: mappings.dynamicTemplates,
         context,
       });
       initFns.push(

x-pack/platform/plugins/shared/alerting/server/alerts_service/alerts_service.test.ts
@@ -698,6 +698,36 @@ describe('Alerts Service', () => {
           }
         });
 
+        test('should save the dynamic_templates', async () => {
+          const dynamicTemplates = [
+            {
+              strings_as_keywords: {
+                path_match: 'test-path',
+                match_mapping_type: 'string',
+                mapping: {
+                  type: 'keyword',
+                  ignore_above: 1024,
+                },
+              },
+            },
+          ] satisfies IRuleTypeAlerts['mappings']['dynamicTemplates'];
+
+          alertsService.register({
+            ...TestRegistrationContext,
+            mappings: {
+              ...TestRegistrationContext.mappings,
+              dynamicTemplates,
+            },
+          });
+          await retryUntil(
+            'context initialized',
+            async () => (await getContextInitialized(alertsService)) === true
+          );
+
+          const componentTemplate = clusterClient.cluster.putComponentTemplate.mock.calls[3][0];
+          expect(componentTemplate.template.mappings?.dynamic_templates).toEqual(dynamicTemplates);
+        });
+
         test('should correctly install resources for custom namespace on demand when isSpaceAware is true', async () => {
           alertsService.register({ ...TestRegistrationContext, isSpaceAware: true });
           await retryUntil(

x-pack/test/alerting_api_integration/spaces_only/tests/alerting/group4/alerts_as_data/alerts_as_data_dynamic_templates.ts
@@ -73,7 +73,7 @@ export default function createAlertsAsDataDynamicTemplatesTest({ getService }: F
         // therefore we add 9 to get the real number.
         const nestedObjectsAndMultiFields = 9;
         // Number of free slots that we want to have, so we can add dynamic fields as many
-        const numberofFreeSlots = 3;
+        const numberofFreeSlots = 2;
         const totalFields =
           numberOfExistingFields + nestedObjectsAndMultiFields + numberofFreeSlots;
 
@@ -128,7 +128,7 @@ export default function createAlertsAsDataDynamicTemplatesTest({ getService }: F
         );
 
         // new dynamic field has been added
-        expect(dynamicField).to.eql('text');
+        expect(dynamicField).to.eql('keyword');
       });
     });
 