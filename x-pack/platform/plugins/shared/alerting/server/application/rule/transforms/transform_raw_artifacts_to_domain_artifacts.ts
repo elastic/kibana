415aa9e98ebe23fabe8280d3a7c44cff63f82439
Justin Kambic <jk@elastic.co>
2025-05-02 14:50:00 -0400

    Remove console.log

--- x-pack/platform/plugins/shared/alerting/server/application/rule/transforms/transform_raw_artifacts_to_domain_artifacts.ts
+++ x-pack/platform/plugins/shared/alerting/server/application/rule/transforms/transform_raw_artifacts_to_domain_artifacts.ts
@@ -15,6 +15,5 @@ export function transformRawArtifactsToDomainArtifacts(
   rawArtifacts?: RawRule['artifacts'],
   references?: SavedObjectReference[]
 ): Required<RuleDomain['artifacts']> {
-  console.log('raw artifacts param', rawArtifacts);
   return injectReferencesIntoArtifacts(id, rawArtifacts, references || []);
 }
