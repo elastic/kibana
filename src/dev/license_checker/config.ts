668c7a41e4c3fd4a8f3d62a0da7dfddb1a2ed101
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-05-02 14:33:48 +0200

    chore: update eui packages

--- src/dev/license_checker/config.ts
+++ src/dev/license_checker/config.ts
@@ -90,6 +90,7 @@ export const LICENSE_OVERRIDES = {
   '@elastic/ems-client@8.6.3': ['Elastic License 2.0'],
   '@elastic/eui@102.0.0': ['Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0'],
   '@elastic/eui-theme-borealis@1.0.0': ['Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0'],
+  '@elastic/eui-theme-common@1.0.0': ['Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0'],
   'language-subtag-registry@0.3.21': ['CC-BY-4.0'], // retired ODC‑By license https://github.com/mattcg/language-subtag-registry
   'buffers@0.1.1': ['MIT'], // license in importing module https://www.npmjs.com/package/binary
   '@bufbuild/protobuf@1.2.1': ['Apache-2.0'], // license (Apache-2.0 AND BSD-3-Clause)
