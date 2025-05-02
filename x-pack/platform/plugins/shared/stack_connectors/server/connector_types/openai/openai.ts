3992f8558d3543777388251b940f2d2e5e81418e
antman1p <antman1p@gmail.com>
2025-05-02 19:27:37 +0200

    more logging

--- x-pack/platform/plugins/shared/stack_connectors/server/connector_types/openai/openai.ts
+++ x-pack/platform/plugins/shared/stack_connectors/server/connector_types/openai/openai.ts
@@ -98,6 +98,9 @@ function formatPEMContent(pemContent: string, logger: Logger): string {
   logger.debug(`PEM content ends with footer: ${result.endsWith(`-----END ${type}-----\n`)}`);
   logger.debug(`PEM content has newline after header: ${result.includes(`-----BEGIN ${type}-----\n`)}`);
   logger.debug(`PEM content has newline before footer: ${result.includes(`\n-----END ${type}-----\n`)}`);
+  logger.debug(`PEM content line count: ${result.split('\n').length}`);
+  logger.debug(`PEM content first line: ${result.split('\n')[0]}`);
+  logger.debug(`PEM content last line: ${result.split('\n')[result.split('\n').length - 1]}`);
 
   return result;
 }
