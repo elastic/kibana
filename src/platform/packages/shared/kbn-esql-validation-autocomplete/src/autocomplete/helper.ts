0deb043c5ac02916618312a1266961d1ab79552a
drewdaemon <drew.tate@elastic.co>
2025-05-02 09:40:55 -0600

    add explanatory comment

--- src/platform/packages/shared/kbn-esql-validation-autocomplete/src/autocomplete/helper.ts
+++ src/platform/packages/shared/kbn-esql-validation-autocomplete/src/autocomplete/helper.ts
@@ -126,6 +126,16 @@ export function getQueryForFields(queryString: string, root: ESQLAstQueryExpress
   const commands = root.commands;
   const lastCommand = commands[commands.length - 1];
   if (lastCommand.name === 'fork') {
+    /**
+     * This translates the current fork command branch into a simpler but equivalent
+     * query that is compatible with the existing field computation/caching strategy.
+     *
+     * The intuition here is that if the cursor is within a fork branch, the
+     * previous context is equivalent to a query without the FORK command.:
+     *
+     * Original query: FROM lolz | EVAL foo = 1 | FORK (EVAL bar = 2) (EVAL baz = 3 | WHERE /)
+     * Simplified: FROM lolz | EVAL foo = 1 | EVAL baz = 3 | WHERE /
+     */
     const currentBranch = lastCommand.args[lastCommand.args.length - 1] as ESQLAstQueryExpression;
     const newCommands = commands.slice(0, -1).concat(currentBranch.commands.slice(0, -1));
     return BasicPrettyPrinter.print({ ...root, commands: newCommands });
