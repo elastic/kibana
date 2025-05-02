3d14533638b2d4727765bf6a66fdae9230533c57
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-05-02 11:25:51 +0200

    fix archives paths

--- x-pack/test/api_integration/deployment_agnostic/apis/observability/apm/constants/archiver.ts
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/apm/constants/archiver.ts
@@ -5,6 +5,8 @@
  * 2.0.
  */
 
+import path from 'path';
+
 type ArchiveName =
   | '8.0.0'
   | 'apm_mappings_only_8.0.0'
@@ -14,18 +16,15 @@ type ArchiveName =
   | 'rum_8.0.0'
   | 'rum_test_data';
 
+const ARCHIVES_DIR_REL_PATH =
+  'x-pack/solutions/observability/test/apm_api_integration/common/fixtures/es_archiver';
+
 export const ARCHIVER_ROUTES: { [key in ArchiveName]: string } = {
-  '8.0.0':
-    'x-pack/solutions/observability/test/apm_api_integration/common/fixtures/es_archiver/8.0.0',
-  'apm_mappings_only_8.0.0':
-    'x-pack/solutions/observability/test/apm_api_integration/common/fixtures/es_archiver/apm_mappings_only_8.0.0',
-  infra_metrics_and_apm:
-    'x-pack/solutions/observability/test/apm_api_integration/common/fixtures/es_archiver/infra_metrics_and_apm',
-  'metrics_8.0.0': 'x-pack/test/apm_api_integration/common/fixtures/es_archiver/metrics_8.0.0',
-  observability_overview:
-    'x-pack/solutions/observability/test/apm_api_integration/common/fixtures/es_archiver/observability_overview',
-  'rum_8.0.0':
-    'x-pack/solutions/observability/test/apm_api_integration/common/fixtures/es_archiver/rum_8.0.0',
-  rum_test_data:
-    'x-pack/solutions/observability/test/apm_api_integration/common/fixtures/es_archiver/rum_test_data',
+  '8.0.0': path.join(ARCHIVES_DIR_REL_PATH, '8.0.0'),
+  'apm_mappings_only_8.0.0': path.join(ARCHIVES_DIR_REL_PATH, 'apm_mappings_only_8.0.0'),
+  infra_metrics_and_apm: path.join(ARCHIVES_DIR_REL_PATH, 'infra_metrics_and_apm'),
+  'metrics_8.0.0': path.join(ARCHIVES_DIR_REL_PATH, 'metrics_8.0.0'),
+  observability_overview: path.join(ARCHIVES_DIR_REL_PATH, 'observability_overview'),
+  'rum_8.0.0': path.join(ARCHIVES_DIR_REL_PATH, 'rum_8.0.0'),
+  rum_test_data: path.join(ARCHIVES_DIR_REL_PATH, 'rum_test_data'),
 };
