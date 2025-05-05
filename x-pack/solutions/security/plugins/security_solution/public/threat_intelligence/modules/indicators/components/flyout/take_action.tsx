576cc3eaaafa6bf77f4d3a1b6c39f23050c9c01b
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-05-05 14:31:39 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/modules/indicators/components/flyout/take_action.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/modules/indicators/components/flyout/take_action.tsx
@@ -5,14 +5,15 @@
  * 2.0.
  */
 
-import React, { useState, VFC } from 'react';
+import type { VFC } from 'react';
+import React, { useState } from 'react';
 import { EuiButton, EuiContextMenuPanel, EuiPopover, useGeneratedHtmlId } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { canAddToBlockList } from '../../../block_list/utils/can_add_to_block_list';
 import { AddToBlockListContextMenu } from '../../../block_list/components/add_to_block_list';
 import { AddToNewCase } from '../../../cases/components/add_to_new_case';
 import { AddToExistingCase } from '../../../cases/components/add_to_existing_case';
-import { Indicator } from '../../../../../../common/threat_intelligence/types/indicator';
+import type { Indicator } from '../../../../../../common/threat_intelligence/types/indicator';
 import { InvestigateInTimelineContextMenu } from '../../../timeline/components/investigate_in_timeline';
 import {
   ADD_TO_BLOCK_LIST_TEST_ID,
