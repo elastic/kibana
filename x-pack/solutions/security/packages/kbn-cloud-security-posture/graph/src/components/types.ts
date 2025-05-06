502f272ab57386ed748deb586822e8a9c4e86268
alex prozorov <alex.prozorov@elastic.co>
2025-05-06 12:47:45 +0300

    add snapToGrid snapGrid and minimap storybook controls

--- x-pack/solutions/security/packages/kbn-cloud-security-posture/graph/src/components/types.ts
+++ x-pack/solutions/security/packages/kbn-cloud-security-posture/graph/src/components/types.ts
@@ -14,7 +14,14 @@ import type {
   NodeShape,
   Color as NodeColor,
 } from '@kbn/cloud-security-posture-common/types/graph/latest';
-import type { Node, NodeProps as xyNodeProps, Edge, EdgeProps as xyEdgeProps } from '@xyflow/react';
+import type {
+  Node,
+  NodeProps as xyNodeProps,
+  Edge,
+  EdgeProps as xyEdgeProps,
+  PanelPosition,
+  XYPosition,
+} from '@xyflow/react';
 
 export interface Size {
   width: number;
@@ -70,3 +77,77 @@ export type EdgeProps = xyEdgeProps<
     }
   >
 >;
+
+/**
+ * Props for the minimap component in the graph
+ */
+export interface MinimapProps {
+  /**
+   * Determines whether panning is enabled in the minimap
+   */
+  pannable?: boolean;
+  /**
+   * Determines whether zooming is enabled in the minimap
+   */
+  zoomable?: boolean;
+  /**
+   * Callback function called when minimap is clicked.
+   */
+  onClick?: (event: React.MouseEvent<Element, MouseEvent>, position: XYPosition) => void;
+  /**
+   * Color for nodes in the minimap
+   */
+  nodeColor?: string;
+  /**
+   * Stroke color for nodes in the minimap
+   */
+  nodeStrokeColor?: string;
+  /**
+   * Custom CSS class name for nodes in the minimap
+   */
+  nodeClassName?: string;
+  /**
+   * Border radius for nodes in the minimap (in pixels)
+   */
+  nodeBorderRadius?: number;
+  /**
+   * Stroke width for nodes in the minimap (in pixels)
+   */
+  nodeStrokeWidth?: number;
+  /**
+   * Background color of the minimap
+   */
+  bgColor?: string;
+  /**
+   * Color for the mask overlay in the minimap (RGBA format recommended)
+   */
+  maskColor?: string;
+  /**
+   * Stroke color for the mask in the minimap
+   */
+  maskStrokeColor?: string;
+  /**
+   * Stroke width for the mask in the minimap (in pixels)
+   */
+  maskStrokeWidth?: number;
+  /**
+   * Position of the minimap panel
+   */
+  position?: PanelPosition;
+  /**
+   * Accessibility label for the minimap
+   */
+  ariaLabel?: string;
+  /**
+   * Inverts panning direction when set to true
+   */
+  inversePan?: boolean;
+  /**
+   * Step size for zooming in the minimap
+   */
+  zoomStep?: number;
+  /**
+   * Scale offset for the minimap
+   */
+  offsetScale?: number;
+}
