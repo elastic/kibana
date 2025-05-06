b9303a9f3ec2f322bcddea529ae3d3727dea9ef1
Drew Tate <drew.tate@elastic.co>
2025-05-06 09:19:13 -0600

    [ES|QL] better field/source lists (#220172)
    Close https://github.com/elastic/kibana/issues/214498
    
    https://github.com/user-attachments/assets/3df43e08-e9dc-42f9-a6f0-5facc5a04a37
    
    - doesn't suggest an index twice (doesn't take into account index
    patterns, though we could)
    - works with quoted indices
    
    https://github.com/user-attachments/assets/086b098c-6104-47e9-838b-54618df2aee9
    
    - `KEEP`-style list suggestion pattern
    
    - fix ranges bug
    
    https://github.com/user-attachments/assets/65ed3179-b378-4d4c-9f52-7e1604b9f28f
    
    https://github.com/user-attachments/assets/ab09b83f-10b8-470c-a2c9-7862039f42a1
    
    - support for "Invoke" trigger kind
    - `KEEP`-style list suggestion pattern
    - fix ranges bug
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
    (cherry picked from commit a09b95d7539ec1fbda9669ecfd45bebc93824da9)

--- src/platform/packages/shared/kbn-esql-validation-autocomplete/src/autocomplete/helper.test.ts
+++ src/platform/packages/shared/kbn-esql-validation-autocomplete/src/autocomplete/helper.test.ts
@@ -7,7 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { buildPartialMatcher, getOverlapRange } from './helper';
+import { parse } from '@kbn/esql-ast';
+import { buildPartialMatcher, getOverlapRange, getQueryForFields } from './helper';
 
 describe('getOverlapRange', () => {
   it('should return the overlap range', () => {
@@ -36,5 +37,37 @@ describe('buildPartialMatcher', () => {
     expect(matcher.test('not')).toEqual(false);
     expect(matcher.test('is null')).toEqual(false);
     expect(matcher.test('is not nullz')).toEqual(false);
+  });
+});
+
+describe('getQueryForFields', () => {
+  const assert = (query: string, expected: string) => {
+    const { root } = parse(query);
+
+    const result = getQueryForFields(query, root);
+
+    expect(result).toEqual(expected);
+  };
+
+  it('should return everything up till the last command', () => {
+    const query = 'FROM index | EVAL foo = 1 | STATS field1 | KEEP esql_editor_marker';
+    assert(query, 'FROM index | EVAL foo = 1 | STATS field1');
+  });
+
+  it('should convert FORK branches into vanilla queries', () => {
+    const query = `FROM index
+    | EVAL foo = 1
+    | FORK (STATS field1 | EVAL esql_editor_marker)`;
+    assert(query, 'FROM index | EVAL foo = 1 | STATS field1');
+
+    const query2 = `FROM index 
+    | EVAL foo = 1
+    | FORK (STATS field1) (LIMIT 10) (WHERE field1 == 1 | EVAL esql_editor_marker)`;
+    assert(query2, 'FROM index | EVAL foo = 1 | WHERE field1 == 1');
+  });
+
+  it('should return empty string if non-FROM source command', () => {
+    assert('ROW field1 = 1', '');
+    assert('SHOW INFO', '');
   });
 });
