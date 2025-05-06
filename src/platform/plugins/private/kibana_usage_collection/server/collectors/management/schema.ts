2f74810e5c06a5206ab562d580af2f4eeda35f4c
Sergi Romeu <sergi.romeu@elastic.co>
2025-05-06 13:13:57 +0200

    fixes

--- src/platform/plugins/private/kibana_usage_collection/server/collectors/management/schema.ts
+++ src/platform/plugins/private/kibana_usage_collection/server/collectors/management/schema.ts
@@ -456,10 +456,6 @@ export const stackManagementSchema: MakeSchemaFrom<UsageStats> = {
     type: 'boolean',
     _meta: { description: 'Non-default value of setting.' },
   },
-  'observability:apmAgentExplorerView': {
-    type: 'boolean',
-    _meta: { description: 'Non-default value of setting.' },
-  },
   'observability:apmEnableTableSearchBar': {
     type: 'boolean',
     _meta: { description: 'Non-default value of setting.' },
