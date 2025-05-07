e70e2a6c2be5358fcff7e76eac3278d3647a5a47
lgestc <lukasz.gmys@elastic.co>
2025-05-07 10:17:25 +0200

    Revert "removed unused hoc"
    This reverts commit 352e2738ff35a73685f0cd83675d65f75a9371d8.

---
+++ x-pack/solutions/security/plugins/security_solution/public/common/components/with_data_view/index.tsx
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import type { ComponentType } from 'react';
+import type { ReactElement } from 'react-markdown';
+import type { DataView } from '@kbn/data-views-plugin/common';
+import { DataViewManagerScopeName } from '../../../data_view_manager/constants';
+import { useDataView } from '../../../data_view_manager/hooks/use_data_view';
+import { DataViewErrorComponent } from './data_view_error';
+import { useIsExperimentalFeatureEnabled } from '../../hooks/use_experimental_features';
+
+import { useGetScopedSourcererDataView } from '../../../sourcerer/components/use_get_sourcerer_data_view';
+import { SourcererScopeName } from '../../../sourcerer/store/model';
+
+type OmitDataView<T> = T extends { dataView: DataView } ? Omit<T, 'dataView'> : T;
+
+interface WithDataViewArg {
+  dataView: DataView;
+}
+
+/**
+ *
+ * This HOC makes sure the dataView is populated
+ * otherwise it will render the provided/default Error component
+ *
+ * */
+export const withDataView = <P extends WithDataViewArg>(
+  Component: ComponentType<P & WithDataViewArg>,
+  fallback?: ReactElement
+) => {
+  const ComponentWithDataView = (props: OmitDataView<P>) => {
+    const oldDataView = useGetScopedSourcererDataView({
+      sourcererScope: SourcererScopeName.timeline,
+    });
+    const { dataView: experimentalDataView } = useDataView(DataViewManagerScopeName.timeline);
+
+    const newDataViewPickerEnabled = useIsExperimentalFeatureEnabled('newDataViewPickerEnabled');
+
+    const dataView = newDataViewPickerEnabled ? experimentalDataView : oldDataView;
+
+    if (!dataView) {
+      return fallback ?? <DataViewErrorComponent />;
+    }
+
+    return <Component {...(props as unknown as P)} dataView={dataView} />;
+  };
+
+  return ComponentWithDataView;
+};
