9a7bda6e38a2d41216fcfc4c43678a58ea1055ce
Ying <ying.mao@elastic.co>
2025-05-07 09:46:24 -0400

    Fixing bad merge

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/types.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/types.ts
@@ -5,8 +5,4 @@
  * 2.0.
  */
 
-<<<<<<<< HEAD:x-pack/platform/plugins/private/reporting/server/saved_objects/scheduled_report/schemas/index.ts
-export { rawScheduledReportSchema as rawScheduledReportSchemaV1 } from './v1';
-========
 export type PrivMonUserSource = 'csv' | 'api' | 'index_sync';
->>>>>>>> scheduled-reports-api:x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/types.ts
