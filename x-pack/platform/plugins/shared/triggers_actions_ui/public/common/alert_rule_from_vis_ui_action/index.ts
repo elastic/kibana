68867bdae4e30758948af4b3326000d171c90efd
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-05-08 14:50:19 -0500

    Split query assembly into separate functions

--- x-pack/platform/plugins/shared/triggers_actions_ui/public/common/alert_rule_from_vis_ui_action/index.ts
+++ x-pack/platform/plugins/shared/triggers_actions_ui/public/common/alert_rule_from_vis_ui_action/index.ts
@@ -12,8 +12,6 @@ import type {
 } from '@kbn/alerts-ui-shared';
 import { DimensionType } from '@kbn/expressions-plugin/common';
 import type { Datatable } from '@kbn/expressions-plugin/common';
-import { sanitazeESQLInput } from '@kbn/esql-utils';
-import { parse, walk, type ESQLColumn, type ESQLFunction } from '@kbn/esql-ast';
 import { i18n } from '@kbn/i18n';
 import type { LensApi, TextBasedPersistedState } from '@kbn/lens-plugin/public';
 import { apiIsOfType, hasBlockingError } from '@kbn/presentation-publishing';
@@ -21,7 +19,8 @@ import type { RuleFormData } from '@kbn/response-ops-rule-form';
 import type { EsQueryRuleParams } from '@kbn/response-ops-rule-params/es_query';
 import { ALERT_RULE_TRIGGER } from '@kbn/ui-actions-browser/src/triggers';
 import type { Action } from '@kbn/ui-actions-plugin/public';
-import { pick, snakeCase } from 'lodash';
+import { pick } from 'lodash';
+import { buildAdditionalQuery } from './build_additional_query';
 
 interface Context {
   data?: AlertRuleFromVisUIActionData;
@@ -60,7 +59,7 @@ export class AlertRuleFromVisAction implements Action<Context> {
   public shouldAutoExecute = async () => true;
 
   public async execute({ embeddable, data }: Context) {
-    const { query, thresholdValues, xValues, usesPlaceholderValues } = data?.query
+    const embeddableData = data?.query
       ? data
       : data
       ? {
@@ -72,6 +71,7 @@ export class AlertRuleFromVisAction implements Action<Context> {
           ]),
         }
       : getDataFromEmbeddable(embeddable);
+    const { query } = embeddableData;
 
     // Get the timeField, default to `timestamp` if it can't be found
     const datatable = getDataTableFromEmbeddable(embeddable);
@@ -84,123 +84,10 @@ export class AlertRuleFromVisAction implements Action<Context> {
     const [firstLayer] = Object.values(layers ?? {});
     const { timeField = 'timestamp' } = firstLayer ?? { timeField: dataView?.timeFieldName };
 
-    // Set up a helper function to rename fields that need to be escaped
-    const { root } = parse(query ?? '');
-    const columns: ESQLColumn[] = [];
-    const functions: ESQLFunction[] = [];
+    // Generate additional query to append to the base ES|QL query from the visualization
+    const additionalQuery = buildAdditionalQuery(embeddableData);
 
-    walk(root, {
-      visitColumn: (node) => columns.push(node),
-    });
-
-    walk(root, {
-      visitFunction: (node) => functions.push(node),
-    });
-
-    let renameQuery = '';
-    const escapeFieldName = (fieldName: string) => {
-      if (!fieldName || fieldName === 'undefined') return missingSourceFieldPlaceholder;
-      const column = columns.find((col) => col.name === fieldName);
-      if (column) {
-        return column.name;
-      }
-
-      // Find all ESQL functions and rename them
-      const WHITESPACE_AFTER_COMMA_REGEX = /, +/g;
-      const esqlFunction = functions.find(
-        (func) => func.text === fieldName.replace(WHITESPACE_AFTER_COMMA_REGEX, ',')
-      );
-      if (esqlFunction) {
-        const colName = `_${snakeCase(fieldName)}`;
-        const sanitizedFieldName = sanitazeESQLInput(esqlFunction.text);
-        // Add this to the renameQuery as a side effect
-        const renameClause = `| RENAME ${sanitizedFieldName} as ${colName} `;
-        if (!renameQuery.includes(renameClause)) renameQuery += renameClause;
-        return colName;
-      }
-
-      return fieldName;
-    };
-
-    const stringValueToESQLCondition = (
-      fieldName: string,
-      v: string | number | null | undefined
-    ) => {
-      try {
-        // If the value is a string containing a comma, first attempt to parse it as a JSON-formatted array
-        if (typeof v === 'string' && v.includes(',')) {
-          const stringToParse = v.startsWith('[')
-            ? v
-            : `[${v
-                .split(',')
-                .map((item) => `"${item}"`)
-                .join()}]`;
-          const parsed = JSON.parse(stringToParse);
-          if (Array.isArray(parsed)) {
-            return parsed
-              .map(
-                (multiVal) =>
-                  `MATCH(${escapeFieldName(fieldName)}, ${formatStringForESQL(multiVal)})`
-              )
-              .join(' AND ');
-          }
-        }
-      } catch {
-        // Do nothing, continue to return statement
-      }
-      return `${escapeFieldName(fieldName)} == ${
-        typeof v === 'number' ? v : formatStringForESQL(v ?? '')
-      }`;
-    };
-
-    // Generate an addition to the query that sets an alert threshold
-    const thresholdQuery = thresholdValues
-      .map(({ values, yField }) => {
-        const conditions = Object.entries(values)
-          // Always move the Y axis value to the end of the query
-          .sort(([sourceField]) => (sourceField === yField ? 1 : -1))
-          .map(([sourceField, value]) =>
-            sourceField === yField
-              ? `${escapeFieldName(sourceField)} >= ${value}`
-              : stringValueToESQLCondition(sourceField, value)
-          )
-          .join(' AND ');
-        return thresholdValues.length > 1 && Object.keys(values).length > 1
-          ? `(${conditions})`
-          : conditions;
-      })
-      .join(' OR ');
-    const thresholdQueryComment = usesPlaceholderValues
-      ? i18n.translate('xpack.triggersActionsUI.alertRuleFromVis.thresholdPlaceholderComment', {
-          defaultMessage:
-            'Modify the following conditions to set an alert threshold for this rule:',
-        })
-      : i18n.translate('xpack.triggersActionsUI.alertRuleFromVis.thresholdComment', {
-          defaultMessage:
-            'Threshold automatically generated from the selected {thresholdValues, plural, one {value} other {values} } on the chart. This rule will generate an alert based on the following conditions:',
-          values: { thresholdValues: Object.keys(thresholdValues).length },
-        });
-
-    const xValueQueries = Object.entries(xValues).map(([fieldName, value]) =>
-      stringValueToESQLCondition(fieldName, value)
-    );
-
-    const conditionsQuery = [
-      ...xValueQueries,
-      xValueQueries.length && thresholdValues.length > 1 ? `(${thresholdQuery})` : thresholdQuery,
-    ].join(' AND ');
-
-    // Generate ES|QL to escape function columns
-    if (renameQuery.length)
-      renameQuery = `// ${i18n.translate('xpack.triggersActionsUI.alertRuleFromVis.renameComment', {
-        defaultMessage:
-          'Rename the following columns so they can be used as part of the alerting threshold:',
-      })}\n${renameQuery}\n`;
-
-    // Combine the escaped columns with the threshold conditions query
-    const additionalQuery = `${renameQuery}// ${thresholdQueryComment}\n| WHERE ${conditionsQuery}`;
-
-    // Generate the full ES|QL code
+    // Assemble the full ES|QL code
     let initialValues: Partial<RuleFormData<Partial<EsQueryRuleParams>>>;
     if (query) {
       const queryHeader = i18n.translate(
@@ -309,24 +196,6 @@ const getDataFromEmbeddable = (embeddable: Context['embeddable']): AlertRuleFrom
     usesPlaceholderValues: true,
   };
 };
-
-const formatStringForESQL = (value: string) => {
-  let sanitizedValue = value;
-  // This is how you escape backslashes in Javascript. This language was invented in 10 days in 1995 and now we have to do this
-
-  // This translates to "If value includes a '\' character" in reasonable human being language
-  if (value.includes('\\')) {
-    // Split value by every '\' and replace them all with '\\'. I promise this is what this code means.
-    sanitizedValue = value.split('\\').join('\\\\');
-  }
-  return `"${sanitizedValue}"`;
-};
-const missingSourceFieldPlaceholder = i18n.translate(
-  'xpack.triggersActionsUI.alertRuleFromVis.fieldNamePlaceholder',
-  {
-    defaultMessage: '[FIELD NAME]',
-  }
-);
 
 const missingYFieldPlaceholder = i18n.translate(
   'xpack.triggersActionsUI.alertRuleFromVis.yAxisPlaceholder',
