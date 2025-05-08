4aecb151822c5168b473dc8469bab0e7423b00b7
Paul Tavares <paul.tavares@elastic.co>
2025-05-08 14:59:32 -0400

    Enhanced FleetError stack trace + utilities for wrapping errors with FleetError

--- x-pack/platform/plugins/shared/fleet/server/errors/utils.ts
+++ x-pack/platform/plugins/shared/fleet/server/errors/utils.ts
@@ -7,6 +7,8 @@
 
 import { errors } from '@elastic/elasticsearch';
 
+import { FleetError } from '../../common';
+
 export function isESClientError(error: unknown): error is errors.ResponseError {
   return error instanceof errors.ResponseError;
 }
@@ -14,3 +16,32 @@ export function isESClientError(error: unknown): error is errors.ResponseError {
 export function isElasticsearchVersionConflictError(error: Error): boolean {
   return isESClientError(error) && error.meta.statusCode === 409;
 }
+
+export const wrapWithFleetErrorIfNeeded = (err: Error): FleetError => {
+  if (err instanceof FleetError) {
+    return err;
+  }
+
+  let message = err.message;
+
+  if (isESClientError(err)) {
+    message += ` (Status Code: [${err.statusCode}], body: [${JSON.stringify(err.body)}])`;
+  }
+
+  return new FleetError(message, err);
+};
+
+/**
+ * Error handling utility for use with promises that will wrap any thrown error with a `FleetError`.
+ * Especially useful when working with ES/SO client, as errors thrown by those client normally do
+ * not include a very helpful stack trace.
+ *
+ * @param error
+ *
+ * @example
+ *
+ *    esClient.search(...).catch(catchAndWrapError);
+ */
+export const catchAndWrapError = (error: Error): Promise<never> => {
+  return Promise.reject(wrapWithFleetErrorIfNeeded(error));
+};
