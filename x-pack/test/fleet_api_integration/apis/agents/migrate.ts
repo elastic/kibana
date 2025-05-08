9e483dbf4b172371ab9d54ad4e368528a7f976cf
Supplementing <mason.d.herron@gmail.com>
2025-05-07 22:03:56 -0600

    integration tests complete

--- x-pack/test/fleet_api_integration/apis/agents/migrate.ts
+++ x-pack/test/fleet_api_integration/apis/agents/migrate.ts
@@ -4,14 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
 
 import expect from '@kbn/expect';
+
+import { AGENTS_INDEX, AGENT_POLICY_INDEX } from '@kbn/fleet-plugin/common';
 
 import { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
 
@@ -25,29 +21,79 @@ export default function (providerContext: FtrProviderContext) {
   describe('fleet_agents_migrate', () => {
     before(async () => {
       await esArchiver.load('x-pack/test/functional/es_archives/fleet/agents');
+      // Create agent policies
+      await es.index({
+        refresh: 'wait_for',
+        index: AGENT_POLICY_INDEX,
+        document: {
+          revision_idx: 2,
+          policy_id: 'policy1',
+          coordinator_idx: 0,
+          '@timestamp': '2023-03-15T13:00:00.000Z',
+        },
+      });
+
+      await es.index({
+        refresh: 'wait_for',
+        index: AGENT_POLICY_INDEX,
+        document: {
+          revision_idx: 3,
+          policy_id: 'policy2',
+          coordinator_idx: 0,
+          '@timestamp': '2023-03-15T13:00:00.000Z',
+        },
+      });
+
+      // Create agents
+      await es.index({
+        refresh: 'wait_for',
+        index: AGENTS_INDEX,
+        id: 'agent1',
+        document: {
+          policy_id: 'policy1',
+        },
+      });
+
+      await es.index({
+        refresh: 'wait_for',
+        index: AGENTS_INDEX,
+        id: 'agent2',
+        document: {
+          policy_id: 'policy1',
+          is_protected: true,
+        },
+      });
+      await es.index({
+        refresh: 'wait_for',
+        index: AGENTS_INDEX,
+        id: 'agent3',
+        document: {
+          policy_id: 'policy1',
+          components: [
+            {
+              type: 'fleet-server',
+              id: 'fleet-server',
+              revision: 1,
+            },
+          ],
+        },
+      });
     });
+
     after(async () => {
       await esArchiver.unload('x-pack/test/functional/es_archives/fleet/agents');
     });
-    // TODO: create some agents in here so we can check things. DO it in the before (not beforeEach) so we can
-    // reuse them in the other tests (above)
 
-    // END TODO
     describe('POST /agents/{agentId}/migrate', () => {
       it('should return a 200 if the migration action is successful', async () => {
         const { body: apiResponse } = await supertest
           .post(`/api/fleet/agents/agent1/migrate`)
           .set('kbn-xsrf', 'xx')
           .send({
-            action: {
-              type: 'SETTINGS',
-              data: { log_level: 'debug' },
-            },
+            enrollment_token: '1234',
+            uri: 'https://example.com',
           })
           .expect(200);
-
-        expect(apiResponse.item.type).to.eql('SETTINGS');
-        expect(apiResponse.item.data).to.eql({ log_level: 'debug' });
       });
 
       it('should return a 500 if the agent is tamper protected', async () => {
@@ -55,15 +101,10 @@ export default function (providerContext: FtrProviderContext) {
           .post(`/api/fleet/agents/agent2/migrate`)
           .set('kbn-xsrf', 'xx')
           .send({
-            action: {
-              type: 'SETTINGS',
-              data: { log_level: null },
-            },
+            enrollment_token: '1234',
+            uri: 'https://example.com',
           })
           .expect(200);
-
-        expect(apiResponse.item.type).to.eql('SETTINGS');
-        expect(apiResponse.item.data).to.eql({ log_level: null });
       });
 
       it('should return a 500 is the agent is a fleet-agent', async () => {
@@ -71,16 +112,10 @@ export default function (providerContext: FtrProviderContext) {
           .post(`/api/fleet/agents/agent3/migrate`)
           .set('kbn-xsrf', 'xx')
           .send({
-            action: {
-              type: 'SETTINGS',
-              data: { log_level: 'thisnotavalidloglevel' },
-            },
+            enrollment_token: '1234',
+            uri: 'https://example.com',
           })
-          .expect(400);
-
-        expect(apiResponse.message).to.match(
-          /\[request body.action\.[0-9]*\.data\.log_level]: types that failed validation/
-        );
+          .expect(500);
       });
 
       it('should return a 404 when agent does not exist', async () => {
@@ -88,10 +123,8 @@ export default function (providerContext: FtrProviderContext) {
           .post(`/api/fleet/agents/agent100/migrate`)
           .set('kbn-xsrf', 'xx')
           .send({
-            action: {
-              type: 'SETTINGS',
-              data: { log_level: 'debug' },
-            },
+            enrollment_token: '1234',
+            uri: 'https://example.com',
           })
           .expect(404);
       });
