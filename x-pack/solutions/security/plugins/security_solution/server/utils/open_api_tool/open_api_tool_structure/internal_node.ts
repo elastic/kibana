28fe5ffeb3e5623476be187059b6fcfcdc77fc66
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-05-09 12:05:08 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/security/plugins/security_solution/server/utils/open_api_tool/open_api_tool_structure/internal_node.ts
+++ x-pack/solutions/security/plugins/security_solution/server/utils/open_api_tool/open_api_tool_structure/internal_node.ts
@@ -1,4 +1,11 @@
-import { OperationNode } from "./operation_node";
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { OperationNode } from './operation_node';
 
 export class InternalNode {
   name: string;
