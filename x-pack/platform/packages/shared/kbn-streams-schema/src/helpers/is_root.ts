e08fe1f081c59f61c1195abf2c5b922f9a220eee
Joe Reuter <johannes.reuter@elastic.co>
2025-05-09 16:10:37 +0200

    fix and tests

--- x-pack/platform/packages/shared/kbn-streams-schema/src/helpers/is_root.ts
+++ x-pack/platform/packages/shared/kbn-streams-schema/src/helpers/is_root.ts
@@ -9,7 +9,7 @@ import { Streams } from '../models/streams';
 import { createIsNarrowSchema } from '../shared/type_guards';
 
 export const isRootStreamDefinition = createIsNarrowSchema(
-  Streams.WiredStream.Definition.right,
+  Streams.all.Definition.right,
   Streams.WiredStream.Definition.right.refine((stream) => {
     return stream.name.split('.').length === 1;
   })
