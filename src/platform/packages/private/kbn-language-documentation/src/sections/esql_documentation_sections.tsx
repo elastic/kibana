a721d48358d54bf39b33b463be6a17a1abbc999f
Liam Thompson <32779855+leemthompo@users.noreply.github.com>
2025-05-15 18:31:55 +0200

    Use 8.18 docs link

--- src/platform/packages/private/kbn-language-documentation/src/sections/esql_documentation_sections.tsx
+++ src/platform/packages/private/kbn-language-documentation/src/sections/esql_documentation_sections.tsx
@@ -413,7 +413,7 @@ ROW language_code = 1
 | LOOKUP JOIN languages ON language_code
 \`\`\`
 
-An index that is used in \`LOOKUP JOIN\` needs to be in lookup mode. This [index mode](https://www.elastic.co/docs/reference/elasticsearch/index-settings/index-modules#_static_index_settings) needs to be set when the index is created.
+An index that is used in \`LOOKUP JOIN\` needs to be in lookup mode. This [index mode](https://www.elastic.co/guide/en/elasticsearch/reference/8.18/index-modules.html#_static_index_settings) needs to be set when the index is created.
 
 \`\`\`
 PUT languages
