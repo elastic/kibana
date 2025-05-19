b8dadd57e0fc04acd987ed0c6b18384254dfb652
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-05-19 11:50:09 +0200

    fix imports: MlIndexUtils

--- x-pack/platform/plugins/shared/ml/public/application/components/custom_urls/custom_urls.tsx
+++ x-pack/platform/plugins/shared/ml/public/application/components/custom_urls/custom_urls.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { Component } from 'react';
-import type { TimeRange as EsQueryTimeRange } from '@kbn/es-query';
+
 import {
   EuiButton,
   EuiButtonEmpty,
@@ -22,13 +22,16 @@ import {
   EuiModalFooter,
   EuiScreenReaderOnly,
 } from '@elastic/eui';
-import { FormattedMessage } from '@kbn/i18n-react';
 
+import type { TimeRange as EsQueryTimeRange } from '@kbn/es-query';
+import { FormattedMessage } from '@kbn/i18n-react';
 import { i18n } from '@kbn/i18n';
 import { context } from '@kbn/kibana-react-plugin/public';
 import type { DataViewListItem } from '@kbn/data-views-plugin/common';
 import type { MlUrlConfig } from '@kbn/ml-anomaly-utils';
 import { isDataFrameAnalyticsConfigs } from '@kbn/ml-data-frame-analytics-utils';
+import { indexServiceFactory, type MlIndexUtils } from '@kbn/ml-services/index_service';
+
 import type { DashboardService, DashboardItems } from '../../services/dashboard_service';
 import type { ToastNotificationService } from '../../services/toast_notification_service';
 import { toastNotificationServiceProvider } from '../../services/toast_notification_service';
@@ -43,7 +46,6 @@ import {
 } from './custom_url_editor/utils';
 import { openCustomUrlWindow } from '../../util/custom_url_utils';
 import type { CustomUrlsWrapperProps } from './custom_urls_wrapper';
-import { indexServiceFactory, type MlIndexUtils } from '../../util/index_service';
 
 interface CustomUrlsState {
   customUrls: MlUrlConfig[];
@@ -317,7 +319,7 @@ export class CustomUrls extends Component<CustomUrlsProps, CustomUrlsState> {
       <EuiModal
         onClose={this.closeEditor}
         initialFocus="[name=label]"
-        style={{ width: 500 }}
+        css={{ width: 500 }}
         data-test-subj="mlJobNewCustomUrlFormModal"
         aria-label={i18n.translate(
           'xpack.ml.jobsList.editJobFlyout.customUrls.addCustomUrlAriaLabel',
