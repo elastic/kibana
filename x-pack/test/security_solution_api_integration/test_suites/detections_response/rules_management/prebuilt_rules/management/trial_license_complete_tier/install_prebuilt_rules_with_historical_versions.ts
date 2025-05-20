768371a5350491411ee61cbc7f39ef5cbc5182fa
Davis Plumlee <davis.plumlee@elastic.co>
2025-05-20 11:59:09 -0400

    fixes test

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/management/trial_license_complete_tier/install_prebuilt_rules_with_historical_versions.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/management/trial_license_complete_tier/install_prebuilt_rules_with_historical_versions.ts
@@ -108,24 +108,40 @@ export default ({ getService }: FtrProviderContext): void => {
         ]);
         await installPrebuiltRulesAndTimelines(es, supertest);
 
-        const { body: hookAction } = await supertest
-          .post('/api/actions/connector')
-          .set('kbn-xsrf', 'true')
-          .send(getWebHookAction())
-          .expect(200);
+        // create connector/action
+        const createConnector = async (payload: Record<string, unknown>) =>
+          (
+            await supertest
+              .post('/api/actions/action')
+              .set('kbn-xsrf', 'true')
+              .send(payload)
+              .expect(200)
+          ).body;
+
+        const createWebHookConnector = () => createConnector(getWebHookAction());
+
+        const webHookAction = await createWebHookConnector();
+
+        const defaultRuleAction = {
+          id: webHookAction.id,
+          action_type_id: '.webhook' as const,
+          group: 'default' as const,
+          params: {
+            body: '{"test":"a default action"}',
+          },
+          frequency: {
+            notifyWhen: 'onThrottleInterval' as const,
+            summary: true,
+            throttle: '1h' as const,
+          },
+          uuid: 'd487ec3d-05f2-44ad-8a68-11c97dc92202',
+        };
 
         await securitySolutionApi
           .patchRule({
             body: {
               rule_id: 'rule-1',
-              actions: [
-                {
-                  group: 'default',
-                  id: hookAction.id,
-                  action_type_id: hookAction.connector_type_id,
-                  params: {},
-                },
-              ],
+              actions: [defaultRuleAction],
             },
           })
           .expect(200);
@@ -145,15 +161,7 @@ export default ({ getService }: FtrProviderContext): void => {
         });
 
         // Check the actions field of existing prebuilt rules is not overwritten
-        expect(prebuiltRule.actions).toEqual([
-          expect.objectContaining({
-            action_type_id: hookAction.connector_type_id,
-            frequency: { notifyWhen: 'onActiveAlert', summary: true, throttle: null },
-            group: 'default',
-            id: hookAction.id,
-            params: {},
-          }),
-        ]);
+        expect(prebuiltRule.actions).toEqual([defaultRuleAction]);
       });
 
       it('should not overwrite existing exceptions lists', async () => {
