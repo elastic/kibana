3f4cf818e57a193d4d105aaed884525d51cffb35
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-05-20 14:33:10 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/saved_object/monitoring_entity_source.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/saved_object/monitoring_entity_source.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import type { SavedObjectsClientContract, SavedObjectsFindResponse } from '@kbn/core/server';
+import type { SavedObjectsClientContract } from '@kbn/core/server';
 import { monitoringEntitySourceTypeName } from './monitoring_entity_source_type';
 
 interface MonitoringEntitySourceDependencies {
