95511451f46063305df5336dbbc47327fba6bd66
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-05-20 12:25:23 +0200

    @kbn/ml-server-api-schemas

--- x-pack/platform/packages/shared/ml/services/results_service/get_partition_fields_values.ts
+++ x-pack/platform/packages/shared/ml/services/results_service/get_partition_fields_values.ts
@@ -6,11 +6,12 @@
  */
 
 import Boom from '@hapi/boom';
+
 import type { estypes } from '@elastic/elasticsearch';
 import { type MlPartitionFieldsType, type CriteriaField } from '@kbn/ml-anomaly-utils';
 import { ML_PARTITION_FIELDS } from '@kbn/ml-anomaly-utils/constants';
 import type { MlClient } from '@kbn/ml-client';
-import type { FieldConfig, FieldsConfig } from '../../routes/schemas/results_service_schema';
+import type { FieldConfig, FieldsConfig } from '@kbn/ml-server-api-schemas/results_service_schema';
 
 type SearchTerm =
   | {
