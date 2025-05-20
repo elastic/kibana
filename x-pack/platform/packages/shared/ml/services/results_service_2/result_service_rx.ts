62589c8c1b7de6228f352a0649779ade2176ccfe
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-05-20 11:28:36 +0200

    fix imports: error types

--- x-pack/platform/packages/shared/ml/services/results_service_2/result_service_rx.ts
+++ x-pack/platform/packages/shared/ml/services/results_service_2/result_service_rx.ts
@@ -18,7 +18,7 @@ import { map } from 'rxjs';
 import { each, get } from 'lodash';
 
 import { isPopulatedObject } from '@kbn/ml-is-populated-object';
-import type { ErrorType } from '@kbn/ml-error-utils';
+import type { ErrorType } from '@kbn/ml-common-types/errors';
 import { aggregationTypeTransform } from '@kbn/ml-anomaly-utils/anomaly_utils';
 import { ES_AGGREGATION } from '@kbn/ml-anomaly-utils/es_aggregation';
 import { isRuntimeMappings } from '@kbn/ml-runtime-field-utils';
