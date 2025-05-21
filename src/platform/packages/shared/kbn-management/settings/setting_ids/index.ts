e4b401b2808d7554a0eb3deb9a9e47e332bb7156
PhilippeOberti <philippe.oberti@elastic.co>
2025-05-21 16:49:57 -0500

    undo schema and config changes as these have to be done in a follow up PR

--- src/platform/packages/shared/kbn-management/settings/setting_ids/index.ts
+++ src/platform/packages/shared/kbn-management/settings/setting_ids/index.ts
@@ -164,6 +164,8 @@ export const SECURITY_SOLUTION_DEFAULT_ALERT_TAGS_KEY = 'securitySolution:alertT
 /** This Kibana Advanced Setting allows users to enable/disable the Asset Criticality feature */
 export const SECURITY_SOLUTION_ENABLE_ASSET_CRITICALITY_SETTING =
   'securitySolution:enableAssetCriticality' as const;
+export const SECURITY_SOLUTION_ENABLE_VISUALIZATIONS_IN_FLYOUT_SETTING =
+  'securitySolution:enableVisualizationsInFlyout' as const;
 export const SECURITY_SOLUTION_ENABLE_GRAPH_VISUALIZATION_SETTING =
   'securitySolution:enableGraphVisualization' as const;
 export const SECURITY_SOLUTION_ENABLE_ASSET_INVENTORY_SETTING =
