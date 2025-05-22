e4f7b063184c3bcc887bad329c37908e12303877
Nick Partridge <nicholas.partridge@elastic.co>
2025-05-22 11:34:49 -0700

    test: update tests and snapshots

--- src/platform/plugins/shared/charts/public/services/palettes/palettes.test.tsx
+++ src/platform/plugins/shared/charts/public/services/palettes/palettes.test.tsx
@@ -7,13 +7,14 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
+import { euiPaletteColorBlind, euiPaletteColorBlindBehindText } from '@elastic/eui';
+
 import { createColorPalette as createLegacyColorPalette } from '../..';
 import { buildPalettes } from './palettes';
 import { colorsServiceMock } from '../legacy_colors/mock';
-import { euiPaletteColorBlind, euiPaletteColorBlindBehindText } from '@elastic/eui';
 
 describe('palettes', () => {
-  const palettes = buildPalettes({ darkMode: false }, {} as any);
+  const palettes = buildPalettes({ darkMode: false }, colorsServiceMock);
 
   describe('default palette', () => {
     describe('syncColors: false', () => {
