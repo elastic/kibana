d5262b1483a1e10dd00cadebc0ae9abc90d1a973
Ola Pawlus <ola.pawlus@elastic.co>
2025-05-22 15:04:53 +0200

    Prevent unnecessary re-registrations in ScaleControl with useRef

--- x-pack/platform/plugins/shared/maps/public/connected_components/mb_map/scale_control/scale_control.tsx
+++ x-pack/platform/plugins/shared/maps/public/connected_components/mb_map/scale_control/scale_control.tsx
@@ -6,7 +6,7 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { UseEuiTheme } from '@elastic/eui';
 import type { Map as MapboxMap } from '@kbn/mapbox-gl';
 import { css } from '@emotion/react';
@@ -22,9 +22,11 @@ interface Props {
 const getScaleDistance = (value: number) => {
   const orderOfMagnitude = Math.floor(Math.log10(value));
   const pow10 = Math.pow(10, orderOfMagnitude);
+  // reduce value to single order of magnitude to making rounding simple regardless of order of magnitude
   const distance = value / pow10;
 
   if (distance < 1) return pow10 * (Math.round(distance * 10) / 10);
+  // provide easy to multiple round numbers for scale distance so its easy to measure distances longer then the scale
   if (distance >= 10) return 10 * pow10;
   if (distance >= 5) return 5 * pow10;
   if (distance >= 3) return 3 * pow10;
@@ -58,44 +60,46 @@ export const ScaleControl: React.FC<Props> = ({ isFullScreen, mbMap }) => {
   const [label, setLabel] = useState('');
   const [width, setWidth] = useState(0);
   const styles = useMemoizedStyles(componentStyles);
+  const mbMapRef = useRef(mbMap);
+  const onUpdate = () => {
+    const map = mbMapRef.current;
+    if (!map) return;
+    const container = map.getContainer();
+    const centerHeight = container.clientHeight / 2;
+    const leftLatLon = map.unproject([0, centerHeight]);
+    const rightLatLon = map.unproject([MAX_WIDTH, centerHeight]);
+    const maxDistanceMeters = leftLatLon.distanceTo(rightLatLon);
+    const isKm = maxDistanceMeters >= 1000;
+    const unit = i18n.translate(isKm ? 'xpack.maps.kilometersAbbr' : 'xpack.maps.metersAbbr', {
+      defaultMessage: isKm ? 'km' : 'm',
+    });
+    const maxDistance = isKm ? maxDistanceMeters / 1000 : maxDistanceMeters;
+    const scaleDistance = getScaleDistance(maxDistance);
+
+    const zoom = map.getZoom();
+    const bounds = map.getBounds();
+    let nextLabel = `${scaleDistance} ${unit}`;
+    if (
+      zoom <= 4 ||
+      (zoom <= 6 && (bounds.getNorth() > 23.5 || bounds.getSouth() < -23.5)) ||
+      (zoom <= 8 && (bounds.getNorth() > 45 || bounds.getSouth() < -45))
+    ) {
+      nextLabel = `~${nextLabel}`;
+    }
+
+    setLabel(nextLabel);
+    setWidth(MAX_WIDTH * (scaleDistance / maxDistance));
+  };
 
   useEffect(() => {
-    const onUpdate = () => {
-      const container = mbMap.getContainer();
-      const centerHeight = container.clientHeight / 2;
-      const leftLatLon = mbMap.unproject([0, centerHeight]);
-      const rightLatLon = mbMap.unproject([MAX_WIDTH, centerHeight]);
-      const maxDistanceMeters = leftLatLon.distanceTo(rightLatLon);
-
-      const isKm = maxDistanceMeters >= 1000;
-      const unit = i18n.translate(isKm ? 'xpack.maps.kilometersAbbr' : 'xpack.maps.metersAbbr', {
-        defaultMessage: isKm ? 'km' : 'm',
-      });
-      const maxDistance = isKm ? maxDistanceMeters / 1000 : maxDistanceMeters;
-      const scaleDistance = getScaleDistance(maxDistance);
-
-      const zoom = mbMap.getZoom();
-      const bounds = mbMap.getBounds();
-      let nextLabel = `${scaleDistance} ${unit}`;
-      if (
-        zoom <= 4 ||
-        (zoom <= 6 && (bounds.getNorth() > 23.5 || bounds.getSouth() < -23.5)) ||
-        (zoom <= 8 && (bounds.getNorth() > 45 || bounds.getSouth() < -45))
-      ) {
-        nextLabel = `~${nextLabel}`;
-      }
-
-      setLabel(nextLabel);
-      setWidth(MAX_WIDTH * (scaleDistance / maxDistance));
-    };
-
-    mbMap.on('move', onUpdate);
+    const map = mbMapRef.current;
+    if (!map) return;
+    map.on('move', onUpdate);
     onUpdate();
-
     return () => {
-      mbMap.off('move', onUpdate);
+      map.off('move', onUpdate);
     };
-  }, [mbMap]);
+  }, []);
 
   return (
     <div
