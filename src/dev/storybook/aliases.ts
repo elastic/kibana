e0080640e334afeb15257055a09e0481a43a6fd9
Nathan L Smith <nathan.smith@elastic.co>
2025-05-22 01:39:22 -0500

    Storybook for unified doc viewer

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -64,6 +64,7 @@ export const storybookAliases = {
   threat_intelligence: 'x-pack/solutions/security/plugins/threat_intelligence/.storybook',
   triggers_actions_ui: 'x-pack/platform/plugins/shared/triggers_actions_ui/.storybook',
   ui_actions_enhanced: 'src/platform/plugins/shared/ui_actions_enhanced/.storybook',
+  unified_doc_viewer: 'src/platform/plugins/shared/unified_doc_viewer/.storybook',
   unified_search: 'src/platform/plugins/shared/unified_search/.storybook',
   unified_tabs: 'src/platform/packages/shared/kbn-unified-tabs/.storybook',
   upgrade_assistant: 'x-pack/platform/packages/private/upgrade-assistant/.storybook',
---
+++ src/platform/plugins/shared/unified_doc_viewer/.storybook/main.js
@@ -0,0 +1,1 @@
+module.exports = require('@kbn/storybook').defaultConfig;
