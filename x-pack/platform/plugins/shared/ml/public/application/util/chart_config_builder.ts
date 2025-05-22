9ae1b30a90d1ed831b14f14574aefb8cfe93ea08
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-05-22 12:04:49 +0200

    fix imports: common utils

--- x-pack/platform/plugins/shared/ml/public/application/util/chart_config_builder.ts
+++ x-pack/platform/plugins/shared/ml/public/application/util/chart_config_builder.ts
@@ -15,11 +15,9 @@ import { get } from 'lodash';
 import { ES_AGGREGATION } from '@kbn/ml-anomaly-utils/es_aggregation';
 import { ML_JOB_AGGREGATION } from '@kbn/ml-anomaly-utils/aggregation_types';
 import { DOC_COUNT, _DOC_COUNT } from '@kbn/ml-anomaly-utils/field_types';
-
 import type { SeriesConfig } from '@kbn/ml-common-types/results';
 import type { Job } from '@kbn/ml-common-types/anomaly_detection_jobs/job';
-
-import { mlFunctionToESAggregation } from '../../../common/util/job_utils';
+import { mlFunctionToESAggregation } from '@kbn/ml-common-utils/job_utils';
 
 /**
  * Builds the basic configuration to plot a chart of the source data
