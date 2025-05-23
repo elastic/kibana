fe56b3bcce268b284723c15960ad6cf342c72d75
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-05-23 15:11:04 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/fleet/server/services/agent_policies/package_policies_to_agent_inputs.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/agent_policies/package_policies_to_agent_inputs.ts
@@ -123,10 +123,8 @@ export const getFullInputStreams = (
             .filter((stream) => stream.enabled || allStreamEnabled)
             .map((stream) => {
               const streamId = stream.id;
-              const {
-                data_stream : compiledDataStream,
-                ...compiledStream
-              } = stream.compiled_stream ?? {};
+              const { data_stream: compiledDataStream, ...compiledStream } =
+                stream.compiled_stream ?? {};
               const fullStream: FullAgentPolicyInputStream = {
                 id: streamId,
                 data_stream: {
