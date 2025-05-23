9197e5e5b3d763b9af69f72cbc432762dbbbad57
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-05-23 17:38:55 +0200

    create skeleton for main component

---
+++ src/platform/packages/shared/shared-ux/esql_data_pooler/impl/src/components/data_pooler/data_pooler.stories.tsx
@@ -0,0 +1,41 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React from 'react';
+import { StoryObj, Meta } from '@storybook/react';
+import { DataPoolerProvider } from '../../lib';
+import { ESQLDataPooler } from '..';
+
+/**
+ * @description story for dropdown component which allows selecting options based of provided ES|QL query',
+ */
+export default {
+  title: 'Data Pooler/Data Pooler',
+} satisfies Meta;
+
+export const GridImplementation: StoryObj<{ query: string }> = {
+  render: (args) => {
+    return (
+      <DataPoolerProvider query={args.query}>
+        <ESQLDataPooler />
+      </DataPoolerProvider>
+    );
+  },
+  argTypes: {
+    query: {
+      name: 'ES|QL Editor Query',
+      type: 'string' as const,
+      description: 'Simulation of The ES|QL query that the user provided into the esql editor',
+    },
+  },
+  args: {
+    query:
+      'FROM kibana_sample_data_ecommerce | STATS count = COUNT(*) by customer_full_name, customer_birth_date , customer_first_name ',
+  },
+};
