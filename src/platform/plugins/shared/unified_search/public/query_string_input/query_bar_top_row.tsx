b0e97286a64fabccc0dcc3ed0df7ac9a187a7bdf
Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
2025-05-23 15:43:20 +0200

    Update src/platform/plugins/shared/unified_search/public/query_string_input/query_bar_top_row.tsx

--- src/platform/plugins/shared/unified_search/public/query_string_input/query_bar_top_row.tsx
+++ src/platform/plugins/shared/unified_search/public/query_string_input/query_bar_top_row.tsx
@@ -712,7 +712,7 @@ export const QueryBarTopRow = React.memo(
       return (
         <EuiFlexItem
           grow={!shouldShowDatePickerAsBadge()}
-          css={{ minWidth: shouldShowDatePickerAsBadge() ? 'auto' : 320, maxWidth: '100%' }}
+          style={{ minWidth: shouldShowDatePickerAsBadge() ? 'auto' : 320, maxWidth: '100%' }}
         >
           <EuiFlexGroup gutterSize="s" responsive={false}>
             {filterButtonGroup}
