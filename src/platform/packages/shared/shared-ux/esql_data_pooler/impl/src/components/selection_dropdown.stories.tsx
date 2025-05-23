74a1f9783a694db6454c9749aec15e6de943f56b
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-05-23 15:06:29 +0200

    create query aware group selection button

---
+++ src/platform/packages/shared/shared-ux/esql_data_pooler/impl/src/components/selection_dropdown.stories.tsx
@@ -0,0 +1,70 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React, { type ComponentProps } from 'react';
+import { StoryObj, Meta } from '@storybook/react';
+import { AbstractStorybookMock } from '@kbn/shared-ux-storybook-mock';
+import { DataPoolerProvider } from '../lib/store';
+import { SelectionDropdown } from './selection_dropdown';
+
+/**
+ * @description story for dropdown component which allows selecting options based of provided ES|QL query',
+ */
+export default {
+  title: 'Data Pooler/Selection Dropdown',
+} satisfies Meta;
+
+type SelectionDropdownProps = ComponentProps<typeof SelectionDropdown>;
+type SelectionDropdownServiceArguments = Record<string, unknown>;
+type Arguments = SelectionDropdownProps & SelectionDropdownServiceArguments;
+
+class SelectionDropdownStorybookMock extends AbstractStorybookMock<
+  SelectionDropdownProps,
+  SelectionDropdownServiceArguments,
+  SelectionDropdownProps,
+  SelectionDropdownServiceArguments
+> {
+  propArguments = {
+    query: {
+      name: 'ES|QL Editor Query',
+      control: 'text',
+      description: 'Simulation of The ES|QL query that the user provided into the esql editor',
+    },
+  };
+  serviceArguments = {};
+  dependencies = [];
+
+  getProps(params?: Arguments): SelectionDropdownProps {
+    return {
+      ...this.getArgumentValue('props', params),
+    };
+  }
+
+  getServices(params?: Arguments): SelectionDropdownServiceArguments {
+    return {};
+  }
+}
+
+const selectionDropdownStorybookMock = new SelectionDropdownStorybookMock();
+const argTypes = selectionDropdownStorybookMock.getArgumentTypes();
+
+export const ValidQueryScenario: StoryObj = {
+  render: (args) => {
+    return (
+      <DataPoolerProvider query={args.query}>
+        <SelectionDropdown />
+      </DataPoolerProvider>
+    );
+  },
+  argTypes,
+  args: {
+    query:
+      'FROM kibana_sample_data_ecommerce | STATS count = COUNT(*) by customer_full_name, customer_birth_date , customer_first_name ',
+  },
+};
