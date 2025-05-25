fae2ccddec4c7e7f02e33fcaf3f329369b0211b4
alex prozorov <alex.prozorov@elastic.co>
2025-05-25 17:43:02 +0300

    fix lint errors

--- x-pack/solutions/security/packages/kbn-cloud-security-posture/public/index.ts
+++ x-pack/solutions/security/packages/kbn-cloud-security-posture/public/index.ts
@@ -15,7 +15,7 @@ export { CspEvaluationBadge } from './src/components/csp_evaluation_badge';
 export { getSeverityStatusColor, getCvsScoreColor } from './src/utils/get_vulnerability_colors';
 export { getSeverityText } from './src/utils/get_vulnerability_text';
 export { getVulnerabilityStats, hasVulnerabilitiesData } from './src/utils/vulnerability_helpers';
-export { CVSScoreBadge, SeverityStatusBadge,  } from './src/components/vulnerability_badges';
+export { CVSScoreBadge, SeverityStatusBadge } from './src/components/vulnerability_badges';
 export { getNormalizedSeverity } from './src/utils/get_normalized_severity';
 export { ActionableBadge, type MultiValueCellAction } from './src/components/actionable_badge';
 export { MultiValueCellPopover } from './src/components/multi_value_cell_popover';
