9a3ad587fa1331b8e367362f13e4d88bfb278edd
José Luis González <joseluisgj@gmail.com>
2025-05-26 15:38:15 +0200

    Tour component management

--- x-pack/solutions/search/plugins/search_query_rules/public/components/query_ruleset_detail/query_ruleset_detail.tsx
+++ x-pack/solutions/search/plugins/search_query_rules/public/components/query_ruleset_detail/query_ruleset_detail.tsx
@@ -5,10 +5,18 @@
  * 2.0.
  */
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import { KibanaPageTemplate } from '@kbn/shared-ux-page-kibana-template';
-import { EuiButton, EuiButtonEmpty, EuiFlexGroup, EuiFlexItem, EuiIcon } from '@elastic/eui';
+import {
+  EuiButton,
+  EuiButtonEmpty,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiText,
+  EuiTourStep,
+} from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { useParams } from 'react-router-dom';
 import { QueryRulesQueryRule } from '@elastic/elasticsearch/lib/api/types';
@@ -38,11 +46,29 @@ export const QueryRulesetDetail: React.FC = () => {
 
   const [rules, setRules] = useState<QueryRulesQueryRule[]>(queryRulesetData?.rules ?? []);
 
+  const TOUR_TRY_IN_CONSOLE_STORAGE_KEY = 'queryRules.tour.tryInConsole';
+
+  const [isOpenTour, setIsOpenTour] = useState(() => {
+    const storedPreference = localStorage.getItem(TOUR_TRY_IN_CONSOLE_STORAGE_KEY);
+    return storedPreference === null ? true : storedPreference !== 'false';
+  });
+
+  const tourTriggerRef = useRef<HTMLElement | null>(null);
+
   useEffect(() => {
     if (queryRulesetData?.rules) {
       setRules(queryRulesetData.rules);
+      const hasRules = queryRulesetData.rules.length > 0;
+      if (!hasRules) {
+        setIsOpenTour(false);
+      }
     }
   }, [queryRulesetData?.rules]);
+
+  const handleCloseTour = () => {
+    localStorage.setItem(TOUR_TRY_IN_CONSOLE_STORAGE_KEY, 'false');
+    setIsOpenTour(false);
+  };
 
   return (
     <QueryRulesPageTemplate>
@@ -73,6 +99,7 @@ export const QueryRulesetDetail: React.FC = () => {
             <EuiFlexGroup alignItems="center" key="queryRulesetDetailHeaderButtons">
               <EuiFlexItem grow={false}>
                 <EuiButtonEmpty
+                  buttonRef={tourTriggerRef as React.RefObject<HTMLButtonElement>}
                   iconType="database"
                   color="primary"
                   data-test-subj="queryRulesetDetailHeaderDataButton"
@@ -85,6 +112,23 @@ export const QueryRulesetDetail: React.FC = () => {
                     defaultMessage="Data"
                   />
                 </EuiButtonEmpty>
+
+                <EuiTourStep
+                  anchor={() => tourTriggerRef.current || document.body}
+                  content={
+                    <EuiText>
+                      <p>Now you can try out the query rule results in the console</p>
+                    </EuiText>
+                  }
+                  isStepOpen={isOpenTour}
+                  minWidth={300}
+                  onFinish={handleCloseTour}
+                  step={1}
+                  stepsTotal={1}
+                  title="Test in Console"
+                  anchorPosition="rightCenter"
+                  zIndex={1}
+                />
               </EuiFlexItem>
               <EuiFlexItem grow={false}>
                 <EuiButton
