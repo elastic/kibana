9559b2f2db3ba436247e2e072ef81f93617fd6b2
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-05-26 10:35:05 +0000

    [CI] Auto-commit changed files from 'yarn openapi:generate'

--- x-pack/solutions/security/plugins/security_solution/common/api/quickstart_client.gen.ts
+++ x-pack/solutions/security/plugins/security_solution/common/api/quickstart_client.gen.ts
@@ -380,6 +380,7 @@ import type {
   GetRuleMigrationRequestParamsInput,
   GetRuleMigrationResponse,
   GetRuleMigrationIntegrationsResponse,
+  GetRuleMigrationIntegrationsStatsResponse,
   GetRuleMigrationPrebuiltRulesRequestParamsInput,
   GetRuleMigrationPrebuiltRulesResponse,
   GetRuleMigrationPrivilegesResponse,
@@ -1553,6 +1554,21 @@ finalize it.
     return this.kbnClient
       .request<GetRuleMigrationIntegrationsResponse>({
         path: '/internal/siem_migrations/rules/integrations',
+        headers: {
+          [ELASTIC_HTTP_VERSION_HEADER]: '1',
+        },
+        method: 'GET',
+      })
+      .catch(catchAxiosErrorFormatAndThrow);
+  }
+  /**
+   * Retrieves the stats of all the integrations for all the rule migrations, including the number of rules associated with the integration
+   */
+  async getRuleMigrationIntegrationsStats() {
+    this.log.info(`${new Date().toISOString()} Calling API GetRuleMigrationIntegrationsStats`);
+    return this.kbnClient
+      .request<GetRuleMigrationIntegrationsStatsResponse>({
+        path: '/internal/siem_migrations/rules/integrations/stats',
         headers: {
           [ELASTIC_HTTP_VERSION_HEADER]: '1',
         },
