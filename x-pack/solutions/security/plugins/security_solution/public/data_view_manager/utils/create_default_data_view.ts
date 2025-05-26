2f1f5d0b7a693b44d70c7b7f56c3e89defdfa82f
lgestc <lukasz.gmys@elastic.co>
2025-05-26 12:18:40 +0200

    --wip-- [skip ci]

--- x-pack/solutions/security/plugins/security_solution/public/data_view_manager/utils/create_default_data_view.ts
+++ x-pack/solutions/security/plugins/security_solution/public/data_view_manager/utils/create_default_data_view.ts
@@ -6,8 +6,8 @@
  */
 
 import type { CoreStart } from '@kbn/core/public';
-import type { DataViewsServicePublic } from '@kbn/data-views-plugin/public';
 import type { SpacesPluginStart } from '@kbn/spaces-plugin/public';
+import type { DataViewsServicePublic } from '@kbn/data-views-plugin/public/types';
 import type { KibanaDataView, SourcererModel } from '../../sourcerer/store/model';
 import { initDataView } from '../../sourcerer/store/model';
 import { createSourcererDataView } from '../../sourcerer/containers/create_sourcerer_data_view';
