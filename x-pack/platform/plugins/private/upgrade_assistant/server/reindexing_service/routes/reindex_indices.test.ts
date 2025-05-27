f1973368f994eca54faaf00c9eb78b01dd30db61
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-05-27 14:05:38 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/private/upgrade_assistant/server/reindexing_service/routes/reindex_indices.test.ts
+++ x-pack/platform/plugins/private/upgrade_assistant/server/reindexing_service/routes/reindex_indices.test.ts
@@ -9,11 +9,7 @@ import { kibanaResponseFactory } from '@kbn/core/server';
 import { loggingSystemMock } from '@kbn/core/server/mocks';
 import { licensingMock } from '@kbn/licensing-plugin/server/mocks';
 import { securityMock } from '@kbn/security-plugin/server/mocks';
-import {
-  createMockRouter,
-  MockRouter,
-  routeHandlerContextMock,
-} from '../__mocks__/routes.mock';
+import { createMockRouter, MockRouter, routeHandlerContextMock } from '../__mocks__/routes.mock';
 import { createRequestMock } from '../__mocks__/request.mock';
 import { handleEsError } from '../../shared_imports';
 import { errors as esErrors } from '@elastic/elasticsearch';
