ffcc279a9e60d5694d5ef0caab7b6b6ee019def1
Arturo Lidueña <arturo.liduena@elastic.co>
2025-05-29 00:13:25 +0200

    add registerGetConnectorInfoFunction

--- x-pack/platform/plugins/shared/observability_ai_assistant/server/functions/execute_connector.ts
+++ x-pack/platform/plugins/shared/observability_ai_assistant/server/functions/execute_connector.ts
@@ -5,6 +5,16 @@
  * 2.0.
  */
 
+import {
+  EmailParamsSchema,
+  SlackParamsSchema,
+  JiraParamsSchema,
+  PagerdutyParamsSchema,
+  SlackApiParamsSchema,
+  WebhookParamsSchema,
+} from '@kbn/stack-connectors-plugin/server';
+import { CompatibleJSONSchema } from '../../common/functions/types';
+import { convertSchemaToOpenApi } from './convert_schema_to_open_api';
 import { FunctionRegistrationParameters } from '.';
 import { FunctionVisibility } from '../../common';
 
@@ -40,6 +50,49 @@ export function registerExecuteConnectorFunction({
       ).getActionsClientWithRequest(resources.request);
       const content = await actionsClient.execute({ actionId: id, params });
       return { content };
+    }
+  );
+}
+
+export const connectorParamsSchemas: Record<string, CompatibleJSONSchema> = {
+  '.slack': convertSchemaToOpenApi(SlackParamsSchema),
+  '.slack_api': convertSchemaToOpenApi(SlackApiParamsSchema),
+  '.email': convertSchemaToOpenApi(EmailParamsSchema),
+  '.webhook': convertSchemaToOpenApi(WebhookParamsSchema),
+  '.jira': convertSchemaToOpenApi(JiraParamsSchema),
+  '.pagerduty': convertSchemaToOpenApi(PagerdutyParamsSchema),
+};
+
+export const GET_CONNECTOR_INFO_FUNCTION_NAME = 'get_connector_info';
+
+export function registerGetConnectorInfoFunction({
+  functions,
+  resources,
+}: FunctionRegistrationParameters) {
+  functions.registerFunction(
+    {
+      name: GET_CONNECTOR_INFO_FUNCTION_NAME,
+      description:
+        'Get information about a connectors. Returns the schema of the connector parameters.',
+      visibility: FunctionVisibility.AssistantOnly,
+    },
+    async () => {
+      const actionsClient = await (
+        await resources.plugins.actions.start()
+      ).getActionsClientWithRequest(resources.request);
+      const connectorsList = await actionsClient.getAll().then((connectors) => {
+        return connectors.map((connector) => {
+          if (connector.actionTypeId in connectorParamsSchemas) {
+            return {
+              ...connector,
+              parameters: connectorParamsSchemas[connector.actionTypeId],
+            };
+          }
+
+          return connector;
+        });
+      });
+      return { content: connectorsList };
     }
   );
 }
