4820b4464efb1bd0020e8e76c12a26514a1b192b
miriam.aparicio <miriam.aparicio@gmail.com>
2025-05-28 09:10:13 +0100

    changes in storybook_test.sh

--- .buildkite/scripts/steps/storybooks/storybook_tests.sh
+++ .buildkite/scripts/steps/storybooks/storybook_tests.sh
@@ -8,27 +8,8 @@ echo "Starting Storybook server..."
 yarn storybook apm & 
 STORYBOOK_PID=$!
 
-cleanup() {
-  echo "Killing Storybook server..."
-  kill "$STORYBOOK_PID"
-}
-trap cleanup EXIT
-
 echo "Waiting for Storybook to be ready..."
-TIMEOUT=60
-RETRY_INTERVAL=2
-SECONDS_WAITED=0
-
-until curl --silent --fail "$STORYBOOK_URL" > /dev/null; do
-  if [ "$SECONDS_WAITED" -ge "$TIMEOUT" ]; then
-    echo "Timed out waiting for Storybook to start"
-    exit 1
-  fi
-  sleep "$RETRY_INTERVAL"
-  SECONDS_WAITED=$((SECONDS_WAITED + RETRY_INTERVAL))
-done
-
-echo "Storybook is ready."
+sleep 60
 
 echo "Running Storybook tests..."
 yarn test:storybook --url "$STORYBOOK_URL"
