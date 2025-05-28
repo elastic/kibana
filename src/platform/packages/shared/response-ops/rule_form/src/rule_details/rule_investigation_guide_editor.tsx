a7dbeb2fb7e4ff5372f11b82604908923d6929a3
Justin Kambic <jk@elastic.co>
2025-05-28 10:50:55 -0400

    Further isolate in search of CI failure.

--- src/platform/packages/shared/response-ops/rule_form/src/rule_details/rule_investigation_guide_editor.tsx
+++ src/platform/packages/shared/response-ops/rule_form/src/rule_details/rule_investigation_guide_editor.tsx
@@ -7,11 +7,11 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { EuiMarkdownAstNode, EuiMarkdownEditor, EuiMarkdownParseError } from '@elastic/eui';
-import { css } from '@emotion/react';
+import { EuiMarkdownEditor } from '@elastic/eui';
+// import { css } from '@emotion/react';
 import { i18n } from '@kbn/i18n';
-import React, { useCallback } from 'react';
-import { MAX_ARTIFACTS_INVESTIGATION_GUIDE_LENGTH } from '../constants';
+import React from 'react';
+// import { MAX_ARTIFACTS_INVESTIGATION_GUIDE_LENGTH } from '../constants';
 
 interface Props {
   setRuleParams: (v: { investigation_guide: { blob: string } }) => void;
@@ -19,41 +19,44 @@ interface Props {
 }
 
 export function InvestigationGuideEditor({ setRuleParams, value }: Props) {
-  const [errorMessages, setErrorMessages] = React.useState<string[]>([]);
-  const onParse = useCallback(
-    (_: EuiMarkdownParseError | null, { ast }: { ast: EuiMarkdownAstNode }) => {
-      const length = ast.position?.end.offset ?? 0;
-      if (length > MAX_ARTIFACTS_INVESTIGATION_GUIDE_LENGTH) {
-        setErrorMessages([
-          i18n.translate('responseOpsRuleForm.investigationGuide.editor.errorMessage', {
-            defaultMessage:
-              'The Investigation Guide is too long. Please shorten it.\nCurrent length: {length}.\nMax length: {maxLength}.',
-            values: { length, maxLength: MAX_ARTIFACTS_INVESTIGATION_GUIDE_LENGTH },
-          }),
-        ]);
-      } else if (errorMessages.length) {
-        setErrorMessages([]);
-      }
-    },
-    [errorMessages]
-  );
+  // const [errorMessages, setErrorMessages] = React.useState<string[]>([]);
+  // const onParse = useCallback(
+  //   (_: EuiMarkdownParseError | null, { ast }: { ast: EuiMarkdownAstNode }) => {
+  //     const length = ast.position?.end.offset ?? 0;
+  //     if (length > MAX_ARTIFACTS_INVESTIGATION_GUIDE_LENGTH) {
+  //       setErrorMessages([
+  //         i18n.translate('responseOpsRuleForm.investigationGuide.editor.errorMessage', {
+  //           defaultMessage:
+  //             'The Investigation Guide is too long. Please shorten it.\nCurrent length: {length}.\nMax length: {maxLength}.',
+  //           values: { length, maxLength: MAX_ARTIFACTS_INVESTIGATION_GUIDE_LENGTH },
+  //         }),
+  //       ]);
+  //     } else if (errorMessages.length) {
+  //       setErrorMessages([]);
+  //     }
+  //   },
+  //   [errorMessages]
+  // );
   return (
     <EuiMarkdownEditor
-      aria-label={i18n.translate('responseOpsRuleForm.investigationGuide.editor.ariaLabel', {
-        defaultMessage: 'Add guidelines for addressing alerts created by this rule',
-      })}
-      placeholder={i18n.translate('responseOpsRuleForm.investigationGuide.editor.placeholder', {
-        defaultMessage: 'Add guidelines for addressing alerts created by this rule',
-      })}
-      css={css`
-        .euiMarkdownFormat {
-          word-wrap: break-word;
+      aria-label={i18n.translate(
+        'responseOpsRuleForm.ruleDetails.investigationGuide.editor.ariaLabel',
+        {
+          defaultMessage: 'Add guidelines for addressing alerts created by this rule',
         }
-      `}
+      )}
+      // placeholder={i18n.translate('responseOpsRuleForm.ruleDetails.investigationGuide.editor.placeholder', {
+      //   defaultMessage: 'Add guidelines for addressing alerts created by this rule',
+      // })}
+      // css={css`
+      //   .euiMarkdownFormat {
+      //     word-wrap: break-word;
+      //   }
+      // `}
       value={value}
       onChange={(blob) => setRuleParams({ investigation_guide: { blob } })}
-      onParse={onParse}
-      errors={errorMessages}
+      // onParse={onParse}
+      // errors={errorMessages}
       height={200}
       data-test-subj="investigationGuideEditor"
       initialViewMode="editing"
