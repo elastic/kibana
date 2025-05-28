8eb2fd21af224a1f624e9c0889c300697736e719
Sergi Massaneda <sergi.massaneda@elastic.co>
2025-05-28 15:27:59 +0200

    [Security Solution] Integrations card in automatic migrations (#221493)
    ## Summary
    
    issue: https://github.com/elastic/security-team/issues/12309
    
    Introduces the integrations card in the SIEM migrations topic, after the
    "start rule migration" card.
    
    Requirements:
    
    - The same integrations card UI as the rest of the onboarding topics.
    - When no migration is complete, show the same recommended integrations
    as the default topic.
    - When some migration is complete, show the integrations detected by the
    rule migrations (all of them).
    - Render a label next to the title showing the number of migration rules
    linked to the integration (both prebuilt and custom rules)
      - Sort the integrations list by the number of rules linked.
    - As the last integration item in the list, render a card to link the
    integration auto-import
    
    ### Screenshots
    
    When no migration has been executed yet:
    
    ![without
    migration](https://github.com/user-attachments/assets/de1ccd87-37b0-43fd-b52e-a044712520b0)
    
    When some migrations have been completed:
    
    ![with
    migration](https://github.com/user-attachments/assets/0bc520cd-ce35-4299-a242-bbc89a5be061)
    
    Auto import card is appended at the end:
    
    ![auto-import
    card](https://github.com/user-attachments/assets/577172f9-d348-4764-9f4e-f769a4c4a03b)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit b5915de827ecb1656e242c4e0bc4317b12d546cf)
    
    # Conflicts:
    #	x-pack/solutions/security/plugins/security_solution/public/common/lib/integrations/hooks/use_integration_card_list.ts

--- x-pack/solutions/security/plugins/security_solution/public/common/lib/integrations/hooks/use_integration_card_list.ts
+++ x-pack/solutions/security/plugins/security_solution/public/common/lib/integrations/hooks/use_integration_card_list.ts
@@ -4,159 +4,88 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import type { IntegrationCardItem } from '@kbn/fleet-plugin/public';
-import { SECURITY_UI_APP_ID } from '@kbn/security-solution-navigation';
 import type { GetInstalledPackagesResponse } from '@kbn/fleet-plugin/common/types';
 import { useNavigation } from '../../kibana';
-import { APP_INTEGRATIONS_PATH, APP_UI_ID, ONBOARDING_PATH } from '../../../../../common/constants';
+import { APP_INTEGRATIONS_PATH, ONBOARDING_PATH } from '../../../../../common/constants';
 
 import {
   CARD_DESCRIPTION_LINE_CLAMP,
   CARD_TITLE_LINE_CLAMP,
   INTEGRATION_APP_ID,
   MAX_CARD_HEIGHT_IN_PX,
-  RETURN_APP_ID,
-  RETURN_PATH,
   TELEMETRY_INTEGRATION_CARD,
 } from '../constants';
-import type { GetAppUrl, NavigateTo } from '../../kibana';
-import type { ReportLinkClick } from './integration_context';
 import { useIntegrationContext } from './integration_context';
+import { getIntegrationLinkState } from '../../../hooks/integrations/use_integration_link_state';
+import { addPathParamToUrl } from '../../../utils/integrations';
+import { useSelectedTab } from './use_selected_tab';
 
-const addPathParamToUrl = (url: string, onboardingLink: string) => {
-  const encoded = encodeURIComponent(onboardingLink);
-  const paramsString = `${RETURN_PATH}=${encoded}&${RETURN_APP_ID}=${APP_UI_ID}`;
+export type GetCardItemExtraProps = (card: IntegrationCardItem) => Partial<IntegrationCardItem>;
 
-  if (url.indexOf('?') >= 0) {
-    return `${url}&${paramsString}`;
-  }
-  return `${url}?${paramsString}`;
-};
+const useAddSecurityProps = (activeIntegrations: GetInstalledPackagesResponse['items']) => {
+  const { navigateTo, getAppUrl } = useNavigation();
+  const { telemetry } = useIntegrationContext();
 
-const extractFeaturedCards = (filteredCards: IntegrationCardItem[], featuredCardIds: string[]) => {
-  return filteredCards.reduce<IntegrationCardItem[]>((acc, card) => {
-    if (featuredCardIds.includes(card.id)) {
-      acc.push(card);
-    }
-    return acc;
-  }, []);
-};
+  return useCallback(
+    (card: IntegrationCardItem): IntegrationCardItem => {
+      const integrationRootUrl = getAppUrl({ appId: INTEGRATION_APP_ID });
+      const state = getIntegrationLinkState(ONBOARDING_PATH, getAppUrl);
+      const url = card.url.includes(APP_INTEGRATIONS_PATH)
+        ? addPathParamToUrl(card.url, ONBOARDING_PATH)
+        : card.url;
+      const isActive = activeIntegrations.some((integration) => integration.name === card.name);
 
-const getFilteredCards = ({
-  activeIntegrations,
-  featuredCardIds,
-  getAppUrl,
-  integrationsList,
-  navigateTo,
-  reportLinkClick,
-}: {
-  activeIntegrations: GetInstalledPackagesResponse['items'];
-  featuredCardIds?: string[];
-  getAppUrl: GetAppUrl;
-  integrationsList: IntegrationCardItem[];
-  navigateTo: NavigateTo;
-  reportLinkClick?: ReportLinkClick;
-}) => {
-  const securityIntegrationsList = integrationsList.map((card) =>
-    addSecuritySpecificProps({
-      activeIntegrations,
-      navigateTo,
-      getAppUrl,
-      card,
-      reportLinkClick,
-    })
-  );
-  if (!featuredCardIds) {
-    return { featuredCards: [], integrationCards: securityIntegrationsList };
-  }
-  const featuredCards = extractFeaturedCards(securityIntegrationsList, featuredCardIds);
-  return {
-    featuredCards,
-    integrationCards: securityIntegrationsList,
-  };
-};
-
-export const addSecuritySpecificProps = ({
-  activeIntegrations,
-  navigateTo,
-  getAppUrl,
-  card,
-  reportLinkClick,
-}: {
-  activeIntegrations: GetInstalledPackagesResponse['items'];
-  navigateTo: NavigateTo;
-  getAppUrl: GetAppUrl;
-  card: IntegrationCardItem;
-  reportLinkClick?: ReportLinkClick;
-}): IntegrationCardItem => {
-  const onboardingLink = getAppUrl({ appId: SECURITY_UI_APP_ID, path: ONBOARDING_PATH });
-  const integrationRootUrl = getAppUrl({ appId: INTEGRATION_APP_ID });
-  const state = {
-    onCancelNavigateTo: [APP_UI_ID, { path: ONBOARDING_PATH }],
-    onCancelUrl: onboardingLink,
-    onSaveNavigateTo: [APP_UI_ID, { path: ONBOARDING_PATH }],
-  };
-  const url =
-    card.url.indexOf(APP_INTEGRATIONS_PATH) >= 0 && onboardingLink
-      ? addPathParamToUrl(card.url, ONBOARDING_PATH)
-      : card.url;
-  const isActive = activeIntegrations.some((integration) => integration.name === card.name);
-  return {
-    ...card,
-    titleLineClamp: CARD_TITLE_LINE_CLAMP,
-    descriptionLineClamp: CARD_DESCRIPTION_LINE_CLAMP,
-    maxCardHeight: MAX_CARD_HEIGHT_IN_PX,
-    showInstallationStatus: true,
-    url,
-    hasDataStreams: isActive,
-    onCardClick: () => {
-      const trackId = `${TELEMETRY_INTEGRATION_CARD}_${card.id}`;
-      reportLinkClick?.(trackId);
-      if (url.startsWith(APP_INTEGRATIONS_PATH)) {
-        navigateTo({
-          appId: INTEGRATION_APP_ID,
-          path: url.slice(integrationRootUrl.length),
-          state,
-        });
-      } else if (url.startsWith('http') || url.startsWith('https')) {
-        window.open(url, '_blank');
-      } else {
-        navigateTo({ url, state });
-      }
+      return {
+        ...card,
+        titleLineClamp: CARD_TITLE_LINE_CLAMP,
+        descriptionLineClamp: CARD_DESCRIPTION_LINE_CLAMP,
+        maxCardHeight: MAX_CARD_HEIGHT_IN_PX,
+        showInstallationStatus: true,
+        url,
+        hasDataStreams: isActive,
+        onCardClick: () => {
+          const trackId = `${TELEMETRY_INTEGRATION_CARD}_${card.id}`;
+          telemetry.reportLinkClick?.(trackId);
+          if (url.startsWith(APP_INTEGRATIONS_PATH)) {
+            navigateTo({
+              appId: INTEGRATION_APP_ID,
+              path: url.slice(integrationRootUrl.length),
+              state,
+            });
+          } else if (url.startsWith('http') || url.startsWith('https')) {
+            window.open(url, '_blank');
+          } else {
+            navigateTo({ url, state });
+          }
+        },
+      };
     },
-  };
+    [activeIntegrations, navigateTo, getAppUrl, telemetry]
+  );
 };
 
+interface UseIntegrationCardListProps {
+  integrationsList: IntegrationCardItem[];
+  activeIntegrations: GetInstalledPackagesResponse['items'];
+}
 export const useIntegrationCardList = ({
   integrationsList,
   activeIntegrations,
-  featuredCardIds,
-}: {
-  integrationsList: IntegrationCardItem[];
-  activeIntegrations: GetInstalledPackagesResponse['items'];
-  featuredCardIds?: string[] | undefined;
-}): IntegrationCardItem[] => {
-  const { navigateTo, getAppUrl } = useNavigation();
+}: UseIntegrationCardListProps): IntegrationCardItem[] => {
+  const { selectedTab } = useSelectedTab();
+  const featuredCardIds = selectedTab?.featuredCardIds;
 
-  const {
-    telemetry: { reportLinkClick },
-  } = useIntegrationContext();
-  const { featuredCards, integrationCards } = useMemo(
-    () =>
-      getFilteredCards({
-        activeIntegrations,
-        navigateTo,
-        getAppUrl,
-        integrationsList,
-        featuredCardIds,
-        reportLinkClick,
-      }),
-    [activeIntegrations, navigateTo, getAppUrl, integrationsList, featuredCardIds, reportLinkClick]
+  const addSecurityProps = useAddSecurityProps(activeIntegrations);
+
+  const integrationCards = useMemo(
+    () => integrationsList.map((card) => addSecurityProps(card)),
+    [integrationsList, addSecurityProps]
   );
 
   if (featuredCardIds && featuredCardIds.length > 0) {
-    return featuredCards;
+    return integrationCards.filter((card) => featuredCardIds.includes(card.id));
   }
   return integrationCards ?? [];
 };
