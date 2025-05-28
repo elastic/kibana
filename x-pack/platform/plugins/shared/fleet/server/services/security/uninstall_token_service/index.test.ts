80bf8f769b53747dd531626778f328d54ce19080
Paul Tavares <paul.tavares@elastic.co>
2025-05-28 08:11:33 -0400

    Fix tests for Uninstall token service

--- x-pack/platform/plugins/shared/fleet/server/services/security/uninstall_token_service/index.test.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/security/uninstall_token_service/index.test.ts
@@ -290,7 +290,7 @@ describe('UninstallTokenService', () => {
           expect(getAgentPoliciesByIDsMock).toHaveBeenCalledWith(
             soClientMock,
             [so.attributes.policy_id],
-            { ignoreMissing: true }
+            { ignoreMissing: true, spaceId: '*' }
           );
         });
 
@@ -326,7 +326,7 @@ describe('UninstallTokenService', () => {
           expect(getAgentPoliciesByIDsMock).toHaveBeenCalledWith(
             soClientMock,
             [so.attributes.policy_id],
-            { ignoreMissing: true }
+            { ignoreMissing: true, spaceId: '*' }
           );
         });
 
@@ -362,7 +362,7 @@ describe('UninstallTokenService', () => {
           expect(getAgentPoliciesByIDsMock).toHaveBeenCalledWith(
             soClientMock,
             [so.attributes.policy_id],
-            { ignoreMissing: true }
+            { ignoreMissing: true, spaceId: '*' }
           );
         });
 
