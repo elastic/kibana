a717d40d5772ec820e5c45f2648f0c581465cc1a
Justin Kambic <jk@elastic.co>
2025-05-29 15:31:08 -0400

    Fix import.

--- src/platform/packages/shared/response-ops/rule_form/src/rule_type_modal/components/index.tsx
+++ src/platform/packages/shared/response-ops/rule_form/src/rule_type_modal/components/index.tsx
@@ -11,7 +11,7 @@ import { countBy } from 'lodash';
 import React, { useMemo, useState } from 'react';
 import type { HttpStart } from '@kbn/core-http-browser';
 import type { ToastsStart } from '@kbn/core-notifications-browser';
-import { useLoadRuleTypesQuery } from '@kbn/alerts-ui-shared';
+import { type RuleTypeModel, useLoadRuleTypesQuery } from '@kbn/alerts-ui-shared';
 import { type RuleTypeModal, type RuleTypeModalProps } from './rule_type_modal';
 import { filterAndCountRuleTypes } from './helpers/filter_and_count_rule_types';
 
