4bcf0ae5a994b91f58ef04591d7641a2ba2ef306
Nick Peihl <nick.peihl@elastic.co>
2025-05-29 17:11:17 -0400

    [Dashboards as Code] Register example book saved object and content management (#221981)
    Note: _This PR does not need to be reviewed by external teams. This PR
    merges into a feature branch that Kibana presentation team is working on
    to support embeddable integrations with Dashboards as Code._

--- examples/embeddable_examples/server/book/content_management/latest.ts
+++ examples/embeddable_examples/server/book/content_management/latest.ts
@@ -7,15 +7,4 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { VersionableEmbeddableObject } from '@kbn/embeddable-plugin/common';
-import type { SavedBookAttributes } from '../../../../server/types';
-import type { BookAttributes } from '../../../../server/book/content_management/v1';
-import { itemToSavedObject, savedObjectToItem } from './transforms';
-
-export const bookAttributesDefinition: VersionableEmbeddableObject<
-  SavedBookAttributes,
-  BookAttributes
-> = {
-  itemToSavedObject,
-  savedObjectToItem,
-};
+export * from './v1';
