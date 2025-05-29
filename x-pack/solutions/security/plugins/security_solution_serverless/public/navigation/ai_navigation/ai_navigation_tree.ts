2d809a5f313caf355aeb85625b929b363df9608c
Sergi Massaneda <sergi.massaneda@elastic.co>
2025-05-29 19:24:01 +0200

    intermediate state

--- x-pack/solutions/security/plugins/security_solution_serverless/public/navigation/ai_navigation/ai_navigation_tree.ts
+++ x-pack/solutions/security/plugins/security_solution_serverless/public/navigation/ai_navigation/ai_navigation_tree.ts
@@ -9,12 +9,10 @@ import type { NavigationTreeDefinition } from '@kbn/core-chrome-browser';
 import { i18n } from '@kbn/i18n';
 
 import { SecurityPageName } from '@kbn/security-solution-navigation';
-import { defaultNavigationTree } from '@kbn/security-solution-navigation/navigation_tree';
 import { i18nStrings, securityLink } from '@kbn/security-solution-navigation/links';
 
 import { AiNavigationIcon } from './icon';
 import { createStackManagementNavigationTree } from '../stack_management_navigation';
-import { renderAiSocCallout } from './callout';
 
 const SOLUTION_NAME = i18n.translate(
   'xpack.securitySolutionServerless.aiNavigation.projectType.title',
@@ -36,37 +34,8 @@ export const createAiNavigationTree = (): NavigationTreeDefinition => ({
           breadcrumbStatus: 'hidden',
           children: [
             {
-              id: SecurityPageName.alertSummary,
-              link: securityLink(SecurityPageName.alertSummary),
-            },
-            {
               id: SecurityPageName.attackDiscovery,
               link: securityLink(SecurityPageName.attackDiscovery),
-            },
-          ],
-        },
-        {
-          breadcrumbStatus: 'hidden',
-          children: [
-            defaultNavigationTree.cases(),
-            {
-              id: SecurityPageName.configurations,
-              link: securityLink(SecurityPageName.configurations),
-              renderAs: 'item',
-              children: [
-                {
-                  id: SecurityPageName.configurationsIntegrations,
-                  link: securityLink(SecurityPageName.configurationsIntegrations),
-                },
-                {
-                  id: SecurityPageName.configurationsBasicRules,
-                  link: securityLink(SecurityPageName.configurationsBasicRules),
-                },
-                {
-                  id: SecurityPageName.configurationsAiSettings,
-                  link: securityLink(SecurityPageName.configurationsAiSettings),
-                },
-              ],
             },
           ],
         },
@@ -77,19 +46,6 @@ export const createAiNavigationTree = (): NavigationTreeDefinition => ({
               link: 'discover',
             },
           ],
-        },
-      ],
-    },
-  ],
-  callout: [
-    {
-      type: 'navGroup',
-      id: 'calloutGroup',
-      defaultIsCollapsed: false,
-      isCollapsible: false,
-      children: [
-        {
-          renderItem: renderAiSocCallout,
         },
       ],
     },
