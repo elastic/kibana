a97cf14a1402aaa1b55f5eb2194002603f7dd223
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-05-30 13:16:14 -0500

    Remove unneeded flow call

--- src/platform/plugins/shared/dashboard/server/dashboard_saved_object/migrations/dashboard_saved_object_migrations.ts
+++ src/platform/plugins/shared/dashboard/server/dashboard_saved_object/migrations/dashboard_saved_object_migrations.ts
@@ -15,6 +15,7 @@ import { mergeSavedObjectMigrationMaps } from '@kbn/core-saved-objects-utils-ser
 import { EmbeddableSetup, EmbeddableStart } from '@kbn/embeddable-plugin/server';
 import { SavedObjectMigration, SavedObjectMigrationMap } from '@kbn/core-saved-objects-server';
 
+import { CoreSetup } from '@kbn/core/server';
 import { migrations730, migrations700 } from './migrate_to_730';
 import { migrateMatchAllQuery } from './migrate_match_all_query';
 import { migrateExplicitlyHiddenTitles } from './migrate_hidden_titles';
@@ -40,7 +41,7 @@ export const createDashboardSavedObjectTypeMigrations = (
     '7.0.0': flow(migrations700),
     '7.3.0': flow(migrations730),
     '7.9.3': flow(migrateMatchAllQuery),
-    '7.11.0': flow(createExtractPanelReferencesMigration(deps)),
+    '7.11.0': createExtractPanelReferencesMigration(deps),
     '7.14.0': flow(replaceIndexPatternReference),
     '7.17.3': flow(migrateExplicitlyHiddenTitles),
   };
