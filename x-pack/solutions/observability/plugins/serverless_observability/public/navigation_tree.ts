8e32312d295cb122307dc37ac48ec61c62702755
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-05-30 21:04:26 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/solutions/observability/plugins/serverless_observability/public/navigation_tree.ts
+++ x-pack/solutions/observability/plugins/serverless_observability/public/navigation_tree.ts
@@ -140,7 +140,7 @@ export const createNavigationTree = ({
                     breadcrumbStatus: 'hidden',
                   },
                 ],
-              }
+              },
             ],
           },
           {
