096d6e9b5aa6b1bc88991010f1a9d26bc3c9a1fe
Francesco Fagnani <francesco.fagnani@elastic.co>
2025-05-30 16:30:21 +0200

    fix(prevent errors): label of a private location is editable only if monitors are all in one space
    the user needs to update the label from that space

--- x-pack/solutions/observability/plugins/synthetics/server/routes/settings/private_locations/edit_private_location.ts
+++ x-pack/solutions/observability/plugins/synthetics/server/routes/settings/private_locations/edit_private_location.ts
@@ -8,6 +8,7 @@
 import { TypeOf, schema } from '@kbn/config-schema';
 import { SavedObjectsErrorHelpers } from '@kbn/core/server';
 import { ALL_SPACES_ID } from '@kbn/spaces-plugin/common/constants';
+import { i18n } from '@kbn/i18n';
 import { getPrivateLocations } from '../../../synthetics_service/get_private_locations';
 import { PrivateLocationAttributes } from '../../../runtime_types/private_locations';
 import { PrivateLocationRepository } from '../../../repositories/private_location_repository';
@@ -50,7 +51,7 @@ export const editPrivateLocationRoute: SyntheticsRestApiRouteFactory<
   },
   requiredPrivileges: [PRIVATE_LOCATION_WRITE_API],
   handler: async (routeContext) => {
-    const { response, request, savedObjectsClient } = routeContext;
+    const { response, request, savedObjectsClient, spaceId } = routeContext;
     const { locationId } = request.params;
     const newLocationLabel = request.body.label;
 
@@ -68,7 +69,24 @@ export const editPrivateLocationRoute: SyntheticsRestApiRouteFactory<
         }),
       ]);
 
-      // TODO: add check if user has access to all spaces
+      const monitorsSpaces = monitorsInLocation.map(({ attributes: { namespace } }) => namespace);
+
+      // TODO: this will be changed with a check to know if the user is part of all the spaces the monitors are part of
+      // Right now it's not possible to update monitors of other spaces so we're allowing changing the label only if all the monitors are part
+      // of the space the user is currently logged in
+      if (
+        monitorsSpaces.length &&
+        ((monitorsSpaces.length === 1 && monitorsSpaces[0] !== spaceId) ||
+          monitorsSpaces.length > 1)
+      ) {
+        return response.conflict({
+          body: {
+            message: i18n.translate('xpack.synthetics.editPrivateLocation.monitorsInOtherSpaces', {
+              defaultMessage: 'Monitors in other spaces cannot be updated',
+            }),
+          },
+        });
+      }
 
       const newLocation = await repo.editPrivateLocation(locationId, {
         label: newLocationLabel,
