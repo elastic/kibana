ca4903e47b13bb05994c143eb063cf9f8d6454f4
Justin Kambic <jk@elastic.co>
2025-05-29 23:50:25 -0400

    Fix an import.

--- src/platform/packages/shared/response-ops/rule_form/src/rule_type_modal/components/index.tsx
+++ src/platform/packages/shared/response-ops/rule_form/src/rule_type_modal/components/index.tsx
@@ -12,7 +12,7 @@ import React, { useMemo, useState } from 'react';
 import type { HttpStart } from '@kbn/core-http-browser';
 import type { ToastsStart } from '@kbn/core-notifications-browser';
 import { type RuleTypeModel, useLoadRuleTypesQuery } from '@kbn/alerts-ui-shared';
-import { type RuleTypeModal, type RuleTypeModalProps } from './rule_type_modal';
+import { RuleTypeModal, type RuleTypeModalProps } from './rule_type_modal';
 import { filterAndCountRuleTypes } from './helpers/filter_and_count_rule_types';
 
 export interface RuleTypeModalComponentProps {
