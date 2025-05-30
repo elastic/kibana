b8ae59a9873a1f0fed860dd8ea66ea00d4bff19e
Nick Partridge <nicholas.partridge@elastic.co>
2025-05-30 12:26:52 -0700

    chore: fix type errors

--- src/platform/plugins/shared/kibana_react/public/dark_mode/use_dark_mode.test.tsx
+++ src/platform/plugins/shared/kibana_react/public/dark_mode/use_dark_mode.test.tsx
@@ -37,7 +37,7 @@ describe('useDarkMode', () => {
 
   const mock = (): [KibanaServices, BehaviorSubject<CoreTheme>] => {
     const core = coreMock.createStart();
-    const subject = new BehaviorSubject<CoreTheme>({ darkMode: false });
+    const subject = new BehaviorSubject<CoreTheme>({ darkMode: false, name: 'borealis' });
     core.theme.theme$ = subject.asObservable();
 
     return [core, subject];
@@ -73,7 +73,7 @@ describe('useDarkMode', () => {
     expect(div!.textContent).toBe('false');
 
     act(() => {
-      subject.next({ darkMode: true });
+      subject.next({ darkMode: true, name: 'borealis' });
     });
 
     div = container!.querySelector('div');
