39773bb81f8e160518ee2b0b3d8a13c27c85bf2a
Matthew Kime <matt@mattki.me>
2025-06-01 15:51:02 -0500

    type fixes

--- x-pack/platform/plugins/private/upgrade_assistant/server/routes/register_routes.ts
+++ x-pack/platform/plugins/private/upgrade_assistant/server/routes/register_routes.ts
@@ -21,18 +21,14 @@ import { registerNodeDiskSpaceRoute } from './node_disk_space';
 import { registerClusterSettingsRoute } from './cluster_settings';
 import { registerMigrateDataStreamRoutes } from './migrate_data_streams';
 import { registerUpdateIndexRoute } from './update_index';
-import { ReindexingService } from '../reindexing_service';
 
-export function registerRoutes(
-  dependencies: RouteDependencies,
-  reindexingService: ReindexingService
-) {
+export function registerRoutes(dependencies: RouteDependencies) {
   registerAppRoutes(dependencies);
 
   registerCloudBackupStatusRoutes(dependencies);
   registerClusterUpgradeStatusRoutes(dependencies);
   registerSystemIndicesMigrationRoutes(dependencies);
-  registerESDeprecationRoutes(dependencies, reindexingService);
+  registerESDeprecationRoutes(dependencies);
   registerDeprecationLoggingRoutes(dependencies);
   registerUpdateSettingsRoute(dependencies);
   registerMlSnapshotRoutes(dependencies);
