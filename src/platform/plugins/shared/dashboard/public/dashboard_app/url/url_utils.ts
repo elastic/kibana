c8d5b53382c3f4efca302dbe851ed0c44e67868b
Nathan Reese <reese.nathan@gmail.com>
2025-06-02 10:32:01 -0600

    move panels too old check to extract panels

--- src/platform/plugins/shared/dashboard/public/dashboard_app/url/url_utils.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_app/url/url_utils.ts
@@ -11,39 +11,10 @@ import { replaceUrlHashQuery } from '@kbn/kibana-utils-plugin/common';
 import { IKbnUrlStateStorage } from '@kbn/kibana-utils-plugin/public';
 import { History } from 'history';
 import { skip } from 'rxjs';
-import semverSatisfies from 'semver/functions/satisfies';
 import type { DashboardState } from '../../../common/types';
 import { DashboardApi } from '../../dashboard_api/types';
-import { coreServices } from '../../services/kibana_services';
 import { DASHBOARD_STATE_STORAGE_KEY, createDashboardEditUrl } from '../../utils/urls';
-import { getPanelTooOldErrorString } from '../_dashboard_app_strings';
 import { extractDashboardState } from './bwc/extract_dashboard_state';
-
-/**
- * We no longer support loading panels from a version older than 7.3 in the URL.
- * @returns whether or not there is a panel in the URL state saved with a version before 7.3
- */
-export const isPanelVersionTooOld = (panels: unknown[]) => {
-  for (const panel of panels) {
-    if (!panel || typeof panel !== 'object') {
-      continue;
-    }
-
-    const panelAsObject = panel as { [key: string]: unknown };
-
-    if ('panels' in panel) {
-      continue; // ignore sections
-    }
-
-    if (
-      !panelAsObject.gridData ||
-      !(panelAsObject.panelConfig || panelAsObject.embeddableConfig) ||
-      (panelAsObject.version && semverSatisfies(panelAsObject.version as string, '<7.3'))
-    )
-      return true;
-  }
-  return false;
-};
 
 /**
  * Loads any dashboard state from the URL, and removes the state from the URL.
@@ -61,16 +32,6 @@ export const loadAndRemoveDashboardState = (
     return hashQuery;
   });
   kbnUrlStateStorage.kbnUrlControls.update(nextUrl, true);
-
-  const panels =
-    typeof rawAppStateInUrl === 'object' &&
-    'panels' in rawAppStateInUrl &&
-    Array.isArray(rawAppStateInUrl.panels)
-      ? rawAppStateInUrl.panels
-      : [];
-  if (isPanelVersionTooOld(panels)) {
-    coreServices.notifications.toasts.addWarning(getPanelTooOldErrorString());
-  }
 
   return extractDashboardState(rawAppStateInUrl);
 };
