4d8dcbe4aec7cac69b2c4a24cd441b09ab45e987
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-06-02 06:28:07 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/stack_connectors/public/connector_types/thehive/template_component/index.tsx
+++ x-pack/platform/plugins/shared/stack_connectors/public/connector_types/thehive/template_component/index.tsx
@@ -52,127 +52,139 @@ const templates: Template[] = [
   },
 ];
 
-const ToolTipContent = React.memo(({ description, label }: { description: string; label: string }) => {
-  const { euiTheme } = useEuiTheme();
-  return (
-    <>
-      <EuiText
-        size="s"
-        style={{
-          fontWeight: euiTheme.font.weight.bold,
-        }}
-      >
-        {label}
-      </EuiText>
-      <EuiSpacer size="s" />
-      <hr />
-      <EuiSpacer size="s" />
-      <EuiText size="xs">{description}</EuiText>
-    </>
-  );
-});
-
-const RenderOption = React.memo(({ option, searchValue }: { option: EuiSelectableOption<{ description?: string }>; searchValue: string }) => {
-  const { euiTheme } = useEuiTheme();
-  return (
-    <EuiFlexGroup data-test-subj={`templateMenuButton-${option.label}`}>
-      <EuiFlexItem>
+const ToolTipContent = React.memo(
+  ({ description, label }: { description: string; label: string }) => {
+    const { euiTheme } = useEuiTheme();
+    return (
+      <>
         <EuiText
           size="s"
           style={{
             fontWeight: euiTheme.font.weight.bold,
           }}
         >
-          <EuiHighlight search={searchValue}>{option.label}</EuiHighlight>
+          {label}
         </EuiText>
-        <EuiSpacer size="xs" />
-        {option.description && (
-          <>
-            <EuiToolTip
-              display="block"
-              position="top"
-              content={<ToolTipContent description={option.description} label={option.label} />}
-              data-test-subj={`${option.label}-tooltip`}
-            >
-              <TruncatedText text={option.description || ''} />
-            </EuiToolTip>
-          </>
-        )}
-      </EuiFlexItem>
-    </EuiFlexGroup>
-  );
-});
+        <EuiSpacer size="s" />
+        <hr />
+        <EuiSpacer size="s" />
+        <EuiText size="xs">{description}</EuiText>
+      </>
+    );
+  }
+);
 
-export const TemplateOptions = React.memo(({ buttonTitle, paramsProperty, onSelectEventHandler }: Props) => {
-  const [isTemplatesPopoverOpen, setIsTemplatesPopoverOpen] = useState<boolean>(false);
+const RenderOption = React.memo(
+  ({
+    option,
+    searchValue,
+  }: {
+    option: EuiSelectableOption<{ description?: string }>;
+    searchValue: string;
+  }) => {
+    const { euiTheme } = useEuiTheme();
+    return (
+      <EuiFlexGroup data-test-subj={`templateMenuButton-${option.label}`}>
+        <EuiFlexItem>
+          <EuiText
+            size="s"
+            style={{
+              fontWeight: euiTheme.font.weight.bold,
+            }}
+          >
+            <EuiHighlight search={searchValue}>{option.label}</EuiHighlight>
+          </EuiText>
+          <EuiSpacer size="xs" />
+          {option.description && (
+            <>
+              <EuiToolTip
+                display="block"
+                position="top"
+                content={<ToolTipContent description={option.description} label={option.label} />}
+                data-test-subj={`${option.label}-tooltip`}
+              >
+                <TruncatedText text={option.description || ''} />
+              </EuiToolTip>
+            </>
+          )}
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    );
+  }
+);
 
-  const templatesObject: Record<string, Template> = {};
-  templates?.forEach((template) => {
-    templatesObject[template.name] = template;
-  });
+export const TemplateOptions = React.memo(
+  ({ buttonTitle, paramsProperty, onSelectEventHandler }: Props) => {
+    const [isTemplatesPopoverOpen, setIsTemplatesPopoverOpen] = useState<boolean>(false);
 
-  const optionsToShow = templates?.map((template) => ({
-    label: template.name,
-    data: {
-      description: template.description,
-    },
-    'data-test-subj': `${template.name}-selectableOption`,
-  }));
+    const templatesObject: Record<string, Template> = {};
+    templates?.forEach((template) => {
+      templatesObject[template.name] = template;
+    });
 
-  const Button = useMemo(
-    () => (
-      <EuiButtonIcon
-        id={`${paramsProperty}AddVariableButton`}
-        data-test-subj={`${paramsProperty}TemplateSelectButton`}
-        title={buttonTitle}
-        onClick={() => setIsTemplatesPopoverOpen(!isTemplatesPopoverOpen)}
-        iconType="documents"
-        aria-label={buttonTitle}
-      />
-    ),
-    [buttonTitle, isTemplatesPopoverOpen, paramsProperty]
-  );
+    const optionsToShow = templates?.map((template) => ({
+      label: template.name,
+      data: {
+        description: template.description,
+      },
+      'data-test-subj': `${template.name}-selectableOption`,
+    }));
 
-  const renderOption = useCallback(
-    (option: EuiSelectableOption<{ description?: string }>, searchValue: string) => (
-      <RenderOption option={option} searchValue={searchValue} />
-    ),
-    []
-  );
+    const Button = useMemo(
+      () => (
+        <EuiButtonIcon
+          id={`${paramsProperty}AddVariableButton`}
+          data-test-subj={`${paramsProperty}TemplateSelectButton`}
+          title={buttonTitle}
+          onClick={() => setIsTemplatesPopoverOpen(!isTemplatesPopoverOpen)}
+          iconType="documents"
+          aria-label={buttonTitle}
+        />
+      ),
+      [buttonTitle, isTemplatesPopoverOpen, paramsProperty]
+    );
 
-  return (
-    <EuiPopover
-      button={Button}
-      isOpen={isTemplatesPopoverOpen}
-      closePopover={() => setIsTemplatesPopoverOpen(false)}
-      panelPaddingSize="s"
-      anchorPosition="upLeft"
-      panelStyle={{ minWidth: 350 }}
-    >
-      <EuiSelectable
-        height={300}
-        data-test-subj={`${paramsProperty}TemplateSelectableList`}
-        isLoading={false}
-        options={optionsToShow}
-        listProps={{
-          rowHeight: 70,
-          showIcons: false,
-          paddingSize: 'none',
-          textWrap: 'wrap',
-        }}
-        renderOption={renderOption}
-        onChange={(templateList) => {
-          templateList.map((template) => {
-            if (template.checked === 'on' && templatesObject) {
-              onSelectEventHandler(templatesObject[template.label]);
-            }
-          });
-          setIsTemplatesPopoverOpen(false);
-        }}
-        singleSelection
+    const renderOption = useCallback(
+      (option: EuiSelectableOption<{ description?: string }>, searchValue: string) => (
+        <RenderOption option={option} searchValue={searchValue} />
+      ),
+      []
+    );
+
+    return (
+      <EuiPopover
+        button={Button}
+        isOpen={isTemplatesPopoverOpen}
+        closePopover={() => setIsTemplatesPopoverOpen(false)}
+        panelPaddingSize="s"
+        anchorPosition="upLeft"
+        panelStyle={{ minWidth: 350 }}
       >
-        {(list) => <>{list}</>}
-      </EuiSelectable>
-    </EuiPopover>
-  );
-});
+        <EuiSelectable
+          height={300}
+          data-test-subj={`${paramsProperty}TemplateSelectableList`}
+          isLoading={false}
+          options={optionsToShow}
+          listProps={{
+            rowHeight: 70,
+            showIcons: false,
+            paddingSize: 'none',
+            textWrap: 'wrap',
+          }}
+          renderOption={renderOption}
+          onChange={(templateList) => {
+            templateList.map((template) => {
+              if (template.checked === 'on' && templatesObject) {
+                onSelectEventHandler(templatesObject[template.label]);
+              }
+            });
+            setIsTemplatesPopoverOpen(false);
+          }}
+          singleSelection
+        >
+          {(list) => <>{list}</>}
+        </EuiSelectable>
+      </EuiPopover>
+    );
+  }
+);
