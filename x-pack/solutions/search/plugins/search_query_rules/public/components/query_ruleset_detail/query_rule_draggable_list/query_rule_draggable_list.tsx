41cd311558225fd29c21ad9982add7f38fc53125
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2025-06-02 15:28:16 +0200

    [Query Rules]Query rule flyout metadata editor (#220760)
    ## Summary
    
    Adds a metadata editor to Query Rule Flyout. Document selector and
    validations are going to be added as a followup PR.
    
    
    
    https://github.com/user-attachments/assets/dcbb790a-855b-4ae1-bdca-206ce948c624
    
    
    
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/solutions/search/plugins/search_query_rules/public/components/query_ruleset_detail/query_rule_draggable_list/query_rule_draggable_list.tsx
+++ x-pack/solutions/search/plugins/search_query_rules/public/components/query_ruleset_detail/query_rule_draggable_list/query_rule_draggable_list.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import {
   EuiDragDropContext,
@@ -19,19 +19,185 @@ import {
   EuiButtonIcon,
   useEuiTheme,
   euiDragDropReorder,
+  EuiPopover,
+  EuiContextMenuPanel,
+  EuiContextMenuItem,
   EuiNotificationBadge,
 } from '@elastic/eui';
 import { QueryRulesQueryRule } from '@elastic/elasticsearch/lib/api/types';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { i18n } from '@kbn/i18n';
+import { SearchQueryRulesQueryRule } from '../../../../common/types';
 import { DroppableContainer } from '../styles';
 import { QueryRuleDraggableListHeader } from './query_rule_draggable_list_header';
 import { QueryRuleDraggableListItemActionTypeBadge } from './query_rule_draggable_item_action_type_badge';
 import { QueryRuleDraggableItemCriteriaDisplay } from './query_rule_draggable_item_criteria_display';
 
+export interface QueryRuleDraggableListItemProps {
+  queryRule: QueryRulesQueryRule;
+  index: number;
+  onEditRuleFlyoutOpen: (ruleId: string) => void;
+  isLastItem?: boolean;
+  tourInfo?: {
+    title: string;
+    content: string;
+    tourTargetRef?: React.RefObject<HTMLDivElement>;
+  };
+}
+
+export const QueryRuleDraggableListItem: React.FC<QueryRuleDraggableListItemProps> = ({
+  index,
+  onEditRuleFlyoutOpen,
+  queryRule,
+  tourInfo,
+  isLastItem = false,
+}) => {
+  const [isPopoverOpen, setIsPopoverOpen] = React.useState(false);
+  const localTourTargetRef = useRef<HTMLDivElement>(null);
+  const effectiveRef = tourInfo?.tourTargetRef || localTourTargetRef;
+  const closePopover = useCallback(() => {
+    setIsPopoverOpen(false);
+  }, []);
+  const openPopover = useCallback(() => {
+    setIsPopoverOpen(true);
+  }, []);
+  return (
+    <>
+      <EuiDraggable
+        spacing="m"
+        key={queryRule.rule_id}
+        index={index}
+        draggableId={queryRule.rule_id}
+        customDragHandle={true}
+        hasInteractiveChildren={true}
+        data-test-subj="searchQueryRulesDraggableItem"
+      >
+        {(provided) => (
+          <EuiPanel paddingSize="s" hasShadow={false}>
+            <EuiFlexGroup alignItems="center" gutterSize="m">
+              <EuiFlexItem grow={false} ref={isLastItem ? effectiveRef : undefined}>
+                <EuiPanel
+                  color="transparent"
+                  paddingSize="s"
+                  {...provided.dragHandleProps}
+                  aria-label="Drag Handle"
+                >
+                  <EuiIcon type="grab" />
+                </EuiPanel>
+              </EuiFlexItem>
+              <EuiFlexItem grow={true}>
+                <EuiFlexGroup responsive={false} alignItems="center">
+                  <EuiFlexItem grow={false}>
+                    <EuiNotificationBadge color="subdued">{index + 1}</EuiNotificationBadge>
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={7}>
+                    <EuiFlexGroup direction="column" gutterSize="xs" responsive={false}>
+                      {Array.isArray(queryRule.criteria) ? (
+                        <>
+                          {queryRule.criteria.slice(0, 3).map((criteria, criteriaIndex) => (
+                            <EuiFlexItem key={criteria.type + criteriaIndex} grow={false}>
+                              <QueryRuleDraggableItemCriteriaDisplay criteria={criteria} />
+                            </EuiFlexItem>
+                          ))}
+                          {queryRule.criteria.length > 3 && (
+                            <EuiFlexItem grow={false}>
+                              <EuiText size="s">
+                                <FormattedMessage
+                                  id="xpack.search.queryRulesetDetail.draggableList.queryRulesCriteriaMoreLabel"
+                                  defaultMessage="+{count} more"
+                                  values={{ count: queryRule.criteria.length - 3 }}
+                                />
+                              </EuiText>
+                            </EuiFlexItem>
+                          )}
+                        </>
+                      ) : (
+                        <EuiFlexItem grow={false}>
+                          <QueryRuleDraggableItemCriteriaDisplay criteria={queryRule.criteria} />
+                        </EuiFlexItem>
+                      )}
+                    </EuiFlexGroup>
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={1}>
+                    <QueryRuleDraggableListItemActionTypeBadge queryRule={queryRule} />
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    <EuiPopover
+                      id="queryRuleActionsPopover"
+                      button={
+                        <EuiButtonIcon
+                          aria-label={i18n.translate(
+                            'xpack.search.queryRulesetDetail.draggableList.actionsButton.aria.label',
+                            {
+                              defaultMessage: 'Click to reach actions menu for rule {ruleId}',
+                              values: {
+                                ruleId: queryRule.rule_id,
+                              },
+                            }
+                          )}
+                          data-test-subj="searchQueryRulesQueryRulesetDetailButton"
+                          iconType="boxesHorizontal"
+                          color="text"
+                          onClick={() => {
+                            openPopover();
+                          }}
+                        />
+                      }
+                      isOpen={isPopoverOpen}
+                      closePopover={closePopover}
+                      panelPaddingSize="none"
+                      anchorPosition="downLeft"
+                    >
+                      <EuiContextMenuPanel
+                        items={[
+                          <EuiContextMenuItem
+                            key="edit"
+                            icon="pencil"
+                            data-test-subj="searchQueryRulesQueryRulesetDetailEditButton"
+                            onClick={() => {
+                              onEditRuleFlyoutOpen(queryRule.rule_id);
+                              closePopover();
+                            }}
+                          >
+                            <FormattedMessage
+                              id="xpack.search.queryRulesetDetail.draggableList.actions.edit"
+                              defaultMessage="Edit"
+                            />
+                          </EuiContextMenuItem>,
+                          <EuiContextMenuItem
+                            key="delete"
+                            icon="trash"
+                            data-test-subj="searchQueryRulesQueryRulesetDetailDeleteButton"
+                            onClick={() => {
+                              // Logic to handle delete action
+                              closePopover();
+                            }}
+                          >
+                            <FormattedMessage
+                              id="xpack.search.queryRulesetDetail.draggableList.actions.delete"
+                              defaultMessage="Delete"
+                            />
+                          </EuiContextMenuItem>,
+                        ]}
+                        className="euiContextMenuPanel--inPopover"
+                        data-test-subj="searchQueryRulesQueryRulesetDetailContextMenu"
+                      />
+                    </EuiPopover>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiPanel>
+        )}
+      </EuiDraggable>
+    </>
+  );
+};
+
 export interface QueryRuleDraggableListProps {
-  rules: QueryRulesQueryRule[];
-  onReorder: (queryRules: QueryRulesQueryRule[]) => void;
+  rules: SearchQueryRulesQueryRule[];
+  onReorder: (queryRules: SearchQueryRulesQueryRule[]) => void;
+  onEditRuleFlyoutOpen: (ruleId: string) => void;
   tourInfo?: {
     title: string;
     content: string;
@@ -41,12 +207,11 @@ export interface QueryRuleDraggableListProps {
 
 export const QueryRuleDraggableList: React.FC<QueryRuleDraggableListProps> = ({
   rules,
+  onEditRuleFlyoutOpen,
   onReorder,
   tourInfo,
 }) => {
   const { euiTheme } = useEuiTheme();
-  const localTourTargetRef = useRef<HTMLDivElement>(null);
-  const effectiveRef = tourInfo?.tourTargetRef || localTourTargetRef;
 
   return (
     <EuiDragDropContext
@@ -66,94 +231,14 @@ export const QueryRuleDraggableList: React.FC<QueryRuleDraggableListProps> = ({
           data-test-subj="searchQueryRulesDroppable"
         >
           {rules.map((queryRule, index) => (
-            <EuiDraggable
-              spacing="m"
+            <QueryRuleDraggableListItem
               key={queryRule.rule_id}
+              queryRule={queryRule}
               index={index}
-              draggableId={queryRule.rule_id}
-              customDragHandle={true}
-              hasInteractiveChildren={true}
-              data-test-subj="searchQueryRulesDraggableItem"
-            >
-              {(provided) => (
-                <EuiPanel paddingSize="s" hasShadow={false}>
-                  <EuiFlexGroup alignItems="center" gutterSize="m">
-                    <EuiFlexItem
-                      grow={false}
-                      ref={index === rules.length - 1 ? effectiveRef : undefined}
-                    >
-                      <EuiPanel
-                        color="transparent"
-                        paddingSize="s"
-                        {...provided.dragHandleProps}
-                        aria-label="Drag Handle"
-                      >
-                        <EuiIcon type="grab" />
-                      </EuiPanel>
-                    </EuiFlexItem>
-                    <EuiFlexItem grow={true}>
-                      <EuiFlexGroup responsive={false} alignItems="center">
-                        <EuiFlexItem grow={false}>
-                          <EuiNotificationBadge color="subdued">{index + 1}</EuiNotificationBadge>
-                        </EuiFlexItem>
-                        <EuiFlexItem grow={7}>
-                          <EuiFlexGroup direction="column" gutterSize="xs" responsive={false}>
-                            {Array.isArray(queryRule.criteria) ? (
-                              <>
-                                {queryRule.criteria.slice(0, 3).map((criteria, criteriaIndex) => (
-                                  <EuiFlexItem key={criteria.type + criteriaIndex} grow={false}>
-                                    <QueryRuleDraggableItemCriteriaDisplay criteria={criteria} />
-                                  </EuiFlexItem>
-                                ))}
-                                {queryRule.criteria.length > 3 && (
-                                  <EuiFlexItem grow={false}>
-                                    <EuiText size="s">
-                                      <FormattedMessage
-                                        id="xpack.search.queryRulesetDetail.draggableList.queryRulesCriteriaMoreLabel"
-                                        defaultMessage="+{count} more"
-                                        values={{ count: queryRule.criteria.length - 3 }}
-                                      />
-                                    </EuiText>
-                                  </EuiFlexItem>
-                                )}
-                              </>
-                            ) : (
-                              <EuiFlexItem grow={false}>
-                                <QueryRuleDraggableItemCriteriaDisplay
-                                  criteria={queryRule.criteria}
-                                />
-                              </EuiFlexItem>
-                            )}
-                          </EuiFlexGroup>
-                        </EuiFlexItem>
-                        <EuiFlexItem grow={1}>
-                          <QueryRuleDraggableListItemActionTypeBadge queryRule={queryRule} />
-                        </EuiFlexItem>
-                        <EuiFlexItem grow={false}>
-                          <EuiButtonIcon
-                            aria-label={i18n.translate(
-                              'xpack.search.queryRulesetDetail.draggableList.actionsButton.aria.label',
-                              {
-                                defaultMessage: 'Click to reach actions menu for rule {ruleId}',
-                                values: {
-                                  ruleId: queryRule.rule_id,
-                                },
-                              }
-                            )}
-                            data-test-subj="searchQueryRulesQueryRulesetDetailButton"
-                            iconType="boxesHorizontal"
-                            color="text"
-                            onClick={() => {
-                              // Logic to open actions menu
-                            }}
-                          />
-                        </EuiFlexItem>
-                      </EuiFlexGroup>
-                    </EuiFlexItem>
-                  </EuiFlexGroup>
-                </EuiPanel>
-              )}
-            </EuiDraggable>
+              data-test-subj={`searchQueryRulesDraggableItem-${queryRule.rule_id}`}
+              onEditRuleFlyoutOpen={onEditRuleFlyoutOpen}
+              isLastItem={index === rules.length - 1}
+            />
           ))}
         </EuiDroppable>
       </>
