614ce911913d82d08c5f3c6ad2e2ce0acd850e8a
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-06-03 13:42:33 +0200

    [Obs Serverless] Enabel Anomalies and Categories]

--- x-pack/solutions/observability/plugins/infra/server/routes/log_analysis/results/log_entry_category_examples.ts
+++ x-pack/solutions/observability/plugins/infra/server/routes/log_analysis/results/log_entry_category_examples.ts
@@ -18,9 +18,6 @@ export const initGetLogEntryCategoryExamplesRoute = ({
   framework,
   getStartServices,
 }: Pick<InfraBackendLibs, 'framework' | 'getStartServices'>) => {
-  if (!framework.config.featureFlags.logsUIEnabled) {
-    return;
-  }
   framework
     .registerVersionedRoute({
       access: 'internal',
