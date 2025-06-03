96b9a7389dc35eb860b846b005ef9e4477d089c3
Bena Kansara <bena.kansara@elastic.co>
2025-06-03 10:46:23 +0200

    make alert title consistent, fix selected tab id

--- x-pack/solutions/observability/plugins/observability/public/components/alerts_flyout/alerts_flyout_header.tsx
+++ x-pack/solutions/observability/plugins/observability/public/components/alerts_flyout/alerts_flyout_header.tsx
@@ -5,9 +5,10 @@
  * 2.0.
  */
 import React, { ComponentProps } from 'react';
-import { ALERT_RULE_NAME } from '@kbn/rule-data-utils';
-import { EuiSpacer, EuiTitle } from '@elastic/eui';
+import { ALERT_RULE_CATEGORY, ALERT_RULE_NAME } from '@kbn/rule-data-utils';
+import { EuiSpacer, EuiText, EuiTitle } from '@elastic/eui';
 import { GetObservabilityAlertsTableProp } from '../alerts_table/types';
+import { getAlertTitle } from '../../utils/format_alert_title';
 
 export type AlertsFlyoutHeaderProps = Pick<
   ComponentProps<GetObservabilityAlertsTableProp<'renderFlyoutHeader'>>,
@@ -19,8 +20,12 @@ export function AlertsFlyoutHeader({ alert }: AlertsFlyoutHeaderProps) {
     <>
       <EuiSpacer size="s" />
       <EuiTitle size="m" data-test-subj="alertsFlyoutTitle">
-        <h2>{alert[ALERT_RULE_NAME]?.[0] as string}</h2>
+        <h2>{getAlertTitle(alert[ALERT_RULE_CATEGORY]?.[0] as string)}</h2>
       </EuiTitle>
+      <EuiSpacer size="s" />
+      <EuiText size="s">
+        <p>{alert[ALERT_RULE_NAME]?.[0] as string}</p>
+      </EuiText>
     </>
   );
 }
