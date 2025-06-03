d88ed089a55e137fe29adb837e2f859e536baf49
drewdaemon <drew.tate@elastic.co>
2025-06-03 11:54:24 -0600

    Add autocomplete test

---
+++ src/platform/packages/shared/kbn-esql-validation-autocomplete/src/autocomplete/__tests__/function_args.test.ts
@@ -0,0 +1,168 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { setTestFunctions } from '../../shared/test_functions';
+import { FunctionDefinitionTypes, Location } from '../../definitions/types';
+import { getFunctionSignaturesByReturnType, setup } from './helpers';
+import { uniq } from 'lodash';
+
+describe('functions arg suggestions', () => {
+  afterEach(() => {
+    setTestFunctions([]);
+  });
+
+  it('suggests based on available signatures', async () => {
+    setTestFunctions([
+      {
+        type: FunctionDefinitionTypes.SCALAR,
+        name: 'func',
+        description: '',
+        signatures: [
+          {
+            params: [
+              {
+                name: 'arg',
+                type: 'integer',
+              },
+            ],
+            returnType: 'double',
+          },
+          {
+            params: [
+              {
+                name: 'arg',
+                type: 'long',
+              },
+            ],
+            returnType: 'double',
+          },
+          {
+            params: [
+              {
+                name: 'arg',
+                type: 'double',
+              },
+              {
+                name: 'arg2',
+                type: 'double',
+              },
+            ],
+            returnType: 'double',
+          },
+          {
+            params: [
+              {
+                name: 'arg',
+                type: 'double',
+              },
+              {
+                name: 'arg2',
+                type: 'long',
+              },
+              {
+                name: 'arg3',
+                type: 'long',
+              },
+            ],
+            returnType: 'double',
+          },
+        ],
+        locationsAvailable: [Location.EVAL],
+      },
+    ]);
+
+    const doubleSuggestions = [
+      'doubleField',
+      'kubernetes.something.something',
+      '`any#Char$Field`',
+      ...getFunctionSignaturesByReturnType(Location.EVAL, ['double'], { scalar: true }).map(
+        ({ text }) => text
+      ),
+    ];
+
+    const integerSuggestions = [
+      'integerField',
+      ...getFunctionSignaturesByReturnType(Location.EVAL, ['integer'], { scalar: true }).map(
+        ({ text }) => text
+      ),
+    ];
+
+    const longSuggestions = [
+      'longField',
+      ...getFunctionSignaturesByReturnType(Location.EVAL, ['long'], { scalar: true }).map(
+        ({ text }) => text
+      ),
+    ];
+
+    const { assertSuggestions } = await setup();
+
+    await assertSuggestions(
+      'FROM index | EVAL FUNC(/)',
+      uniq([...doubleSuggestions, ...integerSuggestions, ...longSuggestions])
+    );
+    await assertSuggestions(
+      'FROM index | EVAL FUNC(doubleField, /)',
+      uniq([...doubleSuggestions, ...longSuggestions])
+    );
+    await assertSuggestions('FROM index | EVAL FUNC(doubleField, doubleField, /)', []);
+    await assertSuggestions('FROM index | EVAL FUNC(doubleField, longField, /)', longSuggestions);
+  });
+
+  it('treats text and keyword as interchangeable', async () => {
+    setTestFunctions([
+      {
+        type: FunctionDefinitionTypes.SCALAR,
+        name: 'accepts_keyword',
+        description: '',
+        signatures: [
+          {
+            params: [
+              {
+                name: 'arg',
+                type: 'text',
+              },
+            ],
+            returnType: 'keyword',
+          },
+        ],
+        locationsAvailable: [Location.EVAL],
+      },
+      {
+        type: FunctionDefinitionTypes.SCALAR,
+        name: 'accepts_text',
+        description: '',
+        signatures: [
+          {
+            params: [
+              {
+                name: 'arg',
+                type: 'text',
+              },
+            ],
+            returnType: 'keyword',
+          },
+        ],
+        locationsAvailable: [Location.EVAL],
+      },
+    ]);
+
+    const expected = [
+      'keywordField',
+      'textField',
+      ...getFunctionSignaturesByReturnType(Location.EVAL, ['keyword', 'text'], {
+        scalar: true,
+      }).map(({ text }) => text),
+    ];
+
+    const { assertSuggestions } = await setup();
+
+    await assertSuggestions('FROM index | EVAL ACCEPTS_KEYWORD(/)', expected);
+    await assertSuggestions('FROM index | EVAL ACCEPTS_TEXT(/)', expected);
+  });
+});
