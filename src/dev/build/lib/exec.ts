c0de375a8277896c46bdaecb2f94c5243123855d
Brad White <Ikuni17@users.noreply.github.com>
2025-06-02 18:49:02 -0600

    [Build] Parallel artifact builds (#217929)
    ## Summary
    
    Closes #218143
    Reverts #39292
    
    This PR parallelizes the artifact outputs and archive creation tasks to
    reduce build runtime by about 49% (104min down to 53min). It required
    buffering the logs as they were all interweaved from each task and were
    not easily parsed by a human. So, the PR also cleans up the logging a
    little bit by utilizing the dropdowns available in Buildkite.
    
    
    ### Testing
    https://buildkite.com/elastic/kibana-artifacts-snapshot/builds/6253
    
    I also tried bumping the machine to `c2-standard-30` which increases
    vCPU from 16 to 30 and Memory from 64GB to 120GB:
    https://buildkite.com/elastic/kibana-artifacts-snapshot/builds/6254
    
    This results in a 13% reduction in run time for the parallel builds but
    is about twice the cost, which isn't worth it.

--- src/dev/build/lib/exec.ts
+++ src/dev/build/lib/exec.ts
@@ -9,24 +9,27 @@
 
 import execa from 'execa';
 import chalk from 'chalk';
+import { fromEvent, merge, map, toArray, takeUntil } from 'rxjs';
 import { ToolingLog, LogLevel } from '@kbn/tooling-log';
 
 import { watchStdioForLine } from './watch_stdio_for_line';
+import { Build } from './build';
 
 interface Options {
   level?: Exclude<LogLevel, 'silent' | 'error'>;
   cwd?: string;
   env?: Record<string, string>;
   exitAfter?: RegExp;
+  build?: Build;
 }
 
 export async function exec(
   log: ToolingLog,
   cmd: string,
   args: string[],
-  { level = 'debug', cwd, env, exitAfter }: Options = {}
+  { level = 'debug', cwd, env, exitAfter, build }: Options = {}
 ) {
-  log[level](chalk.dim('$'), cmd, ...args);
+  const bufferLogs = build && build?.getBufferLogs();
 
   const proc = execa(cmd, args, {
     stdio: ['ignore', 'pipe', 'pipe'],
@@ -35,5 +38,28 @@ export async function exec(
     preferLocal: true,
   });
 
-  await watchStdioForLine(proc, (line) => log[level](line), exitAfter);
+  if (bufferLogs) {
+    const stdout$ = fromEvent<Buffer>(proc.stdout!, 'data').pipe(map((chunk) => chunk.toString()));
+    const stderr$ = fromEvent<Buffer>(proc.stderr!, 'data').pipe(map((chunk) => chunk.toString()));
+    const close$ = fromEvent(proc, 'close');
+
+    await merge(stdout$, stderr$)
+      .pipe(takeUntil(close$), toArray())
+      .toPromise()
+      .then((logs) => {
+        log.write(`--- ${build.getBuildDesc()} [${build.getBuildArch()}]`);
+
+        log.indent(4, () => {
+          log[level](chalk.dim('$'), cmd, ...args);
+
+          if (logs?.length) {
+            logs.forEach((line: string) => log[level](line.trim()));
+          }
+        });
+      });
+  } else {
+    log[level](chalk.dim('$'), cmd, ...args);
+
+    await watchStdioForLine(proc, (line: string) => log[level](line), exitAfter);
+  }
 }
