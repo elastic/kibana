b33252824dff7735c229d710f8897ac6d180b2ed
Luke Gmys <11671118+lgestc@users.noreply.github.com>
2025-06-03 16:51:57 +0200

    [Security Solution] Move threat intelligence to security solution (#214369)
    # Summary
    
    This PR merges Threat Intelligence plugin with security solution, in
    order to remove duplicated and unnecessary code introduced as a bridge
    between the TI plugin and Security Solution + to simpify maintenance. No
    new functionality is implemented here, other than changing imports and
    some dependendies to use security solution code directly, without weird
    bridges or hacks / indirections.
    
    ## Testing
    
    Navigate to threat intelligence plugin / indicators and try clicking
    around. It is hard to list every feature we have there
    but in general it should work without errors.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/dev/storybook/aliases.ts
+++ src/dev/storybook/aliases.ts
@@ -61,7 +61,6 @@ export const storybookAliases = {
   // security_solution_packages: 'x-pack/solutions/security/packages/storybook/config',
   serverless: 'src/platform/packages/shared/serverless/storybook/config',
   shared_ux: 'src/platform/packages/private/shared-ux/storybook/config',
-  threat_intelligence: 'x-pack/solutions/security/plugins/threat_intelligence/.storybook',
   triggers_actions_ui: 'x-pack/platform/plugins/shared/triggers_actions_ui/.storybook',
   ui_actions_enhanced: 'src/platform/plugins/shared/ui_actions_enhanced/.storybook',
   unified_search: 'src/platform/plugins/shared/unified_search/.storybook',
