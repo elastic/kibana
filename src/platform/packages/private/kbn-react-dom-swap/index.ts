96548c8e3de01b32b1135feedf365f76efc90956
Anton Dosov <anton.dosov@elastic.co>
2025-06-03 15:27:36 +0200

    code clean up

--- src/platform/packages/private/kbn-react-dom-swap/index.ts
+++ src/platform/packages/private/kbn-react-dom-swap/index.ts
@@ -7,46 +7,41 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import ReactDOM, { Renderer } from 'react-dom';
-import { createRoot, Root } from 'react-dom/client';
+import { ReactElement } from 'react';
+import ReactDOM from 'react-dom';
+import { createRoot, Root, Container } from 'react-dom/client';
+
+declare global {
+  interface Window {
+    _reactDomSwapEnableConcurrentRoot?: boolean;
+    _reactDomSwapElements?: WeakMap<Container, Root>;
+  }
+}
 
 /**
  * This module is a development-only version of ReactDOM that can be used to render
  * easily swap between legacy and modern React rendering methods.
  */
-const USE_CONCURRENT_ROOT_KEY: string = `_reactDomSwapUseConcurrentRoot`;
-// @ts-ignore
-let isConcurrentRoot = window[USE_CONCURRENT_ROOT_KEY] === true;
-
 export const enableConcurrentReact = () => {
-  isConcurrentRoot = true;
-  // @ts-ignore
-  window[USE_CONCURRENT_ROOT_KEY] = true;
+  window._reactDomSwapEnableConcurrentRoot = true;
 };
 
-// @ts-ignore
-if (!window._elements) {
-  // @ts-ignore
-  window._elements = new WeakMap();
+if (!window._reactDomSwapElements) {
+  window._reactDomSwapElements = new WeakMap();
 }
 
-// @ts-ignore
-const elements: WeakMap<Container, Root> = window._elements;
+const elements = window._reactDomSwapElements;
+const isConcurrentRoot = window._reactDomSwapEnableConcurrentRoot === true;
 
-const render: Renderer = (element: any, container: any, cb: any) => {
+const render = (element: ReactElement, container: Container) => {
   if (isConcurrentRoot) {
     const root = elements.get(container) ?? createRoot(container);
     elements.set(container, root);
     root.render(element);
-    if (cb) {
-      requestIdleCallback(() => {
-        cb();
-      });
-    }
 
     return element;
   } else {
-    return ReactDOM.render(element, container, cb);
+    return ReactDOM.render(element, container);
   }
 };
 
