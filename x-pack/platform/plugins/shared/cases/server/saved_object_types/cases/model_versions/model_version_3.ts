784318e999246544266e9e838252d8311be159ae
Jan Monschke <jan.monschke@elastic.co>
2025-06-03 09:41:34 +0200

    moving to unsigned_long here as well

--- x-pack/platform/plugins/shared/cases/server/saved_object_types/cases/model_versions/model_version_3.ts
+++ x-pack/platform/plugins/shared/cases/server/saved_object_types/cases/model_versions/model_version_3.ts
@@ -17,7 +17,7 @@ export const modelVersion3: SavedObjectsModelVersion = {
       type: 'mappings_addition',
       addedMappings: {
         incremental_id: {
-          type: 'integer',
+          type: 'unsigned_long',
         },
       },
     },
