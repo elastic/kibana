1f11477bbc320a4c8b701f94e05bd45a1f9df64b
Cristina Amico <criamico@users.noreply.github.com>
2025-06-03 10:35:24 +0200

    fix schema

--- x-pack/platform/plugins/shared/fleet/server/services/telemetry/fleet_usages_schema.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/telemetry/fleet_usages_schema.ts
@@ -112,13 +112,6 @@ export const fleetAgentsSchema: RootSchema<any> = {
               description: 'Number of agents enrolled whose output uses the latency preset',
             },
           },
-          sync_integrations: {
-            type: 'boolean',
-            _meta: {
-              description:
-                'Boolean field, indicates if remote sync integrations feature is enabled on the remote elasticsearch output',
-            },
-          },
         },
       },
       output_preset: {
@@ -138,6 +131,13 @@ export const fleetAgentsSchema: RootSchema<any> = {
         type: 'long',
         _meta: {
           description: 'Number of agents enrolled that use this output type as monitoring output',
+        },
+      },
+      sync_integrations: {
+        type: 'boolean',
+        _meta: {
+          description:
+            'Boolean field, indicates if remote sync integrations feature is enabled on the remote elasticsearch output',
         },
       },
     },
