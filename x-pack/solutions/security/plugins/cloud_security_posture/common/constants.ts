bb16e07bfdaab04cc792f613eac67d63f824d3af
Ido Cohen <cohenido42@gmail.com>
2025-06-04 18:35:31 +0300

    fix typo

--- x-pack/solutions/security/plugins/cloud_security_posture/common/constants.ts
+++ x-pack/solutions/security/plugins/cloud_security_posture/common/constants.ts
@@ -41,9 +41,6 @@ export const FINDINGS_INDEX_DEFAULT_NS = 'logs-cloud_security_posture.findings-d
 
 export const LATEST_FINDINGS_INDEX_TEMPLATE_NAME = 'logs-cloud_security_posture.findings_latest';
 
-// export const CDR_LATEST_NATIVE_MISCONFIGURATIONS_INDEX_ALIAS =
-//   'logs-cloud_security_posture.findings_latest-default';
-
 export const BENCHMARK_SCORE_INDEX_TEMPLATE_NAME = 'logs-cloud_security_posture.scores';
 export const BENCHMARK_SCORE_INDEX_PATTERN = 'logs-cloud_security_posture.scores-*';
 export const BENCHMARK_SCORE_INDEX_DEFAULT_NS = 'logs-cloud_security_posture.scores-default';
