9479c2b114db250c742e4711247b48effc642557
Nicholas Peretti <nicholas.peretti@elastic.co>
2025-06-05 14:01:29 +0100

    [Security Solution][Timeline] Fix flyout z-index

--- src/platform/packages/private/kbn-language-documentation/src/components/as_flyout/index.tsx
+++ src/platform/packages/private/kbn-language-documentation/src/components/as_flyout/index.tsx
@@ -81,7 +81,7 @@ function DocumentationFlyout({
           onClose={() => onHelpMenuVisibilityChange(false)}
           aria-labelledby="esqlInlineDocumentationFlyout"
           data-test-subj="esqlInlineDocumentationFlyout"
-          css={{ zIndex: 1 }}
+          css={{ zIndex: 1001 }}
           type="push"
           size={DEFAULT_WIDTH}
           paddingSize="m"
