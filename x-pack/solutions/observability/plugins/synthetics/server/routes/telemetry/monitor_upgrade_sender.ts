d61a5da51aad26f09a43c129fefc0150e78dd97e
Maryam Saeidi <maryam.saeidi@elastic.co>
2025-06-05 13:15:04 +0200

    Rename e to error

--- x-pack/solutions/observability/plugins/synthetics/server/routes/telemetry/monitor_upgrade_sender.ts
+++ x-pack/solutions/observability/plugins/synthetics/server/routes/telemetry/monitor_upgrade_sender.ts
@@ -37,8 +37,8 @@ export function sendTelemetryEvents(
   try {
     eventsTelemetry.queueTelemetryEvents(MONITOR_UPDATE_CHANNEL, [updateEvent]);
     eventsTelemetry.queueTelemetryEvents(MONITOR_CURRENT_CHANNEL, [updateEvent]);
-  } catch (e) {
-    logger.error(`queuing telemetry events failed ${e.message}`, { error: e });
+  } catch (error) {
+    logger.error(`queuing telemetry events failed ${error.message}`, { error });
   }
 }
 
@@ -53,8 +53,8 @@ export function sendErrorTelemetryEvents(
 
   try {
     eventsTelemetry.queueTelemetryEvents(MONITOR_ERROR_EVENTS_CHANNEL, [updateEvent]);
-  } catch (e) {
-    logger.error(`queuing telemetry events failed ${e.message}`, { error: e });
+  } catch (error) {
+    logger.error(`queuing telemetry events failed ${error.message}`, { error });
   }
 }
 
