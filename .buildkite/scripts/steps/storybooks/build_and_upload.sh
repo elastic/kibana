e8c6d4fa13194ae4fcc26eea1cb097881fa536be
Brad White <brad.white@elastic.co>
2025-06-06 15:30:04 -0600

    add download build artifacts

--- .buildkite/scripts/steps/storybooks/build_and_upload.sh
+++ .buildkite/scripts/steps/storybooks/build_and_upload.sh
@@ -3,5 +3,7 @@
 set -euo pipefail
 
 .buildkite/scripts/bootstrap.sh
+# Some Storybooks depend on Kibana React plugin
+.buildkite/scripts/download_build_artifacts.sh
 
 ts-node .buildkite/scripts/steps/storybooks/build_and_upload.ts
