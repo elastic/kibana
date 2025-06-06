1b5be7cc2e96f5ca4c03f215891969e45f062b30
Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>
2025-06-06 14:28:23 +0200

    [Upgrade Assistant] Change resolution column icons to filled (#222764)
    Part of https://github.com/elastic/kibana/issues/222036
    
    ## Summary
    
    Small PR to change the icons in the ES deprecations table resolution
    column to the filled version as required in the mocks. I've also updated
    the icons in the Kibana Deprecations table for consistency.
    
    Before:
    <img width="583" alt="Screenshot 2025-06-05 at 10 26 35"
    src="https://github.com/user-attachments/assets/f22449dd-6859-4459-ba1d-bdfdaf535fde"
    />
    <img width="522" alt="Screenshot 2025-06-05 at 10 25 22"
    src="https://github.com/user-attachments/assets/acb96c78-14b7-49b1-903d-c7f36237b15b"
    />
    
    After:
    <img width="528" alt="Screenshot 2025-06-05 at 10 23 09"
    src="https://github.com/user-attachments/assets/6a7e888f-6fb7-44a7-a970-6b48d04f6f96"
    />
    
    <img width="556" alt="Screenshot 2025-06-05 at 10 24 31"
    src="https://github.com/user-attachments/assets/fdb15ad7-825e-42d4-810f-869bf86dd36f"
    />
    
    
    ## How to test
    
    * Follow the instructions in
    https://github.com/elastic/kibana-team/issues/1521. Use the any data
    folder.

--- x-pack/platform/plugins/private/upgrade_assistant/public/application/components/es_deprecations/deprecation_types/index_settings/resolution_table_cell.tsx
+++ x-pack/platform/plugins/private/upgrade_assistant/public/application/components/es_deprecations/deprecation_types/index_settings/resolution_table_cell.tsx
@@ -85,7 +85,7 @@ export const IndexSettingsResolutionCell: React.FunctionComponent<Props> = ({ st
         data-test-subj="indexSettingsResolutionStatusCell"
       >
         <EuiFlexItem grow={false}>
-          <EuiIcon type="check" color="success" />
+          <EuiIcon type="checkInCircleFilled" color="success" />
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
           <EuiText size="s">{i18nTexts.deleteCompleteText}</EuiText>
@@ -102,7 +102,7 @@ export const IndexSettingsResolutionCell: React.FunctionComponent<Props> = ({ st
         data-test-subj="indexSettingsResolutionStatusCell"
       >
         <EuiFlexItem grow={false}>
-          <EuiIcon type="warning" color="danger" />
+          <EuiIcon type="warningFilled" color="danger" />
         </EuiFlexItem>
         <EuiFlexItem grow={false}>
           <EuiText size="s">{i18nTexts.deleteFailedText}</EuiText>
