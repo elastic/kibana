302ff0de2aaf23f146871f4be4eb918fece19c1b
Rodney Norris <rodney.norris@elastic.co>
2025-06-06 21:07:23 +0000

    search_playground: handle playground name breadcrumbs

--- x-pack/solutions/search/plugins/search_playground/public/hooks/use_playground_breadcrumbs.ts
+++ x-pack/solutions/search/plugins/search_playground/public/hooks/use_playground_breadcrumbs.ts
@@ -9,31 +9,38 @@ import { useEffect } from 'react';
 
 import { i18n } from '@kbn/i18n';
 
+import { PLUGIN_PATH } from '../../common';
 import { useKibana } from './use_kibana';
 
-export const usePlaygroundBreadcrumbs = () => {
-  const { searchNavigation } = useKibana().services;
+export const usePlaygroundBreadcrumbs = (playgroundName?: string) => {
+  const { http, searchNavigation } = useKibana().services;
 
   useEffect(() => {
-    searchNavigation?.breadcrumbs.setSearchBreadCrumbs(
-      [
-        {
-          text: i18n.translate('xpack.searchPlayground.breadcrumbs.build', {
-            defaultMessage: 'Build',
-          }),
-        },
-        {
-          text: i18n.translate('xpack.searchPlayground.breadcrumbs.playground', {
-            defaultMessage: 'Playground',
-          }),
-        },
-      ],
-      { forClassicChromeStyle: true }
-    );
+    searchNavigation?.breadcrumbs.setSearchBreadCrumbs([
+      // TODO: confirm how this behaves with classic navigation
+      // {
+      //   text: i18n.translate('xpack.searchPlayground.breadcrumbs.build', {
+      //     defaultMessage: 'Build',
+      //   }),
+      // },
+      {
+        text: i18n.translate('xpack.searchPlayground.breadcrumbs.playground', {
+          defaultMessage: 'Playground',
+        }),
+        href: playgroundName !== undefined ? http.basePath.prepend(PLUGIN_PATH) : undefined,
+      },
+      ...(playgroundName !== undefined
+        ? [
+            {
+              text: playgroundName,
+            },
+          ]
+        : []),
+    ]);
 
     return () => {
       // Clear breadcrumbs on unmount;
       searchNavigation?.breadcrumbs.clearBreadcrumbs();
     };
-  }, [searchNavigation]);
+  }, [http, searchNavigation, playgroundName]);
 };
