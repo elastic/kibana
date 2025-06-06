e5a88f894bdd1f30e5d78450193fd7b4340bd1df
Sandra Gonzales <sandra.gonzales@elastic.co>
2025-06-06 07:16:41 -0400

    remove file

--- x-pack/platform/plugins/shared/observability_ai_assistant/public/index.ts
+++ x-pack/platform/plugins/shared/observability_ai_assistant/public/index.ts
@@ -91,7 +91,6 @@ export { ObservabilityAIAssistantTelemetryEventType } from './analytics/telemetr
 
 export { createFunctionRequestMessage } from '../common/utils/create_function_request_message';
 export { createFunctionResponseMessage } from '../common/utils/create_function_response_message';
-export { buildDetectedEntitiesMap } from '../common/utils/anonymization/build_detected_entities_map';
 export { redactEntities, unhashString } from '../common/utils/anonymization/redaction';
 
 export type {
