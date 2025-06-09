6920f124c6a8fdf9b65806b0a37dfa7f0b485eb9
Larry Gregory <larry.gregory@elastic.co>
2025-06-09 12:47:50 -0400

    Attempt to debug test failures

--- x-pack/test/functional/services/user_menu.js
+++ x-pack/test/functional/services/user_menu.js
@@ -8,48 +8,69 @@
 export function UserMenuProvider({ getService }) {
   const testSubjects = getService('testSubjects');
   const retry = getService('retry');
+  const log = getService('log');
 
   return new (class UserMenu {
     async clickLogoutButton() {
+      log.debug('Clicking logout button in user menu');
       await this._ensureMenuOpen();
       await testSubjects.click('userMenu > logoutLink');
     }
 
     async clickProvileLink() {
+      log.debug('Clicking profile link in user menu');
       await this._ensureMenuOpen();
       await testSubjects.click('userMenu > profileLink');
     }
 
     async logoutLinkExists() {
+      log.debug('Checking if logout link exists in user menu');
       if (!(await testSubjects.exists('userMenuButton'))) {
+        log.debug('User menu button does not exist, skipping logout link check');
         return;
       }
 
+      log.debug('Ensuring user menu is open to check for logout link');
       await this._ensureMenuOpen();
+      log.debug('Checking for existence of logout link');
       return await testSubjects.exists('userMenu > logoutLink');
     }
 
     async openMenu() {
+      log.debug('Opening user menu');
       await this._ensureMenuOpen();
     }
 
     async closeMenu() {
+      log.debug('Closing user menu if it is open');
       if (!(await testSubjects.exists('userMenu'))) {
+        log.debug('User menu is not open, skipping close action');
         return;
       }
 
+      log.debug('Closing user menu');
       await testSubjects.click('userMenuButton');
+      log.debug('Waiting for user menu to be closed');
       await testSubjects.missingOrFail('userMenu');
     }
 
     async _ensureMenuOpen() {
+      log.debug('Ensuring user menu is open');
       if (await testSubjects.exists('userMenu')) {
+        log.debug('User menu is already open, skipping open action');
         return;
       }
 
       await retry.try(async () => {
+        log.debug('Opening user menu');
         await testSubjects.click('userMenuButton');
-        await testSubjects.existOrFail('userMenu', { timeout: 2500 });
+        log.debug('Waiting for user menu to be visible');
+        try {
+          await testSubjects.existOrFail('userMenu', { timeout: 2500 });
+        } catch (error) {
+          log.error('Failed to open user menu:', error);
+          throw error;
+        }
       });
     }
   })();
