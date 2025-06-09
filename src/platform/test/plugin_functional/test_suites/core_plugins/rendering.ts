ac718f89262bd0a937cad64a232ed1a6493e187f
Ying Mao <ying.mao@elastic.co>
2025-06-09 11:37:14 -0400

    [Response Ops][Alerting] Explicitly allow rule types to be registered via config (#221707)
    Resolves https://github.com/elastic/kibana/issues/219974
    
    ## Summary
    
    Adds kibana config `xpack.alerting.enabledRuleTypes: string[]`, which
    can be used to "enable" specific rule types. If config is not set, all
    rule types will be registered (as long as they are not disabled). If
    config is set, only rule types explicitly allow-listed will be
    registered.
    
    If a rule type is both in the disabled and enabled allow-list, it will
    not be registered and a warning will be logged.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/platform/test/plugin_functional/test_suites/core_plugins/rendering.ts
+++ src/platform/test/plugin_functional/test_suites/core_plugins/rendering.ts
@@ -358,6 +358,7 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
         'xpack.alerting.maintenanceWindow.enabled (boolean?)',
         'xpack.alerting.rulesSettings.enabled (boolean?)',
         'xpack.alerting.disabledRuleTypes (array?)',
+        'xpack.alerting.enabledRuleTypes (array?)',
         'xpack.upgrade_assistant.featureSet.migrateSystemIndices (boolean?)',
         'xpack.upgrade_assistant.featureSet.mlSnapshots (boolean?)',
         'xpack.upgrade_assistant.featureSet.reindexCorrectiveActions (boolean?)',
