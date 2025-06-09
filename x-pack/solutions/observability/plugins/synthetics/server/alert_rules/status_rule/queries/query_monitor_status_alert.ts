2e7b78dc4558e78bc6aa929551d8138aa9243bba
Francesco Fagnani <francesco.fagnani@elastic.co>
2025-06-09 09:25:53 +0200

    refactor(helpers): created helper file for query monitor status alert

--- x-pack/solutions/observability/plugins/synthetics/server/alert_rules/status_rule/queries/query_monitor_status_alert.ts
+++ x-pack/solutions/observability/plugins/synthetics/server/alert_rules/status_rule/queries/query_monitor_status_alert.ts
@@ -8,14 +8,12 @@
 import pMap from 'p-map';
 import { times } from 'lodash';
 import { intersection } from 'lodash';
-import moment from 'moment';
 import { SavedObjectsFindResult } from '@kbn/core/server';
 import { Logger } from '@kbn/core/server';
 import {
   AlertStatusConfigs,
   AlertStatusMetaData,
   AlertPendingStatusConfigs,
-  MissingPingMonitorInfo,
 } from '../../../../common/runtime_types/alert_rules/common';
 import {
   EncryptedSyntheticsMonitorAttributes,
@@ -23,14 +21,10 @@ import {
 } from '../../../../common/runtime_types';
 import { SyntheticsEsClient } from '../../../lib';
 import { getSearchPingsParams } from './get_search_ping_params';
+import { ConfigStats, getConfigStats, getIsValidPing, getPendingConfigs } from './helpers';
 
 const DEFAULT_MAX_ES_BUCKET_SIZE = 10000;
 
-interface ConfigStats {
-  up: number;
-  down: number;
-  pending: number;
-}
 export interface AlertStatusResponse {
   upConfigs: AlertStatusConfigs;
   downConfigs: AlertStatusConfigs;
@@ -38,151 +32,6 @@ export interface AlertStatusResponse {
   enabledMonitorQueryIds: string[];
   configStats: Record<string, ConfigStats>;
 }
-
-const getMissingPingMonitorInfo = ({
-  monitors,
-  configId,
-  locationId,
-}: {
-  monitors: Array<SavedObjectsFindResult<EncryptedSyntheticsMonitorAttributes>>;
-  configId: string;
-  locationId: string;
-}): (MissingPingMonitorInfo & { createdAt?: string }) | undefined => {
-  const monitor = monitors.find((m) => m.id === configId);
-  if (!monitor) {
-    // This should never happen
-    return;
-  }
-
-  // For some reason 'urls' is not considered a valid attribute in the monitor attributes, there's probably a problem with the EncryptedSyntheticsMonitorAttributes type
-  const fullUrl =
-    'urls' in monitor.attributes && typeof monitor.attributes.urls === 'string'
-      ? monitor.attributes.urls
-      : '';
-
-  return {
-    monitor: {
-      name: monitor.attributes.name,
-      id: configId,
-      type: monitor.attributes.type,
-    },
-    observer: {
-      geo: {
-        name: monitor.attributes.locations.find((l) => l.id === locationId)?.label || '',
-      },
-    },
-    labels: monitor.attributes.labels,
-    tags: monitor.attributes.tags,
-    url: { full: fullUrl },
-    createdAt: monitor.created_at,
-  };
-};
-
-const getPendingConfigs = async ({
-  monitorQueryIds,
-  monitorLocationIds,
-  upConfigs,
-  downConfigs,
-  monitorsData,
-  monitors,
-  logger,
-}: {
-  monitorQueryIds: string[];
-  monitorLocationIds: string[];
-  upConfigs: AlertStatusConfigs;
-  downConfigs: AlertStatusConfigs;
-  monitorsData: Record<string, { scheduleInMs: number; locations: string[] }>;
-  monitors: Array<SavedObjectsFindResult<EncryptedSyntheticsMonitorAttributes>>;
-  logger: Logger;
-}) => {
-  // Check if a config is missing, if it is it means that the monitor is pending
-  const pendingConfigs: AlertPendingStatusConfigs = {};
-
-  for (const monitorQueryId of monitorQueryIds) {
-    for (const locationId of monitorLocationIds) {
-      const configWithLocationId = `${monitorQueryId}-${locationId}`;
-
-      const isConfigMissing =
-        !upConfigs[configWithLocationId] &&
-        !downConfigs[configWithLocationId] &&
-        monitorsData[monitorQueryId].locations.includes(locationId);
-
-      if (isConfigMissing) {
-        const res = getMissingPingMonitorInfo({
-          configId: monitorQueryId,
-          locationId,
-          monitors,
-        });
-        if (res) {
-          const { createdAt, ...monitorInfo } = res;
-          if (
-            !createdAt ||
-            // Wait at least 5 minutes since monitor creation before setting the monitor as pending
-            (createdAt && moment(createdAt).isBefore(moment().subtract(5, 'minutes')))
-          ) {
-            pendingConfigs[configWithLocationId] = {
-              status: 'pending',
-              configId: monitorQueryId,
-              monitorQueryId,
-              locationId,
-              monitorInfo,
-            };
-          }
-        } else {
-          logger.error(
-            `Config ${configWithLocationId} not added to pending configs because the monitor info is missing`
-          );
-        }
-      }
-    }
-  }
-
-  return pendingConfigs;
-};
-
-const getConfigStats = ({
-  monitorQueryIds,
-  upConfigs,
-  downConfigs,
-  pendingConfigs,
-}: {
-  monitorQueryIds: string[];
-  upConfigs: AlertStatusConfigs;
-  downConfigs: AlertStatusConfigs;
-  pendingConfigs: AlertPendingStatusConfigs;
-}) => {
-  // Pre-organize configs by monitorId for faster lookup
-  const configsByMonitor = new Map<string, ConfigStats>();
-
-  // Initialize all monitors with zero counts
-  for (const monitorId of monitorQueryIds) {
-    configsByMonitor.set(monitorId, { up: 0, down: 0, pending: 0 });
-  }
-
-  // Count up configs
-  for (const configKey of Object.keys(upConfigs)) {
-    const monitorId = upConfigs[configKey].monitorQueryId;
-    const stats = configsByMonitor.get(monitorId);
-    if (stats) stats.up++;
-  }
-
-  // Count down configs
-  for (const configKey of Object.keys(downConfigs)) {
-    const monitorId = downConfigs[configKey].monitorQueryId;
-    const stats = configsByMonitor.get(monitorId);
-    if (stats) stats.down++;
-  }
-
-  // Count pending configs
-  for (const configKey of Object.keys(pendingConfigs)) {
-    const monitorId = pendingConfigs[configKey].monitorQueryId;
-    const stats = configsByMonitor.get(monitorId);
-    if (stats) stats.pending++;
-  }
-
-  // Convert Map to the expected Record structure
-  return Object.fromEntries(configsByMonitor.entries());
-};
 
 export async function queryMonitorStatusAlert({
   esClient,
@@ -263,15 +112,10 @@ export async function queryMonitorStatusAlert({
             const configId = latestPing.config_id;
             const monitorQueryId = latestPing.monitor.id;
 
-            const msSinceLastPing =
-              new Date().getTime() - new Date(latestPing['@timestamp']).getTime();
-            const msBeforeIsPending =
-              monitorsData[monitorQueryId].scheduleInMs +
-              moment.duration(60, 'seconds').asMilliseconds();
-
-            // Example: if a monitor has a schedule of 5m the last valid ping can be at (5+1)m
-            // If it's greater than that it means the monitor is pending
-            const isValidPing = msBeforeIsPending - msSinceLastPing > 0;
+            const isValidPing = getIsValidPing({
+              scheduleInMs: monitorsData[monitorQueryId].scheduleInMs,
+              timestamp: latestPing['@timestamp'],
+            });
 
             const meta: AlertStatusMetaData = {
               ping: latestPing,
@@ -308,7 +152,7 @@ export async function queryMonitorStatusAlert({
     { concurrency: 5 }
   );
 
-  const pendingConfigs = await getPendingConfigs({
+  const pendingConfigs = getPendingConfigs({
     monitorQueryIds,
     monitorLocationIds,
     upConfigs,
