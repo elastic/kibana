a087d9deae939ea3a3c817e5a413a623c0bcc351
Stratoula <efstratia.kalafateli@elastic.co>
2025-06-10 15:06:03 +0200

    Fix type checks

--- src/platform/packages/shared/kbn-esql-validation-autocomplete/src/definitions/types.ts
+++ src/platform/packages/shared/kbn-esql-validation-autocomplete/src/definitions/types.ts
@@ -174,6 +174,8 @@ export interface Signature {
   }>;
   minParams?: number;
   returnType: FunctionReturnType;
+  // Not used yet, but we will in the future.
+  license?: string;
 }
 
 export enum FunctionDefinitionTypes {
