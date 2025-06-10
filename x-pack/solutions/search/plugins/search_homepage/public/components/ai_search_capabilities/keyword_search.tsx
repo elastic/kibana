48d06115cbd3ccdee9bc38aff0261a640d338186
Saikat Sarkar <saikat.sarkar@elastic.co>
2025-06-10 15:44:08 -0600

    Add keyword search section

--- x-pack/solutions/search/plugins/search_homepage/public/components/ai_search_capabilities/keyword_search.tsx
+++ x-pack/solutions/search/plugins/search_homepage/public/components/ai_search_capabilities/keyword_search.tsx
@@ -6,5 +6,131 @@
  */
 
 import React from 'react';
+import {
+  EuiTitle,
+  EuiText,
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiSpacer,
+  EuiImage,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
 
-export const KeywordSearch: React.FC = () => <div>Keyword Search Component</div>;
+import KeywordSearchImage from '../../../public/assets/keyword_search.svg';
+
+export const KeywordSearch: React.FC = () => (
+  <EuiFlexGroup direction="column">
+    <EuiFlexItem grow={false}>
+      <EuiSpacer size="m" />
+    </EuiFlexItem>
+    <EuiFlexItem grow={false}>
+      <EuiFlexGroup>
+        <EuiFlexItem grow={false}>
+          <EuiImage
+            src={KeywordSearchImage}
+            alt={i18n.translate(
+              'xpack.searchHomepage.aiSearchCapabilities.keywordSearch.imageAlt',
+              {
+                defaultMessage: 'Keyword Search',
+              }
+            )}
+            size="s"
+          />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiFlexGroup direction="column" gutterSize="m">
+            <EuiFlexItem grow={false}>
+              <EuiTitle size="xs">
+                <span>
+                  {i18n.translate('xpack.searchHomepage.aiSearchCapabilities.keywordSearch.title', {
+                    defaultMessage: 'Boost results with keyword search.',
+                  })}
+                </span>
+              </EuiTitle>
+            </EuiFlexItem>
+
+            <EuiFlexItem grow={false}>
+              {i18n.translate(
+                'xpack.searchHomepage.aiSearchCapabilities.keywordSearch.description',
+                {
+                  defaultMessage:
+                    'Leverage traditional text matching techniques to quickly retrieve precise keyword-based results from your data.',
+                }
+              )}
+            </EuiFlexItem>
+
+            <EuiFlexItem grow={false}>
+              <EuiFlexGroup direction="column" gutterSize="xs">
+                <EuiFlexItem grow={false}>
+                  <EuiFlexGroup gutterSize="m">
+                    <EuiFlexItem grow={false}>
+                      <EuiIcon type="checkInCircleFilled" color="subdued" />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText size="s">
+                        {i18n.translate(
+                          'xpack.searchHomepage.aiSearchCapabilities.keywordSearch.firstLine',
+                          {
+                            defaultMessage: 'Fast text matching and filtering capabilities',
+                          }
+                        )}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiFlexGroup gutterSize="m">
+                    <EuiFlexItem grow={false}>
+                      <EuiIcon type="checkInCircleFilled" color="subdued" />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText size="s">
+                        {i18n.translate(
+                          'xpack.searchHomepage.aiSearchCapabilities.keywordSearch.secondLine',
+                          {
+                            defaultMessage: 'Optimized for large-scale data retrieval',
+                          }
+                        )}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiFlexGroup gutterSize="m">
+                    <EuiFlexItem grow={false}>
+                      <EuiIcon type="checkInCircleFilled" color="subdued" />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText size="s">
+                        {i18n.translate(
+                          'xpack.searchHomepage.aiSearchCapabilities.keywordSearch.thirdLine',
+                          {
+                            defaultMessage: 'Easily integrate with existing keyword indices',
+                          }
+                        )}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <div>
+                <EuiButton iconType="plusInCircle" size="s">
+                  {i18n.translate(
+                    'xpack.searchHomepage.aiSearchCapabilities.keywordSearch.createKeywordIndex',
+                    {
+                      defaultMessage: 'Create a keyword index',
+                    }
+                  )}
+                </EuiButton>
+              </div>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiFlexItem>
+  </EuiFlexGroup>
+);
