554ebbea238bc6476bf38238e3e0ec43c65b94ad
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-06-10 13:59:46 +0200

    [8.19] [One Discover] Handle Alerts and SLOs Privileges (#221786) (#222979)
    # Backport
    
    This will backport the following commits from `main` to `8.19`:
    - [[One Discover] Handle Alerts and SLOs Privileges
    (#221786)](https://github.com/elastic/kibana/pull/221786)
    
    <!--- Backport version: 10.0.0 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT
    [{"author":{"name":"mohamedhamed-ahmed","email":"mohamed.ahmed@elastic.co"},"sourceCommit":{"committedDate":"2025-06-04T08:16:25Z","message":"[One
    Discover] Handle Alerts and SLOs Privileges (#221786)\n\n##
    Summary\n\ncloses
    https://github.com/elastic/kibana/issues/177067\ncloses
    https://github.com/elastic/kibana/issues/184564\n\nThis PR check on
    assigned privileges for the user to hide/show Alerts\nand SLOs depending
    on whether the user has access to them or
    not.\n\n---------\n\nCo-authored-by: kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"7f5aa3e2692fd12cdff14847261952dc294391e4","branchLabelMapping":{"^v9.1.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["Feature:Discover","release_note:skip","backport
    missing","Team:DataDiscovery","Team:obs-ux-logs","Project:OneDiscover","backport:version","v9.1.0","v8.19.0"],"title":"[One
    Discover] Handle Alerts and SLOs
    Privileges","number":221786,"url":"https://github.com/elastic/kibana/pull/221786","mergeCommit":{"message":"[One
    Discover] Handle Alerts and SLOs Privileges (#221786)\n\n##
    Summary\n\ncloses
    https://github.com/elastic/kibana/issues/177067\ncloses
    https://github.com/elastic/kibana/issues/184564\n\nThis PR check on
    assigned privileges for the user to hide/show Alerts\nand SLOs depending
    on whether the user has access to them or
    not.\n\n---------\n\nCo-authored-by: kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"7f5aa3e2692fd12cdff14847261952dc294391e4"}},"sourceBranch":"main","suggestedTargetBranches":["8.19"],"targetPullRequestStates":[{"branch":"main","label":"v9.1.0","branchLabelMappingKey":"^v9.1.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/221786","number":221786,"mergeCommit":{"message":"[One
    Discover] Handle Alerts and SLOs Privileges (#221786)\n\n##
    Summary\n\ncloses
    https://github.com/elastic/kibana/issues/177067\ncloses
    https://github.com/elastic/kibana/issues/184564\n\nThis PR check on
    assigned privileges for the user to hide/show Alerts\nand SLOs depending
    on whether the user has access to them or
    not.\n\n---------\n\nCo-authored-by: kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"7f5aa3e2692fd12cdff14847261952dc294391e4"}},{"branch":"8.19","label":"v8.19.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->

--- src/platform/plugins/shared/discover/public/__mocks__/services.ts
+++ src/platform/plugins/shared/discover/public/__mocks__/services.ts
@@ -19,6 +19,7 @@ import {
   chromeServiceMock,
   coreMock,
   docLinksServiceMock,
+  notificationServiceMock,
   scopedHistoryMock,
   themeServiceMock,
 } from '@kbn/core/public/mocks';
@@ -220,6 +221,9 @@ export function createDiscoverServicesMock(): DiscoverServices {
       addWarning: jest.fn(),
       addDanger: jest.fn(),
       addSuccess: jest.fn(),
+    },
+    notifications: {
+      toasts: notificationServiceMock.createStartContract().toasts,
     },
     expressions: expressionsPlugin,
     savedObjectsTagging: {
