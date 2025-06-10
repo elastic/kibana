a11ac2a4ca2ec371a69e68e3635d5257f3d973bf
Saikat Sarkar <saikat.sarkar@elastic.co>
2025-06-10 15:41:11 -0600

    Add vector search section

--- x-pack/solutions/search/plugins/search_homepage/public/components/ai_search_capabilities/vector_search.tsx
+++ x-pack/solutions/search/plugins/search_homepage/public/components/ai_search_capabilities/vector_search.tsx
@@ -6,5 +6,128 @@
  */
 
 import React from 'react';
+import {
+  EuiTitle,
+  EuiText,
+  EuiButton,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  EuiSpacer,
+  EuiImage,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
 
-export const VectorSearch: React.FC = () => <div>Vector Search Component</div>;
+import VectorSearchImage from '../../../public/assets/vector_search.svg';
+
+export const VectorSearch: React.FC = () => (
+  <EuiFlexGroup direction="column">
+    <EuiFlexItem grow={false}>
+      <EuiSpacer size="m" />
+    </EuiFlexItem>
+    <EuiFlexItem grow={false}>
+      <EuiFlexGroup>
+        <EuiFlexItem grow={false}>
+          <EuiImage
+            src={VectorSearchImage}
+            alt={i18n.translate('xpack.searchHomepage.aiSearchCapabilities.vectorSearch.imageAlt', {
+              defaultMessage: 'Vector Search',
+            })}
+            size="s"
+          />
+        </EuiFlexItem>
+        <EuiFlexItem grow={false}>
+          <EuiFlexGroup direction="column" gutterSize="m">
+            <EuiFlexItem grow={false}>
+              <EuiTitle size="xs">
+                <span>
+                  {i18n.translate('xpack.searchHomepage.aiSearchCapabilities.vectorSearch.title', {
+                    defaultMessage: 'Supercharge retrieval with vector search.',
+                  })}
+                </span>
+              </EuiTitle>
+            </EuiFlexItem>
+
+            <EuiFlexItem grow={false}>
+              {i18n.translate(
+                'xpack.searchHomepage.aiSearchCapabilities.vectorSearch.description',
+                {
+                  defaultMessage:
+                    'Store and search dense vector representations of your data for similarity-based retrieval and advanced AI-powered search experiences.',
+                }
+              )}
+            </EuiFlexItem>
+
+            <EuiFlexItem grow={false}>
+              <EuiFlexGroup direction="column" gutterSize="xs">
+                <EuiFlexItem grow={false}>
+                  <EuiFlexGroup gutterSize="m">
+                    <EuiFlexItem grow={false}>
+                      <EuiIcon type="checkInCircleFilled" color="subdued" />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText size="s">
+                        {i18n.translate(
+                          'xpack.searchHomepage.aiSearchCapabilities.vectorSearch.firstLine',
+                          {
+                            defaultMessage: 'Supports multiple vector similarity algorithms',
+                          }
+                        )}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiFlexGroup gutterSize="m">
+                    <EuiFlexItem grow={false}>
+                      <EuiIcon type="checkInCircleFilled" color="subdued" />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText size="s">
+                        {i18n.translate(
+                          'xpack.searchHomepage.aiSearchCapabilities.vectorSearch.secondLine',
+                          {
+                            defaultMessage: 'Integrates with popular ML frameworks',
+                          }
+                        )}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiFlexGroup gutterSize="m">
+                    <EuiFlexItem grow={false}>
+                      <EuiIcon type="checkInCircleFilled" color="subdued" />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText size="s">
+                        {i18n.translate(
+                          'xpack.searchHomepage.aiSearchCapabilities.vectorSearch.thirdLine',
+                          {
+                            defaultMessage: 'Scale to billions of vectors with efficient indexing',
+                          }
+                        )}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <div>
+                <EuiButton iconType="plusInCircle" size="s">
+                  {i18n.translate(
+                    'xpack.searchHomepage.aiSearchCapabilities.vectorSearch.createVectorIndex',
+                    {
+                      defaultMessage: 'Create a vector index',
+                    }
+                  )}
+                </EuiButton>
+              </div>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiFlexItem>
+  </EuiFlexGroup>
+);
