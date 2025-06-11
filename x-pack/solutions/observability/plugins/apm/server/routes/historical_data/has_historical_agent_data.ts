a804266fa2af19ca38bea0614938c31778faacd9
Gonçalo Rica <goncalo.rica@elastic.co>
2025-06-11 16:53:20 +0200

    [APM] Limit has_data query range progressively

--- x-pack/solutions/observability/plugins/apm/server/routes/historical_data/has_historical_agent_data.ts
+++ x-pack/solutions/observability/plugins/apm/server/routes/historical_data/has_historical_agent_data.ts
@@ -7,16 +7,25 @@
 
 import { ProcessorEvent } from '@kbn/observability-plugin/common';
 import type { DataTier } from '@kbn/observability-shared-plugin/common';
+import type { QueryDslQueryContainer } from '@kbn/data-views-plugin/common/types';
+import type { QueryDslRangeQuery } from '@elastic/elasticsearch/lib/api/types';
 import type { APMEventClient } from '../../lib/helpers/create_es_client/create_apm_event_client';
 
-export async function hasHistoricalAgentData(apmEventClient: APMEventClient) {
-  const hasDataInWarmOrHotDataTiers = await hasDataRequest(apmEventClient, [
-    'data_hot',
-    'data_warm',
-  ]);
+export async function hasHistoricalAgentData(
+  apmEventClient: APMEventClient,
+  timeRange?: QueryDslRangeQuery
+) {
+  const ranges = resolveRanges(timeRange);
 
-  if (hasDataInWarmOrHotDataTiers) {
-    return true;
+  for (const range of ranges) {
+    const hasDataInWarmOrHotDataTiers = await hasDataRequest(apmEventClient, range, [
+      'data_hot',
+      'data_warm',
+    ]);
+
+    if (hasDataInWarmOrHotDataTiers) {
+      return true;
+    }
   }
 
   const hasDataUnbounded = await hasDataRequest(apmEventClient);
@@ -24,10 +33,22 @@ export async function hasHistoricalAgentData(apmEventClient: APMEventClient) {
   return hasDataUnbounded;
 }
 
-async function hasDataRequest(apmEventClient: APMEventClient, dataTiers?: DataTier[]) {
+async function hasDataRequest(
+  apmEventClient: APMEventClient,
+  range?: QueryDslRangeQuery,
+  dataTiers?: DataTier[]
+) {
   // the `observability:searchExcludedDataTiers` setting will also be considered
   // in the `search` function to exclude data tiers from the search
-  const query = dataTiers ? { terms: { _tier: dataTiers } } : undefined;
+  const query: QueryDslQueryContainer[] = [];
+
+  if (dataTiers) {
+    query.push({ terms: { _tier: dataTiers } });
+  }
+
+  if (range) {
+    query.push({ range: { '@timestamp': range } });
+  }
 
   const params = {
     apm: {
@@ -36,9 +57,24 @@ async function hasDataRequest(apmEventClient: APMEventClient, dataTiers?: DataTi
     terminate_after: 1,
     track_total_hits: 1,
     size: 0,
-    query,
+    query: { bool: { filter: query } },
   };
 
   const resp = await apmEventClient.search('has_historical_agent_data', params);
   return resp.hits.total.value > 0;
+}
+
+function resolveRanges(timeRange?: QueryDslRangeQuery): QueryDslRangeQuery[] {
+  if (!timeRange) {
+    return [
+      { gte: 'now-15m', lte: 'now' },
+      { gte: 'now-3d', lte: 'now' },
+    ];
+  }
+
+  if (timeRange.gte === 'now-15m') {
+    return [timeRange, { gte: 'now-3d', lte: 'now' }];
+  }
+
+  return [{ gte: 'now-15m', lte: 'now' }, timeRange];
 }
