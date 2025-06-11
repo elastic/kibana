4a592dc6c97f67f680ce9e999a42f33294b373db
pgayvallet <pierre.gayvallet@elastic.co>
2025-06-11 11:31:51 +0200

    tweak system prompt to use the new default tools

--- x-pack/platform/plugins/shared/onechat/server/services/agents/conversational/system_prompt.ts
+++ x-pack/platform/plugins/shared/onechat/server/services/agents/conversational/system_prompt.ts
@@ -5,16 +5,35 @@
  * 2.0.
  */
 
-import { BaseMessage, BaseMessageLike } from '@langchain/core/messages';
+import type { BaseMessage, BaseMessageLike } from '@langchain/core/messages';
+import { OnechatToolIds } from '@kbn/onechat-common';
 
-export const defaultSystemPrompt =
-  'You are a helpful chat assistant from the Elasticsearch company.';
+export const defaultSystemPrompt = `
+   You are a helpful chat assistant from the Elasticsearch company.
+
+   You have a set of tools at your disposal that can be used to help you answering questions.
+   In particular, you have tools to access the Elasticsearch cluster on behalf of the user, to search and retrieve documents
+   they have access to.
+
+   - Never infer an index name from the user's input. Instead, use the ${OnechatToolIds.listIndices} tool
+     to list the indices in the Elasticsearch cluster the current user has access to.
+     E.g if the user asks "Can you find documents in the alerts index", Don't assume the index name is "alerts",
+     and use the ${OnechatToolIds.listIndices} instead to retrieve the list of indices and identify the correct one.
+
+   - Once you have identified the correct index, use the ${OnechatToolIds.getIndexMapping} tool to retrieve its mappings,
+     as you will need it to call any search tool.
+
+   - When doing fulltext search, prefer the ${OnechatToolIds.searchFulltext} tool over the ${OnechatToolIds.searchDsl} one
+     when possible.
+
+   - Search tools return highlights of the documents that match the query. The full content of a document can be retrieved
+     using the ${OnechatToolIds.getDocumentById} tool.
+     `;
 
 const getFullSystemPrompt = (systemPrompt: string) => {
   return `${systemPrompt}
 
   ### Additional info
-  - You have tools at your disposal that you can use
   - The current date is: ${new Date().toISOString()}
   - You can use markdown format to structure your response
   `;
