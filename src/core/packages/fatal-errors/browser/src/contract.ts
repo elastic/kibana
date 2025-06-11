3d2ee5fc850d1a0dee08b9257f8c4b631c965a25
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-06-11 16:03:44 +0200

    Remove code returning fatal error info that became obsolete after #59781 and #48301

--- src/core/packages/fatal-errors/browser/src/contract.ts
+++ src/core/packages/fatal-errors/browser/src/contract.ts
@@ -7,8 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { Observable } from 'rxjs';
-import type { FatalErrorInfo } from './get_error_info';
 /**
  * FatalErrors stop the Kibana Public Core and displays a fatal error screen
  * with details about the Kibana build and the error.
@@ -24,11 +22,6 @@ export interface FatalErrorsSetup {
    * @param source - Adds a prefix of the form `${source}: ` to the error message
    */
   add: (error: string | Error, source?: string) => never;
-
-  /**
-   * An Observable that will emit whenever a fatal error is added with `add()`
-   */
-  get$: () => Observable<FatalErrorInfo>;
 }
 
 /**
