e9412e6ce1240f5fbb4022e864775e5ab7ef83a5
Nathan Reese <reese.nathan@gmail.com>
2025-06-11 11:35:49 -0600

    [dashboard] remove url_field_formatter functional test

--- src/platform/test/functional/apps/dashboard/group1/index.ts
+++ src/platform/test/functional/apps/dashboard/group1/index.ts
@@ -35,7 +35,6 @@ export default function ({ getService, loadTestFile }: FtrProviderContext) {
 
     // This has to be first since the other tests create some embeddables as side affects and our counting assumes
     // a fresh index.
-    loadTestFile(require.resolve('./url_field_formatter'));
     loadTestFile(require.resolve('./embeddable_rendering'));
     loadTestFile(require.resolve('./embeddable_data_grid'));
     loadTestFile(require.resolve('./edit_embeddable_redirects'));
