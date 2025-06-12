8f23842670f7dff2c4350beb42c72f1f7d19cde7
Robert Oskamp <robert.oskamp@elastic.co>
2025-06-12 18:31:34 +0200

    [Obs Logs Essentials] Initial FTR test setup

--- x-pack/test/api_integration/deployment_agnostic/default_configs/serverless.config.base.ts
+++ x-pack/test/api_integration/deployment_agnostic/default_configs/serverless.config.base.ts
@@ -25,6 +25,7 @@ interface CreateTestConfigOptions<T extends DeploymentAgnosticCommonServices> {
   testFiles: string[];
   junit: { reportName: string };
   suiteTags?: { include?: string[]; exclude?: string[] };
+  tier?: 'oblt_logs_essentials';
 }
 
 // include settings from elasticsearch controller
@@ -134,6 +135,13 @@ export function createServerlessTestConfig<T extends DeploymentAgnosticCommonSer
             : []),
           ...(dockerRegistryPort
             ? [`--xpack.fleet.registryUrl=http://localhost:${dockerRegistryPort}`]
+            : []),
+          ...(options.tier && options.tier === 'oblt_logs_essentials'
+            ? [
+                `--pricing.tiers.products=${JSON.stringify([
+                  { name: 'observability', tier: 'logs_essentials' },
+                ])}`,
+              ]
             : []),
         ],
       },
