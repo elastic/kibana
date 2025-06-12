58759ff19ceb07dd69c7977aba4aa75a4e55b88f
Konrad Szwarc <konrad.szwarc@elastic.co>
2025-06-12 11:49:37 +0200

    remove pack policy execution after policy changes space

--- x-pack/platform/plugins/shared/osquery/server/plugin.ts
+++ x-pack/platform/plugins/shared/osquery/server/plugin.ts
@@ -35,7 +35,10 @@ import type { OsqueryAppContext } from './lib/osquery_app_context_services';
 import { OsqueryAppContextService } from './lib/osquery_app_context_services';
 import type { ConfigType } from '../common/config';
 import { OSQUERY_INTEGRATION_NAME } from '../common';
-import { getPackagePolicyDeleteCallback } from './lib/fleet_integration';
+import {
+  getPackagePolicyDeleteCallback,
+  getAgentPolicyPostUpdateCallback,
+} from './lib/fleet_integration';
 import { TelemetryEventsSender } from './lib/telemetry/sender';
 import { TelemetryReceiver } from './lib/telemetry/receiver';
 import { initializeTransformsIndices } from './create_indices/create_transforms_indices';
@@ -194,6 +197,7 @@ export class OsqueryPlugin implements Plugin<OsqueryPluginSetup, OsqueryPluginSt
           );
 
           registerIngestCallback('packagePolicyPostDelete', getPackagePolicyDeleteCallback(client));
+          registerIngestCallback('agentPolicyPostUpdate', getAgentPolicyPostUpdateCallback(core));
         }
       })
       .catch(() => {
