e4e5b237c2d92e0bd6e9938b1b897de6b49569e4
Davis McPhee <davis.mcphee@elastic.co>
2025-06-11 21:34:08 -0300

    Fix tab truncation and fade effect

--- src/platform/packages/shared/kbn-unified-tabs/src/components/tab/tab.tsx
+++ src/platform/packages/shared/kbn-unified-tabs/src/components/tab/tab.tsx
@@ -206,8 +206,19 @@ export const Tab: React.FC<TabProps> = (props) => {
               {previewData.status === TabStatus.RUNNING && (
                 <EuiProgress size="xs" color="accent" position="absolute" />
               )}
-              <EuiText id={tabLabelId} color="inherit" size="s" css={getTabLabelCss(euiTheme)}>
-                <EuiTextTruncate text={item.label} width={MAX_TAB_WIDTH} truncation="middle" />
+              <EuiText
+                id={tabLabelId}
+                color="inherit"
+                size="s"
+                css={getTabLabelCss(euiTheme)}
+                className="unifiedTabs__tabLabelText"
+              >
+                <EuiTextTruncate
+                  text={item.label}
+                  // Truncation width must be equal to max tab width minus padding
+                  width={tabsSizeConfig.regularTabMaxWidth - euiTheme.base}
+                  truncation="middle"
+                />
               </EuiText>
             </div>
           )}
@@ -305,6 +316,14 @@ function getTabContainerCss(
       .unifiedTabs__tabLabel {
         width: calc(100% - ${euiTheme.size.l} * 2);
       }
+
+      .unifiedTabs__tabLabelText {
+        mask-image: linear-gradient(
+          to right,
+          rgb(255, 0, 0) calc(100% - ${euiTheme.size.s}),
+          rgba(255, 0, 0, 0.1) 100%
+        );
+      }
     }
 
     ${!isSelected
@@ -344,13 +363,7 @@ function getTabLabelContainerCss(euiTheme: EuiThemeComputed) {
 
 function getTabLabelCss(euiTheme: EuiThemeComputed) {
   return css`
-    padding-right: ${euiTheme.size.s};
     white-space: nowrap;
-    mask-image: linear-gradient(
-      to right,
-      rgb(255, 0, 0) calc(100% - ${euiTheme.size.s}),
-      rgba(255, 0, 0, 0.1) 100%
-    );
     transform: translateZ(0);
     overflow: hidden;
   `;
