ba30a9d346db624f3c8da99696a105ad452052b3
Sid <siddharthmantri1@gmail.com>
2025-06-12 10:03:59 +0200

    remove circular references

--- src/core/packages/saved-objects/server/src/serialization.ts
+++ src/core/packages/saved-objects/server/src/serialization.ts
@@ -8,7 +8,7 @@
  */
 
 import type { SavedObjectsMigrationVersion } from '@kbn/core-saved-objects-common';
-import type { SavedObjectAccessControl } from '@kbn/core/server';
+import type { SavedObjectAccessControl } from '@kbn/core-saved-objects-api-server';
 import type { SavedObjectReference } from '..';
 
 /**
