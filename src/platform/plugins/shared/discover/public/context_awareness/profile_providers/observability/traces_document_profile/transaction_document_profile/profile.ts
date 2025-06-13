6b7e4f56f8f53e3b40c329496b9edf1d758fe523
Katerina <aikaterini.patticha@elastic.co>
2025-06-13 15:16:11 +0300

    [Discover][Oblt] Display Attributes doc viewer tab for Observability (#222391)
    Closing https://github.com/elastic/kibana/issues/221919 
    
    
    ## Summary
    
    
    In this PR, I'm adding the Attributes doc viwer tab for the
    **Observability solution view.**
    The tab should be visible for all discover documents that have any of
    the following prefixes
    
    ```
    const attributesPrefixes = ['attributes.', 'scope.attributes.', 'resource.attributes.'];
    ```
    
    Also, 
    - Group the observability document profile providers into one file
    
    > [!IMPORTANT]  
    > The content of the tab is not inlcuded in this PR. it will be part of
    this [ticket](https://github.com/elastic/kibana/issues/221927)
    
    ## Additional changes
    
    
    For the existing profiles, logs, traces I updated the condition to check
    the solution
    
    From `params.rootContext.profileId === OBSERVABILITY_ROOT_PROFILE_ID` to
    `params.rootContext.solutionType === SolutionType.Observability`
     
     
    IMO both are  prone to errors. 
    
    - As I'm extending the root profile, the above statement is not true. it
    returns the new extended profile ID
    - Also, if we add more extensions to the root profile and we add another
    solutionType by mistake the profiles won't work
    
    I updated the type
    https://github.com/elastic/kibana/pull/222391/files#diff-ff7a53f0c234902226be3e34978326985dfffadabb8ae722b6c3fc2115085d11R14
    but it might not be enough.
    
    
    ## Test 
    #### How to generate OTel data
    - Follow
    https://github.com/smith/elastic-stack-docker-compose?tab=readme-ov-file#elastic-stack-docker-compose
    
    #### How to test
    - Make sure your solution view is Observability
    - update your `kibana.yaml` 
    
    ```
    discover.experimental.enabledProfiles:
       - observability-root-profile-with-attributes-tab
       #  if you want to test it with the additional profiles add the following to your `kibana.yaml` 
       - observability-traces-data-source-profile
       - observability-traces-transaction-document-profile
       - observability-traces-span-document-profile
    ```
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/platform/plugins/shared/discover/public/context_awareness/profile_providers/observability/traces_document_profile/transaction_document_profile/profile.ts
+++ src/platform/plugins/shared/discover/public/context_awareness/profile_providers/observability/traces_document_profile/transaction_document_profile/profile.ts
@@ -11,9 +11,8 @@ import type { DataTableRecord } from '@kbn/discover-utils';
 import { DATASTREAM_TYPE_FIELD, getFieldValue, PROCESSOR_EVENT_FIELD } from '@kbn/discover-utils';
 import { TRACES_PRODUCT_FEATURE_ID } from '../../../../../../common/constants';
 import type { DocumentProfileProvider } from '../../../../profiles';
-import { DocumentType } from '../../../../profiles';
+import { DocumentType, SolutionType } from '../../../../profiles';
 import { createGetDocViewer } from './accessors';
-import { OBSERVABILITY_ROOT_PROFILE_ID } from '../../consts';
 import type { ProfileProviderServices } from '../../../profile_provider_services';
 
 const OBSERVABILITY_TRACES_TRANSACTION_DOCUMENT_PROFILE_ID =
@@ -37,7 +36,7 @@ export const createObservabilityTracesTransactionDocumentProfileProvider = ({
     }),
   },
   resolve: ({ record, rootContext }) => {
-    const isObservabilitySolutionView = rootContext.profileId === OBSERVABILITY_ROOT_PROFILE_ID;
+    const isObservabilitySolutionView = rootContext.solutionType === SolutionType.Observability;
 
     if (!isObservabilitySolutionView) {
       return { isMatch: false };
