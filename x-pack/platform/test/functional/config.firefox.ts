fdcc75e1a6789e95d7ac6fb705aa66c4335e0009
fake-haris <charis.kalpakis@elastic.co>
2025-06-13 13:37:51 +0300

    created x-pack/platform/test/functional/

---
+++ x-pack/platform/test/functional/config.firefox.ts
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { FtrConfigProviderContext } from "@kbn/test";
+
+export default async function ({ readConfigFile }: FtrConfigProviderContext) {
+  const chromeConfig = await readConfigFile(require.resolve('./config.base.js'));
+
+  return {
+    ...chromeConfig.getAll(),
+
+    browser: {
+      type: 'firefox',
+    },
+
+    suiteTags: {
+      include: ['includeFirefox'],
+      exclude: ['skipFirefox'],
+    },
+
+    junit: {
+      reportName: 'Firefox XPack UI Functional Tests',
+    },
+  };
+}
