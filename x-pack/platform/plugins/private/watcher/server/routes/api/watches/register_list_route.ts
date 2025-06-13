f4ccab6ca3b2f9106463844a1206449137ad7bc1
Elena Stoeva <elenastoeva99@gmail.com>
2025-06-13 12:11:52 +0100

    Revert "[Watcher] Fix watches table (#218853)"
    This reverts commit d9c481799f0edb3088595d21e4d7615b1a4e12f6.

--- x-pack/platform/plugins/private/watcher/server/routes/api/watches/register_list_route.ts
+++ x-pack/platform/plugins/private/watcher/server/routes/api/watches/register_list_route.ts
@@ -5,18 +5,8 @@
  * 2.0.
  */
 
-import { schema } from '@kbn/config-schema';
-import { WatcherQueryWatchesRequest } from '@elastic/elasticsearch/lib/api/types';
 import { RouteDependencies } from '../../../types';
 import { Watch } from '../../../models/watch';
-
-const querySchema = schema.object({
-  pageSize: schema.number(),
-  pageIndex: schema.number(),
-  sortField: schema.maybe(schema.string()),
-  sortDirection: schema.maybe(schema.string()),
-  query: schema.string(),
-});
 
 export function registerListRoute({ router, license, lib: { handleEsError } }: RouteDependencies) {
   router.get(
@@ -28,51 +18,12 @@ export function registerListRoute({ router, license, lib: { handleEsError } }: R
           reason: 'Relies on es client for authorization',
         },
       },
-      validate: {
-        query: querySchema,
-      },
+      validate: false,
     },
     license.guardApiRoute(async (ctx, request, response) => {
       try {
-        const { pageSize, pageIndex, sortField, sortDirection, query } = request.query;
         const esClient = (await ctx.core).elasticsearch.client;
-        const body: WatcherQueryWatchesRequest = {
-          from: pageIndex * pageSize,
-          size: pageSize,
-        };
-        if (sortField && sortDirection) {
-          const order: 'asc' | 'desc' = sortDirection === 'desc' ? 'desc' : 'asc';
-          // The Query Watch API only allows sorting by metadata.* fields
-          body.sort = [
-            {
-              [`metadata.${sortField}.keyword`]: {
-                order,
-              },
-            },
-          ];
-        }
-        if (query) {
-          // The Query Watch API only allows searching by _id or by metadata.* fields
-          body.query = {
-            bool: {
-              should: [
-                {
-                  wildcard: {
-                    ['metadata.name.keyword']: `*${query}*`,
-                  },
-                },
-                {
-                  match: {
-                    _id: {
-                      query,
-                    },
-                  },
-                },
-              ],
-            },
-          };
-        }
-        const { watches: hits, count } = await esClient.asCurrentUser.watcher.queryWatches(body);
+        const { watches: hits } = await esClient.asCurrentUser.watcher.queryWatches();
         const watches = hits.map(({ _id, watch, status }) => {
           return Watch.fromUpstreamJson(
             {
@@ -91,7 +42,6 @@ export function registerListRoute({ router, license, lib: { handleEsError } }: R
         return response.ok({
           body: {
             watches: watches.map((watch) => watch.downstreamJson),
-            watchCount: count,
           },
         });
       } catch (e) {
