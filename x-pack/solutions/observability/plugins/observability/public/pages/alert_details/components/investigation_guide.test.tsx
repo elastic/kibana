72c81c924ed1b942ec6a128ebf21bad77f8d4a28
Justin Kambic <jk@elastic.co>
2025-06-16 13:54:46 -0400

    Fix mocks in test file.

--- x-pack/solutions/observability/plugins/observability/public/pages/alert_details/components/investigation_guide.test.tsx
+++ x-pack/solutions/observability/plugins/observability/public/pages/alert_details/components/investigation_guide.test.tsx
@@ -8,27 +8,38 @@
 import React from 'react';
 import { InvestigationGuide } from './investigation_guide';
 import { render } from '../../../utils/test_helper';
-import * as hook from '../hooks/use_edit_rule_form_flyout';
-import { fireEvent, waitFor } from '@testing-library/react';
+import * as kibana from '../../../utils/kibana_react';
+import { act, fireEvent } from '@testing-library/react';
+
+jest.mock('@kbn/response-ops-rule-form/flyout', () => {
+  return {
+    // we mock the response-ops flyout because we aren't testing it here
+    RuleFormFlyout: () => <div>Mock Flyout</div>,
+  };
+});
 
 describe('InvestigationGuide', () => {
-  let useEditRuleFormFlyoutSpy: jest.SpyInstance;
   beforeEach(() => {
-    useEditRuleFormFlyoutSpy = jest.spyOn(hook, 'useEditRuleFormFlyout');
-  });
-  afterEach(() => {
-    useEditRuleFormFlyoutSpy.mockRestore();
+    jest.spyOn(kibana, 'useKibana').mockReturnValue({
+      services: {
+        triggersActionsUi: {
+          // @ts-expect-error partial implementation for mocking
+          ruleTypeRegistry: {
+            get: jest.fn(),
+          },
+          // @ts-expect-error partial implementation for mocking
+          actionTypeRegistry: {
+            get: jest.fn(),
+          },
+        },
+      },
+    });
+    jest.clearAllMocks();
   });
 
   it('provides an empty state that will open the rule form flyout', async () => {
-    const AlertDetailsRuleFormFlyoutMock = jest.fn().mockReturnValue(<div>Test Component</div>);
-    const handleEditRuleDetailsMock = jest.fn();
-    useEditRuleFormFlyoutSpy.mockReturnValue({
-      AlertDetailsRuleFormFlyout: AlertDetailsRuleFormFlyoutMock,
-      handleEditRuleDetails: handleEditRuleDetailsMock,
-    });
     const mockRule = { id: 'mock' };
-    const { getByText, getByRole } = render(
+    const { getByRole } = render(
       <InvestigationGuide
         onUpdate={() => {}}
         refetch={() => {}}
@@ -37,35 +48,18 @@ describe('InvestigationGuide', () => {
       />
     );
 
-    // verify the "flyout" gets rendered
-    expect(getByText('Test Component'));
-
     // grab add guide button for functionality testing
     const addGuideButton = getByRole('button', { name: 'Add guide' });
     expect(addGuideButton).toBeInTheDocument();
 
     // verify that clicking the add guide button opens the flyout
-    fireEvent.click(addGuideButton);
-
-    await waitFor(() => {
-      expect(handleEditRuleDetailsMock).toHaveBeenCalledTimes(1);
-    });
-
-    expect(AlertDetailsRuleFormFlyoutMock).toHaveBeenCalledTimes(1);
-    expect(AlertDetailsRuleFormFlyoutMock.mock.calls[0][0]).toEqual({
-      initialEditStep: 'rule-details',
-    });
+    await act(() => fireEvent.click(addGuideButton));
   });
 
   it('renders the investigation guide when one is provided', async () => {
     // provide actual markdown and test it's getting rendered properly
     const mockMarkdown =
       '## This is an investigation guide\n\nCall **The team** to resolve _any issues_.\n';
-    const handleEditRuleDetailsMock = jest.fn();
-    useEditRuleFormFlyoutSpy.mockReturnValue({
-      AlertDetailsRuleFormFlyout: jest.fn().mockReturnValue(<div>Test Component</div>),
-      handleEditRuleDetails: handleEditRuleDetailsMock,
-    });
     const mockRule = { id: 'mock' };
 
     const { getByRole } = render(
