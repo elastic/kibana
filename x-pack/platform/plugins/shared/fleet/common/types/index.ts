19565a53de0f94107e7fa67a22ad6ee8d60c2904
Kylie Meli <kylie.geller@elastic.co>
2025-06-16 15:41:12 -0400

    hiding dashboard refs from integrations pages for ai4soc

--- x-pack/platform/plugins/shared/fleet/common/types/index.ts
+++ x-pack/platform/plugins/shared/fleet/common/types/index.ts
@@ -94,6 +94,7 @@ export interface FleetConfigType {
   };
   integrationsHomeOverride?: string;
   prereleaseEnabledByDefault?: boolean;
+  hideDashboards?: boolean;
 }
 
 // Calling Object.entries(PackagesGroupedByStatus) gave `status: string`
