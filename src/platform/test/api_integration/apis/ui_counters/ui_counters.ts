7538b3aad18d3a89fcf18d611c319b5b89d081d7
Bamieh <ahmad.bamyeh@elastic.co>
2025-06-16 15:03:16 -0700

    add initial delay option to retry service

--- src/platform/test/api_integration/apis/ui_counters/ui_counters.ts
+++ src/platform/test/api_integration/apis/ui_counters/ui_counters.ts
@@ -11,7 +11,6 @@ import expect from '@kbn/expect';
 import { ReportManager, METRIC_TYPE, UiCounterMetricType, Report } from '@kbn/analytics';
 import { UsageCountersSavedObject } from '@kbn/usage-collection-plugin/server';
 import { X_ELASTIC_INTERNAL_ORIGIN_REQUEST } from '@kbn/core-http-common';
-import { sleep } from '@kbn/test';
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 const APP_NAME = 'myApp';
@@ -93,7 +92,6 @@ export default function ({ getService }: FtrProviderContext) {
 
       await sendReport(report);
       // Wait for the report to be query-able in ES since sending report uses (refresh = false)
-      await sleep(3000);
       await retry.tryWithRetries(
         'reported events to be stored into ES',
         async () => {
@@ -123,7 +121,6 @@ export default function ({ getService }: FtrProviderContext) {
 
       await sendReport(report);
       // Wait for the report to be query-able in ES since sending report uses (refresh = false)
-      await sleep(3000);
       await retry.tryWithRetries(
         'reported events to be stored into ES',
         async () => {
@@ -153,7 +150,7 @@ export default function ({ getService }: FtrProviderContext) {
           expect(secondEventWithCountTypeEvents[0].attributes.count).to.eql(1);
           return true;
         },
-        { retryCount: 5, retryDelay: 1500 }
+        { retryCount: 5, retryDelay: 1500, initialDelay: 3000 }
       );
     });
   });
