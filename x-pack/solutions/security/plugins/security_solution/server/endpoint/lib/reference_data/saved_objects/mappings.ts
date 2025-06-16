7d0536317393340e4ec345e3f8d338ac06b1adb2
Paul Tavares <paul.tavares@elastic.co>
2025-06-16 16:54:32 -0400

    correct mapping of ref. data SO (bad prior merge)

--- x-pack/solutions/security/plugins/security_solution/server/endpoint/lib/reference_data/saved_objects/mappings.ts
+++ x-pack/solutions/security/plugins/security_solution/server/endpoint/lib/reference_data/saved_objects/mappings.ts
@@ -28,7 +28,6 @@ export const referenceDataSavedObjectType: SavedObjectsType = {
       id: { type: 'keyword' },
       type: { type: 'keyword' },
       owner: { type: 'keyword' },
-      metadata: { type: 'object' },
     },
   },
   modelVersions: {
