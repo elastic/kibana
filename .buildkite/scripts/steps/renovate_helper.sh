24178fb108890e399ef8a335f533bcd00a7297ee
Brad White <brad.white@elastic.co>
2025-06-16 14:59:32 -0600

    base pipeline file and script

---
+++ .buildkite/scripts/steps/renovate_helper.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source .buildkite/scripts/common/util.sh
+
+echo --- Deduplicate yarn.lock
+cmd="node scripts/yarn_deduplicate.js && yarn kbn bootstrap && node scripts/yarn_deduplicate.js"
+eval "$cmd"
+check_for_changed_files "$cmd" true
