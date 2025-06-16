194e129f42981cef1983d1a5a973ad9e69b08c6d
lgestc <lukasz.gmys@elastic.co>
2025-06-16 09:46:40 +0200

    fix type error

--- x-pack/solutions/security/plugins/security_solution/public/data_view_manager/redux/listeners/data_view_selected.test.ts
+++ x-pack/solutions/security/plugins/security_solution/public/data_view_manager/redux/listeners/data_view_selected.test.ts
@@ -10,7 +10,7 @@ import { selectDataViewAsync } from '../actions';
 import type { DataViewsServicePublic, FieldSpec } from '@kbn/data-views-plugin/public';
 import type { AnyAction, Dispatch, ListenerEffectAPI } from '@reduxjs/toolkit';
 import type { RootState } from '../reducer';
-import { DataViewManagerScopeName } from '../../constants';
+import { DataViewManagerScopeName, DEFAULT_SECURITY_SOLUTION_DATA_VIEW_ID } from '../../constants';
 
 const mockDataViewsService = {
   getDataViewLazy: jest.fn(),
@@ -66,6 +66,7 @@ const mockedState: RootState = {
       ],
       status: 'pristine',
       signalIndexName: '',
+      defaultDataViewId: DEFAULT_SECURITY_SOLUTION_DATA_VIEW_ID,
     },
   },
 };
