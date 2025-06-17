elastic/kibana

commit 3f3c8c8a4898217c3d3c9314e50e6eb46ec4b9e0

[Security Solution] Adds normalization for filter `meta` field diff (#210191)
**Fixes: https://github.com/elastic/kibana/issues/206527**
**Partially addresses: https://github.com/elastic/kibana/issues/209518**

## Summary

Adds a normalization to the `filters` field in the rule diffing
calculation that omits all filter fields other than the `query` field
and the `negate` and `disabled` fields within the `meta` object. This
makes our diffing logic much more robust and resilient as we only
compare data in the rule fields that have an impact on the query itself
and not the fields that relate to UI implementation (`alias`, `key`,
etc).

### To test

- Open a prebuilt rule with `filters` in the non-customized rule
parameters (e.g. `PowerShell Script with Discovery Capabilities`)
- Edit the rule and save without editing
- The rule should remain unmodified even though more fields have been
added to the rule's `filters` field

Unless the user adds or deletes a filter on the rule, the rule should
only be marked as customized under 3 circumstances:

- The user negates the filter (adds NOT to the beginning of the filter)
- The user disables the filter
- The user changes the filter query

All other scenarios (such as adding a custom name for the filter) should
not change the rule's customized status

### Checklist

Check the PR satisfies following conditions. 

Reviewers should verify this PR satisfies this list as well.

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/solutions/security/plugins/security_solution/common/detection_engine/prebuilt_rules/diff/extract_rule_data_query.ts
@@ -84,19 +84,21 @@ export const extractRuleEsqlQuery = (
 };
 
 /**
- * Removes the null `alias` field that gets appended from the internal kibana filter util for comparison
- * Relevant issue: https://github.com/elastic/kibana/issues/202966
+ * Normalizes filter properties to only include ones relevant to the query itself
+ * Relevant issues:
+ *  - https://github.com/elastic/kibana/issues/202966
+ *  - https://github.com/elastic/kibana/issues/206527
  */
 const normalizeFilterArray = (filters: RuleFilterArray | undefined): RuleFilterArray => {
   if (!filters?.length) {
     return [];
   }
   return (filters as Filter[]).map((filter) => ({
-    ...filter,
+    query: filter.query,
     meta: filter.meta
       ? {
-          ...filter.meta,
-          alias: filter.meta.alias ?? undefined,
+          negate: filter.meta.negate,
+          disabled: filter.meta.disabled !== undefined ? filter.meta.disabled : false,
         }
       : undefined,
   }));

.github/CODEOWNERS
@@ -2396,6 +2396,7 @@ x-pack/test/security_solution_cypress/cypress/tasks/expandable_flyout  @elastic/
 /x-pack/solutions/security/plugins/security_solution/common/api/detection_engine/prebuilt_rules @elastic/security-detection-rule-management
 /x-pack/solutions/security/plugins/security_solution/common/api/detection_engine/rule_management @elastic/security-detection-rule-management
 /x-pack/solutions/security/plugins/security_solution/common/api/detection_engine/rule_monitoring @elastic/security-detection-rule-management
+/x-pack/solutions/security/plugins/security_solution/common/detection_engine/prebuilt_rules @elastic/security-detection-rule-management
 /x-pack/solutions/security/plugins/security_solution/common/detection_engine/rule_management @elastic/security-detection-rule-management
 
 /x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_management @elastic/security-detection-rule-management

x-pack/solutions/security/plugins/security_solution/common/detection_engine/prebuilt_rules/diff/extract_rule_data_query.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { Filter } from '@kbn/es-query';
+import { FilterStateStore, type Filter } from '@kbn/es-query';
 import { KqlQueryType } from '../../../api/detection_engine';
 import {
   extractRuleEqlQuery,
@@ -29,6 +29,9 @@ const mockFilter: Filter = {
       field: 'value',
     },
   },
+  $state: {
+    store: FilterStateStore.APP_STATE,
+  },
 };
 
 describe('extract rule data queries', () => {
@@ -44,36 +47,107 @@ describe('extract rule data queries', () => {
       });
     });
 
-    it('normalizes filters', () => {
-      const extractedKqlQuery = extractRuleKqlQuery(
-        'event.kind:alert',
-        'kuery',
-        [mockFilter],
-        undefined
-      );
+    describe('filters normalization', () => {
+      it('normalizes filters[].query when all fields present', () => {
+        const extractedKqlQuery = extractRuleKqlQuery(
+          'some:query',
+          'kuery',
+          [mockFilter],
+          undefined
+        );
 
-      expect(extractedKqlQuery).toEqual({
-        type: KqlQueryType.inline_query,
-        query: 'event.kind:alert',
-        language: 'kuery',
-        filters: [
-          {
-            meta: {
-              negate: false,
-              disabled: false,
-              type: 'phrase',
-              key: 'test',
-              params: {
-                query: 'value',
+        expect(extractedKqlQuery).toMatchObject({
+          filters: [
+            {
+              query: {
+                term: {
+                  field: 'value',
+                },
               },
             },
-            query: {
-              term: {
-                field: 'value',
-              },
+          ],
+        });
+      });
+
+      it('normalizes filters[].query when query object is missing', () => {
+        const extractedKqlQuery = extractRuleKqlQuery(
+          'some:query',
+          'kuery',
+          [{ ...mockFilter, query: undefined }],
+          undefined
+        );
+
+        expect(extractedKqlQuery).not.toMatchObject({
+          filters: [
+            {
+              query: expect.anything(),
             },
+          ],
+        });
+      });
+
+      it.each([
+        {
+          caseName: 'when all fields present',
+          filter: mockFilter,
+          expectedFilterMeta: {
+            negate: false,
+            disabled: false,
           },
-        ],
+        },
+        {
+          caseName: 'when disabled field is missing',
+          filter: { ...mockFilter, meta: { ...mockFilter.meta, disabled: undefined } },
+          expectedFilterMeta: {
+            negate: false,
+            disabled: false,
+          },
+        },
+        {
+          caseName: 'when negate field is missing',
+          filter: { ...mockFilter, meta: { ...mockFilter.meta, negate: undefined } },
+          expectedFilterMeta: {
+            disabled: false,
+          },
+        },
+        {
+          caseName: 'when query object is missing',
+          filter: { ...mockFilter, query: undefined },
+          expectedFilterMeta: {
+            negate: false,
+            disabled: false,
+          },
+        },
+      ])('normalizes filters[].meta $caseName', ({ filter, expectedFilterMeta }) => {
+        const extractedKqlQuery = extractRuleKqlQuery('some:query', 'kuery', [filter], undefined);
+
+        expect(extractedKqlQuery).toMatchObject({
+          filters: [
+            {
+              meta: expectedFilterMeta,
+            },
+          ],
+        });
+      });
+
+      it('normalizes filters[].meta when query object is missing', () => {
+        const extractedKqlQuery = extractRuleKqlQuery(
+          'some:query',
+          'kuery',
+          [{ ...mockFilter, query: undefined }],
+          undefined
+        );
+
+        expect(extractedKqlQuery).toMatchObject({
+          filters: [
+            {
+              meta: {
+                negate: false,
+                disabled: false,
+              },
+            },
+          ],
+        });
       });
     });
   });

commit f0829b7c1179ef998380153ad7dd5b6a3bee6ea2

[Security Solution] Fix rule filters display issues (#222963)
**Resolves: https://github.com/elastic/kibana/issues/203615**
**Resolves: https://github.com/elastic/kibana/issues/203600**

## Summary
This PR resolves a few bugs related to how query filters are displayed:
- fixes display of filters with "AND"/"OR" logic in view mode of rule
upgrade flyout (bug introduced in PR #210191)
 - fixes warnings shown in edit mode of rule upgrade flyout
- fixes warning flashes on rule edit page when updating the index
pattern

## Changes

### Display of filters with "AND"/"OR" logic in view mode of rule
upgrade flyout
Caused by change in recent [PR](#210191) where we stopped responding
with a full filter object.

<details>
  <summary>Click to see screenshots</summary>
  **Before**
<img width="951" alt="broken_filters_flyout_view"
src="https://github.com/user-attachments/assets/3b06feb6-595f-4140-a795-3ac7a3d81fed"
/>

  **After**
<img width="948" alt="fixed_filters_flyout_view"
src="https://github.com/user-attachments/assets/6a59c2a5-7165-4e4e-9a2f-8cb4250125a0"
/>
</details>

### Warnings shown in edit mode of rule upgrade flyout
Caused by a mismatch between index patterns in filters and data view
objects. We path both filters and a data view into a unified search
component. If it detects a mismatch, it shows a confusing warning.

<details>
  <summary>Click to see screenshots</summary>
  **Before**
<img width="1265" alt="broken_filters_flyout_edit"
src="https://github.com/user-attachments/assets/947be729-a247-4eee-8046-155c2599e0bd"
/>

  **After**
<img width="1265" alt="fixed_filters_flyout_edit"
src="https://github.com/user-attachments/assets/7bec19bc-5180-47f1-876e-850677d1503d"
/>
</details>

### Warning flashes on rule edit page when updating the index pattern
Caused by a re-render with an incomplete DataView object.

<details>
  <summary>Click to see screen recordings</summary>
  **Before**

x-pack/solutions/security/plugins/security_solution/common/detection_engine/prebuilt_rules/diff/extract_rule_data_query.ts
@@ -99,6 +99,11 @@ const normalizeFilterArray = (filters: RuleFilterArray | undefined): RuleFilterA
       ? {
           negate: filter.meta.negate,
           disabled: filter.meta.disabled !== undefined ? filter.meta.disabled : false,
+          params: filter.meta.params,
+          relation: 'relation' in filter.meta ? filter.meta?.relation : undefined,
+          type: filter.meta.type ?? 'custom',
+          alias: filter.meta.alias ?? undefined,
+          key: filter.meta.key ?? undefined,
         }
       : undefined,
   }));

x-pack/solutions/security/plugins/security_solution/public/common/components/query_bar/index.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { cloneDeep, isEmpty } from 'lodash';
+import { isEmpty } from 'lodash';
 import type { PropsWithChildren, FC } from 'react';
 import React, { memo, useMemo, useCallback, useState, useEffect } from 'react';
 import deepEqual from 'fast-deep-equal';
@@ -20,6 +20,7 @@ import { Storage } from '@kbn/kibana-utils-plugin/public';
 import { css, Global } from '@emotion/react';
 import { useKibana } from '../../lib/kibana';
 import { convertToQueryType } from './convert_to_query_type';
+import { matchFiltersToIndexPattern } from './match_filters_to_index_pattern';
 
 export interface QueryBarComponentProps {
   dataTestSubj?: string;
@@ -168,18 +169,12 @@ export const QueryBar = memo<QueryBarComponentProps>(
     }, [data.dataViews, indexPattern, isEsql, preventCacheClearOnUnmount]);
 
     const searchBarFilters = useMemo(() => {
-      if (isDataView(indexPattern) || isEsql) {
+      if (!dataView?.id || isEsql) {
         return filters;
       }
 
-      /**
-       * We update filters and set new data view id to make sure that SearchBar does not show data view picker
-       * More details in https://github.com/elastic/kibana/issues/174026
-       */
-      const updatedFilters = cloneDeep(filters);
-      updatedFilters.forEach((filter) => (filter.meta.index = indexPattern.title));
-      return updatedFilters;
-    }, [filters, indexPattern, isEsql]);
+      return matchFiltersToIndexPattern(dataView.id, filters);
+    }, [filters, isEsql, dataView?.id]);
 
     const timeHistory = useMemo(() => new TimeHistory(new Storage(localStorage)), []);
     const arrDataView = useMemo(() => (dataView != null ? [dataView] : []), [dataView]);

x-pack/solutions/security/plugins/security_solution/public/common/components/query_bar/match_filters_to_index_pattern.ts
@@ -1,0 +1,36 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { Filter } from '@kbn/es-query';
+import { cloneDeep } from 'lodash';
+
+/**
+ * Updates each filter's `meta.index` property to match the specified index pattern.
+ *
+ * This ensures compatibility with the filters UI component, which requires `meta.index`
+ * to match the current index pattern. If it doesn't, the UI will:
+ *  - Display a vague warning: "Field does not exist in current view"
+ *    (see: https://github.com/elastic/kibana/issues/178908)
+ *  - Display the word "warning" instead of an actual value for filters with "AND" or "OR" operators
+ *    (see: https://github.com/elastic/kibana/issues/203615)
+ *  - Show an unnecessary data view selector in the filter editor
+ *    (see: https://github.com/elastic/kibana/pull/174922)
+ *
+ * To prevent this confusing behavior, we explicitly set `meta.index` on each filter
+ * to match the provided index pattern string.
+ *
+ * @param indexPattern Index pattern string to associate the filters with.
+ * @param filters An array of filters to update.
+ * @returns A new array of filters with the updated index property.
+ */
+export function matchFiltersToIndexPattern(indexPattern: string, filters: Filter[]): Filter[] {
+  const updatedFilters = cloneDeep(filters);
+  updatedFilters.forEach((filter) => {
+    filter.meta.index = indexPattern;
+  });
+  return updatedFilters;
+}

x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_creation_ui/components/threatmatch_input/index.tsx
@@ -1,119 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import React, { memo, useCallback, useEffect, useState } from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiSpacer, EuiFormRow } from '@elastic/eui';
-import type { DataViewBase } from '@kbn/es-query';
-import type { ThreatMapEntries } from '../../../../common/components/threat_match/types';
-import { ThreatMatchComponent } from '../../../../common/components/threat_match';
-import type { FieldHook } from '../../../../shared_imports';
-import { UseField, getFieldValidityAndErrorMessage } from '../../../../shared_imports';
-import { schema } from '../step_define_rule/schema';
-import { QueryBarField } from '../query_bar_field';
-
-interface ThreatMatchEditProps {
-  path: string;
-  threatIndexPatterns: DataViewBase;
-  indexPatterns: DataViewBase;
-  threatIndexPatternsLoading: boolean;
-  onValidityChange?: (isValid: boolean) => void;
-}
-
-export const ThreatMatchEdit = memo(function ThreatMatchEdit({
-  path,
-  indexPatterns,
-  threatIndexPatterns,
-  threatIndexPatternsLoading,
-  onValidityChange,
-}: ThreatMatchEditProps): JSX.Element {
-  const componentProps = {
-    indexPatterns,
-    threatIndexPatterns,
-    threatIndexPatternsLoading,
-    onValidityChange,
-  };
-
-  return <UseField path={path} component={ThreatMatchField} componentProps={componentProps} />;
-});
-
-interface ThreatMatchFieldProps {
-  field: FieldHook<ThreatMapEntries[]>;
-  threatIndexPatterns: DataViewBase;
-  indexPatterns: DataViewBase;
-  threatIndexPatternsLoading: boolean;
-  onValidityChange?: (isValid: boolean) => void;
-}
-
-function ThreatMatchField({
-  field,
-  threatIndexPatterns,
-  indexPatterns,
-  threatIndexPatternsLoading,
-  onValidityChange,
-}: ThreatMatchFieldProps): JSX.Element {
-  const { isInvalid: isThreatMappingInvalid, errorMessage } =
-    getFieldValidityAndErrorMessage(field);
-  const [isThreatIndexPatternValid, setIsThreatIndexPatternValid] = useState(false);
-
-  useEffect(() => {
-    if (onValidityChange) {
-      onValidityChange(!isThreatMappingInvalid && isThreatIndexPatternValid);
-    }
-  }, [isThreatIndexPatternValid, isThreatMappingInvalid, onValidityChange]);
-
-  const handleMappingsEntryChange = useCallback(
-    (newEntryItems: ThreatMapEntries[]): void => {
-      field.setValue(newEntryItems);
-    },
-    [field]
-  );
-
-  return (
-    <>
-      <EuiFlexGroup direction="column">
-        <EuiFlexItem grow>
-          <UseField
-            path="threatQueryBar"
-            config={{
-              ...schema.threatQueryBar,
-              labelAppend: null,
-            }}
-            component={QueryBarField}
-            componentProps={{
-              idAria: 'detectionEngineStepDefineThreatRuleQueryBar',
-              indexPattern: threatIndexPatterns,
-              isDisabled: false,
-              isLoading: threatIndexPatternsLoading,
-              dataTestSubj: 'detectionEngineStepDefineThreatRuleQueryBar',
-              openTimelineSearch: false,
-              onValidityChange: setIsThreatIndexPatternValid,
-            }}
-          />
-        </EuiFlexItem>
-      </EuiFlexGroup>
-      <EuiSpacer size="m" />
-      <EuiFormRow
-        label={field.label}
-        labelAppend={field.labelAppend}
-        helpText={field.helpText}
-        error={errorMessage}
-        isInvalid={isThreatMappingInvalid}
-        fullWidth
-      >
-        <ThreatMatchComponent
-          mappingEntries={field.value}
-          indexPatterns={indexPatterns}
-          threatIndexPatterns={threatIndexPatterns}
-          data-test-subj="threatmatch-builder"
-          id-aria="threatmatch-builder"
-          onMappingEntriesChange={handleMappingsEntryChange}
-        />
-      </EuiFormRow>
-      <EuiSpacer size="m" />
-    </>
-  );
-}

x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/rule_definition_section.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { useMemo } from 'react';
+import React from 'react';
 import { isEmpty } from 'lodash/fp';
 import type { EuiDescriptionListProps } from '@elastic/eui';
 import {
@@ -27,7 +27,6 @@ import type { SavedQuery } from '@kbn/data-plugin/public';
 import { mapAndFlattenFilters } from '@kbn/data-plugin/public';
 import { FilterItems } from '@kbn/unified-search-plugin/public';
 import useToggle from 'react-use/lib/useToggle';
-import { isDataView } from '../../../../common/components/query_bar';
 import type {
   AlertSuppressionMissingFieldsStrategy,
   EqlOptionalFields,
@@ -68,6 +67,7 @@ import {
   EQL_OPTIONS_EVENT_TIMESTAMP_FIELD_LABEL,
 } from '../../../rule_creation/components/eql_query_edit/translations';
 import { useDataView } from './three_way_diff/final_edit/fields/hooks/use_data_view';
+import { matchFiltersToIndexPattern } from '../../../../common/components/query_bar/match_filters_to_index_pattern';
 
 interface SavedQueryNameProps {
   savedQueryName: string;
@@ -97,29 +97,16 @@ export const Filters = ({
     ? { dataViewId }
     : { indexPatterns: index ?? defaultIndexPattern };
   const { dataView } = useDataView(useDataViewParams);
+
   const isEsql = filters.some((filter) => filter?.query?.language === 'esql');
-  const searchBarFilters = useMemo(() => {
-    if (!index || isDataView(index) || isEsql) {
-      return filters;
-    }
-    const filtersWithUpdatedMetaIndex = filters.map((filter) => {
-      return {
-        ...filter,
-        meta: {
-          ...filter.meta,
-          index: index.join(','),
-        },
-      };
-    });
 
-    return filtersWithUpdatedMetaIndex;
-  }, [filters, index, isEsql]);
-
-  if (!dataView) {
+  if (!dataView?.id || isEsql) {
     return null;
   }
 
-  const flattenedFilters = mapAndFlattenFilters(searchBarFilters);
+  const flattenedFilters = mapAndFlattenFilters(filters);
+  const matchedFilters = matchFiltersToIndexPattern(dataView.id, flattenedFilters);
+
   const styles = filtersStyles;
 
   return (
@@ -130,7 +117,7 @@ export const Filters = ({
       responsive={false}
       gutterSize="xs"
     >
-      <FilterItems filters={flattenedFilters} indexPatterns={[dataView]} readOnly />
+      <FilterItems filters={matchedFilters} indexPatterns={[dataView]} readOnly />
     </EuiFlexGroup>
   );
 };