060f86f24a0a3b89d780c172001d9abc60fd26b2
Dennis Tismenko <dennis.tismenko@elastic.co>
2025-06-17 16:52:04 -0400

    Add tools page, show registered base tools

---
+++ x-pack/platform/plugins/shared/onechat/public/application/pages/chat.tsx
@@ -0,0 +1,13 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+
+export const OnechatChatPage = () => {
+  // TODO: Integrate with Chat UI: https://github.com/elastic/search-team/issues/10045
+  return <div>OnechatChatPage</div>;
+};
---
+++ x-pack/platform/plugins/shared/onechat/public/application/pages/tools.tsx
@@ -0,0 +1,15 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { useBreadcrumb } from '../hooks/use_breadcrumbs';
+import { OnechatTools } from '../components/tools/tools';
+
+export const OnechatStackToolsPage = () => {
+  useBreadcrumb([{ text: 'Analyze' }, { text: 'Chat' }, { text: 'Tools' }]);
+  return <OnechatTools />;
+};
