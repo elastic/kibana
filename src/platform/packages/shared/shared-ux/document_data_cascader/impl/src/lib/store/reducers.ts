6589654a9bff206d38aed9a67245101d5145af4b
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-06-17 17:20:10 +0200

    start on making cascade work for an infinitely nested data grouping

--- src/platform/packages/shared/shared-ux/document_data_cascader/impl/src/lib/store/reducers.ts
+++ src/platform/packages/shared/shared-ux/document_data_cascader/impl/src/lib/store/reducers.ts
@@ -7,86 +7,121 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
+import { produce } from 'immer';
+
+/**
+ * Represents a group node document, with a base expectation that specifies
+ * that all documents in the store have a unique identifier.
+ */
+export type GroupNode = Record<string, any> & { id: string; children?: GroupNode[] };
+
+export type LeafNode = Record<string, any> & { id: string };
+
 type DispatchActionType =
   | 'UPDATE_QUERY'
   | 'SET_GROUP_BY_COLUMN'
   | 'SET_INITIAL_STATE'
-  | 'EMPTY_GROUP_BY_COLUMN_SELECTION'
-  | 'UPDATE_ROW_DATA';
+  | 'RESET_GROUP_BY_COLUMN_SELECTION'
+  | 'UPDATE_ROW_GROUP_NODE_DATA'
+  | 'UPDATE_ROW_GROUP_LEAF_DATA';
 
-export type IDispatchAction =
+export type IDispatchAction<G extends GroupNode, L extends Record<string, any>> =
   | {
       type: Extract<DispatchActionType, 'UPDATE_QUERY'>;
       payload: string;
     }
   | {
       type: Extract<DispatchActionType, 'SET_GROUP_BY_COLUMN'>;
-      payload: string | null;
+      payload: string[];
     }
   | {
       type: Extract<DispatchActionType, 'SET_INITIAL_STATE'>;
-      payload: Array<Record<string, any>>;
+      payload: G[];
     }
   | {
-      type: Extract<DispatchActionType, 'EMPTY_GROUP_BY_COLUMN_SELECTION'>;
-      payload?: string;
+      type: Extract<DispatchActionType, 'RESET_GROUP_BY_COLUMN_SELECTION'>;
     }
   | {
-      type: Extract<DispatchActionType, 'UPDATE_ROW_DATA'>;
+      type: Extract<DispatchActionType, 'UPDATE_ROW_GROUP_NODE_DATA'>;
       payload: {
         id: string;
-        data: Record<string, any>;
+        data: G[];
+      };
+    }
+  | {
+      type: Extract<DispatchActionType, 'UPDATE_ROW_GROUP_LEAF_DATA'>;
+      payload: {
+        cacheKey: string;
+        data: L[];
       };
     };
 
-/**
- * Represents a document with an ID field.
- * Defines a base expectation to ensure that all documents in the store have a unique identifier.
- */
-export type DocWithId = Record<string, any> & { id: string; children?: DocWithId[] };
-
-export interface IStoreState<T extends DocWithId> {
-  data: T[];
+export interface IStoreState<G extends GroupNode, L extends LeafNode> {
+  groupNodes: G[];
+  leafNodes: Map<string, L[]>;
   currentQueryString: string;
-  groupByColumns: string[] | null;
-  currentGroupByColumn: string | null;
+  /**
+   * The available columns that can be used to group the data.
+   */
+  groupByColumns: string[];
+  /**
+   * The currently selected group by column. in the order in which they are nested
+   */
+  currentGroupByColumns: string[];
 }
 
-export const storeReducer = <T extends DocWithId = DocWithId>(
-  state: IStoreState<T>,
-  action: IDispatchAction
+export const storeReducer = <G extends GroupNode = GroupNode, L extends LeafNode = LeafNode>(
+  state: IStoreState<G, L>,
+  action: IDispatchAction<G, L>
 ) => {
   switch (action.type) {
-    case 'UPDATE_QUERY':
-      return {
-        ...state,
-        query: action.payload,
-      };
-    case 'SET_GROUP_BY_COLUMN':
-      return {
-        ...state,
-        currentGroupByColumn: action.payload,
-      };
-    case 'SET_INITIAL_STATE':
-      return {
-        ...state,
-        data: action.payload,
-      };
-    case 'EMPTY_GROUP_BY_COLUMN_SELECTION':
-      return {
-        ...state,
-        currentGroupByColumn: state.groupByColumns ? state?.groupByColumns[0] : null,
-      };
-    case 'UPDATE_ROW_DATA': {
-      return {
-        ...state,
-        data: state.data.map((row) => {
-          if (row.id === action.payload.id) {
-            return { ...row, children: action.payload.data || [] };
+    case 'UPDATE_QUERY': {
+      return produce(state, (draft) => {
+        draft.currentQueryString = action.payload;
+        return draft;
+      });
+    }
+    case 'SET_INITIAL_STATE': {
+      return produce(state, (draft) => {
+        draft.groupNodes = [...action.payload];
+        return draft;
+      });
+    }
+    case 'SET_GROUP_BY_COLUMN': {
+      return produce(state, (draft) => {
+        draft.currentGroupByColumns = action.payload;
+        return draft;
+      });
+    }
+    case 'RESET_GROUP_BY_COLUMN_SELECTION': {
+      return produce(state, (draft) => {
+        draft.currentGroupByColumns = state.groupByColumns.length ? [state.groupByColumns[0]] : [];
+        return draft;
+      });
+    }
+    case 'UPDATE_ROW_GROUP_NODE_DATA': {
+      return produce(state, (draft) => {
+        const stack: GroupNode[] = Array.isArray(draft.groupNodes)
+          ? [...draft.groupNodes]
+          : [draft.groupNodes];
+        while (stack.length > 0) {
+          const node = stack.pop()!;
+          if (node.id === action.payload.id) {
+            node.children = Array.isArray(action.payload.data) ? action.payload.data : [];
+            break; // Early exit after update
           }
-          return row;
-        }),
-      };
+          if (node.children && node.children.length > 0) {
+            stack.push(...node.children);
+          }
+        }
+      });
+    }
+    case 'UPDATE_ROW_GROUP_LEAF_DATA': {
+      return produce(state, (draft) => {
+        const { cacheKey, data } = action.payload;
+        draft.leafNodes.set(cacheKey, data);
+        return draft;
+      });
     }
     default:
       return state;
