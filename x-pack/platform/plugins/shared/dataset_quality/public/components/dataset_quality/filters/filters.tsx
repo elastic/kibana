caf68a480ff67786e873994f78a2cb0c227acc6e
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-06-17 10:55:53 +0200

    Add Tests

--- x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality/filters/filters.tsx
+++ x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality/filters/filters.tsx
@@ -72,7 +72,7 @@ export default function Filters() {
         />
         {isDatasetQualityAllSignalsAvailable && (
           <Selector
-            dataTestSubj="datasetQualityFilterType"
+            data-test-subj="datasetQualityFilterType"
             label={typesLabel}
             searchPlaceholder={typesSearchPlaceholder}
             noneMatchingMessage={typesNoneMatching}
