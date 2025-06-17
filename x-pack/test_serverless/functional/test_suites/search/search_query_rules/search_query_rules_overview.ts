ebd740e828d27cb53107f07f81ce4b7be81af2d9
José Luis González <joseluisgj@gmail.com>
2025-06-17 15:19:48 +0200

    Removing unnused browser

--- x-pack/test_serverless/functional/test_suites/search/search_query_rules/search_query_rules_overview.ts
+++ x-pack/test_serverless/functional/test_suites/search/search_query_rules/search_query_rules_overview.ts
@@ -15,7 +15,6 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
     'embeddedConsole',
     'common',
   ]);
-  const browser = getService('browser');
   const es = getService('es');
   const kibanaServer = getService('kibanaServer');
 
