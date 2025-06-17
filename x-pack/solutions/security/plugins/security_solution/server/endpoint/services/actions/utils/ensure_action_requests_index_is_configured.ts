c17446db48330cca3307c0097158037d43af8c2f
Paul Tavares <paul.tavares@elastic.co>
2025-06-17 08:36:51 -0400

    Feedback from code review

--- x-pack/solutions/security/plugins/security_solution/server/endpoint/services/actions/utils/ensure_action_requests_index_is_configured.ts
+++ x-pack/solutions/security/plugins/security_solution/server/endpoint/services/actions/utils/ensure_action_requests_index_is_configured.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import type { MappingPropertyBase } from '@elastic/elasticsearch/lib/api/types';
 import { stringify } from '../../../utils/stringify';
 import type { EndpointAppContextService } from '../../../endpoint_app_context_services';
 import { ENDPOINT_ACTIONS_INDEX } from '../../../../../common/endpoint/constants';
@@ -34,140 +35,127 @@ export const ensureActionRequestsIndexIsConfigured = async (
     logger.debug(`Index for [${ENDPOINT_ACTIONS_INDEX}] already exists`);
   });
 
-  if (isSpacesEnabled) {
+  if (!isSpacesEnabled) {
+    logger.debug(`Space awareness feature is disabled. Nothing to do.`);
+    return;
+  }
+
+  logger.debug(
+    `Checking field mappings for index [${ENDPOINT_ACTIONS_INDEX}] in support of space awareness`
+  );
+
+  const agentPolicyMapping = await esClient.indices
+    .getFieldMapping({
+      index: ENDPOINT_ACTIONS_INDEX,
+      fields: 'agent.policy.integrationPolicyId',
+    })
+    .catch(catchAndWrapError);
+
+  logger.debug(
+    () =>
+      `Index [${ENDPOINT_ACTIONS_INDEX}] existing mapping for [agent.policy.integrationPolicyId]: ${stringify(
+        agentPolicyMapping
+      )}`
+  );
+
+  const newAgentPolicyMappings: MappingPropertyBase['properties'] = {
+    agentId: { type: 'keyword', ignore_above: 1024 },
+    elasticAgentId: { type: 'keyword', ignore_above: 1024 },
+    integrationPolicyId: { type: 'keyword', ignore_above: 1024 },
+    agentPolicyId: { type: 'keyword', ignore_above: 1024 },
+  };
+  const backingIndexName = Object.keys(agentPolicyMapping)[0];
+
+  if (!agentPolicyMapping[backingIndexName].mappings['agent.policy.integrationPolicyId']) {
     logger.debug(
-      `Checking field mappings for index [${ENDPOINT_ACTIONS_INDEX}] in support of space awareness`
+      `adding mappings to index [${ENDPOINT_ACTIONS_INDEX}] - Endpoint package v9.1.x not yet installed`
     );
 
-    const agentPolicyMapping = await esClient.indices
-      .getFieldMapping({
+    await esClient.indices
+      .putMapping({
         index: ENDPOINT_ACTIONS_INDEX,
-        fields: 'agent.policy.integrationPolicyId',
-      })
-      .catch(catchAndWrapError);
-
-    logger.debug(
-      () =>
-        `Index [${ENDPOINT_ACTIONS_INDEX}] existing mapping for [agent.policy.integrationPolicyId]: ${stringify(
-          agentPolicyMapping
-        )}`
-    );
-
-    const backingIndexName = Object.keys(agentPolicyMapping)[0];
-
-    if (!agentPolicyMapping[backingIndexName].mappings['agent.policy.integrationPolicyId']) {
-      logger.debug(
-        `adding mappings to index [${ENDPOINT_ACTIONS_INDEX}] - Endpoint package v9.1.x not yet installed`
-      );
-
-      await esClient.indices
-        .putMapping({
-          index: ENDPOINT_ACTIONS_INDEX,
-          properties: {
-            originSpaceId: { type: 'keyword', ignore_above: 1024 },
-            agent: {
-              properties: {
-                policy: {
-                  properties: {
-                    agentId: { type: 'keyword', ignore_above: 1024 },
-                    elasticAgentId: { type: 'keyword', ignore_above: 1024 },
-                    integrationPolicyId: { type: 'keyword', ignore_above: 1024 },
-                    agentPolicyId: { type: 'keyword', ignore_above: 1024 },
-                  },
-                },
+        properties: {
+          originSpaceId: { type: 'keyword', ignore_above: 1024 },
+          agent: {
+            properties: {
+              policy: {
+                properties: newAgentPolicyMappings,
               },
             },
           },
+        },
+      })
+      .catch(catchAndWrapError);
+
+    logger.info(
+      `Index [${ENDPOINT_ACTIONS_INDEX}] was updated with new field mappings in support of space awareness`
+    );
+  } else {
+    logger.debug(
+      `Nothing to do - index [${ENDPOINT_ACTIONS_INDEX}] already has required mappings in support of space awareness`
+    );
+  }
+
+  const componentTemplateExists = await esClient.cluster
+    .existsComponentTemplate({ name: COMPONENT_TEMPLATE_NAME })
+    .catch(catchAndWrapError);
+
+  if (componentTemplateExists) {
+    logger.debug(
+      `Checking component template [${COMPONENT_TEMPLATE_NAME}] to ensure it has required mappings`
+    );
+
+    const currentComponentTemplate = await esClient.cluster.getComponentTemplate({
+      name: COMPONENT_TEMPLATE_NAME,
+    });
+
+    logger.debug(() => `current component template: ${stringify(currentComponentTemplate, 15)}`);
+
+    const componentMappings =
+      currentComponentTemplate.component_templates[0]?.component_template.template.mappings;
+
+    if (
+      componentMappings &&
+      componentMappings.properties &&
+      !componentMappings.properties.originSpaceId
+    ) {
+      logger.debug(`Adding mappings to component template [${COMPONENT_TEMPLATE_NAME}]`);
+
+      componentMappings.properties.originSpaceId = { type: 'keyword', ignore_above: 1024 };
+
+      if (
+        componentMappings.properties.agent &&
+        'properties' in componentMappings.properties.agent
+      ) {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        componentMappings.properties.agent.properties!.policy = {
+          properties: newAgentPolicyMappings,
+        };
+      }
+
+      logger.debug(
+        () =>
+          `Updating component template [${COMPONENT_TEMPLATE_NAME}] with:${stringify(
+            currentComponentTemplate,
+            15
+          )}`
+      );
+
+      await esClient.cluster
+        .putComponentTemplate({
+          name: COMPONENT_TEMPLATE_NAME,
+          template: { mappings: componentMappings },
         })
         .catch(catchAndWrapError);
 
       logger.info(
-        `Index [${ENDPOINT_ACTIONS_INDEX}] was updated with new field mappings in support of space awareness`
-      );
-    } else {
-      logger.debug(
-        `Nothing to do - index [${ENDPOINT_ACTIONS_INDEX}] already has required mappings in support of space awareness`
+        `Component template [${COMPONENT_TEMPLATE_NAME}] was updated with mappings in support of space awareness`
       );
     }
-
-    const componentTemplateExits = await esClient.cluster
-      .existsComponentTemplate({ name: COMPONENT_TEMPLATE_NAME })
-      .catch(catchAndWrapError);
-
-    if (componentTemplateExits) {
-      logger.debug(
-        `Checking component template [${COMPONENT_TEMPLATE_NAME}] to ensure it has required mappings`
-      );
-
-      const currentComponentTemplate = await esClient.cluster.getComponentTemplate({
-        name: COMPONENT_TEMPLATE_NAME,
-      });
-
-      logger.debug(() => `current component template: ${stringify(currentComponentTemplate, 15)}`);
-
-      const componentMappings =
-        currentComponentTemplate.component_templates[0]?.component_template.template.mappings;
-
-      if (
-        componentMappings &&
-        componentMappings.properties &&
-        !componentMappings.properties.originSpaceId
-      ) {
-        logger.debug(`Adding mappings to component template [${COMPONENT_TEMPLATE_NAME}]`);
-
-        componentMappings.properties.originSpaceId = { type: 'keyword', ignore_above: 1024 };
-
-        if (
-          componentMappings.properties.agent &&
-          'properties' in componentMappings.properties.agent
-        ) {
-          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-          componentMappings.properties.agent.properties!.policy = {
-            properties: {
-              agentId: {
-                type: 'keyword',
-                ignore_above: 1024,
-              },
-              elasticAgentId: {
-                type: 'keyword',
-                ignore_above: 1024,
-              },
-              integrationPolicyId: {
-                type: 'keyword',
-                ignore_above: 1024,
-              },
-              agentPolicyId: {
-                type: 'keyword',
-                ignore_above: 1024,
-              },
-            },
-          };
-        }
-
-        logger.debug(
-          () =>
-            `Updating component template [${COMPONENT_TEMPLATE_NAME}] with:${stringify(
-              currentComponentTemplate,
-              15
-            )}`
-        );
-
-        await esClient.cluster
-          .putComponentTemplate({
-            name: COMPONENT_TEMPLATE_NAME,
-            template: { mappings: componentMappings },
-          })
-          .catch(catchAndWrapError);
-
-        logger.info(
-          `Component template [${COMPONENT_TEMPLATE_NAME}] was updated with mappings in support of space awareness`
-        );
-      }
-    } else {
-      logger.debug(
-        `Component template [${COMPONENT_TEMPLATE_NAME}] does not exist. Nothing to check`
-      );
-    }
+  } else {
+    logger.debug(
+      `Component template [${COMPONENT_TEMPLATE_NAME}] does not exist. Nothing to check`
+    );
   }
 
   logger.debug(`Checking of index [${ENDPOINT_ACTIONS_INDEX}] done.`);
