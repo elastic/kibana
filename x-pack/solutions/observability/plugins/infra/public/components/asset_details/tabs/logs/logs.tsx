77a0656f64f8e11cc844bb0e8c740866f19dadce
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2025-06-18 16:00:28 +0200

    Fix height for logs tab in infra

--- x-pack/solutions/observability/plugins/infra/public/components/asset_details/tabs/logs/logs.tsx
+++ x-pack/solutions/observability/plugins/infra/public/components/asset_details/tabs/logs/logs.tsx
@@ -151,7 +151,7 @@ export const Logs = () => {
             index={logSources.value}
             timeRange={dateRange}
             query={filter}
-            height="60vh"
+            height="68vh"
             displayOptions={{
               solutionNavIdOverride: 'oblt',
               enableDocumentViewer: false,
