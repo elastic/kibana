deef7627ce5d6ad82b8443fa57fbcb772bae765e
Paulo Silva <paulo.henrique@elastic.co>
2025-06-18 16:48:35 -0700

    Fix error text from server

--- x-pack/solutions/security/plugins/security_solution/public/flyout/entity_details/generic_right/header_data_cards.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/entity_details/generic_right/header_data_cards.tsx
@@ -127,20 +127,10 @@ export const HeaderDataCards = ({
             iconType="error"
           >
             <p>
-              <FormattedMessage
-                id="xpack.securitySolution.genericEntityFlyout.flyoutHeader.headerDataBoxes.assignCriticalityErrorText"
-                defaultMessage="Something went wrong during validation. Please try again"
+              { assignAssetCriticality.error?.body?.message || i18n.translate('xpack.securitySolution.genericEntityFlyout.flyoutHeader.headerDataBoxes.assignCriticalityErrorText', {
+                defaultMessage: 'Something went wrong during validation. Please try again.',
+              })}
               />
-            </p>
-            <br />
-            <p>
-              <strong>
-                <FormattedMessage
-                  id="xpack.securitySolution.genericEntityFlyout.flyoutHeader.headerDataBoxes.assignCriticalityErrorTextDetails"
-                  defaultMessage="Error:"
-                />
-              </strong>
-              {(assignAssetCriticality.error as Error).message}
             </p>
           </EuiCallOut>
           <EuiSpacer />
