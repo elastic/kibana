d3358c6176e05db9ac128228f0b12cc683bdd5a9
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-06-18 11:36:45 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/test/functional/apps/dataset_quality/dataset_quality_table.ts
+++ x-pack/test/functional/apps/dataset_quality/dataset_quality_table.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 import originalExpect from 'expect';
 import { IndexTemplateName } from '@kbn/apm-synthtrace/src/lib/logs/custom_logsdb_index_templates';
 import { DatasetQualityFtrProviderContext } from './config';
