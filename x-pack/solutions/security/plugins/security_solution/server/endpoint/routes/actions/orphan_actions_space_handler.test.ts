1b94ace73a920d3e13357128ec4408b72b279299
Paul Tavares <paul.tavares@elastic.co>
2025-06-18 18:40:52 -0400

    Test scaffolding for route handler

---
+++ x-pack/solutions/security/plugins/security_solution/server/endpoint/routes/actions/orphan_actions_space_handler.test.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+describe('Orphan response action APIs', () => {
+  describe('GET: read orphan response actions space', () => {
+    it.todo('should return not found error if feature flag is disabled');
+
+    it.todo('should return expected response');
+  });
+
+  describe('POST: update orphan response actions space', () => {
+    it.todo('should return not found error if feature flag is disabled');
+
+    it.todo('should update space id to reference data and return expected response');
+  });
+});
