506ae5b2a3dec742d91c313165e3dad43f419a6e
Jatin Kathuria <jatin.kathuria@elastic.co>
2025-06-18 06:28:54 +0200

    fix: add tags support

---
+++ x-pack/solutions/security/plugins/security_solution/server/lib/siem_migrations/rules/api/util/tags.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { StoredRuleMigration } from '../../types';
+
+const vendorTagMap: Record<StoredRuleMigration['original_rule']['vendor'], string> = {
+  splunk: 'Migrated from SPL',
+};
+
+export const getTagsByVendor = (
+  vendor: StoredRuleMigration['original_rule']['vendor']
+): string[] => {
+  return [vendorTagMap[vendor]];
+};
