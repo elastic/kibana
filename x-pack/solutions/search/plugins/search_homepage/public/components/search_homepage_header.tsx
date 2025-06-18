b736976c68171de95e8ff775d6a1bd29b31361ed
Saikat Sarkar <saikat.sarkar@elastic.co>
2025-06-18 11:15:59 -0600

    Add banner image for dark mode

--- x-pack/solutions/search/plugins/search_homepage/public/components/search_homepage_header.tsx
+++ x-pack/solutions/search/plugins/search_homepage/public/components/search_homepage_header.tsx
@@ -5,67 +5,85 @@
  * 2.0.
  */
 import React from 'react';
-import { EuiPageTemplate, EuiImage, EuiFlexGroup, EuiFlexItem, EuiIcon } from '@elastic/eui';
+import {
+  EuiPageTemplate,
+  EuiImage,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiIcon,
+  useEuiTheme,
+} from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
-import SearchHomePageImage from '../assets/search_homepage.svg';
+import searchHomePageLight from '../assets/search_homepage.svg';
+import searchHomePageDark from '../assets/search_homepage_dark.svg';
 
-export const SearchHomepageHeader: React.FC = () => (
-  <EuiPageTemplate.Header
-    data-test-subj="search-homepage-header"
-    pageTitle={i18n.translate('xpack.searchHomepage.pageTitle', {
-      defaultMessage: 'Your vector database just got faster',
-    })}
-    description={
-      <EuiFlexGroup gutterSize="m" direction="column">
-        <EuiFlexItem grow={false}>
-          {i18n.translate('xpack.searchHomepage.description', {
-            defaultMessage:
-              'Elasticsearch and Lucene now offer “Better binary quantization”, delivering ~95% memory reduction while maintaining high ranking quality.',
-          })}
-        </EuiFlexItem>
-        <EuiFlexItem grow={false}>
-          <EuiFlexGroup gutterSize="xl">
-            <EuiFlexItem grow={false}>
-              <EuiFlexGroup alignItems="center" gutterSize="s">
-                <EuiFlexItem grow={false}>
-                  <EuiIcon type="checkInCircleFilled" color="primary" />
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  {i18n.translate('xpack.searchHomepage.featureUpdateLabel', {
-                    defaultMessage: 'Feature update',
-                  })}
-                </EuiFlexItem>
-              </EuiFlexGroup>
-            </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <EuiFlexGroup alignItems="center" gutterSize="s">
-                <EuiFlexItem grow={false}>
-                  <EuiIcon type="checkInCircleFilled" color="primary" />
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  {i18n.translate('xpack.searchHomepage.featureUpdateLabel', {
-                    defaultMessage: 'Feature update',
-                  })}
-                </EuiFlexItem>
-              </EuiFlexGroup>
-            </EuiFlexItem>
-            <EuiFlexItem grow={false}>
-              <EuiFlexGroup alignItems="center" gutterSize="s">
-                <EuiFlexItem grow={false}>
-                  <EuiIcon type="checkInCircleFilled" color="primary" />
-                </EuiFlexItem>
-                <EuiFlexItem grow={false}>
-                  {i18n.translate('xpack.searchHomepage.featureUpdateLabel', {
-                    defaultMessage: 'Feature update',
-                  })}
-                </EuiFlexItem>
-              </EuiFlexGroup>
-            </EuiFlexItem>
-          </EuiFlexGroup>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    }
-    rightSideItems={[<EuiImage size="xl" url={SearchHomePageImage} alt="" />]}
-  />
-);
+export const SearchHomepageHeader: React.FC = () => {
+  const { colorMode } = useEuiTheme();
+
+  return (
+    <EuiPageTemplate.Header
+      data-test-subj="search-homepage-header"
+      pageTitle={i18n.translate('xpack.searchHomepage.pageTitle', {
+        defaultMessage: 'Your vector database just got faster',
+      })}
+      description={
+        <EuiFlexGroup gutterSize="m" direction="column">
+          <EuiFlexItem grow={false}>
+            {i18n.translate('xpack.searchHomepage.description', {
+              defaultMessage:
+                'Elasticsearch and Lucene now offer “Better binary quantization”, delivering ~95% memory reduction while maintaining high ranking quality.',
+            })}
+          </EuiFlexItem>
+          <EuiFlexItem grow={false}>
+            <EuiFlexGroup gutterSize="xl">
+              <EuiFlexItem grow={false}>
+                <EuiFlexGroup alignItems="center" gutterSize="s">
+                  <EuiFlexItem grow={false}>
+                    <EuiIcon type="checkInCircleFilled" color="primary" />
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    {i18n.translate('xpack.searchHomepage.featureUpdateLabel', {
+                      defaultMessage: 'Feature update',
+                    })}
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiFlexGroup alignItems="center" gutterSize="s">
+                  <EuiFlexItem grow={false}>
+                    <EuiIcon type="checkInCircleFilled" color="primary" />
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    {i18n.translate('xpack.searchHomepage.featureUpdateLabel', {
+                      defaultMessage: 'Feature update',
+                    })}
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiFlexGroup alignItems="center" gutterSize="s">
+                  <EuiFlexItem grow={false}>
+                    <EuiIcon type="checkInCircleFilled" color="primary" />
+                  </EuiFlexItem>
+                  <EuiFlexItem grow={false}>
+                    {i18n.translate('xpack.searchHomepage.featureUpdateLabel', {
+                      defaultMessage: 'Feature update',
+                    })}
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      }
+      rightSideItems={[
+        <EuiImage
+          size="xl"
+          src={colorMode === 'LIGHT' ? searchHomePageLight : searchHomePageDark}
+          alt=""
+        />,
+      ]}
+    />
+  );
+};
