0809e8e7df3f967eab07900195db3ccf16b26771
Elena Stoeva <59341489+ElenaStoeva@users.noreply.github.com>
2025-06-18 10:25:20 +0100

    [Console] Add ESQL autocomplete support (#219980)
    Closes https://github.com/elastic/kibana/issues/208450
    
    ## Summary
    
    This PR adds ESQL autocomplete support to Console. The ESQL suggestions
    are only displayed inside triple-quotes queries (`"query": """...`).
    
    **How to test:**
    1. Open Kibana and load some data (e.g. the sample data sets).
    2. Navigate to Dev Tools -> Console and type in the following query:
    ```
    POST _query
    {
        "query": """ """
    }
    ```
    3. Verify that ESQL suggestions are displayed inside the triple quotes
    and work as in the ESQL editor in Discover. Example ESQL query:
    `FROM kibana_sample_data_ecommerce | WHERE order_date >= ?_tstart AND
    order_date <= ?_tend | LIMIT 10`
    
    4. Verify that no ESQL suggestions are displayed outside triple-quotes
    queries
    5. Verify that no Console suggestions are displayed inside triple-quotes
    queries
    6. Verify that the functionality also works in embeddable Console.
    
    https://github.com/user-attachments/assets/63f1b82d-50a2-4af4-b5ad-812eab9a55a5
    
    ## Release notes
    These changes add autocompletion for ESQL query requests in Console.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Stratoula Kalafateli <efstratia.kalafateli@elastic.co>
    Co-authored-by: Jean-Louis Leysens <jloleysens@gmail.com>
    (cherry picked from commit fe80f762f071681f981a62a1c47def45644be3d8)
    
    # Conflicts:
    #	src/platform/packages/shared/kbn-monaco/src/languages/console/language.ts
    #	src/platform/packages/shared/kbn-monaco/src/languages/index.ts
    #	src/platform/packages/shared/kbn-monaco/src/xjson/index.ts
    #	src/platform/plugins/shared/console/public/application/containers/editor/monaco_editor_actions_provider.ts
    #	src/platform/plugins/shared/console/public/application/containers/editor/utils/index.ts

--- src/platform/packages/shared/kbn-monaco/src/console/language.ts
+++ src/platform/packages/shared/kbn-monaco/src/console/language.ts
@@ -7,24 +7,99 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
+import { type ESQLCallbacks, suggest } from '@kbn/esql-validation-autocomplete';
+import { MutableRefObject } from 'react';
 import { setupConsoleErrorsProvider } from './console_errors_provider';
 import { ConsoleWorkerProxyService } from './console_worker_proxy';
 import { monaco } from '../monaco_imports';
-import { CONSOLE_LANG_ID } from './constants';
+import { CONSOLE_LANG_ID, CONSOLE_OUTPUT_LANG_ID } from './constants';
+import { ESQL_AUTOCOMPLETE_TRIGGER_CHARS } from '../esql';
+import { wrapAsMonacoSuggestions } from '../esql/lib/converters/suggestions';
 import { ConsoleParsedRequestsProvider } from './console_parsed_requests_provider';
 import { buildConsoleTheme } from './theme';
+import { isInsideTripleQuotes } from './utils';
+import type { LangModuleType } from '../types';
 
 const workerProxyService = new ConsoleWorkerProxyService();
 
-export const getParsedRequestsProvider = (model: monaco.editor.ITextModel | null) => {
-  return new ConsoleParsedRequestsProvider(workerProxyService, model);
+import {
+  lexerRules,
+  languageConfiguration,
+  consoleOutputLexerRules,
+  consoleOutputLanguageConfiguration,
+} from './lexer_rules';
+import { foldingRangeProvider } from './folding_range_provider';
+
+export const CONSOLE_TRIGGER_CHARS = ['/', '.', '_', ',', '?', '=', '&', '"'];
+
+/**
+ * @description This language definition is used for the console input panel
+ */
+export const ConsoleLang: LangModuleType = {
+  ID: CONSOLE_LANG_ID,
+  lexerRules,
+  languageConfiguration,
+  foldingRangeProvider,
+  onLanguage: () => {
+    workerProxyService.setup();
+    setupConsoleErrorsProvider(workerProxyService);
+  },
+  languageThemeResolver: buildConsoleTheme,
+  getSuggestionProvider: (
+    esqlCallbacks: Pick<ESQLCallbacks, 'getSources' | 'getPolicies'>,
+    actionsProvider: MutableRefObject<any>
+  ): monaco.languages.CompletionItemProvider => {
+    return {
+      // force suggestions when these characters are used
+      triggerCharacters: [...CONSOLE_TRIGGER_CHARS, ...ESQL_AUTOCOMPLETE_TRIGGER_CHARS],
+      provideCompletionItems: async (
+        model: monaco.editor.ITextModel,
+        position: monaco.Position,
+        context: monaco.languages.CompletionContext
+      ) => {
+        const fullText = model.getValue();
+        const cursorOffset = model.getOffsetAt(position);
+        const textBeforeCursor = fullText.slice(0, cursorOffset);
+        const { insideQuery } = isInsideTripleQuotes(textBeforeCursor);
+        if (esqlCallbacks && insideQuery) {
+          const queryStartOffset = textBeforeCursor.lastIndexOf('"""') + 3;
+          const queryText = textBeforeCursor.slice(queryStartOffset, cursorOffset);
+          const esqlSuggestions = await suggest(
+            queryText,
+            cursorOffset - queryStartOffset,
+            context,
+            esqlCallbacks
+          );
+          return {
+            suggestions: wrapAsMonacoSuggestions(esqlSuggestions, queryText, false),
+          };
+        } else if (actionsProvider.current) {
+          return actionsProvider.current?.provideCompletionItems(model, position, context);
+        }
+        return {
+          suggestions: [],
+        };
+      },
+    };
+  },
+};
+
+/**
+ * @description This language definition is used for the console output panel
+ */
+export const ConsoleOutputLang: LangModuleType = {
+  ID: CONSOLE_OUTPUT_LANG_ID,
+  lexerRules: consoleOutputLexerRules,
+  languageConfiguration: consoleOutputLanguageConfiguration,
+  foldingRangeProvider,
 };
 
 // Theme id is the same as lang id, as we register only one theme resolver that's color mode aware
 export const CONSOLE_THEME_ID = CONSOLE_LANG_ID;
-monaco.editor.registerLanguageThemeResolver(CONSOLE_THEME_ID, buildConsoleTheme);
 
-monaco.languages.onLanguage(CONSOLE_LANG_ID, async () => {
-  workerProxyService.setup();
-  setupConsoleErrorsProvider(workerProxyService);
-});
+// console output theme is the same as console theme
+export const CONSOLE_OUTPUT_THEME_ID = CONSOLE_THEME_ID;
+
+export const getParsedRequestsProvider = (model: monaco.editor.ITextModel | null) => {
+  return new ConsoleParsedRequestsProvider(workerProxyService, model);
+};
