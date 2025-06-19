91fdda42e084edfec28cc1690f33c14b72105564
Alex Szabo <alex.szabo@elastic.co>
2025-06-19 16:29:25 +0200

    fix sh syntax error

--- .buildkite/scripts/pipelines/pull_request/pipeline.sh
+++ .buildkite/scripts/pipelines/pull_request/pipeline.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-if [[ "$(echo $GITHUB_PR_LABELS | grep 'use-qa-image')" != ""]]; then
+if [[ "$(echo "$GITHUB_PR_LABELS" | grep 'use-qa-image')" != "" ]]; then
   ts-node .buildkite/scripts/pipelines/pull_request/pipeline-next.ts
 else
   ts-node .buildkite/scripts/pipelines/pull_request/pipeline.ts
