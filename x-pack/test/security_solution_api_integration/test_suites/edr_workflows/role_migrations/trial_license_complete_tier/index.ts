a4dd40a8afdfa302d1466827b58375b3153c545d
Gergő Ábrahám <gergo.abraham@elastic.co>
2025-06-19 17:17:04 +0200

    type fix: clean up test wrapper

--- x-pack/test/security_solution_api_integration/test_suites/edr_workflows/role_migrations/trial_license_complete_tier/index.ts
+++ x-pack/test/security_solution_api_integration/test_suites/edr_workflows/role_migrations/trial_license_complete_tier/index.ts
@@ -4,38 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { getRegistryUrl as getRegistryUrlFromIngest } from '@kbn/fleet-plugin/server';
 import { FtrProviderContext } from '../../../../ftr_provider_context_edr_workflows';
-import { ROLE } from '../../../../config/services/security_solution_edr_workflows_roles_users';
 
-export default function endpointAPIIntegrationTests(providerContext: FtrProviderContext) {
-  const { loadTestFile, getService } = providerContext;
-
+export default function endpointAPIIntegrationTests({ loadTestFile }: FtrProviderContext) {
   describe('Endpoint related user role migrations', function () {
-    const ingestManager = getService('ingestManager');
-    const rolesUsersProvider = getService('rolesUsersProvider');
-    const kbnClient = getService('kibanaServer');
-    const log = getService('log');
-    const endpointRegistryHelpers = getService('endpointRegistryHelpers');
-    const endpointTestResources = getService('endpointTestResources');
-
-    const roles = Object.values(ROLE);
-    before(async () => {
-      if (!endpointRegistryHelpers.isRegistryEnabled()) {
-        log.warning('These tests are being run with an external package registry');
-      }
-
-      const registryUrl =
-        endpointRegistryHelpers.getRegistryUrlFromTestEnv() ?? getRegistryUrlFromIngest();
-      log.info(`Package registry URL for tests: ${registryUrl}`);
-
-      try {
-        await ingestManager.setup();
-      } catch (err) {
-        log.warning(`Error setting up ingestManager: ${err}`);
-      }
-    });
-
     loadTestFile(require.resolve('./siem_v3_global_artifact_management'));
   });
 }
