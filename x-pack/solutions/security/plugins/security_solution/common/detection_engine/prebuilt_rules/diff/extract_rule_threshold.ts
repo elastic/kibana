e0563c85dbbad675a9e45ba6e01f15cc2fee7b03
Davis Plumlee <davis.plumlee@elastic.co>
2025-06-19 11:24:06 -0400

    adds threshold normalization

---
+++ x-pack/solutions/security/plugins/security_solution/common/detection_engine/prebuilt_rules/diff/extract_rule_threshold.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type {
+  Threshold,
+  ThresholdRule,
+  ThresholdRuleCreateProps,
+} from '../../../api/detection_engine/model/rule_schema';
+
+export const extractRuleThreshold = (rule: ThresholdRule | ThresholdRuleCreateProps): Threshold => {
+  return {
+    value: rule.threshold.value,
+    field: rule.threshold.field,
+    cardinality: rule.threshold.cardinality ?? [],
+  };
+};
