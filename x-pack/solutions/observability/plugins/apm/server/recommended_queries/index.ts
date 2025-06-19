3d6f0974b41276c0be7cdb534168b1b676c967c3
iblancof <irene.blanco@elastic.co>
2025-06-19 19:07:58 +0200

    Update recommended queries

--- x-pack/solutions/observability/plugins/apm/server/recommended_queries/index.ts
+++ x-pack/solutions/observability/plugins/apm/server/recommended_queries/index.ts
@@ -9,9 +9,9 @@ import { i18n } from '@kbn/i18n';
 export const RECOMMENDED_QUERIES = [
   {
     name: i18n.translate('xpack.apm.recommendedQueries.findRootTraces.name', {
-      defaultMessage: 'Find root traces',
+      defaultMessage: 'Root traces',
     }),
-    query: 'FROM traces-* | WHERE parent.id IS NULL OR parent_span_id IS NULL',
+    query: 'FROM traces-* | WHERE parent.id IS NULL',
     description: i18n.translate('xpack.apm.recommendedQueries.findRootTraces.description', {
       defaultMessage:
         'Display all root spans, which represent the start of a request or transaction. This is useful for getting a high-level overview of all operations in your system.',
@@ -19,53 +19,26 @@ export const RECOMMENDED_QUERIES = [
   },
   {
     name: i18n.translate('xpack.apm.recommendedQueries.slowDatabaseQueries.name', {
-      defaultMessage: 'Slow database queries',
+      defaultMessage: 'Database queries',
     }),
-    query:
-      'FROM traces-* | WHERE (span.type == "db" OR db.system.name IS NOT NULL) AND (span.duration.us > 100000 OR duration > 100000000)',
+    query: 'FROM traces-* | WHERE QSTR("span.type:db OR db.system:*")',
     description: i18n.translate('xpack.apm.recommendedQueries.slowDatabaseQueries.description', {
-      defaultMessage:
-        'Pinpoint performance bottlenecks by finding all database (db) spans that took longer than 100ms to execute.',
+      defaultMessage: 'Pinpoint performance bottlenecks by finding all database (db) spans.',
     }),
   },
   {
     name: i18n.translate('xpack.apm.recommendedQueries.allApplicationErrors.name', {
       defaultMessage: 'All application errors',
     }),
-    query: 'FROM logs-* | WHERE processor.event == "error" OR event_name == "exception"',
+    query: 'FROM logs-* | WHERE QSTR("processor.event:error OR event_name:exception")',
     description: i18n.translate('xpack.apm.recommendedQueries.allApplicationErrors.description', {
       defaultMessage:
         'Scan logs to find all documents marked as an error or exception, which is essential for monitoring and troubleshooting application failures.',
     }),
   },
   {
-    name: i18n.translate('xpack.apm.recommendedQueries.top100SlowestTransactions.name', {
-      defaultMessage: 'Top 100 slowest transactions',
-    }),
-    query:
-      'FROM traces-* | WHERE transaction.duration.us > 1000 OR duration > 100000 | SORT transaction.duration.us DESC, duration DESC | LIMIT 100',
-    description: i18n.translate(
-      'xpack.apm.recommendedQueries.top100SlowestTransactions.description',
-      {
-        defaultMessage:
-          'Identify the 100 slowest transactions that took longer than 1ms. The results are sorted from slowest to fastest to help prioritize optimization.',
-      }
-    ),
-  },
-  {
-    name: i18n.translate('xpack.apm.recommendedQueries.slowOrFailedRootSpans.name', {
-      defaultMessage: 'Slow or failed root spans',
-    }),
-    query:
-      'FROM traces-* | WHERE (parent.id IS NULL OR parent_span_id IS NULL) AND (transaction.duration.us > 100000 OR duration > 10000000 OR event.outcome == "failure" OR status.code == "Error")',
-    description: i18n.translate('xpack.apm.recommendedQueries.slowOrFailedRootSpans.description', {
-      defaultMessage:
-        'Find root traces that were either slow (over 100ms) or failed. This is key to identifying poor user experiences and critical errors.',
-    }),
-  },
-  {
     name: i18n.translate('xpack.apm.recommendedQueries.matchingUrlPattern.name', {
-      defaultMessage: 'Find spans matching URL pattern',
+      defaultMessage: 'Spans matching URL pattern',
     }),
     query: 'FROM traces-* | WHERE QSTR("""url.path: \\/api\\/*""")',
     description: i18n.translate('xpack.apm.recommendedQueries.matchingUrlPattern.description', {
