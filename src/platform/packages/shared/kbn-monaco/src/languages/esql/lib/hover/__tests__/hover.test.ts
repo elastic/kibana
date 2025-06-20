2a67fe49f1394dcfc81b13e008a5bc515449ee5c
vadimkibana <vadimkibana@gmail.com>
2025-06-20 11:19:20 +0200

    setup fixtures file for hover tests

--- src/platform/packages/shared/kbn-monaco/src/languages/esql/lib/hover/__tests__/hover.test.ts
+++ src/platform/packages/shared/kbn-monaco/src/languages/esql/lib/hover/__tests__/hover.test.ts
@@ -7,87 +7,11 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import {
-  ESQLFieldWithMetadata,
-  getFunctionDefinition,
-  getFunctionSignatures,
-} from '@kbn/esql-validation-autocomplete';
+import { getFunctionDefinition, getFunctionSignatures } from '@kbn/esql-validation-autocomplete';
 import { modeDescription } from '@kbn/esql-validation-autocomplete/src/autocomplete/commands/enrich/util';
 import { ENRICH_MODES } from '@kbn/esql-validation-autocomplete/src/definitions/commands_helpers';
-import { FieldType } from '@kbn/esql-validation-autocomplete/src/definitions/types';
-import { monaco } from '../../../../monaco_imports';
-import { getHoverItem, HoverMonacoModel } from './hover';
-
-const types: FieldType[] = ['keyword', 'double', 'date', 'boolean', 'ip'];
-
-const fields: Array<ESQLFieldWithMetadata & { suggestedAs?: string }> = [
-  ...types.map((type) => ({
-    name: `${type}Field`,
-    type,
-  })),
-  { name: 'any#Char$Field', type: 'double', suggestedAs: '`any#Char$Field`' },
-  { name: 'kubernetes.something.something', type: 'double' },
-];
-
-const indexes = (
-  [] as Array<{ name: string; hidden: boolean; suggestedAs: string | undefined }>
-).concat(
-  ['a', 'index', 'otherIndex', '.secretIndex', 'my-index'].map((name) => ({
-    name,
-    hidden: name.startsWith('.'),
-    suggestedAs: undefined,
-  })),
-  ['my-index[quoted]', 'my-index$', 'my_index{}'].map((name) => ({
-    name,
-    hidden: false,
-    suggestedAs: `\`${name}\``,
-  }))
-);
-const policies = [
-  {
-    name: 'policy',
-    sourceIndices: ['enrichIndex1'],
-    matchField: 'otherStringField',
-    enrichFields: ['otherField', 'yetAnotherField', 'yet-special-field'],
-    suggestedAs: undefined,
-  },
-  ...['my-policy[quoted]', 'my-policy$', 'my_policy{}'].map((name) => ({
-    name,
-    sourceIndices: ['enrichIndex1'],
-    matchField: 'otherStringField',
-    enrichFields: ['otherField', 'yetAnotherField', 'yet-special-field'],
-    suggestedAs: `\`${name}\``,
-  })),
-];
-
-function createCustomCallbackMocks() {
-  return {
-    getFieldsFor: jest.fn(async () => fields),
-    getSources: jest.fn(async () => indexes),
-    getPolicies: jest.fn(async () => policies),
-  };
-}
-
-function createModelAndPosition(text: string, string: string) {
-  return {
-    model: {
-      getValue: () => text,
-    } as HoverMonacoModel,
-    position: { lineNumber: 1, column: text.lastIndexOf(string) + 1 } as monaco.Position,
-  };
-}
-
-const setupTestbed = (statement: string, triggerString: string) => {
-  const { model, position } = createModelAndPosition(statement, triggerString);
-  const callbacks = createCustomCallbackMocks();
-  const testbed = {
-    model,
-    position,
-    callbacks,
-  };
-
-  return testbed;
-};
+import { getHoverItem } from '../hover';
+import { policies, setupTestbed } from './fixtures';
 
 const assertGetHoverItem = async (statement: string, triggerString: string, expected: string[]) => {
   const kit = setupTestbed(statement, triggerString);
