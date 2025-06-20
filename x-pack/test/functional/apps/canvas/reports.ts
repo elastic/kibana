a62102f89f1e669705e3fd1e0372dfe18fd3caa1
Tim Sullivan <tsullivan@users.noreply.github.com>
2025-06-20 11:37:29 -0700

    [8.15] [Canvas/PDF report] Allow canvas to generate PDF report (#224309) (#224342)
    # Backport
    
    This will backport the following commits from `main` to `8.18.5-1`:
    - [[Canvas/PDF report] Allow canvas to generate PDF report
    (#224309)](https://github.com/elastic/kibana/pull/224309)
    
    <!--- Backport version: 10.0.1 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/test/functional/apps/canvas/reports.ts
+++ x-pack/test/functional/apps/canvas/reports.ts
@@ -66,6 +66,12 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         expect(res.get('content-disposition')).to.equal(
           'attachment; filename=The%20Very%20Cool%20Workpad%20for%20PDF%20Tests.pdf'
         );
+
+        const jobId = await PageObjects.reporting.getReportJobId(60000);
+        const reportInfo = await PageObjects.reporting.getReportInfo(jobId);
+
+        // verify "completed" status (no warnings)
+        expect(reportInfo).to.have.property('status', 'completed');
       });
     });
   });
