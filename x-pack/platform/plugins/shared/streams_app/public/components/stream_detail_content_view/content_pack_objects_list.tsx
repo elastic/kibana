920e846bdabefb1fc502248fe9bb0a7c05a0eacc
klacabane <kevin.lacabane@elastic.co>
2025-06-20 16:09:49 +0200

    link to fields or processors

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_content_view/content_pack_objects_list.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_content_view/content_pack_objects_list.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { EuiBadge, EuiBasicTable } from '@elastic/eui';
+import { EuiBadge, EuiBasicTable, EuiLink } from '@elastic/eui';
 import React from 'react';
 import { DashboardAttributes } from '@kbn/dashboard-plugin/common/content_management/v2';
 import { capitalize } from 'lodash';
@@ -28,11 +28,21 @@ export function ContentPackObjectsList({
         {
           name: 'Name',
           render: (entry: ContentPackEntry) => {
-            if (entry.type === 'dashboard') {
-              return (entry.attributes as DashboardAttributes).title;
+            const title = entry.type === 'dashboard' ? entry.attributes.title : entry.id;
+            const href =
+              entry.type === 'processors'
+                ? './management/enrich'
+                : entry.type === 'fields'
+                ? './management/schemaEditor'
+                : '';
+            if (href) {
+              return (
+                <EuiLink href={href} external target="_blank">
+                  {title}
+                </EuiLink>
+              );
             }
-
-            return entry.id;
+            return title;
           },
           truncateText: true,
         },
@@ -44,6 +54,8 @@ export function ContentPackObjectsList({
                 ? 'dashboardApp'
                 : entry.type === 'fields'
                 ? 'indexMapping'
+                : entry.type === 'processors'
+                ? 'grokApp'
                 : 'questionInCircle';
             return (
               <EuiBadge color="hollow" iconType={iconType} iconSide="left">
