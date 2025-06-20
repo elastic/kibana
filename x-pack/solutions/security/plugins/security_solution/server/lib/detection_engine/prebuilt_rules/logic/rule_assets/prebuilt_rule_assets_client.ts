d9d9dd208dea231d67bf32696fa789472cd6bc21
Nikita Indik <nikita.indik@elastic.co>
2025-06-20 18:17:36 +0200

    Fix types

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/prebuilt_rules/logic/rule_assets/prebuilt_rule_assets_client.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/prebuilt_rules/logic/rule_assets/prebuilt_rule_assets_client.ts
@@ -17,7 +17,8 @@ import { withSecuritySpan } from '../../../../../utils/with_security_span';
 import type { PrebuiltRuleAsset } from '../../model/rule_assets/prebuilt_rule_asset';
 import { validatePrebuiltRuleAssets } from './prebuilt_rule_assets_validation';
 import { PREBUILT_RULE_ASSETS_SO_TYPE } from './prebuilt_rule_assets_type';
-import type { BasicRuleInfo, RuleVersionSpecifier } from '../rule_versions/rule_version_specifier';
+import type { RuleVersionSpecifier } from '../rule_versions/rule_version_specifier';
+import type { BasicRuleInfo } from '../basic_rule_info';
 
 const RULE_ASSET_ATTRIBUTES = `${PREBUILT_RULE_ASSETS_SO_TYPE}.attributes`;
 const MAX_PREBUILT_RULES_COUNT = 10_000;
