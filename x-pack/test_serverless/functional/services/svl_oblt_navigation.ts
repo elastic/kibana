317d5acd27ed6f8803ccc731f8dbc2d618e2ca9e
Bryce Buchanan <bryce.buchanan@elastic.co>
2025-06-20 11:24:35 -0700

    updated discover nav method with testSubject validation

--- x-pack/test_serverless/functional/services/svl_oblt_navigation.ts
+++ x-pack/test_serverless/functional/services/svl_oblt_navigation.ts
@@ -13,7 +13,7 @@ export function SvlObltNavigationServiceProvider({
 }: FtrProviderContext) {
   const retry = getService('retry');
   const testSubjects = getService('testSubjects');
-  const PageObjects = getPageObjects(['common', 'header', 'discover']);
+  const PageObjects = getPageObjects(['common', 'header']);
 
   return {
     async navigateToLandingPage() {
@@ -25,7 +25,7 @@ export function SvlObltNavigationServiceProvider({
     async navigateToDiscoverPage() {
       await retry.tryForTime(60 * 1000, async () => {
         await PageObjects.common.navigateToApp('discover');
-        await PageObjects.header.awaitKibanaChrome();
+        await testSubjects.exists('discoverQueryTotalHits', { timeout: 20_000 });
       });
     },
   };
