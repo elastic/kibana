6213163c312374045713957103e8d2e43a0c11bf
Kris Gross <kristopher.gross@elastic.co>
2025-06-23 13:11:52 -0400

    revert

--- x-pack/test/security_solution_cypress/cypress/e2e/ai4dsoc/capabilities/access.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/ai4dsoc/capabilities/access.cy.ts
@@ -19,25 +19,7 @@ import {
 const MANAGEMENT_PAGE_DESCRIPTION =
   'Manage data and indices, oversee rules and connectors, organize saved objects and files, and create API keys in a central location.';
 
-describe(
-  'Capabilities',
-  {
-    tags: ['@serverless'],
-    env: {
-      ftrConfig: {
-        kbnServerArgs: [
-          '--serverless=security',
-          '--xpack.encryptedSavedObjects.encryptionKey="abcdefghijklmnopqrstuvwxyz123456"',
-          `--xpack.securitySolutionServerless.productTypes=${JSON.stringify([
-            { product_line: 'ai_soc', product_tier: 'search_ai_lake' },
-          ])}`,
-          '--csp.strict=false',
-          '--csp.warnLegacyBrowsers=false',
-        ],
-      },
-    },
-  },
-  () => {
+describe('Capabilities', { tags: ['@serverless'], () => {
   const userRoles = [
     {
       name: 'Admin user',
