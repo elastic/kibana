755738ffeeda7fa6eba4f49791f203a0334b070c
Mike Cote <michel.cote@elastic.co>
2025-06-23 08:53:42 -0400

    Prototype form generation

--- x-pack/platform/plugins/shared/stack_connectors/public/connector_types/keep/connector.tsx
+++ x-pack/platform/plugins/shared/stack_connectors/public/connector_types/keep/connector.tsx
@@ -6,9 +6,139 @@
  */
 
 import React from 'react';
+import { EuiSpacer } from '@elastic/eui';
+import { ConfigurationFormItems } from '@kbn/inference-endpoint-ui-common';
 
-const ConnectorFields: React.FunctionComponent = ({}) => {
-  return <></>;
+const ConnectorFields: React.FunctionComponent = ({ isEdit, actionTypeModel }) => {
+  let fields = [];
+  switch (actionTypeModel?.id) {
+    case 'Airflow':
+      fields = [
+        {
+          "key": "host",
+          "label": "Host",
+          "isValid": true,
+          "validationErrors": [],
+          "value": null,
+          "default_value": null,
+          "required": true,
+          "sensitive": true,
+          "updatable": true,
+          "type": "str",
+          "supported_task_types": ["text_embedding", "completion"]
+        },
+        {
+          "key": "port",
+          "label": "Port",
+          "isValid": true,
+          "validationErrors": [],
+          "value": null,
+          "default_value": null,
+          "required": true,
+          "sensitive": false,
+          "updatable": false,
+          "type": "str",
+          "supported_task_types": ["text_embedding", "completion"]
+        },
+        {
+          "key": "username",
+          "label": "Username",
+          "isValid": true,
+          "validationErrors": [],
+          "value": null,
+          "default_value": null,
+          "required": true,
+          "sensitive": false,
+          "updatable": false,
+          "type": "str",
+          "supported_task_types": ["text_embedding", "completion"]
+        },
+        {
+          "key": "password",
+          "label": "Password",
+          "isValid": true,
+          "validationErrors": [],
+          "value": null,
+          "default_value": null,
+          "required": true,
+          "sensitive": false,
+          "updatable": false,
+          "type": "str",
+          "supported_task_types": ["text_embedding", "completion"]
+        }
+      ];
+      break;
+    case 'AmazonSQS Provider':
+      fields = [
+        {
+          "key": "region_name",
+          "label": "Region Name",
+          "isValid": true,
+          "validationErrors": [],
+          "value": null,
+          "default_value": null,
+          "required": true,
+          "sensitive": true,
+          "updatable": true,
+          "type": "str",
+          "supported_task_types": ["text_embedding", "completion"]
+        },
+        {
+          "key": "sqs_queue_url",
+          "label": "SQS Queue URL",
+          "isValid": true,
+          "validationErrors": [],
+          "value": null,
+          "default_value": null,
+          "required": true,
+          "sensitive": false,
+          "updatable": false,
+          "type": "str",
+          "supported_task_types": ["text_embedding", "completion"]
+        },
+        {
+          "key": "access_key_id",
+          "label": "Access Key Id",
+          "isValid": true,
+          "validationErrors": [],
+          "value": null,
+          "default_value": null,
+          "required": true,
+          "sensitive": false,
+          "updatable": false,
+          "type": "str",
+          "supported_task_types": ["text_embedding", "completion"]
+        },
+        {
+          "key": "secret_access_key",
+          "label": "Secret access key",
+          "isValid": true,
+          "validationErrors": [],
+          "value": null,
+          "default_value": null,
+          "required": true,
+          "sensitive": false,
+          "updatable": false,
+          "type": "str",
+          "supported_task_types": ["text_embedding", "completion"]
+        }
+      ];
+      break;
+  }
+  return (
+    <>
+      <EuiSpacer size="m" />
+      <ConfigurationFormItems
+        isLoading={false}
+        direction="column"
+        items={fields}
+        setConfigEntry={() => {}}
+        isEdit={isEdit}
+        isPreconfigured={false}
+        isInternalProvider={false}
+      />
+    </>
+  );
 };
 
 // eslint-disable-next-line import/no-default-export
