a1b93f6bdd6a006855ecff2e6db05bdecd793430
Ying <ying.mao@elastic.co>
2025-06-23 12:41:07 -0400

    Allowing dtstart for Task Manager rrule

---
+++ x-pack/platform/plugins/shared/task_manager/server/saved_objects/schemas/rrule/v2.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { schema } from '@kbn/config-schema';
+import { Frequency } from '@kbn/rrule';
+import { byhour, byminute, byweekday, bymonthday, rruleCommon as rruleCommonV1 } from './v1';
+
+const rruleCommon = rruleCommonV1.extends({
+  dtstart: schema.maybe(schema.string()),
+});
+
+const rruleMonthly = rruleCommon.extends({
+  freq: schema.literal(Frequency.MONTHLY),
+  byhour,
+  byminute,
+  byweekday,
+  bymonthday,
+});
+
+const rruleWeekly = rruleCommon.extends({
+  freq: schema.literal(Frequency.WEEKLY),
+  byhour,
+  byminute,
+  byweekday,
+  bymonthday: schema.never(),
+});
+
+const rruleDaily = rruleCommon.extends({
+  freq: schema.literal(Frequency.DAILY),
+  byhour,
+  byminute,
+  byweekday,
+  bymonthday: schema.never(),
+});
+
+export const rruleSchedule = schema.oneOf([rruleMonthly, rruleWeekly, rruleDaily]);
+export const scheduleRruleSchema = schema.object({
+  rrule: rruleSchedule,
+});
