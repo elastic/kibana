6f37af9fcff34512bd9d25f1ce40ac96f21400bd
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-06-23 08:53:53 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint --no-cache --fix'

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_management/wrapper.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_management/wrapper.tsx
@@ -5,13 +5,7 @@
  * 2.0.
  */
 
-import {
-  EuiButtonEmpty,
-  EuiFlexGroup,
-  EuiPageHeader,
-  useEuiTheme,
-  EuiBadgeGroup,
-} from '@elastic/eui';
+import { EuiButtonEmpty, EuiFlexGroup, EuiPageHeader, useEuiTheme } from '@elastic/eui';
 import { css } from '@emotion/react';
 import React from 'react';
 import { i18n } from '@kbn/i18n';
