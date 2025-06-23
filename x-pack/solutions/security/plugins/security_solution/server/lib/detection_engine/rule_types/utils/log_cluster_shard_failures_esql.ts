3363493315ed2feabb456aa203ea3bde2cba473b
Nikita Khristinin <nkhristinin@gmail.com>
2025-06-23 11:57:32 +0200

    Create type in the code

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/utils/log_cluster_shard_failures_esql.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/utils/log_cluster_shard_failures_esql.ts
@@ -4,8 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import type { EsqlEsqlShardFailure } from '@elastic/elasticsearch/lib/api/types';
-import type { EsqlTable } from '../esql/esql_request';
+import type { EsqlTable, EsqlEsqlShardFailure } from '../esql/esql_request';
 import * as i18n from '../translations';
 import type { SearchAfterAndBulkCreateReturnType } from '../types';
 
