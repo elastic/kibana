862d48ec81f7b41318c193aa709e9db897122940
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2025-06-23 07:37:07 +0200

    Deangularize the comments

--- src/platform/plugins/shared/unified_search/public/search_bar/search_bar.tsx
+++ src/platform/plugins/shared/unified_search/public/search_bar/search_bar.tsx
@@ -254,14 +254,9 @@ class SearchBarUI<QT extends (Query | AggregateQuery) | Query = Query> extends C
   /*
    Keep the "draft" value in local state until the user actually submits the query. There are a couple advantages:
 
-    1. Each app doesn't have to maintain its own "draft" value if it wants to put off updating the query in app state
-    until the user manually submits their changes. Most apps have watches on the query value in app state so we don't
-    want to trigger those on every keypress. Also, some apps (e.g. dashboard) already juggle multiple query values,
-    each with slightly different semantics and I'd rather not add yet another variable to the mix.
-
-    2. Changes to the local component state won't trigger an Angular digest cycle. Triggering digest cycles on every
-    keypress has been a major source of performance issues for us in previous implementations of the query bar.
-    See https://github.com/elastic/kibana/issues/14086
+   Each app doesn't have to maintain its own "draft" value if it wants to put off updating the query in app state
+   until the user manually submits their changes. Some apps have watches on the query value in app state so we don't
+   want to trigger those on every keypress.
   */
   public state = {
     isFiltersVisible: true,
