5dc735afc59c297adbd0614fdb6332c9d0529492
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2025-06-23 14:24:55 +0200

    feat(streams): add product feature onboarding

--- x-pack/solutions/observability/plugins/observability/public/pages/landing/landing.tsx
+++ x-pack/solutions/observability/plugins/observability/public/pages/landing/landing.tsx
@@ -6,10 +6,26 @@
  */
 import React, { useEffect } from 'react';
 import { type LogsLocatorParams, LOGS_LOCATOR_ID } from '@kbn/logs-shared-plugin/common';
+import { OBSERVABILITY_ONBOARDING_LOCATOR } from '@kbn/deeplinks-observability';
+import { OBSERVABILITY_COMPLETE_LANDING_PAGE_FEATURE } from '../../../common';
 import { useHasData } from '../../hooks/use_has_data';
 import { useKibana } from '../../utils/kibana_react';
 
 export function LandingPage() {
+  const { pricing } = useKibana().services;
+
+  const hasCompleteLandingPage = pricing.isFeatureAvailable(
+    OBSERVABILITY_COMPLETE_LANDING_PAGE_FEATURE.id
+  );
+
+  return hasCompleteLandingPage ? (
+    <ObservabilityCompleteLandingPage />
+  ) : (
+    <ObservabilityLogsEssentialsLandingPage />
+  );
+}
+
+function ObservabilityCompleteLandingPage() {
   const { hasDataMap, isAllRequestsComplete } = useHasData();
   const {
     application: { navigateToUrl, navigateToApp },
@@ -30,6 +46,34 @@ export function LandingPage() {
         navigateToUrl(basePath.prepend('/app/apm/services'));
       } else {
         navigateToUrl(basePath.prepend('/app/observabilityOnboarding'));
+      }
+    }
+  }, [basePath, hasDataMap, isAllRequestsComplete, navigateToApp, navigateToUrl, url.locators]);
+
+  return <></>;
+}
+
+function ObservabilityLogsEssentialsLandingPage() {
+  const { hasDataMap, isAllRequestsComplete } = useHasData();
+  const {
+    application: { navigateToUrl, navigateToApp },
+    http: { basePath },
+    share: { url },
+  } = useKibana().services;
+
+  useEffect(() => {
+    if (isAllRequestsComplete) {
+      const { infra_logs: logs } = hasDataMap;
+      console.log(hasDataMap);
+
+      const hasLogsData = logs?.hasData;
+
+      if (hasLogsData) {
+        const logsLocator = url.locators.get(LOGS_LOCATOR_ID);
+        logsLocator?.navigate({});
+      } else {
+        const onboardingLocator = url.locators.get(OBSERVABILITY_ONBOARDING_LOCATOR);
+        onboardingLocator?.navigate({});
       }
     }
   }, [basePath, hasDataMap, isAllRequestsComplete, navigateToApp, navigateToUrl, url.locators]);
