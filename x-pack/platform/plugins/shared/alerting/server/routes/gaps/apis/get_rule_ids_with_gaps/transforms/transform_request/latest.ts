43f2994bfaf89ca64e507856c9e52a9fa2755446
Khristinin Nikita <nikita.khristinin@elastic.co>
2025-06-23 18:02:44 +0200

    Rule gaps callout (#224268)
    ##  Rule gaps callout
    
    ### Summary
    This PR introduces a new callout on the **Rules** page and improves the
    **Gaps** panel with clearer metrics and filtering logic to enhance the
    user experience when managing gap filling.
    
    ---
    
    ### Key Changes
    
    1. **Rules Page – Callout for Recent Gaps**
    - A dismissible callout is displayed if there are any unfilled gaps in
    the **last 24 hours**.
       - The callout includes:
         - A link to the Gaps dashboard.
         - A link to relevant documentation.
    - Once dismissed, the callout won’t reappear for 24 hours **unless** new
    gaps are detected during that time.
    
    2. **Gaps Panel – Rule Count Display**
       - Displays two separate counts:
         - The **number of rules with unfilled gaps**.
    - The **number of rules currently in progress** (gaps are being filled).
       - Provides users with a quick overview of the current gap state.
    
    3. **Filtering Logic**
    - The filter now only shows **rules with unfilled gaps that can still be
    filled**.
    - Example: If 5 rules had unfilled gaps and filling started for 3, the
    filtered list will only show the remaining 2.
    
    **Note**: we don't immediately update number for rules with gap
    "in-progress", because gap update async and can happen after bulk
    request if finished.
    
    https://github.com/user-attachments/assets/3c913129-16c3-449a-9847-a01e2657e9aa
    
    ### How to test
    
    Enable FF `storeGapsInEventLogEnabled`.
    
    [Use this tool to create a lot of rules with/without
    gaps](https://github.com/elastic/security-documents-generator)
    
    ```
    //  will create 100 without gaps, and 5 minute interval
    yarn start rules --rules 100 -g 0 -c -i "5m" -f 1
    ```
    
    Alternatively you can create a rule with small interval (1m) and
    lookback time(1s). Enable rule, wait for completion and then disable it.
    Wait 5m (> 4x interval). Then enable it and rule should fail and
    generate gap.
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 40feb02ab4a8eadb01685385c8880b0ed3b520fe)
    
    # Conflicts:
    #	src/platform/packages/shared/kbn-doc-links/src/get_doc_links.ts

--- x-pack/platform/plugins/shared/alerting/server/routes/gaps/apis/get_rule_ids_with_gaps/transforms/transform_request/latest.ts
+++ x-pack/platform/plugins/shared/alerting/server/routes/gaps/apis/get_rule_ids_with_gaps/transforms/transform_request/latest.ts
@@ -5,7 +5,4 @@
  * 2.0.
  */
 
-export { transformResponse } from './transform_response/latest';
-
-export { transformResponse as transformResponseV1 } from './transform_response/v1';
-export { transformRequest as transformRequestV1 } from './transform_request/v1';
+export * from './v1';
