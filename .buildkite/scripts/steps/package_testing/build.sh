0549fccb67d495254528eb78177d52dc4146049c
Jonathan Budzenski <jon@elastic.co>
2025-06-23 16:51:33 -0500

    [build] Standardize docker image tarball suffixes for architectures
    Previously amd64 images didn't have a suffix, and arm64 images were
    suffixed with aarch64.  This updates all references to use amd64 or
    arm64.
    
    Closes #222990

--- .buildkite/scripts/steps/package_testing/build.sh
+++ .buildkite/scripts/steps/package_testing/build.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 
 node scripts/build --all-platforms --debug --skip-docker-cloud --skip-docker-serverless --skip-docker-ubi --skip-docker-fips --skip-docker-cloud-fips --skip-docker-contexts --skip-cdn-assets
 
-DOCKER_FILE="kibana-$KIBANA_PKG_VERSION-SNAPSHOT-docker-image.tar.gz"
+DOCKER_FILE="kibana-$KIBANA_PKG_VERSION-SNAPSHOT-docker-image-amd64.tar.gz"
 
 cd target
 buildkite-agent artifact upload "./$DOCKER_FILE;./*.deb;./*.rpm"
