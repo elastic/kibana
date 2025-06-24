elastic/kibana

commit b572bf42e3b45ee23ebd2590bd0300250dcfaa7b

[Console] Add custom context menu (#217865)
Fixes https://github.com/elastic/kibana/issues/204668

## Summary

This PR adds a custom context menu to Console that correctly copies,
cuts, and pastes the selected text in the editor. Previously, when we
did right click (or Ctrl + click), the native browser context menu is
open but only the word on which we clicked was selected, rather than the
whole selected text in the editor. This is because the native browser
context menu is not aware of Monaco's selection and treats the text in
the editor as regular text in the browser. To fix this, we add a custom
context menu that is aware of Monaco editor's selection, which correctly
copies and cuts the selected text.

**Note for changes in CodeEditor component**: In order to enable the
custom context menu, we need to add the `import
'monaco-editor/esm/vs/editor/contrib/contextmenu/browser/contextmenu.js';`
import, which would enable the Monaco context menu in all code editors
in Kibana unless we explicitly disable it with `contextmenu: false`,
which we do in the CodeEditor component as a default behavior. This
ensures that all other code editors in Kibana still have the native
browser context menu. @elastic/appex-sharedux please let us know if you
would like us to add this custom context menu to the Code editor
component so that all editors in Kibana have the same context menu with
a correct behaviour.


https://github.com/user-attachments/assets/ceb65438-a17e-46fd-be6a-a1cce4d98df7

src/platform/packages/shared/shared-ux/code_editor/impl/use_context_menu_utils.ts
@@ -1,0 +1,144 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { i18n } from '@kbn/i18n';
+import { monaco } from '@kbn/monaco';
+import { copyToClipboard } from '@elastic/eui';
+
+interface RegisterContextMenuActionsParams {
+  editor: monaco.editor.IStandaloneCodeEditor;
+  enableWriteActions: boolean;
+  customActions?: ContextMenuAction[];
+}
+
+export interface ContextMenuAction {
+  actionDescriptor: monaco.editor.IActionDescriptor;
+  writeAction: boolean;
+}
+
+const CUT_ACTION_ID = 'cutAction';
+const COPY_ACTION_ID = 'copyAction';
+const PASTE_ACTION_ID = 'pasteAction';
+
+const DEFAULT_ACTIONS: ContextMenuAction[] = [
+  {
+    actionDescriptor: {
+      id: CUT_ACTION_ID,
+      label: i18n.translate('sharedUXPackages.codeEditor.contextMenuAction.cutActionLabel', {
+        defaultMessage: 'Cut',
+      }),
+      // eslint-disable-next-line no-bitwise
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyX],
+      contextMenuGroupId: '9_cutcopypaste',
+      contextMenuOrder: 1,
+      run: async (ed) => {
+        const selection = ed.getSelection();
+        const model = ed.getModel();
+        if (selection && model) {
+          const selectedText = model.getValueInRange(selection);
+          copyToClipboard(selectedText);
+          // ed.executeEdits('Cut selection', [{ range: selection, text: '' }]);
+        }
+      },
+    },
+    writeAction: true,
+  },
+  {
+    actionDescriptor: {
+      id: COPY_ACTION_ID,
+      label: i18n.translate('sharedUXPackages.codeEditor.contextMenuAction.copyActionLabel', {
+        defaultMessage: 'Copy',
+      }),
+      // eslint-disable-next-line no-bitwise
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyC],
+      contextMenuGroupId: '9_cutcopypaste',
+      contextMenuOrder: 2,
+      run: async (ed) => {
+        const selection = ed.getSelection();
+        const model = ed.getModel();
+        const position = ed.getPosition();
+        if (selection && model) {
+          const selectedText = model.getValueInRange(selection);
+          copyToClipboard(selectedText);
+          if (position) {
+            // For some reason (possibly rerendering), calling copyToClipboard
+            // changes the position of the cursor so we set it to the initial position
+            ed.setPosition(position);
+          }
+        }
+      },
+    },
+    writeAction: false,
+  },
+  {
+    actionDescriptor: {
+      id: PASTE_ACTION_ID,
+      label: i18n.translate('sharedUXPackages.codeEditor.contextMenuAction.pasteActionLabel', {
+        defaultMessage: 'Paste',
+      }),
+      // eslint-disable-next-line no-bitwise
+      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyV],
+      contextMenuGroupId: '9_cutcopypaste',
+      contextMenuOrder: 3,
+      run: async (ed) => {
+        const selection = ed.getSelection();
+        if (selection) {
+          const clipboardText = await navigator.clipboard?.readText();
+          if (clipboardText) {
+            ed.executeEdits('Paste from clipboard', [{ range: selection, text: clipboardText }]);
+          }
+        }
+      },
+    },
+    writeAction: true,
+  },
+];
+
+type RegisteredAction = ContextMenuAction & {
+  refObject: { current: monaco.IDisposable | null };
+};
+
+/**
+ * Hook that returns a function for registering context menu actions in the Monaco editor.
+ */
+export const useContextMenuUtils = () => {
+  let registeredActions: RegisteredAction[] = [];
+
+  const disposeAllActions = () => {
+    registeredActions.forEach(({ refObject }) => {
+      refObject.current?.dispose();
+    });
+    registeredActions = [];
+  };
+
+  const registerContextMenuActions = ({
+    editor,
+    enableWriteActions,
+    customActions = [],
+  }: RegisterContextMenuActionsParams) => {
+    disposeAllActions();
+
+    const allActions = [...DEFAULT_ACTIONS, ...customActions];
+
+    registeredActions = allActions.map(({ actionDescriptor, writeAction }) => {
+      const refObject = { current: null as monaco.IDisposable | null };
+
+      if (!writeAction || enableWriteActions) {
+        refObject.current = editor.addAction(actionDescriptor);
+      }
+
+      return { actionDescriptor, writeAction, refObject };
+    });
+  };
+
+  return {
+    registerContextMenuActions,
+    unregisterContextMenuActions: disposeAllActions,
+  };
+};

src/platform/packages/shared/kbn-monaco/src/monaco_imports.ts
@@ -33,6 +33,7 @@ import 'monaco-editor/esm/vs/editor/contrib/codeAction/browser/codeActionModel.j
 
 import 'monaco-editor/esm/vs/editor/contrib/find/browser/findController'; // Needed for Search bar functionality
 import 'monaco-editor/esm/vs/editor/standalone/browser/inspectTokens/inspectTokens.js'; // Needed for inspect tokens functionality
+import 'monaco-editor/esm/vs/editor/contrib/contextmenu/browser/contextmenu.js'; // Needed for enabling custom Monaco context menu
 
 import 'monaco-editor/esm/vs/language/json/monaco.contribution.js';
 import 'monaco-editor/esm/vs/basic-languages/javascript/javascript.contribution.js'; // Needed for basic javascript support

src/platform/packages/shared/shared-ux/code_editor/impl/code_editor.tsx
@@ -35,6 +35,8 @@ import {
   type MonacoEditorProps as ReactMonacoEditorProps,
 } from './react_monaco_editor';
 import { remeasureFonts } from './remeasure_fonts';
+import { useContextMenuUtils } from './use_context_menu_utils';
+import type { ContextMenuAction } from './use_context_menu_utils';
 
 import { PlaceholderWidget } from './placeholder_widget';
 import { styles } from './editor.styles';
@@ -162,6 +164,17 @@ export interface CodeEditorProps {
    * Custom CSS class to apply to the container
    */
   classNameCss?: Interpolation<Theme>;
+
+  /**
+   * Enables a custom context menu with Cut, Copy, Paste actions. Disabled by default.
+   */
+  enableCustomContextMenu?: boolean;
+
+  /**
+   * If the custom context menu is enable through {@link enableCustomContextMenu},
+   * this prop allows adding more custom menu actions, on top of the default Cut, Copy, and Paste actions.
+   */
+  customContextMenuActions?: ContextMenuAction[];
 }
 
 export const CodeEditor: React.FC<CodeEditorProps> = ({
@@ -195,8 +208,11 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
   enableFindAction,
   dataTestSubj,
   classNameCss,
+  enableCustomContextMenu = false,
+  customContextMenuActions = [],
 }) => {
   const { euiTheme } = useEuiTheme();
+  const { registerContextMenuActions, unregisterContextMenuActions } = useContextMenuUtils();
 
   // We need to be able to mock the MonacoEditor in our test in order to not test implementation
   // detail and not have to call methods on the <CodeEditor /> component instance.
@@ -451,20 +467,41 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
         });
       }
 
+      if (enableCustomContextMenu) {
+        registerContextMenuActions({
+          editor,
+          enableWriteActions: !isReadOnly,
+          customActions: customContextMenuActions,
+        });
+      }
+
       editorDidMount?.(editor);
       setEditor(editor);
     },
-    [editorDidMount, onBlurMonaco, onKeydownMonaco, readOnlyMessage]
+    [
+      editorDidMount,
+      onBlurMonaco,
+      onKeydownMonaco,
+      readOnlyMessage,
+      enableCustomContextMenu,
+      registerContextMenuActions,
+      isReadOnly,
+      customContextMenuActions,
+    ]
   );
 
   const _editorWillUnmount = useCallback<NonNullable<ReactMonacoEditorProps['editorWillUnmount']>>(
     (editor) => {
+      if (enableCustomContextMenu) {
+        unregisterContextMenuActions();
+      }
+
       editorWillUnmount?.();
 
       const model = editor.getModel();
       model?.dispose();
     },
-    [editorWillUnmount]
+    [editorWillUnmount, enableCustomContextMenu, unregisterContextMenuActions]
   );
 
   useEffect(() => {
@@ -543,6 +580,7 @@ export const CodeEditor: React.FC<CodeEditorProps> = ({
               fontFamily: 'Roboto Mono',
               fontSize: isFullScreen ? 16 : 12,
               lineHeight: isFullScreen ? 24 : 21,
+              contextmenu: enableCustomContextMenu,
               // @ts-expect-error, see https://github.com/microsoft/monaco-editor/issues/3829
               'bracketPairColorization.enabled': false,
               ...options,

src/platform/plugins/shared/console/public/application/containers/editor/monaco_editor.tsx
@@ -286,10 +286,10 @@ export const MonacoEditor = ({ localStorageValue, value, setValue }: EditorProps
           hover: {
             above: false,
           },
-          lineHeight: 24,
         }}
         suggestionProvider={suggestionProvider}
         enableFindAction={true}
+        enableCustomContextMenu={true}
       />
     </div>
   );

src/platform/plugins/shared/console/public/application/containers/editor/monaco_editor_output.tsx
@@ -187,13 +187,13 @@ export const MonacoEditorOutput: FunctionComponent = () => {
         editorDidMount={editorDidMountCallback}
         editorWillUnmount={editorWillUnmountCallback}
         enableFindAction={true}
+        enableCustomContextMenu={true}
         options={{
           readOnly: true,
           fontSize: readOnlySettings.fontSize,
           wordWrap: readOnlySettings.wrapMode === true ? 'on' : 'off',
           theme: CONSOLE_OUTPUT_THEME_ID,
           automaticLayout: true,
-          lineHeight: 24,
         }}
       />
     </div>

commit b2992eacef41015ebfa187963fb6f05a553d6996

Fix custom context menu (#225006)
Fixes some issues introduced by
https://github.com/elastic/kibana/pull/217865

## Summary
While testing something in Serverles, we realized that the Console
copy/cut/paste commands weren't working as expected: cut wasn't working
and paste something from outside Kibana behaved weird. Those changes
were introduced in https://github.com/elastic/kibana/pull/217865.

This PR fixes it by:
* Uncomment commented line (probably by mistake), allowing cut.
* Deletes the keybindings lines, allowing them to be native to the
user's OS.
* Removes the trick done for set position when copying. This glitch was
caused by the keybindings let them to default by OS fix it.

Note: You may need to do `yarn kbn clean && yarn kbn bootstrap` before
starting the dev server to see the changes.

src/platform/packages/shared/shared-ux/code_editor/impl/use_context_menu_utils.ts
@@ -33,8 +33,6 @@ const DEFAULT_ACTIONS: ContextMenuAction[] = [
       label: i18n.translate('sharedUXPackages.codeEditor.contextMenuAction.cutActionLabel', {
         defaultMessage: 'Cut',
       }),
-      // eslint-disable-next-line no-bitwise
-      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyX],
       contextMenuGroupId: '9_cutcopypaste',
       contextMenuOrder: 1,
       run: async (ed) => {
@@ -43,7 +41,7 @@ const DEFAULT_ACTIONS: ContextMenuAction[] = [
         if (selection && model) {
           const selectedText = model.getValueInRange(selection);
           copyToClipboard(selectedText);
-          // ed.executeEdits('Cut selection', [{ range: selection, text: '' }]);
+          ed.executeEdits('Cut selection', [{ range: selection, text: '' }]);
         }
       },
     },
@@ -55,22 +53,14 @@ const DEFAULT_ACTIONS: ContextMenuAction[] = [
       label: i18n.translate('sharedUXPackages.codeEditor.contextMenuAction.copyActionLabel', {
         defaultMessage: 'Copy',
       }),
-      // eslint-disable-next-line no-bitwise
-      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyC],
       contextMenuGroupId: '9_cutcopypaste',
       contextMenuOrder: 2,
       run: async (ed) => {
         const selection = ed.getSelection();
         const model = ed.getModel();
-        const position = ed.getPosition();
         if (selection && model) {
           const selectedText = model.getValueInRange(selection);
           copyToClipboard(selectedText);
-          if (position) {
-            // For some reason (possibly rerendering), calling copyToClipboard
-            // changes the position of the cursor so we set it to the initial position
-            ed.setPosition(position);
-          }
         }
       },
     },
@@ -82,8 +72,6 @@ const DEFAULT_ACTIONS: ContextMenuAction[] = [
       label: i18n.translate('sharedUXPackages.codeEditor.contextMenuAction.pasteActionLabel', {
         defaultMessage: 'Paste',
       }),
-      // eslint-disable-next-line no-bitwise
-      keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyV],
       contextMenuGroupId: '9_cutcopypaste',
       contextMenuOrder: 3,
       run: async (ed) => {