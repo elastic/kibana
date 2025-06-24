5ab1db780c64606035070f0fcf65a0a3068230c8
Julia Bardi <90178898+juliaElastic@users.noreply.github.com>
2025-06-24 08:43:39 +0200

    [Fleet] Update query in delete unenrolled agents task (#224808)
    Issue reported by users that some unenrolled agents on the UI are not
    being deleted by the background task.
    The cause turned out to be a discrepancy in how unenrolled agents are
    determined on the UI versus the background task.
    
    The UI shows agents as unenrolled if `active:false` field matches (logic
    [here](https://github.com/elastic/kibana/blob/main/x-pack/platform/plugins/shared/fleet/server/services/agents/build_status_runtime_field.ts#L115-L119))
    
    The delete unenrolled agents task also queried on the existence of
    `unenrolled_at` field.
    It seems some agents don't have this field set, those should be deleted
    too.

--- x-pack/platform/plugins/shared/fleet/server/tasks/delete_unenrolled_agents_task.ts
+++ x-pack/platform/plugins/shared/fleet/server/tasks/delete_unenrolled_agents_task.ts
@@ -117,7 +117,6 @@ export class DeleteUnenrolledAgentsTask {
                     active: false,
                   },
                 },
-                { exists: { field: 'unenrolled_at' } },
               ],
             },
           },
