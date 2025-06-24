f8503f4e485ed9e64dea22cb86ac2891018f7439
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2025-06-24 19:57:52 +0100

    siemv3

--- x-pack/solutions/security/plugins/elastic_assistant/common/constants.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/common/constants.ts
@@ -45,5 +45,5 @@ export const CAPABILITIES = `${BASE_PATH}/capabilities`;
 export const MINIMUM_AI_ASSISTANT_LICENSE = 'enterprise' as const;
 
 export const DEFAULT_DATE_FORMAT_TZ = 'dateFormat:tz' as const;
-export const SECURITY_FEATURE_ID = 'siemV2' as const;
+export const SECURITY_FEATURE_ID = 'siemV3' as const;
 export const CASES_FEATURE_ID = 'securitySolutionCasesV3' as const;
