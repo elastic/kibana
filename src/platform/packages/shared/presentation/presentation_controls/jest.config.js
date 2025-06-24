94d744f694d79a64f7fe4d5d88ffde9f7642e4f9
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-06-24 14:57:50 -0500

    Fix presentation-controls jest config

--- src/platform/packages/shared/presentation/presentation_controls/jest.config.js
+++ src/platform/packages/shared/presentation/presentation_controls/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../..',
-  roots: ['<rootDir>/src/platform/packages/private/kbn-controls-utils'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/presentation/presentation_controls'],
 };
