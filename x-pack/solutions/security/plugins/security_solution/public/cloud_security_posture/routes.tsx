1f43165134e6d1de29a5762d39c204e2d81b4cf2
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2025-06-24 08:51:28 +0200

    fix

--- x-pack/solutions/security/plugins/security_solution/public/cloud_security_posture/routes.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/cloud_security_posture/routes.tsx
@@ -9,14 +9,12 @@ import React from 'react';
 import { CLOUD_SECURITY_POSTURE_BASE_PATH } from '@kbn/cloud-security-posture-common';
 import type { CloudSecurityPosturePageId } from '@kbn/cloud-security-posture-plugin/public';
 import { type CspSecuritySolutionContext } from '@kbn/cloud-security-posture-plugin/public';
-import { useExpandableFlyoutApi } from '@kbn/expandable-flyout';
 import { SecurityPageName } from '../app/types';
 import type { SecuritySubPluginRoutes } from '../app/types';
 import { useKibana } from '../common/lib/kibana';
 import { SpyRoute } from '../common/utils/route/spy_routes';
 import { FiltersGlobal } from '../common/components/filters_global';
 import { PluginTemplateWrapper } from '../common/components/plugin_template_wrapper';
-import { useOnExpandableFlyoutClose } from '../flyout/shared/hooks/use_on_expandable_flyout_close';
 import { withSecurityRoutePageWrapper } from '../common/components/security_route_page_wrapper';
 
 // This exists only for the type signature cast
@@ -27,8 +25,6 @@ const CloudPostureSpyRoute = ({ pageName, ...rest }: { pageName?: CloudSecurityP
 const cspSecuritySolutionContext: CspSecuritySolutionContext = {
   getFiltersGlobalComponent: () => FiltersGlobal,
   getSpyRouteComponent: () => CloudPostureSpyRoute,
-  useExpandableFlyoutApi,
-  useOnExpandableFlyoutClose,
 };
 
 const CloudSecurityPosture = () => {
