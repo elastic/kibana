77dd7026129dac8de809e3fedc1751a5a35714a7
Shahzad <shahzad31comp@gmail.com>
2025-06-24 11:25:23 +0200

    tests

--- x-pack/solutions/observability/plugins/synthetics/server/routes/monitor_cruds/monitor_validation.ts
+++ x-pack/solutions/observability/plugins/synthetics/server/routes/monitor_cruds/monitor_validation.ts
@@ -247,8 +247,8 @@ export const normalizeAPIConfig = (monitor: CreateMonitorPayLoad) => {
   let unsupportedKeys = Object.keys(rawConfig).filter((key) => !supportedKeys.includes(key));
 
   const result = omit(rawConfig, unsupportedKeys);
-  let kSpaces = (rawConfig[ConfigKey.KIBANA_SPACES] as string[]) ?? [];
-  if (kSpaces.includes('*')) {
+  let kSpaces = rawConfig[ConfigKey.KIBANA_SPACES] as string[];
+  if (kSpaces?.includes('*')) {
     kSpaces = ['*'];
   }
 
@@ -258,7 +258,7 @@ export const normalizeAPIConfig = (monitor: CreateMonitorPayLoad) => {
     private_locations: _privateLocations,
     retest_on_failure: _retestOnFailure,
     custom_heartbeat_id: _customHeartbeatId,
-    spaces: kSpaces,
+    ...(kSpaces ? { [ConfigKey.KIBANA_SPACES]: kSpaces } : {}),
   } as CreateMonitorPayLoad;
 
   const requestBodyCheck = formattedConfig[ConfigKey.REQUEST_BODY_CHECK];
