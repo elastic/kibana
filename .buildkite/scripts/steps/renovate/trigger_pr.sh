bd515335dae0dc44c07ae27bba3db8f2e679f4c6
Brad White <brad.white@elastic.co>
2025-06-25 15:10:54 -0600

    pass gh env vars to pr trigger

--- .buildkite/scripts/steps/renovate/trigger_pr.sh
+++ .buildkite/scripts/steps/renovate/trigger_pr.sh
@@ -4,4 +4,9 @@ set -euo pipefail
 
 echo --- Triggering Kibana Pull Request Pipeline
 
-ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-pull-request "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT"
+GITHUB_ENV_VARS=()
+for var in $(env | grep ^GITHUB_ | cut -d= -f1); do
+  GITHUB_ENV_VARS+=("$var=${!var}")
+done
+
+ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-pull-request "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT" "" "${GITHUB_ENV_VARS[*]}"
