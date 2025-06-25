be17427b9260709090c05b667330d76f63dbd317
Brad White <brad.white@elastic.co>
2025-06-25 13:00:36 -0600

    add bootstrap

--- .buildkite/scripts/steps/renovate/renovate_helper.sh
+++ .buildkite/scripts/steps/renovate/renovate_helper.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-echo --- Called Renovate Helper
-
 source .buildkite/scripts/common/util.sh
+.buildkite/scripts/bootstrap.sh
 
 echo --- Deduplicate yarn.lock
 cmd="node scripts/yarn_deduplicate.js && yarn kbn bootstrap && node scripts/yarn_deduplicate.js"
