846eaf7ac46964b7d00b1082f33d4beca9f8f635
abhishekbhatia1710 <abhishek.bhatia@elastic.co>
2025-06-25 19:47:02 +0530

    Changes to replace hard coded css values to use euiTheme instead

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring/components/key_insights_panel/index.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring/components/key_insights_panel/index.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, useEuiTheme } from '@elastic/eui';
 import { css } from '@emotion/react';
 import type { DataViewSpec } from '@kbn/data-views-plugin/public';
 
@@ -17,55 +17,82 @@ import { GrantedRightsTile } from './granted_rights_tile';
 import { AccountSwitchesTile } from './account_switches_tile';
 import { AuthenticationsTile } from './authentications_tile';
 
-const tileStyles = css`
-  border: 1px solid #d3dae6;
-  border-radius: 6px;
-  padding: 12px;
-  height: 100%;
-`;
-
 export const KeyInsightsPanel: React.FC<{ spaceId: string; sourcerDataView: DataViewSpec }> = ({
   spaceId,
   sourcerDataView,
 }) => {
+  const { euiTheme } = useEuiTheme();
+
+  const tileStyles = css`
+    border: ${euiTheme.border.width.thin} solid ${euiTheme.colors.lightShade};
+    border-radius: ${euiTheme.border.radius.medium};
+    padding: ${euiTheme.size.s};
+    height: 100%;
+  `;
   return (
-    <EuiFlexGroup
-      wrap
-      css={css`
-        width: 100%;
-        gap: 16px;
-        & > * {
-          min-width: calc(33.33% - 11px) !important;
-          max-width: calc(33.33% - 11px) !important;
-        }
-      `}
-    >
-      <EuiFlexItem grow={false}>
+    <EuiFlexGroup wrap gutterSize="m" responsive={false} style={{ width: '100%' }}>
+      <EuiFlexItem
+        grow={false}
+        style={{
+          minWidth: `calc(33.33% - ${euiTheme.size.s})`,
+          maxWidth: `calc(33.33% - ${euiTheme.size.s})`,
+        }}
+      >
         <div css={tileStyles}>
           <ActivePrivilegedUsersTile spaceId={spaceId} sourcerDataView={sourcerDataView} />
         </div>
       </EuiFlexItem>
-      <EuiFlexItem grow={false}>
+      <EuiFlexItem
+        grow={false}
+        style={{
+          minWidth: `calc(33.33% - ${euiTheme.size.s})`,
+          maxWidth: `calc(33.33% - ${euiTheme.size.s})`,
+        }}
+      >
         <div css={tileStyles}>
           <AlertsTriggeredTile spaceId={spaceId} />
         </div>
       </EuiFlexItem>
-      <EuiFlexItem grow={false}>
+      <EuiFlexItem
+        grow={false}
+        style={{
+          minWidth: `calc(33.33% - ${euiTheme.size.s})`,
+          maxWidth: `calc(33.33% - ${euiTheme.size.s})`,
+        }}
+      >
         <div css={tileStyles}>
           <AnomaliesDetectedTile spaceId={spaceId} />
         </div>
       </EuiFlexItem>
-      <EuiFlexItem grow={false}>
+      <EuiFlexItem
+        grow={false}
+        style={{
+          minWidth: `calc(33.33% - ${euiTheme.size.s})`,
+          maxWidth: `calc(33.33% - ${euiTheme.size.s})`,
+        }}
+      >
         <div css={tileStyles}>
           <GrantedRightsTile spaceId={spaceId} sourcerDataView={sourcerDataView} />
         </div>
       </EuiFlexItem>
-      <EuiFlexItem grow={false}>
+      <EuiFlexItem
+        grow={false}
+        style={{
+          minWidth: `calc(33.33% - ${euiTheme.size.s})`,
+          maxWidth: `calc(33.33% - ${euiTheme.size.s})`,
+        }}
+      >
         <div css={tileStyles}>
           <AccountSwitchesTile spaceId={spaceId} sourcerDataView={sourcerDataView} />
         </div>
       </EuiFlexItem>
-      <EuiFlexItem grow={false}>
+      <EuiFlexItem
+        grow={false}
+        style={{
+          minWidth: `calc(33.33% - ${euiTheme.size.s})`,
+          maxWidth: `calc(33.33% - ${euiTheme.size.s})`,
+        }}
+      >
         <div css={tileStyles}>
           <AuthenticationsTile spaceId={spaceId} sourcerDataView={sourcerDataView} />
         </div>
