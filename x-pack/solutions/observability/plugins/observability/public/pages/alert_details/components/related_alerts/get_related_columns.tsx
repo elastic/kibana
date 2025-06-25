97ca6bfcad48766089e0cae8cf138d1f28c8527e
Bailey Cash <bailey.cash@elastic.co>
2025-06-25 11:53:28 -0400

    change verbiage from created to triggered

--- x-pack/solutions/observability/plugins/observability/public/pages/alert_details/components/related_alerts/get_related_columns.tsx
+++ x-pack/solutions/observability/plugins/observability/public/pages/alert_details/components/related_alerts/get_related_columns.tsx
@@ -27,7 +27,7 @@ export const getRelatedColumns = (): EuiDataGridColumn[] => {
     {
       id: ALERT_START,
       displayAsText: i18n.translate('xpack.observability.alertsTGrid.startColumnDescription', {
-        defaultMessage: 'Created',
+        defaultMessage: 'Triggered',
       }),
       initialWidth: 120,
       isSortable: false,
