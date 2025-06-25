905c6683351bda05f160f8bbb932f7067234e67c
Dominique Belcher <dominique.clarke@elastic.co>
2025-06-25 14:10:30 -0400

    adjust tests

--- x-pack/platform/test/cases_api_integration/security_and_spaces/tests/trial/attachments_framework/registered_persistable_state_trial.ts
+++ x-pack/platform/test/cases_api_integration/security_and_spaces/tests/trial/attachments_framework/registered_persistable_state_trial.ts
@@ -34,7 +34,6 @@ export default ({ getService }: FtrProviderContext): void => {
 
         expect(types).to.eql({
           '.lens': '78559fd806809ac3a1008942ead2a079864054f5',
-          '.page': '8e5143e79705c8cebe2d5d9949e22d52944629d0',
           '.test': 'ab2204830c67f5cf992c9aa2f7e3ead752cc60a1',
           aiopsChangePointChart: 'a1212d71947ec34487b374cecc47ab9941b5d91c',
           ml_anomaly_charts: '23e92e824af9db6e8b8bb1d63c222e04f57d2147',
