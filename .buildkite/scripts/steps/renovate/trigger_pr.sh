4ddba874d74fc5b33c0562a15e9bd96ef54bef88
Brad White <brad.white@elastic.co>
2025-06-25 13:17:22 -0600

    add commit to trigger pr

--- .buildkite/scripts/steps/renovate/trigger_pr.sh
+++ .buildkite/scripts/steps/renovate/trigger_pr.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 echo --- Triggering Kibana Pull Request Pipeline
 
-ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-pull-request "$BUILDKITE_BRANCH"
+ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-pull-request "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT"
