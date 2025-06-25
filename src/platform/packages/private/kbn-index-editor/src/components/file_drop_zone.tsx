75895edadb12eb50f5cf0b8d1809a77b8cc3b357
Dima Arnautov <dmitrii.arnautov@elastic.co>
2025-06-25 19:15:36 +0200

    scrollable table

--- src/platform/packages/private/kbn-index-editor/src/components/file_drop_zone.tsx
+++ src/platform/packages/private/kbn-index-editor/src/components/file_drop_zone.tsx
@@ -150,7 +150,7 @@ export const FileDropzone: FC<PropsWithChildren<{ noResults: boolean }>> = ({
 
   return (
     <FileSelectorContext.Provider value={{ onFileSelectorClick }}>
-      <div {...getRootProps()}>
+      <div {...getRootProps({ css: { height: '100%' } })}>
         {isDragActive ? <div css={overlayDraggingFile} /> : null}
         {showLoadingOverlay ? loadingIndicator : null}
         <input {...getInputProps()} />
--- src/platform/packages/private/kbn-index-editor/src/components/flyout_content.tsx
+++ src/platform/packages/private/kbn-index-editor/src/components/flyout_content.tsx
@@ -16,7 +16,7 @@ import type { FC } from 'react';
 import React, { lazy } from 'react';
 import useObservable from 'react-use/lib/useObservable';
 import { FormattedMessage } from '@kbn/i18n-react';
-import { css } from '@emotion/css';
+import { css } from '@emotion/react';
 import type { EditLookupIndexContentContext, FlyoutDeps } from '../types';
 import { CustomPanel } from './custom_panel';
 import { FileDropzone } from './file_drop_zone';
@@ -96,7 +96,11 @@ export const FlyoutContent: FC<FlyoutContentProps> = ({ deps, props }) => {
             `}
           >
             <FileDropzone noResults={noResults}>
-              <EuiFlexGroup direction="column" gutterSize="s" css={{ flex: 1 }}>
+              <EuiFlexGroup
+                direction="column"
+                gutterSize="s"
+                css={{ overflow: 'hidden', height: '100%' }}
+              >
                 <EuiFlexItem grow={false}>
                   <CustomPanel />
                 </EuiFlexItem>
