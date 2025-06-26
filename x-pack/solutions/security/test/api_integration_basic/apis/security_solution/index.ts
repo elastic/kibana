340202d889a66978f48088bff8b874ea044632c5
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-06-26 19:43:58 +0300

    [ska] relocate api_integration_basic & defend_workflows_cypress tests (#225374)
    ## Summary
    
    Part of https://github.com/elastic/kibana-team/issues/1503
    
    Before:
    
    ```
    x-pack/test/
         | - api_integration_basic/
         | - defend_workflows_cypress/
    ```
    
    After:
    ```
    x-pack/solutions/security/test/
         | - api_integration_basic/
         | - defend_workflows_cypress/
    ```
    
    ---------
    
    Co-authored-by: Cesare de Cal <cesare.decal@elastic.co>
    (cherry picked from commit 868a4657ee8060f4beb61b14d5db982876b03834)
    
    # Conflicts:
    #	.buildkite/ftr_security_serverless_configs.yml
    #	.buildkite/ftr_security_stateful_configs.yml
    #	.github/CODEOWNERS

--- x-pack/solutions/security/test/api_integration_basic/apis/security_solution/index.ts
+++ x-pack/solutions/security/test/api_integration_basic/apis/security_solution/index.ts
@@ -8,7 +8,7 @@
 import { FtrProviderContext } from '../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
-  describe('SecuritySolution Endpoints basic licsense', () => {
+  describe('Security Solution Endpoints basic license', () => {
     loadTestFile(require.resolve('./cases_privileges'));
   });
 }
