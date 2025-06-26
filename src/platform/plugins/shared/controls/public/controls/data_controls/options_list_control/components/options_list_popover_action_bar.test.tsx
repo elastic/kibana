74b472bb894d0326136aeb6636503a5dd342a466
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2025-06-26 11:47:15 +0200

    make the test more user focused
    make the

--- src/platform/plugins/shared/controls/public/controls/data_controls/options_list_control/components/options_list_popover_action_bar.test.tsx
+++ src/platform/plugins/shared/controls/public/controls/data_controls/options_list_control/components/options_list_popover_action_bar.test.tsx
@@ -8,9 +8,7 @@
  */
 
 import React from 'react';
-
-import { render } from '@testing-library/react';
-
+import { render, screen } from '@testing-library/react';
 import { take } from 'lodash';
 import { getOptionsListContextMock } from '../../mocks/api_mocks';
 import { OptionsListControlContext } from '../options_list_context_provider';
@@ -19,64 +17,71 @@ import { OptionsListPopoverActionBar } from './options_list_popover_action_bar';
 import { OptionsListDisplaySettings } from '../../../../../common/options_list';
 import { MAX_OPTIONS_LIST_BULK_SELECT_SIZE } from '../constants';
 
-describe('Options list popover', () => {
-  const allOptions = [
-    { value: 'moo', docCount: 1 },
-    { value: 'tweet', docCount: 2 },
-    { value: 'oink', docCount: 3 },
-    { value: 'bark', docCount: 4 },
-    { value: 'meow', docCount: 5 },
-    { value: 'woof', docCount: 6 },
-    { value: 'roar', docCount: 7 },
-    { value: 'honk', docCount: 8 },
-    { value: 'beep', docCount: 9 },
-    { value: 'chirp', docCount: 10 },
-    { value: 'baa', docCount: 11 },
-    { value: 'toot', docCount: 11 },
-  ];
+const allOptions = [
+  { value: 'moo', docCount: 1 },
+  { value: 'tweet', docCount: 2 },
+  { value: 'oink', docCount: 3 },
+  { value: 'bark', docCount: 4 },
+  { value: 'meow', docCount: 5 },
+  { value: 'woof', docCount: 6 },
+  { value: 'roar', docCount: 7 },
+  { value: 'honk', docCount: 8 },
+  { value: 'beep', docCount: 9 },
+  { value: 'chirp', docCount: 10 },
+  { value: 'baa', docCount: 11 },
+  { value: 'toot', docCount: 11 },
+];
 
-  const mountComponent = ({
-    componentApi,
-    displaySettings,
-    showOnlySelected,
-  }: {
-    componentApi: OptionsListComponentApi;
-    displaySettings: OptionsListDisplaySettings;
-    showOnlySelected?: boolean;
-  }) => {
-    return render(
-      <OptionsListControlContext.Provider
-        value={{
-          componentApi,
-          displaySettings,
-        }}
-      >
-        <OptionsListPopoverActionBar
-          showOnlySelected={showOnlySelected ?? false}
-          setShowOnlySelected={() => {}}
-        />
-      </OptionsListControlContext.Provider>
-    );
-  };
+
+const renderComponent = ({
+  componentApi,
+  displaySettings,
+  showOnlySelected,
+}: {
+  componentApi: OptionsListComponentApi;
+  displaySettings: OptionsListDisplaySettings;
+  showOnlySelected?: boolean;
+}) => {
+  return render(
+    <OptionsListControlContext.Provider
+      value={{
+        componentApi,
+        displaySettings,
+      }}
+    >
+      <OptionsListPopoverActionBar
+        showOnlySelected={showOnlySelected ?? false}
+        setShowOnlySelected={() => {}}
+      />
+    </OptionsListControlContext.Provider>
+  );
+};
+
+const getSelectAllCheckbox = () => screen.getByRole('checkbox', { name: /Select all/i });
+
+const getSearchInput = () => screen.getByRole('searchbox', { name: /Filter suggestions/i });
+
+
+describe('Options list popover', () => {
 
   test('displays search input', async () => {
     const contextMock = getOptionsListContextMock();
     contextMock.componentApi.setTotalCardinality(allOptions.length);
     contextMock.componentApi.setAvailableOptions(take(allOptions, 5));
     contextMock.componentApi.setSearchString('moo');
-    const actionBarComponent = mountComponent(contextMock);
+    renderComponent(contextMock);
 
-    expect(actionBarComponent.queryByTestId('optionsList-control-search-input')).toBeEnabled();
-    expect(actionBarComponent.queryByTestId('optionsList-control-search-input')).toHaveValue('moo');
+    expect(getSearchInput()).toBeEnabled();
+    expect(getSearchInput()).toHaveValue('moo');
   });
 
   test('displays total cardinality for available options', async () => {
     const contextMock = getOptionsListContextMock();
     contextMock.componentApi.setTotalCardinality(allOptions.length);
     contextMock.componentApi.setAvailableOptions(take(allOptions, 5));
-    const actionBarComponent = mountComponent(contextMock);
-
-    expect(actionBarComponent.queryByTestId('optionsList-cardinality-label')).toHaveTextContent(
+    renderComponent(contextMock);
+    
+    expect(screen.getByTestId('optionsList-cardinality-label')).toHaveTextContent(
       allOptions.length.toString()
     );
   });
@@ -85,10 +90,10 @@ describe('Options list popover', () => {
     const contextMock = getOptionsListContextMock();
     contextMock.componentApi.setTotalCardinality(80);
     contextMock.componentApi.setAvailableOptions(take(allOptions, 10));
-    const actionBarComponent = mountComponent(contextMock);
+    renderComponent(contextMock);
 
-    expect(actionBarComponent.queryByTestId('optionsList-control-selectAll')).toBeEnabled();
-    expect(actionBarComponent.queryByTestId('optionsList-control-selectAll')).not.toBeChecked();
+    expect(getSelectAllCheckbox()).toBeEnabled();
+    expect(getSelectAllCheckbox()).not.toBeChecked();
   });
 
   test('Select all is checked when all available options are selected ', async () => {
@@ -96,36 +101,36 @@ describe('Options list popover', () => {
     contextMock.componentApi.setTotalCardinality(80);
     contextMock.componentApi.setAvailableOptions([{ value: 'moo', docCount: 1 }]);
     contextMock.componentApi.setSelectedOptions(['moo']);
-    const actionBarComponent = mountComponent(contextMock);
+    renderComponent(contextMock);
 
-    expect(actionBarComponent.queryByTestId('optionsList-control-selectAll')).toBeEnabled();
-    expect(actionBarComponent.queryByTestId('optionsList-control-selectAll')).toBeChecked();
+    expect(getSelectAllCheckbox()).toBeEnabled();
+    expect(getSelectAllCheckbox()).toBeChecked();
   });
 
   test('bulk selections are disabled when there are more than 100 available options', async () => {
     const contextMock = getOptionsListContextMock();
     contextMock.componentApi.setTotalCardinality(MAX_OPTIONS_LIST_BULK_SELECT_SIZE + 1);
     contextMock.componentApi.setAvailableOptions(take(allOptions, 10));
-    const actionBarComponent = mountComponent(contextMock);
+    renderComponent(contextMock);
 
-    expect(actionBarComponent.queryByTestId('optionsList-control-selectAll')).toBeDisabled();
+    expect(getSelectAllCheckbox()).toBeDisabled();
   });
 
   test('bulk selections are disabled when there are no available options', async () => {
     const contextMock = getOptionsListContextMock();
     contextMock.componentApi.setTotalCardinality(0);
     contextMock.componentApi.setAvailableOptions([]);
-    const actionBarComponent = mountComponent(contextMock);
+    renderComponent(contextMock);
 
-    expect(actionBarComponent.queryByTestId('optionsList-control-selectAll')).toBeDisabled();
+    expect(getSelectAllCheckbox()).toBeDisabled();
   });
 
   test('bulk selections are disabled when showOnlySelected is true', async () => {
     const contextMock = getOptionsListContextMock();
     contextMock.componentApi.setTotalCardinality(0);
     contextMock.componentApi.setAvailableOptions([]);
-    const actionBarComponent = mountComponent({ ...contextMock, showOnlySelected: true });
+    renderComponent({ ...contextMock, showOnlySelected: true });
 
-    expect(actionBarComponent.queryByTestId('optionsList-control-selectAll')).toBeDisabled();
+    expect(getSelectAllCheckbox()).toBeDisabled();
   });
 });
