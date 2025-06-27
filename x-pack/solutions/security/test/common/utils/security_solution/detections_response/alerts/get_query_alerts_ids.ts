0d09d679d99de14d9c98641becf4b837e603de84
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-06-27 10:42:04 +0200

    [ska][x-pack] relocate security_solution_api_integration

---
+++ x-pack/solutions/security/test/common/utils/security_solution/detections_response/alerts/get_query_alerts_ids.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { ALERT_RULE_UUID } from '@kbn/rule-data-utils';
+
+/**
+ * Given an array of ids for a test this will get the alerts
+ * created from that rule's regular id.
+ * @param ids The rule_id to search for alerts
+ */
+export const getQueryAlertsId = (ids: string[], size = 10) => ({
+  size,
+  sort: ['@timestamp'],
+  query: {
+    terms: {
+      [ALERT_RULE_UUID]: ids,
+    },
+  },
+});
--- x-pack/test/functional/es_archives/security_solution/README.md
+++ x-pack/test/functional/es_archives/security_solution/README.md
@@ -1,7 +1,7 @@
 Collection of data sets for use within various tests. Most of the tests to these live in either:
 
 ```
-x-pack/test/security_solution_api_integration/test_suites/
+x-pack/solutions/security/test/security_solution_api_integration/test_suites/
 ```
 
 or
@@ -10,4 +10,4 @@ or
 x-pack/test/api_integration/apis/security_solution
 ```
 
-- Folder `telemetry` is for the tests underneath `x-pack/test/security_solution_api_integration/test_suites/detections_response/telemetry`.
+- Folder `telemetry` is for the tests underneath `x-pack/solutions/security/test/security_solution_api_integration/test_suites/detections_response/telemetry`.
