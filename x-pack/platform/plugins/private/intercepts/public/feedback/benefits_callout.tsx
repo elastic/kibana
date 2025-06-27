fff87c0b7e605b2e57857af595eea38acbea6eee
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-06-27 11:29:35 +0200

    CSS changes

---
+++ x-pack/platform/plugins/private/intercepts/public/feedback/benefits_callout.tsx
@@ -0,0 +1,43 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { EuiCallOut, EuiLink, EuiSpacer } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n-react';
+
+const ELASTIC_SUPPORT_LINK = 'https://support.elastic.co/';
+
+export const BenefitsCallout = () => (
+  <>
+    <EuiSpacer size="s" />
+    <EuiCallOut
+      color="warning"
+      title={
+        <FormattedMessage
+          id="xpack.intercept.feedbackFlyout.platinumOrHigherCallout.title"
+          defaultMessage="Use your Platinum license benefits instead"
+        />
+      }
+    >
+      <FormattedMessage
+        id="xpack.intercept.feedbackFlyout.platinumOrHigherCallout.content"
+        defaultMessage="Please submit your improvement request in the dedicated {supportLink} so we can get back to you faster."
+        values={{
+          supportLink: (
+            <EuiLink href={ELASTIC_SUPPORT_LINK} target="_blank" external={true}>
+              <FormattedMessage
+                id="xpack.intercept.feedbackFlyout.platinumOrHigherCallout.supportLink"
+                defaultMessage="support channel"
+              />
+            </EuiLink>
+          ),
+        }}
+      />
+    </EuiCallOut>
+    <EuiSpacer size="s" />
+  </>
+);
