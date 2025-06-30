ca3ee1807eb86a06d5ca41f857c1f67224ceb39b
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2025-06-30 09:21:51 +0100

    settings change

--- src/platform/plugins/shared/ai_assistant_management/selection/server/config.ts
+++ src/platform/plugins/shared/ai_assistant_management/selection/server/config.ts
@@ -18,6 +18,7 @@ const configSchema = schema.object({
       schema.literal(AIAssistantType.Default),
       schema.literal(AIAssistantType.Never),
       schema.literal(AIAssistantType.Observability),
+      schema.literal(AIAssistantType.Security),
     ],
     { defaultValue: AIAssistantType.Default }
   ),
--- x-pack/solutions/security/plugins/elastic_assistant/public/src/hooks/is_nav_control_visible/use_is_nav_control_visible.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/public/src/hooks/is_nav_control_visible/use_is_nav_control_visible.ts
@@ -18,9 +18,19 @@ function getVisibility(
 ) {
   // The "Global assistant" stack management setting for the security assistant still needs to be developed.
   // In the meantime, while testing, show the Security assistant everywhere except in Observability.
+  if (preferredAssistantType === AIAssistantType.Never) {
+    return false;
+  }
 
   const categoryId =
     (appId && applications.get(appId)?.category?.id) || DEFAULT_APP_CATEGORIES.kibana.id;
+
+  if (preferredAssistantType === AIAssistantType.Security) {
+    return (
+      categoryId !== DEFAULT_APP_CATEGORIES.observability.id &&
+      categoryId !== DEFAULT_APP_CATEGORIES.enterpriseSearch.id
+    );
+  }
 
   return DEFAULT_APP_CATEGORIES.security.id === categoryId;
 }
