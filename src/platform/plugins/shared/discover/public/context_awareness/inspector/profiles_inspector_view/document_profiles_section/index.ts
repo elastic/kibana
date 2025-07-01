b884ae1c1779edc24421aab9cb783a5449d22b6f
Alejandro García Parrondo <31973472+AlexGPlay@users.noreply.github.com>
2025-07-01 08:20:33 +0200

    [Discover] Add profiles inspector (#184077) (#222999)
    ## Summary
    
    Closes https://github.com/elastic/kibana/issues/184077
    
    Adds a new tab to the inspector flyout that shows the contexts/profiles
    that are currently active.
    
    
    https://github.com/user-attachments/assets/8cdde24c-169a-4679-ab9d-3edfa83a417f
    
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios

--- src/platform/plugins/shared/discover/public/context_awareness/inspector/profiles_inspector_view/document_profiles_section/index.ts
+++ src/platform/plugins/shared/discover/public/context_awareness/inspector/profiles_inspector_view/document_profiles_section/index.ts
@@ -7,6 +7,4 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export * from './context_awareness';
-export * from './data_table_record_with_context';
-export * from './profiles';
+export { DocumentProfilesSection } from './document_profiles_section';
--- src/platform/plugins/shared/discover/public/context_awareness/inspector/profiles_inspector_view/index.ts
+++ src/platform/plugins/shared/discover/public/context_awareness/inspector/profiles_inspector_view/index.ts
@@ -7,6 +7,5 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export * from './context_awareness';
-export * from './data_table_record_with_context';
-export * from './profiles';
+// eslint-disable-next-line import/no-default-export
+export { ProfilesInspectorView as default } from './profiles_inspector_view';
