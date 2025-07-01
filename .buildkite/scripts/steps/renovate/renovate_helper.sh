e7c112cae6dc315427e8265604bb8e5fdc4d037b
Brad White <brad.white@elastic.co>
2025-06-30 18:07:23 -0600

    Revert "test labeller"
    This reverts commit ee053244213ca59f123c42b6ebd528607fdc5851.

--- .buildkite/scripts/steps/renovate/renovate_helper.sh
+++ .buildkite/scripts/steps/renovate/renovate_helper.sh
@@ -12,7 +12,7 @@ check_for_changed_files "$cmd" true
 
 echo --- Additional helpers
 # We only want the deploy label on the main branch instead of all branches in the Renovate group
-if [ "$GITHUB_PR_BRANCH" = "feature/213293/renovate-helper-scripts" ] && ! is_pr_with_label "ci:cloud-deploy"; then
+if [ "$GITHUB_PR_BRANCH" = "renovate/main-chainguard" ] && ! is_pr_with_label "ci:cloud-deploy"; then
   echo "Adding deploy label to main chainguard PR"
   gh pr edit "${GITHUB_PR_NUMBER}" --add-label "ci:cloud-deploy"
 fi
