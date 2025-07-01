f9ee2d0fa298430f9464b76c363138bfedef2ffe
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-01 15:11:41 +0200

    cleanup

--- x-pack/test/api_integration/deployment_agnostic/services/index.ts
+++ x-pack/test/api_integration/deployment_agnostic/services/index.ts
@@ -9,13 +9,8 @@ import { commonFunctionalServices } from '@kbn/ftr-common-functional-services';
 import { AlertingApiProvider } from './alerting_api';
 import { DataViewApiProvider } from './data_view_api';
 import { deploymentAgnosticServices } from './deployment_agnostic_services';
-import { PackageApiProvider } from './package_api';
 import { RoleScopedSupertestProvider, SupertestWithRoleScope } from './role_scoped_supertest';
 import { CustomRoleScopedSupertestProvider } from './custom_role_scoped_supertest';
-import { SloApiProvider } from './slo_api';
-import { SynthtraceProvider } from './synthtrace';
-import { ApmApiProvider } from './apm_api';
-import { ObservabilityAIAssistantApiProvider } from './observability_ai_assistant_api';
 
 export type {
   InternalRequestHeader,
@@ -29,14 +24,9 @@ export const services = {
   samlAuth: commonFunctionalServices.samlAuth,
   alertingApi: AlertingApiProvider,
   dataViewApi: DataViewApiProvider,
-  packageApi: PackageApiProvider,
-  sloApi: SloApiProvider,
   roleScopedSupertest: RoleScopedSupertestProvider,
   customRoleScopedSupertest: CustomRoleScopedSupertestProvider,
   // create a new deployment-agnostic service and load here
-  synthtrace: SynthtraceProvider,
-  apmApi: ApmApiProvider,
-  observabilityAIAssistantApi: ObservabilityAIAssistantApiProvider,
 };
 
 export type SupertestWithRoleScopeType = SupertestWithRoleScope;
