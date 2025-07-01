95ee11ff69d19bd30042e51ba1e30976c350e502
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-07-01 13:48:49 +0200

    Add footer tests

---
+++ x-pack/platform/plugins/private/intercepts/public/feedback/flyout/feedback_flyout_footer.test.tsx
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { screen, fireEvent } from '@testing-library/react';
+import { FeedbackFlyoutFooter } from './feedback_flyout_footer';
+import { renderWithI18n } from '@kbn/test-jest-helpers';
+
+describe('FeedbackFlyoutFooter', () => {
+  const submitFeedback = jest.fn();
+
+  it('renders flyout footer', () => {
+    renderWithI18n(
+      <FeedbackFlyoutFooter isSendFeedbackButtonDisabled={false} submitFeedback={submitFeedback} />
+    );
+    expect(screen.getByTestId('feedbackFlyoutFooter')).toBeInTheDocument();
+
+    const sendButton = screen.getByTestId('sendFeedbackButton');
+
+    expect(sendButton).toBeEnabled();
+  });
+
+  it('renders disabled send button when isSendFeedbackButtonDisabled is true', () => {
+    renderWithI18n(
+      <FeedbackFlyoutFooter isSendFeedbackButtonDisabled={true} submitFeedback={submitFeedback} />
+    );
+    expect(screen.getByTestId('feedbackFlyoutFooter')).toBeInTheDocument();
+
+    const sendButton = screen.getByTestId('sendFeedbackButton');
+
+    expect(sendButton).toBeDisabled();
+  });
+
+  it('calls submitFeedback when send button is clicked', () => {
+    renderWithI18n(
+      <FeedbackFlyoutFooter isSendFeedbackButtonDisabled={false} submitFeedback={submitFeedback} />
+    );
+    expect(screen.getByTestId('feedbackFlyoutFooter')).toBeInTheDocument();
+
+    const sendButton = screen.getByTestId('sendFeedbackButton');
+
+    fireEvent.click(sendButton);
+
+    expect(submitFeedback).toHaveBeenCalledTimes(1);
+  });
+});
