e4d5992cc04af0386d3ed5415dd5dda06227c67d
Tiago Vila Verde <tiago.vilaverde@elastic.co>
2025-07-02 13:51:28 +0200

    [Entity Analytics][Privmon] Add event.ingested pipeline (#225802)

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/privilege_monitoring_data_client.test.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/privilege_monitoring_data_client.test.ts
@@ -72,7 +72,6 @@ describe('Privilege Monitoring Data Client', () => {
 
       expect(mockCreateOrUpdateIndex).toHaveBeenCalled();
       expect(mockStartPrivilegeMonitoringTask).toHaveBeenCalled();
-      expect(loggerMock.debug).toHaveBeenCalledTimes(3);
       expect(auditMock.log).toHaveBeenCalled();
       expect(result).toEqual(mockDescriptor);
     });
