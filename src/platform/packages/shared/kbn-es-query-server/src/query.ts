bbc5d4384a793d9ee5fed35035af48ac21dbf4e2
Nick Peihl <nick.peihl@elastic.co>
2025-07-02 15:05:29 -0400

    Add query schema and type

---
+++ src/platform/packages/shared/kbn-es-query-server/src/query.ts
@@ -0,0 +1,30 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { schema, Type } from '@kbn/config-schema';
+
+export const querySchema = <T extends string[]>(supportedLanguages?: T) => {
+  const languageSchema =
+    supportedLanguages && supportedLanguages.length > 0
+      ? schema.oneOf(supportedLanguages.map((lang) => schema.literal(lang)) as [Type<T[number]>])
+      : schema.string();
+
+  return schema.object({
+    query: schema.oneOf([
+      schema.string({
+        meta: {
+          description:
+            'A text-based query such as Kibana Query Language (KQL) or Lucene query language.',
+        },
+      }),
+      schema.recordOf(schema.string(), schema.any()),
+    ]),
+    language: languageSchema,
+  });
+};
--- src/platform/packages/shared/kbn-es-query-server/src/types.ts
+++ src/platform/packages/shared/kbn-es-query-server/src/types.ts
@@ -10,7 +10,10 @@
 import { TypeOf } from '@kbn/config-schema';
 import { Writable } from '@kbn/utility-types';
 import { timeRangeSchema, absoluteTimeRangeSchema, relativeTimeRangeSchema } from './time_range';
+import { querySchema } from './query';
 
 export type TimeRange = Writable<TypeOf<typeof timeRangeSchema>>;
 export type AbsoluteTimeRange = TypeOf<typeof absoluteTimeRangeSchema>;
 export type RelativeTimeRange = TypeOf<typeof relativeTimeRangeSchema>;
+
+export type Query = Writable<TypeOf<ReturnType<typeof querySchema>>>;
