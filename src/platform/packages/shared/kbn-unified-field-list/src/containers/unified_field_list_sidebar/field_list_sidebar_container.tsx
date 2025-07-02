c0f97b7b0ab87acc1c03b2e1c93c77fb0a7fb9d2
Julia Rechkunova <julia.rechkunova@elastic.co>
2025-07-02 12:11:04 +0200

    [Discover][Restorable State] Wrap with memo by default (#225667)
    - Follow up for
    https://github.com/elastic/kibana/pull/224299#discussion_r2160730000
    
    ## Summary
    
    This PR improves the restorable state package by memoizing the component
    by default as suggested in the comment by @davismcphee
    
    🤔 Should we backport to 9.1? The data table was not wrapped yet with
    memo in https://github.com/elastic/kibana/pull/224299.
    
    (cherry picked from commit 77070c9e17f34620a11afeb4269b0aa525b3c48a)
    
    # Conflicts:
    #	src/platform/plugins/shared/discover/public/application/main/components/layout/discover_resizable_layout.tsx

--- src/platform/packages/shared/kbn-unified-field-list/src/containers/unified_field_list_sidebar/field_list_sidebar_container.tsx
+++ src/platform/packages/shared/kbn-unified-field-list/src/containers/unified_field_list_sidebar/field_list_sidebar_container.tsx
@@ -8,7 +8,6 @@
  */
 
 import React, {
-  memo,
   useCallback,
   useState,
   forwardRef,
@@ -91,7 +90,7 @@ const InternalUnifiedFieldListSidebarContainer: React.FC<
 };
 
 const UnifiedFieldListSidebarContainerWithRestorableState = withRestorableState(
-  memo(InternalUnifiedFieldListSidebarContainer)
+  InternalUnifiedFieldListSidebarContainer
 );
 
 type UnifiedFieldListSidebarContainerPropsWithRestorableState = ComponentProps<
