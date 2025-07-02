358922660e10a81fbb8002f6bb78367816a670d8
christineweng <18648970+christineweng@users.noreply.github.com>
2025-07-02 11:04:31 -0500

    [Security Solution] Update entity links to open flyout (#225381)
    ## Summary
    
    This PR updates various entity links to open the flyout instead of going
    to another page. Refactored `PreviewLink` to `FlyoutLink`. It will check
    whether a flyout is opened, if so it will open preview, if not it will
    open a new flyout.
    
    - Alert/Event flyout -> Details panel -> Insights -> Entity
       - Ip links opens network previews
    
    ![image](https://github.com/user-attachments/assets/99f294e4-5812-42c8-b367-0697661e2f03)
    
    - IP flyout
       - Host ID and host name opens host preview
    
    ![image](https://github.com/user-attachments/assets/465a4ff2-9367-4960-9274-57e15ac688ea)
    
    - Host overview (page for 1 host)
       - Ips at the top section opens IP flyout 
    
    
    ![image](https://github.com/user-attachments/assets/8bd9a2c7-29ca-4b06-8ee7-5de95d1a7fe0)
    
    - User overview (page for 1 user)
       - Ips at the top section opens IP flyout 
    
    ![image](https://github.com/user-attachments/assets/2465fcec-2a01-4d96-b3d9-10b347c46b4f)
    
    - Network overview (page for 1 IP)
       - Host name and host id at the top section opens host flyout 
    
    ![image](https://github.com/user-attachments/assets/0a9990ca-fa25-4d13-8920-3108c6c21701)
    
    - IP address in event renderer opens flyout/preview 
    
    ![image](https://github.com/user-attachments/assets/7f99c1a3-9b73-48b0-a8ec-eaaa5c2f4b10)
    
    ![image](https://github.com/user-attachments/assets/16c6447f-2dbd-439d-865d-7a52bdd2ced5)
    
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- x-pack/solutions/security/plugins/security_solution/public/flyout/shared/components/test_ids.ts
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/shared/components/test_ids.ts
@@ -8,6 +8,7 @@
 import { PREFIX } from '../test_ids';
 
 export const FLYOUT_PREVIEW_LINK_TEST_ID = `${PREFIX}PreviewLink` as const;
+export const FLYOUT_LINK_TEST_ID = `${PREFIX}Link` as const;
 
 export const FLYOUT_ERROR_TEST_ID = `${PREFIX}Error` as const;
 export const FLYOUT_LOADING_TEST_ID = `${PREFIX}Loading` as const;
