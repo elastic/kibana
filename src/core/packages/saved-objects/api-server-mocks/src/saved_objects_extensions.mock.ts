a15e67f2ddd5889eae6bac102ef159542eaf3461
Sid <siddharthmantri1@gmail.com>
2025-07-02 22:13:32 +0200

    Update types

--- src/core/packages/saved-objects/api-server-mocks/src/saved_objects_extensions.mock.ts
+++ src/core/packages/saved-objects/api-server-mocks/src/saved_objects_extensions.mock.ts
@@ -45,6 +45,7 @@ const createSecurityExtension = (): jest.Mocked<ISavedObjectsSecurityExtension> 
   getCurrentUser: jest.fn(),
   includeSavedObjectNames: jest.fn(),
   authorizeChangeOwnership: jest.fn(),
+  authorizeChangeAccessMode: jest.fn(),
 });
 
 const createSpacesExtension = (): jest.Mocked<ISavedObjectsSpacesExtension> => ({
