eeb476f254767f97af50450a1c294aae19359cc7
Shahzad <shahzad31comp@gmail.com>
2025-07-03 12:18:31 +0200

    [Logs Essential] Limit set of rules !! (#226255)
    ## Summary
    
    Fixes https://github.com/elastic/observability-dev/issues/4501 !!
    
    <img width="1724" alt="image"
    src="https://github.com/user-attachments/assets/f061114b-fdd8-4641-99b3-c17df06cefe2"
    />
    
    
    ### Tests
    Added an API test !!

--- x-pack/test_serverless/api_integration/test_suites/observability/logs_essentials_only/connector_and_rules.ts
+++ x-pack/test_serverless/api_integration/test_suites/observability/logs_essentials_only/connector_and_rules.ts
@@ -49,5 +49,18 @@ export default function ({ getService }: FtrProviderContext) {
         '.resilient',
       ]);
     });
+
+    it('limit set of available rules', async () => {
+      const resp = await supertestAdminWithCookieCredentials
+        .get('/api/alerting/rule_types')
+        .set(svlCommonApi.getInternalRequestHeader())
+        .expect(200);
+      const listIds = resp.body.map((item: { id: string; enabled: boolean }) => item.id);
+      expect(listIds).to.eql([
+        '.es-query',
+        'observability.rules.custom_threshold',
+        'datasetQuality.degradedDocs',
+      ]);
+    });
   });
 }
