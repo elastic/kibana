27f6c712d7119efc469388ee7f11f388be1452e8
Brad White <brad.white@elastic.co>
2025-07-02 21:30:47 -0600

    Revert "Reapply "test labeller""
    This reverts commit 31787c6dd8b09b4adf6272ce419df591db8f498a.

--- .buildkite/scripts/steps/renovate/renovate_helper.sh
+++ .buildkite/scripts/steps/renovate/renovate_helper.sh
@@ -12,7 +12,7 @@ check_for_changed_files "$cmd" true
 
 echo --- Additional helpers
 # We only want the deploy label on the main branch instead of all branches in the Renovate group
-if [ "$GITHUB_PR_BRANCH" = "feature/213293/renovate-helper-scripts" ] && ! is_pr_with_label "ci:cloud-deploy"; then
+if [ "$GITHUB_PR_BRANCH" = "renovate/main-chainguard" ] && ! is_pr_with_label "ci:cloud-deploy"; then
   echo "Adding deploy label to main chainguard PR"
   gh api "repos/elastic/kibana/issues/${GITHUB_PR_NUMBER}/labels" --method POST -f "labels[]=ci:cloud-deploy" >/dev/null
 fi
