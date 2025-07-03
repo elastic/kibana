6fca74be3fc2e7af6f9cba14e3ce840af982d0db
Brad White <brad.white@elastic.co>
2025-07-02 20:54:17 -0600

    git config and use api method

--- .buildkite/scripts/steps/renovate/renovate_helper.sh
+++ .buildkite/scripts/steps/renovate/renovate_helper.sh
@@ -14,5 +14,8 @@ echo --- Additional helpers
 # We only want the deploy label on the main branch instead of all branches in the Renovate group
 if [ "$GITHUB_PR_BRANCH" = "feature/213293/renovate-helper-scripts" ] && ! is_pr_with_label "ci:cloud-deploy"; then
   echo "Adding deploy label to main chainguard PR"
-  GH_TOKEN="$GITHUB_TOKEN" gh pr edit "${GITHUB_PR_NUMBER}" --add-label "ci:cloud-deploy"
+  git config --global user.name kibanamachine
+  git config --global user.email '42973632+kibanamachine@users.noreply.github.com'
+  # gh pr edit "${GITHUB_PR_NUMBER}" --add-label "ci:cloud-deploy"
+  gh api "repos/elastic/kibana/issues/${GITHUB_PR_NUMBER}/labels" --method POST -f "labels[]=ci:cloud-deploy"
 fi
