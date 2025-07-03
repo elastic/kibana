e2e21795b2aae7e170fb9e786e6cc42f5b5cf10e
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-07-03 12:19:37 +0200

    fix types: make generic symbol for group node consistent with other usages

--- src/platform/packages/shared/shared-ux/document_data_cascader/impl/src/components/data_cascade_provider/index.tsx
+++ src/platform/packages/shared/shared-ux/document_data_cascader/impl/src/components/data_cascade_provider/index.tsx
@@ -28,27 +28,29 @@ import {
 export type { GroupNode, LeafNode, IStoreState, IDispatchAction } from './reducers';
 export { storeReducer } from './reducers';
 
-interface IStoreContext<N extends GroupNode, L extends LeafNode> {
-  state: IStoreState<N, L>;
-  dispatch: Dispatch<IDispatchAction<N, L>>;
+interface IStoreContext<G extends GroupNode, L extends LeafNode> {
+  state: IStoreState<G, L>;
+  dispatch: Dispatch<IDispatchAction<G, L>>;
 }
 
 interface IDataCascadeProviderProps {
   cascadeGroups: string[];
 }
 
-export const createStoreContext = once(<N extends GroupNode, L extends LeafNode>() => {
+export const createStoreContext = once(<G extends GroupNode, L extends LeafNode>() => {
   enableMapSet(); // Enable Map and Set support for immer
-  return createContext<IStoreContext<N, L> | null>(null);
+  return createContext<IStoreContext<G, L> | null>(null);
 });
 
-export function DataCascadeProvider<N extends GroupNode, L extends LeafNode>({
+export function DataCascadeProvider<G extends GroupNode, L extends LeafNode>({
   cascadeGroups,
   children,
 }: PropsWithChildren<IDataCascadeProviderProps>) {
-  const StoreContext = createStoreContext<N, L>();
+  const StoreContext = createStoreContext<G, L>();
 
-  const initialState = useRef<IStoreContext<N, L>['state']>({
+  type StoreContextState = IStoreContext<G, L>['state'];
+
+  const initialState = useRef<StoreContextState>({
     groupNodes: [],
     leafNodes: new Map<string, L[]>(), // TODO: consider externalizing this so the consumer might provide their own external cache
     groupByColumns: [],
@@ -56,7 +58,7 @@ export function DataCascadeProvider<N extends GroupNode, L extends LeafNode>({
   });
 
   const createInitialState = useCallback(
-    (state: IStoreContext<N, L>['state']): IStoreContext<N, L>['state'] => {
+    (state: StoreContextState): StoreContextState => {
       return {
         ...state,
         groupByColumns: cascadeGroups,
@@ -71,8 +73,8 @@ export function DataCascadeProvider<N extends GroupNode, L extends LeafNode>({
   return <StoreContext.Provider value={{ state, dispatch }}>{children}</StoreContext.Provider>;
 }
 
-function useDataCascadeStore<T extends GroupNode, L extends LeafNode>() {
-  const ctx = useContext(createStoreContext<T, L>());
+function useDataCascadeStore<G extends GroupNode, L extends LeafNode>() {
+  const ctx = useContext(createStoreContext<G, L>());
   if (!ctx) {
     throw new Error('useDataCascadeStore must be used within a DataCascadeProvider');
   }
@@ -87,17 +89,17 @@ export function useDataCascadeDispatch<
   return ctx.dispatch;
 }
 
-export function useDataCascadeState<T extends GroupNode, L extends LeafNode>() {
-  const ctx = useDataCascadeStore<T, L>();
+export function useDataCascadeState<G extends GroupNode, L extends LeafNode>() {
+  const ctx = useDataCascadeStore<G, L>();
   return ctx.state;
 }
 
-export function useCascadeGroupNodes<T extends GroupNode, L extends LeafNode>() {
-  const { groupNodes } = useDataCascadeState<T, L>();
+export function useCascadeGroupNodes<G extends GroupNode, L extends LeafNode>() {
+  const { groupNodes } = useDataCascadeState<G, L>();
   return groupNodes;
 }
 
-export function useCascadeLeafNode<T extends GroupNode, L extends LeafNode>(cacheKey: string) {
-  const { leafNodes } = useDataCascadeState<T, L>();
+export function useCascadeLeafNode<G extends GroupNode, L extends LeafNode>(cacheKey: string) {
+  const { leafNodes } = useDataCascadeState<G, L>();
   return leafNodes.get(cacheKey) ?? [];
 }
