0b12de988372242b24be48a35e867af4f6643129
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2025-07-03 15:58:09 +0200

    [UiSettings] Add technical preview badge configuration for advance settings (#225620)
    ## 📓 Summary
    
    From the o11y team, we wanted to have a technical preview badge
    available next to our new advanced settings.
    
    Historically, new settings in technical preview have been created added
    some custom text in the description, which is not much highlighted.
    
    To align with the design definition and make this available to every
    solution, a new `technicalPreview` option is available when configuring
    a setting now:
    
    ```ts
    type TechnicalPreviewSettings =
      | boolean
      | {
          /** Technical Preview custom message */
          message?: string;
          /** Key to documentation links */
          docLinksKey?: string;
        };
    
    interface UISettingsParams {
    // ...
    /** A flag indicating that this setting is a technical preview. If true, the setting will display a tech preview badge after the title. */
      technicalPreview?: TechnicalPreviewSettings;
    }
    ```
    
    This has been applied to most of the detected settings in technical
    preview.
    
    <img width="3005" alt="Screenshot 2025-06-27 at 13 12 00"
    src="https://github.com/user-attachments/assets/5e8a4770-3926-4017-acb7-30cd05db6eab"
    />

--- x-pack/solutions/security/plugins/security_solution/server/ui_settings.ts
+++ x-pack/solutions/security/plugins/security_solution/server/ui_settings.ts
@@ -216,8 +216,7 @@ export const initUiSettings = (
       description: i18n.translate(
         'xpack.securitySolution.uiSettings.enableGraphVisualizationDescription',
         {
-          defaultMessage: `<em>[technical preview]</em> Enable the Graph Visualization feature within the Security Solution.`,
-          values: { em: (chunks) => `<em>${chunks}</em>` },
+          defaultMessage: `Enable the Graph Visualization feature within the Security Solution.`,
         }
       ),
       type: 'boolean',
@@ -226,6 +225,7 @@ export const initUiSettings = (
       requiresPageReload: true,
       schema: schema.boolean(),
       solution: 'security',
+      technicalPreview: true,
     },
     [ENABLE_ASSET_INVENTORY_SETTING]: {
       name: i18n.translate('xpack.securitySolution.uiSettings.enableAssetInventoryLabel', {
@@ -234,8 +234,7 @@ export const initUiSettings = (
       description: i18n.translate(
         'xpack.securitySolution.uiSettings.enableAssetInventoryDescription',
         {
-          defaultMessage: `<em>[technical preview]</em> Enable the Asset Inventory experience within the Security Solution. When enabled, you can access the new Inventory feature through the Security Solution navigation. Note: Disabling this setting will not disable the Entity Store or clear persistent Entity metadata. To manage or disable the Entity Store, please visit the Entity Store Management page.`,
-          values: { em: (chunks) => `<em>${chunks}</em>` },
+          defaultMessage: `Enable the Asset Inventory experience within the Security Solution. When enabled, you can access the new Inventory feature through the Security Solution navigation. Note: Disabling this setting will not disable the Entity Store or clear persistent Entity metadata. To manage or disable the Entity Store, please visit the Entity Store Management page.`,
         }
       ),
       type: 'boolean',
@@ -243,6 +242,7 @@ export const initUiSettings = (
       category: [APP_ID],
       requiresPageReload: true,
       schema: schema.boolean(),
+      technicalPreview: true,
     },
     [ENABLE_CLOUD_CONNECTOR_SETTING]: {
       name: i18n.translate('xpack.securitySolution.uiSettings.enableAssetInventoryLabel', {
@@ -251,8 +251,7 @@ export const initUiSettings = (
       description: i18n.translate(
         'xpack.securitySolution.uiSettings.enableAssetInventoryDescription',
         {
-          defaultMessage: `<em>[technical preview]</em> Enable the Cloud Connector experience within the Security Solution. When enabled, you can access the new Cloud Connector feature through the setting up an Agentless CSPM or Asset Inventory Integration.`,
-          values: { em: (chunks) => `<em>${chunks}</em>` },
+          defaultMessage: `Enable the Cloud Connector experience within the Security Solution. When enabled, you can access the new Cloud Connector feature through the setting up an Agentless CSPM or Asset Inventory Integration.`,
         }
       ),
       type: 'boolean',
@@ -260,6 +259,7 @@ export const initUiSettings = (
       category: [APP_ID],
       requiresPageReload: true,
       schema: schema.boolean(),
+      technicalPreview: true,
     },
     [DEFAULT_RULES_TABLE_REFRESH_SETTING]: {
       name: i18n.translate('xpack.securitySolution.uiSettings.rulesTableRefresh', {
