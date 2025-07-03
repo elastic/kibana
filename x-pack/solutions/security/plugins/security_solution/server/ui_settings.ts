26f4cf296a05e3047d7e44a4d2764a43e38d16d9
Mark Hopkin <mark.hopkin@elastic.co>
2025-07-03 15:39:40 +0100

    put advanced setting behind feature flag

--- x-pack/solutions/security/plugins/security_solution/server/ui_settings.ts
+++ x-pack/solutions/security/plugins/security_solution/server/ui_settings.ts
@@ -341,28 +341,6 @@ export const initUiSettings = (
       schema: schema.boolean(),
       solution: 'security',
     },
-    [ENABLE_PRIVILEGED_USER_MONITORING_SETTING]: {
-      name: i18n.translate(
-        'xpack.securitySolution.uiSettings.enablePrivilegedUserMonitoringLabel',
-        {
-          defaultMessage: 'Privileged user monitoring',
-        }
-      ),
-      value: true,
-      description: i18n.translate(
-        'xpack.securitySolution.uiSettings.enablePrivilegedUserMonitoringDescription',
-        {
-          defaultMessage:
-            '<p>Enables the privileged user monitoring dashboard and onboarding experience which are in technical preview.</p>',
-          values: { p: (chunks) => `<p>${chunks}</p>` },
-        }
-      ),
-      type: 'boolean',
-      category: [APP_ID],
-      requiresPageReload: true,
-      schema: schema.boolean(),
-      solution: 'security',
-    },
     [SHOW_RELATED_INTEGRATIONS_SETTING]: {
       name: i18n.translate('xpack.securitySolution.uiSettings.showRelatedIntegrationsLabel', {
         defaultMessage: 'Related integrations',
@@ -422,6 +400,32 @@ export const initUiSettings = (
       requiresPageReload: false,
       solution: 'security',
     },
+    ...(experimentalFeatures.privilegedUserMonitoringDisabled
+      ? {}
+      : {
+          [ENABLE_PRIVILEGED_USER_MONITORING_SETTING]: {
+            name: i18n.translate(
+              'xpack.securitySolution.uiSettings.enablePrivilegedUserMonitoringLabel',
+              {
+                defaultMessage: 'Privileged user monitoring',
+              }
+            ),
+            value: true,
+            description: i18n.translate(
+              'xpack.securitySolution.uiSettings.enablePrivilegedUserMonitoringDescription',
+              {
+                defaultMessage:
+                  '<p>Enables the privileged user monitoring dashboard and onboarding experience which are in technical preview.</p>',
+                values: { p: (chunks) => `<p>${chunks}</p>` },
+              }
+            ),
+            type: 'boolean',
+            category: [APP_ID],
+            requiresPageReload: true,
+            schema: schema.boolean(),
+            solution: 'security',
+          },
+        }),
     ...(experimentalFeatures.extendedRuleExecutionLoggingEnabled
       ? {
           [EXTENDED_RULE_EXECUTION_LOGGING_ENABLED_SETTING]: {
