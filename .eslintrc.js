642985bd55580332bf6f5a4c014ba3600edbb955
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-03 14:53:36 +0300

    [ska][x-pack] relocate spaces_api_integration tests (#225377)
    ## Summary
    
    Part of https://github.com/elastic/kibana-team/issues/1503
    
    This PR is mostly about moving tests from x-pack/test/
    
    Before:
    
    ```
    x-pack/test/
         | - spaces_api_integration/
    ```
    
    After:
    ```
    x-pack/platform/test/
         | - spaces_api_integration/
    ```
    
    deployment-agnostic base configs were relocated together with serverless
    base config in order to resolve circular dependency. We will remove
    duplicates as soon as we finish relocation
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 8c144762dbdd8c64034a46fb012f8e56f233efa7)
    
    # Conflicts:
    #	.github/CODEOWNERS

--- .eslintrc.js
+++ .eslintrc.js
@@ -2113,7 +2113,7 @@ module.exports = {
         'x-pack/test/security_functional/**/*.{js,mjs,ts,tsx}',
 
         'x-pack/platform/plugins/shared/spaces/**/*.{js,mjs,ts,tsx}',
-        'x-pack/test/spaces_api_integration/**/*.{js,mjs,ts,tsx}',
+        'x-pack/platform/test/spaces_api_integration/**/*.{js,mjs,ts,tsx}',
       ],
       rules: {
         '@typescript-eslint/consistent-type-imports': 1,
