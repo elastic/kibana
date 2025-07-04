57e967d1e558695084fbb10a59415bf459b31175
klacabane <kevin.lacabane@elastic.co>
2025-07-04 14:12:25 +0200

    eslint disable

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_lifecycle/ilm_summary.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_lifecycle/ilm_summary.tsx
@@ -59,6 +59,7 @@ export function IlmSummary({
         signal,
       });
     },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     [streamsRepositoryClient, definition, stats]
   );
 
