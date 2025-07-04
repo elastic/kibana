816aa7e68a5f0eb6b3613a8adb760fb1155eb5f6
Saikat Sarkar <saikat.sarkar@elastic.co>
2025-07-03 22:00:38 -0600

    Show the session api key instead of create_api button

--- x-pack/solutions/search/plugins/search_homepage/public/components/api_key_form.tsx
+++ x-pack/solutions/search/plugins/search_homepage/public/components/api_key_form.tsx
@@ -6,8 +6,16 @@
  */
 
 import React, { useState } from 'react';
-import { EuiBadge, EuiButton, EuiButtonEmpty, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
+import {
+  EuiBadge,
+  EuiButton,
+  EuiButtonEmpty,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiButtonIcon,
+} from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
+import { FormInfoField } from '@kbn/search-shared-ui';
 import { i18n } from '@kbn/i18n';
 import { ApiKeyFlyoutWrapper, useSearchApiKey, Status } from '@kbn/search-api-keys-components';
 import { useGetApiKeys } from '../hooks/api/use_api_key';
@@ -17,13 +25,37 @@ interface ApiKeyFormProps {
   hasTitle?: boolean;
 }
 
+const API_KEY_MASK = '•'.repeat(60);
+
 export const ApiKeyForm: React.FC<ApiKeyFormProps> = () => {
   const { share } = useKibana().services;
-  const { status, updateApiKey } = useSearchApiKey();
+  const { apiKey, status, updateApiKey, toggleApiKeyVisibility } = useSearchApiKey();
   const { data } = useGetApiKeys();
   const [showFlyout, setShowFlyout] = useState(false);
   const locator = share?.url?.locators.get('MANAGEMENT_APP_LOCATOR');
   const manageKeysLink = locator?.useUrl({ sectionId: 'security', appId: 'api_keys' });
+
+  if (apiKey) {
+    return (
+      <FormInfoField
+        value={status === Status.showPreviewKey ? apiKey : API_KEY_MASK}
+        copyValue={apiKey}
+        dataTestSubj="apiKeyFormAPIKey"
+        copyValueDataTestSubj="APIKeyButtonCopy"
+        actions={[
+          <EuiButtonIcon
+            iconType={status === Status.showPreviewKey ? 'eyeClosed' : 'eye'}
+            color="text"
+            onClick={toggleApiKeyVisibility}
+            data-test-subj="showAPIKeyButton"
+            aria-label={i18n.translate('searchApiKeysComponents.apiKeyForm.showApiKey', {
+              defaultMessage: 'Show API Key',
+            })}
+          />,
+        ]}
+      />
+    );
+  }
 
   if (status === Status.showUserPrivilegesError) {
     return (
