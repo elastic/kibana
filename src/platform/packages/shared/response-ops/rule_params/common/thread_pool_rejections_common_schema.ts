61d8ace84c0830499a68c14bbcb89cefe7e85679
Georgiana-Andreea Onoleata <georgiana.onoleata@elastic.co>
2025-07-04 11:45:11 +0300

    created schemas for thread_pool_search_rejections, thread_pool_write_rejections rule types

--- src/platform/packages/shared/response-ops/rule_params/common/thread_pool_rejections_common_schema.ts
+++ src/platform/packages/shared/response-ops/rule_params/common/thread_pool_rejections_common_schema.ts
@@ -7,9 +7,14 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export * from './search_configuration_schema';
-export * from './stack_monitoring_common_schema';
-export * from './thread_pool_rejections_common_schema';
-export { dataViewSpecSchema, type DataViewSpec } from './data_view_spec_schema';
-export { MAX_GROUPS } from './constants';
-export { ComparatorFns } from './utils';
+import type { TypeOf } from '@kbn/config-schema';
+import { schema } from '@kbn/config-schema';
+
+export const threadPoolRejectionsCommonSchema = schema.object({
+  threshold: schema.number(),
+  duration: schema.string(),
+  filterQuery: schema.maybe(schema.string()),
+  filterQueryText: schema.maybe(schema.string()),
+});
+
+export type ThreadPoolRejectionsCommonParams = TypeOf<typeof threadPoolRejectionsCommonSchema>;
