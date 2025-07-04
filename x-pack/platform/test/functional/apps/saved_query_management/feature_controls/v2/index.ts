5208e49eefb2999d334c84f16ffad6084412652c
fake-haris <charis.kalpakis@elastic.co>
2025-07-04 13:34:14 +0300

    updated v2 index file

--- x-pack/platform/test/functional/apps/saved_query_management/feature_controls/v2/index.ts
+++ x-pack/platform/test/functional/apps/saved_query_management/feature_controls/v2/index.ts
@@ -9,6 +9,6 @@ import { FtrProviderContext } from '../../../../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('Saved query management', function () {
-    loadTestFile(require.resolve('./feature_controls'));
+    loadTestFile(require.resolve('./security.v2'));
   });
 }
