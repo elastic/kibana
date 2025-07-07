27f411c15480d63adea6daa98de6adab9ea52699
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-07-07 11:49:02 +0200

    make changes to accomodate async share integration

--- src/platform/plugins/shared/share/public/types.ts
+++ src/platform/plugins/shared/share/public/types.ts
@@ -255,6 +255,7 @@ export interface SharingData {
 }
 
 export type ShareIntegrationMapKey = `integration-${string}`;
+
 export interface ShareRegistryInternalApi {
   registerShareIntegration<I extends ShareIntegration>(
     shareObject: string,
