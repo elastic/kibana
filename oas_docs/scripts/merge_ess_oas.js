13a73aa16a51ee77e82a81669d37d4f932607347
Nick Peihl <nick.peihl@elastic.co>
2025-07-07 14:33:20 -0400

    Add openapi docs for internal dashboards apis

--- oas_docs/scripts/merge_ess_oas.js
+++ oas_docs/scripts/merge_ess_oas.js
@@ -17,6 +17,7 @@ const { REPO_ROOT } = require('@kbn/repo-info');
       `${REPO_ROOT}/oas_docs/bundle.json`,
       `${REPO_ROOT}/x-pack/platform/plugins/shared/alerting/docs/openapi/bundled.yaml`,
       `${REPO_ROOT}/x-pack/platform/plugins/shared/cases/docs/openapi/bundled.yaml`,
+      `${REPO_ROOT}/src/platform/plugins/shared/dashboard/docs/openapi/bundled.yaml`,
       `${REPO_ROOT}/src/platform/plugins/shared/data_views/docs/openapi/bundled.yaml`,
       `${REPO_ROOT}/x-pack/platform/plugins/shared/features/docs/openapi/feature_apis.yaml`,
       `${REPO_ROOT}/x-pack/platform/plugins/shared/ml/common/openapi/ml_apis.yaml`,
