733a57db2ca12aac9ca1699a72d3d1b90e464c23
iblancof <irene.blanco@elastic.co>
2025-07-07 11:43:17 +0200

    Export getSpanIcon

--- src/platform/packages/shared/kbn-apm-ui-shared/src/components/index.ts
+++ src/platform/packages/shared/kbn-apm-ui-shared/src/components/index.ts
@@ -12,3 +12,5 @@ export * from './duration_distribution_chart';
 export * from './http_status_code';
 export * from './span_icon';
 export * from './timestamp';
+
+export { getSpanIcon } from './span_icon/get_span_icon';
