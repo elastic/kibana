5238bd33813596d9d926b38771834623ca87deae
Tiago Costa <tiago.costa@elastic.co>
2025-07-07 21:53:24 +0100

    chore(NA): extend ignores to decrease the amount of watching files by parcel/watcher

--- packages/kbn-cli-dev-mode/src/watcher.ts
+++ packages/kbn-cli-dev-mode/src/watcher.ts
@@ -98,10 +98,13 @@ export class Watcher {
         // some basic high-level ignore statements. Additional filtering is done above
         // before paths are passed to `fire()`, using the RepoSourceClassifier mostly
         ignore: [
-          '**/{node_modules,target,public,coverage,__*__}/**',
+          '**/{node_modules,target,public,coverage,__*__,build,.es,.yarn-local-mirror,.git,.github,.buildkite,.vscode,.idea}/**',
+          '**/{bazel-bin,bazel-kibana,bazel-out,bazel-testlogs}/**',
+          '**/{.cache,.temp,.tmp,temp,tmp}/**',
           '**/*.{test,spec,story,stories}.*',
-          '**/*.{http,md,sh,txt}',
-          '**/debug.log',
+          '**/*.{http,md,sh,txt,log,pid,swp,swo}',
+          '**/*~',
+          '**/.DS_Store',
         ],
       }
     ).then(
