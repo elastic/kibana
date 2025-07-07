c26dc9d4c2ebbcf98132500a17c3dfb8c951ae41
Alejandro Fernández Haro <alejandro.haro@elastic.co>
2025-07-08 01:33:53 +0200

    Add comments for future work to undo these changes

--- src/platform/packages/private/kbn-import-resolver/src/import_resolver.ts
+++ src/platform/packages/private/kbn-import-resolver/src/import_resolver.ts
@@ -147,6 +147,8 @@ export class ImportResolver {
       return Path.resolve(REPO_ROOT, `node_modules/@modelcontextprotocol/sdk/dist/esm/${relPath}`);
     }
 
+    // We need this "hack" because our current import-resolver doesn't support "exports" in package.json.
+    // We should be able to remove this once we support cjs/esm interop.
     if (req.startsWith('@elastic/opentelemetry-node/sdk')) {
       return Path.resolve(REPO_ROOT, `node_modules/@elastic/opentelemetry-node/lib/sdk.js`);
     }
