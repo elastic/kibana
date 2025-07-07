52d10f0be04f9b9de279878eba814587b186ed1c
Maxim Palenov <maxim.palenov@elastic.co>
2025-07-07 11:26:55 +0200

    enable prebuilt rules package integration tests

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/common/configs/edge_cases/ess_air_gapped.config.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/common/configs/edge_cases/ess_air_gapped.config.ts
@@ -8,20 +8,21 @@
 import { FtrConfigProviderContext } from '@kbn/test';
 import path from 'path';
 
-export const BUNDLED_PACKAGE_DIR = path.join(
+const SECURITY_DETECTION_ENGINE_PACKAGES_PATH = path.join(
   path.dirname(__filename),
-  '../fixtures/packages/large'
+  '../../fixtures/packages'
 );
 
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const functionalConfig = await readConfigFile(
-    require.resolve('../../../../../../config/ess/config.base.basic')
+    require.resolve('../../../../../../../config/ess/config.base.basic')
   );
 
   return {
     ...functionalConfig.getAll(),
     testFiles: [
-      require.resolve('../prebuilt_rules_package/air_gapped/install_large_bundled_package'),
+      require.resolve('../../import_export/import_with_installing_package'),
+      require.resolve('../../prebuilt_rules_package/air_gapped'),
     ],
     kbnTestServer: {
       ...functionalConfig.get('kbnTestServer'),
@@ -30,12 +31,14 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
         /*  Tests in this directory simulate an air-gapped environment in which the instance doesn't have access to EPR.
          *  To do that, we point the Fleet url to an invalid URL, and instruct Fleet to fetch bundled packages at the
          *  location defined in BUNDLED_PACKAGE_DIR.
-         *  Since we want to test the installation of a large package, we created a specific package `security_detection_engine-100.0.0`
-         *  which contains 15000 rules assets and 750 unique rules, and attempt to install it.
          */
         `--xpack.fleet.isAirGapped=true`,
-        `--xpack.fleet.developer.bundledPackageLocation=${BUNDLED_PACKAGE_DIR}`,
+        `--xpack.fleet.developer.bundledPackageLocation=${SECURITY_DETECTION_ENGINE_PACKAGES_PATH}`,
       ],
+    },
+    junit: {
+      reportName:
+        'Rules Management - Prebuilt Rules (Common) Integration Tests - ESS Basic License (Air Gapped)',
     },
   };
 }
--- x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/common/prebuilt_rules_package/index.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/common/prebuilt_rules_package/index.ts
@@ -10,7 +10,6 @@ import { FtrProviderContext } from '../../../../../../ftr_provider_context';
 export default ({ loadTestFile }: FtrProviderContext): void => {
   describe('Prebuilt rules package', function () {
     loadTestFile(require.resolve('./bootstrap_prebuilt_rules'));
-    loadTestFile(require.resolve('./install_package_from_epr'));
     loadTestFile(require.resolve('./update_package'));
   });
 };
