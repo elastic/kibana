55800e0a50fe431830b3c6aacd9fdf5e52f0baa6
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-07 17:46:33 +0300

    [ska] relocate upgrade, usage_collection, search_sessions_integration and licensing tests (#226669)
    ## Summary
    
    Part of https://github.com/elastic/kibana-team/issues/1503
    
    This PR is mostly about moving platform tests from x-pack/test
    
    ```
    x-pack/test/
         | - licensing_plugin/
         | - search_sessions_integration/
         | - upgrade/
         | - usage_collection/
    ```
    
    After:
    ```
    x-pack/platform/test/
         | - licensing_plugin/
         | - search_sessions_integration/
         | - upgrade/
         | - usage_collection/
    ```
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 2961e8c16ce0c609a08d1aadda834430e11b249f)

--- x-pack/platform/test/usage_collection/config.ts
+++ x-pack/platform/test/usage_collection/config.ts
@@ -15,7 +15,7 @@ import { pageObjects } from './page_objects';
 
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const xpackFunctionalConfig = await readConfigFile(
-    require.resolve('../functional/config.base.js')
+    require.resolve('../functional/config.base.ts')
   );
 
   return {
