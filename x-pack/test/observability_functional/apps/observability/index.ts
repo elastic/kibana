bb6c3575c52693e897501140787e890475728194
Justin Kambic <jk@elastic.co>
2025-07-08 14:09:17 -0400

    Introduce FTR test for pasting images and creating a comment.

--- x-pack/test/observability_functional/apps/observability/index.ts
+++ x-pack/test/observability_functional/apps/observability/index.ts
@@ -20,6 +20,7 @@ export default function ({ loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./pages/alerts/custom_threshold_preview_chart'));
     loadTestFile(require.resolve('./pages/alerts/custom_threshold'));
     loadTestFile(require.resolve('./pages/cases/case_details'));
+    loadTestFile(require.resolve('./pages/cases/case_comment'));
     loadTestFile(require.resolve('./pages/overview/alert_table'));
     loadTestFile(require.resolve('./exploratory_view'));
     loadTestFile(require.resolve('./feature_controls'));
