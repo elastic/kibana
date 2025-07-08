d4d6f15482aeff65a8f11138b94a58ab4b2a7583
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-08 14:54:40 +0300

    [scout] re-org test directory (#226411)
    ## Summary
    
    Since Scout will soon support `api` testing, we need to re-org scout
    test directory so that both ui and api playwright configs can be
    discovered and executed independently. Proposed structure:
    
    ```
    - plugin
     |- test
       |- scout
         |- common // fixtures/constants to share across both 'ui' and 'api' tests
         |- ui
            |-tests
            |-parallel_tests
            |-playwright.config.ts
            |-playwright.parallel.config.ts
         |- api
            |-tests
            |-parallel_tests
            |-playwright.config.ts
            |-playwright.parallel.config.ts
    ```
    
    This PR updates all the existing scout directories
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1596,9 +1596,9 @@ module.exports = {
         'src/platform/packages/shared/kbn-scout/src/playwright/**/*.ts',
         'x-pack/solutions/observability/packages/kbn-scout-oblt/src/playwright/**/*.ts',
         'x-pack/solutions/security/packages/kbn-scout-security/src/playwright/**/*.ts',
-        'src/platform/plugins/**/ui_tests/**/*.ts',
-        'x-pack/platform/plugins/**/ui_tests/**/*.ts',
-        'x-pack/solutions/**/plugins/**/ui_tests/**/*.ts',
+        'src/platform/plugins/**/test/scout/**/*.ts',
+        'x-pack/platform/plugins/**/test/scout/**/*.ts',
+        'x-pack/solutions/**/plugins/**/test/scout/**/*.ts',
       ],
       excludedFiles: ['src/platform/packages/shared/kbn-scout/src/playwright/**/*.test.ts'],
       extends: ['plugin:playwright/recommended'],
@@ -2319,8 +2319,8 @@ module.exports = {
     },
     {
       files: [
-        'src/platform/plugins/**/ui_tests/**/*.ts',
-        'x-pack/platform/**/plugins/**/ui_tests/**/*.ts',
+        'src/platform/plugins/**/test/scout/**/*.ts',
+        'x-pack/platform/**/plugins/**/test/scout/**/*.ts',
       ],
       rules: {
         'no-restricted-imports': [
@@ -2341,7 +2341,7 @@ module.exports = {
       },
     },
     {
-      files: ['x-pack/solutions/observability/plugins/**/ui_tests/**/*.ts'],
+      files: ['x-pack/solutions/observability/plugins/**/test/scout/**/*.ts'],
       rules: {
         'no-restricted-imports': [
           'error',
@@ -2368,7 +2368,7 @@ module.exports = {
       },
     },
     {
-      files: ['x-pack/solutions/security/plugins/**/ui_tests/**/*.ts'],
+      files: ['x-pack/solutions/security/plugins/**/test/scout/**/*.ts'],
       rules: {
         'no-restricted-imports': [
           'error',
