4f793b61d35d3f43c264818356c69e88f3ad461c
Sid <siddharthmantri1@gmail.com>
2025-07-08 16:47:39 +0200

    remove type field

--- src/core/packages/saved-objects/migration-server-internal/src/core/build_active_mappings.ts
+++ src/core/packages/saved-objects/migration-server-internal/src/core/build_active_mappings.ts
@@ -96,7 +96,6 @@ export function getBaseMappings(): IndexMapping {
         type: 'boolean',
       },
       accessControl: {
-        type: 'object',
         dynamic: false,
         properties: {
           owner: {
