33f6d600b132fc9d04a291c02b0d0efbc347725f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-09 16:38:05 +0300

    [ska] relocate platform tests & services (#226857)
    ## Summary
    
    ## Summary
    
    Part of https://github.com/elastic/kibana-team/issues/1503
    
    This PR is mostly about moving platform tests from x-pack/test
    
    ```
    x-pack/test/
         | - functional/aiops/
         | - functional_with_es_ssl/
         | - screenshot_creation/
    ```
    
    After: 
    ```
    x-pack/platform/test/
         | - functional/aiops/
         | - functional_with_es_ssl/
         | - screenshot_creation/
    ```
    
    Some FTR services were relocated to the new path as well (`ml`,
    `transform`, etc.) Few small services were duplicated as we can't
    relocate it without circular dependency due to cross imports.
    
    
    
    <!--ONMERGE {"backportTargets":["8.19","9.1"]} ONMERGE-->
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Cesare de Cal <cesare.decal@elastic.co>

--- x-pack/platform/test/functional/page_objects/index.ts
+++ x-pack/platform/test/functional/page_objects/index.ts
@@ -23,6 +23,7 @@ import { UserProfilePageProvider } from './user_profile_page';
 import { SnapshotRestorePageProvider } from './snapshot_restore_page';
 import { SearchSessionsPageProvider } from './search_sessions_management_page';
 import { GraphPageObject } from './graph_page';
+import { MaintenanceWindowsPageProvider } from './maintenance_windows_page';
 
 // just like services, PageObjects are defined as a map of
 // names to Providers. Merge in Kibana's or pick specific ones
@@ -45,4 +46,5 @@ export const pageObjects = {
   snapshotRestore: SnapshotRestorePageProvider,
   upgradeAssistant: UpgradeAssistantFlyoutObject,
   userProfiles: UserProfilePageProvider,
+  maintenanceWindows: MaintenanceWindowsPageProvider,
 };
--- x-pack/test/screenshot_creation/apps/index.ts
+++ x-pack/test/screenshot_creation/apps/index.ts
@@ -9,8 +9,6 @@ import { FtrProviderContext } from '../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
   describe('apps', function () {
-    loadTestFile(require.resolve('./ml_docs'));
     loadTestFile(require.resolve('./response_ops_docs'));
-    loadTestFile(require.resolve('./transform_docs'));
   });
 }
