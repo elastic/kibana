9a51e675c8fd8c47ac8aa164240bbb4501d7aaa1
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-09 13:09:20 +0200

    leave some oblt helpers as copy in x-pack/test

---
+++ x-pack/test/api_integration/deployment_agnostic/apis/observability/apm/alerts/helpers/alerting_helper.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { ParsedTechnicalFields } from '@kbn/rule-registry-plugin/common';
+import { ObservabilityApmAlert } from '@kbn/alerts-as-data-utils';
+
+export const APM_ALERTS_INDEX = '.alerts-observability.apm.alerts-*';
+export const APM_ACTION_VARIABLE_INDEX = 'apm-index-connector-test';
+
+function getTimerange() {
+  return {
+    start: new Date(Date.now() - 24 * 60 * 60 * 1000).toISOString(),
+    end: new Date(Date.now() + 5 * 60 * 1000).toISOString(),
+  };
+}
+
+export type ApmAlertFields = ParsedTechnicalFields & ObservabilityApmAlert;
