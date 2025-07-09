bc3027f5f9d11562ac19f4129f1e03c058b659d9
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2025-07-09 16:49:13 +0200

    Add logic to enable search bar in APM Logs and sync it with the state

--- x-pack/solutions/observability/plugins/apm/public/components/routing/mobile_service_detail/index.tsx
+++ x-pack/solutions/observability/plugins/apm/public/components/routing/mobile_service_detail/index.tsx
@@ -263,7 +263,7 @@ export const mobileServiceDetailRoute = {
         element: <ServiceLogs />,
         searchBarOptions: {
           showMobileFilters: false,
-          showQueryInput: false,
+          showQueryInput: true,
         },
       }),
       '/mobile-services/{serviceName}/alerts': {
