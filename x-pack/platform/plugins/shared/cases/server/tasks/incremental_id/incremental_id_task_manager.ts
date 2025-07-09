b9c88adc4674d0dc5e27759027e1e040e3225f9a
Jan Monschke <jan.monschke@elastic.co>
2025-07-09 14:43:29 +0200

    [Cases] Incremental id telemetry and config improvements (#226935)
    ## Summary
    
    Adds success/error counting to the incremental ID service and fixes the
    missing usage of `delayMinutes`.

--- x-pack/platform/plugins/shared/cases/server/tasks/incremental_id/incremental_id_task_manager.ts
+++ x-pack/platform/plugins/shared/cases/server/tasks/incremental_id/incremental_id_task_manager.ts
@@ -10,6 +10,8 @@ import {
   type TaskManagerSetupContract,
   type TaskManagerStartContract,
 } from '@kbn/task-manager-plugin/server';
+import type { UsageCollectionSetup } from '@kbn/usage-collection-plugin/server';
+import type { IUsageCounter } from '@kbn/usage-collection-plugin/server/usage_counters/usage_counter';
 import { CASE_SAVED_OBJECT, CASE_ID_INCREMENTER_SAVED_OBJECT } from '../../../common/constants';
 import { CasesIncrementalIdService } from '../../services/incremental_id';
 import type { ConfigType } from '../../config';
@@ -24,15 +26,20 @@ export class IncrementalIdTaskManager {
   private logger: Logger;
   private internalSavedObjectsClient?: SavedObjectsClient;
   private taskManager?: TaskManagerStartContract;
-
+  private successErrorUsageCounter?: IUsageCounter;
   constructor(
     taskManager: TaskManagerSetupContract,
     config: ConfigType['incrementalId'],
-    logger: Logger
+    logger: Logger,
+    usageCollection?: UsageCollectionSetup
   ) {
     this.config = config;
     this.logger = logger.get('incremental_id_task');
     this.logger.info('Registering Case Incremental ID Task Manager');
+
+    if (usageCollection) {
+      this.successErrorUsageCounter = usageCollection?.createUsageCounter('CasesIncrementalId');
+    }
 
     taskManager.registerTaskDefinitions({
       [CASES_INCREMENTAL_ID_SYNC_TASK_TYPE]: {
@@ -64,20 +71,32 @@ export class IncrementalIdTaskManager {
               this.logger.debug(
                 `${casesWithoutIncrementalId.length} cases without incremental ids`
               );
-              // Increment the case ids
-              const processedAmount = await casesIncrementService.incrementCaseIds(
-                casesWithoutIncrementalId
-              );
-              this.logger.debug(
-                `Applied incremental ids to ${processedAmount} out of ${casesWithoutIncrementalId.length} cases`
-              );
 
-              const endTime = performance.now();
-              this.logger.debug(
-                `Task terminated ${CASES_INCREMENTAL_ID_SYNC_TASK_ID}. Task run took ${
-                  endTime - startTime
-                }ms [ started: ${initializedTime}, ended: ${new Date().toISOString()} ]`
-              );
+              try {
+                // Increment the case ids
+                const processedAmount = await casesIncrementService.incrementCaseIds(
+                  casesWithoutIncrementalId
+                );
+                this.logger.debug(
+                  `Applied incremental ids to ${processedAmount} out of ${casesWithoutIncrementalId.length} cases`
+                );
+
+                const endTime = performance.now();
+                this.logger.debug(
+                  `Task terminated ${CASES_INCREMENTAL_ID_SYNC_TASK_ID}. Task run took ${
+                    endTime - startTime
+                  }ms [ started: ${initializedTime}, ended: ${new Date().toISOString()} ]`
+                );
+                this.successErrorUsageCounter?.incrementCounter({
+                  counterName: 'incrementIdTaskSuccess',
+                  incrementBy: 1,
+                });
+              } catch (_) {
+                this.successErrorUsageCounter?.incrementCounter({
+                  counterName: 'incrementIdTaskError',
+                  incrementBy: 1,
+                });
+              }
             },
             cancel: async () => {
               casesIncrementService.stopService();
@@ -125,7 +144,7 @@ export class IncrementalIdTaskManager {
         id: CASES_INCREMENTAL_ID_SYNC_TASK_ID,
         taskType: CASES_INCREMENTAL_ID_SYNC_TASK_TYPE,
         // start delayed to give the system some time to start up properly
-        runAt: new Date(new Date().getTime() + this.config.taskIntervalMinutes * 60 * 1000),
+        runAt: new Date(new Date().getTime() + this.config.taskStartDelayMinutes * 60 * 1000),
         schedule: {
           interval: `${this.config.taskIntervalMinutes}m`,
         },
