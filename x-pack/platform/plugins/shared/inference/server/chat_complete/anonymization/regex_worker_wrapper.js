330d36a05b7df4c35cf66ffb4b2f1485bce78cfd
Sandra Gonzales <sandra.gonzales@elastic.co>
2025-07-09 09:29:01 -0400

    fix setup_node_env path

--- x-pack/platform/plugins/shared/inference/server/chat_complete/anonymization/regex_worker_wrapper.js
+++ x-pack/platform/plugins/shared/inference/server/chat_complete/anonymization/regex_worker_wrapper.js
@@ -4,9 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-// eslint-disable-next-line @kbn/imports/no_boundary_crossing
-require('../../../../../../../../src/setup_node_env');
+const path = require('path');
+const { REPO_ROOT } = require('@kbn/repo-info');
+// eslint-disable-next-line import/no-dynamic-require
+require(path.resolve(REPO_ROOT, 'src/setup_node_env'));
 const { workerData } = require('worker_threads');
 // eslint-disable-next-line import/no-dynamic-require
 module.exports = require(workerData.fullpath);
