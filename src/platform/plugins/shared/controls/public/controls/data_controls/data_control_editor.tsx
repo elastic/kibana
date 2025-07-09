2e6459d5ad742ec7505b3d0c31b225faa3c7d1e8
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-07-09 11:21:55 -0500

    Move delete control button to flyout footer and add confirm prompt

--- src/platform/plugins/shared/controls/public/controls/data_controls/data_control_editor.tsx
+++ src/platform/plugins/shared/controls/public/controls/data_controls/data_control_editor.tsx
@@ -46,7 +46,7 @@ import {
   type ControlWidth,
   type DefaultDataControlState,
 } from '../../../common';
-import { dataViewsService } from '../../services/kibana_services';
+import { coreServices, dataViewsService } from '../../services/kibana_services';
 import { getAllControlTypes, getControlFactory } from '../../control_factory_registry';
 import type { ControlGroupApi } from '../../control_group/types';
 import { DataControlEditorStrings } from './data_control_constants';
@@ -58,6 +58,7 @@ import {
   type DataControlFieldRegistry,
 } from './types';
 import { ControlFactory } from '../types';
+import { confirmDeleteControl } from '../../common';
 
 export interface ControlEditorProps<
   State extends DefaultDataControlState = DefaultDataControlState
@@ -414,23 +415,6 @@ export const DataControlEditor = <State extends DefaultDataControlState = Defaul
             </EuiFormRow>
           )}
           {!editorConfig?.hideAdditionalSettings && CustomSettingsComponent}
-          {controlId && (
-            <>
-              <EuiSpacer size="l" />
-              <EuiButtonEmpty
-                aria-label={`delete-${editorState.title ?? editorState.fieldName}`}
-                iconType="trash"
-                flush="left"
-                color="danger"
-                onClick={() => {
-                  onCancel(initialState); // don't want to show "lost changes" warning
-                  controlGroupApi.removePanel(controlId!);
-                }}
-              >
-                {DataControlEditorStrings.manageControl.getDeleteButtonTitle()}
-              </EuiButtonEmpty>
-            </>
-          )}
         </EuiForm>
       </EuiFlyoutBody>
       <EuiFlyoutFooter>
@@ -447,25 +431,44 @@ export const DataControlEditor = <State extends DefaultDataControlState = Defaul
             </EuiButtonEmpty>
           </EuiFlexItem>
           <EuiFlexItem grow={false}>
-            <EuiButton
-              aria-label={`save-${editorState.title ?? editorState.fieldName}`}
-              data-test-subj="control-editor-save"
-              fill
-              color="primary"
-              disabled={
-                !(
-                  controlOptionsValid &&
-                  Boolean(editorState.fieldName) &&
-                  Boolean(selectedDataView) &&
-                  Boolean(selectedControlType)
-                )
-              }
-              onClick={() => {
-                onSave(editorState, selectedControlType!);
-              }}
-            >
-              {DataControlEditorStrings.manageControl.getSaveChangesTitle()}
-            </EuiButton>
+            <EuiFlexGroup responsive={false} justifyContent="flexEnd" gutterSize="s">
+              {controlId && (
+                <EuiButton
+                  aria-label={`delete-${editorState.title ?? editorState.fieldName}`}
+                  iconType="trash"
+                  color="danger"
+                  onClick={() => {
+                    confirmDeleteControl().then((confirmed) => {
+                      if (confirmed) {
+                        onCancel(initialState); // don't want to show "lost changes" warning
+                        controlGroupApi.removePanel(controlId!);
+                      }
+                    });
+                  }}
+                >
+                  {DataControlEditorStrings.manageControl.getDeleteButtonTitle()}
+                </EuiButton>
+              )}
+              <EuiButton
+                aria-label={`save-${editorState.title ?? editorState.fieldName}`}
+                data-test-subj="control-editor-save"
+                fill
+                color="primary"
+                disabled={
+                  !(
+                    controlOptionsValid &&
+                    Boolean(editorState.fieldName) &&
+                    Boolean(selectedDataView) &&
+                    Boolean(selectedControlType)
+                  )
+                }
+                onClick={() => {
+                  onSave(editorState, selectedControlType!);
+                }}
+              >
+                {DataControlEditorStrings.manageControl.getSaveChangesTitle()}
+              </EuiButton>
+            </EuiFlexGroup>
           </EuiFlexItem>
         </EuiFlexGroup>
       </EuiFlyoutFooter>
