e40877dd2de661e219907e81c34a69028ef6a898
Alex Szabo <alex.szabo@elastic.co>
2025-07-09 15:27:53 +0200

    ALL elastic-images-prod changed to elastic-images-qa to test the new image

--- .buildkite/pipeline-utils/agent_images.ts
+++ .buildkite/pipeline-utils/agent_images.ts
@@ -11,7 +11,7 @@ import { dump } from 'js-yaml';
 import { BuildkiteClient, BuildkiteAgentTargetingRule } from './buildkite';
 
 const ELASTIC_IMAGES_QA_PROJECT = 'elastic-images-qa';
-const ELASTIC_IMAGES_PROD_PROJECT = 'elastic-images-prod';
+const ELASTIC_IMAGES_PROD_PROJECT = 'elastic-images-qa';
 
 // constrain AgentImageConfig to the type that doesn't have the `queue` property
 const DEFAULT_AGENT_IMAGE_CONFIG: BuildkiteAgentTargetingRule = {
