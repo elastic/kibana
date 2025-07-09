1ed467d7e6220d0d514e029bf4827669d8802d9a
Ignacio Rivas <rivasign@gmail.com>
2025-07-09 14:07:27 +0300

    Fix jest

--- src/platform/packages/shared/kbn-data-view-validation/jest.config.js
+++ src/platform/packages/shared/kbn-data-view-validation/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '.',
-  roots: ['<rootDir>/src'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-data-view-validation'],
 };
