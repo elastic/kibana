9cae3bf269b0fea7521a2839aed6280118d83d1c
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-09 11:55:36 +0200

    cleanup of unused services

--- x-pack/test/api_integration/deployment_agnostic/services/index.ts
+++ x-pack/test/api_integration/deployment_agnostic/services/index.ts
@@ -8,9 +8,7 @@
 import { services as platformDeploymentAgnosticServices } from '@kbn/test-suites-xpack-platform/api_integration_deployment_agnostic/services';
 import { SupertestWithRoleScope } from '@kbn/test-suites-xpack-platform/api_integration_deployment_agnostic/services/role_scoped_supertest';
 import { AlertingApiProvider } from './alerting_api';
-import { SloApiProvider } from './slo_api';
 import { SynthtraceProvider } from './synthtrace';
-import { ApmApiProvider } from './apm_api';
 import { ObservabilityAIAssistantApiProvider } from './observability_ai_assistant_api';
 
 export type {
@@ -23,9 +21,7 @@ export const services = {
   ...platformDeploymentAgnosticServices,
   // create a new deployment-agnostic service and load here
   alertingApi: AlertingApiProvider,
-  sloApi: SloApiProvider,
   synthtrace: SynthtraceProvider,
-  apmApi: ApmApiProvider,
   observabilityAIAssistantApi: ObservabilityAIAssistantApiProvider,
 };
 
