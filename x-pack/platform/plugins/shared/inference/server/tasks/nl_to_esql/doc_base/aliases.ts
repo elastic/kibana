fa9a5b2d04d457f9b87f74529a4fdf4a635b4e5d
Quynh Nguyen <quynh.nguyen@elastic.co>
2025-07-09 09:35:22 -0500

    Update logic for look up join

--- x-pack/platform/plugins/shared/inference/server/tasks/nl_to_esql/doc_base/aliases.ts
+++ x-pack/platform/plugins/shared/inference/server/tasks/nl_to_esql/doc_base/aliases.ts
@@ -12,7 +12,7 @@
 const aliases: Record<string, string[]> = {
   STATS: ['STATS_BY', 'BY', 'STATS...BY', 'STATS ... BY'],
   OPERATORS: ['LIKE', 'RLIKE', 'IN'],
-  'LOOKUP JOIN': ['LOOKUP', 'JOIN', 'LOOKUP JOIN', 'LOOKUP_JOIN'],
+  JOIN: ['LOOKUP JOIN'],
 };
 
 const getAliasMap = () => {
