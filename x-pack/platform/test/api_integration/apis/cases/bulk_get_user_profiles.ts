358e61201d14e9ce6aa0e84ce76b0aad0534ea4c
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-07-10 16:54:59 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/platform/test/api_integration/apis/cases/bulk_get_user_profiles.ts
+++ x-pack/platform/test/api_integration/apis/cases/bulk_get_user_profiles.ts
@@ -8,7 +8,10 @@
 import expect from '@kbn/expect';
 import { APP_ID as CASES_APP_ID } from '@kbn/cases-plugin/common/constants';
 import { deleteAllCaseItems } from '../../../cases_api_integration/common/lib/api';
-import { bulkGetUserProfiles, suggestUserProfiles } from '../../../cases_api_integration/common/lib/api/user_profiles';
+import {
+  bulkGetUserProfiles,
+  suggestUserProfiles,
+} from '../../../cases_api_integration/common/lib/api/user_profiles';
 import type { FtrProviderContext } from '../../ftr_provider_context';
 import {
   casesAllUser,
