d4c7c6b1edcb7a78b391fcec226844447488dcd1
Tiago Costa <tiago.costa@elastic.co>
2025-07-10 20:23:42 +0100

    fix(NA): exclude files during pdf generation

--- packages/kbn-cli-dev-mode/src/watcher.ts
+++ packages/kbn-cli-dev-mode/src/watcher.ts
@@ -75,8 +75,11 @@ export class Watcher {
           //
           // This happens mainly when switching branches, running a package manager, or otherwise changing a lot of
           // files at once. This results of a new handling introduced in parcel v2.5.1
+          //
+          // For now we are force restarting the server but if this breaks anything we can just ignore this error
+          // as this was the behaviour previously in place before the update.
           if (error.message && error.message.includes('Events were dropped by the FSEvents client')) {
-            return fire(`${this.repoRoot}`);
+            return fire(`${this.repoRoot} (cache invalid)`);
           }
 
           // Other runtime errors should still fail
@@ -110,14 +113,14 @@ export class Watcher {
         // some basic high-level ignore statements. Additional filtering is done above
         // before paths are passed to `fire()`, using the RepoSourceClassifier mostly
         ignore: [
-          '**/{node_modules,target,public,coverage,__*__,build,.es,.yarn-local-mirror,.git,.github,.buildkite,.vscode,.idea}/**',
+          '**/{node_modules,target,public,coverage,__*__,build,.chromium,.es,.yarn-local-mirror,.git,.github,.buildkite,.vscode,.idea}/**',
           '**/{bazel-bin,bazel-kibana,bazel-out,bazel-testlogs}/**',
           '**/{.cache,.temp,.tmp,temp,tmp}/**',
           '**/*.{test,spec,story,stories}.*',
           '**/*.{http,md,sh,txt,log,pid,swp,swo}',
           '**/*~',
           '**/.DS_Store',
-          '/data',
+          '/data/**',
         ],
       }
     ).then(
