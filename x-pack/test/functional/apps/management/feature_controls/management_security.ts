8606b9e58c53538370f0a0dc57649670748f42cd
Robert Jaszczurek <robert.jaszczurek@elastic.co>
2025-07-10 16:51:48 +0200

    Update management section tests to not expect aiAssistantManagementSelection under Kibana section

--- x-pack/test/functional/apps/management/feature_controls/management_security.ts
+++ x-pack/test/functional/apps/management/feature_controls/management_security.ts
@@ -85,7 +85,6 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
           'dataViews',
           'filesManagement',
           'objects',
-          'aiAssistantManagementSelection',
           'tags',
           'spaces',
           'settings',
