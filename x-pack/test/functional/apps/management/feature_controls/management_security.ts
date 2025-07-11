b187adb48fb97a3c4569892b8c342d0d4a4067b7
Robert Jaszczurek <robert.jaszczurek@elastic.co>
2025-07-11 10:34:12 +0200

    Update management security test to expect 5 sections instead of 4

--- x-pack/test/functional/apps/management/feature_controls/management_security.ts
+++ x-pack/test/functional/apps/management/feature_controls/management_security.ts
@@ -63,7 +63,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
       it('should only render management entries controllable via Kibana privileges', async () => {
         await PageObjects.common.navigateToApp('management');
         const sections = await managementMenu.getSections();
-        expect(sections).to.have.length(4);
+        expect(sections).to.have.length(5);
 
         // Order of the sections in Stack Management might change in the future
         // so we need to find the sections by their id
