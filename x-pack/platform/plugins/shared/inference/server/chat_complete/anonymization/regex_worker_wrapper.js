b9ba0e097c92e0974be551d8ab48b156a75cafb7
Sandra Gonzales <sandra.gonzales@elastic.co>
2025-07-11 13:16:04 -0400

    try REPO_ROOT

--- x-pack/platform/plugins/shared/inference/server/chat_complete/anonymization/regex_worker_wrapper.js
+++ x-pack/platform/plugins/shared/inference/server/chat_complete/anonymization/regex_worker_wrapper.js
@@ -4,14 +4,16 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+const { REPO_ROOT } = require('@kbn/repo-info');
+const { join } = require('path');
+
 /* eslint-disable @kbn/imports/no_boundary_crossing */
+/* eslint-disable import/no-dynamic-require */
 
 if (process.env.NODE_ENV !== 'production') {
-  require('../../../../../../../../src/setup_node_env');
+  require(join(REPO_ROOT, 'src', 'setup_node_env'));
 } else {
-  // eslint-disable-next-line @kbn/imports/no_unresolvable_imports
-  require('../../../../../../src/setup_node_env/dist');
+  require(join(REPO_ROOT, 'src', 'setup_node_env/dist'));
 }
 
-// eslint-disable-next-line import/no-dynamic-require
 module.exports = require('./regex_worker_task');
