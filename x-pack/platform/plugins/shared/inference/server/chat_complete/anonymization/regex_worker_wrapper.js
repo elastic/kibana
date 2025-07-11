fe3fabbfcfb66c1a2bfba1cf368f984cc6e3e338
Sandra Gonzales <sandra.gonzales@elastic.co>
2025-07-11 09:41:20 -0400

    try same path setup_node_env

--- x-pack/platform/plugins/shared/inference/server/chat_complete/anonymization/regex_worker_wrapper.js
+++ x-pack/platform/plugins/shared/inference/server/chat_complete/anonymization/regex_worker_wrapper.js
@@ -4,14 +4,13 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+/* eslint-disable @kbn/imports/no_boundary_crossing */
 const { workerData } = require('worker_threads');
 
 if (process.env.NODE_ENV !== 'production') {
-  /* eslint-disable @kbn/imports/no_boundary_crossing */
   require('../../../../../../../../src/setup_node_env');
 } else {
-  // eslint-disable-next-line @kbn/imports/no_unresolvable_imports
-  require('../../../../../../src/setup_node_env/dist');
+  require('../../../../../../../../src/setup_node_env/dist');
 }
 
 // eslint-disable-next-line import/no-dynamic-require
