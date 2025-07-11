fc7355684bd666ac6c9e85d8bc575592bf6699b3
Nikita Indik <nikita.indik@elastic.co>
2025-07-11 11:20:51 +0200

    test

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/ml_disabled/review_upgrade/review_upgrade.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/prebuilt_rules/ml_disabled/review_upgrade/review_upgrade.ts
@@ -1,10 +1,10 @@
-/* eslint-disable no-console */
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+/* eslint-disable no-console */
 
 import expect from 'expect';
 import { FtrProviderContext } from '../../../../../../ftr_provider_context';
