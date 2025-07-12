4b3ed70f1ac2fe4d475171452cfe8179667bab8b
Davis McPhee <davis.mcphee@elastic.co>
2025-07-12 00:36:43 -0300

    Fix tabs initialization

--- src/platform/plugins/shared/discover/public/application/main/state_management/redux/types.ts
+++ src/platform/plugins/shared/discover/public/application/main/state_management/redux/types.ts
@@ -74,6 +74,7 @@ export interface DiscoverInternalState {
   initialDocViewerTabId?: string;
   isESQLToDataViewTransitionModalVisible: boolean;
   tabs: {
+    areInitializing: boolean;
     byId: Record<string, TabState | RecentlyClosedTabState>;
     allIds: string[];
     recentlyClosedTabIds: string[];
