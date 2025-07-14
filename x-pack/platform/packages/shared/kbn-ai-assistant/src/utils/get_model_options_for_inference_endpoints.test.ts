1513c2257a87f11117c7ddefee2fb837cab792d9
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-07-14 13:37:33 -0400

    Add new test to cover EIS endpoint

--- x-pack/platform/packages/shared/kbn-ai-assistant/src/utils/get_model_options_for_inference_endpoints.test.ts
+++ x-pack/platform/packages/shared/kbn-ai-assistant/src/utils/get_model_options_for_inference_endpoints.test.ts
@@ -17,6 +17,7 @@ import type { InferenceAPIConfigResponse } from '@kbn/ml-trained-models-utils';
 import {
   ELSER_ON_ML_NODE_INFERENCE_ID,
   E5_SMALL_INFERENCE_ID,
+  ELSER_IN_EIS_INFERENCE_ID,
 } from '@kbn/observability-ai-assistant-plugin/public';
 
 describe('getModelOptionsForInferenceEndpoints', () => {
@@ -46,6 +47,23 @@ describe('getModelOptionsForInferenceEndpoints', () => {
 
   it('does not show any EIS models if EIS pre-configured endpoints are not available', () => {
     const endpoints = [
+      { inference_id: ELSER_ON_ML_NODE_INFERENCE_ID },
+      { inference_id: E5_SMALL_INFERENCE_ID },
+    ] as InferenceAPIConfigResponse[];
+
+    const options = getModelOptionsForInferenceEndpoints({
+      endpoints,
+    });
+
+    expect(options.map((o) => o.key)).toEqual([
+      ELSER_ON_ML_NODE_INFERENCE_ID,
+      E5_SMALL_INFERENCE_ID,
+    ]);
+  });
+
+  it('does not show any EIS models even if EIS pre-configured endpoints are available', () => {
+    const endpoints = [
+      { inference_id: ELSER_IN_EIS_INFERENCE_ID },
       { inference_id: ELSER_ON_ML_NODE_INFERENCE_ID },
       { inference_id: E5_SMALL_INFERENCE_ID },
     ] as InferenceAPIConfigResponse[];
