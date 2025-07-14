8de69e5c51750859a5d541ebdf643263ed92f225
machadoum <pablo.nevesmachado@elastic.co>
2025-07-14 09:49:49 +0200

    Format the ESQL query

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring/queries/account_switches_esql_query.ts
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring/queries/account_switches_esql_query.ts
@@ -20,14 +20,14 @@ export const getAccountSwitchesEsqlSource = (
   | WHERE to_lower(process.command_line) RLIKE "(su .*|su|sudo su|sudo -i|sudo -s|ssh [^@]+@[^\s]+)"
   | FORK
     (
-        WHERE event.dataset != "endpoint.events.process" AND event.action =="logged-on"
-      | EVAL target_user = REPLACE(user.effective.name, "\\\\(.*\\\\)", "")
-      | EVAL group_name = COALESCE(user.group.name, user.group.id)
-      | RENAME process.command_line AS command_process, user.name as privileged_user, host.ip AS host_ip
+         WHERE event.dataset != "endpoint.events.process" AND event.action =="logged-on"
+      |  EVAL target_user = REPLACE(user.effective.name, "\\\\(.*\\\\)", "")
+      |  EVAL group_name = COALESCE(user.group.name, user.group.id)
+      |  RENAME process.command_line AS command_process, user.name as privileged_user, host.ip AS host_ip
     )
     (
-        WHERE event.dataset == "endpoint.events.process"
-      | RENAME process.command_line AS command_process, process.group_leader.user.name AS target_user, process.parent.real_group.name AS group_name, process.real_user.name as privileged_user, host.ip AS host_ip
+         WHERE event.dataset == "endpoint.events.process"
+      |  RENAME process.command_line AS command_process, process.group_leader.user.name AS target_user, process.parent.real_group.name AS group_name, process.real_user.name as privileged_user, host.ip AS host_ip
     )
     | KEEP @timestamp, privileged_user, host_ip, target_user, group_name, command_process, _id, _index`
   );
