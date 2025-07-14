18d1e539a4309e11be43a536321b413495ef33a3
abhishekbhatia1710 <abhishek.bhatia@elastic.co>
2025-07-14 19:06:47 +0530

    Addressing review comment : - to remove the hasUrlError state - to keep the text "View all events" consistent

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring/components/privileged_user_activity/index.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring/components/privileged_user_activity/index.tsx
@@ -70,8 +70,8 @@ export const UserActivityPrivilegedUsersPanel: React.FC<{
   const toggleOptions = useToggleOptions();
   const { getAppUrl } = useNavigation();
 
-  const { discoverUrl, hasUrlError } = useMemo(() => {
-    if (!generateTableQuery) return { discoverUrl: '', hasUrlError: false };
+  const { discoverUrl } = useMemo(() => {
+    if (!generateTableQuery) return { discoverUrl: '' };
 
     const query = generateTableQuery('@timestamp', 'DESC', 100);
     const appState = {
@@ -87,16 +87,13 @@ export const UserActivityPrivilegedUsersPanel: React.FC<{
           appId: 'discover',
           path: `#/?_a=${encodedAppState}`,
         }),
-        hasUrlError: false,
       };
     } catch (error) {
-      // Return a fallback URL that opens Discover without the pre-filled query
       return {
         discoverUrl: getAppUrl({
           appId: 'discover',
           path: '#/',
         }),
-        hasUrlError: true,
       };
     }
   }, [generateTableQuery, getAppUrl]);
@@ -115,17 +112,10 @@ export const UserActivityPrivilegedUsersPanel: React.FC<{
       >
         {generateTableQuery && (
           <LinkAnchor href={discoverUrl}>
-            {hasUrlError ? (
-              <FormattedMessage
-                id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.userActivity.linkDescriptionError"
-                defaultMessage="Open Discover (query not pre-filled)"
-              />
-            ) : (
-              <FormattedMessage
-                id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.userActivity.linkDescription"
-                defaultMessage="View all events"
-              />
-            )}
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.privilegedUserMonitoring.userActivity.linkDescription"
+              defaultMessage="View all events"
+            />
           </LinkAnchor>
         )}
       </HeaderSection>
