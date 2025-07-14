66e48e69d3dd94fb4a91551b03a6925a5d4f8ec8
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-14 11:56:21 +0200

    [ska] relocate observability functional and api_integration tests (#227754)
    ## Summary
    
    Part of https://github.com/elastic/kibana-team/issues/1503
    
    This PR is mostly about moving oblt functional tests from x-pack/test
    
    ```
    x-pack/test/
         | - observability_api_integration/
         | - observability_functional/
         | - observability_onboarding_api_integration/
         | - observability_ai_assistant_functional/
         | - functional/apps/
             | - infra/
             | - uptime/
         | - accessibility/apps/
    ```
    
    After:
    ```
    x-pack/solutions/observability/test/
         | - observability_api_integration/
         | - observability_functional/
         | - observability_onboarding_api_integration/
         | - observability_ai_assistant_functional/
         | - functional/apps/
             | - infra/
             | - uptime/
         | - accessibility/apps/
    ```
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 6c32e428859a8072a856cfcbe25ace94822b4258)
    
    # Conflicts:
    #	.github/CODEOWNERS
    #	x-pack/solutions/observability/test/accessibility/apps/uptime.ts
    #	x-pack/solutions/observability/test/functional/apps/infra/metrics_source_configuration.ts
    #	x-pack/solutions/observability/test/functional/apps/uptime/certificates.ts
    #	x-pack/solutions/observability/test/functional/apps/uptime/missing_mappings.ts
    #	x-pack/solutions/observability/test/functional/apps/uptime/settings.ts

--- x-pack/platform/test/functional/page_objects/index.ts
+++ x-pack/platform/test/functional/page_objects/index.ts
@@ -25,6 +25,7 @@ import { SearchSessionsPageProvider } from './search_sessions_management_page';
 import { GraphPageObject } from './graph_page';
 import { MaintenanceWindowsPageProvider } from './maintenance_windows_page';
 import { BannersPageObject } from './banners_page';
+import { NavigationalSearchPageObject } from './navigational_search';
 
 // just like services, PageObjects are defined as a map of
 // names to Providers. Merge in Kibana's or pick specific ones
@@ -49,4 +50,5 @@ export const pageObjects = {
   userProfiles: UserProfilePageProvider,
   maintenanceWindows: MaintenanceWindowsPageProvider,
   banners: BannersPageObject,
+  navigationalSearch: NavigationalSearchPageObject,
 };
