9acf9f93723c1f809cf83d9be95fb6b1694e9867
Eyo O. Eyo <7893459+eokoneyo@users.noreply.github.com>
2025-07-15 14:56:52 +0200

    [Share] Resolves bug on attempting to copy dashboard link from non-default space (#227625)
    ## Summary
    
    Closes https://github.com/elastic/kibana/issues/191090
    Closes https://github.com/elastic/kibana/issues/227976
    
    Only pass along url intended to be shortened, when validating that we
    aren't shortening some URL external to Kibana
    
    <!--
    ### Checklist
    
    Check the PR satisfies following conditions.
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [ ] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    ### Identify risks
    
    Does this PR introduce any risks? For example, consider risks like hard
    to test bugs, performance regression, potential of data loss.
    
    Describe the risk, its severity, and mitigation for each identified
    risk. Invite stakeholders and evaluate how to proceed before merging.
    
    - [ ] [See some risk
    examples](https://github.com/elastic/kibana/blob/main/RISK_MATRIX.mdx)
    - [ ] ...
    
    -->
    
    (cherry picked from commit 245c8fb86326d0ac25a9a6106e5fcfec09f4fab2)

---
+++ x-pack/test/functional/apps/dashboard/group3/share/share_from_custom_space.ts
@@ -0,0 +1,88 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from '@kbn/expect';
+import type { FtrProviderContext } from '../../../../ftr_provider_context';
+
+export default function sharingFromSpace({ getPageObjects, getService }: FtrProviderContext) {
+  const kibanaServer = getService('kibanaServer');
+  const spacesService = getService('spaces');
+  const browser = getService('browser');
+  const { dashboard, common, share, security, spaceSelector } = getPageObjects([
+    'dashboard',
+    'common',
+    'share',
+    'security',
+    'spaceSelector',
+  ]);
+
+  const spaceId = 'another-space';
+
+  describe('Dashboard Custom Space share', () => {
+    before(async () => {
+      await spacesService.create({
+        id: spaceId,
+        name: 'Another Space',
+        disabledFeatures: [],
+      });
+
+      await kibanaServer.importExport.load(
+        'test/functional/fixtures/kbn_archiver/dashboard/current/kibana',
+        {
+          space: spaceId,
+        }
+      );
+
+      await kibanaServer.uiSettings.replace({
+        defaultIndex: '0bf35f60-3dc9-11e8-8660-4d65aa086b3c',
+      });
+
+      await security.forceLogout();
+
+      await security.login(undefined, undefined, {
+        expectSpaceSelector: true,
+      });
+
+      await spaceSelector.clickSpaceCard(spaceId);
+
+      await common.navigateToApp(dashboard.APP_ID, { basePath: `/s/${spaceId}` });
+      await dashboard.preserveCrossAppState();
+      await dashboard.loadSavedDashboard('few panels');
+      await dashboard.switchToEditMode();
+      await dashboard.waitForRenderComplete();
+    });
+
+    after(async () => {
+      await security.forceLogout();
+
+      await kibanaServer.importExport.unload(
+        'test/functional/fixtures/kbn_archiver/dashboard/current/kibana',
+        {
+          space: spaceId,
+        }
+      );
+      await spacesService.delete(spaceId);
+      await kibanaServer.savedObjects.cleanStandardList();
+    });
+
+    it('should copy the dashboard url', async () => {
+      await share.openShareModalItem('link');
+      const shareUrl = await share.getSharedUrl();
+      expect(shareUrl).to.contain(`/s/${spaceId}/`);
+      await browser.openNewTab();
+      await browser.navigateTo(shareUrl);
+      await dashboard.expectOnDashboard('few panels');
+      // need to make sure there aren't extra tabs or it will impact future test suites
+      // close any new tabs that were opened
+      const windowHandlers = await browser.getAllWindowHandles();
+      if (windowHandlers.length > 1) {
+        await browser.closeCurrentWindow();
+        await browser.switchToWindow(windowHandlers[0]);
+      }
+    });
+  });
+}
