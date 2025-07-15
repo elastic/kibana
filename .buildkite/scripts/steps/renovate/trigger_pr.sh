366c7b0e1dd228937b08ff7220c703851e03e207
Brad White <brad.white@elastic.co>
2025-07-15 11:53:26 -0600

    Revert "[CI] Renovate helper scripts (#225345)"
    This reverts commit e128b39326bff5112c99984f9f828200cd3d7f86.

--- .buildkite/scripts/steps/renovate/trigger_pr.sh
+++ .buildkite/scripts/steps/renovate/trigger_pr.sh
@@ -3,12 +3,6 @@
 set -euo pipefail
 
 echo --- Triggering Kibana Pull Request Pipeline
+exit 0
 
-# The Github env vars need to be passed along manually
-# GITHUB_PR_* is used to avoid any sensitive vars being passed along
-GITHUB_ENV_VARS=()
-for var in $(env | grep ^GITHUB_PR_ | cut -d= -f1); do
-  GITHUB_ENV_VARS+=("$var=${!var}")
-done
-
-ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-pull-request "$BUILDKITE_BRANCH" "$BUILDKITE_COMMIT" "" "${GITHUB_ENV_VARS[*]}"
+ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-pull-request "$BUILDKITE_BRANCH"
