c2d3349ba0f2e26b06abdc3a08765a5071aaf030
Ania Kowalska <ania.kowalska@elastic.co>
2025-07-15 12:06:33 +0200

    remove redundant import

--- src/platform/plugins/shared/data/public/plugin.ts
+++ src/platform/plugins/shared/data/public/plugin.ts
@@ -7,8 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import './index.scss';
-
 import { PluginInitializerContext, CoreSetup, CoreStart, Plugin } from '@kbn/core/public';
 import {
   Storage,
