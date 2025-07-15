49cd020c2ca23a2c6d4cd863de04e0fc9074d98e
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-15 17:25:37 +0200

    fix imports

--- x-pack/solutions/observability/test/api_integration_deployment_agnostic/apis/ai_assistant/public_complete/public_complete.spec.ts
+++ x-pack/solutions/observability/test/api_integration_deployment_agnostic/apis/ai_assistant/public_complete/public_complete.spec.ts
@@ -16,7 +16,7 @@ import {
 } from '@kbn/observability-ai-assistant-plugin/common/conversation_complete';
 import { type Instruction } from '@kbn/observability-ai-assistant-plugin/common/types';
 import { createLlmProxy, LlmProxy } from '../utils/create_llm_proxy';
-import type { DeploymentAgnosticFtrProviderContext } from '../../../../ftr_provider_context';
+import type { DeploymentAgnosticFtrProviderContext } from '../../../ftr_provider_context';
 import { decodeEvents } from '../utils/conversation';
 
 export default function ApiTest({ getService }: DeploymentAgnosticFtrProviderContext) {
