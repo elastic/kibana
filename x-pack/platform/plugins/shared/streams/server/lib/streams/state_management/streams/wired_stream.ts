5d2966960b81c3938eeb37bd533ccb190100caf4
klacabane <kevin.lacabane@elastic.co>
2025-07-15 19:18:06 +0200

    fix inherited lifecycle at creation

--- x-pack/platform/plugins/shared/streams/server/lib/streams/state_management/streams/wired_stream.ts
+++ x-pack/platform/plugins/shared/streams/server/lib/streams/state_management/streams/wired_stream.ts
@@ -440,8 +440,10 @@ export class WiredStream extends StreamActiveRecord<Streams.WiredStream.Definiti
     }
   }
 
-  protected async doDetermineCreateActions(): Promise<ElasticsearchAction[]> {
-    const ancestors = await this.dependencies.streamsClient.getAncestors(this._definition.name);
+  protected async doDetermineCreateActions(desiredState: State): Promise<ElasticsearchAction[]> {
+    const ancestors = getAncestorsAndSelf(this._definition.name).map(
+      (id) => desiredState.get(id)!.definition as Streams.WiredStream.Definition
+    );
     const lifecycle = findInheritedLifecycle(this._definition, ancestors);
 
     return [
