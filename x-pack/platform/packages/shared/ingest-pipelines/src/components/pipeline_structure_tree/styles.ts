9917e03709719fc353af4de91be3fc868ac2f6e8
Elena Stoeva <59341489+ElenaStoeva@users.noreply.github.com>
2025-07-15 15:24:20 +0100

    [Ingest Pipelines] Add structure tree components (#226883)
    Addresses https://github.com/elastic/kibana/issues/227405
    
    
    ## Summary
    
    This PR adds the Pipeline Structure Tree components and the relevant
    functionality in a new `ingest-pipelines` package. To test the component
    in Storybook, run `yarn storybook ingest_pipelines`. See design mocks in
    https://github.com/elastic/kibana/issues/227405.
    
    
    <img width="613" height="740" alt="Screenshot 2025-07-11 at 14 54 11"
    src="https://github.com/user-attachments/assets/cbe1c3f3-2150-4548-a8bc-5705fa5cbf51"
    />
    
    
    Note: There is no out-of-the-box EUI component that exactly matches the
    component from the design mocks so we are using EuiTreeView with added
    CSS for the tree nodes. However, this component doesn't have lines
    connecting the tree nodes and fo now we are creating the component
    without the lines. We could try adding them at the end. There is also an
    open issue (https://github.com/elastic/platform-ux-team/issues/780) for
    improving the EuiTreeView component.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

---
+++ x-pack/platform/packages/shared/ingest-pipelines/src/components/pipeline_structure_tree/styles.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { css } from '@emotion/react';
+import { EuiThemeComputed } from '@elastic/eui';
+
+export const getStyles = (euiTheme: EuiThemeComputed) => css`
+  [class*='cssTreeNode-'] {
+    background-color: ${euiTheme.colors.backgroundBasePlain};
+    padding: ${euiTheme.size.base} ${euiTheme.size.m};
+    border: ${euiTheme.border.thin};
+    width: 400px;
+    margin: ${euiTheme.size.base} 0;
+  }
+
+  [class*='cssTreeNode-']:hover {
+    background-color: ${euiTheme.colors.backgroundTransparentPrimary};
+  }
+
+  [class*='cssTreeNode-children'] {
+    margin-left: ${euiTheme.size.l};
+  }
+
+  // We want to disable EUI's logic for activating nodes but EuiTreeViewItems's
+  // isActive prop is not working correctly so we temporarily overwrite its active class
+  .euiTreeView__node--active {
+    background-color: ${euiTheme.colors.backgroundBasePlain} !important;
+  }
+
+  .cssTreeNode-root--active,
+  .cssTreeNode-children--active {
+    background-color: ${euiTheme.colors.backgroundLightPrimary} !important;
+    border: ${euiTheme.border.width.thick} solid ${euiTheme.colors.borderBasePrimary};
+  }
+
+  .cssTreeNode-morePipelines {
+    margin-left: ${euiTheme.size.base};
+    background-color: ${euiTheme.colors.backgroundLightPrimary};
+    border: none;
+  }
+`;
