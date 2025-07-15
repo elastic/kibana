292adca6c40df012336c0e04337cafa413e24a13
paulina.shakirova <paulina.shakirova@elastic.co>
2025-07-15 16:26:13 +0200

    fix tests

--- src/platform/test/api_integration/apis/guided_onboarding/get_config.ts
+++ src/platform/test/api_integration/apis/guided_onboarding/get_config.ts
@@ -18,7 +18,7 @@ export default function testGetGuideConfig({ getService }: FtrProviderContext) {
 
   describe(`GET ${getConfigsPath}`, () => {
     // check that production guides are present
-    ['siem', 'databaseSearch', 'kubernetes'].map((guideId) => {
+    ['siem', 'databaseSearch'].map((guideId) => {
       it(`returns config for ${guideId}`, async () => {
         const response = await supertest
           .get(`${getConfigsPath}/${guideId}`)
