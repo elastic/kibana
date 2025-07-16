77c5eaf949d7d1682ffc76e62ed076b279dae5bb
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2025-07-16 12:44:43 +0200

    feat(streams): processing indicator move

---
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/processor_status_indicator.tsx
@@ -0,0 +1,136 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import {
+  EuiAvatar,
+  EuiAvatarProps,
+  EuiLoadingSpinner,
+  EuiToolTip,
+  useEuiTheme,
+} from '@elastic/eui';
+import { useSelector } from '@xstate5/react';
+import { i18n } from '@kbn/i18n';
+import React from 'react';
+import { ProcessorConfigurationProps } from './processors';
+import { useSimulatorSelector } from './state_management/stream_enrichment_state_machine';
+
+export const ProcessorStatusIndicator = ({
+  processorRef,
+}: {
+  processorRef: ProcessorConfigurationProps['processorRef'];
+}) => {
+  const { euiTheme } = useEuiTheme();
+  const isNew = useSelector(processorRef, (snapshot) => snapshot.context.isNew);
+  const hasSimulation = useSimulatorSelector((snapshot) => Boolean(snapshot.context.simulation));
+  const isParticipatingInSimulation = useSimulatorSelector((snapshot) =>
+    snapshot.context.processors.find((p) => p.id === processorRef.id)
+  );
+  const isSimulationRunning = useSimulatorSelector((snapshot) =>
+    snapshot.matches('runningSimulation')
+  );
+
+  const isPending = useSimulatorSelector(
+    (snapshot) =>
+      snapshot.context.simulation &&
+      !snapshot.context.simulation.processors_metrics[processorRef.id]
+  );
+  const isFailing = useSimulatorSelector((snapshot) =>
+    Boolean(snapshot.context.simulation?.processors_metrics[processorRef.id]?.failed_rate === 1)
+  );
+
+  let variant:
+    | (EuiAvatarProps & {
+        tooltipContent?: string;
+      })
+    | null = null;
+
+  if (isParticipatingInSimulation) {
+    if (isSimulationRunning) {
+      const name = i18n.translate(
+        'xpack.streams.streamDetailView.managementTab.enrichment.simulatingProcessorTooltip',
+        { defaultMessage: 'Simulating processor' }
+      );
+      variant = {
+        name,
+        iconType: () => <EuiLoadingSpinner />,
+        color: euiTheme.colors.backgroundBasePrimary,
+      };
+    } else if (!hasSimulation || isPending) {
+      const name = i18n.translate(
+        'xpack.streams.streamDetailView.managementTab.enrichment.pendingProcessorTooltip',
+        { defaultMessage: 'Pending to run for simulation.' }
+      );
+      variant = {
+        name,
+        iconType: 'clock',
+        color: euiTheme.colors.backgroundBaseDisabled,
+        tooltipContent: name,
+      };
+    } else if (isFailing) {
+      const name = i18n.translate(
+        'xpack.streams.streamDetailView.managementTab.enrichment.failingProcessorTooltip',
+        {
+          defaultMessage: 'This processor configuration has failed the simulation.',
+        }
+      );
+      variant = {
+        name,
+        iconType: 'cross',
+        color: euiTheme.colors.backgroundBaseDanger,
+        tooltipContent: name,
+      };
+    } else {
+      const name = i18n.translate(
+        'xpack.streams.streamDetailView.managementTab.enrichment.newProcessorTooltip',
+        {
+          defaultMessage:
+            'This processor configuration has successfully gone throught the simulation.',
+        }
+      );
+      variant = {
+        name,
+        iconType: 'check',
+        color: euiTheme.colors.backgroundBaseSuccess,
+        tooltipContent: name,
+      };
+    }
+  } else {
+    const name = isNew
+      ? i18n.translate(
+          'xpack.streams.streamDetailView.managementTab.enrichment.skippedNewProcessorTooltip',
+          {
+            defaultMessage:
+              'This processor is skipped since it comes after the processor under edit.',
+          }
+        )
+      : i18n.translate(
+          'xpack.streams.streamDetailView.managementTab.enrichment.skippedConfiguredProcessorTooltip',
+          {
+            defaultMessage: 'This processor is skipped since it is persisted already.',
+          }
+        );
+
+    variant = {
+      name,
+      iconType: 'dot',
+      color: euiTheme.colors.backgroundBaseDisabled,
+      tooltipContent: name,
+    };
+  }
+
+  if (!variant) {
+    return null;
+  }
+
+  const { tooltipContent, ...avatarProps } = variant;
+
+  return (
+    <EuiToolTip content={tooltipContent}>
+      <EuiAvatar size="m" {...avatarProps} />
+    </EuiToolTip>
+  );
+};
--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/processors_list.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/processors_list.tsx
@@ -6,19 +6,10 @@
  */
 
 import React from 'react';
-import {
-  EuiAvatar,
-  EuiDraggable,
-  EuiLoadingSpinner,
-  EuiTimelineItem,
-  EuiToolTip,
-  useEuiTheme,
-} from '@elastic/eui';
+import { EuiDraggable, EuiTimelineItem } from '@elastic/eui';
 import { css } from '@emotion/react';
-import { i18n } from '@kbn/i18n';
-import { useSelector } from '@xstate5/react';
 import { ProcessorConfiguration, ProcessorConfigurationProps } from './processors';
-import { useSimulatorSelector } from './state_management/stream_enrichment_state_machine';
+import { ProcessorStatusIndicator } from './processor_status_indicator';
 
 export const DraggableProcessorListItem = ({
   idx,
@@ -57,114 +48,5 @@ export const DraggableProcessorListItem = ({
         )}
       </EuiDraggable>
     </EuiTimelineItem>
-  );
-};
-
-const ProcessorStatusIndicator = ({
-  processorRef,
-}: {
-  processorRef: ProcessorConfigurationProps['processorRef'];
-}) => {
-  const { euiTheme } = useEuiTheme();
-  const isNew = useSelector(processorRef, (snapshot) => snapshot.context.isNew);
-  const hasSimulation = useSimulatorSelector((snapshot) => Boolean(snapshot.context.simulation));
-  const isParticipatingInSimulation = useSimulatorSelector((snapshot) =>
-    snapshot.context.processors.find((p) => p.id === processorRef.id)
-  );
-  const isSimulationRunning = useSimulatorSelector((snapshot) =>
-    snapshot.matches('runningSimulation')
-  );
-
-  const isPending = useSimulatorSelector(
-    (snapshot) =>
-      snapshot.context.simulation &&
-      !snapshot.context.simulation.processors_metrics[processorRef.id]
-  );
-  const isFailing = useSimulatorSelector((snapshot) =>
-    Boolean(snapshot.context.simulation?.processors_metrics[processorRef.id]?.failed_rate === 1)
-  );
-
-  let variant = {};
-
-  if (isParticipatingInSimulation) {
-    if (isSimulationRunning) {
-      const name = i18n.translate(
-        'xpack.streams.streamDetailView.managementTab.enrichment.simulatingProcessorTooltip',
-        { defaultMessage: 'Simulating processor' }
-      );
-      variant = {
-        name,
-        iconType: () => <EuiLoadingSpinner />,
-        color: euiTheme.colors.backgroundBasePrimary,
-      };
-    } else if (!hasSimulation || isPending) {
-      const name = i18n.translate(
-        'xpack.streams.streamDetailView.managementTab.enrichment.pendingProcessorTooltip',
-        { defaultMessage: 'Pending to run for simulation.' }
-      );
-      variant = {
-        name,
-        iconType: 'clock',
-        color: euiTheme.colors.backgroundBaseDisabled,
-        tooltipContent: name,
-      };
-    } else if (isFailing) {
-      const name = i18n.translate(
-        'xpack.streams.streamDetailView.managementTab.enrichment.failingProcessorTooltip',
-        {
-          defaultMessage: 'This processor configuration has failed the simulation.',
-        }
-      );
-      variant = {
-        name,
-        iconType: 'cross',
-        color: euiTheme.colors.backgroundBaseDanger,
-        tooltipContent: name,
-      };
-    } else {
-      const name = i18n.translate(
-        'xpack.streams.streamDetailView.managementTab.enrichment.newProcessorTooltip',
-        {
-          defaultMessage:
-            'This processor configuration has successfully gone throught the simulation.',
-        }
-      );
-      variant = {
-        name,
-        iconType: 'check',
-        color: euiTheme.colors.backgroundBaseSuccess,
-        tooltipContent: name,
-      };
-    }
-  } else {
-    const name = isNew
-      ? i18n.translate(
-          'xpack.streams.streamDetailView.managementTab.enrichment.skippedNewProcessorTooltip',
-          {
-            defaultMessage:
-              'This processor is skipped since it comes after the processor under edit.',
-          }
-        )
-      : i18n.translate(
-          'xpack.streams.streamDetailView.managementTab.enrichment.skippedConfiguredProcessorTooltip',
-          {
-            defaultMessage: 'This processor is skipped since it is persisted already.',
-          }
-        );
-
-    variant = {
-      name,
-      iconType: 'dot',
-      color: euiTheme.colors.backgroundBaseDisabled,
-      tooltipContent: name,
-    };
-  }
-
-  const { tooltipContent, ...avatarProps } = variant;
-
-  return (
-    <EuiToolTip content={tooltipContent}>
-      <EuiAvatar size="m" {...avatarProps} />
-    </EuiToolTip>
   );
 };
