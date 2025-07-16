794b84ec109877f461124e2c09797bf638269443
Arturo Lidueña <arturo.liduena@elastic.co>
2025-07-16 12:39:08 +0200

    [8.19] fix: product docs installation status (#226919) (#228144)
    # Backport
    
    This will backport the following commits from `main` to `8.19`:
    - [fix: product docs installation status
    (#226919)](https://github.com/elastic/kibana/pull/226919)
    
    <!--- Backport version: 10.0.1 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Arturo
    Lidueña","email":"arturo.liduena@elastic.co"},"sourceCommit":{"committedDate":"2025-07-15T11:51:32Z","message":"fix:
    product docs installation status (#226919)\n\nCloses
    [226914](https://github.com/elastic/kibana/issues/226914)\n##
    Summary\n\nAfter switching the KB model, product documentation was
    not\nautomatically installed, even if it had been installed previously.
    The\nuser must reload the page to view the current installation status
    of the\nproduct
    documentation.\n\n\nhttps://github.com/user-attachments/assets/a60a98e2-30a3-4253-b423-366fe00f1845\n\n---------\n\nCo-authored-by:
    Viduni Wickramarachchi
    <viduni.wickramarachchi@elastic.co>","sha":"28e765c1cee10dc6f6c5ea266b45130fb6789e05","branchLabelMapping":{"^v9.2.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:fix","Team:Obs
    AI
    Assistant","ci:project-deploy-observability","backport:version","v9.1.0","v8.19.0","v9.2.0"],"title":"fix:
    product docs installation
    status","number":226919,"url":"https://github.com/elastic/kibana/pull/226919","mergeCommit":{"message":"fix:
    product docs installation status (#226919)\n\nCloses
    [226914](https://github.com/elastic/kibana/issues/226914)\n##
    Summary\n\nAfter switching the KB model, product documentation was
    not\nautomatically installed, even if it had been installed previously.
    The\nuser must reload the page to view the current installation status
    of the\nproduct
    documentation.\n\n\nhttps://github.com/user-attachments/assets/a60a98e2-30a3-4253-b423-366fe00f1845\n\n---------\n\nCo-authored-by:
    Viduni Wickramarachchi
    <viduni.wickramarachchi@elastic.co>","sha":"28e765c1cee10dc6f6c5ea266b45130fb6789e05"}},"sourceBranch":"main","suggestedTargetBranches":["8.19"],"targetPullRequestStates":[{"branch":"9.1","label":"v9.1.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"url":"https://github.com/elastic/kibana/pull/227992","number":227992,"state":"MERGED","mergeCommit":{"sha":"5dba1bdbca9f1b2bd41614cd4a4a08308f8cce76","message":"[9.1]
    fix: product docs installation status (#226919) (#227992)\n\n#
    Backport\n\nThis will backport the following commits from `main` to
    `9.1`:\n- [fix: product docs installation
    status\n(#226919)](https://github.com/elastic/kibana/pull/226919)\n\n\n\n###
    Questions ?\nPlease refer to the [Backport
    tool\ndocumentation](https://github.com/sorenlouv/backport)\n\n\n\nCo-authored-by:
    Arturo Lidueña <arturo.liduena@elastic.co>\nCo-authored-by: Viduni
    Wickramarachchi
    <viduni.wickramarachchi@elastic.co>"}},{"branch":"8.19","label":"v8.19.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v9.2.0","branchLabelMappingKey":"^v9.2.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/226919","number":226919,"mergeCommit":{"message":"fix:
    product docs installation status (#226919)\n\nCloses
    [226914](https://github.com/elastic/kibana/issues/226914)\n##
    Summary\n\nAfter switching the KB model, product documentation was
    not\nautomatically installed, even if it had been installed previously.
    The\nuser must reload the page to view the current installation status
    of the\nproduct
    documentation.\n\n\nhttps://github.com/user-attachments/assets/a60a98e2-30a3-4253-b423-366fe00f1845\n\n---------\n\nCo-authored-by:
    Viduni Wickramarachchi
    <viduni.wickramarachchi@elastic.co>","sha":"28e765c1cee10dc6f6c5ea266b45130fb6789e05"}}]}]
    BACKPORT-->

--- x-pack/solutions/observability/plugins/observability_ai_assistant_management/public/hooks/use_get_product_doc.ts
+++ x-pack/solutions/observability/plugins/observability_ai_assistant_management/public/hooks/use_get_product_doc.ts
@@ -5,14 +5,21 @@
  * 2.0.
  */
 
+import { useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
 import { REACT_QUERY_KEYS } from '../constants';
 import { useKibana } from './use_kibana';
+import { useUninstallProductDoc } from './use_uninstall_product_doc';
+import { useInstallProductDoc } from './use_install_product_doc';
 
-export function useGetProductDocStatus(inferenceId: string | undefined) {
+export function useGetProductDoc(inferenceId: string | undefined) {
   const { productDocBase } = useKibana().services;
 
-  const { isLoading, isError, isSuccess, isRefetching, data, refetch } = useQuery({
+  const { mutateAsync: installProductDoc, isLoading: isInstalling } = useInstallProductDoc();
+
+  const { mutateAsync: uninstallProductDoc, isLoading: isUninstalling } = useUninstallProductDoc();
+
+  const { isLoading, data, refetch } = useQuery({
     queryKey: [REACT_QUERY_KEYS.GET_PRODUCT_DOC_STATUS, inferenceId],
     queryFn: async () => {
       return productDocBase!.installation.getStatus({ inferenceId });
@@ -21,12 +28,15 @@ export function useGetProductDocStatus(inferenceId: string | undefined) {
     refetchOnWindowFocus: false,
   });
 
+  useEffect(() => {
+    refetch();
+  }, [inferenceId, isInstalling, isUninstalling, refetch]);
+
   return {
-    status: data,
+    status: data?.inferenceId === inferenceId ? data?.overall : undefined,
     refetch,
     isLoading,
-    isRefetching,
-    isSuccess,
-    isError,
+    installProductDoc,
+    uninstallProductDoc,
   };
 }
