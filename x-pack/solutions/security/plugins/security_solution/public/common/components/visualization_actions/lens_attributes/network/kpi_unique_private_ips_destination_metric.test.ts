255fcbfc421d67a2744c6fc3ba37d1422db86c2f
lgestc <lukasz.gmys@elastic.co>
2025-07-16 15:15:31 +0200

    broken tests

--- x-pack/solutions/security/plugins/security_solution/public/common/components/visualization_actions/lens_attributes/network/kpi_unique_private_ips_destination_metric.test.ts
+++ x-pack/solutions/security/plugins/security_solution/public/common/components/visualization_actions/lens_attributes/network/kpi_unique_private_ips_destination_metric.test.ts
@@ -11,6 +11,8 @@ import { wrapper } from '../../mocks';
 import { useLensAttributes } from '../../use_lens_attributes';
 
 import { kpiUniquePrivateIpsDestinationMetricLensAttributes } from './kpi_unique_private_ips_destination_metric';
+import { getMockDataViewWithMatchedIndices } from '../../../../../data_view_manager/mocks/mock_data_view';
+import { useDataView } from '../../../../../data_view_manager/hooks/use_data_view';
 
 jest.mock('../../../../../sourcerer/containers', () => ({
   useSourcererDataView: jest.fn().mockReturnValue({
@@ -32,6 +34,16 @@ jest.mock('../../../../utils/route/use_route_spy', () => ({
 }));
 
 describe('kpiUniquePrivateIpsDestinationMetricLensAttributes', () => {
+  beforeAll(() => {
+    const dataView = getMockDataViewWithMatchedIndices(['auditbeat-mytest-*']);
+    dataView.id = 'security-solution-my-test';
+
+    jest.mocked(useDataView).mockReturnValue({
+      dataView,
+      status: 'ready',
+    });
+  });
+
   it('should render', () => {
     const { result } = renderHook(
       () =>
