f8b6aadf9f858d7bbb0877fd0c069ea6d36e3321
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-07-16 19:49:04 -0400

    [Obs AI Assistant][Anonymization] Register UI setting for anonymization (#224607)
    Closes https://github.com/elastic/obs-ai-assistant-team/issues/296
    
    ## Summary
    
    This UI setting needs to be registered for anonymization to appear in
    advance settings.
    This should be merged once, all other MVP tasks have been merged.
    
    ### Checklist
    
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    
    Code contributions: o3
    
    ---------
    
    Co-authored-by: Dario Gieselaar <dario.gieselaar@elastic.co>
    Co-authored-by: Sandra G <neptunian@users.noreply.github.com>
    Co-authored-by: Sandra Gonzales <sandra.gonzales@elastic.co>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/platform/plugins/private/kibana_usage_collection/server/collectors/management/schema.ts
+++ src/platform/plugins/private/kibana_usage_collection/server/collectors/management/schema.ts
@@ -488,6 +488,10 @@ export const stackManagementSchema: MakeSchemaFrom<UsageStats> = {
     type: 'text',
     _meta: { description: 'Non-default value of setting.' },
   },
+  'ai:anonymizationSettings': {
+    type: 'text',
+    _meta: { description: 'Non-default value of setting.' },
+  },
   'observability:logSources': {
     type: 'array',
     items: {
