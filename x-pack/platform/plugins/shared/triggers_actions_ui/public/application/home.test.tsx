77264134110169bcbf1116e901edaf42f153ccba
Kevin Delemme <kevin.delemme@elastic.co>
2025-07-16 16:39:36 -0400

    Fix test

--- x-pack/platform/plugins/shared/triggers_actions_ui/public/application/home.test.tsx
+++ x-pack/platform/plugins/shared/triggers_actions_ui/public/application/home.test.tsx
@@ -5,19 +5,17 @@
  * 2.0.
  */
 
-import * as React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
-import { RouteComponentProps } from 'react-router-dom';
-import { Router } from '@kbn/shared-ux-router';
 import { __IntlProvider as IntlProvider } from '@kbn/i18n-react';
-import { usePerformanceContext } from '@kbn/ebt-tools';
-import { createMemoryHistory, createLocation } from 'history';
+import { Router } from '@kbn/shared-ux-router';
 import { mountWithIntl } from '@kbn/test-jest-helpers';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { render, screen } from '@testing-library/react';
+import { createLocation, createMemoryHistory } from 'history';
+import * as React from 'react';
+import { RouteComponentProps } from 'react-router-dom';
+import { getIsExperimentalFeatureEnabled } from '../common/get_experimental_features';
 import TriggersActionsUIHome, { MatchParams } from './home';
 import { hasShowActionsCapability } from './lib/capabilities';
-import { getIsExperimentalFeatureEnabled } from '../common/get_experimental_features';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 jest.mock('../common/lib/kibana');
 jest.mock('../common/get_experimental_features');
@@ -33,6 +31,9 @@ jest.mock('./components/health_check', () => ({
 jest.mock('./context/health_context', () => ({
   HealthContextProvider: ({ children }: { children: React.ReactNode }) => <>{children}</>,
 }));
+jest.mock('@kbn/ebt-tools', () => ({
+  PerformanceContextProvider: ({ children }: { children: React.ReactNode }) => <>{children}</>,
+}));
 
 jest.mock('@kbn/alerts-ui-shared/src/common/hooks/use_get_rule_types_permissions', () => ({
   useGetRuleTypesPermissions: jest.fn().mockReturnValue({
@@ -43,10 +44,6 @@ jest.mock('@kbn/alerts-ui-shared/src/common/hooks/use_get_rule_types_permissions
 const { useGetRuleTypesPermissions } = jest.requireMock(
   '@kbn/alerts-ui-shared/src/common/hooks/use_get_rule_types_permissions'
 );
-jest.mock('@kbn/ebt-tools');
-
-const usePerformanceContextMock = usePerformanceContext as jest.Mock;
-usePerformanceContextMock.mockReturnValue({ onPageReady: jest.fn() });
 
 const queryClient = new QueryClient();
 
@@ -83,9 +80,7 @@ describe('home', () => {
       </IntlProvider>
     );
 
-    await waitFor(() => {
-      expect(screen.getByTestId('rulesListComponents')).toBeInTheDocument();
-    });
+    expect(await screen.findByTestId('rulesListComponents')).toBeInTheDocument();
   });
 
   it('shows the correct number of tabs', async () => {
