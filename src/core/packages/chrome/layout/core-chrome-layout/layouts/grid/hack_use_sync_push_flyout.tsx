2c584995161af1cefd1ae41151b88da344018c36
Anton Dosov <anton.dosov@elastic.co>
2025-07-16 12:40:16 +0200

    fix

--- src/core/packages/chrome/layout/core-chrome-layout/layouts/grid/hack_use_sync_push_flyout.tsx
+++ src/core/packages/chrome/layout/core-chrome-layout/layouts/grid/hack_use_sync_push_flyout.tsx
@@ -7,8 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { useEffect } from 'react';
-import { useEuiThemeCSSVariables } from '@elastic/eui';
+// import { useEffect } from 'react';
+// import { useEuiThemeCSSVariables } from '@elastic/eui';
 
 export const hackEuiPushFlyoutPaddingInlineEnd = '--euiPushFlyoutOffsetInlineEnd';
 export const hackEuiPushFlyoutPaddingInlineStart = '--euiPushFlyoutOffsetInlineStart';
