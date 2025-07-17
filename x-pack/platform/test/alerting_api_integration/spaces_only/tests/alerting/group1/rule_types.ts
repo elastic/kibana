cc96d49624d1aac43a4f0fed10e8cec16d1ac206
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-07-17 10:49:25 +0200

    Fix tests

--- x-pack/platform/test/alerting_api_integration/spaces_only/tests/alerting/group1/rule_types.ts
+++ x-pack/platform/test/alerting_api_integration/spaces_only/tests/alerting/group1/rule_types.ts
@@ -132,6 +132,7 @@ export default function listRuleTypes({ getService }: FtrProviderContext) {
             id: 'recovered',
             name: 'Recovered',
           },
+          autoRecoverAlerts: true,
           category: 'kibana',
           producer: 'alertsFixture',
           minimumLicenseRequired: 'basic',
