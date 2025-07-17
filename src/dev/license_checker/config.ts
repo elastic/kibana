6bee0cd52e0539614942f556d032adcd971b5a0d
Arturo Castillo Delgado <arturo.castillo@elastic.co>
2025-07-17 16:27:41 +0200

    Update eui packages with local tarballs

--- src/dev/license_checker/config.ts
+++ src/dev/license_checker/config.ts
@@ -93,6 +93,7 @@ export const LICENSE_OVERRIDES = {
     'Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0',
   ],
   '@elastic/eui-theme-borealis@3.2.0': ['Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0'],
+  '@elastic/eui-theme-common@3.0.0': ['Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0'],
   'language-subtag-registry@0.3.21': ['CC-BY-4.0'], // retired ODC‑By license https://github.com/mattcg/language-subtag-registry
   'buffers@0.1.1': ['MIT'], // license in importing module https://www.npmjs.com/package/binary
   '@bufbuild/protobuf@2.5.2': ['Apache-2.0'], // license (Apache-2.0 AND BSD-3-Clause)
