295a0a95e211d457ca4c6a20e1b2c04f73336116
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-07-17 10:25:25 +0200

    Address PR feedback

--- src/core/packages/saved-objects/api-server-internal/src/lib/utils/decorate_es_error.ts
+++ src/core/packages/saved-objects/api-server-internal/src/lib/utils/decorate_es_error.ts
@@ -9,6 +9,7 @@
 
 import { get } from 'lodash';
 import { errors as esErrors } from '@elastic/elasticsearch';
+import type { ErrorCause } from '@elastic/elasticsearch/lib/api/types';
 import type { ElasticsearchErrorDetails } from '@kbn/es-errors';
 import { isSupportedEsServer } from '@kbn/core-elasticsearch-server-internal';
 import { SavedObjectsErrorHelpers } from '@kbn/core-saved-objects-server';
@@ -38,9 +39,9 @@ export function decorateEsError(error: EsErrors) {
     throw new Error('Expected an instance of Error');
   }
 
-  const { reason: esErrorReason } = get(error, 'body.error', { reason: undefined }) as {
-    reason?: string | null;
-  };
+  const { reason: esErrorReason } = get(error, 'body.error', {
+    reason: undefined,
+  }) as ErrorCause;
   const reason = esErrorReason || undefined;
   if (
     error instanceof ConnectionError ||
