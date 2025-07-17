81f0f81cd89c0ddaabbfd27cc69fa9bc8539e177
Jean-Louis Leysens <jloleysens@gmail.com>
2025-07-17 11:52:17 +0200

    Kibana OAS validator CLI

---
+++ src/platform/packages/private/kbn-validate-oas/index.ts
@@ -0,0 +1,93 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import Path from 'node:path';
+import Fs from 'node:fs';
+import Util from 'node:util';
+import chalk from 'chalk';
+import { run } from '@kbn/dev-cli-runner';
+import { REPO_ROOT } from '@kbn/repo-info';
+
+const kibanaYamlPath = Path.resolve(REPO_ROOT, './oas_docs/output/kibana.yaml');
+const kibanaServerlessYamlPath = Path.resolve(
+  REPO_ROOT,
+  './oas_docs/output/kibana.serverless.yaml'
+);
+
+run(
+  async ({ log, flagsReader }) => {
+    const paths = flagsReader.arrayOfStrings('path');
+    const only = flagsReader.string('only') as 'traditional' | 'serverless' | undefined;
+    if (only && only !== 'traditional' && only !== 'serverless') {
+      log.error('Invalid value for --only flag, must be "traditional" or "serverless"');
+      process.exit(1);
+    }
+    // Load CommonJS version
+    const { Validator } = await import('@seriousme/openapi-schema-validator');
+    const validator = new Validator({ strict: false, allErrors: true });
+
+    let invalidSpec = false;
+
+    const yamlPaths: string[] = [];
+
+    if (only === 'traditional') {
+      yamlPaths.push(kibanaYamlPath);
+    } else if (only === 'serverless') {
+      yamlPaths.push(kibanaServerlessYamlPath);
+    } else {
+      yamlPaths.push(kibanaYamlPath, kibanaServerlessYamlPath);
+    }
+
+    for (const yamlPath of yamlPaths) {
+      log.info(`About to validate spec at ${chalk.underline(yamlPath)}`);
+      await log.indent(4, async () => {
+        const result = await validator.validate(Fs.readFileSync(yamlPath).toString('utf-8'));
+        if (!result.valid) {
+          log.warning(`${chalk.underline(yamlPath)} is NOT valid`);
+          const errors =
+            paths?.length && Array.isArray(result.errors)
+              ? result.errors.filter((error) =>
+                  paths.some((path) => error.instancePath.startsWith(path))
+                )
+              : result.errors;
+          log.warning(Util.inspect(errors, { depth: Infinity }));
+          log.warning(
+            `Found ${chalk.bold(
+              Array.isArray(errors) ? errors.length : 1
+            )} errors in ${chalk.underline(yamlPath)}`
+          );
+          invalidSpec = true;
+        } else {
+          log.success(`${chalk.underline(yamlPath)} is valid`);
+        }
+      });
+    }
+
+    log.info('Done');
+    if (invalidSpec) {
+      process.exit(1);
+    }
+  },
+  {
+    description: `Validate Kibana OAS YAML files (in oas_docs/output)`,
+    usage: `node ./scripts/validate_oas_docs.js`,
+    flags: {
+      string: ['path', 'only'],
+      help: `
+      --path             Pass in the (start of) a custom path to focus OAS validation error reporting, can be specified multiple times.
+      --only             Validate only OAS for the a specific offering, one of "traditional" or "serverless". Omitting this will validate all offerings.
+`,
+      examples: `
+node ./scripts/validate_oas_docs.js
+node ./scripts/validate_oas_docs.js --path /paths/~1api~1fleet~1agent_policies --path /paths/~1api~1fleet~1agent_policies
+node ./scripts/validate_oas_docs.js --only serverless --path /paths/~1api~1fleet~1agent_policies
+`,
+    },
+  }
+);
