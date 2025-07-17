31fb4472fa9abb5b43086b82d81f60b9005e9e6c
Weronika Olejniczak <weronika.olejniczak@elastic.co>
2025-07-17 21:55:30 +0200

    fix: pass href to side nav footer item

--- src/core/packages/chrome/navigation/src/components/side_nav/footer_item.tsx
+++ src/core/packages/chrome/navigation/src/components/side_nav/footer_item.tsx
@@ -12,19 +12,20 @@ import { css } from '@emotion/react';
 import { EuiButtonIcon, EuiButtonIconProps, EuiToolTip, IconType } from '@elastic/eui';
 
 export interface SideNavFooterItemProps extends Omit<EuiButtonIconProps, 'iconType'> {
+  hasContent?: boolean;
+  href?: string;
+  iconType?: IconType;
   isCurrent: boolean;
+  label: string;
   onClick: () => void;
   onKeyDown?: (e: React.KeyboardEvent) => void;
-  label: string;
-  iconType?: IconType;
-  hasContent?: boolean;
 }
 
 /**
  * Toggle button pattern: https://eui.elastic.co/docs/components/navigation/buttons/button/#toggle-button
  */
 export const SideNavFooterItem = forwardRef<HTMLDivElement, SideNavFooterItemProps>(
-  ({ isCurrent, label, iconType, hasContent, ...props }, ref: ForwardedRef<HTMLDivElement>) => {
+  ({ hasContent, iconType, isCurrent, label, ...props }, ref: ForwardedRef<HTMLDivElement>) => {
     const wrapperStyles = css`
       display: flex;
       justify-content: center;
