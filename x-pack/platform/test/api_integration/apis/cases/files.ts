dcd78470aede574aeeb619a49b4a539c1569a591
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-17 11:47:44 +0200

    [9.1] [ska] relocate api and func tests (#227466) (#228349)
    # Backport
    
    This will backport the following commits from `main` to `9.1`:
    - [[ska] relocate api and func tests
    (#227466)](https://github.com/elastic/kibana/pull/227466)
    
    <!--- Backport version: 10.0.1 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Dzmitry
    Lemechko","email":"dzmitry.lemechko@elastic.co"},"sourceCommit":{"committedDate":"2025-07-11T16:07:34Z","message":"[ska]
    relocate api and func tests (#227466)\n\n## Summary\n\nPart of
    https://github.com/elastic/kibana-team/issues/1503\n\nThis PR is mostly
    about moving platform and solutions tests
    from\nx-pack/test\n\nBefore:\n```\nx-pack/test/api_integration/apis/cases\nx-pack/test/api_integration/apis/security\nx-pack/test/functional/apps/slo\n```\n\nAfter:
    \n```\nx-pack/platform/test/\n | -
    api_integration/apis/cases\n\nx-pack/solutions/observability/test/\n | -
    api_integration/apis/cases\n | -
    functional/apps/slo\n\nx-pack/solutions/security/test/\n | -
    api_integration/apis/cases\n | -
    api_integration/apis/security\n```\n\n---------\n\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"a2ed624e85efd8fec8f98d12e2614f9413726731","branchLabelMapping":{"^v9.2.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","backport:version","v9.1.0","v8.19.0","v9.2.0"],"title":"[ska]
    relocate api and func
    tests","number":227466,"url":"https://github.com/elastic/kibana/pull/227466","mergeCommit":{"message":"[ska]
    relocate api and func tests (#227466)\n\n## Summary\n\nPart of
    https://github.com/elastic/kibana-team/issues/1503\n\nThis PR is mostly
    about moving platform and solutions tests
    from\nx-pack/test\n\nBefore:\n```\nx-pack/test/api_integration/apis/cases\nx-pack/test/api_integration/apis/security\nx-pack/test/functional/apps/slo\n```\n\nAfter:
    \n```\nx-pack/platform/test/\n | -
    api_integration/apis/cases\n\nx-pack/solutions/observability/test/\n | -
    api_integration/apis/cases\n | -
    functional/apps/slo\n\nx-pack/solutions/security/test/\n | -
    api_integration/apis/cases\n | -
    api_integration/apis/security\n```\n\n---------\n\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"a2ed624e85efd8fec8f98d12e2614f9413726731"}},"sourceBranch":"main","suggestedTargetBranches":["9.1","8.19"],"targetPullRequestStates":[{"branch":"9.1","label":"v9.1.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.19","label":"v8.19.0","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"main","label":"v9.2.0","branchLabelMappingKey":"^v9.2.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/227466","number":227466,"mergeCommit":{"message":"[ska]
    relocate api and func tests (#227466)\n\n## Summary\n\nPart of
    https://github.com/elastic/kibana-team/issues/1503\n\nThis PR is mostly
    about moving platform and solutions tests
    from\nx-pack/test\n\nBefore:\n```\nx-pack/test/api_integration/apis/cases\nx-pack/test/api_integration/apis/security\nx-pack/test/functional/apps/slo\n```\n\nAfter:
    \n```\nx-pack/platform/test/\n | -
    api_integration/apis/cases\n\nx-pack/solutions/observability/test/\n | -
    api_integration/apis/cases\n | -
    functional/apps/slo\n\nx-pack/solutions/security/test/\n | -
    api_integration/apis/cases\n | -
    api_integration/apis/security\n```\n\n---------\n\nCo-authored-by:
    kibanamachine
    <42973632+kibanamachine@users.noreply.github.com>","sha":"a2ed624e85efd8fec8f98d12e2614f9413726731"}}]}]
    BACKPORT-->

--- x-pack/platform/test/api_integration/apis/cases/files.ts
+++ x-pack/platform/test/api_integration/apis/cases/files.ts
@@ -8,7 +8,7 @@
 import expect from '@kbn/expect';
 
 import type { BaseFilesClient } from '@kbn/shared-ux-file-types';
-import type { User } from '@kbn/test-suites-xpack-platform/cases_api_integration/common/lib/authentication/types';
+import type { User } from '../../../cases_api_integration/common/lib/authentication/types';
 import {
   createFile,
   uploadFile,
@@ -18,12 +18,12 @@ import {
   getFileById,
   deleteAllFilesForKind,
   deleteFileForFileKind,
-} from '@kbn/test-suites-xpack-platform/cases_api_integration/common/lib/api';
+} from '../../../cases_api_integration/common/lib/api';
 import {
   CASES_FILE_KIND,
   OBSERVABILITY_FILE_KIND,
   SECURITY_SOLUTION_FILE_KIND,
-} from '@kbn/test-suites-xpack-platform/cases_api_integration/common/lib/constants';
+} from '../../../cases_api_integration/common/lib/constants';
 import type { FtrProviderContext } from '../../ftr_provider_context';
 import {
   casesAllUser,
