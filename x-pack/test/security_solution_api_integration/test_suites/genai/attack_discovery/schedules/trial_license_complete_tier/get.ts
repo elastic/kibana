a2f96bf011e0868737f6f148c71cf2f887e56db9
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-07-18 10:05:06 +0200

    [Attack Discovery][Scheduling] Add integration tests to cover AD scheduling rule management endpoints (#12400) (#228061)
    ## Summary
    
    Main ticket ([Internal
    link](https://github.com/elastic/security-team/issues/12400))
    
    This PR adds basic integration tests for the attack discovery schedule
    feature API routes:
    - Create AD scheduling rule: `POST
    /internal/elastic_assistant/attack_discovery/schedules`
    - Read AD scheduling rule by id: `GET
    /internal/elastic_assistant/attack_discovery/schedules/{id}`
    - Update AD scheduling rule by id: `PUT
    /internal/elastic_assistant/attack_discovery/schedules/{id}`
    - Enable AD scheduling rule by id: `POST
    /internal/elastic_assistant/attack_discovery/schedules/{id}/_enable`
    - Disable AD scheduling rule by id: `POST
    /internal/elastic_assistant/attack_discovery/schedules/{id}/_disable`
    - Delete AD scheduling rule by id: `DELETE
    /internal/elastic_assistant/attack_discovery/schedules/{id}`
    - Find all existing AD scheduling rules: `GET
    /internal/elastic_assistant/attack_discovery/schedules/_find`
    
    ### Notes
    
    * This is a first round of integration tests and more (corner cases,
    RBAC etc.) will be added in following PR/s.
    
    (cherry picked from commit bf05b4ea6fac45ac409092307b47f6f06ea8883c)

---
+++ x-pack/test/security_solution_api_integration/test_suites/genai/attack_discovery/schedules/trial_license_complete_tier/get.ts
@@ -0,0 +1,53 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from 'expect';
+import { FtrProviderContext } from '../../../../../ftr_provider_context';
+import {
+  deleteAllAttackDiscoverySchedules,
+  enableAttackDiscoverySchedulesFeature,
+  getScheduleNotFoundError,
+} from '../utils/helpers';
+import { getAttackDiscoverySchedulesApis } from '../utils/apis';
+import { getSimpleAttackDiscoverySchedule } from '../mocks';
+
+export default ({ getService }: FtrProviderContext) => {
+  const supertest = getService('supertest');
+  const log = getService('log');
+
+  describe('@ess @serverless @serverlessQA Get', () => {
+    before(async () => {
+      await enableAttackDiscoverySchedulesFeature(supertest);
+    });
+
+    beforeEach(async () => {
+      await deleteAllAttackDiscoverySchedules({ supertest, log });
+    });
+
+    describe('Happy path', () => {
+      it('should return a schedule by `id`', async () => {
+        const apis = getAttackDiscoverySchedulesApis({ supertest });
+
+        const createdSchedule = await apis.create({ schedule: getSimpleAttackDiscoverySchedule() });
+
+        const schedule = await apis.get({ id: createdSchedule.id });
+
+        expect(schedule).toEqual(createdSchedule);
+      });
+    });
+
+    describe('Errors handling', () => {
+      it('should return a `Not Found` error if schedule does not exist', async () => {
+        const apis = getAttackDiscoverySchedulesApis({ supertest });
+
+        const result = await apis.get({ id: 'test-id', expectedHttpCode: 404 });
+
+        expect(result).toEqual(getScheduleNotFoundError('test-id'));
+      });
+    });
+  });
+};
---
+++ x-pack/test/security_solution_api_integration/test_suites/genai/attack_discovery/schedules/trial_license_complete_tier/index.ts
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { FtrProviderContext } from '../../../../../ftr_provider_context';
+
+export default function ({ loadTestFile }: FtrProviderContext) {
+  describe('GenAI - Attack Discovery Schedules APIs', function () {
+    loadTestFile(require.resolve('./create'));
+    loadTestFile(require.resolve('./delete'));
+    loadTestFile(require.resolve('./disable'));
+    loadTestFile(require.resolve('./enable'));
+    loadTestFile(require.resolve('./find'));
+    loadTestFile(require.resolve('./get'));
+    loadTestFile(require.resolve('./update'));
+  });
+}
