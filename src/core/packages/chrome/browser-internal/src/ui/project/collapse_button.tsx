be03b7e2f72b3a5f75a28be21430824efd855438
Anton Dosov <anton.dosov@elastic.co>
2025-07-18 14:16:39 +0200

    hacky integration

---
+++ src/core/packages/chrome/browser-internal/src/ui/project/collapse_button.tsx
@@ -0,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React, { FC } from 'react';
+import { EuiButtonIcon } from '@elastic/eui';
+import useObservable from 'react-use/lib/useObservable';
+import type { Observable } from 'rxjs';
+
+interface Props {
+  toggleSideNav: (isVisible: boolean) => void;
+  isSideNavCollapsed$: Observable<boolean>;
+}
+
+export const CollapseButton: FC<Props> = ({ isSideNavCollapsed$, toggleSideNav }) => {
+  const isCollapsed = useObservable(isSideNavCollapsed$, false);
+
+  // TODO: a11y
+  return (
+    <EuiButtonIcon
+      iconType={isCollapsed ? 'transitionLeftIn' : 'transitionLeftOut'}
+      color={'text'}
+      onClick={() => toggleSideNav(!isCollapsed)}
+    />
+  );
+};
