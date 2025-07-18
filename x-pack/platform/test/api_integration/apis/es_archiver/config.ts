38b240b47a7fdbaa36aeb91137ff1a2acf0206b3
Robert Oskamp <robert.oskamp@elastic.co>
2025-07-18 12:53:37 +0200

    Running with basic license and ssl enabled

--- x-pack/platform/test/api_integration/apis/es_archiver/config.ts
+++ x-pack/platform/test/api_integration/apis/es_archiver/config.ts
@@ -6,12 +6,43 @@
  */
 
 import { FtrConfigProviderContext } from '@kbn/test';
+import { CA_CERT_PATH } from '@kbn/dev-utils';
 
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const baseIntegrationTestsConfig = await readConfigFile(require.resolve('../../config.ts'));
 
+  const ssl = true;
+  const license = 'basic';
+
+  const servers = {
+    ...baseIntegrationTestsConfig.get('servers'),
+    elasticsearch: {
+      ...baseIntegrationTestsConfig.get('servers.elasticsearch'),
+      protocol: ssl ? 'https' : 'http',
+    },
+  };
+
   return {
     ...baseIntegrationTestsConfig.getAll(),
+    servers,
+    esTestCluster: {
+      ...baseIntegrationTestsConfig.get('esTestCluster'),
+      license,
+      ssl,
+      serverArgs: [`xpack.license.self_generated.type=${license}`],
+    },
+    kbnTestServer: {
+      ...baseIntegrationTestsConfig.get('kbnTestServer'),
+      serverArgs: [
+        ...baseIntegrationTestsConfig.get('kbnTestServer.serverArgs'),
+        ...(ssl
+          ? [
+              `--elasticsearch.hosts=${servers.elasticsearch.protocol}://${servers.elasticsearch.hostname}:${servers.elasticsearch.port}`,
+              `--elasticsearch.ssl.certificateAuthorities=${CA_CERT_PATH}`,
+            ]
+          : []),
+      ],
+    },
     testFiles: [require.resolve('.')],
     junit: {
       reportName: 'esArcxhiver Flakiness Tests',
