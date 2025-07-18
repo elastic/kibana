f61a7dbd78450adfa64085ba927b86f69a50b1a9
Robert Oskamp <robert.oskamp@elastic.co>
2025-07-18 10:24:18 +0200

    Investigate esArchiver flakiness

---
+++ x-pack/platform/test/api_integration/apis/es_archiver/index.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import expect from 'expect';
+import type { FtrProviderContext } from '../../ftr_provider_context';
+
+export default function ({ getService }: FtrProviderContext) {
+  const esArchiver = getService('esArchiver');
+  const es = getService('es');
+  const logger = getService('log');
+
+  describe('esArchiver', function () {
+    beforeEach(async () => {
+      const indexExistsResponse = await es.indices.exists({
+        index: 'myfakeindex-3',
+      });
+      logger.info('index exists', indexExistsResponse);
+      await esArchiver.load(
+        'x-pack/test/functional/es_archives/security_solution/timestamp_override_3'
+      );
+    });
+
+    afterEach(async () => {
+      await esArchiver.unload(
+        'x-pack/test/functional/es_archives/security_solution/timestamp_override_3'
+      );
+    });
+
+    describe('assertions on inserted documents', () => {
+      Array(100)
+        .fill(0)
+        .forEach(() => {
+          it('should have one document from the timestamp_override_3 archive', async () => {
+            const searchResponse = await es.search({
+              index: 'myfakeindex-3',
+            });
+
+            logger.info('shards', searchResponse._shards);
+            logger.info('hits.total', searchResponse.hits.total);
+
+            expect(searchResponse.hits.hits).toEqual([
+              expect.objectContaining({
+                _index: 'myfakeindex-3',
+                _source: {
+                  message: 'hello world 3',
+                  '@timestamp': '2020-12-16T15:16:18.570Z',
+                },
+              }),
+            ]);
+          });
+        });
+    });
+  });
+}
