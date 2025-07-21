5e60f5ce31e2b5aab16553d4ef2849f3499be831
iblancof <irene.blanco@elastic.co>
2025-07-21 12:57:57 +0200

    Create LogsClient

---
+++ x-pack/solutions/observability/plugins/apm/server/lib/helpers/create_es_client/create_logs_client/index.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { QueryDslQueryContainer } from '@kbn/data-views-plugin/common/types';
+import type { InferSearchResponseOf } from '@kbn/es-types';
+import type { APMRouteHandlerResources } from '../../../../routes/apm_routes/register_apm_server_routes';
+
+interface LogsClientSearchRequest {
+  query: QueryDslQueryContainer;
+  fields: string[];
+}
+
+export interface LogsClient {
+  search: <T = unknown>(props: LogsClientSearchRequest) => Promise<InferSearchResponseOf<T>>;
+}
+
+export const createLogsClient = async (
+  resources: APMRouteHandlerResources
+): Promise<LogsClient> => {
+  const { context } = resources;
+  const { savedObjects } = await context.core;
+
+  const logsDataAccess = await resources.plugins.logsDataAccess.start();
+  const logSourcesService = logsDataAccess.services.logSourcesServiceFactory.getLogSourcesService(
+    savedObjects.client
+  );
+  const logsIndexPattern = await (await logSourcesService).getFlattenedLogSources();
+
+  const esClient = (await context.core).elasticsearch.client.asCurrentUser;
+
+  async function search<T = unknown>(
+    props: LogsClientSearchRequest
+  ): Promise<InferSearchResponseOf<T>> {
+    const response = await esClient.search({
+      index: logsIndexPattern,
+      size: 1000,
+      track_total_hits: false,
+      query: props.query,
+      fields: props.fields,
+    });
+    return response as InferSearchResponseOf<T>;
+  }
+  return {
+    search,
+  };
+};
