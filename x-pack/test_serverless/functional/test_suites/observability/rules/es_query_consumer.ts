a6e5764c92cc64a9adfb51586a23384f4643c952
Dominique Belcher <dominique.clarke@elastic.co>
2025-07-21 15:50:44 -0400

    unskip functional tests

--- x-pack/test_serverless/functional/test_suites/observability/rules/es_query_consumer.ts
+++ x-pack/test_serverless/functional/test_suites/observability/rules/es_query_consumer.ts
@@ -61,9 +61,6 @@ export default ({ getPageObject, getService }: FtrProviderContext) => {
   }
 
   describe('ES Query rule - consumers', function () {
-    // Custom roles not yet supported in MKI
-    this.tags(['failsOnMKI']);
-
     const ruleIdList: string[] = [];
 
     before(async () => {
