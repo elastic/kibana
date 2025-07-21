6b721cfcea845222415c747146c4481a99fa378a
Jonathan Budzenski <jon@elastic.co>
2025-07-21 11:57:27 -0500

    [8.19][ci] Rely on yarn-local-mirror instead of node_modules

--- .buildkite/scripts/bootstrap.sh
+++ .buildkite/scripts/bootstrap.sh
@@ -16,10 +16,10 @@ fi
 # But only for agents not mounting the workspace on a local ssd or in memory
 # It actually ends up being slower to move all of the tiny files between the disks vs extracting archives from the yarn cache
 if [[ "$(pwd)" != *"/local-ssd/"* && "$(pwd)" != "/dev/shm"* ]]; then
-  if [[ -d ~/.kibana/node_modules ]]; then
-    echo "Using ~/.kibana/node_modules as a starting point"
-    mv ~/.kibana/node_modules ./
-  fi
+  # if [[ -d ~/.kibana/node_modules ]]; then
+  #   echo "Using ~/.kibana/node_modules as a starting point"
+  #   mv ~/.kibana/node_modules ./
+  # fi
   if [[ -d ~/.kibana/.yarn-local-mirror ]]; then
     echo "Using ~/.kibana/.yarn-local-mirror as a starting point"
     mv ~/.kibana/.yarn-local-mirror ./
