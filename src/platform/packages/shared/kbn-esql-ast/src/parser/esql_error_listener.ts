0e62edae8a393a7e4e8e427356dc16a18e0ebbe8
vadimkibana <vadimkibana@gmail.com>
2025-07-21 14:18:39 +0200

    cleanup error listener file

--- src/platform/packages/shared/kbn-esql-ast/src/parser/esql_error_listener.ts
+++ src/platform/packages/shared/kbn-esql-ast/src/parser/esql_error_listener.ts
@@ -7,41 +7,37 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { Recognizer, RecognitionException } from 'antlr4';
-import { ErrorListener } from 'antlr4';
+import * as antlr from 'antlr4';
 import { getPosition } from './helpers';
 import type { EditorError } from '../types';
 
 // These will need to be manually updated whenever the relevant grammar changes.
 const SYNTAX_ERRORS_TO_IGNORE = [`mismatched input '<EOF>' expecting {'row', 'from', 'show'}`];
 
-const REPLACE_DEV = /,{0,1}(?<!\s)\s*DEV_\w+\s*/g;
-const REPLACE_ORPHAN_COMMA = /{, /g;
-export class ESQLErrorListener extends ErrorListener<any> {
+export class ESQLErrorListener extends antlr.ErrorListener<antlr.Token> {
   protected errors: EditorError[] = [];
 
   syntaxError(
-    recognizer: Recognizer<any>,
-    offendingSymbol: any,
+    recognizer: antlr.Recognizer<any>,
+    offendingSymbol: antlr.Token,
     line: number,
     column: number,
     message: string,
-    error: RecognitionException | undefined
+    error: antlr.RecognitionException | undefined
   ): void {
     // Remove any DEV_ tokens from the error message
-    message = message.replace(REPLACE_DEV, '');
+    message = message.replace(/,{0,1}(?<!\s)\s*DEV_\w+\s*/g, '');
 
     // Remove any trailing commas from the error message... this handles
     // cases where the dev token was at the start of a list
     // e.g. "mismatched input 'PROJECT' expecting {, 'enrich', 'dissect', 'eval', 'grok'}"
-    message = message.replace(REPLACE_ORPHAN_COMMA, '{');
+    message = message.replace(/{, /g, '{');
 
     if (SYNTAX_ERRORS_TO_IGNORE.includes(message)) {
       return;
     }
 
     const textMessage = `SyntaxError: ${message}`;
-
     const tokenPosition = getPosition(offendingSymbol);
     const startColumn = offendingSymbol && tokenPosition ? tokenPosition.min + 1 : column + 1;
     const endColumn = offendingSymbol && tokenPosition ? tokenPosition.max + 1 : column + 2;
