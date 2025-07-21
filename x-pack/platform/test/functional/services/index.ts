b1e57cfd229a6246a4d06fc8771da493ce0a3a6b
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-21 14:56:35 +0200

    load missing service

--- x-pack/platform/test/functional/services/index.ts
+++ x-pack/platform/test/functional/services/index.ts
@@ -23,6 +23,7 @@ import { TransformProvider } from './transform';
 import { ReportingFunctionalProvider } from '../../reporting_functional/services';
 import { RandomProvider } from './random';
 import { PipelineListProvider } from './pipeline_list';
+import { PipelineEditorProvider } from './pipeline_editor';
 import { DataStreamProvider } from './data_stream';
 
 // define the name and providers for services that should be
@@ -50,5 +51,6 @@ export const services = {
   reporting: ReportingFunctionalProvider,
   random: RandomProvider,
   pipelineList: PipelineListProvider,
+  pipelineEditor: PipelineEditorProvider,
   dataStream: DataStreamProvider,
 };
