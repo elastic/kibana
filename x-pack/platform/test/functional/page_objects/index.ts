8bbdb3656b2d5af3e63d1bd1eff1822413dc9d54
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-21 12:22:27 +0200

    relocate POs

--- x-pack/platform/test/functional/page_objects/index.ts
+++ x-pack/platform/test/functional/page_objects/index.ts
@@ -35,6 +35,8 @@ import { IndexManagementPageProvider } from './index_management_page';
 import { ShareSavedObjectsToSpacePageProvider } from './share_saved_objects_to_space_page';
 import { StatusPageObject } from './status_page';
 import { GeoFileUploadPageObject } from './geo_file_upload';
+import { LogstashPageObject } from './logstash_page';
+import { IndexLifecycleManagementPageProvider } from './index_lifecycle_management_page';
 
 // just like services, PageObjects are defined as a map of
 // names to Providers. Merge in Kibana's or pick specific ones
@@ -69,4 +71,6 @@ export const pageObjects = {
   shareSavedObjectsToSpace: ShareSavedObjectsToSpacePageProvider,
   statusPage: StatusPageObject,
   geoFileUpload: GeoFileUploadPageObject,
+  indexLifecycleManagement: IndexLifecycleManagementPageProvider,
+  logstash: LogstashPageObject,
 };
