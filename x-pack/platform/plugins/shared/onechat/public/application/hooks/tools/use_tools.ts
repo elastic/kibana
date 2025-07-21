22d51c2a1658390dcb9573beb28171ec9601e908
Dennis Tismenko <dennis.tismenko@elastic.co>
2025-07-21 10:19:16 -0400

    [onechat] Implement tool deletion for ES|QL tools (#228660)
    ## Summary
    
    Implements ES|QL tool deletion from the ES|QL tools table.
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [x]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [x] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [x] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)

--- x-pack/platform/plugins/shared/onechat/public/application/hooks/tools/use_tools.ts
+++ x-pack/platform/plugins/shared/onechat/public/application/hooks/tools/use_tools.ts
@@ -8,9 +8,9 @@
 import { ToolDefinitionWithSchema, ToolType } from '@kbn/onechat-common';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { useMemo } from 'react';
-import { CreateToolPayload } from '../../../common/http_api/tools';
-import { queryKeys } from '../query_keys';
-import { useOnechatServices } from './use_onechat_service';
+import { CreateToolPayload } from '../../../../common/http_api/tools';
+import { queryKeys } from '../../query_keys';
+import { useOnechatServices } from '../use_onechat_service';
 
 export const useOnechatTools = () => {
   const { toolsService } = useOnechatServices();
@@ -27,21 +27,21 @@ export const useOnechatTools = () => {
   return { tools: tools ?? [], isLoading, error };
 };
 
-export const useOnechatBaseTools = () => {
+export const useBaseTools = () => {
   const { tools, ...rest } = useOnechatTools();
 
   const baseTools = useMemo(() => tools.filter((tool) => tool.type === ToolType.builtin), [tools]);
   return { tools: baseTools, ...rest };
 };
 
-export const useOnechatEsqlTools = () => {
+export const useEsqlTools = () => {
   const { tools, ...rest } = useOnechatTools();
 
   const esqlTools = useMemo(() => tools.filter((tool) => tool.type === ToolType.esql), [tools]);
   return { tools: esqlTools, ...rest };
 };
 
-export const useOnechatCreateTool = ({
+export const useCreateTool = ({
   onSuccess,
   onError,
 }: {
@@ -59,22 +59,4 @@ export const useOnechatCreateTool = ({
   });
 
   return { createTool: mutateAsync, isLoading };
-};
-
-export const useOnechatToolsTags = () => {
-  const { tools, isLoading, error } = useOnechatTools();
-
-  const tags = useMemo((): string[] => {
-    if (isLoading || error) return [];
-
-    // Return unique tags across all tools
-    return [
-      ...tools.reduce((tagsSet, tool) => {
-        tool.tags.forEach((tag) => tagsSet.add(tag));
-        return tagsSet;
-      }, new Set<string>()),
-    ];
-  }, [tools, isLoading, error]);
-
-  return { tags, isLoading };
 };
