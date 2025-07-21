42a7cca715f02517b3e9214b599cc3a3228ccd9f
Jonathan Budzenski <jon@elastic.co>
2025-07-21 18:05:30 -0500

    [ci] Move instead of copy es snapshot cache
    We're not using the original copies anywhere, populating the cache
    folder is sufficient.

--- .buildkite/scripts/setup_es_snapshot_cache.sh
+++ .buildkite/scripts/setup_es_snapshot_cache.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-# If cached snapshots are baked into the agent, copy them into our workspace first
+# If cached snapshots are baked into the agent, move them into our workspace first
 # We are doing this rather than simply changing the ES base path because many workers
 #   run with the workspace mounted in memory or on a local ssd
 cacheDir="$ES_CACHE_DIR/cache"
@@ -10,5 +10,5 @@ if [[ -d "$cacheDir" ]]; then
   mkdir -p .es/cache
   echo "--- Copying ES snapshot cache"
   echo "Copying cached snapshots from $cacheDir to .es/cache"
-  cp -R "$cacheDir"/* .es/cache/
+  mv "$cacheDir"/* .es/cache/
 fi
