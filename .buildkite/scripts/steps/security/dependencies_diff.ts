690d6d35c95de260fc1dbe13eb83a4eac88fd76e
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-07-22 14:10:07 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- .buildkite/scripts/steps/security/dependencies_diff.ts
+++ .buildkite/scripts/steps/security/dependencies_diff.ts
@@ -86,7 +86,9 @@ async function main() {
     console.info(`Reverting changes for ${filePath}`);
 
     try {
-      execSync(`git cat-file -e ${process.env.GITHUB_PR_MERGE_BASE}:${filePath}`, { stdio: 'ignore' });
+      execSync(`git cat-file -e ${process.env.GITHUB_PR_MERGE_BASE}:${filePath}`, {
+        stdio: 'ignore',
+      });
     } catch (error) {
       console.info(`File does not exist in merge base, skipping revert`);
       return;
