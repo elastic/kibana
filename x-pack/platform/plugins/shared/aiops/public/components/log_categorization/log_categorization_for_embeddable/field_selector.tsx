7ba4eb47322620099657a950ea87317a228d555e
James Gowdy <jgowdy@elastic.co>
2025-07-22 11:21:58 +0100

    adding tooltip for field options

--- x-pack/platform/plugins/shared/aiops/public/components/log_categorization/log_categorization_for_embeddable/field_selector.tsx
+++ x-pack/platform/plugins/shared/aiops/public/components/log_categorization/log_categorization_for_embeddable/field_selector.tsx
@@ -18,6 +18,7 @@ import {
   EuiFlexItem,
   EuiToken,
   useGeneratedHtmlId,
+  EuiTextTruncate,
 } from '@elastic/eui';
 import type { DataViewField } from '@kbn/data-views-plugin/public';
 import { i18n } from '@kbn/i18n';
@@ -88,7 +89,11 @@ export const FieldSelector: FC<Props> = ({
   WarningComponent,
 }) => {
   const fieldOptions = useMemo(
-    () => fields.map((field) => ({ inputDisplay: field.name, value: field })),
+    () =>
+      fields.map((field) => ({
+        inputDisplay: <EuiTextTruncate text={field.name} />,
+        value: field,
+      })),
     [fields]
   );
   const fieldId = useGeneratedHtmlId({ prefix: 'fieldSelector', suffix: 'select' });
