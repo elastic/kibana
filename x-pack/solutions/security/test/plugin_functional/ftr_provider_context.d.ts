b6e339b80fa4c95d70781d8fcab510892537e19d
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-22 18:06:42 +0200

    Update ftr_provider_context.d.ts

--- x-pack/solutions/security/test/plugin_functional/ftr_provider_context.d.ts
+++ x-pack/solutions/security/test/plugin_functional/ftr_provider_context.d.ts
@@ -5,19 +5,8 @@
  * 2.0.
  */
 
-<<<<<<<< HEAD:x-pack/platform/plugins/shared/streams/server/lib/streams/state_management/errors/failed_to_rollback_error.ts
-import { StatusError } from '../../errors/status_error';
-
-export class FailedToRollbackError extends StatusError {
-  constructor(message: string) {
-    super(message, 500);
-    this.name = 'FailedToRollbackError';
-  }
-}
-========
 import { GenericFtrProviderContext } from '@kbn/test';
 import { services } from './services';
 import { pageObjects } from './page_objects';
 
 export type FtrProviderContext = GenericFtrProviderContext<typeof services, typeof pageObjects>;
->>>>>>>> 16417b6d6c2 ([ska] relocate cypress & plugin functional tests (#228681)):x-pack/solutions/security/test/plugin_functional/ftr_provider_context.d.ts
