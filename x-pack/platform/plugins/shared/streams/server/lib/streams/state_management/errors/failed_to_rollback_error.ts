bdff4d45eb3031396f0689681b49af1bbbd63a60
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-22 18:04:54 +0200

    revert bad merge

--- x-pack/platform/plugins/shared/streams/server/lib/streams/state_management/errors/failed_to_rollback_error.ts
+++ x-pack/platform/plugins/shared/streams/server/lib/streams/state_management/errors/failed_to_rollback_error.ts
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-<<<<<<<< HEAD:x-pack/platform/plugins/shared/streams/server/lib/streams/state_management/errors/failed_to_rollback_error.ts
 import { StatusError } from '../../errors/status_error';
 
 export class FailedToRollbackError extends StatusError {
@@ -14,10 +13,3 @@ export class FailedToRollbackError extends StatusError {
     this.name = 'FailedToRollbackError';
   }
 }
-========
-import { GenericFtrProviderContext } from '@kbn/test';
-import { services } from './services';
-import { pageObjects } from './page_objects';
-
-export type FtrProviderContext = GenericFtrProviderContext<typeof services, typeof pageObjects>;
->>>>>>>> 16417b6d6c2 ([ska] relocate cypress & plugin functional tests (#228681)):x-pack/solutions/security/test/plugin_functional/ftr_provider_context.d.ts
