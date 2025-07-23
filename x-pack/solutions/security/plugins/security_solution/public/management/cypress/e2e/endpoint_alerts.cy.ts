3f5da8526d43ca45c947f399bd98c1644c871252
Paul Tavares <paul.tavares@elastic.co>
2025-07-23 12:44:58 -0400

    Refactor: simplify test setup

--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/endpoint_alerts.cy.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/endpoint_alerts.cy.ts
@@ -5,19 +5,17 @@
  * 2.0.
  */
 
+import { getHostVmClient } from '../../../../scripts/endpoint/common/vm_services';
 import { deleteAllLoadedEndpointData } from '../tasks/delete_all_endpoint_data';
 import { getAlertsTableRows, navigateToAlertsList } from '../screens/alerts';
 import { waitForEndpointAlerts } from '../tasks/alerts';
-import { request } from '../tasks/common';
 import { createAgentPolicyTask, getEndpointIntegrationVersion } from '../tasks/fleet';
 import { createEndpointHost } from '../tasks/create_endpoint_host';
 import type { IndexedFleetEndpointPolicyResponse } from '../../../../common/endpoint/data_loaders/index_fleet_endpoint_policy';
 import { enableAllPolicyProtections } from '../tasks/endpoint_policy';
-import type { PolicyData, ResponseActionApiResponse } from '../../../../common/endpoint/types';
+import type { PolicyData } from '../../../../common/endpoint/types';
 import type { CreateAndEnrollEndpointHostResponse } from '../../../../scripts/endpoint/common/endpoint_host_services';
 import { login, ROLE } from '../tasks/login';
-import { EXECUTE_ROUTE } from '../../../../common/endpoint/constants';
-import { waitForActionToComplete } from '../tasks/response_actions';
 
 describe('Endpoint generated alerts', { tags: ['@ess', '@serverless'] }, () => {
   let indexedPolicy: IndexedFleetEndpointPolicyResponse;
@@ -27,7 +25,7 @@ describe('Endpoint generated alerts', { tags: ['@ess', '@serverless'] }, () => {
   beforeEach(() => {
     login(ROLE.soc_manager);
     getEndpointIntegrationVersion().then((version) => {
-      createAgentPolicyTask(version, 'alerts test').then((data) => {
+      return createAgentPolicyTask(version, 'alerts test').then((data) => {
         indexedPolicy = data;
         policy = indexedPolicy.integrationPolicies[0];
 
@@ -61,19 +59,15 @@ describe('Endpoint generated alerts', { tags: ['@ess', '@serverless'] }, () => {
       .toString(16)
       .substring(2)}`;
 
-    // Send `execute` command that triggers malicious behaviour using the `execute` response action
-    request<ResponseActionApiResponse>({
-      method: 'POST',
-      url: EXECUTE_ROUTE,
-      body: {
-        endpoint_ids: [createdHost.agentId],
-        parameters: {
-          command: executeMaliciousCommand,
-        },
-      },
-    })
-      .then((response) => waitForActionToComplete(response.body.data.id))
-      .then(() => {
+    // Execute shell command that trigger Endpoint to send alert to Kibana
+    cy.wrap(getHostVmClient(createdHost.hostname).exec(executeMaliciousCommand))
+      .then((execResponse) => {
+        cy.log(
+          `Command [${executeMaliciousCommand}] was executed on host VM [${
+            createdHost.hostname
+          }]: ${JSON.stringify(execResponse)}`
+        );
+
         return waitForEndpointAlerts(createdHost.agentId, [
           {
             term: { 'process.group_leader.args': executeMaliciousCommand },
@@ -85,6 +79,7 @@ describe('Endpoint generated alerts', { tags: ['@ess', '@serverless'] }, () => {
           `query=(language:kuery,query:'agent.id: "${createdHost.agentId}" ')`
         );
       });
+
     getAlertsTableRows().should('have.length.greaterThan', 0);
   });
 });
