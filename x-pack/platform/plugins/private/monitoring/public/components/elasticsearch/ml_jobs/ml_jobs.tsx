e50c16aef622f43eb6e32cd12b5037b3976b2e0f
Jenny <dzheni.pavlova@elastic.co>
2025-07-23 19:08:50 +0200

    Lowercase 'jobs'

--- x-pack/platform/plugins/private/monitoring/public/components/elasticsearch/ml_jobs/ml_jobs.tsx
+++ x-pack/platform/plugins/private/monitoring/public/components/elasticsearch/ml_jobs/ml_jobs.tsx
@@ -68,7 +68,7 @@ export const ElasticsearchMLJobs = ({
               'xpack.monitoring.elasticsearch.mlJobListing.noJobsDescription',
               {
                 defaultMessage:
-                  'There are no Machine learning Jobs that match your query. Try changing the time range selection.',
+                  'There are no Machine learning jobs that match your query. Try changing the time range selection.',
               }
             )}
             search={{
