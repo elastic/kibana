344c2d586ca1a1f36f8982bf5876167b1add47dc
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-07-24 12:41:50 +0200

    relocate api_integration to test_serverless

--- x-pack/test/api_integration/deployment_agnostic/services/index.ts
+++ x-pack/test/api_integration/deployment_agnostic/services/index.ts
@@ -7,7 +7,6 @@
 
 import { services as platformDeploymentAgnosticServices } from '@kbn/test-suites-xpack-platform/api_integration_deployment_agnostic/services';
 import { SupertestWithRoleScope } from '@kbn/test-suites-xpack-platform/api_integration_deployment_agnostic/services/role_scoped_supertest';
-import { AlertingApiProvider } from './alerting_api';
 
 export type {
   InternalRequestHeader,
@@ -18,7 +17,6 @@ export type {
 export const services = {
   ...platformDeploymentAgnosticServices,
   // these services are left for compatibility with existing tests in x-pack/test and should not be used in new tests
-  alertingApi: AlertingApiProvider,
 };
 
 export type SupertestWithRoleScopeType = SupertestWithRoleScope;
--- x-pack/test_serverless/shared/services/deployment_agnostic_services.ts
+++ x-pack/test_serverless/shared/services/deployment_agnostic_services.ts
@@ -6,10 +6,10 @@
  */
 
 import { services as apiIntegrationServices } from '@kbn/test-suites-xpack/api_integration/services';
-import { services as apiIntegrationDeploymentAgnosticServices } from '@kbn/test-suites-xpack/api_integration/deployment_agnostic/services';
 import { commonFunctionalServices } from '@kbn/ftr-common-functional-services';
 import { SecuritySolutionApiProvider } from '@kbn/test-suites-xpack/api_integration/services/security_solution_api.gen';
 import { services as platformApiIntegrationServices } from '@kbn/test-suites-xpack-platform/api_integration/services';
+import { AlertingApiProvider } from './alerting_api';
 
 // pick only services that work for any FTR config, e.g. 'samlAuth' requires SAML setup in config file
 const {
@@ -44,5 +44,5 @@ export const services = {
   usageAPI: platformApiIntegrationServices.usageAPI,
   console,
   securitySolutionApi: SecuritySolutionApiProvider,
-  alertingApi: apiIntegrationDeploymentAgnosticServices.alertingApi,
+  alertingApi: AlertingApiProvider,
 };
