e8fbe6bbcba07a62874d59b52b819a46f77e9166
Ihor Panasiuk <igorskynet13@gmail.com>
2025-07-24 17:30:32 +0200

    Remove unused route definitions and simplify plugin setup method

--- examples/workflows/server/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import type { CoreSetup } from '@kbn/core/server';
-import { WorkflowsExamplePluginStartDeps } from '../types';
-
-export function defineRoutes(
-  core: CoreSetup<WorkflowsExamplePluginStartDeps, WorkflowsExamplePluginStartDeps>
-) {
-  const router = core.http.createRouter();
-}
