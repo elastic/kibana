e2abcc5777598b4ee548cea3f9141e0b36f1a94f
Nikita Indik <nikita.indik@elastic.co>
2025-07-24 17:40:33 +0200

    Allow logging

--- x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/rule_import_export/trial_license_complete_tier/configs/ess.config.ts
+++ x-pack/test/security_solution_api_integration/test_suites/detections_response/rules_management/rule_import_export/trial_license_complete_tier/configs/ess.config.ts
@@ -19,5 +19,26 @@ export default async function ({ readConfigFile }: FtrConfigProviderContext) {
       reportName:
         'Rules Management - Rule Import And Export Integration Tests - ESS Env - Trial License',
     },
+    kbnTestServer: {
+      ...functionalConfig.get('kbnTestServer'),
+      serverArgs: [
+        ...functionalConfig.get('kbnTestServer.serverArgs'),
+        `--logging.loggers=${JSON.stringify([
+          {
+            name: 'plugins.securitySolution',
+            level: 'debug',
+          },
+          {
+            name: 'plugins.fleet',
+            level: 'debug',
+          },
+        ])}`,
+      ],
+    },
+    mochaReporter: {
+      ...functionalConfig.get('mochaReporter'),
+      captureLogOutput: true,
+      sendToCiStats: true,
+    },
   };
 }
