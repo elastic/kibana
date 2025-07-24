fd827519df86f653dd913c35832f0dc12101eb6c
Bailey Cash <bailey.cash@elastic.co>
2025-07-24 15:30:03 -0400

    Update update_default_alert.ts

--- x-pack/solutions/observability/plugins/synthetics/server/routes/default_alerts/update_default_alert.ts
+++ x-pack/solutions/observability/plugins/synthetics/server/routes/default_alerts/update_default_alert.ts
@@ -8,15 +8,13 @@
 import { getSyntheticsDynamicSettings } from '../../saved_objects/synthetics_settings';
 import { DefaultAlertService } from './default_alert_service';
 import { SyntheticsRestApiRouteFactory } from '../types';
-import { ALERTING_SAVE, SYNTHETICS_API_URLS } from '../../../common/constants';
+import { SYNTHETICS_API_URLS } from '../../../common/constants';
 import { DEFAULT_ALERT_RESPONSE } from '../../../common/types/default_alerts';
 
 export const updateDefaultAlertingRoute: SyntheticsRestApiRouteFactory = () => ({
   method: 'PUT',
   path: SYNTHETICS_API_URLS.ENABLE_DEFAULT_ALERTING,
   validate: {},
-  requiredPrivileges: [ALERTING_SAVE],
-  writeAccess: false, // setting to false to cancel automatic requirement of uptime write privilege
   handler: async ({ context, server, savedObjectsClient }): Promise<DEFAULT_ALERT_RESPONSE> => {
     const defaultAlertService = new DefaultAlertService(context, server, savedObjectsClient);
     const { defaultTLSRuleEnabled, defaultStatusRuleEnabled } = await getSyntheticsDynamicSettings(
