368fb1e2021c90269dd781ea38ff582ed1afeaaf
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2025-07-25 13:00:47 -0400

    Disallow destructive actions

--- x-pack/platform/plugins/shared/observability_ai_assistant/server/functions/elasticsearch.ts
+++ x-pack/platform/plugins/shared/observability_ai_assistant/server/functions/elasticsearch.ts
@@ -13,11 +13,23 @@ export function registerElasticsearchFunction({
   functions,
   resources,
 }: FunctionRegistrationParameters) {
+  functions.registerInstruction(({ availableFunctionNames }) => {
+    if (availableFunctionNames.includes(ELASTICSEARCH_FUNCTION_NAME)) {
+      return `You can use the ${ELASTICSEARCH_FUNCTION_NAME} tool to call Elasticsearch APIs on behalf of the user.
+      You are only allowed to perform GET requests (Some exampples for GET requests are: Retrieving cluster information, cluster license, cluster health, indices stats, index stats, etc.) and GET/POST requests for the \`/_search\` endpoint (for search operations).
+      If the user asks to perform destructive actions or actions that are not allowed (e.g. PUT, PATCH, DELETE requests or POST requests that are not to the \`/_search\` endpoint), **NEVER** attempt to call the ${ELASTICSEARCH_FUNCTION_NAME} tool.
+      Instead, inform the user that you do not have the capability to perform those actions.
+      If you attempt to call the ${ELASTICSEARCH_FUNCTION_NAME} tool with disallowed methods (PUT, DELETE, PATCH, POST requests that are not to the \`/_search\` endpoint), it will fail.
+      For POST \`/_search\` operations, if there a request body is needed, make sure the request body is valid object.`;
+    }
+    return '';
+  });
+
   functions.registerFunction(
     {
       name: ELASTICSEARCH_FUNCTION_NAME,
       description:
-        'Call Elasticsearch APIs on behalf of the user. Make sure the request body is valid for the API that you are using. Only call this function when the user has explicitly requested it.',
+        'Call Elasticsearch APIs on behalf of the user. Make sure the request body is valid for the API that you are using. Only call this function when the user has explicitly requested it. Only GET requests and requests for /_search (GET and POST) are allowed',
       descriptionForUser: 'Call Elasticsearch APIs on behalf of the user',
       parameters: {
         type: 'object',
@@ -25,7 +37,7 @@ export function registerElasticsearchFunction({
           method: {
             type: 'string',
             description: 'The HTTP method of the Elasticsearch endpoint',
-            enum: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'] as const,
+            enum: ['GET', 'POST'] as const,
           },
           path: {
             type: 'string',
@@ -40,6 +52,14 @@ export function registerElasticsearchFunction({
       },
     },
     async ({ arguments: { method, path, body } }) => {
+      // Allowlist: (1) all GET requests, (2) _search requests via GET or POST.
+      const isSearchEndpoint = /(^|\/)[^/]*_search(\?|$)/.test(path);
+      if (method !== 'GET' && !(method === 'POST' && isSearchEndpoint)) {
+        throw new Error(
+          'Only GET requests or POST requests to the "_search" endpoint are permitted through this assistant function.'
+        );
+      }
+
       const esClient = (await resources.context.core).elasticsearch.client;
       const response = await esClient.asCurrentUser.transport.request({
         method,
