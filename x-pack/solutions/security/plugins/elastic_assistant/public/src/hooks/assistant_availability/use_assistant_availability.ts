ccbe608d2b1dc3e3352e29d885ccf562369d9405
Angela Chuang <yi-chun.chuang@elastic.co>
2025-07-25 21:25:48 +0100

    unit tests

--- x-pack/solutions/security/plugins/elastic_assistant/public/src/hooks/assistant_availability/use_assistant_availability.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/public/src/hooks/assistant_availability/use_assistant_availability.ts
@@ -26,7 +26,8 @@ export const useAssistantAvailability = (): UseAssistantAvailability => {
   const hasManageGlobalKnowledgeBase =
     capabilities[ASSISTANT_FEATURE_ID]?.manageGlobalKnowledgeBaseAIAssistant === true;
   const hasSearchAILakeConfigurations = capabilities[SECURITY_FEATURE_ID]?.configurations === true;
-  const hasManageAssistantPrivilege = capabilities.management.kibana.aiAssistantManagementSelection;
+  const hasManageAssistantPrivilege =
+    capabilities?.management?.kibana?.aiAssistantManagementSelection === true;
 
   // Connectors & Actions capabilities as defined in x-pack/plugins/actions/server/feature.ts
   // `READ` ui capabilities defined as: { ui: ['show', 'execute'] }
