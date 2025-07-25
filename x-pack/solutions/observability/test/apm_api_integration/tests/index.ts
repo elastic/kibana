e9824ecf7e236be1d52dbf357015667a57391543
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2025-07-25 04:04:57 +0200

    skip failing test suite (#228130)

--- x-pack/solutions/observability/test/apm_api_integration/tests/index.ts
+++ x-pack/solutions/observability/test/apm_api_integration/tests/index.ts
@@ -32,6 +32,7 @@ export default function apmApiIntegrationTests({ getService, loadTestFile }: Ftr
   // Failing: See https://github.com/elastic/kibana/issues/176948
   // Failing: See https://github.com/elastic/kibana/issues/228131
   // Failing: See https://github.com/elastic/kibana/issues/228130
+  // Failing: See https://github.com/elastic/kibana/issues/228130
   describe.skip('APM API tests', function () {
     const filePattern = getGlobPattern();
     const tests = globby.sync(filePattern, { cwd });
