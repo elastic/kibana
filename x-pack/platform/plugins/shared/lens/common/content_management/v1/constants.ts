dd1d576a175970a6f8b488bf399cfe102c1fe141
Nick Partridge <nicholas.partridge@elastic.co>
2025-07-26 16:02:30 -0500

    move and add constants

--- x-pack/platform/plugins/shared/lens/common/content_management/v1/constants.ts
+++ x-pack/platform/plugins/shared/lens/common/content_management/v1/constants.ts
@@ -5,7 +5,4 @@
  * 2.0.
  */
 
-export const PUBLIC_API_VERSION = '1';
-export const PUBLIC_API_CONTENT_MANAGEMENT_VERSION = 1;
-export const PUBLIC_API_PATH = '/api/lens';
-export const PUBLIC_API_ACCESS = 'internal';
+export const LENS_ITEM_VERSION = 1 as const;
