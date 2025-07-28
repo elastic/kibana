9f70c39633ff1aaba7d6642044304730e04e73f6
Anton Dosov <anton.dosov@elastic.co>
2025-07-28 17:43:29 +0200

    post merge fix

--- src/core/packages/http/rate-limiter-browser-internal/jest.config.js
+++ src/core/packages/http/rate-limiter-browser-internal/jest.config.js
@@ -7,18 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-<<<<<<<< HEAD:src/core/packages/chrome/navigation/src/utils/get_has_submenu.ts
-import { MenuItem } from '../../types';
-
-/**
- * Utility function for checking whether the menu item has a submenu
- */
-export const getHasSubmenu = (item: MenuItem): boolean => {
-  return !!item.sections && item.sections.length > 0;
-========
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
   roots: ['<rootDir>/src/core/packages/http/rate-limiter-browser-internal'],
->>>>>>>> 839cb471382ba41c40b8f66a6177648bef635ad1:src/core/packages/http/rate-limiter-browser-internal/jest.config.js
 };
