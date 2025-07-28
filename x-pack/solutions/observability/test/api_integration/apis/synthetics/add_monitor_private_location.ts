fb3629ae80a8c742ad3af133472d4e8b5faf9ee9
Shahzad <shahzad31comp@gmail.com>
2025-07-28 13:44:27 +0200

    update tests

--- x-pack/solutions/observability/test/api_integration/apis/synthetics/add_monitor_private_location.ts
+++ x-pack/solutions/observability/test/api_integration/apis/synthetics/add_monitor_private_location.ts
@@ -27,7 +27,7 @@ import { SyntheticsMonitorTestService } from './services/synthetics_monitor_test
 
 export default function ({ getService }: FtrProviderContext) {
   // Failing: See https://github.com/elastic/kibana/issues/229365
-  describe.skip('PrivateLocationAddMonitor', function () {
+  describe('PrivateLocationAddMonitor', function () {
     this.tags('skipCloud');
     const kibanaServer = getService('kibanaServer');
     const supertestAPI = getService('supertest');
