50e46a02610136c8bd351c30271773762406f523
Sergi Romeu <sergi.romeu@elastic.co>
2025-07-28 09:13:37 +0200

    fix tests

--- x-pack/solutions/observability/test/api_integration/profiling/tests/functions.spec.ts
+++ x-pack/solutions/observability/test/api_integration/profiling/tests/functions.spec.ts
@@ -48,9 +48,9 @@ export default function featureControlsTests({ getService }: FtrProviderContext)
 
       it(`returns correct result`, async () => {
         expect(functions.TopN.length).to.equal(7047);
-        expect(functions.TotalCount).to.equal(80555);
-        expect(functions.selfCPU).to.equal(3534);
-        expect(functions.totalCPU).to.equal(80555);
+        expect(functions.TotalCount).to.equal(81049);
+        expect(functions.selfCPU).to.equal(3599);
+        expect(functions.totalCPU).to.equal(81049);
         expectSnapshot(functions).toMatch();
       });
     });
