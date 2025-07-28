4091241d29f84df47df38e3335e5fa9676e1f7dd
Christos Nasikas <christos.nasikas@elastic.co>
2025-07-28 14:00:17 +0300

    Change rule execution

--- x-pack/platform/test/rule_registry/security_and_spaces/tests/basic/get_alert_fields_by_rule_type_ids.ts
+++ x-pack/platform/test/rule_registry/security_and_spaces/tests/basic/get_alert_fields_by_rule_type_ids.ts
@@ -7,7 +7,6 @@
 
 import expect from 'expect';
 import { OBSERVABILITY_RULE_TYPE_IDS } from '@kbn/rule-data-utils';
-import type { SearchHit } from '@elastic/elasticsearch/lib/api/types';
 import type { Alert } from '@kbn/alerts-as-data-utils';
 import type { FieldDescriptor } from '@kbn/data-views-plugin/server';
 import {
@@ -19,6 +18,7 @@ import {
   obsOnlySpacesAll,
   obsOnlyReadSpacesAll,
 } from '../../../common/lib/authentication/users';
+import { getEventLog } from '../../../../alerting_api_integration/common/lib';
 import type { User } from '../../../common/lib/authentication/types';
 import type { FtrProviderContext } from '../../../common/ftr_provider_context';
 import { getSpaceUrlPrefix } from '../../../common/lib/authentication/spaces';
@@ -32,173 +32,8 @@ export default ({ getService }: FtrProviderContext) => {
   const esArchiver = getService('esArchiver');
   const retry = getService('retry');
   const es = getService('es');
+
   const TEST_URL = '/internal/rac/alerts/fields';
-
-  const createEsQueryRule = async (spaceId: string = 'default') => {
-    const name = 'stack-rule';
-    const { body: createdRule } = await supertest
-      .post(`${getSpaceUrlPrefix(spaceId)}/api/alerting/rule`)
-      .set('kbn-xsrf', 'foo')
-      .send({
-        name,
-        schedule: {
-          interval: '5s',
-        },
-        consumer: 'stackAlerts',
-        rule_type_id: '.es-query',
-        actions: [],
-        tags: [name],
-        params: {
-          searchType: 'esQuery',
-          timeWindowSize: 5,
-          timeWindowUnit: 'd',
-          threshold: [0],
-          thresholdComparator: '>',
-          size: 100,
-          esQuery: '{\n    "query":{\n      "match_all" : {}\n    }\n  }',
-          aggType: 'count',
-          groupBy: 'all',
-          termSize: 5,
-          excludeHitsFromPreviousRun: false,
-          sourceFields: [],
-          index: ['kibana_sample_data_logs'],
-          timeField: '@timestamp',
-        },
-      })
-      .expect(200);
-    return createdRule;
-  };
-
-  const createSecurityRule = async (index: string, spaceId: string = 'default') => {
-    const { body: createdRule } = await supertest
-      .post(`${getSpaceUrlPrefix(spaceId)}/api/detection_engine/rules`)
-      .set('kbn-xsrf', 'foo')
-      .send({
-        type: 'query',
-        filters: [],
-        language: 'kuery',
-        query: '_id: *',
-        required_fields: [],
-        data_view_id: index,
-        author: [],
-        false_positives: [],
-        references: [],
-        risk_score: 21,
-        risk_score_mapping: [],
-        severity: 'low',
-        severity_mapping: [],
-        threat: [],
-        max_signals: 100,
-        name: 'security-rule',
-        description: 'security-rule',
-        tags: ['security-rule'],
-        setup: '',
-        license: '',
-        interval: '4s',
-        from: 'now-10m',
-        to: 'now',
-        actions: [],
-        enabled: true,
-        meta: {
-          kibana_siem_app_url: 'http://localhost:5620/app/security',
-        },
-      })
-      .expect(200);
-    return createdRule;
-  };
-
-  async function waitForAlertDocs(
-    index: string,
-    ruleId: string,
-    count: number = 1
-  ): Promise<Array<SearchHit<AlertDoc>>> {
-    return await retry.try(async () => {
-      const searchResult = await es.search<AlertDoc>({
-        index,
-        size: count,
-        query: {
-          bool: {
-            must: [{ term: { 'kibana.alert.rule.uuid': ruleId } }],
-          },
-        },
-      });
-
-      const docs = searchResult.hits.hits as Array<SearchHit<AlertDoc>>;
-      if (docs.length < count) throw new Error(`only ${docs.length} out of ${count} docs found`);
-
-      return docs;
-    });
-  }
-
-  const getAlertFieldsByFeatureId = async (
-    user: User,
-    ruleTypeIds: string[],
-    spaceId: string = 'default',
-    expectedStatusCode: number = 200
-  ) => {
-    const resp = await supertestWithoutAuth
-      .get(`${getSpaceUrlPrefix(spaceId)}${TEST_URL}`)
-      .query({ rule_type_ids: ruleTypeIds })
-      .auth(user.username, user.password)
-      .set('kbn-xsrf', 'true')
-      .expect(expectedStatusCode);
-
-    return resp.body;
-  };
-
-  const getSampleWebLogsDataView = async () => {
-    const { body } = await supertest
-      .post(`/api/content_management/rpc/search`)
-      .set('kbn-xsrf', 'foo')
-      .send({
-        contentTypeId: 'index-pattern',
-        query: { limit: 10 },
-        version: 1,
-      })
-      .expect(200);
-    return body.result.result.hits.find(
-      (dataView: { attributes: { title: string } }) =>
-        dataView.attributes.title === 'kibana_sample_data_logs'
-    );
-  };
-
-  const verifyFields = ({
-    fields,
-    allowedPrefixes,
-    notAllowedPrefixes = [],
-  }: {
-    fields: FieldDescriptor[];
-    allowedPrefixes: string[];
-    notAllowedPrefixes: string[];
-  }) => {
-    // Check at least one of each prefix exists
-    allowedPrefixes.forEach((prefix) => {
-      expect(
-        fields.some((field) => {
-          return field.name.startsWith(prefix);
-        })
-      ).toBeTruthy();
-    });
-
-    notAllowedPrefixes.forEach((prefix) => {
-      // Check no fields with these prefixes exist
-      expect(
-        fields.some((field) => {
-          return field.name.startsWith(prefix);
-        })
-      ).toBeFalsy();
-    });
-  };
-
-  const verifyBaseFields = (fields: FieldDescriptor[], requiredFieldNames: string[] = []) => {
-    const defaultRequiredFieldNames = ['@timestamp', '_id', '_index'];
-    const allRequiredFieldNames =
-      requiredFieldNames?.length > 0 ? requiredFieldNames : defaultRequiredFieldNames;
-
-    allRequiredFieldNames.forEach((fieldName) => {
-      expect(fields.some((field) => field.name === fieldName)).toBeTruthy();
-    });
-  };
 
   describe('Alert - Get alert fields by rule type IDs', () => {
     const ruleTypeIds = [
@@ -211,28 +46,18 @@ export default ({ getService }: FtrProviderContext) => {
     let securityRuleId: string;
 
     before(async () => {
-      await supertest
-        .post(`/api/sample_data/logs`)
-        .set('kbn-xsrf', 'true')
-        .set('x-elastic-internal-origin', 'foo')
-        .expect(200);
+      await installKibanaSampleData();
 
-      await retry.try(async () => {
-        const dataView = await getSampleWebLogsDataView();
+      const [securityRule, stackRule] = await Promise.all([
+        createSecurityRule(),
+        createEsQueryRule(),
+      ]);
 
-        if (dataView?.id) {
-          const [securityRule, stackRule] = await Promise.all([
-            createSecurityRule(dataView.id),
-            createEsQueryRule(),
-          ]);
+      securityRuleId = securityRule.id;
+      stackRuleId = stackRule.id;
 
-          securityRuleId = securityRule.id;
-          stackRuleId = stackRule.id;
-        }
-      });
-
-      await waitForAlertDocs('*.alerts-security.*', securityRuleId, 1);
-      await waitForAlertDocs('*.alerts-stack.*', stackRuleId, 1);
+      await waitForRuleToExecute(stackRuleId);
+      await waitForRuleToExecute(securityRuleId);
     });
 
     after(async () => {
@@ -240,6 +65,7 @@ export default ({ getService }: FtrProviderContext) => {
         .delete(`/api/alerting/rule/${stackRuleId}`)
         .set('kbn-xsrf', 'foo')
         .expect(204);
+
       await supertest
         .post(`/api/detection_engine/rules/_bulk_action?dry_run=false`)
         .set('kbn-xsrf', 'foo')
@@ -248,12 +74,12 @@ export default ({ getService }: FtrProviderContext) => {
           ids: [securityRuleId],
         })
         .expect(200);
+
       await supertest
         .delete(`/api/sample_data/logs`)
         .set('kbn-xsrf', 'true')
         .set('x-elastic-internal-origin', 'foo')
         .expect(204);
-      await esArchiver.unload('x-pack/test/functional/es_archives/rule_registry/alerts');
     });
 
     describe('Users:', () => {
@@ -414,4 +240,136 @@ export default ({ getService }: FtrProviderContext) => {
       });
     });
   });
+
+  const installKibanaSampleData = async () => {
+    await supertest
+      .post(`/api/sample_data/logs`)
+      .set('kbn-xsrf', 'true')
+      .set('x-elastic-internal-origin', 'Kibana')
+      .expect(200);
+  };
+
+  const createEsQueryRule = async () => {
+    const name = 'stack-rule';
+    const { body: createdRule } = await supertest
+      .post('/api/alerting/rule')
+      .set('kbn-xsrf', 'foo')
+      .send({
+        name,
+        schedule: {
+          interval: '5s',
+        },
+        consumer: 'stackAlerts',
+        rule_type_id: '.es-query',
+        actions: [],
+        tags: [name],
+        params: {
+          searchType: 'esQuery',
+          timeWindowSize: 5,
+          timeWindowUnit: 'd',
+          threshold: [0],
+          thresholdComparator: '>',
+          size: 100,
+          esQuery: '{\n    "query":{\n      "match_all" : {}\n    }\n  }',
+          aggType: 'count',
+          groupBy: 'all',
+          termSize: 5,
+          excludeHitsFromPreviousRun: false,
+          sourceFields: [],
+          index: ['kibana_sample_data_logs'],
+          timeField: '@timestamp',
+        },
+      })
+      .expect(200);
+    return createdRule;
+  };
+
+  const createSecurityRule = async () => {
+    const { body: createdRule } = await supertest
+      .post('/api/detection_engine/rules')
+      .set('kbn-xsrf', 'foo')
+      .send({
+        name: `spammy_rule`,
+        description: 'Spammy query rule',
+        enabled: true,
+        risk_score: 1,
+        rule_id: 'rule-1',
+        severity: 'low',
+        type: 'query',
+        query: '_id: *',
+        index: ['kibana_sample_data_logs'],
+        from: 'now-1y',
+        interval: '1m',
+      })
+      .expect(200);
+
+    return createdRule;
+  };
+
+  const waitForRuleToExecute = async (ruleId: string, index: string) => {
+    await retry.try(async () => {
+      return await getEventLog({
+        getService,
+        spaceId: 'default',
+        type: 'alert',
+        id: ruleId,
+        provider: 'alerting',
+        actions: new Map([['active-instance', { gte: 1 }]]),
+      });
+    });
+  };
+
+  const getAlertFieldsByFeatureId = async (
+    user: User,
+    ruleTypeIds: string[],
+    spaceId: string = 'default',
+    expectedStatusCode: number = 200
+  ) => {
+    const resp = await supertestWithoutAuth
+      .get(`${getSpaceUrlPrefix(spaceId)}${TEST_URL}`)
+      .query({ rule_type_ids: ruleTypeIds })
+      .auth(user.username, user.password)
+      .set('kbn-xsrf', 'true')
+      .expect(expectedStatusCode);
+
+    return resp.body;
+  };
+
+  const verifyFields = ({
+    fields,
+    allowedPrefixes,
+    notAllowedPrefixes = [],
+  }: {
+    fields: FieldDescriptor[];
+    allowedPrefixes: string[];
+    notAllowedPrefixes: string[];
+  }) => {
+    // Check at least one of each prefix exists
+    allowedPrefixes.forEach((prefix) => {
+      expect(
+        fields.some((field) => {
+          return field.name.startsWith(prefix);
+        })
+      ).toBeTruthy();
+    });
+
+    notAllowedPrefixes.forEach((prefix) => {
+      // Check no fields with these prefixes exist
+      expect(
+        fields.some((field) => {
+          return field.name.startsWith(prefix);
+        })
+      ).toBeFalsy();
+    });
+  };
+
+  const verifyBaseFields = (fields: FieldDescriptor[], requiredFieldNames: string[] = []) => {
+    const defaultRequiredFieldNames = ['@timestamp', '_id', '_index'];
+    const allRequiredFieldNames =
+      requiredFieldNames?.length > 0 ? requiredFieldNames : defaultRequiredFieldNames;
+
+    allRequiredFieldNames.forEach((fieldName) => {
+      expect(fields.some((field) => field.name === fieldName)).toBeTruthy();
+    });
+  };
 };
