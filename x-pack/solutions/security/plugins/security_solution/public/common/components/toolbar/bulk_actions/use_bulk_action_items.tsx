7c30af222701757fc64ca710922aa724930295f4
Nicholas Peretti <nicholas.peretti@elastic.co>
2025-07-28 17:17:29 +0100

    Add missing translations

--- x-pack/solutions/security/plugins/security_solution/public/common/components/toolbar/bulk_actions/use_bulk_action_items.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/common/components/toolbar/bulk_actions/use_bulk_action_items.tsx
@@ -207,9 +207,12 @@ export const useBulkActionItems = ({
           id: ALERT_CLOSING_REASON_PANEL_ID,
           title: 'Choose one option to mark closed as',
           items: [
-            { name: 'None', onClick: () => onClickUpdate(FILTER_CLOSED as AlertWorkflowStatus) },
             {
-              name: 'False positive',
+              name: i18n.BULK_ACTION_CLOSE_SELECTED_AS_NONE,
+              onClick: () => onClickUpdate(FILTER_CLOSED as AlertWorkflowStatus),
+            },
+            {
+              name: i18n.BULK_ACTION_CLOSE_SELECTED_AS_FALSE_POSITIVE,
               onClick: () =>
                 onClickUpdate(
                   FILTER_CLOSED as AlertWorkflowStatus,
@@ -217,7 +220,7 @@ export const useBulkActionItems = ({
                 ),
             },
             {
-              name: 'Duplicate',
+              name: i18n.BULK_ACTION_CLOSE_SELECTED_AS_DUPLICATE,
               onClick: () =>
                 onClickUpdate(
                   FILTER_CLOSED as AlertWorkflowStatus,
@@ -225,7 +228,7 @@ export const useBulkActionItems = ({
                 ),
             },
             {
-              name: 'Investigation required',
+              name: i18n.BULK_ACTION_CLOSE_SELECTED_AS_INVESTIGATION_REQUIRED,
               onClick: () =>
                 onClickUpdate(
                   FILTER_CLOSED as AlertWorkflowStatus,
