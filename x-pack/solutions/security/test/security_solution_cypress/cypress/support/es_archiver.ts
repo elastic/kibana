3b204db6628b980ba811fb2fe5a25690fbacf8e4
dkirchan <diamantis.kirchantzoglou@elastic.co>
2025-07-29 23:16:54 +0300

    Changed the path of the es archiver

--- x-pack/solutions/security/test/security_solution_cypress/cypress/support/es_archiver.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/support/es_archiver.ts
@@ -77,7 +77,7 @@ export const esArchiver = (
     log,
     client,
     kbnClient,
-    baseDir: '../../../../../test/functional/es_archives/security_solution',
+    baseDir: '../../../../test/functional/es_archives/security_solution',
   });
 
   on('task', {
