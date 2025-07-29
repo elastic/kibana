b23744a70b7b80ab92f8289ae3235b088ade9f0b
Ashokaditya <ashokaditya@elastic.co>
2025-07-29 17:54:41 +0200

    Update search_ai_lake tier specific roles
    refs https://github.com/elastic/security-team/issues/12377

--- packages/kbn-mock-idp-plugin/server/plugin.ts
+++ packages/kbn-mock-idp-plugin/server/plugin.ts
@@ -42,10 +42,17 @@ const projectToAlias = new Map<string, string>([
   // requires update of config/serverless.chat.yml (currently uses projectType 'search')
 ]);
 
-const readServerlessRoles = (projectType: string) => {
+const readServerlessRoles = (
+  projectType: string,
+  productTier: 'complete' | 'logs_essentials' | 'essentials' | 'search_ai_lake'
+) => {
   if (projectToAlias.has(projectType)) {
     const alias = projectToAlias.get(projectType)!;
-    const rolesResourcePath = resolve(SERVERLESS_ROLES_ROOT_PATH, alias, 'roles.yml');
+    const rolesResourcePath = resolve(
+      SERVERLESS_ROLES_ROOT_PATH,
+      alias,
+      productTier === 'search_ai_lake' ? 'search_ai_lake.roles.yml' : 'roles.yml'
+    );
     return readRolesFromResource(rolesResourcePath);
   } else {
     throw new Error(`Unsupported projectType: ${projectType}`);
@@ -93,7 +100,11 @@ export const plugin: PluginInitializer<
         try {
           if (roles.length === 0) {
             const projectType = plugins.cloud?.serverless?.projectType;
-            roles.push(...(projectType ? readServerlessRoles(projectType) : readStatefulRoles()));
+            const productTier = plugins.cloud?.serverless?.productTier;
+            const shouldLoadServerlessRoles = projectType && productTier;
+            roles.push(
+              ...(shouldLoadServerlessRoles ? readServerlessRoles(projectType, productTier) : readStatefulRoles())
+            );
           }
           return response.ok({
             body: {
