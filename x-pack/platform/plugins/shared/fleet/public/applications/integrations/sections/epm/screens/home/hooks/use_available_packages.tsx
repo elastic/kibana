elastic/kibana

commit a3fc388b6600e086241a285ce18cbe2671766ee7

[Fleet] exclude metrics data streams (#227842)
## Summary

Closes https://github.com/elastic/ingest-dev/issues/5789

Hiding excluded metrics data streams and packages for Logs Essentials
projects.

To verify:
- Add to `kibana.dev.yml`: `xpack.fleet.internal.excludeDataStreamTypes:
['metrics']`
- Go to Kibana Integrations, check that packages with metrics only data
streams are not showing up e.g. Apache Spark
- Go to Add Nginx integration, check that metrics data streams are not
showing up on the form
- On the Config tab, check that metrics inputs are not showing up

<img width="1097" height="1008" alt="image"
src="https://github.com/user-attachments/assets/fc441adc-4652-419c-be87-544dae7d5590"
/>

Callout in Integrations Overview e.g. Nginx

<img width="1101" height="1015" alt="image"
src="https://github.com/user-attachments/assets/f01090fc-9415-476d-a25e-f11eb937d97b"
/>

<img width="1093" height="1244" alt="image"
src="https://github.com/user-attachments/assets/a965bd83-868e-4ce8-b628-9e29e219b2e5"
/>

<img width="1102" height="1100" alt="image"
src="https://github.com/user-attachments/assets/793e1810-9a0e-40f4-b82a-965e1455a390"
/>


### Checklist

- [x] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/platform/plugins/shared/fleet/public/applications/integrations/sections/epm/screens/home/hooks/use_available_packages.tsx
@@ -105,11 +105,23 @@ const packageListToIntegrationsList = (packages: PackageList): PackageList => {
         })
       : [];
 
-    const tiles = filterPolicyTemplatesTiles<PackageListItem>(
+    let tiles = filterPolicyTemplatesTiles<PackageListItem>(
       pkg.policy_templates_behavior,
       topPackage,
       integrationsPolicyTemplates
     );
+
+    // remove tiles that only have data streams of excluded types e.g. aws.ebs, but keep partial matches e.g. aws.cloudfront_logs
+    tiles = tiles.filter((tile) => {
+      return (
+        !tile.integration ||
+        !tile.data_streams ||
+        tile.data_streams.some((dataStream) =>
+          dataStream.dataset.includes(`${tile.name}.${tile.integration}`)
+        )
+      );
+    });
+
     return [...acc, ...tiles];
   }, []);
 };

config/serverless.oblt.logs_essentials.yml
@@ -47,6 +47,7 @@ xpack.fleet.internal.registry.excludePackages: [
   'profiler_agent',
   'synthetics_dashboards',
 ]
+xpack.fleet.internal.excludeDataStreamTypes: ['metrics']
 
 xpack.actions.enabledActionTypes: [
   '.email',

src/platform/test/plugin_functional/test_suites/core_plugins/rendering.ts
@@ -268,6 +268,7 @@ export default function ({ getService }: PluginFunctionalProviderContext) {
         'xpack.fleet.agentless.customIntegrations.enabled (boolean?)',
         'xpack.fleet.enableExperimental (array?)',
         'xpack.fleet.internal.activeAgentsSoftLimit (number?)',
+        'xpack.fleet.internal.excludeDataStreamTypes (array?)',
         'xpack.fleet.internal.fleetServerStandalone (boolean?)',
         'xpack.fleet.internal.onlyAllowAgentUpgradeToKnownVersions (boolean?)',
         'xpack.fleet.developer.maxAgentPoliciesWithInactivityTimeout (number?)',

x-pack/platform/plugins/shared/fleet/common/types/index.ts
@@ -91,6 +91,7 @@ export interface FleetConfigType {
       excludePackages: string[];
       searchAiLakePackageAllowlistEnabled?: boolean;
     };
+    excludeDataStreamTypes?: string[];
   };
   createArtifactsBulkBatchSize?: number;
   autoUpgrades?: {

x-pack/platform/plugins/shared/fleet/public/applications/integrations/sections/epm/screens/detail/overview/overview.tsx
@@ -6,6 +6,7 @@
  */
 import React, { memo, useMemo, useEffect, useState, useCallback, useRef } from 'react';
 import type { MouseEventHandler } from 'react';
+import { isEqual } from 'lodash';
 
 import styled from 'styled-components';
 import {
@@ -108,6 +109,23 @@ const UnverifiedCallout: React.FC = () => {
           />
         </p>
       </EuiCallOut>
+      <EuiSpacer size="l" />
+    </>
+  );
+};
+
+const LogsEssentialsCallout: React.FC = () => {
+  return (
+    <>
+      <EuiCallOut
+        data-test-subj="logsEssentialsCallout"
+        title={i18n.translate('xpack.fleet.epm.logsEssentialsCalloutTitle', {
+          defaultMessage:
+            'As this is a Logs Essentials project, these integrations will only install and configure for logs collection, even if the description mentions metrics.',
+        })}
+        iconType="info"
+        color="primary"
+      />
       <EuiSpacer size="l" />
     </>
   );
@@ -291,6 +309,10 @@ export const OverviewPage: React.FC<Props> = memo(
     const requireAgentRootPrivileges = isRootPrivilegesRequired(packageInfo);
     const hideDashboards = config?.hideDashboards;
 
+    const showLogsEssentialsCallout = isEqual(config?.internal?.excludeDataStreamTypes, [
+      'metrics',
+    ]);
+
     return (
       <EuiFlexGroup alignItems="flexStart" data-test-subj="epm.OverviewPage">
         <SideBar grow={2}>
@@ -305,6 +327,7 @@ export const OverviewPage: React.FC<Props> = memo(
         </SideBar>
         <EuiFlexItem grow={9} className="eui-textBreakWord">
           {isUnverified && <UnverifiedCallout />}
+          {showLogsEssentialsCallout && <LogsEssentialsCallout />}
 
           <BidirectionalIntegrationsBanner integrationPackageName={packageInfo.name} />
           <CloudPostureThirdPartySupportCallout packageInfo={packageInfo} />

x-pack/platform/plugins/shared/fleet/server/config.ts
@@ -50,6 +50,7 @@ export const config: PluginConfigDescriptor = {
       fleetServerStandalone: true,
       activeAgentsSoftLimit: true,
       onlyAllowAgentUpgradeToKnownVersions: true,
+      excludeDataStreamTypes: true,
     },
     integrationsHomeOverride: true,
     prereleaseEnabledByDefault: true,
@@ -292,6 +293,9 @@ export const config: PluginConfigDescriptor = {
             },
           }
         ),
+        excludeDataStreamTypes: schema.arrayOf(schema.string(), {
+          defaultValue: () => [],
+        }),
       }),
       enabled: schema.boolean({ defaultValue: true }),
       /**

x-pack/platform/plugins/shared/fleet/server/services/epm/packages/get.test.ts
@@ -613,6 +613,76 @@ owner: elastic`,
       });
       expect(packages.find((item) => item.id === 'nginx')).toBeUndefined();
     });
+
+    it('should filter packages containing only data streams in xpack.fleet.internal.excludeDataStreamTypes', async () => {
+      MockRegistry.fetchList.mockResolvedValue([
+        {
+          name: 'nginx',
+          version: '1.0.0',
+          title: 'Nginx',
+          data_streams: [
+            {
+              dataset: 'nginx.access',
+              type: 'logs',
+              namespace: 'default',
+            },
+            {
+              dataset: 'nginx.stubstatus',
+              type: 'metrics',
+              namespace: 'default',
+            },
+          ],
+        } as any,
+        {
+          id: 'fleet_server',
+          name: 'fleet_server',
+          version: '1.0.0',
+          title: 'Fleet Server',
+        } as any,
+        {
+          name: 'apache_spark',
+          version: '1.0.0',
+          title: 'Apache Spark',
+          data_streams: [
+            {
+              dataset: 'apache_spark.metrics',
+              type: 'metrics',
+              namespace: 'default',
+            },
+          ],
+        } as any,
+      ]);
+      const mockContract = createAppContextStartContractMock({
+        internal: {
+          excludeDataStreamTypes: ['metrics'],
+        },
+      } as any);
+      appContextService.start(mockContract);
+
+      const soClient = savedObjectsClientMock.create();
+      soClient.find.mockResolvedValue({
+        saved_objects: [],
+      } as any);
+      const packages = await getPackages({
+        savedObjectsClient: soClient,
+      });
+      expect(packages.find((item) => item.id === 'apache_spark')).toBeUndefined();
+      expect(packages.find((item) => item.id === 'nginx')).toEqual({
+        data_streams: [
+          {
+            dataset: 'nginx.access',
+            namespace: 'default',
+            type: 'logs',
+          },
+        ],
+        id: 'nginx',
+        name: 'nginx',
+        status: 'not_installed',
+        title: 'Nginx',
+        version: '1.0.0',
+      });
+      expect(packages.find((item) => item.id === 'fleet_server')).toBeDefined();
+    });
   });
 
   describe('getInstalledPackages', () => {
@@ -969,6 +1039,138 @@ owner: elastic`,
       });
 
       expect(MockRegistry.getPackage).not.toHaveBeenCalled();
+    });
+
+    it('should remove excluded data stream types and policy templates', async () => {
+      const mockContract = createAppContextStartContractMock({
+        internal: {
+          excludeDataStreamTypes: ['metrics'],
+        },
+      } as any);
+      appContextService.start(mockContract);
+
+      const soClient = savedObjectsClientMock.create();
+      soClient.get.mockRejectedValue(SavedObjectsErrorHelpers.createGenericNotFoundError());
+      MockRegistry.fetchFindLatestPackageOrUndefined.mockResolvedValue({
+        name: 'nginx',
+        version: '1.0.0',
+      } as RegistryPackage);
+      const packageInfo = {
+        name: 'nginx',
+        version: '1.0.0',
+        assets: [],
+        data_streams: [
+          {
+            dataset: 'nginx.access',
+            type: 'logs',
+            namespace: 'default',
+          },
+          {
+            dataset: 'nginx.stubstatus',
+            type: 'metrics',
+            namespace: 'default',
+          },
+        ],
+        policy_templates: [
+          {
+            name: 'nginx',
+            inputs: [
+              {
+                type: 'nginx/metrics',
+              },
+              {
+                type: 'logfile',
+              },
+            ],
+          },
+        ],
+      } as unknown as RegistryPackage;
+      MockRegistry.fetchInfo.mockResolvedValue(packageInfo);
+      MockRegistry.getPackage.mockResolvedValue({
+        paths: [],
+        assetsMap: new Map(),
+        archiveIterator: createArchiveIteratorFromMap(new Map()),
+        packageInfo,
+      });
+
+      await expect(
+        getPackageInfo({
+          savedObjectsClient: soClient,
+          pkgName: 'nginx',
+          pkgVersion: '1.0.0',
+        })
+      ).resolves.toMatchObject({
+        latestVersion: '1.0.0',
+        status: 'not_installed',
+        data_streams: [
+          {
+            dataset: 'nginx.access',
+            type: 'logs',
+            namespace: 'default',
+          },
+        ],
+        policy_templates: [
+          {
+            name: 'nginx',
+            inputs: [
+              {
+                type: 'logfile',
+              },
+            ],
+          },
+        ],
+      });
+    });
+
+    it('should do nothing if no excluded data streams', async () => {
+      const mockContract = createAppContextStartContractMock({
+        internal: {
+          excludeDataStreamTypes: ['metrics'],
+        },
+      } as any);
+      appContextService.start(mockContract);
+
+      const soClient = savedObjectsClientMock.create();
+      soClient.get.mockRejectedValue(SavedObjectsErrorHelpers.createGenericNotFoundError());
+      MockRegistry.fetchFindLatestPackageOrUndefined.mockResolvedValue({
+        name: 'pkg',
+        version: '1.0.0',
+      } as RegistryPackage);
+      const packageInfo = {
+        name: 'pkg',
+        version: '1.0.0',
+        assets: [],
+        policy_templates: [
+          {
+            name: 'pkg',
+            inputs: [],
+          },
+        ],
+      } as unknown as RegistryPackage;
+      MockRegistry.fetchInfo.mockResolvedValue(packageInfo);
+      MockRegistry.getPackage.mockResolvedValue({
+        paths: [],
+        assetsMap: new Map(),
+        archiveIterator: createArchiveIteratorFromMap(new Map()),
+        packageInfo,
+      });
+
+      await expect(
+        getPackageInfo({
+          savedObjectsClient: soClient,
+          pkgName: 'pkg',
+          pkgVersion: '1.0.0',
+        })
+      ).resolves.toMatchObject({
+        latestVersion: '1.0.0',
+        status: 'not_installed',
+        policy_templates: [
+          {
+            name: 'pkg',
+            inputs: [],
+          },
+        ],
+      });
     });
 
     describe('installation status', () => {

x-pack/platform/plugins/shared/fleet/server/services/epm/packages/get.ts
@@ -39,6 +39,7 @@ import type {
   PackageSpecManifest,
   AssetsMap,
   PackagePolicyAssetsMap,
+  RegistryPolicyIntegrationTemplate,
 } from '../../../../common/types';
 import {
   PACKAGES_SAVED_OBJECT_TYPE,
@@ -71,6 +72,8 @@ import { auditLoggingService } from '../../audit_logging';
 import { getFilteredSearchPackages } from '../filtered_packages';
 import { filterAssetPathForParseAndVerifyArchive } from '../archive/parse';
 import { airGappedUtils } from '../airgapped';
+
+import type { RegistryPolicyTemplate } from '../../../../common/types/models/epm';
 
 import { createInstallableFrom } from '.';
 import {
@@ -161,7 +164,7 @@ export async function getPackages(
   ).filter((p): p is Installable<any> => p !== null);
 
   const filteredPackages = getFilteredSearchPackages();
-  const packageList = registryItems
+  let packageList = registryItems
     .map((item) =>
       createInstallableFrom(
         item,
@@ -181,6 +184,8 @@ export async function getPackages(
     });
   }
 
+  packageList = filterOutExcludedDataStreamTypes(packageList);
+
   if (!excludeInstallStatus) {
     return packageList;
   }
@@ -197,6 +202,33 @@ export async function getPackages(
   });
 
   return packageListWithoutStatus as PackageList;
+}
+
+function filterOutExcludedDataStreamTypes(
+  packageList: Array<Installable<any>>
+): Array<Installable<any>> {
+  const excludeDataStreamTypes =
+    appContextService.getConfig()?.internal?.excludeDataStreamTypes ?? [];
+  if (excludeDataStreamTypes.length > 0) {
+    // filter out packages where all data streams have excluded types e.g. metrics
+    return packageList.reduce((acc, pkg) => {
+      const shouldInclude =
+        (pkg.data_streams || [])?.length === 0 ||
+        pkg.data_streams?.some((dataStream: any) => {
+          return !excludeDataStreamTypes.includes(dataStream.type);
+        });
+      if (shouldInclude) {
+        // filter out excluded data stream types
+        const filteredDataStreams =
+          pkg.data_streams?.filter(
+            (dataStream: any) => !excludeDataStreamTypes.includes(dataStream.type)
+          ) ?? [];
+        acc.push({ ...pkg, data_streams: filteredDataStreams });
+      }
+      return acc;
+    }, []);
+  }
+  return packageList;
 }
 
 interface GetInstalledPackagesOptions {
@@ -538,12 +570,56 @@ export async function getPackageInfo({
     licensePath: Registry.getLicensePath(paths || []),
     keepPoliciesUpToDate: savedObject?.attributes.keep_policies_up_to_date ?? false,
   };
-  const updated = { ...packageInfo, ...additions };
+
+  const { filteredDataStreams, filteredPolicyTemplates } =
+    getFilteredDataStreamsAndPolicyTemplates(packageInfo);
+
+  const updated = {
+    ...packageInfo,
+    ...additions,
+    data_streams: filteredDataStreams,
+    policy_templates: filteredPolicyTemplates,
+  };
 
   const installable = createInstallableFrom(updated, savedObject);
   setPackageInfoCache(pkgName, pkgVersion, installable);
 
   return installable;
+}
+
+function getFilteredDataStreamsAndPolicyTemplates(packageInfo: ArchivePackage | RegistryPackage) {
+  const excludeDataStreamTypes =
+    appContextService.getConfig()?.internal?.excludeDataStreamTypes ?? [];
+  let filteredDataStreams = packageInfo.data_streams;
+  let filteredPolicyTemplates = packageInfo.policy_templates;
+
+  if (excludeDataStreamTypes.length > 0) {
+    filteredDataStreams = packageInfo.data_streams?.filter(
+      (dataStream) => !excludeDataStreamTypes.includes(dataStream.type)
+    );
+    // filter out matching types e.g. nginx/metrics
+    filteredPolicyTemplates = packageInfo.policy_templates?.reduce(
+      (acc: RegistryPolicyTemplate[], policyTemplate: RegistryPolicyTemplate) => {
+        const policyTemplateIntegrationTemplate =
+          policyTemplate as RegistryPolicyIntegrationTemplate;
+        if (policyTemplateIntegrationTemplate.inputs) {
+          const filteredInputs = policyTemplateIntegrationTemplate.inputs.filter((input: any) => {
+            const shouldInclude = !excludeDataStreamTypes.some((excludedType) =>
+              input.type.includes(excludedType)
+            );
+            return shouldInclude;
+          });
+          acc.push({ ...policyTemplate, inputs: filteredInputs ?? [] });
+        } else {
+          acc.push(policyTemplate);
+        }
+        return acc;
+      },
+      []
+    );
+  }
+
+  return { filteredDataStreams, filteredPolicyTemplates };
 }
 
 export const getPackageUsageStats = async ({

commit 6ee7273074e148c5cb7b0718f2aa66ecc58a588c

[Fleet] Fix incorrectly removing integration tiles (#229748)
## Summary

Fix bug introduced in https://github.com/elastic/kibana/pull/227842

Removed logic to filter out child integration tiles as there is no
reliable way to say if a tile has no data streams.
E.g. for CSPM, data stream naming doesn't follow the `name.integration`
pattern like aws.

This means we can't remove those child tiles that only have metrics data
streams.

Before:
<img width="1437" height="1053" alt="image"
src="https://github.com/user-attachments/assets/5e69c9e4-60a5-4eb5-9318-12905635871c"
/>

After:
<img width="1438" height="1073" alt="image"
src="https://github.com/user-attachments/assets/bbe285ec-d358-4eef-9ccb-f2e6b62b4021"
/>

```
{
  "name": "cloud_security_posture",
  "integration": "cspm",
  "data_streams": [
    {
      "type": "logs",
      "dataset": "cloud_security_posture.findings",
      "title": "Cloud Security Posture Findings"
    },
    {
      "type": "logs",
      "dataset": "cloud_security_posture.vulnerabilities",
      "title": "Cloud Vulnerabilities"
    }
  ]
}
```

x-pack/platform/plugins/shared/fleet/public/applications/integrations/sections/epm/screens/home/hooks/use_available_packages.tsx
@@ -106,22 +106,11 @@ const packageListToIntegrationsList = (packages: PackageList): PackageList => {
         })
       : [];
 
-    let tiles = filterPolicyTemplatesTiles<PackageListItem>(
+    const tiles = filterPolicyTemplatesTiles<PackageListItem>(
       pkg.policy_templates_behavior,
       topPackage,
       integrationsPolicyTemplates
     );
-
-    // remove tiles that only have data streams of excluded types e.g. aws.ebs, but keep partial matches e.g. aws.cloudfront_logs
-    tiles = tiles.filter((tile) => {
-      return (
-        !tile.integration ||
-        !tile.data_streams ||
-        tile.data_streams.some((dataStream) =>
-          dataStream.dataset.includes(`${tile.name}.${tile.integration}`)
-        )
-      );
-    });
 
     return [...acc, ...tiles];
   }, []);