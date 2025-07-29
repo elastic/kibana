56d9bae622b1652da10fb615b450ded6d3bb0de9
Anton Dosov <anton.dosov@elastic.co>
2025-07-29 11:41:02 +0200

    improve

--- src/platform/packages/shared/kbn-ftr-common-functional-ui-services/services/web_element_wrapper/scroll_into_view_if_necessary.ts
+++ src/platform/packages/shared/kbn-ftr-common-functional-ui-services/services/web_element_wrapper/scroll_into_view_if_necessary.ts
@@ -1,63 +1,43 @@
-/* eslint-disable @kbn/eslint/require-license-header */
-
-/* @notice
- * Based on the scroll-into-view-if-necessary module from npm
- * https://github.com/stipsan/compute-scroll-into-view/blob/master/src/index.ts#L269-L340
- *
- * MIT License
- *
- * Copyright (c) 2018 Cody Olsen
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
 export function scrollIntoViewIfNecessary(
   target: HTMLElement,
+  scrollContainerId?: string,
   scrollFixedHeader: number = 0,
   scrollFixedFooter: number = 0
 ) {
-  // Get bounding rectangles
-  const scroller =
-    document.getElementById('app-main-scroll') ||
-    document.scrollingElement ||
-    document.documentElement;
-
-  // Check if the target is in the scroller
-  if (!scroller.contains(target)) return;
+  const scrollContainer =
+    (scrollContainerId && document.getElementById(scrollContainerId)) || document.documentElement;
 
   // Measure helper, logic is different for scrolling the viewport vs a scroller element
   const getRects = () => {
     const targetRect = target.getBoundingClientRect();
-    const scrollIsViewport = scroller === document.documentElement || scroller === document.body;
+    const scrollIsViewport =
+      scrollContainer === document.documentElement || scrollContainer === document.body;
     const scrollReact = scrollIsViewport
       ? { top: 0, left: 0, right: window.innerWidth, bottom: window.innerHeight }
-      : scroller.getBoundingClientRect();
+      : scrollContainer.getBoundingClientRect();
     return { targetRect, scrollReact };
   };
 
   const isVisible = ({ targetRect, scrollReact }: ReturnType<typeof getRects>) =>
     targetRect.top >= scrollReact.top + scrollFixedHeader &&
-    targetRect.bottom <= scrollReact.bottom - scrollFixedFooter;
+    targetRect.bottom <= scrollReact.bottom - scrollFixedFooter &&
+    targetRect.left >= scrollReact.left &&
+    targetRect.right <= scrollReact.right;
 
   let { targetRect, scrollReact } = getRects();
 
-  if (isVisible({ targetRect, scrollReact })) return;
+  if (isVisible({ targetRect, scrollReact })) {
+    return;
+  }
 
   // First try native scrollIntoView on the correct container
   target.scrollIntoView();
@@ -69,7 +49,7 @@ export function scrollIntoViewIfNecessary(
     // Now adjust for fixed headers
     const deltaTop = targetRect.top - (scrollReact.top + scrollFixedHeader);
     if (deltaTop < 0) {
-      scroller.scrollBy({ top: deltaTop });
+      scrollContainer.scrollBy({ top: deltaTop });
     }
   }
 
@@ -80,7 +60,7 @@ export function scrollIntoViewIfNecessary(
     // Adjust for fixed footers
     const deltaBottom = targetRect.bottom - (scrollReact.bottom - scrollFixedFooter);
     if (deltaBottom > 0) {
-      scroller.scrollBy({ top: deltaBottom });
+      scrollContainer.scrollBy({ top: deltaBottom });
     }
   }
 }
