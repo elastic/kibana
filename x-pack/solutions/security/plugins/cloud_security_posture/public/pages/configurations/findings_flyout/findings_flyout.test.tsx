78e6efd8b5d65602db92898a6eca327be06c08ed
orouz <or.ouziel@elastic.co>
2025-07-30 10:18:46 +0300

    [Cloud Security] use resource.raw as evidence in findings flyout for cis_gcp (#227263)
    (cherry picked from commit dd31c3012d004f14b865642945345cd71a9c27b7)
    
    # Conflicts:
    #	x-pack/solutions/security/plugins/cloud_security_posture/public/pages/configurations/findings_flyout/overview_tab.tsx

--- x-pack/solutions/security/plugins/cloud_security_posture/public/pages/configurations/findings_flyout/findings_flyout.test.tsx
+++ x-pack/solutions/security/plugins/cloud_security_posture/public/pages/configurations/findings_flyout/findings_flyout.test.tsx
@@ -76,6 +76,68 @@ describe('<FindingsFlyout/>', () => {
       const missingInfoCallout = queryByText('Some fields not provided by Wiz');
       expect(missingInfoCallout).toBeNull();
     });
+
+    it('does not display evidence field when result.evidence and resource.raw are missing', () => {
+      (useMisconfigurationFinding as jest.Mock).mockReturnValue({
+        data: {
+          result: {
+            hits: [
+              {
+                _source: {
+                  ...mockFindingsHit,
+                  result: { ...mockFindingsHit.result, evidence: undefined },
+                  rule: {
+                    ...mockFindingsHit.rule,
+                    // resource.raw is only shown for CIS GCP rules
+                    benchmark: { ...mockFindingsHit.rule.benchmark, id: 'cis_gcp' },
+                  },
+                  resource: {
+                    ...mockFindingsHit.resource,
+                    raw: undefined,
+                  },
+                },
+              },
+            ],
+          },
+        },
+      });
+      const { queryByText } = render(<TestComponent />);
+      const missingEvidenceTitle = queryByText('Evidence');
+      expect(missingEvidenceTitle).toBeNull();
+    });
+
+    it('displays evidence field when it exists', () => {
+      (useMisconfigurationFinding as jest.Mock).mockReturnValue({
+        data: { result: { hits: [{ _source: mockFindingsHit }] } },
+      });
+      const { getByText } = render(<TestComponent />);
+      const evidenceTitle = getByText('Evidence');
+      expect(evidenceTitle).toBeInTheDocument();
+    });
+
+    it('displays evidence as resource.raw for CIS_GCP when evidence field does not exists', () => {
+      (useMisconfigurationFinding as jest.Mock).mockReturnValue({
+        data: {
+          result: {
+            hits: [
+              {
+                _source: {
+                  ...mockFindingsHit,
+                  result: { ...mockFindingsHit.result, evidence: undefined },
+                  rule: {
+                    ...mockFindingsHit.rule,
+                    benchmark: { ...mockFindingsHit.rule.benchmark, id: 'cis_gcp' },
+                  },
+                },
+              },
+            ],
+          },
+        },
+      });
+      const { getByText } = render(<TestComponent />);
+      const evidenceTitle = getByText('Evidence');
+      expect(evidenceTitle).toBeInTheDocument();
+    });
   });
 
   describe('Table Tab', () => {
