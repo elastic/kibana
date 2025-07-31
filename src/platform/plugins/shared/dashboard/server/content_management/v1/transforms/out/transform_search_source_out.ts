fdae60af521fac0597445cbc94e4cccc1e6edfc2
Nick Peihl <nick.peihl@elastic.co>
2025-07-31 15:57:03 -0400

    Log warning instead of throwing an error if search source transforms fail

--- src/platform/plugins/shared/dashboard/server/content_management/v1/transforms/out/transform_search_source_out.ts
+++ src/platform/plugins/shared/dashboard/server/content_management/v1/transforms/out/transform_search_source_out.ts
@@ -12,6 +12,7 @@ import { injectReferences, parseSearchSourceJSON } from '@kbn/data-plugin/common
 import { DashboardSavedObjectAttributes } from '../../../../dashboard_saved_object';
 import { DashboardAttributes } from '../../types';
 import { migrateLegacyQuery, cleanFiltersForSerialize } from '../../../../../common';
+import { logger } from '../../../../kibana_services';
 
 export function transformSearchSourceOut(
   kibanaSavedObjectMeta: DashboardSavedObjectAttributes['kibanaSavedObjectMeta'],
@@ -21,12 +22,19 @@ export function transformSearchSourceOut(
   if (!searchSourceJSON) {
     return {};
   }
-  const parsedSearchSource = parseSearchSourceJSON(searchSourceJSON);
-  const searchSource = injectReferences(parsedSearchSource, references);
-  const filters = cleanFiltersForSerialize(searchSource.filter);
-  const query = searchSource.query ? migrateLegacyQuery(searchSource.query) : undefined;
 
-  return {
-    searchSource: { filters, query },
-  };
+  try {
+    const parsedSearchSource = parseSearchSourceJSON(searchSourceJSON);
+    const searchSource = injectReferences(parsedSearchSource, references);
+    const filters = cleanFiltersForSerialize(searchSource.filter);
+    const query = searchSource.query ? migrateLegacyQuery(searchSource.query) : undefined;
+    return {
+      searchSource: { filters, query },
+    };
+  } catch (error) {
+    // If the searchSourceJSON is malformed or references are missing, we log a warning
+    // and return an empty object to avoid breaking the dashboard loading.
+    logger.warn(`Unable to transform filter and query state on read. Error: ${error.message}`);
+    return {};
+  }
 }
