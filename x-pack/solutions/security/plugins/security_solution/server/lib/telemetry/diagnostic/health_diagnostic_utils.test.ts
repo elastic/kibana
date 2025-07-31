04f53ddc737f4501d1e4b050c7b37464304dccdc
Sebastián Zaffarano <sebastian.zaffarano@elastic.co>
2025-07-31 16:38:14 +0200

    Update utils tests

--- x-pack/solutions/security/plugins/security_solution/server/lib/telemetry/diagnostic/health_diagnostic_utils.test.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/telemetry/diagnostic/health_diagnostic_utils.test.ts
@@ -5,7 +5,8 @@
  * 2.0.
  */
 
-import { applyFilterlist, fieldNames } from './health_diagnostic_utils';
+import { intervalFromDate } from '@kbn/task-manager-plugin/server/lib/intervals';
+import { shouldExecute, applyFilterlist, fieldNames } from './health_diagnostic_utils';
 import { Action } from './health_diagnostic_service.types';
 
 describe('Security Solution - Health Diagnostic Queries - utils', () => {
@@ -379,6 +380,37 @@ describe('Security Solution - Health Diagnostic Queries - utils', () => {
         'user.name',
         'user.roles[]',
       ]);
+    });
+  });
+
+  describe('nextExecution', () => {
+    test.each([
+      ['5m', '2025-05-14T17:00:00.000Z', '2025-05-14T17:03:00.000Z', false],
+      ['5m', '2025-05-14T17:00:00.000Z', '2025-05-14T17:06:00.000Z', true],
+      ['5m', '2025-05-14T17:00:00.000Z', '2025-05-14T18:03:00.000Z', true],
+      ['1h', '2025-05-14T17:00:00.000Z', '2025-05-14T18:00:00.000Z', false],
+      ['1h', '2025-05-14T17:00:00.000Z', '2025-05-14T18:01:00.000Z', true],
+      ['1h', '2025-05-14T17:00:00.000Z', '2025-05-15T01:00:00.000Z', true],
+      ['1h', '2025-05-14T17:00:00.000Z', '2025-05-12T01:00:00.000Z', false],
+      ['1s', '2025-05-02T17:00:00.000Z', '2025-06-30T17:00:00.000Z', true],
+      ['30d', '2025-05-31T17:00:00.000Z', '2025-06-29T17:00:00.000Z', false],
+      ['365d', '2025-01-31T17:00:00.000Z', '2025-12-31T17:00:00.000Z', false],
+    ])(
+      'should add %s to %s when endDate is %s and return %s',
+      (interval, startDate, endDate, expected) => {
+        expect(shouldExecute(new Date(startDate), new Date(endDate), interval)).toBe(expected);
+      }
+    );
+
+    test.each([
+      ['5m', '2025-05-14T17:00:00.000Z', '2025-05-14T17:05:00.000Z'],
+      ['1h', '2025-05-14T17:00:00.000Z', '2025-05-14T18:00:00.000Z'],
+      ['24h', '2025-05-14T17:00:00.000Z', '2025-05-15T17:00:00.000Z'],
+      ['30d', '2025-05-31T17:00:00.000Z', '2025-06-30T17:00:00.000Z'],
+      ['365d', '2025-05-31T17:00:00.000Z', '2026-05-31T17:00:00.000Z'],
+    ])('adding %s to %s should be equal to %s', (interval, dateFrom, expected) => {
+      const next = intervalFromDate(new Date(dateFrom), interval);
+      expect(next).toEqual(new Date(expected));
     });
   });
 });
