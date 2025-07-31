3dcd03d3a1d85468cbea59e1b6129df571c5dbb7
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-07-31 10:29:47 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- examples/embeddable_examples/public/react_embeddables/data_table/create_data_table_action.ts
+++ examples/embeddable_examples/public/react_embeddables/data_table/create_data_table_action.ts
@@ -10,9 +10,7 @@
 import { i18n } from '@kbn/i18n';
 import { apiIsPresentationContainer } from '@kbn/presentation-containers';
 import { EmbeddableApiContext } from '@kbn/presentation-publishing';
-import {
-  IncompatibleActionError,
-} from '@kbn/ui-actions-plugin/public';
+import { IncompatibleActionError } from '@kbn/ui-actions-plugin/public';
 import { embeddableExamplesGrouping } from '../embeddable_examples_grouping';
 import { ADD_DATA_TABLE_ACTION_ID, DATA_TABLE_ID } from './constants';
 
@@ -22,23 +20,23 @@ import { ADD_DATA_TABLE_ACTION_ID, DATA_TABLE_ID } from './constants';
 // -----------------------------------------------------------------------------
 
 export const createDataTableAction = {
-    id: ADD_DATA_TABLE_ACTION_ID,
-    grouping: [embeddableExamplesGrouping],
-    getIconType: () => 'tableDensityNormal',
-    isCompatible: async ({ embeddable }: EmbeddableApiContext) => {
-      return apiIsPresentationContainer(embeddable);
-    },
-    execute: async ({ embeddable }: EmbeddableApiContext) => {
-      if (!apiIsPresentationContainer(embeddable)) throw new IncompatibleActionError();
-      embeddable.addNewPanel(
-        {
-          panelType: DATA_TABLE_ID,
-        },
-        true
-      );
-    },
-    getDisplayName: () =>
-      i18n.translate('embeddableExamples.dataTable.ariaLabel', {
-        defaultMessage: 'Data table',
-      }),
-  }
+  id: ADD_DATA_TABLE_ACTION_ID,
+  grouping: [embeddableExamplesGrouping],
+  getIconType: () => 'tableDensityNormal',
+  isCompatible: async ({ embeddable }: EmbeddableApiContext) => {
+    return apiIsPresentationContainer(embeddable);
+  },
+  execute: async ({ embeddable }: EmbeddableApiContext) => {
+    if (!apiIsPresentationContainer(embeddable)) throw new IncompatibleActionError();
+    embeddable.addNewPanel(
+      {
+        panelType: DATA_TABLE_ID,
+      },
+      true
+    );
+  },
+  getDisplayName: () =>
+    i18n.translate('embeddableExamples.dataTable.ariaLabel', {
+      defaultMessage: 'Data table',
+    }),
+};
