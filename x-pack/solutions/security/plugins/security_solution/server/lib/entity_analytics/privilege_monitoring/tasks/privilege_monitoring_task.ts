51d37d8a60a7c4c0cf4387450025b5a87e538f6d
Tiago Vila Verde <tiago.vilaverde@elastic.co>
2025-08-01 16:34:13 +0200

    fix import

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/tasks/privilege_monitoring_task.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/tasks/privilege_monitoring_task.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { CoreStart, type Logger, type AnalyticsServiceSetup } from '@kbn/core/server';
+import type { CoreStart, Logger, AnalyticsServiceSetup } from '@kbn/core/server';
 import type {
   ConcreteTaskInstance,
   TaskManagerSetupContract,
