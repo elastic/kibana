01b2430e266e115a49f9a1c0fa98b2a9b8d5100e
Sebastian Delle Donne <sebastian.delledonne@elastic.co>
2025-08-01 09:52:12 +0200

    move refresh dataview after saving values

--- src/platform/packages/private/kbn-index-editor/src/index_update_service.ts
+++ src/platform/packages/private/kbn-index-editor/src/index_update_service.ts
@@ -197,19 +197,6 @@ export class IndexUpdateService {
   public readonly dataView$: Observable<DataView> = combineLatest([
     this._indexName$,
     this._indexCrated$,
-    // this.pendingColumnsToBeSaved$.pipe(
-    //   // Refetch the dataView to look for new field types when there are new columns saved
-    //   // (when pendingColumnsToBeSaved$ length decreases)
-    //   scan(
-    //     (acc, curr) => ({
-    //       prevLength: acc.currLength,
-    //       currLength: curr.length,
-    //     }),
-    //     { prevLength: 0, currLength: 0 }
-    //   ),
-    //   filter(({ prevLength, currLength }) => currLength < prevLength),
-    //   startWith({ prevLength: 0, currLength: 0 })
-    // ),
   ]).pipe(
     skipWhile(([indexName, indexCreated]) => {
       return !indexName;
@@ -225,10 +212,6 @@ export class IndexUpdateService {
     this.pendingColumnsToBeSaved$.pipe(startWith([])),
   ]).pipe(
     switchMap(async ([dataView, pendingColumnsToBeSaved]) => {
-      // Ensure the data view is refreshed to include any new fields
-      // this.data.dataViews.clearCache();
-      await this.data.dataViews.refreshFields(dataView);
-
       const unsavedFields = pendingColumnsToBeSaved
         .filter((column) => !dataView.fields.getByName(column.name))
         .map((column) => {
@@ -307,8 +290,13 @@ export class IndexUpdateService {
           switchMap((updates) => {
             return from(this.bulkUpdate(updates)).pipe(
               withLatestFrom(this._rows$, this.dataView$),
-              map(([response, rows, dataView]) => {
-                return { updates, response, rows, dataView };
+              switchMap(([response, rows, dataView]) => {
+                // Refresh the data view fields after the update, to get any new field type added
+                return from(this.data.dataViews.refreshFields(dataView)).pipe(
+                  map(() => {
+                    return { updates, response, rows, dataView };
+                  })
+                );
               })
             );
           })
