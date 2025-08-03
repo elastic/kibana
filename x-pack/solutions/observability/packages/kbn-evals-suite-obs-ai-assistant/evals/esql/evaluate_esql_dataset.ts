21ab8b8c6850f58d6feb928a6498498f3c191cc8
Dario Gieselaar <dario.gieselaar@elastic.co>
2025-08-03 10:00:03 +0200

    Run evaluations against ES|QL agent

--- x-pack/solutions/observability/packages/kbn-evals-suite-obs-ai-assistant/evals/esql/evaluate_esql_dataset.ts
+++ x-pack/solutions/observability/packages/kbn-evals-suite-obs-ai-assistant/evals/esql/evaluate_esql_dataset.ts
@@ -6,8 +6,12 @@
  */
 
 import type { Example } from '@arizeai/phoenix-client/dist/esm/types/datasets';
+import { executeAsEsqlAgent } from '@kbn/ai-tools';
+import type { ElasticsearchClient, Logger } from '@kbn/core/server';
 import type { DefaultEvaluators, KibanaPhoenixClient } from '@kbn/evals';
 import type { EvaluationDataset } from '@kbn/evals/src/types';
+import type { BoundInferenceClient } from '@kbn/inference-common';
+import { MessageRole } from '@kbn/inference-common';
 import type { ObservabilityAIAssistantEvaluationChatClient } from '../../src/chat_client';
 
 interface EsqlExample extends Example {
@@ -35,10 +39,18 @@ export function createEvaluateEsqlDataset({
   evaluators,
   phoenixClient,
   chatClient,
+  inferenceClient,
+  logger,
+  signal,
+  esClient,
 }: {
   evaluators: DefaultEvaluators;
   phoenixClient: KibanaPhoenixClient;
   chatClient: ObservabilityAIAssistantEvaluationChatClient;
+  signal: AbortSignal;
+  inferenceClient: BoundInferenceClient;
+  logger: Logger;
+  esClient: ElasticsearchClient;
 }): EvaluateEsqlDataset {
   return async function evaluateEsqlDataset({
     dataset: { name, description, examples },
@@ -59,14 +71,53 @@ export function createEvaluateEsqlDataset({
       {
         dataset,
         task: async ({ input }) => {
-          const response = await chatClient.complete({
-            messages: input.question,
+          const response = await executeAsEsqlAgent({
+            inferenceClient,
+            esClient,
+            logger,
+            prompt: input.question,
+            signal,
+            toolCallbacks: {},
+            tools: {},
+          });
+
+          const executedQueries: string[] = [];
+
+          response.input.forEach((message) => {
+            if (message.role === MessageRole.Tool && message.name === 'run_queries') {
+              executedQueries.push(
+                ...message.response.responses.map((res) =>
+                  typeof res.query === 'string' ? res.query : res.query.output
+                )
+              );
+            }
           });
 
           return {
-            errors: response.errors,
-            messages: response.messages,
+            content: response.content,
+            executed_queries: executedQueries,
           };
+          // const response = await chatClient.complete({
+          //   messages: [
+          //     {
+          //       role: 'user' as MessageRole,
+          //       content: `${input.question}. If you end up generating and/or executing a query, provide the full query in your final response. Don't ask for feedback or guidance, just provide a definitive answer and this is an unsupervised chat.`,
+          //     },
+          //   ],
+          // });
+
+          // const messages: Array<Message['message']> = [];
+
+          // if (response.messages[response.messages.length - 1].name?.includes('query')) {
+          //   messages.push(...response.messages.slice(-3));
+          // } else {
+          //   messages.push(...response.messages.slice(-1));
+          // }
+
+          // return {
+          //   messages,
+          //   errors: response.errors,
+          // };
         },
       },
       [
@@ -76,6 +127,7 @@ export function createEvaluateEsqlDataset({
           evaluate: async ({ input, output, expected, metadata }) => {
             const result = await evaluators
               .criteria([
+                ...(expected.criteria ?? []),
                 ...(expected.expected
                   ? [
                       `The generated ES|QL query should be syntactically valid and align with the intent of the user prompt. The query should be functionally equivalent
