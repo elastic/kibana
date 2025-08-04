ed82db7c54d2b359f53cc9b6a366384f5a0d0473
Steph Milovic <stephanie.milovic@elastic.co>
2025-08-04 10:09:24 -0600

    fix import

--- x-pack/platform/packages/shared/kbn-elastic-assistant/impl/assistant/settings/settings_context_menu/settings_context_menu.test.tsx
+++ x-pack/platform/packages/shared/kbn-elastic-assistant/impl/assistant/settings/settings_context_menu/settings_context_menu.test.tsx
@@ -7,7 +7,7 @@
 
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-
+import userEvent from '@testing-library/user-event';
 import {
   mockAssistantAvailability,
   TestProviders,
