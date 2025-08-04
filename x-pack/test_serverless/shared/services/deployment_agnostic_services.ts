561027d0c74955a977a306645ed3f40820c34e06
Maxim Palenov <maxim.palenov@elastic.co>
2025-08-04 16:21:35 +0200

    fix failed CI

--- x-pack/test_serverless/shared/services/deployment_agnostic_services.ts
+++ x-pack/test_serverless/shared/services/deployment_agnostic_services.ts
@@ -5,11 +5,17 @@
  * 2.0.
  */
 
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 import _ from 'lodash';
 
 import { services as apiIntegrationServices } from '@kbn/test-suites-xpack/api_integration/services';
-import { SecuritySolutionApiProvider as SecuritySolutionExceptionsApiProvider } from '@kbn/test-suites-xpack/api_integration/services/security_solution_exceptions_api.gen';
-
+import { services as apiIntegrationDeploymentAgnosticServices } from '@kbn/test-suites-xpack/api_integration/deployment_agnostic/services';
 /*
  * Some FTR services from api integration stateful tests are compatible with serverless environment
  * While adding a new one, make sure to verify that it works on both Kibana CI and MKI
@@ -31,11 +37,11 @@ const pickedServices = _.pick(apiIntegrationServices, [
   'usageAPI',
   'console',
   'securitySolutionApi',
+  'securitySolutionExceptionsApi',
 ]);
 
 export const services = {
   // deployment agnostic FTR services
   ...pickedServices,
-  alertingApi: AlertingApiProvider,
-  securitySolutionExceptionsApi: SecuritySolutionExceptionsApiProvider,
+  alertingApi: apiIntegrationDeploymentAgnosticServices.alertingApi,
 };
