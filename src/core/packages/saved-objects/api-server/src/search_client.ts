17b5cf5e7eb4d41e424f6e4f9b2d9a1cdc825ad3
Jean-Louis Leysens <jloleysens@gmail.com>
2025-08-04 15:35:40 +0200

    comment

--- src/core/packages/saved-objects/api-server/src/search_client.ts
+++ src/core/packages/saved-objects/api-server/src/search_client.ts
@@ -21,6 +21,24 @@ export interface SearchParams extends Omit<estypes.SearchRequest, 'index'> {
  * TODO(@jloleysens): Write more docs
  */
 export interface SearchClient {
+  /**
+   * Exposes a low-level client for using ES search functionality on saved objects.
+   *
+   * @example
+   * ```typescript
+   * const response = await savedObjectsClient.getSearchClient().search({
+   *  query: { ... },
+   *  runtime_mappings: { ... },
+   * });
+   * ```
+   *
+   * @remarks
+   * - Use this client for mult-type searche and aggergations that need more powerful ES search
+   *   capabilities than what SavedObjectsClient.find provides.
+   * - This client does not support providing an index to search against. That is inferred from the saved objects type
+   * - This client still adheres to saved object concepts like namespaces and type mappings
+   * - This client returns raw Elasticsearch documents, not saved objects
+   */
   search: <T extends SavedObjectsRawDoc = SavedObjectsRawDoc, A = unknown>(
     params: SearchParams,
     options?: TransportRequestOptionsWithOutMeta
