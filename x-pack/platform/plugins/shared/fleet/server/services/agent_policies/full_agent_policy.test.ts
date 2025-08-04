elastic/kibana

commit b8d52767b9d9009cf6a0fdc105f9a025297a5446

[Fleet] Fix ssl config overridden from advanced yaml in full agent policy (#219902)
## Summary

Issue reported by users, when ssl config is added in advanced yaml in
Logstash output, it is not there in full agent policy.

To verify:
- create Logstash output with Advanced YAML config: 
```
ssl:
  verification_mode: "none" 
```
- use output in agent policy, go to View policy
- check that the full policy yaml includes `verification_mode`

<img width="447" alt="image"
src="https://github.com/user-attachments/assets/4aeaa51d-64b3-4555-a8f6-44b83106c8ae"
/>


### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios

x-pack/platform/plugins/shared/fleet/server/services/agent_policies/full_agent_policy.ts
@@ -579,11 +579,14 @@ export function transformOutputToFullPolicyOutput(
     ...(!isShipperDisabled ? generalShipperData : {}),
     ...(ca_sha256 ? { ca_sha256 } : {}),
     ...(ca_trusted_fingerprint ? { 'ssl.ca_trusted_fingerprint': ca_trusted_fingerprint } : {}),
-    ...((output.type === outputType.Kafka || output.type === outputType.Logstash) && ssl
-      ? { ssl }
-      : {}),
     ...(secrets ? { secrets } : {}),
   };
+  if ((output.type === outputType.Kafka || output.type === outputType.Logstash) && ssl) {
+    newOutput.ssl = {
+      ...newOutput.ssl,
+      ...ssl,
+    };
+  }
 
   if (proxy) {
     newOutput.proxy_url = proxy.url;

x-pack/platform/plugins/shared/fleet/server/services/agent_policies/full_agent_policy.test.ts
@@ -1437,6 +1437,40 @@ ssl.test: 123
     `);
   });
 
+  it('should not override advanced yaml ssl fields for logstash output type', () => {
+    const policyOutput = transformOutputToFullPolicyOutput(
+      {
+        id: 'id123',
+        hosts: ['host.fr:3332'],
+        is_default: false,
+        is_default_monitoring: false,
+        name: 'test output',
+        type: 'logstash',
+        config_yaml: 'ssl:\n  verification_mode: "none" ',
+        ssl: {
+          certificate: '',
+          certificate_authorities: [],
+        },
+      },
+      undefined,
+      false
+    );
+
+    expect(policyOutput).toMatchInlineSnapshot(`
+      Object {
+        "hosts": Array [
+          "host.fr:3332",
+        ],
+        "ssl": Object {
+          "certificate": "",
+          "certificate_authorities": Array [],
+          "verification_mode": "none",
+        },
+        "type": "logstash",
+      }
+    `);
+  });
+
   it('should work with kafka output', () => {
     const policyOutput = transformOutputToFullPolicyOutput({
       id: 'id123',

commit 4ef6c5d08050b7e689b87d3dfd171941c5665cc5

[Fleet] fix ssl config in output preconfig (#230211)
## Summary

Fix bug introduced with https://github.com/elastic/kibana/pull/219902.
SSL config was not added to full agent policy from elasticsearch output
types

To verify:
- Add output preconfig to `kibana.yml`
```
xpack.fleet.outputs:
  - hosts:
    - https://localhost:9200
    id: eck-fleet-agent-output-elasticsearch
    name: eck-elasticsearch
    ssl:
      certificate_authorities:
      - /test/ca.crt
    type: elasticsearch
```
- Create agent policy and use preconfigured output
- Check full agent policy (View policy on UI) that ssl config is under
outputs

<img width="624" height="666" alt="image"
src="https://github.com/user-attachments/assets/a8342646-d2db-4791-a6f9-4b7a602f240f"
/>


### Checklist

Check the PR satisfies following conditions. 

Reviewers should verify this PR satisfies this list as well.

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] If a plugin configuration key changed, check if it needs to be
allowlisted in the cloud and added to the [docker
list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
- [ ] This was checked for breaking HTTP API changes, and any breaking
changes have been approved by the breaking-change committee. The
`release_note:breaking` label should be applied in these situations.
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was

x-pack/platform/plugins/shared/fleet/server/services/agent_policies/full_agent_policy.ts
@@ -606,7 +606,7 @@ export function transformOutputToFullPolicyOutput(
     ...(ca_trusted_fingerprint ? { 'ssl.ca_trusted_fingerprint': ca_trusted_fingerprint } : {}),
     ...(secrets ? { secrets } : {}),
   };
-  if ((output.type === outputType.Kafka || output.type === outputType.Logstash) && ssl) {
+  if (ssl) {
     newOutput.ssl = {
       ...newOutput.ssl,
       ...ssl,

x-pack/platform/plugins/shared/fleet/server/services/agent_policies/full_agent_policy.test.ts
@@ -1360,6 +1360,39 @@ ssl.test: 123
     `);
   });
 
+  it('should keep ssl fields for es output type', () => {
+    const policyOutput = transformOutputToFullPolicyOutput(
+      {
+        id: 'id123',
+        hosts: ['http://host.fr'],
+        is_default: false,
+        is_default_monitoring: false,
+        name: 'test output',
+        type: 'elasticsearch',
+        ssl: {
+          certificate: '',
+          certificate_authorities: [],
+        },
+      },
+      undefined,
+      false
+    );
+
+    expect(policyOutput).toMatchInlineSnapshot(`
+      Object {
+        "hosts": Array [
+          "http://host.fr",
+        ],
+        "preset": "balanced",
+        "ssl": Object {
+          "certificate": "",
+          "certificate_authorities": Array [],
+        },
+        "type": "elasticsearch",
+      }
+    `);
+  });
+
   it('should works with proxy', () => {
     const policyOutput = transformOutputToFullPolicyOutput(
       {