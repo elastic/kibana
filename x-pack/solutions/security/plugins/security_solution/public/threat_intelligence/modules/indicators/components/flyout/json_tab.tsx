c2bce153500d0fee9b5ff50952edae95c44d25d6
Nicholas Peretti <nicholas.peretti@elastic.co>
2025-08-05 14:57:05 +0100

    Update json view

--- x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/modules/indicators/components/flyout/json_tab.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/modules/indicators/components/flyout/json_tab.tsx
@@ -7,7 +7,8 @@
 
 import type { FC } from 'react';
 import React from 'react';
-import { EuiCodeBlock } from '@elastic/eui';
+import { Global, css } from '@emotion/react';
+import { JsonCodeEditor } from '@kbn/unified-doc-viewer-plugin/public';
 import type { Indicator } from '../../../../../../common/threat_intelligence/types/indicator';
 import { IndicatorEmptyPrompt } from './empty_prompt';
 import { CODE_BLOCK_TEST_ID } from './test_ids';
@@ -27,8 +28,26 @@ export const IndicatorsFlyoutJson: FC<IndicatorsFlyoutJsonProps> = ({ indicator 
   return Object.keys(indicator).length === 0 ? (
     <IndicatorEmptyPrompt />
   ) : (
-    <EuiCodeBlock language="json" lineNumbers data-test-subj={CODE_BLOCK_TEST_ID}>
-      {JSON.stringify(indicator, null, 2)}
-    </EuiCodeBlock>
+    <>
+      {/**
+       * Sets the height of the tab container (in flyout.tsx) to 100%
+       * to allow the json editor to be rendered at full height.
+       * Without this, the height of the editor would be 0 and it wouldn't
+       * be visible on the page
+       */}
+      <Global
+        styles={css`
+          .euiFlyoutBody__overflowContent {
+            height: 100%;
+          }
+        `}
+      />
+      <JsonCodeEditor
+        json={indicator as unknown as Record<string, unknown>}
+        hasLineNumbers={true}
+        data-test-subj={CODE_BLOCK_TEST_ID}
+        height={'100%'}
+      />
+    </>
   );
 };
