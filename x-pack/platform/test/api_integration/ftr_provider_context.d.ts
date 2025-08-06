6d28b3876241ced6e13f3649e296bff6134fad66
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-08-06 13:37:13 +0200

    fix FtrService

--- x-pack/platform/test/api_integration/ftr_provider_context.d.ts
+++ x-pack/platform/test/api_integration/ftr_provider_context.d.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { GenericFtrProviderContext } from '@kbn/test';
+import { GenericFtrProviderContext, GenericFtrService } from '@kbn/test';
 
 import { services } from './services';
 
