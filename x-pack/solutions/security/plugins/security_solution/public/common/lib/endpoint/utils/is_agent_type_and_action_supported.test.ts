39293baf0d90f82585058dbd2beb5c4dd20772cd
Paul Tavares <paul.tavares@elastic.co>
2025-08-06 14:59:35 -0400

    tests for `isAgentTypeAndActionSupported()`

--- x-pack/solutions/security/plugins/security_solution/public/common/lib/endpoint/utils/is_agent_type_and_action_supported.test.ts
+++ x-pack/solutions/security/plugins/security_solution/public/common/lib/endpoint/utils/is_agent_type_and_action_supported.test.ts
@@ -24,6 +24,7 @@ describe('isAgentTypeAndActionSupported() util', () => {
       responseActionsSentinelOneGetFileEnabled: true,
       responseActionsCrowdstrikeManualHostIsolationEnabled: true,
       responseActionsMSDefenderEndpointEnabled: true,
+      responseActionsSentinelOneRunScriptEnabled: true,
       ...overrides,
     });
   };
@@ -36,6 +37,9 @@ describe('isAgentTypeAndActionSupported() util', () => {
   };
   const disableMicrosoftIsolationFeature = () => {
     enableFeatures({ responseActionsMSDefenderEndpointEnabled: false });
+  };
+  const disableS1RunScript = () => {
+    enableFeatures({ responseActionsSentinelOneRunScriptEnabled: false });
   };
 
   const resetFeatures = (): void => {
@@ -53,21 +57,24 @@ describe('isAgentTypeAndActionSupported() util', () => {
   });
 
   it.each`
-    agentType                        | actionName    | actionType     | expectedValue | runSetup
-    ${'endpoint'}                    | ${undefined}  | ${undefined}   | ${true}       | ${undefined}
-    ${'endpoint'}                    | ${'isolate'}  | ${'manual'}    | ${true}       | ${undefined}
-    ${'endpoint'}                    | ${'isolate'}  | ${'automated'} | ${true}       | ${undefined}
-    ${'sentinel_one'}                | ${undefined}  | ${undefined}   | ${true}       | ${undefined}
-    ${'sentinel_one'}                | ${'isolate'}  | ${'manual'}    | ${true}       | ${undefined}
-    ${'sentinel_one'}                | ${'get-file'} | ${'manual'}    | ${true}       | ${undefined}
-    ${'sentinel_one'}                | ${'get-file'} | ${undefined}   | ${false}      | ${disableS1GetFileFeature}
-    ${'crowdstrike'}                 | ${undefined}  | ${undefined}   | ${true}       | ${undefined}
-    ${'crowdstrike'}                 | ${'isolate'}  | ${'manual'}    | ${true}       | ${undefined}
-    ${'crowdstrike'}                 | ${'isolate'}  | ${undefined}   | ${false}      | ${disableCSIsolateFeature}
-    ${'microsoft_defender_endpoint'} | ${undefined}  | ${undefined}   | ${true}       | ${undefined}
-    ${'microsoft_defender_endpoint'} | ${'isolate'}  | ${'manual'}    | ${true}       | ${undefined}
-    ${'microsoft_defender_endpoint'} | ${'isolate'}  | ${'automated'} | ${false}      | ${undefined}
-    ${'microsoft_defender_endpoint'} | ${'isolate'}  | ${undefined}   | ${false}      | ${disableMicrosoftIsolationFeature}
+    agentType                        | actionName     | actionType     | expectedValue | runSetup
+    ${'endpoint'}                    | ${undefined}   | ${undefined}   | ${true}       | ${undefined}
+    ${'endpoint'}                    | ${'isolate'}   | ${'manual'}    | ${true}       | ${undefined}
+    ${'endpoint'}                    | ${'isolate'}   | ${'automated'} | ${true}       | ${undefined}
+    ${'sentinel_one'}                | ${undefined}   | ${undefined}   | ${true}       | ${undefined}
+    ${'sentinel_one'}                | ${'isolate'}   | ${'manual'}    | ${true}       | ${undefined}
+    ${'sentinel_one'}                | ${'get-file'}  | ${'manual'}    | ${true}       | ${undefined}
+    ${'sentinel_one'}                | ${'get-file'}  | ${undefined}   | ${false}      | ${disableS1GetFileFeature}
+    ${'sentinel_one'}                | ${'runscript'} | ${'manual'}    | ${true}       | ${undefined}
+    ${'sentinel_one'}                | ${'runscript'} | ${'automated'} | ${false}      | ${undefined}
+    ${'sentinel_one'}                | ${'runscript'} | ${undefined}   | ${false}      | ${disableS1RunScript}
+    ${'crowdstrike'}                 | ${undefined}   | ${undefined}   | ${true}       | ${undefined}
+    ${'crowdstrike'}                 | ${'isolate'}   | ${'manual'}    | ${true}       | ${undefined}
+    ${'crowdstrike'}                 | ${'isolate'}   | ${undefined}   | ${false}      | ${disableCSIsolateFeature}
+    ${'microsoft_defender_endpoint'} | ${undefined}   | ${undefined}   | ${true}       | ${undefined}
+    ${'microsoft_defender_endpoint'} | ${'isolate'}   | ${'manual'}    | ${true}       | ${undefined}
+    ${'microsoft_defender_endpoint'} | ${'isolate'}   | ${'automated'} | ${false}      | ${undefined}
+    ${'microsoft_defender_endpoint'} | ${'isolate'}   | ${undefined}   | ${false}      | ${disableMicrosoftIsolationFeature}
   `(
     'should return `$expectedValue` for $agentType $actionName ($actionType)',
     ({
