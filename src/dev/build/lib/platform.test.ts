f29d5af4db28254884a6f0c539b9f8aee73153c1
Jonathan Budzenski <jon@elastic.co>
2025-08-07 14:21:41 -0500

    add tests

--- src/dev/build/lib/platform.test.ts
+++ src/dev/build/lib/platform.test.ts
@@ -55,3 +55,35 @@ describe('isMac()', () => {
     expect(new Platform('darwin', 'arm64', 'foo', null, null).isMac()).toBe(true);
   });
 });
+
+describe('isServerless()', () => {
+  it('returns true if serverless', () => {
+    expect(new Platform('linux', 'x64', 'test', 'serverless', null).isServerless()).toBe(true);
+    expect(new Platform('linux', 'x64', 'test', null, null).isServerless()).toBe(false);
+  });
+});
+
+describe('getSolution()', () => {
+  it('returns the solution or null', () => {
+    expect(new Platform('linux', 'x64', 'test', null, 'observability').getSolution()).toBe(
+      'observability'
+    );
+    expect(new Platform('linux', 'x64', 'test', null, null).getSolution()).toBe(null);
+  });
+});
+
+describe('toString()', () => {
+  it('returns the correct string', () => {
+    expect(new Platform('linux', 'x64', 'test', null, null).toString()).toBe('linux-x64');
+    expect(new Platform('darwin', 'arm64', 'test', null, null).toString()).toBe('darwin-arm64');
+    expect(new Platform('linux', 'x64', 'test', 'serverless', null).toString()).toBe(
+      'linux-x64-serverless'
+    );
+    expect(new Platform('linux', 'x64', 'test', 'serverless', 'chat').toString()).toBe(
+      'linux-x64-serverless-chat'
+    );
+    expect(new Platform('linux', 'x64', 'test', null, 'observability').toString()).toBe(
+      'linux-x64-observability'
+    );
+  });
+});
