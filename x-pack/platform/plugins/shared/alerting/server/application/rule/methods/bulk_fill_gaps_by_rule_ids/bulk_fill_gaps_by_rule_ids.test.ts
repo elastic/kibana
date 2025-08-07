4ff68d58c740c7fdc16b80b1a23989b04f000eb7
Edgar Santos <edgar.santos@elastic.co>
2025-08-07 14:56:14 +0200

    unskip test

--- x-pack/platform/plugins/shared/alerting/server/application/rule/methods/bulk_fill_gaps_by_rule_ids/bulk_fill_gaps_by_rule_ids.test.ts
+++ x-pack/platform/plugins/shared/alerting/server/application/rule/methods/bulk_fill_gaps_by_rule_ids/bulk_fill_gaps_by_rule_ids.test.ts
@@ -69,8 +69,7 @@ const rulesThatAttemptedToBackfill = [...successfulRules, skippedRule, erroredRu
 
 let rulesClientContext: RulesClientContext;
 
-// Failing: See https://github.com/elastic/kibana/issues/230949
-describe.skip('bulkFillGapsByRuleIds', () => {
+describe('bulkFillGapsByRuleIds', () => {
   let refreshIndexMock: jest.Mock;
   let results: BulkFillGapsByRuleIdsResult;
   let ensuredAuthorizedMock: jest.Mock;
