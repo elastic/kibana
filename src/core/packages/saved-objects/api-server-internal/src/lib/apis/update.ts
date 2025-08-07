92753f5ee3737e1370516467854e48eefd6a3818
Sid <siddharthmantri1@gmail.com>
2025-08-07 16:30:51 +0200

    specifically disallow accesscontrol for upsert

--- src/core/packages/saved-objects/api-server-internal/src/lib/apis/update.ts
+++ src/core/packages/saved-objects/api-server-internal/src/lib/apis/update.ts
@@ -182,6 +182,8 @@ export const executeUpdate = async <T>(
 
   // UPSERT CASE START
   if (shouldPerformUpsert) {
+    // Note: Upsert does not support adding accessControl properties. To do so, the `create` API should be used.
+
     // ignore attributes if creating a new doc: only use the upsert attributes
     // don't include upsert if the object already exists; ES doesn't allow upsert in combination with version properties
     const migratedUpsert = migrationHelper.migrateInputDocument({
@@ -195,9 +197,6 @@ export const executeUpdate = async <T>(
       created_at: time,
       updated_at: time,
       ...(updatedBy && { created_by: updatedBy, updated_by: updatedBy }),
-      ...(accessControl && {
-        accessControl,
-      }),
       ...(Array.isArray(references) && { references }),
     }) as SavedObjectSanitizedDoc<T>;
     validationHelper.validateObjectForCreate(type, migratedUpsert);
