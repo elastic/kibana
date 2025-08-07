5848b9d1b2573f91798f3507695364773886bf8e
Shahzad <shahzad31comp@gmail.com>
2025-08-07 22:25:45 +0200

    preserver name space

--- x-pack/solutions/observability/plugins/synthetics/server/routes/monitor_cruds/add_monitor/add_monitor_api.test.ts
+++ x-pack/solutions/observability/plugins/synthetics/server/routes/monitor_cruds/add_monitor/add_monitor_api.test.ts
@@ -124,12 +124,13 @@ describe('AddNewMonitorsPublicAPI', () => {
     });
   });
 
+  const syntheticsService = new SyntheticsService({
+    config: {
+      enabled: true,
+    },
+  } as any);
+
   it('should normalize schedule', async function () {
-    const syntheticsService = new SyntheticsService({
-      config: {
-        enabled: true,
-      },
-    } as any);
     const api = new AddEditMonitorAPI({
       syntheticsMonitorClient: new SyntheticsMonitorClient(syntheticsService, {} as any),
       request: {
@@ -165,10 +166,33 @@ describe('AddNewMonitorsPublicAPI', () => {
     expect(result.schedule).toEqual({ number: 3, unit: 'm' });
   });
 
-  describe('normalizeMonitor defaults', () => {
-    const syntheticsService = new SyntheticsService({
-      config: {},
+  it('should normalize namespace', async function () {
+    const api = new AddEditMonitorAPI({
+      syntheticsMonitorClient: new SyntheticsMonitorClient(syntheticsService, {} as any),
+      request: {
+        body: {},
+        query: {},
+      },
+      spaceId: 'default',
     } as any);
+    expect(api.getMonitorNamespace('testnamespace')).toEqual('testnamespace');
+  });
+
+  it('should normalize namespace in test space', async function () {
+    const api = new AddEditMonitorAPI({
+      syntheticsMonitorClient: new SyntheticsMonitorClient(syntheticsService, {} as any),
+      request: {
+        body: {},
+        query: {},
+      },
+      spaceId: 'test',
+    } as any);
+
+    expect(api.getMonitorNamespace('testnamespace')).toEqual('testnamespace');
+    expect(api.getMonitorNamespace('default')).toEqual('default');
+  });
+
+  describe('normalizeMonitor defaults', () => {
     const api = new AddEditMonitorAPI({
       syntheticsMonitorClient: new SyntheticsMonitorClient(syntheticsService, {} as any),
       request: {
