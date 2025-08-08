fab9c4e706f788237f6a6e460b282bbe50cb5b97
Sean Rathier <sean.rathier@elastic.co>
2025-08-08 11:46:09 -0400

    fixed all ftrs

--- x-pack/solutions/security/plugins/cloud_security_posture/public/components/fleet_extensions/policy_template_form.tsx
+++ x-pack/solutions/security/plugins/cloud_security_posture/public/components/fleet_extensions/policy_template_form.tsx
@@ -14,6 +14,7 @@ import {
   EuiTitle,
 } from '@elastic/eui';
 import semverGte from 'semver/functions/gte';
+import semverCompare from 'semver/functions/compare';
 import { FormattedMessage } from '@kbn/i18n-react';
 import type { PackagePolicyReplaceDefineStepExtensionComponentProps } from '@kbn/fleet-plugin/public/types';
 import {
