6de0667fab5a2e47e22dda1e47b2ede5f8f62b1f
Stratoula <efstratia.kalafateli@elastic.co>
2025-08-08 06:54:36 +0200

    More licensing types fixes

--- x-pack/platform/plugins/shared/licensing/public/index.ts
+++ x-pack/platform/plugins/shared/licensing/public/index.ts
@@ -9,15 +9,5 @@ import type { PluginInitializerContext } from '@kbn/core/public';
 
 import { LicensingPlugin } from './plugin';
 
-export type {
-  LicenseCheckState,
-  LicenseStatus,
-  LicenseFeature,
-  PublicLicense,
-  PublicFeatures,
-  PublicLicenseJSON,
-  LicenseCheck,
-  ILicense,
-} from '../common/types';
 export type { LicensingPluginSetup, LicensingPluginStart } from './types';
 export const plugin = (context: PluginInitializerContext) => new LicensingPlugin(context);
