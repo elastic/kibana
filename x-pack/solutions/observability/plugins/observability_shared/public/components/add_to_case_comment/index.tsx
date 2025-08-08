a27687ac72d5a0fdc4399757209969a6ea9e88db
Dominique Belcher <dominique.clarke@elastic.co>
2025-08-08 15:01:26 -0400

    remove unused hook

--- x-pack/solutions/observability/plugins/observability_shared/public/components/add_to_case_comment/index.tsx
+++ x-pack/solutions/observability/plugins/observability_shared/public/components/add_to_case_comment/index.tsx
@@ -5,13 +5,11 @@
  * 2.0.
  */
 
-import React, { useCallback, useEffect } from 'react';
-import { FormattedMessage } from '@kbn/i18n-react';
-import { EuiTextArea, EuiFormRow, EuiSkeletonText, EuiIcon } from '@elastic/eui';
+import React from 'react';
+import { EuiTextArea, EuiFormRow } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { ObservabilityAIAssistantPublicStart } from '@kbn/observability-ai-assistant-plugin/public';
 import { NotificationsStart } from '@kbn/core/public';
-import { usePageSummary } from '../../hooks/use_page_summary';
 
 interface AddToCaseCommentProps {
   comment: string;
@@ -21,54 +19,7 @@ interface AddToCaseCommentProps {
   notifications: NotificationsStart;
 }
 
-export function AddToCaseComment({
-  comment,
-  onCommentChange,
-  setIsLoading,
-  observabilityAIAssistant,
-  notifications,
-}: AddToCaseCommentProps) {
-  const handleStreamingUpdate = useCallback(
-    (partialSummary: string) => {
-      onCommentChange((prevComment) => (prevComment || '') + partialSummary); // Append new data
-    },
-    [onCommentChange]
-  );
-
-  const { generateSummary, isObsAIAssistantEnabled, isLoading, errors } = usePageSummary({
-    onChunk: handleStreamingUpdate, // Add streaming update handler
-    observabilityAIAssistant,
-  });
-
-  useEffect(() => {
-    if (isObsAIAssistantEnabled) {
-      generateSummary();
-    }
-  }, [generateSummary, isObsAIAssistantEnabled]);
-
-  useEffect(() => {
-    if (isObsAIAssistantEnabled && isLoading) {
-      setIsLoading(true);
-    } else {
-      setIsLoading(false);
-    }
-  }, [isObsAIAssistantEnabled, isLoading, setIsLoading]);
-
-  useEffect(() => {
-    if (errors.length > 0) {
-      errors.forEach((error) => {
-        notifications.toasts.addError(error, {
-          title: i18n.translate(
-            'xpack.observabilityShared.cases.addPageToCaseModal.errorGeneratingSummary',
-            {
-              defaultMessage: 'Could not initialize AI-generated summary',
-            }
-          ),
-        });
-      });
-    }
-  }, [errors, notifications]);
-
+export function AddToCaseComment({ comment, onCommentChange }: AddToCaseCommentProps) {
   const input = (
     <EuiTextArea
       data-test-subj="syntheticsAddToCaseCommentTextArea"
@@ -87,34 +38,15 @@ export function AddToCaseComment({
     />
   );
 
-  const showAIEnhancedExperience = isObsAIAssistantEnabled && !errors.length;
-
   return (
     <>
       <EuiFormRow
         label={i18n.translate('xpack.observabilityShared.cases.addPageToCaseModal.commentLabel', {
           defaultMessage: 'Add a comment (optional)',
         })}
-        helpText={
-          showAIEnhancedExperience ? (
-            <FormattedMessage
-              id="xpack.observabilityShared.cases.addPageToCaseModal.commentHelpText"
-              defaultMessage="{icon} Initial comment AI generated. AI can be wrong or incomplete. Please review and edit as necessary."
-              values={{ icon: <EuiIcon type="sparkles" /> }}
-            />
-          ) : undefined
-        }
         fullWidth
       >
-        {showAIEnhancedExperience ? (
-          !isLoading ? (
-            input
-          ) : (
-            <EuiSkeletonText data-test-subj="addPageToCaseCommentSkeleton" lines={5} />
-          )
-        ) : (
-          input
-        )}
+        {input}
       </EuiFormRow>
     </>
   );
