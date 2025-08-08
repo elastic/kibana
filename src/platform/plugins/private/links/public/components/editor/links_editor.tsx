532d4bd587531d345573571a5e10ac9c5bf7b943
Ola Pawlus <ola.pawlus@elastic.co>
2025-08-08 16:41:33 +0200

    reverting unnecessary changes

--- src/platform/plugins/private/links/public/components/editor/links_editor.tsx
+++ src/platform/plugins/private/links/public/components/editor/links_editor.tsx
@@ -335,6 +335,7 @@ const styles = {
       opacity: 0;
       transform: translateX(100%);
     }
+
     100% {
       opacity: 1;
       transform: translateX(0%);
@@ -346,6 +347,7 @@ const styles = {
       opacity: 1;
       transform: translateX(0%);
     }
+
     100% {
       opacity: 0;
       transform: translateX(100%);
