2108183b8e02a2c253a90e2e8a6af48eb5d0a9f8
Milosz Radzynski <milosz.radzynski@elastic.co>
2025-08-09 17:06:00 +0200

    Create a TodoItem component

---
+++ examples/todo_example/public/components/todo_item.tsx
@@ -0,0 +1,131 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import {
+  EuiCheckbox,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiListGroupItem,
+  EuiPanel,
+  EuiText,
+  EuiToolTip,
+  useEuiTheme,
+} from '@elastic/eui';
+import React from 'react';
+import { css } from '@emotion/react';
+import { i18n } from '@kbn/i18n';
+import type { Todo } from '../../server/plugin';
+import { TodoActions } from './todo_actions';
+
+interface TodoItemProps {
+  onDelete: (todo: Todo) => void;
+  onEdit: (todo: Todo) => void;
+  onStatusChange: (todo: Todo, completed: boolean) => void;
+  todo: Todo;
+}
+
+export const TodoItem = ({ onDelete, onEdit, onStatusChange, todo }: TodoItemProps) => {
+  const { euiTheme } = useEuiTheme();
+
+  const priorityColor =
+    todo.priority === 'High'
+      ? euiTheme.colors.danger
+      : todo.priority === 'Medium'
+      ? euiTheme.colors.warning
+      : euiTheme.colors.success;
+
+  const priorityDotStyles = css`
+    background-color: ${priorityColor};
+    border-radius: 50%;
+    display: block;
+    height: 12px;
+    width: 12px;
+  `;
+
+  const titleStyles = css`
+    display: block;
+    overflow: hidden;
+    text-decoration: ${todo.completed ? 'line-through' : 'none'};
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  `;
+
+  const checkboxAriaLabel = i18n.translate('todoExample.todoItem.checkboxAriaLabel', {
+    defaultMessage: 'Mark {taskTitle} as {status}',
+    values: {
+      taskTitle: todo.title,
+      status: todo.completed
+        ? i18n.translate('todoExample.todoItem.incomplete', { defaultMessage: 'incomplete' })
+        : i18n.translate('todoExample.todoItem.complete', { defaultMessage: 'complete' }),
+    },
+  });
+
+  const priorityTooltip = i18n.translate('todoExample.todoItem.priorityTooltip', {
+    defaultMessage: 'Priority: {priority}',
+    values: { priority: todo.priority },
+  });
+
+  return (
+    <EuiListGroupItem
+      color={todo.completed ? 'primary' : 'text'}
+      key={todo.id}
+      label={
+        <EuiPanel
+          borderRadius="m"
+          color="plain"
+          css={css`
+            width: 400px;
+          `}
+          hasBorder
+          hasShadow
+          paddingSize="m"
+        >
+          <EuiFlexGroup alignItems="center" gutterSize="m" justifyContent="spaceBetween">
+            <EuiFlexItem
+              css={css`
+                min-width: 0;
+              `}
+            >
+              <EuiFlexGroup alignItems="center" gutterSize="s" responsive={false}>
+                <EuiFlexItem grow={false}>
+                  <EuiCheckbox
+                    aria-label={checkboxAriaLabel}
+                    checked={todo.completed}
+                    id={`todo-checkbox-${todo.id}`}
+                    onChange={() => onStatusChange(todo, !todo.completed)}
+                  />
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiToolTip content={priorityTooltip} delay="regular">
+                    <span aria-label={priorityTooltip} css={priorityDotStyles} />
+                  </EuiToolTip>
+                </EuiFlexItem>
+                <EuiFlexItem
+                  css={css`
+                    min-width: 0;
+                  `}
+                >
+                  <EuiText css={titleStyles}>{todo.title}</EuiText>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <TodoActions
+                onDelete={() => onDelete(todo)}
+                onEdit={() => onEdit(todo)}
+                todo={todo}
+              />
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiPanel>
+      }
+      size="l"
+    />
+  );
+};
