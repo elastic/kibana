df066a751f2cdac0cee776a4d33de8d1b320708c
Jean-Louis Leysens <jloleysens@gmail.com>
2025-08-11 17:19:54 +0200

    simpler?

--- src/core/packages/append-x/server/src/contracts.ts
+++ src/core/packages/append-x/server/src/contracts.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { Type, schema } from '@kbn/config-schema';
+import { Type, TypeOf, schema } from '@kbn/config-schema';
 import type { Client } from '@elastic/elasticsearch';
 import type api from '@elastic/elasticsearch/lib/api/types';
 
@@ -19,36 +19,43 @@ const esClient: Client = {} as any;
 const appendXSetup: AppendXServiceSetup = {} as any;
 
 /**
- * A type checked schema and mappings declaration similar to SOs, but tried to be a bit simpler. With the current typings
- * we can also check that you provide, for ex., keyword/text mappings for
- * string fields.
+ * A type checked schema and mappings declaration similar to SOs.
  *
  * My assumption is that this one declaration will/can only evolve in bwc ways. To "break" this data stream you will need
  * to create a new one, with a new name.
  */
-const myDataStream = dataStream.create({
-  name: 'my-data-stream-1',
-  schema: schema.object({
-    '@timestamp': schema.string(),
-    object: schema.object({}),
-    someField: schema.string(),
+const myDocumentSchema = schema.object({
+  '@timestamp': schema.string(),
+  object: schema.object({
+    test: schema.string(),
   }),
+  someField: schema.string(),
+});
+
+type MyDocument = TypeOf<typeof myDocumentSchema>;
+
+const myDataStream: DataStreamDefinition<MyDocument> = {
+  name: 'my-data-stream',
+  schema: myDocumentSchema,
+  autoRollover: true,
   mappings: {
     dynamic: false,
     properties: {
-      '@timestamp': mappings.keyword(),
+      '@timestamp': mappings.date(),
+      object: {
+        type: 'object',
+        properties: {
+          test: mappings.text(),
+        },
+      },
     },
   },
-  migrations: {
-    '1': {
-      operations: [{ type: 'backfill_last', duration: '30d' }],
-    },
-  },
-});
+};
 
 // This could be a nice way to expose the "low-level" requests we are making so that
 // the data stream abstraction feels more like a thin helper or layer around the
-// the Elasticsearch APIs.
+// the Elasticsearch APIs. Intended to be used by plugin developers for test
+// authoring purposes.
 esClient.indices.putIndexTemplate(dataStream.asPutIndexTemplateRequestArgs(myDataStream));
 esClient.indices.createDataStream(dataStream.asCreateDataStreamRequestArgs(myDataStream));
 
@@ -69,10 +76,13 @@ type StrictMappingTypeMapping = Strict<api.MappingTypeMapping>;
 
 type KeywordMapping = Strict<api.MappingKeywordProperty>;
 type TextMapping = Strict<api.MappingTextProperty>;
+type DateMapping = Strict<api.MappingDateProperty>;
+type DateNanosMapping = Strict<api.MappingDateNanosProperty>;
 
-type StringMapping = KeywordMapping | TextMapping;
+type StringMapping = KeywordMapping | TextMapping | DateMapping | DateNanosMapping;
 
 interface MappingsHelpers {
+  date: () => KeywordMapping;
   keyword: () => KeywordMapping;
   text: () => TextMapping;
 }
@@ -84,16 +94,7 @@ type DataStreamDeclarationMappings<Schema extends Record<string, unknown>> = Omi
   properties: ObjectToPropertiesDefinition<Schema>;
 };
 
-interface Migration {
-  operations: Array<
-    | { type: 'backfill_last'; duration: '30d' }
-    // Theoretically we can figure this out from the declarations...
-    | { type: 'add_mappings'; subsetOfNewMappings: Record<string, unknown> }
-    | { type: 'etc'; data: unknown } // TODO: define more migrations}
-  >;
-}
-
-interface DataStreamDeclaration<Schema extends Record<string, unknown> = {}> {
+interface DataStreamDefinition<Schema extends Record<string, unknown> = {}> {
   /**
    * @remark Once delcared this can never change.
    */
@@ -104,35 +105,21 @@ interface DataStreamDeclaration<Schema extends Record<string, unknown> = {}> {
    */
   schema: Type<Schema>;
   mappings?: DataStreamDeclarationMappings<Schema>;
-
   /**
-   * A set of migrations to run. Progress of migrations will likely be tracked in
-   * data stream meta...
+   * TODO: discuss, this can control whether new mappings will trigger a rollover
+   *      or not. If set to true, the data stream will be rolled over when the "_meta"
+   *      field is updated. This ensures that _new_ documents will reflect the new mappings,
    */
-  migrations?: {
-    '1': Migration;
-    '2'?: Migration;
-    '3'?: Migration;
-    '4'?: Migration;
-    '6'?: Migration;
-    '7'?: Migration;
-    '8'?: Migration;
-    '9'?: Migration;
-    '10'?: Migration;
-    '11'?: Migration;
-  };
+  autoRollover?: boolean;
 }
 
 interface DataStreamHelpers {
-  create: <Schema extends Record<string, unknown>>(
-    arg: DataStreamDeclaration<Schema>
-  ) => DataStreamDeclaration<Schema>;
-  asPutIndexTemplateRequestArgs(ds: DataStreamDeclaration): api.IndicesPutIndexTemplateRequest;
-  asCreateDataStreamRequestArgs(ds: DataStreamDeclaration): api.IndicesCreateDataStreamRequest;
+  asPutIndexTemplateRequestArgs(ds: DataStreamDefinition): api.IndicesPutIndexTemplateRequest;
+  asCreateDataStreamRequestArgs(ds: DataStreamDefinition): api.IndicesCreateDataStreamRequest;
 }
 
 export interface AppendXServiceSetup {
-  registerDataStream: (dataStream: DataStreamDeclaration) => void;
+  registerDataStream: (dataStream: DataStreamDefinition) => void;
 }
 
 // An attempt at getting TS to check mapping properties match the schema
