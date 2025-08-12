9d3e1dadfdb02d05fe69b4c9f6786748891c252e
Jen Huang <its.jenetic@gmail.com>
2025-08-12 11:53:41 -0700

    Fix brackets

--- x-pack/platform/test/fleet_api_integration/apis/package_policy/input_package_create_upgrade.ts
+++ x-pack/platform/test/fleet_api_integration/apis/package_policy/input_package_create_upgrade.ts
@@ -216,36 +216,33 @@ export default function (providerContext: FtrProviderContext) {
       } = packageComponentTemplate!.component_template as any;
       expect(packageComponentTemplate!.name).eql('logs-dataset1@package');
       expect(definitionWithouTimestamps).eql({
-        name: 'logs-dataset1@package',
-        component_template: {
-          template: {
-            settings: {
-              index: {
-                lifecycle: { name: 'logs' },
-                default_pipeline: 'logs-dataset1-1.0.0',
-                mapping: {
-                  total_fields: { limit: '1000' },
-                },
+        template: {
+          settings: {
+            index: {
+              lifecycle: { name: 'logs' },
+              default_pipeline: 'logs-dataset1-1.0.0',
+              mapping: {
+                total_fields: { limit: '1000' },
               },
             },
-            mappings: {
-              properties: {
-                input: {
-                  properties: {
-                    name: {
-                      type: 'constant_keyword',
-                      value: 'logs',
-                    },
+          },
+          mappings: {
+            properties: {
+              input: {
+                properties: {
+                  name: {
+                    type: 'constant_keyword',
+                    value: 'logs',
                   },
                 },
               },
             },
           },
-          _meta: {
-            package: { name: 'input_package_upgrade' },
-            managed_by: 'fleet',
-            managed: true,
-          },
+        },
+        _meta: {
+          package: { name: 'input_package_upgrade' },
+          managed_by: 'fleet',
+          managed: true,
         },
       });
     });
