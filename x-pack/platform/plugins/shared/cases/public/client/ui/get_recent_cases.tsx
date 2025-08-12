65d291f3e8b1272392798bb8f962e3b2f15d314f
Francesco Fagnani <francesco.fagnani@elastic.co>
2025-08-12 14:37:29 +0200

    feat(attachment registry): added attachmentSuggestionRegistry to case context

--- x-pack/platform/plugins/shared/cases/public/client/ui/get_recent_cases.tsx
+++ x-pack/platform/plugins/shared/cases/public/client/ui/get_recent_cases.tsx
@@ -16,6 +16,7 @@ export type GetRecentCasesProps = Omit<
   GetRecentCasesPropsInternal,
   | 'externalReferenceAttachmentTypeRegistry'
   | 'persistableStateAttachmentTypeRegistry'
+  | 'attachmentSuggestionRegistry'
   | 'getFilesClient'
 >;
 
@@ -25,6 +26,7 @@ const RecentCasesLazy: React.FC<RecentCasesProps> = lazy(
 export const getRecentCasesLazy = ({
   externalReferenceAttachmentTypeRegistry,
   persistableStateAttachmentTypeRegistry,
+  attachmentSuggestionRegistry,
   getFilesClient,
   owner,
   permissions,
@@ -34,6 +36,7 @@ export const getRecentCasesLazy = ({
     value={{
       externalReferenceAttachmentTypeRegistry,
       persistableStateAttachmentTypeRegistry,
+      attachmentSuggestionRegistry,
       getFilesClient,
       owner,
       permissions,
