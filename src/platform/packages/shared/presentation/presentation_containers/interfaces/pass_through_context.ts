03ed9249af4bf8014f619e1ee9e547d792b677c8
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-08-12 22:52:25 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- src/platform/packages/shared/presentation/presentation_containers/interfaces/pass_through_context.ts
+++ src/platform/packages/shared/presentation/presentation_containers/interfaces/pass_through_context.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { SerializableRecord } from "@kbn/utility-types";
+import { SerializableRecord } from '@kbn/utility-types';
 
 /**
  * This API provides pass through context.
