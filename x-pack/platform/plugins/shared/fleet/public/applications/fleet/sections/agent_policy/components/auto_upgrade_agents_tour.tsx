7361450381742051706fdf5efe752ca1cbf6e7bb
Jen Huang <its.jenetic@gmail.com>
2025-08-13 15:13:32 -0700

    Initial pass at creating a tour manager for fleet

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/components/auto_upgrade_agents_tour.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/components/auto_upgrade_agents_tour.tsx
@@ -6,36 +6,13 @@
  */
 
 import { EuiText, EuiTourStep } from '@elastic/eui';
-import React, { useState } from 'react';
+import React from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
 
-import type { TOUR_STORAGE_CONFIG } from '../../../constants';
-import { TOUR_STORAGE_KEYS } from '../../../constants';
-import { useStartServices } from '../../../hooks';
+import { useDismissableTour } from '../../../hooks';
 
 export const AutoUpgradeAgentsTour: React.FC<{ anchor: string }> = ({ anchor }) => {
-  const { storage, uiSettings } = useStartServices();
-
-  const [tourState, setTourState] = useState({ isOpen: true });
-
-  const isTourHidden =
-    uiSettings.get('hideAnnouncements', false) ||
-    (
-      storage.get(TOUR_STORAGE_KEYS.AUTO_UPGRADE_AGENTS) as
-        | TOUR_STORAGE_CONFIG['AUTO_UPGRADE_AGENTS']
-        | undefined
-    )?.active === false;
-
-  const setTourAsHidden = () => {
-    storage.set(TOUR_STORAGE_KEYS.AUTO_UPGRADE_AGENTS, {
-      active: false,
-    } as TOUR_STORAGE_CONFIG['AUTO_UPGRADE_AGENTS']);
-  };
-
-  const onFinish = () => {
-    setTourState({ isOpen: false });
-    setTourAsHidden();
-  };
+  const { isOpen, dismiss } = useDismissableTour('AUTO_UPGRADE_AGENTS');
 
   return (
     <>
@@ -48,8 +25,8 @@ export const AutoUpgradeAgentsTour: React.FC<{ anchor: string }> = ({ anchor }) 
             />
           </EuiText>
         }
-        isStepOpen={!isTourHidden && tourState.isOpen}
-        onFinish={onFinish}
+        isStepOpen={isOpen}
+        onFinish={dismiss}
         minWidth={360}
         maxWidth={360}
         step={1}
--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agents/agent_list_page/components/agent_export_csv_tour.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agents/agent_list_page/components/agent_export_csv_tour.tsx
@@ -6,36 +6,13 @@
  */
 
 import { EuiText, EuiTourStep } from '@elastic/eui';
-import React, { useState } from 'react';
+import React from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
 
-import type { TOUR_STORAGE_CONFIG } from '../../../../constants';
-import { TOUR_STORAGE_KEYS } from '../../../../constants';
-import { useStartServices } from '../../../../hooks';
+import { useDismissableTour } from '../../../../hooks';
 
 export const AgentExportCSVTour: React.FC<{}> = () => {
-  const { storage, uiSettings } = useStartServices();
-
-  const [tourState, setTourState] = useState({ isOpen: true });
-
-  const isTourHidden =
-    uiSettings.get('hideAnnouncements', false) ||
-    (
-      storage.get(TOUR_STORAGE_KEYS.AGENT_EXPORT_CSV) as
-        | TOUR_STORAGE_CONFIG['AGENT_EXPORT_CSV']
-        | undefined
-    )?.active === false;
-
-  const setTourAsHidden = () => {
-    storage.set(TOUR_STORAGE_KEYS.AGENT_EXPORT_CSV, {
-      active: false,
-    } as TOUR_STORAGE_CONFIG['AGENT_EXPORT_CSV']);
-  };
-
-  const onFinish = () => {
-    setTourState({ isOpen: false });
-    setTourAsHidden();
-  };
+  const { isOpen, dismiss } = useDismissableTour('AGENT_EXPORT_CSV');
 
   return (
     <>
@@ -48,8 +25,8 @@ export const AgentExportCSVTour: React.FC<{}> = () => {
             />
           </EuiText>
         }
-        isStepOpen={!isTourHidden && tourState.isOpen}
-        onFinish={onFinish}
+        isStepOpen={isOpen}
+        onFinish={dismiss}
         minWidth={360}
         maxWidth={360}
         step={1}
