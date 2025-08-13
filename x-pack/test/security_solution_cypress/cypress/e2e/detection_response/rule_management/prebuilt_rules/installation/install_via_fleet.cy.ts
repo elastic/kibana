272f8b82f4d1d54f028d83cfaa14fae2a577337f
Georgii Gorbachev <georgii.gorbachev@elastic.co>
2025-08-13 15:57:10 +0200

    Backport

--- x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_management/prebuilt_rules/installation/install_via_fleet.cy.ts
+++ x-pack/test/security_solution_cypress/cypress/e2e/detection_response/rule_management/prebuilt_rules/installation/install_via_fleet.cy.ts
@@ -21,7 +21,7 @@ import { expectManagementTableRules } from '../../../../../tasks/alerts_detectio
 const PREBUILT_RULES_PACKAGE_INSTALLATION_TIMEOUT_MS = 120000; // 2 minutes
 
 describe(
-  'Detection rules, Prebuilt Rules Installation and Update workflow',
+  'Detection rules, Prebuilt Rules Installation Workflow (Real package)',
   { tags: ['@ess', '@serverless'] },
   () => {
     describe('Installation of prebuilt rules package via Fleet', () => {
@@ -35,7 +35,7 @@ describe(
         login();
       });
 
-      it('should install prebuilt rules from the Fleet package', () => {
+      it('installs prebuilt rules from the "security_detection_engine" Fleet package', () => {
         visitAddRulesPage();
 
         // Expect the package to be installed
