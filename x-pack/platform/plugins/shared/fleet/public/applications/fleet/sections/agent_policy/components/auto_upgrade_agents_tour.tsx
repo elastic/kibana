b66a47032e75da0fd677e13c01c69e8904dfd57d
Jen Huang <its.jenetic@gmail.com>
2025-08-13 12:05:53 -0700

    Revert "Initial pass at a global tour helper hook"
    This reverts commit 9683e40ec4315c3991bfa5f0325ccd02d771c398.

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/components/auto_upgrade_agents_tour.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/components/auto_upgrade_agents_tour.tsx
@@ -5,21 +5,36 @@
  * 2.0.
  */
 
-import { EuiText, EuiTourStep, EuiButtonEmpty } from '@elastic/eui';
+import { EuiText, EuiTourStep } from '@elastic/eui';
 import React, { useState } from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
 
-import { useGlobalFleetTours } from '../../../../../hooks';
+import type { TOUR_STORAGE_CONFIG } from '../../../constants';
+import { TOUR_STORAGE_KEYS } from '../../../constants';
+import { useStartServices } from '../../../hooks';
 
 export const AutoUpgradeAgentsTour: React.FC<{ anchor: string }> = ({ anchor }) => {
-  const { shouldShowTour, nextTour } = useGlobalFleetTours('AUTO_UPGRADE_AGENTS', {
-    isConditionMet: () => true, // Always available on agent policy tab
-  });
+  const { storage, uiSettings } = useStartServices();
 
   const [tourState, setTourState] = useState({ isOpen: true });
+
+  const isTourHidden =
+    uiSettings.get('hideAnnouncements', false) ||
+    (
+      storage.get(TOUR_STORAGE_KEYS.AUTO_UPGRADE_AGENTS) as
+        | TOUR_STORAGE_CONFIG['AUTO_UPGRADE_AGENTS']
+        | undefined
+    )?.active === false;
+
+  const setTourAsHidden = () => {
+    storage.set(TOUR_STORAGE_KEYS.AUTO_UPGRADE_AGENTS, {
+      active: false,
+    } as TOUR_STORAGE_CONFIG['AUTO_UPGRADE_AGENTS']);
+  };
+
   const onFinish = () => {
     setTourState({ isOpen: false });
-    nextTour();
+    setTourAsHidden();
   };
 
   return (
@@ -33,7 +48,7 @@ export const AutoUpgradeAgentsTour: React.FC<{ anchor: string }> = ({ anchor }) 
             />
           </EuiText>
         }
-        isStepOpen={shouldShowTour() && tourState.isOpen}
+        isStepOpen={!isTourHidden && tourState.isOpen}
         onFinish={onFinish}
         minWidth={360}
         maxWidth={360}
@@ -46,11 +61,6 @@ export const AutoUpgradeAgentsTour: React.FC<{ anchor: string }> = ({ anchor }) 
           />
         }
         anchorPosition="downLeft"
-        footerAction={
-          <EuiButtonEmpty onClick={onFinish}>
-            <FormattedMessage id="xpack.fleet.tour.finishButton" defaultMessage="Finish" />
-          </EuiButtonEmpty>
-        }
         anchor={anchor}
       />
     </>
