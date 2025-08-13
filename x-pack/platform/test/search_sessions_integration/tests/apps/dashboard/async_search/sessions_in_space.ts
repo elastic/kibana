7d414c777138187ea782200eca3b1f6714859aa6
AlexGPlay <alex.garcia@elastic.co>
2025-08-13 10:18:14 +0200

    Add discover feature

--- x-pack/platform/test/search_sessions_integration/tests/apps/dashboard/async_search/sessions_in_space.ts
+++ x-pack/platform/test/search_sessions_integration/tests/apps/dashboard/async_search/sessions_in_space.ts
@@ -109,6 +109,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         {
           feature: {
             dashboard: dashboards,
+            discover: ['all'],
           },
           spaces: ['another-space'],
         },
