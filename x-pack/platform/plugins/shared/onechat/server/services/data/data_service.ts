55f52014f98fcb517aa441162f8e39a3d1c3b6d5
Lorena Balan <lorena.balan@elastic.co>
2025-08-13 17:01:21 +0200

    Add boilerplate for new data service and initial skeleton for data type registry

---
+++ x-pack/platform/plugins/shared/onechat/server/services/data/data_service.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { Logger } from '@kbn/core/server';
+import type { KibanaRequest } from '@kbn/core-http-server';
+import type { Runner } from '@kbn/onechat-server';
+import {
+  createDataTypeRegistry,
+  type DataTypeRegistry,
+  type DataTypeDefinition,
+  registerDataTypes,
+} from './data_type_registry';
+
+export interface DataServiceSetupDeps {
+  logger: Logger;
+}
+
+export interface DataServiceStartDeps {
+  getRunner: () => Runner;
+}
+
+export interface DataServiceSetup {
+  register(dataType: DataTypeDefinition): void;
+}
+
+export interface DataServiceStart {
+  getRegistry(opts: { request: KibanaRequest }): Promise<DataTypeRegistry>;
+}
+
+export class DataService {
+  private setupDeps?: DataServiceSetupDeps;
+  private dataTypeRegistry: DataTypeRegistry;
+
+  constructor() {
+    this.dataTypeRegistry = createDataTypeRegistry();
+  }
+
+  setup(deps: DataServiceSetupDeps): DataServiceSetup {
+    this.setupDeps = deps;
+    registerDataTypes({ registry: this.dataTypeRegistry });
+
+    return {
+      register: (dataType) => this.dataTypeRegistry.register(dataType),
+    };
+  }
+
+  start(deps: DataServiceStartDeps): DataServiceStart {
+    const { logger } = this.setupDeps!;
+    logger.info('Starting data service...');
+
+    const getRegistry: DataServiceStart['getRegistry'] = async ({ request }) => {
+      return this.dataTypeRegistry;
+    };
+
+    return {
+      getRegistry,
+    };
+  }
+}
