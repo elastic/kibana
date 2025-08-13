af6590a8e0fef23558228e91f1bbe851566fb4c6
Julia Rechkunova <julia.rechkunova@elastic.co>
2025-08-13 15:23:18 +0200

    [Discover] Revert unnecessary changes

--- src/platform/plugins/shared/field_formats/common/converters/number.test.ts
+++ src/platform/plugins/shared/field_formats/common/converters/number.test.ts
@@ -36,13 +36,13 @@ describe('NumberFormat', () => {
     ).toMatchInlineSnapshot(`"{\\"min\\":150,\\"max\\":1000,\\"sum\\":5000,\\"value_count\\":10}"`);
     expect(formatter.convert({ min: 150, max: 1000, sum: 5000, value_count: 10 }, 'html'))
       .toMatchInlineSnapshot(`
-          "{
-            \\"min\\": 150,
-            \\"max\\": 1000,
-            \\"sum\\": 5000,
-            \\"value_count\\": 10
-          }"
-      `);
+    "{
+      \\"min\\": 150,
+      \\"max\\": 1000,
+      \\"sum\\": 5000,
+      \\"value_count\\": 10
+    }"
+  `);
   });
 
   test('object input stringified', () => {
