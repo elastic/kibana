a18be128c354cc5f34c6a6dfea6050f29acc5ef9
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-08-13 12:53:44 +0200

    [ska] relocate Observability serverless api & functional tests (#231256)
    ## Summary
    
    Part of https://github.com/elastic/kibana-team/issues/1503
    
    This PR is mostly about moving observability FTR/Cypress tests from
    `x-pack/test_serverless/` dir to `x-pack/solutions/observability/test`
    
    Before
    ```
    x-pack/test_serverless/
      | - functional/test_suites/observability/
      | - api_integration/test_suites/observability/
    ```
    
    After:
    ```
    x-pack/solutions/observability/test/serverless
      | - functional/
      |  |- test_suites/
      |  |- configs/
      | - api_integration/
         |- test_suites/
         |- configs/
    ```
    
    All the tests are run under the renamed FTR configs:
    <img width="1713" height="481" alt="Screenshot 2025-08-11 at 12 08 17"
    src="https://github.com/user-attachments/assets/a1588e1a-609d-4c28-a0f8-d442eaa4a1da"
    />
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 996b489e2d950a34a459baafcddefced2c44debc)
    
    # Conflicts:
    #	.buildkite/ftr_oblt_serverless_configs.yml
    #	x-pack/platform/test/tsconfig.json
    #	x-pack/solutions/observability/plugins/observability_logs_explorer/README.md
    #	x-pack/solutions/observability/test/serverless/api_integration/configs/index.logs_essentials.ts
    #	x-pack/solutions/observability/test/serverless/api_integration/test_suites/logs_essentials_only/connector_and_rules.ts
    #	x-pack/solutions/observability/test/serverless/api_integration/test_suites/platform_security/authorization.ts
    #	x-pack/solutions/observability/test/serverless/api_integration/test_suites/platform_security/index.ts
    #	x-pack/solutions/observability/test/serverless/functional/test_suites/cases/index.ts
    #	x-pack/solutions/observability/test/serverless/functional/test_suites/discover/embeddables/_get_doc_viewer.ts
    #	x-pack/solutions/observability/test/serverless/functional/test_suites/discover/logs/_get_doc_viewer.ts
    #	x-pack/solutions/observability/test/serverless/functional/test_suites/landing_page.ts
    #	x-pack/solutions/observability/test/serverless/functional/test_suites/logs_essentials_only/privileges.ts
    #	x-pack/solutions/observability/test/serverless/functional/test_suites/privileges.ts
    #	x-pack/test_serverless/api_integration/services/index.ts
    #	x-pack/test_serverless/api_integration/services/synthtrace.ts
    #	x-pack/test_serverless/functional/page_objects/index.ts
    #	x-pack/test_serverless/functional/test_suites/observability/index.ts

--- x-pack/test_serverless/functional/page_objects/index.ts
+++ x-pack/test_serverless/functional/page_objects/index.ts
@@ -15,9 +15,6 @@ import { SvlApiKeysProvider } from '@kbn/test-suites-xpack-platform/serverless/f
 import { SvlDataUsagePageProvider } from '@kbn/test-suites-xpack-platform/serverless/functional/page_objects/svl_data_usage';
 import { SvlRuleDetailsPageProvider } from '@kbn/test-suites-xpack-platform/serverless/functional/page_objects/svl_rule_details_ui_page';
 import { SvlTriggersActionsPageProvider } from '@kbn/test-suites-xpack-platform/serverless/functional/page_objects/svl_triggers_actions_ui_page';
-import { SvlObltOnboardingPageProvider } from './svl_oblt_onboarding_page';
-import { SvlObltOnboardingStreamLogFilePageProvider } from './svl_oblt_onboarding_stream_log_file';
-import { SvlObltOverviewPageProvider } from './svl_oblt_overview_page';
 import { SvlSecLandingPageProvider } from './svl_sec_landing_page';
 import { CspDashboardPageProvider } from './csp_dashboard_page';
 import { AddCisIntegrationFormPageProvider } from './add_cis_integration_form_page';
@@ -28,9 +25,6 @@ export const pageObjects = {
 
   svlCommonPage: SvlCommonPageProvider,
   svlCommonNavigation: SvlCommonNavigationProvider,
-  svlObltOnboardingPage: SvlObltOnboardingPageProvider,
-  SvlObltOnboardingStreamLogFilePage: SvlObltOnboardingStreamLogFilePageProvider,
-  svlObltOverviewPage: SvlObltOverviewPageProvider,
   svlSecLandingPage: SvlSecLandingPageProvider,
   svlTriggersActionsUI: SvlTriggersActionsPageProvider,
   svlRuleDetailsUI: SvlRuleDetailsPageProvider,
