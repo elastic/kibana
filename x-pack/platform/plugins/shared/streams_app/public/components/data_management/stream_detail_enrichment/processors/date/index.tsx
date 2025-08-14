f800ce337993b737022a7ddd9dbbc59e00160bc2
Kerry Gallagher <471693+Kerry350@users.noreply.github.com>
2025-08-14 20:19:35 +0100

    Fix date processor
    - target_field -> to
    - Remove timezone and locale

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/processors/date/index.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/processors/date/index.tsx
@@ -19,12 +19,7 @@ import { ProcessorFieldSelector } from '../processor_field_selector';
 import { FieldsAccordion } from '../optional_fields_accordion';
 import { ProcessorConditionEditor } from '../processor_condition_editor';
 import { IgnoreFailureToggle } from '../ignore_toggles';
-import {
-  DateTargetField,
-  DateTimezoneField,
-  DateOutputFormatField,
-  DateLocaleField,
-} from './date_optional_fields';
+import { DateTargetField, DateOutputFormatField } from './date_optional_fields';
 import { DateFormatsField } from './date_formats_field';
 
 import { selectPreviewRecords } from '../../state_management/simulation_state_machine/selectors';
@@ -147,8 +142,6 @@ export const DateProcessorForm = () => {
       <EuiSpacer size="m" />
       <FieldsAccordion>
         <DateTargetField />
-        <DateTimezoneField />
-        <DateLocaleField />
         <DateOutputFormatField />
         <EuiSpacer size="m" />
         <ProcessorConditionEditor />
