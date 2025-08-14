18736ea556ee16186ca71acef834d0a63a106b0a
Ihor Panasiuk <igorskynet13@gmail.com>
2025-08-14 11:39:22 +0200

    small fix

--- src/platform/plugins/shared/workflows_management/common/lib/build_execution_graph/build_execution_graph.ts
+++ src/platform/plugins/shared/workflows_management/common/lib/build_execution_graph/build_execution_graph.ts
@@ -39,7 +39,7 @@ function visitAbstractStep(graph: graphlib.Graph, previousStep: any, currentStep
     return visitForeachStep(graph, previousStep, currentStep);
   }
 
-  if (currentStep.type === 'wait' || currentStep.type === 'delay') {
+  if (currentStep.type === 'wait') {
     return visitWaitStep(graph, previousStep, currentStep);
   }
 
