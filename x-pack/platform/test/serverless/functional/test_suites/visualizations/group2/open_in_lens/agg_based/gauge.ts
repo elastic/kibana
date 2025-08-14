5df315aae4cf4a83f1313347b99134e0c83f9177
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-08-15 00:00:17 +0200

    [ska] relocate fixtures from x-pack/test_serverless

--- x-pack/platform/test/serverless/functional/test_suites/visualizations/group2/open_in_lens/agg_based/gauge.ts
+++ x-pack/platform/test/serverless/functional/test_suites/visualizations/group2/open_in_lens/agg_based/gauge.ts
@@ -24,7 +24,7 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
 
   describe('Gauge', function describeIndexTests() {
     const fixture =
-      'x-pack/test_serverless/functional/fixtures/kbn_archiver/lens/open_in_lens/agg_based/gauge.json';
+      'x-pack/platform/test/serverless/fixtures/kbn_archives/lens/open_in_lens/agg_based/gauge.json';
 
     before(async () => {
       await kibanaServer.importExport.load(fixture);
