55d33ff1a1b3f5351b0749c797975f0d7ee04d17
Davis Plumlee <davis.plumlee@elastic.co>
2025-08-14 11:54:01 -0400

    address comment

--- x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_edit/fields/hooks/use_data_view.ts
+++ x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/final_edit/fields/hooks/use_data_view.ts
@@ -26,14 +26,12 @@ export function useDataView(indexPatternsOrDataViewId: UseDataViewParams): UseDa
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    if (dataView !== undefined) {
+      return;
+    }
     setIsLoading(true);
 
     (async () => {
-      if (dataView !== undefined) {
-        setIsLoading(false);
-        return;
-      }
-
       try {
         if (indexPatternsOrDataViewId.indexPatterns) {
           const indexPatternsDataView = await dataViewsService.create({
