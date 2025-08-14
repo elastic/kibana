bd87b333c21afa5a3d3abf25474565a19f099814
AlexGPlay <alex.garcia@elastic.co>
2025-08-14 13:21:01 +0200

    Split setup+rename functions

--- src/platform/plugins/shared/data/public/search/session/sessions_mgmt/index.ts
+++ src/platform/plugins/shared/data/public/search/session/sessions_mgmt/index.ts
@@ -16,7 +16,7 @@ import type {
   StartServicesAccessor,
 } from '@kbn/core/public';
 import type { CoreSetup } from '@kbn/core/public';
-import type { ManagementSetup } from '@kbn/management-plugin/public';
+import type { ManagementApp, ManagementSetup } from '@kbn/management-plugin/public';
 import type { SharePluginStart } from '@kbn/share-plugin/public';
 import type { ISessionsClient, SearchUsageCollector } from '../../..';
 import { SEARCH_SESSIONS_MANAGEMENT_ID, BACKGROUND_SEARCH_FEATURE_FLAG_KEY } from '../constants';
@@ -61,14 +61,13 @@ export const APP = {
         }),
 };
 
-export async function registerSearchSessionsMgmt(
+export function registerSearchSessionsMgmt(
   coreSetup: CoreSetup<IManagementSectionsPluginsStart>,
-  getStartServices: StartServicesAccessor,
   deps: IManagementSectionsPluginsSetup,
   config: SearchSessionsConfigSchema,
   kibanaVersion: string
 ) {
-  deps.management.sections.section.kibana.registerApp({
+  return deps.management.sections.section.kibana.registerApp({
     id: APP.id,
     title: APP.getI18nName(false),
     order: 1.75,
@@ -78,16 +77,21 @@ export async function registerSearchSessionsMgmt(
       return mgmtApp.mountManagementSection();
     },
   });
+}
 
+export async function updateSearchSessionMgmtSectionTitle(
+  getStartServices: StartServicesAccessor,
+  app: ManagementApp
+) {
   const [coreStart] = await getStartServices();
   const hasBackgroundSearchEnabled = coreStart.featureFlags.getBooleanValue(
     BACKGROUND_SEARCH_FEATURE_FLAG_KEY,
     false
   );
-  const app = deps.management.sections.section.kibana.getApp(APP.id);
 
-  if (hasBackgroundSearchEnabled && app) {
-    // @ts-ignore this apps are supposed to be readonly but I can't find a different workaround to make it work.
+  if (hasBackgroundSearchEnabled) {
+    // @ts-expect-error
+    // This apps are supposed to be readonly but I can't find a different workaround to make it work.
     // If I go in manually everything works correctly but if I await the start services before registering the app the
     // functional tests can't find it. This is temporary until we remove the feature flag.
     app.title = APP.getI18nName(true);
