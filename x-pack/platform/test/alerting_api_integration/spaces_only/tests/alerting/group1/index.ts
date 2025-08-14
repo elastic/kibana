bfa1bb66ae8fa10c7d40b47b90e6e9a5f03419b7
Jiawei Wu <jiawei.wu@elastic.co>
2025-08-13 20:08:14 -0700

    Comment other tests for flaky test runner

--- x-pack/platform/test/alerting_api_integration/spaces_only/tests/alerting/group1/index.ts
+++ x-pack/platform/test/alerting_api_integration/spaces_only/tests/alerting/group1/index.ts
@@ -13,21 +13,21 @@ export default function alertingTests({ loadTestFile, getService }: FtrProviderC
     before(async () => await buildUp(getService));
     after(async () => await tearDown(getService));
 
-    loadTestFile(require.resolve('./aggregate'));
-    loadTestFile(require.resolve('./create'));
-    loadTestFile(require.resolve('./delete'));
-    loadTestFile(require.resolve('./disable'));
-    loadTestFile(require.resolve('./enable'));
-    loadTestFile(require.resolve('./find'));
-    loadTestFile(require.resolve('./find_internal'));
-    loadTestFile(require.resolve('./get'));
-    loadTestFile(require.resolve('./get_alert_state'));
-    loadTestFile(require.resolve('./get_alert_summary'));
-    loadTestFile(require.resolve('./get_execution_log'));
-    loadTestFile(require.resolve('./get_action_error_log'));
-    loadTestFile(require.resolve('./get_rule_tags'));
-    loadTestFile(require.resolve('./rule_types'));
-    loadTestFile(require.resolve('./rule_types_internal'));
+    // loadTestFile(require.resolve('./aggregate'));
+    // loadTestFile(require.resolve('./create'));
+    // loadTestFile(require.resolve('./delete'));
+    // loadTestFile(require.resolve('./disable'));
+    // loadTestFile(require.resolve('./enable'));
+    // loadTestFile(require.resolve('./find'));
+    // loadTestFile(require.resolve('./find_internal'));
+    // loadTestFile(require.resolve('./get'));
+    // loadTestFile(require.resolve('./get_alert_state'));
+    // loadTestFile(require.resolve('./get_alert_summary'));
+    // loadTestFile(require.resolve('./get_execution_log'));
+    // loadTestFile(require.resolve('./get_action_error_log'));
+    // loadTestFile(require.resolve('./get_rule_tags'));
+    // loadTestFile(require.resolve('./rule_types'));
+    // loadTestFile(require.resolve('./rule_types_internal'));
     loadTestFile(require.resolve('./event_log'));
   });
 }
