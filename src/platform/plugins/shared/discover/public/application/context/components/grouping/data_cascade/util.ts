d7513b51ae4fc1069bb2f38125ec3fc067198852
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-08-15 04:57:32 +0200

    rework approach for generating cascade group internal queries

--- src/platform/plugins/shared/discover/public/application/context/components/grouping/data_cascade/util.ts
+++ src/platform/plugins/shared/discover/public/application/context/components/grouping/data_cascade/util.ts
@@ -8,16 +8,21 @@
  */
 
 import { type AggregateQuery } from '@kbn/es-query';
-import { mutate, BasicPrettyPrinter, Builder, EsqlQuery } from '@kbn/esql-ast';
 import {
-  isESQLAstBaseItem,
-  isESQLFunction,
+  BasicPrettyPrinter,
+  Builder,
+  EsqlQuery,
+  isColumn,
+  mutate,
+  type ESQLCommand,
   type ESQLColumn,
   type ESQLCommandOption,
-} from '@kbn/esql-ast/src/types';
+} from '@kbn/esql-ast';
+import { isESQLAstBaseItem, isESQLFunction } from '@kbn/esql-ast/src/types';
 
-type SupportedOperations = 'stats' | 'categorize';
 type NodeType = 'group' | 'leaf';
+
+type StatsCommand = ESQLCommand<'stats'>;
 
 const isESQLCommandOption = (node: unknown): node is ESQLCommandOption =>
   isESQLAstBaseItem(node) && (node as ESQLCommandOption).type === 'option';
@@ -34,16 +39,13 @@ export const getESQLStatsQueryMeta = (queryString: string) => {
   const esqlQuery = EsqlQuery.fromSrc(queryString);
 
   Array.from(mutate.commands.stats.list(esqlQuery.ast)).forEach((statsCommand) => {
-    statsCommand.args.forEach((statsArgNode) => {
+    (statsCommand as StatsCommand).args.forEach((statsArgNode) => {
       if (isESQLFunction(statsArgNode)) {
         const appliedFunctionMeta: Partial<AppliedStatsFunction> = {};
 
         statsArgNode.args.forEach((argNode) => {
-          if (
-            Object.prototype.toString.call(argNode) === '[object Object]' &&
-            (argNode as ESQLColumn).type === 'column'
-          ) {
-            appliedFunctionMeta.identifier = (argNode as ESQLColumn).text;
+          if (isColumn(argNode)) {
+            appliedFunctionMeta.identifier = argNode.text;
           } else if (Array.isArray(argNode)) {
             argNode.forEach((node) => {
               if (isESQLFunction(node)) {
@@ -58,8 +60,8 @@ export const getESQLStatsQueryMeta = (queryString: string) => {
         });
       } else if (isESQLCommandOption(statsArgNode) && statsArgNode.name === 'by') {
         statsArgNode.args.forEach((byOptionNode) => {
-          if ((byOptionNode as ESQLColumn).type === 'column') {
-            groupByFields.push((byOptionNode as ESQLColumn).text);
+          if (isColumn(byOptionNode)) {
+            groupByFields.push(byOptionNode.text);
           } else if (isESQLFunction(byOptionNode)) {
             groupByFields.push(byOptionNode.text);
           }
@@ -72,61 +74,115 @@ export const getESQLStatsQueryMeta = (queryString: string) => {
 };
 
 export interface CascadeQueryArgs {
+  /**
+   * anchor query for generating the next valid query
+   */
   query: AggregateQuery;
-  operation: SupportedOperations;
+  /**
+   * Node type (group or leaf) for which we are constructing the cascade query
+   */
   nodeType: NodeType;
+  /**
+   * Node path for the current node in the cascade experience we'd like to generate a query for
+   */
   nodePath: string[];
+  /**
+   * Mapping of node paths to their corresponding values, used to populate the query with literal values
+   */
   nodePathMap: Record<string, string>;
 }
 
+/**
+ * Constructs a cascade query from the provided query, based on the node type, node path and node path map.
+ */
 export const constructCascadeQuery = ({
   query,
-  operation,
   nodeType,
   nodePath,
   nodePathMap,
-}: CascadeQueryArgs) => {
+}: CascadeQueryArgs): AggregateQuery => {
   const ESQLQuery = EsqlQuery.fromSrc(query.esql);
 
-  const operationCheck = ESQLQuery.ast.commands.find((cmd) => cmd.name === operation);
+  const statsCommands = Array.from(mutate.commands.stats.list(ESQLQuery.ast));
 
-  if (!operationCheck) {
-    throw new Error(`Operation "${operation}" not found in query`);
+  if (statsCommands.length === 0) {
+    throw new Error(`Query does not include a "stats" command`);
   }
 
-  switch (operation) {
-    case 'stats': {
-      return nodeType === 'leaf'
-        ? handleLeafStatsOperation(ESQLQuery, nodePathMap)
-        : handleGroupStatsOperation(ESQLQuery, nodePathMap);
-    }
+  // Attempt to handle every stats query present appropriately
+  statsCommands.forEach((statsCommand) => {
+    (statsCommand as StatsCommand).args.forEach((statsArgNode) => {
+      if (isESQLCommandOption(statsArgNode) && statsArgNode.name === 'by') {
+        let hasMultipleColumns: boolean;
 
-    case 'categorize': {
-      // Handle categorize operation
-      return handleCategorizeOperation(ESQLQuery, nodePathMap);
-    }
+        statsArgNode.args.forEach((byOptionArg, _, args) => {
+          if (isColumn(byOptionArg) && nodePath.includes(byOptionArg.name)) {
+            if (nodeType === 'leaf') {
+              handleStatsByColumnLeafOperation(ESQLQuery, {
+                [byOptionArg.name]: nodePathMap[byOptionArg.name],
+              });
+            } else if (
+              nodeType === 'group' &&
+              args.length > 1 &&
+              // it's not enough to check that we have multiple args for the stats by options
+              (typeof hasMultipleColumns === 'undefined'
+                ? (hasMultipleColumns = args.filter((arg) => isColumn(arg)).length > 1)
+                : hasMultipleColumns)
+            ) {
+              handleStatsByColumnGroupOperation(
+                ESQLQuery,
+                statsCommand as StatsCommand,
+                byOptionArg,
+                {
+                  [byOptionArg.name]: nodePathMap[byOptionArg.name],
+                }
+              );
+            }
+          } else if (isESQLFunction(byOptionArg)) {
+            switch (byOptionArg.name) {
+              case 'categorize': {
+                // TODO: handle categorize option
+                break;
+              }
+              case 'bucket': {
+                // TODO: handle bucket option
+                break;
+              }
+              default: {
+                // unsupported by function, nothing to do here
+                break;
+              }
+            }
+          }
+        });
+      }
+    });
 
-    default: {
-      throw new Error(`Unsupported operation: ${operation}`);
-    }
-  }
+    // Add check to only remove the stats command if it has been fully handled
+    mutate.generic.commands.remove(ESQLQuery.ast, statsCommand);
+  });
+
+  // open question: should we remove the limit command as well, seems a little naive to assume it's always safe?
+  const limitCommands = Array.from(mutate.commands.limit.list(ESQLQuery.ast));
+
+  limitCommands.forEach((cmd) => {
+    mutate.generic.commands.remove(ESQLQuery.ast, cmd);
+  });
+
+  return {
+    esql: BasicPrettyPrinter.print(ESQLQuery.ast),
+  };
 };
 
 /**
- * @description Handles fetching leaf node data for stats operation in the data cascade experience
+ * @description adds a where command with current value for a matched column option as a side-effect on the passed query,
+ * helps us with fetching leaf node data for stats operation in the data cascade experience.
  */
-function handleLeafStatsOperation(query: EsqlQuery, nodePathMap: Record<string, string>) {
-  // extracts commands we'd like to remove from the query, if exist 'stats' and 'limit'
-  const commandsToRemove = ['stats' as const, 'limit' as const].flatMap((operation) => {
-    return Array.from(mutate.commands[operation].list(query.ast));
-  });
-
-  commandsToRemove.forEach((cmd) => {
-    mutate.generic.commands.remove(query.ast, cmd);
-  });
-
-  // Add where command with current value
-  const newCommands = Object.entries(nodePathMap).map(([key, value]) => {
+function handleStatsByColumnLeafOperation(
+  query: EsqlQuery,
+  columnInterpolationRecord: Record<string, string>
+) {
+  const newCommands = Object.entries(columnInterpolationRecord).map(([key, value]) => {
     return Builder.command({
       name: 'where',
       args: [
@@ -143,21 +199,20 @@ function handleLeafStatsOperation(query: EsqlQuery, nodePathMap: Record<string, 
   newCommands.forEach((command, idx) => {
     mutate.generic.commands.insert(query.ast, command, 1 + idx);
   });
-
-  return BasicPrettyPrinter.print(query.ast);
 }
 
-function handleGroupStatsOperation(query: EsqlQuery, nodePathMap: Record<string, string>) {
-  const commandsToRemove = ['limit' as const].flatMap((operation) => {
-    return Array.from(mutate.commands[operation].list(query.ast));
-  });
-
-  commandsToRemove.forEach((cmd) => {
-    mutate.generic.commands.remove(query.ast, cmd);
-  });
+function handleStatsByColumnGroupOperation(
+  query: EsqlQuery,
+  statsCommand: StatsCommand,
+  byOptionArg: ESQLColumn,
+  columnInterpolationRecord: Record<string, string>
+) {
+  // TODO: ideally we'd want to modify the passed stats command,
+  // especially if for some reason it contains the column we want to group by
+  // we should remove it from the stats command
 
   // Add where command with current value
-  const newCommands = Object.entries(nodePathMap).map(([key, value]) => {
+  const newCommands = Object.entries(columnInterpolationRecord).map(([key, value]) => {
     return Builder.command({
       name: 'where',
       args: [
@@ -174,10 +229,18 @@ function handleGroupStatsOperation(query: EsqlQuery, nodePathMap: Record<string,
   newCommands.forEach((command, idx) => {
     mutate.generic.commands.insert(query.ast, command, 1 + idx);
   });
-
-  return BasicPrettyPrinter.print(query.ast);
 }
 
-function handleCategorizeOperation(query: EsqlQuery, nodePathMap: Record<string, string>) {
+function handleStatsByCategorizeLeafOperation(
+  query: EsqlQuery,
+  nodePathMap: Record<string, string>
+) {
+  throw new Error('Not yet implemented!');
+}
+
+function handleStatsByCategorizeGroupOperation(
+  query: EsqlQuery,
+  nodePathMap: Record<string, string>
+) {
   throw new Error('Not yet implemented!');
 }
