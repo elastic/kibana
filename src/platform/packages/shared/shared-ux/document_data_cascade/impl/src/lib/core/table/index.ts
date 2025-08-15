fcfcf1ea94b874abb238854c165772b667f0db73
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-08-15 21:28:42 +0200

    switch from disabling rows to automatically closing open row on toggling new row

--- src/platform/packages/shared/shared-ux/document_data_cascade/impl/src/lib/core/table/index.ts
+++ src/platform/packages/shared/shared-ux/document_data_cascade/impl/src/lib/core/table/index.ts
@@ -12,25 +12,109 @@ import {
   getCoreRowModel,
   getExpandedRowModel,
   useReactTable,
+  type Table,
   type TableOptions,
   type ColumnDef,
 } from '@tanstack/react-table';
-import type { GroupNode } from '../../../store_provider';
+import { useRef } from 'react';
+import {
+  LeafNode,
+  useDataCascadeActions,
+  useDataCascadeState,
+  type GroupNode,
+} from '../../../store_provider';
 
 interface TableProps<G>
-  extends Omit<TableOptions<G>, 'columns' | 'data' | 'getCoreRowModel' | 'getExpandedRowModel'> {
+  extends Omit<
+    TableOptions<G>,
+    | 'columns'
+    | 'data'
+    | 'state'
+    | 'getRowId'
+    | 'getSubRows'
+    | 'getCoreRowModel'
+    | 'getExpandedRowModel'
+    | 'onExpandedChange'
+    | 'getRowCanExpand'
+  > {
   columns: (columnsHelper: ReturnType<typeof createColumnHelper<G>>) => ColumnDef<G>[];
-  data: Array<G>;
+  allowExpandMultiple: boolean;
 }
 
-export const useTableHelper = <G extends GroupNode>({ columns, data, ...rest }: TableProps<G>) => {
+export const useTableHelper = <G extends GroupNode, L extends LeafNode>({
+  columns,
+  allowExpandMultiple,
+  ...rest
+}: TableProps<G>) => {
+  const tableRef = useRef<Table<G>>();
   const columnHelper = createColumnHelper<G>();
+  const actions = useDataCascadeActions<G, L>();
+  const state = useDataCascadeState<G, L>();
 
-  return useReactTable<G>({
-    data,
+  tableRef.current = useReactTable<G>({
+    data: state.groupNodes,
+    state: state.table,
     columns: columns(columnHelper),
     getCoreRowModel: getCoreRowModel(),
     getExpandedRowModel: getExpandedRowModel(),
+    getRowCanExpand: () => true,
+    onExpandedChange: (updater) => {
+      const newExpandedState =
+        typeof updater === 'function' ? updater(state.table.expanded) : updater;
+
+      if (allowExpandMultiple) {
+        return actions.setExpandedRows(newExpandedState);
+      }
+
+      const previousExpandedRows = Object.keys(state.table.expanded ?? {});
+      const newlyExpandedRows = Object.keys(newExpandedState ?? {});
+
+      // escape early if it's just one row that is expanded, or no rows are expanded
+      if (newlyExpandedRows.length <= 1) {
+        return actions.setExpandedRows(newExpandedState);
+      }
+
+      // Compute the rows to keep, comparing the newly expanded rows with the previous expanded rows
+      const rowsToKeep = newlyExpandedRows.reduce((acc, id) => {
+        const row = tableRef.current!.getRow?.(id);
+
+        if (
+          // when the row is root, and its id is not in previousExpandedRows we want to keep it
+          (!row?.parentId && !previousExpandedRows.includes(row?.id ?? '')) ||
+          // when row is a child, and its parentId is not in previousExpandedRows we want to keep it
+          (row?.parentId && !previousExpandedRows.includes(row?.parentId))
+        ) {
+          acc[id] = true;
+        } else if (row?.parentId && previousExpandedRows.includes(row?.parentId)) {
+          // when row is a child, and its parent id is in previous expanded row, we need to check if it has a sibling then apply a fitting treatment
+          const siblings = tableRef.current!.getRow?.(row?.parentId)?.getLeafRows() ?? [];
+          const expandedRowSibling = siblings.find(
+            (sibling) => newlyExpandedRows.includes(sibling.id) && sibling.id !== id
+          );
+
+          if (!expandedRowSibling) {
+            acc[id] = true;
+            acc[row.parentId] = true; // keep the parent as well
+          } else if (expandedRowSibling && !previousExpandedRows.includes(id)) {
+            // keep the row if it has a sibling that is expanded but it was not in previousExpandedRows
+            acc[id] = true;
+            acc[row.parentId] = true; // keep the parent as well
+          }
+        }
+
+        return acc;
+      }, {} as Record<string, boolean>);
+
+      return actions.setExpandedRows(rowsToKeep);
+    },
+    getRowId: (rowData) => rowData.id,
+    getSubRows: (row) => row.children as G[],
     ...rest,
   });
+
+  return {
+    get table() {
+      return tableRef.current!;
+    },
+  };
 };
