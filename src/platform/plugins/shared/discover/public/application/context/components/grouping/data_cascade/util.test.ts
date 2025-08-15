6fa4ec45f855fe9a1979792bc7965bf45c512c02
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-08-15 04:57:32 +0200

    rework approach for generating cascade group internal queries

---
+++ src/platform/plugins/shared/discover/public/application/context/components/grouping/data_cascade/util.test.ts
@@ -0,0 +1,91 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { getESQLStatsQueryMeta, constructCascadeQuery } from './util';
+
+describe('utils', () => {
+  describe('getESQLStatsQueryMeta', () => {
+    it('should return an array of the columns the query has been denoted to be grouped by with the STATS command', () => {
+      const queryString = `
+    FROM kibana_sample_data_logs, another_index
+    | KEEP bytes, clientip, url.keyword, response.keyword
+    | STATS Visits = COUNT(), Unique = COUNT_DISTINCT(clientip),
+        p95 = PERCENTILE(bytes, 95), median = MEDIAN(bytes)
+            BY type, url.keyword
+    | EVAL total_records = TO_DOUBLE(count_4xx + count_5xx + count_rest)
+    | DROP count_4xx, count_rest, total_records
+    | LIMIT 123`;
+
+      const result = getESQLStatsQueryMeta(queryString);
+      expect(result.groupByFields).toEqual(['type', 'url.keyword']);
+      expect(result.appliedFunctions).toEqual([
+        { identifier: 'Visits', operator: 'COUNT' },
+        { identifier: 'Unique', operator: 'COUNT_DISTINCT' },
+        { identifier: 'p95', operator: 'PERCENTILE' },
+        { identifier: 'median', operator: 'MEDIAN' },
+      ]);
+    });
+  });
+
+  describe('constructCascadeQuery', () => {
+    describe('query with single stats command', () => {
+      describe('column options', () => {
+        it('should construct a valid cascade leaf query', () => {
+          const query = {
+            esql: `
+          FROM kibana_sample_data_logs
+          | STATS count() BY clientip
+          | LIMIT 100
+          `,
+          };
+
+          const nodeType = 'leaf';
+          const nodePath = ['clientip'];
+          const nodePathMap = { clientip: '192.168.1.1' };
+
+          const result = constructCascadeQuery({
+            query,
+            nodeType,
+            nodePath,
+            nodePathMap,
+          });
+
+          expect(result).toEqual({
+            esql: "FROM kibana_sample_data_logs | WHERE clientip == '192.168.1.1'",
+          });
+        });
+
+        it('returns the query as is when there is only one column specified for the stats by option', () => {
+          const query = {
+            esql: `
+          FROM kibana_sample_data_logs
+          | STATS count() BY clientip
+          | LIMIT 100
+          `,
+          };
+
+          const nodeType = 'group';
+          const nodePath = ['clientip'];
+          const nodePathMap = { clientip: '192.168.1.1' };
+
+          const result = constructCascadeQuery({
+            query,
+            nodeType,
+            nodePath,
+            nodePathMap,
+          });
+
+          expect(result).toEqual({
+            esql: 'FROM kibana_sample_data_logs | STATS count() BY clientip | LIMIT 100',
+          });
+        });
+      });
+    });
+  });
+});
