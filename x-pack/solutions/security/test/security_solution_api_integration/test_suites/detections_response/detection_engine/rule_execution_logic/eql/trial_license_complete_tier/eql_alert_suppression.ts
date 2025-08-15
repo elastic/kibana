fcd66fd1a30141ed5b59de0f2b6e91977f1c97ee
Nikita Khristinin <nkhristinin@gmail.com>
2025-08-15 15:29:50 +0200

    remove only

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/eql/trial_license_complete_tier/eql_alert_suppression.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/detections_response/detection_engine/rule_execution_logic/eql/trial_license_complete_tier/eql_alert_suppression.ts
@@ -73,9 +73,7 @@ export default ({ getService }: FtrProviderContext) => {
   ) => partition(alerts, (alert) => alert?._source?.['kibana.alert.group.index'] == null);
 
   // NOTE: Add to second quality gate after feature is GA
-  // Failing: See https://github.com/elastic/kibana/issues/202940
-  // Failing: See https://github.com/elastic/kibana/issues/202940
-  describe.only('@ess @serverless Alert Suppression for EQL rules', () => {
+  describe('@ess @serverless Alert Suppression for EQL rules', () => {
     before(async () => {
       await esArchiver.load(
         'x-pack/solutions/security/test/fixtures/es_archives/security_solution/ecs_compliant'
