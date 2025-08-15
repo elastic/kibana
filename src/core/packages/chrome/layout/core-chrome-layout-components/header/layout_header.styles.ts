elastic/kibana

commit bca77cec607a619268703f21760be4585d677053

[Workspace Chrome] fix timelines modal for new layout (#226995)
## Summary

Part of https://github.com/elastic/kibana/issues/225263

We're preparing Kibana for new layout (available behind feature flag)
and currently addressing obvious bugs that appear in new layout.

to enable new layout: 
```
feature_flags.overrides:
  core.chrome.layoutType: 'grid'
```

Ideally needs to be tested in all: 
- [x] old layout + classic nav; 
- [x] old layout + solution nav; 
- [x] new layout + classic nav; 
- [x] new layout + solution nav

**new layout before the fix:**

![Screenshot 2025-07-08 at 13 49
44](https://github.com/user-attachments/assets/6a824ace-08c3-4bff-82e3-6be552a412d5)

The reason for the bug in the new layout `euiFixedHeadersOffset` is 0
because there are no longer fixed headers in the new layout. So we have
to adjust for new layout variables usage


**after the fix:**

![Screenshot 2025-07-08 at 13 49
11](https://github.com/user-attachments/assets/943d402e-82f4-4bfa-89ed-40a88e852731)

src/core/packages/chrome/layout/core-chrome-layout/layouts/legacy-fixed/legacy_fixed_global_app_style.tsx
@@ -70,6 +70,12 @@ const globalLayoutStyles = (euiTheme: UseEuiTheme['euiTheme']) => css`
     --kbn-application--sticky-headers-offset: calc(
       var(--euiFixedHeadersOffset, 0px) + var(--kbn-application--top-bar-height, 0px)
     );
+
+    // for forward compatibility with grid layout,
+    --kbn-application--content-top: var(--kbnAppHeadersOffset, var(--euiFixedHeadersOffset, 0));
+    --kbn-application--content-left: 0px;
+    --kbn-application--content-bottom: 0px;
+    --kbn-application--content-right: 0px;
   }
 
   // Conditionally override :root CSS fixed header variable. Updating \`--euiFixedHeadersOffset\`

x-pack/solutions/security/plugins/security_solution/public/timelines/components/modal/index.styles.tsx
@@ -23,10 +23,10 @@ export const usePaneStyles = () => {
   return css`
     // euiOverlayMask styles
     position: fixed;
-    top: var(--euiFixedHeadersOffset, 0);
-    left: 0;
-    right: 0;
-    bottom: 0;
+    top: var(--kbn-application--content-top, 0px);
+    left: var(--kbn-application--content-left, 0px);
+    right: var(--kbn-application--content-right, 0px);
+    bottom: var(--kbn-application--content-bottom, 0px);
     // TODO EUI: add color with transparency
     background: ${transparentize(euiTheme.colors.ink, 0.5)};
     z-index: ${(euiTheme.levels.flyout as number) +
@@ -43,10 +43,10 @@ export const usePaneStyles = () => {
     .timeline-container {
       min-width: 150px;
       position: fixed;
-      top: var(--euiFixedHeadersOffset, 0);
-      right: 0;
-      bottom: 0;
-      left: 0;
+      top: var(--kbn-application--content-top, 0px);
+      left: var(--kbn-application--content-left, 0px);
+      right: var(--kbn-application--content-right, 0px);
+      bottom: var(--kbn-application--content-bottom, 0px);
       background: ${euiBackgroundColor(EuiTheme, 'plain')};
       ${euiCanAnimate} {
         animation: ${euiAnimSlideInUp(euiTheme.size.xxl)} ${euiTheme.animation.normal}

commit 0c741346ec5675915b205a1fcac7ee5417a5aabf

Fix timeline modal fullscreen (#231937)
## Summary

Fix the regression introduced in
https://github.com/elastic/kibana/pull/226995/. The timeline modal with
solution navigation height isn't covering the top menu bar like it did
before.

I tested the fix with all both: 

- classic nav old layout
- solution nav old layout
- classic nav new layout
- solution nav new layout


Issue:

<img width="1040" height="767" alt="9 2 - security - flyout+timeline
png"
src="https://github.com/user-attachments/assets/9c38f5ae-e9cb-4d80-8931-93efc00ae261"
/>


Fixed:

<img width="2399" height="1527" alt="Screenshot 2025-08-15 at 12 29 42"
src="https://github.com/user-attachments/assets/32298356-76f4-4af9-92be-6f48a8e1aaa4"
/>

src/core/packages/chrome/layout/core-chrome-layout-components/header/layout_header.styles.ts
@@ -15,7 +15,7 @@ const root: EmotionFn = ({ euiTheme }) => css`
   grid-area: header;
   height: var(--kbn-layout--header-height);
   max-width: var(--kbn-layout--header-width);
-  z-index: var(--kbn-layout--aboveFlyoutLevel);
+  z-index: 1; /* has to be just above main application content */
 `;
 
 export const styles = {

src/core/packages/chrome/layout/core-chrome-layout/layouts/legacy-fixed/legacy_fixed_global_app_style.tsx
@@ -71,11 +71,20 @@ const globalLayoutStyles = (euiTheme: UseEuiTheme['euiTheme']) => css`
     );
 
     // for forward compatibility with grid layout,
-    --kbn-application--content-height: calc(100vh - var(--kbnAppHeadersOffset, 0px));
-    --kbn-application--content-top: var(--kbnAppHeadersOffset, var(--euiFixedHeadersOffset, 0));
+    // --kbn-layout--application includes everything except chrome's fixed headers
+    // for solution navigation, it also includes the top bar height (action menu)
+    --kbn-layout--application-top: var(--euiFixedHeadersOffset, 0px);
+    --kbn-layout--application-left: 0px;
+    --kbn-layout--application-bottom: 0px;
+    --kbn-layout--application-right: 0px;
+    --kbn-layout--application-height: calc(100vh - var(--kbn-layout--application-top, 0px));
+
+    // --kbn-application--content also excludes the top bar height (action menu)
+    --kbn-application--content-top: var(--kbnAppHeadersOffset, var(--euiFixedHeadersOffset, 0px));
     --kbn-application--content-left: 0px;
     --kbn-application--content-bottom: 0px;
     --kbn-application--content-right: 0px;
+    --kbn-application--content-height: calc(100vh - var(--kbn-application--content-top, 0px));
   }
 
   // Conditionally override :root CSS fixed header variable. Updating \`--euiFixedHeadersOffset\`

x-pack/solutions/security/plugins/security_solution/public/timelines/components/modal/index.styles.tsx
@@ -23,10 +23,10 @@ export const usePaneStyles = () => {
   return css`
     // euiOverlayMask styles
     position: fixed;
-    top: var(--kbn-application--content-top, 0px);
-    left: var(--kbn-application--content-left, 0px);
-    right: var(--kbn-application--content-right, 0px);
-    bottom: var(--kbn-application--content-bottom, 0px);
+    top: var(--kbn-layout--application-top, 0px);
+    left: var(--kbn-layout--application-left, 0px);
+    right: var(--kbn-layout--application-right, 0px);
+    bottom: var(--kbn-layout--application-bottom, 0px);
     // TODO EUI: add color with transparency
     background: ${transparentize(euiTheme.colors.ink, 0.5)};
     z-index: ${(euiTheme.levels.flyout as number) +
@@ -43,10 +43,10 @@ export const usePaneStyles = () => {
     .timeline-container {
       min-width: 150px;
       position: fixed;
-      top: var(--kbn-application--content-top, 0px);
-      left: var(--kbn-application--content-left, 0px);
-      right: var(--kbn-application--content-right, 0px);
-      bottom: var(--kbn-application--content-bottom, 0px);
+      top: var(--kbn-layout--application-top, 0px);
+      left: var(--kbn-layout--application-left, 0px);
+      right: var(--kbn-layout--application-right, 0px);
+      bottom: var(--kbn-layout--application-bottom, 0px);
       background: ${euiBackgroundColor(EuiTheme, 'plain')};
       ${euiCanAnimate} {
         animation: ${euiAnimSlideInUp(euiTheme.size.xxl)} ${euiTheme.animation.normal}