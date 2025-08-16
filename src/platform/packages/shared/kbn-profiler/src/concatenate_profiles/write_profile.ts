2e494f516f157e10edacb5d1ba8c368da21e1ae8
Dario Gieselaar <dario.gieselaar@elastic.co>
2025-08-16 16:40:21 +0200

    @kbn/bench: Benchmarking for the masses

---
+++ src/platform/packages/shared/kbn-profiler/src/concatenate_profiles/write_profile.ts
@@ -0,0 +1,45 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { createWriteStream } from 'fs';
+import type { CpuProfile } from './types';
+
+export async function writeProfile(path: string, profile: CpuProfile): Promise<void> {
+  await new Promise<void>((resolve, reject) => {
+    const ws = createWriteStream(path, { encoding: 'utf8' });
+    ws.on('error', reject);
+    ws.on('finish', () => resolve());
+
+    ws.write('{');
+    ws.write('"nodes":[');
+    profile.nodes.forEach((n, i) => {
+      if (i) ws.write(',');
+      ws.write(JSON.stringify(n));
+    });
+    ws.write('],');
+    ws.write('"samples":[');
+    profile.samples.forEach((s, i) => {
+      if (i) ws.write(',');
+      ws.write(String(s));
+    });
+    ws.write('],');
+    ws.write('"timeDeltas":[');
+    profile.timeDeltas.forEach((d, i) => {
+      if (i) ws.write(',');
+      ws.write(String(d));
+    });
+    if (typeof profile.startTime === 'number') ws.write(`],"startTime":${profile.startTime}`);
+    else ws.write('],"startTime":0');
+    if (typeof profile.endTime === 'number') ws.write(`,"endTime":${profile.endTime}`);
+    else ws.write(',"endTime":0');
+    if (typeof profile.title === 'string') ws.write(`,"title":${JSON.stringify(profile.title)}`);
+    ws.write('}');
+    ws.end();
+  });
+}
