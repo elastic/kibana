a19b5f8f932177f2038508a7b4fad3fc11bf621b
Alexey Antonov <alexwizp@gmail.com>
2025-08-19 14:51:59 +0300

    Update src/platform/test/functional/apps/context/_context_accessibility.ts
    Co-authored-by: Julia Rechkunova <julia.rechkunova@gmail.com>

--- src/platform/test/functional/apps/context/_context_accessibility.ts
+++ src/platform/test/functional/apps/context/_context_accessibility.ts
@@ -38,7 +38,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       const rowActions = await dataGrid.getRowActions();
       await rowActions[1].click();
       await PageObjects.header.waitUntilLoadingHasFinished();
-      // Skip tp main content button
+      // Skip to main content button
       await browser.pressKeys(browser.keys.TAB);
       await browser.pressKeys(browser.keys.ENTER);
       await browser.pressKeys(browser.keys.TAB);
