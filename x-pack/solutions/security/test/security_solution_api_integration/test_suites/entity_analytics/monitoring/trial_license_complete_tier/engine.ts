5adff0ebcaae9d6c2c5f433cb0a050e4916e8c48
Mark Hopkin <mark.hopkin@elastic.co>
2025-08-20 16:18:24 +0100

    remove unused code

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/monitoring/trial_license_complete_tier/engine.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/monitoring/trial_license_complete_tier/engine.ts
@@ -17,7 +17,6 @@ export default ({ getService }: FtrProviderContext) => {
   const privMonUtils = PrivMonUtils(getService);
   const log = getService('log');
   const es = getService('es');
-  const retry = getService('retry');
   const spaces = getService('spaces');
   const customSpace = 'privmontestspace';
   const supertest = getService('supertest');
@@ -42,13 +41,6 @@ export default ({ getService }: FtrProviderContext) => {
           },
         },
       },
-    });
-
-  const waitForPrivMonUsersToBeSynced = async (length = 1) =>
-    retry.waitForWithTimeout('Wait for PrivMon users to be synced', 90000, async () => {
-      const res = await api.listPrivMonUsers({ query: {} });
-      log.info(`PrivMon users sync check: found ${res.body.length} users`);
-      return res.body.length >= length; // wait until we have at least one user
     });
 
   async function getPrivMonSoStatus(space: string = 'default') {
