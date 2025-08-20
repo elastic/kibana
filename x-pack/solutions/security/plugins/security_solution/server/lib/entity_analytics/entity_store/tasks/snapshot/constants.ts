6b867ae282b7d71fa27ba4467f5af0ce30139e24
kubasobon <kuba.sobon@elastic.co>
2025-08-20 16:47:56 +0200

    clarify frequency

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/entity_store/tasks/snapshot/constants.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/entity_store/tasks/snapshot/constants.ts
@@ -14,6 +14,7 @@ export const VERSION = '1.0.0';
 export const TIMEOUT = '1h';
 export const MAX_ATTEMPTS = 5;
 export const MAX_CONCURRENCY = 1;
+// Run every day at 00:01 UTC
 export const SCHEDULE = {
   rrule: {
     freq: Frequency.DAILY,
