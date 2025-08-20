b7486c50fafe831cdd37e70a6be4b9b7a1ede1de
Michael Dokolin <mikhail.dokolin@elastic.co>
2025-08-20 16:01:24 +0200

    Add the management service binding in the container module

--- src/platform/plugins/shared/saved_objects_management/server/module.ts
+++ src/platform/plugins/shared/saved_objects_management/server/module.ts
@@ -11,9 +11,11 @@ import { ContainerModule } from 'inversify';
 import { CapabilitiesProvider, Route } from '@kbn/core-di-server';
 import { capabilitiesProvider } from './capabilities_provider';
 import { BulkDeleteRoute, GetAllowedTypesRoute, ScrollCountRoute } from './routes';
+import { SavedObjectsManagement } from './services';
 
 export const module = new ContainerModule(({ bind }) => {
   bind(CapabilitiesProvider).toConstantValue(capabilitiesProvider);
+  bind(SavedObjectsManagement).toSelf().inSingletonScope();
   bind(Route).toConstantValue(BulkDeleteRoute);
   bind(Route).toConstantValue(GetAllowedTypesRoute);
   bind(Route).toConstantValue(ScrollCountRoute);
