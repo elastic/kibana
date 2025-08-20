460f5f5601603f9cb32636fe4c8ff45b96fde107
Gergő Ábrahám <gergo.abraham@elastic.co>
2025-08-20 14:29:42 +0200

    add cypress tests

--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/artifacts/endpoint_exceptions.no_ff.cy.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/artifacts/endpoint_exceptions.no_ff.cy.ts
@@ -6,6 +6,7 @@
  */
 import * as essSecurityHeaders from '@kbn/test-suites-xpack-security/security_solution_cypress/cypress/screens/security_header';
 import * as serverlessSecurityHeaders from '@kbn/test-suites-xpack-security/security_solution_cypress/cypress/screens/serverless_security_header';
+import { APP_ENDPOINT_EXCEPTIONS_PATH, APP_PATH } from '../../../../../common/constants';
 import { login, ROLE } from '../../tasks/login';
 
 describe('Endpoint exceptions - preserving behaviour without `endpointExceptionsMovedUnderManagement` feature flag', () => {
@@ -18,24 +19,36 @@ describe('Endpoint exceptions - preserving behaviour without `endpointExceptions
 
     it('should not display Endpoint Exceptions in Manage side panel', () => {
       login(ROLE.t1_analyst);
-      cy.visit('app/security');
+      cy.visit(APP_PATH);
 
       essSecurityHeaders.openNavigationPanelFor(essSecurityHeaders.ENDPOINT_EXCEPTIONS);
       cy.getByTestSubj('solutionSideNavPanel')
         .find(essSecurityHeaders.ENDPOINT_EXCEPTIONS)
         .should('not.exist');
     });
+
+    it('should display Not Found page when opening url directly', () => {
+      login(ROLE.t1_analyst);
+      cy.visit(APP_ENDPOINT_EXCEPTIONS_PATH);
+      cy.getByTestSubj('notFoundPage').should('exist');
+    });
   });
 
   describe('Serverless', { tags: ['@serverless', '@skipInServerlessMKI'] }, () => {
     it('should not display Endpoint Exceptions in Assets side panel ', () => {
       login(ROLE.t1_analyst);
-      cy.visit('app/security');
+      cy.visit(APP_PATH);
 
       serverlessSecurityHeaders.openNavigationPanelFor(
         serverlessSecurityHeaders.ENDPOINT_EXCEPTIONS
       );
       cy.get(serverlessSecurityHeaders.ENDPOINT_EXCEPTIONS).should('not.exist');
+    });
+
+    it('should display Not Found page when opening url directly', () => {
+      login(ROLE.t1_analyst);
+      cy.visit(APP_ENDPOINT_EXCEPTIONS_PATH);
+      cy.getByTestSubj('notFoundPage').should('exist');
     });
   });
 });
