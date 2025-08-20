e301a03fe9072b8eb359fb162ec742021126f8e0
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-08-20 16:29:36 +0200

    [8.19][ska] relocate kubernetes_security (#232381)
    ## Summary
    
    Part of https://github.com/elastic/kibana-team/issues/1503
    
    This PR is 8.19 branch only:
    
    Before:
    ```
    /x-pack/test/
         | - kubernetes_security/
    ```
    
    After: 
    ```
    /x-pack/solutions/security/test/
         | - kubernetes_security/
    ```
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/solutions/security/test/kubernetes_security/basic/tests/index.ts
+++ x-pack/solutions/security/test/kubernetes_security/basic/tests/index.ts
@@ -7,7 +7,6 @@
 
 import { FtrProviderContext } from '../../common/ftr_provider_context';
 
-// eslint-disable-next-line import/no-default-export
 export default function kubernetesSecurityApiIntegrationTests({
   loadTestFile,
 }: FtrProviderContext) {
