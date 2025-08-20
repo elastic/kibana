d4bca81ba0d64849e37d4b0c05231e78b1236092
Abdul Zahid <awahab07@yahoo.com>
2025-08-20 14:28:05 +0200

    Tests for ES|QL transpiler.

---
+++ x-pack/platform/packages/shared/kbn-streamlang/src/transpilers/esql/index.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { pipe } from 'fp-ts/function';
+import type { StreamlangDSL } from '../../../types/streamlang';
+import { flattenSteps } from '../shared/flatten_steps';
+import { convertStreamlangDSLActionsToEsql } from './conversions';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface EsqlTranspilationOptions {}
+
+export const transpile = (
+  streamlang: StreamlangDSL,
+  transpilationOptions?: EsqlTranspilationOptions
+) => {
+  const processors = pipe(flattenSteps(streamlang.steps), (steps) =>
+    convertStreamlangDSLActionsToEsql(steps, transpilationOptions)
+  );
+
+  return {
+    processors,
+  };
+};
