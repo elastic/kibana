2bbaaf86d569e9e93b0d65c2e8ed00ddc78b120a
Sean Rathier <sean.rathier@elastic.co>
2025-08-20 17:48:33 -0400

    lint and type checks

--- x-pack/solutions/security/plugins/security_solution/public/asset_inventory/components/fleet_extensions/constants.ts
+++ x-pack/solutions/security/plugins/security_solution/public/asset_inventory/components/fleet_extensions/constants.ts
@@ -9,4 +9,6 @@ export const CLOUDBEAT_AWS = 'cloudbeat/asset_inventory_aws';
 export const CLOUDBEAT_AZURE = 'cloudbeat/asset_inventory_azure';
 export const CLOUDBEAT_GCP = 'cloudbeat/asset_inventory_gcp';
 
+export const SUPPORTED_CLOUDBEAT_INPUTS = [CLOUDBEAT_AWS, CLOUDBEAT_GCP, CLOUDBEAT_AZURE] as const;
+
 export const ASSET_POLICY_TEMPLATE = 'asset_inventory';
