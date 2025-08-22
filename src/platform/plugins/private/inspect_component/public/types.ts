a1011c559d5339ee9c3ce10dd7ade7d87d390026
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-08-22 23:51:14 +0200

    Add interface docs

--- src/platform/plugins/private/inspect_component/public/types.ts
+++ src/platform/plugins/private/inspect_component/public/types.ts
@@ -10,76 +10,157 @@
 import type { CoreStart, OverlayRef } from '@kbn/core/public';
 import type { Dispatch, SetStateAction } from 'react';
 
-export interface GetElementFromPointOptions {
-  event: PointerEvent;
-  overlayId: string;
+/**
+ * Response type for POST "/internal/inspect_component/inspect" route.
+ */
+export interface InspectComponentResponse {
+  /** List of all teams who are codeowners for specified file. */
+  codeowners: string[];
+  /** File path relative to the repository root. */
+  relativePath: string;
+  /** File name with extension. */
+  baseFileName: string;
 }
 
+/**
+ * Represents the content of _debugSource property on a React Fiber node.
+ */
 export interface FileData {
+  /** Component definition column number. */
   columnNumber: number;
+  /** Component definition line number. */
   lineNumber: number;
+  /** Full path to the component (including root). */
   fileName: string;
 }
 
-export interface EuiInfo {
-  componentName: string;
-  docsLink: string;
-}
-
-export interface ComponentData extends FileData {
-  codeowners: string[];
-  euiInfo: EuiInfo;
-  iconType?: string;
-  relativePath: string;
-  baseFileName: string;
-  image?: string | null;
-  sourceComponent?: string;
-}
-
+/**
+ * Represents the name information associated with a React Fiber node.
+ */
 export interface ReactFiberName {
+  /** The display name of the component. */
   displayName?: string;
+  /** The actual name of the function or class component. */
   name?: string;
 }
 
+/**
+ * The subset of React Fiber node properties we care about.
+ */
 export interface ReactFiberNode {
+  /** Metadata about the source file where this Fiber was created.
+   * @see {@link FileData}
+   */
   _debugSource?: FileData;
+  /** The Fiber node that created this node. */
   _debugOwner?: ReactFiberNode | null;
+  /** The type of the React element represented by this Fiber node. */
   elementType: string | null;
+  /** The resolved type of the component.
+   * @see {@link ReactFiberName}
+   */
   type: string | ReactFiberName;
 }
 
-export interface GetComponentDataOptions {
-  core: CoreStart;
-  euiInfo: EuiInfo;
-  fileData: FileData;
-  target: HTMLElement | SVGElement;
-  iconType?: string;
-  sourceComponent?: string;
-  setFlyoutOverlayRef: Dispatch<SetStateAction<OverlayRef | undefined>>;
-  setIsInspecting: Dispatch<SetStateAction<boolean>>;
+/**
+ * Represents information about an EUI component.
+ */
+export interface EuiInfo {
+  /** The React component name of this EUI component. */
+  componentName: string;
+  /** Link to the EUI documentation for this EUI component. */
+  docsLink: string;
 }
 
-export interface GetInspectedElementOptions {
-  event: PointerEvent;
-  core: CoreStart;
-  componentPath: string | undefined;
-  overlayId: string;
-  sourceComponent?: string;
-  setFlyoutOverlayRef: Dispatch<SetStateAction<OverlayRef | undefined>>;
-  setIsInspecting: Dispatch<SetStateAction<boolean>>;
-}
-
-export interface InspectComponentResponse {
+/**
+ * Represents information about an component.
+ */
+export interface ComponentData extends FileData, InspectComponentResponse {
+  /** List of all teams who are codeowners for specified file. */
   codeowners: string[];
-  relativePath: string;
-  baseFileName: string;
+  /**
+   * Represents information about an EUI component.
+   * @see {@link EuiInfo}
+   */
+  euiInfo: EuiInfo;
+  /** The EUI name of the icon inside this component. */
+  iconType?: string;
+  /** A base64 encoded image string representing a screenshot of the component. */
+  image?: string | null;
+  /** The name of the top level React component. */
+  sourceComponent?: string;
 }
 
+/**
+ * Options for the `getComponentData` function.
+ */
+export interface GetComponentDataOptions {
+  /** Kibana Core Start services. */
+  core: CoreStart;
+  /**
+   * Represents information about an EUI component.
+   * @see {@link EuiInfo}
+   */
+  euiInfo: EuiInfo;
+  /** Represents the content of _debugSource property on a React Fiber node.
+   * @see {@link FileData}
+   */
+  fileData: FileData;
+  /** The HTML element to target. */
+  target: HTMLElement | SVGElement;
+  /** The EUI name of the icon inside this component. */
+  iconType?: string;
+  /** The name of the top level React component. */
+  sourceComponent?: string;
+  /** React state setter for the current Flyout overlay reference. */
+  setFlyoutOverlayRef: Dispatch<SetStateAction<OverlayRef | undefined>>;
+  /** React state setter for whether the inspect mode is active. */
+  setIsInspecting: Dispatch<SetStateAction<boolean>>;
+}
+
+/**
+ * Parameters for the `getElementFromPoint` function.
+ */
+export interface GetElementFromPointOptions {
+  /** Pointer event from clicking on an component. */
+  event: PointerEvent;
+  /** HTML id for inspect overlay. */
+  overlayId: string;
+}
+
+/**
+ * Parameters for the `getInspectedElementData` function.
+ */
+export interface GetInspectedElementOptions {
+  /** Pointer event from clicking on an component. */
+  event: PointerEvent;
+  /** Kibana Core Start services. */
+  core: CoreStart;
+  /** The component path from the React Fiber node. */
+  componentPath: string | undefined;
+  /** HTML id for inspect overlay. */
+  overlayId: string;
+  /** The name of the top level React component. */
+  sourceComponent?: string;
+  /** React state setter for the current Flyout overlay reference. */
+  setFlyoutOverlayRef: Dispatch<SetStateAction<OverlayRef | undefined>>;
+  /** React state setter for whether the inspect mode is active. */
+  setIsInspecting: Dispatch<SetStateAction<boolean>>;
+}
+
+/**
+ * Represents a link action in the links section of the inspect flyout.
+ */
 export interface ActionLink {
+  /** The URL for the link. */
   href: string;
+  /** The internationalization ID for the link label. */
   i18nId: string;
+  /** EUI icon type for the link component. */
   icon: string;
+  /** Unique id. */
   id: string;
+  /** Visible text for the link. */
   label: string;
 }
 
