b99f9861015bf9b1135004b15321db09f3041b28
Supplementing <mason.d.herron@gmail.com>
2025-08-22 14:39:05 -0600

    fixed another missing type

--- x-pack/solutions/security/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/mocks.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/mocks.tsx
@@ -185,6 +185,7 @@ export const generateFleetPackageInfo = (): PackageInfo => {
         ilm_policy: [],
         data_stream_ilm_policy: [],
         ml_model: [],
+        knowledge_base: [],
       },
     },
     status: 'not_installed',
