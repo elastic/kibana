cf0bb2d6debc93bfffcad342bfb8ea808c42dca6
Faisal Kanout <faisal.kanout@elastic.co>
2025-08-22 17:35:44 +0200

    WIP

--- x-pack/solutions/observability/plugins/synthetics/public/cases/suggestion_component.tsx
+++ x-pack/solutions/observability/plugins/synthetics/public/cases/suggestion_component.tsx
@@ -5,28 +5,68 @@
  * 2.0.
  */
 
+import React, { useEffect } from 'react';
+import { i18n } from '@kbn/i18n';
+import { useSelector, useDispatch } from 'react-redux';
 import type { SuggestionChildrenProps } from '@kbn/cases-plugin/public';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { OverviewStatusMetaData } from '../../common/runtime_types';
-import { SyntheticsSuggestion } from '../../common/types';
+
+import { EuiBadge, EuiFlexItem, EuiSpacer, EuiTitle } from '@elastic/eui';
 import { MetricItem } from '../apps/synthetics/components/monitors_page/overview/overview/metric_item/metric_item';
-import { FlyoutParamProps } from '../apps/synthetics/components/monitors_page/overview/overview/types';
-import { store } from '../apps/synthetics/state';
+import { selectOverviewTrends, trendStatsBatch } from '../apps/synthetics/state';
+import type { OverviewStatusMetaData } from '../../common/runtime_types';
+import type { SyntheticsSuggestion } from '../../common/types';
+import type { FlyoutParamProps } from '../apps/synthetics/components/monitors_page/overview/overview/types';
 export function SyntheticsSuggestionChildren(props: SuggestionChildrenProps<SyntheticsSuggestion>) {
   const { suggestion } = props;
+  const trendData = useSelector(selectOverviewTrends);
+  const syntheticsSuggestions = suggestion.data[0].payload as unknown as OverviewStatusMetaData;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (
+      syntheticsSuggestions &&
+      !trendData[syntheticsSuggestions.configId + syntheticsSuggestions.locationId]
+    ) {
+      dispatch(
+        trendStatsBatch.get([
+          {
+            configId: syntheticsSuggestions.configId,
+            locationId: syntheticsSuggestions.locationId,
+            schedule: syntheticsSuggestions.schedule,
+          },
+        ])
+      );
+    }
+  }, [dispatch, syntheticsSuggestions, trendData]);
   if (suggestion.data.length === 1) {
-    const syntheticsSuggestions = suggestion.data[0].payload as unknown as OverviewStatusMetaData;
     return (
-      <Provider store={store}>
-        <MetricItem
-          key={syntheticsSuggestions.monitorQueryId}
-          monitor={syntheticsSuggestions}
-          onClick={function (params: FlyoutParamProps): void {
-            throw new Error('Function not implemented.');
-          }}
-        />
-      </Provider>
+      <MetricItem
+        key={syntheticsSuggestions.monitorQueryId}
+        monitor={syntheticsSuggestions}
+        onClick={function (params: FlyoutParamProps): void {
+          throw new Error('Function not implemented.');
+        }}
+      />
+      // <div>
+      //   <EuiFlexItem grow={false} style={{ minHight: '300px' }}>
+      //     <EuiTitle>
+      //       <h3>{syntheticsSuggestions.name}</h3>
+      //     </EuiTitle>
+      //     <EuiTitle size="xxs">
+      //       <h4 className="eui-textSubdued">{syntheticsSuggestions.urls}</h4>
+      //     </EuiTitle>
+      //     <EuiTitle size="xxs">
+      //       <h4 className="eui-textSubdued">{syntheticsSuggestions.locationLabel}</h4>
+      //     </EuiTitle>
+      //     <EuiSpacer size="xl" />
+      //     <EuiSpacer size="xl" />
+
+      //     <EuiFlexItem grow={false}>
+      //       <EuiBadge color={syntheticsSuggestions.status === 'up' ? 'success' : 'danger'}>
+      //         {syntheticsSuggestions.status.toUpperCase()}
+      //       </EuiBadge>
+      //     </EuiFlexItem>
+      //   </EuiFlexItem>
+      // </div>
     );
   }
 }
