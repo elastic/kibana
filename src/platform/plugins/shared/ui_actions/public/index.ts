71345a510aa187e214d289e35b6fae3ccae790fb
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-08-22 20:48:12 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- src/platform/plugins/shared/ui_actions/public/index.ts
+++ src/platform/plugins/shared/ui_actions/public/index.ts
@@ -19,7 +19,7 @@ export type {
   UiActionsPublicStart as UiActionsStart,
 } from './plugin';
 export type { UiActionsServiceParams } from './service';
-//export { UiActionsService } from './service';
+// export { UiActionsService } from './service';
 export type { Action, ActionDefinition as UiActionsActionDefinition } from './actions';
 export { ActionInternal, createAction, IncompatibleActionError } from './actions';
 export { buildContextMenuForActions } from './context_menu';
