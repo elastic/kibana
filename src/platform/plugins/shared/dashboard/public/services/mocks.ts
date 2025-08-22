e5b08dab275f0ee49b9f889a0654df27a815a61d
Nathan Reese <reese.nathan@gmail.com>
2025-08-22 11:39:56 -0600

    fix jest tests

--- src/platform/plugins/shared/dashboard/public/services/mocks.ts
+++ src/platform/plugins/shared/dashboard/public/services/mocks.ts
@@ -49,10 +49,16 @@ export const setStubKibanaServices = () => {
   const core = coreMock.createStart();
   (core.application.capabilities as any).dashboard_v2 = defaultDashboardCapabilities;
 
+  const dataServiceMock = dataPluginMock.createStartContract();
+  dataServiceMock.dataViews.getDefaultDataView = jest.fn().mockImplementation(async () => ({
+    isPersisted: () => true, // provide isPersisted implement when not provided by original mock
+    ...(await dataServiceMock.dataViews.getDefaultDataView()),
+  }));
+
   setKibanaServices(core, {
     contentManagement: contentManagementMock.createStartContract(),
     customBranding: customBrandingServiceMock.createStartContract(),
-    data: dataPluginMock.createStartContract(),
+    data: dataServiceMock,
     dataViewEditor: indexPatternEditorPluginMock.createStartContract(),
     embeddable: embeddablePluginMock.createStartContract(),
     fieldFormats: fieldFormatsServiceMock.createStartContract(),
