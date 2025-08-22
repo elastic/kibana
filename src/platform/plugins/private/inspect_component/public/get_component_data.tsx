9d69008d3635bd7fae608095890c889012ce00e6
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-08-22 14:24:48 +0200

    Fix import and tsconfig

--- src/platform/plugins/private/inspect_component/public/get_component_data.tsx
+++ src/platform/plugins/private/inspect_component/public/get_component_data.tsx
@@ -9,7 +9,7 @@
 
 import React from 'react';
 import { toMountPoint } from '@kbn/react-kibana-mount';
-import { capturePreviewScreenshot } from '../../../../packages/shared/shared-ux/preview_screenshots';
+import { capturePreviewScreenshot } from '@kbn/preview-screenshots';
 import { setElementHighlight } from './utils';
 import type { GetComponentDataOptions, InspectComponentResponse } from './types';
 import { flyoutOptions, InspectFlyout } from './inspect';
