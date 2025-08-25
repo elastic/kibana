77664744866c2e6f2771de7209713cc39abf202a
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-08-25 15:57:35 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/platform/plugins/shared/fleet/server/services/cloud_connector.test.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/cloud_connector.test.ts
@@ -5,10 +5,10 @@
  * 2.0.
  */
 
-import { SavedObjectsClientContract } from '@kbn/core-saved-objects-api-server';
+import type { SavedObjectsClientContract } from '@kbn/core-saved-objects-api-server';
 import { loggerMock } from '@kbn/logging-mocks';
 
-import { CreateCloudConnectorRequest } from '../routes/cloud_connector/handlers';
+import type { CreateCloudConnectorRequest } from '../routes/cloud_connector/handlers';
 import { CLOUD_CONNECTOR_SAVED_OBJECT_TYPE } from '../../common/constants';
 
 import { createSavedObjectClientMock } from '../mocks';
--- x-pack/platform/test/fleet_api_integration/apis/cloud_connector/privileges.ts
+++ x-pack/platform/test/fleet_api_integration/apis/cloud_connector/privileges.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
+import type { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
 import { runPrivilegeTests } from '../../privileges_helpers';
 import { setupTestUsers, testUsers } from '../test_users';
 
