d820d8157fffffb0c25088ccb5b96b76ea3695c0
Nathan Reese <reese.nathan@gmail.com>
2025-08-25 11:22:38 -0600

    tslint

--- src/platform/plugins/shared/visualizations/public/legacy/embeddable/visualize_embeddable.tsx
+++ src/platform/plugins/shared/visualizations/public/legacy/embeddable/visualize_embeddable.tsx
@@ -370,7 +370,6 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
   hasInspector = () => Boolean(this.getInspectorAdapters());
 
   onContainerLoading = () => {
-    this.renderComplete.dispatchInProgress();
     this.updateOutput({
       ...this.getOutput(),
       loading: true,
@@ -388,7 +387,6 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
   };
 
   onContainerRender = () => {
-    this.renderComplete.dispatchComplete();
     this.updateOutput({
       ...this.getOutput(),
       rendered: true,
@@ -399,7 +397,6 @@ export class VisualizeEmbeddable extends Embeddable<VisualizeInput, VisualizeOut
     if (this.abortController) {
       this.abortController.abort();
     }
-    this.renderComplete.dispatchError();
 
     if (isFallbackDataView(this.vis.data.indexPattern)) {
       error = new Error(
