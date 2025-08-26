af9fb1ae889844f6f6d587636ecd2bab732443b4
Drew Tate <drew.tate@elastic.co>
2025-08-26 08:21:30 -0600

    Add scalar functions to nested time series context

--- src/platform/packages/shared/kbn-esql-ast/scripts/constants.ts
+++ src/platform/packages/shared/kbn-esql-ast/scripts/constants.ts
@@ -28,6 +28,7 @@ export const defaultScalarFunctionLocations: Location[] = [
   Location.STATS,
   Location.STATS_BY,
   Location.STATS_WHERE,
+  Location.STATS_TIMESERIES,
   Location.COMPLETION,
 ];
 
