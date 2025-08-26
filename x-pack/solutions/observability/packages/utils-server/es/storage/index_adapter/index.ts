1b312af19e75f9ff8d06b246b4ec5ae47d3d9a74
Pierre Gayvallet <pierre.gayvallet@elastic.co>
2025-08-26 13:30:51 +0200

    `@kbn/storage-adapter`: do not update settings when updating mappings (#232840)
    ## Summary
    
    Fix a bug causing a mapping update of a `index-storage` to cause an
    error during `updateMappingsOfExistingIndex`.
    
    During that phase, we were trying to update the index's settings in
    addition to its mappings. However, the `simulateIndexTemplate` API
    returns the full set of settings, and not only the ones which can be
    updated, so the following `putSettings` call was failing with errors
    such as:
    
    ```
     Root causes:
                    illegal_argument_exception: can not update private setting [index.allocation.existing_shards_allocator]; this setting is managed by Elasticsearch
    ```
    
    Given there is apparently no proper way to identify which setting(s) are
    private vs which ones can be updated, this PR simply fully disables the
    setting update part, as discussed with @elastic/kibana-core.
    
    (cherry picked from commit 55c1587eca63d9a62415363a353a9746793703ef)

--- x-pack/solutions/observability/packages/utils-server/es/storage/index_adapter/index.ts
+++ x-pack/solutions/observability/packages/utils-server/es/storage/index_adapter/index.ts
@@ -229,13 +229,6 @@ export class StorageIndexAdapter<TStorageSettings extends IndexStorageSettings> 
       name: getBackingIndexName(this.storage.name, 999999),
     });
 
-    if (simulateIndexTemplateResponse.template.settings) {
-      await this.esClient.indices.putSettings({
-        index: name,
-        settings: simulateIndexTemplateResponse.template.settings,
-      });
-    }
-
     if (simulateIndexTemplateResponse.template.mappings) {
       await this.esClient.indices.putMapping({
         index: name,
