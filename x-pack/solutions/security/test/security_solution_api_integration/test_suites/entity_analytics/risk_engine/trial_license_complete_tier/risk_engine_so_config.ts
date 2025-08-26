1ffdc580580d3994f52a4357961e857bffaa8aa3
Mark Hopkin <mark.hopkin@elastic.co>
2025-08-26 17:33:19 +0100

    remove unused waits

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/risk_engine_so_config.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/risk_engine_so_config.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect';
+import expect from '@kbn/expect/expect';
 import { riskEngineConfigurationTypeName } from '@kbn/security-solution-plugin/server/lib/entity_analytics/risk_engine/saved_object';
 import type { FtrProviderContext } from '../../../../ftr_provider_context';
 import {
@@ -78,7 +78,6 @@ export default ({ getService }: FtrProviderContext) => {
 
     it('should include the right keys as per the update', async () => {
       await riskEngineRoutes.init();
-      await waitForRiskEngineRun;
 
       const currentSoConfig = await getRiskEngineConfigSO({ kibanaServer });
 
@@ -104,7 +103,6 @@ export default ({ getService }: FtrProviderContext) => {
       await riskEngineRoutes.soConfig(updatedSoBody, 200);
 
       await riskEngineRoutes.enable();
-      await waitForRiskEngineRun;
 
       const currentSoConfig3 = await getRiskEngineConfigSO({ kibanaServer });
       expect(JSON.stringify(currentSoConfig3.attributes.excludeAlertStatuses)).to.equal(
@@ -114,7 +112,6 @@ export default ({ getService }: FtrProviderContext) => {
 
     it('should succeed while updating the saved object', async () => {
       await riskEngineRoutes.init();
-      await waitForRiskEngineRun;
 
       const updatedSoBody = {
         exclude_alert_tags: ['False Positive'],
@@ -127,7 +124,6 @@ export default ({ getService }: FtrProviderContext) => {
     it('should update the config in the right space', async () => {
       await riskEngineRoutesForNamespace.init();
       await riskEngineRoutes.init();
-      await waitForRiskEngineRun;
 
       const updatedSoBody = {
         exclude_alert_tags: ['False Positive'],
