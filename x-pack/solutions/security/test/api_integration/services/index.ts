a1f08b9340afec9067fd62986b18b16e02ee1818
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-08-26 17:26:28 +0200

    [ska] relocate security_solution_* dirs

--- x-pack/solutions/security/test/api_integration/services/index.ts
+++ x-pack/solutions/security/test/api_integration/services/index.ts
@@ -5,8 +5,24 @@
  * 2.0.
  */
 
-import { services as platformServices } from '@kbn/test-suites-xpack-platform/api_integration/services';
+import { services as kibanaApiIntegrationServices } from '@kbn/test-suites-src/api_integration/services';
+import { MachineLearningProvider } from '@kbn/test-suites-xpack-platform/api_integration/services/ml';
+import { IngestManagerProvider } from '@kbn/test-suites-xpack-platform/api_integration/services/ingest_manager';
+import { UsageAPIProvider } from '@kbn/test-suites-xpack-platform/api_integration/services/usage_api';
+import { services as commonServices } from '../../common/services';
+
+// @ts-ignore not ts yet
+import { EsSupertestWithoutAuthProvider } from './es_supertest_without_auth';
+import { SecuritySolutionApiProvider } from './security_solution_api.gen';
 
 export const services = {
-  ...platformServices,
+  ...commonServices,
+
+  esSupertest: kibanaApiIntegrationServices.esSupertest,
+  supertest: kibanaApiIntegrationServices.supertest,
+  esSupertestWithoutAuth: EsSupertestWithoutAuthProvider,
+  usageAPI: UsageAPIProvider,
+  ml: MachineLearningProvider,
+  ingestManager: IngestManagerProvider,
+  securitySolutionApi: SecuritySolutionApiProvider,
 };
---
+++ x-pack/solutions/security/test/common/services/index.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { services as kibanaApiIntegrationServices } from '@kbn/test-suites-src/api_integration/services';
+import { commonFunctionalServices } from '@kbn/ftr-common-functional-services';
+import { SpacesServiceProvider } from './spaces';
+import { BsearchSecureService } from './bsearch_secure';
+
+export const services = {
+  ...commonFunctionalServices,
+  supertest: kibanaApiIntegrationServices.supertest,
+  spaces: SpacesServiceProvider,
+  secureBsearch: BsearchSecureService,
+};
