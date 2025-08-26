3fef52a302af51a1f7ebf69256762d75b3ea426a
Damian Polewski <125268832+damian-polewski@users.noreply.github.com>
2025-08-26 10:30:41 +0200

    fix: lint issue

--- x-pack/platform/plugins/private/snapshot_restore/public/application/components/disable_tooltip.tsx
+++ x-pack/platform/plugins/private/snapshot_restore/public/application/components/disable_tooltip.tsx
@@ -40,9 +40,8 @@ export interface Props {
 }
 
 export const DisableToolTip = ({ isManaged, tooltipMessage, children, ...props }: Props) => {
-  // Ensures that any implicitly passed down props from meta parent component via `cloneElement` like in EuiFormRow [here](https://github.com/elastic/eui/blob/bc9c0a0b3faab449f88e45a588dfe0a53d842cf7/packages/eui/src/components/form/form_row/form_row.tsx#L210-L213) are forwarded down to anchor element. 
-  
-// At the same time, explicitly provided props on anchor component, should always take precedence to avoid confusion.
+  // Ensures that any implicitly passed down props from meta parent component via `cloneElement` like in EuiFormRow [here](https://github.com/elastic/eui/blob/bc9c0a0b3faab449f88e45a588dfe0a53d842cf7/packages/eui/src/components/form/form_row/form_row.tsx#L210-L213) are forwarded down to anchor element.
+  // At the same time, explicitly provided props on anchor component, should always take precedence to avoid confusion.
   const childrenWithProps = cloneElement(children, {
     ...props,
     ...children.props,
