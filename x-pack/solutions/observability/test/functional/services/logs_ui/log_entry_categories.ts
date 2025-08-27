4073c772f4457925cdcec81152307b0f0ffdac85
paulina.shakirova <paulina.shakirova@elastic.co>
2025-08-27 15:02:45 +0200

    fix 2 ftr tests testSubj selector

--- x-pack/solutions/observability/test/functional/services/logs_ui/log_entry_categories.ts
+++ x-pack/solutions/observability/test/functional/services/logs_ui/log_entry_categories.ts
@@ -18,7 +18,7 @@ export function LogEntryCategoriesPageProvider({ getPageObjects, getService }: F
     },
 
     async getNoDataScreen(): Promise<WebElementWrapper> {
-      return await testSubjects.find('NoDataPage');
+      return await testSubjects.find('kbnNoDataPage');
     },
 
     async getSetupScreen(): Promise<WebElementWrapper> {
