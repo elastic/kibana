dfa5ba0d8edcd9ab5237ef5e9c4a9df8806eaccc
Arturo Castillo Delgado <arturo.castillo@elastic.co>
2025-08-27 13:35:48 +0200

    [Unified Doc Viewer][Unified Data Table] Remove custom background color on hover
    in favor of EUI defaults

--- src/platform/plugins/shared/unified_doc_viewer/public/components/doc_viewer_table/table.tsx
+++ src/platform/plugins/shared/unified_doc_viewer/public/components/doc_viewer_table/table.tsx
@@ -391,17 +391,10 @@ export const DocViewerTable = ({
 };
 
 const componentStyles = {
-  fieldsGridWrapper: ({ euiTheme }: UseEuiTheme) =>
+  fieldsGridWrapper: () =>
     css({
       minBlockSize: 0,
       display: 'block',
-
-      '.euiDataGridRow': {
-        '&:hover': {
-          // we keep using a deprecated shade until proper token is available
-          backgroundColor: euiTheme.colors.lightestShade,
-        },
-      },
     }),
   noFieldsFound: css({
     minHeight: 300,
