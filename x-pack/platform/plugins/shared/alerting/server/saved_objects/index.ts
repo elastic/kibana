33674ec8da39b3e66128ba8046c4d2d39ffc1927
MichelLosier <michel.losier@elastic.co>
2025-08-27 13:56:31 -0700

    Have alerting_rule_template use agnostic namespaceType

--- x-pack/platform/plugins/shared/alerting/server/saved_objects/index.ts
+++ x-pack/platform/plugins/shared/alerting/server/saved_objects/index.ts
@@ -218,7 +218,7 @@ export function setupSavedObjects(
     name: RULE_TEMPLATE_SAVED_OBJECT_TYPE,
     indexPattern: ALERTING_CASES_SAVED_OBJECT_INDEX,
     hidden: false,
-    namespaceType: 'multiple-isolated',
+    namespaceType: 'agnostic',
     mappings: ruleTemplateMappings,
     modelVersions: ruleTemplateModelVersions,
   });
