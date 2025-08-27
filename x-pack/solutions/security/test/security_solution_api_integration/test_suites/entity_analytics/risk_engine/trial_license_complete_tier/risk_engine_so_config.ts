786c728426212d4fbdaeed3c5b5b53afde31935a
Mark Hopkin <mark.hopkin@elastic.co>
2025-08-27 11:20:02 +0100

    remove unused import

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/risk_engine_so_config.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/risk_engine/trial_license_complete_tier/risk_engine_so_config.ts
@@ -11,7 +11,6 @@ import type { FtrProviderContext } from '../../../../ftr_provider_context';
 import {
   riskEngineRouteHelpersFactory,
   getRiskEngineConfigSO,
-  waitForRiskEngineRun,
   waitForRiskEngineTaskToBeGone,
 } from '../../utils';
 
