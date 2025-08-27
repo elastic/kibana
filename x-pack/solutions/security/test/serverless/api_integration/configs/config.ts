4f5e5f4ce6e7c292f8bf50f306588dec9e25c938
Jonathan Budzenski <jon@elastic.co>
2025-08-27 17:27:08 -0500

    solution filtering no-op

--- x-pack/solutions/security/test/serverless/api_integration/configs/config.ts
+++ x-pack/solutions/security/test/serverless/api_integration/configs/config.ts
@@ -14,7 +14,7 @@ export default createTestConfig({
   junit: {
     reportName: 'Serverless Security API Integration Tests',
   },
-  suiteTags: { exclude: ['skipSvlSec'] },
+  suiteTags: { exclude: ['skipSvlSec', 'no-op'] },
 
   // include settings from project controller
   // https://github.com/elastic/project-controller/blob/main/internal/project/security/config/elasticsearch.yml
