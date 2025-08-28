dcc244a8cfedd0d4376c8c3fb3e227751a3ce397
Paul Tavares <paul.tavares@elastic.co>
2025-08-28 10:42:30 -0400

    Added tests to validate non-endpoint exceptions error conditions

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/artifacts/trial_license_complete_tier/endpoint_exceptions.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/artifacts/trial_license_complete_tier/endpoint_exceptions.ts
@@ -7,7 +7,13 @@
 
 import type TestAgent from 'supertest/lib/agent';
 import expect from '@kbn/expect';
-import { EXCEPTION_LIST_ITEM_URL, EXCEPTION_LIST_URL } from '@kbn/securitysolution-list-constants';
+import {
+  ENDPOINT_ARTIFACT_LISTS,
+  EXCEPTION_LIST_ITEM_URL,
+  EXCEPTION_LIST_URL,
+} from '@kbn/securitysolution-list-constants';
+import { ALL_ENDPOINT_ARTIFACT_LIST_IDS } from '@kbn/security-solution-plugin/common/endpoint/service/artifacts/constants';
+import { ExceptionsListItemGenerator } from '@kbn/security-solution-plugin/common/endpoint/data_generators/exceptions_list_item_generator';
 import { ROLE } from '../../../../config/services/security_solution_edr_workflows_roles_users';
 import { createSupertestErrorLogger } from '../../utils';
 import type { FtrProviderContext } from '../../../../ftr_provider_context_edr_workflows';
@@ -25,27 +31,93 @@ export default function ({ getService }: FtrProviderContext) {
     });
 
     describe(`and using Import API`, function () {
-      beforeEach(async () => {
-        await endpointArtifactTestResources.deleteList('endpoint_list');
-      });
+      const buildImportBuffer = (
+        listId: (typeof ALL_ENDPOINT_ARTIFACT_LIST_IDS)[number]
+      ): Buffer => {
+        const generator = new ExceptionsListItemGenerator();
+        const listInfo = Object.values(ENDPOINT_ARTIFACT_LISTS).find((listDefinition) => {
+          return listDefinition.id === listId;
+        });
 
-      it('should import endpoint exceptions and add global artifact tag if missing', async () => {
-        const importDataBuffer: Buffer = Buffer.from(
+        if (!listInfo) {
+          throw new Error(`Unknown listId: ${listId}. Unable to generate exception list item.`);
+        }
+
+        const createItem = () => {
+          switch (listId) {
+            case ENDPOINT_ARTIFACT_LISTS.endpointExceptions.id:
+              return generator.generateEndpointException();
+
+            case ENDPOINT_ARTIFACT_LISTS.blocklists.id:
+              return generator.generateBlocklist();
+
+            case ENDPOINT_ARTIFACT_LISTS.eventFilters.id:
+              return generator.generateEventFilter();
+
+            case ENDPOINT_ARTIFACT_LISTS.hostIsolationExceptions.id:
+              return generator.generateHostIsolationException();
+
+            case ENDPOINT_ARTIFACT_LISTS.trustedApps.id:
+              return generator.generateTrustedApp();
+
+            case ENDPOINT_ARTIFACT_LISTS.trustedDevices.id:
+              return generator.generateTrustedDevice();
+
+            default:
+              throw new Error(`Unknown listId: ${listId}. Unable to generate exception list item.`);
+          }
+        };
+
+        return Buffer.from(
           `
-{"_version":"WzEsMV0=","created_at":"2025-08-21T14:20:07.012Z","created_by":"kibana","description":"Endpoint Security Exception List","id":"endpoint_list","immutable":false,"list_id":"endpoint_list","name":"Endpoint Security Exception List","namespace_type":"agnostic","os_types":[],"tags":[],"tie_breaker_id":"034d07f4-fa33-43bb-adfa-6f6bda7921ce","type":"endpoint","updated_at":"2025-08-21T14:20:07.012Z","updated_by":"kibana","version":1}
-{"_version":"WzQyODIsMV0=","comments":[],"created_at":"2025-08-26T19:43:06.080Z","created_by":"elastic","description":"Exception list item","entries":[{"type":"match","field":"@timestamp","value":"4","operator":"included"}],"id":"87cc9dd1-1bf3-4c05-a09f-3bb04f36ac8e","item_id":"87cc9dd1-1bf3-4c05-a09f-3bb04f36ac8e","list_id":"endpoint_list","name":"three","namespace_type":"agnostic","os_types":["windows"],"tags":["policy:all"],"tie_breaker_id":"959e5832-7505-4835-8e92-98d0f5d1ffef","type":"simple","updated_at":"2025-08-26T19:43:06.080Z","updated_by":"elastic"}
-{"_version":"WzQyODAsMV0=","comments":[],"created_at":"2025-08-26T19:42:36.182Z","created_by":"elastic","description":"Exception list item","entries":[{"type":"match","field":"@timestamp","value":"1","operator":"included"}],"id":"4707f48a-79e0-43a6-a4a5-3eb923fc192a","item_id":"4707f48a-79e0-43a6-a4a5-3eb923fc192a","list_id":"endpoint_list","name":"one","namespace_type":"agnostic","os_types":["macos"],"tags":[],"tie_breaker_id":"d2505730-fd9e-434b-a3e1-1b9ce141da79","type":"simple","updated_at":"2025-08-26T19:42:36.182Z","updated_by":"elastic"}
-{"_version":"WzQyODEsMV0=","comments":[],"created_at":"2025-08-26T19:42:47.024Z","created_by":"elastic","description":"Exception list item","entries":[{"type":"match","field":"@timestamp","value":"2","operator":"included"}],"id":"a813960d-9c0b-4360-be30-b801c419b3a4","item_id":"a813960d-9c0b-4360-be30-b801c419b3a4","list_id":"endpoint_list","name":"two","namespace_type":"agnostic","os_types":["linux"],"tags":[],"tie_breaker_id":"efb08bff-cc34-4324-8df0-fa1af60f2ea7","type":"simple","updated_at":"2025-08-26T19:42:47.024Z","updated_by":"elastic"}
+{"_version":"WzEsMV0=","created_at":"2025-08-21T14:20:07.012Z","created_by":"kibana","description":"${
+            listInfo!.description
+          }","id":"${listId}","immutable":false,"list_id":"${listId}","name":"${
+            listInfo!.name
+          }","namespace_type":"agnostic","os_types":[],"tags":[],"tie_breaker_id":"034d07f4-fa33-43bb-adfa-6f6bda7921ce","type":"endpoint","updated_at":"2025-08-21T14:20:07.012Z","updated_by":"kibana","version":1}
+${JSON.stringify(createItem())}
+${JSON.stringify(createItem())}
+${JSON.stringify(createItem())}
 {"exported_exception_list_count":1,"exported_exception_list_item_count":3,"missing_exception_list_item_count":0,"missing_exception_list_items":[],"missing_exception_lists":[],"missing_exception_lists_count":0}
 `,
           'utf8'
+        );
+      };
+
+      // All non-Endpoint exceptions artifacts are not allowed to import
+      ALL_ENDPOINT_ARTIFACT_LIST_IDS.filter(
+        (listId) => listId !== ENDPOINT_ARTIFACT_LISTS.endpointExceptions.id
+      ).forEach((listId) => {
+        it(`should error when importing ${listId} artifacts`, async () => {
+          await endpointArtifactTestResources.deleteList(listId);
+
+          const { body } = await endpointOpsAnalystSupertest
+            .post(`${EXCEPTION_LIST_URL}/_import`)
+            .set('kbn-xsrf', 'true')
+            .on('error', createSupertestErrorLogger(log).ignoreCodes([400]))
+            .attach('file', buildImportBuffer(listId), 'import_data.ndjson')
+            .expect(400);
+
+          expect(body.message).to.eql(
+            'EndpointArtifactError: Import is not supported for Endpoint artifact exceptions'
+          );
+        });
+      });
+
+      it('should import endpoint exceptions and add global artifact tag if missing', async () => {
+        await endpointArtifactTestResources.deleteList(
+          ENDPOINT_ARTIFACT_LISTS.endpointExceptions.id
         );
 
         await endpointOpsAnalystSupertest
           .post(`${EXCEPTION_LIST_URL}/_import`)
           .set('kbn-xsrf', 'true')
           .on('error', createSupertestErrorLogger(log))
-          .attach('file', importDataBuffer, 'import_exceptions.ndjson')
+          .attach(
+            'file',
+            buildImportBuffer(ENDPOINT_ARTIFACT_LISTS.endpointExceptions.id),
+            'import_exceptions.ndjson'
+          )
           .expect(200);
 
         const { body } = await endpointOpsAnalystSupertest
@@ -60,6 +132,7 @@ export default function ({ getService }: FtrProviderContext) {
           .send()
           .expect(200);
 
+        // After import - all items should be returned on a GET `find` request.
         expect(body.data.length).to.eql(3);
       });
     });
