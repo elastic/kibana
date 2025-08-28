07d935e764916760f5d7aa5c38d4a1d86974bbfa
Tiago Vila Verde <tiago.vilaverde@elastic.co>
2025-08-28 13:23:02 +0200

    wip

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/risk_score/calculate_risk_scores.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/risk_score/calculate_risk_scores.ts
@@ -18,6 +18,9 @@ import {
 } from '@kbn/rule-registry-plugin/common/technical_rule_data_field_names';
 import { toEntries } from 'fp-ts/Record';
 import { EntityDiscoveryAPIKey } from '@kbn/entityManager-plugin/server/lib/auth/api_key/api_key';
+import type { Either } from 'fp-ts/Either';
+import * as E from 'fp-ts/Either';
+import { diff } from 'jest-diff';
 import { EntityTypeToIdentifierField } from '../../../../common/entity_analytics/types';
 import { getEntityAnalyticsEntityTypes } from '../../../../common/entity_analytics/utils';
 import type { EntityType } from '../../../../common/search_strategy';
@@ -211,6 +214,9 @@ export const getGlobalWeightForIdentifierType = (
 ): number | undefined =>
   weights?.find((weight) => weight.type === RiskWeightTypes.global)?.[identifierType];
 
+type ESQLResults = Array<
+  [EntityType, { scores: EntityRiskScoreRecord[]; afterKey: EntityAfterKey }]
+>;
 const calculateWithESQL = async (
   params: {
     assetCriticalityService: AssetCriticalityService;
@@ -218,7 +224,7 @@ const calculateWithESQL = async (
     logger: Logger;
     experimentalFeatures: ExperimentalFeatures;
   } & CalculateScoresParams
-) => {
+): Promise<Either<string, ESQLResults>> => {
   const { identifierType, logger, esClient } = params;
   const now = new Date().toISOString();
 
@@ -229,34 +235,29 @@ const calculateWithESQL = async (
 
   const compositeQuery = getCompositeQuery(identifierTypes, filter, params);
 
-  logger.info(
+  logger.trace(
     `STEP ONE: Executing ESQL Risk Score composite query:\n${JSON.stringify(compositeQuery)}`
   );
   const response = await esClient.search<never, RiskScoreCompositeBuckets>(compositeQuery);
 
-  logger.info(
-    `STEP ONE: Received ESQL Risk Score composite response:\n${JSON.stringify(response)}`
-  );
-
   if (!response.aggregations) {
-    return {
-      after_keys: {},
-      scores: {
-        host: [],
-        user: [],
-        service: [],
-      },
-    };
+    return E.left('No aggregations in composite response');
   }
 
   const promises = toEntries(response.aggregations).map(
     ([entityType, { buckets, after_key: afterKey }]) => {
       const entities = buckets.map(({ key }) => key[EntityTypeToIdentifierField[entityType]]);
+
+      if (entities.length === 0) {
+        return Promise.resolve([
+          entityType as EntityType,
+          { afterKey, scores: [] },
+        ] satisfies ESQLResults[number]);
+      }
       const query = getESQL(entityType as EntityType, entities);
       return esClient.esql
         .query({ query })
         .then((rs) => {
-          console.log('esql response', rs);
           const legacyBuckets = rs.values.map((row): RiskScoreBucket => {
             const [count, score, inputs, entity] = row as [number, number, string[], string];
 
@@ -285,28 +286,28 @@ const calculateWithESQL = async (
             identifierField: EntityTypeToIdentifierField[entityType],
             logger,
             now,
-          }).then((scores) => {
+          }).then((scores: EntityRiskScoreRecord[]): ESQLResults[number] => {
             return [
-              entityType,
+              entityType as EntityType,
               {
                 scores,
                 afterKey: afterKey as EntityAfterKey,
               },
-            ] as const;
+            ];
           });
         })
         .catch((error) => {
           logger.error(
             `Error executing ESQL query for entity type ${entityType}: ${error.message}`
           );
-          console.log(query);
-          return [entityType, { afterKey, scores: [] }] as const;
+          logger.error(`Query: ${query}`);
+          return [entityType as EntityType, { afterKey, scores: [] }] satisfies ESQLResults[number];
         });
     }
   );
   const results = await Promise.all(promises);
 
-  return results;
+  return E.right(results);
 };
 
 interface RiskScoreCompositeBuckets {
@@ -428,98 +429,148 @@ export const calculateRiskScores = async (
   } & CalculateScoresParams
 ): Promise<RiskScoresPreviewResponse> =>
   withSecuritySpan('calculateRiskScores', async () => {
-    const {
-      afterKeys: userAfterKeys,
-      assetCriticalityService,
-      debug,
-      esClient,
-      filter: userFilter,
-      identifierType,
-      index,
-      logger,
-      pageSize,
-      range,
-      runtimeMappings,
-      weights,
-      alertSampleSizePerShard = 10_000,
-      excludeAlertStatuses = [],
-      experimentalFeatures,
-      excludeAlertTags = [],
-    } = params;
+    // const {
+    //   afterKeys: userAfterKeys,
+    //   assetCriticalityService,
+    //   debug,
+    //   esClient,
+    //   filter: userFilter,
+    //   identifierType,
+    //   index,
+    //   logger,
+    //   pageSize,
+    //   range,
+    //   runtimeMappings,
+    //   weights,
+    //   alertSampleSizePerShard = 10_000,
+    //   excludeAlertStatuses = [],
+    //   experimentalFeatures,
+    //   excludeAlertTags = [],
+    // } = params;
 
-    const now = new Date().toISOString();
-    const scriptedMetricPainless = await getPainlessScripts();
-    const filter = [filterFromRange(range), { exists: { field: ALERT_RISK_SCORE } }];
-    if (excludeAlertStatuses.length > 0) {
-      filter.push({
-        bool: { must_not: { terms: { [ALERT_WORKFLOW_STATUS]: excludeAlertStatuses } } },
-      });
-    }
-    if (!isEmpty(userFilter)) {
-      filter.push(userFilter as QueryDslQueryContainer);
-    }
-    if (excludeAlertTags.length > 0) {
-      filter.push({
-        bool: { must_not: { terms: { [ALERT_WORKFLOW_TAGS]: excludeAlertTags } } },
-      });
-    }
-    const identifierTypes: EntityType[] = identifierType
-      ? [identifierType]
-      : getEntityAnalyticsEntityTypes();
+    // const now = new Date().toISOString();
+    // const scriptedMetricPainless = await getPainlessScripts();
+    // const filter = [filterFromRange(range), { exists: { field: ALERT_RISK_SCORE } }];
+    // if (excludeAlertStatuses.length > 0) {
+    //   filter.push({
+    //     bool: { must_not: { terms: { [ALERT_WORKFLOW_STATUS]: excludeAlertStatuses } } },
+    //   });
+    // }
+    // if (!isEmpty(userFilter)) {
+    //   filter.push(userFilter as QueryDslQueryContainer);
+    // }
+    // if (excludeAlertTags.length > 0) {
+    //   filter.push({
+    //     bool: { must_not: { terms: { [ALERT_WORKFLOW_TAGS]: excludeAlertTags } } },
+    //   });
+    // }
+    // const identifierTypes: EntityType[] = identifierType
+    //   ? [identifierType]
+    //   : getEntityAnalyticsEntityTypes();
 
-    const request = {
-      size: 0,
-      _source: false,
-      index,
-      ignore_unavailable: true,
-      runtime_mappings: runtimeMappings,
-      query: {
-        function_score: {
-          query: {
-            bool: {
-              filter,
-              should: [
-                {
-                  match_all: {}, // This forces ES to calculate score
-                },
-              ],
-            },
-          },
-          field_value_factor: {
-            field: ALERT_RISK_SCORE, // sort by risk score
-          },
-        },
-      },
-      aggs: identifierTypes.reduce((aggs, _identifierType) => {
-        aggs[_identifierType] = buildIdentifierTypeAggregation({
-          afterKeys: userAfterKeys,
-          identifierType: _identifierType,
-          pageSize,
-          weights,
-          alertSampleSizePerShard,
-          scriptedMetricPainless,
-        });
-        return aggs;
-      }, {} as Record<string, AggregationsAggregationContainer>),
-    };
+    // const request = {
+    //   size: 0,
+    //   _source: false,
+    //   index,
+    //   ignore_unavailable: true,
+    //   runtime_mappings: runtimeMappings,
+    //   query: {
+    //     function_score: {
+    //       query: {
+    //         bool: {
+    //           filter,
+    //           should: [
+    //             {
+    //               match_all: {}, // This forces ES to calculate score
+    //             },
+    //           ],
+    //         },
+    //       },
+    //       field_value_factor: {
+    //         field: ALERT_RISK_SCORE, // sort by risk score
+    //       },
+    //     },
+    //   },
+    //   aggs: identifierTypes.reduce((aggs, _identifierType) => {
+    //     aggs[_identifierType] = buildIdentifierTypeAggregation({
+    //       afterKeys: userAfterKeys,
+    //       identifierType: _identifierType,
+    //       pageSize,
+    //       weights,
+    //       alertSampleSizePerShard,
+    //       scriptedMetricPainless,
+    //     });
+    //     return aggs;
+    //   }, {} as Record<string, AggregationsAggregationContainer>),
+    // };
 
-    console.log('request', request);
+    // if (debug) {
+    //   logger.info(`Executing Risk Score query:\n${JSON.stringify(request)}`);
+    // }
+    // const response = await esClient.search<never, CalculateRiskScoreAggregations>(request);
 
-    if (debug) {
-      logger.info(`Executing Risk Score query:\n${JSON.stringify(request)}`);
-    }
+    // if (debug) {
+    //   logger.info(`Received Risk Score response:\n${JSON.stringify(response)}`);
+    // }
 
-    const response = await esClient.search<never, CalculateRiskScoreAggregations>(request);
+    // if (response.aggregations == null) {
+    //   return {
+    //     ...(debug ? { request, response } : {}),
+    //     after_keys: {},
+    //     scores: {
+    //       host: [],
+    //       user: [],
+    //       service: [],
+    //     },
+    //   };
+    // }
 
-    console.log('response', response);
+    // const userBuckets = response.aggregations.user?.buckets ?? [];
+    // const hostBuckets = response.aggregations.host?.buckets ?? [];
+    // const serviceBuckets = response.aggregations.service?.buckets ?? [];
 
-    if (debug) {
-      logger.info(`Received Risk Score response:\n${JSON.stringify(response)}`);
-    }
+    // const afterKeys = {
+    //   host: response.aggregations.host?.after_key,
+    //   user: response.aggregations.user?.after_key,
+    //   service: experimentalFeatures ? response.aggregations.service?.after_key : undefined,
+    // };
 
-    if (response.aggregations == null) {
+    // const hostScores = await processScores({
+    //   assetCriticalityService,
+    //   buckets: hostBuckets,
+    //   identifierField: 'host.name',
+    //   logger,
+    //   now,
+    // });
+    // const userScores = await processScores({
+    //   assetCriticalityService,
+    //   buckets: userBuckets,
+    //   identifierField: 'user.name',
+    //   logger,
+    //   now,
+    // });
+    // const serviceScores = await processScores({
+    //   assetCriticalityService,
+    //   buckets: serviceBuckets,
+    //   identifierField: 'service.name',
+    //   logger,
+    //   now,
+    // });
+
+    // const legacyResults = {
+    //   ...(debug ? { request, response } : {}),
+    //   after_keys: afterKeys,
+    //   scores: {
+    //     host: hostScores,
+    //     user: userScores,
+    //     service: serviceScores,
+    //   },
+    // };
+
+    const esqlResults = await calculateWithESQL(params);
+
+    if (E.isLeft(esqlResults)) {
       return {
-        ...(debug ? { request, response } : {}),
         after_keys: {},
         scores: {
           host: [],
@@ -529,55 +580,26 @@ export const calculateRiskScores = async (
       };
     }
 
-    const userBuckets = response.aggregations.user?.buckets ?? [];
-    const hostBuckets = response.aggregations.host?.buckets ?? [];
-    const serviceBuckets = response.aggregations.service?.buckets ?? [];
-
-    const afterKeys = {
-      host: response.aggregations.host?.after_key,
-      user: response.aggregations.user?.after_key,
-      service: experimentalFeatures ? response.aggregations.service?.after_key : undefined,
-    };
-
-    const hostScores = await processScores({
-      assetCriticalityService,
-      buckets: hostBuckets,
-      identifierField: 'host.name',
-      logger,
-      now,
-    });
-    const userScores = await processScores({
-      assetCriticalityService,
-      buckets: userBuckets,
-      identifierField: 'user.name',
-      logger,
-      now,
-    });
-    const serviceScores = await processScores({
-      assetCriticalityService,
-      buckets: serviceBuckets,
-      identifierField: 'service.name',
-      logger,
-      now,
-    });
-
-    const esqlResults = await calculateWithESQL(params);
-    console.log('esql', esqlResults);
-    // const afterKeysESQL = Object.fromEntries(
-    //   esqlResults.map(([entityType, { afterKey }]) => [entityType, afterKey])
-    // );
-    // const scores = esqlResults.reduce((acc, [entityType, { scores }]) => {
-    //   acc[entityType] = scores;
-    //   return acc;
-    // }, {} as Record<string, unknown>);
-
-    return {
-      ...(debug ? { request, response } : {}),
-      after_keys: afterKeys,
-      scores: {
-        host: hostScores,
-        user: userScores,
-        service: serviceScores,
+    const results = esqlResults.right.reduce<{
+      after_keys: Record<string, EntityAfterKey>;
+      scores: Record<string, EntityRiskScoreRecord[]>;
+    }>(
+      (res, [entityType, { afterKey, scores }]) => {
+        res.after_keys[entityType] = afterKey;
+        res.scores[entityType] = scores;
+        return res;
       },
-    };
+      { after_keys: {}, scores: {} }
+    );
+
+    return results;
+    // return {
+    //   ...(debug ? { request, response } : {}),
+    //   after_keys: afterKeys,
+    //   scores: {
+    //     host: hostScores,
+    //     user: userScores,
+    //     service: serviceScores,
+    //   },
+    // };
   });
