90b874eb017cc6371b23d266a99485d84e095953
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2025-08-28 22:16:08 +0200

    [9.0] [Console] Fix keyboard shortcut test (#232425) (#233379)
    # Backport
    
    This will backport the following commits from `main` to `9.0`:
    - [[Console] Fix keyboard shortcut test
    (#232425)](https://github.com/elastic/kibana/pull/232425)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Sonia Sanz
    Vivas","email":"sonia.sanzvivas@elastic.co"},"sourceCommit":{"committedDate":"2025-08-28T18:01:57Z","message":"[Console]
    Fix keyboard shortcut test (#232425)\n\nFixes
    https://github.com/elastic/kibana/issues/224988\n\n## Summary\nThe
    `toggleKeyboardShortcuts` tests wasn't using the `enabled` param.\nThat
    was causing that the shortcuts were disabled when the test runs.\nThis
    change is applied asynchronously, so also when the toggle is\nworking
    properly, it can cause the tests to fail. To avoid that, I've\nadded a
    wait time after the change is applied.\n\nFlaky test
    runner:\nhttps://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/9200","sha":"506618176dd5a7dccc1acfc5932c21e3b43d1851","branchLabelMapping":{"^v9.2.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["Feature:Console","Team:Kibana
    Management","release_note:skip","backport:all-open","v9.2.0"],"title":"[Console]
    Fix keyboard shortcut
    test","number":232425,"url":"https://github.com/elastic/kibana/pull/232425","mergeCommit":{"message":"[Console]
    Fix keyboard shortcut test (#232425)\n\nFixes
    https://github.com/elastic/kibana/issues/224988\n\n## Summary\nThe
    `toggleKeyboardShortcuts` tests wasn't using the `enabled` param.\nThat
    was causing that the shortcuts were disabled when the test runs.\nThis
    change is applied asynchronously, so also when the toggle is\nworking
    properly, it can cause the tests to fail. To avoid that, I've\nadded a
    wait time after the change is applied.\n\nFlaky test
    runner:\nhttps://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/9200","sha":"506618176dd5a7dccc1acfc5932c21e3b43d1851"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v9.2.0","branchLabelMappingKey":"^v9.2.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/232425","number":232425,"mergeCommit":{"message":"[Console]
    Fix keyboard shortcut test (#232425)\n\nFixes
    https://github.com/elastic/kibana/issues/224988\n\n## Summary\nThe
    `toggleKeyboardShortcuts` tests wasn't using the `enabled` param.\nThat
    was causing that the shortcuts were disabled when the test runs.\nThis
    change is applied asynchronously, so also when the toggle is\nworking
    properly, it can cause the tests to fail. To avoid that, I've\nadded a
    wait time after the change is applied.\n\nFlaky test
    runner:\nhttps://buildkite.com/elastic/kibana-flaky-test-suite-runner/builds/9200","sha":"506618176dd5a7dccc1acfc5932c21e3b43d1851"}}]}]
    BACKPORT-->
    
    Co-authored-by: Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>

--- test/functional/page_objects/console_page.ts
+++ test/functional/page_objects/console_page.ts
@@ -330,10 +330,6 @@ export class ConsolePageObject extends FtrService {
     return await this.isConsoleTabOpen('consoleHistoryPanel');
   }
 
-  public async openSettings() {
-    await this.testSubjects.click('consoleConfigButton');
-  }
-
   public async toggleA11yOverlaySetting(enabled: boolean) {
     await this.testSubjects.waitForEnabled('enableA11yOverlay');
     await this.testSubjects.setEuiSwitch('enableA11yOverlay', enabled ? 'check' : 'uncheck');
@@ -402,13 +398,16 @@ export class ConsolePageObject extends FtrService {
   }
 
   public async toggleKeyboardShortcuts(enabled: boolean) {
-    await this.openSettings();
+    await this.testSubjects.waitForEnabled('enableKeyboardShortcuts');
+    await this.testSubjects.setEuiSwitch('enableKeyboardShortcuts', enabled ? 'check' : 'uncheck');
+  }
 
-    // while the settings form opens/loads this may fail, so retry for a while
-    await this.retry.try(async () => {
-      const toggle = await this.testSubjects.find('enableKeyboardShortcuts');
-      await toggle.click();
-    });
+  public async setKeyboardShortcutsEnabled(enabled: boolean) {
+    await this.openConfig();
+    await this.toggleKeyboardShortcuts(enabled);
+    // The sleep is necessary to allow the switch state to be propagated
+    await this.common.sleep(500);
+    await this.openConsole();
   }
 
   public async hasSuccessBadge() {
