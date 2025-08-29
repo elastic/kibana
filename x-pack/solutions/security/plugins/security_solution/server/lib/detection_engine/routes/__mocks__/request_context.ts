7b0c5a0d5ac352400f7323188f9e38c85b480705
YulNaumenko <jo.naumenko@gmail.com>
2025-08-28 20:43:46 -0700

    -

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/routes/__mocks__/request_context.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/routes/__mocks__/request_context.ts
@@ -49,6 +49,8 @@ import { createProductFeaturesServiceMock } from '../../../product_features_serv
 import type { EndpointAppContextService } from '../../../../endpoint/endpoint_app_context_services';
 import { padPackageInstallationClientMock } from '../../../entity_analytics/privilege_monitoring/privileged_access_detection/pad_package_installation_client.mock';
 import { privilegeMonitorDataClientMock } from '../../../entity_analytics/privilege_monitoring/engine/data_client.mock';
+import { contentManagementMock } from '@kbn/content-management-plugin/public/mocks';
+import type { ContentManagementServerSetup } from '@kbn/content-management-plugin/server';
 
 export const createMockClients = () => {
   const core = coreMock.createRequestHandlerContext();
@@ -93,6 +95,7 @@ export const createMockClients = () => {
     assetInventoryDataClient: AssetInventoryDataClientMock.create(),
     productFeaturesService: createProductFeaturesServiceMock(),
     logger: loggerMock.create(),
+    contentManagement: contentManagementMock.createSetupContract(),
   };
 };
 
@@ -127,6 +130,8 @@ const createRequestContextMock = (
       getExtensionPointClient: jest.fn(),
       getInternalListClient: jest.fn(),
     },
+    contentManagement:
+      clients.contentManagement as unknown as jest.Mocked<ContentManagementServerSetup>,
   };
 };
 
