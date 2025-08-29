f3ab81b5907b9f784e7c26c3b90c16210fab897f
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-08-29 15:43:27 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/platform/plugins/private/data_visualizer/public/application/common/components/expanded_row/geo_point_content/format_utils.ts
+++ x-pack/platform/plugins/private/data_visualizer/public/application/common/components/expanded_row/geo_point_content/format_utils.ts
@@ -7,7 +7,8 @@
 
 import type { Feature, Point } from 'geojson';
 import { euiPaletteColorBlind } from '@elastic/eui';
-import { LAYER_TYPE, SOURCE_TYPES, VectorLayerDescriptor } from '@kbn/maps-plugin/common';
+import type { VectorLayerDescriptor } from '@kbn/maps-plugin/common';
+import { LAYER_TYPE, SOURCE_TYPES } from '@kbn/maps-plugin/common';
 import { DEFAULT_GEO_REGEX } from './geo_point_content';
 
 export const convertWKTGeoToLonLat = (
