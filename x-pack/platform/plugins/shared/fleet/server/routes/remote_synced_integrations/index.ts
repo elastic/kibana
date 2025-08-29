d992f9a3aeb689783272bfd792a3b4fa1be0c974
Julia Bardi <90178898+juliaElastic@users.noreply.github.com>
2025-08-29 16:04:17 +0200

    Update x-pack/platform/plugins/shared/fleet/server/routes/remote_synced_integrations/index.ts
    Co-authored-by: Visha Angelova <91186315+vishaangelova@users.noreply.github.com>

--- x-pack/platform/plugins/shared/fleet/server/routes/remote_synced_integrations/index.ts
+++ x-pack/platform/plugins/shared/fleet/server/routes/remote_synced_integrations/index.ts
@@ -37,7 +37,7 @@ export const registerRoutes = (router: FleetAuthzRouter, isServerless?: boolean)
       },
       summary: `Get Fleet Remote synced integrations status`,
       options: {
-        tags: ['oas-tag:Fleet Remote synced integrations'],
+        tags: ['oas-tag:Fleet remote synced integrations'],
       },
     })
     .addVersion(
