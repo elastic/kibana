bffeaa45fb8cfafe2db23e814bcdc2bf0dc7f585
Jatin Kathuria <jatin.kathuria@elastic.co>
2025-08-29 16:39:39 +0200

    fix: remove unnecesary type

--- src/platform/packages/shared/kbn-utility-types/index.ts
+++ src/platform/packages/shared/kbn-utility-types/index.ts
@@ -178,8 +178,3 @@ export type RecursivePartial<T> = {
     : RecursivePartial<T[P]>;
 };
 type NonAny = number | boolean | string | symbol | null;
-
-/**
- * Utility type for making all properties of an object nullable.
- */
-export type Nullable<T extends object> = { [K in keyof T]: T[K] | null };
