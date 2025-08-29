10217b0e55f39ed93d11f04cc1b990a7b30906db
Kurt Greiner <kurt.greiner@elastic.co>
2025-08-29 12:26:48 -0400

    Fix orig handlebars ref

--- src/platform/packages/shared/kbn-openapi-generator/src/template_service/template_service.ts
+++ src/platform/packages/shared/kbn-openapi-generator/src/template_service/template_service.ts
@@ -7,8 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-// eslint-disable-next-line @kbn/imports/no_direct_handlebars_import
-import Handlebars from 'handlebars';
+import Handlebars from '@kbn/handlebars';
 import { resolve } from 'path';
 import type { BundleGenerationContext, GenerationContext } from '../parser/get_generation_context';
 import { registerHelpers } from './register_helpers';
