f83b11f19a3a954ffbeb8d328a42c1eda61f0744
Quynh Nguyen <quynh.nguyen@elastic.co>
2025-08-29 14:23:54 -0500

    Remove throw

--- x-pack/platform/plugins/shared/inference/server/util/get_connector_by_id.ts
+++ x-pack/platform/plugins/shared/inference/server/util/get_connector_by_id.ts
@@ -5,7 +5,9 @@
  * 2.0.
  */
 
-import type { ActionsClient } from '@kbn/actions-plugin/server';
+import type { ActionResult as ActionConnector } from '@kbn/actions-plugin/server';
+import type { PluginStartContract as ActionsPluginStart } from '@kbn/actions-plugin/server';
+import type { KibanaRequest } from '@kbn/core/server';
 import {
   createInferenceRequestError,
   connectorToInference,
@@ -17,20 +19,22 @@ import {
  */
 export const getConnectorById = async ({
   connectorId,
-  actionsClient,
+  actions,
+  request,
 }: {
-  actionsClient: ActionsClient;
+  actions: ActionsPluginStart;
+  request: KibanaRequest;
   connectorId: string;
 }): Promise<InferenceConnector> => {
-  let connector;
+  let connector: ActionConnector;
   try {
-    connector = await actionsClient.get({ id: connectorId });
-    if (!connector) {
-      throw createInferenceRequestError(`No connector found for id '${connectorId}'`, 400);
-    }
+    const actionsClient = await actions.getActionsClientWithRequest(request);
+    connector = await actionsClient.get({
+      id: connectorId,
+    });
   } catch (error) {
     throw createInferenceRequestError(
-      `An error occur fetching connectors for id '${connectorId}'\n${error.message}`,
+      `No connector found for id '${connectorId}'\n${error.message}`,
       400
     );
   }
