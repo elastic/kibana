0eaf262cc2e14c0be1260c2c2b808c57c30a91b5
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-08-29 11:54:50 +0200

    remove duplicate anomaly charts action in ML ui_actions

--- x-pack/platform/plugins/shared/ml/public/ui_actions/index.ts
+++ x-pack/platform/plugins/shared/ml/public/ui_actions/index.ts
@@ -97,11 +97,6 @@ export function registerMlUiActions(
     const addSwimlanePanelAction = createAddSwimlanePanelAction(core.getStartServices);
     return addSwimlanePanelAction;
   });
-  uiActions.addTriggerActionAsync(ADD_PANEL_TRIGGER, ACTION_ADD_PANEL, async () => {
-    const { createAddAnomalyChartsPanelAction } = await import('./create_anomaly_chart');
-    const addAnomalyChartsPanelAction = createAddAnomalyChartsPanelAction(core.getStartServices);
-    return addAnomalyChartsPanelAction;
-  });
 
   uiActions.addTriggerActionAsync(CONTEXT_MENU_TRIGGER, 'ACTION_CONTEXT_MENU', async () => {
     const { createOpenInExplorerAction } = await import('./open_in_anomaly_explorer_action');
