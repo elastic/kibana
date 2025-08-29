24b6370a2b6d519e8f4aa3dc8fed7abd60ac429f
Elena Stoeva <elenastoeva99@gmail.com>
2025-08-29 14:48:31 +0100

    Fix quick check errors

--- x-pack/platform/packages/private/kbn-data-quality/jest.config.js
+++ x-pack/platform/packages/private/kbn-data-quality/jest.config.js
@@ -8,5 +8,5 @@
 module.exports = {
   preset: '@kbn/test/jest_node',
   rootDir: '../../../../..',
-  roots: ['<rootDir>/x-pack/platform/packages/shared/kbn-data-quality'],
+  roots: ['<rootDir>/x-pack/platform/packages/private/kbn-data-quality'],
 };
