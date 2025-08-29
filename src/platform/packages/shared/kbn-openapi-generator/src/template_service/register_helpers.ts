2ad45a18360ec4b1c6b5117da61e4b0c8aeee0d5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-08-29 16:50:44 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- src/platform/packages/shared/kbn-openapi-generator/src/template_service/register_helpers.ts
+++ src/platform/packages/shared/kbn-openapi-generator/src/template_service/register_helpers.ts
@@ -6,7 +6,7 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
-import type Handlebars from '@kbn/handlebars'
+import type Handlebars from '@kbn/handlebars';
 import type { HelperOptions } from '@kbn/handlebars';
 import { snakeCase, camelCase, upperCase } from 'lodash';
 
