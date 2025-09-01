365ed00c67ed52c28d9f9d305d76b89394b224f1
Sid <siddharthmantri1@gmail.com>
2025-09-01 13:57:07 +0200

    throw left correctly

--- src/core/packages/saved-objects/api-server-internal/src/lib/apis/bulk_create.ts
+++ src/core/packages/saved-objects/api-server-internal/src/lib/apis/bulk_create.ts
@@ -106,26 +106,25 @@ export const performBulkCreate = async <T>(
         error = e;
       }
     }
-
-    if (error) {
-      return left({ id: requestId, type, error: errorContent(error) });
-    }
-
     const method = requestId && overwrite ? 'index' : 'create';
     const requiresNamespacesCheck = requestId && registry.isMultiNamespace(type);
     const accessMode = options.accessControl?.accessMode;
     const typeSupportsAccessControl = registry.supportsAccessControl(type);
 
     if (!typeSupportsAccessControl && accessMode) {
-      throw SavedObjectsErrorHelpers.createBadRequestError(
+      error = SavedObjectsErrorHelpers.createBadRequestError(
         `The "accessMode" field is not supported for saved objects of type "${type}".`
       );
     }
 
     if (!createdBy && accessMode === 'read_only') {
-      throw SavedObjectsErrorHelpers.createBadRequestError(
+      error = SavedObjectsErrorHelpers.createBadRequestError(
         `Cannot create a saved object of type "${type}" with "read_only" access mode because Kibana could not determine the user profile ID for the caller. This access mode requires an identifiable user profile.`
       );
+    }
+
+    if (error) {
+      return left({ id: requestId, type, error: errorContent(error) });
     }
 
     return right({
