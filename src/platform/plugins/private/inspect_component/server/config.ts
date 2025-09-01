f97f3a16054c835303e0a0741329a5b2b42e9ca9
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-09-01 10:50:41 +0200

    Make config dev only

--- src/platform/plugins/private/inspect_component/server/config.ts
+++ src/platform/plugins/private/inspect_component/server/config.ts
@@ -11,8 +11,17 @@ import type { TypeOf } from '@kbn/config-schema';
 import { schema } from '@kbn/config-schema';
 
 export const configSchema = schema.object({
-  /** Controls whether inspect component funcionality is enabled. */
-  enabled: schema.boolean({ defaultValue: false }),
+  /** Controls whether inspect component plugin is enabled. */
+  enabled: schema.conditional(
+    schema.contextRef('dev'),
+    true,
+    /** Allowed to be configured when in dev. */
+    schema.boolean(),
+    /** When not in dev, only false is allowed. */
+    schema.literal(false),
+    /** Default to false, even in dev */
+    { defaultValue: false }
+  ),
 });
 
 export type ConfigSchema = TypeOf<typeof configSchema>;
