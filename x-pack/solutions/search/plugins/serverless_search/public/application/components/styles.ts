78e52c9674fdc921263fd8eb0a31c6e61fafc6a5
Brittany LaGambina <brittany.lagambina@elastic.co>
2025-09-01 09:30:36 +0100

    Convert all serverless SCSS files to Emotion

---
+++ x-pack/solutions/search/plugins/serverless_search/public/application/components/styles.ts
@@ -0,0 +1,27 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { css } from '@emotion/react';
+
+import { transparentize, type EuiThemeComputed } from '@elastic/eui';
+
+export const serverlessSearchHeaderSection = css`
+  > div {
+    padding-bottom: 0;
+    padding-top: 0;
+  }
+`;
+
+export const cloudDetailsPanel = css`
+  .serverlessSearchCloudDetailsCopyPanel {
+    word-break: break-all;
+  }
+`;
+
+export const serverlessSearchOverviewFooterSection = (euiTheme: EuiThemeComputed<{}>) => css`
+  background-color: ${transparentize(euiTheme.colors.primary, 0.9)};
+`;
