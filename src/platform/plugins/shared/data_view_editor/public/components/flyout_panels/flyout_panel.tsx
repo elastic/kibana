94173d6fa643f581d1b95e9a33cac411466c3bd9
Panaghiotis Maurice Van de goor <panaghiotis.vandegoor@elastic.co>
2025-09-01 16:21:52 +0300

    correctly sets up mobile css rule

--- src/platform/plugins/shared/data_view_editor/public/components/flyout_panels/flyout_panel.tsx
+++ src/platform/plugins/shared/data_view_editor/public/components/flyout_panels/flyout_panel.tsx
@@ -10,7 +10,7 @@
 import type { CSSProperties } from 'react';
 import React, { useState, useLayoutEffect, useCallback, createContext, useContext } from 'react';
 import { css } from '@emotion/react';
-import { EuiFlexItem, type UseEuiTheme } from '@elastic/eui';
+import { EuiFlexItem, useEuiMaxBreakpoint, type UseEuiTheme } from '@elastic/eui';
 import { useMemoCss } from '@kbn/css-utils/public/use_memo_css';
 
 import { useFlyoutPanelsContext } from './flyout_panels';
@@ -117,13 +117,14 @@ export const useFlyoutPanelContext = (): Context => {
 };
 
 const componentStyles = {
-  flyoutColumn: css({
-    height: '100%',
-    overflow: 'hidden',
-    ['@media only screen and (max-width: 767px)']: {
-      height: 'auto',
-    },
-  }),
+  flyoutColumn: () =>
+    css({
+      height: '100%',
+      overflow: 'hidden',
+      [useEuiMaxBreakpoint('m')]: {
+        height: 'auto',
+      },
+    }),
   flyoutPanel: ({ euiTheme }: UseEuiTheme) =>
     css({
       height: '100%',
