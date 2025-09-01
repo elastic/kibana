66f6ac5e328509d16bf97cd83f3807d23bca374a
iblancof <irene.blanco@elastic.co>
2025-09-01 16:55:42 +0200

    Create first version of new trace section (tests to be added)

---
+++ src/platform/plugins/shared/unified_doc_viewer/public/components/observability/traces/components/trace2/field_configurations.tsx
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+import React from 'react';
+
+import { TRACE_ID, TRANSACTION_ID } from '@kbn/apm-types';
+import { i18n } from '@kbn/i18n';
+import { TraceIdLink } from '../trace_id_link';
+
+// This is currently kind of a duplication of what we have in:
+// src/platform/plugins/shared/unified_doc_viewer/public/components/observability/traces/resources/get_field_configuration.tsx
+// that will be removed as part of the refactor
+
+export const traceFieldConfigurations = {
+  [TRACE_ID]: {
+    title: i18n.translate('unifiedDocViewer.observability.traces.traceId.title', {
+      defaultMessage: 'Trace ID',
+    }),
+    formatter: (value: unknown, formattedValue: string) => (
+      <TraceIdLink traceId={value as string} formattedTraceId={formattedValue} />
+    ), // TODO should I update the link to go to discover instead of APM? (same as the span links links)
+  },
+  [TRANSACTION_ID]: {
+    title: i18n.translate('unifiedDocViewer.observability.traces.transactionId.title', {
+      defaultMessage: 'Transaction ID',
+    }), // TODO check if we want a formatter in here too or not, do I want to add a link to Discover as similar spans?
+  },
+};
---
+++ src/platform/plugins/shared/unified_doc_viewer/public/components/observability/traces/components/trace2/index.tsx
@@ -0,0 +1,140 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React, { useCallback, useMemo, useState } from 'react';
+import { i18n } from '@kbn/i18n';
+import type { DataTableRecord } from '@kbn/discover-utils';
+import type { DocViewRenderProps } from '@kbn/unified-doc-viewer/types';
+import { SERVICE_NAME, SPAN_ID, TRACE_ID, TRANSACTION_ID } from '@kbn/apm-types';
+import { EmbeddableRenderer } from '@kbn/embeddable-plugin/public';
+import { ContentFrameworkTable } from '../../../../content_framework';
+import { ContentFrameworkSection } from '../../../../content_framework/section';
+import { getUnifiedDocViewerServices } from '../../../../../plugin';
+import { FullScreenWaterfall } from '../full_screen_waterfall';
+import { traceFieldConfigurations } from './field_configurations';
+
+const fullScreenButtonLabel = i18n.translate(
+  'unifiedDocViewer.observability.traces.trace.fullScreen.button',
+  {
+    defaultMessage: 'Expand trace timeline',
+  }
+);
+
+const sectionTip = i18n.translate('unifiedDocViewer.observability.traces.trace.description', {
+  defaultMessage:
+    'Trace attributes and a timeline of all spans in the trace, including their duration and hierarchy.',
+});
+
+export interface Trace2Props
+  extends Pick<DocViewRenderProps, 'filter' | 'onAddColumn' | 'onRemoveColumn'> {
+  hit: DataTableRecord;
+  displayType: 'span' | 'transaction';
+  dataView: DocViewRenderProps['dataView'];
+  showWaterfall?: boolean;
+}
+
+// TODO replace Trace With Trace2 in the overviews and rename it once everything is ready
+export const Trace2 = ({
+  hit,
+  displayType,
+  dataView,
+  filter,
+  onAddColumn,
+  onRemoveColumn,
+  showWaterfall = true,
+}: Trace2Props) => {
+  const { data } = getUnifiedDocViewerServices();
+  const [showFullScreenWaterfall, setShowFullScreenWaterfall] = useState(false);
+
+  const traceId = hit.flattened[TRACE_ID] as string;
+  const docId = hit.flattened[displayType === 'span' ? SPAN_ID : TRANSACTION_ID] as string;
+  const serviceName = hit.flattened[SERVICE_NAME] as string;
+
+  const { from: rangeFrom, to: rangeTo } = data.query.timefilter.timefilter.getAbsoluteTime();
+
+  const getParentApi = useCallback(
+    () => ({
+      getSerializedStateForChild: () => ({
+        rawState: {
+          traceId,
+          rangeFrom,
+          rangeTo,
+          docId,
+        },
+      }),
+    }),
+    [docId, rangeFrom, rangeTo, traceId]
+  );
+
+  const fieldNames = useMemo(
+    () => [TRACE_ID, ...(displayType === 'span' ? [TRANSACTION_ID] : [])],
+    [displayType]
+  );
+  const sectionActions = useMemo(
+    () =>
+      showWaterfall
+        ? [
+            {
+              label: fullScreenButtonLabel,
+              icon: 'fullScreen',
+              ariaLabel: fullScreenButtonLabel,
+              onClick: () => {
+                setShowFullScreenWaterfall(true);
+              },
+            },
+          ]
+        : [],
+    [showWaterfall]
+  );
+
+  return (
+    <>
+      <ContentFrameworkSection
+        id={'traceSection'}
+        title={i18n.translate('unifiedDocViewer.observability.traces.trace.title', {
+          defaultMessage: 'Trace',
+        })}
+        description={showWaterfall ? sectionTip : undefined}
+        actions={sectionActions}
+      >
+        <ContentFrameworkTable
+          fieldNames={fieldNames}
+          id={'traceTable'}
+          fieldConfigurations={traceFieldConfigurations}
+          dataView={dataView}
+          hit={hit}
+          filter={filter}
+          onAddColumn={onAddColumn}
+          onRemoveColumn={onRemoveColumn}
+        />
+
+        {showWaterfall && (
+          <EmbeddableRenderer
+            type="APM_TRACE_WATERFALL_EMBEDDABLE"
+            getParentApi={getParentApi}
+            hidePanelChrome
+          />
+        )}
+      </ContentFrameworkSection>
+
+      {showFullScreenWaterfall && (
+        <FullScreenWaterfall
+          traceId={traceId}
+          rangeFrom={rangeFrom}
+          rangeTo={rangeTo}
+          dataView={dataView}
+          serviceName={serviceName}
+          onExitFullScreen={() => {
+            setShowFullScreenWaterfall(false);
+          }}
+        />
+      )}
+    </>
+  );
+};
