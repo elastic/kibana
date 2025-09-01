a930da242098b676257b64dc11fa3edad13013a9
Jesus Wahrman <41008968+jesuswr@users.noreply.github.com>
2025-09-01 17:04:56 +0200

    Update src/core/packages/saved-objects/migration-server-internal/src/zdt/model/stages/outdated_documents_search_bulk_index.test.ts
    Co-authored-by: Rudolf Meijering <skaapgif@gmail.com>

--- src/core/packages/saved-objects/migration-server-internal/src/zdt/model/stages/outdated_documents_search_bulk_index.test.ts
+++ src/core/packages/saved-objects/migration-server-internal/src/zdt/model/stages/outdated_documents_search_bulk_index.test.ts
@@ -71,25 +71,6 @@ describe('Stage: outdatedDocumentsSearchBulkIndex', () => {
     });
   });
 
-  it('OUTDATED_DOCUMENTS_SEARCH_BULK_INDEX -> OUTDATED_DOCUMENTS_SEARCH_READ updates logs correctly', () => {
-    const state = createState({
-      currentBatch: 1,
-      bulkOperationBatches: [[{ create: {} }], [{ create: {} }]],
-    });
-    const res: StateActionResponse<'OUTDATED_DOCUMENTS_SEARCH_BULK_INDEX'> =
-      Either.right('bulk_index_succeeded');
-
-    const newState = outdatedDocumentsSearchBulkIndex(state, res, context);
-
-    expect(newState).toEqual({
-      ...state,
-      controlState: 'OUTDATED_DOCUMENTS_SEARCH_READ',
-      corruptDocumentIds: [],
-      transformErrors: [],
-      hasTransformedDocs: true,
-    });
-  });
-
   it('OUTDATED_DOCUMENTS_SEARCH_BULK_INDEX -> FATAL in case of request_entity_too_large_exception', () => {
     const state = createState({
       currentBatch: 1,
