f4eb9b843ee34e013741eb2305fb98c3dc59fac2
Faisal Kanout <faisal.kanout@elastic.co>
2025-09-02 17:39:30 +0200

    TEST LAZY LOADING NOT TO MERGE

--- x-pack/solutions/observability/plugins/observability/public/pages/alert_details/components/add_to_case_button.tsx
+++ x-pack/solutions/observability/plugins/observability/public/pages/alert_details/components/add_to_case_button.tsx
@@ -5,16 +5,19 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { lazy, Suspense, useState } from 'react';
 import { i18n } from '@kbn/i18n';
 import type { CaseAttachmentsWithoutOwner } from '@kbn/cases-plugin/public/types';
 import { AttachmentType } from '@kbn/cases-plugin/common';
-import { EuiButton, EuiText } from '@elastic/eui';
+import { EuiButton, EuiText, EuiLoadingSpinner } from '@elastic/eui';
 import { ALERT_UUID } from '@kbn/rule-data-utils';
 
 import type { Rule } from '@kbn/alerts-ui-shared';
 import { useKibana } from '../../../utils/kibana_react';
 import type { TopAlert } from '../../../typings/alerts';
+
+// Lazy load the modal component
+const CaseModal = lazy(() => import('./case_modal'));
 
 export function AddToCaseButton({
   alert,
@@ -27,24 +30,11 @@ export function AddToCaseButton({
   rule?: Rule;
   setIsPopoverOpen: (isOpen: boolean) => void;
 }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const {
     services,
     services: { telemetryClient },
   } = useKibana();
-
-  const selectCaseModal = services.cases?.hooks.useCasesAddToExistingCaseModal({
-    onSuccess: ({ updatedAt }) => {
-      // If the case is newly created the updatedAt will be null
-      // onSuccess doesn't provide a way to know if the case was created or updated
-      // onCreateCaseClicked callback is NOT triggered
-      const isNewCaseCreated = !updatedAt;
-      telemetryClient.reportAlertAddedToCase(
-        isNewCaseCreated,
-        'alertDetails.addToCaseBtn',
-        rule?.ruleTypeId || 'unknown'
-      );
-    },
-  });
 
   const attachments: CaseAttachmentsWithoutOwner =
     alert && rule
@@ -63,21 +53,47 @@ export function AddToCaseButton({
 
   const handleAddToCase = () => {
     setIsPopoverOpen(false);
-    selectCaseModal?.open({ getAttachments: () => attachments });
+    setIsModalOpen(true);
+  };
+
+  const handleCaseSuccess = ({ updatedAt }: { updatedAt?: string }) => {
+    // If the case is newly created the updatedAt will be null
+    // onSuccess doesn't provide a way to know if the case was created or updated
+    // onCreateCaseClicked callback is NOT triggered
+    const isNewCaseCreated = !updatedAt;
+    telemetryClient.reportAlertAddedToCase(
+      isNewCaseCreated,
+      'alertDetails.addToCaseBtn',
+      rule?.ruleTypeId || 'unknown'
+    );
+    setIsModalOpen(false);
   };
 
   return (
-    <EuiButton
-      fill
-      iconType="plus"
-      onClick={handleAddToCase}
-      data-test-subj={`add-to-cases-button-${rule?.ruleTypeId}`}
-    >
-      <EuiText size="s">
-        {i18n.translate('xpack.observability.alertDetails.addToCase', {
-          defaultMessage: 'Add to case',
-        })}
-      </EuiText>
-    </EuiButton>
+    <>
+      <EuiButton
+        fill
+        iconType="plus"
+        onClick={handleAddToCase}
+        data-test-subj={`add-to-cases-button-${rule?.ruleTypeId}`}
+      >
+        <EuiText size="s">
+          {i18n.translate('xpack.observability.alertDetails.addToCase', {
+            defaultMessage: 'Add to case',
+          })}
+        </EuiText>
+      </EuiButton>
+
+      {isModalOpen && (
+        <Suspense fallback={<EuiLoadingSpinner size="l" />}>
+          <CaseModal
+            services={services}
+            attachments={attachments}
+            onClose={() => setIsModalOpen(false)}
+            onSuccess={handleCaseSuccess}
+          />
+        </Suspense>
+      )}
+    </>
   );
 }
