14f3275e5b10139d083d1160e62f0344daf7aaa3
Elena Stoeva <elenastoeva99@gmail.com>
2025-09-02 22:21:05 +0100

    [Streams] Update edit data retention modal

---
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_lifecycle/modal/dsl.tsx
@@ -0,0 +1,147 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { useMemo, useState } from 'react';
+import type { Streams, IngestStreamLifecycle } from '@kbn/streams-schema';
+import { isDslLifecycle } from '@kbn/streams-schema';
+import {
+  EuiButton,
+  EuiContextMenuItem,
+  EuiContextMenuPanel,
+  EuiFieldText,
+  EuiPopover,
+  EuiSpacer,
+  EuiText,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { useBoolean } from '@kbn/react-hooks';
+import { parseDuration } from '../helpers';
+
+interface Props {
+  definition: Streams.ingest.all.GetResponse;
+  isDisabled: boolean;
+  value?: string;
+  setLifecycle: (lifecycle: IngestStreamLifecycle) => void;
+}
+
+const isInvalidRetention = (value: string) => {
+  const num = Number(value);
+  return isNaN(num) || num < 1 || num % 1 > 0;
+};
+
+export function DslField({ definition, isDisabled, value, setLifecycle }: Props) {
+  const timeUnits = [
+    { name: 'Days', value: 'd' },
+    { name: 'Hours', value: 'h' },
+    { name: 'Minutes', value: 'm' },
+    { name: 'Seconds', value: 's' },
+  ];
+
+  const existingRetention = value
+    ? parseDuration(value)
+    : isDslLifecycle(definition.effective_lifecycle)
+    ? parseDuration(definition.effective_lifecycle.dsl.data_retention)
+    : undefined;
+  const [selectedUnit, setSelectedUnit] = useState(
+    (existingRetention && timeUnits.find((unit) => unit.value === existingRetention.unit)) ||
+      timeUnits[0]
+  );
+  const [retentionValue, setRetentionValue] = useState(
+    (existingRetention && existingRetention.value?.toString()) || '1'
+  );
+  const [showUnitMenu, { on: openUnitMenu, off: closeUnitMenu }] = useBoolean(false);
+  const invalidRetention = useMemo(() => isInvalidRetention(retentionValue), [retentionValue]);
+
+  return (
+    <>
+      <EuiFieldText
+        data-test-subj="streamsAppDslModalDaysField"
+        value={retentionValue}
+        onChange={(e) => {
+          setRetentionValue(e.target.value);
+          setLifecycle({
+            dsl: {
+              data_retention: `${Number(retentionValue)}${selectedUnit.value}`,
+            },
+          });
+        }}
+        disabled={isDisabled}
+        fullWidth
+        isInvalid={invalidRetention}
+        append={
+          <EuiPopover
+            isOpen={showUnitMenu}
+            panelPaddingSize="none"
+            closePopover={closeUnitMenu}
+            button={
+              <EuiButton
+                data-test-subj="streamsAppDslModalButton"
+                disabled={isDisabled}
+                iconType="arrowDown"
+                iconSide="right"
+                color="text"
+                onClick={openUnitMenu}
+              >
+                {selectedUnit.name}
+              </EuiButton>
+            }
+          >
+            <EuiContextMenuPanel
+              size="s"
+              items={timeUnits.map((unit) => (
+                <EuiContextMenuItem
+                  key={unit.value}
+                  icon={selectedUnit.value === unit.value ? 'check' : 'empty'}
+                  onClick={() => {
+                    closeUnitMenu();
+                    setSelectedUnit(unit);
+                    setLifecycle({
+                      dsl: {
+                        data_retention: `${Number(retentionValue)}${selectedUnit.value}`,
+                      },
+                    });
+                  }}
+                >
+                  {unit.name}
+                </EuiContextMenuItem>
+              ))}
+            />
+          </EuiPopover>
+        }
+      />
+      {invalidRetention && !isDisabled ? (
+        <>
+          <EuiSpacer size="xs" />
+          <EuiText color="danger" size="xs">
+            {i18n.translate('xpack.streams.streamDetailLifecycle.invalidRetentionValue', {
+              defaultMessage: 'A positive integer is required',
+            })}
+          </EuiText>
+        </>
+      ) : null}
+    </>
+  );
+}
+
+{
+  /* <ModalFooter
+  definition={definition}
+  confirmationLabel="Save"
+  closeModal={closeModal}
+  confirmationIsDisabled={invalidRetention}
+  onConfirm={() => {
+    updateLifecycle({
+      dsl: {
+        data_retention: noRetention
+          ? undefined
+          : `${Number(retentionValue)}${selectedUnit.value}`,
+      },
+    });
+  }}
+  updateInProgress={updateInProgress}
+/> */
+}
