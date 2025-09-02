ddcf01feef09c179f5b413bba556887e352e3c5a
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-09-02 06:02:19 -0400

    [Fleet] Allow to configure monitoring runtime in agent policy (#233345)
    (cherry picked from commit 52c02e9d73625926f1f3592275db62ed1cbd71ec)
    
    # Conflicts:
    #	oas_docs/bundle.json
    #	oas_docs/output/kibana.yaml
    #	x-pack/platform/plugins/private/translations/translations/de-DE.json
    #	x-pack/platform/plugins/shared/fleet/server/types/models/agent_policy.ts

--- x-pack/platform/plugins/shared/fleet/server/types/models/agent_policy.test.ts
+++ x-pack/platform/plugins/shared/fleet/server/types/models/agent_policy.test.ts
@@ -4,7 +4,11 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+import { set } from '@kbn/safer-lodash-set';
+
 import type { AgentlessPolicy, GlobalDataTag } from '../../../common/types';
+import { getSettings } from '../../services/form_settings';
 
 import { AgentPolicyBaseSchema } from './agent_policy';
 
