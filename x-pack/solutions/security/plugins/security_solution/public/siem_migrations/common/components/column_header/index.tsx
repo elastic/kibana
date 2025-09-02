06119f01e0e655b89ce06a563d2f257575b58d07
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-09-02 13:17:41 +0200

    [Automatic migrations][UI] Common components - Step 1 (#13759) (#233680)
    ## Summary
    
    Part of: https://github.com/elastic/security-team/issues/13759
    
    With this PR, I moved some of the common components from the siem
    migration `rules` domain into a common directory. This will allow us to
    reuse those components while implementing `dashboards` UI.
    
    This is a first extraction of the common components and more will follow
    later. Chunking this restructuring into a smaller bits for an easier
    review process and to avoid too many conflicts with other PRs.

---
+++ x-pack/solutions/security/plugins/security_solution/public/siem_migrations/common/components/column_header/index.tsx
@@ -0,0 +1,31 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+
+import { EuiToolTip, EuiIcon } from '@elastic/eui';
+
+interface TableHeaderProps {
+  id?: string;
+  title: string;
+  tooltipContent?: React.ReactNode;
+}
+
+export const TableHeader: React.FC<TableHeaderProps> = React.memo(
+  ({ id, title, tooltipContent }) => {
+    return (
+      <EuiToolTip content={tooltipContent}>
+        <div id={id}>
+          {title}
+          &nbsp;
+          <EuiIcon size="s" type="question" color="subdued" className="eui-alignTop" />
+        </div>
+      </EuiToolTip>
+    );
+  }
+);
+TableHeader.displayName = 'TableHeader';
