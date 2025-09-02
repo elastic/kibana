e99ef4ff859cbd94d5233ec3f7355d7c67b5adfc
Timothy Sullivan <tsullivan@elastic.co>
2025-09-02 12:23:17 -0700

    Plugin rename

--- examples/flyout_system/public/index.ts
+++ examples/flyout_system/public/index.ts
@@ -7,8 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { GlobalFlyoutOrchestratorExamplePlugin } from './plugin';
+import { FlyoutSystemExamplesPlugin } from './plugin';
 
 export function plugin() {
-  return new GlobalFlyoutOrchestratorExamplePlugin();
+  return new FlyoutSystemExamplesPlugin();
 }
