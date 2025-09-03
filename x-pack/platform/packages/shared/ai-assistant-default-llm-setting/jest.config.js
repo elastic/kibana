934564bb177103f086e98f780b3f2e649a27f793
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2025-09-03 17:36:45 +0100

    add tests

--- x-pack/platform/packages/shared/ai-assistant-default-llm-setting/jest.config.js
+++ x-pack/platform/packages/shared/ai-assistant-default-llm-setting/jest.config.js
@@ -6,6 +6,12 @@
  */
 
 module.exports = {
+  coverageDirectory: '<rootDir>/target/kibana-coverage/jest/x-pack/packages/kbn_ai_assistant_src',
+  coverageReporters: ['text', 'html'],
+  collectCoverageFrom: [
+    '<rootDir>/x-pack/platform/packages/shared/kbn-ai-assistant/src/**/*.{ts,tsx}',
+    '!<rootDir>/x-pack/platform/packages/shared/kbn-ai-assistant/src/*.test.{ts,tsx}',
+  ],
   preset: '@kbn/test',
   rootDir: '../../../../..',
   roots: ['<rootDir>/x-pack/platform/packages/shared/ai-assistant-default-llm-setting'],
