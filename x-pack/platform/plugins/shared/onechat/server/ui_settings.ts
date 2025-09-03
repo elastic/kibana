8d5e6ed95f8914915c210fd242c58d6396fe0c95
Jedr Blaszyk <jedr.blaszyk@elastic.co>
2025-09-03 15:32:36 +0200

    feat: add 1chat unified ui setting

--- x-pack/platform/plugins/shared/onechat/server/ui_settings.ts
+++ x-pack/platform/plugins/shared/onechat/server/ui_settings.ts
@@ -21,8 +21,9 @@ export const registerUISettings = ({ uiSettings }: { uiSettings: UiSettingsServi
       }),
       schema: schema.boolean(),
       value: false,
+      readonly: true,
+      readonlyMode: 'ui',
       technicalPreview: true,
-      category: ['discover'],
       requiresPageReload: true,
     },
   });
