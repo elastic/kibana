e657c1edad65c3149e56c7d9f0be11bae309fb37
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-09-04 16:19:36 +0200

    Change type import

--- src/platform/packages/shared/content-management/content_editor/src/types.ts
+++ src/platform/packages/shared/content-management/content_editor/src/types.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { SavedObjectAccessControl } from '@kbn/core/server';
+import type { SavedObjectAccessControl } from '@kbn/core-saved-objects-common';
 import type { SavedObjectsReference } from './services';
 
 export interface Item {
