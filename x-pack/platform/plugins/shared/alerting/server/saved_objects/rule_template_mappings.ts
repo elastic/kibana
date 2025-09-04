8b1a3b348fc6bc6997afbccf7ee341b5bc467aeb
MichelLosier <michel.losier@elastic.co>
2025-09-04 14:49:49 -0700

    Have rule template mappings use own definitions

--- x-pack/platform/plugins/shared/alerting/server/saved_objects/rule_template_mappings.ts
+++ x-pack/platform/plugins/shared/alerting/server/saved_objects/rule_template_mappings.ts
@@ -5,15 +5,28 @@
  * 2.0.
  */
 
-import { pick } from 'lodash';
 import type { SavedObjectsTypeMappingDefinition } from '@kbn/core/server';
-
-import { alertMappings } from '../../common/saved_objects/rules/mappings';
 
 export const ruleTemplateMappings: SavedObjectsTypeMappingDefinition = {
   dynamic: false,
   properties: {
-    ...pick(alertMappings.properties, ['name', 'tags', 'consumer']),
-    ruleTypeId: alertMappings.properties.alertTypeId,
+    name: {
+      type: 'text',
+      fields: {
+        keyword: {
+          type: 'keyword',
+          normalizer: 'lowercase',
+        },
+      },
+    },
+    consumer: {
+      type: 'keyword',
+    },
+    ruleTypeId: {
+      type: 'keyword',
+    },
+    tags: {
+      type: 'keyword',
+    },
   },
 };
