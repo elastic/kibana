f4f808ecc1af74529079b00773aa228c303f817a
Justin Kambic <jk@elastic.co>
2025-09-04 17:10:35 -0400

    Add `Open in Discover` feature to Synthetics alerts.

---
+++ x-pack/solutions/observability/plugins/observability/public/pages/util/synthetics_alert_params_to_kql.ts
@@ -0,0 +1,120 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { fromKueryExpression, type KueryNode, nodeBuilder, toKqlExpression } from '@kbn/es-query';
+import type { SyntheticsMonitorStatusRuleParams } from '@kbn/response-ops-rule-params/synthetics_monitor_status';
+import type { TLSRuleParams } from '@kbn/response-ops-rule-params/synthetics_tls';
+
+const MS_TO_DAYS = 1000 * 60 * 60 * 24;
+
+const supportedAlertParams = [
+  'tags',
+  'locations',
+  'monitorIds',
+  'monitorTypes',
+  'projects',
+] as const;
+
+type SupportedAlertParam = (typeof supportedAlertParams)[number];
+
+const fieldMap: Record<SupportedAlertParam, string> = {
+  locations: 'observer.name',
+  monitorIds: 'monitor.id',
+  monitorTypes: 'monitor.type',
+  projects: 'project.id',
+  tags: 'tags',
+};
+function getFieldnameForKey(key: SupportedAlertParam): string {
+  if (!fieldMap[key]) {
+    throw new Error(`Unsupported alert param: ${key}`);
+  }
+  return fieldMap[key];
+}
+
+function listToKqlNode(fieldname: string, list: string[]): KueryNode {
+  if (list.length === 0) {
+    throw new Error(`Alert params ${fieldname ?? 'field'} must be a non-empty array`);
+  }
+  return nodeBuilder.or(list.map((item) => nodeBuilder.is(fieldname, item)));
+}
+
+function mapExtraSyntheticsFilters(
+  extraFilters: Partial<Record<SupportedAlertParam, string[] | undefined>>,
+  kqlQuery?: string
+): KueryNode[] {
+  const filters: KueryNode[] = [];
+
+  if (kqlQuery) {
+    filters.push(fromKueryExpression(kqlQuery));
+  }
+
+  filters.push(
+    ...supportedAlertParams
+      .filter((key) => Array.isArray(extraFilters[key]) && extraFilters[key]!.length > 0)
+      .map((key) => listToKqlNode(getFieldnameForKey(key), extraFilters[key]!))
+  );
+
+  return filters;
+}
+
+/**
+ *
+ * @param params from a Synthetics Monitor Status Alert
+ * @returns KQL query string
+ */
+export function syntheticsMonitorStatusAlertParamsToKqlQuery(
+  params: SyntheticsMonitorStatusRuleParams
+): string {
+  const { condition, kqlQuery, ...rest } = params;
+
+  const filters = mapExtraSyntheticsFilters(rest, kqlQuery);
+
+  filters.push(nodeBuilder.is('monitor.status', 'down'));
+
+  return toKqlExpression(nodeBuilder.and(filters));
+}
+
+/**
+ * Maps the params for a Synthetics TLS Alert to a KQL query string.
+ *
+ * @param params from a Synthetics TLS Alert
+ * @returns KQL query string
+ */
+export function syntheticsTlsAlertParamsToKqlQuery(params: TLSRuleParams): string {
+  const { certAgeThreshold, certExpirationThreshold, search, kqlQuery, ...rest } = params;
+
+  const expirationFilters: KueryNode[] = [];
+  const filters: KueryNode[] = [];
+
+  if (certExpirationThreshold) {
+    expirationFilters.push(
+      nodeBuilder.range(
+        'tls.server.x509.not_after',
+        'lt',
+        new Date(Date.now() + certExpirationThreshold * MS_TO_DAYS).toISOString()
+      )
+    );
+  }
+
+  if (certAgeThreshold) {
+    expirationFilters.push(
+      nodeBuilder.range(
+        'tls.server.x509.not_before',
+        'lt',
+        new Date(Date.now() - certAgeThreshold * MS_TO_DAYS).toISOString()
+      )
+    );
+  }
+
+  if (expirationFilters.length) {
+    filters.push(nodeBuilder.or(expirationFilters));
+  }
+
+  filters.push(...mapExtraSyntheticsFilters(rest, kqlQuery));
+
+  return toKqlExpression(nodeBuilder.and(filters));
+}
