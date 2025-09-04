8ea2b8a20e749185f6870447bbd4f4d1d3b52964
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2025-09-04 20:12:00 +0200

    crowdstrike script

---
+++ x-pack/solutions/security/plugins/security_solution/scripts/endpoint/crowdstrike_host/index.ts
@@ -0,0 +1,298 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { run, type RunFn } from '@kbn/dev-cli-runner';
+import { ok } from 'assert';
+import { existsSync as fileExistsSync } from 'fs';
+import { DEFAULT_SPACE_ID } from '@kbn/spaces-plugin/common';
+import { DEFAULT_API_URL } from './services/constants';
+import { createDetectionEngineCrowdStrikeRuleIfNeeded } from './services/create_detection_engine_rule';
+import { onboardVmHostWithCrowdStrike } from './services/install_crowdstrike_agent';
+import {
+  getMultipassVmCountNotice,
+  createVm,
+  generateVmName,
+  findVm,
+  createMultipassHostVmClient,
+} from '../common/vm_services';
+import { createKbnClient } from '../common/stack_services';
+import { ensureSpaceIdExists, fetchActiveSpace } from '../common/spaces';
+import { createToolingLogger } from '../../../common/endpoint/data_loaders/utils';
+import { createCrowdStrikeConnectorIfNeeded } from './services/create_crowdstrike_connector';
+import {
+  addCrowdStrikeIntegrationToAgentPolicy,
+  DEFAULT_AGENTLESS_INTEGRATIONS_AGENT_POLICY_NAME,
+  enableFleetSpaceAwareness,
+  enrollHostVmWithFleet,
+  fetchAgentPolicy,
+  getOrCreateDefaultAgentPolicy,
+} from '../common/fleet_services';
+import {
+  isFleetServerRunning,
+  startFleetServer,
+} from '../common/fleet_server/fleet_server_services';
+import type { HostVm } from '../common/types';
+
+export const cli = async () => {
+  return run(runCli, {
+    description: `Sets up the kibana system so that CrowdStrike Falcon hosts data can be be streamed to Elasticsearch.
+It will first setup a host VM that runs the CrowdStrike Falcon sensor on it. This VM will ensure that data is being
+created in CrowdStrike's system.
+
+It will then also setup a second VM (if necessary) that runs Elastic Agent along with the CrowdStrike integration
+policy (an agent-less integration) - this is the process that then connects to the CrowdStrike Falcon API
+and pushes the data to Elasticsearch.
+
+Finally, it will also setup a CrowdStrike connector that connects to the CrowdStrike Falcon API and allows
+for response actions to be performed on hosts.`,
+    flags: {
+      string: [
+        'clientId',
+        'clientSecret',
+        'customerId',
+        'apiUrl',
+        'sensorInstaller',
+        'platform',
+        'kibanaUrl',
+        'username',
+        'password',
+        'vmName',
+        'spaceId',
+        'apiKey',
+        'version',
+        'policy',
+      ],
+      boolean: ['forceFleetServer', 'forceNewHost'],
+      default: {
+        apiUrl: DEFAULT_API_URL,
+        platform: 'ubuntu',
+        kibanaUrl: 'http://127.0.0.1:5601',
+        username: 'elastic',
+        password: 'changeme',
+        spaceId: '',
+        apiKey: '',
+        policy: '',
+      },
+      help: `
+      --sensorInstaller   Optional. The local path to the CrowdStrike Falcon sensor installer package. If not provided,
+                          the sensor will be downloaded automatically from CrowdStrike based on the specified platform.
+      --platform          Optional. The target platform for the sensor installation: windows, ubuntu, or macos.
+                          (Default: ubuntu)
+      --clientId          Required. The CrowdStrike Falcon API client ID with privileges to access the Falcon API.
+      --clientSecret      Required. The client secret created for the registered application's API access.
+      --customerId        Required. The CrowdStrike Customer ID (CID) used for sensor enrollment. This is different from
+                          clientId and can be found in your CrowdStrike Falcon console under "Host Setup and Management".
+      --apiUrl:           Optional. The URL for making API calls to CrowdStrike Falcon management system.
+                          Must match your CrowdStrike cloud region (e.g., https://api.us-2.crowdstrike.com for US-2,
+                          https://api.eu-1.crowdstrike.com for EU-1, https://api.us-gov-1.crowdstrike.com for GovCloud).
+                          (Default: https://api.crowdstrike.com)
+      --vmName            Optional. The name for the VM.
+                          Default: [current login user name]-crowdstrike-[unique number]
+      --policy            Optional. The UUID of the Fleet Agent Policy that should be used to setup
+                          the CrowdStrike Integration
+                          Default: re-uses existing dev policy (if found) or creates a new one
+      --forceFleetServer  Optional. If fleet server should be started/configured even if it seems
+                          like it is already setup. (Default: false)
+      --forceNewHost      Optional. Force a new VM host to be created and enrolled with CrowdStrike.
+                          By default, a check is done to see if a host running CrowdStrike is
+                          already running and if so, a new one will not be created - unless this
+                          option is used (Default: false)
+      --username          Optional. User name to be used for auth against elasticsearch and
+                          kibana (Default: elastic).
+      --password          Optional. Password associated with the username (Default: changeme)
+      --apiKey            Optional. A Kibana API key to use for authz. When defined, 'username'
+                          and 'password' arguments are ignored.
+      --spaceId           Optional. The space id where the host should be added to in kibana. The
+                          space will be created if it does not exist. Default: default space.
+      --kibanaUrl         Optional. The url to Kibana (Default: http://127.0.0.1:5601)
+      --version           Optional. The version of the Agent to use for enrolling the new host.
+                          Default: uses the same version as the stack (kibana). Version
+                          can also be from 'SNAPSHOT'.
+                          Examples: 8.6.0, 8.7.0-SNAPSHOT
+`,
+    },
+  });
+};
+
+const runCli: RunFn = async ({ log, flags }) => {
+  const sensorInstaller = flags.sensorInstaller as string;
+  const platform = flags.platform as string;
+  const clientId = flags.clientId as string;
+  const clientSecret = flags.clientSecret as string;
+  const customerId = flags.customerId as string;
+  const apiUrl = flags.apiUrl as string;
+  const username = flags.username as string;
+  const password = flags.password as string;
+  const kibanaUrl = flags.kibanaUrl as string;
+  const spaceId = flags.spaceId as string;
+  const apiKey = flags.apiKey as string;
+  const forceNewHost = flags.forceNewHost as boolean;
+  const forceFleetServer = flags.forceFleetServer as boolean;
+  const policy = flags.policy as string;
+  const version = flags.version as string | undefined;
+  const vmName = flags.vmName as string;
+
+  const getRequiredArgMessage = (argName: string) => `${argName} argument is required`;
+
+  createToolingLogger.setDefaultLogLevelFromCliFlags(flags);
+
+  ok(clientId, getRequiredArgMessage('clientId'));
+  ok(clientSecret, getRequiredArgMessage('clientSecret'));
+  ok(customerId, getRequiredArgMessage('customerId'));
+  ok(apiUrl, getRequiredArgMessage('apiUrl'));
+
+  // Validate platform
+  const validPlatforms = ['windows', 'ubuntu', 'macos'];
+  ok(validPlatforms.includes(platform), `platform must be one of: ${validPlatforms.join(', ')}`);
+
+  // Validate sensor installer if provided
+  if (sensorInstaller) {
+    ok(
+      fileExistsSync(sensorInstaller),
+      `sensorInstaller file path does not exist! [${sensorInstaller}]`
+    );
+  }
+
+  const kbnClient = createKbnClient({
+    log,
+    url: kibanaUrl,
+    username,
+    password,
+    spaceId,
+    apiKey,
+  });
+
+  if (spaceId && spaceId !== DEFAULT_SPACE_ID) {
+    await ensureSpaceIdExists(kbnClient, spaceId, { log });
+    await enableFleetSpaceAwareness(kbnClient);
+  }
+
+  const activeSpace = await fetchActiveSpace(kbnClient);
+  const activeSpaceId = activeSpace.id;
+
+  const crowdStrikeVm = await onboardVmHostWithCrowdStrike({
+    kbnClient,
+    log,
+    vmName,
+    forceNewHost,
+    sensorInstaller,
+    platform,
+    clientId,
+    clientSecret,
+    customerId,
+    apiUrl,
+  });
+
+  // Get or create agent policy for CrowdStrike integration
+  const {
+    id: agentPolicyId,
+    agents = 0,
+    name: agentPolicyName,
+    namespace: agentPolicyNamespace,
+  } = policy
+    ? await fetchAgentPolicy(kbnClient, policy)
+    : await getOrCreateDefaultAgentPolicy({
+        kbnClient,
+        log,
+        policyName: `${DEFAULT_AGENTLESS_INTEGRATIONS_AGENT_POLICY_NAME} - ${activeSpaceId}`,
+      });
+
+  // Add CrowdStrike integration to agent policy
+  const integrationPolicy = await addCrowdStrikeIntegrationToAgentPolicy({
+    kbnClient,
+    log,
+    agentPolicyId,
+    apiUrl,
+    clientId,
+    clientSecret,
+  });
+
+  let agentPolicyVm: HostVm | undefined;
+
+  // If no agents are running against the given Agent policy for agentless integrations, then add one now
+  if (!agents) {
+    // Check for existing Fleet agent VMs before creating a new one
+    const agentVmName = generateVmName(`agentless-integrations-${activeSpaceId}`);
+    const existingAgentVms = await findVm(
+      'multipass',
+      new RegExp(`^${agentVmName.substring(0, agentVmName.lastIndexOf('-'))}`)
+    );
+
+    if (existingAgentVms.data.length > 0) {
+      log.info(
+        `A Fleet agent VM already exists - will reuse it: ${existingAgentVms.data[0].name}.\nTIP: Use 'multipass delete ${existingAgentVms.data[0].name}' to force creation of a new one`
+      );
+
+      agentPolicyVm = await createMultipassHostVmClient(existingAgentVms.data[0], log);
+    } else {
+      log.info(`Creating VM and enrolling it with Fleet using policy [${agentPolicyName}]`);
+
+      agentPolicyVm = await createVm({
+        type: 'multipass',
+        name: agentVmName,
+      });
+    }
+
+    if (forceFleetServer || !(await isFleetServerRunning(kbnClient, log))) {
+      await startFleetServer({
+        kbnClient,
+        logger: log,
+        force: forceFleetServer,
+        version,
+      });
+
+      // Wait a moment for Fleet Server to be fully ready
+      log.info('Waiting for Fleet Server to be fully operational...');
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+
+    await enrollHostVmWithFleet({
+      hostVm: agentPolicyVm,
+      kbnClient,
+      log,
+      agentPolicyId,
+      version,
+    });
+  } else {
+    log.debug(
+      `No host VM created for Fleet agent policy [${agentPolicyName}]. It already shows to have [${agents}] enrolled`
+    );
+  }
+
+  await Promise.all([
+    createCrowdStrikeConnectorIfNeeded({
+      kbnClient,
+      log,
+      clientId,
+      clientSecret,
+      apiUrl,
+    }),
+    createDetectionEngineCrowdStrikeRuleIfNeeded(
+      kbnClient,
+      log,
+      integrationPolicy.namespace || agentPolicyNamespace
+    ),
+    // Trigger alert on the Linux VM
+    crowdStrikeVm
+      .exec('curl -o /tmp/eicar.com.txt https://secure.eicar.org/eicar.com.txt')
+      .then(() => {
+        console.log('Eicar triggered on host!');
+      })
+      .catch((err) => {
+        log.warning(
+          `Attempted to trigger an alert on host [${crowdStrikeVm.name}], but failed with: ${err.message}`
+        );
+      }),
+  ]);
+
+  log.info(`Done!
+
+${crowdStrikeVm.info()}
+${agentPolicyVm ? `${agentPolicyVm.info()}\n` : ''}
+${await getMultipassVmCountNotice(agentPolicyVm ? 2 : 1)}
+`);
+};
---
+++ x-pack/solutions/security/plugins/security_solution/scripts/endpoint/crowdstrike_host/services/create_detection_engine_rule.test.ts
@@ -0,0 +1,95 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { KbnClient } from '@kbn/test';
+import { createToolingLogger } from '../../../../common/endpoint/data_loaders/utils';
+import { createDetectionEngineCrowdStrikeRuleIfNeeded } from './create_detection_engine_rule';
+import * as detectionRulesServices from '../../common/detection_rules_services';
+
+jest.mock('../../common/detection_rules_services');
+
+const mockedDetectionRulesServices = detectionRulesServices as jest.Mocked<
+  typeof detectionRulesServices
+>;
+
+describe('createDetectionEngineCrowdStrikeRuleIfNeeded', () => {
+  let mockKbnClient: jest.Mocked<KbnClient>;
+  let mockLog: ReturnType<typeof createToolingLogger>;
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    mockKbnClient = {
+      request: jest.fn(),
+    } as jest.Mocked<KbnClient>;
+
+    mockLog = createToolingLogger();
+  });
+
+  it('should skip creation if rule already exists', async () => {
+    const existingRule = {
+      name: 'Promote CrowdStrike alerts',
+      id: 'existing-rule-id',
+    };
+    mockedDetectionRulesServices.findRules.mockResolvedValue({
+      data: [existingRule],
+    });
+
+    const result = await createDetectionEngineCrowdStrikeRuleIfNeeded(mockKbnClient, mockLog);
+
+    expect(result).toBe(existingRule);
+    expect(mockedDetectionRulesServices.findRules).toHaveBeenCalledWith(mockKbnClient, {
+      filter: 'alert.attributes.tags:("dev-script-run-crowdstrike-host")',
+    });
+    expect(mockedDetectionRulesServices.createRule).not.toHaveBeenCalled();
+  });
+
+  it('should create new rule if none exists', async () => {
+    mockedDetectionRulesServices.findRules.mockResolvedValue({ data: [] });
+    const newRule = {
+      name: 'Promote CrowdStrike alerts',
+      id: 'new-rule-id',
+    };
+    mockedDetectionRulesServices.createRule.mockResolvedValue(newRule);
+
+    const result = await createDetectionEngineCrowdStrikeRuleIfNeeded(mockKbnClient, mockLog);
+
+    expect(result).toBe(newRule);
+    expect(mockedDetectionRulesServices.createRule).toHaveBeenCalledWith(mockKbnClient, {
+      index: ['logs-crowdstrike.alert*', 'logs-crowdstrike.detection*', 'logs-crowdstrike.fdr*'],
+      query: 'crowdstrike.event.aid:* OR crowdstrike.detection.aid:*',
+      from: 'now-3660s',
+      name: 'Promote CrowdStrike alerts',
+      description: expect.stringContaining('Created by dev script'),
+      tags: ['dev-script-run-crowdstrike-host'],
+    });
+  });
+
+  it('should include namespace in index patterns when provided', async () => {
+    mockedDetectionRulesServices.findRules.mockResolvedValue({ data: [] });
+    const newRule = {
+      name: 'Promote CrowdStrike alerts',
+      id: 'new-rule-id',
+    };
+    mockedDetectionRulesServices.createRule.mockResolvedValue(newRule);
+
+    await createDetectionEngineCrowdStrikeRuleIfNeeded(mockKbnClient, mockLog, 'test-namespace');
+
+    expect(mockedDetectionRulesServices.createRule).toHaveBeenCalledWith(mockKbnClient, {
+      index: [
+        'logs-crowdstrike.alert-test-namespace*',
+        'logs-crowdstrike.detection-test-namespace*',
+        'logs-crowdstrike.fdr-test-namespace*',
+      ],
+      query: 'crowdstrike.event.aid:* OR crowdstrike.detection.aid:*',
+      from: 'now-3660s',
+      name: 'Promote CrowdStrike alerts',
+      description: expect.stringContaining('Created by dev script'),
+      tags: ['dev-script-run-crowdstrike-host'],
+    });
+  });
+});
