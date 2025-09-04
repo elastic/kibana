5f8c9dcf10d52e82f1e0db35700a6ce5dc6bee96
Angela Chuang <yi-chun.chuang@elastic.co>
2025-09-04 23:29:56 +0800

    lint

--- src/platform/plugins/shared/dashboard/public/services/dashboard_content_management_service/lib/save_dashboard_state.ts
+++ src/platform/plugins/shared/dashboard/public/services/dashboard_content_management_service/lib/save_dashboard_state.ts
@@ -61,7 +61,6 @@ export const saveDashboardState = async ({
             references,
           },
         });
-    console.log('result-----', result);
     const newId = result.item.id;
 
     if (newId) {
