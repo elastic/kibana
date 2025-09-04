8e4de22de047677a341397d3982ec6f49bc486d1
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2025-09-04 16:44:17 +0200

    Fix more places where additionalFieldGroups failed

--- src/platform/packages/shared/kbn-unified-data-table/src/components/data_table.tsx
+++ src/platform/packages/shared/kbn-unified-data-table/src/components/data_table.tsx
@@ -58,7 +58,6 @@ import {
 } from '@kbn/data-grid-in-table-search';
 import { useThrottleFn } from '@kbn/react-hooks';
 import { getDataViewFieldOrCreateFromColumnMeta } from '@kbn/data-view-utils';
-import type { AdditionalFieldGroups } from '@kbn/unified-field-list';
 import { DATA_GRID_DENSITY_STYLE_MAP, useDataGridDensity } from '../hooks/use_data_grid_density';
 import type {
   UnifiedDataTableSettings,
@@ -403,10 +402,6 @@ interface InternalUnifiedDataTableProps {
    */
   externalCustomRenderers?: CustomCellRenderer;
   /**
-   * An optional prop to provide awareness of additional field groups when paired with the Unified Field List.
-   */
-  additionalFieldGroups?: AdditionalFieldGroups;
-  /**
    * An optional settings for customising the column
    */
   customGridColumnsConfiguration?: CustomGridColumnsConfiguration;
@@ -511,7 +506,6 @@ const InternalUnifiedDataTable = ({
   externalAdditionalControls,
   rowsPerPageOptions,
   externalCustomRenderers,
-  additionalFieldGroups,
   consumer = 'discover',
   componentsTourSteps,
   gridStyleOverride,
@@ -1303,7 +1297,6 @@ const InternalUnifiedDataTable = ({
               getDocById={getDocById}
               replaceSelectedDocs={replaceSelectedDocs}
               setIsCompareActive={setIsCompareActive}
-              additionalFieldGroups={additionalFieldGroups}
             />
           ) : (
             <EuiDataGridMemoized
