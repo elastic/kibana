6afe9aa2634d5a09097026c7900b42f5eaa7437a
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2025-09-04 15:17:15 +0200

    remove missed import

--- src/platform/packages/shared/kbn-unified-data-table/src/components/compare_documents/hooks/use_comparison_fields.ts
+++ src/platform/packages/shared/kbn-unified-data-table/src/components/compare_documents/hooks/use_comparison_fields.ts
@@ -9,7 +9,6 @@
 
 import type { DataView } from '@kbn/data-views-plugin/common';
 import type { DataTableRecord } from '@kbn/discover-utils/types';
-import type { AdditionalFieldGroups } from '@kbn/unified-field-list';
 import { isEqual } from 'lodash';
 import { useMemo } from 'react';
 
@@ -22,7 +21,6 @@ export interface UseComparisonFieldsProps {
   showAllFields: boolean;
   showMatchingValues: boolean;
   getDocById: (id: string) => DataTableRecord | undefined;
-  additionalFieldGroups?: AdditionalFieldGroups;
 }
 
 export const useComparisonFields = ({
@@ -32,7 +30,6 @@ export const useComparisonFields = ({
   showAllFields,
   showMatchingValues,
   getDocById,
-  additionalFieldGroups,
 }: UseComparisonFieldsProps) => {
   const { baseDoc, comparisonDocs } = useMemo(() => {
     const [baseDocId, ...comparisonDocIds] = selectedDocIds;
@@ -83,13 +80,5 @@ export const useComparisonFields = ({
     }
 
     return { comparisonFields, totalFields };
-  }, [
-    additionalFieldGroups,
-    baseDoc,
-    comparisonDocs,
-    dataView,
-    selectedFieldNames,
-    showAllFields,
-    showMatchingValues,
-  ]);
+  }, [baseDoc, comparisonDocs, dataView, selectedFieldNames, showAllFields, showMatchingValues]);
 };
