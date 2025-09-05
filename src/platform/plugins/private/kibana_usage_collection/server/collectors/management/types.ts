78382a82e39c63e3b20fa148596c88abe79e2def
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2025-09-05 23:28:14 +0200

    [Streams] Enable UI by default (#233997)
    closes https://github.com/elastic/kibana/issues/232455
    
    ## Summary
    This PR removes the below `enable streams UI` advanced setting option. 
    The setting is enabled by default for observability serverless
    deployment as well as for stateful.
    
    
    ![enable_streams_ui](https://github.com/user-attachments/assets/5e533ab5-ca73-40e4-bbe7-89e9e1c6659c)

--- src/platform/plugins/private/kibana_usage_collection/server/collectors/management/types.ts
+++ src/platform/plugins/private/kibana_usage_collection/server/collectors/management/types.ts
@@ -175,7 +175,6 @@ export interface UsageStats {
   'securitySolution:excludedDataTiersForRuleExecution': string[];
   'securitySolution:maxUnassociatedNotes': number;
   'observability:searchExcludedDataTiers': string[];
-  'observability:enableStreamsUI': boolean;
   'observability:enableDiagnosticMode': boolean;
   'observability:streamsEnableSignificantEvents': boolean;
   'genAiSettings:defaultAIConnector': string;
