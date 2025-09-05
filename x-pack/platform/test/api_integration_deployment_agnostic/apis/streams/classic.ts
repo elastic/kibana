ca8b3f52262384801c310e8175bd805eaf7d1767
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-09-05 13:51:29 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/classic.ts
+++ x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/classic.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 import { Streams, emptyAssets } from '@kbn/streams-schema';
 import type { DeploymentAgnosticFtrProviderContext } from '../../ftr_provider_context';
 import type { StreamsSupertestRepositoryClient } from './helpers/repository_client';
