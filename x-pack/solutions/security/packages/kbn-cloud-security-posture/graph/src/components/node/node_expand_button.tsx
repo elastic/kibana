26d1adc3cfb6f8c85aa1a247e4f69c6ef8057ebe
Alberto Blázquez <alberto.blazquez@elastic.co>
2025-09-05 16:09:53 +0200

    Rename button

--- x-pack/solutions/security/packages/kbn-cloud-security-posture/graph/src/components/node/node_expand_button.tsx
+++ x-pack/solutions/security/packages/kbn-cloud-security-posture/graph/src/components/node/node_expand_button.tsx
@@ -7,7 +7,7 @@
 
 import React, { useCallback, useState } from 'react';
 import { useEuiTheme } from '@elastic/eui';
-import { StyledNodeExpandButton, RoundEuiButtonIcon, ExpandButtonSize } from './styles';
+import { NodeExpandButtonContainer, RoundEuiButtonIcon, ExpandButtonSize } from './styles';
 import type { EntityNodeViewModel, LabelNodeViewModel } from '..';
 import { GRAPH_NODE_EXPAND_BUTTON_ID } from '../test_ids';
 
@@ -37,7 +37,7 @@ export const NodeExpandButton = ({ x, y, color, onClick, ...props }: NodeExpandB
   );
 
   return (
-    <StyledNodeExpandButton x={x} y={y} className={isToggled ? 'toggled' : undefined} {...props}>
+    <NodeExpandButtonContainer x={x} y={y} className={isToggled ? 'toggled' : undefined} {...props}>
       <RoundEuiButtonIcon
         color={color ?? 'primary'}
         backgroundColor={euiTheme.colors.backgroundBasePlain}
@@ -47,7 +47,7 @@ export const NodeExpandButton = ({ x, y, color, onClick, ...props }: NodeExpandB
         aria-label="Open or close node actions"
         data-test-subj={GRAPH_NODE_EXPAND_BUTTON_ID}
       />
-    </StyledNodeExpandButton>
+    </NodeExpandButtonContainer>
   );
 };
 
