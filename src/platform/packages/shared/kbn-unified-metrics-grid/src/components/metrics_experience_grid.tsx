0a3ebeb5fc5d94a4766702d783a1d0be86799e36
jennypavlova <dzheni.pavlova@elastic.co>
2025-09-05 18:27:32 +0200

    [Metrics][Discover] Add an option to clear the selection in the dropdowns (#233776)
    Closes #233653
    
    ## Summary
    
    This PR adds an option to clear the selection in the dimension and value
    selectors. When all dimensions are cleared, it will clear all values as
    well as those related to the corresponding dimensions. The PR also
    extracts the toolbar_selector to a shared place and uses it in the
    metrics grid and the histogram without adding the dependency issue.
    
    <img width="1276" height="669" alt="image"
    src="https://github.com/user-attachments/assets/e83d3338-fbe2-4f87-b2b0-d66ec50b79c9"
    />
    <img width="2328" height="687" alt="image"
    src="https://github.com/user-attachments/assets/6c13b429-8566-4f8f-aa88-35207f524abf"
    />
    
    
    ## How to test
    - Set the following config to `kibana.dev.yml`
    ```yml
    discover.experimental.enabledProfiles:
      - observability-metrics-data-source-profile
    metricsExperience.enabled: true
     ```
    - Navigate to Discover and Switch to ESQL mode
    - Set the space to Observability
    - Run `FROM metrics-*` search
    - Clone https://github.com/simianhacker/simian-forge/tree/main
    - Run ` ./forge --dataset hosts --count 25 --interval 30s`
    - Check the drop-downs in the metrics grid:
    
    
    https://github.com/user-attachments/assets/77313c81-59fa-41ee-b3f9-60384212cfab
    
    
    - In case of `logs-*` search, for example, the breakdown selector should work as before: 
    
    <img width="3544" height="1332" alt="image" src="https://github.com/user-attachments/assets/ce33654f-9835-456a-b47f-6a60ed58edd8" />
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/platform/packages/shared/kbn-unified-metrics-grid/src/components/metrics_experience_grid.tsx
+++ src/platform/packages/shared/kbn-unified-metrics-grid/src/components/metrics_experience_grid.tsx
@@ -83,6 +83,14 @@ export const MetricsExperienceGrid = ({
     [dispatch]
   );
 
+  // Clear both dimensions and filters because if there are no dimensions selected the values should also be cleared
+  const onClearAllDimensions = useCallback(() => {
+    dispatch(setDimensions([]));
+    dispatch(setValueFilters([]));
+  }, [dispatch]);
+
+  const onClearValues = useCallback(() => onValuesChange([]), [onValuesChange]);
+
   const pageSize = columns === 4 ? 20 : 15;
   const actions: IconButtonGroupProps['buttons'] = [
     {
@@ -112,6 +120,7 @@ export const MetricsExperienceGrid = ({
         fields={fields}
         onChange={onDimensionsChange}
         selectedDimensions={dimensions}
+        onClear={onClearAllDimensions}
       />,
       dimensions.length > 0 ? (
         <ValuesSelector
@@ -121,18 +130,21 @@ export const MetricsExperienceGrid = ({
           disabled={dimensions.length === 0}
           indices={[indexPattern]}
           timeRange={timeRange}
+          onClear={onClearValues}
         />
       ) : null,
     ],
     [
-      dimensions,
-      fields,
-      timeRange,
-      indexPattern,
-      onDimensionsChange,
-      onValuesChange,
       renderToggleActions,
+      fields,
+      onDimensionsChange,
+      dimensions,
+      onClearAllDimensions,
       valueFilters,
+      onValuesChange,
+      indexPattern,
+      timeRange,
+      onClearValues,
     ]
   );
 
