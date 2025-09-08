c3120adf257cdd67b48259a2ebcc772b93dcee7d
Umberto Pepato <umberto.pepato@elastic.co>
2025-09-08 14:58:28 +0200

    Fix alerts table columns mgmt and persistence layer

---
+++ src/platform/packages/shared/response-ops/alerts-table/hooks/use_alerts_table_configuration.tsx
@@ -0,0 +1,111 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { type Dispatch, type SetStateAction, useCallback, useState } from 'react';
+import type { IStorageWrapper } from '@kbn/kibana-utils-plugin/public';
+import { noop } from 'lodash';
+import type { NotificationsStart } from '@kbn/core-notifications-browser';
+import { i18n } from '@kbn/i18n';
+import {
+  type AlertsTableConfiguration,
+  alertsTableConfigurationSchema,
+} from '../schemas/alerts_table_configuration_schema';
+
+export interface UseAlertsTableConfigurationParams {
+  id: string;
+  configurationStorage?: IStorageWrapper | null;
+  notifications: NotificationsStart;
+}
+
+export const useAlertsTableConfiguration = ({
+  id,
+  configurationStorage,
+  notifications,
+}: UseAlertsTableConfigurationParams) => {
+  const { toasts } = notifications;
+
+  const loadConfiguration = useCallback(() => {
+    if (!configurationStorage) {
+      return null;
+    }
+    const savedConfig = configurationStorage.get(id);
+
+    if (!savedConfig) {
+      return null;
+    }
+
+    try {
+      return alertsTableConfigurationSchema.parse(JSON.parse(savedConfig));
+    } catch (e) {
+      configurationStorage.remove(id);
+      toasts.addWarning({
+        title: i18n.translate('xpack.responseOpsAlertsTable.saveConfigurationError', {
+          defaultMessage: 'Unable to load alerts table configuration',
+        }),
+        text: i18n.translate('xpack.responseOpsAlertsTable.saveConfigurationError', {
+          defaultMessage:
+            'The saved configuration was invalid and has been reset. Any new table customizations will be saved from now on.',
+        }),
+      });
+    }
+
+    return null;
+  }, [configurationStorage, id, toasts]);
+
+  const [configuration, setConfiguration] = useState(loadConfiguration);
+
+  const safeSetConfiguration = useCallback<
+    Dispatch<SetStateAction<AlertsTableConfiguration | null>>
+  >(
+    (setStateAction) => {
+      if (!configurationStorage) {
+        return;
+      }
+      // It's important to use the functional form of setState to reference the old state instead
+      // of using the `configuration` variable from the outer scope, as that would make this callback
+      // referentially unstable and cause an unnecessary configuration save
+      setConfiguration((prevConfig) => {
+        const newConfig =
+          typeof setStateAction === 'function' ? setStateAction(prevConfig) : setStateAction;
+
+        try {
+          // Parsing the configuration ensures that it is valid and contains only the minimal
+          // user overrides
+          const parsedConfig =
+            newConfig === null ? null : alertsTableConfigurationSchema.parse(newConfig);
+          const serializedConfig = JSON.stringify(parsedConfig);
+
+          if (serializedConfig === JSON.stringify(prevConfig)) {
+            // Avoid causing rerenders and saving to storage if the previous and new configurations
+            // are structurally equivalent
+            return prevConfig;
+          }
+          if (parsedConfig === null) {
+            // If the new config is null, remove the saved configuration
+            configurationStorage.remove(id);
+          } else {
+            configurationStorage.set(id, serializedConfig);
+          }
+          return newConfig;
+        } catch {
+          // TODO add toast?
+        }
+
+        return prevConfig;
+      });
+    },
+    [configurationStorage, id]
+  );
+
+  if (!configurationStorage) {
+    return [null, noop] as const;
+  }
+
+  return [configuration, safeSetConfiguration] as const;
+};
