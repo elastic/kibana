7f82b9767ec158a0ad34cd744fd82fae9eff221a
Shahar Glazner <shaharglazner@gmail.com>
2025-09-08 10:49:16 +0300

    feat: wip

--- x-pack/solutions/security/packages/siem-readiness/jest.config.js
+++ x-pack/solutions/security/packages/siem-readiness/jest.config.js
@@ -5,17 +5,8 @@
  * 2.0.
  */
 
-<<<<<<<< HEAD:x-pack/platform/plugins/shared/alerting/server/saved_objects/schemas/raw_rule_template/index.ts
-import type { TypeOf } from '@kbn/config-schema';
-
-import { rawRuleTemplateSchema } from './v1';
-
-export { rawRuleTemplateSchema as rawRuleTemplateSchemaV1 };
-export type RawRuleTemplate = TypeOf<typeof rawRuleTemplateSchema>;
-========
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
   roots: ['<rootDir>/x-pack/solutions/security/packages/siem-readiness'],
 };
->>>>>>>> main:x-pack/solutions/security/packages/siem-readiness/jest.config.js
--- x-pack/solutions/security/plugins/security_solution/server/lib/siem_readiness/types.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/siem_readiness/types.ts
@@ -5,13 +5,6 @@
  * 2.0.
  */
 
-<<<<<<<< HEAD:x-pack/platform/plugins/shared/lens/server/content_management/v1/schema/get.ts
-import { objectTypeToGetResultSchema } from '@kbn/content-management-utils';
-
-import { lensSavedObjectSchema } from './common';
-
-export const lensCMGetResultSchema = objectTypeToGetResultSchema(lensSavedObjectSchema);
-========
 import type { Logger } from '@kbn/core/server';
 import type { SecuritySolutionPluginRouter } from '../../types';
 
@@ -19,4 +12,3 @@ export interface SiemReadinessRoutesDeps {
   router: SecuritySolutionPluginRouter;
   logger: Logger;
 }
->>>>>>>> main:x-pack/solutions/security/plugins/security_solution/server/lib/siem_readiness/types.ts
