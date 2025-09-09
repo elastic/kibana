7e6824d17e347a6ccd58ba13e8299c6781083bc8
Coen Warmer <coen.warmer@gmail.com>
2025-09-09 17:08:59 +0200

    Revert

--- src/platform/packages/shared/kbn-test/src/jest/run.ts
+++ src/platform/packages/shared/kbn-test/src/jest/run.ts
@@ -148,7 +148,7 @@ export async function runJest(configName = 'jest.config.js') {
     }
 
     log.verbose(`no config provided, found ${configPath}`);
-    resolvedConfigPath = configPath;
+    process.argv.push('--config', configPath);
 
     if (!testFilesProvided) {
       log.verbose(`no test files provided, setting to current directory`);
