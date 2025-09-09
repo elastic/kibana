c87955e89587c6e60ccb692eb80ec727d62d6335
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2025-09-09 12:52:23 +0200

    fix: disable explore in discover action

--- src/platform/packages/shared/kbn-unified-metrics-grid/src/components/chart/lens_wrapper.tsx
+++ src/platform/packages/shared/kbn-unified-metrics-grid/src/components/chart/lens_wrapper.tsx
@@ -18,7 +18,12 @@ export type LensWrapperProps = {
   lensProps: LensProps;
 } & Pick<ChartSectionProps, 'services' | 'onBrushEnd' | 'onFilter' | 'abortController'>;
 
-const DEFAULT_DISABLED_ACTIONS = ['ACTION_CUSTOMIZE_PANEL', 'ACTION_EXPORT_CSV'];
+const DEFAULT_DISABLED_ACTIONS = [
+  'ACTION_CUSTOMIZE_PANEL',
+  'ACTION_EXPORT_CSV',
+  // temporarily disabiling it until we figure out this action
+  'ACTION_OPEN_IN_DISCOVER',
+];
 
 export function LensWrapper({
   lensProps,
