6e327c02270005b5cdb928836adf819f1b59544e
Melissa <melissa.alvarez@elastic.co>
2025-09-09 15:09:18 -0600

    add deprecated callout for connector flyout

--- x-pack/platform/plugins/shared/actions/server/application/connector/lib/is_connector_deprecated.ts
+++ x-pack/platform/plugins/shared/actions/server/application/connector/lib/is_connector_deprecated.ts
@@ -12,6 +12,7 @@ export type ConnectorWithOptionalDeprecation = Omit<InMemoryConnector, 'isDeprec
   Pick<Partial<InMemoryConnector>, 'isDeprecated'>;
 
 const isObject = (obj: unknown): obj is Record<string, unknown> => isPlainObject(obj);
+const deprecatedActionTypeIds = ['.bedrock', '.gemini', '.gen-ai'];
 
 export const isConnectorDeprecated = (
   connector: RawAction | ConnectorWithOptionalDeprecation
@@ -56,6 +57,8 @@ export const isConnectorDeprecated = (
      */
     return !!connector.config.usesTableApi;
   }
+
+  if (deprecatedActionTypeIds.includes(connector.actionTypeId)) return true;
 
   return false;
 };
