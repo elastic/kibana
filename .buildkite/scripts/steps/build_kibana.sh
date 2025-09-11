a58cdec210a7acfa33ed1b042b06b69bd9cabf53
Alex Szabo <alex.szabo@elastic.co>
2025-09-11 12:06:30 +0200

    remove explicit dependencies, see if that makes it wait for the wait step

--- .buildkite/scripts/steps/build_kibana.sh
+++ .buildkite/scripts/steps/build_kibana.sh
@@ -3,5 +3,6 @@
 set -euo pipefail
 
 .buildkite/scripts/bootstrap.sh
-.buildkite/scripts/build_kibana.sh
-.buildkite/scripts/post_build_kibana.sh
+#.buildkite/scripts/build_kibana.sh
+#.buildkite/scripts/post_build_kibana.sh
+# Skip these too to get a state where build finishes early
