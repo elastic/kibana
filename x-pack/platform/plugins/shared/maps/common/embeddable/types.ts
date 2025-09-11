f9edc14c8e48a6baea1e85127b31625f55dde420
Nathan Reese <reese.nathan@gmail.com>
2025-09-11 12:32:30 -0600

    fix types

---
+++ x-pack/platform/plugins/shared/maps/common/embeddable/types.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { DynamicActionsSerializedState } from '@kbn/embeddable-enhanced-plugin/public';
+import type { SerializedTimeRange, SerializedTitles } from '@kbn/presentation-publishing';
+import type { MapCenterAndZoom, MapExtent, MapSettings } from '../descriptor_types';
+import type { MapAttributes } from '../content_management';
+
+export type MapEmbeddableBaseState = SerializedTimeRange &
+  SerializedTitles &
+  Partial<DynamicActionsSerializedState> & {
+    isLayerTOCOpen?: boolean;
+    openTOCDetails?: string[];
+    mapCenter?: MapCenterAndZoom;
+    mapBuffer?: MapExtent;
+    mapSettings?: Partial<MapSettings>;
+    hiddenLayers?: string[];
+    filterByMapExtent?: boolean;
+    isMovementSynchronized?: boolean;
+  };
+
+export type MapByReferenceState = MapEmbeddableBaseState & {
+  savedObjectId: string;
+};
+
+export type MapByValueState = MapEmbeddableBaseState & {
+  attributes: MapAttributes;
+};
+
+export type MapEmbeddableState = MapByReferenceState | MapByValueState;
