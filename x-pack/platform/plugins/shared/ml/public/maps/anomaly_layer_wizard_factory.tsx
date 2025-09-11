fc37b9250220b6802bb1ece2c216e536def9ac27
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-09-11 12:10:27 +0200

    make maps anomaly source editor lazy loaded

--- x-pack/platform/plugins/shared/ml/public/maps/anomaly_layer_wizard_factory.tsx
+++ x-pack/platform/plugins/shared/ml/public/maps/anomaly_layer_wizard_factory.tsx
@@ -6,63 +6,20 @@
  */
 
 import React from 'react';
-import { htmlIdGenerator, useEuiTheme } from '@elastic/eui';
+
+import { ML_APP_LOCATOR } from '@kbn/ml-common-types/locator_app_locator';
 import type { CoreStart, StartServicesAccessor } from '@kbn/core/public';
 import type { LocatorPublic } from '@kbn/share-plugin/common';
 import type { SerializableRecord } from '@kbn/utility-types';
 import type { LayerWizard, RenderWizardArguments } from '@kbn/maps-plugin/public';
-import { LAYER_TYPE } from '@kbn/maps-plugin/common';
-import type { VectorLayerDescriptor } from '@kbn/maps-plugin/common/descriptor_types';
-import { ML_APP_LOCATOR } from '@kbn/ml-common-types/locator_app_locator';
-import { ML_PAGES } from '@kbn/ml-common-types/locator_ml_pages';
 import type { MlPluginStart } from '@kbn/ml-plugin-contracts';
 
 import type { MlStartDependencies } from '../plugin';
 
-import { getActualStyle } from './util';
-import { CreateAnomalySourceEditor } from './create_anomaly_source_editor';
-import type { AnomalySourceDescriptor } from './anomaly_source';
-import { AnomalySource } from './anomaly_source';
 import { anomalyLayerWizard } from './anomaly_layer_wizard';
+import { AnomalySourceEditorWithThemeLazy } from './anomaly_source_editor_with_theme_lazy';
 
 export const ML_ANOMALY = 'ML_ANOMALIES';
-
-const AnomalySourceEditorWithTheme: React.FC<{
-  coreStart: CoreStart;
-  jobsManagementPath?: string;
-  canCreateJobs: boolean;
-  previewLayers: (layers: VectorLayerDescriptor[]) => void;
-}> = ({ coreStart, jobsManagementPath, canCreateJobs, previewLayers }) => {
-  const { euiTheme } = useEuiTheme();
-
-  const handleSourceConfigChange = (sourceConfig: Partial<AnomalySourceDescriptor> | null) => {
-    if (!sourceConfig) {
-      previewLayers([]);
-      return;
-    }
-
-    const anomalyLayerDescriptor: VectorLayerDescriptor = {
-      id: htmlIdGenerator()(),
-      type: LAYER_TYPE.GEOJSON_VECTOR,
-      sourceDescriptor: AnomalySource.createDescriptor({
-        jobId: sourceConfig.jobId,
-        typicalActual: sourceConfig.typicalActual,
-      }),
-      style: getActualStyle(euiTheme) as VectorLayerDescriptor['style'],
-    };
-
-    previewLayers([anomalyLayerDescriptor]);
-  };
-
-  return (
-    <CreateAnomalySourceEditor
-      onSourceConfigChange={handleSourceConfigChange}
-      coreStart={coreStart}
-      jobsManagementPath={jobsManagementPath}
-      canCreateJobs={canCreateJobs}
-    />
-  );
-};
 
 export class AnomalyLayerWizardFactory {
   public readonly type = ML_ANOMALY;
@@ -88,22 +45,14 @@ export class AnomalyLayerWizardFactory {
 
   public async create(): Promise<LayerWizard> {
     const { coreStart, mlLocator } = await this.getServices();
-    let jobsManagementPath: string | undefined;
-    if (mlLocator) {
-      jobsManagementPath = await mlLocator.getUrl({
-        page: ML_PAGES.ANOMALY_DETECTION_JOBS_MANAGE,
-      });
-    } else {
-      // eslint-disable-next-line no-console
-      console.error('Unable to get job management path.');
-    }
+
     anomalyLayerWizard.getIsDisabled = () => !this.canGetJobs;
 
     anomalyLayerWizard.renderWizard = ({ previewLayers }: RenderWizardArguments) => {
       return (
-        <AnomalySourceEditorWithTheme
+        <AnomalySourceEditorWithThemeLazy
           coreStart={coreStart}
-          jobsManagementPath={jobsManagementPath}
+          mlLocator={mlLocator}
           canCreateJobs={this.canCreateJobs}
           previewLayers={previewLayers}
         />
---
+++ x-pack/platform/plugins/shared/ml/public/maps/anomaly_source_editor_with_theme.tsx
@@ -0,0 +1,74 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { useState, useEffect } from 'react';
+
+import { htmlIdGenerator, useEuiTheme } from '@elastic/eui';
+
+import type { CoreStart } from '@kbn/core/public';
+import { LAYER_TYPE } from '@kbn/maps-plugin/common';
+import type { VectorLayerDescriptor } from '@kbn/maps-plugin/common/descriptor_types';
+import type { LocatorPublic } from '@kbn/share-plugin/common';
+import type { SerializableRecord } from '@kbn/utility-types';
+import { ML_PAGES } from '@kbn/ml-common-types/locator_ml_pages';
+
+import { getActualStyle } from './util';
+import { CreateAnomalySourceEditor } from './create_anomaly_source_editor';
+import type { AnomalySourceDescriptor } from './anomaly_source';
+import { AnomalySource } from './anomaly_source';
+
+export const AnomalySourceEditorWithTheme: React.FC<{
+  coreStart: CoreStart;
+  mlLocator?: LocatorPublic<SerializableRecord>;
+  canCreateJobs: boolean;
+  previewLayers: (layers: VectorLayerDescriptor[]) => void;
+}> = ({ coreStart, mlLocator, canCreateJobs, previewLayers }) => {
+  const { euiTheme } = useEuiTheme();
+
+  const [jobsManagementPath, setJobsManagementPath] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    if (mlLocator) {
+      mlLocator
+        .getUrl({
+          page: ML_PAGES.ANOMALY_DETECTION_JOBS_MANAGE,
+        })
+        .then(setJobsManagementPath);
+    } else {
+      // eslint-disable-next-line no-console
+      console.error('Unable to get job management path.');
+    }
+  }, [mlLocator]);
+
+  const handleSourceConfigChange = (sourceConfig: Partial<AnomalySourceDescriptor> | null) => {
+    if (!sourceConfig) {
+      previewLayers([]);
+      return;
+    }
+
+    const anomalyLayerDescriptor: VectorLayerDescriptor = {
+      id: htmlIdGenerator()(),
+      type: LAYER_TYPE.GEOJSON_VECTOR,
+      sourceDescriptor: AnomalySource.createDescriptor({
+        jobId: sourceConfig.jobId,
+        typicalActual: sourceConfig.typicalActual,
+      }),
+      style: getActualStyle(euiTheme) as VectorLayerDescriptor['style'],
+    };
+
+    previewLayers([anomalyLayerDescriptor]);
+  };
+
+  return (
+    <CreateAnomalySourceEditor
+      onSourceConfigChange={handleSourceConfigChange}
+      coreStart={coreStart}
+      jobsManagementPath={jobsManagementPath}
+      canCreateJobs={canCreateJobs}
+    />
+  );
+};
