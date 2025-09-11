9bdf1c56aca9eb181818a5edcd070adc5ee719b0
Alex Szabo <alex.szabo@elastic.co>
2025-09-11 16:43:47 +0200

    re-enable cache, configure buildkite parallelism

--- .buildkite/scripts/steps/check_types.sh
+++ .buildkite/scripts/steps/check_types.sh
@@ -7,4 +7,13 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/bootstrap.sh
 
 echo --- Check Types
-yarn moon ci :typecheck -c 3
+if [[ "${RUN_FULL_CHECK:-}" == "true" ]]; then
+  echo "Running full type check..."
+  node scripts/type_check
+else
+  echo "Running type-check with caches..."
+  yarn moon ci :typecheck -c 3 \
+    --job $BUILDKITE_PARALLEL_JOB \
+    --jobTotal $BUILDKITE_PARALLEL_JOB_COUNT \
+    --base $BUILDKITE_PULL_REQUEST_BASE_BRANCH
+fi
