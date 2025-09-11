47409c69c0adc126fd8e7579678461f2384a3c03
Rômulo Farias <romulo.farias@elastic.co>
2025-09-11 14:51:55 +0200

    Change case to snake and store on custom ecs case

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/entity_store/painless/build_update_script.test.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/entity_store/painless/build_update_script.test.ts
@@ -45,8 +45,8 @@ describe('buildUpdateEntityPainlessScript', () => {
         entity: {
           id: '1',
           attributes: {
-            StorageClass: 'cold',
-            Managed: true,
+            storage_class: 'cold',
+            managed: true,
           },
         },
       })
@@ -55,8 +55,8 @@ describe('buildUpdateEntityPainlessScript', () => {
     expect(script).toBe(
       `ctx._source['entity'] = ctx._source['entity'] == null ? [:] : ctx._source['entity'];` +
         `ctx._source['entity']['attributes'] = ctx._source['entity']['attributes'] == null ? [:] : ctx._source['entity']['attributes'];` +
-        `ctx._source['entity']['attributes']['StorageClass'] = 'cold';` +
-        `ctx._source['entity']['attributes']['Managed'] = true;`
+        `ctx._source['entity']['attributes']['storage_class'] = 'cold';` +
+        `ctx._source['entity']['attributes']['managed'] = true;`
     );
   });
 
@@ -71,15 +71,15 @@ describe('buildUpdateEntityPainlessScript', () => {
           type: 'test-type',
           sub_type: 'test-sub_type',
           attributes: {
-            StorageClass: 'cold',
-            Managed: true,
+            storage_class: 'cold',
+            managed: true,
           },
           lifecycle: {
-            FirstSeen: '2024-08-30T11:03:33.594Z',
+            first_seen: '2024-08-30T11:03:33.594Z',
           },
           behavior: {
-            BruteForceVictim: false,
-            UsedUsbDevice: true,
+            brute_force_victim: false,
+            used_usb_device: true,
           },
         },
         host: {
@@ -95,15 +95,34 @@ describe('buildUpdateEntityPainlessScript', () => {
         `ctx._source['entity']['type'] = 'test-type';` +
         `ctx._source['entity']['sub_type'] = 'test-sub_type';` +
         `ctx._source['entity']['attributes'] = ctx._source['entity']['attributes'] == null ? [:] : ctx._source['entity']['attributes'];` +
-        `ctx._source['entity']['attributes']['StorageClass'] = 'cold';` +
-        `ctx._source['entity']['attributes']['Managed'] = true;` +
+        `ctx._source['entity']['attributes']['storage_class'] = 'cold';` +
+        `ctx._source['entity']['attributes']['managed'] = true;` +
         `ctx._source['entity']['lifecycle'] = ctx._source['entity']['lifecycle'] == null ? [:] : ctx._source['entity']['lifecycle'];` +
-        `ctx._source['entity']['lifecycle']['FirstSeen'] = '2024-08-30T11:03:33.594Z';` +
+        `ctx._source['entity']['lifecycle']['first_seen'] = '2024-08-30T11:03:33.594Z';` +
         `ctx._source['entity']['behavior'] = ctx._source['entity']['behavior'] == null ? [:] : ctx._source['entity']['behavior'];` +
-        `ctx._source['entity']['behavior']['BruteForceVictim'] = false;` +
-        `ctx._source['entity']['behavior']['UsedUsbDevice'] = true;` +
+        `ctx._source['entity']['behavior']['brute_force_victim'] = false;` +
+        `ctx._source['entity']['behavior']['used_usb_device'] = true;` +
         `ctx._source['host'] = ctx._source['host'] == null ? [:] : ctx._source['host'];` +
         `ctx._source['host']['macAddress'] = 'cf:45:2e:a3:20:96';`
+    );
+  });
+
+  it('small example', () => {
+    const script = buildUpdateEntityPainlessScript(
+      getFlattenedObject({
+        entity: {
+          id: 'entity-test',
+          attributes: {
+            Privileged: true,
+          },
+        },
+      })
+    );
+
+    expect(script).toBe(
+      `ctx._source['entity'] = ctx._source['entity'] == null ? [:] : ctx._source['entity'];` +
+        `ctx._source['entity']['attributes'] = ctx._source['entity']['attributes'] == null ? [:] : ctx._source['entity']['attributes'];` +
+        `ctx._source['entity']['attributes']['Privileged'] = true;`
     );
   });
 });
