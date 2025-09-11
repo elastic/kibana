e142232055af9fa8bec4ecb06df81261582de19f
Rômulo Farias <romulo.farias@elastic.co>
2025-09-11 10:36:58 +0200

    Remove complex logic of flattened props

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/entity_store/painless/build_update_script.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/entity_store/painless/build_update_script.ts
@@ -5,27 +5,26 @@
  * 2.0.
  */
 
-import type { FlattenProps } from '../utils/flatten_props';
+const immutableFields = [`entity.id`];
 
-const immutableFields = [`['entity']['id']`];
-
-export const buildUpdateEntityPainlessScript = (props: FlattenProps[]) => {
+export const buildUpdateEntityPainlessScript = (props: Record<string, unknown>) => {
   let script = ``;
 
   const initializedProperties: Record<string, boolean> = {};
 
-  for (let i = 0; i < props.length; i++) {
-    const { path, value } = props[i];
-
-    const fullPathAccess = convertToPainlessObjectAccess(path);
-    if (immutableFields.indexOf(fullPathAccess) >= 0) {
+  for (const path of Object.keys(props)) {
+    if (immutableFields.indexOf(path) >= 0) {
       // eslint-disable-next-line no-continue
       continue;
     }
 
+    const value = props[path];
+    const splitPath = path.split('.');
+    const fullPathAccess = convertToPainlessObjectAccess(splitPath);
+
     // init objects in painless
-    for (let sliceSize = 1; sliceSize < path.length; sliceSize++) {
-      const subPath = convertToPainlessObjectAccess(path.slice(0, sliceSize));
+    for (let sliceSize = 1; sliceSize < splitPath.length; sliceSize++) {
+      const subPath = convertToPainlessObjectAccess(splitPath.slice(0, sliceSize));
       if (!initializedProperties[subPath as keyof typeof initializedProperties]) {
         const subPathAccess = `ctx._source${subPath}`;
         script += `${subPathAccess} = ${subPathAccess} == null ? [:] : ${subPathAccess};`;
@@ -58,3 +57,20 @@ function convertToPainlessValue(value: unknown): string {
       throw new Error(`Can't convert ${typeof value} to a painless expression`);
   }
 }
+
+/*
+ctx._source['host'] = ctx._source['host'] == null ? [:] : ctx._source['host'];
+ctx._source['host']['name'] = ctx._source['host']['name'] == null ? [:] : ctx._source['host']['name'];
+ctx._source['host']['name'] = 'not-allowed';
+ctx._source['host']['id'] = ctx._source['host']['id'] == null ? [:] : ctx._source['host']['id'];
+ctx._source['host']['id'] = ['123'];
+ctx._source['entity'] = ctx._source['entity'] == null ? [:] : ctx._source['entity'];
+ctx._source['entity']['attributes'] = ctx._source['entity']['attributes'] == null ? [:] : ctx._source['entity']['attributes'];
+ctx._source['entity']['attributes']['Privileged'] = ctx._source['entity']['attributes']['Privileged'] == null ? [:] : ctx._source['entity']['attributes']['Privileged'];
+ctx._source['entity']['attributes']['Privileged'] = true;
+ctx._source['entity']['lifecycle'] = ctx._source['entity']['lifecycle'] == null ? [:] : ctx._source['entity']['lifecycle'];
+ctx._source['entity']['lifecycle']['FirstSeen'] = ctx._source['entity']['lifecycle']['FirstSeen'] == null ? [:] : ctx._source['entity']['lifecycle']['FirstSeen'];
+ctx._source['entity']['lifecycle']['FirstSeen'] = '1995-12-17T03:24:00';
+
+
+*/
