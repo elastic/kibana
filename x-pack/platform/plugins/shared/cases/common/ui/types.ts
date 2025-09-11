efdd3f3cc848044c6ab47fb4b37231aa74cffd09
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-09-11 10:33:01 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/platform/plugins/shared/cases/common/ui/types.ts
+++ x-pack/platform/plugins/shared/cases/common/ui/types.ts
@@ -7,7 +7,8 @@
 
 import type { AlertsTableProps } from '@kbn/response-ops-alerts-table/types';
 import type { ResolvedSimpleSavedObject } from '@kbn/core/public';
-import type { DataView, FieldSpec } from '@kbn/data-views-plugin/common';
+import type { DataView } from '@kbn/data-views-plugin/common';
+import type { DataTableRecord } from '@kbn/discover-utils';
 import type {
   CREATE_CASES_CAPABILITY,
   DELETE_CASES_CAPABILITY,
@@ -53,7 +54,6 @@ import type {
   CaseSummaryResponse,
   InferenceConnectorsResponse,
 } from '../types/api';
-import { DataTableRecord } from '@kbn/discover-utils';
 
 type DeepRequired<T> = { [K in keyof T]: DeepRequired<T[K]> } & Required<T>;
 
