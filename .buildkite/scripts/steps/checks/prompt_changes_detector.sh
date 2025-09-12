bdb7de82dc8ef94bd8671a864dee9928e71dbba1
Steph Milovic <stephanie.milovic@elastic.co>
2025-09-12 11:48:56 -0600

    rm condition

--- .buildkite/scripts/steps/checks/prompt_changes_detector.sh
+++ .buildkite/scripts/steps/checks/prompt_changes_detector.sh
@@ -4,12 +4,5 @@ set -euo pipefail
 
 source "$(dirname "${BASH_SOURCE[0]}")/../../common/util.sh"
 
-echo "--- Check for prompt file changes"
-
-if ! is_pr; then
-  echo "Not a PR, skipping prompt changes detection"
-  exit 0
-fi
-
-echo "Running prompt changes detector..."
+echo "Running prompt changes comment post..."
 ts-node .buildkite/scripts/steps/checks/prompt_changes_detector.ts
