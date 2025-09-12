8d31a46e5111cf6072d8a187dda9db9c611873f7
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-09-12 14:49:18 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/solutions/security/plugins/elastic_assistant/server/__mocks__/space_test_helpers.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/server/__mocks__/space_test_helpers.ts
@@ -18,7 +18,7 @@ interface MockElasticsearchClient {
   };
 }
 
-type SpaceAwareContext = 
+type SpaceAwareContext =
   | (Partial<ElasticAssistantApiRequestHandlerContext> & {
       elasticAssistant?: {
         getSpaceId?: jest.MockedFunction<() => string>;
