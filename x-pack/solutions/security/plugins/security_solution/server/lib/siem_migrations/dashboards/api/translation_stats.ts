90f8b3217369ac6b7dbb38452de7a2d8bf745ac5
Kevin Qualters <56408403+kqualters-elastic@users.noreply.github.com>
2025-09-11 21:15:44 -0400

    [Automatic Migration] Dashboard stats all and translation stats (#233737)
    ## Summary
    
    This pr adds the apis for dashboards stats all, at the url
    /internal/siem_migrations/dashboards/stats, which is the same as the
    base implementation of getAllStats, as well as the translationStats api
    located at the url
    /internal/siem_migrations/dashboards/{migration_id}/translation_stats.
    
    ### Checklist
    
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.

--- x-pack/solutions/security/plugins/security_solution/server/lib/siem_migrations/dashboards/api/translation_stats.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/siem_migrations/dashboards/api/translation_stats.ts
@@ -7,21 +7,21 @@
 
 import type { IKibanaResponse, Logger } from '@kbn/core/server';
 import { buildRouteValidationWithZod } from '@kbn/zod-helpers';
-import type { GetDashboardMigrationStatsResponse } from '../../../../../common/siem_migrations/model/api/dashboards/dashboard_migration.gen';
-import { GetDashboardMigrationStatsRequestParams } from '../../../../../common/siem_migrations/model/api/dashboards/dashboard_migration.gen';
-import { SIEM_DASHBOARD_MIGRATION_STATS_PATH } from '../../../../../common/siem_migrations/dashboards/constants';
+import type { GetAllTranslationStatsDashboardMigrationResponse } from '../../../../../common/siem_migrations/model/api/dashboards/dashboard_migration.gen';
+import { GetAllTranslationStatsDashboardMigrationRequestParams } from '../../../../../common/siem_migrations/model/api/dashboards/dashboard_migration.gen';
+import { SIEM_DASHBOARD_MIGRATION_TRANSLATION_STATS_PATH } from '../../../../../common/siem_migrations/dashboards/constants';
 import type { SecuritySolutionPluginRouter } from '../../../../types';
-import { withLicense } from '../../common/api/util/with_license';
 import { authz } from '../../common/api/util/authz';
+import { withLicense } from '../../common/api/util/with_license';
 import { withExistingMigration } from '../../common/api/util/with_existing_migration_id';
 
-export const registerSiemDashboardMigrationsStatsRoute = (
+export const registerSiemDashboardMigrationsTranslationStatsRoute = (
   router: SecuritySolutionPluginRouter,
   logger: Logger
 ) => {
   router.versioned
     .get({
-      path: SIEM_DASHBOARD_MIGRATION_STATS_PATH,
+      path: SIEM_DASHBOARD_MIGRATION_TRANSLATION_STATS_PATH,
       access: 'internal',
       security: { authz },
     })
@@ -29,7 +29,11 @@ export const registerSiemDashboardMigrationsStatsRoute = (
       {
         version: '1',
         validate: {
-          request: { params: buildRouteValidationWithZod(GetDashboardMigrationStatsRequestParams) },
+          request: {
+            params: buildRouteValidationWithZod(
+              GetAllTranslationStatsDashboardMigrationRequestParams
+            ),
+          },
         },
       },
       withLicense(
@@ -38,16 +42,18 @@ export const registerSiemDashboardMigrationsStatsRoute = (
             context,
             req,
             res
-          ): Promise<IKibanaResponse<GetDashboardMigrationStatsResponse>> => {
+          ): Promise<IKibanaResponse<GetAllTranslationStatsDashboardMigrationResponse>> => {
             const migrationId = req.params.migration_id;
             try {
               const ctx = await context.resolve(['securitySolution']);
-              const dashboardMigrationClient =
+              const dashboardMigrationsClient =
                 ctx.securitySolution.siemMigrations.getDashboardsClient();
 
-              const stats = await dashboardMigrationClient.task.getStats(migrationId);
+              const stats = await dashboardMigrationsClient.data.items.getTranslationStats(
+                migrationId
+              );
 
-              if (stats.items?.total === 0) {
+              if (stats.dashboards.total === 0) {
                 return res.noContent();
               }
               return res.ok({ body: stats });
