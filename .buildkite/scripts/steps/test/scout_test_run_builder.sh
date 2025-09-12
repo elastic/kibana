eed983d679b9f03e9a28032c396efd2526ec3d5d
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-09-12 13:04:20 +0200

    move steps generation to the end

--- .buildkite/scripts/steps/test/scout_test_run_builder.sh
+++ .buildkite/scripts/steps/test/scout_test_run_builder.sh
@@ -12,11 +12,11 @@ node scripts/scout discover-playwright-configs --save
 cp .scout/test_configs/scout_playwright_configs.json scout_playwright_configs.json
 buildkite-agent artifact upload "scout_playwright_configs.json"
 
-echo '--- Scout Test Run Builder'
-ts-node "$(dirname "${0}")/scout_test_run_builder.ts"
-
-echo '--- Scout Integration Tests'
+echo '--- Running Scout Integration Tests'
 node scripts/scout.js run-tests \
 --serverless=security \
 --config src/platform/packages/shared/kbn-scout/integration_tests/playwright.config.ts \
 --kibana-install-dir "$KIBANA_BUILD_LOCATION"
+
+echo '--- Producing Scout Test Execution Steps'
+ts-node "$(dirname "${0}")/scout_test_run_builder.ts"
