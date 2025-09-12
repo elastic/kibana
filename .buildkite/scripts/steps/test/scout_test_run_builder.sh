f4a1c4da2b0faaf1cdd338a36e01c4a1d35abaf2
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-09-12 09:24:28 +0200

    run int tests agaist kibana build

--- .buildkite/scripts/steps/test/scout_test_run_builder.sh
+++ .buildkite/scripts/steps/test/scout_test_run_builder.sh
@@ -2,10 +2,7 @@
 
 set -euo pipefail
 
-source .buildkite/scripts/common/util.sh
-.buildkite/scripts/bootstrap.sh
-.buildkite/scripts/download_build_artifacts.sh
-.buildkite/scripts/setup_es_snapshot_cache.sh
+source .buildkite/scripts/steps/functional/common.sh
 
 echo '--- Verify Playwright CLI is functional'
 node scripts/scout run-playwright-test-check
@@ -21,4 +18,5 @@ ts-node "$(dirname "${0}")/scout_test_run_builder.ts"
 echo '--- Scout Integration Tests'
 node scripts/scout.js run-tests \
 --serverless=security \
---config src/platform/packages/shared/kbn-scout/src/integration_tests/playwright.config.ts
+--config src/platform/packages/shared/kbn-scout/src/integration_tests/playwright.config.ts \
+--kibana-install-dir "$KIBANA_BUILD_LOCATION"
