fa06a26478b6f749ecb3c0d05b6574a3c02f0d4b
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-09-12 19:33:24 +0200

    [8.19] [Security Solution][AI Assistant] Handle conflicts during the bulk update (#233288) (#234869)
    # Backport
    
    This will backport the following commits from `main` to `8.19`:
    - [[Security Solution][AI Assistant] Handle conflicts during the bulk
    update (#233288)](https://github.com/elastic/kibana/pull/233288)
    
    <!--- Backport version: 10.0.2 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Ievgen
    Sorokopud","email":"ievgen.sorokopud@elastic.co"},"sourceCommit":{"committedDate":"2025-08-29T15:04:39Z","message":"[Security
    Solution][AI Assistant] Handle conflicts during the bulk update
    (#233288)\n\n## Summary\n\nPart of\nEpic:
    https://github.com/elastic/security-team/issues/12768\nMeta:
    https://github.com/elastic/security-team/issues/13657\n\nIn my [previous
    PR](https://github.com/elastic/kibana/pull/232288), I\naccidentally
    removed \"retry on
    conflict\"\n[logic](https://github.com/elastic/kibana/pull/232288/files#r2305375978).\n\nThese
    changes add a retry logic on bulk update conflicts. For that we\nuse a
    `retry_on_conflict`
    option\nhttps://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-bulk#operation-bulk-body-application-json-update-retry_on_conflict.\nThe
    retry count is set to 3 to match previous behaviour.\n\ncc
    @stephmilovic","sha":"8170cff4f95f5dad24f3d40f0b2a84ab083af93e","branchLabelMapping":{"^v9.2.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","backport:skip","Team:
    SecuritySolution","Team:Security Generative
    AI","v9.2.0"],"title":"[Security Solution][AI Assistant] Handle
    conflicts during the bulk
    update","number":233288,"url":"https://github.com/elastic/kibana/pull/233288","mergeCommit":{"message":"[Security
    Solution][AI Assistant] Handle conflicts during the bulk update
    (#233288)\n\n## Summary\n\nPart of\nEpic:
    https://github.com/elastic/security-team/issues/12768\nMeta:
    https://github.com/elastic/security-team/issues/13657\n\nIn my [previous
    PR](https://github.com/elastic/kibana/pull/232288), I\naccidentally
    removed \"retry on
    conflict\"\n[logic](https://github.com/elastic/kibana/pull/232288/files#r2305375978).\n\nThese
    changes add a retry logic on bulk update conflicts. For that we\nuse a
    `retry_on_conflict`
    option\nhttps://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-bulk#operation-bulk-body-application-json-update-retry_on_conflict.\nThe
    retry count is set to 3 to match previous behaviour.\n\ncc
    @stephmilovic","sha":"8170cff4f95f5dad24f3d40f0b2a84ab083af93e"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v9.2.0","branchLabelMappingKey":"^v9.2.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/233288","number":233288,"mergeCommit":{"message":"[Security
    Solution][AI Assistant] Handle conflicts during the bulk update
    (#233288)\n\n## Summary\n\nPart of\nEpic:
    https://github.com/elastic/security-team/issues/12768\nMeta:
    https://github.com/elastic/security-team/issues/13657\n\nIn my [previous
    PR](https://github.com/elastic/kibana/pull/232288), I\naccidentally
    removed \"retry on
    conflict\"\n[logic](https://github.com/elastic/kibana/pull/232288/files#r2305375978).\n\nThese
    changes add a retry logic on bulk update conflicts. For that we\nuse a
    `retry_on_conflict`
    option\nhttps://www.elastic.co/docs/api/doc/elasticsearch/operation/operation-bulk#operation-bulk-body-application-json-update-retry_on_conflict.\nThe
    retry count is set to 3 to match previous behaviour.\n\ncc
    @stephmilovic","sha":"8170cff4f95f5dad24f3d40f0b2a84ab083af93e"}}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/solutions/security/plugins/elastic_assistant/server/lib/data_stream/documents_data_writer.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/server/lib/data_stream/documents_data_writer.ts
@@ -202,13 +202,14 @@ export class DocumentsDataWriter implements DocumentsDataWriter {
           _id: document.id,
           _index: responseToUpdate?.hits.hits.find((c) => c._id === document.id)?._index,
           _source: true,
+          retry_on_conflict: 3,
         },
       },
       getUpdateScript(document, updatedAt),
     ]);
   };
 
-  private getDeletedocumentsQuery = async (
+  private getDeleteDocumentsQuery = async (
     documentsToDelete: string[],
     authenticatedUser?: AuthenticatedUser
   ) => {
@@ -263,7 +264,7 @@ export class DocumentsDataWriter implements DocumentsDataWriter {
 
     const documentDeletedBody =
       params.documentsToDelete && params.documentsToDelete.length > 0
-        ? await this.getDeletedocumentsQuery(params.documentsToDelete, params.authenticatedUser)
+        ? await this.getDeleteDocumentsQuery(params.documentsToDelete, params.authenticatedUser)
         : [];
 
     const documentUpdatedBody =
