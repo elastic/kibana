4cffc6d7682eb8f35e0fa4c9dab735833265a861
Stratoula <efstratia.kalafateli@elastic.co>
2025-09-12 09:04:23 +0200

    No need to reset on new search

--- src/platform/plugins/shared/discover/public/application/main/components/top_nav/use_top_nav_links.tsx
+++ src/platform/plugins/shared/discover/public/application/main/components/top_nav/use_top_nav_links.tsx
@@ -40,7 +40,6 @@ import { useProfileAccessor } from '../../../../context_awareness';
 import {
   internalStateActions,
   useCurrentDataView,
-  useCurrentTabAction,
   useInternalStateDispatch,
 } from '../../state_management/redux';
 import type { DiscoverAppLocatorParams } from '../../../../../common';
@@ -72,7 +71,6 @@ export const useTopNavLinks = ({
   hasShareIntegration: boolean;
 }): TopNavMenuData[] => {
   const dispatch = useInternalStateDispatch();
-  const setControlGroupState = useCurrentTabAction(internalStateActions.setControlGroupState);
   const currentDataView = useCurrentDataView();
   const { authorizedRuleTypes }: { authorizedRuleTypes: RuleTypeWithDescription[] } =
     useGetRuleTypesPermissions({
@@ -144,12 +142,6 @@ export const useTopNavLinks = ({
                 ? createDataViewDataSource({ dataViewId: currentDataView.id })
                 : undefined,
             };
-            // Reset control group state to undefined when starting a new session
-            dispatch(
-              setControlGroupState({
-                controlGroupState: undefined,
-              })
-            );
             services.application.navigateToApp(DISCOVER_APP_ID, { state: { defaultState } });
           },
         });
@@ -176,8 +168,6 @@ export const useTopNavLinks = ({
       return items;
     }, [
       defaultMenu,
-      dispatch,
-      setControlGroupState,
       services,
       onOpenInspector,
       discoverParams,
