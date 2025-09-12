827211dd6512f7f78f51a291dfffbb43cae73205
Ola Pawlus <ola.pawlus@elastic.co>
2025-09-12 16:24:59 +0200

    cd fixing test - click on the breadcrumbs to dismiss the tooltip (prevents flakiness)

--- src/platform/test/functional/page_objects/time_picker.ts
+++ src/platform/test/functional/page_objects/time_picker.ts
@@ -114,7 +114,7 @@ export class TimePickerPageObject extends FtrService {
 
   private async showStartEndTimes() {
     // click on the breadcrumbs to dismiss the tooltip (prevents flakiness)
-    await this.testSubjects.pressEnter('breadcrumbs');
+    await this.testSubjects.click('breadcrumbs');
     // This first await makes sure the superDatePicker has loaded before we check for the ShowDatesButton
     await this.testSubjects.exists('superDatePickerToggleQuickMenuButton', { timeout: 20000 });
     await this.retry.tryForTime(5000, async () => {
