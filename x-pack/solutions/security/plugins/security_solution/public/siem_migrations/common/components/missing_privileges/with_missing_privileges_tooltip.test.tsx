0e5eaa61d6c0fca1262b13b63ba3d4a925b11f69
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-09-12 14:26:03 +0200

    Fix types

--- x-pack/solutions/security/plugins/security_solution/public/siem_migrations/common/components/missing_privileges/with_missing_privileges_tooltip.test.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/siem_migrations/common/components/missing_privileges/with_missing_privileges_tooltip.test.tsx
@@ -8,8 +8,13 @@
 import React from 'react';
 import { EuiButton } from '@elastic/eui';
 import { WithMissingPrivilegesTooltip } from './with_missing_privileges_tooltip';
+import { useKibana } from '../../../../common/lib/kibana';
 import { render, fireEvent, waitFor } from '@testing-library/react';
 import type { SiemMigrationsService } from '../../../service';
+import type { MigrationType } from '../../../../../common/siem_migrations/types';
+
+jest.mock('../../../../common/lib/kibana');
+const useKibanaMock = useKibana as jest.MockedFunction<typeof useKibana>;
 
 const mockedMissingCapabilities = [
   {
@@ -22,20 +27,21 @@ const mockSiemMigrationsService = {
   rules: {
     getMissingCapabilities: jest.fn(),
   },
+  dashboards: {
+    getMissingCapabilities: jest.fn(),
+  },
 } as unknown as jest.MockedObjectDeep<SiemMigrationsService>;
 
 const TestComponent = ({ isAuthorized }: { isAuthorized: boolean }) => {
   return <EuiButton isDisabled={!isAuthorized} data-test-subj="test-component" />;
 };
 
-const TestcomponentWithTooltip = WithMissingPrivilegesTooltip(
-  TestComponent,
-  mockSiemMigrationsService.rules,
-  'all'
-);
+const getTestComponentWithTooltip = (migrationType: MigrationType) =>
+  WithMissingPrivilegesTooltip(TestComponent, migrationType, 'all');
 
-const renderTestComponent = () => {
-  return render(<TestcomponentWithTooltip />);
+const renderTestComponent = (migrationType: MigrationType) => {
+  const TestComponentWithTooltip = getTestComponentWithTooltip(migrationType);
+  return render(<TestComponentWithTooltip />);
 };
 
 describe('WithMissingPrivileges Tooltip', () => {
@@ -43,26 +49,62 @@ describe('WithMissingPrivileges Tooltip', () => {
     mockSiemMigrationsService.rules.getMissingCapabilities.mockReturnValue(
       mockedMissingCapabilities
     );
-  });
-  it('renders the component without tooltip when no missing capabilities', () => {
-    mockSiemMigrationsService.rules.getMissingCapabilities.mockReturnValue([]);
-    const { getByTestId, queryByTestId } = renderTestComponent();
-    expect(getByTestId('test-component')).toBeInTheDocument();
-    expect(getByTestId('test-component')).not.toBeDisabled();
-    expect(queryByTestId('missingPrivilegesTooltipAnchor')).not.toBeInTheDocument();
+    mockSiemMigrationsService.dashboards.getMissingCapabilities.mockReturnValue(
+      mockedMissingCapabilities
+    );
+
+    useKibanaMock.mockReturnValue({
+      services: {
+        siemMigrations: mockSiemMigrationsService,
+      },
+    } as unknown as ReturnType<typeof useKibana>);
   });
 
-  it('renders the component with tooltip when there are missing capabilities', async () => {
-    const { getByTestId, queryByTestId } = renderTestComponent();
-    expect(getByTestId('test-component')).toBeInTheDocument();
-    expect(getByTestId('test-component')).toBeDisabled();
-    expect(queryByTestId('missingPrivilegesTooltipAnchor')).toBeInTheDocument();
-    fireEvent.mouseOver(getByTestId('test-component'));
-    await waitFor(() => {
-      expect(getByTestId('missingPrivilegesTooltip')).toBeVisible();
-      expect(getByTestId('missingPrivilegesTooltip')).toHaveTextContent(
-        'Test capability description'
-      );
+  describe('Rules', () => {
+    it('renders the component without tooltip when no missing capabilities', () => {
+      mockSiemMigrationsService.rules.getMissingCapabilities.mockReturnValue([]);
+      const { getByTestId, queryByTestId } = renderTestComponent('rule');
+      expect(getByTestId('test-component')).toBeInTheDocument();
+      expect(getByTestId('test-component')).not.toBeDisabled();
+      expect(queryByTestId('missingPrivilegesTooltipAnchor')).not.toBeInTheDocument();
+    });
+
+    it('renders the component with tooltip when there are missing capabilities', async () => {
+      const { getByTestId, queryByTestId } = renderTestComponent('rule');
+      expect(getByTestId('test-component')).toBeInTheDocument();
+      expect(getByTestId('test-component')).toBeDisabled();
+      expect(queryByTestId('missingPrivilegesTooltipAnchor')).toBeInTheDocument();
+      fireEvent.mouseOver(getByTestId('test-component'));
+      await waitFor(() => {
+        expect(getByTestId('missingPrivilegesTooltip')).toBeVisible();
+        expect(getByTestId('missingPrivilegesTooltip')).toHaveTextContent(
+          'Test capability description'
+        );
+      });
+    });
+  });
+
+  describe('Dashboards', () => {
+    it('renders the component without tooltip when no missing capabilities', () => {
+      mockSiemMigrationsService.dashboards.getMissingCapabilities.mockReturnValue([]);
+      const { getByTestId, queryByTestId } = renderTestComponent('dashboard');
+      expect(getByTestId('test-component')).toBeInTheDocument();
+      expect(getByTestId('test-component')).not.toBeDisabled();
+      expect(queryByTestId('missingPrivilegesTooltipAnchor')).not.toBeInTheDocument();
+    });
+
+    it('renders the component with tooltip when there are missing capabilities', async () => {
+      const { getByTestId, queryByTestId } = renderTestComponent('dashboard');
+      expect(getByTestId('test-component')).toBeInTheDocument();
+      expect(getByTestId('test-component')).toBeDisabled();
+      expect(queryByTestId('missingPrivilegesTooltipAnchor')).toBeInTheDocument();
+      fireEvent.mouseOver(getByTestId('test-component'));
+      await waitFor(() => {
+        expect(getByTestId('missingPrivilegesTooltip')).toBeVisible();
+        expect(getByTestId('missingPrivilegesTooltip')).toHaveTextContent(
+          'Test capability description'
+        );
+      });
     });
   });
 });
