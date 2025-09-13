2dd4cf7c81dc9c36f0eef2aa0f105b8c44784750
Brad White <brad.white@elastic.co>
2025-09-12 22:48:01 -0600

    logging change

--- .buildkite/scripts/steps/storybooks/build_and_upload.ts
+++ .buildkite/scripts/steps/storybooks/build_and_upload.ts
@@ -48,16 +48,16 @@ const buildStorybook = (storybook: string): Promise<{ logs: string }> => {
 
     child.on('close', (code) => {
       if (code === 0) {
-        logsBuffer.unshift(`--- ✅ ${storybook} Storybook\n`);
+        logsBuffer.unshift(`--- ✅ ${storybook} storybook\n`);
         resolve({ logs: logsBuffer.join('') });
       } else {
-        logsBuffer.unshift(`--- ❌ ${storybook} Storybook\n`);
+        logsBuffer.unshift(`--- ❌ ${storybook} storybook\n`);
         reject(new Error(logsBuffer.join('')));
       }
     });
 
     child.on('error', () => {
-      logsBuffer.unshift(`--- ❌ ${storybook} Storybook\n`);
+      logsBuffer.unshift(`--- ❌ ${storybook} storybook\n`);
       reject(new Error(logsBuffer.join('')));
     });
   });
