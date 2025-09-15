ac5289cafb597b64740df2229081302f847c8dbb
Marshall Main <marshall.main@elastic.co>
2025-09-15 14:41:35 -0400

    Exclude missing events from EQL sequence alert generation logic

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/eql/build_alert_group_from_sequence.test.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_types/eql/build_alert_group_from_sequence.test.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import type { EqlHitsSequence } from '@elastic/elasticsearch/lib/api/types';
 import { ALERT_REASON, ALERT_RULE_CONSUMER, ALERT_URL } from '@kbn/rule-data-utils';
 
 import { sampleDocNoSortId, sampleRuleGuid } from '../__mocks__/es_results';
@@ -23,6 +24,7 @@ import {
 } from '../../../../../common/field_maps/field_names';
 import { buildReasonMessageForEqlAlert } from '../utils/reason_formatters';
 import { getSharedParamsMock } from '../__mocks__/shared_params';
+import type { SignalSource } from '../types';
 
 const sharedParams = getSharedParamsMock({
   ruleParams: getQueryRuleParams(),
@@ -39,6 +41,116 @@ describe('buildAlert', () => {
       join_keys: [],
       events: [
         sampleDocNoSortId('d5e8eb51-a6a0-456d-8a15-4b79bfec3d71'),
+        sampleDocNoSortId('619389b2-b077-400e-b40b-abde20d675d3'),
+      ],
+    };
+    const { shellAlert, buildingBlocks } = buildAlertGroupFromSequence({
+      sharedParams,
+      sequence: eqlSequence,
+      buildReasonMessage: buildReasonMessageForEqlAlert,
+    });
+    expect(buildingBlocks.length).toEqual(2);
+    expect(buildingBlocks[0]).toEqual(
+      expect.objectContaining({
+        _source: expect.objectContaining({
+          [ALERT_ANCESTORS]: [
+            {
+              depth: 0,
+              id: 'd5e8eb51-a6a0-456d-8a15-4b79bfec3d71',
+              index: 'myFakeSignalIndex',
+              type: 'event',
+            },
+          ],
+          [ALERT_DEPTH]: 1,
+          [ALERT_RULE_CONSUMER]: SERVER_APP_ID,
+          [ALERT_BUILDING_BLOCK_TYPE]: 'default',
+        }),
+      })
+    );
+    expect(buildingBlocks[0]?._source?.[ALERT_URL]).toContain(
+      'http://testkibanabaseurl.com/s/space/app/security/alerts/redirect/f2db3574eaf8450e3f4d1cf4f416d70b110b035ae0a7a00026242df07f0a6c90?index=.alerts-security.alerts-space'
+    );
+    expect(buildingBlocks[1]).toEqual(
+      expect.objectContaining({
+        _source: expect.objectContaining({
+          [ALERT_ANCESTORS]: [
+            {
+              depth: 0,
+              id: '619389b2-b077-400e-b40b-abde20d675d3',
+              index: 'myFakeSignalIndex',
+              type: 'event',
+            },
+          ],
+          [ALERT_DEPTH]: 1,
+          [ALERT_RULE_CONSUMER]: SERVER_APP_ID,
+          [ALERT_BUILDING_BLOCK_TYPE]: 'default',
+        }),
+      })
+    );
+    expect(buildingBlocks[1]._source[ALERT_URL]).toContain(
+      'http://testkibanabaseurl.com/s/space/app/security/alerts/redirect/1dbc416333244efbda833832eb83f13ea5d980a33c2f981ca8d2b35d82a045da?index=.alerts-security.alerts-space'
+    );
+    expect(shellAlert).toEqual(
+      expect.objectContaining({
+        _source: expect.objectContaining({
+          [ALERT_ANCESTORS]: expect.arrayContaining([
+            {
+              depth: 0,
+              id: 'd5e8eb51-a6a0-456d-8a15-4b79bfec3d71',
+              index: 'myFakeSignalIndex',
+              type: 'event',
+            },
+            {
+              depth: 0,
+              id: '619389b2-b077-400e-b40b-abde20d675d3',
+              index: 'myFakeSignalIndex',
+              type: 'event',
+            },
+            {
+              depth: 1,
+              id: buildingBlocks[0]?._id,
+              index: '',
+              rule: sampleRuleGuid,
+              type: 'signal',
+            },
+            {
+              depth: 1,
+              id: buildingBlocks[0]._id,
+              index: '',
+              rule: sampleRuleGuid,
+              type: 'signal',
+            },
+          ]),
+          [ALERT_DEPTH]: 2,
+          [ALERT_BUILDING_BLOCK_TYPE]: 'default',
+          [ALERT_RULE_CONSUMER]: SERVER_APP_ID,
+          [ALERT_REASON]: 'event with source 127.0.0.1 created high alert rule-name.',
+        }),
+      })
+    );
+    expect(shellAlert?._source[ALERT_URL]).toContain(
+      'http://testkibanabaseurl.com/s/space/app/security/alerts/redirect/1b7d06954e74257140f3bf73f139078483f9658fe829fd806cc307fc0388fb23?index=.alerts-security.alerts-space'
+    );
+    const groupIds = buildingBlocks.map((alert) => alert?._source?.[ALERT_GROUP_ID]);
+    for (const groupId of groupIds) {
+      expect(groupId).toEqual(groupIds[0]);
+    }
+  });
+
+  // "Missing events" here refer to use of the NOT operator in EQL sequences, not scenarios where the doc should exist but
+  // we didn't find it
+  test('it builds an alert correctly with some missing events in the sequence', () => {
+    const eqlSequence: EqlHitsSequence<SignalSource> = {
+      join_keys: [],
+      events: [
+        sampleDocNoSortId('d5e8eb51-a6a0-456d-8a15-4b79bfec3d71'),
+        {
+          // Not just a lazy mock, this is actually what the EQL search API returns for missing events
+          _index: '',
+          _id: '',
+          _source: {},
+          missing: true,
+        },
         sampleDocNoSortId('619389b2-b077-400e-b40b-abde20d675d3'),
       ],
     };
