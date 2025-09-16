23ece6a3baf09eae681305f0ad4a47d6b8e4772f
Christos Nasikas <christos.nasikas@elastic.co>
2025-09-16 14:08:47 +0300

    Remove .only in test

--- x-pack/platform/test/alerting_api_integration/security_and_spaces/group3/tests/alerting/bulk_edit.ts
+++ x-pack/platform/test/alerting_api_integration/security_and_spaces/group3/tests/alerting/bulk_edit.ts
@@ -1046,7 +1046,7 @@ export default function createUpdateTests({ getService }: FtrProviderContext) {
       });
     });
 
-    describe.only('internally managed rule types', () => {
+    describe('internally managed rule types', () => {
       const rulePayload = getAlwaysFiringInternalRule();
 
       const payloadWithFilter = {
