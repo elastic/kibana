c0d54ace1825a44cb17fbaca9d1d0a811e29f496
Julia Rechkunova <julia.rechkunova@elastic.co>
2025-09-16 19:54:15 +0200

    [Discover] Update tests

--- src/platform/test/functional/apps/discover/tabs/_on_tab_change.ts
+++ src/platform/test/functional/apps/discover/tabs/_on_tab_change.ts
@@ -84,87 +84,83 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         expect(timeConfig.end).to.be(toTime);
       };
 
-      const expectCustomTimeRange = async () => {
+      const expectLogstashTimeRange = async () => {
         const timeConfig = await timePicker.getTimeConfig();
-        expect(timeConfig.start).to.be('~ 3 months ago');
-        expect(timeConfig.end).to.be('now');
+        expect(timeConfig.start).to.be('Sep 19, 2015 @ 06:31:44.000');
+        expect(timeConfig.end).to.be('Sep 23, 2015 @ 18:31:44.000');
       };
 
-      await timePicker.setAbsoluteRange(fromTime, toTime);
-      await discover.waitUntilTabIsLoaded();
-      await discover.selectIndexPattern('kibana_sample_data_flights');
-      await discover.waitUntilTabIsLoaded();
-      await expectDisabledAllTime(false);
-      await expectFlightsTimeRange();
-      expect(await discover.getHitCount()).to.be('7,485');
-
-      await unifiedTabs.createNewTab();
-      await discover.waitUntilTabIsLoaded();
       await discover.selectTextBaseLang();
       await discover.waitUntilTabIsLoaded();
-      expect(await monacoEditor.getCodeEditorValue()).to.be(
-        'FROM kibana_sample_data_flights | WHERE timestamp >= ?_tstart AND timestamp <= ?_tend'
-      );
       await expectDisabledAllTime(false);
-      await expectFlightsTimeRange();
+      await expectLogstashTimeRange();
       expect(await discover.getHitCount()).to.be('1,000');
 
+      const queryWithTimeField =
+        'FROM kibana_sample_data_flights | WHERE timestamp >= ?_tstart AND timestamp <= ?_tend | LIMIT 50';
       await unifiedTabs.createNewTab();
       await discover.waitUntilTabIsLoaded();
       await expectDisabledAllTime(false);
+      await timePicker.setAbsoluteRange(fromTime, toTime);
+      await discover.waitUntilTabIsLoaded();
+      await esql.setEsqlEditorQuery(queryWithTimeField);
+      await testSubjects.click('querySubmitButton');
+      await discover.waitUntilTabIsLoaded();
+      expect(await monacoEditor.getCodeEditorValue()).to.be(queryWithTimeField);
+      await expectDisabledAllTime(false);
+      await expectFlightsTimeRange();
+      expect(await discover.getHitCount()).to.be('50');
+
       const queryWithoutTimeField = 'FROM kibana_sample_data_flights';
+      await unifiedTabs.createNewTab();
+      await discover.waitUntilTabIsLoaded();
+      await expectDisabledAllTime(false);
       await esql.setEsqlEditorQuery(queryWithoutTimeField);
       await testSubjects.click('querySubmitButton');
       await discover.waitUntilTabIsLoaded();
-      expect(await monacoEditor.getCodeEditorValue()).to.be('FROM kibana_sample_data_flights');
+      expect(await monacoEditor.getCodeEditorValue()).to.be(queryWithoutTimeField);
       await expectDisabledAllTime(true);
       expect(await discover.getHitCount()).to.be('1,000');
-
-      await unifiedTabs.createNewTab();
-      await discover.waitUntilTabIsLoaded();
-      await expectDisabledAllTime(true);
-      const queryWithDefaultTimeField = 'FROM logstash-*';
-      await esql.setEsqlEditorQuery(queryWithDefaultTimeField);
-      await testSubjects.click('querySubmitButton');
-      await discover.waitUntilTabIsLoaded();
-      await expectDisabledAllTime(false);
-      await expectFlightsTimeRange();
-      expect(await monacoEditor.getCodeEditorValue()).to.be('FROM logstash-*');
-      await discover.waitUntilTabIsLoaded();
-      await expectDisabledAllTime(false);
-      await timePicker.setCommonlyUsedTime('Last_90 days');
-      await discover.waitUntilTabIsLoaded();
-      await expectDisabledAllTime(false);
-      await expectCustomTimeRange();
-      expect(await discover.hasNoResults()).to.be(true);
 
       await unifiedTabs.selectTab(0);
       await discover.waitUntilTabIsLoaded();
       await expectDisabledAllTime(false);
-      await expectFlightsTimeRange();
-      expect(await discover.getHitCount()).to.be('7,485');
+      await expectLogstashTimeRange();
+      expect(await monacoEditor.getCodeEditorValue()).to.be('FROM logstash-*');
+      expect(await discover.getHitCount()).to.be('1,000');
 
       await unifiedTabs.selectTab(1);
       await discover.waitUntilTabIsLoaded();
       await expectDisabledAllTime(false);
       await expectFlightsTimeRange();
-      expect(await monacoEditor.getCodeEditorValue()).to.be(
-        'FROM kibana_sample_data_flights | WHERE timestamp >= ?_tstart AND timestamp <= ?_tend'
-      );
-      expect(await discover.getHitCount()).to.be('1,000');
+      expect(await monacoEditor.getCodeEditorValue()).to.be(queryWithTimeField);
+      expect(await discover.getHitCount()).to.be('50');
 
       await unifiedTabs.selectTab(2);
       await discover.waitUntilTabIsLoaded();
       await expectDisabledAllTime(true);
-      expect(await monacoEditor.getCodeEditorValue()).to.be('FROM kibana_sample_data_flights');
+      expect(await monacoEditor.getCodeEditorValue()).to.be(queryWithoutTimeField);
       expect(await discover.getHitCount()).to.be('1,000');
 
-      await unifiedTabs.selectTab(3);
+      await unifiedTabs.selectTab(1);
       await discover.waitUntilTabIsLoaded();
       await expectDisabledAllTime(false);
-      await expectCustomTimeRange();
-      expect(await monacoEditor.getCodeEditorValue()).to.be('FROM logstash-*');
-      expect(await discover.hasNoResults()).to.be(true);
+      await expectFlightsTimeRange();
+      expect(await monacoEditor.getCodeEditorValue()).to.be(queryWithTimeField);
+      expect(await discover.getHitCount()).to.be('50');
+
+      await unifiedTabs.selectTab(2);
+      await discover.waitUntilTabIsLoaded();
+      await expectDisabledAllTime(true);
+      expect(await monacoEditor.getCodeEditorValue()).to.be(queryWithoutTimeField);
+      expect(await discover.getHitCount()).to.be('1,000');
+      await esql.setEsqlEditorQuery(queryWithTimeField);
+      await testSubjects.click('querySubmitButton');
+      await discover.waitUntilTabIsLoaded();
+      expect(await monacoEditor.getCodeEditorValue()).to.be(queryWithTimeField);
+      await expectDisabledAllTime(false);
+      await expectFlightsTimeRange();
+      expect(await discover.getHitCount()).to.be('50');
     });
   });
 }
