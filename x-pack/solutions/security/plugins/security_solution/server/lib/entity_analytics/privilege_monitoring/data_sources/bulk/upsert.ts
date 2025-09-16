21d73e7efdc78f8ba5e764a68be9ee63c78ee754
CAWilson94 <charlotte.wilson@elastic.co>
2025-09-16 13:37:57 +0100

    Update roles on update

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/data_sources/bulk/upsert.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/data_sources/bulk/upsert.ts
@@ -139,6 +139,7 @@ export const makeIntegrationOpsBuilder = (dataClient: PrivilegeMonitoringDataCli
       buildUpdateParams: (user, sourceLabel) => ({
         new_privileged_status: user.isPrivileged,
         sourceLabel,
+        new_roles: user.roles ?? [],
       }),
       buildCreateDoc: (user, sourceLabel) => ({
         user: { name: user.username, is_privileged: user.isPrivileged },
--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/data_sources/sync/integrations/update_detection/queries.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/data_sources/sync/integrations/update_detection/queries.ts
@@ -96,7 +96,13 @@ if (params.new_privileged_status == false) {
   if (!ctx._source.labels.sources.contains(params.sourceLabel)) {
     ctx._source.labels.sources.add(params.sourceLabel);
   }
-}`;
+}
+if(params.containsKey('new_roles')&& params.new_roles != null) {
+    if (ctx._source.user == null) { ctx._source.user = new HashMap(); }
+    if (ctx._source.user.roles == null) { ctx._source.user.roles = new ArrayList(); }
+    ctx._source.user.roles = new ArrayList(params.new_roles);
+  }
+`;
 
 export const buildBulkUpsertBody = async (
   // merge with the index sync code, they are very similar
@@ -122,6 +128,7 @@ export const buildBulkUpsertBody = async (
             params: {
               new_privileged_status: user.isPrivileged,
               sourceLabel,
+              new_roles: user.roles ?? [],
             },
           },
         }
