5b3bdde67cc28ff1234ee54c0d0b2cc540e21d41
dej611 <dej611@gmail.com>
2025-09-16 11:33:34 +0200

    :label: Add missing type

--- x-pack/platform/plugins/shared/lens/public/datasources/form_based/operations/definitions/index.ts
+++ x-pack/platform/plugins/shared/lens/public/datasources/form_based/operations/definitions/index.ts
@@ -15,6 +15,7 @@ import type { DataPublicPluginStart } from '@kbn/data-plugin/public';
 import type { FieldFormatsStart } from '@kbn/field-formats-plugin/public';
 import type { UnifiedSearchPublicPluginStart } from '@kbn/unified-search-plugin/public';
 import { DataViewsPublicPluginStart } from '@kbn/data-views-plugin/public';
+import type { EuiThemeComputed } from '@elastic/eui';
 import { termsOperation } from './terms';
 import { filtersOperation } from './filters';
 import { cardinalityOperation } from './cardinality';
