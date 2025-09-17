a4fdfd5035c1d533440f342056bc97f8228ecbb3
inge4pres <francesco.gualazzi@elastic.co>
2025-09-17 15:57:44 +0200

    export type
    Signed-off-by: inge4pres <francesco.gualazzi@elastic.co>

--- x-pack/solutions/observability/plugins/profiling/public/services.ts
+++ x-pack/solutions/observability/plugins/profiling/public/services.ts
@@ -31,7 +31,7 @@ export interface APMTransactionsPerService {
 }
 
 // Use the common type for backward compatibility
-type ProfilingSetupStatus = ProfilingSetupStatusResponse;
+export type ProfilingSetupStatus = ProfilingSetupStatusResponse;
 
 export interface Services {
   fetchTopN: (params: {
