cb5d152aef1c41139a3d5f08d00f5545e6f532d7
Michael Olorunnisola <michael.olorunnisola@elastic.co>
2025-09-17 15:48:10 -0400

    fix mapping change

--- x-pack/platform/plugins/shared/cases/server/saved_object_types/cases/versioning.test.ts
+++ x-pack/platform/plugins/shared/cases/server/saved_object_types/cases/versioning.test.ts
@@ -59,7 +59,7 @@ describe('caseSavedObjectType model version transformations', () => {
   });
 
   describe('Model version 3 to 4', () => {
-    const version4Fields = ['incremental_id.text'];
+    const version4Fields = ['incremental_id.keyword'];
 
     it('by default does not add the new fields to the object', () => {
       const migrated = migrator.migrate({
