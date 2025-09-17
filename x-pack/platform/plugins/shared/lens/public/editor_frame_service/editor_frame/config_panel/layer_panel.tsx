caeef018b3201a843c70a5fc768abda81b5d5c4d
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-09-17 16:09:59 +0200

    remove horizontal padding

--- x-pack/platform/plugins/shared/lens/public/editor_frame_service/editor_frame/config_panel/layer_panel.tsx
+++ x-pack/platform/plugins/shared/lens/public/editor_frame_service/editor_frame/config_panel/layer_panel.tsx
@@ -389,7 +389,7 @@ export function LayerPanel(props: LayerPanelProps) {
           <header
             className="lnsLayerPanel__layerHeader"
             css={css`
-              padding: ${euiTheme.size.base};
+              padding: ${euiTheme.size.base} 0;
               border-bottom: ${euiTheme.border.thin};
             `}
           >
@@ -514,7 +514,7 @@ export function LayerPanel(props: LayerPanelProps) {
               return (
                 <EuiFormRow
                   css={css`
-                    padding: ${euiTheme.size.base};
+                    padding: ${euiTheme.size.base} 0;
                     &:last-child {
                       border-radius: 0 0 ${euiTheme.border.radius.medium}
                         ${euiTheme.border.radius.medium};
@@ -578,8 +578,8 @@ export function LayerPanel(props: LayerPanelProps) {
                       <ReorderProvider
                         dataTestSubj="lnsDragDrop"
                         css={css`
-                          margin: -${euiTheme.size.xs} -${euiTheme.size.base};
-                          padding: ${euiTheme.size.xs} ${euiTheme.size.base};
+                          margin: -${euiTheme.size.xs} 0;
+                          padding: ${euiTheme.size.xs} 0;
                         `}
                       >
                         {group.accessors.map((accessorConfig, accessorIndex) => {
@@ -850,7 +850,7 @@ export function LayerPanel(props: LayerPanelProps) {
                 <>
                   <div
                     css={css`
-                      padding: ${euiTheme.size.base};
+                      padding: ${euiTheme.size.base} 0;
                     `}
                   >
                     <activeVisualization.DimensionEditorComponent
