bef06334139309b306a2c0a73c9764a7ecdaa843
Joe Reuter <johannes.reuter@elastic.co>
2025-09-17 13:29:23 +0200

    fix type

--- x-pack/platform/plugins/shared/streams_app/public/util/esql_result_to_timeseries.ts
+++ x-pack/platform/plugins/shared/streams_app/public/util/esql_result_to_timeseries.ts
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import type { AbortableAsyncState } from '@kbn/react-hooks';
 import type { UnparsedEsqlResponse } from '@kbn/traced-es-client';
 import { orderBy } from 'lodash';
 import type { AsyncState } from 'react-use/lib/useAsync';
@@ -19,7 +20,7 @@ export function esqlResultToTimeseries<T extends string>({
   result,
   metricNames,
 }: {
-  result: AsyncState<UnparsedEsqlResponse>;
+  result: AsyncState<UnparsedEsqlResponse> | AbortableAsyncState<UnparsedEsqlResponse>;
   metricNames: T[];
 }): Array<Timeseries<T>> {
   const columns = result.value?.columns;
