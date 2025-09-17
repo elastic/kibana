3d4c032ffd2f327c73e5d15e6e70e2ef46148dc2
Timothy Sullivan <tsullivan@elastic.co>
2025-09-17 10:26:11 -0700

    Fix CSSInterpolation type imports

--- src/platform/packages/shared/kbn-css-utils/public/use_memo_css.ts
+++ src/platform/packages/shared/kbn-css-utils/public/use_memo_css.ts
@@ -7,11 +7,15 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { useEuiTheme, type UseEuiTheme } from '@elastic/eui';
-import type { Interpolation } from '@emotion/react';
 import { useMemo } from 'react';
+import type { CSSInterpolation } from '@emotion/serialize';
+import type { UseEuiTheme } from '@elastic/eui';
+import { useEuiTheme } from '@elastic/eui';
 
-export type EmotionStyles = Record<string, Interpolation | ((theme: UseEuiTheme) => Interpolation)>;
+export type EmotionStyles = Record<
+  string,
+  CSSInterpolation | ((theme: UseEuiTheme) => CSSInterpolation)
+>;
 
 /**
  * Custom hook to reduce boilerplate when working with Emotion styles that may depend on
@@ -34,16 +38,16 @@ export type EmotionStyles = Record<string, Interpolation | ((theme: UseEuiTheme)
  */
 export const useMemoCss = <T extends EmotionStyles>(
   styleMap: T
-): { [K in keyof T]: Interpolation } => {
+): { [K in keyof T]: CSSInterpolation } => {
   const euiThemeContext = useEuiTheme();
 
   const outputStyles = useMemo(() => {
-    return Object.entries(styleMap).reduce<{ [K in keyof T]: Interpolation }>(
+    return Object.entries(styleMap).reduce<{ [K in keyof T]: CSSInterpolation }>(
       (acc, [key, value]) => {
         acc[key as keyof T] = typeof value === 'function' ? value(euiThemeContext) : value;
         return acc;
       },
-      {} as { [K in keyof T]: Interpolation }
+      {} as { [K in keyof T]: CSSInterpolation }
     );
   }, [euiThemeContext, styleMap]);
 
