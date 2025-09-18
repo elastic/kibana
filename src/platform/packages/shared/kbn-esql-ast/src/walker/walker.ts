e5b8a9c36931defb017ac671150157524491afd9
vadimkibana <vadimkibana@gmail.com>
2025-09-18 11:53:42 +0200

    add replace properties helper

--- src/platform/packages/shared/kbn-esql-ast/src/walker/walker.ts
+++ src/platform/packages/shared/kbn-esql-ast/src/walker/walker.ts
@@ -9,7 +9,7 @@
 
 import type * as types from '../types';
 import type { NodeMatchTemplate } from './helpers';
-import { templateToPredicate } from './helpers';
+import { replaceProperties, templateToPredicate } from './helpers';
 
 type Node = types.ESQLAstNode | types.ESQLAstNode[];
 
@@ -280,11 +280,7 @@ export class Walker {
       typeof matcher === 'function' ? Walker.find(tree, matcher) : Walker.match(tree, matcher);
     if (!node) return;
     const replacement = typeof newValue === 'function' ? newValue(node) : newValue;
-    for (const key in node) {
-      if (typeof key === 'string' && Object.prototype.hasOwnProperty.call(node, key))
-        delete (node as any)[key];
-    }
-    Object.assign(node, replacement);
+    replaceProperties(node, replacement);
     return node;
   };
 
@@ -309,11 +305,7 @@ export class Walker {
     if (nodes.length === 0) return [];
     for (const node of nodes) {
       const replacement = typeof newValue === 'function' ? newValue(node) : newValue;
-      for (const key in node) {
-        if (typeof key === 'string' && Object.prototype.hasOwnProperty.call(node, key))
-          delete (node as any)[key];
-      }
-      Object.assign(node, replacement);
+      replaceProperties(node, replacement);
     }
     return nodes;
   };
