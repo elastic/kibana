0e86e2ff65134452603bb2036d742bb1367e54d9
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-09-18 09:42:10 +0200

    [Fleet] Update frozen variables for preconfigured managed policies (#235306)
    (cherry picked from commit 2738a40a6ec1dc07db5864bf309a54b430f2b57a)
    
    # Conflicts:
    #	x-pack/platform/plugins/shared/fleet/server/services/package_policy.test.ts
    #	x-pack/platform/plugins/shared/fleet/server/services/package_policy.ts
    #	x-pack/platform/plugins/shared/fleet/server/services/preconfiguration.ts

--- x-pack/platform/plugins/shared/fleet/server/services/package_policy.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/package_policy.ts
@@ -72,6 +72,7 @@ import type {
   PolicySecretReference,
   AssetsMap,
   AgentPolicy,
+  PreconfiguredInputs,
 } from '../../common/types';
 import {
   FleetError,
@@ -925,7 +926,12 @@ class PackagePolicyClientImpl implements PackagePolicyClient {
     esClient: ElasticsearchClient,
     id: string,
     packagePolicyUpdate: UpdatePackagePolicy,
-    options?: { user?: AuthenticatedUser; force?: boolean; skipUniqueNameVerification?: boolean }
+    options?: {
+      user?: AuthenticatedUser;
+      force?: boolean;
+      skipUniqueNameVerification?: boolean;
+      bumpRevision?: boolean;
+    }
   ): Promise<PackagePolicy> {
     const savedObjectType = await getPackagePolicySavedObjectType();
     auditLoggingService.writeCustomSoAuditLog({
@@ -1120,6 +1126,7 @@ class PackagePolicyClientImpl implements PackagePolicyClient {
       const assignedInOldPolicy = oldPackagePolicy.policy_ids.includes(policyId);
       const assignedInNewPolicy = newPolicy.policy_ids.includes(policyId);
 
+<<<<<<< HEAD
       // Remove protection if policy is unassigned (in old but not in updated) or policy is assigned (in updated but not in old)
       const removeProtection =
         isEndpointPolicy &&
@@ -1133,6 +1140,22 @@ class PackagePolicyClientImpl implements PackagePolicyClient {
         })
       );
     }
+=======
+        // Remove protection if policy is unassigned (in old but not in updated) or policy is assigned (in updated but not in old)
+        const removeProtection =
+          isEndpointPolicy &&
+          ((assignedInOldPolicy && !assignedInNewPolicy) ||
+            (!assignedInOldPolicy && assignedInNewPolicy));
+        if (options?.bumpRevision !== false) {
+          return agentPolicyService.bumpRevision(soClient, esClient, policyId, {
+            user: options?.user,
+            removeProtection,
+          });
+        }
+      },
+      { concurrency: MAX_CONCURRENT_AGENT_POLICIES_OPERATIONS }
+    );
+>>>>>>> 2738a40a6ec ([Fleet] Update frozen variables for preconfigured managed policies (#235306))
 
     const assetRemovePromise = removeOldAssets({
       soClient,
@@ -2961,7 +2984,7 @@ export function updatePackageInputs(
 export function preconfigurePackageInputs(
   basePackagePolicy: NewPackagePolicy,
   packageInfo: PackageInfo,
-  preconfiguredInputs?: InputsOverride[]
+  preconfiguredInputs?: PreconfiguredInputs[]
 ): NewPackagePolicy {
   if (!preconfiguredInputs) return basePackagePolicy;
 
