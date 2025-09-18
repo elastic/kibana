31ddde4f3da9cd210a853eb5091b6d8f4d64be04
Catherine Liu <catherine.liu@elastic.co>
2025-09-18 00:25:58 -0700

    Clean up

--- src/platform/plugins/shared/dashboard/public/dashboard_app/_dashboard_app_strings.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_app/_dashboard_app_strings.ts
@@ -246,14 +246,6 @@ export const topNavStrings = {
       defaultMessage: 'Create a copy of your dashboard',
     }),
   },
-  add: {
-    label: i18n.translate('dashboard.topNave.addButtonAriaLabel', {
-      defaultMessage: 'add',
-    }),
-    description: i18n.translate('dashboard.topNave.addConfigDescription', {
-      defaultMessage: 'Add content to your dashboard',
-    }),
-  },
 };
 
 export const getControlButtonTitle = () =>
