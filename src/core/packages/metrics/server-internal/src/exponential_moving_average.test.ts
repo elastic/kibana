53f3cc0dc09e4de9ab4194e8c269dc5b020175c3
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2025-09-18 07:36:06 -0700

    adds comment

--- src/core/packages/metrics/server-internal/src/exponential_moving_average.test.ts
+++ src/core/packages/metrics/server-internal/src/exponential_moving_average.test.ts
@@ -63,9 +63,11 @@ describe('exponentialMovingAverage', () => {
 
   describe('transition from mean to exponential moving average', () => {
     // Using realistic ELU values from Kibana startup
+    // TODO: change inputs back to originally used values: { a: 1, b: 1, c: 2, d: 2, e: 1, f: 1} for easier maths
     it('should switch to EMA after mean period (short window)', () => {
       // Short window: after 3 intervals, switches to EMA
       testScheduler.run(({ cold, expectObservable }) => {
+        // change inputs back to originally used values: { a: 1, b: 1, c: 2, d: 2, e: 1, f: 1}
         const observable = cold('abcd|', { a: 1.0, b: 1.0, c: 0.48, d: 0.03 }).pipe(
           exponentialMovingAverage(15, 5)
         );
