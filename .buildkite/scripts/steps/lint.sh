6260e5bba6de0d2d1e5e2456728f0b959f1b5be8
Alex Szabo <alex.szabo@elastic.co>
2025-09-18 16:00:31 +0200

    run lint through moon

--- .buildkite/scripts/steps/lint.sh
+++ .buildkite/scripts/steps/lint.sh
@@ -15,11 +15,11 @@ echo '--- Lint: eslint'
 # after possibly commiting fixed files to the repo
 set +e;
 if is_pr && ! is_auto_commit_disabled; then
-  desc="node scripts/eslint_all_files --no-cache --fix"
-  node scripts/eslint_all_files --no-cache --fix
+  desc="yarn moon ci :eslint-fix"
+  yarn moon ci :eslint-fix
 else
-  desc="node scripts/eslint_all_files --no-cache"
-  node scripts/eslint_all_files --no-cache
+  desc="yarn moon ci :eslint"
+  yarn moon ci :eslint
 fi
 
 eslint_exit=$?
