c557b71fd49a28ecff13f754167e6ee4b4efc45c
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-09-18 11:36:04 +0200

    Replace generic Layer tabs with layer type-specific names

--- src/platform/packages/shared/kbn-lens-common/index.ts
+++ src/platform/packages/shared/kbn-lens-common/index.ts
@@ -378,3 +378,4 @@ export {
 } from './visualizations/xy/constants';
 export { LENS_SHARE_STATE_ACTION } from './locator_types';
 export { defaultSeriesType } from './visualizations/xy/types';
+export { getLensLayerTypeDisplayName } from './visualizations/layer_type_display_name';
