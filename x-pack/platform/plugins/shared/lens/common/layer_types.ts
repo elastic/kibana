208f2fda4971c36d30afd92b6d38e7696cdec479
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-09-18 11:36:04 +0200

    Replace generic Layer tabs with layer type-specific names

--- x-pack/platform/plugins/shared/lens/common/layer_types.ts
+++ x-pack/platform/plugins/shared/lens/common/layer_types.ts
@@ -5,9 +5,38 @@
  * 2.0.
  */
 
+import { i18n } from '@kbn/i18n';
+
 export const layerTypes = {
   DATA: 'data',
   REFERENCELINE: 'referenceLine',
   ANNOTATIONS: 'annotations',
   METRIC_TRENDLINE: 'metricTrendline',
 } as const;
+export type LayerType = (typeof layerTypes)[keyof typeof layerTypes];
+
+// Mapping of layer types to display names
+export const layerTypeDisplayNames = {
+  data: i18n.translate('xpack.lens.layerTypes.displayName.data', {
+    defaultMessage: 'Data',
+  }),
+  referenceLine: i18n.translate('xpack.lens.layerTypes.displayName.referenceLine', {
+    defaultMessage: 'Reference line',
+  }),
+  annotations: i18n.translate('xpack.lens.layerTypes.displayName.annotations', {
+    defaultMessage: 'Annotation',
+  }),
+  metricTrendline: i18n.translate('xpack.lens.layerTypes.displayName.metricTrendline', {
+    defaultMessage: 'Metric trendline',
+  }),
+} as const;
+
+// Utility function to get the display name for a layer type
+export function getLayerTypeDisplayName(layerType?: LayerType): string {
+  if (!layerType)
+    return i18n.translate('xpack.lens.layerTypes.displayName.layer', {
+      defaultMessage: 'Layer',
+    });
+
+  return layerTypeDisplayNames[layerType] || layerType;
+}
