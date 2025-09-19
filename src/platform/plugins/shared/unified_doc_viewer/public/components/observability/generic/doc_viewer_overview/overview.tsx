8a3a9c2cce34699b55c2f2a7e7a27245a8c5dd5b
Gonçalo Rica Pais da Silva <goncalo.rica@elastic.co>
2025-09-19 14:35:24 +0200

    [Discover][APM] Generic document overview fallback profile (#235388)
    ## Summary
    
    This PR provides a minimal fallback document profile that does not care
    about the source/index of the document it is looking at in a
    Observability Discover profile, only that it has a `trace.id`. This then
    allows for a document overview to be loaded to have some minimal
    summaries and provide a Trace Waterfall widget. This should allow to
    attach some additional context to documents from mixed sources or
    complex queries, as long as they have certain fields available.
    
    <img alt="Generic Document Profile example"
    src="https://github.com/user-attachments/assets/2bb1b743-3cf5-4c9b-9660-1dd84b1cbfe1"
    />
    
    
    ## How to Test
    
    - Ensure space is set to Observability mode, go to Discover and into
    ES|QL mode.
    - Craft a query targetting a specific traces index, and open a document
    overview. This overview should be a Trace Overview.
    - Change the query to have `KEEP` filters, filter down to just a couple
    of fields, like `@timestamp` and `trace.id`. Open a document, and it
    should be just a generic Overview tab.

--- src/platform/plugins/shared/unified_doc_viewer/public/components/observability/generic/doc_viewer_overview/overview.tsx
+++ src/platform/plugins/shared/unified_doc_viewer/public/components/observability/generic/doc_viewer_overview/overview.tsx
@@ -13,31 +13,18 @@ import { getFlattenedTraceDocumentOverview } from '@kbn/discover-utils';
 import type { DocViewRenderProps } from '@kbn/unified-doc-viewer/types';
 import React, { useMemo, useState } from 'react';
 import { css } from '@emotion/react';
-import {
-  DURATION,
-  SERVICE_NAME,
-  SPAN_DURATION,
-  SPAN_ID,
-  SPAN_NAME,
-  TRACE_ID,
-  TRANSACTION_DURATION,
-  TRANSACTION_ID,
-  TRANSACTION_NAME,
-  TRANSACTION_TYPE,
-} from '@kbn/apm-types';
-import { SpanLinks } from '../components/span_links';
-import { DataSourcesProvider } from '../hooks/use_data_sources';
+import { SERVICE_NAME, SPAN_ID, TRACE_ID, TRANSACTION_ID } from '@kbn/apm-types';
+import { DataSourcesProvider } from '../../traces/hooks/use_data_sources';
 import {
   getTabContentAvailableHeight,
   DEFAULT_MARGIN_BOTTOM,
 } from '../../../doc_viewer_source/get_height';
-import { About } from '../components/about';
-import { TraceContextLogEvents } from '../components/trace_context_log_events';
-import { SimilarSpans } from '../components/similar_spans';
-import { isTransaction } from '../helpers';
-import { TraceRootItemProvider } from './hooks/use_fetch_trace_root_item';
-import { TraceWaterfall } from '../components/trace_waterfall';
-import { ErrorsTable } from '../components/errors';
+import { About } from '../../traces/components/about';
+import { TraceRootItemProvider } from '../../traces/doc_viewer_overview/hooks/use_fetch_trace_root_item';
+import { TraceWaterfall } from '../../traces/components/trace_waterfall';
+import { ErrorsTable } from '../../traces/components/errors';
+import { TraceContextLogEvents } from '../../traces/components/trace_context_log_events';
+import { isTransaction } from '../../traces/helpers';
 
 export type OverviewProps = DocViewRenderProps & {
   indexes: TraceIndexes;
@@ -59,9 +46,6 @@ export function Overview({
   const flattenedHit = useMemo(() => getFlattenedTraceDocumentOverview(hit), [hit]);
 
   const isSpan = !isTransaction(hit);
-  const apmDurationField = flattenedHit[TRANSACTION_DURATION] ?? flattenedHit[SPAN_DURATION];
-  const isOtelSpan = apmDurationField == null && flattenedHit[DURATION] != null;
-  const duration = apmDurationField ?? flattenedHit[DURATION]! * 0.001;
 
   const traceId = flattenedHit[TRACE_ID];
   const transactionId = flattenedHit[TRANSACTION_ID];
@@ -96,25 +80,14 @@ export function Overview({
             onRemoveColumn={onRemoveColumn}
           />
           <EuiSpacer size="m" />
-          <SimilarSpans
-            spanName={flattenedHit[SPAN_NAME]}
-            serviceName={serviceName}
-            transactionName={flattenedHit[TRANSACTION_NAME]}
-            transactionType={flattenedHit[TRANSACTION_TYPE]}
-            isOtelSpan={isOtelSpan}
-            duration={duration || 0}
-          />
-          <EuiSpacer size="m" />
           {showWaterfall ? (
             <TraceWaterfall
               dataView={dataView}
               traceId={traceId}
-              serviceName={serviceName}
               docId={docId}
+              serviceName={serviceName}
             />
           ) : null}
-          <EuiSpacer size="m" />
-          {docId ? <SpanLinks traceId={traceId} docId={docId} /> : null}
           <EuiSpacer size="m" />
           <ErrorsTable traceId={traceId} docId={docId} />
           <EuiSpacer size="m" />
