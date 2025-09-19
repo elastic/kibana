3c132dfa9b546492950509250b442b3e46a54ae4
Clint Andrew Hall <clint@clintandrewhall.com>
2025-09-19 13:58:43 -0400

    [wip] Workspace Components

---
+++ src/core/packages/chrome/workspace/core-workspace-chrome-components/navigation/workspace_navigation.tsx
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React, { useState } from 'react';
+import type { NavigationProps } from '@kbn/core-chrome-navigation';
+import { Navigation } from '@kbn/core-chrome-navigation';
+import { useIsNavigationCollapsed, useWorkspaceDispatch } from '@kbn/core-workspace-chrome-state';
+import { setNavigationWidth } from '@kbn/core-workspace-chrome-state';
+import { WorkspaceNavControl } from './workspace_nav_control';
+
+export type WorkspaceNavigationProps = Pick<NavigationProps, 'items' | 'logo'>;
+
+export const WorkspaceNavigation = (props: WorkspaceNavigationProps) => {
+  const isCollapsed = useIsNavigationCollapsed();
+  const dispatch = useWorkspaceDispatch();
+  const setWidth = (width: number) => dispatch(setNavigationWidth(width));
+  const [activeItemId, setActiveItemId] = useState(props.items.primaryItems[0].id);
+  const onItemClick: NavigationProps['onItemClick'] = (item) => setActiveItemId(item.id);
+
+  return (
+    <Navigation
+      variant="workspace"
+      {...{ isCollapsed, setWidth, activeItemId, onItemClick, ...props }}
+    >
+      <WorkspaceNavControl />
+    </Navigation>
+  );
+};
