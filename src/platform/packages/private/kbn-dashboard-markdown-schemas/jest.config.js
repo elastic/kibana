ebe1c00c860ddb7829abe021ebbc875e997fee1e
Nathan Reese <reese.nathan@gmail.com>
2025-09-19 14:31:18 -0600

    fix jest config

--- src/platform/packages/private/kbn-dashboard-markdown-schemas/jest.config.js
+++ src/platform/packages/private/kbn-dashboard-markdown-schemas/jest.config.js
@@ -10,5 +10,5 @@
 module.exports = {
   preset: '@kbn/test/jest_node',
   rootDir: '../../../../..',
-  roots: ['<rootDir>/src/platform/packages/private/kbn-dashboard_markdown_schemas'],
+  roots: ['<rootDir>/src/platform/packages/private/kbn-dashboard-markdown-schemas'],
 };
