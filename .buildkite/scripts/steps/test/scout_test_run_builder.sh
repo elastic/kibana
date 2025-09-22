1b713644fd7667145df1ab44df95d78fe5997179
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-09-22 16:07:28 +0200

    [scout] update failed test reporter to support package (#235612)
    ## Summary
    
    This PR fixes `ScoutFailedTestReporter` when it processes tests located
    in Kibana package:
    
    - adjust `kibana.jsonc` parsing, package and plugin have different
    content
    - rename types/function to use `kibanaModule` instead of `plugin`
    - move `kbn/scout` internal tests into `test/scout` dir to follow the
    pattern
    
    Html report for package test
    <img width="1141" height="658" alt="image"
    src="https://github.com/user-attachments/assets/0b9f312e-4be5-493e-a1a1-f08eb7c60875"
    />
    
    ---------
    
    Co-authored-by: David Olaru <dolaru@vaevixen.com>
    (cherry picked from commit 0390bf59a8b8ba4b5bd112fb3a62f21c1bab6874)
    
    # Conflicts:
    #	src/platform/packages/shared/kbn-scout/test/scout/fixtures/constants.ts

--- .buildkite/scripts/steps/test/scout_test_run_builder.sh
+++ .buildkite/scripts/steps/test/scout_test_run_builder.sh
@@ -15,7 +15,7 @@ buildkite-agent artifact upload "scout_playwright_configs.json"
 echo '--- Running Scout Integration Tests'
 node scripts/scout.js run-tests \
 --serverless=security \
---config src/platform/packages/shared/kbn-scout/integration_tests/playwright.config.ts \
+--config src/platform/packages/shared/kbn-scout/test/scout/playwright.config.ts \
 --kibana-install-dir "$KIBANA_BUILD_LOCATION"
 
 echo '--- Producing Scout Test Execution Steps'
