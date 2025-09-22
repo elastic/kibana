59f96adc60a804a642fa23d226f2f26dd76c03d3
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2025-09-22 20:03:53 +0200

    bring back locator

--- x-pack/platform/packages/shared/ml/plugin_contracts/index.ts
+++ x-pack/platform/packages/shared/ml/plugin_contracts/index.ts
@@ -5,17 +5,20 @@
  * 2.0.
  */
 
+import type { LocatorPublic } from '@kbn/share-plugin/public';
 import type { IElasticModels } from '@kbn/ml-trained-models-utils';
-import type { MlManagementLocator } from '@kbn/ml-common-types/locator';
+import type { MlLocatorParams, MlManagementLocator } from '@kbn/ml-common-types/locator';
 import type { MlApi } from '@kbn/ml-services/ml_api_service';
 import type { AnomalySwimLaneComponentType } from '@kbn/ml-common-types/anomaly_swim_lane';
 
 export interface MlPluginSetup {
+  locator?: LocatorPublic<MlLocatorParams>;
   getManagementLocator?: (() => Promise<MlManagementLocator>) | undefined;
   getElasticModels?: (() => Promise<IElasticModels>) | undefined;
 }
 
 export interface MlPluginStart {
+  locator?: LocatorPublic<MlLocatorParams>;
   getManagementLocator?: (() => Promise<MlManagementLocator>) | undefined;
   getElasticModels?: (() => Promise<IElasticModels>) | undefined;
   getMlApi: () => Promise<MlApi>;
