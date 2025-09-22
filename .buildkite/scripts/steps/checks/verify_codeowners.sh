db4ff7b8e32fd6b19248b7fda5ce7993bb1322a3
Brad White <brad.white@elastic.co>
2025-09-22 16:33:11 -0600

    restrict to main base branch

--- .buildkite/scripts/steps/checks/verify_codeowners.sh
+++ .buildkite/scripts/steps/checks/verify_codeowners.sh
@@ -6,7 +6,7 @@ source .buildkite/scripts/common/util.sh
 
 echo --- Verify CODEOWNERS
 
-if is_pr && ! is_auto_commit_disabled; then
+if is_pr && ! is_auto_commit_disabled && [[ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ]]; then
   node scripts/generate codeowners
   check_for_changed_files "node scripts/generate codeowners" true
 fi
