41686e4cfe5f880d3e5d6099786739d4bfad32c7
Nicholas Peretti <nicholas.peretti@elastic.co>
2025-09-22 13:20:52 +0100

    Revert "Apply PR feedback"
    This reverts commit 3b02b7719da96fd22e65105606f1e061f8c3473e.

--- x-pack/solutions/security/plugins/security_solution/common/api/detection_engine/signals/set_signal_status/set_signals_status_route.gen.ts
+++ x-pack/solutions/security/plugins/security_solution/common/api/detection_engine/signals/set_signal_status/set_signals_status_route.gen.ts
@@ -34,21 +34,21 @@ export const ReasonEnum = z.enum([
 export type ReasonEnumEnum = typeof ReasonEnum.enum;
 export const ReasonEnumEnum = ReasonEnum.enum;
 
-/**
- * List of alert ids. Use field `_id` on alert document or `kibana.alert.uuid`. Note: signals are a deprecated term for alerts.
- */
-export type SignalIds = z.infer<typeof SignalIds>;
-export const SignalIds = z.array(z.string().min(1).superRefine(isNonEmptyString)).min(1);
-
 export type SetAlertsStatusByIdsBase = z.infer<typeof SetAlertsStatusByIdsBase>;
 export const SetAlertsStatusByIdsBase = z.object({
-  signal_ids: SignalIds,
+  /**
+   * List of alert ids. Use field `_id` on alert document or `kibana.alert.uuid`. Note: signals are a deprecated term for alerts.
+   */
+  signal_ids: z.array(z.string().min(1).superRefine(isNonEmptyString)).min(1),
   status: AlertStatusExceptClosed,
 });
 
 export type CloseAlertsByIds = z.infer<typeof CloseAlertsByIds>;
 export const CloseAlertsByIds = z.object({
-  signal_ids: SignalIds,
+  /**
+   * List of alert ids. Use field `_id` on alert document or `kibana.alert.uuid`. Note: signals are a deprecated term for alerts.
+   */
+  signal_ids: z.array(z.string().min(1).superRefine(isNonEmptyString)).min(1),
   status: z.literal('closed'),
   reason: ReasonEnum.optional(),
 });
