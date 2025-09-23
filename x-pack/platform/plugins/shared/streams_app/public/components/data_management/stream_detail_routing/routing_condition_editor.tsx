cfcef0c49f75099d6ae6b7a17de99ffe3a946608
achyutjhunjhunwala <achyut.jhunjhunwala@elastic.co>
2025-09-23 15:08:04 +0200

    Fix more import issues

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_routing/routing_condition_editor.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_routing/routing_condition_editor.tsx
@@ -1,9 +1,18 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 import React from 'react';
-import { isRoutingEnabled, RoutingDefinition } from '@kbn/streams-schema';
-import { useRoutingFieldSuggestions } from '@kbn/streams-app-plugin/public/hooks/use_field_suggestions';
+import type { RoutingDefinition } from '@kbn/streams-schema';
+import { isRoutingEnabled } from '@kbn/streams-schema';
 import { EuiFlexGroup, EuiForm, EuiFormRow, EuiIconTip, EuiSwitch } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { ConditionEditor, ConditionEditorProps } from '../shared/condition_editor';
+import { useRoutingFieldSuggestions } from '../../../hooks/use_field_suggestions';
+import type { ConditionEditorProps } from '../shared/condition_editor';
+import { ConditionEditor } from '../shared/condition_editor';
 
 type RoutingConditionChangeParams = Omit<RoutingDefinition, 'destination'>;
 
