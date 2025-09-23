e777dee4a100fa4914b4bc34d59746c96b8a57de
Cesare de Cal <cesare.decal@elastic.co>
2025-09-23 12:54:22 +0200

    fix: import path

--- src/platform/packages/shared/kbn-scout/test/scout/fixtures/constants.ts
+++ src/platform/packages/shared/kbn-scout/test/scout/fixtures/constants.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { CasePostRequest } from '../../src/playwright/fixtures/scope/worker/apis/cases/types';
+import type { CasePostRequest } from '../../../src/playwright/fixtures/scope/worker/apis/cases/types';
 
 export const createAlertRuleParams = {
   aggType: 'count',
