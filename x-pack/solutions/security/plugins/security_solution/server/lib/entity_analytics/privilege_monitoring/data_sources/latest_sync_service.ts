fcf64c5030a34c4c1314bd1185775fd2beafeb65
CAWilson94 <charlotte.wilson@elastic.co>
2025-09-24 20:21:37 +0100

    latestSync stub WiP need findByType here for integrations entity index

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/data_sources/latest_sync_service.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/data_sources/latest_sync_service.ts
@@ -5,13 +5,24 @@
  * 2.0.
  */
 
+import type { SavedObjectsClientContract } from '@kbn/core/server';
 import type { PrivilegeMonitoringDataClient } from '../engine/data_client';
+import { MonitoringEntitySourceDescriptorClient } from '../saved_objects';
 
-export const latestSyncService = (dataClient: PrivilegeMonitoringDataClient) => {
+export const latestSyncService = (
+  soClient: SavedObjectsClientContract,
+  dataClient: PrivilegeMonitoringDataClient
+) => {
+  const { deps } = dataClient;
+  const monitoringIndexSourceClient = new MonitoringEntitySourceDescriptorClient({
+    soClient,
+    namespace: deps.namespace,
+  });
   const updateLatestSync = async () => {
     /**
      * 1. When was the latest sync?
      * 2. Update the latest sync timestamp on the Saved Object
      */
+    // need to get the indexPattern of entity okta from the saved object
   };
 };
