c4bc07f1eee5edbafcd3330b520468756cff50b4
Alex Szabo <alex.szabo@elastic.co>
2025-09-24 16:51:18 +0200

    try exporting NODE_ENV to get priorty

--- .buildkite/scripts/steps/check_types.sh
+++ .buildkite/scripts/steps/check_types.sh
@@ -12,6 +12,8 @@ if [[ "${RUN_FULL_CHECK:-}" == "true" ]]; then
   node scripts/type_check
 else
   echo "Running type-check with caches..."
+  echo "NODE_OPTIONS=$NODE_OPTIONS"
+  export NODE_OPTIONS="--max-old-space-size=8192"
   yarn moon ci :typecheck -c 1 \
     --base $BUILDKITE_PULL_REQUEST_BASE_BRANCH
 fi
