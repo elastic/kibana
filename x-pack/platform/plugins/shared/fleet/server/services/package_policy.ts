482020433e23c9e3c4ac5124e40fd7e307f81511
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-09-25 14:31:59 +0200

    [8.18] [Fleet] Update frozen variables for preconfigured managed policies (#235306) (#236244)
    # Backport
    
    This will backport the following commits from `main` to `8.18`:
    - [[Fleet] Update frozen variables for preconfigured managed policies
    (#235306)](https://github.com/elastic/kibana/pull/235306)
    
    <!--- Backport version: 10.0.2 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Nicolas
    Chaulet","email":"nicolas.chaulet@elastic.co"},"sourceCommit":{"committedDate":"2025-09-18T07:42:10Z","message":"[Fleet]
    Update frozen variables for preconfigured managed policies
    (#235306)","sha":"2738a40a6ec1dc07db5864bf309a54b430f2b57a","branchLabelMapping":{"^v9.2.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","Team:Fleet","backport:version","v9.2.0","v8.18.8","v8.19.5","v9.0.8","v9.1.5"],"title":"[Fleet]
    Update frozen variables for preconfigured managed
    policies","number":235306,"url":"https://github.com/elastic/kibana/pull/235306","mergeCommit":{"message":"[Fleet]
    Update frozen variables for preconfigured managed policies
    (#235306)","sha":"2738a40a6ec1dc07db5864bf309a54b430f2b57a"}},"sourceBranch":"main","suggestedTargetBranches":["8.18","8.19","9.0","9.1"],"targetPullRequestStates":[{"branch":"main","label":"v9.2.0","branchLabelMappingKey":"^v9.2.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/235306","number":235306,"mergeCommit":{"message":"[Fleet]
    Update frozen variables for preconfigured managed policies
    (#235306)","sha":"2738a40a6ec1dc07db5864bf309a54b430f2b57a"}},{"branch":"8.18","label":"v8.18.8","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"8.19","label":"v8.19.5","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"9.0","label":"v9.0.8","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"},{"branch":"9.1","label":"v9.1.5","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/plugins/shared/fleet/server/services/package_policy.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/package_policy.ts
@@ -72,6 +72,7 @@ import type {
   PolicySecretReference,
   AssetsMap,
   AgentPolicy,
+  PreconfiguredInputs,
 } from '../../common/types';
 import {
   FleetError,
@@ -925,7 +926,12 @@ class PackagePolicyClientImpl implements PackagePolicyClient {
     esClient: ElasticsearchClient,
     id: string,
     packagePolicyUpdate: UpdatePackagePolicy,
-    options?: { user?: AuthenticatedUser; force?: boolean; skipUniqueNameVerification?: boolean }
+    options?: {
+      user?: AuthenticatedUser;
+      force?: boolean;
+      skipUniqueNameVerification?: boolean;
+      bumpRevision?: boolean;
+    }
   ): Promise<PackagePolicy> {
     const savedObjectType = await getPackagePolicySavedObjectType();
     auditLoggingService.writeCustomSoAuditLog({
@@ -1126,12 +1132,14 @@ class PackagePolicyClientImpl implements PackagePolicyClient {
         ((assignedInOldPolicy && !assignedInNewPolicy) ||
           (!assignedInOldPolicy && assignedInNewPolicy));
 
-      bumpPromises.push(
-        agentPolicyService.bumpRevision(soClient, esClient, policyId, {
-          user: options?.user,
-          removeProtection,
-        })
-      );
+      if (options?.bumpRevision !== false) {
+        bumpPromises.push(
+          agentPolicyService.bumpRevision(soClient, esClient, policyId, {
+            user: options?.user,
+            removeProtection,
+          })
+        );
+      }
     }
 
     const assetRemovePromise = removeOldAssets({
@@ -2961,7 +2969,7 @@ export function updatePackageInputs(
 export function preconfigurePackageInputs(
   basePackagePolicy: NewPackagePolicy,
   packageInfo: PackageInfo,
-  preconfiguredInputs?: InputsOverride[]
+  preconfiguredInputs?: PreconfiguredInputs[]
 ): NewPackagePolicy {
   if (!preconfiguredInputs) return basePackagePolicy;
 
