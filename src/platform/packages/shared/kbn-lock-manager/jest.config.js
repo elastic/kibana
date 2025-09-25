5b472b6ad72f0579d25fad8c48023c90c2409360
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-09-25 15:10:48 +0200

    Relocating module `@kbn/lock-manager`

--- src/platform/packages/shared/kbn-lock-manager/jest.config.js
+++ src/platform/packages/shared/kbn-lock-manager/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test/jest_node',
-  rootDir: '../..',
-  roots: ['<rootDir>/packages/kbn-lock-manager'],
+  rootDir: '../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/kbn-lock-manager'],
 };
