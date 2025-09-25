ccecd02bce642804c21f35e06c70f7444a1b9733
Ignacio Rivas <rivasign@gmail.com>
2025-09-25 14:32:47 +0200

    Remove mocks

--- x-pack/platform/plugins/private/monitoring/server/routes/api/v1/cluster/cluster.ts
+++ x-pack/platform/plugins/private/monitoring/server/routes/api/v1/cluster/cluster.ts
@@ -15,10 +15,6 @@ import { getClustersFromRequest } from '../../../../lib/cluster/get_clusters_fro
 import { handleError } from '../../../../lib/errors';
 import type { MonitoringCore } from '../../../../types';
 
-const mockClusters = [
-  {"cluster_uuid":"9HiBinviQNalkR2Cauq5iw","cluster_name":"My deployment (22b5a7)","version":"9.1.4","license":{"status":"active","type":"enterprise","expiry_date_in_millis":1835481599999},"elasticsearch":{"cluster_stats":{"indices":{"count":51,"docs":{"count":4360},"shards":{"total":102,"primaries":51},"store":{"size_in_bytes":17803692}},"nodes":{"fs":{"total_in_bytes":493921239040,"available_in_bytes":493885562880},"count":{"total":3},"jvm":{"max_uptime_in_millis":478755,"mem":{"heap_max_in_bytes":8640266240,"heap_used_in_bytes":1984305288}}},"status":"green"},"logs":{"enabled":true,"types":[{"type":"server","levels":[{"level":"info","count":89},{"level":"warn","count":52}]}]}},"logstash":{},"kibana":{"status":null,"some_status_is_stale":true,"requests_total":0,"concurrent_connections":0,"response_time_max":0,"memory_size":0,"memory_limit":0,"count":0,"rules":{"cluster":null,"instance":null}},"ml":{"jobs":0},"beats":{"totalEvents":0,"bytesSent":0,"beats":{"total":0,"types":[]}},"apm":{"totalEvents":0,"memRss":53977088,"apms":{"total":1},"versions":["9.1.4"],"config":{"container":true}},"enterpriseSearch":{"clusterUuid":"9HiBinviQNalkR2Cauq5iw","stats":{"appSearchEngines":0,"workplaceSearchOrgSources":0,"workplaceSearchPrivateSources":0,"totalInstances":0,"uptime":0,"memUsed":0,"memCommitted":0,"memTotal":0,"versions":[]}},"isPrimary":false,"status":"green","isCcrEnabled":true}
-];
-
 export function clusterRoute(server: MonitoringCore) {
   /*
    * Cluster Overview
@@ -52,9 +48,8 @@ export function clusterRoute(server: MonitoringCore) {
       };
 
       try {
-        return mockClusters;
-        // const clusters = await getClustersFromRequest(req, options);
-        // return postClusterResponsePayloadRT.encode(clusters);
+        const clusters = await getClustersFromRequest(req, options);
+        return postClusterResponsePayloadRT.encode(clusters);
       } catch (err) {
         throw handleError(err, req);
       }
--- x-pack/platform/plugins/private/monitoring/server/routes/api/v1/cluster/clusters.ts
+++ x-pack/platform/plugins/private/monitoring/server/routes/api/v1/cluster/clusters.ts
@@ -15,11 +15,6 @@ import { verifyMonitoringAuth } from '../../../../lib/elasticsearch/verify_monit
 import { handleError } from '../../../../lib/errors';
 import type { MonitoringCore } from '../../../../types';
 
-
-const mockClusters = [
-  {"cluster_uuid":"9HiBinviQNalkR2Cauq5iw","cluster_name":"My deployment (22b5a7)","version":"9.1.4","license":{"status":"active","type":"enterprise","expiry_date_in_millis":1835481599999},"elasticsearch":{"cluster_stats":{"indices":{"count":51,"docs":{"count":4360},"shards":{"total":102,"primaries":51},"store":{"size_in_bytes":17803692}},"nodes":{"fs":{"total_in_bytes":493921239040,"available_in_bytes":493885562880},"count":{"total":3},"jvm":{"max_uptime_in_millis":478755,"mem":{"heap_max_in_bytes":8640266240,"heap_used_in_bytes":1984305288}}},"status":"green"},"logs":{"enabled":true,"types":[{"type":"server","levels":[{"level":"info","count":89},{"level":"warn","count":52}]}]}},"logstash":{},"kibana":{"status":null,"some_status_is_stale":true,"requests_total":0,"concurrent_connections":0,"response_time_max":0,"memory_size":0,"memory_limit":0,"count":0,"rules":{"cluster":null,"instance":null}},"ml":{"jobs":0},"beats":{"totalEvents":0,"bytesSent":0,"beats":{"total":0,"types":[]}},"apm":{"totalEvents":0,"memRss":53977088,"apms":{"total":1},"versions":["9.1.4"],"config":{"container":true}},"enterpriseSearch":{"clusterUuid":"9HiBinviQNalkR2Cauq5iw","stats":{"appSearchEngines":0,"workplaceSearchOrgSources":0,"workplaceSearchPrivateSources":0,"totalInstances":0,"uptime":0,"memUsed":0,"memCommitted":0,"memTotal":0,"versions":[]}},"isPrimary":false,"status":"green","isCcrEnabled":true}
-];
-
 export function clustersRoute(server: MonitoringCore) {
   /*
    * Monitoring Home
@@ -49,14 +44,12 @@ export function clustersRoute(server: MonitoringCore) {
       // an error when current logged-in user doesn't have permission to read
       // the monitoring data. `try/catch` makes it a little more explicit.
       try {
-        return mockClusters;
-        // await verifyMonitoringAuth(req);
+        await verifyMonitoringAuth(req);
 
-        // const clusters = await getClustersFromRequest(req, {
-          // codePaths: req.payload.codePaths,
-        // });
-        // return postClustersResponsePayloadRT.encode(clusters);
-
+        const clusters = await getClustersFromRequest(req, {
+          codePaths: req.payload.codePaths,
+        });
+        return postClustersResponsePayloadRT.encode(clusters);
       } catch (err) {
         throw handleError(err, req);
       }
