20d28c948aebd8bae434b9c49acaf83359fd85ea
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-09-25 21:26:01 +0200

    Fix conflict resolve gone wrong

--- src/core/packages/saved-objects/api-server-mocks/src/repository.mock.ts
+++ src/core/packages/saved-objects/api-server-mocks/src/repository.mock.ts
@@ -37,7 +37,7 @@ const create = () => {
     asScopedToNamespace: jest.fn().mockImplementation(create),
     changeOwnership: jest.fn(),
     changeAccessMode: jest.fn(),
-  };
+  });
 
   mock.createPointInTimeFinder = savedObjectsPointInTimeFinderMock.create({
     savedObjectsMock: mock,
