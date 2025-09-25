70236a3f5228d65e8147037b849a33ab07c3d3e5
Alexi Doak <109488926+doakalexi@users.noreply.github.com>
2025-09-24 17:42:52 -0700

    [ResponseOps] ES|QL queries that return too many fields/columns blocks the event loop (#234689)
    Resolves https://github.com/elastic/kibana/issues/227724
    
    ## Summary
    
    In this PR I implemented a couple changes to improve the performance of
    our ES|QL query data processing.
    
    Changes to help improve performance:
    - Changed `reduce` to a `for` loop
    - Added an `isPreview` flag to prevent some of the processing that isn't
    necessary for rule executions and is only used in the UI
    - Removed object spreads and replaced them with direct assignments
    - Where it was possible, tried to combine processing into one for loop
    instead of doing multiple subsequent loops
    
    I also implemented a larger change to chunk the data and use
    `setImmediate` to help prevent event loop blocks. The chunk size is
    currently 100, but we can change that if needed.
    
    ### Checklist
    
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ### To verify
    1. Make sure that the ES|QL rule works as expected and that we didn't
    break anything
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/plugins/shared/stack_alerts/public/rule_types/es_query/expression/esql_query_expression.tsx
+++ x-pack/platform/plugins/shared/stack_alerts/public/rule_types/es_query/expression/esql_query_expression.tsx
@@ -170,10 +170,11 @@ export const EsqlQueryExpression: React.FC<
     );
     if (table) {
       const esqlTable = transformDatatableToEsqlTable(table);
-      const { results, duplicateAlertIds, longAlertIds, rows, cols } = getEsqlQueryHits(
+      const { results, duplicateAlertIds, longAlertIds, rows, cols } = await getEsqlQueryHits(
         esqlTable,
         esqlQuery.esql,
-        isGroupAgg
+        isGroupAgg,
+        true
       );
       const warning = getWarning(duplicateAlertIds, longAlertIds);
 
