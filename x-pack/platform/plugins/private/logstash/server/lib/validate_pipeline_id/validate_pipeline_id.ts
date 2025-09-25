5943c8bbdb275735a660e397e5f90c9543c3e8fc
Mashhur <mashhur.sattorov@elastic.co>
2025-09-25 14:19:23 -0700

    Apply Oxford comma.

--- x-pack/platform/plugins/private/logstash/server/lib/validate_pipeline_id/validate_pipeline_id.ts
+++ x-pack/platform/plugins/private/logstash/server/lib/validate_pipeline_id/validate_pipeline_id.ts
@@ -9,7 +9,7 @@ import { i18n } from '@kbn/i18n';
 
 /**
  * Validates a pipeline ID in Kibana API level.
- * Pipeline ID must begin with a letter or underscore and contain only letters, underscores, dashes, hyphens and numbers
+ * Pipeline ID must begin with a letter or underscore and contain only letters, underscores, dashes, hyphens, and numbers
  * Note that, frontend already validates the pipeline ID on UI.
  */
 export function validatePipelineId(value: string): string | undefined {
@@ -17,7 +17,7 @@ export function validatePipelineId(value: string): string | undefined {
   if (!pipelineIdPattern.test(value)) {
     return i18n.translate('xpack.logstash.invalidPipelineIdErrorMessage', {
       defaultMessage:
-        'Pipeline ID must begin with a letter or underscore and contain only letters, underscores, dashes, hyphens and numbers',
+        'Pipeline ID must begin with a letter or underscore and contain only letters, underscores, dashes, hyphens, and numbers',
     });
   }
   return undefined;
