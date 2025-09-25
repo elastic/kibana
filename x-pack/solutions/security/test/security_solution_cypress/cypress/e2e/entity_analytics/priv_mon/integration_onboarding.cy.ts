4774cd0100a3ade51c58ae1995aa4974d3cd3f54
Pablo Machado <pablo.nevesmachado@elastic.co>
2025-09-25 09:21:43 +0200

    [SecuritySolution] Add Privmon integrations onboarding (#235359)
    ## Summary
    
    Add integration cards for Privmon onboarding page.
    
    
    https://github.com/user-attachments/assets/59213ff9-c92a-45ba-85b3-50e6627eede8
    
    
    ### Fleet changes
    I had to update the fleet app because an error occurred when trying to
    install pre-release integrations.
    Passing the prerelease query param forward to the install package page
    fixed the problem.
    
    
    ### UI Changes
    * UX designer suggested several small improvements to the page, which
    were implemented in this PR
    
    
    
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [x] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    ### Identify risks
    
    Does this PR introduce any risks? For example, consider risks like hard
    to test bugs, performance regression, potential of data loss.
    
    Describe the risk, its severity, and mitigation for each identified
    risk. Invite stakeholders and evaluate how to proceed before merging.
    
    - [ ] [See some risk
    examples](https://github.com/elastic/kibana/blob/main/RISK_MATRIX.mdx)
    - [ ] ...

--- x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/entity_analytics/priv_mon/integration_onboarding.cy.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/entity_analytics/priv_mon/integration_onboarding.cy.ts
@@ -4,41 +4,53 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { ONBOARDING_PANEL } from '../../../screens/privileged_user_monitoring';
-import { togglePrivilegedUserMonitoring } from '../../../tasks/entity_analytics/privmon';
+
+import { ONBOARDING_CALLOUT } from '../../../screens/privileged_user_monitoring';
+import { cleanFleet } from '../../../tasks/api_calls/fleet';
+import {
+  clickOktaCard,
+  togglePrivilegedUserMonitoring,
+} from '../../../tasks/entity_analytics/privmon';
+import { installIntegration } from '../../../tasks/integrations';
 import { login } from '../../../tasks/login';
 import { visit } from '../../../tasks/navigation';
+import { deletePrivMonEngine } from '../../../tasks/privileged_user_monitoring';
 import { ENTITY_ANALYTICS_PRIVILEGED_USER_MONITORING_URL } from '../../../urls/navigation';
 
 describe(
-  'Privileged User Monitoring - Page',
+  'Privileged User Monitoring - Integrations onboarding',
   {
     tags: ['@ess'],
   },
   () => {
     before(() => {
       cy.task('esArchiverLoad', { archiveName: 'linux_process' });
+      deletePrivMonEngine(); // Just in case another test left it behind
     });
 
     beforeEach(() => {
       login();
-
       togglePrivilegedUserMonitoring();
     });
 
     afterEach(() => {
       togglePrivilegedUserMonitoring();
+      deletePrivMonEngine();
+      cleanFleet();
     });
 
     after(() => {
       cy.task('esArchiverUnload', { archiveName: 'linux_process' });
-      togglePrivilegedUserMonitoring();
     });
 
-    it('renders page as expected', () => {
+    it('should install okta integration and display the dashboard when opening privileged user monitoring page', () => {
       visit(ENTITY_ANALYTICS_PRIVILEGED_USER_MONITORING_URL);
 
-      cy.get(ONBOARDING_PANEL).should('contain.text', 'Privileged user monitoring');
+      clickOktaCard();
+      installIntegration();
+
+      visit(ENTITY_ANALYTICS_PRIVILEGED_USER_MONITORING_URL);
+      cy.get(ONBOARDING_CALLOUT).should('contain.text', 'Privileged user monitoring set up');
     });
   }
 );
