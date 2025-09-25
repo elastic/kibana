e7aec23c076650bcf317e84cfdcc1d524882a810
Coen Warmer <coen.warmer@gmail.com>
2025-09-25 17:33:34 +0200

    Update script

--- src/platform/packages/shared/kbn-test/src/mocha/junit_report_generation.test.js
+++ src/platform/packages/shared/kbn-test/src/mocha/junit_report_generation.test.js
@@ -55,7 +55,9 @@ describe('dev/mocha/junit report generation', () => {
     const [testsuite] = report.testsuites.testsuite;
     expect(testsuite.$.time).toMatch(DURATION_REGEX);
     expect(testsuite.$.timestamp).toMatch(ISO_DATE_SEC_REGEX);
-    const expectedCommandLine = 'node node_modules/jest-worker/build/workers/processChild.js';
+    const expectedCommandLine = process.env.CI
+      ? 'node scripts/jest --config src/platform/packages/shared/kbn-test/jest.config.js --runInBand --coverage=false --passWithNoTests'
+      : 'node node_modules/jest-worker/build/workers/processChild.js';
 
     expect(testsuite.$).toMatchObject({
       'command-line': expectedCommandLine,
