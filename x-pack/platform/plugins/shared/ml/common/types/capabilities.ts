a19422f3e486ee7abdc16c7b107a50a15b0c3cf5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-09-26 14:10:12 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/platform/plugins/shared/ml/common/types/capabilities.ts
+++ x-pack/platform/plugins/shared/ml/common/types/capabilities.ts
@@ -6,7 +6,7 @@
  */
 
 import type { KibanaRequest } from '@kbn/core/server';
-import { AlertConsumers, DEPRECATED_ALERTING_CONSUMERS } from '@kbn/rule-data-utils';
+import { AlertConsumers } from '@kbn/rule-data-utils';
 import { ALERTING_FEATURE_ID } from '@kbn/alerting-plugin/common';
 import { PLUGIN_ID } from '../constants/app';
 import {
