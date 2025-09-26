2782f93983be589708849d22e583b34b97cdbcd0
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-09-26 21:48:13 +0200

    fix playwright.config.ts

--- src/platform/packages/shared/kbn-scout/test/scout/playwright.config.ts
+++ src/platform/packages/shared/kbn-scout/test/scout/playwright.config.ts
@@ -7,16 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-<<<<<<<< HEAD:src/core/packages/fatal-errors/browser-internal/src/fatal_errors_service.test.mocks.ts
-export const mockRender = jest.fn();
-jest.mock('react-dom', () => {
-  return {
-    render: mockRender,
-  };
-========
 import { createPlaywrightConfig } from '../..';
 
 export default createPlaywrightConfig({
   testDir: './tests',
->>>>>>>> 0390bf59a8b ([scout] update failed test reporter to support package (#235612)):src/platform/packages/shared/kbn-scout/test/scout/playwright.config.ts
 });
