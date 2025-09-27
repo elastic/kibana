edfe13738acdb6e5a3d25ee9d84fd62752877955
Christos Nasikas <christos.nasikas@elastic.co>
2025-09-27 13:05:31 +0300

    Prevent disabling internal rule types

--- x-pack/platform/plugins/shared/alerting/server/routes/rule/apis/disable/disable_rule_route.ts
+++ x-pack/platform/plugins/shared/alerting/server/routes/rule/apis/disable/disable_rule_route.ts
@@ -57,7 +57,10 @@ export const disableRuleRoute = (
     },
     router.handleLegacyErrors(
       verifyAccessAndContext(licenseState, async function (context, req, res) {
-        const rulesClient = await (await context.alerting).getRulesClient();
+        const alertingContext = await context.alerting;
+        const rulesClient = await alertingContext.getRulesClient();
+        const ruleTypes = alertingContext.listTypes();
+
         const { id }: DisableRuleRequestParamsV1 = req.params;
         const body: DisableRuleRequestBodyV1 = req.body || {};
         const { untrack = false } = body;
@@ -65,6 +68,22 @@ export const disableRuleRoute = (
         const disableParams = { id, untrack };
 
         try {
+          const rule = await rulesClient.get({ id });
+          const ruleType = ruleTypes.get(rule.alertTypeId);
+
+          /**
+           * Throws a bad request (400) if the rule type is internallyManaged
+           * ruleType will always exist here because ruleTypes.get will throw a 400
+           * error if the rule type is not registered.
+           */
+          if (ruleType?.internallyManaged) {
+            return res.badRequest({
+              body: {
+                message: `Cannot disable rule of type "${rule.alertTypeId}" because it is internally managed.`,
+              },
+            });
+          }
+
           await rulesClient.disableRule(disableParams);
           return res.noContent();
         } catch (e) {
