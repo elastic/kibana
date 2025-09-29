b0250cf8a19034b95795856d222beed600068cb4
Jan Monschke <jan.monschke@elastic.co>
2025-09-29 21:38:04 +0200

    use inner form for state handling

--- x-pack/platform/plugins/shared/cases/public/components/connectors/resilient/translations.ts
+++ x-pack/platform/plugins/shared/cases/public/components/connectors/resilient/translations.ts
@@ -52,3 +52,17 @@ export const ADDITIONAL_FIELDS_LABEL = i18n.translate(
     defaultMessage: 'Additional Fields',
   }
 );
+
+export const ADDITIONAL_FIELDS_PLACEHOLDER = i18n.translate(
+  'xpack.cases.connectors.resilient.additionalFieldsPlaceholder',
+  {
+    defaultMessage: 'Choose IBM Resilient fields',
+  }
+);
+
+export const ADDITIONAL_FIELDS_HELP_TEXT = i18n.translate(
+  'xpack.cases.connectors.resilient.additionalFieldsHelpText',
+  {
+    defaultMessage: 'Add additional IBM Resilient fields.',
+  }
+);
