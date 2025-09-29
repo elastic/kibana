32f1ffc59deb5e45e4084c6d6d3b680d9398f9ff
Omri Rosner <omri.rosner@elastic.co>
2025-09-29 14:57:32 +0300

    support liquid only - remove nuncks

--- src/platform/plugins/shared/workflows_execution_engine/server/templating_engine.ts
+++ src/platform/plugins/shared/workflows_execution_engine/server/templating_engine.ts
@@ -7,62 +7,19 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import nunjucks from 'nunjucks';
 import { Liquid } from 'liquidjs';
 
-
-export type TemplateEngineType = 'nunjucks' | 'liquid';
-
 export class WorkflowTemplatingEngine {
-  private readonly engineType: TemplateEngineType;
+  private readonly engine: Liquid;
 
-  constructor(engineType: TemplateEngineType = 'liquid') {
-    this.engineType = engineType;
-  }
-
-  public render(template: string, context: Record<string, any>): string {
-    switch (this.engineType) {
-      case 'nunjucks':
-        return this.renderNunjucks(template, context);
-      case 'liquid':
-        return this.renderLiquid(template, context);
-      default:
-        throw new Error(`Unsupported template engine: ${this.engineType}`);
-    }
-  }
-
-  private renderNunjucks(template: string, context: Record<string, any>): string {
-    const env = nunjucks.configure({
-      autoescape: true,
-    });
-
-    // We can add custom functions to the Nunjucks environment here.
-    // In theory, this could be same as `keep.` functions
-    env.addGlobal('now', function (format: string = 'iso') {
-      const date = new Date();
-      if (format === 'iso') return date.toISOString();
-      if (format === 'locale') return date.toLocaleString();
-      return date;
-    });
-
-    env.addFilter('json', function (value, spaces) {
-      if (value instanceof nunjucks.runtime.SafeString) {
-        value = value.toString();
-      }
-      const jsonString = JSON.stringify(value, null, spaces)?.replace(/</g, '\\u003c');
-      return new nunjucks.runtime.SafeString(jsonString);
-    });
-
-    return env.renderString(template, context);
-  }
-
-  private renderLiquid(template: string, context: Record<string, any>): string {
-    const engine = new Liquid({
+  constructor() {
+    this.engine = new Liquid({
       strictFilters: true,
       strictVariables: false,
     });
-
-    return engine.parseAndRenderSync(template, context);
   }
 
+  public render(template: string, context: Record<string, any>): string {
+    return this.engine.parseAndRenderSync(template, context);
+  }
 }
