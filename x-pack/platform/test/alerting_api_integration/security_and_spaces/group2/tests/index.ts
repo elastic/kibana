a45b6d61042a065f2b4ed64dc469dae69aa68101
Ying <ying.mao@elastic.co>
2025-09-29 17:40:23 -0400

    Cleanup

--- x-pack/platform/test/alerting_api_integration/security_and_spaces/group2/tests/index.ts
+++ x-pack/platform/test/alerting_api_integration/security_and_spaces/group2/tests/index.ts
@@ -10,7 +10,7 @@ import type { FtrProviderContext } from '../../../../common/ftr_provider_context
 export default function alertingApiIntegrationTests({ loadTestFile }: FtrProviderContext) {
   describe('alerting api integration security and spaces enabled - Group 2', function () {
     loadTestFile(require.resolve('./telemetry'));
-    // loadTestFile(require.resolve('./actions'));
-    // loadTestFile(require.resolve('./alerting'));
+    loadTestFile(require.resolve('./actions'));
+    loadTestFile(require.resolve('./alerting'));
   });
 }
