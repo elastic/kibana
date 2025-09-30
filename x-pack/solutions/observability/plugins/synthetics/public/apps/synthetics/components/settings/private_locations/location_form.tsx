be36e81b2735ec832c398a8d2c59ac2dc3a98382
Francesco Fagnani <francesco.fagnani@elastic.co>
2025-09-30 12:00:35 +0200

    fix(location help text): slightly modified message

--- x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/settings/private_locations/location_form.tsx
+++ x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/settings/private_locations/location_form.tsx
@@ -110,7 +110,7 @@ const LOCATION_HELP_TEXT = i18n.translate(
   'xpack.synthetics.privateLocation.locationSpacesHelpText',
   {
     defaultMessage:
-      'Available spaces are filtered based on the selected agent policy. You can select from the spaces that the agent policy has access to. If no spaces is selected it will automatically use agent policy spaces.',
+      'Available spaces are filtered based on the selected agent policy. You can select from the spaces that the agent policy has access to. If no space is selected, the private location will automatically use all spaces that the agent policy has access to.',
   }
 );
 
