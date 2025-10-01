elastic/kibana

commit 0cfc6f272baf33715d917c2fe2e03ba6e5e1903a

[Streams][Data Quality] Improve date picker (#236776)
Closes https://github.com/elastic/kibana/issues/236149

## Summary

This PR replaces the date picker in Data quality with the one from
`unifiedSearch` so that the Data quality tab in Streams is consistent
with the time picker in the Streams Main page.




https://github.com/user-attachments/assets/93239698-268e-4ebc-aee6-6fb6069844f8

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality/filters/filters.tsx
@@ -5,11 +5,11 @@
  * 2.0.
  */
 
-import { EuiFilterGroup, EuiFlexGroup, EuiFlexItem, EuiSuperDatePicker } from '@elastic/eui';
+import { EuiFilterGroup, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
 import { useDatasetQualityFilters } from '../../../hooks/use_dataset_quality_filters';
-import { useQuickTimeRanges } from '../../../utils/use_quick_time_ranges';
+import { useKibanaContextForPlugin } from '../../../utils';
 import { FilterBar } from './filter_bar';
 import { IntegrationsSelector } from './integrations_selector';
 import { NamespacesSelector } from './namespaces_selector';
@@ -57,7 +57,7 @@ export default function Filters() {
     isDatasetQualityAllSignalsAvailable,
   } = useDatasetQualityFilters();
 
-  const commonlyUsedRanges = useQuickTimeRanges();
+  const { unifiedSearch } = useKibanaContextForPlugin().services;
 
   return (
     <EuiFlexGroup data-test-subj="datasetQualityFiltersContainer" gutterSize="s" wrap>
@@ -93,16 +93,27 @@ export default function Filters() {
         />
       </EuiFilterGroup>
       <EuiFlexItem grow={false}>
-        <EuiSuperDatePicker
-          start={timeRange.from}
-          end={timeRange.to}
-          onTimeChange={onTimeChange}
+        <unifiedSearch.ui.SearchBar
+          appName="datasetQuality"
+          showDatePicker={true}
+          showFilterBar={false}
+          showQueryMenu={false}
+          showQueryInput={false}
+          submitButtonStyle="iconOnly"
+          displayStyle="inPage"
+          disableQueryLanguageSwitcher
+          query={undefined}
+          dateRangeFrom={timeRange.from}
+          dateRangeTo={timeRange.to}
+          onQuerySubmit={(payload) =>
+            onTimeChange({ start: payload.dateRange.from, end: payload.dateRange.to })
+          }
           onRefresh={onRefresh}
-          onRefreshChange={onRefreshChange}
-          commonlyUsedRanges={commonlyUsedRanges}
-          showUpdateButton={true}
-          isPaused={timeRange.refresh.pause}
+          onRefreshChange={({ refreshInterval, isPaused }) =>
+            onRefreshChange({ refreshInterval, isPaused })
+          }
           refreshInterval={timeRange.refresh.value}
+          isRefreshPaused={timeRange.refresh.pause}
         />
       </EuiFlexItem>
     </EuiFlexGroup>

x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality_details/overview/header.tsx
@@ -5,11 +5,12 @@
  * 2.0.
  */
 
-import type { OnRefreshProps, OnTimeChangeProps } from '@elastic/eui';
-import { EuiFlexGroup, EuiSuperDatePicker, EuiTitle } from '@elastic/eui';
+import { EuiFlexGroup, EuiTitle } from '@elastic/eui';
 import { css } from '@emotion/react';
 import React, { useCallback } from 'react';
+import type { TimeRange } from '@kbn/es-query';
 import { useDatasetQualityDetailsState } from '../../../hooks';
+import { useKibanaContextForPlugin } from '../../../utils/use_kibana';
 import { overviewHeaderTitle } from '../../../../common/translations';
 
 // Allow for lazy loading
@@ -17,17 +18,16 @@ import { overviewHeaderTitle } from '../../../../common/translations';
 export default function OverviewHeader({
   handleRefresh,
 }: {
-  handleRefresh: (refreshProps: OnRefreshProps) => void;
+  handleRefresh: (dateRange: TimeRange) => void;
 }) {
-  const { timeRange, updateTimeRange } = useDatasetQualityDetailsState();
+  const { timeRange, updateTimeRange, onRefreshChange } = useDatasetQualityDetailsState();
+  const { unifiedSearch } = useKibanaContextForPlugin().services;
 
   const onTimeChange = useCallback(
-    ({ isInvalid, ...timeRangeProps }: OnTimeChangeProps) => {
-      if (!isInvalid) {
-        updateTimeRange({ refreshInterval: timeRange.refresh.value, ...timeRangeProps });
-      }
+    ({ ...timeRangeProps }: { start: string; end: string }) => {
+      updateTimeRange({ ...timeRangeProps });
     },
-    [updateTimeRange, timeRange.refresh]
+    [updateTimeRange]
   );
 
   return (
@@ -50,17 +50,27 @@ export default function OverviewHeader({
           flex-grow: 0;
         `}
       >
-        <EuiSuperDatePicker
-          width="auto"
-          compressed={true}
-          isLoading={false}
-          start={timeRange.from}
-          end={timeRange.to}
-          onTimeChange={onTimeChange}
-          onRefresh={handleRefresh}
-          isQuickSelectOnly={false}
-          showUpdateButton="iconOnly"
-          updateButtonProps={{ fill: false }}
+        <unifiedSearch.ui.SearchBar
+          appName="datasetQualityDetails"
+          showDatePicker={true}
+          showFilterBar={false}
+          showQueryMenu={false}
+          showQueryInput={false}
+          submitButtonStyle="iconOnly"
+          displayStyle="inPage"
+          disableQueryLanguageSwitcher
+          query={undefined}
+          dateRangeFrom={timeRange.from}
+          dateRangeTo={timeRange.to}
+          onQuerySubmit={(payload) =>
+            onTimeChange({ start: payload.dateRange.from, end: payload.dateRange.to })
+          }
+          onRefresh={(payload) => handleRefresh(payload.dateRange)}
+          onRefreshChange={({ refreshInterval, isPaused }) =>
+            onRefreshChange({ refreshInterval, isPaused })
+          }
+          refreshInterval={timeRange.refresh.value}
+          isRefreshPaused={timeRange.refresh.pause}
         />
       </EuiFlexGroup>
     </EuiFlexGroup>

x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality_details/overview/index.tsx
@@ -7,7 +7,7 @@
 
 import React, { useCallback, useState } from 'react';
 import { dynamic } from '@kbn/shared-ux-utility';
-import type { OnRefreshProps } from '@elastic/eui';
+import type { TimeRange } from '@kbn/es-query';
 import {
   EuiFlexGroup,
   EuiFlexItem,
@@ -71,8 +71,8 @@ export function Overview({ openAlertFlyout }: { openAlertFlyout: () => void }) {
     React.useState<QualityIssueType>('degraded');
 
   const handleRefresh = useCallback(
-    (refreshProps: OnRefreshProps) => {
-      updateTimeRange(refreshProps);
+    (refreshProps: TimeRange) => {
+      updateTimeRange({ start: refreshProps.from, end: refreshProps.to });
       setLastReloadTime(Date.now());
     },
     [updateTimeRange]

x-pack/platform/plugins/shared/dataset_quality/public/hooks/use_dataset_quality_details_state.ts
@@ -7,8 +7,7 @@
 
 import { useCallback } from 'react';
 import { useSelector } from '@xstate/react';
-import type { OnRefreshProps } from '@elastic/eui';
-import { DEFAULT_DATEPICKER_REFRESH } from '../../common/constants';
+import type { OnRefreshChangeProps } from '@elastic/eui';
 import { useDatasetQualityDetailsContext } from '../components/dataset_quality_details/context';
 import { indexNameToDataStreamParts } from '../../common/utils';
 import type { BasicDataStream } from '../../common/types';
@@ -152,18 +151,34 @@ export const useDatasetQualityDetailsState = () => {
     state.matches('initializing.qualityIssueFlyout.open')
   );
 
-  const updateTimeRange = useCallback(
-    ({ start, end, refreshInterval }: OnRefreshProps) => {
+  const onRefreshChange = useCallback(
+    ({ refreshInterval, isPaused }: Pick<OnRefreshChangeProps, 'refreshInterval' | 'isPaused'>) => {
       service.send({
         type: 'UPDATE_TIME_RANGE',
         timeRange: {
-          from: start,
-          to: end,
-          refresh: { ...DEFAULT_DATEPICKER_REFRESH, value: refreshInterval },
+          ...timeRange,
+          refresh: {
+            pause: isPaused,
+            value: refreshInterval,
+          },
         },
       });
     },
-    [service]
+    [service, timeRange]
+  );
+
+  const updateTimeRange = useCallback(
+    ({ start, end }: { start: string; end: string }) => {
+      service.send({
+        type: 'UPDATE_TIME_RANGE',
+        timeRange: {
+          ...timeRange,
+          from: start,
+          to: end,
+        },
+      });
+    },
+    [service, timeRange]
   );
 
   const updateFailureStore = useCallback(
@@ -208,6 +223,7 @@ export const useDatasetQualityDetailsState = () => {
     timeRange,
     loadingState,
     updateTimeRange,
+    onRefreshChange,
     updateFailureStore,
     dataStreamSettings,
     integrationDetails,

x-pack/platform/plugins/shared/streams_app/public/hooks/use_dataset_quality_controller.ts
@@ -12,7 +12,6 @@ import type {
   DatasetQualityDetailsController,
   DatasetQualityView,
 } from '@kbn/dataset-quality-plugin/public/controller/dataset_quality_details';
-import { DEFAULT_DATEPICKER_REFRESH } from '@kbn/dataset-quality-plugin/common';
 import {
   getDatasetQualityDetailsStateFromUrl,
   updateUrlFromDatasetQualityDetailsState,
@@ -35,7 +34,7 @@ export const useDatasetQualityController = (
   const urlStateStorageContainer = useKbnUrlStateStorageFromRouterContext();
 
   const history = useHistory();
-  const { timeState, setTime } = useTimefilter();
+  const { timeState, setTime, refreshInterval, setRefreshInterval } = useTimefilter();
 
   useEffect(() => {
     async function getDatasetQualityDetailsController() {
@@ -59,7 +58,7 @@ export const useDatasetQualityController = (
           timeRange: {
             from: timeState.timeRange.from,
             to: timeState.timeRange.to,
-            refresh: DEFAULT_DATEPICKER_REFRESH,
+            refresh: refreshInterval,
           },
         };
       }
@@ -89,6 +88,7 @@ export const useDatasetQualityController = (
             urlStateStorageContainer,
             datasetQualityDetailsState: state,
             setTime,
+            setRefreshInterval,
           });
         }
       );
@@ -111,6 +111,8 @@ export const useDatasetQualityController = (
     timeState.timeRange.from,
     timeState.timeRange.to,
     setTime,
+    refreshInterval,
+    setRefreshInterval,
   ]);
 
   return controller;

x-pack/platform/plugins/shared/streams_app/public/hooks/use_timefilter.tsx
@@ -5,10 +5,14 @@
  * 2.0.
  */
 
-import type { TimefilterContract, TimefilterHook } from '@kbn/data-plugin/public';
+import type { TimefilterContract, TimefilterHook, RefreshInterval } from '@kbn/data-plugin/public';
 import { useKibana } from './use_kibana';
 
-export function useTimefilter(): TimefilterHook & { setTime: TimefilterContract['setTime'] } {
+export function useTimefilter(): TimefilterHook & {
+  setTime: TimefilterContract['setTime'];
+  readonly refreshInterval: RefreshInterval;
+  setRefreshInterval: TimefilterContract['setRefreshInterval'];
+} {
   const {
     dependencies: {
       start: {
@@ -18,9 +22,12 @@ export function useTimefilter(): TimefilterHook & { setTime: TimefilterContract[
   } = useKibana();
 
   const result = query.timefilter.timefilter.useTimefilter();
+  const refreshInterval = query.timefilter.timefilter.getRefreshInterval();
 
   return {
     ...result,
     setTime: query.timefilter.timefilter.setTime,
+    refreshInterval,
+    setRefreshInterval: query.timefilter.timefilter.setRefreshInterval,
   };
 }

x-pack/platform/plugins/shared/streams_app/public/util/url_state_storage_service.ts
@@ -21,10 +21,12 @@ export const updateUrlFromDatasetQualityDetailsState = ({
   urlStateStorageContainer,
   datasetQualityDetailsState,
   setTime,
+  setRefreshInterval,
 }: {
   urlStateStorageContainer: IKbnUrlStateStorage;
   datasetQualityDetailsState?: DatasetQualityDetailsPublicState;
   setTime: (time: { from: string; to: string }) => void;
+  setRefreshInterval: ({ value, pause }: { value: number; pause: boolean }) => void;
 }) => {
   if (!datasetQualityDetailsState) {
     return;
@@ -42,6 +44,9 @@ export const updateUrlFromDatasetQualityDetailsState = ({
   const { timeRange } = datasetQualityDetailsState;
   if (timeRange?.from && timeRange?.to) {
     setTime({ from: timeRange.from, to: timeRange.to });
+  }
+  if (timeRange.refresh) {
+    setRefreshInterval(timeRange.refresh);
   }
 };
 

x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality_details/overview/document_trends/index.tsx
@@ -62,9 +62,9 @@ export default function DocumentTrends({
 
   const onTimeRangeChange = useCallback(
     ({ start, end }: Pick<OnTimeChangeProps, 'start' | 'end'>) => {
-      updateTimeRange({ start, end, refreshInterval: timeRange.refresh.value });
+      updateTimeRange({ start, end });
     },
-    [updateTimeRange, timeRange.refresh]
+    [updateTimeRange]
   );
 
   const {

commit 8f39865b7a8fa62801dd02c195181e61a0c3062c

[Streams] Fix issues from date picker (#237075)
This PR fixes the broken Data quality tab due to a bug introduced in
https://github.com/elastic/kibana/pull/236776.
It removes the refresh interval in Data quality tab and Streams main
page as the refresh functionality is not implemented anyway.

---------

Co-authored-by: Joe Reuter <johannes.reuter@elastic.co>

x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality/filters/filters.tsx
@@ -8,6 +8,7 @@
 import { EuiFilterGroup, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
+import { css } from '@emotion/css';
 import { useDatasetQualityFilters } from '../../../hooks/use_dataset_quality_filters';
 import { useKibanaContextForPlugin } from '../../../utils';
 import { FilterBar } from './filter_bar';
@@ -34,6 +35,15 @@ const typesNoneMatching = i18n.translate('xpack.datasetQuality.selector.types.no
 const typesNoneAvailable = i18n.translate('xpack.datasetQuality.selector.types.noneAvailable', {
   defaultMessage: 'No types available',
 });
+
+const datePickerStyle = css`
+  .euiFormControlLayout {
+    height: 40px;
+  }
+  .euiButton {
+    height: 40px;
+  }
+`;
 
 // Allow for lazy loading
 // eslint-disable-next-line import/no-default-export
@@ -92,7 +102,7 @@ export default function Filters() {
           onQualitiesChange={onQualitiesChange}
         />
       </EuiFilterGroup>
-      <EuiFlexItem grow={false}>
+      <EuiFlexItem grow={false} className={datePickerStyle}>
         <unifiedSearch.ui.SearchBar
           appName="datasetQuality"
           showDatePicker={true}

x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality_details/overview/header.tsx
@@ -16,18 +16,18 @@ import { overviewHeaderTitle } from '../../../../common/translations';
 // Allow for lazy loading
 // eslint-disable-next-line import/no-default-export
 export default function OverviewHeader({
-  handleRefresh,
+  handleTimeChange,
 }: {
-  handleRefresh: (dateRange: TimeRange) => void;
+  handleTimeChange: (dateRange: TimeRange) => void;
 }) {
-  const { timeRange, updateTimeRange, onRefreshChange } = useDatasetQualityDetailsState();
+  const { timeRange } = useDatasetQualityDetailsState();
   const { unifiedSearch } = useKibanaContextForPlugin().services;
 
   const onTimeChange = useCallback(
-    ({ ...timeRangeProps }: { start: string; end: string }) => {
-      updateTimeRange({ ...timeRangeProps });
+    ({ ...timeRangeProps }: TimeRange) => {
+      handleTimeChange({ ...timeRangeProps });
     },
-    [updateTimeRange]
+    [handleTimeChange]
   );
 
   return (
@@ -62,15 +62,8 @@ export default function OverviewHeader({
           query={undefined}
           dateRangeFrom={timeRange.from}
           dateRangeTo={timeRange.to}
-          onQuerySubmit={(payload) =>
-            onTimeChange({ start: payload.dateRange.from, end: payload.dateRange.to })
-          }
-          onRefresh={(payload) => handleRefresh(payload.dateRange)}
-          onRefreshChange={({ refreshInterval, isPaused }) =>
-            onRefreshChange({ refreshInterval, isPaused })
-          }
-          refreshInterval={timeRange.refresh.value}
-          isRefreshPaused={timeRange.refresh.pause}
+          onQuerySubmit={(payload) => onTimeChange(payload.dateRange)}
+          isAutoRefreshDisabled={true}
         />
       </EuiFlexGroup>
     </EuiFlexGroup>

x-pack/platform/plugins/shared/dataset_quality/public/components/dataset_quality_details/overview/index.tsx
@@ -70,7 +70,7 @@ export function Overview({ openAlertFlyout }: { openAlertFlyout: () => void }) {
   const [selectedQualityCard, setSelectedQualityCard] =
     React.useState<QualityIssueType>('degraded');
 
-  const handleRefresh = useCallback(
+  const handleTimeChange = useCallback(
     (refreshProps: TimeRange) => {
       updateTimeRange({ start: refreshProps.from, end: refreshProps.to });
       setLastReloadTime(Date.now());
@@ -80,7 +80,7 @@ export function Overview({ openAlertFlyout }: { openAlertFlyout: () => void }) {
   return (
     <>
       {isNonAggregatable && <AggregationNotSupported dataStream={dataStream} />}
-      <OverviewHeader handleRefresh={handleRefresh} />
+      <OverviewHeader handleTimeChange={handleTimeChange} />
       <EuiSpacer size="m" />
 
       {!dataStreamSettingsLoading && !canUserReadFailureStore && (

x-pack/platform/plugins/shared/dataset_quality/public/hooks/use_dataset_quality_details_state.ts
@@ -7,7 +7,6 @@
 
 import { useCallback } from 'react';
 import { useSelector } from '@xstate/react';
-import type { OnRefreshChangeProps } from '@elastic/eui';
 import { useDatasetQualityDetailsContext } from '../components/dataset_quality_details/context';
 import { indexNameToDataStreamParts } from '../../common/utils';
 import type { BasicDataStream } from '../../common/types';
@@ -151,22 +150,6 @@ export const useDatasetQualityDetailsState = () => {
     state.matches('initializing.qualityIssueFlyout.open')
   );
 
-  const onRefreshChange = useCallback(
-    ({ refreshInterval, isPaused }: Pick<OnRefreshChangeProps, 'refreshInterval' | 'isPaused'>) => {
-      service.send({
-        type: 'UPDATE_TIME_RANGE',
-        timeRange: {
-          ...timeRange,
-          refresh: {
-            pause: isPaused,
-            value: refreshInterval,
-          },
-        },
-      });
-    },
-    [service, timeRange]
-  );
-
   const updateTimeRange = useCallback(
     ({ start, end }: { start: string; end: string }) => {
       service.send({
@@ -223,7 +206,6 @@ export const useDatasetQualityDetailsState = () => {
     timeRange,
     loadingState,
     updateTimeRange,
-    onRefreshChange,
     updateFailureStore,
     dataStreamSettings,
     integrationDetails,

x-pack/platform/plugins/shared/streams_app/public/components/streams_app_search_bar/uncontrolled_streams_app_bar.tsx
@@ -24,6 +24,7 @@ export function UncontrolledStreamsAppSearchBar(props: UncontrolledStreamsAppSea
       displayStyle="inPage"
       disableQueryLanguageSwitcher
       query={undefined}
+      isAutoRefreshDisabled={true}
       {...props}
     />
   );

x-pack/platform/plugins/shared/streams_app/public/hooks/use_dataset_quality_controller.ts
@@ -12,6 +12,7 @@ import type {
   DatasetQualityDetailsController,
   DatasetQualityView,
 } from '@kbn/dataset-quality-plugin/public/controller/dataset_quality_details';
+import { DEFAULT_DATEPICKER_REFRESH } from '@kbn/dataset-quality-plugin/common';
 import {
   getDatasetQualityDetailsStateFromUrl,
   updateUrlFromDatasetQualityDetailsState,
@@ -34,7 +35,7 @@ export const useDatasetQualityController = (
   const urlStateStorageContainer = useKbnUrlStateStorageFromRouterContext();
 
   const history = useHistory();
-  const { timeState, setTime, refreshInterval, setRefreshInterval } = useTimefilter();
+  const { timeState, setTime } = useTimefilter();
 
   useEffect(() => {
     async function getDatasetQualityDetailsController() {
@@ -58,7 +59,7 @@ export const useDatasetQualityController = (
           timeRange: {
             from: timeState.timeRange.from,
             to: timeState.timeRange.to,
-            refresh: refreshInterval,
+            refresh: DEFAULT_DATEPICKER_REFRESH,
           },
         };
       }
@@ -88,7 +89,6 @@ export const useDatasetQualityController = (
             urlStateStorageContainer,
             datasetQualityDetailsState: state,
             setTime,
-            setRefreshInterval,
           });
         }
       );
@@ -111,8 +111,6 @@ export const useDatasetQualityController = (
     timeState.timeRange.from,
     timeState.timeRange.to,
     setTime,
-    refreshInterval,
-    setRefreshInterval,
   ]);
 
   return controller;

x-pack/platform/plugins/shared/streams_app/public/hooks/use_timefilter.tsx
@@ -5,13 +5,11 @@
  * 2.0.
  */
 
-import type { TimefilterContract, TimefilterHook, RefreshInterval } from '@kbn/data-plugin/public';
+import type { TimefilterContract, TimefilterHook } from '@kbn/data-plugin/public';
 import { useKibana } from './use_kibana';
 
 export function useTimefilter(): TimefilterHook & {
   setTime: TimefilterContract['setTime'];
-  readonly refreshInterval: RefreshInterval;
-  setRefreshInterval: TimefilterContract['setRefreshInterval'];
 } {
   const {
     dependencies: {
@@ -22,12 +20,9 @@ export function useTimefilter(): TimefilterHook & {
   } = useKibana();
 
   const result = query.timefilter.timefilter.useTimefilter();
-  const refreshInterval = query.timefilter.timefilter.getRefreshInterval();
 
   return {
     ...result,
     setTime: query.timefilter.timefilter.setTime,
-    refreshInterval,
-    setRefreshInterval: query.timefilter.timefilter.setRefreshInterval,
   };
 }

x-pack/platform/plugins/shared/streams_app/public/util/url_state_storage_service.ts
@@ -21,12 +21,10 @@ export const updateUrlFromDatasetQualityDetailsState = ({
   urlStateStorageContainer,
   datasetQualityDetailsState,
   setTime,
-  setRefreshInterval,
 }: {
   urlStateStorageContainer: IKbnUrlStateStorage;
   datasetQualityDetailsState?: DatasetQualityDetailsPublicState;
   setTime: (time: { from: string; to: string }) => void;
-  setRefreshInterval: ({ value, pause }: { value: number; pause: boolean }) => void;
 }) => {
   if (!datasetQualityDetailsState) {
     return;
@@ -44,9 +42,6 @@ export const updateUrlFromDatasetQualityDetailsState = ({
   const { timeRange } = datasetQualityDetailsState;
   if (timeRange?.from && timeRange?.to) {
     setTime({ from: timeRange.from, to: timeRange.to });
-  }
-  if (timeRange.refresh) {
-    setRefreshInterval(timeRange.refresh);
   }
 };
 