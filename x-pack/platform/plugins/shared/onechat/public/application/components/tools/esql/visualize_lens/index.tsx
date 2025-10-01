c7f1705298f703adb7835f99a3e85c974d750aec
ppisljar <peter.pisljar@elastic.co>
2025-10-01 17:39:08 +0200

    updating to latest version

---
+++ x-pack/platform/plugins/shared/onechat/public/application/components/tools/esql/visualize_lens/index.tsx
@@ -0,0 +1,91 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiLoadingSpinner, useEuiTheme } from '@elastic/eui';
+import type { DataViewsServicePublic } from '@kbn/data-views-plugin/public/types';
+import type { InlineEditLensEmbeddableContext, LensPublicStart } from '@kbn/lens-plugin/public';
+import React, { useCallback, useState } from 'react';
+import type { UiActionsStart } from '@kbn/ui-actions-plugin/public';
+import { visualizationWrapper } from './styles';
+import { useLensInput } from './use_lens_input';
+import { VisualizationActions } from './visualization_actions';
+
+const VISUALIZATION_HEIGHT = 240;
+
+export function VisualizeLens({
+  lens,
+  dataViews,
+  uiActions,
+  lensConfig,
+}: {
+  lens: LensPublicStart;
+  dataViews: DataViewsServicePublic;
+  uiActions: UiActionsStart;
+  lensConfig: any;
+}) {
+  const [isSaveModalOpen, setIsSaveModalOpen] = useState(false);
+  const [lensLoadEvent, setLensLoadEvent] = useState<
+    InlineEditLensEmbeddableContext['lensEvent'] | null
+  >(null);
+
+  const { lensInput, setLensInput, isLoading } = useLensInput({
+    lens,
+    dataViews,
+    lensConfig,
+  });
+
+  const { euiTheme } = useEuiTheme();
+
+  const onLoad = useCallback(
+    (
+      _isLoading: boolean,
+      adapters: InlineEditLensEmbeddableContext['lensEvent']['adapters'] | undefined,
+      dataLoading$?: InlineEditLensEmbeddableContext['lensEvent']['dataLoading$']
+    ) => {
+      if (!_isLoading && adapters?.tables?.tables) {
+        setLensLoadEvent({ adapters, dataLoading$ });
+      }
+    },
+    []
+  );
+
+  const onOpenSave = useCallback(() => setIsSaveModalOpen(true), []);
+  const onCloseSave = useCallback(() => setIsSaveModalOpen(false), []);
+
+  return (
+    <>
+      <div
+        data-test-subj="lensVisualization"
+        css={visualizationWrapper(euiTheme, VISUALIZATION_HEIGHT)}
+      >
+        {!isLoading && lensInput && (
+          <VisualizationActions
+            onSave={onOpenSave}
+            uiActions={uiActions}
+            lensInput={lensInput}
+            lensLoadEvent={lensLoadEvent}
+            setLensInput={setLensInput}
+          />
+        )}
+        {isLoading ? (
+          <EuiLoadingSpinner />
+        ) : (
+          lensInput && (
+            <lens.EmbeddableComponent {...lensInput} style={{ height: '100%' }} onLoad={onLoad} />
+          )
+        )}
+      </div>
+      {isSaveModalOpen && lensInput && (
+        <lens.SaveModalComponent
+          initialInput={lensInput}
+          onClose={onCloseSave}
+          isSaveable={false}
+        />
+      )}
+    </>
+  );
+}
--- x-pack/platform/plugins/shared/onechat/public/application/components/tools/esql/visualize_lens/use_lens_input.ts
+++ x-pack/platform/plugins/shared/onechat/public/application/components/tools/esql/visualize_lens/use_lens_input.ts
@@ -5,23 +5,27 @@
  * 2.0.
  */
 
-import { EuiLoadingSpinner } from '@elastic/eui';
+import { useMemo, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import type { DataViewsServicePublic } from '@kbn/data-views-plugin/public/types';
 import type { LensPublicStart, TypedLensByValueInput } from '@kbn/lens-plugin/public';
-import React, { useMemo, useState } from 'react';
-import useAsync from 'react-use/lib/useAsync';
 import { LensConfigBuilder } from '@kbn/lens-embeddable-utils/config_builder';
+import useAsync from 'react-use/lib/useAsync';
 
-const VISUALIZATION_HEIGHT = 240;
-
-interface VisualizeLensProps {
-  lens: LensPublicStart;
+interface Params {
   dataViews: DataViewsServicePublic;
+  lens: LensPublicStart;
   lensConfig: any;
 }
 
-export function VisualizeLens({ lens, dataViews, lensConfig }: VisualizeLensProps) {
+interface ReturnValue {
+  lensInput: TypedLensByValueInput | undefined;
+  setLensInput: (v: TypedLensByValueInput) => void;
+  isLoading: boolean;
+  error?: Error;
+}
+
+export function useLensInput({ dataViews, lens, lensConfig }: Params): ReturnValue {
   const lensHelpersAsync = useAsync(() => {
     return lens.stateHelperApi();
   }, [lens]);
@@ -34,22 +38,17 @@ export function VisualizeLens({ lens, dataViews, lensConfig }: VisualizeLensProp
   const [lensInput, setLensInput] = useState<TypedLensByValueInput | undefined>({
     attributes: lensAttributes,
     id: uuidv4(),
+    renderMode: 'edit',
+    viewMode: 'edit',
+    hidePanelTitles: false,
   });
 
   const isLoading = !lensHelpersAsync.value || !lensInput;
 
-  return (
-    <div style={{ height: VISUALIZATION_HEIGHT }}>
-      {isLoading ? (
-        <EuiLoadingSpinner />
-      ) : (
-        <lens.EmbeddableComponent
-          {...lensInput}
-          style={{
-            height: VISUALIZATION_HEIGHT,
-          }}
-        />
-      )}
-    </div>
-  );
+  return {
+    lensInput,
+    setLensInput,
+    isLoading,
+    error: lensHelpersAsync.error || undefined,
+  };
 }
