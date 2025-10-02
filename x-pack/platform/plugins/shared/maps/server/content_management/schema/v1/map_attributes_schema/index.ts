e9f2741b30f8ade6462f6b40578b4c355b7bf544
Nathan Reese <reese.nathan@elastic.co>
2025-10-02 08:16:46 -0600

    [dashboards as code] destringify map state in REST APIs and by-value embeddable (#236319)
    PR destringifies map state in Map application CRUD API and map by-value
    embeddable.
    
    Instead of passing state with `layerListJSON`, `mapStateJSON`, and
    `uiStateJSON`, these values are destringified. In the screen shot below,
    notice how attributes does not contain any stringified values
    <img width="2042" height="1011" alt="Screenshot 2025-09-25 at 12 40
    48 PM"
    src="https://github.com/user-attachments/assets/70152a56-bfbc-4b85-b3ac-c9c4c5dd3ee8"
    />
    
    This PR does not change map state in saved objects. Saved objects still
    contain stringified values. In the screen shot below, notice how
    attributes contains stringified values
    <img width="400" alt="Screenshot 2025-09-25 at 12 44 20 PM"
    src="https://github.com/user-attachments/assets/5ce1fb14-41cd-4528-9152-5a35402d6ac6"
    />
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/plugins/shared/maps/server/content_management/schema/v1/map_attributes_schema/index.ts
+++ x-pack/platform/plugins/shared/maps/server/content_management/schema/v1/map_attributes_schema/index.ts
@@ -5,9 +5,5 @@
  * 2.0.
  */
 
-export type * from './schema/v1/layer_schemas';
-export type * from './schema/v1/style_schemas';
-export type * from './schema/v1/source_schemas';
-export type * from './schema/v1/map_attributes_schema';
-
-export { MapsStorage } from './maps_storage';
+export { mapAttributesSchema } from './map_attributes_schema';
+export type * from './types';
