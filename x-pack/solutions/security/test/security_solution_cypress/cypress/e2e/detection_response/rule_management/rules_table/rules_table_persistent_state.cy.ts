e67022a64546405297ae33eb60e9065c53273a85
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2025-10-02 05:59:23 -0500

    skip failing test suite (#237258)

--- x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/detection_response/rule_management/rules_table/rules_table_persistent_state.cy.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/detection_response/rule_management/rules_table/rules_table_persistent_state.cy.ts
@@ -102,6 +102,7 @@ function expectDefaultRulesTableState(): void {
 // Failing: See https://github.com/elastic/kibana/issues/237236
 // Failing: See https://github.com/elastic/kibana/issues/237217
 // Failing: See https://github.com/elastic/kibana/issues/237214
+// Failing: See https://github.com/elastic/kibana/issues/237258
 describe.skip(
   'Rules table: persistent state',
   { tags: ['@ess', '@serverless', '@skipInServerlessMKI'] },
