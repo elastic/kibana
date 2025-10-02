4e294bfeccd80e8839796893850fd9d16c922d39
Mark Hopkin <mark.hopkin@elastic.co>
2025-10-02 09:48:16 +0100

    fix tests

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/engine/initialisation_service.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/engine/initialisation_service.ts
@@ -60,11 +60,10 @@ export const createInitialisationService = (
     );
     const descriptor = await descriptorClient.init();
     dataClient.log('info', `Initialized privileged monitoring engine saved object`);
-
-    // upsert index AND integration sources
-    await InitSourceCreationService.upsertSources(monitoringIndexSourceClient);
-
     try {
+      dataClient.log('debug', 'Upserting privilege monitoring sources');
+      await InitSourceCreationService.upsertSources(monitoringIndexSourceClient);
+
       dataClient.log('debug', 'Creating privilege user monitoring event.ingested pipeline');
       await IndexService.initialisePrivmonIndex();
 
