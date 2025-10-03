05c2d3ebeac448a44c17631b3a51b05fc06fe6d0
Hannah Mudge <hannah.wright@elastic.co>
2025-10-03 14:39:46 -0600

    Remove react control example

--- examples/controls_example/public/app/app.tsx
+++ examples/controls_example/public/app/app.tsx
@@ -7,26 +7,13 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import {
-  EuiPage,
-  EuiPageBody,
-  EuiPageHeader,
-  EuiPageSection,
-  EuiPageTemplate,
-  EuiSpacer,
-  EuiTab,
-  EuiTabs,
-} from '@elastic/eui';
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
+import { EuiPage, EuiPageBody, EuiPageHeader, EuiPageSection, EuiPageTemplate } from '@elastic/eui';
 import type { AppMountParameters, CoreStart } from '@kbn/core/public';
+import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import type { ControlsExampleStartDeps } from '../plugin';
 import { ControlGroupRendererExamples } from './control_group_renderer_examples';
-import { ReactControlExample } from './react_control_example/react_control_example';
-
-const CONTROLS_AS_A_BUILDING_BLOCK = 'controls_as_a_building_block';
-const CONTROLS_REFACTOR_TEST = 'controls_refactor_test';
 
 const App = ({
   core,
@@ -34,20 +21,6 @@ const App = ({
   navigation,
   uiActions,
 }: { core: CoreStart } & ControlsExampleStartDeps) => {
-  const [selectedTabId, setSelectedTabId] = useState(CONTROLS_REFACTOR_TEST); // TODO: Make this the first tab
-
-  function onSelectedTabChanged(tabId: string) {
-    setSelectedTabId(tabId);
-  }
-
-  function renderTabContent() {
-    if (selectedTabId === CONTROLS_REFACTOR_TEST) {
-      return <ReactControlExample dataViews={data.dataViews} core={core} />;
-    }
-
-    return <ControlGroupRendererExamples data={data} navigation={navigation} />;
-  }
-
   return core.rendering.addContext(
     <KibanaContextProvider services={{ ...core, data, navigation, uiActions }}>
       <EuiPage>
@@ -57,24 +30,7 @@ const App = ({
           </EuiPageSection>
           <EuiPageTemplate.Section>
             <EuiPageSection>
-              <EuiTabs>
-                <EuiTab
-                  onClick={() => onSelectedTabChanged(CONTROLS_REFACTOR_TEST)}
-                  isSelected={CONTROLS_REFACTOR_TEST === selectedTabId}
-                >
-                  Register a new React control
-                </EuiTab>
-                <EuiTab
-                  onClick={() => onSelectedTabChanged(CONTROLS_AS_A_BUILDING_BLOCK)}
-                  isSelected={CONTROLS_AS_A_BUILDING_BLOCK === selectedTabId}
-                >
-                  Controls as a building block
-                </EuiTab>
-              </EuiTabs>
-
-              <EuiSpacer />
-
-              {renderTabContent()}
+              <ControlGroupRendererExamples data={data} navigation={navigation} />;{' '}
             </EuiPageSection>
           </EuiPageTemplate.Section>
         </EuiPageBody>
