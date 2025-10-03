20a5c01a68cf8fea7b5ca8e5ad63ccf6cdb70d60
Elena Shostak <elena.shostak@elastic.co>
2025-10-03 13:58:20 +0200

    Fixes

--- x-pack/platform/test/spaces_api_integration/deployment_agnostic/spaces_only/apis/get_all.ts
+++ x-pack/platform/test/spaces_api_integration/deployment_agnostic/spaces_only/apis/get_all.ts
@@ -5,7 +5,8 @@
  * 2.0.
  */
 
-import { SPACE_1, SPACE_2, SPACE_3, SPACES } from '../../../common/lib/spaces';
+import { createSpaces, deleteSpaces } from '../../../common/lib/space_test_utils';
+import { SPACES } from '../../../common/lib/spaces';
 import { getAllTestSuiteFactory } from '../../../common/suites/get_all.agnostic';
 import type { DeploymentAgnosticFtrProviderContext } from '../../ftr_provider_context';
 
@@ -14,24 +15,12 @@ export default function getAllSpacesTestSuite(context: DeploymentAgnosticFtrProv
   const spacesService = context.getService('spaces');
   const isServerless = context.getService('config').get('serverless');
 
-  const createSpaces = async () => {
-    await spacesService.create(SPACE_1);
-    await spacesService.create(SPACE_2);
-    await spacesService.create({ ...SPACE_3, ...(isServerless ? {} : { solution: 'es' }) });
-  };
-
-  const deleteSpaces = async () => {
-    await spacesService.delete(SPACE_1.id);
-    await spacesService.delete(SPACE_2.id);
-    await spacesService.delete(SPACE_3.id);
-  };
-
   describe('get all', () => {
     before(async () => {
-      await createSpaces();
+      await createSpaces(spacesService, isServerless);
     });
     after(async () => {
-      await deleteSpaces();
+      await deleteSpaces(spacesService);
     });
 
     [
--- x-pack/platform/test/spaces_api_integration/deployment_agnostic/spaces_only/apis/update.ts
+++ x-pack/platform/test/spaces_api_integration/deployment_agnostic/spaces_only/apis/update.ts
@@ -5,7 +5,8 @@
  * 2.0.
  */
 
-import { SPACE_1, SPACE_2, SPACE_3, SPACES } from '../../../common/lib/spaces';
+import { createSpaces, deleteSpaces } from '../../../common/lib/space_test_utils';
+import { SPACES } from '../../../common/lib/spaces';
 import { updateTestSuiteFactory } from '../../../common/suites/update.agnostic';
 import type { DeploymentAgnosticFtrProviderContext } from '../../ftr_provider_context';
 
@@ -15,23 +16,13 @@ export default function updateSpaceTestSuite(context: DeploymentAgnosticFtrProvi
   const spacesService = context.getService('spaces');
   const isServerless = context.getService('config').get('serverless');
 
-  const createSpaces = async () => {
-    await spacesService.create(SPACE_1);
-    await spacesService.create(SPACE_2);
-    await spacesService.create({ ...SPACE_3, ...(isServerless ? {} : { solution: 'es' }) });
-  };
-  const deleteSpaces = async () => {
-    await spacesService.delete(SPACE_1.id);
-    await spacesService.delete(SPACE_2.id);
-    await spacesService.delete(SPACE_3.id);
-  };
-
   describe('update', () => {
     before(async () => {
-      await createSpaces();
+      await createSpaces(spacesService, isServerless);
     });
+
     after(async () => {
-      await deleteSpaces();
+      await deleteSpaces(spacesService);
     });
 
     [
