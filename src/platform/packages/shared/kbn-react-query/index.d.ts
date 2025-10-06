54da0e5eb82bf0fcdfe006ccd866461a570f5b48
Gerard Soldevila <gerard.soldevila@elastic.co>
2025-10-06 11:09:33 +0200

    Introduce `@kbn/react-query` package

---
+++ src/platform/packages/shared/kbn-react-query/index.d.ts
@@ -0,0 +1,6 @@
+/*
+ * This file is forked from the DefinitelyTyped project (https://github.com/DefinitelyTyped/DefinitelyTyped),
+ * and may include modifications made by Elasticsearch B.V.
+ * Elasticsearch B.V. licenses this file to you under the MIT License.
+ * See `src/platform/packages/shared/kbn-react-query/LICENSE` for more information.
+ */
