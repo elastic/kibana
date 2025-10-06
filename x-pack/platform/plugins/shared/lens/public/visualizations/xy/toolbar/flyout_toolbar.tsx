b071bb6d0504462139f5b669b8821227027f70a3
Maria Iriarte <maria.iriarte@elastic.co>
2025-10-06 17:47:43 +0200

    Move xy axis settigns

--- x-pack/platform/plugins/shared/lens/public/visualizations/xy/toolbar/flyout_toolbar.tsx
+++ x-pack/platform/plugins/shared/lens/public/visualizations/xy/toolbar/flyout_toolbar.tsx
@@ -5,27 +5,237 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 
 import { EuiAccordion, EuiHorizontalRule } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
+import type { AxesSettingsConfig, AxisExtentConfig } from '@kbn/visualizations-plugin/common';
+import { ScaleType } from '@elastic/charts';
+import type { YScaleType } from '@kbn/expression-xy-plugin/common';
+
+import {
+  hasNumericHistogramDimension,
+  type AxesSettingsConfigKeys,
+} from '../../../shared_components';
 import type { ContentMap } from '../../../shared_components/flyout_toolbar';
 import { FlyoutToolbar } from '../../../shared_components/flyout_toolbar';
 import type { VisualizationToolbarProps } from '../../../types';
 import type { XYState } from '../types';
 import { XyAppearanceSettings } from './visual_options_popover/visual_options_popover';
 import { XyTitlesAndTextSettings } from './titles_and_text_popover';
+import { XyAxisSettings, popoverConfig } from './axis_settings_popover';
+import { getAxesConfiguration, getXDomain } from '../axes_configuration';
+import { getDataLayers } from '../visualization_helpers';
+import { hasBarSeries, isHorizontalChart } from '../state_helpers';
+import { axisKeyToTitleMapping, getDataBounds, hasPercentageAxis } from './toolbar';
+import { getScaleType } from '../to_expression';
 
-export const XyFlyoutToolbar: React.FC<VisualizationToolbarProps<XYState>> = (props) => {
+type Props = VisualizationToolbarProps<XYState>;
+
+export const XyFlyoutToolbar: React.FC<Props> = (props) => {
   const xyToolbarContentMap: ContentMap<XYState> = {
-    style: XystyleSettings,
+    style: XyStyleSettings,
   };
   return <FlyoutToolbar {...props} contentMap={xyToolbarContentMap} />;
 };
 
-const XystyleSettings: React.FC<VisualizationToolbarProps<XYState>> = (props) => {
+const XyStyleSettings: React.FC<Props> = (props) => {
+  const { state, setState, frame } = props;
+  const dataLayers = getDataLayers(state?.layers);
+  const shouldRotate = state?.layers.length ? isHorizontalChart(state.layers) : false;
+  const axisGroups = getAxesConfiguration(dataLayers, shouldRotate, frame.activeData);
+  const dataBounds = getDataBounds(frame.activeData, axisGroups);
+  const xDataBounds = getXDomain(dataLayers, frame.activeData);
+
+  const tickLabelsVisibilitySettings = {
+    x: state?.tickLabelsVisibilitySettings?.x ?? true,
+    yLeft: state?.tickLabelsVisibilitySettings?.yLeft ?? true,
+    yRight: state?.tickLabelsVisibilitySettings?.yRight ?? true,
+  };
+  const onTickLabelsVisibilitySettingsChange = (optionId: AxesSettingsConfigKeys): void => {
+    const newTickLabelsVisibilitySettings = {
+      ...tickLabelsVisibilitySettings,
+      ...{
+        [optionId]: !tickLabelsVisibilitySettings[optionId],
+      },
+    };
+    setState({
+      ...state,
+      tickLabelsVisibilitySettings: newTickLabelsVisibilitySettings,
+    });
+  };
+
+  const gridlinesVisibilitySettings = {
+    x: state?.gridlinesVisibilitySettings?.x ?? true,
+    yLeft: state?.gridlinesVisibilitySettings?.yLeft ?? true,
+    yRight: state?.gridlinesVisibilitySettings?.yRight ?? true,
+  };
+
+  const onGridlinesVisibilitySettingsChange = (optionId: AxesSettingsConfigKeys): void => {
+    const newGridlinesVisibilitySettings = {
+      ...gridlinesVisibilitySettings,
+      ...{
+        [optionId]: !gridlinesVisibilitySettings[optionId],
+      },
+    };
+    setState({
+      ...state,
+      gridlinesVisibilitySettings: newGridlinesVisibilitySettings,
+    });
+  };
+
+  const labelsOrientation = {
+    x: state?.labelsOrientation?.x ?? 0,
+    yLeft: state?.labelsOrientation?.yLeft ?? 0,
+    yRight: state?.labelsOrientation?.yRight ?? 0,
+  };
+
+  const onLabelsOrientationChange = (axis: AxesSettingsConfigKeys, orientation: number): void => {
+    const newLabelsOrientation = {
+      ...labelsOrientation,
+      ...{
+        [axis]: orientation,
+      },
+    };
+    setState({
+      ...state,
+      labelsOrientation: newLabelsOrientation,
+    });
+  };
+
+  const axisTitlesVisibilitySettings = useMemo(
+    () => ({
+      x: state?.axisTitlesVisibilitySettings?.x ?? true,
+      yLeft: state?.axisTitlesVisibilitySettings?.yLeft ?? true,
+      yRight: state?.axisTitlesVisibilitySettings?.yRight ?? true,
+    }),
+    [
+      state?.axisTitlesVisibilitySettings?.x,
+      state?.axisTitlesVisibilitySettings?.yLeft,
+      state?.axisTitlesVisibilitySettings?.yRight,
+    ]
+  );
+
+  const onTitleStateChange = useCallback(
+    ({ title, visible }: { title?: string; visible: boolean }, axis: keyof AxesSettingsConfig) =>
+      setState({
+        ...state,
+        [axisKeyToTitleMapping[axis]]: title,
+        axisTitlesVisibilitySettings: { ...axisTitlesVisibilitySettings, [axis]: visible },
+      }),
+    [axisTitlesVisibilitySettings, setState, state]
+  );
+
+  const isTimeVis = dataLayers.every(
+    (layer) =>
+      layer.xAccessor &&
+      getScaleType(
+        frame.datasourceLayers[layer.layerId]?.getOperationForColumnId(layer.xAccessor) ?? null,
+        ScaleType.Linear
+      ) === ScaleType.Time
+  );
+
+  // only allow changing endzone visibility if it could show up theoretically (if it's a time viz)
+  const onChangeEndzoneVisiblity = isTimeVis
+    ? (checked: boolean): void => {
+        setState({
+          ...state,
+          hideEndzones: !checked,
+        });
+      }
+    : undefined;
+
+  const onChangeCurrentTimeMarkerVisibility = isTimeVis
+    ? (checked: boolean): void => {
+        setState({
+          ...state,
+          showCurrentTimeMarker: checked,
+        });
+      }
+    : undefined;
+
+  const hasBarOrAreaOnLeftAxis = Boolean(
+    axisGroups
+      .find((group) => group.groupId === 'left')
+      ?.series?.some((series) => {
+        const seriesType = dataLayers.find((l) => l.layerId === series.layer)?.seriesType;
+        return seriesType?.includes('bar') || seriesType?.includes('area');
+      })
+  );
+
+  const setScaleWithExtentFn = useCallback(
+    (extentKey: 'yLeftExtent' | 'yRightExtent', scaleKey: 'yLeftScale' | 'yRightScale') =>
+      (extent?: AxisExtentConfig, scale?: YScaleType) => {
+        setState({
+          ...state,
+          [extentKey]: extent,
+          [scaleKey]: scale,
+        });
+      },
+    [setState, state]
+  );
+
+  const setExtentFn = useCallback(
+    (extentKey: 'xExtent' | 'yLeftExtent' | 'yRightExtent') => (extent?: AxisExtentConfig) => {
+      setState({
+        ...state,
+        [extentKey]: extent,
+      });
+    },
+    [setState, state]
+  );
+
+  const setScaleFn = useCallback(
+    (scaleKey: 'yLeftScale' | 'yRightScale') => (scale?: YScaleType) => {
+      setState({
+        ...state,
+        [scaleKey]: scale,
+      });
+    },
+    [setState, state]
+  );
+
+  const hasBarOrAreaOnRightAxis = Boolean(
+    axisGroups
+      .find((group) => group.groupId === 'right')
+      ?.series?.some((series) => {
+        const seriesType = dataLayers.find((l) => l.layerId === series.layer)?.seriesType;
+        return seriesType?.includes('bar') || seriesType?.includes('area');
+      })
+  );
+
+  const filteredBarLayers = dataLayers.filter((layer) => layer.seriesType.includes('bar'));
+  const chartHasMoreThanOneBarSeries =
+    filteredBarLayers.length > 1 ||
+    filteredBarLayers.some((layer) => layer.accessors.length > 1 || layer.splitAccessor);
+
+  const isTimeHistogramModeEnabled = dataLayers.some(
+    ({ xAccessor, layerId, seriesType, splitAccessor }) => {
+      if (!xAccessor) {
+        return false;
+      }
+      const xAccessorOp =
+        frame.datasourceLayers[layerId]?.getOperationForColumnId(xAccessor) ?? null;
+      return (
+        getScaleType(xAccessorOp, ScaleType.Linear) === ScaleType.Time &&
+        xAccessorOp?.isBucketed &&
+        (seriesType.includes('stacked') || !splitAccessor) &&
+        (seriesType.includes('stacked') ||
+          !seriesType.includes('bar') ||
+          !chartHasMoreThanOneBarSeries)
+      );
+    }
+  );
+
+  const hasNumberHistogram = dataLayers.some(({ layerId, xAccessor }) =>
+    hasNumericHistogramDimension(frame.datasourceLayers[layerId], xAccessor)
+  );
+
+  const isHorizontal = state.layers?.length ? isHorizontalChart(state.layers) : false;
+
   return (
     <>
+      {/* Appearance  */}
       <EuiAccordion
         id={''}
         buttonContent={i18n.translate('xpack.lens.visualization.toolbar.appearance', {
@@ -34,9 +244,10 @@ const XystyleSettings: React.FC<VisualizationToolbarProps<XYState>> = (props) =>
         paddingSize="s"
         initialIsOpen
       >
-        <XyAppearanceSettings datasourceLayers={props.frame.datasourceLayers} {...props} />
+        <XyAppearanceSettings datasourceLayers={frame.datasourceLayers} {...props} />
       </EuiAccordion>
       <EuiHorizontalRule margin="m" />
+      {/* Titles and text. TODO: hide when has bar series  */}
       <EuiAccordion
         id={''}
         buttonContent={i18n.translate('xpack.lens.visualization.toolbar.titlesAndText', {
@@ -44,10 +255,113 @@ const XystyleSettings: React.FC<VisualizationToolbarProps<XYState>> = (props) =>
         })}
         paddingSize="s"
         initialIsOpen
+        isDisabled={!hasBarSeries(state.layers)}
+        {...(!hasBarSeries(state.layers) && { forceState: 'closed' })}
       >
         <XyTitlesAndTextSettings datasourceLayers={props.frame.datasourceLayers} {...props} />
       </EuiAccordion>
-      {/* TODO: Add axis */}
+      <EuiHorizontalRule margin="m" />
+      {/* Axis 1. TODO: Add condition for adding tooltip info */}
+      <EuiAccordion
+        id={''}
+        buttonContent={popoverConfig('yLeft', isHorizontal).popoverTitle}
+        paddingSize="s"
+        initialIsOpen
+        isDisabled={
+          Object.keys(axisGroups.find((group) => group.groupId === 'left') || {}).length === 0
+        }
+        {...(Object.keys(axisGroups.find((group) => group.groupId === 'left') || {}).length ===
+          0 && { forceState: 'closed' })}
+      >
+        <XyAxisSettings
+          axis="yLeft"
+          layers={state?.layers}
+          axisTitle={state?.yTitle}
+          updateTitleState={onTitleStateChange}
+          areTickLabelsVisible={tickLabelsVisibilitySettings.yLeft}
+          toggleTickLabelsVisibility={onTickLabelsVisibilitySettingsChange}
+          areGridlinesVisible={gridlinesVisibilitySettings.yLeft}
+          toggleGridlinesVisibility={onGridlinesVisibilitySettingsChange}
+          orientation={labelsOrientation.yLeft}
+          setOrientation={onLabelsOrientationChange}
+          isTitleVisible={axisTitlesVisibilitySettings.yLeft}
+          extent={state?.yLeftExtent || { mode: 'full' }}
+          setExtent={setExtentFn('yLeftExtent')}
+          hasBarOrAreaOnAxis={hasBarOrAreaOnLeftAxis}
+          dataBounds={dataBounds.left}
+          hasPercentageAxis={hasPercentageAxis(axisGroups, 'left', state)}
+          scale={state?.yLeftScale}
+          setScale={setScaleFn('yLeftScale')}
+          setScaleWithExtent={setScaleWithExtentFn('yLeftExtent', 'yLeftScale')}
+        />
+      </EuiAccordion>
+      <EuiHorizontalRule margin="m" />
+      {/* Axis 2 */}
+      <EuiAccordion
+        id={''}
+        buttonContent={popoverConfig('x', isHorizontal).popoverTitle}
+        paddingSize="s"
+        initialIsOpen
+      >
+        <XyAxisSettings
+          axis="x"
+          layers={state?.layers}
+          axisTitle={state?.xTitle}
+          updateTitleState={onTitleStateChange}
+          areTickLabelsVisible={tickLabelsVisibilitySettings.x}
+          toggleTickLabelsVisibility={onTickLabelsVisibilitySettingsChange}
+          areGridlinesVisible={gridlinesVisibilitySettings.x}
+          toggleGridlinesVisibility={onGridlinesVisibilitySettingsChange}
+          orientation={labelsOrientation.x}
+          setOrientation={onLabelsOrientationChange}
+          isTitleVisible={axisTitlesVisibilitySettings.x}
+          endzonesVisible={!state?.hideEndzones}
+          setEndzoneVisibility={onChangeEndzoneVisiblity}
+          currentTimeMarkerVisible={state?.showCurrentTimeMarker}
+          setCurrentTimeMarkerVisibility={onChangeCurrentTimeMarkerVisibility}
+          hasBarOrAreaOnAxis={false}
+          hasPercentageAxis={false}
+          useMultilayerTimeAxis={isTimeHistogramModeEnabled && !shouldRotate}
+          extent={hasNumberHistogram ? state?.xExtent || { mode: 'dataBounds' } : undefined}
+          setExtent={setExtentFn('xExtent')}
+          dataBounds={xDataBounds}
+        />
+      </EuiAccordion>
+      <EuiHorizontalRule margin="m" />
+      {/* Axis 3. TODO: Add condition for adding tooltip info */}
+      <EuiAccordion
+        id={''}
+        buttonContent={popoverConfig('yRight', isHorizontal).popoverTitle}
+        paddingSize="s"
+        initialIsOpen
+        isDisabled={
+          Object.keys(axisGroups.find((group) => group.groupId === 'right') || {}).length === 0
+        }
+        {...(Object.keys(axisGroups.find((group) => group.groupId === 'right') || {}).length ===
+          0 && { forceState: 'closed' })}
+      >
+        <XyAxisSettings
+          axis="yRight"
+          layers={state?.layers}
+          axisTitle={state?.yRightTitle}
+          updateTitleState={onTitleStateChange}
+          areTickLabelsVisible={tickLabelsVisibilitySettings.yRight}
+          toggleTickLabelsVisibility={onTickLabelsVisibilitySettingsChange}
+          areGridlinesVisible={gridlinesVisibilitySettings.yRight}
+          toggleGridlinesVisibility={onGridlinesVisibilitySettingsChange}
+          orientation={labelsOrientation.yRight}
+          setOrientation={onLabelsOrientationChange}
+          hasPercentageAxis={hasPercentageAxis(axisGroups, 'right', state)}
+          isTitleVisible={axisTitlesVisibilitySettings.yRight}
+          extent={state?.yRightExtent || { mode: 'full' }}
+          setExtent={setExtentFn('yRightExtent')}
+          hasBarOrAreaOnAxis={hasBarOrAreaOnRightAxis}
+          dataBounds={dataBounds.right}
+          scale={state?.yRightScale}
+          setScale={setScaleFn('yRightScale')}
+          setScaleWithExtent={setScaleWithExtentFn('yRightExtent', 'yRightScale')}
+        />
+      </EuiAccordion>
     </>
   );
 };
