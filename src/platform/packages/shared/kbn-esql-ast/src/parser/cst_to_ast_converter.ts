753dc9680b59c4d59443740c895a692ded6de31d
Sebastian Delle Donne <sebastian.delledonne@elastic.co>
2025-10-06 14:14:21 +0200

    [ES|QL] Add AST support for new `FUSE` parameters (#237485)
    Part of https://github.com/elastic/kibana/issues/236938
    ## Summary
    This PR adds `AST` support for the new`FUSE` parameters.
    
    ```
    fuseCommand
        : DEV_FUSE (fuseType=identifier)? (fuseConfiguration)*
        ;
    
    fuseConfiguration
        : SCORE BY score=qualifiedName
        | KEY BY key=fields
        | GROUP BY group=qualifiedName
        | WITH options=mapExpression
        ;
    ```
    
    `FUSE <fuse_method> SCORE BY <score_column> GROUP BY <group_column> KEY
    BY <key_columns> WITH <options>`
    
    ### Some Examples
    `| FUSE LINEAR # Merge results using linear combination of scores (equal
    weights by default)`
    
    `| FUSE LINEAR WITH { "normalizer": "minmax" } # Linear combination with
    min-max normalization (scales scores to 0-1 range)
     `
    
    `| FUSE LINEAR WITH { "weights": { "fork1": 0.7, "fork2": 0.3 },
    "normalizer": "minmax" } # Weighted linear combination: 70% lexical, 30%
    semantic, with min-max normalization`
    
    ### Note
    No tests has been added to check parameters or fields with dots, as the
    parser needs this
    [fix](https://github.com/elastic/elasticsearch/pull/135901) to be merged
    to support them.
    
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios

--- src/platform/packages/shared/kbn-esql-ast/src/parser/cst_to_ast_converter.ts
+++ src/platform/packages/shared/kbn-esql-ast/src/parser/cst_to_ast_converter.ts
@@ -407,22 +407,29 @@ export class CstToAstConverter {
     return command;
   }
 
-  private toOption(name: string, ctx: antlr.ParserRuleContext): ast.ESQLCommandOption {
+  private toOption(
+    name: string,
+    ctx: antlr.ParserRuleContext,
+    args: ast.ESQLAstItem[] = [],
+    incomplete?: boolean
+  ): ast.ESQLCommandOption {
     return {
       type: 'option',
       name,
       text: ctx.getText(),
       location: getPosition(ctx.start, ctx.stop),
-      args: [],
-      incomplete: Boolean(
-        ctx.exception ||
-          ctx.children?.some((c) => {
-            // TODO: 1. Remove this expect error comment
-            // TODO: 2. .isErrorNode is function: .isErrorNode()
-            // @ts-expect-error not exposed in type but exists see https://github.com/antlr/antlr4/blob/v4.11.1/runtime/JavaScript/src/antlr4/tree/ErrorNodeImpl.js#L19
-            return Boolean(c.isErrorNode);
-          })
-      ),
+      args,
+      incomplete:
+        incomplete ??
+        Boolean(
+          ctx.exception ||
+            ctx.children?.some((c) => {
+              // TODO: 1. Remove this expect error comment
+              // TODO: 2. .isErrorNode is function: .isErrorNode()
+              // @ts-expect-error not exposed in type but exists see https://github.com/antlr/antlr4/blob/v4.11.1/runtime/JavaScript/src/antlr4/tree/ErrorNodeImpl.js#L19
+              return Boolean(c.isErrorNode);
+            })
+        ),
     };
   }
 
@@ -1453,9 +1460,85 @@ export class CstToAstConverter {
   // --------------------------------------------------------------------- FUSE
 
   private fromFuseCommand(ctx: cst.FuseCommandContext): ast.ESQLCommand<'fuse'> {
-    const command = this.createCommand('fuse', ctx);
+    const fuseTypeCtx = ctx.identifier();
 
-    return command;
+    const args: ast.ESQLAstItem[] = [];
+    let incomplete = false;
+
+    // FUSE <fuse_method>
+    if (fuseTypeCtx) {
+      args.push(this.fromIdentifier(fuseTypeCtx));
+    }
+
+    // FUSE SCORE BY <score_column> GROUP BY <group_column> KEY BY <key_columns> WITH <options>
+    for (const config of ctx.fuseConfiguration_list()) {
+      const configurationItemCommandOption = this.fromFuseConfigurationItem(config);
+      if (configurationItemCommandOption) {
+        args.push(configurationItemCommandOption);
+      }
+      incomplete ||= configurationItemCommandOption?.incomplete ?? true;
+    }
+
+    return this.createCommand('fuse', ctx, { args, incomplete });
+  }
+
+  private fromFuseConfigurationItem(
+    configCtx: cst.FuseConfigurationContext
+  ): ast.ESQLCommandOption | null {
+    const byContext = configCtx.BY();
+
+    // SCORE BY <score_column>
+    const scoreCtx = configCtx.SCORE();
+    if (scoreCtx && byContext) {
+      const args: ast.ESQLAstItem[] = [];
+
+      const scoreColumnCtx = configCtx.qualifiedName();
+      if (textExistsAndIsValid(scoreColumnCtx.getText())) {
+        args.push(this.toColumn(scoreColumnCtx));
+      }
+
+      const incomplete = args.length === 0;
+      return this.toOption('score by', configCtx, args, incomplete);
+    }
+
+    // KEY BY <key_columns>
+    const keyCtx = configCtx.KEY();
+    if (keyCtx && byContext) {
+      const args = this.fromFields(configCtx.fields());
+      const incomplete = args.length === 0;
+      return this.toOption('key by', configCtx, args, incomplete);
+    }
+
+    // GROUP BY <group_column>
+    const groupCtx = configCtx.GROUP();
+    if (groupCtx && byContext) {
+      const args: ast.ESQLAstItem[] = [];
+      const groupColumnCtx = configCtx.qualifiedName();
+
+      if (textExistsAndIsValid(groupColumnCtx.getText())) {
+        args.push(this.toColumn(groupColumnCtx));
+      }
+
+      const incomplete = args.length === 0;
+      return this.toOption('group by', configCtx, args, incomplete);
+    }
+
+    // WITH <map_expression>
+    const withCtx = configCtx.WITH();
+    if (withCtx) {
+      const args: ast.ESQLAstItem[] = [];
+      const mapExpressionCtx = configCtx.mapExpression();
+
+      const map = this.fromMapExpression(mapExpressionCtx);
+      if (map && textExistsAndIsValid(map.text)) {
+        args.push(map);
+      }
+
+      const incomplete = args.length === 0 || map.incomplete;
+      return this.toOption('with', configCtx, args, incomplete);
+    }
+
+    return null;
   }
 
   // --------------------------------------------------------------------- FORK
