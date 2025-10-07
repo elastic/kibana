1dbde630ab607be9e3e3881107a36fe12d087427
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2025-10-07 12:36:15 +0200

    fix: Add sync check for saved objects for trained models.
    While a model is deploying we prevent user to use an infenence endpoint
    in mappings editor with a modal. The modal either asks for a force
    writing (which would end up working eventually) or there is a try again
    button if user wants to wait.
    
    The try again button wouldn't work because the status of the models are
    checked and synced to the Saved Objects only when Trained Models page is
    visited or refresh button is pressed.
    
    This commit adds sync call to the try again button so we are certain
    that we get the latest data from the Saved Object.

--- x-pack/platform/plugins/shared/index_management/public/application/sections/home/index_list/details_page/trained_models_deployment_modal.tsx
+++ x-pack/platform/plugins/shared/index_management/public/application/sections/home/index_list/details_page/trained_models_deployment_modal.tsx
@@ -49,7 +49,6 @@ export function TrainedModelsDeploymentModal({
   forceSaveMappings,
   saveMappings,
   saveMappingsLoading,
-  setErrorsInTrainedModelDeployment,
 }: TrainedModelsDeploymentModalProps) {
   const modalTitleId = useGeneratedHtmlId();
   const { fields, inferenceToModelIdMap } = useMappingsState();
@@ -173,6 +172,7 @@ export function TrainedModelsDeploymentModal({
         </ul>
         <EuiSpacer />
         <EuiCallOut
+          announceOnMount
           iconType="warning"
           color="warning"
           title={i18n.translate(
@@ -208,7 +208,15 @@ export function TrainedModelsDeploymentModal({
       <EuiModalFooter>
         <EuiFlexGroup alignItems="center" justifyContent="flexEnd">
           <EuiFlexItem grow={false}>
-            <EuiButtonEmpty onClick={closeModal}>
+            <EuiButtonEmpty
+              onClick={closeModal}
+              aria-label={i18n.translate(
+                'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.cancelButtonAriaLabel',
+                {
+                  defaultMessage: 'Cancel and close modal',
+                }
+              )}
+            >
               {i18n.translate(
                 'xpack.idxMgmt.indexDetails.trainedModelsDeploymentModal.cancelButtonLabel',
                 {
