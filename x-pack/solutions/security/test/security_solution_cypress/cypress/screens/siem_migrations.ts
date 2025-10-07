f032195404f3ade1e100c844156b3ec57614be85
PhilippeOberti <philippe.oberti@elastic.co>
2025-10-07 00:00:12 -0500

    [Security Solution] remove unused Cypress screens and tasks

--- x-pack/solutions/security/test/security_solution_cypress/cypress/screens/siem_migrations.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/screens/siem_migrations.ts
@@ -30,7 +30,6 @@ export const ONBOARDING_RULE_MIGRATIONS_LIST = getDataTestSubjectSelectorStartWi
 
 export const ONBOARDING_TRANSLATIONS_RESULT_TABLE = {
   TABLE: getDataTestSubjectSelector('translationsResults'),
-  TRANSLATION_STATUS: (status: string) => getDataTestSubjectSelector(`translationStatus-${status}`),
   TRANSLATION_STATUS_COUNT: (status: string) =>
     getDataTestSubjectSelector(`translationStatusCount-${status}`),
 };
@@ -49,8 +48,6 @@ export const TRANSLATED_RULE_DETAILS_FLYOUT = getDataTestSubjectSelector(
 export const TRANSLATED_RULE_QUERY_EDITOR_PARENT = `${getDataTestSubjectSelector(
   'kibanaCodeEditor'
 )}`;
-
-export const TRANSLATED_RULE_QUERY_EDITOR_QUERY_CONTAINER = `${TRANSLATED_RULE_QUERY_EDITOR_PARENT} .view-lines`;
 
 export const TRANSLATED_RULE_QUERY_EDITOR_INPUT = `${TRANSLATED_RULE_QUERY_EDITOR_PARENT} textarea`;
 
