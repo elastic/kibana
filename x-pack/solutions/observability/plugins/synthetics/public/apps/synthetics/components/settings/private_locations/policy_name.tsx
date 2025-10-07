b457fca0f1b2f980a1f53466ca1a4238b78d6228
Francesco Fagnani <francesco.fagnani@elastic.co>
2025-10-07 12:32:29 +0200

    fix(policy not found): changed message when policy not found

--- x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/settings/private_locations/policy_name.tsx
+++ x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/settings/private_locations/policy_name.tsx
@@ -48,7 +48,7 @@ export const PolicyName = ({ agentPolicyId }: { agentPolicyId: string }) => {
             </EuiLink>
           ) : (
             <EuiText color="danger" size="s" className="eui-displayInline">
-              {POLICY_IS_DELETED}
+              {POLICY_NOT_FOUND}
             </EuiText>
           )}
         </EuiTextColor>
@@ -68,8 +68,8 @@ export const PolicyName = ({ agentPolicyId }: { agentPolicyId: string }) => {
   );
 };
 
-const POLICY_IS_DELETED = i18n.translate('xpack.synthetics.monitorManagement.deletedPolicy', {
-  defaultMessage: 'Policy is deleted',
+const POLICY_NOT_FOUND = i18n.translate('xpack.synthetics.monitorManagement.policyNotFound', {
+  defaultMessage: 'Policy not found in the current space',
 });
 
 const AGENTS_LABEL = i18n.translate('xpack.synthetics.monitorManagement.agents', {
