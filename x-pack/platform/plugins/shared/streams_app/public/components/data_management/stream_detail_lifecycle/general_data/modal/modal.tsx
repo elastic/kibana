4357e34d0c8afb66caec397c85151ef87ddd9488
klacabane <kevin.lacabane@elastic.co>
2025-10-08 18:52:12 +0200

    remove casting

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_lifecycle/general_data/modal/modal.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_lifecycle/general_data/modal/modal.tsx
@@ -7,8 +7,19 @@
 
 import React, { useState, useMemo } from 'react';
 import type { PolicyFromES } from '@kbn/index-lifecycle-management-common-shared';
-import type { IngestStreamLifecycle, IngestStreamLifecycleDSL } from '@kbn/streams-schema';
-import { isDslLifecycle, isIlmLifecycle, isInheritLifecycle } from '@kbn/streams-schema';
+import type {
+  IngestStreamLifecycle,
+  IngestStreamLifecycleAll,
+  IngestStreamLifecycleDSL,
+} from '@kbn/streams-schema';
+import {
+  effectiveToIngestLifecycle,
+  isDisabledLifecycle,
+  isDslLifecycle,
+  isErrorLifecycle,
+  isIlmLifecycle,
+  isInheritLifecycle,
+} from '@kbn/streams-schema';
 import { Streams, isRoot } from '@kbn/streams-schema';
 import {
   EuiButton,
@@ -27,7 +38,6 @@ import {
   EuiSpacer,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { omit } from 'lodash';
 import { IlmField } from './ilm';
 import { DslField, DEFAULT_RETENTION_UNIT, DEFAULT_RETENTION_VALUE } from './dsl';
 import { useKibana } from '../../../../../hooks/use_kibana';
@@ -61,8 +71,8 @@ export function EditLifecycleModal({
 
   const [isInheritToggleOn, setIsInheritToggleOn] = useState<boolean>(isCurrentLifecycleInherit);
   const [selectedAction, setSelectedAction] = useState<LifecycleEditAction>(initialSelectedAction);
-  const [lifecycle, setLifecycle] = useState<IngestStreamLifecycle>(
-    omit(definition.effective_lifecycle, 'from') as IngestStreamLifecycle
+  const [lifecycle, setLifecycle] = useState<IngestStreamLifecycleAll>(
+    effectiveToIngestLifecycle(definition.effective_lifecycle)
   );
 
   const [isSaveButtonDisabled, setIsSaveButtonDisabled] = useState<boolean>(
@@ -156,9 +166,7 @@ export function EditLifecycleModal({
                 onChange={(event) => {
                   if (event.target.checked) {
                     if (isCurrentLifecycleInherit) {
-                      setLifecycle(
-                        omit(definition.effective_lifecycle, 'from') as IngestStreamLifecycle
-                      );
+                      setLifecycle(effectiveToIngestLifecycle(definition.effective_lifecycle));
                       setSelectedAction(initialSelectedAction);
                     }
                     setIsInheritToggleOn(true);
@@ -260,7 +268,18 @@ export function EditLifecycleModal({
               fill
               disabled={isSaveButtonDisabled}
               isLoading={updateInProgress}
-              onClick={() => updateLifecycle(isInheritToggleOn ? { inherit: {} } : lifecycle)}
+              onClick={() => {
+                if (isInheritToggleOn) {
+                  updateLifecycle({ inherit: {} });
+                  return;
+                }
+
+                if (isDisabledLifecycle(lifecycle) || isErrorLifecycle(lifecycle)) {
+                  return;
+                }
+
+                updateLifecycle(lifecycle);
+              }}
             >
               {i18n.translate('xpack.streams.streamDetailLifecycle.saveButton', {
                 defaultMessage: 'Save',
