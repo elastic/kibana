fb677d6071f15a96c654777eaff1f5af7dab1094
Nikita Khristinin <nkhristinin@gmail.com>
2025-10-08 10:43:32 +0200

    Fix tests

--- x-pack/platform/plugins/shared/alerting/server/routes/backfill/transforms/transform_backfill_to_backfill_response/v1.test.ts
+++ x-pack/platform/plugins/shared/alerting/server/routes/backfill/transforms/transform_backfill_to_backfill_response/v1.test.ts
@@ -47,6 +47,7 @@ describe('transformBackfillToBackfillResponse', () => {
         created_at: '2024-01-30T00:00:00.000Z',
         duration: '12h',
         enabled: true,
+        initiator: 'user',
         rule: {
           name: 'my rule name',
           tags: ['foo'],
