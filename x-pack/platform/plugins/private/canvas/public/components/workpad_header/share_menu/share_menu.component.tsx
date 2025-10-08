8fd2f2de3f92c6b736f13c6982e768f1adb4894f
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-10-08 15:26:03 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/platform/plugins/private/canvas/public/components/workpad_header/share_menu/share_menu.component.tsx
+++ x-pack/platform/plugins/private/canvas/public/components/workpad_header/share_menu/share_menu.component.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { EuiButtonEmpty, EuiContextMenu, EuiIcon } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
