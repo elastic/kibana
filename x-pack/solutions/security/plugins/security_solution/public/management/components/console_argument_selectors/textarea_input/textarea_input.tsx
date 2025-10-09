0b7badfb73b2a275625bd79ac34d729e63d09321
Paul Tavares <paul.tavares@elastic.co>
2025-10-09 16:08:25 -0400

    Skeleton for new component

---
+++ x-pack/solutions/security/plugins/security_solution/public/management/components/console_argument_selectors/textarea_input/textarea_input.tsx
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { memo } from 'react';
+import type { EndpointCommandDefinitionMeta } from '../../endpoint_responder/types';
+import type { CommandArgumentValueSelectorProps } from '../../console/types';
+
+/**
+ * Console argument component that displays a popup textarea for user to enter free-form data.
+ */
+export const TextareaInput = memo<
+  CommandArgumentValueSelectorProps<unknown, unknown, EndpointCommandDefinitionMeta>
+>((props) => {
+  return <div>{'TextareaInput placeholder'}</div>;
+});
+TextareaInput.displayName = 'TextareaInput';
