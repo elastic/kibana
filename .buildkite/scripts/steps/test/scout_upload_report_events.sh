c9e4e0c2094969299fd7b851fefa39d1b1e0af29
Maryam Saeidi <maryam.saeidi@elastic.co>
2025-10-09 17:09:03 +0200

    Revert scout_upload_report_events.sh changes

--- .buildkite/scripts/steps/test/scout_upload_report_events.sh
+++ .buildkite/scripts/steps/test/scout_upload_report_events.sh
@@ -2,15 +2,9 @@
 
 set -euo pipefail
 
-SCOUT_EXTRA_ARGS=""
-if [ -n "${SCOUT_EVENT_LOG_PATH:-}" ]; then
-  SCOUT_EXTRA_ARGS="--eventLogPath $SCOUT_EVENT_LOG_PATH"
-fi
-
 echo "--- Upload Scout reporter events to AppEx QA's team cluster"
 if [[ "${SCOUT_REPORTER_ENABLED:-}" == "true" ]]; then
-  # shellcheck disable=SC2086
-  node scripts/scout upload-events --dontFailOnError $SCOUT_EXTRA_ARGS
+  node scripts/scout upload-events --dontFailOnError
 else
   echo "⚠️ The SCOUT_REPORTER_ENABLED environment variable is not 'true'. Skipping event upload."
 fi
