b4e1d7ead3a17ed3570ee84248e53d09a5ffcc45
Alex Szabo <alex.szabo@elastic.co>
2025-10-09 12:06:57 +0200

    some reconnissance

--- .buildkite/scripts/bootstrap.sh
+++ .buildkite/scripts/bootstrap.sh
@@ -26,6 +26,8 @@ if [[ "$(pwd)" != *"/local-ssd/"* && "$(pwd)" != "/dev/shm"* ]]; then
   # Check if the download succeeds before trying to extract the cache
   if (buildkite-agent artifact download --step "pre_build" "**/moon-cache.tar.gz" ~/); then
     echo "Found moon-cache.tar.gz artifact, extracting to ./.moon/cache"
+    ls -la ~/
+    pwd
     mkdir -p ./.moon/cache
     tar -xzf ~/moon-cache.tar.gz -C ./.moon/cache
   elif [[ -d ~/.kibana-moon-cache ]]; then
