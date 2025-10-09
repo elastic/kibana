761a84c3766acc182f1c47605e3d100be5e4071e
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-10-09 13:41:28 +0000

    [CI] Auto-commit changed files from 'yarn openapi:generate'

--- x-pack/solutions/security/plugins/security_solution/common/api/endpoint/actions/file_info/file_info.gen.ts
+++ x-pack/solutions/security/plugins/security_solution/common/api/endpoint/actions/file_info/file_info.gen.ts
@@ -20,7 +20,7 @@ export type EndpointFileInfoRequestParams = z.infer<typeof EndpointFileInfoReque
 export const EndpointFileInfoRequestParams = z.object({
   action_id: z.string(),
   /** 
-      * The file identifier is constructed one of two ways:
+      * The file identifier is constructed in one of two ways:
 - For Elastic Defend agents (`agentType` of `endpoint`): combine the `action_id` and `agent_id` values using a dot (`.`) separator:
 `{file_id}` = `{action_id}.{agent_id}`
 - For all other agent types: the `file_id` is the `agent_id` for which the response action was sent to.
