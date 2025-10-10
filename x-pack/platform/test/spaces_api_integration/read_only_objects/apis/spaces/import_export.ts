b6f0242351f866740177c30a0a31f9cc3d253780
“jeramysoucy” <jeramy.soucy@elastic.co>
2025-10-10 13:41:16 +0200

    Removes unused import

--- x-pack/platform/test/spaces_api_integration/read_only_objects/apis/spaces/import_export.ts
+++ x-pack/platform/test/spaces_api_integration/read_only_objects/apis/spaces/import_export.ts
@@ -4,7 +4,6 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { result } from 'lodash';
 import { parse as parseCookie } from 'tough-cookie';
 
 import expect from '@kbn/expect';
