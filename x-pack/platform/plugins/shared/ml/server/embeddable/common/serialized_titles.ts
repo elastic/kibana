d324be7a8ab1717d678541b648741ba1a4e29ce5
Angela Chuang <yi-chun.chuang@elastic.co>
2025-10-10 11:49:31 +0100

    comments

--- x-pack/platform/plugins/shared/ml/server/embeddable/common/serialized_titles.ts
+++ x-pack/platform/plugins/shared/ml/server/embeddable/common/serialized_titles.ts
@@ -6,6 +6,12 @@
  */
 import { schema } from '@kbn/config-schema';
 
+/*
+ ** Serialized titles schema will soon be importable from a
+ ** @kbn/presentation-publishing-schemas package.
+ ** See https://github.com/elastic/kibana/pull/235977.
+ */
+
 export const serializedTitlesSchema = schema.object({
   title: schema.maybe(schema.string()),
   description: schema.maybe(schema.string()),
