4852ca5a07a207bd7d9673b48f568d1321f8c3da
Angela Chuang <yi-chun.chuang@elastic.co>
2025-10-14 15:40:23 +0100

    remove unused schemas

--- x-pack/platform/plugins/shared/ml/server/embeddable/anomaly_charts/anomaly_charts.ts
+++ x-pack/platform/plugins/shared/ml/server/embeddable/anomaly_charts/anomaly_charts.ts
@@ -7,25 +7,13 @@
 
 import { schema } from '@kbn/config-schema';
 import { timeRangeSchema } from '@kbn/es-query-server';
-import { ML_ENTITY_FIELD_OPERATIONS, ML_ENTITY_FIELD_TYPE } from '@kbn/ml-anomaly-utils';
 import { mlEntityFieldSchema } from '@kbn/ml-anomaly-utils/schemas';
 import { serializedTitlesSchema } from '../schemas';
 
-export const severityThresholdSchema = schema.object({
+const severityThresholdSchema = schema.object({
   min: schema.number(),
   max: schema.maybe(schema.number()),
 });
-
-export const mlEntityFieldTypeSchema = schema.oneOf([
-  schema.literal(ML_ENTITY_FIELD_TYPE.BY),
-  schema.literal(ML_ENTITY_FIELD_TYPE.OVER),
-  schema.literal(ML_ENTITY_FIELD_TYPE.PARTITON),
-]);
-
-export const mlEntityFieldOperationSchema = schema.oneOf([
-  schema.literal(ML_ENTITY_FIELD_OPERATIONS.ADD),
-  schema.literal(ML_ENTITY_FIELD_OPERATIONS.REMOVE),
-]);
 
 export const anomalyChartsEmbeddableRuntimeStateSchema = schema.object({
   jobIds: schema.arrayOf(schema.string()),
