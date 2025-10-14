95feafaf6c30a616718436722f130d48c79a8431
Georgiana-Andreea Onoleata <georgiana.onoleata@elastic.co>
2025-10-14 10:31:54 +0300

    fixed type checks

--- x-pack/solutions/observability/plugins/uptime/common/rules/alert_actions.ts
+++ x-pack/solutions/observability/plugins/uptime/common/rules/alert_actions.ts
@@ -134,14 +134,16 @@ export function populateAlertActions({
 
 function getSlackAPIActionParams(
   { defaultActionMessage, defaultRecoveryMessage }: Translations,
-  allowedChannels: Array<{ id: string; name: string }>,
+  allowedChannels: Array<{ id?: string; name: string }>,
   recovery = false
 ): SlackApiActionParams {
   return {
     subAction: 'postMessage',
     subActionParams: {
       text: recovery ? defaultRecoveryMessage : defaultActionMessage,
-      channelIds: allowedChannels.map((channel) => channel.id),
+      channelIds: allowedChannels
+        .map((channel) => channel.id)
+        .filter((id): id is string => id !== undefined),
     },
   };
 }
