eff0c493be3317b85ab26a1488f224d85d32eb55
Devon Thomson <devon.thomson@elastic.co>
2025-10-14 15:46:49 -0400

    [Controls] Fix initialization race condition (#237726)
    Fixes a race condition in the controls initialization step.
    
    (cherry picked from commit e26e842a094f95c0396bdd91f70ecd409956082d)
    
    # Conflicts:
    #	src/platform/plugins/shared/controls/public/control_group/selections_manager.ts
    #	src/platform/plugins/shared/controls/public/controls/data_controls/data_control_manager.ts
    #	src/platform/plugins/shared/controls/public/controls/data_controls/options_list_control/get_options_list_control_factory.test.tsx

--- src/platform/plugins/shared/dashboard/public/dashboard_api/control_group_manager.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_api/control_group_manager.ts
@@ -25,7 +25,7 @@ export function initializeControlGroupManager(
         .pipe(
           skipWhile((controlGroupApi) => !controlGroupApi),
           switchMap(async (controlGroupApi) => {
-            await controlGroupApi?.untilInitialized();
+            await controlGroupApi?.untilFiltersPublished();
           }),
           first()
         )
