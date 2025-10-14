b14fff51021f034704d84073d23f6763b5636ffe
Eyo Okon Eyo <eyo.eyo@elastic.co>
2025-10-14 14:31:26 +0200

    restrict auto-docs functionality to shared_ux storybook

--- src/platform/packages/shared/kbn-storybook/src/lib/default_config.ts
+++ src/platform/packages/shared/kbn-storybook/src/lib/default_config.ts
@@ -49,7 +49,6 @@ export const defaultConfig: StorybookConfig = {
     // https://storybook.js.org/docs/essentials
     '@storybook/addon-essentials',
     '@storybook/addon-jest',
-    '@storybook/addon-docs',
     {
       /**
        * This addon replaces rules in the default SB webpack config
