3b752b37c6c091179a908f5aebd954e8e67bb09f
Christiane (Tina) Heiligers <christiane.heiligers@elastic.co>
2025-10-16 15:20:34 -0700

    Use successful response mock for nodes info to remove retry complexity

--- src/core/packages/elasticsearch/server-internal/src/elasticsearch_service.test.ts
+++ src/core/packages/elasticsearch/server-internal/src/elasticsearch_service.test.ts
@@ -75,8 +75,8 @@ beforeEach(() => {
   mockConfig$ = new BehaviorSubject({
     hosts: ['http://1.2.3.4'],
     healthCheck: {
-      delay: duration(TICK), // default unit ms, configured default is 2500ms = 2.5s
-      startupDelay: duration(TICK), // default unit ms≈
+      delay: duration(TICK),
+      startupDelay: duration(TICK),
       retry: 1,
     },
     ssl: {
@@ -268,205 +268,206 @@ describe('#setup', () => {
 
     expect(mockedClient.nodes.info).toHaveBeenCalledTimes(2);
   });
-});
 
-describe('#start', () => {
-  it('throws if called before `setup`', async () => {
-    await expect(() => elasticsearchService.start()).rejects.toMatchInlineSnapshot(
-      `[Error: ElasticsearchService needs to be setup before calling start]`
-    );
-  });
+  it('esNodeVersionCompatibility$ only emits new values when something changes', async () => {});
 
-  it('returns elasticsearch client as a part of the contract', async () => {
-    await elasticsearchService.setup(setupDeps);
-    const startContract = await elasticsearchService.start();
-    const client = startContract.client;
-
-    expect(client.asInternalUser).toBe(mockClusterClientInstance.asInternalUser);
-  });
-
-  it('should log.error non-compatible nodes error', async () => {
-    const defaultMessage = {
-      isCompatible: true,
-      kibanaVersion: '8.0.0',
-      incompatibleNodes: [],
-      warningNodes: [],
-    };
-    const observable$ = new BehaviorSubject<NodesVersionCompatibility>(defaultMessage);
-
-    // @ts-expect-error this module is mocked, so `mockImplementation` is an allowed property
-    pollEsNodesVersionMocked.mockImplementation(() => observable$);
-
-    await elasticsearchService.setup(setupDeps);
-    tick();
-    await elasticsearchService.start();
-    expect(loggingSystemMock.collect(coreContext.logger).error).toEqual([]);
-    observable$.next({
-      ...defaultMessage,
-      isCompatible: false,
-      message: 'Something went terribly wrong!',
-    });
-    expect(loggingSystemMock.collect(coreContext.logger).error).toEqual([
-      ['Something went terribly wrong!'],
-    ]);
-  });
-
-  it('logs an info message about connecting to ES', async () => {
-    isValidConnectionMock.mockImplementation(async () => {
-      tick();
-    });
-
-    await elasticsearchService.setup(setupDeps);
-    await elasticsearchService.start();
-
-    expect(isValidConnectionMock).toHaveBeenCalledTimes(1);
-
-    const infoMessages = loggingSystemMock.collect(coreContext.logger).info;
-    expect(infoMessages).toHaveLength(1);
-
-    const esMessage = infoMessages[0][0];
-    expect(esMessage).toMatch(
-      /^Successfully connected to Elasticsearch after waiting for ([0-9]+) milliseconds$/
-    );
-  });
-
-  it('returns the information about the time spent waiting for Elasticsearch', async () => {
-    isValidConnectionMock.mockImplementation(async () => {
-      tick();
-    });
-
-    await elasticsearchService.setup(setupDeps);
-    const startContract = await elasticsearchService.start();
-
-    expect(isValidConnectionMock).toHaveBeenCalledTimes(1);
-
-    const infoMessages = loggingSystemMock.collect(coreContext.logger).info;
-    expect(infoMessages).toHaveLength(1);
-
-    const regexp =
-      /^Successfully connected to Elasticsearch after waiting for ([0-9]+) milliseconds$/;
-
-    const esMessage = infoMessages[0][0];
-    const groups = regexp.exec(esMessage);
-    const esWaitTime = parseInt(groups![1], 10);
-
-    expect(startContract.metrics.elasticsearchWaitTime).toEqual(esWaitTime);
-  });
-
-  describe('skipStartupConnectionCheck', () => {
-    it('should validate the connection by default', async () => {
-      await elasticsearchService.setup(setupDeps);
-      expect(isValidConnectionMock).not.toHaveBeenCalled();
-      await elasticsearchService.start();
-      expect(isValidConnectionMock).toHaveBeenCalledTimes(1);
-    });
-
-    it('should validate the connection when `false`', async () => {
-      mockConfig$.next({
-        ...(await mockConfig$.pipe(first()).toPromise()),
-        skipStartupConnectionCheck: false,
-      });
-      await elasticsearchService.setup(setupDeps);
-      expect(isValidConnectionMock).not.toHaveBeenCalled();
-      await elasticsearchService.start();
-      expect(isValidConnectionMock).toHaveBeenCalledTimes(1);
-    });
-
-    it('should not validate the connection when `true`', async () => {
-      mockConfig$.next({
-        ...(await mockConfig$.pipe(first()).toPromise()),
-        skipStartupConnectionCheck: true,
-      });
-      await elasticsearchService.setup(setupDeps);
-      expect(isValidConnectionMock).not.toHaveBeenCalled();
-      await elasticsearchService.start();
-      expect(isValidConnectionMock).not.toHaveBeenCalled();
-    });
-  });
-
-  describe('isInlineScriptingEnabled', () => {
-    it('does not throw error when scripting is enabled', async () => {
-      isScriptingEnabledMock.mockResolvedValue(true);
-
-      await elasticsearchService.setup(setupDeps);
-      expect(isScriptingEnabledMock).not.toHaveBeenCalled();
-
-      await expect(elasticsearchService.start()).resolves.toBeDefined();
-      expect(isScriptingEnabledMock).toHaveBeenCalledTimes(1);
-    });
-
-    it('throws an error if scripting is disabled', async () => {
-      isScriptingEnabledMock.mockResolvedValue(false);
-
-      await elasticsearchService.setup(setupDeps);
-
-      await expect(elasticsearchService.start()).rejects.toThrowError(
-        'Inline scripting is disabled'
+  describe('#start', () => {
+    it('throws if called before `setup`', async () => {
+      await expect(() => elasticsearchService.start()).rejects.toMatchInlineSnapshot(
+        `[Error: ElasticsearchService needs to be setup before calling start]`
       );
     });
 
-    it('does not throw error when `skipStartupConnectionCheck` is true', async () => {
-      isScriptingEnabledMock.mockResolvedValue(false);
-      mockConfig$.next({
-        ...(await mockConfig$.pipe(first()).toPromise()),
-        skipStartupConnectionCheck: true,
-      });
-
-      await elasticsearchService.setup(setupDeps);
-      await expect(elasticsearchService.start()).resolves.toBeDefined();
-    });
-  });
-
-  describe('#createClient', () => {
-    it('allows to specify config properties', async () => {
+    it('returns elasticsearch client as a part of the contract', async () => {
       await elasticsearchService.setup(setupDeps);
       const startContract = await elasticsearchService.start();
+      const client = startContract.client;
 
-      // reset all mocks called during setup phase
-      MockClusterClient.mockClear();
-
-      const customConfig = { keepAlive: true };
-      const clusterClient = startContract.createClient('custom-type', customConfig);
-
-      expect(clusterClient).toBe(mockClusterClientInstance);
-
-      expect(MockClusterClient).toHaveBeenCalledTimes(1);
-      expect(MockClusterClient.mock.calls[0][0]).toEqual(
-        expect.objectContaining({ config: expect.objectContaining(customConfig) })
-      );
-    });
-    it('creates a new client on each call', async () => {
-      await elasticsearchService.setup(setupDeps);
-      const startContract = await elasticsearchService.start();
-
-      // reset all mocks called during setup phase
-      MockClusterClient.mockClear();
-
-      const customConfig = { keepAlive: true };
-
-      startContract.createClient('custom-type', customConfig);
-      startContract.createClient('another-type', customConfig);
-
-      expect(MockClusterClient).toHaveBeenCalledTimes(2);
+      expect(client.asInternalUser).toBe(mockClusterClientInstance.asInternalUser);
     });
 
-    it('falls back to elasticsearch default config values if property not specified', async () => {
-      await elasticsearchService.setup(setupDeps);
-      const startContract = await elasticsearchService.start();
-
-      // reset all mocks called during setup phase
-      MockClusterClient.mockClear();
-
-      const customConfig = {
-        hosts: ['http://8.8.8.8'],
-        logQueries: true,
-        ssl: { certificate: 'certificate-value' },
+    it('should log.error non-compatible nodes error', async () => {
+      const defaultMessage = {
+        isCompatible: true,
+        kibanaVersion: '8.0.0',
+        incompatibleNodes: [],
+        warningNodes: [],
       };
+      const observable$ = new BehaviorSubject<NodesVersionCompatibility>(defaultMessage);
 
-      startContract.createClient('some-custom-type', customConfig);
-      const config = MockClusterClient.mock.calls[0][0].config;
+      // @ts-expect-error this module is mocked, so `mockImplementation` is an allowed property
+      pollEsNodesVersionMocked.mockImplementation(() => observable$);
 
-      expect(config).toMatchInlineSnapshot(`
+      await elasticsearchService.setup(setupDeps);
+      tick();
+      await elasticsearchService.start();
+      expect(loggingSystemMock.collect(coreContext.logger).error).toEqual([]);
+      observable$.next({
+        ...defaultMessage,
+        isCompatible: false,
+        message: 'Something went terribly wrong!',
+      });
+      expect(loggingSystemMock.collect(coreContext.logger).error).toEqual([
+        ['Something went terribly wrong!'],
+      ]);
+    });
+
+    it('logs an info message about connecting to ES', async () => {
+      isValidConnectionMock.mockImplementation(async () => {
+        tick();
+      });
+
+      await elasticsearchService.setup(setupDeps);
+      await elasticsearchService.start();
+
+      expect(isValidConnectionMock).toHaveBeenCalledTimes(1);
+
+      const infoMessages = loggingSystemMock.collect(coreContext.logger).info;
+      expect(infoMessages).toHaveLength(1);
+
+      const esMessage = infoMessages[0][0];
+      expect(esMessage).toMatch(
+        /^Successfully connected to Elasticsearch after waiting for ([0-9]+) milliseconds$/
+      );
+    });
+
+    it('returns the information about the time spent waiting for Elasticsearch', async () => {
+      isValidConnectionMock.mockImplementation(async () => {
+        tick();
+      });
+
+      await elasticsearchService.setup(setupDeps);
+      const startContract = await elasticsearchService.start();
+
+      expect(isValidConnectionMock).toHaveBeenCalledTimes(1);
+
+      const infoMessages = loggingSystemMock.collect(coreContext.logger).info;
+      expect(infoMessages).toHaveLength(1);
+
+      const regexp =
+        /^Successfully connected to Elasticsearch after waiting for ([0-9]+) milliseconds$/;
+
+      const esMessage = infoMessages[0][0];
+      const groups = regexp.exec(esMessage);
+      const esWaitTime = parseInt(groups![1], 10);
+
+      expect(startContract.metrics.elasticsearchWaitTime).toEqual(esWaitTime);
+    });
+
+    describe('skipStartupConnectionCheck', () => {
+      it('should validate the connection by default', async () => {
+        await elasticsearchService.setup(setupDeps);
+        expect(isValidConnectionMock).not.toHaveBeenCalled();
+        await elasticsearchService.start();
+        expect(isValidConnectionMock).toHaveBeenCalledTimes(1);
+      });
+
+      it('should validate the connection when `false`', async () => {
+        mockConfig$.next({
+          ...(await mockConfig$.pipe(first()).toPromise()),
+          skipStartupConnectionCheck: false,
+        });
+        await elasticsearchService.setup(setupDeps);
+        expect(isValidConnectionMock).not.toHaveBeenCalled();
+        await elasticsearchService.start();
+        expect(isValidConnectionMock).toHaveBeenCalledTimes(1);
+      });
+
+      it('should not validate the connection when `true`', async () => {
+        mockConfig$.next({
+          ...(await mockConfig$.pipe(first()).toPromise()),
+          skipStartupConnectionCheck: true,
+        });
+        await elasticsearchService.setup(setupDeps);
+        expect(isValidConnectionMock).not.toHaveBeenCalled();
+        await elasticsearchService.start();
+        expect(isValidConnectionMock).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('isInlineScriptingEnabled', () => {
+      it('does not throw error when scripting is enabled', async () => {
+        isScriptingEnabledMock.mockResolvedValue(true);
+
+        await elasticsearchService.setup(setupDeps);
+        expect(isScriptingEnabledMock).not.toHaveBeenCalled();
+
+        await expect(elasticsearchService.start()).resolves.toBeDefined();
+        expect(isScriptingEnabledMock).toHaveBeenCalledTimes(1);
+      });
+
+      it('throws an error if scripting is disabled', async () => {
+        isScriptingEnabledMock.mockResolvedValue(false);
+
+        await elasticsearchService.setup(setupDeps);
+
+        await expect(elasticsearchService.start()).rejects.toThrowError(
+          'Inline scripting is disabled'
+        );
+      });
+
+      it('does not throw error when `skipStartupConnectionCheck` is true', async () => {
+        isScriptingEnabledMock.mockResolvedValue(false);
+        mockConfig$.next({
+          ...(await mockConfig$.pipe(first()).toPromise()),
+          skipStartupConnectionCheck: true,
+        });
+
+        await elasticsearchService.setup(setupDeps);
+        await expect(elasticsearchService.start()).resolves.toBeDefined();
+      });
+    });
+
+    describe('#createClient', () => {
+      it('allows to specify config properties', async () => {
+        await elasticsearchService.setup(setupDeps);
+        const startContract = await elasticsearchService.start();
+
+        // reset all mocks called during setup phase
+        MockClusterClient.mockClear();
+
+        const customConfig = { keepAlive: true };
+        const clusterClient = startContract.createClient('custom-type', customConfig);
+
+        expect(clusterClient).toBe(mockClusterClientInstance);
+
+        expect(MockClusterClient).toHaveBeenCalledTimes(1);
+        expect(MockClusterClient.mock.calls[0][0]).toEqual(
+          expect.objectContaining({ config: expect.objectContaining(customConfig) })
+        );
+      });
+      it('creates a new client on each call', async () => {
+        await elasticsearchService.setup(setupDeps);
+        const startContract = await elasticsearchService.start();
+
+        // reset all mocks called during setup phase
+        MockClusterClient.mockClear();
+
+        const customConfig = { keepAlive: true };
+
+        startContract.createClient('custom-type', customConfig);
+        startContract.createClient('another-type', customConfig);
+
+        expect(MockClusterClient).toHaveBeenCalledTimes(2);
+      });
+
+      it('falls back to elasticsearch default config values if property not specified', async () => {
+        await elasticsearchService.setup(setupDeps);
+        const startContract = await elasticsearchService.start();
+
+        // reset all mocks called during setup phase
+        MockClusterClient.mockClear();
+
+        const customConfig = {
+          hosts: ['http://8.8.8.8'],
+          logQueries: true,
+          ssl: { certificate: 'certificate-value' },
+        };
+
+        startContract.createClient('some-custom-type', customConfig);
+        const config = MockClusterClient.mock.calls[0][0].config;
+
+        expect(config).toMatchInlineSnapshot(`
         Object {
           "healthCheckDelay": "PT0.01S",
           "healthCheckRetry": 1,
@@ -484,41 +485,42 @@ describe('#start', () => {
           },
         }
       `);
+      });
     });
   });
-});
 
-describe('#stop', () => {
-  it('stops both legacy and new clients', async () => {
-    await elasticsearchService.setup(setupDeps);
-    await elasticsearchService.start();
-    await elasticsearchService.stop();
+  describe('#stop', () => {
+    it('stops both legacy and new clients', async () => {
+      await elasticsearchService.setup(setupDeps);
+      await elasticsearchService.start();
+      await elasticsearchService.stop();
 
-    expect(mockClusterClientInstance.close).toHaveBeenCalledTimes(1);
-  });
+      expect(mockClusterClientInstance.close).toHaveBeenCalledTimes(1);
+    });
 
-  it('stops pollEsNodeVersions even if there are active subscriptions', async () => {
-    expect.assertions(3);
+    it('stops pollEsNodeVersions even if there are active subscriptions', async () => {
+      expect.assertions(3);
 
-    const mockedClient = mockClusterClientInstance.asInternalUser;
-    mockedClient.nodes.info.mockResolvedValue({
-      nodes: {},
-    } as any);
+      const mockedClient = mockClusterClientInstance.asInternalUser;
+      mockedClient.nodes.info.mockResolvedValue({
+        nodes: {},
+      } as any);
 
-    const setupContract = await elasticsearchService.setup(setupDeps);
+      const setupContract = await elasticsearchService.setup(setupDeps);
 
-    await firstValueFrom(
-      setupContract.esNodesCompatibility$.pipe(
-        concatMap(async () => {
-          expect(mockedClient.nodes.info).toHaveBeenCalledTimes(1);
-          await tick();
-          expect(mockedClient.nodes.info).toHaveBeenCalledTimes(2);
+      await firstValueFrom(
+        setupContract.esNodesCompatibility$.pipe(
+          concatMap(async () => {
+            expect(mockedClient.nodes.info).toHaveBeenCalledTimes(1);
+            await tick();
+            expect(mockedClient.nodes.info).toHaveBeenCalledTimes(2);
 
-          await elasticsearchService.stop();
-          await tick(10);
-          expect(mockedClient.nodes.info).toHaveBeenCalledTimes(2);
-        })
-      )
-    );
+            await elasticsearchService.stop();
+            await tick(10);
+            expect(mockedClient.nodes.info).toHaveBeenCalledTimes(2);
+          })
+        )
+      );
+    });
   });
 });
