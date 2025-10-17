elastic/kibana

commit 88db9718f6d33e86a3c9a61893146e50512d606f

[Discover] SCSS to emotion migration - shared modules (#227946)

src/platform/packages/shared/kbn-unified-data-table/src/components/data_table.tsx
@@ -10,7 +10,7 @@
 import React, { ComponentProps, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import classnames from 'classnames';
 import { FormattedMessage } from '@kbn/i18n-react';
-import './data_table.scss';
+import { css } from '@emotion/react';
 import type { Storage } from '@kbn/kibana-utils-plugin/public';
 import {
   EuiDataGridSorting,
@@ -27,7 +27,9 @@ import {
   EuiDataGridStyle,
   EuiDataGridProps,
   EuiDataGridToolBarVisibilityDisplaySelectorOptions,
+  type UseEuiTheme,
 } from '@elastic/eui';
+import { useMemoCss } from '@kbn/css-utils/public/use_memo_css';
 import type { DataView } from '@kbn/data-views-plugin/public';
 import {
   useDataGridColumnsCellActions,
@@ -514,6 +516,7 @@ const InternalUnifiedDataTable = ({
   onUpdateDataGridDensity,
   onUpdatePageIndex,
 }: InternalUnifiedDataTableProps) => {
+  const styles = useMemoCss(componentStyles);
   const { fieldFormats, toastNotifications, dataViewFieldEditor, uiSettings, storage, data } =
     services;
   const dataGridRef = useRef<EuiDataGridRefProps>(null);
@@ -1217,7 +1220,11 @@ const InternalUnifiedDataTable = ({
 
   if (!rowCount && loadingState === DataLoadingState.loading) {
     return (
-      <div className="euiDataGrid__loading" data-test-subj="unifiedDataTableLoading">
+      <div
+        className="euiDataGrid__loading"
+        css={styles.loadingAndEmpty}
+        data-test-subj="unifiedDataTableLoading"
+      >
         <EuiText size="xs" color="subdued">
           <EuiLoadingSpinner />
           <EuiSpacer size="s" />
@@ -1231,6 +1238,7 @@ const InternalUnifiedDataTable = ({
     return (
       <div
         className="euiDataGrid__noResults"
+        css={styles.loadingAndEmpty}
         data-render-complete={isRenderComplete}
         data-shared-item=""
         data-title={searchTitle}
@@ -1251,7 +1259,7 @@ const InternalUnifiedDataTable = ({
 
   return (
     <UnifiedDataTableContext.Provider value={unifiedDataTableContextValue}>
-      <span className="unifiedDataTable__inner">
+      <span className="unifiedDataTable__inner" css={styles.dataTableInner}>
         <div
           ref={setDataGridWrapper}
           key={isCompareActive ? 'comparisonTable' : 'docTable'}
@@ -1263,7 +1271,7 @@ const InternalUnifiedDataTable = ({
           data-description={searchDescription}
           data-document-number={displayedRows.length}
           className={classnames(className, 'unifiedDataTable__table')}
-          css={inTableSearchTermCss}
+          css={[styles.dataTable, inTableSearchTermCss]}
         >
           {isCompareActive ? (
             <CompareDocuments
@@ -1360,3 +1368,79 @@ const InternalUnifiedDataTable = ({
 export const UnifiedDataTable = withRestorableState(InternalUnifiedDataTable);
 
 export type UnifiedDataTableProps = ComponentProps<typeof UnifiedDataTable>;
+
+const componentStyles = {
+  loadingAndEmpty: css({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: '1 0 100%',
+    height: '100%',
+    width: '100%',
+  }),
+  dataTableInner: ({ euiTheme }: UseEuiTheme) =>
+    css({
+      display: 'flex',
+      flexDirection: 'column',
+      flexWrap: 'nowrap',
+      height: '100%',
+      '.unifiedDataTable__cell--highlight': {
+        backgroundColor: euiTheme.colors.backgroundBaseWarning,
+      },
+      '.unifiedDataTable__cell--expanded': {
+        backgroundColor: euiTheme.colors.highlight,
+      },
+      '.euiDataGrid__content': {
+        background: 'transparent',
+      },
+      '.euiDataGrid--bordersHorizontal .euiDataGridHeader': {
+        borderTop: 'none',
+      },
+      '.euiDataGrid--headerUnderline .euiDataGridHeader': {
+        borderBottom: euiTheme.border.thin,
+      },
+      '.euiDataGridRowCell--controlColumn .euiDataGridRowCell__content, .euiDataGridRowCell.euiDataGridRowCell--controlColumn[data-gridcell-column-id="openDetails"], .euiDataGridRowCell.euiDataGridRowCell--controlColumn[data-gridcell-column-id="select"], .euiDataGridRowCell.euiDataGridRowCell--controlColumn[data-gridcell-column-id^="additionalRowControl_"], .euiDataGridHeaderCell.euiDataGridHeaderCell--controlColumn[data-gridcell-column-id^="additionalRowControl_"]':
+        {
+          paddingLeft: 0,
+          paddingRight: 0,
+          borderLeft: 0,
+          borderRight: 0,
+        },
+      '.euiDataGridRowCell.euiDataGridRowCell--controlColumn[data-gridcell-column-id="additionalRowControl_menuControl"] .euiDataGridRowCell__content':
+        {
+          paddingBottom: 0,
+        },
+      '.euiDataGridHeaderCell.euiDataGridHeaderCell--controlColumn[data-gridcell-column-id="select"]':
+        {
+          paddingLeft: euiTheme.size.xs,
+          paddingRight: 0,
+        },
+      '.euiDataGridHeaderCell.euiDataGridHeaderCell--controlColumn[data-gridcell-column-id="colorIndicator"], .euiDataGridRowCell.euiDataGridRowCell--controlColumn[data-gridcell-column-id="colorIndicator"]':
+        {
+          padding: 0,
+          borderLeft: 0,
+          borderRight: 0,
+        },
+      '.euiDataGridRowCell.euiDataGridRowCell--controlColumn[data-gridcell-column-id="colorIndicator"] .euiDataGridRowCell__content':
+        {
+          height: '100%',
+          borderBottom: 0,
+        },
+      '.euiDataGrid--rowHoverHighlight .euiDataGridRow:hover': {
+        backgroundColor: euiTheme.colors.lightestShade, // we keep using a deprecated shade until a proper token is available
+      },
+      '.euiDataGrid__scrollOverlay .euiDataGrid__scrollBarOverlayRight': {
+        backgroundColor: 'transparent', // otherwise the grid scrollbar border visually conflicts with the grid toolbar controls
+      },
+      '.euiDataGridRowCell__content--autoHeight, .euiDataGridRowCell__content--lineCountHeight, .euiDataGridHeaderCell__content':
+        {
+          whiteSpace: 'pre-wrap',
+        },
+    }),
+  dataTable: css({
+    flexGrow: 1,
+    flexShrink: 1,
+    minHeight: 0,
+  }),
+};

src/platform/plugins/shared/discover/public/application/main/components/layout/discover_documents.tsx
@@ -15,9 +15,11 @@ import {
   EuiScreenReaderOnly,
   EuiSpacer,
   EuiText,
+  type UseEuiTheme,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { css } from '@emotion/react';
+import { useMemoCss } from '@kbn/css-utils/public/use_memo_css';
 import type { DataView } from '@kbn/data-views-plugin/public';
 import type { SortOrder } from '@kbn/saved-search-plugin/public';
 import { CellActionsProvider } from '@kbn/cell-actions';
@@ -109,6 +111,7 @@ function DiscoverDocumentsComponent({
   stateContainer: DiscoverStateContainer;
   onFieldEdited?: () => void;
 }) {
+  const styles = useMemoCss(componentStyles);
   const services = useDiscoverServices();
   const { scopedEBTManager } = useScopedServices();
   const dispatch = useInternalStateDispatch();
@@ -382,7 +385,7 @@ function DiscoverDocumentsComponent({
           css={styles.progress}
         />
       ) : null,
-    [isDataLoading]
+    [isDataLoading, styles.progress]
   );
 
   const renderCustomToolbarWithElements = useMemo(
@@ -428,7 +431,7 @@ function DiscoverDocumentsComponent({
           <FormattedMessage id="discover.documentsAriaLabel" defaultMessage="Documents" />
         </h2>
       </EuiScreenReaderOnly>
-      <div className="unifiedDataTable">
+      <div className="unifiedDataTable" css={styles.dataTable}>
         <CellActionsProvider getTriggerCompatibleActions={uiActions.getTriggerCompatibleActions}>
           <DiscoverGridMemoized
             ariaLabelledBy="documentsAriaLabel"
@@ -496,7 +499,7 @@ function DiscoverDocumentsComponent({
 
 export const DiscoverDocuments = memo(DiscoverDocumentsComponent);
 
-const styles = {
+const componentStyles = {
   container: css({
     position: 'relative',
     minHeight: 0,
@@ -512,4 +515,53 @@ const styles = {
     height: '100%',
     width: '100%',
   }),
+  // Following guidelines for CSS-in-JS - styles for high granularity components should be assigned to a parent and targeting classes of repeating children
+  dataTable: ({ euiTheme }: UseEuiTheme) =>
+    css({
+      width: '100%',
+      maxWidth: '100%',
+      height: '100%',
+      overflow: 'hidden',
+
+      '.unifiedDataTable__cell--highlight': {
+        backgroundColor: euiTheme.colors.backgroundBaseWarning,
+      },
+
+      '.unifiedDataTable__cell--expanded': {
+        backgroundColor: euiTheme.colors.highlight,
+      },
+
+      '.unifiedDataTable__cellValue': {
+        fontFamily: euiTheme.font.familyCode,
+      },
+
+      // Custom styles for data grid header cell.
+      // It can also be inside a portal (outside of `unifiedDataTable__inner`) when dragged.
+      '.unifiedDataTable__headerCell': {
+        alignItems: 'start',
+
+        '.euiDataGridHeaderCell__draggableIcon': {
+          paddingBlock: `calc(${euiTheme.size.xs} / 2)`, // to align with a token height
+        },
+        '.euiDataGridHeaderCell__button': {
+          marginBlock: -euiTheme.size.xs, // to override Eui value for Density "Expanded"
+        },
+      },
+
+      '.unifiedDataTable__rowControl': {
+        marginTop: -1, // fine-tuning the vertical alignment with the text for any row height setting
+      },
+      // Compact density - 'auto & custom' row height
+      '.euiDataGrid--fontSizeSmall .euiDataGridRowCell__content:not(.euiDataGridRowCell__content--defaultHeight) .unifiedDataTable__rowControl':
+        {
+          marginTop: -2.5,
+        },
+
+      // Compact density - 'single' row height
+      '.euiDataGrid--fontSizeSmall .euiDataGridRowCell__content--defaultHeight .unifiedDataTable__rowControl':
+        {
+          alignSelf: 'flex-start',
+          marginTop: -3,
+        },
+    }),
 };

src/platform/plugins/shared/data/public/utils/table_inspector_view/components/data_table.tsx
@@ -211,18 +211,7 @@ class DataTableFormatClass extends Component<
         sorting={sorting}
         pagination={pagination}
         onTableChange={onTableChange}
-        css={css`
-          // Set a min width on each column - you can use [data-test-subj] to target specific columns
-          .euiTableHeaderCell {
-            min-width: 100px;
-          }
-
-          // Make sure the pagination follows the scroll
-          > div:last-child {
-            position: sticky;
-            left: 0;
-          }
-        `}
+        css={styles.table}
       />
     );
   }
@@ -233,3 +222,24 @@ export const DataTableFormat = withEuiTablePersist(DataTableFormatClass, {
   pageSizeOptions: PAGE_SIZE_OPTIONS,
   initialPageSize: 20,
 });
+
+const styles = {
+  table: css({
+    // Set a min width on each column
+    '.euiTableHeaderCell': {
+      minWidth: '100px',
+    },
+    // Make sure the pagination follows the scroll
+    '> div:last-child': {
+      position: 'sticky',
+      left: 0,
+    },
+    // Show filter buttons on row hover or focus
+    'tr:hover .insDataTableFormat__filter, .insDataTableFormat__filter:focus': {
+      opacity: 1,
+    },
+    '.insDataTableFormat__filter': {
+      opacity: 0,
+    },
+  }),
+};

src/platform/packages/shared/kbn-unified-data-table/src/components/compare_documents/comparison_toolbar.test.tsx
@@ -9,6 +9,7 @@
 
 import { cleanup, render, screen } from '@testing-library/react';
 import React from 'react';
+import { EuiThemeProvider } from '@elastic/eui';
 import { renderComparisonToolbar } from './comparison_toolbar';
 
 const renderToolbar = ({
@@ -25,16 +26,18 @@ const renderToolbar = ({
     totalFields,
   });
   render(
-    <ComparisonToolbar
-      hasRoomForGridControls={hasRoomForGridControls}
-      keyboardShortcutsControl={
-        <div data-test-subj="keyboardShortcutsControl">keyboardShortcutsControl</div>
-      }
-      fullScreenControl={<div data-test-subj="fullScreenControl">fullScreenControl</div>}
-      columnControl={<div data-test-subj="columnControl">columnControl</div>}
-      columnSortingControl={<div data-test-subj="columnSortingControl">columnSortingControl</div>}
-      displayControl={<div data-test-subj="displayControl">displayControl</div>}
-    />
+    <EuiThemeProvider>
+      <ComparisonToolbar
+        hasRoomForGridControls={hasRoomForGridControls}
+        keyboardShortcutsControl={
+          <div data-test-subj="keyboardShortcutsControl">keyboardShortcutsControl</div>
+        }
+        fullScreenControl={<div data-test-subj="fullScreenControl">fullScreenControl</div>}
+        columnControl={<div data-test-subj="columnControl">columnControl</div>}
+        columnSortingControl={<div data-test-subj="columnSortingControl">columnSortingControl</div>}
+        displayControl={<div data-test-subj="displayControl">displayControl</div>}
+      />
+    </EuiThemeProvider>
   );
   return {
     getAdditionalControls: () => screen.queryByTestId('additionalControls'),

src/platform/packages/shared/kbn-unified-data-table/src/components/custom_toolbar/render_custom_toolbar.scss
@@ -1,67 +1,0 @@
-.unifiedDataTableToolbar {
-  padding: $euiSizeS $euiSizeS $euiSizeXS;
-
-  .unifiedDataTableToolbarControlButton {
-    .euiDataGridToolbarControl {
-      block-size: $euiSizeXL;
-      border: $euiBorderWidthThin solid $euiFormBorderColor;
-      border-radius: $euiBorderRadiusSmall;
-
-      // making the icons larger than the default size
-      & svg {
-        inline-size: $euiSize;
-        block-size: $euiSize;
-      }
-    }
-  }
-
-  .unifiedDataTableToolbarControlGroup {
-    position: relative;
-    overflow: hidden;
-    border-radius: $euiBorderRadiusSmall;
-    display: inline-flex;
-    align-items: stretch;
-    flex-direction: row;
-
-    &::after {
-      content: '';
-      position: absolute;
-      inset: 0;
-      border: $euiBorderWidthThin solid $euiColorBorderBasePlain;
-      border-radius: inherit;
-      pointer-events: none;
-    }
-
-    & .unifiedDataTableToolbarControlButton .euiDataGridToolbarControl {
-      border-radius: 0;
-      border: none;
-    }
-
-    & .unifiedDataTableToolbarControlButton + .unifiedDataTableToolbarControlButton {
-      border-inline-start: $euiBorderThin;
-      border-radius: 0;
-    }
-  }
-
-  .unifiedDataTableToolbarControlIconButton .euiToolTipAnchor .euiButtonIcon {
-    inline-size: $euiSizeXL;
-    block-size: $euiSizeXL;
-    border-radius: inherit;
-
-    // cancel default behaviour
-    &:hover, &:active, &:focus {
-      background: transparent;
-      animation: none !important;
-      transform: none !important;
-    }
-  }
-
-  .unifiedDataTableToolbarControlIconButton + .unifiedDataTableToolbarControlIconButton {
-    border-inline-start: $euiBorderWidthThin solid $euiColorBorderBasePlain;
-    border-radius: 0;
-  }
-
-  .unifiedDataTableToolbarBottom {
-    position: relative; // for placing a loading indicator correctly
-  }
-}

src/platform/packages/shared/kbn-unified-data-table/src/components/custom_toolbar/render_custom_toolbar.tsx
@@ -8,8 +8,13 @@
  */
 
 import React from 'react';
-import { EuiDataGridCustomToolbarProps, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
-import './render_custom_toolbar.scss';
+import {
+  EuiDataGridCustomToolbarProps,
+  EuiFlexGroup,
+  EuiFlexItem,
+  type UseEuiTheme,
+} from '@elastic/eui';
+import { css } from '@emotion/react';
 
 export interface UnifiedDataTableRenderCustomToolbarProps {
   toolbarProps: EuiDataGridCustomToolbarProps;
@@ -54,12 +59,16 @@ export const internalRenderCustomToolbar = (
       )}
       {columnControl && (
         <EuiFlexItem grow={false}>
-          <div className="unifiedDataTableToolbarControlButton">{columnControl}</div>
+          <div className="unifiedDataTableToolbarControlButton" css={styles.controlButton}>
+            {columnControl}
+          </div>
         </EuiFlexItem>
       )}
       {columnSortingControl && (
         <EuiFlexItem grow={false}>
-          <div className="unifiedDataTableToolbarControlButton">{columnSortingControl}</div>
+          <div className="unifiedDataTableToolbarControlButton" css={styles.controlButton}>
+            {columnSortingControl}
+          </div>
         </EuiFlexItem>
       )}
       {!leftSide && additionalControls && (
@@ -78,6 +87,7 @@ export const internalRenderCustomToolbar = (
         justifyContent="spaceBetween"
         alignItems="center"
         className="unifiedDataTableToolbar"
+        css={styles.toolbar}
         data-test-subj="unifiedDataTableToolbar"
         wrap
       >
@@ -98,22 +108,36 @@ export const internalRenderCustomToolbar = (
                 inTableSearchControl
             ) && (
               <EuiFlexItem grow={false}>
-                <div className="unifiedDataTableToolbarControlGroup">
+                <div className="unifiedDataTableToolbarControlGroup" css={styles.controlGroup}>
                   {Boolean(inTableSearchControl) && (
-                    <div className="unifiedDataTableToolbarControlIconButton">
+                    <div
+                      className="unifiedDataTableToolbarControlIconButton"
+                      css={styles.controlGroupIconButton}
+                    >
                       {inTableSearchControl}
                     </div>
                   )}
                   {Boolean(keyboardShortcutsControl) && (
-                    <div className="unifiedDataTableToolbarControlIconButton">
+                    <div
+                      className="unifiedDataTableToolbarControlIconButton"
+                      css={styles.controlGroupIconButton}
+                    >
                       {keyboardShortcutsControl}
                     </div>
                   )}
                   {Boolean(displayControl) && (
-                    <div className="unifiedDataTableToolbarControlIconButton">{displayControl}</div>
+                    <div
+                      className="unifiedDataTableToolbarControlIconButton"
+                      css={styles.controlGroupIconButton}
+                    >
+                      {displayControl}
+                    </div>
                   )}
                   {Boolean(fullScreenControl) && (
-                    <div className="unifiedDataTableToolbarControlIconButton">
+                    <div
+                      className="unifiedDataTableToolbarControlIconButton"
+                      css={styles.controlGroupIconButton}
+                    >
                       {fullScreenControl}
                     </div>
                   )}
@@ -126,6 +150,7 @@ export const internalRenderCustomToolbar = (
       {bottomSection ? (
         <div
           className="unifiedDataTableToolbarBottom"
+          css={styles.toolbarBottom}
           data-test-subj="unifiedDataTableToolbarBottom"
         >
           {bottomSection}
@@ -154,4 +179,70 @@ export const getRenderCustomToolbarWithElements = ({
       leftSide: leftSide || reservedSpace,
       bottomSection,
     });
+};
+
+const styles = {
+  toolbar: ({ euiTheme }: UseEuiTheme) =>
+    css({
+      padding: `${euiTheme.size.s} ${euiTheme.size.s} ${euiTheme.size.xs}`,
+    }),
+  controlButton: ({ euiTheme }: UseEuiTheme) =>
+    css({
+      '.euiDataGridToolbarControl': {
+        blockSize: euiTheme.size.xl,
+        border: `${euiTheme.border.width.thin} solid ${euiTheme.colors.backgroundBaseFormsControlDisabled}`,
+        borderRadius: euiTheme.border.radius.small,
+
+        // making the icons larger than the default size
+        '& svg': {
+          inlineSize: euiTheme.size.base,
+          blockSize: euiTheme.size.base,
+        },
+      },
+    }),
+  controlGroup: ({ euiTheme }: UseEuiTheme) =>
+    css({
+      position: 'relative',
+      overflow: 'hidden',
+      borderRadius: euiTheme.border.radius.small,
+      display: 'inline-flex',
+      alignItems: 'stretch',
+      flexDirection: 'row',
+
+      '&::after': {
+        content: '""',
+        position: 'absolute',
+        inset: 0,
+        border: `${euiTheme.border.width.thin} solid ${euiTheme.colors.borderBasePlain}`,
+        borderRadius: 'inherit',
+        pointerEvents: 'none',
+      },
+
+      '& .unifiedDataTableToolbarControlIconButton .euiDataGridToolbarControl': {
+        borderRadius: 0,
+        border: 'none',
+      },
+
+      '& .unifiedDataTableToolbarControlIconButton + .unifiedDataTableToolbarControlIconButton': {
+        borderInlineStart: `${euiTheme.border.width.thin} solid ${euiTheme.colors.borderBasePlain}`,
+      },
+    }),
+  controlGroupIconButton: ({ euiTheme }: UseEuiTheme) =>
+    css({
+      '.euiToolTipAnchor .euiButtonIcon': {
+        inlineSize: euiTheme.size.xl,
+        blockSize: euiTheme.size.xl,
+        borderRadius: 'inherit',
+
+        // cancel default behavior
+        '&:hover, &:active, &:focus': {
+          background: 'transparent',
+          animation: 'none !important',
+          transform: 'none !important',
+        },
+      },
+    }),
+  toolbarBottom: css({
+    position: 'relative', // for placing a loading indicator correctly
+  }),
 };

commit 9b8ed28c8bd73ce5c9772db8bbcd8553c2eb7d30

[Discover] Fix data grid styles (#239148)
- Closes https://github.com/elastic/kibana/issues/239034

## Summary

This PR fixes missing styles for discover grid on Dashboard. The issue
was introduced in https://github.com/elastic/kibana/pull/227946


### Checklist

- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
- [x] Review the [backport
guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
and apply applicable `backport:*` labels.

src/platform/packages/shared/kbn-unified-data-table/src/components/data_table.tsx
@@ -18,7 +18,7 @@ import React, {
 } from 'react';
 import classnames from 'classnames';
 import { FormattedMessage } from '@kbn/i18n-react';
-import { css } from '@emotion/react';
+import { Global, css } from '@emotion/react';
 import type { Storage } from '@kbn/kibana-utils-plugin/public';
 import type {
   EuiDataGridSorting,
@@ -1309,6 +1309,7 @@ const InternalUnifiedDataTable = React.forwardRef<
     return (
       <UnifiedDataTableContext.Provider value={unifiedDataTableContextValue}>
         <span className="unifiedDataTable__inner" css={styles.dataTableInner}>
+          <Global styles={styles.dataTableGlobal} />
           <div
             ref={setDataGridWrapper}
             key={isCompareActive ? 'comparisonTable' : 'docTable'}
@@ -1429,6 +1430,21 @@ const componentStyles = {
     height: '100%',
     width: '100%',
   }),
+  dataTableGlobal: ({ euiTheme }: UseEuiTheme) =>
+    css({
+      // Custom styles for data grid header cell.
+      // It can also be inside a portal (outside of `unifiedDataTable__inner`) when dragged.
+      '.unifiedDataTable__headerCell': {
+        alignItems: 'start !important',
+
+        '.euiDataGridHeaderCell__draggableIcon': {
+          paddingBlock: `calc(${euiTheme.size.xs} / 2) !important`, // to align with a token height
+        },
+        '.euiDataGridHeaderCell__button': {
+          marginTop: `-${euiTheme.size.xs} !important`, // to override Eui value for Density "Expanded"
+        },
+      },
+    }),
   dataTableInner: ({ euiTheme }: UseEuiTheme) =>
     css({
       display: 'flex',
@@ -1441,6 +1457,23 @@ const componentStyles = {
       '.unifiedDataTable__cell--expanded': {
         backgroundColor: euiTheme.colors.backgroundBaseInteractiveSelect,
       },
+      '.unifiedDataTable__cellValue': {
+        fontFamily: euiTheme.font.familyCode,
+      },
+      '.unifiedDataTable__rowControl': {
+        marginTop: -1, // fine-tuning the vertical alignment with the text for any row height setting
+      },
+      // Compact density - 'auto & custom' row height
+      '.euiDataGrid--fontSizeSmall .euiDataGridRowCell__content:not(.euiDataGridRowCell__content--defaultHeight) .unifiedDataTable__rowControl':
+        {
+          marginTop: -2.5,
+        },
+      // Compact density - 'single' row height
+      '.euiDataGrid--fontSizeSmall .euiDataGridRowCell__content--defaultHeight .unifiedDataTable__rowControl':
+        {
+          alignSelf: 'flex-start',
+          marginTop: -3,
+        },
       '.euiDataGrid__content': {
         background: 'transparent',
       },

src/platform/plugins/shared/discover/public/application/main/components/layout/discover_documents.tsx
@@ -15,7 +15,6 @@ import {
   EuiScreenReaderOnly,
   EuiSpacer,
   EuiText,
-  type UseEuiTheme,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { css } from '@emotion/react';
@@ -526,53 +525,10 @@ const componentStyles = {
     height: '100%',
     width: '100%',
   }),
-  // Following guidelines for CSS-in-JS - styles for high granularity components should be assigned to a parent and targeting classes of repeating children
-  dataTable: ({ euiTheme }: UseEuiTheme) =>
-    css({
-      width: '100%',
-      maxWidth: '100%',
-      height: '100%',
-      overflow: 'hidden',
-
-      '.unifiedDataTable__cell--highlight': {
-        backgroundColor: euiTheme.colors.backgroundBaseWarning,
-      },
-
-      '.unifiedDataTable__cell--expanded': {
-        backgroundColor: euiTheme.colors.highlight,
-      },
-
-      '.unifiedDataTable__cellValue': {
-        fontFamily: euiTheme.font.familyCode,
-      },
-
-      // Custom styles for data grid header cell.
-      // It can also be inside a portal (outside of `unifiedDataTable__inner`) when dragged.
-      '.unifiedDataTable__headerCell': {
-        alignItems: 'start',
-
-        '.euiDataGridHeaderCell__draggableIcon': {
-          paddingBlock: `calc(${euiTheme.size.xs} / 2)`, // to align with a token height
-        },
-        '.euiDataGridHeaderCell__button': {
-          marginBlock: -euiTheme.size.xs, // to override Eui value for Density "Expanded"
-        },
-      },
-
-      '.unifiedDataTable__rowControl': {
-        marginTop: -1, // fine-tuning the vertical alignment with the text for any row height setting
-      },
-      // Compact density - 'auto & custom' row height
-      '.euiDataGrid--fontSizeSmall .euiDataGridRowCell__content:not(.euiDataGridRowCell__content--defaultHeight) .unifiedDataTable__rowControl':
-        {
-          marginTop: -2.5,
-        },
-
-      // Compact density - 'single' row height
-      '.euiDataGrid--fontSizeSmall .euiDataGridRowCell__content--defaultHeight .unifiedDataTable__rowControl':
-        {
-          alignSelf: 'flex-start',
-          marginTop: -3,
-        },
-    }),
+  dataTable: css({
+    width: '100%',
+    maxWidth: '100%',
+    height: '100%',
+    overflow: 'hidden',
+  }),
 };