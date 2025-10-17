425fade96a8085b7915d44168bf07cae25ea19d6
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2025-10-17 12:17:20 +0200

    refactor: use getIndexPattern instead of dataviewId; refresh data view

--- x-pack/solutions/observability/plugins/infra/public/utils/data_view.ts
+++ x-pack/solutions/observability/plugins/infra/public/utils/data_view.ts
@@ -16,6 +16,11 @@ export interface ResolvedDataView {
   timeFieldName: string;
 }
 
+interface AdhocDataView {
+  dataViewsService: DataViewsContract;
+  dataViewId: string;
+  attributes: DataViewAttributes;
+}
 interface PersistedDataView {
   dataViewsService: DataViewsContract;
   dataViewId: string;
@@ -25,6 +30,13 @@ interface DataViewAttributes {
   timeFieldName: string;
   name?: string;
 }
+
+const buildResolvedDataView = (dataViewReference: DataView): ResolvedDataView => ({
+  indices: dataViewReference.getIndexPattern(),
+  timeFieldName: dataViewReference.timeFieldName ?? TIMESTAMP_FIELD,
+  fields: dataViewReference.fields ?? [],
+  dataViewReference,
+});
 
 export const resolveDataView = async ({
   dataViewId,
@@ -51,20 +63,14 @@ export const resolvePersistedDataView = async ({
   dataViewId,
 }: PersistedDataView): Promise<ResolvedDataView> => {
   const dataView = await dataViewsService.get(dataViewId, false);
-
-  return {
-    indices: dataView.getIndexPattern(),
-    timeFieldName: dataView.timeFieldName ?? TIMESTAMP_FIELD,
-    fields: dataView.fields ?? [],
-    dataViewReference: dataView,
-  };
+  return buildResolvedDataView(dataView);
 };
 
 export const resolveAdHocDataView = async ({
   dataViewsService,
   dataViewId,
   attributes,
-}: PersistedDataView & { attributes: DataViewAttributes }): Promise<ResolvedDataView> => {
+}: AdhocDataView): Promise<ResolvedDataView> => {
   const { name, timeFieldName } = attributes;
 
   const dataViewReference = await dataViewsService.get(dataViewId, false, true).catch(() => {
@@ -80,10 +86,5 @@ export const resolveAdHocDataView = async ({
     );
   });
 
-  return {
-    indices: dataViewId,
-    timeFieldName: dataViewReference.timeFieldName ?? TIMESTAMP_FIELD,
-    fields: dataViewReference.fields ?? [],
-    dataViewReference,
-  };
+  return buildResolvedDataView(dataViewReference);
 };
