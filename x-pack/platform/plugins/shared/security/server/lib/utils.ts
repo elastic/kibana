42437a0aa56581806e02b7ec76dcd42823572d8e
Elena Shostak <elena.shostak@elastic.co>
2025-10-17 11:08:52 +0200

    wip

---
+++ x-pack/platform/plugins/shared/security/server/lib/utils.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export function escapeString(val: string): string {
+  return `"${val.replace(/["]/g, '\\"')}"`;
+}
+
+export function replaceAll(str: string, search: string, replace: string): string {
+  return str.replaceAll(`/`, `\/`).replaceAll(`.`, `\.`);
+}
