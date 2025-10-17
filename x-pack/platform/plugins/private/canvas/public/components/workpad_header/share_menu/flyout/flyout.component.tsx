a9e9ebcaf8ae3d6dac77138fb0d59fa0f82f12bc
paulina.shakirova <paulina.shakirova@elastic.co>
2025-10-17 14:25:26 +0200

    resolve manual conflict

--- x-pack/platform/plugins/private/canvas/public/components/workpad_header/share_menu/flyout/flyout.component.tsx
+++ x-pack/platform/plugins/private/canvas/public/components/workpad_header/share_menu/flyout/flyout.component.tsx
@@ -181,8 +181,7 @@ export const ShareWebsiteFlyout: FC<Props> = ({
       <EuiSpacer size="xs" key="spacer" />,
     ];
     warningText = [
-      <EuiCallOut title={warning} color="warning" size="s" iconType="warning" key="callout"
-      />
+      <EuiCallOut title={warning} color="warning" size="s" iconType="warning" key="callout" />,
       <EuiSpacer key="spacer" />,
     ];
   }
