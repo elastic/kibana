1d8ec31d71d17835adfa1fd7c8ade33d46003a2b
Alex Szabo <alex.szabo@elastic.co>
2025-10-17 15:27:15 +0200

    uncomment, but remove verify_moon_projects from quick-checks

--- .buildkite/scripts/steps/checks/verify_moon_projects.sh
+++ .buildkite/scripts/steps/checks/verify_moon_projects.sh
@@ -6,10 +6,10 @@ source .buildkite/scripts/common/util.sh
 
 echo --- Verify Moon projects are generated
 
-#if is_pr && ! is_auto_commit_disabled && [[ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ]]; then
-#  node scripts/regenerate_moon_projects.js --dependencies --update
-#  check_for_changed_files "node scripts/regenerate_moon_projects.js --dependencies --update" true
-#else
-#  node scripts/regenerate_moon_projects.js --dependencies --update
-#  exit 0
-#fi
+if is_pr && ! is_auto_commit_disabled && [[ "$BUILDKITE_PULL_REQUEST_BASE_BRANCH" == "main" ]]; then
+  node scripts/regenerate_moon_projects.js --update
+  check_for_changed_files "node scripts/regenerate_moon_projects.js --update" true
+else
+  node scripts/regenerate_moon_projects.js --update
+  exit 0
+fi
