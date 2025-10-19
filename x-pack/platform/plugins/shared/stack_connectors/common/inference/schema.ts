7881f282c7be3616d0a5711c330b8c2dc82b649b
Melissa <melissa.alvarez@elastic.co>
2025-10-19 11:07:31 -0700

    remove schema update done in a separate PR

--- x-pack/platform/plugins/shared/stack_connectors/common/inference/schema.ts
+++ x-pack/platform/plugins/shared/stack_connectors/common/inference/schema.ts
@@ -19,7 +19,6 @@ export const ConfigSchema = schema.object({
   providerConfig: schema.object({}, { unknowns: 'allow', defaultValue: {} }),
   taskTypeConfig: schema.object({}, { unknowns: 'allow', defaultValue: {} }),
   contextWindowLength: schema.maybe(schema.number({})),
-  headers: schema.maybe(schema.recordOf(schema.string(), schema.string())),
 });
 
 export const SecretsSchema = schema.object({
