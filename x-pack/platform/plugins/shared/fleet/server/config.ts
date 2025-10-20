d2de6e59a0e04d97d31f23f93541088ad0feaefe
Jen Huang <its.jenetic@gmail.com>
2025-10-20 11:25:56 -0700

    Add `installIntegrationsKnowledge` feature flag

--- x-pack/platform/plugins/shared/fleet/server/config.ts
+++ x-pack/platform/plugins/shared/fleet/server/config.ts
@@ -389,6 +389,7 @@ export const config: PluginConfigDescriptor = {
       integrationsHomeOverride: schema.maybe(schema.string()),
       prereleaseEnabledByDefault: schema.boolean({ defaultValue: false }),
       hideDashboards: schema.boolean({ defaultValue: false }),
+      installIntegrationsKnowledge: schema.boolean({ defaultValue: false }),
     },
     {
       validate: (configToValidate) => {
