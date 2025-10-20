eb6a433b7b750f6247082d06690b4a20d7bc4bf7
Matt Wilde <matt.wilde@elastic.co>
2025-10-20 10:03:07 -0400

    add data and go home button

---
+++ x-pack/solutions/search/plugins/search_getting_started/public/components/header/add_data_button.tsx
@@ -0,0 +1,91 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React, { useState } from 'react';
+import { EuiButton, EuiContextMenuPanel, EuiContextMenuItem, EuiPopover } from '@elastic/eui';
+
+import { i18n } from '@kbn/i18n';
+import { useKibana } from '../../hooks/use_kibana';
+
+export const AddDataButton: React.FC = () => {
+  const {
+    services: { application },
+  } = useKibana();
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const onButtonClick = () => {
+    setIsPopoverOpen(!isPopoverOpen);
+  };
+
+  const closePopover = () => {
+    setIsPopoverOpen(false);
+  };
+
+  const items = [
+    <EuiContextMenuItem
+      key="upload"
+      icon="exportAction"
+      onClick={() => {
+        closePopover();
+        application.navigateToUrl('/app/ml/filedatavisualizer');
+      }}
+    >
+      {i18n.translate('xpack.search.gettingStarted.addDataButton.uploadFile', {
+        defaultMessage: 'Upload a file',
+      })}
+    </EuiContextMenuItem>,
+    <EuiContextMenuItem
+      key="sample"
+      icon="folderOpen"
+      onClick={() => {
+        closePopover();
+        application.navigateToUrl('/app/home#/tutorial_directory/sampleData');
+      }}
+    >
+      {i18n.translate('xpack.search.gettingStarted.addDataButton.browseSampleData', {
+        defaultMessage: 'Browse sample data sets',
+      })}
+    </EuiContextMenuItem>,
+    <EuiContextMenuItem
+      key="empty"
+      icon="indexOpen"
+      onClick={() => {
+        closePopover();
+        application.navigateToUrl('/app/elasticsearch/indices/create');
+      }}
+    >
+      {i18n.translate('xpack.search.gettingStarted.addDataButton.createEmptyIndex', {
+        defaultMessage: 'Create an empty index',
+      })}
+    </EuiContextMenuItem>,
+  ];
+
+  const button = (
+    <EuiButton
+      color="primary"
+      fill={true}
+      iconType="arrowDown"
+      iconSide="right"
+      onClick={onButtonClick}
+    >
+      {i18n.translate('xpack.search.gettingStarted.addDataButton.label', {
+        defaultMessage: 'Add data',
+      })}
+    </EuiButton>
+  );
+
+  return (
+    <EuiPopover
+      button={button}
+      isOpen={isPopoverOpen}
+      closePopover={closePopover}
+      panelPaddingSize="none"
+      anchorPosition="downLeft"
+    >
+      <EuiContextMenuPanel items={items} />
+    </EuiPopover>
+  );
+};
--- x-pack/solutions/search/plugins/search_getting_started/public/components/header/index.tsx
+++ x-pack/solutions/search/plugins/search_getting_started/public/components/header/index.tsx
@@ -6,7 +6,6 @@
  */
 import React from 'react';
 import {
-  EuiPageTemplate,
   EuiFlexGroup,
   EuiFlexItem,
   EuiImage,
@@ -14,65 +13,69 @@ import {
   EuiTitle,
   EuiSpacer,
   EuiText,
-  EuiButton,
   EuiButtonEmpty,
 } from '@elastic/eui';
 
 import { i18n } from '@kbn/i18n';
 import { useAssetBasePath } from '../../hooks/use_asset_base_path';
+import { useKibana } from '../../hooks/use_kibana';
 import { PLUGIN_NAME } from '../../../common';
 import { SearchGettingStartedSectionHeading } from '../section_heading';
+import { AddDataButton } from './add_data_button';
 
 export const SearchGettingStartedHeader: React.FC = () => {
   const assetBasePath = useAssetBasePath();
+  const {
+    services: { application },
+  } = useKibana();
 
   return (
-    <EuiPageTemplate.Section data-test-subj="search-getting-started-header" paddingSize="none">
-      <EuiFlexGroup gutterSize="m" alignItems="stretch">
-        <EuiFlexItem style={{ alignSelf: 'center' }}>
-          <EuiPanel color="transparent">
-            <SearchGettingStartedSectionHeading title={PLUGIN_NAME} icon="launch" />
-            <EuiSpacer size="s" />
-            <EuiTitle size="l">
-              <h1>
-                {i18n.translate('xpack.search.gettingStarted.page.title', {
-                  defaultMessage: 'Start your building with Elasticsearch.',
+    <EuiFlexGroup gutterSize="m" alignItems="stretch">
+      <EuiFlexItem style={{ alignSelf: 'center' }}>
+        <EuiPanel color="transparent" paddingSize="none">
+          <SearchGettingStartedSectionHeading title={PLUGIN_NAME} icon="launch" />
+          <EuiSpacer size="s" />
+          <EuiTitle size="l">
+            <h1>
+              {i18n.translate('xpack.search.gettingStarted.page.title', {
+                defaultMessage: 'Start your building with Elasticsearch.',
+              })}
+            </h1>
+          </EuiTitle>
+          <EuiSpacer size="m" />
+          <EuiText grow={false}>
+            <p>
+              {i18n.translate('xpack.search.gettingStarted.page.description', {
+                defaultMessage:
+                  'Dive into an API tutorial or connect your deployment to start building.',
+              })}
+            </p>
+          </EuiText>
+          <EuiSpacer size="l" />
+          <EuiFlexGroup gutterSize="m" alignItems="stretch">
+            <EuiFlexItem grow={false}>
+              <AddDataButton />
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <EuiButtonEmpty
+                data-test-subj="elasticLLMCostsTourCloseBtn"
+                onClick={() => {
+                  application.navigateToUrl('/app/elasticsearch/home');
+                }}
+                color="text"
+              >
+                {i18n.translate('xpack.search.gettingStarted.page.headerCtaEmpty', {
+                  defaultMessage: 'Skip and go to Home',
                 })}
-              </h1>
-            </EuiTitle>
-            <EuiSpacer size="m" />
-            <EuiText grow={false}>
-              <p>
-                {i18n.translate('xpack.search.gettingStarted.page.description', {
-                  defaultMessage:
-                    'Dive into an API tutorial or connect your deployment to start building.',
-                })}
-              </p>
-            </EuiText>
-            <EuiSpacer size="l" />
-            <EuiFlexGroup gutterSize="m" alignItems="stretch">
-              <EuiFlexItem grow={false}>
-                <EuiButton color="primary" fill={true} iconType="arrowDown" iconSide='right'>
-                  {i18n.translate('xpack.search.gettingStarted.page.headerCTA', {
-                    defaultMessage: 'Add data',
-                  })}
-                </EuiButton>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiButtonEmpty data-test-subj="elasticLLMCostsTourCloseBtn" onClick={() => {}} color='text'>
-                  {i18n.translate('xpack.search.gettingStarted.page.headerCtaEmpty', {
-                    defaultMessage: 'Skip and go to Home',
-                  })}
-                </EuiButtonEmpty>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiPanel>
-        </EuiFlexItem>
+              </EuiButtonEmpty>
+            </EuiFlexItem>
+          </EuiFlexGroup>
+        </EuiPanel>
+      </EuiFlexItem>
 
-        <EuiFlexItem>
-          <EuiImage size="xl" src={`${assetBasePath}/search_getting_started_header.svg`} alt="" />
-        </EuiFlexItem>
-      </EuiFlexGroup>
-    </EuiPageTemplate.Section>
+      <EuiFlexItem>
+        <EuiImage size="xl" src={`${assetBasePath}/search_getting_started_header.svg`} alt="" />
+      </EuiFlexItem>
+    </EuiFlexGroup>
   );
 };
