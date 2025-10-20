0438b89af3be079845212280cc7364020d9c8d05
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-10-20 16:21:39 +0000

    [CI] Auto-commit changed files from 'node scripts/eslint_all_files --no-cache --fix'

--- x-pack/platform/packages/shared/kbn-elastic-assistant/impl/assistant/assistant_body/starter_prompts.tsx
+++ x-pack/platform/packages/shared/kbn-elastic-assistant/impl/assistant/assistant_body/starter_prompts.tsx
@@ -9,8 +9,8 @@ import React, { useMemo, useCallback } from 'react';
 import { EuiFlexGroup, EuiFlexItem, EuiIcon, EuiPanel, EuiSpacer, EuiText } from '@elastic/eui';
 import { css } from '@emotion/css';
 import type { PromptItemArray } from '@kbn/elastic-assistant-common/impl/schemas/security_ai_prompts/common_attributes.gen';
-import { useAssistantContext, useFindPrompts } from '../../..';
 import { kebabCase } from 'lodash';
+import { useAssistantContext, useFindPrompts } from '../../..';
 
 interface Props {
   connectorId?: string;
