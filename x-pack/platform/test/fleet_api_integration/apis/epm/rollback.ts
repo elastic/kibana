ab5ad1e040cf00c893aad79bec9f2564e7796c3c
Julia Bardi <julia.bardi@elastic.co>
2025-10-20 15:59:45 +0200

    replace wait with SO update

--- x-pack/platform/test/fleet_api_integration/apis/epm/rollback.ts
+++ x-pack/platform/test/fleet_api_integration/apis/epm/rollback.ts
@@ -270,7 +270,15 @@ export default function (providerContext: FtrProviderContext) {
       it('should fail when rollback TTL expired', async () => {
         await upgradePackage(pkgName, oldPkgVersion, newPkgVersion, policyIds, true);
 
-        await new Promise((resolve) => setTimeout(resolve, 30001));
+        // Setting 8d ago to ensure TTL of 7d is expired
+        const previousInstallDate = new Date(Date.now() - 8 * 24 * 60 * 60 * 1000).toISOString();
+        await kibanaServer.savedObjects.update({
+          id: pkgName,
+          type: PACKAGES_SAVED_OBJECT_TYPE,
+          attributes: {
+            install_started_at: previousInstallDate,
+          },
+        });
 
         const res = await supertest
           .post(`/api/fleet/epm/packages/${pkgName}/rollback`)
