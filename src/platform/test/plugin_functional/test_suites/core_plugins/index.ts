d92bb79c0601199447157ff416ec7540560d93f8
Larry Gregory <larry.gregory@elastic.co>
2025-10-20 16:08:34 -0400

    Revert "REVERT THIS. Temporary test changes"
    This reverts commit d83b89fd50becca17e707cc5a5cead182667585e.

--- src/platform/test/plugin_functional/test_suites/core_plugins/index.ts
+++ src/platform/test/plugin_functional/test_suites/core_plugins/index.ts
@@ -11,22 +11,22 @@ import type { PluginFunctionalProviderContext } from '../../services';
 
 export default function ({ loadTestFile }: PluginFunctionalProviderContext) {
   describe('core plugins', () => {
-    // loadTestFile(require.resolve('./applications'));
-    // loadTestFile(require.resolve('./elasticsearch_client'));
-    // loadTestFile(require.resolve('./execution_context'));
-    // loadTestFile(require.resolve('./server_plugins'));
-    // loadTestFile(require.resolve('./ui_plugins'));
-    // loadTestFile(require.resolve('./ui_settings'));
-    // loadTestFile(require.resolve('./top_nav'));
-    // loadTestFile(require.resolve('./application_leave_confirm'));
-    // loadTestFile(require.resolve('./application_status'));
-    // loadTestFile(require.resolve('./application_deep_links'));
+    loadTestFile(require.resolve('./applications'));
+    loadTestFile(require.resolve('./elasticsearch_client'));
+    loadTestFile(require.resolve('./execution_context'));
+    loadTestFile(require.resolve('./server_plugins'));
+    loadTestFile(require.resolve('./ui_plugins'));
+    loadTestFile(require.resolve('./ui_settings'));
+    loadTestFile(require.resolve('./top_nav'));
+    loadTestFile(require.resolve('./application_leave_confirm'));
+    loadTestFile(require.resolve('./application_status'));
+    loadTestFile(require.resolve('./application_deep_links'));
     loadTestFile(require.resolve('./rendering'));
-    // loadTestFile(require.resolve('./chrome_help_menu_links'));
-    // loadTestFile(require.resolve('./history_block'));
-    // loadTestFile(require.resolve('./http'));
-    // loadTestFile(require.resolve('./http_versioned'));
-    // loadTestFile(require.resolve('./dynamic_contract_resolving'));
-    // loadTestFile(require.resolve('./error_response'));
+    loadTestFile(require.resolve('./chrome_help_menu_links'));
+    loadTestFile(require.resolve('./history_block'));
+    loadTestFile(require.resolve('./http'));
+    loadTestFile(require.resolve('./http_versioned'));
+    loadTestFile(require.resolve('./dynamic_contract_resolving'));
+    loadTestFile(require.resolve('./error_response'));
   });
 }
