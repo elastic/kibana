619bb810981f958f9a585545b2a73f526b24efb6
Chris Georgakopoulos <chris.georgakopoulos@elastic.co>
2025-10-20 11:21:28 +0300

    import css

--- x-pack/solutions/observability/plugins/observability/public/components/loading_observability.tsx
+++ x-pack/solutions/observability/plugins/observability/public/components/loading_observability.tsx
@@ -6,6 +6,7 @@
  */
 
 import React from 'react';
+import { css } from '@emotion/react';
 import { EuiFlexGroup, EuiFlexItem, EuiLoadingSpinner, EuiText } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { usePluginContext } from '../hooks/use_plugin_context';
