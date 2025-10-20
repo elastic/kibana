cd562f51441b79eb7fa7024b6e738ee84fd506ba
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2025-10-20 17:07:00 +0200

    skip failing test suite (#237553)

--- x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/entity_analytics/priv_mon/index_onboarding.cy.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/entity_analytics/priv_mon/index_onboarding.cy.ts
@@ -26,6 +26,7 @@ import { ENTITY_ANALYTICS_PRIVILEGED_USER_MONITORING_URL } from '../../../urls/n
 const sourceIndexName = 'test_index';
 
 // Failing: See https://github.com/elastic/kibana/issues/237554
+// Failing: See https://github.com/elastic/kibana/issues/237553
 describe.skip(
   'Privileged User Monitoring - Index onboarding',
   {
