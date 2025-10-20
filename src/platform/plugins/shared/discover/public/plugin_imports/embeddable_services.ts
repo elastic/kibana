b42d1fa518836a8f60c5340fd231c542158707ea
Nick Peihl <nick.peihl@elastic.co>
2025-10-20 07:53:00 -0400

    Fix imports

--- src/platform/plugins/shared/discover/public/plugin_imports/embeddable_services.ts
+++ src/platform/plugins/shared/discover/public/plugin_imports/embeddable_services.ts
@@ -10,5 +10,5 @@
 export { ViewSavedSearchAction } from '../embeddable/actions/view_saved_search_action';
 export { getSearchEmbeddableFactory } from '../embeddable/get_search_embeddable_factory';
 export { getLegacyLogStreamEmbeddableFactory } from '../embeddable/get_legacy_log_stream_embeddable_factory';
-export { searchEmbeddableTransforms } from '../../common/embeddable';
+export { getSearchEmbeddableTransforms } from '../../common/embeddable';
 export { addControlsFromSavedSession } from '../embeddable/utils/add_controls_from_saved_session';
