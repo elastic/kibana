91f893625dc2e04a0d4d2683888cecdc63449bfe
Sergi Massaneda <sergi.massaneda@elastic.co>
2025-10-21 15:52:00 +0200

    enable ui setting by default

--- src/platform/plugins/shared/workflows_management/public/plugin.ts
+++ src/platform/plugins/shared/workflows_management/public/plugin.ts
@@ -15,6 +15,7 @@ import {
   type Plugin,
 } from '@kbn/core/public';
 import { Storage } from '@kbn/kibana-utils-plugin/public';
+import { WORKFLOWS_UI_SETTING_ID } from '@kbn/workflows/common/constants';
 import { PLUGIN_ID, PLUGIN_NAME } from '../common';
 // Lazy import to avoid bundling connector dependencies in main plugin
 import type {
@@ -45,7 +46,7 @@ export class WorkflowsPlugin
     // Register the connector type immediately but load it lazily
     registerConnectorType();
 
-    const isWorkflowsUiEnabled = true; // core.uiSettings.get<boolean>(WORKFLOWS_UI_SETTING_ID, false);
+    const isWorkflowsUiEnabled = core.uiSettings.get<boolean>(WORKFLOWS_UI_SETTING_ID, false);
 
     if (isWorkflowsUiEnabled) {
       const storage = this.storage;
