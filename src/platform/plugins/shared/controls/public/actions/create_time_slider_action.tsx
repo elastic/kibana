bfa8f823cbc2f34ef58020fc1b9cf3236f710ad5
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-10-21 15:04:32 -0500

    Make timeslider medium minimum width

--- src/platform/plugins/shared/controls/public/actions/create_time_slider_action.tsx
+++ src/platform/plugins/shared/controls/public/actions/create_time_slider_action.tsx
@@ -41,7 +41,7 @@ export const createTimeSliderAction = (): ActionDefinition<EmbeddableApiContext>
       serializedState: {
         rawState: {
           grow: true,
-          width: 'large',
+          width: 'medium',
         },
       },
     });
