f912aa37e5931a456ece6bd37a75489230765324
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2025-10-21 02:17:58 +0200

    skip failing test suite (#239774)

--- x-pack/platform/test/functional_with_es_ssl/apps/cases/group1/view_case.ts
+++ x-pack/platform/test/functional_with_es_ssl/apps/cases/group1/view_case.ts
@@ -43,6 +43,7 @@ export default ({ getPageObject, getService }: FtrProviderContext) => {
   // https://github.com/elastic/kibana/pull/190690
   // fails after missing `awaits` were added
   // Failing: See https://github.com/elastic/kibana/issues/239759
+  // Failing: See https://github.com/elastic/kibana/issues/239774
   describe.skip('View case', () => {
     describe('page', () => {
       createOneCaseBeforeDeleteAllAfter(getPageObject, getService);
