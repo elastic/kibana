784d8c028e0e2af2c08b056b0d28cf3e06f8c68e
Stratoula <efstratia.kalafateli@elastic.co>
2025-10-21 11:55:26 +0200

    Unskip

--- x-pack/platform/test/api_integration/apis/maps/search.ts
+++ x-pack/platform/test/api_integration/apis/maps/search.ts
@@ -14,8 +14,7 @@ import type { FtrProviderContext } from '../../ftr_provider_context';
 export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
 
-  // Failing: See https://github.com/elastic/kibana/issues/238754
-  describe.skip('search', () => {
+  describe('search', () => {
     describe('ES|QL', () => {
       it(`should return getColumns response in expected shape`, async () => {
         const resp = await supertest
