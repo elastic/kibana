04ddd5400332495d91018e5b725aeda2d48560ce
Ihor Panasiuk <igorskynet13@gmail.com>
2025-10-22 10:23:59 +0200

    fixes

--- src/platform/packages/shared/kbn-workflows/common/utils/extract_template_variables/extract_template_variables.test.ts
+++ src/platform/packages/shared/kbn-workflows/common/utils/extract_template_variables/extract_template_variables.test.ts
@@ -65,4 +65,111 @@ describe('extractTemplateVariables', () => {
     const variables = extractTemplateVariables(template);
     expect(variables).toEqual(['order.items', 'order.total']);
   });
+
+  it('should handle RangeToken in for loops', () => {
+    const template = `
+      {% for i in (1..5) %}
+        Item {{ i }}: {{ products[i] }}
+      {% endfor %}
+      {% for j in (start..end) %}
+        {{ items[j] }}
+      {% endfor %}
+    `;
+
+    const variables = extractTemplateVariables(template);
+    expect(variables).toEqual(['products[i]', 'start', 'end', 'items']);
+  });
+
+  it('should handle LiteralToken (true, false, nil)', () => {
+    const template = `
+      {% if user.isActive == true %}
+        Active user
+      {% endif %}
+      {% if product.stock == nil %}
+        Out of stock
+      {% endif %}
+      {% assign isValid = false %}
+      {{ user.permissions }}
+    `;
+
+    const variables = extractTemplateVariables(template);
+    expect(variables).toEqual(['user.isActive', 'product.stock', 'user.permissions']);
+  });
+
+  it('should handle QuotedToken (string literals)', () => {
+    const template = `
+      {% if user.role == "admin" %}
+        {{ user.name }}
+      {% endif %}
+      {% assign greeting = 'Hello' %}
+      {{ product.category }}
+    `;
+
+    const variables = extractTemplateVariables(template);
+    expect(variables).toEqual(['user.role', 'user.name', 'product.category']);
+  });
+
+  it('should handle NumberToken in comparisons and array access', () => {
+    const template = `
+      {% if user.age > 18 %}
+        {{ user.name }}
+      {% endif %}
+      {{ items[0] }}
+      {{ products[123] }}
+      {% if price < 100.50 %}
+        Affordable
+      {% endif %}
+      {{ cart.total }}
+    `;
+
+    const variables = extractTemplateVariables(template);
+    expect(variables).toEqual([
+      'user.age',
+      'user.name',
+      'items',
+      'products',
+      'price',
+      'cart.total',
+    ]);
+  });
+
+  it('should handle complex expressions with multiple token types', () => {
+    const template = `
+      {% for i in (0..count) %}
+        {% if items[i].price < 50 and items[i].inStock == true %}
+          {{ items[i].name }} - {{ items[i].price }}
+        {% endif %}
+      {% endfor %}
+      {{ user.name | default: "Guest" }}
+    `;
+
+    const variables = extractTemplateVariables(template);
+    expect(variables).toEqual(['count', 'items', 'user.name']);
+  });
+
+  it('should handle filters without extracting filter names as variables', () => {
+    const template = `
+      {{ user.name | upcase }}
+      {{ product.price | minus: 10 | divided_by: 2 }}
+      {{ date | date: "%Y-%m-%d" }}
+    `;
+
+    const variables = extractTemplateVariables(template);
+    expect(variables).toEqual(['user.name', 'product.price', 'date']);
+  });
+
+  it('should handle nested property access with array indices', () => {
+    const template = `
+      {{ user.orders[0].items[1].name }}
+      {{ data.results[5].nested.value }}
+      {{ array[0][1][2] }}
+    `;
+
+    const variables = extractTemplateVariables(template);
+    expect(variables).toEqual([
+      'user.orders[0].items[1].name',
+      'data.results[5].nested.value',
+      'array[0][1][2]',
+    ]);
+  });
 });
--- src/platform/packages/shared/kbn-workflows/common/utils/extract_template_variables/extract_template_variables.ts
+++ src/platform/packages/shared/kbn-workflows/common/utils/extract_template_variables/extract_template_variables.ts
@@ -7,20 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { Liquid, Template, IfTag, Output, TokenKind, Expression, ForTag, Token } from 'liquidjs';
-
-// Internal types not exported by liquidjs
-// See this GH issue - https://github.com/harttle/liquidjs/issues/822
-// Will be removed once the types are exported from liquidjs
-interface PropertyAccessToken extends Token {
-  kind: TokenKind.PropertyAccess;
-  props: Array<{ kind: TokenKind; content: string | number }>;
-}
-
-// Type guards
-function isPropertyAccessToken(token: Token): token is PropertyAccessToken {
-  return token.kind === TokenKind.PropertyAccess && 'props' in token;
-}
+import { Liquid, Template, IfTag, Output, Expression, ForTag, Token, TokenKind } from 'liquidjs';
+import { parseJsPropertyAccess } from '../parse_js_property_access/parse_js_property_access';
 
 const liquidEngine = new Liquid({
   strictFilters: true,
@@ -47,26 +35,14 @@ function visitLiquidAST(node: unknown, localVariablesSet: Set<string>): string[]
     const fromCollection = visitLiquidAST(node.collection, localVariablesSet);
     const fromBody = node.templates
       .flatMap((template) => visitLiquidAST(template, localVariablesSet))
-      .filter((x) => !localVariablesSet.has(x.split('.')[0]));
+      .filter((prop) => !localVariablesSet.has(parseJsPropertyAccess(prop)[0]));
     localVariablesSet.delete(node.variable);
     return fromCollection.concat(fromBody);
   }
 
   if (node instanceof Token) {
-    let path = '';
-
-    if (isPropertyAccessToken(node)) {
-      node.props.forEach((prop) => {
-        if (prop.kind === TokenKind.Word) {
-          path += path ? `.${prop.content}` : prop.content;
-        } else if (prop.kind === TokenKind.Number) {
-          path += `[${prop.content}]`;
-        }
-      });
-    }
-
-    if (path) {
-      return [path];
+    if (node.kind === TokenKind.PropertyAccess) {
+      return [node.getText()];
     }
   }
 
