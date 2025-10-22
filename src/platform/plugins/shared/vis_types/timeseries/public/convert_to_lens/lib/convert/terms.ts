89435ef9d6c9114df3b0474cdc6cc3c6cbf87a9d
dej611 <dej611@gmail.com>
2025-10-22 18:41:27 +0200

    :wrench: Fix exports

--- src/platform/plugins/shared/vis_types/timeseries/public/convert_to_lens/lib/convert/terms.ts
+++ src/platform/plugins/shared/vis_types/timeseries/public/convert_to_lens/lib/convert/terms.ts
@@ -11,7 +11,7 @@ import type { DataView } from '@kbn/data-views-plugin/common';
 import type { TermsParams } from '@kbn/visualizations-plugin/common';
 import { v4 as uuidv4 } from 'uuid';
 import type { DataType } from '@kbn/lens-common';
-import { isColumnMetricWithSourceField } from '@kbn/visualizations-plugin/common/convert_to_lens/lib/utils';
+import { isColumnMetricWithSourceField } from '@kbn/visualizations-plugin/common/convert_to_lens';
 import { excludeMetaFromColumn, getFormat, isColumnWithMeta } from './column';
 import type { Column, TermsColumn, TermsSeries } from './types';
 
--- src/platform/plugins/shared/visualizations/common/convert_to_lens/index.ts
+++ src/platform/plugins/shared/visualizations/common/convert_to_lens/index.ts
@@ -11,3 +11,4 @@ export type { AggBasedColumn } from './lib/convert/types';
 export type * from './types';
 export * from './constants';
 export * from './utils';
+export { isColumnMetricWithSourceField } from './lib/utils';
