1c8ea9eec67f5487e8ccee84ddb61f5854dc2477
Ying <ying.mao@elastic.co>
2025-10-23 12:24:45 -0400

    Fixing types

--- src/platform/packages/shared/kbn-connector-schemas/sentinelone/index.ts
+++ src/platform/packages/shared/kbn-connector-schemas/sentinelone/index.ts
@@ -61,4 +61,5 @@ export type {
   SentinelOneGetRemoteScriptResultsParams,
   SentinelOneGetRemoteScriptResultsApiResponse,
   SentinelOneGetRemoteScriptStatusApiResponse,
+  SentinelOneRemoteScriptExecutionStatus,
 } from './types/latest';
