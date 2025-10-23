22337c07003195aff9022b3d8d9f441b6c8ec475
dej611 <dej611@gmail.com>
2025-10-23 18:02:57 +0200

    :ok_hand: Applied feedback

--- x-pack/platform/plugins/shared/lens/public/visualizations/xy/persistence.ts
+++ x-pack/platform/plugins/shared/lens/public/visualizations/xy/persistence.ts
@@ -9,7 +9,7 @@ import type { Reference } from '@kbn/content-management-utils';
 import { EVENT_ANNOTATION_GROUP_TYPE } from '@kbn/event-annotation-common';
 
 import type { AnnotationGroups } from '@kbn/lens-common';
-import { LENS_LAYER_TYPES as layerTypes } from '@kbn/lens-common';
+import { LENS_LAYER_TYPES } from '@kbn/lens-common';
 import type {
   XYLayerConfig,
   XYDataLayerConfig,
@@ -164,7 +164,7 @@ export function convertToPersistable(state: XYState) {
 export const isPersistedAnnotationsLayer = (
   layer: XYPersistedLayerConfig
 ): layer is XYPersistedAnnotationLayerConfig =>
-  layer.layerType === layerTypes.ANNOTATIONS && !('indexPatternId' in layer);
+  layer.layerType === LENS_LAYER_TYPES.ANNOTATIONS && !('indexPatternId' in layer);
 
 export const isPersistedByValueAnnotationsLayer = (
   layer: XYPersistedLayerConfig
