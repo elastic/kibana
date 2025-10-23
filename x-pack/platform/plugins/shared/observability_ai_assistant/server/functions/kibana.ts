8c6db1d856ccfceb8749e108cced9a01510408f8
Søren Louv-Jansen <soren.louv@elastic.co>
2025-10-24 00:17:02 +0200

    Use `publicBaseUrl ` instead of origin from header

--- x-pack/platform/plugins/shared/observability_ai_assistant/server/functions/kibana.ts
+++ x-pack/platform/plugins/shared/observability_ai_assistant/server/functions/kibana.ts
@@ -7,9 +7,10 @@
 
 import axios from 'axios';
 import { format } from 'url';
-import { castArray, first, pick, pickBy } from 'lodash';
+import { castArray, first, pickBy } from 'lodash';
 import type { KibanaRequest } from '@kbn/core/server';
 import type { FunctionRegistrationParameters } from '.';
+import { addSpaceIdToPath, getSpaceIdFromPath } from '@kbn/spaces-plugin/common';
 import { KIBANA_FUNCTION_NAME } from '..';
 
 export function registerKibanaFunction({
@@ -50,23 +51,46 @@ export function registerKibanaFunction({
     },
     async ({ arguments: { method, pathname, body, query } }, signal) => {
       const { request, logger } = resources;
-      const { protocol, host, pathname: pathnameFromRequest } = request.rewrittenUrl || request.url;
+      const requestUrl = request.rewrittenUrl || request.url;
+      const core = await resources.plugins.core.start();
 
-      const originUrl = first(castArray(request.headers.origin));
+      function getParsedBaseUrl() {
+        const { protocol, host } = requestUrl;
+        const { publicBaseUrl } = core.http.basePath;
+        const baseUrl = publicBaseUrl || `${protocol}//${host}`;
+        const parsedBaseUrl = new URL(baseUrl);
+        return parsedBaseUrl;
+      }
 
-      
+      function getPathnameWithSpaceId() {
+        const { spaceId } = getSpaceIdFromPath(
+          requestUrl.toString(),
+          core.http.basePath.serverBasePath
+        );
 
+        const basePath = core.http.basePath.get(request);
+        const pathnameWithSpaceId = addSpaceIdToPath(basePath, spaceId, pathname);
+        return pathnameWithSpaceId;
+      }
+
+      const parsedBaseUrl = getParsedBaseUrl();
       const nextUrl = {
-        host,
-        protocol,
-        pathname: pathnameFromRequest.replace(
-          '/internal/observability_ai_assistant/chat/complete',
-          pathname
-        ),
+        host: parsedBaseUrl.host,
+        protocol: parsedBaseUrl.protocol,
+        pathname: getPathnameWithSpaceId(),
         query: query ? (query as Record<string, string>) : undefined,
       };
 
-      logger.info(`kibana tool: originUrl = ${originUrl}, nextUrl = ${nextUrl}`);
+      const originUrl = first(castArray(request.headers.origin));
+      logger.info(
+        `kibana tool: originUrl = ${originUrl}, parsedBaseUrl = ${JSON.stringify(
+          parsedBaseUrl
+        )}, requestUrl = ${JSON.stringify(requestUrl)}, nextUrl = ${JSON.stringify(nextUrl)}`
+      );
+
+      logger.info(
+        `Forwarding requests from ${parsedBaseUrl} to call Kibana API: ${method} ${format(nextUrl)}`
+      );
 
       const copiedHeaderNames = [
         'authorization',
@@ -90,12 +114,6 @@ export function registerKibanaFunction({
           copiedHeaderNames.includes(key.toLowerCase()) || key.toLowerCase().startsWith('sec-')
         );
       });
-
-      logger.info(
-        `Forwarding requests from ${
-          request.headers.origin ?? host
-        } to call Kibana API: ${method} ${format(nextUrl)}`
-      );
 
       try {
         const response = await axios({
