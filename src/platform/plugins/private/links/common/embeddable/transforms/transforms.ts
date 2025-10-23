7fdb0b22b5dd58935e3e2b9a36462ec7d52e2bad
Nathan Reese <reese.nathan@elastic.co>
2025-10-23 07:19:04 -0600

    [dashboard as code] fix references for the links panel are passed when updating a dashboard (#239804)
    Closes https://github.com/elastic/kibana/issues/239367
    
    Issue caused because server returns references for all panels. Then,
    when dashboard is saved, panel state provided by server is returned.
    This caused the references to be passed back to the server. The reason
    client passes back panel state provided by the server is that client can
    not call embeddableApi.serializeState when serializing a dashboard since
    embeddableApi does not exist for embeddables in collapsed sections.
    `embeddableApi.serializeState` is only called when an embeddable reports
    unsaved changes.
    
    Resolves issue by dropping references for panels that inject references
    server side. This way, references are never passed to the client.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/platform/plugins/private/links/common/embeddable/transforms/transforms.ts
+++ src/platform/plugins/private/links/common/embeddable/transforms/transforms.ts
@@ -11,6 +11,7 @@ import { transformIn } from './transform_in';
 import { transformOut } from './transform_out';
 
 export const transforms = {
+  transformOutInjectsReferences: true,
   transformIn,
   transformOut,
 };
--- src/platform/plugins/shared/visualizations/common/embeddable/transforms/get_transforms.ts
+++ src/platform/plugins/shared/visualizations/common/embeddable/transforms/get_transforms.ts
@@ -16,6 +16,7 @@ export function getTransforms(
   transformEnhancementsOut: EnhancementsRegistry['transformOut']
 ) {
   return {
+    transformOutInjectsReferences: true,
     transformIn: getTransformIn(transformEnhancementsIn),
     transformOut: getTransformOut(transformEnhancementsOut),
   };
