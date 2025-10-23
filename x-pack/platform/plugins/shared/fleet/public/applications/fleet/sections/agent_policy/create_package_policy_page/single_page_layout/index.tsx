elastic/kibana

commit 894c8f9c9c684c21c77f1df25e26ded2726360e3

[Cloud Security] Create CloudSetup shared component for CSPM (#228670)

x-pack/solutions/security/packages/kbn-cloud-security-posture/public/src/components/fleet_extensions/cloud_setup.tsx
@@ -1,0 +1,307 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import React, { memo } from 'react';
+import { EuiAccordion, EuiSpacer, EuiText, EuiTitle, useEuiTheme } from '@elastic/eui';
+import type { NewPackagePolicy } from '@kbn/fleet-plugin/public';
+import {
+  NamespaceComboBox,
+  SetupTechnology,
+  SetupTechnologySelector,
+} from '@kbn/fleet-plugin/public';
+import { FormattedMessage } from '@kbn/i18n-react';
+import { PackagePolicyValidationResults } from '@kbn/fleet-plugin/common/services';
+import { CloudSetup as ICloudSetup } from '@kbn/cloud-plugin/public';
+import { PackageInfo } from '@kbn/fleet-plugin/common';
+import { IUiSettingsClient } from '@kbn/core/public';
+import type { CloudSecurityPolicyTemplate, PostureInput, UpdatePolicy } from './types';
+import { getPosturePolicy, getDefaultCloudCredentialsType } from './utils';
+import { PolicyTemplateInputSelector } from './policy_template_selectors';
+import { AwsAccountTypeSelect } from './aws_credentials_form/aws_account_type_selector';
+import { GcpAccountTypeSelect } from './gcp_credentials_form/gcp_account_type_selector';
+import { AzureAccountTypeSelect } from './azure_credentials_form/azure_account_type_selector';
+import { IntegrationSettings } from './integration_settings';
+import { AwsCredentialsForm } from './aws_credentials_form/aws_credentials_form';
+import { AwsCredentialsFormAgentless } from './aws_credentials_form/aws_credentials_form_agentless';
+import { GcpCredentialsFormAgentless } from './gcp_credentials_form/gcp_credentials_form_agentless';
+import { GcpCredentialsForm } from './gcp_credentials_form/gcp_credential_form';
+import { AzureCredentialsFormAgentless } from './azure_credentials_form/azure_credentials_form_agentless';
+import { AzureCredentialsForm } from './azure_credentials_form/azure_credentials_form';
+import { useLoadCloudSetup } from './hooks/use_load_cloud_setup';
+
+const EditScreenStepTitle = () => (
+  <>
+    <EuiTitle size="s">
+      <h4>
+        <FormattedMessage
+          id="securitySolutionPackages.fleetIntegration.integrationSettingsTitle"
+          defaultMessage="Integration Settings"
+        />
+      </h4>
+    </EuiTitle>
+    <EuiSpacer />
+  </>
+);
+interface CloudSetupProps {
+  cloud: ICloudSetup;
+  defaultSetupTechnology?: SetupTechnology;
+  handleSetupTechnologyChange?: (setupTechnology: SetupTechnology) => void;
+  integrationToEnable?: CloudSecurityPolicyTemplate;
+  isAgentlessEnabled?: boolean;
+  isEditPage: boolean;
+  isValid: boolean;
+  namespaceSupportEnabled?: boolean;
+  newPolicy: NewPackagePolicy;
+  updatePolicy: UpdatePolicy;
+  packageInfo: PackageInfo;
+  validationResults?: PackagePolicyValidationResults;
+  uiSettings: IUiSettingsClient;
+}
+
+export const CloudSetup = memo<CloudSetupProps>(
+  ({
+    cloud,
+    defaultSetupTechnology,
+    handleSetupTechnologyChange,
+    integrationToEnable,
+    isAgentlessEnabled,
+    isEditPage,
+    isValid,
+    namespaceSupportEnabled = false,
+    newPolicy,
+    updatePolicy,
+    packageInfo,
+    validationResults,
+    uiSettings,
+  }: CloudSetupProps) => {
+    const {
+      input,
+      setEnabledPolicyInput,
+      setupTechnology,
+      updateSetupTechnology,
+      shouldRenderAgentlessSelector,
+      isServerless,
+      showCloudConnectors,
+      hasInvalidRequiredVars,
+    } = useLoadCloudSetup({
+      newPolicy,
+      updatePolicy,
+      validationResults,
+      isEditPage,
+      packageInfo,
+      handleSetupTechnologyChange,
+      isAgentlessEnabled,
+      defaultSetupTechnology,
+      integrationToEnable,
+      cloud,
+      uiSettings,
+    });
+
+    const { euiTheme } = useEuiTheme();
+
+    return (
+      <>
+        {isEditPage && <EditScreenStepTitle />}
+        {/* Shows info on the active policy template */}
+        <FormattedMessage
+          id="securitySolutionPackages.fleetIntegration.configureCspmIntegrationDescription"
+          defaultMessage="Select the cloud service provider (CSP) you want to monitor and then fill in the name and description to help identify this integration"
+        />
+        <EuiSpacer size="l" />
+        {/* Defines the single enabled input of the active policy template */}
+        <PolicyTemplateInputSelector
+          input={input}
+          setInput={setEnabledPolicyInput}
+          disabled={isEditPage}
+        />
+        <EuiSpacer size="l" />
+
+        {/* AWS account type selection box */}
+        {input.type === 'cloudbeat/cis_aws' && (
+          <AwsAccountTypeSelect
+            input={input}
+            newPolicy={newPolicy}
+            updatePolicy={updatePolicy}
+            packageInfo={packageInfo}
+            disabled={isEditPage}
+          />
+        )}
+
+        {input.type === 'cloudbeat/cis_gcp' && (
+          <GcpAccountTypeSelect
+            input={input}
+            newPolicy={newPolicy}
+            updatePolicy={updatePolicy}
+            packageInfo={packageInfo}
+            disabled={isEditPage}
+          />
+        )}
+
+        {input.type === 'cloudbeat/cis_azure' && (
+          <AzureAccountTypeSelect
+            input={input}
+            newPolicy={newPolicy}
+            updatePolicy={updatePolicy}
+            packageInfo={packageInfo}
+            disabled={isEditPage}
+            setupTechnology={setupTechnology}
+          />
+        )}
+
+        <EuiSpacer size="l" />
+        <IntegrationSettings
+          newPolicy={newPolicy}
+          validationResults={validationResults}
+          onChange={(field, value) =>
+            updatePolicy({ updatedPolicy: { ...newPolicy, [field]: value } })
+          }
+        />
+
+        {/* Namespace selector */}
+        {namespaceSupportEnabled && (
+          <>
+            <EuiSpacer size="m" />
+            <EuiAccordion
+              id="advancedOptions"
+              data-test-subj="advancedOptionsAccordion"
+              buttonContent={
+                <EuiText
+                  size="xs"
+                  color={euiTheme.colors.textPrimary}
+                  css={{
+                    fontWeight: euiTheme.font.weight.medium,
+                  }}
+                >
+                  <FormattedMessage
+                    id="securitySolutionPackages.fleetIntegration.advancedOptionsLabel"
+                    defaultMessage="Advanced options"
+                  />
+                </EuiText>
+              }
+              paddingSize="m"
+            >
+              <NamespaceComboBox
+                fullWidth
+                namespace={newPolicy.namespace}
+                placeholder="default"
+                isEditPage={isEditPage}
+                validationError={validationResults?.namespace}
+                onNamespaceChange={(namespace: string) => {
+                  updatePolicy({ updatedPolicy: { ...newPolicy, namespace } });
+                }}
+                data-test-subj="namespaceInput"
+                labelId="securitySolutionPackages.fleetIntegration.namespaceLabel"
+                helpTextId="securitySolutionPackages.fleetIntegration.awsAccountType.awsOrganizationDescription"
+              />
+            </EuiAccordion>
+          </>
+        )}
+        {shouldRenderAgentlessSelector && (
+          <>
+            <EuiSpacer size="m" />
+            <SetupTechnologySelector
+              showLimitationsMessage={!isServerless}
+              disabled={isEditPage}
+              setupTechnology={setupTechnology}
+              allowedSetupTechnologies={[SetupTechnology.AGENT_BASED, SetupTechnology.AGENTLESS]}
+              showBetaBadge={false}
+              useDescribedFormGroup={false}
+              onSetupTechnologyChange={(value) => {
+                updateSetupTechnology(value);
+                updatePolicy({
+                  updatedPolicy: getPosturePolicy(
+                    newPolicy,
+                    input.type,
+                    getDefaultCloudCredentialsType(
+                      value === SetupTechnology.AGENTLESS,
+                      input.type as Extract<
+                        PostureInput,
+                        'cloudbeat/cis_aws' | 'cloudbeat/cis_azure' | 'cloudbeat/cis_gcp'
+                      >,
+                      packageInfo,
+                      showCloudConnectors
+                    )
+                  ),
+                });
+              }}
+            />
+          </>
+        )}
+
+        {input.type === 'cloudbeat/cis_aws' && setupTechnology === SetupTechnology.AGENTLESS && (
+          <AwsCredentialsFormAgentless
+            input={input}
+            newPolicy={newPolicy}
+            packageInfo={packageInfo}
+            updatePolicy={updatePolicy}
+            isEditPage={isEditPage}
+            setupTechnology={setupTechnology}
+            hasInvalidRequiredVars={hasInvalidRequiredVars}
+            showCloudConnectors={showCloudConnectors}
+            cloud={cloud}
+          />
+        )}
+        {input.type === 'cloudbeat/cis_aws' && setupTechnology !== SetupTechnology.AGENTLESS && (
+          <AwsCredentialsForm
+            input={input}
+            newPolicy={newPolicy}
+            packageInfo={packageInfo}
+            updatePolicy={updatePolicy}
+            disabled={isEditPage}
+            hasInvalidRequiredVars={hasInvalidRequiredVars}
+            isValid={isValid}
+          />
+        )}
+        {input.type === 'cloudbeat/cis_gcp' && setupTechnology === SetupTechnology.AGENTLESS && (
+          <GcpCredentialsFormAgentless
+            input={input}
+            newPolicy={newPolicy}
+            packageInfo={packageInfo}
+            updatePolicy={updatePolicy}
+            disabled={isEditPage}
+            hasInvalidRequiredVars={hasInvalidRequiredVars}
+          />
+        )}
+        {input.type === 'cloudbeat/cis_gcp' && setupTechnology !== SetupTechnology.AGENTLESS && (
+          <GcpCredentialsForm
+            input={input}
+            newPolicy={newPolicy}
+            packageInfo={packageInfo}
+            updatePolicy={updatePolicy}
+            disabled={isEditPage}
+            hasInvalidRequiredVars={hasInvalidRequiredVars}
+          />
+        )}
+        {input.type === 'cloudbeat/cis_azure' && setupTechnology === SetupTechnology.AGENTLESS && (
+          <AzureCredentialsFormAgentless
+            input={input}
+            newPolicy={newPolicy}
+            packageInfo={packageInfo}
+            updatePolicy={updatePolicy}
+            hasInvalidRequiredVars={hasInvalidRequiredVars}
+          />
+        )}
+        {input.type === 'cloudbeat/cis_azure' && setupTechnology !== SetupTechnology.AGENTLESS && (
+          <AzureCredentialsForm
+            input={input}
+            newPolicy={newPolicy}
+            packageInfo={packageInfo}
+            updatePolicy={updatePolicy}
+            disabled={isEditPage}
+            hasInvalidRequiredVars={hasInvalidRequiredVars}
+            isValid={isValid}
+          />
+        )}
+        <EuiSpacer />
+      </>
+    );
+  }
+);
+
+CloudSetup.displayName = 'CloudSetup';
+
+// eslint-disable-next-line import/no-default-export
+export { CloudSetup as default };

x-pack/solutions/security/plugins/security_solution/public/asset_inventory/components/fleet_extensions/policy_template_form.tsx
@@ -315,7 +315,7 @@ export const CloudAssetInventoryPolicyTemplateForm =
               }}
               data-test-subj="namespaceInput"
               labelId="xpack.csp.fleetIntegration.namespaceLabel"
-              helpTextId="xpack.csp.fleetIntegration.awsAccountType.awsOrganizationDescription"
+              helpTextId="securitySolutionPackages.fleetIntegration.awsAccountType.awsOrganizationDescription"
             />
           </EuiAccordion>
           {shouldRenderAgentlessSelector && (

commit b0bc77ebde34310fa4d93eb0705eb55307b518d7

[9.2] Remove traffic filter warning code entirely from Fleet and security integrations (#230368) (#240010)
# Backport

This will backport the following commits from `main` to `9.2`:
- [Remove traffic filter warning code entirely from Fleet and security
integrations (#230368)](https://github.com/elastic/kibana/pull/230368)

<!--- Backport version: 10.1.0 -->

### Questions ?
Please refer to the [Backport tool
documentation](https://github.com/sorenlouv/backport)

<!--BACKPORT
[{"author":{"name":"Copilot","email":"198982749+Copilot@users.noreply.github.com"},"sourceCommit":{"committedDate":"2025-10-09T14:30:41Z","message":"Remove
traffic filter warning code entirely from Fleet and security
integrations (#230368)\n\nRemoves the traffic filtering warning code
entirely from the Fleet\n`SetupTechnologySelector` component and
security integrations, as\nrequested by the Fleet team. The
`showLimitationsMessage` parameter and\nassociated warning were created
specifically for this traffic filtering\nuse case and are no longer
needed since agentless deployments now\nsupport traffic filtering in
version 9.1.1.\n\n## Changes Made\n\n**Fleet SetupTechnologySelector
Component**\n- Removed `showLimitationsMessage` parameter from component
interface\n- Removed traffic filtering warning callout and related JSX
logic\n- Removed unused `EuiCallOut` and `EuiLink` imports\n- Simplified
component without conditional warning display\n\n**Fleet Single Page
Layout**\n- Removed `showLimitationsMessage={!isServerless}` parameter
usage\n\n**Security Integrations**\n- Removed
`showLimitationsMessage={false}` from CSPM integration\n(resolved via
merge with main branch)\n- Removed `showLimitationsMessage={false}` from
Cloud Asset Discovery\nintegration\n- Removed
`showLimitationsMessage={!isServerless}` from
CloudSetup\ncomponent\n\n**Tests**\n- Removed test case that verified
traffic filtering warning was not\ndisplayed\n\n**Merge Conflict
Resolution**\n- Resolved conflicts with main branch caused by PR #228670

x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/index.tsx
@@ -121,8 +121,7 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
   } = useConfig();
   const hasFleetAddAgentsPrivileges = useAuthz().fleet.addAgents;
   const fleetStatus = useFleetStatus();
-  const { docLinks, cloud } = useStartServices();
-  const isServerless = !!cloud?.isServerlessEnabled;
+  const { docLinks } = useStartServices();
   const spaceSettings = useSpaceSettingsContext();
   const [newAgentPolicy, setNewAgentPolicy] = useState<NewAgentPolicy>(
     generateNewAgentPolicyWithDefaults({
@@ -472,7 +471,6 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
           {/* Show SetupTechnologySelector for all agentless integrations, including extension views */}
           {!isAddIntegrationFlyout && isAgentlessIntegration(packageInfo) && (
             <SetupTechnologySelector
-              showLimitationsMessage={!isServerless}
               disabled={false}
               allowedSetupTechnologies={allowedSetupTechnologies}
               setupTechnology={selectedSetupTechnology}
@@ -532,7 +530,6 @@ export const CreatePackagePolicySinglePage: CreatePackagePolicyParams = ({
       handleSetupTechnologyChange,
       allowedSetupTechnologies,
       isAddIntegrationFlyout,
-      isServerless,
     ]
   );
 

x-pack/platform/plugins/shared/fleet/public/services/setup_technology_selector.tsx
@@ -16,8 +16,6 @@ import {
   EuiSpacer,
   EuiBadge,
   EuiTitle,
-  EuiCallOut,
-  EuiLink,
 } from '@elastic/eui';
 
 import { useStartServices } from '../hooks';
@@ -32,7 +30,6 @@ interface SetupTechnologySelectorProps {
   setupTechnology?: SetupTechnology;
   isAgentlessDefault?: boolean;
   showBetaBadge?: boolean;
-  showLimitationsMessage?: boolean;
   useDescribedFormGroup?: boolean;
 }
 
@@ -43,7 +40,6 @@ export const SetupTechnologySelector = ({
   onSetupTechnologyChange,
   isAgentlessDefault = false,
   showBetaBadge = true,
-  showLimitationsMessage = false,
   useDescribedFormGroup = true,
 }: SetupTechnologySelectorProps) => {
   const { docLinks } = useStartServices();
@@ -148,37 +144,6 @@ export const SetupTechnologySelector = ({
     />
   );
 
-  const limitationsMessage = showLimitationsMessage && (
-    <>
-      <EuiSpacer size="s" />
-      <EuiCallOut
-        color="warning"
-        iconType="alert"
-        size="s"
-        title={
-          <FormattedMessage
-            id="xpack.fleet.setupTechnology.comingSoon"
-            defaultMessage="Agentless deployment is not supported if you are using {link}."
-            values={{
-              link: (
-                <EuiLink
-                  href="https://www.elastic.co/guide/en/cloud-enterprise/current/ece-traffic-filtering-deployment-configuration.html"
-                  target="_blank"
-                >
-                  <FormattedMessage
-                    id="xpack.fleet.setupTechnology.comingSoon.trafficFilteringLinkText"
-                    defaultMessage="Traffic filtering"
-                  />
-                </EuiLink>
-              ),
-            }}
-          />
-        }
-      />
-      <EuiSpacer size="s" />
-    </>
-  );
-
   if (useDescribedFormGroup) {
     return (
       <EuiDescribedFormGroup
@@ -191,13 +156,10 @@ export const SetupTechnologySelector = ({
           </h3>
         }
         description={
-          <>
-            <FormattedMessage
-              id="xpack.fleet.setupTechnology.setupTechnologyDescription"
-              defaultMessage="Select a deployment mode for this integration."
-            />
-            {limitationsMessage}
-          </>
+          <FormattedMessage
+            id="xpack.fleet.setupTechnology.setupTechnologyDescription"
+            defaultMessage="Select a deployment mode for this integration."
+          />
         }
       >
         {radioGroup}
@@ -216,7 +178,7 @@ export const SetupTechnologySelector = ({
           />
         </h2>
       </EuiTitle>
-      {limitationsMessage || <EuiSpacer size="s" />}
+      <EuiSpacer size="s" />
       {radioGroup}
     </>
   );

x-pack/solutions/security/packages/kbn-cloud-security-posture/public/src/components/fleet_extensions/cloud_setup.tsx
@@ -101,7 +101,6 @@ const CloudIntegrationSetup = memo<CloudIntegrationSetupProps>(
       setupTechnology,
       updateSetupTechnology,
       shouldRenderAgentlessSelector,
-      isServerless,
       hasInvalidRequiredVars,
     } = useLoadCloudSetup({
       newPolicy,
@@ -241,7 +240,6 @@ const CloudIntegrationSetup = memo<CloudIntegrationSetupProps>(
           <>
             <EuiSpacer size="m" />
             <SetupTechnologySelector
-              showLimitationsMessage={!isServerless}
               disabled={isEditPage}
               setupTechnology={setupTechnology}
               allowedSetupTechnologies={[SetupTechnology.AGENT_BASED, SetupTechnology.AGENTLESS]}

x-pack/solutions/security/plugins/cloud_security_posture/public/components/fleet_extensions/policy_template_form.tsx
@@ -239,7 +239,6 @@ export const CspPolicyTemplateForm = memo<PackagePolicyReplaceDefineStepExtensio
             input={input}
           />
         )}
-
         <EuiSpacer />
       </>
     );