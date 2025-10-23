387e42b00e04939b2557a66d0cb828148975a93d
CAWilson94 <charlotte.wilson@elastic.co>
2025-10-23 09:29:01 +0100

    Remove skip

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/monitoring/trial_license_complete_tier/engine.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/entity_analytics/monitoring/trial_license_complete_tier/engine.ts
@@ -37,7 +37,7 @@ export default ({ getService }: FtrProviderContext) => {
     });
   }
 
-  describe.skip('@ess @serverless @skipInServerlessMKI Entity Privilege Monitoring APIs', () => {
+  describe('@ess @serverless @skipInServerlessMKI Entity Privilege Monitoring APIs', () => {
     const dataView = dataViewRouteHelpersFactory(supertest);
     const dataViewWithNamespace = dataViewRouteHelpersFactory(supertest, customSpace);
 
