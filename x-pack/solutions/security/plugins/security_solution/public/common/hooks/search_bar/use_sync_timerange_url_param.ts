9c9bccfd1db4139d7df2b2580680f31ed7d24d52
PhilippeOberti <philippe.oberti@elastic.co>
2025-10-23 22:18:12 -0500

    [Security Solution] remove socTrendsEnabled feature flag and related code

--- x-pack/solutions/security/plugins/security_solution/public/common/hooks/search_bar/use_sync_timerange_url_param.ts
+++ x-pack/solutions/security/plugins/security_solution/public/common/hooks/search_bar/use_sync_timerange_url_param.ts
@@ -4,10 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import { useEffect, useMemo } from 'react';
 import { useSelector } from 'react-redux';
 import { useKibana } from '../../lib/kibana';
-import { useIsExperimentalFeatureEnabled } from '../use_experimental_features';
 import type { UrlInputsModel } from '../../store/inputs/model';
 import { inputsSelectors } from '../../store/inputs';
 import { useUpdateUrlParam } from '../../utils/global_query_string';
@@ -17,26 +17,12 @@ export const useSyncTimerangeUrlParam = () => {
   const updateTimerangeUrlParam = useUpdateUrlParam<UrlInputsModel>(URL_PARAM_KEY.timerange);
   const getInputSelector = useMemo(() => inputsSelectors.inputsSelector(), []);
   const inputState = useSelector(getInputSelector);
-  const isSocTrendsEnabled = useIsExperimentalFeatureEnabled('socTrendsEnabled');
   const { serverless } = useKibana().services;
   // only on serverless
   const isValueReportEnabled = !!serverless;
 
   const { linkTo: globalLinkTo, timerange: globalTimerange } = inputState.global;
   const { linkTo: timelineLinkTo, timerange: timelineTimerange } = inputState.timeline;
-
-  const socTrendsUrlParams = useMemo(() => {
-    if (isSocTrendsEnabled && inputState.socTrends) {
-      const { linkTo: socTrendsLinkTo, timerange: socTrendsTimerange } = inputState.socTrends;
-      return {
-        socTrends: {
-          [URL_PARAM_KEY.timerange]: socTrendsTimerange,
-          linkTo: socTrendsLinkTo,
-        },
-      };
-    }
-    return {};
-  }, [inputState.socTrends, isSocTrendsEnabled]);
 
   const valueReportUrlParams = useMemo(() => {
     if (isValueReportEnabled && inputState.valueReport) {
@@ -62,7 +48,6 @@ export const useSyncTimerangeUrlParam = () => {
         linkTo: timelineLinkTo,
       },
       ...valueReportUrlParams,
-      ...socTrendsUrlParams,
     });
   }, [
     updateTimerangeUrlParam,
@@ -70,7 +55,6 @@ export const useSyncTimerangeUrlParam = () => {
     globalTimerange,
     timelineLinkTo,
     timelineTimerange,
-    socTrendsUrlParams,
     valueReportUrlParams,
   ]);
 };
