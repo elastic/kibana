0520e77e83f11bab2de930ef7ab3ebd61fb70d62
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2025-10-24 19:27:51 +0200

    chore: clean up

--- src/platform/packages/shared/kbn-unified-metrics-grid/src/components/metrics_grid.tsx
+++ src/platform/packages/shared/kbn-unified-metrics-grid/src/components/metrics_grid.tsx
@@ -90,7 +90,7 @@ export const MetricsGrid = ({
   }, []);
 
   const handleCloseFlyout = useCallback(() => {
-    if (expandedMetric === undefined) {
+    if (!expandedMetric) {
       return;
     }
 
