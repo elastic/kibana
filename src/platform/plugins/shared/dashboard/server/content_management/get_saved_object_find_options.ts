8c84d2aba4adff22c2582a5c124ced9fd0ada99a
Nathan Reese <reese.nathan@gmail.com>
2025-10-24 10:46:17 -0600

    clean up

--- src/platform/plugins/shared/dashboard/server/content_management/get_saved_object_find_options.ts
+++ src/platform/plugins/shared/dashboard/server/content_management/get_saved_object_find_options.ts
@@ -10,32 +10,66 @@
 import { tagsToFindOptions } from '@kbn/content-management-utils';
 import type { SavedObjectsFindOptions } from '@kbn/core-saved-objects-api-server';
 import type { SearchQuery } from '@kbn/content-management-plugin/common';
+import type { DashboardSavedObjectAttributes } from '../dashboard_saved_object';
 import { DASHBOARD_SAVED_OBJECT_TYPE } from '../dashboard_saved_object';
-import type { DashboardSearchOptions } from './latest';
+import type { DashboardSearchOptions, DashboardState } from './latest';
+
+type DashboardStateKeys = keyof DashboardState;
+type StoredDashboardStateKeys = keyof DashboardSavedObjectAttributes;
+
+// Dashboard API field names can not be passed directly
+// to saved object search since dashboard API and
+// saved object fields have diverged.
+function getSavedObjectFields(fields: DashboardStateKeys[]): StoredDashboardStateKeys[] {
+  const soFields: StoredDashboardStateKeys[] = [];
+  fields.forEach((field) => {
+    if (field === 'query' || field === 'filters') {
+      if (!soFields.includes('kibanaSavedObjectMeta')) {
+        soFields.push('kibanaSavedObjectMeta');
+      }
+      return;
+    }
+
+    if (field === 'options') {
+      soFields.push('optionsJSON');
+      return;
+    }
+
+    if (field === 'panels') {
+      soFields.push('panelsJSON');
+      return;
+    }
+
+    if (field === 'timeRange') {
+      // Saved object API stores timeRange as timeFrom, timeTo, and timeRestore
+      // All fields are needed to return timeRange in dashboard REST response
+      soFields.push('timeFrom', 'timeTo', 'timeRestore');
+      return;
+    }
+
+    if (
+      field === 'references' ||
+      field === 'spaces' ||
+      field === 'namespaces' ||
+      field === 'tags'
+    ) {
+      return;
+    }
+
+    soFields.push(field);
+  });
+
+  return soFields;
+}
 
 export function getSavedObjectFindOptions(
   query: SearchQuery,
   options: DashboardSearchOptions
 ): SavedObjectsFindOptions {
-  // options.fields contains dashboard API field names.
-  // Dashboard API field names can not be passed directly
-  // to saved object search since dashboard API and
-  // saved object fields have diverged.
-  const soFields: string[] = [];
-  (options?.fields ?? []).forEach((field) => {
-    if (field === 'timeRange') {
-      // Saved object API stores timeRange as timeFrom, timeTo, and timeRestore
-      // All fields are needed to return timeRange in dashboard REST response
-      soFields.push('timeFrom', 'timeTo', 'timeRestore');
-    } else {
-      soFields.push(field);
-    }
-  });
-
   return {
     type: DASHBOARD_SAVED_OBJECT_TYPE,
     searchFields: options?.onlyTitle ? ['title'] : ['title^3', 'description'],
-    fields: soFields,
+    fields: getSavedObjectFields(options?.fields as DashboardStateKeys[]),
     search: query.text,
     perPage: query.limit,
     page: query.cursor ? +query.cursor : undefined,
