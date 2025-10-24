1cf15d03757071eb76048cf29bd56b8bcdaea5fa
Katerina <aikaterini.patticha@elastic.co>
2025-10-24 17:04:18 +0300

    [Discover][Traces]: consolidate APM context services to avoid dublica… (#239566)
    …te requests
    
    ## Summary
    
    closes #235760
    
    - Refactor context services to centralize fetching error and traces
    indices
    This refactors the APM context services to consolidate the logic for
    fetching error and traces indices, removing duplicate API requests.
    
    ## Before
    
    https://github.com/user-attachments/assets/1551e8d2-51cc-478f-9995-3ca9f78adbc1
    
    ## After
    
    https://github.com/user-attachments/assets/a9149300-e880-4c68-b16d-3370370eb071
    (cherry picked from commit 771f5d4dc59a0d80926fded470bfe699f320ab19)

--- src/platform/packages/shared/kbn-discover-utils/src/__mocks__/apm_context_service.ts
+++ src/platform/packages/shared/kbn-discover-utils/src/__mocks__/apm_context_service.ts
@@ -7,10 +7,10 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export * from './data_view';
-export * from './es_hits';
-export * from './logs_context_service';
-export * from './additional_field_groups';
-export * from './data_table_record';
-export * from './metrics_context_service';
-export * from './apm_context_service';
+import { createErrorsContextServiceMock } from './apm_errors_context_service';
+import { createTracesContextServiceMock } from './traces_context_service';
+
+export const createApmContextServiceMock = () => ({
+  tracesService: createTracesContextServiceMock(),
+  errorsService: createErrorsContextServiceMock(),
+});
