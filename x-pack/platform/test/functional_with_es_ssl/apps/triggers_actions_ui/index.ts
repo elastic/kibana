8b459eed1dbf4ab2dd53e6cfdebd8904d0d87236
Maryam Saeidi <maryam.saeidi@elastic.co>
2025-10-24 15:55:30 +0200

    Split x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/config.ts config

--- x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/index.ts
+++ x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/index.ts
@@ -10,9 +10,7 @@ import type { FtrProviderContext } from '../../ftr_provider_context';
 export default ({ loadTestFile }: FtrProviderContext) => {
   describe('Actions and Triggers app', function () {
     loadTestFile(require.resolve('./home_page'));
-    loadTestFile(require.resolve('./rules_list'));
     loadTestFile(require.resolve('./alert_create_flyout'));
-    loadTestFile(require.resolve('./details'));
     loadTestFile(require.resolve('./connectors'));
     loadTestFile(require.resolve('./logs_list'));
     loadTestFile(require.resolve('./rules_settings'));
