9a91b722d1c5664a99e8c674a33d023d3be65ddb
Julian Gernun <17549662+jcger@users.noreply.github.com>
2025-10-24 14:13:40 +0200

    fix param type

--- x-pack/solutions/observability/plugins/observability/public/rules/custom_threshold_description_fields.ts
+++ x-pack/solutions/observability/plugins/observability/public/rules/custom_threshold_description_fields.ts
@@ -5,13 +5,15 @@
  * 2.0.
  */
 
-import type { SearchConfigurationType } from '@kbn/response-ops-rule-params/common';
 import { RULE_PREBUILD_DESCRIPTION_FIELDS } from '@kbn/triggers-actions-ui-plugin/public';
-import type { GetDescriptionFieldsFn, Rule } from '@kbn/triggers-actions-ui-plugin/public/types';
+import type { GetDescriptionFieldsFn } from '@kbn/triggers-actions-ui-plugin/public/types';
 
-export const getDescriptionFields: GetDescriptionFieldsFn<
-  Omit<Rule, 'searchConfiguration'> & { searchConfiguration: SearchConfigurationType }
-> = ({ rule, prebuildFields }) => {
+import type { CustomThresholdParams } from '@kbn/response-ops-rule-params/custom_threshold/latest';
+
+export const getDescriptionFields: GetDescriptionFieldsFn<CustomThresholdParams> = ({
+  rule,
+  prebuildFields,
+}) => {
   if (!rule || !prebuildFields) return [];
 
   const searchConfig = rule.params.searchConfiguration;
@@ -19,7 +21,7 @@ export const getDescriptionFields: GetDescriptionFieldsFn<
 
   const fields = [];
 
-  if (searchConfig.index) {
+  if (typeof searchConfig.index === 'string') {
     fields.push(
       prebuildFields[RULE_PREBUILD_DESCRIPTION_FIELDS.DATA_VIEW_INDEX_PATTERN](searchConfig.index)
     );
