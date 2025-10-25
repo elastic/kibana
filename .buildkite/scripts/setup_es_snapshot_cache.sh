21af53796d9de0cbd0a110a8ffc034a2f2592612
Dario Gieselaar <dario.gieselaar@elastic.co>
2025-10-25 18:19:10 +0200

    Disable snapshot cache

--- .buildkite/scripts/setup_es_snapshot_cache.sh
+++ .buildkite/scripts/setup_es_snapshot_cache.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+# set -euo pipefail
 
-# If cached snapshots are baked into the agent, move them into our workspace first
-# We are doing this rather than simply changing the ES base path because many workers
-#   run with the workspace mounted in memory or on a local ssd
-cacheDir="$ES_CACHE_DIR/cache"
-if [[ -d "$cacheDir" ]]; then
-  mkdir -p .es/cache
-  echo "--- Move ES snapshot cache"
-  echo "Moving cached snapshots from $cacheDir to .es/cache"
-  mv "$cacheDir"/* .es/cache/
-fi
+# # If cached snapshots are baked into the agent, move them into our workspace first
+# # We are doing this rather than simply changing the ES base path because many workers
+# #   run with the workspace mounted in memory or on a local ssd
+# cacheDir="$ES_CACHE_DIR/cache"
+# if [[ -d "$cacheDir" ]]; then
+#   mkdir -p .es/cache
+#   echo "--- Move ES snapshot cache"
+#   echo "Moving cached snapshots from $cacheDir to .es/cache"
+#   mv "$cacheDir"/* .es/cache/
+# fi
