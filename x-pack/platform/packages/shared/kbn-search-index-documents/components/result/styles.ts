04d58a15d70c049b9d1c23e0c8cb4df03e5ac25a
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2025-10-27 11:14:33 +0300

    Remove backport leftovers

--- x-pack/platform/packages/shared/kbn-search-index-documents/components/result/styles.ts
+++ x-pack/platform/packages/shared/kbn-search-index-documents/components/result/styles.ts
@@ -6,46 +6,6 @@
  */
 
 import { css } from '@emotion/react';
-import styled from '@emotion/styled';
-import { type EuiThemeComputed } from '@elastic/eui';
-
-export const resultField = (euiTheme: EuiThemeComputed<{}>) =>
-  css({
-    padding: 0,
-    borderBottom: `1px solid ${euiTheme.colors.lightShade}`,
-    position: 'relative',
-
-    '&:last-child': {
-      borderBottom: 'none',
-    },
-
-    '> .euiTableRow:hover': {
-      backgroundColor: euiTheme.colors.emptyShade,
-    },
-
-    '> .euiTableRowCell': {
-      borderTop: 'none',
-      borderBottom: 'none',
-
-      '> .euiTableCellContent': {
-        padding: euiTheme.size.s,
-        fontFamily: euiTheme.font.familyCode,
-        color: euiTheme.colors.mediumShade,
-      },
-    },
-
-    '.denseVectorFieldValue': {
-      position: 'absolute',
-      right: 0,
-      top: euiTheme.size.s, // replaced $euiSizeS
-      backgroundColor: euiTheme.colors.emptyShade, // replaced $euiColorEmptyShade
-      padding: `0 ${euiTheme.size.s}`,
-    },
-  });
-
-export const ResultHeader = styled.div<{ euiTheme: EuiThemeComputed<{}> }>`
-  padding: ${({ euiTheme }) => `0 ${euiTheme.size.s} ${euiTheme.size.xs} 0`};
-`;
 
 export const definitionStyle = css({
   textOverflow: 'ellipsis',
