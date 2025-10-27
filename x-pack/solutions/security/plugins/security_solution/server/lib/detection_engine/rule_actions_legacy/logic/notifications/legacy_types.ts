3d4837af7b2b27727739fd624cf474393b973171
Ersin Erdal <ersin.erdal@elastic.co>
2025-10-27 23:22:14 +0100

    revert rule type change

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_actions_legacy/logic/notifications/legacy_types.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_actions_legacy/logic/notifications/legacy_types.ts
@@ -20,7 +20,6 @@ import type {
 } from '@kbn/alerting-plugin/server';
 import type { Rule, RuleAction } from '@kbn/alerting-plugin/common';
 import type { DefaultAlert } from '@kbn/alerts-as-data-utils';
-import type { ALERT_STATE_NAMESPACE } from '@kbn/rule-data-utils';
 import { LEGACY_NOTIFICATIONS_ID } from '../../../../../../common/constants';
 
 /**
@@ -89,10 +88,6 @@ export const isLegacyRuleType = (
   return partialRule.alertTypeId === LEGACY_NOTIFICATIONS_ID;
 };
 
-type LegacyNotificationAlert = DefaultAlert & {
-  [ALERT_STATE_NAMESPACE]: { signals_count: number };
-};
-
 /**
  * @deprecated Once we are confident all rules relying on side-car actions SO's have been migrated to SO references we should remove this function
  */
@@ -102,7 +97,7 @@ export type LegacyNotificationExecutorOptions = RuleExecutorOptions<
   AlertInstanceState,
   AlertInstanceContext,
   'default',
-  LegacyNotificationAlert
+  DefaultAlert
 >;
 
 /**
