e0d4e616b1dfe4bcd1d724a7e34f0586e3ccf6a4
Alex Szabo <alex.szabo@elastic.co>
2025-10-27 13:09:43 +0100

    remove var

--- kbn_pm/src/cli.mjs
+++ kbn_pm/src/cli.mjs
@@ -24,9 +24,8 @@ import { getCmd } from './commands/index.mjs';
 import { Log } from './lib/log.mjs';
 import External from './lib/external_packages.js';
 
-const BIG_CHANGES = 777;
 
-console.log({ BIG_CHANGES });
+
 
 const start = Date.now();
 const args = new Args(process.argv.slice(2), []);
