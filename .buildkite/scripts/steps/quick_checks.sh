d2deb86c6ec79d43ba7348100e4ac7e58dc3257e
Alex Szabo <alex.szabo@elastic.co>
2025-10-27 12:26:25 +0100

    commit changes after the quick-checks ran

--- .buildkite/scripts/steps/quick_checks.sh
+++ .buildkite/scripts/steps/quick_checks.sh
@@ -16,13 +16,17 @@ if [[ "${COLLECT_QUICK_CHECK_CHANGES:-}" == "true" ]]; then
   ORIGINAL_HEAD=$(git rev-parse HEAD)
 fi
 
+set +e
 node scripts/quick_checks --file .buildkite/scripts/steps/checks/quick_checks.json
+exit_status=$?
+set -e
 
 if [[ "${COLLECT_QUICK_CHECK_CHANGES:-}" == "true" ]]; then
   # Check if any commits were made during the checks
   CURRENT_HEAD=$(git rev-parse HEAD)
 
   if [[ "$ORIGINAL_HEAD" != "$CURRENT_HEAD" ]]; then
+    echo "--- Committing quick_checks changes"
     echo "Changes were committed during quick checks. Pushing to remote..."
 
     if ! is_auto_commit_disabled && [[ "${BUILDKITE_PULL_REQUEST:-false}" != "false" ]]; then
@@ -40,3 +44,5 @@ if [[ "${COLLECT_QUICK_CHECK_CHANGES:-}" == "true" ]]; then
   export COLLECT_QUICK_CHECK_CHANGES=false
   check_for_changed_files 'node scripts/quick_checks' true "Collected changes from quick checks"
 fi
+
+exit $exit_status
