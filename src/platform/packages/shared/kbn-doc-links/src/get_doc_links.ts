91948154799023897fb8b5481388ef668d092b29
Konrad Szwarc <konrad.szwarc@elastic.co>
2025-10-27 11:22:45 +0100

    [EDR Workflows][Device Control] Fix trusted devices documentation link (#240463)
    Proper documentation link for "Learn more" about trusted devices.
    
    (cherry picked from commit 4938c5e780a2c025150014b6c4cac3353dd4d863)

--- src/platform/packages/shared/kbn-doc-links/src/get_doc_links.ts
+++ src/platform/packages/shared/kbn-doc-links/src/get_doc_links.ts
@@ -449,7 +449,7 @@ export const getDocLinks = ({ kibanaBranch, buildFlavor }: GetDocLinkOptions): D
       avcResults: `https://www.elastic.co/blog/elastic-security-av-comparatives-business-test`,
       bidirectionalIntegrations: `${ELASTIC_DOCS}solutions/security/endpoint-response-actions/third-party-response-actions`,
       trustedApps: `${ELASTIC_DOCS}solutions/security/manage-elastic-defend/trusted-applications`,
-      trustedDevices: `${ELASTIC_DOCS}solutions/security/manage-elastic-defend/trusted-applications`, // TODO: Update this link when trusted devices is available
+      trustedDevices: `${ELASTIC_DOCS}solutions/security/manage-elastic-defend/trusted-devices`,
       elasticAiFeatures: `${ELASTIC_DOCS}solutions/security/ai`,
       eventFilters: `${ELASTIC_DOCS}solutions/security/manage-elastic-defend/event-filters`,
       blocklist: `${ELASTIC_DOCS}solutions/security/manage-elastic-defend/blocklist`,
