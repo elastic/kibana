364f172b9894f5c54bc451868326ce8c523fe971
christineweng <weng67osu@gmail.com>
2025-10-28 14:19:28 -0500

    remove draggable wrapper

---
+++ x-pack/solutions/security/plugins/security_solution/public/common/components/cell_actions/cell_actions_renderer.test.tsx
@@ -0,0 +1,268 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { render } from '@testing-library/react';
+import React from 'react';
+import { TableId } from '@kbn/securitysolution-data-table';
+import { TestProviders } from '../../mock';
+import { TimelineId } from '../../../../common/types';
+import { ROW_RENDERER_BROWSER_EXAMPLE_TIMELINE_ID } from '../../../timelines/components/row_renderers_browser/constants';
+
+import { CellActionsRenderer } from './cell_actions_renderer';
+
+jest.mock('../../lib/kibana');
+
+const MockSecurityCellActions = jest.fn(({ children }: { children: React.ReactNode }) => (
+  <div data-test-subj="mockSecurityCellActions">{children}</div>
+));
+jest.mock('.', () => ({
+  ...jest.requireActual('.'),
+  SecurityCellActions: (params: { children: React.ReactNode }) => MockSecurityCellActions(params),
+}));
+
+const mockSourcererScopeId = 'testSourcererScopeId';
+jest.mock('../../../helpers', () => ({
+  getSourcererScopeId: jest.fn(() => mockSourcererScopeId),
+}));
+
+describe('cell actions renderer', () => {
+  describe('rendering', () => {
+    test('it renders theCellActionsRenderer', () => {
+      const { container } = render(
+        <CellActionsRenderer field="some-field" value="some-value" queryValue="some-query-value">
+          <span>{'A child of this'}</span>
+        </CellActionsRenderer>
+      );
+      expect(container.firstChild).toMatchSnapshot();
+    });
+  });
+
+  describe('CellActionsRenderer', () => {
+    test('it works with just an id, field, and value and is some value', () => {
+      const field = 'some-field';
+      const value = 'some value';
+      const { getByText } = render(
+        <TestProviders>
+          <CellActionsRenderer field={field} value={value} />
+        </TestProviders>
+      );
+      expect(getByText(value)).toBeInTheDocument();
+    });
+
+    test('it returns null if value is undefined', () => {
+      const { container } = render(<CellActionsRenderer field="some-field" value={undefined} />);
+      expect(container.firstChild).toBeNull();
+    });
+
+    test('it returns null if value is null', () => {
+      const { container } = render(<CellActionsRenderer field="some-field" value={null} />);
+      expect(container.firstChild).toBeNull();
+    });
+
+    test('it renders content with tooltip when tooltip is not explicitly provided', () => {
+      const { getByText } = render(
+        <TestProviders>
+          <CellActionsRenderer field="source.bytes" value="a field value" />
+        </TestProviders>
+      );
+
+      expect(getByText('a field value')).toBeInTheDocument();
+    });
+
+    test('it renders content with custom tooltip when string is provided', () => {
+      const { getByText } = render(
+        <TestProviders>
+          <CellActionsRenderer
+            field="source.bytes"
+            tooltipContent="default string tooltip"
+            value="a field value"
+          />
+        </TestProviders>
+      );
+
+      expect(getByText('a field value')).toBeInTheDocument();
+    });
+
+    test('it renders content with custom tooltip when element is provided', () => {
+      const { getByText } = render(
+        <TestProviders>
+          <CellActionsRenderer
+            field="source.bytes"
+            tooltipContent={<span>{'tooltip'}</span>}
+            value="a field value"
+          />
+        </TestProviders>
+      );
+
+      expect(getByText('a field value')).toBeInTheDocument();
+    });
+
+    test('it does NOT render a tooltip when tooltipContent is null', () => {
+      const { queryByTestId } = render(
+        <TestProviders>
+          <CellActionsRenderer field="source.bytes" tooltipContent={null} value="a field value" />
+        </TestProviders>
+      );
+
+      expect(queryByTestId('source.bytes-tooltip')).not.toBeInTheDocument();
+    });
+  });
+
+  describe('CellActionsRenderer functionality', () => {
+    const field = 'host.name';
+    const value = '12345';
+    const data = { field, value };
+
+    const scopeIdsWithHoverActions = [
+      undefined,
+      TimelineId.active,
+      TableId.alternateTest,
+      TimelineId.casePage,
+      TableId.alertsOnAlertsPage,
+      TableId.alertsOnRuleDetailsPage,
+      TableId.hostsPageEvents,
+      TableId.hostsPageSessions,
+      TableId.kubernetesPageSessions,
+      TableId.networkPageEvents,
+      TimelineId.test,
+      TableId.usersPageEvents,
+    ];
+
+    const scopeIdsNoHoverActions = [TableId.rulePreview, ROW_RENDERER_BROWSER_EXAMPLE_TIMELINE_ID];
+
+    beforeEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should render cell actions with the content', () => {
+      const result = render(
+        <CellActionsRenderer field={field} value={value}>
+          {'test children'}
+        </CellActionsRenderer>
+      );
+      expect(result.queryByTestId('mockSecurityCellActions')).toBeInTheDocument();
+      expect(result.queryByText('test children')).toBeInTheDocument();
+    });
+
+    it('should call cell actions with the default props', () => {
+      render(
+        <CellActionsRenderer field={field} value={value}>
+          {'test children'}
+        </CellActionsRenderer>
+      );
+      expect(MockSecurityCellActions).toHaveBeenCalledWith(
+        expect.objectContaining({
+          data,
+          mode: 'hover-down',
+          sourcererScopeId: mockSourcererScopeId,
+          disabledActionTypes: [],
+          metadata: { scopeId: TimelineId.active },
+        })
+      );
+    });
+
+    describe('when value is empty', () => {
+      it('should set an empty array value to the SecurityCellActions component', () => {
+        render(
+          <CellActionsRenderer field={field} value={''}>
+            {'test children'}
+          </CellActionsRenderer>
+        );
+        expect(MockSecurityCellActions).toHaveBeenCalledWith(
+          expect.objectContaining({ data: { ...data, value: [] } })
+        );
+      });
+    });
+
+    describe('when queryValue is provided', () => {
+      it('should set value to queryValue', () => {
+        render(
+          <CellActionsRenderer field={field} value={value} queryValue="testQueryValue">
+            {'test children'}
+          </CellActionsRenderer>
+        );
+        expect(MockSecurityCellActions).toHaveBeenCalledWith(
+          expect.objectContaining({ data: { ...data, value: 'testQueryValue' } })
+        );
+      });
+    });
+
+    describe('when scopeId is defined', () => {
+      it('should set the scopeId to the SecurityCellActions component metadata', () => {
+        render(
+          <CellActionsRenderer field={field} value={value} scopeId="testScopeId">
+            {'test children'}
+          </CellActionsRenderer>
+        );
+        expect(MockSecurityCellActions).toHaveBeenCalledWith(
+          expect.objectContaining({ metadata: { scopeId: 'testScopeId' } })
+        );
+      });
+    });
+
+    describe('when hideTopN is true', () => {
+      it('should set the disabledActionTypes to the SecurityCellActions component', () => {
+        render(
+          <CellActionsRenderer field={field} value={value} hideTopN>
+            {'test children'}
+          </CellActionsRenderer>
+        );
+        expect(MockSecurityCellActions).toHaveBeenCalledWith(
+          expect.objectContaining({ disabledActionTypes: ['security-cellAction-type-showTopN'] })
+        );
+      });
+    });
+
+    scopeIdsWithHoverActions.forEach((scopeId) => {
+      test(`it renders hover actions (by default) when scopeId is '${scopeId}'`, async () => {
+        const { getByTestId } = render(
+          <CellActionsRenderer field={field} value={value} scopeId={scopeId}>
+            {'test children'}
+          </CellActionsRenderer>
+        );
+        expect(getByTestId('mockSecurityCellActions')).toBeInTheDocument();
+      });
+    });
+
+    scopeIdsNoHoverActions.forEach((scopeId) => {
+      test(`it does NOT render hover actions when scopeId is '${scopeId}'`, async () => {
+        const { queryByTestId } = render(
+          <CellActionsRenderer field={field} value={value} scopeId={scopeId}>
+            {'test children'}
+          </CellActionsRenderer>
+        );
+        expect(queryByTestId('mockSecurityCellActions')).not.toBeInTheDocument();
+      });
+    });
+
+    describe('text truncation styling', () => {
+      test('it applies text truncation styling when truncate IS specified', () => {
+        const { getByTestId } = render(
+          <TestProviders>
+            <CellActionsRenderer field={field} value={value} truncate>
+              {'test children'}
+            </CellActionsRenderer>
+          </TestProviders>
+        );
+
+        expect(getByTestId('render-truncatable-content')).toBeInTheDocument();
+      });
+
+      test('it does NOT apply text truncation styling when truncate is NOT specified', () => {
+        const { queryByTestId } = render(
+          <TestProviders>
+            <CellActionsRenderer field={field} value={value}>
+              {'test children'}
+            </CellActionsRenderer>
+          </TestProviders>
+        );
+
+        expect(queryByTestId('render-truncatable-content')).not.toBeInTheDocument();
+      });
+    });
+  });
+});
---
+++ x-pack/solutions/security/plugins/security_solution/public/common/components/cell_actions/cell_actions_renderer.tsx
@@ -0,0 +1,177 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { ToolTipPositions } from '@elastic/eui';
+import { EuiToolTip } from '@elastic/eui';
+import React, { useContext, useMemo } from 'react';
+import styled from '@emotion/styled';
+import { TimelineId } from '../../../../common/types';
+import type { SecurityCellActionsData } from '.';
+import {
+  CellActionsMode,
+  SecurityCellActions,
+  SecurityCellActionsTrigger,
+  SecurityCellActionType,
+} from '.';
+import { getSourcererScopeId } from '../../../helpers';
+import { TimelineContext } from '../../../timelines/components/timeline/context';
+import { TruncatableText } from '../truncatable_text';
+import { TableContext } from '../events_viewer/shared';
+import {
+  disableHoverActions,
+  tooltipContentIsExplicitlyNull,
+  getDefaultWhenTooltipIsUnspecified,
+} from './utils';
+
+export const ProviderContentWrapper = styled.span`
+  > span.euiToolTipAnchor {
+    display: block; /* allow EuiTooltip content to be truncatable */
+  }
+
+  > span.euiToolTipAnchor.eui-textTruncate {
+    display: inline-block; /* do not override display when a tooltip is truncated via eui-textTruncate */
+  }
+`;
+
+export interface CellActionsRendererProps {
+  hideTopN?: boolean;
+  field: string;
+  value?: string | number | null;
+  queryValue?: string | null;
+  children?: React.ReactNode;
+  scopeId?: string;
+  tooltipContent?: React.ReactNode;
+  tooltipPosition?: ToolTipPositions;
+  truncate?: boolean;
+}
+
+/**
+ * Renders the content of the cell actions, wrapped in a tooltip
+ */
+export const Content = React.memo<{
+  children?: React.ReactNode;
+  field: string;
+  tooltipContent?: React.ReactNode;
+  tooltipPosition?: ToolTipPositions;
+  value?: string | number | null;
+}>(({ children, field, tooltipContent, tooltipPosition, value }) =>
+  !tooltipContentIsExplicitlyNull(tooltipContent) ? (
+    <EuiToolTip
+      data-test-subj={`${field}-tooltip`}
+      position={tooltipPosition}
+      content={getDefaultWhenTooltipIsUnspecified({ tooltipContent, field })}
+    >
+      <>{children ? children : value}</>
+    </EuiToolTip>
+  ) : (
+    <>{children ? children : value}</>
+  )
+);
+
+Content.displayName = 'Content';
+
+/**
+ * Renders cell actions content (or an arbitrary visualization specified by `children`)
+ * that's only displayed when the specified value is non-`null`.
+ * @param field - the name of the field, e.g. `network.transport`
+ * @param value - value of the field e.g. `tcp`
+ * @param children - defaults to displaying `value`, this allows an arbitrary visualization to be displayed in lieu of the default behavior
+ * @param tooltipContent - defaults to displaying `field`, pass `null` to
+ * prevent a tooltip from being displayed, or pass arbitrary content
+ * @param tooltipPosition - defaults to eui's default tooltip position
+ * @param queryValue - defaults to `value`, this query overrides the `queryMatch.value` used by the `DataProvider` that represents the data
+ * @param hideTopN - defaults to `false`, when true, the option to aggregate this field will be hidden
+ * @param scopeId - the id of the scope, e.g. `timelineId` or `tableId`
+ * @param truncate - defaults to `false`, when true, the content will be truncated
+ */
+export const CellActionsRenderer = React.memo<CellActionsRendererProps>(
+  ({
+    hideTopN = false,
+    field,
+    value,
+    children,
+    scopeId = TimelineId.active,
+    tooltipContent,
+    tooltipPosition,
+    queryValue,
+    truncate = false,
+  }) => {
+    const { timelineId: timelineIdFind } = useContext(TimelineContext);
+    const { tableId: tableIdFind } = useContext(TableContext);
+
+    const sourcererScopeId = useMemo(
+      () => getSourcererScopeId(scopeId ?? timelineIdFind ?? tableIdFind),
+      [scopeId, tableIdFind, timelineIdFind]
+    );
+
+    const data = useMemo<SecurityCellActionsData>(() => {
+      if (queryValue) {
+        return { value: queryValue, field };
+      }
+      return { value: value || [], field };
+    }, [value, field, queryValue]);
+
+    const disabledActionTypes = useMemo(
+      () => (hideTopN ? [SecurityCellActionType.SHOW_TOP_N] : []),
+      [hideTopN]
+    );
+
+    const content = useMemo(
+      () => (
+        <div tabIndex={-1}>
+          {truncate ? (
+            <TruncatableText data-test-subj="render-truncatable-content">
+              <Content
+                field={field}
+                tooltipContent={tooltipContent}
+                tooltipPosition={tooltipPosition}
+                value={value}
+              >
+                {children}
+              </Content>
+            </TruncatableText>
+          ) : (
+            <ProviderContentWrapper data-test-subj={`render-content-${field}`}>
+              <Content
+                field={field}
+                tooltipContent={tooltipContent}
+                tooltipPosition={tooltipPosition}
+                value={value}
+              >
+                {children}
+              </Content>
+            </ProviderContentWrapper>
+          )}
+        </div>
+      ),
+      [children, field, truncate, tooltipContent, tooltipPosition, value]
+    );
+
+    if (value == null) return null;
+
+    if (disableHoverActions(scopeId)) {
+      return <>{content}</>;
+    }
+
+    return (
+      <SecurityCellActions
+        mode={CellActionsMode.HOVER_DOWN}
+        visibleCellActions={6}
+        showActionTooltips
+        triggerId={SecurityCellActionsTrigger.DEFAULT}
+        data={data}
+        disabledActionTypes={disabledActionTypes}
+        sourcererScopeId={sourcererScopeId}
+        metadata={{ scopeId }}
+      >
+        {content}
+      </SecurityCellActions>
+    );
+  }
+);
+
+CellActionsRenderer.displayName = 'CellActionsRenderer';
