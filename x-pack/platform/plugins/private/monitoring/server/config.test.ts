efc9a4a1bdf8a17533ab0e79a63dd41b80fb7e9a
Hammad Nasir <hammad.nasir@elastic.co>
2025-10-28 11:34:14 -0400

    adding pingTimeout field back into xpack config.test.ts as its still in the config validation

--- x-pack/platform/plugins/private/monitoring/server/config.test.ts
+++ x-pack/platform/plugins/private/monitoring/server/config.test.ts
@@ -72,6 +72,7 @@ describe('config schema', () => {
             "maxIdleSockets": 256,
             "maxResponseSize": false,
             "maxSockets": 800,
+            "pingTimeout": "PT30S",
             "requestHeadersWhitelist": Array [
               "authorization",
               "es-client-authentication",
