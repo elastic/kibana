7ed756319ec1c2c05c2a006105c552055bf09729
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-10-29 11:10:28 -0400

    [Fleet] Fix space awareness agent action tests

--- x-pack/platform/test/fleet_api_integration/apis/space_awareness/actions.ts
+++ x-pack/platform/test/fleet_api_integration/apis/space_awareness/actions.ts
@@ -28,7 +28,7 @@ export default function (providerContext: FtrProviderContext) {
   let TEST_SPACE_1: string;
 
   // Failing: See https://github.com/elastic/kibana/issues/236306
-  describe.skip('actions', function () {
+  describe('actions', function () {
     skipIfNoDockerRegistry(providerContext);
     const apiClient = new SpaceTestApiClient(supertest);
 
