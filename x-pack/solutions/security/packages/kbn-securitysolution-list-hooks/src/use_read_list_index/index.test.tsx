556a4efbd5ebe47b0c3f8f6e633fa86ff683bfe1
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-10-29 05:10:41 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/security/packages/kbn-securitysolution-list-hooks/src/use_read_list_index/index.test.tsx
+++ x-pack/solutions/security/packages/kbn-securitysolution-list-hooks/src/use_read_list_index/index.test.tsx
@@ -13,7 +13,7 @@
  */
 
 import React from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from '@kbn/react-query';
 import { waitFor, renderHook } from '@testing-library/react';
 
 import type { SecurityAppError } from '@kbn/securitysolution-t-grid';
