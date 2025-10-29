68eeb16e8e10966383c099ede6074bbf4b325901
Alex Szabo <alex.szabo@elastic.co>
2025-10-29 14:32:22 +0100

    get extended log from typecheck, and upload it

--- .buildkite/scripts/steps/typecheck/check_types.sh
+++ .buildkite/scripts/steps/typecheck/check_types.sh
@@ -10,7 +10,17 @@ source .buildkite/scripts/common/util.sh
 restore_target_folders
 
 echo --- Check Types
-node scripts/type_check
+set +e
+node scripts/type_check --extendedDiagnostics > type_check.log
+EXIT_CODE=$?
+set -e
+
+if [[ -f 'type_check.log' ]]; then
+  tail -n 50 type_check.log
+  buildkite-agent artifact upload type_check.log
+fi
 
 # Try to archive target folders for future caching
 archive_target_folders
+
+exit $EXIT_CODE
