e58a6e6bec37c224d5484245b4ec9627aab0c802
Brad White <brad.white@elastic.co>
2025-10-30 12:08:26 +1000

    word

--- packages/kbn-ci-stats-shipper-cli/ship_ci_stats_cli.ts
+++ packages/kbn-ci-stats-shipper-cli/ship_ci_stats_cli.ts
@@ -51,7 +51,7 @@ run(
       for (const metric of metrics) {
         if (metric.limit !== undefined && metric.limit < metric.value) {
           overLimit.push(
-            `${metric.group} for ${metric.id} plugin is greater than the limit of ${metric.limit}. The current value of ${metric.value}.
+            `${metric.group} for ${metric.id} plugin is greater than the limit of ${metric.limit}. The current value is ${metric.value}.
             To update the limit, run the following command locally:
             node scripts/build_kibana_platform_plugins --focus ${metric.id} --update-limits
             `
