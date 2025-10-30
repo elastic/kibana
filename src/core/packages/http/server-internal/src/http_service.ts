3297b32d3e91b249119f7e6593d4ed8ac497a60a
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-10-30 18:42:32 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/core/packages/http/server-internal/src/http_service.ts
+++ src/core/packages/http/server-internal/src/http_service.ts
@@ -107,7 +107,7 @@ export class HttpService
 
   public async preboot(deps: PrebootDeps): Promise<InternalHttpServicePreboot> {
     this.log.debug('setting up preboot server');
-    
+
     const config = await firstValueFrom(this.config$);
 
     const prebootSetup = await this.prebootServer.setup({
@@ -117,7 +117,9 @@ export class HttpService
       path: '/{p*}',
       method: '*',
       handler: (req, responseToolkit) => {
-        this.log.debug(`Kibana server is not ready yet ${req.method}:${req.url.href}. For troubleshooting guidance, see ${deps.docLinks.links.server.troubleshootServerNotReady}`);
+        this.log.debug(
+          `Kibana server is not ready yet ${req.method}:${req.url.href}. For troubleshooting guidance, see ${deps.docLinks.links.server.troubleshootServerNotReady}`
+        );
 
         // If server is not ready yet, because plugins or core can perform
         // long running tasks (build assets, saved objects migrations etc.)
