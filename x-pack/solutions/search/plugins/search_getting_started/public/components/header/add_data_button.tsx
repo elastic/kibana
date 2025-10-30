04699d5f4c90832a8d4b68545344134e160a75c5
Matt Wilde <matt.wilde@elastic.co>
2025-10-30 12:06:45 -0400

    upload file link goes to add data page

--- x-pack/solutions/search/plugins/search_getting_started/public/components/header/add_data_button.tsx
+++ x-pack/solutions/search/plugins/search_getting_started/public/components/header/add_data_button.tsx
@@ -32,7 +32,7 @@ export const AddDataButton: React.FC = () => {
       data-test-subj="gettingStartedUploadMenuItem"
       onClick={() => {
         closePopover();
-        application.navigateToApp('ml', { path: 'filedatavisualizer' });
+        application.navigateToApp('home', { path: '#/tutorial_directory/sampleData' });
       }}
     >
       {i18n.translate('xpack.search.gettingStarted.addDataButton.uploadFile', {
