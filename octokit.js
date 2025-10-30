72d3e10b50a2d9dc8b6c421db410934646fe27d8
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-10-30 15:26:10 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- octokit.js
+++ octokit.js
@@ -1,7 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
 const { Octokit } = require('@octokit/rest');
 const github = new Octokit({ auth: process.env.GITHUB_TOKEN });
 async function main() {
-const content = await github.rest.repos.getContent({
+  const content = await github.rest.repos.getContent({
     mediaType: {
       format: 'application/vnd.github.VERSION.raw',
     },
