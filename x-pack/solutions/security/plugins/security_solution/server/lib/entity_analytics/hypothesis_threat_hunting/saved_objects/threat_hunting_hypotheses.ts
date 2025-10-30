850180b1ae8da77bfbc428b40b6ba4a5248209a7
CAWilson94 <charlotte.wilson@elastic.co>
2025-10-30 17:21:13 +0000

    WiP threat hypotheses schema and saved objects type plus logger and folder structure

---
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/hypothesis_threat_hunting/saved_objects/threat_hunting_hypotheses.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import type { SavedObjectsClientContract } from '@kbn/core/server';
+import { threatHuntingHypothesesTypeName } from './threat_hunting_hypotheses_type';
+
+interface ThreatHuntingHypothesesDependencies {
+  soClient: SavedObjectsClientContract;
+  namespace: string;
+}
+
+export class ThreatHuntingHypothesesClient {
+  constructor(private readonly deps: ThreatHuntingHypothesesDependencies) {}
+  getSavedObjectId() {
+    return `threat-hunting-hypotheses-${this.deps.namespace}`;
+  }
+
+  async get() {
+    // TODO: Implement ThreatHuntingHypotheses Schema
+    const { attributes } = await this.deps.soClient.get<ThreatHuntingHypotheses>(
+      threatHuntingHypothesesTypeName,
+      this.getSavedObjectId()
+    );
+    return attributes;
+  }
+  async delete() {
+    const id = this.getSavedObjectId();
+    await this.deps.soClient.delete(threatHuntingHypothesesTypeName, id);
+  }
+}
