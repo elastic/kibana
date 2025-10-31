487036e6603eef858684e6f7a5e1f632131e88c1
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-10-31 11:58:27 +0100

    Update EUI to v108.0.0 (#240888)
    - `@elastic/eui`: `v107.0.1` ⏩ `v108.0.0`
    - `@elastic/eui-theme-borealis`: `v3.6.0` ⏩ `v4.0.0`
    
    ---
    
    ## Changes
    
    >[!IMPORTANT]
    This PR removes code related to the legacy Amsterdam theme. But it
    purposefully keeps Amsterdam palettes in `kbn-palettes` as those are
    standalone.
    
    - removed code related to the legacy theme Amsterdam
    [[#9090](https://github.com/elastic/eui/pull/9090)]
    - updated custom types to align with changes to the event type of
    `onClose` on `EuiModal`
    [[#9137](https://github.com/elastic/eui/pull/9137)]
    
    ## Package updates
    
    ### [`v108.0.0`](https://github.com/elastic/eui/releases/v108.0.0)
    
    - Updated `EuiModal` to support closing on outside click, via the new
    `outsideClickCloses` prop
    ([#9137](https://github.com/elastic/eui/pull/9137))
    
    **Breaking changes**
    
    - Removed all "Amsterdam" theme related code in `src/themes/amsterdam` -
    EUI now only supports the "Borealis" theme in `eui-theme-borealis`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed `euiTheme.flags.hasGlobalFocusColor`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed `euiTheme.flags.hasVisColorAdjustment`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed `hasVisColorAdjustment` argument from color palettes (used in
    `euiPaletteColorBlindBehindText`, `euiPaletteForTemperature`,
    `euiPaletteComplementary`, `euiPaletteCool`)
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed `euiTheme.flags.buttonVariant` and
    `euiTheme.flags.formVariant`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed `euiTheme.components.keyPadMenuItemBackgroundDisabledSelect`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed legacy SCSS files from `src/global_styling/variables`,
    `src/global_styling/mixins` and `src/global_styling/functions` - if
    needed, use them from `eui-theme-common` instead
    ([#9090](https://github.com/elastic/eui/pull/9090))
    
    ### [`v4.0.0`](https://github.com/elastic/eui/releases/v4.0.0)
    
    **Breaking changes**
    
    - Removed `euiTheme.flags.hasGlobalFocusColor`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed `euiTheme.flags.hasVisColorAdjustment`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed `euiTheme.flags.buttonVariant` and
    `euiTheme.flags.formVariant`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    - Removed `euiTheme.components.keyPadMenuItemBackgroundDisabledSelect`
    ([#9090](https://github.com/elastic/eui/pull/9090))
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- src/core/packages/rendering/server-internal/src/bootstrap/bootstrap_renderer.ts
+++ src/core/packages/rendering/server-internal/src/bootstrap/bootstrap_renderer.ts
@@ -83,9 +83,7 @@ export const bootstrapRendererFactory: BootstrapRendererFactory = ({
     }
 
     const colorMode = darkMode === false ? 'light' : darkMode === true ? 'dark' : 'system';
-    // Amsterdam theme is called `v8` internally
-    // and should be kept this way for compatibility reasons.
-    const themeTagName = themeName === 'amsterdam' ? 'v8' : themeName;
+    const themeTagName = themeName;
     const bundlesHref = getBundlesHref(baseHref);
 
     const bundlePaths = getPluginsBundlePaths({
