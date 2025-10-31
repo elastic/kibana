89eb4305776304aae290c5d49df960f9fd4ca165
christineweng <18648970+christineweng@users.noreply.github.com>
2025-10-31 14:27:42 -0500

    [Cases] Add total observables to cases SO mapping (#241388)
    ## Summary
    
    This PR adds `total_observables` to SO mapping so that we can do
    aggregation on how many cases have reached maximum number of observables
    allowed.
    
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [x] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/plugins/shared/cases/server/saved_object_types/cases/schemas/v8.ts
+++ x-pack/platform/plugins/shared/cases/server/saved_object_types/cases/schemas/v8.ts
@@ -4,5 +4,9 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { schema } from '@kbn/config-schema';
+import { casesSchema as casesSchemaV7 } from './v7';
 
-export * from './v8';
+export const casesSchema = casesSchemaV7.extends({
+  total_observables: schema.number(),
+});
