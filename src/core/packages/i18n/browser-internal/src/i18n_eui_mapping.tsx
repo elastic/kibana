39d290e8c7942797d94683046f0bd98db1f40b65
Lene Gadewoll <lene.gadewoll@elastic.co>
2025-10-31 12:49:22 +0100

    chore: update i18n
    - triggered due to flyout system changes

--- src/core/packages/i18n/browser-internal/src/i18n_eui_mapping.tsx
+++ src/core/packages/i18n/browser-internal/src/i18n_eui_mapping.tsx
@@ -1857,5 +1857,11 @@ export const getEuiContextMapping = (): EuiTokensObject => {
     'euiColorPicker.selectedColorLabel': i18n.translate('core.euiColorPicker.selectedColorLabel', {
       defaultMessage: 'Selected color',
     }),
+    'euiFlyoutMenu.back': i18n.translate('core.euiFlyoutMenu.back', {
+      defaultMessage: 'Back',
+    }),
+    'euiFlyoutMenu.history': i18n.translate('core.euiFlyoutMenu.history', {
+      defaultMessage: 'History',
+    }),
   };
 };
