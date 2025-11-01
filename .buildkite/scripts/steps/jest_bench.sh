cab69a300370add935cfbf6ef66d3556ed46ea98
Brad White <brad.white@elastic.co>
2025-11-01 14:18:00 +1000

    adjust runs and logging

--- .buildkite/scripts/steps/jest_bench.sh
+++ .buildkite/scripts/steps/jest_bench.sh
@@ -5,7 +5,7 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/bootstrap.sh
 
 echo '--- Jest Bench against Merge Base'
-node scripts/bench.js --config src/platform/packages/shared/kbn-jest-benchmarks/benchmark.config.ts --right "${GITHUB_PR_MERGE_BASE}" --profile
+node scripts/bench.js --config src/platform/packages/shared/kbn-jest-benchmarks/benchmark.config.ts --right "${GITHUB_PR_MERGE_BASE}" --profile --runs 2 --debug
 
 # Always exit 0 to avoid blocking the build
 exit 0
