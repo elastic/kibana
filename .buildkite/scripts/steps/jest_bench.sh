852c2139e0cb87fe81a88754abf66e4a57ebcc5e
Brad White <brad.white@elastic.co>
2025-11-01 11:54:32 +1000

    wording

--- .buildkite/scripts/steps/jest_bench.sh
+++ .buildkite/scripts/steps/jest_bench.sh
@@ -4,5 +4,5 @@ source .buildkite/scripts/common/util.sh
 
 .buildkite/scripts/bootstrap.sh
 
-echo '--- Jest Bench with Merge Base'
+echo '--- Jest Bench against Merge Base'
 node scripts/bench.js --config src/platform/packages/shared/kbn-jest-benchmarks/benchmark.config.ts --right "${GITHUB_PR_MERGE_BASE}" --profile
