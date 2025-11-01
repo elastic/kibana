1c32be1214170d36232bbf60ef13d2350f852ed4
Brad White <brad.white@elastic.co>
2025-11-01 11:52:00 +1000

    add bench call

--- .buildkite/scripts/steps/jest_bench.sh
+++ .buildkite/scripts/steps/jest_bench.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 
+source .buildkite/scripts/common/util.sh
+
+.buildkite/scripts/bootstrap.sh
+
+echo '--- Jest Bench with Merge Base'
+node scripts/bench.js --config src/platform/packages/shared/kbn-jest-benchmarks/benchmark.config.ts --right "${GITHUB_PR_MERGE_BASE}" --profile
