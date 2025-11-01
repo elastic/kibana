a6d3362501dbea305e2544b08d254465f43b9db4
Valerio Bartolini <vabarjs@gmail.com>
2025-11-01 23:57:52 +0100

    [ES|QL] subquery support for after columns and validation

--- src/platform/packages/shared/kbn-esql-ast/src/commands_registry/commands/from/columns_after.ts
+++ src/platform/packages/shared/kbn-esql-ast/src/commands_registry/commands/from/columns_after.ts
@@ -6,15 +6,49 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
-import { type ESQLCommand } from '../../../types';
+import { uniqBy } from 'lodash';
+import { esqlCommandRegistry } from '../..';
+import { isSubQuery } from '../../../ast/is';
+import { type ESQLCommand, type ESQLAstQueryExpression } from '../../../types';
 import type { ESQLColumnData } from '../../types';
 import type { IAdditionalFields } from '../../registry';
 
-export const columnsAfter = (
+export const columnsAfter = async (
   command: ESQLCommand,
-  _previousColumns: ESQLColumnData[], // will always be empty for FROM
-  _query: string,
+  _previousColumns: ESQLColumnData[],
+  query: string,
   additionalFields: IAdditionalFields
 ) => {
-  return additionalFields.fromFrom(command);
+  // Process each source/subquery: FROM index1, (FROM index2 | KEEP a), index3
+  const promises = command.args.map((arg) => {
+    if (isSubQuery(arg)) {
+      return processSubquery(arg.child, query, additionalFields);
+    } else {
+      return additionalFields.fromFrom({ ...command, args: [arg] });
+    }
+  });
+
+  const results = await Promise.all(promises);
+  const allColumns = results.flat();
+
+  return uniqBy(allColumns, 'name');
 };
+
+async function processSubquery(
+  subquery: ESQLAstQueryExpression,
+  query: string,
+  additionalFields: IAdditionalFields
+): Promise<ESQLColumnData[]> {
+  let columns: ESQLColumnData[] = [];
+
+  // Execute each command in subquery pipeline: FROM index | KEEP
+  for (const subCommand of subquery.commands) {
+    const commandDef = esqlCommandRegistry.getCommandByName(subCommand.name);
+
+    if (commandDef?.methods?.columnsAfter) {
+      columns = await commandDef.methods.columnsAfter(subCommand, columns, query, additionalFields);
+    }
+  }
+
+  return columns;
+}
