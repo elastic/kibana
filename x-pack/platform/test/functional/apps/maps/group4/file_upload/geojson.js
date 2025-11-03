7ae9a5531317607ea36aa874ed76a1b5de02f143
Ola Pawlus <ola.pawlus@elastic.co>
2025-11-03 11:14:56 +0100

    revert changes

--- x-pack/platform/test/functional/apps/maps/group4/file_upload/geojson.js
+++ x-pack/platform/test/functional/apps/maps/group4/file_upload/geojson.js
@@ -63,9 +63,7 @@ export default function ({ getPageObjects, getService }) {
       await retry.try(async () => {
         await maps.waitForLayersToLoad();
         const tooltipText = await maps.getLayerTocTooltipMsg(indexName);
-        expect(tooltipText).to.contain(`${indexName}`);
-        expect(tooltipText).to.match(/Found ~\d+ documents/);
-        expect(tooltipText).to.contain('This count is approximate.');
+        expect(tooltipText).to.be(`${indexName}\nFound ~281 documents. This count is approximate.`);
       });
     });
   });
