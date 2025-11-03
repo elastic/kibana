2aa3805ff7740f12ba2ee3fb799065adf91b093e
Omri Rosner <omri.rosner@elastic.co>
2025-11-03 17:37:37 +0200

    conflicts

--- src/platform/plugins/shared/workflows_management/public/features/workflow_execution_detail/ui/workflow_execution_panel.tsx
+++ src/platform/plugins/shared/workflows_management/public/features/workflow_execution_detail/ui/workflow_execution_panel.tsx
@@ -20,11 +20,10 @@ import {
 import React from 'react';
 import { i18n } from '@kbn/i18n';
 import type { WorkflowExecutionDto, WorkflowYaml } from '@kbn/workflows';
-import { ExecutionStatus } from '@kbn/workflows';
+import { ExecutionStatus, isCancelableStatus } from '@kbn/workflows';
 import { CancelExecutionButton } from './cancel_execution_button';
 import { WorkflowStepExecutionTree } from './workflow_step_execution_tree';
 import { WorkflowExecutionListItem } from '../../workflow_execution_list/ui/workflow_execution_list_item';
-import { isCancelableStatus } from '../lib/execution_status';
 
 const i18nTexts = {
   backToExecutions: i18n.translate('workflows.workflowStepExecutionList.backToExecution', {
