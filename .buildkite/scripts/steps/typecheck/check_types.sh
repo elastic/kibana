a1b69c966eadc60c10c0d75aebe7582cf6826594
Dario Gieselaar <dario.gieselaar@elastic.co>
2025-11-04 14:41:59 +0100

    Remove legacy upload

--- .buildkite/scripts/steps/typecheck/check_types.sh
+++ .buildkite/scripts/steps/typecheck/check_types.sh
@@ -9,12 +9,3 @@ source .buildkite/scripts/common/util.sh
 echo --- Check Types
 set +e
 node scripts/type_check --with-archive
-EXIT_CODE=$?
-set -e
-
-if [[ -f 'type_check.log' ]]; then
-  tail -n 50 type_check.log
-  buildkite-agent artifact upload type_check.log
-fi
-
-exit $EXIT_CODE
