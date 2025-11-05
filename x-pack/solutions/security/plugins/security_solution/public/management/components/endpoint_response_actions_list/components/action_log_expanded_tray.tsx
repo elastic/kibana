95e6fe54b3ddcb823c54456c587505d9f5272568
Paul Tavares <paul.tavares@elastic.co>
2025-11-05 16:50:30 -0500

    remove old output component from ActionLogExpandedTray

--- x-pack/solutions/security/plugins/security_solution/public/management/components/endpoint_response_actions_list/components/action_log_expanded_tray.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/management/components/endpoint_response_actions_list/components/action_log_expanded_tray.tsx
@@ -6,34 +6,15 @@
  */
 
 import React, { memo, useMemo } from 'react';
-import {
-  EuiCodeBlock,
-  EuiDescriptionList,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiSpacer,
-} from '@elastic/eui';
+import { EuiCodeBlock, EuiDescriptionList, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { css, euiStyled } from '@kbn/kibana-react-plugin/common';
 import { reduce } from 'lodash';
 import { i18n } from '@kbn/i18n';
-import { RunscriptActionResult } from '../../runscript_action_result';
-import { RunningProcessesActionResults } from '../../running_processes_action_results';
+import { ActionResponseOutputs } from './action_response_outputs';
 import { getAgentTypeName } from '../../../../common/translations';
 import { RESPONSE_ACTION_API_COMMAND_TO_CONSOLE_COMMAND_MAP } from '../../../../../common/endpoint/service/response_actions/constants';
-import {
-  isExecuteAction,
-  isGetFileAction,
-  isProcessesAction,
-  isRunScriptAction,
-  isUploadAction,
-} from '../../../../../common/endpoint/service/response_actions/type_guards';
-import { EndpointUploadActionResult } from '../../endpoint_upload_action_result';
-import { useUserPrivileges } from '../../../../common/components/user_privileges';
 import { OUTPUT_MESSAGES } from '../translations';
 import { useTestIdGenerator } from '../../../hooks/use_test_id_generator';
-import { ResponseActionFileDownloadLink } from '../../response_action_file_download_link';
-import { EndpointActionFailureMessage } from '../../endpoint_action_failure_message';
-import { ExecuteActionHostResponse } from '../../endpoint_execute_action';
 import { getEmptyValue } from '../../../../common/components/empty_value';
 import { type ActionDetails, type MaybeImmutable } from '../../../../../common/endpoint/types';
 
@@ -93,167 +74,6 @@ const StyledEuiFlexGroup = euiStyled(EuiFlexGroup).attrs({
   min-height: 270px;
   overflow-y: auto;
 `;
-
-const OutputContent = memo<{
-  action: MaybeImmutable<ActionDetails>;
-  fromAlertWorkaround?: boolean;
-  'data-test-subj'?: string;
-}>(({ action, fromAlertWorkaround = false, 'data-test-subj': dataTestSubj }) => {
-  const getTestId = useTestIdGenerator(dataTestSubj);
-
-  const {
-    canWriteFileOperations,
-    canReadActionsLogManagement,
-    canAccessEndpointActionsLogManagement,
-  } = useUserPrivileges().endpointPrivileges;
-
-  const { command: _command, isCompleted, isExpired, wasSuccessful } = action;
-  const command = RESPONSE_ACTION_API_COMMAND_TO_CONSOLE_COMMAND_MAP[_command];
-
-  // FIXME:PT remove once automated response actions are corrected to use `ActionDetails` (team issue 9822)
-  if (fromAlertWorkaround && action.errors?.length) {
-    return (
-      <>
-        {(
-          action.errors ?? [
-            i18n.translate('xpack.securitySolution.actionLogExpandedTray.missingErrors', {
-              defaultMessage: 'Action did not specify any errors',
-            }),
-          ]
-        ).map((error) => (
-          <EuiFlexItem>{error}</EuiFlexItem>
-        ))}
-      </>
-    );
-  }
-
-  if (isExpired) {
-    return <>{OUTPUT_MESSAGES.hasExpired(command)}</>;
-  }
-
-  if (!isCompleted) {
-    return <>{OUTPUT_MESSAGES.isPending(command)}</>;
-  }
-
-  if (!wasSuccessful) {
-    return (
-      <>
-        {OUTPUT_MESSAGES.hasFailed(command)}
-        <EuiSpacer size="s" />
-        <EndpointActionFailureMessage
-          action={action}
-          data-test-subj={getTestId('failureMessage')}
-        />
-      </>
-    );
-  }
-
-  if (isGetFileAction(action)) {
-    return (
-      <>
-        {OUTPUT_MESSAGES.wasSuccessful(command)}
-        <ResponseActionFileDownloadLink
-          action={action}
-          canAccessFileDownloadLink={canWriteFileOperations}
-          textSize="xs"
-          data-test-subj={getTestId('getFileDownloadLink')}
-        />
-      </>
-    );
-  }
-
-  if (isExecuteAction(action)) {
-    return (
-      <EuiFlexGroup direction="column" data-test-subj={getTestId('executeDetails')}>
-        {action.agents.map((agentId) => (
-          <div key={agentId}>
-            {OUTPUT_MESSAGES.wasSuccessful(command)}
-            <ExecuteActionHostResponse
-              action={action}
-              agentId={agentId}
-              canAccessFileDownloadLink={
-                canAccessEndpointActionsLogManagement || canReadActionsLogManagement
-              }
-              textSize="xs"
-              data-test-subj={getTestId('actionsLogTray')}
-            />
-          </div>
-        ))}
-      </EuiFlexGroup>
-    );
-  }
-
-  if (isUploadAction(action)) {
-    return (
-      <EuiFlexGroup direction="column" data-test-subj={getTestId('uploadDetails')}>
-        <p>{OUTPUT_MESSAGES.wasSuccessful(command)}</p>
-
-        <EndpointUploadActionResult
-          action={action}
-          data-test-subj={getTestId('uploadOutput')}
-          textSize="xs"
-        />
-      </EuiFlexGroup>
-    );
-  }
-
-  if (isProcessesAction(action)) {
-    return (
-      <EuiFlexGroup direction="column" data-test-subj={getTestId('processesDetails')}>
-        <p>{OUTPUT_MESSAGES.wasSuccessful(command)}</p>
-
-        <RunningProcessesActionResults
-          action={action}
-          data-test-subj="processesOutput"
-          textSize="xs"
-        />
-      </EuiFlexGroup>
-    );
-  }
-
-  if (isRunScriptAction(action)) {
-    return (
-      <EuiFlexGroup direction="column" data-test-subj={getTestId('runScriptDetails')}>
-        {action.agents.map((agentId) => (
-          <div key={agentId}>
-            {OUTPUT_MESSAGES.wasSuccessful(command)}
-
-            {action.agentType === 'sentinel_one' ? (
-              <RunscriptActionResult
-                action={action}
-                agentId={agentId}
-                textSize="xs"
-                data-test-subj={getTestId('actionsLogTray')}
-              />
-            ) : (
-              <ExecuteActionHostResponse
-                action={action}
-                agentId={agentId}
-                canAccessFileDownloadLink={
-                  canAccessEndpointActionsLogManagement || canReadActionsLogManagement
-                }
-                textSize="xs"
-                data-test-subj={getTestId('actionsLogTray')}
-                hideFile={action.agentType === 'crowdstrike'}
-                hideContext={true}
-                showPasscode={action.agentType !== 'microsoft_defender_endpoint'}
-              />
-            )}
-          </div>
-        ))}
-      </EuiFlexGroup>
-    );
-  }
-
-  // CrowdStrike Isolate/Release actions
-  if (action.agentType === 'crowdstrike') {
-    return <>{OUTPUT_MESSAGES.submittedSuccessfully(command)}</>;
-  }
-
-  return <>{OUTPUT_MESSAGES.wasSuccessful(command)}</>;
-});
-
-OutputContent.displayName = 'OutputContent';
 
 export const ActionsLogExpandedTray = memo<{
   action: MaybeImmutable<ActionDetails>;
@@ -354,16 +174,26 @@ export const ActionsLogExpandedTray = memo<{
         description: (
           // codeblock for output
           <StyledEuiCodeBlock data-test-subj={getTestId('details-tray-output')}>
-            <OutputContent
-              action={action}
-              data-test-subj={dataTestSubj}
-              fromAlertWorkaround={fromAlertWorkaround}
-            />
+            {fromAlertWorkaround && action.errors?.length ? (
+              <>
+                {(
+                  action.errors ?? [
+                    i18n.translate('xpack.securitySolution.actionLogExpandedTray.missingErrors', {
+                      defaultMessage: 'Action did not specify any errors',
+                    }),
+                  ]
+                ).map((error) => (
+                  <EuiFlexItem>{error}</EuiFlexItem>
+                ))}
+              </>
+            ) : (
+              <ActionResponseOutputs action={action} data-test-subj="output" />
+            )}
           </StyledEuiCodeBlock>
         ),
       },
     ],
-    [action, dataTestSubj, fromAlertWorkaround, getTestId]
+    [action, fromAlertWorkaround, getTestId]
   );
 
   return (
