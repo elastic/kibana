aec8e8a53b4752f9e6a884c5d3adf2a8d5e93e27
Brad White <brad.white@elastic.co>
2025-11-06 09:31:13 +1000

    set left, right for step script

--- .buildkite/scripts/steps/jest_bench.sh
+++ .buildkite/scripts/steps/jest_bench.sh
@@ -5,7 +5,7 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/bootstrap.sh
 
 echo '--- Jest Bench against Merge Base'
-node scripts/bench.js --config src/platform/packages/shared/kbn-jest-benchmarks/benchmark.config.ts --right "${GITHUB_PR_MERGE_BASE}" --profile --debug
+node scripts/bench.js --config src/platform/packages/shared/kbn-jest-benchmarks/benchmark.config.ts --left "${GITHUB_PR_MERGE_BASE}" --right "${BUILDKITE_COMMIT}" --profile --debug --config-from-cwd
 
 # For now, exit 0 to avoid blocking the build
 exit 0
