785be31421211b28f7cb789def2f76d66dff475e
Ihor Panasiuk <igorskynet13@gmail.com>
2025-11-05 14:07:44 +0100

    add createIndexes function

--- src/platform/plugins/shared/workflows_execution_engine/common/mappings.ts
+++ src/platform/plugins/shared/workflows_execution_engine/common/mappings.ts
@@ -96,3 +96,114 @@ export const WORKFLOWS_STEP_EXECUTIONS_INDEX_MAPPINGS: MappingTypeMapping = {
     },
   },
 };
+
+export const WORKFLOW_EXECUTION_LOGS_INDEX_MAPPINGS: MappingTypeMapping = {
+  dynamic: false,
+  properties: {
+    '@timestamp': {
+      type: 'date',
+    },
+    spaceId: {
+      type: 'keyword',
+    },
+    message: {
+      type: 'text',
+      fields: {
+        keyword: {
+          type: 'keyword',
+          ignore_above: 1024,
+        },
+      },
+    },
+    level: {
+      type: 'keyword',
+    },
+    workflow: {
+      type: 'object',
+      properties: {
+        id: {
+          type: 'keyword',
+        },
+        name: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              ignore_above: 256,
+            },
+          },
+        },
+        execution_id: {
+          type: 'keyword',
+        },
+        step_id: {
+          type: 'keyword',
+        },
+        step_name: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              ignore_above: 256,
+            },
+          },
+        },
+        step_type: {
+          type: 'keyword',
+        },
+      },
+    },
+    event: {
+      type: 'object',
+      properties: {
+        action: {
+          type: 'keyword',
+        },
+        category: {
+          type: 'keyword',
+        },
+        type: {
+          type: 'keyword',
+        },
+        provider: {
+          type: 'keyword',
+        },
+        outcome: {
+          type: 'keyword',
+        },
+        duration: {
+          type: 'long',
+        },
+        start: {
+          type: 'date',
+        },
+        end: {
+          type: 'date',
+        },
+      },
+    },
+    error: {
+      type: 'object',
+      properties: {
+        message: {
+          type: 'text',
+          fields: {
+            keyword: {
+              type: 'keyword',
+              ignore_above: 1024,
+            },
+          },
+        },
+        type: {
+          type: 'keyword',
+        },
+        stack_trace: {
+          type: 'text',
+        },
+      },
+    },
+    tags: {
+      type: 'keyword',
+    },
+  },
+};
