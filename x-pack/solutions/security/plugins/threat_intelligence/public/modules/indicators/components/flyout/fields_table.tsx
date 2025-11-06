a711333751b6f45860c62288a610c02b572bbc4d
Agustina Nahir Ruidiaz <61565784+agusruidiazgd@users.noreply.github.com>
2025-11-06 14:09:33 +0100

    [Security Solution][Intelligence] support multiple values in IOC flyout table tab (#236110)
    ## Summary
    
    Closes https://github.com/elastic/kibana/issues/227161
    
    https://github.com/user-attachments/assets/fdbf6f8e-3572-493f-8ed0-033648c7fe4b
    
    https://github.com/user-attachments/assets/42e77701-34f4-4a91-869d-2aefa6dea61f
    
    ### Checklist
    
    Check the PR satisfies following conditions.
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 9a16f5437b0549f34348b5a44032a108bf5402ba)
    
    # Conflicts:
    #	x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/modules/indicators/components/flyout/fields_table.tsx
    #	x-pack/solutions/security/plugins/threat_intelligence/public/modules/indicators/components/common/field_value.tsx

--- x-pack/solutions/security/plugins/threat_intelligence/public/modules/indicators/components/flyout/fields_table.tsx
+++ x-pack/solutions/security/plugins/threat_intelligence/public/modules/indicators/components/flyout/fields_table.tsx
@@ -11,12 +11,18 @@ import React, { useMemo, VFC } from 'react';
 import { Indicator } from '../../../../../common/types/indicator';
 import { IndicatorFieldValue } from '../common/field_value';
 import { IndicatorValueActions } from './indicator_value_actions';
+import { unwrapValue } from '../../utils/unwrap_value';
 
+interface TableItem {
+  key: string;
+  value: string | string[] | null;
+}
 export interface IndicatorFieldsTableProps {
   fields: string[];
   indicator: Indicator;
   search: EuiInMemoryTableProps['search'];
   ['data-test-subj']?: string;
+  compressed?: boolean;
 }
 
 export const IndicatorFieldsTable: VFC<IndicatorFieldsTableProps> = ({
@@ -64,10 +70,22 @@ export const IndicatorFieldsTable: VFC<IndicatorFieldsTableProps> = ({
     [indicator, dataTestSubj]
   );
 
+  const items = useMemo(() => {
+    return fields.toSorted().reduce<TableItem[]>((acc, field) => {
+      const value = unwrapValue(indicator, field);
+      return [
+        ...acc,
+        {
+          key: field,
+          value,
+        },
+      ];
+    }, []);
+  }, [fields, indicator]);
+
   return (
     <EuiInMemoryTable
-      // @ts-expect-error - EuiInMemoryTable wants an array of objects, but will accept strings if coerced
-      items={fields.sort()}
+      items={items}
       // @ts-expect-error - EuiInMemoryTable wants an array of objects, but will accept strings if coerced
       columns={columns}
       sorting={true}
