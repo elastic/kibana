8449bdcd25f91ea7eeee0c36f873671a42b85b8b
Sergi Romeu <sergi.romeu@elastic.co>
2025-11-06 12:58:04 +0100

    uncomment other tests

--- x-pack/solutions/observability/test/api_integration_deployment_agnostic/apis/apm/alerts/index.ts
+++ x-pack/solutions/observability/test/api_integration_deployment_agnostic/apis/apm/alerts/index.ts
@@ -9,11 +9,11 @@ import type { DeploymentAgnosticFtrProviderContext } from '../../../ftr_provider
 
 export default function ({ loadTestFile }: DeploymentAgnosticFtrProviderContext) {
   describe('alerts', () => {
-    // loadTestFile(require.resolve('./error_count_threshold.spec.ts'));
-    // loadTestFile(require.resolve('./preview_chart_error_count.spec.ts'));
-    // loadTestFile(require.resolve('./preview_chart_error_rate.spec.ts'));
-    // loadTestFile(require.resolve('./preview_chart_transaction_duration.spec.ts'));
+    loadTestFile(require.resolve('./error_count_threshold.spec.ts'));
+    loadTestFile(require.resolve('./preview_chart_error_count.spec.ts'));
+    loadTestFile(require.resolve('./preview_chart_error_rate.spec.ts'));
+    loadTestFile(require.resolve('./preview_chart_transaction_duration.spec.ts'));
     loadTestFile(require.resolve('./transaction_duration.spec.ts'));
-    // loadTestFile(require.resolve('./transaction_error_rate.spec.ts'));
+    loadTestFile(require.resolve('./transaction_error_rate.spec.ts'));
   });
 }
