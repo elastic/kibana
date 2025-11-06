69a10b63395f6602e4066ab25ed92811a5c564b0
lgestc <lukasz.gmys@elastic.co>
2025-11-06 12:56:19 +0100

    --wip-- [skip ci]

--- x-pack/platform/plugins/shared/cases/server/services/mocks.ts
+++ x-pack/platform/plugins/shared/cases/server/services/mocks.ts
@@ -158,7 +158,7 @@ const createAttachmentGetterServiceMock = (): AttachmentGetterServiceMock => {
   const service: PublicMethodsOf<AttachmentGetter> = lazyObject({
     get: jest.fn(),
     bulkGet: jest.fn(),
-    getAllAlertsAttachToCase: jest.fn(),
+    getAllDocumentsAttachedToCase: jest.fn(),
     getCaseAttatchmentStats: jest.fn(),
     getAttachmentIdsForCases: jest.fn(),
     getFileAttachments: jest.fn(),
