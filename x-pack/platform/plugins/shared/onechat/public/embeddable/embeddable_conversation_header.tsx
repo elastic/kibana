e994530c47ba3a7a89f175029b7e6d307fdf4617
Chris Martin <chris.martin@elastic.co>
2025-11-06 15:47:29 +0000

    pr feedback

--- x-pack/platform/plugins/shared/onechat/public/embeddable/embeddable_conversation_header.tsx
+++ x-pack/platform/plugins/shared/onechat/public/embeddable/embeddable_conversation_header.tsx
@@ -11,6 +11,25 @@ import { i18n } from '@kbn/i18n';
 import { css } from '@emotion/react';
 import { useConversationTitle } from '../application/hooks/use_conversation';
 
+const labels = {
+  ariaLabel: i18n.translate('xpack.onechat.embeddable.conversationHeader.ariaLabel', {
+    defaultMessage: 'Conversation title',
+  }),
+  newConversationDisplay: i18n.translate(
+    'xpack.onechat.embeddable.conversationHeader.newConversationDisplay',
+    {
+      defaultMessage: 'New conversation',
+    }
+  ),
+};
+
+const containerStyles = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
 interface EmbeddableConversationHeaderProps {
   onClose: () => void;
   ariaLabelledBy: string;
@@ -22,40 +41,21 @@ export const EmbeddableConversationHeader: React.FC<EmbeddableConversationHeader
 }) => {
   const { title, isLoading } = useConversationTitle();
 
-  const labels = {
-    ariaLabel: i18n.translate('xpack.onechat.conversationTitle.ariaLabel', {
-      defaultMessage: 'Conversation title',
-    }),
-    newConversationDisplay: i18n.translate(
-      'xpack.onechat.conversationTitle.newConversationDisplay',
-      {
-        defaultMessage: 'New conversation',
-      }
-    ),
-  };
-
-  const titleDisplayText = isLoading ? '' : title ? title : labels.newConversationDisplay;
-
-  const containerStyles = css`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 100%;
-  `;
+  const titleDisplayText = isLoading ? '' : title || labels.newConversationDisplay;
 
   return (
     <div css={containerStyles}>
       <EuiTitle
         aria-label={labels.ariaLabel}
         size="xxs"
-        data-test-subj="agentBuilderConversationTitle"
+        data-test-subj="agentBuilderEmbeddableConversationTitle"
       >
         <h1 id={ariaLabelledBy}>{titleDisplayText}</h1>
       </EuiTitle>
       <EuiButtonIcon
         iconType="cross"
         onClick={onClose}
-        aria-label={i18n.translate('xpack.onechat.embeddableConversationHeader.closeLabel', {
+        aria-label={i18n.translate('xpack.onechat.embeddable.conversationHeader.closeLabel', {
           defaultMessage: 'Close',
         })}
       />
