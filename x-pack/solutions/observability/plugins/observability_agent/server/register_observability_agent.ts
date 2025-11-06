81c2844a0f58f506a30d6ecd92992c1d73a96187
Søren Louv-Jansen <sorenlouv@gmail.com>
2025-11-06 12:43:53 +0100

    Update x-pack/solutions/observability/plugins/observability_agent/server/register_observability_agent.ts
    Co-authored-by: Viduni Wickramarachchi <viduni.ushanka@gmail.com>

--- x-pack/solutions/observability/plugins/observability_agent/server/register_observability_agent.ts
+++ x-pack/solutions/observability/plugins/observability_agent/server/register_observability_agent.ts
@@ -64,7 +64,7 @@ export async function registerObservabilityAgent({
     return;
   }
 
-  const observabilityGetServicesTool = await createObservabilityGetServicesTool({
+  const getServicesTool = await createObservabilityGetServicesTool({
     core,
     plugins,
     logger,
