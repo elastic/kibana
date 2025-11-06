eaa58c0e6c3ece0813f2871a6beed7ea00155486
Alex Szabo <alex.szabo@elastic.co>
2025-11-06 11:08:57 +0100

    unskip lint_ts_projects quick check

--- .buildkite/scripts/steps/checks/ts_projects.sh
+++ .buildkite/scripts/steps/checks/ts_projects.sh
@@ -5,12 +5,10 @@ set -euo pipefail
 source .buildkite/scripts/common/util.sh
 
 echo --- Lint TS Projects
-# Skip this temporarily, there's some issue with linting:
-# cannot reference @kbn/fleet-plugin or @kbn/osquery-plugin from inside their ./cypress folders
-#cmd="node scripts/lint_ts_projects"
-#if is_pr && ! is_auto_commit_disabled; then
-#  cmd="$cmd --fix"
-#fi
-#
-#eval "$cmd"
-#check_for_changed_files "$cmd" true
+cmd="node scripts/lint_ts_projects"
+if is_pr && ! is_auto_commit_disabled; then
+  cmd="$cmd --fix"
+fi
+
+eval "$cmd"
+check_for_changed_files "$cmd" true
