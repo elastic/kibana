dc3e0b92be15dc5066565b60f2a688bfe857801c
Tiago Costa <tiago.costa@elastic.co>
2025-11-07 13:39:36 +0000

    fix(NA): cypress config paths for fleet plugin

--- x-pack/platform/plugins/shared/fleet/cypress/cypress.config.space_awareness.ts
+++ x-pack/platform/plugins/shared/fleet/cypress/cypress.config.space_awareness.ts
@@ -23,10 +23,10 @@ export default defineCypressConfig({
     grepFilterSpecs: false,
   },
 
-  screenshotsFolder: '../../../../../target/kibana-fleet/cypress/screenshots',
+  screenshotsFolder: '../../../../../../target/kibana-fleet/cypress/screenshots',
   trashAssetsBeforeRuns: false,
   video: false,
-  videosFolder: '../../../../../target/kibana-fleet/cypress/videos',
+  videosFolder: '../../../../../../target/kibana-fleet/cypress/videos',
   viewportHeight: 900,
   viewportWidth: 1440,
   screenshotOnRunFailure: true,
@@ -38,8 +38,8 @@ export default defineCypressConfig({
     experimentalMemoryManagement: true,
     numTestsKeptInMemory: 3,
 
-    specPattern: './cypress/e2e/space_awareness/**/*.cy.ts',
-    supportFile: './cypress/support/e2e.ts',
+    specPattern: './e2e/space_awareness/**/*.cy.ts',
+    supportFile: './support/e2e.ts',
 
     setupNodeEvents(on, config) {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
--- x-pack/platform/plugins/shared/fleet/cypress/cypress.config.ts
+++ x-pack/platform/plugins/shared/fleet/cypress/cypress.config.ts
@@ -22,10 +22,10 @@ export default defineCypressConfig({
     grepFilterSpecs: false,
   },
 
-  screenshotsFolder: '../../../../../target/kibana-fleet/cypress/screenshots',
+  screenshotsFolder: '../../../../../../target/kibana-fleet/cypress/screenshots',
   trashAssetsBeforeRuns: false,
   video: false,
-  videosFolder: '../../../../../target/kibana-fleet/cypress/videos',
+  videosFolder: '../../../../../../target/kibana-fleet/cypress/videos',
   viewportHeight: 900,
   viewportWidth: 1440,
   screenshotOnRunFailure: true,
@@ -37,9 +37,9 @@ export default defineCypressConfig({
     experimentalMemoryManagement: true,
     numTestsKeptInMemory: 3,
 
-    specPattern: './cypress/e2e/**/*.cy.ts',
-    supportFile: './cypress/support/e2e.ts',
-    excludeSpecPattern: './cypress/e2e/space_awareness/**/*.cy.ts',
+    specPattern: './e2e/**/*.cy.ts',
+    supportFile: './support/e2e.ts',
+    excludeSpecPattern: './e2e/space_awareness/**/*.cy.ts',
 
     setupNodeEvents(on, config) {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
