ca8973e47cf8225a80c4102dfd6b9e53f950993f
Justin Kambic <jk@elastic.co>
2025-11-07 16:46:16 -0500

    Test Tabs.

--- x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/rules_page.ts
+++ x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/rules_page.ts
@@ -47,5 +47,76 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
         await testSubjects.existOrFail('rulesList');
       });
     });
+
+    describe('Tab Functionality', () => {
+      before(async () => {
+        await pageObjects.common.navigateToApp('rules');
+        await pageObjects.header.waitUntilLoadingHasFinished();
+      });
+
+      it('Rules tab is selected by default', async () => {
+        await retry.try(async () => {
+          const url = await browser.getCurrentUrl();
+          expect(url).to.contain('/app/rules');
+          expect(url).to.not.contain('/app/rules/logs');
+        });
+        await testSubjects.existOrFail('rulesList');
+      });
+
+      it('Logs tab is visible when user has permissions', async () => {
+        const logsTabExists = await testSubjects.exists('logsTab');
+        expect(logsTabExists).to.be(true);
+      });
+
+      it('clicking Logs tab navigates to /app/rules/logs', async () => {
+        await testSubjects.click('logsTab');
+        await pageObjects.header.waitUntilLoadingHasFinished();
+
+        await retry.try(async () => {
+          const url = await browser.getCurrentUrl();
+          expect(url).to.contain('/app/rules/logs');
+        });
+      });
+
+      it('clicking Rules tab navigates to /app/rules', async () => {
+        await testSubjects.click('rulesTab');
+        await pageObjects.header.waitUntilLoadingHasFinished();
+
+        await retry.try(async () => {
+          const url = await browser.getCurrentUrl();
+          expect(url).to.contain('/app/rules');
+          expect(url).to.not.contain('/app/rules/logs');
+        });
+        await testSubjects.existOrFail('rulesList');
+      });
+
+      it('URL updates correctly when switching tabs', async () => {
+        await testSubjects.click('rulesTab');
+        await pageObjects.header.waitUntilLoadingHasFinished();
+
+        await retry.try(async () => {
+          const url = await browser.getCurrentUrl();
+          expect(url).to.contain('/app/rules');
+          expect(url).to.not.contain('/app/rules/logs');
+        });
+
+        await testSubjects.click('logsTab');
+        await pageObjects.header.waitUntilLoadingHasFinished();
+
+        await retry.try(async () => {
+          const url = await browser.getCurrentUrl();
+          expect(url).to.contain('/app/rules/logs');
+        });
+
+        await testSubjects.click('rulesTab');
+        await pageObjects.header.waitUntilLoadingHasFinished();
+
+        await retry.try(async () => {
+          const url = await browser.getCurrentUrl();
+          expect(url).to.contain('/app/rules');
+          expect(url).to.not.contain('/app/rules/logs');
+        });
+      });
+    });
   });
 };
