46f461b49bbb0fc6cbcba220d8ebfd6f4170d003
Julia Bardi <julia.bardi@elastic.co>
2025-11-07 12:55:04 +0100

    fix SO error

--- x-pack/platform/plugins/shared/fleet/server/saved_objects/index.ts
+++ x-pack/platform/plugins/shared/fleet/server/saved_objects/index.ts
@@ -33,7 +33,7 @@ import {
   SPACE_SETTINGS_SAVED_OBJECT_TYPE,
 } from '../constants';
 
-import { SettingsResponseSchemaV5 } from '../types';
+import { SettingsSchemaV5 } from '../types';
 
 import { migrateSyntheticsPackagePolicyToV8120 } from './migrations/synthetics/to_v8_12_0';
 
@@ -243,8 +243,7 @@ export const getSavedObjectTypes = (
             },
           ],
           schemas: {
-            forwardCompatibility: SettingsResponseSchemaV5.extends({}, { unknowns: 'ignore' }),
-            create: SettingsResponseSchemaV5,
+            forwardCompatibility: SettingsSchemaV5.extends({}, { unknowns: 'ignore' }),
           },
         },
       },
