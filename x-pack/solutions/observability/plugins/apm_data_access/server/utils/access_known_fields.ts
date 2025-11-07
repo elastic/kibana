ab0c68565e7767208719aa5869a7a5008f677464
Gonçalo Rica <goncalo.rica@elastic.co>
2025-11-07 10:36:33 +0100

    Document the method and interfaces + example usages

--- x-pack/solutions/observability/plugins/apm_data_access/server/utils/access_known_fields.ts
+++ x-pack/solutions/observability/plugins/apm_data_access/server/utils/access_known_fields.ts
@@ -18,9 +18,22 @@ type RequiredApmFields<
   R extends keyof FlattenedApmEvent
 > = Partial<T> & Required<Pick<T, R>>;
 
+/**
+ * A Proxied APM document that is strongly typed and runtime checked to be correct.
+ * Accessing fields from the document will correctly return single or multi values
+ * according to known field types.
+ */
+type ProxiedApmEvent<
+  T extends Partial<FlattenedApmEvent>,
+  R extends keyof FlattenedApmEvent
+> = MapToSingleOrMultiValue<RequiredApmFields<T, R>>;
+
 const KNOWN_SINGLE_VALUED_FIELDS_SET = new Set<string>(KNOWN_SINGLE_VALUED_FIELDS);
 
-export interface Unflatten<T extends Partial<FlattenedApmEvent>> {
+/**
+ * Interface for exposing an `unflatten()` method on proxied APM documents.
+ */
+interface UnflattenApmDocument<T extends Partial<FlattenedApmEvent>> {
   /**
    * Unflattens the APM Event, so fields can be accessed via `event.service?.name`.
    *
@@ -33,10 +46,28 @@ export interface Unflatten<T extends Partial<FlattenedApmEvent>> {
   unflatten(): UnflattenedKnownFields<T>;
 }
 
+/**
+ * Validates an APM Event document, checking if it has all the required fields if provided,
+ * returning a proxied version of the document to allow strongly typed access to known single
+ * or multi-value fields. The proxy also exposes an `unflatten()` method to return an unflattened
+ * version of the document.
+ *
+ * ## Example
+ *
+ * ```ts
+ * const event = accessKnownApmEventFields(hit, ['service.name']);
+ *
+ * // The key is strongly typed to be `keyof FlattenedApmEvent`.
+ * console.log(event['service.name']) // => outputs `"node-svc"`, not `["node-svc"]` as in the original doc
+ *
+ * const unflattened = event.unflatten();
+ *
+ * console.log(unflattened.service.name); // => outputs "node-svc" like above
+ */
 export function accessKnownApmEventFields<
   T extends Partial<FlattenedApmEvent>,
   R extends keyof FlattenedApmEvent
->(fields: T, required?: R[]): Unflatten<T> & MapToSingleOrMultiValue<RequiredApmFields<T, R>>;
+>(fields: T, required?: R[]): UnflattenApmDocument<T> & ProxiedApmEvent<T, R>;
 
 export function accessKnownApmEventFields(fields: Record<string, any>, required?: string[]) {
   if (required) {
