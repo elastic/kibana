a430ea297bfa98e52676a5ca1253dbcee88b8bc1
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-11-10 19:44:15 +0100

    Add cross-tab syncing to PersistedLog

--- src/platform/plugins/shared/data/public/query/persisted_log/persisted_log.ts
+++ src/platform/plugins/shared/data/public/query/persisted_log/persisted_log.ts
@@ -20,6 +20,7 @@ interface PersistedLogOptions<T = any> {
   maxLength?: number | string;
   filterDuplicates?: boolean;
   isDuplicate?: (oldItem: T, newItem: T) => boolean;
+  enableBrowserTabsSync?: boolean;
 }
 
 export class PersistedLog<T = any> {
@@ -31,6 +32,9 @@ export class PersistedLog<T = any> {
   public items: T[];
 
   private update$ = new Rx.BehaviorSubject(undefined);
+  private storageEventListener?: (event: StorageEvent) => void;
+  private enableBrowserTabsSync: boolean;
+  private subscriberCount = 0;
 
   constructor(name: string, options: PersistedLogOptions<T> = {}, storage: IStorageWrapper) {
     this.name = name;
@@ -42,8 +46,39 @@ export class PersistedLog<T = any> {
     this.isDuplicate = options.isDuplicate || defaultIsDuplicate;
     this.storage = storage;
     this.items = this.storage.get(this.name) || [];
+    this.enableBrowserTabsSync = options?.enableBrowserTabsSync || false;
+
     if (this.maxLength !== undefined && !isNaN(this.maxLength)) {
       this.items = _.take(this.items, this.maxLength);
+    }
+  }
+
+  /** Keeps browser tabs in sync. */
+  private addStorageEventListener() {
+    if (typeof window !== 'undefined' && !this.storageEventListener) {
+      this.storageEventListener = (event: StorageEvent) => {
+        // Only handle events for this specific storage key
+        if (event.key === this.name && event.newValue !== null) {
+          try {
+            const newItems = JSON.parse(event.newValue);
+            // Only update if the items have actually changed
+            if (!_.isEqual(this.items, newItems)) {
+              this.items = newItems;
+              this.update$.next(undefined);
+            }
+          } catch (error) {
+            return;
+          }
+        }
+      };
+      window.addEventListener('storage', this.storageEventListener);
+    }
+  }
+
+  private removeStorageEventListener() {
+    if (typeof window !== 'undefined' && this.storageEventListener) {
+      window.removeEventListener('storage', this.storageEventListener);
+      this.storageEventListener = undefined;
     }
   }
 
@@ -76,7 +111,26 @@ export class PersistedLog<T = any> {
     return _.cloneDeep(this.items);
   }
 
-  public get$() {
-    return this.update$.pipe(map(() => this.get()));
+  public get$(): Rx.Observable<T[]> {
+    return new Rx.Observable<T[]>((subscriber) => {
+      // Increment subscriber count and add listener if this is the first subscriber
+      this.subscriberCount++;
+      if (this.subscriberCount === 1 && this.enableBrowserTabsSync) {
+        this.addStorageEventListener();
+      }
+
+      // Subscribe to the internal update stream
+      const subscription = this.update$.pipe(map(() => this.get())).subscribe(subscriber);
+
+      // Cleanup function called when this subscriber unsubscribes
+      return () => {
+        subscription.unsubscribe();
+        // Decrement subscriber count and remove listener if no more subscribers
+        this.subscriberCount--;
+        if (this.subscriberCount === 0) {
+          this.removeStorageEventListener();
+        }
+      };
+    });
   }
 }
