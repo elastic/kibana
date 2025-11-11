c61ee8754520ba4985c0c3ce684700dbeb3dfbba
Elena Shostak <elena.shostak@elastic.co>
2025-11-11 12:09:03 +0100

    Replaced monaco-editor bundled dompurify with 3.2.4 version

--- src/platform/packages/private/kbn-ui-shared-deps-src/webpack.config.js
+++ src/platform/packages/private/kbn-ui-shared-deps-src/webpack.config.js
@@ -20,6 +20,7 @@ const UiSharedDepsNpm = require('@kbn/ui-shared-deps-npm');
 const { distDir: UiSharedDepsSrcDistDir } = require('./src/definitions');
 
 const MOMENT_SRC = require.resolve('moment/min/moment-with-locales.js');
+const DOMPURIFY_SRC = require.resolve('dompurify/dist/purify.js');
 const { REPO_ROOT } = require('@kbn/repo-info');
 
 /** @returns {import('webpack').Configuration} */
@@ -154,5 +155,11 @@ module.exports = {
       context: REPO_ROOT,
       manifest: require(UiSharedDepsNpm.dllManifestPath), // eslint-disable-line import/no-dynamic-require
     }),
+
+    // Replace Monaco Editor's bundled DOMPurify 3.0.5 with the project's DOMPurify 3.2.4
+    new webpack.NormalModuleReplacementPlugin(
+      /monaco-editor[/\\].*dompurify[/\\]dompurify\.js$/,
+      DOMPURIFY_SRC
+    ),
   ],
 };
