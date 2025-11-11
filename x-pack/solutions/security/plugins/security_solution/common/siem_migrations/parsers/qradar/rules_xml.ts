291579db23b42b0ef23025acdae51715b3ffe873
Jatin Kathuria <jatin.kathuria@elastic.co>
2025-11-11 13:32:51 +0100

    fix: parallel rules extraction for qradar

--- x-pack/solutions/security/plugins/security_solution/common/siem_migrations/parsers/qradar/rules_xml.ts
+++ x-pack/solutions/security/plugins/security_solution/common/siem_migrations/parsers/qradar/rules_xml.ts
@@ -11,47 +11,47 @@ import { XmlParser } from '../xml/xml';
 import type { QradarRule, ResourceDetailType, ResourceTypeMap } from './types';
 
 export class QradarRulesXmlParser extends XmlParser {
+  private async processRuleXml(qradarRule: XmlElement): Promise<QradarRule | undefined> {
+    const ruleData = this.getStrValue(
+      this.findDeep(qradarRule, 'rule_data') as string | Array<string>
+    );
+    let decodedRuleData: string;
+    try {
+      decodedRuleData = Buffer.from(ruleData, 'base64').toString('utf-8');
+    } catch (error) {
+      throw new Error(`Failed to decode rule_data from base64: ${error.message}`);
+    }
+
+    const parsedRuleData = await this.parseRuleData(decodedRuleData);
+
+    const id = this.findDeepValue(parsedRuleData, 'rule', 'id') as string;
+    const name = this.findDeep(parsedRuleData, 'name') as string | Array<string>;
+    const notes = this.findDeep(parsedRuleData, 'notes') as string | Array<string>;
+    const isBuildingBlockVal =
+      this.findDeepValue(parsedRuleData, 'rule', 'buildingBlock') ?? 'false';
+
+    if (name && notes && isBuildingBlockVal) {
+      const title = this.getStrValue(name);
+
+      const description = this.getStrValue(notes) as string;
+      const isBuildingBlock = isBuildingBlockVal === 'true';
+
+      return {
+        id: id as string,
+        title,
+        description,
+        rule_type: isBuildingBlock ? 'building_block' : 'default',
+        rule_data: decodedRuleData,
+      };
+    }
+  }
   public async getRules(): Promise<QradarRule[]> {
     const parsedXml = await this.parse();
     const rules = this.findDeep(parsedXml, 'custom_rule');
-    const parsedRules: QradarRule[] = [];
-
-    for (const qradarRule of Array.isArray(rules) ? rules : []) {
-      const ruleData = this.getStrValue(
-        this.findDeep(qradarRule, 'rule_data') as string | Array<string>
-      );
-      let decodedRuleData: string;
-      try {
-        decodedRuleData = Buffer.from(ruleData, 'base64').toString('utf-8');
-      } catch (error) {
-        throw new Error(`Failed to decode rule_data from base64: ${error.message}`);
-      }
-
-      const parsedRuleData = await this.parseRuleData(decodedRuleData);
-
-      const id = this.findDeepValue(parsedRuleData, 'rule', 'id') as string;
-      const name = this.findDeep(parsedRuleData, 'name') as string | Array<string>;
-      const notes = this.findDeep(parsedRuleData, 'notes') as string | Array<string>;
-      const isBuildingBlockVal =
-        this.findDeepValue(parsedRuleData, 'rule', 'buildingBlock') ?? 'false';
-
-      if (name && notes && isBuildingBlockVal) {
-        const title = this.getStrValue(name);
-
-        const description = this.getStrValue(notes) as string;
-        const isBuildingBlock = isBuildingBlockVal === 'true';
-
-        parsedRules.push({
-          id: id as string,
-          title,
-          description,
-          rule_type: isBuildingBlock ? 'building_block' : 'default',
-          rule_data: decodedRuleData,
-        });
-      }
-    }
-
-    return Array.isArray(parsedRules) ? parsedRules : [];
+    const qradarRules = Array.isArray(rules) ? rules : [];
+    const processQradarRulePromises = qradarRules.map(this.processRuleXml.bind(this));
+    const parsedRules = await Promise.all(processQradarRulePromises);
+    return parsedRules.filter(Boolean) as QradarRule[];
   }
 
   private async parseRuleData(ruleData: string): Promise<XmlElement> {
