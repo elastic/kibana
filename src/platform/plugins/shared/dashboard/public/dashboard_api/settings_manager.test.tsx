elastic/kibana

commit e438b4c8505cea144af25611f4c6e498fe988022

[dashboards as code] Make options parameter optional and avoid injecting defaults in REST API response (#238129)
PR does the following:
* Updates options schema to make options object optional and option keys
optional
* Updates server logic to not inject default values for options on write
and on read
* Updates client typing to consume options with same type as defined in
schema. Previously, options where spread into client state. Now options
are under the options key just like REST API shape

---------

Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

src/platform/plugins/shared/dashboard/public/dashboard_api/settings_manager.tsx
@@ -7,130 +7,115 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { StateComparators } from '@kbn/presentation-publishing';
-import {
-  diffComparators,
-  initializeTitleManager,
-  titleComparators,
-} from '@kbn/presentation-publishing';
-import fastIsEqual from 'fast-deep-equal';
-import { BehaviorSubject, combineLatest, combineLatestWith, debounceTime, map } from 'rxjs';
-import type { DashboardSettings, DashboardState } from '../../common';
-import { DEFAULT_DASHBOARD_STATE } from './default_dashboard_state';
+import type { StateComparators, WithAllKeys } from '@kbn/presentation-publishing';
+import { diffComparators, initializeStateManager } from '@kbn/presentation-publishing';
+import type { BehaviorSubject } from 'rxjs';
+import { combineLatestWith, debounceTime, map } from 'rxjs';
+import type { DashboardState } from '../../common';
+import type { DashboardAttributes, DashboardOptions } from '../../server/content_management';
+import { DEFAULT_DASHBOARD_OPTIONS } from '../../common/content_management';
 
-// SERIALIZED STATE ONLY TODO: This could be simplified by using src/platform/packages/shared/presentation/presentation_publishing/state_manager/state_manager.ts
-export function initializeSettingsManager(initialState?: DashboardState) {
-  const syncColors$ = new BehaviorSubject<boolean>(
-    initialState?.syncColors ?? DEFAULT_DASHBOARD_STATE.syncColors
-  );
-  function setSyncColors(syncColors: boolean) {
-    if (syncColors !== syncColors$.value) syncColors$.next(syncColors);
-  }
-  const syncCursor$ = new BehaviorSubject<boolean>(
-    initialState?.syncCursor ?? DEFAULT_DASHBOARD_STATE.syncCursor
-  );
-  function setSyncCursor(syncCursor: boolean) {
-    if (syncCursor !== syncCursor$.value) syncCursor$.next(syncCursor);
-  }
-  const syncTooltips$ = new BehaviorSubject<boolean>(
-    initialState?.syncTooltips ?? DEFAULT_DASHBOARD_STATE.syncTooltips
-  );
-  function setSyncTooltips(syncTooltips: boolean) {
-    if (syncTooltips !== syncTooltips$.value) syncTooltips$.next(syncTooltips);
-  }
-  const tags$ = new BehaviorSubject<string[]>(initialState?.tags ?? DEFAULT_DASHBOARD_STATE.tags);
-  function setTags(tags: string[]) {
-    if (!fastIsEqual(tags, tags$.value)) tags$.next(tags);
-  }
-  const titleManager = initializeTitleManager(initialState ?? {});
-  const timeRestore$ = new BehaviorSubject<boolean | undefined>(
-    initialState?.timeRestore ?? DEFAULT_DASHBOARD_STATE.timeRestore
-  );
-  function setTimeRestore(timeRestore: boolean) {
-    if (timeRestore !== timeRestore$.value) timeRestore$.next(timeRestore);
-  }
-  const useMargins$ = new BehaviorSubject<boolean>(
-    initialState?.useMargins ?? DEFAULT_DASHBOARD_STATE.useMargins
-  );
-  function setUseMargins(useMargins: boolean) {
-    if (useMargins !== useMargins$.value) useMargins$.next(useMargins);
-  }
+export type DashboardSettings = Required<DashboardOptions> & {
+  description?: DashboardAttributes['description'];
+  tags: DashboardAttributes['tags'];
+  timeRestore: DashboardAttributes['timeRestore'];
+  title: DashboardAttributes['description'];
+};
 
-  function getSettings(): DashboardSettings {
-    const titleState = titleManager.getLatestState();
+const DEFAULT_SETTINGS: WithAllKeys<DashboardSettings> = {
+  ...DEFAULT_DASHBOARD_OPTIONS,
+  description: undefined,
+  tags: [],
+  timeRestore: false,
+  title: '',
+};
+
+const comparators: StateComparators<DashboardSettings> = {
+  title: 'referenceEquality',
+  description: 'referenceEquality',
+  hidePanelTitles: 'referenceEquality',
+  syncColors: 'referenceEquality',
+  syncCursor: 'referenceEquality',
+  syncTooltips: 'referenceEquality',
+  timeRestore: 'referenceEquality',
+  useMargins: 'referenceEquality',
+  tags: 'deepEquality',
+};
+
+function deserializeState(state: DashboardState) {
+  return {
+    ...state.options,
+    description: state.description,
+    tags: state.tags,
+    timeRestore: state.timeRestore,
+    title: state.title,
+  };
+}
+
+export function initializeSettingsManager(initialState: DashboardState) {
+  const stateManager = initializeStateManager(
+    deserializeState(initialState),
+    DEFAULT_SETTINGS,
+    comparators
+  );
+
+  function serializeSettings() {
+    const { description, tags, timeRestore, title, ...options } = stateManager.getLatestState();
     return {
-      title: titleState.title ?? '',
-      description: titleState.description,
-      hidePanelTitles: titleState.hidePanelTitles ?? DEFAULT_DASHBOARD_STATE.hidePanelTitles,
-      syncColors: syncColors$.value,
-      syncCursor: syncCursor$.value,
-      syncTooltips: syncTooltips$.value,
-      tags: tags$.value,
-      timeRestore: timeRestore$.value ?? DEFAULT_DASHBOARD_STATE.timeRestore,
-      useMargins: useMargins$.value,
+      ...(description && { description }),
+      tags,
+      timeRestore,
+      title,
+      options,
     };
   }
 
-  function setSettings(settings: DashboardSettings) {
-    setSyncColors(settings.syncColors);
-    setSyncCursor(settings.syncCursor);
-    setSyncTooltips(settings.syncTooltips);
-    setTags(settings.tags);
-    setTimeRestore(settings.timeRestore);
-    setUseMargins(settings.useMargins);
-    titleManager.api.setHideTitle(settings.hidePanelTitles);
-    titleManager.api.setDescription(settings.description);
-    titleManager.api.setTitle(settings.title);
-  }
-
-  const comparators: StateComparators<DashboardSettings> = {
-    title: titleComparators.title,
-    description: titleComparators.description,
-    hidePanelTitles: 'referenceEquality',
-    syncColors: 'referenceEquality',
-    syncCursor: 'referenceEquality',
-    syncTooltips: 'referenceEquality',
-    timeRestore: 'referenceEquality',
-    useMargins: 'referenceEquality',
-    tags: 'deepEquality',
-  };
-
   return {
     api: {
-      ...titleManager.api,
-      getSettings,
+      description$: stateManager.api.description$,
+      getSettings: stateManager.getLatestState,
+      hideTitle$: stateManager.api.hidePanelTitles$,
       settings: {
-        syncColors$,
-        syncCursor$,
-        syncTooltips$,
-        useMargins$,
+        syncColors$: stateManager.api.syncColors$,
+        syncCursor$: stateManager.api.syncCursor$,
+        syncTooltips$: stateManager.api.syncTooltips$,
+        useMargins$: stateManager.api.useMargins$,
       },
-      setSettings,
-      setTags,
-      timeRestore$,
+      setSettings: stateManager.reinitializeState,
+      setTags: stateManager.api.setTags,
+      timeRestore$: stateManager.api.timeRestore$,
+      title$: stateManager.api.title$,
     },
     internalApi: {
+      serializeSettings,
       startComparing$: (lastSavedState$: BehaviorSubject<DashboardState>) => {
-        return combineLatest([
-          syncColors$,
-          syncCursor$,
-          syncTooltips$,
-          tags$,
-          timeRestore$,
-          useMargins$,
-
-          titleManager.anyStateChange$,
-        ]).pipe(
+        return stateManager.anyStateChange$.pipe(
           debounceTime(100),
-          map(() => getSettings()),
+          map(() => stateManager.getLatestState()),
           combineLatestWith(lastSavedState$),
-          map(([latestState, lastSavedState]) =>
-            diffComparators(comparators, lastSavedState, latestState)
-          )
+          map(([latestState, lastSavedState]) => {
+            const { description, tags, timeRestore, title, ...optionDiffs } = diffComparators(
+              comparators,
+              deserializeState(lastSavedState),
+              latestState,
+              DEFAULT_SETTINGS
+            );
+            // options needs to contain all values and not just diffs since is spread into saved state
+            const options = Object.keys(optionDiffs).length
+              ? { ...serializeSettings().options, ...optionDiffs }
+              : undefined;
+            return {
+              ...(description && { description }),
+              ...(tags && { tags }),
+              ...(typeof timeRestore === 'boolean' && { timeRestore }),
+              ...(title && { title }),
+              ...(options && { options }),
+            };
+          })
         );
       },
       reset: (lastSavedState: DashboardState) => {
-        setSettings(lastSavedState);
+        stateManager.reinitializeState(deserializeState(lastSavedState));
       },
     },
   };

src/platform/plugins/shared/dashboard/public/dashboard_api/settings_manager.test.tsx
@@ -1,0 +1,57 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { BehaviorSubject } from 'rxjs';
+import { getSampleDashboardState } from '../mocks';
+import type { DashboardState } from '../../common';
+import { initializeSettingsManager } from './settings_manager';
+
+describe('initializeSettingsManager', () => {
+  describe('startComparing$', () => {
+    test('Should return no changes when there are no changes', (done) => {
+      const lastSavedState$ = new BehaviorSubject<DashboardState>(getSampleDashboardState());
+      const settingsManager = initializeSettingsManager(lastSavedState$.value);
+      settingsManager.internalApi.startComparing$(lastSavedState$).subscribe((changes) => {
+        expect(changes).toMatchInlineSnapshot(`Object {}`);
+        done();
+      });
+    });
+
+    test('Should return only changed keys when there are changes', (done) => {
+      const lastSavedState$ = new BehaviorSubject<DashboardState>({
+        ...getSampleDashboardState(),
+        timeRestore: true,
+      });
+      const settingsManager = initializeSettingsManager(lastSavedState$.value);
+      settingsManager.internalApi.startComparing$(lastSavedState$).subscribe((changes) => {
+        expect(changes).toMatchInlineSnapshot(`
+          Object {
+            "options": Object {
+              "hidePanelTitles": true,
+              "syncColors": false,
+              "syncCursor": true,
+              "syncTooltips": false,
+              "useMargins": true,
+            },
+            "timeRestore": false,
+            "title": "updated title",
+          }
+        `);
+        done();
+      });
+      const currentSettings = settingsManager.api.getSettings();
+      settingsManager.api.setSettings({
+        ...currentSettings,
+        title: 'updated title',
+        hidePanelTitles: !currentSettings.hidePanelTitles,
+        timeRestore: false,
+      });
+    });
+  });
+});

src/platform/packages/shared/presentation/presentation_publishing/state_manager/state_comparators.ts
@@ -35,13 +35,16 @@ export const runComparator = <StateType extends object = object>(
  */
 export const diffComparators = <StateType extends object = object>(
   comparators: StateComparators<StateType>,
-  lastSavedState?: StateType,
-  latestState?: StateType
+  lastSavedState?: Partial<StateType>,
+  latestState?: Partial<StateType>,
+  defaultState?: Partial<StateType>
 ): Partial<StateType> => {
   return Object.keys(comparators).reduce((acc, key) => {
     const comparator = comparators[key as keyof StateType];
-    const lastSavedValue = lastSavedState?.[key as keyof StateType];
-    const currentValue = latestState?.[key as keyof StateType];
+    const lastSavedValue =
+      lastSavedState?.[key as keyof StateType] ?? defaultState?.[key as keyof StateType];
+    const currentValue =
+      latestState?.[key as keyof StateType] ?? defaultState?.[key as keyof StateType];
 
     if (!runComparator(comparator, lastSavedState, latestState, lastSavedValue, currentValue)) {
       acc[key as keyof StateType] = currentValue;

src/platform/packages/shared/presentation/presentation_publishing/state_manager/state_manager.ts
@@ -28,7 +28,7 @@ type KeyToSubjectMap<StateType extends object> = {
  * @param comparators - Optional StateComparators. When provided, subject will only emit when value changes.
  */
 export const initializeStateManager = <StateType extends object>(
-  initialState: StateType,
+  initialState: Partial<StateType>,
   defaultState: WithAllKeys<StateType>,
   comparators?: StateComparators<StateType>
 ): StateManager<StateType> => {

src/platform/plugins/shared/dashboard/common/index.ts
@@ -7,12 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export type {
-  DashboardCapabilities,
-  DashboardLocatorParams,
-  DashboardSettings,
-  DashboardState,
-} from './types';
+export type { DashboardCapabilities, DashboardLocatorParams, DashboardState } from './types';
 
 export {
   getReferencesForPanelId,

src/platform/plugins/shared/dashboard/common/types.ts
@@ -8,12 +8,12 @@
  */
 
 import type { Reference } from '@kbn/content-management-utils';
-import type { SerializableRecord, Writable } from '@kbn/utility-types';
+import type { SerializableRecord } from '@kbn/utility-types';
 import type { ViewMode } from '@kbn/presentation-publishing';
 import type { RefreshInterval } from '@kbn/data-plugin/public';
 import type { ControlsGroupState } from '@kbn/controls-schemas';
 
-import type { DashboardAttributes, DashboardOptions } from '../server/content_management';
+import type { DashboardAttributes } from '../server/content_management';
 
 export interface DashboardCapabilities {
   showWriteControls: boolean;
@@ -27,14 +27,12 @@ export interface DashboardAttributesAndReferences {
   references: Reference[];
 }
 
-export type DashboardSettings = Writable<DashboardOptions> & {
+export interface DashboardState {
+  options?: DashboardAttributes['options'];
   description?: DashboardAttributes['description'];
-  tags: string[];
+  tags?: DashboardAttributes['tags'];
   timeRestore: DashboardAttributes['timeRestore'];
   title: DashboardAttributes['description'];
-};
-
-export interface DashboardState extends DashboardSettings {
   query?: DashboardAttributes['query'];
   filters?: DashboardAttributes['filters'];
   timeRange?: DashboardAttributes['timeRange'];

src/platform/plugins/shared/dashboard/public/dashboard_api/default_dashboard_state.ts
@@ -17,11 +17,4 @@ export const DEFAULT_DASHBOARD_STATE: DashboardState = {
   panels: [],
   title: '',
   tags: [],
-
-  // options
-  useMargins: true,
-  syncColors: false,
-  syncCursor: true,
-  syncTooltips: false,
-  hidePanelTitles: false,
 };

src/platform/plugins/shared/dashboard/public/dashboard_api/get_dashboard_api.ts
@@ -120,7 +120,7 @@ export function getDashboardApi({
     const { panels, references: panelReferences } = layoutManager.internalApi.serializeLayout();
     const unifiedSearchState = unifiedSearchManager.internalApi.getState();
     const dashboardState: DashboardState = {
-      ...settingsManager.api.getSettings(),
+      ...settingsManager.internalApi.serializeSettings(),
       ...unifiedSearchState,
       panels,
     };

commit fd0545a8231a1043bfb37b16478b1d56f67cab11

[dashboard] fix 'sync colors' and 'sync tooltips' must be ON by default (#242442)
Closes https://github.com/elastic/kibana/issues/242373

Regression caused by https://github.com/elastic/kibana/pull/238129. PR
removed options from `DEFAULT_DASHBOARD_STATE` and starting using
existing
[DEFAULT_DASHBOARD_OPTIONS](https://github.com/elastic/kibana/blob/main/src/platform/plugins/shared/dashboard/common/content_management/constants.ts#L19).
`DEFAULT_DASHBOARD_OPTIONS` added in
https://github.com/elastic/kibana/pull/193067. The problem is that
`DEFAULT_DASHBOARD_OPTIONS` does not have the same values as options in
`DEFAULT_DASHBOARD_STATE` and this was missed during review.

---------

Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

src/platform/plugins/shared/dashboard/common/content_management/constants.ts
@@ -19,7 +19,7 @@ export const DEFAULT_PANEL_HEIGHT = 15;
 export const DEFAULT_DASHBOARD_OPTIONS = {
   hidePanelTitles: false,
   useMargins: true,
-  syncColors: true,
+  syncColors: false,
   syncCursor: true,
-  syncTooltips: true,
+  syncTooltips: false,
 } as const;

src/platform/plugins/shared/dashboard/public/dashboard_api/settings_manager.tsx
@@ -79,7 +79,12 @@ export function initializeSettingsManager(initialState: DashboardState) {
         syncTooltips$: stateManager.api.syncTooltips$,
         useMargins$: stateManager.api.useMargins$,
       },
-      setSettings: stateManager.reinitializeState,
+      setSettings: (settings: Partial<DashboardSettings>) => {
+        stateManager.reinitializeState({
+          ...stateManager.getLatestState(),
+          ...settings,
+        });
+      },
       setTags: stateManager.api.setTags,
       timeRestore$: stateManager.api.timeRestore$,
       title$: stateManager.api.title$,

src/platform/plugins/shared/dashboard/public/dashboard_api/settings_manager.test.tsx
@@ -11,8 +11,44 @@ import { BehaviorSubject } from 'rxjs';
 import { getSampleDashboardState } from '../mocks';
 import type { DashboardState } from '../../common';
 import { initializeSettingsManager } from './settings_manager';
+import { DEFAULT_DASHBOARD_OPTIONS } from '../../common/content_management';
 
 describe('initializeSettingsManager', () => {
+  describe('default values', () => {
+    test('Should set syncCursor to false when value not provided', () => {
+      const settingsManager = initializeSettingsManager({
+        title: 'dashboard 1',
+        panels: [],
+      });
+      expect(settingsManager.api.getSettings().syncColors).toBe(false);
+    });
+
+    test('Should set syncTooltips to false when value not provided', () => {
+      const settingsManager = initializeSettingsManager({
+        title: 'dashboard 1',
+        panels: [],
+      });
+      expect(settingsManager.api.getSettings().syncTooltips).toBe(false);
+    });
+  });
+
+  describe('setSettings', () => {
+    test('Should not overwrite settings when setting partial state', () => {
+      const settingsManager = initializeSettingsManager({
+        title: 'dashboard 1',
+        panels: [],
+        options: {
+          ...DEFAULT_DASHBOARD_OPTIONS,
+          useMargins: false,
+        },
+      });
+      settingsManager.api.setSettings({ timeRestore: true });
+      const settings = settingsManager.api.getSettings();
+      expect(settings.timeRestore).toBe(true);
+      expect(settings.useMargins).toBe(false);
+    });
+  });
+
   describe('startComparing$', () => {
     test('Should return no changes when there are no changes', (done) => {
       const lastSavedState$ = new BehaviorSubject<DashboardState>(getSampleDashboardState());

src/platform/plugins/shared/dashboard/server/content_management/v1/transforms/in/transform_dashboard_in.test.ts
@@ -114,7 +114,7 @@ describe('transformDashboardIn', () => {
           "kibanaSavedObjectMeta": Object {
             "searchSourceJSON": "{}",
           },
-          "optionsJSON": "{\\"hidePanelTitles\\":false,\\"useMargins\\":true,\\"syncColors\\":true,\\"syncCursor\\":true,\\"syncTooltips\\":true}",
+          "optionsJSON": "{\\"hidePanelTitles\\":false,\\"useMargins\\":true,\\"syncColors\\":false,\\"syncCursor\\":true,\\"syncTooltips\\":false}",
           "panelsJSON": "[]",
           "timeRestore": false,
           "title": "title",