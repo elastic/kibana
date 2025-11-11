04642f069aa80a8ea6010ce2cd98a35c6ad511e1
Paul Tavares <paul.tavares@elastic.co>
2025-11-11 08:28:55 -0500

    Fix labels

--- x-pack/solutions/security/plugins/security_solution/public/management/common/translations.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/common/translations.ts
@@ -307,13 +307,14 @@ export const CONSOLE_COMMANDS = {
       }
     ),
     pidArgAbout: i18n.translate('xpack.securitySolution.translations.memoryDump.pidArgAbout', {
-      defaultMessage: 'Process ID to generate a memory dump for. Valid only "--process" is used',
+      defaultMessage:
+        'Process ID to generate a memory dump for. Valid only when "--process" is used',
     }),
     entityIdArgAbout: i18n.translate(
       'xpack.securitySolution.translations.memoryDump.entityIdArgAbout',
       {
         defaultMessage:
-          'Process Entity ID to generate a memory dump for.  Valid only "--process" is used',
+          'Process Entity ID to generate a memory dump for.  Valid only when "--process" is used',
       }
     ),
     agentResultMissing: i18n.translate(
