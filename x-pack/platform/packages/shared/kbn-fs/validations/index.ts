134ebc56e46aa5e068f287c0443751f65a42639e
Elena Shostak <165678770+elena-shostak@users.noreply.github.com>
2025-11-11 18:44:25 +0100

    [FSH] validations (#241596)
    ## Summary
    
    Validation is covering the following scenarios:
    - [x] validate that directory is a safe path
    - [x] validate allowed file extensions
    - [x] validate allowed mime types
    - [x] validate allowed file sizes
    - [x] perform sanitization of file content for svg
    
    
    ### Checklist
    
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    __Closes: https://github.com/elastic/kibana/issues/239383__
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/packages/shared/kbn-fs/validations/index.ts
+++ x-pack/platform/packages/shared/kbn-fs/validations/index.ts
@@ -6,3 +6,8 @@
  */
 
 export { validateNoPathTraversal } from './path_traversal';
+export { validateFileSize } from './file_size';
+export { validateMimeType } from './file_mimetype';
+export { validateFileExtension } from './file_extension';
+export { validateAndSanitizeFileData } from './file_content';
+export { sanitizeSvg } from '../sanitizations/svg';
