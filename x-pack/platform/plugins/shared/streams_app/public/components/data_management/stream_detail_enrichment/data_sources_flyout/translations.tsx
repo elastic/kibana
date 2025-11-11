37ab2050bde851f6cc539470edfa828e755dbfa1
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2025-11-11 08:42:09 +0100

    Update x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/data_sources_flyout/translations.tsx
    Co-authored-by: Kerry Gallagher <k.gallagher.05@gmail.com>

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/data_sources_flyout/translations.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/data_sources_flyout/translations.tsx
@@ -66,7 +66,7 @@ export const DATA_SOURCES_I18N = {
       'xpack.streams.streamDetailView.managementTab.enrichment.dataSourcesFlyout.dataSourceCard.deleteDataSourceDisabledLabel',
       {
         defaultMessage:
-          'Cannot delete data source while is selected. Switch to another data source first.',
+          'Cannot delete data source while it is selected. Switch to another data source first.',
       }
     ),
     dataPreviewAccordionLabel: i18n.translate(
