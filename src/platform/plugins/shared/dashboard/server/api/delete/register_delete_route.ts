611dba32ec85065cfe613f51508ba5aec6f886d8
Nathan Reese <reese.nathan@gmail.com>
2025-11-11 11:56:27 -0700

    delete route

---
+++ src/platform/plugins/shared/dashboard/server/api/delete/register_delete_route.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import type { VersionedRouter } from '@kbn/core-http-server';
+import type { RequestHandlerContext } from '@kbn/core/server';
+import { INTERNAL_API_VERSION, PUBLIC_API_PATH, commonRouteConfig } from '../constants';
+import { schema } from '@kbn/config-schema';
+import { DASHBOARD_SAVED_OBJECT_TYPE } from '../../dashboard_saved_object';
+
+export function registerDeleteRoute(router: VersionedRouter<RequestHandlerContext>) {
+  const deleteRoute = router.delete({
+    path: `${PUBLIC_API_PATH}/{id}`,
+    summary: `Delete a dashboard`,
+    ...commonRouteConfig,
+  });
+
+  deleteRoute.addVersion(
+    {
+      version: INTERNAL_API_VERSION,
+      validate: {
+        request: {
+          params: schema.object({
+            id: schema.string({
+              meta: {
+                description: 'A unique identifier for the dashboard.',
+              },
+            }),
+          }),
+        },
+      },
+    },
+    async (ctx, req, res) => {
+      try {
+        const { core } = await ctx.resolve(['core']);
+        await core.savedObjects.client.delete(DASHBOARD_SAVED_OBJECT_TYPE, req.params.id);
+      } catch (e) {
+        if (e.isBoom && e.output.statusCode === 404) {
+          return res.notFound({
+            body: {
+              message: `A dashboard with ID ${req.params.id} was not found.`,
+            },
+          });
+        }
+        if (e.isBoom && e.output.statusCode === 403) {
+          return res.forbidden();
+        }
+        return res.badRequest();
+      }
+
+      return res.ok();
+    }
+  );
+}
