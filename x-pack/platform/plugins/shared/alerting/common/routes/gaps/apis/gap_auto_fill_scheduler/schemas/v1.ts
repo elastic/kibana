7ff212ea2638dc8051355e1586dbc149d363c81e
Nikita Khristinin <nkhristinin@gmail.com>
2025-11-11 09:12:10 +0100

    Validate gap_fill_range

--- x-pack/platform/plugins/shared/alerting/common/routes/gaps/apis/gap_auto_fill_scheduler/schemas/v1.ts
+++ x-pack/platform/plugins/shared/alerting/common/routes/gaps/apis/gap_auto_fill_scheduler/schemas/v1.ts
@@ -5,25 +5,36 @@
  * 2.0.
  */
 import { schema } from '@kbn/config-schema';
+import dateMath from '@kbn/datemath';
 
-export const gapAutoFillSchedulerBodySchema = schema.object({
-  id: schema.maybe(schema.string()),
-  name: schema.string({ defaultValue: '' }),
-  enabled: schema.boolean({ defaultValue: true }),
-  max_backfills: schema.number({ defaultValue: 1000, min: 1, max: 5000 }),
-  num_retries: schema.number({ defaultValue: 3, min: 1 }),
-  gap_fill_range: schema.string({ defaultValue: 'now-90d' }),
-  schedule: schema.object({
-    interval: schema.string(),
-  }),
-  scope: schema.maybe(schema.arrayOf(schema.string())),
-  rule_types: schema.arrayOf(
-    schema.object({
-      type: schema.string(),
-      consumer: schema.string(),
-    })
-  ),
-});
+export const gapAutoFillSchedulerBodySchema = schema.object(
+  {
+    id: schema.maybe(schema.string()),
+    name: schema.string({ defaultValue: '' }),
+    enabled: schema.boolean({ defaultValue: true }),
+    max_backfills: schema.number({ defaultValue: 1000, min: 1, max: 5000 }),
+    num_retries: schema.number({ defaultValue: 3, min: 1 }),
+    gap_fill_range: schema.string({ defaultValue: 'now-90d' }),
+    schedule: schema.object({
+      interval: schema.string(),
+    }),
+    scope: schema.maybe(schema.arrayOf(schema.string())),
+    rule_types: schema.arrayOf(
+      schema.object({
+        type: schema.string(),
+        consumer: schema.string(),
+      })
+    ),
+  },
+  {
+    validate({ gap_fill_range: gapFillRange }) {
+      const parsed = dateMath.parse(gapFillRange);
+      if (!parsed || !parsed.isValid()) {
+        return 'gap_fill_range is invalid';
+      }
+    },
+  }
+);
 
 export const gapAutoFillSchedulerResponseSchema = schema.object({
   id: schema.string(),
