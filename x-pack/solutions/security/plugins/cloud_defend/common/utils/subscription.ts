737fb61cfbbfd9ec43614c187a1072ab456fc9d3
animehart <rickyangwyn@gmail.com>
2025-11-12 11:09:05 -0800

    lint fix

--- x-pack/solutions/security/plugins/cloud_defend/common/utils/subscription.ts
+++ x-pack/solutions/security/plugins/cloud_defend/common/utils/subscription.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import type { ILicense, LicenseType } from '@kbn/licensing-plugin/common/types';
+import type { LicenseType, ILicense } from '@kbn/licensing-types';
 import { PLUGIN_NAME } from '../constants';
 
 const MINIMUM_NON_CLOUD_LICENSE_TYPE: LicenseType = 'enterprise';
