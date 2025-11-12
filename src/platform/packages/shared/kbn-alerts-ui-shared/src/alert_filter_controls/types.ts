0f66334d9d789af847b0c68e08de382b0d586b10
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-11-12 12:23:19 -0600

    Scope writrable type to setStatysInCintrolConfigs

--- src/platform/packages/shared/kbn-alerts-ui-shared/src/alert_filter_controls/types.ts
+++ src/platform/packages/shared/kbn-alerts-ui-shared/src/alert_filter_controls/types.ts
@@ -11,7 +11,6 @@ import type { ControlGroupRendererApi } from '@kbn/control-group-renderer';
 import type { OptionsListDSLControlState } from '@kbn/controls-schemas';
 import type { Filter, Query, TimeRange } from '@kbn/es-query';
 import type { Storage } from '@kbn/kibana-utils-plugin/public';
-import type { Writable } from '@kbn/utility-types';
 
 export type FilterUrlFormat = Record<
   string,
@@ -26,7 +25,7 @@ export interface FilterContextType {
   addControl: (controls: FilterControlConfig) => void;
 }
 
-export type FilterControlConfig = Omit<Writable<OptionsListDSLControlState>, 'dataViewId'> & {
+export type FilterControlConfig = Omit<OptionsListDSLControlState, 'dataViewId'> & {
   /*
    * Determines the presence and order of a control
    * */
