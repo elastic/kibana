4175b053b1d59115ae06fdda545b4d1c83579860
Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>
2025-11-12 13:29:40 +0100

    Update FTs

--- x-pack/platform/plugins/shared/streams_app/test/scout/ui/tests/data_management/data_retention/failure_store.spec.ts
+++ x-pack/platform/plugins/shared/streams_app/test/scout/ui/tests/data_management/data_retention/failure_store.spec.ts
@@ -25,19 +25,25 @@ test.describe(
         },
         { meta: true }
       );
+      await apiServices.streams.forkStream('logs', 'logs.nginx', {
+        field: 'service.name',
+        eq: 'nginx',
+      });
+      await esClient.indices.putDataStreamOptions(
+        {
+          name: 'logs.nginx',
+          failure_store: {
+            enabled: true,
+          },
+        },
+        { meta: true }
+      );
     });
 
     test.beforeEach(async ({ apiServices, browserAuth }) => {
       await browserAuth.loginAsAdmin();
       // Clear existing processors before each test
       await apiServices.streams.clearStreamProcessors('logs-generic-default');
-      // Clear existing rules
-      await apiServices.streams.clearStreamChildren('logs');
-      // Create a test stream with routing rules first
-      await apiServices.streams.forkStream('logs', 'logs.nginx', {
-        field: 'service.name',
-        eq: 'nginx',
-      });
     });
 
     test.afterAll(async ({ apiServices, logsSynthtraceEsClient }) => {
@@ -51,8 +57,10 @@ test.describe(
     }) => {
       await pageObjects.streams.gotoDataRetentionTab('logs-generic-default');
 
-      // Update the retention period
       await page.getByTestId('streamFailureStoreEditRetention').click();
+      // Disable inherit failure store
+      await page.getByTestId('inheritFailureStoreSwitch').click();
+      // Update the retention period
       await page.getByTestId('custom').click();
       const dialog = page.getByRole('dialog');
       await dialog.getByTestId('selectFailureStorePeriodValue').fill('7');
@@ -96,48 +104,100 @@ test.describe(
       ).toBeVisible();
     });
 
-    test('should not allow edit failure store for wired streams', async ({
-      page,
-      pageObjects,
-      esClient,
-    }) => {
-      // Enable failure store for the forked stream first
-      await esClient.indices.putDataStreamOptions(
-        {
-          name: 'logs.nginx',
-          failure_store: {
-            enabled: true,
-          },
-        },
-        { meta: true }
-      );
+    test('should inherit failure store for classic streams', async ({ page, pageObjects }) => {
+      await pageObjects.streams.gotoDataRetentionTab('logs-generic-default');
 
-      await pageObjects.streams.gotoDataRetentionTab('logs.nginx');
-      // Verify the retention card is present but the edit button is not shown
-      await expect(page.getByTestId('failureStoreRetention-metric')).toBeVisible();
-      await expect(page.getByTestId('streamFailureStoreEditRetention')).toBeHidden();
+      // Enable inherit failure store
+      await page.getByTestId('streamFailureStoreEditRetention').click();
+      await page.getByTestId('inheritFailureStoreSwitch').click();
+      await page.getByTestId('failureStoreModalSaveButton').click();
+      await expect(
+        page.getByTestId('failureStoreRetention-metric').getByText('30 days')
+      ).toBeVisible();
+      await expect(
+        page
+          .getByTestId('failureStoreRetention-metric-subtitle')
+          .getByText('Inherit from index template')
+      ).toBeVisible();
     });
 
-    test('should not allow enabling failure store for wired streams', async ({
+    test('should edit failure store successfully for wired streams', async ({
       page,
       pageObjects,
-      esClient,
     }) => {
-      // Enable failure store for the forked stream first
-      await esClient.indices.putDataStreamOptions(
-        {
-          name: 'logs.nginx',
-          failure_store: {
-            enabled: false,
-          },
-        },
-        { meta: true }
-      );
-
       await pageObjects.streams.gotoDataRetentionTab('logs.nginx');
-      // Verify the retention no failure store panel is present but the enable button is not shown
-      await expect(page.getByTestId('disabledFailureStorePanel')).toBeVisible();
-      await expect(page.getByTestId('streamsAppFailureStoreEnableButton')).toBeHidden();
+
+      await page.getByTestId('streamFailureStoreEditRetention').click();
+      // Disable inherit failure store
+      await page.getByTestId('inheritFailureStoreSwitch').click();
+      // Update the retention period
+      await page.getByTestId('custom').click();
+      const dialog = page.getByRole('dialog');
+      await dialog.getByTestId('selectFailureStorePeriodValue').fill('7');
+      await page.getByTestId('failureStoreModalSaveButton').click();
+      await expect(
+        page.getByTestId('failureStoreRetention-metric').getByText('7 days')
+      ).toBeVisible();
+      await expect(
+        page
+          .getByTestId('failureStoreRetention-metric-subtitle')
+          .getByText('Custom retention period')
+      ).toBeVisible();
+    });
+
+    test('should disable failure store for wired streams', async ({ page, pageObjects }) => {
+      await pageObjects.streams.gotoDataRetentionTab('logs.nginx');
+
+      // Disable failure store
+      await page.getByTestId('streamFailureStoreEditRetention').click();
+      await page.getByTestId('enableFailureStoreToggle').click();
+      await page.getByTestId('failureStoreModalSaveButton').click();
+      await expect(
+        page.getByTestId('disabledFailureStorePanel').getByText('Failure store disabled')
+      ).toBeVisible();
+    });
+
+    test('should enable failure store for wired streams', async ({ page, pageObjects }) => {
+      await pageObjects.streams.gotoDataRetentionTab('logs.nginx');
+
+      // Enable failure store again
+      await page.getByTestId('streamsAppFailureStoreEnableButton').click();
+      await page.getByTestId('enableFailureStoreToggle').click();
+      await page.getByTestId('failureStoreModalSaveButton').click();
+      await expect(
+        page.getByTestId('failureStoreRetention-metric').getByText('30 days')
+      ).toBeVisible();
+      await expect(
+        page
+          .getByTestId('failureStoreRetention-metric-subtitle')
+          .getByText('Default retention period')
+      ).toBeVisible();
+    });
+
+    test('should inherit failure store for child wired streams', async ({ page, pageObjects }) => {
+      await pageObjects.streams.gotoDataRetentionTab('logs.nginx');
+
+      // Enable inherit failure store
+      await page.getByTestId('streamFailureStoreEditRetention').click();
+      await page.getByTestId('inheritFailureStoreSwitch').click();
+      await page.getByTestId('failureStoreModalSaveButton').click();
+      await expect(
+        page.getByTestId('failureStoreRetention-metric').getByText('30 days')
+      ).toBeVisible();
+      await expect(
+        page.getByTestId('failureStoreRetention-metric-subtitle').getByText('Inherit from parent')
+      ).toBeVisible();
+    });
+
+    test('should not inherit failure store for root wired streams', async ({
+      page,
+      pageObjects,
+    }) => {
+      await pageObjects.streams.gotoDataRetentionTab('logs');
+
+      // Try to enable inherit failure store - the switch should not be visible for root streams
+      await page.getByTestId('streamFailureStoreEditRetention').click();
+      await expect(page.getByTestId('inheritFailureStoreSwitch')).toBeHidden();
     });
   }
 );
