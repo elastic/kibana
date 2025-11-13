a82f22378456ff56e50fc67fe787387166edbcbc
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-11-13 10:37:46 -0500

    [Fleet] Make the agentless client logs less verbose

--- x-pack/platform/plugins/shared/fleet/server/services/agents/agentless_agent.test.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/agents/agentless_agent.test.ts
@@ -26,6 +26,7 @@ import { listEnrollmentApiKeys } from '../api_keys';
 import { fleetServerHostService } from '../fleet_server_host';
 
 import { agentlessAgentService } from './agentless_agent';
+import M from 'minimatch';
 
 jest.mock('axios');
 
@@ -1449,6 +1450,22 @@ describe('Agentless Agent service', () => {
 
       // Assert that the error is logged
       expect(mockedLogger.error).toHaveBeenCalledTimes(1);
+      expect(mockedLogger.error).toHaveBeenCalledWith(
+        expect.stringMatching(
+          /\[Agentless API\] Creating the agentless agent failed with a status 500/
+        ),
+        {
+          trace: expect.anything(),
+          http: {
+            request: {
+              id: undefined,
+            },
+            response: {
+              status_code: 500,
+            },
+          },
+        }
+      );
     });
 
     it('should throw an error and log and error when the Agentless API returns status 429', async () => {
