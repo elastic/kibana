0d90bda00dd336f31b0637143a43d58bcb23ee46
Hannah Mudge <Heenawter@users.noreply.github.com>
2025-11-13 16:27:32 -0700

    Start fixing BWC

--- src/platform/plugins/shared/dashboard/public/dashboard_app/url/bwc/extract_control_group_state.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_app/url/bwc/extract_control_group_state.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { ControlWidth } from '@kbn/controls-schemas';
+import type { ControlWidth, ControlsGroupState } from '@kbn/controls-schemas';
 import type { DashboardState, DashboardControlsState } from '../../../../common';
 
 interface State816To819 {
@@ -27,6 +27,19 @@ interface State816To819 {
 const isState816To819 = (state: unknown): state is State816To819 =>
   Boolean((state as State816To819).controlGroupState?.initialChildControlState);
 
+interface State819To94 {
+  controlGroupInput: {
+    controls: Array<{ controlConfig: object }>;
+  };
+}
+
+const isState819To94 = (state: unknown): state is State819To94 => {
+  return Boolean(
+    (state as State819To94).controlGroupInput.controls.length &&
+      (state as State819To94).controlGroupInput.controls[0].controlConfig
+  );
+};
+
 export function extractControlGroupState(state: {
   [key: string]: unknown;
 }): DashboardState['controlGroupInput'] {
@@ -36,14 +49,14 @@ export function extractControlGroupState(state: {
     return {
       controls: Object.entries(state.controlGroupState.initialChildControlState).map(
         ([controlId, value]) => {
-          const { grow, order, type, width, ...controlConfig } = value;
+          const { grow, order, type, width, ...config } = value;
           return {
             id: controlId,
             grow,
             order,
             type,
             width,
-            controlConfig,
+            config,
           };
         }
       ) as DashboardControlsState,
@@ -53,6 +66,13 @@ export function extractControlGroupState(state: {
   if (!state.controlGroupInput || typeof state.controlGroupInput !== 'object') return;
 
   const controlGroupInput = state.controlGroupInput as { [key: string]: unknown };
+
+  if (isState819To94(state)) {
+    // URL state created in 8.19 up to 9.3 had `config` stored under `controlConfig`
+    controlGroupInput.controls = state.controlGroupInput.controls.map(
+      ({ controlConfig, ...rest }) => ({ ...rest, config: controlConfig })
+    );
+  }
 
   // TODO: Update bwc for autoApplySelections
 
