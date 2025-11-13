edf4aff1f645353cab3fd7a131d66ebebc80c2a3
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2025-11-13 13:21:05 +0100

    feat: prefer TS when index contains tsdb fields

--- src/platform/packages/shared/kbn-esql-utils/src/utils/get_initial_esql_query.ts
+++ src/platform/packages/shared/kbn-esql-utils/src/utils/get_initial_esql_query.ts
@@ -34,10 +34,18 @@ const getFinalWhereClause = (timeFilter?: string, queryFilter?: string) => {
   return timeFilter || queryFilter ? ` | WHERE ${timeFilter || queryFilter}` : '';
 };
 
+const getSourceCommand = (dataView: DataView) => {
+  if (dataView.isTSDBMode()) {
+    return 'TS';
+  }
+  return 'FROM';
+};
+
 /**
  * Builds an ES|QL query for the provided dataView
  * If there is @timestamp field in the index, we don't add the WHERE clause
  * If there is no @timestamp and there is a dataView timeFieldName, we add the WHERE clause with the timeFieldName
+ * If the index pattern contains TSDB fields, we add the TS command, otherwise we add the FROM command
  * @param dataView
  */
 export function getInitialESQLQuery(
@@ -55,5 +63,8 @@ export function getInitialESQLQuery(
   const filterBySearchText = getFilterBySearchText(query);
 
   const whereClause = getFinalWhereClause(filterByTimeParams, filterBySearchText);
-  return `FROM ${dataView.getIndexPattern()}${whereClause}${removeLimit ? '' : ' | LIMIT 10'}`;
+  const sourceCommand = getSourceCommand(dataView);
+  return `${sourceCommand} ${dataView.getIndexPattern()}${whereClause}${
+    removeLimit ? '' : ' | LIMIT 10'
+  }`;
 }
