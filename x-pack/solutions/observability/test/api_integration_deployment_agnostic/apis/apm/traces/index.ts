399727e78bc13cb6bb6105d9ad3b65de7f5aa6d7
iblancof <irene.blanco@elastic.co>
2025-11-13 18:49:50 +0100

    Add Unified trace span API tests

--- x-pack/solutions/observability/test/api_integration_deployment_agnostic/apis/apm/traces/index.ts
+++ x-pack/solutions/observability/test/api_integration_deployment_agnostic/apis/apm/traces/index.ts
@@ -17,5 +17,6 @@ export default function ({ loadTestFile }: DeploymentAgnosticFtrProviderContext)
     loadTestFile(require.resolve('./transaction_details.spec.ts'));
     loadTestFile(require.resolve('./focused_trace.spec.ts'));
     loadTestFile(require.resolve('./unified_trace_errors.spec.ts'));
+    loadTestFile(require.resolve('./unified_trace_span.spec.ts'));
   });
 }
