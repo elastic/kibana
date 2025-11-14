4fc45ca8d7205676c1c8aee9971c857da1dd0261
James Gowdy <jgowdy@elastic.co>
2025-11-14 13:02:23 +0000

    code clean up

--- x-pack/platform/plugins/shared/ml/public/application/datavisualizer/file_based/file_datavisualizer.tsx
+++ x-pack/platform/plugins/shared/ml/public/application/datavisualizer/file_based/file_datavisualizer.tsx
@@ -6,17 +6,14 @@
  */
 
 import type { FC } from 'react';
-import React, { Fragment, useState, useEffect, useMemo, useCallback } from 'react';
+import React, { useMemo, useCallback } from 'react';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
-// import { getFieldsStatsGrid } from '@kbn/data-visualizer-plugin/public';
 import { useTimefilter } from '@kbn/ml-date-picker';
 
-import type { ResultLinks } from '@kbn/file-upload-common';
+// import type { ResultLinks } from '@kbn/file-upload-common';
 import type { GetAdditionalLinks, GetAdditionalLinksParams } from '@kbn/file-upload';
 import { FileDataVisualizerWrapper } from '@kbn/file-upload';
-
-// import type { FileDataVisualizerSpec } from '@kbn/file-upload/src/file_upload_component';
 
 import { HelpMenu } from '../../components/help_menu';
 import {
@@ -32,13 +29,13 @@ import { mlNodesAvailable, getMlNodeCount } from '../../ml_nodes_check/check_ml_
 import { checkPermission } from '../../capabilities/check_capabilities';
 import { MlPageHeader } from '../../components/page_header';
 import { PageTitle } from '../../components/page_title';
+import { buildDependencies } from './util';
 
 export const FileDataVisualizerPage: FC = () => {
   useTimefilter({ timeRangeSelector: false, autoRefreshSelector: false });
   const { services } = useMlKibana();
   const {
     docLinks,
-    dataVisualizer,
     data: {
       dataViews: { get: getDataView },
     },
@@ -48,49 +45,11 @@ export const FileDataVisualizerPage: FC = () => {
   const mlManagementLocator = useMlManagementLocatorInternal();
   getMlNodeCount(mlApi);
 
-  const getDependencies = useCallback(
-    async () => ({
-      analytics: services.analytics,
-      application: services.application,
-      data: services.data,
-      fieldFormats: services.fieldFormats,
-      fileUpload: services.fileUpload,
-      http: services.http,
-      notifications: services.notifications,
-      share: services.share,
-      uiActions: services.uiActions,
-      uiSettings: services.uiSettings,
-      coreStart: {
-        analytics: services.analytics,
-        application: services.application,
-        chrome: services.chrome,
-        customBranding: services.customBranding,
-        docLinks: services.docLinks,
-        executionContext: services.executionContext,
-        featureFlags: services.featureFlags,
-        http: services.http,
-        injection: services.injection,
-        i18n: services.i18n,
-        notifications: services.notifications,
-        overlays: services.overlays,
-        uiSettings: services.uiSettings,
-        settings: services.settings,
-        fatalErrors: services.fatalErrors,
-        deprecations: services.deprecations,
-        theme: services.theme,
-        plugins: services.plugins,
-        pricing: services.pricing,
-        security: services.security,
-        userProfile: services.userProfile,
-        rendering: services.rendering,
-      },
-    }),
-    [services]
-  );
+  const getDependencies = useCallback(async () => buildDependencies(services), [services]);
 
   // const [FileDataVisualizer, setFileDataVisualizer] = useState<FileDataVisualizerSpec | null>(null);
   // resultLinks is for filebeat !!!!!!!!!!!!!!!
-  const [resultLinks, setResultLinks] = useState<ResultLinks | null>(null);
+  // const [resultLinks, setResultLinks] = useState<ResultLinks | null>(null);
 
   const getAdditionalLinks: GetAdditionalLinks = useMemo(
     () => [
@@ -153,40 +112,8 @@ export const FileDataVisualizerPage: FC = () => {
     [mlLocator]
   );
 
-  useEffect(() => {
-    // ML uses this function
-    if (dataVisualizer !== undefined) {
-      getMlNodeCount(mlApi);
-      // const { getFileDataVisualizerComponent } = dataVisualizer;
-      // getFileDataVisualizerComponent().then((resp) => {
-      //   const items = resp();
-      //   setFileDataVisualizer(() => items.component);
-      //   setResultLinks(items.resultLinks);
-      // });
-
-      // const gg = getFileDataVisualizerWrapper(
-      //   async () => ({
-      //     analytics: services.analytics,
-      //     application: services.application,
-      //     data: services.data,
-      //     fieldFormats: services.fieldFormats,
-      //     fileUpload: services.fileUpload,
-      //     http: services.http,
-      //     notifications: services.notifications,
-      //     share: services.share,
-      //     uiActions: services.uiActions,
-      //   }),
-      //   'data-visualizer-file-page',
-      //   undefined,
-      //   getFieldsStatsGrid
-      // );
-      // setFileDataVisualizer(() => gg as unknown as FileDataVisualizerSpec);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
-    <Fragment>
+    <>
       <>
         <MlPageHeader>
           <PageTitle
@@ -202,11 +129,11 @@ export const FileDataVisualizerPage: FC = () => {
           getDependencies={getDependencies}
           location={'ml-file-data-visualizer'}
           getAdditionalLinks={getAdditionalLinks}
-          resultLinks={resultLinks ?? undefined}
+          // resultLinks={resultLinks ?? undefined}
         />
       </>
 
       <HelpMenu docLink={docLinks.links.ml.guide} />
-    </Fragment>
+    </>
   );
 };
