9ca265cabc5d6dbaee964c3024b9e9b10568eb53
Sean Rathier <sean.rathier@elastic.co>
2025-11-13 20:41:15 -0500

    fixed the bugs

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/hooks/form.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/single_page_layout/hooks/form.tsx
@@ -204,7 +204,6 @@ async function savePackagePolicy(pkgPolicy: CreatePackagePolicyRequest['body']) 
     function formatPackage(pkg: NewPackagePolicy['package']) {
       return omit(pkg, 'title');
     }
-
     const result = await sendCreateAgentlessPolicy(
       {
         package: formatPackage(pkgPolicy.package),
@@ -218,7 +217,8 @@ async function savePackagePolicy(pkgPolicy: CreatePackagePolicyRequest['body']) 
           'id',
           'supports_agentless',
           'supports_cloud_connector',
-          'cloud_connector_id'
+          'cloud_connector_id',
+          'cloud_connector_name'
         ),
         id: pkgPolicy.id ? String(pkgPolicy.id) : undefined,
         inputs: formatInputs(pkgPolicy.inputs),
@@ -230,9 +230,9 @@ async function savePackagePolicy(pkgPolicy: CreatePackagePolicyRequest['body']) 
             ...(pkgPolicy.cloud_connector_id && {
               cloud_connector_id: pkgPolicy.cloud_connector_id,
             }),
-            // Only pass the name if creating a new connector (no cloud_connector_id)
-            ...(!pkgPolicy.cloud_connector_id && {
-              name: generateCloudConnectorName(pkgPolicy),
+
+            ...(pkgPolicy.cloud_connector_name && {
+              cloud_connector_name: pkgPolicy.cloud_connector_name,
             }),
           },
         }),
