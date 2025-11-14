9d5cf66c7c604218affb77de149ed86b2c6e3f53
Steph Milovic <stephanie.milovic@elastic.co>
2025-11-14 13:02:25 -0700

    fix header

--- x-pack/platform/plugins/shared/catchup_agent/server/tools/workflow/workflow_simplified_tools.ts
+++ x-pack/platform/plugins/shared/catchup_agent/server/tools/workflow/workflow_simplified_tools.ts
@@ -5,13 +5,6 @@
  * 2.0.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the "Elastic License
- * 2.0"; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
 import { z } from '@kbn/zod';
 import { ToolType } from '@kbn/onechat-common';
 import type { BuiltinToolDefinition } from '@kbn/onechat-server';
