3672787e269dc290db633e5faaa8918011295044
boriskirov <boris.kirov@elastic.co>
2025-11-14 09:47:55 +0100

    turn back the tests

--- x-pack/platform/plugins/shared/streams_app/test/scout/ui/fixtures/page_objects/streams_app.ts
+++ x-pack/platform/plugins/shared/streams_app/test/scout/ui/fixtures/page_objects/streams_app.ts
@@ -77,7 +77,10 @@ export class StreamsApp {
   async gotoAdvancedTab(streamName: string) {
     await this.gotoStreamManagementTab(streamName, 'advanced');
   }
- 
+
+  async clickGoBackToStreams() {    
+    await this.page.getByTestId('backToStreamsButton').click();
+  }
 
   async clickStreamNameLink(streamName: string) {
     await this.page.getByTestId(`streamsNameLink-${streamName}`).click();
