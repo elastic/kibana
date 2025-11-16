d5c6e8a86e884e581803716089e0a778d56eb970
Nathan Reese <reese.nathan@gmail.com>
2025-11-16 15:04:41 -0700

    [links] fix Can not save Dashboard after switching a Dashboard Link to an External Link

---
+++ src/platform/plugins/private/links/common/embeddable/transforms/get_options.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { DEFAULT_DASHBOARD_DRILLDOWN_OPTIONS } from "@kbn/presentation-util-plugin/public";
+import type { DashboardLink, ExternalLink, LinkOptions } from "../../../server";
+import { DASHBOARD_LINK_TYPE, LinkType } from "../../content_management";
+import { DEFAULT_URL_DRILLDOWN_OPTIONS } from "@kbn/ui-actions-enhanced-plugin/public";
+
+export function getOptions(type: LinkType, options: LinkOptions) {
+  if (!options) return undefined;
+
+  if (type === DASHBOARD_LINK_TYPE) {
+    const dashboardOptions = options as Required<DashboardLink>['options'];
+    return {
+      ...DEFAULT_DASHBOARD_DRILLDOWN_OPTIONS,
+      ...(typeof dashboardOptions.openInNewTab === 'boolean' && { openInNewTab: dashboardOptions.openInNewTab }),
+      ...(typeof dashboardOptions.useCurrentFilters === 'boolean' && { useCurrentFilters: dashboardOptions.useCurrentFilters }),
+      ...(typeof dashboardOptions.useCurrentDateRange === 'boolean' && { useCurrentDateRange: dashboardOptions.useCurrentDateRange }),
+    } as DashboardLink['options'];
+  }
+
+  const urlOptions = options as Required<ExternalLink>['options'];
+  return {
+    ...DEFAULT_URL_DRILLDOWN_OPTIONS,
+    ...(typeof urlOptions.openInNewTab === 'boolean' && { openInNewTab: urlOptions.openInNewTab }),
+    ...(typeof urlOptions.encodeUrl === 'boolean' && { encodeUrl: urlOptions.encodeUrl }),
+  } as ExternalLink['options'];
+}
--- src/platform/plugins/private/links/server/content_management/schema/v1/transform_utils.ts
+++ src/platform/plugins/private/links/server/content_management/schema/v1/transform_utils.ts
@@ -10,11 +10,12 @@
 import type { SavedObject, SavedObjectReference } from '@kbn/core-saved-objects-api-server';
 import type { Reference } from '@kbn/content-management-utils/src/types';
 import type { LinksItem } from '../../../../common/content_management';
-import type { LinksState, StoredLinksState } from './types';
+import type { DashboardLink, ExternalLink, LinksState, StoredLinksState } from './types';
 import {
   extractReferences,
   injectReferences,
 } from '../../../../common/embeddable/transforms/references';
+import { getOptions } from '../../../../common/embeddable/transforms/get_options';
 
 type PartialSavedObject<T> = Omit<SavedObject<Partial<T>>, 'references'> & {
   references: SavedObjectReference[] | undefined;
@@ -34,7 +35,10 @@ export function savedObjectToItem(
     ...rest,
     attributes: {
       ...attributes,
-      links,
+      links: links.map(link => ({
+        ...link,
+        ...(link.options && { options: getOptions(link.type, link.options) })
+      }) as DashboardLink | ExternalLink),
     },
     references: (references ?? []).filter(({ type }) => type === 'tag'),
   };
