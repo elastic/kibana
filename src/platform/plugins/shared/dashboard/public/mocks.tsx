6c49c8aa4d9ab4e4dbda3e9dc3e22e1fe0f08319
Zacqary Xeper <zacqary.xeper@elastic.co>
2025-11-17 11:00:38 -0600

    Fix dashboard api mock

--- src/platform/plugins/shared/dashboard/public/mocks.tsx
+++ src/platform/plugins/shared/dashboard/public/mocks.tsx
@@ -77,6 +77,7 @@ export function buildMockDashboardApi({
   const results = getDashboardApi({
     initialState,
     savedObjectId,
+    incomingEmbeddables: [],
     savedObjectResult: savedObjectId
       ? ({
           id: savedObjectId,
