5a97d5ef933d9d268ec8e8fa3f2aa593930072df
Nathan Reese <reese.nathan@gmail.com>
2025-11-18 14:50:03 -0700

    move .only

--- src/platform/test/api_integration/apis/dashboards/create_dashboard/schema.ts
+++ src/platform/test/api_integration/apis/dashboards/create_dashboard/schema.ts
@@ -13,7 +13,7 @@ import type { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getService }: FtrProviderContext) {
   const supertest = getService('supertest');
-  describe.only('dashboard REST schema', () => {
+  describe('dashboard REST schema', () => {
     /**
      * Only additive changes are allowed to Dashboard REST schemas
      * 
