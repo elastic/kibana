4660ddceca057b09e64b0deb30f17880995fa440
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-11-19 14:57:17 -0500

    [8.19] [Fleet] Add in progress agent count to automatic upgrade status (#243066) (#243526)
    # Backport
    
    This will backport the following commits from `main` to `8.19`:
    - [[Fleet] Add in progress agent count to automatic upgrade status
    (#243066)](https://github.com/elastic/kibana/pull/243066)
    
    <!--- Backport version: 10.2.0 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"Nicolas
    Chaulet","email":"nicolas.chaulet@elastic.co"},"sourceCommit":{"committedDate":"2025-11-17T17:20:59Z","message":"[Fleet]
    Add in progress agent count to automatic upgrade status
    (#243066)","sha":"08e6c9dbbe9eed37c061599e2c5d0086b6ad957b","branchLabelMapping":{"^v9.3.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:skip","backport:skip","Team:Fleet","v9.3.0"],"title":"[Fleet]
    Add in progress agent count to automatic upgrade
    status","number":243066,"url":"https://github.com/elastic/kibana/pull/243066","mergeCommit":{"message":"[Fleet]
    Add in progress agent count to automatic upgrade status
    (#243066)","sha":"08e6c9dbbe9eed37c061599e2c5d0086b6ad957b"}},"sourceBranch":"main","suggestedTargetBranches":[],"targetPullRequestStates":[{"branch":"main","label":"v9.3.0","branchLabelMappingKey":"^v9.3.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/243066","number":243066,"mergeCommit":{"message":"[Fleet]
    Add in progress agent count to automatic upgrade status
    (#243066)","sha":"08e6c9dbbe9eed37c061599e2c5d0086b6ad957b"}}]}]
    BACKPORT-->
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agents/components/manage_auto_upgrade_agents_modal/status_column.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agents/components/manage_auto_upgrade_agents_modal/status_column.tsx
@@ -33,7 +33,7 @@ export const StatusColumn: React.FunctionComponent<{
                   .join(' OR ')})`
               : ''
           }`
-        : `policy_id:"${agentPolicyId}" AND agent.version:"${version}"`;
+        : `policy_id:"${agentPolicyId}" AND (agent.version:"${version}" OR upgrade_details.target_version:"${version}") `;
       return getHref('agent_list', {
         kuery,
       });
@@ -55,6 +55,7 @@ export const StatusColumn: React.FunctionComponent<{
         version,
         agents: 0,
         failedUpgradeAgents: 0,
+        inProgressUpgradeAgents: 0,
       }
     );
   }, [autoUpgradeAgentsStatus, version]);
@@ -106,7 +107,10 @@ export const StatusColumn: React.FunctionComponent<{
 
     if (agentVersionCounts.failedUpgradeAgents > 0) {
       statusButton = failedStatus;
-    } else if (agentVersionCounts.agents === 0) {
+    } else if (
+      agentVersionCounts.agents === 0 &&
+      agentVersionCounts.inProgressUpgradeAgents === 0
+    ) {
       statusButton = notStartedStatus;
     } else {
       const currPercentage = calcPercentage(agentVersionCounts.agents);
