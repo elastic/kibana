00edd4d2ba9c0baefd3fea3e43633bc938a4fab4
paulina.shakirova <paulina.shakirova@elastic.co>
2025-11-19 15:02:43 +0100

    style

--- src/platform/packages/private/kbn-esql-editor/src/esql_editor.tsx
+++ src/platform/packages/private/kbn-esql-editor/src/esql_editor.tsx
@@ -938,8 +938,8 @@ const ESQLEditorInternal = function ESQLEditor({
 
   onLayoutChangeRef.current = onLayoutChange;
 
-  const codeEditorOptions = useMemo(
-    (): CodeEditorProps['options'] => ({
+  const codeEditorOptions: CodeEditorProps['options'] = useMemo(
+    () => ({
       hover: {
         above: false,
       },
