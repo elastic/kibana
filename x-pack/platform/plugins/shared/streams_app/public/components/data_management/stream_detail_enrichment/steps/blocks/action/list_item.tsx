2acdedc5bbbf5769c050725678ecb3139c042853
Quentin Couvelaire <quentin.couvelaire@elastic.co>
2025-11-19 14:56:36 -0500

    remove unneccessary space from drop processor description text

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/steps/blocks/action/list_item.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/steps/blocks/action/list_item.tsx
@@ -64,7 +64,7 @@ export const ActionBlockListItem = ({
   const dropProcessorDescription = i18n.translate(
     'xpack.streams.streamDetailView.managementTab.enrichment.dropProcessorDescription',
     {
-      defaultMessage: 'Drops documents ',
+      defaultMessage: 'Drops documents',
     }
   );
 
