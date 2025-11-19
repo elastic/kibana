dc90e7eaf8d004038165cebfc718eaa7e3676c1c
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-11-19 18:30:26 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/test/fleet_api_integration/apis/data_streams/deprecated_ilm_check.ts
+++ x-pack/platform/test/fleet_api_integration/apis/data_streams/deprecated_ilm_check.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 
 import type { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
 
