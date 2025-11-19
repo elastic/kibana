5bc466d0d9b24afa796aa211abd039879ec10e53
Ievgen Sorokopud <ievgen.sorokopud@elastic.co>
2025-11-19 14:50:43 +0300

    [Security Solution][Attacks/Alerts][Attack discovery page] Announcement icon with the tooltip (#243485)

--- x-pack/solutions/security/plugins/security_solution/public/attack_discovery/pages/page_title/index.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/attack_discovery/pages/page_title/index.tsx
@@ -5,25 +5,64 @@
  * 2.0.
  */
 
-import { EuiTitle, useEuiTheme } from '@elastic/eui';
+import { EuiBetaBadge, EuiFlexGroup, EuiFlexItem, EuiTitle, useEuiTheme } from '@elastic/eui';
 import { css } from '@emotion/react';
 import React from 'react';
 
+import { ATTACKS_ALERTS_ALIGNMENT_ENABLED } from '@kbn/security-solution-navigation';
+import { useKibana } from '../../../common/lib/kibana';
 import * as i18n from './translations';
+import { IconAnnouncementBadge } from './announcement_badge';
 
 const PageTitleComponent: React.FC = () => {
   const { euiTheme } = useEuiTheme();
+  const {
+    services: { featureFlags },
+  } = useKibana();
+
+  const attacksAlertsAlignmentEnabled = featureFlags.getBooleanValue(
+    ATTACKS_ALERTS_ALIGNMENT_ENABLED,
+    false
+  );
 
   return (
-    <EuiTitle
-      css={css`
-        margin-bottom: ${euiTheme.size.s};
-      `}
-      data-test-subj="attackDiscoveryPageTitle"
-      size="l"
+    <EuiFlexGroup
+      alignItems="center"
+      data-test-subj="pageTitle"
+      gutterSize="none"
+      responsive={false}
+      wrap={true}
     >
-      <h1>{i18n.ATTACK_DISCOVERY_PAGE_TITLE}</h1>
-    </EuiTitle>
+      <EuiFlexItem grow={false}>
+        <EuiTitle data-test-subj="attackDiscoveryPageTitle" size="l">
+          <h1>{i18n.ATTACK_DISCOVERY_PAGE_TITLE}</h1>
+        </EuiTitle>
+      </EuiFlexItem>
+
+      {attacksAlertsAlignmentEnabled && (
+        <EuiFlexItem
+          css={css`
+            margin: ${euiTheme.size.s} 0 0 ${euiTheme.size.m};
+          `}
+          grow={false}
+        >
+          <EuiBetaBadge
+            data-test-subj="attackDiscoveryAnnouncementBadge"
+            iconType={IconAnnouncementBadge}
+            label={''}
+            tooltipContent={i18n.ATTACK_DISCOVERY_ANNOUNCEMENT_TOOLTIP}
+            size="m"
+            color="hollow"
+            css={css`
+              .euiBetaBadge__icon {
+                position: relative;
+                top: 5px;
+              }
+            `}
+          />
+        </EuiFlexItem>
+      )}
+    </EuiFlexGroup>
   );
 };
 
