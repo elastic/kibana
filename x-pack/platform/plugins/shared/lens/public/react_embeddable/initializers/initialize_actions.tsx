5fb4c6e63a3fc0e7ef2b92aec3c48a2c9c8e7150
dej611 <dej611@gmail.com>
2025-11-19 18:30:07 +0100

    :label: Fix type

--- x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_actions.tsx
+++ x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_actions.tsx
@@ -29,7 +29,6 @@ import type {
   LensRuntimeState,
   ViewInDiscoverCallbacks,
   ViewUnderlyingDataArgs,
-  LensSerializedAPIConfig,
 } from '@kbn/lens-common';
 import type { LensSerializedAPIConfig } from '@kbn/lens-common-2';
 import { combineQueryAndFilters, getLayerMetaInfo } from '../../app_plugin/show_underlying_data';
