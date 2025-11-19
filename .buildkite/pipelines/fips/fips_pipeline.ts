501ca9abbae264bbb05878d015d94b4d95c073ad
Brad White <brad.white@elastic.co>
2025-11-19 12:56:37 +1000

    add pipeline upload

--- .buildkite/pipelines/fips/fips_pipeline.ts
+++ .buildkite/pipelines/fips/fips_pipeline.ts
@@ -7,10 +7,20 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-// import {
-//   areChangesSkippable,
-//   doAnyChangesMatch,
-//   getAgentImageConfig,
-//   emitPipeline,
-//   getPipeline
-// } from '#pipeline-utils';
+import {
+  emitPipeline,
+  getPipeline
+} from '#pipeline-utils';
+
+(async () => {
+  const pipeline: string[] = [];
+
+  try {
+    pipeline.push(getPipeline('.buildkite/pipelines/fips.yml', false));
+
+    emitPipeline(pipeline);
+  } catch (ex) {
+    console.error('Error while generating the pipeline steps: ' + ex.message, ex);
+    process.exit(1);
+  }
+})();
