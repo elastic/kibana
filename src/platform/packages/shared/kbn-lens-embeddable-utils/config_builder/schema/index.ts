ac4698a21bdee81cf52b6244a07e9c1bbc492c7f
Nick Partridge <nicholas.partridge@elastic.co>
2025-11-19 20:53:59 -0700

    fix: lens type errors due to max serialization length

--- src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/schema/index.ts
+++ src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/schema/index.ts
@@ -7,21 +7,34 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { TypeOf } from '@kbn/config-schema';
+import type { Type } from '@kbn/config-schema';
 import { schema } from '@kbn/config-schema';
+import type { MetricState } from './charts/metric';
 import { metricStateSchema } from './charts/metric';
+import type { LegacyMetricState } from './charts/legacy_metric';
 import { legacyMetricStateSchema } from './charts/legacy_metric';
+import type { GaugeState } from './charts/gauge';
 import { gaugeStateSchema } from './charts/gauge';
 import type { LensApiAllMetricOperations } from './metric_ops';
 import type { LensApiBucketOperations } from './bucket_ops';
 
-export const lensApiStateSchema = schema.oneOf([
+/**
+ * We need to break the type inference here to avoid exceeding the ts compiler serialization limit.
+ *
+ * This requires:
+ *  - Casting the schema as any
+ *  - Defining the `LensApiState` type from the schema types
+ *  - Exporting this value as `Type<LensApiState>`
+ */
+export const _lensApiStateSchema: any = schema.oneOf([
   metricStateSchema,
   legacyMetricStateSchema,
   gaugeStateSchema,
 ]);
 
-export type LensApiState = TypeOf<typeof lensApiStateSchema>;
+export type LensApiState = MetricState | LegacyMetricState | GaugeState;
+
+export const lensApiStateSchema: Type<LensApiState> = _lensApiStateSchema;
 
 export type { MetricState, metricStateSchemaNoESQL } from './charts/metric';
 export type { LegacyMetricState, legacyMetricStateSchemaNoESQL } from './charts/legacy_metric';
