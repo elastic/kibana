a88fd9f6a4bdc6fc2b036b7b7422c9b8e9be99db
Alex Szabo <alex.szabo@elastic.co>
2025-11-20 15:08:30 +0100

    tidy up run_lint_ts_projects_cli.ts

--- packages/kbn-lint-ts-projects-cli/run_lint_ts_projects_cli.ts
+++ packages/kbn-lint-ts-projects-cli/run_lint_ts_projects_cli.ts
@@ -109,70 +109,69 @@ function validateProjectOwnership(
   return failed;
 }
 
-run(
-  async ({ log, flagsReader }) => {
-    const filter = flagsReader.getPositionals();
-    const packages = getPackages(REPO_ROOT);
-    const allTargets = Array.from(TS_PROJECTS, (p) => new TsProjectLintTarget(p)).sort(
-      (a, b) => b.repoRel.length - a.repoRel.length
-    );
-
-    const toLint = Array.from(
-      new Set(
-        !filter.length
-          ? allTargets
-          : filter.map((input) => {
-              const pkg = allTargets.find(getFilter(input));
-
-              if (!pkg) {
-                throw createFailError(
-                  `unable to find a package matching [${input}]. Supply either a package id/name or path to a package`
-                );
-              }
-
-              return pkg;
-            })
-      )
-    ).sort((a, b) => a.repoRel.localeCompare(b.repoRel));
-
-    const skipRefs =
-      flagsReader.boolean('refs-check') === false || flagsReader.boolean('no-refs-check') === true;
-
-    const allFiles = await getRepoFiles();
-    const fileMap = new TsProjectFileMap(new PackageFileMap(packages, allFiles), TS_PROJECTS);
-    const { lintingErrorCount } = await runLintRules(
-      log,
-      toLint,
-      RULES.filter((r) => r.name !== 'referenceUsedPkgs' || skipRefs === false),
-      {
-        fix: flagsReader.boolean('fix'),
-        getFiles: (target) => fileMap.getFiles(target.tsProject),
-      }
-    );
-
-    const failed =
-      lintingErrorCount > 0 ||
-      (filter.length > 0 ? false : validateProjectOwnership(allTargets, allFiles, fileMap, log));
-
-    if (failed) {
-      throw createFailError('see above errors');
-    } else {
-      log.success('All TS projects linted successfully');
-    }
-  },
-  {
-    usage: `node scripts/package_linter [...packages]`,
-    flags: {
-      boolean: ['fix', 'refs-check', 'no-refs-check'],
-      alias: { f: 'fix', R: 'no-refs-check' },
-      default: { 'refs-check': true },
-      help: `
-        --no-lint          Disables linting rules, only validting that every file is a member of just one project
+const scriptOptions = {
+  usage: `node scripts/lint_ts_projects [...packages]`,
+  flags: {
+    boolean: ['fix', 'refs-check', 'no-refs-check'],
+    alias: { f: 'fix', R: 'no-refs-check' },
+    default: { 'refs-check': true },
+    help: `
+        --no-lint          Disables linting rules, only validating that every file is a member of just one project
         --fix              Automatically fix some issues in tsconfig.json files
         -R, --no-refs-check  Disables the reference checking rules, making the linting much faster, but less accruate
       `,
-    },
-    description:
-      'Validate packages using a set of rules that evolve over time. The vast majority of violations can be auto-fixed, so running with `--fix` is recommended.',
+  },
+  description:
+    'Validate packages using a set of rules that evolve over time. The vast majority of violations can be auto-fixed, so running with `--fix` is recommended.',
+};
+
+run(async ({ log, flagsReader }) => {
+  const filter = flagsReader.getPositionals();
+  const packages = getPackages(REPO_ROOT);
+  const allTargets = Array.from(TS_PROJECTS, (p) => new TsProjectLintTarget(p)).sort(
+    (a, b) => b.repoRel.length - a.repoRel.length
+  );
+
+  const toLint = Array.from(
+    new Set(
+      !filter.length
+        ? allTargets
+        : filter.map((input) => {
+            const pkg = allTargets.find(getFilter(input));
+
+            if (!pkg) {
+              throw createFailError(
+                `unable to find a package matching [${input}]. Supply either a package id/name or path to a package`
+              );
+            }
+
+            return pkg;
+          })
+    )
+  ).sort((a, b) => a.repoRel.localeCompare(b.repoRel));
+
+  const skipRefs =
+    flagsReader.boolean('refs-check') === false || flagsReader.boolean('no-refs-check') === true;
+
+  const allFiles = await getRepoFiles();
+  const fileMap = new TsProjectFileMap(new PackageFileMap(packages, allFiles), TS_PROJECTS);
+  const { lintingErrorCount } = await runLintRules(
+    log,
+    toLint,
+    RULES.filter((r) => r.name !== 'referenceUsedPkgs' || skipRefs === false),
+    {
+      fix: flagsReader.boolean('fix'),
+      getFiles: (target) => fileMap.getFiles(target.tsProject),
+    }
+  );
+
+  const failed =
+    lintingErrorCount > 0 ||
+    (filter.length > 0 ? false : validateProjectOwnership(allTargets, allFiles, fileMap, log));
+
+  if (failed) {
+    throw createFailError('see above errors');
+  } else {
+    log.success('All TS projects linted successfully');
   }
-);
+}, scriptOptions);
