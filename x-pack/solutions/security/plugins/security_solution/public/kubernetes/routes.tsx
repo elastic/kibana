66f82023aa7b014782fcd673c3e59836881f8d94
animehart <rickyangwyn@gmail.com>
2025-11-20 12:23:40 -0800

    hide kubernetes page behind feature flag

--- x-pack/solutions/security/plugins/security_solution/public/kubernetes/routes.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/kubernetes/routes.tsx
@@ -6,26 +6,34 @@
  */
 
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import { TrackApplicationView } from '@kbn/usage-collection-plugin/public';
-import { allowedExperimentalValues } from '../../common';
 import { KubernetesContainer } from './pages';
-
+import { useIsExperimentalFeatureEnabled } from '../common/hooks/use_experimental_features';
 import type { SecuritySubPluginRoutes } from '../app/types';
 import { SecurityPageName } from '../app/types';
-import { KUBERNETES_PATH } from '../../common/constants';
+import { KUBERNETES_PATH, APP_PATH } from '../../common/constants';
 import { PluginTemplateWrapper } from '../common/components/plugin_template_wrapper';
 
-export const KubernetesRoutes = () => (
-  <PluginTemplateWrapper>
-    <TrackApplicationView viewId={SecurityPageName.kubernetes}>
-      <KubernetesContainer />
-    </TrackApplicationView>
-  </PluginTemplateWrapper>
-);
+const KubernetesRoutesGuard = () => {
+  const isKubernetesEnabled = useIsExperimentalFeatureEnabled('kubernetesEnabled');
+
+  if (!isKubernetesEnabled) {
+    return <Redirect to={APP_PATH} />;
+  }
+
+  return (
+    <PluginTemplateWrapper>
+      <TrackApplicationView viewId={SecurityPageName.kubernetes}>
+        <KubernetesContainer />
+      </TrackApplicationView>
+    </PluginTemplateWrapper>
+  );
+};
 
 export const routes: SecuritySubPluginRoutes = [
   {
-    path: allowedExperimentalValues.kubernetesEnabled ? KUBERNETES_PATH : [],
-    component: KubernetesRoutes,
+    path: KUBERNETES_PATH,
+    component: KubernetesRoutesGuard,
   },
 ];
