43b4fecd93033752ae6f8c66f61f4b8a3edb4217
iblancof <irene.blanco@elastic.co>
2025-11-21 09:39:37 +0100

    Cleanup and loading in SimilarErrorsOccurrencesChart

--- src/platform/plugins/shared/unified_doc_viewer/public/components/doc_viewer_logs_overview/sub_components/similar_errors/similar_errors_occurrences_chart.tsx
+++ src/platform/plugins/shared/unified_doc_viewer/public/components/doc_viewer_logs_overview/sub_components/similar_errors/similar_errors_occurrences_chart.tsx
@@ -16,10 +16,10 @@ import {
   type LensSeriesLayer,
 } from '@kbn/lens-embeddable-utils/config_builder';
 import type { LensAttributes } from '@kbn/lens-embeddable-utils/config_builder';
-import type { XYLegendValue } from '@kbn/chart-expressions-common';
-import { ContentFrameworkChart } from '../../../content_framework/chart';
+import { EuiLoadingChart, EuiFlexGroup, EuiCallOut } from '@elastic/eui';
 import { useDataSourcesContext } from '../../../observability/traces/hooks/use_data_sources';
 import { getUnifiedDocViewerServices } from '../../../../plugin';
+import { ContentFrameworkChart } from '../../../content_framework/chart';
 
 const chartTitle = i18n.translate(
   'unifiedDocViewer.docViewerLogsOverview.subComponents.similarErrors.occurrences.title',
@@ -28,18 +28,61 @@ const chartTitle = i18n.translate(
   }
 );
 
+const errorMessage = i18n.translate(
+  'unifiedDocViewer.docViewerLogsOverview.subComponents.similarErrors.occurrences.error',
+  {
+    defaultMessage: 'An error occurred while fetching chart data.',
+  }
+);
+
+const chartLayers: LensSeriesLayer[] = [
+  {
+    type: 'series',
+    seriesType: 'bar',
+    xAxis: {
+      type: 'dateHistogram',
+      field: '@timestamp',
+    },
+    yAxis: [
+      {
+        value: 'occurrences',
+        label: 'Occurrences',
+        format: 'number',
+      },
+    ],
+  },
+];
+
+const defaultLensConfig: Omit<LensXYConfig, 'title' | 'dataset'> = {
+  chartType: 'xy',
+  layers: chartLayers,
+  legend: {
+    show: false,
+  },
+  axisTitleVisibility: {
+    showXAxisTitle: false,
+    showYAxisTitle: false,
+    showYRightAxisTitle: false,
+  },
+  fittingFunction: 'Linear',
+};
+
 export interface SimilarErrorsOccurrencesChartProps {
   baseEsqlQuery: ReturnType<typeof import('./get_esql_query').getEsqlQuery>;
 }
 
 export function SimilarErrorsOccurrencesChart({
   baseEsqlQuery,
-  title = chartTitle,
 }: SimilarErrorsOccurrencesChartProps) {
   const { data, lens } = getUnifiedDocViewerServices();
   const { indexes } = useDataSourcesContext();
-  const EmbeddableComponent = lens?.EmbeddableComponent;
-  const timeRange = data.query.timefilter.timefilter.getTime();
+  const [lensAttributes, setLensAttributes] = useState<LensAttributes | undefined>(undefined);
+  const [hasError, setHasError] = useState<boolean>(false);
+  const EmbeddableComponent = useMemo(() => lens?.EmbeddableComponent, [lens]);
+  const timeRange = useMemo(
+    () => data.query.timefilter.timefilter.getTime(),
+    [data.query.timefilter.timefilter]
+  );
 
   const chartEsqlQuery = useMemo(() => {
     if (!baseEsqlQuery || !indexes.logs) {
@@ -55,106 +98,47 @@ export function SimilarErrorsOccurrencesChart({
       .toString();
   }, [baseEsqlQuery, indexes.logs]);
 
-  // Build Lens attributes from ES|QL query
-  const [lensAttributes, setLensAttributes] = useState<LensAttributes | undefined>(undefined);
-
   useEffect(() => {
     if (!chartEsqlQuery || !data.dataViews) {
       setLensAttributes(undefined);
+      setHasError(false);
       return;
     }
 
+    const builder = new LensConfigBuilder(data.dataViews);
+    const lensConfig: LensXYConfig = {
+      ...defaultLensConfig,
+      title: chartTitle,
+      dataset: {
+        esql: chartEsqlQuery,
+      },
+    };
+
     const buildAttributes = async () => {
       try {
-        const builder = new LensConfigBuilder(data.dataViews);
-        const chartLayers: LensSeriesLayer[] = [
-          {
-            type: 'series',
-            seriesType: 'bar',
-            xAxis: {
-              type: 'dateHistogram',
-              field: '@timestamp',
-            },
-            yAxis: [
-              {
-                value: 'occurrences',
-                label: 'Occurrences',
-                format: 'number',
-              },
-            ],
-          },
-        ];
-        const lensConfig: LensXYConfig = {
-          chartType: 'xy',
-          title,
-          dataset: {
-            esql: chartEsqlQuery,
-          },
-          layers: chartLayers,
-          legend: {
-            show: true,
-            position: 'bottom',
-            legendStats: ['total' as XYLegendValue],
-          },
-          axisTitleVisibility: {
-            showXAxisTitle: false,
-            showYAxisTitle: false,
-            showYRightAxisTitle: false,
-          },
-          fittingFunction: 'Linear',
-        };
-
         const attributes = (await builder.build(lensConfig, {
           query: {
             esql: chartEsqlQuery,
           },
         })) as LensAttributes;
 
-        // Ensure legend shows even with single series and displays total
-        if (attributes.state?.visualization) {
-          const visualization = attributes.state.visualization as any;
-          if (visualization.legend) {
-            visualization.legend.showSingleSeries = true;
-            visualization.legend.isVisible = true;
-          }
-        }
-
-        // Update column label to "Occurrences" in datasource layers
-        // Note: LensConfigBuilder doesn't support passing label directly for ES|QL columns
-        // when using textBased datasource, so we update it after building the attributes
-        if (attributes.state?.datasourceStates?.textBased?.layers) {
-          const layers = attributes.state.datasourceStates.textBased.layers;
-          Object.keys(layers).forEach((layerId) => {
-            const layer = layers[layerId];
-            if (layer.columns) {
-              layer.columns.forEach((column: any) => {
-                // Find the column that references "occurrences" field from our ES|QL query
-                if (column.fieldName === 'occurrences') {
-                  column.label = 'Occurrences';
-                  column.customLabel = true;
-                }
-              });
-            }
-          });
-        }
-
         setLensAttributes(attributes);
-      } catch (error) {
-        // TODO handle error gracefully (?)
-        console.error('Error building Lens attributes:', error);
+        setHasError(false);
+      } catch (err) {
         setLensAttributes(undefined);
+        setHasError(true);
       }
     };
 
     buildAttributes();
-  }, [chartEsqlQuery, data.dataViews, title]);
+  }, [chartEsqlQuery, data.dataViews]);
 
-  if (!lensAttributes || !EmbeddableComponent) {
-    return null;
-  }
+  const content = useMemo(() => {
+    if (!lensAttributes || !EmbeddableComponent) {
+      return <EuiLoadingChart size="l" />; // TODO Place loading in the center of the area
+    }
 
-  return (
-    <ContentFrameworkChart data-test-subj="docViewerSimilarErrorsLatencyChart" title={title}>
+    return (
       <EmbeddableComponent
         id="similar-errors-chart"
         attributes={lensAttributes}
@@ -162,8 +146,28 @@ export function SimilarErrorsOccurrencesChart({
         esqlVariables={[]}
         viewMode="view"
         noPadding={true}
-        style={{ height: 150 }}
+        style={{ height: 120 }}
       />
-    </ContentFrameworkChart>
+    );
+  }, [lensAttributes, EmbeddableComponent, timeRange]);
+
+  if (hasError) {
+    return <EuiCallOut color="danger" announceOnMount size="s" title={errorMessage} />;
+  }
+
+  return (
+    <EuiFlexGroup
+      data-test-subj="docViewerSimilarErrorsOccurrencesChart"
+      justifyContent="center"
+      alignItems="center"
+      style={{ height: 132 }}
+    >
+      <ContentFrameworkChart
+        data-test-subj="docViewerSimilarErrorsOccurrencesChart"
+        title={chartTitle}
+      >
+        {content}
+      </ContentFrameworkChart>
+    </EuiFlexGroup>
   );
 }
