2c0450b676ad82a3a43f1e4f0bda9a4046613c79
Gonçalo Rica <goncalo.rica@elastic.co>
2025-11-21 16:08:29 +0100

    compactMap and less lodash

--- x-pack/solutions/observability/plugins/apm/server/routes/span_links/get_span_links_details.ts
+++ x-pack/solutions/observability/plugins/apm/server/routes/span_links/get_span_links_details.ts
@@ -6,7 +6,7 @@
  */
 import { kqlQuery, rangeQuery } from '@kbn/observability-plugin/server';
 import { ProcessorEvent } from '@kbn/observability-plugin/common';
-import { chunk, compact, keyBy } from 'lodash';
+import { chunk } from 'lodash';
 import { accessKnownApmEventFields } from '@kbn/apm-data-access-plugin/server/utils';
 import type { SpanLinkDetails } from '@kbn/apm-types';
 import { asMutableArray } from '../../../common/utils/as_mutable_array';
@@ -34,6 +34,7 @@ import { getBufferedTimerange } from './utils';
 import type { APMEventClient } from '../../lib/helpers/create_es_client/create_apm_event_client';
 import type { AgentName } from '../../../typings/es_schemas/ui/fields/agent';
 import { parseOtelDuration } from '../../lib/helpers/parse_otel_duration';
+import { compactMap } from '../../utils/compact_map';
 
 async function fetchSpanLinksDetails({
   apmEventClient,
@@ -131,76 +132,76 @@ async function fetchSpanLinksDetails({
     { skipProcessorEventFilter: true }
   );
 
-  // Avoid overhead of parsing a string accessor with a function
-  const spanIdsMap = keyBy(spanLinks, (link) => link.span.id);
+  const spanIdsMap = spanLinks.reduce<Record<string, SpanLink>>((acc, link) => {
+    acc[link.span.id] = link;
+    return acc;
+  }, {});
 
-  return compact(
-    response.hits.hits.map((hit): SpanLinkDetails | null => {
-      const commonEvent = accessKnownApmEventFields(hit.fields).requireFields(requiredFields);
+  return compactMap(response.hits.hits, (hit): SpanLinkDetails | null => {
+    const commonEvent = accessKnownApmEventFields(hit.fields).requireFields(requiredFields);
 
-      const commonDetails = {
-        serviceName: commonEvent[SERVICE_NAME],
-        environment: commonEvent[SERVICE_ENVIRONMENT] as Environment,
-      };
+    const commonDetails = {
+      serviceName: commonEvent[SERVICE_NAME],
+      environment: commonEvent[SERVICE_ENVIRONMENT] as Environment,
+    };
 
-      switch (commonEvent[PROCESSOR_EVENT]) {
-        case ProcessorEvent.transaction: {
-          const event = commonEvent.requireFields(requiredTxFields);
+    switch (commonEvent[PROCESSOR_EVENT]) {
+      case ProcessorEvent.transaction: {
+        const event = commonEvent.requireFields(requiredTxFields);
 
-          return {
-            traceId: event[TRACE_ID],
-            spanId: event[TRANSACTION_ID],
-            details: {
-              ...commonDetails,
-              transactionId: event[TRANSACTION_ID],
-              agentName: event[AGENT_NAME],
-              spanName: event[TRANSACTION_NAME],
-              duration: event[TRANSACTION_DURATION],
-            },
-          };
-        }
-
-        case ProcessorEvent.span: {
-          const event = commonEvent.requireFields(requiredSpanFields);
-
-          // The above query might return other spans from the same transaction because siblings spans share the same transaction.id
-          // so, if it is a span we need to guarantee that the span.id is the same as the span links ids
-          if (!spanIdsMap[event[SPAN_ID]]) {
-            return null;
-          }
-
-          return {
-            traceId: event[TRACE_ID],
-            spanId: event[SPAN_ID],
-            details: {
-              ...commonDetails,
-              transactionId: event[TRANSACTION_ID],
-              agentName: event[AGENT_NAME],
-              spanName: event[SPAN_NAME],
-              duration: event[SPAN_DURATION],
-              spanSubtype: event[SPAN_SUBTYPE],
-              spanType: event[SPAN_TYPE],
-            },
-          };
-        }
-
-        default: {
-          const event = commonEvent.requireFields(requiredUnprocessedOtelSpanFields);
-
-          return {
-            traceId: event[TRACE_ID],
-            spanId: event[SPAN_ID],
-            details: {
-              ...commonDetails,
-              agentName: event[SERVICE_LANGUAGE_NAME] as AgentName,
-              spanName: event[SPAN_NAME],
-              duration: parseOtelDuration(event.duration),
-            },
-          };
-        }
+        return {
+          traceId: event[TRACE_ID],
+          spanId: event[TRANSACTION_ID],
+          details: {
+            ...commonDetails,
+            transactionId: event[TRANSACTION_ID],
+            agentName: event[AGENT_NAME],
+            spanName: event[TRANSACTION_NAME],
+            duration: event[TRANSACTION_DURATION],
+          },
+        };
       }
-    })
-  );
+
+      case ProcessorEvent.span: {
+        const event = commonEvent.requireFields(requiredSpanFields);
+
+        // The above query might return other spans from the same transaction because siblings spans share the same transaction.id
+        // so, if it is a span we need to guarantee that the span.id is the same as the span links ids
+        if (!spanIdsMap[event[SPAN_ID]]) {
+          return null;
+        }
+
+        return {
+          traceId: event[TRACE_ID],
+          spanId: event[SPAN_ID],
+          details: {
+            ...commonDetails,
+            transactionId: event[TRANSACTION_ID],
+            agentName: event[AGENT_NAME],
+            spanName: event[SPAN_NAME],
+            duration: event[SPAN_DURATION],
+            spanSubtype: event[SPAN_SUBTYPE],
+            spanType: event[SPAN_TYPE],
+          },
+        };
+      }
+
+      default: {
+        const event = commonEvent.requireFields(requiredUnprocessedOtelSpanFields);
+
+        return {
+          traceId: event[TRACE_ID],
+          spanId: event[SPAN_ID],
+          details: {
+            ...commonDetails,
+            agentName: event[SERVICE_LANGUAGE_NAME] as AgentName,
+            spanName: event[SPAN_NAME],
+            duration: parseOtelDuration(event.duration),
+          },
+        };
+      }
+    }
+  });
 }
 
 export async function getSpanLinksDetails({
@@ -251,17 +252,15 @@ export async function getSpanLinksDetails({
   // It's important to keep the original order of the span links,
   // so loops trough the original list merging external links and links with details.
   // external links are links that the details were not found in the ES query.
-  return compact(
-    spanLinks.map((item) => {
-      const key = `${item.trace.id}:${item.span.id}`;
-      const details = spanLinksDetailsMap[key];
+  return compactMap(spanLinks, (item) => {
+    const key = `${item.trace.id}:${item.span.id}`;
+    const details = spanLinksDetailsMap[key];
 
-      if (details) {
-        return details;
-      }
+    if (details) {
+      return details;
+    }
 
-      // When kuery is not set, we return external links, else we hide this item.
-      return !kuery.length ? { traceId: item.trace.id, spanId: item.span.id } : undefined;
-    })
-  );
+    // When kuery is not set, we return external links, else we hide this item.
+    return !kuery.length ? { traceId: item.trace.id, spanId: item.span.id } : undefined;
+  });
 }
