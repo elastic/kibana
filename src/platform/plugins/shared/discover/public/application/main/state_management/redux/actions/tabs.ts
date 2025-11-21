bc7d6e48caea1f60f6bb7fd8373303987051c808
Milosz Radzynski <milosz.radzynski@elastic.co>
2025-11-21 11:18:40 +0100

    Apply requested chagnes

--- src/platform/plugins/shared/discover/public/application/main/state_management/redux/actions/tabs.ts
+++ src/platform/plugins/shared/discover/public/application/main/state_management/redux/actions/tabs.ts
@@ -16,7 +16,7 @@ import { getInitialESQLQuery } from '@kbn/esql-utils';
 import type { TabItem } from '@kbn/unified-tabs';
 import type { DiscoverSession } from '@kbn/saved-search-plugin/common';
 import type { UISession } from '@kbn/data-plugin/public/search/session/sessions_mgmt/types';
-import type { OpenInNewTabExtPointAction } from '../../../../../context_awareness';
+import type { OpenInNewTabParams } from '../../../../../context_awareness';
 import { createDataSource } from '../../../../../../common/data_sources/utils';
 import { type TabState } from '../types';
 import { selectAllTabs, selectRecentlyClosedTabs, selectTab } from '../selectors';
@@ -463,9 +463,11 @@ export const openInNewTab: InternalStateThunkActionCreator<
     );
   };
 
-export const openInNewTabExtPointAction: InternalStateThunkActionCreator<
-  [OpenInNewTabExtPointAction]
-> = ({ query, tabLabel, timeRange }) =>
+export const openInNewTabExtPointAction: InternalStateThunkActionCreator<[OpenInNewTabParams]> = ({
+  query,
+  tabLabel,
+  timeRange,
+}) =>
   function openInNewTabExtPointActionThunkFn(dispatch) {
     const appState: TabState['appState'] = { query };
     const globalState: TabState['globalState'] = { timeRange };
