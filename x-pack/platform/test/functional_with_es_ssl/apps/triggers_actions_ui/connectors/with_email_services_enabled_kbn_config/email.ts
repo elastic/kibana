d0c5e2ffa1cfcaa9f673edb283ecdb8d7364d73d
Christos Nasikas <christos.nasikas@elastic.co>
2025-11-22 14:26:07 +0200

    Fix integration test

--- x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/connectors/with_email_services_enabled_kbn_config/email.ts
+++ x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/connectors/with_email_services_enabled_kbn_config/email.ts
@@ -24,10 +24,12 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
       const emailServicesOptions = await find.allByCssSelector(
         '[data-test-subj="emailServiceSelectInput"] > option'
       );
+
       expect(emailServicesOptions.length).to.be(3);
-      expect(await emailServicesOptions[0].getVisibleText()).to.be(' '); // empty option
-      expect(await emailServicesOptions[1].getVisibleText()).to.be('Gmail');
-      expect(await emailServicesOptions[2].getVisibleText()).to.be('Amazon SES');
+
+      expect(await emailServicesOptions[0].getVisibleText()).to.be('Gmail');
+      expect(await emailServicesOptions[1].getVisibleText()).to.be('Amazon SES');
+      expect(await emailServicesOptions[2].getVisibleText()).to.be('Other');
     });
   });
 };
