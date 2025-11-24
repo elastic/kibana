19294d3537c20ae306f4f7d35afa6e272d5be90c
Nicholas Peretti <nicholas.peretti@elastic.co>
2025-11-24 18:54:51 +0000

    Add assignee filter on new attacks page

--- x-pack/solutions/security/plugins/security_solution/public/detections/components/attacks/content.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/detections/components/attacks/content.tsx
@@ -11,7 +11,9 @@ import styled from '@emotion/styled';
 import { noop } from 'lodash/fp';
 import type { DataView } from '@kbn/data-views-plugin/common';
 
+import { isEqual } from 'lodash';
 import { Schedule } from '../../../attack_discovery/pages/header/schedule';
+import { FilterByAssigneesPopover } from '../../../common/components/filter_by_assignees_popover/filter_by_assignees_popover';
 import { PAGE_TITLE } from '../../pages/attacks/translations';
 import { HeaderPage } from '../../../common/components/header_page';
 import { SecuritySolutionPageWrapper } from '../../../common/components/page_wrapper';
@@ -20,6 +22,7 @@ import { Display } from '../../../explore/hosts/pages/display';
 import { SearchBarSection } from './search_bar/search_bar_section';
 import { SchedulesFlyout } from './schedule_flyout';
 import { TableSection } from './table/table_section';
+import type { AssigneesIdsSelection } from '../../../common/components/assignees/types';
 
 export const CONTENT_TEST_ID = 'attacks-page-content';
 export const SECURITY_SOLUTION_PAGE_WRAPPER_TEST_ID = 'attacks-page-security-solution-page-wrapper';
@@ -54,6 +57,16 @@ export const AttacksPageContent = React.memo(({ dataView }: AttacksPageContentPr
     setShowFlyout(true);
   }, []);
   const onClose = useCallback(() => setShowFlyout(false), []);
+  const [assignees, setAssignees] = useState<AssigneesIdsSelection[]>([]);
+
+  const onAssigneesSelectionChange = useCallback(
+    (newAssignees: AssigneesIdsSelection[]) => {
+      if (!isEqual(newAssignees, assignees)) {
+        setAssignees(newAssignees);
+      }
+    },
+    [assignees]
+  );
 
   return (
     <StyledFullHeightContainer data-test-subj={CONTENT_TEST_ID} ref={containerElement}>
@@ -66,6 +79,10 @@ export const AttacksPageContent = React.memo(({ dataView }: AttacksPageContentPr
         <Display show={!globalFullScreen}>
           <HeaderPage title={PAGE_TITLE}>
             <Schedule openFlyout={openFlyout} />
+            <FilterByAssigneesPopover
+              selectedUserIds={assignees}
+              onSelectionChange={onAssigneesSelectionChange}
+            />
           </HeaderPage>
           <EuiHorizontalRule margin="none" />
           <EuiSpacer size="l" />
