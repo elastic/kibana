84f95bb9e29f8a8b2fdc91793cdb289080e09297
pgayvallet <pierre.gayvallet@elastic.co>
2025-11-25 14:24:10 +0100

    add new plugin

---
+++ x-pack/platform/plugins/shared/agent_builder_platform/server/types.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { WorkflowsServerPluginSetup } from '@kbn/workflows-management-plugin/server';
+
+export interface PluginSetupDependencies {
+  workflowsManagement?: WorkflowsServerPluginSetup;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface PluginStartDependencies {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AgentBuilderPlatformPluginSetup {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface AgentBuilderPlatformPluginStart {}
