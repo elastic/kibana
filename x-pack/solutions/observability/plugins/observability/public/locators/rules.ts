7ff1640bd9a99d66210b8338b23ea9ede849be28
Francesco Fagnani <francesco.fagnani@elastic.co>
2025-11-25 16:43:30 +0100

    refactor(locators): moved rules and slo locator ids to shared package

--- x-pack/solutions/observability/plugins/observability/public/locators/rules.ts
+++ x-pack/solutions/observability/plugins/observability/public/locators/rules.ts
@@ -6,21 +6,11 @@
  */
 
 import { setStateToKbnUrl } from '@kbn/kibana-utils-plugin/public';
-import type { SerializableRecord } from '@kbn/utility-types';
 import type { LocatorDefinition } from '@kbn/share-plugin/public';
-import type { RuleStatus } from '@kbn/triggers-actions-ui-plugin/public';
-import { rulesLocatorID } from '../../common';
+import { rulesLocatorID, type RulesLocatorParams } from '@kbn/deeplinks-observability';
 import { RULES_PATH } from '../../common/locators/paths';
 
-export interface RulesParams extends SerializableRecord {
-  lastResponse?: string[];
-  params?: Record<string, string | number>;
-  search?: string;
-  status?: RuleStatus[];
-  type?: string[];
-}
-
-export type RulesLocatorParams = RulesParams;
+export type { RulesLocatorParams };
 
 export class RulesLocatorDefinition implements LocatorDefinition<RulesLocatorParams> {
   public readonly id = rulesLocatorID;
