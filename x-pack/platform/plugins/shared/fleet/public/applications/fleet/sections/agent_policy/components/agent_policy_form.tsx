e3a6ce682e21b5e288af45d16e56f356fb05e848
Cristina Amico <criamico@users.noreply.github.com>
2025-11-26 15:45:26 +0100

    remove unused export

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/components/agent_policy_form.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/components/agent_policy_form.tsx
@@ -45,7 +45,7 @@ interface Props {
   updateAdvancedSettingsHasErrors: (hasErrors: boolean) => void;
   setInvalidSpaceError: (hasErrors: boolean) => void;
 }
-export const AgentPolicyFormContext = React.createContext<
+const AgentPolicyFormContext = React.createContext<
   | {
       agentPolicy: Partial<NewAgentPolicy | AgentPolicy> & { [key: string]: any };
       updateAgentPolicy: (u: Partial<NewAgentPolicy | AgentPolicy>) => void;
