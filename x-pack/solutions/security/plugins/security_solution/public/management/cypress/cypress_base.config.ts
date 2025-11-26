d487d5d7a92716eea90a1408d636ff100ae97d22
Ashokaditya <ashokaditya@elastic.co>
2025-11-26 11:35:47 +0100

    revert spec change

--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/cypress_base.config.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/cypress_base.config.ts
@@ -77,8 +77,7 @@ export const getCypressBaseConfig = (
         // baseUrl: To override, set Env. variable `CYPRESS_BASE_URL`
         baseUrl: 'http://localhost:5601',
         supportFile: 'public/management/cypress/support/e2e.ts',
-        specPattern:
-          'public/management/cypress/e2e/**/automated_response_actions.cy.{js,jsx,ts,tsx}',
+        specPattern: 'public/management/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
         experimentalRunAllSpecs: true,
         experimentalMemoryManagement: true,
         experimentalInteractiveRunEvents: true,
