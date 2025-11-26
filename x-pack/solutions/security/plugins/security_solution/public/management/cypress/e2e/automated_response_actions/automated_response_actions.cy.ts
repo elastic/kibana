bc468a741f7ac94faeece97c74bbdaf637828387
Ashokaditya <ashokaditya@elastic.co>
2025-11-26 10:05:23 +0100

    update spec correctly for flaky test runner

--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/automated_response_actions/automated_response_actions.cy.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/automated_response_actions/automated_response_actions.cy.ts
@@ -76,7 +76,7 @@ describe(
       }
     });
 
-    it.only('should have been called against a created host', () => {
+    it('should have been called against a created host', () => {
       waitForEndpointListPageToBeLoaded(createdHost.hostname);
       toggleRuleOffAndOn(ruleName);
 
