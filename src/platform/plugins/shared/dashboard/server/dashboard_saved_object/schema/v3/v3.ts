fedf6162a7d7777f72c51c3aa4893f52466c45ee
Marta Bondyra <4283304+mbondyra@users.noreply.github.com>
2025-11-26 18:42:12 +0100

    Update v3.ts

--- src/platform/plugins/shared/dashboard/server/dashboard_saved_object/schema/v3/v3.ts
+++ src/platform/plugins/shared/dashboard/server/dashboard_saved_object/schema/v3/v3.ts
@@ -33,7 +33,6 @@ export const sectionSchema = schema.object({
 export const dashboardAttributesSchema = dashboardAttributesSchemaV2.extends(
   {
     sections: schema.maybe(schema.arrayOf(sectionSchema)),
-    project_routing: schema.maybe(schema.string()),
   },
-  { unknowns: 'ignore' }
+  { unknowns: 'allow' }
 );
