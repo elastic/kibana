b0e6efc78afc2526a85d9b55d9603e3f725f2679
Julian Gernun <17549662+jcger@users.noreply.github.com>
2025-11-27 10:50:53 +0100

    lazy load zod and generator

--- x-pack/platform/plugins/shared/stack_connectors/public/connector_types_from_spec/index.tsx
+++ x-pack/platform/plugins/shared/stack_connectors/public/connector_types_from_spec/index.tsx
@@ -9,29 +9,43 @@ import { lazy } from 'react';
 import type { ActionTypeModel } from '@kbn/alerts-ui-shared';
 import { type ConnectorSpec } from '@kbn/connector-specs';
 import type { TriggersAndActionsUIPublicPluginSetup } from '@kbn/triggers-actions-ui-plugin/public';
-import { generateFormFields } from '@kbn/response-ops-form-generator';
 import { getIcon } from './get_icon';
-import { generateSchema } from './generate_schema';
 
 export function registerConnectorTypesFromSpecs({
   connectorTypeRegistry,
 }: {
   connectorTypeRegistry: TriggersAndActionsUIPublicPluginSetup['actionTypeRegistry'];
 }) {
-  // Creating an async chunk for the connectors specs.
-  // This is a workaround to avoid webpack from bundling the entire @kbn/connector-specs package into the main stackConnectors plugin bundle.
-  // If this chunk grows too much, we could have problems in some pages since the connectors won't be registered in time for rendering.
-  import(
-    /* webpackChunkName: "connectorsSpecs" */
-    '@kbn/connector-specs'
-  ).then(({ connectorsSpecs }) => {
+  Promise.all([
+    // Creating an async chunk for the connectors specs.
+    // This is a workaround to avoid webpack from bundling the entire @kbn/connector-specs package into the main stackConnectors plugin bundle.
+    // If this chunk grows too much, we could have problems in some pages since the connectors won't be registered in time for rendering.
+    import(
+      /* webpackChunkName: "connectorsSpecs" */
+      '@kbn/connector-specs'
+    ),
+    import(
+      /* webpackChunkName: "formGenerator" */
+      '@kbn/response-ops-form-generator'
+    ),
+    import(
+      /* webpackChunkName: "generateSchema" */
+      './generate_schema'
+    ),
+  ]).then(([{ connectorsSpecs }, { generateFormFields }, { generateSchema }]) => {
     for (const spec of Object.values(connectorsSpecs)) {
-      connectorTypeRegistry.register(createConnectorTypeFromSpec(spec));
+      connectorTypeRegistry.register(
+        createConnectorTypeFromSpec(spec, generateFormFields, generateSchema)
+      );
     }
   });
 }
 
-const createConnectorTypeFromSpec = (spec: ConnectorSpec): ActionTypeModel => {
+const createConnectorTypeFromSpec = (
+  spec: ConnectorSpec,
+  generateFormFields: typeof import('@kbn/response-ops-form-generator').generateFormFields,
+  generateSchema: typeof import('./generate_schema').generateSchema
+): ActionTypeModel => {
   const schema = generateSchema(spec);
 
   return {
