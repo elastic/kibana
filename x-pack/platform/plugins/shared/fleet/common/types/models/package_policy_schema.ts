3d24aeef4604e78d3d2325e10a05c362863100b8
Sean Rathier <sean.rathier@elastic.co>
2025-11-26 20:27:18 -0500

    matrix working again

--- x-pack/platform/plugins/shared/fleet/common/types/models/package_policy_schema.ts
+++ x-pack/platform/plugins/shared/fleet/common/types/models/package_policy_schema.ts
@@ -147,6 +147,15 @@ export const PackagePolicyBaseSchema = {
       })
     )
   ),
+  cloud_connector_name: schema.maybe(
+    schema.nullable(
+      schema.string({
+        meta: {
+          description: 'Transient field for cloud connector name during creation.',
+        },
+      })
+    )
+  ),
   enabled: schema.boolean(),
   is_managed: schema.maybe(schema.boolean()),
   package: schema.maybe(PackagePolicyPackageSchema),
