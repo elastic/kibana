7d7fb9016075ab8766e59cbbfeee311ef6206928
Nikita Indik <nikita.indik@elastic.co>
2025-11-28 15:01:37 +0100

    More tests and request shape changes

--- x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/logic/prebuilt_rules/use_prebuilt_rules_install_review.ts
+++ x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/logic/prebuilt_rules/use_prebuilt_rules_install_review.ts
@@ -6,13 +6,19 @@
  */
 
 import type { UseQueryOptions } from '@kbn/react-query';
-import type {
-  ReviewRuleInstallationRequestBody,
-  ReviewRuleInstallationResponseBody,
-} from '../../../../../common/api/detection_engine/prebuilt_rules';
+import type { ReviewRuleInstallationResponseBody } from '../../../../../common/api/detection_engine/prebuilt_rules';
 import { useAppToasts } from '../../../../common/hooks/use_app_toasts';
 import * as i18n from '../translations';
 import { useFetchPrebuiltRulesInstallReviewQuery } from '../../api/hooks/prebuilt_rules/use_fetch_prebuilt_rules_install_review_query';
+import type { ReviewPrebuiltRuleInstallationSort } from '../../../../../common/api/detection_engine/prebuilt_rules/common/review_prebuilt_rules_installation_sort';
+import type { AddPrebuiltRulesTableFilterOptions } from '../../../rule_management_ui/components/rules_table/add_prebuilt_rules_table/use_filter_prebuilt_rules_to_install';
+
+interface UsePrebuiltRulesInstallReviewParams {
+  page: number;
+  perPage: number;
+  filterOptions: AddPrebuiltRulesTableFilterOptions;
+  sortingOptions: ReviewPrebuiltRuleInstallationSort;
+}
 
 /**
  * A wrapper around useQuery provides default values to the underlying query,
@@ -21,14 +27,31 @@ import { useFetchPrebuiltRulesInstallReviewQuery } from '../../api/hooks/prebuil
  * @returns useQuery result
  */
 export const usePrebuiltRulesInstallReview = (
-  request: ReviewRuleInstallationRequestBody,
+  requestParameters: UsePrebuiltRulesInstallReviewParams,
   options?: UseQueryOptions<ReviewRuleInstallationResponseBody>
 ) => {
   const { addError } = useAppToasts();
 
-  return useFetchPrebuiltRulesInstallReviewQuery(request, {
-    // TODO: Add better error message
-    onError: (error) => addError(error, { title: i18n.RULE_AND_TIMELINE_FETCH_FAILURE }),
-    ...options,
-  });
+  return useFetchPrebuiltRulesInstallReviewQuery(
+    {
+      page: requestParameters.page,
+      per_page: requestParameters.perPage,
+      filter: {
+        fields: {
+          name: {
+            include: [requestParameters.filterOptions.name],
+          },
+          tags: {
+            include: requestParameters.filterOptions.tags,
+          },
+        },
+      },
+      sort: requestParameters.sortingOptions,
+    },
+    {
+      // TODO: Add better error message
+      onError: (error) => addError(error, { title: i18n.RULE_AND_TIMELINE_FETCH_FAILURE }),
+      ...options,
+    }
+  );
 };
