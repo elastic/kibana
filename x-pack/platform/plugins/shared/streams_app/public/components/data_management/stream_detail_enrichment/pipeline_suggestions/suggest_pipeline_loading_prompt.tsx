9a4ae41d110aa5dc2b1b38479fd128b754a27543
Joe Reuter <johannes.reuter@elastic.co>
2025-11-28 13:13:29 +0100

    ui touches

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/pipeline_suggestions/suggest_pipeline_loading_prompt.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/pipeline_suggestions/suggest_pipeline_loading_prompt.tsx
@@ -13,6 +13,8 @@ import {
   EuiFlexItem,
   EuiProgress,
   EuiSkeletonText,
+  EuiCallOut,
+  EuiSpacer,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 
@@ -22,35 +24,32 @@ export interface SuggestPipelineLoadingPromptProps {
 
 export function SuggestPipelineLoadingPrompt({ onCancel }: SuggestPipelineLoadingPromptProps) {
   return (
-    <>
-      <EuiFlexItem grow={false}>
-        <EuiFlexGroup direction="column" responsive={false} gutterSize="m">
-          <EuiFlexItem>
-            <EuiText size="s" color="subdued">
-              {i18n.translate('xpack.streams.stepsEditor.loadingDashboardsLabel', {
-                defaultMessage: 'Analysing your data...',
-              })}
-            </EuiText>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiSkeletonText lines={3} />
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiSkeletonText lines={3} />
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiSkeletonText lines={3} />
-          </EuiFlexItem>
-          <EuiProgress size="xs" color="accent" />
-          <EuiFlexItem>
-            <EuiLink onClick={onCancel}>
-              {i18n.translate('xpack.streams.stepsEditor.cancelLabel', {
-                defaultMessage: 'Cancel',
-              })}
-            </EuiLink>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiFlexItem>
-    </>
+    <EuiCallOut
+      iconType="sparkles"
+      title={i18n.translate('xpack.streams.stepsEditor.loadingDashboardsLabel', {
+        defaultMessage: 'Analysing your data...',
+      })}
+      color="primary"
+    >
+      <EuiFlexGroup direction="column" responsive={false} gutterSize="m">
+        <EuiFlexItem>
+          <EuiSkeletonText lines={3} />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiSkeletonText lines={3} />
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <EuiSkeletonText lines={3} />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer size="s" />
+      <EuiProgress size="m" color="accent" />
+      <EuiSpacer size="s" />
+      <EuiLink onClick={onCancel}>
+        {i18n.translate('xpack.streams.stepsEditor.cancelLabel', {
+          defaultMessage: 'Cancel',
+        })}
+      </EuiLink>
+    </EuiCallOut>
   );
 }
