25588ad17428f7ec7eb9a2a56a8e5a5d5e7a8637
fake-haris <charis.kalpakis@elastic.co>
2025-11-28 12:00:27 +0200

    changed Download import from

--- src/platform/packages/shared/kbn-scout/src/playwright/page_objects/discover_app.ts
+++ src/platform/packages/shared/kbn-scout/src/playwright/page_objects/discover_app.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { Download } from 'playwright-core';
+import type { Download } from '@playwright/test';
 import type { Locator } from '../../..';
 import type { ScoutPage } from '..';
 import { expect } from '..';
