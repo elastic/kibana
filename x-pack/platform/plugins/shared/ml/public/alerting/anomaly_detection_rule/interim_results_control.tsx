2f09eea152ad8b9bc3a058c1712ca273a6578fa1
Robert Jaszczurek <robert.jaszczurek@elastic.co>
2025-11-28 16:21:41 +0100

    cleanup..

--- x-pack/platform/plugins/shared/ml/public/alerting/anomaly_detection_rule/interim_results_control.tsx
+++ x-pack/platform/plugins/shared/ml/public/alerting/anomaly_detection_rule/interim_results_control.tsx
@@ -7,47 +7,29 @@
 
 import type { FC } from 'react';
 import React from 'react';
-import { EuiFormRow, EuiSwitch, EuiToolTip } from '@elastic/eui';
+import { EuiFormRow, EuiSwitch } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
-import { i18n } from '@kbn/i18n';
 
 interface InterimResultsControlProps {
   value: boolean;
   onChange: (update: boolean) => void;
-  disabled?: boolean;
 }
 
 export const InterimResultsControl: FC<InterimResultsControlProps> = React.memo(
-  ({ value, onChange, disabled }) => {
-    const switchComponent = (
-      <EuiSwitch
-        data-test-subj="mlAnomalyAlertIncludeInterimSwitch"
-        label={
-          <FormattedMessage
-            id="xpack.ml.interimResultsControl.label"
-            defaultMessage="Include interim results"
-          />
-        }
-        checked={value ?? false}
-        onChange={onChange.bind(null, !value)}
-        disabled={disabled}
-      />
-    );
-
+  ({ value, onChange }) => {
     return (
       <EuiFormRow>
-        {disabled ? (
-          <EuiToolTip
-            content={i18n.translate('xpack.ml.interimResultsControl.disabledTooltip', {
-              defaultMessage:
-                'This control is disabled because the anomaly filter includes an is_interim condition.',
-            })}
-          >
-            {switchComponent}
-          </EuiToolTip>
-        ) : (
-          switchComponent
-        )}
+        <EuiSwitch
+          data-test-subj="mlAnomalyAlertIncludeInterimSwitch"
+          label={
+            <FormattedMessage
+              id="xpack.ml.interimResultsControl.label"
+              defaultMessage="Include interim results"
+            />
+          }
+          checked={value ?? false}
+          onChange={onChange.bind(null, !value)}
+        />
       </EuiFormRow>
     );
   }
