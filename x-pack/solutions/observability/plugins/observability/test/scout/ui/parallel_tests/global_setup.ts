633e069e615e2365e649933c6d1b37410dddfa4f
Samuel Brito <samuel.brito@elastic.co>
2025-11-28 15:38:09 +0000

    Added parallel functionality to rules tests

---
+++ x-pack/solutions/observability/plugins/observability/test/scout/ui/parallel_tests/global_setup.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { globalSetupHook } from '@kbn/scout-oblt';
+import { generateApmData, generateLogsData } from '../fixtures/generators';
+
+globalSetupHook(
+  'Ingest data to Elasticsearch',
+  { tag: ['@ess', '@svlOblt'] },
+  async ({ apmSynthtraceEsClient, logsSynthtraceEsClient, log }) => {
+    log.info('Generating Observability data...');
+    await generateApmData(apmSynthtraceEsClient);
+    await generateLogsData(logsSynthtraceEsClient);
+  }
+);
