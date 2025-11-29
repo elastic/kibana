42f61b7cde4c69a262dbc90bd728ca35001b92b3
Hannah Mudge <Heenawter@users.noreply.github.com>
2025-11-28 17:56:46 -0700

    Fix panel listing tests

--- src/platform/test/functional/apps/dashboard/group5/dashboard_panel_listing.ts
+++ src/platform/test/functional/apps/dashboard/group5/dashboard_panel_listing.ts
@@ -61,7 +61,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         panelGroupByOrder.set(order, panelGroupTitle);
       }
 
-      expect(panelGroupByOrder.size).to.eql(3);
+      expect(panelGroupByOrder.size).to.eql(4);
 
       expect([...panelGroupByOrder.values()]).to.eql([
         'visualizationsGroup',
@@ -71,7 +71,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
       ]);
 
       // Any changes to the number of panels needs to be audited by @elastic/kibana-presentation
-      expect(panelTypes.length).to.eql(11);
+      expect(panelTypes.length).to.eql(13);
     });
   });
 }
