bc96f24a6111b008466dea12c0de5df13048fe37
Faisal Kanout <faisal@kanout.com>
2025-12-01 09:36:26 +0100

    Update x-pack/platform/plugins/shared/alerting/server/alerts_service/alerts_service.ts
    Co-authored-by: Christos Nasikas <xristosnasikas@gmail.com>

--- x-pack/platform/plugins/shared/alerting/server/alerts_service/alerts_service.ts
+++ x-pack/platform/plugins/shared/alerting/server/alerts_service/alerts_service.ts
@@ -569,7 +569,7 @@ export class AlertsService implements IAlertsService {
       });
     } catch (error) {
       logger.error(
-        `Error updating muted field to ${muted} for query: ${JSON.stringify(query)} - ${
+        `Error updating muted field to ${muted} for rule ID ${ruleId} and alert instance ID ${alertInstanceId} - ${
           error.message
         }`
       );
