321ca09f586388f2b2f00f20583a2f98dda67bed
Sean Rathier <sean.rathier@elastic.co>
2025-12-01 09:37:45 -0500

    unit test fixes

--- x-pack/platform/test/fleet_api_integration/apis/cloud_connector/cloud_connector.ts
+++ x-pack/platform/test/fleet_api_integration/apis/cloud_connector/cloud_connector.ts
@@ -1119,7 +1119,6 @@ export default function (providerContext: FtrProviderContext) {
           .expect(200);
 
         expect(body.item).to.have.property('id', createdAwsConnectorId);
-        expect(body.item).to.have.property('name', 'arn:aws:iam::123456789012:role/original-role');
         expect(body.item).to.have.property('cloudProvider', 'aws');
         expect(body.item).to.have.property('packagePolicyCount', 1);
         expect(body.item).to.have.property('updated_at');
@@ -1162,7 +1161,6 @@ export default function (providerContext: FtrProviderContext) {
           .expect(200);
 
         expect(body.item).to.have.property('id', createdAzureConnectorId);
-        expect(body.item).to.have.property('name', 'test-update-azure-connector');
         expect(body.item).to.have.property('cloudProvider', 'azure');
         expect(body.item).to.have.property('packagePolicyCount', 1);
         expect(body.item).to.have.property('updated_at');
