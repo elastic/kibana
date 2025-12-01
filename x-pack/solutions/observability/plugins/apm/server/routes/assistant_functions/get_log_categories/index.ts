e78e28f71d2038ca80660b1e4dc99e819f06bf6e
Gonçalo Rica <goncalo.rica@elastic.co>
2025-12-01 15:18:16 +0100

    [APM] Refactor assistant functions to use accessKnownApmEventFields

--- x-pack/solutions/observability/plugins/apm/server/routes/assistant_functions/get_log_categories/index.ts
+++ x-pack/solutions/observability/plugins/apm/server/routes/assistant_functions/get_log_categories/index.ts
@@ -8,8 +8,7 @@
 import datemath from '@elastic/datemath';
 import type { ElasticsearchClient } from '@kbn/core-elasticsearch-server';
 import type { LogSourcesService } from '@kbn/logs-data-access-plugin/common/types';
-import { unflattenKnownApmEventFields } from '@kbn/apm-data-access-plugin/server/utils';
-import { getFlattenedKeyValuePairs } from '@kbn/key-value-metadata-table';
+import { accessKnownApmEventFields } from '@kbn/apm-data-access-plugin/server/utils';
 import type { KeyValuePair } from '@kbn/key-value-metadata-table';
 import { maybe } from '../../../../common/utils/maybe';
 import { asMutableArray } from '../../../../common/utils/as_mutable_array';
@@ -126,10 +125,10 @@ export async function getLogCategories({
   const promises = categorizedLogsRes.aggregations?.sampling.categories?.buckets.map(
     async ({ doc_count: docCount, key, sample }) => {
       const hit = sample.hits.hits[0];
-      const event = unflattenKnownApmEventFields(hit?.fields);
+      const event = accessKnownApmEventFields(hit.fields);
 
       const sampleMessage = event.message as string;
-      const sampleTraceId = event.trace?.id;
+      const sampleTraceId = event[TRACE_ID];
       const errorCategory = key as string;
 
       if (!sampleTraceId) {
@@ -147,12 +146,16 @@ export async function getLogCategories({
     }
   );
 
-  const event = unflattenKnownApmEventFields(maybe(categorizedLogsRes.hits.hits[0])?.fields);
+  const hits = maybe(categorizedLogsRes.hits.hits[0])?.fields;
 
-  const sampleDoc = event as Record<string, string>;
+  const event = hits && accessKnownApmEventFields(hits);
 
   return {
     logCategories: await Promise.all(promises ?? []),
-    entities: getFlattenedKeyValuePairs(sampleDoc),
+    entities: event
+      ? Object.keys(event)
+          .sort()
+          .map((key) => ({ key, value: event[key] }))
+      : [],
   };
 }
