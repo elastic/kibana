a4c833ab0f4e7f45238d74a7e18c7123b013bcf1
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2025-12-01 18:48:43 +0000

    [Security Solution][Detection Engine] skips processing gaps in auto backfill scheduler after number of retries reaches configured value

--- x-pack/platform/plugins/shared/alerting/server/task_runner/ad_hoc_task_runner.ts
+++ x-pack/platform/plugins/shared/alerting/server/task_runner/ad_hoc_task_runner.ts
@@ -75,6 +75,7 @@ export class AdHocTaskRunner implements CancellableTask {
 
   private adHocRunSchedule: AdHocRunSchedule[] = [];
   private adHocRange: { start: string; end: string | undefined } | null = null;
+  private adHocRunData: AdHocRun | null = null;
   private alertingEventLogger: AlertingEventLogger;
   private cancelled = false;
   private logger: Logger;
@@ -376,6 +377,7 @@ export class AdHocTaskRunner implements CancellableTask {
 
       const { rule, apiKeyToUse, schedule, start, end } = adHocRunData;
       this.apiKeyToUse = apiKeyToUse;
+      this.adHocRunData = adHocRunData;
 
       let ruleType: UntypedNormalizedRuleType;
       try {
@@ -685,6 +687,7 @@ export class AdHocTaskRunner implements CancellableTask {
       savedObjectsRepository: this.internalSavedObjectsRepository,
       backfillClient: this.context.backfillClient,
       actionsClient,
+      initiator: this.adHocRunData?.initiator,
     });
   }
 
