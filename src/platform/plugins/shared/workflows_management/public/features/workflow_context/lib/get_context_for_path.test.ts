554c33bf097b85c9f25e4560d6c30be22c64f0d8
Sergi Massaneda <sergi.massaneda@elastic.co>
2025-12-02 11:02:32 +0100

    fix tests

--- src/platform/plugins/shared/workflows_management/public/features/workflow_context/lib/get_context_for_path.test.ts
+++ src/platform/plugins/shared/workflows_management/public/features/workflow_context/lib/get_context_for_path.test.ts
@@ -14,6 +14,8 @@ import { z } from '@kbn/zod';
 import { getContextSchemaForPath } from './get_context_for_path';
 import { expectZodSchemaEqual } from '../../../../common/lib/zod/zod_utils';
 
+jest.mock('../../../../common/schema');
+
 describe('getContextSchemaForPath', () => {
   const definition = {
     version: '1' as const,
