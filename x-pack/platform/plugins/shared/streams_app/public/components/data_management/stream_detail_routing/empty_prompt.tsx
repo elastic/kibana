6cdd53311ca0c132fbc3aa88be4b936b7dcbf881
diogolima-elastic <diogo.lima@elastic.co>
2025-12-02 11:17:32 +0000

    change card to panel and image

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_routing/empty_prompt.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_routing/empty_prompt.tsx
@@ -13,14 +13,16 @@ import {
   useEuiTheme,
   EuiEmptyPrompt,
   EuiText,
-  EuiCard,
   EuiButton,
   EuiHorizontalRule,
+  EuiPanel,
+  EuiTitle,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { css } from '@emotion/css';
 import type { AIFeatures } from '../../../hooks/use_ai_features';
 import { GenerateSuggestionButton } from './review_suggestions_form/generate_suggestions_button';
+import { AssetImage } from '../../asset_image';
 
 interface NoDataEmptyPromptProps {
   aiFeatures: AIFeatures | null;
@@ -67,49 +69,55 @@ export const NoDataEmptyPrompt = ({
           </EuiFlexItem>
           <EuiSpacer size="m" />
           {aiFeatures && aiFeatures.enabled && (
-            <EuiCard
-              layout="horizontal"
-              display="transparent"
-              hasBorder
-              titleSize="s"
-              title={i18n.translate(
-                'xpack.streams.streamDetailView.routingTab.noDataEmptyPrompt.cardTitle',
-                {
-                  defaultMessage: 'Suggest partitioning',
-                }
-              )}
-              description={
-                <EuiFlexGroup direction="column" gutterSize="s" alignItems="flexStart">
-                  <EuiFlexItem grow={false}>
-                    <EuiText size="s">
-                      {i18n.translate(
-                        'xpack.streams.streamDetailView.routingTab.noDataEmptyPrompt.cardDescription',
-                        {
-                          defaultMessage:
-                            'Use the power of AI to generate the most effective partitioning',
-                        }
-                      )}
-                    </EuiText>
-                  </EuiFlexItem>
-                  <EuiFlexItem grow={false}>
-                    <GenerateSuggestionButton
-                      size="s"
-                      onClick={getSuggestionsForStream}
-                      isLoading={isLoadingSuggestions}
-                      isDisabled={isEditingOrReorderingStreams}
-                      aiFeatures={aiFeatures}
+            <EuiPanel color="transparent" hasBorder paddingSize="m">
+              <EuiFlexGroup responsive={false} gutterSize="m">
+                <EuiFlexItem>
+                  <EuiTitle size="xs">
+                    <h4
+                      className={css`
+                        color: ${euiTheme.colors.textHeading} !important;
+                      `}
                     >
                       {i18n.translate(
-                        'xpack.streams.streamDetailView.routingTab.noDataEmptyPrompt.cardButton',
+                        'xpack.streams.streamDetailView.routingTab.noDataEmptyPrompt.cardTitle',
                         {
-                          defaultMessage: 'Suggest partitions',
+                          defaultMessage: 'Suggest partitioning',
                         }
                       )}
-                    </GenerateSuggestionButton>
-                  </EuiFlexItem>
-                </EuiFlexGroup>
-              }
-            />
+                    </h4>
+                  </EuiTitle>
+                  <EuiText size="s">
+                    {i18n.translate(
+                      'xpack.streams.streamDetailView.routingTab.noDataEmptyPrompt.cardDescription',
+                      {
+                        defaultMessage:
+                          'Use the power of AI to generate the most effective partitioning',
+                      }
+                    )}
+                  </EuiText>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <AssetImage type="routingSuggestionEmptyState" size={75} />
+                </EuiFlexItem>
+              </EuiFlexGroup>
+              <EuiSpacer size="m" />
+              <EuiFlexItem grow={false}>
+                <GenerateSuggestionButton
+                  size="s"
+                  onClick={getSuggestionsForStream}
+                  isLoading={isLoadingSuggestions}
+                  isDisabled={isEditingOrReorderingStreams}
+                  aiFeatures={aiFeatures}
+                >
+                  {i18n.translate(
+                    'xpack.streams.streamDetailView.routingTab.noDataEmptyPrompt.cardButton',
+                    {
+                      defaultMessage: 'Suggest partitions',
+                    }
+                  )}
+                </GenerateSuggestionButton>
+              </EuiFlexItem>
+            </EuiPanel>
           )}
           <EuiSpacer size="s" />
           <EuiFlexItem>
