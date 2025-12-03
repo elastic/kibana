ad3707adb7ca92f68b0287576b5e03aded541497
Gergő Ábrahám <gergo.abraham@elastic.co>
2025-12-03 14:50:55 +0100

    [api/test] add initial api test for import

---
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/artifacts/trial_license_complete_tier/configs/artifact_import_ff.serverless.config.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { FtrConfigProviderContext } from '@kbn/test';
+import type { ExperimentalFeatures as SecuritySolutionExperimentalFeatures } from '@kbn/security-solution-plugin/common';
+
+/** This is a TEMPORARY config file created to perform Endpoint Exceptions related API tests with
+ * the feature flag `endpointArtifactsExportImportEnabled` enabled.
+ */
+export default async function ({ readConfigFile }: FtrConfigProviderContext) {
+  const functionalConfig = await readConfigFile(
+    require.resolve('../../../../../config/serverless/config.base.edr_workflows')
+  );
+  const securitySolutionEnableExperimental: Array<keyof SecuritySolutionExperimentalFeatures> = [
+    'trustedAppsAdvancedMode',
+    'filterProcessDescendantsForTrustedAppsEnabled',
+    'trustedDevices',
+    'endpointArtifactsExportImportEnabled',
+  ];
+
+  return {
+    ...functionalConfig.getAll(),
+    testFiles: [require.resolve('../artifact_import.ts')],
+    junit: {
+      reportName: 'EDR Workflows - Artifact Import Integration Tests - Serverless Env - Complete',
+    },
+    kbnTestServer: {
+      ...functionalConfig.get('kbnTestServer'),
+      serverArgs: [
+        ...functionalConfig.get('kbnTestServer.serverArgs').filter(
+          // Exclude Security solution experimental features
+          // properties since we are overriding them here
+          (arg: string) => !arg.includes('xpack.securitySolution.enableExperimental')
+        ),
+        // SECURITY SOLUTION: set any experimental feature flags for testing
+        `--xpack.securitySolution.enableExperimental=${JSON.stringify(
+          securitySolutionEnableExperimental
+        )}`,
+      ],
+    },
+  };
+}
