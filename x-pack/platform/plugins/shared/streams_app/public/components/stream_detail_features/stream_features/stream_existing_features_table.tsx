8865b012bc1ccd83e7ea6de092ab2617136710f6
klacabane <kevin.lacabane@elastic.co>
2025-12-04 14:58:55 +0100

    create dedicated component

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_features/stream_features/stream_existing_features_table.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_features/stream_features/stream_existing_features_table.tsx
@@ -28,7 +28,7 @@ import {
 import { useStreamsAppRouter } from '../../../hooks/use_streams_app_router';
 import { useStreamFeaturesApi } from '../../../hooks/use_stream_features_api';
 import { StreamFeatureDetailsFlyout } from './stream_feature_details_flyout';
-import { FeatureEventsSparkline } from './feature_events_sparkline';
+import { FeatureEventsSparklineLast24hrs } from './feature_events_sparkline';
 import { TableTitle } from './table_title';
 import { useStreamFeaturesTable } from './hooks/use_stream_features_table';
 
@@ -87,18 +87,7 @@ export function StreamExistingFeaturesTable({
       width: '15%',
       render: (feature: Feature) => {
         if (isFeatureWithFilter(feature)) {
-          const now = Date.now();
-          return (
-            <FeatureEventsSparkline
-              feature={feature}
-              definition={definition}
-              timeRange={{
-                from: new Date(now - 24 * 60 * 60 * 1000).toISOString(),
-                to: new Date(now).toISOString(),
-                mode: 'absolute',
-              }}
-            />
-          );
+          return <FeatureEventsSparklineLast24hrs feature={feature} definition={definition} />;
         }
       },
     },
