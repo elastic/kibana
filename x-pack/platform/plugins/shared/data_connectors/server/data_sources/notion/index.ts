d4ad16c754b67d0a1ff14bf7a46dffb6fcd88e17
Lorena Bălan <lorena.balan@elastic.co>
2025-12-04 19:59:57 +0100

    [Workplace AI] Use `dataSourcesRegistry` plugin to register various data types for data connectors (#244963)
    ## Summary
    
    Taken out of https://github.com/mattnowzari/kibana/pull/27/files
    Related to https://github.com/elastic/search-team/issues/11321
    
    Formula:
    1 data connector = 1 Kibana Stack Connector (KSC) + workflows + Agent
    Builder (AB) tools
    
    We have 2 plugins:
    * `dataSourcesRegistry` _(dependency of `dataConnectors` plugin)_ -
    where the declarative "recipe" for a data connector is defined: here is
    where you can find the workflows yaml, here is what type of ksc is
    needed, etc.
    * `dataConnectors` _(dependency of `workplaceai` solution)_ - this is
    the orchestrator that actually triggers the **instantiation** of the
    KSC, workflows, and ABC tools, based on the recipe of the type in its
    dataSourcesRegistry
    
    If a team in Observability wants to create a new data connector type of
    their own `LogsConnector` ("recipe"), they would pull in
    `dataSourcesRegistry` as a dependency, define `LogsConnector:
    DataTypeDefinition` in their part of the codebase in Kibana, and at
    `setup` time of their plugin register their "recipe" with the
    `dataSourcesRegistry`. Then they would appear in WorkplaceAI for free.
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [x] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- x-pack/platform/plugins/shared/data_connectors/server/data_sources/notion/index.ts
+++ x-pack/platform/plugins/shared/data_connectors/server/data_sources/notion/index.ts
@@ -4,7 +4,4 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-export type { DataCatalog } from './data_catalog';
-export type { DataTypeDefinition } from './data_type';
-export { createDataCatalog } from './data_catalog';
+export { notionDataSource } from './data_type';
