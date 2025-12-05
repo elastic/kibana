8860e4bbdbf189b63be8104a55b9c390bd4fd0a1
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2025-12-05 08:51:33 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/attachments/attachments.ts
+++ x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/attachments/attachments.ts
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 import { OBSERVABILITY_STREAMS_ENABLE_ATTACHMENTS } from '@kbn/management-settings-ids';
 import {
   disableStreams,
