c3d7e778fdf2fb9df4c7cd420711cdf6272ff990
Dario Gieselaar <dario.gieselaar@elastic.co>
2025-12-07 10:40:26 +0100

    Remove duplicate license header

--- x-pack/platform/packages/shared/kbn-ink/src/prepare_ink.ts
+++ x-pack/platform/packages/shared/kbn-ink/src/prepare_ink.ts
@@ -4,12 +4,6 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
 import Module from 'node:module';
 
 // eval import() to prevent babel from translating it to a CJS require,
