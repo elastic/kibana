b08206faa9b9548ed71d3fa6f326628b6794ddc9
Angela Chuang <yi-chun.chuang@elastic.co>
2025-12-08 11:30:08 +0000

    remove unused

--- x-pack/solutions/security/plugins/security_solution/public/siem_migrations/common/components/migration_steps/types.ts
+++ x-pack/solutions/security/plugins/security_solution/public/siem_migrations/common/components/migration_steps/types.ts
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import type { SiemMigrationResourceData } from '../../../../../common/siem_migrations/model/common.gen';
 
 export type UploadedLookups = Record<string, string>;
