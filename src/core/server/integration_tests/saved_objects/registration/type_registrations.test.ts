3ceac5cea9bb59c9bb9d8efbd98bc0bfe5a9f049
Kevin Delemme <kdelemme@gmail.com>
2025-12-09 14:30:23 -0500

    Add slo_template saved object

--- src/core/server/integration_tests/saved_objects/registration/type_registrations.test.ts
+++ src/core/server/integration_tests/saved_objects/registration/type_registrations.test.ts
@@ -149,6 +149,7 @@ const previouslyRegisteredTypes = [
   'siem-ui-timeline-pinned-event',
   'slo',
   'slo-settings',
+  'slo_template',
   'space',
   'spaces-usage-stats',
   'synthetics-monitor',
