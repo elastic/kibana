fcbfeb87657a75bcc1c117c014f0a05912b33266
Julia Bardi <julia.bardi@elastic.co>
2025-12-09 13:04:51 +0100

    remove newlines and fix test

--- x-pack/solutions/observability/test/api_integration_deployment_agnostic/apis/ai_assistant/complete/functions/context.spec.ts
+++ x-pack/solutions/observability/test/api_integration_deployment_agnostic/apis/ai_assistant/complete/functions/context.spec.ts
@@ -206,7 +206,9 @@ export default function ApiTest({ getService }: DeploymentAgnosticFtrProviderCon
           const extractedDocs = await getDocumentsToScore();
           const expectedTexts = sampleDocsForInternalKb.map((doc) => doc.text).sort();
           const actualTexts = extractedDocs.map((doc) => doc.text).sort();
-          expect(actualTexts).to.eql(expectedTexts);
+          expectedTexts.forEach((text) => {
+            expect(actualTexts).to.contain(text);
+          });
         });
       });
 
