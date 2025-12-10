9409f30cec0a0a7f34e35da468920ad6dfa08b3a
Shahzad <shahzad31comp@gmail.com>
2025-12-10 10:22:26 +0100

    [Synthetics] Only update relevant space monitors where global params changes happens !! (#241715)
    ## Summary
    
    Fixes https://github.com/elastic/kibana/issues/241433
    Fixes https://github.com/elastic/kibana/issues/241432
    
    Only update the monitors in which params changes have been made, this is
    done via a one time task which gets triggered when some changes to
    global params to happens.
    
    When some modification to params happens, we get `spaces` and pass that
    to the async task `paramsSpacesToSync`, this is a no-recurring task,
    which only runs once. The task uses the `paramsSpacesToSync` value from
    state and propagate global params values to monitors.
    
    Global params triggers from the existing recurring task have been
    removed.
    
    When params are updated, we call following API, since we aren't
    providing schedule, this will create one time instance of the task.
    paramsSpacesToSync is spaceIds of the params.
    
    ```
    
      await taskManager.ensureScheduled({
        id: `${TASK_TYPE}:${uuidv4()}`,
        params: {},
        taskType: TASK_TYPE,
        runAt: new Date(Date.now() + 3 * 1000),
        state: { paramsSpacesToSync },
      });
    ```
    
    ### Testing
    Try adding/editing/deleting params in any space and it should task debug
    logs state, to enable logs
    
    ```
    logging.loggers:
      - name: plugins.synthetics
        level: debug
        appenders: [console]
    ```
    `paramsSpacesToSync` value will get logged as debug log via
    
    ` this.debugLog(`current task state is
    ${JSON.stringify(taskInstance.state)}`);
    `
    
    Also following log will indicate correctly logged spaces of monitors
    
    ```
          this.debugLog(
            `Starting sync of private location monitors for spaces: ${Array.from(monitorSpaceIds).join(
              ', '
            )}`
          );
    ```
    
    ### Code changes
    A new file deploy_private_location_monitors.ts has been added with
    `DeployPrivateLocationMonitors` which will handle updating package
    policies in relevant spaces
    
    A new task file has been added with task
    Synthetics:Sync-Global-Params-Private-Locations
    
    ---------
    
    Co-authored-by: Francesco Fagnani <fagnani.francesco@gmail.com>
    (cherry picked from commit c035cc327abe373ff5211111dbafd370ec8f9ab7)
    
    # Conflicts:
    #	x-pack/solutions/observability/plugins/synthetics/server/plugin.ts
    #	x-pack/solutions/observability/plugins/synthetics/server/routes/settings/params/add_param.ts
    #	x-pack/solutions/observability/plugins/synthetics/server/routes/settings/params/delete_param.ts
    #	x-pack/solutions/observability/plugins/synthetics/server/routes/settings/params/delete_params_bulk.ts
    #	x-pack/solutions/observability/plugins/synthetics/server/tasks/sync_private_locations_monitors_task.ts

---
+++ x-pack/solutions/observability/plugins/synthetics/server/tasks/clean_up_duplicate_policies.ts
@@ -0,0 +1,121 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import type { SavedObjectsClientContract } from '@kbn/core-saved-objects-api-server';
+import { syntheticsMonitorSOTypes } from '../../common/types/saved_objects';
+import type { EncryptedSyntheticsMonitorAttributes } from '../../common/runtime_types';
+import { SyntheticsPrivateLocation } from '../synthetics_service/private_location/synthetics_private_location';
+import { getFilterForTestNowRun } from '../synthetics_service/private_location/clean_up_task';
+import type { SyncTaskState } from './sync_private_locations_monitors_task';
+import type { SyntheticsServerSetup } from '../types';
+
+export async function cleanUpDuplicatedPackagePolicies(
+  serverSetup: SyntheticsServerSetup,
+  soClient: SavedObjectsClientContract,
+  taskState: SyncTaskState
+) {
+  let performCleanupSync = false;
+  const { fleet } = serverSetup.pluginsStart;
+  const { logger } = serverSetup;
+
+  const debugLog = (msg: string) => {
+    logger.debug(`[PrivateLocationCleanUpTask] ${msg}`);
+  };
+
+  if (taskState.hasAlreadyDoneCleanup) {
+    debugLog('Skipping cleanup of duplicated package policies as it has already been done once');
+    return { performCleanupSync };
+  } else if (taskState.maxCleanUpRetries <= 0) {
+    debugLog('Skipping cleanup of duplicated package policies as max retries have been reached');
+    taskState.hasAlreadyDoneCleanup = true;
+    taskState.maxCleanUpRetries = 3;
+    return { performCleanupSync };
+  }
+  debugLog('Starting cleanup of duplicated package policies');
+
+  try {
+    const esClient = serverSetup.coreStart?.elasticsearch?.client.asInternalUser;
+
+    const finder = soClient.createPointInTimeFinder<EncryptedSyntheticsMonitorAttributes>({
+      type: syntheticsMonitorSOTypes,
+      fields: ['id', 'name', 'locations', 'origin'],
+      namespaces: ['*'],
+    });
+
+    const privateLocationAPI = new SyntheticsPrivateLocation(serverSetup);
+
+    const expectedPackagePolicies = new Set<string>();
+    for await (const result of finder.find()) {
+      result.saved_objects.forEach((monitor) => {
+        monitor.attributes.locations?.forEach((location) => {
+          const spaceId = monitor.namespaces?.[0];
+          if (!location.isServiceManaged && spaceId) {
+            const policyId = privateLocationAPI.getPolicyId(
+              {
+                origin: monitor.attributes.origin,
+                id: monitor.id,
+              },
+              location.id,
+              spaceId
+            );
+            expectedPackagePolicies.add(policyId);
+          }
+        });
+      });
+    }
+
+    finder.close().catch(() => {});
+
+    const packagePoliciesKuery = getFilterForTestNowRun(true);
+
+    const policiesIterator = await fleet.packagePolicyService.fetchAllItemIds(soClient, {
+      kuery: packagePoliciesKuery,
+      spaceIds: ['*'],
+      perPage: 100,
+    });
+    const packagePoliciesToDelete: string[] = [];
+
+    for await (const packagePoliciesIds of policiesIterator) {
+      for (const packagePolicyId of packagePoliciesIds) {
+        if (!expectedPackagePolicies.has(packagePolicyId)) {
+          packagePoliciesToDelete.push(packagePolicyId);
+        }
+        // remove it from the set to mark it as found
+        expectedPackagePolicies.delete(packagePolicyId);
+      }
+    }
+
+    // if we have any to delete or any expected that were not found we need to perform a sync
+    performCleanupSync = packagePoliciesToDelete.length > 0 || expectedPackagePolicies.size > 0;
+
+    if (packagePoliciesToDelete.length > 0) {
+      logger.info(
+        ` [PrivateLocationCleanUpTask] Found ${
+          packagePoliciesToDelete.length
+        } duplicate package policies to delete: ${packagePoliciesToDelete.join(', ')}`
+      );
+      await fleet.packagePolicyService.delete(soClient, esClient, packagePoliciesToDelete, {
+        force: true,
+        spaceIds: ['*'],
+      });
+    }
+    taskState.hasAlreadyDoneCleanup = true;
+    taskState.maxCleanUpRetries = 3;
+    return { performCleanupSync };
+  } catch (e) {
+    taskState.maxCleanUpRetries -= 1;
+    if (taskState.maxCleanUpRetries <= 0) {
+      debugLog('Skipping cleanup of duplicated package policies as max retries have been reached');
+      taskState.hasAlreadyDoneCleanup = true;
+      taskState.maxCleanUpRetries = 3;
+    }
+    logger.error(
+      '[SyncPrivateLocationMonitorsTask] Error cleaning up duplicated package policies',
+      { error: e }
+    );
+    return { performCleanupSync };
+  }
+}
