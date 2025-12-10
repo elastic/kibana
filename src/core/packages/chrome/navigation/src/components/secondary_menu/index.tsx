142aa1f0d887c24688df994ca5fa421f1fc3e26c
Timothy Sullivan <tsullivan@elastic.co>
2025-12-09 17:36:44 -0700

    fix panel title background style

--- src/core/packages/chrome/navigation/src/components/secondary_menu/index.tsx
+++ src/core/packages/chrome/navigation/src/components/secondary_menu/index.tsx
@@ -32,7 +32,7 @@ interface SecondaryMenuComponent
 }
 
 const SecondaryMenuBase = forwardRef<HTMLDivElement, SecondaryMenuProps>(
-  ({ badgeType, children, isPanel = false, title }, ref) => {
+  ({ badgeType, children, title }, ref) => {
     const { euiTheme } = useEuiTheme();
     const headerStyle = useMenuHeaderStyle();
 
@@ -44,7 +44,7 @@ const SecondaryMenuBase = forwardRef<HTMLDivElement, SecondaryMenuProps>(
 
     const titleStyles = css`
       ${headerStyle}
-      background: ${isPanel ? 'transparent' : euiTheme.colors.backgroundBasePlain};
+      background: ${euiTheme.colors.backgroundBasePlain};
       border-radius: ${euiTheme.border.radius.medium};
     `;
 
