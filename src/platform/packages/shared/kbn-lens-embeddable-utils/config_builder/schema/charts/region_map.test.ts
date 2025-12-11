42de0ac55e67ba3a1a4dd0078e7d8bddcf75381b
Andreana Malama <andreana.malama@elastic.co>
2025-12-11 16:18:02 +0200

    Remove duplicated license comment

--- src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/schema/charts/region_map.test.ts
+++ src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/schema/charts/region_map.test.ts
@@ -6,14 +6,6 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the "Elastic License
- * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
- * Public License v 1"; you may not use this file except in compliance with, at
- * your election, the "Elastic License 2.0", the "GNU Affero General Public
- * License v3.0 only", or the "Server Side Public License, v 1".
- */
 
 import { LENS_EMPTY_AS_NULL_DEFAULT_VALUE } from '../../transforms/columns/utils';
 import type { RegionMapState } from './region_map';
