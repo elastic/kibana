75a30c1de4ddbc037cfb9df7b022b88546d31047
Konrad Szwarc <konrad.szwarc@elastic.co>
2025-12-11 16:46:49 +0100

    [EDR Workflows] Update endpoint management icons with dark mode support (#245355)
    This PR updates the icons used on the Security > Manage page for
    endpoint functionalities including blocklist, endpoints, policies,
    trusted applications, and other endpoint management features. Both light
    and dark mode variants are now supported.
    
    <img width="1596" height="1283" alt="Screenshot 2025-12-10 at 09 58 17"
    src="https://github.com/user-attachments/assets/bc483413-abeb-4b4d-b710-e9258dd6f5ac"
    />
    
    <img width="1596" height="1284" alt="Screenshot 2025-12-05 at 10 29 10"
    src="https://github.com/user-attachments/assets/aaaaec2e-2f07-4123-9cad-21fe682d3cdf"
    />

--- x-pack/solutions/security/plugins/security_solution/public/common/icons/endpoint_exceptions.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/common/icons/endpoint_exceptions.tsx
@@ -8,30 +8,24 @@ import type { SVGProps } from 'react';
 import React from 'react';
 import { useKibanaIsDarkMode } from '@kbn/react-kibana-context-theme';
 
-export const IconEndpoints: React.FC<SVGProps<SVGSVGElement>> = ({ ...props }) => {
+export const IconEndpointExceptions: React.FC<SVGProps<SVGSVGElement>> = ({ ...props }) => {
   const isDarkMode = useKibanaIsDarkMode();
 
   const light = (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 32 32"
-      width={16}
-      height={16}
-      {...props}
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" width={16} height={16} viewBox="0 0 32 32" {...props}>
       <path
         fill="#101c3f"
-        fillRule="evenodd"
-        d="m7.745 2.856 7.403 13.147-.277.49-7.126 12.655C5.896 32.43 1 31.078 1 27.312V4.693C1 .927 5.896-.425 7.745 2.856M3 27.31c0 1.727 2.173 2.327 3.002.854L12.852 16 6.002 3.835C5.173 2.363 3 2.964 3 4.69zM10.012 31l7.675-14h12.795l-.893 1.51-5.686 9.608A6.26 6.26 0 0 1 18.623 31zM22.2 27.07 26.975 19h-8.104L13.39 29h5.235c1.46 0 2.816-.74 3.575-1.93"
-        clipRule="evenodd"
+        d="M1 4.693C1 .928 5.896-.425 7.745 2.856l7.403 13.147-.277.49-7.126 12.655C5.897 32.43 1 31.078 1 27.313zm5.002-.857C5.172 2.364 3 2.964 3 4.69v22.62c0 1.727 2.173 2.326 3.002.854L12.852 16z"
       />
       <path
         fill="#0b64dd"
-        fillRule="evenodd"
-        d="M17.137 14 10.012 1h8.612a6.26 6.26 0 0 1 5.28 2.882L30.323 14h-2.369l-5.74-9.044A4.26 4.26 0 0 0 18.624 3h-5.235l6.029 11z"
-        clipRule="evenodd"
+        d="M18.624 1a6.26 6.26 0 0 1 5.28 2.882L30.323 14h-2.368l-5.74-9.044A4.26 4.26 0 0 0 18.624 3h-5.235l6.029 11h-2.28L10.011 1z"
       />
+      <path
+        fill="#101c3f"
+        d="M29 24a6 6 0 1 0-6 6v2a8 8 0 1 1 0-16 8 8 0 0 1 0 16v-2a6 6 0 0 0 6-6"
+      />
+      <path fill="#101c3f" d="M19 23h8v2h-8z" />
     </svg>
   );
 
@@ -52,10 +46,8 @@ export const IconEndpoints: React.FC<SVGProps<SVGSVGElement>> = ({ ...props }) =
         fill="#48efcf"
         d="M18.624 1a6.26 6.26 0 0 1 5.279 2.882L30.323 14h-2.367l-5.74-9.044A4.26 4.26 0 0 0 18.624 3h-5.235l6.029 11h-2.28L10.011 1z"
       />
-      <path
-        fill="#fff"
-        d="m30.482 17-.893 1.509-5.686 9.61A6.26 6.26 0 0 1 18.623 31h-8.61l7.674-14zM18.87 19l-5.482 10h5.235c1.46 0 2.816-.74 3.575-1.929L26.975 19z"
-      />
+      <path fill="#fff" d="M29 24a6 6 0 1 0-6 6v2a8 8 0 1 1 0-16 8 8 0 0 1 0 16v-2a6 6 0 0 0 6-6" />
+      <path fill="#101c3f" stroke="#fff" d="M19.5 23.5h7v1h-7z" />
     </svg>
   );
 
