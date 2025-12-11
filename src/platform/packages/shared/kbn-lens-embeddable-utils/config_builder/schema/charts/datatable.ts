7f3fd1cb81745613fe29a135c785c555ba283e10
Andreana Malama <andreana.malama@elastic.co>
2025-12-11 13:40:33 +0200

    Add datatable schema

---
+++ src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/schema/charts/datatable.ts
@@ -0,0 +1,280 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import type { TypeOf } from '@kbn/config-schema';
+import { schema } from '@kbn/config-schema';
+import { esqlColumnSchema, genericOperationOptionsSchema } from '../metric_ops';
+import { bucketOperationDefinitionSchema } from '../bucket_ops';
+import { applyColorToSchema, colorByValueSchema, colorMappingSchema } from '../color';
+import { datasetSchema, datasetEsqlTableSchema } from '../dataset';
+import {
+  collapseBySchema,
+  dslOnlyPanelInfoSchema,
+  layerSettingsSchema,
+  sharedPanelInfoSchema,
+} from '../shared';
+import {
+  mergeAllMetricsWithChartDimensionSchemaWithRefBasedOps,
+  mergeAllBucketsWithChartDimensionSchema,
+} from './shared';
+import { horizontalAlignmentSchema } from '../alignments';
+
+const datatableStateSharedOptionsSchema = {
+  /**
+   * Density  configuration
+   */
+  density: schema.maybe(
+    schema.object(
+      {
+        /**
+         * Density mode
+         */
+        mode: schema.maybe(
+          schema.oneOf(
+            [schema.literal('compact'), schema.literal('normal'), schema.literal('expanded')],
+            {
+              defaultValue: 'normal',
+              meta: { description: 'Density mode' },
+            }
+          )
+        ),
+        /**
+         * Height configuration
+         */
+        height: schema.maybe(
+          schema.object({
+            header: schema.maybe(
+              schema.oneOf(
+                [
+                  schema.object({ type: schema.literal('auto') }),
+                  schema.object({
+                    type: schema.literal('custom'),
+                    max_lines: schema.number({ defaultValue: 3, min: 1, max: 5 }),
+                  }),
+                ],
+                {
+                  meta: {
+                    description: 'Maximum number of lines to use before header is truncated',
+                  },
+                }
+              )
+            ),
+            value: schema.maybe(
+              schema.oneOf(
+                [
+                  schema.object({ type: schema.literal('auto') }),
+                  schema.object({
+                    type: schema.literal('custom'),
+                    lines: schema.number({ defaultValue: 1, min: 1, max: 20 }),
+                  }),
+                ],
+                {
+                  meta: {
+                    description: 'Number of lines to display per table body cell',
+                  },
+                }
+              )
+            ),
+          })
+        ),
+      },
+      {
+        meta: {
+          description: 'Density configuration for the datatable',
+        },
+      }
+    )
+  ),
+  /**
+   * Paging configuration
+   */
+  paging: schema.maybe(
+    schema.oneOf(
+      [schema.literal(10), schema.literal(20), schema.literal(50), schema.literal(100)],
+      {
+        meta: {
+          description: 'Enables pagination and sets the number of rows to display per page',
+        },
+      }
+    )
+  ),
+};
+
+const datatableStateCommonOptionsSchema = {
+  /**
+   * Where to apply the color (background or value)
+   */
+  apply_color_to: schema.maybe(applyColorToSchema),
+  /**
+   * Whether to show the row
+   */
+  visible: schema.maybe(schema.boolean({ defaultValue: true })),
+};
+
+const datatableStateRowsOptionsSchema = schema.object({
+  ...datatableStateCommonOptionsSchema,
+  /**
+   * Alignment of the rows
+   */
+  alignment: schema.maybe(
+    horizontalAlignmentSchema({
+      defaultValue: 'left',
+      meta: { description: 'Alignment of the rows' },
+    })
+  ),
+  /**
+   * Color configuration
+   */
+  color: schema.maybe(colorMappingSchema),
+  /**
+   * Whether to enable the one click filter
+   */
+  click_filter: schema.maybe(
+    schema.boolean({
+      defaultValue: false,
+      meta: { description: 'Whether to enable the one click filter' },
+    })
+  ),
+  /**
+   * Collapse by function. This parameter is used to collapse the
+   * metric chart when the number of columns is bigger than the
+   * number of columns specified in the columns parameter.
+   * Possible values:
+   * - 'avg': Collapse by average
+   * - 'sum': Collapse by sum
+   * - 'max': Collapse by max
+   * - 'min': Collapse by min
+   * - 'none': Do not collapse
+   */
+  collapse_by: schema.maybe(collapseBySchema),
+});
+
+const datatableStateColumnsOptionsSchema = schema.object({
+  ...datatableStateCommonOptionsSchema,
+  /**
+   * Color configuration
+   */
+  color: schema.maybe(colorByValueSchema),
+  /**
+   * Alignment of the columns
+   */
+  alignment: schema.maybe(
+    horizontalAlignmentSchema({
+      defaultValue: 'right',
+      meta: { description: 'Alignment of the columns' },
+    })
+  ),
+  /**
+   * Summary configuration
+   */
+  summary: schema.maybe(
+    schema.oneOf(
+      [
+        schema.literal('none'),
+        schema.literal('sum'),
+        schema.literal('avg'),
+        schema.literal('count'),
+        schema.literal('min'),
+        schema.literal('max'),
+      ],
+      { defaultValue: 'none', meta: { description: 'Summary function to apply to the column' } }
+    )
+  ),
+});
+
+export const datatableStateSchemaNoESQL = schema.object({
+  type: schema.literal('datatable'),
+  ...sharedPanelInfoSchema,
+  ...dslOnlyPanelInfoSchema,
+  ...layerSettingsSchema,
+  ...datasetSchema,
+  ...datatableStateSharedOptionsSchema,
+  /**
+   * Metric columnsconfiguration, must define operation.
+   */
+  columns: schema.arrayOf(
+    mergeAllMetricsWithChartDimensionSchemaWithRefBasedOps(datatableStateColumnsOptionsSchema),
+    { minSize: 1 }
+  ),
+  /**
+   * Row configuration, optional bucket operations.
+   */
+  rows: schema.maybe(
+    schema.arrayOf(mergeAllBucketsWithChartDimensionSchema(datatableStateRowsOptionsSchema), {
+      minSize: 1,
+    })
+  ),
+  /**
+   * Split columns by configuration, optional bucket operations.
+   */
+  split_columns_by: schema.maybe(
+    schema.arrayOf(bucketOperationDefinitionSchema, {
+      minSize: 1,
+    })
+  ),
+});
+
+export const datatableStateSchemaESQL = schema.object({
+  type: schema.literal('datatable'),
+  ...sharedPanelInfoSchema,
+  ...layerSettingsSchema,
+  ...datasetEsqlTableSchema,
+  ...datatableStateSharedOptionsSchema,
+  /**
+   * Metric columnsconfiguration, must define operation.
+   */
+  columns: schema.arrayOf(
+    schema.allOf([
+      schema.object({
+        ...genericOperationOptionsSchema,
+      }),
+      datatableStateColumnsOptionsSchema,
+      esqlColumnSchema,
+    ]),
+    { minSize: 1 }
+  ),
+  /**
+   * Row configuration, optional operations.
+   */
+  rows: schema.maybe(
+    schema.arrayOf(
+      schema.allOf([
+        schema.object({
+          ...genericOperationOptionsSchema,
+        }),
+        datatableStateRowsOptionsSchema,
+        esqlColumnSchema,
+      ]),
+      { minSize: 1 }
+    )
+  ),
+  /**
+   * Split columns by configuration, optional operations.
+   */
+  split_columns_by: schema.maybe(
+    schema.arrayOf(
+      schema.allOf([
+        schema.object({
+          ...genericOperationOptionsSchema,
+        }),
+        esqlColumnSchema,
+      ]),
+      { minSize: 1 }
+    )
+  ),
+});
+
+export const datatableStateSchema = schema.oneOf([
+  datatableStateSchemaNoESQL,
+  datatableStateSchemaESQL,
+]);
+
+export type DatatableState = TypeOf<typeof datatableStateSchema>;
+export type DatatableStateNoESQL = TypeOf<typeof datatableStateSchemaNoESQL>;
+export type DatatableStateESQL = TypeOf<typeof datatableStateSchemaESQL>;
