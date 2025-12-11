1f5b9bb2735649ac6a61137bab42f0663be26498
Ihor Panasiuk <igorskynet13@gmail.com>
2025-12-11 16:44:27 +0100

    Update included_operations.ts

--- src/platform/packages/shared/kbn-workflows/scripts/generate_es_connectors/included_operations.ts
+++ src/platform/packages/shared/kbn-workflows/scripts/generate_es_connectors/included_operations.ts
@@ -7,17 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-// export const INCLUDED_OPERATIONS_V2 = {
-//   '/{index}/_doc/{id}': new Set(['PUT', 'GET', 'DELETE', 'HEAD']),
-//   '/{index}/_doc': new Set(['POST']),
-//   '/{index}/_create/{id}': new Set(['POST', 'PUT']),
-//   '/{index}/_update/{id}': new Set(['POST']),
-//   '/_search': new Set(['POST']),
-//   '/{index}/_search': new Set(['POST']),
-//   '/_count': new Set(['POST']),
-//   '/{index}/_count': new Set(['POST']),
-// } as Record<string, Set<string>>;
-
 export const INCLUDED_OPERATIONS = [
   'search',
   'update',
