f2e782999c64ca78f2b388e59257168b08990a9e
Nikita Indik <nikita.indik@elastic.co>
2025-12-12 13:14:21 +0100

    Add CI checks back, remove k6 files from the repo

--- k6-performance-test.run.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# This is how we run it
-docker compose -f k6-performance-test.docker-compose.yml up
-
-# Or if we just want dockerised kibana on http://localhost:5601
-# docker compose -f k6-performance-test.docker-compose.yml run kibana
