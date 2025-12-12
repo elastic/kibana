523b35a91af41c41beffd62ecdfc1c753c169d3d
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2025-12-12 13:01:33 +0100

    Integrate new app menu with dashboards

--- src/platform/plugins/shared/dashboard/public/dashboard_app/hooks/dashboard_mount_context.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_app/hooks/dashboard_mount_context.ts
@@ -19,6 +19,7 @@ export const DashboardMountContext = createContext<DashboardMountContextProps>({
   scopedHistory: () => ({} as ScopedHistory),
   onAppLeave: (handler) => {},
   setHeaderActionMenu: (mountPoint) => {},
+  setAppMenu: (config) => {},
 });
 
 export const useDashboardMountContext = () => {
