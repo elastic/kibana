15d86494549791c25664a25f43a29e255ca0ba0e
Eyo O. Eyo <7893459+eokoneyo@users.noreply.github.com>
2025-12-12 16:11:17 +0100

    Fix skipped intercept flaky test (#245980)
    ## Summary
    
    Fixes https://github.com/elastic/kibana/issues/245943 and
    https://github.com/elastic/kibana/issues/245800
    
    Ran through flaky tests runner with 50 passes.
    
    <!--
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [ ] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    ### Identify risks
    
    Does this PR introduce any risks? For example, consider risks like hard
    to test bugs, performance regression, potential of data loss.
    
    Describe the risk, its severity, and mitigation for each identified
    risk. Invite stakeholders and evaluate how to proceed before merging.
    
    - [ ] [See some risk
    examples](https://github.com/elastic/kibana/blob/main/RISK_MATRIX.mdx)
    - [ ] ...
    
    
    -->
    
    ---------
    
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/platform/plugins/private/intercepts/public/prompter/prompter.ts
+++ x-pack/platform/plugins/private/intercepts/public/prompter/prompter.ts
@@ -23,7 +23,7 @@ export type { Intercept } from './service';
 type ProductInterceptPrompterSetupDeps = Pick<CoreSetup, 'analytics' | 'notifications'>;
 type ProductInterceptPrompterStartDeps = Omit<
   InterceptServiceStartDeps,
-  'persistInterceptRunId' | 'staticAssetsHelper'
+  'persistInterceptRunId' | 'staticAssetsHelper' | 'resetInterceptTimingRecord'
 > &
   Pick<CoreStart, 'http'>;
 
@@ -38,23 +38,29 @@ export class InterceptPrompter {
   private interceptTimerRegistry = new Proxy<Record<Intercept['id'], { timerStart: Date }>>(
     {},
     {
-      get: (target, prop) => {
+      get: (_target, prop) => {
         if (typeof prop === 'symbol') return undefined;
         const storage = JSON.parse(
-          localStorage.getItem(INTERCEPT_PROMPTER_LOCAL_STORAGE_KEY) || JSON.stringify(target)
+          localStorage.getItem(INTERCEPT_PROMPTER_LOCAL_STORAGE_KEY) || '{}'
         );
         return storage[prop];
       },
-      set: (target, prop, value) => {
+      set: (_target, prop, value) => {
         if (typeof prop === 'symbol') return false;
-        target[prop] = value;
-        localStorage.setItem(INTERCEPT_PROMPTER_LOCAL_STORAGE_KEY, JSON.stringify(target));
+        const storage = JSON.parse(
+          localStorage.getItem(INTERCEPT_PROMPTER_LOCAL_STORAGE_KEY) || '{}'
+        );
+        storage[prop] = value;
+        localStorage.setItem(INTERCEPT_PROMPTER_LOCAL_STORAGE_KEY, JSON.stringify(storage));
         return true;
       },
-      deleteProperty: (target, prop) => {
+      deleteProperty: (_target, prop) => {
         if (typeof prop === 'symbol') return false;
-        delete target[prop];
-        localStorage.setItem(INTERCEPT_PROMPTER_LOCAL_STORAGE_KEY, JSON.stringify(target));
+        const storage = JSON.parse(
+          localStorage.getItem(INTERCEPT_PROMPTER_LOCAL_STORAGE_KEY) || '{}'
+        );
+        delete storage[prop];
+        localStorage.setItem(INTERCEPT_PROMPTER_LOCAL_STORAGE_KEY, JSON.stringify(storage));
         return true;
       },
     }
@@ -74,6 +80,7 @@ export class InterceptPrompter {
       ...dialogServiceDeps,
       persistInterceptRunId: updateUserTriggerData,
       staticAssetsHelper: http.staticAssets,
+      resetInterceptTimingRecord: this.clearInterceptTimerStartRecord.bind(this),
     }));
 
     this.pageHidden$ = Rx.fromEvent(document, 'visibilitychange').pipe(
@@ -138,9 +145,6 @@ export class InterceptPrompter {
               const timeElapsedSinceTimerStart = now - timerStart;
 
               if (timeElapsedSinceTimerStart >= response.triggerIntervalInMs) {
-                // Reset the timer start record, so the next interval starts fresh
-                this.clearInterceptTimerStartRecord(intercept.id);
-
                 // fetch user trigger again because it's possible that the user has already interacted with the intercept,
                 // especially that the user might have interacted with the intercept in a different tab
                 return getUserTriggerData$(intercept.id);
