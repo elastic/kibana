fd880a41bee45339af4f0a813865758d2e6d468d
Ignacio Rivas <rivasign@gmail.com>
2025-12-12 11:34:33 +0100

    Add homepage links

---
+++ src/platform/plugins/shared/home/public/application/components/setup_cloud_connect/setup_cloud_connect.test.tsx
@@ -0,0 +1,29 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React from 'react';
+import { SetupCloudConnect } from './setup_cloud_connect';
+import { shallowWithIntl } from '@kbn/test-jest-helpers';
+
+const addBasePathMock = jest.fn((path: string) => (path ? path : 'path'));
+const applicationMock = {
+  getUrlForApp: jest.fn(() => '/app/cloud_connect'),
+  navigateToApp: jest.fn(),
+} as any;
+
+describe('SetupCloudConnect', () => {
+  test('renders as expected', () => {
+    const component = shallowWithIntl(
+      <SetupCloudConnect addBasePath={addBasePathMock} application={applicationMock} />
+    );
+
+    const $button = component.find('EuiButton');
+    expect($button.props().href).toBe('/app/cloud_connect');
+  });
+});
