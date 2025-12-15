8835173931a044fc8b38be5f6cb1b09cc523844f
Sebastian Delle Donne <sebastian.delledonne@elastic.co>
2025-12-15 17:53:26 +0100

    fix import

--- src/platform/packages/shared/kbn-esql-ast/src/composer/__tests__/esql.scenarios.test.ts
+++ src/platform/packages/shared/kbn-esql-ast/src/composer/__tests__/esql.scenarios.test.ts
@@ -6,7 +6,7 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
-import type { SynthQualifiedColumnShorthand } from '../../synth';
+import type { SynthQualifiedColumnShorthand } from '../synth';
 import { ComposerQuery } from '../composer_query';
 import { esql } from '../esql';
 
