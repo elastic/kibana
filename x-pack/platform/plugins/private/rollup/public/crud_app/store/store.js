9b86b50caa0923ecc5c7be29a199056111380ae9
Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>
2025-12-15 12:15:10 +0100

    Update kibana management team imports

--- x-pack/platform/plugins/private/rollup/public/crud_app/store/store.js
+++ x-pack/platform/plugins/private/rollup/public/crud_app/store/store.js
@@ -6,7 +6,7 @@
  */
 
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { thunk } from 'redux-thunk';
 
 import { rollupJobs } from './reducers';
 
--- x-pack/platform/plugins/shared/license_management/public/application/store/store.js
+++ x-pack/platform/plugins/shared/license_management/public/application/store/store.js
@@ -6,12 +6,12 @@
  */
 
 import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { withExtraArgument } from 'redux-thunk';
 
 import { licenseManagement } from './reducers';
 
 export const licenseManagementStore = (initialState = {}, services = {}) => {
-  const enhancers = [applyMiddleware(thunk.withExtraArgument(services))];
+  const enhancers = [applyMiddleware(withExtraArgument(services))];
 
   window.__REDUX_DEVTOOLS_EXTENSION__ && enhancers.push(window.__REDUX_DEVTOOLS_EXTENSION__());
   return createStore(licenseManagement, initialState, compose(...enhancers));
