ffa0d76e0a5999f2a97bee87ec978d7297524905
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2025-12-15 11:08:27 -0500

    fix after review

--- x-pack/platform/plugins/shared/fleet/public/applications/integrations/sections/epm/screens/browse_integrations/index.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/integrations/sections/epm/screens/browse_integrations/index.tsx
@@ -5,92 +5,17 @@
  * 2.0.
  */
 
-import type { FunctionComponent } from 'react';
 import React from 'react';
-import { i18n } from '@kbn/i18n';
-import { FormattedMessage } from '@kbn/i18n-react';
-import { EuiFlexItem, EuiCallOut, EuiLink, EuiFlexGroup, useEuiTheme } from '@elastic/eui';
-
-import { useStartServices } from '../../../../hooks';
+import { EuiFlexItem, EuiFlexGroup, useEuiTheme, EuiSpacer } from '@elastic/eui';
 
 import { useBreadcrumbs } from '../../../../hooks';
-
+import { NoEprCallout } from '../../components/no_epr_callout';
 import { categoryExists } from '../home';
 
 import { PackageGrid } from './components/package_grid';
 import { SearchAndFiltersBar } from './components/search_and_filters_bar';
 import { Sidebar } from './components/side_bar';
 import { useBrowseIntegrationHook } from './hooks';
-
-const NoEprCallout: FunctionComponent<{ statusCode?: number }> = ({
-  statusCode,
-}: {
-  statusCode?: number;
-}) => {
-  let titleMessage;
-  let descriptionMessage;
-  if (statusCode === 502) {
-    titleMessage = i18n.translate('xpack.fleet.epmList.eprUnavailableBadGatewayCalloutTitle', {
-      defaultMessage:
-        'Kibana cannot reach the Elastic Package Registry, which provides Elastic Agent integrations\n',
-    });
-    descriptionMessage = (
-      <FormattedMessage
-        id="xpack.fleet.epmList.eprUnavailableCallouBdGatewaytTitleMessage"
-        defaultMessage="To view these integrations, configure a  {registryproxy} or host {onpremregistry}."
-        values={{
-          registryproxy: <ProxyLink />,
-          onpremregistry: <OnPremLink />,
-        }}
-      />
-    );
-  } else {
-    titleMessage = i18n.translate('xpack.fleet.epmList.eprUnavailable400500CalloutTitle', {
-      defaultMessage:
-        'Kibana cannot connect to the Elastic Package Registry, which provides Elastic Agent integrations\n',
-    });
-    descriptionMessage = (
-      <FormattedMessage
-        id="xpack.fleet.epmList.eprUnavailableCallout400500TitleMessage"
-        defaultMessage="Ensure the {registryproxy} or {onpremregistry} is configured correctly, or try again later."
-        values={{
-          registryproxy: <ProxyLink />,
-          onpremregistry: <OnPremLink />,
-        }}
-      />
-    );
-  }
-
-  return (
-    <EuiCallOut title={titleMessage} iconType="info" color={'warning'}>
-      <p>{descriptionMessage}</p>
-    </EuiCallOut>
-  );
-};
-
-function ProxyLink() {
-  const { docLinks } = useStartServices();
-
-  return (
-    <EuiLink href={docLinks.links.fleet.settingsFleetServerProxySettings} target="_blank">
-      {i18n.translate('xpack.fleet.epmList.proxyLinkSnippedText', {
-        defaultMessage: 'proxy server',
-      })}
-    </EuiLink>
-  );
-}
-
-function OnPremLink() {
-  const { docLinks } = useStartServices();
-
-  return (
-    <EuiLink href={docLinks.links.fleet.onPremRegistry} target="_blank">
-      {i18n.translate('xpack.fleet.epmList.onPremLinkSnippetText', {
-        defaultMessage: 'your own registry',
-      })}
-    </EuiLink>
-  );
-}
 
 export const BrowseIntegrationsPage: React.FC<{ prereleaseIntegrationsEnabled: boolean }> = ({
   prereleaseIntegrationsEnabled,
@@ -131,7 +56,12 @@ export const BrowseIntegrationsPage: React.FC<{ prereleaseIntegrationsEnabled: b
   let noEprCallout;
   if (eprPackageLoadingError || eprCategoryLoadingError) {
     const error = eprPackageLoadingError || eprCategoryLoadingError;
-    noEprCallout = <NoEprCallout statusCode={error?.statusCode} />;
+    noEprCallout = (
+      <EuiFlexItem grow={1}>
+        <NoEprCallout statusCode={error?.statusCode} />
+        <EuiSpacer size="s" />
+      </EuiFlexItem>
+    );
   }
 
   return (
@@ -150,6 +80,7 @@ export const BrowseIntegrationsPage: React.FC<{ prereleaseIntegrationsEnabled: b
       <EuiFlexItem grow={5}>
         <EuiFlexGroup direction="column" gutterSize="none">
           <SearchAndFiltersBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          {noEprCallout ? noEprCallout : null}
           <EuiFlexItem
             grow={1}
             data-test-subj="epmList.mainColumn"
