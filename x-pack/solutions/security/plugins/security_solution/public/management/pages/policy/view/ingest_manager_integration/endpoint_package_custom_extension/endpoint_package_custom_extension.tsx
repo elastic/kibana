7535e18440495d278ed9da5fc6591c9a85284fca
parkiino <candace.park@elastic.co>
2025-12-15 23:13:50 -0500

    linting

--- x-pack/solutions/security/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/endpoint_package_custom_extension/endpoint_package_custom_extension.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/management/pages/policy/view/ingest_manager_integration/endpoint_package_custom_extension/endpoint_package_custom_extension.tsx
@@ -228,7 +228,7 @@ export const EndpointPackageCustomExtension = memo<PackageCustomExtensionCompone
       props,
       trustedDevicesVisible,
       canReadEventFilters,
-      canReadEndpointExceptions,
+      endpointExceptionsVisible,
       canReadHostIsolationExceptions,
       canReadBlocklist,
     ]);
