624eba93ac6581b1089d07ee74c2cf4e0752336b
klacabane <kevin.lacabane@elastic.co>
2025-12-16 13:58:10 +0100

    exhaustive deps

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_significant_events_view/add_significant_event_flyout/add_significant_event_flyout.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_significant_events_view/add_significant_event_flyout/add_significant_event_flyout.tsx
@@ -238,6 +238,8 @@ export function AddSignificantEventFlyout({
     telemetryClient,
     features.length,
     definition,
+    generateDescription,
+    saveDescription,
   ]);
 
   useEffect(() => {
