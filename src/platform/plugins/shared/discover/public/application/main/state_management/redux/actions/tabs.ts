9e7ee730f87eafc8d5bea93181076f2d8a39ed70
Ania Kowalska <ania.kowalska@elastic.co>
2025-12-17 14:48:11 +0100

    create action for dismissing flyouts

--- src/platform/plugins/shared/discover/public/application/main/state_management/redux/actions/tabs.ts
+++ src/platform/plugins/shared/discover/public/application/main/state_management/redux/actions/tabs.ts
@@ -22,6 +22,7 @@ import type { DiscoverAppState, TabState } from '../types';
 import { selectAllTabs, selectRecentlyClosedTabs, selectTab } from '../selectors';
 import {
   internalStateSlice,
+  discardFlyoutsOnTabChange,
   type TabActionPayload,
   type InternalStateThunkActionCreator,
 } from '../internal_state';
@@ -284,6 +285,8 @@ export const updateTabs: InternalStateThunkActionCreator<
         searchSessionManager.removeSearchSessionIdFromURL({ replace: true });
         services.data.search.session.reset();
       }
+
+      dispatch(discardFlyoutsOnTabChange());
     }
 
     dispatch(
