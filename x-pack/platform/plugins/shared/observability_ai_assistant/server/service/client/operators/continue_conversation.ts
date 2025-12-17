f2ea86930017d5383e1144dbac05c2650282b428
Arturo Lidueña <arturo.liduena@elastic.co>
2025-12-17 11:11:42 +0100

    fix args on createToolNotFoundError

--- x-pack/platform/plugins/shared/observability_ai_assistant/server/service/client/operators/continue_conversation.ts
+++ x-pack/platform/plugins/shared/observability_ai_assistant/server/service/client/operators/continue_conversation.ts
@@ -331,10 +331,7 @@ export function continueConversation({
       return of(
         createServerSideFunctionResponseError({
           name: functionCallName,
-          error: createToolNotFoundError({
-            name: functionCallName,
-            args: lastMessage.function_call!.arguments ?? '',
-          }),
+          error: createToolNotFoundError(functionCallName),
         })
       );
     }
