e01e5950fb5947e4b2600632322073af5fb0b9e2
Brad White <brad.white@elastic.co>
2025-12-17 15:01:58 +1000

    add integration test

---
+++ src/platform/packages/private/kbn-babel-plugin-transform-barrels/__tests__/build_integration.test.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import Path from 'path';
+import Fsp from 'fs/promises';
+
+import { REPO_ROOT } from '@kbn/repo-info';
+import { ToolingLog } from '@kbn/tooling-log';
+import { Package } from '@kbn/repo-packages';
+
+import { BuildPackages } from '../../../../../dev/build/tasks/build_packages_task';
+import { Config } from '../../../../../dev/build/lib/config';
+import { Build } from '../../../../../dev/build/lib/build';
+
+const TEST_PACKAGE_PATH = 'src/core/packages/http/server-internal';
+const TEST_PACKAGE_MANIFEST = Path.join(REPO_ROOT, TEST_PACKAGE_PATH, 'kibana.jsonc');
+
+jest.setTimeout(600 * 1000);
+
+describe('barrel transform build integration', () => {
+  let outputDir: string;
+  let testPackage: Package;
+  let log: ToolingLog;
+
+  beforeAll(async () => {
+    testPackage = Package.fromManifest(REPO_ROOT, TEST_PACKAGE_MANIFEST);
+    outputDir = Path.join(REPO_ROOT, 'build', 'kibana', testPackage.normalizedRepoRelativeDir);
+    log = new ToolingLog({ level: 'info', writeTo: process.stdout });
+
+    // Cleanup package build if it already exists
+    await Fsp.rm(outputDir, { recursive: true, force: true });
+  });
+
+  it('builds package with barrel transforms applied', async () => {
+    const realConfig = await Config.create({
+      isRelease: false,
+      targetAllPlatforms: false,
+      targetServerlessPlatforms: false,
+      dockerContextUseLocalArtifact: null,
+      dockerCrossCompile: false,
+      dockerNamespace: null,
+      dockerPush: false,
+      dockerTag: null,
+      dockerTagQualifier: null,
+      downloadFreshNode: false,
+      withExamplePlugins: false,
+      withTestPlugins: false,
+    });
+
+    const mockConfig = Object.create(realConfig);
+    mockConfig.getDistPackagesFromRepo = () => [testPackage];
+
+    const build = new Build(mockConfig);
+    await BuildPackages.run(mockConfig, log, build);
+
+    const outputSrcDir = Path.join(outputDir, 'src');
+
+    const httpConfigPath = Path.join(outputSrcDir, 'http_config.js');
+    const httpConfigContent = await Fsp.readFile(httpConfigPath, 'utf-8');
+
+    // Assert barrel imports were transformed to direct paths
+    // Original: import { CspConfig } from './csp'
+    expect(httpConfigContent).not.toMatch(/require\(['"]\.\/csp['"]\)/);
+    expect(httpConfigContent).toMatch(/require\(['"]\.\/csp\/csp_config['"]\)/);
+  });
+});
