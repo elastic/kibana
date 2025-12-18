f5615eee166c33acde3e66b42ef699420f8ad5ec
Quentin Couvelaire <quentin.couvelaire@elastic.co>
2025-12-18 08:39:43 -0500

    change processor description to only include from

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/steps/blocks/action/utils.ts
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/steps/blocks/action/utils.ts
@@ -44,18 +44,6 @@ export const getStepDescription = (step: StreamlangProcessorDefinitionWithUIAttr
         }
       );
     } else if (step.action === 'uppercase') {
-      if (step.to) {
-        return i18n.translate(
-          'xpack.streams.streamDetailView.managementTab.enrichment.uppercaseProcessorDescriptionWithTo',
-          {
-            defaultMessage: 'Uppercases the value of "{from}" and stores it in "{to}"',
-            values: {
-              from: step.from,
-              to: step.to,
-            },
-          }
-        );
-      }
       return i18n.translate(
         'xpack.streams.streamDetailView.managementTab.enrichment.uppercaseProcessorDescription',
         {
