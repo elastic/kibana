477619e08c7f57f3b4782830052dfdce2d317934
Julia Bardi <julia.bardi@elastic.co>
2025-12-18 09:13:25 +0100

    check delete pipelines step

--- x-pack/platform/test/fleet_api_integration/apis/package_policy/upgrade.ts
+++ x-pack/platform/test/fleet_api_integration/apis/package_policy/upgrade.ts
@@ -9,6 +9,7 @@ import type {
   UpgradePackagePolicyDryRunResponse,
   UpgradePackagePolicyResponse,
 } from '@kbn/fleet-plugin/common/types';
+import { sortBy } from 'lodash';
 import type { FtrProviderContext } from '../../../api_integration/ftr_provider_context';
 import { skipIfNoDockerRegistry } from '../../helpers';
 import { getInstallationInfo } from './helper';
@@ -1269,6 +1270,10 @@ export default function (providerContext: FtrProviderContext) {
         });
       });
 
+      const expectIdArraysEqual = (arr1: any[], arr2: any[]) => {
+        expect(sortBy(arr1, 'id')).to.eql(sortBy(arr2, 'id'));
+      };
+
       describe('upgrade', function () {
         it('upgrades the package policy and creates the correct templates', async function () {
           await supertest
@@ -1285,13 +1290,17 @@ export default function (providerContext: FtrProviderContext) {
               'integration_to_input',
               '3.0.0'
             );
-            expectedAssets.forEach((item) => {
-              expect(
-                installation.installed_es.find(
-                  (asset: any) => asset.type === item.type && asset.id === item.id
-                )
-              ).to.not.be(undefined);
-            });
+            // expectedAssets.forEach((item) => {
+            //   expect(
+            //     installation.installed_es.find(
+            //       (asset: any) => asset.type === item.type && asset.id === item.id
+            //     )
+            //   ).to.not.be(undefined);
+            // });
+            expectIdArraysEqual(
+              installation.installed_es.filter((item: any) => item.type !== 'knowledge_base'),
+              expectedAssets
+            );
           });
 
           const expectedComponentTemplates = expectedAssets.filter(
