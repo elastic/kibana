a2646789aa87c68ebea7658bb5a4f01c5fdf1e4f
Kevin Delemme <kdelemme@gmail.com>
2025-12-18 16:19:41 -0500

    Handle gorupby

--- x-pack/solutions/observability/plugins/slo/server/services/slo_template_repository.ts
+++ x-pack/solutions/observability/plugins/slo/server/services/slo_template_repository.ts
@@ -7,6 +7,8 @@
 
 import type { SavedObjectsClientContract } from '@kbn/core-saved-objects-api-server';
 import { SavedObjectsErrorHelpers } from '@kbn/core-saved-objects-server';
+import * as t from 'io-ts';
+import { arrayToStringRt } from '@kbn/io-ts-utils';
 import {
   budgetingMethodSchema,
   indicatorSchema,
@@ -118,6 +120,10 @@ export class DefaultSLOTemplateRepository implements SLOTemplateRepository {
         if (isRight(decoded)) {
           template.settings = decoded.right;
         }
+      }
+
+      if (stored.groupBy && t.array(t.string).is(stored.groupBy)) {
+        template.groupBy = stored.groupBy;
       }
 
       return template;
