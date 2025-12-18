b8c93a2a3476aa88f43fd4efd7e08379050a1887
Brad White <brad.white@elastic.co>
2025-12-18 17:17:32 +1000

    inceares sampling interval

--- .buildkite/scripts/steps/ftr_bench.sh
+++ .buildkite/scripts/steps/ftr_bench.sh
@@ -7,6 +7,8 @@ source .buildkite/scripts/common/util.sh
 .buildkite/scripts/bootstrap.sh
 .buildkite/scripts/setup_es_snapshot_cache.sh
 
+export KBN_PROFILER_SAMPLING_INTERVAL=10000
+
 echo '--- FTR Bench against Merge Base'
 node scripts/bench.js --config src/platform/packages/shared/kbn-ftr-benchmarks/benchmark.config.ts --left "${GITHUB_PR_MERGE_BASE}" --right "${BUILDKITE_COMMIT}" --profile --debug --config-from-cwd
 
