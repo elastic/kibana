732a9dc81603ae83cd9b843cd40bfe9eb6dbab14
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2025-12-18 13:41:15 +0100

    Update condition to run EUI helpers tests

--- .buildkite/scripts/steps/test/scout_test_run_builder.sh
+++ .buildkite/scripts/steps/test/scout_test_run_builder.sh
@@ -24,15 +24,15 @@ node scripts/scout.js run-tests \
 echo '--- Checking EUI Website Availability'
 eui_check_exit=0
 ts-node src/platform/packages/shared/kbn-scout/test/scout/scripts/check_eui_availability.ts || eui_check_exit=$?
-if [ "$eui_check_exit" -eq 2 ]; then
-  echo 'EUI website is not available - skipping EUI Helpers Tests'
-else
+if [ "$eui_check_exit" -eq 0 ]; then
   echo '--- Running Scout EUI Helpers Tests'
   SCOUT_TARGET_TYPE='local' SCOUT_TARGET_MODE='serverless=security' \
   "${KIBANA_DIR:-$(pwd)}/node_modules/.bin/playwright" test \
     --project local \
     --grep @svlSecurity \
     --config src/platform/packages/shared/kbn-scout/test/scout/ui/playwright.config.ts
+else
+  echo 'EUI website is not available - skipping EUI Helpers Tests'
 fi
 
 source .buildkite/scripts/steps/test/scout_upload_report_events.sh
