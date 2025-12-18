de712d70fb8be022cfcde5b79d42c64b5e4e2f5f
Paul Tavares <paul.tavares@elastic.co>
2025-12-18 15:54:38 -0500

    ENable feature flag in FTR tests

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/scripts_library/trial_license_complete_tier/configs/ess.config.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/scripts_library/trial_license_complete_tier/configs/ess.config.ts
@@ -6,17 +6,32 @@
  */
 
 import type { FtrConfigProviderContext } from '@kbn/test';
+import type { ExperimentalFeatures } from '@kbn/security-solution-plugin/common';
 
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const functionalConfig = await readConfigFile(
     require.resolve('../../../../../config/ess/config.base.edr_workflows.trial')
   );
 
+  const enabledExperimentalFeatures: Array<keyof ExperimentalFeatures> = [
+    'responseActionsScriptLibraryManagement',
+  ];
+
   return {
     ...functionalConfig.getAll(),
     testFiles: [require.resolve('..')],
     junit: {
       reportName: 'EDR Workflows - Policy Response Integration Tests - ESS Env - Trial License',
+    },
+    kbnTestServer: {
+      ...functionalConfig.get('kbnTestServer'),
+      serverArgs: [
+        ...functionalConfig.get('kbnTestServer.serverArgs'),
+        // SECURITY SOLUTION: set any experimental feature flags for testing
+        `--xpack.securitySolution.enableExperimental=${JSON.stringify(
+          enabledExperimentalFeatures
+        )}`,
+      ],
     },
   };
 }
--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/scripts_library/trial_license_complete_tier/configs/serverless.config.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/scripts_library/trial_license_complete_tier/configs/serverless.config.ts
@@ -6,17 +6,32 @@
  */
 
 import type { FtrConfigProviderContext } from '@kbn/test';
+import type { ExperimentalFeatures } from '@kbn/security-solution-plugin/common';
 
 export default async function ({ readConfigFile }: FtrConfigProviderContext) {
   const functionalConfig = await readConfigFile(
     require.resolve('../../../../../config/serverless/config.base.edr_workflows')
   );
 
+  const enabledExperimentalFeatures: Array<keyof ExperimentalFeatures> = [
+    'responseActionsScriptLibraryManagement',
+  ];
+
   return {
     ...functionalConfig.getAll(),
     testFiles: [require.resolve('..')],
     junit: {
       reportName: 'EDR Workflows - Policy Response Integration Tests - Serverless Env - Complete',
+    },
+    kbnTestServer: {
+      ...functionalConfig.get('kbnTestServer'),
+      serverArgs: [
+        ...functionalConfig.get('kbnTestServer.serverArgs'),
+        // SECURITY SOLUTION: set any experimental feature flags for testing
+        `--xpack.securitySolution.enableExperimental=${JSON.stringify(
+          enabledExperimentalFeatures
+        )}`,
+      ],
     },
   };
 }
