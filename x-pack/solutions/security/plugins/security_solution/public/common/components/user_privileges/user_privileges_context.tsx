2cc62ebcc08ed5574b2f950d4334a0c3ac12c9d2
Edgar Santos <edgar.santos@elastic.co>
2025-12-18 14:32:10 +0100

    update UserPrivilegesContext and useAlertsPrivileges
    add support to the new alert permissions

--- x-pack/solutions/security/plugins/security_solution/public/common/components/user_privileges/user_privileges_context.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/common/components/user_privileges/user_privileges_context.tsx
@@ -19,6 +19,11 @@ import {
   extractRulesCapabilities,
   getRulesCapabilitiesInitialState,
 } from '../../utils/rules_capabilities';
+import type { AlertsUICapabilities } from '../../utils/alerts_capabilities';
+import {
+  extractAlertsCapabilities,
+  getAlertsCapabilitiesInitialState,
+} from '../../utils/alerts_capabilities';
 
 export interface UserPrivilegesState {
   listPrivileges: ReturnType<typeof useFetchListPrivileges>;
@@ -28,6 +33,7 @@ export interface UserPrivilegesState {
   timelinePrivileges: { crud: boolean; read: boolean };
   notesPrivileges: { crud: boolean; read: boolean };
   rulesPrivileges: RulesUICapabilities;
+  alertsPrivileges: AlertsUICapabilities;
 }
 
 export const initialUserPrivilegesState = (): UserPrivilegesState => ({
@@ -38,6 +44,7 @@ export const initialUserPrivilegesState = (): UserPrivilegesState => ({
   timelinePrivileges: { crud: false, read: false },
   notesPrivileges: { crud: false, read: false },
   rulesPrivileges: getRulesCapabilitiesInitialState(),
+  alertsPrivileges: getAlertsCapabilitiesInitialState(),
 });
 export const UserPrivilegesContext = createContext<UserPrivilegesState>(
   initialUserPrivilegesState()
@@ -59,6 +66,12 @@ export const UserPrivilegesProvider = ({
     () => extractRulesCapabilities(kibanaCapabilities),
     [kibanaCapabilities]
   );
+
+  const alertsPrivileges = useMemo(
+    () => extractAlertsCapabilities(kibanaCapabilities),
+    [kibanaCapabilities]
+  );
+
   const shouldFetchListPrivileges = read || rulesPrivileges.rules.read;
 
   const listPrivileges = useFetchListPrivileges(shouldFetchListPrivileges);
@@ -92,6 +105,7 @@ export const UserPrivilegesProvider = ({
       timelinePrivileges,
       notesPrivileges,
       rulesPrivileges,
+      alertsPrivileges,
     }),
     [
       listPrivileges,
@@ -101,6 +115,7 @@ export const UserPrivilegesProvider = ({
       timelinePrivileges,
       notesPrivileges,
       rulesPrivileges,
+      alertsPrivileges,
     ]
   );
 
