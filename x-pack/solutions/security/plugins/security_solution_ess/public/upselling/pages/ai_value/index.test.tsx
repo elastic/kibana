f9838ee43f8e2c49ae9c5a4ae5495a9670f7b22f
Steph Milovic <stephanie.milovic@elastic.co>
2025-12-19 11:35:25 -0700

    add licensing check and upselling for ai value

---
+++ x-pack/solutions/security/plugins/security_solution_ess/public/upselling/pages/ai_value/index.test.tsx
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { AIValueUpsellingPageESS } from './index';
+
+jest.mock('../attack_discovery/upgrade_actions', () => ({
+  UpgradeActions: () => <button type="button">Upgrade</button>,
+}));
+
+jest.mock('@kbn/shared-ux-page-kibana-template', () => {
+  const KibanaPageTemplate = ({ children }: { children: React.ReactNode }) => <div>{children}</div>;
+  KibanaPageTemplate.Section = ({ children }: { children: React.ReactNode }) => (
+    <div>{children}</div>
+  );
+  return { KibanaPageTemplate };
+});
+
+describe('AIValueUpsellingPageESS', () => {
+  it('renders the assistant beacon', () => {
+    render(<AIValueUpsellingPageESS />);
+
+    expect(screen.getByTestId('assistantAvatar')).toBeInTheDocument();
+  });
+});
+
+
