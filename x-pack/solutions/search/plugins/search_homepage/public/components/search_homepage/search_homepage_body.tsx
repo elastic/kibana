74097a9d80874bb1e6440a4e0c91343c8320d4bf
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2025-12-19 16:02:36 +0100

    Padding fixes for home

--- x-pack/solutions/search/plugins/search_homepage/public/components/search_homepage/search_homepage_body.tsx
+++ x-pack/solutions/search/plugins/search_homepage/public/components/search_homepage/search_homepage_body.tsx
@@ -8,22 +8,31 @@
 import React from 'react';
 
 import { KibanaPageTemplate } from '@kbn/shared-ux-page-kibana-template';
-import { EuiFlexGroup, EuiFlexItem, EuiHorizontalRule, EuiSpacer } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiHorizontalRule, EuiSpacer, useEuiTheme } from '@elastic/eui';
 
+import { css } from '@emotion/react';
 import { MetricPanels } from './metric_panels';
 import { CloudResources } from './cloud_resources';
 import { BodyLinks } from './body_links';
 import { useAuthenticatedUser } from '../../hooks/use_authenticated_user';
 import { useKibana } from '../../hooks/use_kibana';
 import { GettingStartedBanner } from './getting_started_banner';
+import { AdminSettingsLinks } from './homepage_nav_links';
 
 export const SearchHomepageBody = () => {
+  const { euiTheme } = useEuiTheme();
   const {
     services: { cloud },
   } = useKibana();
   const { isAdmin, isBillingAdmin } = useAuthenticatedUser();
   return (
-    <KibanaPageTemplate.Section alignment="top" restrictWidth={true} grow paddingSize="none">
+    <KibanaPageTemplate.Section
+      alignment="top"
+      restrictWidth={true}
+      grow
+      paddingSize="none"
+      css={css({ padding: `0 ${euiTheme.size.l}` })}
+    >
       <EuiFlexGroup gutterSize="l" direction="column">
         <EuiFlexItem>
           <EuiSpacer size="l" />
