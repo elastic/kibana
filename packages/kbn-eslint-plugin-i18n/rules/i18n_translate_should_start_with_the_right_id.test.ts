279531f86bc3672d0d1339501154641407dd2a99
Coen Warmer <coen.warmer@gmail.com>
2025-12-21 16:57:26 +0100

    Report different warning when package / plugin is not added to .i18nrc.json yet

--- packages/kbn-eslint-plugin-i18n/rules/i18n_translate_should_start_with_the_right_id.test.ts
+++ packages/kbn-eslint-plugin-i18n/rules/i18n_translate_should_start_with_the_right_id.test.ts
@@ -11,6 +11,7 @@ import { RuleTester } from 'eslint';
 import {
   I18nTranslateShouldStartWithTheRightId,
   RULE_WARNING_MESSAGE,
+  NO_IDENTIFIER_MESSAGE,
 } from './i18n_translate_should_start_with_the_right_id';
 
 const tsTester = [
@@ -43,6 +44,22 @@ const babelTester = [
 ] as const;
 
 const invalid: RuleTester.InvalidTestCase[] = [
+  {
+    name: 'When a file is not in a known package or the package has no i18n identifier, it should report an error',
+    filename: '/some/fake/path/that/does/not/exist/test_component.ts',
+    code: `
+import { i18n } from '@kbn/i18n';
+
+function TestComponent() {
+  const foo = i18n.translate('some.id', { defaultMessage: 'test' });
+}`,
+    errors: [
+      {
+        line: 5,
+        message: NO_IDENTIFIER_MESSAGE.replace('APP_ID', 'Unknown package'),
+      },
+    ],
+  },
   {
     name: 'When a string literal is passed to i18n.translate, it should start with the correct i18n identifier, and if no existing defaultMessage is passed, it should add an empty default.',
     filename: '/x-pack/solutions/observability/plugins/observability/public/test_component.ts',
