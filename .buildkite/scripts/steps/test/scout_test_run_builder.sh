64514e6f78bcbff976a714a602fb9a9045098d5a
Brad White <Ikuni17@users.noreply.github.com>
2025-12-23 02:17:00 +1000

    [9.3] Disable serverless tests (#246989)
    ## Summary
    
    Usual disable for non-main branches on version bump
    
    ---------
    
    Co-authored-by: Dzmitry Lemechko <dzmitry.lemechko@elastic.co>

--- .buildkite/scripts/steps/test/scout_test_run_builder.sh
+++ .buildkite/scripts/steps/test/scout_test_run_builder.sh
@@ -10,14 +10,14 @@ node scripts/scout run-playwright-test-check
 echo '--- Update Scout Test Config Manifests'
 node scripts/scout.js update-test-config-manifests
 
-echo '--- Discover Playwright Configs and upload to Buildkite artifacts'
-node scripts/scout discover-playwright-configs --save
+echo '--- Discover Playwright Configs and upload to Buildkite artifacts (only stateful tests)'
+node scripts/scout discover-playwright-configs --target ech --save
 cp .scout/test_configs/scout_playwright_configs.json scout_playwright_configs.json
 buildkite-agent artifact upload "scout_playwright_configs.json"
 
 echo '--- Running Scout API Integration Tests'
 node scripts/scout.js run-tests \
---serverless=security \
+--stateful \
 --config src/platform/packages/shared/kbn-scout/test/scout/api/playwright.config.ts \
 --kibana-install-dir "$KIBANA_BUILD_LOCATION"
 
