0abeb20b390a0d7f8a3a0d6d2016dac5faa39a83
Andreana Malama <andreana.malama@elastic.co>
2025-12-23 17:36:13 +0200

    Fix rows and split_metrics_by esql column field type

--- src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/transforms/charts/datatable/to_state/index.ts
+++ src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/transforms/charts/datatable/to_state/index.ts
@@ -74,18 +74,14 @@ export function buildFormBasedLayer(
 
 export function getValueColumns(config: DatatableStateESQL) {
   return [
-    ...config.metrics.map((metric, index) =>
-      getValueColumn(getAccessorName(METRIC_ACCESSOR_PREFIX, index), metric.column, 'number')
-    ),
     ...(config.rows ?? []).map((row, index) =>
-      getValueColumn(getAccessorName(ROW_ACCESSOR_PREFIX, index), row.column, 'number')
+      getValueColumn(getAccessorName(ROW_ACCESSOR_PREFIX, index), row.column)
     ),
     ...(config.split_metrics_by ?? []).map((splitBy, index) =>
-      getValueColumn(
-        getAccessorName(SPLIT_METRIC_BY_ACCESSOR_PREFIX, index),
-        splitBy.column,
-        'number'
-      )
+      getValueColumn(getAccessorName(SPLIT_METRIC_BY_ACCESSOR_PREFIX, index), splitBy.column)
+    ),
+    ...config.metrics.map((metric, index) =>
+      getValueColumn(getAccessorName(METRIC_ACCESSOR_PREFIX, index), metric.column, 'number')
     ),
   ];
 }
