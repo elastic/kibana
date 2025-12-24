01bdf19f4d886f0cb635ef5324b9102c0c11cc5e
Christos Nasikas <christos.nasikas@elastic.co>
2025-12-24 13:51:01 +0200

    Create alerting feature privilege

---
+++ x-pack/platform/plugins/shared/alerting_v2/server/lib/security/privileges.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { FeaturesPluginSetup } from '@kbn/features-plugin/server';
+import type { AppCategory } from '@kbn/core/types';
+import { APP_ID } from '../constants';
+
+const ALERTS_FEATURE_ID = 'alerting_v2_alerts';
+const RULES_FEATURE_ID = 'alerting_v2_rules';
+const CATEGORY_ID = 'alerting';
+
+const getPrivileges = () => ({
+  all: {
+    app: [APP_ID],
+    savedObject: {
+      all: [],
+      read: [],
+    },
+    ui: [],
+    api: [],
+  },
+  read: {
+    app: [APP_ID],
+    savedObject: {
+      all: [],
+      read: [],
+    },
+    ui: [],
+    api: [],
+  },
+});
+
+const category: AppCategory = {
+  id: CATEGORY_ID,
+  label: 'Alerting',
+  order: 1000,
+  euiIconType: 'watchesApp',
+};
+
+const alertsFeature = {
+  id: ALERTS_FEATURE_ID,
+  name: 'Alerts',
+  category,
+  app: [APP_ID],
+  privileges: getPrivileges(),
+};
+
+const rulesFeature = {
+  id: RULES_FEATURE_ID,
+  name: 'Rules',
+  category,
+  app: [APP_ID],
+  privileges: getPrivileges(),
+};
+
+export const registerFeaturePrivileges = (features: FeaturesPluginSetup) => {
+  features.registerKibanaFeature(alertsFeature);
+  features.registerKibanaFeature(rulesFeature);
+};
