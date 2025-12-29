b5c84d4e60e5448cf1079b742886f4178f33f99b
Ersin Erdal <ersin.erdal@elastic.co>
2025-12-29 01:44:22 +0100

    fix task runner

--- x-pack/platform/plugins/shared/alerting/server/task_runner/task_runner.ts
+++ x-pack/platform/plugins/shared/alerting/server/task_runner/task_runner.ts
@@ -440,7 +440,7 @@ export class TaskRunner<
     // we don't need to keep this information around.
     if (this.ruleType.autoRecoverAlerts) {
       const alerts = alertsClient.getRawAlertInstancesForState(true);
-      alertsToReturn = Object.assign({}, alerts.rawActiveAlerts, alerts.rawDelayedAlerts);
+      alertsToReturn = alerts.rawActiveAlerts;
       recoveredAlertsToReturn = alerts.rawRecoveredAlerts;
       alertsToUpdateWithLastScheduledActions =
         alertsClient.getAlertsToUpdateWithLastScheduledActions();
