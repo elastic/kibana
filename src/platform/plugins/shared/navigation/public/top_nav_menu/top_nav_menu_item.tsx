elastic/kibana

commit 7160a45051a665e77256fa3c96a099589df49c1c

[Write Restricted Dashboards] Feature branch  (#224552)
Closes https://github.com/elastic/kibana-team/issues/808

The respective teams have been raising PRs against this feature branch.
Approved PRs merged so far:
- #221916
- #224411 
- #239973 
- https://github.com/elastic/kibana/pull/241101
- https://github.com/elastic/kibana/pull/238468
- https://github.com/elastic/kibana/pull/233552
- #228416 
- https://github.com/elastic/kibana/pull/241168
- https://github.com/elastic/kibana/pull/244746
- https://github.com/elastic/kibana/pull/244830

## Summary
This pull request overhauls the saved object management workflow by
introducing the concept of ownership for SOs - specifically enabled for
dashboards only at the moment. Owners and administrators can now control
a new write-restricted flag on their objects, allowing them to keep work
draft/uneditable state before publishing. This change enables users to
define who can modify shared objects, providing a crucial capability to
manage and share dashboards.


## Release note
Kibana Dashboards now support ownership and "write_restricted" mode.
Users can now keep dashboards publicly editable or in a write-restricted
state until they are ready to publish, giving them more control over who
can edit their dashboards, regardless of broader space permissions.


## How to test

### Serverless
Please reach out to me via slack or in the project channel
(#read-only-dashboards) to be invited to the serverless environment
where this feature has been enabled.

### Local

- Clone this PR
- Enable the feature by editing kibana.yml to include 
```
savedObjects.enableAccessControl: true
```
- Start ES and Kibana as you would
- Once started, seed Kibana with sample data. This should create a few
dashboards.
- Navigate to dashboards and create a new one.
- In the share modal, change the view mode `Everybody in the space Can
View`,
<img width="500" height="410" alt="image"

src/platform/plugins/shared/navigation/public/top_nav_menu/top_nav_menu_item.tsx
@@ -112,6 +112,8 @@ export function TopNavMenuItem(props: TopNavMenuItemProps) {
       ? { onClick: undefined, href: props.href, target: props.target }
       : {};
 
+  const showFragment = props.disableButton || props.tooltip;
+
   const btn = props.splitButtonProps ? (
     <SplitButton
       {...commonButtonProps}
@@ -125,7 +127,7 @@ export function TopNavMenuItem(props: TopNavMenuItemProps) {
   ) : props.iconOnly && props.iconType && !props.isMobileMenu ? (
     // icon only buttons are not supported by EuiHeaderLink
     React.createElement(
-      props.disableButton ? React.Fragment : EuiToolTip,
+      showFragment ? React.Fragment : EuiToolTip,
       // @ts-expect-error - EuiToolTip does not accept `key` prop, we pass to react Fragment
       {
         ...(props.disableButton
@@ -162,7 +164,11 @@ export function TopNavMenuItem(props: TopNavMenuItemProps) {
 
   const tooltip = getTooltip();
   if (tooltip) {
-    return <EuiToolTip content={tooltip}>{btn}</EuiToolTip>;
+    return (
+      <EuiToolTip title={props.tooltipTitle} content={tooltip}>
+        {btn}
+      </EuiToolTip>
+    );
   }
   return btn;
 }

commit 27bc476a2517bd413bae0b5eb7c87c0816db2163

[Navigation] Fix props for icon only top nav menu item (#247634)
## Summary

Closes #247633.

Bug introduced in #224552. This changes the conditional for passing the
correct props to match the condition for rendering this nested component
as a `Fragment` or `EuiTooltip`. This prevents the invalid tooltips
props from being passed on the `Fragment`.


### Checklist

Check the PR satisfies following conditions. 

Reviewers should verify this PR satisfies this list as well.

- [ ] Any text added follows [EUI's writing
guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
sentence case text and includes [i18n
support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
- [ ]
[Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
was added for features that require explanation or tutorials
- [ ] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [ ] If a plugin configuration key changed, check if it needs to be
allowlisted in the cloud and added to the [docker
list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
- [ ] This was checked for breaking HTTP API changes, and any breaking
changes have been approved by the breaking-change committee. The
`release_note:breaking` label should be applied in these situations.
- [ ] [Flaky Test
Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
used on any tests changed
- [ ] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
- [ ] Review the [backport
guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)

src/platform/plugins/shared/navigation/public/top_nav_menu/top_nav_menu_item.tsx
@@ -131,7 +131,7 @@ export function TopNavMenuItem(props: TopNavMenuItemProps) {
       showFragment ? React.Fragment : EuiToolTip,
       // @ts-expect-error - EuiToolTip does not accept `key` prop, we pass to react Fragment
       {
-        ...(props.disableButton
+        ...(showFragment
           ? { key: props.label || props.id! }
           : {
               content: upperFirst(props.label || props.id!),