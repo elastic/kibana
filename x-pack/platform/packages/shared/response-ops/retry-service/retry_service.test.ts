3cb4dd0e0480d4c12101ac43befdcd02939c6b5b
Christos Nasikas <christos.nasikas@elastic.co>
2025-12-30 19:41:52 +0200

    [ResponseOps][Retry service] Move retry service to package (#247441)
    ## Summary
    
    This PR moves the retry service implemented in the case action to a
    package so it can be used by other plugins. It will be owned by the
    ResponseOps team.
    
    
    ### Checklist
    
    Check the PR satisfies following conditions. 
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/platform/packages/shared/response-ops/retry-service/retry_service.test.ts
+++ x-pack/platform/packages/shared/response-ops/retry-service/retry_service.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { loggingSystemMock } from '@kbn/core-logging-browser-mocks';
+import { loggingSystemMock } from '@kbn/core-logging-server-mocks';
 import type { Logger } from '@kbn/core/server';
 import { RetryService } from './retry_service';
 import type { BackoffFactory } from './types';
