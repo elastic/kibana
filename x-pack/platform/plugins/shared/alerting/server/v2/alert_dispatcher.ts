336be636a8bf0a5de8ff9482687a9b609539c550
Mike Cote <michel.cote@elastic.co>
2025-12-31 16:41:04 -0500

    Boilerplate to dispatch alert actions

--- x-pack/platform/plugins/shared/alerting/server/v2/alert_dispatcher.ts
+++ x-pack/platform/plugins/shared/alerting/server/v2/alert_dispatcher.ts
@@ -12,43 +12,85 @@ export interface AlertDispatcherOpts {
   esClient: ElasticsearchClient;
 }
 
+export const INTERVAL = 1000;
 export const DISPATCHER_EVENTS_QUERY = `FROM .kibana_alert_events
   | RENAME alert_series_id AS event_alert_series_id, @timestamp AS event_timestamp
   | LOOKUP JOIN .kibana_alert_actions
       ON
         rule.id == rule_id AND
           alert_series_id == event_alert_series_id AND
-          action_type == "fire"
+          action_type == "fire-event"
   | INLINE STATS last_fire = MAX(@timestamp)
         BY rule.id, event_alert_series_id
   | WHERE event_timestamp > last_fire OR last_fire IS NULL
   | KEEP event_timestamp, rule.id, event_alert_series_id
   | RENAME event_alert_series_id AS alert_series_id
   | STATS @timestamp = MAX(event_timestamp) BY rule.id, alert_series_id`;
-export const INTERVAL = 1000;
+export const DISPATCHER_ACTIONS_QUERY = `FROM .kibana_alert_actions
+  | WHERE action_type != "fire-action" AND action_type != "fire-event"
+  | RENAME alert_series_id AS action_alert_series_id, @timestamp AS action_timestamp,
+      action_type AS action_action_type, rule_id AS action_rule_id
+  | LOOKUP JOIN .kibana_alert_actions
+        ON
+          action_rule_id == rule_id AND
+            action_alert_series_id == alert_series_id AND
+            action_type == "fire-action"
+  | INLINE STATS last_fire = MAX(@timestamp)
+        BY action_rule_id, action_alert_series_id
+  | WHERE action_timestamp > last_fire OR last_fire IS NULL
+  | KEEP action_timestamp, action_rule_id, action_alert_series_id, action_action_type
+  | RENAME action_alert_series_id AS alert_series_id, action_rule_id AS rule.id
+  | STATS @timestamp = MAX(action_timestamp)
+        BY rule.id, alert_series_id, action_action_type`;
 
 export function alertDispatcher({ esClient }: AlertDispatcherOpts) {
   setInterval(async () => {
-    try {
-      const result = await esClient.esql.query({
-        query: DISPATCHER_EVENTS_QUERY,
-      });
-      const columns = result.columns.map((col) => col.name);
-      const results = result.values.map((val) => {
-        const obj: Record<string, unknown> = {};
-        for (let i = 0; i < val.length; i++) {
-          obj[columns[i]] = val[i];
-        }
-        return obj;
-      });
-      dispatchEvents(results, esClient);
-    } catch (e) {
-      console.error(`Dispatcher error: ${e.message}`);
-    }
+    await Promise.all([
+      runAlertEventDispatcher({ esClient }),
+      runAlertActionDispatcher({ esClient }),
+    ]);
   }, INTERVAL);
 }
 
-async function dispatchEvents(rows: Record<string, unknown>[], esClient: ElasticsearchClient) {
+async function runAlertEventDispatcher({ esClient }: AlertDispatcherOpts) {
+  try {
+    const result = await esClient.esql.query({
+      query: DISPATCHER_EVENTS_QUERY,
+    });
+    const columns = result.columns.map((col) => col.name);
+    const results = result.values.map((val) => {
+      const obj: Record<string, unknown> = {};
+      for (let i = 0; i < val.length; i++) {
+        obj[columns[i]] = val[i];
+      }
+      return obj;
+    });
+    dispatchAlertEvents(results, esClient);
+  } catch (e) {
+    console.error(`Dispatcher error: ${e.message}`);
+  }
+}
+
+async function runAlertActionDispatcher({ esClient }: AlertDispatcherOpts) {
+  try {
+    const result = await esClient.esql.query({
+      query: DISPATCHER_ACTIONS_QUERY,
+    });
+    const columns = result.columns.map((col) => col.name);
+    const results = result.values.map((val) => {
+      const obj: Record<string, unknown> = {};
+      for (let i = 0; i < val.length; i++) {
+        obj[columns[i]] = val[i];
+      }
+      return obj;
+    });
+    dispatchAlertActions(results, esClient);
+  } catch (e) {
+    console.error(`Dispatcher error: ${e.message}`);
+  }
+}
+
+async function dispatchAlertEvents(rows: Record<string, unknown>[], esClient: ElasticsearchClient) {
   if (rows.length === 0) return;
   try {
     const fireActions: Record<string, Record<string, Date>> = {};
@@ -74,7 +116,7 @@ async function dispatchEvents(rows: Record<string, unknown>[], esClient: Elastic
           '@timestamp': fireActions[ruleId][alertSeriesId].toISOString(),
           rule_id: ruleId,
           alert_series_id: alertSeriesId,
-          action_type: 'fire',
+          action_type: 'fire-event',
         });
       }
     }
@@ -82,7 +124,50 @@ async function dispatchEvents(rows: Record<string, unknown>[], esClient: Elastic
       index: ALERT_ACTIONS_INDEX,
       body: bulkRequest,
     });
-    console.log(`Dispatched ${rows.length} events`);
+    console.log(`Dispatched ${rows.length} alert events`);
+  } catch (e) {
+    console.error(`Failed to dispatch: ${e.message}`);
+  }
+}
+
+async function dispatchAlertActions(
+  rows: Record<string, unknown>[],
+  esClient: ElasticsearchClient
+) {
+  if (rows.length === 0) return;
+  try {
+    const fireActions: Record<string, Record<string, Date>> = {};
+    for (const row of rows) {
+      const ruleId = row['rule.id'] as string;
+      const timestamp = row['@timestamp'] as string;
+      // console.log(`Dispatching: ${JSON.stringify(row)}`);
+      if (fireActions[ruleId] === undefined) {
+        fireActions[ruleId] = [];
+      }
+      if (
+        fireActions[ruleId][row.alert_series_id] === undefined ||
+        fireActions[ruleId][row.alert_series_id] < new Date(timestamp)
+      ) {
+        fireActions[ruleId][row.alert_series_id] = new Date(timestamp);
+      }
+    }
+    const bulkRequest = [];
+    for (const ruleId of Object.keys(fireActions)) {
+      for (const alertSeriesId of Object.keys(fireActions[ruleId])) {
+        bulkRequest.push({ index: {} });
+        bulkRequest.push({
+          '@timestamp': fireActions[ruleId][alertSeriesId].toISOString(),
+          rule_id: ruleId,
+          alert_series_id: alertSeriesId,
+          action_type: 'fire-action',
+        });
+      }
+    }
+    await esClient.bulk({
+      index: ALERT_ACTIONS_INDEX,
+      body: bulkRequest,
+    });
+    console.log(`Dispatched ${rows.length} alert actions`);
   } catch (e) {
     console.error(`Failed to dispatch: ${e.message}`);
   }
