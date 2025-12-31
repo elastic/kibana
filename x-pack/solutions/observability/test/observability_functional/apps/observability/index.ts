4d0e4ceef740ab709590449ec3b1eefbd79e98ab
Miguel Martín <miguel.martin@elastic.co>
2025-12-31 13:31:22 +0000

    remove FTR tests

--- x-pack/solutions/observability/test/observability_functional/apps/observability/index.ts
+++ x-pack/solutions/observability/test/observability_functional/apps/observability/index.ts
@@ -26,7 +26,6 @@ export default function ({ loadTestFile }: FtrProviderContext) {
     loadTestFile(require.resolve('./pages/rules_page'));
     loadTestFile(require.resolve('./pages/rules_page/custom_threshold_rule'));
     loadTestFile(require.resolve('./pages/rule_details_page'));
-    loadTestFile(require.resolve('./pages/alert_details_page'));
     loadTestFile(require.resolve('./pages/alerts/metric_threshold'));
     loadTestFile(require.resolve('./sidenav/sidenav'));
   });
