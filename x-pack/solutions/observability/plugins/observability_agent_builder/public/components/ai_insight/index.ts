d86255ff4391379f6b825902235516e287c9274f
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-01-02 09:54:34 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/observability/plugins/observability_agent_builder/public/components/ai_insight/index.ts
+++ x-pack/solutions/observability/plugins/observability_agent_builder/public/components/ai_insight/index.ts
@@ -5,10 +5,6 @@
  * 2.0.
  */
 
-export {
-  AiInsight,
-  type AiInsightProps,
-  type AiInsightAttachment,
-} from './ai_insight';
+export { AiInsight, type AiInsightProps, type AiInsightAttachment } from './ai_insight';
 export { AiInsightErrorBanner, type AiInsightErrorBannerProps } from './ai_insight_error_banner';
 export { StartConversationButton } from './start_conversation_button';
