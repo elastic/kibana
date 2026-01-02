3fc080b129b740b0cdcb07389137ce8228d6e77c
Samiul Monir <samiul.monir@elastic.co>
2026-01-02 08:21:48 -0500

    fix import statements

--- x-pack/platform/plugins/shared/data_connectors/server/data_sources/notion/data_type.ts
+++ x-pack/platform/plugins/shared/data_connectors/server/data_sources/notion/data_type.ts
@@ -7,7 +7,7 @@
 
 import { i18n } from '@kbn/i18n';
 import type { DataTypeDefinition } from '@kbn/data-sources-registry-plugin';
-import { EARSSupportedOAuthProvider } from '@kbn/data-sources-registry-plugin/common';
+import { EARSSupportedOAuthProvider } from '@kbn/data-sources-registry-plugin';
 import {
   generateGetDataSourceWorkflow,
   generateGetPageWorkflow,
