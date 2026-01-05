b3f761169a2ce7ddb7a5ce86a771e93b613e785f
Kevin Delemme <kdelemme@gmail.com>
2026-01-05 10:17:08 -0500

    Remove other function

--- x-pack/solutions/observability/plugins/slo/public/components/alert_details/utils/time_range.ts
+++ x-pack/solutions/observability/plugins/slo/public/components/alert_details/utils/time_range.ts
@@ -6,16 +6,20 @@
  */
 
 import { getPaddedAlertTimeRange } from '@kbn/observability-get-padded-alert-time-range-util/src/get_padded_alert_time_range';
-import type { DateRange } from '@kbn/alerting-plugin/common';
 import {
   ALERT_END,
   ALERT_START,
-  ALERT_TIME_RANGE,
 } from '@kbn/rule-registry-plugin/common/technical_rule_data_field_names';
 import type { TimeRange } from '../../slo/error_rate_chart/use_lens_definition';
 import type { BurnRateAlert } from '../types';
 import { getActionGroupWindow } from './alert';
 
+/**
+ * Returns a time range for charting purposes, extending the alert's time range
+ * backwards by the duration of the alert's action group window.
+ * For example, if the alert was triggered for a 72-hour window (low criticality), we need to show charts with data
+ * from 72 hours before the alert's start time to provide context.
+ */
 export function getChartTimeRange(alert: BurnRateAlert): TimeRange {
   const timeRange = getPaddedAlertTimeRange(alert.fields[ALERT_START]!, alert.fields[ALERT_END]);
   const actionGroupWindow = getActionGroupWindow(alert);
@@ -23,15 +27,5 @@ export function getChartTimeRange(alert: BurnRateAlert): TimeRange {
   return {
     from: new Date(new Date(timeRange.from).getTime() - windowDurationInMs),
     to: timeRange.to ? new Date(timeRange.to) : new Date(),
-  };
-}
-
-export function getDataTimeRange(alert: BurnRateAlert): TimeRange {
-  const timeRange = alert.fields[ALERT_TIME_RANGE] as DateRange;
-  const actionGroupWindow = getActionGroupWindow(alert);
-  const windowDurationInMs = actionGroupWindow.longWindow.value * 60 * 60 * 1000;
-  return {
-    from: new Date(new Date(timeRange.gte).getTime() - windowDurationInMs),
-    to: timeRange.lte ? new Date(timeRange.lte) : new Date(),
   };
 }
