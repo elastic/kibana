16852ab4e3273b8d0598160b6a8e1f70adc7d5f0
Jonathan Budzenski <jon@elastic.co>
2026-01-05 12:24:35 -0600

    [.buildkite] Version global installs

--- .buildkite/scripts/common/setup_buildkite_deps.sh
+++ .buildkite/scripts/common/setup_buildkite_deps.sh
@@ -8,15 +8,12 @@ if [[ "$(type -t retry)" != "function" ]]; then
   source "$(dirname "${BASH_SOURCE[0]}")/util.sh"
 fi
 
-# `rm -rf <ts-node node_modules dir>; npm install -g ts-node` will cause ts-node bin files to be messed up
-# but literally just calling `npm install -g ts-node` a second time fixes it
-# this is only on newer versions of npm
-npm_install_global ts-node
-if ! ts-node --version; then
-  npm_install_global ts-node
-  ts-node --version;
-fi
-
 cd '.buildkite'
 retry 5 15 npm ci --no-audit
+GLOBAL_NODE_MODULES="$(npm root)/.bin"
 cd -
+
+export PATH="$GLOBAL_NODE_MODULES:$PATH"
+
+echo "yarn: $(yarn --version)"
+echo "ts-node: $(ts-node --version)"
