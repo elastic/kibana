09f321bb5a1d322e170a9747e7f4d895371e3dd9
Paul Tavares <paul.tavares@elastic.co>
2026-01-06 15:46:22 -0500

    Add code to check for duplicate file uploads based on hash

--- x-pack/solutions/security/plugins/security_solution/server/endpoint/services/scripts_library/scripts_library_client.ts
+++ x-pack/solutions/security/plugins/security_solution/server/endpoint/services/scripts_library/scripts_library_client.ts
@@ -258,6 +258,23 @@ export class ScriptsLibraryClient implements ScriptsLibraryClientInterface {
         fileStorage.data.hash && fileStorage.data.hash.sha256,
         new ScriptLibraryError('File hash was not generated after upload!')
       );
+
+      // Now that we have a Hash, check to ensure that file has not been uploaded before
+      const existingScriptWithSameFile =
+        await this.soClient.find<ScriptsLibrarySavedObjectAttributes>({
+          type: SCRIPTS_LIBRARY_SAVED_OBJECT_TYPE,
+          filter: this.getKueryWithPrefixedSoType(
+            `file_hash_sha256:"${fileStorage.data.hash.sha256}" AND NOT id:"${scriptId}"`
+          ),
+          perPage: 1,
+        });
+
+      if (existingScriptWithSameFile.saved_objects.length > 0) {
+        throw new ScriptLibraryError(
+          `The file you are attempting to upload (hash: [${fileStorage.data.hash.sha256}]) already exists and is associated with with a script entry named [${existingScriptWithSameFile.saved_objects[0].attributes.name}] (script ID: [${existingScriptWithSameFile.saved_objects[0].id}])`,
+          400
+        );
+      }
     } catch (error) {
       this.logger.error(`Error encountered while attempting to store file: ${error.message}`, {
         error,
