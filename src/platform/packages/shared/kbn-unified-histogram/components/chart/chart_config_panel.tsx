27ff2e9ec6a2e01575a964e4dc7cb67a03c4293a
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2026-01-07 15:35:34 +0100

    remove unnecessary prop

--- src/platform/packages/shared/kbn-unified-histogram/components/chart/chart_config_panel.tsx
+++ src/platform/packages/shared/kbn-unified-histogram/components/chart/chart_config_panel.tsx
@@ -114,7 +114,6 @@ export function ChartConfigPanel({
             setIsFlyoutVisible(false);
           }}
           wrapInFlyout
-          datasourceId="textBased"
           hidesSuggestions={currentSuggestionType !== UnifiedHistogramSuggestionType.lensSuggestion}
         />
       );
