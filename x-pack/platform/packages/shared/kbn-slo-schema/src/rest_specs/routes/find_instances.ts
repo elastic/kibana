e0c0824a85cbc6b5a13d6d099255153e825118c2
Kevin Delemme <kdelemme@gmail.com>
2026-01-07 14:53:08 -0500

    Address comment

--- x-pack/platform/packages/shared/kbn-slo-schema/src/rest_specs/routes/find_instances.ts
+++ x-pack/platform/packages/shared/kbn-slo-schema/src/rest_specs/routes/find_instances.ts
@@ -7,18 +7,19 @@
 import { toNumberRt } from '@kbn/io-ts-utils';
 import * as t from 'io-ts';
 
-const findSLOInstancesParamsSchema = t.type({
-  path: t.type({ id: t.string }),
-  query: t.union([
-    t.undefined,
-    t.partial({
+const findSLOInstancesParamsSchema = t.intersection([
+  t.type({
+    path: t.type({ id: t.string }),
+  }),
+  t.partial({
+    query: t.partial({
       search: t.string,
       size: toNumberRt,
       searchAfter: t.string,
       remoteName: t.string,
     }),
-  ]),
-});
+  }),
+]);
 
 interface FindSLOInstancesResponse {
   results: Array<{ instanceId: string; groupings: Record<string, string | number> }>;
