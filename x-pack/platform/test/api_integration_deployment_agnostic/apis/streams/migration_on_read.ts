7ba81020f3d0d945244d266f1e166d5783784004
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-01-08 15:01:52 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/migration_on_read.ts
+++ x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/migration_on_read.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 import type { Streams } from '@kbn/streams-schema';
 import {
   OBSERVABILITY_STREAMS_ENABLE_ATTACHMENTS,
