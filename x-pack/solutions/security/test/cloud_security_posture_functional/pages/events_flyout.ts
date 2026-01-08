ba7bd94987b0fdde5b6fe838221477ab0350eb72
Alberto Blázquez <alberto.blazquez@elastic.co>
2026-01-08 09:46:21 +0100

    dismiss callout in test

--- x-pack/solutions/security/test/cloud_security_posture_functional/pages/events_flyout.ts
+++ x-pack/solutions/security/test/cloud_security_posture_functional/pages/events_flyout.ts
@@ -445,6 +445,7 @@ export default function ({ getPageObjects, getService }: SecurityTelemetryFtrPro
 
         await expandedFlyoutGraph.expandGraph();
         await expandedFlyoutGraph.waitGraphIsLoaded();
+        await expandedFlyoutGraph.dismissCallout();
         await expandedFlyoutGraph.assertGraphNodesNumber(expectedNodes);
 
         const actorNodeId = '71373527ad0e2cf75e214cd168630ad1';
