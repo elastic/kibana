8b4708173bbb5eb1e9a56d7356e682ea982adafb
Alex Fernandez <fernandez.fernandez@elastic.co>
2026-01-08 13:45:59 +0100

    load data sequentially

--- x-pack/solutions/observability/plugins/infra/test/scout/ui/parallel_tests/global.setup.ts
+++ x-pack/solutions/observability/plugins/infra/test/scout/ui/parallel_tests/global.setup.ts
@@ -34,57 +34,68 @@ import { generateDockerContainersData } from '../fixtures/synthtrace/docker_cont
 globalSetupHook(
   'Ingest data to Elasticsearch',
   { tag: ['@ess', '@svlOblt'] },
-  async ({ infraSynthtraceEsClient, logsSynthtraceEsClient, apmSynthtraceEsClient }) => {
-    await Promise.all([
-      infraSynthtraceEsClient.index(
-        generateHostData({
-          from: DATE_WITH_HOSTS_DATA_FROM,
-          to: DATE_WITH_HOSTS_DATA_TO,
-          hosts: HOSTS,
-        })
-      ),
-      infraSynthtraceEsClient.index(
-        generateHostData({
-          from: DATE_WITH_HOSTS_WITHOUT_DATA_FROM,
-          to: DATE_WITH_HOSTS_WITHOUT_DATA_TO,
-          hosts: HOSTS_WITHOUT_DATA,
-        })
-      ),
-      infraSynthtraceEsClient.index(
-        generateHostsWithK8sNodeData({
-          from: DATE_WITH_K8S_HOSTS_DATA_FROM,
-          to: DATE_WITH_K8S_HOSTS_DATA_TO,
-        })
-      ),
-      infraSynthtraceEsClient.index(
-        generatePodsData({
-          from: DATE_WITH_POD_DATA_FROM,
-          to: DATE_WITH_POD_DATA_TO,
-          count: POD_COUNT,
-        })
-      ),
-      infraSynthtraceEsClient.index(
-        generateDockerContainersData({
-          from: DATE_WITH_DOCKER_DATA_FROM,
-          to: DATE_WITH_DOCKER_DATA_TO,
-          count: CONTAINER_COUNT,
-        })
-      ),
-      logsSynthtraceEsClient.index(
-        generateLogsDataForHosts({
-          from: DATE_WITH_HOSTS_DATA_FROM,
-          to: DATE_WITH_HOSTS_DATA_TO,
-          hosts: HOSTS,
-        })
-      ),
-      apmSynthtraceEsClient.index(
-        generateAddServicesToExistingHost({
-          from: DATE_WITH_HOSTS_DATA_FROM,
-          to: DATE_WITH_HOSTS_DATA_TO,
-          hostName: HOST_NAME_WITH_SERVICES,
-          servicesPerHost: SERVICE_PER_HOST_COUNT,
-        })
-      ),
-    ]);
+  async ({ infraSynthtraceEsClient, logsSynthtraceEsClient, apmSynthtraceEsClient, log }) => {
+    await infraSynthtraceEsClient.index(
+      generateHostData({
+        from: DATE_WITH_HOSTS_DATA_FROM,
+        to: DATE_WITH_HOSTS_DATA_TO,
+        hosts: HOSTS,
+      })
+    );
+    log.info('Host data indexed');
+
+    await logsSynthtraceEsClient.index(
+      generateLogsDataForHosts({
+        from: DATE_WITH_HOSTS_DATA_FROM,
+        to: DATE_WITH_HOSTS_DATA_TO,
+        hosts: HOSTS,
+      })
+    );
+    log.info('Logs data for hosts indexed');
+
+    await apmSynthtraceEsClient.index(
+      generateAddServicesToExistingHost({
+        from: DATE_WITH_HOSTS_DATA_FROM,
+        to: DATE_WITH_HOSTS_DATA_TO,
+        hostName: HOST_NAME_WITH_SERVICES,
+        servicesPerHost: SERVICE_PER_HOST_COUNT,
+      })
+    );
+    log.info('Service data for hosts indexed');
+
+    await infraSynthtraceEsClient.index(
+      generateHostData({
+        from: DATE_WITH_HOSTS_WITHOUT_DATA_FROM,
+        to: DATE_WITH_HOSTS_WITHOUT_DATA_TO,
+        hosts: HOSTS_WITHOUT_DATA,
+      })
+    );
+    log.info('Hosts without data indexed');
+
+    await infraSynthtraceEsClient.index(
+      generateHostsWithK8sNodeData({
+        from: DATE_WITH_K8S_HOSTS_DATA_FROM,
+        to: DATE_WITH_K8S_HOSTS_DATA_TO,
+      })
+    );
+    log.info('Hosts with K8s node data indexed');
+
+    await infraSynthtraceEsClient.index(
+      generatePodsData({
+        from: DATE_WITH_POD_DATA_FROM,
+        to: DATE_WITH_POD_DATA_TO,
+        count: POD_COUNT,
+      })
+    );
+    log.info('Pods data indexed');
+
+    await infraSynthtraceEsClient.index(
+      generateDockerContainersData({
+        from: DATE_WITH_DOCKER_DATA_FROM,
+        to: DATE_WITH_DOCKER_DATA_TO,
+        count: CONTAINER_COUNT,
+      })
+    );
+    log.info('Docker containers data indexed');
   }
 );
