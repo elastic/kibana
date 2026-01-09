2e9afabab4d97e06c5d679aa0b8f39779f8b8040
Ania Kowalska <ania.kowalska@elastic.co>
2026-01-09 13:30:46 +0100

    add unit tests for retainable flyout expanded state

--- src/platform/plugins/shared/discover/public/application/main/state_management/redux/internal_state.test.ts
+++ src/platform/plugins/shared/discover/public/application/main/state_management/redux/internal_state.test.ts
@@ -17,6 +17,7 @@ import {
   selectTab,
 } from '.';
 import { dataViewMock } from '@kbn/discover-utils/src/__mocks__';
+import { buildDataTableRecord } from '@kbn/discover-utils';
 import { mockControlState } from '../../../../__mocks__/esql_controls';
 import { mockCustomizationContext } from '../../../../customizations/__mocks__/customization_context';
 import { createKbnUrlStateStorage } from '@kbn/kibana-utils-plugin/public';
@@ -198,5 +199,90 @@ describe('InternalStateStore', () => {
         "fieldListExistingFieldsInfo": undefined,
       }
     `);
+  });
+
+  it('should set expandedDoc and initialDocViewerTabId for a specific tab', async () => {
+    const { store } = await createTestStore();
+    const tabId = store.getState().tabs.unsafeCurrentId;
+    const mockDoc = buildDataTableRecord({ _index: 'test', _id: 'doc1' }, dataViewMock);
+
+    expect(selectTab(store.getState(), tabId).expandedDoc).toBeUndefined();
+    expect(selectTab(store.getState(), tabId).initialDocViewerTabId).toBeUndefined();
+
+    store.dispatch(
+      internalStateActions.setExpandedDoc({
+        tabId,
+        expandedDoc: mockDoc,
+        initialDocViewerTabId: 'Table',
+      })
+    );
+
+    expect(selectTab(store.getState(), tabId).expandedDoc).toBe(mockDoc);
+    expect(selectTab(store.getState(), tabId).initialDocViewerTabId).toBe('Table');
+  });
+
+  it('should maintain separate expandedDoc state for different tabs', async () => {
+    const { store } = await createTestStore();
+    const initialTabId = store.getState().tabs.unsafeCurrentId;
+    const mockDoc1 = buildDataTableRecord({ _index: 'test', _id: 'doc1' }, dataViewMock);
+    const mockDoc2 = buildDataTableRecord({ _index: 'test', _id: 'doc2' }, dataViewMock);
+
+    store.dispatch(
+      internalStateActions.setExpandedDoc({
+        tabId: initialTabId,
+        expandedDoc: mockDoc1,
+        initialDocViewerTabId: 'Table',
+      })
+    );
+
+    await store.dispatch(
+      internalStateActions.openInNewTab({
+        tabLabel: 'Second tab',
+      })
+    );
+    const secondTabId = store.getState().tabs.unsafeCurrentId;
+
+    store.dispatch(
+      internalStateActions.setExpandedDoc({
+        tabId: secondTabId,
+        expandedDoc: mockDoc2,
+        initialDocViewerTabId: 'JSON',
+      })
+    );
+
+    expect(selectTab(store.getState(), initialTabId).expandedDoc).toBe(mockDoc1);
+    expect(selectTab(store.getState(), initialTabId).initialDocViewerTabId).toBe('Table');
+    expect(selectTab(store.getState(), secondTabId).expandedDoc).toBe(mockDoc2);
+    expect(selectTab(store.getState(), secondTabId).initialDocViewerTabId).toBe('JSON');
+  });
+
+  it('should clear expandedDoc state when resetOnSavedSearchChange is dispatched', async () => {
+    const { store } = await createTestStore();
+    const tabId = store.getState().tabs.unsafeCurrentId;
+    const mockDoc = buildDataTableRecord({ _index: 'test', _id: 'doc1' }, dataViewMock);
+
+    store.dispatch(
+      internalStateActions.setExpandedDoc({
+        tabId,
+        expandedDoc: mockDoc,
+        initialDocViewerTabId: 'Table',
+      })
+    );
+
+    expect(selectTab(store.getState(), tabId).expandedDoc).toBe(mockDoc);
+
+    store.dispatch(internalStateActions.resetOnSavedSearchChange({ tabId }));
+
+    expect(selectTab(store.getState(), tabId).expandedDoc).toBeUndefined();
+    expect(selectTab(store.getState(), tabId).initialDocViewerTabId).toBeUndefined();
+  });
+
+  it('should include expandedDoc in default tab state', async () => {
+    const { store } = await createTestStore();
+    const tabId = store.getState().tabs.unsafeCurrentId;
+    const tab = selectTab(store.getState(), tabId);
+
+    expect(tab).toHaveProperty('expandedDoc');
+    expect(tab.expandedDoc).toBeUndefined();
   });
 });
