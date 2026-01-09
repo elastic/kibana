8a698d13a42355cbb6feebb06fff535c0aa3483b
Kevin Delemme <kdelemme@gmail.com>
2026-01-09 16:51:17 -0500

    Address comments

--- x-pack/solutions/observability/plugins/slo/server/services/tasks/stale_instances_cleanup_task/stale_instances_cleanup_task.ts
+++ x-pack/solutions/observability/plugins/slo/server/services/tasks/stale_instances_cleanup_task/stale_instances_cleanup_task.ts
@@ -56,7 +56,6 @@ export class StaleInstancesCleanupTask {
             run: async () => {
               return this.runTask(taskInstance, core, abortController);
             },
-            cancel: async () => {},
           };
         },
       },
