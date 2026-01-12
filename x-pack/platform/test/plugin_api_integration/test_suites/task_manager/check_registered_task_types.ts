dc6b4c346fd89196cddb1fcdd754607f1688ad0c
Francesco Fagnani <francesco.fagnani@elastic.co>
2026-01-12 20:02:35 +0100

    fix(test): fix test

--- x-pack/platform/test/plugin_api_integration/test_suites/task_manager/check_registered_task_types.ts
+++ x-pack/platform/test/plugin_api_integration/test_suites/task_manager/check_registered_task_types.ts
@@ -227,6 +227,7 @@ export default function ({ getService }: FtrProviderContext) {
         'slo:bulk-delete-task',
         'slo:temp-summary-cleanup-task',
         'streams_feature_identification',
+        'streams_significant_events_queries_generation',
         'task_manager:delete_inactive_background_task_nodes',
         'task_manager:invalidate_api_keys',
         'task_manager:mark_removed_tasks_as_unrecognized',
