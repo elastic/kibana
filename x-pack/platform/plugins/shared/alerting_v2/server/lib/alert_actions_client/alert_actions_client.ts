b253ebae862e42767c0b2bfb809fff6f39a4be60
Kevin Delemme <kdelemme@gmail.com>
2026-01-12 13:23:12 -0500

    Fetch alert event from alert series id or throw when not found

--- x-pack/platform/plugins/shared/alerting_v2/server/lib/alert_actions_client/alert_actions_client.ts
+++ x-pack/platform/plugins/shared/alerting_v2/server/lib/alert_actions_client/alert_actions_client.ts
@@ -5,19 +5,22 @@
  * 2.0.
  */
 
-import { inject, injectable, optional } from 'inversify';
 import { PluginStart } from '@kbn/core-di';
 import { Request } from '@kbn/core-di-server';
 import type { KibanaRequest } from '@kbn/core-http-server';
+import type { ESQLSearchResponse } from '@kbn/es-types';
 import type { SecurityPluginStart } from '@kbn/security-plugin/server';
+import { inject, injectable, optional } from 'inversify';
 import type { AlertAction } from '../../routes/schemas/alert_action_schema';
 import { LoggerService } from '../services/logger_service/logger_service';
+import { QueryService } from '../services/query_service/query_service';
 
 @injectable()
 export class AlertActionsClient {
   constructor(
     @inject(Request) private readonly request: KibanaRequest,
     @inject(LoggerService) private readonly logger: LoggerService,
+    @inject(QueryService) private readonly queryService: QueryService,
     @optional() @inject(PluginStart('security')) private readonly security?: SecurityPluginStart
   ) {}
 
@@ -37,8 +40,26 @@ export class AlertActionsClient {
       }] by user [${username ?? 'unknown'}]`,
     });
 
-    // find alert by series id
-    // throw if not found
-    // perform action on alert
+    const result = await this.findLastAlertEventBySeriesIdOrThrow(params.alertSeriesId);
+
+    this.logger.info({
+      message: () =>
+        `Found alert event for series id [${params.alertSeriesId}]: ${JSON.stringify(result)}`,
+    });
+
+    // store alert action
+  }
+
+  private async findLastAlertEventBySeriesIdOrThrow(
+    alertSeriesId: string
+  ): Promise<ESQLSearchResponse> {
+    const result = await this.queryService.executeQuery({
+      query: `FROM .alerts-events | WHERE alert_series_id == "${alertSeriesId}" | SORT @timestamp DESC | LIMIT 1`,
+    });
+    if (result.values.length === 0) {
+      throw new Error(`Alert with series id [${alertSeriesId}] not found`);
+    }
+
+    return result;
   }
 }
