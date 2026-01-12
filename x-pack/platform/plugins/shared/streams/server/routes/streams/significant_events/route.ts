93fb7d2edb96624e8746de2eb364294707e62481
Francesco Fagnani <francesco.fagnani@elastic.co>
2026-01-12 14:43:36 +0100

    fix(generate endpoint): bring back generate endpoint

--- x-pack/platform/plugins/shared/streams/server/routes/streams/significant_events/route.ts
+++ x-pack/platform/plugins/shared/streams/server/routes/streams/significant_events/route.ts
@@ -4,29 +4,35 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { conflict } from '@hapi/boom';
+import { conditionSchema } from '@kbn/streamlang';
 import {
   systemSchema,
+  type SignificantEventsGenerateResponse,
   type SignificantEventsGetResponse,
   type SignificantEventsPreviewResponse,
-  type SignificantEventsQueriesGenerationTaskResult,
   type SignificantEventsQueriesGenerationResult,
+  type SignificantEventsQueriesGenerationTaskResult,
 } from '@kbn/streams-schema';
 import { z } from '@kbn/zod';
-import { conditionSchema } from '@kbn/streamlang';
-import { conflict } from '@hapi/boom';
+import { catchError, from as fromRxjs, map } from 'rxjs';
+import { STREAMS_API_PRIVILEGES } from '../../../../common/constants';
+import { PromptsConfigService } from '../../../lib/saved_objects/significant_events/prompts_config_service';
+import { generateSignificantEventDefinitions } from '../../../lib/significant_events/generate_significant_events';
+import { previewSignificantEvents } from '../../../lib/significant_events/preview_significant_events';
+import { readSignificantEventsFromAlertsIndices } from '../../../lib/significant_events/read_significant_events_from_alerts_indices';
+import { AcknowledgingIncompleteError } from '../../../lib/tasks/acknowledging_incomplete_error';
+import { CancellationInProgressError } from '../../../lib/tasks/cancellation_in_progress_error';
+import { isStale } from '../../../lib/tasks/is_stale';
 import {
   SIGNIFICANT_EVENTS_QUERIES_GENERATION_TASK_TYPE,
   type SignificantEventsQueriesGenerationTaskParams,
 } from '../../../lib/tasks/task_definitions/significant_events_queries_generation';
-import { resolveConnectorId } from '../../utils/resolve_connector_id';
-import { STREAMS_API_PRIVILEGES } from '../../../../common/constants';
-import { previewSignificantEvents } from '../../../lib/significant_events/preview_significant_events';
-import { readSignificantEventsFromAlertsIndices } from '../../../lib/significant_events/read_significant_events_from_alerts_indices';
 import { createServerRoute } from '../../create_server_route';
 import { assertSignificantEventsAccess } from '../../utils/assert_significant_events_access';
-import { AcknowledgingIncompleteError } from '../../../lib/tasks/acknowledging_incomplete_error';
-import { CancellationInProgressError } from '../../../lib/tasks/cancellation_in_progress_error';
-import { isStale } from '../../../lib/tasks/is_stale';
+import { createConnectorSSEError } from '../../utils/create_connector_sse_error';
+import { getRequestAbortSignal } from '../../utils/get_request_abort_signal';
+import { resolveConnectorId } from '../../utils/resolve_connector_id';
 
 // Make sure strings are expected for input, but still converted to a
 // Date, without breaking the OpenAPI generator
@@ -163,10 +169,116 @@ const readSignificantEventsRoute = createServerRoute({
   },
 });
 
+const generateSignificantEventsRoute = createServerRoute({
+  endpoint: 'POST /api/streams/{name}/significant_events/_generate 2023-10-31',
+  params: z.object({
+    path: z.object({ name: z.string() }),
+    query: z.object({
+      connectorId: z
+        .string()
+        .optional()
+        .describe(
+          'Optional connector ID. If not provided, the default AI connector from settings will be used.'
+        ),
+      from: dateFromString,
+      to: dateFromString,
+      sampleDocsSize: z
+        .number()
+        .optional()
+        .describe(
+          'Number of sample documents to use for generation from the current data of stream'
+        ),
+    }),
+    body: z.object({
+      system: systemSchema.optional(),
+    }),
+  }),
+  options: {
+    access: 'public',
+    summary: 'Generate significant events',
+    description: 'Generate significant events queries based on the stream data',
+    availability: {
+      since: '9.2.0',
+      stability: 'experimental',
+    },
+  },
+  security: {
+    authz: {
+      requiredPrivileges: [STREAMS_API_PRIVILEGES.read],
+    },
+  },
+  handler: async ({
+    params,
+    request,
+    getScopedClients,
+    server,
+    logger,
+  }): Promise<SignificantEventsGenerateResponse> => {
+    const {
+      streamsClient,
+      scopedClusterClient,
+      licensing,
+      inferenceClient,
+      uiSettingsClient,
+      soClient,
+    } = await getScopedClients({ request });
+
+    await assertSignificantEventsAccess({ server, licensing, uiSettingsClient });
+    await streamsClient.ensureStream(params.path.name);
+
+    const connectorId = await resolveConnectorId({
+      connectorId: params.query.connectorId,
+      uiSettingsClient,
+      logger,
+    });
+
+    const promptsConfigService = new PromptsConfigService({
+      soClient,
+      logger,
+    });
+
+    // Get connector info for error enrichment
+    const connector = await inferenceClient.getConnectorById(connectorId);
+
+    const definition = await streamsClient.getStream(params.path.name);
+
+    const { significantEventsPromptOverride } = await promptsConfigService.getPrompt();
+
+    return fromRxjs(
+      generateSignificantEventDefinitions(
+        {
+          definition,
+          system: params.body?.system,
+          connectorId,
+          start: params.query.from.valueOf(),
+          end: params.query.to.valueOf(),
+          sampleDocsSize: params.query.sampleDocsSize,
+          systemPromptOverride: significantEventsPromptOverride,
+        },
+        {
+          inferenceClient,
+          esClient: scopedClusterClient.asCurrentUser,
+          logger: logger.get('significant_events'),
+          signal: getRequestAbortSignal(request),
+        }
+      )
+    ).pipe(
+      map(({ queries, tokensUsed }) => ({
+        type: 'generated_queries' as const,
+        queries,
+        tokensUsed,
+      })),
+      catchError((error: Error) => {
+        throw createConnectorSSEError(error, connector);
+      })
+    );
+  },
+});
+
 const significantEventsQueriesGenerationStatusRoute = createServerRoute({
   endpoint: 'GET /api/streams/{name}/significant_events/_status 2023-10-31',
   params: z.object({
-    path: z.object({ name: z.string() }),
+    path: z.object({ name: z.string().describe('The name of the stream') }),
   }),
   options: {
     access: 'public',
@@ -174,7 +286,7 @@ const significantEventsQueriesGenerationStatusRoute = createServerRoute({
     description:
       'Significant events query generation happens as a background task, this endpoint allows the user to check the status of this task. This endpoint combines with POST /api/streams/{name}/significant_events/_task which manages the task lifecycle.',
     availability: {
-      since: '9.2.0',
+      since: '9.4.0',
       stability: 'experimental',
     },
   },
@@ -227,12 +339,12 @@ const significantEventsQueriesGenerationStatusRoute = createServerRoute({
 const significantEventsQueriesGenerationTaskRoute = createServerRoute({
   endpoint: 'POST /api/streams/{name}/significant_events/_task 2023-10-31',
   params: z.object({
-    path: z.object({ name: z.string() }),
+    path: z.object({ name: z.string().describe('The name of the stream') }),
     body: z.discriminatedUnion('action', [
       z.object({
-        action: z.literal('schedule'),
-        from: dateFromString,
-        to: dateFromString,
+        action: z.literal('schedule').describe('Schedule a new generation task'),
+        from: dateFromString.describe('Start of the time range'),
+        to: dateFromString.describe('End of the time range'),
         connectorId: z
           .string()
           .optional()
@@ -245,13 +357,13 @@ const significantEventsQueriesGenerationTaskRoute = createServerRoute({
           .describe(
             'Number of sample documents to use for generation from the current data of stream'
           ),
-        systems: z.array(systemSchema).optional(),
+        systems: z.array(systemSchema).optional().describe('Optional array of systems'),
       }),
       z.object({
-        action: z.literal('cancel'),
+        action: z.literal('cancel').describe('Cancel an in-progress generation task'),
       }),
       z.object({
-        action: z.literal('acknowledge'),
+        action: z.literal('acknowledge').describe('Acknowledge a completed generation task'),
       }),
     ]),
   }),
@@ -261,7 +373,7 @@ const significantEventsQueriesGenerationTaskRoute = createServerRoute({
     description:
       'Generate significant events queries based on the stream data, this happens as a background task and this endpoint manages the task lifecycle.',
     availability: {
-      since: '9.2.0',
+      since: '9.4.0',
       stability: 'experimental',
     },
   },
@@ -361,6 +473,7 @@ const significantEventsQueriesGenerationTaskRoute = createServerRoute({
 export const significantEventsRoutes = {
   ...readSignificantEventsRoute,
   ...previewSignificantEventsRoute,
+  ...generateSignificantEventsRoute,
   ...significantEventsQueriesGenerationStatusRoute,
   ...significantEventsQueriesGenerationTaskRoute,
 };
