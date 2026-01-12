226ef4ffdc715bd44cb31d5174a072c5fb50228f
Gerard Soldevila <gerard.soldevila@elastic.co>
2026-01-12 18:16:44 +0100

    Apply missing fixes

--- x-pack/platform/plugins/shared/index_management/server/routes/api/templates/lib.ts
+++ x-pack/platform/plugins/shared/index_management/server/routes/api/templates/lib.ts
@@ -42,15 +42,8 @@ export const saveTemplate = async ({
     : serializeTemplate(template, dataStreamOptions);
 
   if (isLegacy) {
-    const {
-      order,
-
-      index_patterns,
-      version,
-      settings,
-      mappings,
-      aliases,
-    } = serializedTemplate as LegacyTemplateSerialized;
+    const { order, index_patterns, version, settings, mappings, aliases } =
+      serializedTemplate as LegacyTemplateSerialized;
 
     return await client.asCurrentUser.indices.putTemplate({
       name: template.name,
