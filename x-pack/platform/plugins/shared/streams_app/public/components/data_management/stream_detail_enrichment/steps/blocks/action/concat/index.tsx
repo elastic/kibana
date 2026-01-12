17175b8a0109e9a65eb6990786a73d800290c92c
Quentin Couvelaire <quentin.couvelaire@elastic.co>
2026-01-12 16:26:04 -0500

    basic ui with non-functional form

---
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/steps/blocks/action/concat/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiSpacer } from '@elastic/eui';
+import React from 'react';
+import { IgnoreFailureToggle } from '../ignore_toggles';
+
+export const ConcatProcessorForm = () => {
+  return (
+    <>
+      TODO: Implement Concat Processor Form
+      <EuiSpacer size="m" />
+      <IgnoreFailureToggle />
+    </>
+  );
+};
