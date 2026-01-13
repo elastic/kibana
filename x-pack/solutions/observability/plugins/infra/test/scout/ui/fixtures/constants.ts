1b4cf87a5f19d0e12a2c4ad15d2d212f721e36b9
Alex Fernandez <fernandez.fernandez@elastic.co>
2026-01-13 11:44:56 +0100

    fix dates

--- x-pack/solutions/observability/plugins/infra/test/scout/ui/fixtures/constants.ts
+++ x-pack/solutions/observability/plugins/infra/test/scout/ui/fixtures/constants.ts
@@ -48,9 +48,9 @@ export const HOSTS = [
   },
 ];
 
-export const DATE_WITH_HOSTS_WITHOUT_DATA_FROM = '2023-03-28T18:20:00.000Z';
-export const DATE_WITH_HOSTS_WITHOUT_DATA_TO = '2023-03-28T18:21:00.000Z';
-export const DATE_WITH_HOSTS_WITHOUT_DATA = '03/28/2023 8:20:59 PM';
+export const DATE_WITH_HOSTS_WITHOUT_DATA_FROM = '2023-03-29T18:20:00.000Z';
+export const DATE_WITH_HOSTS_WITHOUT_DATA_TO = '2023-03-29T18:21:00.000Z';
+export const DATE_WITH_HOSTS_WITHOUT_DATA = '03/29/2023 8:20:59 PM';
 
 export const HOST7_NAME = 'host-7';
 
@@ -60,9 +60,9 @@ export const HOSTS_WITHOUT_DATA = [
   },
 ];
 
-export const DATE_WITH_K8S_HOSTS_DATA_FROM = '2023-03-28T18:20:00.000Z';
-export const DATE_WITH_K8S_HOSTS_DATA_TO = '2023-03-28T18:21:00.000Z';
-export const DATE_WITH_K8S_HOSTS_DATA = '03/28/2023 8:20:59 PM';
+export const DATE_WITH_K8S_HOSTS_DATA_FROM = '2023-03-30T18:20:00.000Z';
+export const DATE_WITH_K8S_HOSTS_DATA_TO = '2023-03-30T18:21:00.000Z';
+export const DATE_WITH_K8S_HOSTS_DATA = '03/30/2023 8:20:59 PM';
 
 export const K8S_POD_NAME = 'demo-stack-kubernetes-pod-1';
 export const K8S_HOST_NAME = 'demo-stack-kubernetes-01';
@@ -76,18 +76,18 @@ export const K8S_HOSTS = [
   },
 ];
 
-export const DATE_WITH_DOCKER_DATA_FROM = '2023-03-28T18:20:00.000Z';
-export const DATE_WITH_DOCKER_DATA_TO = '2023-03-28T18:21:00.000Z';
-export const DATE_WITH_DOCKER_DATA = '03/28/2023 8:20:59 PM';
+export const DATE_WITH_DOCKER_DATA_FROM = '2023-03-31T18:20:00.000Z';
+export const DATE_WITH_DOCKER_DATA_TO = '2023-03-31T18:21:00.000Z';
+export const DATE_WITH_DOCKER_DATA = '03/31/2023 8:20:59 PM';
 export const CONTAINER_COUNT = 1;
 export const CONTAINER_NAMES = Array.from(
   { length: CONTAINER_COUNT },
   (_, i) => `container-container-${i}`
 );
 
-export const DATE_WITH_POD_DATA_FROM = '2023-03-28T18:20:00.000Z';
-export const DATE_WITH_POD_DATA_TO = '2023-03-28T18:21:00.000Z';
-export const DATE_WITH_POD_DATA = '03/28/2023 8:20:59 PM';
+export const DATE_WITH_POD_DATA_FROM = '2023-04-01T18:20:00.000Z';
+export const DATE_WITH_POD_DATA_TO = '2023-04-01T18:21:00.000Z';
+export const DATE_WITH_POD_DATA = '04/01/2023 8:20:59 PM';
 export const POD_COUNT = 1;
 export const POD_NAMES = Array.from({ length: POD_COUNT }, (_, i) => `pod-${i}`);
 
