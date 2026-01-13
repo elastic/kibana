c5e89a5abe87274687c4299f231695b2f87ef25f
Saikat Sarkar <saikat.sarkar@elastic.co>
2026-01-13 10:48:51 -0700

    Remove FTR tests for getting_started page

--- x-pack/solutions/search/test/functional_search/index.ts
+++ x-pack/solutions/search/test/functional_search/index.ts
@@ -17,6 +17,5 @@ export default ({ loadTestFile }: FtrProviderContext): void => {
     loadTestFile(require.resolve('./tests/search_index_details'));
     loadTestFile(require.resolve('./tests/index_management'));
     loadTestFile(require.resolve('./tests/inference_management'));
-    loadTestFile(require.resolve('./tests/search_getting_started'));
   });
 };
