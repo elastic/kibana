c61cd1259abe5391e1316bf8df8a3464a788ed5c
pgayvallet <pierre.gayvallet@elastic.co>
2026-01-14 13:30:35 +0100

    Add tests for gemini adapter

--- x-pack/platform/plugins/shared/inference/server/chat_complete/adapters/gemini/gemini_adapter.test.ts
+++ x-pack/platform/plugins/shared/inference/server/chat_complete/adapters/gemini/gemini_adapter.test.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { processVertexStreamMock } from './gemini_adapter.test.mocks';
+import { processVertexStreamMock, processVertexResponseMock } from './gemini_adapter.test.mocks';
 import { PassThrough } from 'stream';
 import { noop, tap, lastValueFrom, toArray, of } from 'rxjs';
 import { loggerMock } from '@kbn/logging-mocks';
@@ -35,6 +35,7 @@ describe('geminiAdapter', () => {
       capabilities: {},
     });
     processVertexStreamMock.mockReset().mockImplementation(() => tap(noop));
+    processVertexResponseMock.mockReset().mockImplementation(() => tap(noop));
   });
 
   function getCallParams() {
@@ -75,7 +76,7 @@ describe('geminiAdapter', () => {
 
       expect(executorMock.invoke).toHaveBeenCalledTimes(1);
       expect(executorMock.invoke).toHaveBeenCalledWith({
-        subAction: 'invokeStream',
+        subAction: 'invokeAIRaw',
         subActionParams: {
           messages: [
             {
@@ -528,40 +529,6 @@ describe('geminiAdapter', () => {
       expect(toolConfig).toEqual({ mode: 'ANY', allowedFunctionNames: ['foobar'] });
     });
 
-    it('process response events via processVertexStream', async () => {
-      const source$ = of({ chunk: 1 }, { chunk: 2 });
-
-      const tapFn = jest.fn();
-      processVertexStreamMock.mockImplementation(() => tap(tapFn));
-
-      executorMock.invoke.mockImplementation(async () => {
-        return {
-          actionId: '',
-          status: 'ok',
-          data: observableIntoEventSourceStream(source$, logger),
-        };
-      });
-
-      const response$ = geminiAdapter.chatComplete({
-        logger,
-        executor: executorMock,
-        messages: [
-          {
-            role: MessageRole.User,
-            content: 'question',
-          },
-        ],
-      });
-
-      const allChunks = await lastValueFrom(response$.pipe(toArray()));
-
-      expect(allChunks).toEqual([{ chunk: 1 }, { chunk: 2 }]);
-
-      expect(tapFn).toHaveBeenCalledTimes(2);
-      expect(tapFn).toHaveBeenCalledWith({ chunk: 1 });
-      expect(tapFn).toHaveBeenCalledWith({ chunk: 2 });
-    });
-
     it('propagates the abort signal when provided', () => {
       const abortController = new AbortController();
 
@@ -576,7 +543,7 @@ describe('geminiAdapter', () => {
 
       expect(executorMock.invoke).toHaveBeenCalledTimes(1);
       expect(executorMock.invoke).toHaveBeenCalledWith({
-        subAction: 'invokeStream',
+        subAction: 'invokeAIRaw',
         subActionParams: expect.objectContaining({
           signal: abortController.signal,
         }),
@@ -595,7 +562,7 @@ describe('geminiAdapter', () => {
 
       expect(executorMock.invoke).toHaveBeenCalledTimes(1);
       expect(executorMock.invoke).toHaveBeenCalledWith({
-        subAction: 'invokeStream',
+        subAction: 'invokeAIRaw',
         subActionParams: expect.objectContaining({
           temperature: 0.6,
         }),
@@ -614,36 +581,136 @@ describe('geminiAdapter', () => {
 
       expect(executorMock.invoke).toHaveBeenCalledTimes(1);
       expect(executorMock.invoke).toHaveBeenCalledWith({
-        subAction: 'invokeStream',
+        subAction: 'invokeAIRaw',
         subActionParams: expect.objectContaining({
           model: 'gemini-1.5',
         }),
       });
     });
 
-    it('throws an error if the connector response is in error', async () => {
-      executorMock.invoke.mockImplementation(async () => {
-        return {
-          actionId: 'actionId',
-          status: 'error',
-          serviceMessage: 'something went wrong',
-          data: undefined,
-        };
+    describe('non-streaming mode', () => {
+      it('process response events via processVertexResponse', async () => {
+        const response = { dummy: 'response' };
+
+        const tapFn = jest.fn();
+        processVertexResponseMock.mockImplementation(() => tap(tapFn));
+
+        executorMock.invoke.mockImplementation(async () => {
+          return {
+            actionId: '',
+            status: 'ok',
+            data: response,
+          };
+        });
+
+        const response$ = geminiAdapter.chatComplete({
+          stream: false,
+          logger,
+          executor: executorMock,
+          messages: [
+            {
+              role: MessageRole.User,
+              content: 'question',
+            },
+          ],
+        });
+
+        const events = await lastValueFrom(response$.pipe(toArray()));
+
+        expect(tapFn).toHaveBeenCalledTimes(1);
+        expect(tapFn).toHaveBeenCalledWith(response);
+
+        expect(events).toEqual([response]);
       });
 
-      await expect(
-        lastValueFrom(
-          geminiAdapter
-            .chatComplete({
-              logger,
-              executor: executorMock,
-              messages: [{ role: MessageRole.User, content: 'Hello' }],
-            })
-            .pipe(toArray())
-        )
-      ).rejects.toThrowErrorMatchingInlineSnapshot(
-        `"Error calling connector: something went wrong"`
-      );
+      it('throws an error if the connector response is in error', async () => {
+        executorMock.invoke.mockImplementation(async () => {
+          return {
+            actionId: 'actionId',
+            status: 'error',
+            serviceMessage: 'something went wrong',
+            data: undefined,
+          };
+        });
+
+        await expect(
+          lastValueFrom(
+            geminiAdapter
+              .chatComplete({
+                stream: false,
+                logger,
+                executor: executorMock,
+                messages: [{ role: MessageRole.User, content: 'Hello' }],
+              })
+              .pipe(toArray())
+          )
+        ).rejects.toThrowErrorMatchingInlineSnapshot(
+          `"Error calling connector: something went wrong"`
+        );
+      });
+    });
+
+    describe('streaming mode', () => {
+      it('process response events via processVertexStream', async () => {
+        const source$ = of({ chunk: 1 }, { chunk: 2 });
+
+        const tapFn = jest.fn();
+        processVertexStreamMock.mockImplementation(() => tap(tapFn));
+
+        executorMock.invoke.mockImplementation(async () => {
+          return {
+            actionId: '',
+            status: 'ok',
+            data: observableIntoEventSourceStream(source$, logger),
+          };
+        });
+
+        const response$ = geminiAdapter.chatComplete({
+          stream: true,
+          logger,
+          executor: executorMock,
+          messages: [
+            {
+              role: MessageRole.User,
+              content: 'question',
+            },
+          ],
+        });
+
+        const allChunks = await lastValueFrom(response$.pipe(toArray()));
+
+        expect(allChunks).toEqual([{ chunk: 1 }, { chunk: 2 }]);
+
+        expect(tapFn).toHaveBeenCalledTimes(2);
+        expect(tapFn).toHaveBeenCalledWith({ chunk: 1 });
+        expect(tapFn).toHaveBeenCalledWith({ chunk: 2 });
+      });
+
+      it('throws an error if the connector response is in error', async () => {
+        executorMock.invoke.mockImplementation(async () => {
+          return {
+            actionId: 'actionId',
+            status: 'error',
+            serviceMessage: 'something went wrong',
+            data: undefined,
+          };
+        });
+
+        await expect(
+          lastValueFrom(
+            geminiAdapter
+              .chatComplete({
+                stream: true,
+                logger,
+                executor: executorMock,
+                messages: [{ role: MessageRole.User, content: 'Hello' }],
+              })
+              .pipe(toArray())
+          )
+        ).rejects.toThrowErrorMatchingInlineSnapshot(
+          `"Error calling connector: something went wrong"`
+        );
+      });
     });
   });
 });
