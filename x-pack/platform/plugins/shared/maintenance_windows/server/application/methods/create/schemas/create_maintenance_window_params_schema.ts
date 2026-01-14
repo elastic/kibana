cf05de2fdbb40893302a1fc43af6883eacb7aa94
Janki Salvi <jankigaurav.salvi@elastic.co>
2026-01-14 14:24:24 +0000

    update create params schema

--- x-pack/platform/plugins/shared/maintenance_windows/server/application/methods/create/schemas/create_maintenance_window_params_schema.ts
+++ x-pack/platform/plugins/shared/maintenance_windows/server/application/methods/create/schemas/create_maintenance_window_params_schema.ts
@@ -6,9 +6,9 @@
  */
 
 import { schema } from '@kbn/config-schema';
-import { scheduleSchema } from '../../../schemas/schedule';
 import { alertsFilterQuerySchema } from '../../../schemas/alerts_filter_query_schemas';
 import { rRuleRequestSchema } from '../../../../routes/apis/r_rule/request/schemas/v1';
+import { scheduleRequestSchema } from '../../../../routes/schemas/schedule';
 import { maintenanceWindowCategoryIdsSchema } from '../../../schemas/maintenance_window';
 
 export const createMaintenanceWindowParamsSchema = schema.object({
@@ -19,7 +19,7 @@ export const createMaintenanceWindowParamsSchema = schema.object({
     categoryIds: maintenanceWindowCategoryIdsSchema,
     scopedQuery: schema.maybe(schema.nullable(alertsFilterQuerySchema)),
     enabled: schema.maybe(schema.boolean({ defaultValue: true })),
-    schedule: schema.object({ custom: scheduleSchema }),
+    schedule: schema.object({ custom: scheduleRequestSchema }),
     scope: schema.maybe(
       schema.object({
         alerting: schema.nullable(alertsFilterQuerySchema),
