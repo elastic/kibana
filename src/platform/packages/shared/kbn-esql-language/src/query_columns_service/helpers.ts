759115cf02bcfd296c8cd6df30d2b97999b0f507
Sebastian Delle Donne <sebastian.delledonne@elastic.co>
2026-01-15 10:08:59 +0100

    fix typos
    Co-authored-by: Stratou <stratoula1@gmail.com>

--- src/platform/packages/shared/kbn-esql-language/src/query_columns_service/helpers.ts
+++ src/platform/packages/shared/kbn-esql-language/src/query_columns_service/helpers.ts
@@ -130,7 +130,7 @@ export function getUnmappedFields(
         unmappedFields.push({
           name: node.parts.join('.'),
           // TODO: handle unmapped fields types correctly. For now we treat them as 'unknown'.
-          // If the stratetgy is NULLIFY return 'null', this can't be done today as null is not well supported on function definitions.
+          // If the strategy is NULLIFY return 'null', this can't be done today as null is not well supported on function definitions.
           // Depends on https://github.com/elastic/elasticsearch/issues/140575
           // For LOAD strategy it's still not clear which type will come from ES if the source data is not compatible with keyword.
           type: 'unknown',
