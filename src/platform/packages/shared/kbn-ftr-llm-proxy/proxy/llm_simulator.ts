3eb45bcfa87da5bb1c4a34df5f855f4f39036e28
pgayvallet <pierre.gayvallet@elastic.co>
2026-01-15 11:06:36 +0100

    create @kbn/ftr-llm-proxy

--- src/platform/packages/shared/kbn-ftr-llm-proxy/proxy/llm_simulator.ts
+++ src/platform/packages/shared/kbn-ftr-llm-proxy/proxy/llm_simulator.ts
@@ -1,14 +1,16 @@
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
 import { once } from 'lodash';
 import type { ChatCompletionStreamParams } from 'openai/lib/ChatCompletionStream';
 import type { ToolingLog } from '@kbn/tooling-log';
-import { createOpenAiChunk, createOpenAIResponse } from './create_openai_chunk';
+import { createOpenAiChunk, createOpenAIResponse } from './create_response';
 import type { HttpResponse, LLMMessage, ToolMessage } from './types';
 
 /**
---
+++ src/platform/packages/shared/kbn-ftr-llm-proxy/proxy/types.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import type http from 'http';
+import type { ChatCompletionMessage } from 'openai/resources';
+
+export type HttpRequest = http.IncomingMessage;
+export type HttpResponse = http.ServerResponse<http.IncomingMessage>;
+
+export type ToolMessage = Omit<ChatCompletionMessage, 'refusal'>;
+export type LLMMessage = string | string[] | ToolMessage | undefined;
