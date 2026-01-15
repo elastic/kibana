6298074ec3a37edb0dcf986ac5ecb5909df8a5de
AlexGPlay <alexgp1234@gmail.com>
2026-01-15 10:15:46 +0100

    Revert to code previous to controls anywhere

--- src/platform/plugins/shared/discover/public/embeddable/utils/get_discover_locator_params.ts
+++ src/platform/plugins/shared/discover/public/embeddable/utils/get_discover_locator_params.ts
@@ -10,22 +10,13 @@
 import type { AggregateQuery, Query } from '@kbn/es-query';
 import { isOfAggregateQueryType, type Filter } from '@kbn/es-query';
 import {
-  apiHasSerializableState,
-  apiHasType,
-  apiHasUniqueId,
   type HasParentApi,
   type PublishesSavedObjectId,
   type PublishesUnifiedSearch,
 } from '@kbn/presentation-publishing';
 import { getESQLQueryVariables } from '@kbn/esql-utils';
 import { ESQL_CONTROL } from '@kbn/controls-constants';
-import type { ESQLControlState } from '@kbn/esql-types';
-import type { Serializable, SerializableRecord } from '@kbn/utility-types';
-import type { ControlPanelsState } from '@kbn/control-group-renderer';
-import type { PresentationContainer } from '@kbn/presentation-containers';
-import { apiIsPresentationContainer } from '@kbn/presentation-containers';
-import { apiPublishesControlsLayout } from '@kbn/controls-renderer';
-import { omit } from 'lodash';
+import { isApiESQLVariablesCompatible } from '@kbn/lens-plugin/public/react_embeddable/type_guards';
 import { type PublishesSavedSearch } from '../types';
 import type { DiscoverAppLocatorParams } from '../../../common';
 
@@ -38,18 +29,13 @@ export const getDiscoverLocatorParams = (
 
   const dataView = savedSearch?.searchSource.getField('index');
   const savedObjectId = api.savedObjectId$?.getValue();
-  const presentationContainer = apiIsPresentationContainer(api.parentApi)
-    ? api.parentApi
-    : undefined;
 
   const locatorParams: DiscoverAppLocatorParams = savedObjectId
     ? { savedSearchId: savedObjectId }
     : {
         dataViewId: dataView?.id,
         dataViewSpec: dataView?.toMinimalSpec(),
-        esqlControls: presentationContainer
-          ? getEsqlControls(presentationContainer, query)
-          : undefined,
+        esqlControls: getEsqlControls(api.parentApi, query),
         timeRange: savedSearch?.timeRange,
         refreshInterval: savedSearch?.refreshInterval,
         filters: savedSearch?.searchSource.getField('filter') as Filter[],
@@ -63,51 +49,37 @@ export const getDiscoverLocatorParams = (
   return locatorParams;
 };
 
-function getEsqlControls(
-  presentationContainer: PresentationContainer,
-  query: AggregateQuery | Query | undefined
-) {
+function getEsqlControls(embeddableParentApi: unknown, query: AggregateQuery | Query | undefined) {
   if (!isOfAggregateQueryType(query)) return;
+  if (!isApiESQLVariablesCompatible(embeddableParentApi)) return undefined;
+
+  const controlGroupApi = embeddableParentApi.controlGroupApi$.getValue();
+  if (!controlGroupApi) return undefined;
+
+  const serializedState = controlGroupApi.serializeState?.();
+  if (!serializedState) return undefined;
 
   const usedVariables = getESQLQueryVariables(query.esql);
-  const controlsLayout = apiPublishesControlsLayout(presentationContainer)
-    ? presentationContainer.layout$.getValue().controls
-    : {};
 
-  const esqlControlState = Object.values(presentationContainer.children$.getValue()).reduce(
-    (acc: { [uuid: string]: Serializable }, api, index) => {
-      if (
-        !(
-          apiHasType(api) &&
-          api.type === ESQL_CONTROL &&
-          apiHasUniqueId(api) &&
-          apiHasSerializableState(api)
-        )
-      ) {
-        return acc;
-      }
+  return serializedState.rawState.controls.reduce((acc, control) => {
+    if (!control.id) return acc;
+    if (control.type !== ESQL_CONTROL) return acc; // only include ESQL controls
+    if (!control.controlConfig) return acc;
 
-      const controlState = api.serializeState() as ESQLControlState;
-      const variableName = 'variableName' in controlState && (controlState.variableName as string);
-      if (!variableName) return acc;
-      const isUsed = usedVariables.includes(variableName);
-      if (!isUsed) return acc;
+    const variableName =
+      'variableName' in control.controlConfig && (control.controlConfig.variableName as string);
+    if (!variableName) return acc;
 
-      return {
-        ...acc,
-        [api.uuid]: {
-          type: api.type,
-          ...controlState,
-          ...(controlsLayout[api.uuid]
-            ? {
-                ...omit(controlsLayout[api.uuid], 'type'),
-              }
-            : { order: index }),
-        },
-      };
-    },
-    {}
-  );
+    const isUsed = usedVariables.includes(variableName);
+    if (!isUsed) return acc;
 
-  return esqlControlState as ControlPanelsState<ESQLControlState> & SerializableRecord;
+    return {
+      ...acc,
+      [control.id]: {
+        ...control.controlConfig,
+        type: control.type,
+        order: control.order,
+      },
+    };
+  }, {});
 }
