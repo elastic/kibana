91153ecc4cb76e3902de3129fc868c47f61d8f52
AlexGPlay <alex.garcia@elastic.co>
2026-01-15 12:27:15 +0100

    Migrate schemas to model Versions

--- src/platform/plugins/shared/data/server/search/saved_objects/search_session.ts
+++ src/platform/plugins/shared/data/server/search/saved_objects/search_session.ts
@@ -7,11 +7,15 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { schema } from '@kbn/config-schema';
 import { ANALYTICS_SAVED_OBJECT_INDEX } from '@kbn/core-saved-objects-server';
 import type { SavedObjectsType } from '@kbn/core/server';
 import { SEARCH_SESSION_TYPE } from '../../../common';
 import { searchSessionSavedObjectMigrations } from './search_session_migration';
+import {
+  SCHEMA_SEARCH_SESSION_V1,
+  SCHEMA_SEARCH_SESSION_V2,
+  SCHEMA_SEARCH_SESSION_V8_8_O,
+} from './search_session_schema';
 
 export const searchSessionSavedObjectType: SavedObjectsType = {
   name: SEARCH_SESSION_TYPE,
@@ -41,33 +45,31 @@ export const searchSessionSavedObjectType: SavedObjectsType = {
       },
     },
   },
+  modelVersions: {
+    1: {
+      changes: [],
+      schemas: {
+        forwardCompatibility: SCHEMA_SEARCH_SESSION_V1.extends({}, { unknowns: 'ignore' }),
+        create: SCHEMA_SEARCH_SESSION_V1,
+      },
+    },
+    2: {
+      changes: [
+        {
+          type: 'mappings_addition',
+          addedMappings: {
+            status: { type: 'keyword' },
+          },
+        },
+      ],
+      schemas: {
+        forwardCompatibility: SCHEMA_SEARCH_SESSION_V2.extends({}, { unknowns: 'ignore' }),
+        create: SCHEMA_SEARCH_SESSION_V2,
+      },
+    },
+  },
   schemas: {
-    '8.8.0': schema.object({
-      sessionId: schema.string(),
-      status: schema.maybe(schema.string()),
-      name: schema.maybe(schema.string()),
-      created: schema.string(),
-      expires: schema.string(),
-      appId: schema.maybe(schema.string()),
-      locatorId: schema.maybe(schema.string()),
-      initialState: schema.maybe(schema.object({}, { unknowns: 'allow' })),
-      restoreState: schema.maybe(schema.object({}, { unknowns: 'allow' })),
-      idMapping: schema.mapOf(
-        schema.string(),
-        schema.object({
-          id: schema.string(),
-          strategy: schema.string(),
-          status: schema.maybe(schema.string()),
-          startedAt: schema.maybe(schema.string()),
-          completedAt: schema.maybe(schema.string()),
-        })
-      ),
-      realmType: schema.maybe(schema.string()),
-      realmName: schema.maybe(schema.string()),
-      username: schema.maybe(schema.string()),
-      version: schema.string(),
-      isCanceled: schema.maybe(schema.boolean()),
-    }),
+    '8.8.0': SCHEMA_SEARCH_SESSION_V8_8_O,
   },
   migrations: searchSessionSavedObjectMigrations,
   excludeOnUpgrade: async () => {
