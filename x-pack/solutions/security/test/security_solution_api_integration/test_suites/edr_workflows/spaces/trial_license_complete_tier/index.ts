d14addd4eb09a0a40674a8f53de24b6224a5ac7a
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2026-01-15 10:18:00 +0100

    skip failing test suite (#249143)

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/spaces/trial_license_complete_tier/index.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/edr_workflows/spaces/trial_license_complete_tier/index.ts
@@ -13,7 +13,8 @@ import { ROLE } from '../../../../config/services/security_solution_edr_workflow
 export default function endpointAPIIntegrationTests(providerContext: FtrProviderContext) {
   const { loadTestFile, getService } = providerContext;
 
-  describe('Endpoint plugin spaces support', function () {
+  // Failing: See https://github.com/elastic/kibana/issues/249143
+  describe.skip('Endpoint plugin spaces support', function () {
     const ingestManager = getService('ingestManager');
     const rolesUsersProvider = getService('rolesUsersProvider');
     const kbnClient = getService('kibanaServer');
