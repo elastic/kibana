90d6fa8c0b624db195aaed3848fb1a19dc781673
Bryce Buchanan <75274611+bryce-b@users.noreply.github.com>
2026-01-15 09:15:59 -0800

    Update layer_types.ts
    I don't believe this file should have ever been updated in the backport.

--- x-pack/platform/plugins/shared/lens/common/layer_types.ts
+++ x-pack/platform/plugins/shared/lens/common/layer_types.ts
@@ -5,23 +5,9 @@
  * 2.0.
  */
 
-import Path from 'node:path';
-
 export const layerTypes = {
   DATA: 'data',
   REFERENCELINE: 'referenceLine',
   ANNOTATIONS: 'annotations',
   METRIC_TRENDLINE: 'metricTrendline',
 } as const;
-
-// Test data time range constants
-export const PROFILING_TEST_DATES = {
-  rangeFrom: '2023-04-18T00:00:00.000Z',
-  rangeTo: '2023-04-18T00:00:30.000Z',
-} as const;
-
-export const APM_AGENT_POLICY_ID = 'policy-elastic-agent-on-cloud';
-export const COLLECTOR_PACKAGE_POLICY_NAME = 'elastic-universal-profiling-collector';
-export const SYMBOLIZER_PACKAGE_POLICY_NAME = 'elastic-universal-profiling-symbolizer';
-export const esArchiversPath = Path.join(__dirname, 'es_archiver', 'profiling', 'data.json');
-export const esResourcesEndpoint = 'api/profiling/setup/es_resources';
