33127635830541e5be71170b00951003e0fbceb1
Jean-Louis Leysens <jloleysens@gmail.com>
2026-01-15 14:56:41 +0100

    slightly more safe tests and small re-ordering

--- x-pack/platform/plugins/shared/licensing/server/on_pre_response_handler.test.ts
+++ x-pack/platform/plugins/shared/licensing/server/on_pre_response_handler.test.ts
@@ -4,9 +4,9 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { of, BehaviorSubject, NEVER } from 'rxjs';
 import { createOnPreResponseHandler } from './on_pre_response_handler';
 import { httpServiceMock, httpServerMock } from '@kbn/core/server/mocks';
-import { of, BehaviorSubject, NEVER } from 'rxjs';
 import { licenseMock } from '../common/licensing.mock';
 
 describe('createOnPreResponseHandler', () => {
@@ -17,24 +17,6 @@ describe('createOnPreResponseHandler', () => {
   afterAll(() => {
     jest.useRealTimers();
   });
-
-  it('sets unknown if license.signature is unset', async () => {
-    const refresh = jest.fn();
-    const toolkit = httpServiceMock.createOnPreResponseToolkit();
-    const noLicense$ = NEVER;
-
-    const interceptor = createOnPreResponseHandler(refresh, noLicense$);
-    await interceptor(httpServerMock.createKibanaRequest(), { statusCode: 200 }, toolkit);
-
-    expect(refresh).toHaveBeenCalledTimes(0);
-    expect(toolkit.next).toHaveBeenCalledTimes(1);
-    expect(toolkit.next).toHaveBeenCalledWith({
-      headers: {
-        'kbn-license-sig': 'unknown',
-      },
-    });
-  });
-
   it('sets license.signature header immediately for non-error responses', async () => {
     const refresh = jest.fn();
     const toolkit = httpServiceMock.createOnPreResponseToolkit();
@@ -74,10 +56,13 @@ describe('createOnPreResponseHandler', () => {
   it('sets license.signature header after refresh for other error responses', async () => {
     const updatedLicense = licenseMock.createLicense({ signature: 'bar' });
     const license$ = new BehaviorSubject(licenseMock.createLicense({ signature: 'foo' }));
-    const refresh = jest.fn().mockImplementation(async () => {
-      setTimeout(() => {
-        license$.next(updatedLicense);
-      }, 1);
+    const refresh = jest.fn().mockImplementation(() => {
+      return new Promise((resolve) =>
+        setTimeout(() => {
+          license$.next(updatedLicense);
+          resolve(undefined);
+        }, 1)
+      );
     });
 
     const toolkit = httpServiceMock.createOnPreResponseToolkit();
@@ -102,6 +87,23 @@ describe('createOnPreResponseHandler', () => {
     expect(toolkit.next).toHaveBeenCalledWith({
       headers: {
         'kbn-license-sig': 'bar',
+      },
+    });
+  });
+
+  it('sets unknown if license.signature is unset', async () => {
+    const refresh = jest.fn();
+    const toolkit = httpServiceMock.createOnPreResponseToolkit();
+    const noLicense$ = NEVER;
+
+    const interceptor = createOnPreResponseHandler(refresh, noLicense$);
+    await interceptor(httpServerMock.createKibanaRequest(), { statusCode: 200 }, toolkit);
+
+    expect(refresh).toHaveBeenCalledTimes(0);
+    expect(toolkit.next).toHaveBeenCalledTimes(1);
+    expect(toolkit.next).toHaveBeenCalledWith({
+      headers: {
+        'kbn-license-sig': 'unknown',
       },
     });
   });
