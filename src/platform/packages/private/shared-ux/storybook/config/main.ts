580ba600ec881b4ad2459cdf7fd42d7e5476cda4
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2026-01-19 00:20:26 +0100

    Move Storybook

--- src/platform/packages/private/shared-ux/storybook/config/main.ts
+++ src/platform/packages/private/shared-ux/storybook/config/main.ts
@@ -17,7 +17,7 @@ module.exports = {
     '../../../../shared/shared-ux/**/guide.mdx',
     '../../../../../../core/packages/chrome/**/*.stories.+(tsx|mdx)',
     '../../../../shared/kbn-developer-toolbar/**/*.stories.+(tsx|mdx)',
-    '../app_menu.stories.tsx',
+    '../../../../../../core/packages/chrome/app-menu/core-chrome-app-menu-components/**/*.stories.+(tsx|mdx)',
   ],
   typescript: {
     reactDocgen: 'react-docgen-typescript',
