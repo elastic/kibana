702ece30502b634064fa17001393373d52017213
Quentin Couvelaire <quentin.couvelaire@elastic.co>
2026-01-19 10:12:45 -0500

    fix ui references to ignore_empty_value that are no longer valid

--- x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/utils.ts
+++ x-pack/platform/plugins/shared/streams_app/public/components/data_management/stream_detail_enrichment/utils.ts
@@ -277,7 +277,7 @@ const defaultConcatProcessorFormState = (): ConcatFormState => ({
   from: [],
   to: '',
   ignore_failure: true,
-  ignore_empty_value: true,
+  ignore_missing: true,
   where: ALWAYS_CONDITION,
 });
 
@@ -619,14 +619,14 @@ export const convertFormStateToProcessor = (
     }
 
     if (formState.action === 'concat') {
-      const { from, to, ignore_failure, ignore_empty_value } = formState;
+      const { from, to, ignore_failure, ignore_missing } = formState;
       return {
         processorDefinition: {
           action: 'concat',
           from,
           to,
           ignore_failure,
-          ignore_empty_value,
+          ignore_missing,
           description,
           where: 'where' in formState ? formState.where : undefined,
         } as ConcatProcessor,
