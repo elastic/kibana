b98c6cd1f7d1efd81e0e9be1dfba17948bbd5a35
Hannah Mudge <Heenawter@users.noreply.github.com>
2026-01-19 13:50:27 -0700

    Fix `formatPageFilterSearchParam`

--- x-pack/solutions/security/plugins/elastic_assistant/public/src/hooks/navigate_to_alerts_page_with_filters/format_page_filter_search_param.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/public/src/hooks/navigate_to_alerts_page_with_filters/format_page_filter_search_param.ts
@@ -18,11 +18,11 @@ export const formatPageFilterSearchParam = (filters: FilterControlConfig[]) => {
       display_settings = { hide_action_bar: false },
     }) => ({
       title: title ?? field_name,
-      selected_options,
-      field_name,
-      exists_selected,
+      selectedOptions: selected_options,
+      fieldName: field_name,
+      existsSelected: exists_selected,
       exclude,
-      displaySettings: { hide_action_bar: display_settings.hide_action_bar },
+      displaySettings: { hideActionBar: display_settings.hide_action_bar },
     })
   );
 };
