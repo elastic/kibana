4ae4ac9efa888e846aff3381e81ca0a293f584eb
Anton Dosov <anton.dosov@elastic.co>
2026-01-20 16:18:21 +0100

    fix jest configs

--- src/core/packages/chrome/sidebar-internal/jest.config.js
+++ src/core/packages/chrome/sidebar-internal/jest.config.js
@@ -10,5 +10,5 @@
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
-  roots: ['<rootDir>/src/core/packages/chrome/sidebar'],
+  roots: ['<rootDir>/src/core/packages/chrome/sidebar-internal'],
 };
