86ec2ec867c70ce22a04640e288f56da5937e684
Yiannis Nikolopoulos <yiannis.nikolopoulos@elastic.co>
2026-01-21 15:00:43 +0200

    Fix navigation to edit rule page by removing unnecessary URL formatting in tests

--- x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/rules_page/edit_rule_flow.ts
+++ x-pack/platform/test/functional_with_es_ssl/apps/triggers_actions_ui/rules_page/edit_rule_flow.ts
@@ -194,7 +194,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     describe('Edit from rule details page', () => {
       it('navigates to edit page when clicking edit button from details', async () => {
         // Navigate directly to rule details page
-        await pageObjects.common.navigateToUrl('rules', `/${testRuleId}`, {
+        await pageObjects.common.navigateToUrl('rules', testRuleId, {
           shouldUseHashForSubUrl: false,
         });
         await pageObjects.header.waitUntilLoadingHasFinished();
@@ -275,7 +275,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
 
       it('returns to rule details page after clicking cancel from details', async () => {
         // Navigate to rule details page
-        await pageObjects.common.navigateToUrl('rules', `/${testRuleId}`, {
+        await pageObjects.common.navigateToUrl('rules', testRuleId, {
           shouldUseHashForSubUrl: false,
         });
         await pageObjects.header.waitUntilLoadingHasFinished();
