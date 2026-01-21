9e77280803628d29716e6e70cbfcf77acf88d7af
James Gowdy <jgowdy@elastic.co>
2026-01-21 14:39:32 +0000

    adding where clause for es|ql mode

--- x-pack/platform/plugins/shared/aiops/public/components/log_categorization/category_table/use_open_in_discover.ts
+++ x-pack/platform/plugins/shared/aiops/public/components/log_categorization/category_table/use_open_in_discover.ts
@@ -9,20 +9,51 @@ import { useCallback } from 'react';
 
 import moment from 'moment';
 
-import { type QueryMode } from '@kbn/aiops-log-pattern-analysis/get_category_query';
-import type { Filter } from '@kbn/es-query';
+import { QUERY_MODE, type QueryMode } from '@kbn/aiops-log-pattern-analysis/get_category_query';
+import { isOfAggregateQueryType, type Filter } from '@kbn/es-query';
 import type { Category } from '@kbn/aiops-log-pattern-analysis/types';
 import type { DataViewField } from '@kbn/data-views-plugin/common';
-import type { TimefilterContract } from '@kbn/data-plugin/public';
+import type { QueryStringContract, TimefilterContract } from '@kbn/data-plugin/public';
 import type { CategorizationAdditionalFilter } from '@kbn/aiops-log-pattern-analysis/create_category_request';
+import { appendToESQLQuery } from '@kbn/esql-utils';
+import { useAiopsAppContext } from '../../../hooks/use_aiops_app_context';
 import { useDiscoverLinks, createFilter } from '../use_discover_links';
 import type { LogCategorizationAppState } from '../../../application/url_state/log_pattern_analysis';
 import { getLabels } from './labels';
+
+const DOC_LIMIT = 10000;
 
 export interface OpenInDiscover {
   openFunction: (mode: QueryMode, navigateToDiscover: boolean, category?: Category) => void;
   getLabels: (navigateToDiscover: boolean) => ReturnType<typeof getLabels>;
   count: number;
+}
+
+export function onPopulateWhereClause(
+  queryString: QueryStringContract,
+  fieldName: string,
+  value: string,
+  mode: QueryMode
+) {
+  const query = queryString.getQuery();
+  if (!isOfAggregateQueryType(query)) {
+    return;
+  }
+
+  const operator = mode === QUERY_MODE.INCLUDE ? '' : 'NOT ';
+
+  const updatedQuery = appendToESQLQuery(
+    query.esql,
+    `| WHERE ${operator}MATCH(${fieldName}, "${value}", {"auto_generate_synonyms_phrase_query": false, "fuzziness": 0, "operator": "AND"})\n  | LIMIT ${DOC_LIMIT}`
+  );
+
+  if (!updatedQuery) {
+    return;
+  }
+
+  queryString.setQuery({
+    esql: updatedQuery,
+  });
 }
 
 export function useOpenInDiscover(
@@ -36,6 +67,7 @@ export function useOpenInDiscover(
   onClose: () => void = () => {}
 ): OpenInDiscover {
   const { openInDiscoverWithFilter } = useDiscoverLinks();
+  const { data } = useAiopsAppContext();
 
   const openFunction = useCallback(
     (mode: QueryMode, navigateToDiscover: boolean, category?: Category) => {
@@ -45,6 +77,19 @@ export function useOpenInDiscover(
         typeof selectedField !== 'string' &&
         navigateToDiscover === false
       ) {
+        const isEsql = isOfAggregateQueryType(data.query.queryString.getQuery());
+
+        if (isEsql) {
+          onPopulateWhereClause(
+            data.query.queryString,
+            selectedField.name,
+            selectedCategories[0].key,
+            mode
+          );
+          onClose();
+          return;
+        }
+
         onAddFilter(
           createFilter('', selectedField.name, selectedCategories, mode, category),
           `Patterns - ${selectedField.name}`
@@ -85,6 +130,7 @@ export function useOpenInDiscover(
       dataViewId,
       selectedCategories,
       aiopsListState,
+      data.query.queryString,
       onClose,
     ]
   );
