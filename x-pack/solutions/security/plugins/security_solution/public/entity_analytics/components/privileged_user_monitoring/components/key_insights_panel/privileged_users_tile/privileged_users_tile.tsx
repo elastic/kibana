ddab7748e34eab4e82aea2db88a2ff1b3db39f8f
abhishekbhatia1710 <abhishek.bhatia@elastic.co>
2026-01-21 15:41:11 +0530

    Removing the DSL changes as a modified ESQL query gets the right results

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring/components/key_insights_panel/privileged_users_tile/privileged_users_tile.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/privileged_user_monitoring/components/key_insights_panel/privileged_users_tile/privileged_users_tile.tsx
@@ -8,37 +8,22 @@
 import React from 'react';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { i18n } from '@kbn/i18n';
-import { getPrivilegedMonitorUsersIndex } from '../../../../../../../common/entity_analytics/privileged_user_monitoring/utils';
-import { createKeyInsightsPanelFormBasedLensAttributes } from '../common/lens_attributes';
-import { getPrivilegedUsersDslFilter } from './privileged_user_count_filter';
+import { right } from 'fp-ts/Either';
+import { getPrivilegedUsersEsqlCount } from './esql_query';
 import { KeyInsightsTile } from '../common/key_insights_tile';
 
 export const PrivilegedUsersTile: React.FC<{
   spaceId: string;
 }> = ({ spaceId }) => {
-  const title = i18n.translate('xpack.securitySolution.privmon.activePrivilegedUsers.title', {
-    defaultMessage: 'Privileged users',
-  });
-  const label = i18n.translate('xpack.securitySolution.privmon.activePrivilegedUsers.label', {
-    defaultMessage: 'Privileged users',
-  });
-
   return (
     <KeyInsightsTile
-      title={title}
-      label={label}
-      getLensAttributes={(namespace: string) => {
-        const dataViewId = `privileged-users-${namespace}`;
-        const dataViewTitle = getPrivilegedMonitorUsersIndex(namespace);
-
-        return createKeyInsightsPanelFormBasedLensAttributes({
-          title,
-          label,
-          dataViewId,
-          dataViewTitle,
-          filters: [getPrivilegedUsersDslFilter(dataViewId)],
-        });
-      }}
+      title={i18n.translate('xpack.securitySolution.privmon.activePrivilegedUsers.title', {
+        defaultMessage: 'Privileged users',
+      })}
+      label={i18n.translate('xpack.securitySolution.privmon.activePrivilegedUsers.label', {
+        defaultMessage: 'Privileged users',
+      })}
+      getEsqlQuery={(namespace: string) => right(getPrivilegedUsersEsqlCount(namespace))}
       id="privileged-user-monitoring-active-users"
       spaceId={spaceId}
       inspectTitle={
