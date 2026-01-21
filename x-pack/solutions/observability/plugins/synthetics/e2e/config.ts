876424cb9a3af19ba055bc6ab2c63c387cf8d6b2
Bailey Cash <bailey.cash@elastic.co>
2026-01-21 17:02:28 -0500

    fix synthetics test

--- x-pack/solutions/observability/plugins/synthetics/e2e/config.ts
+++ x-pack/solutions/observability/plugins/synthetics/e2e/config.ts
@@ -75,6 +75,7 @@ async function config({ readConfigFile }: FtrConfigProviderContext) {
         }`,
         `--xpack.uptime.service.password=${servicePassword}`,
         '--uiSettings.overrides.observability:enableLegacyUptimeApp=true',
+        `--xpack.trigger_actions_ui.enableExperimental=${JSON.stringify(['unifiedRulesPage'])}`,
       ],
     },
   };
