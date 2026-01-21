3417f3bdc558999ecaf3f0b35e92f75b301406f8
Francesco Fagnani <francesco.fagnani@elastic.co>
2026-01-21 12:09:06 +0100

    fix(build esql query): buildEsqlQuery always uses esql.where

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_significant_events_view/add_significant_event_flyout/add_significant_event_flyout.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_significant_events_view/add_significant_event_flyout/add_significant_event_flyout.tsx
@@ -21,7 +21,12 @@ import {
 } from '@elastic/eui';
 import { omit } from 'lodash';
 import { i18n } from '@kbn/i18n';
-import { type StreamQuery, type Streams, type System } from '@kbn/streams-schema';
+import {
+  buildEsqlWhereCondition,
+  type StreamQuery,
+  type Streams,
+  type System,
+} from '@kbn/streams-schema';
 import { streamQuerySchema } from '@kbn/streams-schema';
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { css } from '@emotion/css';
@@ -138,7 +143,12 @@ export function AddSignificantEventFlyout({
           .map((nextQuery) => ({
             id: v4(),
             kql: { query: nextQuery.kql },
-            esql: { where: '' },
+            esql: {
+              where: buildEsqlWhereCondition({
+                kql: { query: nextQuery.kql },
+                feature: nextQuery.feature,
+              }),
+            },
             title: nextQuery.title,
             feature: nextQuery.feature,
             severity_score: nextQuery.severity_score,
