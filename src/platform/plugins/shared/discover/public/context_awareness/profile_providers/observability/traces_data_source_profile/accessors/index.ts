12aa896800029a0abef7313759ba7f85c7f2d00c
iblancof <irene.blanco@elastic.co>
2026-01-21 16:02:07 +0100

    Remove traces sub-profiles

--- src/platform/plugins/shared/discover/public/context_awareness/profile_providers/observability/traces_data_source_profile/accessors/index.ts
+++ src/platform/plugins/shared/discover/public/context_awareness/profile_providers/observability/traces_data_source_profile/accessors/index.ts
@@ -7,5 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export * from './get_cell_renderers';
+export { getCellRenderers } from './get_cell_renderers';
 export { getColumnsConfiguration } from './get_columns_configuration';
+export { getDefaultAppState } from './get_default_app_state';
+export { getChartSectionConfiguration } from './get_chart_section_configuration';
