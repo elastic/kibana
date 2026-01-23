c871b9175dec2dcc767ebbff96804a61a4604dbb
Tiago Vila Verde <tiago.vilaverde@elastic.co>
2026-01-23 15:21:20 +0100

    Add a new saved object for watchlists

---
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/privilege_monitoring/saved_objects/watchlist_config_type.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { SECURITY_SOLUTION_SAVED_OBJECT_INDEX } from '@kbn/core-saved-objects-server';
+import type { SavedObjectsType } from '@kbn/core/server';
+
+export const watchlistConfigTypeName = 'watchlist-config';
+
+export const watchlistConfigTypeNameMappings: SavedObjectsType['mappings'] = {
+  dynamic: false,
+  properties: {
+    name: {
+      type: 'keyword',
+    },
+    description: {
+      type: 'text',
+    },
+    riskModifier: {
+      type: 'float',
+    },
+
+    createdAt: {
+      type: 'date',
+    },
+    modifiedAt: {
+      type: 'date',
+    },
+  },
+};
+
+export const watchlistConfigType: SavedObjectsType = {
+  name: watchlistConfigTypeName,
+  indexPattern: SECURITY_SOLUTION_SAVED_OBJECT_INDEX,
+  hidden: false,
+  namespaceType: 'multiple-isolated',
+  mappings: watchlistConfigTypeNameMappings,
+  modelVersions: {},
+};
