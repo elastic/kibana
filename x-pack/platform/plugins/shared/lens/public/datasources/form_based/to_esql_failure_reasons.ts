6bc91699185f07877a62d3c95dcadd7a00ef0195
Walter Rafelsberger <walter.rafelsberger@elastic.co>
2026-01-23 10:12:18 +0100

    tweak esqlConversionFailureReasonMessages ids

--- x-pack/platform/plugins/shared/lens/public/datasources/form_based/to_esql_failure_reasons.ts
+++ x-pack/platform/plugins/shared/lens/public/datasources/form_based/to_esql_failure_reasons.ts
@@ -27,60 +27,79 @@ export type EsqlConversionFailureReason =
   | 'unknown';
 
 export const esqlConversionFailureReasonMessages: Record<EsqlConversionFailureReason, string> = {
-  multi_layer_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlMultiLayer', {
-    defaultMessage:
-      'Cannot convert to ES|QL: Multi-layer visualizations will be supported in an upcoming update.',
-  }),
-  trend_line_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlTrendLine', {
-    defaultMessage:
-      'Cannot convert to ES|QL: Metric visualizations with trend lines will be supported in an upcoming update.',
-  }),
-  non_utc_timezone: i18n.translate('xpack.lens.config.cannotConvertToEsqlNonUtcTimezone', {
+  multi_layer_not_supported: i18n.translate(
+    'xpack.lens.config.cannotConvertToEsqlMultiLayerTooltip',
+    {
+      defaultMessage:
+        'Cannot convert to ES|QL: Multi-layer visualizations will be supported in an upcoming update.',
+    }
+  ),
+  trend_line_not_supported: i18n.translate(
+    'xpack.lens.config.cannotConvertToEsqlTrendLineTooltip',
+    {
+      defaultMessage:
+        'Cannot convert to ES|QL: Metric visualizations with trend lines will be supported in an upcoming update.',
+    }
+  ),
+  non_utc_timezone: i18n.translate('xpack.lens.config.cannotConvertToEsqlNonUtcTimezoneTooltip', {
     defaultMessage:
       'Cannot convert to ES|QL: Non-UTC timezones will be supported in an upcoming update.',
   }),
-  formula_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlFormula', {
+  formula_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlFormulaTooltip', {
     defaultMessage:
       'Cannot convert to ES|QL: Formula operations will be supported in an upcoming update.',
   }),
-  time_shift_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlTimeShift', {
-    defaultMessage: 'Cannot convert to ES|QL: Time shift will be supported in an upcoming update.',
-  }),
-  runtime_field_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlRuntimeField', {
-    defaultMessage:
-      'Cannot convert to ES|QL: Runtime fields will be supported in an upcoming update.',
-  }),
+  time_shift_not_supported: i18n.translate(
+    'xpack.lens.config.cannotConvertToEsqlTimeShiftTooltip',
+    {
+      defaultMessage:
+        'Cannot convert to ES|QL: Time shift will be supported in an upcoming update.',
+    }
+  ),
+  runtime_field_not_supported: i18n.translate(
+    'xpack.lens.config.cannotConvertToEsqlRuntimeFieldTooltip',
+    {
+      defaultMessage:
+        'Cannot convert to ES|QL: Runtime fields will be supported in an upcoming update.',
+    }
+  ),
   reduced_time_range_not_supported: i18n.translate(
-    'xpack.lens.config.cannotConvertToEsqlReducedTimeRange',
+    'xpack.lens.config.cannotConvertToEsqlReducedTimeRangeTooltip',
     {
       defaultMessage:
         'Cannot convert to ES|QL: Reduced time range will be supported in an upcoming update.',
     }
   ),
-  function_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlOperation', {
+  function_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlOperationTooltip', {
     defaultMessage:
       'Cannot convert to ES|QL: Support for one or more functions used will be coming in an upcoming update.',
   }),
-  drop_partials_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlDropPartials', {
-    defaultMessage:
-      'Cannot convert to ES|QL: "Drop partial buckets" will be supported in an upcoming update.',
-  }),
+  drop_partials_not_supported: i18n.translate(
+    'xpack.lens.config.cannotConvertToEsqlDropPartialsTooltip',
+    {
+      defaultMessage:
+        'Cannot convert to ES|QL: "Drop partial buckets" will be supported in an upcoming update.',
+    }
+  ),
   include_empty_rows_not_supported: i18n.translate(
-    'xpack.lens.config.cannotConvertToEsqlIncludeEmptyRows',
+    'xpack.lens.config.cannotConvertToEsqlIncludeEmptyRowsTooltip',
     {
       defaultMessage:
         'Cannot convert to ES|QL: "Include empty rows" will be supported in an upcoming update.',
     }
   ),
-  terms_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlTerms', {
+  terms_not_supported: i18n.translate('xpack.lens.config.cannotConvertToEsqlTermsTooltip', {
     defaultMessage:
       'Cannot convert to ES|QL: Top values (terms) aggregation will be supported in an upcoming update.',
   }),
-  unsupported_settings: i18n.translate('xpack.lens.config.cannotConvertToEsqlUnsupportedSettings', {
-    defaultMessage:
-      'Cannot convert to ES|QL: Some settings used will be supported in an upcoming update.',
-  }),
-  unknown: i18n.translate('xpack.lens.config.cannotConvertToEsqlUnknown', {
+  unsupported_settings: i18n.translate(
+    'xpack.lens.config.cannotConvertToEsqlUnsupportedSettingsTooltip',
+    {
+      defaultMessage:
+        'Cannot convert to ES|QL: Some settings used will be supported in an upcoming update.',
+    }
+  ),
+  unknown: i18n.translate('xpack.lens.config.cannotConvertToEsqlUnknownTooltip', {
     defaultMessage:
       'Cannot convert to ES|QL: This visualization will be supported in an upcoming update.',
   }),
