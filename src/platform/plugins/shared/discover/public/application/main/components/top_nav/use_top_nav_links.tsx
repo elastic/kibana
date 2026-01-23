6814a1d06b3bd8103c62069de922d40a81537660
Davis McPhee <davis.mcphee@elastic.co>
2026-01-23 19:21:50 -0400

    Fix app menu focus when closing background search flyout

--- src/platform/plugins/shared/discover/public/application/main/components/top_nav/use_top_nav_links.tsx
+++ src/platform/plugins/shared/discover/public/application/main/components/top_nav/use_top_nav_links.tsx
@@ -139,7 +139,7 @@ export const useTopNavLinks = ({
       services.capabilities.discover_v2.storeSearchSession
     ) {
       const backgroundSearchFlyoutMenuItem = getBackgroundSearchFlyout({
-        onClick: () => {
+        onClick: ({ context: { onFinishAction } }) => {
           services.data.search.showSearchSessionsFlyout({
             appId,
             trackingProps: { openedFrom: 'background search button' },
@@ -147,6 +147,7 @@ export const useTopNavLinks = ({
               event?.preventDefault();
               dispatch(internalStateActions.openSearchSessionInNewTab({ searchSession: session }));
             },
+            onClose: onFinishAction,
           });
         },
       });
