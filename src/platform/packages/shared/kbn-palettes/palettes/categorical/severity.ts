09c672e70ae54f32bb4d313847d56d565e9028f1
Maryam Saeidi <maryam.saeidi@elastic.co>
2026-01-26 17:44:15 +0100

    [Lens] Introduction of Severity Color palette in color mapping (#250198)
    Closes https://github.com/elastic/kibana/issues/237262
    
    ## Summary
    
    This PR adds a new Severity Color palette:
    
    |Theme|Screenshot|
    |---|---|
    |Light|<img width="1512" height="864" alt="image"
    src="https://github.com/user-attachments/assets/a83d481e-0d0d-4005-b08a-993e46807a80"
    />|
    |Dark|<img width="1510" height="858" alt="image"
    src="https://github.com/user-attachments/assets/6b19f89a-6d22-488c-806d-c924d40184e5"
    />|
    
    And here is the value saved in the saved object:
    
    <img width="500" alt="image"
    src="https://github.com/user-attachments/assets/6ed34875-d052-4a59-abf2-1b363e80c337"
    />

--- src/platform/packages/shared/kbn-palettes/palettes/categorical/severity.ts
+++ src/platform/packages/shared/kbn-palettes/palettes/categorical/severity.ts
@@ -7,6 +7,15 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export * from './neutral';
-export * from './elastic';
-export * from './severity';
+import { i18n } from '@kbn/i18n';
+import { KbnCategoricalPalette } from '../../classes/categorical_palette';
+import { KbnPalette } from '../../constants';
+
+export const severityPalette = new KbnCategoricalPalette({
+  id: KbnPalette.Severity,
+  name: i18n.translate('palettes.severity.name', {
+    defaultMessage: 'Severity',
+  }),
+  colorCount: 6,
+  colors: ['#24C292', '#B5E5F2', '#FCD883', '#FF995E', '#EE4C48', '#E3E8F2'],
+});
