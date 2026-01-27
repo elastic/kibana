a79c3b1dc8912df67b2d76741103cced35f41fc6
Edgar Santos <edgar.santos@elastic.co>
2026-01-27 16:50:52 +0100

    add a switcher to support the deprecated alerts ui capabilities
    Since there is no way to declaratively do this in the feature declaration because the deprecated ui capability does not map to anything in the most recent version of the alerts feature, we opted for adding a switcher that will determine if the user has access to the capability by checking the API privilege instead and injecting it as a boolean in the response of the capabilities request (POST /api/core/capabilities)

--- x-pack/solutions/security/plugins/security_solution/server/lib/capabilities/alerts_capabilities_switcher.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/capabilities/alerts_capabilities_switcher.ts
@@ -8,47 +8,77 @@
 import type { CoreSetup, Logger } from '@kbn/core/server';
 import type { SecurityPluginStart } from '@kbn/security-plugin/server';
 import {
-  ALERTS_UI_UPDATE_DEPRECATED_PRIVILEGE,
-  SECURITY_FEATURE_ID_V1,
-  SECURITY_FEATURE_ID_V2,
-  SECURITY_FEATURE_ID_V3,
-  SECURITY_FEATURE_ID_V4,
-  RULES_FEATURE_ID_V1,
-  RULES_FEATURE_ID_V2,
+    ALERTS_FEATURE_ID,
+    ALERTS_UI_UPDATE_DEPRECATED_PRIVILEGE,
+    ALERTS_API_UPDATE_DEPRECATED_PRIVILEGE,
+    ALERTS_UI_EDIT,
+    ALERTS_UI_READ,
 } from '@kbn/security-solution-features/constants';
 
 interface SetupDeps {
-  core: CoreSetup;
-  logger: Logger;
-  getSecurityStart: () => Promise<SecurityPluginStart | undefined>;
+    core: CoreSetup;
+    logger: Logger;
+    getSecurityStart: () => Promise<SecurityPluginStart | undefined>;
 }
 
 /**
  * Registers a capability switcher that grants the deprecated alerts update capability
  * to users who have read access on deprecated features (siem, siemV2, siemV3, siemV4, securityRulesV1 and securityRulesV2).
- *
+ * 
  * This maintains backward compatibility: users with deprecated feature privileges can still
  * trigger alert updates from the UI, while users with only the new alerts feature 'read' privilege cannot.
  */
 export const setupAlertsCapabilitiesSwitcher = ({ core, logger, getSecurityStart }: SetupDeps) => {
-  // Since deprecated UI privileges do not appear in the latest version of the alerts feature, we need to register them here.
-  const deprecatedFeatures = [
-    SECURITY_FEATURE_ID_V1,
-    SECURITY_FEATURE_ID_V2,
-    SECURITY_FEATURE_ID_V3,
-    SECURITY_FEATURE_ID_V4,
-    RULES_FEATURE_ID_V1,
-    RULES_FEATURE_ID_V2,
-  ];
+    // Since the deprecated privileges do not appear in the latest version of the alerts feature, we need to register it here.
+    core.capabilities.registerProvider(() => ({
+        [ALERTS_FEATURE_ID]: {
+            [ALERTS_UI_UPDATE_DEPRECATED_PRIVILEGE]: false,
+        },
+    }));
 
-  core.capabilities.registerProvider(() =>
-    deprecatedFeatures.reduce((acc, featureId) => {
-      acc[featureId] = {
-        // Even though we set it to true here, the privilege is only granted
-        // if it is explicitly listed in the ui privileges in the feature configuration
-        [ALERTS_UI_UPDATE_DEPRECATED_PRIVILEGE]: true,
-      };
-      return acc;
-    }, {} as Record<string, { [ALERTS_UI_UPDATE_DEPRECATED_PRIVILEGE]: boolean }>)
-  );
+    core.capabilities.registerSwitcher(
+        async (request, capabilities) => {
+            const alertsCapabilities = capabilities[ALERTS_FEATURE_ID] as Record<string, boolean> | undefined;
+
+            if (
+                // Users with edit access are already able to update alerts
+                alertsCapabilities?.[ALERTS_UI_EDIT] === true || 
+                // This user doesn't have read access
+                !alertsCapabilities?.[ALERTS_UI_READ]) {
+                return {};
+            }
+
+            try {
+                const security = await getSecurityStart();
+                if (!security) {
+                    return {};
+                }
+
+                const checkPrivileges = security.authz.checkPrivilegesDynamicallyWithRequest(request);
+
+                const deprecatedApiAction = `api:${ALERTS_API_UPDATE_DEPRECATED_PRIVILEGE}`;
+                const { hasAllRequested } = await checkPrivileges({
+                    kibana: [deprecatedApiAction],
+                });
+
+                if (hasAllRequested) {
+                    // Inject the deprecated UI capability into the alerts feature
+                    return {
+                        [ALERTS_FEATURE_ID]: {
+                            ...capabilities[ALERTS_FEATURE_ID],
+                            [ALERTS_UI_UPDATE_DEPRECATED_PRIVILEGE]: true,
+                        },
+                    };
+                }
+
+                return {};
+            } catch (error) {
+                logger.debug(`Error in alerts capabilities switcher: ${error}`);
+                return {};
+            }
+        },
+        {
+            capabilityPath: `${ALERTS_FEATURE_ID}.*`,
+        }
+    );
 };
