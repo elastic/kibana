c5ba26a7fc81af865fb5b42f1a88e942acf55aa7
mohamedhamed-ahmed <mohamed.ahmed@elastic.co>
2026-01-28 22:43:22 +0100

    fix config

--- x-pack/solutions/observability/test/serverless/functional/configs/index.onboarding.ts
+++ x-pack/solutions/observability/test/serverless/functional/configs/index.onboarding.ts
@@ -8,7 +8,7 @@
 import type { FtrProviderContext } from '../ftr_provider_context';
 
 export default function ({ loadTestFile }: FtrProviderContext) {
-  describe('serverless observability onboarding', function () {
+  describe('serverless observability UI - onboarding', function () {
     this.tags(['esGate']);
 
     loadTestFile(require.resolve('../test_suites/onboarding'));
