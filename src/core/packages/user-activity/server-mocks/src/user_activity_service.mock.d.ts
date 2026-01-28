947a5933c3547f510bbab9ef606581c892a08e6a
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-01-28 15:25:22 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/core/packages/user-activity/server-mocks/src/user_activity_service.mock.d.ts
+++ src/core/packages/user-activity/server-mocks/src/user_activity_service.mock.d.ts
@@ -1,8 +1,23 @@
-import type { UserActivityServiceSetup, UserActivityServiceStart } from '@kbn/core-user-activity-server';
-import type { InternalUserActivityServiceSetup, InternalUserActivityServiceStart } from '@kbn/core-user-activity-server-internal';
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import type {
+  UserActivityServiceSetup,
+  UserActivityServiceStart,
+} from '@kbn/core-user-activity-server';
+import type {
+  InternalUserActivityServiceSetup,
+  InternalUserActivityServiceStart,
+} from '@kbn/core-user-activity-server-internal';
 export declare const userActivityServiceMock: {
-    createInternalSetupContract: () => jest.Mocked<InternalUserActivityServiceSetup>;
-    createInternalStartContract: () => jest.Mocked<InternalUserActivityServiceStart>;
-    createSetupContract: () => jest.Mocked<UserActivityServiceSetup>;
-    createStartContract: () => jest.Mocked<UserActivityServiceStart>;
+  createInternalSetupContract: () => jest.Mocked<InternalUserActivityServiceSetup>;
+  createInternalStartContract: () => jest.Mocked<InternalUserActivityServiceStart>;
+  createSetupContract: () => jest.Mocked<UserActivityServiceSetup>;
+  createStartContract: () => jest.Mocked<UserActivityServiceStart>;
 };
