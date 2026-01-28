815dd6027e13c6804d3ef2fde1cd3b4f57f1cf98
Matthew Kime <matt@mattki.me>
2026-01-28 15:11:02 -0600

    move abort signal up one level

---
+++ x-pack/platform/plugins/shared/index_management/public/application/store/actions/load_indices.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { createAction } from 'redux-actions';
+import { loadIndices as request } from '../../services';
+
+export const loadIndicesStart = createAction('INDEX_MANAGEMENT_LOAD_INDICES_START');
+export const loadIndicesSuccess = createAction('INDEX_MANAGEMENT_LOAD_INDICES_SUCCESS');
+export const loadIndicesError = createAction('INDEX_MANAGEMENT_LOAD_INDICES_ERROR');
+export const loadIndicesEnrichmentError = createAction(
+  'INDEX_MANAGEMENT_LOAD_INDICES_ENRICHMENT_ERROR'
+);
+
+let abortController: AbortController;
+
+export const loadIndices = () => async (dispatch: any) => {
+  if (abortController && !abortController.signal.aborted) {
+    abortController.abort();
+  }
+
+  abortController = new AbortController();
+
+  dispatch(loadIndicesStart());
+  try {
+    await request(
+      (indices) => dispatch(loadIndicesSuccess({ indices })),
+      (source) => dispatch(loadIndicesEnrichmentError({ source })),
+      abortController.signal
+    );
+  } catch (error) {
+    return dispatch(loadIndicesError(error));
+  }
+};
