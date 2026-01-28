c5bc06682f6534d39f27897f073f46b8c5973395
Edgar Santos <edgar.santos@elastic.co>
2026-01-28 13:00:37 +0100

    fix authorization tests

--- x-pack/solutions/security/packages/features/src/security/v1_features/kibana_features.ts
+++ x-pack/solutions/security/packages/features/src/security/v1_features/kibana_features.ts
@@ -44,7 +44,6 @@ import {
   EXCEPTIONS_SUBFEATURE_ALL,
   ALERTS_FEATURE_ID,
   ALERTS_API_UPDATE_DEPRECATED_PRIVILEGE,
-  ALERTS_UI_UPDATE_DEPRECATED_PRIVILEGE,
 } from '../../constants';
 import type { SecurityFeatureParams } from '../types';
 import type { BaseKibanaFeatureConfig } from '../../types';
@@ -219,7 +218,7 @@ export const getSecurityBaseKibanaFeature = ({
       management: {
         insightsAndAlerting: ['triggersActions'],
       },
-      ui: [SECURITY_UI_SHOW, ALERTS_UI_UPDATE_DEPRECATED_PRIVILEGE],
+      ui: [SECURITY_UI_SHOW],
     },
   },
 });
