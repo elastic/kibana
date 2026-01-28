7f1f288e2bf9b3f4e556b72281fa5452f3169aaf
Nathan Reese <reese.nathan@gmail.com>
2026-01-28 13:01:19 -0700

    slo plugin

--- x-pack/solutions/observability/plugins/synthetics/server/plugin.ts
+++ x-pack/solutions/observability/plugins/synthetics/server/plugin.ts
@@ -16,6 +16,7 @@ import type {
 import { SavedObjectsClient } from '@kbn/core/server';
 import { mappingFromFieldMap } from '@kbn/alerting-plugin/common';
 import { Dataset } from '@kbn/rule-registry-plugin/server';
+import type { DrilldownTransforms } from '@kbn/embeddable-plugin/common';
 import { SyncGlobalParamsPrivateLocationsTask } from './tasks/sync_global_params_task';
 import type {
   SyntheticsPluginsSetupDependencies,
@@ -111,8 +112,10 @@ export class Plugin implements PluginType {
 
     this.syncGlobalParamsTask.registerTaskDefinition(plugins.taskManager);
     plugins.embeddable.registerTransforms(SYNTHETICS_STATS_OVERVIEW_EMBEDDABLE, {
-      transformIn: getTransformIn(plugins.embeddable.transformEnhancementsIn),
-      transformOut: getTransformOut(plugins.embeddable.transformEnhancementsOut),
+      getTransforms: (drilldownTransforms: DrilldownTransforms) => ({
+        transformIn: getTransformIn(drilldownTransforms.transformIn),
+        transformOut: getTransformOut(drilldownTransforms.transformOut),
+      }),
     });
 
     return {};
