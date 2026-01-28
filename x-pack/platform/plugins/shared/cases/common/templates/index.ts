66103958965c6fc873c3e0ead853e80cfd2fd616
lgestc <lukasz.gmys@elastic.co>
2026-01-28 11:30:53 +0100

    --wip-- [skip ci]

---
+++ x-pack/platform/plugins/shared/cases/common/templates/index.ts
@@ -0,0 +1,60 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export interface Template {
+  /*
+   * Template identifier, can be shared across multiple SO's as we are storing all the changes made to the template
+   */
+  templateId: string;
+
+  /**
+   * Display name
+   */
+  name: string;
+
+  /**
+   * Yaml definition for the template
+   */
+  definition: string;
+
+  /**
+   * Template version
+   */
+  templateVersion: number;
+
+  /**
+   * Deletion date, used to indicate soft-deletion
+   */
+  deletedAt: Date | null;
+}
+
+export interface ParsedTemplate extends Omit<Template, 'definition'> {
+  /**
+   * Parsed definition for the template. Needs to be validated programmatically.
+   */
+  definition: {
+    fields: Array<{
+      control: string;
+      name: string;
+      label?: string;
+      type: 'keyword';
+      metadata: Record<string, unknown>;
+    }>;
+  };
+  isLatest: boolean;
+  latestVersion: number;
+}
+
+export type CreateTemplateInput = Omit<
+  Template,
+  'id' | 'templateId' | 'templateVersion' | 'deletedAt'
+>;
+
+export type UpdateTemplateInput = Omit<
+  Template,
+  'id' | 'templateId' | 'templateVersion' | 'deletedAt'
+>;
---
+++ x-pack/platform/plugins/shared/cases/server/saved_object_types/templates/index.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { SavedObjectsType } from '@kbn/core/server';
+import { ALERTING_CASES_SAVED_OBJECT_INDEX } from '@kbn/core-saved-objects-server';
+import { CASE_TEMPLATE_SAVED_OBJECT } from '../../../common/constants';
+
+/**
+ * The comments in the mapping indicate the additional properties that are stored in Elasticsearch but are not indexed.
+ * Remove these comments when https://github.com/elastic/kibana/issues/152756 is resolved.
+ */
+
+export const caseTemplateSavedObjectType: SavedObjectsType = {
+  name: CASE_TEMPLATE_SAVED_OBJECT,
+  indexPattern: ALERTING_CASES_SAVED_OBJECT_INDEX,
+  hidden: true,
+  namespaceType: 'multiple-isolated',
+  convertToMultiNamespaceTypeVersion: '8.0.0',
+  mappings: {
+    dynamic: false,
+    properties: {
+      // this should be used to identify individual templates, not the _id as we are storing revisions for all the templates.
+      templateId: {
+        type: 'keyword',
+      },
+      name: {
+        type: 'keyword',
+      },
+      templateVersion: {
+        type: 'integer',
+      },
+      owner: {
+        type: 'keyword',
+      },
+      // NOTE: yaml-based template definition
+      definition: {
+        type: 'text',
+      },
+      // NOTE: other timestamp fields are provided by the SO api / model itself
+      deletedAt: {
+        type: 'date',
+      },
+    },
+  },
+};
