3ae10a9151f0d132b92af38a3b0369b785315dda
Larry Gregory <larry.gregory@elastic.co>
2026-01-29 14:18:08 -0500

    Test updates

--- x-pack/platform/test/functional/apps/api_keys/home_page.ts
+++ x-pack/platform/test/functional/apps/api_keys/home_page.ts
@@ -630,8 +630,11 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
 
       it('should refresh data when clicking Refresh button', async () => {
         // Get initial count
-        const initialCount = await pageObjects.apiKeys.getDisplayedApiKeyCount();
-        expect(initialCount).to.be.greaterThan(0);
+        let initialCount = 0;
+        await retry.try(async () => {
+          initialCount = await pageObjects.apiKeys.getDisplayedApiKeyCount();
+          expect(initialCount).to.be.greaterThan(0);
+        });
 
         // Click refresh and verify page still works
         await pageObjects.apiKeys.clickRefreshButton();
