9f22d03b4a7eb9006839256e706523550f3f3a3d
Angela Chuang <yi-chun.chuang@elastic.co>
2026-01-30 15:57:22 +0000

    Updated `getMockQRadarRuleDataXml` to accept an `isBuildingBlock` parameter

--- x-pack/solutions/security/plugins/security_solution/common/siem_migrations/parsers/qradar/mock/data/get_mock_qradar_rule.ts
+++ x-pack/solutions/security/plugins/security_solution/common/siem_migrations/parsers/qradar/mock/data/get_mock_qradar_rule.ts
@@ -7,10 +7,10 @@
 
 const getRandomNumberId = () => Math.floor(Math.random() * 1000000);
 
-const getMockQRadarRuleDataXml = (ruleName: string) => {
+const getMockQRadarRuleDataXml = (ruleName: string, isBuildingBlock: boolean = false) => {
   const id = getRandomNumberId();
   return {
-    normal: `<rule buildingBlock="true" enabled="true" id="${id}" name="BB:CategoryDefinition: Authentication Success" origin="SYSTEM" owner="admin" roleDefinition="false" scope="LOCAL" type="EVENT">
+    normal: `<rule buildingBlock="${isBuildingBlock}" enabled="true" id="${id}" name="BB:CategoryDefinition: Authentication Success" origin="SYSTEM" owner="admin" roleDefinition="false" scope="LOCAL" type="EVENT">
   <name>${ruleName}</name>
   <notes>Edit this BB to include all events that indicate successful attempts to access the network.</notes>
   <testDefinitions>
@@ -32,7 +32,7 @@ const getMockQRadarRuleDataXml = (ruleName: string) => {
   </responses>
 </rule>
 `,
-    sanitized: `<rule buildingBlock="true" enabled="true" id="${id}" name="BB:CategoryDefinition: Authentication Success" origin="SYSTEM" owner="admin" roleDefinition="false" scope="LOCAL" type="EVENT">
+    sanitized: `<rule buildingBlock="${isBuildingBlock}" enabled="true" id="${id}" name="BB:CategoryDefinition: Authentication Success" origin="SYSTEM" owner="admin" roleDefinition="false" scope="LOCAL" type="EVENT">
   <name>${ruleName}</name>
   <notes>Edit this BB to include all events that indicate successful attempts to access the network.</notes>
   <testDefinitions>
@@ -57,8 +57,10 @@ const getMockQRadarRuleDataXml = (ruleName: string) => {
   };
 };
 
-export const getMockQRadarXml = (ruleNames: string[]) => {
-  const mockRuleDataXmls = ruleNames.map((ruleName) => getMockQRadarRuleDataXml(ruleName));
+export const getMockQRadarXml = (ruleNames: string[], isBuildingBlock: boolean = false) => {
+  const mockRuleDataXmls = ruleNames.map((ruleName) =>
+    getMockQRadarRuleDataXml(ruleName, isBuildingBlock)
+  );
   const mockRuleDataBase64s = mockRuleDataXmls.map((mockRuleDataXml) =>
     Buffer.from(mockRuleDataXml.normal).toString('base64')
   );
--- x-pack/solutions/security/plugins/security_solution/server/lib/siem_migrations/rules/api/rules/qradar/create.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/siem_migrations/rules/api/rules/qradar/create.ts
@@ -59,6 +59,15 @@ export const registerSiemRuleMigrationsCreateQRadarRulesRoute = (
             // Parse QRadar XML
             const parser = new QradarRulesXmlParser(xml);
             const qradarRules = await parser.getRules();
+
+            const qRadarResources = await parser.getResources();
+
+            if (!qradarRules || qradarRules.length === 0) {
+              return res.badRequest({
+                body: { message: 'No rules found in the provided XML' },
+              });
+            }
+
             const isEligibleForTranslation = qradarRules.filter(
               (rule) => rule.rule_type !== 'building_block'
             );
@@ -66,14 +75,6 @@ export const registerSiemRuleMigrationsCreateQRadarRulesRoute = (
             if (isEligibleForTranslation.length === 0) {
               return res.badRequest({
                 body: { message: 'No valid rules could be extracted from the XML' },
-              });
-            }
-
-            const qRadarResources = await parser.getResources();
-
-            if (!qradarRules || qradarRules.length === 0) {
-              return res.badRequest({
-                body: { message: 'No rules found in the provided XML' },
               });
             }
 
