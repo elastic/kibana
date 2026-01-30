0240e2e8c0081b307778162d841695df537a1c35
Carlos Crespo <carloshenrique.leonelcrespo@elastic.co>
2026-01-30 17:50:06 +0100

    fix: global setup

--- x-pack/solutions/observability/plugins/infra/test/scout/ui/parallel_tests/global.setup.ts
+++ x-pack/solutions/observability/plugins/infra/test/scout/ui/parallel_tests/global.setup.ts
@@ -5,7 +5,6 @@
  * 2.0.
  */
 
-// eslint-disable-next-line @kbn/eslint/scout_test_file_naming
 import { globalSetupHook } from '@kbn/scout-oblt';
 
 globalSetupHook('Ingest data to Elasticsearch', { tag: ['@ess', '@svlOblt'] }, async ({}) => {
