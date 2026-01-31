1b4b65f6318bd312f36e45b6356831fce4704c3f
Georgii Gorbachev <georgii.gorbachev@elastic.co>
2026-01-31 14:12:01 +0100

    Improve deleteAllRules test helper

--- x-pack/solutions/security/test/security_solution_api_integration/config/services/detections_response/spaces.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/config/services/detections_response/spaces.ts
@@ -5,6 +5,16 @@
  * 2.0.
  */
 
+const DEFAULT_SPACE_ID = 'default';
+
+/**
+ * Returns a normalized space ID that can't be undefined.
+ * The default space has an ID of 'default'.
+ */
+export function getSpaceId(spaceId?: string): string {
+  return spaceId ? String(spaceId) : DEFAULT_SPACE_ID;
+}
+
 /**
  * Wraps a provided URL with the space ID if it is not the default space.
  *
@@ -13,6 +23,7 @@
  * - `withSpaceUrl('/api/some_endpoint', 'default')` returns `/api/some_endpoint`
  * - `withSpaceUrl('/api/some_endpoint', 'my_space') returns `/s/my_space/api/some_endpoint`
  */
-export function withSpaceUrl(url: string, spaceId = 'default'): string {
-  return spaceId === 'default' ? url : `/s/${spaceId}${url}`;
+export function withSpaceUrl(url: string, spaceId = DEFAULT_SPACE_ID): string {
+  const normalizedSpaceId = getSpaceId(spaceId);
+  return normalizedSpaceId === DEFAULT_SPACE_ID ? url : `/s/${spaceId}${url}`;
 }
