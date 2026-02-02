e12697f01aef28557a8d99feb71cba5f478f8a6b
Kevin Delemme <kdelemme@gmail.com>
2026-02-02 11:17:48 -0500

    Fix query

--- x-pack/platform/plugins/shared/alerting_v2/server/lib/dispatcher/queries.ts
+++ x-pack/platform/plugins/shared/alerting_v2/server/lib/dispatcher/queries.ts
@@ -20,12 +20,12 @@ export const getDispatchableAlertEventsQuery = (): EsqlRequest => {
   const alertEventType: AlertEventType = 'alert';
 
   return esql`FROM ${ALERT_EVENTS_DATA_STREAM},${ALERT_ACTIONS_DATA_STREAM} METADATA _index
-      | WHERE (_index LIKE "${ALERT_ACTIONS_BACKING_INDEX}") OR (_index LIKE "${ALERT_EVENTS_BACKING_INDEX}" and type == "${alertEventType}")
+      | WHERE (_index LIKE ${ALERT_ACTIONS_BACKING_INDEX}) OR (_index LIKE ${ALERT_EVENTS_BACKING_INDEX} and type == ${alertEventType})
       | EVAL rule_id = COALESCE(rule.id, rule_id)
-      | INLINE STATS last_fired = max(last_series_event_timestamp) WHERE _index LIKE "${ALERT_ACTIONS_BACKING_INDEX}" AND action_type == "fire-event" BY rule_id, group_hash
-      | WHERE (last_fired IS NULL OR last_fired < @timestamp) or (_index LIKE "${ALERT_ACTIONS_BACKING_INDEX}")
+      | INLINE STATS last_fired = max(last_series_event_timestamp) WHERE _index LIKE ${ALERT_ACTIONS_BACKING_INDEX} AND action_type == "fire-event" BY rule_id, group_hash
+      | WHERE (last_fired IS NULL OR last_fired < @timestamp) or (_index LIKE ${ALERT_ACTIONS_BACKING_INDEX})
       | STATS
-          last_event_timestamp = MAX(@timestamp) WHERE _index LIKE "${ALERT_EVENTS_BACKING_INDEX}"
+          last_event_timestamp = MAX(@timestamp) WHERE _index LIKE ${ALERT_EVENTS_BACKING_INDEX}
           BY rule_id, group_hash, episode_id, episode_status
       | WHERE last_event_timestamp IS NOT NULL
       | KEEP last_event_timestamp, rule_id, group_hash, episode_id, episode_status
