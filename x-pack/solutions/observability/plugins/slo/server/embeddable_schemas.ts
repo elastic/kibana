def1e4ab5e6470e5772d5d618690de76f2f821d0
Panagiota Mitsopoulou <giota85@gmail.com>
2026-02-02 14:44:05 +0200

    add custom validator

--- x-pack/solutions/observability/plugins/slo/server/embeddable_schemas.ts
+++ x-pack/solutions/observability/plugins/slo/server/embeddable_schemas.ts
@@ -9,31 +9,43 @@ import type { TypeOf } from '@kbn/config-schema';
 import { serializedTitlesSchema } from '@kbn/presentation-publishing-schemas';
 import { schema } from '@kbn/config-schema';
 
+// slo_id is optional, it's required when overview_mode is 'single'
+const sloOverviewStateSchema = schema.object(
+  {
+    slo_id: schema.maybe(schema.string()),
+    slo_instance_id: schema.maybe(schema.string()),
+    show_all_group_by_instances: schema.maybe(schema.boolean()),
+    remote_name: schema.maybe(schema.string()),
+    overview_mode: schema.maybe(schema.oneOf([schema.literal('single'), schema.literal('groups')])),
+    group_filters: schema.maybe(
+      schema.object({
+        group_by: schema.oneOf([
+          schema.literal('slo.tags'),
+          schema.literal('status'),
+          schema.literal('slo.indicator.type'),
+        ]),
+        groups: schema.maybe(schema.arrayOf(schema.string())),
+        filters: schema.maybe(schema.arrayOf(schema.object({}, { unknowns: 'allow' }))),
+        kql_query: schema.maybe(schema.string()),
+      })
+    ),
+  },
+  {
+    validate: (value) => {
+      // Require slo_id when overview_mode is 'single'
+      if (value.overview_mode === 'single' && !value.slo_id) {
+        return 'slo_id is required when overview_mode is "single"';
+      }
+      // Require group_filters when overview_mode is 'groups'
+      if (value.overview_mode === 'groups' && !value.group_filters) {
+        return 'group_filters is required when overview_mode is "groups"';
+      }
+    },
+  }
+);
+
 export const sloOverviewEmbeddableSchema = schema.allOf(
-  [
-    schema.object({
-      slo_id: schema.maybe(schema.string()),
-      slo_instance_id: schema.maybe(schema.string()),
-      show_all_group_by_instances: schema.maybe(schema.boolean()),
-      remote_name: schema.maybe(schema.string()),
-      overview_mode: schema.maybe(
-        schema.oneOf([schema.literal('single'), schema.literal('groups')])
-      ),
-      group_filters: schema.maybe(
-        schema.object({
-          group_by: schema.oneOf([
-            schema.literal('slo.tags'),
-            schema.literal('status'),
-            schema.literal('slo.indicator.type'),
-          ]),
-          groups: schema.maybe(schema.arrayOf(schema.string())),
-          filters: schema.maybe(schema.arrayOf(schema.object({}, { unknowns: 'allow' }))),
-          kql_query: schema.maybe(schema.string()),
-        })
-      ),
-    }),
-    serializedTitlesSchema,
-  ],
+  [sloOverviewStateSchema, serializedTitlesSchema],
   {
     meta: {
       description: 'SLO overview embeddable schema',
