0b5d632938b41425748cebc7eecd9964cb481974
Sebastian Delle Donne <sebastian.delledonne@elastic.co>
2026-02-02 14:59:39 +0100

    clean double licence

--- src/platform/packages/shared/kbn-esql-language/src/commands/definitions/utils/autocomplete/fields_list.ts
+++ src/platform/packages/shared/kbn-esql-language/src/commands/definitions/utils/autocomplete/fields_list.ts
@@ -6,15 +6,6 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
-
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the "Elastic License
- * 2.0"async , the "GNU Affero General Public License v3.0 only", and the "Server Side
- * Public License v 1"; you may not use this file except in compliance with, at
- * your election, the "Elastic License 2.0", the "GNU Affero General Public
- * License v3.0 only", or the "Server Side Public License, v 1".
- */
 import { isFunctionExpression, within, isAssignment, isColumn } from '../../../../ast';
 import type { ESQLAstAllCommands, ESQLAstField } from '../../../../types';
 import {
