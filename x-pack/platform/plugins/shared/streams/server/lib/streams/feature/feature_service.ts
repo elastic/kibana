37d306e8859b1cfe3fb94304d24093ce2c56e786
klacabane <kevin.lacabane@elastic.co>
2026-02-02 12:00:57 +0100

    fix migration

--- x-pack/platform/plugins/shared/streams/server/lib/streams/feature/feature_service.ts
+++ x-pack/platform/plugins/shared/streams/server/lib/streams/feature/feature_service.ts
@@ -7,14 +7,13 @@
 
 import type { CoreSetup, KibanaRequest, Logger } from '@kbn/core/server';
 import { StorageIndexAdapter } from '@kbn/storage-adapter';
-import { v4 as uuid } from 'uuid';
-import { featureSchema } from '@kbn/streams-schema';
 import type { StreamsPluginStartDependencies } from '../../../types';
 import { FeatureClient } from './feature_client';
 import type { StoredFeature } from './stored_feature';
 import type { FeatureStorageSettings } from './storage_settings';
 import { featureStorageSettings } from './storage_settings';
 import { FEATURE_ID, FEATURE_PROPERTIES, FEATURE_SUBTYPE, FEATURE_UUID } from './fields';
+import { storedFeatureSchema } from './stored_feature';
 
 export class FeatureService {
   constructor(
@@ -32,18 +31,16 @@ export class FeatureService {
       {
         migrateSource: (source) => {
           if (!(FEATURE_ID in source)) {
-            const id = uuid();
             const migrated: Record<string, unknown> = {
               ...source,
-              [FEATURE_ID]: id,
-              [FEATURE_UUID]: id,
+              [FEATURE_ID]: source[FEATURE_UUID],
               [FEATURE_SUBTYPE]: source['feature.name'],
               [FEATURE_PROPERTIES]: source['feature.value'],
             };
             delete migrated['feature.name'];
             delete migrated['feature.value'];
 
-            featureSchema.parse(migrated);
+            storedFeatureSchema.parse(migrated);
             return migrated as unknown as StoredFeature;
           }
 
