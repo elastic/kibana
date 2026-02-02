55198d6c2027b2fc9e8adfe3f63181b4843bdd3f
Angela Chuang <yi-chun.chuang@elastic.co>
2026-02-02 14:04:37 +0000

    Updated the logic to determine if QRadar rules are eligible for translation by changing from a filter to a some method

--- x-pack/solutions/security/plugins/security_solution/server/lib/siem_migrations/rules/api/rules/qradar/create.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/siem_migrations/rules/api/rules/qradar/create.ts
@@ -68,11 +68,11 @@ export const registerSiemRuleMigrationsCreateQRadarRulesRoute = (
               });
             }
 
-            const isEligibleForTranslation = qradarRules.filter(
+            const isEligibleForTranslation = qradarRules.some(
               (rule) => rule.rule_type !== 'building_block'
             );
 
-            if (isEligibleForTranslation.length === 0) {
+            if (!isEligibleForTranslation) {
               return res.badRequest({
                 body: { message: 'No valid rules could be extracted from the XML' },
               });
