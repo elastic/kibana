33c3df38e50ec52cf12bec562e3c615fd6767b41
Eyo Okon Eyo <eyo.eyo@elastic.co>
2026-02-02 19:01:50 +0100

    update reference to undocumented API

--- src/platform/packages/shared/shared-ux/document_data_cascade/impl/src/lib/core/virtualizer/index.tsx
+++ src/platform/packages/shared/shared-ux/document_data_cascade/impl/src/lib/core/virtualizer/index.tsx
@@ -151,7 +151,7 @@ export const useCascadeVirtualizer = <G extends GroupNode>({
       onChange: (rowVirtualizerInstance) => {
         // @ts-expect-error -- the itemsSizeCache property does exist,
         // but it not included in the type definition because it is marked as a private property,
-        // see {@link https://github.com/TanStack/virtual/blob/v3.13.2/packages/virtual-core/src/index.ts#L360}
+        // see {@link https://github.com/TanStack/virtual/blob/%40tanstack/virtual-core%403.13.18/packages/virtual-core/src/index.ts#L362}
         virtualizedRowsSizeCacheRef.current = rowVirtualizerInstance.itemSizeCache;
       },
     }),
