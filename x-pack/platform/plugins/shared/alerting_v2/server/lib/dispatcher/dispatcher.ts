d6618cec2ceced676d2d1d15d7232f308f23a81b
Kevin Delemme <kdelemme@gmail.com>
2026-02-02 10:19:53 -0500

    Remove non-implemented code

--- x-pack/platform/plugins/shared/alerting_v2/server/lib/dispatcher/dispatcher.ts
+++ x-pack/platform/plugins/shared/alerting_v2/server/lib/dispatcher/dispatcher.ts
@@ -19,7 +19,7 @@ import type { StorageServiceContract } from '../services/storage_service/storage
 import { StorageServiceInternalToken } from '../services/storage_service/tokens';
 import { LOOKBACK_WINDOW_MINUTES } from './constants';
 import { getDispatchableAlertEventsQuery } from './queries';
-import type { AlertEpisode, Policy, RuleId } from './types';
+import type { AlertEpisode } from './types';
 
 export interface DispatcherServiceContract {
   run({ previousStartedAt }: { previousStartedAt?: Date }): Promise<{ startedAt: Date }>;
@@ -60,51 +60,32 @@ export class DispatcherService implements DispatcherServiceContract {
         `Dispatcher found ${dispatchableAlertEvents.length} alert episodes to dispatch.`,
     });
 
-    const ruleIds = Array.from(new Set(dispatchableAlertEvents.map((n) => n.rule_id)));
+    const ruleIds = Array.from(new Set(dispatchableAlertEvents.map((event) => event.rule_id)));
     this.logger.debug({
       message: () =>
         `Dispatcher found ${ruleIds.length} unique rules with alert episodes to dispatch.`,
     });
 
-    // TODO: Rule might have more than one policy attached to it
-    const policies = await this.findPoliciesAttachedToRules(ruleIds);
-    this.logger.debug({
-      message: () =>
-        `Dispatcher found ${policies.size} policies attached to rules with alert episodes to dispatch.`,
+    // TODO:
+    // Fetch policies associated to ruleIds to determine how to dispatch each alert episode
+    // Suppress dispatchable alert events based on policies
+    // Log suppressed alert events
+    // Call policy defined workflow to dispatch alert events
+    // insert fire-event for non-suppressed alert events
+
+    const now = new Date().toISOString();
+    await this.storageService.bulkIndexDocs<AlertAction>({
+      index: ALERT_ACTIONS_DATA_STREAM,
+      docs: dispatchableAlertEvents.map((alertEpisode) => ({
+        '@timestamp': now,
+        group_hash: alertEpisode.group_hash,
+        last_series_event_timestamp: alertEpisode.last_event_timestamp,
+        actor: 'system',
+        action_type: 'fire-event',
+        rule_id: alertEpisode.rule_id,
+        source: 'internal',
+      })),
     });
-
-    for (const alertEpisode of dispatchableAlertEvents) {
-      // TODO: Rule might have more than one policy attached to it
-      const policy = policies.get(alertEpisode.rule_id);
-      if (!policy) {
-        this.logger.debug({
-          message: () =>
-            `No policy found for rule ${alertEpisode.rule_id}, skipping alert episode dispatch for group_hash ${alertEpisode.group_hash} and episode_id ${alertEpisode.episode_id}.`,
-        });
-      }
-
-      if (policy) {
-        this.logger.debug({
-          message: () =>
-            `Queuing workflow alert episode for rule ${alertEpisode.rule_id}, group_hash ${alertEpisode.group_hash}, episode_id ${alertEpisode.episode_id} using policy ${policy.name}.`,
-        });
-
-        await this.storageService.bulkIndexDocs<AlertAction>({
-          index: ALERT_ACTIONS_DATA_STREAM,
-          docs: [
-            {
-              '@timestamp': new Date().toISOString(),
-              group_hash: alertEpisode.group_hash,
-              last_series_event_timestamp: alertEpisode.last_event_timestamp,
-              actor: 'system',
-              action_type: 'fire-event',
-              rule_id: alertEpisode.rule_id,
-              source: 'internal',
-            },
-          ],
-        });
-      }
-    }
 
     this.logger.debug({
       message: () =>
@@ -112,12 +93,5 @@ export class DispatcherService implements DispatcherServiceContract {
     });
 
     return { startedAt };
-  }
-
-  private async findPoliciesAttachedToRules(ruleIds: string[]): Promise<Map<RuleId, Policy>> {
-    return ruleIds.reduce((acc, ruleId) => {
-      acc.set(ruleId, { id: `policy-${ruleId}`, name: `policy-name-${ruleId}` });
-      return acc;
-    }, new Map<RuleId, Policy>());
   }
 }
