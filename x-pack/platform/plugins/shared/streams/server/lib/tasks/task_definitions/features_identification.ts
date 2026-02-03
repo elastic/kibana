69a2c9cd8852d9caf23d05d223eeb7750f44f932
Francesco Fagnani <francesco.fagnani@elastic.co>
2026-02-03 15:12:45 +0100

    feat(computed features): add error_logs, log_patterns and log_samples computed features

--- x-pack/platform/plugins/shared/streams/server/lib/tasks/task_definitions/features_identification.ts
+++ x-pack/platform/plugins/shared/streams/server/lib/tasks/task_definitions/features_identification.ts
@@ -87,6 +87,7 @@ export function createStreamsFeaturesIdentificationTask(taskContext: TaskContext
                     start,
                     end,
                     esClient,
+                    logger: taskContext.logger.get('computed_features'),
                   }),
                 ]);
 
