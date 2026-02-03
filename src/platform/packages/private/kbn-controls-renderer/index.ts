3e2ac047907e031be190b4054b8905f0b881d43b
AlexGPlay <alex.garcia@elastic.co>
2026-02-03 10:40:30 +0100

    Refactor getEsqlControls into kbn-controls-renderer

--- src/platform/packages/private/kbn-controls-renderer/index.ts
+++ src/platform/packages/private/kbn-controls-renderer/index.ts
@@ -9,3 +9,4 @@
 
 export { ControlsRenderer } from './src/controls_renderer';
 export type { ControlsRendererParentApi, ControlsLayout } from './src/types';
+export { getEsqlControls } from './src/esql_controls';
