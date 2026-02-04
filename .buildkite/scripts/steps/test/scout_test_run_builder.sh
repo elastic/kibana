b81b3eefd4c6aa23f30a74e4631c6604f3a9cf5d
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2026-02-04 22:21:32 +0100

    [scout] discover tests with custom server configs (#251297)
    ## Summary
    
    This PR update Scout discovery command to optionally include configs
    associated with custom servers directories (located under
    `test/scout_*/` path):
    
    ```bash
    node scripts/scout.js discover-playwright-configs --include-custom-servers
    ```
    
    Since some Teams may decide not to run their config in the unified Scout
    CI execution, I added `excluded_configs` in
    `.buildkite/scout_ci_config.yml` to list them explicitly:
    
    ```
    plugins:
      enabled:
        - apm
        - console
        ...
      disabled:
    
    packages:
        ...
    
    # Define test configs to be excluded from automatic discovery & execution in CI environment (process.env.CI=true)
    excluded_configs:
      - x-pack/solutions/security/plugins/cloud_security_posture/test/scout_cspm_agentless/ui/parallel.playwright.config.ts
    ```
    
    While running on CI environment `node scripts/scout.js
    discover-playwright-configs` will exclude those configs and they won't
    be run.
    
    This PR also removed uiam tests custom CI script/step definition as it
    is no longer needed.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 7b13120890ee62c2dd3de2cf86d99e967fdcd06f)
    
    # Conflicts:
    #	.buildkite/pipelines/es_serverless/verify_es_serverless_image.yml
    #	.buildkite/pipelines/on_merge.yml
    #	.buildkite/pipelines/pull_request/profiling_cypress.yml
    #	.buildkite/scout_ci_config.yml
    #	.buildkite/scripts/pipelines/pull_request/pipeline.ts
    #	.buildkite/scripts/steps/test/scout_test_run_builder.sh
    #	src/platform/packages/shared/kbn-scout/src/cli/config_discovery.test.ts

--- .buildkite/scripts/steps/test/scout_test_run_builder.sh
+++ .buildkite/scripts/steps/test/scout_test_run_builder.sh
@@ -11,7 +11,7 @@ echo '--- Update Scout Test Config Manifests'
 node scripts/scout.js update-test-config-manifests
 
 echo '--- Discover Playwright Configs and upload to Buildkite artifacts (only stateful tests)'
-node scripts/scout discover-playwright-configs --target ech --save
+node scripts/scout discover-playwright-configs --include-custom-servers --target ech --save
 cp .scout/test_configs/scout_playwright_configs.json scout_playwright_configs.json
 buildkite-agent artifact upload "scout_playwright_configs.json"
 
