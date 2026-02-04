1d75e511ad64f7e919ab6c011615b8fa2f714b57
lgestc <lukasz.gmys@elastic.co>
2026-02-04 10:58:57 +0100

    --wip-- [skip ci]

--- x-pack/platform/plugins/shared/cases/public/components/templates_v2/field_types_registry.tsx
+++ x-pack/platform/plugins/shared/cases/public/components/templates_v2/field_types_registry.tsx
@@ -4,3 +4,51 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+import { z } from '@kbn/zod';
+import type { FC } from 'react';
+
+export const FieldType = {
+  INPUT_TEXT: 'INPUT_TEXT',
+  SELECT_BASIC: 'SELECT_BASIC',
+} as const;
+
+export type FieldType = (typeof FieldType)[keyof typeof FieldType];
+
+export const InputTextFieldSchema = z.object({
+  control: z.literal(FieldType.INPUT_TEXT),
+});
+
+export const SelectBasicFieldSchema = z.object({
+  control: z.literal(FieldType.SELECT_BASIC),
+  metadata: z.object({
+    options: z.array(z.string()),
+  }),
+});
+
+/**
+ * This can be used to parse `fields` section in the YAML `definition` of the template.
+ */
+export const FieldSchema = z.discriminatedUnion('control', [
+  InputTextFieldSchema,
+  SelectBasicFieldSchema,
+]);
+
+const InputText = (props: z.infer<typeof InputTextFieldSchema>) => {
+  return null;
+};
+InputText.displayName = 'InputText';
+
+const SelectBasic = (props: z.infer<typeof SelectBasicFieldSchema>) => {
+  return null;
+};
+SelectBasic.displayName = 'SelectBasic ';
+
+export type FieldMap = {
+  [K in FieldType]: FC<typeof FieldSchema & { control: K }>;
+};
+
+export const fieldRegistry: FieldMap = {
+  [FieldType.INPUT_TEXT]: InputText,
+  [FieldType.SELECT_BASIC]: SelectBasic,
+};
