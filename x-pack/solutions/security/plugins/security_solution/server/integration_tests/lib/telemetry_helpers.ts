ccea33166a4284baa3951eed0a4ebbe39e00d085
Hannah Brooks <hannah.brooks@elastic.co>
2026-02-04 09:08:26 -0500

    [Security Solution] Fix exception list os_types field returning empty array (#250279)
    ## Summary
    When a user would attempt to create a new exception list and they
    included `os_types` in the request body, the created list would not save
    or return the desired `os_types`.
    
    ## Changes
    The root of the issue was that `os_types` was not being extracted from
    the request body. This led to the downstream creation never receiving
    the value. I added the field to the `CreateExceptionListOptions` type in
    order for the `createExceptionList` to accept it. The downstream client
    function and type were also updated. The duplication service was also
    updated.
    
    (cherry picked from commit 5e43b5b377e8af3fb466221a5ccae279f6f5f78e)

--- x-pack/solutions/security/plugins/security_solution/server/integration_tests/lib/telemetry_helpers.ts
+++ x-pack/solutions/security/plugins/security_solution/server/integration_tests/lib/telemetry_helpers.ts
@@ -14,6 +14,7 @@ import type {
 import type {
   ExceptionListItemSchema,
   ExceptionListSchema,
+  OsTypeArray,
 } from '@kbn/securitysolution-io-ts-list-types';
 import { asyncForEach } from '@kbn/std';
 import { ToolingLog } from '@kbn/tooling-log';
@@ -334,6 +335,7 @@ export async function createMockedExceptionList(so: SavedObjectsServiceStart) {
   const meta = undefined;
   const user = '';
   const version = 1;
+  const osTypes: OsTypeArray = ['linux'];
   const tags: string[] = [];
   const tieBreaker = '';
 
@@ -346,6 +348,7 @@ export async function createMockedExceptionList(so: SavedObjectsServiceStart) {
     description,
     meta,
     user,
+    osTypes,
     tags,
     tieBreaker,
     type,
