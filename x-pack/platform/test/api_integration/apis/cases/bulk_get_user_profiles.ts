943dda2bfa54c7d4c2ee5660b462e642b2eb7975
“jeramysoucy” <jeramy.soucy@elastic.co>
2026-02-04 16:29:53 +0100

    Fixes cases tests

--- x-pack/platform/test/api_integration/apis/cases/bulk_get_user_profiles.ts
+++ x-pack/platform/test/api_integration/apis/cases/bulk_get_user_profiles.ts
@@ -37,7 +37,7 @@ export default ({ getService }: FtrProviderContext): void => {
       } with roles(s) ${user.roles.join()} can bulk get valid user profiles`, async () => {
         const suggestedProfiles = await suggestUserProfiles({
           supertest: supertestWithoutAuth,
-          req: { name: user.username, owners: [owner], size: 1 },
+          req: { name: user.username, owners: [owner] },
           auth: { user, space: null },
         });
 
@@ -50,8 +50,9 @@ export default ({ getService }: FtrProviderContext): void => {
           auth: { user, space: null },
         });
 
-        expect(profiles.length).to.be(1);
-        expect(profiles[0].user.username).to.eql(user.username);
+        expect(profiles.length).to.be(8);
+        const found = profiles.find((profile) => profile.user.username === user.username);
+        expect(found !== undefined).to.be.ok();
       });
     }
 
