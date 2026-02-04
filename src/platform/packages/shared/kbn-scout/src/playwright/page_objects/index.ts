b870e77661443b1d5be7445320575b63962b78d5
Maria Iriarte <106958839+mariairiartef@users.noreply.github.com>
2026-02-04 18:24:21 +0100

    [Lens] Init scout and add `should display ES|QL conversion modal` test (#249819)
    ## Summary
    
    Close https://github.com/elastic/kibana/issues/250383
    Close https://github.com/elastic/kibana/issues/250384
    
    - Init scout in Lens
    - Add `switchToEditMode` and `openInLineEditor` to the dashboard app
    - Create lens app and add the following methods:
      - `getConvertToEsqlButton`
      - `getConvertToEsqModal` 
      - `getConvertToEsqModalConfirmButton`
    - Add `should display ES|QL conversion modal` test
    
    For the test, we have used the LOGSTASH ES archive.
    
    > [!NOTE]
    > The complete conversion flow will be tested in a separate PR, once
    https://github.com/elastic/kibana/pull/248078 is merged.
    
    > [!IMPORTANT]
    > The test introduced in this pull request has been skipped until we
    introduce a feature flag to hide the feature we want to test:
    https://github.com/elastic/kibana-team/issues/2740
    
    ### How to run the tests
    
    
    Run server:
    ```
    node scripts/scout.js start-server --stateful
    ```
    
    In a separate terminal, run tests:
    ```
    npx playwright test --project local --grep @ess --config x-pack/platform/plugins/shared/lens/test/scout/ui/  --ui
    ```
    
    ## Checklist
    
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [x] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/platform/packages/shared/kbn-scout/src/playwright/page_objects/index.ts
+++ src/platform/packages/shared/kbn-scout/src/playwright/page_objects/index.ts
@@ -20,6 +20,7 @@ import { RenderablePage } from './renderable_page';
 import { Toasts } from './toasts';
 import { createLazyPageObject } from './utils';
 import { Inspector } from './inspector';
+import { LensApp } from './lens_app';
 
 export interface PageObjectsFixtures {
   page: ScoutPage;
@@ -37,6 +38,7 @@ export interface PageObjects {
   collapsibleNav: CollapsibleNav;
   toasts: Toasts;
   inspector: Inspector;
+  lens: LensApp;
 }
 
 /**
@@ -56,6 +58,7 @@ export function createCorePageObjects(fixtures: PageObjectsFixtures): PageObject
     collapsibleNav: createLazyPageObject(CollapsibleNav, fixtures.page, fixtures.config),
     toasts: createLazyPageObject(Toasts, fixtures.page),
     inspector: createLazyPageObject(Inspector, fixtures.page),
+    lens: createLazyPageObject(LensApp, fixtures.page),
     // Add new page objects here
   };
 }
