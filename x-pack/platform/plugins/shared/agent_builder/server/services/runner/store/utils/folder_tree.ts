461a0386237caf56f729c7dea5471e41173fd49b
pgayvallet <pierre.gayvallet@elastic.co>
2026-02-04 11:53:38 +0100

    ts failures

--- x-pack/platform/plugins/shared/agent_builder/server/services/runner/store/utils/folder_tree.ts
+++ x-pack/platform/plugins/shared/agent_builder/server/services/runner/store/utils/folder_tree.ts
@@ -6,7 +6,7 @@
  */
 
 import type {
-  FilestoreVersionedEntry,
+  FilestoreEntry,
   IFileStore,
   LsEntry,
   DirEntryWithChildren,
@@ -81,7 +81,7 @@ function buildTreeLines(
 ): void {
   // Separate directories and files
   const dirs = entries.filter((e): e is DirEntryWithChildren => e.type === 'dir');
-  const files = entries.filter((e): e is FilestoreVersionedEntry => e.type === 'file');
+  const files = entries.filter((e): e is FilestoreEntry => e.type === 'file');
 
   // Sort directories alphabetically by their basename
   dirs.sort((a, b) => {
