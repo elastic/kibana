35bd7eb6cb11e65e2b9ad4ee763d4054f455a4ca
Matt Wilde <matt.wilde@elastic.co>
2026-02-04 15:56:26 -0500

    sample data set composition method moved to plugin start

--- x-pack/platform/plugins/shared/sample_data_ingest/public/services/installation/installation_service.ts
+++ x-pack/platform/plugins/shared/sample_data_ingest/public/services/installation/installation_service.ts
@@ -6,7 +6,6 @@
  */
 
 import type { HttpSetup } from '@kbn/core-http-browser';
-import type { SampleDataSet } from '@kbn/home-sample-data-types';
 import {
   STATUS_API_PATH,
   INSTALL_API_PATH,
@@ -15,7 +14,6 @@ import {
   type InstalledResponse,
   type InstallingResponse,
 } from '../../../common';
-import { createSampleDataSet } from '../sample_data_set';
 
 export class InstallationService {
   private readonly http: HttpSetup;
@@ -34,18 +32,5 @@ export class InstallationService {
 
   async uninstall(): Promise<void> {
     await this.http.delete(UNINSTALL_API_PATH);
-  }
-
-  /**
-   * Returns the Elasticsearch documentation sample data set in the standard SampleDataSet format.
-   * Returns null if the status cannot be fetched (e.g., plugin not available).
-   */
-  async getSampleDataSet(): Promise<SampleDataSet | null> {
-    try {
-      const statusResponse = await this.getInstallationStatus();
-      return createSampleDataSet(statusResponse, this.http);
-    } catch {
-      return null;
-    }
   }
 }
