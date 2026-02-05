16acc23289fe5545b793eecb57c963d9881aad72
Alex Szabo <alex.szabo@elastic.co>
2026-02-05 11:31:42 +0100

    tidy up moon lint script

--- src/dev/eslint/run_eslint_through_moon.ts
+++ src/dev/eslint/run_eslint_through_moon.ts
@@ -15,51 +15,50 @@ import os from 'os';
 
 const IS_CI = !!process.env.CI;
 
-run(
-  async ({ log, flags }) => {
-    const bail = !!(flags.bail || false);
-
-    const moonCommand = IS_CI ? 'ci' : 'run';
-    const lintCommand = flags.fix ? ':eslint-fix' : ':eslint';
-    const cacheFlag = IS_CI ? [] : flags.updateCache ? ['-u'] : [];
-
-    const fullArgs = [moonCommand, lintCommand, ...cacheFlag];
-
-    log.info(`Running ESLint: 'moon ${fullArgs.join(' ')}'`);
-
-    const { exitCode } = await execa('moon', fullArgs, {
-      cwd: REPO_ROOT,
-      env: {
-        // Disable CI stats for individual runs, to avoid overloading ci-stats
-        CI_STATS_DISABLED: 'true',
-        PATH: process.env.PATH + `:${path.join('node_modules', '.bin')}`,
-        MOON_NO_ACTIONS: 'true',
-        MOON_CONCURRENCY: '' + (os.availableParallelism() - 1),
-      },
-      stdio: 'inherit',
-      reject: bail, // Don't throw on non-zero exit code
-    });
-
-    if (exitCode > 0) {
-      log.error(`Linting errors found ❌`);
-      process.exit(exitCode);
-    } else {
-      log.info('Linting successful ✅');
-    }
-  },
-  {
-    description: 'Run ESLint on all JavaScript/TypeScript files in the repository',
-    flags: {
-      boolean: ['bail', 'update-cache', 'fix'],
-      default: {
-        bail: false,
-        updateCache: false,
-      },
-      help: `
+const options = {
+  description: 'Run ESLint on all Moon projects in the repository.',
+  flags: {
+    boolean: ['bail', 'update-cache', 'fix'],
+    default: {
+      bail: false,
+      updateCache: false,
+    },
+    help: `
         --bail            Stop on the first linting error
         --update-cache    Updates Moon's caches for the lint targets
         --fix             Fix files
       `,
+  },
+};
+
+run(async ({ log, flags }) => {
+  const bail = !!(flags.bail || false);
+
+  const moonCommand = IS_CI ? 'ci' : 'run';
+  const lintCommand = flags.fix ? ':eslint-fix' : ':eslint';
+  const cacheFlag = IS_CI ? [] : flags.updateCache ? ['-u'] : [];
+
+  const fullArgs = [moonCommand, lintCommand, ...cacheFlag];
+
+  log.info(`Running ESLint: 'moon ${fullArgs.join(' ')}'`);
+
+  const { exitCode } = await execa('moon', fullArgs, {
+    cwd: REPO_ROOT,
+    env: {
+      // Disable CI stats for individual runs, to avoid overloading ci-stats
+      CI_STATS_DISABLED: 'true',
+      PATH: process.env.PATH + `:${path.join('node_modules', '.bin')}`,
+      MOON_NO_ACTIONS: 'true',
+      MOON_CONCURRENCY: '' + (os.availableParallelism() - 1),
     },
+    stdio: 'inherit',
+    reject: bail, // Don't throw on non-zero exit code
+  });
+
+  if (exitCode > 0) {
+    log.error(`Linting errors found ❌`);
+    process.exit(exitCode);
+  } else {
+    log.info('Linting successful ✅');
   }
-);
+}, options);
