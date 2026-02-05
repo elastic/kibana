7a923add42407fc93c2b503831eb169232d9e1eb
Samuel Brito <samuel.brito@elastic.co>
2026-02-05 13:10:11 +0000

    Re-added validations to new synthtrace scenario

--- src/platform/packages/shared/kbn-synthtrace/src/scenarios/kubernetes_logs_traces_pods.ts
+++ src/platform/packages/shared/kbn-synthtrace/src/scenarios/kubernetes_logs_traces_pods.ts
@@ -26,7 +26,7 @@ import {
   getKubernetesMessages,
   KUBERNETES_SERVICES,
 } from './helpers/logs_mock_data';
-import { parseStringToBoolean } from './helpers/logs_scenario_opts_parser';
+import { parseLogsScenarioOpts, parseStringToBoolean } from './helpers/logs_scenario_opts_parser';
 
 const ENVIRONMENT = getSynthtraceEnvironment(__filename);
 
@@ -115,7 +115,7 @@ function getTimestampBlock(timestamp: number, milliInterval: number) {
 }
 
 const scenario: Scenario<LogDocument | InfraDocument | ApmFields> = async (runOptions) => {
-  const { isLogsDb } = runOptions.scenarioOpts ?? DEFAULT_SCENARIO_OPTS;
+  const { isLogsDb } = parseLogsScenarioOpts(runOptions.scenarioOpts);
 
   return {
     bootstrap: async ({ logsEsClient }) => {
