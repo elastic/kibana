21b9b28636d35923211a176eacfc1d21376167e3
James Gowdy <jgowdy@elastic.co>
2026-02-06 12:27:13 +0000

    fixing file size check

--- x-pack/platform/plugins/private/file_upload/public/importer/get_max_bytes.ts
+++ x-pack/platform/plugins/private/file_upload/public/importer/get_max_bytes.ts
@@ -10,7 +10,6 @@ import {
   ABSOLUTE_MAX_FILE_SIZE_BYTES,
   FILE_SIZE_DISPLAY_FORMAT,
   MAX_FILE_SIZE,
-  MAX_FILE_SIZE_BYTES,
   MAX_TIKA_FILE_SIZE_BYTES,
   UI_SETTING_MAX_FILE_SIZE,
 } from '@kbn/file-upload-common/src/constants';
@@ -19,11 +18,8 @@ import { getUiSettings } from '../kibana_services';
 
 export function getMaxBytes() {
   const maxFileSize = getUiSettings().get(UI_SETTING_MAX_FILE_SIZE, MAX_FILE_SIZE);
-  // @ts-ignore
-  const maxBytes = numeral(maxFileSize.toUpperCase()).value();
-  if (maxBytes < MAX_FILE_SIZE_BYTES) {
-    return MAX_FILE_SIZE_BYTES;
-  }
+  // @ts-expect-error value does exist, but the elastic numeral type is incorrect
+  const maxBytes: number = numeral(maxFileSize.toUpperCase()).value();
   return maxBytes <= ABSOLUTE_MAX_FILE_SIZE_BYTES ? maxBytes : ABSOLUTE_MAX_FILE_SIZE_BYTES;
 }
 
