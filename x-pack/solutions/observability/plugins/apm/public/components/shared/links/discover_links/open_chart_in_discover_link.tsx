c3879f6b56a34340f9fa608633378899110cc310
TFCMarques <tiago.marques@elastic.co>
2026-02-06 10:32:32 +0000

    add get esql query util

--- x-pack/solutions/observability/plugins/apm/public/components/shared/links/discover_links/open_chart_in_discover_link.tsx
+++ x-pack/solutions/observability/plugins/apm/public/components/shared/links/discover_links/open_chart_in_discover_link.tsx
@@ -7,78 +7,11 @@
 
 import { i18n } from '@kbn/i18n';
 import React from 'react';
-import type { ApmIndexSettingsResponse } from '@kbn/apm-sources-access-plugin/server/routes/settings';
-import { from } from '@kbn/esql-composer';
 import { useApmServiceContext } from '../../../../context/apm_service/use_apm_service_context';
 import { useAnyOfApmParams } from '../../../../hooks/use_apm_params';
 import { useEnvironmentsContext } from '../../../../context/environments_context/use_environments_context';
-import {
-  ENVIRONMENT_ALL_VALUE,
-  ENVIRONMENT_NOT_DEFINED_VALUE,
-} from '../../../../../common/environment_filter_values';
 import { BaseDiscoverLink } from './base_discover_link';
-import {
-  filterByEnvironment,
-  filterByKuery,
-  filterByServiceName,
-  filterByTransactionType,
-  filterByTransactionNameOrSpanName,
-} from './filters';
-
-const getESQLQuery = ({
-  params,
-  indexSettings,
-}: {
-  params: {
-    serviceName?: string;
-    kuery?: string;
-    environment?: string;
-    transactionName?: string;
-    transactionType?: string;
-  };
-  indexSettings: ApmIndexSettingsResponse['apmIndexSettings'];
-}) => {
-  if (!indexSettings || indexSettings?.length === 0) {
-    return null;
-  }
-
-  const { serviceName, kuery, environment, transactionType, transactionName } = params;
-
-  const tracesIndices = indexSettings
-    .filter((indexSetting) => ['span', 'transaction'].includes(indexSetting.configurationName))
-    .map((indexSetting) => indexSetting.savedValue ?? indexSetting.defaultValue);
-  const dedupedIndices = Array.from(new Set(tracesIndices)).join(',');
-
-  const filters = [];
-
-  if (serviceName) {
-    filters.push(filterByServiceName(serviceName));
-  }
-
-  if (
-    environment &&
-    environment !== ENVIRONMENT_ALL_VALUE &&
-    environment !== ENVIRONMENT_NOT_DEFINED_VALUE
-  ) {
-    filters.push(filterByEnvironment(environment));
-  }
-
-  if (transactionName) {
-    filters.push(filterByTransactionNameOrSpanName(transactionName, undefined));
-  }
-
-  if (transactionType) {
-    filters.push(filterByTransactionType(transactionType));
-  }
-
-  if (kuery) {
-    filters.push(filterByKuery(kuery));
-  }
-
-  return from(dedupedIndices)
-    .pipe(...filters)
-    .toString();
-};
+import { getESQLQuery } from './get_esql_query';
 
 export function OpenChartInDiscoverLink({ dataTestSubj }: { dataTestSubj: string }) {
   const { serviceName, transactionType, indexSettings } = useApmServiceContext();
@@ -99,9 +32,10 @@ export function OpenChartInDiscoverLink({ dataTestSubj }: { dataTestSubj: string
     'transactionName' in queryParams ? queryParams.transactionName : undefined;
 
   const esqlQuery = getESQLQuery({
+    indexType: 'traces',
     params: {
-      serviceName,
       kuery,
+      serviceName,
       environment,
       transactionName,
       transactionType,
