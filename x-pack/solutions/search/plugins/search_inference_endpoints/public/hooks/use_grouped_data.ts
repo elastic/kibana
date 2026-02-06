42097169fff1fa3e1ae3070164a94eccbfb1b2d9
Rodney Norris <rodney.norris@elastic.co>
2026-02-06 22:09:16 +0000

    add grouped data hook

---
+++ x-pack/solutions/search/plugins/search_inference_endpoints/public/hooks/use_grouped_data.ts
@@ -0,0 +1,82 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useMemo } from 'react';
+import type { InferenceInferenceEndpointInfo } from '@elastic/elasticsearch/lib/api/types';
+import type { InferenceAPIConfigResponse } from '@kbn/ml-trained-models-utils';
+
+import {
+  type FilterOptions,
+  type QueryParams,
+  type GroupedInferenceEndpointsData,
+  GroupByOptions,
+} from '../types';
+import { getModelId } from '../utils/get_model_id';
+import { useFilteredInferenceEndpoints } from './use_filtered_endpoints';
+
+export interface UseGroupedDataResult {
+  data: Record<string, GroupedInferenceEndpointsData>;
+}
+
+function GroupByReducer(groupBy: GroupByOptions) {
+  switch (groupBy) {
+    case GroupByOptions.Model:
+      return GroupByModelReducer;
+    case GroupByOptions.None:
+    default:
+      throw new Error('Grouping is not enabled');
+  }
+}
+
+export const GroupByModelReducer = (
+  acc: Record<string, GroupedInferenceEndpointsData>,
+  endpoint: InferenceInferenceEndpointInfo,
+  index: number,
+  array: InferenceInferenceEndpointInfo[]
+): Record<string, GroupedInferenceEndpointsData> => {
+  const modelId = getModelId(endpoint) ?? 'unknown_model';
+  if (modelId in acc) {
+    acc[modelId].endpoints.push(endpoint);
+  } else {
+    acc[modelId] = {
+      groupId: modelId,
+      groupLabel: modelId,
+      endpoints: [endpoint],
+    };
+  }
+  return acc;
+};
+
+export const useGroupedData = (
+  inferenceEndpoints: InferenceAPIConfigResponse[],
+  queryParams: QueryParams,
+  filterOptions: FilterOptions,
+  searchKey: string
+): UseGroupedDataResult => {
+  if (queryParams.groupBy === GroupByOptions.None) {
+    throw new Error('Grouping is not enabled');
+  }
+
+  const filteredEndpoints = useFilteredInferenceEndpoints(
+    inferenceEndpoints,
+    filterOptions,
+    searchKey
+  );
+
+  const groupedEndpoints = useMemo(
+    () =>
+      filteredEndpoints.reduce<Record<string, GroupedInferenceEndpointsData>>(
+        GroupByReducer(queryParams.groupBy),
+        {}
+      ),
+    [queryParams.groupBy, filteredEndpoints]
+  );
+
+  return {
+    data: groupedEndpoints,
+  };
+};
