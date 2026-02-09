ac05cb215bc906245ef95c8e8a3287dcae83b880
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-02-09 15:26:18 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/platform/packages/shared/kbn-connector-specs/src/connector_icons_map.ts
+++ src/platform/packages/shared/kbn-connector-specs/src/connector_icons_map.ts
@@ -76,7 +76,8 @@ export const ConnectorIconsMap: Map<
   [
     '.jira-cloud',
     lazy(
-      () => import(/* webpackChunkName: "connectorIconJiraCloud" */ './specs/atlassian/jira-cloud/icon')
+      () =>
+        import(/* webpackChunkName: "connectorIconJiraCloud" */ './specs/atlassian/jira-cloud/icon')
     ),
   ],
   [
