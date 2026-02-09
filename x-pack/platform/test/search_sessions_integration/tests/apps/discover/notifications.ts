62a1bb14ae8e04d1fbaab5cdc1e5af141791bc30
AlexGPlay <alex.garcia@elastic.co>
2026-02-09 10:37:50 +0100

    Update tests

--- x-pack/platform/test/search_sessions_integration/tests/apps/discover/notifications.ts
+++ x-pack/platform/test/search_sessions_integration/tests/apps/discover/notifications.ts
@@ -5,116 +5,38 @@
  * 2.0.
  */
 
-import { ELASTIC_HTTP_VERSION_HEADER } from '@kbn/core-http-common';
-import { INITIAL_SEARCH_SESSION_REST_VERSION } from '@kbn/data-plugin/server';
-import expect from '@kbn/expect';
 import type { FtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
-  const kibanaServer = getService('kibanaServer');
   const retry = getService('retry');
   const monacoEditor = getService('monacoEditor');
   const searchSessions = getService('searchSessions');
   const toasts = getService('toasts');
-  const testSubjects = getService('testSubjects');
-  const security = getService('security');
+  const esArchiver = getService('esArchiver');
 
-  const { common, unifiedFieldList, discover, home, searchSessionsManagement } = getPageObjects([
-    'common',
-    'unifiedFieldList',
-    'discover',
-    'home',
-    'searchSessionsManagement',
-  ]);
+  const { common, timePicker, discover } = getPageObjects(['common', 'discover', 'timePicker']);
 
   describe('Discover background search notifications', function () {
     before(async () => {
-      await kibanaServer.importExport.load(
-        'x-pack/platform/test/functional/fixtures/kbn_archives/discover/default'
+      await esArchiver.loadIfNeeded(
+        'src/platform/test/functional/fixtures/es_archiver/logstash_functional'
       );
-      await kibanaServer.uiSettings.replace({
-        enableESQL: true,
-      });
-      await common.navigateToUrl('home', '/tutorial_directory/sampleData', {
-        useActualUrl: true,
-      });
-      await retry.tryForTime(10000, async () => {
-        await home.addSampleDataSet('flights');
-        const isInstalled = await home.isSampleDataSetInstalled('flights');
-        expect(isInstalled).to.be(true);
-      });
-
       await common.navigateToApp('discover');
-      await unifiedFieldList.waitUntilSidebarHasLoaded();
       await discover.selectTextBaseLang();
-    });
-
-    afterEach(async () => {
-      await toasts.dismissAll();
-      await searchSessions.deleteAllSearchSessions();
     });
 
     it('shows a completion toast for background search', async () => {
-      await monacoEditor.setCodeEditorValue(
-        'FROM kibana_sample_data_flights | LIMIT 10 | WHERE DELAY(1000ms)'
-      );
+      // Set the time range so there are results for the query
+      await timePicker.setDefaultAbsoluteRange();
+      await monacoEditor.setCodeEditorValue('FROM logstash-* | LIMIT 1 | EVAL DELAY(5000ms)');
 
+      // Run the query and send the search session to background
       await searchSessions.save({ withRefresh: true });
 
-      await searchSessionsManagement.goTo();
-      await retry.waitFor('session should be in a completed status', async () => {
-        const searchSessionList = await searchSessionsManagement.getList();
-        return searchSessionList[0].status === 'complete';
-      });
-
-      await retry.waitFor('completion toast link should appear', async () => {
-        return await testSubjects.exists('backgroundSearchCompletedToastLink');
-      });
-    });
-
-    it('shows a failure toast for background search', async () => {
-      await common.navigateToApp('discover');
-      await unifiedFieldList.waitUntilSidebarHasLoaded();
-      await discover.selectTextBaseLang();
-
-      await monacoEditor.setCodeEditorValue(
-        'FROM kibana_sample_data_flights | WHERE DELAY(1000ms) | LIMIT 10'
-      );
-      await searchSessions.save({ withRefresh: true });
-
-      const sessionId = await retry.waitFor('session id should be available', async () => {
-        const { body } = await security.testUserSupertest
-          .post('/internal/session/_find')
-          .set(ELASTIC_HTTP_VERSION_HEADER, INITIAL_SEARCH_SESSION_REST_VERSION)
-          .set('kbn-xsrf', 'anything')
-          .set('kbn-system-request', 'true')
-          .send({
-            page: 1,
-            perPage: 1,
-            sortField: 'created',
-            sortOrder: 'desc',
-          })
-          .expect(200);
-
-        const latestSessionId = body.saved_objects?.[0]?.id;
-        return latestSessionId ? latestSessionId : false;
-      });
-
-      await security.testUserSupertest
-        .post(`/internal/session/${sessionId}/cancel`)
-        .set(ELASTIC_HTTP_VERSION_HEADER, INITIAL_SEARCH_SESSION_REST_VERSION)
-        .set('kbn-xsrf', 'anything')
-        .set('kbn-system-request', 'true')
-        .expect(200);
-
-      await searchSessionsManagement.goTo();
-      await retry.waitFor('session should be in a cancelled status', async () => {
-        const searchSessionList = await searchSessionsManagement.getList();
-        return searchSessionList[0].status === 'cancelled';
-      });
-
-      await retry.waitFor('failure toast link should appear', async () => {
-        return await testSubjects.exists('backgroundSearchFailedToastLink');
+      // Verify the completion toast is shown
+      await retry.waitFor('completion toast is shown', async () => {
+        const toastContent = await toasts.getContentByIndex(1); // 0 is the sent to background toast, 1 is the completion toast
+        return toastContent.includes('Background search completed');
       });
     });
   });
