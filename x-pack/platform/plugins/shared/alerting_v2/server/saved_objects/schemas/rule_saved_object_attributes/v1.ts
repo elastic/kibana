1cefc954ac41c4dfb79ba032e2f9d1577c9eec01
Christos Nasikas <christos.nasikas@elastic.co>
2026-02-09 17:19:27 +0200

    Fix SO mapping

--- x-pack/platform/plugins/shared/alerting_v2/server/saved_objects/schemas/rule_saved_object_attributes/v1.ts
+++ x-pack/platform/plugins/shared/alerting_v2/server/saved_objects/schemas/rule_saved_object_attributes/v1.ts
@@ -19,12 +19,10 @@ export const ruleSavedObjectAttributesSchema = schema.object({
     custom: schema.string(),
   }),
   enabled: schema.boolean({ defaultValue: true }),
-
   query: schema.string(),
   timeField: schema.string(),
   lookbackWindow: schema.string(),
   groupingKey: schema.arrayOf(schema.string(), { defaultValue: [] }),
-
   createdBy: schema.nullable(schema.string()),
   updatedBy: schema.nullable(schema.string()),
   updatedAt: schema.string(),
