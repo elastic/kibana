82e24ae1cf24bd0c59504fc9c056a3421e563be3
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2026-02-09 21:17:01 +0100

    [Scout] Move banners FTR tests to Scout

---
+++ x-pack/platform/plugins/private/banners/test/scout_banners/ui/fixtures/page_objects/banners.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { ScoutPage } from '@kbn/scout';
+
+export class BannersPageObject {
+  constructor(private readonly page: ScoutPage) {}
+
+  async isTopBannerVisible(): Promise<boolean> {
+    try {
+      await this.page.testSubj.waitForSelector('bannerInnerWrapper', {
+        state: 'visible',
+        timeout: 2000,
+      });
+      return true;
+    } catch {
+      return false;
+    }
+  }
+
+  async getTopBannerText(): Promise<string> {
+    if (!(await this.isTopBannerVisible())) {
+      return '';
+    }
+
+    const bannerText = await this.page.testSubj.locator('bannerInnerWrapper').innerText();
+    return bannerText;
+  }
+}
---
+++ x-pack/platform/plugins/private/banners/test/scout_banners/ui/tests/global.spec.ts
@@ -0,0 +1,19 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { tags } from '@kbn/scout';
+import { expect } from '@kbn/scout/ui';
+import { test } from '../fixtures';
+
+test.describe('global pages', { tag: tags.ESS_ONLY }, () => {
+  test('displays the global banner on the login page', async ({ page, pageObjects, kbnUrl }) => {
+    await page.goto(kbnUrl.get('/login'));
+
+    expect(await pageObjects.banners.isTopBannerVisible()).toBe(true);
+    expect(await pageObjects.banners.getTopBannerText()).toBe('global banner text');
+  });
+});
