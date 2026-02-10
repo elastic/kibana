b3e2b36c6ce9c3e2ef04f5e6296c82ba2253f972
miriam.aparicio <miriam.aparicio@gmail.com>
2026-02-10 14:50:15 +0000

    add loading state to health panel

--- x-pack/solutions/observability/plugins/apm/public/components/app/service_overview/service_health_panel/index.tsx
+++ x-pack/solutions/observability/plugins/apm/public/components/app/service_overview/service_health_panel/index.tsx
@@ -13,6 +13,9 @@ import {
   EuiText,
   EuiLink,
   EuiSpacer,
+  EuiHealth,
+  EuiLoadingSpinner,
+  useEuiTheme,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { ALERT_STATUS_ACTIVE } from '@kbn/rule-data-utils';
@@ -20,13 +23,17 @@ import React from 'react';
 import { useApmServiceContext } from '../../../../context/apm_service/use_apm_service_context';
 import { useApmParams } from '../../../../hooks/use_apm_params';
 import { useApmRouter } from '../../../../hooks/use_apm_router';
-import { useFetcher } from '../../../../hooks/use_fetcher';
+import { useFetcher, isPending } from '../../../../hooks/use_fetcher';
 import { useTimeRange } from '../../../../hooks/use_time_range';
 import { HealthBadge } from '../../service_inventory/service_list/health_badge';
+import { ServiceHealthStatus } from '../../../../../common/service_health_status';
+import { getServiceHealthStatusColor } from '../../../../../common/service_health_status';
+import { asInteger, asDuration } from '../../../../../common/utils/formatters';
 
 export function ServiceHealthPanel() {
   const { serviceName } = useApmServiceContext();
   const router = useApmRouter();
+  const { euiTheme } = useEuiTheme();
   const {
     query,
     query: { rangeFrom, rangeTo, environment },
@@ -35,7 +42,7 @@ export function ServiceHealthPanel() {
   const { start, end } = useTimeRange({ rangeFrom, rangeTo });
 
   // Fetch service health data
-  const { data: serviceHealthData } = useFetcher(
+  const { data: serviceHealthData, status } = useFetcher(
     (callApmApi) => {
       if (serviceName && start && end) {
         return callApmApi('GET /internal/apm/services/{serviceName}/health_status', {
@@ -53,19 +60,17 @@ export function ServiceHealthPanel() {
     [serviceName, start, end, environment]
   );
 
+  const isLoading = isPending(status);
+
   const combinedHealthStatus = serviceHealthData?.combinedHealthStatus;
   const alertsCount = serviceHealthData?.alertsCount;
   const sloStatus = serviceHealthData?.sloStatus;
   const sloCount = serviceHealthData?.sloCount;
+  const anomalyHealthStatus = serviceHealthData?.anomalyHealthStatus;
+  const anomalyScore = serviceHealthData?.anomalyScore;
+  const actualValue = serviceHealthData?.actualValue;
 
-  // Don't show panel if there's no health data
-  if (
-    !combinedHealthStatus &&
-    (!alertsCount || alertsCount === 0) &&
-    (!sloCount || sloCount === 0)
-  ) {
-    return null;
-  }
+  const hasAnomalyScore = anomalyScore !== undefined && anomalyScore !== null;
 
   return (
     <EuiPanel hasBorder={true}>
@@ -77,8 +82,15 @@ export function ServiceHealthPanel() {
         </h3>
       </EuiTitle>
       <EuiSpacer size="m" />
-      <EuiFlexGroup direction="column" gutterSize="s">
-        {combinedHealthStatus && (
+      {isLoading ? (
+        <EuiFlexGroup justifyContent="center" alignItems="center" style={{ minHeight: 60 }}>
+          <EuiFlexItem grow={false}>
+            <EuiLoadingSpinner size="m" />
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      ) : (
+        <EuiFlexGroup direction="column" gutterSize="s">
+          {/* Always show combined health status */}
           <EuiFlexItem>
             <EuiFlexGroup alignItems="center" gutterSize="s">
               <EuiFlexItem grow={false}>
@@ -89,68 +101,105 @@ export function ServiceHealthPanel() {
                 </EuiText>
               </EuiFlexItem>
               <EuiFlexItem grow={false}>
-                <HealthBadge healthStatus={combinedHealthStatus} />
+                <HealthBadge healthStatus={combinedHealthStatus ?? ServiceHealthStatus.unknown} />
               </EuiFlexItem>
             </EuiFlexGroup>
           </EuiFlexItem>
-        )}
 
-        {alertsCount !== undefined && alertsCount > 0 && (
-          <EuiFlexItem>
-            <EuiFlexGroup alignItems="center" gutterSize="s">
-              <EuiFlexItem grow={false}>
-                <EuiText size="s" color="subdued">
-                  {i18n.translate('xpack.apm.serviceOverview.healthPanel.alerts', {
-                    defaultMessage: 'Alerts:',
-                  })}
-                </EuiText>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiLink
-                  data-test-subj="serviceHealthPanelAlertsBadgeLink"
-                  href={router.link('/services/{serviceName}/alerts', {
-                    path: { serviceName },
-                    query: {
-                      ...query,
-                      alertStatus: ALERT_STATUS_ACTIVE,
-                    },
-                  })}
-                >
-                  <EuiText size="s">
-                    {i18n.translate('xpack.apm.serviceOverview.healthPanel.alertsCount', {
-                      defaultMessage:
-                        '{count, plural, one {# active alert} other {# active alerts}}',
-                      values: { count: alertsCount },
+          {alertsCount !== undefined && alertsCount > 0 && (
+            <EuiFlexItem>
+              <EuiFlexGroup alignItems="center" gutterSize="s">
+                <EuiFlexItem grow={false}>
+                  <EuiText size="s" color="subdued">
+                    {i18n.translate('xpack.apm.serviceOverview.healthPanel.alerts', {
+                      defaultMessage: 'Alerts:',
                     })}
                   </EuiText>
-                </EuiLink>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiFlexItem>
-        )}
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiLink
+                    data-test-subj="serviceHealthPanelAlertsBadgeLink"
+                    href={router.link('/services/{serviceName}/alerts', {
+                      path: { serviceName },
+                      query: {
+                        ...query,
+                        alertStatus: ALERT_STATUS_ACTIVE,
+                      },
+                    })}
+                  >
+                    <EuiText size="s">
+                      {i18n.translate('xpack.apm.serviceOverview.healthPanel.alertsCount', {
+                        defaultMessage:
+                          '{count, plural, one {# active alert} other {# active alerts}}',
+                        values: { count: alertsCount },
+                      })}
+                    </EuiText>
+                  </EuiLink>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexItem>
+          )}
 
-        {sloStatus && sloCount !== undefined && sloCount > 0 && (
-          <EuiFlexItem>
-            <EuiFlexGroup alignItems="center" gutterSize="s">
-              <EuiFlexItem grow={false}>
-                <EuiText size="s" color="subdued">
-                  {i18n.translate('xpack.apm.serviceOverview.healthPanel.slos', {
-                    defaultMessage: 'SLOs:',
-                  })}
-                </EuiText>
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiText size="s">
-                  {i18n.translate('xpack.apm.serviceOverview.healthPanel.sloStatus', {
-                    defaultMessage: '{count, plural, one {# SLO} other {# SLOs}} - {status}',
-                    values: { count: sloCount, status: sloStatus },
-                  })}
-                </EuiText>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          </EuiFlexItem>
-        )}
-      </EuiFlexGroup>
+          {sloStatus && sloCount !== undefined && sloCount > 0 && (
+            <EuiFlexItem>
+              <EuiFlexGroup alignItems="center" gutterSize="s">
+                <EuiFlexItem grow={false}>
+                  <EuiText size="s" color="subdued">
+                    {i18n.translate('xpack.apm.serviceOverview.healthPanel.slos', {
+                      defaultMessage: 'SLOs:',
+                    })}
+                  </EuiText>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiText size="s">
+                    {i18n.translate('xpack.apm.serviceOverview.healthPanel.sloStatus', {
+                      defaultMessage: '{count, plural, one {# SLO} other {# SLOs}} - {status}',
+                      values: { count: sloCount, status: sloStatus },
+                    })}
+                  </EuiText>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexItem>
+          )}
+
+          {hasAnomalyScore && (
+            <EuiFlexItem>
+              <EuiFlexGroup alignItems="center" gutterSize="s">
+                <EuiFlexItem grow={false}>
+                  <EuiText size="s" color="subdued">
+                    {i18n.translate('xpack.apm.serviceOverview.healthPanel.anomalyScore', {
+                      defaultMessage: 'Anomaly score:',
+                    })}
+                  </EuiText>
+                </EuiFlexItem>
+                <EuiFlexItem grow={false}>
+                  <EuiFlexGroup gutterSize="xs" alignItems="center" responsive={false}>
+                    <EuiFlexItem grow={false}>
+                      <EuiHealth
+                        color={getServiceHealthStatusColor(
+                          euiTheme,
+                          anomalyHealthStatus ?? ServiceHealthStatus.unknown
+                        )}
+                      />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText size="s">
+                        {anomalyScore > 0 && anomalyScore < 1 ? '< 1' : asInteger(anomalyScore)}
+                        {actualValue && (
+                          <EuiText size="s" color="subdued" component="span">
+                            {' '}
+                            ({asDuration(actualValue)})
+                          </EuiText>
+                        )}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                </EuiFlexItem>
+              </EuiFlexGroup>
+            </EuiFlexItem>
+          )}
+        </EuiFlexGroup>
+      )}
     </EuiPanel>
   );
 }
