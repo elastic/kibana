7be8d2e12239b8ba1b6d7a7848f31ce21ac493a1
Nathan Reese <reese.nathan@gmail.com>
2026-02-11 14:36:37 -0700

    update discover plugins load expects

--- x-pack/platform/plugins/private/discover_enhanced/test/scout/ui/tests/discover_cdp_perf.spec.ts
+++ x-pack/platform/plugins/private/discover_enhanced/test/scout/ui/tests/discover_cdp_perf.spec.ts
@@ -71,6 +71,7 @@ test.describe(
       ).toStrictEqual([
         'aiops',
         'discover',
+        'embeddable',
         'eventAnnotation',
         'expressionXY',
         'kbn-ui-shared-deps-npm',
