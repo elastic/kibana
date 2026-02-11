b194fbe8f61c642fb58a6610f9e176f08c7a778b
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2026-02-11 11:07:48 +0100

    [Scout] Move spaces_selection FTR tests to Scout

---
+++ x-pack/platform/plugins/shared/spaces/test/scout/ui/tests/spaces_selection_serverless.spec.ts
@@ -0,0 +1,69 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { test } from '../fixtures';
+import { expect } from '@kbn/scout/ui';
+
+test.describe('Spaces selection', { tag: ['@svlOblt', '@svlSearch', '@svlSecurity'] }, () => {
+  test('as Viewer - displays the space selection menu in header', async ({
+    browserAuth,
+    pageObjects,
+  }) => {
+    await browserAuth.loginAsViewer();
+    await pageObjects.spaces.navigateToHome();
+
+    const isHeaderVisible = await pageObjects.spaces.isProjectHeaderVisible();
+    expect(isHeaderVisible).toBe(true);
+
+    const isSelectorVisible = await pageObjects.spaces.isSpacesSelectorVisible();
+    expect(isSelectorVisible).toBe(true);
+  });
+
+  test('as Viewer - does not display the manage button in the space selection menu', async ({
+    browserAuth,
+    pageObjects,
+  }) => {
+    await browserAuth.loginAsViewer();
+    await pageObjects.spaces.navigateToHome();
+
+    const isHeaderVisible = await pageObjects.spaces.isProjectHeaderVisible();
+    expect(isHeaderVisible).toBe(true);
+
+    await pageObjects.spaces.openSpacesSelector();
+
+    const isManageVisible = await pageObjects.spaces.isManageButtonVisible();
+    expect(isManageVisible).toBe(false);
+  });
+
+  test('as Admin - displays the space selection menu in header', async ({
+    browserAuth,
+    pageObjects,
+  }) => {
+    await browserAuth.loginAsAdmin();
+    await pageObjects.spaces.navigateToHome();
+
+    const isHeaderVisible = await pageObjects.spaces.isProjectHeaderVisible();
+    expect(isHeaderVisible).toBe(true);
+
+    const isSelectorVisible = await pageObjects.spaces.isSpacesSelectorVisible();
+    expect(isSelectorVisible).toBe(true);
+  });
+
+  test('as Admin - displays the manage button in the space selection menu', async ({
+    browserAuth,
+    pageObjects,
+  }) => {
+    await browserAuth.loginAsAdmin();
+    await pageObjects.spaces.navigateToHome();
+
+    const isHeaderVisible = await pageObjects.spaces.isProjectHeaderVisible();
+    expect(isHeaderVisible).toBe(true);
+
+    await pageObjects.spaces.openSpacesSelector();
+    await pageObjects.spaces.waitForManageButton();
+  });
+});
