0112ff7646d4b835067adaf4d2f952658e7087e0
Stratoula <efstratia.kalafateli@elastic.co>
2026-02-12 10:23:55 +0100

    [ES|QL] Views in Discover

--- src/platform/packages/shared/kbn-esql-utils/src/index.ts
+++ src/platform/packages/shared/kbn-esql-utils/src/index.ts
@@ -31,7 +31,10 @@ export {
   hasDateBreakdown,
   hasOnlySourceCommand,
 } from './utils/query_parsing_helpers';
-export { getIndexPatternFromESQLQuery } from './utils/get_index_pattern_from_query';
+export {
+  expandIndexPatternWithViewsList,
+  getIndexPatternFromESQLQuery,
+} from './utils/get_index_pattern_from_query';
 export { queryCannotBeSampled } from './utils/query_cannot_be_sampled';
 export { appendToESQLQuery } from './utils/append_to_query/utils';
 export { appendStatsByToQuery } from './utils/append_to_query/append_stats_by';
