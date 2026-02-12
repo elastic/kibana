7f728221a5ca3be6a0dd854f42114ae83b85fd57
Alainna Halliday <alainna.halliday@elastic.co>
2026-02-12 14:37:24 -0500

    Upgrade security detection rule management owned files to zod v4

--- x-pack/solutions/security/plugins/security_solution/common/api/timeline/persist_favorite/persist_favorite_route.gen.ts
+++ x-pack/solutions/security/plugins/security_solution/common/api/timeline/persist_favorite/persist_favorite_route.gen.ts
@@ -14,7 +14,7 @@
  *   version: 2023-10-31
  */
 
-import { z } from '@kbn/zod';
+import { z } from '@kbn/zod/v4';
 
 import { TimelineType, FavoriteTimelineResponse } from '../model/components.gen';
 
