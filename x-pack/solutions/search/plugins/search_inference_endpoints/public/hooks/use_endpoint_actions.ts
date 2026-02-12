612e5ed61a44ff74f1def6674e7f0f9cecf35779
Saikat Sarkar <saikat.sarkar@elastic.co>
2026-02-11 19:14:55 -0700

    Refactor code

--- x-pack/solutions/search/plugins/search_inference_endpoints/public/hooks/use_endpoint_actions.ts
+++ x-pack/solutions/search/plugins/search_inference_endpoints/public/hooks/use_endpoint_actions.ts
@@ -26,7 +26,6 @@ export const useEndpointActions = () => {
         () => {
           toasts?.addSuccess({
             title: message,
-            'aria-label': message,
           });
         },
         () => {
