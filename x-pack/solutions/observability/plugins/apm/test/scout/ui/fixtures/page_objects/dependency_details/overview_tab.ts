ec3743f54e0b1e9d380b6019b3bf829474b12b43
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-02-13 15:12:37 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/observability/plugins/apm/test/scout/ui/fixtures/page_objects/dependency_details/overview_tab.ts
+++ x-pack/solutions/observability/plugins/apm/test/scout/ui/fixtures/page_objects/dependency_details/overview_tab.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 import type { KibanaUrl, Locator, ScoutPage } from '@kbn/scout-oblt';
 import capitalize from 'lodash/capitalize';
 import type { DependencyDetailsPageTabName } from './dependency_details_tab';
