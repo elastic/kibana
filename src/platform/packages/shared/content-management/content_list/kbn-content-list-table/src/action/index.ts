6486ad9877fd2e6927cf53de64e5b46784f9c078
Clint Andrew Hall <clint@clintandrewhall.com>
2026-02-13 13:52:05 -0500

    [content-list] Add Actions column, delete modal

--- src/platform/packages/shared/content-management/content_list/kbn-content-list-table/src/action/index.ts
+++ src/platform/packages/shared/content-management/content_list/kbn-content-list-table/src/action/index.ts
@@ -7,16 +7,15 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-// Declarative components and props.
-export { Column, type ColumnProps } from './part';
-export { NameColumn, type NameColumnProps, NameCell, type NameCellProps } from './name';
-export { ActionsColumn, type ActionsColumnProps } from './actions';
-export {
-  UpdatedAtColumn,
-  type UpdatedAtColumnProps,
-  UpdatedAtCell,
-  type UpdatedAtCellProps,
-} from './updated_at';
+// Declarative components.
+export { Action, EditAction, DeleteAction, action } from './part';
 
-// Namespace type for TypeScript typing of `Column`.
-export type { ColumnNamespace } from './part';
+// Types.
+export type {
+  ActionProps,
+  ActionNamespace,
+  EditActionProps,
+  DeleteActionProps,
+  ActionOutput,
+  ActionBuilderContext,
+} from './types';
