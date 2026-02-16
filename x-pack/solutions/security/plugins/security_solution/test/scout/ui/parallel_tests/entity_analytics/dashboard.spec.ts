053488038d62d8c1a6dc9d0c0e372374584cde71
Gloria Hornero <gloria.hornero@elastic.co>
2026-02-16 22:14:12 +0100

    adjusting timeout

--- x-pack/solutions/security/plugins/security_solution/test/scout/ui/parallel_tests/entity_analytics/dashboard.spec.ts
+++ x-pack/solutions/security/plugins/security_solution/test/scout/ui/parallel_tests/entity_analytics/dashboard.spec.ts
@@ -56,7 +56,7 @@ spaceTest.describe(
         async () => {
           const entityStoreStatus = await apiServices.entityAnalytics.waitForEntityStoreStatus(
             'running',
-            180000
+            40000
           );
           expect(entityStoreStatus.status).toBe('running');
 
