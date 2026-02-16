1eda94d1f2bd55bee712807a78c8a12c52e7d49b
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-02-16 15:31:49 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/plugins/shared/maintenance_windows/common/transforms/index.ts
+++ x-pack/platform/plugins/shared/maintenance_windows/common/transforms/index.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 export { getDurationInMilliseconds } from './custom_to_rrule/util';
 export { transformCustomScheduleToRRule } from './custom_to_rrule/latest';
 export { transformRRuleToCustomSchedule } from './rrule_to_custom/latest';
