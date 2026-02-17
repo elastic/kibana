d033a66cbe799de2f754ba4915285d9786e548f7
CAWilson94 <charlotte.wilson@elastic.co>
2026-02-17 10:40:06 +0000

    fetch watchlist names from management API

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/api/api.ts
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/api/api.ts
@@ -37,6 +37,7 @@ import type {
   UploadAssetCriticalityRecordsResponse,
   ConfigureRiskEngineSavedObjectRequestBodyInput,
 } from '../../../common/api/entity_analytics';
+import type { ListWatchlistsResponse } from '../../../common/api/entity_analytics/watchlists/management/list.gen';
 import {
   API_VERSIONS,
   ASSET_CRITICALITY_INTERNAL_PRIVILEGES_URL,
@@ -65,6 +66,7 @@ import {
   RISK_SCORE_ENTITY_CALCULATION_URL,
   RISK_SCORE_PREVIEW_URL,
 } from '../../../common/constants';
+import { WATCHLISTS_MANAGEMENT_URL } from '../../../common/entity_analytics/watchlists/constants';
 import type { SnakeToCamelCase } from '../common/utils';
 import { useKibana } from '../../common/lib/kibana/kibana_react';
 
@@ -467,6 +469,25 @@ export const useEntityAnalyticsRoutes = () => {
         signal,
       });
 
+    /**
+     * List all watchlists
+     */
+    const fetchWatchlists = async ({ signal }: { signal?: AbortSignal } = {}) =>
+      http.fetch<ListWatchlistsResponse>(`${WATCHLISTS_MANAGEMENT_URL}/list`, {
+        version: API_VERSIONS.public.v1,
+        method: 'GET',
+        signal,
+      });
+
+    /**
+     * List unique watchlist names
+     * TODO: risk score weighting next
+     */
+    const fetchWatchlistNames = async ({ signal }: { signal?: AbortSignal } = {}) => {
+      const watchlists = await fetchWatchlists({ signal });
+      return Array.from(new Set(watchlists.map((watchlist) => watchlist.name)));
+    };
+
     return {
       fetchRiskScorePreview,
       fetchRiskEngineStatus,
@@ -497,6 +518,8 @@ export const useEntityAnalyticsRoutes = () => {
       updateSavedObjectConfiguration,
       listPrivMonMonitoredIndices,
       fetchEntityDetailsHighlights,
+      fetchWatchlists,
+      fetchWatchlistNames,
     };
   }, [http]);
 };
---
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/watchlists/components/watchlists_management_table/hooks/use_fetch_watchlist_names.ts
@@ -0,0 +1,21 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useQuery } from '@kbn/react-query';
+import { useEntityAnalyticsRoutes } from '../../../../../api/api';
+
+export const useFetchWatchlistNames = () => {
+  const { fetchWatchlistNames } = useEntityAnalyticsRoutes();
+  return useQuery(
+    ['GET', 'FETCH_WATCHLIST_NAMES'],
+    ({ signal }) => fetchWatchlistNames({ signal }),
+    {
+      keepPreviousData: true,
+      refetchOnWindowFocus: false,
+    }
+  );
+};
