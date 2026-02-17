0f17b02ec669b826b9dafac954442e9ad886cc93
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-02-17 19:15:43 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/platform/packages/shared/kbn-es-query/src/filters/build_filters/types.ts
+++ src/platform/packages/shared/kbn-es-query/src/filters/build_filters/types.ts
@@ -8,12 +8,12 @@
  */
 
 import type { FilterStateStore } from '@kbn/es-query-constants';
+import type { SerializableRecord } from '@kbn/utility-types';
 import type { ExistsFilter } from './exists_filter';
 import type { PhrasesFilter, PhrasesFilterMeta } from './phrases_filter';
 import type { PhraseFilter, PhraseFilterMeta, PhraseFilterMetaParams } from './phrase_filter';
 import type { RangeFilter, RangeFilterMeta, RangeFilterParams } from './range_filter';
 import type { MatchAllFilter, MatchAllFilterMeta } from './match_all_filter';
-import { SerializableRecord } from '@kbn/utility-types';
 
 /**
  * A common type for filters supported by this package
