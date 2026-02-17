df8e140d70ba2afa5638dd837e6e7b2329375c08
Julia Bardi <julia.bardi@elastic.co>
2026-02-17 14:05:05 +0100

    hide outdated policy for opamp

--- x-pack/platform/plugins/shared/fleet/public/components/agent_policy_summary_line.tsx
+++ x-pack/platform/plugins/shared/fleet/public/components/agent_policy_summary_line.tsx
@@ -47,7 +47,8 @@ export const AgentPolicySummaryLine = memo<{
     const { name, id, is_managed: isManaged, description } = policy;
 
     const revision = agent ? agent.policy_revision : policy.revision;
-    const isOutdated = agent?.policy_revision && policy.revision > agent.policy_revision;
+    const isOutdated =
+      agent?.type !== 'OPAMP' && agent?.policy_revision && policy.revision > agent.policy_revision;
 
     return (
       <EuiFlexGroup gutterSize="m" css={MIN_WIDTH} alignItems="center">
@@ -150,7 +151,7 @@ export const AgentPolicySummaryLine = memo<{
             >
               <EuiFlexGroup alignItems="center" gutterSize="xs">
                 <EuiFlexItem>
-                  <EuiIcon size="m" type="warning" color="warning" />
+                  <EuiIcon size="m" type="warning" color="warning" aria-hidden={true} />
                 </EuiFlexItem>
                 <EuiFlexItem>
                   <EuiText color="subdued" size="xs">
