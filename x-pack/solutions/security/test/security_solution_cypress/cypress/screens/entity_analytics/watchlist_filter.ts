13dda290c8a54360c61a2357e9d78e484d1eb091
Susan Chang <shuhsuan.chang@elastic.co>
2026-02-17 13:13:48 -0500

    Resolve merge conflicts

--- x-pack/solutions/security/test/security_solution_cypress/cypress/screens/entity_analytics/watchlist_filter.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/screens/entity_analytics/watchlist_filter.ts
@@ -5,6 +5,13 @@
  * 2.0.
  */
 
+<<<<<<< HEAD:x-pack/solutions/security/test/security_solution_cypress/cypress/screens/entity_analytics/watchlist_filter.ts
 import { getDataTestSubjectSelector } from '../../helpers/common';
 
 export const WATCHLIST_FILTER_COMBO_BOX = getDataTestSubjectSelector('watchlistFilterComboBox');
+=======
+// Internal evaluators - not exported from package
+export { createRuleFieldMatchEvaluator } from './rule_field_match';
+export { createQuerySimilarityEvaluator } from './query_similarity';
+export { createLlmJudgeEvaluator } from './llm_judge_evaluator';
+>>>>>>> f4102e1d49e7 (Tidy up file structure):x-pack/solutions/security/packages/kbn-evals-suite-ai-rules/src/evaluators/index.ts
