845191490bd9c310888fd784e24bb981011610fe
Milton Hultgren <milton.hultgren@elastic.co>
2026-02-17 20:36:38 +0100

    Shorten file path

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_management/data_management/stream_detail_lifecycle/downsampling/edit_ilm_phases_flyout/form/fields/index.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_management/data_management/stream_detail_lifecycle/downsampling/edit_ilm_phases_flyout/form/fields/index.tsx
@@ -6,7 +6,7 @@
  */
 
 export * from './downsample_interval_field';
-export * from './delete_searchable_snapshot_toggle_field';
+export * from './delete_snapshot_toggle_field';
 export * from './min_age_field';
 export * from './read_only_toggle_field';
 export * from './searchable_snapshot_repository_field';
