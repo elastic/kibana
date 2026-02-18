9082773c204267770b8d54bfc1467ef2359d471d
Nick Partridge <nicholas.partridge@elastic.co>
2026-02-17 21:30:01 -0700

    chore: remove old types and update tests

--- src/platform/plugins/private/vis_types/vega/public/data_model/search_api.ts
+++ src/platform/plugins/private/vis_types/vega/public/data_model/search_api.ts
@@ -9,7 +9,6 @@
 
 import { combineLatest, from } from 'rxjs';
 import { map, tap, switchMap } from 'rxjs';
-import type { estypes } from '@elastic/elasticsearch';
 import type { IUiSettingsClient, KibanaExecutionContext } from '@kbn/core/public';
 import type { IEsSearchResponse } from '@kbn/search-types';
 import type { SearchRequest, DataPublicPluginStart } from '@kbn/data-plugin/public';
@@ -20,22 +19,10 @@ import type { RequestResponder } from '@kbn/inspector-plugin/public';
 import type { ProjectRouting } from '@kbn/es-query';
 import type { VegaInspectorAdapters } from '../vega_inspector';
 
-interface DeprecatedBodyParams {
-  /**
-   * The `body` parameter is no longer supported in the ES client.
-   * The `body.runtime_mappings` will be overridden by the `runtime_mappings` parameter.
-   *
-   * see https://github.com/elastic/kibana/issues/253545
-   *
-   * @deprecated
-   */
-  body?: estypes.SearchRequest['body'] & Pick<estypes.SearchRequest, 'runtime_mappings'>;
-}
-
 /** @internal **/
 export const extendSearchParamsWithRuntimeFields = async (
   indexPatterns: SearchAPIDependencies['indexPatterns'],
-  requestParams: Omit<ReturnType<typeof getSearchParamsFromRequest>, 'body'> & DeprecatedBodyParams,
+  requestParams: ReturnType<typeof getSearchParamsFromRequest>,
   indexPatternString?: string
 ) => {
   if (indexPatternString) {
