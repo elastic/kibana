88522575a49809620985ee528f76a886ca8d8d42
Zacqary Xeper <zacqary.xeper@elastic.co>
2026-02-18 13:38:19 -0600

    Revert "Log warning if options list config fails to validate after denulling"
    This reverts commit 20660f626b476c86104268cebd6aa4746b486583.

--- src/platform/plugins/shared/controls/server/plugin.ts
+++ src/platform/plugins/shared/controls/server/plugin.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { CoreSetup, Plugin, PluginInitializerContext, Logger } from '@kbn/core/server';
+import type { CoreSetup, Plugin } from '@kbn/core/server';
 import type { PluginSetup as DataSetup } from '@kbn/data-plugin/server';
 import type { EmbeddableSetup } from '@kbn/embeddable-plugin/server';
 import type { PluginSetup as KqlSetup } from '@kbn/kql/server';
@@ -18,7 +18,6 @@ import { registerRangeSliderControlTransforms } from './transforms/range_slider_
 import { registerTimeSliderControlTransforms } from './transforms/time_slider_control_transforms';
 import { setupOptionsListClusterSettingsRoute } from './options_list/options_list_cluster_settings_route';
 import { setupOptionsListSuggestionsRoute } from './options_list/options_list_suggestions_route';
-import { setKibanaServices } from './kibana_services';
 
 interface SetupDeps {
   embeddable: EmbeddableSetup;
@@ -27,11 +26,6 @@ interface SetupDeps {
 }
 
 export class ControlsPlugin implements Plugin<object, object, SetupDeps> {
-  private readonly logger: Logger;
-
-  constructor(initializerContext: PluginInitializerContext) {
-    this.logger = initializerContext.logger.get();
-  }
   public setup(core: CoreSetup, { embeddable, kql }: SetupDeps) {
     registerESQLControlTransforms(embeddable);
     registerOptionsListControlTransforms(embeddable);
@@ -44,7 +38,6 @@ export class ControlsPlugin implements Plugin<object, object, SetupDeps> {
   }
 
   public start() {
-    setKibanaServices(this.logger);
     return {};
   }
 
