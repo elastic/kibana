7b1005c00dcb33b89d7d7800a1a10b0631b4f817
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-02-18 21:09:51 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/plugins/shared/cases/public/components/attachments/lens/types.ts
+++ x-pack/platform/plugins/shared/cases/public/components/attachments/lens/types.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { TimeRange } from '@kbn/es-query';
+import type { TimeRange } from '@kbn/es-query';
 import type { TypedLensByValueInput } from '@kbn/lens-plugin/public';
 
 export type LensProps = Pick<TypedLensByValueInput, 'attributes'> & {
