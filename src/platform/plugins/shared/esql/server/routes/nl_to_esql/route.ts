08062bb3bce12b0bb9a25d53a9300bdac9d6c48f
Stratoula <efstratia.kalafateli@elastic.co>
2026-02-19 09:34:41 +0100

    Adds the route

---
+++ src/platform/plugins/shared/esql/server/routes/nl_to_esql/route.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+import { lastValueFrom } from 'rxjs';
+import { naturalLanguageToEsql } from '@kbn/inference-plugin/server';
+import { schema } from '@kbn/config-schema';
+import type { CoreSetup, IRouter, PluginInitializerContext } from '@kbn/core/server';
+import { getConnectorList, getDefaultConnector } from './utils';
+
+import type { EsqlServerPluginStart } from '../../types';
+
+export const registerNLtoESQLRoute = (
+  router: IRouter,
+  getStartServices: CoreSetup<EsqlServerPluginStart>['getStartServices'],
+  context: PluginInitializerContext
+) => {
+  router.post(
+    {
+      path: '/internal/esql/nl_to_esql',
+      validate: {
+        body: schema.object({
+          query: schema.string(),
+        }),
+      },
+      security: {
+        authz: {
+          enabled: false,
+          reason: 'This route delegates authorization to the scoped ES client',
+        },
+      },
+    },
+    async (requestHandlerContext, request, response) => {
+      const logger = context.logger.get();
+      try {
+        const { query } = request.body;
+        const [, { inference, actions }] = await getStartServices();
+
+        const connectors = await getConnectorList({ actions, request });
+        const connector = getDefaultConnector({ connectors });
+
+        const result = await lastValueFrom(
+          naturalLanguageToEsql({
+            client: inference.getClient({ request }),
+            connectorId: connector.connectorId,
+            input: query,
+            functionCalling: 'auto',
+            logger,
+            system: "Just produce the query fenced by the esql tag. Don't explain it.",
+          })
+        );
+        return response.ok({
+          body: result,
+        });
+      } catch (error) {
+        logger.debug(error);
+        throw error;
+      }
+    }
+  );
+};
