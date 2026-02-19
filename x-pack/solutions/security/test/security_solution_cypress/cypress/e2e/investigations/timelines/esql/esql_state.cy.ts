9fe3fc8f22b12b22c045de4193b25c5d53f2f71d
Julia Rechkunova <julia.rechkunova@elastic.co>
2026-02-19 16:54:44 +0100

    [Discover] Update tests

--- x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/investigations/timelines/esql/esql_state.cy.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/investigations/timelines/esql/esql_state.cy.ts
@@ -27,6 +27,7 @@ import {
   openActiveTimeline,
   addNameAndDescriptionToTimeline,
   addNameToTimelineAndSave,
+  createTimelineFromBottomBar,
 } from '../../../../tasks/timeline';
 import { ALERTS_URL } from '../../../../urls/navigation';
 import { getTimeline } from '../../../../objects/timeline';
@@ -63,10 +64,7 @@ describe(
     beforeEach(() => {
       login();
       visitWithTimeRange(ALERTS_URL);
-      openActiveTimeline();
-      cy.window().then((win) => {
-        win.onbeforeunload = null;
-      });
+      createTimelineFromBottomBar();
       goToEsqlTab();
       addDiscoverEsqlQuery(esqlQuery);
       updateDateRangeInLocalDatePickers(DISCOVER_CONTAINER, INITIAL_START_DATE, INITIAL_END_DATE);
