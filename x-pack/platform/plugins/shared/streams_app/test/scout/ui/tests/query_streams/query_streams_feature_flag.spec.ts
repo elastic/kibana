107995a6a46d4fc705645559dc3540638a24bcbe
Quentin Couvelaire <quentin.couvelaire@elastic.co>
2026-02-19 08:11:19 -0500

    initial tests for query stream feature flag gating

--- x-pack/platform/plugins/shared/streams_app/test/scout/ui/tests/query_streams/query_streams_feature_flag.spec.ts
+++ x-pack/platform/plugins/shared/streams_app/test/scout/ui/tests/query_streams/query_streams_feature_flag.spec.ts
@@ -47,7 +47,7 @@ test.describe(
       });
       await page.reload();
 
-      await expect(pageObjects.streams.createQueryStreamButton).not.toBeVisible();
+      await expect(pageObjects.streams.createQueryStreamButton).toBeVisible();
     });
   }
 );
