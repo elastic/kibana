cc0c38a772129e50308d1cfd12fc1e2419bf24cf
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-02-19 11:32:10 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/platform/plugins/shared/vis_types/timeseries/public/application/components/lib/convert_series_to_vars.js
+++ src/platform/plugins/shared/vis_types/timeseries/public/application/components/lib/convert_series_to_vars.js
@@ -58,9 +58,12 @@ export const convertSeriesToVars = (series, model, getConfig = null, fieldFormat
           const fieldsForTerms = getFieldsForTerms(seriesModel.terms_field);
 
           if (fieldsForTerms.length === 1) {
-            rowLabel = createFieldFormatter(fieldsForTerms[0], fieldFormatMap, false, dataView)(
-              row.label
-            );
+            rowLabel = createFieldFormatter(
+              fieldsForTerms[0],
+              fieldFormatMap,
+              false,
+              dataView
+            )(row.label);
           }
         }
 
