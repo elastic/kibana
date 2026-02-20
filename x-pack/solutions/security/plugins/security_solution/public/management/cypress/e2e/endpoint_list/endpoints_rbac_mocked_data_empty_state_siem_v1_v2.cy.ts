4bc4c12bd03befbeee977bc93f5af2d4cad77152
Patryk Kopycinski <patryk.kopycinski@elastic.co>
2026-02-20 10:52:39 +0100

    chore: apply auto-formatting to split RBAC spec files

--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/endpoint_list/endpoints_rbac_mocked_data_empty_state_siem_v1_v2.cy.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/e2e/endpoint_list/endpoints_rbac_mocked_data_empty_state_siem_v1_v2.cy.ts
@@ -27,28 +27,27 @@ describe(
                 const shouldAllowOnboarding =
                   fleetPrivilege === 'all' && integrationsPrivilege === 'all';
 
-                it(`should show onboarding screen ${
-                  shouldAllowOnboarding ? 'with' : 'without'
-                } 'Add Elastic Defend' button with fleet:${fleetPrivilege} and integrations:${integrationsPrivilege}`, () => {
-                  login.withCustomKibanaPrivileges({
-                    [siemVersion]: [
-                      'all',
-                      `endpoint_list_all`,
-                      `policy_management_${endpointPolicyManagementPrivilege}`,
-                    ],
-                    fleet: [integrationsPrivilege],
-                    fleetv2: [fleetPrivilege],
+                it(`should show onboarding screen ${shouldAllowOnboarding ? 'with' : 'without'
+                  } 'Add Elastic Defend' button with fleet:${fleetPrivilege} and integrations:${integrationsPrivilege}`, () => {
+                    login.withCustomKibanaPrivileges({
+                      [siemVersion]: [
+                        'all',
+                        `endpoint_list_all`,
+                        `policy_management_${endpointPolicyManagementPrivilege}`,
+                      ],
+                      fleet: [integrationsPrivilege],
+                      fleetv2: [fleetPrivilege],
+                    });
+
+                    loadPage(APP_ENDPOINTS_PATH);
+
+                    cy.getByTestSubj('policyOnboardingInstructions').should('exist');
+                    if (shouldAllowOnboarding) {
+                      cy.getByTestSubj('onboardingStartButton').should('exist');
+                    } else {
+                      cy.getByTestSubj('onboardingStartButton').should('not.exist');
+                    }
                   });
-
-                  loadPage(APP_ENDPOINTS_PATH);
-
-                  cy.getByTestSubj('policyOnboardingInstructions').should('exist');
-                  if (shouldAllowOnboarding) {
-                    cy.getByTestSubj('onboardingStartButton').should('exist');
-                  } else {
-                    cy.getByTestSubj('onboardingStartButton').should('not.exist');
-                  }
-                });
               }
             }
           });
