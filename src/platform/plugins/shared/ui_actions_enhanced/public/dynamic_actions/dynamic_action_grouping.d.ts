99624358698e33693ce93f7242e71a53e45a0034
Nathan Reese <reese.nathan@gmail.com>
2026-02-20 15:29:10 -0700

    remove more .d.ts

--- src/platform/plugins/shared/ui_actions_enhanced/public/dynamic_actions/dynamic_action_grouping.d.ts
+++
@@ -1,3 +0,0 @@
-import type { EmbeddableApiContext } from '@kbn/presentation-publishing';
-import type { UiActionsPresentableGrouping as PresentableGrouping } from '@kbn/ui-actions-plugin/public';
-export declare const dynamicActionGrouping: PresentableGrouping<EmbeddableApiContext>;
--- src/platform/plugins/shared/ui_actions_enhanced/public/dynamic_actions/index.d.ts
+++
@@ -1,6 +0,0 @@
-export type * from './types';
-export * from './action_factory';
-export type * from './action_factory_definition';
-export * from './dynamic_action_storage';
-export * from './dynamic_action_manager_state';
-export * from './dynamic_action_manager';
