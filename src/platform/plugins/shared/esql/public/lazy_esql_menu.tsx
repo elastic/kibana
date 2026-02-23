030e4a1a50269417bdf1a11512ef2a1339d5f902
Stratoula <efstratia.kalafateli@elastic.co>
2026-02-23 16:06:06 +0100

    [ES|QL] Passes the missing dependencies

--- src/platform/plugins/shared/esql/public/lazy_esql_menu.tsx
+++ src/platform/plugins/shared/esql/public/lazy_esql_menu.tsx
@@ -8,7 +8,10 @@
  */
 import React, { Suspense } from 'react';
 import { EuiButtonIcon, EuiToolTip } from '@elastic/eui';
+import useAsync from 'react-use/lib/useAsync';
+import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
 import { helpLabel } from '@kbn/esql-editor';
+import { untilPluginStartServicesReady } from './kibana_services';
 
 const LazyESQLMenu = React.lazy(async () => {
   const module = await import('@kbn/esql-editor');
@@ -20,27 +23,37 @@ const LazyEsqlEditorActionsProvider = React.lazy(async () => {
   return { default: module.EsqlEditorActionsProvider };
 });
 
+const helpPopoverFallback = (
+  <EuiToolTip position="top" content={helpLabel} disableScreenReaderOutput>
+    <EuiButtonIcon
+      iconType="question"
+      size="xs"
+      aria-label={helpLabel}
+      data-test-subj="esql-help-popover-button"
+      color="text"
+      isDisabled
+    />
+  </EuiToolTip>
+);
+
 export const ESQLMenu: React.FC<{
   hideHistory?: boolean;
   onESQLDocsFlyoutVisibilityChanged?: (isOpen: boolean) => void;
-}> = (props) => (
-  <Suspense
-    fallback={
-      <EuiToolTip position="top" content={helpLabel} disableScreenReaderOutput>
-        <EuiButtonIcon
-          iconType="question"
-          size="xs"
-          aria-label={helpLabel}
-          data-test-subj="esql-help-popover-button"
-          color="text"
-          isDisabled
-        />
-      </EuiToolTip>
-    }
-  >
-    <LazyESQLMenu {...props} />
-  </Suspense>
-);
+}> = (props) => {
+  const { loading, value: deps } = useAsync(untilPluginStartServicesReady, []);
+
+  if (loading || !deps) {
+    return helpPopoverFallback;
+  }
+
+  return (
+    <KibanaContextProvider services={{ ...deps }}>
+      <Suspense fallback={helpPopoverFallback}>
+        <LazyESQLMenu {...props} />
+      </Suspense>
+    </KibanaContextProvider>
+  );
+};
 
 export const EsqlEditorActionsProvider: React.FC<{ children: React.ReactNode }> = ({
   children,
