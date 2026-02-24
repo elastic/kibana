21c04595dcd477adb64cb76754bd3c1f654e5ac9
Jan Monschke <jan.monschke@elastic.co>
2026-02-24 09:52:02 +0100

    use max comment const

--- x-pack/platform/plugins/shared/cases/common/workflows/steps/add_comment.ts
+++ x-pack/platform/plugins/shared/cases/common/workflows/steps/add_comment.ts
@@ -9,12 +9,13 @@ import { z } from '@kbn/zod/v4';
 import type { CommonStepDefinition } from '@kbn/workflows-extensions/common';
 import { CaseResponseProperties as CaseResponsePropertiesSchema } from '../../bundled-types.gen';
 import { CasesStepBaseConfigSchema } from './shared';
+import { MAX_COMMENT_LENGTH } from '../../constants';
 
 export const AddCommentStepTypeId = 'cases.addComment';
 
 export const InputSchema = z.object({
   case_id: z.string().min(1, 'case_id is required'),
-  comment: z.string().min(1, 'comment is required').max(30000),
+  comment: z.string().min(1, 'comment is required').max(MAX_COMMENT_LENGTH),
 });
 
 export const OutputSchema = z.object({
