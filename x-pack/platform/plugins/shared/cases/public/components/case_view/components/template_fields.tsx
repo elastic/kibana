3c85dfbb41a1bcc85269668aa90f19d79a47de3b
Mykhailo Kondrat <mykhailo.kondrat@elastic.co>
2026-02-24 10:26:42 +0100

    fetch real data for template fields on cases page

---
+++ x-pack/platform/plugins/shared/cases/public/components/case_view/components/template_fields.tsx
@@ -0,0 +1,97 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { camelCase } from 'lodash';
+import type { CaseUI } from '../../../../common';
+import { FieldType } from '../../templates_v2/field_types/constants';
+import { useGetTemplate } from '../../templates_v2/hooks/use_get_template';
+import { EditTextField } from './edit_text_field';
+import { EditTextareaField } from './edit_textarea_field';
+import { EditNumberField } from './edit_number_field';
+import { EditComboboxField } from './edit_combobox_field';
+import type { OnUpdateFields } from '../types';
+
+interface TemplateFieldsProps {
+  caseData: CaseUI;
+  onUpdateField: (args: OnUpdateFields) => void;
+  isLoading: boolean;
+  loadingKey: string | null;
+}
+
+export const TemplateFields = React.memo<TemplateFieldsProps>(
+  ({ caseData, onUpdateField, isLoading, loadingKey }) => {
+    const { data: templateData, isLoading: isLoadingTemplate } = useGetTemplate(
+      caseData.template?.id
+    );
+    const templateFields = templateData?.definition?.fields ?? [];
+
+    return (
+      <>
+        {templateFields.map((field) => {
+          const fieldIsLoading = isLoadingTemplate || (isLoading && loadingKey === field.name);
+          const onSubmit = (value: unknown) => onUpdateField({ key: field.name, value });
+          const extendedFieldValue =
+            caseData.extendedFields?.[camelCase(`${field.name}_as_${field.type}`)] ?? '';
+
+          switch (field.control) {
+            case FieldType.INPUT_TEXT:
+              return (
+                <EditTextField
+                  key={field.name}
+                  title={field.label ?? field.name}
+                  value={String(extendedFieldValue)}
+                  onSubmit={onSubmit}
+                  isLoading={fieldIsLoading}
+                  data-test-subj={`template-field-${field.name}`}
+                />
+              );
+            case FieldType.INPUT_NUMBER:
+              return (
+                <EditNumberField
+                  key={field.name}
+                  title={field.label ?? field.name}
+                  value={String(extendedFieldValue)}
+                  onSubmit={onSubmit}
+                  isLoading={fieldIsLoading}
+                  data-test-subj={`template-field-${field.name}`}
+                />
+              );
+            case FieldType.TEXTAREA:
+              return (
+                <EditTextareaField
+                  key={field.name}
+                  title={field.label ?? field.name}
+                  value={String(extendedFieldValue)}
+                  onSubmit={onSubmit}
+                  isLoading={fieldIsLoading}
+                  data-test-subj={`template-field-${field.name}`}
+                />
+              );
+            case FieldType.SELECT_BASIC:
+              return (
+                <EditComboboxField
+                  key={field.name}
+                  title={field.label ?? field.name}
+                  value={extendedFieldValue ? [String(extendedFieldValue)] : []}
+                  options={field.metadata?.options ?? []}
+                  onSubmit={onSubmit}
+                  isLoading={fieldIsLoading}
+                  singleSelection
+                  data-test-subj={`template-field-${field.name}`}
+                />
+              );
+            default:
+              return null;
+          }
+        })}
+      </>
+    );
+  }
+);
+
+TemplateFields.displayName = 'TemplateFields';
