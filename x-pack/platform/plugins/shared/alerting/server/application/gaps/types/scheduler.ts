a2b10176ca1e53f2cf28054c5b4f91822457ae9a
Nikita Khristinin <nkhristinin@gmail.com>
2026-02-25 10:35:46 +0100

    change how many gaps we fetch to reflect rule batch changes

--- x-pack/platform/plugins/shared/alerting/server/application/gaps/types/scheduler.ts
+++ x-pack/platform/plugins/shared/alerting/server/application/gaps/types/scheduler.ts
@@ -12,7 +12,7 @@ export type SchedulerSoAttributes = TypeOf<typeof rawGapAutoFillSchedulerSchemaV
 export const GAP_AUTO_FILL_SCHEDULER_TASK_TYPE = 'gap-auto-fill-scheduler-task' as const;
 
 export const DEFAULT_RULES_BATCH_SIZE = 10;
-export const DEFAULT_GAPS_PER_PAGE = 5000;
+export const DEFAULT_GAPS_PER_PAGE = DEFAULT_RULES_BATCH_SIZE * 50;
 
 export const DEFAULT_GAP_AUTO_FILL_SCHEDULER_TIMEOUT = '60s' as const;
 
