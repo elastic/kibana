d9f9b516311cf2fcef9bdacb015d89b6393e18c9
Nikita Khristinin <nkhristinin@gmail.com>
2026-02-25 13:58:54 +0100

    fix tests

--- x-pack/platform/plugins/shared/alerting/server/backfill_client/backfill_client.test.ts
+++ x-pack/platform/plugins/shared/alerting/server/backfill_client/backfill_client.test.ts
@@ -274,6 +274,7 @@ describe('BackfillClient', () => {
           title: 'Alerting Backfill Rule Run',
           priority: TaskPriority.Low,
           createTaskRunner: expect.any(Function),
+          maxConcurrency: 3,
         },
       });
     });
