9794fd11815981e5665cc38f2a18f4627a815c1e
Ryan Keairns <contactryank@gmail.com>
2026-02-25 12:48:10 -0800

    Save progress

--- x-pack/solutions/observability/plugins/apm/public/components/app/service_inventory/index.tsx
+++ x-pack/solutions/observability/plugins/apm/public/components/app/service_inventory/index.tsx
@@ -28,6 +28,7 @@ import type { SortFunction } from '../../shared/managed_table';
 import { MLCallout, shouldDisplayMlCallout } from '../../shared/ml_callout';
 import { SearchBar } from '../../shared/search_bar/search_bar';
 import { isTimeComparison } from '../../shared/time_comparison/get_comparison_options';
+import { getServiceInventoryHeaderAppActionsConfig } from '../../../header_app_actions/header_app_actions_config';
 import { ApmServicesTable } from './service_list/apm_services_table';
 import { getAvailableFields, orderServiceItems } from './service_list/order_service_items';
 import { TracesInDiscoverCallout } from './traces_in_discover_callout';
@@ -175,6 +176,14 @@ function useServicesDetailedStatisticsFetcher({
 export function ServiceInventory() {
   const [debouncedSearchQuery, setDebouncedSearchQuery] = useStateDebounced('');
   const { onPageReady } = usePerformanceContext();
+  const { core } = useApmPluginContext();
+
+  useEffect(() => {
+    core.chrome.setHeaderAppActionsConfig(getServiceInventoryHeaderAppActionsConfig());
+    return () => {
+      core.chrome.setHeaderAppActionsConfig(undefined);
+    };
+  }, [core.chrome]);
   const mainStatisticsFetch = useServicesMainStatisticsFetcher(debouncedSearchQuery);
   const [renderedItems, setRenderedItems] = useState<ServiceListItem[]>([]);
   const { mainStatisticsData, mainStatisticsStatus } = mainStatisticsFetch;
