02aabe57d33ff4d2c9159b16a46bd45cfaf199fa
Steph Milovic <stephanie.milovic@elastic.co>
2026-02-25 10:41:06 -0700

    dhurley14 feedback

--- x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_management/logic/detection_rules_client/methods/patch_rule.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/detection_engine/rule_management/logic/detection_rules_client/methods/patch_rule.ts
@@ -64,15 +64,18 @@ export const patchRule = async ({
 
   validateNonCustomizablePatchFields(rulePatch, existingRule);
 
-  const rulePatchDefinedFields = Object.fromEntries(
-    Object.entries(rulePatchObjWithoutIds).filter(([key, value]) => {
-      if (value === undefined) {
-        return false;
-      }
-      const existingValue = (existingRule as Record<string, unknown>)[key];
-      return !isEqual(value, existingValue);
-    })
-  );
+  // Zod v4 materializes some defaulted optional fields on PATCH payloads (e.g. max_signals),
+  // even when the client did not send them. Filter to fields that are both defined and
+  // actually changed so RBAC decisions are based on effective user changes.
+  const rulePatchDefinedFields: Record<string, unknown> = {};
+  let allKeysUpdateable = true;
+
+  for (const [key, value] of Object.entries(rulePatchObjWithoutIds)) {
+    if (value !== undefined && !isEqual(value, (existingRule as Record<string, unknown>)[key])) {
+      rulePatchDefinedFields[key] = value;
+      allKeysUpdateable = allKeysUpdateable && isKeyUpdateableWithReadPermission(key);
+    }
+  }
 
   /**
    * RBAC logic branch
@@ -85,15 +88,14 @@ export const patchRule = async ({
    * provided by the alerting rules client to update the rule fields individually. Otherwise the user will
    * need `all` privileges for rules.
    */
-  if (
-    !isEmpty(rulePatchDefinedFields) &&
-    Object.keys(rulePatchDefinedFields).every((key) => isKeyUpdateableWithReadPermission(key))
-  ) {
+  if (!isEmpty(rulePatchDefinedFields) && allKeysUpdateable) {
     const appliedPatchWithReadPrivs: BulkEditResult<RuleParams> = await patchReadAuthEditRuleFields(
       {
         rulesClient,
         // Don't want to pass ID fields to the read authz PATCH method as it will apply patches on all fields in the object
-        rulePatch: rulePatchDefinedFields,
+        rulePatch: rulePatchDefinedFields as Parameters<
+          typeof patchReadAuthEditRuleFields
+        >[0]['rulePatch'],
         existingRule,
         prebuiltRuleAssetClient,
       }
