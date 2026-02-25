7bfdac92876aa80fcc65291dea072f40949d4cc6
Yan Savitski <yan.savitski@elastic.co>
2026-02-25 16:29:16 +0100

    Use em values instead of px

--- x-pack/solutions/search/plugins/search_playground/public/components/chat.tsx
+++ x-pack/solutions/search/plugins/search_playground/public/components/chat.tsx
@@ -138,8 +138,8 @@ export const Chat = () => {
               paddingBottom: euiTheme.size.l,
               // don't allow the chat to shrink below 700px
               flexBasis: 0,
-              minWidth: 700,
-              minHeight: 300,
+              minWidth: '50em',
+              minHeight: '22em',
             }}
           >
             <EuiFlexGroup direction="column" className="eui-fullHeight">
