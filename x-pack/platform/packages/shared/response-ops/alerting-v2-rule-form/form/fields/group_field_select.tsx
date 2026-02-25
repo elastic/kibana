c0b34f31420da2e87d4c5c7fe54525e0feb73c01
Dominique Belcher <dominique.clarke@elastic.co>
2026-02-25 14:15:29 -0500

    adjust group field select

--- x-pack/platform/packages/shared/response-ops/alerting-v2-rule-form/form/fields/group_field_select.tsx
+++ x-pack/platform/packages/shared/response-ops/alerting-v2-rule-form/form/fields/group_field_select.tsx
@@ -6,7 +6,7 @@
  */
 
 import React, { useCallback, useMemo } from 'react';
-import { Controller, useFormContext } from 'react-hook-form';
+import { Controller, useFormContext, useWatch } from 'react-hook-form';
 import { EuiComboBox, EuiFormRow } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import type { DataPublicPluginStart } from '@kbn/data-plugin/public';
@@ -20,8 +20,8 @@ interface GroupBySelectProps {
 }
 
 export const GroupFieldSelect: React.FC<GroupBySelectProps> = ({ services }) => {
-  const { control, setValue, getValues, watch } = useFormContext<FormValues>();
-  const query = watch('evaluation.query.base');
+  const { control, setValue, getValues } = useFormContext<FormValues>();
+  const query = useWatch({ name: 'evaluation.query.base', control });
   const groupByRowId = 'ruleV2FormGroupByField';
 
   // When columns change, filter out any invalid selections
