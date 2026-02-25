9e84748690f22cb26bffa46db462522882d61ca7
Nathan Reese <reese.nathan@gmail.com>
2026-02-25 15:12:59 -0700

    fix jest tests

--- x-pack/platform/plugins/shared/lens/public/app_plugin/save_modal_container.test.tsx
+++ x-pack/platform/plugins/shared/lens/public/app_plugin/save_modal_container.test.tsx
@@ -121,7 +121,7 @@ describe('runSaveLensVisualization', () => {
             expect.objectContaining({
               state: expect.objectContaining({
                 title: 'blah',
-                timeRange: { from: 'now-7d', to: 'now' },
+                time_range: { from: 'now-7d', to: 'now' },
               }),
             })
           );
