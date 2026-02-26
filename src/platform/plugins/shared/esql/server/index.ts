f4d7c9d7a762e26d312ddb76c631b157b1649b4e
Alexandra Shatova <shshatova@gmail.com>
2026-02-26 13:13:10 +0000

    feat: fix typescript errors

--- src/platform/plugins/shared/esql/server/index.ts
+++ src/platform/plugins/shared/esql/server/index.ts
@@ -8,11 +8,12 @@
  */
 
 import type { PluginInitializerContext } from '@kbn/core/server';
+import type { EsqlServerPluginSetup } from './plugin';
 
 export const plugin = async (initContext: PluginInitializerContext) => {
   const { EsqlServerPlugin } = await import('./plugin');
   return new EsqlServerPlugin(initContext);
 };
 
-export type { EsqlServerPluginSetup, EsqlServerPluginSetup as PluginSetup } from './plugin';
+export type { EsqlServerPluginSetup as PluginSetup };
 export { buildServerESQLCallbacks } from './services/build_server_esql_callbacks';
