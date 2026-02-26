6e0a1cd70eb94f0b7d5848b082ad343ca742adf4
Jon <jon@elastic.co>
2026-02-26 15:10:52 -0600

    [ci] Do not cancel on-merge builds (#255162)
    We need on-merge to run for all commits to pick up bundle size baselines
    to compare pull requests to.
    
    (cherry picked from commit c5493416c0c4205c655ecef7e2d13f38d875bf48)
    
    # Conflicts:
    #	.buildkite/pipeline-utils/ci-stats/pick_test_group_run_order.ts
    #	.buildkite/pipelines/on_merge_fanout.yml

--- .buildkite/pipeline-utils/ci-stats/pick_test_group_run_order.ts
+++ .buildkite/pipeline-utils/ci-stats/pick_test_group_run_order.ts
@@ -444,6 +444,8 @@ export async function pickTestGroupRunOrder() {
   console.log('test run order is determined by builds:');
   console.dir(sources, { depth: Infinity, maxArrayLength: Infinity });
 
+  const cancelOnBuildFailing = pipelineSlug !== 'kibana-on-merge';
+
   const unit = getRunGroup(bk, types, UNIT_TYPE);
   const integration = getRunGroup(bk, types, INTEGRATION_TYPE);
 
@@ -513,7 +515,7 @@ export async function pickTestGroupRunOrder() {
         ? {
             label: 'Jest Tests',
             command: getRequiredEnv('JEST_UNIT_SCRIPT'),
-            cancel_on_build_failing: true,
+            cancel_on_build_failing: cancelOnBuildFailing,
             parallelism: unit.count,
             timeout_in_minutes: 120,
             key: 'jest',
@@ -533,7 +535,7 @@ export async function pickTestGroupRunOrder() {
         ? {
             label: 'Jest Integration Tests',
             command: getRequiredEnv('JEST_INTEGRATION_SCRIPT'),
-            cancel_on_build_failing: true,
+            cancel_on_build_failing: cancelOnBuildFailing,
             parallelism: integration.count,
             // TODO: Reduce once we have identified the cause of random long-running tests
             timeout_in_minutes: 75,
@@ -572,7 +574,7 @@ export async function pickTestGroupRunOrder() {
                 ({ title, key, queue = defaultQueue }): BuildkiteStep => ({
                   label: title,
                   command: getRequiredEnv('FTR_CONFIGS_SCRIPT'),
-                  cancel_on_build_failing: true,
+                  cancel_on_build_failing: cancelOnBuildFailing,
                   timeout_in_minutes: 120,
                   agents: expandAgentQueue(queue, 105),
                   env: {
