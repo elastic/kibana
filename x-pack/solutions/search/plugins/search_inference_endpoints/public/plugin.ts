7cfaa0b07627aa63f5c7672ac467ed3bd2e30921
Saikat Sarkar <saikat.sarkar@elastic.co>
2026-02-26 15:12:54 -0700

    Refactor licensing issue

--- x-pack/solutions/search/plugins/search_inference_endpoints/public/plugin.ts
+++ x-pack/solutions/search/plugins/search_inference_endpoints/public/plugin.ts
@@ -5,10 +5,7 @@
  * 2.0.
  */
 
-import type { Subscription } from 'rxjs';
-
 import type { CoreSetup, CoreStart, Plugin, PluginInitializerContext } from '@kbn/core/public';
-import type { ManagementSetup } from '@kbn/management-plugin/public';
 import { INFERENCE_ENDPOINTS_APP_ID, PLUGIN_TITLE } from '../common/constants';
 import { docLinks } from '../common/doc_links';
 import type {
@@ -24,12 +21,6 @@ export class SearchInferenceEndpointsPlugin
   implements Plugin<SearchInferenceEndpointsPluginSetup, SearchInferenceEndpointsPluginStart>
 {
   private config: SearchInferenceEndpointsConfigType;
-  private licenseSubscription: Subscription | undefined;
-  private managementSetup: ManagementSetup | undefined;
-  private coreSetup:
-    | CoreSetup<AppPluginStartDependencies, SearchInferenceEndpointsPluginStart>
-    | undefined;
-  private managementAppRegistered = false;
 
   constructor(initializerContext: PluginInitializerContext) {
     this.config = initializerContext.config.get<SearchInferenceEndpointsConfigType>();
@@ -41,10 +32,24 @@ export class SearchInferenceEndpointsPlugin
   ): SearchInferenceEndpointsPluginSetup {
     if (!this.config.ui?.enabled) return {};
 
-    this.coreSetup = core;
-    this.managementSetup = plugins.management;
-
     registerLocators(plugins.share);
+
+    plugins.management.sections.section.machineLearning.registerApp({
+      id: INFERENCE_ENDPOINTS_APP_ID,
+      title: PLUGIN_TITLE,
+      order: 2,
+      async mount(params) {
+        const { renderInferenceEndpointsMgmtApp } = await import('./application');
+        const [coreStart, depsStart] = await core.getStartServices();
+        const startDeps: AppPluginStartDependencies = {
+          ...depsStart,
+          history: params.history,
+          searchNavigation: undefined,
+        };
+
+        return renderInferenceEndpointsMgmtApp(coreStart, startDeps, params.element);
+      },
+    });
 
     return {};
   }
@@ -53,51 +58,9 @@ export class SearchInferenceEndpointsPlugin
     core: CoreStart,
     deps: AppPluginStartDependencies
   ): SearchInferenceEndpointsPluginStart {
-    const { licensing } = deps;
     docLinks.setDocLinks(core.docLinks.links);
-
-    this.licenseSubscription = licensing.license$.subscribe((license) => {
-      const hasEnterpriseLicense =
-        license && license.isAvailable && license.isActive && license.hasAtLeast('enterprise');
-
-      const canAccessInferenceEndpoints =
-        core.application.capabilities.management?.ml?.inference_endpoints === true;
-
-      if (
-        hasEnterpriseLicense &&
-        canAccessInferenceEndpoints &&
-        !this.managementAppRegistered &&
-        this.managementSetup &&
-        this.coreSetup
-      ) {
-        const coreSetupRef = this.coreSetup;
-        this.managementSetup.sections.section.machineLearning.registerApp({
-          id: INFERENCE_ENDPOINTS_APP_ID,
-          title: PLUGIN_TITLE,
-          order: 2,
-          async mount(params) {
-            const { renderInferenceEndpointsMgmtApp } = await import('./application');
-            const [coreStart, depsStart] = await coreSetupRef.getStartServices();
-            const startDeps: AppPluginStartDependencies = {
-              ...depsStart,
-              history: params.history,
-              searchNavigation: undefined,
-            };
-
-            return renderInferenceEndpointsMgmtApp(coreStart, startDeps, params.element);
-          },
-        });
-        this.managementAppRegistered = true;
-      }
-    });
-
     return {};
   }
 
-  public stop() {
-    if (this.licenseSubscription) {
-      this.licenseSubscription.unsubscribe();
-      this.licenseSubscription = undefined;
-    }
-  }
+  public stop() {}
 }
