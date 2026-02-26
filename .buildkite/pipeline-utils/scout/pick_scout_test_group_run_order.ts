6af734652fffe67bf2f98d2f7e641bbcf4b90df9
Jonathan Budzenski <jon@elastic.co>
2026-02-26 12:32:27 -0600

    [ci] Do not cancel on-merge builds

--- .buildkite/pipeline-utils/scout/pick_scout_test_group_run_order.ts
+++ .buildkite/pipeline-utils/scout/pick_scout_test_group_run_order.ts
@@ -43,6 +43,8 @@ if (process.env.UIAM_COSMOSDB_DOCKER_IMAGE) {
 export async function pickScoutTestGroupRunOrder(scoutConfigsPath: string) {
   const bk = new BuildkiteClient();
   const envFromlabels: Record<string, string> = collectEnvFromLabels();
+  const pipelineSlug = process.env.BUILDKITE_PIPELINE_SLUG as string;
+  const cancelOnBuildFailing = pipelineSlug !== 'kibana-on-merge';
 
   if (!Fs.existsSync(scoutConfigsPath)) {
     throw new Error(`Scout configs file not found at ${scoutConfigsPath}`);
@@ -87,7 +89,7 @@ export async function pickScoutTestGroupRunOrder(scoutConfigsPath: string) {
           ({ label, key, group, agents }): BuildkiteStep => ({
             label,
             command: getRequiredEnv('SCOUT_CONFIGS_SCRIPT'),
-            cancel_on_build_failing: true,
+            cancel_on_build_failing: cancelOnBuildFailing,
             timeout_in_minutes: 60,
             agents,
             env: {
