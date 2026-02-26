b4e717ee46b84b684518461356684072478a55f8
Tiago Costa <tiago.costa@elastic.co>
2026-02-26 04:56:32 +0000

    fix(NA): management group4 and esql 3 ftr tests

--- src/platform/test/functional/apps/management/group4/_kibana_settings.ts
+++ src/platform/test/functional/apps/management/group4/_kibana_settings.ts
@@ -19,6 +19,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
     before(async function () {
       // delete .kibana index and then wait for Kibana to re-create it
       await kibanaServer.uiSettings.replace({});
+      await PageObjects.settings.navigateTo();
       await PageObjects.settings.createIndexPattern('logstash-*');
       await PageObjects.settings.navigateTo();
     });
