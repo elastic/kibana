7efbba9078d14dc3dee5ac158b20b66a16e98ea7
Nicholas Peretti <nicholas.peretti@elastic.co>
2026-02-27 16:07:44 +0000

    Apply PR feedback

--- x-pack/solutions/security/plugins/security_solution/public/timelines/components/timeline/unified_components/data_table/is_attack_discovery_row.ts
+++ x-pack/solutions/security/plugins/security_solution/public/timelines/components/timeline/unified_components/data_table/is_attack_discovery_row.ts
@@ -8,13 +8,14 @@
 import type { DataTableRecord } from '@kbn/discover-utils/types';
 import {
   ATTACK_DISCOVERY_AD_HOC_RULE_TYPE_ID,
+  ATTACK_DISCOVERY_ALERTS_COMMON_INDEX_PREFIX,
   ATTACK_DISCOVERY_SCHEDULES_ALERT_TYPE_ID,
 } from '@kbn/elastic-assistant-common';
 import { ALERT_RULE_TYPE_ID } from '@kbn/rule-data-utils';
 import type { TimelineItem } from '../../../../../../common/search_strategy';
 
 const ATTACK_DISCOVERY_ALERT_IDS_FIELD = 'kibana.alert.attack_discovery.alert_ids';
-const ATTACK_DISCOVERY_ALERT_INDEX_PREFIX = '.internal.alerts-security.attack.discovery.alerts-';
+const ATTACK_DISCOVERY_ALERT_INDEX_PREFIX = `.internal${ATTACK_DISCOVERY_ALERTS_COMMON_INDEX_PREFIX}`;
 const ATTACK_DISCOVERY_RULE_TYPE_IDS = [
   ATTACK_DISCOVERY_SCHEDULES_ALERT_TYPE_ID,
   ATTACK_DISCOVERY_AD_HOC_RULE_TYPE_ID,
