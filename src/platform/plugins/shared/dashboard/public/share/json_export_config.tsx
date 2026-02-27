11f787681b12e7868af4c3759cc7ed2fa0d510bc
Nick Peihl <nick.peihl@elastic.co>
2026-02-27 16:14:40 -0500

    Lazy load

---
+++ src/platform/plugins/shared/dashboard/public/share/json_export_config.tsx
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import React from 'react';
+import { i18n } from '@kbn/i18n';
+import { downloadFileAs, type ExportShare } from '@kbn/share-plugin/public';
+import type { ShareContext } from '@kbn/share-plugin/public/types';
+import type { DashboardState } from '../../server';
+import { getSanitizedExportSource } from './dashboard_export_source_client';
+import { ExportSourceAssetPanel } from './export_source_asset_panel';
+import { buildExportSourceFilename } from './export_source_share_utils';
+
+export interface ExportSourceSharingData {
+  title: string;
+  exportSource: DashboardState;
+}
+
+export const getShareMenuItems =
+  () =>
+  ({
+    sharingData,
+  }: ShareContext): ReturnType<ExportShare['config']> extends Promise<infer R> ? R : never => {
+    const typedSharingData = sharingData as unknown as ExportSourceSharingData;
+
+    return {
+      icon: 'code',
+      label: i18n.translate('dashboard.exportSource.label', {
+        defaultMessage: 'JSON',
+      }),
+      exportType: 'dashboard_export_source',
+      generateExportButtonLabel: i18n.translate('dashboard.exportSource.downloadButtonLabel', {
+        defaultMessage: 'Download JSON',
+      }),
+      generateAssetComponent: (
+        <ExportSourceAssetPanel dashboardState={typedSharingData.exportSource} />
+      ),
+      generateAssetExport: async (_opts) => {
+        const data = await getSanitizedExportSource(typedSharingData.exportSource)
+          .then((result) => result.data)
+          .catch(() => typedSharingData.exportSource);
+        const filename = buildExportSourceFilename(typedSharingData.title, '.json');
+        const content = JSON.stringify(data, null, 2);
+        await downloadFileAs(filename, { content, type: 'application/json' });
+      },
+    };
+  };
