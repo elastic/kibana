d3189cf02228a8b07c867d0c8582808793219a33
Jorge Oliveira <jorge.oliveira@elastic.co>
2026-02-27 16:48:25 +0000

    Refactor METRICS_INFO fetching logic by removing it from fetchAll and implementing useMetricsInfo hook for conditional fetching in Metrics Experience. Add fetchMetricsInfo utility for executing ESQL METRICS_INFO queries

---
+++ src/platform/plugins/shared/discover/public/context_awareness/profile_providers/common/metrics_data_source_profile/accessor/use_metrics_info.ts
@@ -0,0 +1,70 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { useEffect } from 'react';
+import { isOfAggregateQueryType } from '@kbn/es-query';
+import { buildMetricsInfoQuery, hasTransformationalCommand } from '@kbn/esql-utils';
+import type {
+  UnifiedHistogramFetchParams,
+  UnifiedHistogramServices,
+} from '@kbn/unified-histogram/types';
+import { fetchMetricsInfo } from '../utils/fetch_metrics_info';
+
+/**
+ * Triggers a METRICS_INFO fetch when in Metrics Experience (non-transformational ES|QL, chart visible).
+ */
+export function useMetricsInfo(
+  fetchParams: UnifiedHistogramFetchParams,
+  services: UnifiedHistogramServices,
+  isComponentVisible: boolean
+): void {
+  const esql =
+    fetchParams.query && isOfAggregateQueryType(fetchParams.query)
+      ? (fetchParams.query as { esql: string }).esql
+      : undefined;
+  const shouldFetch =
+    isComponentVisible && !!esql && !!fetchParams.isESQLQuery && !hasTransformationalCommand(esql);
+
+  useEffect(() => {
+    if (!shouldFetch || !fetchParams.dataView) {
+      return;
+    }
+
+    const metricsInfoQuery = buildMetricsInfoQuery(esql);
+    if (!metricsInfoQuery) {
+      return;
+    }
+
+    const signal = fetchParams.abortController?.signal;
+
+    fetchMetricsInfo({
+      esqlQuery: metricsInfoQuery,
+      search: services.data.search.search,
+      signal,
+      dataView: fetchParams.dataView,
+      timeRange: fetchParams.timeRange,
+      filters: fetchParams.filters ?? [],
+      variables: fetchParams.esqlVariables,
+      uiSettings: services.uiSettings,
+    }).catch(() => {
+      // Fire-and-forget; ignore errors (e.g. abort or network)
+    });
+  }, [
+    shouldFetch,
+    esql,
+    fetchParams.dataView?.id,
+    fetchParams.timeRange?.from,
+    fetchParams.timeRange?.to,
+    fetchParams.abortController,
+    fetchParams.filters,
+    fetchParams.esqlVariables,
+    services.data.search.search,
+    services.uiSettings,
+  ]);
+}
