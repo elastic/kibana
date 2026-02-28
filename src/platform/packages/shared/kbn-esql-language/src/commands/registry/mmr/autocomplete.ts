adf9c6276fca519ef9d9c10850353a64a4f73411
Panaghiotis Maurice Van de goor <panaghiotis.vandegoor@elastic.co>
2026-02-28 15:13:18 +0200

    review: extracts extra functions to utils.ts

--- src/platform/packages/shared/kbn-esql-language/src/commands/registry/mmr/autocomplete.ts
+++ src/platform/packages/shared/kbn-esql-language/src/commands/registry/mmr/autocomplete.ts
@@ -8,21 +8,12 @@
  */
 
 import { i18n } from '@kbn/i18n';
-import { isMap, isOptionNode } from '../../../ast/is';
-import type {
-  ESQLAstAllCommands,
-  ESQLAstItem,
-  ESQLAstMmrCommand,
-  ESQLCommandOption,
-} from '../../../types';
+import type { ESQLAstAllCommands, ESQLAstMmrCommand } from '../../../types';
 import type { MapParameters } from '../../definitions/utils/autocomplete/map_expression';
 import { getCommandMapExpressionSuggestions } from '../../definitions/utils/autocomplete/map_expression';
 import {
-  columnExists,
   getFunctionsSuggestions,
-  getFieldsSuggestions,
   getLiteralsSuggestions,
-  handleFragment,
 } from '../../definitions/utils/autocomplete/helpers';
 import {
   mmrLambdaMapSuggestion,
@@ -36,133 +27,7 @@ import {
 } from '../complete_items';
 import type { ICommandCallbacks, ICommandContext, ISuggestionItem } from '../types';
 import { Location } from '../types';
-
-enum MmrPosition {
-  AFTER_MMR_KEYWORD = 'after_mmr_keyword',
-  AFTER_QUERY_VECTOR = 'after_query_vector',
-  AFTER_ON_KEYWORD = 'after_on_keyword',
-  AFTER_FIELD = 'after_field',
-  AFTER_LIMIT_KEYWORD = 'after_limit_keyword',
-  AFTER_LIMIT_VALUE = 'after_limit_value',
-  AFTER_WITH_KEYWORD = 'after_with_keyword',
-  WITHIN_OPTIONS = 'within_options',
-  AFTER_COMMAND = 'after_command',
-}
-
-const MMR_VECTOR_TYPES = ['dense_vector'];
-
-async function getVectorFieldSuggestions(
-  innerText: string,
-  callbacks?: ICommandCallbacks,
-  context?: ICommandContext
-): Promise<ISuggestionItem[]> {
-  const controlAndLiteralSuggestions = getLiteralsSuggestions(MMR_VECTOR_TYPES, Location.MMR, {
-    includeDateLiterals: false,
-    includeCompatibleLiterals: true,
-    addComma: false,
-    advanceCursorAndOpenSuggestions: false,
-    supportsControls: true,
-    variables: context?.variables,
-  });
-
-  const functionSuggestions = getFunctionsSuggestions({
-    location: Location.MMR,
-    types: MMR_VECTOR_TYPES,
-    options: {},
-    context,
-    callbacks,
-  });
-
-  if (!callbacks?.getByType) {
-    return [];
-  }
-
-  const fieldSuggestions = await getFieldsSuggestions(MMR_VECTOR_TYPES, callbacks.getByType, {
-    addSpaceAfterField: true,
-    openSuggestions: true,
-  });
-
-  const filteredFieldSuggestions = context?.columns
-    ? fieldSuggestions.filter((suggestion) => columnExists(suggestion.label, context))
-    : fieldSuggestions;
-
-  return handleFragment(
-    innerText,
-    (fragment) => columnExists(fragment, context),
-    (_fragment, rangeToReplace) => [
-      ...filteredFieldSuggestions.map((suggestion) => ({
-        ...suggestion,
-        rangeToReplace,
-      })),
-      ...controlAndLiteralSuggestions,
-      ...functionSuggestions,
-    ],
-    () => [...controlAndLiteralSuggestions, ...functionSuggestions]
-  );
-}
-
-function isAstItemIncomplete(item: ESQLAstItem | undefined): boolean {
-  if (!item) {
-    return true;
-  }
-
-  if (Array.isArray(item)) {
-    return item.length === 0 || isAstItemIncomplete(item[0]);
-  }
-
-  return item.incomplete;
-}
-
-function getPosition(command: ESQLAstMmrCommand): MmrPosition {
-  const onOption = command.args.find(
-    (arg) => isOptionNode(arg) && arg.name.toLowerCase() === 'on'
-  ) as ESQLCommandOption;
-  const limitOption = command.args.find(
-    (arg) => isOptionNode(arg) && arg.name.toLowerCase() === 'limit'
-  ) as ESQLCommandOption;
-  const withOption = command.args.find(
-    (arg) => isOptionNode(arg) && arg.name.toLowerCase() === 'with'
-  ) as ESQLCommandOption;
-
-  if (withOption) {
-    const map = isMap(withOption.args[0]) ? withOption.args[0] : undefined;
-
-    if (!map || (map.incomplete && !map.text)) {
-      return MmrPosition.AFTER_WITH_KEYWORD;
-    }
-
-    if (map.incomplete) {
-      return MmrPosition.WITHIN_OPTIONS;
-    }
-
-    return MmrPosition.AFTER_COMMAND;
-  }
-
-  if (limitOption) {
-    const limitValue = limitOption.args[0];
-    if (isAstItemIncomplete(limitValue)) {
-      return MmrPosition.AFTER_LIMIT_KEYWORD;
-    }
-    return MmrPosition.AFTER_LIMIT_VALUE;
-  }
-
-  if (onOption) {
-    const onField = onOption.args[0];
-    if (isAstItemIncomplete(onField)) {
-      return MmrPosition.AFTER_ON_KEYWORD;
-    }
-    return MmrPosition.AFTER_FIELD;
-  }
-
-  if (command.queryVector) {
-    if (command.queryVector.incomplete) {
-      return MmrPosition.AFTER_MMR_KEYWORD;
-    }
-    return MmrPosition.AFTER_QUERY_VECTOR;
-  }
-
-  return MmrPosition.AFTER_MMR_KEYWORD;
-}
+import { getPosition, getVectorFieldSuggestions, MmrPosition, MMR_VECTOR_TYPES } from './utils';
 
 export async function autocomplete(
   query: string,
