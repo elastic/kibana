e0746b90f11f48aaae54f674ae2e9d0a4b45b7b3
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2026-03-02 12:07:55 +0100

    simplify setup hook

--- x-pack/solutions/observability/plugins/synthetics/test/scout/ui/tests/global.setup.ts
+++ x-pack/solutions/observability/plugins/synthetics/test/scout/ui/tests/global.setup.ts
@@ -11,22 +11,14 @@ import { testData } from '../fixtures';
 globalSetupHook(
   'Ingest Synthetics test data',
   { tag: tags.stateful.classic },
-  async ({ esArchiver, esClient, log }) => {
+  async ({ esArchiver, log }) => {
     log.debug('[setup] loading test data (only if indexes do not exist)...');
-    await esArchiver.loadIfNeeded(testData.ES_ARCHIVES.FULL_HEARTBEAT);
-    await esArchiver.loadIfNeeded(testData.ES_ARCHIVES.BROWSER);
-
-    // synthetics_data targets Fleet-managed data streams (no mappings.json),
-    // so loadIfNeeded can't detect existing data streams. Check manually.
-    const { count } = await esClient.count({
-      index: 'synthetics-browser-default',
-      ignore_unavailable: true,
-    });
-    if (count === 0) {
-      log.debug('[setup] loading synthetics_data archive...');
-      await esArchiver.loadIfNeeded(testData.ES_ARCHIVES.SYNTHETICS_DATA);
-    } else {
-      log.debug('[setup] synthetics_data already loaded, skipping...');
+    for (const archive of [
+      testData.ES_ARCHIVES.FULL_HEARTBEAT,
+      testData.ES_ARCHIVES.BROWSER,
+      testData.ES_ARCHIVES.SYNTHETICS_DATA,
+    ]) {
+      await esArchiver.loadIfNeeded(archive);
     }
   }
 );
