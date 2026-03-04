df1b08b4a554c9d4596766f1e46f3b27b9453fde
Sonia Sanz Vivas <sonia.sanzvivas@elastic.co>
2026-03-04 12:43:29 +0100

    Change approach

---
+++ x-pack/platform/plugins/shared/index_management/public/application/components/template_form/build_template_from_wizard_data.ts
@@ -0,0 +1,77 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { TemplateDeserialized } from '../../../../common';
+import type { DataStreamOptions } from '../../../../common/types/data_streams';
+import { serializeAsESLifecycle } from '../../../../common/lib';
+
+export interface TemplateFormWizardData {
+  logistics: Omit<TemplateDeserialized, '_kbnMeta' | 'template'>;
+  settings?: Record<string, unknown>;
+  mappings?: Record<string, unknown>;
+  aliases?: Record<string, unknown>;
+  components: TemplateDeserialized['composedOf'];
+}
+
+/**
+ * If no mappings, settings or aliases are defined, it is better to not send empty
+ * object for those values.
+ * This method takes care of that and other cleanup of empty fields.
+ * @param template The template object to clean up
+ */
+const cleanupTemplateObject = (template: TemplateDeserialized) => {
+  const outputTemplate = { ...template };
+
+  if (outputTemplate.template) {
+    if (outputTemplate.template.settings === undefined) {
+      delete outputTemplate.template.settings;
+    }
+    if (outputTemplate.template.mappings === undefined) {
+      delete outputTemplate.template.mappings;
+    }
+    if (outputTemplate.template.aliases === undefined) {
+      delete outputTemplate.template.aliases;
+    }
+    if (Object.keys(outputTemplate.template).length === 0) {
+      delete outputTemplate.template;
+    }
+    if (outputTemplate.lifecycle) {
+      delete outputTemplate.lifecycle;
+    }
+  }
+
+  return outputTemplate;
+};
+
+export const buildTemplateFromWizardData = ({
+  initialTemplate,
+  wizardData,
+  dataStreamOptions,
+}: {
+  initialTemplate: TemplateDeserialized;
+  wizardData: TemplateFormWizardData;
+  dataStreamOptions?: DataStreamOptions;
+}): TemplateDeserialized => {
+  const outputTemplate = {
+    ...wizardData.logistics,
+    _kbnMeta: initialTemplate._kbnMeta,
+    deprecated: initialTemplate.deprecated,
+    composedOf: wizardData.components,
+    template: {
+      settings: wizardData.settings,
+      mappings: wizardData.mappings,
+      aliases: wizardData.aliases,
+      lifecycle: wizardData.logistics.lifecycle
+        ? serializeAsESLifecycle(wizardData.logistics.lifecycle)
+        : undefined,
+      ...(dataStreamOptions && { data_stream_options: dataStreamOptions }),
+    },
+    ignoreMissingComponentTemplates: initialTemplate.ignoreMissingComponentTemplates,
+  };
+
+  return cleanupTemplateObject(outputTemplate as TemplateDeserialized);
+};
