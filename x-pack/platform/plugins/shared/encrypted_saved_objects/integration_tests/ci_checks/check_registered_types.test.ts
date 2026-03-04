0ebdc28fa18a62d846186a3cef465b57c8e7ec4a
Kevin Delemme <kdelemme@gmail.com>
2026-03-04 10:28:36 -0500

    Fix so test

--- x-pack/platform/plugins/shared/encrypted_saved_objects/integration_tests/ci_checks/check_registered_types.test.ts
+++ x-pack/platform/plugins/shared/encrypted_saved_objects/integration_tests/ci_checks/check_registered_types.test.ts
@@ -68,7 +68,7 @@ describe('checking changes on all registered encrypted SO types', () => {
         "action_task_params": "06aa563283bdcd5c07ec433a7d0b8425019ad11d75595ee1431691667ecd2cec",
         "ad_hoc_run_params": "6539367aa4ae8340c62f123c3457c6b8d7873c92de68651c70d41028dfe7ed32",
         "alert": "878a3b83179bbf2ad9d3862fcba539b7066429869b14c120a1dc7a8d39f4a7fa",
-        "alerting_notification_policy": "a4b6de55aa3de9bed3e4922456bb58038a4be40cc32bbfae0657443f55ef0a48",
+        "alerting_notification_policy": "b16f664f29e77eeeea9c0bfe0a5dbb8a1ca52d77ed79ab0092ab9127225dc808",
         "api_key_pending_invalidation": "4dafadadaaca2f2f3f6038ee8363b71b2d101371ca98c34d2b6aa2a96f7e71c5",
         "cloud-connect-api-key": "8c0ae7a780c411145ae4aaf7a70235672c9ccfb56d011c322da3c4eeb258f32d",
         "connector_token": "16ca2154c13c5ee3d3a45b55d4ea6cd33aeaceaef3dc229b002d25470bfc9b3b",
