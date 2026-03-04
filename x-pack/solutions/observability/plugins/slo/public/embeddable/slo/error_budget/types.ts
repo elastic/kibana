da915817db9fa59debfea7e43e3ec4d9ba78014f
Panagiota Mitsopoulou <giota85@gmail.com>
2026-03-04 23:12:19 +0200

    cleanup unused types

--- x-pack/solutions/observability/plugins/slo/public/embeddable/slo/error_budget/types.ts
+++ x-pack/solutions/observability/plugins/slo/public/embeddable/slo/error_budget/types.ts
@@ -7,19 +7,12 @@
 import type { PublishesWritableTitle, PublishesTitle } from '@kbn/presentation-publishing';
 import type { DefaultEmbeddableApi, HasDrilldowns } from '@kbn/embeddable-plugin/public';
 import type { Subject } from 'rxjs';
-import type { IUiSettingsClient, ApplicationStart, NotificationsStart } from '@kbn/core/public';
-import { type CoreStart } from '@kbn/core/public';
 import type { ErrorBudgetEmbeddableState } from '../../../../common/embeddables/error_budget/types';
 
 export type {
   ErrorBudgetCustomState,
   ErrorBudgetEmbeddableState,
 } from '../../../../common/embeddables/error_budget/types';
-
-export interface ErrorBudgetCustomInput {
-  sloId: string | undefined;
-  sloInstanceId: string | undefined;
-}
 
 export interface EmbeddableSloProps {
   sloId: string | undefined;
@@ -32,11 +25,3 @@ export type ErrorBudgetApi = DefaultEmbeddableApi<ErrorBudgetEmbeddableState> &
   PublishesWritableTitle &
   PublishesTitle &
   HasDrilldowns;
-
-export interface SloEmbeddableDeps {
-  uiSettings: IUiSettingsClient;
-  http: CoreStart['http'];
-  i18n: CoreStart['i18n'];
-  application: ApplicationStart;
-  notifications: NotificationsStart;
-}
