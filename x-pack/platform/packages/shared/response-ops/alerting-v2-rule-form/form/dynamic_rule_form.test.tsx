050da5f5742745d7f5595cc713c5123fcf74c6fa
Dominique Clarke <dominique.clarke@elastic.co>
2026-03-04 12:50:50 -0500

    Apply suggestion from @dominiqueclarke

--- x-pack/platform/packages/shared/response-ops/alerting-v2-rule-form/form/dynamic_rule_form.test.tsx
+++ x-pack/platform/packages/shared/response-ops/alerting-v2-rule-form/form/dynamic_rule_form.test.tsx
@@ -8,7 +8,6 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { notificationServiceMock } from '@kbn/core/public/mocks';
 import { httpServiceMock } from '@kbn/core-http-browser-mocks';
 import { dataPluginMock } from '@kbn/data-plugin/public/mocks';
 import { dataViewPluginMocks } from '@kbn/data-views-plugin/public/mocks';
