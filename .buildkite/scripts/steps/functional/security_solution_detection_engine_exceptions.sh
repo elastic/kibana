c2644e8ace574891242f0f80d0e439eb990bf772
Patryk Kopycinski <patryk.kopycinski@elastic.co>
2026-03-05 07:53:39 +0100

    Clean up no-op blank line changes in shell scripts

--- .buildkite/scripts/steps/functional/security_solution_detection_engine_exceptions.sh
+++ .buildkite/scripts/steps/functional/security_solution_detection_engine_exceptions.sh
@@ -7,7 +7,6 @@ source .buildkite/scripts/steps/functional/common.sh
 export JOB=kibana-security-solution-chrome
 export KIBANA_INSTALL_DIR=${KIBANA_BUILD_LOCATION}
 
-
 echo "--- Detection Engine - Exceptions - Security Solution Cypress Tests"
 
 cd x-pack/solutions/security/test/security_solution_cypress
