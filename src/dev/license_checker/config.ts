51621f3dc56994b776567609b017fa6a81d9d65e
Tiago Costa <tiago.costa@elastic.co>
2026-03-06 06:52:39 +0000

    chore(NA): add license override

--- src/dev/license_checker/config.ts
+++ src/dev/license_checker/config.ts
@@ -98,5 +98,7 @@ export const LICENSE_OVERRIDES = {
   'language-subtag-registry@0.3.21': ['CC-BY-4.0'], // retired ODC‑By license https://github.com/mattcg/language-subtag-registry
   'buffers@0.1.1': ['MIT'], // license in importing module https://www.npmjs.com/package/binary
   '@bufbuild/protobuf@2.5.2': ['Apache-2.0'], // license (Apache-2.0 AND BSD-3-Clause)
-  '@arizeai/phoenix-client@4.2.0': ['Elastic License 2.0'], // see https://github.com/Arize-ai/phoenix/blob/main/LICENSE
+  '@arizeai/phoenix-client@4.2.0': ['Elastic License 2.0'], // see https://github.com/Arize-ai/phoenix/blob/main/LICENSE,
+  '@swc/core-linux-x64-gnu@1.15.18': ['MIT'],
+  '@swc/core-linux-x64-musl@1.15.18': ['MIT'],
 };
