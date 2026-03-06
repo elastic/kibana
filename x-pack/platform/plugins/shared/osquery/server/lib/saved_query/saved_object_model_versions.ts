56cb176b700b4ca2743722b4661655a3160fe950
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2026-03-06 12:16:21 +0100

    [Osquery] Fix model schema

--- x-pack/platform/plugins/shared/osquery/server/lib/saved_query/saved_object_model_versions.ts
+++ x-pack/platform/plugins/shared/osquery/server/lib/saved_query/saved_object_model_versions.ts
@@ -5,7 +5,68 @@
  * 2.0.
  */
 
+import { schema } from '@kbn/config-schema';
 import type { SavedObjectsModelVersion } from '@kbn/core-saved-objects-server';
+
+const packQuerySchemaV1 = schema.object(
+  {
+    id: schema.maybe(schema.string()),
+    query: schema.maybe(schema.string()),
+    interval: schema.maybe(schema.string()),
+    timeout: schema.maybe(schema.number()),
+    platform: schema.maybe(schema.string()),
+    version: schema.maybe(schema.string()),
+    ecs_mapping: schema.maybe(schema.object({}, { unknowns: 'allow' })),
+  },
+  { unknowns: 'allow' }
+);
+
+const packSchemaV1 = schema.object(
+  {
+    description: schema.maybe(schema.string()),
+    name: schema.maybe(schema.string()),
+    created_at: schema.maybe(schema.string()),
+    created_by: schema.maybe(schema.string()),
+    updated_at: schema.maybe(schema.string()),
+    updated_by: schema.maybe(schema.string()),
+    enabled: schema.maybe(schema.boolean()),
+    shards: schema.maybe(schema.object({}, { unknowns: 'allow' })),
+    version: schema.maybe(schema.number()),
+    queries: schema.maybe(schema.arrayOf(packQuerySchemaV1)),
+  },
+  { unknowns: 'allow' }
+);
+
+const packQuerySchemaV2 = schema.object(
+  {
+    id: schema.maybe(schema.string()),
+    query: schema.maybe(schema.string()),
+    interval: schema.maybe(schema.string()),
+    timeout: schema.maybe(schema.number()),
+    platform: schema.maybe(schema.string()),
+    version: schema.maybe(schema.string()),
+    schedule_id: schema.maybe(schema.string()),
+    start_date: schema.maybe(schema.string()),
+    ecs_mapping: schema.maybe(schema.object({}, { unknowns: 'allow' })),
+  },
+  { unknowns: 'allow' }
+);
+
+const packSchemaV2 = schema.object(
+  {
+    description: schema.maybe(schema.string()),
+    name: schema.maybe(schema.string()),
+    created_at: schema.maybe(schema.string()),
+    created_by: schema.maybe(schema.string()),
+    updated_at: schema.maybe(schema.string()),
+    updated_by: schema.maybe(schema.string()),
+    enabled: schema.maybe(schema.boolean()),
+    shards: schema.maybe(schema.object({}, { unknowns: 'allow' })),
+    version: schema.maybe(schema.number()),
+    queries: schema.maybe(schema.arrayOf(packQuerySchemaV2)),
+  },
+  { unknowns: 'allow' }
+);
 
 export const savedQueryModelVersion1: SavedObjectsModelVersion = {
   changes: [
@@ -47,6 +108,10 @@ export const packSavedObjectModelVersion2: SavedObjectsModelVersion = {
       },
     },
   ],
+  schemas: {
+    forwardCompatibility: packSchemaV1.extends({}, { unknowns: 'ignore' }),
+    create: packSchemaV2,
+  },
 };
 
 export const packAssetSavedObjectModelVersion1: SavedObjectsModelVersion = {
