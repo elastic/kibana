7b525caf6fbb138b826df46d7cbb410a4a7bb557
Garrett Spong <garrett.spong@elastic.co>
2026-03-06 10:39:42 -0700

    Add evals nav item to serverless when enabled

--- x-pack/solutions/observability/plugins/serverless_observability/public/navigation_tree.ts
+++ x-pack/solutions/observability/plugins/serverless_observability/public/navigation_tree.ts
@@ -24,11 +24,13 @@ export const createNavigationTree = ({
   overviewAvailable = true,
   isCasesAvailable = true,
   showAiAssistant = true,
+  showEvals = false,
 }: {
   streamsAvailable?: boolean;
   overviewAvailable?: boolean;
   isCasesAvailable?: boolean;
   showAiAssistant?: boolean;
+  showEvals?: boolean;
 }): NavigationTreeDefinition => {
   return {
     body: [
@@ -380,6 +382,16 @@ export const createNavigationTree = ({
           },
         ],
       },
+      ...filterForFeatureAvailability(
+        {
+          link: 'evals',
+          title: i18n.translate('xpack.serverlessObservability.nav.evals', {
+            defaultMessage: 'Evaluations',
+          }),
+          icon: 'beaker',
+        },
+        showEvals
+      ),
     ],
     footer: [
       {
