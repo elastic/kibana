cff5fd132f8c92678bc06529225e09f9855c0496
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-03-06 10:04:41 +0000

    Changes from yarn openapi:generate

--- x-pack/platform/plugins/shared/osquery/common/api/unified_history/unified_history.gen.ts
+++ x-pack/platform/plugins/shared/osquery/common/api/unified_history/unified_history.gen.ts
@@ -14,15 +14,7 @@
  *   version: 2023-10-31
  */
 
-import type { ZodTypeDef } from '@kbn/zod/v4';
 import { z } from '@kbn/zod/v4';
-import {
-  requiredOptional,
-  isValidDateMath,
-  isNonEmptyString,
-  ArrayFromString,
-  BooleanFromString,
-} from '@kbn/zod-helpers/v4';
 
 import { GetUnifiedHistoryResponse } from './get_unified_history.gen';
 
