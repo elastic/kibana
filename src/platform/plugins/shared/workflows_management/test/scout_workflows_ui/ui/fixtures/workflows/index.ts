8983a7a7ecf9b622f909f7d9cb176a33fe586dc9
Marco Liberati <dej611@users.noreply.github.com>
2026-03-09 11:02:42 +0100

    [One Workflow] Fix liquidjs validation for variables used before assignment (#255337)
    ## Summary
    
    This PR fixes a bug found when testing #253405 where inline variables
    used before assignment could be wrongly marked as valid.
    
    The validation flow now scans the liquidjs template to find the right
    offset for the scalar value and make validation mark correctly invalid
    variables.
    
    Added both unit and e2e tests fro the fix.
    
    #### Testing
    
    For manual testing I've prepared the following workflow spec to use:
    ```yaml
    name: New workflow
    enabled: false
    description: This is a new workflow
    triggers:
      - type: manual
    
    inputs:
      - name: message
        type: string
        default: "hello world"
    
    steps:
      - name: hello_world_step
        type: console
        with:
          message: |
            {% assign b = 1 %} 
            {{ b }}
            {{ a }} {% assign a = 1 %} {{ a }}
            {% assign c = 1 %} {{ c }}{{ d }}{% assign d = 1 %} 
            {{ cap }}{% capture cap %}body{% endcapture %}
            {{ x }}{%- liquid
              assign x = 1
            -%}
            {{ cap3 }}
            {% capture cap3 %}body{% endcapture %}
    
    ```
    
    
    ### Checklist
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios

--- src/platform/plugins/shared/workflows_management/test/scout_workflows_ui/ui/fixtures/workflows/index.ts
+++ src/platform/plugins/shared/workflows_management/test/scout_workflows_ui/ui/fixtures/workflows/index.ts
@@ -29,3 +29,16 @@ export {
   getCreateObsAlertRuleWorkflowYaml,
   getTriggerAlertWorkflowYaml,
 } from './alert_workflows';
+export {
+  getAssignAfterUseSameLine,
+  getAssignBeforeUseSameLine,
+  getCaptureAfterUseSameLine,
+  getCaptureBeforeUseSameLine,
+  getAssignAfterUseCrossLine,
+  getAssignBeforeUseCrossLine,
+  getCaptureAfterUseCrossLine,
+  getCaptureBeforeUseCrossLine,
+  getMixedAssignAndCaptureSameLine,
+  getMultipleAssignsInterleaved,
+  getCaptureUsedBeforeAndAfter,
+} from './variable_validation_workflows';
