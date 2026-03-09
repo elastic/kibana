62e4e8881418957bd8c3efae5f396010e1d802c5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-03-09 14:42:16 +0000

    Changes from node scripts/lint.js --fix

--- x-pack/platform/plugins/shared/inference/server/util/get_inference_endpoints.ts
+++ x-pack/platform/plugins/shared/inference/server/util/get_inference_endpoints.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { InferenceTaskType } from '@elastic/elasticsearch/lib/api/types';
+import type { InferenceTaskType } from '@elastic/elasticsearch/lib/api/types';
 import type { ElasticsearchClient } from '@kbn/core/server';
 
 export interface InferenceEndpoint {
