a80e49c2ccd8f344209b91ee9290677d968e3fa5
Arturo Lidueña <arturo.liduena@elastic.co>
2026-03-09 11:10:20 +0100

    fix AB error

--- x-pack/platform/plugins/shared/agent_builder/server/services/agents/modes/default/run_chat_agent.ts
+++ x-pack/platform/plugins/shared/agent_builder/server/services/agents/modes/default/run_chat_agent.ts
@@ -16,6 +16,7 @@ import { HookLifecycle } from '@kbn/agent-builder-server';
 import type { ConversationInternalState } from '@kbn/agent-builder-common/chat';
 import type { ToolManager } from '@kbn/agent-builder-server/runner';
 import { ToolManagerToolType, type PromptManager } from '@kbn/agent-builder-server/runner';
+import { reverseMap } from '@kbn/agent-builder-genai-utils/langchain/tools';
 import type { ProcessedConversation } from '../utils/prepare_conversation';
 import { createResultTransformer } from '../utils/create_result_transformer';
 import {
@@ -202,7 +203,7 @@ export const runDefaultAgentMode: RunChatAgentFn = async (
   const eventStream = agentGraph.streamEvents(
     createInitializerCommand({
       conversation: processedConversation,
-      agentBuilderToLangchainIdMap: toolManager.getToolIdMapping(),
+      agentBuilderToLangchainIdMap: reverseMap(toolManager.getToolIdMapping()),
       cycleLimit,
     }),
     {
