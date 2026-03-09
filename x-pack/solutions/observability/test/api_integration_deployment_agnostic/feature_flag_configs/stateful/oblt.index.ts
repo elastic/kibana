80fc92b6319497e115bacf1881537467f10d4acb
Tiago Costa <tiago.costa@elastic.co>
2026-03-09 08:26:15 +0000

    chore(NA): missing configs, final changes

--- x-pack/solutions/observability/test/api_integration_deployment_agnostic/feature_flag_configs/stateful/oblt.index.ts
+++ x-pack/solutions/observability/test/api_integration_deployment_agnostic/feature_flag_configs/stateful/oblt.index.ts
@@ -10,5 +10,6 @@ export default function ({ loadTestFile }: DeploymentAgnosticFtrProviderContext)
   describe('Stateful Observability feature flag testing  - Deployment-agnostic API integration tests', function () {
     loadTestFile(require.resolve('../../apis/alerting/synthetics/synthetics_default_rule'));
     loadTestFile(require.resolve('../../apis/alerting/synthetics/custom_status_rule'));
+    loadTestFile(require.resolve('../../apis/alerting/synthetics/alert_on_no_data'));
   });
 }
