c77bf775c3d66ced1a03c94b54358dcf78a21ed8
CAWilson94 <charlotte.wilson@elastic.co>
2026-03-10 11:06:09 +0000

    Remove index upload in favour of create watchlist by custom query rule

--- x-pack/solutions/security/plugins/security_solution/public/flyout/entity_details/watchlists_right/watchlist_form.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/entity_details/watchlists_right/watchlist_form.tsx
@@ -5,9 +5,8 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
-  EuiComboBox,
   EuiFieldText,
   EuiFilePicker,
   EuiFlexGroup,
@@ -17,10 +16,16 @@ import {
   EuiHorizontalRule,
   EuiRange,
   EuiSpacer,
+  EuiSuperSelect,
   EuiText,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { i18n } from '@kbn/i18n';
+import type { DataView } from '@kbn/data-views-plugin/public';
+import type { Filter, Query } from '@kbn/es-query';
+import type { SavedQuery } from '@kbn/data-plugin/public';
+import { QueryBar } from '../../../common/components/query_bar';
+import { useKibana } from '../../../common/lib/kibana';
 import type { CreateWatchlistRequestBodyInput } from '../../../../common/api/entity_analytics/watchlists/management/create.gen';
 import { SUPPORTED_FILE_TYPES } from './constants';
 
@@ -33,6 +38,49 @@ export interface WatchlistFormProps {
 }
 
 export const WatchlistForm = ({ watchlist, onFieldChange }: WatchlistFormProps) => {
+  const {
+    services: { data },
+  } = useKibana();
+  const [dataView, setDataView] = useState<DataView>();
+  const [filterQuery, setFilterQuery] = useState<Query>({ query: '', language: 'kuery' });
+  const [savedQuery, setSavedQuery] = useState<SavedQuery | undefined>(undefined);
+  const [filters, setFilters] = useState<Filter[]>([]);
+  const [entityField, setEntityField] = useState<string>('');
+  const filterManager = data.query.filterManager;
+
+  useEffect(() => {
+    setFilters(filterManager.getFilters());
+    const subscription = filterManager.getUpdates$().subscribe(() => {
+      setFilters(filterManager.getFilters());
+    });
+
+    return () => subscription.unsubscribe();
+  }, [filterManager]);
+
+  useEffect(() => {
+    let isSubscribed = true;
+    const loadDefaultDataView = async () => {
+      const defaultDataView = await data.dataViews.getDefaultDataView();
+      if (isSubscribed && defaultDataView) {
+        setDataView(defaultDataView);
+      }
+    };
+
+    loadDefaultDataView();
+
+    return () => {
+      isSubscribed = false;
+    };
+  }, [data.dataViews]);
+
+  const onSubmitQuery = useCallback((query: Query) => {
+    setFilterQuery(query);
+  }, []);
+
+  const onSavedQuery = useCallback((newSavedQuery: SavedQuery | undefined) => {
+    setSavedQuery(newSavedQuery);
+  }, []);
+
   return (
     <EuiForm component="form" fullWidth>
       <EuiFormRow label="Name">
@@ -104,34 +152,79 @@ export const WatchlistForm = ({ watchlist, onFieldChange }: WatchlistFormProps) 
         </EuiFlexItem>
       </EuiFlexGroup>
       <EuiSpacer size="m" />
+      <EuiFlexGroup alignItems="center" gutterSize="s" responsive={false}>
+        <EuiFlexItem grow={false}>
+          <EuiText size="s">
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.watchlists.flyout.createByQueryTitle"
+              defaultMessage="Create watchlist by query"
+            />
+          </EuiText>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+      <EuiSpacer size="m" />
       <EuiFormRow
-        label="Index Patterns"
+        label={i18n.translate(
+          'xpack.securitySolution.entityAnalytics.watchlists.flyout.filterQueryLabel',
+          { defaultMessage: 'Watchlist filter' }
+        )}
         helpText={i18n.translate(
-          'xpack.securitySolution.entityAnalytics.watchlists.flyout.indexPatternsHelpText',
-          {
-            defaultMessage:
-              'Enter the pattern of Elasticsearch indicies where you would like this rule to run. By default, these will include index patterns defined in Security Solutions advanced settings. ',
-          }
+          'xpack.securitySolution.entityAnalytics.watchlists.flyout.filterQueryHelpText',
+          { defaultMessage: 'Build a query to filter matching events for this watchlist (POC).' }
         )}
       >
-        <EuiComboBox
-          placeholder="Select or create index patterns"
-          selectedOptions={[{ label: 'one', value: 'one' }]}
-          options={[
-            { label: 'one', value: 'one' },
-            { label: 'two', value: 'two' },
-            { label: 'three', value: 'three' },
-          ]}
-          onChange={() => {}}
-          isClearable
-          fullWidth
-          aria-label={i18n.translate(
-            'xpack.securitySolution.entityAnalytics.watchlists.flyout.indexSelector.AriaLabel',
-            {
-              defaultMessage: 'Watchlist index selector',
-            }
-          )}
-        />
+        <EuiFlexGroup direction="column" gutterSize="m">
+          <EuiFlexItem>
+            {dataView ? (
+              // TODO: plug into endpoint when available: https://github.com/elastic/security-team/issues/15538
+              <QueryBar
+                indexPattern={dataView}
+                isRefreshPaused={true}
+                filterQuery={filterQuery}
+                filterManager={filterManager}
+                filters={filters}
+                onSubmitQuery={onSubmitQuery}
+                savedQuery={savedQuery}
+                onSavedQuery={onSavedQuery}
+                hideSavedQuery={false}
+                displayStyle="inPage"
+                dataTestSubj="watchlistFilterQuery"
+              />
+            ) : (
+              <EuiText size="s" color="subdued">
+                <FormattedMessage
+                  id="xpack.securitySolution.entityAnalytics.watchlists.flyout.filterQueryLoading"
+                  defaultMessage="Loading data view..."
+                />
+              </EuiText>
+            )}
+          </EuiFlexItem>
+        </EuiFlexGroup>
+      </EuiFormRow>
+      <EuiFormRow label="Identify entities by">
+        <EuiFlexGroup alignItems="center" gutterSize="s" responsive={false}>
+          <EuiFlexItem grow={false}>
+            <EuiSuperSelect
+              valueOfSelected={entityField}
+              placeholder={i18n.translate(
+                'xpack.securitySolution.entityAnalytics.watchlists.flyout.entityFieldPlaceholder',
+                { defaultMessage: 'Select a field' }
+              )}
+              // TODO: remove this when backend route available: https://github.com/elastic/security-team/issues/15538
+              options={[
+                { value: 'user.name', inputDisplay: 'user.name' },
+                { value: 'host.name', inputDisplay: 'host.name' },
+                { value: 'source.ip', inputDisplay: 'source.ip' },
+              ]}
+              onChange={(value) => setEntityField(value)}
+              aria-label={i18n.translate(
+                'xpack.securitySolution.entityAnalytics.watchlists.flyout.entityFieldAriaLabel',
+                { defaultMessage: 'Watchlist entity field selector' }
+              )}
+              style={{ width: 240 }}
+            />
+          </EuiFlexItem>
+        </EuiFlexGroup>
       </EuiFormRow>
     </EuiForm>
   );
