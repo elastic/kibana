e94d162ee8b6846f5a0373d08bbb3e8c536a2155
Marco Vettorello <marco.vettorello@elastic.co>
2026-03-10 18:17:47 +0100

    Build lens bundle

--- x-pack/platform/plugins/shared/lens/server/api/routes/visualizations/get.ts
+++ x-pack/platform/plugins/shared/lens/server/api/routes/visualizations/get.ts
@@ -10,7 +10,7 @@ import { boomify, isBoom } from '@hapi/boom';
 import type { TypeOf } from '@kbn/config-schema';
 
 import { LENS_CONTENT_TYPE } from '@kbn/lens-common/content_management/constants';
-import { LENS_VIS_API_PATH, LENS_API_VERSION } from '../../../../common/constants';
+import { LENS_VIS_API_PATH, LENS_API_VERSION, LENS_API_ACCESS } from '../../../../common/constants';
 import type { LensSavedObject } from '../../../content_management';
 import { lensGetRequestParamsSchema, lensGetResponseBodySchema } from './schema';
 import { getLensResponseItem } from './utils';
@@ -22,7 +22,7 @@ export const registerLensVisualizationsGetAPIRoute: RegisterAPIRouteFn = (
 ) => {
   const getRoute = router.get({
     path: `${LENS_VIS_API_PATH}/{id}`,
-    access: 'internal', // to go public in 9.4
+    access: LENS_API_ACCESS,
     enableQueryVersion: true,
     summary: 'Get Lens visualization',
     description: 'Get a Lens visualization from id.',
