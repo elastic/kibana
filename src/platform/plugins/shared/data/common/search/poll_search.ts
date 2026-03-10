07f16148280fd99c72139e1389253f35a9320a50
Drew Tate <drew.tate@elastic.co>
2026-03-10 15:52:00 -0600

    remove the final poll request

--- src/platform/plugins/shared/data/common/search/poll_search.ts
+++ src/platform/plugins/shared/data/common/search/poll_search.ts
@@ -8,18 +8,7 @@
  */
 
 import type { Observable } from 'rxjs';
-import {
-  defer,
-  EMPTY,
-  expand,
-  from,
-  fromEvent,
-  switchMap,
-  takeUntil,
-  takeWhile,
-  tap,
-  throwError,
-} from 'rxjs';
+import { defer, EMPTY, expand, from, fromEvent, switchMap, takeUntil, tap, throwError } from 'rxjs';
 import { AbortError } from '@kbn/kibana-utils-plugin/common';
 import type { IKibanaSearchResponse } from '@kbn/search-types';
 import type { IAsyncSearchOptions } from '..';
@@ -49,15 +38,14 @@ export const pollSearch = <Response extends IKibanaSearchResponse>(
     );
 
     return from(search()).pipe(
-      expand(() => {
-        return search();
+      expand((response) => {
+        return isRunningResponse(response) ? search() : EMPTY;
       }),
       tap((response) => {
         if (isAbortResponse(response)) {
           throw new AbortError();
         }
       }),
-      takeWhile<Response>(isRunningResponse, true),
       takeUntil<Response>(aborted$)
     );
   });
