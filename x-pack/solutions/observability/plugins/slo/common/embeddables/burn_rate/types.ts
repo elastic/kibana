40be68a399638fd9cc926686c6066103c277a5be
Panagiota Mitsopoulou <panagiota.mitsopoulou@elastic.co>
2026-03-10 20:14:51 +0200

    [SLO] Burn rate schema registration (#256566)
    Fixes https://github.com/elastic/kibana/issues/252818
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>

--- x-pack/solutions/observability/plugins/slo/common/embeddables/burn_rate/types.ts
+++ x-pack/solutions/observability/plugins/slo/common/embeddables/burn_rate/types.ts
@@ -5,5 +5,8 @@
  * 2.0.
  */
 
-export { SLO_BURN_RATE_EMBEDDABLE_ID } from '../../../../common/embeddables/burn_rate/constants';
-export const ADD_BURN_RATE_ACTION_ID = 'CREATE_SLO_BURN_RATE_EMBEDDABLE';
+/** Re-exported from server (derived from schemas there) */
+export type {
+  BurnRateCustomState,
+  BurnRateEmbeddableState,
+} from '../../../server/lib/embeddables/burn_rate_schema';
