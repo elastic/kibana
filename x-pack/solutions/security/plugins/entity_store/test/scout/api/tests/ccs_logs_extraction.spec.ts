4b82d23f3f70ac86c20209c9defe736d2ee811cb
Sean Rathier <sean.rathier@elastic.co>
2026-03-10 18:37:28 -0400

    unskip test

--- x-pack/solutions/security/plugins/entity_store/test/scout/api/tests/ccs_logs_extraction.spec.ts
+++ x-pack/solutions/security/plugins/entity_store/test/scout/api/tests/ccs_logs_extraction.spec.ts
@@ -479,11 +479,7 @@ apiTest.describe.skip(
       }
     );
 
-    // TODO: Fix bug - entity.id field conflict in ES|QL query for generic entity type
-    // The query uses entity.id both as aggregation field and GROUP BY key which is invalid
-    // See: https://github.com/elastic/kibana/issues/XXXXX
-    // eslint-disable-next-line playwright/no-skipped-test
-    apiTest.skip(
+    apiTest(
       'Should run CCS extraction for generic and write to updates then latest index',
       async ({ apiClient, esClient }) => {
         await createCcsTestLogsIndex(esClient);
