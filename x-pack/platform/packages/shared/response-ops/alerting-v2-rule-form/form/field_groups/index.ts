3be641aa81ec891c34cc6b795f4791b13ea96a5b
ana.davydova@elastic.co <ana.davydova@elastic.co>
2026-03-11 13:16:06 +0100

    small typo fix

--- x-pack/platform/packages/shared/response-ops/alerting-v2-rule-form/form/field_groups/index.ts
+++ x-pack/platform/packages/shared/response-ops/alerting-v2-rule-form/form/field_groups/index.ts
@@ -8,8 +8,5 @@
 export { ConditionFieldGroup } from './condition_field_group';
 export { RuleDetailsFieldGroup } from './rule_details_field_group';
 export { RuleExecutionFieldGroup } from './rule_execution_field_group';
-<<<<<<< alertingv2-add-runbook
 export { AttacmentRunbookGroup } from './attachment_runbook_group';
-=======
 export { StateTransitionFieldGroup } from './state_transition_field_group';
->>>>>>> alerting_v2
