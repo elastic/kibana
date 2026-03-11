2158f8d43cd83e0359fb71f23ce325852839b976
Ryland Herrick <ryalnd@gmail.com>
2026-03-11 17:27:21 -0500

    Guard v2 flyout with Alerts Read (if relevant)

--- x-pack/solutions/security/plugins/security_solution/public/flyout_v2/document/tabs/overview_tab.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout_v2/document/tabs/overview_tab.tsx
@@ -9,6 +9,9 @@ import React, { memo } from 'react';
 import { EuiHorizontalRule, EuiPanel } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import type { DataTableRecord } from '@kbn/discover-utils';
+import { DEFAULT_ALERTS_INDEX } from '../../../../common/constants';
+import { useAlertsPrivileges } from '../../../detections/containers/detection_engine/alerts/use_alerts_privileges';
+import { FlyoutMissingAlertsPrivilege } from '../../../flyout/shared/components/flyout_missing_alerts_privilege';
 import { AboutSection } from '../components/about_section';
 import { InvestigationSection } from '../components/investigation_section';
 import { VisualizationsSection } from '../components/visualizations_section';
@@ -17,6 +20,11 @@ const OVERVIEW_ARIA_LABEL = i18n.translate(
   'xpack.securitySolution.flyout.document.overview.overviewContentAriaLabel',
   { defaultMessage: 'Overview' }
 );
+
+function isAlertHit(hit: DataTableRecord): boolean {
+  const eventIndex = (hit.raw as { _index?: string })?._index;
+  return eventIndex != null && eventIndex.includes(DEFAULT_ALERTS_INDEX);
+}
 
 export interface OverviewTabProps {
   /**
@@ -29,6 +37,13 @@ export interface OverviewTabProps {
  * Overview view displayed in the document details expandable flyout right section
  */
 export const OverviewTab = memo(({ hit }: OverviewTabProps) => {
+  const { hasAlertsRead } = useAlertsPrivileges();
+  const missingAlertsPrivilege = isAlertHit(hit) && !hasAlertsRead;
+
+  if (missingAlertsPrivilege) {
+    return <FlyoutMissingAlertsPrivilege />;
+  }
+
   return (
     <EuiPanel hasBorder={false} hasShadow={false} aria-label={OVERVIEW_ARIA_LABEL}>
       <AboutSection hit={hit} />
