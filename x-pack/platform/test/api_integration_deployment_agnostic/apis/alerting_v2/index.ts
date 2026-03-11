7c01a5b0a5bcb4b637b5805919e23bf9de30bb9e
Dominique Belcher <dominique.clarke@elastic.co>
2026-03-11 10:24:53 -0400

    add integration tests for rule crud

--- x-pack/platform/test/api_integration_deployment_agnostic/apis/alerting_v2/index.ts
+++ x-pack/platform/test/api_integration_deployment_agnostic/apis/alerting_v2/index.ts
@@ -12,5 +12,6 @@ export default function ({ loadTestFile }: DeploymentAgnosticFtrProviderContext)
     loadTestFile(require.resolve('./create_alert_action'));
     loadTestFile(require.resolve('./bulk_create_alert_action'));
     loadTestFile(require.resolve('./notification_policy'));
+    loadTestFile(require.resolve('./rule'));
   });
 }
