ce031ad80b0eb25af11bd06e156b40e7bd591efd
Nathan Reese <reese.nathan@elastic.co>
2026-03-11 20:34:07 -0600

    [Dashboards as code] Transform panel type names (#254079)
    Closes https://github.com/elastic/kibana/issues/253825
    
    Dashboards as code project is creating a public CRUD REST API for
    dashboards. Embeddable types are part of this API and exposed from
    `panel.type` key. This PR changes embeddable types to be lower case
    snake_cased strings. The table below shows current embeddable type value
    and "as code" embeddable type value.
    
    | Current value      | "as code" value |
    | ----------- | ----------- |
    | search | discover_session |
    | image | image |
    | links | links |
    | optionsListControl | options_list_control |
    | rangeSliderControl | range_slider_control |
    | esqlControl | esql_control |
    | timeSlider | time_slider_control |
    | DASHBOARD_MARKDOWN | markdown |
    | LOG_STREAM_EMBEDDABLE | log_stream |
    | visualization | visualization |
    | field_stats_table | field_stats_table |
    | aiopsChangePointChart | aiops_change_point_chart |
    | aiopsPatternAnalysisEmbeddable | aiops_pattern_analysis |
    | aiopsLogRateAnalysisEmbeddable | aiops_log_rate_analysis |
    | alerts_table | alerts_table |
    | lens | lens |
    | map | map |
    | ml_anomaly_swimlane | ml_anomaly_swimlane |
    | ml_anomaly_charts | ml_anomaly_charts |
    | ml_single_metric_viewer | ml_single_metric_viewer |
    | APM_ALERTING_LATENCY_CHART_EMBEDDABLE | apm_alerting_latency_chart |
    | APM_ALERTING_THROUGHPUT_CHART_EMBEDDABLE |
    apm_alerting_throughput_chart |
    | APM_ALERTING_FAILED_TRANSACTIONS_CHART_EMBEDDABLE |
    apm_alerting_failed_transactions_chart |
    | SLO_EMBEDDABLE | slo_overview |
    | SLO_ALERTS_EMBEDDABLE | slo_alerts |
    | SLO_ERROR_BUDGET_EMBEDDABLE | slo_error_budget |
    | SLO_BURN_RATE_EMBEDDABLE | slo_burn_rate |
    | SYNTHETICS_STATS_OVERVIEW_EMBEDDABLE | synthetics_stats_overview |
    | SYNTHETICS_MONITORS_EMBEDDABLE | synthetics_monitors | 
    
    You can see these new types in action by inspecting dashboard API
    requests. Notice how `panel.type` values.
    <img width="2038" height="737" alt="Screenshot 2026-02-19 at 12 25
    21 PM"
    src="https://github.com/user-attachments/assets/245dec65-c06c-4ddf-ad49-63a9ba0407e9"
    />
    
    ### Please review all of the following
    1) Spelling
    2) Ensure you like your "as code" embeddable type value. This value can
    not be changed once the dashboards as code API goes public
    3) Can value be shorted? 
    4) Are there any places other then dashboard state that stored
    embeddable type constant value and need to migrate from the old type to
    the new type?
    
    ### Testing instructions
    * Open existing dashboard with a panel that is your embeddable type
    * Ensure it opens as expected
    * Ensure actions such as adding panel use constant and still work as
    expected
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    Co-authored-by: Panagiota Mitsopoulou <giota85@gmail.com>

--- src/platform/plugins/shared/dashboard/public/dashboard_app/url/bwc/extract_pinned_panels_state.test.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_app/url/bwc/extract_pinned_panels_state.test.ts
@@ -47,7 +47,7 @@ describe('extractDashboardState', () => {
             selected_options: ['win 7'],
           },
           grow: true,
-          type: 'optionsListControl',
+          type: 'options_list_control',
           width: 'small',
         },
         {
@@ -57,7 +57,7 @@ describe('extractDashboardState', () => {
             selected_options: ['US'],
           },
           grow: false,
-          type: 'optionsListControl',
+          type: 'options_list_control',
           width: 'medium',
         },
       ]);
@@ -90,7 +90,7 @@ describe('extractDashboardState', () => {
             selected_options: ['win 7'],
           },
           grow: true,
-          type: 'optionsListControl',
+          type: 'options_list_control',
           width: 'small',
         },
       ]);
@@ -128,7 +128,7 @@ describe('extractDashboardState', () => {
             use_global_filters: false,
             ignore_validations: false,
           },
-          type: 'optionsListControl',
+          type: 'options_list_control',
         },
         {
           config: {
@@ -137,7 +137,7 @@ describe('extractDashboardState', () => {
             use_global_filters: false,
             ignore_validations: false,
           },
-          type: 'optionsListControl',
+          type: 'options_list_control',
         },
       ]);
     });
@@ -172,7 +172,7 @@ describe('extractDashboardState', () => {
             use_global_filters: false,
             ignore_validations: false,
           },
-          type: 'optionsListControl',
+          type: 'options_list_control',
         },
         {
           config: {
@@ -181,7 +181,7 @@ describe('extractDashboardState', () => {
             use_global_filters: false,
             ignore_validations: false,
           },
-          type: 'optionsListControl',
+          type: 'options_list_control',
         },
       ]);
     });
@@ -218,7 +218,7 @@ describe('extractDashboardState', () => {
             use_global_filters: true,
             ignore_validations: true,
           },
-          type: 'optionsListControl',
+          type: 'options_list_control',
         },
         {
           config: {
@@ -227,7 +227,7 @@ describe('extractDashboardState', () => {
             use_global_filters: true,
             ignore_validations: true,
           },
-          type: 'optionsListControl',
+          type: 'options_list_control',
         },
       ]);
     });
@@ -267,7 +267,7 @@ describe('extractDashboardState', () => {
             field_name: 'name.keyword',
             selected_options: ['US', 'Canada'],
           },
-          type: 'optionsListControl',
+          type: 'options_list_control',
           grow: true,
           width: 'small',
           uid: 'd3d7af60-4c81-11e8-b3d7-01146121b73d',
@@ -278,7 +278,7 @@ describe('extractDashboardState', () => {
             field_name: 'machine.os.keyword',
             selected_options: ['win 7'],
           },
-          type: 'optionsListControl',
+          type: 'options_list_control',
           grow: false,
           uid: '6c4f5ff4-92ff-4b40-bcc7-9aea6b06d693',
         },
@@ -319,7 +319,7 @@ describe('extractDashboardState', () => {
             ignore_validations: false,
           },
           uid: '6c4f5ff4-92ff-4b40-bcc7-9aea6b06d693',
-          type: 'optionsListControl',
+          type: 'options_list_control',
         },
       ]);
     });
@@ -352,7 +352,7 @@ describe('extractDashboardState', () => {
           },
           uid: '8311639d-92e5-4aa5-99a4-9502b10eead5',
           grow: true,
-          type: 'optionsListControl',
+          type: 'options_list_control',
           width: 'small',
         },
       ]);
