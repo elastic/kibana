6494a17eb2c6a941b7b5d98724fbdb4f8e1d4e53
Jonathan Budzenski <jon@elastic.co>
2026-03-12 17:39:31 -0500

    Revert "[8.19] Optimize Security Solution Cypress CI: right-size parallelism, remove dependency chain, add spot zones (#255183) (#257002)"
    This reverts commit 153890da2262897acb5423a94e3ebd27fd5f9454.

--- x-pack/solutions/security/plugins/security_solution/scripts/run_cypress/parallel_serverless.ts
+++ x-pack/solutions/security/plugins/security_solution/scripts/run_cypress/parallel_serverless.ts
@@ -30,14 +30,10 @@ import { createToolingLogger } from '../../common/endpoint/data_loaders/utils';
 import { renderSummaryTable } from './print_run';
 import {
   getOnBeforeHook,
-  orderSpecFilesForLoadBalance,
   parseTestFileConfig,
   retrieveIntegrations,
-  retrieveIntegrationsConfigAware,
   setDefaultToolingLoggingLevel,
 } from './utils';
-import type { LoadBalancerConfig } from './utils';
-import { resolveLoadBalancerConfig } from './lb_config_registry';
 import { prefixedOutputLogger } from '../endpoint/common/utils';
 
 import type { ProductType, Credentials, ProjectHandler } from './project_handler/project_handler';
@@ -424,16 +420,7 @@ ${JSON.stringify(cypressConfigFile, null, 2)}
         ? grepSpecPattern // use the returned concrete file paths
         : globby.sync(specPattern); // convert the glob pattern to concrete file paths
 
-      const shareStacks = process.env.CYPRESS_SHARE_STACKS === 'true';
-      const lbConfig: LoadBalancerConfig | undefined = resolveLoadBalancerConfig();
-
-      let files: string[];
-      if (shareStacks && lbConfig) {
-        files = retrieveIntegrationsConfigAware(concreteFilePaths, lbConfig);
-      } else {
-        const orderedFilePaths = orderSpecFilesForLoadBalance(concreteFilePaths, lbConfig);
-        files = retrieveIntegrations(orderedFilePaths, lbConfig);
-      }
+      const files = retrieveIntegrations(concreteFilePaths);
 
       log.info('Resolved spec files after retrieveIntegrations:', files);
 
