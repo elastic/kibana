5b1c7b0f4d0c2ec23a047d9f75da35993afbfe2b
Valerio Bartolini <vabarjs@gmail.com>
2026-03-12 16:38:36 +0100

    enable subqueries

--- src/platform/packages/shared/kbn-esql-language/src/commands/registry/from/index.ts
+++ src/platform/packages/shared/kbn-esql-language/src/commands/registry/from/index.ts
@@ -26,7 +26,7 @@ export const fromCommand = {
   methods: fromCommandMethods,
   metadata: {
     type: 'source' as const,
-    subquerySupport: process.env.NODE_ENV === 'test' ? true : false, // Temporary until making it Preview
+    subquerySupport: true,
     viewsSupport: process.env.NODE_ENV === 'test' ? true : false, // Temporary until making it Preview
     description: i18n.translate('kbn-esql-language.esql.definitions.fromDoc', {
       defaultMessage:
