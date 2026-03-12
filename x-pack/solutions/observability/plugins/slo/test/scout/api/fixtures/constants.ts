21b3208259613bb0224aade9f428edea140f83eb
Panagiota Mitsopoulou <panagiota.mitsopoulou@elastic.co>
2026-03-12 19:08:15 +0200

    [Scout] Api integration tests for SLO embeddables (#256994)
    Fixes https://github.com/elastic/kibana/issues/256993

---
+++ x-pack/solutions/observability/plugins/slo/test/scout/api/fixtures/constants.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+/** The base API path for dashboard endpoints (no leading slash for apiClient). */
+export const DASHBOARD_API_PATH = 'api/dashboards';
+
+/** Common headers for Dashboard API requests (internal API version 1) */
+export const COMMON_HEADERS = {
+  'kbn-xsrf': 'some-xsrf-token',
+  'x-elastic-internal-origin': 'kibana',
+  'elastic-api-version': '1',
+} as const;
