086ed582451ff91e84b39b01331e41d3566880be
Dominique Belcher <dominique.clarke@elastic.co>
2026-03-13 09:10:47 -0400

    add api integration tests

--- x-pack/platform/test/api_integration_deployment_agnostic/apis/alerting_v2/rule/index.ts
+++ x-pack/platform/test/api_integration_deployment_agnostic/apis/alerting_v2/rule/index.ts
@@ -8,11 +8,13 @@
 import type { DeploymentAgnosticFtrProviderContext } from '../../../ftr_provider_context';
 
 export default function ({ loadTestFile }: DeploymentAgnosticFtrProviderContext) {
-  describe('rule', () => {
+  describe('rule_v2', () => {
     loadTestFile(require.resolve('./create_rule'));
     loadTestFile(require.resolve('./get_rule'));
     loadTestFile(require.resolve('./list_rules'));
     loadTestFile(require.resolve('./update_rule'));
     loadTestFile(require.resolve('./delete_rule'));
+    loadTestFile(require.resolve('./bulk_delete_rules'));
+    loadTestFile(require.resolve('./bulk_enable_disable_rules'));
   });
 }
