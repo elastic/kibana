7c11610512f79b164a20065d1e680d597cd60874
Bailey Cash <bailey.cash@elastic.co>
2026-03-13 13:53:32 -0400

    separate details chunks

--- x-pack/platform/plugins/shared/alerting_v2/public/components/rule_details/rule_detail_page.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/components/rule_details/rule_detail_page.tsx
@@ -5,21 +5,9 @@
  * 2.0.
  */
 
-import {
-  EuiButtonEmpty,
-  EuiCodeBlock,
-  EuiDescriptionList,
-  EuiHorizontalRule,
-  EuiPageHeader,
-  EuiSpacer,
-  EuiTitle,
-} from '@elastic/eui';
-import { formatDuration } from '@kbn/alerting-plugin/common';
-import { CoreStart, useService } from '@kbn/core-di-browser';
-import { getIndexPatternFromESQLQuery } from '@kbn/esql-utils';
+import { EuiButtonEmpty, EuiHorizontalRule, EuiPageHeader, EuiSpacer } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
-import moment from 'moment';
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import type { RuleApiResponse } from '../../services/rules_api';
@@ -28,45 +16,19 @@ import { useBreadcrumbs } from '../../hooks/use_breadcrumbs';
 import { useDeleteRule } from '../../hooks/use_delete_rule';
 import { DeleteConfirmationModal } from '../rule/modals/delete_confirmation_modal';
 import { RuleHeaderDescription, RuleTitleWithBadges } from './rule_header_description';
-import { ItemValueRuleSummary } from './item_value_rule_summary';
-import { RecoveryPolicy } from './recovery_policy';
-import { EMPTY_VALUE, formatAlertDelay } from './utils';
+import { RuleConditions } from './rule_conditions';
+import { RuleMetadata } from './rule_metadata';
 
 export interface RuleDetailPageProps {
   rule: RuleApiResponse;
 }
 
-const MODE_LABELS: Record<string, string> = {
-  signal: i18n.translate('xpack.alertingV2.ruleDetails.modeSignal', {
-    defaultMessage: 'Detect only',
-  }),
-  alert: i18n.translate('xpack.alertingV2.ruleDetails.modeAlert', {
-    defaultMessage: 'Alert',
-  }),
-};
-
-const NO_DATA_BEHAVIOR_LABELS: Record<string, string> = {
-  no_data: i18n.translate('xpack.alertingV2.ruleDetails.noDataBehaviorNoData', {
-    defaultMessage: 'No data',
-  }),
-  last_status: i18n.translate('xpack.alertingV2.ruleDetails.noDataBehaviorLastStatus', {
-    defaultMessage: 'Keep last status',
-  }),
-  recover: i18n.translate('xpack.alertingV2.ruleDetails.noDataBehaviorRecover', {
-    defaultMessage: 'Recover',
-  }),
-};
-
 export const RuleDetailPage: React.FunctionComponent<RuleDetailPageProps> = ({ rule }) => {
   useBreadcrumbs('rule_details', { ruleName: rule.metadata?.name });
 
   const history = useHistory();
-  const uiSettings = useService(CoreStart('uiSettings'));
   const { mutate: deleteRule, isLoading: isDeleting } = useDeleteRule();
   const [showDeleteConfirmation, setShowDeleteConfirmation] = React.useState(false);
-
-  const dateFormat = uiSettings.get('dateFormat');
-  const formatDate = (value: string) => moment(value).format(dateFormat);
 
   const showDeleteConfirmationModal = () => {
     setShowDeleteConfirmation(true);
@@ -80,145 +42,6 @@ export const RuleDetailPage: React.FunctionComponent<RuleDetailPageProps> = ({ r
       },
     });
   };
-
-  const isAlertMode = rule.kind === 'alert';
-
-  const dataSource = getIndexPatternFromESQLQuery(rule.evaluation?.query?.base) || EMPTY_VALUE;
-
-  const conditionItems = [
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.dataSource', {
-        defaultMessage: 'Data source',
-      }),
-      description: (
-        <ItemValueRuleSummary
-          data-test-subj="alertingV2RuleDetailsDataSource"
-          itemValue={dataSource}
-        />
-      ),
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.groupKey', {
-        defaultMessage: 'Group key',
-      }),
-      description: (
-        <ItemValueRuleSummary
-          data-test-subj="alertingV2RuleDetailsGroupBy"
-          itemValue={rule.grouping?.fields?.length ? rule.grouping.fields.join(', ') : EMPTY_VALUE}
-        />
-      ),
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.timeField', {
-        defaultMessage: 'Time field',
-      }),
-      description: (
-        <ItemValueRuleSummary
-          data-test-subj="alertingV2RuleDetailsTimeField"
-          itemValue={rule.time_field ?? EMPTY_VALUE}
-        />
-      ),
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.schedule', {
-        defaultMessage: 'Schedule',
-      }),
-      description: (
-        <ItemValueRuleSummary
-          data-test-subj="alertingV2RuleDetailsSchedule"
-          itemValue={i18n.translate('xpack.alertingV2.ruleDetails.scheduleValue', {
-            defaultMessage: 'Every {interval}',
-            values: { interval: formatDuration(rule.schedule.every) },
-          })}
-        />
-      ),
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.lookback', {
-        defaultMessage: 'Lookback',
-      }),
-      description: (
-        <ItemValueRuleSummary
-          data-test-subj="alertingV2RuleDetailsLookback"
-          itemValue={rule.schedule.lookback ? formatDuration(rule.schedule.lookback) : EMPTY_VALUE}
-        />
-      ),
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.mode', {
-        defaultMessage: 'Mode',
-      }),
-      description: (
-        <ItemValueRuleSummary
-          data-test-subj="alertingV2RuleDetailsMode"
-          itemValue={MODE_LABELS[rule.kind] ?? rule.kind}
-        />
-      ),
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.recovery', {
-        defaultMessage: 'Recovery',
-      }),
-      description: <RecoveryPolicy recoveryPolicy={rule.recovery_policy} />,
-    },
-    ...(isAlertMode
-      ? [
-          {
-            title: i18n.translate('xpack.alertingV2.ruleDetails.alertDelay', {
-              defaultMessage: 'Alert delay',
-            }),
-            description: (
-              <ItemValueRuleSummary
-                data-test-subj="alertingV2RuleDetailsAlertDelay"
-                itemValue={formatAlertDelay(rule.state_transition)}
-              />
-            ),
-          },
-        ]
-      : []),
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.noDataConfig', {
-        defaultMessage: 'No data config',
-      }),
-      description: (
-        <ItemValueRuleSummary
-          data-test-subj="alertingV2RuleDetailsNoDataConfig"
-          itemValue={
-            rule.no_data?.behavior
-              ? NO_DATA_BEHAVIOR_LABELS[rule.no_data.behavior] ?? rule.no_data.behavior
-              : EMPTY_VALUE
-          }
-        />
-      ),
-    },
-  ];
-
-  const metadataItems = [
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.createdBy', {
-        defaultMessage: 'Created by',
-      }),
-      description: rule.createdBy ?? EMPTY_VALUE,
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.createdDate', {
-        defaultMessage: 'Created date',
-      }),
-      description: formatDate(rule.createdAt),
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.lastUpdate', {
-        defaultMessage: 'Last update',
-      }),
-      description: formatDate(rule.updatedAt),
-    },
-    {
-      title: i18n.translate('xpack.alertingV2.ruleDetails.updatedBy', {
-        defaultMessage: 'Updated by',
-      }),
-      description: rule.updatedBy ?? EMPTY_VALUE,
-    },
-  ];
 
   return (
     <>
@@ -255,83 +78,11 @@ export const RuleDetailPage: React.FunctionComponent<RuleDetailPageProps> = ({ r
 
       <EuiSpacer size="l" />
 
-      <EuiTitle size="s">
-        <h2>
-          {i18n.translate('xpack.alertingV2.ruleDetails.ruleConditions', {
-            defaultMessage: 'Rule conditions',
-          })}
-        </h2>
-      </EuiTitle>
-
-      <EuiSpacer size="m" />
-
-      <EuiTitle size="xxs">
-        <h3>
-          {i18n.translate('xpack.alertingV2.ruleDetails.baseQuery', {
-            defaultMessage: 'Base query',
-          })}
-        </h3>
-      </EuiTitle>
-      <EuiSpacer size="s" />
-      <EuiCodeBlock
-        language="esql"
-        isCopyable
-        overflowHeight={360}
-        paddingSize="m"
-        data-test-subj="alertingV2RuleDetailsBaseQuery"
-      >
-        {rule.evaluation?.query?.base || EMPTY_VALUE}
-      </EuiCodeBlock>
-
-      {isAlertMode && rule.evaluation?.query?.condition && (
-        <>
-          <EuiSpacer size="m" />
-          <EuiTitle size="xxs">
-            <h3>
-              {i18n.translate('xpack.alertingV2.ruleDetails.alertCondition', {
-                defaultMessage: 'Alert condition',
-              })}
-            </h3>
-          </EuiTitle>
-          <EuiSpacer size="s" />
-          <EuiCodeBlock
-            language="esql"
-            isCopyable
-            overflowHeight={200}
-            paddingSize="m"
-            data-test-subj="alertingV2RuleDetailsAlertCondition"
-          >
-            {rule.evaluation.query.condition}
-          </EuiCodeBlock>
-        </>
-      )}
-
-      <EuiSpacer size="l" />
-
-      <EuiDescriptionList
-        compressed
-        type="column"
-        listItems={conditionItems}
-        css={{ maxWidth: 600 }}
-      />
+      <RuleConditions rule={rule} />
 
       <EuiHorizontalRule margin="l" />
 
-      <EuiTitle size="s">
-        <h2>
-          {i18n.translate('xpack.alertingV2.ruleDetails.metadata', {
-            defaultMessage: 'Metadata',
-          })}
-        </h2>
-      </EuiTitle>
-      <EuiSpacer size="m" />
-
-      <EuiDescriptionList
-        compressed
-        type="column"
-        listItems={metadataItems}
-        css={{ maxWidth: 600 }}
-      />
+      <RuleMetadata rule={rule} />
 
       <EuiSpacer size="l" />
       {showDeleteConfirmation && (
