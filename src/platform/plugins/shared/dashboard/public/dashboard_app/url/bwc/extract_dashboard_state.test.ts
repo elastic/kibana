a53874ee5126e075e1be2f2b0ef46ab70a42754e
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2026-03-13 17:19:24 +0100

    skip failing test suite (#257722)

--- src/platform/plugins/shared/dashboard/public/dashboard_app/url/bwc/extract_dashboard_state.test.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_app/url/bwc/extract_dashboard_state.test.ts
@@ -11,7 +11,8 @@ import { omit } from 'lodash';
 import { DEFAULT_DASHBOARD_STATE } from '../../../dashboard_api/default_dashboard_state';
 import { extractDashboardState } from './extract_dashboard_state';
 
-describe('extractDashboardState', () => {
+// Failing: See https://github.com/elastic/kibana/issues/257722
+describe.skip('extractDashboardState', () => {
   test('should extract all DashboardState fields', () => {
     const optionalState = {
       time_range: {
