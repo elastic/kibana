3ff1d519ed755e15946eb9e8d1041ea3ff284467
Vitalii Dmyterko <92328789+vitaliidm@users.noreply.github.com>
2026-03-13 12:29:13 +0000

    [Security Solution][Detection Engine] cypress

--- x-pack/solutions/security/test/security_solution_cypress/cypress/screens/agent_builder.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/screens/agent_builder.ts
@@ -7,8 +7,16 @@
 
 export const NEW_AGENT_BUILDER_ATTACHMENT_BUTTON = '[data-test-subj="newAgentBuilderAttachment"]';
 
-export const AGENT_BUILDER_CONVERSATION_FLYOUT_WRAPPER =
-  '[data-test-subj="agent-builder-conversation-flyout-wrapper"]';
+export const AGENT_BUILDER_SIDEBAR_PANEL = '[data-test-subj="sidebarPanel"]';
 
 export const AGENT_BUILDER_CONVERSATION_INPUT_EDITOR =
   '[data-test-subj="agentBuilderConversationInputEditor"]';
+
+export const CREATE_RULE_BUTTON = '[data-test-subj="create-rule-button"]';
+
+export const CREATE_RULE_CONTEXT_MENU_POPOVER =
+  '[data-test-subj="create-rule-context-menu-popover"]';
+
+export const AI_RULE_CREATION_MENU_ITEM = '[data-test-subj="ai-rule-creation"]';
+
+export const MANUAL_RULE_CREATION_MENU_ITEM = '[data-test-subj="manual-rule-creation"]';
