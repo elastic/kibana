61958a8fa57bf8b993cba35ae9f3613146b03fe7
Yan Savitski <yan.savitski@elastic.co>
2026-03-13 16:17:54 +0100

    Add Model Settings management app registration and routing

--- x-pack/platform/plugins/shared/search_inference_endpoints/common/constants.ts
+++ x-pack/platform/plugins/shared/search_inference_endpoints/common/constants.ts
@@ -29,6 +29,7 @@ export const MODEL_SETTINGS_SECTION_TITLE = i18n.translate(
 
 export const INFERENCE_ENDPOINTS_QUERY_KEY = 'inferenceEndpointsQueryKey';
 export const TRAINED_MODEL_STATS_QUERY_KEY = 'trainedModelStats';
+export const INFERENCE_SETTINGS_QUERY_KEY = 'inferenceSettingsQueryKey';
 export const MODEL_SETTINGS_FEATURE_FLAG_ID = 'searchInferenceEndpoints:modelSettingsEnabled';
 
 export const INFERENCE_SETTINGS_SO_TYPE = 'inference-settings';
