1726097c0c5261d0e0e9ebc6838ae35002676654
Ryland Herrick <ryalnd@gmail.com>
2026-03-13 11:18:30 -0500

    Reuse existing NoPrivileges component
    In our related component for use on the Alert Details flyout.
    
    This both simplifies code, and aligns styling/design.

--- x-pack/solutions/security/plugins/security_solution/public/flyout/shared/components/flyout_missing_alerts_privilege.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/shared/components/flyout_missing_alerts_privilege.tsx
@@ -6,51 +6,29 @@
  */
 
 import React from 'react';
-import { EuiEmptyPrompt, EuiFlexItem, EuiLink } from '@elastic/eui';
-import { FormattedMessage } from '@kbn/i18n-react';
-import { useKibana } from '../../../common/lib/kibana';
+
+import type { DocLinks } from '@kbn/doc-links';
+import { i18n } from '@kbn/i18n';
 import { FLYOUT_MISSING_ALERTS_PRIVILEGE_TEST_ID } from './test_ids';
+import { NoPrivileges } from '../../../common/components/no_privileges';
+
+const alertDetailsPageName = i18n.translate(
+  'xpack.securitySolution.flyout.shared.alertDetailsPageName',
+  {
+    defaultMessage: 'Alert details',
+  }
+);
+const docLinkSelector = (links: DocLinks) => links.siem.detectionsReq;
 
 /**
  * Shown in the alert flyout when the user lacks the Alerts feature (securitySolutionAlertsV1) read privilege
  */
-export const FlyoutMissingAlertsPrivilege: React.VFC = () => {
-  const { docLinks } = useKibana().services;
-  const detectionsPrivilegesUrl = docLinks.links.siem.detectionsReq;
-
+export const FlyoutMissingAlertsPrivilege: React.FC = () => {
   return (
-    <EuiFlexItem>
-      <EuiEmptyPrompt
-        iconType="lock"
-        color="subdued"
-        title={
-          <h2>
-            <FormattedMessage
-              id="xpack.securitySolution.flyout.shared.missingAlertsPrivilegeTitle"
-              defaultMessage="Privileges required"
-            />
-          </h2>
-        }
-        body={
-          <p>
-            <FormattedMessage
-              id="xpack.securitySolution.flyout.shared.missingAlertsPrivilegeDescription"
-              defaultMessage="To view alert details, you need the Alerts feature read privilege. Contact your Kibana administrator to request access."
-            />
-          </p>
-        }
-        actions={
-          <EuiLink href={detectionsPrivilegesUrl} target="_blank" external>
-            <FormattedMessage
-              id="xpack.securitySolution.flyout.shared.missingAlertsPrivilegeDocLink"
-              defaultMessage="View documentation"
-            />
-          </EuiLink>
-        }
-        data-test-subj={FLYOUT_MISSING_ALERTS_PRIVILEGE_TEST_ID}
-      />
-    </EuiFlexItem>
+    <NoPrivileges
+      pageName={alertDetailsPageName}
+      docLinkSelector={docLinkSelector}
+      data-test-subj={FLYOUT_MISSING_ALERTS_PRIVILEGE_TEST_ID}
+    />
   );
 };
-
-FlyoutMissingAlertsPrivilege.displayName = 'FlyoutMissingAlertsPrivilege';
