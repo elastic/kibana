254e6a404c332215e0322d148462ecb2b2ef4fe3
Chris Martin <chris.martin@elastic.co>
2026-03-16 13:56:54 +0000

    plugins service

--- x-pack/platform/plugins/shared/agent_builder/public/plugin.tsx
+++ x-pack/platform/plugins/shared/agent_builder/public/plugin.tsx
@@ -209,6 +209,7 @@ export class AgentBuilderPlugin
       navigationService,
       toolsService,
       skillsService,
+      pluginsService,
       startDependencies,
       accessChecker,
       eventsService,
