db682662171955d372c7d7fbcf66938bd8bc6367
animehart <rickyangwyn@gmail.com>
2026-03-16 11:35:58 -0700

    small fix

--- x-pack/solutions/security/plugins/security_solution/public/siem_readiness/pages/components/integrations_selectable_popover.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/siem_readiness/pages/components/integrations_selectable_popover.tsx
@@ -54,7 +54,7 @@ export const IntegrationSelectablePopover = (props: IntegrationSelectablePopover
   const renderOption = (option: EuiSelectableOption) => {
     const statusInfo = statusMap?.get(option.key as string);
     return (
-      <EuiFlexGroup gutterSize="xs" alignItems="center" responsive={false}>
+      <EuiFlexGroup gutterSize="s" alignItems="center" responsive={false}>
         {statusInfo && (
           <EuiFlexItem grow={false}>
             <EuiToolTip content={statusInfo.tooltip}>
