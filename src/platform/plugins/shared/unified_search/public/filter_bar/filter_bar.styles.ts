5e9bfa29f4a8060f23b9dbc4ccaf9bb4f4a39934
Zacqary Xeper <zacqary.xeper@elastic.co>
2026-03-16 18:44:26 -0500

    Update collapsible filter bar design to address feedback

--- src/platform/plugins/shared/unified_search/public/filter_bar/filter_bar.styles.ts
+++ src/platform/plugins/shared/unified_search/public/filter_bar/filter_bar.styles.ts
@@ -12,15 +12,6 @@ import { css } from '@emotion/react';
 
 export const filterBarStyles = ({ euiTheme }: UseEuiTheme, afterQueryBar?: boolean) => {
   return {
-    filterBarWrapperCollaped: css`
-      margin-bottom: -${euiTheme.size.s};
-      padding: ${euiTheme.size.xs} 0;
-    `,
-    filterBarWrapperExpanded: css`
-      & .euiButtonIcon {
-        margin-top: ${euiTheme.size.m};
-      }
-    `,
     filterPillGroup: css`
       gap: ${euiTheme.size.xs};
       max-height: calc(${euiTheme.size.base} * 10);
@@ -34,18 +25,10 @@ export const filterBarStyles = ({ euiTheme }: UseEuiTheme, afterQueryBar?: boole
     pillsScrollContainer: css`
       max-height: 150px;
       overflow-y: auto;
+      transition: max-height 200ms ease;
     `,
-    /** These two classes reduce horizontal whitespace between the collapse/expand button and the filter bar
-     * content while preserving the minimum a11y-mandated click target size of 24x24px
-     */
-    filterBarCollapseExpandButton: css`
-      position: relative;
-      left: -${euiTheme.size.xs};
-    `,
-    filterBarContent: css`
-      position: relative;
-      min-width: 0;
-      left: -${euiTheme.size.s};
+    filterBarContentCollapsed: css`
+      max-height: 0;
     `,
   };
 };
