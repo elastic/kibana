b7ae79e1026f1f52b0ecf36319caa1116c404bb0
Sergi Massaneda <sergi.massaneda@elastic.co>
2026-03-16 17:22:19 +0100

    extract validation callout

---
+++ src/platform/plugins/shared/workflows_management/public/features/run_workflow/ui/input_validation_callout.tsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { EuiCallOut } from '@elastic/eui';
+import React from 'react';
+import { i18n } from '@kbn/i18n';
+
+export interface InputValidationCalloutProps {
+  errors: string;
+}
+
+const TITLE = i18n.translate('workflows.inputValidationCallout.title', {
+  defaultMessage: 'Input data is not valid',
+});
+
+export const InputValidationCallout: React.FC<InputValidationCalloutProps> = ({ errors }) => (
+  <EuiCallOut announceOnMount color="danger" iconType="help" size="s" title={TITLE}>
+    <p>{errors}</p>
+  </EuiCallOut>
+);
