d824f0f00a010ab00b2672a587882114d182d6cd
Konrad Szwarc <konrad.szwarc@elastic.co>
2026-03-16 13:06:54 -0600

    Reorder history table columns to match updated mocks

--- x-pack/platform/plugins/shared/osquery/public/actions/unified_history_table.tsx
+++ x-pack/platform/plugins/shared/osquery/public/actions/unified_history_table.tsx
@@ -413,6 +413,21 @@ const UnifiedHistoryTableComponent = () => {
   const columns = useMemo(
     () => [
       {
+        name: i18n.translate('xpack.osquery.liveQueryActions.table.actionsColumnTitle', {
+          defaultMessage: 'Actions',
+        }),
+        width: '120px',
+        actions: [
+          {
+            available: isPlayButtonAvailable,
+            render: renderPlayButton,
+          },
+          {
+            render: renderDetailsAction,
+          },
+        ],
+      },
+      {
         field: 'queryText',
         name: i18n.translate('xpack.osquery.liveQueryActions.table.queryColumnTitle', {
           defaultMessage: 'Query',
@@ -422,20 +437,20 @@ const UnifiedHistoryTableComponent = () => {
         render: renderQueryColumn,
       },
       {
-        field: 'totalRows',
-        name: i18n.translate('xpack.osquery.liveQueryActions.table.resultsColumnTitle', {
-          defaultMessage: 'Results',
-        }),
-        width: '120px',
-        render: renderResultsColumn,
-      },
-      {
         field: 'tags',
         name: i18n.translate('xpack.osquery.liveQueryActions.table.tagsColumnTitle', {
           defaultMessage: 'Tags',
         }),
         width: '100px',
         render: renderTagsColumn,
+      },
+      {
+        field: 'totalRows',
+        name: i18n.translate('xpack.osquery.liveQueryActions.table.resultsColumnTitle', {
+          defaultMessage: 'Results',
+        }),
+        width: '120px',
+        render: renderResultsColumn,
       },
       {
         field: 'source',
@@ -468,21 +483,6 @@ const UnifiedHistoryTableComponent = () => {
         }),
         width: '200px',
         render: renderRunByColumn,
-      },
-      {
-        name: i18n.translate('xpack.osquery.liveQueryActions.table.actionsColumnTitle', {
-          defaultMessage: 'Actions',
-        }),
-        width: '120px',
-        actions: [
-          {
-            available: isPlayButtonAvailable,
-            render: renderPlayButton,
-          },
-          {
-            render: renderDetailsAction,
-          },
-        ],
       },
     ],
     [
