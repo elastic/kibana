416f353a8f6fcf1f123c8b2bef43d0649a6e69ad
Søren Louv-Jansen <soren.louv@elastic.co>
2026-03-17 17:52:43 +0100

    Revert change to exclude skills

--- x-pack/solutions/observability/plugins/observability_agent_builder/server/agent/register_observability_agent.ts
+++ x-pack/solutions/observability/plugins/observability_agent_builder/server/agent/register_observability_agent.ts
@@ -54,8 +54,6 @@ export async function registerObservabilityAgent({
         ${getEntityLinkingInstructions({ urlPrefix })}
       `),
         tools: [{ tool_ids: OBSERVABILITY_AGENT_TOOL_IDS }],
-        skill_ids: ['data-exploration', 'visualization-creation', 'dashboard-management'],
-        enable_elastic_capabilities: false,
       };
     },
   });
