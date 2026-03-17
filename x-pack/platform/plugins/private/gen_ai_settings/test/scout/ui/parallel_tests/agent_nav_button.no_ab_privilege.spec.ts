d99646bd42e94a99a4ae5488ab9b84cbbcef3a44
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2026-03-17 18:53:55 +0100

    Update agent_nav_button.no_ab_privilege.spec.ts

--- x-pack/platform/plugins/private/gen_ai_settings/test/scout/ui/parallel_tests/agent_nav_button.no_ab_privilege.spec.ts
+++ x-pack/platform/plugins/private/gen_ai_settings/test/scout/ui/parallel_tests/agent_nav_button.no_ab_privilege.spec.ts
@@ -13,7 +13,7 @@ import { spaceTest } from '../fixtures';
 
 spaceTest.describe(
   'GenAI Settings - Agent Nav Button without Agent Builder Privileges',
-  { tag: [...tags.stateful.classic, ...tags.serverless.observability.complete, ...tags.serverless.security.complete, ...tags.serverless.search] },
+  { tag: [...tags.deploymentAgnostic] },
   () => {
     spaceTest.beforeEach(async ({ browserAuth, pageObjects, scoutSpace }) => {
       await scoutSpace.uiSettings.set({ [AI_CHAT_EXPERIENCE_TYPE]: AIChatExperience.Agent });
