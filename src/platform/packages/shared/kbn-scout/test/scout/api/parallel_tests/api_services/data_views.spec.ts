b905d8969fb1b8ed23537a5bdc180bf332f11c16
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-03-17 18:22:09 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/platform/packages/shared/kbn-scout/test/scout/api/parallel_tests/api_services/data_views.spec.ts
+++ src/platform/packages/shared/kbn-scout/test/scout/api/parallel_tests/api_services/data_views.spec.ts
@@ -7,8 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
+import { expect } from '@kbn/scout/api';
 import { apiTest, tags } from '../../../../../src/playwright';
-import { expect } from '../../../../../api';
 
 apiTest.describe(
   'Data Views API Service',
