ad298de97a088f020209319019fc14f02acb844a
Jonathan Budzenski <jon@elastic.co>
2026-03-17 12:13:49 -0500

    without side effects

--- src/platform/packages/shared/kbn-zod/v4/index.ts
+++ src/platform/packages/shared/kbn-zod/v4/index.ts
@@ -7,14 +7,5 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { globalRegistry } from 'zod/v4';
-const reg = globalRegistry as any;
-reg._map = new WeakMap();
-reg.clear = function () {
-  reg._map = new WeakMap();
-  reg._idmap = new Map();
-  return this;
-};
-
 export * from 'zod/v4';
 export { isZod } from './util';
