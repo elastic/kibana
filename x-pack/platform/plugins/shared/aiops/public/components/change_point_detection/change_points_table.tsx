59de23b407e96fe5d31d7a8de04a3b5ac02b624d
Melissa <melissa.alvarez@elastic.co>
2026-03-18 16:20:02 -0600

    add set widths for type and pvalue in chart

--- x-pack/platform/plugins/shared/aiops/public/components/change_point_detection/change_points_table.tsx
+++ x-pack/platform/plugins/shared/aiops/public/components/change_point_detection/change_points_table.tsx
@@ -174,6 +174,7 @@ export const ChangePointsTable: FC<ChangePointsTableProps> = ({
       sortable: true,
       truncateText: false,
       render: (type: ChangePointAnnotation['type']) => <EuiBadge color="hollow">{type}</EuiBadge>,
+      width: '150px',
     },
     {
       id: 'pValue',
@@ -192,6 +193,7 @@ export const ChangePointsTable: FC<ChangePointsTableProps> = ({
       truncateText: false,
       render: (pValue: ChangePointAnnotation['p_value']) =>
         pValue !== undefined ? pValue.toPrecision(3) : '-',
+      width: '130px',
     },
     ...(fieldConfig.splitField
       ? [
@@ -204,7 +206,6 @@ export const ChangePointsTable: FC<ChangePointsTableProps> = ({
             }),
             truncateText: false,
             sortable: true,
-            width: '15%',
           },
           ...(hasActions
             ? [
