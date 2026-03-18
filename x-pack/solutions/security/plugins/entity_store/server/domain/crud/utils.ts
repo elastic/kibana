d1df90ba19428fec0b6f607abbc7e79d0bfd3913
kubasobon <kuba.sobon@elastic.co>
2026-03-18 15:07:51 +0100

    todo: add todo for myself

--- x-pack/solutions/security/plugins/entity_store/server/domain/crud/utils.ts
+++ x-pack/solutions/security/plugins/entity_store/server/domain/crud/utils.ts
@@ -26,6 +26,7 @@ export function hashEuid(id: string): string {
   return createHash('md5').update(id).digest('hex');
 }
 
+// TODO: update description - ID and Identity
 // validateUpdateDoc checks provided and generated EUIDs according to rules
 // expected by updateEntity() method. updateEntity() and bulkUpdateEntity()
 // methods are the only ones that consume this validator.
