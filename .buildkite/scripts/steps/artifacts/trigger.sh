e9e0a30e6dd4980d6abd72aa1386ff772b5ac3f4
Tamerlan <tamerlan.gudabayev@elastic.co>
2026-03-20 11:59:06 +0100

    echo

--- .buildkite/scripts/steps/artifacts/trigger.sh
+++ .buildkite/scripts/steps/artifacts/trigger.sh
@@ -4,13 +4,22 @@ set -euo pipefail
 
 source .buildkite/scripts/steps/artifacts/env.sh
 
-BEATS_MANIFEST_LATEST_URL=$(curl  "$BEATS_MANIFEST_LATEST" | jq -r '.manifest_url')
-KIBANA_MANIFEST_URL=$(curl  "$KIBANA_MANIFEST_LATEST" | jq -r '.manifest_url')
-KIBANA_BEATS_MANIFEST_URL=$(curl $KIBANA_MANIFEST_URL | jq -r '.projects.kibana.dependencies[] | select(.prefix == "beats") | .build_uri')
 
-echo "--- Trigger artifact builds"
-if [ "$BEATS_MANIFEST_LATEST_URL" = "$KIBANA_BEATS_MANIFEST_URL" ]; then
-  echo "Kibana has the latest version of beats, skipping trigger"
-else
-  ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-artifacts-staging "$BUILDKITE_BRANCH"
-fi
+echo "BEATS URL: $BEATS_MANIFEST_LATEST"
+curl -sv "$BEATS_MANIFEST_LATEST" 2>&1 | head -50
+
+# BEATS_MANIFEST_LATEST_URL=$(curl  "$BEATS_MANIFEST_LATEST" | jq -r '.manifest_url')
+# KIBANA_MANIFEST_URL=$(curl  "$KIBANA_MANIFEST_LATEST" | jq -r '.manifest_url')
+# KIBANA_BEATS_MANIFEST_URL=$(curl $KIBANA_MANIFEST_URL | jq -r '.projects.kibana.dependencies[] | select(.prefix == "beats") | .build_uri')
+#
+#
+# echo $BEATS_MANIFEST_LATEST
+# echo $KIBANA_MANIFEST_URL
+# echo $KIBANA_BEATS_MANIFEST_URL
+
+# echo "--- Trigger artifact builds"
+# if [ "$BEATS_MANIFEST_LATEST_URL" = "$KIBANA_BEATS_MANIFEST_URL" ]; then
+#   echo "Kibana has the latest version of beats, skipping trigger"
+# else
+#   ts-node .buildkite/scripts/steps/trigger_pipeline.ts kibana-artifacts-staging "$BUILDKITE_BRANCH"
+# fi
