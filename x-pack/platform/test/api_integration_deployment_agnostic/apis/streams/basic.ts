3f7a876625d5ff3432e58881090b87835aedf7e9
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2026-03-20 13:09:42 +0100

    skip failing test suite (#258810)

--- x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/basic.ts
+++ x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/basic.ts
@@ -70,7 +70,8 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
     });
   }
 
-  describe('Basic functionality', () => {
+  // Failing: See https://github.com/elastic/kibana/issues/258810
+  describe.skip('Basic functionality', () => {
     async function getWiredStatus() {
       const response = await viewerApiClient.fetch('GET /api/streams/_status').expect(200);
       return response.body;
