97a3d4aacdb967b8df6eac879cb2bcc17a8ca603
Arturo Castillo Delgado <arturo.castillo@elastic.co>
2026-03-23 22:05:59 +0100

    Revert "[Tests] Use legacy time picker in FTR and Scout configs"
    This reverts commit 9bebf1d2e9d60a8956757d244ab6041176ed89b5.

--- src/platform/packages/shared/kbn-scout/src/servers/configs/config_sets/default/stateful/base.config.ts
+++ src/platform/packages/shared/kbn-scout/src/servers/configs/config_sets/default/stateful/base.config.ts
@@ -121,7 +121,6 @@ export const defaultConfig: ScoutServerConfig = {
       `--security.showInsecureClusterWarning=false`,
       '--telemetry.banner=false',
       '--telemetry.optIn=false',
-      '--uiSettings.overrides.timepicker:useLegacyTimePicker=true',
       // These are *very* important to have them pointing to staging
       '--telemetry.sendUsageTo=staging',
       `--server.maxPayload=1679958`,
--- src/platform/test/functional/config.base.js
+++ src/platform/test/functional/config.base.js
@@ -51,7 +51,6 @@ export default async function ({ readConfigFile }) {
       defaults: {
         'accessibility:disableAnimations': true,
         'dateFormat:tz': 'UTC',
-        'timepicker:useLegacyTimePicker': true,
       },
     },
 
