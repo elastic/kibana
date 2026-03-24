bf391e10b66e959716c3126d40c2962313d0afb2
Philippe Oberti <philippe.oberti@elastic.co>
2026-03-24 15:02:48 -0500

    [Security Solution] render correlations overview new document flyout in Security Solution and Discover (#258045)
    > [!WARNING]
    > While it seems that the PR does a lot, most of the changes are
    actually import changes. Only a handful of files have meaningful logic
    changes. Also, some of the changes in this PR are similar to those
    applied in this other
    [PR](https://github.com/elastic/kibana/pull/257781) and that other
    [one](https://github.com/elastic/kibana/pull/257784)
    
    ## Summary
    
    ### Code changes
    
    The PR focuses on the Correlations overview component, which is an item
    under the Insights section in the alert flyout. Here are the main
    changes introduced here:
    - add a new `Insights` section to the new flyout (`flyout_v2` folder)
    - move the `CorrelationsOverview` component to the `flyout_v2` folder.
    While moving the component, some refactor was performed:
    - ensure that the components do not get their data from the
    `useDocumentDetailsContext` anymore, as it isn't available in the new
    flyout experience. The data is instead passed down via props.
    - ensure that we use the `hit: DataTableRecord` as source of data to
    make the code reusable in Discover.
      - refactor move the related hooks to the `flyout_v2` folder
      - move the `InsightsSummaryRow` component to the `flyout_v2` folder
    
    
    > [!TIP]
    > CorrelationsOverview is used both in the expandable flyout (in
    Security Solution) and in this new EUI flyout system (in Security
    Solution and Discover). The UIUX experience in the expandable flyout
    remains totally unchanged.
    
    ### UI changes
    
    The UI of the current alert/event flyouts (using the expandable flyout
    framework) in Security Solution should remain unchanged after this PR
    (when the feature flag is off).
    
    
    https://github.com/user-attachments/assets/6c609444-e678-4eeb-8510-5081f188e281
    
    Then when the feature flag is on, the new flyout show the prevalence
    overview:
    
    
    https://github.com/user-attachments/assets/e733f233-4f3f-408c-addb-c2b2b676f049
    
    
    > [!NOTE]
    > No click interaction yet, this will be implemented in the follow up PR
    that will move the Correlations Details component in a new tools flyout.
    
    The UI of the current document flyout in Discover should remain
    unchanged after this PR (when the experimental profile off).
    
    ## How to test
    
    To see the new (emtpy) flyout in Security Solution, add this to your
    `kibana.dev.yml` file:
    ```xpack.securitySolution.enableExperimental: [ 'newFlyoutSystemEnabled' ]```
    
    Too see the new (emtpy) flyout in Discover, add this to your `kibana.dev.yml` file:
    ```discover.experimental.enabledProfiles: [
    'enhanced-security-document-profile' ]```
    
    ## What to look for when testing
    
    - verify that the Insights section on the Security Solution side has not
    changed in the expandable flyout (`newFlyoutSystemEnabled` feature flag
    off)
    - verify that threat correlations overview in the new flyout
    (`newFlyoutSystemEnabled` feature flag on)
    - verify that correlations overview shows up in Discover
    
    ### Checklist
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [x] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [x] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    https://github.com/elastic/kibana/issues/256915
    
    _PR developed with Cursor + gpt-5.2_

--- x-pack/solutions/security/plugins/security_solution/public/flyout/document_details/left/components/correlations_details.test.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/document_details/left/components/correlations_details.test.tsx
@@ -10,12 +10,12 @@ import { render } from '@testing-library/react';
 import { CorrelationsDetails } from './correlations_details';
 import { TestProviders } from '../../../../common/mock';
 import { DocumentDetailsContext } from '../../shared/context';
-import { useShowRelatedAlertsByAncestry } from '../../shared/hooks/use_show_related_alerts_by_ancestry';
-import { useShowRelatedAlertsBySameSourceEvent } from '../../shared/hooks/use_show_related_alerts_by_same_source_event';
-import { useShowRelatedAlertsBySession } from '../../shared/hooks/use_show_related_alerts_by_session';
-import { useShowRelatedAttacks } from '../../shared/hooks/use_show_related_attacks';
-import { useShowRelatedCases } from '../../shared/hooks/use_show_related_cases';
-import { useShowSuppressedAlerts } from '../../shared/hooks/use_show_suppressed_alerts';
+import { useShowRelatedAlertsByAncestry } from '../../../../flyout_v2/document/hooks/use_show_related_alerts_by_ancestry';
+import { useShowRelatedAlertsBySameSourceEvent } from '../../../../flyout_v2/document/hooks/use_show_related_alerts_by_same_source_event';
+import { useShowRelatedAlertsBySession } from '../../../../flyout_v2/document/hooks/use_show_related_alerts_by_session';
+import { useShowRelatedAttacks } from '../../../../flyout_v2/document/hooks/use_show_related_attacks';
+import { useShowRelatedCases } from '../../../../flyout_v2/document/hooks/use_show_related_cases';
+import { useShowSuppressedAlerts } from '../../../../flyout_v2/document/hooks/use_show_suppressed_alerts';
 import {
   CORRELATIONS_DETAILS_BY_ANCESTRY_SECTION_TABLE_TEST_ID,
   CORRELATIONS_DETAILS_RELATED_ATTACKS_SECTION_TABLE_TEST_ID,
@@ -24,10 +24,10 @@ import {
   CORRELATIONS_DETAILS_CASES_SECTION_TABLE_TEST_ID,
   CORRELATIONS_DETAILS_SUPPRESSED_ALERTS_SECTION_TEST_ID,
 } from './test_ids';
-import { useFetchRelatedAlertsBySession } from '../../shared/hooks/use_fetch_related_alerts_by_session';
-import { useFetchRelatedAlertsByAncestry } from '../../shared/hooks/use_fetch_related_alerts_by_ancestry';
-import { useFetchRelatedAlertsBySameSourceEvent } from '../../shared/hooks/use_fetch_related_alerts_by_same_source_event';
-import { useFetchRelatedCases } from '../../shared/hooks/use_fetch_related_cases';
+import { useFetchRelatedAlertsBySession } from '../../../../flyout_v2/document/hooks/use_fetch_related_alerts_by_session';
+import { useFetchRelatedAlertsByAncestry } from '../../../../flyout_v2/document/hooks/use_fetch_related_alerts_by_ancestry';
+import { useFetchRelatedAlertsBySameSourceEvent } from '../../../../flyout_v2/document/hooks/use_fetch_related_alerts_by_same_source_event';
+import { useFetchRelatedCases } from '../../../../flyout_v2/document/hooks/use_fetch_related_cases';
 import { mockContextValue } from '../../shared/mocks/mock_context';
 import { EXPANDABLE_PANEL_HEADER_TITLE_TEXT_TEST_ID } from '../../../../flyout_v2/shared/components/test_ids';
 import { useSecurityDefaultPatterns } from '../../../../data_view_manager/hooks/use_security_default_patterns';
@@ -37,16 +37,16 @@ jest.mock('react-router-dom', () => {
   const actual = jest.requireActual('react-router-dom');
   return { ...actual, useLocation: jest.fn().mockReturnValue({ pathname: '' }) };
 });
-jest.mock('../../shared/hooks/use_show_related_alerts_by_ancestry');
-jest.mock('../../shared/hooks/use_show_related_alerts_by_same_source_event');
-jest.mock('../../shared/hooks/use_show_related_alerts_by_session');
-jest.mock('../../shared/hooks/use_show_related_attacks');
-jest.mock('../../shared/hooks/use_show_related_cases');
-jest.mock('../../shared/hooks/use_show_suppressed_alerts');
-jest.mock('../../shared/hooks/use_fetch_related_alerts_by_session');
-jest.mock('../../shared/hooks/use_fetch_related_alerts_by_ancestry');
-jest.mock('../../shared/hooks/use_fetch_related_alerts_by_same_source_event');
-jest.mock('../../shared/hooks/use_fetch_related_cases');
+jest.mock('../../../../flyout_v2/document/hooks/use_show_related_alerts_by_ancestry');
+jest.mock('../../../../flyout_v2/document/hooks/use_show_related_alerts_by_same_source_event');
+jest.mock('../../../../flyout_v2/document/hooks/use_show_related_alerts_by_session');
+jest.mock('../../../../flyout_v2/document/hooks/use_show_related_attacks');
+jest.mock('../../../../flyout_v2/document/hooks/use_show_related_cases');
+jest.mock('../../../../flyout_v2/document/hooks/use_show_suppressed_alerts');
+jest.mock('../../../../flyout_v2/document/hooks/use_fetch_related_alerts_by_session');
+jest.mock('../../../../flyout_v2/document/hooks/use_fetch_related_alerts_by_ancestry');
+jest.mock('../../../../flyout_v2/document/hooks/use_fetch_related_alerts_by_same_source_event');
+jest.mock('../../../../flyout_v2/document/hooks/use_fetch_related_cases');
 jest.mock('../../../../data_view_manager/hooks/use_security_default_patterns');
 jest.mock('../../../../common/hooks/use_experimental_features');
 
@@ -78,7 +78,7 @@ describe('CorrelationsDetails', () => {
   it('renders all sections', () => {
     jest
       .mocked(useShowRelatedAlertsByAncestry)
-      .mockReturnValue({ show: true, documentId: 'event-id' });
+      .mockReturnValue({ show: true, ancestryDocumentId: 'event-id' });
     jest
       .mocked(useShowRelatedAlertsBySameSourceEvent)
       .mockReturnValue({ show: true, originalEventId: 'originalEventId' });
@@ -130,7 +130,7 @@ describe('CorrelationsDetails', () => {
   it('should render no section and show error message if show values are false', () => {
     jest
       .mocked(useShowRelatedAlertsByAncestry)
-      .mockReturnValue({ show: false, documentId: 'event-id' });
+      .mockReturnValue({ show: false, ancestryDocumentId: 'event-id' });
     jest
       .mocked(useShowRelatedAlertsBySameSourceEvent)
       .mockReturnValue({ show: false, originalEventId: 'originalEventId' });
