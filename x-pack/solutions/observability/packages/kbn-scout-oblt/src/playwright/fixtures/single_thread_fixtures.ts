81d48c1fda76716402306f812518d39c1aaba310
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2026-03-24 17:56:01 +0100

    clean deps in kbn/scout-* packages (#259342)
    ## Summary
    
    Follow-up to https://github.com/elastic/kibana/pull/258959
    
    - removing `@kbn/test` direct dependency from `@kbn/scout-oblt`
    - removing `@kbn/cloud-security-posture-common` and `@kbn/fleet-plugin`
    deps from `@kbn/scout-security`
    - with Scout on-code-change test execution these dependencies will
    trigger all Security Solution scout tests, even though not every plugin
    depends on them
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 21fe8ac6fc7e527dca554c8d67c7ac9526ec0897)
    
    # Conflicts:
    #	x-pack/solutions/observability/packages/kbn-scout-oblt/src/playwright/fixtures/parallel_run_fixtures.ts
    #	x-pack/solutions/observability/packages/kbn-scout-oblt/src/playwright/fixtures/single_thread_fixtures.ts
    #	x-pack/solutions/security/packages/kbn-scout-security/src/playwright/fixtures/test/page_objects/cspm_integration_page.ts

--- x-pack/solutions/observability/packages/kbn-scout-oblt/src/playwright/fixtures/single_thread_fixtures.ts
+++ x-pack/solutions/observability/packages/kbn-scout-oblt/src/playwright/fixtures/single_thread_fixtures.ts
@@ -27,15 +27,10 @@ const baseFixture = base.extend<ObltTestFixtures, ObltWorkerFixtures>({
     await use(extendedPageObjects);
   },
   apiServices: [
-    async (
-      { apiServices }: { apiServices: ApiServicesFixture },
-      use: (extendedApiServices: ObltApiServicesFixture) => Promise<void>
-    ) => {
-      const extendedApiServices = apiServices as ObltApiServicesFixture;
+    async ({ apiServices }, use) => {
       // extend with Observability specific API services
-      // extendedApiServices.<service_name> = getServiceApiHelper(kbnClient);
-
-      await use(extendedApiServices);
+      // apiServices.<service_name> = getServiceApiHelper(kbnClient);
+      await use(apiServices);
     },
     { scope: 'worker' },
   ],
