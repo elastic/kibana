bd40bd0db89972405f6cb58c4e573ac2099682ca
paulina.shakirova <paulina.shakirova@elastic.co>
2026-03-25 12:31:48 +0100

    remove kbn-ai-assistant from pr, search team will take care

--- x-pack/platform/packages/shared/kbn-ai-assistant/src/buttons/ask_assistant_button.tsx
+++ x-pack/platform/packages/shared/kbn-ai-assistant/src/buttons/ask_assistant_button.tsx
@@ -6,11 +6,40 @@
  */
 
 import React from 'react';
+import type { EuiButtonSize, EuiButtonEmptySizes } from '@elastic/eui';
+import { EuiButton, EuiButtonEmpty, EuiToolTip, EuiButtonIcon } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { EuiToolTip } from '@elastic/eui';
-import { AiButton, type AiButtonProps } from '@kbn/shared-ux-ai-components';
 
-export function AskAssistantButton({ size, variant, onClick, ...props }: AiButtonProps) {
+export type AskAssistantButtonProps = (
+  | {
+      variant: 'basic';
+      size: EuiButtonSize;
+      fill?: boolean;
+      flush?: false;
+    }
+  | {
+      variant: 'empty';
+      size: EuiButtonEmptySizes;
+      fill?: false;
+      flush?: 'both';
+    }
+  | {
+      variant: 'iconOnly';
+      size: EuiButtonSize;
+      fill?: boolean;
+      flush?: false;
+    }
+) & {
+  onClick: () => void;
+};
+
+export function AskAssistantButton({
+  fill,
+  flush,
+  size,
+  variant,
+  onClick,
+}: AskAssistantButtonProps) {
   const buttonLabel = i18n.translate('xpack.aiAssistant.askAssistantButton.buttonLabel', {
     defaultMessage: 'Ask Assistant',
   });
@@ -19,39 +48,52 @@ export function AskAssistantButton({ size, variant, onClick, ...props }: AiButto
     defaultMessage: 'AI Assistant',
   });
 
-  if (variant === 'base' || variant === 'accent') {
-    return (
-      <AiButton iconType="sparkles" size={size} variant={variant} onClick={onClick} {...props}>
-        {buttonLabel}
-      </AiButton>
-    );
-  }
-
-  if (variant === 'empty') {
-    return (
-      <AiButton iconType="sparkles" size={size} variant="empty" onClick={onClick} {...props}>
-        {buttonLabel}
-      </AiButton>
-    );
-  }
-
-  if (props.iconOnly) {
-    return (
-      <EuiToolTip
-        position="top"
-        title={aiAssistantLabel}
-        content={i18n.translate('xpack.aiAssistant.askAssistantButton.popoverContent', {
-          defaultMessage: 'Get insights into your data with the Elastic Assistant',
-        })}
-      >
-        <AiButton
-          {...props}
-          onClick={onClick}
-          iconOnly
+  switch (variant) {
+    case 'basic':
+      return (
+        <EuiButton
+          data-test-subj="observabilityAiAssistantAskAssistantButton"
+          fill={fill}
+          size={size}
           iconType="sparkles"
-          aria-label={aiAssistantLabel}
-        />
-      </EuiToolTip>
-    );
+          onClick={onClick}
+        >
+          {buttonLabel}
+        </EuiButton>
+      );
+
+    case 'empty':
+      return (
+        <EuiButtonEmpty
+          data-test-subj="observabilityAiAssistantAskAssistantButton"
+          size={size}
+          flush={flush}
+          iconType="sparkles"
+          onClick={onClick}
+        >
+          {buttonLabel}
+        </EuiButtonEmpty>
+      );
+
+    case 'iconOnly':
+      return (
+        <EuiToolTip
+          position="top"
+          title={aiAssistantLabel}
+          content={i18n.translate('xpack.aiAssistant.askAssistantButton.popoverContent', {
+            defaultMessage: 'Get insights into your data with the Elastic Assistant',
+          })}
+        >
+          <EuiButtonIcon
+            aria-label={aiAssistantLabel}
+            data-test-subj="observabilityAiAssistantAskAssistantButtonButtonIcon"
+            display={fill ? 'fill' : 'base'}
+            iconType="sparkles"
+            size={size}
+            style={{ minWidth: 'auto' }}
+            onClick={onClick}
+          />
+        </EuiToolTip>
+      );
   }
 }
