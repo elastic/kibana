787d5b056da6a9b7109dffd71dc20d97fca47ec1
James Gowdy <jgowdy@elastic.co>
2026-03-26 09:13:07 +0000

    removing odd autocorrect

--- x-pack/platform/test/api_integration/apis/ml/jobs/stop_datafeeds.ts
+++ x-pack/platform/test/api_integration/apis/ml/jobs/stop_datafeeds.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import expect from '@kbn/expect/expect';
+import expect from '@kbn/expect';
 import { JOB_STATE, DATAFEED_STATE } from '@kbn/ml-plugin/common/constants/states';
 import type { FtrProviderContext } from '../../../ftr_provider_context';
 import { USER } from '../../../services/ml/security_common';
