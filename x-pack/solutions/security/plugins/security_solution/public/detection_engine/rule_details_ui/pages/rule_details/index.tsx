7ac45d744874846027d058682c380c0828300a3c
Nikita Indik <nikita.indik@elastic.co>
2026-03-26 13:05:15 +0100

    Wire up the table with the real API

--- x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_details_ui/pages/rule_details/index.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_details_ui/pages/rule_details/index.tsx
@@ -936,7 +936,10 @@ export const RuleDetailsPage = connector(
                     >
                       <>
                         {newExecutionResultsTableEnabled ? (
-                          <ExecutionResultsPocTable ruleId={ruleId} />
+                          <ExecutionResultsPocTable
+                            ruleId={ruleId}
+                            onFilterByExecutionId={() => navigateToAlertsTab()}
+                          />
                         ) : (
                           <ExecutionLogTable
                             ruleId={ruleId}
