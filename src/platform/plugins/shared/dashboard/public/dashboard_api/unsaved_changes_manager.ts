32ec94de762dade475a77dd43e35d059693a5f9b
Devon Thomson <devon.thomson@elastic.co>
2026-03-26 14:30:04 -0400

    [Dashboards] Fix backups service (#257762)
    Fixes the backup service not working in non-default spaces by solving the underlying race condition.
    
    (cherry picked from commit 523f0637b94930dc02701221aeec844986f3ff87)
    
    # Conflicts:
    #	src/platform/plugins/shared/dashboard/public/dashboard_api/load_dashboard_api/load_dashboard_api.ts
    #	src/platform/plugins/shared/dashboard/public/dashboard_api/unsaved_changes_manager.test.ts
    #	src/platform/plugins/shared/dashboard/public/dashboard_api/unsaved_changes_manager.ts
    #	src/platform/plugins/shared/dashboard/public/dashboard_app/top_nav/share/share_options_utils.ts
    #	src/platform/plugins/shared/dashboard/public/dashboard_app/top_nav/share/show_share_modal.test.tsx
    #	src/platform/plugins/shared/dashboard/public/dashboard_renderer/dashboard_module.ts
    #	src/platform/plugins/shared/dashboard/public/services/kibana_services.ts

--- src/platform/plugins/shared/dashboard/public/dashboard_api/unsaved_changes_manager.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_api/unsaved_changes_manager.ts
@@ -28,6 +28,11 @@ import {
 } from 'rxjs';
 import type { DashboardBackupState } from '../services/dashboard_backup_service';
 import { getDashboardBackupService } from '../services/dashboard_backup_service';
+
+import { of } from 'rxjs';
+import type { DashboardState } from '../../common';
+import { type DashboardBackupState } from '../services/dashboard_backup_service';
+import { getDashboardBackupService } from '../services/dashboard_api_services';
 import type { initializeLayoutManager } from './layout_manager';
 import type { initializeSettingsManager } from './settings_manager';
 import type { DashboardState } from '../../common';
