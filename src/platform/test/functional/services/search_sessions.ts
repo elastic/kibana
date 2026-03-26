e3de37fce7a7212bf9e847e52727ff00b60ec4a7
AlexGPlay <alex.garcia@elastic.co>
2026-03-26 10:59:18 +0100

    Try existsOrFail

--- src/platform/test/functional/services/search_sessions.ts
+++ src/platform/test/functional/services/search_sessions.ts
@@ -83,12 +83,10 @@ export class SearchSessionsService extends FtrService {
   }
 
   public async openFlyout() {
-    await this.retry.waitFor('background search button is visible', async () => {
-      if (await this.testSubjects.exists(APP_MENU_OVERFLOW_BUTTON)) {
-        await this.testSubjects.click(APP_MENU_OVERFLOW_BUTTON);
-      }
-      return this.testSubjects.exists(BACKGROUND_SEARCH_FLYOUT_ENTRYPOINT);
-    });
+    if (await this.testSubjects.exists(APP_MENU_OVERFLOW_BUTTON)) {
+      await this.testSubjects.click(APP_MENU_OVERFLOW_BUTTON);
+    }
+    await this.testSubjects.existOrFail(BACKGROUND_SEARCH_FLYOUT_ENTRYPOINT);
     await this.testSubjects.click(BACKGROUND_SEARCH_FLYOUT_ENTRYPOINT);
     await this.expectManagementTable();
   }
