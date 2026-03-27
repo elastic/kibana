7385c2d89a69ce9028ca6ae668d9bb2d765faec8
Catherine Liu <catherine.liu@elastic.co>
2026-03-27 08:15:00 -0700

    Fix jest config

--- src/platform/packages/shared/as-code/shared-schemas/jest.config.js
+++ src/platform/packages/shared/as-code/shared-schemas/jest.config.js
@@ -10,5 +10,5 @@
 module.exports = {
   preset: '@kbn/test/jest_node',
   rootDir: '../../../..',
-  roots: ['<rootDir>/packages/shared/as-code/shared-schemas'],
+  roots: ['<rootDir>/src/platform/packages/shared/as-code/shared-schemas'],
 };
