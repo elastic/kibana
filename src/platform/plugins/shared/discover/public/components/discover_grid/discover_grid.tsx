8b8caebb93bee2e021dca10394e1045056aa9fa4
Eyo Okon Eyo <eyo.eyo@elastic.co>
2026-03-27 16:30:19 +0100

    ensure the first cascade group has at least one value

--- src/platform/plugins/shared/discover/public/components/discover_grid/discover_grid.tsx
+++ src/platform/plugins/shared/discover/public/components/discover_grid/discover_grid.tsx
@@ -98,7 +98,10 @@ export const DiscoverGrid: React.FC<DiscoverGridProps> = React.memo(
     });
 
     const isCascadedDocumentsAvailable =
-      props.isPlainRecord && !!cascadedDocumentsContext?.availableCascadeGroups.length;
+      props.isPlainRecord &&
+      !!cascadedDocumentsContext?.availableCascadeGroups.length &&
+      // the first group column should have at least one row with a value
+      props.rows?.some((row) => row.flattened[cascadedDocumentsContext.availableCascadeGroups[0]]);
 
     const externalAdditionalControls = useMemo(() => {
       const additionalControls: ReactNode[] = [];
