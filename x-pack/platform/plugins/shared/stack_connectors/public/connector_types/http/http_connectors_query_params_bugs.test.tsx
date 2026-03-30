41668e5ead931092ba3b94f2e6c1b0ba2f03665b
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-03-30 10:05:57 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/plugins/shared/stack_connectors/public/connector_types/http/http_connectors_query_params_bugs.test.tsx
+++ x-pack/platform/plugins/shared/stack_connectors/public/connector_types/http/http_connectors_query_params_bugs.test.tsx
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { render, screen, waitFor, within } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import HttpActionConnectorFields from './http_connectors';
 import { ConnectorFormTestProvider } from '../lib/test_utils';
