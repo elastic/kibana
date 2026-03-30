b1c14d00bdd58d02d0e7bf20a6014290f08b7620
PhilippeOberti <philippe.oberti@elastic.co>
2026-03-30 12:56:54 -0500

    [Discover] add optional custom footer to document flyout

--- src/platform/plugins/shared/discover/public/context_awareness/profile_providers/example/example_data_source_profile/profile.tsx
+++ src/platform/plugins/shared/discover/public/context_awareness/profile_providers/example/example_data_source_profile/profile.tsx
@@ -16,7 +16,7 @@ import React from 'react';
 import type { DataSourceProfileProvider } from '../../../profiles';
 import { DataSourceCategory } from '../../../profiles';
 import { extractIndexPatternFrom } from '../../extract_index_pattern_from';
-import { ChartWithCustomButtons, CustomDocViewerHeader } from './components';
+import { ChartWithCustomButtons, CustomDocViewerFooter, CustomDocViewerHeader } from './components';
 import { CustomDocView } from './components/custom_doc_view';
 import { RestorableStateDocView } from './components/restorable_state_doc_view';
 
@@ -94,6 +94,7 @@ export const createExampleDataSourceProfileProvider = (): DataSourceProfileProvi
             return prevValue.docViewsRegistry(registry);
           },
           renderHeader: (props) => <CustomDocViewerHeader {...props} />,
+          renderFooter: (props) => <CustomDocViewerFooter {...props} />,
         };
       },
     /**
