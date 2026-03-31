3e1e051ad5b86495285d5352b82084c83fc231da
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-03-31 14:53:13 +0000

    Changes from node scripts/lint.js --fix

--- x-pack/platform/plugins/shared/lens/server/embeddable/types.ts
+++ x-pack/platform/plugins/shared/lens/server/embeddable/types.ts
@@ -11,7 +11,7 @@ import type {
 } from '@kbn/embeddable-plugin/server';
 
 import type { LensRuntimeState } from '../../public';
-import { LENS_EMBEDDABLE_TYPE } from '@kbn/lens-common';
+import type { LENS_EMBEDDABLE_TYPE } from '@kbn/lens-common';
 
 export type LensEmbeddableStateWithType = EmbeddableStateWithType &
   LensRuntimeState & {
