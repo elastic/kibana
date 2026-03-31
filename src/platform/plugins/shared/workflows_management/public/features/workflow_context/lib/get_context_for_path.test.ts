35d74f3d6a19ed1fff5987ebab997f66a37897f1
Ihor Panasiuk <igorskynet13@gmail.com>
2026-03-31 10:54:21 +0200

    Update get_context_for_path.test.ts

--- src/platform/plugins/shared/workflows_management/public/features/workflow_context/lib/get_context_for_path.test.ts
+++ src/platform/plugins/shared/workflows_management/public/features/workflow_context/lib/get_context_for_path.test.ts
@@ -11,7 +11,6 @@ import type { Step, WorkflowYaml } from '@kbn/workflows';
 import { DynamicStepContextSchema, ForEachContextSchema, WhileContextSchema } from '@kbn/workflows';
 import { expectZodSchemaEqual } from '@kbn/workflows/common/utils/zod/test_utils/expect_zod_schema_equal';
 import { WorkflowGraph } from '@kbn/workflows/graph';
-import { ManualTriggerEventSchema } from '@kbn/workflows/spec/schema/triggers/manual_trigger_schema';
 import { z } from '@kbn/zod/v4';
 import { getContextSchemaForPath } from './get_context_for_path';
 
@@ -86,7 +85,6 @@ describe('getContextSchemaForPath', () => {
     expectZodSchemaEqual(
       context,
       DynamicStepContextSchema.extend({
-        event: ManualTriggerEventSchema,
         inputs: z.object({}),
         consts: z.object({
           test: z.literal('test'),
@@ -107,7 +105,6 @@ describe('getContextSchemaForPath', () => {
     expectZodSchemaEqual(
       context,
       DynamicStepContextSchema.extend({
-        event: ManualTriggerEventSchema,
         inputs: z.object({}),
         steps: z.object({
           'first-step': z.object({
