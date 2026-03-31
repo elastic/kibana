7f2f1f75f4a6cdb81fe19661589d7eac759fdbc5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-03-31 09:24:37 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/watchlists/entity_sources/entity_sources_service.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lib/entity_analytics/watchlists/entity_sources/entity_sources_service.ts
@@ -1,10 +1,11 @@
-42
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
+42;
 
 import type { ElasticsearchClient, Logger, SavedObjectsClientContract } from '@kbn/core/server';
 import { WatchlistEntitySourceClient } from './infra';
@@ -39,8 +40,8 @@ export const createEntitySourcesService = ({
     const watchlist = await watchlistClient.get(watchlistId);
     const sourceIds = await watchlistClient.getEntitySourceIds(watchlistId);
     const targetIndex = getIndexForWatchlist(watchlist.name, namespace);
-    
-    const { sources } = await descriptorClient.list({}); // default to 10 sources, each watchlist has 1/2 sources so this is ok for now but not ideal. 
+
+    const { sources } = await descriptorClient.list({}); // default to 10 sources, each watchlist has 1/2 sources so this is ok for now but not ideal.
     const entitiesBySource = await Promise.all(
       sources
         .filter((s) => sourceIds.includes(s.id))
