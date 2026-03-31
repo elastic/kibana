f39e6566e7b10e594e57359ad6c8280e7d64f1d6
Jonathan Budzenski <jon@elastic.co>
2026-03-31 13:22:42 -0500

    Upgrade to Node.js 24.14.1

--- src/dev/build/lib/get_build_number.ts
+++ src/dev/build/lib/get_build_number.ts
@@ -7,18 +7,9 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import os from 'os';
 import execa from 'execa';
 
 export async function getBuildNumber() {
-  if (/^win/.test(os.platform())) {
-    // Windows does not have the wc process and `find /C /V ""` does not consistently work
-    const log = await execa('git', ['log', '--format="%h"']);
-    return log.stdout.split('\n').length;
-  }
-
-  const wc = await execa.command('git log --format="%h" | wc -l', {
-    shell: true,
-  });
-  return parseFloat(wc.stdout.trim());
+  const count = await execa('git', ['rev-list', '--count', 'HEAD']);
+  return parseFloat(count.stdout.trim());
 }
