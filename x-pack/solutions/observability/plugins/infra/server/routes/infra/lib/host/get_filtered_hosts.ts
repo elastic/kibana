3aacb2111e4a59b6e0b428d5f867435276b50cd9
Sergi Romeu <sergi.romeu@elastic.co>
2026-04-01 11:11:10 +0200

    [Infra] Post-enrichment filtering for Hosts exclusion filters (#260426)
    (cherry picked from commit 705ddb99317ff5143909e5c8352db962affac98b)
    
    # Conflicts:
    #	x-pack/solutions/observability/plugins/infra/server/routes/infra/lib/host/get_filtered_hosts.ts
    #	x-pack/solutions/observability/plugins/infra/server/routes/infra/lib/host/get_hosts_count.ts

--- x-pack/solutions/observability/plugins/infra/server/routes/infra/lib/host/get_filtered_hosts.ts
+++ x-pack/solutions/observability/plugins/infra/server/routes/infra/lib/host/get_filtered_hosts.ts
@@ -21,7 +21,7 @@ export const getFilteredHostNames = async ({
   schema,
 }: Required<Pick<GetHostParameters, 'infraMetricsClient' | 'from' | 'to' | 'limit' | 'schema'>> & {
   query?: estypes.QueryDslQueryContainer;
-}) => {
+}): Promise<string[]> => {
   const inventoryModel = findInventoryModel('host');
 
   const response = await infraMetricsClient.search({
@@ -31,27 +31,24 @@ export const getFilteredHostNames = async ({
     query: {
       bool: {
         filter: [
-          ...castArray(query),
           ...rangeQuery(from, to),
           ...(inventoryModel.nodeFilter?.({ schema }) ?? []),
+          ...(query ? [query] : []),
         ],
       },
     },
     aggs: {
-      uniqueHostNames: {
+      filteredHosts: {
         terms: {
           field: HOST_NAME_FIELD,
           size: limit,
-          order: {
-            _key: 'asc',
-          },
+          order: { _key: 'asc' },
         },
       },
     },
   });
 
-  const { uniqueHostNames } = response.aggregations ?? {};
-  return uniqueHostNames?.buckets?.map((p) => p.key as string) ?? [];
+  return response.aggregations?.filteredHosts?.buckets?.map((p) => p.key as string) ?? [];
 };
 
 export const getHasDataFromSystemIntegration = async ({
