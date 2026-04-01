1f53c55d9af52bd09ebf1565243aed218fc34f2a
Gergő Ábrahám <gergo.abraham@elastic.co>
2026-04-01 11:00:07 +0200

    update error message texts

--- x-pack/solutions/security/plugins/security_solution/server/lists_integration/endpoint/validators/trusted_app_validator.ts
+++ x-pack/solutions/security/plugins/security_solution/server/lists_integration/endpoint/validators/trusted_app_validator.ts
@@ -227,7 +227,7 @@ export class TrustedAppValidator extends BaseValidator {
     await this.validatePreImportItems(items, async (item) => {
       // import specific validations
       await this.validateImportOwnerSpaceIds(item); // instead of validateCreateOwnerSpaceIds
-      await this.validateCanCreateGlobalArtifacts(item);
+      await this.validateCanImportGlobalArtifacts(item); // instead of validateCanCreateGlobalArtifacts
       await this.removeInvalidPolicyIds(item); // instead of validateByPolicyItem
 
       // usual validators from pre-create
