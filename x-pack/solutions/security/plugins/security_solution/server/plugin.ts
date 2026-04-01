0bbed56b075142d588d9205aefe1d95e3ca659f9
Jon Wålstedt <jon.walstedt@elastic.co>
2026-04-01 11:24:51 +0200

    refactor(security_solution): remove anonymization platform profile initializer

--- x-pack/solutions/security/plugins/security_solution/server/plugin.ts
+++ x-pack/solutions/security/plugins/security_solution/server/plugin.ts
@@ -162,7 +162,6 @@ import {
 import { AIValueReportLocatorDefinition } from '../common/locators/ai_value_report/locator';
 import type { TrialCompanionRoutesDeps } from './lib/trial_companion/types';
 import { setupAlertsCapabilitiesSwitcher } from './lib/capabilities/alerts_capabilities_switcher';
-import { securityAlertsProfileInitializer } from './lib/anonymization';
 import { registerWatchlistMaintainer } from './lib/entity_analytics/watchlists/maintainer/register_watchlist_maintainer';
 import { registerEndpointExceptionsRoutes } from './endpoint/routes/endpoint_exceptions_per_policy_opt_in';
 import {
@@ -830,8 +829,6 @@ export class Plugin implements ISecuritySolutionPlugin {
     this.licensing$ = plugins.licensing.license$;
 
     this.telemetryConfigProvider.start(plugins.telemetry.isOptedIn$);
-    plugins.anonymization.registerProfileInitializer(securityAlertsProfileInitializer);
-
     // Assistant Tool and Feature Registration
     const filteredTools = config.experimentalFeatures.riskScoreAssistantToolDisabled
       ? assistantTools.filter(({ id }) => id !== ENTITY_RISK_SCORE_TOOL_ID)
