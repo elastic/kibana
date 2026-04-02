137bc1db20813e02fabfc27a9acd23ed5886ca17
Gerard Soldevila <gerard.soldevila@elastic.co>
2026-04-02 16:04:37 +0200

    Fix ZDT integration test

--- src/core/packages/saved-objects/migration-server-internal/src/model/extract_errors.ts
+++ src/core/packages/saved-objects/migration-server-internal/src/model/extract_errors.ts
@@ -27,9 +27,13 @@ export function extractTransformFailuresReason(
   // we have both the saved object Id and the stack trace in each `transformErrors` item.
   const transformErrorsReason =
     transformErrors.length > 0
-      ? ` ${transformErrors.length} transformation errors were encountered (showing the first 5 - check the logs for the full list):\n` +
+      ? ` ${transformErrors.length} transformation errors were encountered${
+          transformErrors.length > 10
+            ? '(showing the first 10 - check the logs for the full list)'
+            : ''
+        }:\n` +
         transformErrors
-          .slice(0, 5)
+          .slice(0, 10)
           .map((errObj) => `- ${errObj.rawId}: ${errObj.err.stack ?? errObj.err.message}\n`)
           .join('')
       : '';
