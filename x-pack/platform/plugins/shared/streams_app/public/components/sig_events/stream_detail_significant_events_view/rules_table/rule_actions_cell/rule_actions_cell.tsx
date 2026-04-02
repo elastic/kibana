07243a94fd6c04d43882e66ad8d7ac634295fde7
Mykola Harmash <mykola.harmash@elastic.co>
2026-04-02 13:54:03 +0200

    rearrenge context menu actions

--- x-pack/platform/plugins/shared/streams_app/public/components/sig_events/stream_detail_significant_events_view/rules_table/rule_actions_cell/rule_actions_cell.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/sig_events/stream_detail_significant_events_view/rules_table/rule_actions_cell/rule_actions_cell.tsx
@@ -45,6 +45,7 @@ export function RuleActionsCell({
           <EuiContextMenuItem
             key="query-delete"
             icon="trash"
+            color="danger"
             disabled={isDisabled}
             onClick={() => {
               setIsActionsMenuOpen(false);
