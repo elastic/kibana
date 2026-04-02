7c3f631481eaa832284b9ead3661a0c5a84bddff
Jon Wålstedt <jon.walstedt@elastic.co>
2026-04-02 15:13:36 +0200

    [Security AI Assistant] revert changes to chat_complete_route.ts

--- x-pack/solutions/security/plugins/elastic_assistant/server/routes/chat/chat_complete_route.ts
+++ x-pack/solutions/security/plugins/elastic_assistant/server/routes/chat/chat_complete_route.ts
@@ -22,6 +22,7 @@ import { buildRouteValidationWithZod } from '@kbn/elastic-assistant-common/impl/
 import { getRequestAbortedSignal } from '@kbn/data-plugin/server';
 import { defaultInferenceEndpoints } from '@kbn/inference-common';
 import { v4 as uuidv4 } from 'uuid';
+import { getInferenceConnectorById } from '../../lib/prompt';
 import { INVOKE_ASSISTANT_ERROR_EVENT } from '../../lib/telemetry/event_based_telemetry';
 import type { ElasticAssistantPluginRouter } from '../../types';
 import { buildResponse } from '../../lib/build_response';
@@ -124,9 +125,10 @@ export const chatCompleteRoute = (
           // get the actions plugin start contract from the request context:
           const actions = ctx.elasticAssistant.actions;
           const actionsClient = await actions.getActionsClientWithRequest(request);
-          const inferenceConnector = await ctx.elasticAssistant.inference
-            .getConnectorById(connectorId, request)
-            .catch(() => undefined);
+          const inferenceConnector = await getInferenceConnectorById(
+            inference,
+            request
+          )(connectorId).catch(() => undefined);
           actionTypeId = inferenceConnector?.type ?? '.inference';
           const isOssModel = isOpenSourceModel(inferenceConnector);
           const savedObjectsClient = ctx.elasticAssistant.savedObjectsClient;
