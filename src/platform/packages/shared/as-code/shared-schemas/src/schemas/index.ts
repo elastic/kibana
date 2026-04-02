0937b68bc333d08d80a2b72ac00c5bc9c5930c89
Nick Partridge <nicholas.partridge@elastic.co>
2026-04-02 08:41:00 -0700

    align lens meta in public apis to dashboard

--- src/platform/packages/shared/as-code/shared-schemas/src/schemas/index.ts
+++ src/platform/packages/shared/as-code/shared-schemas/src/schemas/index.ts
@@ -8,4 +8,5 @@
  */
 
 export { asCodeIdSchema } from './id';
-export { asCodeMetaSchema, getMeta } from './meta';
+export { asCodeMetaSchema, getMeta, type AsCodeMeta } from './meta';
+export { asCodeResolveSchema } from './resolve_schema';
