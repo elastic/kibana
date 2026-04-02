bb5e807b50493af5eedb12bd3f9b7b842da10696
Paul Tavares <paul.tavares@elastic.co>
2026-04-02 11:26:40 -0400

    Un-skip tests for console and fix jest warnings about use of fake timers

--- x-pack/solutions/security/plugins/security_solution/public/management/components/console/mocks.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/management/components/console/mocks.tsx
@@ -245,8 +245,7 @@ export const triggerConsoleCommandInputEvent = async (
 };
 
 export const getConsoleTestSetup = (): ConsoleTestSetup => {
-  // Workaround for timeout via https://github.com/testing-library/user-event/issues/833#issuecomment-1171452841
-  const user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
+  const user = userEvent as unknown as UserEvent;
   const mockedContext = createAppRootMockRenderer();
   const { startServices, coreStart, depsStart, queryClient, history, setExperimentalFlag } =
     mockedContext;
