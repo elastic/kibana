b91a8b2983fbb7b351ab4a7731d64cb14b6721e6
robester0403 <robert.so@elastic.co>
2026-04-02 20:23:27 -0400

    FEAT: added EIS config

---
+++ x-pack/platform/plugins/shared/automatic_import/server/inference_feature.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+import { defaultInferenceEndpoints } from '@kbn/inference-common';
+import type { InferenceFeatureConfig } from '@kbn/search-inference-endpoints/server';
+import {
+  AUTOMATIC_IMPORT_INFERENCE_FEATURE_ID,
+  AUTOMATIC_IMPORT_PARENT_INFERENCE_FEATURE_ID,
+} from '../common/constants';
+import {
+  AUTOMATIC_IMPORT_FEATURE_DESCRIPTION,
+  AUTOMATIC_IMPORT_FEATURE_NAME,
+  AUTOMATIC_IMPORT_PARENT_FEATURE_DESCRIPTION,
+  AUTOMATIC_IMPORT_PARENT_FEATURE_NAME,
+} from './inference_feature_translations';
+
+export const automaticImportParentInferenceFeature: InferenceFeatureConfig = {
+  featureId: AUTOMATIC_IMPORT_PARENT_INFERENCE_FEATURE_ID,
+  featureName: AUTOMATIC_IMPORT_PARENT_FEATURE_NAME,
+  featureDescription: AUTOMATIC_IMPORT_PARENT_FEATURE_DESCRIPTION,
+  taskType: 'completion',
+  recommendedEndpoints: [defaultInferenceEndpoints.KIBANA_DEFAULT_CHAT_COMPLETION],
+};
+
+export const automaticImportInferenceFeature: InferenceFeatureConfig = {
+  parentFeatureId: AUTOMATIC_IMPORT_PARENT_INFERENCE_FEATURE_ID,
+  featureId: AUTOMATIC_IMPORT_INFERENCE_FEATURE_ID,
+  featureName: AUTOMATIC_IMPORT_FEATURE_NAME,
+  featureDescription: AUTOMATIC_IMPORT_FEATURE_DESCRIPTION,
+  taskType: 'completion',
+  recommendedEndpoints: [
+    '.anthropic-claude-4.6-opus-chat_completion',
+    '.openai-gpt-5.2-chat_completion',
+    '.anthropic-claude-4.6-sonnet-chat_completion',
+  ],
+};
