0771b25a4afa004c60abbe672a5a9a2991d520d6
Tyler Smalley <tyler.smalley@elastic.co>
2026-04-03 20:19:55 -0700

    Add local Moon-based validation via scripts/check (#256599)
    Introduce a new `scripts/check` entrypoint that uses Moon changed-file
    resolution to run eslint, jest, and type_check against local, staged, or
    branch-scoped changes.
    
    Keep the existing `eslint`, `jest`, and `type_check` commands intact for
    their default direct usage, and only route them through the new
    validation contract when scoped validation flags are present. This also
    adds dedicated contract runners for jest, eslint, and type_check, plus
    clearer help and shallow-repo warnings for the new workflow.
    
    Updates https://github.com/elastic/kibana/issues/255391
    
    ---------
    
    Signed-off-by: Tyler Smalley <tyler.smalley@elastic.co>
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    (cherry picked from commit 3a3c8c5eb62f90058f8cedad35598f3720db1acc)

--- scripts/check.js
+++ scripts/check.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the "Elastic License
@@ -7,11 +8,5 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export {
-  executeTypeCheckValidation,
-  TSC_LABEL,
-  type ExecuteTypeCheckValidationOptions,
-  type TscValidationResult,
-} from './execute_type_check_validation';
-export { runLegacyTypeCheckCli } from './run_type_check_legacy_cli';
-export { runTypeCheckContractCli } from './run_type_check_contract_cli';
+require('@kbn/setup-node-env');
+require('../src/dev/run_check');
