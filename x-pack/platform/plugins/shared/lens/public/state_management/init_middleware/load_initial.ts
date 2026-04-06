1e5c271af01baf6e787e58e74e52e6cb81804d65
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-04-06 18:33:25 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/plugins/shared/lens/public/state_management/init_middleware/load_initial.ts
+++ x-pack/platform/plugins/shared/lens/public/state_management/init_middleware/load_initial.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { LENS_DATASOURCE_ID, LENS_EMBEDDABLE_TYPE } from '@kbn/lens-common';
+import { LENS_DATASOURCE_ID } from '@kbn/lens-common';
 
 import type { MiddlewareAPI } from '@reduxjs/toolkit';
 import { i18n } from '@kbn/i18n';
