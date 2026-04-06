9fab4dc0725504b379359b058ab4672822f31e17
konrad.szwarc <29123534+szwarckonrad@users.noreply.github.com>
2026-04-06 12:42:21 -0600

    Remove unnecessary loadEndpointData from artifact and trusted apps tests

--- x-pack/solutions/security/test/security_solution_endpoint/apps/integrations/artifact_entries_list.ts
+++ x-pack/solutions/security/test/security_solution_endpoint/apps/integrations/artifact_entries_list.ts
@@ -8,7 +8,6 @@
 import { unzip } from 'zlib';
 import { promisify } from 'util';
 import expect from '@kbn/expect';
-import type { IndexedHostsAndAlertsResponse } from '@kbn/security-solution-plugin/common/endpoint/index_data';
 import { ENDPOINT_ARTIFACT_LIST_IDS } from '@kbn/securitysolution-list-constants';
 import type { ArtifactElasticsearchProperties } from '@kbn/fleet-plugin/server/services';
 import type { FtrProviderContext } from '../../configs/ftr_provider_context';
@@ -27,7 +26,6 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
   const testSubjects = getService('testSubjects');
   const browser = getService('browser');
   const endpointArtifactsTestResources = getService('endpointArtifactTestResources');
-  const endpointTestResources = getService('endpointTestResources');
   const retry = getService('retry');
   const esClient = getService('es');
   const toasts = getService('toasts');
@@ -44,15 +42,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
     targetTags(this, ['@ess', '@serverless']);
     this.timeout(60_000 * 5);
 
-    let indexedData: IndexedHostsAndAlertsResponse;
     let policyInfo: PolicyTestResourceInfo;
-
-    before(async () => {
-      indexedData = await endpointTestResources.loadEndpointData();
-    });
-    after(async () => {
-      await endpointTestResources.unloadEndpointData(indexedData);
-    });
 
     const checkFleetArtifacts = async (
       identifier: string,
