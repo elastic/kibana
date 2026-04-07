e740857590bba5a2128cb90c8cc85934dc2d2781
Timothy Sullivan <tsullivan@elastic.co>
2026-04-07 13:26:35 -0700

    add new language selector for cloud links

---
+++ x-pack/platform/plugins/private/cloud_integrations/cloud_links/public/maybe_add_cloud_links/language_selector/language_modal.tsx
@@ -0,0 +1,96 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { type FC } from 'react';
+import {
+  EuiButton,
+  EuiFormRow,
+  EuiModal,
+  EuiModalBody,
+  EuiModalFooter,
+  EuiModalHeader,
+  EuiModalHeaderTitle,
+  EuiSelect,
+  EuiButtonEmpty,
+  useGeneratedHtmlId,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+
+import { useLanguage } from './use_language_hook';
+
+const SUPPORTED_LOCALES = [
+  { value: 'en', text: 'English' },
+  { value: 'fr-FR', text: 'Français' },
+  { value: 'ja-JP', text: '日本語' },
+  { value: 'zh-CN', text: '中文' },
+  { value: 'de-DE', text: 'Deutsch' },
+];
+
+interface Props {
+  closeModal: () => void;
+}
+
+export const LanguageModal: FC<Props> = ({ closeModal }) => {
+  const modalTitleId = useGeneratedHtmlId();
+  const selectId = useGeneratedHtmlId();
+
+  const { locale, initialLocaleValue, isLoading, onChange } = useLanguage();
+
+  return (
+    <EuiModal aria-labelledby={modalTitleId} onClose={closeModal}>
+      <EuiModalHeader>
+        <EuiModalHeaderTitle size="m" id={modalTitleId}>
+          {i18n.translate('xpack.cloudLinks.userMenuLinks.languageModalTitle', {
+            defaultMessage: 'Language',
+          })}
+        </EuiModalHeaderTitle>
+      </EuiModalHeader>
+
+      <EuiModalBody>
+        <EuiFormRow
+          label={i18n.translate('xpack.cloudLinks.userMenuLinks.languageModalSelectLabel', {
+            defaultMessage: 'Display language',
+          })}
+          fullWidth
+        >
+          <EuiSelect
+            id={selectId}
+            options={SUPPORTED_LOCALES}
+            value={locale}
+            onChange={(e) => onChange(e.target.value, false)}
+            data-test-subj="languageSelect"
+            fullWidth
+          />
+        </EuiFormRow>
+      </EuiModalBody>
+
+      <EuiModalFooter>
+        <EuiButtonEmpty data-test-subj="languageModalDiscardButton" onClick={closeModal}>
+          {i18n.translate('xpack.cloudLinks.userMenuLinks.languageModalDiscardBtnLabel', {
+            defaultMessage: 'Discard',
+          })}
+        </EuiButtonEmpty>
+
+        <EuiButton
+          data-test-subj="languageModalSaveButton"
+          onClick={async () => {
+            if (locale !== initialLocaleValue) {
+              onChange(locale, true);
+            }
+            closeModal();
+          }}
+          fill
+          isLoading={isLoading}
+        >
+          {i18n.translate('xpack.cloudLinks.userMenuLinks.languageModalSaveBtnLabel', {
+            defaultMessage: 'Save changes',
+          })}
+        </EuiButton>
+      </EuiModalFooter>
+    </EuiModal>
+  );
+};
---
+++ x-pack/platform/plugins/private/cloud_integrations/cloud_links/public/maybe_add_cloud_links/language_selector/use_language_hook.ts
@@ -0,0 +1,90 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useCallback, useEffect, useState } from 'react';
+import { i18n } from '@kbn/i18n';
+import {
+  useUpdateUserProfile,
+  type LocaleValue as Locale,
+} from '@kbn/user-profile-components';
+
+const DEFAULT_LOCALE = 'en';
+
+interface LanguageAPI {
+  locale: Locale;
+  initialLocaleValue: Locale;
+  isVisible: boolean;
+  isLoading: boolean;
+  onChange: (locale: Locale, updateUserProfile: boolean) => void;
+}
+
+export const useLanguage = (): LanguageAPI => {
+  const { userProfileData, isLoading, update, userProfileLoaded } = useUpdateUserProfile({
+    notificationSuccess: {
+      title: i18n.translate('xpack.cloudLinks.userMenuLinks.language.successNotificationTitle', {
+        defaultMessage: 'Language settings updated',
+      }),
+      pageReloadText: i18n.translate(
+        'xpack.cloudLinks.userMenuLinks.language.successNotificationText',
+        {
+          defaultMessage: 'Reload the page to see the changes',
+        }
+      ),
+    },
+    pageReloadChecker: (prev, next) => {
+      return prev?.userSettings?.locale !== next.userSettings?.locale;
+    },
+  });
+
+  const { userSettings: { locale: localeUserProfile = DEFAULT_LOCALE } = {} } =
+    userProfileData ?? { userSettings: {} };
+
+  const [locale, setLocale] = useState<Locale>(localeUserProfile);
+  const [initialLocaleValue, setInitialLocaleValue] = useState<Locale>(localeUserProfile);
+
+  const onChange = useCallback(
+    (updatedLocale: Locale, persist: boolean) => {
+      if (isLoading) {
+        return;
+      }
+
+      setLocale(updatedLocale);
+
+      if (!persist) {
+        return;
+      }
+
+      return update({
+        userSettings: {
+          locale: updatedLocale,
+        },
+      });
+    },
+    [isLoading, update]
+  );
+
+  useEffect(() => {
+    setLocale(localeUserProfile);
+  }, [localeUserProfile]);
+
+  useEffect(() => {
+    if (userProfileLoaded) {
+      const storedLocale = userProfileData?.userSettings?.locale;
+      if (storedLocale) {
+        setInitialLocaleValue(storedLocale);
+      }
+    }
+  }, [userProfileData, userProfileLoaded]);
+
+  return {
+    locale,
+    initialLocaleValue,
+    isVisible: Boolean(userProfileData),
+    isLoading,
+    onChange,
+  };
+};
