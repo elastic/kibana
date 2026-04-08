b7c65756041d527af44e593ab859d51a19c928b4
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-04-08 22:18:31 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/package_policy_input_panel.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/agent_policy/create_package_policy_page/components/steps/components/package_policy_input_panel.tsx
@@ -525,7 +525,8 @@ export const PackagePolicyInputPanel: React.FunctionComponent<{
               showDescriptionColumn={!isSingleInputAndStreams}
               streamAdvancedVars={consolidatedStreamAdvancedVars}
             />
-            {hasInputStreams && !shouldConsolidateAdvancedSections &&
+            {hasInputStreams &&
+            !shouldConsolidateAdvancedSections &&
             packageInput.show_divider !== false ? (
               <ShortenedHorizontalRule margin="m" />
             ) : (
