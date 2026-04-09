928d5f789fc04f26660d0a1c93a0d24cb28bc152
Francesco Fagnani <francesco.fagnani@elastic.co>
2026-04-09 08:12:00 +0200

    refactor(hook): add useModelSettingsUrl hook

---
+++ x-pack/platform/plugins/shared/streams_app/public/hooks/use_model_settings_url.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useMemo } from 'react';
+import { MANAGEMENT_APP_LOCATOR } from '@kbn/deeplinks-management/constants';
+import { useKibana } from './use_kibana';
+
+export const useModelSettingsUrl = (): string => {
+  const {
+    dependencies: {
+      start: { share },
+    },
+  } = useKibana();
+
+  return useMemo(() => {
+    const managementLocator = share.url.locators.get(MANAGEMENT_APP_LOCATOR);
+    return (
+      managementLocator?.getRedirectUrl({
+        sectionId: 'modelManagement',
+        appId: 'model_settings',
+      }) ?? ''
+    );
+  }, [share.url.locators]);
+};
