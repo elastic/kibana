10a1b8fdbbfe72d7bd0ec47e9db4e44d01f8093f
diogolima-elastic <diogo.lima@elastic.co>
2026-04-09 17:43:59 +0100

    fix popover not closing on button click

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_management/data_management/stream_detail_routing/review_suggestions_form/refinement_popover.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_management/data_management/stream_detail_routing/review_suggestions_form/refinement_popover.tsx
@@ -20,6 +20,7 @@ import {
 } from '@elastic/eui';
 import { css } from '@emotion/react';
 import { i18n } from '@kbn/i18n';
+import { AiButtonIcon, type AiButtonIconType } from '@kbn/shared-ux-ai-components';
 import type { AIFeatures } from '../../../../../hooks/use_ai_features';
 import { ConnectorIcon } from '../../../../connector_list_button/connector_icon';
 import { ConnectorPickerPopover } from '../../../../connector_list_button/connector_picker_popover';
@@ -45,8 +46,16 @@ export const RefinementPopover = ({
   const editorRef = useRef<HTMLDivElement>(null);
   const editorId = useGeneratedHtmlId({ prefix: 'refinementEditor' });
 
-  const openPopover = useCallback(() => {
-    setIsPopoverOpen(true);
+  const togglePopover = useCallback(() => {
+    setIsPopoverOpen((prev) => {
+      if (prev) {
+        setUserPrompt('');
+        if (editorRef.current) {
+          editorRef.current.textContent = '';
+        }
+      }
+      return !prev;
+    });
   }, []);
 
   const closePopover = useCallback(() => {
@@ -123,25 +132,10 @@ export const RefinementPopover = ({
     border-color: ${euiTheme.colors.borderBaseSubdued};
   `;
 
-  const aiGradient = `linear-gradient(130.84deg, ${euiTheme.colors.backgroundFilledPrimary} 2.98%, ${euiTheme.colors.backgroundFilledAssistance} 66.24%)`;
-
-  const submitButtonStyles = css`
-    background: ${aiGradient};
-    border-color: transparent;
-    &:hover:not(:disabled) {
-      background: ${aiGradient};
-      filter: brightness(1.1);
-      transform: none;
-    }
-    &:focus {
-      background: ${aiGradient};
-    }
-  `;
-
   const triggerButton = (
     <GenerateSuggestionButton
       size="s"
-      onClick={openPopover}
+      onClick={togglePopover}
       isLoading={isLoading}
       isDisabled={isDisabled}
       aiFeatures={aiFeatures}
@@ -257,13 +251,12 @@ export const RefinementPopover = ({
                 </EuiButtonEmpty>
               </EuiFlexItem>
               <EuiFlexItem grow={false}>
-                <EuiButtonIcon
-                  iconType="sortUp"
-                  display="fill"
+                <AiButtonIcon
+                  iconType={'sortUp' as AiButtonIconType}
+                  variant="accent"
                   size="s"
-                  css={submitButtonStyles}
                   onClick={handleRefine}
-                  disabled={!aiFeatures.genAiConnectors.selectedConnector}
+                  isDisabled={!aiFeatures.genAiConnectors.selectedConnector}
                   aria-label={submitAriaLabel}
                   data-test-subj="streamsAppRefinementSubmitButton"
                 />
