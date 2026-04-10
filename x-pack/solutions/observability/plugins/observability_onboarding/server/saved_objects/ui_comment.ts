7b6c256ec5d11d9f2eadab1558d4db5da4418836
Christian Siles <christiansiles@Christians-MacBook-Pro.local>
2026-04-10 17:59:47 +0200

    Created saved object to store comments

---
+++ x-pack/solutions/observability/plugins/observability_onboarding/server/saved_objects/ui_comment.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { SavedObjectsType } from '@kbn/core/server';
+
+export const UI_COMMENT_SAVED_OBJECT_TYPE = 'observability-onboarding-ui-comment';
+
+export const uiComment: SavedObjectsType = {
+  name: UI_COMMENT_SAVED_OBJECT_TYPE,
+  hidden: false,
+  namespaceType: 'agnostic',
+  mappings: {
+    properties: {
+      clientX: { type: 'integer' },
+      clientY: { type: 'integer' },
+      text: { type: 'text' },
+      author: { type: 'keyword' },
+      createdAt: { type: 'date' },
+      resolved: { type: 'boolean' },
+      replies: { type: 'object', dynamic: false },
+      pathname: { type: 'keyword' },
+    },
+  },
+};
