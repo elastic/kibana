1a8b55b7f9f830c1497f33e65c47dfc986cb3ea5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-04-10 09:22:44 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/packages/shared/response-ops/rule_params/v1.ts
+++ x-pack/platform/packages/shared/response-ops/rule_params/v1.ts
@@ -47,22 +47,55 @@ import { uptimeTLSRuleParamsSchemaV1 } from './uptime_tls';
 export const RULE_TYPE_ID = 'rule_type_id';
 
 const ruleParamsSchemasWithRuleTypeId: Record<string, { title: string; params: Type<unknown> }> = {
-  monitoring_ccr_read_exceptions: { title: 'CCR read exceptions', params: ccrReadExceptionsParamsSchemaV1 },
+  monitoring_ccr_read_exceptions: {
+    title: 'CCR read exceptions',
+    params: ccrReadExceptionsParamsSchemaV1,
+  },
   monitoring_alert_cluster_health: { title: 'Cluster health', params: clusterHealthParamsSchemaV1 },
   monitoring_alert_cpu_usage: { title: 'CPU usage', params: cpuUsageParamsSchemaV1 },
   monitoring_alert_disk_usage: { title: 'Disk usage', params: diskUsageParamsSchemaV1 },
-  monitoring_alert_elasticsearch_version_mismatch: { title: 'Elasticsearch version mismatch', params: esVersionMismatchParamsSchemaV1 },
-  monitoring_alert_kibana_version_mismatch: { title: 'Kibana version mismatch', params: kibanaVersionMismatchParamsSchemaV1 },
-  monitoring_alert_license_expiration: { title: 'License expiration', params: licenseExpirationParamsSchemaV1 },
-  monitoring_alert_logstash_version_mismatch: { title: 'Logstash version mismatch', params: logstashVersionMismatchParamsSchemaV1 },
-  monitoring_alert_jvm_memory_usage: { title: 'JVM memory usage', params: memoryUsageParamsSchemaV1 },
-  monitoring_alert_missing_monitoring_data: { title: 'Missing monitoring data', params: missingMonitoringDataParamsSchemaV1 },
+  monitoring_alert_elasticsearch_version_mismatch: {
+    title: 'Elasticsearch version mismatch',
+    params: esVersionMismatchParamsSchemaV1,
+  },
+  monitoring_alert_kibana_version_mismatch: {
+    title: 'Kibana version mismatch',
+    params: kibanaVersionMismatchParamsSchemaV1,
+  },
+  monitoring_alert_license_expiration: {
+    title: 'License expiration',
+    params: licenseExpirationParamsSchemaV1,
+  },
+  monitoring_alert_logstash_version_mismatch: {
+    title: 'Logstash version mismatch',
+    params: logstashVersionMismatchParamsSchemaV1,
+  },
+  monitoring_alert_jvm_memory_usage: {
+    title: 'JVM memory usage',
+    params: memoryUsageParamsSchemaV1,
+  },
+  monitoring_alert_missing_monitoring_data: {
+    title: 'Missing monitoring data',
+    params: missingMonitoringDataParamsSchemaV1,
+  },
   monitoring_alert_nodes_changed: { title: 'Nodes changed', params: nodesChangedParamsSchemaV1 },
   monitoring_shard_size: { title: 'Large shard size', params: largeShardSizeParamsSchemaV1 },
-  monitoring_alert_thread_pool_search_rejections: { title: 'Thread pool search rejections', params: threadPoolSearchRejectionsParamsSchemaV1 },
-  monitoring_alert_thread_pool_write_rejections: { title: 'Thread pool write rejections', params: threadPoolWriteRejectionsParamsSchemaV1 },
-  'xpack.ml.anomaly_detection_alert': { title: 'Anomaly detection', params: mlAnomalyDetectionAlertParamsSchemaV1 },
-  'xpack.ml.anomaly_detection_jobs_health': { title: 'Anomaly detection jobs health', params: anomalyDetectionJobsHealthRuleParamsSchemaV1 },
+  monitoring_alert_thread_pool_search_rejections: {
+    title: 'Thread pool search rejections',
+    params: threadPoolSearchRejectionsParamsSchemaV1,
+  },
+  monitoring_alert_thread_pool_write_rejections: {
+    title: 'Thread pool write rejections',
+    params: threadPoolWriteRejectionsParamsSchemaV1,
+  },
+  'xpack.ml.anomaly_detection_alert': {
+    title: 'Anomaly detection',
+    params: mlAnomalyDetectionAlertParamsSchemaV1,
+  },
+  'xpack.ml.anomaly_detection_jobs_health': {
+    title: 'Anomaly detection jobs health',
+    params: anomalyDetectionJobsHealthRuleParamsSchemaV1,
+  },
   'datasetQuality.degradedDocs': { title: 'Degraded docs', params: degradedDocsParamsSchemaV1 },
   '.es-query': { title: 'ES query', params: EsQueryRuleParamsSchemaV1 },
   '.index-threshold': { title: 'Index threshold', params: IndexThresholdRuleParamsSchemaV1 },
@@ -70,16 +103,43 @@ const ruleParamsSchemasWithRuleTypeId: Record<string, { title: string; params: T
   transform_health: { title: 'Transform health', params: transformHealthRuleParamsSchemaV1 },
   'apm.anomaly': { title: 'APM anomaly', params: anomalyParamsSchemaV1 },
   'apm.error_rate': { title: 'Error rate', params: errorCountParamsSchemaV1 },
-  'apm.transaction_error_rate': { title: 'Transaction error rate', params: transactionErrorRateParamsSchemaV1 },
-  'apm.transaction_duration': { title: 'Transaction duration', params: transactionDurationParamsSchemaV1 },
-  'xpack.synthetics.alerts.monitorStatus': { title: 'Synthetics monitor status', params: syntheticsMonitorStatusRuleParamsSchemaV1 },
+  'apm.transaction_error_rate': {
+    title: 'Transaction error rate',
+    params: transactionErrorRateParamsSchemaV1,
+  },
+  'apm.transaction_duration': {
+    title: 'Transaction duration',
+    params: transactionDurationParamsSchemaV1,
+  },
+  'xpack.synthetics.alerts.monitorStatus': {
+    title: 'Synthetics monitor status',
+    params: syntheticsMonitorStatusRuleParamsSchemaV1,
+  },
   'xpack.synthetics.alerts.tls': { title: 'Synthetics TLS', params: tlsRuleParamsSchemaV1 },
-  'xpack.uptime.alerts.monitorStatus': { title: 'Uptime monitor status', params: uptimeMonitorStatusRuleParamsSchemaV1 },
-  'xpack.uptime.alerts.tlsCertificate': { title: 'Uptime TLS certificate', params: uptimeTLSRuleParamsSchemaV1 },
-  'xpack.uptime.alerts.durationAnomaly': { title: 'Uptime duration anomaly', params: uptimeDurationAnomalyRuleParamsSchemaV1 },
-  'metrics.alert.inventory.threshold': { title: 'Metric inventory threshold', params: metricInventoryThresholdRuleParamsSchemaV1 },
-  'metrics.alert.threshold': { title: 'Metric threshold', params: metricThresholdRuleParamsSchemaV1 },
-  'observability.rules.custom_threshold': { title: 'Custom threshold', params: customThresholdParamsSchemaV1 },
+  'xpack.uptime.alerts.monitorStatus': {
+    title: 'Uptime monitor status',
+    params: uptimeMonitorStatusRuleParamsSchemaV1,
+  },
+  'xpack.uptime.alerts.tlsCertificate': {
+    title: 'Uptime TLS certificate',
+    params: uptimeTLSRuleParamsSchemaV1,
+  },
+  'xpack.uptime.alerts.durationAnomaly': {
+    title: 'Uptime duration anomaly',
+    params: uptimeDurationAnomalyRuleParamsSchemaV1,
+  },
+  'metrics.alert.inventory.threshold': {
+    title: 'Metric inventory threshold',
+    params: metricInventoryThresholdRuleParamsSchemaV1,
+  },
+  'metrics.alert.threshold': {
+    title: 'Metric threshold',
+    params: metricThresholdRuleParamsSchemaV1,
+  },
+  'observability.rules.custom_threshold': {
+    title: 'Custom threshold',
+    params: customThresholdParamsSchemaV1,
+  },
   'logs.alert.document.count': { title: 'Log threshold', params: logThresholdParamsSchemaV1 },
   'slo.rules.burnRate': { title: 'SLO burn rate', params: sloBurnRateParamsSchemaV1 },
 };
