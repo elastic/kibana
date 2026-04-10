1e35b0da83539d7a07dea3a25aa36ebdda056ee1
Panagiota Mitsopoulou <giota85@gmail.com>
2026-04-10 14:09:36 +0300

    Define CCS settings and remote monitor io-ts types
    Add runtime type definitions for cross-cluster search (CCS) settings
    and remote monitor info, in preparation for federated views support.
    
    Closes #259264

---
+++ x-pack/solutions/observability/plugins/synthetics/common/runtime_types/ccs_settings.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import * as t from 'io-ts';
+
+export const remoteSyntheticsClusterSchema = t.type({
+  name: t.string,
+  isConnected: t.boolean,
+});
+
+export const storedSyntheticsCCSSettingsSchema = t.intersection([
+  t.type({
+    useAllRemoteClusters: t.boolean,
+    selectedRemoteClusters: t.array(t.string),
+  }),
+  t.partial({
+    remoteKibanaUrls: t.record(t.string, t.string),
+  }),
+]);
+
+export const syntheticsCCSSettingsSchema = t.type({
+  useAllRemoteClusters: t.boolean,
+  selectedRemoteClusters: t.array(t.string),
+  remoteKibanaUrls: t.record(t.string, t.string),
+});
+
+export type RemoteSyntheticsCluster = t.TypeOf<typeof remoteSyntheticsClusterSchema>;
+export type StoredSyntheticsCCSSettings = t.TypeOf<typeof storedSyntheticsCCSSettingsSchema>;
+export type SyntheticsCCSSettings = t.TypeOf<typeof syntheticsCCSSettingsSchema>;
