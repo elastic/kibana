447f8a0c4b91c33af34fa019462ade78093f7565
Bailey Cash <bailey.cash@elastic.co>
2026-04-10 11:04:05 -0400

    rewire triggers actions rules links to unified rules

--- x-pack/solutions/security/plugins/security_solution_ess/public/navigation/navigation_tree.ts
+++ x-pack/solutions/security/plugins/security_solution_ess/public/navigation/navigation_tree.ts
@@ -212,7 +212,7 @@ export const createNavigationTree = (
           {
             title: i18nStrings.stackManagementV2.alertsAndInsights.title,
             children: [
-              { link: 'management:triggersActions' },
+              { id: 'stackRules', link: 'rules' },
               { link: 'management:cases' },
               { link: 'management:triggersActionsConnectors' },
               { link: 'management:reporting' },
