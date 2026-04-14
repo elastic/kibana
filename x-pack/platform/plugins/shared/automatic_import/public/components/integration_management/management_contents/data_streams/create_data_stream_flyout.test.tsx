cec2d6933713508f9c21b88a69ab75699e6b9038
Robert So (robester0403) <85914248+robester0403@users.noreply.github.com>
2026-04-14 04:45:13 -0400

    [Automatic Import] Added name checking for uploading packages (#262667)
    ## Summary
    
    Closes https://github.com/elastic/kibana/issues/262198
    
    This PR adds a check for uploaded packages so that the package upload
    shows the correct error and state when a conflicting name in Elastic
    Packages and Custom packages is detected.
    
    Existing name check function is refactored into the reusable library and
    exported from there.
    
    <img width="1217" height="287" alt="image"
    src="https://github.com/user-attachments/assets/efd57fef-0d1f-4e7d-bc07-9f71e3786cba"
    />
    
    This PR addresses:
    https://github.com/elastic/kibana/issues/262198
    
    ### Checklist
    
    Check the PR satisfies following conditions.
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [x] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    ~~- [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials~~
    - [x] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    ~~- [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)~~
    ~~- [ ] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.~~
    ~~- [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed~~
    ~~- [ ] The PR description includes the appropriate Release Notes
    section, and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)~~
    ~~- [ ] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.~~
    
    (cherry picked from commit 81c6feb671c1f841cd8d8692e7d0cabd59c311b5)

--- x-pack/platform/plugins/shared/automatic_import/public/components/integration_management/management_contents/data_streams/create_data_stream_flyout.test.tsx
+++ x-pack/platform/plugins/shared/automatic_import/public/components/integration_management/management_contents/data_streams/create_data_stream_flyout.test.tsx
@@ -52,6 +52,7 @@ jest.mock('../../../../common/lib/api', () => ({
       items: [],
     })
   ),
+  getAllIntegrations: jest.fn(() => Promise.resolve([])),
 }));
 
 const mockReportAnalyzeLogsTriggered = jest.fn();
@@ -300,7 +301,6 @@ describe('CreateDataStreamFlyout', () => {
         expect(getByTestId('logsSourceUploadCard')).toBeInTheDocument();
       });
 
-      // Upload card should be checked by default
       const uploadCard = getByTestId('logsSourceUploadCard');
       const radio = uploadCard.querySelector('input[type="radio"]');
       expect(radio).toBeChecked();
@@ -318,12 +318,10 @@ describe('CreateDataStreamFlyout', () => {
         expect(getByTestId('logsSourceIndexCard')).toBeInTheDocument();
       });
 
-      // Click the index source card
       const indexCard = getByTestId('logsSourceIndexCard');
       const radio = indexCard.querySelector('input[type="radio"]');
       fireEvent.click(radio!);
 
-      // Index card should now be checked
       expect(radio).toBeChecked();
     });
 
@@ -339,16 +337,13 @@ describe('CreateDataStreamFlyout', () => {
         expect(getByTestId('logsSourceIndexCard')).toBeInTheDocument();
       });
 
-      // Index select should be disabled initially (upload is default)
       const indexSelect = getByTestId('indexSelect');
       expect(indexSelect.querySelector('[data-test-subj="comboBoxSearchInput"]')).toBeDisabled();
 
-      // Click the index source card
       const indexCard = getByTestId('logsSourceIndexCard');
       const radio = indexCard.querySelector('input[type="radio"]');
       fireEvent.click(radio!);
 
-      // Index select should now be enabled
       await waitFor(() => {
         expect(
           indexSelect.querySelector('[data-test-subj="comboBoxSearchInput"]')
@@ -377,12 +372,10 @@ describe('CreateDataStreamFlyout', () => {
         expect(getByTestId('logsSourceIndexCard')).toBeInTheDocument();
       });
 
-      // Switch to index source
       const indexCard = getByTestId('logsSourceIndexCard');
       const radio = indexCard.querySelector('input[type="radio"]');
       fireEvent.click(radio!);
 
-      // Error should be displayed
       await waitFor(() => {
         expect(getByText('Index is missing event.original field')).toBeInTheDocument();
       });
@@ -410,12 +403,10 @@ describe('CreateDataStreamFlyout', () => {
         expect(getByTestId('logsSourceIndexCard')).toBeInTheDocument();
       });
 
-      // Switch to index source
       const indexCard = getByTestId('logsSourceIndexCard');
       const radio = indexCard.querySelector('input[type="radio"]');
       fireEvent.click(radio!);
 
-      // The combobox should show loading state
       const indexSelect = getByTestId('indexSelect');
       expect(indexSelect.querySelector('.euiLoadingSpinner')).toBeInTheDocument();
     });
@@ -439,12 +430,10 @@ describe('CreateDataStreamFlyout', () => {
         expect(getByTestId('logsSourceIndexCard')).toBeInTheDocument();
       });
 
-      // Switch to index source
       const indexCard = getByTestId('logsSourceIndexCard');
       const radio = indexCard.querySelector('input[type="radio"]');
       fireEvent.click(radio!);
 
-      // The combobox should show loading state
       const indexSelect = getByTestId('indexSelect');
       expect(indexSelect.querySelector('.euiLoadingSpinner')).toBeInTheDocument();
     });
