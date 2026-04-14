573c55b66d91e5fea79972eb852f36897f369aa8
Tamerlan <tamerlan.gudabayev@elastic.co>
2026-04-14 15:21:25 +0200

    get the metadata

--- .buildkite/scripts/steps/es_snapshots/build.sh
+++ .buildkite/scripts/steps/es_snapshots/build.sh
@@ -125,20 +125,7 @@ cat << EOF | buildkite-agent annotate --style "info"
 EOF
 
 if [ "$BUILDKITE_TRIGGERED_FROM_BUILD_PIPELINE_SLUG" = "kibana-version-bump" ]; then
-  echo "--- Uploading snapshot metadata for use in version bump pipeline"
-  ES_SNAPSHOT_VERSION=$(buildkite-agent meta-data get ES_SNAPSHOT_VERSION)
-  ES_SNAPSHOT_ID=$(buildkite-agent meta-data get ES_SNAPSHOT_ID)
-  JSON_FILE="snapshot_info-${BUILDKITE_TRIGGERED_FROM_BUILD_ID}.json"
-
-  cat > "snapshot_info-${BUILDKITE_TRIGGERED_FROM_BUILD_ID}.env" << EOF
-  ES_SNAPSHOT_MANIFEST=$ES_SNAPSHOT_MANIFEST
-  ES_SNAPSHOT_VERSION=$(buildkite-agent meta-data get ES_SNAPSHOT_VERSION)
-  ES_SNAPSHOT_ID=$(buildkite-agent meta-data get ES_SNAPSHOT_ID)
-  ELASTICSEARCH_BRANCH=$ELASTICSEARCH_BRANCH
-  ELASTICSEARCH_GIT_COMMIT=$ELASTICSEARCH_GIT_COMMIT
-  EOF
-
-  buildkite-agent artifact upload "$JSON_FILE"
+  buildkite-agent meta-data set es_snapshot_manifest "$ES_SNAPSHOT_MANIFEST" --job "$PARENT_TRIGGER_JOB_ID"
 fi
 
 cat << EOF | buildkite-agent pipeline upload
