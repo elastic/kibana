dc3148e9c3a2d283174ab854eea7a3cdb4d0637b
Timothy Sullivan <tsullivan@elastic.co>
2026-04-15 08:39:35 -0700

    update test mocks, assertions, and descriptions

--- src/core/packages/i18n/server-internal/src/i18n_service.test.ts
+++ src/core/packages/i18n/server-internal/src/i18n_service.test.ts
@@ -8,7 +8,7 @@
  */
 
 import {
-  getKibanaTranslationFilesMock,
+  getAllKibanaTranslationFilesMock,
   initTranslationsMock,
   registerRoutesMock,
 } from './i18n_service.test.mocks';
@@ -55,19 +55,19 @@ describe('I18nService', () => {
   });
 
   describe('#preboot', () => {
-    it('calls `getKibanaTranslationFiles` with the correct parameters', async () => {
-      getKibanaTranslationFilesMock.mockResolvedValue([]);
+    it('calls `getAllKibanaTranslationFiles` with the correct parameters', async () => {
+      getAllKibanaTranslationFilesMock.mockResolvedValue([]);
 
       const pluginPaths = ['/pathA', '/pathB'];
       await service.preboot({ pluginPaths, http: httpPreboot });
 
-      expect(getKibanaTranslationFilesMock).toHaveBeenCalledTimes(1);
-      expect(getKibanaTranslationFilesMock).toHaveBeenCalledWith('en', pluginPaths);
+      expect(getAllKibanaTranslationFilesMock).toHaveBeenCalledTimes(1);
+      expect(getAllKibanaTranslationFilesMock).toHaveBeenCalledWith(pluginPaths);
     });
 
     it('calls `initTranslations` with the correct parameters', async () => {
       const translationFiles = ['/path/to/file', 'path/to/another/file'];
-      getKibanaTranslationFilesMock.mockResolvedValue(translationFiles);
+      getAllKibanaTranslationFilesMock.mockResolvedValue(translationFiles);
 
       await service.preboot({ pluginPaths: [], http: httpPreboot });
 
@@ -83,7 +83,7 @@ describe('I18nService', () => {
         locale: 'en',
         router: expect.any(Object),
         isDist: coreContext.env.packageInfo.dist,
-        translationHash: expect.any(String),
+        translationHashes: expect.any(Object),
       });
     });
   });
@@ -93,24 +93,24 @@ describe('I18nService', () => {
       await service.preboot({ pluginPaths: ['/pathPrebootA'], http: httpPreboot });
 
       // Reset mocks that were used in the `preboot`.
-      getKibanaTranslationFilesMock.mockClear();
+      getAllKibanaTranslationFilesMock.mockClear();
       initTranslationsMock.mockClear();
       registerRoutesMock.mockClear();
     });
 
-    it('calls `getKibanaTranslationFiles` with the correct parameters', async () => {
-      getKibanaTranslationFilesMock.mockResolvedValue([]);
+    it('calls `getAllKibanaTranslationFiles` with the correct parameters', async () => {
+      getAllKibanaTranslationFilesMock.mockResolvedValue([]);
 
       const pluginPaths = ['/pathA', '/pathB'];
       await service.setup({ pluginPaths, http: httpSetup });
 
-      expect(getKibanaTranslationFilesMock).toHaveBeenCalledTimes(1);
-      expect(getKibanaTranslationFilesMock).toHaveBeenCalledWith('en', pluginPaths);
+      expect(getAllKibanaTranslationFilesMock).toHaveBeenCalledTimes(1);
+      expect(getAllKibanaTranslationFilesMock).toHaveBeenCalledWith(pluginPaths);
     });
 
     it('calls `initTranslations` with the correct parameters', async () => {
       const translationFiles = ['/path/to/file', 'path/to/another/file'];
-      getKibanaTranslationFilesMock.mockResolvedValue(translationFiles);
+      getAllKibanaTranslationFilesMock.mockResolvedValue(translationFiles);
 
       await service.setup({ pluginPaths: [], http: httpSetup });
 
@@ -126,13 +126,13 @@ describe('I18nService', () => {
         locale: 'en',
         router: expect.any(Object),
         isDist: coreContext.env.packageInfo.dist,
-        translationHash: expect.any(String),
+        translationHashes: expect.any(Object),
       });
     });
 
     it('returns accessors for locale and translation files', async () => {
       const translationFiles = ['/path/to/file', 'path/to/another/file'];
-      getKibanaTranslationFilesMock.mockResolvedValue(translationFiles);
+      getAllKibanaTranslationFilesMock.mockResolvedValue(translationFiles);
 
       const { getLocale, getTranslationFiles } = await service.setup({
         pluginPaths: [],
