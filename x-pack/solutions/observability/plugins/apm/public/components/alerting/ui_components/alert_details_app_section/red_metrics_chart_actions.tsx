88391d59c50f0c50a6a0a07131db5646addabde8
TFCMarques <tiago.marques@elastic.co>
2026-04-15 16:38:19 +0100

    guard chart actions for apm context availability

--- x-pack/solutions/observability/plugins/apm/public/components/alerting/ui_components/alert_details_app_section/red_metrics_chart_actions.tsx
+++ x-pack/solutions/observability/plugins/apm/public/components/alerting/ui_components/alert_details_app_section/red_metrics_chart_actions.tsx
@@ -9,6 +9,8 @@ import React, { useMemo, useState } from 'react';
 import { EuiButtonEmpty, EuiContextMenuItem, EuiContextMenuPanel, EuiPopover } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { DISCOVER_APP_LOCATOR } from '@kbn/deeplinks-analytics';
+import type { ApmSourceAccessPluginStart } from '@kbn/apm-sources-access-plugin/public';
+import type { LocatorPublic } from '@kbn/share-plugin/common';
 import { useKibana } from '@kbn/kibana-react-plugin/public';
 import { FETCH_STATUS, useFetcher } from '../../../../hooks/use_fetcher';
 import type { ApmPluginStartDeps } from '../../../../plugin';
@@ -40,17 +42,39 @@ interface RedMetricsChartActionsProps {
   ruleTypeId?: string;
 }
 
-export function RedMetricsChartActions({
-  queryParams,
-  timeRange,
-  ruleTypeId,
-}: RedMetricsChartActionsProps) {
+export function RedMetricsChartActions(props: RedMetricsChartActionsProps) {
   const {
     services: { share, apmSourcesAccess },
   } = useKibana<ApmPluginStartDeps>();
 
+  const apmLocator = share?.url?.locators?.get(APM_APP_LOCATOR_ID);
+
+  if (!apmLocator || !apmSourcesAccess) return null;
+
+  return (
+    <RedMetricsChartActionsPopover
+      {...props}
+      apmLocator={apmLocator}
+      apmSourcesAccess={apmSourcesAccess}
+      share={share}
+    />
+  );
+}
+
+function RedMetricsChartActionsPopover({
+  queryParams,
+  timeRange,
+  ruleTypeId,
+  apmLocator,
+  apmSourcesAccess,
+  share,
+}: RedMetricsChartActionsProps & {
+  apmLocator: LocatorPublic<any>;
+  apmSourcesAccess: ApmSourceAccessPluginStart;
+  share: ApmPluginStartDeps['share'];
+}) {
   const { data, status: indexSettingsStatus } = useFetcher(
-    (_, signal) => apmSourcesAccess?.getApmIndexSettings({ signal }),
+    (_, signal) => apmSourcesAccess.getApmIndexSettings({ signal }),
     [apmSourcesAccess]
   );
 
@@ -61,9 +85,7 @@ export function RedMetricsChartActions({
   const { serviceName, ...queryForApm } = queryParams;
 
   const apmLink = useMemo(() => {
-    const apmLocator = share?.url?.locators?.get(APM_APP_LOCATOR_ID);
-
-    return apmLocator?.getRedirectUrl({
+    return apmLocator.getRedirectUrl({
       serviceName,
       serviceOverviewTab: queryParams.transactionName ? 'transactions' : undefined,
       query: {
@@ -72,7 +94,7 @@ export function RedMetricsChartActions({
         rangeTo: timeRange.to,
       },
     });
-  }, [share, serviceName, queryParams.transactionName, queryForApm, timeRange]);
+  }, [apmLocator, serviceName, queryParams.transactionName, queryForApm, timeRange]);
 
   const discoverLink = useMemo(() => {
     if (indexSettingsStatus !== FETCH_STATUS.SUCCESS) return undefined;
