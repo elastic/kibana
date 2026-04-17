00eda2891ef68bbb9c8b5ee9608eedf511f52b81
Julia <iuliia.guskova@elastic.co>
2026-04-17 15:06:38 +0200

    reverse the change

--- x-pack/platform/plugins/shared/cases/public/components/attachments/lens/index.test.tsx
+++ x-pack/platform/plugins/shared/cases/public/components/attachments/lens/index.test.tsx
@@ -130,7 +130,7 @@ describe('getVisualizationAttachmentType', () => {
       );
 
       await waitFor(() => {
-        expect(screen.getByTestId('embeddableComponent')).toBeInTheDocument();
+        expect(screen.getByTestId('embeddableComponent'));
       });
     });
   });
