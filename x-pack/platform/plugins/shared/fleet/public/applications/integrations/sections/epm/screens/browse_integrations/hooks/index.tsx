43de03ba8d395675304e994ec249de2e14124e00
Jen Huang <its.jenetic@gmail.com>
2026-04-17 11:42:23 -0700

    Fix type check

--- x-pack/platform/plugins/shared/fleet/public/applications/integrations/sections/epm/screens/browse_integrations/hooks/index.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/integrations/sections/epm/screens/browse_integrations/hooks/index.tsx
@@ -25,7 +25,6 @@ export function useBrowseIntegrationHook({
   const {
     initialSelectedCategory,
     allCategories,
-    mainCategories,
     isLoading,
     isLoadingCategories,
     isLoadingAllPackages,
