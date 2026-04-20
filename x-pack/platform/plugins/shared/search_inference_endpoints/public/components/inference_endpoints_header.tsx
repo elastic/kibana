bca3a96d9ac5bba909f5ecd5366969ad19258300
Efe Gürkan YALAMAN <efeguerkan.yalaman@elastic.co>
2026-04-20 13:33:15 +0200

    Move all i18n inline (#262363)
    ## Summary
    
    Moves i18n labels to inline as this is the pattern we use everywhere
    else in Search
    
    ### Checklist
    
    Check the PR satisfies following conditions.
    
    Reviewers should verify this PR satisfies this list as well.
    
    - [ ] Any text added follows [EUI's writing
    guidelines](https://elastic.github.io/eui/#/guidelines/writing), uses
    sentence case text and includes [i18n
    support](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-i18n/README.md)
    - [ ]
    [Documentation](https://www.elastic.co/guide/en/kibana/master/development-documentation.html)
    was added for features that require explanation or tutorials
    - [ ] [Unit or functional
    tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
    were updated or added to match the most common scenarios
    - [ ] If a plugin configuration key changed, check if it needs to be
    allowlisted in the cloud and added to the [docker
    list](https://github.com/elastic/kibana/blob/main/src/dev/build/tasks/os_packages/docker_generator/resources/base/bin/kibana-docker)
    - [ ] This was checked for breaking HTTP API changes, and any breaking
    changes have been approved by the breaking-change committee. The
    `release_note:breaking` label should be applied in these situations.
    - [ ] [Flaky Test
    Runner](https://ci-stats.kibana.dev/trigger_flaky_test_runner/1) was
    used on any tests changed
    - [ ] The PR description includes the appropriate Release Notes section,
    and the correct `release_note:*` label is applied per the
    [guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
    - [ ] Review the [backport
    guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
    and apply applicable `backport:*` labels.
    
    ### Identify risks
    
    Does this PR introduce any risks? For example, consider risks like hard
    to test bugs, performance regression, potential of data loss.
    
    Describe the risk, its severity, and mitigation for each identified
    risk. Invite stakeholders and evaluate how to proceed before merging.
    
    - [ ] [See some risk
    examples](https://github.com/elastic/kibana/blob/main/RISK_MATRIX.mdx)
    - [ ] ...
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Elastic Machine <elasticmachine@users.noreply.github.com>
    (cherry picked from commit 4c38614cb4b4ca05bed17092a64c71e03db6320c)
    
    # Conflicts:
    #	x-pack/platform/plugins/private/translations/translations/de-DE.json
    #	x-pack/platform/plugins/private/translations/translations/fr-FR.json
    #	x-pack/platform/plugins/private/translations/translations/ja-JP.json
    #	x-pack/platform/plugins/private/translations/translations/zh-CN.json
    #	x-pack/platform/plugins/shared/search_inference_endpoints/common/constants.ts
    #	x-pack/platform/plugins/shared/search_inference_endpoints/public/components/settings/add_model_popover.tsx
    #	x-pack/platform/plugins/shared/search_inference_endpoints/public/components/settings/sub_feature_card.tsx
    #	x-pack/platform/plugins/shared/search_inference_endpoints/public/plugin.ts

--- x-pack/platform/plugins/shared/search_inference_endpoints/public/components/inference_endpoints_header.tsx
+++ x-pack/platform/plugins/shared/search_inference_endpoints/public/components/inference_endpoints_header.tsx
@@ -7,10 +7,9 @@
 
 import { EuiPageTemplate, EuiButton, EuiLink } from '@elastic/eui';
 import React from 'react';
-import * as i18n from '../../common/translations';
-import { PLUGIN_TITLE } from '../../common/constants';
 import { docLinks } from '../../common/doc_links';
 import { useKibana } from '../hooks/use_kibana';
+import { i18n } from '@kbn/i18n';
 
 interface InferenceEndpointsHeaderProps {
   onFlyoutOpen: () => void;
@@ -25,44 +24,91 @@ export const InferenceEndpointsHeader: React.FC<InferenceEndpointsHeaderProps> =
   return (
     <EuiPageTemplate.Header
       data-test-subj="allInferenceEndpointsPage"
-      pageTitle={PLUGIN_TITLE}
-      description={i18n.MANAGE_INFERENCE_ENDPOINTS_LABEL}
+      pageTitle={i18n.translate(
+        'xpack.searchInferenceEndpoints.inferenceEndpointsTitle',
+        {
+          defaultMessage: 'Inference endpoints',
+        }
+      )}
+      description={i18n.translate(
+        'xpack.searchInferenceEndpoints.allInferenceEndpoints.description',
+        {
+          defaultMessage:
+            'Inference endpoints streamline the deployment and management of machine\nlearning models in Elasticsearch. Set up and manage NLP tasks using unique\nendpoints, to build AI-powered search.',
+        }
+      )}
       bottomBorder={true}
-      rightSideItems={[
-        <EuiButton
-          iconType="plusCircle"
-          fill
-          data-test-subj="add-inference-endpoint-header-button"
-          onClick={onFlyoutOpen}
-        >
-          {i18n.ADD_ENDPOINT_LABEL}
-        </EuiButton>,
-        <EuiLink
-          aria-label={i18n.API_DOCUMENTATION_LINK}
-          target="_blank"
-          data-test-subj="api-documentation"
-          href={docLinks.createInferenceEndpoint}
-          external
-        >
-          {i18n.API_DOCUMENTATION_LINK}
-        </EuiLink>,
-        <EuiLink
-          aria-label={i18n.VIEW_YOUR_MODELS_LINK}
-          onClick={() => application.navigateToApp('ml', { path: 'trained_models' })}
-          data-test-subj="view-your-models"
-        >
-          {i18n.VIEW_YOUR_MODELS_LINK}
-        </EuiLink>,
-        <EuiLink
-          aria-label={i18n.EIS_DOCUMENTATION_LINK}
-          href={docLinks.elasticInferenceService}
-          target="_blank"
-          data-test-subj="eis-documentation"
-          external
-        >
-          {i18n.EIS_DOCUMENTATION_LINK}
-        </EuiLink>,
-      ]}
+      rightSideItems={
+        [
+          <EuiButton
+            iconType="plusCircle"
+            fill
+            data-test-subj="add-inference-endpoint-header-button"
+            onClick={onFlyoutOpen}
+          >
+            {i18n.translate(
+              'xpack.searchInferenceEndpoints.addConnectorButtonLabel',
+              {
+                defaultMessage: 'Add endpoint',
+              }
+            )}
+          </EuiButton>,
+          <EuiLink
+            aria-label={i18n.translate(
+              'xpack.searchInferenceEndpoints.apiDocumentationLink',
+              {
+                defaultMessage: 'API Documentation',
+              }
+            )}
+            target="_blank"
+            data-test-subj="api-documentation"
+            href={docLinks.createInferenceEndpoint}
+            external
+          >
+            {i18n.translate(
+              'xpack.searchInferenceEndpoints.apiDocumentationLink',
+              {
+                defaultMessage: 'API Documentation',
+              }
+            )}
+          </EuiLink>,
+          <EuiLink
+            aria-label={i18n.translate(
+              'xpack.searchInferenceEndpoints.viewYourModels',
+              {
+                defaultMessage: 'ML Trained Models',
+              }
+            )}
+            onClick={() => application.navigateToApp('ml', { path: 'trained_models' })}
+            data-test-subj="view-your-models"
+          >
+            {i18n.translate(
+              'xpack.searchInferenceEndpoints.viewYourModels',
+              {
+                defaultMessage: 'ML Trained Models',
+              }
+            )}
+          </EuiLink>,
+          <EuiLink
+            aria-label={i18n.translate(
+              'xpack.searchInferenceEndpoints.eisDocumentationLink',
+              {
+                defaultMessage: 'Elastic Inference Service',
+              }
+            )}
+            href={docLinks.elasticInferenceService}
+            target="_blank"
+            data-test-subj="eis-documentation"
+            external
+          >
+            {i18n.translate(
+              'xpack.searchInferenceEndpoints.eisDocumentationLink',
+              {
+                defaultMessage: 'Elastic Inference Service',
+              }
+            )}
+          </EuiLink>,
+        ]}
     />
   );
 };
