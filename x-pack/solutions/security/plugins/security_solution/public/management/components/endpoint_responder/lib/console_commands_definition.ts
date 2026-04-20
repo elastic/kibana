62ddb4b016852b9b3def5d0c8dc099c608f75b13
Paul Tavares <paul.tavares@elastic.co>
2026-04-20 16:12:40 -0400

    Fix `memory-dump` descriptions of `--kernel` and `--process`

--- x-pack/solutions/security/plugins/security_solution/public/management/components/endpoint_responder/lib/console_commands_definition.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/components/endpoint_responder/lib/console_commands_definition.ts
@@ -785,7 +785,11 @@ export const getEndpointConsoleCommands = ({
       mustHaveArgs: true,
       args: {
         process: {
-          about: CONSOLE_COMMANDS.memoryDump.processArgAbout,
+          about:
+            CONSOLE_COMMANDS.memoryDump.processArgAbout +
+            (endpointSupportsProcessDump
+              ? ''
+              : ` (${CONSOLE_COMMANDS.memoryDump.argNotSupported})`),
           required: false,
           allowMultiples: false,
           mustHaveValue: false,
@@ -799,7 +803,9 @@ export const getEndpointConsoleCommands = ({
           },
         },
         kernel: {
-          about: CONSOLE_COMMANDS.memoryDump.kernelArgAbout,
+          about:
+            CONSOLE_COMMANDS.memoryDump.kernelArgAbout +
+            (endpointSupportsKernelDump ? '' : ` (${CONSOLE_COMMANDS.memoryDump.argNotSupported})`),
           required: false,
           allowMultiples: false,
           mustHaveValue: false,
