0b14682f9f4e2b1e72d1a06c279ad271554ef3dc
Kenneth Kreindler <kenneth.kreindler@elastic.co>
2026-04-21 13:49:37 +0200

    fix feature flag defaulta

--- x-pack/solutions/security/plugins/security_solution/common/constants.ts
+++ x-pack/solutions/security/plugins/security_solution/common/constants.ts
@@ -727,10 +727,6 @@ export const SECURITY_RULE_ATTACHMENT_ID = 'ai-rule-creation';
 
 export const THREAT_HUNTING_AGENT_ID = `${internalNamespaces.security}.agent`;
 
-export const PREINSTALLED_WORKFLOWS_FEATURE_FLAG =
-  'securitySolution.preinstalledWorkflowsEnabled' as const;
-export const PREINSTALLED_WORKFLOWS_FEATURE_FLAG_DEFAULT = true as const;
-
 export const REGISTER_ALERT_VALIDATION_STEPS_FEATURE_FLAG =
   'securitySolution.registerAlertValidationStepsEnabled' as const;
-export const REGISTER_ALERT_VALIDATION_STEP_FEATURE_FLAG_DEFAULT = true as const;
+export const REGISTER_ALERT_VALIDATION_STEP_FEATURE_FLAG_DEFAULT = false as const;
