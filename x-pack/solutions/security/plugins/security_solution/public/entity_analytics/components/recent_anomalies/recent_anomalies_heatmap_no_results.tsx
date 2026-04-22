f8c7609f688bc6ca7eba90e5a6f95dab0487b512
CAWilson94 <charlotte.wilson@elastic.co>
2026-04-22 20:53:56 +0100

    Update anomaly bands colors to use EUI theme and integrate elastic charts theme in anomaly heatmap component
    - Changed the color for the first anomaly band to use `euiTheme.colors.severity.unknown` for better alignment with ML usage.
    - Imported `useElasticChartsTheme` in the `anomaly_heatmap` component and applied it to the chart settings for consistent theming.
    - Added a `noResultsComponent` to the `RecentAnomaliesChart` to handle cases where no data is available, improving user experience.

---
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/recent_anomalies/recent_anomalies_heatmap_no_results.tsx
@@ -0,0 +1,49 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { EuiFlexGroup, EuiFlexItem, EuiImage, EuiText, EuiTitle } from '@elastic/eui';
+import { FormattedMessage } from '@kbn/i18n-react';
+import { i18n } from '@kbn/i18n';
+import React from 'react';
+import illustration from '../../../common/images/illustration_product_no_results_magnifying_glass.svg';
+
+export const RecentAnomaliesHeatmapNoResults: React.FC = () => {
+  return (
+    <EuiFlexGroup css={{ maxWidth: '600px' }}>
+      <EuiFlexItem>
+        <EuiText size="s">
+          <EuiTitle>
+            <h3>
+              <FormattedMessage
+                id="xpack.securitySolution.entityAnalytics.recentAnomalies.noResultsTitle"
+                defaultMessage="No anomaly results match your search criteria"
+              />
+            </h3>
+          </EuiTitle>
+          <p>
+            <FormattedMessage
+              id="xpack.securitySolution.entityAnalytics.recentAnomalies.noResultsDescription"
+              defaultMessage={`To get started, click "ML job settings" above to configure and run anomaly detection jobs within your environment.`}
+            />
+          </p>
+        </EuiText>
+      </EuiFlexItem>
+      <EuiFlexItem grow={false}>
+        <EuiImage
+          size="200px"
+          alt={i18n.translate(
+            'xpack.securitySolution.entityAnalytics.recentAnomalies.emptyState.illustrationAlt',
+            {
+              defaultMessage: 'No results',
+            }
+          )}
+          url={illustration}
+        />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+};
