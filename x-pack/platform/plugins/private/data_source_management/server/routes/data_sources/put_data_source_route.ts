6924162f443f12b6d0e1b811a0a2e518d2645889
Matthew Kime <matt@mattki.me>
2026-04-22 15:11:21 -0500

    add create and delete endpoints

---
+++ x-pack/platform/plugins/private/data_source_management/server/routes/data_sources/put_data_source_route.ts
@@ -0,0 +1,54 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { schema } from '@kbn/config-schema';
+import type { IRouter } from '@kbn/core/server';
+import { i18n } from '@kbn/i18n';
+
+import type { DataSource } from '../../../common/types';
+import { DATA_SOURCE_BY_ID_ROUTE_PATH } from '../../../common';
+import { DataSourcesClient } from '../../data_sources_client';
+
+const putDataSourceBodySchema = schema.object({
+  type: schema.string(),
+  description: schema.string(),
+});
+
+export function registerPutDataSourceRoute(router: IRouter): void {
+  router.put(
+    {
+      path: DATA_SOURCE_BY_ID_ROUTE_PATH,
+      security: {
+        authz: {
+          enabled: false,
+          reason:
+            'This route is opted out from authorization because permissions will be checked by elasticsearch',
+        },
+      },
+      options: {
+        access: 'internal',
+        summary: i18n.translate('dataSourceManagement.routes.putDataSource.summary', {
+          defaultMessage: 'Create or update a data source',
+        }),
+      },
+      validate: {
+        params: schema.object({
+          id: schema.string(),
+        }),
+        body: putDataSourceBodySchema,
+      },
+    },
+    router.handleLegacyErrors(async (context, request, response) => {
+      const { id } = request.params;
+      const payload: DataSource = request.body;
+      const { client } = (await context.core).elasticsearch;
+      const dataSourcesClient = new DataSourcesClient(client.asCurrentUser);
+      const body = await dataSourcesClient.put(id, payload);
+      return response.ok({ body });
+    })
+  );
+}
