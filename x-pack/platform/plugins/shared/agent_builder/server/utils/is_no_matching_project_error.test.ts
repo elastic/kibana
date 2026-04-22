c2fb2c171b1c75646a8285afcae2f7ef3a06b57e
Arturo Lidueña <arturo.liduena@elastic.co>
2026-04-22 17:18:28 +0200

    update isIndexNotFoundError and isNoMatchingProjectError tests

---
+++ x-pack/platform/plugins/shared/agent_builder/server/utils/is_no_matching_project_error.test.ts
@@ -0,0 +1,72 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { isNoMatchingProjectError } from './is_no_matching_project_error';
+
+describe('isNoMatchingProjectError', () => {
+  it('returns true for error with caused_by.type = no_matching_project_exception', () => {
+    const error = {
+      attributes: {
+        caused_by: {
+          type: 'no_matching_project_exception',
+        },
+      },
+    };
+    expect(isNoMatchingProjectError(error)).toBe(true);
+  });
+
+  it('returns true for error with error.caused_by.type = no_matching_project_exception', () => {
+    const error = {
+      attributes: {
+        error: {
+          caused_by: {
+            type: 'no_matching_project_exception',
+          },
+        },
+      },
+    };
+    expect(isNoMatchingProjectError(error)).toBe(true);
+  });
+
+  it('returns true for error with meta.body.error.type = no_matching_project_exception', () => {
+    const error = {
+      meta: {
+        body: {
+          error: {
+            type: 'no_matching_project_exception',
+          },
+        },
+      },
+    };
+    expect(isNoMatchingProjectError(error)).toBe(true);
+  });
+
+  it('returns true for error with message containing no_matching_project_exception', () => {
+    const error = {
+      message: 'no_matching_project_exception: no matching project',
+    };
+    expect(isNoMatchingProjectError(error)).toBe(true);
+  });
+
+  it('returns false for index_not_found_exception', () => {
+    const error = {
+      meta: {
+        body: {
+          error: {
+            type: 'index_not_found_exception',
+          },
+        },
+      },
+    };
+    expect(isNoMatchingProjectError(error)).toBe(false);
+  });
+
+  it('returns false for null and undefined', () => {
+    expect(isNoMatchingProjectError(null)).toBe(false);
+    expect(isNoMatchingProjectError(undefined)).toBe(false);
+  });
+});
