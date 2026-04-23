f04d28f56cf9c76b87b73f89925cb79cfa90f381
iblancof <irene.blanco@elastic.co>
2026-04-23 11:51:50 +0200

    Create TraceWaterfallFlyoutFooter

---
+++ x-pack/solutions/observability/plugins/apm/public/components/app/transaction_details/waterfall_with_summary/trace_waterfall_flyout/flyout_footer.tsx
@@ -0,0 +1,117 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import {
+  EuiButton,
+  EuiContextMenuItem,
+  EuiContextMenuPanel,
+  EuiFlexGroup,
+  EuiFlexItem,
+  EuiFlyoutFooter,
+  EuiPopover,
+} from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import { TRANSACTION_DETAILS_BY_TRACE_ID_LOCATOR } from '@kbn/deeplinks-observability/locators';
+import React, { useState } from 'react';
+import { useApmPluginContext } from '../../../../../context/apm_plugin/use_apm_plugin_context';
+import { useDiscoverHref } from '../../../../shared/links/discover_links/use_discover_href';
+
+interface Props {
+  traceId: string;
+  rangeFrom: string;
+  rangeTo: string;
+}
+
+export function TraceWaterfallFlyoutFooter({ traceId, rangeFrom, rangeTo }: Props) {
+  const { share } = useApmPluginContext();
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+
+  const discoverHref = useDiscoverHref({
+    indexType: 'traces',
+    rangeFrom,
+    rangeTo,
+    queryParams: { traceId },
+  });
+
+  const apmHref = share.url.locators
+    .get<{ traceId: string; rangeFrom: string; rangeTo: string }>(
+      TRANSACTION_DETAILS_BY_TRACE_ID_LOCATOR
+    )
+    ?.getRedirectUrl({ traceId, rangeFrom, rangeTo });
+
+  if (!discoverHref && !apmHref) {
+    return null;
+  }
+
+  return (
+    <EuiFlyoutFooter>
+      <EuiFlexGroup justifyContent="flexEnd">
+        <EuiFlexItem grow={false}>
+          <EuiPopover
+            button={
+              <EuiButton
+                fill
+                iconType="arrowDown"
+                iconSide="right"
+                size="s"
+                data-test-subj="apmTraceWaterfallFlyoutActionsButton"
+                onClick={() => setIsPopoverOpen(!isPopoverOpen)}
+              >
+                {i18n.translate('xpack.apm.traceWaterfallFlyoutFooter.actionsButton', {
+                  defaultMessage: 'Actions',
+                })}
+              </EuiButton>
+            }
+            isOpen={isPopoverOpen}
+            closePopover={() => setIsPopoverOpen(false)}
+            panelPaddingSize="none"
+            anchorPosition="upRight"
+            aria-label={i18n.translate(
+              'xpack.apm.traceWaterfallFlyoutFooter.actionsPopoverAriaLabel',
+              {
+                defaultMessage: 'Actions for full trace',
+              }
+            )}
+          >
+            <EuiContextMenuPanel
+              items={[
+                ...(discoverHref
+                  ? [
+                      <EuiContextMenuItem
+                        key="discover"
+                        data-test-subj="apmTraceWaterfallOpenInDiscover"
+                        href={discoverHref}
+                        onClick={() => setIsPopoverOpen(false)}
+                      >
+                        {i18n.translate('xpack.apm.traceWaterfallFlyoutFooter.openInDiscover', {
+                          defaultMessage: 'Open in Discover',
+                        })}
+                      </EuiContextMenuItem>,
+                    ]
+                  : []),
+                ...(apmHref
+                  ? [
+                      <EuiContextMenuItem
+                        key="apm"
+                        data-test-subj="apmTraceWaterfallOpenInApm"
+                        href={apmHref}
+                        onClick={() => setIsPopoverOpen(false)}
+                      >
+                        {i18n.translate('xpack.apm.traceWaterfallFlyoutFooter.openInApm', {
+                          defaultMessage: 'Open in APM',
+                        })}
+                      </EuiContextMenuItem>,
+                    ]
+                  : []),
+              ]}
+            />
+          </EuiPopover>
+        </EuiFlexItem>
+      </EuiFlexGroup>
+    </EuiFlyoutFooter>
+  );
+}
