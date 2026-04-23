5ed0144aca89d8cfda391ee63287a810d524d24c
Cesare de Cal <cesare.decal@elastic.co>
2026-04-23 10:10:30 +0200

    [Scout] Use Kibana's `babel-register` when running Playwright (#264927)
    We wrap Playwright calls in Scout with `@kbn/babel-register/install` so
    specs and their imports transpile through Kibana's Babel config. This
    matches what we already
    [do](https://github.com/elastic/kibana/blob/main/src/platform/packages/shared/kbn-dev-utils/src/worker/index.ts#L37)
    for forked TS workers.
    
    (cherry picked from commit 1753e986e4c4ab335459e4de1d11464011fe103f)

--- src/platform/packages/shared/kbn-scout/src/playwright/utils/index.ts
+++ src/platform/packages/shared/kbn-scout/src/playwright/utils/index.ts
@@ -7,6 +7,12 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export { isValidUTCDate, formatTime, getPlaywrightGrepTag, execPromise } from './runner_utils';
+export {
+  isValidUTCDate,
+  formatTime,
+  getPlaywrightGrepTag,
+  execPromise,
+  withKibanaBabelRegister,
+} from './runner_utils';
 export { resolveSelector, type SelectorInput } from './locator_helper';
 export { checkA11y, type RunA11yScanOptions } from './axe';
