4dd2111c4db54a74a3b15ef17ec37145f184496d
Nikita Indik <nikita.indik@elastic.co>
2026-04-23 12:15:14 +0200

    Address code review feedback

--- x-pack/solutions/security/test/security_solution_api_integration/test_suites/detections_response/telemetry/trial_license_complete_tier/usage_collector/detection_rule_status.ts
+++ x-pack/solutions/security/test/security_solution_api_integration/test_suites/detections_response/telemetry/trial_license_complete_tier/usage_collector/detection_rule_status.ts
@@ -46,55 +46,65 @@ export default ({ getService }: FtrProviderContext) => {
   const retry = getService('retry');
   const es = getService('es');
 
-  // Makes sure that "execution-metrics" event is available to be read by usage collector.
-  const assertExecutionMetricsAvailable = async (ruleCategory: string): Promise<void> => {
-    const { count } = await es.count({
-      index: '.kibana-event-log-*',
-      query: {
-        bool: {
-          filter: [
-            { term: { 'event.action': 'execution-metrics' } },
-            { term: { 'rule.category': ruleCategory } },
-          ],
+  /**
+   * Makes sure that "execution-metrics" event is available to be read by usage collector.
+   */
+  const waitForExecutionMetricsVisible = async (ruleCategory: string): Promise<void> => {
+    await retry.try(async () => {
+      const { count } = await es.count({
+        index: '.kibana-event-log-*',
+        query: {
+          bool: {
+            filter: [
+              { term: { 'event.action': 'execution-metrics' } },
+              { term: { 'rule.category': ruleCategory } },
+            ],
+          },
         },
-      },
+      });
+      expect(count).to.be.above(0);
     });
-    expect(count).to.be.above(0);
+  };
+
+  const cleanup = async () => {
+    // Note that `deleteAllAlerts` removes the alerts index
+    await deleteAllAlerts(supertest, log, es);
+    await deleteAllRules(supertest, log);
+    await deleteAllEventLogExecutionEvents(es, log);
   };
 
   describe('@ess @serverless Detection rule status telemetry', () => {
     before(async () => {
-      // Just in case other tests do not clean up the event logs, let us clear them now and here only once.
-      await deleteAllEventLogExecutionEvents(es, log);
+      // Index source documents for rules to match against
       await esArchiver.load(
         'x-pack/solutions/security/test/fixtures/es_archives/security_solution/telemetry'
       );
     });
 
+    beforeEach(async () => {
+      await cleanup();
+      await createAlertsIndex(supertest, log);
+    });
+
     after(async () => {
+      // Remove the source documents
       await esArchiver.unload(
         'x-pack/solutions/security/test/fixtures/es_archives/security_solution/telemetry'
       );
-    });
 
-    beforeEach(async () => {
-      await deleteAllAlerts(supertest, log, es);
-      await deleteAllRules(supertest, log);
-      await deleteAllEventLogExecutionEvents(es, log);
-
-      await createAlertsIndex(supertest, log);
+      await cleanup();
     });
 
     describe('"kql" rule type', () => {
       let stats: DetectionMetrics | undefined;
       before(async () => {
-        await deleteAllRules(supertest, log);
+        // Setting the interval to a large value to avoid scheduling more than one rule execution during the test
         const rule = { ...getRuleForAlertTesting(['telemetry']), interval: '1d' };
         const { id } = await createRule(supertest, log, rule);
         await waitForRuleSuccess({ supertest, log, id });
         await waitForAlertsToBePresent(supertest, log, 4, [id]);
+        await waitForExecutionMetricsVisible('siem.queryRule');
         await retry.try(async () => {
-          await assertExecutionMetricsAvailable('siem.queryRule');
           stats = await getStats(supertest, log);
           expect(stats.detection_rules.detection_rule_status.custom_rules.total.succeeded).to.eql(
             1
@@ -249,12 +259,13 @@ export default ({ getService }: FtrProviderContext) => {
     describe('"eql" rule type', () => {
       let stats: DetectionMetrics | undefined;
       before(async () => {
+        // Setting the interval to a large value to avoid scheduling more than one rule execution during the test
         const rule = { ...getEqlRuleForAlertTesting(['telemetry']), interval: '1d' };
         const { id } = await createRule(supertest, log, rule);
         await waitForRuleSuccess({ supertest, log, id });
         await waitForAlertsToBePresent(supertest, log, 4, [id]);
+        await waitForExecutionMetricsVisible('siem.eqlRule');
         await retry.try(async () => {
-          await assertExecutionMetricsAvailable('siem.eqlRule');
           stats = await getStats(supertest, log);
           expect(stats.detection_rules.detection_rule_status.custom_rules.total.succeeded).to.eql(
             1
@@ -414,13 +425,14 @@ export default ({ getService }: FtrProviderContext) => {
             field: 'keyword',
             value: 1,
           },
+          // Setting the interval to a large value to avoid scheduling more than one rule execution during the test
           interval: '1d',
         };
         const { id } = await createRule(supertest, log, rule);
         await waitForRuleSuccess({ supertest, log, id });
         await waitForAlertsToBePresent(supertest, log, 4, [id]);
+        await waitForExecutionMetricsVisible('siem.thresholdRule');
         await retry.try(async () => {
-          await assertExecutionMetricsAvailable('siem.thresholdRule');
           stats = await getStats(supertest, log);
           expect(stats.detection_rules.detection_rule_status.custom_rules.total.succeeded).to.eql(
             1
@@ -596,13 +608,14 @@ export default ({ getService }: FtrProviderContext) => {
               ],
             },
           ],
+          // Setting the interval to a large value to avoid scheduling more than one rule execution during the test
           interval: '1d',
         };
         const { id } = await createRule(supertest, log, rule);
         await waitForRuleSuccess({ supertest, log, id });
         await waitForAlertsToBePresent(supertest, log, 4, [id]);
+        await waitForExecutionMetricsVisible('siem.indicatorRule');
         await retry.try(async () => {
-          await assertExecutionMetricsAvailable('siem.indicatorRule');
           stats = await getStats(supertest, log);
           expect(stats.detection_rules.detection_rule_status.custom_rules.total.succeeded).to.eql(
             1
