4af8b4844e8e16e8973ae504edad2ed24a8a44eb
Yan Savitski <yan.savitski@elastic.co>
2026-04-24 23:51:57 +0200

    Fix validation error messages not displaying in AdditionalOptionsFields
    Move EuiFormRow inside UseField render prop so isInvalid and
    errorMessage are accessible. Replace fieldValidators.isInteger with
    inline validator matching the temperature pattern. Both fields now
    use isBlocking: false for consistent inline error feedback.

--- x-pack/platform/packages/shared/kbn-inference-endpoint-ui-common/src/components/additional_options_fields.tsx
+++ x-pack/platform/packages/shared/kbn-inference-endpoint-ui-common/src/components/additional_options_fields.tsx
@@ -24,7 +24,6 @@ import {
 } from '@kbn/es-ui-shared-plugin/static/forms/hook_form_lib';
 import { FormattedMessage } from '@kbn/i18n-react';
 
-import { fieldValidators } from '@kbn/es-ui-shared-plugin/static/forms/helpers';
 import * as LABELS from '../translations';
 import { CHAT_COMPLETION_TASK_TYPE, DEFAULT_TASK_TYPE } from '../constants';
 import type { Config } from '../types/types';
@@ -60,52 +59,62 @@ export const AdditionalOptionsFields: React.FC<AdditionalOptionsFieldsProps> = (
   const contextWindowLengthSettings = useMemo(
     () =>
       showContextWindow ? (
-        <EuiFormRow
-          id="contextWindowLength"
-          fullWidth
-          label={
-            <EuiFlexGroup alignItems="center" justifyContent="spaceBetween">
-              <EuiFlexItem grow={false}>
-                <FormattedMessage
-                  id="xpack.inferenceEndpointUICommon.components.additionalInfo.contextWindowLengthLabel"
-                  defaultMessage="Context window length"
-                />
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiText color="subdued" size="xs">
-                  {LABELS.OPTIONALTEXT}
-                </EuiText>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          }
-          helpText={
-            <FormattedMessage
-              id="xpack.inferenceEndpointUICommon.components.additionalInfo.contextWindowLengthHelpInfo"
-              defaultMessage="Can be set to manually define the context length of the default model used by the connector. Useful for open source or more recent models."
-            />
-          }
-          data-test-subj={'configuration-formrow-contextWindowLength'}
-        >
-          <UseField
-            path="config.contextWindowLength"
-            config={{
-              validations: [
-                {
-                  validator: fieldValidators.isInteger({
-                    message: LABELS.CONTEXT_WINDOW_VALIDATION_MESSAGE,
-                  }),
-                  isBlocking: true,
+        <UseField
+          path="config.contextWindowLength"
+          config={{
+            validations: [
+              {
+                validator: ({ value, path }) => {
+                  if (value !== undefined && value !== null && value !== '') {
+                    const numValue = Number(value);
+                    if (!Number.isInteger(numValue) || numValue < 0) {
+                      return {
+                        code: 'ERR_FIELD_INVALID',
+                        path,
+                        message: LABELS.CONTEXT_WINDOW_VALIDATION_MESSAGE,
+                      };
+                    }
+                  }
                 },
-              ],
-            }}
-          >
-            {(field) => {
-              const { isInvalid, errorMessage } = getFieldValidityAndErrorMessage(field);
-              return (
+                isBlocking: false,
+              },
+            ],
+          }}
+        >
+          {(field) => {
+            const { isInvalid, errorMessage } = getFieldValidityAndErrorMessage(field);
+            return (
+              <EuiFormRow
+                id="contextWindowLength"
+                fullWidth
+                isInvalid={isInvalid}
+                error={errorMessage}
+                label={
+                  <EuiFlexGroup alignItems="center" justifyContent="spaceBetween">
+                    <EuiFlexItem grow={false}>
+                      <FormattedMessage
+                        id="xpack.inferenceEndpointUICommon.components.additionalInfo.contextWindowLengthLabel"
+                        defaultMessage="Context window length"
+                      />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText color="subdued" size="xs">
+                        {LABELS.OPTIONALTEXT}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                }
+                helpText={
+                  <FormattedMessage
+                    id="xpack.inferenceEndpointUICommon.components.additionalInfo.contextWindowLengthHelpInfo"
+                    defaultMessage="Can be set to manually define the context length of the default model used by the connector. Useful for open source or more recent models."
+                  />
+                }
+                data-test-subj={'configuration-formrow-contextWindowLength'}
+              >
                 <EuiFormControlLayout
                   fullWidth
                   isInvalid={isInvalid}
-                  error={errorMessage}
                   clear={{
                     onClick: () => {
                       setFieldValue('config.contextWindowLength', '');
@@ -123,10 +132,10 @@ export const AdditionalOptionsFields: React.FC<AdditionalOptionsFieldsProps> = (
                     }}
                   />
                 </EuiFormControlLayout>
-              );
-            }}
-          </UseField>
-        </EuiFormRow>
+              </EuiFormRow>
+            );
+          }}
+        </UseField>
       ) : null,
     [showContextWindow, setFieldValue, config.contextWindowLength]
   );
@@ -134,61 +143,62 @@ export const AdditionalOptionsFields: React.FC<AdditionalOptionsFieldsProps> = (
   const temperatureSettings = useMemo(
     () =>
       showTemperature ? (
-        <EuiFormRow
-          id="temperatureSettings"
-          fullWidth
-          label={
-            <EuiFlexGroup alignItems="center" justifyContent="spaceBetween">
-              <EuiFlexItem grow={false}>
-                <FormattedMessage
-                  id="xpack.inferenceEndpointUICommon.components.additionalInfo.temperatureLabel"
-                  defaultMessage="Temperature"
-                />
-              </EuiFlexItem>
-              <EuiFlexItem grow={false}>
-                <EuiText color="subdued" size="xs">
-                  {LABELS.OPTIONALTEXT}
-                </EuiText>
-              </EuiFlexItem>
-            </EuiFlexGroup>
-          }
-          helpText={
-            <FormattedMessage
-              id="xpack.inferenceEndpointUICommon.components.additionalInfo.temperatureHelpInfo"
-              defaultMessage="Controls the randomness of the model's output. Changing the temperature can affect the general performance of AI Assistant and AI-driven features in Kibana, and we recommend keeping the default value."
-            />
-          }
-          data-test-subj={'configuration-formrow-temperatureSettings'}
-        >
-          <UseField
-            path="config.temperature"
-            config={{
-              validations: [
-                {
-                  validator: ({ value, path }) => {
-                    if (value !== undefined && value !== null && value !== '') {
-                      const numValue = Number(value);
-                      if (isNaN(numValue) || numValue < 0 || numValue > 1) {
-                        return {
-                          code: 'ERR_FIELD_INVALID',
-                          path,
-                          message: LABELS.TEMPERATURE_VALIDATION_MESSAGE,
-                        };
-                      }
+        <UseField
+          path="config.temperature"
+          config={{
+            validations: [
+              {
+                validator: ({ value, path }) => {
+                  if (value !== undefined && value !== null && value !== '') {
+                    const numValue = Number(value);
+                    if (isNaN(numValue) || numValue < 0 || numValue > 1) {
+                      return {
+                        code: 'ERR_FIELD_INVALID',
+                        path,
+                        message: LABELS.TEMPERATURE_VALIDATION_MESSAGE,
+                      };
                     }
-                  },
-                  isBlocking: false,
+                  }
                 },
-              ],
-            }}
-          >
-            {(field) => {
-              const { isInvalid, errorMessage } = getFieldValidityAndErrorMessage(field);
-              return (
+                isBlocking: false,
+              },
+            ],
+          }}
+        >
+          {(field) => {
+            const { isInvalid, errorMessage } = getFieldValidityAndErrorMessage(field);
+            return (
+              <EuiFormRow
+                id="temperatureSettings"
+                fullWidth
+                isInvalid={isInvalid}
+                error={errorMessage}
+                label={
+                  <EuiFlexGroup alignItems="center" justifyContent="spaceBetween">
+                    <EuiFlexItem grow={false}>
+                      <FormattedMessage
+                        id="xpack.inferenceEndpointUICommon.components.additionalInfo.temperatureLabel"
+                        defaultMessage="Temperature"
+                      />
+                    </EuiFlexItem>
+                    <EuiFlexItem grow={false}>
+                      <EuiText color="subdued" size="xs">
+                        {LABELS.OPTIONALTEXT}
+                      </EuiText>
+                    </EuiFlexItem>
+                  </EuiFlexGroup>
+                }
+                helpText={
+                  <FormattedMessage
+                    id="xpack.inferenceEndpointUICommon.components.additionalInfo.temperatureHelpInfo"
+                    defaultMessage="Controls the randomness of the model's output. Changing the temperature can affect the general performance of AI Assistant and AI-driven features in Kibana, and we recommend keeping the default value."
+                  />
+                }
+                data-test-subj={'configuration-formrow-temperatureSettings'}
+              >
                 <EuiFormControlLayout
                   fullWidth
                   isInvalid={isInvalid}
-                  error={errorMessage}
                   clear={{
                     onClick: () => {
                       setFieldValue('config.temperature', undefined);
@@ -209,10 +219,10 @@ export const AdditionalOptionsFields: React.FC<AdditionalOptionsFieldsProps> = (
                     }}
                   />
                 </EuiFormControlLayout>
-              );
-            }}
-          </UseField>
-        </EuiFormRow>
+              </EuiFormRow>
+            );
+          }}
+        </UseField>
       ) : null,
     [showTemperature, setFieldValue, config.temperature]
   );
