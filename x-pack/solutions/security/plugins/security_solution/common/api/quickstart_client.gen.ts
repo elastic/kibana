812647dc614d39f2b728ca0a9ddcc533b6fdf252
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-04-24 17:34:15 +0000

    Changes from yarn openapi:generate

--- x-pack/solutions/security/plugins/security_solution/common/api/quickstart_client.gen.ts
+++ x-pack/solutions/security/plugins/security_solution/common/api/quickstart_client.gen.ts
@@ -339,7 +339,10 @@ import type {
 import type { DisableRiskEngineResponse } from './entity_analytics/risk_engine/engine_disable_route.gen';
 import type { EnableRiskEngineResponse } from './entity_analytics/risk_engine/engine_enable_route.gen';
 import type { InitRiskEngineResponse } from './entity_analytics/risk_engine/engine_init_route.gen';
-import type { ScheduleRiskEngineNowResponse } from './entity_analytics/risk_engine/engine_schedule_now_route.gen';
+import type {
+  ScheduleRiskEngineNowRequestBodyInput,
+  ScheduleRiskEngineNowResponse,
+} from './entity_analytics/risk_engine/engine_schedule_now_route.gen';
 import type { ReadRiskEngineSettingsResponse } from './entity_analytics/risk_engine/engine_settings_route.gen';
 import type { GetRiskEngineStatusResponse } from './entity_analytics/risk_engine/engine_status_route.gen';
 import type {
@@ -3091,7 +3094,7 @@ The difference between the `id` and `rule_id` is that the `id` is a unique rule 
   /**
    * Schedule the risk scoring engine to run as soon as possible. You can use this to recalculate entity risk scores after updating their asset criticality.
    */
-  async scheduleRiskEngineNow() {
+  async scheduleRiskEngineNow(props: ScheduleRiskEngineNowProps) {
     this.log.info(`${new Date().toISOString()} Calling API ScheduleRiskEngineNow`);
     return this.kbnClient
       .request<ScheduleRiskEngineNowResponse>({
@@ -3100,6 +3103,7 @@ The difference between the `id` and `rule_id` is that the `id` is a unique rule 
           [ELASTIC_HTTP_VERSION_HEADER]: '2023-10-31',
         },
         method: 'POST',
+        body: props.body,
       })
       .catch(catchAxiosErrorFormatAndThrow);
   }
@@ -4048,6 +4052,9 @@ export interface RulePreviewProps {
 }
 export interface RunScriptActionProps {
   body: RunScriptActionRequestBodyInput;
+}
+export interface ScheduleRiskEngineNowProps {
+  body: ScheduleRiskEngineNowRequestBodyInput;
 }
 export interface SearchAlertsProps {
   body: SearchAlertsRequestBodyInput;
