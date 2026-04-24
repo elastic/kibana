9458cbc1b9256642366a26657a433e940a489676
Paulo Silva <paulo.henrique@elastic.co>
2026-04-23 23:15:35 -0700

    adding link to job id in entity analytics

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/recent_anomalies/recent_anomalies_chart.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/components/recent_anomalies/recent_anomalies_chart.tsx
@@ -5,7 +5,7 @@
  * 2.0.
  */
 import React, { useState } from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiSelect, EuiText } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiSelect } from '@elastic/eui';
 import { css } from '@emotion/react';
 import { useGlobalTime } from '../../../common/containers/use_global_time';
 import { AnomalySeverityFilter } from './anomaly_severity_filter';
@@ -13,8 +13,8 @@ import { useRecentAnomaliesQuery } from './hooks/recent_anomalies_query_hooks';
 import type { ViewByMode } from './hooks/recent_anomalies_esql_source_query_hooks';
 import { useAnomalyBands } from './anomaly_bands';
 import { EntityNameList } from './entity_name_list';
+import { JobIdList } from './job_id_list';
 import { AnomalyHeatmap } from './anomaly_heatmap';
-import { getAnomalyChartStyling } from './anomaly_chart_styling';
 import { useQueryInspector } from '../../../common/components/page/manage_query';
 import { RecentAnomaliesHeatmapNoResults } from './recent_anomalies_heatmap_no_results';
 
@@ -25,39 +25,6 @@ const VIEW_BY_OPTIONS = [
   { value: 'entity' as const, text: 'Entity' },
   { value: 'jobId' as const, text: 'Job ID' },
 ];
-
-const LabelList: React.FC<{ labels: string[]; compressed?: boolean }> = ({
-  labels,
-  compressed = false,
-}) => {
-  const styling = getAnomalyChartStyling(compressed);
-  return (
-    <EuiFlexItem
-      css={css`
-        margin-top: ${styling.heightOfTopLegend}px;
-        height: ${styling.heightOfEntityNamesList(labels.length)}px;
-      `}
-      grow={false}
-    >
-      <EuiFlexGroup gutterSize={'none'} direction={'column'} justifyContent={'center'}>
-        {labels.map((label) => (
-          <EuiFlexItem
-            key={label}
-            css={css`
-              justify-content: center;
-              height: ${styling.heightOfEachCell}px;
-            `}
-            grow={false}
-          >
-            <EuiText textAlign={'right'} size="s">
-              {label}
-            </EuiText>
-          </EuiFlexItem>
-        ))}
-      </EuiFlexGroup>
-    </EuiFlexItem>
-  );
-};
 
 interface RecentAnomaliesChartProps {
   watchlistId?: string;
@@ -127,7 +94,7 @@ export const RecentAnomaliesChart: React.FC<RecentAnomaliesChartProps> = ({
             compressed
           />
         ) : (
-          <LabelList labels={rowLabels} compressed />
+          <JobIdList jobIds={rowLabels} compressed />
         )}
         <AnomalyHeatmap
           anomalyBands={bands}
