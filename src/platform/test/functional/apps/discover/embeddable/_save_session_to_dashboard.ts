1032cef33795e0f7b1086525bd6637ea12ac2d43
Gonçalo Rica <goncalo.rica@elastic.co>
2026-04-29 16:03:55 +0200

    [Discover][Embeddable] Do not override panels with Save As on dashboard

--- src/platform/test/functional/apps/discover/embeddable/_save_session_to_dashboard.ts
+++ src/platform/test/functional/apps/discover/embeddable/_save_session_to_dashboard.ts
@@ -117,6 +117,45 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
         expect(await discover.getAllSavedSearchDocumentCount()).to.eql(['13 documents']);
       });
 
+      it('can save a new session from an existing one to a dashboard without overriding the original', async () => {
+        const existingDashboardName = 'Existing Target Dashboard 2';
+        const originalSession = 'Original Session for Copy';
+
+        await discover.navigateToApp();
+        await discover.clickNewSearchButton();
+        await header.waitUntilLoadingHasFinished();
+        await discover.waitUntilSearchingHasFinished();
+        await discover.saveSearch(originalSession);
+        await header.waitUntilLoadingHasFinished();
+
+        await dashboard.navigateToApp();
+        await dashboard.gotoDashboardLandingPage();
+        await dashboard.clickNewDashboard();
+        await dashboardAddPanel.addSavedSearch(originalSession);
+        await header.waitUntilLoadingHasFinished();
+        await dashboard.saveDashboard(existingDashboardName);
+        await dashboard.switchToEditMode();
+
+        await discover.editEmbeddableInDiscover();
+        await header.waitUntilLoadingHasFinished();
+        await queryBar.setQuery('test');
+        await queryBar.submitQuery();
+        await discover.waitUntilTabIsLoaded();
+        await discover.saveSearchToDashboard(
+          'Session for not Overridding panels',
+          {
+            existing: existingDashboardName,
+          },
+          { saveAsNew: true }
+        );
+        await dashboard.waitForRenderComplete();
+        await dashboard.verifyNoRenderErrors();
+        expect(await discover.getAllSavedSearchDocumentCount()).to.eql([
+          '4,633 documents',
+          '13 documents',
+        ]);
+      });
+
       it('can save as a copy to a new dashboard via Save As menu', async () => {
         await discover.navigateToApp();
         await discover.clickNewSearchButton();
