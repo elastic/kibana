9410e254b11934f8fc5e1ff29ad85f54a7cbe95f
Julia Bardi <julia.bardi@elastic.co>
2026-04-29 09:08:56 +0200

    fix tests

--- x-pack/platform/plugins/shared/fleet/server/services/agents/crud.test.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/agents/crud.test.ts
@@ -118,9 +118,10 @@ describe('Agents CRUD test', () => {
           query: expect.any(Object),
           index: '.fleet-agents',
           size: 0,
-          fields: ['status'],
+          fields: ['status', 'effective_config_label'],
           runtime_mappings: {
             status: expect.anything(),
+            effective_config_label: expect.anything(),
           },
         })
       );
@@ -163,9 +164,10 @@ describe('Agents CRUD test', () => {
           aggs: { tags: { terms: { field: 'tags', size: 10000 } } },
           index: '.fleet-agents',
           size: 0,
-          fields: ['status'],
+          fields: ['status', 'effective_config_label'],
           runtime_mappings: {
             status: expect.anything(),
+            effective_config_label: expect.anything(),
           },
         })
       );
