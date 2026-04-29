3d2fe280b32906b127aa6e72c076899f28c70726
PhilippeOberti <philippe.oberti@elastic.co>
2026-04-29 16:54:30 -0500

    Reuse Discover's actions instead of redefining them

--- src/platform/packages/shared/kbn-unified-doc-viewer/src/hooks/index.ts
+++ src/platform/packages/shared/kbn-unified-doc-viewer/src/hooks/index.ts
@@ -7,8 +7,4 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export * from './components';
-export * from './services';
-export * from './analytics';
-export * from './constants';
-export * from './hooks';
+export * from './use_field_actions';
