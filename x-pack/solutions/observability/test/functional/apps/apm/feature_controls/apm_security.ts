f435d79df23834b0fde3bbfe15d39111a8433628
Sergi Massaneda <sergi.massaneda@elastic.co>
2026-04-29 13:31:13 +0200

    update tests in classic nav

--- x-pack/solutions/observability/test/functional/apps/apm/feature_controls/apm_security.ts
+++ x-pack/solutions/observability/test/functional/apps/apm/feature_controls/apm_security.ts
@@ -66,7 +66,6 @@ export default function ({ getPageObjects, getService }: FtrProviderContext) {
           'Alerts',
           'Applications',
           'User Experience',
-          'Workflows',
           'Stack Management',
         ]);
       });
