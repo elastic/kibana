a3474ff3bd4c9b6cf324ef4b9610e0b4f4b5e665
Kelvin Tan <kelvin.tan@elastic.co>
2026-04-30 14:46:17 -0400

    conditional link in column

--- x-pack/solutions/security/plugins/security_solution/public/timelines/components/timeline/body/renderers/host_name.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/timelines/components/timeline/body/renderers/host_name.tsx
@@ -9,12 +9,19 @@ import React, { useCallback, useContext, useMemo } from 'react';
 import type { EuiButtonEmpty, EuiButtonIcon } from '@elastic/eui';
 import { isString } from 'lodash/fp';
 import { useExpandableFlyoutApi } from '@kbn/expandable-flyout';
+import { useHistory } from 'react-router-dom';
+import { useStore } from 'react-redux';
 import { HostPanelKey } from '../../../../../flyout/entity_details/shared/constants';
 import { StatefulEventContext } from '../../../../../common/components/events_viewer/stateful_event_context';
 import { HostDetailsLink } from '../../../../../common/components/links';
 import { getEmptyTagValue } from '../../../../../common/components/empty_value';
 import { TruncatableText } from '../../../../../common/components/truncatable_text';
 import { useIsInSecurityApp } from '../../../../../common/hooks/is_in_security_app';
+import { useIsExperimentalFeatureEnabled } from '../../../../../common/hooks/use_experimental_features';
+import { useKibana } from '../../../../../common/lib/kibana';
+import { Host } from '../../../../../flyout_v2/entity_details/host';
+import { flyoutProviders } from '../../../../../flyout_v2/shared/components/flyout_provider';
+import { defaultToolsFlyoutProperties } from '../../../../../flyout_v2/shared/hooks/use_default_flyout_properties';
 
 interface Props {
   contextId: string;
@@ -36,6 +43,11 @@ const HostNameComponent: React.FC<Props> = ({
   entityId,
 }) => {
   const { openFlyout } = useExpandableFlyoutApi();
+  const { services } = useKibana();
+  const { overlays } = services;
+  const store = useStore();
+  const history = useHistory();
+  const newFlyoutSystemEnabled = useIsExperimentalFeatureEnabled('newFlyoutSystemEnabled');
 
   const isInSecurityApp = useIsInSecurityApp();
 
@@ -51,28 +63,52 @@ const HostNameComponent: React.FC<Props> = ({
         onClick();
       }
 
-      /*
-       * if and only if renderer is running inside security solution app
-       * we check for event and timeline context
-       * */
       if (!eventContext || !isInTimelineContext || !eventContext.enableHostDetailsFlyout) {
         return;
       }
 
-      const { timelineID } = eventContext;
-      openFlyout({
-        right: {
-          id: HostPanelKey,
-          params: {
-            hostName,
-            entityId,
-            contextID: contextId,
-            scopeId: timelineID,
+      if (newFlyoutSystemEnabled) {
+        overlays.openSystemFlyout(
+          flyoutProviders({
+            services,
+            store,
+            history,
+            children: <Host hostName={hostName} entityId={entityId} />,
+          }),
+          {
+            ...defaultToolsFlyoutProperties,
+            session: 'start',
+          }
+        );
+      } else {
+        const { timelineID } = eventContext;
+        openFlyout({
+          right: {
+            id: HostPanelKey,
+            params: {
+              hostName,
+              entityId,
+              contextID: contextId,
+              scopeId: timelineID,
+            },
           },
-        },
-      });
+        });
+      }
     },
-    [onClick, eventContext, isInTimelineContext, hostName, entityId, openFlyout, contextId]
+    [
+      onClick,
+      eventContext,
+      isInTimelineContext,
+      hostName,
+      entityId,
+      openFlyout,
+      contextId,
+      newFlyoutSystemEnabled,
+      overlays,
+      services,
+      store,
+      history,
+    ]
   );
 
   // The below is explicitly defined this way as the onClick takes precedence when it and the href are both defined
