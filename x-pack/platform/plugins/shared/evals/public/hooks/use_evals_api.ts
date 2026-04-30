46f9f6b9764fae19f39d4a32ba64b3d285e8add7
Viduni Wickramarachchi <viduni.wickramarachchi@elastic.co>
2026-04-30 16:40:11 -0400

    Revert

--- x-pack/platform/plugins/shared/evals/public/hooks/use_evals_api.ts
+++ x-pack/platform/plugins/shared/evals/public/hooks/use_evals_api.ts
@@ -257,21 +257,11 @@ export const useUpdateExample = () => {
         }
       );
     },
-    onSuccess: async (response, { datasetId, exampleId }) => {
-      queryClient.setQueryData<GetEvaluationDatasetResponse>(
-        queryKeys.datasets.detail(datasetId),
-        (old) => {
-          if (!old) return old;
-          const { dataset_id: _, ...updatedExample } = response;
-          return {
-            ...old,
-            examples: old.examples.map((e) => (e.id === exampleId ? updatedExample : e)),
-          };
-        }
-      );
-      await queryClient.invalidateQueries({
-        queryKey: ['evals', 'datasets', 'list'],
-      });
+    onSuccess: async (_response, { datasetId }) => {
+      await Promise.all([
+        queryClient.invalidateQueries({ queryKey: queryKeys.datasets.all }),
+        queryClient.invalidateQueries({ queryKey: queryKeys.datasets.detail(datasetId) }),
+      ]);
     },
   });
 };
@@ -292,20 +282,11 @@ export const useDeleteExample = () => {
         }
       );
     },
-    onSuccess: async (_response, { datasetId, exampleId }) => {
-      queryClient.setQueryData<GetEvaluationDatasetResponse>(
-        queryKeys.datasets.detail(datasetId),
-        (old) => {
-          if (!old) return old;
-          return {
-            ...old,
-            examples: old.examples.filter((e) => e.id !== exampleId),
-          };
-        }
-      );
-      await queryClient.invalidateQueries({
-        queryKey: ['evals', 'datasets', 'list'],
-      });
+    onSuccess: async (_response, { datasetId }) => {
+      await Promise.all([
+        queryClient.invalidateQueries({ queryKey: queryKeys.datasets.all }),
+        queryClient.invalidateQueries({ queryKey: queryKeys.datasets.detail(datasetId) }),
+      ]);
     },
   });
 };
@@ -419,7 +400,6 @@ export const useEvaluationRuns = (filters: RunsListFilters = {}) => {
       }
       return true;
     },
-    refetchOnWindowFocus: false,
   });
 };
 
@@ -511,7 +491,6 @@ export const useExampleScores = (exampleId: string) => {
       });
     },
     enabled: exampleId.length > 0,
-    refetchOnWindowFocus: false,
   });
 };
 
