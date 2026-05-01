56d28bd14df6aabc6fa6d5fafafb2cac466fedb1
Catherine Liu <catherine.liu@elastic.co>
2026-05-01 10:20:13 -0700

    Fix links embeddable test

--- src/platform/plugins/private/links/public/embeddable/links_embeddable.test.tsx
+++ src/platform/plugins/private/links/public/embeddable/links_embeddable.test.tsx
@@ -17,32 +17,30 @@ import type { Link } from '../../server';
 import type { LinksApi, ResolvedLink } from '../types';
 import { linksClient } from '../content_management';
 import { getMockLinksParentApi } from '../mocks';
-import { DEFAULT_DASHBOARD_NAVIGATION_OPTIONS } from '@kbn/dashboard-navigation-options-common';
-import { DEFAULT_EXTERNAL_LINK_OPTIONS } from '../../common/constants';
 
 const getLinks = (): Link[] => [
   {
     type: 'dashboardLink',
     label: '',
     destination: '999',
-    options: DEFAULT_DASHBOARD_NAVIGATION_OPTIONS,
+    options: { open_in_new_tab: false, use_time_range: false, use_filters: false },
   },
   {
     type: 'dashboardLink',
     label: 'Dashboard 2',
     destination: '888',
-    options: DEFAULT_DASHBOARD_NAVIGATION_OPTIONS,
+    options: { open_in_new_tab: false, use_time_range: false, use_filters: false },
   },
   {
     type: 'externalLink',
     label: 'Example homepage',
     destination: 'https://example.com',
-    options: DEFAULT_EXTERNAL_LINK_OPTIONS,
+    options: { open_in_new_tab: false, encode_url: true },
   },
   {
     type: 'externalLink',
     destination: 'https://elastic.co',
-    options: DEFAULT_EXTERNAL_LINK_OPTIONS,
+    options: { open_in_new_tab: true, encode_url: false },
   },
 ];
 
@@ -54,7 +52,7 @@ const getResolvedLinks: () => ResolvedLink[] = () => [
     destination: '999',
     title: 'Dashboard 1',
     description: 'Dashboard 1 description',
-    options: DEFAULT_DASHBOARD_NAVIGATION_OPTIONS,
+    options: { open_in_new_tab: false, use_time_range: false, use_filters: false },
   },
   {
     id: '002',
@@ -63,7 +61,7 @@ const getResolvedLinks: () => ResolvedLink[] = () => [
     destination: '888',
     title: 'Dashboard 2',
     description: 'Dashboard 2 description',
-    options: DEFAULT_DASHBOARD_NAVIGATION_OPTIONS,
+    options: { open_in_new_tab: false, use_time_range: false, use_filters: false },
   },
   {
     id: '003',
@@ -71,14 +69,14 @@ const getResolvedLinks: () => ResolvedLink[] = () => [
     label: 'Example homepage',
     destination: 'https://example.com',
     title: 'Example homepage',
-    options: DEFAULT_EXTERNAL_LINK_OPTIONS,
+    options: { open_in_new_tab: false, encode_url: true },
   },
   {
     id: '004',
     type: 'externalLink',
     destination: 'https://elastic.co',
     title: 'https://elastic.co',
-    options: DEFAULT_EXTERNAL_LINK_OPTIONS,
+    options: { open_in_new_tab: true, encode_url: false },
   },
 ];
 
