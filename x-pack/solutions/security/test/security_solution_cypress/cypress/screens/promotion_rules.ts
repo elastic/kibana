03c1faf6b2893334c9a3ce62fa738fc0cf88561e
Nikita Indik <nikita.indik@elastic.co>
2026-05-03 17:24:05 +0200

    Address Glo's review comments from previous PR

---
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/screens/promotion_rules.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export const CONFIGURATIONS_INTEGRATIONS_URL = '/app/security/configurations/integrations/browse';
+
+export const CONFIGURATIONS_BASIC_RULES_URL_FRAGMENT = '/configurations/basic_rules';
+
+export const BASIC_RULES_TAB = '[data-test-subj="configurationsTab-basic_rules"]';
