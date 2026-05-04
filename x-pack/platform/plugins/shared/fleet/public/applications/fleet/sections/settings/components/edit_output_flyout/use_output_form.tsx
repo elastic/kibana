56d24d11f4eb7b6785519cec1cfe5e4ebfa2a30a
Supplementing <mason.d.herron@gmail.com>
2026-05-04 11:39:56 -0600

    made toggle disabling and save button stick on data fetching for read-only users

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/settings/components/edit_output_flyout/use_output_form.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/settings/components/edit_output_flyout/use_output_form.tsx
@@ -1240,6 +1240,7 @@ export function useOutputForm(onSucess: () => void, output?: Output, defaultOutp
     isShipperEnabled: !isShipperDisabled,
     isDisabled:
       isLoading ||
+      (!authz.fleet.allSettings && !authz.fleet.allAgentPolicies) ||
       (output && !hasChanged) ||
       !nameInput.value ||
       isHostsMissing ||
