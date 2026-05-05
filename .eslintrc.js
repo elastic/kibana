5712f65c3556097aa06fec1dbaf00dd8ef0ffda1
Damian Polewski <125268832+damian-polewski@users.noreply.github.com>
2026-05-05 12:36:06 +0200

    [Hook Form Lib] Deprecate hook_form_lib (#266730)
    ## Summary
    
    This PR:
    - Added an eslint deprecation warning when importing
    `@kbn/es-ui-shared-plugin/static/forms/hook_form_lib`.
    - Updated the form-lib docs landing page with a deprecation notice for
    `hook_form_lib`.
    - Updated `hook_form_lib` public APIs to be marked `@deprecated`,
    prompting IDE warnings and guiding contributors to consider
    `react-hook-form` for new and existing forms.
    
    (cherry picked from commit 2915168e40c1b2e272d7e862a460247dfdf4a230)

--- .eslintrc.js
+++ .eslintrc.js
@@ -491,6 +491,11 @@ const DEPRECATED_IMPORTS = [
     message:
       'Enzyme is deprecated and no longer maintained. Please use @testing-library/react instead.',
   },
+  {
+    name: '@kbn/es-ui-shared-plugin/static/forms/hook_form_lib',
+    message:
+      '`hook_form_lib` is deprecated and will no longer be supported. Consider using `react-hook-form` for new and existing forms.',
+  },
 ];
 
 module.exports = {
@@ -1027,7 +1032,19 @@ module.exports = {
         '@kbn/eslint/no_unsafe_dynamic_http_path': 'warn',
         '@kbn/eslint/no_wrapped_error_in_logger': 'error',
         'no-restricted-imports': ['error', ...RESTRICTED_IMPORTS],
-        '@kbn/eslint/no_deprecated_imports': ['warn', ...DEPRECATED_IMPORTS],
+        '@kbn/eslint/no_deprecated_imports': [
+          'warn',
+          {
+            paths: DEPRECATED_IMPORTS,
+            patterns: [
+              {
+                group: ['@kbn/es-ui-shared-plugin/static/forms/hook_form_lib/**'],
+                message:
+                  '`hook_form_lib` is deprecated and will no longer be supported. Consider using `react-hook-form` for new and existing forms.',
+              },
+            ],
+          },
+        ],
         'no-restricted-modules': [
           'error',
           {
