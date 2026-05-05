241a150059cfd7850642d8ee39b9466bb59e7ec0
heenawter <hannah.wright@elastic.co>
2026-05-04 18:58:29 -0600

    Update tests

--- src/platform/plugins/shared/dashboard/public/dashboard_app/top_nav/add_panel_button/get_menu_item_groups.test.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_app/top_nav/add_panel_button/get_menu_item_groups.test.ts
@@ -7,7 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { ACTION_CREATE_TIME_SLIDER } from '@kbn/controls-constants';
 import { buildMockDashboardApi } from '../../../mocks';
 import { getMenuItemGroups } from './get_menu_item_groups';
 
@@ -81,40 +80,7 @@ describe('getMenuItemGroups', () => {
     expect(mockGetAction).not.toBeCalled();
   });
 
-  test('do not fetch time slider action if control group does not exist', async () => {
-    mockGetTriggerCompatibleActions.mockResolvedValueOnce([
-      {
-        id: 'mockAddPanelAction',
-        type: '',
-        order: 1,
-        grouping: [
-          {
-            id: 'myGroup',
-            order: 950,
-            getDisplayName: () => 'My group',
-          },
-        ],
-        getDisplayName: () => 'mockAddPanelAction',
-        getIconType: () => 'empty',
-        execute: () => {},
-        isCompatible: async () => true,
-      },
-    ]);
-
-    const api = {
-      ...buildMockDashboardApi().api,
-      getAppContext: () => ({
-        currentAppId: 'dashboards',
-      }),
-      openOverlay: () => {},
-      clearOverlays: () => {},
-    };
-    const groups = await getMenuItemGroups(api);
-    expect(groups.length).toBe(1);
-    expect(mockGetAction).not.toBeCalled();
-  });
-
-  test('fetch time slider action if control group exists', async () => {
+  test('shows actions that are compatible but disabled', async () => {
     mockGetTriggerCompatibleActions.mockResolvedValueOnce([
       {
         id: 'createSomeControl',
@@ -131,12 +97,9 @@ describe('getMenuItemGroups', () => {
         getIconType: (): string => 'controls',
         execute: () => {},
         isCompatible: async (): Promise<boolean> => true,
+        isDisabled: () => true,
       },
     ]);
-    mockGetAction.mockResolvedValueOnce({
-      getDisplayName: () => 'Time slider',
-      isCompatible: async (): Promise<boolean> => true,
-    });
 
     const api = {
       ...buildMockDashboardApi().api,
@@ -148,48 +111,7 @@ describe('getMenuItemGroups', () => {
     };
     const groups = await getMenuItemGroups(api);
     expect(groups.length).toBe(1);
-    expect(mockGetAction).toBeCalledWith(ACTION_CREATE_TIME_SLIDER);
-    expect(groups[0].title).toBe('Controls');
-    expect(groups[0].items.length).toBe(2);
-    expect(groups[0].items[1].isDisabled).toBe(false);
-  });
-
-  test('disable time slider action if it is incompatible', async () => {
-    mockGetTriggerCompatibleActions.mockResolvedValueOnce([
-      {
-        id: 'createSomeControl',
-        type: '',
-        order: 1,
-        grouping: [
-          {
-            id: 'controls',
-            order: 900,
-            getDisplayName: (): string => 'Controls',
-          },
-        ],
-        getDisplayName: (): string => 'A control',
-        getIconType: (): string => 'controls',
-        execute: () => {},
-        isCompatible: async (): Promise<boolean> => true,
-      },
-    ]);
-
-    mockGetAction.mockResolvedValueOnce({
-      getDisplayName: () => 'Time slider',
-      isCompatible: async (): Promise<boolean> => false,
-    });
-
-    const api = {
-      ...buildMockDashboardApi().api,
-      getAppContext: () => ({
-        currentAppId: 'dashboards',
-      }),
-      openOverlay: () => {},
-      clearOverlays: () => {},
-    };
-    const groups = await getMenuItemGroups(api);
-    expect(groups.length).toBe(1);
-    expect(groups[0].items.length).toBe(2);
-    expect(groups[0].items[1].isDisabled).toBe(true);
+    expect(groups[0].items.length).toBe(1);
+    expect(groups[0].items[0].isDisabled).toBe(true);
   });
 });
