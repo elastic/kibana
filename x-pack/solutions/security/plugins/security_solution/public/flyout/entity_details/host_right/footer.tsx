066f8196a402be070987d27441402876e50445df
tcalopes <tatiana.lopes@elastic.co>
2026-05-05 16:21:46 -0400

    Add 'add to chat' button to entity flyout

--- x-pack/solutions/security/plugins/security_solution/public/flyout/entity_details/host_right/footer.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout/entity_details/host_right/footer.tsx
@@ -9,9 +9,14 @@ import React, { useMemo } from 'react';
 import { EuiFlyoutFooter, EuiPanel, EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { useEntityStoreEuidApi } from '@kbn/entity-store/public';
 import { TakeAction } from '../shared/components/take_action';
-import { EntityIdentifierFields } from '../../../../common/entity_analytics/types';
+import { EntityIdentifierFields, EntityType } from '../../../../common/entity_analytics/types';
 import type { IdentityFields } from '../../document_details/shared/utils';
 import type { EntityStoreRecord } from '../shared/hooks/use_entity_from_store';
+import { useAgentBuilderAvailability } from '../../../agent_builder/hooks/use_agent_builder_availability';
+import { NewAgentBuilderAttachment } from '../../../agent_builder/components/new_agent_builder_attachment';
+import { SecurityAgentBuilderAttachments } from '../../../../common/constants';
+import { useAgentBuilderAttachment } from '../../../agent_builder/hooks/use_agent_builder_attachment';
+import { ENTITY_PROMPT } from '../../../agent_builder/components/prompts';
 
 export const HostPanelFooter = ({
   identityFields,
@@ -21,10 +26,26 @@ export const HostPanelFooter = ({
   /** When entity store v2 is enabled: entity record from the store. */
   entity?: EntityStoreRecord;
 }) => {
+  const { isAgentChatExperienceEnabled } = useAgentBuilderAvailability();
+
   const hostName = useMemo(
     () => identityFields[EntityIdentifierFields.hostName] || Object.values(identityFields)[0] || '',
     [identityFields]
   );
+
+  const entityAttachment = useMemo(
+    () => ({
+      attachmentType: SecurityAgentBuilderAttachments.entity,
+      attachmentData: {
+        identifierType: EntityType.host,
+        identifier: hostName,
+        attachmentLabel: `${EntityType.host}: ${hostName}`,
+      },
+      attachmentPrompt: ENTITY_PROMPT,
+    }),
+    [hostName]
+  );
+  const { openAgentBuilderFlyout } = useAgentBuilderAttachment(entityAttachment);
 
   const euidApi = useEntityStoreEuidApi();
   const euidEntityFilter = useMemo((): string | undefined => {
@@ -38,6 +59,17 @@ export const HostPanelFooter = ({
     <EuiFlyoutFooter>
       <EuiPanel color="transparent">
         <EuiFlexGroup justifyContent="flexEnd" alignItems="center">
+          {isAgentChatExperienceEnabled && (
+            <EuiFlexItem grow={false}>
+              <NewAgentBuilderAttachment
+                onClick={openAgentBuilderFlyout}
+                telemetry={{
+                  pathway: 'entity_flyout',
+                  attachments: ['entity'],
+                }}
+              />
+            </EuiFlexItem>
+          )}
           <EuiFlexItem grow={false}>
             <TakeAction
               isDisabled={!hostName}
