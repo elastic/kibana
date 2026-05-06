22cd812622594491e334431d899d5303e47787e6
Nathan Reese <reese.nathan@gmail.com>
2026-05-06 07:23:24 -0600

    unskip add panel flyout tests

--- src/platform/plugins/shared/dashboard/test/scout/ui/parallel_tests/dashboard_add_panel_flyout.spec.ts
+++ src/platform/plugins/shared/dashboard/test/scout/ui/parallel_tests/dashboard_add_panel_flyout.spec.ts
@@ -7,33 +7,49 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { spaceTest, tags } from '@kbn/scout';
+import { ScoutTestConfig, spaceTest, tags } from '@kbn/scout';
 import { expect } from '@kbn/scout/ui';
 import { DASHBOARD_DEFAULT_INDEX_TITLE, DASHBOARD_SAVED_SEARCH_ARCHIVE } from '../constants';
 
-// may include "observabilityGroup" panel group (and other panel groups)
-const DASHBOARD_PANEL_GROUP_ORDER = [
-  'visualizationsGroup',
-  'controlsGroup',
-  'annotation-and-navigationGroup',
-  'mlGroup',
-  'legacyGroup',
-];
+const getExpected = (config: ScoutTestConfig) => {
+  return {
+    groups: [
+      'visualizationsGroup',
+      'controlsGroup',
+      'annotation-and-navigationGroup',
+      'mlGroup',
+      'logs-aiopsGroup',
+      'observabilityGroup',
+      'legacyGroup',
+    ],
+    count: 27
+  }
+};
 
-const DASHBOARD_PANEL_TYPE_COUNT = 19;
-
-// Failing: See https://github.com/elastic/kibana/issues/259443
-spaceTest.describe.skip(
-  'Dashboard panel listing',
+/**
+ * Dashboard's add panel menu is populated 
+ * by ui_actions trigger ADD_PANEL_TRIGGER.
+ * This test exists to ensures additions to menu 
+ * notify our team and can be reviewed by design.
+ */
+spaceTest.describe.only(
+  'Dashboard add panel flyout',
   {
     tag: [
       ...tags.stateful.classic,
       ...tags.serverless.search,
       ...tags.serverless.security.complete,
+      ...tags.serverless.observability.complete,
     ],
   },
   () => {
-    spaceTest.beforeAll(async ({ scoutSpace }) => {
+    let expectedCount: number;
+    let expectedGroups: string[];
+
+    spaceTest.beforeAll(async ({ scoutSpace, config }) => {
+      const expected = getExpected(config);
+      expectedCount = expected.count;
+      expectedGroups = expected.groups;
       await scoutSpace.savedObjects.cleanStandardList();
       await scoutSpace.savedObjects.load(DASHBOARD_SAVED_SEARCH_ARCHIVE);
       await scoutSpace.uiSettings.setDefaultIndex(DASHBOARD_DEFAULT_INDEX_TITLE);
@@ -44,26 +60,24 @@ spaceTest.describe.skip(
     });
 
     spaceTest.afterAll(async ({ scoutSpace }) => {
+      debugger;
       await scoutSpace.uiSettings.unset('defaultIndex');
       await scoutSpace.savedObjects.cleanStandardList();
     });
 
-    spaceTest('renders panel groups and panel count', async ({ pageObjects }) => {
+    spaceTest('renders add panel flyout', async ({ pageObjects }) => {
       await spaceTest.step('open new dashboard and add panel flyout', async () => {
         await pageObjects.dashboard.openNewDashboard();
         await pageObjects.dashboard.openAddPanelFlyout();
       });
 
-      await spaceTest.step('verify panel groups order', async () => {
-        const panelGroupOrder = await pageObjects.dashboard.getPanelGroupOrder();
-        expect(panelGroupOrder.length).toBeGreaterThanOrEqual(DASHBOARD_PANEL_GROUP_ORDER.length);
-        expect(panelGroupOrder).toStrictEqual(expect.arrayContaining(DASHBOARD_PANEL_GROUP_ORDER));
+      await spaceTest.step('verify panel groups', async () => {
+        const groups = await pageObjects.dashboard.getAddPanelFlyoutGroups();
+        expect(groups).toEqual(expectedGroups);
       });
 
       await spaceTest.step('verify total panel count', async () => {
-        expect(await pageObjects.dashboard.getPanelTypeCount()).toBeGreaterThanOrEqual(
-          DASHBOARD_PANEL_TYPE_COUNT
-        );
+        expect(await pageObjects.dashboard.getAddPanelFlyoutPanelTypesCount()).toBe(expectedCount);
       });
     });
   }
