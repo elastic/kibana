a46eb40f96a4ab347da112c585ab19e1d62d39cf
Milosz Marcinkowski <milosz.marcinkowski@elastic.co>
2026-05-07 14:13:00 +0200

    Integrations grid

--- x-pack/solutions/observability/plugins/observability_onboarding/public/application/shared/logo_icon.tsx
+++ x-pack/solutions/observability/plugins/observability_onboarding/public/application/shared/logo_icon.tsx
@@ -6,13 +6,15 @@
  */
 
 import type { EuiAvatarProps, EuiIconProps } from '@elastic/eui';
-import { EuiAvatar, EuiIcon } from '@elastic/eui';
+import { EuiAvatar, EuiIcon, useEuiTheme } from '@elastic/eui';
 import type { EuiIconType } from '@elastic/eui/src/components/icon/icon';
+import { css } from '@emotion/react';
 import { useKibana } from '@kbn/kibana-react-plugin/public';
 import React from 'react';
 
 export type SupportedLogo =
   | 'aws'
+  | 'aws_ecs'
   | 'azure'
   | 'docker'
   | 'dotnet'
@@ -35,13 +37,16 @@ export type SupportedLogo =
   | 'mongodb'
   | 'apache_tomcat'
   | 'firehose'
+  | 'fluentbit'
   | 'linux'
+  | 'windows'
   | 'apple_black'
   | 'apple_white';
 
 export function isSupportedLogo(logo: string): logo is SupportedLogo {
   return [
     'aws',
+    'aws_ecs',
     'azure',
     'docker',
     'dotnet',
@@ -63,7 +68,9 @@ export function isSupportedLogo(logo: string): logo is SupportedLogo {
     'kafka',
     'mongodb',
     'apache_tomcat',
+    'fluentbit',
     'linux',
+    'windows',
     'apple',
   ].includes(logo);
 }
@@ -88,6 +95,8 @@ function useIconForLogo(logo?: SupportedLogo): string | undefined {
       return 'logoPrometheus';
     case 'docker':
       return 'logoDocker';
+    case 'windows':
+      return 'logoWindows';
     default:
       return http?.staticAssets.getPluginAssetHref(`${logo}.svg`);
   }
@@ -101,27 +110,49 @@ export interface LogoIconProps {
   isAvatar?: boolean;
   size?: LogoIconSizeProp;
   className?: string;
+  type?: EuiAvatarProps['type'];
+  hasBorder?: boolean;
+  color?: EuiAvatarProps['color'];
 }
 
 function isAvatarSize(size: LogoIconSizeProp): size is EuiAvatarProps['size'] {
   return size !== 'original' && size !== 'xxl';
 }
 
-export function LogoIcon({ logo, euiIconType, isAvatar, size, className }: LogoIconProps) {
+export function LogoIcon({
+  logo,
+  euiIconType,
+  isAvatar,
+  size,
+  className,
+  type,
+  hasBorder,
+  color = 'subdued',
+}: LogoIconProps) {
   const iconType = useIconForLogo(logo);
-  if (euiIconType && isAvatar && isAvatarSize(size)) {
+  const resolvedIconType = euiIconType ?? iconType;
+  const { euiTheme } = useEuiTheme();
+  if (resolvedIconType && isAvatar && isAvatarSize(size)) {
     return (
       <EuiAvatar
-        color="subdued"
-        iconType={euiIconType}
+        color={color}
+        iconType={resolvedIconType}
         name="logoIcon"
         size={size}
+        type={type}
         className={className}
+        css={
+          hasBorder
+            ? css`
+                border: ${euiTheme.border.thin};
+              `
+            : undefined
+        }
       />
     );
   }
-  if (iconType || euiIconType) {
-    return <EuiIcon type={euiIconType ?? iconType!} size={size} className={className} />;
+  if (resolvedIconType) {
+    return <EuiIcon type={resolvedIconType} size={size} className={className} />;
   }
   return null;
 }
