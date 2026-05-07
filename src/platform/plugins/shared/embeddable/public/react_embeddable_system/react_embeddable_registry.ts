4e85d9bd5448ef8de02f509604eb92199c5acf36
Nathan Reese <reese.nathan@gmail.com>
2026-05-07 13:23:03 -0600

    Revert "revert embeddable check"
    This reverts commit f39a253e40c067dcc55e1d3fe1c2686d8ecfcf95.

--- src/platform/plugins/shared/embeddable/public/react_embeddable_system/react_embeddable_registry.ts
+++ src/platform/plugins/shared/embeddable/public/react_embeddable_system/react_embeddable_registry.ts
@@ -14,6 +14,8 @@ const registry: { [key: string]: () => Promise<EmbeddableFactory<any, any>> } = 
 
 export const TYPE_REGEX = /^[a-z_]+$/; // lowercase letters and underscores
 
+let isSetupComplete = false;
+
 /**
  * Registers an embeddable public defintion. This should be called at plugin setup time.
  * Be sure to register an embeddable server definition for this type.
@@ -30,6 +32,13 @@ export const registerEmbeddablePublicDefinition = <
   type: string,
   getFactory: () => Promise<EmbeddableFactory<SerializedState, Api>>
 ) => {
+  if (isSetupComplete)
+    throw new Error(
+      i18n.translate('embeddableApi.reactEmbeddable.setCompleteError', {
+        defaultMessage:
+          'Embeddables must be registered during plugin setup phase. Do not register embeddables asynchronously',
+      })
+    );
   if (registry[type] !== undefined)
     throw new Error(
       i18n.translate('embeddableApi.reactEmbeddable.factoryAlreadyExistsError', {
@@ -56,3 +65,7 @@ export const getReactEmbeddableFactory = async <
 ): Promise<EmbeddableFactory<SerializedState, Api> | undefined> => {
   return registry[key]?.();
 };
+
+export function closeSetup() {
+  isSetupComplete = true;
+}
