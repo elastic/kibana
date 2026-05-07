88eb18c4604ff66df29961540a987d2e70df2380
pgayvallet <pierre.gayvallet@elastic.co>
2026-05-07 08:19:06 +0200

    feat(agent-builder-workflows): add common constants module

---
+++ x-pack/platform/plugins/shared/agent_builder_workflows/common/index.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export {
+  WORKFLOW_YAML_ATTACHMENT_TYPE,
+  WORKFLOW_YAML_DIFF_ATTACHMENT_TYPE,
+  WORKFLOW_SML_TYPE,
+  workflowTools,
+} from './constants';
