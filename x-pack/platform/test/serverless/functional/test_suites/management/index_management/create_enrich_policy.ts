79a828d5a3e3200f4c3f154b131c8d3c0283011d
Ignacio Rivas <rivasign@gmail.com>
2026-05-08 09:06:27 +0200

    Remove skip label

--- x-pack/platform/test/serverless/functional/test_suites/management/index_management/create_enrich_policy.ts
+++ x-pack/platform/test/serverless/functional/test_suites/management/index_management/create_enrich_policy.ts
@@ -20,7 +20,7 @@ export default ({ getPageObjects, getService }: FtrProviderContext) => {
 
   describe('Create enrich policy', function () {
     // TimeoutError:  Waiting for element to be located By(css selector, [data-test-subj="enrichPoliciesEmptyPromptCreateButton"])
-    this.tags(['failsOnMKI']);
+    // this.tags(['failsOnMKI']);
 
     before(async () => {
       log.debug('Creating test index');
