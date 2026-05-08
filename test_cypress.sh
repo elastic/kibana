1690ebe7bc40d8b8b4e3c84a9a1306193feabe3d
Alex Szabo <alex.szabo@elastic.co>
2026-05-08 12:26:59 +0200

    try fixing axios with patches

--- test_cypress.sh
+++ test_cypress.sh
@@ -5,4 +5,4 @@ node "$REPO_ROOT/scripts/generate_tsconfig_runtime.js"
 # Cypress embeds tsx; it reads TSX_TSCONFIG_PATH (not TSX_TSCONFIG) for path resolution
 export TSX_TSCONFIG_PATH="$REPO_ROOT/tsconfig.runtime.json"
 cd "$REPO_ROOT/x-pack/solutions/security/test/security_solution_cypress"
-yarn cypress:ess --spec './cypress/e2e/detection_response/**/bulk_edit_rules.cy.ts'
+yarn cypress:ess --spec './cypress/e2e/detection_response/**/bulk_edit_rules.cy.ts' | tee "$REPO_ROOT/test_cypress.log"
