75ba6c385d655779faf4d6ecd435794253e08aad
heenawter <hannah.wright@elastic.co>
2026-05-08 11:07:08 -0600

    Add another test for errors

--- src/platform/plugins/shared/dashboard/public/services/user_activity_service.test.ts
+++ src/platform/plugins/shared/dashboard/public/services/user_activity_service.test.ts
@@ -11,6 +11,7 @@ import { waitFor } from '@testing-library/react';
 import { buildMockDashboardApi } from '../mocks';
 import { coreServices } from './kibana_services';
 import { getDashboardUserActivityService } from './user_activity_service';
+import { BehaviorSubject } from 'rxjs';
 
 describe(`user activity service`, () => {
   afterEach(() => {
@@ -25,20 +26,22 @@ describe(`user activity service`, () => {
     expect(coreServices.http.post).not.toBeCalled();
 
     api.userActivity$.next({ type: 'view', end: 1777676707204 });
-    expect(coreServices.http.post).toBeCalledWith(
-      expect.stringContaining('/internal/dashboard/user_activity/view/'),
-      {
-        asSystemRequest: true,
-        keepalive: true,
-        method: 'POST',
-        body: JSON.stringify({
-          title: 'My Dashboard',
-          start: 1777676707154,
-          end: 1777676707204,
-          tags: [],
-        }),
-      }
-    );
+    await waitFor(() => {
+      expect(coreServices.http.post).toBeCalledWith(
+        expect.stringContaining('/internal/dashboard/user_activity/view/'),
+        {
+          asSystemRequest: true,
+          keepalive: true,
+          method: 'POST',
+          body: JSON.stringify({
+            title: 'My Dashboard',
+            start: 1777676707154,
+            end: 1777676707204,
+            tags: [],
+          }),
+        }
+      );
+    });
   });
 
   it('tracks refresh', async () => {
@@ -49,28 +52,30 @@ describe(`user activity service`, () => {
     expect(coreServices.http.post).not.toBeCalled();
 
     api.userActivity$.next({ type: 'refresh', end: 1777676707204 });
-    expect(coreServices.http.post).toBeCalledWith(
-      expect.stringContaining('/internal/dashboard/user_activity/refresh/'),
-      {
-        asSystemRequest: true,
-        method: 'POST',
-        body: JSON.stringify({
-          title: 'My Dashboard',
-          start: 1777676707154,
-          end: 1777676707204,
-          tags: [],
-          meta: {
-            time_range: {
-              from: 'now-15m',
-              to: 'now',
+    await waitFor(() => {
+      expect(coreServices.http.post).toBeCalledWith(
+        expect.stringContaining('/internal/dashboard/user_activity/refresh/'),
+        {
+          asSystemRequest: true,
+          method: 'POST',
+          body: JSON.stringify({
+            title: 'My Dashboard',
+            start: 1777676707154,
+            end: 1777676707204,
+            tags: [],
+            meta: {
+              time_range: {
+                from: 'now-15m',
+                to: 'now',
+              },
+              query: { expression: 'hi', language: 'kql' },
+              panel_count: 0,
+              errors: [],
             },
-            query: { expression: 'hi', language: 'kql' },
-            panel_count: 0,
-            errors: [],
-          },
-        }),
-      }
-    );
+          }),
+        }
+      );
+    });
   });
 
   it('can track both view and refresh at the same time', async () => {
@@ -114,13 +119,13 @@ describe(`user activity service`, () => {
     api.userActivity$.next({ type: 'view', end: 1777676707204 });
     await waitFor(() => {
       expect(coreServices.http.post).toBeCalledTimes(1);
-      expect(coreServices.http.post).toBeCalledWith(
-        expect.stringContaining('/internal/dashboard/user_activity/view/'),
-        expect.objectContaining({
-          body: expect.stringContaining(`\"start\":1777676707154,\"end\":1777676707204`),
-        })
-      );
     });
+    expect(coreServices.http.post).toBeCalledWith(
+      expect.stringContaining('/internal/dashboard/user_activity/view/'),
+      expect.objectContaining({
+        body: expect.stringContaining(`\"start\":1777676707154,\"end\":1777676707204`),
+      })
+    );
   });
 
   it('can track multiple refresh events', async () => {
@@ -129,24 +134,27 @@ describe(`user activity service`, () => {
     api.userActivity$.next({ type: 'refresh', start: 1777676707154 });
     api.userActivity$.next({ type: 'refresh', start: 1777676707160 });
     api.userActivity$.next({ type: 'refresh', end: 1777676707204 });
-    api.userActivity$.next({ type: 'refresh', end: 1777676707210 });
+    await waitFor(() => {
+      expect(coreServices.http.post).toBeCalledTimes(1);
+    });
+    api.userActivity$.next({ type: 'refresh', end: 1777676707209 });
     await waitFor(() => {
       expect(coreServices.http.post).toBeCalledTimes(2);
-      expect(coreServices.http.post).nthCalledWith(
-        1,
-        expect.stringContaining('/internal/dashboard/user_activity/refresh/'),
-        expect.objectContaining({
-          body: expect.stringContaining(`\"start\":1777676707154,\"end\":1777676707204`),
-        })
-      );
-      expect(coreServices.http.post).nthCalledWith(
-        2,
-        expect.stringContaining('/internal/dashboard/user_activity/refresh/'),
-        expect.objectContaining({
-          body: expect.stringContaining(`\"start\":1777676707160,\"end\":1777676707210`),
-        })
-      );
     });
+    expect(coreServices.http.post).nthCalledWith(
+      1,
+      expect.stringContaining('/internal/dashboard/user_activity/refresh/'),
+      expect.objectContaining({
+        body: expect.stringContaining(`\"start\":1777676707154,\"end\":1777676707204`),
+      })
+    );
+    expect(coreServices.http.post).nthCalledWith(
+      2,
+      expect.stringContaining('/internal/dashboard/user_activity/refresh/'),
+      expect.objectContaining({
+        body: expect.stringContaining(`\"start\":1777676707160,\"end\":1777676707209`),
+      })
+    );
   });
 
   it('does not track if no matching start event', async () => {
@@ -154,5 +162,41 @@ describe(`user activity service`, () => {
     getDashboardUserActivityService(api);
     api.userActivity$.next({ type: 'view', end: 1777676707204 });
     expect(coreServices.http.post).not.toBeCalled();
+  });
+
+  it('reports panel errors for refresh events', async () => {
+    const { api } = buildMockDashboardApi();
+    const childrenSpy = jest.spyOn(api.children$, 'getValue');
+    childrenSpy.mockReturnValue({
+      child1: {},
+      child2: { blockingError$: new BehaviorSubject(new Error('this is an error')) },
+    });
+    getDashboardUserActivityService(api);
+
+    api.userActivity$.next({ type: 'refresh', start: 1777676707154 });
+    api.userActivity$.next({ type: 'refresh', end: 1777676707204 });
+    await waitFor(() => {
+      expect(coreServices.http.post).toBeCalled();
+    });
+    expect(coreServices.http.post).toBeCalledWith(
+      expect.stringContaining('/internal/dashboard/user_activity/refresh/'),
+      expect.objectContaining({
+        body: JSON.stringify({
+          title: 'My Dashboard',
+          start: 1777676707154,
+          end: 1777676707204,
+          tags: [],
+          meta: {
+            time_range: {
+              from: 'now-15m',
+              to: 'now',
+            },
+            query: { expression: 'hi', language: 'kql' },
+            panel_count: 0,
+            errors: [{ panel_id: 'child2', error: 'this is an error' }],
+          },
+        }),
+      })
+    );
   });
 });
