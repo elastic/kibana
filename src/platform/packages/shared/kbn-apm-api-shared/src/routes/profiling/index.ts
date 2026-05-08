ac53a9ef76e56ca30b07424b5532da25c5718a0b
Caue Marcondes <caue.marcondes@elastic.co>
2026-05-08 16:33:46 -0400

    hosts profiling

--- src/platform/packages/shared/kbn-apm-api-shared/src/routes/profiling/index.ts
+++ src/platform/packages/shared/kbn-apm-api-shared/src/routes/profiling/index.ts
@@ -9,13 +9,19 @@
 import { servicesFlamegraphRoute } from './flamegraph';
 import { servicesFunctionsRoute } from './functions';
 import { profilingStatusRoute } from './status';
+import { profilingHostsFlamegraphRoute } from './hosts_flamegraph';
+import { profilingHostsFunctionsRoute } from './hosts_functions';
 
 export const profilingRouteDefinitions = {
   flamegraph: servicesFlamegraphRoute,
   functions: servicesFunctionsRoute,
   status: profilingStatusRoute,
+  hostsFlamegraph: profilingHostsFlamegraphRoute,
+  hostsFunctions: profilingHostsFunctionsRoute,
 };
 
 export type { ServicesFlamegraphResponse } from './flamegraph';
 export type { ServicesFunctionsResponse } from './functions';
 export type { ProfilingStatusResponse } from './status';
+export type { ProfilingHostsFlamegraphResponse } from './hosts_flamegraph';
+export type { ProfilingHostsFunctionsResponse } from './hosts_functions';
