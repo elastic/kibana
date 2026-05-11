714b6dd812b87fc0437e4e72ec2025fcd009e2ef
Hashem <hashem.naara@elastic.co>
2026-05-11 12:21:48 +0300

    dedup

--- src/platform/plugins/shared/workflows_execution_engine/server/concurrency/concurrency_queue_drainer.test.ts
+++ src/platform/plugins/shared/workflows_execution_engine/server/concurrency/concurrency_queue_drainer.test.ts
@@ -7,15 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the "Elastic License
- * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
- * Public License, v 1"; you may not use this file except in compliance with, at
- * your election, the "Elastic License 2.0", the "GNU Affero General Public
- * License v3.0 only", or the "Server Side Public License, v 1".
- */
-
 import { ExecutionStatus } from '@kbn/workflows';
 import { drainConcurrencyQueueSlots } from './concurrency_queue_drainer';
 import type { WorkflowExecutionRepository } from '../repositories/workflow_execution_repository';
