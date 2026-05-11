327b6dd0b5a3e5e68b914657597b5c5fb299dcac
iblancof <irene.blanco@elastic.co>
2026-05-11 12:10:57 +0200

    Remove unused exitSpan helper

--- src/platform/packages/shared/kbn-synthtrace-client/src/lib/apm/otel/otel_instance.ts
+++ src/platform/packages/shared/kbn-synthtrace-client/src/lib/apm/otel/otel_instance.ts
@@ -62,14 +62,6 @@ export class OtelInstance extends Entity<ApmOtelFields> {
     });
   }
 
-  exitSpan({ name }: { name: string }) {
-    return new OtelSpan({
-      ...this.fields,
-      name,
-      kind: 'Client',
-    });
-  }
-
   dbExitSpan({ name, type, statement }: { name: string; type: string; statement?: string }) {
     return new OtelSpan({
       ...this.fields,
