89dbf1f62da25ea05f47e76d9c0ff337f4bc0492
Anton Dosov <anton.dosov@elastic.co>
2026-05-11 12:33:49 +0200

    load devtool async

--- src/platform/plugins/shared/developer_toolbar/public/plugin.tsx
+++ src/platform/plugins/shared/developer_toolbar/public/plugin.tsx
@@ -15,7 +15,6 @@ import type { InternalChromeStart } from '@kbn/core-chrome-browser-internal-type
 import { BehaviorSubject } from 'rxjs';
 import type { DeveloperToolbarItemProps } from '@kbn/developer-toolbar';
 import { NEXT_CHROME_FEATURE_FLAG_KEY } from '@kbn/core-chrome-feature-flags';
-import { ChromeNextToggle } from '@kbn/core-chrome-feature-flags/chrome_next_toggle';
 
 export type UnregisterItemFn = () => void;
 export interface DeveloperToolbarItemRegistry {
@@ -62,10 +61,12 @@ export class DeveloperToolbarPlugin
     });
 
     if (core.featureFlags.getBooleanValue(NEXT_CHROME_FEATURE_FLAG_KEY, false)) {
-      this.registerItem({
-        id: 'Chrome Next',
-        children: <ChromeNextToggle featureFlags={core.featureFlags} />,
-        priority: 1,
+      import('@kbn/core-chrome-feature-flags/chrome_next_toggle').then(({ ChromeNextToggle }) => {
+        this.registerItem({
+          id: 'Chrome Next',
+          children: <ChromeNextToggle featureFlags={core.featureFlags} />,
+          priority: 1,
+        });
       });
     }
 
