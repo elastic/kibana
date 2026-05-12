904ce36f5b51db317d6c6fed5172c14139eea4cd
ana.davydova@elastic.co <ana.davydova@elastic.co>
2026-05-12 15:56:24 +0200

    [SLO] Follow up PR - Register sloComposite saved object type unconditionally

--- x-pack/solutions/observability/plugins/slo/server/plugin.ts
+++ x-pack/solutions/observability/plugins/slo/server/plugin.ts
@@ -164,10 +164,7 @@ export class SLOPlugin
 
     core.savedObjects.registerType(slo);
     core.savedObjects.registerType(sloSettings);
-    if (isCompositeSloEnabled) {
-      // eslint-disable-next-line @kbn/eslint/no_conditional_saved_object_type_registration -- TODO: remove conditional registration; tracked for follow-up PR
-      core.savedObjects.registerType(sloComposite);
-    }
+    core.savedObjects.registerType(sloComposite);
 
     registerBurnRateRule(plugins.alerting, core.http.basePath, this.logger, ruleDataService, {
       alertsLocator,
