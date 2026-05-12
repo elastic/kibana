2e7dec05dd9f841cd851702392d5a98e1a967588
Caue Marcondes <caue.marcondes@elastic.co>
2026-05-12 15:44:08 -0400

    updating apm agent config exports

--- x-pack/solutions/observability/plugins/apm/common/agent_configuration/all_option.ts
+++ x-pack/solutions/observability/plugins/apm/common/agent_configuration/all_option.ts
@@ -5,26 +5,4 @@
  * 2.0.
  */
 
-import { i18n } from '@kbn/i18n';
-export const ALL_OPTION_VALUE = 'ALL_OPTION_VALUE';
-
-// human-readable label for service and environment. The "All" option should be translated.
-// Everything else should be returned verbatim
-export function getOptionLabel(value: string | undefined) {
-  if (value === undefined || value === ALL_OPTION_VALUE) {
-    return i18n.translate('xpack.apm.agentConfig.allOptionLabel', {
-      defaultMessage: 'All',
-    });
-  }
-
-  return value;
-}
-
-export function omitAllOption(value?: string) {
-  return value === ALL_OPTION_VALUE ? undefined : value;
-}
-
-export const ALL_OPTION = {
-  value: ALL_OPTION_VALUE,
-  label: getOptionLabel(ALL_OPTION_VALUE),
-};
+export { ALL_OPTION_VALUE, ALL_OPTION, getOptionLabel, omitAllOption } from '@kbn/apm-common';
