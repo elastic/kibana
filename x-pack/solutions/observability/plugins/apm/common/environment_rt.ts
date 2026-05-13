9a2e45b695a0377eed57bed93a422a6562e06ea1
Caue Marcondes <caue.marcondes@elastic.co>
2026-05-13 08:11:22 -0400

    moving env definitions

--- x-pack/solutions/observability/plugins/apm/common/environment_rt.ts
+++ x-pack/solutions/observability/plugins/apm/common/environment_rt.ts
@@ -5,4 +5,4 @@
  * 2.0.
  */
 
-export { environmentStringRt, environmentRt, type Environment } from '@kbn/apm-types-shared';
+export { environmentStringRt, environmentRt, type Environment } from '@kbn/apm-types';
