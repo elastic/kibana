19ad7acbcea21a183295118f405d72aea0c91110
Alex Szabo <alex.szabo@elastic.co>
2026-05-13 13:10:12 +0200

    update download artifact call sites to use the GCP-backed method to download kibana distributables

--- .buildkite/scripts/steps/cloud/build_and_deploy.sh
+++ .buildkite/scripts/steps/cloud/build_and_deploy.sh
@@ -47,7 +47,7 @@ esac
 echo "--- Download Kibana Distribution"
 
 mkdir -p ./target
-download_artifact "kibana-$VERSION-linux-x86_64.tar.zst" ./target --build "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
+download_tmp_artifact "kibana-$VERSION-linux-x86_64.tar.zst" ./target "${KIBANA_BUILD_ID:-$BUILDKITE_BUILD_ID}"
 
 echo "--- Build Cloud Distribution"
 ELASTICSEARCH_MANIFEST_URL="https://storage.googleapis.com/kibana-ci-es-snapshots-daily/$(jq -r '.version' package.json)/manifest-latest-verified.json"
