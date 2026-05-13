33ef576a8db8495133d556eee07e208cfab4e025
Coen Warmer <coen.warmer@gmail.com>
2026-05-13 14:14:03 +0200

    Cleanup

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_overview/overview_time_filter.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_detail_overview/overview_time_filter.tsx
@@ -5,15 +5,8 @@
  * 2.0.
  */
 
-import { EuiButtonIcon, EuiFlexGroup, EuiFlexItem, EuiSuperDatePicker } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiSuperDatePicker } from '@elastic/eui';
 import React from 'react';
-import { i18n } from '@kbn/i18n';
-import type { DiscoverAppLocatorParams } from '@kbn/discover-plugin/common';
-import { DISCOVER_APP_LOCATOR } from '@kbn/discover-plugin/common';
-import { Streams, getDiscoverEsqlQuery } from '@kbn/streams-schema';
-import { useKibana } from '../../hooks/use_kibana';
-import { useStreamDetail } from '../../hooks/use_stream_detail';
-import { useStreamsPrivileges } from '../../hooks/use_streams_privileges';
 import { useTimeRange } from '../../hooks/use_time_range';
 import { useTimeRangeUpdate } from '../../hooks/use_time_range_update';
 import { useTimefilter } from '../../hooks/use_timefilter';
@@ -22,32 +15,6 @@ export function OverviewTimeFilter() {
   const { rangeFrom, rangeTo } = useTimeRange();
   const { updateTimeRange } = useTimeRangeUpdate();
   const { refresh } = useTimefilter();
-  const { definition } = useStreamDetail();
-  const { features } = useStreamsPrivileges();
-  const {
-    dependencies: {
-      start: { share },
-    },
-  } = useKibana();
-
-  const isIngestStream = Streams.ingest.all.GetResponse.is(definition);
-  const esqlQuery = getDiscoverEsqlQuery({
-    definition: definition.stream,
-    indexMode: isIngestStream ? definition.index_mode ?? 'standard' : undefined,
-    includeMetadata: Streams.WiredStream.GetResponse.is(definition),
-    useViews: features.wiredStreamViews.enabled,
-  });
-
-  const discoverHref = share.url.locators.useUrl<DiscoverAppLocatorParams>(
-    () => ({
-      id: DISCOVER_APP_LOCATOR,
-      params: {
-        query: { esql: esqlQuery ?? '' },
-        timeRange: { from: rangeFrom, to: rangeTo },
-      },
-    }),
-    [esqlQuery, rangeFrom, rangeTo]
-  );
 
   return (
     <EuiFlexGroup gutterSize="xs">
