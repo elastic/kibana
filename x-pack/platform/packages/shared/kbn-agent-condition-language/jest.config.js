10f782c9d0871edaa69f2a3e99fdc00ff9a63605
MichelLosier <michel.losier@elastic.co>
2026-05-13 11:12:40 -0700

    Fix package dir casing for agent-condition-language

--- x-pack/platform/packages/shared/kbn-agent-condition-language/jest.config.js
+++ x-pack/platform/packages/shared/kbn-agent-condition-language/jest.config.js
@@ -8,5 +8,5 @@
 module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../..',
-  roots: ['<rootDir>/x-pack/platform/packages/shared/kbn_agent_condition_language'],
+  roots: ['<rootDir>/x-pack/platform/packages/shared/kbn-agent-condition-language'],
 };
