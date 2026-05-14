ff70a3ce059070fec8eee187fd0d9e1c80950633
heenawter <hannah.wright@elastic.co>
2026-05-14 16:01:34 -0600

    Fix import

--- src/platform/plugins/shared/dashboard/public/dashboard_api/load_dashboard_api/load_dashboard_api.test.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_api/load_dashboard_api/load_dashboard_api.test.ts
@@ -11,7 +11,7 @@ import { Subject } from 'rxjs';
 
 import { DEFAULT_DASHBOARD_STATE } from '../default_dashboard_state';
 import { loadDashboardApi } from './load_dashboard_api';
-jest.mock('../performance/query_performance_tracking', () => {
+jest.mock('../telemetry/dashboard_load_telemetry', () => {
   return {
     startQueryPerformanceTracking: jest.fn(),
   };
