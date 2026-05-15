96519cd89918403a399ddfedf21dc47cfcc094d2
Caue Marcondes <caue.marcondes@elastic.co>
2026-05-15 11:31:35 -0400

    limits

--- x-pack/platform/plugins/shared/apm_sources_access/common/config_schema.ts
+++ x-pack/platform/plugins/shared/apm_sources_access/common/config_schema.ts
@@ -7,9 +7,30 @@
 
 import { type TypeOf, schema } from '@kbn/config-schema';
 
-export {
-  indicesSchema,
-  configSchema,
-  type APMSourcesAccessConfig,
-  type APMIndices,
-} from '@kbn/apm-types';
+/**
+ * Schema for APM indices
+ */
+export const indicesSchema = schema.object({
+  transaction: schema.string({ defaultValue: 'traces-apm*,apm-*,traces-*.otel-*' }), // TODO: remove apm-* pattern in 9.0
+  span: schema.string({ defaultValue: 'traces-apm*,apm-*,traces-*.otel-*' }),
+  error: schema.string({ defaultValue: 'logs-apm*,apm-*,logs-*.otel-*' }),
+  metric: schema.string({ defaultValue: 'metrics-apm*,apm-*,metrics-*.otel-*' }),
+  onboarding: schema.string({ defaultValue: 'apm-*' }), // Unused: to be deleted
+  sourcemap: schema.string({ defaultValue: 'apm-*' }), // Unused: to be deleted
+});
+
+/**
+ * Schema for APM Sources configuration
+ */
+export const configSchema = schema.object({
+  indices: indicesSchema,
+});
+
+/**
+ * Schema for APM Sources configuration
+ */
+export type APMSourcesAccessConfig = TypeOf<typeof configSchema>;
+/**
+ * Schema for APM indices
+ */
+export type APMIndices = APMSourcesAccessConfig['indices'];
