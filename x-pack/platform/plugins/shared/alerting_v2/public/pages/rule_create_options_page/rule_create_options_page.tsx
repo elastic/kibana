85eb510ef758d54d0aa90e1692d4b8092a8a90a8
Dominique Belcher <dominique.clarke@elastic.co>
2026-05-15 17:13:14 -0400

    alerting v2 - add create with agent entry point for onboarding and rule list

--- x-pack/platform/plugins/shared/alerting_v2/public/pages/rule_create_options_page/rule_create_options_page.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/pages/rule_create_options_page/rule_create_options_page.tsx
@@ -7,17 +7,31 @@
 
 import React from 'react';
 import { EuiPageHeader, EuiSpacer } from '@elastic/eui';
+import { CoreStart, useService } from '@kbn/core-di-browser';
 import { FormattedMessage } from '@kbn/i18n-react';
+import { AGENT_BUILDER_APP_ID } from '@kbn/deeplinks-agent-builder';
 import { useBreadcrumbs } from '../../hooks/use_breadcrumbs';
 import { useComposeDiscoverFlyout } from '../../hooks/use_compose_discover_flyout';
 import { RuleCreateOptionsPanel } from '../../components/rule_create_options/rule_create_options_panel';
-import { paths } from '../../constants';
+import {
+  paths,
+  CREATE_WITH_AGENT_INITIAL_PROMPT,
+  AGENT_BUILDER_NEW_CONVERSATION_PATH,
+} from '../../constants';
 
 export const RuleCreateOptionsPage = () => {
+  const application = useService(CoreStart('application'));
   useBreadcrumbs('rule_create_options');
   const { flyout, openCreateFlyout } = useComposeDiscoverFlyout({
     createSuccessRedirectPath: paths.ruleList,
   });
+
+  const navigateToAgentBuilder = () => {
+    application.navigateToApp(AGENT_BUILDER_APP_ID, {
+      path: AGENT_BUILDER_NEW_CONVERSATION_PATH,
+      state: { initialMessage: CREATE_WITH_AGENT_INITIAL_PROMPT },
+    });
+  };
 
   return (
     <div>
@@ -30,7 +44,10 @@ export const RuleCreateOptionsPage = () => {
         }
       />
       <EuiSpacer size="m" />
-      <RuleCreateOptionsPanel onCreateEsqlRule={openCreateFlyout} />
+      <RuleCreateOptionsPanel
+        onCreateEsqlRule={openCreateFlyout}
+        onCreateWithAgent={navigateToAgentBuilder}
+      />
       {flyout}
     </div>
   );
--- x-pack/platform/plugins/shared/alerting_v2/public/pages/rules_list_page/rules_list_page.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/pages/rules_list_page/rules_list_page.tsx
@@ -25,12 +25,17 @@ import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
 import { useBoolean, useDebouncedValue } from '@kbn/react-hooks';
 import type { FindRulesSortField } from '@kbn/alerting-v2-schemas';
+import { AGENT_BUILDER_APP_ID } from '@kbn/deeplinks-agent-builder';
 import type { RuleApiResponse } from '../../services/rules_api';
 import { useFetchRules } from '../../hooks/use_fetch_rules';
 import { useFetchRuleTags } from '../../hooks/use_fetch_rule_tags';
 import { useBreadcrumbs } from '../../hooks/use_breadcrumbs';
 import { useComposeDiscoverFlyout } from '../../hooks/use_compose_discover_flyout';
-import { paths } from '../../constants';
+import {
+  paths,
+  CREATE_WITH_AGENT_INITIAL_PROMPT,
+  AGENT_BUILDER_NEW_CONVERSATION_PATH,
+} from '../../constants';
 
 import { RulesListTableContainer } from './rules_list_table_container';
 import type { RulesListTableSortField } from './rules_list_table';
@@ -55,11 +60,19 @@ const TABLE_FIELD_TO_API_SORT_FIELD = Object.fromEntries(
 
 export const RulesListPage = () => {
   const http = useService(CoreStart('http'));
+  const application = useService(CoreStart('application'));
   useBreadcrumbs('rules_list');
 
   const [isCreateMenuOpen, { off: closeCreateMenu, toggle: toggleCreateMenu }] = useBoolean(false);
   const createMenuId = useGeneratedHtmlId({ prefix: 'createRuleMenu' });
   const { flyout, openCreateFlyout, openEditFlyout } = useComposeDiscoverFlyout();
+
+  const navigateToAgentBuilder = () => {
+    application.navigateToApp(AGENT_BUILDER_APP_ID, {
+      path: AGENT_BUILDER_NEW_CONVERSATION_PATH,
+      state: { initialMessage: CREATE_WITH_AGENT_INITIAL_PROMPT },
+    });
+  };
 
   const [page, setPage] = useState(1);
   const [perPage, setPerPage] = useState(DEFAULT_PER_PAGE);
@@ -182,6 +195,18 @@ export const RulesListPage = () => {
                                   },
                                   'data-test-subj': 'createRuleFlyoutButton',
                                 },
+                                {
+                                  name: i18n.translate(
+                                    'xpack.alertingV2.rulesList.createWithAgentButton',
+                                    { defaultMessage: 'Create with agent' }
+                                  ),
+                                  icon: 'sparkles',
+                                  onClick: () => {
+                                    closeCreateMenu();
+                                    navigateToAgentBuilder();
+                                  },
+                                  'data-test-subj': 'createWithAgentButton',
+                                },
                               ],
                             },
                           ]}
@@ -220,7 +245,12 @@ export const RulesListPage = () => {
           <EuiSpacer />
         </>
       ) : null}
-      {showEmptyState ? <RuleCreateOptionsPanel onCreateEsqlRule={openCreateFlyout} /> : null}
+      {showEmptyState ? (
+        <RuleCreateOptionsPanel
+          onCreateEsqlRule={openCreateFlyout}
+          onCreateWithAgent={navigateToAgentBuilder}
+        />
+      ) : null}
       {hasRules || hasActiveFilters ? (
         <>
           <EuiFlexGroup gutterSize="s">
