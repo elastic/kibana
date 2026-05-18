819caadda0798946a10967242c536efaccb022d1
Bharat Pasupula <bharat.pasupula@elastic.co>
2026-05-18 10:25:02 +0200

    Add step state hook

---
+++ x-pack/platform/plugins/shared/ingest_hub/public/onboarding/use_step_state.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useCallback, useMemo } from 'react';
+import useSessionStorage from 'react-use/lib/useSessionStorage';
+import { ONBOARDING_STEPS } from './steps';
+
+type StepStatus = 'incomplete' | 'complete';
+type StepState = Record<string, StepStatus>;
+
+function buildDefaultState(): StepState {
+  return Object.fromEntries(ONBOARDING_STEPS.map((s) => [s.id, 'incomplete' as StepStatus]));
+}
+
+export function useStepState(integrationId: string) {
+  const storageKey = `onboarding.${integrationId}.stepState`;
+  const [state, setState] = useSessionStorage<StepState>(storageKey, buildDefaultState());
+
+  const completedSteps = useMemo(
+    () =>
+      new Set(
+        Object.entries(state ?? {})
+          .filter(([, v]) => v === 'complete')
+          .map(([k]) => k)
+      ),
+    [state]
+  );
+
+  const markStepComplete = useCallback(
+    (stepId: string) => {
+      setState({ ...state, [stepId]: 'complete' });
+    },
+    [state, setState]
+  );
+
+  const resetSteps = useCallback(() => {
+    setState(buildDefaultState());
+  }, [setState]);
+
+  const firstIncompleteStepId = useMemo(() => {
+    const step = ONBOARDING_STEPS.find((s) => !completedSteps.has(s.id));
+    return step?.id ?? ONBOARDING_STEPS[0].id;
+  }, [completedSteps]);
+
+  return { completedSteps, markStepComplete, resetSteps, firstIncompleteStepId };
+}
