3b852edd5e4a8cc61714c8bd425ed8b169367e58
Bharat Pasupula <bharat.pasupula@elastic.co>
2026-05-18 10:27:25 +0200

    Add steps shell components

---
+++ x-pack/platform/plugins/shared/ingest_hub/public/onboarding/onboarding_shell.tsx
@@ -0,0 +1,79 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { useEffect, useMemo } from 'react';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
+import type { EuiStepProps } from '@elastic/eui';
+import { EuiSteps } from '@elastic/eui';
+
+import { ONBOARDING_STEPS } from './steps';
+import { useStepState } from './use_step_state';
+import { ConnectStep } from './steps/connect_step';
+import { ServicesStep } from './steps/services_step';
+import { NameAndScopeStep } from './steps/name_and_scope_step';
+import { DeploymentStep } from './steps/deployment_step';
+import { SeeDataStep } from './steps/see_data_step';
+
+const STEP_COMPONENTS: Record<string, React.ComponentType> = {
+  connect: ConnectStep,
+  services: ServicesStep,
+  'name-and-scope': NameAndScopeStep,
+  deployment: DeploymentStep,
+  'see-data': SeeDataStep,
+};
+
+export function OnboardingShell() {
+  const { integrationId } = useParams<{ integrationId: string }>();
+  const history = useHistory();
+  const location = useLocation();
+  const { completedSteps, firstIncompleteStepId } = useStepState(integrationId);
+
+  const currentStepId = location.hash ? location.hash.slice(1) : '';
+  const isValidStep = ONBOARDING_STEPS.some((s) => s.id === currentStepId);
+
+  useEffect(() => {
+    if (!isValidStep) {
+      history.replace({ ...location, hash: `#${firstIncompleteStepId}` });
+    }
+  }, [isValidStep, firstIncompleteStepId, history, location]);
+
+  const stepsConfig: EuiStepProps[] = useMemo(
+    () =>
+      ONBOARDING_STEPS.map((step) => {
+        const isCurrent = step.id === currentStepId;
+        const isComplete = completedSteps.has(step.id);
+
+        let status: EuiStepProps['status'] = 'incomplete';
+        if (isCurrent) {
+          status = 'current';
+        } else if (isComplete) {
+          status = 'complete';
+        }
+
+        const StepComponent = isCurrent ? STEP_COMPONENTS[step.id] : undefined;
+
+        return {
+          title: step.title,
+          status,
+          children: StepComponent ? <StepComponent /> : null,
+          'data-test-subj': `onboardingStepIndicator-${step.id}`,
+          ...(isComplete && !isCurrent
+            ? {
+                onClick: () => history.push({ ...location, hash: `#${step.id}` }),
+              }
+            : {}),
+        };
+      }),
+    [currentStepId, completedSteps, history, location]
+  );
+
+  if (!isValidStep) {
+    return null;
+  }
+
+  return <EuiSteps steps={stepsConfig} data-test-subj="onboardingShell" />;
+}
---
+++ x-pack/platform/plugins/shared/ingest_hub/public/onboarding/steps/index.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export { ConnectStep } from './connect_step';
+export { ServicesStep } from './services_step';
+export { NameAndScopeStep } from './name_and_scope_step';
+export { DeploymentStep } from './deployment_step';
+export { SeeDataStep } from './see_data_step';
