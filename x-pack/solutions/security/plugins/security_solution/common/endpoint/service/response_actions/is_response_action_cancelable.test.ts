f1d529e0699bef6e4d6f0041b777bc28402707c6
Paul Tavares <paul.tavares@elastic.co>
2026-05-18 11:41:06 -0400

    Fix tests

--- x-pack/solutions/security/plugins/security_solution/common/endpoint/service/response_actions/is_response_action_cancelable.test.ts
+++ x-pack/solutions/security/plugins/security_solution/common/endpoint/service/response_actions/is_response_action_cancelable.test.ts
@@ -10,126 +10,123 @@ import {
   isResponseActionCancelable,
 } from './is_response_action_cancelable';
 
-describe('isResponseActionCancelable()', () => {
-  describe('when agentType is `endpoint`', () => {
-    it.each([
-      'get-file',
-      'execute',
-      'upload',
-      'scan',
-      'runscript',
-      'cancel',
-      'memory-dump',
-    ] as const)('should return true for command `%s`', (command) => {
-      expect(isResponseActionCancelable(command, 'endpoint')).toBe(true);
+describe('cancelable response actions util', () => {
+  describe('isResponseActionCancelable()', () => {
+    describe('when agentType is `endpoint`', () => {
+      it.each(['get-file', 'execute', 'upload', 'scan', 'runscript', 'memory-dump'] as const)(
+        'should return true for command `%s`',
+        (command) => {
+          expect(isResponseActionCancelable(command, 'endpoint')).toBe(true);
+        }
+      );
+
+      it.each([
+        'cancel',
+        'isolate',
+        'unisolate',
+        'kill-process',
+        'suspend-process',
+        'running-processes',
+      ] as const)('should return false for command `%s`', (command) => {
+        expect(isResponseActionCancelable(command, 'endpoint')).toBe(false);
+      });
     });
 
-    it.each([
-      'isolate',
-      'unisolate',
-      'kill-process',
-      'suspend-process',
-      'running-processes',
-    ] as const)('should return false for command `%s`', (command) => {
-      expect(isResponseActionCancelable(command, 'endpoint')).toBe(false);
+    describe('when agentType is `sentinel_one`', () => {
+      it.each([
+        'isolate',
+        'unisolate',
+        'kill-process',
+        'suspend-process',
+        'running-processes',
+        'get-file',
+        'execute',
+        'upload',
+        'scan',
+        'cancel',
+        'runscript',
+        'memory-dump',
+      ] as const)('should return false for command `%s`', (command) => {
+        expect(isResponseActionCancelable(command, 'sentinel_one')).toBe(false);
+      });
+    });
+
+    describe('when agentType is `crowdstrike`', () => {
+      it.each([
+        'isolate',
+        'unisolate',
+        'kill-process',
+        'suspend-process',
+        'running-processes',
+        'get-file',
+        'execute',
+        'upload',
+        'scan',
+        'cancel',
+        'runscript',
+        'memory-dump',
+      ] as const)('should return false for command `%s`', (command) => {
+        expect(isResponseActionCancelable(command, 'crowdstrike')).toBe(false);
+      });
+    });
+
+    describe('when agentType is `microsoft_defender_endpoint`', () => {
+      it.each([
+        'isolate',
+        'unisolate',
+        'kill-process',
+        'suspend-process',
+        'running-processes',
+        'get-file',
+        'execute',
+        'upload',
+        'scan',
+        'runscript',
+        'memory-dump',
+      ] as const)('should return true for command `%s`', (command) => {
+        expect(isResponseActionCancelable(command, 'microsoft_defender_endpoint')).toBe(true);
+      });
+
+      it('should return false for command `cancel`', () => {
+        expect(isResponseActionCancelable('cancel', 'microsoft_defender_endpoint')).toBe(false);
+      });
     });
   });
 
-  describe('when agentType is `sentinel_one`', () => {
-    it.each([
-      'isolate',
-      'unisolate',
-      'kill-process',
-      'suspend-process',
-      'running-processes',
-      'get-file',
-      'execute',
-      'upload',
-      'scan',
-      'cancel',
-      'runscript',
-      'memory-dump',
-    ] as const)('should return false for command `%s`', (command) => {
-      expect(isResponseActionCancelable(command, 'sentinel_one')).toBe(false);
-    });
-  });
-
-  describe('when agentType is `crowdstrike`', () => {
-    it.each([
-      'isolate',
-      'unisolate',
-      'kill-process',
-      'suspend-process',
-      'running-processes',
-      'get-file',
-      'execute',
-      'upload',
-      'scan',
-      'cancel',
-      'runscript',
-      'memory-dump',
-    ] as const)('should return false for command `%s`', (command) => {
-      expect(isResponseActionCancelable(command, 'crowdstrike')).toBe(false);
-    });
-  });
-
-  describe('when agentType is `microsoft_defender_endpoint`', () => {
-    it.each([
-      'isolate',
-      'unisolate',
-      'kill-process',
-      'suspend-process',
-      'running-processes',
-      'get-file',
-      'execute',
-      'upload',
-      'scan',
-      'runscript',
-      'memory-dump',
-    ] as const)('should return true for command `%s`', (command) => {
-      expect(isResponseActionCancelable(command, 'microsoft_defender_endpoint')).toBe(true);
+  describe('getListOfCancelableResponseActions()', () => {
+    it('should return cancelable commands for `endpoint`', () => {
+      expect(getListOfCancelableResponseActions('endpoint')).toEqual([
+        'get-file',
+        'execute',
+        'upload',
+        'scan',
+        'runscript',
+        'memory-dump',
+      ]);
     });
 
-    it('should return false for command `cancel`', () => {
-      expect(isResponseActionCancelable('cancel', 'microsoft_defender_endpoint')).toBe(false);
+    it('should return empty list for `sentinel_one`', () => {
+      expect(getListOfCancelableResponseActions('sentinel_one')).toEqual([]);
     });
-  });
-});
 
-describe('getListOfCancelableResponseActions()', () => {
-  it('should return cancelable commands for `endpoint`', () => {
-    expect(getListOfCancelableResponseActions('endpoint')).toEqual([
-      'get-file',
-      'execute',
-      'upload',
-      'scan',
-      'runscript',
-      'cancel',
-      'memory-dump',
-    ]);
-  });
+    it('should return empty list for `crowdstrike`', () => {
+      expect(getListOfCancelableResponseActions('crowdstrike')).toEqual([]);
+    });
 
-  it('should return empty list for `sentinel_one`', () => {
-    expect(getListOfCancelableResponseActions('sentinel_one')).toEqual([]);
-  });
-
-  it('should return empty list for `crowdstrike`', () => {
-    expect(getListOfCancelableResponseActions('crowdstrike')).toEqual([]);
-  });
-
-  it('should return cancelable commands for `microsoft_defender_endpoint`', () => {
-    expect(getListOfCancelableResponseActions('microsoft_defender_endpoint')).toEqual([
-      'isolate',
-      'unisolate',
-      'kill-process',
-      'suspend-process',
-      'running-processes',
-      'get-file',
-      'execute',
-      'upload',
-      'scan',
-      'runscript',
-      'memory-dump',
-    ]);
+    it('should return cancelable commands for `microsoft_defender_endpoint`', () => {
+      expect(getListOfCancelableResponseActions('microsoft_defender_endpoint')).toEqual([
+        'isolate',
+        'unisolate',
+        'kill-process',
+        'suspend-process',
+        'running-processes',
+        'get-file',
+        'execute',
+        'upload',
+        'scan',
+        'runscript',
+        'memory-dump',
+      ]);
+    });
   });
 });
