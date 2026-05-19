9d46cb5edb4eef5d3a3d63805b4e114f8e84fb8e
Caue Marcondes <caue.marcondes@elastic.co>
2026-05-19 09:33:38 -0400

    addressing PR comments

--- x-pack/platform/packages/shared/kbn-apm-types/src/service_anomaly_stats.ts
+++ x-pack/platform/packages/shared/kbn-apm-types/src/service_anomaly_stats.ts
@@ -4,12 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import type { ServiceHealthStatus } from './service_health_status';
 
 export interface ServiceAnomalyStats {
   transactionType?: string;
   anomalyScore?: number;
   actualValue?: number;
   jobId?: string;
-  healthStatus: ServiceHealthStatus;
 }
