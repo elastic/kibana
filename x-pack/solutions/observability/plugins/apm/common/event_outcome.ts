2350c9e55c1f0811b0bce3baa6c6370fdeaa04b5
Caue Marcondes <caue.marcondes@elastic.co>
2026-05-19 14:17:04 -0400

    fixing event outcome

--- x-pack/solutions/observability/plugins/apm/common/event_outcome.ts
+++ x-pack/solutions/observability/plugins/apm/common/event_outcome.ts
@@ -4,4 +4,8 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-export { EventOutcome } from '@kbn/apm-types';
+export enum EventOutcome {
+  success = 'success',
+  failure = 'failure',
+  unknown = 'unknown',
+}
