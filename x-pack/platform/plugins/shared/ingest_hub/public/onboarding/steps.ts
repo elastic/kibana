bbb0eb7eaa00b796551cd482732e44b0fd6e8b78
Bharat Pasupula <bharat.pasupula@elastic.co>
2026-05-20 14:13:52 +0200

    Use i18n for step headers

--- x-pack/platform/plugins/shared/ingest_hub/public/onboarding/steps.ts
+++ x-pack/platform/plugins/shared/ingest_hub/public/onboarding/steps.ts
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+import { i18n } from '@kbn/i18n';
 
 export interface OnboardingStepConfig {
   id: string;
@@ -11,9 +12,34 @@ export interface OnboardingStepConfig {
 }
 
 export const ONBOARDING_STEPS: OnboardingStepConfig[] = [
-  { id: 'connect', title: 'Connect' },
-  { id: 'services', title: 'Services' },
-  { id: 'name-and-scope', title: 'Name & Scope' },
-  { id: 'deployment', title: 'Deployment' },
-  { id: 'see-data', title: 'See Data' },
+  {
+    id: 'connect',
+    title: i18n.translate('xpack.ingestHub.onboarding.steps.connect.title', {
+      defaultMessage: 'Connect',
+    }),
+  },
+  {
+    id: 'services',
+    title: i18n.translate('xpack.ingestHub.onboarding.steps.services.title', {
+      defaultMessage: 'Services',
+    }),
+  },
+  {
+    id: 'name-and-scope',
+    title: i18n.translate('xpack.ingestHub.onboarding.steps.nameAndScope.title', {
+      defaultMessage: 'Name & Scope',
+    }),
+  },
+  {
+    id: 'deployment',
+    title: i18n.translate('xpack.ingestHub.onboarding.steps.deployment.title', {
+      defaultMessage: 'Deployment',
+    }),
+  },
+  {
+    id: 'see-data',
+    title: i18n.translate('xpack.ingestHub.onboarding.steps.seeData.title', {
+      defaultMessage: 'See Data',
+    }),
+  },
 ];
