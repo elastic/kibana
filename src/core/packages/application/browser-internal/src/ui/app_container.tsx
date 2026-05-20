47ca722c1f6c46b13dd9e60f73d52d3b2b71f5ea
Tyler Smalley <tylersmalley@gmail.com>
2026-05-20 17:23:36 +0000

    Replace Elastic logo loading icon with heart shape
    Co-authored-by: Ona <no-reply@ona.com>

--- src/core/packages/application/browser-internal/src/ui/app_container.tsx
+++ src/core/packages/application/browser-internal/src/ui/app_container.tsx
@@ -11,7 +11,7 @@ import { css, keyframes } from '@emotion/react';
 import type { Observable } from 'rxjs';
 import type { FC, MutableRefObject } from 'react';
 import React, { Fragment, useLayoutEffect, useRef, useState } from 'react';
-import { EuiLoadingElastic, EuiLoadingSpinner, useEuiTheme } from '@elastic/eui';
+import { EuiLoadingSpinner, EuiIcon, useEuiTheme } from '@elastic/eui';
 
 import { i18n } from '@kbn/i18n';
 import type { CoreTheme } from '@kbn/core-theme-browser';
@@ -128,6 +128,74 @@ export const AppContainer: FC<Props> = ({
   );
 };
 
+const heartPulse = keyframes({
+  '0%': { transform: 'scale3d(.2, .2, -.7)' },
+  '40%': { transform: 'scale3d(1, 1, 2)' },
+  '50%': { transform: 'scale3d(.99, .99, 2)' },
+  '70%': { transform: 'scale3d(.96, .96, -2.5)' },
+  '100%': { transform: 'scale3d(.98, .98, 2)' },
+});
+
+const heartLoadingStyles = css({
+  '& .euiIcon path': {
+    animationName: heartPulse,
+    animationFillMode: 'forwards',
+    animationDirection: 'alternate',
+    transformStyle: 'preserve-3d',
+    animationDuration: '1s',
+    animationTimingFunction: 'cubic-bezier(0, 0.63, 0.49, 1)',
+    animationIterationCount: 'infinite',
+    transformOrigin: '50% 50%',
+    '&:nth-of-type(1)': { animationDelay: '0s' },
+    '&:nth-of-type(2)': { animationDelay: '0.035s' },
+    '&:nth-of-type(3)': { animationDelay: '0.125s' },
+    '&:nth-of-type(4)': { animationDelay: '0.155s' },
+    '&:nth-of-type(5)': { animationDelay: '0.075s' },
+    '&:nth-of-type(6)': { animationDelay: '0.06s' },
+  },
+});
+
+const HeartIcon: FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} viewBox="0 0 32 32" {...props}>
+    <path
+      fill="#FEC514"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M16 7 C14.5 4.5 12 3 9.5 3 C5.5 3 2 6 2 10.5 C2 12.5 2.8 14.5 4.5 17 L16 7 Z"
+    />
+    <path
+      fill="#02BCB7"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M4.5 17 C7 20.5 11 24.5 16 29 L16 7 L4.5 17 Z"
+    />
+    <path
+      fill="#F04E98"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M16 7 C17.5 4.5 20 3 22.5 3 C26.5 3 30 6 30 10.5 C30 12.5 29.2 14.5 27.5 17 L16 7 Z"
+    />
+    <path
+      fill="#0B64DD"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M27.5 17 C25 20.5 21 24.5 16 29 L16 7 L27.5 17 Z"
+    />
+    <path
+      fill="#9ADC30"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M9.5 3 C5.5 3 2 6 2 10.5 C2 11 2.05 11.5 2.15 12 L16 7 C14.5 4.5 12 3 9.5 3 Z"
+    />
+    <path
+      fill="#1BA9F5"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M22.5 3 C26.5 3 30 6 30 10.5 C30 11 29.95 11.5 29.85 12 L16 7 C17.5 4.5 20 3 22.5 3 Z"
+    />
+  </svg>
+);
+
 const AppLoadingPlaceholder: FC<{ showPlainSpinner: boolean }> = ({ showPlainSpinner }) => {
   const { euiTheme } = useEuiTheme();
   const appContainerFadeIn = keyframes({
@@ -160,12 +228,13 @@ const AppLoadingPlaceholder: FC<{ showPlainSpinner: boolean }> = ({ showPlainSpi
     );
   }
   return (
-    <EuiLoadingElastic
-      css={appContainerStyles}
+    <EuiIcon
+      type={HeartIcon}
+      size="xxl"
+      css={[appContainerStyles, heartLoadingStyles]}
       aria-label={i18n.translate('core.application.appContainer.loadingAriaLabel', {
         defaultMessage: 'Loading application',
       })}
-      size="xxl"
     />
   );
 };
--- src/core/packages/chrome/browser-components/src/shared/loading_indicator.tsx
+++ src/core/packages/chrome/browser-components/src/shared/loading_indicator.tsx
@@ -8,7 +8,7 @@
  */
 
 import { Global, css } from '@emotion/react';
-import { EuiLoadingSpinner, EuiProgress, EuiIcon, EuiImage } from '@elastic/eui';
+import { EuiLoadingSpinner, EuiProgress, EuiImage } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import React from 'react';
 import { useIsLoading } from './chrome_hooks';
@@ -17,6 +17,54 @@ export interface LoadingIndicatorProps {
   showAsBar?: boolean;
   customLogo?: string;
 }
+
+const HeartLogo = ({ size = 24, ...props }: { size?: number } & React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 32 32"
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="#FEC514"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M16 7 C14.5 4.5 12 3 9.5 3 C5.5 3 2 6 2 10.5 C2 12.5 2.8 14.5 4.5 17 L16 7 Z"
+    />
+    <path
+      fill="#02BCB7"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M4.5 17 C7 20.5 11 24.5 16 29 L16 7 L4.5 17 Z"
+    />
+    <path
+      fill="#F04E98"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M16 7 C17.5 4.5 20 3 22.5 3 C26.5 3 30 6 30 10.5 C30 12.5 29.2 14.5 27.5 17 L16 7 Z"
+    />
+    <path
+      fill="#0B64DD"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M27.5 17 C25 20.5 21 24.5 16 29 L16 7 L27.5 17 Z"
+    />
+    <path
+      fill="#9ADC30"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M9.5 3 C5.5 3 2 6 2 10.5 C2 11 2.05 11.5 2.15 12 L16 7 C14.5 4.5 12 3 9.5 3 Z"
+    />
+    <path
+      fill="#1BA9F5"
+      stroke="#fff"
+      strokeWidth="0.8"
+      d="M22.5 3 C26.5 3 30 6 30 10.5 C30 11 29.95 11.5 29.85 12 L16 7 C17.5 4.5 20 3 22.5 3 Z"
+    />
+  </svg>
+);
 
 export const LoadingIndicator = ({ showAsBar = false, customLogo }: LoadingIndicatorProps) => {
   const isLoading = useIsLoading();
@@ -39,14 +87,14 @@ export const LoadingIndicator = ({ showAsBar = false, customLogo }: LoadingIndic
       })}
     />
   ) : (
-    <EuiIcon
-      type={'logoElastic'}
-      size="l"
+    <span
       data-test-subj={testSubj}
       aria-label={i18n.translate('core.ui.chrome.headerGlobalNav.logoAriaLabel', {
         defaultMessage: 'Elastic Logo',
       })}
-    />
+    >
+      <HeartLogo size={24} />
+    </span>
   );
 
   const logo = isLoading ? (
