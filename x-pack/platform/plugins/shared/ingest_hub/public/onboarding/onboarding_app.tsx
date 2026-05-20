fe275bc16bc79348121aa62b443fb4735703f626
Bharat Pasupula <bharat.pasupula@elastic.co>
2026-05-20 16:06:37 +0200

    redirect to integrations on base path

--- x-pack/platform/plugins/shared/ingest_hub/public/onboarding/onboarding_app.tsx
+++ x-pack/platform/plugins/shared/ingest_hub/public/onboarding/onboarding_app.tsx
@@ -5,22 +5,42 @@
  * 2.0.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import type { AppMountParameters, CoreStart } from '@kbn/core/public';
 import { Router, Route } from '@kbn/shared-ux-router';
+import { useLocation } from 'react-router-dom';
+import { useKibana } from '@kbn/kibana-react-plugin/public';
+import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
 
 import { OnboardingShell } from './onboarding_shell';
+
+const DEFAULT_RETURN_APP = 'integrations';
+
+function RootRedirect() {
+  const { services } = useKibana();
+  const location = useLocation<{ returnApp?: string } | undefined>();
+  useEffect(() => {
+    const returnApp = location.state?.returnApp || DEFAULT_RETURN_APP;
+    services.application?.navigateToApp(returnApp);
+  }, [services.application, location.state]);
+  return null;
+}
 
 export function renderOnboardingApp(coreStart: CoreStart, params: AppMountParameters) {
   const root = createRoot(params.element);
   root.render(
     coreStart.rendering.addContext(
-      <Router history={params.history}>
-        <Route path="/:integrationId">
-          <OnboardingShell />
-        </Route>
-      </Router>
+      <KibanaContextProvider services={coreStart}>
+        <Router history={params.history}>
+          <Route exact path="/">
+            <RootRedirect />
+          </Route>
+          <Route path="/:integrationId">
+            <OnboardingShell />
+          </Route>
+        </Router>
+      </KibanaContextProvider>
     )
   );
   return () => root.unmount();
