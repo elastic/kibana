7bac73afa7f80dc3a7b638ed6e2d1396f50cee4f
Kevin Delemme <kdelemme@gmail.com>
2026-05-20 12:26:16 -0400

    Reuse workflowApi from workflow-ui

--- x-pack/platform/plugins/shared/alerting_v2/public/hooks/use_fetch_workflows.ts
+++ x-pack/platform/plugins/shared/alerting_v2/public/hooks/use_fetch_workflows.ts
@@ -9,7 +9,7 @@ import { useQuery } from '@kbn/react-query';
 import { useService, CoreStart } from '@kbn/core-di-browser';
 import { i18n } from '@kbn/i18n';
 import type { WorkflowListDto } from '@kbn/workflows';
-import { WorkflowsApi } from '../services/workflows_api';
+import { WorkflowApi } from '@kbn/workflows-ui';
 import { workflowKeys } from './query_key_factory';
 
 interface UseFetchWorkflowsParams {
@@ -19,12 +19,12 @@ interface UseFetchWorkflowsParams {
 }
 
 export const useFetchWorkflows = ({ query, tags, isEnabled = true }: UseFetchWorkflowsParams) => {
-  const workflowsApi = useService(WorkflowsApi);
+  const workflowsApi = useService(WorkflowApi);
   const { toasts } = useService(CoreStart('notifications'));
 
   return useQuery<WorkflowListDto, Error>({
     queryKey: workflowKeys.search({ query, tags }),
-    queryFn: () => workflowsApi.searchWorkflows({ query, tags, size: 100, page: 1 }),
+    queryFn: () => workflowsApi.getWorkflows({ query, tags, size: 100, page: 1 }),
     enabled: isEnabled,
     refetchOnWindowFocus: false,
     keepPreviousData: true,
