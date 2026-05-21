5fa04fd7162b88d599a36f1dedc7c785d705e079
Jordan <51442161+JordanSh@users.noreply.github.com>
2026-05-21 09:09:56 +0300

    SIEM Readiness init agent skill (#269284)

--- x-pack/solutions/security/plugins/security_solution/public/siem_readiness/hooks/use_siem_readiness_api.ts
+++ x-pack/solutions/security/plugins/security_solution/public/siem_readiness/hooks/use_siem_readiness_api.ts
@@ -13,15 +13,15 @@ import type {
   SiemReadinessPackageInfo,
   RelatedIntegrationRuleResponse,
   DataQualityResultDocument,
-  PipelineStats,
-  RetentionResponse,
-} from './types';
+  ContinuityPayload,
+  RetentionPayload,
+} from '@kbn/siem-readiness';
 import {
   GET_SIEM_READINESS_CATEGORIES_API_PATH,
   GET_SIEM_READINESS_PIPELINES_API_PATH,
   GET_SIEM_READINESS_RETENTION_API_PATH,
   GET_INDEX_RESULTS_LATEST_API_PATH,
-} from './constants';
+} from '@kbn/siem-readiness';
 
 const REFETCH_INTERVAL = 30000;
 
@@ -77,7 +77,7 @@ export const useSiemReadinessApi = () => {
   const getReadinessPipelines = useQuery({
     queryKey: GET_READINESS_PIPELINES_QUERY_KEY,
     queryFn: () => {
-      return http.get<PipelineStats[]>(GET_SIEM_READINESS_PIPELINES_API_PATH);
+      return http.get<ContinuityPayload>(GET_SIEM_READINESS_PIPELINES_API_PATH);
     },
     refetchInterval: REFETCH_INTERVAL,
   });
@@ -85,7 +85,7 @@ export const useSiemReadinessApi = () => {
   const getReadinessRetention = useQuery({
     queryKey: GET_READINESS_RETENTION_QUERY_KEY,
     queryFn: () => {
-      return http.get<RetentionResponse>(GET_SIEM_READINESS_RETENTION_API_PATH);
+      return http.get<RetentionPayload>(GET_SIEM_READINESS_RETENTION_API_PATH);
     },
     refetchInterval: REFETCH_INTERVAL,
   });
