c935397b137a590fba180b283844c857190d468a
Kelvin Tan <kelvin.tan@elastic.co>
2026-05-21 11:18:54 -0400

    Revert "merge conflicts"
    This reverts commit 8dd7f28a02b4172d59ede69e3ae2e93c11bd89bf.

--- x-pack/solutions/security/plugins/security_solution/public/detections/components/alerts_table/timeline_actions/use_run_alert_workflow_panel.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/detections/components/alerts_table/timeline_actions/use_run_alert_workflow_panel.tsx
@@ -9,11 +9,7 @@ import React, { useMemo } from 'react';
 
 import type { EuiContextMenuPanelDescriptor } from '@elastic/eui';
 import type { EcsSecurityExtension as Ecs } from '@kbn/securitysolution-ecs';
-import {
-  useOptionalExecutionTracker,
-  useWorkflowsCapabilities,
-  useWorkflowsUIEnabledSetting,
-} from '@kbn/workflows-ui';
+import { useWorkflowsCapabilities, useWorkflowsUIEnabledSetting } from '@kbn/workflows-ui';
 import type { AlertTableContextMenuItem } from '../types';
 import { useAlertsPrivileges } from '../../../containers/detection_engine/alerts/use_alerts_privileges';
 import * as i18n from '../translations';
@@ -42,24 +38,6 @@ export const AlertWorkflowsPanel = ({ alertIds, onClose, onExecute }: AlertWorkf
     [alertIds]
   );
 
-  const executionTracker = useOptionalExecutionTracker();
-
-  const onSuccess = (executionId: string, workflowId: string, workflowName?: string) => {
-    executionTracker?.trackExecutions([
-      {
-        id: executionId,
-        workflowId,
-        workflowName,
-        inputSummary: [
-          {
-            label: i18n.ALERTS_COUNT_LABEL,
-            value: String(alertIds.length),
-          },
-        ],
-      },
-    ]);
-  };
-
   return (
     <RunWorkflowPanel
       inputs={inputs}
@@ -67,7 +45,6 @@ export const AlertWorkflowsPanel = ({ alertIds, onClose, onExecute }: AlertWorkf
       executeButtonTestSubj="execute-alert-workflow-button"
       onClose={onClose}
       onExecute={onExecute}
-      onSuccess={onSuccess}
     />
   );
 };
