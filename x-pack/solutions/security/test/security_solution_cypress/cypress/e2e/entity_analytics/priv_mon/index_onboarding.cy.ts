acb50a22767c1462e87d9ff55fbeea2b17ef7f2c
Ying <ying.mao@elastic.co>
2026-05-20 21:34:01 -0400

    Fixing tests

--- x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/entity_analytics/priv_mon/index_onboarding.cy.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/entity_analytics/priv_mon/index_onboarding.cy.ts
@@ -36,6 +36,7 @@ describe(
         kbnServerArgs: [
           `--xpack.securitySolution.enableExperimental=${JSON.stringify([
             'disable:entityAnalyticsEntityStoreV2',
+            'disable:entityAnalyticsWatchlistEnabled',
           ])}`,
           '--uiSettings.overrides.securitySolution:entityStoreEnableV2=false',
         ],
