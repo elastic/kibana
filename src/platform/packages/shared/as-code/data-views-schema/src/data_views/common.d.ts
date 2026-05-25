fc70c0fdbea68dd583c71e258d07c256c2955b3a
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-05-25 08:16:53 +0000

    Changes from node scripts/check

---
+++ src/platform/packages/shared/as-code/data-views-schema/src/data_views/common.d.ts
@@ -0,0 +1,3 @@
+export declare const indexPatternSchema: import("@kbn/config-schema").Type<string>;
+export declare const timeFieldSchema: import("@kbn/config-schema").Type<string | undefined>;
+export declare const fieldSettingsFieldNameSchema: import("@kbn/config-schema").Type<string>;
---
+++ src/platform/packages/shared/as-code/data-views-transforms/index.d.ts
@@ -0,0 +1,4 @@
+export * from './src/from_stored_data_view';
+export * from './src/from_stored_fields';
+export * from './src/to_stored_data_view';
+export * from './src/to_stored_fields';
