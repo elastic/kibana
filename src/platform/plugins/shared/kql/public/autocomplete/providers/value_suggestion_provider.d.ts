3b9ca5d28fbd1f5c395dfaf5d558398a334f48f5
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-05-27 08:36:55 +0000

    Changes from node scripts/check

--- src/platform/plugins/shared/kql/public/autocomplete/providers/value_suggestion_provider.d.ts
+++ src/platform/plugins/shared/kql/public/autocomplete/providers/value_suggestion_provider.d.ts
@@ -1,3 +1,12 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
 import type { CoreSetup } from '@kbn/core/public';
 import type { ValueSuggestionsMethod } from '@kbn/data-plugin/common';
 import type { DataView, DataViewField } from '@kbn/data-views-plugin/common';
@@ -5,18 +14,24 @@ import type { TimefilterSetup } from '@kbn/data-plugin/public';
 import type { AutocompleteUsageCollector } from '../collectors';
 export type ValueSuggestionsGetFn = (args: ValueSuggestionsGetFnArgs) => Promise<any[]>;
 interface ValueSuggestionsGetFnArgs {
-    indexPattern: DataView;
-    field: DataViewField;
-    query: string;
-    useTimeRange?: boolean;
-    boolFilter?: any[];
-    signal?: AbortSignal;
-    method?: ValueSuggestionsMethod;
-    querySuggestionKey?: 'rules' | 'cases' | 'alerts' | 'endpoints' | 'action_policies';
+  indexPattern: DataView;
+  field: DataViewField;
+  query: string;
+  useTimeRange?: boolean;
+  boolFilter?: any[];
+  signal?: AbortSignal;
+  method?: ValueSuggestionsMethod;
+  querySuggestionKey?: 'rules' | 'cases' | 'alerts' | 'endpoints' | 'action_policies';
 }
 export declare const getEmptyValueSuggestions: ValueSuggestionsGetFn;
-export declare const setupValueSuggestionProvider: (core: CoreSetup, { timefilter, usageCollector, }: {
+export declare const setupValueSuggestionProvider: (
+  core: CoreSetup,
+  {
+    timefilter,
+    usageCollector,
+  }: {
     timefilter: TimefilterSetup;
     usageCollector?: AutocompleteUsageCollector;
-}) => ValueSuggestionsGetFn;
+  }
+) => ValueSuggestionsGetFn;
 export {};
--- src/platform/plugins/shared/kql/public/components/typeahead/index.d.ts
+++ src/platform/plugins/shared/kql/public/components/typeahead/index.d.ts
@@ -1,4 +1,17 @@
-import React from 'react';
-declare const LazySuggestionsComponent: React.LazyExoticComponent<typeof import("./suggestions_component").SuggestionsComponent>;
-export declare const SuggestionsComponent: (props: React.ComponentProps<typeof LazySuggestionsComponent>) => React.JSX.Element;
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import type React from 'react';
+declare const LazySuggestionsComponent: React.LazyExoticComponent<
+  typeof import('./suggestions_component').SuggestionsComponent
+>;
+export declare const SuggestionsComponent: (
+  props: React.ComponentProps<typeof LazySuggestionsComponent>
+) => React.JSX.Element;
 export {};
