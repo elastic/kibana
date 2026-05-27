bdf60b416c951fa04a858217973527e288b68149
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-05-27 08:39:38 +0000

    Changes from node scripts/check

--- x-pack/solutions/observability/plugins/observability_onboarding/public/application/quickstart_flows/otel_apm/index.tsx
+++ x-pack/solutions/observability/plugins/observability_onboarding/public/application/quickstart_flows/otel_apm/index.tsx
@@ -346,15 +346,22 @@ function ConfigureSDKInstructions({
             {value}
           </EuiText>
           {value && (
-            <EuiButtonIcon
-              data-test-subj="apmConfigurationValueColumnButton"
-              aria-label={i18n.translate('xpack.observability_onboarding.otelApm.copyIconText', {
+            <EuiToolTip
+              content={i18n.translate('xpack.observability_onboarding.otelApm.copyIconText', {
                 defaultMessage: 'Copy to clipboard',
               })}
-              color="text"
-              iconType="copy"
-              onClick={() => copyToClipboard(value)}
-            />
+              disableScreenReaderOutput
+            >
+              <EuiButtonIcon
+                data-test-subj="apmConfigurationValueColumnButton"
+                aria-label={i18n.translate('xpack.observability_onboarding.otelApm.copyIconText', {
+                  defaultMessage: 'Copy to clipboard',
+                })}
+                color="text"
+                iconType="copy"
+                onClick={() => copyToClipboard(value)}
+              />
+            </EuiToolTip>
           )}
         </>
       ),
