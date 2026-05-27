6e624cfc9aefe350e76dd37275000f588ab6c4c6
Ihor Panasiuk <igorskynet13@gmail.com>
2026-05-27 11:56:22 +0200

    Update workflow_not_found_page.tsx

--- src/platform/plugins/shared/workflows_management/public/pages/workflow_detail/ui/workflow_not_found_page.tsx
+++ src/platform/plugins/shared/workflows_management/public/pages/workflow_detail/ui/workflow_not_found_page.tsx
@@ -7,8 +7,10 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { EuiButtonEmpty } from '@elastic/eui';
+import { EuiButtonEmpty, useEuiTheme } from '@elastic/eui';
+import { css } from '@emotion/react';
 import React from 'react';
+import { kbnFullBodyHeightCss } from '@kbn/css-utils/public/full_body_height_css';
 import { i18n } from '@kbn/i18n';
 import { NotFoundPrompt } from '@kbn/shared-ux-prompt-not-found';
 
@@ -21,26 +23,42 @@ export interface WorkflowNotFoundPageProps {
 }
 
 export const WorkflowNotFoundPage = ({ onBackToWorkflows }: WorkflowNotFoundPageProps) => {
+  const { euiTheme } = useEuiTheme();
+
   return (
-    <NotFoundPrompt
-      title={i18n.translate('workflows.workflowDetail.notFound.title', {
-        defaultMessage: 'Workflow not found',
-      })}
-      body={i18n.translate('workflows.workflowDetail.notFound.body', {
-        defaultMessage:
-          "Sorry, the workflow you're looking for can't be found. It might have been removed or renamed, or maybe it never existed at all.",
-      })}
-      actions={
-        <EuiButtonEmpty
-          iconType="chevronSingleLeft"
-          flush="both"
-          onClick={onBackToWorkflows}
-          aria-label={backToWorkflowsLabel}
-          data-test-subj="workflowDetailBackToWorkflowsButton"
-        >
-          {backToWorkflowsLabel}
-        </EuiButtonEmpty>
-      }
-    />
+    <div
+      css={[
+        kbnFullBodyHeightCss(),
+        css({
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          overflow: 'hidden',
+          backgroundColor: euiTheme.colors.emptyShade,
+        }),
+      ]}
+      data-test-subj="workflow-not-found-page"
+    >
+      <NotFoundPrompt
+        title={i18n.translate('workflows.workflowDetail.notFound.title', {
+          defaultMessage: 'Workflow not found',
+        })}
+        body={i18n.translate('workflows.workflowDetail.notFound.body', {
+          defaultMessage:
+            "Sorry, the workflow you're looking for can't be found. It might have been removed or renamed, or maybe it never existed at all.",
+        })}
+        actions={
+          <EuiButtonEmpty
+            iconType="chevronSingleLeft"
+            flush="both"
+            onClick={onBackToWorkflows}
+            aria-label={backToWorkflowsLabel}
+            data-test-subj="workflowDetailBackToWorkflowsButton"
+          >
+            {backToWorkflowsLabel}
+          </EuiButtonEmpty>
+        }
+      />
+    </div>
   );
 };
