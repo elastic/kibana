7d51747fa8c78932f64301bd73d29f7f72b8db18
Weronika Olejniczak <weronika.olejniczak@elastic.co>
2026-05-27 16:18:35 +0200

    feat(security-detection-rule-management): replace title props and wrap EuiButtonIcon with EuiToolTip

--- x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/comparison_side/comparison_side_help_info.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/detection_engine/rule_management/components/rule_details/three_way_diff/comparison_side/comparison_side_help_info.tsx
@@ -7,7 +7,7 @@
 
 import React from 'react';
 import useToggle from 'react-use/lib/useToggle';
-import { EuiPopover, EuiText, EuiButtonIcon } from '@elastic/eui';
+import { EuiButtonIcon, EuiPopover, EuiText, EuiToolTip } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import * as i18n from './translations';
 import type { VersionsPickerOptionEnum } from './versions_picker/versions_picker';
@@ -32,11 +32,13 @@ export function ComparisonSideHelpInfo({ options }: ComparisonSideHelpInfoProps)
   const optionsWithDescriptions = options.map((option) => getOptionDetails(option));
 
   const button = (
-    <EuiButtonIcon
-      iconType="question"
-      onClick={togglePopover}
-      aria-label={i18n.VERSION_COMPARISON_HELP_ARIA_LABEL}
-    />
+    <EuiToolTip content={i18n.VERSION_COMPARISON_HELP_ARIA_LABEL} disableScreenReaderOutput>
+      <EuiButtonIcon
+        iconType="question"
+        onClick={togglePopover}
+        aria-label={i18n.VERSION_COMPARISON_HELP_ARIA_LABEL}
+      />
+    </EuiToolTip>
   );
 
   return (
