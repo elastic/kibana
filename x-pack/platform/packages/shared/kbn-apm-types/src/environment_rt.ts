5ef49276e7b96b95d28badfa5c8c3806971fb233
Caue Marcondes <caue.marcondes@elastic.co>
2026-05-27 15:16:30 -0400

    reverting env

--- x-pack/platform/packages/shared/kbn-apm-types/src/environment_rt.ts
+++ x-pack/platform/packages/shared/kbn-apm-types/src/environment_rt.ts
@@ -4,10 +4,10 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
 import * as t from 'io-ts';
 import { nonEmptyStringRt } from '@kbn/io-ts-utils';
 import { ENVIRONMENT_ALL, ENVIRONMENT_NOT_DEFINED } from './environment_filter_values';
+
 export const environmentStringRt = t.union([
   t.literal(ENVIRONMENT_NOT_DEFINED.value),
   t.literal(ENVIRONMENT_ALL.value),
