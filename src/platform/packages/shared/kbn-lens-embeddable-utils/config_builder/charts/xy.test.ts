90f2d4ea5764ad2703e70b6201f8ac06ec6e1e0b
Abdul Zahid <awahab07@yahoo.com>
2026-05-29 01:35:52 +0200

    Update test after the preferredType is true advertised series type.

--- src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/charts/xy.test.ts
+++ src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/charts/xy.test.ts
@@ -138,7 +138,7 @@ test('generates xy chart config', async () => {
             "isVisible": true,
             "position": "left",
           },
-          "preferredSeriesType": "line",
+          "preferredSeriesType": "bar",
           "tickLabelsVisibilitySettings": Object {
             "x": true,
             "yLeft": true,
