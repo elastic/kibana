b385cfa2eebfc64348b2c7ce5cabf686fb915477
Gergő Ábrahám <gergo.abraham@elastic.co>
2026-05-28 15:22:50 +0200

    Revert "TEMP: run only affected test for flaky runner"
    This reverts commit 5837829ab99d96f9c128d58a7d91c6cf7ee86184.

--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/cypress_base.config.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/cypress_base.config.ts
@@ -77,13 +77,8 @@ export const getCypressBaseConfig = (
         // baseUrl: To override, set Env. variable `CYPRESS_BASE_URL`
         baseUrl: 'http://localhost:5601',
         supportFile: 'public/management/cypress/support/e2e.ts',
-
-        // TEMPORARY CHANGE --- REVERT BEFORE MERGE
-        specPattern:
-          'public/management/cypress/e2e/artifacts/endpoint_exceptions.cy.{js,jsx,ts,tsx}',
-        // TEMPORARY CHANGE --- REVERT BEFORE MERGE
+        specPattern: 'public/management/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
         experimentalRunAllSpecs: true,
-
         experimentalMemoryManagement: true,
         experimentalInteractiveRunEvents: true,
         setupNodeEvents: (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
