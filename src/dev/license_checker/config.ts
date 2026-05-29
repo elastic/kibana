903ad4db8ee16a2742c1239ee31e2f0caaf53dc8
Arturo Castillo Delgado <arturo.castillo@elastic.co>
2026-05-29 18:04:53 +0200

    [EUI] Test EUI #9690 drop setCustomValidity with local build
    Tests EUI PR https://github.com/elastic/eui/pull/9690 against Kibana
    using a local @elastic/eui tarball.

--- src/dev/license_checker/config.ts
+++ src/dev/license_checker/config.ts
@@ -96,6 +96,7 @@ export const LICENSE_OVERRIDES = {
   '@elastic/esql': ['Elastic License 2.0'],
   '@elastic/eui': ['Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0'],
   '@elastic/eui-theme-borealis': ['Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0'],
+  '@elastic/eui-theme-common': ['Elastic License 2.0 OR AGPL-3.0-only OR SSPL-1.0'],
   'language-subtag-registry@0.3.21': ['CC-BY-4.0'], // retired ODC‑By license https://github.com/mattcg/language-subtag-registry
   'buffers@0.1.1': ['MIT'], // license in importing module https://www.npmjs.com/package/binary
   '@bufbuild/protobuf@2.5.2': ['Apache-2.0'], // license (Apache-2.0 AND BSD-3-Clause)
