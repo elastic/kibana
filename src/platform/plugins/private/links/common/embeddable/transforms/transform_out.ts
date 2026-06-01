c4df9191617f9e07a6d6ab16afe5713811425313
heenawter <hannah.wright@elastic.co>
2026-06-01 13:25:11 -0600

    Undo part of d3b92f2cfe1279baf932c3861c32c3fdaa1210c3

--- src/platform/plugins/private/links/common/embeddable/transforms/transform_out.ts
+++ src/platform/plugins/private/links/common/embeddable/transforms/transform_out.ts
@@ -9,8 +9,9 @@
 
 import type { Reference } from '@kbn/content-management-utils';
 import { transformTitlesOut } from '@kbn/presentation-publishing';
+import type { LinksState } from '../../../server';
 import { LINKS_SAVED_OBJECT_TYPE } from '../../constants';
-import type { LinksEmbeddableState, StoredLinksEmbeddableState } from '../types';
+import type { LinksByValueState, LinksEmbeddableState, StoredLinksEmbeddableState } from '../types';
 import { type StoredLinksByValueState910, isLegacyState, transformLegacyState } from './bwc';
 import { getOptions } from './get_options';
 import { injectReferences } from './references';
@@ -18,35 +19,42 @@ import { injectReferences } from './references';
 export function transformOut(
   storedState: LinksEmbeddableState | StoredLinksEmbeddableState | StoredLinksByValueState910,
   references?: Reference[]
-) {
+): LinksEmbeddableState {
   const latestState = isLegacyState(storedState)
     ? transformLegacyState(storedState)
     : (storedState as StoredLinksEmbeddableState);
-  const state = {
-    ...transformTitlesOut(latestState),
-    // Strip legacy properties
-    ...(latestState.links
-      ? { links: latestState.links.map(({ order, id, ...link }) => link) }
-      : {}),
+
+  /** Handle title state, which is shared for both by-ref and by-value links panels */
+  const transformedTitleState = transformTitlesOut(latestState);
+  const titleState = {
+    ...(transformedTitleState.title?.length && { title: transformedTitleState.title }),
+    ...(transformedTitleState.description?.length && {
+      description: transformedTitleState.description,
+    }),
+    ...(transformedTitleState.hide_border && { hide_border: transformedTitleState.hide_border }),
+    ...(transformedTitleState.hide_title && { hide_title: transformedTitleState.hide_title }),
   };
 
-  // inject saved object reference when by-reference
+  /** Inject saved object reference when by-reference */
   const savedObjectRef = (references ?? []).find(
     ({ name, type }) => name === 'savedObjectRef' && type === LINKS_SAVED_OBJECT_TYPE
   );
   if (savedObjectRef) {
     return {
-      ...state,
+      ...titleState,
       ref_id: savedObjectRef.id,
     };
   }
 
-  // inject dashboard references when by-value
+  /** Inject dashboard references when by-value */
+  const byValueState = latestState as LinksByValueState;
+  const updatedLinks = latestState.links?.map(({ order, id, ...link }) => link); // strip legacy properties on each link
   return {
-    ...state,
-    links: injectReferences(state.links, references).map((link) => ({
+    ...titleState,
+    layout: byValueState.layout,
+    links: injectReferences(updatedLinks, references).map((link) => ({
       ...link,
       ...(link.options && { options: getOptions(link.type, link.options) }),
-    })),
+    })) as LinksState['links'],
   };
 }
