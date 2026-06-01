618ddee4c9943f882a86e5409b2eb9018545d717
Anton Dosov <anton.dosov@elastic.co>
2026-06-01 11:49:39 +0200

    remove AppHeaderWithFallback

--- src/core/packages/chrome/app-header/src/index.ts
+++ src/core/packages/chrome/app-header/src/index.ts
@@ -19,5 +19,3 @@ export type {
   AppHeaderPadding,
   AppHeaderConfig,
 } from './types';
-export type { AppHeaderWithFallbackProps } from './app_header_with_fallback';
-export { AppHeaderWithFallback } from './app_header_with_fallback';
