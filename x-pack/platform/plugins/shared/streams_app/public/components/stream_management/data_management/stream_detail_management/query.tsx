e6757bc56fbb369524b6a2b2b44ef5f8bfc04de4
Kerry Gallagher <471693+Kerry350@users.noreply.github.com>
2026-06-03 16:26:14 +0100

    Consolidate advanced tab

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_management/data_management/stream_detail_management/query.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_management/data_management/stream_detail_management/query.tsx
@@ -4,7 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { EuiFlexGroup, EuiPageHeader, EuiToolTip, useEuiTheme } from '@elastic/eui';
+import { EuiFlexGroup, EuiFlexItem, EuiPageHeader, useEuiTheme } from '@elastic/eui';
 import { css } from '@emotion/react';
 import { i18n } from '@kbn/i18n';
 import type { Streams } from '@kbn/streams-schema';
@@ -13,7 +13,6 @@ import { useStreamsAppParams } from '../../../../hooks/use_streams_app_params';
 import { useStreamsAppRouter } from '../../../../hooks/use_streams_app_router';
 import { useTimeRange } from '../../../../hooks/use_time_range';
 import { QueryStreamSchemaEditor } from '../../../query_streams/query_stream_schema_editor';
-import { QueryStreamsAdvancedView } from '../../../query_streams/query_streams_advanced_view';
 import { RedirectTo } from '../../../redirect_to';
 import { DiscoverBadgeButton, QueryStreamBadge } from '../../../stream_badges';
 import { StreamDetailAttachments } from '../../../stream_detail_attachments';
@@ -22,11 +21,11 @@ import { StreamsAppPageTemplate } from '../../../streams_app_page_template';
 import { useStreamsDetailManagementTabs } from './use_streams_detail_management_tabs';
 import type { ManagementTabs } from './wrapper';
 import { QueryStreamPartitioning } from '../stream_detail_routing/query_stream_partitioning';
+import { StreamDetailActionsMenu } from './stream_detail_actions_menu';
 
 const queryStreamManagementSubTabs = [
   'overview',
   'partitioning',
-  'advanced',
   'schema',
   'significantEvents',
   'attachments',
@@ -96,25 +95,6 @@ export function QueryStreamDetailManagement({
     tabs.significantEvents = significantEvents;
   }
 
-  tabs.advanced = {
-    content: (
-      <QueryStreamsAdvancedView definition={definition} refreshDefinition={refreshDefinition} />
-    ),
-    label: (
-      <EuiToolTip
-        content={i18n.translate('xpack.streams.queryStreamDetailManagement.advanced.tooltip', {
-          defaultMessage: 'View technical details about this query stream’s underlying setup',
-        })}
-      >
-        <span tabIndex={0}>
-          {i18n.translate('xpack.streams.queryStreamDetailManagement.advancedTab', {
-            defaultMessage: 'Advanced',
-          })}
-        </span>
-      </EuiToolTip>
-    ),
-  };
-
   if (!isValidManagementSubTab(tab) || !tabs[tab]?.content) {
     return (
       <RedirectTo path="/{key}/management/{tab}" params={{ path: { key, tab: 'overview' } }} />
@@ -130,14 +110,26 @@ export function QueryStreamDetailManagement({
           background: ${euiTheme.colors.backgroundBasePlain};
         `}
         pageTitle={
-          <EuiFlexGroup gutterSize="s" alignItems="center" justifyContent="spaceBetween">
-            <EuiFlexGroup alignItems="center" gutterSize="s">
-              {key}
-              <QueryStreamBadge />
-            </EuiFlexGroup>
-            <DiscoverBadgeButton stream={definition.stream} hasDataStream spellOut />
+          <EuiFlexGroup alignItems="center" gutterSize="s" responsive={false}>
+            {key}
+            <QueryStreamBadge />
           </EuiFlexGroup>
         }
+        rightSideItems={[
+          <EuiFlexGroup
+            key="streamDetailActions"
+            alignItems="center"
+            gutterSize="xs"
+            responsive={false}
+          >
+            <EuiFlexItem grow={false}>
+              <DiscoverBadgeButton stream={definition.stream} hasDataStream spellOut />
+            </EuiFlexItem>
+            <EuiFlexItem grow={false}>
+              <StreamDetailActionsMenu canDelete={true} definition={definition} />
+            </EuiFlexItem>
+          </EuiFlexGroup>,
+        ]}
         tabs={Object.entries(tabs).map(([tabKey, { label }]) => ({
           label,
           href: router.link('/{key}/management/{tab}', {
