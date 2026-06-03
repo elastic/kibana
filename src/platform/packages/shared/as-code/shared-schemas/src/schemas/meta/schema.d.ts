71c2ec138959559a0308371922c450b04095fbfd
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-06-03 13:37:49 +0000

    Changes from node scripts/check

--- src/platform/packages/shared/as-code/shared-schemas/src/schemas/meta/schema.d.ts
+++ src/platform/packages/shared/as-code/shared-schemas/src/schemas/meta/schema.d.ts
@@ -1,11 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
 import type { TypeOf } from '@kbn/config-schema';
-export declare const asCodeMetaSchema: import("@kbn/config-schema").ObjectType<{
-    created_at: import("@kbn/config-schema").Type<string | undefined>;
-    created_by: import("@kbn/config-schema").Type<string | undefined>;
-    managed: import("@kbn/config-schema").Type<boolean | undefined>;
-    owner: import("@kbn/config-schema").Type<string | undefined>;
-    updated_at: import("@kbn/config-schema").Type<string | undefined>;
-    updated_by: import("@kbn/config-schema").Type<string | undefined>;
-    version: import("@kbn/config-schema").Type<string | undefined>;
+export declare const asCodeMetaSchema: import('@kbn/config-schema').ObjectType<{
+  created_at: import('@kbn/config-schema').Type<string | undefined>;
+  created_by: import('@kbn/config-schema').Type<string | undefined>;
+  managed: import('@kbn/config-schema').Type<boolean | undefined>;
+  owner: import('@kbn/config-schema').Type<string | undefined>;
+  updated_at: import('@kbn/config-schema').Type<string | undefined>;
+  updated_by: import('@kbn/config-schema').Type<string | undefined>;
+  version: import('@kbn/config-schema').Type<string | undefined>;
 }>;
 export type AsCodeMeta = TypeOf<typeof asCodeMetaSchema>;
--- src/platform/packages/shared/as-code/shared-schemas/src/schemas/pagination/pagination.d.ts
+++ src/platform/packages/shared/as-code/shared-schemas/src/schemas/pagination/pagination.d.ts
@@ -1,9 +1,18 @@
-export declare const asCodePaginationParamsSchema: import("@kbn/config-schema").ObjectType<{
-    page: import("@kbn/config-schema").Type<number>;
-    per_page: import("@kbn/config-schema").Type<number>;
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+export declare const asCodePaginationParamsSchema: import('@kbn/config-schema').ObjectType<{
+  page: import('@kbn/config-schema').Type<number>;
+  per_page: import('@kbn/config-schema').Type<number>;
 }>;
-export declare const asCodePaginationResponseMetaSchema: import("@kbn/config-schema").ObjectType<{
-    page: import("@kbn/config-schema").Type<number>;
-    per_page: import("@kbn/config-schema").Type<number>;
-    total: import("@kbn/config-schema").Type<number>;
+export declare const asCodePaginationResponseMetaSchema: import('@kbn/config-schema').ObjectType<{
+  page: import('@kbn/config-schema').Type<number>;
+  per_page: import('@kbn/config-schema').Type<number>;
+  total: import('@kbn/config-schema').Type<number>;
 }>;
