9638392ae04f0d35e75546d2732d4e4cf1602ee5
Kevin Delemme <kevin.delemme@elastic.co>
2026-06-05 10:02:16 -0400

    Extract get composite suggestions

---
+++ x-pack/platform/packages/shared/kbn-slo-schema/src/rest_specs/routes/composite_slo/index.ts
@@ -0,0 +1,16 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+export * from './composite_slo_create';
+export * from './composite_slo_get';
+export * from './composite_slo_batch_get';
+export * from './composite_slo_find';
+export * from './composite_slo_update';
+export * from './composite_slo_delete';
+export * from './composite_slo_historical_summary';
+export * from './composite_slo_summary_refresh';
+export * from './get_composite_slo_suggestions';
