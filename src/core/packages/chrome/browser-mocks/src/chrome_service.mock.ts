622d996040e0b6343512555e7bb35ff9bb258d1a
Anton Dosov <anton.dosov@elastic.co>
2026-06-05 15:42:06 +0200

    post merge fix

--- src/core/packages/chrome/browser-mocks/src/chrome_service.mock.ts
+++ src/core/packages/chrome/browser-mocks/src/chrome_service.mock.ts
@@ -24,7 +24,6 @@ import type {
 } from '@kbn/core-chrome-browser-internal-types';
 import { lazyObject } from '@kbn/lazy-object';
 import { sidebarServiceMock } from '@kbn/core-chrome-sidebar-mocks';
-import type { ReactNode } from 'react';
 
 const createSetupContractMock = (): DeeplyMockedKeys<InternalChromeSetup> => {
   return lazyObject({
@@ -151,12 +150,6 @@ const createStartContractMock = () => {
         set: jest.fn((content?: ReactNode) => {
           nextContextSwitcherState$.next(content ?? null);
         }),
-      }),
-      contextSwitcher: lazyObject({
-        set: jest.fn((content?: ReactNode) => {
-          new BehaviorSubject<ReactNode>(null).next(content ?? null);
-        }),
-        get$: jest.fn().mockReturnValue(new BehaviorSubject<ReactNode>(null)),
       }),
       inlineAppHeader: lazyObject({
         get$: jest.fn().mockReturnValue(inlineAppHeaderState$),
