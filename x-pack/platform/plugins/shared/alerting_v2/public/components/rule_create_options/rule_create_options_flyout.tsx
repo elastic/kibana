e3b1aac7513f8530b96bfb08ffb6dd51ef67615a
ana.davydova@elastic.co <ana.davydova@elastic.co>
2026-06-05 12:14:31 +0200

    fix for ci

--- x-pack/platform/plugins/shared/alerting_v2/public/components/rule_create_options/rule_create_options_flyout.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/components/rule_create_options/rule_create_options_flyout.tsx
@@ -14,6 +14,7 @@ import {
   EuiFlyoutBody,
   EuiFlyoutHeader,
   EuiTitle,
+  EuiToolTip,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { FormattedMessage } from '@kbn/i18n-react';
