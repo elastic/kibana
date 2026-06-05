f05e68784adb5624e83720a7449ebbdd1b7a38c4
Michael Dokolin <mikhail.dokolin@elastic.co>
2026-06-05 16:00:35 +0200

    Add plugin B

---
+++ examples/dependency_injection_isolation/b/server/route.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { inject, injectable } from 'inversify';
+import { Response } from '@kbn/core-di-server';
+import type { KibanaResponseFactory } from '@kbn/core-http-server';
+
+@injectable()
+export class TestRoute {
+  static method = 'get' as const;
+  static path = '/api/di/b';
+  static validate = {};
+  static options = {
+    access: 'public',
+  } as const;
+  static security = {
+    authc: {
+      enabled: false,
+      reason: '',
+    },
+    authz: {
+      enabled: false,
+      reason: '',
+    },
+  } as const;
+
+  constructor(@inject(Response) private readonly response: KibanaResponseFactory) {}
+
+  handle() {
+    return this.response.ok({
+      headers: { 'content-type': 'application/json' },
+      body: { message: 'B' },
+    });
+  }
+}
