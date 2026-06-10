9b8bcd768d5acee03a14564a7e89e4a5d84b513b
ana.davydova@elastic.co <ana.davydova@elastic.co>
2026-06-10 14:43:40 +0200

    fix to ci

--- x-pack/platform/plugins/shared/alerting_v2/public/create_rule_options_flyout.test.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/create_rule_options_flyout.test.tsx
@@ -26,7 +26,6 @@ const createMockServices = (): RuleFormServices => ({
   notifications: notificationServiceMock.createStartContract(),
   application: applicationServiceMock.createStartContract(),
   lens: lensPluginMock.createStartContract(),
-  workflowForm: { Component: () => null, defaultValue: () => ({}) },
   uiActions: uiActionsPluginMock.createStartContract(),
 });
 
