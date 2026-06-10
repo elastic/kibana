6869e806711b45fe618d57f11ce58979defb7c8f
Kevin Delemme <kevin.delemme@elastic.co>
2026-06-10 13:50:09 +0200

    feat(alerting_v2): Emit and register workflow triggers for alert actions (#272504)

--- x-pack/platform/plugins/shared/alerting_v2/public/lib/workflow_extensions/triggers/episode_unsnoozed.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/lib/workflow_extensions/triggers/episode_unsnoozed.tsx
@@ -6,10 +6,10 @@
  */
 
 import type { PublicTriggerDefinition } from '@kbn/workflows-extensions/public';
-import { episodeAssignedTriggerCommonDefinition } from '../../../../common/workflows/triggers';
+import { episodeUnsnoozedTriggerCommonDefinition } from '../../../../common/workflows/triggers';
 import { AlertingTriggerIcon } from './alerting_trigger_icon';
 
-export const episodeAssignedTriggerPublicDefinition: PublicTriggerDefinition = {
-  ...episodeAssignedTriggerCommonDefinition,
+export const episodeUnsnoozedTriggerPublicDefinition: PublicTriggerDefinition = {
+  ...episodeUnsnoozedTriggerCommonDefinition,
   icon: AlertingTriggerIcon,
 };
