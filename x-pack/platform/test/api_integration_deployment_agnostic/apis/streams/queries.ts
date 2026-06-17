3068a23d370c016e15cfa0e9d27ad4a47520a8f5
klacabane <kevin.lacabane@elastic.co>
2026-06-17 12:02:41 +0000

    sort queries before comparing

--- x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/queries.ts
+++ x-pack/platform/test/api_integration_deployment_agnostic/apis/streams/queries.ts
@@ -425,11 +425,8 @@ export default function ({ getService }: DeploymentAgnosticFtrProviderContext) {
       expect(bulkResponse).to.have.property('acknowledged', true);
 
       const getQueriesResponse = await getQueries(apiClient, STREAM_NAME);
-      expect(getQueriesResponse.queries).to.eql([
-        firstQuery,
-        { ...updateThirdQuery, type: 'match' },
-        { ...newQuery, type: 'match' },
-      ]);
+      const expectedQueries = [firstQuery, { ...updateThirdQuery, type: 'match' }, { ...newQuery, type: 'match' }];
+      expect(sortBy(getQueriesResponse.queries, 'id')).to.eql(sortBy(expectedQueries, 'id'));
 
       const updatedRules = await alertingApi.searchRules(roleAuthc, '');
       expect(updatedRules.body.data).to.have.length(3);
