f2f3b0bf564e43ecc7dc71c974e4abeb1b51ce11
Hannah Mudge <Heenawter@users.noreply.github.com>
2026-06-17 08:43:00 -0600

    Update limit of title + description

--- src/platform/packages/shared/presentation/presentation_publishing_schemas/src/titles_schema.ts
+++ src/platform/packages/shared/presentation/presentation_publishing_schemas/src/titles_schema.ts
@@ -12,7 +12,7 @@ import { schema } from '@kbn/config-schema';
 export const serializedTitlesSchema = schema.object({
   description: schema.maybe(
     schema.string({
-      maxLength: 1000,
+      maxLength: 10_000,
       meta: {
         description: 'A short description of the panel.',
       },
@@ -27,7 +27,7 @@ export const serializedTitlesSchema = schema.object({
   ),
   title: schema.maybe(
     schema.string({
-      maxLength: 500,
+      maxLength: 1_000,
       meta: {
         description: 'The panel title.',
       },
