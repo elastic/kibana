9259128de6acf1b9a2b93b0c41a9e441e0b28caf
Mykhailo Kondrat <mykhailo.kondrat@elastic.co>
2026-06-18 10:58:33 +0200

    redesgin show more button

---
+++ x-pack/platform/plugins/shared/cases/public/components/cases_redesign/user_actions/hooks/use_builder_context.ts
@@ -0,0 +1,85 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useMemo } from 'react';
+import { useEuiTheme } from '@elastic/eui';
+import type { UserProfileWithAvatar } from '@kbn/user-profile-components';
+
+import type { CaseConnectors, CaseUI, CasesConfigurationUI } from '../../../../containers/types';
+import type { CurrentUserProfile } from '../../../types';
+import { useCasesContext } from '../../../cases_context/use_cases_context';
+
+interface UseBuilderContextArgs {
+  caseData: CaseUI;
+  casesConfiguration: CasesConfigurationUI;
+  caseConnectors: CaseConnectors;
+  userProfiles: Map<string, UserProfileWithAvatar>;
+  currentUserProfile: CurrentUserProfile;
+  manageMarkdownEditIds: string[];
+  selectedOutlineCommentId: string;
+  loadingCommentIds: string[];
+  handleOutlineComment: (id: string) => void;
+  handleDeleteComment: (id: string, successToasterTitle: string) => void;
+}
+
+export const useBuilderContext = ({
+  caseData,
+  casesConfiguration,
+  caseConnectors,
+  userProfiles,
+  currentUserProfile,
+  manageMarkdownEditIds,
+  selectedOutlineCommentId,
+  loadingCommentIds,
+  handleOutlineComment,
+  handleDeleteComment,
+}: UseBuilderContextArgs) => {
+  const { euiTheme } = useEuiTheme();
+  const {
+    externalReferenceAttachmentTypeRegistry,
+    persistableStateAttachmentTypeRegistry,
+    unifiedAttachmentTypeRegistry,
+    owner,
+  } = useCasesContext();
+
+  return useMemo(
+    () => ({
+      appId: owner[0],
+      caseData,
+      casesConfiguration,
+      caseConnectors,
+      userProfiles,
+      currentUserProfile,
+      externalReferenceAttachmentTypeRegistry,
+      persistableStateAttachmentTypeRegistry,
+      unifiedAttachmentTypeRegistry,
+      manageMarkdownEditIds,
+      selectedOutlineCommentId,
+      loadingCommentIds,
+      euiTheme,
+      handleOutlineComment,
+      handleDeleteComment,
+    }),
+    [
+      owner,
+      caseData,
+      casesConfiguration,
+      caseConnectors,
+      userProfiles,
+      currentUserProfile,
+      externalReferenceAttachmentTypeRegistry,
+      persistableStateAttachmentTypeRegistry,
+      unifiedAttachmentTypeRegistry,
+      manageMarkdownEditIds,
+      selectedOutlineCommentId,
+      loadingCommentIds,
+      euiTheme,
+      handleOutlineComment,
+      handleDeleteComment,
+    ]
+  );
+};
---
+++ x-pack/platform/plugins/shared/cases/public/components/cases_redesign/user_actions/hooks/use_user_actions_pagination.ts
@@ -0,0 +1,77 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { useMemo } from 'react';
+
+import { useInfiniteFindCaseUserActions } from '../../../../containers/use_infinite_find_case_user_actions';
+import type { AttachmentUIV2, UserActionUI } from '../../../../containers/types';
+import type { UserActivityParams } from '../../../user_actions_activity_bar/types';
+
+interface UserActionsPagination {
+  userActivityQueryParams: UserActivityParams;
+  caseId: string;
+  lastPage: number;
+}
+
+export const useUserActionsPagination = ({
+  userActivityQueryParams,
+  caseId,
+  lastPage,
+}: UserActionsPagination) => {
+  const {
+    data: caseInfiniteUserActionsData,
+    isLoading: isLoadingInfiniteUserActions,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteFindCaseUserActions(caseId, userActivityQueryParams, true);
+
+  const infiniteCaseUserActions = useMemo<{
+    userActions: UserActionUI[];
+    latestAttachments: AttachmentUIV2[];
+  }>(() => {
+    if (!caseInfiniteUserActionsData?.pages?.length || isLoadingInfiniteUserActions) {
+      return { userActions: [], latestAttachments: [] };
+    }
+
+    const userActionsData: UserActionUI[] = [];
+    const latestAttachments: AttachmentUIV2[] = [];
+
+    caseInfiniteUserActionsData.pages.forEach((page) => userActionsData.push(...page.userActions));
+    caseInfiniteUserActionsData.pages.forEach((page) =>
+      latestAttachments.push(...page.latestAttachments)
+    );
+
+    return { userActions: userActionsData, latestAttachments };
+  }, [caseInfiniteUserActionsData, isLoadingInfiniteUserActions]);
+
+  const remainingActionCount = useMemo(() => {
+    if (!caseInfiniteUserActionsData?.pages?.length) {
+      return 0;
+    }
+
+    const lastPageData =
+      caseInfiniteUserActionsData.pages[caseInfiniteUserActionsData.pages.length - 1];
+    const loadedCount = caseInfiniteUserActionsData.pages.reduce(
+      (sum, page) => sum + page.userActions.length,
+      0
+    );
+
+    return Math.max(lastPageData.total - loadedCount, 0);
+  }, [caseInfiniteUserActionsData]);
+
+  return {
+    lastPage,
+    isLoadingInfiniteUserActions,
+    infiniteCaseUserActions: infiniteCaseUserActions.userActions,
+    infiniteLatestAttachments: infiniteCaseUserActions.latestAttachments,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+    remainingActionCount,
+  };
+};
