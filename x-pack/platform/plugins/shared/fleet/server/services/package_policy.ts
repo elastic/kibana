1f78ca49adcce555f77b00ead548ba078cfefc70
Jen Huang <its.jenetic@gmail.com>
2026-06-22 16:05:52 -0700

    Preserve v1 inputs_for_versions from :prev copy on rollback

--- x-pack/platform/plugins/shared/fleet/server/services/package_policy.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/package_policy.ts
@@ -3201,7 +3201,7 @@ class PackagePolicyClientImpl implements PackagePolicyClient {
             ...policy,
             attributes: {
               ...previousRevision?.attributes,
-              inputs_for_versions: null,
+              inputs_for_versions: previousRevision?.attributes.inputs_for_versions ?? {},
               revision: (policy?.attributes.revision ?? 0) + 1, // Bump revision
               latest_revision: true,
             },
