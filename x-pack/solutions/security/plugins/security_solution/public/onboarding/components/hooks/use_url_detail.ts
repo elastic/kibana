f54d77651299bd7ff3281659a1eec94293e726b0
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-06-22 10:24:49 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/security/plugins/security_solution/public/onboarding/components/hooks/use_url_detail.ts
+++ x-pack/solutions/security/plugins/security_solution/public/onboarding/components/hooks/use_url_detail.ts
@@ -81,11 +81,11 @@ export const useSyncUrlDetails = ({ pathTopicId, hashCardId }: UseSyncUrlDetails
 
   const onComplete = useCallback((cloudTopicId: OnboardingTopicId | null) => {
     if (cloudTopicId && config.has(cloudTopicId)) {
-  if (cloudTopicId === OnboardingTopicId.siemMigrations) {
-     navigateToDetail(cloudTopicId);
-     } else {
-     setTopic(cloudTopicId);
-   }
+      if (cloudTopicId === OnboardingTopicId.siemMigrations) {
+        navigateToDetail(cloudTopicId);
+      } else {
+        setTopic(cloudTopicId);
+      }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
