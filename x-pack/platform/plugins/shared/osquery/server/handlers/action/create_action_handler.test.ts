865956d840caedd86dccf303b75faec34614998a
Tomasz Ciecierski <tomasz.ciecierski@elastic.co>
2026-06-22 14:27:35 +0200

    [Osquery] fix import

--- x-pack/platform/plugins/shared/osquery/server/handlers/action/create_action_handler.test.ts
+++ x-pack/platform/plugins/shared/osquery/server/handlers/action/create_action_handler.test.ts
@@ -4,8 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-
-import { DEFAULT_SPACE_ID } from '@kbn/spaces-utils';
+import { DEFAULT_SPACE_ID } from '@kbn/spaces-plugin/common';
 
 import { createActionHandler } from './create_action_handler';
 import { createDynamicQueries } from './create_queries';
