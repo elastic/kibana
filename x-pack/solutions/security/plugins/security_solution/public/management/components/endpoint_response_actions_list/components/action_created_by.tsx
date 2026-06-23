014d0460861a47b3de0547c7dac2ad3ef6143855
Paul Tavares <paul.tavares@elastic.co>
2026-06-23 14:54:15 -0400

    Add agent selection to the cancel modal

---
+++ x-pack/solutions/security/plugins/security_solution/public/management/components/endpoint_response_actions_list/components/action_created_by.tsx
@@ -0,0 +1,88 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React, { memo } from 'react';
+import { EuiAvatar, EuiFacetButton, EuiText, EuiToolTip } from '@elastic/eui';
+import { SecurityPageName } from '@kbn/deeplinks-security';
+import styled from '@emotion/styled';
+import { useTestIdGenerator } from '../../../hooks/use_test_id_generator';
+import { UX_MESSAGES } from '../translations';
+import { SecuritySolutionLinkAnchor } from '../../../../common/components/links';
+import { getRuleDetailsUrl } from '../../../../common/components/link_to';
+import type { ActionDetails } from '../../../../../common/endpoint/types';
+
+// Truncated usernames
+const StyledFacetButtonBase = styled(EuiFacetButton)`
+  padding-left: 0;
+  pointer-events: none;
+
+  .euiText {
+    margin-top: 0.38rem;
+    overflow-y: visible !important;
+  }
+`;
+const StyledFacetButton = (props: React.ComponentProps<typeof EuiFacetButton>) => (
+  <StyledFacetButtonBase {...props} title={undefined} />
+);
+
+export interface ActionCreatedByProps {
+  action: ActionDetails;
+  'data-test-subj'?: string;
+}
+
+export const ActionCreatedBy = memo<ActionCreatedByProps>(
+  ({ action: { createdBy, ruleId }, 'data-test-subj': dataTestSubj }) => {
+    const getTestId = useTestIdGenerator(dataTestSubj);
+
+    if (createdBy === 'unknown' && ruleId) {
+      return (
+        <EuiToolTip content={UX_MESSAGES.triggeredByRule} anchorClassName="eui-textTruncate">
+          <SecuritySolutionLinkAnchor
+            data-test-subj="ruleName"
+            deepLinkId={SecurityPageName.rules}
+            path={getRuleDetailsUrl(ruleId)}
+          >
+            <EuiText
+              size="s"
+              className="eui-textTruncate eui-fullWidth"
+              data-test-subj={getTestId('userName')}
+            >
+              {UX_MESSAGES.triggeredByRule}
+            </EuiText>
+          </SecuritySolutionLinkAnchor>
+        </EuiToolTip>
+      );
+    }
+    return (
+      <StyledFacetButton
+        icon={
+          <EuiAvatar
+            aria-hidden={true}
+            // We've a EuiTooltip that shows for createdBy below,
+            // Thus we don't need to add a title tooltip as well.
+            title=""
+            name={createdBy}
+            data-test-subj={getTestId('userAvatar')}
+            size="s"
+          />
+        }
+      >
+        <EuiToolTip content={createdBy} anchorClassName="eui-textTruncate">
+          <EuiText
+            size="s"
+            className="eui-textTruncate eui-fullWidth"
+            data-test-subj={getTestId('userName')}
+            tabIndex={0}
+          >
+            {createdBy}
+          </EuiText>
+        </EuiToolTip>
+      </StyledFacetButton>
+    );
+  }
+);
+ActionCreatedBy.displayName = 'ActionCreatedBy';
--- x-pack/solutions/security/plugins/security_solution/public/management/components/endpoint_response_actions_list/components/cancel_action_modal.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/management/components/endpoint_response_actions_list/components/cancel_action_modal.tsx
@@ -8,6 +8,7 @@
 import React, { memo, useCallback, useMemo, useState } from 'react';
 import type { EuiSelectableOption, EuiSwitchEvent } from '@elastic/eui';
 import {
+  EuiToolTip,
   EuiFormRow,
   EuiSelectable,
   EuiSpacer,
@@ -20,19 +21,40 @@ import {
   EuiModalHeaderTitle,
   EuiSwitch,
   EuiTextArea,
+  EuiText,
+  EuiFlexGroup,
+  EuiFlexItem,
 } from '@elastic/eui';
 import { useIsMounted } from '@kbn/securitysolution-hook-utils';
 import { i18n } from '@kbn/i18n';
+import { ActionCreatedBy } from './action_created_by';
+import { FormattedDate } from '../../../../common/components/formatted_date';
 import { FormattedError } from '../../formatted_error';
 import { useToasts } from '../../../../common/lib/kibana';
 import { useSendCancelRequest } from '../../../hooks/response_actions/use_send_cancel_request';
 import { CONSOLE_COMMANDS } from '../../../common/translations';
 import type { CancelActionRequestBody } from '../../../../../common/api/endpoint';
 import { canUserCancelCommand } from '../../../../../common/endpoint/service/authz/cancel_authz_utils';
-import { UX_MESSAGES } from '../translations';
+import { OUTPUT_MESSAGES, TABLE_COLUMN_NAMES, UX_MESSAGES } from '../translations';
 import { useTestIdGenerator } from '../../../hooks/use_test_id_generator';
 import type { ActionDetails } from '../../../../../common/endpoint/types';
 import { useUserPrivileges } from '../../../../common/components/user_privileges';
+
+interface ResponseActionPendingInfo {
+  hasMultiplePendingAgents: boolean;
+  pendingAgentIds: string[];
+}
+
+const getResponseActionPendingInfo = (action: ActionDetails): ResponseActionPendingInfo => {
+  const pendingAgentIds = action.agents.filter(
+    (agentId) => !action.agentState[agentId].isCompleted
+  );
+
+  return {
+    hasMultiplePendingAgents: pendingAgentIds.length > 1,
+    pendingAgentIds,
+  };
+};
 
 export interface CancelActionModalProps {
   action: ActionDetails;
@@ -42,7 +64,9 @@ export interface CancelActionModalProps {
 
 export const CancelActionModal = memo<CancelActionModalProps>(
   ({ action, onClose, 'data-test-subj': dataTestSubj }) => {
-    const isMultiAgentAction = action.agents.length > 1;
+    const isMultiAgentAction = useMemo(() => {
+      return getResponseActionPendingInfo(action).hasMultiplePendingAgents;
+    }, [action]);
 
     const getTestId = useTestIdGenerator(dataTestSubj);
     const authz = useUserPrivileges().endpointPrivileges;
@@ -53,8 +77,7 @@ export const CancelActionModal = memo<CancelActionModalProps>(
     const [cancelApiBody, setCancelApiBody] = useState<
       CancelActionRequestBody & { parameters: { force?: boolean } }
     >({
-      // FIXME:PT need proper initializer
-      endpoint_ids: !isMultiAgentAction ? [action.agents[0]] : [],
+      endpoint_ids: !isMultiAgentAction ? getResponseActionPendingInfo(action).pendingAgentIds : [],
       agent_type: action.agentType,
       parameters: {
         id: action.id,
@@ -89,10 +112,6 @@ export const CancelActionModal = memo<CancelActionModalProps>(
         return <EuiCallOut announceOnMount color="warning" title={msg} />;
       }
     }, [action.command, action.isCompleted, authz]);
-
-    const setCommentHandler = useCallback<React.ChangeEventHandler<HTMLTextAreaElement>>((ev) => {
-      setCancelApiBody((prevState) => ({ ...prevState, comment: ev.target.value }));
-    }, []);
 
     const agentSelector = useMemo(() => {
       if (!isMultiAgentAction) {
@@ -151,6 +170,10 @@ export const CancelActionModal = memo<CancelActionModalProps>(
       isMultiAgentAction,
     ]);
 
+    const setCommentHandler = useCallback<React.ChangeEventHandler<HTMLTextAreaElement>>((ev) => {
+      setCancelApiBody((prevState) => ({ ...prevState, comment: ev.target.value }));
+    }, []);
+
     const setForceFlag = useCallback((ev: EuiSwitchEvent) => {
       setCancelApiBody((prevState) => ({
         ...prevState,
@@ -189,7 +212,87 @@ export const CancelActionModal = memo<CancelActionModalProps>(
         <EuiModalBody>
           {notPermittedMessage || (
             <>
-              {/* FIXME:PT Show info. about action/host */}
+              <EuiFormRow fullWidth>
+                <EuiFlexGroup>
+                  <EuiFlexItem>
+                    <EuiFlexGroup direction="column" justifyContent="spaceBetween" gutterSize="xs">
+                      <EuiFlexItem>
+                        <EuiText size="xs">
+                          <strong>{TABLE_COLUMN_NAMES.command}</strong>
+                        </EuiText>
+                      </EuiFlexItem>
+                      <EuiFlexItem>
+                        <EuiText size="s">
+                          <EuiToolTip content={action.command} anchorClassName="eui-textTruncate">
+                            <EuiText
+                              size="s"
+                              className="eui-textTruncate eui-fullWidth"
+                              data-test-subj={getTestId('actionCommand')}
+                              tabIndex={0}
+                            >
+                              {action.command}
+                            </EuiText>
+                          </EuiToolTip>
+                        </EuiText>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiFlexGroup direction="column" justifyContent="spaceBetween" gutterSize="xs">
+                      <EuiFlexItem>
+                        <EuiText size="xs">
+                          <strong>{TABLE_COLUMN_NAMES.time}</strong>
+                        </EuiText>
+                      </EuiFlexItem>
+                      <EuiFlexItem>
+                        <EuiText size="s">
+                          <FormattedDate
+                            fieldName={TABLE_COLUMN_NAMES.time}
+                            value={action.startedAt}
+                            className="eui-textTruncate"
+                          />
+                        </EuiText>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiFlexItem>
+                  <EuiFlexItem>
+                    <EuiFlexGroup
+                      direction="column"
+                      justifyContent="spaceBetween"
+                      gutterSize="none"
+                    >
+                      <EuiFlexItem>
+                        <EuiText size="xs">
+                          <strong>{TABLE_COLUMN_NAMES.user}</strong>
+                        </EuiText>
+                      </EuiFlexItem>
+                      <EuiFlexItem>
+                        <EuiText size="s">
+                          <ActionCreatedBy action={action} data-test-subj={getTestId('user')} />
+                        </EuiText>
+                      </EuiFlexItem>
+                    </EuiFlexGroup>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              </EuiFormRow>
+
+              {!isMultiAgentAction && (
+                <EuiFlexGroup gutterSize="xs" direction="column">
+                  <EuiFlexItem>
+                    <EuiText size="xs">
+                      <strong>{OUTPUT_MESSAGES.expandSection.hostname}</strong>
+                    </EuiText>
+                  </EuiFlexItem>
+                  <EuiFlexItem className="eui-textTruncate">
+                    <EuiText size="s" className="eui-textTruncate">
+                      {action.hosts[cancelApiBody.endpoint_ids[0]]?.name ||
+                        cancelApiBody.endpoint_ids[0]}
+                    </EuiText>
+                  </EuiFlexItem>
+                </EuiFlexGroup>
+              )}
+
+              <EuiSpacer />
 
               {agentSelector}
 
@@ -209,6 +312,7 @@ export const CancelActionModal = memo<CancelActionModalProps>(
                 <>
                   <EuiFormRow fullWidth>
                     <EuiSwitch
+                      compressed
                       label={CONSOLE_COMMANDS.cancel.forceArgInfo}
                       checked={Boolean(cancelApiBody.parameters.force)}
                       onChange={setForceFlag}
