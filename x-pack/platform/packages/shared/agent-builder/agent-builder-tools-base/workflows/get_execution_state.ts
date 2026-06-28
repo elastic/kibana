c7d593d10da30664bc8cc509d8af4cb685a3a98f
Hashem <hashem.naara@elastic.co>
2026-06-28 18:04:18 +0300

    dedup

--- x-pack/platform/packages/shared/agent-builder/agent-builder-tools-base/workflows/get_execution_state.ts
+++ x-pack/platform/packages/shared/agent-builder/agent-builder-tools-base/workflows/get_execution_state.ts
@@ -5,13 +5,6 @@
  * 2.0.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the "Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
 import type { JsonValue } from '@kbn/utility-types';
 import type { WorkflowsServerPluginSetup } from '@kbn/workflows-management-plugin/server';
 import type { WaitForApprovalStep, WaitForInputStep, WorkflowExecutionDto } from '@kbn/workflows';
