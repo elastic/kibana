eab8d25a50d47960f433b07dc5fdf72e8c44130d
Ania Kowalska <63072419+akowalska622@users.noreply.github.com>
2026-07-01 11:53:03 +0200

    rename directory to tabs

--- src/platform/plugins/shared/discover/test/scout/ui/parallel_tests/core/tabs/new_tab.spec.ts
+++ src/platform/plugins/shared/discover/test/scout/ui/parallel_tests/core/tabs/new_tab.spec.ts
@@ -16,8 +16,6 @@ import { expect } from '@kbn/scout/ui';
 import { spaceTest } from '../../../fixtures/common';
 
 spaceTest.describe('Discover tabs - opening a new tab', { tag: '@local-stateful-classic' }, () => {
-  spaceTest.use({ viewport: { width: 1920, height: 1080 } });
-
   spaceTest.beforeAll(async ({ discoverScoutSpace }) => {
     await discoverScoutSpace.setupDiscoverDefaults();
   });
