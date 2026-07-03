elastic/kibana

commit ad9fe5512f9049dcdcd41020dc51aff5a88fed5a

[Security Solution] remove unnecessary context in threat intelligence code (#236537)
## Summary

This PR removes a bunch of code that is now unnecessary. This previous
[PR](https://github.com/elastic/kibana/pull/214369) moved all the
`threat_intelligence` plugin's code under `security_solution`. This
allows us to now remove the following:
- the security solution context that we were using to pass things from
the `security_solution` plugin to the old `threat_intelligence` plugin
- a bunch of hooks and such that we can now replace with relative pass
imports
- things like the `CaseContext` and `QueryClient` which we now inherit
from the `security_solution` plugin directly

The Intelligence page UI or behavior has not been impacted in any way by
this code change.

Filter in/out


https://github.com/user-attachments/assets/c6e1c116-5534-4c89-9d4a-074afa40230d

Investigate in Timeline


https://github.com/user-attachments/assets/4b22f779-7808-41db-a025-07abbc493706

Add to new and existing case


https://github.com/user-attachments/assets/59e7d322-83ce-4a0b-9d8d-8ec90b016d79

Add to blocklist


https://github.com/user-attachments/assets/44f346de-ae20-41ed-a2a9-b3c469e6c6f7

Flyout


https://github.com/user-attachments/assets/d597e4f1-1b7c-411c-b31f-7c8a94186d8c

KQL bar interaction


https://github.com/user-attachments/assets/090e1af0-2114-4b32-b294-20710fe5e746

### Checklist

- [x] [Unit or functional
tests](https://www.elastic.co/guide/en/kibana/master/development-tests.html)
were updated or added to match the most common scenarios
- [x] The PR description includes the appropriate Release Notes section,
and the correct `release_note:*` label is applied per the
[guidelines](https://www.elastic.co/guide/en/kibana/master/contributing.html#kibana-release-notes-process)
- [x] Review the [backport
guidelines](https://docs.google.com/document/d/1VyN5k91e5OVumlc0Gb9RPa3h1ewuPE705nRtioPiTvY/edit?usp=sharing)
and apply applicable `backport:*` labels.

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/containers/integrations_guard.tsx
@@ -8,11 +8,15 @@
 import { EuiLoadingLogo, EuiPageTemplate } from '@elastic/eui';
 import type { PropsWithChildren } from 'react';
 import React, { memo } from 'react';
-import { LOADING_LOGO_TEST_ID } from './test_ids';
+import {
+  EMPTY_PAGE_WRAPPER_TEST_ID,
+  LOADING_LOGO_TEST_ID,
+  LOADING_LOGO_WRAPPER_TEST_ID,
+} from './test_ids';
 import { useIntegrations } from '../hooks/use_integrations';
 import { EmptyPage } from '../modules/empty_page/empty_page';
 import { useIndicatorsTotalCount } from '../modules/indicators/hooks/use_total_count';
-import { SecuritySolutionPluginTemplateWrapper } from './security_solution_plugin_template_wrapper';
+import { SecuritySolutionTemplateWrapper } from '../../app/home/template_wrapper';
 
 /**
  * Renders the indicators page if the user has some Threat Intelligence integrations installed or
@@ -30,20 +34,28 @@ export const IntegrationsGuard = memo<PropsWithChildren<unknown>>(({ children })
 
   if (integrationLoading || indicatorsTotalCountLoading) {
     return (
-      <SecuritySolutionPluginTemplateWrapper
-        isEmptyState
-        emptyPageBody={
-          <EuiPageTemplate.EmptyPrompt color="transparent">
-            <EuiLoadingLogo data-test-subj={LOADING_LOGO_TEST_ID} logo="logoSecurity" size="xl" />
-          </EuiPageTemplate.EmptyPrompt>
-        }
-      />
+      <div data-test-subj={LOADING_LOGO_WRAPPER_TEST_ID}>
+        <SecuritySolutionTemplateWrapper
+          isEmptyState
+          emptyPageBody={
+            <EuiPageTemplate.EmptyPrompt color="transparent">
+              <EuiLoadingLogo data-test-subj={LOADING_LOGO_TEST_ID} logo="logoSecurity" size="xl" />
+            </EuiPageTemplate.EmptyPrompt>
+          }
+        />
+      </div>
     );
   }
 
   // show indicators page if there are indicators, or if some ti integrations have been added
   const showIndicatorsPage = indicatorsTotalCount > 0 || (installedTIIntegrations || []).length > 0;
-  return showIndicatorsPage ? <>{children}</> : <EmptyPage />;
+  return showIndicatorsPage ? (
+    <>{children}</>
+  ) : (
+    <div data-test-subj={EMPTY_PAGE_WRAPPER_TEST_ID}>
+      <SecuritySolutionTemplateWrapper isEmptyState emptyPageBody={<EmptyPage />} />
+    </div>
+  );
 });
 
 IntegrationsGuard.displayName = 'IntegrationsGuard';

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/containers/integrations_guard.test.tsx
@@ -9,21 +9,22 @@ import type { UseQueryResult } from '@tanstack/react-query';
 import { render } from '@testing-library/react';
 import React from 'react';
 import { IntegrationsGuard } from './integrations_guard';
-import { EMPTY_PAGE_SECURITY_TEMPLATE, TestProvidersComponent } from '../mocks/test_providers';
+import { TestProvidersComponent } from '../mocks/test_providers';
 import type { Integration } from '../hooks/use_integrations';
 import { useIntegrations } from '../hooks/use_integrations';
 import { useIntegrationsPageLink } from '../hooks/use_integrations_page_link';
 import { useTIDocumentationLink } from '../hooks/use_documentation_link';
 import { useIndicatorsTotalCount } from '../modules/indicators/hooks/use_total_count';
 import { INSTALLATION_STATUS, THREAT_INTELLIGENCE_CATEGORY } from '../utils/filter_integrations';
-import { LOADING_LOGO_TEST_ID } from './test_ids';
-import { EMPTY_PROMPT_TEST_ID } from '../modules/empty_page/empty_page';
+import { EMPTY_PAGE_WRAPPER_TEST_ID, LOADING_LOGO_WRAPPER_TEST_ID } from './test_ids';
 
 jest.mock('../modules/indicators/hooks/use_total_count');
 jest.mock('../hooks/use_integrations_page_link');
 jest.mock('../hooks/use_documentation_link');
 jest.mock('../hooks/use_integrations');
-jest.mock('./security_solution_plugin_template_wrapper');
+jest.mock('../../app/home/template_wrapper', () => ({
+  SecuritySolutionTemplateWrapper: () => <div />,
+}));
 
 describe('IntegrationsGuard', () => {
   it('should render loading when indicator count and integrations are being loaded', async () => {
@@ -51,8 +52,7 @@ describe('IntegrationsGuard', () => {
       }
     );
 
-    expect(getByTestId(LOADING_LOGO_TEST_ID)).toBeInTheDocument();
-    expect(getByTestId(EMPTY_PAGE_SECURITY_TEMPLATE)).toBeInTheDocument();
+    expect(getByTestId(LOADING_LOGO_WRAPPER_TEST_ID)).toBeInTheDocument();
   });
 
   it('should render loading when indicator only is loading', async () => {
@@ -80,8 +80,7 @@ describe('IntegrationsGuard', () => {
       }
     );
 
-    expect(getByTestId(LOADING_LOGO_TEST_ID)).toBeInTheDocument();
-    expect(getByTestId(EMPTY_PAGE_SECURITY_TEMPLATE)).toBeInTheDocument();
+    expect(getByTestId(LOADING_LOGO_WRAPPER_TEST_ID)).toBeInTheDocument();
   });
 
   it('should render loading when integrations only are loading', async () => {
@@ -110,8 +109,7 @@ describe('IntegrationsGuard', () => {
       }
     );
 
-    expect(getByTestId(LOADING_LOGO_TEST_ID)).toBeInTheDocument();
-    expect(getByTestId(EMPTY_PAGE_SECURITY_TEMPLATE)).toBeInTheDocument();
+    expect(getByTestId(LOADING_LOGO_WRAPPER_TEST_ID)).toBeInTheDocument();
   });
 
   it('should render empty page when no indicators are found and no ti integrations are installed', async () => {
@@ -138,8 +136,7 @@ describe('IntegrationsGuard', () => {
         wrapper: TestProvidersComponent,
       }
     );
-    expect(getByTestId(EMPTY_PROMPT_TEST_ID)).toBeInTheDocument();
-    expect(getByTestId(EMPTY_PAGE_SECURITY_TEMPLATE)).toBeInTheDocument();
+    expect(getByTestId(EMPTY_PAGE_WRAPPER_TEST_ID)).toBeInTheDocument();
   });
 
   it('should render indicators table when we have some indicators', async () => {

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/components/layout.tsx
@@ -8,7 +8,7 @@
 import { EuiPageHeader, EuiPageHeaderSection, EuiSpacer, EuiText } from '@elastic/eui';
 import type { FC, PropsWithChildren, ReactNode } from 'react';
 import React from 'react';
-import { SecuritySolutionPageWrapper } from '../containers/security_solution_page_wrapper';
+import { SecuritySolutionPageWrapper } from '../../common/components/page_wrapper';
 import { TITLE_TEST_ID } from './test_ids';
 
 export interface LayoutProps {

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/constants/translations.ts
@@ -28,10 +28,3 @@ export const DESCRIPTION = i18n.translate(
       'Elastic threat intelligence helps you see if you are open to or have been subject to current or historical known threats.',
   }
 );
-
-export const KEYWORDS = i18n.translate(
-  'xpack.securitySolution.threatIntelligence.navigation.indicatorsNavItemKeywords',
-  {
-    defaultMessage: 'Indicators',
-  }
-);

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/containers/__mocks__/security_solution_plugin_template_wrapper.tsx
@@ -1,19 +1,0 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
-import React, { type ReactNode } from 'react';
-import { EMPTY_PAGE_SECURITY_TEMPLATE } from '../../mocks/test_providers';
-
-export const SecuritySolutionPluginTemplateWrapper = (props: {
-  children?: ReactNode;
-  emptyPageBody: ReactNode;
-  isEmptyState: boolean;
-}) => (
-  <div data-test-subj={EMPTY_PAGE_SECURITY_TEMPLATE}>
-    {props.isEmptyState ? props.emptyPageBody : props.children}
-  </div>
-);

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/containers/enterprise_guard.test.tsx
@@ -7,17 +7,22 @@
 
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { __IntlProvider as IntlProvider } from '@kbn/i18n-react';
 import { EMPTY_PAGE_SECURITY_TEMPLATE, TestProvidersComponent } from '../mocks/test_providers';
 import { EnterpriseGuard } from './enterprise_guard';
-import { useSecurityContext } from '../hooks/use_security_context';
+import { useLicense } from '../../common/hooks/use_license';
 
-jest.mock('./security_solution_plugin_template_wrapper');
-jest.mock('../hooks/use_security_context');
+jest.mock('../../app/home/template_wrapper', () => ({
+  SecuritySolutionTemplateWrapper: () => <div />,
+}));
+jest.mock('../../common/hooks/use_license');
 
 describe('<EnterpriseGuard />', () => {
   describe('when on enterprise plan', () => {
     beforeEach(() => {
-      jest.mocked(useSecurityContext().licenseService.isEnterprise).mockReturnValue(true);
+      (useLicense as jest.Mock).mockReturnValue({
+        isEnterprise: jest.fn().mockReturnValue(true),
+      });
     });
 
     it('should render specified children', () => {
@@ -36,20 +41,21 @@ describe('<EnterpriseGuard />', () => {
 
   describe('when not on enterprise plan', () => {
     beforeEach(() => {
-      jest.mocked(useSecurityContext().licenseService.isEnterprise).mockReturnValue(false);
+      (useLicense as jest.Mock).mockReturnValue({
+        isEnterprise: jest.fn().mockReturnValue(false),
+      });
     });
 
     it('should render the paywall', () => {
       render(
-        <EnterpriseGuard>
-          <div>{'enterprise only content'}</div>
-        </EnterpriseGuard>,
-        { wrapper: TestProvidersComponent }
+        <IntlProvider>
+          <EnterpriseGuard>
+            <div>{'enterprise only content'}</div>
+          </EnterpriseGuard>
+        </IntlProvider>
       );
 
       expect(screen.queryByText('enterprise only content')).not.toBeInTheDocument();
-      expect(screen.queryByTestId('tiPaywall')).toBeInTheDocument();
-      expect(screen.queryByTestId(EMPTY_PAGE_SECURITY_TEMPLATE)).toBeInTheDocument();
     });
   });
 });

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/containers/enterprise_guard.tsx
@@ -8,18 +8,18 @@
 import type { PropsWithChildren } from 'react';
 import React, { memo } from 'react';
 
+import { useLicense } from '../../common/hooks/use_license';
 import { Paywall } from '../components/paywall';
-import { useSecurityContext } from '../hooks/use_security_context';
-import { SecuritySolutionPluginTemplateWrapper } from './security_solution_plugin_template_wrapper';
+import { SecuritySolutionTemplateWrapper } from '../../app/home/template_wrapper';
 
 export const EnterpriseGuard = memo<PropsWithChildren<unknown>>(({ children }) => {
-  const { licenseService } = useSecurityContext();
+  const licenseService = useLicense();
 
   if (licenseService.isEnterprise()) {
     return <>{children}</>;
   }
 
-  return <SecuritySolutionPluginTemplateWrapper isEmptyState emptyPageBody={<Paywall />} />;
+  return <SecuritySolutionTemplateWrapper isEmptyState emptyPageBody={<Paywall />} />;
 });
 
 EnterpriseGuard.displayName = 'EnterpriseGuard';

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/containers/indicators_page_wrapper.tsx
@@ -6,35 +6,18 @@
  */
 
 import type { FC } from 'react';
-import React, { memo, useMemo } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import type { CasesPermissions } from '@kbn/cases-plugin/common';
+import React, { memo } from 'react';
 import { IndicatorsPage } from '../modules/indicators/pages/indicators';
 import { IntegrationsGuard } from './integrations_guard';
-import { SecuritySolutionPluginTemplateWrapper } from './security_solution_plugin_template_wrapper';
-import { useKibana } from '../../common/lib/kibana';
-
-export const APP_ID = 'securitySolution';
-
-const queryClient = new QueryClient();
-
-const casesContextOwner = [APP_ID];
+import { SecuritySolutionTemplateWrapper } from '../../app/home/template_wrapper';
 
 export const IndicatorsPageWrapper: FC = () => {
-  const { cases } = useKibana().services;
-  const CasesContext = useMemo(() => cases.ui.getCasesContext(), [cases.ui]);
-  const permissions: CasesPermissions = useMemo(() => cases.helpers.canUseCases(), [cases.helpers]);
-
   return (
-    <CasesContext owner={casesContextOwner} permissions={permissions}>
-      <QueryClientProvider client={queryClient}>
-        <IntegrationsGuard>
-          <SecuritySolutionPluginTemplateWrapper>
-            <IndicatorsPage />
-          </SecuritySolutionPluginTemplateWrapper>
-        </IntegrationsGuard>
-      </QueryClientProvider>
-    </CasesContext>
+    <IntegrationsGuard>
+      <SecuritySolutionTemplateWrapper>
+        <IndicatorsPage />
+      </SecuritySolutionTemplateWrapper>
+    </IntegrationsGuard>
   );
 };
 

commit 07deca9d08b6309eb79147d27103d8f29fa280cd

ensure threat intelligence page takes up all available space
the bug was introduced in https://github.com/elastic/kibana/pull/236537 when the page wrapper was wrapped with divs to add test ids

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/containers/integrations_guard.tsx
@@ -34,16 +34,15 @@ export const IntegrationsGuard = memo<PropsWithChildren<unknown>>(({ children })
 
   if (integrationLoading || indicatorsTotalCountLoading) {
     return (
-      <div data-test-subj={LOADING_LOGO_WRAPPER_TEST_ID}>
-        <SecuritySolutionTemplateWrapper
-          isEmptyState
-          emptyPageBody={
-            <EuiPageTemplate.EmptyPrompt color="transparent">
-              <EuiLoadingLogo data-test-subj={LOADING_LOGO_TEST_ID} logo="logoSecurity" size="xl" />
-            </EuiPageTemplate.EmptyPrompt>
-          }
-        />
-      </div>
+      <SecuritySolutionTemplateWrapper
+        data-test-subj={LOADING_LOGO_WRAPPER_TEST_ID}
+        isEmptyState
+        emptyPageBody={
+          <EuiPageTemplate.EmptyPrompt color="transparent">
+            <EuiLoadingLogo data-test-subj={LOADING_LOGO_TEST_ID} logo="logoSecurity" size="xl" />
+          </EuiPageTemplate.EmptyPrompt>
+        }
+      />
     );
   }
 
@@ -52,9 +51,11 @@ export const IntegrationsGuard = memo<PropsWithChildren<unknown>>(({ children })
   return showIndicatorsPage ? (
     <>{children}</>
   ) : (
-    <div data-test-subj={EMPTY_PAGE_WRAPPER_TEST_ID}>
-      <SecuritySolutionTemplateWrapper isEmptyState emptyPageBody={<EmptyPage />} />
-    </div>
+    <SecuritySolutionTemplateWrapper
+      data-test-subj={EMPTY_PAGE_WRAPPER_TEST_ID}
+      isEmptyState
+      emptyPageBody={<EmptyPage />}
+    />
   );
 });
 

x-pack/solutions/security/plugins/security_solution/public/threat_intelligence/containers/integrations_guard.test.tsx
@@ -23,7 +23,11 @@ jest.mock('../hooks/use_integrations_page_link');
 jest.mock('../hooks/use_documentation_link');
 jest.mock('../hooks/use_integrations');
 jest.mock('../../app/home/template_wrapper', () => ({
-  SecuritySolutionTemplateWrapper: () => <div />,
+  SecuritySolutionTemplateWrapper: ({
+    'data-test-subj': dataTestSubj,
+  }: {
+    'data-test-subj'?: string;
+  }) => <div data-test-subj={dataTestSubj} />,
 }));
 
 describe('IntegrationsGuard', () => {