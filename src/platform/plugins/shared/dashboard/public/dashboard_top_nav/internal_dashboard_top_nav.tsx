a0843d300411f856bfac46f212faa4db2eef6de1
Stratou <efstratia.kalafateli@elastic.co>
2026-07-06 12:06:07 +0200

    Update src/platform/plugins/shared/dashboard/public/dashboard_top_nav/internal_dashboard_top_nav.tsx
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/platform/plugins/shared/dashboard/public/dashboard_top_nav/internal_dashboard_top_nav.tsx
+++ src/platform/plugins/shared/dashboard/public/dashboard_top_nav/internal_dashboard_top_nav.tsx
@@ -457,7 +457,6 @@ export function InternalDashboardTopNav({
               }
             ),
           }}
-          }
         />
       )}
       {viewMode !== 'print' && isLabsEnabled && isLabsShown ? (
