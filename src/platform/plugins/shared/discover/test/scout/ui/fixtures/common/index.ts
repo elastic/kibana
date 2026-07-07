e31957696194f68b1e5e69432e9652fa8baafd87
Ania Kowalska <ania.kowalska@elastic.co>
2026-07-07 14:48:15 +0200

    address TS issues

--- src/platform/plugins/shared/discover/test/scout/ui/fixtures/common/index.ts
+++ src/platform/plugins/shared/discover/test/scout/ui/fixtures/common/index.ts
@@ -34,10 +34,12 @@ export interface DiscoverTestFixtures extends ScoutParallelTestFixtures {
 
 export const spaceTest = spaceBaseTest.extend<DiscoverTestFixtures, DiscoverWorkerFixtures>({
   pageObjects: async ({ pageObjects, page }, use) => {
-    await use({
+    const discoverPageObjects: DiscoverTestFixtures['pageObjects'] = {
       ...pageObjects,
       inspector: createLazyPageObject(Inspector, page),
-    });
+    };
+
+    await use(discoverPageObjects);
   },
   discoverScoutSpace: [
     async ({ scoutSpace }, use) => {
