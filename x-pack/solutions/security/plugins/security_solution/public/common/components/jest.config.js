38926daed1b8c4ad3806d1ee4ff4b0500aa9fb07
Jonathan Budzenski <jon@elastic.co>
2026-07-07 12:35:06 -0500

    split more jest tests

--- x-pack/solutions/security/plugins/security_solution/public/common/components/jest.config.js
+++ x-pack/solutions/security/plugins/security_solution/public/common/components/jest.config.js
@@ -9,11 +9,17 @@ module.exports = {
   preset: '@kbn/test',
   rootDir: '../../../../../../../..',
   roots: ['<rootDir>/x-pack/solutions/security/plugins/security_solution/public/common/components'],
+  testPathIgnorePatterns: [
+    '<rootDir>/x-pack/solutions/security/plugins/security_solution/public/common/components/ml/',
+    '<rootDir>/x-pack/solutions/security/plugins/security_solution/public/common/components/visualization_actions/',
+  ],
   coverageDirectory:
     '<rootDir>/target/kibana-coverage/jest/x-pack/solutions/security/plugins/security_solution/public/common/components',
   coverageReporters: ['text', 'html'],
   collectCoverageFrom: [
     '<rootDir>/x-pack/solutions/security/plugins/security_solution/public/common/components/**/*.{ts,tsx}',
+    '!<rootDir>/x-pack/solutions/security/plugins/security_solution/public/common/components/ml/**/*.{ts,tsx}',
+    '!<rootDir>/x-pack/solutions/security/plugins/security_solution/public/common/components/visualization_actions/**/*.{ts,tsx}',
   ],
   moduleNameMapper: require('../../../server/__mocks__/module_name_map'),
 };
