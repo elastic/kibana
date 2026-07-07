ef88ecfe50b258b70a44bbd4e5f4b8a4beed9c1c
Caue Marcondes <caue.marcondes@elastic.co>
2026-07-07 12:12:57 -0400

    use memo instead

--- x-pack/solutions/observability/plugins/apm/public/embeddable/embeddable_context.tsx
+++ x-pack/solutions/observability/plugins/apm/public/embeddable/embeddable_context.tsx
@@ -131,7 +131,7 @@ export function ApmEmbeddableContext({
     OBSERVABILITY_APM_CPS_ENABLED_FEATURE_FLAG,
     OBSERVABILITY_APM_CPS_ENABLED_DEFAULT
   );
-  useEffect(() => {
+  useMemo(() => {
     const cpsManager = isCpsEnabled ? deps.pluginsStart.cps?.cpsManager : undefined;
     const callApmApi = createCallApmApiV2(deps.coreStart, { cpsManager });
     setApmInternalServices({ callApmApi, cpsManager });
