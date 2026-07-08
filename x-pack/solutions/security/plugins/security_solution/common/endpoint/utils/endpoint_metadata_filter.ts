f6cba534c20090e6d1443b3854689c1ff365c013
Paul Tavares <paul.tavares@elastic.co>
2026-07-08 09:55:10 -0400

    First attempt at a fix to the query

--- x-pack/solutions/security/plugins/security_solution/common/endpoint/utils/endpoint_metadata_filter.ts
+++ x-pack/solutions/security/plugins/security_solution/common/endpoint/utils/endpoint_metadata_filter.ts
@@ -27,20 +27,42 @@ export function buildBaseEndpointMetadataFilter(policyIds?: string[]): QueryDslQ
     { exists: { field: 'united.endpoint.agent.id' } },
     { exists: { field: 'united.agent.agent.id' } },
     // agent is enrolled
-    {
-      term: {
-        'united.agent.active': {
-          value: true,
-        },
-      },
-    },
+    { term: { 'united.agent.active': { value: true } } },
   ];
 
   // Only include policy filter if policyIds are explicitly provided
   if (policyIds) {
+    // FIXME:PT DO NOT COMMIT THIS. Cleanup needed
+    // https://www.elastic.co/docs/reference/query-languages/query-dsl/query-dsl-wildcard-query
+    // {
+    //   "query": {
+    //     "wildcard": {
+    //       "user.id": {
+    //         "value": "ki*y",
+    //         "boost": 1.0,
+    //         "rewrite": "constant_score_blended"
+    //       }
+    //     }
+    //   }
+    // }
+
     baseFilters.push({
-      terms: { 'united.agent.policy_id': uniq(policyIds) },
+      bool: {
+        should: [
+          { terms: { 'united.agent.policy_id': uniq(policyIds) } },
+          // Wildcard match needed due to change made in Fleet in support of Agent specific policies
+          // See https://github.com/elastic/ingest-dev/issues/8624
+          ...uniq(policyIds).map((policyId) => {
+            return { wildcard: { 'united.agent.policy_id': { value: `${policyId}#*` } } };
+          }),
+        ],
+        minimum_should_match: 1,
+      },
     });
+
+    // baseFilters.push({
+    //   terms: { 'united.agent.policy_id': uniq(policyIds) },
+    // });
   }
 
   const filterEndpointPolicyAgents = {
