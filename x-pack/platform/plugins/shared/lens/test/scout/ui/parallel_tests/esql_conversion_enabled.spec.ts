61e0ee3bfacdecaa21ee8387ddae418b42b39818
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2026-07-08 14:11:54 +0200

    [tests] wait for landing page to load

--- x-pack/platform/plugins/shared/lens/test/scout/ui/parallel_tests/esql_conversion_enabled.spec.ts
+++ x-pack/platform/plugins/shared/lens/test/scout/ui/parallel_tests/esql_conversion_enabled.spec.ts
@@ -37,6 +37,7 @@ spaceTest.describe('Lens Convert to ES|QL', { tag: '@local-stateful-classic' }, 
     const { dashboard } = pageObjects;
 
     await dashboard.goto();
+    await pageObjects.listingTable.waitUntilTableIsLoaded();
     await page.getByTestId(testData.ESQL_CONVERSION_DASHBOARD_TEST_ID).click();
     await dashboard.waitForPanelsToLoad(2);
     await dashboard.switchToEditMode();
--- x-pack/platform/plugins/shared/lens/test/scout/ui/tests/esql_conversion_disabled.spec.ts
+++ x-pack/platform/plugins/shared/lens/test/scout/ui/tests/esql_conversion_disabled.spec.ts
@@ -35,6 +35,7 @@ test.describe('Lens Convert to ES|QL button', { tag: '@local-stateful-classic' }
     const { dashboard, lens } = pageObjects;
 
     await dashboard.goto();
+    await pageObjects.listingTable.waitUntilTableIsLoaded();
     await page.getByTestId(testData.ESQL_CONVERSION_DASHBOARD_TEST_ID).click();
     await dashboard.waitForPanelsToLoad(2);
     await dashboard.switchToEditMode();
