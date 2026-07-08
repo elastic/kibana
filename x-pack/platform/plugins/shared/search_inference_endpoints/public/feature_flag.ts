19d1e49c9e3add5debd72ee3dba651957ccd2cd2
Saikat Sarkar <132922331+saikatsarkar056@users.noreply.github.com>
2026-07-08 09:11:01 -0600

    Update x-pack/platform/plugins/shared/search_inference_endpoints/public/feature_flag.ts
    Co-authored-by: Rodney Norris <rodney@tattdcodemonkey.com>

--- x-pack/platform/plugins/shared/search_inference_endpoints/public/feature_flag.ts
+++ x-pack/platform/plugins/shared/search_inference_endpoints/public/feature_flag.ts
@@ -8,5 +8,10 @@
 import { useUiSetting } from '@kbn/kibana-react-plugin/public';
 import { INFERENCE_PREFERENCES_FEATURE_FLAG_ID } from '../common/constants';
 
-export const useInferencePreferencesEnabled = (): boolean =>
-  useUiSetting<boolean>(INFERENCE_PREFERENCES_FEATURE_FLAG_ID, false);
+export const useInferencePreferencesEnabled = (): boolean => {
+  try {
+    return useUiSetting<boolean>(INFERENCE_PREFERENCES_FEATURE_FLAG_ID, false);
+  catch {
+    return false;
+  }
+}  
