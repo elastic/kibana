4bf40da1d6ee1d51239a12a186e4b4943ff03950
Stratoula <efstratia.kalafateli@elastic.co>
2026-07-08 07:33:00 +0200

    Further cleanup

--- src/platform/plugins/shared/dashboard/public/dashboard_app/no_data/dashboard_app_no_data.tsx
+++ src/platform/plugins/shared/dashboard/public/dashboard_app/no_data/dashboard_app_no_data.tsx
@@ -83,7 +83,6 @@ export const DashboardAppNoDataPage = ({
           search: dataService.search.search,
           signal: abc.signal,
           timeRange: dataService.query.timefilter.timefilter.getAbsoluteTime(),
-          includeColumnMetadata: true,
         });
 
         // lens suggestions api context
