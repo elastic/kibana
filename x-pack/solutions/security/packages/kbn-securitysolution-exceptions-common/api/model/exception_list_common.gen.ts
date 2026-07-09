76186efb146e22b3e93945c222d8b50b512afe72
Gergő Ábrahám <gergo.abraham@elastic.co>
2026-07-09 09:43:18 +0200

    [docs] remove YARA from list_id description

--- x-pack/solutions/security/packages/kbn-securitysolution-exceptions-common/api/model/exception_list_common.gen.ts
+++ x-pack/solutions/security/packages/kbn-securitysolution-exceptions-common/api/model/exception_list_common.gen.ts
@@ -37,7 +37,6 @@ For endpoint artifacts, use one of the following values:
 * `endpoint_event_filters`: [Event filters list](https://www.elastic.co/docs/solutions/security/manage-elastic-defend/event-filters)
 * `endpoint_host_isolation_exceptions`: [Host isolation exceptions list](https://www.elastic.co/docs/solutions/security/manage-elastic-defend/host-isolation-exceptions)
 * `endpoint_blocklists`: [Blocklists list](https://www.elastic.co/docs/solutions/security/manage-elastic-defend/blocklist)
-* `endpoint_custom_yara_signatures`: [Custom YARA signatures list](https://www.elastic.co/docs/solutions/security/manage-elastic-defend/custom-yara-signatures)
 
   */
 export const ExceptionListHumanId = lazySchema(() =>
