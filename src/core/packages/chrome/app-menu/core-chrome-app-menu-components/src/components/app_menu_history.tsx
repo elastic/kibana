79edb34494dfb7bfec54f5ab23c9a37362a0c7fa
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-07-09 17:26:33 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/core/packages/chrome/app-menu/core-chrome-app-menu-components/src/components/app_menu_history.tsx
+++ src/core/packages/chrome/app-menu/core-chrome-app-menu-components/src/components/app_menu_history.tsx
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { EuiButtonIcon, EuiToolTip, useEuiTheme } from '@elastic/eui';
+import { EuiButtonIcon, EuiToolTip } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
 import { isMac } from '@kbn/shared-ux-utility';
 import React from 'react';
