1d592be844ee9155c1ee46b5e2d21ec22f0bd940
heenawter <hannah.wright@elastic.co>
2026-07-09 14:13:17 -0600

    Revert "Changes from node scripts/check"
    This reverts commit 39e2ddd444665d64a9696c96c629d5e1c80afa0a.

--- src/platform/packages/shared/kbn-shared-ux-utility/src/fallback.d.ts
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-/**
- * A simple implementation of `React.Suspense.fallback` that renders a loading spinner.
- */
-export declare const Fallback: () => React.JSX.Element;
--- src/platform/packages/shared/shared-ux/error_boundary/src/services/index.d.ts
+++
@@ -1,1 +0,0 @@
-export { useErrorBoundary } from './error_boundary_provider';
