76947146c537b1af8bfae8ab6632f6062cccaf49
Kelvin Tan <kelvin.tan@elastic.co>
2026-07-13 13:23:13 -0400

    Revert "[Scout] Add missing testChannels to config_discovery test fixture"
    This reverts commit 41cec202428a6765475b3dc8439a7383e05e808f.

--- src/platform/packages/shared/kbn-scout/src/cli/config_discovery.test.ts
+++ src/platform/packages/shared/kbn-scout/src/cli/config_discovery.test.ts
@@ -393,7 +393,6 @@ describe('runDiscoverPlaywrightConfigs', () => {
                 path: customPath,
                 exists: true,
                 sha1: 'custom123',
-                testChannels: [],
                 tests: [
                   {
                     id: 'customTest1',
