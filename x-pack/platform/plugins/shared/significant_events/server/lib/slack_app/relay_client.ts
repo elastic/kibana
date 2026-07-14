9f65e939bd77bd42c5e504c7af3b83d778bbbef0
Nicolas Chaulet <nicolas.chaulet@elastic.co>
2026-07-14 15:07:10 -0400

    [SigEvents] Fix relay client ssl options type errors

--- x-pack/platform/plugins/shared/significant_events/server/lib/slack_app/relay_client.ts
+++ x-pack/platform/plugins/shared/significant_events/server/lib/slack_app/relay_client.ts
@@ -82,7 +82,7 @@ export class RelayClient {
         key,
         allowPartialTrustChain: true,
         rejectUnauthorized,
-        checkServerIdentity,
+        ...(checkServerIdentity ? { checkServerIdentity } : {}),
       },
     });
   }
