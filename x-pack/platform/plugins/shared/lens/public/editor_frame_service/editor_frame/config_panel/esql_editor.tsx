c7f79c70fd3a8ebf13817e8db08678642f01f74e
Stratoula <efstratia.kalafateli@elastic.co>
2026-07-14 09:35:55 +0200

    [ES|QL] Guard validdtion after abort

--- x-pack/platform/plugins/shared/lens/public/editor_frame_service/editor_frame/config_panel/esql_editor.tsx
+++ x-pack/platform/plugins/shared/lens/public/editor_frame_service/editor_frame/config_panel/esql_editor.tsx
@@ -151,6 +151,7 @@ export function ESQLEditor({
 
   const runQuery = useCallback(
     async (q: AggregateQuery, abortController?: AbortController, shouldUpdateAttrs?: boolean) => {
+      setErrors([]);
       const attrs = await getSuggestions(
         q,
         data,
