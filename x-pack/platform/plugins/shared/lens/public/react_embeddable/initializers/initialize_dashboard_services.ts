73e108e4b1e815e7602d9880a910e52157e27a6c
heenawter <hannah.wright@elastic.co>
2026-07-13 18:01:19 -0600

    Flatten Lens state

--- x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_dashboard_services.ts
+++ x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_dashboard_services.ts
@@ -29,6 +29,7 @@ import type {
 import type { LensApi, LensWireAPIConfig } from '@kbn/lens-common-2';
 
 import { stripInheritedContext } from '../../../common/transforms/helpers';
+import { flattenAPIConfig } from '../../../common/transforms/utils';
 import { isTextBasedLanguage, transformToApiConfig } from '../helper';
 import type { LensEmbeddableStartServices } from '../types';
 import { apiHasLensComponentProps } from '../type_guards';
@@ -125,7 +126,7 @@ export function initializeDashboardServices(
       },
       getSerializedStateByValue: () => {
         const { ref_id: refId, ...byValueRuntimeState } = stripInheritedContext(getLatestState());
-        return transformToApiConfig(byValueRuntimeState);
+        return flattenAPIConfig(transformToApiConfig(byValueRuntimeState));
       },
     },
     anyStateChange$: merge(
