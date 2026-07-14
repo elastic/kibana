bed1d13f72528df91270534db2fababa7dcc33ec
Matthew Kime <matt@mattki.me>
2026-07-13 21:03:15 -0500

    scout test cleanup

--- x-pack/platform/plugins/private/data_federation/test/scout_data_federation/ui/fixtures/page_objects/data_federation_page.ts
+++ x-pack/platform/plugins/private/data_federation/test/scout_data_federation/ui/fixtures/page_objects/data_federation_page.ts
@@ -11,21 +11,25 @@ export class DataFederationPage {
   readonly pageTitle;
   readonly tabs;
   readonly dataSourcesTable;
+  readonly dataSetsTable;
   readonly connectDataSourceButton;
   readonly createDataSetButton;
   readonly createDataSourceFlyout;
   readonly createDataSourceFlyoutCancel;
   readonly createDataSetFlyout;
+  readonly createDataSetFlyoutCancel;
 
   constructor(private readonly page: ScoutPage) {
     this.pageTitle = page.testSubj.locator('dataSetsPageTitle');
     this.tabs = page.testSubj.locator('dataSetsTabs');
     this.dataSourcesTable = page.testSubj.locator('dataSetsTable');
+    this.dataSetsTable = page.testSubj.locator('dataSetsSetsTable');
     this.connectDataSourceButton = page.testSubj.locator('dataSetsCreateButton');
     this.createDataSetButton = page.testSubj.locator('dataSetsSetsCreateButton');
     this.createDataSourceFlyout = page.testSubj.locator('createDataSourceFlyout');
     this.createDataSourceFlyoutCancel = page.testSubj.locator('createDataSourceFlyoutCancel');
     this.createDataSetFlyout = page.testSubj.locator('createDatasetFlyout');
+    this.createDataSetFlyoutCancel = page.testSubj.locator('createDatasetFlyoutCancel');
   }
 
   async goto(): Promise<void> {
