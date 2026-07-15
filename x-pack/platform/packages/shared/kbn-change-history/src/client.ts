57bd80603ea4ab504efbfab6bcac2e348e8c06a9
Maxim Palenov <maxim.palenov@elastic.co>
2026-07-15 10:51:37 +0200

    remove kbn-change-history package FLAGS

--- x-pack/platform/packages/shared/kbn-change-history/src/client.ts
+++ x-pack/platform/packages/shared/kbn-change-history/src/client.ts
@@ -19,13 +19,13 @@ import type { ClientCreateRequest } from '@kbn/data-streams/src/types/es_api';
 import type { Logger } from '@kbn/logging';
 import { changeHistoryMappings } from './mappings';
 import {
-  FLAGS,
   DATA_STREAM_NAME,
   SEPARATOR_CHAR,
   ECS_VERSION,
   DEFAULT_RESULT_SIZE,
   DEFAULT_FIELD_AGGREGATION_SIZE,
 } from './constants';
+import { DATA_STREAM_NAME, SEPARATOR_CHAR, ECS_VERSION, DEFAULT_RESULT_SIZE } from './constants';
 import type {
   ChangeHistoryAggregateField,
   ChangeHistoryDocument,
@@ -116,11 +116,6 @@ export class ChangeHistoryClient implements IChangeHistoryClient {
    * @throws An error if the data stream is not initialized properly.
    */
   async initialize(elasticsearchClient: ElasticsearchClient) {
-    if (!FLAGS.FEATURE_ENABLED) {
-      const error = new Error(`Change history is disabled. Skipping initialization.`);
-      this.logger.error(error);
-      throw error;
-    }
     const definition: DataStreamDefinition<typeof changeHistoryMappings.v1, ChangeHistoryDocument> =
       {
         name: DATA_STREAM_NAME,
