d3562e27ae2a17d7a8128fc0f7f0e9b1bb43f8fa
Edgar Santos <edgar.santos@elastic.co>
2026-07-17 17:48:50 +0200

    [Detection Engineering] Fix input schema in prebuilt rule upgrade endpoint (#278125)
    (cherry picked from commit f73f17344d8b15d509918534e03b35872f0f8500)
    
    # Conflicts:
    #	x-pack/solutions/security/plugins/security_solution/common/api/detection_engine/prebuilt_rules/perform_rule_upgrade/perform_rule_upgrade_route.ts

--- x-pack/solutions/security/plugins/security_solution/common/api/detection_engine/prebuilt_rules/perform_rule_upgrade/perform_rule_upgrade_route.ts
+++ x-pack/solutions/security/plugins/security_solution/common/api/detection_engine/prebuilt_rules/perform_rule_upgrade/perform_rule_upgrade_route.ts
@@ -9,7 +9,7 @@ import { z } from '@kbn/zod';
 import { mapValues } from 'lodash';
 import { RuleResponse } from '../../model/rule_schema/rule_schemas.gen';
 import { AggregatedPrebuiltRuleError, DiffableAllFields, ThreeWayDiffConflict } from '../model';
-import { RuleSignatureId, RuleVersion } from '../../model';
+import { RequiredFieldInput, RuleSignatureId, RuleVersion } from '../../model';
 import { PrebuiltRulesFilter } from '../common/prebuilt_rules_filter';
 
 export type Mode = z.infer<typeof Mode>;
@@ -66,9 +66,20 @@ export const DiffableFieldsToOmit = NON_UPGRADEABLE_DIFFABLE_FIELDS.reduce((acc,
  * Specific fields are omitted because they are not upgradeable, and
  * handled under the hood by endpoint logic.
  * See: https://github.com/elastic/kibana/issues/186544
+ *
+ * These field schemas are used exclusively to validate a field's
+ * `resolved_value` on input (i.e. `pick_version: 'RESOLVED'`), so they describe
+ * the input shape, not the response shape. `required_fields` is overridden to
+ * `RequiredFieldInput` (name + type) because its `ecs` boolean is computed by
+ * the server (`addEcsToRequiredFields`) and only present in responses. Reusing
+ * the response schema (`RequiredField`, where `ecs` is mandatory) made resolving
+ * a `required_fields` conflict impossible.
+ * See: https://github.com/elastic/kibana/issues/232614
  */
 export type DiffableUpgradableFields = z.infer<typeof DiffableUpgradableFields>;
-export const DiffableUpgradableFields = DiffableAllFields.omit(DiffableFieldsToOmit);
+export const DiffableUpgradableFields = DiffableAllFields.omit(DiffableFieldsToOmit).extend({
+  required_fields: z.array(RequiredFieldInput),
+});
 
 export type FieldUpgradeSpecifier<T> = z.infer<
   ReturnType<typeof fieldUpgradeSpecifier<z.ZodType<T>>>
