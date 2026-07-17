fc6660dfeb145d69e49f1a9981dea476064fdaf0
Kelvin Tan <kelvin.tan@elastic.co>
2026-07-17 15:29:34 -0400

    fox prevalence host link

--- x-pack/solutions/security/plugins/security_solution/public/flyout_v2/document/main/components/insights_section.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/flyout_v2/document/main/components/insights_section.tsx
@@ -127,8 +127,8 @@ export const InsightsSection = memo(
     );
 
     const renderFlyoutLink = useCallback(
-      (props: OpenFlyoutLinkProps) => <OpenFlyoutLink {...props} />,
-      []
+      (props: OpenFlyoutLinkProps) => <OpenFlyoutLink {...props} hit={hit} />,
+      [hit]
     );
 
     const onShowPrevalenceDetails = useCallback(() => {
