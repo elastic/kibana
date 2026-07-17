716413efa326a4be8bef10321f8d7aae268d523c
Anton Dosov <anton.dosov@elastic.co>
2026-07-17 16:32:18 +0200

    Preserve Visualize rendering in reports

--- src/platform/plugins/shared/visualizations/public/plugin.ts
+++ src/platform/plugins/shared/visualizations/public/plugin.ts
@@ -204,6 +204,8 @@ const styles = {
     kbn-top-nav,
     filter-bar,
     .kbnTopNavMenu__wrapper,
+    .kbnChromeLayoutHeader,
+    .kbnChromeLayoutNavigation,
     ::-webkit-scrollbar,
     .euiNavDrawer {
       display: none !important;
@@ -447,7 +449,6 @@ export class VisualizationsPlugin
 
         params.element.classList.add(styles.visAppWrapper);
         if (pluginsStart.screenshotMode.isScreenshotMode()) {
-          coreStart.chrome.setIsVisible(false);
           params.element.classList.add(styles.visEditorScreenshotMode);
           injectGlobal(styles.globalScreenshotMode);
         }
