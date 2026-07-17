e898bf392695ca320288293a470ae237dd5840a0
kubasobon <kuba.sobon@elastic.co>
2026-07-17 11:05:29 +0200

    copy: adjust copy according to figma

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/pages/entity_store_disabled_empty_prompt.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/pages/entity_store_disabled_empty_prompt.tsx
@@ -41,14 +41,20 @@ export const EntityStoreDisabledEmptyPromptBody = React.memo(() => (
     data-test-subj="entityStoreDisabledEmptyPrompt"
     icon={<EuiImage size={128} alt="" url={illustrationSearchAnalytics} />}
     layout="horizontal"
-    style={{ maxWidth: 624 }}
+    style={{ maxWidth: 800 }}
     title={
       <h2>
         <FormattedMessage
           id="xpack.securitySolution.entityAnalytics.homePage.enableEntityAnalytics"
-          defaultMessage="Enable Entity analytics to collect entity data and access analytics capabilities"
+          defaultMessage="Enable Entity analytics"
         />
       </h2>
+    }
+    body={
+      <FormattedMessage
+        id="xpack.securitySolution.entityAnalytics.homePage.enableEntityAnalytics.body"
+        defaultMessage="Start collecting entity data across your environment, so you can spot your riskiest entities and anomalous behavior at a glance, and catch threats before they escalate."
+      />
     }
     actions={
       <SecuritySolutionLinkButton
