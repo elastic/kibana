e03cf3e8c1a5fbb5e666b71b95f52c4d86322d93
Matthew Kime <matt@mattki.me>
2026-07-17 12:11:28 -0500

    localize product names

--- x-pack/platform/plugins/private/data_federation/public/get_data_source_type_label.ts
+++ x-pack/platform/plugins/private/data_federation/public/get_data_source_type_label.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import { i18n } from '@kbn/i18n';
 import type { DataSourceType } from '../common/datasource_types';
 
 /**
@@ -14,9 +15,14 @@ export function getDataSourceTypeVerbose(type: DataSourceType): string {
   return messages[type];
 }
 
-// todo localize
 const messages: Record<DataSourceType, string> = {
-  s3: 'Amazon S3',
-  gcs: 'Google Cloud Storage',
-  azure: 'Azure',
+  s3: i18n.translate('xpack.dataFederation.dataSourceType.s3Label', {
+    defaultMessage: 'Amazon S3',
+  }),
+  gcs: i18n.translate('xpack.dataFederation.dataSourceType.gcsLabel', {
+    defaultMessage: 'Google Cloud Storage',
+  }),
+  azure: i18n.translate('xpack.dataFederation.dataSourceType.azureLabel', {
+    defaultMessage: 'Azure',
+  }),
 };
