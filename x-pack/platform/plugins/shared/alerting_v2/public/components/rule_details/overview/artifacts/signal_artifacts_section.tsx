6a1da920ebdaa403bb27f547a52ef84e53d10b0a
Bailey Cash <bailey.cash@elastic.co>
2026-07-21 12:00:13 -0400

    add artifacts section to signal rule details

--- x-pack/platform/plugins/shared/alerting_v2/public/components/rule_details/overview/artifacts/signal_artifacts_section.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/components/rule_details/overview/artifacts/signal_artifacts_section.tsx
@@ -6,17 +6,16 @@
  */
 
 import React from 'react';
-import { EuiAccordion, EuiFlexGroup, EuiFlexItem, EuiText, useGeneratedHtmlId } from '@elastic/eui';
+import { EuiAccordion, EuiText, useGeneratedHtmlId } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { useService } from '@kbn/core-di-browser';
-import { UserCapabilities } from '../../../../services/user_capabilities';
-import { ActionPoliciesArtifactsSubsection } from './action_policies_artifacts_subsection';
 import { DashboardArtifactsSubsection } from './dashboard_artifacts_subsection';
 
-export const ArtifactsSection: React.FC = () => {
+/**
+ * Artifacts section for `signal` rules. Signal rules have no notification
+ * policies, so only the dashboards subsection is shown, at full width.
+ */
+export const SignalArtifactsSection: React.FC = () => {
   const artifactsAccordionId = useGeneratedHtmlId({ prefix: 'ruleArtifactsSection' });
-
-  const canReadActionPolicies = useService(UserCapabilities).canRead('actionPolicies');
 
   return (
     <EuiAccordion
@@ -33,16 +32,7 @@ export const ArtifactsSection: React.FC = () => {
       }
       initialIsOpen
     >
-      <EuiFlexGroup gutterSize="l" responsive={true} data-test-subj="ruleArtifactsSubsectionsRow">
-        <EuiFlexItem grow={true} style={{ minWidth: 0 }}>
-          <DashboardArtifactsSubsection />
-        </EuiFlexItem>
-        {canReadActionPolicies ? (
-          <EuiFlexItem grow={true} style={{ minWidth: 0 }}>
-            <ActionPoliciesArtifactsSubsection />
-          </EuiFlexItem>
-        ) : null}
-      </EuiFlexGroup>
+      <DashboardArtifactsSubsection />
     </EuiAccordion>
   );
 };
