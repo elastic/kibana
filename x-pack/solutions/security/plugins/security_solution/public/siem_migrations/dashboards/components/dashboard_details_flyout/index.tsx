7c658293f4121c0a7700ca2267c7b2ee10376155
Jatin Kathuria <jtn.kathuria@gmail.com>
2026-07-21 13:22:42 +0200

    feat(securitySolution): add navigation arrows to dashboard migration details flyout

--- x-pack/solutions/security/plugins/security_solution/public/siem_migrations/dashboards/components/dashboard_details_flyout/index.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/siem_migrations/dashboards/components/dashboard_details_flyout/index.tsx
@@ -8,6 +8,7 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import {
   EuiButtonEmpty,
+  EuiButtonIcon,
   EuiFlexGroup,
   EuiFlexItem,
   EuiFlyout,
@@ -27,7 +28,10 @@ import type { DashboardMigrationDashboard } from '../../../../../common/siem_mig
 import {
   CLOSE_BUTTON_LABEL,
   SUMMARY_TAB_LABEL,
+  PREVIOUS_BUTTON_LABEL,
+  NEXT_BUTTON_LABEL,
 } from '../../../common/components/details_flyout/translation';
+import type { MigrationFlyoutNavigation } from '../../../common/components/details_flyout';
 import {
   ScrollableFlyoutTabbedContent,
   TabContentPadding,
@@ -41,6 +45,7 @@ export interface DashboardMigrationDashboardDetailsFlyoutProps {
   size?: EuiFlyoutProps['size'];
   isLoading?: boolean;
   dashboardActions?: React.ReactNode;
+  navigation?: MigrationFlyoutNavigation;
 }
 
 export const DashboardMigrationDetailsFlyout = React.memo(function DashboardMigrationDetailsFlyout({
@@ -49,6 +54,7 @@ export const DashboardMigrationDetailsFlyout = React.memo(function DashboardMigr
   size,
   isLoading = false,
   dashboardActions,
+  navigation,
 }: DashboardMigrationDashboardDetailsFlyoutProps) {
   const summaryTab: EuiTabbedContentTab = useMemo(
     () => ({
@@ -101,6 +107,37 @@ export const DashboardMigrationDetailsFlyout = React.memo(function DashboardMigr
       data-test-subj="dashboardDetailsFlyout"
     >
       <EuiFlyoutHeader>
+        {navigation && (
+          <>
+            <EuiFlexGroup gutterSize="xs" responsive={false} alignItems="center">
+              <EuiFlexItem grow={false}>
+                <EuiButtonIcon
+                  iconType="arrowLeft"
+                  color="text"
+                  display="base"
+                  size="s"
+                  isDisabled={!navigation.hasPrevious || isLoading}
+                  onClick={navigation.goToPrevious}
+                  data-test-subj="migrationDashboardFlyoutPreviousButton"
+                  aria-label={PREVIOUS_BUTTON_LABEL}
+                />
+              </EuiFlexItem>
+              <EuiFlexItem grow={false}>
+                <EuiButtonIcon
+                  iconType="arrowRight"
+                  color="text"
+                  display="base"
+                  size="s"
+                  isDisabled={!navigation.hasNext || isLoading}
+                  onClick={navigation.goToNext}
+                  data-test-subj="migrationDashboardFlyoutNextButton"
+                  aria-label={NEXT_BUTTON_LABEL}
+                />
+              </EuiFlexItem>
+            </EuiFlexGroup>
+            <EuiSpacer size="s" />
+          </>
+        )}
         <EuiTitle size="m" data-test-subj="detailsFlyoutTitle">
           <h2 id={migrationsDashboardsFlyoutTitleId}>
             {migrationDashboard.elastic_dashboard?.title ||
