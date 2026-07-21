9e7aa2c8125f6eb5bd64492edbdb681656d7603e
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-07-21 10:47:45 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/security/plugins/security_solution/public/common/utils/flyout_pagination/use_paginated_flyout.test.ts
+++ x-pack/solutions/security/plugins/security_solution/public/common/utils/flyout_pagination/use_paginated_flyout.test.ts
@@ -33,8 +33,8 @@ jest.mock('../../lib/kibana', () => ({
   useKibana: jest.fn(),
 }));
 
-jest.mock('react-redux', () => ({
-  ...jest.requireActual('react-redux'),
+jest.mock('react-redux-v7', () => ({
+  ...jest.requireActual('react-redux-v7'),
   useStore: jest.fn().mockReturnValue({}),
 }));
 
