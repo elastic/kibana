efd16fe806b18c540984f2c725bc79bd5f9745eb
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-07-22 08:48:29 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/platform/plugins/shared/fleet/server/routes/agent_policy/handlers.test.ts
+++ x-pack/platform/plugins/shared/fleet/server/routes/agent_policy/handlers.test.ts
@@ -8,7 +8,6 @@
 import { httpServerMock } from '@kbn/core/server/mocks';
 
 import { agentPolicyService } from '../../services';
-import { listFleetProxies } from '../../services/fleet_proxies';
 
 import type { FleetRequestHandlerContext } from '../..';
 import { xpackMocks } from '../../mocks';
@@ -17,7 +16,6 @@ import type { AgentPolicy } from '../../types';
 
 import {
   bulkGetAgentPoliciesHandler,
-  downloadFullAgentPolicy,
   getFullAgentPolicy,
   GetListAgentPolicyOutputsHandler,
   populateAssignedAgentsCount,
