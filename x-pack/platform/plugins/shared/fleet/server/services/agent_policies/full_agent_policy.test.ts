caf0d157a9799274d1b0ea9ed0b765d9ff91e19a
Cristina Amico <mariacristina.amico@elastic.co>
2026-07-22 11:20:35 +0200

    Fix linter

--- x-pack/platform/plugins/shared/fleet/server/services/agent_policies/full_agent_policy.test.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/agent_policies/full_agent_policy.test.ts
@@ -1603,50 +1603,48 @@ describe('generateFleetConfig', () => {
       }
     `);
   });
-});
 
-it('should work with proxy with headers and certificate authorities and certificate and key', () => {
-  const res = generateFleetConfig(
-    {
-      host_urls: ['https://test.fr'],
-      proxy_id: 'proxy-1',
-    } as any,
-    [
+  it('should work with proxy with headers and certificate authorities and certificate and key', () => {
+    const res = generateFleetConfig(
       {
-        id: 'proxy-1',
-        url: 'https://proxy.fr',
-        certificate_authorities: ['/tmp/ssl/ca.crt'],
-        proxy_headers: { Authorization: 'xxx' },
-        certificate: 'my-cert',
-        certificate_key: 'my-key',
+        host_urls: ['https://test.fr'],
+        proxy_id: 'proxy-1',
       } as any,
-    ]
-  );
+      [
+        {
+          id: 'proxy-1',
+          url: 'https://proxy.fr',
+          certificate_authorities: ['/tmp/ssl/ca.crt'],
+          proxy_headers: { Authorization: 'xxx' },
+          certificate: 'my-cert',
+          certificate_key: 'my-key',
+        } as any,
+      ]
+    );
 
-  expect(res).toMatchInlineSnapshot(`
-    Object {
-      "hosts": Array [
-        "https://test.fr",
-      ],
-      "proxy_headers": Object {
-        "Authorization": "xxx",
-      },
-      "proxy_url": "https://proxy.fr",
-      "ssl": Object {
-        "certificate": "my-cert",
-        "certificate_authorities": Array [
-          Array [
-            "/tmp/ssl/ca.crt",
-          ],
+    expect(res).toMatchInlineSnapshot(`
+      Object {
+        "hosts": Array [
+          "https://test.fr",
         ],
-        "key": "my-key",
-        "renegotiation": "never",
-        "verification_mode": "",
-      },
-    }
-  `);
+        "proxy_headers": Object {
+          "Authorization": "xxx",
+        },
+        "proxy_url": "https://proxy.fr",
+        "ssl": Object {
+          "certificate": "my-cert",
+          "certificate_authorities": Array [
+            Array [
+              "/tmp/ssl/ca.crt",
+            ],
+          ],
+          "key": "my-key",
+          "renegotiation": "never",
+          "verification_mode": "",
+        },
+      }
+    `);
   });
-
 });
 
 describe('generateFleetServerOutputSSLConfig', () => {
