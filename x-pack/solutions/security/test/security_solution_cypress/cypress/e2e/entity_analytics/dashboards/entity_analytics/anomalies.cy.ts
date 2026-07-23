911f8258b09c55854d90dd01c1b381f9a43b943b
Kibana Machine <42973632+kibanamachine@users.noreply.github.com>
2026-07-23 09:47:46 -0700

    skip failing test suite (#279202)

--- x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/entity_analytics/dashboards/entity_analytics/anomalies.cy.ts
+++ x-pack/solutions/security/test/security_solution_cypress/cypress/e2e/entity_analytics/dashboards/entity_analytics/anomalies.cy.ts
@@ -33,7 +33,8 @@ describe.skip('Entity Analytics Dashboard', { tags: ['@ess', '@serverless'] }, (
     cy.task('esArchiverUnload', { archiveName: 'auditbeat_multiple' });
   });
 
-  describe('With anomalies data', () => {
+  // Failing: See https://github.com/elastic/kibana/issues/279202
+  describe.skip('With anomalies data', () => {
     before(() => {
       cy.task('esArchiverLoad', { archiveName: 'network' });
       login();
