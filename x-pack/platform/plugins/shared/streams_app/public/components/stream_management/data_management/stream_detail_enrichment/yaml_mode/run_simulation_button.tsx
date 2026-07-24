2c44b86be8e961176565863485b1f9c1e5eafad6
Alexey Antonov <alexwizp@gmail.com>
2026-07-24 12:39:48 +0300

    Apply suggestion from @github-actions[bot]
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- x-pack/platform/plugins/shared/streams_app/public/components/stream_management/data_management/stream_detail_enrichment/yaml_mode/run_simulation_button.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/stream_management/data_management/stream_detail_enrichment/yaml_mode/run_simulation_button.tsx
@@ -39,7 +39,7 @@ export const RunSimulationButton = () => {
           : undefined
       }
     >
-      <span style={{ display: 'inline-flex' }} tabIndex={0}>
+      <span style={{ display: 'inline-flex' }} tabIndex={canRunSimulation ? undefined : 0}>
         <EuiButtonIcon
           iconType="play"
           aria-label={i18n.translate(
