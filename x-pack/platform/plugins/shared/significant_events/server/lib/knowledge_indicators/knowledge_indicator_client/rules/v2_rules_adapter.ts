a33ae6a0b3f1e8d7d662b29ef286d76e2e519cda
klacabane <kevin.lacabane@elastic.co>
2026-07-24 14:21:10 +0000

    codeql comment

--- x-pack/platform/plugins/shared/significant_events/server/lib/knowledge_indicators/knowledge_indicator_client/rules/v2_rules_adapter.ts
+++ x-pack/platform/plugins/shared/significant_events/server/lib/knowledge_indicators/knowledge_indicator_client/rules/v2_rules_adapter.ts
@@ -83,7 +83,7 @@ export class RulesAdapterV2 implements IRulesManagementClient {
 
   async findStreamNamesWithOwnedRules(): Promise<string[]> {
     // Filters rules, not tags: matched rules return all their tags, so drop non-ownership ones below.
-    const escapedPrefix = STREAMS_RULE_STREAM_TAG_PREFIX.replace(/:/g, '\\:');
+    const escapedPrefix = STREAMS_RULE_STREAM_TAG_PREFIX.replace(/[\\:]/g, '\\$&');
     const tags = await this.rulesClient.getTags({ filter: `metadata.tags: ${escapedPrefix}*` });
     const streamNames = new Set<string>();
     for (const tag of tags) {
