3588e0cf4323e6259e2a225d4f6f305657fd68cb
“jeramysoucy” <jeramy.soucy@elastic.co>
2026-07-24 11:04:22 +0200

    Re-adds getCurrent mock

--- x-pack/platform/plugins/shared/alerting_v2/server/lib/services/user_service/user_service.mock.ts
+++ x-pack/platform/plugins/shared/alerting_v2/server/lib/services/user_service/user_service.mock.ts
@@ -5,12 +5,25 @@
  * 2.0.
  */
 
+import type { UserProfileWithSecurity } from '@kbn/core-user-profile-common';
 import type { UserProfileServiceStart } from '@kbn/core-user-profile-server';
 import { userProfileServiceMock } from '@kbn/core-user-profile-server-mocks';
 import { httpServerMock } from '@kbn/core-http-server-mocks';
 import { UserService } from './user_service';
 
 const DEFAULT_PROFILE_UID = 'elastic_profile_uid';
+
+const mockProfile: UserProfileWithSecurity = {
+  uid: DEFAULT_PROFILE_UID,
+  enabled: true,
+  user: {
+    username: 'elastic',
+    roles: [],
+    realm_name: 'realm',
+  },
+  data: {},
+  labels: {},
+};
 
 export function createUserService(): {
   userService: UserService;
@@ -20,6 +33,7 @@ export function createUserService(): {
   const userProfileService = userProfileServiceMock.createStart();
 
   userProfileService.getCurrentProfileId.mockResolvedValue(DEFAULT_PROFILE_UID);
+  userProfileService.getCurrent.mockResolvedValue(mockProfile);
 
   return {
     userService: new UserService(request, userProfileService),
