5a0d5357bb2282c4ce0b5a31535ce7f1a7dda03f
jillguyonnet <jill.guyonnet@elastic.co>
2026-07-27 10:55:09 +0200

    Skip failing test

--- x-pack/platform/test/fleet_api_integration/apis/package_policy/input_package_rollback.ts
+++ x-pack/platform/test/fleet_api_integration/apis/package_policy/input_package_rollback.ts
@@ -124,7 +124,8 @@ export default function (providerContext: FtrProviderContext) {
       expect(pkg?.status).to.eql('not_installed');
     });
 
-    it('should not add es references on package policy create failure when package is already installed', async () => {
+    // Failing: See https://github.com/elastic/kibana/issues/270585
+    it.skip('should not add es references on package policy create failure when package is already installed', async () => {
       await installPackage(PACKAGE_NAME, START_VERSION);
       await createPackagePolicyWithDataset(agentPolicyId, 'test*', 400);
 
