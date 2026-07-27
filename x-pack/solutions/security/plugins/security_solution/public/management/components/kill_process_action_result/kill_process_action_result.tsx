dd0b00c014d5ca17913f89949d79f0c3718c2006
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-07-27 14:00:34 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/security/plugins/security_solution/public/management/components/kill_process_action_result/kill_process_action_result.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/management/components/kill_process_action_result/kill_process_action_result.tsx
@@ -77,7 +77,11 @@ export const KillSuspendProcessActionResult = memo<KillSuspendProcessActionResul
 
       if (hostOutput?.entity_id) {
         if (processResultData.length > 0) {
-          processResultData.push(<EuiTextColor key={`${agentId}-entityId-sep`} color="subdued">{' | '}</EuiTextColor>);
+          processResultData.push(
+            <EuiTextColor key={`${agentId}-entityId-sep`} color="subdued">
+              {' | '}
+            </EuiTextColor>
+          );
         }
 
         processResultData.push(
