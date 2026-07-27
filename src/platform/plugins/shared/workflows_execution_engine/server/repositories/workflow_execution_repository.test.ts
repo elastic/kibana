bb2c902a2a3b22161b80f1721d9fdf8e4a76a7d6
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-07-27 10:35:01 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/platform/plugins/shared/workflows_execution_engine/server/repositories/workflow_execution_repository.test.ts
+++ src/platform/plugins/shared/workflows_execution_engine/server/repositories/workflow_execution_repository.test.ts
@@ -7,7 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import { loggerMock } from '@kbn/logging-mocks';
 import {
   ConcurrencySlotOccupyingExecutionStatuses,
   ExecutionStatus,
