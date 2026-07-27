521339816dead800d8ac66de3e65485926e558e0
Jesus Wahrman <jesus.wahrman@elastic.co>
2026-07-27 11:28:52 +0200

    user activity adapter

---
+++ src/core/packages/di/server/src/services/user_activity.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import type { ServiceIdentifier } from 'inversify';
+import type { UserActivityServiceSetup } from '@kbn/core-user-activity-server';
+
+/**
+ * The user activity tracking API.
+ * @see {@link UserActivityServiceSetup}
+ * @public
+ */
+export type IUserActivity = UserActivityServiceSetup;
+
+/**
+ * The user activity tracking service.
+ * @see {@link IUserActivity}
+ * @public
+ */
+export const UserActivity = Symbol('UserActivity') as ServiceIdentifier<IUserActivity>;
