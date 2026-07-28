975bd712f7f563dc8b99505e27a89288cd4b3545
Drew Tate <drew.tate@elastic.co>
2026-07-28 12:41:53 -0600

    tie request cancellation to dashboard panel unmount

--- src/platform/plugins/shared/dashboard/public/dashboard_api/layout_manager/layout_manager.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_api/layout_manager/layout_manager.ts
@@ -39,7 +39,6 @@ import { DEFAULT_PINNED_CONTROL_STATE } from '@kbn/controls-constants';
 import { i18n } from '@kbn/i18n';
 import type { SerializedTitles, PanelPackage } from '@kbn/presentation-publishing';
 import {
-  apiCanCancelRequests,
   childrenUnsavedChanges$,
   apiHasLibraryTransforms,
   apiHasSerializableState,
@@ -692,11 +691,6 @@ export function initializeLayoutManager(
       },
     },
     cleanup: () => {
-      Object.values(children$.value).forEach((childApi) => {
-        if (apiCanCancelRequests(childApi)) {
-          childApi.cancelRequests();
-        }
-      });
       childrenChangesSubscription.unsubscribe();
       gridLayoutSubscription.unsubscribe();
     },
