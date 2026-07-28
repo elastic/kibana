d65364f53e5226122d7cac7761b7c8991124469a
Larry Gregory <larry.gregory@elastic.co>
2026-07-28 15:36:46 -0400

    Apply suggestion from @legrego

--- packages/kbn-eslint-plugin-disable/src/helpers/protected_rules.ts
+++ packages/kbn-eslint-plugin-disable/src/helpers/protected_rules.ts
@@ -16,7 +16,7 @@ export const PROTECTED_RULES = new Set([
   '@kbn/imports/no_group_crossing_manifests',
   '@kbn/eslint/no_unsafe_hash',
   'no-new-func',
-  'new-implied-eval',
+  'no-implied-eval',
   'no-eval',
   'no-script-url',
 ]);
