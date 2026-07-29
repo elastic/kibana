e70486beb29bcb908dbf534920c6ea88d8a48701
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-07-29 12:22:08 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/vectordb/plugins/serverless_vectordb/public/home/home_page_banner.tsx
+++ x-pack/solutions/vectordb/plugins/serverless_vectordb/public/home/home_page_banner.tsx
@@ -13,7 +13,6 @@ import { useKibana } from '../hooks/use_kibana';
 import { useLocalStorage } from '../hooks/use_local_storage';
 import { BANNER_DISMISSED_KEY, HOME_PAGE_BANNER_COPY } from '../constants';
 import { GETTING_STARTED_DEEP_LINK_ID, VECTORDB_APP_ID } from '../../common/constants';
-import { bannerCallOutStyle, bannerButtonFlexItemStyle } from './home_page_banner_styles';
 
 interface HomePageBannerProps {
   hasData: boolean;
