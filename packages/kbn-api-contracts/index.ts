263c24f325d698e401727f6ded3e4c2eaef1f6f0
Tamerlan <37669316+TamerlanG@users.noreply.github.com>
2026-07-29 11:43:11 +0200

    Discard changes to packages/kbn-api-contracts/index.ts

--- packages/kbn-api-contracts/index.ts
+++ packages/kbn-api-contracts/index.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export { loadOas } from './src/input/load_oas'; // test comment
+export { loadOas } from './src/input/load_oas';
 export { runOasdiff, parseOasdiff, applyAllowlist } from './src/diff';
 export type { OasdiffEntry, BreakingChange, FilterResult } from './src/diff';
 export { formatFailure } from './src/report/format_failure';
