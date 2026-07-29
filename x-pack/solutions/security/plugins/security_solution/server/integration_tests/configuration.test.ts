390941c624d5761a89ec564db5a0100ccce20024
Sebastián Zaffarano <sebastian.zaffarano@elastic.co>
2026-07-29 16:27:12 +0200

    fix integration tests

--- x-pack/solutions/security/plugins/security_solution/server/integration_tests/configuration.test.ts
+++ x-pack/solutions/security/plugins/security_solution/server/integration_tests/configuration.test.ts
@@ -218,6 +218,35 @@ describe('configuration', () => {
             expectedClusterHealth: ['green', 'yellow'],
             validationIntervalMs: getRandomInt(1000, 5000),
           },
+          apiQueryAllowlist: [
+            {
+              path: '_transform/*/_stats',
+            },
+            {
+              path: '_nodes/stats',
+            },
+            {
+              path: '_nodes/*/stats',
+            },
+            {
+              path: '_cluster/stats',
+            },
+            {
+              path: '_ml/anomaly_detectors/_stats',
+            },
+            {
+              path: '_ml/anomaly_detectors/*/_stats',
+            },
+            {
+              path: '_ml/trained_models/_stats',
+            },
+            {
+              path: '_ilm/status',
+            },
+            {
+              path: '_watcher/stats',
+            },
+          ],
         },
       };
 
