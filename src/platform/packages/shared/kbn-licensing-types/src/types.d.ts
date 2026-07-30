035c003dd08c6e7d9c9c15135c2b4e4712d61458
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-07-30 17:53:19 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/platform/packages/shared/kbn-licensing-types/src/types.d.ts
+++ src/platform/packages/shared/kbn-licensing-types/src/types.d.ts
@@ -1,11 +1,20 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
 export type LicenseCheckState = 'unavailable' | 'invalid' | 'valid' | 'expired';
 export declare enum LICENSE_TYPE {
-    basic = 10,
-    standard = 20,
-    gold = 30,
-    platinum = 40,
-    enterprise = 50,
-    trial = 60
+  basic = 10,
+  standard = 20,
+  gold = 30,
+  platinum = 40,
+  enterprise = 50,
+  trial = 60,
 }
 /** @public */
 export type LicenseType = keyof typeof LICENSE_TYPE;
@@ -13,8 +22,8 @@ export type LicenseType = keyof typeof LICENSE_TYPE;
 export type LicenseStatus = 'active' | 'invalid' | 'expired';
 /** @public */
 export interface LicenseFeature {
-    isAvailable: boolean;
-    isEnabled: boolean;
+  isAvailable: boolean;
+  isEnabled: boolean;
 }
 /**
  * Subset of license data considered as non-sensitive information.
@@ -22,28 +31,28 @@ export interface LicenseFeature {
  * @public
  * */
 export interface PublicLicense {
-    /**
-     * UID for license.
-     */
-    uid: string;
-    /**
-     * The validity status of the license.
-     */
-    status: LicenseStatus;
-    /**
-     * Unix epoch of the expiration date of the license.
-     */
-    expiryDateInMillis: number;
-    /**
-     * The license type, being usually one of basic, standard, gold, platinum, or trial.
-     */
-    type: LicenseType;
-    /**
-     * The license type, being usually one of basic, standard, gold, platinum, or trial.
-     * @deprecated use 'type' instead
-     * @removeBy 8.8.0
-     */
-    mode: LicenseType;
+  /**
+   * UID for license.
+   */
+  uid: string;
+  /**
+   * The validity status of the license.
+   */
+  status: LicenseStatus;
+  /**
+   * Unix epoch of the expiration date of the license.
+   */
+  expiryDateInMillis: number;
+  /**
+   * The license type, being usually one of basic, standard, gold, platinum, or trial.
+   */
+  type: LicenseType;
+  /**
+   * The license type, being usually one of basic, standard, gold, platinum, or trial.
+   * @deprecated use 'type' instead
+   * @removeBy 8.8.0
+   */
+  mode: LicenseType;
 }
 /**
  * Provides information about feature availability for the current license.
@@ -56,9 +65,9 @@ export type PublicFeatures = Record<string, LicenseFeature>;
  * @public
  * */
 export interface PublicLicenseJSON {
-    license?: PublicLicense;
-    features?: PublicFeatures;
-    signature: string;
+  license?: PublicLicense;
+  features?: PublicFeatures;
+  signature: string;
 }
 /**
  * @public
@@ -66,77 +75,77 @@ export interface PublicLicenseJSON {
  * requirements of the license type.
  */
 export interface LicenseCheck {
-    /**
-     * The state of checking the results of a license type meeting the license minimum.
-     */
-    state: LicenseCheckState;
-    /**
-     * A message containing the reason for a license type not being valid.
-     */
-    message?: string;
+  /**
+   * The state of checking the results of a license type meeting the license minimum.
+   */
+  state: LicenseCheckState;
+  /**
+   * A message containing the reason for a license type not being valid.
+   */
+  message?: string;
 }
 /** @public */
 export interface ILicense {
-    /**
-     * UID for license.
-     */
-    uid?: string;
-    /**
-     * The validity status of the license.
-     */
-    status?: LicenseStatus;
-    /**
-     * Determine if the status of the license is active.
-     */
-    isActive: boolean;
-    /**
-     * Unix epoch of the expiration date of the license.
-     */
-    expiryDateInMillis?: number;
-    /**
-     * The license type, being usually one of basic, standard, gold, platinum, or trial.
-     */
-    type?: LicenseType;
-    /**
-     * The license type, being usually one of basic, standard, gold, platinum, or trial.
-     * @deprecated use 'type' instead.
-     * @removeBy 8.8.0
-     */
-    mode?: LicenseType;
-    /**
-     * Signature of the license content.
-     */
-    signature: string;
-    /**
-     * Determine if the license container has information.
-     */
-    isAvailable: boolean;
-    /**
-     * Returns
-     */
-    toJSON: () => PublicLicenseJSON;
-    /**
-     * A potential error denoting the failure of the license from being retrieved.
-     */
-    error?: string;
-    /**
-     * If the license is not available, provides a string or Error containing the reason.
-     */
-    getUnavailableReason: () => string | undefined;
-    /**
-     * Determine if license type >= minimal required license type.
-     * @param minimumLicenseRequired the minimum valid license required for the given feature
-     */
-    hasAtLeast(minimumLicenseRequired: LicenseType): boolean;
-    /**
-     * For a given plugin and license type, receive information about the status of the license.
-     * @param pluginName the name of the plugin
-     * @param minimumLicenseRequired the minimum valid license for operating the given plugin
-     */
-    check(pluginName: string, minimumLicenseRequired: LicenseType): LicenseCheck;
-    /**
-     * A specific API for interacting with the specific features of the license.
-     * @param name the name of the feature to interact with
-     */
-    getFeature(name: string): LicenseFeature;
+  /**
+   * UID for license.
+   */
+  uid?: string;
+  /**
+   * The validity status of the license.
+   */
+  status?: LicenseStatus;
+  /**
+   * Determine if the status of the license is active.
+   */
+  isActive: boolean;
+  /**
+   * Unix epoch of the expiration date of the license.
+   */
+  expiryDateInMillis?: number;
+  /**
+   * The license type, being usually one of basic, standard, gold, platinum, or trial.
+   */
+  type?: LicenseType;
+  /**
+   * The license type, being usually one of basic, standard, gold, platinum, or trial.
+   * @deprecated use 'type' instead.
+   * @removeBy 8.8.0
+   */
+  mode?: LicenseType;
+  /**
+   * Signature of the license content.
+   */
+  signature: string;
+  /**
+   * Determine if the license container has information.
+   */
+  isAvailable: boolean;
+  /**
+   * Returns
+   */
+  toJSON: () => PublicLicenseJSON;
+  /**
+   * A potential error denoting the failure of the license from being retrieved.
+   */
+  error?: string;
+  /**
+   * If the license is not available, provides a string or Error containing the reason.
+   */
+  getUnavailableReason: () => string | undefined;
+  /**
+   * Determine if license type >= minimal required license type.
+   * @param minimumLicenseRequired the minimum valid license required for the given feature
+   */
+  hasAtLeast(minimumLicenseRequired: LicenseType): boolean;
+  /**
+   * For a given plugin and license type, receive information about the status of the license.
+   * @param pluginName the name of the plugin
+   * @param minimumLicenseRequired the minimum valid license for operating the given plugin
+   */
+  check(pluginName: string, minimumLicenseRequired: LicenseType): LicenseCheck;
+  /**
+   * A specific API for interacting with the specific features of the license.
+   * @param name the name of the feature to interact with
+   */
+  getFeature(name: string): LicenseFeature;
 }
