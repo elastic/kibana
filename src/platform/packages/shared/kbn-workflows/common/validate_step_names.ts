5ab51f70643a4a886384354df4ed983906c874a4
dej611 <dej611@gmail.com>
2026-07-30 18:26:39 +0200

    :truck: move part of validation logic into package

--- src/platform/packages/shared/kbn-workflows/common/validate_step_names.ts
+++ src/platform/packages/shared/kbn-workflows/common/validate_step_names.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { WorkflowYaml } from '@kbn/workflows/spec/schema';
+import type { WorkflowYaml } from '../spec/schema';
 
 interface StepNameValidationError {
   stepName: string;
