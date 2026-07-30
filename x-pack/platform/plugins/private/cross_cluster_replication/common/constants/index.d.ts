59ec88c8c2641bd46035deebfae1ba8744720ea9
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-07-30 15:43:57 +0000

    Changes from node scripts/check

--- x-pack/platform/plugins/private/cross_cluster_replication/common/constants/index.d.ts
+++ x-pack/platform/plugins/private/cross_cluster_replication/common/constants/index.d.ts
@@ -1,34 +1,27 @@
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the Elastic License
- * 2.0; you may not use this file except in compliance with the Elastic License
- * 2.0.
- */
-
 export declare const PLUGIN: {
-  ID: string;
-  TITLE: string;
-  minimumLicenseType: 'platinum';
+    ID: string;
+    TITLE: string;
+    minimumLicenseType: "platinum";
 };
-export declare const MAJOR_VERSION = '8.0.0';
+export declare const MAJOR_VERSION = "8.0.0";
 export declare const APPS: {
-  CCR_APP: string;
-  REMOTE_CLUSTER_APP: string;
+    CCR_APP: string;
+    REMOTE_CLUSTER_APP: string;
 };
-export declare const MANAGEMENT_ID = 'cross_cluster_replication';
-export declare const BASE_PATH_REMOTE_CLUSTERS = 'data/remote_clusters';
-export declare const API_BASE_PATH = '/api/cross_cluster_replication';
-export declare const API_REMOTE_CLUSTERS_BASE_PATH = '/api/remote_clusters';
-export declare const API_INDEX_MANAGEMENT_BASE_PATH = '/api/index_management';
+export declare const MANAGEMENT_ID = "cross_cluster_replication";
+export declare const BASE_PATH_REMOTE_CLUSTERS = "data/remote_clusters";
+export declare const API_BASE_PATH = "/api/cross_cluster_replication";
+export declare const API_REMOTE_CLUSTERS_BASE_PATH = "/api/remote_clusters";
+export declare const API_INDEX_MANAGEMENT_BASE_PATH = "/api/index_management";
 export declare const FOLLOWER_INDEX_ADVANCED_SETTINGS: {
-  maxReadRequestOperationCount: number;
-  maxOutstandingReadRequests: number;
-  maxReadRequestSize: string;
-  maxWriteRequestOperationCount: number;
-  maxWriteRequestSize: string;
-  maxOutstandingWriteRequests: number;
-  maxWriteBufferCount: number;
-  maxWriteBufferSize: string;
-  maxRetryDelay: string;
-  readPollTimeout: string;
+    maxReadRequestOperationCount: number;
+    maxOutstandingReadRequests: number;
+    maxReadRequestSize: string;
+    maxWriteRequestOperationCount: number;
+    maxWriteRequestSize: string;
+    maxOutstandingWriteRequests: number;
+    maxWriteBufferCount: number;
+    maxWriteBufferSize: string;
+    maxRetryDelay: string;
+    readPollTimeout: string;
 };
