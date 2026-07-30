d45d97f6cc9f70050c31a61a29b047413b78658f
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2026-07-30 19:10:13 +0200

    fix import

--- src/platform/plugins/shared/discover/test/scout/ui/fixtures/index.ts
+++ src/platform/plugins/shared/discover/test/scout/ui/fixtures/index.ts
@@ -19,7 +19,7 @@ import { UnifiedFieldList } from '@kbn/unified-field-list/test/scout/ui/fixtures
 import { DocViewer } from '@kbn/unified-doc-viewer/test/scout/ui/fixtures/page_objects';
 import { spaceTest as spaceBaseTest } from './common';
 
-export type DiscoverPageObjects = Omit<PageObjects, 'unifiedFieldList'> & {
+export type DiscoverPageObjects = PageObjects & {
   inspector: Inspector;
   unifiedFieldList: UnifiedFieldList;
   docViewer: DocViewer;
