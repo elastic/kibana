4c8005e333267759adf104ef3e9d910a210e181a
Coen Warmer <coen.warmer@gmail.com>
2026-07-30 23:14:45 +0200

    Update logo

--- src/platform/packages/shared/kbn-connector-specs/src/specs/new_relic/icon/index.tsx
+++ src/platform/packages/shared/kbn-connector-specs/src/specs/new_relic/icon/index.tsx
@@ -11,19 +11,14 @@ import type { ConnectorIconProps } from '../../../types';
 
 export default (props: ConnectorIconProps) => (
   <svg
+    viewBox="0 0 832.8 959.8"
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 32 32"
     width="32"
     height="32"
-    role="img"
-    aria-label="New Relic"
     {...props}
   >
-    <rect width="32" height="32" rx="6" fill="#1CE783" />
-    <path
-      fill="#000"
-      d="M9 11.2l7-4 7 4v9.6l-7 4-7-4v-9.6zm2 1.16v7.28l5 2.86 5-2.86v-7.28l-5-2.86-5 2.86z"
-    />
-    <circle cx="16" cy="16" r="2.6" fill="#000" />
+    <path d="M672.6 332.3l160.2-92.4v480L416.4 959.8V775.2l256.2-147.6z" fill="#00ac69" />
+    <path d="M416.4 184.6L160.2 332.3 0 239.9 416.4 0l416.4 239.9-160.2 92.4z" fill="#1ce783" />
+    <path d="M256.2 572.3L0 424.6V239.9l416.4 240v479.9l-160.2-92.2z" fill="#1d252c" />
   </svg>
 );
