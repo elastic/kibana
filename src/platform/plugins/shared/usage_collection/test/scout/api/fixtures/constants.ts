c536b2041fc5ef3507e71c5cabee5a488da7620c
sbelastic <samuel.brito@elastic.co>
2026-07-31 16:17:11 +0100

    Migrate tests

--- src/platform/plugins/shared/usage_collection/test/scout/api/fixtures/constants.ts
+++ src/platform/plugins/shared/usage_collection/test/scout/api/fixtures/constants.ts
@@ -7,10 +7,16 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import type { FtrProviderContext } from '../../ftr_provider_context';
+import {
+  ELASTIC_HTTP_VERSION_HEADER,
+  X_ELASTIC_INTERNAL_ORIGIN_REQUEST,
+} from '@kbn/core-http-common';
 
-export default function ({ loadTestFile }: FtrProviderContext) {
-  describe('UI Metric', () => {
-    loadTestFile(require.resolve('./ui_metric'));
-  });
-}
+export const UI_COUNTERS_REPORT_PATH = '/api/ui_counters/_report';
+
+export const INTERNAL_HEADERS = {
+  'kbn-xsrf': 'scout',
+  'content-type': 'application/json',
+  [X_ELASTIC_INTERNAL_ORIGIN_REQUEST]: 'kibana',
+  [ELASTIC_HTTP_VERSION_HEADER]: '1',
+};
