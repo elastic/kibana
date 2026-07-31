173b8685c8c110d0e2adf7ade436e439214e7467
Andreana Malama <andreana.malama@elastic.co>
2026-07-31 15:55:35 +0300

    Fix bad merge

--- src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/tests/utils/validator/normalizers/common.ts
+++ src/platform/packages/shared/kbn-lens-embeddable-utils/config_builder/tests/utils/validator/normalizers/common.ts
@@ -239,16 +239,15 @@ function normalizeFormBasedAdHocDataViews(
       continue;
     }
 
-      if (ref) {
-        ref.id = newId;
-        // Keep the original layerId in the name so getCommonNormalizer can apply layerRemapping to it.
-      } else {
-        refs.push({
-          id: newId,
-          name: `indexpattern-datasource-layer-${DEFAULT_LAYER_ID}`,
-          type: 'index-pattern',
-        });
-      }
+    if (ref) {
+      ref.id = newId;
+      // Keep the original layerId in the name so getCommonNormalizer can apply layerRemapping to it.
+    } else {
+      refs.push({
+        id: newId,
+        name: layerRefName,
+        type: 'index-pattern',
+      });
     }
   }
 
