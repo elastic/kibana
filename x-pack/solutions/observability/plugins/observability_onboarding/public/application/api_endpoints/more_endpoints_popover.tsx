a3448faad7faec0373841e2eac9f8e4fd3e84bd9
Charlotte Hoblik <116336412+charlotte-hoblik@users.noreply.github.com>
2026-08-04 10:01:50 +0200

    Remove aria-hidden from EuiButtonEmpty
    Removed aria-hidden attribute from EuiButtonEmpty.

--- x-pack/solutions/observability/plugins/observability_onboarding/public/application/api_endpoints/more_endpoints_popover.tsx
+++ x-pack/solutions/observability/plugins/observability_onboarding/public/application/api_endpoints/more_endpoints_popover.tsx
@@ -82,7 +82,6 @@ export const MoreEndpointsPopover = ({
                 css={css`
                   margin-left: ${euiTheme.size.xs};
                 `}
-                aria-hidden={true}
               />
             </EuiButtonEmpty>
           }
