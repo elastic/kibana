0ff563a2c2b1d171a678f5cb668ebba4d84d4a17
Tyler Smalley <tyler.smalley@elastic.co>
2026-08-04 15:08:30 +0000

    Use 9.5 Redux dependencies for license management

--- x-pack/platform/plugins/shared/license_management/public/application/store/types.ts
+++ x-pack/platform/plugins/shared/license_management/public/application/store/types.ts
@@ -9,9 +9,8 @@ import type { ScopedHistory } from '@kbn/core/public';
 import type { HttpSetup, NotificationsStart } from '@kbn/core/public';
 import type { LicensingPluginSetup } from '@kbn/licensing-plugin/public';
 import type { ILicense } from '@kbn/licensing-types';
-import type { ThunkAction } from 'redux-thunk-v2';
-import type { Action as ReduxAction, Store } from 'redux-v4';
-import type { ThunkDispatch } from 'redux-thunk-v2';
+import type { Action as ReduxAction, Store } from 'redux';
+import type { ThunkAction, ThunkDispatch } from 'redux-thunk';
 import type { TelemetryPluginStart } from '@kbn/telemetry-plugin/public';
 import type { BreadcrumbService } from '../breadcrumbs';
 
