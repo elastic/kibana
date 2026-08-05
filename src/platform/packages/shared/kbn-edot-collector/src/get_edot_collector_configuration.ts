a9407ae525c5bb8dc0e63b701d3a14e7f684e1a7
Alex Fernandez <fernandez.fernandez@elastic.co>
2026-08-05 15:38:39 +0200

    Revert otel demo change

--- src/platform/packages/shared/kbn-edot-collector/src/get_edot_collector_configuration.ts
+++ src/platform/packages/shared/kbn-edot-collector/src/get_edot_collector_configuration.ts
@@ -72,9 +72,6 @@ export function getEdotCollectorConfig({
         flush: {
           interval: '1s',
         },
-        tls: {
-          insecure_skip_verify: true,
-        },
       },
     },
     service: {
