e6e557f12ed5fdeedfa3a0168c2722af6a7fdd87
Matthew Kime <matt@mattki.me>
2026-08-05 17:09:13 -0500

    Update type import for DataSource in tests

--- x-pack/platform/plugins/private/data_federation/public/main.test.tsx
+++ x-pack/platform/plugins/private/data_federation/public/main.test.tsx
@@ -14,7 +14,7 @@ import { MockAppHeaderProvider } from '@kbn/app-header/mocks';
 import { KibanaContextProvider } from '@kbn/kibana-react-plugin/public';
 import { mainTranslations } from './main_i18n';
 import { Main } from './main';
-import type { DataSetWithName, DataSourceName } from '../common';
+import type { DataSetWithName, DataSource } from '../common';
 
 jest.mock('./datasets_tab_content', () => ({
   DatasetsTabContent: () => <div data-test-subj="datasetsTabContent" />,
@@ -33,7 +33,7 @@ const createServicesMock = ({
   dataSources,
   dataSets,
 }: {
-  dataSources: DataSourceName[];
+  dataSources: DataSource[];
   dataSets: DataSetWithName[];
 }) => ({
   dataSourcesClient: {
