d31c8c0b07af79e22d5537d676f5c07f88aec312
Jean-Louis Leysens <jloleysens@gmail.com>
2026-08-05 16:08:18 +0200

    [kbn-bench] Remove duplicate warm-start headers

--- src/platform/packages/shared/kbn-core-server-benchmarks/ci_warm_start_memory/memory_regression_threshold.test.ts
+++ src/platform/packages/shared/kbn-core-server-benchmarks/ci_warm_start_memory/memory_regression_threshold.test.ts
@@ -7,13 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the "Elastic License
- * 2.0", the "GNU Affero General Public License v3.0 only", or the "Server Side
- * Public License, v 1".
- */
-
 import {
   evaluatePairedMemoryRule,
   MIN_VALID_WARM_START_MEMORY_PAIRS,
