883652f45204fd659228f03ea4601702bf00d6e5
Anton Dosov <anton.dosov@elastic.co>
2026-08-06 11:01:10 +0200

    Update src/core/packages/application/browser-internal/src/ui/app_not_found_screen.tsx
    Co-authored-by: Florent LB <florent.leborgne@elastic.co>

--- src/core/packages/application/browser-internal/src/ui/app_not_found_screen.tsx
+++ src/core/packages/application/browser-internal/src/ui/app_not_found_screen.tsx
@@ -28,7 +28,7 @@ export const AppNotFound = () => (
         <p>
           <FormattedMessage
             id="core.application.appNotFound.pageDescription"
-            defaultMessage="The application at this URL might not exist, or you might not have permission to access it. Go back or choose another app from the menu."
+            defaultMessage="This app doesn't exist, or you don't have permission to access it. Go back or choose another app from the menu."
           />
         </p>
       }
