a3f3177dd2d7650cc1f9a4c829ea0f1ec9484639
Drew Tate <drew.tate@elastic.co>
2026-08-06 15:36:59 -0600

    fix test

--- src/platform/packages/private/kbn-controls-renderer/src/components/control_panel.test.tsx
+++ src/platform/packages/private/kbn-controls-renderer/src/components/control_panel.test.tsx
@@ -14,7 +14,6 @@ import { OPTIONS_LIST_CONTROL, DEFAULT_PINNED_CONTROL_STATE } from '@kbn/control
 import type { DefaultEmbeddableApi } from '@kbn/embeddable-plugin/public';
 import { act, render } from '@testing-library/react';
 import type { Action } from '@kbn/ui-actions-plugin/public';
-import { render } from '@testing-library/react';
 
 import type { ControlsRendererParentApi } from '../types';
 import { ControlPanel } from './control_panel';
