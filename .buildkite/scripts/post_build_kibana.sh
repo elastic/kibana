f1edda2f1e6678ed12996373ca48aaea81e450f3
Tamerlan Gudabayev <tamerlan.gudabayev@elastic.co>
2026-08-06 15:50:59 +0200

    ship bundle sizes

--- .buildkite/scripts/post_build_kibana.sh
+++ .buildkite/scripts/post_build_kibana.sh
@@ -4,13 +4,6 @@ set -euo pipefail
 
 source "$(dirname "$0")/common/util.sh"
 
-
-# [rspack-transition] avoid shipping bundle sizes while RSPack is not the default
-if [[ "${KBN_USE_RSPACK:-}" == "true" ]]; then
-  echo "Skipping shipping bundle sizes to CI Stats (rspack build)"
-  export DISABLE_CI_STATS_SHIPPING=true
-fi
-
 if [[ ! "${DISABLE_CI_STATS_SHIPPING:-}" ]]; then
   cmd=(
     "node" "scripts/ship_ci_stats"
