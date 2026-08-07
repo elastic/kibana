d6f353b648c59f96e676e31f47573c9d83dec4f3
heenawter <hannah.wright@elastic.co>
2026-08-07 13:16:58 -0600

    Fix more types

--- src/platform/plugins/shared/controls/public/controls/esql_control/types.ts
+++ src/platform/plugins/shared/controls/public/controls/esql_control/types.ts
@@ -57,7 +57,7 @@ export type ESQLOptionsListComponentState = Pick<
    * being impacted by default embeddable title handling, we switch to `label` for the implementation
    */
   label: string;
-} & Omit<TemporaryState<string>, 'requestSize'>;
+} & Omit<TemporaryState<string>, 'requestSize' | 'isPartial'>;
 
 export type ESQLOptionsListComponentApi = HasType &
   HasUniqueId &
@@ -67,7 +67,7 @@ export type ESQLOptionsListComponentApi = HasType &
   SettersOf<
     Omit<
       TemporaryState<string>,
-      'availableOptions' | 'requestSize' | 'searchStringValid' | 'totalCardinality'
+      'availableOptions' | 'requestSize' | 'searchStringValid' | 'totalCardinality' | 'isPartial'
     >
   > &
   OptionsListSelectionsApi & {
