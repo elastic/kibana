28724eeccb13687fb2c0d8c009aeed521737d728
Matthias Wilhelm <matthias.wilhelm@elastic.co>
2026-08-10 16:51:23 +0200

    Refactor jest mock for useInternalStateDispatch to use jest.mocked for improved type safety

--- src/platform/plugins/shared/discover/public/context_awareness/profile_providers/common/metrics_data_source_profile/accessor/chart_section.test.tsx
+++ src/platform/plugins/shared/discover/public/context_awareness/profile_providers/common/metrics_data_source_profile/accessor/chart_section.test.tsx
@@ -184,7 +184,7 @@ describe('MetricsExperienceGridWrapper', () => {
     (useAppStateSelector as jest.Mock).mockImplementation((selector) =>
       selector({ breakdownField: 'host.name' })
     );
-    (useInternalStateDispatch as unknown as jest.Mock).mockReturnValue(mockDispatch);
+    jest.mocked(useInternalStateDispatch).mockReturnValue(mockDispatch);
     (useCurrentTabAction as jest.Mock).mockReturnValue(mockUpdateAppStateAction);
     mockDispatch.mockClear();
     mockUpdateAppStateAction.mockClear();
