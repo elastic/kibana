f11a5cbce357d7d0687ddaa2d30b8f484c260e25
Nick Peihl <nick.peihl@elastic.co>
2026-08-11 09:50:34 -0400

    Review feedback

--- src/platform/plugins/private/vis_types/vega/public/data_model/time_cache.ts
+++ src/platform/plugins/private/vis_types/vega/public/data_model/time_cache.ts
@@ -86,7 +86,8 @@ export class TimeCache {
    * @internal
    */
   _getBounds(): CacheBounds {
-    const bounds = this._timefilter.calculateBounds(this._timeRange!);
+    const timeRange = this._timeRange ?? this._timefilter.getTime();
+    const bounds = this._timefilter.calculateBounds(timeRange);
     return {
       min: bounds.min!.valueOf(),
       max: bounds.max!.valueOf(),
