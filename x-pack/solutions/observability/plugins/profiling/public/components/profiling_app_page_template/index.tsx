8a21315c2685db809023146fae0e0f061d6ac66e
Alex Fernandez <fernandez.fernandez@elastic.co>
2026-08-11 17:26:53 +0200

    Initial migration

--- x-pack/solutions/observability/plugins/profiling/public/components/profiling_app_page_template/index.tsx
+++ x-pack/solutions/observability/plugins/profiling/public/components/profiling_app_page_template/index.tsx
@@ -5,31 +5,21 @@
  * 2.0.
  */
 
-import type { EuiPageHeaderContentProps, EuiPageHeaderProps } from '@elastic/eui';
-import {
-  EuiBetaBadge,
-  EuiButton,
-  EuiCallOut,
-  EuiFlexGroup,
-  EuiFlexItem,
-  EuiTab,
-  EuiTabs,
-} from '@elastic/eui';
+import type { AppHeaderTab, AppHeaderTitle } from '@kbn/app-header';
+import { EuiFlexGroup, EuiFlexItem } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
-import { css } from '@emotion/react';
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import type { NoDataPageProps } from '@kbn/shared-ux-page-no-data-types';
+import { KbnWarningCallout } from '@kbn/ui-callout';
+import { AppHeader } from '@kbn/app-header';
+import { IndexLifecyclePhaseSelectOption } from '../../../common/storage_explorer';
 import { useProfilingDependencies } from '../contexts/profiling_dependencies/use_profiling_dependencies';
 import { PrimaryProfilingSearchBar } from './primary_profiling_search_bar';
 import { useLocalStorage } from '../../hooks/use_local_storage';
 import { useProfilingSetupStatus } from '../contexts/profiling_setup_status/use_profiling_setup_status';
-
-const headerPaddingFixCss = css`
-  .euiPageHeaderContent {
-    padding-bottom: 0;
-  }
-`;
+import { useProfilingRouter } from '../../hooks/use_profiling_router';
+import { AddDataTabs } from '../../views/add_data_view';
 
 export function ProfilingAppPageTemplate({
   children,
@@ -43,12 +33,12 @@ export function ProfilingAppPageTemplate({
   showBetaBadge = false,
   customSearchBar,
 }: {
-  children: React.ReactElement;
-  tabs?: EuiPageHeaderContentProps['tabs'];
+  children?: React.ReactElement;
+  tabs?: AppHeaderTab[];
   hideSearchBar?: boolean;
   noDataConfig?: NoDataPageProps;
   restrictWidth?: boolean;
-  pageTitle?: React.ReactNode;
+  pageTitle?: AppHeaderTitle;
   showBetaBadge?: boolean;
   customSearchBar?: React.ReactNode;
 }) {
@@ -66,6 +56,8 @@ export function ProfilingAppPageTemplate({
 
   const history = useHistory();
 
+  const router = useProfilingRouter();
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [history.location.pathname]);
@@ -73,49 +65,6 @@ export function ProfilingAppPageTemplate({
   return (
     <ObservabilityPageTemplate
       noDataConfig={noDataConfig}
-      pageHeader={{
-        'data-test-subj': 'profilingPageTemplate',
-        pageTitle: (
-          <EuiFlexGroup gutterSize="s" alignItems="baseline">
-            <EuiFlexItem grow={false}>{pageTitle}</EuiFlexItem>
-            {showBetaBadge && (
-              <EuiFlexItem grow={false}>
-                <EuiBetaBadge
-                  label="Beta"
-                  color="hollow"
-                  tooltipContent={i18n.translate('xpack.profiling.header.betaBadgeTooltip', {
-                    defaultMessage: 'This module is not GA. Please help us by reporting any bugs.',
-                  })}
-                />
-              </EuiFlexItem>
-            )}
-          </EuiFlexGroup>
-        ),
-        color: 'subdued' as unknown as EuiPageHeaderProps['color'], // This value is valid but not properly typed
-        children:
-          tabs.length > 0 || !hideSearchBar ? (
-            <EuiFlexGroup direction="column">
-              {tabs.length > 0 && (
-                <EuiFlexItem grow={false}>
-                  <EuiTabs size="m" bottomBorder={!hideSearchBar}>
-                    {tabs.map(({ label, ...tabRest }) => (
-                      <EuiTab key={tabRest.href} {...tabRest}>
-                        {label}
-                      </EuiTab>
-                    ))}
-                  </EuiTabs>
-                </EuiFlexItem>
-              )}
-              {!hideSearchBar && (
-                <EuiFlexItem grow={false}>
-                  {customSearchBar ?? <PrimaryProfilingSearchBar />}
-                </EuiFlexItem>
-              )}
-            </EuiFlexGroup>
-          ) : undefined,
-        bottomBorder: 'extended',
-        css: hideSearchBar && tabs.length > 0 ? headerPaddingFixCss : undefined,
-      }}
       restrictWidth={restrictWidth}
       pageSectionProps={{
         contentProps: {
@@ -127,32 +76,80 @@ export function ProfilingAppPageTemplate({
       }}
     >
       <EuiFlexGroup direction="column" style={{ maxWidth: '100%' }}>
+        <AppHeader
+          spacing="largeBleed"
+          title={pageTitle}
+          tabs={tabs}
+          menu={{
+            items: [
+              {
+                id: 'storage-explorer',
+                label: 'Storage explorer',
+                href: router.link('/storage-explorer', {
+                  query: {
+                    kuery: '',
+                    rangeFrom: 'now-15m',
+                    rangeTo: 'now',
+                    indexLifecyclePhase: IndexLifecyclePhaseSelectOption.All,
+                  },
+                }),
+                iconType: 'database',
+              },
+              {
+                id: 'settings',
+                label: 'Settings',
+                href: router.link('/settings'),
+                iconType: 'gear',
+                overflow: true,
+              },
+            ],
+            primaryActionItem: {
+              id: 'add-data',
+              label: 'Add data',
+              href: router.link('/add-data-instructions', {
+                query: { selectedTab: AddDataTabs.Kubernetes },
+              }),
+              iconType: 'plusInCircle',
+            },
+          }}
+          badges={
+            showBetaBadge
+              ? [
+                  {
+                    label: 'Beta',
+                    color: 'hollow',
+                    tooltip: 'This module is not GA. Please help us by reporting any bugs.',
+                  },
+                ]
+              : undefined
+          }
+        />
+        {!hideSearchBar && (
+          <EuiFlexItem grow={false}>{customSearchBar ?? <PrimaryProfilingSearchBar />}</EuiFlexItem>
+        )}
         {profilingSetupStatus?.unauthorized === true && privilegesWarningDismissed !== true ? (
           <EuiFlexItem grow={false}>
-            <EuiCallOut
-              announceOnMount
-              iconType="warning"
+            <KbnWarningCallout
               title={i18n.translate('xpack.profiling.privilegesWarningTitle', {
                 defaultMessage: 'User privilege limitation',
               })}
-            >
-              <p>
-                {i18n.translate('xpack.profiling.privilegesWarningDescription', {
-                  defaultMessage:
-                    'Due to privileges issues we could not check the Universal Profiling status. If you encounter any issues or if data fails to load, please contact your administrator for assistance.',
-                })}
-              </p>
-              <EuiButton
-                data-test-subj="profilingProfilingAppPageTemplateDismissButton"
-                onClick={() => {
-                  setPrivilegesWarningDismissed(true);
-                }}
-              >
-                {i18n.translate('xpack.profiling.dismissPrivilegesCallout', {
-                  defaultMessage: 'Dismiss',
-                })}
-              </EuiButton>
-            </EuiCallOut>
+              text={i18n.translate('xpack.profiling.privilegesWarningDescription', {
+                defaultMessage:
+                  'Due to privileges issues we could not check the Universal Profiling status. If you encounter any issues or if data fails to load, please contact your administrator for assistance.',
+              })}
+              actionProps={{
+                primary: {
+                  children: i18n.translate('xpack.profiling.dismissPrivilegesCallout', {
+                    defaultMessage: 'Dismiss',
+                  }),
+                  onClick: () => {
+                    setPrivilegesWarningDismissed(true);
+                  },
+                  'data-test-subj': 'profilingProfilingAppPageTemplateDismissButton',
+                },
+              }}
+              announceOnMount
+            />
           </EuiFlexItem>
         ) : null}
         <EuiFlexItem>{children}</EuiFlexItem>
