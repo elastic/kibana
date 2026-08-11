2bb2211dcfb4ef68abdae844fb46966f420a03e7
Tyler Smalley <tyler.smalley@elastic.co>
2026-08-11 14:49:12 +0000

    Fix TypeScript 7 backport conflicts on 9.3

--- src/platform/packages/shared/kbn-test/src/mocha/junit_report_generation.test.js
+++ src/platform/packages/shared/kbn-test/src/mocha/junit_report_generation.test.js
@@ -55,14 +55,6 @@ describe('dev/mocha/junit report generation', () => {
     const [testsuite] = report.testsuites.testsuite;
     expect(testsuite.$.time).toMatch(DURATION_REGEX);
     expect(testsuite.$.timestamp).toMatch(ISO_DATE_SEC_REGEX);
-<<<<<<< HEAD
-    const expectedCommandLine = process.env.CI
-      ? 'node scripts/jest --config src/platform/packages/shared/kbn-test/jest.config.js --runInBand --coverage=false --passWithNoTests'
-      : 'node node_modules/jest-worker/build/workers/processChild.js';
-
-    expect(testsuite.$).toMatchObject({
-      'command-line': expectedCommandLine,
-=======
     const expectedCommandLineMultiple =
       'node scripts/jest --config src/platform/packages/shared/kbn-test/jest.config.js --runInBand --coverage=false --passWithNoTests';
     const expectedMoonCommandLine =
@@ -75,7 +67,6 @@ describe('dev/mocha/junit report generation', () => {
           `(${expectedCommandLineMultiple}|${expectedMoonCommandLine}|${expectedCommandLineSingle})`
         )
       ),
->>>>>>> 6eb83a3d5a73 ([CI] Migrate to TypeScript 7 (#277950))
       failures: '2',
       name: 'test',
       skipped: '1',
