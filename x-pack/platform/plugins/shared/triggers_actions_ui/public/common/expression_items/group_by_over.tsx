aca85ab0540edf00347bf5b40a2d2579224ed08f
adcoelho <antonio.coelho@elastic.co>
2026-08-12 14:00:08 +0200

    Revert "Add missing error message."
    This reverts commit 1bbeb28f4fc5b94b14cb9a8391d8ab5412a7487e.

--- x-pack/platform/plugins/shared/triggers_actions_ui/public/common/expression_items/group_by_over.tsx
+++ x-pack/platform/plugins/shared/triggers_actions_ui/public/common/expression_items/group_by_over.tsx
@@ -19,13 +19,11 @@ import {
   EuiSelect,
   EuiFieldNumber,
   EuiComboBox,
-  EuiFormErrorText,
 } from '@elastic/eui';
 import { builtInGroupByTypes } from '../constants';
 import type { FieldOption, GroupByType } from '../types';
 import { ClosablePopoverTitle } from './components';
 import type { IErrorObject } from '../../types';
-import * as i18nTranslations from './translations';
 
 interface GroupByOverFieldOption {
   label: string;
@@ -123,182 +121,155 @@ export const GroupByExpression = ({
     }
   }, [selectedTermsFieldsOptions, fields, onChangeSelectedTermField]);
 
-  const hasTermField = Boolean(termField && termField.length > 0);
-  const hasTermSize = Boolean(termSize);
-  const isExpressionIncomplete = groupBy !== 'all' && !(hasTermSize && hasTermField);
-  const termFieldErrors = (errors.termField ?? []) as string[];
-  const termSizeErrors = (errors.termSize ?? []) as string[];
-  const hasValidationErrors = termFieldErrors.length > 0 || termSizeErrors.length > 0;
-  const isExpressionInvalid = isExpressionIncomplete || hasValidationErrors;
-
-  let expressionErrors: string[] = [];
-  if (hasValidationErrors) {
-    expressionErrors = [...termFieldErrors, ...termSizeErrors];
-  } else if (isExpressionIncomplete) {
-    if (!hasTermField) {
-      expressionErrors.push(i18nTranslations.TERM_FIELD_REQUIRED);
-    }
-    if (!hasTermSize) {
-      expressionErrors.push(i18nTranslations.TERM_SIZE_REQUIRED);
-    }
-  }
-
   return (
-    <>
-      <EuiPopover
-        aria-label={i18n.translate(
-          'xpack.triggersActionsUI.common.expressionItems.groupByOver.popoverAriaLabel',
-          { defaultMessage: 'Over' }
-        )}
-        button={
-          <EuiExpression
-            description={`${
-              groupByTypes[groupBy].sizeRequired
-                ? i18n.translate(
-                    'xpack.triggersActionsUI.common.expressionItems.groupByType.groupedOverLabel',
-                    {
-                      defaultMessage: 'grouped over',
-                    }
-                  )
-                : i18n.translate(
-                    'xpack.triggersActionsUI.common.expressionItems.groupByType.overLabel',
-                    {
-                      defaultMessage: 'over',
-                    }
-                  )
-            }`}
-            data-test-subj="groupByExpression"
-            value={`${groupByTypes[groupBy].text} ${
-              groupByTypes[groupBy].sizeRequired
-                ? `${termSize} ${termField ? `'${termField}'` : ''}`
-                : ''
-            }`}
-            isActive={groupByPopoverOpen || (groupBy === 'top' && !(termSize && termField))}
-            onClick={() => {
-              setGroupByPopoverOpen(true);
-            }}
-            display={display === 'inline' ? 'inline' : 'columns'}
-            isInvalid={isExpressionInvalid}
-          />
-        }
-        isOpen={groupByPopoverOpen}
-        closePopover={() => {
-          setGroupByPopoverOpen(false);
-        }}
-        ownFocus
-        display={display === 'fullWidth' ? 'block' : 'inline-block'}
-        anchorPosition={popupPosition ?? 'downRight'}
-        repositionOnScroll
-      >
-        <div>
-          <ClosablePopoverTitle onClose={() => setGroupByPopoverOpen(false)}>
-            <FormattedMessage
-              id="xpack.triggersActionsUI.common.expressionItems.groupByType.overButtonLabel"
-              defaultMessage="over"
-            />
-          </ClosablePopoverTitle>
-          <EuiFlexGroup justifyContent="spaceBetween">
-            <EuiFlexItem grow={1}>
-              <EuiSelect
-                data-test-subj="overExpressionSelect"
-                value={groupBy}
-                onChange={(e) => {
-                  if (groupByTypes[e.target.value].sizeRequired) {
-                    onChangeSelectedTermSize(MIN_TERM_SIZE);
-                    onChangeSelectedTermField('');
-                  } else {
-                    onChangeSelectedTermSize(undefined);
-                    onChangeSelectedTermField(undefined);
-                  }
-                  onChangeSelectedGroupBy(e.target.value);
-                }}
-                options={Object.values(groupByTypes).map(({ text, value }) => {
-                  return {
-                    text,
-                    value,
-                  };
-                })}
-                aria-label={i18n.translate(
-                  'xpack.triggersActionsUI.common.expressionItems.groupByType.groupBySelectAriaLabel',
+    <EuiPopover
+      aria-label={i18n.translate(
+        'xpack.triggersActionsUI.common.expressionItems.groupByOver.popoverAriaLabel',
+        { defaultMessage: 'Over' }
+      )}
+      button={
+        <EuiExpression
+          description={`${
+            groupByTypes[groupBy].sizeRequired
+              ? i18n.translate(
+                  'xpack.triggersActionsUI.common.expressionItems.groupByType.groupedOverLabel',
                   {
-                    defaultMessage: 'Group by',
+                    defaultMessage: 'grouped over',
                   }
-                )}
-              />
-            </EuiFlexItem>
+                )
+              : i18n.translate(
+                  'xpack.triggersActionsUI.common.expressionItems.groupByType.overLabel',
+                  {
+                    defaultMessage: 'over',
+                  }
+                )
+          }`}
+          data-test-subj="groupByExpression"
+          value={`${groupByTypes[groupBy].text} ${
+            groupByTypes[groupBy].sizeRequired
+              ? `${termSize} ${termField ? `'${termField}'` : ''}`
+              : ''
+          }`}
+          isActive={groupByPopoverOpen || (groupBy === 'top' && !(termSize && termField))}
+          onClick={() => {
+            setGroupByPopoverOpen(true);
+          }}
+          display={display === 'inline' ? 'inline' : 'columns'}
+          isInvalid={!(groupBy === 'all' || (termSize && termField && termField.length > 0))}
+        />
+      }
+      isOpen={groupByPopoverOpen}
+      closePopover={() => {
+        setGroupByPopoverOpen(false);
+      }}
+      ownFocus
+      display={display === 'fullWidth' ? 'block' : 'inline-block'}
+      anchorPosition={popupPosition ?? 'downRight'}
+      repositionOnScroll
+    >
+      <div>
+        <ClosablePopoverTitle onClose={() => setGroupByPopoverOpen(false)}>
+          <FormattedMessage
+            id="xpack.triggersActionsUI.common.expressionItems.groupByType.overButtonLabel"
+            defaultMessage="over"
+          />
+        </ClosablePopoverTitle>
+        <EuiFlexGroup justifyContent="spaceBetween">
+          <EuiFlexItem grow={1}>
+            <EuiSelect
+              data-test-subj="overExpressionSelect"
+              value={groupBy}
+              onChange={(e) => {
+                if (groupByTypes[e.target.value].sizeRequired) {
+                  onChangeSelectedTermSize(MIN_TERM_SIZE);
+                  onChangeSelectedTermField('');
+                } else {
+                  onChangeSelectedTermSize(undefined);
+                  onChangeSelectedTermField(undefined);
+                }
+                onChangeSelectedGroupBy(e.target.value);
+              }}
+              options={Object.values(groupByTypes).map(({ text, value }) => {
+                return {
+                  text,
+                  value,
+                };
+              })}
+              aria-label={i18n.translate(
+                'xpack.triggersActionsUI.common.expressionItems.groupByType.groupBySelectAriaLabel',
+                {
+                  defaultMessage: 'Group by',
+                }
+              )}
+            />
+          </EuiFlexItem>
 
-            {groupByTypes[groupBy].sizeRequired ? (
-              <>
-                <EuiFlexItem grow={1}>
-                  <EuiFormRow
+          {groupByTypes[groupBy].sizeRequired ? (
+            <>
+              <EuiFlexItem grow={1}>
+                <EuiFormRow
+                  isInvalid={Number(errors.termSize.length) > 0}
+                  error={errors.termSize as string[]}
+                >
+                  <EuiFieldNumber
+                    data-test-subj="fieldsNumberSelect"
+                    css={css`
+                      min-width: 50px;
+                    `}
                     isInvalid={Number(errors.termSize.length) > 0}
-                    error={errors.termSize as string[]}
-                  >
-                    <EuiFieldNumber
-                      data-test-subj="fieldsNumberSelect"
-                      css={css`
-                        min-width: 50px;
-                      `}
-                      isInvalid={Number(errors.termSize.length) > 0}
-                      value={termSize || ''}
-                      onChange={(e) => {
-                        const { value } = e.target;
-                        const termSizeVal = value !== '' ? parseFloat(value) : undefined;
-                        onChangeSelectedTermSize(termSizeVal);
-                      }}
-                      min={MIN_TERM_SIZE}
-                      max={MAX_TERM_SIZE}
-                    />
-                  </EuiFormRow>
-                </EuiFlexItem>
-                <EuiFlexItem grow={2}>
-                  <EuiFormRow
-                    isInvalid={Number(errors.termField.length) > 0}
-                    error={errors.termField as string[]}
-                    aria-label={i18n.translate(
-                      'xpack.triggersActionsUI.common.expressionItems.groupByOver.termFieldAriaLabel',
-                      { defaultMessage: 'Group by field' }
+                    value={termSize || ''}
+                    onChange={(e) => {
+                      const { value } = e.target;
+                      const termSizeVal = value !== '' ? parseFloat(value) : undefined;
+                      onChangeSelectedTermSize(termSizeVal);
+                    }}
+                    min={MIN_TERM_SIZE}
+                    max={MAX_TERM_SIZE}
+                  />
+                </EuiFormRow>
+              </EuiFlexItem>
+              <EuiFlexItem grow={2}>
+                <EuiFormRow
+                  isInvalid={Number(errors.termField.length) > 0}
+                  error={errors.termField as string}
+                  aria-label={i18n.translate(
+                    'xpack.triggersActionsUI.common.expressionItems.groupByOver.termFieldAriaLabel',
+                    { defaultMessage: 'Group by field' }
+                  )}
+                >
+                  <EuiComboBox
+                    singleSelection={canSelectMultiTerms ? false : { asPlainText: true }}
+                    placeholder={i18n.translate(
+                      'xpack.triggersActionsUI.common.expressionItems.groupByType.timeFieldOptionLabel',
+                      {
+                        defaultMessage: 'Select a field',
+                      }
                     )}
-                  >
-                    <EuiComboBox
-                      singleSelection={canSelectMultiTerms ? false : { asPlainText: true }}
-                      placeholder={i18n.translate(
-                        'xpack.triggersActionsUI.common.expressionItems.groupByType.timeFieldOptionLabel',
-                        {
-                          defaultMessage: 'Select a field',
-                        }
-                      )}
-                      data-test-subj="fieldsExpressionSelect"
-                      isInvalid={Number(errors.termField.length) > 0}
-                      selectedOptions={selectedTermsFieldsOptions}
-                      onChange={(
-                        selectedOptions: Array<EuiComboBoxOptionOption<GroupByOverFieldOption>>
-                      ) => {
-                        const selectedTermFields = selectedOptions.map((option) => option.label);
+                    data-test-subj="fieldsExpressionSelect"
+                    isInvalid={Number(errors.termField.length) > 0}
+                    selectedOptions={selectedTermsFieldsOptions}
+                    onChange={(
+                      selectedOptions: Array<EuiComboBoxOptionOption<GroupByOverFieldOption>>
+                    ) => {
+                      const selectedTermFields = selectedOptions.map((option) => option.label);
 
-                        const termsToSave =
-                          Array.isArray(selectedTermFields) && selectedTermFields.length > 1
-                            ? selectedTermFields
-                            : selectedTermFields[0];
+                      const termsToSave =
+                        Array.isArray(selectedTermFields) && selectedTermFields.length > 1
+                          ? selectedTermFields
+                          : selectedTermFields[0];
 
-                        onChangeSelectedTermField(termsToSave);
-                        setSelectedTermsFieldsOptions(selectedOptions);
-                      }}
-                      options={availableFieldOptions}
-                    />
-                  </EuiFormRow>
-                </EuiFlexItem>
-              </>
-            ) : null}
-          </EuiFlexGroup>
-        </div>
-      </EuiPopover>
-      {isExpressionInvalid && expressionErrors.length > 0 ? (
-        <EuiFormErrorText data-test-subj="groupByExpressionErrorText">
-          {expressionErrors.join(' ')}
-        </EuiFormErrorText>
-      ) : null}
-    </>
+                      onChangeSelectedTermField(termsToSave);
+                      setSelectedTermsFieldsOptions(selectedOptions);
+                    }}
+                    options={availableFieldOptions}
+                  />
+                </EuiFormRow>
+              </EuiFlexItem>
+            </>
+          ) : null}
+        </EuiFlexGroup>
+      </div>
+    </EuiPopover>
   );
 };
 
