5ec81995588815e591156a6cd08b3c03ae013e77
Sid <siddharthmantri1@gmail.com>
2026-08-12 09:17:36 +0200

    Update use_last_agent_id.ts

--- x-pack/platform/plugins/shared/agent_builder/public/application/hooks/use_last_agent_id.ts
+++ x-pack/platform/plugins/shared/agent_builder/public/application/hooks/use_last_agent_id.ts
@@ -14,7 +14,6 @@ import { useEffectiveSpaceDefaultAgent } from './use_space_default_agent';
 
 /**
  * Reads the last used agent ID for the active space directly from localStorage.
- *
  * Unlike useLastAgentId, this is not a hook and can be called conditionally
  * or inside callbacks to get the current value at call time.
  */
