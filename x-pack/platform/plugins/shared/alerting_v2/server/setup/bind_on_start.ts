2b9a0b59503543e20da59d643300aaeab9bd5403
adcoelho <antonio.coelho@elastic.co>
2026-08-12 15:02:02 +0200

    Adding back some catches because of linting.

--- x-pack/platform/plugins/shared/alerting_v2/server/setup/bind_on_start.ts
+++ x-pack/platform/plugins/shared/alerting_v2/server/setup/bind_on_start.ts
@@ -14,10 +14,12 @@ import { ChangeHistoryClientToken } from '../lib/rule_changes_history';
 import { ResourceManager } from '../lib/services/resource_service/resource_manager';
 import { initializeResources } from '../resources/register_resources';
 import { scheduleApiKeyInvalidationTask } from '../lib/tasks/invalidate_pending_api_keys/schedule_task';
+import { INVALIDATE_API_KEYS_TASK_ID } from '../lib/tasks/invalidate_pending_api_keys/task_definition';
 import type { PluginConfig } from '../config';
 import type { AlertingServerStartDependencies } from '../types';
 import { scheduleDispatcherTask } from '../lib/dispatcher/schedule_task';
 import { scheduleTelemetryTask } from '../lib/usage/schedule_task';
+import { TASK_ID as TELEMETRY_TASK_ID } from '../lib/usage/constants';
 import { initSubscribers } from '../lib/events/init_subscribers';
 import { ALERTING_LOG_CODES } from '../lib/errors/error_codes';
 import { LoggerServiceToken } from '../lib/services/logger_service/logger_service';
@@ -59,11 +61,23 @@ export function bindOnStart({ bind }: ContainerModuleLoadOptions) {
       logger: tasksLogger,
       taskManager,
       interval: config.invalidateApiKeysTask.interval,
+    }).catch((error) => {
+      tasksLogger.error({
+        error,
+        code: ALERTING_LOG_CODES.TASKS_SCHEDULE_FAILED,
+        labels: { task_id: INVALIDATE_API_KEYS_TASK_ID },
+      });
     });
 
     scheduleTelemetryTask({
       logger: tasksLogger,
       taskManager,
+    }).catch((error) => {
+      tasksLogger.error({
+        error,
+        code: ALERTING_LOG_CODES.TASKS_SCHEDULE_FAILED,
+        labels: { task_id: TELEMETRY_TASK_ID },
+      });
     });
   });
 }
