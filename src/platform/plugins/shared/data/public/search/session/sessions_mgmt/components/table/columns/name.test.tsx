61bd6c80d84db76b998ab0d660fc7d86677ab71e
Matthias Polman <matthias.wilhelm@elastic.co>
2026-08-13 18:53:46 +0200

    undo redundant change

--- src/platform/plugins/shared/data/public/search/session/sessions_mgmt/components/table/columns/name.test.tsx
+++ src/platform/plugins/shared/data/public/search/session/sessions_mgmt/components/table/columns/name.test.tsx
@@ -6,6 +6,7 @@
  * your election, the "Elastic License 2.0", the "GNU Affero General Public
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
+
 import { coreMock } from '@kbn/core/public/mocks';
 import { createSearchUsageCollectorMock } from '../../../../../collectors/mocks';
 import { nameColumn } from './name';
