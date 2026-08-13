1de0b5a424ac58f8218abce2e33a240cfbb12e08
James Gowdy <jgowdy@elastic.co>
2026-08-13 19:54:54 +0100

    fixing test

--- x-pack/platform/packages/shared/file-upload/src/utils.test.ts
+++ x-pack/platform/packages/shared/file-upload/src/utils.test.ts
@@ -79,6 +79,11 @@ describe('utils', () => {
         '{"c":3}',
         '{"d":4}',
         '{"e":5}',
+        '{"f":6}',
+        '{"g":7}',
+        '{"h":8}',
+        '{"i":9}',
+        '{"j":10}',
         'not-json-but-beyond-sample',
       ];
       expect(looksLikeNdjson(lines.join('\n'))).toBe(true);
