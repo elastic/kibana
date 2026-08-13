bf3dc9153c39fb293a3082f6842c067f8348ff20
Yngrid Coello <yngrid.coello@elastic.co>
2026-08-13 09:45:40 +0200

    Add connector events bridge plugin

---
+++ x-pack/platform/plugins/shared/connector_events_bridge/server/plugin.ts
@@ -0,0 +1,37 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { CoreSetup, Logger, Plugin, PluginInitializerContext } from '@kbn/core/server';
+import type { PluginSetupContract as ActionsPluginSetupContract } from '@kbn/actions-plugin/server';
+import type { WorkflowsExtensionsServerPluginStart } from '@kbn/workflows-extensions/server';
+
+export interface ConnectorEventsBridgeSetupDeps {
+  actions: ActionsPluginSetupContract;
+}
+
+export interface ConnectorEventsBridgeStartDeps {
+  workflowsExtensions?: WorkflowsExtensionsServerPluginStart;
+}
+
+export class ConnectorEventsBridgePlugin
+  implements Plugin<{}, {}, ConnectorEventsBridgeSetupDeps, ConnectorEventsBridgeStartDeps>
+{
+  private readonly logger: Logger;
+
+  constructor(initializerContext: PluginInitializerContext) {
+    this.logger = initializerContext.logger.get();
+  }
+
+  setup(_core: CoreSetup<ConnectorEventsBridgeStartDeps>, _deps: ConnectorEventsBridgeSetupDeps) {
+    this.logger.debug('connectorEventsBridge loaded');
+    return {};
+  }
+
+  start() {
+    return {};
+  }
+}
