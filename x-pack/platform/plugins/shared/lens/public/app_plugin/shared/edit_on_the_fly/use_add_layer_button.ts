481e0c7cad08222ccec9ff6ae2fe6d0a2098b6fb
Walter M. Rafelsberger <walter.rafelsberger@elastic.co>
2026-08-14 14:00:23 +0200

    add layers to selected datasource

--- x-pack/platform/plugins/shared/lens/public/app_plugin/shared/edit_on_the_fly/use_add_layer_button.ts
+++ x-pack/platform/plugins/shared/lens/public/app_plugin/shared/edit_on_the_fly/use_add_layer_button.ts
@@ -42,6 +42,14 @@ export const getDatasourceIdForNewLayer = (
     ? LENS_DATASOURCE_ID.FORM_BASED
     : selectedLayerDatasourceId;
 
+export const getDatasourceIdForNewLayer = (
+  layerType: Parameters<AddLayerFunction>[0],
+  selectedLayerDatasourceId?: LensDatasourceId
+): LensDatasourceId | undefined =>
+  layerType === LENS_LAYER_TYPES.REFERENCELINE
+    ? LENS_DATASOURCE_ID.FORM_BASED
+    : selectedLayerDatasourceId;
+
 export const useAddLayerButton = (
   framePublicAPI: FramePublicAPI,
   coreStart: CoreStart,
