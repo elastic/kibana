cb20116ea0965a6d9101a9c48886225df2f56d44
Ryan Keairns <ryan.keairns@elastic.co>
2026-08-14 16:16:00 -0700

    Change gutter size to match

--- x-pack/platform/plugins/shared/index_management/public/application/sections/home/index_list/index_table/index_table.tsx
+++ x-pack/platform/plugins/shared/index_management/public/application/sections/home/index_list/index_table/index_table.tsx
@@ -805,7 +805,7 @@ export class IndexTable extends Component<IndexTableProps, IndexTableState> {
 
               {this.renderBanners(extensionsService)}
 
-              <EuiFlexGroup gutterSize="m" alignItems="center">
+              <EuiFlexGroup gutterSize="s" alignItems="center">
                 {atLeastOneItemSelected ? (
                   <EuiFlexItem grow={false}>
                     <Route
