e1952ebe4ffb60b01697d2b208985a3133aba820
Larry Gregory <larry.gregory@elastic.co>
2026-08-14 11:17:27 -0400

    Scaffolding to support dependency quarantine

---
+++ packages/kbn-eslint-plugin-imports/src/rules/no_quarantined_imports.ts
@@ -0,0 +1,56 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import Path from 'path';
+import type { Rule } from 'eslint';
+import { REPO_ROOT } from '@kbn/repo-info';
+import {
+  formatQuarantineMessage,
+  isPathAllowed,
+  loadQuarantineConfigs,
+  matchQuarantinedPackage,
+} from '@kbn/dependency-quarantine';
+import { visitAllImportStatements } from '../helpers/visit_all_import_statements';
+import { report } from '../helpers/report';
+import { getSourcePath } from '../helpers/source';
+
+export const NoQuarantinedImportsRule: Rule.RuleModule = {
+  meta: {
+    type: 'problem',
+    docs: {
+      description: 'Disallow imports of quarantined dependencies outside their allowlisted files.',
+    },
+  },
+
+  create(context) {
+    const configs = loadQuarantineConfigs();
+    const filename = getSourcePath(context);
+    const repoRelPath = Path.relative(REPO_ROOT, filename).split(Path.sep).join('/');
+
+    return visitAllImportStatements((req, { node }) => {
+      if (!req) {
+        return;
+      }
+
+      const config = matchQuarantinedPackage(req, configs);
+      if (!config) {
+        return;
+      }
+
+      if (isPathAllowed(repoRelPath, config.allowed)) {
+        return;
+      }
+
+      report(context, {
+        node,
+        message: formatQuarantineMessage(config),
+      });
+    });
+  },
+};
