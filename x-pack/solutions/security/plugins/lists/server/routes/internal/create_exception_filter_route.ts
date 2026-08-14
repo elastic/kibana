62783ff4feee5704888403982f9713fab7291232
Nikita Khristinin <nkhristinin@gmail.com>
2026-08-14 10:14:20 +0200

    fix lint

--- x-pack/solutions/security/plugins/lists/server/routes/internal/create_exception_filter_route.ts
+++ x-pack/solutions/security/plugins/lists/server/routes/internal/create_exception_filter_route.ts
@@ -13,7 +13,6 @@ import type {
 } from '@kbn/securitysolution-io-ts-list-types';
 import { INTERNAL_EXCEPTION_FILTER } from '@kbn/securitysolution-list-constants';
 import { APP_ID } from '@kbn/security-solution-features/constants';
-
 import { buildExceptionFilter } from '../../services/exception_lists/build_exception_filter';
 
 const MAX_EXCEPTION_LIST_IDS = 10_000;
