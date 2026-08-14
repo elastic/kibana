cddecb1149210baaa28ff1a71574db74225d0062
jillguyonnet <jill.guyonnet@elastic.co>
2026-08-14 16:31:44 +0200

    Extract downloadSourceSchemaV2

--- x-pack/platform/plugins/shared/fleet/server/saved_objects/index.ts
+++ x-pack/platform/plugins/shared/fleet/server/saved_objects/index.ts
@@ -140,6 +140,23 @@ import { bumpProfilingSymbolizerPolicy } from './model_versions/bump_profiling_s
  * Please update typings in `/common/types` as well as
  * schemas in `/server/types` if mappings are updated.
  */
+
+const _nullableString = schema.oneOf([schema.literal(null), schema.string()]);
+const downloadSourceSchemaV2 = schema.object(
+  {
+    source_id: schema.maybe(schema.string()),
+    name: schema.maybe(schema.string()),
+    is_default: schema.maybe(schema.boolean()),
+    is_preconfigured: schema.maybe(schema.boolean()),
+    host: schema.maybe(schema.string()),
+    proxy_id: schema.maybe(_nullableString),
+    ssl: schema.maybe(_nullableString),
+    auth: schema.maybe(_nullableString),
+    secrets: schema.maybe(schema.object({}, { unknowns: 'allow' })),
+  },
+  { unknowns: 'ignore' }
+);
+
 export const getSavedObjectTypes = (
   options = { useSpaceAwareness: false }
 ): { [key: string]: SavedObjectsType } => {
@@ -1756,27 +1773,10 @@ export const getSavedObjectTypes = (
               addedMappings: {},
             },
           ],
-          schemas: (() => {
-            const nullableString = schema.oneOf([schema.literal(null), schema.string()]);
-            const downloadSourceSchemaV2 = schema.object(
-              {
-                source_id: schema.maybe(schema.string()),
-                name: schema.maybe(schema.string()),
-                is_default: schema.maybe(schema.boolean()),
-                is_preconfigured: schema.maybe(schema.boolean()),
-                host: schema.maybe(schema.string()),
-                proxy_id: schema.maybe(nullableString),
-                ssl: schema.maybe(nullableString),
-                auth: schema.maybe(nullableString),
-                secrets: schema.maybe(schema.object({}, { unknowns: 'allow' })),
-              },
-              { unknowns: 'ignore' }
-            );
-            return {
-              forwardCompatibility: downloadSourceSchemaV2,
-              create: downloadSourceSchemaV2,
-            };
-          })(),
+          schemas: {
+            forwardCompatibility: downloadSourceSchemaV2,
+            create: downloadSourceSchemaV2,
+          },
         },
       },
     },
