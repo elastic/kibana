ff10a4440f387eef744d9ce255199ea5dcfde856
Walter M. Rafelsberger <walter.rafelsberger@elastic.co>
2026-08-17 16:21:23 +0200

    replace euicallout with kbnwarningcallout in esql modal

--- x-pack/platform/plugins/shared/lens/public/app_plugin/shared/edit_on_the_fly/convert_to_esql_modal.tsx
+++ x-pack/platform/plugins/shared/lens/public/app_plugin/shared/edit_on_the_fly/convert_to_esql_modal.tsx
@@ -15,7 +15,6 @@ import type {
 import {
   EuiBasicTable,
   EuiButtonIcon,
-  EuiCallOut,
   EuiCodeBlock,
   EuiConfirmModal,
   EuiFlexGroup,
@@ -30,6 +29,7 @@ import {
   useEuiTheme,
 } from '@elastic/eui';
 import { i18n } from '@kbn/i18n';
+import { KbnWarningCallout } from '@kbn/ui-callout';
 import { esql } from '@elastic/esql';
 
 import { layerTypes } from '../../..';
@@ -255,9 +255,7 @@ export const ConvertToEsqlModal: React.FunctionComponent<{
         </EuiLink>
       </p>
 
-      <EuiCallOut
-        color="warning"
-        iconType="warning"
+      <KbnWarningCallout
         size="s"
         title={i18n.translate('xpack.lens.config.queryModeWarningDescription', {
           defaultMessage: `Once you save the chart after switching to query mode, you can't switch back.`,
