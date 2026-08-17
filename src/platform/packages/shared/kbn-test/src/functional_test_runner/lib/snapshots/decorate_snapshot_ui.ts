3e3309a7395c673e990434da73e1350b422c5994
Tyler Smalley <tyler.smalley@elastic.co>
2026-08-17 10:26:19 -0700

    [Jest] Fix SWC test regressions

--- src/platform/packages/shared/kbn-test/src/functional_test_runner/lib/snapshots/decorate_snapshot_ui.ts
+++ src/platform/packages/shared/kbn-test/src/functional_test_runner/lib/snapshots/decorate_snapshot_ui.ts
@@ -26,6 +26,7 @@ type SnapshotUpdateState = 'all' | 'new' | 'none';
 interface SnapshotContext {
   snapshotState: ISnapshotState;
   currentTestName: string;
+  error: Error;
 }
 
 const globalState: {
@@ -197,6 +198,7 @@ export function expectSnapshot(received: any) {
   const context: SnapshotContext = {
     snapshotState,
     currentTestName: getTestTitle(test),
+    error: new Error(),
   };
 
   return {
