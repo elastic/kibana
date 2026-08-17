51ce6e9966152b22f5a03f6bdf16dce62ea37aea
Anton Dosov <anton.dosov@elastic.co>
2026-08-17 16:14:31 +0200

    Restore APP_FIXED_VIEWPORT_ID re-export from app_fixed_viewport

--- src/core/packages/rendering/browser/src/index.ts
+++ src/core/packages/rendering/browser/src/index.ts
@@ -7,6 +7,8 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export { APP_FIXED_VIEWPORT_ID } from '@kbn/core-chrome-layout';
-export { useAppFixedViewport } from '@kbn/core-chrome-layout/app_fixed_viewport';
+export {
+  APP_FIXED_VIEWPORT_ID,
+  useAppFixedViewport,
+} from '@kbn/core-chrome-layout/app_fixed_viewport';
 export type { RenderingService } from './rendering_service';
