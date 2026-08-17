6097d6d0d0ed64d43c0f3155f8e737d53923435b
heenawter <hannah.wright@elastic.co>
2026-08-17 11:09:26 -0600

    Fix missing icons

--- src/platform/plugins/shared/dashboard/public/dashboard_actions/copy_to_dashboard_action.tsx
+++ src/platform/plugins/shared/dashboard/public/dashboard_actions/copy_to_dashboard_action.tsx
@@ -72,7 +72,7 @@ export class CopyToDashboardAction implements Action<EmbeddableApiContext> {
 
   public getIconType({ embeddable }: EmbeddableApiContext) {
     if (!apiIsCompatible(embeddable)) throw new IncompatibleActionError();
-    return 'exit';
+    return 'copy';
   }
 
   public async isCompatible({ embeddable }: EmbeddableApiContext) {
