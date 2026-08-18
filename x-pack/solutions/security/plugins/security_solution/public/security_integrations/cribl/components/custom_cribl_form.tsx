35b26c654896f9c104462b19b3e94c571aa87f34
Bharat Pasupula <bharat.pasupula@elastic.co>
2026-08-18 12:17:05 +0200

    fix issues

--- x-pack/solutions/security/plugins/security_solution/public/security_integrations/cribl/components/custom_cribl_form.tsx
+++ x-pack/solutions/security/plugins/security_solution/public/security_integrations/cribl/components/custom_cribl_form.tsx
@@ -9,6 +9,7 @@ import { useKibana } from '@kbn/kibana-react-plugin/public';
 import {
   EuiButton,
   EuiButtonIcon,
+  EuiCallOut,
   EuiComboBox,
   EuiFieldText,
   EuiFlexGroup,
@@ -25,7 +26,6 @@ import type {
   NewPackagePolicy,
   PackagePolicyReplaceDefineStepExtensionComponentProps,
 } from '@kbn/fleet-plugin/public/types';
-import { KbnInfoCallout } from '@kbn/ui-callout';
 import { getFleetManagedIndexTemplates } from '../api/api';
 import type { RouteEntry } from '../../../../common/security_integrations/cribl/types';
 import {
@@ -297,7 +297,7 @@ export const CustomCriblForm = memo<PackagePolicyReplaceDefineStepExtensionCompo
       <>
         {missingReqPermissions && (
           <>
-            <KbnInfoCallout
+            <EuiCallOut
               announceOnMount={false}
               size="s"
               title={i18n.translate(
@@ -306,15 +306,15 @@ export const CustomCriblForm = memo<PackagePolicyReplaceDefineStepExtensionCompo
                   defaultMessage: 'Be sure you have the necessary privileges',
                 }
               )}
-              text={
-                <p>
-                  <FormattedMessage
-                    id="xpack.securitySolution.securityIntegration.cribl.missingPermissionsCalloutDescription"
-                    defaultMessage="To configure this integration, you must have `manage_index_templates` privileges and `manage_pipeline` or `manage_ingest_pipelines` privileges."
-                  />
-                </p>
-              }
-            />
+              iconType="question"
+            >
+              <p>
+                <FormattedMessage
+                  id="xpack.securitySolution.securityIntegration.cribl.missingPermissionsCalloutDescription"
+                  defaultMessage="To configure this integration, you must have `manage_index_templates` privileges and `manage_pipeline` or `manage_ingest_pipelines` privileges."
+                />
+              </p>
+            </EuiCallOut>
             <EuiSpacer size="l" />
           </>
         )}
