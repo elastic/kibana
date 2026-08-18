87466378ab6c062c1a974179f5e6c0a4a44f7434
Davis McPhee <davis.mcphee@elastic.co>
2026-08-17 22:08:46 -0300

    Update leftover arrow icons to chevron icons

--- x-pack/platform/plugins/shared/alerting_v2/public/pages/rules_list_page/rules_list_header.tsx
+++ x-pack/platform/plugins/shared/alerting_v2/public/pages/rules_list_page/rules_list_header.tsx
@@ -40,7 +40,7 @@ const getRulesListMenu = ({
     testId: 'createRuleButton',
     popoverTestId: 'createRulePopoverPanel',
     splitButtonProps: {
-      iconType: 'arrowDown',
+      iconType: 'chevronSingleDown',
       secondaryButtonAriaLabel: i18n.translate('xpack.alertingV2.rulesList.createRuleMoreOptions', {
         defaultMessage: 'More create options',
       }),
