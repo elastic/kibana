9807046b3031327d23eac362cd99c75491c72447
Patrick Mueller <patrick.mueller@elastic.co>
2026-08-19 08:48:37 -0400

    [ResponseOps][Task Manager] allow updates to task documents while the task is running - take 2 (#281668)
    resolves https://github.com/elastic/kibana/issues/274007
    
    ## Summary
    
    When a task comples and updates the task document, a conflict can occur
    if the task document has been updated since it started running.
    
    This PR resolves the conflict by applying the task-run updates to the
    document against the current version of the document, with some
    overrides for scheduling and checks to see if the task has been
    cancelled and re-started.
    
    (cherry picked from commit 0eb7433a609619e19fe1e6b9e9b846ec38eb305c)
    
    # Conflicts:
    #	x-pack/platform/plugins/shared/task_manager/server/task_running/task_runner.test.ts
    #	x-pack/platform/plugins/shared/task_manager/server/task_running/task_runner.ts

--- x-pack/platform/plugins/shared/task_manager/server/task_running/task_runner.ts
+++ x-pack/platform/plugins/shared/task_manager/server/task_running/task_runner.ts
@@ -60,7 +60,8 @@ import type {
 import { isFailedRunResult, TaskStatus, TaskCost, getTaskCostFromInstance } from '../task';
 import type { TaskTypeDictionary } from '../task_type_dictionary';
 import { isUnrecoverableError, isUserError, type DecoratedError } from './errors';
-import { CLAIM_STRATEGY_MGET, type TaskManagerConfig } from '../config';
+import { resolveTaskDocumentConflicts } from './resolve_so_conflicts';
+import { type TaskManagerConfig, CLAIM_STRATEGY_MGET } from '../config';
 import type { ApiKeyStrategy } from '../api_key_strategy';
 import { TaskValidator } from '../task_validator';
 import { getRetryAt, getRetryDate, getTimeout } from '../lib/get_retry_at';
@@ -115,6 +116,7 @@ export interface Updatable {
     options: { validate: boolean; doc: ConcreteTaskInstance }
   ): Promise<ConcreteTaskInstance>;
   remove(id: string): Promise<void>;
+  get(id: string): Promise<ConcreteTaskInstance>;
 }
 
 type Opts = {
@@ -859,6 +861,7 @@ export class TaskManagerRunner implements TaskRunner {
       const { shouldValidate = true } = unwrap(result);
 
       let shouldUpdateTask: boolean = false;
+      const originalTask = this.instance.task;
       let partialTask: PartialConcreteTaskInstance = {
         id: this.instance.task.id,
         version: this.instance.task.version,
@@ -908,12 +911,38 @@ export class TaskManagerRunner implements TaskRunner {
       }
 
       if (shouldUpdateTask) {
-        this.instance = asRan(
-          await this.bufferedTaskStore.partialUpdate(partialTask, {
-            validate: shouldValidate,
-            doc: this.instance.task,
-          })
-        );
+        try {
+          this.instance = asRan(
+            await this.bufferedTaskStore.partialUpdate(partialTask, {
+              validate: shouldValidate,
+              doc: this.instance.task,
+            })
+          );
+        } catch (error) {
+          const isVersionConflict =
+            SavedObjectsErrorHelpers.isConflictError(error) ||
+            error.status === 409 ||
+            error.statusCode === 409 ||
+            error.error?.type === 'version_conflict_engine_exception';
+
+          if ((this.isExpired || this.isCancelled) && isVersionConflict) {
+            const label = `${this.taskType}:${this.instance.task.id}`;
+            this.logger.warn(
+              `Skipping the update of expired/cancelled task ${label} because it was reclaimed by another Kibana while running.`,
+              { tags: [this.id, this.taskType] }
+            );
+          } else if (isVersionConflict) {
+            await resolveTaskDocumentConflicts({
+              taskId: this.id,
+              partialTask,
+              originalTask,
+              bufferedTaskStore: this.bufferedTaskStore,
+              logger: this.logger,
+            });
+          } else {
+            throw error;
+          }
+        }
       }
     }
 
