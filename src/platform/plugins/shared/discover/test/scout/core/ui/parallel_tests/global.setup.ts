740cae2bf6116a81e488a1cf7a760a5ebc111c23
Drew Tate <drew.tate@elastic.co>
2026-08-19 11:04:37 -0600

    move more tests

--- src/platform/plugins/shared/discover/test/scout/core/ui/parallel_tests/global.setup.ts
+++ src/platform/plugins/shared/discover/test/scout/core/ui/parallel_tests/global.setup.ts
@@ -15,23 +15,4 @@ globalSetupHook('Setup Discover core tests data', async ({ esArchiver, log }) =>
     'src/platform/test/functional/fixtures/es_archiver/logstash_functional'
   );
   log.debug('[setup:logstash] logstash_functional ES data ready');
-
-  log.debug(
-    '[setup:index_pattern_without_timefield] loading index_pattern_without_timefield ES data (only if it does not exist)...'
-  );
-  await esArchiver.loadIfNeeded(
-    'src/platform/test/functional/fixtures/es_archiver/index_pattern_without_timefield'
-  );
-  log.debug(
-    '[setup:index_pattern_without_timefield] index_pattern_without_timefield ES data ready'
-  );
-
-  // Long window logstash data for request_counts tests.
-  log.debug(
-    '[setup:long_window_logstash] loading long_window_logstash ES data (only if it does not exist)...'
-  );
-  await esArchiver.loadIfNeeded(
-    'src/platform/test/functional/fixtures/es_archiver/long_window_logstash'
-  );
-  log.debug('[setup:long_window_logstash] long_window_logstash ES data ready');
 });
