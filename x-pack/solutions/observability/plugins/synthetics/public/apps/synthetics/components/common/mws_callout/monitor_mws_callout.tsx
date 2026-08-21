60c41e2fc1936d5792d209b6d620f00cd5389148
Shahzad <shahzad31comp@gmail.com>
2026-08-21 11:33:37 +0200

    [9.4] [Synthetics] Warn when a private-location agent predates Maintenance Window support (#285327)
    Uses a lightweight GET /internal/synthetics/private_locations/outdated_mw_agents
    Fleet version check instead of the 9.6-only agent-stats payload.

--- x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/common/mws_callout/monitor_mws_callout.tsx
+++ x-pack/solutions/observability/plugins/synthetics/public/apps/synthetics/components/common/mws_callout/monitor_mws_callout.tsx
@@ -8,12 +8,15 @@
 import React from 'react';
 import { MwsCalloutContent } from './mws_callout_content';
 import { MwsPendingSyncCallout } from './mws_pending_sync_callout';
+import { MwsAgentVersionCallout } from './mws_agent_version_callout';
 import { useHasPendingMwChanges } from './use_has_pending_mw_changes';
+import { useOutdatedMwAgentLocationIds } from './use_outdated_mw_agent_locations';
 import { ConfigKey } from '../../../../../../common/runtime_types';
 import { useSelectedMonitor } from '../../monitor_details/hooks/use_selected_monitor';
 
 export const MonitorMWsCallout = () => {
   const { monitor } = useSelectedMonitor();
+  const { outdatedLocationIds } = useOutdatedMwAgentLocationIds();
 
   const monitorMWIds = monitor?.[ConfigKey.MAINTENANCE_WINDOWS] ?? [];
   const { activeMWs, hasPendingChanges, syncInterval } = useHasPendingMwChanges(monitorMWIds);
@@ -22,12 +25,22 @@ export const MonitorMWsCallout = () => {
     return null;
   }
 
+  const hasOutdatedAgent =
+    monitorMWIds.length > 0 &&
+    (monitor.locations ?? []).some((location) => outdatedLocationIds.has(location.id));
+
   if (activeMWs.length) {
-    return <MwsCalloutContent activeMWs={activeMWs} />;
+    return <MwsCalloutContent activeMWs={activeMWs} hasOutdatedAgent={hasOutdatedAgent} />;
   }
 
   if (hasPendingChanges) {
-    return <MwsPendingSyncCallout syncInterval={syncInterval} />;
+    return (
+      <MwsPendingSyncCallout syncInterval={syncInterval} hasOutdatedAgent={hasOutdatedAgent} />
+    );
+  }
+
+  if (hasOutdatedAgent) {
+    return <MwsAgentVersionCallout />;
   }
 
   return null;
