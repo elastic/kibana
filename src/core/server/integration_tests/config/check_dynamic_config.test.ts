1378efcd5dbdb96feda9938a1029c8bb17924baf
Tyler Smalley <tyler.smalley@elastic.co>
2026-08-21 20:09:25 +0000

    fix dynamic config allowlist for alerting v2

--- src/core/server/integration_tests/config/check_dynamic_config.test.ts
+++ src/core/server/integration_tests/config/check_dynamic_config.test.ts
@@ -140,6 +140,8 @@ if (getFips() === 0) {
         // We need this for enriching our Perf tests with more valuable data regarding the steps of the test
         // Also helpful in Cloud & Serverless testing because we can't control the labels in those offerings
         'telemetry.labels',
+        // Allows Scout tests to switch ES|QL response formats without restarting Kibana
+        'xpack.alerting_v2.esql.responseFormat',
         // This allow to test fleet features flags while in devlopment
         'xpack.fleet.experimentalFeatures',
       ]);
