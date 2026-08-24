32cd7f10f9ae23686e29bd118f513079ff2593ba
Ihor Panasiuk <igorskynet13@gmail.com>
2026-08-24 15:53:32 +0200

    instrument data client with apm

--- src/platform/plugins/shared/workflows_execution_engine/server/repositories/data_access_layer/implementations/create_data_client_bundle.ts
+++ src/platform/plugins/shared/workflows_execution_engine/server/repositories/data_access_layer/implementations/create_data_client_bundle.ts
@@ -7,16 +7,23 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
+import { instrumentAsyncMethods } from '@kbn/apm-utils';
 import { PlainIndexDataClientBundle } from './plain_index/plain_index_data_client_bundle';
 import { createUnsupportedStorageSourceError } from '../lib/unsupported_storage_source';
 import type { CreateDataClientDeps, DataClientBundle } from '../types';
 
 export function createDataClientBundle(deps: CreateDataClientDeps): DataClientBundle {
+  let instance: DataClientBundle;
   switch (deps.source) {
     case 'system_index':
-      return new PlainIndexDataClientBundle(deps);
+      instance = new PlainIndexDataClientBundle(deps);
+      break;
     default: {
       throw createUnsupportedStorageSourceError('DataClient', deps.source);
     }
   }
+
+  instrumentAsyncMethods(`DataClientBundle(${deps.source})`, instance);
+
+  return instance;
 }
