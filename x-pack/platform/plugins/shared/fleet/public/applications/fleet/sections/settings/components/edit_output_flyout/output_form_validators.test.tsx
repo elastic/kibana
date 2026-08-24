e0b5d1b09184a9297f43650f80643bcb855f9724
jillguyonnet <jill.guyonnet@elastic.co>
2026-08-24 13:54:01 +0200

    Fix validation

--- x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/settings/components/edit_output_flyout/output_form_validators.test.tsx
+++ x-pack/platform/plugins/shared/fleet/public/applications/fleet/sections/settings/components/edit_output_flyout/output_form_validators.test.tsx
@@ -311,5 +311,19 @@ describe('Output form validation', () => {
       const res = validateDynamicKafkaTopics(invalidPercentTopic);
       expect(res).toEqual(['Opening brackets should be preceded by a percent sign']);
     });
+    it('should return error when fallback terminator is missing', () => {
+      const res = validateDynamicKafkaTopics([
+        { label: 'field', value: '%{[service.common_name]:agent-monitoring' },
+      ]);
+      expect(res).toEqual([
+        'The topic should have a matching number of opening and closing brackets',
+      ]);
+    });
+    it('should return error when closing bracket appears before opening bracket', () => {
+      const res = validateDynamicKafkaTopics([{ label: 'field', value: ']}%{[field' }]);
+      expect(res).toEqual([
+        'The topic should have a matching number of opening and closing brackets',
+      ]);
+    });
   });
 });
