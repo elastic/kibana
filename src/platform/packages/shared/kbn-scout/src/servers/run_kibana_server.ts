324b31e53dae636a4442f4fea58a3546f65b96d4
David Olaru <dolaru@elastic.co>
2026-08-24 11:30:15 +0100

    Run tests in IPv6-only mode daily
    Adds a new schedule to the batch testing pipeline that runs tests with Kibana running in IPv6-only mode.
    
    To support this change, test server startup logic has been modified to accept a `KIBANA_TEST_IPV6_ONLY` environment variable that will set `server.host: '::1'` in the Kibana server configuration if the value is set to `true`.

--- src/platform/packages/shared/kbn-scout/src/servers/run_kibana_server.ts
+++ src/platform/packages/shared/kbn-scout/src/servers/run_kibana_server.ts
@@ -27,16 +27,20 @@ export async function runKibanaServer(options: {
 }
 
 export function getExtraKbnOpts(installDir: string | undefined, isServerless: boolean) {
-  if (installDir) {
-    return [];
+  const extraOpts: string[] = [];
+
+  if (!installDir) {
+    extraOpts.push(
+      '--dev',
+      '--no-dev-config',
+      '--no-dev-credentials',
+      `--server.versioned.versionResolution=${isServerless ? 'newest' : 'oldest'}`
+    );
   }
 
-  return [
-    '--dev',
-    '--no-dev-config',
-    '--no-dev-credentials',
-    isServerless
-      ? '--server.versioned.versionResolution=newest'
-      : '--server.versioned.versionResolution=oldest',
-  ];
+  if (process.env.KIBANA_TEST_IPV6_ONLY === 'true') {
+    extraOpts.push('--server.host=::1');
+  }
+
+  return extraOpts;
 }
