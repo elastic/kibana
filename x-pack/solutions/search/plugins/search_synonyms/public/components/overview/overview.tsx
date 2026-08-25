c20133fdb85e32319681d9e34057d1124dd0f76e
Michael DeFazio <michael.defazio@elastic.co>
2026-08-25 16:35:04 -0400

    Move empty prompt into own component

--- x-pack/solutions/search/plugins/search_synonyms/public/components/overview/overview.tsx
+++ x-pack/solutions/search/plugins/search_synonyms/public/components/overview/overview.tsx
@@ -8,8 +8,7 @@
 import React, { useMemo, useState } from 'react';
 
 import { KibanaPageTemplate } from '@kbn/shared-ux-page-kibana-template';
-import { EuiFlexGroup, EuiFlexItem, EuiLoadingSpinner, EuiEmptyPrompt, EuiIllustration, EuiButton, EuiLink, EuiTitle } from '@elastic/eui';
-import { highFive } from '@elastic/eui-illustrations';
+import { EuiFlexGroup, EuiFlexItem, EuiLoadingSpinner } from '@elastic/eui';
 import { AppHeader } from '@kbn/app-header';
 import type { AppHeaderMenu } from '@kbn/app-header';
 import { i18n } from '@kbn/i18n';
@@ -22,8 +21,9 @@ import { useFetchSynonymsSets } from '../../hooks/use_fetch_synonyms_sets';
 import { useSynonymsBreadcrumbs } from '../../hooks/use_synonyms_breadcrumbs';
 import { CreateSynonymsSetModal } from '../synonym_sets/create_new_set_modal';
 import { ErrorPrompt } from '../error_prompt/error_prompt';
+import { EmptyPrompt } from '../empty_prompt/empty_prompt';
 import { isPermissionError } from '../../utils/synonyms_utils';
-import { FormattedMessage } from '@kbn/i18n-react';
+
 
 export const SearchSynonymsOverview = () => {
   const {
@@ -108,66 +108,9 @@ export const SearchSynonymsOverview = () => {
           >
             <EuiFlexItem>
 
-              <EuiEmptyPrompt
-                layout="horizontal"
-                color="plain"
-                icon={
-                  <EuiIllustration
-                    type={highFive}
-                    alt=""
-                    style={{ maxInlineSize: 240, marginInline: 'auto' }}
-                  />
-                }
-                title={
-                  <h2 style={{ whiteSpace: 'nowrap' }}>
-                    <FormattedMessage
-                      id="xpack.searchSynonyms.emptyPrompt.title"
-                      defaultMessage="Search with synonyms"
-                    />
-                  </h2>
-                }
-                body={<p>
-                  <FormattedMessage
-                    id="xpack.searchSynonyms.emptyPrompt.subtitle"
-                    defaultMessage="Create and manage Elasticsearch synonym sets and rules, which expand search results by matching different terms that express the same concept."
-                  />
-                </p>}
-                actions={
-                  <EuiButton color="primary" fill iconType="plusCircle" iconSide="left" onClick={() => {
-                    setIsCreateModalVisible(true);
-                  }}>
-
-                    <FormattedMessage
-                      id="xpack.searchSynonyms.emptyPrompt.getStartedButton"
-                      defaultMessage="Create a synonym set"
-                    />
-                  </EuiButton>
-                }
-                footer={
-                  <>
-                    <EuiTitle size="xxs">
-                      <span>
-                        <FormattedMessage
-                          id="xpack.searchSynonyms.emptyPrompt.footer"
-                          defaultMessage="Prefer to use the APIs?"
-                        />
-                      </span>
-                    </EuiTitle>{' '}
-                    <EuiLink
-                      data-test-subj="searchSynonymsEmptyPromptFooterLink"
-                      href={docLinks.synonymsApi}
-                      target="_blank"
-                      external
-                    >
-                      <FormattedMessage
-                        id="xpack.searchSynonyms.emptyPrompt.footerLink"
-                        defaultMessage="View documentation"
-                      />
-                    </EuiLink>
-                  </>
-                }
-
-              />
+              <EmptyPrompt getStartedAction={() => {
+                setIsCreateModalVisible(true);
+              }} />
             </EuiFlexItem>
           </EuiFlexGroup>
         )}
