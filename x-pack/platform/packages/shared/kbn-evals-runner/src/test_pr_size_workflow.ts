fef5cc3a09e28e02f6ac4d79ce226eadbdf6d027
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-08-26 10:45:36 +0000

    Changes from node scripts/check

--- x-pack/platform/packages/shared/kbn-evals-runner/src/test_pr_size_workflow.ts
+++ x-pack/platform/packages/shared/kbn-evals-runner/src/test_pr_size_workflow.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
 // This file exists solely to test the PR size reminder workflow.
 // It will be deleted after the workflow is confirmed to work.
 // Lines below are intentional padding to exceed the 500-line threshold.
