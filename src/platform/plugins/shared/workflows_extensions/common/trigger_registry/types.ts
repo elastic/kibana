68dae299c5b6a3ead7920f13a1c359c336704201
Yngrid Coello <yngrdyn@gmail.com>
2026-08-26 12:45:58 +0200

    Update src/platform/plugins/shared/workflows_extensions/common/trigger_registry/types.ts
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/platform/plugins/shared/workflows_extensions/common/trigger_registry/types.ts
+++ src/platform/plugins/shared/workflows_extensions/common/trigger_registry/types.ts
@@ -7,7 +7,6 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-/*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the "Elastic License
  * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
