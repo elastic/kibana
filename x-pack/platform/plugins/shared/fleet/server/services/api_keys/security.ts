e9a40dfa340c2f1ef751bc2d3b49fee7befd1a9f
Nikshaan <shettynikshaan@gmail.com>
2026-08-26 13:12:12 +0530

    [Fleet] Chunk API key invalidation to avoid ES max_result_window limit

--- x-pack/platform/plugins/shared/fleet/server/services/api_keys/security.ts
+++ x-pack/platform/plugins/shared/fleet/server/services/api_keys/security.ts
@@ -5,21 +5,53 @@
  * 2.0.
  */
 
+import { chunk } from 'lodash';
+import type { InvalidateAPIKeyResult } from '@kbn/core-security-server';
+import { API_KEY_INVALIDATION_BATCH_SIZE } from '../../constants';
 import { appContextService } from '../app_context';
 
-export async function invalidateAPIKeys(ids: string[]) {
+export async function invalidateAPIKeys(ids: string[]): Promise<InvalidateAPIKeyResult | null> {
   const security = appContextService.getSecurity();
   if (!security) {
     throw new Error('Missing security plugin');
   }
 
-  try {
+  if (ids.length === 0) {
+    return {
+      invalidated_api_keys: [],
+      previously_invalidated_api_keys: [],
+      error_count: 0,
+    };
+  }
+
+  const idBatches = chunk(ids, API_KEY_INVALIDATION_BATCH_SIZE);
+  const aggregated: InvalidateAPIKeyResult = {
+    invalidated_api_keys: [],
+    previously_invalidated_api_keys: [],
+    error_count: 0,
+  };
+  const errorDetails: NonNullable<InvalidateAPIKeyResult['error_details']> = [];
+
+  for (const batch of idBatches) {
     const res = await security.authc.apiKeys.invalidateAsInternalUser({
-      ids,
+      ids: batch,
     });
 
-    return res;
-  } catch (err) {
-    throw err;
+    if (res) {
+      aggregated.invalidated_api_keys.push(...(res.invalidated_api_keys ?? []));
+      aggregated.previously_invalidated_api_keys.push(
+        ...(res.previously_invalidated_api_keys ?? [])
+      );
+      aggregated.error_count += res.error_count ?? 0;
+      if (res.error_details && res.error_details.length > 0) {
+        errorDetails.push(...res.error_details);
+      }
+    }
   }
+
+  if (aggregated.error_count > 0 && errorDetails.length > 0) {
+    aggregated.error_details = errorDetails;
+  }
+
+  return aggregated;
 }
