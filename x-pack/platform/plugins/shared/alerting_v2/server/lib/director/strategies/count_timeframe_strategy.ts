9739fab8ea5f2fddc900324a6907868fb32c2a84
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-08-26 18:26:36 +0000

    Changes from node scripts/check

--- x-pack/platform/plugins/shared/alerting_v2/server/lib/director/strategies/count_timeframe_strategy.ts
+++ x-pack/platform/plugins/shared/alerting_v2/server/lib/director/strategies/count_timeframe_strategy.ts
@@ -133,7 +133,10 @@ export class CountTimeframeStrategy extends BasicTransitionStrategy {
       return basicResult;
     }
 
-    if (alertEvent.status === alertEventStatus.no_data && rule.no_data_strategy === noDataStrategy.last_known_status) {
+    if (
+      alertEvent.status === alertEventStatus.no_data &&
+      rule.no_data_strategy === noDataStrategy.last_known_status
+    ) {
       return basicResult.status !== alertEpisodeStatus.inactive
         ? { status: basicResult.status, statusCount: 0 }
         : basicResult;
