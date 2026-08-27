0a9f3f0372477794032584532b577e5704b83778
Andreana Malama <andreana.malama@elastic.co>
2026-08-27 11:55:46 +0300

    Fix the disablement of sort for time based axis in heatmap

--- x-pack/platform/plugins/shared/lens/public/visualizations/heatmap/time_series.ts
+++ x-pack/platform/plugins/shared/lens/public/visualizations/heatmap/time_series.ts
@@ -11,3 +11,10 @@ type OperationLike = Pick<OperationMetadata, 'dataType' | 'scale'>;
 
 export const isTimeSeriesOperation = (operation?: OperationLike | null): boolean =>
   operation?.dataType === 'date' && operation?.scale === 'interval';
+
+/**
+ * Returns true if the axis is time based, either by the operation or the column type (query result type).
+ */
+export function isTimeBasedAxis(operation?: OperationLike | null, actualColumnType?: string) {
+  return actualColumnType === 'date' || isTimeSeriesOperation(operation);
+}
