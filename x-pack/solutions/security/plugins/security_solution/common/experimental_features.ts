6c9f9dab43d2ff7b9d3dc7d55a33ecf6ed486225
Paul Tavares <paul.tavares@elastic.co>
2026-08-27 15:13:24 -0400

    Enable feature flag and add param to OpenAPI docs

--- x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts
+++ x-pack/solutions/security/plugins/security_solution/common/experimental_features.ts
@@ -46,9 +46,9 @@ export const allowedExperimentalValues = Object.freeze({
 
   /**
    * `physical` memory dump type for the Memory Dump response action for Elastic Defend Endpoint
-   * Release: 9.6
+   * Release: 9.6, backported to 9.5.x
    */
-  responseActionsEndpointMemoryDumpRaw: false,
+  responseActionsEndpointMemoryDumpRaw: true,
 
   /**
    * `runscript` response action for Elastic Defend Endpoint
