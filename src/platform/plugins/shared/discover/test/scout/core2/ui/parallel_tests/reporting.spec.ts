d4f72cc2f1dcdfd43c371d70768cb8c3e6563ed5
Panos Van de goor <panaghiotis.vandegoor@elastic.co>
2026-08-31 14:46:14 +0300

    reporting spec

---
+++ src/platform/plugins/shared/discover/test/scout/core2/ui/parallel_tests/reporting.spec.ts
@@ -0,0 +1,85 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { expect } from '@kbn/scout/ui';
+import { spaceTest, tags, testData } from '../fixtures';
+
+spaceTest.describe('Discover CSV export', () => {
+  spaceTest.beforeAll(async ({ discoverScoutSpace }) => {
+    await discoverScoutSpace.setupDiscoverDefaults();
+  });
+
+  spaceTest.beforeEach(async ({ browserAuth, pageObjects }) => {
+    await browserAuth.loginAsPrivilegedUser();
+    await pageObjects.discover.goto({ queryMode: 'classic' });
+    await pageObjects.discover.waitUntilTabIsLoaded();
+  });
+
+  spaceTest.afterAll(async ({ discoverScoutSpace }) => {
+    await discoverScoutSpace.teardownDiscoverDefaults();
+  });
+
+  spaceTest(
+    'exports an unsaved classic session',
+    { tag: tags.deploymentAgnostic },
+    async ({ pageObjects }) => {
+      const download = await pageObjects.discover.exportAsCsv();
+
+      expect(download.suggestedFilename()).toMatch(/\.csv$/);
+    }
+  );
+
+  spaceTest(
+    'exports an ES|QL session',
+    { tag: '@local-stateful-classic' },
+    async ({ pageObjects }) => {
+      await pageObjects.discover.writeAndSubmitEsqlQuery(
+        'from logstash-* | stats count = count(bytes) by geo.dest | sort count desc'
+      );
+
+      const download = await pageObjects.discover.exportAsCsv();
+
+      expect(download.suggestedFilename()).toMatch(/\.csv$/);
+    }
+  );
+
+  spaceTest(
+    'exports a saved Discover session',
+    { tag: tags.deploymentAgnostic },
+    async ({ pageObjects }) => {
+      await pageObjects.discover.loadSavedSearch(testData.SAVED_SEARCH_TITLE);
+
+      const download = await pageObjects.discover.exportAsCsv();
+
+      expect(download.suggestedFilename()).toMatch(/\.csv$/);
+    }
+  );
+
+  spaceTest(
+    'exports a filtered saved Discover session',
+    { tag: tags.deploymentAgnostic },
+    async ({ pageObjects }) => {
+      await pageObjects.discover.loadSavedSearch(testData.SAVED_SEARCH_TITLE);
+      await pageObjects.filterBar.addFilter({
+        field: 'extension',
+        operator: 'is',
+        value: 'png',
+      });
+      await pageObjects.discover.waitUntilSearchingHasFinished();
+
+      expect(await pageObjects.filterBar.hasFilter({ field: 'extension', value: 'png' })).toBe(
+        true
+      );
+
+      const download = await pageObjects.discover.exportAsCsv();
+
+      expect(download.suggestedFilename()).toMatch(/\.csv$/);
+    }
+  );
+});
