684d3422d687e63aff976fea99456910c20767a6
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-09-01 23:32:49 +0000

    Changes from node scripts/check

---
+++ x-pack/platform/packages/shared/custom-content/custom-content-server/index.d.ts
@@ -0,0 +1,3 @@
+export type { CustomContentTemplateResolverDeps } from './src/custom_content_resolver';
+export { createCustomContentTemplateResolver } from './src/custom_content_resolver';
+export { sanitizeCellValue } from './src/sanitize_cell_value';
