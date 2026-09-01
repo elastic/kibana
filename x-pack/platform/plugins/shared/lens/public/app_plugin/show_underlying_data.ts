8179383c7c06372ecd404bc57ccec50b589a653e
Andreana Malama <andreana.malama@elastic.co>
2026-09-01 15:40:10 +0300

    Add activeData parameter to getLayerMetaInfo function for enhanced data handling

--- x-pack/platform/plugins/shared/lens/public/app_plugin/show_underlying_data.ts
+++ x-pack/platform/plugins/shared/lens/public/app_plugin/show_underlying_data.ts
@@ -164,6 +164,7 @@ export function getLayerMetaInfo(
       layerId: dataLayerIds[0],
       state: datasourceState,
       indexPatterns,
+      activeData: activeData?.[dataLayerIds[0]],
     });
   } catch (error) {
     showMemoizedErrorNotification(error);
