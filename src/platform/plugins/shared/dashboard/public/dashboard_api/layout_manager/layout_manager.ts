0cb44b199acc397bc703e831118b29ffeb7614e4
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-09-01 20:19:39 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- src/platform/plugins/shared/dashboard/public/dashboard_api/layout_manager/layout_manager.ts
+++ src/platform/plugins/shared/dashboard/public/dashboard_api/layout_manager/layout_manager.ts
@@ -133,9 +133,9 @@ export function initializeLayoutManager(
   const childrenChanges$ = childrenUnsavedChanges$(children$);
   const stateChangedSubscription = latestChildrenState$.subscribe((childrenState) => {
     for (const { uuid, latestState } of childrenState) {
-    for (const { uuid, latestState } of childrenState) {
-      currentChildState[uuid] = latestState;
-    }
+      for (const { uuid, latestState } of childrenState) {
+        currentChildState[uuid] = latestState;
+      }
       currentChildState[uuid] = latestState;
     }
   });
