ca31a4b7dd03b44072d6f1db28391967d94d0443
Asuka Nakajima <asuka.nakajima@elastic.co>
2026-09-02 05:39:05 +0000

    update advanced policy

--- x-pack/solutions/security/plugins/security_solution/public/management/pages/policy/models/advanced_policy_schema.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/pages/policy/models/advanced_policy_schema.ts
@@ -1807,15 +1807,25 @@ export const AdvancedPolicySchema: AdvancedPolicySchemaType[] = [
     ),
   },
   {
-    key: 'windows.advanced.kernel.dev_drives.harden',
-    first_supported_version: '8.16',
+    key: 'windows.advanced.kernel.enable_sync_driver_loads',
+    first_supported_version: '8.13',
     documentation: i18n.translate(
-      'xpack.securitySolution.endpoint.policy.advanced.windows.advanced.kernel.dev_drives.harden',
+      'xpack.securitySolution.endpoint.policy.advanced.windows.advanced.kernel.enable_sync_driver_loads',
       {
-        defaultMessage: 'Apply malware protection to dev drives. Default: false.',
+        defaultMessage: 'Mitigates vulnerable driver abuse by providing additional safeguards and enrichment for driver loads. Default: true.',
       }
     ),
   },
+  {                                                                                                                                                                                                       
+    key: 'windows.advanced.kernel.dev_drives.harden',                                                                                                                                                     
+    first_supported_version: '8.16',                                                                                                                                                                      
+    documentation: i18n.translate(                                                                                                                                                                        
+      'xpack.securitySolution.endpoint.policy.advanced.windows.advanced.kernel.dev_drives.harden',                                                                                                        
+      {                                                                                                                                                                                                   
+        defaultMessage: 'Apply malware protection to dev drives. Default: false.',                                                                                                                        
+      }                                                                                                                                                                                                   
+    ),                                                                                                                                                                                                    
+  },  
   {
     key: 'windows.advanced.malware.networkshare',
     first_supported_version: '8.9',
