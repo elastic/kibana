2314addf1195176ded8bf749883f454ed41f832a
Nathan L Smith <nathan.smith@elastic.co>
2026-09-03 11:57:46 -0500

    update rangeToApply

--- src/platform/packages/shared/shared-ux/datetime/kbn-date-range-picker/date_range_picker_context.tsx
+++ src/platform/packages/shared/shared-ux/datetime/kbn-date-range-picker/date_range_picker_context.tsx
@@ -302,9 +302,11 @@ export function DateRangePickerProvider({
     (range?: TimeRangeBounds, textOverride?: string) => {
       let rangeToApply: TimeRange;
 
-      if (range ?? textOverride) {
-        const rangeText =
-          textOverride ?? `${range!.start} ${DATE_RANGE_INPUT_DELIMITER} ${range!.end}`;
+      if (textOverride !== undefined) {
+        rangeToApply = textToTimeRange(textOverride, transformOptions);
+        setText(textOverride);
+      } else if (range) {
+        const rangeText = `${range.start} ${DATE_RANGE_INPUT_DELIMITER} ${range.end}`;
         rangeToApply = textToTimeRange(rangeText, transformOptions);
         setText(rangeText);
       } else {
