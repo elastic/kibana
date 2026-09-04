b9bb1d67004b3293ff3f24372254884ba3e0906c
Steph Milovic <stephanie.milovic@elastic.co>
2026-09-04 11:17:00 -0600

    Import Scout tags through @kbn/evals in the enrichment suite
    @kbn/evals re-exports Scout tags so eval suites do not need a direct
    @kbn/scout dependency, and every other security eval suite imports them
    that way. Follow the same convention and drop @kbn/scout from the
    package references.

--- x-pack/solutions/security/packages/kbn-evals-suite-threat-intel-enrichment/evals/threat_intel_enrichment/assess_relevance.spec.ts
+++ x-pack/solutions/security/packages/kbn-evals-suite-threat-intel-enrichment/evals/threat_intel_enrichment/assess_relevance.spec.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { tags } from '@kbn/scout';
+import { tags } from '@kbn/evals';
 import { evaluate } from '../../src/evaluate';
 import { assessRelevanceDataset } from '../../src/datasets/assess_relevance_dataset';
 import {
--- x-pack/solutions/security/packages/kbn-evals-suite-threat-intel-enrichment/evals/threat_intel_enrichment/classify_severity.spec.ts
+++ x-pack/solutions/security/packages/kbn-evals-suite-threat-intel-enrichment/evals/threat_intel_enrichment/classify_severity.spec.ts
@@ -5,7 +5,7 @@
  * 2.0.
  */
 
-import { tags } from '@kbn/scout';
+import { tags } from '@kbn/evals';
 import { evaluate } from '../../src/evaluate';
 import { classifySeverityDataset } from '../../src/datasets/classify_severity_dataset';
 import {
