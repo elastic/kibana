cddeed6e9646a4bff4358c14e5278a2daf99f423
Gergő Ábrahám <gergo.abraham@elastic.co>
2026-09-07 15:25:21 +0200

    refactor: reuse existing `ensureArtifactListExists` for all artifacts

--- x-pack/solutions/security/plugins/security_solution/scripts/endpoint/custom_yara_signatures/index.ts
+++ x-pack/solutions/security/plugins/security_solution/scripts/endpoint/custom_yara_signatures/index.ts
@@ -10,16 +10,11 @@ import { run } from '@kbn/dev-cli-runner';
 import { createFailError } from '@kbn/dev-cli-errors';
 import { KbnClient } from '@kbn/test';
 import pMap from 'p-map';
-import type { CreateExceptionListSchema } from '@kbn/securitysolution-io-ts-list-types';
-import { ExceptionListTypeEnum } from '@kbn/securitysolution-io-ts-list-types';
-import {
-  ENDPOINT_ARTIFACT_LISTS,
-  EXCEPTION_LIST_ITEM_URL,
-  EXCEPTION_LIST_URL,
-} from '@kbn/securitysolution-list-constants';
+import { EXCEPTION_LIST_ITEM_URL } from '@kbn/securitysolution-list-constants';
 import { randomPolicyIdGenerator } from '../common/random_policy_id_generator';
 import { ExceptionsListItemGenerator } from '../../../common/endpoint/data_generators/exceptions_list_item_generator';
 import { isArtifactByPolicy } from '../../../common/endpoint/service/artifacts';
+import { ensureArtifactListExists } from '../common/endpoint_artifact_services';
 
 export const cli = () => {
   run(
@@ -65,7 +60,7 @@ const createCustomYaraSignatures: RunFn = async ({ flags, log }) => {
   const generator = new ExceptionsListItemGenerator();
   const kbn = new KbnClient({ log, url: flags.kibana as string });
 
-  await ensureCreateEndpointCustomYaraSignaturesList(kbn);
+  await ensureArtifactListExists(kbn, 'customYaraSignatures');
 
   const randomPolicyId = await randomPolicyIdGenerator(kbn, log);
 
@@ -90,30 +85,4 @@ const createCustomYaraSignatures: RunFn = async ({ flags, log }) => {
     },
     { concurrency: 10 }
   );
-};
-
-const ensureCreateEndpointCustomYaraSignaturesList = async (kbn: KbnClient) => {
-  const newListDefinition: CreateExceptionListSchema = {
-    description: ENDPOINT_ARTIFACT_LISTS.customYaraSignatures.description,
-    list_id: ENDPOINT_ARTIFACT_LISTS.customYaraSignatures.id,
-    meta: undefined,
-    name: ENDPOINT_ARTIFACT_LISTS.customYaraSignatures.name,
-    os_types: [],
-    tags: [],
-    type: ExceptionListTypeEnum.ENDPOINT_CUSTOM_YARA_SIGNATURES,
-    namespace_type: 'agnostic',
-  };
-
-  await kbn
-    .request({
-      method: 'POST',
-      path: EXCEPTION_LIST_URL,
-      body: newListDefinition,
-    })
-    .catch((e) => {
-      // Ignore if list was already created
-      if (e.status !== 409) {
-        handleThrowHttpError(e);
-      }
-    });
 };
