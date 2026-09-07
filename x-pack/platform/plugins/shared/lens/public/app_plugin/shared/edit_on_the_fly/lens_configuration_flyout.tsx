5e02c3332c0d20d985d5524f3c8ff50ae48b68c2
Walter M. Rafelsberger <walter.rafelsberger@elastic.co>
2026-09-07 17:53:36 +0200

    assign to DatasourceStates

--- x-pack/platform/plugins/shared/lens/public/app_plugin/shared/edit_on_the_fly/lens_configuration_flyout.tsx
+++ x-pack/platform/plugins/shared/lens/public/app_plugin/shared/edit_on_the_fly/lens_configuration_flyout.tsx
@@ -21,7 +21,11 @@ import {
   EuiButtonIcon,
   EuiToolTip,
 } from '@elastic/eui';
-import type { TypedLensSerializedState, LensDatasourceId } from '@kbn/lens-common';
+import type {
+  DatasourceStates,
+  TypedLensSerializedState,
+  LensDatasourceId,
+} from '@kbn/lens-common';
 import { LENS_DATASOURCE_ID, isTextBasedAttributes } from '@kbn/lens-common';
 import { buildExpression } from '../../../editor_frame_service/editor_frame/expression_helpers';
 import type { TextBasedQueryState } from '../../../editor_frame_service/editor_frame/config_panel/types';
@@ -187,7 +191,7 @@ export function LensEditConfigurationFlyout({
           : previousAttrs.state.datasourceStates[previousDatasourceId];
         // restore every datasource state from the previous attributes, not only the
         // active one, so non-active loaded states are not dropped on cancel
-        const allPreviousDatasourceStates = Object.fromEntries(
+        const allPreviousDatasourceStates: DatasourceStates = Object.fromEntries(
           Object.entries(previousAttrs.state.datasourceStates).map(([id, state]) => [
             id,
             {
