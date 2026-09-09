5166ca71d482ee57fe225b6b74095efedc9ba240
adcoelho <antonio.coelho@elastic.co>
2026-09-09 13:41:07 +0200

    Fixed outdated test.

--- x-pack/platform/plugins/shared/alerting_v2/server/lib/dispatcher/steps/fetch_policies_step.ts
+++ x-pack/platform/plugins/shared/alerting_v2/server/lib/dispatcher/steps/fetch_policies_step.ts
@@ -18,7 +18,6 @@ import type {
   ActionPolicyId,
   DispatcherPipelineState,
   DispatcherStep,
-  PolicyMatcherAttributes,
   DispatcherStepOutput,
 } from '../types';
 
@@ -58,10 +57,7 @@ export class FetchPoliciesStep implements DispatcherStep {
         name: doc.attributes.name,
         enabled: doc.attributes.enabled,
         destinations: doc.attributes.destinations ?? [],
-        matcher: (doc.attributes.matcher ?? undefined) as
-          | PolicyMatcherAttributes
-          | null
-          | undefined,
+        matcher: doc.attributes.matcher ?? undefined,
         groupBy: doc.attributes.groupBy ?? [],
         tags: doc.attributes.tags ?? [],
         groupingMode: doc.attributes.groupingMode ?? DEFAULT_GROUPING_MODE,
