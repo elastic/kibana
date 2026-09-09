15d8126f20490de42eb397b354a3d0d63a3ad9d8
Rachel Forshee <rachel.forshee@elastic.co>
2026-09-09 15:49:38 -0700

    ci: Harden the status check spoofer and limit it to an allow-list

--- .buildkite/scripts/steps/spoof_status_checks.sh
+++ .buildkite/scripts/steps/spoof_status_checks.sh
@@ -5,8 +5,18 @@ set -euo pipefail
 # Publishes a "success" GitHub commit status for each newline-delimited context
 # name in $STATUS_CHECKS.
 
+case ${BUILDKITE_PIPELINE_SLUG:-} in
+  kibana-merge-queue) ;;
+
+  *)
+    echo "error: trying to spoof status checks outside of an approved pipeline" >&2
+    exit 1
+    ;;
+esac
+
 while IFS= read -r context; do
-  [[ -z "$context" ]] && continue
+  [[ -z "${context//[[:space:]]/}" ]] && continue # ignore empty lines
+
   gh api "repos/elastic/kibana/statuses/$BUILDKITE_COMMIT" \
     -f state=success \
     -f target_url="$BUILDKITE_BUILD_URL" \
