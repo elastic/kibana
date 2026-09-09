4548d041a8be903ba39ad5339090174f93a9951d
konrad.szwarc <konrad.szwarc@elastic.co>
2026-09-09 17:29:15 -0600

    Limit Defend Workflows flaky runs to kill-process spec

--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/cypress.config.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/cypress.config.ts
@@ -13,5 +13,9 @@ export default defineCypressConfig(
     env: {
       grepTags: '@ess',
     },
+    e2e: {
+      specPattern:
+        'public/management/cypress/e2e/response_actions/response_console_mocked/kill_process.cy.ts',
+    },
   })
 );
--- x-pack/solutions/security/plugins/security_solution/public/management/cypress/cypress_serverless.config.ts
+++ x-pack/solutions/security/plugins/security_solution/public/management/cypress/cypress_serverless.config.ts
@@ -14,7 +14,11 @@ export default defineCypressConfig(
     env: {
       IS_SERVERLESS: true,
 
-      grepTags: '@serverless --@brokenInServerless --@skipInServerless',
+      grepTags: '@serverless --@skipInServerless',
+    },
+    e2e: {
+      specPattern:
+        'public/management/cypress/e2e/response_actions/response_console_mocked/kill_process.cy.ts',
     },
   })
 );
