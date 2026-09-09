e572e49a2438a870019e1f04b84de79723470b0b
Rachel <rachel.forshee@elastic.co>
2026-09-09 22:30:55 +0000

    Revert "Change the return type of SetMap.add (#288166)" (#290142)
    ## Summary
    
    Small change to verify the merge-queue status checks behavior. The
    ruleset has been updated to include all the same status checks as `main`
    (minus CLA and `docs-build-pr`). We're checking that we actually need to
    take any action on the pipeline here.
    
    ## Validation
    
    - `node scripts/type_check --project packages/kbn-set-map/tsconfig.json`
    - `node scripts/jest packages/kbn-set-map/set_map.test.ts`

--- packages/kbn-set-map/set_map.ts
+++ packages/kbn-set-map/set_map.ts
@@ -10,15 +10,12 @@
 export class SetMap<Key, Value> {
   private readonly map = new Map<Key, Set<Value>>();
 
-  add(key: Key, value: Value): boolean {
+  add(key: Key, value: Value) {
     const group = this.map.get(key);
     if (!group) {
       this.map.set(key, new Set([value]));
-      return true;
     } else {
-      const size = group.size;
       group.add(value);
-      return group.size !== size;
     }
   }
 
