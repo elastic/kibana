97e908e302b5d74d01ebe9b0fdeb6c146abafe7f
Nathan Reese <reese.nathan@gmail.com>
2026-09-10 10:29:19 -0600

    fix tests

--- x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_search_context.test.ts
+++ x-pack/platform/plugins/shared/lens/public/react_embeddable/initializers/initialize_search_context.test.ts
@@ -24,20 +24,10 @@ describe('Context API', () => {
       ...internalApi.attributes$.getValue(),
       state: {
         ...internalApi.attributes$.getValue().state,
-        datasourceStates: {
-          textBased: {
-            layers: {
-              layer1: {
-                query: { esql: 'FROM kibana_sample_data_logs | LIMIT 1' },
-                columns: [],
-              },
-            },
-          },
-        },
         filters: [{ meta: { alias: 'test', disabled: false, negate: false, index: 'test' } }],
       },
     });
-    expect(api.query$.getValue()).toEqual({ esql: 'FROM kibana_sample_data_logs | LIMIT 1' });
+    expect(api.query$.getValue()).toEqual(internalApi.attributes$.getValue().state.query);
     expect(api.filters$.getValue()).toEqual(internalApi.attributes$.getValue().state.filters);
 
     cleanup();
@@ -80,16 +70,7 @@ describe('Context API', () => {
         ...internalApi.attributes$.getValue(),
         state: {
           ...internalApi.attributes$.getValue().state,
-          datasourceStates: {
-            textBased: {
-              layers: {
-                layer1: {
-                  query: { esql: 'FROM kibana_sample_data_logs | LIMIT 1' },
-                  columns: [],
-                },
-              },
-            },
-          },
+          query: { query: 'new-query', language: 'kuery' },
         },
       });
 
