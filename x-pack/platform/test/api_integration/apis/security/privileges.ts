3b597ef3229e160fe638e15670500e37af003ecf
Sergi Massaneda <sergi.massaneda@elastic.co>
2026-09-10 13:56:33 +0200

    configs and tests for plugin disabledby default

--- x-pack/platform/test/api_integration/apis/security/privileges.ts
+++ x-pack/platform/test/api_integration/apis/security/privileges.ts
@@ -100,7 +100,6 @@ export default function ({ getService }: FtrProviderContext) {
       ],
       agentBuilderSml: ['all', 'read', 'minimal_all', 'minimal_read'],
       contextEngine: ['all', 'read', 'minimal_all', 'minimal_read'],
-      agenticInvestigations: ['all', 'read', 'minimal_all', 'minimal_read'],
       automatic_import: ['all', 'read', 'minimal_all', 'minimal_read'],
       slo: ['all', 'read', 'minimal_all', 'minimal_read'],
       searchPlayground: ['all', 'read', 'minimal_all', 'minimal_read'],
