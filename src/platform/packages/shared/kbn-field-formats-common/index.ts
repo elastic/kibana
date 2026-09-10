52b6036ac4c51fa1ffe228331d40f1e38a143134
Nathan Reese <reese.nathan@gmail.com>
2026-09-10 15:21:26 -0600

    export function

--- src/platform/packages/shared/kbn-field-formats-common/index.ts
+++ src/platform/packages/shared/kbn-field-formats-common/index.ts
@@ -7,4 +7,10 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-export { EMPTY_LABEL, NULL_LABEL, NAN_LABEL, MISSING_TOKEN } from './constants';
+export {
+  EMPTY_LABEL,
+  NULL_LABEL,
+  NAN_LABEL,
+  MISSING_TOKEN,
+  getMissingValueLabel,
+} from './constants';
