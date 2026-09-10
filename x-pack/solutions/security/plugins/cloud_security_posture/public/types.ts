532b942b85ceae810ce2128d31f031242532e24a
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-09-10 15:42:55 +0000

    Changes from node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/security/plugins/cloud_security_posture/public/types.ts
+++ x-pack/solutions/security/plugins/cloud_security_posture/public/types.ts
@@ -83,7 +83,6 @@ export interface CspClientPluginSetupDeps {
   usageCollection?: UsageCollectionSetup;
 }
 
-
 /**
  * Methods exposed from the security solution to the cloud security posture application.
  */
