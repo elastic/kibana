847733358a1f5143720fded1d73f67f821c3a6a1
Nathan Reese <reese.nathan@gmail.com>
2026-09-10 14:16:54 -0600

    [filters] fix filter pill displaying 'filter value is invalid or incomplete' for values that are empty strings

--- src/platform/packages/shared/kbn-field-formats-common/constants.ts
+++ src/platform/packages/shared/kbn-field-formats-common/constants.ts
@@ -22,3 +22,8 @@ export const NULL_LABEL = i18n.translate('fieldFormats.nullLabel', {
 export const NAN_LABEL = 'NaN';
 
 export const MISSING_TOKEN = '__missing__';
+
+export function getMissingValueLabel(val: unknown): string | undefined {
+  if (val === '') return EMPTY_LABEL;
+  if (val == null || val === MISSING_TOKEN) return NULL_LABEL;
+}
--- src/platform/plugins/shared/data/public/query/filter_manager/lib/mappers/map_phrase.ts
+++ src/platform/plugins/shared/data/public/query/filter_manager/lib/mappers/map_phrase.ts
@@ -17,6 +17,7 @@ import {
   isPhraseFilter,
 } from '@kbn/es-query';
 import type { FieldFormat } from '@kbn/field-formats-plugin/common';
+import { getMissingValueLabel } from '@kbn/field-formats-common';
 
 const getScriptedPhraseValue = (filter: PhraseFilter) =>
   get(filter, ['query', 'script', 'script', 'params', 'value']);
@@ -31,7 +32,7 @@ export function getPhraseDisplayValue(
   if (formatter?.convertToText) {
     return formatter.convertToText(updatedValue);
   }
-  return updatedValue === undefined ? '' : `${updatedValue}`;
+  return getMissingValueLabel(updatedValue) ?? `${updatedValue}`;
 }
 
 const getParams = (filter: PhraseFilter) => {
