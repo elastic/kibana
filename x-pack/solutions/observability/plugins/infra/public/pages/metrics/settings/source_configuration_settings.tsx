6d733bf4c67a8770aa49db5f1e4f84ec0fe0de79
Samuel Brito <samuel.brito@elastic.co>
2026-09-10 09:20:36 +0100

    Update x-pack/solutions/observability/plugins/infra/public/pages/metrics/settings/source_configuration_settings.tsx
    Co-authored-by: Justin Kambic <jk@elastic.co>

--- x-pack/solutions/observability/plugins/infra/public/pages/metrics/settings/source_configuration_settings.tsx
+++ x-pack/solutions/observability/plugins/infra/public/pages/metrics/settings/source_configuration_settings.tsx
@@ -120,7 +120,7 @@ export const SourceConfigurationSettings = ({
   const showLoading = isLoading && !source;
 
   return (
-    <PageTemplate data-test-subj="sourceConfigurationContent" paddingSize="none">
+    <PageTemplate data-test-subj="sourceConfigurationContent" pageSectionProps={{ paddingSize: 'none'}}>
       <AppHeader title={settingsTitle} menu={menu} spacing="standard" />
       {flyouts}
       <EuiPageSection restrictWidth paddingSize="l">
