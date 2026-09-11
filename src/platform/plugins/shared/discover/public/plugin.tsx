0aedea9ecb5cea2d53ead15ec6ba7e8666986bc4
Anton Dosov <anton.dosov@elastic.co>
2026-09-11 14:43:15 +0200

    Rename Discover hover list to Recent sessions

--- src/platform/plugins/shared/discover/public/plugin.tsx
+++ src/platform/plugins/shared/discover/public/plugin.tsx
@@ -266,8 +266,8 @@ export class DiscoverPlugin
       lists: [
         {
           id: 'recentlyViewed',
-          title: i18n.translate('discover.navigation.recentlyViewedTitle', {
-            defaultMessage: 'Recently viewed',
+          title: i18n.translate('discover.navigation.recentSessionsTitle', {
+            defaultMessage: 'Recent sessions',
           }),
           items$: getDiscoverRecentlyAccessedService(core.http)
             .get$()
