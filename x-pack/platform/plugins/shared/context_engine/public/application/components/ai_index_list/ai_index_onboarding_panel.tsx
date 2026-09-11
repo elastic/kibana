42a869a4b371daed8708b3a086f79a41eff6f580
boriskirov <boris.kirov@elastic.co>
2026-09-11 17:46:14 +0200

    fix: Restore colorMode from useEuiTheme for onboarding illustration.

--- x-pack/platform/plugins/shared/context_engine/public/application/components/ai_index_list/ai_index_onboarding_panel.tsx
+++ x-pack/platform/plugins/shared/context_engine/public/application/components/ai_index_list/ai_index_onboarding_panel.tsx
@@ -13,6 +13,7 @@ import {
   EuiPanel,
   EuiText,
   EuiTitle,
+  useEuiTheme,
 } from '@elastic/eui';
 import { FormattedMessage } from '@kbn/i18n-react';
 import React from 'react';
@@ -23,7 +24,7 @@ import onboardingIllustrationLight from './assets/ai_index_onboarding_light.svg'
 const ILLUSTRATION_SIZE_PX = 236;
 
 export const AiIndexOnboardingPanel = () => {
-  const isDarkMode = useIsDarkMode();
+  const { colorMode } = useEuiTheme();
   const onboardingIllustration =
     colorMode === COLOR_MODES_STANDARD.dark
       ? onboardingIllustrationDark
