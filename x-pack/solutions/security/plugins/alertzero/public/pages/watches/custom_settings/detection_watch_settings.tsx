6fe721e7b55577367d938b347978e8c82ddbece2
Maxim Kholod <maxim.kholod@elastic.co>
2026-09-14 16:06:23 +0200

    [AlertZero] Let Watch teams add Worker settings without changing common persist.
    Nested settings PATCH plus per-Worker schemas so Detection can own analysis window (days); Watch-level Save/Discard drafts the page, and Rule Tuning YAML consumes the saved window.

---
+++ x-pack/solutions/security/plugins/alertzero/public/pages/watches/custom_settings/detection_watch_settings.tsx
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+import { EuiSpacer, EuiTitle } from '@elastic/eui';
+import {
+  ANALYSIS_WINDOW_DAYS_DEFAULT,
+  SYSTEM_SECURITY_WORKER_DETECTION_RULE_TUNING_ID,
+} from '@kbn/alertzero-common';
+import { AnalysisWindowDaysField } from '../components/analysis_window_days_field';
+import * as i18n from '../settings_translations';
+import type { WatchCustomSettingsComponent } from './types';
+
+export const DetectionWatchSettings: WatchCustomSettingsComponent = ({
+  worker,
+  settings,
+  isDisabled,
+  onSettingsChange,
+}) => {
+  if (worker.id !== SYSTEM_SECURITY_WORKER_DETECTION_RULE_TUNING_ID) {
+    return null;
+  }
+
+  return (
+    <>
+      <EuiSpacer size="m" />
+      <EuiTitle size="xxs">
+        <h3>{i18n.TUNING_THRESHOLDS_TITLE}</h3>
+      </EuiTitle>
+      <EuiSpacer size="s" />
+      <AnalysisWindowDaysField
+        current={settings.analysisWindowDays ?? ANALYSIS_WINDOW_DAYS_DEFAULT}
+        isDisabled={isDisabled}
+        onChange={(analysisWindowDays) => onSettingsChange({ analysisWindowDays })}
+      />
+    </>
+  );
+};
+
+DetectionWatchSettings.coveredFields = {
+  [SYSTEM_SECURITY_WORKER_DETECTION_RULE_TUNING_ID]: ['analysisWindowDays'],
+};
