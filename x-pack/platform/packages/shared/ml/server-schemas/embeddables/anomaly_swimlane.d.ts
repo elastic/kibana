516137a2ac3bb15f2a000b199c9cd6503630db72
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-09-14 03:56:21 +0000

    Changes from node scripts/check

--- x-pack/platform/packages/shared/ml/server-schemas/embeddables/anomaly_swimlane.d.ts
+++ x-pack/platform/packages/shared/ml/server-schemas/embeddables/anomaly_swimlane.d.ts
@@ -1,64 +1,116 @@
-import { z } from '@kbn/zod/v4';
-export declare const anomalySwimLaneOverallSchema: z.ZodObject<{
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import type { z } from '@kbn/zod/v4';
+export declare const anomalySwimLaneOverallSchema: z.ZodObject<
+  {
     description: z.ZodOptional<z.ZodString>;
     hide_title: z.ZodOptional<z.ZodBoolean>;
     title: z.ZodOptional<z.ZodString>;
     hide_border: z.ZodOptional<z.ZodBoolean>;
-    time_range: z.ZodOptional<z.ZodObject<{
-        from: z.ZodString;
-        to: z.ZodString;
-        mode: z.ZodOptional<z.ZodUnion<readonly [z.ZodLiteral<"absolute">, z.ZodLiteral<"relative">]>>;
-    }, z.core.$strict>>;
+    time_range: z.ZodOptional<
+      z.ZodObject<
+        {
+          from: z.ZodString;
+          to: z.ZodString;
+          mode: z.ZodOptional<
+            z.ZodUnion<readonly [z.ZodLiteral<'absolute'>, z.ZodLiteral<'relative'>]>
+          >;
+        },
+        z.core.$strict
+      >
+    >;
     job_ids: z.ZodArray<z.ZodString>;
     per_page: z.ZodOptional<z.ZodNumber>;
     severity_threshold: z.ZodOptional<z.ZodNumber>;
-    swimlane_type: z.ZodLiteral<"overall">;
-}, z.core.$strip>;
-export declare const anomalySwimLaneViewBySchema: z.ZodObject<{
+    swimlane_type: z.ZodLiteral<'overall'>;
+  },
+  z.core.$strip
+>;
+export declare const anomalySwimLaneViewBySchema: z.ZodObject<
+  {
     description: z.ZodOptional<z.ZodString>;
     hide_title: z.ZodOptional<z.ZodBoolean>;
     title: z.ZodOptional<z.ZodString>;
     hide_border: z.ZodOptional<z.ZodBoolean>;
-    time_range: z.ZodOptional<z.ZodObject<{
-        from: z.ZodString;
-        to: z.ZodString;
-        mode: z.ZodOptional<z.ZodUnion<readonly [z.ZodLiteral<"absolute">, z.ZodLiteral<"relative">]>>;
-    }, z.core.$strict>>;
+    time_range: z.ZodOptional<
+      z.ZodObject<
+        {
+          from: z.ZodString;
+          to: z.ZodString;
+          mode: z.ZodOptional<
+            z.ZodUnion<readonly [z.ZodLiteral<'absolute'>, z.ZodLiteral<'relative'>]>
+          >;
+        },
+        z.core.$strict
+      >
+    >;
     job_ids: z.ZodArray<z.ZodString>;
     per_page: z.ZodOptional<z.ZodNumber>;
     severity_threshold: z.ZodOptional<z.ZodNumber>;
-    swimlane_type: z.ZodLiteral<"viewBy">;
+    swimlane_type: z.ZodLiteral<'viewBy'>;
     view_by: z.ZodString;
-}, z.core.$strip>;
-export declare const anomalySwimLaneEmbeddableStateSchema: z.ZodUnion<readonly [z.ZodObject<{
-    description: z.ZodOptional<z.ZodString>;
-    hide_title: z.ZodOptional<z.ZodBoolean>;
-    title: z.ZodOptional<z.ZodString>;
-    hide_border: z.ZodOptional<z.ZodBoolean>;
-    time_range: z.ZodOptional<z.ZodObject<{
-        from: z.ZodString;
-        to: z.ZodString;
-        mode: z.ZodOptional<z.ZodUnion<readonly [z.ZodLiteral<"absolute">, z.ZodLiteral<"relative">]>>;
-    }, z.core.$strict>>;
-    job_ids: z.ZodArray<z.ZodString>;
-    per_page: z.ZodOptional<z.ZodNumber>;
-    severity_threshold: z.ZodOptional<z.ZodNumber>;
-    swimlane_type: z.ZodLiteral<"overall">;
-}, z.core.$strip>, z.ZodObject<{
-    description: z.ZodOptional<z.ZodString>;
-    hide_title: z.ZodOptional<z.ZodBoolean>;
-    title: z.ZodOptional<z.ZodString>;
-    hide_border: z.ZodOptional<z.ZodBoolean>;
-    time_range: z.ZodOptional<z.ZodObject<{
-        from: z.ZodString;
-        to: z.ZodString;
-        mode: z.ZodOptional<z.ZodUnion<readonly [z.ZodLiteral<"absolute">, z.ZodLiteral<"relative">]>>;
-    }, z.core.$strict>>;
-    job_ids: z.ZodArray<z.ZodString>;
-    per_page: z.ZodOptional<z.ZodNumber>;
-    severity_threshold: z.ZodOptional<z.ZodNumber>;
-    swimlane_type: z.ZodLiteral<"viewBy">;
-    view_by: z.ZodString;
-}, z.core.$strip>]>;
+  },
+  z.core.$strip
+>;
+export declare const anomalySwimLaneEmbeddableStateSchema: z.ZodUnion<
+  readonly [
+    z.ZodObject<
+      {
+        description: z.ZodOptional<z.ZodString>;
+        hide_title: z.ZodOptional<z.ZodBoolean>;
+        title: z.ZodOptional<z.ZodString>;
+        hide_border: z.ZodOptional<z.ZodBoolean>;
+        time_range: z.ZodOptional<
+          z.ZodObject<
+            {
+              from: z.ZodString;
+              to: z.ZodString;
+              mode: z.ZodOptional<
+                z.ZodUnion<readonly [z.ZodLiteral<'absolute'>, z.ZodLiteral<'relative'>]>
+              >;
+            },
+            z.core.$strict
+          >
+        >;
+        job_ids: z.ZodArray<z.ZodString>;
+        per_page: z.ZodOptional<z.ZodNumber>;
+        severity_threshold: z.ZodOptional<z.ZodNumber>;
+        swimlane_type: z.ZodLiteral<'overall'>;
+      },
+      z.core.$strip
+    >,
+    z.ZodObject<
+      {
+        description: z.ZodOptional<z.ZodString>;
+        hide_title: z.ZodOptional<z.ZodBoolean>;
+        title: z.ZodOptional<z.ZodString>;
+        hide_border: z.ZodOptional<z.ZodBoolean>;
+        time_range: z.ZodOptional<
+          z.ZodObject<
+            {
+              from: z.ZodString;
+              to: z.ZodString;
+              mode: z.ZodOptional<
+                z.ZodUnion<readonly [z.ZodLiteral<'absolute'>, z.ZodLiteral<'relative'>]>
+              >;
+            },
+            z.core.$strict
+          >
+        >;
+        job_ids: z.ZodArray<z.ZodString>;
+        per_page: z.ZodOptional<z.ZodNumber>;
+        severity_threshold: z.ZodOptional<z.ZodNumber>;
+        swimlane_type: z.ZodLiteral<'viewBy'>;
+        view_by: z.ZodString;
+      },
+      z.core.$strip
+    >
+  ]
+>;
 export type AnomalySwimLaneEmbeddableState = z.output<typeof anomalySwimLaneEmbeddableStateSchema>;
 export type SwimlaneType = AnomalySwimLaneEmbeddableState['swimlane_type'];
