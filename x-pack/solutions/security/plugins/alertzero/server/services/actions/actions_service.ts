810f238260dfed7e5016b6b5bf3747abbbe67da1
Patryk Kopyciński <contact@patrykkopycinski.com>
2026-09-14 16:40:43 +0200

    chore: dedupe license headers in actions routes and service files

--- x-pack/solutions/security/plugins/alertzero/server/services/actions/actions_service.ts
+++ x-pack/solutions/security/plugins/alertzero/server/services/actions/actions_service.ts
@@ -5,21 +5,6 @@
  * 2.0.
  */
 
-/*
- * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
- * or more contributor license agreements. Licensed under the "Elastic License
- * 2.0"; you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { ACTION_WORKFLOW_TAG, actionMetadataSchema } from '@kbn/workflows/managed';
 import type { WorkflowListDto } from '@kbn/workflows';
 import type { Logger } from '@kbn/logging';
