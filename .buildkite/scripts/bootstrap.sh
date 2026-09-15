fae398dc8c7d68a8ee5288897eae513e11e585d7
Alex Szabo <alex.szabo@elastic.co>
2026-09-15 14:13:38 +0200

    remove node_modules erasure, if node_modules is present, just skip the cache activation

--- .buildkite/scripts/bootstrap.sh
+++ .buildkite/scripts/bootstrap.sh
@@ -37,14 +37,9 @@ fi
 # But only for agents not mounting the workspace on a local ssd or in memory
 # It actually ends up being slower to move all of the tiny files between the disks vs extracting archives from the yarn cache
 if [[ "$(pwd)" != *"/local-ssd/"* && "$(pwd)" != "/dev/shm"* ]]; then
-  if [[ -d ~/.cache/kibana/pnpm/node_modules ]]; then
-    if [[ -d ./node_modules ]]; then
-      echo "node_modules already exists, skipping image cache restore"
-      rm -rf ~/.cache/kibana/pnpm/node_modules
-    else
+  if [[ -d ~/.cache/kibana/pnpm/node_modules ]] && [[ ! -d ./node_modules ]]; then
       echo "Using ~/.cache/kibana/pnpm/node_modules as a starting point"
       mv ~/.cache/kibana/pnpm/node_modules ./
-    fi
   fi
   if [[ -d ~/.cache/kibana/pnpm/.pnpm-store ]]; then
     echo "Using ~/.cache/kibana/pnpm/.pnpm-store as a starting point"
