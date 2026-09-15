39af3506eb1cc944666707b8c5fae6ff71795716
boriskirov <boris.kirov@elastic.co>
2026-09-15 07:50:56 +0200

    first commit

--- src/platform/plugins/shared/unified_search/public/dataview_picker/change_dataview.tsx
+++ src/platform/plugins/shared/unified_search/public/dataview_picker/change_dataview.tsx
@@ -33,7 +33,6 @@ import type { DataView } from '@kbn/data-views-plugin/public';
 import type { IUnifiedSearchPluginServices } from '../types';
 import { type DataViewPickerProps } from './data_view_picker';
 import type { DataViewListItemEnhanced } from './dataview_list';
-import adhoc from './assets/adhoc.svg';
 import { changeDataViewStyles } from './change_dataview.styles';
 import { DataViewSelector } from './data_view_selector';
 
@@ -150,7 +149,9 @@ export function ChangeDataView({
           css={{ maxWidth: '100%' }}
         >
           {/* we don't want to display the adHoc icon on text based mode */}
-          {isAdHocSelected && <EuiIcon type={adhoc} color="primary" size="s" aria-hidden={true} />}
+          {isAdHocSelected && (
+            <EuiIcon type="tableTime"  size="m" aria-hidden={true} />
+          )}
           <span className="eui-textTruncate">{trigger.label}</span>
         </EuiFlexGroup>
       </EuiFormControlButton>
