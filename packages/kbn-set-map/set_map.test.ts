73a85bf5f135b5ead7e70bc184fca818da68014d
Rachel Forshee <rachel.forshee@elastic.co>
2026-09-15 11:37:11 -0700

    Remove typed consumer

--- packages/kbn-set-map/set_map.test.ts
+++ packages/kbn-set-map/set_map.test.ts
@@ -19,9 +19,6 @@ describe('SetMap', () => {
     sm.add('foo', 'baz');
     sm.add('baz', 'foo');
 
-    const addResult: void = sm.add('queue', 'value');
-    expect(addResult).toBeUndefined();
-
     expect(sm.get('foo')).toMatchInlineSnapshot(`
       Set {
         "bar",
