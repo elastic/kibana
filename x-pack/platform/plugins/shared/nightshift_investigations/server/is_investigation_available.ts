43d5ceac7484989453bc83c47c30cec45454dca8
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-09-15 13:13:40 +0000

    Changes from node scripts/check

--- x-pack/platform/plugins/shared/nightshift_investigations/server/is_investigation_available.ts
+++ x-pack/platform/plugins/shared/nightshift_investigations/server/is_investigation_available.ts
@@ -39,7 +39,10 @@ export const isInvestigationAvailable = async ({
   workflowsExtensions?: WorkflowsExtensionsServerPluginStart;
   workflowsManagement?: WorkflowsServerPluginSetup;
 }): Promise<boolean> => {
-  if (featureFlags && !(await featureFlags.getBooleanValue(STREAMS_SIGNIFICANT_EVENTS_AVAILABLE_FLAG, false))) {
+  if (
+    featureFlags &&
+    !(await featureFlags.getBooleanValue(STREAMS_SIGNIFICANT_EVENTS_AVAILABLE_FLAG, false))
+  ) {
     return false;
   }
 
