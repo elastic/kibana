e482180dd8c80e9daf4800764c17aa40d53b4f1a
Krzysztof Kowalczyk <krzysztof.kowalczyk@elastic.co>
2026-09-15 22:43:53 +0200

    Use new AppMenu scout helpers

--- x-pack/solutions/observability/plugins/slo/test/scout/ui/fixtures/page_objects/annotations_app.ts
+++ x-pack/solutions/observability/plugins/slo/test/scout/ui/fixtures/page_objects/annotations_app.ts
@@ -4,17 +4,19 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
-import { APP_MENU_TEST_SUBJECTS } from '@kbn/app-header';
-import type { ScoutPage } from '@kbn/scout-oblt';
+import { AppMenu, type ScoutPage } from '@kbn/scout-oblt';
 
 export class AnnotationsApp {
-  constructor(private readonly page: ScoutPage) {}
+  private readonly appMenu: AppMenu;
+
+  constructor(private readonly page: ScoutPage) {
+    this.appMenu = new AppMenu(page);
+  }
 
   async goto() {
     await this.page.gotoApp('slo', {});
-    await this.page.getByRole('link', { name: 'Manage SLOs' }).waitFor({ state: 'visible' });
-    await this.page.getByTestId(APP_MENU_TEST_SUBJECTS.overflowButton).click();
-    await this.page.getByTestId('sloHeaderAnnotationsLink').click();
+    await this.appMenu.revealItem('sloHeaderManageLink');
+    await this.appMenu.clickOverflowItem('sloHeaderAnnotationsLink');
     await this.page.getByTestId('annotationsPage').waitFor({ state: 'visible' });
   }
 }
