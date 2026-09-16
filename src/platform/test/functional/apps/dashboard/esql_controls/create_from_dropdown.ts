f4cd049b5f9986b4c0023743c6dd7c6f3898cf2d
Nathan Reese <reese.nathan@elastic.co>
2026-09-16 16:28:42 -0600

    [controls] use shared component for "Values preview" for data control and variable control (#290657)
    Closes https://github.com/elastic/kibana/issues/274439
    
    PR resolves issue by creating a shared component that is used by both
    control and variable control editor to ensure both components display
    same information. Created new package for shared component.
    
    ### Test instructions
    * Install sample web logs
    * Create dashboard
    * Add variable control with ESQL statement `FROM kibana_sample_data_logs
    | STATS BY machine.os.keyword`.
    * Add control. Click "write a query" and use `FROM
    kibana_sample_data_logs
    | STATS BY machine.os.keyword`
    * Ensure "Values preview" shows filter pills for each
    * Try ESQL parse error, set query to `not valid`. Ensure compile error
    is displayed
    * Try query that does not return any values. Set query to `FROM
    kibana_sample_data_logs | where bytes < -1`
    * Try query that returns multiple columns. Set query to `FROM
    kibana_sample_data_logs
    | STATS BY machine.os.keyword, bytes`
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>
    Co-authored-by: Stratou <stratoula1@gmail.com>

--- src/platform/test/functional/apps/dashboard/esql_controls/create_from_dropdown.ts
+++ src/platform/test/functional/apps/dashboard/esql_controls/create_from_dropdown.ts
@@ -48,7 +48,7 @@ export default function ({ getService, getPageObjects }: FtrProviderContext) {
 
       // run the query
       await testSubjects.click('ESQLEditor-run-query-button');
-      expect(await testSubjects.exists('esqlValuesPreview')).to.be(true);
+      expect(await testSubjects.exists('esqlValuesPreviewStrings')).to.be(true);
       await testSubjects.click('saveEsqlControlsFlyoutButton');
 
       await dashboard.waitForRenderComplete();
