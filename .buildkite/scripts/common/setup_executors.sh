eb4c25e7626c083c5d9584bb3be283aeee4b98b6
Tere <teresa.romero@elastic.co>
2026-09-16 16:16:51 +0200

    Update Docker image references to version lite-pr-3161 in pipeline and scripts

--- .buildkite/scripts/common/setup_executors.sh
+++ .buildkite/scripts/common/setup_executors.sh
@@ -8,7 +8,7 @@ echo '--- Executor Setup'
 # if FLEET_PACKAGE_REGISTRY_DOCKER_IMAGE is set,
 # we should clear previously cached EPR images,
 # (as the package registry image is quite big to keep 2 versions of)
-DEFAULT_FLEET_PACKAGE_REGISTRY_DOCKER_IMAGE='docker.elastic.co/observability-ci/distribution:lite-pr-3159'
+DEFAULT_FLEET_PACKAGE_REGISTRY_DOCKER_IMAGE='docker.elastic.co/observability-ci/distribution:lite-pr-3161'
 
 # Test docker is available
 if [[ `command -v docker` \
