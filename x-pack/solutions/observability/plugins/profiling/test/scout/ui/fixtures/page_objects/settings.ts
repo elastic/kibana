5c8e653f138a8090c298e23cdc42a4446a1232de
Alex Fernandez <fernandez.fernandez@elastic.co>
2026-09-16 17:43:35 +0200

    Improve E2E tests

--- x-pack/solutions/observability/plugins/profiling/test/scout/ui/fixtures/page_objects/settings.ts
+++ x-pack/solutions/observability/plugins/profiling/test/scout/ui/fixtures/page_objects/settings.ts
@@ -16,6 +16,7 @@ export class ProfilingSettingsPage {
     await this.page.goto(`${this.kbnUrl.app('profiling')}/settings`);
     await this.page
       .getByTestId(APP_HEADER_TEST_SUBJECTS.title)
+      .getByText('Settings')
       .waitFor({ timeout: EXTENDED_TIMEOUT });
   }
 
