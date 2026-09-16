88ff8ff00ed826cce26333e598788727d8722463
Sean Story <sean.story@elastic.co>
2026-09-16 08:32:12 -0500

    Undoing the exlusion for generate_workflow, in favor of #291238

--- x-pack/platform/plugins/shared/agent_builder_workflows/server/tools/generate_workflow.ts
+++ x-pack/platform/plugins/shared/agent_builder_workflows/server/tools/generate_workflow.ts
@@ -106,7 +106,6 @@ When the workflow is alert-triggered (\`type: alert\`), runtime alert data is ex
 
     `),
     schema: generateWorkflowSchema,
-    excludeFromMcp: true,
     annotations: {
       title: 'Generate Workflow',
       readOnlyHint: true,
