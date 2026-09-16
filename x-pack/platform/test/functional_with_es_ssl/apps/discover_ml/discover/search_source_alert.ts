533377cd7b8233381b3002737e27c6aa43033a35
AlexGPlay <alex.garcia@elastic.co>
2026-09-16 11:09:00 +0200

    Move util file + remove ftr config

--- x-pack/platform/test/functional_with_es_ssl/apps/discover_ml/discover/search_source_alert.ts
+++ x-pack/platform/test/functional_with_es_ssl/apps/discover_ml/discover/search_source_alert.ts
@@ -29,7 +29,7 @@
 import expect from '@kbn/expect';
 import { asyncForEach } from '@kbn/std';
 import type { FtrProviderContext } from '../../../ftr_provider_context';
-import { openDiscoverSearchThresholdRuleFlyout } from '../../../../functional/apps/discover/open_search_threshold_rule_flyout';
+import { openDiscoverSearchThresholdRuleFlyout } from './open_search_threshold_rule_flyout';
 
 export default function ({ getService, getPageObjects }: FtrProviderContext) {
   const log = getService('log');
