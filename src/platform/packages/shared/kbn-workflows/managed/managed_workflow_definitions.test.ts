b655ca5af4c5136cddbeb315f264be289c74105c
Ying Mao <ying.mao@elastic.co>
2026-09-16 12:41:36 -0400

    [AlertZero] Adding managed action workflows for issuing `isolate-host`, `kill-process`, `suspend-process` response actions (#291168)
    ## Summary
    
    Adding AlertZero managed system workflows for running 3 response
    actions: `isolate-host`, `kill-process`, `suspend-process`. The
    workflows dispatch the response action via a `kibana.request` step and
    retrieves the action ID from the API response, then polls for completion
    and returns a final status. Action metadata should conform to the specs
    merged in https://github.com/elastic/kibana/pull/290705. No error
    handling in these workflows as errors should bubble up and be handled in
    a common manner by the AlertZero proposal framework
    (https://github.com/elastic/kibana/pull/291038)
    
    ## To Verify
    
    1. Start ES and Kibana with the following FF:
    
    ```
    xpack.alertzero.enabled: true
    xpack.agenticInvestigations.enabled: true
    uiSettings.overrides:
      workflows:ui:showManagedWorkflows: true
    ```
    
    2. Run setup script `node
    x-pack/solutions/security/plugins/security_solution/scripts/endpoint/run_endpoint_agent.js`.
    This requires you to have Docker and
    [Multipass](https://canonical.com/multipass)
    3. Get IDs of endpoint(s) that were enrolled and the PID or entity_id of
    some processes on the endpoint.
    4. Try running the manual workflows to test the steps:

--- src/platform/packages/shared/kbn-workflows/managed/managed_workflow_definitions.test.ts
+++ src/platform/packages/shared/kbn-workflows/managed/managed_workflow_definitions.test.ts
@@ -12,6 +12,9 @@ import { z } from '@kbn/zod/v4';
 import { managedWorkflowDefinitions } from '.';
 import type { ManagedWorkflowTemplateValuesById } from '.';
 import {
+  ALERTZERO_ACTION_ISOLATE_HOST_WORKFLOW_ID,
+  ALERTZERO_ACTION_KILL_PROCESS_WORKFLOW_ID,
+  ALERTZERO_ACTION_SUSPEND_PROCESS_WORKFLOW_ID,
   ALERTZERO_WORKER_DETECTION_RULE_CREATION_WORKFLOW_ID,
   ALERTZERO_WORKER_DETECTION_RULE_TUNING_WORKFLOW_ID,
   ALERTZERO_WORKER_FLOOR_ALERT_TRIAGE_WORKFLOW_ID,
@@ -23,6 +26,9 @@ import {
   SIGNIFICANT_EVENTS_SCHEDULED_DETECTION_WORKFLOW_ID,
   SIGNIFICANT_EVENTS_SCHEDULED_REVIEW_WORKFLOW_ID,
 } from './definitions';
+import ACTION_ISOLATE_HOST_YAML from './definitions/alertzero/actions/defend/action_isolate_host.yaml';
+import ACTION_KILL_PROCESS_YAML from './definitions/alertzero/actions/defend/action_kill_process.yaml';
+import ACTION_SUSPEND_PROCESS_YAML from './definitions/alertzero/actions/defend/action_suspend_process.yaml';
 import DETECTION_RULE_CREATION_YAML from './definitions/alertzero/detection_rule_creation.yaml';
 import DETECTION_RULE_TUNING_YAML from './definitions/alertzero/detection_rule_tuning.yaml';
 import FLOOR_ALERT_TRIAGE_YAML from './definitions/alertzero/floor_alert_triage.yaml';
@@ -165,6 +171,9 @@ it.each([
     DETECTION_RULE_CREATION_YAML,
     '1:a6804a44',
   ],
+  [ALERTZERO_ACTION_ISOLATE_HOST_WORKFLOW_ID, ACTION_ISOLATE_HOST_YAML, '1:f2aacd90'],
+  [ALERTZERO_ACTION_KILL_PROCESS_WORKFLOW_ID, ACTION_KILL_PROCESS_YAML, '1:1437f9a0'],
+  [ALERTZERO_ACTION_SUSPEND_PROCESS_WORKFLOW_ID, ACTION_SUSPEND_PROCESS_YAML, '1:5f7bc458'],
 ] as const)(
   'requires bumping %s definition.version together with the imported YAML fingerprint',
   (workflowId, importedYaml, expectedFingerprint) => {
