82c7316c05114ef99a272472ae7a41947f0619ee
Michael DeFazio <michael.defazio@elastic.co>
2026-09-17 07:42:36 -0400

    Fix unused variable

--- x-pack/platform/packages/shared/kbn-search-index-documents/components/result/rich_result_header.tsx
+++ x-pack/platform/packages/shared/kbn-search-index-documents/components/result/rich_result_header.tsx
@@ -76,17 +76,17 @@ const CopyButton: React.FC<{ textToCopy: string }> = ({ textToCopy }) => {
       content={
         isTextCopied
           ? i18n.translate(
-              'xpack.searchIndexDocuments.result.header.compactCard.metadata.copiedTextToClipboard',
-              {
-                defaultMessage: 'Copied to clipboard',
-              }
-            )
+            'xpack.searchIndexDocuments.result.header.compactCard.metadata.copiedTextToClipboard',
+            {
+              defaultMessage: 'Copied to clipboard',
+            }
+          )
           : i18n.translate(
-              'xpack.searchIndexDocuments.result.header.compactCard.metadata.copyTextToClipboard',
-              {
-                defaultMessage: 'Copy text to clipboard',
-              }
-            )
+            'xpack.searchIndexDocuments.result.header.compactCard.metadata.copyTextToClipboard',
+            {
+              defaultMessage: 'Copy text to clipboard',
+            }
+          )
       }
       data-test-subj="copyTextToClipboardButtonTooltip"
     >
@@ -115,7 +115,6 @@ const MetadataPopover: React.FC<MetaDataProps> = ({
 }) => {
   const [popoverIsOpen, setPopoverIsOpen] = useState(false);
   const closePopover = () => setPopoverIsOpen(false);
-  const popoverTitleId = useGeneratedHtmlId();
   const metaDataLabel = i18n.translate(
     'xpack.searchIndexDocuments.result.header.metadata.icon.ariaLabel',
     { defaultMessage: 'Metadata for document: {id}', values: { id } }
@@ -181,11 +180,11 @@ const MetadataPopover: React.FC<MetaDataProps> = ({
               /* for serverless search users hasDeleteDocumentsPrivilege flag indicates if user has privilege to delete documents, for stack hasDeleteDocumentsPrivilege would be undefined */
               hasDeleteDocumentsPrivilege === false
                 ? i18n.translate(
-                    'xpack.searchIndexDocuments.result.header.compactCard.metadata.deleteDocumentToolTip',
-                    {
-                      defaultMessage: 'You do not have permision to delete documents',
-                    }
-                  )
+                  'xpack.searchIndexDocuments.result.header.compactCard.metadata.deleteDocumentToolTip',
+                  {
+                    defaultMessage: 'You do not have permision to delete documents',
+                  }
+                )
                 : undefined
             }
             position="bottom"
