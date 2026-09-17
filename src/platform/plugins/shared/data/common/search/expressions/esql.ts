a6ab303f7439739f2a9e97f20269529ecf82d33b
Nathan Reese <reese.nathan@gmail.com>
2026-09-17 17:25:00 -0600

    revert changes to src/platform/plugins/shared/data/common/search/expressions/esql.ts

--- src/platform/plugins/shared/data/common/search/expressions/esql.ts
+++ src/platform/plugins/shared/data/common/search/expressions/esql.ts
@@ -99,8 +99,7 @@ function mapResponseToDatatable(
   const hasEmptyColumns = body.all_columns && body.all_columns?.length > body.columns.length;
   const lookup = new Set(hasEmptyColumns ? body.columns?.map(({ name }) => name) || [] : []);
   const indexPattern = getIndexPatternFromESQLQuery(query);
-  // const approximationApplied = body.approximation_applied;
-  const approximationApplied = true;
+  const approximationApplied = body.approximation_applied;
 
   const appliedTimeRange = input?.timeRange
     ? {
