elastic/kibana

commit 67de5ef0d5cb909649f48d8f5d849edb2f61002c

[Alerting] Allow alert tags to be modified in bulk (#241883)
Closes #240356
Replaces [this PR](https://github.com/elastic/kibana/pull/241051)

## Summary

This pull request introduces a new API route and supporting backend
logic for bulk updating tags on alerts in the rule registry. The main
changes include the addition of a `patchTags` method to the
`AlertsClient`, a new route for bulk patching alert tags, and reusable
scripts for tag updates. These improvements make it possible to
add/remove or replace tags on multiple alerts efficiently, either by IDs
or by query.

### Alerting authorization
The `AlertingAuthorization` class changed to support bulk authorized
multiple rule type IDs and consumers. The `ensureAuthorized` was renamed
to `_ensureAuthorized` and accepts `Array<{ ruleTypeId: string;
consumers: string[] }>;`. The logic is the same as before, but it
constructs all security actions based on the input. This is needed to
avoid having to do one authorization call per `(ruleTypeId, consumer)`
pair. Lastly, a `bulkEnsureAuthorized` is exposed which is a wrapper of
the private `_ensureAuthorized`.

### Alerts client
The code in the `AlertsClient` is pretty outdated. For this reason, I
decided not to use the existing functionality and code from scratch. The
`bulkUpdateTags` is introduced, and it bulk updates the tags of multiple
alerts either by using `alertIds` or by using a KQL `query`. In the
first scenario, an aggregation is made to get the rule type ID and the
consumer of each alert. Then we bulk authorize. If the user has access,
we move forward and update the tags of the alerts. If not, we throw an
error. For the `query` scenario, we apply the authorization filter along
with the `query` to filter out the alerts that the user does not have
access to. Lastly, we audit log only once for the whole bulk operation
and not for each alert found.

### API and Backend Enhancements

* Added a new API route `POST /internal/rac/alerts/tags` for bulk

x-pack/platform/plugins/shared/rule_registry/server/alert_data_client/alerts_client.ts
@@ -4,6 +4,7 @@
  * 2.0; you may not use this file except in compliance with the Elastic License
  * 2.0.
  */
+
 import Boom from '@hapi/boom';
 import { v4 as uuidv4 } from 'uuid';
 import type { estypes } from '@elastic/elasticsearch';
@@ -64,6 +65,7 @@ import type { alertsAggregationsSchema } from '../../common/types';
 import {
   MAX_ALERTS_GROUPING_QUERY_SIZE,
   MAX_ALERTS_PAGES,
+  MAX_ALERT_IDS_PER_REQUEST,
   MAX_PAGINATED_ALERTS,
 } from './constants';
 import { getRuleTypeIdsFilter } from '../lib/get_rule_type_ids_filter';
@@ -71,6 +73,9 @@ import { getConsumersFilter } from '../lib/get_consumers_filter';
 import { mergeUniqueFieldsByName } from '../utils/unique_fields';
 import { getAlertFieldsFromIndexFetcher } from '../utils/get_alert_fields_from_index_fetcher';
 import type { GetAlertFieldsResponseV1 } from '../routes/get_alert_fields';
+import { getBulkUpdateTagsPainlessScript } from '../lib/bulk_update_tags_scripts';
+import type { BulkUpdateApiResponse } from '../lib/transform_update_by_query_response';
+import { transformUpdateByQueryResponse } from '../lib/transform_update_by_query_response';
 
 // TODO: Fix typings https://github.com/elastic/kibana/issues/101776
 type NonNullableProps<Obj extends {}, Props extends keyof Obj> = Omit<Obj, Props> & {
@@ -111,6 +116,26 @@ export interface UpdateOptions<Params extends RuleTypeParams> {
   index: string;
 }
 
+export interface BulkUpdateTagArgs {
+  alertIds?: string[] | null;
+  add?: string[] | null;
+  remove?: string[] | null;
+  index: string;
+  query?: string | null;
+}
+
+interface BulkUpdateTagsByIdsArgs {
+  alertIds: string[];
+  index: string;
+  script: estypes.Script;
+}
+
+interface BulkUpdateTagsByQueryArgs {
+  query: string;
+  index: string;
+  script: estypes.Script;
+}
+
 export interface BulkUpdateOptions<Params extends RuleTypeParams> {
   ids?: string[] | null;
   status: STATUS_VALUES;
@@ -121,6 +146,17 @@ export interface BulkUpdateOptions<Params extends RuleTypeParams> {
 interface MgetAndAuditAlert {
   id: string;
   index: string;
+}
+
+interface EnsureAllAlertsAuthorizedArgs {
+  alerts: MgetAndAuditAlert[];
+  operation: ReadOperations.Find | ReadOperations.Get | WriteOperations.Update;
+}
+
+interface EnsureAllAlertsAuthorizedByAggsArgs {
+  alertIds: string[];
+  index: string;
+  operation: ReadOperations.Find | ReadOperations.Get | WriteOperations.Update;
 }
 
 export interface BulkUpdateCasesOptions {
@@ -162,6 +198,15 @@ interface SearchAlertsParams {
   ruleTypeIds?: string[];
   consumers?: string[];
   runtimeMappings?: MappingRuntimeFields;
+}
+
+interface RuleTypeIdsConsumersAggsResponse {
+  ruleTypeIds: {
+    buckets: Array<{
+      key: string;
+      consumers: { buckets: Array<{ key: string }> };
+    }>;
+  };
 }
 
 /**
@@ -582,13 +627,7 @@ export class AlertsClient {
    * Ensures that the user has access to the alerts
    * for a given operation
    */
-  private async ensureAllAlertsAuthorized({
-    alerts,
-    operation,
-  }: {
-    alerts: MgetAndAuditAlert[];
-    operation: ReadOperations.Find | ReadOperations.Get | WriteOperations.Update;
-  }) {
+  private async ensureAllAlertsAuthorized({ alerts, operation }: EnsureAllAlertsAuthorizedArgs) {
     try {
       const mgetRes = await this.esClient.mget<ParsedTechnicalFields>({
         docs: alerts.map(({ id, index }) => ({ _id: id, _index: index })),
@@ -856,9 +895,283 @@ export class AlertsClient {
         throw err;
       }
     } else {
-      throw Boom.badRequest('no ids or query were provided for updating');
+      throw Boom.badRequest('no alert ids or query were provided for updating');
     }
   }
+
+  public async bulkUpdateTags({
+    alertIds,
+    query,
+    index,
+    add,
+    remove,
+  }: BulkUpdateTagArgs): Promise<BulkUpdateApiResponse> {
+    if (alertIds && alertIds.length > MAX_ALERT_IDS_PER_REQUEST) {
+      throw Boom.badRequest(`Cannot use more than ${MAX_ALERT_IDS_PER_REQUEST} ids`);
+    }
+
+    if (
+      (isEmpty(add) && isEmpty(remove)) ||
+      (add != null && isEmpty(add)) ||
+      (remove != null && isEmpty(remove))
+    ) {
+      throw Boom.badRequest('No tags to add or remove were provided');
+    }
+
+    const script = getBulkUpdateTagsPainlessScript(add, remove);
+
+    if (alertIds && alertIds.length > 0) {
+      const bulkUpdateTagsByIdsResponse = await this.bulkUpdateTagsByIds({
+        alertIds,
+        script,
+        index,
+      });
+
+      return transformUpdateByQueryResponse(bulkUpdateTagsByIdsResponse);
+    }
+
+    if (query) {
+      const bulkUpdateTagsByQueryResponse = await this.bulkUpdateTagsByQuery({
+        query,
+        script,
+        index,
+      });
+
+      return transformUpdateByQueryResponse(bulkUpdateTagsByQueryResponse);
+    }
+
+    throw Boom.badRequest('No alert ids or query were provided for updating');
+  }
+
+  private async bulkUpdateTagsByIds({
+    alertIds,
+    script,
+    index,
+  }: BulkUpdateTagsByIdsArgs): Promise<estypes.UpdateByQueryResponse> {
+    await this.ensureAllAlertsAuthorizedByAggs({
+      alertIds,
+      operation: WriteOperations.Update,
+      index,
+    });
+
+    const bulkUpdateResponse = await this.esClient.updateByQuery({
+      query: { ids: { values: alertIds } },
+      index,
+      script,
+      refresh: true,
+      conflicts: 'proceed',
+      ignore_unavailable: true,
+    });
+
+    return bulkUpdateResponse;
+  }
+
+  private async bulkUpdateTagsByQuery({
+    query,
+    script,
+    index,
+  }: BulkUpdateTagsByQueryArgs): Promise<estypes.UpdateByQueryResponse> {
+    try {
+      const config = getEsQueryConfig();
+      const authzFilter = (await getAuthzFilter(
+        this.authorization,
+        WriteOperations.Update
+      )) as Filter;
+
+      const finalQuery = buildEsQuery(
+        undefined,
+        { query, language: 'kuery' },
+        [authzFilter],
+        config
+      );
+
+      const auditEvent = alertAuditEvent({
+        action: operationAlertAuditActionMap[WriteOperations.Update],
+        ...this.getOutcome(WriteOperations.Update),
+      });
+
+      const finalAuditMessage = `${auditEvent.message}. Bulk updating tags for alerts matching query: ${query}`;
+
+      this.auditLogger?.log({
+        ...auditEvent,
+        message: finalAuditMessage,
+      });
+
+      const bulkUpdateResponse = await this.esClient.updateByQuery({
+        query: finalQuery,
+        index,
+        script,
+        refresh: true,
+        conflicts: 'proceed',
+        ignore_unavailable: true,
+      });
+
+      return bulkUpdateResponse;
+    } catch (error) {
+      this.auditLogger?.log(
+        alertAuditEvent({
+          action: operationAlertAuditActionMap[WriteOperations.Update],
+          error,
+        })
+      );
+
+      throw error;
+    }
+  }
+
+  private async ensureAllAlertsAuthorizedByAggs({
+    alertIds,
+    operation,
+    index,
+  }: EnsureAllAlertsAuthorizedByAggsArgs) {
+    const res = await this.getAuthorizedRuleTypeIdsConsumersPairs({ alertIds, index });
+    const ruleTypeIdConsumersPairs = this.parseRuleTypeIdsConsumersAggsResponse(res);
+
+    this.validateRuleTypeIdConsumersPairs(ruleTypeIdConsumersPairs);
+
+    await this.bulkEnsureAuthorizedAndAuditLog({ alertIds, operation, ruleTypeIdConsumersPairs });
+  }
+
+  private getAuthorizedRuleTypeIdsConsumersPairs = async ({
+    alertIds,
+    index,
+    query,
+  }: {
+    alertIds?: string[];
+    query?: string;
+    index: string;
+  }) => {
+    const spacesFilter = getSpacesFilter(this.spaceId);
+    const filters: estypes.QueryDslBoolQuery['filter'] = [];
+    const finalQuery: {
+      bool: Omit<NonNullable<estypes.QueryDslBoolQuery>, 'filter'> & {
+        filter: estypes.QueryDslQueryContainer[];
+      };
+    } = {
+      bool: { filter: [] },
+    };
+
+    if (spacesFilter != null) {
+      filters.push(spacesFilter);
+    }
+
+    if (alertIds != null) {
+      filters.push({
+        ids: {
+          values: alertIds,
+        },
+      });
+    }
+
+    if (query != null) {
+      const config = getEsQueryConfig();
+      const kqlQueryAsDsl = buildEsQuery(undefined, { query, language: 'kuery' }, [], config);
+
+      finalQuery.bool = { ...finalQuery.bool, ...kqlQueryAsDsl.bool };
+    }
+
+    finalQuery.bool.filter = [...finalQuery.bool.filter, ...filters];
+
+    /**
+     * Here we are using the internal user to perform the aggregation query
+     * to retrieve the ruleTypeId and consumer for the provided alertIds.
+     *
+     * We do this because the scoped user may not have access to all the
+     * alerts being checked, which would lead to incomplete aggregation results
+     * and incorrect authorization decisions.
+     */
+    return this.esClient.search<unknown, RuleTypeIdsConsumersAggsResponse>({
+      index,
+      query: finalQuery,
+      aggs: {
+        ruleTypeIds: {
+          terms: { field: ALERT_RULE_TYPE_ID, size: 100 },
+          aggs: { consumers: { terms: { field: ALERT_RULE_CONSUMER, size: 100 } } },
+        },
+      },
+      // We do not need any hits back. We care about the aggs only.
+      size: 0,
+    });
+  };
+
+  private parseRuleTypeIdsConsumersAggsResponse = (
+    res: estypes.SearchResponse<unknown, RuleTypeIdsConsumersAggsResponse>
+  ) => {
+    const ruleTypeIdConsumersMap: Map<string, string[]> = new Map(
+      res.aggregations?.ruleTypeIds.buckets.map((bucket) => [
+        bucket.key,
+        bucket.consumers.buckets.map((consumerBucket) => consumerBucket.key),
+      ])
+    );
+
+    const ruleTypeIdConsumersPairs = Array.from(ruleTypeIdConsumersMap.entries()).map(
+      ([ruleTypeId, consumers]) => ({
+        ruleTypeId,
+        consumers,
+      })
+    );
+
+    return ruleTypeIdConsumersPairs;
+  };
+
+  /**
+   * Rule type id and consumers pairs should always have at least one entry
+   * In the rare scenario where the alert documents do not have the info needed
+   * to perform authorization, we throw a forbidden error
+   */
+  private validateRuleTypeIdConsumersPairs = (
+    ruleTypeIdConsumersPairs: Array<{ ruleTypeId: string; consumers: string[] }>
+  ) => {
+    if (ruleTypeIdConsumersPairs.length === 0) {
+      throw Boom.notFound('No alerts found');
+    }
+
+    for (const { consumers } of ruleTypeIdConsumersPairs) {
+      if (consumers.length === 0) {
+        throw Boom.forbidden('Not authorized to access any of the requested alerts');
+      }
+    }
+  };
+
+  private bulkEnsureAuthorizedAndAuditLog = async ({
+    ruleTypeIdConsumersPairs,
+    operation,
+    alertIds,
+  }: {
+    ruleTypeIdConsumersPairs: Array<{ ruleTypeId: string; consumers: string[] }>;
+    operation: EnsureAllAlertsAuthorizedByAggsArgs['operation'];
+    alertIds: string[];
+  }) => {
+    try {
+      await this.authorization.bulkEnsureAuthorized({
+        ruleTypeIdConsumersPairs,
+        operation,
+        entity: AlertingAuthorizationEntity.Alert,
+      });
+
+      for (const alertId of alertIds) {
+        this.auditLogger?.log(
+          alertAuditEvent({
+            action: operationAlertAuditActionMap[operation],
+            id: alertId,
+            ...this.getOutcome(operation),
+          })
+        );
+      }
+    } catch (error) {
+      for (const alertId of alertIds) {
+        this.auditLogger?.log(
+          alertAuditEvent({
+            action: operationAlertAuditActionMap[operation],
+            id: alertId,
+            error,
+          })
+        );
+      }
+
+      throw error;
+    }
+  };
 
   /**
    * This function updates the case ids of multiple alerts per index.

commit 76ba49094d8028e8b62559beb87f06bc856ed156

[RuleRegistry] Fix cross-space writes in bulkUpdateTags (#285874)
## Summary

Fixes two cross-space write bugs introduced in #241883 (`bulkUpdateTags`
on `AlertsClient`).

### Bug 1 — ID path: spaces filter missing from the write

The authorization check in `getAuthorizedRuleTypeIdsConsumersPairs`
correctly applied `getSpacesFilter` when aggregating `(ruleTypeId,
consumer)` pairs. But the actual `updateByQuery` used only `{ ids: {
values: alertIds } }` with no spaces filter — meaning alert IDs from
another space could be updated once the auth check passed on in-space
alerts.

**Fix:** AND `getSpacesFilter(this.spaceId)` into the `updateByQuery`
query alongside the ids filter.

### Bug 2 — Query path: no spaces filter at all

`bulkUpdateTagsByQuery` built its ES query from `authzFilter` (rule type
+ consumer) only. There was no `getSpacesFilter` call, so a KQL query
would match and update alerts across all spaces the caller is authorized
for — not just the current space.

**Fix:** Wrap the `buildEsQuery` result in an outer `bool.filter` that
also includes `getSpacesFilter(this.spaceId)`, matching the pattern used
by `buildEsQueryWithAuthz` elsewhere in the file.

### Tests

- Added a test for the ID path asserting that `updateByQuery` is called
with the spaces filter in the query.
- Updated the existing inline snapshot for the query path to reflect the
new outer `bool.filter` wrapper.

## Checklist

- [x] Unit tests pass
- [x] TypeScript type check passes
- [x] ESLint clean

Co-authored-by: Claude Sonnet 4.6 <noreply@anthropic.com>

x-pack/platform/plugins/shared/rule_registry/server/alert_data_client/alerts_client.ts
@@ -990,8 +990,12 @@ export class AlertsClient {
       index,
     });
 
+    const spacesFilter = getSpacesFilter(this.spaceId);
+    const idsQuery: estypes.QueryDslQueryContainer = { ids: { values: alertIds } };
+    const updateQuery = spacesFilter ? { bool: { filter: [idsQuery, spacesFilter] } } : idsQuery;
+
     const bulkUpdateResponse = await this.esClient.updateByQuery({
-      query: { ids: { values: alertIds } },
+      query: updateQuery,
       index,
       script,
       refresh: true,
@@ -1014,12 +1018,14 @@ export class AlertsClient {
         WriteOperations.Update
       )) as Filter;
 
-      const finalQuery = buildEsQuery(
+      const spacesFilter = getSpacesFilter(this.spaceId);
+      const baseQuery = buildEsQuery(
         undefined,
         { query, language: 'kuery' },
         [authzFilter],
         config
       );
+      const finalQuery = spacesFilter ? { bool: { filter: [baseQuery, spacesFilter] } } : baseQuery;
 
       const auditEvent = alertAuditEvent({
         action: operationAlertAuditActionMap[WriteOperations.Update],

x-pack/platform/plugins/shared/rule_registry/server/alert_data_client/alerts_client.test.ts
@@ -643,6 +643,24 @@ describe('AlertsClient', () => {
         expect(esClientMock.updateByQuery).toHaveBeenCalledTimes(1);
       });
 
+      it('should scope the updateByQuery to the current space', async () => {
+        await alertsClient.bulkUpdateTags({
+          alertIds: ['alert-1', 'alert-2'],
+          index: '.alerts-security.alerts-default',
+          add: ['urgent'],
+        });
+
+        const query = esClientMock.updateByQuery.mock.calls[0][0].query;
+        expect(query).toEqual({
+          bool: {
+            filter: [
+              { ids: { values: ['alert-1', 'alert-2'] } },
+              { terms: { 'kibana.space_ids': ['space-1', '*'] } },
+            ],
+          },
+        });
+      });
+
       it('should construct the query and aggs correctly when getting the rule type ids and consumers', async () => {
         await alertsClient.bulkUpdateTags({
           alertIds: ['alert-1', 'alert-2'],
@@ -811,54 +829,68 @@ describe('AlertsClient', () => {
             "bool": Object {
               "filter": Array [
                 Object {
-                  "multi_match": Object {
-                    "lenient": true,
-                    "query": "some-query",
-                    "type": "best_fields",
+                  "bool": Object {
+                    "filter": Array [
+                      Object {
+                        "multi_match": Object {
+                          "lenient": true,
+                          "query": "some-query",
+                          "type": "best_fields",
+                        },
+                      },
+                      Object {
+                        "arguments": Array [
+                          Object {
+                            "arguments": Array [
+                              Object {
+                                "isQuoted": false,
+                                "type": "literal",
+                                "value": "alert.attributes.alertTypeId",
+                              },
+                              Object {
+                                "isQuoted": false,
+                                "type": "literal",
+                                "value": "test-rule-type-1",
+                              },
+                            ],
+                            "function": "is",
+                            "type": "function",
+                          },
+                          Object {
+                            "arguments": Array [
+                              Object {
+                                "isQuoted": false,
+                                "type": "literal",
+                                "value": "alert.attributes.consumer",
+                              },
+                              Object {
+                                "isQuoted": false,
+                                "type": "literal",
+                                "value": "foo",
+                              },
+                            ],
+                            "function": "is",
+                            "type": "function",
+                          },
+                        ],
+                        "function": "and",
+                        "type": "function",
+                      },
+                    ],
+                    "must": Array [],
+                    "must_not": Array [],
+                    "should": Array [],
                   },
                 },
                 Object {
-                  "arguments": Array [
-                    Object {
-                      "arguments": Array [
-                        Object {
-                          "isQuoted": false,
-                          "type": "literal",
-                          "value": "alert.attributes.alertTypeId",
-                        },
-                        Object {
-                          "isQuoted": false,
-                          "type": "literal",
-                          "value": "test-rule-type-1",
-                        },
-                      ],
-                      "function": "is",
-                      "type": "function",
-                    },
-                    Object {
-                      "arguments": Array [
-                        Object {
-                          "isQuoted": false,
-                          "type": "literal",
-                          "value": "alert.attributes.consumer",
-                        },
-                        Object {
-                          "isQuoted": false,
-                          "type": "literal",
-                          "value": "foo",
-                        },
-                      ],
-                      "function": "is",
-                      "type": "function",
-                    },
-                  ],
-                  "function": "and",
-                  "type": "function",
+                  "terms": Object {
+                    "kibana.space_ids": Array [
+                      "space-1",
+                      "*",
+                    ],
+                  },
                 },
               ],
-              "must": Array [],
-              "must_not": Array [],
-              "should": Array [],
             },
           }
         `);

x-pack/platform/test/rule_registry/security_and_spaces/tests/basic/bulk_update_tags.ts
@@ -113,9 +113,17 @@ export default ({ getService }: FtrProviderContext) => {
           expectedStatusCode: 207,
         });
 
+        const kqlQuery = buildEsQuery(undefined, { query, language: 'kuery' }, []);
+        const verifyQuery = spaceId
+          ? {
+              bool: {
+                filter: [kqlQuery, { terms: { 'kibana.space_ids': [spaceId, '*'] } }],
+              },
+            }
+          : kqlQuery;
         const res = await esClient.search<{ 'kibana.alert.workflow_tags': string[] }>({
           index,
-          query: buildEsQuery(undefined, { query, language: 'kuery' }, []),
+          query: verifyQuery,
         });
 
         for (const hit of res.hits.hits) {
@@ -137,9 +145,17 @@ export default ({ getService }: FtrProviderContext) => {
           expectedStatusCode: 207,
         });
 
+        const kqlQuery2 = buildEsQuery(undefined, { query, language: 'kuery' }, []);
+        const verifyQuery2 = spaceId
+          ? {
+              bool: {
+                filter: [kqlQuery2, { terms: { 'kibana.space_ids': [spaceId, '*'] } }],
+              },
+            }
+          : kqlQuery2;
         const res = await esClient.search<{ 'kibana.alert.workflow_tags': string[] }>({
           index,
-          query: buildEsQuery(undefined, { query, language: 'kuery' }, []),
+          query: verifyQuery2,
         });
 
         for (const hit of res.hits.hits) {