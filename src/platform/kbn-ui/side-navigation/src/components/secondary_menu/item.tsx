1786aa82c042cd46613fdeab1df89dc0e6abfe88
Anton Dosov <anton.dosov@elastic.co>
2026-09-18 13:14:02 +0200

    [Sidenav] Drop duplicate More chevron color from CSS
    EuiIcon already gets textDisabled via the color prop.

--- src/platform/kbn-ui/side-navigation/src/components/secondary_menu/item.tsx
+++ src/platform/kbn-ui/side-navigation/src/components/secondary_menu/item.tsx
@@ -98,7 +98,6 @@ export const SecondaryMenuItemComponent = ({
   `;
 
   const submenuIconStyles = css`
-    color: ${euiTheme.colors.textDisabled};
     flex-shrink: 0;
     margin-left: auto;
     opacity: 0.6;
