165a54e353b1b2b1aa7b46aa38feb35d3fdc887f
Elena Stoeva <elena.stoeva@elastic.co>
2026-09-18 14:49:17 +0100

    Address Libra feedback

--- x-pack/platform/plugins/private/esql_views/public/plugin.test.ts
+++ x-pack/platform/plugins/private/esql_views/public/plugin.test.ts
@@ -5,6 +5,7 @@
  * 2.0.
  */
 
+import { act } from 'react-dom/test-utils';
 import { coreMock } from '@kbn/core/public/mocks';
 import { managementPluginMock } from '@kbn/management-plugin/public/mocks';
 import { PLUGIN_ID, PLUGIN_NAME } from '../common';
@@ -26,10 +27,13 @@ describe('EsqlViewsPlugin', () => {
     expect(management.sections.section.data.registerApp).not.toHaveBeenCalled();
   });
 
-  it('registers the management application when the UI is enabled', () => {
+  it('registers and mounts the management application when the UI is enabled', async () => {
+    const core = coreMock.createSetup();
+    const coreStart = coreMock.createStart();
     const management = managementPluginMock.createSetupContract();
+    core.getStartServices.mockResolvedValue([coreStart, {}, {}]);
 
-    createPlugin(true).setup(coreMock.createSetup(), { management });
+    createPlugin(true).setup(core, { management });
 
     expect(management.sections.section.data.registerApp).toHaveBeenCalledWith({
       id: PLUGIN_ID,
@@ -38,5 +42,30 @@ describe('EsqlViewsPlugin', () => {
       keywords: ['esql', 'views'],
       mount: expect.any(Function),
     });
+
+    const registerApp = management.sections.section.data.registerApp as jest.MockedFunction<
+      typeof management.sections.section.data.registerApp
+    >;
+    const [[registeredApp]] = registerApp.mock.calls;
+    const mountParams = {
+      ...coreMock.createAppMountParameters(),
+      basePath: '/',
+      setBreadcrumbs: jest.fn(),
+      theme: coreStart.theme,
+    };
+    let unmount: (() => void) | undefined;
+
+    await act(async () => {
+      unmount = await registeredApp.mount(mountParams);
+    });
+
+    expect(core.getStartServices).toHaveBeenCalledTimes(1);
+    expect(mountParams.setBreadcrumbs).toHaveBeenCalledWith([{ text: PLUGIN_NAME }]);
+    expect(
+      mountParams.element.querySelector('[data-test-subj="esqlViewsManagementPage"]')
+    ).not.toBe(null);
+
+    act(() => unmount?.());
+    expect(coreStart.chrome.docTitle.reset).toHaveBeenCalledTimes(1);
   });
 });
