180b134d8ce44876775daed2ad1ccfc78546a7a9
kibanamachine <42973632+kibanamachine@users.noreply.github.com>
2026-09-18 11:34:09 +0000

    Changes from node scripts/lint.js --fix && node scripts/eslint_all_files --no-cache --fix

--- x-pack/solutions/security/plugins/security_solution/public/entity_analytics/api/api.ts
+++ x-pack/solutions/security/plugins/security_solution/public/entity_analytics/api/api.ts
@@ -1005,7 +1005,7 @@ export const useEntityAnalyticsRoutes = () => {
       context,
     }: {
       signal?: AbortSignal;
-      params: { connectorId: string; };
+      params: { connectorId: string };
       context?: KibanaExecutionContext;
     }) =>
       http.fetch<GenerateLeadsResponse>(GENERATE_LEADS_URL, {
