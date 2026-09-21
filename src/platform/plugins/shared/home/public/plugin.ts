10dabea7b15969dbc8cf626388d3171e71c96f42
Anton Dosov <anton.dosov@elastic.co>
2026-09-21 15:49:26 +0200

    Test: import @kbn/developer-toolbar from home
    Temporary probe so CI can show no_group_crossing_imports rejecting a prod plugin importing a devOnly package. Revert after.

--- src/platform/plugins/shared/home/public/plugin.ts
+++ src/platform/plugins/shared/home/public/plugin.ts
@@ -14,6 +14,7 @@ import type {
   Plugin,
   PluginInitializerContext,
 } from '@kbn/core/public';
+import { DeveloperToolbar } from '@kbn/developer-toolbar';
 import { i18n } from '@kbn/i18n';
 
 import type { DataViewsPublicPluginStart } from '@kbn/data-views-plugin/public';
@@ -38,6 +39,8 @@ import {
   AddDataService,
   WelcomeService,
 } from './services';
+
+void DeveloperToolbar;
 
 export interface HomePluginStartDependencies {
   dataViews: DataViewsPublicPluginStart;
