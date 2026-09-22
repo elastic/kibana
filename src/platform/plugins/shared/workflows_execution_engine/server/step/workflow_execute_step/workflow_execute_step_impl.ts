dc90abd935a9dd78177a16d201fe279bd900ea5a
Sergi Massaneda <sergi.massaneda@elastic.co>
2026-09-22 12:19:12 +0200

    rollback unrelated change

--- src/platform/plugins/shared/workflows_execution_engine/server/step/workflow_execute_step/workflow_execute_step_impl.ts
+++ src/platform/plugins/shared/workflows_execution_engine/server/step/workflow_execute_step/workflow_execute_step_impl.ts
@@ -197,8 +197,7 @@ export class WorkflowExecuteStepImpl implements NodeImplementation, CancellableN
   }
 
   private async getWorkflow(workflowId: string): Promise<EsWorkflow | null> {
-    // GITIGNORE next line: This is a temporary fix to allow the workflow to be executed in a managed parent run.
-    const isManagedParentRun = true || this.isManagedParentExecution();
+    const isManagedParentRun = this.isManagedParentExecution();
     return this.init.workflowRepository.getWorkflow(workflowId, this.init.spaceId, {
       includeGlobal: isManagedParentRun,
       managedFilter: isManagedParentRun ? 'all' : 'unmanaged',
