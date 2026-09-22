ae9d0d0798758d41d8a0a8b96c5c1265cec83ad0
Timothy Sullivan <tsullivan@elastic.co>
2026-09-22 16:04:28 -0700

    expand header collapse tests

--- examples/flyout_system/public/components/_flyout_with_overlays.tsx
+++ examples/flyout_system/public/components/_flyout_with_overlays.tsx
@@ -118,6 +118,16 @@ const FillerContent: React.FC = () => (
       Aenean facilisis nulla vitae urna tincidunt congue sed ut dui. Morbi malesuada nulla nec purus
       convallis consequat. Vivamus id mollis quam. Morbi ac commodo nulla.
     </p>
+    <p>
+      Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo
+      pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris
+      eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a
+      elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id
+      tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque
+      malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque.
+      Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam
+      lacus eget erat.
+    </p>
   </EuiText>
 );
 
