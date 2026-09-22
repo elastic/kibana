3a939b949c205bb7475002d21ec4f0eb7aa137d8
Timothy Sullivan <tsullivan@elastic.co>
2026-09-22 16:03:49 -0700

    add a11y tests for footer action

--- examples/flyout_system/test/scout_examples/ui/fixtures/index.ts
+++ examples/flyout_system/test/scout_examples/ui/fixtures/index.ts
@@ -36,4 +36,4 @@ export const test = baseTest.extend<FlyoutSystemTestFixtures, ScoutWorkerFixture
 });
 
 export { PORTAL_SELECTOR } from './page_objects';
-export type { ChildLabel, FlyoutForm } from './page_objects';
+export type { ChildLabel, FlyoutForm, FlyoutSystemApp } from './page_objects';
