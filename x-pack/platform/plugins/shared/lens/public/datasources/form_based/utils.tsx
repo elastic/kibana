fdd80d87973ef1da83ed76d1469543de0aaab1db
Andreana Malama <andreana.malama@elastic.co>
2026-09-23 15:04:02 +0300

    Update warning message for custom rank last value sort field in Lens editor

--- x-pack/platform/plugins/shared/lens/public/datasources/form_based/utils.tsx
+++ x-pack/platform/plugins/shared/lens/public/datasources/form_based/utils.tsx
@@ -668,7 +668,7 @@ export function getCustomRankLastValueSortFieldWarningMessages(
           <>
             <FormattedMessage
               id="xpack.lens.indexPattern.terms.customRankLastValueMissingSortField"
-              defaultMessage="{name} ranks top values by their last value, but no sort field is set, so {field} is used by default. Edit in Lens editor to fix the error."
+              defaultMessage="{name} ranks top values by their last value, but no sort field is set, so {field} is used by default. Edit in the Lens editor to set a sort field."
               values={{
                 name: <strong>{columnName}</strong>,
                 field: <strong>{defaultField}</strong>,
