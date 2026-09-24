374ba80014bbf5d08828d7544ac9e989043fae23
Marco Antonio Ghiani <marcoantonio.ghiani01@gmail.com>
2026-09-24 12:47:58 +0200

    refactor: switch to nightshift privileges

--- x-pack/solutions/observability/plugins/significant_events/server/routes/internal/apps/slack/route.ts
+++ x-pack/solutions/observability/plugins/significant_events/server/routes/internal/apps/slack/route.ts
@@ -8,7 +8,7 @@
 import { z } from '@kbn/zod/v4';
 import { badGateway, conflict, forbidden, notFound } from '@hapi/boom';
 import { RelayRequestError } from '@kbn/actions-plugin/server';
-import { STREAMS_API_PRIVILEGES } from '@kbn/streams-plugin/common/constants';
+import { NIGHTSHIFT_API_PRIVILEGES } from '@kbn/nightshift-shared';
 import { createServerRoute } from '../../../create_server_route';
 import type {
   SlackAppBindChannelResponse,
@@ -30,7 +30,7 @@ const connectSlackAppRoute = createServerRoute({
   },
   security: {
     authz: {
-      requiredPrivileges: [STREAMS_API_PRIVILEGES.manage],
+      requiredPrivileges: [NIGHTSHIFT_API_PRIVILEGES.manage],
     },
   },
   params: z.object({}),
@@ -59,7 +59,7 @@ const statusSlackAppRoute = createServerRoute({
   },
   security: {
     authz: {
-      requiredPrivileges: [STREAMS_API_PRIVILEGES.read],
+      requiredPrivileges: [NIGHTSHIFT_API_PRIVILEGES.read],
     },
   },
   params: z.object({}),
@@ -78,7 +78,7 @@ const disconnectSlackAppRoute = createServerRoute({
   },
   security: {
     authz: {
-      requiredPrivileges: [STREAMS_API_PRIVILEGES.manage],
+      requiredPrivileges: [NIGHTSHIFT_API_PRIVILEGES.manage],
     },
   },
   params: z.object({}),
@@ -102,7 +102,7 @@ const bindingsSlackAppRoute = createServerRoute({
   },
   security: {
     authz: {
-      requiredPrivileges: [STREAMS_API_PRIVILEGES.read],
+      requiredPrivileges: [NIGHTSHIFT_API_PRIVILEGES.read],
     },
   },
   params: z.object({
@@ -139,7 +139,7 @@ const bindChannelSlackAppRoute = createServerRoute({
   },
   security: {
     authz: {
-      requiredPrivileges: [STREAMS_API_PRIVILEGES.manage],
+      requiredPrivileges: [NIGHTSHIFT_API_PRIVILEGES.manage],
     },
   },
   params: z.object({
@@ -166,7 +166,7 @@ const unbindChannelSlackAppRoute = createServerRoute({
   },
   security: {
     authz: {
-      requiredPrivileges: [STREAMS_API_PRIVILEGES.manage],
+      requiredPrivileges: [NIGHTSHIFT_API_PRIVILEGES.manage],
     },
   },
   params: z.object({
