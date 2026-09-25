772a76d56a1b1f19eda5e4a33052f5c79d8dca9d
Stratou <efstratia.kalafateli@elastic.co>
2026-09-25 08:04:26 +0000

    [ES|QL] Group shared esql packages under one directory (#292917)
    ## Summary
    
    Just moves our packages under the esql folder to be with the
    kbn-esql-browser package 💅
    
    ---------
    
    Co-authored-by: kibanamachine <42973632+kibanamachine@users.noreply.github.com>

--- src/platform/packages/shared/esql/kbn-esql-types/jest.config.js
+++ src/platform/packages/shared/esql/kbn-esql-types/jest.config.js
@@ -9,6 +9,6 @@
 
 module.exports = {
   preset: '@kbn/test',
-  rootDir: '../../../../..',
-  roots: ['<rootDir>/src/platform/packages/shared/kbn-esql-types'],
+  rootDir: '../../../../../..',
+  roots: ['<rootDir>/src/platform/packages/shared/esql/kbn-esql-types'],
 };
