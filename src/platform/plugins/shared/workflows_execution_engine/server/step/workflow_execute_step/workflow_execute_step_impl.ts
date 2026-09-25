5a22b9d281c04b550b0ce565b7fb9bf77da4c83c
ashokaditya <ashokaditya@elastic.co>
2026-09-25 09:36:42 +0200

    Revert "wip"
    This reverts commit 85e876a2d496dfd69958f90091a9884d84127da8.

--- src/platform/plugins/shared/workflows_execution_engine/server/step/workflow_execute_step/workflow_execute_step_impl.ts
+++ src/platform/plugins/shared/workflows_execution_engine/server/step/workflow_execute_step/workflow_execute_step_impl.ts
@@ -197,7 +197,7 @@ export class WorkflowExecuteStepImpl implements NodeImplementation, CancellableN
   }
 
   private async getWorkflow(workflowId: string): Promise<EsWorkflow | null> {
-    const isManagedParentRun = true; // this.isManagedParentExecution();
+    const isManagedParentRun = this.isManagedParentExecution();
     return this.init.workflowRepository.getWorkflow(workflowId, this.init.spaceId, {
       includeGlobal: isManagedParentRun,
       managedFilter: isManagedParentRun ? 'all' : 'unmanaged',
