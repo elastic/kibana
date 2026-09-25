f2d6e2397e2ca8f243964a2e710dcee188190891
Umberto Pepato <umberto.pepato@elastic.co>
2026-09-25 09:27:48 +0200

    Fix UIAM key test regression

--- x-pack/platform/test/serverless/api_integration/test_suites/alerting/summary_actions.ts
+++ x-pack/platform/test/serverless/api_integration/test_suites/alerting/summary_actions.ts
@@ -42,6 +42,16 @@ export default function ({ getService }: FtrProviderContext) {
   const alertingApi = getService('alertingApi');
   let roleAdmin: RoleCredentials;
 
+  const getRuleTags = async (ruleId: string): Promise<string[]> => {
+    const { body } = await supertest
+      .get(`/api/alerting/rule/${ruleId}`)
+      .set('kbn-xsrf', 'foo')
+      .set('x-elastic-internal-origin', 'foo')
+      .expect(200);
+
+    return body.tags;
+  };
+
   describe('Summary actions', function () {
     const RULE_TYPE_ID = '.es-query';
     const ALERT_ACTION_INDEX = 'alert-action-es-query';
@@ -171,6 +181,7 @@ export default function ({ getService }: FtrProviderContext) {
         },
       });
       expect(resp2.hits.hits.length).to.be(1);
+      const ruleTags = await getRuleTags(ruleId);
 
       const document = resp.hits.hits[0];
       expect(omit(document, '_source.date')._source).to.eql({
@@ -219,13 +230,10 @@ export default function ({ getService }: FtrProviderContext) {
         [ALERT_RULE_PRODUCER]: alertDocument[ALERT_RULE_PRODUCER],
         [ALERT_RULE_REVISION]: 0,
         [ALERT_RULE_TYPE_ID]: '.es-query',
-        // On MKI the rule is created with an ES API key and no UIAM key, so alerting adds the
-        // "Missing Elastic Cloud API Key" tag. Compare against what the create API returned
-        // instead of hardcoding [] so this holds both locally and on MKI.
-        [ALERT_RULE_TAGS]: createdRule.tags,
+        [ALERT_RULE_TAGS]: ruleTags,
         [ALERT_RULE_UUID]: ruleId,
         [SPACE_IDS]: ['default'],
-        [TAGS]: createdRule.tags,
+        [TAGS]: ruleTags,
         [ALERT_PENDING_RECOVERED_COUNT]: 0,
       });
     });
@@ -311,6 +319,7 @@ export default function ({ getService }: FtrProviderContext) {
         },
       });
       expect(resp2.hits.hits.length).to.be(1);
+      const ruleTags = await getRuleTags(ruleId);
 
       const document = resp.hits.hits[0];
       expect(omit(document, '_source.date')._source).to.eql({
@@ -359,10 +368,10 @@ export default function ({ getService }: FtrProviderContext) {
         [ALERT_RULE_PRODUCER]: alertDocument[ALERT_RULE_PRODUCER],
         [ALERT_RULE_REVISION]: 0,
         [ALERT_RULE_TYPE_ID]: '.es-query',
-        [ALERT_RULE_TAGS]: createdRule.tags,
+        [ALERT_RULE_TAGS]: ruleTags,
         [ALERT_RULE_UUID]: ruleId,
         [SPACE_IDS]: ['default'],
-        [TAGS]: createdRule.tags,
+        [TAGS]: ruleTags,
         [ALERT_PENDING_RECOVERED_COUNT]: 0,
       });
     });
@@ -525,6 +534,7 @@ export default function ({ getService }: FtrProviderContext) {
         },
       });
       expect(resp2.hits.hits.length).to.be(1);
+      const ruleTags = await getRuleTags(ruleId);
 
       const document = resp.hits.hits[0];
       expect(omit(document, '_source.date')._source).to.eql({
@@ -585,10 +595,10 @@ export default function ({ getService }: FtrProviderContext) {
         [ALERT_RULE_PRODUCER]: alertDocument[ALERT_RULE_PRODUCER],
         [ALERT_RULE_REVISION]: 0,
         [ALERT_RULE_TYPE_ID]: '.es-query',
-        [ALERT_RULE_TAGS]: createdRule.tags,
+        [ALERT_RULE_TAGS]: ruleTags,
         [ALERT_RULE_UUID]: ruleId,
         [SPACE_IDS]: ['default'],
-        [TAGS]: createdRule.tags,
+        [TAGS]: ruleTags,
         [ALERT_PENDING_RECOVERED_COUNT]: 0,
       });
     });
