4bdc1a5e1f667165f30f0f2536a03385866220d5
James Gowdy <jgowdy@elastic.co>
2026-09-25 16:06:24 +0100

    [8.19] [ML] Limiting detector list size for job estimation apis (#291203) (#293461)
    # Backport
    
    This will backport the following commits from `main` to `8.19`:
    - [[ML] Limiting detector list size for job estimation apis
    (#291203)](https://github.com/elastic/kibana/pull/291203)
    
    <!--- Backport version: 12.0.4 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)
    
    <!--BACKPORT [{"author":{"name":"James
    Gowdy","email":"jgowdy@elastic.co"},"sourceCommit":{"committedDate":"2026-09-25T09:14:43Z","message":"[ML]
    Limiting detector list size for job estimation apis (#291203)\n\nFixes a
    potential issue where a bad payload could be sent
    to\n`internal/ml/validate/estimate_bucket_span` causing excessive
    resource\nconsumption by the kibana server.\nBy limiting the number of
    detectors (`aggTypes` and `fields`) it\neliminates the risk of the loop
    inside the `BucketSpanEstimator` growing\ntoo large.\n\nTested using the
    instructions
    in\nhttps://github.com/elastic/security/issues/12940","sha":"22a54f90c44b9a773a01063157f31b001428c8f5","branchLabelMapping":{"^v9.6.0$":"main","^v(\\d+).(\\d+).\\d+$":"$1.$2"}},"sourcePullRequest":{"labels":["release_note:fix",":ml","Feature:Anomaly
    Detection","backport:version","v9.6.0","v9.5.5","v9.4.8","v8.19.23"],"title":"[ML]
    Limiting detector list size for job estimation
    apis","number":291203,"url":"https://github.com/elastic/kibana/pull/291203","mergeCommit":{"message":"[ML]
    Limiting detector list size for job estimation apis (#291203)\n\nFixes a
    potential issue where a bad payload could be sent
    to\n`internal/ml/validate/estimate_bucket_span` causing excessive
    resource\nconsumption by the kibana server.\nBy limiting the number of
    detectors (`aggTypes` and `fields`) it\neliminates the risk of the loop
    inside the `BucketSpanEstimator` growing\ntoo large.\n\nTested using the
    instructions
    in\nhttps://github.com/elastic/security/issues/12940","sha":"22a54f90c44b9a773a01063157f31b001428c8f5"}},"sourceBranch":"main","suggestedTargetBranches":["8.19"],"targetPullRequestStates":[{"branch":"main","label":"v9.6.0","branchLabelMappingKey":"^v9.6.0$","isSourceBranch":true,"state":"MERGED","url":"https://github.com/elastic/kibana/pull/291203","number":291203,"mergeCommit":{"message":"[ML]
    Limiting detector list size for job estimation apis (#291203)\n\nFixes a
    potential issue where a bad payload could be sent
    to\n`internal/ml/validate/estimate_bucket_span` causing excessive
    resource\nconsumption by the kibana server.\nBy limiting the number of
    detectors (`aggTypes` and `fields`) it\neliminates the risk of the loop
    inside the `BucketSpanEstimator` growing\ntoo large.\n\nTested using the
    instructions
    in\nhttps://github.com/elastic/security/issues/12940","sha":"22a54f90c44b9a773a01063157f31b001428c8f5"}},{"branch":"9.5","label":"v9.5.5","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"url":"https://github.com/elastic/kibana/pull/293440","number":293440,"state":"OPEN"},{"branch":"9.4","label":"v9.4.8","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"url":"https://github.com/elastic/kibana/pull/293458","number":293458,"state":"OPEN"},{"branch":"8.19","label":"v8.19.23","branchLabelMappingKey":"^v(\\d+).(\\d+).\\d+$","isSourceBranch":false,"state":"NOT_CREATED"}]}]
    BACKPORT-->

--- x-pack/platform/plugins/shared/ml/server/routes/schemas/job_validation_schema.ts
+++ x-pack/platform/plugins/shared/ml/server/routes/schemas/job_validation_schema.ts
@@ -23,12 +23,13 @@ export const estimateBucketSpanSchema = schema.object({
         schema.literal(ES_AGGREGATION.PERCENTILES),
         schema.literal(ES_AGGREGATION.CARDINALITY),
       ])
-    )
+    ),
+    { maxSize: 100 }
   ),
   duration: schema.object({ start: schema.number(), end: schema.number() }),
-  fields: schema.arrayOf(schema.nullable(schema.string())),
-  filters: schema.maybe(schema.arrayOf(schema.any())),
-  index: schema.string(),
+  fields: schema.arrayOf(schema.nullable(schema.string({ maxLength: 10000 })), { maxSize: 100 }),
+  filters: schema.maybe(schema.arrayOf(schema.any(), { maxSize: 10000 })),
+  index: schema.string({ maxLength: 10000 }),
   query: schema.any(),
   splitField: schema.maybe(schema.string()),
   timeField: schema.maybe(schema.string()),
