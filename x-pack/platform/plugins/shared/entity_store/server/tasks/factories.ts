7c2583c9097cc013291fa89fa95d9e98d15b46ca
Tiago Vila Verde <tiago.vilaverde@elastic.co>
2026-09-25 12:00:34 +0200

    Format rebased Entity Store task factory

--- x-pack/platform/plugins/shared/entity_store/server/tasks/factories.ts
+++ x-pack/platform/plugins/shared/entity_store/server/tasks/factories.ts
@@ -13,10 +13,7 @@ import { AssetManagerClient } from '../domain/asset_manager';
 import { LogsExtractionClient } from '../domain/logs_extraction';
 import { EngineDescriptorClient, EntityStoreGlobalStateClient } from '../domain/saved_objects';
 import type { TelemetryReporter } from '../telemetry/events';
-import {
-  getLegacySecurityAssetsMigrationFlag,
-  isDualProcessEnabled,
-} from '../infra/feature_flags';
+import { getLegacySecurityAssetsMigrationFlag, isDualProcessEnabled } from '../infra/feature_flags';
 
 export interface LogsExtractionClientFactoryResult {
   logsExtractionClient: LogsExtractionClient;
