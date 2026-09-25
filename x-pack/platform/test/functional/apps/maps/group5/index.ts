e19b9dfba0e9f8e93ac031bb8b2f70828d886108
Nathan Reese <reese.nathan@gmail.com>
2026-09-25 09:02:52 -0600

    convert layer_visibility FTR tests to unit tests

--- x-pack/platform/test/functional/apps/maps/group5/index.ts
+++ x-pack/platform/test/functional/apps/maps/group5/index.ts
@@ -62,6 +62,5 @@ export default function ({ loadTestFile, getService }: FtrProviderContext) {
     loadTestFile(require.resolve('./saved_object_management'));
     loadTestFile(require.resolve('./sample_data'));
     loadTestFile(require.resolve('./auto_fit_to_bounds'));
-    loadTestFile(require.resolve('./layer_visibility'));
   });
 }
